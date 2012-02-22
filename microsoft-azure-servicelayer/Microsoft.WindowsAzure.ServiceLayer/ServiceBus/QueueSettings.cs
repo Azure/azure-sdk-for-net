6ca763a966bdc24717245d7ed36c6f14db9b3340
Aliaksei Baturytski <aliakb@microsoft.com>
2012-02-22 09:35:00 -0800

    Updated file headers

--- microsoft-azure-servicelayer/Microsoft.WindowsAzure.ServiceLayer/ServiceBus/QueueSettings.cs
+++ microsoft-azure-servicelayer/Microsoft.WindowsAzure.ServiceLayer/ServiceBus/QueueSettings.cs
@@ -1,17 +1,17 @@
-﻿/*
- * Copyright 2012 Microsoft Corporation
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *    http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+﻿//
+// Copyright 2012 Microsoft Corporation
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 using System;
 using System.Collections.Generic;
@@ -51,11 +51,5 @@ namespace Microsoft.WindowsAzure.ServiceLayer.ServiceBus
 
         [DataMember(Order = 8)]
         public bool? EnableBatchedOperations { get; internal set; }
-
-        [DataMember(Order = 9)]
-        public int? SizeInBytes { get; internal set; }
-
-        [DataMember(Order = 10)]
-        public int? MessageCount { get; internal set; }
     }
 }
