e13a0e125d91ac74f5136061ea7bc52f4d69a22b
Aliaksei Baturytski <aliakb@microsoft.com>
2012-02-21 17:16:38 -0800

    Unit tests for queue management

---
+++ microsoft-azure-servicelayer/Microsoft.WindowsAzure.ServiceLayer.UnitTests/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.WindowsAzure.ServiceLayer.UnitTests
+{
+    static class Configuration
+    {
+        public static string ServiceNamespace { get { return "aliakb-test"; } }
+
+        public static string UserName { get { return "owner"; } }
+
+        public static string Password { get { return "0Myg+PyST7O5Bj+j9+Byjlljr4lomN1Y1QJR7D9JgZg="; } }
+    }
+}
