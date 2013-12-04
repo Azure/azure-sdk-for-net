b754db8059bbcda25161f35936eb17779220477a
Chris Tavares <ctavares@microsoft.com>
2013-12-04 09:30:58 -0800

    Generating source for compute management

--- src/ComputeManagement/Generated/ComputeManagementClient.cs
+++ src/ComputeManagement/Generated/ComputeManagementClient.cs
@@ -1,5 +1,5 @@
 // 
-// Copyright (c) Microsoft.  All rights reserved.
+// Copyright (c) Microsoft and contributors.  All rights reserved.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
