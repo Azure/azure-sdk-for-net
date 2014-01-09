ef99c70703f9a7225d31d8a4cd182faa228f3b69
Denis Stankovski <denis.stankovski@gmail.com>
2014-01-08 17:35:30 -0800

    Added Common.WindowsStore library and associated test project Created NuGet package for Windows8 version of the Common library

---
+++ src/Common.WindowsStore.Test/StoreAppTest.cs
@@ -0,0 +1,78 @@
+﻿//
+// Copyright (c) Microsoft.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Microsoft.WindowsAzure.Management;
+using Windows.Security.Cryptography.Certificates;
+using Windows.Storage;
+
+namespace Microsoft.WindowsAzure.Common.WindowsStoreApp.Test
+{
+    [TestClass]
+    public class StoreAppTest
+    {
+        private static string _certificateString;
+        private static string _subscription;
+        private static string _certificatePassword;
+        
+        [TestInitialize]
+        public void Setup()
+        {
+            GetCertificate().Wait();
+        }
+
+        [TestMethod]
+        public void ManagementClientReturnsLocationList()
+        {
+            // Import certificate
+            CertificateEnrollmentManager.ImportPfxDataAsync(_certificateString, _certificatePassword, ExportOption.NotExportable,
+                                                                KeyProtectionLevel.NoConsent, InstallOptions.None,
+                                                                "test").AsTask().Wait();
+
+            var credentials = new CertificateCloudCredentials(_subscription);
+            var client = new ManagementClient(credentials);
+            var result = client.Locations.List();
+
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            Assert.IsTrue(result.Locations.Count > 0);
+        }
+
+        private static async Task GetCertificate()
+        {
+            const string path = "certificate.keys";
+            var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var file = await folder.GetFileAsync(path);
+            var lines = await FileIO.ReadLinesAsync(file);
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("TEST_CERTIFICATE="))
+                {
+                    _certificateString = line.Substring("TEST_CERTIFICATE=".Length);
+                }
+                if (line.StartsWith("TEST_CERTIFICATE_PASSWORD="))
+                {
+                    _certificatePassword = line.Substring("TEST_CERTIFICATE_PASSWORD=".Length);
+                }
+                if (line.StartsWith("TEST_SUBSCRIPTION="))
+                {
+                    _subscription = line.Substring("TEST_SUBSCRIPTION=".Length);
+                }
+            }
+        }
+    }
+}
---
+++ src/Common.WindowsStore/Platform/CryptographyProvider.cs
@@ -0,0 +1,43 @@
+﻿//
+// Copyright (c) Microsoft.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+
+namespace Microsoft.WindowsAzure.Common.Platform
+{
+    internal class CryptographyProvider : ICryptographyProvider
+    {
+        /// <summary>
+        /// Computes SHA256 hash from key and data using HMACSHA256.
+        /// </summary>
+        /// <param name="key">Key to use as hash salt.</param>
+        /// <param name="data">Data to hash.</param>
+        /// <returns>Hash value.</returns>
+        public byte[] ComputeHmacSha256Hash(byte[] key, byte[] data)
+        {
+            MacAlgorithmProvider macAlgorithmProvider = MacAlgorithmProvider.OpenAlgorithm("HMAC_SHA256");
+            var dataBuffer = CryptographicBuffer.CreateFromByteArray(data);
+            var keyBuffer = CryptographicBuffer.CreateFromByteArray(key);
+            var hmacKey = macAlgorithmProvider.CreateKey(keyBuffer);
+            var hmacBuffer = CryptographicEngine.Sign(hmacKey, dataBuffer);
+
+            byte[] sha256Hash;
+            CryptographicBuffer.CopyToByteArray(hmacBuffer, out sha256Hash);
+
+            return sha256Hash;
+        }
+    }
+}
