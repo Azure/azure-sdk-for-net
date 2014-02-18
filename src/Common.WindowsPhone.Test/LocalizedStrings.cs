f313e62c24ef6c083cf4469e5b3772f9aa6eeae4
Denis Stankovski <denis.stankovski@gmail.com>
2014-02-18 10:04:23 -0800

    Added support for Windows Phone 8

---
+++ src/Common.WindowsPhone.Test/LocalizedStrings.cs
@@ -0,0 +1,29 @@
+﻿//
+// Copyright (c) Microsoft.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using Common.Phone.Test.Resources;
+
+namespace Common.Phone.Test
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
