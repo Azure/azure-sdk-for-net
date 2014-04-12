32b706b2147d2f22aad5cd01841c904adb716798
yugangw-msft <yugangw@microsoft.com>
2014-04-11 20:23:09 -0700

    code cleaning up, add comments

--- tools/SetupEnv.cmd
+++ tools/SetupEnv.cmd
@@ -2,17 +2,20 @@
 
 if defined SDKNetRoot exit /b 0
 
+echo Initializing environment
+
 if exist "%USERPROFILE%\SetNugetFeed.cmd" call "%USERPROFILE%\SetNugetFeed.cmd"
 
 if not defined PRIVATE_FEED_URL (
-    echo Error, please set following environment variables so build script can download Hydra Spec Nuget Packages:
-	echo     PRIVATE_FEED_URL, PRIVATE_FEED_USER_NAME and PRIVATE_FEED_PASSWORD
+    echo Error, please set following environment variables so that build script can download Spec Nuget Packages:
+    echo     PRIVATE_FEED_URL, PRIVATE_FEED_USER_NAME and PRIVATE_FEED_PASSWORD
 	exit /b 1
 )
 
 set "SDKNetRoot=%~dp0"
-set "SDKNetRoot=%SDKNetRoot:~0,-7%"
-echo Initializing environment
+::get rid of the \tools\
+set "SDKNetRoot=%SDKNetRoot:~0,-7%" 
+
 if defined ProgramFiles(x86) (
     set "ADXSDKProgramFiles=%ProgramFiles(x86)%"
 ) else (
