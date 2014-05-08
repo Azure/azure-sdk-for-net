a2f84ebca313a18a5e3265f31cd2bf665242948a
Denis Stankovski <denis.stankovski@gmail.com>
2014-05-07 17:22:01 -0700

    Updated AssemblyFileVersion to match NuGet package version

--- src/ComputeManagement/Properties/AssemblyInfo.cs
+++ src/ComputeManagement/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides management functionality for Windows Azure Virtual Machines and Hosted Services.")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
