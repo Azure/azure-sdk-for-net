a738224f4f5d9d0ef30e2c3083e4d54459eed9b2
bradygaster <brady.gaster@gmail.com>
2014-06-10 08:59:08 -0700

    bumping SQL client version number to prepare for publish

--- src/SqlManagement/Properties/AssemblyInfo.cs
+++ src/SqlManagement/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Provides management functionality for Windows Azure SQL in the cloud.")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
