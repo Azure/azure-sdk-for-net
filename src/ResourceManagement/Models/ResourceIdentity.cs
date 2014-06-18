3e94e7e966a780896a570f2e831ec291fb4683d8
Andre Rodrigues <andrerod@microsoft.com>
2014-06-18 12:57:37 -0400

    Adding friendlier constructor to resource identity

---
+++ src/ResourceManagement/Models/ResourceIdentity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Azure.Management.Resources.Models
+{
+    public partial class ResourceIdentity
+    {
+        public ResourceIdentity(string name, string resourceType, string apiVersion)
+        {
+            ResourceName = name;
+            ResourceProviderNamespace = GetProviderFromResourceType(resourceType);
+            ResourceType = GetTypeFromResourceType(resourceType);
+            ResourceProviderApiVersion = apiVersion;
+        }
+
+        public static string GetProviderFromResourceType(string resourceType)
+        {
+            if (resourceType == null)
+            {
+                return null;
+            }
+
+            int indexOfSlash = resourceType.IndexOf('/');
+            if (indexOfSlash < 0)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return resourceType.Substring(0, indexOfSlash);
+            }
+        }
+
+        public static string GetTypeFromResourceType(string resourceType)
+        {
+            if (resourceType == null)
+            {
+                return null;
+            }
+
+            int lastIndexOfSlash = resourceType.LastIndexOf('/');
+            if (lastIndexOfSlash < 0)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return resourceType.Substring(lastIndexOfSlash + 1);
+            }
+        }
+    }
+}
