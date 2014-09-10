35aea3a56069fc6dfe4f9e79852281f477b6d0d5
sichatro <sichatro@microsoft.com>
2014-09-09 18:00:19 -0700

    assembly version update

--- src/RedisCacheManagement/Properties/AssemblyInfo.cs
+++ src/RedisCacheManagement/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Provides Microsoft Azure redis cache management operations.")]
 
 [assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
