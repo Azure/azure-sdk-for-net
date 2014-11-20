7f37f53517d5796045835942e6bb270a4711d795
Harshitha Amit <harshiam@microsoft.com>
2014-11-19 16:11:49 -0800

    Create new sln for HDInsight and fix build scripts.

--- src/HDInsight/Tools/AzureStorageLogAnalysis/Downloader/LogDownloader.cs
+++ src/HDInsight/Tools/AzureStorageLogAnalysis/Downloader/LogDownloader.cs
@@ -1,14 +1,15 @@
-﻿namespace AzureLogTool
+﻿using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureLogTool
 {
     using System;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Globalization;
-    using System.IO;
     using System.Linq;
-    using Microsoft.WindowsAzure.Storage;
-    using Microsoft.WindowsAzure.Storage.Auth;
-    using Microsoft.WindowsAzure.Storage.Blob;
 
     /// <summary>
     /// Pulls down azure storage logs into a local filesystem cache.
