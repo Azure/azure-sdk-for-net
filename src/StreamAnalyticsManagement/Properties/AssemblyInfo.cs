36921b924fd0ec7fb0fd234b134a17081b622129
stankovski <denis.stankovski@gmail.com>
2015-01-06 07:41:22 -0800

    Updated AssemblyInfo.cs files

--- src/StreamAnalyticsManagement/Properties/AssemblyInfo.cs
+++ src/StreamAnalyticsManagement/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Provides Microsoft Azure Stream Analytics management operations.")]
 
 [assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.12.1.0")]
+[assembly: AssemblyFileVersion("0.13.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
