72d68188baa7b09d52dcb9d96fc9987493fe9c32
Yadav Bhattarai <yadavb@microsoft.com>
2015-02-24 16:01:28 -0800

    Azure RemoteApp OneSDK v 1.0.6

--- src/RemoteApp/Properties/AssemblyInfo.cs
+++ src/RemoteApp/Properties/AssemblyInfo.cs
@@ -1,16 +1,28 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+//
+// Copyright (c) Microsoft.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("RemoteApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Microsoft.Azure.Management.RemoteApp")]
+[assembly: AssemblyDescription("REST endpoint specification for Microsoft Azure RemoteApp service management.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RemoteApp")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Microsoft.Azure.Management.RemoteApp")]
+[assembly: AssemblyCopyright("Copyright � Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +43,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// [assembly: AssemblyVersion("1.0.0.0*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
