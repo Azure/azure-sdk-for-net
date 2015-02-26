67f1f3e8fb749cb032eaea607d287cf669c1f5bf
stankovski <denis.stankovski@gmail.com>
2015-02-26 09:29:37 -0800

    Added IDeserializationModel to AzureOperationResponse

--- src/Common/AzureOperationResponse.cs
+++ src/Common/AzureOperationResponse.cs
@@ -9,12 +9,30 @@ namespace Microsoft.Azure
     /// A standard service response including an HTTP status code and request
     /// ID.
     /// </summary>
-    public class AzureOperationResponse
+    public class AzureOperationResponse : IDeserializationModel
     {
         /// <summary>
         /// Gets or sets the value that uniquely identifies a request 
         /// made against the service.
         /// </summary>
         public string RequestId { get; set; }
+
+        /// <summary>
+        /// In an impementing class, deserialize the instance with data from the given Json Token.
+        /// </summary>
+        /// <param name="inputObject">The Json Token that contains serialized data.</param>
+        public virtual void DeserializeJson(Newtonsoft.Json.Linq.JToken inputObject)
+        {
+            // Do nothing
+        }
+
+        /// <summary>
+        /// In an implementing class, deserialize the instance with data from the given Xml Container.
+        /// </summary>
+        /// <param name="inputObject">The Xml Container containing the serialized data.</param>
+        public virtual void DeserializeXml(System.Xml.Linq.XContainer inputObject)
+        {
+            // Do nothing
+        }
     }
 }
