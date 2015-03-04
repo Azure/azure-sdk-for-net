0a703d2f80977be5cde92b796baed0e994c71523
Andrey Ilnitsky <ailn@microsoft.com>
2015-03-03 16:56:59 -0800

    increased package version

--- src/ApiManagementManagement/Properties/AssemblyInfo.cs
+++ src/ApiManagementManagement/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Provides Microsoft Azure API Management management operations.")]
 
 [assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
