bffa5362b6aa76c3be98fe66ca327ea77fc4b575
Vikas Hamine <vhamine@microsoft.com>
2015-03-13 08:37:48 -0700

    Fixing regression in GetCluster for IaaS clusters

--- src/HDInsight/Microsoft.WindowsAzure.Management.HDInsight.Contracts/Iaas/Jan2015/Credentials/SSHFullConnectivityCredential.cs
+++ src/HDInsight/Microsoft.WindowsAzure.Management.HDInsight.Contracts/Iaas/Jan2015/Credentials/SSHFullConnectivityCredential.cs
@@ -5,6 +5,7 @@ namespace Microsoft.WindowsAzure.Management.HDInsight.Contracts.Iaas.Jan2015
     /// <summary>
     /// Class representing a SSH credential containing both private and public keys
     /// </summary>
+    [DataContract(Namespace = Constants.HdInsightManagementNamespace)]
     internal class SSHFullConnectivityCredential : SSHConnectivityCredential
     {
         /// <summary>
