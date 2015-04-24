d06a02272517a72635dea5cf4bc3a4602a16faf8
jagadisk <jagadisk@microsoft.com>
2015-04-24 11:16:46 -0700

    Revert "WORK: 3587839 [SDK] Support storage analytic metric schema natively in Insights SDK to remove dependency on  on legacy infra"
    This reverts commit 0c8adf78f3e6afa70f1ea55cd9df5edbcfe3bab3.

--- src/Insights/Customizations/Shoebox/ShoeboxHelper.cs
+++ src/Insights/Customizations/Shoebox/ShoeboxHelper.cs
@@ -22,9 +22,6 @@ using System.Xml;
 
 namespace Microsoft.Azure.Insights
 {
-    /// <summary>
-    /// Helper class for shoebox operations
-    /// </summary>
     internal static class ShoeboxHelper
     {
         private const int KeyLimit = 432;
