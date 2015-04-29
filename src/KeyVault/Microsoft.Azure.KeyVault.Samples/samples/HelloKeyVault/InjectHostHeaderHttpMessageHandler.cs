fe1c56031323a10a49ba1be830d3e37adc729876
Greg Kostal <gnk_qcf@hotmail.com>
2015-04-29 15:30:45 -0700

    Added ability to override HOST header value for testing purposes

---
+++ src/KeyVault/Microsoft.Azure.KeyVault.Samples/samples/HelloKeyVault/InjectHostHeaderHttpMessageHandler.cs
@@ -0,0 +1,61 @@
+﻿// 
+// Copyright © Microsoft Corporation, All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+// ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+// PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache License, Version 2.0 for the specific language
+// governing permissions and limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+using System.Configuration;
+
+namespace Sample.Microsoft.HelloKeyVault
+{
+    public class InjectHostHeaderHttpMessageHandler : DelegatingHandler
+    {
+        /// <summary>
+        /// Adds the Host header to every request if the "KmsNetworkUrl" configuration setting is specified.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            var requestUri = request.RequestUri;
+            var authority = string.Empty;
+            var targetUri = requestUri;
+
+            // NOTE: The KmsNetworkUrl setting is purely for development testing on the
+            //       Microsoft Azure Development Fabric and should not be used outside that environment.
+            string networkUrl = ConfigurationManager.AppSettings["KmsNetworkUrl"];
+
+            if (!string.IsNullOrEmpty(networkUrl))
+            {
+                authority = targetUri.Authority;
+                targetUri = new Uri(new Uri(networkUrl), targetUri.PathAndQuery);
+
+                request.Headers.Add("Host", authority);
+                request.RequestUri = targetUri;
+            }
+
+            return base.SendAsync(request, cancellationToken).ContinueWith<HttpResponseMessage>(response =>
+            {
+                return response.Result;
+            });
+        }
+    }
+}
+
