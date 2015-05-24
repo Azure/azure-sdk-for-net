438e521713bdea009d22db9ea179d11cb20228ac
Nilambari <nilambari.deshpande@hotmail.com>
2015-05-23 23:56:48 -0700

    WORK: Recorded failing tests

--- src/ServiceManagement/Network/Network.Tests/Gateways/VirtualNetworkGatewayTests.cs
+++ src/ServiceManagement/Network/Network.Tests/Gateways/VirtualNetworkGatewayTests.cs
@@ -96,5 +96,91 @@ namespace Network.Tests.Gateways
                     "Virtual network: " + virtualNetworkSiteName + " gateway: " + vnetGatewayName + " is not deleted even after DeleteVirtualNetworkGateway API call!");
             }
         }
+
+        [Fact]
+        [Trait("Feature", "Gateways")]
+        [Trait("Operation", "VirtualNetworkGateway")]
+        public void VirtualNetworkGatewayStandardSkuValidationTest()
+        {
+            using (NetworkTestClient networkTestClient = new NetworkTestClient())
+            {
+                string virtualNetworkSiteName = "coexistenceVirtualNetworkSiteName";
+                string vnetGatewayName = "coexistenceVnetGateway";
+
+                networkTestClient.EnsureNoNetworkConfigurationExists(virtualNetworkSiteName);
+
+                // 1.CreateVirtualnetworkGateway API with Standard Gateway SKU
+                networkTestClient.SetNetworkConfiguration(NetworkTestConstants.CoexistenceFeatureNetworkConfigurationParameters);
+                NetworkListResponse listResponse = networkTestClient.ListNetworkConfigurations();
+                Assert.NotNull(listResponse);
+                Assert.True(listResponse.VirtualNetworkSites.Any(vnet => vnet.Name.Equals(virtualNetworkSiteName)),
+                    "Virtual network:" + virtualNetworkSiteName + " is not found!");
+                string vnetId = listResponse.First(vnet => vnet.Name.Equals(virtualNetworkSiteName)).Id;
+
+                GatewayGetOperationStatusResponse response =
+                    networkTestClient.Gateways.CreateVirtualNetworkGateway(
+                        virtualNetworkSiteName,
+                        new VirtualNetworkGatewayCreateParameters()
+                        {
+                            GatewayName = vnetGatewayName,
+                            GatewaySKU = GatewaySKU.Standard,
+                            GatewayType = GatewayType.DynamicRouting
+                        });
+                Assert.NotNull(response);
+                Assert.Equal(HttpStatusCode.OK, response.HttpStatusCode);
+
+                // 2.Listvirtualnetworkgateways API
+                ListVirtualNetworkGatewaysResponse listVirtualNetworkGatewaysResponse = networkTestClient.Gateways.ListVirtualNetworkGateways();
+                Assert.True(listVirtualNetworkGatewaysResponse.VirtualNetworkGateways.Count >= 1);
+                string vnetGatewayId = listVirtualNetworkGatewaysResponse.VirtualNetworkGateways.First(vnetGateway => vnetGateway.GatewayName.Equals(vnetGatewayName)).GatewayId.ToString();
+
+                // 3.Getvirtualnetworkgateway API
+                VirtualNetworkGatewayGetResponse virtualNetworkGatewayGetResponse = networkTestClient.Gateways.GetVirtualNetworkGateway(vnetGatewayId);
+                Assert.NotNull(virtualNetworkGatewayGetResponse);
+                Assert.Equal(HttpStatusCode.OK, virtualNetworkGatewayGetResponse.StatusCode);
+                Assert.Equal(vnetGatewayName, virtualNetworkGatewayGetResponse.GatewayName);
+                Assert.Equal(GatewaySKU.Standard, virtualNetworkGatewayGetResponse.GatewaySKU);
+                Assert.Equal(GatewayType.DynamicRouting, virtualNetworkGatewayGetResponse.GatewayType);
+
+                // 4.ResizeVirtualnetworkGateway API to HighPerformace Gateway SKU
+                ResizeGatewayParameters parameters = new ResizeGatewayParameters()
+                {
+                    GatewaySKU = GatewaySKU.HighPerformance
+                };
+                response = networkTestClient.Gateways.ResizeVirtualNetworkGateway(vnetGatewayId, parameters);
+                Assert.NotNull(response);
+                Assert.Equal(HttpStatusCode.OK, response.HttpStatusCode);
+
+                virtualNetworkGatewayGetResponse = networkTestClient.Gateways.GetVirtualNetworkGateway(vnetGatewayId);
+                Assert.NotNull(virtualNetworkGatewayGetResponse);
+                Assert.Equal(HttpStatusCode.OK, virtualNetworkGatewayGetResponse.StatusCode);
+                Assert.Equal(vnetGatewayName, virtualNetworkGatewayGetResponse.GatewayName);
+                Assert.Equal(GatewaySKU.HighPerformance, virtualNetworkGatewayGetResponse.GatewaySKU);
+
+                // 5.ResizeVirtualnetworkGateway API to Standard Gateway SKU again
+                parameters = new ResizeGatewayParameters()
+                {
+                    GatewaySKU = GatewaySKU.Standard
+                };
+                response = networkTestClient.Gateways.ResizeVirtualNetworkGateway(vnetGatewayId, parameters);
+                Assert.NotNull(response);
+                Assert.Equal(HttpStatusCode.OK, response.HttpStatusCode);
+
+                virtualNetworkGatewayGetResponse = networkTestClient.Gateways.GetVirtualNetworkGateway(vnetGatewayId);
+                Assert.NotNull(virtualNetworkGatewayGetResponse);
+                Assert.Equal(HttpStatusCode.OK, virtualNetworkGatewayGetResponse.StatusCode);
+                Assert.Equal(vnetGatewayName, virtualNetworkGatewayGetResponse.GatewayName);
+                Assert.Equal(GatewaySKU.Standard, virtualNetworkGatewayGetResponse.GatewaySKU);
+
+                // 6.DeleteVirtualNetworkGateway API
+                response = networkTestClient.Gateways.DeleteVirtualNetworkGateway(vnetGatewayId);
+                Assert.NotNull(response);
+                Assert.Equal(HttpStatusCode.OK, response.HttpStatusCode);
+
+                listVirtualNetworkGatewaysResponse = networkTestClient.Gateways.ListVirtualNetworkGateways();
+                Assert.False(listVirtualNetworkGatewaysResponse.VirtualNetworkGateways.Any(vnetGateway => vnetGateway.GatewayName.Equals(vnetGatewayName)),
+                    "Virtual network: " + virtualNetworkSiteName + " gateway: " + vnetGatewayName + " is not deleted even after DeleteVirtualNetworkGateway API call!");
+            }
+        }
     }
 }
