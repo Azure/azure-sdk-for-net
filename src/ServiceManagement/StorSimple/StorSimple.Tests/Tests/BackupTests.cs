5c9d00b641d61619a754f89ab09c8f03c5bd35a9
parvezah <parvezah@microsoft.com>
2015-05-25 13:36:23 +0530

    Making changes to fix the build

--- src/ServiceManagement/StorSimple/StorSimple.Tests/Tests/BackupTests.cs
+++ src/ServiceManagement/StorSimple/StorSimple.Tests/Tests/BackupTests.cs
@@ -1,9 +1,21 @@
-﻿using System.Linq;
-using Microsoft.Azure;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
 using Microsoft.WindowsAzure.Management.StorSimple;
 using Microsoft.Azure.Test;
 using Xunit;
-using System.Diagnostics;
 using Microsoft.WindowsAzure.Management.StorSimple.Models;
 using System;
 using System.Collections.Generic;
