57b79957d918d03dd7b8f2f13faf312fa5ac45b4
stankovski <denis.stankovski@gmail.com>
2015-05-26 17:45:53 -0700

    Generated Storage swagger json and client from swagger

--- src/ResourceManagement/Storage/StorageManagement/Generated/Models/StorageAccountCheckNameAvailabilityParameters.cs
+++ src/ResourceManagement/Storage/StorageManagement/Generated/Models/StorageAccountCheckNameAvailabilityParameters.cs
@@ -8,20 +8,17 @@ namespace Microsoft.Azure.Management.Storage.Models
 {
     /// <summary>
     /// </summary>
-    public partial class CustomDomain
+    public partial class StorageAccountCheckNameAvailabilityParameters
     {
         /// <summary>
-        /// Gets or sets the custom domain name. Name is the CNAME source.
         /// </summary>
         [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
 
         /// <summary>
-        /// Indicates whether indirect CName validation is enabled. Default
-        /// value is false. This should only be set on updates
         /// </summary>
-        [JsonProperty(PropertyName = "useSubDomain")]
-        public bool UseSubDomain { get; set; }
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
 
         /// <summary>
         /// Validate the object. Throws ArgumentException or ArgumentNullException if validation fails.
--- src/ResourceManagement/Storage/StorageManagement/Generated/Models/StorageAccountPropertiesCreateParametersJson.cs
+++ src/ResourceManagement/Storage/StorageManagement/Generated/Models/StorageAccountPropertiesCreateParametersJson.cs
@@ -8,20 +8,13 @@ namespace Microsoft.Azure.Management.Storage.Models
 {
     /// <summary>
     /// </summary>
-    public partial class CustomDomain
+    public partial class StorageAccountPropertiesCreateParametersJson
     {
         /// <summary>
-        /// Gets or sets the custom domain name. Name is the CNAME source.
+        /// Gets or sets the account type.
         /// </summary>
-        [JsonProperty(PropertyName = "name")]
-        public string Name { get; set; }
-
-        /// <summary>
-        /// Indicates whether indirect CName validation is enabled. Default
-        /// value is false. This should only be set on updates
-        /// </summary>
-        [JsonProperty(PropertyName = "useSubDomain")]
-        public bool UseSubDomain { get; set; }
+        [JsonProperty(PropertyName = "accountType")]
+        public string AccountType { get; set; }
 
         /// <summary>
         /// Validate the object. Throws ArgumentException or ArgumentNullException if validation fails.
