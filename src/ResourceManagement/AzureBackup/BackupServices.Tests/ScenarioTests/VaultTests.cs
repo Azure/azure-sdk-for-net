ceefa7230885f914db322ea961c0a1983370adbd
dragonfly91 <anudeeb@microsoft.com>
2015-06-12 19:36:01 +0530

    Including the session record for cert upload test

--- src/ResourceManagement/AzureBackup/BackupServices.Tests/ScenarioTests/VaultTests.cs
+++ src/ResourceManagement/AzureBackup/BackupServices.Tests/ScenarioTests/VaultTests.cs
@@ -11,7 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace BackupServices.Tests.ScenarioTests
+namespace BackupServices.Tests
 {
     public class VaultTests : BackupServicesTestsBase
     {
