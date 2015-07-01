4b1bafb4ec762854b12ada9548288fbe36a6d0c6
alextolp <alextolp@microsoft.com>
2015-06-30 19:05:55 -0700

    Moving activity runtime to management assembly

---
+++ src/ResourceManagement/DataFactory/DataFactoryManagement/Customizations/Runtime/ActivityLogger.cs
@@ -0,0 +1,50 @@
+﻿//
+// Copyright (c) Microsoft.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System;
+using System.Globalization;
+
+namespace Microsoft.Azure.Management.DataFactories.Runtime
+{
+    internal class ActivityLogger : IActivityLogger
+    {
+        private Action<string> logAction;
+
+        public ActivityLogger(Action<string> logAction)
+        {
+            this.logAction = logAction;
+        }
+
+        #region IActivityLogger implementation
+
+        public void Write(string format, params object[] args)
+        {
+            string value;
+
+            if (args != null && args.Length > 0)
+            {
+                value = string.Format(CultureInfo.InvariantCulture, format, args);
+            }
+            else
+            {
+                value = format;
+            }
+
+            this.logAction(value);
+        }
+
+        #endregion
+    }
+}
