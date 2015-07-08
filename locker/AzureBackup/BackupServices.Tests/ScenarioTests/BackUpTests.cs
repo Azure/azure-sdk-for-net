4aba273f60b7074e37b7756d65034e47beeb4bdc
dragonfly91 <anudeeb@microsoft.com>
2015-07-08 12:57:35 +0530

    Adding hydra test cases

---
+++ locker/AzureBackup/BackupServices.Tests/ScenarioTests/BackUpTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Management.BackupServices.Models;
+using Microsoft.Azure.Management.BackupServices;
+using Microsoft.Azure.Test;
+using System.Net;
+using Xunit;
+using System.Configuration;
+using System;
+
+namespace BackupServices.Tests
+{
+    public class BackUpTests : BackupServicesTestsBase
+    {
+        [Fact]
+        public void TriggerBackUpTest()
+        {
+            using (UndoContext context = UndoContext.Current)
+            {
+                context.Start();
+                var client = GetServiceClient<BackupServicesManagementClient>();
+
+                string containerName = ConfigurationManager.AppSettings["ContainerName"];
+                string dataSourceType = ConfigurationManager.AppSettings["DataSourceType"];
+                string dataSourceId = ConfigurationManager.AppSettings["DataSourceId"];
+
+                var response = client.BackUp.TriggerBackUp(GetCustomRequestHeaders(), containerName, dataSourceType, dataSourceId);
+                Assert.Equal(HttpStatusCode.Accepted, response.StatusCode);
+            }
+        }
+    }
+}
