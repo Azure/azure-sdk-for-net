b906e7fe7f9b80f1867ded8cdc63bf4d34632b3b
yugangw-msft <yugangw@microsoft.com>
2015-07-17 16:01:28 -0700

    remove net40 solution configurations from auto rest based libraries

--- tools/Microsoft.WindowsAzure.Build.Tasks/StrongNameUtility.cs
+++ tools/Microsoft.WindowsAzure.Build.Tasks/StrongNameUtility.cs
@@ -12,13 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.WindowsAzure.Build.Tasks
 {
