1a3e650df9b3c33c674f3da392fd757d6da5d6f7
Shefali <shvohra@microsoft.com>
2015-07-21 19:05:56 -0700

    move procdetails, netmapred, tools, internal, hadoop appliance

--- src/ServiceManagement/HDInsight/Tools/AzureStorageLogAnalysis/Properties/AssemblyInfo.cs
+++ src/ServiceManagement/HDInsight/Tools/AzureStorageLogAnalysis/Properties/AssemblyInfo.cs
@@ -6,13 +6,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("AzureLogAnalysis")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("AzureLogAnalysis")]
-[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -23,17 +17,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("a1beebd8-c17a-4a23-8853-be7321f4da87")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: AssemblyFileVersion("1.0.0.21201")]
