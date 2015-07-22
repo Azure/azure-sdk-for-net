effb8a7955e25863c84ff2e445756c5d678cbc8b
panbha <panbha@microsoft.com>
2015-07-22 23:53:32 +0530

    restorecamel

--- src/ResourceManagement/AzureBackup/BackupServicesManagement/Generated/RestoreOperations.cs
+++ src/ResourceManagement/AzureBackup/BackupServicesManagement/Generated/RestoreOperations.cs
@@ -122,7 +122,7 @@ namespace Microsoft.Azure.Management.BackupServices
             
             // Construct URL
             string url = "";
-            url = url + "/Subscriptions/";
+            url = url + "/subscriptions/";
             if (this.Client.Credentials.SubscriptionId != null)
             {
                 url = url + Uri.EscapeDataString(this.Client.Credentials.SubscriptionId);
