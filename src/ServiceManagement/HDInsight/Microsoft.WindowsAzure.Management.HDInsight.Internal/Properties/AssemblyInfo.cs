5e377ab8995df3852ed8a25bd517ff1f6519a523
Shefali <shvohra@microsoft.com>
2015-07-21 19:54:20 -0700

    update assembly infos

--- src/ServiceManagement/HDInsight/Microsoft.WindowsAzure.Management.HDInsight.Internal/Properties/AssemblyInfo.cs
+++ src/ServiceManagement/HDInsight/Microsoft.WindowsAzure.Management.HDInsight.Internal/Properties/AssemblyInfo.cs
@@ -34,3 +34,9 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0e70a436-c58d-4bc9-8c18-496efc7b469c")]
+
+#if CODESIGN
+[assembly: InternalsVisibleTo("Microsoft.WindowsAzure.Management.HDInsight.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002592552baf0c4819857c12145baeaea2e7191d00874d9df03d9b3deec1efcca0ae56fbcba8e8aea60c29c84e7679269e020c32c7efd5cd93a821caa0d0e9a7247623396cc1c280133caefcc8d585901f709ba3fbd96628e6554678b7b1dcc446748433b00599efdb447cb8fd35ef318be24fe5728fe14fcfb67b85197c049fdb")]
+#else
+[assembly: InternalsVisibleTo("Microsoft.WindowsAzure.Management.HDInsight.Tests")]
+#endif
