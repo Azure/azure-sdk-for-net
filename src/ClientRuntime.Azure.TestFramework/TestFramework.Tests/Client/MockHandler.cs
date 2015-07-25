a49101df1e8ec70d2e4e5d42c90de5c96c3fd54e
stankovski <denis.stankovski@gmail.com>
2015-07-24 17:07:04 -0700

    Updated ClientRuntime to latest version and fixed TestFramework.Tests

--- src/ClientRuntime.Azure.TestFramework/TestFramework.Tests/Client/MockHandler.cs
+++ src/ClientRuntime.Azure.TestFramework/TestFramework.Tests/Client/MockHandler.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Azure;
+using Microsoft.Rest.Azure;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using System;
 using System.Net.Http;
