89f39ae950d76e83f1b0ad16d15334e07cd1038e
yugangw-msft <yugangw@microsoft.com>
2015-07-30 14:52:27 -0700

    update code-gen script and produce mit license header

--- src/ResourceManagement/Storage/StorageManagement/generate.cmd
+++ src/ResourceManagement/Storage/StorageManagement/generate.cmd
@@ -2,16 +2,16 @@
 :: Microsoft Azure SDK for Net - Generate library code
 :: Copyright (C) Microsoft Corporation. All Rights Reserved.
 ::
- 
-set autoRestVersion=0.11.0-Nightly20150727
-set specUrl="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-storage/2015-05-01-preview/swagger/storage.json?token=AFvFAVctRHBXqAdfxX2kk3jAvsCrCqxXks5Vv-IDwA=="
-set source=-Source https://www.myget.org/F/autorest/api/v2
 
+@echo off
+set autoRestVersion=0.11.0-Nightly20150727
+if  "%1" == "" (
+    set specFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-storage/2015-05-01-preview/swagger/storage.json?token=AFvFAVctRHBXqAdfxX2kk3jAvsCrCqxXks5Vv-IDwA=="
+) else (
+    set specFile="%1"
+)
 set repoRoot=%~dp0..\..\..\..
-set autoRestExe=%repoRoot%\packages\autorest.%autoRestVersion%\tools\AutoRest.exe
 set generateFolder=%~dp0Generated
 
-%repoRoot%\tools\nuget.exe install autorest %source% -Version %autoRestVersion% -o %repoRoot%\packages
-
 if exist %generateFolder% rd /S /Q  %generateFolder%
-%autoRestExe% -Modeler Swagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Management.Storage -Input %specUrl% -outputDirectory %generateFolder% -Header NONE
+call "%repoRoot%\tools\autorest.gen.cmd" %specFile% Microsoft.Azure.Management.Storage %autoRestVersion% %generateFolder% 
