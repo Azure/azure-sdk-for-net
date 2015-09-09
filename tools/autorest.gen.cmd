00f5bab0e00adbaaa4e354100776600a43972542
huangpf <phuang@microsoft.com>
2015-09-08 17:05:02 -0700

    address CR

--- tools/autorest.gen.cmd
+++ tools/autorest.gen.cmd
@@ -8,7 +8,6 @@ set source=-Source https://www.myget.org/F/autorest/api/v2
 
 set repoRoot=%~dp0..
 set autoRestExe=%repoRoot%\packages\autorest.%autoRestVersion%\tools\AutoRest.exe
-REM set autoRestExe=%repoRoot%\..\autorest\binaries\net45\AutoRest.exe
 
 %repoRoot%\tools\nuget.exe install autorest %source% -Version %autoRestVersion% -o %repoRoot%\packages -verbosity quiet
 
