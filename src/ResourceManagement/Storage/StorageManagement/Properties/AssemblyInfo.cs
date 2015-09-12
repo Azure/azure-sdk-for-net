0bd46d0885ab3e2579ae9feb01266d6d2fb65371
huangpf <phuang@microsoft.com>
2015-09-11 18:11:06 -0700

    move

--- src/ResourceManagement/Storage/StorageManagement/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/Storage/StorageManagement/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Provides Microsoft Azure Storage management functions for managing the Microsoft Azure Storage service.")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.4.1.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
