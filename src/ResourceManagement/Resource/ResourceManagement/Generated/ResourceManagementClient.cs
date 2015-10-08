e2664bb66e6a204b5524fed7e82dae5aa33b5c37
vivsriaus <visriniv@microsoft.com>
2015-10-08 10:46:40 -0700

    Add request body to move

--- src/ResourceManagement/Resource/ResourceManagement/Generated/ResourceManagementClient.cs
+++ src/ResourceManagement/Resource/ResourceManagement/Generated/ResourceManagementClient.cs
@@ -436,11 +436,11 @@ namespace Microsoft.Azure.Management.Resources
                     {
                         result.Status = OperationStatus.Failed;
                     }
-                    if (statusCode == HttpStatusCode.NoContent)
+                    if (statusCode == HttpStatusCode.OK)
                     {
                         result.Status = OperationStatus.Succeeded;
                     }
-                    if (statusCode == HttpStatusCode.OK)
+                    if (statusCode == HttpStatusCode.NoContent)
                     {
                         result.Status = OperationStatus.Succeeded;
                     }
