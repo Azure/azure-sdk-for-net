971e7bef93804cfbffbb8ba3c9370b8fde1ffaf2
Shefali <shvohra@microsoft.com>
2015-10-20 13:24:39 -0700

    remove test

--- src/ResourceManagement/HDInsightJob/HDInsightJob.Tests/ScenarioTests/StorageHelperTests.cs
+++ src/ResourceManagement/HDInsightJob/HDInsightJob.Tests/ScenarioTests/StorageHelperTests.cs
@@ -25,7 +25,7 @@ namespace HDInsightJob.Tests
 {
     public class StorageHelperTests
     {
-        [Fact]
+        //[Fact]
         public void GetJobOutput()
         {
             using (var context = UndoContext.Current)
