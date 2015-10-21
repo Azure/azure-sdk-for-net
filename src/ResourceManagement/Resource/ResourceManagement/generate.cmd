988475a66a1d3b72975b4dcf29e87fbb29af2024
stankovski <denis.stankovski@gmail.com>
2015-10-21 16:50:57 -0700

    Updated package version and removed BCL dependency

--- src/ResourceManagement/Resource/ResourceManagement/generate.cmd
+++ src/ResourceManagement/Resource/ResourceManagement/generate.cmd
@@ -5,7 +5,7 @@
 
 @echo off
 setlocal
-set autoRestVersion=0.11.0-Nightly20150727
+set autoRestVersion=0.11.0-Nightly20151021
 set source=-Source https://www.myget.org/F/autorest/api/v2
 
 set resSpecFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-resources/2014-04-01-preview/swagger/resources.json"
