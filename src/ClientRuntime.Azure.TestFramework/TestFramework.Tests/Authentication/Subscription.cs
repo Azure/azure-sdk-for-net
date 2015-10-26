6eed06e4fcfe0e671964054c5d01eeb838f0f5f8
markcowl <markcowl@microsoft.com>
2015-10-26 14:53:16 -0700

    Fixing merge issue in csrpoj and responding to feedback

--- src/ClientRuntime.Azure.TestFramework/TestFramework.Tests/Authentication/Subscription.cs
+++ src/ClientRuntime.Azure.TestFramework/TestFramework.Tests/Authentication/Subscription.cs
@@ -1,18 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Linq;
-using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using Microsoft.Azure.Test.HttpRecorder;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework.Test.Client;
 using System;
 using System.Net;
 using Xunit;
-using Xunit.Extensions;
 
 namespace Microsoft.Rest.ClientRuntime.Azure.TestFramework.Test.Authentication
 {
-        [Collection("SerialCollection1")]
+    [Collection("SerialCollection1")]
     public class Subscription : TestBase, IDisposable
     {
         private string TEST_CONNECTION_STRING;
