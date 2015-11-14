7fa11ab56010dbe02f2d8a2c38f07eb34a52fc73
Venkata Ramanamurthy Chandaka <vrmurthy@microsoft.com>
2015-11-13 18:02:50 -0800

    Removed applicationhost.config & commented some environment initialization

--- src/ResourceManagement/Intune/Intune.Tests/Helpers/IntuneClientHelper.cs
+++ src/ResourceManagement/Intune/Intune.Tests/Helpers/IntuneClientHelper.cs
@@ -69,11 +69,10 @@ namespace Microsoft.Azure.Management.Intune.Tests.Helpers
         }
 
         internal static void InitializeEnvironment()
-        {
-            //Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", "SubscriptionId=3e9e6f07-6225-4d10-8fd4-5f0236c28f5a;Environment=Dogfood;UserId=admin@aad296.ccsctp.net;Password=Pa$$w0rd");
-            Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", "SubscriptionId=None;Environment=Dogfood;UserId=admin@uxmdmonly.ccsctp.net;Password=P0rsche911");
+        {   //Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", "SubscriptionId=3e9e6f07-6225-4d10-8fd4-5f0236c28f5a;Environment=Dogfood;UserId=admin@aad296.ccsctp.net;Password=XXXXXXX");
+            //Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", "SubscriptionId=None;Environment=Dogfood;UserId=admin@uxmdmonly.ccsctp.net;Password=XXXXXX");         
             //AZURE_TEST_MODE = None, Record, Playback
-            Environment.SetEnvironmentVariable("AZURE_TEST_MODE", "Playback");
+            //Environment.SetEnvironmentVariable("AZURE_TEST_MODE", "Playback");
         }
     }
 }
