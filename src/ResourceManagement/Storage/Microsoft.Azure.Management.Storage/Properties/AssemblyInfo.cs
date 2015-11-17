8761f7d6da6a28f8ea86c3bba4bac5d57150c1a1
yugangw-msft <yugangw@microsoft.com>
2015-11-16 21:15:25 -0800

    update version, and target to net45

--- src/ResourceManagement/Storage/Microsoft.Azure.Management.Storage/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/Storage/Microsoft.Azure.Management.Storage/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure Storage Management Library")]
 [assembly: AssemblyDescription("Provides Microsoft Azure Storage management functions for managing the Microsoft Azure Storage service.")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
