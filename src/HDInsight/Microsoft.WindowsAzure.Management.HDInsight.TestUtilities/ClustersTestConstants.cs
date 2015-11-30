82ae61d00f90d88fdcc65c9e0337f8d0b1492ade
yugangw-msft <yugangw@microsoft.com>
2015-11-30 11:53:54 -0800

    remove uselss project as we decided AutoRest branch will not merge back to master

--- src/HDInsight/Microsoft.WindowsAzure.Management.HDInsight.TestUtilities/ClustersTestConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace Microsoft.WindowsAzure.Management.HDInsight.TestUtilities
-{
-    public static class ClustersTestConstants
-    {
-        public const string NotSupportedBySubscriptionException = "The current subscription does not support this operation.";
-        public const string NoDataNodeException = "The cluster does not have a data node.";
-        public const string ClusterDoesNotExistException = "Cluster {0} for subscription {1} does not exist.";
-    }
-}
