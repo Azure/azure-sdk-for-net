c2800735bba885b633cfc6f29027fc52c00d8e55
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2015-12-17 14:43:26 -0800

    Regenerated Graph.Rbac SDK's

--- src/ResourceManagement/Graph.RBAC/Microsoft.Azure.Graph.RBAC/generate.cmd
+++ src/ResourceManagement/Graph.RBAC/Microsoft.Azure.Graph.RBAC/generate.cmd
@@ -4,7 +4,7 @@
 ::
 
 @echo off
-set autoRestVersion=0.13.0-Nightly20151115
+set autoRestVersion=0.14.0-Nightly20151216
 if  "%1" == "" (
     set specFile="https://raw.githubusercontent.com/stankovski/azure-rest-api-specs/master/arm-graphrbac/1.42-previewInternal/swagger/graphrbac.json"
 ) else (
