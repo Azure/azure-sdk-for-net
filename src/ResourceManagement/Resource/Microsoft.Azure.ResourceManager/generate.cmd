d6356b05945f689a86e874b3c61daa4d7156cb31
stankovski <denis.stankovski@gmail.com>
2015-12-23 14:14:52 -0800

    Updated all packages to use Azure ClientRuntime 2.5.4 and regenerated ResourceManagement package

--- src/ResourceManagement/Resource/Microsoft.Azure.ResourceManager/generate.cmd
+++ src/ResourceManagement/Resource/Microsoft.Azure.ResourceManager/generate.cmd
@@ -5,7 +5,7 @@
 
 @echo off
 setlocal
-set autoRestVersion=0.13.0-Nightly20151209
+set autoRestVersion=0.14.0-Nightly20151223
 set source=-Source https://www.myget.org/F/autorest/api/v2
 
 set resSpecFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-resources/resources/2014-04-01-preview/swagger/resources.json"
