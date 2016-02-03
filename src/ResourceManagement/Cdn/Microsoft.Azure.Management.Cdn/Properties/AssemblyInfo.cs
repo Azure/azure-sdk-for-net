4abacf43a2d2b26d5217fde3e611a56474d50f50
Injy Zarif <inzarif@microsoft.com>
2016-02-03 15:10:49 -0800

    Update SDK Version
    Update version in Project.json and AssemblyInfo.cs

--- src/ResourceManagement/Cdn/Microsoft.Azure.Management.Cdn/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/Cdn/Microsoft.Azure.Management.Cdn/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure CDN.")]
 
 [assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Azure .NET SDK")]
