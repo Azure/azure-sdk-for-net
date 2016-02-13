834c0396529931a29e1765bf0b8e6bc23f4729e6
huangpf <phuang@microsoft.com>
2016-02-12 16:31:12 -0800

    Use Composite Swagger for Compute

--- src/ResourceManagement/Compute/Microsoft.Azure.Management.Compute/generate.cmd
+++ src/ResourceManagement/Compute/Microsoft.Azure.Management.Compute/generate.cmd
@@ -4,9 +4,9 @@
 ::
 
 @echo off
-set autoRestVersion=0.14.0-Nightly20160125
+set autoRestVersion=0.15.0-Nightly20160212
 if  "%1" == "" (
-    set specFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-compute/2015-06-15/swagger/compute.json"
+    set specFile="ComputeCompositeClient.json"
 ) else (
     set specFile="%1"
 )
@@ -14,4 +14,4 @@ set repoRoot=%~dp0..\..\..\..
 set generateFolder=%~dp0Generated
 
 if exist %generateFolder% rd /S /Q  %generateFolder%
-call "%repoRoot%\tools\autorest.gen.cmd" %specFile% Microsoft.Azure.Management.Compute %autoRestVersion% %generateFolder% "-FT 1"
+call "%repoRoot%\tools\autorest.composite.gen.cmd" %specFile% Microsoft.Azure.Management.Compute %autoRestVersion% %generateFolder% "-FT 1"
