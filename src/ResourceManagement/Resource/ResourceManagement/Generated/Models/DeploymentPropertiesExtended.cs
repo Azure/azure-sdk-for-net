c1f88b5898182b3973b9ce77c035b303e7a11340
vivsriaus <visriniv@microsoft.com>
2016-02-16 12:44:37 -0800

    Update RM client api version

--- src/ResourceManagement/Resource/ResourceManagement/Generated/Models/DeploymentPropertiesExtended.cs
+++ src/ResourceManagement/Resource/ResourceManagement/Generated/Models/DeploymentPropertiesExtended.cs
@@ -54,6 +54,17 @@ namespace Microsoft.Azure.Management.Resources.Models
             set { this._dependencies = value; }
         }
         
+        private TimeSpan _duration;
+        
+        /// <summary>
+        /// Optional. Gets or sets the duration of the deployment.
+        /// </summary>
+        public TimeSpan Duration
+        {
+            get { return this._duration; }
+            set { this._duration = value; }
+        }
+        
         private string _outputs;
         
         /// <summary>
@@ -100,6 +111,17 @@ namespace Microsoft.Azure.Management.Resources.Models
             set { this._timestamp = value; }
         }
         
+        private IList<DeploymentPreFlightResource> _validatedResources;
+        
+        /// <summary>
+        /// Optional. Gets the list of validated resources.
+        /// </summary>
+        public IList<DeploymentPreFlightResource> ValidatedResources
+        {
+            get { return this._validatedResources; }
+            set { this._validatedResources = value; }
+        }
+        
         /// <summary>
         /// Initializes a new instance of the DeploymentPropertiesExtended
         /// class.
@@ -108,6 +130,7 @@ namespace Microsoft.Azure.Management.Resources.Models
         {
             this.Dependencies = new LazyList<Dependency>();
             this.Providers = new LazyList<Provider>();
+            this.ValidatedResources = new LazyList<DeploymentPreFlightResource>();
         }
     }
 }
