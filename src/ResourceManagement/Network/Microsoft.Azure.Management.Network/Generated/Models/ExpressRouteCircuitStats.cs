05b803caf21238ae6d88c94c3c6bd985e27d3a64
dihan <dihan@microsoft.com>
2016-03-22 18:49:58 -0700

    Co exist API with new api version

--- src/ResourceManagement/Network/Microsoft.Azure.Management.Network/Generated/Models/ExpressRouteCircuitStats.cs
+++ src/ResourceManagement/Network/Microsoft.Azure.Management.Network/Generated/Models/ExpressRouteCircuitStats.cs
@@ -29,23 +29,37 @@ namespace Microsoft.Azure.Management.Network.Models
         /// <summary>
         /// Initializes a new instance of the ExpressRouteCircuitStats class.
         /// </summary>
-        public ExpressRouteCircuitStats(int? bytesIn = default(int?), int? bytesOut = default(int?))
+        public ExpressRouteCircuitStats(int? primarybytesIn = default(int?), int? primarybytesOut = default(int?), int? secondarybytesIn = default(int?), int? secondarybytesOut = default(int?))
         {
-            BytesIn = bytesIn;
-            BytesOut = bytesOut;
+            PrimarybytesIn = primarybytesIn;
+            PrimarybytesOut = primarybytesOut;
+            SecondarybytesIn = secondarybytesIn;
+            SecondarybytesOut = secondarybytesOut;
         }
 
         /// <summary>
         /// Gets BytesIn of the peering.
         /// </summary>
-        [JsonProperty(PropertyName = "bytesIn")]
-        public int? BytesIn { get; set; }
+        [JsonProperty(PropertyName = "primarybytesIn")]
+        public int? PrimarybytesIn { get; set; }
 
         /// <summary>
         /// Gets BytesOut of the peering.
         /// </summary>
-        [JsonProperty(PropertyName = "bytesOut")]
-        public int? BytesOut { get; set; }
+        [JsonProperty(PropertyName = "primarybytesOut")]
+        public int? PrimarybytesOut { get; set; }
+
+        /// <summary>
+        /// Gets BytesIn of the peering.
+        /// </summary>
+        [JsonProperty(PropertyName = "secondarybytesIn")]
+        public int? SecondarybytesIn { get; set; }
+
+        /// <summary>
+        /// Gets BytesOut of the peering.
+        /// </summary>
+        [JsonProperty(PropertyName = "secondarybytesOut")]
+        public int? SecondarybytesOut { get; set; }
 
     }
 }
