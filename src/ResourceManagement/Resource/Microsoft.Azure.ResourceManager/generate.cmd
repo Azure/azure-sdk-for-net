d7a23e76cf0b7a97eaf93dd8e41be21dc04d26da
vivsriaus <visriniv@microsoft.com>
2016-04-15 18:02:15 -0700

    Revert "Change api-version of partner tests"
    This reverts commit 2d7d10264e1dc48348cc5c856c43858d7af5058a.

--- src/ResourceManagement/Resource/Microsoft.Azure.ResourceManager/generate.cmd
+++ src/ResourceManagement/Resource/Microsoft.Azure.ResourceManager/generate.cmd
@@ -8,13 +8,11 @@ setlocal
 set autoRestVersion=0.14.0-Nightly20160125
 set source=-Source https://www.myget.org/F/autorest/api/v2
 
-::set resSpecFile="https://raw.githubusercontent.com/vivsriaus/azure-rest-api-specs/deploymentdebugSetting/arm-resources/resources/2016-02-01/swagger/resources.json"
-set resSpecFile="https://raw.githubusercontent.com/vivsriaus/azure-rest-api-specs/AddPolicySpec/arm-resources/resources/2016-02-01/swagger/resources.json"
-set lockSpecFile="https://raw.githubusercontent.com/vivsriaus/azure-rest-api-specs/AddPolicySpec/arm-resources/locks/2015-01-01/swagger/locks.json"
+set resSpecFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-resources/resources/2016-02-01/swagger/resources.json"
+set lockSpecFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-resources/locks/2015-01-01/swagger/locks.json"
 set featureSpecFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-resources/features/2015-12-01/swagger/features.json"
 set subscriptionSpecFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-resources/subscriptions/2015-11-01/swagger/subscriptions.json"
-::set policySpecFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-resources/policy/2015-10-01-preview/swagger/policy.json"
-set policySpecFile="https://raw.githubusercontent.com/vivsriaus/azure-rest-api-specs/AddPolicySpec/arm-resources/policy/2015-10-01-preview/swagger/policy.json"
+set policySpecFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/master/arm-resources/policy/2015-10-01-preview/swagger/policy.json"
 
 set repoRoot=%~dp0..\..\..\..
 set generateFolder=%~dp0Generated
