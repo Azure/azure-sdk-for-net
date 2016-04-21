44ea67ceecba164c8c533b7b76d693e01a7f4b73
Hervey Wilson <herveyw@microsoft.com>
2016-04-21 11:17:57 -0700

    Initial Cryptography port

---
+++ src/KeyVault/Microsoft.Azure.KeyVault.Cryptography/Algorithm.cs
@@ -0,0 +1,45 @@
+﻿//
+// Copyright © Microsoft Corporation, All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+// ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+// PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache License, Version 2.0 for the specific language
+// governing permissions and limitations under the License.
+
+using System;
+
+namespace Microsoft.Azure.KeyVault.Cryptography
+{
+    /// <summary>
+    /// Abstract Algorithm.
+    /// </summary>
+    public abstract class Algorithm 
+    {
+        private readonly string _name;
+
+        protected Algorithm( string name )
+        {
+            if ( string.IsNullOrEmpty( name ) )
+                throw new ArgumentNullException( "name" );
+
+            _name = name;
+        }
+
+        /// <summary>
+        /// The name of the algorithm
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+}
---
+++ src/KeyVault/Microsoft.Azure.KeyVault.Cryptography/KeyWrapAlgorithm.cs
@@ -0,0 +1,48 @@
+﻿//
+// Copyright © Microsoft Corporation, All Rights Reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+// ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
+// PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache License, Version 2.0 for the specific language
+// governing permissions and limitations under the License.
+
+using System.Security.Cryptography;
+
+namespace Microsoft.Azure.KeyVault.Cryptography
+{
+    /// <summary>
+    /// An abstract Key Wrap Algorithm
+    /// </summary>
+    public abstract class KeyWrapAlgorithm : Algorithm
+    {
+        protected KeyWrapAlgorithm( string name )
+            : base( name )
+        {
+        }
+
+        /// <summary>
+        /// Create an encryptor for the specified key
+        /// </summary>
+        /// <param name="key">The key</param>
+        /// <param name="iv">The initialization vector</param>
+        /// <returns>An ICryptoTranform for encrypting data</returns>
+        public abstract ICryptoTransform CreateEncryptor( byte[] key, byte[] iv );
+
+        /// <summary>
+        /// Crea a decryptor for the specified key
+        /// </summary>
+        /// <param name="key">The key</param>
+        /// <param name="iv">The initialization vector</param>
+        /// <returns>An ICryptoTransform for decrypting data</returns>
+        public abstract ICryptoTransform CreateDecryptor( byte[] key, byte[] iv );
+    }
+}
