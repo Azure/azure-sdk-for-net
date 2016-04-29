1b8f2c647d9eda63a1ea35163162898b806f4289
smithab <smithab@microsoft.com>
2016-04-29 12:19:34 -0700

    NotificationHub generated from Swagger file

---
+++ src/ResourceManagement/NotificationHubs/NotificationHubs.Tests/Tests/ScenarioTests.NamespaceTests.CRUD.cs
@@ -0,0 +1,143 @@
+﻿//  
+//  
+// Copyright (c) Microsoft.  All rights reserved.
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+
+namespace NotificationHubs.Tests.ScenarioTests
+{
+    using Microsoft.Azure.Management.NotificationHubs;
+    using Microsoft.Azure.Management.NotificationHubs.Models;
+    using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+    using System;
+    using System.Linq;
+    using System.Net;
+    using TestHelper;
+    using Xunit;
+    using System.Collections.Generic;
+    public partial class ScenarioTests 
+    {
+        [Fact]
+        public void NamespaceCreateGetUpdateDelete()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                InitializeClients(context);
+
+                var location = NotificationHubsManagementHelper.DefaultLocation;
+                var resourceGroup = this.ResourceManagementClient.TryGetResourceGroup(location);
+                if (string.IsNullOrWhiteSpace(resourceGroup))
+                {
+                    resourceGroup = TestUtilities.GenerateName(NotificationHubsManagementHelper.ResourceGroupPrefix);
+                    this.ResourceManagementClient.TryRegisterResourceGroup(location, resourceGroup);
+                }
+
+                var namespaceName = TestUtilities.GenerateName(NotificationHubsManagementHelper.NamespacePrefix);
+
+                var createNamespaceResponse = this.NotificationHubsManagementClient.Namespaces.CreateOrUpdate(resourceGroup, namespaceName,
+                    new NamespaceCreateOrUpdateParameters()
+                    {
+                        Location = location,
+                        Properties = new NamespaceProperties()
+                        {
+                            NamespaceType = NamespaceType.NotificationHub
+                        }
+                    });
+
+                Assert.NotNull(createNamespaceResponse);
+                Assert.Equal(createNamespaceResponse.Name, namespaceName);
+
+                TestUtilities.Wait(TimeSpan.FromSeconds(5));
+
+                //Get the created namespace
+                var getNamespaceResponse = NotificationHubsManagementClient.Namespaces.Get(resourceGroup, namespaceName);
+                if (string.Compare(getNamespaceResponse.Properties.ProvisioningState, "Succeeded", true) != 0)
+                    TestUtilities.Wait(TimeSpan.FromSeconds(5));
+
+                getNamespaceResponse = NotificationHubsManagementClient.Namespaces.Get(resourceGroup, namespaceName);
+                Assert.NotNull(getNamespaceResponse);
+                Assert.Equal("Succeeded", getNamespaceResponse.Properties.ProvisioningState, StringComparer.CurrentCultureIgnoreCase);
+                Assert.Equal("Active", getNamespaceResponse.Properties.Status, StringComparer.CurrentCultureIgnoreCase);
+                Assert.Equal(NamespaceType.NotificationHub, getNamespaceResponse.Properties.NamespaceType);
+                Assert.Equal(location, getNamespaceResponse.Location, StringComparer.CurrentCultureIgnoreCase);
+
+                //Get all namespaces created within a resourceGroup
+                var getAllNamespacesResponse = NotificationHubsManagementClient.Namespaces.List(resourceGroup);
+                Assert.NotNull(getAllNamespacesResponse);
+                Assert.True(getAllNamespacesResponse.Count() >= 1);
+                Assert.True(getAllNamespacesResponse.Any(ns => ns.Name == namespaceName));
+                Assert.True(getAllNamespacesResponse.All(ns => ns.Id.Contains(resourceGroup)));
+
+                //Get all namespaces created within the subscription irrespective of the resourceGroup
+                getAllNamespacesResponse = NotificationHubsManagementClient.Namespaces.ListAll();
+                Assert.NotNull(getAllNamespacesResponse);
+                Assert.True(getAllNamespacesResponse.Count() >= 1);
+                Assert.True(getAllNamespacesResponse.Any(ns => ns.Name == namespaceName));
+
+                //Update namespace tags and make the namespace critical
+                var updateNamespaceParameter = new NamespaceCreateOrUpdateParameters()
+                {
+                    Location = location,
+                    Tags = new Dictionary<string, string>()
+                        {
+                            {"tag1", "value1"},
+                            {"tag2", "value2"},
+                            {"tag3", "value3"},
+                            {"tag4", "value4"}
+                        },
+
+                    Properties = new NamespaceProperties()
+                };
+
+                var updateNamespaceResponse = NotificationHubsManagementClient.Namespaces.CreateOrUpdate(resourceGroup, namespaceName, updateNamespaceParameter);
+
+                Assert.NotNull(updateNamespaceResponse);
+                Assert.True(updateNamespaceResponse.Properties.ProvisioningState.Equals("Active", StringComparison.CurrentCultureIgnoreCase) || 
+                    updateNamespaceResponse.Properties.ProvisioningState.Equals("Succeeded", StringComparison.CurrentCultureIgnoreCase));
+                Assert.Equal(namespaceName, updateNamespaceResponse.Name);
+                //Regression in the service . uncomment after the fix goes out 
+                //Assert.Equal(updateNamespaceResponse.Tags.Count, 4);
+                //foreach (var tag in updateNamespaceParameter.Tags)
+                //{
+                //    Assert.True(updateNamespaceResponse.Tags.Any(t => t.Key.Equals(tag.Key)));
+                //    Assert.True(updateNamespaceResponse.Tags.Any(t => t.Value.Equals(tag.Value)));
+                //}
+
+                TestUtilities.Wait(TimeSpan.FromSeconds(5));
+
+                //Get the updated namespace
+                getNamespaceResponse = NotificationHubsManagementClient.Namespaces.Get(resourceGroup, namespaceName);
+                Assert.NotNull(getNamespaceResponse);
+                Assert.Equal(NamespaceType.NotificationHub, getNamespaceResponse.Properties.NamespaceType);
+                Assert.Equal(location, getNamespaceResponse.Location, StringComparer.CurrentCultureIgnoreCase);
+                Assert.Equal(namespaceName, getNamespaceResponse.Name);
+                Assert.Equal(getNamespaceResponse.Tags.Count, 4);
+                foreach (var tag in updateNamespaceParameter.Tags)
+                {
+                    Assert.True(getNamespaceResponse.Tags.Any(t => t.Key.Equals(tag.Key)));
+                    Assert.True(getNamespaceResponse.Tags.Any(t => t.Value.Equals(tag.Value)));
+                }
+                
+                try
+                {
+                    //Delete namespace
+                    NotificationHubsManagementClient.Namespaces.Delete(resourceGroup, namespaceName);
+                }
+                catch (Exception ex)
+                {
+                    Assert.True(ex.Message.Contains("NotFound"));
+                }
+            }
+        }
+    }
+}
---
+++ src/ResourceManagement/NotificationHubs/NotificationHubs.Tests/Tests/ScenarioTests.cs
@@ -0,0 +1,83 @@
+﻿﻿//  
+// Copyright (c) Microsoft.  All rights reserved.
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+
+namespace NotificationHubs.Tests.ScenarioTests
+{
+    using Microsoft.Azure.Management.NotificationHubs;
+    using Microsoft.Azure.Management.Resources;
+    using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+    using TestHelper;
+    using System.Net;
+
+    public partial class ScenarioTests 
+    {
+        private ResourceManagementClient _resourceManagementClient;
+        private NotificationHubsManagementClient _notificationHubsManagementClient;
+        private RecordedDelegatingHandler handler = new RecordedDelegatingHandler();
+
+        protected bool m_initialized = false;
+        protected object m_lock = new object();
+        public string Location { get; set; }
+        public string ResourceGroupName { get; set; }
+        public string NamespaceName { get; set; }
+               
+
+        protected void InitializeClients(MockContext context)
+        {
+            if (!m_initialized)
+            {
+                lock (m_lock)
+                {
+                    if (!m_initialized)
+                    {
+                        _resourceManagementClient = NotificationHubsManagementHelper.GetResourceManagementClient(context, new RecordedDelegatingHandler { StatusCodeToReturn = HttpStatusCode.OK });
+                        _notificationHubsManagementClient = NotificationHubsManagementHelper.GetNotificationHubsManagementClient(context, new RecordedDelegatingHandler { StatusCodeToReturn = HttpStatusCode.OK });
+                    }
+                }
+            }
+        }
+
+        public ResourceManagementClient ResourceManagementClient
+        {
+            get
+            {                
+                return _resourceManagementClient;
+            }
+        }
+
+        public NotificationHubsManagementClient NotificationHubsManagementClient
+        {
+            get
+            {               
+                return _notificationHubsManagementClient;
+            }
+        }
+
+        protected void TryCreateNamespace()
+        {
+            this.ResourceGroupName = this.ResourceManagementClient.TryGetResourceGroup(Location);
+            this.Location = NotificationHubsManagementHelper.DefaultLocation;
+
+            if (string.IsNullOrWhiteSpace(ResourceGroupName))
+            {
+                ResourceGroupName = TestUtilities.GenerateName(NotificationHubsManagementHelper.ResourceGroupPrefix);
+                this.ResourceManagementClient.TryRegisterResourceGroup(Location, ResourceGroupName);
+            }
+
+            NamespaceName = TestUtilities.GenerateName(NotificationHubsManagementHelper.NamespacePrefix);
+            this.NotificationHubsManagementClient.TryCreateNamespace(ResourceGroupName, NamespaceName, Location);
+        }        
+    }
+}
