fa1fecdff08c917b49baa9a41218fd3e3396a660
Brian <l.brian.hoang@gmail.com>
2016-05-03 16:34:30 -0700

    Changed assembly versions for management lib to 2.0.0

--- src/ResourceManagement/Batch/BatchManagement/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/Batch/BatchManagement/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Microsoft Azure Batch Management Library")]
 [assembly: AssemblyDescription("Provides management functions for Microsoft Azure Batch services.")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
