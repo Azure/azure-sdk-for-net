6f47c5b026336d77bdf65ec107a305bcf23208e8
Garrett Serack <garretts@microsoft.com>
2016-05-04 13:30:18 -0700

    fixing issues

--- src/ResourceManagement/ServerManagement/ServerManagement.Tests/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/ServerManagement/ServerManagement.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeyVault.Management.Tests")]
+[assembly: AssemblyTitle("ServerManagement.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KeyVault.Management.Tests")]
+[assembly: AssemblyProduct("ServerManagement.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/ResourceManagement/ServerManagement/ServerManagement/generate.cmd
+++ src/ResourceManagement/ServerManagement/ServerManagement/generate.cmd
@@ -2,16 +2,16 @@
 :: Microsoft Azure SDK for Net - Generate library code
 :: Copyright (C) Microsoft Corporation. All Rights Reserved.
 ::
- 
-set autoRestVersion=0.16.0-Nightly20160329
-set specUrl="https://raw.githubusercontent.com/fearthecowboy/azure-rest-api-specs/master/arm-servermanagement/2015-07-01-preview/servermanagement.json"
-set source=-Source https://www.myget.org/F/autorest/api/v2
 
+@echo off
+set autoRestVersion=0.16.0-Nightly20160406
+if  "%1" == "" (
+    set specFile="https://raw.githubusercontent.com/fearthecowboy/azure-rest-api-specs/master/arm-servermanagement/2015-07-01-preview/servermanagement.json"
+) else (
+    set specFile="%1"
+)
 set repoRoot=%~dp0..\..\..\..
-set autoRestExe=%repoRoot%\packages\autorest.%autoRestVersion%\tools\AutoRest.exe
 set generateFolder=%~dp0Generated
 
-%repoRoot%\tools\nuget.exe install autorest %source% -Version %autoRestVersion% -o %repoRoot%\packages
-
 if exist %generateFolder% rd /S /Q  %generateFolder%
-%autoRestExe% -Modeler Swagger -CodeGenerator Azure.CSharp -Namespace  Microsoft.Azure.Management.ServerManagement -Input %specUrl% -outputDirectory %generateFolder% -Header NONE
+call "%repoRoot%\tools\autorest.gen.cmd" %specFile% Microsoft.Azure.Management.ServerManagement %autoRestVersion% %generateFolder% 
