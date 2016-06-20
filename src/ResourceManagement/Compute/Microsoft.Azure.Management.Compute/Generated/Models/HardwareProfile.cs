5b7eaf73102d4f67777834aa0896d5907ae10c9f
Hyonho Lee <Hyonho.Lee@microsoft.com>
2016-06-19 21:52:40 -0700

    Update descriptions, add more vm sizes, and vmId property, and set provisioning state to read only (#2111)
    * Update descriptions, add more vm sizes, and vmId property, and set provisioning state to read only

--- src/ResourceManagement/Compute/Microsoft.Azure.Management.Compute/Generated/Models/HardwareProfile.cs
+++ src/ResourceManagement/Compute/Microsoft.Azure.Management.Compute/Generated/Models/HardwareProfile.cs
@@ -45,11 +45,15 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// 'Standard_D1_v2', 'Standard_D2_v2', 'Standard_D3_v2',
         /// 'Standard_D4_v2', 'Standard_D5_v2', 'Standard_D11_v2',
         /// 'Standard_D12_v2', 'Standard_D13_v2', 'Standard_D14_v2',
-        /// 'Standard_DS1', 'Standard_DS2', 'Standard_DS3', 'Standard_DS4',
-        /// 'Standard_DS11', 'Standard_DS12', 'Standard_DS13',
-        /// 'Standard_DS14', 'Standard_G1', 'Standard_G2', 'Standard_G3',
-        /// 'Standard_G4', 'Standard_G5', 'Standard_GS1', 'Standard_GS2',
-        /// 'Standard_GS3', 'Standard_GS4', 'Standard_GS5'
+        /// 'Standard_D15_v2', 'Standard_DS1', 'Standard_DS2',
+        /// 'Standard_DS3', 'Standard_DS4', 'Standard_DS11', 'Standard_DS12',
+        /// 'Standard_DS13', 'Standard_DS14', 'Standard_DS1_v2',
+        /// 'Standard_DS2_v2', 'Standard_DS3_v2', 'Standard_DS4_v2',
+        /// 'Standard_DS5_v2', 'Standard_DS11_v2', 'Standard_DS12_v2',
+        /// 'Standard_DS13_v2', 'Standard_DS14_v2', 'Standard_DS15_v2',
+        /// 'Standard_G1', 'Standard_G2', 'Standard_G3', 'Standard_G4',
+        /// 'Standard_G5', 'Standard_GS1', 'Standard_GS2', 'Standard_GS3',
+        /// 'Standard_GS4', 'Standard_GS5'
         /// </summary>
         [JsonProperty(PropertyName = "vmSize")]
         public string VmSize { get; set; }
