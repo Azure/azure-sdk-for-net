c76d324914bff8987c129d75d20754cdc648c8f0
Bala Ganapathy <bganapa@microsoft.com>
2016-06-28 12:26:51 -0700

    Adding SDK level tests for azurestack admin management

---
+++ src/ResourceManagement/AzureStackAdmin/AzureStackAdmin.Tests/PlanTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.PeerToPeer.Collaboration;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.AzureStack.Management;
+using Microsoft.AzureStack.Management.Models;
+using Xunit;
+
+namespace AzureStackAdmin.Tests
+{
+    public class PlanTests
+    {
+        public AzureStackClient GetAzureStackAdminClient(RecordedDelegatingHandler handler)
+        {
+            var token = new TokenCloudCredentials(Guid.NewGuid().ToString(), "abc123");
+            handler.IsPassThrough = false;
+            return new AzureStackClient(new Uri("https://armuri"), token, "2015-11-01").WithHandler(handler);
+        }
+
+        [Fact]
+        public void GetPlan()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(@"{
+                    'id': '/subscriptions/7354d868-a0a8-47b0-9566-6e753aec1754/resourceGroups/TestRG/providers/Microsoft.Subscriptions.Admin/plans/TestPlan',
+                    'name': 'TestPlan',
+                    'type': 'Microsoft.Subscriptions.Admin/plans',
+                    'location': 'local',
+                    'properties': {
+                        'name': 'TestPlan',
+                        'displayName': 'TestPlan',
+                        'subscriptionCount': 0,
+                        'quotaIds': [
+                            '/subscriptions/7354d868-a0a8-47b0-9566-6e753aec1754/providers/Microsoft.Sql.Admin/locations/local/quotas/Default_BasicTierQuota'
+                        ]
+                    }
+                }")
+            };
+
+            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.OK };
+            var client = this.GetAzureStackAdminClient(handler);
+
+            var result = client.ManagedPlans.Get("TestRG", "TestPlan");
+            
+            // Validate Headers
+            Assert.Equal(HttpMethod.Get, handler.Method);
+            Assert.NotNull(handler.RequestHeaders.GetValues("Authorization"));
+
+            // Validate result
+            Assert.Equal("TestPlan", result.Plan.Name);
+            Assert.Equal("local", result.Plan.Location);
+            Assert.Equal(0, result.Plan.Properties.SubscriptionCount);
+            Assert.Equal("/subscriptions/7354d868-a0a8-47b0-9566-6e753aec1754/providers/Microsoft.Sql.Admin/locations/local/quotas/Default_BasicTierQuota", result.Plan.Properties.QuotaIds[0]);
+        }
+
+        [Fact]
+        public void CreateOrUpdatePlan()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(@"{
+                    'id': '/subscriptions/7354d868-a0a8-47b0-9566-6e753aec1754/resourceGroups/TestRG-3e1d9655-b0c4-487d-9b9e-027748e8914d/providers/Microsoft.Subscriptions.Admin/plans/TestPlan',
+                    'name': 'TestPlan',
+                    'type': 'Microsoft.Subscriptions.Admin/plans',
+                    'location': 'local',
+                    'properties': {
+                        'name': 'TestPlan',
+                        'displayName': 'TestPlan',
+                        'subscriptionCount': 0,
+                        'quotaIds': [
+                            '/subscriptions/7354d868-a0a8-47b0-9566-6e753aec1754/providers/Microsoft.Sql.Admin/locations/local/quotas/Default_BasicTierQuota'
+                        ]
+                    }
+                }")
+            };
+
+            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.OK };
+            var client = this.GetAzureStackAdminClient(handler);
+
+            var result = client.ManagedPlans.CreateOrUpdate(
+                "TestRG",
+                new ManagedPlanCreateOrUpdateParameters()
+                {
+                    Plan = new AdminPlanModel()
+                           {
+                               Location = "local",
+                               Name = "TestPlan",
+                               Properties = new AdminPlanPropertiesDefinition()
+                                            {
+                                                Name = "TestPlan",
+                                                DisplayName = "TestPlan",
+                                                QuotaIds = new []{"/subscriptions/7354d868-a0a8-47b0-9566-6e753aec1754/providers/Microsoft.Sql.Admin/locations/local/quotas/Default_BasicTierQuota"}
+                                            }
+                           }
+                }
+            );
+
+            // Validate Headers
+            Assert.Equal(HttpMethod.Put, handler.Method);
+            Assert.NotNull(handler.RequestHeaders.GetValues("Authorization"));
+
+            // Validate result
+            Assert.Equal("TestPlan", result.Plan.Name);
+            Assert.Equal("local", result.Plan.Location);
+            Assert.Equal(0, result.Plan.Properties.SubscriptionCount);
+            Assert.Equal("/subscriptions/7354d868-a0a8-47b0-9566-6e753aec1754/providers/Microsoft.Sql.Admin/locations/local/quotas/Default_BasicTierQuota", result.Plan.Properties.QuotaIds[0]);
+        }
+
+        [Fact]
+        public void DeletePlan()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.OK };
+            var client = this.GetAzureStackAdminClient(handler);
+
+            var result = client.ManagedPlans.Delete("TestRG", "TestPlan");
+
+            // Validate headers
+            Assert.Equal(HttpMethod.Delete, handler.Method);
+            Assert.NotNull(handler.RequestHeaders.GetValues("Authorization"));
+
+            // Validate result
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+
+        }
+    }
+}
---
+++ src/ResourceManagement/AzureStackAdmin/AzureStackAdmin.Tests/ResourceProviderManifest.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.PeerToPeer.Collaboration;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.AzureStack.Management;
+using Microsoft.AzureStack.Management.Models;
+using Xunit;
+
+namespace AzureStackAdmin.Tests
+{
+    public class ResourceProviderManifestTests
+    {
+        public AzureStackClient GetAzureStackAdminClient(RecordedDelegatingHandler handler)
+        {
+            var token = new TokenCloudCredentials(Guid.NewGuid().ToString(), "fake");
+            handler.IsPassThrough = false;
+            return new AzureStackClient(new Uri("https://armuri"), token, "2015-11-01").WithHandler(handler);
+        }
+
+        [Fact]
+        public void GetResourceProviderManifest()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(@"{
+                    'id': '/subscriptions/b5c4cce5-bf48-4c87-a083-eb99fda1f60e/resourceGroups/System/providers/Microsoft.Subscriptions.Providers/manifests/Microsoft.Sql.Admin',
+                    'name': 'Microsoft.Sql.Admin',
+                    'type': 'Microsoft.Subscriptions.Providers/manifests',
+                    'location': 'local',
+                    'properties': {
+                        'displayName': 'Microsoft.Sql.Admin',
+                        'namespace': 'Microsoft.Sql.Admin',
+                        'providerLocation': 'local',
+                        'providerType': 'NotSpecified',
+                        'extensions': [
+                            {
+                                'name': 'SqlAdminExtension',
+                                'uri': 'https://azstack:13002/'
+                            }
+                        ],
+                        'resourceTypes': [
+                            {
+                                'name': 'hostingservers',
+                                'routingType': 'Default',
+                                'resourceDeletionPolicy': 'NotSpecified',
+                                'endpoints': [
+                                    {
+                                        'apiVersions': [
+                                            '2014-04-01-preview'
+                                        ],
+                                        'enabled': true,
+                                        'endpointUri': 'https://azstack:30010',
+                                        'timeout': 'PT0S'
+                                    }
+                                ],
+                                'marketplaceType': 'NotSpecified'
+                            }
+                        ],
+                        'enabled': true,
+                        'provisioningState': 'Succeeded'
+                    }
+                }")
+            };
+
+            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.OK };
+            var client = this.GetAzureStackAdminClient(handler);
+
+            var result = client.ProviderRegistrations.Get("TestRG", "Microsoft.Sql.Admin");
+            
+            // Validate Headers
+            Assert.Equal(HttpMethod.Get, handler.Method);
+            Assert.NotNull(handler.RequestHeaders.GetValues("Authorization"));
+
+            // Validate result
+            Assert.Equal("Microsoft.Sql.Admin", result.ProviderRegistration.Name);
+            Assert.Equal("local", result.ProviderRegistration.Location);
+            Assert.Equal("Microsoft.Sql.Admin", result.ProviderRegistration.Properties.Namespace);
+            Assert.Equal("local", result.ProviderRegistration.Properties.ProviderLocation);
+            Assert.Equal("hostingservers", result.ProviderRegistration.Properties.ResourceTypes[0].Name);
+            Assert.Equal("hostingservers", result.ProviderRegistration.Properties.Extensions[0].Name);
+            Assert.Equal("2014-04-01-preview", result.ProviderRegistration.Properties.ResourceTypes[0].Endpoints[0].ApiVersions[0]);
+            Assert.Equal("https://azstack:30010", result.ProviderRegistration.Properties.ResourceTypes[0].Endpoints[0].EndpointUri);
+            Assert.Equal(new TimeSpan(0, 0, 0), result.ProviderRegistration.Properties.ResourceTypes[0].Endpoints[0].Timeout);
+            Assert.Equal(true, result.ProviderRegistration.Properties.ResourceTypes[0].Endpoints[0].Enabled);
+            Assert.Equal(RoutingType.Default, result.ProviderRegistration.Properties.ResourceTypes[0].RoutingType);
+            Assert.Equal(ResourceDeletionPolicy.NotSpecified, result.ProviderRegistration.Properties.ResourceTypes[0].ResourceDeletionPolicy);
+            Assert.Equal(MarketplaceType.NotSpecified, result.ProviderRegistration.Properties.ResourceTypes[0].MarketplaceType);
+            // Assert.Equal(ProvisioningState.Succeeded, result.ProviderRegistration.Properties.ProvisioningState);
+        }
+
+        [Fact]
+        public void CreateOrUpdateResourceProviderManifest()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(@"{
+                    'id': '/subscriptions/b5c4cce5-bf48-4c87-a083-eb99fda1f60e/resourceGroups/System/providers/Microsoft.Subscriptions.Providers/manifests/Microsoft.Sql.Admin',
+                    'name': 'Microsoft.Sql.Admin',
+                    'type': 'Microsoft.Subscriptions.Providers/manifests',
+                    'location': 'local',
+                    'properties': {
+                        'displayName': 'Microsoft.Sql.Admin',
+                        'namespace': 'Microsoft.Sql.Admin',
+                        'providerLocation': 'local',
+                        'providerType': 'NotSpecified',
+                        'extensions': [
+                            {
+                                'name': 'SqlAdminExtension',
+                                'uri': 'https://azstack:13002/'
+                            }
+                        ],
+                        'resourceTypes': [
+                            {
+                                'name': 'hostingservers',
+                                'routingType': 'Default',
+                                'resourceDeletionPolicy': 'NotSpecified',
+                                'endpoints': [
+                                    {
+                                        'apiVersions': [
+                                            '2014-04-01-preview'
+                                        ],
+                                        'enabled': true,
+                                        'endpointUri': 'https://azstack:30010',
+                                        'timeout': 'PT0S'
+                                    }
+                                ],
+                                'marketplaceType': 'NotSpecified'
+                            }
+                        ],
+                        'enabled': true,
+                        'provisioningState': 'Succeeded'
+                    }
+                }")
+            };
+
+            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.OK };
+            var client = this.GetAzureStackAdminClient(handler);
+
+            var result = client.ProviderRegistrations.CreateOrUpdate(
+                "system",
+                new ProviderRegistrationCreateOrUpdateParameters(
+                    new ProviderRegistrationModel()
+                    {
+                        Location = "local",
+                        Name = "Microsoft.Sql.Admin",
+                        Properties = new ManifestPropertiesDefinition()
+                                     {
+                                         DisplayName = "Microsoft.Sql.Admin",
+                                         ExtensionName = "Microsoft.Sql.Admin",
+                                         Enabled = true,
+                                         Extensions = new Extension[]
+                                                      {
+                                                          new Extension() {Name = "SqlAdminExtension", Uri = "https://azstack:13002/"}
+                                                      },
+                                         ResourceTypes = new ResourceType[]
+                                                         {
+                                                             new ResourceType()
+                                                             {
+                                                                 Name = "hostingservers",
+                                                                 RoutingType = RoutingType.Default,
+                                                                 Endpoints = new ResourceProviderEndpoint[]
+                                                                             {
+                                                                                 new ResourceProviderEndpoint()
+                                                                                 {
+                                                                                     ApiVersions = new[] {"2014-04-01-preview"},
+                                                                                     Enabled = true,
+                                                                                     EndpointUri = "https://azstack:30010",
+                                                                                 }
+                                                                             }
+
+                                                             }
+                                                         },
+                                         ProvisioningState = ProvisioningState.Succeeded
+                                     }
+                    }
+                    ));
+
+            // Validate Headers
+            Assert.Equal(HttpMethod.Put, handler.Method);
+            Assert.NotNull(handler.RequestHeaders.GetValues("Authorization"));
+
+            // Validate result
+            Assert.Equal("Microsoft.Sql.Admin", result.ProviderRegistration.Name);
+            Assert.Equal("local", result.ProviderRegistration.Location);
+            Assert.Equal("Microsoft.Sql.Admin", result.ProviderRegistration.Properties.Namespace);
+            Assert.Equal("local", result.ProviderRegistration.Properties.ProviderLocation);
+            Assert.Equal("hostingservers", result.ProviderRegistration.Properties.ResourceTypes[0].Name);
+            Assert.Equal("hostingservers", result.ProviderRegistration.Properties.Extensions[0].Name);
+            Assert.Equal("2014-04-01-preview", result.ProviderRegistration.Properties.ResourceTypes[0].Endpoints[0].ApiVersions[0]);
+            Assert.Equal("https://azstack:30010", result.ProviderRegistration.Properties.ResourceTypes[0].Endpoints[0].EndpointUri);
+            Assert.Equal(new TimeSpan(0, 0, 0), result.ProviderRegistration.Properties.ResourceTypes[0].Endpoints[0].Timeout);
+            Assert.Equal(true, result.ProviderRegistration.Properties.ResourceTypes[0].Endpoints[0].Enabled);
+            Assert.Equal(RoutingType.Default, result.ProviderRegistration.Properties.ResourceTypes[0].RoutingType);
+            Assert.Equal(ResourceDeletionPolicy.NotSpecified, result.ProviderRegistration.Properties.ResourceTypes[0].ResourceDeletionPolicy);
+            Assert.Equal(MarketplaceType.NotSpecified, result.ProviderRegistration.Properties.ResourceTypes[0].MarketplaceType);
+        }
+
+        [Fact]
+        public void DeleteResourceProviderManifest()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.OK };
+            var client = this.GetAzureStackAdminClient(handler);
+
+            var result = client.ProviderRegistrations.Delete("system", "Microsoft.Sql.Admin");
+
+            // Validate headers
+            Assert.Equal(HttpMethod.Delete, handler.Method);
+            Assert.NotNull(handler.RequestHeaders.GetValues("Authorization"));
+
+            // Validate result
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+
+        }
+    }
+}
