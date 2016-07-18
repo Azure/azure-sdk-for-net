bd9fd95f2c6e3aeb6843783f5cc9609055a62b43
anuchan <anuamd@gmail.com>
2016-07-18 09:34:14 -0700

    Adding ChildResource Definition and Update interfaces

---
+++ src/ResourceManagementV2/Resource/Microsoft.Azure.Management.V2.Resource/ChildResource/Definition.cs
@@ -0,0 +1,7 @@
+﻿namespace Microsoft.Azure.Management.V2.Resource.ChildResource.Definition
+{
+    interface InDefinition<ParentT>
+    {
+        ParentT attach();
+    }
+}
---
+++ src/ResourceManagementV2/Resource/Microsoft.Azure.Management.V2.Resource/ChildResource/Update.cs
@@ -0,0 +1,7 @@
+﻿namespace Microsoft.Azure.Management.V2.Resource.ChildResource.Update
+{
+    interface InUpdate<ParentT>
+    {
+        ParentT attach();
+    }
+}
