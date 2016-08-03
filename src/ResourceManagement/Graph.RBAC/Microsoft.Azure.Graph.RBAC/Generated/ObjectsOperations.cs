d68a3ccf863d17d7db55aaa3819a807939d89220
Shubham Agarwal <shuagarw@microsoft.com>
2016-08-02 23:36:33 -0700

    Apply paging hacks and re-record some tests

--- src/ResourceManagement/Graph.RBAC/Microsoft.Azure.Graph.RBAC/Generated/ObjectsOperations.cs
+++ src/ResourceManagement/Graph.RBAC/Microsoft.Azure.Graph.RBAC/Generated/ObjectsOperations.cs
@@ -280,9 +280,7 @@ namespace Microsoft.Azure.Graph.RBAC
             }
             if (_queryParameters.Count > 0)
             {
-                // Temporary hack for nextLink issue since nextLink already contains query param, a '?' is part of that. Remove this once the issue is fixed in AutoRest.                
-                _url += "&" + string.Join("&", _queryParameters);
-                // _url += "?" + string.Join("&", _queryParameters);
+                _url += "?" + string.Join("&", _queryParameters);
             }
             // Create HTTP transport objects
             HttpRequestMessage _httpRequest = new HttpRequestMessage();
