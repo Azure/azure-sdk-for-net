a239aab5de7edd4161ac2ecb9c2d00bbf6d1453c
anuchan <anuamd@gmail.com>
2016-08-03 14:13:36 -0700

    Exposing all resource collections via IResourceManager

---
+++ src/ResourceManagementV2/Resource/Microsoft.Azure.Management.V2.Resource/Feature/FeatureImpl.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Azure.Management.V2.Resource.Core;
+
+namespace Microsoft.Azure.Management.V2.Resource
+{
+    internal class FeatureImpl : IndexableWrapper<FeatureResultInner>,
+        IFeature
+    {
+        internal FeatureImpl(FeatureResultInner innerModel) : base(innerModel.Id, innerModel) { }
+
+        public string Name
+        {
+            get
+            {
+                return Inner.Name;
+            }
+        }
+
+        public string State
+        {
+            get
+            {
+                if (Inner.Properties == null)
+                {
+                    return null;
+                }
+                return Inner.Properties.State;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return Inner.Type;
+            }
+        }
+    }
+}
---
+++ src/ResourceManagementV2/Resource/Microsoft.Azure.Management.V2.Resource/Feature/FeaturesImpl.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Azure.Management.V2.Resource.Core;
+using Microsoft.Azure.Management.V2.Resource.Feature;
+
+namespace Microsoft.Azure.Management.V2.Resource
+{
+    internal class FeaturesImpl : IFeatures
+    {
+        private IFeaturesOperations client;
+
+        internal FeaturesImpl(IFeaturesOperations client)
+        {
+            this.client = client;
+        }
+
+        public PagedList<IFeature> List()
+        {
+            PagedList<FeatureResultInner> innerList = new PagedList<FeatureResultInner>(client.ListAll(), (string nextLink) =>
+            {
+                return client.ListNext(nextLink);
+            });
+            return new PagedList<IFeature>(new WrappedPage<FeatureResultInner, IFeature>(innerList.CurrentPage, WrapModel), (string nextLink) =>
+            {
+                innerList.LoadNextPage();
+                return new WrappedPage<FeatureResultInner, IFeature>(innerList.CurrentPage, WrapModel);
+            });
+        }
+
+        public IInResourceProvider ResourceProvider(string resourceProviderName)
+        {
+            return null;
+        }
+
+        private IFeature WrapModel(FeatureResultInner innerModel)
+        {
+            return new FeatureImpl(innerModel);
+        }
+    }
+}
