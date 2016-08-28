1346067b93808b13d99d5e57404db326be0fc800
Anthony Pham <atpham256@yahoo.com>
2016-08-28 16:58:27 -0700

    Add support for the OutputErroryPolicy property
    -Added generated files necessary to support the OutputErrorPolicy
    property at the ASA job level
    -Updated tests to test the new property
    -Reran tests and updated session records
    -Updated NuGet version to 1.8.1

--- src/ResourceManagement/StreamAnalytics/StreamAnalyticsManagement/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/StreamAnalytics/StreamAnalyticsManagement/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Provides Microsoft Azure Stream Analytics management operations.")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.1.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
