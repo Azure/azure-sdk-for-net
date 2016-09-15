9fd3d9e46b9b9c6b83c66cc1859f5ba72deb9fea
vivsriaus <visriniv@microsoft.com>
2016-09-15 16:34:32 -0700

    Add support for CRUD resources by resourceId

--- src/ResourceManagement/Resource/Microsoft.Azure.Management.ResourceManager/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/Resource/Microsoft.Azure.Management.ResourceManager/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides Microsoft Azure resource management operations including Resource Groups.")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Azure .NET SDK")]
