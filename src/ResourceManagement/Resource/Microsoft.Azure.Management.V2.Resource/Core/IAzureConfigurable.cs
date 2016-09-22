953e663ab177b9ff6936c94c9fc5835e0bdce93b
Hovsep <hovsepm@microsoft.com>
2016-09-22 11:04:04 -0700

    Added license headers to the CS files. (#2382)

--- src/ResourceManagement/Resource/Microsoft.Azure.Management.V2.Resource/Core/IAzureConfigurable.cs
+++ src/ResourceManagement/Resource/Microsoft.Azure.Management.V2.Resource/Core/IAzureConfigurable.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Rest.TransientFaultHandling;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Rest.TransientFaultHandling;
 using System.Net.Http;
 
 namespace Microsoft.Azure.Management.V2.Resource.Core
