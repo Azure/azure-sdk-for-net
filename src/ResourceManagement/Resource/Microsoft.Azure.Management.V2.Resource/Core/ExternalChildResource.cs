ac4d631b6ec8ec6ea81c9395748b92787020fa05
anuchan <anuamd@gmail.com>
2016-09-22 09:59:59 -0700

    Adding license header to handwritten files

--- src/ResourceManagement/Resource/Microsoft.Azure.Management.V2.Resource/Core/ExternalChildResource.cs
+++ src/ResourceManagement/Resource/Microsoft.Azure.Management.V2.Resource/Core/ExternalChildResource.cs
@@ -1,11 +1,7 @@
-﻿
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
 
 using Microsoft.Azure.Management.V2.Resource.Core.ResourceActions;
-/**
-* Copyright (c) Microsoft Corporation. All rights reserved.
-* Licensed under the MIT License. See License.txt in the project root for
-* license information.
-*/
 using System;
 using System.Collections.Generic;
 using System.Linq;
