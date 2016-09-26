c838b284eca421c2e530fa84a2e3b2b22c2b3baf
anuchan <anuamd@gmail.com>
2016-09-26 13:52:47 -0700

    Adding license headers

--- src/ResourceManagement.Samples/Compute.Samples/ManageAvailabilitySet/Program.cs
+++ src/ResourceManagement.Samples/Compute.Samples/ManageAvailabilitySet/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Azure.Management;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information. 
+
+using Microsoft.Azure.Management;
 using Microsoft.Azure.Management.Compute.Models;
 using Microsoft.Azure.Management.V2.Compute;
 using Microsoft.Azure.Management.V2.Network;
