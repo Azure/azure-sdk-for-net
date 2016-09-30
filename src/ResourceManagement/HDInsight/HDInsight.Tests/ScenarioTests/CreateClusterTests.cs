4990bdc08d002a480bb213ee8db37493880d0a37
iyerrb <iyerrb@gmail.com>
2016-09-30 13:54:11 -0700

    Enabled the Spark 2.0 based component version test

--- src/ResourceManagement/HDInsight/HDInsight.Tests/ScenarioTests/CreateClusterTests.cs
+++ src/ResourceManagement/HDInsight/HDInsight.Tests/ScenarioTests/CreateClusterTests.cs
@@ -389,7 +389,7 @@ namespace HDInsight.Tests
             }
         }
 
-        /*[Fact]
+        [Fact]
         public void TestCreateLinuxSparkClusterWithComponentVersion()
         {
             var handler = new RecordedDelegatingHandler { StatusCodeToReturn = HttpStatusCode.OK };
@@ -417,6 +417,6 @@ namespace HDInsight.Tests
                 Assert.Equal(result.StatusCode, HttpStatusCode.OK);
                 Assert.Equal(result.State, AsyncOperationState.Succeeded);
             }
-        }*/
+        }
     }
 }
