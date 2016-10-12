4d1fd2c1a8500f51d90d0b69e904b7f6cc23ae56
Bruce Johnston <bruce.johnston@microsoft.com>
2016-10-11 17:00:45 -0700

    Search SDK: Bumping GA SDK version to 1.1.3

--- src/Search/Microsoft.Azure.Search/Properties/AssemblyInfo.cs
+++ src/Search/Microsoft.Azure.Search/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ namespace Microsoft.Azure.Search
         public const string TargetApiVersion = "2015-02-28";
 
         // Making this a constant so we can use it to set the UserAgent header.
-        public const string AssemblyFileVersion = "1.1.2.0";
+        public const string AssemblyFileVersion = "1.1.3.0";
     }
 }
