9588f90d759f21fffd457132693403950f177b0f
vivsriaus <visriniv@microsoft.com>
2016-10-12 23:34:07 -0700

    Add more link tests

--- src/ResourceManagement/Resource/Resource.Tests/InMemoryTests/ResourceLinkTests.InMemory.cs
+++ src/ResourceManagement/Resource/Resource.Tests/InMemoryTests/ResourceLinkTests.InMemory.cs
@@ -44,7 +44,7 @@ namespace ResourceGroups.Tests
         [Fact]
         public void ResourceLinkCRUD()
         {
-            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            var response = new HttpResponseMessage(HttpStatusCode.Created)
             {
                 Content = new StringContent(@"{
                     'id': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink',
@@ -57,10 +57,10 @@ namespace ResourceGroups.Tests
                 }")
             };
 
-            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.OK };
+            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.Created };
             var client = GetManagementLinkClient(handler);
 
-            var result = client.ResourceLinks.CreateOrUpdateById(
+            var result = client.ResourceLinks.CreateOrUpdate(
                 linkId: "/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink",
                 parameters: new ResourceLink
                 {
@@ -87,6 +87,164 @@ namespace ResourceGroups.Tests
             Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm", result.Properties.SourceId);
             Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2", result.Properties.TargetId);
             Assert.Equal("myLinkNotes", result.Properties.Notes);
+            Assert.Equal("myLink", result.Name);
+
+            //Get resource link and validate properties
+
+            var getResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(@"{
+                    'id': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink',
+                    'name': 'myLink',
+                    'properties': {
+                        'sourceId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm',
+                        'targetId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2',
+                        'notes': 'myLinkNotes'
+                      }
+                }")
+            };
+
+            var getHandler = new RecordedDelegatingHandler(getResponse) { StatusCodeToReturn = HttpStatusCode.OK };
+            client = GetManagementLinkClient(getHandler);
+            var getResult = client.ResourceLinks.Get("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink");
+
+            // Validate response
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink", getResult.Id);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm", getResult.Properties.SourceId);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2", getResult.Properties.TargetId);
+            Assert.Equal("myLinkNotes", getResult.Properties.Notes);
+            Assert.Equal("myLink", getResult.Name);
+
+            //Delete resource link
+            var deleteResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            deleteResponse.Headers.Add("x-ms-request-id", "1");
+            var deleteHandler = new RecordedDelegatingHandler(deleteResponse) { StatusCodeToReturn = HttpStatusCode.OK };
+            client = GetManagementLinkClient(deleteHandler);
+
+            client.ResourceLinks.Delete("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink");
+
+            // Validate headers
+            Assert.Equal(HttpMethod.Delete, deleteHandler.Method);
+
+            // Get a deleted link throws exception
+            Assert.Throws<CloudException>(() => client.ResourceLinks.Get("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink"));
+        }
+
+        [Fact]
+        public void ResourceLinkList()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(@"{
+                    'value': [
+                        {
+                            'id': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink',
+                            'name': 'myLink',
+                            'properties': {
+                                'sourceId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm',
+                                'targetId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2',
+                                'notes': 'myLinkNotes'
+                              }
+                         },
+                         {
+                            'id': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Storage/storageAccounts/myAccount/providers/Microsoft.Resources/links/myLink2',
+                            'name': 'myLink2',
+                            'properties': {
+                                'sourceId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Storage/storageAccounts/myAccount',
+                                'targetId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Storage/storageAccounts/myAccount2',
+                                'notes': 'myLinkNotes2'
+                              }
+                         }],
+                      'nextLink': 'https://wa/subscriptions/subId/links?api-version=1.0&$skiptoken=662idk',
+                }")
+            };
+
+            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.OK };
+            var client = GetManagementLinkClient(handler);
+
+            var result = client.ResourceLinks.ListAtSubscription();
+
+            // Validate headers
+            Assert.Equal(HttpMethod.Get, handler.Method);
+
+            // Validate result
+            Assert.Equal(2, result.Count());
+            Assert.Equal("myLink", result.First().Name);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink", result.First().Id);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm", result.First().Properties.SourceId);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2", result.First().Properties.TargetId);
+            Assert.Equal("myLinkNotes", result.First().Properties.Notes);
+            Assert.Equal("https://wa/subscriptions/subId/links?api-version=1.0&$skiptoken=662idk", result.NextPageLink);
+
+            // Filter by targetId
+
+            var filteredResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(@"{
+                    'value': [
+                        {
+                            'id': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink',
+                            'name': 'myLink',
+                            'properties': {
+                                'sourceId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm',
+                                'targetId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2',
+                                'notes': 'myLinkNotes'
+                              }
+                         }],
+                      'nextLink': 'https://wa/subscriptions/subId/links?api-version=1.0&$skiptoken=662idk',
+                }")
+            };
+
+            var filteredHandler = new RecordedDelegatingHandler(filteredResponse) { StatusCodeToReturn = HttpStatusCode.OK };
+            client = GetManagementLinkClient(filteredHandler);
+            var filteredResult = client.ResourceLinks.ListAtSubscription(new ODataQuery<ResourceLinkFilter>(f => f.TargetId == "/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2"));
+
+            // Validate result
+            Assert.Equal(1, filteredResult.Count());
+            Assert.Equal("myLink", filteredResult.First().Name);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink", filteredResult.First().Id);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm", filteredResult.First().Properties.SourceId);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2", filteredResult.First().Properties.TargetId);
+            Assert.Equal("myLinkNotes", filteredResult.First().Properties.Notes);
+            Assert.Equal("https://wa/subscriptions/subId/links?api-version=1.0&$skiptoken=662idk", filteredResult.NextPageLink);
+        }
+
+        [Fact]
+        public void ResourceLinkListAtSourceScope()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(@"{
+                    'value': [
+                        {
+                            'id': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink',
+                            'name': 'myLink',
+                            'properties': {
+                                'sourceId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm',
+                                'targetId': '/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2',
+                                'notes': 'myLinkNotes'
+                              }
+                         }],
+                      'nextLink': 'https://wa/subscriptions/subId/links?api-version=1.0&$skiptoken=662idk',
+                }")
+            };
+
+            var handler = new RecordedDelegatingHandler(response) { StatusCodeToReturn = HttpStatusCode.OK };
+            var client = GetManagementLinkClient(handler);
+
+            var result = client.ResourceLinks.ListAtSourceScope("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm");
+
+            // Validate headers
+            Assert.Equal(HttpMethod.Get, handler.Method);
+
+            // Validate result
+            Assert.Equal(1, result.Count());
+            Assert.Equal("myLink", result.First().Name);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm/providers/Microsoft.Resources/links/myLink", result.First().Id);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm", result.First().Properties.SourceId);
+            Assert.Equal("/subscriptions/abc123/resourcegroups/myGroup/providers/Microsoft.Web/serverFarms/myFarm2", result.First().Properties.TargetId);
+            Assert.Equal("myLinkNotes", result.First().Properties.Notes);
+            Assert.Equal("https://wa/subscriptions/subId/links?api-version=1.0&$skiptoken=662idk", result.NextPageLink);
         }
     }
 }
