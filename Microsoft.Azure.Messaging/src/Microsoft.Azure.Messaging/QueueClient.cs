99e7093fd5cae78c123d96fcdfea807b84904c16
Vinay Suryanarayana <viny.surya@gmail.com>
2016-10-28 10:49:57 -0700

    Queue Client

--- Microsoft.Azure.Messaging/src/Microsoft.Azure.Messaging/QueueClient.cs
+++ Microsoft.Azure.Messaging/src/Microsoft.Azure.Messaging/QueueClient.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Linq;
-
 namespace Microsoft.Azure.Messaging
 {
     using System;
