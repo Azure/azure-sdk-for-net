ace6ed0923d146858059881aa921315288e28758
shahabhijeet <abhimswork@outlook.com>
2016-11-02 19:17:13 -0700

    Removed pre-built templates. Added project build task, added targets to build.proj, updated Repo-Tasks to build and install VS Project templates. Added tests to buildProjectTask. Updated project template to use latest Client Runtime

---
+++ tools/ProjectTemplates/AzureDotNetSDK-TestProject/SampleTest.cs
@@ -0,0 +1,21 @@
+﻿namespace $safeprojectname$
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class SampleTest
+    {   
+        public SampleTest()
+        {   
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
