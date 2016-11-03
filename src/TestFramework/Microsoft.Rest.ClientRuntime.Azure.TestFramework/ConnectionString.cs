fa38e67cdabf794c3c86710d4c1ababc05a5d475
shahabhijeet <abhimswork@outlook.com>
2016-11-03 16:12:52 -0700

    reverting the default string tenant name to an tenant Id

--- src/TestFramework/Microsoft.Rest.ClientRuntime.Azure.TestFramework/ConnectionString.cs
+++ src/TestFramework/Microsoft.Rest.ClientRuntime.Azure.TestFramework/ConnectionString.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Rest.ClientRuntime.Azure.TestFramework
         private Dictionary<string, string> _keyValuePairs;
         private string _connString;
         private StringBuilder _parseErrorSb;
-        private string DEFAULT_TENANTID = "microsoft.onmicrosoft.com";
+        private string DEFAULT_TENANTID = "72f988bf-86f1-41af-91ab-2d7cd011db47";
 
         #endregion
 
