945283436817070b3e06e6c38b0bfca766918d14
yahnoosh <jlembicz@microsoft.com>
2016-11-07 15:37:28 -0800

    Marking EdgeNGramTokenFilter, NGramTokenFilter, KeywordTokenizer and StandardTokenizer as obsolete and adding V2 versions for all of them.

--- src/Search/Microsoft.Azure.Search/Customizations/Documents/Models/KeywordTokenizer.cs
+++ src/Search/Microsoft.Azure.Search/Customizations/Documents/Models/KeywordTokenizer.cs
@@ -17,39 +17,33 @@ namespace Microsoft.Azure.Search.Models
     using Microsoft.Rest.Azure;
 
     /// <summary>
-    /// Generates n-grams of the given size(s). This token filter is
+    /// Emits the entire input as a single token. This tokenizer is
     /// implemented using Apache Lucene.
-    /// <see href="http://lucene.apache.org/core/4_10_3/analyzers-common/org/apache/lucene/analysis/ngram/NGramTokenFilter.html" />
+    /// <see href="http://lucene.apache.org/core/4_10_3/analyzers-common/org/apache/lucene/analysis/core/KeywordTokenizer.html" />
     /// </summary>
-    [JsonObject("#Microsoft.Azure.Search.NGramTokenFilter")]
-    public partial class NGramTokenFilter : TokenFilter
+    [JsonObject("#Microsoft.Azure.Search.KeywordTokenizer")]
+    [Obsolete("This type is obsolete. Please use KeywordTokenizerV2 instead.")]
+    public partial class KeywordTokenizer : Tokenizer
     {
         /// <summary>
-        /// Initializes a new instance of the NGramTokenFilter class.
+        /// Initializes a new instance of the KeywordTokenizer class.
         /// </summary>
-        public NGramTokenFilter() { }
+        public KeywordTokenizer() { }
 
         /// <summary>
-        /// Initializes a new instance of the NGramTokenFilter class.
+        /// Initializes a new instance of the KeywordTokenizer class.
         /// </summary>
-        public NGramTokenFilter(string name, int? minGram = default(int?), int? maxGram = default(int?))
+        public KeywordTokenizer(string name, int? bufferSize = default(int?))
             : base(name)
         {
-            MinGram = minGram;
-            MaxGram = maxGram;
+            BufferSize = bufferSize;
         }
 
         /// <summary>
-        /// Gets or sets the minimum n-gram length. Default is 1.
+        /// Gets or sets the read buffer size in bytes. Default is 256.
         /// </summary>
-        [JsonProperty(PropertyName = "minGram")]
-        public int? MinGram { get; set; }
-
-        /// <summary>
-        /// Gets or sets the maximum n-gram length. Default is 2.
-        /// </summary>
-        [JsonProperty(PropertyName = "maxGram")]
-        public int? MaxGram { get; set; }
+        [JsonProperty(PropertyName = "bufferSize")]
+        public int? BufferSize { get; set; }
 
         /// <summary>
         /// Validate the object.
