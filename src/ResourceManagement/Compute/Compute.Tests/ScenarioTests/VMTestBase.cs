f3dc05ae32f423641005953161ca5577fe7a43f0
Deepak Rajendran <derajen@microsoft.com>
2016-11-07 17:07:04 -0800

    fix compute failure

--- src/ResourceManagement/Compute/Compute.Tests/ScenarioTests/VMTestBase.cs
+++ src/ResourceManagement/Compute/Compute.Tests/ScenarioTests/VMTestBase.cs
@@ -532,7 +532,7 @@ namespace Compute.Tests
                 {
                     Id = GetChildAppGwResourceId(m_subId, rgName, gatewayName, "frontendIPConfigurations", frontendIPConfigName)
                 },
-                SslCertificate = null,
+                SSLCertificate = null,
                 Protocol = ApplicationGatewayProtocol.Http
             };
 
