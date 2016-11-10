02b7b3b1e962680c93c0838c22292b95e51efe69
Siddharth Chatrola <sichatro@microsoft.com>
2016-11-09 18:23:18 -0800

    Added test to verify BeginCreate

---
+++ src/ResourceManagement/RedisCache/AzureRedisCache.Tests/ScenarioTests/BeginCreateFunctionalTests.cs
@@ -0,0 +1,71 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using AzureRedisCache.Tests.ScenarioTests;
+using Microsoft.Azure.Management.Redis;
+using Microsoft.Azure.Management.Redis.Models;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using System;
+using Xunit;
+
+namespace AzureRedisCache.Tests
+{
+    public class BeginCreateFunctionalTests : TestBase
+    {
+        [Fact]
+        public void BeginCreateFunctionalTest()
+        {
+            using (var context = MockContext.Start(this.GetType().FullName))
+            {
+                var _redisCacheManagementHelper = new RedisCacheManagementHelper(this, context);
+                _redisCacheManagementHelper.TryRegisterSubscriptionForResource();
+                var resourceGroupName = "redisCacheCreateRG";
+                var redisCacheName = "sunnybegincreate-1";
+                var location = "Southeast Asia";
+                var _client = RedisCacheManagementTestUtilities.GetRedisManagementClient(this, context);
+                _redisCacheManagementHelper.TryCreateResourceGroup(resourceGroupName, location);
+                RedisResource response = _client.Redis.BeginCreate(resourceGroupName, redisCacheName,
+                                        parameters: new RedisCreateParameters
+                                        {
+                                            Location = location,
+                                            Sku = new Sku()
+                                            {
+                                                Name = SkuName.Premium,
+                                                Family = SkuFamily.P,
+                                                Capacity = 1
+                                            }
+                                        });
+
+                Assert.Contains(redisCacheName, response.Id);
+                Assert.Equal(redisCacheName, response.Name);
+                Assert.True("creating".Equals(response.ProvisioningState, StringComparison.OrdinalIgnoreCase));
+                Assert.Equal(SkuName.Premium, response.Sku.Name);
+                Assert.Equal(SkuFamily.P, response.Sku.Family);
+
+                for (int i = 0; i < 60; i++)
+                {
+                    response = _client.Redis.Get(resourceGroupName, redisCacheName);
+                    if ("succeeded".Equals(response.ProvisioningState, StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+                    TestUtilities.Wait(new TimeSpan(0, 0, 30));
+                }
+
+                _client.Redis.Delete(resourceGroupName: resourceGroupName, name: redisCacheName);
+                _redisCacheManagementHelper.DeleteResourceGroup(resourceGroupName);
+            }
+        }
+    }
+}
