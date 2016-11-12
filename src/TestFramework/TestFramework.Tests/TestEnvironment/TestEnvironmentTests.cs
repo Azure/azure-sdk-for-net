b6c174fd9b4f0ed76ef95c8a4ff2134a91160b21
shahabhijeet <abhimswork@outlook.com>
2016-11-11 17:25:36 -0800

    Adding test for default tenantId and bumping up the version

--- src/TestFramework/TestFramework.Tests/TestEnvironment/TestEnvironmentTests.cs
+++ src/TestFramework/TestFramework.Tests/TestEnvironment/TestEnvironmentTests.cs
@@ -5,6 +5,8 @@ namespace TestFramework.Tests.TestEnvironment
 {
     using Xunit;
     using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+    using System;
+    using Microsoft.Azure.Test.HttpRecorder;
 
     public class TestEnvironmentTests
     {
@@ -14,5 +16,17 @@ namespace TestFramework.Tests.TestEnvironment
             TestEnvironment env = new TestEnvironment();
             Assert.Equal<int>(4, env.EnvEndpoints.Count);
         }
+
+        [Fact]
+        public void DefaultTenantInTestEnvironment()
+        {
+            Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", "SubscriptionId=2c224e7e-57b-e71f4662e3a6;ServicePrincipal=4c9b80e8e4-7c2233383b4e;ServicePrincipalSecret=kzSj3+tSXIdRsX8mxuQe7yZY=;Environment=Prod");
+            HttpMockServer.Mode = HttpRecorderMode.Playback;
+            TestEnvironment env = TestEnvironmentFactory.GetTestEnvironment();
+            string tenantId = env.ConnectionString.KeyValuePairs[ConnectionStringKeys.AADTenantKey];
+            Assert.False(string.IsNullOrEmpty(tenantId));
+            Assert.Equal<string>("72f988bf-86f1-41af-91ab-2d7cd011db47", tenantId);
+        }
+
     }
 }
