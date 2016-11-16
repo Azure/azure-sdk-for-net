86cea77fb47512e5ae88819af81c650eaea6d302
anuchan <anuamd@gmail.com>
2016-11-16 13:04:16 -0800

    Adding IHasId, IHasName, Country[ISO|Phone]Code

---
+++ src/ResourceManagement/ResourceManager/Microsoft.Azure.Management.ResourceManager.Fluent/Domain/Core/IHasName.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information. 
+
+namespace Microsoft.Azure.Management.Resource.Fluent.Core
+{
+    /// <summary>
+    /// An interface representing a model that has a name.
+    /// </summary>
+    public interface IHasName
+    {
+        /// <summary>
+        /// Gets the name of the resource
+        /// </summary>
+        string Name { get; }
+    }
+}
