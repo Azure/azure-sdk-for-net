c1fb07740669a5bba6aa64a5f89ff7202bd70dfd
Jason Yang <bo.yang@hotmail.com>
2016-11-17 10:01:58 +0800

    [Storage] fix the generation script

--- src/ResourceManagement/Storage/Microsoft.Azure.Management.Storage/generate.cmd
+++ src/ResourceManagement/Storage/Microsoft.Azure.Management.Storage/generate.cmd
@@ -14,4 +14,4 @@ set repoRoot=%~dp0..\..\..\..
 set generateFolder=%~dp0Generated
 
 if exist %generateFolder% rd /S /Q  %generateFolder%
-call "%repoRoot%\tools\autorest.gen.cmd" %specFile% Microsoft.Azure.Management.Storage %autoRestVersion% %generateFolder% "-FT 2"
+call "%repoRoot%\tools\autorest.gen.cmd" %specFile% Microsoft.Azure.Management.Storage %autoRestVersion% %generateFolder% MICROSOFT_MIT "-FT 2"
