82cccd84a2f1b343c8b46d7bba59492fd6aba1f0
anuchan <anuamd@gmail.com>
2016-11-17 11:56:50 -0800

    Adding AutoRest generated code

---
+++ src/ResourceManagement/Dns/Microsoft.Azure.Management.Fluent.Dns/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.Azure.Management.Fluent.Dns")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e14b7bcb-e8d6-41c9-8db5-31dd3a59a90f")]
