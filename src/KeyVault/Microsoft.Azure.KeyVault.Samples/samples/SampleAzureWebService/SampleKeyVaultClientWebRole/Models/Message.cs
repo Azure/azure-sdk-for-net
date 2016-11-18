59b1e7fa23a9db864e951727483010ce39abe226
Amit Bapat <ambapat@microsoft.com>
2016-11-18 11:46:36 -0800

    License changed to MIT

--- src/KeyVault/Microsoft.Azure.KeyVault.Samples/samples/SampleAzureWebService/SampleKeyVaultClientWebRole/Models/Message.cs
+++ src/KeyVault/Microsoft.Azure.KeyVault.Samples/samples/SampleAzureWebService/SampleKeyVaultClientWebRole/Models/Message.cs
@@ -1,19 +1,8 @@
-﻿// 
-// Copyright © Microsoft Corporation, All Rights Reserved
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
-// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
-// ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A
-// PARTICULAR PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
-//
-// See the Apache License, Version 2.0 for the specific language
-// governing permissions and limitations under the License.
 
 using System;
 using Microsoft.WindowsAzure.Storage.Table;
