ec4bcad01c6a5aa1f8c3f6d3c241f13b01f41caf
itowlson <github@hestia.cc>
2016-12-02 18:57:14 +1300

    Update Batch file conventions library to take it out of preview (#2591)

--- src/Batch/FileConventions/Src/AzureBatchFileConventions/Properties/AssemblyInfo.cs
+++ src/Batch/FileConventions/Src/AzureBatchFileConventions/Properties/AssemblyInfo.cs
@@ -19,8 +19,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Azure Batch File Conventions")]
 [assembly: AssemblyDescription("A convention-based library for saving and retrieving Azure Batch task output files.")]
 
-[assembly: AssemblyVersion("0.9.4.0")] 
-[assembly: AssemblyFileVersion("0.9.4.0")]
+[assembly: AssemblyVersion("1.0.0.0")] 
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("Microsoft Azure")]
 [assembly: AssemblyCopyright("Copyright © Microsoft Corporation. All rights reserved.")]
