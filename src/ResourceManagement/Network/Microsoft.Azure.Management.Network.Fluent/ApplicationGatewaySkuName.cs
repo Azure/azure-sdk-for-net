22929c04dde101b16ff05507550c9d19445ee8da
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-12-11 06:52:55 -0800

    rebasing ApplicationGatewaySkuName to use ExpandableStringEnum

--- src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewaySkuName.cs
+++ src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewaySkuName.cs
@@ -1,69 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Azure.Management.Resource.Fluent.Core;
 
 namespace Microsoft.Azure.Management.Network.Fluent.Models
 {
-    public class ApplicationGatewaySkuName
+    public class ApplicationGatewaySkuName : ExpandableStringEnum<ApplicationGatewaySkuName>
     {
-        public static readonly ApplicationGatewaySkuName STANDARD_SMALL = new ApplicationGatewaySkuName("Standard_Small");
-        public static readonly ApplicationGatewaySkuName STANDARD_MEDIUM = new ApplicationGatewaySkuName("Standard_Medium");
-        public static readonly ApplicationGatewaySkuName STANDARD_LARGE = new ApplicationGatewaySkuName("Standard_Large");
-
-        private string value;
-
-        public override int GetHashCode()
-        {
-            return this.value.GetHashCode();
-        }
-
-        public static bool operator ==(ApplicationGatewaySkuName lhs, ApplicationGatewaySkuName rhs)
-        {
-            if (object.ReferenceEquals(lhs, null))
-            {
-                return object.ReferenceEquals(rhs, null);
-            }
-            return lhs.Equals(rhs);
-        }
-
-        public static bool operator !=(ApplicationGatewaySkuName lhs, ApplicationGatewaySkuName rhs)
-        {
-            return !(lhs == rhs);
-        }
-
-        public override bool Equals(object obj)
-        {
-            string value = this.ToString();
-            if (!(obj is ApplicationGatewaySkuName))
-            {
-                return false;
-            }
-
-            if (object.ReferenceEquals(obj, this))
-            {
-                return true;
-            }
-            ApplicationGatewaySkuName rhs = (ApplicationGatewaySkuName)obj;
-            if (value == null)
-            {
-                return rhs.value == null;
-            }
-            return value.Equals(rhs.value);
-        }
-
-        public override string ToString()
-        {
-            return this.value;
-        }
-
-        /// <summary>
-        /// Creates ApplicationGatewaySkuName.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        public ApplicationGatewaySkuName(string value)
-        {
-            this.value = value;
-        }
+        public static readonly ApplicationGatewaySkuName StandardSmall = new ApplicationGatewaySkuName() { Value = "Standard_Small" };
+        public static readonly ApplicationGatewaySkuName StandardMedium = new ApplicationGatewaySkuName() { Value = "Standard_Medium" };
+        public static readonly ApplicationGatewaySkuName StandardLarge = new ApplicationGatewaySkuName() { Value = "Standard_Large" };
     }
 }
