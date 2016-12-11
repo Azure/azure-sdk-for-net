235ce42f8a1fde68e11853e5aeb769ae6cd94233
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-12-11 09:17:13 -0800

    converting app gateway ssl cert impl

--- src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewaySslCertificateImpl.cs
+++ src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewaySslCertificateImpl.cs
@@ -2,24 +2,20 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 namespace Microsoft.Azure.Management.Network.Fluent
 {
-    using ApplicationGateway.Definition;
-    using ApplicationGateway.Update;
     using ApplicationGatewaySslCertificate.Definition;
-    using ApplicationGatewaySslCertificate.Update;
     using ApplicationGatewaySslCertificate.UpdateDefinition;
     using Models;
-    using Microsoft.Azure.Management.Resource.Fluent.Core;
-    using Microsoft.Azure.Management.Resource.Fluent.Core.ChildResource.Definition;
-    using Microsoft.Azure.Management.Resource.Fluent.Core.ChildResource.Update;
+    using Resource.Fluent.Core;
     using System.IO;
     using Resource.Fluent.Core.ChildResourceActions;
+    using System;
 
     /// <summary>
     /// Implementation for ApplicationGatewaySslCertificate.
     /// </summary>
     ///GENTHASH:Y29tLm1pY3Jvc29mdC5henVyZS5tYW5hZ2VtZW50Lm5ldHdvcmsuaW1wbGVtZW50YXRpb24uQXBwbGljYXRpb25HYXRld2F5U3NsQ2VydGlmaWNhdGVJbXBs
     internal partial class ApplicationGatewaySslCertificateImpl :
-        ChildResource<Models.ApplicationGatewaySslCertificateInner, Microsoft.Azure.Management.Network.Fluent.ApplicationGatewayImpl, Microsoft.Azure.Management.Network.Fluent.IApplicationGateway>,
+        ChildResource<Models.ApplicationGatewaySslCertificateInner, ApplicationGatewayImpl, IApplicationGateway>,
         IApplicationGatewaySslCertificate,
         IDefinition<ApplicationGateway.Definition.IWithCreate>,
         IUpdateDefinition<ApplicationGateway.Update.IUpdate>,
@@ -28,72 +24,60 @@ namespace Microsoft.Azure.Management.Network.Fluent
         ///GENMHASH:94F90A4A28B1E983315F51729B27DB77:71EBC245EFF014FF21D0CBEC5F53A54C
         public ApplicationGatewaySslCertificateImpl WithPfxFromFile(FileInfo pfxFile)
         {
-            //$ if (pfxFile == null) {
-            //$ return null;
-            //$ }
-            //$ 
-            //$ byte[] content;
-            //$ try {
-            //$ content = Files.ReadAllBytes(pfxFile.ToPath());
-            //$ return withPfxFromBytes(content);
-            //$ } catch (IOException e) {
-            //$ e.PrintStackTrace();
-            //$ return null;
-            //$ }
+            if (pfxFile == null) {
+                return null;
+            }
 
-            return this;
+            try
+            {
+                byte[] content = File.ReadAllBytes(pfxFile.FullName);
+                return (content != null) ? WithPfxFromBytes(content) : null;
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         ///GENMHASH:10A46E6A29805F84C3DCA9670AF5A0BC:C0847EA0CDA78F6D91EFD239C70F0FA7
         internal ApplicationGatewaySslCertificateImpl(ApplicationGatewaySslCertificateInner inner, ApplicationGatewayImpl parent) : base(inner, parent)
         {
-            //$ super(inner, parent);
-            //$ }
-
         }
 
         ///GENMHASH:F2EE2F07B6724F1FF58043814706A8ED:DB3DCD6CAFDAF739E813082765CD3375
         public ApplicationGatewaySslCertificateImpl WithPfxFromBytes(params byte[] pfxData)
         {
-            //$ String encoded = new String(BaseEncoding.Base64().Encode(pfxData));
-            //$ this.Inner.WithData(encoded);
-            //$ return this;
-
+            string encoded = null;
+            if (pfxData != null) {
+                encoded = Convert.ToBase64String(pfxData);
+            }
+            Inner.Data = encoded;
             return this;
         }
 
         ///GENMHASH:079EAB40DB57656F562BDBA357A86C43:8892D33C89EB45AB71456048E4189668
         public ApplicationGatewaySslCertificateImpl WithPfxPassword(string password)
         {
-            //$ this.Inner.WithPassword(password);
-            //$ return this;
-
+            Inner.Password = password;
             return this;
         }
 
         ///GENMHASH:3E38805ED0E7BA3CAEE31311D032A21C:61C1065B307679F3800C701AE0D87070
         public override string Name()
         {
-            //$ return this.Inner.Name();
-
-            return null;
+            return Inner.Name;
         }
 
         ///GENMHASH:077EB7776EFFBFAA141C1696E75EF7B3:C900710B9B91094E9DE2F1F22E9BCEA4
         public ApplicationGatewayImpl Attach()
         {
-            //$ this.Parent().WithSslCertificate(this);
-            //$ return this.Parent();
-
-            return null;
+            return Parent.WithSslCertificate(this);
         }
 
         ///GENMHASH:37B0EAF4E984261F22FBACEF515D705F:19A294AC60BE3434C95A61817A6A6A02
         public string PublicData()
         {
-            //$ return this.Inner.PublicCertData();
-
-            return null;
+            return Inner.PublicCertData;
         }
 
         ApplicationGateway.Update.IUpdate ISettable<ApplicationGateway.Update.IUpdate>.Parent()
