78ff6b81dd06101dd8cbe2aa8fee1d01b9758200
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-12-11 06:53:25 -0800

    rebasing ApplicationGatewayTier to use ExpandableStringEnum

--- src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewayTier.cs
+++ src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewayTier.cs
@@ -1,67 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Management.Resource.Fluent.Core;
 
 namespace Microsoft.Azure.Management.Network.Fluent.Models
 {
-    public class ApplicationGatewayTier
+    public class ApplicationGatewayTier : ExpandableStringEnum<ApplicationGatewayTier>
     {
-        public static readonly ApplicationGatewayTier STANDARD = new ApplicationGatewayTier("Standard");
-
-        private string value;
-
-        public override int GetHashCode()
-        {
-            return this.value.GetHashCode();
-        }
-
-        public static bool operator ==(ApplicationGatewayTier lhs, ApplicationGatewayTier rhs)
-        {
-            if (object.ReferenceEquals(lhs, null))
-            {
-                return object.ReferenceEquals(rhs, null);
-            }
-            return lhs.Equals(rhs);
-        }
-
-        public static bool operator !=(ApplicationGatewayTier lhs, ApplicationGatewayTier rhs)
-        {
-            return !(lhs == rhs);
-        }
-
-        public override bool Equals(object obj)
-        {
-            string value = this.ToString();
-            if (!(obj is ApplicationGatewayTier))
-            {
-                return false;
-            }
-
-            if (object.ReferenceEquals(obj, this))
-            {
-                return true;
-            }
-            ApplicationGatewayTier rhs = (ApplicationGatewayTier)obj;
-            if (value == null)
-            {
-                return rhs.value == null;
-            }
-            return value.Equals(rhs.value);
-        }
-
-        public override string ToString()
-        {
-            return this.value;
-        }
-
-        /// <summary>
-        /// Creates ApplicationGatewayTier.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        public ApplicationGatewayTier(string value)
-        {
-            this.value = value;
-        }
+        public static readonly ApplicationGatewayTier Standard = new ApplicationGatewayTier() { Value = "Standard" };
     }
 }
