9ff115da3312b7d92d5d63b38a34a5b7fb83b7a6
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-12-12 17:58:46 -0800

    missing license headers

--- src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewayOperationalState.cs
+++ src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewayOperationalState.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.Management.Resource.Fluent.Core;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+using Microsoft.Azure.Management.Resource.Fluent.Core;
 
 namespace Microsoft.Azure.Management.Network.Fluent.Models
 {
