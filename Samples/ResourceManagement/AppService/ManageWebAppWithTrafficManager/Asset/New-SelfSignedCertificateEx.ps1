5fc5ac499f6ccdbc444e2aa9ae0587f7da485fbb
Jianghao Lu <jianghaolu@users.noreply.github.com>
2016-12-16 11:58:14 -0800

    Address CR comments

--- Samples/ResourceManagement/AppService/ManageWebAppWithTrafficManager/Asset/New-SelfSignedCertificateEx.ps1
+++ Samples/ResourceManagement/AppService/ManageWebAppWithTrafficManager/Asset/New-SelfSignedCertificateEx.ps1
@@ -129,6 +129,7 @@ function New-SelfSignedCertificateEx {
 .Example
 	New-SelfsignedCertificateEx -Subject "CN=www.domain.com" -EKU "Server Authentication", "Client authentication" `
 	-KeyUsage "KeyEcipherment, DigitalSignature" -SAN "sub.domain.com","www.domain.com","192.168.1.1" `
+//[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine")]
 	-AllowSMIME -Path C:\test\ssl.pfx -Password (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force) -Exportable `
 	-StoreLocation "LocalMachine"
 	
