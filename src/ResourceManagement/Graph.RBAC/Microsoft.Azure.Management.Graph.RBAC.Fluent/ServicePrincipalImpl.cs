8c44c8c116acb437d35495721886bfbe9abfdd56
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2016-12-20 15:19:25 -0800

    updating converter diff hashes for Graph impls

--- src/ResourceManagement/Graph.RBAC/Microsoft.Azure.Management.Graph.RBAC.Fluent/ServicePrincipalImpl.cs
+++ src/ResourceManagement/Graph.RBAC/Microsoft.Azure.Management.Graph.RBAC.Fluent/ServicePrincipalImpl.cs
@@ -19,6 +19,7 @@ namespace Microsoft.Azure.Management.Graph.RBAC.Fluent
     /// <summary>
     /// Implementation for ServicePrincipal and its parent interfaces.
     /// </summary>
+    ///GENTHASH:Y29tLm1pY3Jvc29mdC5henVyZS5tYW5hZ2VtZW50LmdyYXBocmJhYy5pbXBsZW1lbnRhdGlvbi5TZXJ2aWNlUHJpbmNpcGFsSW1wbA==
     public partial class ServicePrincipalImpl :
         CreatableUpdatable<IServicePrincipal, ServicePrincipalInner, ServicePrincipalImpl, IServicePrincipal, IUpdate>,
         IServicePrincipal,
@@ -39,32 +40,39 @@ namespace Microsoft.Azure.Management.Graph.RBAC.Fluent
             };
         }
 
+        ///GENMHASH:17540EB75C744FB87D329C55BE359E09:CC8D1D4F5D89E231669C5963BF9F8E9C
         public string ObjectId()
         {
             return Inner.ObjectId;
         }
+        ///GENMHASH:29024ACA1EA67366DE27F7A1B972E458:75852A31ACA71709FD61BA0195203BFF
         public string ObjectType()
         {
                 return Inner.ObjectType;
         }
+        ///GENMHASH:19FB5490B29F08AC39628CD5F893E975:54FC41D8034FD612C7047E2055BC6E48
         public string DisplayName()
         {
                 return Inner.DisplayName;
         }
+        ///GENMHASH:CF00964037C1AADDCC0C25C134168C6E:DBDA3E40337A88C78112F81653959508
         public string AppId()
         {
                 return Inner.AppId;
         }
+        ///GENMHASH:3190BDAA4917D0479F1E9EBBDAC6590C:9C7DB50BD87DDC349B690C1F34C49A26
         public IList<string> ServicePrincipalNames()
         {
                 return Inner.ServicePrincipalNames;
         }
+        ///GENMHASH:8B8E171AB3970DFD7516F84B8C19861C:4549C714DB7AE421B3ED125CD30CFEF9
         public ServicePrincipalImpl WithAccountEnabled(bool enabled)
         {
             createParameters.AccountEnabled = enabled;
             return this;
         }
 
+        ///GENMHASH:4002186478A1CB0B59732EBFB18DEB3A:6FBD633E7AC3512A3078AD9811DEC068
         public override IServicePrincipal Refresh()
         {
             var inner = client.List(new Rest.Azure.OData.ODataQuery<ServicePrincipalInner>(string.Format("servicePrincipalNames/any(c:c eq '{0}')", AppId())));
@@ -72,6 +80,7 @@ namespace Microsoft.Azure.Management.Graph.RBAC.Fluent
             return this;
         }
 
+        ///GENMHASH:0202A00A1DCF248D2647DBDBEF2CA865:E9A4DA014B21051979442ACE026C7D1F
         public override async Task<IServicePrincipal> CreateResourceAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
             throw new NotImplementedException();
--- src/ResourceManagement/Graph.RBAC/Microsoft.Azure.Management.Graph.RBAC.Fluent/UserImpl.cs
+++ src/ResourceManagement/Graph.RBAC/Microsoft.Azure.Management.Graph.RBAC.Fluent/UserImpl.cs
@@ -15,6 +15,7 @@ namespace Microsoft.Azure.Management.Graph.RBAC.Fluent
     /// <summary>
     /// Implementation for User and its parent interfaces.
     /// </summary>
+    ///GENTHASH:Y29tLm1pY3Jvc29mdC5henVyZS5tYW5hZ2VtZW50LmdyYXBocmJhYy5pbXBsZW1lbnRhdGlvbi5Vc2VySW1wbA==
     public partial class UserImpl :
         CreatableUpdatable<IUser, UserInner, UserImpl, IUser, IUpdate>,
         IUser,
@@ -34,53 +35,64 @@ namespace Microsoft.Azure.Management.Graph.RBAC.Fluent
             };
         }
 
+        ///GENMHASH:17540EB75C744FB87D329C55BE359E09:CC8D1D4F5D89E231669C5963BF9F8E9C
         public string ObjectId()
         {
             return Inner.ObjectId;
         }
+        ///GENMHASH:29024ACA1EA67366DE27F7A1B972E458:75852A31ACA71709FD61BA0195203BFF
         public string ObjectType()
         {
             return Inner.ObjectType; 
         }
+        ///GENMHASH:EFC2DDE75F20FD4EAFECA334A50B3D22:5BCCB61C94FA98FEF1E7D90DCFADBDAC
         public string UserPrincipalName()
         {
             return Inner.UserPrincipalName;
         }
+        ///GENMHASH:19FB5490B29F08AC39628CD5F893E975:54FC41D8034FD612C7047E2055BC6E48
         public string DisplayName()
         {
             return Inner.DisplayName;
         }
+        ///GENMHASH:A150E806D025822AB5DF5438B386C9F2:B9161EACDBFE5F3F7DD00C6D41D6BAD9
         public string SignInName()
         {
             return Inner.SignInName;
         }
+        ///GENMHASH:16EBDFC8CC602D680E0E32486328D684:D10E9C733A0AA33D44CE2D08891F4EBE
         public string Mail()
         {
             return Inner.Mail;
         }
+        ///GENMHASH:EE18A3AEA61BB82949B949BE68947AEF:63DB386E96A2AED639FF5547DC1C798C
         public string MailNickname()
         {
             return Inner.MailNickname;
         }
 
+        ///GENMHASH:8B8E171AB3970DFD7516F84B8C19861C:4549C714DB7AE421B3ED125CD30CFEF9
         public UserImpl WithAccountEnabled (bool enabled)
         {
             createParameters.AccountEnabled = enabled;
             return this;
         }
 
+        ///GENMHASH:EDAD7B033C93B5FEE1419E438619ABF3:A38066E817331F8A02982D526BB9FCD4
         public UserImpl WithDisplayName (string displayName)
         {
             createParameters.DisplayName = displayName;
             return this;
         }
 
+        ///GENMHASH:75B32599B63ECD9641B6137878074EDC:6FBD2E638868C5040F1A6890F0E21C85
         public UserImpl WithMailNickname (string mailNickname)
         {
             createParameters.MailNickname = mailNickname;
             return this;
         }
 
+        ///GENMHASH:60802F24CA383B85E354D13337FC6F3B:7D053BF8F6F40DCECFA694D1267667E2
         public UserImpl WithPassword (string password)
         {
             createParameters.PasswordProfile = new PasswordProfile
@@ -90,6 +102,7 @@ namespace Microsoft.Azure.Management.Graph.RBAC.Fluent
             return this;
         }
 
+        ///GENMHASH:7F35F6C48498D28343D85BFC4C2F3D05:16256DB0E4447967C89B6A1BC27BBEDB
         public UserImpl WithPassword (string password, bool forceChangePasswordNextLogin)
         {
             createParameters.PasswordProfile = new PasswordProfile
@@ -100,6 +113,7 @@ namespace Microsoft.Azure.Management.Graph.RBAC.Fluent
             return this;
         }
 
+        ///GENMHASH:4002186478A1CB0B59732EBFB18DEB3A:5074689AB449399B3C11F611FD3EFF94
         public override IUser Refresh ()
         {
             var inner = client.Get(UserPrincipalName());
@@ -107,7 +121,8 @@ namespace Microsoft.Azure.Management.Graph.RBAC.Fluent
             return this;
         }
 
-        public override Task<IUser> CreateResourceAsync (CancellationToken cancellationToken = default(CancellationToken))
+        ///GENMHASH:0202A00A1DCF248D2647DBDBEF2CA865:E9A4DA014B21051979442ACE026C7D1F
+        public override Task<IUser> CreateResourceAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
             throw new NotImplementedException();
         }
