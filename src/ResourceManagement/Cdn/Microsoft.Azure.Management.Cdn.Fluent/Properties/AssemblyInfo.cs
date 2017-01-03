05e796f6e75952837b8deb778f19544e54b8213d
Hovsep <hovsepm@microsoft.com>
2017-01-03 13:55:11 -0800

    Release beta 4.1 (#2677)

--- src/ResourceManagement/Cdn/Microsoft.Azure.Management.Cdn.Fluent/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/Cdn/Microsoft.Azure.Management.Cdn.Fluent/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Microsoft Azure CDN Management Library")]
 [assembly: AssemblyDescription("Provides Microsoft Azure CDN management APIs.")]
 
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.41")]
+[assembly: AssemblyFileVersion("1.0.0.41")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
