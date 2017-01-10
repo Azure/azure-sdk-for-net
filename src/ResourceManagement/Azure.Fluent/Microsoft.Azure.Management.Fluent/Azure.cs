6c877f22c9b1576fc00c0948c8c34cb0a3ca9aaa
Hovsep <hovsepm@microsoft.com>
2017-01-09 18:55:25 -0800

    Fixed build break (#2706)
    * Fixed build break
    
    * excluded json file from cred scan.
    
    * Addressed review comment.

--- src/ResourceManagement/Azure.Fluent/Microsoft.Azure.Management.Fluent/Azure.cs
+++ src/ResourceManagement/Azure.Fluent/Microsoft.Azure.Management.Fluent/Azure.cs
@@ -16,6 +16,7 @@ using Microsoft.Azure.Management.Storage.Fluent;
 using System.Linq;
 using Microsoft.Azure.Management.Trafficmanager.Fluent;
 using Microsoft.Azure.Management.AppService.Fluent;
+using System;
 
 namespace Microsoft.Azure.Management.Fluent
 {
@@ -352,7 +353,10 @@ namespace Microsoft.Azure.Management.Fluent
                         RestClient.Configure()
                             .WithBaseUri(restClient.BaseUri)
                             .WithCredentials(restClient.Credentials).Build());
-                    var subscription = resourceManager.Subscriptions.List().FirstOrDefault();
+                    var subscription = resourceManager.Subscriptions.List()
+                        .FirstOrDefault(s =>
+                            StringComparer.OrdinalIgnoreCase.Equals(s.State, "Enabled") ||
+                            StringComparer.OrdinalIgnoreCase.Equals(s.State, "Warned"));
 
                     return WithSubscription(subscription?.SubscriptionId);
                 }
