76d6bc01db7fdc6a3ebbfed14ad3f81a9ca40501
shahabhijeet <abhimswork@outlook.com>
2017-02-24 10:47:00 -0800

    Updating HttpRecorder, TestFramework and tests. Adding Interactive Login support

--- src/TestFramework/TestFramework.Net45Tests/TestEnvironment/TestEnvTests.cs
+++ src/TestFramework/TestFramework.Net45Tests/TestEnvironment/TestEnvTests.cs
@@ -10,13 +10,14 @@ namespace TestFramework.Net45Tests
 
     public class TestEnvTests
     {
-        [Theory(/*Skip = "Interactive Tests, needs to be run manually"*/)]
-        [InlineData("AADTenant=04081b9f-c8b4-424b-86b9-ce44721c2b23")]
-        [InlineData("AADTenant=04081b9f-c8b4-424b-86b9-ce44721c2b23;SubscriptionId=None;Environment=Prod")]
+        [Theory(Skip = "Interactive Tests, needs to be run manually")]
+        [InlineData("AADTenant=<GetCSPtenantId>")]
+        [InlineData("AADTenant=<GetCSPtenantId>;SubscriptionId=None;Environment=Prod")] //Test this with None as the SubId
         public void InteractiveLoginForCSP(string connStr)
         {
             // Use this test case to set connection string without username and password, which will prompt you to enter UserName and password
-            // verify if the test environment has non empty username
+            // use CSP username/pwd that we have. This account has no subscription associated, hence throws exception.
+            // TestEnv checks if the logged in user has the subscription that is provided in the connection string
             Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", connStr);
             HttpMockServer.Mode = HttpRecorderMode.Record;
             try
@@ -32,10 +33,12 @@ namespace TestFramework.Net45Tests
 
 
         [Theory(Skip = "Interactive Tests, needs to be run manually")]
-        [InlineData("AADTenant=04081b9f-c8b4-424b-86b9-ce44721c2b23;SubscriptionId=6b085460-5f21-477e-ba44-1035046e9101;Environment=Prod")]
+        [InlineData("AADTenant=<GetMsftTenantId>;SubscriptionId=<GetAnySubIdUnderYourAlias>;Environment=Prod")]
         public void InteractiveLoginKnownUserName(string connStr)
         {
             // Log in for this test case using your alias
+            // the idea is to initiate auth even on 2FA tenant (in this case msft)
+            // have a valid tenant id and subscription your alias has access to.
             Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", connStr);
             HttpMockServer.Mode = HttpRecorderMode.Record;
             TestEnvironment env = TestEnvironmentFactory.GetTestEnvironment();
@@ -46,23 +49,7 @@ namespace TestFramework.Net45Tests
 
         // Please update the connection string with the right information and then run the test
         [Theory(Skip = "Interactive Tests, needs to be run manually")]
-        [InlineData("SubscriptionId=<updateSubId>;" +
-                "AADTenant=dcfa120f-9293-4f06-b3d2-cf728bcabb10;" +
-                "HttpRecorderMode=Record;" +
-                "Environment=Custom;" +
-                "ResourceManagementUri=https://management.microsoftazure.de/;" +
-                "ServiceManagementUri=https://management.core.cloudapi.de/;" +
-                "GalleryUri=https://gallery.cloudapi.de/;" +
-                "GraphUri=https://graph.cloudapi.de/;" +
-                "AADAuthUri=https://login.microsoftonline.de/;" +
-                "IbizaPortalUri=http://portal.microsoftazure.de/;" +
-                "RdfePortalUri=https://management.core.cloudapi.de/;" +
-                "GraphTokenAudienceUri=https://graph.cloudapi.de/;" +
-                "AADTokenAudienceUri=https://management.core.cloudapi.de/"
-            )]
-
-
-        [InlineData("SubscriptionId=<updateSubId>;" +
+        [InlineData("SubscriptionId=<subId>;" +
                 "AADTenant=dcfa120f-9293-4f06-b3d2-cf728bcabb10;" +
                 "HttpRecorderMode=Record;" +
                 "Environment=Custom;" +
@@ -78,7 +65,6 @@ namespace TestFramework.Net45Tests
             )]
         public void InteractiveLoginGermanCloud(string connStr)
         {
-            //"AADAuthUri=https://login.microsoftonline.de/;" +
             // Log in for this test case using your alias
             Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", connStr);
             HttpMockServer.Mode = HttpRecorderMode.Record;
