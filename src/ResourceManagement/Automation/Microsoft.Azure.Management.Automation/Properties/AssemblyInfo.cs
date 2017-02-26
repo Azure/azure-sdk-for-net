5c9e5a7cd96a53fad93cdd2756540fc8307ef7e6
shahabhijeet <abhimswork@outlook.com>
2017-02-25 19:08:33 -0800

    Adding netcore Automation project structure. Moving existing project to legacy directory. Currently only supports 45. This is just the first commit. Still remaining is test project as well as adding support for .net standard 1.5

---
+++ src/ResourceManagement/Automation/Microsoft.Azure.Management.Automation/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.Azure.Management.Automation")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.52.0.0")]
+[assembly: AssemblyFileVersion("0.52.0.0")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fd8d0334-15a6-4b61-91dd-f6af90a3c9fa")]
