f48e1e0b51c89fec92fa56d17a3a41df42182e8a
shahabhijeet <abhimswork@outlook.com>
2017-03-09 16:45:39 -0800

    Updated RepoTask module with new supported KeyVaule pairs

--- tools/Repo-Tasks.psm1
+++ tools/Repo-Tasks.psm1
@@ -14,7 +14,7 @@ In order to successfully run a test, you will need SubscriptionId, TenantId
 This cmdlet will only prompt you for Subscription and Tenant information, rest all other parameters are optional
 
 #>
-    [CmdletBinding(DefaultParameterSetName='UserIdParamSet')]
+    [CmdletBinding(DefaultParameterSetName='SpnParamSet')]
     param(
         [Parameter(ParameterSetName='UserIdParamSet', Mandatory=$true, HelpMessage = "UserId (OrgId) you would like to use")]
         [ValidateNotNullOrEmpty()]
@@ -44,11 +44,23 @@ This cmdlet will only prompt you for Subscription and Tenant information, rest a
         [ValidateSet("Playback", "Record", "None")]
         [string]$RecordMode='Playback',
 
-        [ValidateSet("Prod", "Dogfood", "Current", "Next")]
-        [string]$TargetEnvironment='Prod'
+        [ValidateSet("Prod", "Dogfood", "Current", "Next", "Custom")]
+        [string]$TargetEnvironment='Prod',
+		
+		[string]$ResourceManagementUri,
+		[string]$GraphUri,
+		[string]$AADAuthUri,
+		[string]$AADTokenAudienceUri,
+		[string]$GraphTokenAudienceUri,		
+		[string]$IbizaPortalUri,
+		[string]$ServiceManagementUri,
+		[string]$RdfePortalUri,
+		[string]$GalleryUri,
+		[string]$DataLakeStoreServiceUri,
+		[string]$DataLakeAnalyticsJobAndCatalogServiceUri
     )
 
-    [string]$uris="https://management.azure.com/"
+    [string]$uris=""
 
     $formattedConnStr = [string]::Format("SubscriptionId={0};HttpRecorderMode={1};Environment={2}", $SubscriptionId, $RecordMode, $TargetEnvironment)
 
@@ -76,11 +88,69 @@ This cmdlet will only prompt you for Subscription and Tenant information, rest a
     {
         $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";ServicePrincipalSecret={0}"), $ServicePrincipalSecret)
     }
+	
+	#Uris
+	if([string]::IsNullOrEmpty($ResourceManagementUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";ResourceManagementUri={0}"), $ResourceManagementUri)
+    }
+	
+	if([string]::IsNullOrEmpty($GraphUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";GraphUri={0}"), $GraphUri)
+    }
+	
+	if([string]::IsNullOrEmpty($AADAuthUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";AADAuthUri={0}"), $AADAuthUri)
+    }
+	
+	if([string]::IsNullOrEmpty($AADTokenAudienceUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";AADTokenAudienceUri={0}"), $AADTokenAudienceUri)
+    }
+	
+	if([string]::IsNullOrEmpty($GraphTokenAudienceUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";GraphTokenAudienceUri={0}"), $GraphTokenAudienceUri)
+    }
+	
+	if([string]::IsNullOrEmpty($IbizaPortalUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";IbizaPortalUri={0}"), $IbizaPortalUri)
+    }
+	
+	if([string]::IsNullOrEmpty($ServiceManagementUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";ServiceManagementUri={0}"), $ServiceManagementUri)
+    }
+	
+	if([string]::IsNullOrEmpty($RdfePortalUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";RdfePortalUri={0}"), $RdfePortalUri)
+    }
+	
+	if([string]::IsNullOrEmpty($GalleryUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";GalleryUri={0}"), $GalleryUri)
+    }
+	
+	if([string]::IsNullOrEmpty($DataLakeStoreServiceUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";DataLakeStoreServiceUri={0}"), $DataLakeStoreServiceUri)
+    }
+	
+	if([string]::IsNullOrEmpty($DataLakeAnalyticsJobAndCatalogServiceUri) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";DataLakeAnalyticsJobAndCatalogServiceUri={0}"), $DataLakeAnalyticsJobAndCatalogServiceUri)
+    }
+	
     
-    $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";BaseUri={0}"), $uris)
+    #$formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";BaseUri={0}"), $uris)
 
     Write-Host "Below connection string is ready to be set"
-    Print-ConnectionString $UserId $Password $SubscriptionId $TenantId $ServicePrincipal $ServicePrincipalSecret $RecordMode $TargetEnvironment $uris
+    #Print-ConnectionString $UserId $Password $SubscriptionId $TenantId $ServicePrincipal $ServicePrincipalSecret $RecordMode $TargetEnvironment $uris 
+	Print-ConnectionString $UserId $Password $SubscriptionId $TenantId $ServicePrincipal $ServicePrincipalSecret $RecordMode $TargetEnvironment $uris $ResourceManagementUri $GraphUri $AADAuthUri $AADTokenAudienceUri $GraphTokenAudienceUri $IbizaPortalUri $ServiceManagementUri $RdfePortalUri $GalleryUri $DataLakeStoreServiceUri $DataLakeAnalyticsJobAndCatalogServiceUri
 
     #Set connection string to Environment variable
     $env:TEST_CSM_ORGID_AUTHENTICATION=$formattedConnStr
@@ -149,11 +219,66 @@ Function Print-ConnectionString([string]$uid, [string]$pwd, [string]$subId, [str
         Write-Host "Environment=" -ForegroundColor Green -NoNewline
         Write-Host $targetEnvironment";" -NoNewline
     }
-
-    if([string]::IsNullOrEmpty($uris) -eq $false)
+	
+	#========================================
+	if([string]::IsNullOrEmpty($ResourceManagementUri) -eq $false)
     {
-        Write-Host "BaseUri=" -ForegroundColor Green -NoNewline
-        Write-Host $uris -NoNewline
+        Write-Host "ResourceManagementUri=" -ForegroundColor Green -NoNewline
+        Write-Host $ResourceManagementUri";" -NoNewline
+    }
+	
+	if([string]::IsNullOrEmpty($GraphUri) -eq $false)
+    {
+        Write-Host "GraphUri=" -ForegroundColor Green -NoNewline
+        Write-Host $GraphUri";" -NoNewline
+    }
+	
+	if([string]::IsNullOrEmpty($AADTokenAudienceUri) -eq $false)
+    {
+        Write-Host "AADTokenAudienceUri=" -ForegroundColor Green -NoNewline
+        Write-Host $AADTokenAudienceUri";" -NoNewline
+    }
+
+    if([string]::IsNullOrEmpty($GraphTokenAudienceUri) -eq $false)
+    {
+        Write-Host "GraphTokenAudienceUri=" -ForegroundColor Green -NoNewline
+        Write-Host $GraphTokenAudienceUri -NoNewline
+    }
+	
+	if([string]::IsNullOrEmpty($IbizaPortalUri) -eq $false)
+    {
+        Write-Host "IbizaPortalUri=" -ForegroundColor Green -NoNewline
+        Write-Host $IbizaPortalUri";" -NoNewline
+    }
+	
+	if([string]::IsNullOrEmpty($ServiceManagementUri) -eq $false)
+    {
+        Write-Host "ServiceManagementUri=" -ForegroundColor Green -NoNewline
+        Write-Host $ServiceManagementUri";" -NoNewline
+    }
+	
+	if([string]::IsNullOrEmpty($RdfePortalUri) -eq $false)
+    {
+        Write-Host "RdfePortalUri=" -ForegroundColor Green -NoNewline
+        Write-Host $RdfePortalUri";" -NoNewline
+    }
+	
+	if([string]::IsNullOrEmpty($GalleryUri) -eq $false)
+    {
+        Write-Host "GalleryUri=" -ForegroundColor Green -NoNewline
+        Write-Host $GalleryUri";" -NoNewline
+    }
+	
+	if([string]::IsNullOrEmpty($DataLakeStoreServiceUri) -eq $false)
+    {
+        Write-Host "DataLakeStoreServiceUri=" -ForegroundColor Green -NoNewline
+        Write-Host $DataLakeStoreServiceUri";" -NoNewline
+    }
+	
+	if([string]::IsNullOrEmpty($DataLakeAnalyticsJobAndCatalogServiceUri) -eq $false)
+    {
+        Write-Host "DataLakeAnalyticsJobAndCatalogServiceUri=" -ForegroundColor Green -NoNewline
+        Write-Host $DataLakeAnalyticsJobAndCatalogServiceUri";" -NoNewline
     }
 
     Write-Host ""
