a04808b22f4756e4e0cca72f48696c7b15752832
Pankaj Singh Negi <pankaj.negi@microsoft.com>
2017-03-17 21:41:15 -0700

    Updated the swagger spec url

--- src/ResourceManagement/Logic/Microsoft.Azure.Management.Logic/generate.cmd
+++ src/ResourceManagement/Logic/Microsoft.Azure.Management.Logic/generate.cmd
@@ -6,7 +6,7 @@
 @echo off
 set autoRestVersion=1.0.0-Nightly20170212
 if  "%1" == "" (
-    set specFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/63df6aadf9c9d2d3e38617d67e86cb3373a97148/arm-logic/2016-06-01/swagger/logic.json"
+    set specFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/bac1c76f3fa8f8d3ffa27264b9f65b32a31e25e5/arm-logic/2016-06-01/swagger/logic.json"
 ) else (
     set specFile="%1"
 )
