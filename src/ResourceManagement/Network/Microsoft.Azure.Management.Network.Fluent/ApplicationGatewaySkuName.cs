23f371186112e3934716520d5707c880ff83464d
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2017-03-22 15:27:01 -0700

    Regenerated Network

--- src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewaySkuName.cs
+++ src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/ApplicationGatewaySkuName.cs
@@ -9,5 +9,7 @@ namespace Microsoft.Azure.Management.Network.Fluent.Models
         public static readonly ApplicationGatewaySkuName StandardSmall = Parse("Standard_Small");
         public static readonly ApplicationGatewaySkuName StandardMedium = Parse("Standard_Medium");
         public static readonly ApplicationGatewaySkuName StandardLarge = Parse("Standard_Large");
+        public static readonly ApplicationGatewaySkuName WAFMedium = Parse("WAF_Medium");
+        public static readonly ApplicationGatewaySkuName WAFLarge = Parse("WAF_Large");
     }
 }
