dbab9efbcef53c1cca3c9c3b80e5132d39bd4afd
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-03-30 13:38:36 -0700

    marking App Gateways and Load Balancers as Beta

--- src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/Domain/IApplicationGateways.cs
+++ src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/Domain/IApplicationGateways.cs
@@ -10,6 +10,9 @@ namespace Microsoft.Azure.Management.Network.Fluent
     /// <summary>
     /// Entry point to application gateway management API in Azure.
     /// </summary>
+    /// <remarks>
+    /// (Beta: This functionality is in preview and as such is subject to change in non-backwards compatible ways in future releases, including removal, regardless of any compatibility expectations set by the containing library version number.)
+    /// </remarks>
     public interface IApplicationGateways  :
         ISupportsCreating<ApplicationGateway.Definition.IBlank>,
         ISupportsListing<Microsoft.Azure.Management.Network.Fluent.IApplicationGateway>,
