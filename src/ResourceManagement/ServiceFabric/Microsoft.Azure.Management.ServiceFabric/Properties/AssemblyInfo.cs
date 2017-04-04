07cc2b38708d376c8840c571b317a2b67f2e92c9
Ching Chen <qingc1489@gmail.com>
2017-04-04 11:32:53 -0700

    Add ServiceFabric SDK (#2903)
    * Add ServiceFabric SDK
    
    * update with latest swagger file
    
    * update sdk with latest Swagger
    
    * update test case
    
    * update with latest swagger file
    
    * fix build warning
    
    * fix swagger file path

---
+++ src/ResourceManagement/ServiceFabric/Microsoft.Azure.Management.ServiceFabric/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServiceFabric")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServiceFabric")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
