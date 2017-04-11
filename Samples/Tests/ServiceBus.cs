521ae2917d03b5f474a37ee182fc0055c8cb89b0
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2017-04-11 13:33:50 -0700

    Fixed missed license headers.

--- Samples/Tests/ServiceBus.cs
+++ Samples/Tests/ServiceBus.cs
@@ -1,4 +1,7 @@
-﻿using Azure.Tests;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Azure.Tests;
 using Fluent.Tests.Common;
 using System;
 using System.Collections.Generic;
