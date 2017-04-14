f11f4168bf27877ffe52bbab0fb3942b04db393c
Guillermo Calderón_Meza <gucalder@microsoft.com>
2017-04-14 13:36:53 -0700

    Adding validate.cmd and improving generate.cmd

--- src/ResourceManagement/Monitor/Microsoft.Azure.Monitor/validate.cmd
+++ src/ResourceManagement/Monitor/Microsoft.Azure.Monitor/validate.cmd
@@ -2,8 +2,8 @@
 :: Microsoft Azure SDK for Net - Generate library code 
 :: Copyright (C) Microsoft Corporation. All Rights Reserved. 
 :: 
- 
-@echo off 
+
+@echo off
 
 if  "%2" == "" (
    set specFile1="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/0fde282c1aac0c9a07c35dd7054b3723bd890d45/monitor/compositeMonitorClient.json"
@@ -17,12 +17,6 @@ if  "%3" == "" (
    set specFile2="%3"
 )
 
-set generateFolder1=%~dp0Generated\Monitor
-set generateFolder2=%~dp0Generated\Management\Monitor
-
-if exist %generateFolder1% rd /S /Q  %generateFolder1%
-if exist %generateFolder2% rd /S /Q  %generateFolder2%
-
 if "%1" == "install" goto install
 goto new
 
@@ -31,5 +25,10 @@ echo **** INFO: Installing AutoRest package
 call npm install autorest -g
 
 :new
-call autoRest -Modeler CompositeSwagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Insights -Input %specFile1% -outputDirectory %generateFolder1% -Header MICROSOFT_MIT -FT 1
-call autoRest -Modeler CompositeSwagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Management.Insights -Input %specFile2% -outputDirectory %generateFolder2% -Header MICROSOFT_MIT -FT 1
+call autoRest -Modeler CompositeSwagger -CodeGenerator None -Input %specFile1% -jsonvalidationmessages
+call autoRest -Modeler CompositeSwagger -CodeGenerator None -Input %specFile2% -jsonvalidationmessages
+
+rem autoRest -CodeGenerator None -I G:\GitHub\azure-rest-api-specs\monitor\2014-04-01\swagger\usageMetrics_API.json -jsonvalidationmessages
+rem autoRest -CodeGenerator None -I G:\GitHub\azure-rest-api-specs\arm-monitor\2015-04-01\swagger\autoscale_API.json
+
+
