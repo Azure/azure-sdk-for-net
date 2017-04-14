48103d1928e11c92ca3b985b4f5cf193c48e636a
Alva Bandy <alvab@microsoft.com>
2017-04-14 15:09:32 -0700

    Test fixes.

--- src/ResourceManagement/Sql/Microsoft.Azure.Management.Sql.Fluent/ServiceTierAdvisorImpl.cs
+++ src/ResourceManagement/Sql/Microsoft.Azure.Management.Sql.Fluent/ServiceTierAdvisorImpl.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Azure.Management.Sql.Fluent
     {
         private ResourceId resourceId;
         private IDatabasesOperations databasesInner;
-        private IEnumerable<ISloUsageMetricInterface> sloUsageMetrics;
+        private IReadOnlyList<ISloUsageMetricInterface> sloUsageMetrics;
 
         ///GENMHASH:0150BB5F92ED226BF84D3AC5255EFE3F:F8EEA9E1BE10E299F96C7CA9D025C464
         public string CurrentServiceLevelObjective()
@@ -60,13 +60,14 @@ namespace Microsoft.Azure.Management.Sql.Fluent
         }
 
         ///GENMHASH:7FD5A8D2A26E9E6B12E7585A7DBE1CE3:9055AAEF8F9A4DFD881586C59581634F
-        public IEnumerable<Microsoft.Azure.Management.Sql.Fluent.ISloUsageMetricInterface> ServiceLevelObjectiveUsageMetrics()
+        public IReadOnlyList<Microsoft.Azure.Management.Sql.Fluent.ISloUsageMetricInterface> ServiceLevelObjectiveUsageMetrics()
         {
             if (sloUsageMetrics == null)
             {
-                Func<SloUsageMetric, SloUsageMetricImpl> convertor
-                    = (sloUsageMetricInner) => new SloUsageMetricImpl(sloUsageMetricInner);
-                sloUsageMetrics = Inner.ServiceLevelObjectiveUsageMetrics.Select(inner => convertor(inner));
+                Func<SloUsageMetric, ISloUsageMetricInterface> convertor
+                    = (sloUsageMetricInner) => 
+                    new SloUsageMetricImpl(sloUsageMetricInner);
+                sloUsageMetrics = Inner.ServiceLevelObjectiveUsageMetrics.Select(inner => convertor(inner)).ToList();
             }
 
             return sloUsageMetrics;
