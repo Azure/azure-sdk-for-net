3869502ffb36177d3122512c9f5804e7dbdd2800
Abhijeet Shah <abhishah@microsoft.com>
2017-04-14 12:40:36 -0700

    adjusting references adding new targets

--- src/SdkCommon/ClientRuntime/Test/ClientRuntime.E2E.Tests/Properties/AssemblyInfo.cs
+++ src/SdkCommon/ClientRuntime/Test/ClientRuntime.E2E.Tests/Properties/AssemblyInfo.cs
@@ -5,10 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+/*
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Microsoft.Rest.ClientRuntime.E2E.Tests")]
 [assembly: AssemblyTrademark("")]
+*/
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
--- src/SdkCommon/TestFramework/TestFramework.Net45Tests/Properties/AssemblyInfo.cs
+++ src/SdkCommon/TestFramework/TestFramework.Net45Tests/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+/*
 [assembly: AssemblyTitle("Net45Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -13,6 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+*/
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +36,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
