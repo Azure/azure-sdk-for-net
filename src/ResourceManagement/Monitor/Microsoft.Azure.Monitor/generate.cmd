dbc18f4833c325dd4cecd4186de094c775a2bd32
Guillermo Calderón_Meza <gucalder@microsoft.com>
2017-04-14 13:09:31 -0700

    Updating generate.cmd to reflect the lastest commit in the Swagger spec repo and to use only the new release of AutoRest

--- src/ResourceManagement/Monitor/Microsoft.Azure.Monitor/generate.cmd
+++ src/ResourceManagement/Monitor/Microsoft.Azure.Monitor/generate.cmd
@@ -6,13 +6,13 @@
 @echo off 
 
 if  "%2" == "" (
-   set specFile1="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/efed63280648eebdec8be513470cd6c23783efb6/monitor/compositeMonitorClient.json"
+   set specFile1="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/0fde282c1aac0c9a07c35dd7054b3723bd890d45/monitor/compositeMonitorClient.json"
 ) else (
    set specFile1="%2"
 )
 
 if  "%3" == "" (
-   set specFile2="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/efed63280648eebdec8be513470cd6c23783efb6/arm-monitor/compositeMonitorManagementClient.json"
+   set specFile2="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/0fde282c1aac0c9a07c35dd7054b3723bd890d45/arm-monitor/compositeMonitorManagementClient.json"
 ) else (
    set specFile2="%3"
 )
@@ -25,19 +25,13 @@ set autoRestVersion="1.0.0-Nightly20170212"
 if exist %generateFolder1% rd /S /Q  %generateFolder1%
 if exist %generateFolder2% rd /S /Q  %generateFolder2%
 
-if "%1" == "new" goto new
 if "%1" == "install" goto install
+goto new
 
-call "%repoRoot%\tools\autorest.composite.gen.cmd" %specFile1% Microsoft.Azure.Insights %autoRestVersion% %generateFolder1% "-FT 1"
-call "%repoRoot%\tools\autorest.composite.gen.cmd" %specFile2% Microsoft.Azure.Management.Insights %autoRestVersion% %generateFolder2% "-FT 1"
-
-goto :eof
 :install
-
 echo **** INFO: Installing AutoRest package
 call npm install autorest -g
 
 :new
-
 call autoRest -Modeler CompositeSwagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Insights -Input %specFile1% -outputDirectory %generateFolder1% -Header MICROSOFT_MIT -FT 1
 call autoRest -Modeler CompositeSwagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Management.Insights -Input %specFile2% -outputDirectory %generateFolder2% -Header MICROSOFT_MIT -FT 1
