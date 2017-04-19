04fd9e07a147d86823122637b538db5dcafdb03a
Bruce Johnston <bruce.johnston@microsoft.com>
2017-04-19 16:14:20 -0700

    Search SDK: Fixing unit tests that were broken by VS2017 migration
    As part of moving to VS2017, the Search SDK is using a newer version of the
    ClientRuntime. Two of the unit tests were pinning constructor validation
    behavior of ServiceClient, which seems to have changed.
    
    Also, the move to VS2017 brought back the default behavior of xunit running
    all tests in parallel. This won't work, since the HttpMockServer is
    apparently not thread-safe. I disabled parallel test execution to avoid this
    problem. All tests now pass in playback mode when run from VS2017.

--- src/SDKs/Search/Management/Search.Management.Tests/Properties/AssemblyInfo.cs
+++ src/SDKs/Search/Management/Search.Management.Tests/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -36,3 +37,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
