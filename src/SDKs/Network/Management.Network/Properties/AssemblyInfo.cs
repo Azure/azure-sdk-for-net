5765ea3221a523c32e49a3aebbec650f87838841
ritwikbasu <ritwikbasu@users.noreply.github.com>
2017-04-26 23:50:22 -0700

    Expose new enums for VPNGatewaySku for VPN Gateways (#3108)
    * Expose new enums for VPNGatewaySku for VPN Gateways
    
    Expose new enums for VPNGatewaySku for VPN Gateways
    
    * Updating version information as per PR request
    
    Updating version information as per PR request
    
    * Fixed versioning as per discussion
    
    Fixed versioning as per discussion

--- src/SDKs/Network/Management.Network/Properties/AssemblyInfo.cs
+++ src/SDKs/Network/Management.Network/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides Microsoft Azure Network management functions for managing the Microsoft Azure Network service.")]
 
 [assembly: AssemblyVersion("10.0.0.0")]
-[assembly: AssemblyFileVersion("10.0.0.0")]
+[assembly: AssemblyFileVersion("10.1.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
