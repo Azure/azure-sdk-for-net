357b4837419d241c2777624d9692e0c7972b1fc5
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-05-02 11:43:44 -0700

    applying IBeta interface to Application Gateway models as pilot

--- src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/Domain/IApplicationGateway.cs
+++ src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/Domain/IApplicationGateway.cs
@@ -13,12 +13,8 @@ namespace Microsoft.Azure.Management.Network.Fluent
     /// <summary>
     /// Entry point for application gateway management API in Azure.
     /// </summary>
-    /// <remarks>
-    /// (Beta: This functionality is in preview and as such is subject to change in non-backwards compatible ways in
-    /// future releases, including removal, regardless of any compatibility expectations set by the containing library
-    /// version number.).
-    /// </remarks>
     public interface IApplicationGateway  :
+        IBeta,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.IGroupableResource<Microsoft.Azure.Management.Network.Fluent.INetworkManager,Models.ApplicationGatewayInner>,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.ResourceActions.IRefreshable<Microsoft.Azure.Management.Network.Fluent.IApplicationGateway>,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.ResourceActions.IUpdatable<ApplicationGateway.Update.IUpdate>,
--- src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/Domain/IApplicationGatewayProbe.cs
+++ src/ResourceManagement/Network/Microsoft.Azure.Management.Network.Fluent/Domain/IApplicationGatewayProbe.cs
@@ -8,12 +8,8 @@ namespace Microsoft.Azure.Management.Network.Fluent
     /// <summary>
     /// An immutable client-side representation of an application gateway probe.
     /// </summary>
-    /// <remarks>
-    /// (Beta: This functionality is in preview and as such is subject to change in non-backwards compatible ways in
-    /// future releases, including removal, regardless of any compatibility expectations set by the containing library
-    /// version number.).
-    /// </remarks>
     public interface IApplicationGatewayProbe  :
+        IBeta,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.IHasInner<Models.ApplicationGatewayProbeInner>,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.IChildResource<Microsoft.Azure.Management.Network.Fluent.IApplicationGateway>,
         Microsoft.Azure.Management.Network.Fluent.IHasProtocol<Models.ApplicationGatewayProtocol>
