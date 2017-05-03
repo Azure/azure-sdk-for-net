11fce08254a8a7d336e5f243e95331967d6f3d06
unknown <marcins@MARCINS-BRICK.redmond.corp.microsoft.com>
2017-05-03 10:05:25 -0700

    removing needless Beta <remarks> from Batch impls

--- src/ResourceManagement/Batch/Microsoft.Azure.Management.Batch.Fluent/Domain/InterfaceImpl/ApplicationPackageImpl.cs
+++ src/ResourceManagement/Batch/Microsoft.Azure.Management.Batch.Fluent/Domain/InterfaceImpl/ApplicationPackageImpl.cs
@@ -14,11 +14,6 @@ namespace Microsoft.Azure.Management.Batch.Fluent
         /// <summary>
         /// Activates the application package.
         /// </summary>
-        /// <remarks>
-        /// (Beta: This functionality is in preview and as such is subject to change in non-backwards compatible ways in
-        /// future releases, including removal, regardless of any compatibility expectations set by the containing library
-        /// version number.).
-        /// </remarks>
         /// <param name="format">The format of the uploaded Batch application package, either "zip" or "tar".</param>
         void Microsoft.Azure.Management.Batch.Fluent.IApplicationPackageBeta.Activate(string format)
         {
@@ -51,11 +46,6 @@ namespace Microsoft.Azure.Management.Batch.Fluent
         /// <summary>
         /// Activates the application package asynchronously.
         /// </summary>
-        /// <remarks>
-        /// (Beta: This functionality is in preview and as such is subject to change in non-backwards compatible ways in
-        /// future releases, including removal, regardless of any compatibility expectations set by the containing library
-        /// version number.).
-        /// </remarks>
         /// <param name="format">The format of the uploaded Batch application package, either "zip" or "tar".</param>
         /// <return>A representation of the deferred computation of this call.</return>
         async Task Microsoft.Azure.Management.Batch.Fluent.IApplicationPackageBeta.ActivateAsync(string format, CancellationToken cancellationToken)
