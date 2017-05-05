cfcb82b12b95b301a127c73b1eb0119099219b7d
vladca <vladca@microsoft.com>
2017-05-05 16:36:07 +0100

    Traffic Manager version 2017-05-01 including new MonitorConfig settings

--- src/SDKs/TrafficManager/Management.TrafficManager/generate.cmd
+++ src/SDKs/TrafficManager/Management.TrafficManager/generate.cmd
@@ -4,7 +4,7 @@
 ::
 
 @echo off
-set autoRestVersion=0.16.0
+set autoRestVersion=0.13.2
 if  "%1" == "" (
     set specFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/776baa315cfda36195fe4b7f38f56e6fa34f1819/arm-trafficmanager/2017-03-01/swagger/trafficmanager.json"
 ) else (
