f4e3cbbddecb6fe4d4448c1c3fc22e5bec9b0af8
Bruce Johnston <bruce.johnston@microsoft.com>
2017-05-04 19:39:13 -0700

    Search SDK: Fixing unit tests that were broken by VS2017 migration
    As part of moving to VS2017, the Search SDK is using a newer version of the
    ClientRuntime. Two of the unit tests were pinning constructor validation
    behavior of ServiceClient, which seems to have changed.
    
    Also, the move to VS2017 required using a newer version of the Resource
    Manager SDK, which required re-recording all mocks.
    
    Finally, for the data plane SDK, we upgraded to Microsoft.Spatial 7.2.0, which
    is compatible with VS2017 and .NET Core.

--- src/SDKs/Search/DataPlane/Search.Tests/Properties/AssemblyInfo.cs
+++ src/SDKs/Search/DataPlane/Search.Tests/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@
 // license information.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
