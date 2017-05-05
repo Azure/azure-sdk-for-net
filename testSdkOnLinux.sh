ca05f788d3ab867835da2cd418c02418b6460ee3
shahabhijeet <abhimswork@outlook.com>
2017-05-05 15:40:14 -0700

    changing to bash shell to get consistent result across

--- testSdkOnLinux.sh
+++ testSdkOnLinux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 base=`dirname {BASH_SOURCE[0]}`
@@ -8,6 +8,16 @@ netcore11='netcoreapp1.1'
 ubuntu1404="ubuntu.14.04-x64"
 nugetOrgSource="https://api.nuget.org/v3/index.json"
 localNugetFeed="./tools/LocalNugetFeed"
+
+skip_Rps() {
+    retVal=false
+    #printf "checking......$1\n"
+    if [[ ("$1" =~ "Authorization")  || ( "$1" =~ "Gallery" ) || ("$1" =~ "Automation") || ( "$1" =~ "Intune" ) || ( "$1" =~ "DataLake.Store" ) 
+                || ( "$1" =~ "Monitor" ) || ( "$1" =~ "RedisCache" ) || ( "$1" =~ "Search" ) || ( "$1" =~ "KeyVault.Tests" ) ]]; then                
+        retVal=true
+    fi
+    echo $retVal
+}
 
 echo "Restore ClientRuntime for $ubuntu1404"
 dotnet restore src/SdkCommon/ClientRuntime.sln -r $ubuntu1404
@@ -34,19 +44,20 @@ sdkdir=$rootdir/src/SDKs
 for folder in $sdkdir/*
 do
     item=`basename $folder`
-
     if [ -d $sdkdir/$item ]; then
         #printf "$sdkdir/$item\n"
-        if [ -f $sdkdir/$item/*.sln ]
-        then
+        if [ -f $sdkdir/$item/*.sln ]; then
             slnFile=($sdkdir/$item/*.sln)
-            printf "Restoring :::::: $slnFile for $ubuntu1404\n"
-            dotnet restore $slnFile -r $ubuntu1404            
+            skipRestore=$( skip_Rps $slnFile )
+            if [ "$skipRestore" == false ]; then
+                printf "Restoring :::::: $slnFile for $ubuntu1404\n"
+                dotnet restore $slnFile -r $ubuntu1404
+            fi
             if [ -d $sdkdir/$item/*.Tests ]; then
                 testProj=($sdkdir/$item/*.Tests/*.csproj)
-                if [[ ("$testProj" =~ "Authorization")  || ( "$testProj" =~ "Gallery" ) || ("$testProj" =~ "Automation") || ( "$testProj" =~ "InTune" ) || ( "$testProj" =~ "DataLake.Store" ) ]]; then
-                    printf "\n"
-                else
+                skipTest=$( skip_Rps $testProj )
+                #printf "$skipRp\n"
+                if [ "$skipTest" == "false" ]; then
                     printf "Test ------ $testProj for framework $netcore11\n"
                     #dotnet build $testProj -f $netcore11
                     dotnet test $testProj -f $netcore11
@@ -57,6 +68,9 @@ do
 done
 
 KVMgmtDir=($sdkdir/KeyVault/Management/*.sln)
+if [ -f $KVMgmtDir ]; then
+    dotnet restore $KVMgmtDir -r ubuntu1404
+fi
 KVDataPlaneDir=$sdkdir/KeyVault/dataPlane
 for kvDir in $KVDataPlaneDir/*
 do
@@ -64,21 +78,22 @@ do
     if [ -d $kvDir ] && [ "$kvItem" != "Microsoft.Azure.KeyVault.Samples" ]
     then
         if [[ "$kvItem" =~ "Tests" ]]; then
-            kvTestProj=($kvDir/ *.csproj)
+            kvTestProj=($kvDir/*.csproj)
             printf "KV TestProject ... $kvTestProj\n"
-            dotnet test $kvTestProj -f netcore11
+            #dotnet restore $kvTestProj -r $ubuntu1404
+            dotnet test $kvTestProj -f $netcore11
         else
             if [ -f $kvDir/*.csproj ]; then
                 kvSdkProj=($kvDir/*.csproj)
                 printf "KvSdkProj ..... $kvSdkProj\n"
-                dotnet build $kvProj -f $netstd14
+                #dotnet restore $kvSdkProj -r $ubuntu1404
+                dotnet build $kvSdkProj -f $netstd14
             fi
         fi
     fi
 done
 
-
-'
+: '
 #echo "base: "$base
 #echo "rootedir: "$rootdir
 #echo "netstandard1.4 " $netstd14
@@ -92,7 +107,7 @@ done
             #    dotnet build $sdkProjFile -f $netstd14
             #fi
 
-: '
+
 else
             if [ -d $sdkDir/$item/Management ]; then
                 printf "Found mgmt $sdkDir/$item/Management\n"
@@ -106,4 +121,9 @@ else
                 fi #mgmtTestProject
             fi #mgmgtSln
         fi #mgmtSln else
+
+
+                #if [[ $("$testProj" =~ "Authorization")  || $( "$testProj" =~ "Gallery" ) || $("$testProj" =~ "Automation") || $( "$testProj" =~ "InTune" ) || $( "$testProj" =~ "DataLake.Store" ) ]]; then
+                #if [[ ("$testProj" =~ "Authorization")  || ( "$testProj" =~ "Gallery" ) || ("$testProj" =~ "Automation") || ( "$testProj" =~ "Intune" ) || ( "$testProj" =~ "DataLake.Store" ) 
+                #|| ( "$testProj" =~ "Monitor" ) || ( "$testProj" =~ "RedisCache" ) ]]; then
 '
