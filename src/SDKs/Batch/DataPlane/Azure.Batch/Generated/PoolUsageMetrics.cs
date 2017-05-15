a70dd0f7d3f7ce364c54255f09972a1074bc65f7
Ivan Towlson <github@hestia.cc>
2017-05-15 16:46:12 -0700

    Fixed a place where Batch VM size support info had not been updated

--- src/SDKs/Batch/DataPlane/Azure.Batch/Generated/PoolUsageMetrics.cs
+++ src/SDKs/Batch/DataPlane/Azure.Batch/Generated/PoolUsageMetrics.cs
@@ -96,9 +96,17 @@ namespace Microsoft.Azure.Batch
         }
 
         /// <summary>
-        /// Gets the size of virtual machine in the pool. All virtual machines in a pool are the same size. Azure Batch supports 
-        /// all Azure virtual machine sizes except ExtraSmall.
+        /// Gets the size of the virtual machines in the pool.  All virtual machines in a pool are the same size.
         /// </summary>
+        /// <remarks>
+        /// <para>For information about available sizes of virtual machines for Cloud Services pools (pools created with 
+        /// a <see cref="CloudServiceConfiguration"/>), see https://azure.microsoft.com/documentation/articles/cloud-services-sizes-specs/. 
+        /// Batch supports all Cloud Services VM sizes except ExtraSmall.</para><para>For information about available VM 
+        /// sizes for pools using images from the Virtual Machines Marketplace (pools created with a <see cref="VirtualMachineConfiguration"/>) 
+        /// see https://azure.microsoft.com/documentation/articles/virtual-machines-linux-sizes/ or https://azure.microsoft.com/documentation/articles/virtual-machines-windows-sizes/. 
+        /// Batch supports all Azure VM sizes except STANDARD_A0 and those with premium storage (for example STANDARD_GS, 
+        /// STANDARD_DS, and STANDARD_DSV2 series).</para>
+        /// </remarks>
         public string VirtualMachineSize
         {
             get { return this.virtualMachineSize; }
