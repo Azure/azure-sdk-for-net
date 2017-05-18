16c23d700766bc4f73680a002caede04bdfef6d1
Monica.lin <Monica.lyy@outlook.com>
2017-05-18 16:45:57 +0800

    Update code according to Swagger spec

--- src/SDKs/Authorization/Management.Authorization/Generated/IAuthorizationManagementClient.cs
+++ src/SDKs/Authorization/Management.Authorization/Generated/IAuthorizationManagementClient.cs
@@ -8,24 +8,27 @@
 
 namespace Microsoft.Azure.Management.Authorization
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Net.Http;
-    using System.Threading;
-    using System.Threading.Tasks;
+	using System;
+	using Microsoft.Azure;
+    using Microsoft.Azure.Management;
+	using Microsoft.Rest;
+	using Models;
     using Newtonsoft.Json;
-    using Microsoft.Rest;
-    using Microsoft.Rest.Azure;
-    using Models;
 
     /// <summary>
+    /// Role based access control provides you a way to apply granular level
+    /// policy administration down to individual resources or resource groups.
+    /// These operations enable you to manage role definitions and role
+    /// assignments. A role definition describes the set of actions that can be
+    /// performed on resources. A role assignment grants access to Azure Active
+    /// Directory users.
     /// </summary>
-    public partial interface IAuthorizationManagementClient : IDisposable
+    public partial interface IAuthorizationManagementClient : System.IDisposable
     {
         /// <summary>
         /// The base URI of the service.
         /// </summary>
-        Uri BaseUri { get; set; }
+        System.Uri BaseUri { get; set; }
 
         /// <summary>
         /// Gets or sets json serialization settings.
@@ -37,50 +40,65 @@ namespace Microsoft.Azure.Management.Authorization
         /// </summary>
         JsonSerializerSettings DeserializationSettings { get; }
 
-        /// <summary>
-        /// Gets Azure subscription credentials.
-        /// </summary>
-        ServiceClientCredentials Credentials { get; }
+		/// <summary>
+		/// Gets Azure subscription credentials.
+		/// </summary>
+		ServiceClientCredentials Credentials { get; }
+
+		/// <summary>
+		/// Gets subscription credentials which uniquely identify Microsoft
+		/// Azure subscription. The subscription ID forms part of the URI for
+		/// every service call.
+		/// </summary>
+		string SubscriptionId { get; set; }
 
         /// <summary>
-        /// Gets subscription credentials which uniquely identify Microsoft
-        /// Azure subscription. The subscription ID forms part of the URI for
-        /// every service call.
-        /// </summary>
-        string SubscriptionId { get; set; }
-
-        /// <summary>
-        /// Client Api Version.
+        /// The API version to use for this operation.
         /// </summary>
         string ApiVersion { get; }
 
-        /// <summary>
-        /// Gets or sets the preferred language for the response.
-        /// </summary>
-        string AcceptLanguage { get; set; }
+		/// <summary>
+		/// Gets or sets the preferred language for the response.
+		/// </summary>
+		string AcceptLanguage { get; set; }
+
+		/// <summary>
+		/// Gets or sets the retry timeout in seconds for Long Running
+		/// Operations. Default value is 30.
+		/// </summary>
+		int? LongRunningOperationRetryTimeout { get; set; }
+
+		/// <summary>
+		/// When set to true a unique x-ms-client-request-id value is
+		/// generated and included in each request. Default is true.
+		/// </summary>
+		bool? GenerateClientRequestId { get; set; }
+
+
+		/// <summary>
+		/// Gets the IClassicAdministrators.
+		/// </summary>
+		IClassicAdministratorsOperations ClassicAdministrators { get; }
+
+		/// <summary>
+		/// Gets the IPermissions.
+		/// </summary>
+		IPermissionsOperations Permissions { get; }
 
         /// <summary>
-        /// Gets or sets the retry timeout in seconds for Long Running
-        /// Operations. Default value is 30.
+        /// Gets the IProviderOperationsMetadataOperations.
         /// </summary>
-        int? LongRunningOperationRetryTimeout { get; set; }
-
-        /// <summary>
-        /// When set to true a unique x-ms-client-request-id value is
-        /// generated and included in each request. Default is true.
-        /// </summary>
-        bool? GenerateClientRequestId { get; set; }
-
-
-        IClassicAdministratorsOperations ClassicAdministrators { get; }
-
-        IPermissionsOperations Permissions { get; }
-
         IProviderOperationsMetadataOperations ProviderOperationsMetadata { get; }
 
-        IRoleAssignmentsOperations RoleAssignments { get; }
+		/// <summary>
+		/// Gets the IRoleAssignments.
+		/// </summary>
+		IRoleAssignmentsOperations RoleAssignments { get; }
 
-        IRoleDefinitionsOperations RoleDefinitions { get; }
+		/// <summary>
+		/// Gets the IRoleDefinitions.
+		/// </summary>
+		IRoleDefinitionsOperations RoleDefinitions { get; }
 
     }
 }
