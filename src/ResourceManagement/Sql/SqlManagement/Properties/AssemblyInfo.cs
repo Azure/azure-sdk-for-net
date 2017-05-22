ba55c281f0da364f76c9f542cd1a26db4a5a70de
Fang Liu <fangl@microsoft.com>
2017-05-22 17:33:07 +0800

    Add Scenario Test

--- src/ResourceManagement/Sql/SqlManagement/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/Sql/SqlManagement/Properties/AssemblyInfo.cs
@@ -20,8 +20,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Microsoft Azure SQL Server Management Library")]
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure SQL.")]
 
-[assembly: AssemblyVersion("0.54.0.0")]
-[assembly: AssemblyFileVersion("0.54.0.0")]
+[assembly: AssemblyVersion("0.55.0.0")]
+[assembly: AssemblyFileVersion("0.55.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Azure .NET SDK")]
