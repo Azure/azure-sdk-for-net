da4ea2d2c317d3367ea19bf250b86740593740b3
Sean Feldman <SeanFeldman@users.noreply.github.com>
2017-05-31 16:04:58 -0600

    API approver (#168)

---
+++ test/Microsoft.Azure.ServiceBus.UnitTests/API/APIApprovals.cs
@@ -0,0 +1,23 @@
+﻿#if NET46
+namespace Microsoft.Azure.ServiceBus.UnitTests.API
+{
+    using System.Runtime.CompilerServices;
+    using ApprovalTests;
+    using ApprovalTests.Reporters;
+    using PublicApiGenerator;
+    using Xunit;
+
+    public class ApiApprovals
+    {
+        [Fact]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        [UseReporter(typeof(DiffReporter))]
+        public void ApproveAzureServiceBus()
+        {
+            //Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(Message).Assembly);
+            Approvals.Verify(publicApi);
+        }
+    }
+}
+#endif
---
+++ test/Microsoft.Azure.ServiceBus.UnitTests/API/ApiApprover.6.0.0/PublicApiApprover.cs
@@ -0,0 +1,37 @@
+﻿#if NET46
+
+using System.IO;
+using System.Reflection;
+using ApprovalTests;
+using ApprovalTests.Namers;
+
+namespace ApiApprover
+{
+    public static class PublicApiApprover
+    {
+        public static void ApprovePublicApi(Assembly assembly)
+        {
+            var publicApi = PublicApiGenerator.ApiGenerator.GeneratePublicApi(assembly);
+            var writer = new ApprovalTextWriter(publicApi, "cs");
+            var approvalNamer = new AssemblyPathNamer(assembly.Location);
+            Approvals.Verify(writer, approvalNamer, Approvals.GetReporter());
+        }
+
+        private class AssemblyPathNamer : UnitTestFrameworkNamer
+        {
+            private readonly string name;
+
+            public AssemblyPathNamer(string assemblyPath)
+            {
+                name = Path.GetFileNameWithoutExtension(assemblyPath);
+            }
+
+            public override string Name
+            {
+                get { return name; }
+            }
+        }
+    }
+}
+
+#endif
