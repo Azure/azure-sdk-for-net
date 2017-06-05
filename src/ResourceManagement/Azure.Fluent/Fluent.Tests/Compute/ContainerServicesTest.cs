db883c68ec4673d27d77431cb37a318d17e63741
Alva <alvab@outlook.com>
2017-06-05 11:29:44 -0700

    Add license header

--- src/ResourceManagement/Azure.Fluent/Fluent.Tests/Compute/ContainerServicesTest.cs
+++ src/ResourceManagement/Azure.Fluent/Fluent.Tests/Compute/ContainerServicesTest.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. 
+// Licensed under the MIT License. See License.txt in the project root for license information. 
+
+using Xunit;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using Fluent.Tests.Common;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
