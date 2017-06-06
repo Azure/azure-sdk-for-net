43eb6b7251884d2907c0a28b9178f051ba3726b1
bganapa <bganapa@microsoft.com>
2017-06-06 12:11:57 -0700

    Updating build.proj for signing with 401 and updating versions for azurestack modules and windowsazure.management for signing purposes (#3308)
    * updating build.proj to sign with 401
    
    * updating azurestack management to 0.10.8-preview
    
    * Azurestack storage admin module updating to 0.10.7 for signing with sha256
    
    * updating to 4.1.3 version for signing with sha256

--- src/ResourceManagement/AzureStackStorageAdmin/StorageManagement/Properties/AssemblyInfo.cs
+++ src/ResourceManagement/AzureStackStorageAdmin/StorageManagement/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.6.0")]
+[assembly: AssemblyFileVersion("0.10.7.0")]
