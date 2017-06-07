8d1589a0e6f734d6f7023535e18eb161efadb29f
Alva Bandy <alvab@microsoft.com>
2017-06-07 11:47:10 -0700

    Add auto registry.

--- src/ResourceManagement/DocumentDB/Microsoft.Azure.Management.DocumentDB.Fluent/DocumentDBManager.cs
+++ src/ResourceManagement/DocumentDB/Microsoft.Azure.Management.DocumentDB.Fluent/DocumentDBManager.cs
@@ -31,6 +31,7 @@ namespace Microsoft.Azure.Management.DocumentDB.Fluent
             return new DocumentDBManager(RestClient.Configure()
                     .WithEnvironment(credentials.Environment)
                     .WithCredentials(credentials)
+                    .WithDelegatingHandler(new ProviderRegistrationDelegatingHandler(credentials))
                     .Build(), subscriptionId);
         }
 
