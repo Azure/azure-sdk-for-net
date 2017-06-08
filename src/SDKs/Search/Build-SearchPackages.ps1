620a9152b6150e573841291dbad1e9d8fad032aa
Monica.lin <Monica.lyy@outlook.com>
2017-06-08 19:47:36 +0800

    Revert "Search SDK: Rename Management SDK project and update build script (#3236)"
    This reverts commit 2b6a2daa0355f0c5c50d768acbd5459241072754.

--- src/SDKs/Search/Build-SearchPackages.ps1
+++
@@ -1,3 +0,0 @@
-$scriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
-msbuild "$scriptDir\..\..\..\build.proj" /t:PublishNuget /p:Scope="SDKs\Search\Management" /p:NugetPackageName="Microsoft.Azure.Management.Search"
-msbuild "$scriptDir\..\..\..\build.proj" /t:PublishNuget /p:Scope="SDKs\Search\DataPlane" /p:NugetPackageName="Microsoft.Azure.Search"
