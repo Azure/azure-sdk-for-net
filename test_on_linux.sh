d616019aed412c9982afefcd579519f90302aeff
Hovsep <hovsepm@microsoft.com>
2017-06-15 16:34:02 -0700

    Fixed build breaks

--- test_on_linux.sh
+++ test_on_linux.sh
@@ -3,17 +3,26 @@
 set -e
 base=`dirname {BASH_SOURCE[0]}`
 rootdir="$( cd "$base" && pwd )"
-  
-dotnet restore
+netstd16="netstandard1.6"
+netcore11='netcoreapp1.1'
+ubuntu1404="ubuntu.14.04-x64"
+nugetOrgSource="https://api.nuget.org/v3/index.json"
+
+echo Restoring...
+dotnet --info
+dotnet restore FluentSDK.sln -r $ubuntu1404
+dotnet restore FluentSamples.sln -r $ubuntu1404
+dotnet restore FluentTests.sln -r $ubuntu1404
+
+echo Building... 
+dotnet build FluentSDK.sln -f $netstd16
+dotnet build FluentSamples.sln -f $netstd16
+dotnet build FluentTests.sln -f $netcore11
 
 echo Running Samples Tests
-cd $rootdir/Samples/Tests
-dotnet restore
-dotnet build --framework netcoreapp1.0
-dotnet test
+cd $rootdir/Tests/Samples.Tests
+dotnet test Samples.Tests.csproj -f $netcore11
 
 echo Running Fluent Tests
-cd $rootdir/src/ResourceManagement/Azure.Fluent/Fluent.Tests
-dotnet restore
-dotnet build --framework netcoreapp1.0
-dotnet test
+cd $rootdir/Tests/Fluent.Tests
+dotnet test Fluent.Tests.csproj -f $netcore11
