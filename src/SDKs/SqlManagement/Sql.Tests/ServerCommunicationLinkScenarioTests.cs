2b42f9abf9b09ca107b6686c0929302c1652ece7
nathannfan <nafan@microsoft.com>
2017-06-15 18:12:21 -0700

    Add delete communication link scenario test

--- src/SDKs/SqlManagement/Sql.Tests/ServerCommunicationLinkScenarioTests.cs
+++ src/SDKs/SqlManagement/Sql.Tests/ServerCommunicationLinkScenarioTests.cs
@@ -50,5 +50,45 @@ namespace Sql.Tests
                 Assert.Equal(server2.Name, link.PartnerServer);
             });
         }
+
+        [Fact]
+        public void TestDeleteCommunicationLinks()
+        {
+            string testPrefix = "sqlcrudtest-";
+            string suiteName = this.GetType().FullName;
+            SqlManagementTestUtilities.RunTestInNewResourceGroup(suiteName, "TestDeleteCommunicationLinks", testPrefix, (resClient, sqlClient, resourceGroup) =>
+            {
+                // Create two servers
+                Server server1 = SqlManagementTestUtilities.CreateServer(sqlClient, resourceGroup, testPrefix);
+                Server server2 = SqlManagementTestUtilities.CreateServer(sqlClient, resourceGroup, testPrefix);
+
+                // Create communication link on server 1
+                string linkName = SqlManagementTestUtilities.GenerateName(testPrefix);
+                ServerCommunicationLink link = new ServerCommunicationLink()
+                {
+                    PartnerServer = server2.Name
+                };
+
+                sqlClient.ServerCommunicationLinks.CreateOrUpdate(resourceGroup.Name, server1.Name, linkName, link);
+
+                // TODO: Update communication link is not yet supported by the API.
+                // When it is, add check here
+
+                // List Communication Link, assert there is one
+                IEnumerable<ServerCommunicationLink> links = sqlClient.ServerCommunicationLinks.ListByServer(resourceGroup.Name, server1.Name);
+
+                Assert.Equal(1, links.Count());
+                link = links.First();
+                Assert.Equal(linkName, link.Name);
+                Assert.Equal(server2.Name, link.PartnerServer);
+                
+                // Delete Communication Link
+                sqlClient.ServerCommunicationLinks.Delete(resourceGroup.Name, server1.Name, linkName);
+                links = sqlClient.ServerCommunicationLinks.ListByServer(resourceGroup.Name, server1.Name);
+
+                // Assert that no links found
+                Assert.Equal(0, links.Count());
+            });
+        }
     }
 }
