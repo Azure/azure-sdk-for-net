a233d999a65b5c7100cac22e238bce89fe73b6da
Jared Moore <jaredmoo@microsoft.com>
2017-06-15 21:18:13 -0700

    Update generate.cmd

--- src/SDKs/SqlManagement/Management.Sql/generate.cmd
+++ src/SDKs/SqlManagement/Management.Sql/generate.cmd
@@ -5,7 +5,7 @@
 
 @echo off
 if  "%1" == "" (
-    set specFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/b80af62b013d06fc8fa347f8446d2e5b5b99e843/arm-sql/compositeSql.json"
+    set specFile="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/0f3d618978c9a410a426657f1ab68d3b571e41ee/arm-sql/compositeSql.json"
 ) else (
     set specFile="%1"
 )
