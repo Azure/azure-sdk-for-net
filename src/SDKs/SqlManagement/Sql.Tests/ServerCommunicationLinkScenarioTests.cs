89418626889ed650678b8e030863fcc283835cc4
nathannfan <nafan@microsoft.com>
2017-06-15 17:20:40 -0700

    Rename test, re-record

--- src/SDKs/SqlManagement/Sql.Tests/ServerCommunicationLinkScenarioTests.cs
+++ src/SDKs/SqlManagement/Sql.Tests/ServerCommunicationLinkScenarioTests.cs
@@ -13,67 +13,41 @@ namespace Sql.Tests
     public class ServerCommunicationLinkScenarioTests
     {
         [Fact]
-        public void TestCreateUpdateGetDropServer()
+        public void TestCreateGetListCommunicationLinks()
         {
             string testPrefix = "sqlcrudtest-";
             string suiteName = this.GetType().FullName;
-            SqlManagementTestUtilities.RunTestInNewResourceGroup(suiteName, "TestCreateUpdateGetDropServer", testPrefix, (resClient, sqlClient, resourceGroup) =>
+            SqlManagementTestUtilities.RunTestInNewResourceGroup(suiteName, "TestCreateGetListCommunicationLinks", testPrefix, (resClient, sqlClient, resourceGroup) =>
             {
-                string server1Name = SqlManagementTestUtilities.GenerateName(testPrefix);
-                string login = "dummylogin";
-                string password = "Un53cuRE!";
-                string version12 = "12.0";
-                Dictionary<string, string> tags = new Dictionary<string, string>()
-                    {
-                        { "tagKey1", "TagValue1" }
-                    };
-
                 // Create two servers
-                var server1 = sqlClient.Servers.CreateOrUpdate(resourceGroup.Name, server1Name, new Server()
-                {
-                    AdministratorLogin = login,
-                    AdministratorLoginPassword = password,
-                    Version = version12,
-                    Tags = tags,
-                    Location = SqlManagementTestUtilities.DefaultLocationId,
-                });
-                SqlManagementTestUtilities.ValidateServer(server1, server1Name, login, version12, tags, SqlManagementTestUtilities.DefaultLocationId);
-                
-                string server2Name = SqlManagementTestUtilities.GenerateName(testPrefix);
-                var server2 = sqlClient.Servers.CreateOrUpdate(resourceGroup.Name, server2Name, new Server()
-                {
-                    AdministratorLogin = login,
-                    AdministratorLoginPassword = password,
-                    Tags = tags,
-                    Location = SqlManagementTestUtilities.DefaultLocationId,
-                });
-                SqlManagementTestUtilities.ValidateServer(server2, server2Name, login, version12, tags, SqlManagementTestUtilities.DefaultLocationId);
+                Server server1 = SqlManagementTestUtilities.CreateServer(sqlClient, resourceGroup, testPrefix);
+                Server server2 = SqlManagementTestUtilities.CreateServer(sqlClient, resourceGroup, testPrefix);
 
                 // Create communication link on server 1
                 string linkName = SqlManagementTestUtilities.GenerateName(testPrefix);
                 ServerCommunicationLink link = new ServerCommunicationLink()
                 {
-                    PartnerServer = server2Name
+                    PartnerServer = server2.Name
                 };
 
-                sqlClient.ServerCommunicationLinks.CreateOrUpdate(resourceGroup.Name, server1Name, linkName, link);
+                sqlClient.ServerCommunicationLinks.CreateOrUpdate(resourceGroup.Name, server1.Name, linkName, link);
 
                 // TODO: Update communication link is not yet supported by the API.
                 // When it is, add check here
 
                 // Get Communication Link
-                link = sqlClient.ServerCommunicationLinks.Get(resourceGroup.Name, server1Name, linkName);
+                link = sqlClient.ServerCommunicationLinks.Get(resourceGroup.Name, server1.Name, linkName);
 
                 Assert.Equal(linkName, link.Name);
-                Assert.Equal(server2Name, link.PartnerServer);
+                Assert.Equal(server2.Name, link.PartnerServer);
 
                 // List Communication Link
-                IEnumerable<ServerCommunicationLink> links = sqlClient.ServerCommunicationLinks.ListByServer(resourceGroup.Name, server1Name);
+                IEnumerable<ServerCommunicationLink> links = sqlClient.ServerCommunicationLinks.ListByServer(resourceGroup.Name, server1.Name);
 
                 Assert.Equal(1, links.Count());
                 link = links.First();
                 Assert.Equal(linkName, link.Name);
-                Assert.Equal(server2Name, link.PartnerServer);
+                Assert.Equal(server2.Name, link.PartnerServer);
             });
         }
     }
