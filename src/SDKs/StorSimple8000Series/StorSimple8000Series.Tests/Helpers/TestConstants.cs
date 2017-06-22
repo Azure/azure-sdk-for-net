e195a57df28b546905de88590c709de80940a007
manaas-microsoft <manaas@microsoft.com>
2017-06-23 01:10:14 +0530

    Dotnet client SDK - generated code and testcases, for StorSimple 8000 Series ARM APIs.

---
+++ src/SDKs/StorSimple8000Series/StorSimple8000Series.Tests/Helpers/TestConstants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorSimple8000Series.Tests
+{
+    public static class TestConstants
+    {
+        public const string DefaultResourceGroupName = "ResourceGroupForSDKTest";
+        public const string DefaultManagerName = "ManagerForSDKTest1";
+        public const string ManagerForManagerOperationTests = "ManagerForSDKTest2";
+        public const string DefaultVirtualNetworkName = "ClientSdkTest";
+        public const string DefaultSubnetName = "Default";
+        public const string DefaultStorageAccountCredential = "safortestrecording";
+        public const string DefaultStorageAccountEndPoint = "blob.core.windows.net";
+        public const string DefaultDeviceName = "Device05ForSDKTest";
+        public const string DeviceForFailover = "jemdeviceforsdk";
+        public const string DeviceForUpdateTests = "sugattdeviceforSDK";
+        public const string DeviceForDeviceOperationTests = "Device001ForSDKTest";
+        public const string SecondaryDnsServers = "10.67.64.15;10.67.64.14";
+        public const string FirstDeviceControllerZeroIp = "10.168.241.122";
+        public const string FirstDeviceControllerOneIp = "10.168.241.121";
+
+        public static readonly DateTime MetricsStartTime = new DateTime(2017, 06, 18);
+        public static readonly DateTime MetricsEndTime = new DateTime(2017, 06, 22);
+    }
+}
---
+++ src/SDKs/StorSimple8000Series/StorSimple8000Series.Tests/Tests/DeviceUpdatesTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading;
+using Xunit;
+using Xunit.Sdk;
+using Xunit.Abstractions;
+using Microsoft.Rest.Azure;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Microsoft.Azure.Management.StorSimple8000Series;
+using Microsoft.Azure.Management.StorSimple8000Series.Models;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Azure.Management.Compute;
+using Microsoft.Azure.Management.Network;
+
+namespace StorSimple8000Series.Tests
+{
+    public class DeviceUpdatesTests : StorSimpleTestBase
+    {
+        public DeviceUpdatesTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { }
+
+        [Fact]
+        public void TestDeviceUpdates()
+        {
+            //checking for prerequisites
+            var device = Helpers.CheckAndGetConfiguredDevice(this, TestConstants.DeviceForUpdateTests);
+            var deviceName = device.Name;
+
+            //Scan for updates
+            ScanForUpdates(deviceName);
+
+            //Get update summary
+            var updateSummary = GetUpdateSummary(deviceName);
+
+            if (updateSummary.RegularUpdatesAvailable == true)
+            {
+                //Install updates
+                InstallUpdates(deviceName);
+            }
+        }
+
+        /// <summary>
+        /// Get the update summary of a device.
+        /// </summary>
+        private Updates GetUpdateSummary(string deviceName)
+        {
+            var updateSummary = this.Client.Devices.GetUpdateSummary(
+                deviceName,
+                this.ResourceGroupName,
+                this.ManagerName);
+
+            return updateSummary;
+        }
+
+        /// <summary>
+        /// Scan for new updates.
+        /// </summary>
+        private void ScanForUpdates(string deviceName)
+        {
+            this.Client.Devices.ScanForUpdates(
+                deviceName,
+                this.ResourceGroupName,
+                this.ManagerName);
+        }
+
+        /// <summary>
+        /// Install updates.
+        /// </summary>
+        private void InstallUpdates(string deviceName)
+        {
+            this.Client.Devices.InstallUpdates(
+                deviceName,
+                this.ResourceGroupName,
+                this.ManagerName);
+        }
+    }
+}
