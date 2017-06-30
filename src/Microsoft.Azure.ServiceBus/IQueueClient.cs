e8d5ccbdb2e2fc29479c39be3ace399660ec848a
Neeraj Makam <neeraj.makam@gmail.com>
2017-06-29 22:24:53 -0700

    Adding RegisterSessionHandler to IQueueClient and ISubscriptionClient

--- src/Microsoft.Azure.ServiceBus/IQueueClient.cs
+++ src/Microsoft.Azure.ServiceBus/IQueueClient.cs
@@ -3,7 +3,11 @@
 
 namespace Microsoft.Azure.ServiceBus
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Core;
+    using Primitives;
 
     /// <summary>
     /// An interface used to describe the <see cref="QueueClient"/>QueueClient.
@@ -14,5 +18,27 @@ namespace Microsoft.Azure.ServiceBus
         /// Gets the name of the queue.
         /// </summary>
         string QueueName { get; }
+
+        /// <summary>
+        /// Receive session messages continously from the queue. Registers a message handler and begins a new thread to receive session-messages.
+        /// This handler(<see cref="Func{IMessageSession, Message, CancellationToken, Task}"/>) is awaited on every time a new message is received by the queue client.
+        /// </summary>
+        /// <param name="handler">A <see cref="Func{IMessageSession, Message, CancellationToken, Task}"/> that processes messages. 
+        /// <see cref="IMessageSession"/> contains the session information, and must be used to perform Complete/Abandon/Deadletter or other such operations on the <see cref="Message"/></param>
+        /// <param name="exceptionReceivedHandler">A <see cref="Func{T1, TResult}"/> that is invoked during exceptions.
+        /// <see cref="ExceptionReceivedEventArgs"/> contains contextual information regarding the exception.</param>
+        /// <remarks>  Enable prefetch to speeden up the receive rate. 
+        /// Use <see cref="RegisterSessionHandler(Func{IMessageSession,Message,CancellationToken,Task}, SessionHandlerOptions)"/> to configure the settings of the pump.</remarks>
+        void RegisterSessionHandler(Func<IMessageSession, Message, CancellationToken, Task> handler, Func<ExceptionReceivedEventArgs, Task> exceptionReceivedHandler);
+
+        /// <summary>
+        /// Receive session messages continously from the queue. Registers a message handler and begins a new thread to receive session-messages.
+        /// This handler(<see cref="Func{IMessageSession, Message, CancellationToken, Task}"/>) is awaited on every time a new message is received by the queue client.
+        /// </summary>
+        /// <param name="handler">A <see cref="Func{IMessageSession, Message, CancellationToken, Task}"/> that processes messages. 
+        /// <see cref="IMessageSession"/> contains the session information, and must be used to perform Complete/Abandon/Deadletter or other such operations on the <see cref="Message"/></param>
+        /// <param name="sessionHandlerOptions">Options used to configure the settings of the session pump.</param>
+        /// <remarks>  Enable prefetch to speeden up the receive rate. </remarks>
+        void RegisterSessionHandler(Func<IMessageSession, Message, CancellationToken, Task> handler, SessionHandlerOptions sessionHandlerOptions);
     }
 }
--- src/Microsoft.Azure.ServiceBus/ISubscriptionClient.cs
+++ src/Microsoft.Azure.ServiceBus/ISubscriptionClient.cs
@@ -3,9 +3,12 @@
 
 namespace Microsoft.Azure.ServiceBus
 {
+    using System;
+    using System.Threading;
     using System.Threading.Tasks;
     using Core;
     using Filters;
+    using Primitives;
 
     /// <summary>
     /// An interface used to describe the <see cref="SubscriptionClient"/>.
@@ -43,5 +46,27 @@ namespace Microsoft.Azure.ServiceBus
         /// <param name="ruleName">The name of the rule.</param>
         /// <returns>A task instance that represents the asynchronous remove rule operation.</returns>
         Task RemoveRuleAsync(string ruleName);
+
+        /// <summary>
+        /// Receive session messages continously from the queue. Registers a message handler and begins a new thread to receive session-messages.
+        /// This handler(<see cref="Func{T1,T2,T3,TResult}"/>) is awaited on every time a new message is received by the queue client.
+        /// </summary>
+        /// <param name="handler">A <see cref="Func{IMessageSession, Message, CancellationToken, Task}"/> that processes messages. 
+        /// <see cref="IMessageSession"/> contains the session information, and must be used to perform Complete/Abandon/Deadletter or other such operations on the <see cref="Message"/></param>
+        /// <param name="exceptionReceivedHandler">A <see cref="Func{T1, TResult}"/> that is invoked during exceptions.
+        /// <see cref="ExceptionReceivedEventArgs"/> contains contextual information regarding the exception.</param>
+        /// <remarks>  Enable prefetch to speeden up the receive rate. 
+        /// Use <see cref="RegisterSessionHandler(Func{IMessageSession,Message,CancellationToken,Task}, SessionHandlerOptions)"/> to configure the settings of the pump.</remarks>
+        void RegisterSessionHandler(Func<IMessageSession, Message, CancellationToken, Task> handler, Func<ExceptionReceivedEventArgs, Task> exceptionReceivedHandler);
+
+        /// <summary>
+        /// Receive session messages continously from the queue. Registers a message handler and begins a new thread to receive session-messages.
+        /// This handler(<see cref="Func{IMessageSession, Message, CancellationToken, Task}"/>) is awaited on every time a new message is received by the queue client.
+        /// </summary>
+        /// <param name="handler">A <see cref="Func{IMessageSession, Message, CancellationToken, Task}"/> that processes messages. 
+        /// <see cref="IMessageSession"/> contains the session information, and must be used to perform Complete/Abandon/Deadletter or other such operations on the <see cref="Message"/></param>
+        /// <param name="sessionHandlerOptions">Options used to configure the settings of the session pump.</param>
+        /// <remarks>  Enable prefetch to speeden up the receive rate. </remarks>
+        void RegisterSessionHandler(Func<IMessageSession, Message, CancellationToken, Task> handler, SessionHandlerOptions sessionHandlerOptions);
     }
 }
