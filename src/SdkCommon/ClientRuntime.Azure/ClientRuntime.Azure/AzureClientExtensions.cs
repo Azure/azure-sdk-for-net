c24fbeebaed94f28d926581c76a2ee99a05ed341
shahabhijeet <abhishah@microsoft.com>
2017-07-05 13:32:51 -0700

    exposing underlying exception during LRO. Tests will be added next

--- src/SdkCommon/ClientRuntime.Azure/ClientRuntime.Azure/AzureClientExtensions.cs
+++ src/SdkCommon/ClientRuntime.Azure/ClientRuntime.Azure/AzureClientExtensions.cs
@@ -395,17 +395,57 @@ namespace Microsoft.Rest.Azure
                 pollingState.Status = AzureAsyncOperation.SuccessStatus;
             }
 
-            pollingState.Error = new CloudError()
+            pollingState = UpdatePollingStateForFailedState<TBody, THeader>(pollingState, responseWithResource, client);
+
+            if(pollingState?.Error == null)
             {
-                Code = pollingState.Status,
-                Message = string.Format(Resources.LongRunningOperationFailed, pollingState.Status)
-            };
+                pollingState.Error = new CloudError()
+                {
+                    Code = pollingState.Status,
+                    Message = string.Format(Resources.LongRunningOperationFailed, pollingState.Status)
+                };
+            }
+            
             pollingState.Response = responseWithResource.Response;
             pollingState.Request = responseWithResource.Request;
             pollingState.Resource = responseWithResource.Body.ToObject<TBody>(JsonSerializer
                 .Create(client.DeserializationSettings));
             pollingState.ResourceHeaders = responseWithResource.Headers.ToObject<THeader>(JsonSerializer
                 .Create(client.DeserializationSettings));
+        }
+
+        internal static PollingState<TBody, THeader> UpdatePollingStateForFailedState<TBody, THeader>(PollingState<TBody, THeader> pollState,
+            AzureOperationResponse<JObject, JObject> responseWithResource, IAzureClient client) where TBody: class where THeader:class
+        {
+            PollingState<TBody, THeader> pollingState = pollState;
+
+            if (AzureAsyncOperation.FailedStatuses.Any(
+                        s => s.Equals(pollingState.Status, StringComparison.OrdinalIgnoreCase)))
+            {
+                CloudError CError = null;
+
+                string responseContent = Task.Run(async () => await responseWithResource.Response.Content.ReadAsStringAsync().ConfigureAwait(false)).Result;
+                try
+                {
+                    CError = SafeJsonConvert.DeserializeObject<CloudError>(responseContent, client.DeserializationSettings);
+                }
+                catch (JsonException)
+                {
+                    // failed to deserialize, return empty body
+                }
+
+                pollingState.CloudException = new CloudException(string.Format(CultureInfo.InvariantCulture,
+                Resources.LongRunningOperationFailed, pollingState.Status))
+                {
+                    Body = CError,
+                    Request = new HttpRequestMessageWrapper(responseWithResource.Request, null),
+                    Response = new HttpResponseMessageWrapper(responseWithResource.Response, responseContent)
+                };
+
+                pollingState.Error = CError;
+            }
+
+            return pollingState;
         }
 
         /// <summary>
