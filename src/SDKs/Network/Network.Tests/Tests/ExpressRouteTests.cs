1838ef3ae6cb67b3aa130a3fe183d857a1f5ebf3
vinayada1 <vinayadamle@gmail.com>
2017-07-11 13:07:33 -0700

    Er test (#3459)
    * Add Express Route IPv6 test #924393
    
    * Add session recording for IPv6 test
    
    * Fix review comments #924393
    
    * Fix review comments - 2 #924393

--- src/SDKs/Network/Network.Tests/Tests/ExpressRouteTests.cs
+++ src/SDKs/Network/Network.Tests/Tests/ExpressRouteTests.cs
@@ -11,12 +11,14 @@ namespace Networks.Tests
     using System.Net;
     using Microsoft.Azure.Management.Network;
     using Microsoft.Azure.Management.Network.Models;
-    using Microsoft.Azure.Management.Resources;
-    using Microsoft.Azure.Management.Resources.Models;
+    using Microsoft.Azure.Management.ResourceManager;
+    using Microsoft.Azure.Management.ResourceManager.Models;
+
     using Microsoft.Azure.Test;
     using Networks.Tests.Helpers;
     using ResourceGroups.Tests;
     using Xunit;
+    using System.Threading.Tasks;
 
     public class ExpressRouteTests
     {
@@ -24,6 +26,10 @@ namespace Networks.Tests
         public const string MS_SecondaryPrefix = "199.168.202.0/30";
         public const string MS_PeerASN = "1000";
         public const string MS_PublicPrefix = "12.2.3.4/30";
+
+        public const string MS_PrimaryPrefix_V6 = "fc00::/126";
+        public const string MS_SecondaryPrefix_V6 = "fc01::/126";
+        public const string MS_PublicPrefix_V6 = "fc02::1/128";
 
         public const string Circuit_Provider = "bvtazureixp01";
         public const string Circuit_Location = "boydton 1 dc";
@@ -62,7 +68,7 @@ namespace Networks.Tests
 
             using (MockContext context = MockContext.Start(this.GetType().FullName))
             {
-                var resourcesClient = ResourcesManagementTestUtilities.GetResourceManagementClientWithHandler(context, handler1);
+                var resourcesClient = ResourcesManagementTestUtilities.GetResourceManagementClientWithHandler(context, handler1, true);
                 var networkManagementClient = NetworkManagementTestUtilities.GetNetworkManagementClientWithHandler(context, handler2);
                 var location = NetworkManagementTestUtilities.GetResourceLocation(resourcesClient, "Microsoft.Network/routefilters");
 
@@ -100,7 +106,7 @@ namespace Networks.Tests
 
             using (MockContext context = MockContext.Start(this.GetType().FullName))
             {
-                var resourcesClient = ResourcesManagementTestUtilities.GetResourceManagementClientWithHandler(context, handler1);
+                var resourcesClient = ResourcesManagementTestUtilities.GetResourceManagementClientWithHandler(context, handler1, true);
                 var networkManagementClient = NetworkManagementTestUtilities.GetNetworkManagementClientWithHandler(context, handler2);
                 var location = "westus" ;
 
@@ -121,6 +127,44 @@ namespace Networks.Tests
                 Assert.Equal(circuit.ServiceProviderProperties.BandwidthInMbps, Convert.ToInt32(Circuit_BW));
 
                 circuit = TestHelper.UpdateDefaultExpressRouteCircuitWithMicrosoftPeering(resourceGroupName,
+                    circuitName, networkManagementClient);
+
+                Assert.Equal(circuit.Name, circuitName);
+                Assert.Equal(circuit.ServiceProviderProperties.BandwidthInMbps, Convert.ToInt32(Circuit_BW));
+                Assert.NotNull(circuit.Peerings);
+
+                resourcesClient.ResourceGroups.Delete(resourceGroupName);
+            }
+        }
+
+        [Fact]
+        public void ExpressRouteMicrosoftPeeringApiWithIpv6Test()
+        {
+            var handler1 = new RecordedDelegatingHandler { StatusCodeToReturn = HttpStatusCode.OK };
+            var handler2 = new RecordedDelegatingHandler { StatusCodeToReturn = HttpStatusCode.OK };
+
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var resourcesClient = ResourcesManagementTestUtilities.GetResourceManagementClientWithHandler(context, handler1, true);
+                var networkManagementClient = NetworkManagementTestUtilities.GetNetworkManagementClientWithHandler(context, handler2);
+                var location = "westus";
+
+                string resourceGroupName = TestUtilities.GenerateName("csmrg");
+                resourcesClient.ResourceGroups.CreateOrUpdate(resourceGroupName,
+                    new ResourceGroup
+                    {
+                        Location = location
+                    });
+
+                string circuitName = "circuit";
+
+                var circuit = TestHelper.CreateDefaultExpressRouteCircuit(resourceGroupName,
+                    circuitName, location, networkManagementClient);
+
+                Assert.Equal(circuit.Name, circuitName);
+                Assert.Equal(circuit.ServiceProviderProperties.BandwidthInMbps, Convert.ToInt32(Circuit_BW));
+
+                circuit = TestHelper.UpdateDefaultExpressRouteCircuitWithIpv6MicrosoftPeering(resourceGroupName,
                     circuitName, networkManagementClient);
 
                 Assert.Equal(circuit.Name, circuitName);
