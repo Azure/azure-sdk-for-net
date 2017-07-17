353779d7021e7e99bb842e100875cc17530b988f
Johannes Bader <jobader@microsoft.com>
2017-07-17 09:43:30 -0700

    scripts

--- src/SDKs/Search/DataPlane/Microsoft.Azure.Search/generate.cmd
+++ src/SDKs/Search/DataPlane/Microsoft.Azure.Search/generate.cmd
@@ -4,9 +4,24 @@
 ::
 
 @echo off
-call %~dp0..\..\..\..\..\tools\generate.cmd search/data-plane %*
+setlocal
+
+if not "%1" == "" (set version="%1")         else (set version="latest")
+if not "%2" == "" (set specsRepoUser="%2")   else (set specsRepoUser="Azure")
+if not "%3" == "" (set specsRepoBranch="%3") else (set specsRepoBranch="current")
+set specFile="https://github.com/%specsRepoUser%/azure-rest-api-specs/blob/%specsRepoBranch%/specification/search/data-plane/readme.md"
+
+set sdksRoot=%~dp0..\..\..
+
+if "%4" == "" (call npm i -g autorest)
+
+@echo on
+call autorest %specFile% --version=%version% --csharp-sdks-folder=%sdksRoot% --csharp --search-folder=SearchIndex   --input-file=./Microsoft.Search/2016-09-01/searchindex.json
+call autorest %specFile% --version=%version% --csharp-sdks-folder=%sdksRoot% --csharp --search-folder=SearchService --input-file=./Microsoft.Search/2016-09-01/searchservice.json
 
 :: Make any necessary modifications
 pushd %~dp0
 powershell.exe .\Fix-GeneratedCode.ps1
 popd
+
+endlocal
