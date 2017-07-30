216c183d6239ccdecc100ce67f297863856590a9
shahabhijeet <abhishah@microsoft.com>
2017-07-29 17:24:37 -0700

    Fixes issue# 3396 in .NET SDK regarding bubbling up LRO errors. Added tests. Rearranged tests. Cleaned up few build warnings.

--- src/SdkCommon/ClientRuntime.Azure/ClientRuntime.Azure/AzureClientExtensions.cs
+++ src/SdkCommon/ClientRuntime.Azure/ClientRuntime.Azure/AzureClientExtensions.cs
@@ -16,7 +16,7 @@ using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Rest.Azure
 {
-    public static class AzureClientExtensions
+    public static partial class AzureClientExtensions
     {
         /// <summary>
         /// Gets operation result for long running operations.
@@ -356,6 +356,8 @@ namespace Microsoft.Rest.Azure
             }
             return true;
         }
+   
+/*
 
         /// <summary>
         /// Updates PollingState from GET operations.
@@ -509,6 +511,8 @@ namespace Microsoft.Rest.Azure
             {
                 throw new CloudException("The response from long running operation does not have a valid status code.");
             }
+
+            return;
         }
 
         /// <summary>
@@ -556,6 +560,8 @@ namespace Microsoft.Rest.Azure
             }
             catch { };
         }
+        
+        */
 
         /// <summary>
         /// Gets a resource from the specified URL.
