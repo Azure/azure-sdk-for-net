7af6f44b5b12766ca40eba76f7cca043448198c3
Nathan <nafan@microsoft.com>
2017-08-07 11:27:12 -0700

    Update SQL .NET SDK tests to get location from environment variable and fix TDE test (#3535)
    * Remove location hardcoding
    
    * Re-do all test recordings
    
    * Fix validation errors
    
    * Remove .swp files
    
    * Reorganize utilities
    
    * Responded to PR comments

--- src/SDKs/SqlManagement/Sql.Tests/VirtualNetworkRulesCrudScenarioTests.cs
+++ src/SDKs/SqlManagement/Sql.Tests/VirtualNetworkRulesCrudScenarioTests.cs
@@ -24,9 +24,9 @@ namespace Sql.Tests
             using (SqlManagementTestContext context = new SqlManagementTestContext(this))
             {
                 // Basic setup
-                var location = SqlManagementTestUtilities.DefaultEuapPrimaryLocationId;
+                var location = TestEnvironmentUtilities.DefaultEuapPrimaryLocationId;
                 ResourceGroup resourceGroup = context.CreateResourceGroup(location);
-                VirtualNetwork getVnetResponse = CreateVirtualNetwork(context, resourceGroup, subnetCount: 2, location: location);
+                VirtualNetwork getVnetResponse = CreateVirtualNetwork(context, resourceGroup, location: location, subnetCount: 2);
 
                 Server server = context.CreateServer(resourceGroup, location);
                 SqlManagementClient sqlClient = context.GetClient<SqlManagementClient>();
@@ -84,9 +84,9 @@ namespace Sql.Tests
             using (SqlManagementTestContext context = new SqlManagementTestContext(this))
             {
                 // Basic setup
-                var location = SqlManagementTestUtilities.DefaultEuapPrimaryLocationId;
+                var location = TestEnvironmentUtilities.DefaultEuapPrimaryLocationId;
                 ResourceGroup resourceGroup = context.CreateResourceGroup(location);
-                VirtualNetwork getVnetResponse = CreateVirtualNetwork(context, resourceGroup, subnetCount: 1, location: location);
+                VirtualNetwork getVnetResponse = CreateVirtualNetwork(context, resourceGroup, location: location, subnetCount: 1);
 
                 Server server = context.CreateServer(resourceGroup, location);
                 SqlManagementClient sqlClient = context.GetClient<SqlManagementClient>();
@@ -115,9 +115,9 @@ namespace Sql.Tests
             using (SqlManagementTestContext context = new SqlManagementTestContext(this))
             {
                 // Basic setup
-                var location = SqlManagementTestUtilities.DefaultEuapPrimaryLocationId;
+                var location = TestEnvironmentUtilities.DefaultEuapPrimaryLocationId;
                 ResourceGroup resourceGroup = context.CreateResourceGroup(location);
-                VirtualNetwork getVnetResponse = CreateVirtualNetwork(context, resourceGroup, subnetCount: 2, location: location);
+                VirtualNetwork getVnetResponse = CreateVirtualNetwork(context, resourceGroup, location: location, subnetCount: 2);
 
                 Server server = context.CreateServer(resourceGroup, location);
                 SqlManagementClient sqlClient = context.GetClient<SqlManagementClient>();
@@ -142,7 +142,7 @@ namespace Sql.Tests
             }
         }
 
-        public VirtualNetwork CreateVirtualNetwork(SqlManagementTestContext context, ResourceGroup resourceGroup, int subnetCount = 1, string location = SqlManagementTestUtilities.DefaultLocationId)
+        public VirtualNetwork CreateVirtualNetwork(SqlManagementTestContext context, ResourceGroup resourceGroup, string location, int subnetCount = 1)
         {
             NetworkManagementClient networkClient = context.GetClient<NetworkManagementClient>();
 
