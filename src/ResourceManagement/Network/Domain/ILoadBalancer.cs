b85fbaba06a959b8041bf606d50c9bd704dc0953
Martin Sawicki <marcins@microsoft.com>
2017-08-14 13:17:41 -0700

    Proper support for multiple IP configs on NICs to address issue 3185 (#3579)
    * proper support for multiple IP configs on NICs
    
    * minor test tweak
    
    * minor comment correction
    
    * reconverting Domain to apply the Beta annotation fix in the converter to LoadBalancer

--- src/ResourceManagement/Network/Domain/ILoadBalancer.cs
+++ src/ResourceManagement/Network/Domain/ILoadBalancer.cs
@@ -12,11 +12,11 @@ namespace Microsoft.Azure.Management.Network.Fluent
     /// Entry point for load balancer management API in Azure.
     /// </summary>
     public interface ILoadBalancer  :
+        Microsoft.Azure.Management.ResourceManager.Fluent.Core.IBeta,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.IGroupableResource<Microsoft.Azure.Management.Network.Fluent.INetworkManager,Models.LoadBalancerInner>,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.ResourceActions.IRefreshable<Microsoft.Azure.Management.Network.Fluent.ILoadBalancer>,
         Microsoft.Azure.Management.ResourceManager.Fluent.Core.ResourceActions.IUpdatable<LoadBalancer.Update.IUpdate>,
-        Microsoft.Azure.Management.Network.Fluent.IHasLoadBalancingRules,
-        Microsoft.Azure.Management.Network.Fluent.ILoadBalancerBeta
+        Microsoft.Azure.Management.Network.Fluent.IHasLoadBalancingRules
     {
         /// <summary>
         /// Gets inbound NAT pools, indexed by name.
@@ -44,14 +44,37 @@ namespace Microsoft.Azure.Management.Network.Fluent
         System.Collections.Generic.IReadOnlyDictionary<string,Microsoft.Azure.Management.Network.Fluent.ILoadBalancerFrontend> Frontends { get; }
 
         /// <summary>
+        /// Gets public (Internet-facing) frontends.
+        /// </summary>
+        System.Collections.Generic.IReadOnlyDictionary<string,Microsoft.Azure.Management.Network.Fluent.ILoadBalancerPublicFrontend> PublicFrontends { get; }
+
+        /// <summary>
         /// Gets TCP probes of this load balancer, indexed by the name.
         /// </summary>
         System.Collections.Generic.IReadOnlyDictionary<string,Microsoft.Azure.Management.Network.Fluent.ILoadBalancerTcpProbe> TcpProbes { get; }
+
+        /// <summary>
+        /// Gets private (internal) frontends.
+        /// </summary>
+        System.Collections.Generic.IReadOnlyDictionary<string,Microsoft.Azure.Management.Network.Fluent.ILoadBalancerPrivateFrontend> PrivateFrontends { get; }
 
         /// <summary>
         /// Gets backends for this load balancer to load balance the incoming traffic among, indexed by name.
         /// </summary>
         System.Collections.Generic.IReadOnlyDictionary<string,Microsoft.Azure.Management.Network.Fluent.ILoadBalancerBackend> Backends { get; }
 
+        /// <summary>
+        /// Searches for the public frontend that is associated with the provided public IP address, if one exists.
+        /// </summary>
+        /// <param name="publicIPAddress">A public IP address to search by.</param>
+        /// <return>A public frontend associated with the provided public IP address.</return>
+        Microsoft.Azure.Management.Network.Fluent.ILoadBalancerPublicFrontend FindFrontendByPublicIPAddress(IPublicIPAddress publicIPAddress);
+
+        /// <summary>
+        /// Searches for the public frontend that is associated with the provided public IP address, if one exists.
+        /// </summary>
+        /// <param name="publicIPAddressId">The resource ID of a public IP address to search by.</param>
+        /// <return>A public frontend associated with the provided public IP address.</return>
+        Microsoft.Azure.Management.Network.Fluent.ILoadBalancerPublicFrontend FindFrontendByPublicIPAddress(string publicIPAddressId);
     }
 }
