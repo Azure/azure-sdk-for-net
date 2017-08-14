78b51a2e347e01cda387168ed3b3534649921989
anuchan <anuamd@gmail.com>
2017-08-14 16:14:15 -0700

    Adding Managed Service Identity (MSI) sample

---
+++ Samples/Asset/create_resources_with_msi.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+
+/usr/bin/yes | sudo apt-get update
+/usr/bin/yes | sudo apt install python-pip
+/usr/bin/yes | sudo pip install --upgrade pip
+sudo pip install azure-cli
+az login -u $1@$2
+az storage account create -n $3 -g $4 -l $5 --sku Premium_LRS
