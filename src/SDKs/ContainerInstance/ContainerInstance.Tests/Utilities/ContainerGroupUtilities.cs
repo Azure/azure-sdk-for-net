89480675695b29f061ce235dd2d38424a70882ab
Scott Holden <scott@nullops.io>
2017-08-14 22:35:40 +1000

    Added zero second sleep period for playback tests

--- src/SDKs/ContainerInstance/ContainerInstance.Tests/Utilities/ContainerGroupUtilities.cs
+++ src/SDKs/ContainerInstance/ContainerInstance.Tests/Utilities/ContainerGroupUtilities.cs
@@ -1,16 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using Microsoft.Azure.Management.ContainerInstance;
 using Microsoft.Azure.Management.ContainerInstance.Models;
+using Microsoft.Azure.Test.HttpRecorder;
 
 namespace ContainerInstance.Tests
 {
     public class ContainerGroupUtilities
     {
 		private static readonly TimeSpan WaitTillTimeout = TimeSpan.FromMinutes(15);
-		private static readonly TimeSpan SleepPeriod = TimeSpan.FromSeconds(45);
+		private static readonly TimeSpan DefaultSleepPeriod = TimeSpan.FromSeconds(45);
 
 		private const string SucceededProvisioningState = "Succeeded";
 		private const string CreatingProvisioningState = "Creating";
@@ -18,6 +17,8 @@ namespace ContainerInstance.Tests
 		public static void WaitTillProvisioningStateSucceeded(ContainerInstanceManagementClient client, string resourceGroupName, string containerGroupName)
 		{
 			DateTime startTime = DateTime.Now;
+
+			TimeSpan sleepPeriod = HttpMockServer.Mode == HttpRecorderMode.Record ? DefaultSleepPeriod : TimeSpan.Zero;
 
 			string status = null;
 
@@ -28,7 +29,7 @@ namespace ContainerInstance.Tests
 					throw new TimeoutException($"Container Group didn't enter provisioning state '{SucceededProvisioningState}' within timeout");
 				}
 
-				Thread.Sleep((int)SleepPeriod.TotalMilliseconds);
+				Thread.Sleep((int)sleepPeriod.TotalMilliseconds);
 
 				ContainerGroup group = client.ContainerGroups.Get(resourceGroupName, containerGroupName);
 
