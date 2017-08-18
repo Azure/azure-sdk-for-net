45a0144de07b7ef66bf0a9d3174dbd5e04fbff1f
Guillermo Calderón_Meza <gucalder@microsoft.com>
2017-08-18 09:50:53 -0700

    Returning generate.md to its Azure\psSdkJson6 state

--- src/SDKs/Monitor/Management.Monitor/generate.cmd
+++ src/SDKs/Monitor/Management.Monitor/generate.cmd
@@ -1,54 +1,8 @@
-:: 
-:: Microsoft Azure SDK for Net - Generate library code 
-:: Copyright (C) Microsoft Corporation. All Rights Reserved. 
-:: 
- 
-@echo off 
-
-goto newCode
-
-if  "%2" == "" (
-   set specFile1="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/5288e653adfe3dd6ce235a790008a8215eedb918/monitor/compositeMonitorClient.json"
-) else (
-   set specFile1="%2"
-)
-
-if  "%3" == "" (
-   set specFile2="https://raw.githubusercontent.com/Azure/azure-rest-api-specs/5288e653adfe3dd6ce235a790008a8215eedb918/arm-monitor/compositeMonitorManagementClient.json"
-) else (
-   set specFile2="%3"
-)
-
-set generateFolder1=%~dp0Generated\Monitor
-set generateFolder2=%~dp0Generated\Management\Monitor
-
-if exist %generateFolder1% rd /S /Q  %generateFolder1%
-if exist %generateFolder2% rd /S /Q  %generateFolder2%
-
-if "%1" == "install" goto install
-goto new
-
-:install
-echo **** INFO: Installing AutoRest package
-call npm install autorest -g
-
-:new
-call autoRest -Modeler CompositeSwagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Management.Monitor -Input %specFile1% -outputDirectory %generateFolder1% -Header MICROSOFT_MIT -FT 1
-call autoRest -Modeler CompositeSwagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Management.Monitor.Management -Input %specFile2% -outputDirectory %generateFolder2% -Header MICROSOFT_MIT -FT 1
-
-
-autoRest -Modeler CompositeSwagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Management.Monitor -Input G:\temp\monitor\compositeMonitorClient.json -outputDirectory G:\GitHub\azure-sdk-for-net\src\SDKs\monitor\Management.Monitor\Generated\Monitor -Header MICROSOFT_MIT -FT 1
-autoRest -Modeler CompositeSwagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Management.Monitor.Management -Input G:\temp\arm-monitor\compositeMonitorManagementClient.json -outputDirectory G:\GitHub\azure-sdk-for-net\src\SDKs\monitor\Management.Monitor\Generated\Management\Monitor -Header MICROSOFT_MIT -FT 1
-
-:: =======
 ::
 :: Microsoft Azure SDK for Net - Generate library code
 :: Copyright (C) Microsoft Corporation. All Rights Reserved.
 ::
 
-:newCode
-
 @echo off
 call %~dp0..\..\..\..\tools\generate.cmd monitor/resource-manager %*
 call %~dp0..\..\..\..\tools\generate.cmd monitor/data-plane %*
-
