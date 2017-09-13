e63a16c3d1bafefef8c69e9a09ef187ecbb24b37
prbansa <31080710+prbansa@users.noreply.github.com>
2017-09-13 14:37:10 -0700

    Client SDK for MarketplaceOrdering Get and Create Agreement Terms APIs (#3695)
    * Client SDK for MarketplaceOrdering
    
    * Added  PackageReleaseNotes and updated to address other comments

---
+++ src/SDKs/MarketplaceOrdering/MarketplaceOrdering.Tests/Helpers/MarketplaceOrderingHelper.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace MarketplaceOrdering.Tests.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Azure.Management.MarketplaceOrdering;
+    using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+    using Newtonsoft.Json;
+    using System.Security.Cryptography;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json.Serialization;
+    using Newtonsoft.Json.Converters;
+
+    public static class MarketplaceOrderingHelper
+    {
+        public static MarketplaceOrderingAgreementsClient GetMarketplaceOrderingAgreementsClient(MockContext context, RecordedDelegatingHandler handler)
+        {
+            if (handler != null)
+            {
+                handler.IsPassThrough = true;
+                MarketplaceOrderingAgreementsClient marketplaceOrderingAgreementsClient = context.GetServiceClient<MarketplaceOrderingAgreementsClient>(handlers: handler);
+                return marketplaceOrderingAgreementsClient;
+            }
+
+            return null;
+        }
+
+        public static string GenerateRandomKey()
+        {
+            byte[] key256 = new byte[32];
+            using (var rngCryptoServiceProvider = RandomNumberGenerator.Create())
+            {
+                rngCryptoServiceProvider.GetBytes(key256);
+            }
+
+            return Convert.ToBase64String(key256);
+        }
+
+        public static string ConvertObjectToJSon<T>(T obj)
+        {
+            return ConvertObjectToJSonAsync(obj);
+        }
+
+        public static string ConvertObjectToJSonAsync(object obj)
+        {
+            if (obj != null)
+            {
+                return (Task.Factory.StartNew(() => JsonConvert.SerializeObject(obj, SerializeMediaTypeFormatterSettings))).Result;
+            }
+            return String.Empty;
+        }
+
+        private static readonly JsonSerializerSettings SerializeMediaTypeFormatterSettings = new JsonSerializerSettings
+        {
+            NullValueHandling = Newtonsoft.Json.NullValueHandling.Include,
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Converters = new List<JsonConverter>
+            {
+                new StringEnumConverter { CamelCaseText = false },
+            },
+            ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor
+        };
+
+    }
+}
