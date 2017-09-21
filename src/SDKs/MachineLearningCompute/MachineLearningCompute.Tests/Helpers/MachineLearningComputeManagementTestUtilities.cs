f0045ff872590f879d85e481ec34d90273ce475c
Steven Hutchings <sthutchi@microsoft.com>
2017-09-21 14:27:02 -0700

    Moving Machine Learning Compoute to public repo.

---
+++ src/SDKs/MachineLearningCompute/MachineLearningCompute.Tests/Helpers/MachineLearningComputeManagementTestUtilities.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Azure.Management.MachineLearningCompute;
+using Microsoft.Azure.Management.MachineLearningCompute.Models;
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace MachineLearningCompute.Tests.Helpers
+{
+    public static class MachineLearningComputeManagementTestUtilities
+    {
+        public static MachineLearningComputeManagementClient GetMachineLearningComputeManagementClient(MockContext context)
+        {
+            MachineLearningComputeManagementClient client = context.GetServiceClient<MachineLearningComputeManagementClient>();
+
+            return client;
+        }
+
+        public static ResourceGroup CreateResourceGroup(MockContext context, string resourceGroupName, string location)
+        {
+            ResourceManagementClient client = context.GetServiceClient<ResourceManagementClient>();
+
+            return client.ResourceGroups.CreateOrUpdate(
+                resourceGroupName,
+                new ResourceGroup
+                {
+                    Location = location
+                });
+        }
+
+        public static OperationalizationCluster CreateCluster(MachineLearningComputeManagementClient client, string resourceGroupName, 
+            string name, string location = "East US 2 EUAP", string description = "Test cluster", string clusterType = ClusterType.ACS, 
+            string orchestratorType = OrchestratorType.Kubernetes)
+        {
+            var newCluster = new OperationalizationCluster
+            {
+                Location = location,
+                ClusterType = clusterType,
+                Description = description,
+                ContainerService = new AcsClusterProperties
+                {
+                    OrchestratorType = orchestratorType,
+                    OrchestratorProperties = new KubernetesClusterProperties
+                    {
+                        ServicePrincipal = new ServicePrincipalProperties
+                        {
+                            ClientId = GetServicePrincipalId(),
+                            Secret = GetServicePrincipalSecret()
+                        }
+                    }
+                }
+            };
+
+            return client.OperationalizationClusters.CreateOrUpdate(resourceGroupName, name, newCluster);
+        }
+
+        public static string GetServicePrincipalId()
+        {
+            string servicePrincipalId = null;
+            if (HttpMockServer.Mode == HttpRecorderMode.Record)
+            {
+                var environment = TestEnvironmentFactory.GetTestEnvironment();
+                HttpMockServer.Variables[ConnectionStringKeys.ServicePrincipalKey] = environment.ConnectionString.KeyValuePairs.GetValueUsingCaseInsensitiveKey(ConnectionStringKeys.ServicePrincipalKey);
+                servicePrincipalId = HttpMockServer.Variables[ConnectionStringKeys.ServicePrincipalKey];
+            }
+            else if (HttpMockServer.Mode == HttpRecorderMode.Playback)
+            {
+                servicePrincipalId = HttpMockServer.Variables[ConnectionStringKeys.ServicePrincipalKey];
+            }
+            return servicePrincipalId;
+        }
+
+        public static string GetServicePrincipalSecret()
+        {
+            string servicePrincipalSecret = null;
+            if (HttpMockServer.Mode == HttpRecorderMode.Record)
+            {
+                var environment = TestEnvironmentFactory.GetTestEnvironment();
+                servicePrincipalSecret = environment.ConnectionString.KeyValuePairs.GetValueUsingCaseInsensitiveKey(ConnectionStringKeys.ServicePrincipalSecretKey);
+            }
+            else if (HttpMockServer.Mode == HttpRecorderMode.Playback)
+            {
+                servicePrincipalSecret = "abcde";
+            }
+            return servicePrincipalSecret;
+        }
+    }
+}
---
+++ src/SDKs/MachineLearningCompute/MachineLearningCompute.Tests/Tests/MachineLearningComputeTests.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Xunit;
+using MachineLearningCompute.Tests.Helpers;
+using Microsoft.Azure.Management.MachineLearningCompute;
+using Xunit.Abstractions;
+using Microsoft.Azure.Management.MachineLearningCompute.Models;
+using System.Linq;
+
+namespace MachineLearningCompute.Tests
+{
+    public class MachineLearningComputeTests
+    {
+        private readonly ITestOutputHelper output;
+        private const string resourceGroupLocation = "East US 2";
+        private const string resourceGroupNameRoot = "mlcrp-dotnet-client-test";
+        private const string clusterNameRoot = "mlcrp-test";
+
+        public MachineLearningComputeTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void CreateCluster()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var nameSuffix = "-create";
+                var resourceGroupName = resourceGroupNameRoot + nameSuffix;
+                var clusterName = clusterNameRoot + nameSuffix;
+
+                var mlcrpMgmtClient = MachineLearningComputeManagementTestUtilities.GetMachineLearningComputeManagementClient(context);
+
+                MachineLearningComputeManagementTestUtilities.CreateResourceGroup(context, resourceGroupName, resourceGroupLocation);
+                var createdCluster = MachineLearningComputeManagementTestUtilities.CreateCluster(mlcrpMgmtClient, resourceGroupName, clusterName);
+
+                Assert.NotNull(createdCluster);
+                Assert.Equal(clusterName, createdCluster.Name);
+            }
+        }
+
+        [Fact]
+        public void GetCluster()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var nameSuffix = "-get";
+                var resourceGroupName = resourceGroupNameRoot + nameSuffix;
+                var clusterName = clusterNameRoot + nameSuffix;
+
+                var mlcrpMgmtClient = MachineLearningComputeManagementTestUtilities.GetMachineLearningComputeManagementClient(context);
+
+                MachineLearningComputeManagementTestUtilities.CreateResourceGroup(context, resourceGroupName, resourceGroupLocation);
+                var createdCluster = MachineLearningComputeManagementTestUtilities.CreateCluster(mlcrpMgmtClient, resourceGroupName, clusterName);
+                var fetchedCluster = mlcrpMgmtClient.OperationalizationClusters.Get(resourceGroupName, clusterName);
+
+                Assert.NotNull(createdCluster);
+                Assert.NotNull(fetchedCluster);
+
+                Assert.Equal(createdCluster.Name, fetchedCluster.Name);
+                Assert.Equal(createdCluster.Location, fetchedCluster.Location);
+                Assert.Equal(createdCluster.ClusterType, fetchedCluster.ClusterType);
+                Assert.Equal(createdCluster.Description, fetchedCluster.Description);
+                Assert.Equal(createdCluster.ContainerService.OrchestratorType, fetchedCluster.ContainerService.OrchestratorType);
+            }
+        }
+
+        [Fact]
+        public void ListKeys()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var nameSuffix = "-keys";
+                var resourceGroupName = resourceGroupNameRoot + nameSuffix;
+                var clusterName = clusterNameRoot + nameSuffix;
+
+                var mlcrpMgmtClient = MachineLearningComputeManagementTestUtilities.GetMachineLearningComputeManagementClient(context);
+
+                MachineLearningComputeManagementTestUtilities.CreateResourceGroup(context, resourceGroupName, resourceGroupLocation);
+                MachineLearningComputeManagementTestUtilities.CreateCluster(mlcrpMgmtClient, resourceGroupName, clusterName, "East US 2 EUAP", "temp cluster", "ACS", "Kubernetes");
+
+                var keys = mlcrpMgmtClient.OperationalizationClusters.ListKeys(resourceGroupName, clusterName);
+
+                Assert.NotNull(keys.StorageAccount.ResourceId);
+                Assert.NotNull(keys.StorageAccount.PrimaryKey);
+                Assert.NotNull(keys.StorageAccount.SecondaryKey);
+                Assert.NotNull(keys.ContainerRegistry.LoginServer);
+                Assert.NotNull(keys.ContainerRegistry.Password);
+                Assert.NotNull(keys.ContainerRegistry.Password2);
+                Assert.NotNull(keys.ContainerRegistry.Username);
+                Assert.NotNull(keys.ContainerService.AcsKubeConfig);
+                Assert.NotNull(keys.ContainerService.ImagePullSecretName);
+            }
+        }
+
+        [Fact]
+        public void ListClustersInResourceGroup()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var nameSuffix = "-list-rg";
+                var resourceGroupName = resourceGroupNameRoot + nameSuffix;
+                var clusterName = clusterNameRoot + nameSuffix;
+
+                var mlcrpMgmtClient = MachineLearningComputeManagementTestUtilities.GetMachineLearningComputeManagementClient(context);
+                MachineLearningComputeManagementTestUtilities.CreateResourceGroup(context, resourceGroupName, resourceGroupLocation);
+                var createdCluster = MachineLearningComputeManagementTestUtilities.CreateCluster(mlcrpMgmtClient, resourceGroupName, clusterName);
+
+                var clusterNames = mlcrpMgmtClient.OperationalizationClusters.ListByResourceGroup(resourceGroupName).Select(cluster => cluster.Name);
+
+                Assert.True(clusterNames.Contains(createdCluster.Name));
+            }
+        }
+
+        [Fact]
+        public void ListClustersBySubscriptionId()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var nameSuffix = "-list-sub";
+                var resourceGroupName = resourceGroupNameRoot + nameSuffix;
+                var clusterName = clusterNameRoot + nameSuffix;
+
+                var mlcrpMgmtClient = MachineLearningComputeManagementTestUtilities.GetMachineLearningComputeManagementClient(context);
+                MachineLearningComputeManagementTestUtilities.CreateResourceGroup(context, resourceGroupName, resourceGroupLocation);
+                var createdCluster = MachineLearningComputeManagementTestUtilities.CreateCluster(mlcrpMgmtClient, resourceGroupName, clusterName);
+
+                var clusterNames = mlcrpMgmtClient.OperationalizationClusters.ListBySubscriptionId().Select(cluster => cluster.Name);
+
+                Assert.True(clusterNames.Contains(createdCluster.Name));
+            }
+        }
+
+        [Fact]
+        public void DeleteCluster()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var nameSuffix = "-delete";
+                var resourceGroupName = resourceGroupNameRoot + nameSuffix;
+                var clusterName = clusterNameRoot + nameSuffix;
+
+                var mlcrpMgmtClient = MachineLearningComputeManagementTestUtilities.GetMachineLearningComputeManagementClient(context);
+
+                MachineLearningComputeManagementTestUtilities.CreateResourceGroup(context, resourceGroupName, resourceGroupLocation);
+                var createdCluster = MachineLearningComputeManagementTestUtilities.CreateCluster(mlcrpMgmtClient, resourceGroupName, clusterName);
+
+                mlcrpMgmtClient.OperationalizationClusters.Delete(resourceGroupName, createdCluster.Name);
+
+                var exception = Assert.Throws<ErrorResponseWrapperException>(() => mlcrpMgmtClient.OperationalizationClusters.Get(resourceGroupName, createdCluster.Name));
+
+                Assert.Contains("NotFound", exception.Message);
+            }
+        }
+
+        [Fact]
+        public void CheckSystemServicesUpdatesAvailable()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var nameSuffix = "-checkupdate";
+                var resourceGroupName = resourceGroupNameRoot + nameSuffix;
+                var clusterName = clusterNameRoot + nameSuffix;
+
+                var mlcrpMgmtClient = MachineLearningComputeManagementTestUtilities.GetMachineLearningComputeManagementClient(context);
+
+                MachineLearningComputeManagementTestUtilities.CreateResourceGroup(context, resourceGroupName, resourceGroupLocation);
+                var createdCluster = MachineLearningComputeManagementTestUtilities.CreateCluster(mlcrpMgmtClient, resourceGroupName, clusterName);
+
+                var updateAvailables = mlcrpMgmtClient.OperationalizationClusters.CheckSystemServicesUpdatesAvailable(resourceGroupName, clusterName);
+
+                Assert.NotNull(updateAvailables);
+            }
+        }
+
+        [Fact]
+        public void UpdateSystemServices()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var nameSuffix = "-update";
+                var resourceGroupName = resourceGroupNameRoot + nameSuffix;
+                var clusterName = clusterNameRoot + nameSuffix;
+
+                var mlcrpMgmtClient = MachineLearningComputeManagementTestUtilities.GetMachineLearningComputeManagementClient(context);
+
+                MachineLearningComputeManagementTestUtilities.CreateResourceGroup(context, resourceGroupName, resourceGroupLocation);
+                var createdCluster = MachineLearningComputeManagementTestUtilities.CreateCluster(mlcrpMgmtClient, resourceGroupName, clusterName);
+
+                var updateResponse = mlcrpMgmtClient.OperationalizationClusters.UpdateSystemServices(resourceGroupName, clusterName);
+
+                Assert.Equal("Succeeded", updateResponse.UpdateStatus);
+                Assert.NotNull(updateResponse.UpdateStartedOn);
+                Assert.NotNull(updateResponse.UpdateCompletedOn);
+            }
+        }
+    }
+}
