64cdb58c068ebf6bea7e21304bf16ff2533dfef0
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2017-09-22 11:14:44 -0700

    Move sources to a new repo.

--- Samples/Asset/installMySQL.ps1
+++
@@ -1,6 +0,0 @@
-﻿# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco feature enable -n=allowGlobalConfirmation
-choco install mysql
--- src/ResourceManagement/AppService/Domain/InterfaceImpl/AppServiceBaseImpl.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-namespace Microsoft.Azure.Management.AppService.Fluent
-{
-    internal abstract partial class AppServiceBaseImpl<FluentT,FluentImplT,FluentWithCreateT,FluentUpdateT> 
-    {
-    }
-}
