8582990cfe97ea1715de06ba7a0b62f223ce4ff2
REDMOND\muazmian <muazmian@microsoft.com>
2017-09-26 16:42:29 -0700

    Adding InMemory and Scenario Tests for Microsoft.Management and generated code.

---
+++ src/SDKs/Resource/Resource.Tests/ScenarioTests/ManagementGroupsTests.ScenarioTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Resource.Tests.Helpers;
+using Xunit;
+
+namespace ResourceGroups.Tests
+{
+    public class LiveManagementGroupsTests : TestBase
+    {
+
+#if FullNetFx
+
+        [Fact]
+        public void ListManagementGroupsTest()
+        {
+            HttpMockServer.RecordsDirectory = GetSessionsDirectoryPath();
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var managementGroupsClient = ManagementGroupsTestUtilities.GetManagementGroupsApiClient(context,
+                    new RecordedDelegatingHandler {StatusCodeToReturn = HttpStatusCode.OK});
+
+                var managementGroups = managementGroupsClient.ManagementGroups.List();
+
+                Assert.NotNull(managementGroups);
+                Assert.NotEqual(0, managementGroups.Count());
+                Assert.NotNull(managementGroups.First().Id);
+                Assert.NotNull(managementGroups.First().Type);
+                Assert.NotNull(managementGroups.First().Name);
+                
+            }
+
+        }
+
+        [Fact]
+        public void GetManagementGroup()
+        {
+            HttpMockServer.RecordsDirectory = GetSessionsDirectoryPath();
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var managementGroupsClient = ManagementGroupsTestUtilities.GetManagementGroupsApiClient(context,
+                    new RecordedDelegatingHandler { StatusCodeToReturn = HttpStatusCode.OK });
+
+                var groupId = "e16c61d4-c2c9-444d-9ee0-2f89561e6ef9";
+
+                managementGroupsClient.GroupId = new Guid(groupId);
+
+                var managementGroup = managementGroupsClient.ManagementGroups.Get();
+
+                Assert.NotNull(managementGroup);
+                Assert.NotNull(managementGroup.Id);
+                Assert.NotNull(managementGroup.Name);
+                Assert.NotNull(managementGroup.Type);
+            }
+
+        }
+
+        [Fact]
+        public void GetManagementGroupWithExpand()
+        {
+            HttpMockServer.RecordsDirectory = GetSessionsDirectoryPath();
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var managementGroupsClient = ManagementGroupsTestUtilities.GetManagementGroupsApiClient(context,
+                    new RecordedDelegatingHandler { StatusCodeToReturn = HttpStatusCode.OK });
+
+                var groupId = "e16c61d4-c2c9-444d-9ee0-2f89561e6ef9";
+
+                managementGroupsClient.GroupId = new Guid(groupId);
+
+                var managementGroup = managementGroupsClient.ManagementGroups.Get("children");
+
+                Assert.NotNull(managementGroup);
+                Assert.NotNull(managementGroup.Id);
+                Assert.NotNull(managementGroup.Name);
+                Assert.NotNull(managementGroup.Type);
+                Assert.Equal(managementGroup.Details.ManagementGroupType, "Enrollment");
+
+                Assert.NotNull(managementGroup.Children);
+                Assert.Equal("Department", managementGroup.Children.First().ChildType);
+                Assert.Null(managementGroup.Children.First().Children);
+                
+            }
+
+        }
+
+        [Fact]
+        public void GetManagementGroupWithExpandAndRecurse()
+        {
+            HttpMockServer.RecordsDirectory = GetSessionsDirectoryPath();
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                var managementGroupsClient = ManagementGroupsTestUtilities.GetManagementGroupsApiClient(context,
+                    new RecordedDelegatingHandler { StatusCodeToReturn = HttpStatusCode.OK });
+
+                var groupId = "e16c61d4-c2c9-444d-9ee0-2f89561e6ef9";
+
+                managementGroupsClient.GroupId = new Guid(groupId);
+
+                var managementGroup = managementGroupsClient.ManagementGroups.Get("children", true);
+
+                Assert.NotNull(managementGroup);
+                Assert.NotNull(managementGroup.Id);
+                Assert.NotNull(managementGroup.Name);
+                Assert.NotNull(managementGroup.Type);
+                Assert.Equal(managementGroup.Details.ManagementGroupType, "Enrollment");
+
+                Assert.NotNull(managementGroup.Children);
+                Assert.Equal("Department", managementGroup.Children.First().ChildType);
+
+                Assert.NotNull(managementGroup.Children.First().Children);
+                Assert.Equal("Account", managementGroup.Children.First().Children.First().ChildType);
+
+                Assert.NotNull(managementGroup.Children.First().Children.First().Children);
+                Assert.Equal("Subscription", managementGroup.Children.First().Children.First().Children.First().ChildType);
+
+            }
+
+        }
+
+#endif
+
+        private static string GetSessionsDirectoryPath()
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SessionRecords");
+        }
+    }
+}
