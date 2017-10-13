2ffd522a97aa6f88749d30e22355b6aa928ea3e5
Brian Golden <brian.golden09@gmail.com>
2017-10-13 09:58:29 -0700

    Adding data factory CRUD scenario test (#3774)
    * This is the first batch of scenario tests for the ADF V2 SDK.
      * Partially addresses #3726; more tests need to be added

---
+++ src/SDKs/DataFactory/DataFactory.Tests/ScenarioTests/DataFactoryScenarioTests.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using DataFactory.Tests.Utils;
+using Microsoft.Azure.Management.DataFactory;
+using Microsoft.Azure.Management.DataFactory.Models;
+using Microsoft.Rest.Azure;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace DataFactory.Tests.ScenarioTests
+{
+    public class DataFactoryScenarioTests : ScenarioTestBase<DataFactoryScenarioTests>
+    {
+        [Fact]
+        [Trait(TraitName.TestType, TestType.Scenario)]
+        public void DataFactoryCrud()
+        {
+            var expectedFactory = new Factory(location: FactoryLocation);
+
+            Action<DataFactoryManagementClient> action = (client) =>
+            {
+                Factory createResponse = client.Factories.CreateOrUpdate(ResourceGroupName, DataFactoryName, expectedFactory);
+                this.ValidateFactory(createResponse);
+
+                Factory getResponse = client.Factories.Get(ResourceGroupName, DataFactoryName);
+                this.ValidateFactory(getResponse);
+
+                IPage<Factory> listByResourceGroupResponse = client.Factories.ListByResourceGroup(ResourceGroupName);
+                this.ValidateFactory(listByResourceGroupResponse.First());
+            };
+
+            Action<DataFactoryManagementClient> finallyAction = (client) =>
+            {
+                client.Factories.Delete(ResourceGroupName, DataFactoryName);
+            };
+
+            this.RunTest(action, finallyAction);
+        }
+
+        private void ValidateFactory(Factory actualFactory)
+        {
+            Assert.Equal(DataFactoryName, actualFactory.Name);
+            Assert.Equal(FactoryLocation, actualFactory.Location);
+            Assert.Equal("Succeeded", actualFactory.ProvisioningState);
+        }
+    }
+}
