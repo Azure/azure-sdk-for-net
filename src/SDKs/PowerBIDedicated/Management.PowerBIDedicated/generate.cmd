052b7e299cedd041bcbd81ee762e7e795db70f8e
Sagi Sela <sagis@microsoft.com>
2017-10-16 12:30:47 +0100

    Update PowerBIDedicated SDK

--- src/SDKs/PowerBIDedicated/Management.PowerBIDedicated/generate.cmd
+++ src/SDKs/PowerBIDedicated/Management.PowerBIDedicated/generate.cmd
@@ -3,4 +3,5 @@
 :: Copyright (C) Microsoft Corporation. All Rights Reserved.
 ::
 
-autorest -Modeler Swagger -CodeGenerator Azure.CSharp -Namespace Microsoft.Azure.Management.PowerBIDedicated -Input https://github.com/Azure/azure-rest-api-specs-pr/blob/current/specification/powerbidedicated/resource-manager/Microsoft.PowerBIdedicated/2017-10-01/powerbidedicated.json -outputDirectory .\Generated -Header MICROSOFT_MIT
+@echo off
+call %~dp0..\..\..\..\tools\generate.cmd powerbidedicated/resource-manager %*
