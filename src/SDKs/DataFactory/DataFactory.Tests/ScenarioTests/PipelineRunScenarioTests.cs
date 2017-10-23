b17f806d0d4e7f3239d1a6d2c3b4baf57ae5eb4a
Milan Tomic <mtomic@microsoft.com>
2017-10-23 16:24:34 -0700

    Scenario test for pipeline run 2

--- src/SDKs/DataFactory/DataFactory.Tests/ScenarioTests/PipelineRunScenarioTests.cs
+++ src/SDKs/DataFactory/DataFactory.Tests/ScenarioTests/PipelineRunScenarioTests.cs
@@ -1,17 +1,17 @@
-﻿using DataFactory.Tests.Utils;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using DataFactory.Tests.Utils;
 using Microsoft.Azure.Management.DataFactory;
 using Microsoft.Azure.Management.DataFactory.Models;
-using Microsoft.Rest.Azure;
 using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace DataFactory.Tests.ScenarioTests
 {
-    public class PipelineRunTests : ScenarioTestBase<DataFactoryScenarioTests>
+    public class PipelineRunScenarioTests : ScenarioTestBase<PipelineRunScenarioTests>
     {
         [Fact]
         [Trait(TraitName.TestType, TestType.Scenario)]
@@ -22,8 +22,12 @@ namespace DataFactory.Tests.ScenarioTests
             Action<DataFactoryManagementClient> action = async (client) =>
             {
                 Factory createResponse = client.Factories.CreateOrUpdate(ResourceGroupName, DataFactoryName, expectedFactory);
-                AzureOperationResponse result = await client.Factories.CancelPipelineRunWithHttpMessagesAsync(ResourceGroupName, DataFactoryName, "efbe5443-9879-4495-94a6-4d7c394133ad");
-                Assert.Equal(result.Response.StatusCode, HttpStatusCode.BadRequest);
+                ErrorResponseException exception = await Assert.ThrowsAsync<ErrorResponseException>(async () =>
+                {
+                    await client.Factories.CancelPipelineRunWithHttpMessagesAsync(ResourceGroupName, DataFactoryName, "efbe5443-9879-4495-94a6-4d7c394133ad");
+                });
+
+                Assert.Equal(exception.Response.StatusCode, HttpStatusCode.BadRequest);
             };
 
             Action<DataFactoryManagementClient> finallyAction = (client) =>
