eba032972e77a5f8f83bb90d52379a94699a4d3f
Milan Tomic <mtomic@microsoft.com>
2017-10-23 15:49:12 -0700

    Scenario test for cancel run api

---
+++ src/SDKs/DataFactory/DataFactory.Tests/ScenarioTests/PipelineRunTests.cs
@@ -0,0 +1,36 @@
+﻿using DataFactory.Tests.Utils;
+using Microsoft.Azure.Management.DataFactory;
+using Microsoft.Azure.Management.DataFactory.Models;
+using Microsoft.Rest.Azure;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataFactory.Tests.ScenarioTests
+{
+    public class PipelineRunTests : ScenarioTestBase<DataFactoryScenarioTests>
+    {
+        [Fact]
+        [Trait(TraitName.TestType, TestType.Scenario)]
+        public void CancelPipelineRun()
+        {
+            var expectedFactory = new Factory(location: FactoryLocation);
+
+            Action<DataFactoryManagementClient> action = async (client) =>
+            {
+                Factory createResponse = client.Factories.CreateOrUpdate(ResourceGroupName, DataFactoryName, expectedFactory);
+                AzureOperationResponse result = await client.Factories.CancelPipelineRunWithHttpMessagesAsync(ResourceGroupName, DataFactoryName, "efbe5443-9879-4495-94a6-4d7c394133ad");
+                Assert.Equal(result.Response.StatusCode, HttpStatusCode.BadRequest);
+            };
+
+            Action<DataFactoryManagementClient> finallyAction = (client) =>
+            {
+                client.Factories.Delete(ResourceGroupName, DataFactoryName);
+            };
+            this.RunTest(action, finallyAction);
+        }
+    }
+}
