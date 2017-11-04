88235153a683a2a6381d4fc3afffcbb8479a3fc9
hitenjava <hijava@microsoft.com>
2017-11-03 18:39:55 -0700

    Remove encryption for passwords as entire input is now encrypted.

--- src/SDKs/DataMigration/DataMigration.Tests/ScenarioTests/CRUDTaskTests.cs
+++ src/SDKs/DataMigration/DataMigration.Tests/ScenarioTests/CRUDTaskTests.cs
@@ -91,7 +91,7 @@ namespace DataMigration.Tests.ScenarioTests
                         EncryptConnection = true,
                         TrustServerCertificate = true,
                         UserName = "testuser",
-                        Password = Cryptography.Encrypt(service.PublicKey, "testPassword"),
+                        Password = "testPassword",
                         Authentication = AuthenticationType.SqlAuthentication,
                     }
                 )
