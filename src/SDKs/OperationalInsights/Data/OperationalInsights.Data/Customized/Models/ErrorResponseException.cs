8925f1821e5c671db4fb58fe3d9d172ff1dd7b20
Chris Suich <chsuich@microsoft.com>
2017-11-07 13:59:43 -0500

    Adding Microsoft.OperationalInsights Data Plane SDK (moved Management under Management folder)

---
+++ src/SDKs/OperationalInsights/Data/OperationalInsights.Data/Customized/Models/ErrorResponseException.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Azure.Management.OperationalInsights.Data.Models
+{
+    using Microsoft.Rest;
+
+    public partial class ErrorResponseException : RestException
+    {
+        public override string ToString()
+        {
+            return Body != null ? Body.ToString() : Response.Content;
+        }
+    }
+}
