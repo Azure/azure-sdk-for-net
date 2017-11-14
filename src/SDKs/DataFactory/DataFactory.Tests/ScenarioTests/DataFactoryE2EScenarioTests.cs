ce80868cc9f6e876f261d949c8a56d5d83dad62d
Dean Talley <deata@microsoft.com>
2017-11-14 11:15:00 -0800

    CR updates #1

--- src/SDKs/DataFactory/DataFactory.Tests/ScenarioTests/DataFactoryE2EScenarioTests.cs
+++ src/SDKs/DataFactory/DataFactory.Tests/ScenarioTests/DataFactoryE2EScenarioTests.cs
@@ -34,14 +34,10 @@ namespace DataFactory.Tests.ScenarioTests
             Func<DataFactoryManagementClient, Task> action = async (client) =>
             {
 #region DataFactoryTests
-                // Start Factories operations, leaving factory available
-
-                // DataFactory -- Create
                 AzureOperationResponse<Factory> createResponse = await client.Factories.CreateOrUpdateWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, expectedFactory);
                 this.ValidateFactory(createResponse.Body, this.DataFactoryName);
                 Assert.Equal(HttpStatusCode.OK, createResponse.Response.StatusCode);
 
-                // DataFactory -- Update
                 var tags = new Dictionary<string, string>
                 {
                     { "exampleTag", "exampleValue" }
@@ -50,164 +46,97 @@ namespace DataFactory.Tests.ScenarioTests
                 this.ValidateFactory(updateResponse.Body, this.DataFactoryName);
                 Assert.Equal(HttpStatusCode.OK, updateResponse.Response.StatusCode);
 
-                // DataFactory -- Get
                 AzureOperationResponse<Factory> getResponse = await client.Factories.GetWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName);
                 this.ValidateFactory(getResponse.Body, this.DataFactoryName);
                 Assert.Equal(HttpStatusCode.OK, getResponse.Response.StatusCode);
 
-                // DataFactory -- ListByResourceGroup
                 AzureOperationResponse<IPage<Factory>> listByResourceGroupResponse = await client.Factories.ListByResourceGroupWithHttpMessagesAsync(this.ResourceGroupName);
                 this.ValidateFactory(listByResourceGroupResponse.Body.First(), this.DataFactoryName);
                 Assert.Equal(HttpStatusCode.OK, listByResourceGroupResponse.Response.StatusCode);
 
-                // DataFactory -- List
                 AzureOperationResponse<IPage<Factory>> listResponse = await client.Factories.ListByResourceGroupWithHttpMessagesAsync(this.ResourceGroupName);
                 this.ValidateFactory(listResponse.Body.First(), this.DataFactoryName);
                 Assert.Equal(HttpStatusCode.OK, listResponse.Response.StatusCode);
 #endregion
 
 #region LinkedServiceTests
-                // Start LinkedServices operations, leaving linked service available
-
                 var expectedLinkedService = GetLinkedServiceResource(null);
 
-                // LinkedService -- Create
                 AzureOperationResponse<LinkedServiceResource> createLinkServiceResponse = await client.LinkedServices.CreateOrUpdateWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, linkedServiceName, expectedLinkedService);
                 this.ValidateLinkedService(expectedLinkedService, createLinkServiceResponse.Body, linkedServiceName);
                 Assert.Equal(HttpStatusCode.OK, createLinkServiceResponse.Response.StatusCode);
 
-                // LinkedService -- Update
                 var updatedLinkedService = GetLinkedServiceResource("linkedService description");
                 AzureOperationResponse<LinkedServiceResource> updateLinkServiceResponse = await client.LinkedServices.CreateOrUpdateWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, linkedServiceName, updatedLinkedService);
                 this.ValidateLinkedService(updatedLinkedService, updateLinkServiceResponse.Body, linkedServiceName);
                 Assert.Equal(HttpStatusCode.OK, updateLinkServiceResponse.Response.StatusCode);
 
-                // LinkedService -- Get
                 AzureOperationResponse<LinkedServiceResource> getLinkedServiceResponse = await client.LinkedServices.GetWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, linkedServiceName);
                 this.ValidateLinkedService(updatedLinkedService, getLinkedServiceResponse.Body, linkedServiceName);
                 Assert.Equal(HttpStatusCode.OK, getResponse.Response.StatusCode);
 
-                // LinkedService -- ListByFactory
                 AzureOperationResponse<IPage<LinkedServiceResource>> listLinkedServiceResponse = await client.LinkedServices.ListByFactoryWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName);
                 this.ValidateLinkedService(updatedLinkedService, listLinkedServiceResponse.Body.First(), linkedServiceName);
                 Assert.Equal(HttpStatusCode.OK, listLinkedServiceResponse.Response.StatusCode);
 #endregion
 
-#region DatasetTests
 
-                // Start Datasets operations, leaving dataset available
+#region DatasetTests
                 DatasetResource expectedDataset = GetDatasetResource(null);
 
-                // Dataset -- Create
                 AzureOperationResponse<DatasetResource> createDatasetResponse = await client.Datasets.CreateOrUpdateWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, datasetName, expectedDataset);
                 this.ValidateDataset(expectedDataset, createDatasetResponse.Body, datasetName);
                 Assert.Equal(HttpStatusCode.OK, createDatasetResponse.Response.StatusCode);
 
-                // Dataset -- Update
                 DatasetResource updatedDataset = GetDatasetResource("dataset description");
                 AzureOperationResponse<DatasetResource> updateDatasetResponse = await client.Datasets.CreateOrUpdateWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, datasetName, updatedDataset);
                 this.ValidateDataset(updatedDataset, updateDatasetResponse.Body, datasetName);
                 Assert.Equal(HttpStatusCode.OK, updateDatasetResponse.Response.StatusCode);
 
-                // Dataset -- Get
                 AzureOperationResponse<DatasetResource> getDatasetResponse = await client.Datasets.GetWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, datasetName);
                 this.ValidateDataset(updatedDataset, updateDatasetResponse.Body, datasetName);
                 Assert.Equal(HttpStatusCode.OK, updateDatasetResponse.Response.StatusCode);
 
-                // Dataset -- ListByFactoryName
                 AzureOperationResponse<IPage<DatasetResource>> listDatasetResponse = await client.Datasets.ListByFactoryWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName);
                 this.ValidateDataset(updatedDataset, listDatasetResponse.Body.First(), datasetName);
                 Assert.Equal(HttpStatusCode.OK, listDatasetResponse.Response.StatusCode);
 #endregion
 
 #region PipelineTests
-                // All Pipelines and PipelineRuns operations, creating/running/monitoring/deleting pipeline
-
-                // Pipeline -- Create
                 PipelineResource expectedPipeline = GetPipelineResource(null);
                 AzureOperationResponse<PipelineResource> createPipelineResponse = await client.Pipelines.CreateOrUpdateWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, pipelineName, expectedPipeline);
                 this.ValidatePipeline(expectedPipeline, createPipelineResponse.Body, pipelineName);
                 Assert.Equal(HttpStatusCode.OK, createPipelineResponse.Response.StatusCode);
 
-                // Pipeline -- Update
                 PipelineResource updatedPipeline = GetPipelineResource("pipeline description");
                 AzureOperationResponse<PipelineResource> updatePipelineResponse = await client.Pipelines.CreateOrUpdateWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, pipelineName, updatedPipeline);
                 this.ValidatePipeline(updatedPipeline, createPipelineResponse.Body, pipelineName);
                 Assert.Equal(HttpStatusCode.OK, createPipelineResponse.Response.StatusCode);
 
-                // Pipeline -- Get
                 AzureOperationResponse<PipelineResource> getPipelineResponse = await client.Pipelines.GetWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, pipelineName);
                 this.ValidatePipeline(updatedPipeline, createPipelineResponse.Body, pipelineName);
                 Assert.Equal(HttpStatusCode.OK, createPipelineResponse.Response.StatusCode);
 
-                // Pipeline -- ListByFactory
                 AzureOperationResponse<IPage<PipelineResource>> listPipelineResponse = await client.Pipelines.ListByFactoryWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName);
                 this.ValidatePipeline(updatedPipeline, listPipelineResponse.Body.First(), pipelineName);
                 Assert.Equal(HttpStatusCode.OK, listPipelineResponse.Response.StatusCode);
 #endregion
 
-#region PipelineRunTests
-#if PipelineRuns
-                // Pipeline -- CreateRun
-                DateTime beforeStartTime = DateTime.UtcNow.AddMinutes(-1); // allow 1 minute for clock skew
-                //string runId = CapturePipelines_CreateRun(); // 202, ISSUE service doesn't follow long-running pattern
-                string[] outputBlobNameList = new string[1];
-                outputBlobNameList[0] = outputBlobName;
-
-                JArray outputBlobNameArray = JArray.FromObject(outputBlobNameList);
-                Dictionary<string, object> arguments = new Dictionary<string, object>
-                {
-                    { "OutputBlobNameList",  outputBlobNameArray }
-                };
-
-                AzureOperationResponse<CreateRunResponse> createPipelineRunResponse = await client.Pipelines.CreateRunWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, pipelineName, arguments);
-                // 202, ISSUE service doesn't follow long-running pattern
-                Assert.Equal(HttpStatusCode.Accepted, createPipelineRunResponse.Response.StatusCode);
-                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(120)); // Prefer to get succeeded monitoring result on first attempt even if it slows capture
-                DateTime afterEndTime = DateTime.UtcNow.AddMinutes(10); // allow 10 minutes for run time, monitoring latency, and clock skew
-
-                // Pipeline -- CancelRun
-                createPipelineRunResponse = await client.Pipelines.CreateRunWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, pipelineName, arguments);
-                Assert.Equal(HttpStatusCode.Accepted, createPipelineRunResponse.Response.StatusCode);
-                AzureOperationResponse cancelPipelineRunResponse = await client.Factories.CancelPipelineRunWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, createPipelineRunResponse.Body.RunId);
-                Assert.Equal(HttpStatusCode.OK, cancelPipelineRunResponse.Response.StatusCode);
-
-
-                //CapturePipelineRuns_ListByFactory(runId, beforeStartTime, afterEndTime); // 200, waits until succeeded so ready to get logs
-                //CapturePipelineRuns_Get(runId); // 200
-                //CaptureActivityRuns_ListByPipelineRun(runId, beforeStartTime, afterEndTime); // 200
-#endif
-#endregion
-
 #region TriggerTests
-                // Start Trigger operations, leaving triggers available
-
-                // Trigger -- Create
                 TriggerResource expectedTrigger = this.GetTriggerResource(null);
                 AzureOperationResponse<TriggerResource> createTriggerResponse = await client.Triggers.CreateOrUpdateWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, triggerName, expectedTrigger);
                 this.ValidateTrigger(expectedTrigger, createTriggerResponse.Body, triggerName);
                 Assert.Equal(HttpStatusCode.OK, createTriggerResponse.Response.StatusCode);
 
-                // Trigger -- Update
                 TriggerResource updatedTrigger = GetTriggerResource("trigger description");
                 AzureOperationResponse<TriggerResource> updateTriggerResponse = await client.Triggers.CreateOrUpdateWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, triggerName, updatedTrigger);
                 this.ValidateTrigger(updatedTrigger, updateTriggerResponse.Body, triggerName);
                 Assert.Equal(HttpStatusCode.OK, updateTriggerResponse.Response.StatusCode);
 
-                // Trigger -- Get
                 AzureOperationResponse<TriggerResource> getTriggerResponse = await client.Triggers.GetWithHttpMessagesAsync(this.ResourceGroupName, this.DataFactoryName, triggerName);
                 this.ValidateTrigger(updatedTrigger, getTriggerResponse.Body, triggerName);
                 Assert.Equal(HttpStatusCode.OK, getTriggerResponse.Response.StatusCode);
-                #endregion
-
-                #region TriggerRunTests
-#if TriggerRuns
-                //CaptureTriggers_Start(); // 202
-                //CaptureTriggers_ListByFactory(); // 200
-                //CaptureTriggers_ListRuns(); // 200
-                //CaptureTriggers_Stop(); // 202
-#endif
-                #endregion
+#endregion
 
 #region TestCleanup
                 // Triggers - delete
