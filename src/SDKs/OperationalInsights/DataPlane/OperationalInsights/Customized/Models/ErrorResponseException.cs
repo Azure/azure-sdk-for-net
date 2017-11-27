a1215dcae7164758f32ad79b74125676102c95b2
Chris <csuich2@gmail.com>
2017-11-27 17:51:18 -0500

    Adding Microsoft.OperationalInsights Data Plane SDK (moved Management… (#3859)
    * Adding Microsoft.OperationalInsights Data Plane SDK (moved Management under Management folder)
    
    * Regenerated OMS data plane (now uses TimeSpan for timespan param)
    
    * Moved OperationalInsights Data/ to DataPlane/
    
    * Undid whitespace change in OMS Management SDK
    
    * Regenerated OMS data plane from API spec, fixed namespaces and updated a test
    
    * Added releates notes for 1.0.0-preview of Operational Insights Data Plane

---
+++ src/SDKs/OperationalInsights/DataPlane/OperationalInsights/Customized/Models/ErrorResponseException.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Azure.OperationalInsights.Models
+{
+    using Microsoft.Rest;
+
+    public partial class ErrorResponseException : RestException
+    {
+        public override string ToString()
+        {
+            return Body != null ? Body.ToString() : Response.Content;
+        }
+    }
+}
