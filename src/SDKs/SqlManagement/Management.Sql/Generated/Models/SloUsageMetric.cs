560e324b1584693fc0a42392de429380c3b5591a
Jared Moore <jaredmoo@microsoft.com>
2017-12-14 14:22:54 -0800

    Updated available service objectives & editions

--- src/SDKs/SqlManagement/Management.Sql/Generated/Models/SloUsageMetric.cs
+++ src/SDKs/SqlManagement/Management.Sql/Generated/Models/SloUsageMetric.cs
@@ -30,9 +30,16 @@ namespace Microsoft.Azure.Management.Sql.Models
         /// Initializes a new instance of the SloUsageMetric class.
         /// </summary>
         /// <param name="serviceLevelObjective">The serviceLevelObjective for
-        /// SLO usage metric. Possible values include: 'Basic', 'S0', 'S1',
-        /// 'S2', 'S3', 'P1', 'P2', 'P3', 'P4', 'P6', 'P11', 'P15', 'System',
-        /// 'System2', 'ElasticPool'</param>
+        /// SLO usage metric. Possible values include: 'System', 'System0',
+        /// 'System1', 'System2', 'System3', 'System4', 'System2L', 'System3L',
+        /// 'System4L', 'Free', 'Basic', 'S0', 'S1', 'S2', 'S3', 'S4', 'S6',
+        /// 'S7', 'S9', 'S12', 'P1', 'P2', 'P3', 'P4', 'P6', 'P11', 'P15',
+        /// 'PRS1', 'PRS2', 'PRS4', 'PRS6', 'DW100', 'DW200', 'DW300', 'DW400',
+        /// 'DW500', 'DW600', 'DW1000', 'DW1200', 'DW1000c', 'DW1500',
+        /// 'DW1500c', 'DW2000', 'DW2000c', 'DW3000', 'DW2500c', 'DW3000c',
+        /// 'DW6000', 'DW5000c', 'DW6000c', 'DW7500c', 'DW10000c', 'DW15000c',
+        /// 'DW30000c', 'DS100', 'DS200', 'DS300', 'DS400', 'DS500', 'DS600',
+        /// 'DS1000', 'DS1200', 'DS1500', 'DS2000', 'ElasticPool'</param>
         /// <param name="serviceLevelObjectiveId">The serviceLevelObjectiveId
         /// for SLO usage metric.</param>
         /// <param name="inRangeTimeRatio">Gets or sets inRangeTimeRatio for
@@ -52,8 +59,16 @@ namespace Microsoft.Azure.Management.Sql.Models
 
         /// <summary>
         /// Gets the serviceLevelObjective for SLO usage metric. Possible
-        /// values include: 'Basic', 'S0', 'S1', 'S2', 'S3', 'P1', 'P2', 'P3',
-        /// 'P4', 'P6', 'P11', 'P15', 'System', 'System2', 'ElasticPool'
+        /// values include: 'System', 'System0', 'System1', 'System2',
+        /// 'System3', 'System4', 'System2L', 'System3L', 'System4L', 'Free',
+        /// 'Basic', 'S0', 'S1', 'S2', 'S3', 'S4', 'S6', 'S7', 'S9', 'S12',
+        /// 'P1', 'P2', 'P3', 'P4', 'P6', 'P11', 'P15', 'PRS1', 'PRS2', 'PRS4',
+        /// 'PRS6', 'DW100', 'DW200', 'DW300', 'DW400', 'DW500', 'DW600',
+        /// 'DW1000', 'DW1200', 'DW1000c', 'DW1500', 'DW1500c', 'DW2000',
+        /// 'DW2000c', 'DW3000', 'DW2500c', 'DW3000c', 'DW6000', 'DW5000c',
+        /// 'DW6000c', 'DW7500c', 'DW10000c', 'DW15000c', 'DW30000c', 'DS100',
+        /// 'DS200', 'DS300', 'DS400', 'DS500', 'DS600', 'DS1000', 'DS1200',
+        /// 'DS1500', 'DS2000', 'ElasticPool'
         /// </summary>
         [JsonProperty(PropertyName = "serviceLevelObjective")]
         public string ServiceLevelObjective { get; private set; }
