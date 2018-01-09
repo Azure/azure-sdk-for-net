73a7e87ce4e666b4624e13c4d34dd8faab356f14
Deepak Shankargouda <deshank@microsoft.com>
2018-01-08 17:52:41 -0800

    more stashing

--- src/SdkCommon/ClientRuntime/ClientRuntime/RestExceptionGeneric.cs
+++ src/SdkCommon/ClientRuntime/ClientRuntime/RestExceptionGeneric.cs
@@ -1,12 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
+using System;
 using System.Runtime.Serialization;
 namespace Microsoft.Rest
 {
 #if FullNetFx
     [Serializable]
 #endif
-    public class RestException<V> : System.Exception, IRestException, IHttpRestException<V>
+    public class RestException<V> : System.Exception, IRestException
     {
         
         /// <summary>
