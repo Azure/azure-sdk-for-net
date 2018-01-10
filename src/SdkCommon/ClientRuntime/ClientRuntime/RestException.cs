65b3eabdbf71f599fd8449da09acac5acacdf9a7
Deepak Shankargouda <deshank@microsoft.com>
2018-01-09 17:16:26 -0800

    more stashing

--- src/SdkCommon/ClientRuntime/ClientRuntime/RestException.cs
+++ src/SdkCommon/ClientRuntime/ClientRuntime/RestException.cs
@@ -40,34 +40,6 @@ namespace Microsoft.Rest
         {
         }
 
-        /// <summary>
-        /// Gets information about the associated HTTP request.
-        /// </summary>
-        public HttpRequestMessageWrapper Request { get; set; }
-
-        /// <summary>
-        /// Gets information about the associated HTTP response.
-        /// </summary>
-        public HttpResponseMessageWrapper Response { get; set; }
-
-        /// <summary>
-        /// the status code returned by server
-        /// </summary>
-        //public int ResponseStatusCode { get; set; }
-
-        /// <summary>
-        /// Initializes a new instance of the CloudException class caused by another exception.
-        /// </summary>
-        /// <param name="message">A description of the error.</param>
-        /// <param name="requestMessage">The request message.</param>
-        /// <param name="responseMessage">The response message.</param>
-        public RestException(string message, HttpRequestMessageWrapper requestMessage, HttpResponseMessageWrapper responseMessage, int statusCode) : base(message)
-        {
-            Request = requestMessage;
-            Response = responseMessage;
-            //ResponseStatusCode = statusCode;
-        }
-
 #if FullNetFx
         /// <summary>
         /// Initializes a new instance of the RestException class.
