ce213cfe92dec1e8828efe6fae106c183f3a9cbd
Deepak Shankargouda <deshank@microsoft.com>
2018-01-10 16:16:53 -0800

    more stashing

--- src/SdkCommon/ClientRuntime/ClientRuntime/IRestErrorModel.cs
+++ src/SdkCommon/ClientRuntime/ClientRuntime/IRestErrorModel.cs
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
+
 namespace Microsoft.Rest
 {
     public interface IRestErrorModel
     {
-        void CreateAndThrowException(HttpRequestMessageWrapper reqMessage, HttpResponseMessageWrapper respMessage);
+        void CreateAndThrowException(string errorMessage, HttpRequestMessageWrapper request, HttpResponseMessageWrapper response);
     }
 }
