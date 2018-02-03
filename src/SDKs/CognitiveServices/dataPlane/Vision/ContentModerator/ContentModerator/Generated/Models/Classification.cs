9a91315aacfa2811787e13916f2888d9a105cb93
Sonali DSouza <v-sodsou@microsoft.com>
2018-02-02 16:53:07 -0800

    Classify Changes

--- src/SDKs/CognitiveServices/dataPlane/Vision/ContentModerator/ContentModerator/Generated/Models/Classification.cs
+++ src/SDKs/CognitiveServices/dataPlane/Vision/ContentModerator/ContentModerator/Generated/Models/Classification.cs
@@ -25,16 +25,13 @@ namespace Microsoft.CognitiveServices.ContentModerator.Models
         /// <summary>
         /// Initializes a new instance of the Classification class.
         /// </summary>
-        /// <param name="adultScore">The adult score.</param>
-        /// <param name="racyScore">The racy score.</param>
-        /// <param name="offensiveScore">The offensive score.</param>
         /// <param name="reviewRecommended">The review recommended
         /// flag.</param>
-        public Classification(double? adultScore = default(double?), double? racyScore = default(double?), double? offensiveScore = default(double?), bool? reviewRecommended = default(bool?))
+        public Classification(Score category1 = default(Score), Score category2 = default(Score), Score category3 = default(Score), bool? reviewRecommended = default(bool?))
         {
-            AdultScore = adultScore;
-            RacyScore = racyScore;
-            OffensiveScore = offensiveScore;
+            Category1 = category1;
+            Category2 = category2;
+            Category3 = category3;
             ReviewRecommended = reviewRecommended;
             CustomInit();
         }
@@ -45,22 +42,19 @@ namespace Microsoft.CognitiveServices.ContentModerator.Models
         partial void CustomInit();
 
         /// <summary>
-        /// Gets or sets the adult score.
         /// </summary>
-        [JsonProperty(PropertyName = "AdultScore")]
-        public double? AdultScore { get; set; }
+        [JsonProperty(PropertyName = "Category1")]
+        public Score Category1 { get; set; }
 
         /// <summary>
-        /// Gets or sets the racy score.
         /// </summary>
-        [JsonProperty(PropertyName = "RacyScore")]
-        public double? RacyScore { get; set; }
+        [JsonProperty(PropertyName = "Category2")]
+        public Score Category2 { get; set; }
 
         /// <summary>
-        /// Gets or sets the offensive score.
         /// </summary>
-        [JsonProperty(PropertyName = "OffensiveScore")]
-        public double? OffensiveScore { get; set; }
+        [JsonProperty(PropertyName = "Category3")]
+        public Score Category3 { get; set; }
 
         /// <summary>
         /// Gets or sets the review recommended flag.
