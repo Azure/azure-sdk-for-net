ded4b7ac6bd1d85998f40ac6dedc740bf6d02c0e
dsgouda <deshank@microsoft.com>
2018-03-07 23:33:22 +0000

    Addressing comments

--- tools/BuildAssets/psModules/CodeGenerationModules/generateDotNetSdkCode.psm1
+++ tools/BuildAssets/psModules/CodeGenerationModules/generateDotNetSdkCode.psm1
@@ -1,3 +1,44 @@
+$errorStream = New-Object -TypeName "System.Text.StringBuilder";
+$outputStream = New-Object -TypeName "System.Text.StringBuilder";
+
+
+function Clear-OutputStreams {
+    $errorStream.Clear() | Out-Null
+    $outputStream.Clear() | Out-Null
+}
+
+function Write-InfoLog {
+    param(
+        [string] $msg,
+        [switch] $logToConsole,
+        [switch] $logToFile
+    )
+    if(![string]::IsNullOrEmpty($msg) -and $logToFile)
+    {
+        $outputStream.Append("$msg`n") | Out-Null
+    }
+    if($logToConsole)
+    {
+        Write-Host $msg
+    }
+}
+
+function Write-ErrorLog {
+    param(
+        [string] $msg,
+        [switch] $logToConsole,
+        [switch] $logToFile
+    )
+    if(![string]::IsNullOrEmpty($msg) -and $logToFile)
+    {
+        $errorStream.Append("$msg`n") | Out-Null
+    }
+    if($logToConsole)
+    {
+        Write-Error $msg
+    }
+}
+
 function launchProcess {
     param(
         [Parameter(Mandatory = $true)]
@@ -20,98 +61,147 @@ function launchProcess {
     $p.WaitForExit()
     $stdout = $p.StandardOutput.ReadToEnd()
     $stderr = $p.StandardError.ReadToEnd()
-    Write-Host $stdout
-    Write-Host $stderr
-    return $p.ExitCode
+    Write-InfoLog $stdout -logToConsole
+    Write-ErrorLog $stderr
+    if($p.ExitCode -ne 0)
+    {
+        throw [System.Exception] "Command $cmd $args returned $p.ExitCode"
+    }
 }
 
 function Get-AutoRestHelp {
-    Write-Host("Fetching AutoRest help");
+    Write-InfoLog "Fetching AutoRest help" -logToConsole
     launchProcess "cmd.exe" "/c autorest.cmd --help"
 }
 
 function Install-AutoRest {
     param(
         [Parameter(Mandatory = $true)]
-        $Version
+        $AutoRestVersion
     )
-    Write-Host "Installing AutoRest version: $Version";
-    $exitCode = launchProcess "cmd.exe" "/c npm.cmd install -g autorest@$Version"
-    if ($exitCode -ne 0) {
-        Write-Error "AutoRest installation failed; please try again"
+    
+    Write-InfoLog "Installing AutoRest version: $AutoRestVersion" -logToConsole
+    
+    Try {
+        launchProcess "cmd.exe" "/c npm.cmd install -g autorest@$AutoRestVersion"
     }
-    else {
-        Write-Host "AutoRest installed successfully."
+    Catch [System.Exception] {
+        Write-ErrorLog $_.Exception.ToString()
+        throw [System.Exception] "AutoRest Installation failed"
     }
+    Write-InfoLog "AutoRest installed successfully." -logToConsole
 }
 
 function Start-CodeGeneration {
 param(
-    [Parameter(Mandatory = $true, HelpMessage ="Please provide a spec to generate the code")]
-    [string] $ConfigFile,
+    [Parameter(Mandatory = $true)]
+    [string] $SpecsRepoFork,
+    [Parameter(Mandatory = $true)]
+    [string] $SpecsRepoBranch,
+    [Parameter(Mandatory = $true)]
+    [string] $SpecsRepoName,
     [Parameter(Mandatory = $true, HelpMessage ="Please provide an output directory for the generated code")]
     [string] $SdkDirectory,
     [Parameter(Mandatory = $true, HelpMessage ="Please provide a version for the AutoRest release")]
-    [string] $Version
+    [string] $AutoRestVersion
     )
-
-    Write-Host("Generating CSharp code");
+    
+    $configFile="https://github.com/$SpecsRepoFork/$SpecsRepoName/blob/$SpecsRepoBranch/specification/$ResourceProvider/readme.md" 
+    Write-InfoLog "Generating CSharp code" -logToConsole
     $cmd = "cmd.exe"
-    $args = "/c autorest.cmd $ConfigFile --csharp --csharp-sdks-folder=$SdkDirectory --version=$Version --reflect-api-versions"
-    Write-Output "Executing AutoRest command"
-    Write-Output "$cmd $args"
-    launchProcess $cmd $args
+    $args = "/c autorest.cmd $configFile --csharp --csharp-sdks-folder=$SdkDirectory --version=$AutoRestVersion --reflect-api-versions"
+    
+    Write-InfoLog "Executing AutoRest command" -logToFile
+    Write-InfoLog "$cmd $args" -logToFile
+
+    Try {
+        launchProcess $cmd $args
+    }
+    Catch [System.Exception] {
+        Write-ErrorLog $_.Exception.ToString()
+        throw [System.Exception] "AutoRest code generation for $configFile failed. Please try again"
+    }
+    
+    Try {
+        Start-MetadataGeneration -AutoRestVersion $AutoRestVersion -SpecsRepoFork $SpecsRepoFork -SpecsRepoBranch $SpecsRepoBranch
+    }
+    Catch [System.Exception] {
+        Write-ErrorLog $_.Exception.ToString()
+        throw [System.Exception] "Metadata generation for $configFile failed. Please try again"
+    }
 }
 
 function Start-MetadataGeneration {
     param(
+
+        [Parameter(Mandatory = $true)]
+        [string] $AutoRestVersion,
         [Parameter(Mandatory = $true)]
         [string] $SpecsRepoFork,
         [Parameter(Mandatory = $true)]
-        [string] $SpecsRepoBranch,
-        [Parameter(Mandatory = $true)]
-        [string] $Version
+        [string] $SpecsRepoBranch
     )
+    
+    Write-InfoLog $([DateTime]::UtcNow.ToString('u').Replace('Z',' UTC')) -logToFile
 
-    Write-Output $([DateTime]::UtcNow.ToString('u').Replace('Z',' UTC'))
-
-    Write-Output ""
-    Write-Output "1) azure-rest-api-specs repository information"
-    Write-Output "GitHub fork: $SpecsRepoFork"
-    Write-Output "Branch:      $SpecsRepoBranch"
+    Write-InfoLog "" -logToFile
+    Write-InfoLog "1) azure-rest-api-specs repository information" -logToFile
+    Write-InfoLog "GitHub fork: $SpecsRepoFork" -logToFile
+    Write-InfoLog "Branch:      $SpecsRepoBranch" -logToFile
     
     Try
     {
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        Write-Output "Commit:     " (Invoke-RestMethod "https://api.github.com/repos/$($SpecsRepoFork)/azure-rest-api-specs/branches/$($SpecsRepoBranch)").commit.sha
+        $op = (Invoke-RestMethod "https://api.github.com/repos/$($SpecsRepoFork)/azure-rest-api-specs/branches/$($SpecsRepoBranch)").commit.sha | Out-String
+        Write-InfoLog "Commit:      $op" -logToFile
     }
     Catch [System.Exception]
     {
-        Write-Output $_.Exception.ToString()
+        Write-ErrorLog $_.Exception.ToString()
+        throw $_
     }
 
-    Write-Output ""
-    Write-Output "2) AutoRest information"
-    Write-Output "Requested version:" $Version
+    Write-InfoLog "" -logToFile
+    Write-InfoLog "2) AutoRest information" -logToFile
+    Write-InfoLog "Requested version: $AutoRestVersion" -logToFile
+    
     Try
     {
-        Write-Output "Bootstrapper version:   " (npm list -g autorest)
+        $op = $((npm list -g autorest) | Out-String).Replace("`n", " ").Replace("`r"," ").Trim()
+        Write-InfoLog "Bootstrapper version:    $op" -logToFile
+        Write-InfoLog "`n" -logToFile
     }
     Catch{}
     Try
     {
-        Write-Output "Latest installed version:   " (autorest --list-installed | Where {$_ -like "*Latest Core Installed*"}).Split()[-1]
+        $op = (autorest --list-installed | Where {$_ -like "*Latest Core Installed*"}).Split()[-1] | Out-String
+        $op = $op.Replace("`n", " ").Replace("`r"," ").Trim()
+        Write-InfoLog "Latest installed version:    $op" -logToFile
     }
     Catch{}
     Try
     {
-        Write-Output "Latest installed version:   " (autorest --list-installed | Where {$_ -like "*@microsoft.azure/autorest-core*"} | Select -Last 1).Split('|')[3]
+        $op = (autorest --list-installed | Where {$_ -like "*@microsoft.azure/autorest-core*"} | Select -Last 1).Split('|')[3] | Out-String
+        $op = $op.Replace("`n", " ").Replace("`r"," ").Trim()
+        Write-InfoLog "Latest installed version:    $op" -logToFile
     }
     Catch{}
+}
+
+function Get-ErrorStream {
+    $errorStream.ToString()
+}
+
+function Get-OutputStream {
+    $outputStream.ToString()
 }
 
 
 export-modulemember -function Get-AutoRestHelp
 export-modulemember -function Install-AutoRest
 export-modulemember -function Start-CodeGeneration
-export-modulemember -function Start-MetadataGeneration
+export-modulemember -function Get-ErrorStream
+export-modulemember -function Get-OutputStream
+export-modulemember -function Write-InfoLog
+export-modulemember -function Write-ErrorLog
+export-modulemember -function Clear-OutputStreams
