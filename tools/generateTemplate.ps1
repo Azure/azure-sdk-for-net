26560ec3ec3e33b557672d03032aa74fe93fec84
dsgouda <deshank@microsoft.com>
2018-03-10 01:04:27 +0000

    Addressing comments

--- tools/generateTemplate.ps1
+++
@@ -1,4 +0,0 @@
-<#
-    This is a template to describe how to call the generateTool.ps1 script to generate the code using AutoRest
-#>
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "$(split-path $SCRIPT:MyInvocation.MyCommand.Path -parent)\generateTool.ps1" -ResourceProvider "compute/resource-manager" -SpecsRepoFork "Azure" -SpecsRepoBranch "master" -SpecsRepoName "azure-rest-api-specs" -SdkDirectory "$([System.IO.Path]::GetTempPath())\Compute" -AutoRestVersion "latest" -PowershellInvoker
