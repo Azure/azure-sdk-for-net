759c11ba11b0ee0b4d0d062308513a5539ff3df1
Rohit Joy <rohitjoy@microsoft.com>
2018-03-28 19:15:18 -0700

    .Net SDK for PostgreSQL..

---
+++ src/SDKs/DBforPostgreSQL/DBforPostgreSQL.Tests/ScenarioTests/CRUDDBForPostgreSQLTestsBase.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using DBforPostgreSQL.Tests.Helpers;
+using Microsoft.Azure.Management.DBforPostgreSQL;
+using Microsoft.Azure.Management.DBforPostgreSQL.Models;
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+
+namespace DBforPostgreSQL.Tests.ScenarioTests
+{
+    public class CRUDDBForPostgreSQLTestsBase : TestBase
+    {
+        protected static string ResourceGroupName;
+        protected static string ServerName;
+        protected static string DmsProjectName;
+        protected static string DmsTaskName;
+
+        public CRUDDBForPostgreSQLTestsBase()
+        {
+            ResourceGroupName = "pgsdkrg";
+            ServerName = "pgsdkserver";
+        }
+
+        protected Server CreateDBForPostgreSQLInstance(MockContext context,
+            PostgreSQLManagementClient client,
+            ResourceGroup resourceGroup,
+            string serverName)
+        {
+            return client.Servers.Create(
+                resourceGroup.Name,
+                serverName,
+                new ServerForCreate(
+                    properties: new ServerPropertiesForDefaultCreate(
+                        administratorLogin: "testUser",
+                        administratorLoginPassword: "testPassword1!"),
+                    location: resourceGroup.Location,
+                    sku: new Microsoft.Azure.Management.DBforPostgreSQL.Models.Sku(name: "B_Gen5_1")));
+        }
+
+        protected ResourceGroup CreateResourceGroup(MockContext context,
+            RecordedDelegatingHandler handler,
+            string resourceGroupName,
+            string location)
+        {
+            var resourcesClient =
+                Utilities.GetResourceManagementClient(
+                    context,
+                    handler);
+
+            var resourceGroup = resourcesClient.ResourceGroups.CreateOrUpdate(
+                resourceGroupName,
+                new ResourceGroup
+                {
+                    Location = location
+                });
+
+            return resourceGroup;
+        }
+
+        protected void DeleteResourceGroup(MockContext context,
+            RecordedDelegatingHandler handler,
+            string resourceGroupName)
+        {
+            var resourcesClient =
+                Utilities.GetResourceManagementClient(
+                    context,
+                    handler);
+
+            resourcesClient.ResourceGroups.Delete(resourceGroupName);
+        }
+    }
+}
