fd795fa5422709a12be354a9e71382cbfab115d3
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-04-11 11:35:51 -0700

    Added requested changes

--- src/AzureStack/ComputeAdmin/Compute.Admin/generate.cmd
+++ src/AzureStack/ComputeAdmin/Compute.Admin/generate.cmd
@@ -5,6 +5,6 @@
 
 @echo off
 rd Generated /S /Q
-call %~dp0..\..\..\..\tools\generate.cmd azsadmin/resource-manager/compute latest deathly809 azs.compute.admin azure-rest-api-specs %CD%
+call %~dp0..\..\..\..\tools\generate.cmd azsadmin/resource-manager/compute latest Azure master azure-rest-api-specs %CD%
 move /Y Compute\Compute.Admin\Generated .
 rd Compute /S /Q
