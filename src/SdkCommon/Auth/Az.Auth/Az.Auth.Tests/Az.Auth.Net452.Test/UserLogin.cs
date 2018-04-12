d304347e4156da90a28880146f911f8722cd654a
shahabhijeet <abhishah@microsoft.com>
2018-04-12 01:07:03 -0700

    Adding separate test projects for Auth library. Adding a way to honor token Audience URI as is. Adding .NET 461 target framework. Restricting ADAL 2.28.x for .NET 452 target framework

---
+++ src/SdkCommon/Auth/Az.Auth/Az.Auth.Tests/Az.Auth.Net452.Test/UserLogin.cs
@@ -0,0 +1,79 @@
+﻿namespace Az.Auth.Net452.Test
+{
+    using Microsoft.IdentityModel.Clients.ActiveDirectory;
+    using Microsoft.Rest;
+    using Microsoft.Rest.Azure.Authentication;
+    using System;
+    using Xunit;
+    using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class UserLogin: TestBase
+    {  
+        public UserLogin() : base()
+        {
+
+        }
+
+        [Theory(Skip = "Interactive tests")]
+        [InlineData("AADTenant=<tenantId>;" +
+                "HttpRecorderMode=Record;" +
+                "Environment=Custom;" +
+                "ResourceManagementUri=https://management.microsoftazure.de/;" +
+                "ServiceManagementUri=https://management.core.cloudapi.de/;" +
+                "GalleryUri=https://gallery.cloudapi.de/;" +
+                "GraphUri=https://graph.cloudapi.de/;" +
+                "AADAuthUri=https://login.microsoftonline.de/;" +
+                "IbizaPortalUri=http://portal.microsoftazure.de/;" +
+                "RdfePortalUri=https://management.core.cloudapi.de/;" +
+                "GraphTokenAudienceUri=https://graph.cloudapi.de/;" +
+                "AADTokenAudienceUri=https://management.core.cloudapi.de/"
+            )]
+        public void InteractiveUserLogin(string cnnStr)
+        {
+            LiteralCnnString = cnnStr;
+            ServiceClientCredentials svcClientCred = null;
+            svcClientCred = UserTokenProvider.LoginWithPromptAsync( this.TenantId, GetADClientSettings(), ActiveDirectoryServiceSettings.AzureGermany, () => { return TaskScheduler.FromCurrentSynchronizationContext(); }).GetAwaiter().GetResult();
+            Assert.NotNull(svcClientCred);
+        }
+
+
+        [Theory(Skip = "Interactive tests")]
+        [InlineData("AADTenant=<tenantId>;" +
+                "UserId=<userId>;" +
+                "Password=<pwd>;" +
+                "ServicePrincipal=1950a258-227b-4e31-a9cf-717495945fc2;" +
+               "HttpRecorderMode=Record;" +
+               "Environment=Custom;" +
+               "ResourceManagementUri=https://management.microsoftazure.de/;" +
+               "ServiceManagementUri=https://management.core.cloudapi.de/;" +
+               "GalleryUri=https://gallery.cloudapi.de/;" +
+               "GraphUri=https://graph.cloudapi.de/;" +
+               "AADAuthUri=https://login.microsoftonline.de/;" +
+               "IbizaPortalUri=http://portal.microsoftazure.de/;" +
+               "RdfePortalUri=https://management.core.cloudapi.de/;" +
+               "GraphTokenAudienceUri=https://graph.cloudapi.de/;" +
+               "AADTokenAudienceUri=https://management.core.cloudapi.de/"
+           )]
+        public void SilentUserLogin(string cnnStr)
+        {
+            LiteralCnnString = cnnStr;
+            ServiceClientCredentials svcClientCred = null;
+            svcClientCred = UserTokenProvider.LoginSilentAsync(this.ClientId, this.TenantId, this.UserName, this.Password, ActiveDirectoryServiceSettings.AzureGermany).GetAwaiter().GetResult();
+            Assert.NotNull(svcClientCred);
+        }
+
+        public ActiveDirectoryClientSettings GetADClientSettings()
+        {
+            ActiveDirectoryClientSettings adClient = new ActiveDirectoryClientSettings()
+            {
+                ClientId = this.ClientId,
+                ClientRedirectUri = new Uri("urn:ietf:wg:oauth:2.0:oob"),
+                PromptBehavior = PromptBehavior.Always
+            };
+
+            return adClient;
+        }
+    }
+}
