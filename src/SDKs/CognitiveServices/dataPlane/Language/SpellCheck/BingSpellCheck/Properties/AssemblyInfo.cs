7c5c092dad80672051d2ca24cb8c8b09e6e9ad2c
ashku-ms <33531737+ashku-ms@users.noreply.github.com>
2018-04-13 14:18:40 -0700

    Update AssemblyInfo.cs

--- src/SDKs/CognitiveServices/dataPlane/Language/SpellCheck/BingSpellCheck/Properties/AssemblyInfo.cs
+++ src/SDKs/CognitiveServices/dataPlane/Language/SpellCheck/BingSpellCheck/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure Cognitive Services SpellCheck Client Library")]
 [assembly: AssemblyDescription("Provides API functions for consuming the Microsoft Azure Cognitive Services SpellCheck API.")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
