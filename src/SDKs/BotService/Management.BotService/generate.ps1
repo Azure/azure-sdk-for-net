b67917e07f63a4124fdda7773e4c8d872fee96e3
Carlos Castro <ccastro@microsoft.com>
2018-04-17 09:37:30 -0700

    Bot Service: Add generate.ps1

---
+++ src/SDKs/BotService/Management.BotService/generate.ps1
@@ -0,0 +1,1 @@
+﻿powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "$(split-path $SCRIPT:MyInvocation.MyCommand.Path -parent)\..\..\..\..\tools\generateTool.ps1" -ResourceProvider "botservice\resource-manager" -PowershellInvoker -AutoRestVersion "latest"
