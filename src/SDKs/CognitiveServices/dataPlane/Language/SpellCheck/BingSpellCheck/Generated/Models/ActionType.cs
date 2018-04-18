1dae966e9cfbd5a11d1af9ed0dc87ddbc585b80e
ashwin kumar <ashku@microsoft.com>
2018-04-18 14:41:09 -0700

    regenerate using latest autorest

--- src/SDKs/CognitiveServices/dataPlane/Language/SpellCheck/BingSpellCheck/Generated/Models/ActionType.cs
+++ src/SDKs/CognitiveServices/dataPlane/Language/SpellCheck/BingSpellCheck/Generated/Models/ActionType.cs
@@ -3,6 +3,7 @@
 // Changes may cause incorrect behavior and will be lost if the code is
 // regenerated.
 // </auto-generated>
+
 namespace Microsoft.Azure.CognitiveServices.Language.SpellCheck.Models
 {
 
