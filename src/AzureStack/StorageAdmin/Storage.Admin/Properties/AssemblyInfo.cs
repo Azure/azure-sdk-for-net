bd2dcfd11a22e0fff9129f27e45cc5a7edf8da3c
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-04-20 12:55:00 -0700

    Fixed spacing, fixed version issues

--- src/AzureStack/StorageAdmin/Storage.Admin/Properties/AssemblyInfo.cs
+++ src/AzureStack/StorageAdmin/Storage.Admin/Properties/AssemblyInfo.cs
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5f30ad46-8f37-4416-8eae-5b56a1471926")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
