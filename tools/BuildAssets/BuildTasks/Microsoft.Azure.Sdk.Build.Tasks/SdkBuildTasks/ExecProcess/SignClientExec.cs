b1a9bf9f7479bac0fba0ab27cfd96bd689b6a476
Abhijeet Shah <abhishah@microsoft.com>
2018-05-18 18:11:00 -0700

    increasing timeout between packaging and signing

--- tools/BuildAssets/BuildTasks/Microsoft.Azure.Sdk.Build.Tasks/SdkBuildTasks/ExecProcess/SignClientExec.cs
+++ tools/BuildAssets/BuildTasks/Microsoft.Azure.Sdk.Build.Tasks/SdkBuildTasks/ExecProcess/SignClientExec.cs
@@ -96,7 +96,7 @@ namespace Microsoft.Azure.Sdk.Build.ExecProcess
         public override int ExecuteCommand()
         {
             VerifyRequiredProperties();
-            Thread.Sleep(15000);
+            Thread.Sleep(30000);
             int exitCode = ExecuteCommand(BuildShellProcessArgs());
             return exitCode;
         }
