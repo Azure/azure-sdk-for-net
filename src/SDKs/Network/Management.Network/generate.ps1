975b1086a2a4131156e379da5c309e0d35511980
dsgouda <deshank@microsoft.com>
2018-05-30 09:46:51 -0700

    Updating generate.ps1 files to consume powershell cmdlets

--- src/SDKs/Network/Management.Network/generate.ps1
+++ src/SDKs/Network/Management.Network/generate.ps1
@@ -1,1 +1,1 @@
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "$(split-path $SCRIPT:MyInvocation.MyCommand.Path -parent)\..\..\..\..\tools\generateTool.ps1" -ResourceProvider "network/resource-manager" -PowershellInvoker -AutoRestVersion "latest"
+Start-AutoRestCodeGeneration -ResourceProvider "network/resource-manager" -AutoRestVersion "latest"
