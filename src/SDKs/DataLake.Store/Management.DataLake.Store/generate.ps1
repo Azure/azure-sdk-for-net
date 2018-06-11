0a4fd78313a152372c28c17297399aff9f5c61ee
Ray Wu <lewu@microsoft.com>
2018-06-11 15:43:31 -0700

    [ADLS] modify generate.ps1

--- src/SDKs/DataLake.Store/Management.DataLake.Store/generate.ps1
+++ src/SDKs/DataLake.Store/Management.DataLake.Store/generate.ps1
@@ -1,1 +1,1 @@
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "$(split-path $SCRIPT:MyInvocation.MyCommand.Path -parent)\..\..\..\..\tools\generateTool.ps1" -ResourceProvider "datalake-store/resource-manager" -PowershellInvoker  -AutoRestVersion "latest"
+Start-AutoRestCodeGeneration -ResourceProvider "datalake-store/resource-manager" -AutoRestVersion "latest"
