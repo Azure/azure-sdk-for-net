02dd89fb4964d6e07b328229ce70be087bafaa3c
Deepak Shankargouda <deshank@microsoft.com>
2018-06-11 12:22:30 -0700

    Modifying generate.ps1 to use the Start-AutoRestGeneration cmdlet  (#4420)
    * Modifying generate.ps1 to use the Start-AutoRestGeneration cmdlet
    
    * Delete generate.cmds
    
    * retaining legacy custom scripts

--- src/SDKs/TrafficManager/Management.TrafficManager/generate.ps1
+++ src/SDKs/TrafficManager/Management.TrafficManager/generate.ps1
@@ -1,1 +1,1 @@
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "$(split-path $SCRIPT:MyInvocation.MyCommand.Path -parent)\..\..\..\..\tools\generateTool.ps1" -ResourceProvider "trafficmanager/resource-manager" -PowershellInvoker  -AutoRestVersion "latest"
+Start-AutoRestCodeGeneration -ResourceProvider "trafficmanager/resource-manager" -AutoRestVersion "latest"
