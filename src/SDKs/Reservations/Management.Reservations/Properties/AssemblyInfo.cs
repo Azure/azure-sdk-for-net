cf65c6733aaebf633b9f2f977348618c753d5295
REDMOND\juhle <juhle@microsoft.com>
2018-06-13 14:11:47 -0700

    reverting AssemblyVersion to 1.0.0.0

--- src/SDKs/Reservations/Management.Reservations/Properties/AssemblyInfo.cs
+++ src/SDKs/Reservations/Management.Reservations/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure Reserved Instance Management Library")]
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure Reserved Instance Resources.")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
