e2c665067e22028c58d7c66e81123c72d8f86173
Abhijeet Shah <abhishah@microsoft.com>
2018-06-21 18:13:02 -0700

    Bumping publish version. Cleaning commented code

--- src/SdkCommon/TestFramework/HttpRecorder.Tests/BinaryDataMockServerTests.cs
+++ src/SdkCommon/TestFramework/HttpRecorder.Tests/BinaryDataMockServerTests.cs
@@ -1,16 +1,16 @@
-﻿using HttpRecorder.Tests;
-using HttpRecorder.Tests.DelegatingHandlers;
-using Microsoft.Azure.Test.HttpRecorder;
-using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Http;
-using System.Text;
-using Xunit;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
 
 namespace HttpRecorder.BinaryData.Tests
 {
+    using HttpRecorder.Tests;
+    using HttpRecorder.Tests.DelegatingHandlers;
+    using Microsoft.Azure.Test.HttpRecorder;
+    using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+    using System.IO;
+    using System.Net.Http;
+    using Xunit;
+
     public class BinaryDataMockServerTests : BaseMockServerTest
     {
         [Fact]
