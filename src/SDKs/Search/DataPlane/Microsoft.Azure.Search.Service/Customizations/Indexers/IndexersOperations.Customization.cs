41baacc49f29fddfe7551f45aa31877e45b309b9
Bruce Johnston <bruce.johnston@microsoft.com>
2018-07-03 15:31:48 -0700

    [Azure Search] Adding missing documentation to custom code

--- src/SDKs/Search/DataPlane/Microsoft.Azure.Search.Service/Customizations/Indexers/IndexersOperations.Customization.cs
+++ src/SDKs/Search/DataPlane/Microsoft.Azure.Search.Service/Customizations/Indexers/IndexersOperations.Customization.cs
@@ -13,24 +13,40 @@ namespace Microsoft.Azure.Search
     internal partial class IndexersOperations
     {
         /// <summary>
-        /// Creates a new Azure Search indexer or updates an indexer if it
-        /// already exists.
+        /// Creates a new Azure Search indexer or updates an indexer if it already
+        /// exists.
+        /// <see href="https://docs.microsoft.com/rest/api/searchservice/Create-Indexer" />
         /// </summary>
         /// <param name='indexer'>
         /// The definition of the indexer to create or update.
         /// </param>
         /// <param name='searchRequestOptions'>
-        /// Additional parameters for the operation
+        /// Additional parameters for the operation.
         /// </param>
         /// <param name='accessCondition'>
-        /// Additional parameters for the operation
+        /// Additional parameters for the operation.
         /// </param>
         /// <param name='customHeaders'>
-        /// The headers that will be added to request.
+        /// Headers that will be added to request.
         /// </param>
         /// <param name='cancellationToken'>
         /// The cancellation token.
         /// </param>
+        /// <exception cref="CloudException">
+        /// Thrown when the operation returned an invalid status code.
+        /// </exception>
+        /// <exception cref="Microsoft.Rest.SerializationException">
+        /// Thrown when unable to deserialize the response.
+        /// </exception>
+        /// <exception cref="Microsoft.Rest.ValidationException">
+        /// Thrown when a required parameter is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when a required parameter is null.
+        /// </exception>
+        /// <return>
+        /// A response object containing the response body and response headers.
+        /// </return>
         public Task<AzureOperationResponse<Indexer>> CreateOrUpdateWithHttpMessagesAsync(Indexer indexer, SearchRequestOptions searchRequestOptions = default(SearchRequestOptions), AccessCondition accessCondition = default(AccessCondition), Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken))
         {
             return CreateOrUpdateWithHttpMessagesAsync(indexer?.Name, indexer, searchRequestOptions, accessCondition, customHeaders, cancellationToken);
@@ -43,7 +59,7 @@ namespace Microsoft.Azure.Search
         /// The name of the indexer.
         /// </param>
         /// <param name='searchRequestOptions'>
-        /// Additional parameters for the operation
+        /// Additional parameters for the operation.
         /// </param>
         /// <param name='customHeaders'>
         /// The headers that will be added to request.
@@ -51,6 +67,18 @@ namespace Microsoft.Azure.Search
         /// <param name='cancellationToken'>
         /// The cancellation token.
         /// </param>
+        /// <exception cref="CloudException">
+        /// Thrown when the operation returned an invalid status code.
+        /// </exception>
+        /// <exception cref="Microsoft.Rest.SerializationException">
+        /// Thrown when unable to deserialize the response.
+        /// </exception>
+        /// <exception cref="Microsoft.Rest.ValidationException">
+        /// Thrown when a required parameter is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when a required parameter is null.
+        /// </exception>
         /// <returns>
         /// A response with the value <c>true</c> if the indexer exists; <c>false</c> otherwise.
         /// </returns>
