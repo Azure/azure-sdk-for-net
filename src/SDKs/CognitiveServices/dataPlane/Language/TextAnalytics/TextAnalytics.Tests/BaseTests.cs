94bdfc8b648da5c05cf3772e1cfb7ef390347264
Yuan Yang <yuanyang@microsoft.com>
2018-07-26 12:24:34 -0700

    Update Text Analytics C# SDK auto-generated code.

--- src/SDKs/CognitiveServices/dataPlane/Language/TextAnalytics/TextAnalytics.Tests/BaseTests.cs
+++ src/SDKs/CognitiveServices/dataPlane/Language/TextAnalytics/TextAnalytics.Tests/BaseTests.cs
@@ -7,11 +7,16 @@ namespace Language.Tests
     public abstract class BaseTests
     {
         public static bool IsTestTenant = false;
+        // BaseEndpoint only contains protocol and hostname
+        private static string BaseEndpoint = "https://westus.api.cognitive.microsoft.com";
         private static string SubscriptionKey = "000";
 
         protected ITextAnalyticsClient GetClient(DelegatingHandler handler)
         {
-            return new TextAnalyticsClient(new ApiKeyServiceClientCredentials(SubscriptionKey), handlers: handler);
+            return new TextAnalyticsClient(new ApiKeyServiceClientCredentials(SubscriptionKey), handlers: handler)
+            {
+                Endpoint = BaseEndpoint
+            };
         }
     }
 }
