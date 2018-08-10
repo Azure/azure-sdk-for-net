9cf47b91c43f844eace67f7cfa6f1d23542c39f4
Samir Solanki <samirsolanki@outlook.com>
2018-08-10 09:50:32 -0700

    Bug fix for CertificateInformation and add description properties to ApiContract

--- src/SDKs/ApiManagement/Management.ApiManagement/generate.ps1
+++ src/SDKs/ApiManagement/Management.ApiManagement/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "apimanagement/resource-manager" -AutoRestVersion "latest"
+Start-AutoRestCodeGeneration -ResourceProvider "apimanagement/resource-manager" -AutoRestVersion "latest" -AutoRestCodeGenerationFlags "--tag=package-2018-01"
