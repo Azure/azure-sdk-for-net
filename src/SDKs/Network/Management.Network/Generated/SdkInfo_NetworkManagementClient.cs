d888c139218c126d3fc7ef6e113679a674f6cc54
Jack Knudson <knuj@microsoft.com>
2018-09-13 22:29:59 -0700

    generate SDK from swagger update / network profiel return code + increment version to 19.5.0-preview

--- src/SDKs/Network/Management.Network/Generated/SdkInfo_NetworkManagementClient.cs
+++ src/SDKs/Network/Management.Network/Generated/SdkInfo_NetworkManagementClient.cs
@@ -93,10 +93,10 @@ namespace Microsoft.Azure.Management.Network
       // BEGIN: Code Generation Metadata Section
       public static readonly String AutoRestVersion = "latest";
       public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/network/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\Private\\azure-sdk-for-net\\src\\SDKs";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/network/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\Repos\\azure-sdk-for-net\\src\\SDKs";
       public static readonly String GithubForkName = "Azure";
       public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "42006c584234de5dff2c737db1101cbefe3073d8";
+      public static readonly String GithubCommidId = "4d1b5ac80425123cd271697fe46df2a0233ac3e6";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
