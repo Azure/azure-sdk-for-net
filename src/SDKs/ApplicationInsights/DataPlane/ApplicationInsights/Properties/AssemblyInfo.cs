a8b33e727fa7f65fb806cd6bafd08767ed78f50b
Ace Eldeib <aleldeib@microsoft.com>
2018-09-25 15:15:47 -0700

    revert version for new package

--- src/SDKs/ApplicationInsights/DataPlane/ApplicationInsights/Properties/AssemblyInfo.cs
+++ src/SDKs/ApplicationInsights/DataPlane/ApplicationInsights/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Microsoft Azure Application Insights Data Library")]
 [assembly: AssemblyDescription("Provides Microsoft Azure Application Insights data plane query operations operations.")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Microsoft Azure .NET SDK")]
