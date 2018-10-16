946e95cedf5d4eb279e4b054e38d3828e25385ae
maddieclayton <maclayto@microsoft.com>
2018-10-16 13:34:09 -0700

    versioning

--- src/SDKs/DataMigration/Management.DataMigration/Properties/AssemblyInfo.cs
+++ src/SDKs/DataMigration/Management.DataMigration/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure Data Migration Resources.")]
 
 [assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.2.0")]
+[assembly: AssemblyFileVersion("0.3.1")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Microsoft Azure .NET SDK")]
