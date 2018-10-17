a0bc81c9d9693e27d4c1ffa2e2412075775d5422
Deepak Shankargouda <deshank@microsoft.com>
2018-10-17 12:33:30 -0700

    Fixing mono builds  (#4874)
    * Fixing mono builds
    
    * some improvements
    
    * feedback
    
    * Update version and PackageReleaseNotes
    
    * undoing csproj

--- src/SdkCommon/ClientRuntime/ClientRuntime/ServiceClient.cs
+++ src/SdkCommon/ClientRuntime/ClientRuntime/ServiceClient.cs
@@ -55,6 +55,24 @@ namespace Microsoft.Rest
         /// </summary>
         private string _fxVersion;
 
+        private bool IsMonoFramework
+        {
+            get {
+                bool result = false;
+#if FullNetFx
+                switch (Environment.OSVersion.Platform)
+                {
+                    case PlatformID.Unix:
+                    case PlatformID.MacOSX:
+                    case PlatformID.Xbox:
+                        result = true;
+                        break;
+                }
+#endif
+                return result;
+            }
+        }
+
         #region Full Net Fx specific code
 #if FullNetFx
         /// <summary>
@@ -113,16 +131,13 @@ namespace Microsoft.Rest
         /// <returns>Value for provided HKLM key</returns>
         private string ReadHKLMRegistry(string path, string key)
         {
-            switch (Environment.OSVersion.Platform)
+           try
             {
-                case PlatformID.Unix:
-                case PlatformID.MacOSX:
-                case PlatformID.Xbox:
+                if (IsMonoFramework)
+                {
                     return string.Empty;
-            }
+                }
 
-            try
-            {
                 using (RegistryKey rk = Registry.LocalMachine.OpenSubKey(path))
                 {
                     if (rk == null) return "";
@@ -146,8 +161,19 @@ namespace Microsoft.Rest
                     _defaultUserAgentInfoList = new List<ProductInfoHeaderValue>();
                     _defaultUserAgentInfoList.Add(new ProductInfoHeaderValue(FXVERSION, FrameworkVersion));
 #if FullNetFx
-                    _defaultUserAgentInfoList.Add(new ProductInfoHeaderValue(OSNAME, OsName));
-                    _defaultUserAgentInfoList.Add(new ProductInfoHeaderValue(OSVERSION, OsVersion));
+                    if (!IsMonoFramework)
+                    {
+                        _defaultUserAgentInfoList.Add(new ProductInfoHeaderValue(OSNAME, OsName));
+                        _defaultUserAgentInfoList.Add(new ProductInfoHeaderValue(OSVERSION, OsVersion));
+                    }
+                    else
+                    {
+                        string monoOsName = Environment.OSVersion.Platform.ToString();
+                        if (!string.IsNullOrWhiteSpace(monoOsName))
+                        {
+                            _defaultUserAgentInfoList.Add(new ProductInfoHeaderValue(OSNAME, monoOsName));
+                        }   
+                    }
 #endif
                 }
 
