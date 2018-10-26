ee3a3c2a38c973c63f5e89faca0ce3a8d9bf23f8
Maddie Clayton <maclayto@microsoft.com>
2018-10-26 14:24:48 -0700

    [Newtonsoft] DeviceProvisioningServices update (#4945)
    * versioning
    
    * update common targets
    
    * versions

--- src/SDKs/DeviceProvisioningServices/Management.DeviceProvisioningServices/Properties/AssemblyInfo.cs
+++ src/SDKs/DeviceProvisioningServices/Management.DeviceProvisioningServices/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.10.0.0")]
+[assembly: AssemblyFileVersion("0.10.0.0")]
