ecf5adf10556cb80bdcdb11fdac2e4f498ab7adc
Maddie Clayton <maclayto@microsoft.com>
2018-10-26 16:06:52 -0700

    [Newtonsoft] StreamAnalytics update (#4957)
    * versioning
    
    * versioning
    
    * update versions

--- src/SDKs/StreamAnalytics/Management.StreamAnalytics/Properties/AssemblyInfo.cs
+++ src/SDKs/StreamAnalytics/Management.StreamAnalytics/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides Microsoft Azure Stream Analytics management operations.")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Azure .NET SDK")]
