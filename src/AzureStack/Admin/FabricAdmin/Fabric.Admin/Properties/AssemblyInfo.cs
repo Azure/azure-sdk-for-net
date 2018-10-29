8619a4e56960fd691d46816e6508a542cd565ced
zyxyoshine <zyx.pulsars@gmail.com>
2018-10-29 17:31:40 +0800

    Update version number

--- src/AzureStack/Admin/FabricAdmin/Fabric.Admin/Properties/AssemblyInfo.cs
+++ src/AzureStack/Admin/FabricAdmin/Fabric.Admin/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
