f6b6b8ad7b4b4b511654a0bfc4fe32a4e03a74c7
maddieclayton <maclayto@microsoft.com>
2018-10-30 14:11:35 -0700

    bump version

--- src/SDKs/EventGrid/management/Management.EventGrid/Properties/AssemblyInfo.cs
+++ src/SDKs/EventGrid/management/Management.EventGrid/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides developers with a library to create and manage all Azure EventGrid resources.")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
