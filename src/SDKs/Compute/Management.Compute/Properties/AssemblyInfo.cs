7628838a490de3252cf444f1665a18f09b5e11ac
Hyonho Lee <Hyonho.Lee@microsoft.com>
2018-10-30 12:40:55 -0700

    Update for Compute client library version 23.2.0

--- src/SDKs/Compute/Management.Compute/Properties/AssemblyInfo.cs
+++ src/SDKs/Compute/Management.Compute/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure Compute Resources.")]
 
 [assembly: AssemblyVersion("23.0.0.0")]
-[assembly: AssemblyFileVersion("23.1.0.0")]
+[assembly: AssemblyFileVersion("23.2.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Microsoft Azure .NET SDK")]
