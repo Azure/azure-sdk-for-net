8b9f3a23becd7fda6cad9e468c02063253eeb3ab
zyxyoshine <zyx.pulsars@gmail.com>
2018-11-09 02:55:57 +0800

    [AzureStack] Update Fabric Admin version to 2018-10-01 (#4967)

--- src/AzureStack/Admin/FabricAdmin/Fabric.Admin/Properties/AssemblyInfo.cs
+++ src/AzureStack/Admin/FabricAdmin/Fabric.Admin/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
