e685ba8b031f0474dca98369f1d67b09663ba36a
Jarrod Skulavik <jarrod.skulavik@microsoft.com>
2018-12-12 16:39:11 -0700

    Removal of preview objects and replacment of of objects from 2018-03-31 specification

---
+++ src/SDKs/ContainerService/ContainerService.Tests/Tests/Helpers/ContainerServiceTestUtilities.cs
@@ -0,0 +1,35 @@
+﻿using ContainerService.Tests;
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+
+namespace Microsoft.Azure.Management.ContainerService.Tests
+{
+    public class ContainerServiceTestUtilities
+    {
+
+        public static ResourceManagementClient GetResourceManagementClient(MockContext context, RecordedDelegatingHandler handler)
+        {
+            handler.IsPassThrough = true;
+            var client = context.GetServiceClient<ResourceManagementClient>(handlers: handler);
+            return client;
+        }
+
+        internal static ContainerServiceClient GetContainerServiceManagementClient(MockContext context, RecordedDelegatingHandler handler)
+        {
+            handler.IsPassThrough = true;
+            var client = context.GetServiceClient<ContainerServiceClient>(handlers: handler);
+            return client;
+        }
+
+        public static ResourceGroup CreateResourceGroup(ResourceManagementClient client)
+        {
+            var resourceGroupName = TestUtilities.GenerateName("aks_rg");
+
+            return client.ResourceGroups.CreateOrUpdate(resourceGroupName, new ResourceGroup
+            {
+                Location = "westus"
+            });
+        }
+    }
+}
