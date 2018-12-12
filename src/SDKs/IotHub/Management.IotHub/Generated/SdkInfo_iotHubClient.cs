24372991daafcd03d3b908caee1ebf18960aeaba
Sapan Saxena <v-sapsax@microsoft.com>
2018-12-12 13:24:24 -0800

    Updating API version to 2018-12-01-preview for Device streaming

--- src/SDKs/IotHub/Management.IotHub/Generated/SdkInfo_iotHubClient.cs
+++ src/SDKs/IotHub/Management.IotHub/Generated/SdkInfo_iotHubClient.cs
@@ -19,12 +19,23 @@ namespace Microsoft.Azure.Management.IotHub
           {
               return new Tuple<string, string, string>[]
               {
-                new Tuple<string, string, string>("Devices", "Certificates", "2018-04-01"),
-                new Tuple<string, string, string>("Devices", "IotHubResource", "2018-04-01"),
-                new Tuple<string, string, string>("Devices", "Operations", "2018-04-01"),
-                new Tuple<string, string, string>("Devices", "ResourceProviderCommon", "2018-04-01"),
+                new Tuple<string, string, string>("Devices", "Certificates", "2018-12-01-preview"),
+                new Tuple<string, string, string>("Devices", "IotHubResource", "2018-12-01-preview"),
+                new Tuple<string, string, string>("Devices", "Operations", "2018-12-01-preview"),
+                new Tuple<string, string, string>("Devices", "ResourceProviderCommon", "2018-12-01-preview"),
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "latest";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/iothub/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=D:\\azure-sdk-for-net\\src\\SDKs";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "8751b9ca1071b51f56d823e6d121ad54b9c2ccae";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
