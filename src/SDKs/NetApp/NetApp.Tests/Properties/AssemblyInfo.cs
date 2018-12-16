8140c6f7b2315b638f9e2150d77960291a2a803c
leonard <leonard.francis@netapp.com>
2018-12-16 10:57:37 +0000

    NFSAAS-1504 NetApp SDK initial version

---
+++ src/SDKs/NetApp/NetApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetApp.Tests")]
+[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetApp.Tests")]
+[assembly: AssemblyCopyright("${AuthorCopyright}")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
