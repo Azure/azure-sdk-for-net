5ca40aa86b06174c1e2e93c3718ee0616d52a245
leonard <leonard.francis@netapp.com>
2018-12-16 10:57:37 +0000

    NFSAAS-1504 NetApp SDK initial version

---
+++ src/SDKs/NetApp/Management.NetApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Microsoft Azure NetApp Management Library")]
+[assembly: AssemblyDescription("Provides management functionality for Azure NetApp Storage.")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Azure .NET SDK")]
+[assembly: AssemblyCopyright("${AuthorCopyright}")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
