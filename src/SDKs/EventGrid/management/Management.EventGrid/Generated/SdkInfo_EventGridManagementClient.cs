d0ab85a2f664cc0a5367c9d2934862a4676b0e0f
Ashraf Hamad <ahamad@ntdev.microsoft.com>
2018-12-18 18:11:40 -0800

    Address Code Review comments: Using generate.ps1, and update related meta data files and revert asymblyinfo.cs changes for test.

--- src/SDKs/EventGrid/management/Management.EventGrid/Generated/SdkInfo_EventGridManagementClient.cs
+++ src/SDKs/EventGrid/management/Management.EventGrid/Generated/SdkInfo_EventGridManagementClient.cs
@@ -26,5 +26,16 @@ namespace Microsoft.Azure.Management.EventGrid
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "latest";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/eventgrid/management/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\GA19\\SubmitSdk100\\azure-sdk-for-net\\src\\SDKs";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "60ba6c3597808284eb834fb432a0b20e9fc8c837";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
