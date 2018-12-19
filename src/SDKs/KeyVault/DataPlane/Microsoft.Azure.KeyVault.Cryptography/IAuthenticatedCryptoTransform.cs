412376ec5484d5bf2a76368352e5d6a254c6c193
Wes Haggard <Wes.Haggard@microsoft.com>
2018-12-19 09:57:53 -0800

    Move KeyVault/dataPlane to KeyVault/DataPlane to have correct case

---
+++ src/SDKs/KeyVault/DataPlane/Microsoft.Azure.KeyVault.Cryptography/IAuthenticatedCryptoTransform.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using System.Security.Cryptography;
+
+namespace Microsoft.Azure.KeyVault.Cryptography
+{
+    public interface IAuthenticatedCryptoTransform : ICryptoTransform
+    {
+        byte[] Tag { get; }
+    }
+}
