ce6bf5ef707aba8885d531de87f8b2dc2be222d9
Jarrod Skulavik <jarrod.skulavik@microsoft.com>
2018-12-20 17:32:58 -0700

    Orchestrator list and create tests complete:

--- src/SDKs/ContainerService/ContainerService.Tests/Tests/Helpers/ContainerServiceTestUtilities.cs
+++ src/SDKs/ContainerService/ContainerService.Tests/Tests/Helpers/ContainerServiceTestUtilities.cs
@@ -1,35 +1,58 @@
-﻿using ContainerService.Tests;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using ContainerService.Tests;
 using Microsoft.Azure.Management.ResourceManager;
 using Microsoft.Azure.Management.ResourceManager.Models;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 
 namespace Microsoft.Azure.Management.ContainerService.Tests
 {
-    public class ContainerServiceTestUtilities
+    public static class ContainerServiceTestUtilities
     {
+        internal const string DnsPrefix = "aksdotnetsdk";
+        internal const string ResourceGroupPrefix = "aks-dotnet-sdk-RG-";
+        internal const string AgentPoolProfileName = "aksdotnetagent";
+        internal const string VMSize = "Standard_A1";
+        internal const string AKSDotnetDevSpId = "08894bbb-66d9-4ca5-98db-9603a984f257";
 
         public static ResourceManagementClient GetResourceManagementClient(MockContext context, RecordedDelegatingHandler handler)
         {
             handler.IsPassThrough = true;
-            var client = context.GetServiceClient<ResourceManagementClient>(handlers: handler);
-            return client;
+            ResourceManagementClient resourceManagementClient = context.GetServiceClient<ResourceManagementClient>(handlers: handler);
+            return resourceManagementClient;
         }
 
         internal static ContainerServiceClient GetContainerServiceManagementClient(MockContext context, RecordedDelegatingHandler handler)
         {
             handler.IsPassThrough = true;
-            var client = context.GetServiceClient<ContainerServiceClient>(handlers: handler);
-            return client;
+            ContainerServiceClient containerServiceClient = context.GetServiceClient<ContainerServiceClient>(handlers: handler);
+            return containerServiceClient;
         }
 
-        public static ResourceGroup CreateResourceGroup(ResourceManagementClient client)
+        public static string GetLocationFromProvider(this ResourceManagementClient resourceManagementClient)
         {
-            var resourceGroupName = TestUtilities.GenerateName("aks_rg");
+            return "westus2";
+        }
 
-            return client.ResourceGroups.CreateOrUpdate(resourceGroupName, new ResourceGroup
-            {
-                Location = "westus"
-            });
+        public static void TryRegisterResourceGroup(this ResourceManagementClient resourceManagementClient, string location, string resourceGroupName)
+        {
+            resourceManagementClient.ResourceGroups.CreateOrUpdate(resourceGroupName, new ResourceGroup(location));
+        }
+
+        public static string TryGetResourceGroup(this ResourceManagementClient resourceManagementClient, string location)
+        {
+            const string DefaultResourceGroupName = "SDKTests";
+
+            var resourceGroup = resourceManagementClient.ResourceGroups
+                    .List().Where(group => string.IsNullOrWhiteSpace(location) || group.Location.Equals(location.Replace(" ", string.Empty), StringComparison.OrdinalIgnoreCase))
+                    .FirstOrDefault(group => group.Name.Contains(DefaultResourceGroupName));
+
+            return resourceGroup != null
+                ? resourceGroup.Name
+                : string.Empty;
         }
     }
 }
---
+++ src/SDKs/ContainerService/ContainerService.Tests/Tests/Tests/ContainerServiceOpsTests.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using ContainerService.Tests;
+using Microsoft.Azure.Management.ContainerService;
+using Microsoft.Azure.Management.ContainerService.Models;
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Xunit;
+
+namespace Microsoft.Azure.Management.ContainerService.Tests
+{
+    public partial class ContainerServiceTests : TestBase
+    {
+        /// <summary>
+        /// Test the listing of container orchestrators.
+        /// </summary>
+        [Fact]
+        public async Task ContainerServiceListOrchestratorsTest()
+        {
+            using (MockContext context = MockContext.Start(GetType().FullName))
+            {
+                InitializeClients(context);
+
+                var location = ContainerServiceTestUtilities.GetLocationFromProvider(ResourceManagementClient);
+
+                var resourceGroup = ResourceManagementClient.TryGetResourceGroup(location);
+                if (string.IsNullOrWhiteSpace(resourceGroup))
+                {
+                    resourceGroup = TestUtilities.GenerateName(ContainerServiceTestUtilities.ResourceGroupPrefix);
+                    ResourceManagementClient.TryRegisterResourceGroup(location, resourceGroup);
+                }
+
+                var orchestratorsListResult = await ContainerServiceClient.ContainerServices.ListOrchestratorsAsync(location);
+                
+                Assert.NotNull(orchestratorsListResult);
+                Assert.True(orchestratorsListResult.Orchestrators.Count > 0);
+            }
+        }
+
+        /// <summary>
+        /// Test the creation of a managed cluster.
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task ContainerServiceCreateManagedServiceTest()
+        {
+            using (MockContext context = MockContext.Start(GetType().FullName))
+            {
+                InitializeClients(context);
+
+                var location = ContainerServiceTestUtilities.GetLocationFromProvider(ResourceManagementClient);
+
+                var resourceGroup = ResourceManagementClient.TryGetResourceGroup(location);
+                if (string.IsNullOrWhiteSpace(resourceGroup))
+                {
+                    resourceGroup = TestUtilities.GenerateName(ContainerServiceTestUtilities.ResourceGroupPrefix);
+                    ResourceManagementClient.TryRegisterResourceGroup(location, resourceGroup);
+                }
+
+                var servicePrincipalProfile = new ManagedClusterServicePrincipalProfile
+                {
+                    ClientId = ContainerServiceTestUtilities.AKSDotnetDevSpId,
+                    Secret = Environment.GetEnvironmentVariable("AKS_DEV_SP_SECRET")
+                };
+
+                var agentPoolProfiles = new List<ManagedClusterAgentPoolProfile>
+                {
+                    new ManagedClusterAgentPoolProfile
+                    {
+                        Name = ContainerServiceTestUtilities.AgentPoolProfileName,
+                        VmSize = ContainerServiceTestUtilities.VMSize,
+                        Count = 1
+                    }
+                };
+               
+                ManagedCluster desiredManagedCluster = new ManagedCluster
+                {
+                    DnsPrefix = ContainerServiceTestUtilities.DnsPrefix,
+                    Location = location,
+                    AgentPoolProfiles = agentPoolProfiles,
+                    ServicePrincipalProfile = servicePrincipalProfile
+                };
+
+                string clusterName = TestUtilities.GenerateName();
+                ManagedCluster managedClusterResult = await ContainerServiceClient.ManagedClusters.CreateOrUpdateAsync(resourceGroup, clusterName, desiredManagedCluster);
+
+                Assert.NotNull(managedClusterResult);
+                Assert.Equal(clusterName, managedClusterResult.Name);
+                Assert.Equal(ContainerServiceTestUtilities.DnsPrefix, managedClusterResult.DnsPrefix);
+            }
+        }
+    }
+}
