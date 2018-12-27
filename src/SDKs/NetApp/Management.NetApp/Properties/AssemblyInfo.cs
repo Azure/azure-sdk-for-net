542c7641989237e3f11b03c21b1ca385758b26c3
leonard <leonard.francis@netapp.com>
2018-12-27 16:43:24 +0000

    NFSAAS-1504 NetApp SDK Initial Version
    With review corrections.

--- src/SDKs/NetApp/Management.NetApp/Properties/AssemblyInfo.cs
+++ src/SDKs/NetApp/Management.NetApp/Properties/AssemblyInfo.cs
@@ -1,9 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Microsoft Azure NetApp Management Library")]
 [assembly: AssemblyDescription("Provides management functionality for Azure NetApp Storage.")]
+
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -13,5 +19,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
