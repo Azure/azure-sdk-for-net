9cf96facaf6ca08f84bb1a918a39b54e424ac31d
alexghiondea <ghiondea.alexandru@microsoft.com>
2019-02-08 10:31:03 -0800

    Update the copyright headers for the files.

--- src/SDKs/Azure.Core/data-plane/Azure.Core/Net/Pipeline/PipelinePolicy.cs
+++ src/SDKs/Azure.Core/data-plane/Azure.Core/Net/Pipeline/PipelinePolicy.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for
-// license information.
+// Licensed under the MIT License.
 
 using Azure.Core.Diagnostics;
 using System;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Azure.Core.Http.Pipeline
--- src/SDKs/Azure.Core/data-plane/Azure.Core/ResponseOfT.cs
+++ src/SDKs/Azure.Core/data-plane/Azure.Core/ResponseOfT.cs
@@ -1,12 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for
-// license information.
+// Licensed under the MIT License.
 
-using Azure.Core.Http;
 using System;
-using System.Buffers.Text;
 using System.ComponentModel;
-using System.IO;
 using System.Text;
 
 namespace Azure.Core
