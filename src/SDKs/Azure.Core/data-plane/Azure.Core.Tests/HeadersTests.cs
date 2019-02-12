d87a5267464c30d1402c79a926e47fb83362b84f
alexghiondea <ghiondea.alexandru@microsoft.com>
2019-02-11 23:56:02 -0800

    Remove and sort usings

--- src/SDKs/Azure.Core/data-plane/Azure.Core.Tests/HeadersTests.cs
+++ src/SDKs/Azure.Core/data-plane/Azure.Core.Tests/HeadersTests.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+using Azure.Core.Http;
 using System.Text;
 using System.Text.RegularExpressions;
-using Azure.Core.Http;
 using Xunit;
 using static System.Buffers.Text.Encodings;
 
