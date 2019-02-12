b6b1a9f115992c92e78eef5ae4229511ca9c5c8a
stefanb995 <bogdanovicstefan995@gmail.com>
2019-02-13 00:42:35 +0100

    .NET client update (#5197)
    * .NET client update
    
    *Adding two new parameters to Managed Instance (ProxyOverride and PublicDataEndpointEnabled)
    
    * Delete build.err
    
    * Delete build.wrn
    
    * .NET client update
    
    * Update ManagedInstanceCrudScenarioTests.cs
    
    Updated test to include parameters ProxyOverride and PublicDataEndpointEnabled
    
    * Delete sql_resource-manager.txt
    
    * .NET sdk
    
    SDK ganerated with option -AutoRestVersion "latest" on master azure-rest- api-specs
    
    autorest version: 2.0.4283
    
    * Update sql_resource-manager.txt
    
    * Update Microsoft.Azure.Management.Sql.csproj
    
    * Update of session record for updated test
    
    * Update TestCreateUpdateGetDropManagedInstance.json

--- src/SDKs/SqlManagement/Management.Sql/Generated/Models/ManagedInstance.cs
+++ src/SDKs/SqlManagement/Management.Sql/Generated/Models/ManagedInstance.cs
@@ -63,7 +63,12 @@ namespace Microsoft.Azure.Management.Sql.Models
         /// <param name="dnsZonePartner">The resource id of another managed
         /// instance whose DNS zone this managed instance will share after
         /// creation.</param>
-        public ManagedInstance(string location, string id = default(string), string name = default(string), string type = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), ResourceIdentity identity = default(ResourceIdentity), Sku sku = default(Sku), string fullyQualifiedDomainName = default(string), string administratorLogin = default(string), string administratorLoginPassword = default(string), string subnetId = default(string), string state = default(string), string licenseType = default(string), int? vCores = default(int?), int? storageSizeInGB = default(int?), string collation = default(string), string dnsZone = default(string), string dnsZonePartner = default(string))
+        /// <param name="publicDataEndpointEnabled">Whether or not the public
+        /// data endpoint is enabled.</param>
+        /// <param name="proxyOverride">Connection type used for connecting to
+        /// the instance. Possible values include: 'Proxy', 'Redirect',
+        /// 'Default'</param>
+        public ManagedInstance(string location, string id = default(string), string name = default(string), string type = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), ResourceIdentity identity = default(ResourceIdentity), Sku sku = default(Sku), string fullyQualifiedDomainName = default(string), string administratorLogin = default(string), string administratorLoginPassword = default(string), string subnetId = default(string), string state = default(string), string licenseType = default(string), int? vCores = default(int?), int? storageSizeInGB = default(int?), string collation = default(string), string dnsZone = default(string), string dnsZonePartner = default(string), bool? publicDataEndpointEnabled = default(bool?), string proxyOverride = default(string))
             : base(location, id, name, type, tags)
         {
             Identity = identity;
@@ -79,6 +84,8 @@ namespace Microsoft.Azure.Management.Sql.Models
             Collation = collation;
             DnsZone = dnsZone;
             DnsZonePartner = dnsZonePartner;
+            PublicDataEndpointEnabled = publicDataEndpointEnabled;
+            ProxyOverride = proxyOverride;
             CustomInit();
         }
 
@@ -170,6 +177,19 @@ namespace Microsoft.Azure.Management.Sql.Models
         /// </summary>
         [JsonProperty(PropertyName = "properties.dnsZonePartner")]
         public string DnsZonePartner { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether or not the public data endpoint is enabled.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.publicDataEndpointEnabled")]
+        public bool? PublicDataEndpointEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets connection type used for connecting to the instance.
+        /// Possible values include: 'Proxy', 'Redirect', 'Default'
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.proxyOverride")]
+        public string ProxyOverride { get; set; }
 
         /// <summary>
         /// Validate the object.
