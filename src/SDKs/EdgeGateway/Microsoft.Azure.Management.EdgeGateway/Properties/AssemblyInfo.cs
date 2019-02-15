73d8947ca1a0f07881857927b5f2ababe792da46
anponnet <anponnet@microsoft.com>
2019-02-16 00:41:28 +0530

    Fix PR comments - assemblyinfo, assemblyversion, unnecassary properties in proj files

--- src/SDKs/EdgeGateway/Microsoft.Azure.Management.EdgeGateway/Properties/AssemblyInfo.cs
+++ src/SDKs/EdgeGateway/Microsoft.Azure.Management.EdgeGateway/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure DataBox Edge Gateway Management Library")]
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure DataBox Edge Gateway Management Resources.")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
