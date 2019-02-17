1eff00573c0fa2d7558bccaf0352d47296fddaed
Bruce Johnston <bruce.johnston@microsoft.com>
2019-02-16 23:07:54 -0800

    Replacing custom implementation of Suggest with generated code

--- src/SDKs/Search/DataPlane/Search.Tests/Tests/Serialization/SuggestParametersTests.cs
+++ src/SDKs/Search/DataPlane/Search.Tests/Tests/Serialization/SuggestParametersTests.cs
@@ -4,7 +4,6 @@
 
 namespace Microsoft.Azure.Search.Tests
 {
-    using System;
     using Common;
     using Models;
     using Xunit;
@@ -12,79 +11,9 @@ namespace Microsoft.Azure.Search.Tests
     public sealed class SuggestParametersTests
     {
         [Fact]
-        public void AllOptionsUnsetGivesDefaultQueryString()
-        {
-            Assert.Equal("$select=*&fuzzy=false", new SuggestParameters().ToString());
-        }
-
-        [Fact]
-        public void AllOptionsPropagatedToQueryString()
-        {
-            var parameters =
-                new SuggestParameters()
-                {
-                    Filter = "field eq value",
-                    HighlightPreTag = "<b>",
-                    HighlightPostTag = "</b>",
-                    MinimumCoverage = 33.33,
-                    OrderBy = new[] { "field1 asc", "field2 desc" },
-                    SearchFields = new[] { "field1", "field2" },
-                    Select = new[] { "field1", "field2" },
-                    Top = 5,
-                    UseFuzzyMatching = true
-                };
-
-            const string ExpectedQueryString =
-                "$filter=field%20eq%20value&highlightPreTag=%3Cb%3E&highlightPostTag=%3C%2Fb%3E&" +
-                "minimumCoverage=33.33&$orderby=field1 asc,field2 desc&searchFields=field1,field2&" +
-                "$select=field1,field2&$top=5&fuzzy=true";
-
-            Assert.Equal(ExpectedQueryString, parameters.ToString());
-        }
-
-        [Fact]
-        public void SelectStarPropagatesToQueryString()
-        {
-            var parameters = new SuggestParameters() { Select = new[] { "*" } };
-            Assert.Equal("$select=*&fuzzy=false", parameters.ToString());
-        }
-
-        [Fact]
-        public void AllOpenStringParametersAreEscaped()
-        {
-            const string UnescapedString = "a+%=@#b";
-            const string EscapedString = "a%2B%25%3D%40%23b";
-
-            var parameters =
-                new SuggestParameters()
-                {
-                    Filter = UnescapedString,
-                    HighlightPreTag = UnescapedString,
-                    HighlightPostTag = UnescapedString
-                };
-
-            const string ExpectedQueryStringFormat =
-                "$filter={0}&highlightPreTag={0}&highlightPostTag={0}&$select=*&fuzzy=false";
-
-            Assert.Equal(String.Format(ExpectedQueryStringFormat, EscapedString), parameters.ToString());
-        }
-
-        [Fact]
         public void CanConvertToPostRequestPayload()
         {
-            var parameters =
-                new SuggestParameters()
-                {
-                    Filter = "x eq y",
-                    HighlightPostTag = "</em>",
-                    HighlightPreTag = "<em>",
-                    MinimumCoverage = 33.3,
-                    OrderBy = new[] { "a", "b desc" },
-                    SearchFields = new[] { "a", "b", "c" },
-                    Select = new[] { "e", "f", "g" },
-                    Top = 5,
-                    UseFuzzyMatching = true
-                };
+            SuggestParameters parameters = CreateTestParameters();
 
             SuggestRequest request = parameters.ToRequest("find me", "mySuggester");
 
@@ -122,5 +51,62 @@ namespace Microsoft.Azure.Search.Tests
             Assert.True(request.Fuzzy.HasValue);
             Assert.False(request.Fuzzy.Value);  // Fuzzy is non-nullable in the client-side contract.
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData()]
+        public void EnsureSelectConvertsNullOrEmptyToSelectStar(params string[] select)
+        {
+            SuggestParameters parameters = CreateTestParameters(select);
+
+            Assert.Same(select, parameters.Select);
+
+            SuggestParameters newParameters = parameters.EnsureSelect();
+
+            Assert.Collection(newParameters.Select, s => Assert.Equal("*", s));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData()]
+        public void EnsureSelectLeavesOtherPropertiesUnchanged(params string[] select)
+        {
+            SuggestParameters parameters = CreateTestParameters(select);
+            SuggestParameters newParameters = parameters.EnsureSelect();
+
+            Assert.Equal(parameters.Filter, newParameters.Filter);
+            Assert.Equal(parameters.HighlightPostTag, newParameters.HighlightPostTag);
+            Assert.Equal(parameters.HighlightPreTag, newParameters.HighlightPreTag);
+            Assert.Equal(parameters.MinimumCoverage, newParameters.MinimumCoverage);
+            Assert.Equal(parameters.OrderBy, newParameters.OrderBy);
+            Assert.Equal(parameters.SearchFields, newParameters.SearchFields);
+            Assert.Equal(parameters.Top, newParameters.Top);
+            Assert.Equal(parameters.UseFuzzyMatching, newParameters.UseFuzzyMatching);
+        }
+
+        [Fact]
+        public void EnsureSelectReturnsSelfWhenSelectIsPopulated()
+        {
+            SuggestParameters parameters = CreateTestParameters();
+            SuggestParameters newParameters = parameters.EnsureSelect();
+
+            Assert.Same(parameters, newParameters);
+        }
+
+        private static SuggestParameters CreateTestParameters() => CreateTestParameters(new[] { "e", "f", "g" });
+
+        private static SuggestParameters CreateTestParameters(string[] select) =>
+            new SuggestParameters()
+            {
+                Filter = "x eq y",
+                HighlightPostTag = "</em>",
+                HighlightPreTag = "<em>",
+                MinimumCoverage = 33.3,
+                OrderBy = new[] { "a", "b desc" },
+                SearchFields = new[] { "a", "b", "c" },
+                Select = select,
+                Top = 5,
+                UseFuzzyMatching = true
+            };
     }
 }
