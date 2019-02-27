335679a87b4f33630a1770499acf6e70271865b3
alexghiondea <ghiondea.alexandru@microsoft.com>
2019-02-27 11:18:56 -0800

    Changes after the latest changes in master.

--- src/SDKs/Azure.ApplicationModel.Configuration/data-plane/Azure.Configuration/ConfigurationClient.cs
+++ src/SDKs/Azure.ApplicationModel.Configuration/data-plane/Azure.Configuration/ConfigurationClient.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for
 // license information.
 
-using Azure.Base.Diagnostics;
+using Azure.Base;
 using Azure.Base.Http;
 using System;
 using System.Net.Http;
