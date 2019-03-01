846d202dee72e3762cad491a0db08faad7683f93
Bruce Johnston <bruce.johnston@microsoft.com>
2019-02-28 17:56:57 -0800

    [Azure Search] Regen against master spec

--- src/SDKs/Search/DataPlane/Microsoft.Azure.Search.Data/Generated/SdkInfo_SearchIndexClient.cs
+++ src/SDKs/Search/DataPlane/Microsoft.Azure.Search.Data/Generated/SdkInfo_SearchIndexClient.cs
@@ -26,10 +26,10 @@ namespace Microsoft.Azure.Search
       // BEGIN: Code Generation Metadata Section
       public static readonly String AutoRestVersion = "latest";
       public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/brjohnstmsft/azure-rest-api-specs/blob/generate-index/specification/search/data-plane/Microsoft.Azure.Search.Data/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\github\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "brjohnstmsft";
-      public static readonly String GithubBranchName = "generate-index";
-      public static readonly String GithubCommidId = "59c481dcf3e6b9afae31ed6c5a2b022241168c77";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/search/data-plane/Microsoft.Azure.Search.Data/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\github\\azure-sdk-for-net\\src\\SDKs";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "c8b825f5e12855c9b5ff29e144bf35a85f607e4a";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
