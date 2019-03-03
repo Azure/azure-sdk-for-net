59a62bfdade6a8e7a6b44a02d291ac13f9344d7d
Bruce Johnston <bruce.johnston@microsoft.com>
2019-03-02 23:55:46 -0800

    [Azure Search] Replace Search implementation with generated code
    Also remove functionality from SearchParameters that isn't used anymore.

--- src/SDKs/Search/DataPlane/Search.Tests/Tests/Serialization/SearchParametersTests.cs
+++ src/SDKs/Search/DataPlane/Search.Tests/Tests/Serialization/SearchParametersTests.cs
@@ -4,7 +4,6 @@
 
 namespace Microsoft.Azure.Search.Tests
 {
-    using System;
     using System.Linq;
     using Common;
     using Models;
@@ -13,100 +12,6 @@ namespace Microsoft.Azure.Search.Tests
 
     public sealed class SearchParametersTests
     {
-        [Fact]
-        public void AllOptionsUnsetGivesDefaultQueryString()
-        {
-            Assert.Equal("$count=false&queryType=simple&searchMode=any", new SearchParameters().ToString());
-        }
-
-        [Fact]
-        public void AllOptionsPropagatedToQueryString()
-        {
-            var parameters =
-                new SearchParameters()
-                {
-                    IncludeTotalResultCount = true,
-                    Facets = new[] { "field,option:value" },
-                    Filter = "field eq value",
-                    HighlightFields = new[] { "field1", "field2" },
-                    HighlightPreTag = "<b>",
-                    HighlightPostTag = "</b>",
-                    MinimumCoverage = 66.67,
-                    OrderBy = new[] { "field1 asc", "field2 desc" },
-                    QueryType = QueryType.Full,
-                    ScoringParameters = new[] 
-                    { 
-                        new ScoringParameter("name", new[] { "Hello, O'Brien", "Smith" }), 
-                        new ScoringParameter("point", GeographyPoint.Create(48.5, -120.1))
-                    },
-                    ScoringProfile = "myprofile",
-                    SearchFields = new[] { "field1", "field2" },
-                    SearchMode = SearchMode.All,
-                    Select = new[] { "field1", "field2" },
-                    Skip = 10,
-                    Top = 5
-                };
-
-            const string ExpectedQueryString =
-                "$count=true&facet=field%2Coption%3Avalue&$filter=field%20eq%20value&highlight=field1,field2&" +
-                "highlightPreTag=%3Cb%3E&highlightPostTag=%3C%2Fb%3E&minimumCoverage=66.67&" +
-                "$orderby=field1 asc,field2 desc&queryType=full&scoringParameter=name-'Hello, O''Brien','Smith'&" +
-                "scoringParameter=point-'-120.1','48.5'&scoringProfile=myprofile&searchFields=field1,field2&" +
-                "searchMode=all&$select=field1,field2&$skip=10&$top=5";
-
-            Assert.Equal(ExpectedQueryString, parameters.ToString());
-        }
-
-        [Fact]
-        public void SomeCollectionParametersRepeat()
-        {
-            var parameters =
-                new SearchParameters()
-                {
-                    Facets = new[] { "field,option:value", "field2,option2:value2" },
-                    ScoringParameters = new[] 
-                    {
-                        new ScoringParameter("name", new[] { "value1", "value2" }),
-                        new ScoringParameter("name2", new[] { "value3", "value4" })
-                    }
-                };
-
-            const string ExpectedQueryString =
-                "$count=false&facet=field%2Coption%3Avalue&facet=field2%2Coption2%3Avalue2&queryType=simple&" +
-                "scoringParameter=name-'value1','value2'&scoringParameter=name2-'value3','value4'&searchMode=any";
-
-            Assert.Equal(ExpectedQueryString, parameters.ToString());
-        }
-
-        [Fact]
-        public void SelectStarPropagatesToQueryString()
-        {
-            var parameters = new SearchParameters() { Select = new[] { "*" } };
-            Assert.Equal("$count=false&queryType=simple&searchMode=any&$select=*", parameters.ToString());
-        }
-
-        [Fact]
-        public void AllOpenStringParametersAreEscaped()
-        {
-            const string UnescapedString = "a+%=@#b";
-            const string EscapedString = "a%2B%25%3D%40%23b";
-
-            var parameters =
-                new SearchParameters()
-                {
-                    Filter = UnescapedString,
-                    HighlightPreTag = UnescapedString,
-                    HighlightPostTag = UnescapedString,
-                    Facets = new[] { UnescapedString }
-                };
-
-            const string ExpectedQueryStringFormat =
-                "$count=false&facet={0}&$filter={0}&highlightPreTag={0}&highlightPostTag={0}&queryType=simple&" +
-                "searchMode=any";
-
-            Assert.Equal(String.Format(ExpectedQueryStringFormat, EscapedString), parameters.ToString());
-        }
-
         [Fact]
         public void CanConvertToPostRequestPayload()
         {
