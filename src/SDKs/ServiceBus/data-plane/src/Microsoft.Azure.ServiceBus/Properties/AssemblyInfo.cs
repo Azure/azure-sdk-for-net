d10ea6e0d90c55cdd1f97e8a6d1a126f67efc52d
Jesse Squire <jesse.squire@gmail.com>
2019-03-04 16:28:30 -0500

    Service Bus SDK   - Import of Service Bus data plane from legacy repository, with history   - Adjustments to adapt project build process to the SDK engineering system.   - Fixes to existing tests to adjust expectations and fix errors introduced by adoption.
    Engineeriny System
      Additional parameter added to the data plane buid properties to allow test projects to opt-in
      for signing/strong naming.  (needed due to use of InternalsVisibleTo attribute for test exposure)

---
+++ src/SDKs/ServiceBus/data-plane/src/Microsoft.Azure.ServiceBus/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.CompilerServices;
+
+// Allow internal members to be accessed by the tests, adjusting for signing, if needed.
+#if CODESIGN
+[assembly: InternalsVisibleTo("Microsoft.Azure.ServiceBus.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")]
+#else
+[assembly: InternalsVisibleTo("Microsoft.Azure.ServiceBus.Tests")]
+#endif
---
+++ src/SDKs/ServiceBus/data-plane/tests/Microsoft.Azure.ServiceBus.Tests/API/APIApprovals.cs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.Azure.ServiceBus.UnitTests.API
+{
+    using System;
+    using System.Linq;
+    using ApprovalTests;
+    using Xunit;
+
+    public class ApiApprovals
+    {
+        /// <remarks>
+        ///   The output of the API generation is compared to the file tests/Microsoft.Azure.ServiceBus.Tests/API/ApiApprovals.ApproveAzureServiceBus.approved.txt,  
+        ///   which is an implicit assumption of the ApprovalTests framework.  
+        ///   
+        ///   If the expected assembly output from generation changes, that file will need to be updated with the new expectation in order for this test 
+        ///   to pass.
+        /// </remarks>
+        [Fact]
+        public void ApproveAzureServiceBus()
+        {
+            var assembly = typeof(Message).Assembly;
+            var publicApi = Filter(PublicApiGenerator.ApiGenerator.GeneratePublicApi(assembly, whitelistedNamespacePrefixes: new[] { "Microsoft.Azure.ServiceBus." }));
+            Approvals.Verify(publicApi);
+        }
+
+        string Filter(string text)
+        {
+            return string.Join(Environment.NewLine, text.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
+                .Where(l => !l.StartsWith("[assembly: System.Runtime.Versioning.TargetFrameworkAttribute"))
+            );
+        }
+    }
+}
