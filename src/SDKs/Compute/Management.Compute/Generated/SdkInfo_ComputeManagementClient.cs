4f87b9606794ffbe4461e5d85735ad60b86eb244
adxsdknet <adxsdknet@outlook.com>
2019-03-08 20:02:46 +0000

    .NET SDK Resource Provider:'Compute' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5347' REST Spec PR Author 'ashintre' REST Spec PR Last commit

--- src/SDKs/Compute/Management.Compute/Generated/SdkInfo_ComputeManagementClient.cs
+++ src/SDKs/Compute/Management.Compute/Generated/SdkInfo_ComputeManagementClient.cs
@@ -44,16 +44,5 @@ namespace Microsoft.Azure.Management.Compute
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/compute/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\hylee-sdk\\diskrp\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "27ad7f33127e71036da2b8e5e8184a25f36e9d72";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
