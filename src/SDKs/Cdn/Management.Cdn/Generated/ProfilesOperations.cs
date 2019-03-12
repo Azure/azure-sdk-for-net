13aa59db9e620cf839657f43b5954bc85809bf25
nachakra <nachakra@microsoft.com>
2019-03-12 09:21:41 -0700

    Add support and tests for SSL on custom domains in CDN

--- src/SDKs/Cdn/Management.Cdn/Generated/ProfilesOperations.cs
+++ src/SDKs/Cdn/Management.Cdn/Generated/ProfilesOperations.cs
@@ -705,7 +705,7 @@ namespace Microsoft.Azure.Management.Cdn
 
         /// <summary>
         /// Generates a dynamic SSO URI used to sign in to the CDN supplemental portal.
-        /// Supplemnetal portal is used to configure advanced feature capabilities that
+        /// Supplemental portal is used to configure advanced feature capabilities that
         /// are not yet available in the Azure portal, such as core reports in a
         /// standard profile; rules engine, advanced HTTP reports, and real-time stats
         /// and alerts in a premium profile. The SSO URI changes approximately every 10
