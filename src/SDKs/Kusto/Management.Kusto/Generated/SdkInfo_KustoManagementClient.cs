c1dc7ac39abc1a936b49e8564d8b71ea3afe5147
adxsdknet <adxsdknet@outlook.com>
2019-03-20 07:31:58 +0000

    .NET SDK Resource Provider:'Kusto' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5366' REST Spec PR Author 'radennis' REST Spec PR Last commit

--- src/SDKs/Kusto/Management.Kusto/Generated/SdkInfo_KustoManagementClient.cs
+++ src/SDKs/Kusto/Management.Kusto/Generated/SdkInfo_KustoManagementClient.cs
@@ -26,16 +26,5 @@ namespace Microsoft.Azure.Management.Kusto
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/azure-kusto/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\Git\\Clients\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "db5576ab45c5dfebe743a528495612bc1e2a26fe";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
