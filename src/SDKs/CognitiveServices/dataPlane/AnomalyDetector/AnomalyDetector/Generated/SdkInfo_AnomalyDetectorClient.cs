b6ca30d14bff638a0fefcda52e6d831af933255e
adxsdknet <adxsdknet@outlook.com>
2019-03-20 21:19:10 +0000

    .NET SDK Resource Provider:'AnomalyDetector' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5440' REST Spec PR Author 'kpajdzik' REST Spec PR Last commit

--- src/SDKs/CognitiveServices/dataPlane/AnomalyDetector/AnomalyDetector/Generated/SdkInfo_AnomalyDetectorClient.cs
+++ src/SDKs/CognitiveServices/dataPlane/AnomalyDetector/AnomalyDetector/Generated/SdkInfo_AnomalyDetectorClient.cs
@@ -24,16 +24,5 @@ namespace Microsoft.Azure.CognitiveServices.AnomalyDetector
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/cognitiveservices/data-plane/AnomalyDetector/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=D:\\src\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "34ec12b92ffb40aec651bc5b37650b98e318333f";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
