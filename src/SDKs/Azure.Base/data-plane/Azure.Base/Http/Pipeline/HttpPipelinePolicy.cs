4953cd6abd4a2ce26c3f6cbc83f893794704c3c3
Pavel Krymets <pavel@krymets.com>
2019-03-25 11:07:39 -0700

    Policy doc

--- src/SDKs/Azure.Base/data-plane/Azure.Base/Http/Pipeline/HttpPipelinePolicy.cs
+++ src/SDKs/Azure.Base/data-plane/Azure.Base/Http/Pipeline/HttpPipelinePolicy.cs
@@ -10,10 +10,23 @@ namespace Azure.Base.Http.Pipeline
 {
     public abstract class HttpPipelinePolicy
     {
+        /// <summary>
+        /// Applies the policy to the <see cref="message"/>. Implementers are expected to mutate <see cref="HttpPipelineMessage.Request"/> before calling <see cref="ProcessNextAsync"/> and observe the <see cref="HttpPipelineMessage.Response"/> changes after.
+        /// Last policy in the pipeline is expected to set the <see cref="HttpPipelineMessage.Response"/>
+        /// </summary>
+        /// <param name="message">The <see cref="HttpPipelineMessage"/> this policy would be applied to.</param>
+        /// <param name="pipeline">The set of <see cref="HttpPipelinePolicy"/> to execute after current one.</param>
+        /// <returns></returns>
         public abstract Task ProcessAsync(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline);
 
         protected HttpPipelineEventSource Log = HttpPipelineEventSource.Singleton;
 
+        /// <summary>
+        /// Invokes the next <see cref="HttpPipelinePolicy"/> in the <see cref="pipeline"/>.
+        /// </summary>
+        /// <param name="message">The <see cref="HttpPipelineMessage"/> next policy would be applied to.</param>
+        /// <param name="pipeline">The set of <see cref="HttpPipelinePolicy"/> to execute after next one.</param>
+        /// <returns></returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         protected static async Task ProcessNextAsync(ReadOnlyMemory<HttpPipelinePolicy> pipeline, HttpPipelineMessage message)
         {
