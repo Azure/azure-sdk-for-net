3229c268a755934bb834edbe65f8403621849a93
adxsdknet <39844661+adxsdknet@users.noreply.github.com>
2019-03-26 16:03:09 -0700

    AutoPr-Billing-wilcobmsft-REST Spec PrNumber 5423 (#5509)
    * .NET SDK Resource Provider:'Billing'
    REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5423'
    REST Spec PR Author 'wilcobmsft'
    REST Spec PR Last commit
    
    * .NET SDK Resource Provider:'Billing'
    REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5423'
    REST Spec PR Author 'wilcobmsft'
    REST Spec PR Last commit
    
    * .NET SDK Resource Provider:'Billing'
    REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5423'
    REST Spec PR Author 'wilcobmsft'
    REST Spec PR Last commit
    
    * .NET SDK Resource Provider:'Billing'
    REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5423'
    REST Spec PR Author 'wilcobmsft'
    REST Spec PR Last commit
    
    * .NET SDK Resource Provider:'Billing'
    REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5423'
    REST Spec PR Author 'wilcobmsft'
    REST Spec PR Last commit
    
    * .NET SDK Resource Provider:'Billing'
    REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5423'
    REST Spec PR Author 'wilcobmsft'
    REST Spec PR Last commit
    
    * .NET SDK Resource Provider:'Billing'
    REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5423'
    REST Spec PR Author 'wilcobmsft'
    REST Spec PR Last commit

--- src/SDKs/Billing/Management.Billing/Generated/EnrollmentAccountsOperationsExtensions.cs
+++ src/SDKs/Billing/Management.Billing/Generated/EnrollmentAccountsOperationsExtensions.cs
@@ -22,62 +22,56 @@ namespace Microsoft.Azure.Management.Billing
     public static partial class EnrollmentAccountsOperationsExtensions
     {
             /// <summary>
-            /// Lists the enrollment accounts the caller has access to.
+            /// Get the enrollment account by id.
             /// </summary>
             /// <param name='operations'>
             /// The operations group for this extension method.
             /// </param>
-            public static EnrollmentAccountListResult List(this IEnrollmentAccountsOperations operations)
+            /// <param name='billingAccountName'>
+            /// billing Account Id.
+            /// </param>
+            /// <param name='enrollmentAccountName'>
+            /// Enrollment Account Id.
+            /// </param>
+            /// <param name='expand'>
+            /// May be used to expand the Department.
+            /// </param>
+            /// <param name='filter'>
+            /// The filter supports 'eq', 'lt', 'gt', 'le', 'ge', and 'and'. It does not
+            /// currently support 'ne', 'or', or 'not'. Tag filter is a key value pair
+            /// string where key and value is separated by a colon (:).
+            /// </param>
+            public static EnrollmentAccount GetByEnrollmentAccountAccountId(this IEnrollmentAccountsOperations operations, string billingAccountName, string enrollmentAccountName, string expand = default(string), string filter = default(string))
             {
-                return operations.ListAsync().GetAwaiter().GetResult();
+                return operations.GetByEnrollmentAccountAccountIdAsync(billingAccountName, enrollmentAccountName, expand, filter).GetAwaiter().GetResult();
             }
 
             /// <summary>
-            /// Lists the enrollment accounts the caller has access to.
+            /// Get the enrollment account by id.
             /// </summary>
             /// <param name='operations'>
             /// The operations group for this extension method.
+            /// </param>
+            /// <param name='billingAccountName'>
+            /// billing Account Id.
+            /// </param>
+            /// <param name='enrollmentAccountName'>
+            /// Enrollment Account Id.
+            /// </param>
+            /// <param name='expand'>
+            /// May be used to expand the Department.
+            /// </param>
+            /// <param name='filter'>
+            /// The filter supports 'eq', 'lt', 'gt', 'le', 'ge', and 'and'. It does not
+            /// currently support 'ne', 'or', or 'not'. Tag filter is a key value pair
+            /// string where key and value is separated by a colon (:).
             /// </param>
             /// <param name='cancellationToken'>
             /// The cancellation token.
             /// </param>
-            public static async Task<EnrollmentAccountListResult> ListAsync(this IEnrollmentAccountsOperations operations, CancellationToken cancellationToken = default(CancellationToken))
+            public static async Task<EnrollmentAccount> GetByEnrollmentAccountAccountIdAsync(this IEnrollmentAccountsOperations operations, string billingAccountName, string enrollmentAccountName, string expand = default(string), string filter = default(string), CancellationToken cancellationToken = default(CancellationToken))
             {
-                using (var _result = await operations.ListWithHttpMessagesAsync(null, cancellationToken).ConfigureAwait(false))
-                {
-                    return _result.Body;
-                }
-            }
-
-            /// <summary>
-            /// Gets a enrollment account by name.
-            /// </summary>
-            /// <param name='operations'>
-            /// The operations group for this extension method.
-            /// </param>
-            /// <param name='name'>
-            /// Enrollment Account name.
-            /// </param>
-            public static EnrollmentAccount Get(this IEnrollmentAccountsOperations operations, string name)
-            {
-                return operations.GetAsync(name).GetAwaiter().GetResult();
-            }
-
-            /// <summary>
-            /// Gets a enrollment account by name.
-            /// </summary>
-            /// <param name='operations'>
-            /// The operations group for this extension method.
-            /// </param>
-            /// <param name='name'>
-            /// Enrollment Account name.
-            /// </param>
-            /// <param name='cancellationToken'>
-            /// The cancellation token.
-            /// </param>
-            public static async Task<EnrollmentAccount> GetAsync(this IEnrollmentAccountsOperations operations, string name, CancellationToken cancellationToken = default(CancellationToken))
-            {
-                using (var _result = await operations.GetWithHttpMessagesAsync(name, null, cancellationToken).ConfigureAwait(false))
+                using (var _result = await operations.GetByEnrollmentAccountAccountIdWithHttpMessagesAsync(billingAccountName, enrollmentAccountName, expand, filter, null, cancellationToken).ConfigureAwait(false))
                 {
                     return _result.Body;
                 }
