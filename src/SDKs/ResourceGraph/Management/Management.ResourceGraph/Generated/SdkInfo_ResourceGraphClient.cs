238769ca28e139d41c76d069fefd958694977f71
adxsdknet <adxsdknet@outlook.com>
2019-03-26 17:36:07 +0000

    .NET SDK Resource Provider:'ResourceGraph' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5478' REST Spec PR Author 'ericc1103' REST Spec PR Last commit

--- src/SDKs/ResourceGraph/Management/Management.ResourceGraph/Generated/SdkInfo_ResourceGraphClient.cs
+++ src/SDKs/ResourceGraph/Management/Management.ResourceGraph/Generated/SdkInfo_ResourceGraphClient.cs
@@ -24,16 +24,5 @@ namespace Microsoft.Azure.Management.ResourceGraph
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/resourcegraph/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\git\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "15929694cc9babeb19c0e834f8babe23af323b75";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
