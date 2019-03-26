dc9b6dbc0af90d86b4770beb506616aca0eaacd2
Your Name <you@example.com>
2019-03-26 12:02:30 +0800

    Auto generate faceSDK, aligned with latest Face API.

--- src/SDKs/CognitiveServices/dataPlane/Vision/Face/Face/Generated/SdkInfo_FaceClient.cs
+++ src/SDKs/CognitiveServices/dataPlane/Vision/Face/Face/Generated/SdkInfo_FaceClient.cs
@@ -30,5 +30,16 @@ namespace Microsoft.Azure.CognitiveServices.Vision.Face
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "latest";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/cognitiveservices/data-plane/Face/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=D:\\FaceTeam\\Release\\azure-sdk-for-net\\src\\SDKs";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "d15714b66f8373f6140d30f6ccca66c175a8e2f7";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
