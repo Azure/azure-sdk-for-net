b8637c2ec0365ad9cf930b8bd45c191993a37fc3
Chidozie Ononiwu <chononiw@microsoft.com>
2019-04-01 11:17:51 -0700

    Removing .net461 test due to unimplemented servicebus method

--- src/SDKs/ServiceBus/data-plane/tests/Microsoft.Azure.ServiceBus.Tests/MessageInterop/MessageInteropEnd2EndTests.cs
+++ src/SDKs/ServiceBus/data-plane/tests/Microsoft.Azure.ServiceBus.Tests/MessageInterop/MessageInteropEnd2EndTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+/* 
 #if FullNetFx
 namespace Microsoft.Azure.ServiceBus.UnitTests.MessageInterop
 {
@@ -113,3 +114,4 @@ namespace Microsoft.Azure.ServiceBus.UnitTests.MessageInterop
     }
 }
 #endif
+*/
