1692b38923647c73437fc163cd75b41bb24b82d5
adxsdknet <adxsdknet@outlook.com>
2019-04-02 18:45:38 +0000

    .NET SDK Resource Provider:'Blueprint' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5535' REST Spec PR Author 'filizt' REST Spec PR Last commit

--- src/SDKs/Blueprint/Management.Blueprint/Generated/SdkInfo_BlueprintManagementClient.cs
+++ src/SDKs/Blueprint/Management.Blueprint/Generated/SdkInfo_BlueprintManagementClient.cs
@@ -28,16 +28,5 @@ namespace Microsoft.Azure.Management.Blueprint
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/blueprint/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\Repos\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "8aa15b5d44d5487f810a4b992d2e7569abd84341";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
