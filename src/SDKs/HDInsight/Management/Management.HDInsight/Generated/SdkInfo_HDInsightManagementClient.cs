a6ea6759176922855c79275fd071781a6cd456b3
adxsdknet <adxsdknet@outlook.com>
2019-04-03 00:08:32 +0000

    .NET SDK Resource Provider:'HDInsight' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5538' REST Spec PR Author 'idear1203' REST Spec PR Last commit

--- src/SDKs/HDInsight/Management/Management.HDInsight/Generated/SdkInfo_HDInsightManagementClient.cs
+++ src/SDKs/HDInsight/Management/Management.HDInsight/Generated/SdkInfo_HDInsightManagementClient.cs
@@ -30,16 +30,5 @@ namespace Microsoft.Azure.Management.HDInsight
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/hdinsight/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=D:\\code\\AzureSDK\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "14f4c33c0a6cc17d0b3577fbddfaacd38a235b57";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
