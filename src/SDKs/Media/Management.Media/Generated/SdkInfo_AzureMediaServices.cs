12760d3e6bceae09ea17c7b03b799e7964d66797
adxsdknet <adxsdknet@outlook.com>
2019-04-05 14:49:17 +0000

    .NET SDK Resource Provider:'Media' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5558' REST Spec PR Author 'BrianBlum' REST Spec PR Last commit

--- src/SDKs/Media/Management.Media/Generated/SdkInfo_AzureMediaServices.cs
+++ src/SDKs/Media/Management.Media/Generated/SdkInfo_AzureMediaServices.cs
@@ -36,16 +36,5 @@ namespace Microsoft.Azure.Management.Media
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/mediaservices/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --opt-in-extensible-enums --csharp-sdks-folder=E:\\GitHub\\BrianBlum\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "e54dca04fcf7a9918b1dcd154c135e8a511a1615";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
