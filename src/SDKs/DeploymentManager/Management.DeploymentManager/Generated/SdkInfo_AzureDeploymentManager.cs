098d302eef54cd152a767627c50e1a620bc2c885
Devesh Guha Oleti Muni <devesh.oleti@microsoft.com>
2019-04-08 13:29:42 -0700

    Regerate sdk using generate.ps1

--- src/SDKs/DeploymentManager/Management.DeploymentManager/Generated/SdkInfo_AzureDeploymentManager.cs
+++ src/SDKs/DeploymentManager/Management.DeploymentManager/Generated/SdkInfo_AzureDeploymentManager.cs
@@ -29,5 +29,16 @@ namespace Microsoft.Azure.Management.DeploymentManager
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "latest";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/deploymentmanager/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\repos\\azure-sdk-for-net\\src\\SDKs";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "67d6bf5c72fb183114938332e72f5d8d995f9979";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
