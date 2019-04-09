645dc414acbd43c69f2f70cb6c7107d25b3cc737
adxsdknet <adxsdknet@outlook.com>
2019-04-09 00:05:16 +0000

    .NET SDK Resource Provider:'IotCentral' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5495' REST Spec PR Author 'kawayiboy' REST Spec PR Last commit

--- src/SDKs/IotCentral/Management.IotCentral/IotCentral/Management.IotCentral/Generated/AppsOperations.cs
+++ src/SDKs/IotCentral/Management.IotCentral/IotCentral/Management.IotCentral/Generated/AppsOperations.cs
@@ -1115,7 +1115,7 @@ namespace Microsoft.Azure.Management.IotCentral
             // Create HTTP transport objects
             var _httpRequest = new HttpRequestMessage();
             HttpResponseMessage _httpResponse = null;
-            _httpRequest.Method = new HttpMethod("GET");
+            _httpRequest.Method = new HttpMethod("POST");
             _httpRequest.RequestUri = new System.Uri(_url);
             // Set Headers
             if (Client.GenerateClientRequestId != null && Client.GenerateClientRequestId.Value)
@@ -2224,7 +2224,7 @@ namespace Microsoft.Azure.Management.IotCentral
             // Create HTTP transport objects
             var _httpRequest = new HttpRequestMessage();
             HttpResponseMessage _httpResponse = null;
-            _httpRequest.Method = new HttpMethod("GET");
+            _httpRequest.Method = new HttpMethod("POST");
             _httpRequest.RequestUri = new System.Uri(_url);
             // Set Headers
             if (Client.GenerateClientRequestId != null && Client.GenerateClientRequestId.Value)
