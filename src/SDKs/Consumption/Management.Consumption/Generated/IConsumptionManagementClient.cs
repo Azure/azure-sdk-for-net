a79e3e9fdbb1c582160823241387203cd4f7f295
adxsdknet <adxsdknet@outlook.com>
2019-04-12 03:59:00 +0000

    .NET SDK Resource Provider:'Consumption' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5622' REST Spec PR Author 'ampravinr' REST Spec PR Last commit

--- src/SDKs/Consumption/Management.Consumption/Generated/IConsumptionManagementClient.cs
+++ src/SDKs/Consumption/Management.Consumption/Generated/IConsumptionManagementClient.cs
@@ -43,9 +43,14 @@ namespace Microsoft.Azure.Management.Consumption
 
         /// <summary>
         /// Version of the API to be used with the client request. The current
-        /// version is 2018-11-01-preview.
+        /// version is 2019-04-01-preview.
         /// </summary>
         string ApiVersion { get; }
+
+        /// <summary>
+        /// Azure Subscription ID.
+        /// </summary>
+        string SubscriptionId { get; set; }
 
         /// <summary>
         /// The preferred language for the response.
@@ -67,49 +72,74 @@ namespace Microsoft.Azure.Management.Consumption
 
 
         /// <summary>
+        /// Gets the IUsageDetailsOperations.
+        /// </summary>
+        IUsageDetailsOperations UsageDetails { get; }
+
+        /// <summary>
+        /// Gets the IUsageDetailsListOperations.
+        /// </summary>
+        IUsageDetailsListOperations UsageDetailsList { get; }
+
+        /// <summary>
+        /// Gets the IMarketplacesOperations.
+        /// </summary>
+        IMarketplacesOperations Marketplaces { get; }
+
+        /// <summary>
+        /// Gets the IBudgetsOperations.
+        /// </summary>
+        IBudgetsOperations Budgets { get; }
+
+        /// <summary>
+        /// Gets the ITagsOperations.
+        /// </summary>
+        ITagsOperations Tags { get; }
+
+        /// <summary>
+        /// Gets the IChargesOperations.
+        /// </summary>
+        IChargesOperations Charges { get; }
+
+        /// <summary>
+        /// Gets the IBalancesOperations.
+        /// </summary>
+        IBalancesOperations Balances { get; }
+
+        /// <summary>
+        /// Gets the IReservationsSummariesOperations.
+        /// </summary>
+        IReservationsSummariesOperations ReservationsSummaries { get; }
+
+        /// <summary>
+        /// Gets the IReservationsDetailsOperations.
+        /// </summary>
+        IReservationsDetailsOperations ReservationsDetails { get; }
+
+        /// <summary>
+        /// Gets the IReservationRecommendationsOperations.
+        /// </summary>
+        IReservationRecommendationsOperations ReservationRecommendations { get; }
+
+        /// <summary>
+        /// Gets the IPriceSheetOperations.
+        /// </summary>
+        IPriceSheetOperations PriceSheet { get; }
+
+        /// <summary>
+        /// Gets the IForecastsOperations.
+        /// </summary>
+        IForecastsOperations Forecasts { get; }
+
+        /// <summary>
         /// Gets the IOperations.
         /// </summary>
         IOperations Operations { get; }
 
         /// <summary>
-        /// Gets the ICreditSummaryByBillingProfileOperations.
+        /// Gets the IAggregatedCostOperations.
         /// </summary>
-        ICreditSummaryByBillingProfileOperations CreditSummaryByBillingProfile { get; }
-
-        /// <summary>
-        /// Gets the IEventsByBillingProfileOperations.
-        /// </summary>
-        IEventsByBillingProfileOperations EventsByBillingProfile { get; }
-
-        /// <summary>
-        /// Gets the ILotsByBillingProfileOperations.
-        /// </summary>
-        ILotsByBillingProfileOperations LotsByBillingProfile { get; }
-
-        /// <summary>
-        /// Gets the IInvoicePricesheetOperations.
-        /// </summary>
-        IInvoicePricesheetOperations InvoicePricesheet { get; }
-
-        /// <summary>
-        /// Gets the IBillingProfilePricesheetOperations.
-        /// </summary>
-        IBillingProfilePricesheetOperations BillingProfilePricesheet { get; }
-
-        /// <summary>
-        /// Gets the IChargesByBillingAccountOperations.
-        /// </summary>
-        IChargesByBillingAccountOperations ChargesByBillingAccount { get; }
-
-        /// <summary>
-        /// Gets the IChargesByBillingProfileOperations.
-        /// </summary>
-        IChargesByBillingProfileOperations ChargesByBillingProfile { get; }
-
-        /// <summary>
-        /// Gets the IChargesByInvoiceSectionOperations.
-        /// </summary>
-        IChargesByInvoiceSectionOperations ChargesByInvoiceSection { get; }
+        IAggregatedCostOperations AggregatedCost { get; }
 
     }
 }
