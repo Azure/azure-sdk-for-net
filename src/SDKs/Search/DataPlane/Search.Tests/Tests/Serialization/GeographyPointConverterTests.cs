86e5d7d187b378358ba069bcb8c8811458a19a54
Bruce Johnston <bruce.johnston@microsoft.com>
2019-04-12 17:00:08 -0700

    [Azure Search] Make GeographyPointConverter more robust in what JSON it accepts

--- src/SDKs/Search/DataPlane/Search.Tests/Tests/Serialization/GeographyPointConverterTests.cs
+++ src/SDKs/Search/DataPlane/Search.Tests/Tests/Serialization/GeographyPointConverterTests.cs
@@ -18,17 +18,20 @@ namespace Microsoft.Azure.Search.Tests
         [Fact]
         public void CanWriteGeoPoint()
         {
-            var point = GeographyPoint.Create(47.1, 121.9);
-            string json = JsonConvert.SerializeObject(point, _jsonSettings);
+            // NOTE: Spacing here must match exactly.
+            const string ExpectedJson = @"{""type"":""Point"",""coordinates"":[121.9,47.1]}";
 
-            Assert.Equal(@"{""type"":""Point"",""coordinates"":[121.9,47.1]}", json);
+            var point = GeographyPoint.Create(47.1, 121.9);
+            string actualJson = JsonConvert.SerializeObject(point, _jsonSettings);
+
+            Assert.Equal(ExpectedJson, actualJson);
         }
 
         [Fact]
         public void CanReadWellFormedGeoPoint()
         {
             var expectedPoint = GeographyPoint.Create(47.1, 121.9);
-            const string Json = @"{""type"":""Point"",""coordinates"":[121.9,47.1]}";
+            const string Json = @"{ ""type"": ""Point"", ""coordinates"": [ 121.9, 47.1 ] }";
 
             GeographyPoint actualPoint = JsonConvert.DeserializeObject<GeographyPoint>(Json, _jsonSettings);
 
@@ -39,7 +42,7 @@ namespace Microsoft.Azure.Search.Tests
         public void CanReadWellFormedGeoPointWithIntegerCoordinates()
         {
             var expectedPoint = GeographyPoint.Create(47, 121);
-            const string Json = @"{""type"":""Point"",""coordinates"":[121,47]}";
+            const string Json = @"{ ""type"": ""Point"", ""coordinates"": [ 121, 47 ] }";
 
             GeographyPoint actualPoint = JsonConvert.DeserializeObject<GeographyPoint>(Json, _jsonSettings);
 
@@ -51,7 +54,37 @@ namespace Microsoft.Azure.Search.Tests
         {
             var expectedPoint = GeographyPoint.Create(47.1, 121.9);
             const string Json = 
-@"{""type"":""Point"",""coordinates"":[121.9,47.1],""crs"":{""type"":""name"",""properties"":{""name"":""EPSG:4326""}}}";
+@"{
+    ""type"": ""Point"",
+    ""coordinates"": [ 121.9, 47.1 ],
+    ""crs"": {
+        ""type"": ""name"",
+        ""properties"": {
+            ""name"": ""EPSG:4326""
+        }
+    }
+}";
+
+            GeographyPoint actualPoint = JsonConvert.DeserializeObject<GeographyPoint>(Json, _jsonSettings);
+
+            Assert.Equal(expectedPoint, actualPoint);
+        }
+
+        [Fact]
+        public void CanReadWellFormedGeoPointWithPropertiesOutOfOrder()
+        {
+            var expectedPoint = GeographyPoint.Create(47.1, 121.9);
+            const string Json =
+@"{
+    ""coordinates"": [ 121.9, 47.1 ],
+    ""crs"": {
+        ""properties"": {
+            ""name"": ""EPSG:4326""
+        },
+        ""type"": ""name""
+    },
+    ""type"": ""Point""
+}";
 
             GeographyPoint actualPoint = JsonConvert.DeserializeObject<GeographyPoint>(Json, _jsonSettings);
 
@@ -61,7 +94,7 @@ namespace Microsoft.Azure.Search.Tests
         [Fact]
         public void ReadInvalidGeoPointContentThrowsException()
         {
-            const string InvalidJson = @"{""type"":""NotAPoint"",""coordinates"":[121.9,47.1]}";
+            const string InvalidJson = @"{ ""type"": ""NotAPoint"", ""coordinates"": [ 121.9, 47.1 ] }";
 
             Assert.Throws<JsonSerializationException>(
                 () => JsonConvert.DeserializeObject<GeographyPoint>(InvalidJson, _jsonSettings));
@@ -70,19 +103,35 @@ namespace Microsoft.Azure.Search.Tests
         [Fact]
         public void ReadInvalidGeoPointStructureThrowsException()
         {
-            const string InvalidJson = @"{""type"":[""Point""]}";
+            const string InvalidJson = @"{ ""type"": [ ""Point"" ] }";
 
             Assert.Throws<JsonSerializationException>(
                 () => JsonConvert.DeserializeObject<GeographyPoint>(InvalidJson, _jsonSettings));
         }
 
-        [Fact]
-        public void ReadValidGeoPointWithExtraStuffThrowsException()
+        [Theory]
+        [InlineData(@"{ ""type"": ""Point"", ""coordinates"": [ 121.9, 47.1 ], ""nope"": 0 }")]
+        [InlineData(@"{ ""type"": ""Point"", ""coordinates"": [ 121.9, 47.1 ], ""crs"": { ""type"": ""name"", ""properties"": { ""name"": ""EPSG:4326"" }, ""nope"": false } }")]
+        public void ReadValidGeoPointWithExtraStuffThrowsException(string invalidJson)
         {
-            const string InvalidJson = @"{""type"":""Point"",""coordinates"":[121.9,47.1],""nope"":0}";
+            Assert.Throws<JsonSerializationException>(() => JsonConvert.DeserializeObject<GeographyPoint>(invalidJson, _jsonSettings));
+        }
 
-            Assert.Throws<JsonSerializationException>(
-                () => JsonConvert.DeserializeObject<GeographyPoint>(InvalidJson, _jsonSettings));
+        [Fact]
+        public void WhenPropertiesAreDuplicatedTheLastInstanceIsUsed()
+        {
+            const string Json = @"{ ""type"": ""Point"", ""coordinates"": [ 121.9, 47.1 ], ""coordinates"": [ 122.1, 49.1 ] }";
+
+            var expectedPoint = GeographyPoint.Create(49.1, 122.1);
+            var actualPoint = JsonConvert.DeserializeObject<GeographyPoint>(Json, _jsonSettings);
+
+            Assert.Equal(expectedPoint, actualPoint);
+        }
+
+        [Fact]
+        public void ReadEmptyObjectThrowsException()
+        {
+            Assert.Throws<JsonSerializationException>(() => JsonConvert.DeserializeObject<GeographyPoint>("{}", _jsonSettings));
         }
 
         [Fact]
