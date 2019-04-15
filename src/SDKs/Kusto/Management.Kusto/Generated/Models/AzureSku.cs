e5b75c98b7723710629aa9c603447b6440aa91ff
adxsdknet <adxsdknet@outlook.com>
2019-04-15 11:53:50 +0000

    .NET SDK Resource Provider:'Kusto' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5575' REST Spec PR Author 'liatbezalel' REST Spec PR Last commit

--- src/SDKs/Kusto/Management.Kusto/Generated/Models/AzureSku.cs
+++ src/SDKs/Kusto/Management.Kusto/Generated/Models/AzureSku.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Azure.Management.Kusto.Models
         /// 'Standard_DS14_v2+3TB_PS', 'Standard_DS14_v2+4TB_PS',
         /// 'Standard_D13_v2', 'Standard_D14_v2', 'Standard_L8s',
         /// 'Standard_L16s', 'Standard_D11_v2', 'Standard_D12_v2',
-        /// 'Standard_L4s', 'Dev(No SLA)_Standard_D11_v2'</param>
+        /// 'Standard_L4s'</param>
         /// <param name="capacity">SKU capacity.</param>
         public AzureSku(string name, int? capacity = default(int?))
         {
@@ -62,7 +62,7 @@ namespace Microsoft.Azure.Management.Kusto.Models
         /// 'Standard_DS14_v2+3TB_PS', 'Standard_DS14_v2+4TB_PS',
         /// 'Standard_D13_v2', 'Standard_D14_v2', 'Standard_L8s',
         /// 'Standard_L16s', 'Standard_D11_v2', 'Standard_D12_v2',
-        /// 'Standard_L4s', 'Dev(No SLA)_Standard_D11_v2'
+        /// 'Standard_L4s'
         /// </summary>
         [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
