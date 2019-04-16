1d89e4f07973718c4ba7d4b4058846546e293394
adxsdknet <adxsdknet@outlook.com>
2019-04-16 17:19:43 +0000

    .NET SDK Resource Provider:'ResourceGraph' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5478' REST Spec PR Author 'ericc1103' REST Spec PR Last commit

--- src/SDKs/ResourceGraph/Management/Management.ResourceGraph/Generated/SdkInfo_ResourceGraphClient.cs
+++ src/SDKs/ResourceGraph/Management/Management.ResourceGraph/Generated/SdkInfo_ResourceGraphClient.cs
@@ -19,8 +19,8 @@ namespace Microsoft.Azure.Management.ResourceGraph
           {
               return new Tuple<string, string, string>[]
               {
-                new Tuple<string, string, string>("ResourceGraph", "Operations", "2018-09-01-preview"),
-                new Tuple<string, string, string>("ResourceGraph", "Resources", "2018-09-01-preview"),
+                new Tuple<string, string, string>("ResourceGraph", "Operations", "2019-04-01"),
+                new Tuple<string, string, string>("ResourceGraph", "Resources", "2019-04-01"),
               }.AsEnumerable();
           }
       }
