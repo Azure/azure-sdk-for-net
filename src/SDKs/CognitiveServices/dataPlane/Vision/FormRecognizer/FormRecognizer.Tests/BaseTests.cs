be8d5e5b70e244aa62ab74e17ad48bf681ed1536
Sandeep Parab <rparab@ntdev.microsoft.com>
2019-04-19 04:51:08 -0700

    Initial commit

---
+++ src/SDKs/CognitiveServices/dataPlane/Vision/FormRecognizer/FormRecognizer.Tests/BaseTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.FormRecognizer;
+using System.Net.Http;
+
+namespace FormRecognizerSDK.Tests
+{
+    public abstract class BaseTests
+    {
+        public static bool IsTestTenant = false;
+        private static readonly string FormRecognizerSubscriptionKey;
+
+        static BaseTests()
+        {
+            // Retrieve the configuration information.
+            FormRecognizerSubscriptionKey = "";
+        }
+
+        protected IFormRecognizerClient GetFormRecognizerClient(DelegatingHandler handler)
+        {
+            IFormRecognizerClient client = new FormRecognizerClient(new ApiKeyServiceClientCredentials(FormRecognizerSubscriptionKey), handlers: handler)
+            {
+                Endpoint = "https://westus.api.cognitive.microsoft.com"
+            };
+
+            return client;
+        }
+    }
+}
---
+++ src/SDKs/CognitiveServices/dataPlane/Vision/FormRecognizer/FormRecognizer.Tests/FormRecognizerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.FormRecognizer;
+using Microsoft.Azure.CognitiveServices.Vision.FormRecognizer.Models;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Xunit;
+
+namespace FormRecognizerSDK.Tests
+{
+    public class FormRecognizerTests : BaseTests
+    {
+        [Fact]
+        public void TrainModel()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "FaceIdentificationPersonGroupPositive");
+
+                IFormRecognizerClient client = GetFormRecognizerClient(HttpMockServer.CreateInstance());
+
+                TrainRequest trainRequest = new TrainRequest("/input/data");
+
+                TrainResponse trainResponse = client.FormRecognizer.CustomTrain(trainRequest);
+
+                Assert.True(trainResponse.Errors.Count == 0);
+                Assert.True(!string.IsNullOrEmpty(trainResponse.ModelId));
+            }
+        }
+    }
+}
