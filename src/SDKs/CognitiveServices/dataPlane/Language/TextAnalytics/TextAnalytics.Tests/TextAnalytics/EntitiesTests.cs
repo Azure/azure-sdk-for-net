dfb2cf286ca25f54ee9dcadb7f6187f03809d916
Han Li <hali2@microsoft.com>
2019-04-21 16:10:56 -0700

    update all tests

--- src/SDKs/CognitiveServices/dataPlane/Language/TextAnalytics/TextAnalytics.Tests/TextAnalytics/EntitiesTests.cs
+++ src/SDKs/CognitiveServices/dataPlane/Language/TextAnalytics/TextAnalytics.Tests/TextAnalytics/EntitiesTests.cs
@@ -11,11 +11,11 @@ namespace Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Tests
     public class EntitiesTests : BaseTests
     {
         [Fact]
-        public async Task Entities()
+        public async Task EntitiesBatchAsync()
         {
             using (MockContext context = MockContext.Start(this.GetType().FullName))
             {
-                HttpMockServer.Initialize(this.GetType().FullName, "Entities");
+                HttpMockServer.Initialize(this.GetType().FullName, "EntitiesBatchAsync");
                 ITextAnalyticsClient client = GetClient(HttpMockServer.CreateInstance());
                 EntitiesBatchResult result = await client.EntitiesBatchAsync(
                     new MultiLanguageBatchInput(
@@ -37,5 +37,72 @@ namespace Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Tests
                 context.Stop();
             }
         }
+
+        [Fact]
+        public async Task EntitiesAsync()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "EntitiesAsync");
+                ITextAnalyticsClient client = GetClient(HttpMockServer.CreateInstance());
+                EntitiesBatchResult result = await client.EntitiesAsync(
+                    "Microsoft released Windows 10");
+
+                Assert.Equal("Microsoft", result.Documents[0].Entities[0].Name);
+                Assert.Equal("a093e9b9-90f5-a3d5-c4b8-5855e1b01f85", result.Documents[0].Entities[0].BingId);
+                Assert.Equal("Microsoft", result.Documents[0].Entities[0].Matches[0].Text);
+                Assert.Equal(0.12508682244047509, result.Documents[0].Entities[0].Matches[0].WikipediaScore);
+                Assert.Equal(0.99999618530273438, result.Documents[0].Entities[0].Matches[0].EntityTypeScore);
+                context.Stop();
+            }
+        }
+
+        [Fact]
+        public void EntitiesBatch()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "EntitiesBatch");
+                ITextAnalyticsClient client = GetClient(HttpMockServer.CreateInstance());
+                EntitiesBatchResult result = client.EntitiesBatch(
+                    new MultiLanguageBatchInput(
+                        new List<MultiLanguageInput>()
+                        {
+                            new MultiLanguageInput()
+                            {
+                                Id ="id",
+                                Text ="Microsoft released Windows 10",
+                                Language ="en"
+                            }
+                        }));
+
+                Assert.Equal("Microsoft", result.Documents[0].Entities[0].Name);
+                Assert.Equal("a093e9b9-90f5-a3d5-c4b8-5855e1b01f85", result.Documents[0].Entities[0].BingId);
+                Assert.Equal("Microsoft", result.Documents[0].Entities[0].Matches[0].Text);
+                Assert.Equal(0.12508682244047509, result.Documents[0].Entities[0].Matches[0].WikipediaScore);
+                Assert.Equal(0.99999618530273438, result.Documents[0].Entities[0].Matches[0].EntityTypeScore);
+                context.Stop();
+            }
+        }
+
+        [Fact]
+        public void Entities()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "Entities");
+                ITextAnalyticsClient client = GetClient(HttpMockServer.CreateInstance());
+                EntitiesBatchResult result = client.Entities(
+                    "Microsoft released Windows 10");
+
+                Assert.Equal("Microsoft", result.Documents[0].Entities[0].Name);
+                Assert.Equal("a093e9b9-90f5-a3d5-c4b8-5855e1b01f85", result.Documents[0].Entities[0].BingId);
+                Assert.Equal("Microsoft", result.Documents[0].Entities[0].Matches[0].Text);
+                Assert.Equal(0.12508682244047509, result.Documents[0].Entities[0].Matches[0].WikipediaScore);
+                Assert.Equal(0.99999618530273438, result.Documents[0].Entities[0].Matches[0].EntityTypeScore);
+                context.Stop();
+            }
+        }
+
     }
 }
--- src/SDKs/CognitiveServices/dataPlane/Language/TextAnalytics/TextAnalytics.Tests/TextAnalytics/SentimentTests.cs
+++ src/SDKs/CognitiveServices/dataPlane/Language/TextAnalytics/TextAnalytics.Tests/TextAnalytics/SentimentTests.cs
@@ -14,11 +14,11 @@ namespace Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Tests
     public class SentimentTests : BaseTests
     {
         [Fact]
-        public async Task Sentiment()
+        public async Task SentimentBatchAsync()
         {
             using (MockContext context = MockContext.Start(this.GetType().FullName))
             {
-                HttpMockServer.Initialize(this.GetType().FullName, "Sentiment");
+                HttpMockServer.Initialize(this.GetType().FullName, "SentimentBatchAsync");
                 ITextAnalyticsClient client = GetClient(HttpMockServer.CreateInstance());
                 SentimentBatchResult result = await client.SentimentBatchAsync(
                     new MultiLanguageBatchInput(
@@ -36,5 +36,55 @@ namespace Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Tests
             }
         }
 
+        [Fact]
+        public async Task SentimentAsync()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "SentimentAsync");
+                ITextAnalyticsClient client = GetClient(HttpMockServer.CreateInstance());
+                SentimentBatchResult result = await client.SentimentAsync(
+                    "I love my team mates");
+
+                Assert.True(result.Documents[0].Score > 0);
+            }
+        }
+
+        [Fact]
+        public void SentimentBatch()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "SentimentBatch");
+                ITextAnalyticsClient client = GetClient(HttpMockServer.CreateInstance());
+                SentimentBatchResult result = client.SentimentBatch(
+                    new MultiLanguageBatchInput(
+                        new List<MultiLanguageInput>()
+                        {
+                            new MultiLanguageInput()
+                            {
+                                Id ="id",
+                                Text ="I love my team mates",
+                                Language ="en"
+                            }
+                        }));
+
+                Assert.True(result.Documents[0].Score > 0);
+            }
+        }
+
+        [Fact]
+        public void Sentiment()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "Sentiment");
+                ITextAnalyticsClient client = GetClient(HttpMockServer.CreateInstance());
+                SentimentBatchResult result = client.Sentiment(
+                    "I love my team mates");
+
+                Assert.True(result.Documents[0].Score > 0);
+            }
+        }
     }
 }
