8228418b892acaccbfca0492ebfcd414a9641593
tyclintw <tyclintw@microsoft.com>
2019-04-22 09:32:31 -0700

    Initial setup of Personalizer SDK

---
+++ src/SDKs/CognitiveServices/dataPlane/Personalizer/Personalizer.Tests/BaseTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.CognitiveServices.Personalizer;
+using System.Net.Http;
+
+namespace Personalizer.Tests
+{
+    public abstract class BaseTests
+    {
+        public static bool IsTestTenant = false;
+        private static readonly string ApiKey;
+
+        static BaseTests()
+        {
+            // Retrieve the configuration information.
+            ApiKey = "";
+        }
+
+        protected IPersonalizerClient GetClient(DelegatingHandler handler)
+        {
+            IPersonalizerClient client = new PersonalizerClient(new ApiKeyServiceClientCredentials(ApiKey), handlers: handler)
+            {
+                Endpoint = "https://westus.api.cognitive.microsoft.com"
+            };
+
+            return client;
+        }
+    }
+}
---
+++ src/SDKs/CognitiveServices/dataPlane/Personalizer/Personalizer.Tests/ConfigurationsTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Personalizer.Tests
+{
+    public class ConfigurationsTests
+    {
+        [Fact]
+        public void PersonalizerGetServiceConfigurations()
+        {
+
+        }
+    }
+}
