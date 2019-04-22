5cb899e62fef935e0a6020bb4346686af37ffaff
Sean Oh <hyo@microsoft.com>
2019-04-22 14:15:31 -0700

    correcting version

--- src/SDKs/Reservations/Management.Reservations/Properties/AssemblyInfo.cs
+++ src/SDKs/Reservations/Management.Reservations/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure Reserved Instance Management Library")]
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure Reserved Instance Resources.")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.10.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Microsoft Azure .NET SDK")]
