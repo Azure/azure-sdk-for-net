a18ed5d8de6aceb5bbdc3f152302293b329d027b
Pavel Krymets <pavel@krymets.com>
2019-04-22 16:33:29 -0700

    Switch ETag to use string

---
+++ src/SDKs/Azure.Core/data-plane/Azure.Core.Tests/ETagTests.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+
+namespace Azure.Core.Tests
+{
+    public class ETagTests
+    {
+        [Test]
+        public void StringRoundtrips()
+        {
+            var s = "tag";
+            var eTag = new ETag(s);
+            Assert.AreSame(s, eTag.ToString());
+        }
+
+        [Test]
+        public void ComparisonOperators()
+        {
+            var eTag = new ETag("a");
+
+            Assert.True(eTag == new ETag("a"));
+            Assert.False(eTag == new ETag("A"));
+
+            Assert.False(eTag != new ETag("a"));
+            Assert.True(eTag != new ETag("A"));
+
+            Assert.True(eTag.Equals(new ETag("a")));
+            Assert.False(eTag.Equals(new ETag("A")));
+
+            Assert.AreEqual(eTag.GetHashCode(), new ETag("a").GetHashCode());
+            Assert.AreNotEqual(eTag.GetHashCode(), new ETag("A").GetHashCode());
+        }
+
+        [Test]
+        public void DefaultComparisonOperators()
+        {
+            var eTag = new ETag();
+
+            Assert.True(eTag == new ETag(null));
+            Assert.True(eTag == default);
+
+            Assert.True(eTag.Equals(new ETag(null)));;
+
+            Assert.AreEqual(eTag.GetHashCode(), new ETag(null).GetHashCode());
+        }
+    }
+}
