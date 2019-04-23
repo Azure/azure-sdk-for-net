27512037e9085f8a42d77ff06e7782f1e4f717eb
Tyler Clintworth <tyclintw@microsoft.com>
2019-04-22 22:11:28 -0700

    Consolidate tests to controller groups.

--- src/SDKs/CognitiveServices/dataPlane/Personalizer/Personalizer.Tests/Personalizer/LogsTests.cs
+++ src/SDKs/CognitiveServices/dataPlane/Personalizer/Personalizer.Tests/Personalizer/LogsTests.cs
@@ -1,14 +1,13 @@
-using System;
-using System.IO;
+﻿using Microsoft.Azure.CognitiveServices.Personalizer.Models;
 using Microsoft.Azure.Test.HttpRecorder;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
-using System.Threading.Tasks;
 using Xunit;
-using Microsoft.Azure.CognitiveServices.Personalizer.Models;
+using System;
+using System.Threading.Tasks;
 
 namespace Microsoft.Azure.CognitiveServices.Personalizer.Tests
 {
-    public class GetLogsPropertiesTest : BaseTests
+    public class LogsTests : BaseTests
     {
         [Fact]
         public async Task GetLogsProperties()
@@ -22,6 +21,19 @@ namespace Microsoft.Azure.CognitiveServices.Personalizer.Tests
                 LogsProperties properties = await client.GetLogsPropertiesAsync();
                 Assert.Equal(new DateTime(2019, 04, 13), new DateTime(properties.DateRange.FromProperty.Value.Year, properties.DateRange.FromProperty.Value.Month, properties.DateRange.FromProperty.Value.Day));
                 Assert.Equal(new DateTime(2019, 04, 23), new DateTime(properties.DateRange.To.Value.Year, properties.DateRange.To.Value.Month, properties.DateRange.To.Value.Day));
+            }
+        }
+
+        [Fact]
+        public async Task DeleteLogs()
+        {
+            using (MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "DeleteLogs");
+
+                IPersonalizerClient client = GetClient(HttpMockServer.CreateInstance());
+
+                await client.DeleteLogsAsync();
             }
         }
     }
---
+++ src/SDKs/CognitiveServices/dataPlane/Personalizer/Personalizer.Tests/Personalizer/ModelTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.CognitiveServices.Personalizer.Models;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Xunit;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.CognitiveServices.Personalizer.Tests
+{
+    public class ModelTests : BaseTests
+    {
+        [Fact]
+        public async Task GetModel()
+        {
+            using (MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "GetModel");
+
+                IPersonalizerClient client = GetClient(HttpMockServer.CreateInstance());
+
+                Stream stream = await client.GetModelAsync();
+                Assert.NotEqual(stream.ReadByte(), -1);
+            }
+        }
+
+        [Fact]
+        public async Task DeleteModel()
+        {
+            using (MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "DeleteModel");
+
+                IPersonalizerClient client = GetClient(HttpMockServer.CreateInstance());
+
+                await client.DeleteModelAsync();
+            }
+        }
+
+        [Fact]
+        public async Task GetModelProperties()
+        {
+            using (MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "GetModelProperties");
+
+                IPersonalizerClient client = GetClient(HttpMockServer.CreateInstance());
+
+                ModelProperties modelProperties = await client.GetModelPropertiesAsync();
+                Assert.True(modelProperties.CreationTime != null);
+                Assert.True(modelProperties.LastModifiedTime != null);
+            }
+        }
+    }
+}
