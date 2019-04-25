6210f387cf1515f751c695d324d08f02960f7168
Jesse Squire <jesse.squire@gmail.com>
2019-04-25 12:32:34 -0400

    [Event Hubs] Update to New Repository Structure (#5923)
    The Event Hubs area of the repository was structured for compliance with the
      legacy standards.  With a new standard repository structure having been formally adopted,
      the Event Hubs client library should conform to it.   At present, the management client will
      continue to exist as part of the legacy structure.
    
      (see: https://github.com/Azure/azure-sdk/blob/master/docs/engineering-system/repo-structure.md)
    
      Engineering system
        - Annotated the document settings to note the known format discrepancies for the current
          version of the Event Hubs client library ReadMe, as a Track 1 sdk.
    
        - Renamed environment variables to follow the new patterns chosen as the cross-SDK standard.  They
          are now:
              EVENT_HUBS_CONNECTION_STRING
              EVENT_HUBS_STORAGE_CONNECTION_STRING
    
      Project Structure
        - Solution and projects have been move to the new sdk root location, with their containing
          directories renamed to match the standard.
    
        - The directories for shared items (such as Tests) under the client library have been located
          under the root of the main package (Microsoft.Azure.EventHubs) with references  to the additional
          projects.  Though this does impede the goal of having packages entirely independent, it allows for
          the structure to be organized around packages rather than implementation.
    
        - The ReadMe.md file has been renamed to README.md for consistency with other .NET client libraries.

--- sdk/eventhub/Microsoft.Azure.EventHubs/tests/Infrastructure/TestConstants.cs
+++ sdk/eventhub/Microsoft.Azure.EventHubs/tests/Infrastructure/TestConstants.cs
@@ -8,8 +8,8 @@ namespace Microsoft.Azure.EventHubs.Tests
     internal static class TestConstants
     {
         // Environment Variables
-        internal const string EventHubsConnectionStringEnvironmentVariableName = "AZ_EVENT_HUBS_CONNECTION";
-        internal const string StorageConnectionStringEnvironmentVariableName = "AZ_EVENT_HUBS_STORAGE_CONNECTION";
+        internal const string EventHubsConnectionStringEnvironmentVariableName = "EVENT_HUBS_CONNECTION_STRING";
+        internal const string StorageConnectionStringEnvironmentVariableName = "EVENT_HUBS_STORAGE_CONNECTION_STRING";
 
         // General
         internal const string DefultEventHubName = "eventhubs-sdk-test-hub";
