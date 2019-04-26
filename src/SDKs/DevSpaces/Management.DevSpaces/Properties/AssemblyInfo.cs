a1124052621d585cfb0787cd11e5b192a769d914
Sarthak Narang <sanar@microsoft.com>
2019-04-26 13:07:21 -0700

    [DevSpaces] 2019-04-01 API version update

--- src/SDKs/DevSpaces/Management.DevSpaces/Properties/AssemblyInfo.cs
+++ src/SDKs/DevSpaces/Management.DevSpaces/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure Dev Spaces Management Library")]
 [assembly: AssemblyDescription("Provides management operations support for Azure Dev Spaces.")]
 
-[assembly: AssemblyVersion("0.11.0.0")]
-[assembly: AssemblyFileVersion("0.11.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
