9828f544d036636bc3146743794adf7934bfaaf6
Pavel Krymets <pavel@krymets.com>
2019-04-29 10:34:15 -0700

    Expose Raw response as a property (#5968)

--- src/SDKs/Azure.Core/data-plane/Azure.Core/ResponseOfT.cs
+++ src/SDKs/Azure.Core/data-plane/Azure.Core/ResponseOfT.cs
@@ -8,34 +8,21 @@ namespace Azure
 {
     public readonly struct Response<T> : IDisposable
     {
-        private readonly Response _response;
-
         public Response(Response response, T parsed)
         {
-            _response = response;
+            Raw = response;
             Value = parsed;
         }
 
-        public void Deconstruct(out T value, out Response response)
-        {
-            value = Value;
-            response = _response;
-        }
-
-        public static implicit operator T(Response<T> response) => response.Value;
+        public Response Raw { get; }
 
         public T Value { get; }
 
-        public int Status => _response.Status;
+        public static implicit operator T(Response<T> response) => response.Value;
 
         public void Dispose()
         {
-            _response.Dispose();
-        }
-
-        public bool TryGetHeader(string name, out string values)
-        {
-            return _response.TryGetHeader(name, out values);
+            Raw.Dispose();
         }
 
         [EditorBrowsable(EditorBrowsableState.Never)]
