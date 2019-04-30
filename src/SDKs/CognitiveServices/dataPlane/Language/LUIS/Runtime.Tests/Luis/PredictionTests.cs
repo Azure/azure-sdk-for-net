b0f4e47b2e9c147dd50d246a7b186ceb8aee3516
Mohamed Abbas <moaba@microsoft.com>
2019-04-30 22:58:25 +0200

    Update LUIS SDK to support APIs v3.0-preview (#5991)
    * Update LUIS SDK to support APIs v3.0-preview
    
    * Update the Nuget package version to 2.8.0-preview
    
    * Update the AssemblyVersion and the package release notes
    
    * Generate the SDK using the default specs
    
    * Revert AssemblyVersion update

--- src/SDKs/CognitiveServices/dataPlane/Language/LUIS/Runtime.Tests/Luis/PredictionTests.cs
+++ src/SDKs/CognitiveServices/dataPlane/Language/LUIS/Runtime.Tests/Luis/PredictionTests.cs
@@ -1,68 +1,238 @@
 ﻿namespace LUIS.Runtime.Tests
 {
-    using System.Collections.Generic;
+    using System;
+    using System.Linq;
     using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
     using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
     using Microsoft.Rest;
+    using Newtonsoft.Json.Linq;
     using Xunit;
 
     public class PredictionTests : BaseTest
     {
         [Fact]
-        public void Prediction_Post()
+        public void Prediction_Slot()
         {
             UseClientFor(async client =>
             {
-                var utterance = "this is a test with post";
-                var result = await client.Prediction.ResolveAsync(System.Guid.Parse(appId), utterance);
+                var utterance = "today this is a test with post";
+                var slotName = "production";
 
-                Assert.Equal("None", result.TopScoringIntent.Intent);
-                Assert.Equal(utterance, result.Query);
-            });
-        }
-
-        [Fact]
-        public void Prediction_WithSpellCheck()
-        {
-            UseClientFor(async client =>
-            {
-                var utterance = "helo, what dai is todey?";
-                var result = await client.Prediction.ResolveAsync(System.Guid.Parse(appId), utterance, spellCheck: true, bingSpellCheckSubscriptionKey: "00000000000000000000000000000000");
-
-                Assert.True(!string.IsNullOrWhiteSpace(result.AlteredQuery));
-                Assert.Equal("hello, what day is today?", result.AlteredQuery);
-                Assert.Equal(utterance, result.Query);
-            });
-        }
-
-        [Fact]
-        public void Prediction_InvalidKey_ThrowsAPIErrorException()
-        {
-            var headers = new Dictionary<string, List<string>> { ["Ocp-Apim-Subscription-Key"] = new List<string> { "invalid-key" } };
-
-            UseClientFor(async client =>
-            {
-                var ex = await Assert.ThrowsAsync<APIErrorException>(async () =>
+                var requestOptions = new PredictionRequestOptions
                 {
-                    await client.Prediction.ResolveWithHttpMessagesAsync(System.Guid.Parse(appId), "this is a test with post", customHeaders: headers);
+                    DatetimeReference = DateTime.Parse("2019-01-01"),
+                    OverridePredictions = true
+                };
+
+                var externalResolution = JObject.FromObject(new { text = "post", external = true });
+                var externalEntities = new[]
+                {
+                    new ExternalEntity
+                    {
+                        EntityName = "simple",
+                        StartIndex = 26,
+                        EntityLength = 4,
+                        Resolution = externalResolution
+                    }
+                };
+
+                var dynamicLists = new[]
+                {
+                    new DynamicList
+                    {
+                        ListEntityName = "list",
+                        RequestLists = new[]
+                        {
+                            new RequestList
+                            {
+                                Name = "test",
+                                CanonicalForm = "testing",
+                                Synonyms = new[] { "this" }
+                            }
+                        }
+                    }
+                };
+
+                var predictionRequest = new PredictionRequest
+                {
+                    Query = utterance,
+                    Options = requestOptions,
+                    ExternalEntities = externalEntities,
+                    DynamicLists = dynamicLists 
+                };
+
+                var result = await client.Prediction.GetSlotPredictionAsync(
+                    Guid.Parse(appId),
+                    slotName,
+                    predictionRequest,
+                    verbose: true,
+                    showAllIntents: true);
+
+                var prediction = result.Prediction;
+                Assert.Equal(utterance, result.Query);
+                Assert.Equal(utterance, prediction.NormalizedQuery);
+                Assert.Equal("intent", prediction.TopIntent);
+                Assert.Equal(2, prediction.Intents.Count);
+                Assert.Equal(4, prediction.Entities.Count);
+                Assert.Contains("datetimeV2", prediction.Entities.Keys);
+                Assert.Contains("simple", prediction.Entities.Keys);
+                Assert.Contains("list", prediction.Entities.Keys);
+                Assert.Contains("$instance", prediction.Entities.Keys);
+
+                // Test external resolution
+                var actualResolution = (prediction.Entities["simple"] as JArray).Single();
+                Assert.True(JToken.DeepEquals(externalResolution, actualResolution));
+
+                var topIntent = prediction.Intents[prediction.TopIntent];
+                Assert.True(topIntent.Score > 0.5);
+
+                Assert.Equal("positive", prediction.Sentiment.Label);
+                Assert.True(prediction.Sentiment.Score > 0.5);
+
+                // dispatch
+                var child = topIntent.ChildApp;
+                Assert.Equal(utterance, child.NormalizedQuery);
+                Assert.Equal("None", child.TopIntent);
+                Assert.Equal(1, child.Intents.Count);
+                Assert.Equal(2, child.Entities.Count);
+                Assert.Contains("datetimeV2", child.Entities.Keys);
+                Assert.Contains("$instance", child.Entities.Keys);
+
+                var dispatchTopIntent = child.Intents[child.TopIntent];
+                Assert.True(dispatchTopIntent.Score > 0.5);
+
+                Assert.Equal("positive", child.Sentiment.Label);
+                Assert.True(child.Sentiment.Score > 0.5);
+            });
+        }
+
+        [Fact]
+        public void Prediction_Version()
+        {
+            UseClientFor(async client =>
+            {
+                var utterance = "today this is a test with post";
+                var versionId = "0.1";
+
+                var requestOptions = new PredictionRequestOptions
+                {
+                    DatetimeReference = DateTime.Parse("2019-01-01"),
+                    OverridePredictions = true
+                };
+
+                var externalResolution = JObject.FromObject(new { text = "post", external = true });
+                var externalEntities = new[]
+                {
+                    new ExternalEntity
+                    {
+                        EntityName = "simple",
+                        StartIndex = 26,
+                        EntityLength = 4,
+                        Resolution = externalResolution
+                    }
+                };
+
+                var dynamicLists = new[]
+                {
+                    new DynamicList
+                    {
+                        ListEntityName = "list",
+                        RequestLists = new[]
+                        {
+                            new RequestList
+                            {
+                                Name = "test",
+                                CanonicalForm = "testing",
+                                Synonyms = new[] { "this" }
+                            }
+                        }
+                    }
+                };
+
+                var predictionRequest = new PredictionRequest
+                {
+                    Query = utterance,
+                    Options = requestOptions,
+                    ExternalEntities = externalEntities,
+                    DynamicLists = dynamicLists
+                };
+
+                var result = await client.Prediction.GetVersionPredictionAsync(
+                    Guid.Parse(appId),
+                    versionId,
+                    predictionRequest,
+                    verbose: true,
+                    showAllIntents: true);
+
+                var prediction = result.Prediction;
+                Assert.Equal(utterance, result.Query);
+                Assert.Equal(utterance, prediction.NormalizedQuery);
+                Assert.Equal("intent", prediction.TopIntent);
+                Assert.Equal(2, prediction.Intents.Count);
+                Assert.Equal(4, prediction.Entities.Count);
+                Assert.Contains("datetimeV2", prediction.Entities.Keys);
+                Assert.Contains("simple", prediction.Entities.Keys);
+                Assert.Contains("list", prediction.Entities.Keys);
+                Assert.Contains("$instance", prediction.Entities.Keys);
+
+                // Test external resolution
+                var actualResolution = (prediction.Entities["simple"] as JArray).Single();
+                Assert.True(JToken.DeepEquals(externalResolution, actualResolution));
+
+                var topIntent = prediction.Intents[prediction.TopIntent];
+                Assert.True(topIntent.Score > 0.5);
+
+                Assert.Equal("positive", prediction.Sentiment.Label);
+                Assert.True(prediction.Sentiment.Score > 0.5);
+
+                // dispatch
+                var child = topIntent.ChildApp;
+                Assert.Equal(utterance, child.NormalizedQuery);
+                Assert.Equal("None", child.TopIntent);
+                Assert.Equal(1, child.Intents.Count);
+                Assert.Equal(2, child.Entities.Count);
+                Assert.Contains("datetimeV2", child.Entities.Keys);
+                Assert.Contains("$instance", child.Entities.Keys);
+
+                var dispatchTopIntent = child.Intents[child.TopIntent];
+                Assert.True(dispatchTopIntent.Score > 0.5);
+
+                Assert.Equal("positive", child.Sentiment.Label);
+                Assert.True(child.Sentiment.Score > 0.5);
+            });
+        }
+
+        [Fact]
+        public void Prediction_AppNotFound_ThrowsAPIErrorException()
+        {
+            UseClientFor(async client =>
+            {
+                var ex = await Assert.ThrowsAsync<ErrorException>(async () =>
+                {
+                    await client.Prediction.GetSlotPredictionAsync(
+                        Guid.Parse("7555b7c1-e69c-4580-9d95-1abd6dfa8291"),
+                        "production",
+                        new PredictionRequest
+                        {
+                            Query = "this is a test with post"
+                        });
                 });
 
-                Assert.Equal("401", ex.Body.StatusCode);
+                Assert.Equal("NotFound", ex.Body.ErrorProperty.Code);
             });
         }
 
         [Fact]
-        public void Prediction_QueryTooLong_ThrowsValidationException()
+        public void Prediction_NullQuery_ThrowsValidationException()
         {
             UseClientFor(async client =>
             {
-                var query = string.Empty.PadLeft(501, 'x');
                 var ex = await Assert.ThrowsAsync<ValidationException>(async () =>
                 {
-                    await client.Prediction.ResolveWithHttpMessagesAsync(System.Guid.Parse(appId), query);
+                    await client.Prediction.GetSlotPredictionAsync(Guid.Parse(appId), "production", new PredictionRequest());
                 });
 
-                Assert.Equal("query", ex.Target);
+                Assert.Equal("Query", ex.Target);
             });
         }
     }
