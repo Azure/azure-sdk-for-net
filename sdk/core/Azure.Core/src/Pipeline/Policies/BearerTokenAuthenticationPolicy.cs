3e08df02dbab2cf37989f37a545a6a55d2af440c
Pavel Krymets <pavel@krymets.com>
2019-05-07 16:22:53 -0700

    Add TokenCredential and BearerTokenAuthenticationPolicy

---
+++ sdk/core/Azure.Core/src/Pipeline/Policies/BearerTokenAuthenticationPolicy.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azure.Core.Pipeline.Policies
+{
+    public class BearerTokenAuthenticationPolicy: HttpPipelinePolicy
+    {
+        private readonly TokenCredential _credential;
+
+        private readonly string[] _scopes;
+
+        private string _currentToken = null;
+
+        private string _headerValue = null;
+
+        public BearerTokenAuthenticationPolicy(TokenCredential credential, string scope) : this(credential, new []{ scope })
+        {
+        }
+
+        public BearerTokenAuthenticationPolicy(TokenCredential credential, IEnumerable<string> scopes)
+        {
+            if (credential == null)
+            {
+                throw new ArgumentNullException(nameof(credential));
+            }
+
+            if (scopes == null)
+            {
+                throw new ArgumentNullException(nameof(scopes));
+            }
+
+            _credential = credential;
+            _scopes = scopes.ToArray();
+        }
+
+        public override Task ProcessAsync(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            return ProcessAsync(message, pipeline, true);
+        }
+
+        public override void Process(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            ProcessAsync(message, pipeline, false).EnsureCompleted();
+        }
+
+        public async Task ProcessAsync(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline, bool async)
+        {
+            string token = async ?
+                    await _credential.GetTokenAsync(_scopes, message.Cancellation).ConfigureAwait(false) :
+                    _credential.GetTokenAsync(_scopes, message.Cancellation).GetAwaiter().GetResult();
+
+            if (token != _currentToken)
+            {
+                // Avoid per request allocations
+                _currentToken = token;
+                _headerValue = "Bearer " + token;
+            }
+
+            message.Request.SetHeader(HttpHeader.Names.Authorization, _headerValue);
+
+            if (async)
+            {
+                await ProcessNextAsync(pipeline, message);
+            }
+            else
+            {
+                ProcessNext(pipeline, message);
+            }
+        }
+    }
+}
---
+++ sdk/core/Azure.Core/src/TokenCredential.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Core
+{
+    /// <summary>
+    /// Represents a credential capable of providing an OAuth token
+    /// </summary>
+    public abstract class TokenCredential
+    {
+        public abstract ValueTask<string> GetTokenAsync(string[] scopes, CancellationToken cancellationToken);
+    }
+}
