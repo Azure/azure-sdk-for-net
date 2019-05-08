0627193d85729ee0c492015545aca89e3fa76a0e
Scott Schaab <sschaab@microsoft.com>
2019-05-08 02:51:25 -0700

    initial keyvault secrets client library implementation

---
+++ sdk/keyvault/Azure.Security.Keyvault.Secrets/src/Base64Url.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Security.KeyVault.Secrets
+{
+    internal static class Base64Url
+    {
+        public static byte[] Decode(string str)
+        {
+            str = new StringBuilder(str).Replace('-', '+').Replace('_', '/').Append('=', (str.Length % 4 == 0) ? 0 : 4 - (str.Length % 4)).ToString();
+
+            return Convert.FromBase64String(str);
+        }
+
+        public static string Encode(byte[] bytes)
+        {
+            return new StringBuilder(Convert.ToBase64String(bytes)).Replace('+', '-').Replace('/', '_').Replace("=", "").ToString();
+        }
+
+    }
+}
