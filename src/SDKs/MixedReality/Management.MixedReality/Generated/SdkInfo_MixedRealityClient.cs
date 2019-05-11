10db47af2c68ea54bf7026cc9200bf5eda711674
adxsdknet <adxsdknet@outlook.com>
2019-05-11 02:36:01 +0000

    .NET SDK Resource Provider:'MixedReality' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5909' REST Spec PR Author 'XiangyuL-Microsoft' REST Spec PR Last commit

--- src/SDKs/MixedReality/Management.MixedReality/Generated/SdkInfo_MixedRealityClient.cs
+++ src/SDKs/MixedReality/Management.MixedReality/Generated/SdkInfo_MixedRealityClient.cs
@@ -25,16 +25,5 @@ namespace Microsoft.Azure.Management.MixedReality
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/mixedreality/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=D:\\Git\\GitHub\\my\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "547a0b3e90ecab25d6f318fa57db97b35f26f270";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
