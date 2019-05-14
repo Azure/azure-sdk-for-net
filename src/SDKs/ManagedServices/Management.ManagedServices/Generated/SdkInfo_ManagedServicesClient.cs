7b24c6124d36b7810136915d35642ac5b2a7b6ef
adxsdknet <adxsdknet@outlook.com>
2019-05-14 16:56:17 +0000

    .NET SDK Resource Provider:'ManagedServices' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5946' REST Spec PR Author 'ajay-sainy' REST Spec PR Last commit

--- src/SDKs/ManagedServices/Management.ManagedServices/Generated/SdkInfo_ManagedServicesClient.cs
+++ src/SDKs/ManagedServices/Management.ManagedServices/Generated/SdkInfo_ManagedServicesClient.cs
@@ -25,16 +25,5 @@ namespace Microsoft.Azure.Management.ManagedServices
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/managedservices/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\Users\\ajsainy\\Desktop\\Swagger\\SDK\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "aa2e344a3516adf454e8865ea7327c47a864581d";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
