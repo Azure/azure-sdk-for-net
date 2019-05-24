04c3c9aa58f8753d02ad7397b5779447799c32ae
Mariana Rios Flores <mariari@microsoft.com>
2019-05-24 09:34:26 -0700

    API bones

---
+++ sdk/keyvault/Azure.Security.KeyVault.Certificates/src/CertificateClientOptions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using Azure.Core.Pipeline;
+using Azure.Core.Pipeline.Policies;
+using System;
+
+namespace Azure.Security.KeyVault.Certificates
+{
+    public class CertificateClientOptions : HttpClientOptions
+    {
+        public RetryPolicy RetryPolicy { get; set; }
+
+        public CertificateClientOptions()
+        {
+            RetryPolicy = new ExponentialRetryPolicy()
+            {
+                Delay = TimeSpan.FromMilliseconds(800),
+                MaxRetries = 3
+            };
+        }
+    }
+}
