ced452e010420f7ccda75a4c8eff09c4c4d4c1f6
adxsdknet <adxsdknet@outlook.com>
2019-05-24 19:10:13 +0000

    .NET SDK Resource Provider:'Advisor' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/6067' REST Spec PR Author 'mudit794' REST Spec PR Last commit

--- src/SDKs/Advisor/Management.Advisor/Generated/Models/MetadataEntity.cs
+++ src/SDKs/Advisor/Management.Advisor/Generated/Models/MetadataEntity.cs
@@ -40,17 +40,17 @@ namespace Microsoft.Azure.Management.Advisor.Models
         /// <param name="displayName">The display name.</param>
         /// <param name="dependsOn">The list of keys on which this entity
         /// depends on.</param>
-        /// <param name="isAlertable">The flag to know if metadata entity can
-        /// be used in alert condition.</param>
+        /// <param name="applicableScenarios">The list of scenarios applicable
+        /// to this metadata entity.</param>
         /// <param name="supportedValues">The list of supported values.</param>
-        public MetadataEntity(string id = default(string), string type = default(string), string name = default(string), string displayName = default(string), IList<string> dependsOn = default(IList<string>), bool? isAlertable = default(bool?), IList<MetadataSupportedValueDetail> supportedValues = default(IList<MetadataSupportedValueDetail>))
+        public MetadataEntity(string id = default(string), string type = default(string), string name = default(string), string displayName = default(string), IList<string> dependsOn = default(IList<string>), IList<string> applicableScenarios = default(IList<string>), IList<MetadataSupportedValueDetail> supportedValues = default(IList<MetadataSupportedValueDetail>))
         {
             Id = id;
             Type = type;
             Name = name;
             DisplayName = displayName;
             DependsOn = dependsOn;
-            IsAlertable = isAlertable;
+            ApplicableScenarios = applicableScenarios;
             SupportedValues = supportedValues;
             CustomInit();
         }
@@ -91,11 +91,11 @@ namespace Microsoft.Azure.Management.Advisor.Models
         public IList<string> DependsOn { get; set; }
 
         /// <summary>
-        /// Gets or sets the flag to know if metadata entity can be used in
-        /// alert condition.
+        /// Gets or sets the list of scenarios applicable to this metadata
+        /// entity.
         /// </summary>
-        [JsonProperty(PropertyName = "properties.isAlertable")]
-        public bool? IsAlertable { get; set; }
+        [JsonProperty(PropertyName = "properties.applicableScenarios")]
+        public IList<string> ApplicableScenarios { get; set; }
 
         /// <summary>
         /// Gets or sets the list of supported values.
