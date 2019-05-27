decd2fcaaf9676cfa649d0eaa8501c47f5575138
Jesse Squire <jesse.squire@gmail.com>
2019-05-27 08:25:32 -0700

    [Event Hubs Client] Skeleton API Track Two Preview 1 (#6414)
    [Event Hubs Client] Track 2 Prototype
    
    Initial skeleton for project structure and minimal assets.
    
        The package references were preserved from the track one implementation
        in order to allow for the same types to be used in the API surface where
        applicable.  These will be tuned as the prototype is built and the API
        surface becomes more concrete.
    
    
    Incorporation of feedback from the initial architecture board review.
    
        General
          - Replaced "Information" with "Properties" for metadata operations and types.
          - Renamed retry policies to avoid confusion with Azure.Core types, prefixing with `EventHubs` and removing the `Policy` suffix.
          - Renamed `EventPosition.EndOfStream` to `EventPosition.NewEventsOnly`
          - Removed `EventBatch`, used to create a single batch of limited size; to be revisited post-June Preview.
    
        Event Hub Client
          - Renamed `GetEventSender` to `GetSender`
    
        Event Sender
          - Renamed optional timeout to `Timeout` for clarity.
          - Removed the ability to send a single event, per advice from the Event Hubs product team.
          - Restructured `SendAsync` to send all events grouped into a batch, per advice from the Event Hubs product team.
          - Restructured batch options to allow control over the batching strategy applied to the sets of events.
    
        Event Receiver
          - Moved the `EventPosition` into the receiver options and made `NewEventsOnly` as the default
          - Access to the Partition Properties.
    
    
    Incorporation of feedback from the second and third architecture board reviews.
    
            General
              - Removed the concept of `ServiceVersion`, as the Event Hubs service team indicated that
                they will not be supporting multiple versions and will guarantee strong backwards compatibility.
    
              - Revised the names of (internal) properties on EventData that correspond to the event's position in
                the stream, used by receivers for updating checkpoint information when they are populated.  The
                nomenclature inhereited from the track one implementation was unclear as to whether these properties
                were relative to the event or to the partition where the event was located.
    
            Event Hub Client
              - Added documentation to assist in using the expected connection string from the portal.
              - Added a `GetPartitionIds` shortcut method.
    
            Event Receiver
              - Removed the concept of `IsExclusiveReceiver` from the receiver and options; the priority will
                be used as a determination of whether or not a receiver is intended to be exclusive.
    
              - Revised the receiver checkpoint properties to have more clear semantics that indicate
                "this is the last event that the receiver has read" and not the current state of the partition.
    
    Refactoring for better cross-language alignment
    
        Refactoring sending of events to move specifying a partition to the creation of a sender,
        as it was mistakenly believed that a partition could be selected at send;  this was
        the batch label (aka partition key) and not the identifier.
    
        Senders can now be created specific to a partition or non-specific, the latter of which
        will allow the Event Hubs service to assume responsibility for routing of events to a
        partition - the desired majority use case.
    
        Corrected implementation of event properties with respect to positional data
        for checkpoints;  there was a misunderstanding with respect to the information
        available on an event by default and that bound to the "UpdateRuntimeInformation"
        flag from track one.
    
    
    Minor clean-up from code review
    
        - Trimming git metadata dangling file
        - Removing prototype stub implementations in favor of `NotImplementedException` throws.

---
+++ sdk/eventhub/Azure.Messaging.EventHubs/src/Metadata/EventHubProperties.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Azure.Messaging.EventHubs.Metadata
+{
+    /// <summary>
+    ///   A set of information for an Event Hub.
+    /// </summary>
+    ///
+    public sealed class EventHubProperties
+    {
+        /// <summary>
+        ///   The path of the Event Hub, relative to the namespace
+        ///   that contains it.
+        /// </summary>
+        ///
+        public string Path { get; }
+
+        /// <summary>
+        ///   The date and time, in UTC, at which the Event Hub was created.
+        /// </summary>
+        ///
+        public DateTime CreatedDateUtc { get; }
+
+        /// <summary>
+        ///   The number of partitions in the Event Hub.
+        /// </summary>
+        ///
+        public int PartitionCount { get; }
+
+        /// <summary>
+        ///   The set of unique identifiers for each partition in the Event Hub.
+        /// </summary>
+        ///
+        public string[] PartitionIds { get; }
+
+        /// <summary>
+        ///   The date and time, in UTC, that the information was retrieved from the
+        ///   Event Hub.
+        /// </summary>
+        ///
+        public DateTime PropertyRetrievalTimeUtc { get; }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="EventHubProperties"/> class.
+        /// </summary>
+        ///
+        /// <param name="path">The path of the Event Hub.</param>
+        /// <param name="createdUtc">The date and time at which the Event Hub was created.</param>
+        /// <param name="partitionCount">The number of partitions in the Event Hub.</param>
+        /// <param name="partitionIds">The set of unique identifiers for each partition.</param>
+        /// <param name="retrievalTimeUtc">the date and time, in UTC, that the information was retrieved from the service; if not provided, the current date/time will be used.</param>
+        ///
+        internal EventHubProperties(string    path,
+                                    DateTime  createdUtc,
+                                    int       partitionCount,
+                                    string[]  partitionIds,
+                                    DateTime? retrievalTimeUtc = null)
+        {
+            Path = path;
+            CreatedDateUtc = createdUtc;
+            PartitionCount = partitionCount;
+            PartitionIds = partitionIds;
+            PropertyRetrievalTimeUtc = retrievalTimeUtc ?? DateTime.UtcNow;
+        }
+    }
+}
