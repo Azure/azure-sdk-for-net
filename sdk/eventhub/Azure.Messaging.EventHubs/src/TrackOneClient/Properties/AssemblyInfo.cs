b9c6dbd7e8512261e72f6e143a8ca8b34cda53b4
Jesse Squire <jesse.squire@gmail.com>
2019-05-28 18:19:57 -0400

    [Event Hubs Client] Track Two: First Preview     General         - Removed code signing compiler constant, assuming that projects will           be signed, now that keys are owned by the repository.
        Tests and Test Infrastructure
            - Converted prototype skeleton test framework from xUnit to NUnit, to
              conform to the adopted standard for track two client libraries.
    
            - Initial set of test infrastructure for categorizing tests and accessing
              environment variables.
    
            - Tests for the `Guard` class, the first non-prototype implementation for the
              track two preview code.

--- sdk/eventhub/Azure.Messaging.EventHubs/src/TrackOneClient/Properties/AssemblyInfo.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/src/TrackOneClient/Properties/AssemblyInfo.cs
@@ -3,9 +3,5 @@
 
 using System.Runtime.CompilerServices;
 
-#if CODESIGN
 [assembly: InternalsVisibleTo("Azure.Messaging.EventHubs, PublicKey=0024000004800000940000000602000000240000525341310004000001000100097ad52abbeaa2e1a1982747cc0106534f65cfea6707eaed696a3a63daea80de2512746801a7e47f88e7781e71af960d89ba2e25561f70b0e2dbc93319e0af1961a719ccf5a4d28709b2b57a5d29b7c09dc8d269a490ebe2651c4b6e6738c27c5fb2c02469fe9757f0a3479ac310d6588a50a28d7dd431b907fd325e18b9e8ed")]
-#else
-[assembly: InternalsVisibleTo("Azure.Messaging.EventHubs)"]
-#endif
 
