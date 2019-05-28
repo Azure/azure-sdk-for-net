03f9c24cb3fd6fb754a808c0e1ac5eaadde5deea
adxsdknet <adxsdknet@outlook.com>
2019-05-28 18:34:34 +0000

    .NET SDK Resource Provider:'DataMigration' REST Spec PR 'https://github.com/Azure/azure-rest-api-specs/pull/5801' REST Spec PR Author 'huang91shu' REST Spec PR Last commit

--- src/SDKs/DataMigration/Management.DataMigration/Generated/ServiceTasksOperationsExtensions.cs
+++ src/SDKs/DataMigration/Management.DataMigration/Generated/ServiceTasksOperationsExtensions.cs
@@ -82,6 +82,311 @@ namespace Microsoft.Azure.Management.DataMigration
             }
 
             /// <summary>
+            /// Create or update service task
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. The PUT method creates a new service task
+            /// or updates an existing one, although since service tasks have no mutable
+            /// custom properties, there is little reason to update an existing one.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='parameters'>
+            /// Information about the task
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            public static ProjectTask CreateOrUpdate(this IServiceTasksOperations operations, ProjectTask parameters, string groupName, string serviceName, string taskName)
+            {
+                return operations.CreateOrUpdateAsync(parameters, groupName, serviceName, taskName).GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// Create or update service task
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. The PUT method creates a new service task
+            /// or updates an existing one, although since service tasks have no mutable
+            /// custom properties, there is little reason to update an existing one.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='parameters'>
+            /// Information about the task
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            /// <param name='cancellationToken'>
+            /// The cancellation token.
+            /// </param>
+            public static async Task<ProjectTask> CreateOrUpdateAsync(this IServiceTasksOperations operations, ProjectTask parameters, string groupName, string serviceName, string taskName, CancellationToken cancellationToken = default(CancellationToken))
+            {
+                using (var _result = await operations.CreateOrUpdateWithHttpMessagesAsync(parameters, groupName, serviceName, taskName, null, cancellationToken).ConfigureAwait(false))
+                {
+                    return _result.Body;
+                }
+            }
+
+            /// <summary>
+            /// Get service task information
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. The GET method retrieves information
+            /// about a service task.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            /// <param name='expand'>
+            /// Expand the response
+            /// </param>
+            public static ProjectTask Get(this IServiceTasksOperations operations, string groupName, string serviceName, string taskName, string expand = default(string))
+            {
+                return operations.GetAsync(groupName, serviceName, taskName, expand).GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// Get service task information
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. The GET method retrieves information
+            /// about a service task.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            /// <param name='expand'>
+            /// Expand the response
+            /// </param>
+            /// <param name='cancellationToken'>
+            /// The cancellation token.
+            /// </param>
+            public static async Task<ProjectTask> GetAsync(this IServiceTasksOperations operations, string groupName, string serviceName, string taskName, string expand = default(string), CancellationToken cancellationToken = default(CancellationToken))
+            {
+                using (var _result = await operations.GetWithHttpMessagesAsync(groupName, serviceName, taskName, expand, null, cancellationToken).ConfigureAwait(false))
+                {
+                    return _result.Body;
+                }
+            }
+
+            /// <summary>
+            /// Delete service task
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. The DELETE method deletes a service task,
+            /// canceling it first if it's running.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            /// <param name='deleteRunningTasks'>
+            /// Delete the resource even if it contains running tasks
+            /// </param>
+            public static void Delete(this IServiceTasksOperations operations, string groupName, string serviceName, string taskName, bool? deleteRunningTasks = default(bool?))
+            {
+                operations.DeleteAsync(groupName, serviceName, taskName, deleteRunningTasks).GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// Delete service task
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. The DELETE method deletes a service task,
+            /// canceling it first if it's running.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            /// <param name='deleteRunningTasks'>
+            /// Delete the resource even if it contains running tasks
+            /// </param>
+            /// <param name='cancellationToken'>
+            /// The cancellation token.
+            /// </param>
+            public static async Task DeleteAsync(this IServiceTasksOperations operations, string groupName, string serviceName, string taskName, bool? deleteRunningTasks = default(bool?), CancellationToken cancellationToken = default(CancellationToken))
+            {
+                (await operations.DeleteWithHttpMessagesAsync(groupName, serviceName, taskName, deleteRunningTasks, null, cancellationToken).ConfigureAwait(false)).Dispose();
+            }
+
+            /// <summary>
+            /// Create or update service task
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. The PATCH method updates an existing
+            /// service task, but since service tasks have no mutable custom properties,
+            /// there is little reason to do so.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='parameters'>
+            /// Information about the task
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            public static ProjectTask Update(this IServiceTasksOperations operations, ProjectTask parameters, string groupName, string serviceName, string taskName)
+            {
+                return operations.UpdateAsync(parameters, groupName, serviceName, taskName).GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// Create or update service task
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. The PATCH method updates an existing
+            /// service task, but since service tasks have no mutable custom properties,
+            /// there is little reason to do so.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='parameters'>
+            /// Information about the task
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            /// <param name='cancellationToken'>
+            /// The cancellation token.
+            /// </param>
+            public static async Task<ProjectTask> UpdateAsync(this IServiceTasksOperations operations, ProjectTask parameters, string groupName, string serviceName, string taskName, CancellationToken cancellationToken = default(CancellationToken))
+            {
+                using (var _result = await operations.UpdateWithHttpMessagesAsync(parameters, groupName, serviceName, taskName, null, cancellationToken).ConfigureAwait(false))
+                {
+                    return _result.Body;
+                }
+            }
+
+            /// <summary>
+            /// Cancel a service task
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. This method cancels a service task if
+            /// it's currently queued or running.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            public static ProjectTask Cancel(this IServiceTasksOperations operations, string groupName, string serviceName, string taskName)
+            {
+                return operations.CancelAsync(groupName, serviceName, taskName).GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// Cancel a service task
+            /// </summary>
+            /// <remarks>
+            /// The service tasks resource is a nested, proxy-only resource representing
+            /// work performed by a DMS instance. This method cancels a service task if
+            /// it's currently queued or running.
+            /// </remarks>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='groupName'>
+            /// Name of the resource group
+            /// </param>
+            /// <param name='serviceName'>
+            /// Name of the service
+            /// </param>
+            /// <param name='taskName'>
+            /// Name of the Task
+            /// </param>
+            /// <param name='cancellationToken'>
+            /// The cancellation token.
+            /// </param>
+            public static async Task<ProjectTask> CancelAsync(this IServiceTasksOperations operations, string groupName, string serviceName, string taskName, CancellationToken cancellationToken = default(CancellationToken))
+            {
+                using (var _result = await operations.CancelWithHttpMessagesAsync(groupName, serviceName, taskName, null, cancellationToken).ConfigureAwait(false))
+                {
+                    return _result.Body;
+                }
+            }
+
+            /// <summary>
             /// Get service level tasks for a service
             /// </summary>
             /// <remarks>
