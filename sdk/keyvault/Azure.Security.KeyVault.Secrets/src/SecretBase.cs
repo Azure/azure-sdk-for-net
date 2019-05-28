8260b91c910fb63252b73644ed670b2c9cf54c6a
Mariana Rios Flores <mariari@microsoft.com>
2019-05-28 09:45:25 -0700

    Small clean up of secrets project (#6408)
    * small clean up
    
    * update folder in a way git understands

--- sdk/keyvault/Azure.Security.KeyVault.Secrets/src/SecretBase.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Secrets/src/SecretBase.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using System;
 using System.Collections.Generic;
 using System.Text.Json;
 
--- sdk/keyvault/Azure.Security.KeyVault.Secrets/src/VaultBackup.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Secrets/src/VaultBackup.cs
@@ -1,4 +1,8 @@
-﻿using System.Text.Json;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using System.Text.Json;
 
 namespace Azure.Security.KeyVault.Secrets
 {
