e2a0f35aa75152e74107e70643277563b0f19278
Mariana Rios Flores <mariari@microsoft.com>
2019-05-28 10:47:59 -0700

    add copyright to Keys

--- sdk/keyvault/Azure.Security.KeyVault.Keys/src/JsonWebKeyType.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/src/JsonWebKeyType.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using System.Collections.Generic;
 
 namespace Azure.Security.KeyVault.Keys
 {
--- sdk/keyvault/Azure.Security.KeyVault.Keys/src/KeyCreateOptions.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/src/KeyCreateOptions.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+using System;
 using System.Collections.Generic;
 
 namespace Azure.Security.KeyVault.Keys
