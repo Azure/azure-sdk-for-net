ebd1308ceb00d6f83cea6d39626bd1fc9c84e5c7
Yateng Hong <yatengh@microsoft.com>
2019-05-29 14:02:27 +0800

    Add new unit test.

--- sdk/cognitiveservices/Vision.ComputerVision/tests/VisionBatchReadFileTests.cs
+++ sdk/cognitiveservices/Vision.ComputerVision/tests/VisionBatchReadFileTests.cs
@@ -61,6 +61,7 @@ namespace ComputerVisionSDK.Tests
                     Assert.Equal(1, recognitionResult.Page);
                     Assert.Equal(250, recognitionResult.Width);
                     Assert.Equal(258, recognitionResult.Height);
+                    Assert.Equal(TextRecognitionResultDimensionUnit.Pixel, recognitionResult.Unit);
 
                     Assert.Equal(
                         new string[] { "520", "WEST", "Seattle" }.OrderBy(t => t),
@@ -100,6 +101,7 @@ namespace ComputerVisionSDK.Tests
                     Assert.Equal(1, recognitionResult.Page);
                     Assert.Equal(1000, recognitionResult.Width);
                     Assert.Equal(664, recognitionResult.Height);
+                    Assert.Equal(TextRecognitionResultDimensionUnit.Pixel, recognitionResult.Unit);
 
                     Assert.Equal(
                         new string[] { "you must be the change", "you want to see in the world!" }.OrderBy(t => t),
@@ -107,6 +109,41 @@ namespace ComputerVisionSDK.Tests
                     Assert.Equal(2, recognitionResult.Lines.Count);
                     Assert.Equal(5, recognitionResult.Lines[0].Words.Count);
                     Assert.Equal(7, recognitionResult.Lines[1].Words.Count);
+                }
+            }
+        }
+
+        [Fact(Skip = "https://github.com/Azure/azure-sdk-for-net/issues/6214")]
+        public void BatchReadPdfFileInStreamTest()
+        {
+            using (MockContext context = MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "BatchReadPdfFileInStreamTest");
+
+                using (IComputerVisionClient client = GetComputerVisionClient(HttpMockServer.CreateInstance()))
+                using (FileStream stream = new FileStream(GetTestImagePath("menu.pdf"), FileMode.Open))
+                {
+                    BatchReadFileInStreamHeaders headers = client.BatchReadFileInStreamAsync(stream).Result;
+
+                    Assert.NotNull(headers.OperationLocation);
+
+                    ReadOperationResult readOperationResult = GetRecognitionResultWithPolling(client, headers.OperationLocation);
+
+                    Assert.NotNull(readOperationResult);
+                    Assert.Equal(TextOperationStatusCodes.Succeeded, readOperationResult.Status);
+
+                    Assert.NotNull(readOperationResult.RecognitionResults);
+                    Assert.Equal(1, readOperationResult.RecognitionResults.Count);
+
+                    var recognitionResult = readOperationResult.RecognitionResults[0];
+
+                    Assert.Equal(1, recognitionResult.Page);
+                    Assert.Equal(8.5, recognitionResult.Width);
+                    Assert.Equal(11, recognitionResult.Height);
+                    Assert.Equal(TextRecognitionResultDimensionUnit.Inch, recognitionResult.Unit);
+
+                    Assert.Equal(28, recognitionResult.Lines.Count);
+                    Assert.Equal("Microsoft", recognitionResult.Lines[0].Text);
                 }
             }
         }
