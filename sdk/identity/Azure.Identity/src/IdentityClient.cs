dc6c88be9ec348eabe17041e02e2b9c206bc486e
Scott Schaab <sschaab@microsoft.com>
2019-05-29 16:52:50 -0700

    removing unused internal class FixedSizeBufferWriter

--- sdk/identity/Azure.Identity/src/IdentityClient.cs
+++ sdk/identity/Azure.Identity/src/IdentityClient.cs
@@ -123,32 +123,5 @@ namespace Azure.Identity
 
             return new AccessToken(accessToken, expiresOn);
         }
-
-        // TODO (pri 3): CoreFx will soon have a type like this. We should remove this one then.
-        internal class FixedSizedBufferWriter : IBufferWriter<byte>
-        {
-            private readonly byte[] _buffer;
-            private int _count;
-
-            public FixedSizedBufferWriter(byte[] buffer)
-            {
-                _buffer = buffer;
-            }
-
-            public Memory<byte> GetMemory(int minimumLength = 0) => _buffer.AsMemory(_count);
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public Span<byte> GetSpan(int minimumLength = 0) => _buffer.AsSpan(_count);
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void Advance(int bytes)
-            {
-                _count += bytes;
-                if (_count > _buffer.Length)
-                {
-                    throw new InvalidOperationException("Cannot advance past the end of the buffer.");
-                }
-            }
-        }
     }
 }
