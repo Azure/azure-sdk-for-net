f8c8b84ce4caae91997e1295e02c2927b72ea078
Mariana Rios Flores <mariari@microsoft.com>
2019-06-06 14:44:38 -0700

    Async Keys implementation (#6457)

--- sdk/keyvault/Azure.Security.KeyVault.Keys/src/KeyOperations.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/src/KeyOperations.cs
@@ -19,4 +19,42 @@ namespace Azure.Security.KeyVault.Keys
 
         All = uint.MaxValue
     }
+
+    public static class KeyOperationsExtensions
+    {
+        public static KeyOperations ParseFromString(string value)
+        {
+            switch (value)
+            {
+                case "encrypt": return KeyOperations.Encrypt;
+                case "decrypt": return KeyOperations.Decrypt;
+                case "sign": return KeyOperations.Sign;
+                case "verify": return KeyOperations.Verify;
+                case "wrapKey": return KeyOperations.Wrap;
+                case "unwrapKey": return KeyOperations.Unwrap;
+                default: return KeyOperations.Other;
+            }
+        }
+
+        public static string AsString(KeyOperations keyType)
+        {
+            switch (keyType)
+            {
+                case KeyOperations.Encrypt:
+                    return "encrypt";
+                case KeyOperations.Decrypt:
+                    return "decrypt";
+                case KeyOperations.Sign:
+                    return "sign";
+                case KeyOperations.Verify:
+                    return "verify";
+                case KeyOperations.Wrap:
+                    return "wrapKey";
+                case KeyOperations.Unwrap:
+                    return "unwrapKey";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
 }
