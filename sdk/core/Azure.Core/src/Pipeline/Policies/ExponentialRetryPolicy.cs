5b1d93d295383bacd7ff88af0bb9e0a5fb78aa74
Pavel Krymets <pavel@krymets.com>
2019-06-07 13:53:02 -0700

    Extra namespace

--- sdk/core/Azure.Core/src/Pipeline/Policies/ExponentialRetryPolicy.cs
+++ sdk/core/Azure.Core/src/Pipeline/Policies/ExponentialRetryPolicy.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Linq;
-
 namespace Azure.Core.Pipeline.Policies
 {
     public enum RetryMode
