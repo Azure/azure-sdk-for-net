3838f26b0fbb7dc04d2a6bfa843edd10254b8eee
Pavel Krymets <pavel@krymets.com>
2019-06-07 13:49:17 -0700

    Doc

--- sdk/core/Azure.Core/src/Pipeline/Policies/RetryPolicy.cs
+++ sdk/core/Azure.Core/src/Pipeline/Policies/RetryPolicy.cs
@@ -21,18 +21,21 @@ namespace Azure.Core.Pipeline.Policies
         /// <summary>
         /// Gets or sets the maximum number of retry attempts before giving up.
         /// </summary>
-        public int MaxRetries { get; set; } = 10;
+        public int MaxRetries { get; set; } = 3;
 
         /// <summary>
-        /// Gets or sets the timespan used as a base for exponential backoff.
+        /// Gets or sets the timespan used as delay between the retries or as a base for exponential backoff.
         /// </summary>
-        public TimeSpan Delay { get; set; } = TimeSpan.FromSeconds(1);
+        public TimeSpan Delay { get; set; } = TimeSpan.FromSeconds(0.8);
 
         /// <summary>
         /// Gets or sets maximum timespan to pause between requests.
         /// </summary>
         public TimeSpan MaxDelay { get; set; } = TimeSpan.FromMinutes(1);
 
+        /// <summary>
+        /// Gets os sets retry mode
+        /// </summary>
         public RetryMode Mode { get; set; } = RetryMode.Fixed;
 
         public override void Process(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
