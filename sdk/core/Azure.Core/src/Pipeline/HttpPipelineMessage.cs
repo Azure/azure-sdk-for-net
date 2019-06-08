393dec06158d4d4e93e1b0b0a344a2addef033f1
Pavel Krymets <pavel@krymets.com>
2019-06-07 17:10:32 -0700

    Add TryGetProperty and SetProperty to HttpPipelineMessage (#6518)

--- sdk/core/Azure.Core/src/Pipeline/HttpPipelineMessage.cs
+++ sdk/core/Azure.Core/src/Pipeline/HttpPipelineMessage.cs
@@ -1,12 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Azure.Core.Pipeline
 {
     public class HttpPipelineMessage
     {
+        private Dictionary<string, object> _properties;
+
         public CancellationToken CancellationToken { get; }
 
         public HttpPipelineMessage(CancellationToken cancellationToken)
@@ -19,5 +22,18 @@ namespace Azure.Core.Pipeline
         public Response Response { get; set; }
 
         public ResponseClassifier ResponseClassifier { get; set; }
+
+        public bool TryGetProperty(string name, out object value)
+        {
+            value = null;
+            return _properties?.TryGetValue(name, out value) == true;
+        }
+
+        public void SetProperty(string name, object value)
+        {
+            _properties ??= new Dictionary<string, object>();
+
+            _properties[name] = value;
+        }
     }
 }
