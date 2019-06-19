e73d31d0be6c908b119f04bce53a7bf79acce3cb
Scott Schaab <sschaab@microsoft.com>
2019-06-19 10:58:30 -0700

    Adding ClientCertificateCredential to Azure.Idenity

--- sdk/identity/Azure.Identity/src/IdentityClient.cs
+++ sdk/identity/Azure.Identity/src/IdentityClient.cs
@@ -9,6 +9,8 @@ using System.Buffers;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json;
 using System.Threading;
@@ -23,7 +25,8 @@ namespace Azure.Identity
         private readonly IdentityClientOptions _options;
         private readonly HttpPipeline _pipeline;
         private readonly Uri ImdsEndptoint = new Uri("http://169.254.169.254/metadata/identity/oauth2/token");
-        private readonly string MsiApiVersion = "2018-02-01";
+        private const string MsiApiVersion = "2018-02-01";
+        private const string ClientAssertionType = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
 
         public IdentityClient(IdentityClientOptions options = null)
         {
@@ -69,6 +72,39 @@ namespace Azure.Identity
             }
         }
 
+        public virtual async Task<AccessToken> AuthenticateAsync(string tenantId, string clientId, X509Certificate2 clientCertificate, string[] scopes, CancellationToken cancellationToken = default)
+        {
+            using (Request request = CreateClientCertificateAuthRequest(tenantId, clientId, clientCertificate, scopes))
+            {
+                var response = await _pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+
+                if (response.Status == 200 || response.Status == 201)
+                {
+                    var result = await DeserializeAsync(response.ContentStream, cancellationToken).ConfigureAwait(false);
+
+                    return new Response<AccessToken>(response, result);
+                }
+
+                throw await response.CreateRequestFailedExceptionAsync();
+            }
+        }
+
+        public virtual AccessToken Authenticate(string tenantId, string clientId, X509Certificate2 clientCertificate, string[] scopes, CancellationToken cancellationToken = default)
+        {
+            using (Request request = CreateClientCertificateAuthRequest(tenantId, clientId, clientCertificate, scopes))
+            {
+                var response = _pipeline.SendRequest(request, cancellationToken);
+
+                if (response.Status == 200 || response.Status == 201)
+                {
+                    var result = Deserialize(response.ContentStream);
+
+                    return new Response<AccessToken>(response, result);
+                }
+
+                throw response.CreateRequestFailedException();
+            }
+        }
         public virtual async Task<AccessToken> AuthenticateManagedIdentityAsync(string[] scopes, string clientId = null, CancellationToken cancellationToken = default)
         {
             using (Request request = CreateManagedIdentityAuthRequest(scopes, clientId))
@@ -150,6 +186,77 @@ namespace Azure.Identity
             request.Content = HttpPipelineRequestContent.Create(content);
 
             return request;
+        }
+
+        private Request CreateClientCertificateAuthRequest(string tenantId, string clientId, X509Certificate2 clientCertficate, string[] scopes)
+        {
+            Request request = _pipeline.CreateRequest();
+
+            request.Method = HttpPipelineMethod.Post;
+
+            request.Headers.SetValue("Content-Type", "application/x-www-form-urlencoded");
+
+            request.UriBuilder.Uri = _options.AuthorityHost;
+
+            request.UriBuilder.AppendPath(tenantId);
+
+            request.UriBuilder.AppendPath("/oauth2/v2.0/token");
+
+            string clientAssertion = CreateClientAssertionJWT(clientId, request.UriBuilder.ToString(), clientCertficate);
+
+            var bodyStr = $"response_type=token&grant_type=client_credentials&client_id={Uri.EscapeDataString(clientId)}&client_assertion_type={Uri.EscapeDataString(ClientAssertionType)}&client_assertion={Uri.EscapeDataString(clientAssertion)}&scope={Uri.EscapeDataString(string.Join(" ", scopes))}";
+
+            ReadOnlyMemory<byte> content = Encoding.UTF8.GetBytes(bodyStr).AsMemory();
+
+            request.Content = HttpPipelineRequestContent.Create(content);
+
+            return request;
+        }
+
+        private string CreateClientAssertionJWT(string clientId, string audience, X509Certificate2 clientCertificate)
+        {
+            var headerBuff = new ArrayBufferWriter<byte>();
+
+            using (var headerJson = new Utf8JsonWriter(headerBuff))
+            {
+                headerJson.WriteStartObject();
+
+                headerJson.WriteString("typ", "JWT");
+                headerJson.WriteString("alg", "RS256");
+                headerJson.WriteString("x5t", Base64Url.HexToBase64Url(clientCertificate.Thumbprint));
+
+                headerJson.WriteEndObject();
+
+                headerJson.Flush();
+            }
+
+            var payloadBuff = new ArrayBufferWriter<byte>();
+
+            using (var payloadJson = new Utf8JsonWriter(payloadBuff))
+            {
+                payloadJson.WriteStartObject();
+
+                payloadJson.WriteString("jti", Guid.NewGuid());
+                payloadJson.WriteString("aud", audience);
+                payloadJson.WriteString("iss", clientId);
+                payloadJson.WriteString("sub", clientId);
+                payloadJson.WriteNumber("nbf", DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+                payloadJson.WriteNumber("exp", (DateTimeOffset.UtcNow + TimeSpan.FromMinutes(30)).ToUnixTimeSeconds());
+
+                payloadJson.WriteEndObject();
+
+                payloadJson.Flush();
+            }
+
+            string header = Base64Url.Encode(headerBuff.WrittenMemory.ToArray());
+
+            string payload = Base64Url.Encode(payloadBuff.WrittenMemory.ToArray());
+
+            string flattenedJws = header + "." + payload;
+
+            byte[] signature = clientCertificate.GetRSAPrivateKey().SignData(Encoding.ASCII.GetBytes(flattenedJws), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+            return flattenedJws + "." + Base64Url.Encode(signature);
         }
 
         private async Task<AccessToken> DeserializeAsync(Stream content, CancellationToken cancellationToken)
