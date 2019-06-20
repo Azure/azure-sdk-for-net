582c0e80bfd71b05a5c9a0f38720c7e4e1d528f6
Caio Saldanha <camaiaor@microsoft.com>
2019-06-20 16:50:27 -0700

    Final tests added

--- sdk/eventhub/Azure.Messaging.EventHubs/tests/EventHubProducer/EventHubProducerLiveTests.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/tests/EventHubProducer/EventHubProducerLiveTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -485,6 +486,347 @@ namespace Azure.Messaging.EventHubs.Tests
                     {
                         Assert.That(async () => await producer.SendAsync(events), Throws.TypeOf<ArgumentOutOfRangeException>());
                     }
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Verifies that the <see cref="EventHubProducer" /> is able to
+        ///   connect to the Event Hubs service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        public async Task SendUpdatesPartitionProperties()
+        {
+            await using (var scope = await EventHubScope.CreateAsync(1))
+            {
+                var connectionString = TestEnvironment.BuildConnectionStringForEventHub(scope.EventHubName);
+
+                await using (var client = new EventHubClient(connectionString))
+                {
+                    var partition = (await client.GetPartitionIdsAsync()).First();
+                    var events = new[] { new EventData(Encoding.UTF8.GetBytes("I should update stuff")) };
+
+                    await using (var producer = client.CreateProducer(new EventHubProducerOptions { PartitionId = partition }))
+                    {
+                        // Sending events beforehand so the partition has some information
+
+                        await producer.SendAsync(events);
+
+                        var oldPartitionProperties = await client.GetPartitionPropertiesAsync(partition);
+
+                        Assert.That(oldPartitionProperties, Is.Not.Null, "A set of partition properties should have been returned.");
+
+                        await producer.SendAsync(events);
+
+                        var newPartitionProperties = await client.GetPartitionPropertiesAsync(partition);
+
+                        Assert.That(newPartitionProperties, Is.Not.Null, "A set of partition properties should have been returned.");
+
+                        // The following properties should not have been altered
+
+                        Assert.That(newPartitionProperties.Id, Is.EqualTo(oldPartitionProperties.Id));
+                        Assert.That(newPartitionProperties.EventHubPath, Is.EqualTo(oldPartitionProperties.EventHubPath));
+                        Assert.That(newPartitionProperties.BeginningSequenceNumber, Is.EqualTo(oldPartitionProperties.BeginningSequenceNumber));
+
+                        // The following properties should have been updated
+
+                        Assert.That(newPartitionProperties.LastEnqueuedSequenceNumber, Is.GreaterThan(oldPartitionProperties.LastEnqueuedSequenceNumber));
+                        Assert.That(newPartitionProperties.LastEnqueuedOffset, Is.GreaterThan(oldPartitionProperties.LastEnqueuedOffset));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Verifies that the <see cref="EventHubProducer" /> is able to
+        ///   connect to the Event Hubs service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        public async Task SendDoesNotUpdatePartitionPropertiesWhenSendingToDifferentPartition()
+        {
+            await using (var scope = await EventHubScope.CreateAsync(2))
+            {
+                var connectionString = TestEnvironment.BuildConnectionStringForEventHub(scope.EventHubName);
+
+                await using (var client = new EventHubClient(connectionString))
+                {
+                    var partitionIds = await client.GetPartitionIdsAsync();
+                    var events = new[] { new EventData(Encoding.UTF8.GetBytes("I should not update stuff")) };
+
+                    await using (var producer0 = client.CreateProducer(new EventHubProducerOptions { PartitionId = partitionIds[0] }))
+                    await using (var producer1 = client.CreateProducer(new EventHubProducerOptions { PartitionId = partitionIds[1] }))
+                    {
+                        // Sending events beforehand so the partition has some information
+
+                        await producer0.SendAsync(events);
+
+                        var oldPartitionProperties = await client.GetPartitionPropertiesAsync(partitionIds[0]);
+
+                        Assert.That(oldPartitionProperties, Is.Not.Null, "A set of partition properties should have been returned.");
+
+                        await producer1.SendAsync(events);
+
+                        var newPartitionProperties = await client.GetPartitionPropertiesAsync(partitionIds[0]);
+
+                        Assert.That(newPartitionProperties, Is.Not.Null, "A set of partition properties should have been returned.");
+
+                        // All properties should remain the same
+
+                        Assert.That(newPartitionProperties.Id, Is.EqualTo(oldPartitionProperties.Id));
+                        Assert.That(newPartitionProperties.EventHubPath, Is.EqualTo(oldPartitionProperties.EventHubPath));
+                        Assert.That(newPartitionProperties.BeginningSequenceNumber, Is.EqualTo(oldPartitionProperties.BeginningSequenceNumber));
+                        Assert.That(newPartitionProperties.LastEnqueuedSequenceNumber, Is.EqualTo(oldPartitionProperties.LastEnqueuedSequenceNumber));
+                        Assert.That(newPartitionProperties.LastEnqueuedOffset, Is.EqualTo(oldPartitionProperties.LastEnqueuedOffset));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Verifies that the <see cref="EventHubProducer" /> is able to
+        ///   connect to the Event Hubs service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task ProducerDoesNotSendToSpecificPartitionWhenPartitionIdIsNotSpecified(bool nullPartition)
+        {
+            var partitions = 10;
+
+            await using (var scope = await EventHubScope.CreateAsync(partitions))
+            {
+                var connectionString = TestEnvironment.BuildConnectionStringForEventHub(scope.EventHubName);
+
+                await using (var client = new EventHubClient(connectionString))
+                {
+                    var producerOptions = new EventHubProducerOptions { };
+
+                    if (nullPartition)
+                    {
+                        producerOptions.PartitionId = null;
+                    }
+
+                    await using (var producer = client.CreateProducer(producerOptions))
+                    {
+                        var batches = 30;
+                        var partitionIds = await client.GetPartitionIdsAsync();
+                        var partitionsCount = 0;
+                        var consumers = new List<EventHubConsumer>();
+
+                        try
+                        {
+                            for (var index = 0; index < partitions; index++)
+                            {
+                                consumers.Add(client.CreateConsumer(partitionIds[index], EventPosition.Latest));
+
+                                // Initiate an operation to force the consumer to connect and set its position at the
+                                // end of the event stream.
+
+                                await consumers[index].ReceiveAsync(1, TimeSpan.Zero);
+                            }
+
+                            // Send the batches of events.
+
+                            for (var index = 0; index < batches; index++)
+                            {
+                                await producer.SendAsync(new EventData(Encoding.UTF8.GetBytes("It's not healthy to send so many messages")));
+                            }
+
+                            // Receive the events; because there is some non-determinism in the messaging flow, the
+                            // sent events may not be immediately available.  Allow for a small number of attempts to receive, in order
+                            // to account for availability delays.
+
+                            foreach (var consumer in consumers)
+                            {
+                                var receivedEvents = new List<EventData>();
+                                var index = 0;
+
+                                while (++index < 5)
+                                {
+                                    receivedEvents.AddRange(await consumer.ReceiveAsync(batches + 10, TimeSpan.FromMilliseconds(25)));
+                                }
+
+                                if (receivedEvents.Count() > 0)
+                                {
+                                    partitionsCount++;
+                                }
+                            }
+                        }
+                        finally
+                        {
+                            foreach (var consumer in consumers)
+                            {
+                                consumer.Close();
+                            }
+                        }
+
+                        Assert.That(partitionsCount, Is.GreaterThan(1));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Verifies that the <see cref="EventHubProducer" /> is able to
+        ///   connect to the Event Hubs service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        public async Task ProducerSendsEventsInTheSameBatchToTheSamePartition()
+        {
+            var partitions = 10;
+
+            await using (var scope = await EventHubScope.CreateAsync(partitions))
+            {
+                var connectionString = TestEnvironment.BuildConnectionStringForEventHub(scope.EventHubName);
+
+                await using (var client = new EventHubClient(connectionString))
+                await using (var producer = client.CreateProducer())
+                {
+                    var eventBatch = Enumerable
+                        .Range(0, 30)
+                        .Select(index => new EventData(Encoding.UTF8.GetBytes("I'm getting used to this amount of messages")));
+
+                    var partitionIds = await client.GetPartitionIdsAsync();
+                    var partitionsCount = 0;
+                    var receivedEventsCount = 0;
+                    var consumers = new List<EventHubConsumer>();
+
+                    try
+                    {
+                        for (var index = 0; index < partitions; index++)
+                        {
+                            consumers.Add(client.CreateConsumer(partitionIds[index], EventPosition.Latest));
+
+                            // Initiate an operation to force the consumer to connect and set its position at the
+                            // end of the event stream.
+
+                            await consumers[index].ReceiveAsync(1, TimeSpan.Zero);
+                        }
+
+                        // Send the batch of events.
+
+                        await producer.SendAsync(eventBatch);
+
+                        // Receive the events; because there is some non-determinism in the messaging flow, the
+                        // sent events may not be immediately available.  Allow for a small number of attempts to receive, in order
+                        // to account for availability delays.
+
+                        foreach (var consumer in consumers)
+                        {
+                            var receivedEvents = new List<EventData>();
+                            var index = 0;
+
+                            while (++index < 5)
+                            {
+                                receivedEvents.AddRange(await consumer.ReceiveAsync(eventBatch.Count() + 10, TimeSpan.FromMilliseconds(25)));
+                            }
+
+                            if (receivedEvents.Count() > 0)
+                            {
+                                partitionsCount++;
+                                receivedEventsCount += receivedEvents.Count();
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        foreach (var consumer in consumers)
+                        {
+                            consumer.Close();
+                        }
+                    }
+
+                    Assert.That(partitionsCount, Is.EqualTo(1));
+                    Assert.That(receivedEventsCount, Is.EqualTo(eventBatch.Count()));
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Verifies that the <see cref="EventHubProducer" /> is able to
+        ///   connect to the Event Hubs service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        public async Task ProducerSendsEventsWithTheSamePartitionHashKeyToTheSamePartition()
+        {
+            var partitions = 10;
+            var partitionKey = "some123key-!d";
+
+            await using (var scope = await EventHubScope.CreateAsync(partitions))
+            {
+                var connectionString = TestEnvironment.BuildConnectionStringForEventHub(scope.EventHubName);
+
+                await using (var client = new EventHubClient(connectionString))
+                await using (var producer = client.CreateProducer())
+                {
+                    var batches = 5;
+                    var partitionIds = await client.GetPartitionIdsAsync();
+                    var partitionsCount = 0;
+                    var receivedEventsCount = 0;
+                    var consumers = new List<EventHubConsumer>();
+
+                    try
+                    {
+                        for (var index = 0; index < partitions; index++)
+                        {
+                            consumers.Add(client.CreateConsumer(partitionIds[index], EventPosition.Latest));
+
+                            // Initiate an operation to force the consumer to connect and set its position at the
+                            // end of the event stream.
+
+                            await consumers[index].ReceiveAsync(1, TimeSpan.Zero);
+                        }
+
+                        // Send the batches of events.
+
+                        var batchOptions = new SendOptions { PartitionKey = partitionKey };
+
+                        for (var index = 0; index < batches; index++)
+                        {
+                            await producer.SendAsync(new EventData(Encoding.UTF8.GetBytes($"Just a few messages ({ index })")), batchOptions);
+                        }
+
+                        // Receive the events; because there is some non-determinism in the messaging flow, the
+                        // sent events may not be immediately available.  Allow for a small number of attempts to receive, in order
+                        // to account for availability delays.
+
+                        foreach (var consumer in consumers)
+                        {
+                            var receivedEvents = new List<EventData>();
+                            var index = 0;
+
+                            while (++index < 5)
+                            {
+                                receivedEvents.AddRange(await consumer.ReceiveAsync(batches + 10, TimeSpan.FromMilliseconds(25)));
+                            }
+
+                            if (receivedEvents.Count() > 0)
+                            {
+                                partitionsCount++;
+                                receivedEventsCount += receivedEvents.Count();
+
+                                foreach (var receivedEvent in receivedEvents)
+                                {
+                                    Assert.That(receivedEvent.PartitionKey, Is.EqualTo(partitionKey));
+                                }
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        foreach (var consumer in consumers)
+                        {
+                            consumer.Close();
+                        }
+                    }
+
+                    Assert.That(partitionsCount, Is.EqualTo(1));
+                    Assert.That(receivedEventsCount, Is.EqualTo(batches));
                 }
             }
         }
