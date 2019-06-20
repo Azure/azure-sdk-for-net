4799ddbb7394e8f8b029eec8a482f656c8b7115f
Krzysztof Cwalina <kcwalina@microsoft.com>
2019-06-20 14:16:56 -0700

    Added Option<T> type for LROs

---
+++ sdk/core/Azure.Core/tests/OperationTests.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core.Testing;
+using Azure.Core.Tests.TestFramework;
+using NUnit;
+using NUnit.Framework;
+
+namespace Azure.Core.Tests
+{
+    public class OperationTests
+    {
+        [Test]
+        public async Task WaitCompletionAsync()
+        {
+            int updateCalled = 0;
+            var testResult = 100;
+            var testResponse = new MockResponse(200);
+
+            var operation = new TestOperation<int>(TimeSpan.FromMilliseconds(10), testResult, testResponse);
+            operation.PollingInterval = TimeSpan.FromMilliseconds(1);
+            operation.UpdateCalled = () => { updateCalled++; };
+
+            Response<int> operationResult = await operation.WaitCompletionAsync();
+
+            Assert.Greater(updateCalled, 0);
+            Assert.IsTrue(operation.HasCompleted);
+            Assert.IsTrue(operation.HasValue);
+
+            Assert.AreEqual(testResult, operationResult.Value);
+            Assert.AreEqual(testResponse, operationResult.GetRawResponse());
+
+            Assert.AreEqual(testResult, operation.Value);
+            Assert.AreEqual(testResponse, operation.GetRawResponse());
+        }
+
+        [Test]
+        public async Task UpdateStatusAsync()
+        {
+            int updateCalled = 0;
+            var testResult = 100;
+            var testResponse = new MockResponse(200);
+
+            var operation = new TestOperation<int>(TimeSpan.FromMilliseconds(10), testResult, testResponse);
+            operation.UpdateCalled = () => { updateCalled++; };
+
+            while (!operation.HasValue)
+            {
+                Response updateResponse = await operation.UpdateStatusAsync();
+                await Task.Delay(1);
+            }
+
+            Assert.Greater(updateCalled, 0);
+            Assert.IsTrue(operation.HasCompleted);
+            Assert.IsTrue(operation.HasValue);
+
+            Assert.AreEqual(testResult, operation.Value);
+            Assert.AreEqual(testResponse, operation.GetRawResponse());
+        }
+
+        [Test]
+        public void UpdateStatus()
+        {
+            int updateCalled = 0;
+            var testResult = 10;
+            var testResponse = new MockResponse(200);
+
+            var operation = new TestOperation<int>(TimeSpan.FromMilliseconds(10), testResult, testResponse);
+            operation.UpdateCalled = () => { updateCalled++; };
+
+            while (!operation.HasValue)
+            {
+                Response updateResponse = operation.UpdateStatus();
+                Thread.Sleep(1);
+            }
+
+            Assert.Greater(updateCalled, 0);
+            Assert.IsTrue(operation.HasCompleted);
+            Assert.IsTrue(operation.HasValue);
+
+            Assert.AreEqual(testResult, operation.Value);
+            Assert.AreEqual(testResponse, operation.GetRawResponse());
+        }
+    }
+}
+
