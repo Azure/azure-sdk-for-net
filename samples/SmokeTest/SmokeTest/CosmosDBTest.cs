3fff1190bbb6a47da2b51a06955a8e21289f1557
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2019-06-26 16:08:26 -0700

    Cosmos DB implementation
    Tests done with Mirosoft.Azure.DocumentDB SDK.

--- samples/SmokeTest/SmokeTest/CosmosDBTest.cs
+++ samples/SmokeTest/SmokeTest/CosmosDBTest.cs
@@ -3,6 +3,7 @@ using Microsoft.Azure.Documents.Client;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using System.Reflection.Metadata;
 using System.Runtime.ConstrainedExecution;
@@ -15,7 +16,7 @@ namespace SmokeTest
     {
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
-        public bool GassyPlanet { get; set; }
+        public bool HasRings { get; set; }
         public int Radius { get; set; }
         public Moon[] Moons { get; set; }
         public override string ToString()
@@ -29,77 +30,92 @@ namespace SmokeTest
         public string Name { get; set; }
     }
 
-    class CosmosDBTest
+
+    class CosmosDBTest : TestBase
     {
         private DocumentClient client;
-        
+        private Planet planetEarth;
+        private Planet planetMars;
+
         public CosmosDBTest(string endpoint, string authKey)
         {
             client = new DocumentClient(new Uri(endpoint), authKey);
         }
 
+        /// <summary>
+        /// Test the Cosmos DB SDK by creating an example Database called 'SolarSystemDB' and a PlanetsCollection with planets on it.
+        /// </summary>
+        /// <returns></returns>
         public async Task<bool> RunTests()
         {
             Console.WriteLine("\n---------------------------------");
             Console.WriteLine("COSMOS DB");
-            Console.WriteLine("(Microsoft.Azure.DocumentDB)");
             Console.WriteLine("---------------------------------");
             Console.WriteLine("Functionalities to test: #:");
-            Console.WriteLine("1.- Create a Database\n");
+            Console.WriteLine("1.- Create a Database");
+            Console.WriteLine("2.- Create a Collection in the DB");
+            Console.WriteLine("3.- Create 2 JSON Documents (Items) in the collection");
+            Console.WriteLine("4.- Excecute simple query to the DB");
+            Console.WriteLine("5.- Clean up the resource (Delete DB)\n");
+
+            var testPassed = true;
 
             Console.Write("Creating Database 'SolarSystemDB'... ");
-            var result1 = await CreateDatabase();
-            if(result1 != null)
+            var result1 = await ExcecuteTest(CreateDatabase);
+            if(!result1)
             {
-                Console.Write("FAILED\n");
-                Console.WriteLine(result1);
                 //If this test failed, the next ones are going to fail too
-                Console.WriteLine("Cannot create a Collection, Query the collection and neither Delete it.");
+                Console.WriteLine("Cannot create a Collection, Documents, Query the collection and neither Delete the DB.");
                 return false;
-            }
-            else
-            {
-                Console.Write("DB created.\n");
             }
 
             Console.Write("Creating collection 'PlanetsCollection' ");
+            var result2 = await ExcecuteTest(CreateCollection);
+            if (!result2)
+            {
+                testPassed = false;
+            }
 
+            Console.Write("Inserting 'Earth' and 'Mars' JSON Documents... ");
+            var result3 = await ExcecuteTest(CreateDocuments);
+            if (!result3)
+            {
+                testPassed = false;
+            }
 
-            return true;
+            Console.Write("Querying... ");
+            var result4 = await ExcecuteTest(ExecuteSimpleQuery);
+            if (!result4)
+            {
+                testPassed = false;
+            }
+            
+            Console.Write("Cleaning up the resource... ");
+            var result5 = await ExcecuteTest(DeleteDatabase);
+            if (!result5)
+            {
+                testPassed = false;
+            }
+
+            return testPassed;
         }
 
-        private async Task<Exception> CreateDatabase()
+        private async Task CreateDatabase()
         {
-            try
-            {
-                await client.CreateDatabaseIfNotExistsAsync(new Database { Id = "SolarSystemDB" });
-            }
-            catch (Exception ex)
-            {
-                return ex;
-            }            
-            return null;
+            await client.CreateDatabaseIfNotExistsAsync(new Database { Id = "SolarSystemDB" });
         }
 
-        private async Task<Exception> CreateCollection()
+        private async Task CreateCollection()
         {
-            try
-            {
-                await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("SolarSystemDB"), new DocumentCollection { Id = "PlanetsCollection" });
-            }
-            catch (Exception ex)
-            {
-                return ex;
-            }
-            return null;
+            await client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("SolarSystemDB"), new DocumentCollection { Id = "PlanetsCollection" });
         }
 
-        private async Task<Exception> CreateDocuments()
+        private async Task CreateDocuments()
         {
-            Planet planetEarth = new Planet
+            planetEarth = new Planet
             {
                 Id = "Earth",
-                GassyPlanet = false,
+                HasRings = false,
                 Radius = 3959,
                 Moons = new Moon[]
                 {
@@ -110,10 +126,10 @@ namespace SmokeTest
                 }
             };
 
-            Planet planetMars = new Planet
+            planetMars = new Planet
             {
                 Id = "Mars",
-                GassyPlanet = false,
+                HasRings = false,
                 Radius = 2106,
                 Moons = new Moon[]
                 {
@@ -128,19 +144,28 @@ namespace SmokeTest
                 }
             };
 
-            return null;
+            await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri("SolarSystemDB", "PlanetsCollection"), planetEarth);
+            await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri("SolarSystemDB", "PlanetsCollection"), planetMars);
+
         }
 
-        private async Task<Exception> ExecuteSimpleQuery(){
-            try
-            {
+        private async Task ExecuteSimpleQuery(){
+            IQueryable<Planet> planetarySqlQuery = client.CreateDocumentQuery<Planet>(UriFactory.CreateDocumentCollectionUri("SolarSystemDB", "PlanetsCollection"), "SELECT * FROM c");
 
-            }
-            catch (Exception ex)
+            foreach (Planet planet in planetarySqlQuery)
             {
-                return ex;
+                //The only 2 planets that were set before were Earth and Mars, if planet does not match any of those, then there is an error.
+                if (planet.ToString() != planetEarth.ToString() && planet.ToString() != planetMars.ToString())
+                {
+                    throw new Exception(String.Format("Error, the values does not match.\n"));
+                }
             }
-            return null;
         }
+
+        private async Task DeleteDatabase()
+        {
+            await client.DeleteDatabaseAsync(UriFactory.CreateDatabaseUri("SolarSystemDB"));
+        }
+
     }
 }
