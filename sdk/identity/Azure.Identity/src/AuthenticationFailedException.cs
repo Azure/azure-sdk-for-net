cc75eb22eb9ae7094305c119b6d20588626d4316
Scott Schaab <sschaab@microsoft.com>
2019-06-27 14:28:02 -0700

    Adding AuthenticationFailedExcpetion to Azure.Identity

---
+++ sdk/identity/Azure.Identity/src/AuthenticationFailedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Identity
+{
+    public class AuthenticationFailedException : Exception
+    {
+        public AuthenticationFailedException(string message)
+            : this(message, null)
+        {
+        }
+
+        public AuthenticationFailedException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
