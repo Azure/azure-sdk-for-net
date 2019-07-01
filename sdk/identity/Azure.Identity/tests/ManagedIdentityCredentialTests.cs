0fcce50394ceb69435fca973127bb2dad600668f
Scott Schaab <sschaab@microsoft.com>
2019-07-01 15:50:54 -0700

    fixing idms deserialization issue

---
+++ sdk/identity/Azure.Identity/tests/ManagedIdentityCredentialTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Identity.Tests
+{
+    public class ManagedIdentityTests
+    {
+        [Test]
+        public async Task GetSystemTokenLiveAsync()
+        {
+            var credential = new ManagedIdentityCredential();
+
+            var token = await credential.GetTokenAsync(new string[] { "https://management.azure.com//.default" });
+
+            Assert.IsNotNull(token.Token);
+        }
+
+    }
+}
