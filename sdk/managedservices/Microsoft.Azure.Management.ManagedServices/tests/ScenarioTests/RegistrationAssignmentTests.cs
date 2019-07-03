c790cb704a1b882255a53543880be385cf77c1a4
Rajiv Nandivada [MSFT] <rajiv.nandivada@microsoft.com>
2019-07-03 14:09:50 -0700

    Added ManagdServices (GA) dot net sdk

--- sdk/managedservices/Microsoft.Azure.Management.ManagedServices/tests/ScenarioTests/RegistrationAssignmentTests.cs
+++ sdk/managedservices/Microsoft.Azure.Management.ManagedServices/tests/ScenarioTests/RegistrationAssignmentTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Linq;
-using System.Collections;
 using ManagedServices.Tests.Helpers;
 using Microsoft.Azure.Management.ManagedServices;
 using Microsoft.Azure.Management.ManagedServices.Models;
@@ -14,86 +13,75 @@ namespace ManagedServices.Tests.ScenarioTests
     public class RegistrationAssignmentTests : TestBase
     {
         [Fact]
-        public void RegistrationAssignmentTests_CRUD()
+        public async void RegistrationAssignmentTests_CRUD()
         {
             using (var context = MockContext.Start(GetType().FullName))
             {
                 using (var testFixture = new ManagedServicesTestBase(context))
                 {
                     var registrationDefinition = ManagedServicesTestUtilities.GetRegistrationDefintion();
-                    var registrationAssignment= ManagedServicesTestUtilities.GetRegistrationAssignment();
+                    var registrationAssignment = ManagedServicesTestUtilities.GetRegistrationAssignment();
 
                     //1. Create registration assignment
-                    var registrationDefinitionResponse = testFixture.ManagedServicesClient.RegistrationDefinitions
-                        .CreateOrUpdate(
+                    var registrationDefinitionResponse = await testFixture.ManagedServicesClient.RegistrationDefinitions
+                        .CreateOrUpdateAsync(
                             registrationDefinitionId: ManagedServicesTestUtilities.registratonDefinitionId,
-                            apiVersion: ManagedServicesTestUtilities.apiVersion,
                             scope: ManagedServicesTestUtilities.scope,
                             requestBody: registrationDefinition);
 
                     Assert.NotNull(registrationDefinitionResponse);
-                    Assert.Equal(ManagedServicesTestUtilities.registratonDefinitionId, registrationDefinitionResponse.Name);                    
+                    Assert.Equal(ManagedServicesTestUtilities.registratonDefinitionId, registrationDefinitionResponse.Name);
                     Assert.Equal(ProvisioningState.Succeeded, registrationDefinitionResponse.Properties.ProvisioningState);
 
-                    var registrationAssignemntResponse = testFixture.ManagedServicesClient.RegistrationAssignments
-                        .CreateOrUpdate(
+                    var registrationAssignemntResponse = await testFixture.ManagedServicesClient.RegistrationAssignments
+                        .CreateOrUpdateAsync(
                         scope: ManagedServicesTestUtilities.scope,
-                        apiVersion: ManagedServicesTestUtilities.apiVersion,
                         registrationAssignmentId: ManagedServicesTestUtilities.registrationAssignmentId,
                         requestBody: registrationAssignment);
 
                     Assert.NotNull(registrationAssignemntResponse);
-                    Assert.Equal(ManagedServicesTestUtilities.registrationAssignmentId, registrationAssignemntResponse.Name);                    
+                    Assert.Equal(ManagedServicesTestUtilities.registrationAssignmentId, registrationAssignemntResponse.Name);
                     Assert.Equal(ProvisioningState.Succeeded, registrationAssignemntResponse.Properties.ProvisioningState);
 
                     //2. Get registration assignment
-                    registrationAssignemntResponse = testFixture.ManagedServicesClient.RegistrationAssignments
-                        .Get(
+                    registrationAssignemntResponse = await testFixture.ManagedServicesClient.RegistrationAssignments
+                        .GetAsync(
                         scope: ManagedServicesTestUtilities.scope,
-                        apiVersion: ManagedServicesTestUtilities.apiVersion,
                         registrationAssignmentId: ManagedServicesTestUtilities.registrationAssignmentId);
 
                     Assert.NotNull(registrationAssignemntResponse);
                     Assert.Equal(ManagedServicesTestUtilities.registrationAssignmentId, registrationAssignemntResponse.Name);
 
                     //3. Get registration assignment collections
-                    var registrationAssignemntResponses = testFixture.ManagedServicesClient.RegistrationAssignments
-                        .List(
+                    var registrationAssignmentResponses = await testFixture.ManagedServicesClient.RegistrationAssignments
+                        .ListAsync(
                         scope: ManagedServicesTestUtilities.scope,
-                        apiVersion: ManagedServicesTestUtilities.apiVersion,
                         expandRegistrationDefinition: true);
 
-                    Assert.NotNull(registrationAssignemntResponses);
+                    Assert.Contains(ManagedServicesTestUtilities.registrationAssignmentId, registrationAssignmentResponses.Select(x => x.Name));
 
                     //4. Get registration assignment collections expanded
-                    registrationAssignemntResponses = testFixture.ManagedServicesClient.RegistrationAssignments
-                        .List(
+                    registrationAssignmentResponses = await testFixture.ManagedServicesClient.RegistrationAssignments
+                        .ListAsync(
                         scope: ManagedServicesTestUtilities.scope,
-                        apiVersion: ManagedServicesTestUtilities.apiVersion,
                         expandRegistrationDefinition: true);
 
-                    Assert.NotNull(registrationAssignemntResponses);
+                    Assert.NotNull(registrationAssignmentResponses.First().Properties.RegistrationDefinition);
 
                     //5. Delete registration assignment
-                    testFixture.ManagedServicesClient.RegistrationAssignments
-                        .Delete(
+                    await testFixture.ManagedServicesClient.RegistrationAssignments
+                        .DeleteAsync(
                         scope: ManagedServicesTestUtilities.scope,
-                        apiVersion: ManagedServicesTestUtilities.apiVersion,
                         registrationAssignmentId: ManagedServicesTestUtilities.registrationAssignmentId);
 
                     Assert.NotNull(registrationAssignemntResponse);
-                    Assert.Equal(ManagedServicesTestUtilities.registrationAssignmentId, registrationAssignemntResponse.Name);
 
                     //6. Get registration assignment collections
-                    registrationAssignemntResponses = testFixture.ManagedServicesClient.RegistrationAssignments
-                        .List(
-                        scope: ManagedServicesTestUtilities.scope,
-                        apiVersion: ManagedServicesTestUtilities.apiVersion);
+                    registrationAssignmentResponses = await testFixture.ManagedServicesClient.RegistrationAssignments
+                        .ListAsync(
+                        scope: ManagedServicesTestUtilities.scope);
 
-                    foreach (var assignment in registrationAssignemntResponses)
-                    {
-                        Assert.False(assignment.Name == ManagedServicesTestUtilities.registrationAssignmentId);
-                    }
+                    Assert.DoesNotContain(ManagedServicesTestUtilities.registrationAssignmentId, registrationAssignmentResponses.Select(x => x.Name));
                 }
             }
         }
