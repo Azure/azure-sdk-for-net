1d39bf5ca8a9e7c03cc49b2748edc818cc6e6291
Bruce Johnston <brjohnst@microsoft.com>
2019-07-04 15:29:06 -0700

    [Azure Search] Fix management SDK build

--- sdk/search/Build-SearchPackages.ps1
+++ sdk/search/Build-SearchPackages.ps1
@@ -1,7 +1,7 @@
 $scriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 $engDir = "$scriptDir\..\..\eng\"
 
-msbuild "$scriptDir\..\..\build.proj" /t:CreateNugetPackage /p:Scope="search"
+dotnet msbuild "$scriptDir\..\..\mgmt.proj" /t:CreateNugetPackage /p:Scope=Search
 
 dotnet restore
 dotnet build "$engDir/service.proj" /p:ServiceDirectory="search"
