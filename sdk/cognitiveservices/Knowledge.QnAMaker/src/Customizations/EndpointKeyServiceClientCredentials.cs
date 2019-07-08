8d2c5fe991fb3a0f3e233b4a9c71ae7f88fc4266
Anand <44665487+bingisbestest@users.noreply.github.com>
2019-07-08 18:34:35 +0530

    Adding runtime apis

---
+++ sdk/cognitiveservices/Knowledge.QnAMaker/src/Customizations/EndpointKeyServiceClientCredentials.cs
@@ -0,0 +1,40 @@
+﻿namespace Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Rest;
+
+    /// <summary>
+    /// Allows authentication to the API using a basic apiKey mechanism
+    /// </summary>
+    public class EndpointKeyServiceClientCredentials : ServiceClientCredentials
+    {
+        private readonly string endpointKey;
+
+        /// <summary>
+        /// Creates a new instance of the EndpointKeyServiceClientCredentials class
+        /// </summary>
+        /// <param name="endpointKey">The Endpoint key to authenticate and authorize as</param>
+        public EndpointKeyServiceClientCredentials(string endpointKey)
+        {
+            this.endpointKey = endpointKey;
+        }
+
+        /// <summary>
+        /// Add the Basic Authentication Header to each outgoing request
+        /// </summary>
+        /// <param name="request">The outgoing request</param>
+        /// <param name="cancellationToken">A token to cancel the operation</param>
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+                throw new ArgumentNullException("request");
+
+            request.Headers.Add("Authorization", $"EndpointKey {this.endpointKey}");
+
+            return Task.FromResult<object>(null);
+        }
+    }
+}
