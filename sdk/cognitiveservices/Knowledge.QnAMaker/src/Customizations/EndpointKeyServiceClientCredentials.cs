d214895233056926d0cbaa18186c91e0b081775d
Anand <44665487+bingisbestest@users.noreply.github.com>
2019-07-09 12:04:44 +0530

    adding copyright notice

--- sdk/cognitiveservices/Knowledge.QnAMaker/src/Customizations/EndpointKeyServiceClientCredentials.cs
+++ sdk/cognitiveservices/Knowledge.QnAMaker/src/Customizations/EndpointKeyServiceClientCredentials.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+
+namespace Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker
 {
     using System;
     using System.Net.Http;
