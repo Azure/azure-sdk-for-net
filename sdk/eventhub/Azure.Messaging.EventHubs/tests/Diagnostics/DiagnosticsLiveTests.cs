d05b4c1ec4916a6f4458b4a16d93e47bcc3f6ce8
Caio Saldanha <camaiaor@microsoft.com>
2019-07-12 11:23:52 -0700

    Using only LiveTests for now. Single test failing, but complete.

--- sdk/eventhub/Azure.Messaging.EventHubs/tests/Diagnostics/DiagnosticsLiveTests.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/tests/Diagnostics/DiagnosticsLiveTests.cs
@@ -1,7 +1,13 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Messaging.EventHubs.Core;
+using Azure.Messaging.EventHubs.Metadata;
 using Azure.Messaging.EventHubs.Tests.Infrastructure;
 using NUnit.Framework;
 
@@ -17,6 +23,23 @@ namespace Azure.Messaging.EventHubs.Tests
     [Category(TestCategory.DisallowVisualStudioLiveUnitTesting)]
     class DiagnosticsLiveTests
     {
+        public static ConcurrentQueue<(string eventName, object payload, Activity activity)> CreateEventQueue() =>
+            new ConcurrentQueue<(string eventName, object payload, Activity activity)>();
+
+        public static IDisposable SubscribeToEvents(IObserver<DiagnosticListener> listener) =>
+            DiagnosticListener.AllListeners.Subscribe(listener);
+
+        public static FakeDiagnosticListener CreateEventListener(string entityName, ConcurrentQueue<(string eventName, object payload, Activity activity)> eventQueue) =>
+            new FakeDiagnosticListener(kvp =>
+            {
+                if (kvp.Key == null || kvp.Value == null)
+                {
+                    return;
+                }
+
+                eventQueue?.Enqueue((kvp.Key, kvp.Value, Activity.Current));
+            });
+
         [Test]
         [Ignore("Injection step not working")]
         public async Task SendFiresEvents()
@@ -28,10 +51,10 @@ namespace Azure.Messaging.EventHubs.Tests
                 await using (var client = new EventHubClient(connectionString))
                 await using (var producer = client.CreateProducer())
                 {
-                    var eventQueue = DiagnosticsCommon.CreateEventQueue();
+                    var eventQueue = CreateEventQueue();
 
-                    using (var listener = DiagnosticsCommon.CreateEventListener(null, eventQueue))
-                    using (var subscription = DiagnosticsCommon.SubscribeToEvents(listener))
+                    using (var listener = CreateEventListener(null, eventQueue))
+                    using (var subscription = SubscribeToEvents(listener))
                     {
                         var parentActivity = new Activity("RandomName").AddBaggage("k1", "v1").AddBaggage("k2", "v2");
                         var eventData = new EventData(Encoding.UTF8.GetBytes("I hope it works!"));
@@ -60,10 +83,10 @@ namespace Azure.Messaging.EventHubs.Tests
                         Assert.That(TrackOne.EventHubsDiagnosticSource.SerializeCorrelationContext(parentActivity.Baggage.ToList()), Is.EqualTo(eventData.Properties[TrackOne.EventHubsDiagnosticSource.CorrelationContextPropertyName]));
 
                         Assert.That(eventQueue.TryDequeue(out var sendStart), Is.True);
-                        // SendStart
+                        AssertSendStart(sendStart.eventName, sendStart.payload, sendStart.activity, parentActivity, partitionKey, connectionString);
 
                         Assert.That(eventQueue.TryDequeue(out var sendStop), Is.True);
-                        // SendStop
+                        AssertSendStop(sendStop.eventName, sendStop.payload, sendStop.activity, sendStart.activity, partitionKey, connectionString);
 
                         // There should be no more events to dequeue.
 
@@ -71,6 +94,86 @@ namespace Azure.Messaging.EventHubs.Tests
                     }
                 }
             }
+        }
+
+        private void AssertSendStart(string name, object payload, Activity activity, Activity parentActivity, string partitionKey, string connectionString, int eventCount = 1)
+        {
+            var connectionStringProperties = ConnectionStringParser.Parse(connectionString);
+
+            Assert.That(name, Is.EqualTo("Microsoft.Azure.EventHubs.Send.Start"));
+            AssertCommonPayloadProperties(payload, partitionKey, connectionStringProperties);
+
+            var eventDatas = GetPropertyValueFromAnonymousTypeInstance<IList<EventData>>(payload, "EventDatas");
+            Assert.That(eventDatas.Count, Is.EqualTo(eventCount));
+
+            Assert.That(activity, Is.Not.Null);
+            Assert.That(activity.Parent, Is.EqualTo(parentActivity));
+
+            AssertTagMatches(activity, "peer.hostname", connectionStringProperties.Endpoint.Host);
+            AssertTagMatches(activity, "eh.event_hub_name", connectionStringProperties.EventHubPath);
+
+            if (partitionKey != null)
+            {
+                AssertTagMatches(activity, "eh.partition_key", partitionKey);
+            }
+
+            AssertTagMatches(activity, "eh.event_count", eventCount.ToString());
+            AssertTagExists(activity, "eh.client_id");
+        }
+
+        private void AssertSendStop(string name, object payload, Activity activity, Activity sendActivity, string partitionKey, string connectionString, bool isFaulted = false)
+        {
+            var connectionStringProperties = ConnectionStringParser.Parse(connectionString);
+
+            Assert.That(name, Is.EqualTo("Microsoft.Azure.EventHubs.Send.Stop"));
+            AssertCommonStopPayloadProperties(payload, partitionKey, isFaulted, connectionStringProperties); ;
+
+            if (sendActivity != null)
+            {
+                Assert.That(activity, Is.EqualTo(sendActivity));
+            }
+
+            var eventDatas = GetPropertyValueFromAnonymousTypeInstance<IList<EventData>>(payload, "EventDatas");
+            Assert.That(eventDatas, Is.Not.Null);
+        }
+
+        private void AssertTagExists(Activity activity, string tagName)
+        {
+            Assert.That(activity.Tags.Select(t => t.Key).Contains(tagName), Is.True);
+        }
+
+        private void AssertTagMatches(Activity activity, string tagName, string tagValue)
+        {
+            Assert.That(activity.Tags.Select(t => t.Key).Contains(tagName), Is.True);
+            Assert.That(activity.Tags.Single(t => t.Key == tagName).Value, Is.EqualTo(tagValue));
+        }
+
+        private void AssertCommonPayloadProperties(object eventPayload, string partitionKey, ConnectionStringProperties connectionStringProperties)
+        {
+            var endpoint = GetPropertyValueFromAnonymousTypeInstance<Uri>(eventPayload, "Endpoint");
+            var entityPath = GetPropertyValueFromAnonymousTypeInstance<string>(eventPayload, "Entity");
+            var pKey = GetPropertyValueFromAnonymousTypeInstance<string>(eventPayload, "PartitionKey");
+
+            Assert.That(endpoint, Is.EqualTo(connectionStringProperties.Endpoint));
+            Assert.That(entityPath, Is.EqualTo(connectionStringProperties.EventHubPath));
+            Assert.That(pKey, Is.EqualTo(partitionKey));
+        }
+
+        private void AssertCommonStopPayloadProperties(object eventPayload, string partitionKey, bool isFaulted, ConnectionStringProperties connectionStringProperties)
+        {
+            AssertCommonPayloadProperties(eventPayload, partitionKey, connectionStringProperties);
+        }
+
+        private T GetPropertyValueFromAnonymousTypeInstance<T>(object obj, string propertyName)
+        {
+            Type t = obj.GetType();
+            PropertyInfo p = t.GetRuntimeProperty(propertyName);
+            object propertyValue = p.GetValue(obj);
+
+            Assert.That(propertyValue, Is.Not.Null);
+            Assert.That(propertyValue, Is.AssignableFrom<T>());
+
+            return (T)propertyValue;
         }
     }
 }
