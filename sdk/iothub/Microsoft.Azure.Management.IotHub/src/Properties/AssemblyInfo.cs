7777dc9f939a4086cf1718e751b4fd160de1edfe
Sapan Saxena <31940305+anusapan@users.noreply.github.com>
2019-07-11 19:44:02 -0700

    Update IoTHub SDK (#6847)
    * Add test for manual-failover
    
    * Update test for manual-failover
    
    * Add recorded session and executed generate.ps1

--- sdk/iothub/Microsoft.Azure.Management.IotHub/src/Properties/AssemblyInfo.cs
+++ sdk/iothub/Microsoft.Azure.Management.IotHub/src/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.8.0.0")]
+[assembly: AssemblyVersion("2.9.0.0")]
+[assembly: AssemblyFileVersion("2.9.0.0")]
