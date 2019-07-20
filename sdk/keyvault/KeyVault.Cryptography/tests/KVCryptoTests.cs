f14d93476f7c1e75fc761bd47ae98b592725e348
shahabhijeet <abhishah@microsoft.com>
2019-07-19 17:45:27 -0700

    project directory structure created

---
+++ sdk/keyvault/KeyVault.Cryptography/tests/KVCryptoTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Security.KeyVault.Cryptography.Tests
+{
+    public class KVCryptoTests
+    {
+        [Test]
+        void EncryptKey()
+        {
+
+        }
+    }
+}
