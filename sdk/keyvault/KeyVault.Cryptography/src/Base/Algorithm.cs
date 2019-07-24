a5e364bfde705597cff278b4530b6639ad5f5093
shahabhijeet <abhishah@microsoft.com>
2019-07-24 15:11:20 -0700

    adding base types

---
+++ sdk/keyvault/KeyVault.Cryptography/src/Base/Algorithm.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Azure.Security.KeyVault.Cryptography.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Base class for all Algorithms used in the Key Vault .NET SDK
+    /// </summary>
+    public class Algorithm
+    {
+        #region const
+        //const string ARG_ALGORITHM_NAME = "algorithName";
+        #endregion
+
+        #region fields
+
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; private set; }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor for Type of Algorithm that needs to be instantiated
+        /// 
+        /// </summary>
+        /// <param name="algorithmName"></param>
+        protected Algorithm(string algorithmName)
+        {
+            // TODO:
+            // We should have an enum instead of a string.
+            // Serves two purpose, we will have a supported list of algorithms users can instantiate
+            // Secondly, rather than throwing at a later stage for non supported algorithm.
+            // OR
+            // Have a way to validate other than NullCheck on the type of algorithms we support
+
+            if (string.IsNullOrWhiteSpace(algorithmName))
+                throw new ArgumentNullException(nameof(algorithmName));
+
+            Name = algorithmName.Trim();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="algorithmType"></param>
+        protected Algorithm(AlgorithmInfo algorithmType)
+        {
+
+        }
+        #endregion
+
+        #region Public Functions
+        public AlgorithmInfo GetAlgorithmType()
+        {
+
+        }
+        #endregion
+
+        #region private functions
+
+        #endregion
+
+    }
+}
---
+++ sdk/keyvault/KeyVault.Cryptography/src/Base/AlgorithmType.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Azure.Security.KeyVault.Cryptography.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AlgorithmInfo
+    {
+
+        #region const
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int Aes128CbcHmacSha256 = 100;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int Ecdsa = 200;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const int Rsa15 = 300;
+
+        #endregion
+
+        #region fields
+
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// 
+        /// </summary>
+        protected int AlgorithmId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected string AlgorithmName { get; set; }
+
+
+        #region private properties
+        /// <summary>
+        /// 
+        /// </summary>
+        Dictionary<int, string> AlgorithmMap { get; set; }
+
+
+        #endregion
+
+        #endregion
+
+
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="algorithmId"></param>
+        /// <param name="algorithmName"></param>
+        protected AlgorithmInfo(int algorithmId, string algorithmName)
+        {
+
+        }
+        #endregion
+
+        #region Public Functions
+
+        #endregion
+
+        #region private functions
+
+        #endregion
+
+    }
+}
