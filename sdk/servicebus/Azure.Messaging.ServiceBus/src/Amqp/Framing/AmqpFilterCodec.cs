df769bbb4cdfe9de7c1443cda79a8ad1752d7a35
Pavel Krymets <pavel@krymets.com>
2019-07-31 14:12:30 -0700

    Projects and namespaces

---
+++ sdk/servicebus/Azure.Messaging.ServiceBus/src/Amqp/Framing/AmqpFilterCodec.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Azure.Messaging.ServiceBus.Amqp.Framing
+{
+    using Microsoft.Azure.Amqp.Framing;
+    using Microsoft.Azure.Amqp;
+
+    abstract class AmqpFilterCodec : DescribedList
+    {
+        protected AmqpFilterCodec(string name, ulong code)
+            : base(name, code)
+        {
+        }
+
+        protected override int FieldCount => 0;
+
+        protected override void OnEncode(ByteBuffer buffer)
+        {
+        }
+
+        protected override void OnDecode(ByteBuffer buffer, int count)
+        {
+        }
+
+        protected override int OnValueSize()
+        {
+            return 0;
+        }
+    }
+}
