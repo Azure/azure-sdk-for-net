445e45a2a5cd32ec3de290b8b4b123e7cf4d8d58
Sean Zimmerman (AZURE) <szimmer@microsoft.com>
2019-08-02 14:17:48 -0700

    Fixed assembly issue versioning.

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Properties/AssemblyInfo.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure Compute Management Library")]
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure Compute Resources.")]
 
-[assembly: AssemblyVersion("28.1.0.0")]
+[assembly: AssemblyVersion("28.0.0.0")]
 [assembly: AssemblyFileVersion("28.1.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
