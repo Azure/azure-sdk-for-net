0b5f4731bd00a081cb83fb3bacef16fe85122b33
Ankur Khemani <ankheman@microsoft.com>
2019-08-01 23:01:05 -0700

    [ACR] Update sdk and prepare 4.0.0 release (#7054)
    * release acr 4.0.0 sdk
    
    * sdk update per latest swagger
    
    * run generate.cmd
    
    * move file to correct folder

--- sdk/containerregistry/Microsoft.Azure.Management.ContainerRegistry/src/Properties/AssemblyInfo.cs
+++ sdk/containerregistry/Microsoft.Azure.Management.ContainerRegistry/src/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure Container Registry Management Library")]
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure Container Registry.")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
