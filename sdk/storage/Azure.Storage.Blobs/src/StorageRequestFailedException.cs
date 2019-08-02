ce291c1926c8146dd969f7bbb8f4557650a80944
amnguye <amnguye@microsoft.com>
2019-08-02 10:00:23 -0700

    Moving magic values and adding into Constants

--- sdk/storage/Azure.Storage.Blobs/src/StorageRequestFailedException.cs
+++ sdk/storage/Azure.Storage.Blobs/src/StorageRequestFailedException.cs
@@ -29,8 +29,8 @@ namespace Azure.Storage.Blobs.Models
             {
                 switch (element.Name.LocalName)
                 {
-                    case "Code":
-                    case "Message":
+                    case Constants.Xml.Code:
+                    case Constants.Xml.Message:
                         continue;
                     default:
                         error.AdditionalInformation[element.Name.LocalName] = element.Value;
