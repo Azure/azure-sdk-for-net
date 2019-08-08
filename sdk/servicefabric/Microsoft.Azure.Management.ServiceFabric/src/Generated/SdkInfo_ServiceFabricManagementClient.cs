565b3bea5eca6b77e7883c234f538530fdea095c
Alfredo Santamaria Gomez <alsantam@microsoft.com>
2019-08-08 10:35:29 -0700

    adding generated servicefabric_resource-manager.txt

--- sdk/servicefabric/Microsoft.Azure.Management.ServiceFabric/src/Generated/SdkInfo_ServiceFabricManagementClient.cs
+++ sdk/servicefabric/Microsoft.Azure.Management.ServiceFabric/src/Generated/SdkInfo_ServiceFabricManagementClient.cs
@@ -29,5 +29,16 @@ namespace Microsoft.Azure.Management.ServiceFabric
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "latest";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/servicefabric/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\code\\azure-sdk-for-net\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "397e41997764b35729dd4f39ec80a0f98c4456eb";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
