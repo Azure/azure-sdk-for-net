3977ab4589d3d77244f40107ab610fd109616ca8
Pooja Adhikari <poadhika@microsoft.com>
2019-08-08 12:55:07 -0700

    Updated the SDK following th guidelines

--- sdk/healthcareapis/Microsoft.Azure.Management.HealthcareApis/src/generate.ps1
+++ sdk/healthcareapis/Microsoft.Azure.Management.HealthcareApis/src/generate.ps1
@@ -1,1 +1,2 @@
-Start-AutoRestCodeGeneration -ResourceProvider "healthcareapis/resource-manager" -AutoRestVersion "latest"
+Start-AutoRestCodeGeneration -ResourceProvider "healthcareapis/resource-manager" -AutoRestVersion "latest" -SdkRootDirectory  "C:/Repo/Azure-SDK/sdk" -SdkRepoRootPath "C:/Repo/Azure-SDK"
+ 
