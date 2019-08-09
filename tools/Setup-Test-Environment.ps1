541c342f0275b251bd4c433ae9c016d797fbc5fc
Pavel Krymets <pavel@krymets.com>
2019-08-09 13:40:01 -0700

    Feedback

--- tools/Setup-Test-Environment.ps1
+++ tools/Setup-Test-Environment.ps1
@@ -4,8 +4,14 @@ param (
     [string]$keyVault = "AzureSDK-Dev-TestSecrets"
     )
 
+az account show | Out-Null;
+
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Error "Azure CLI with an active login is required. Please check the 'az account show' output."
+}
+
 $secrets = (az keyvault secret list --vault-name $keyVault --output json | ConvertFrom-Json);
-$secrets | Write-Host;
 
 $languagePrefix = "net-";
 $prefix = $languagePrefix + $service + "-";
@@ -18,7 +24,7 @@ foreach ($secret in $secrets)
     if ($name.StartsWith($prefix))
     {
         $value = (az keyvault secret show --id $id | ConvertFrom-Json).value;
-        $variableName = $name.Substring($languagePrefix.Length).Replace('-', '_');
+        $variableName = $name.Substring($languagePrefix.Length).Replace('-', '_').ToUpper();
 
         Write-Host "Setting" $variableName to $value;
         [Environment]::SetEnvironmentVariable($variableName, $value);
