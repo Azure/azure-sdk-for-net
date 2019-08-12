1a5181180516ed303b1264ae16096edc3e0a9318
Pavel Krymets <pavel@krymets.com>
2019-08-12 12:21:33 -0700

    Too much cleanup

--- sdk/core/Azure.Core/src/Shared/PageResponseEnumerator.cs
+++ sdk/core/Azure.Core/src/Shared/PageResponseEnumerator.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#nullable enable
+
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
