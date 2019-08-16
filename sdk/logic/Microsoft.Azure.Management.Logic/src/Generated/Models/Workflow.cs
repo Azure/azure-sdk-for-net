67e2993cbc60ef88d7d114349fb6ec87e7677e65
Vinita Sharma <visharm@microsoft.com>
2019-08-16 13:13:57 -0700

    Added ISE reference to workflow

--- sdk/logic/Microsoft.Azure.Management.Logic/src/Generated/Models/Workflow.cs
+++ sdk/logic/Microsoft.Azure.Management.Logic/src/Generated/Models/Workflow.cs
@@ -53,9 +53,10 @@ namespace Microsoft.Azure.Management.Logic.Models
         /// <param name="accessEndpoint">Gets the access endpoint.</param>
         /// <param name="sku">The sku.</param>
         /// <param name="integrationAccount">The integration account.</param>
+        /// <param name="integrationServiceEnvironment">The integration service environment.</param>
         /// <param name="definition">The definition.</param>
         /// <param name="parameters">The parameters.</param>
-        public Workflow(string id = default(string), string name = default(string), string type = default(string), string location = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), string provisioningState = default(string), System.DateTime? createdTime = default(System.DateTime?), System.DateTime? changedTime = default(System.DateTime?), string state = default(string), string version = default(string), string accessEndpoint = default(string), Sku sku = default(Sku), ResourceReference integrationAccount = default(ResourceReference), object definition = default(object), IDictionary<string, WorkflowParameter> parameters = default(IDictionary<string, WorkflowParameter>))
+        public Workflow(string id = default(string), string name = default(string), string type = default(string), string location = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), string provisioningState = default(string), System.DateTime? createdTime = default(System.DateTime?), System.DateTime? changedTime = default(System.DateTime?), string state = default(string), string version = default(string), string accessEndpoint = default(string), Sku sku = default(Sku), ResourceReference integrationAccount = default(ResourceReference), ResourceReference integrationServiceEnvironment = default(ResourceReference), object definition = default(object), IDictionary<string, WorkflowParameter> parameters = default(IDictionary<string, WorkflowParameter>))
             : base(id, name, type, location, tags)
         {
             ProvisioningState = provisioningState;
@@ -66,6 +67,7 @@ namespace Microsoft.Azure.Management.Logic.Models
             AccessEndpoint = accessEndpoint;
             Sku = sku;
             IntegrationAccount = integrationAccount;
+            IntegrationServiceEnvironment = integrationServiceEnvironment;
             Definition = definition;
             Parameters = parameters;
             CustomInit();
@@ -128,6 +130,12 @@ namespace Microsoft.Azure.Management.Logic.Models
         /// </summary>
         [JsonProperty(PropertyName = "properties.integrationAccount")]
         public ResourceReference IntegrationAccount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the integration service environment.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.integrationServiceEnvironment")]
+        public ResourceReference IntegrationServiceEnvironment { get; set; }
 
         /// <summary>
         /// Gets or sets the definition.
