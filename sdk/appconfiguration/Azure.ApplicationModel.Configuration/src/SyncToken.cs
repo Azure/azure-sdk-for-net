6a59829cdda53b6a332d0c164195c0361486d0ad
Anne Thompson <annelo@microsoft.com>
2019-08-19 15:59:50 -0700

    Saving work locally.

---
+++ sdk/appconfiguration/Azure.ApplicationModel.Configuration/src/SyncToken.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.ApplicationModel.Configuration
+{
+    // TODO: internal/public? 
+    // TODO: implement equality? (needed by SyncTokenUtil...?)
+    internal struct SyncToken
+    {
+        /// <summary>
+        /// The token's ID.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The token's value.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Token sequence number (version). Higher means newer version of the same token. 
+        /// </summary>
+        public long SequenceNumber { get; set; }
+    }
+}
---
+++ sdk/appconfiguration/Azure.ApplicationModel.Configuration/src/SyncTokenPolicy.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Azure.Core.Pipeline;
+//using Azure.Core.Pipeline.TaskExtensions;
+
+namespace Azure.ApplicationModel.Configuration
+{
+    internal class SyncTokenPolicy : HttpPipelinePolicy
+    {
+        /// <summary>
+        /// Map from token id to token.
+        /// </summary>
+        private ConcurrentDictionary<string, SyncToken> _syncTokens;
+
+        public SyncTokenPolicy()
+        {
+        }
+
+        public override Task ProcessAsync(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            return ProcessAsync(message, pipeline, async: true);
+        }
+
+        public override void Process(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            ProcessAsync(message, pipeline, async: false).GetAwaiter().GetResult();
+        }
+
+        private async Task ProcessAsync(HttpPipelineMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline, bool async)
+        {
+
+            //
+            // Use session synchonization tokens
+            foreach (SyncToken token in _syncTokens.Values)
+            {
+                message.Request.Headers.Add("Sync-Token", SyncTokenUtils.Format(token));
+            }
+
+            if (async)
+            {
+                await ProcessNextAsync(message, pipeline).ConfigureAwait(false);
+            }
+            else
+            {
+                ProcessNext(message, pipeline);
+            }
+
+            // TODO: YOU ARE HERE
+
+        }
+    }
+}
