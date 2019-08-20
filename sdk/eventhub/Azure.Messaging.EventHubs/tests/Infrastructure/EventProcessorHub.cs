6e0ea54c3df94ce5ba94467274b2d5dff27640c7
Caio Saldanha <camaiaor@microsoft.com>
2019-08-20 12:03:46 -0700

    First (incomplete) version of Event Processor live tests + Event Processor Hub implemented. It will be changed.

---
+++ sdk/eventhub/Azure.Messaging.EventHubs/tests/Infrastructure/EventProcessorHub.cs
@@ -0,0 +1,180 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Messaging.EventHubs.Processor;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Messaging.EventHubs.Tests
+{
+    /// <summary>
+    ///    TODO.
+    /// </summary>
+    ///
+    internal class EventProcessorHub
+    {
+        /// <summary>
+        ///    TODO.
+        /// </summary>
+        ///
+        private List<EventProcessor> EventProcessors { get; }
+
+        /// <summary>
+        ///    TODO.
+        /// </summary>
+        ///
+        public ConcurrentDictionary<string, int> EventsCount { get; }
+
+        /// <summary>
+        ///    TODO.
+        /// </summary>
+        ///
+        public ConcurrentDictionary<string, int> PartitionDistribution { get; }
+
+        /// <summary>
+        ///    TODO.
+        /// </summary>
+        ///
+        public ConcurrentDictionary<string, PartitionProcessorCloseReason> CloseReason { get; }
+
+        /// <summary>
+        ///    TODO.
+        /// </summary>
+        ///
+        public EventProcessorHub(string consumerGroup,
+                                 EventHubClient client,
+                                 int eventProcessorCount = 1)
+        {
+            Func<PartitionContext, CheckpointManager, IPartitionProcessor> factory =
+                (partitionContext, checkpointManager) =>
+                {
+                    return new PartitionProcessor(this, partitionContext.PartitionId);
+                };
+
+            var partitionManager = new InMemoryPartitionManager();
+
+            var options = new EventProcessorOptions { MaximumReceiveWaitTime = TimeSpan.FromSeconds(2) };
+
+            EventProcessors = new List<EventProcessor>();
+
+            for (int i = 0; i < eventProcessorCount; i++)
+            {
+                EventProcessors.Add
+                (
+                    new EventProcessor(consumerGroup,
+                        client,
+                        factory,
+                        partitionManager,
+                        options
+                ));
+            }
+
+            EventsCount = new ConcurrentDictionary<string, int>();
+            PartitionDistribution = new ConcurrentDictionary<string, int>();
+            CloseReason = new ConcurrentDictionary<string, PartitionProcessorCloseReason>();
+        }
+
+        /// <summary>
+        ///    TODO.
+        /// </summary>
+        ///
+        public Task StartAllAsync()
+        {
+            return Task.WhenAll(EventProcessors
+                .Select(eventProcessor => eventProcessor.StartAsync()));
+        }
+
+        /// <summary>
+        ///    TODO.
+        /// </summary>
+        ///
+        public Task StopAllAsync()
+        {
+            return Task.WhenAll(EventProcessors
+                .Select(eventProcessor => eventProcessor.StopAsync()));
+        }
+
+        /// <summary>
+        ///    TODO.
+        /// </summary>
+        ///
+        private class PartitionProcessor : IPartitionProcessor
+        {
+            /// <summary>
+            ///    TODO.
+            /// </summary>
+            ///
+            private EventProcessorHub Hub;
+
+            /// <summary>
+            ///    TODO.
+            /// </summary>
+            ///
+            private string PartitionId { get; }
+
+            /// <summary>
+            ///    TODO.
+            /// </summary>
+            ///
+            public PartitionProcessor(EventProcessorHub hub, string partitionId)
+            {
+                Hub = hub;
+                PartitionId = partitionId;
+            }
+
+            /// <summary>
+            ///    TODO.
+            /// </summary>
+            ///
+            public Task InitializeAsync()
+            {
+                // If key already exists, the original value won't be overwritten.
+
+                Hub.EventsCount.TryAdd(PartitionId, 0);
+
+                return Task.CompletedTask;
+            }
+
+            /// <summary>
+            ///    TODO.
+            /// </summary>
+            ///
+            public Task CloseAsync(PartitionProcessorCloseReason reason)
+            {
+                Hub.CloseReason[PartitionId] = reason;
+
+                return Task.CompletedTask;
+            }
+
+            /// <summary>
+            ///    TODO.
+            /// </summary>
+            ///
+            public Task ProcessEventsAsync(IEnumerable<EventData> events,
+                                           CancellationToken cancellationToken)
+            {
+                if (events.Count() > 0)
+                {
+                    var existingValue = Hub.EventsCount[PartitionId];
+                    Hub.EventsCount.TryUpdate(PartitionId, existingValue + events.Count(), existingValue);
+                }
+
+                return Task.CompletedTask;
+            }
+
+            /// <summary>
+            ///    TODO.
+            /// </summary>
+            ///
+            public Task ProcessErrorAsync(Exception exception,
+                                          CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ sdk/eventhub/Azure.Messaging.EventHubs/tests/Processor/EventProcessorLiveTests.cs
@@ -0,0 +1,270 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Messaging.EventHubs.Tests.Infrastructure;
+using NUnit.Framework;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Messaging.EventHubs.Tests
+{
+    /// <summary>
+    ///   The suite of live tests for the <see cref="EventProcessor" />
+    ///   class.
+    /// </summary>
+    ///
+    /// <remarks>
+    ///   These tests have a dependency on live Azure services and may
+    ///   incur costs for the associated Azure subscription.
+    /// </remarks>
+    ///
+    [TestFixture]
+    [Category(TestCategory.Live)]
+    [Category(TestCategory.DisallowVisualStudioLiveUnitTesting)]
+    public class EventProcessorLiveTests
+    {
+        /// <summary>
+        ///   Verifies that the <see cref="EventProcessor" /> is able to
+        ///   connect to the Event Hubs service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        public async Task SingleEventProcessorCanReceive()
+        {
+            await using (var scope = await EventHubScope.CreateAsync(10))
+            {
+                var connectionString = TestEnvironment.BuildConnectionStringForEventHub(scope.EventHubName);
+
+                await using (var client = new EventHubClient(connectionString))
+                {
+                    // Create the event processor hub to collect information about our single
+                    // processor host.
+
+                    var hub = new EventProcessorHub(EventHubConsumer.DefaultConsumerGroupName, client);
+
+                    // Start the event processor.
+
+                    await hub.StartAllAsync();
+
+                    // Send some sets of events to the event hub so our processor can receive
+                    // them.
+
+                    var eventSet = new[]
+                    {
+                        new EventData(Encoding.UTF8.GetBytes("One")),
+                        new EventData(Encoding.UTF8.GetBytes("Two")),
+                        new EventData(Encoding.UTF8.GetBytes("Three"))
+                    };
+
+                    var amountOfSets = 5;
+                    var expectedEventsCount = amountOfSets * eventSet.Length;
+
+                    await using (var producer = client.CreateProducer())
+                    {
+                        for (int i = 0; i < amountOfSets; i++)
+                        {
+                            await producer.SendAsync(eventSet);
+                        }
+                    }
+
+                    // Wait a few seconds so the data has enough time to reach the event processor.
+                    // TODO: we will need to extend this delay once the load balancing is implemented.
+
+                    await Task.Delay(5000);
+
+                    // Stop receiving events.
+
+                    await hub.StopAllAsync();
+
+                    // Validate results.
+
+                    Assert.That(hub.EventsCount.Values.Sum(), Is.EqualTo(expectedEventsCount));
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Verifies that the <see cref="EventProcessor" /> is able to
+        ///   connect to the Event Hubs service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        [Ignore("Won't work without load balancing")]
+        public async Task MultipleEventProcessorsCanReceive()
+        {
+            await using (var scope = await EventHubScope.CreateAsync(10))
+            {
+                var connectionString = TestEnvironment.BuildConnectionStringForEventHub(scope.EventHubName);
+
+                await using (var client = new EventHubClient(connectionString))
+                {
+                    // Create the event processor hub to collect information about our multiple
+                    // processor hosts.
+
+                    var hub = new EventProcessorHub(EventHubConsumer.DefaultConsumerGroupName, client, 3);
+
+                    // Start the event processors.
+
+                    await hub.StartAllAsync();
+
+                    // Send some sets of events to the event hub so our processors can receive
+                    // them.
+
+                    var eventSet = new[]
+                    {
+                        new EventData(Encoding.UTF8.GetBytes("One")),
+                        new EventData(Encoding.UTF8.GetBytes("Two")),
+                        new EventData(Encoding.UTF8.GetBytes("Three"))
+                    };
+
+                    var amountOfSets = 5;
+                    var expectedEventsCount = amountOfSets * eventSet.Length;
+
+                    await using (var producer = client.CreateProducer())
+                    {
+                        for (int i = 0; i < amountOfSets; i++)
+                        {
+                            await producer.SendAsync(eventSet);
+                        }
+                    }
+
+                    // Wait a few seconds so the data has enough time to reach the event processors.
+                    // TODO: we will need to extend this delay once the load balancing is implemented.
+
+                    await Task.Delay(5000);
+
+                    // Stop receiving events.
+
+                    await hub.StopAllAsync();
+
+                    // Validate results.
+
+                    Assert.That(hub.EventsCount.Values.Sum(), Is.EqualTo(expectedEventsCount));
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Verifies that the <see cref="EventProcessor" /> is able to
+        ///   connect to the Event Hubs service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        public async Task EventProcessorCanResumeAfterStopping()
+        {
+            await using (var scope = await EventHubScope.CreateAsync(10))
+            {
+                var connectionString = TestEnvironment.BuildConnectionStringForEventHub(scope.EventHubName);
+
+                await using (var client = new EventHubClient(connectionString))
+                {
+                    // Create the event processor hub to collect information about our single
+                    // processor host.
+
+                    var hub = new EventProcessorHub(EventHubConsumer.DefaultConsumerGroupName, client);
+
+                    // Start the event processor.
+
+                    await hub.StartAllAsync();
+
+                    // Send some events to the event hub so our processor can receive them.
+
+                    var eventsCount = 10;
+
+                    await using (var producer = client.CreateProducer())
+                    {
+                        var singleEvent = new EventData(Encoding.UTF8.GetBytes("I'm single"));
+
+                        for (int i = 0; i < eventsCount; i++)
+                        {
+                            await producer.SendAsync(singleEvent);
+                        }
+                    }
+
+                    // Wait a few seconds so the data has enough time to reach the event processor.
+                    // TODO: we will need to extend this delay once the load balancing is implemented.
+
+                    await Task.Delay(5000);
+
+                    // Stop receiving events.
+
+                    await hub.StopAllAsync();
+
+                    // Validate results.
+
+                    Assert.That(hub.EventsCount.Values.Sum(), Is.EqualTo(eventsCount));
+
+                    // Clear the counter so we can restart counting from zero.
+
+                    hub.EventsCount.Clear();
+
+                    // Resume the event processor execution.  There are no checkpoints, so we are expecting
+                    // to receive the same amount events.
+
+                    await hub.StartAllAsync();
+
+                    // Wait a few seconds so the data has enough time to reach the event processor.
+                    // TODO: we will need to extend this delay once the load balancing is implemented.
+
+                    await Task.Delay(5000);
+
+                    // Stop receiving events for good.
+
+                    await hub.StopAllAsync();
+
+                    // Validate results.
+
+                    Assert.That(hub.EventsCount.Values.Sum(), Is.EqualTo(eventsCount));
+                }
+            }
+        }
+
+        /// <summary>
+        ///   Verifies that the <see cref="EventProcessor" /> is able to
+        ///   connect to the Event Hubs service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        public async Task EventProcessorClosesPartitionProcessorWhenStopping()
+        {
+            var partitions = 10;
+
+            await using (var scope = await EventHubScope.CreateAsync(partitions))
+            {
+                var connectionString = TestEnvironment.BuildConnectionStringForEventHub(scope.EventHubName);
+
+                await using (var client = new EventHubClient(connectionString))
+                {
+                    // Create the event processor hub to collect information about our single
+                    // processor host.
+
+                    var hub = new EventProcessorHub(EventHubConsumer.DefaultConsumerGroupName, client);
+
+                    // Start the event processor.
+
+                    await hub.StartAllAsync();
+
+                    // Assert that no close reason has been set.  This way, we know the partition
+                    // processor close method hasn't been called yet.
+
+                    Assert.That(hub.CloseReason.Count, Is.EqualTo(0));
+
+                    // Stop receiving events.
+
+                    await hub.StopAllAsync();
+
+                    // Validate results.
+
+                    Assert.That(hub.CloseReason.Count, Is.EqualTo(partitions));
+
+                    foreach(var kvp in hub.CloseReason)
+                    {
+                        Assert.That(kvp.Value, Is.EqualTo(PartitionProcessorCloseReason.Shutdown));
+                    }
+                }
+            }
+        }
+    }
+}
