2f527b7989c81bffbcb8b1b8ab27741f50b1e8bf
omarsgalal <a-omsami@microsoft.com>
2019-08-21 12:38:23 +0200

    modifying luis authoring v2.0 tests

--- sdk/cognitiveservices/Language.LUIS.Authoring/tests/BaseTest.cs
+++ sdk/cognitiveservices/Language.LUIS.Authoring/tests/BaseTest.cs
@@ -17,6 +17,7 @@ namespace LUIS.Authoring.Tests.Luis
         protected readonly Guid GlobalAppIdError = new Guid("86226c53-b7a6-416f-876b-226b2b5ab07d");
         protected readonly Guid GlobalNoneId = new Guid("9a3fce96-2ab4-4f88-ba80-2011731e3188");
         protected const string AuthoringKey = "00000000000000000000000000000000";
+        protected readonly string OwnerEmail = "AppOwner@microsoft.com";
 
 
         private string ClassName => GetType().FullName;
