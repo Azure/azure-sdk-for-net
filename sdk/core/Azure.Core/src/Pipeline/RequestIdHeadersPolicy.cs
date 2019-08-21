f20d0e657a2a273ffa65d892edf1991c2016343c
Pavel Krymets <pavel@krymets.com>
2019-08-21 12:57:56 -0700

    Add requst id header policy prototype

---
+++ sdk/core/Azure.Core/src/Pipeline/RequestIdHeadersPolicy.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Azure.Core.Pipeline
+{
+    public class ActivityRequestIdHeadersPolicy : SynchronousHttpPipelinePolicy
+    {
+        private const string ActivityId = "Azure.RequestId";
+
+        private static readonly Dictionary<string, bool> SupportedHeaders = new Dictionary<string, bool>(StringComparer.OrdinalIgnoreCase)
+        {
+            {"x-ms-client-request-id", true},
+            {"x-ms-request-id", false},
+            {"x-ms-correlation-request-id", false}
+        };
+
+        public override void OnSendingRequest(HttpPipelineMessage message)
+        {
+            Activity activity = Activity.Current;
+            while (activity != null && activity.OperationName != ActivityId)
+            {
+                activity = activity.Parent;
+            }
+
+            if (activity == null)
+            {
+                return;
+            }
+
+            foreach (KeyValuePair<string, string> tag in activity.Tags)
+            {
+                if (SupportedHeaders.TryGetValue(tag.Key, out bool isClientRequestId))
+                {
+                    if (isClientRequestId)
+                    {
+                        // ClientRequestId policy would set the actual header
+                        message.Request.ClientRequestId = tag.Value;
+                    }
+                    else
+                    {
+                        message.Request.SetHeader(tag.Key, tag.Value);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ sdk/core/Azure.Core/tests/RequestIdHeadersPolicy.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Azure.Core.Pipeline;
+using Azure.Core.Testing;
+using NUnit.Framework;
+
+namespace Azure.Core.Tests
+{
+    public class ActivityRequestIdHeadersPolicyTests : SyncAsyncPolicyTestBase
+    {
+        public ActivityRequestIdHeadersPolicyTests(bool isAsync) : base(isAsync)
+        {
+        }
+        
+        [Test]
+
+        public async Task AllowsClientRequestIdToBeSet()
+        {
+            var mockTransport = new MockTransport(new MockResponse(200));
+
+            var activity = new Activity("Azure.RequestId");
+
+            activity.Start();
+            activity.AddTag("X-MS-CLIENT-REQUEST-ID", "CustomRequestId");
+
+            await SendGetRequest(mockTransport, new ActivityRequestIdHeadersPolicy());
+
+            activity.Stop();
+
+            Assert.AreEqual(mockTransport.SingleRequest.ClientRequestId, "CustomRequestId");
+        }
+
+        [Test]
+
+        public async Task AllowsOtherTelemetryHeadersToBeSet()
+        {
+            var mockTransport = new MockTransport(new MockResponse(200));
+
+            var activity = new Activity("Azure.RequestId");
+
+            activity.Start();
+            activity.AddTag("x-ms-request-id", "CustomRequestId");
+
+            await SendGetRequest(mockTransport, new ActivityRequestIdHeadersPolicy());
+
+            activity.Stop();
+
+            Assert.True(mockTransport.SingleRequest.TryGetHeader("X-MS-REQUEST-ID", out string value));
+            Assert.AreEqual(value, "CustomRequestId");
+        }
+
+        [Test]
+
+        public async Task IgnoresUnsupportedHeaders()
+        {
+            var mockTransport = new MockTransport(new MockResponse(200));
+
+            var activity = new Activity("Azure.RequestId");
+
+            activity.Start();
+            activity.AddTag("X-MS-RANDOM-ID", "CustomRequestId");
+
+            await SendGetRequest(mockTransport, new ActivityRequestIdHeadersPolicy());
+
+            activity.Stop();
+
+            Assert.False(mockTransport.SingleRequest.TryGetHeader("X-MS-REQUEST-ID", out string value));
+        }
+    }
+}
