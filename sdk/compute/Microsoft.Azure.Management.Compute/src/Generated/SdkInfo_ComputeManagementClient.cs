f620abdbdd307d1a619a86494bb21fdf32510b50
Josh Pollock <jopolloc@microsoft.com>
2019-08-21 14:16:58 -0700

    Upgrading the diskrp sdk to use version 2019-03-01

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/SdkInfo_ComputeManagementClient.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/SdkInfo_ComputeManagementClient.cs
@@ -22,7 +22,7 @@ namespace Microsoft.Azure.Management.Compute
                 new Tuple<string, string, string>("Compute", "AvailabilitySets", "2019-03-01"),
                 new Tuple<string, string, string>("Compute", "DedicatedHostGroups", "2019-03-01"),
                 new Tuple<string, string, string>("Compute", "DedicatedHosts", "2019-03-01"),
-                new Tuple<string, string, string>("Compute", "Disks", "2018-09-30"),
+                new Tuple<string, string, string>("Compute", "Disks", "2019-03-01"),
                 new Tuple<string, string, string>("Compute", "Galleries", "2019-07-01"),
                 new Tuple<string, string, string>("Compute", "GalleryApplicationVersions", "2019-07-01"),
                 new Tuple<string, string, string>("Compute", "GalleryApplications", "2019-07-01"),
@@ -33,7 +33,7 @@ namespace Microsoft.Azure.Management.Compute
                 new Tuple<string, string, string>("Compute", "Operations", "2019-03-01"),
                 new Tuple<string, string, string>("Compute", "ProximityPlacementGroups", "2019-03-01"),
                 new Tuple<string, string, string>("Compute", "ResourceSkus", "2019-04-01"),
-                new Tuple<string, string, string>("Compute", "Snapshots", "2018-09-30"),
+                new Tuple<string, string, string>("Compute", "Snapshots", "2019-03-01"),
                 new Tuple<string, string, string>("Compute", "Usage", "2019-03-01"),
                 new Tuple<string, string, string>("Compute", "VirtualMachineExtensionImages", "2019-03-01"),
                 new Tuple<string, string, string>("Compute", "VirtualMachineExtensions", "2019-03-01"),
@@ -52,10 +52,10 @@ namespace Microsoft.Azure.Management.Compute
       // BEGIN: Code Generation Metadata Section
       public static readonly String AutoRestVersion = "latest";
       public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/compute/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=E:\\hylee-sdk\\august2\\sdk";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/compute/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\Users\\jopolloc\\codebase\\azure-sdk-for-net\\sdk";
       public static readonly String GithubForkName = "Azure";
       public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "23326b92394ef630c59833f27e512544162ef4dd";
+      public static readonly String GithubCommidId = "156dd2a4a7549e9d443d7e262129e037846ead1a";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
