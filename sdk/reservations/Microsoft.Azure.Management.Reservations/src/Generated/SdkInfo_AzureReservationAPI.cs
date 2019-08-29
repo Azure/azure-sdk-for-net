07cb1d921c73ef7a433e68cc5bb3b9a26b1ad5b8
Kumar Gautam <kgautam@microsoft.com>
2019-08-29 11:25:57 -0700

    FW: Fixed autorest to be compatible with node LTS

--- sdk/reservations/Microsoft.Azure.Management.Reservations/src/Generated/SdkInfo_AzureReservationAPI.cs
+++ sdk/reservations/Microsoft.Azure.Management.Reservations/src/Generated/SdkInfo_AzureReservationAPI.cs
@@ -33,11 +33,10 @@ namespace Microsoft.Azure.Management.Reservations
       public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/reservations/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=D:\\rd\\azure-sdk-for-net\\sdk";
       public static readonly String GithubForkName = "Azure";
       public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "08e29aba646dd8bdf542c7733f1b62a6a4d88171";
-        public static readonly String CodeGenerationErrors = "(node:32168) UnhandledPromiseRejectionWarning: TypeError: volume[member].bind is not a function";
+      public static readonly String GithubCommidId = "543e24fb91a603ac8395d767824ad6deddd1d20a";
+      public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
   }
 }
-
 
