864f1b65198a44b2a6daf43a3a9e334193e029b2
Pooja Adhikari <poadhika@microsoft.com>
2019-09-04 17:21:24 -0700

    Updated  version number in AssemblyInfo.cs

--- sdk/healthcareapis/Microsoft.Azure.Management.HealthcareApis/src/generate.ps1
+++ sdk/healthcareapis/Microsoft.Azure.Management.HealthcareApis/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "healthcareapis/resource-manager" -AutoRestVersion "latest"
+Start-AutoRestCodeGeneration -ResourceProvider "healthcareapis/resource-manager" -AutoRestVersion "latest" -SdkRepoRootPath "C:/Repo/azuresdk" -SdkDirectory "C:/Repo/azuresdk/sdk/healthcareapis/Microsoft.Azure.Management.HealthcareApis/src/Generated"
