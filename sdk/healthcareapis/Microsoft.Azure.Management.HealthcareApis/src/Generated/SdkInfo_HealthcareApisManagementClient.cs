8aa71c84786b4652b390c840bd8fe1cbac061934
poadhika <42165979+poadhika@users.noreply.github.com>
2019-09-06 17:59:37 -0700

    Updating HealthcareApis SDK to refer to stable api version (#7477)
    * Updated HealthcareApis SDK to stable  version
    
    * Updated .csproj and healthcareapis_resource-manager.txt
    
    * Updated  version number in AssemblyInfo.cs
    
    * Reverted unwanted changes and updated version in csproj
    
    * Code regenerated to fix the path of healthcareapis_resource-manager.txt

--- sdk/healthcareapis/Microsoft.Azure.Management.HealthcareApis/src/Generated/SdkInfo_HealthcareApisManagementClient.cs
+++ sdk/healthcareapis/Microsoft.Azure.Management.HealthcareApis/src/Generated/SdkInfo_HealthcareApisManagementClient.cs
@@ -19,11 +19,22 @@ namespace Microsoft.Azure.Management.HealthcareApis
           {
               return new Tuple<string, string, string>[]
               {
-                new Tuple<string, string, string>("HealthcareApis", "OperationResults", "2018-08-20-preview"),
-                new Tuple<string, string, string>("HealthcareApis", "Operations", "2018-08-20-preview"),
-                new Tuple<string, string, string>("HealthcareApis", "Services", "2018-08-20-preview"),
+                new Tuple<string, string, string>("HealthcareApis", "OperationResults", "2019-09-16"),
+                new Tuple<string, string, string>("HealthcareApis", "Operations", "2019-09-16"),
+                new Tuple<string, string, string>("HealthcareApis", "Services", "2019-09-16"),
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "latest";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/healthcareapis/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\Repo\\azuresdk\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "8a02736ee4e89d8115d4ed5d2001e8c8d78ca878";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
