abd1f83024598504ef94a7ddb51fd2da4511acec
Pavel Krymets <pavel@krymets.com>
2019-09-09 15:32:39 -0700

    undo

--- sdk/eventhub/Azure.Messaging.EventHubs/tests/EventHubConsumer/EventHubConsumerTests.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/tests/EventHubConsumer/EventHubConsumerTests.cs
@@ -642,7 +642,7 @@ namespace Azure.Messaging.EventHubs.Tests
                     firstSubcriberReceiving = true;
                     StartPublishingIfReady();
 
-                    if (eventData != null)
+                    if (eventData != default)
                     {
                         firstSubscriberEvents.Add(eventData);
                     }
@@ -664,7 +664,7 @@ namespace Azure.Messaging.EventHubs.Tests
                     secondSubscriberReceiving = true;
                     StartPublishingIfReady();
 
-                    if (eventData != null)
+                    if (eventData != default)
                     {
                         secondSubscriberEvents.Add(eventData);
                     }
@@ -760,7 +760,7 @@ namespace Azure.Messaging.EventHubs.Tests
                     firstSubcriberReceiving = true;
                     StartPublishingIfReady();
 
-                    if (eventData != null)
+                    if (eventData != default)
                     {
                         firstSubscriberEvents.Add(eventData);
                     }
@@ -782,7 +782,7 @@ namespace Azure.Messaging.EventHubs.Tests
                     secondSubscriberReceiving = true;
                     StartPublishingIfReady();
 
-                    if (eventData != null)
+                    if (eventData != default)
                     {
                         secondSubscriberEvents.Add(eventData);
                     }
