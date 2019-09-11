3fd9df4924e1b8b8208fdb5077f4790bdc8ee88a
Wei Wei <weiwei@microsoft.com>
2019-09-11 12:21:02 +0800

    [Storage] Support Manage File Share (#7550)

--- sdk/storage/Microsoft.Azure.Management.Storage/src/Properties/AssemblyInfo.cs
+++ sdk/storage/Microsoft.Azure.Management.Storage/src/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Reflection;
@@ -8,7 +8,7 @@ using System.Resources;
 [assembly: AssemblyDescription("Provides Microsoft Azure Storage management functions for managing the Microsoft Azure Storage service.")]
 
 [assembly: AssemblyVersion("13.0.0.0")]
-[assembly: AssemblyFileVersion("13.0.0.0")]
+[assembly: AssemblyFileVersion("13.1.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
