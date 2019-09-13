c23326a12df4a2431cfa14410dbb3cd9bf187c1a
Scott Schaab <sschaab@microsoft.com>
2019-09-13 09:58:34 -0700

    minor cleanup

--- sdk/core/Azure.Core/src/Cryptography/IKeyEncryptionKeyResolver.cs
+++ sdk/core/Azure.Core/src/Cryptography/IKeyEncryptionKeyResolver.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System.Threading;
 using System.Threading.Tasks;
 
--- sdk/keyvault/Azure.Security.KeyVault.Keys/src/Cryptography/EncryptionAlgorithm.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/src/Cryptography/EncryptionAlgorithm.cs
@@ -79,43 +79,4 @@ namespace Azure.Security.KeyVault.Keys.Cryptography
         /// <inheritdoc/>
         public override string ToString() => _value;
     }
-
-    ///// <summary>
-    ///// Describes the encryption algorithm
-    ///// </summary>
-    //public enum EncryptionAlgorithm
-    //{
-    //    /// <summary>
-    //    /// RSA-OAEP
-    //    /// </summary>
-    //    RSAOAEP,
-
-    //    /// <summary>
-    //    /// RSA-15
-    //    /// </summary>
-    //    RSA15,
-
-    //    /// <summary>
-    //    /// RSA-OAEP256
-    //    /// </summary>
-    //    RSAOAEP256
-    //}
-
-    //internal static class EncryptionAlgorithmExtensions
-    //{
-    //    public static string GetName(this EncryptionAlgorithm algorithm)
-    //    {
-    //        switch (algorithm)
-    //        {
-    //            case EncryptionAlgorithm.RSAOAEP:
-    //                return "RSA-OAEP";
-    //            case EncryptionAlgorithm.RSA15:
-    //                return "RSA1_5";
-    //            case EncryptionAlgorithm.RSAOAEP256:
-    //                return "RSA-OAEP-256";
-    //            default:
-    //                return null;
-    //        }
-    //    }
-    //}
 }
