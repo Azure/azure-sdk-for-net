31b95096c4841b69a347b2d1c4acda3f5ecdc204
Heath Stewart <heaths@microsoft.com>
2019-09-18 09:25:26 -0700

    Add Values-like attribute for enum-like structs (#7667)
    Fixes #7665

---
+++ sdk/core/Azure.Core/tests/TestFramework/FieldsAttribute.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections;
+using System.Reflection;
+using NUnit.Framework.Interfaces;
+
+namespace Azure.Core.Testing
+{
+    /// <summary>
+    /// Provides values to NUnit that are public, static, read-only fields of the declared type.
+    /// Use this in place of ValuesAttribute for enum-like structs.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public class FieldsAttribute : Attribute, IParameterDataSource
+    {
+        public FieldsAttribute()
+        {
+        }
+
+        public IEnumerable GetData(IParameterInfo parameter)
+        {
+            Type type = parameter.ParameterType;
+            if (type.IsValueType)
+            {
+                FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+                for (int i = 0; i < fields.Length; ++i)
+                {
+                    if (fields[i].FieldType == type)
+                    {
+                        yield return fields[i].GetValue(null);
+                    }
+                }
+            }
+        }
+    }
+}
