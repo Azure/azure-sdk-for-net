181841b146307f9ed1a770b65fc9036ae592240a
Pavel Krymets <pavel@krymets.com>
2019-09-20 08:48:59 -0700

    Single type per file, file name matches class name (#7700)

--- sdk/appconfiguration/Azure.Data.AppConfiguration/tests/ConfigurationLiveTests.cs
+++ sdk/appconfiguration/Azure.Data.AppConfiguration/tests/ConfigurationLiveTests.cs
@@ -701,23 +701,4 @@ namespace Azure.Data.AppConfiguration.Tests
             }
         }
     }
-
-    public static class ConfigurationSettingExtensions
-    {
-        public static ConfigurationSetting Clone(this ConfigurationSetting setting)
-        {
-            Dictionary<string, string> tags = new Dictionary<string, string>();
-            foreach (string key in setting.Tags.Keys)
-            {
-                tags.Add(key, setting.Tags[key]);
-            }
-
-            return new ConfigurationSetting(setting.Key, setting.Value)
-            {
-                Label = setting.Label,
-                ContentType = setting.ContentType,
-                Tags = tags
-            };
-        }
-    }
 }
