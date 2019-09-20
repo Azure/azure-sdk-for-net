747800e34033bd9380df74a92b2828d6d473b2c7
Chidozie Ononiwu <31145988+chidozieononiwu@users.noreply.github.com>
2019-09-20 11:21:41 -0700

    Fix file headers (#7709)

--- sdk/keyvault/Azure.Security.KeyVault.Keys/src/Cryptography/KeyEncryptParameters.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/src/Cryptography/KeyEncryptParameters.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for
-// license information.
-
+// Licensed under the MIT License.
 using System.Text.Json;
 
 namespace Azure.Security.KeyVault.Keys.Cryptography
