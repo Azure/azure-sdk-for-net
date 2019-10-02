90c046d8d6f56879738c7af7d70152635228aaf3
Shenglong Li <shenglol@microsoft.com>
2019-10-02 15:47:13 -0700

    Add license information.

--- sdk/resources/Microsoft.Azure.Management.Resource/tests/ScenarioTests/DeploymentWhatIfTests.ScenarioTests.cs
+++ sdk/resources/Microsoft.Azure.Management.Resource/tests/ScenarioTests/DeploymentWhatIfTests.ScenarioTests.cs
@@ -1,4 +1,7 @@
-﻿namespace ResourceGroups.Tests
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace ResourceGroups.Tests
 {
     using System.IO;
     using System.Linq;
