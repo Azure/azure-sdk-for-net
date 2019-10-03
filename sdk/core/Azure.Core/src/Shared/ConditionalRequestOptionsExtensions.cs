3e12f95cafce183575192d182cd39dec1f2a5520
Anne Loomis Thompson <annelo@microsoft.com>
2019-10-02 21:54:42 -0700

    Missed file.

---
+++ sdk/core/Azure.Core/src/Shared/ConditionalRequestOptionsExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Core.Http
+{
+    internal static class ConditionalRequestOptionsExtensions
+    {
+        public static void ApplyHeaders(Request request, ConditionalRequestOptions options)
+        {
+            ConditionalRequestHelpers.Apply(request, options);
+        }
+
+        private class ConditionalRequestHelpers : ConditionalRequestOptions
+        {
+            public static void Apply(Request request, ConditionalRequestOptions options) =>
+                ApplyHeaders(request, options);
+        }
+    }
+}
