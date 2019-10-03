44b44468e40d4be6563b30b5b6c7bd210e6c64ca
Scott Schaab <sschaab@microsoft.com>
2019-10-03 09:45:28 -0700

    adding xml comments and test

--- sdk/identity/Azure.Identity/src/AuthorizationCodeCredential.cs
+++ sdk/identity/Azure.Identity/src/AuthorizationCodeCredential.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
+using Azure.Core.Pipeline;
 using Microsoft.Identity.Client;
 
 namespace Azure.Identity
@@ -17,71 +19,103 @@ namespace Azure.Identity
     {
         private readonly IConfidentialClientApplication _confidentialClient;
         private readonly string _authCode;
+        private readonly HttpPipeline _pipeline;
         private IAccount _account;
 
         /// <summary>
-        /// 
+        /// Protected constructor for mocking.
         /// </summary>
-        /// <param name="tenantId"></param>
-        /// <param name="clientId"></param>
-        /// <param name="clientSecret"></param>
-        /// <param name="authorizationCode"></param>
+        protected AuthorizationCodeCredential()
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of the ClientSecretCredential with the details needed to authenticate against Azure Active Directory with a prefetched authorization code.
+        /// </summary>
+        /// <param name="tenantId">The Azure Active Directory tenant (directory) Id of the service principal.</param>
+        /// <param name="clientId">The client (application) ID of the service principal</param>
+        /// <param name="clientSecret">A client secret that was generated for the App Registration used to authenticate the client.</param>
+        /// <param name="authorizationCode">The authorization code obtained from a call to authorize. The code should be obtained with all required scopes.
+        /// See https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow for more information.</param>
         public AuthorizationCodeCredential(string tenantId, string clientId, string clientSecret, string authorizationCode)
             : this(tenantId, clientId, clientSecret, authorizationCode, null)
         {
         }
 
         /// <summary>
-        /// 
+        /// Creates an instance of the ClientSecretCredential with the details needed to authenticate against Azure Active Directory with a prefetched authorization code.
         /// </summary>
-        /// <param name="tenantId"></param>
-        /// <param name="clientId"></param>
-        /// <param name="clientSecret"></param>
-        /// <param name="authorizationCode"></param>
-        /// <param name="options"></param>
+        /// <param name="tenantId">The Azure Active Directory tenant (directory) Id of the service principal.</param>
+        /// <param name="clientId">The client (application) ID of the service principal</param>
+        /// <param name="clientSecret">A client secret that was generated for the App Registration used to authenticate the client.</param>
+        /// <param name="authorizationCode">The authorization code obtained from a call to authorize. The code should be obtained with all required scopes.
+        /// See https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow for more information.</param>
+        /// <param name="options">Options that allow to configure the management of the requests sent to the Azure Active Directory service.</param>
         public AuthorizationCodeCredential(string tenantId, string clientId, string clientSecret, string authorizationCode, IdentityClientOptions options)
         {
+            if (tenantId is null) throw new ArgumentNullException(nameof(tenantId));
+            if (clientId is null) throw new ArgumentNullException(nameof(clientId));
+            if (clientSecret is null) throw new ArgumentNullException(nameof(clientSecret));
+
+            _authCode = authorizationCode ?? throw new ArgumentNullException(nameof(authorizationCode));
+
             options ??= new IdentityClientOptions();
 
-            _confidentialClient = ConfidentialClientApplicationBuilder.Create(clientId).WithClientSecret(clientSecret).Build();
+            _pipeline = HttpPipelineBuilder.Build(options);
+
+            _confidentialClient = ConfidentialClientApplicationBuilder.Create(clientId).WithHttpClientFactory(new HttpPipelineClientFactory(_pipeline)).WithClientSecret(clientSecret).Build();
         }
 
         /// <summary>
-        /// 
+        /// Obtains a token from the Azure Active Directory service, using the specified authorization code authenticate.
         /// </summary>
-        /// <param name="request"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <param name="request">The details of the authentication request.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> controlling the request lifetime.</param>
+        /// <returns>An <see cref="AccessToken"/> which can be used to authenticate service client calls.</returns>
         public override AccessToken GetToken(TokenRequest request, CancellationToken cancellationToken = default)
         {
             return GetTokenAsync(request, cancellationToken).GetAwaiter().GetResult();
         }
 
         /// <summary>
-        /// 
+        /// Obtains a token from the Azure Active Directory service, using the specified authorization code authenticate.
         /// </summary>
-        /// <param name="request"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <param name="request">The details of the authentication request.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> controlling the request lifetime.</param>
+        /// <returns>An <see cref="AccessToken"/> which can be used to authenticate service client calls.</returns>
         public override async Task<AccessToken> GetTokenAsync(TokenRequest request, CancellationToken cancellationToken = default)
         {
-            AccessToken token = default;
-            if (_account is null)
+            using DiagnosticScope scope = _pipeline.Diagnostics.CreateScope("Azure.Identity.AuthorizationCodeCredential.GetToken");
+
+            scope.Start();
+
+            try
             {
-                AuthenticationResult result = await _confidentialClient.AcquireTokenByAuthorizationCode(request.Scopes, _authCode).ExecuteAsync().ConfigureAwait(false);
+                AccessToken token = default;
 
-                _account = result.Account;
+                if (_account is null)
+                {
+                    AuthenticationResult result = await _confidentialClient.AcquireTokenByAuthorizationCode(request.Scopes, _authCode).ExecuteAsync().ConfigureAwait(false);
 
-                token = new AccessToken(result.AccessToken, result.ExpiresOn);
+                    _account = result.Account;
+
+                    token = new AccessToken(result.AccessToken, result.ExpiresOn);
+                }
+                else
+                {
+                    AuthenticationResult result = await _confidentialClient.AcquireTokenSilent(request.Scopes, _account).ExecuteAsync().ConfigureAwait(false);
+
+                    token = new AccessToken(result.AccessToken, result.ExpiresOn);
+                }
+
+                return token;
             }
-            else
+            catch (Exception e)
             {
-                AuthenticationResult result = await _confidentialClient.AcquireTokenSilent(request.Scopes, _account).ExecuteAsync().ConfigureAwait(false);
+                scope.Failed(e);
 
-                token = new AccessToken(result.AccessToken, result.ExpiresOn);
+                throw;
             }
-
-            return token;
         }
     }
 }
