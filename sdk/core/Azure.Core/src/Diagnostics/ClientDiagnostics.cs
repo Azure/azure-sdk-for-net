6520d18340a30df4dc98320a8fee9c9082cfb760
Pavel Krymets <pavel@krymets.com>
2019-10-07 12:27:52 -0700

    Move files around

--- sdk/core/Azure.Core/src/Diagnostics/ClientDiagnostics.cs
+++ sdk/core/Azure.Core/src/Diagnostics/ClientDiagnostics.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System.Diagnostics;
-using Azure.Core.Pipeline;
 
 namespace Azure.Core.Diagnostics
 {
--- sdk/core/Azure.Core/src/Shared/ConditionalRequestOptionsExtensions.cs
+++ sdk/core/Azure.Core/src/Shared/ConditionalRequestOptionsExtensions.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System.Globalization;
-using Azure;
 
 namespace Azure.Core
 {
