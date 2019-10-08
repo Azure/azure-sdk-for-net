3ad1ef67730b08d6afb1974d0458ae75c414a9a1
Heath Stewart <heaths@microsoft.com>
2019-10-08 11:53:34 -0700

    Add api-version to allowed query params for Key Vault (#7925)
    * Add api-version to allowed query params for Key Vault
    
    Fixes #7860
    
    * Add additional headers for Key Vault logging

--- sdk/keyvault/Azure.Security.KeyVault.Keys/src/KeyClientOptions.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/src/KeyClientOptions.cs
@@ -50,6 +50,8 @@ namespace Azure.Security.KeyVault.Keys
         public KeyClientOptions(ServiceVersion version = ServiceVersion.V7_0)
         {
             Version = version;
+
+            this.ConfigureLogging();
         }
 
         internal string GetVersionString()
---
+++ sdk/keyvault/Azure.Security.KeyVault.Shared/ClientOptionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using Azure.Core.Pipeline;
+
+namespace Azure.Security.KeyVault
+{
+    internal static class ClientOptionsExtensions
+    {
+        public static void ConfigureLogging(this ClientOptions clientOptions)
+        {
+            IList<string> allowedHeaderNames = clientOptions.Diagnostics.LoggingAllowedHeaderNames;
+            allowedHeaderNames.Add("x-ms-keyvault-network-info");
+            allowedHeaderNames.Add("x-ms-keyvault-region");
+            allowedHeaderNames.Add("x-ms-keyvault-service-version");
+
+            clientOptions.Diagnostics.LoggingAllowedQueryParameters.Add("api-version");
+        }
+    }
+}
