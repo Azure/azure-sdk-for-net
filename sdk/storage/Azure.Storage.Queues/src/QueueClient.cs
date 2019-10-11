0a5d5ffb78c2a37bbd2b75887a88b9560a4e763f
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2019-10-11 10:39:40 -0700

    Make client constants instance properties to support versioning (#7966)

--- sdk/storage/Azure.Storage.Queues/src/QueueClient.cs
+++ sdk/storage/Azure.Storage.Queues/src/QueueClient.cs
@@ -66,12 +66,12 @@ namespace Azure.Storage.Queues
         /// QueueMaxMessagesPeek indicates the maximum number of messages
         /// you can retrieve with each call to Peek.
         /// </summary>
-        public const int MaxMessagesPeek = Constants.Queue.MaxMessagesDequeue;
+        public virtual int MaxMessagesPeek => Constants.Queue.MaxMessagesDequeue;
 
         /// <summary>
-        /// QueueMessageMaxBytes indicates the maximum number of bytes allowed for a message's UTF-8 text.
+        /// Gets the maximum number of bytes allowed for a message's UTF-8 text.
         /// </summary>
-        public const int MessageMaxBytes = Constants.Queue.QueueMessageMaxBytes;
+        public virtual int MessageMaxBytes => Constants.Queue.QueueMessageMaxBytes;
 
         /// <summary>
         /// The Storage account name corresponding to the queue client.
