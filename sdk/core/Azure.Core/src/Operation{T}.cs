66de0fc0f20a3a888716df5e1075d8893b2e1899
Pavel Krymets <pavel@krymets.com>
2019-10-14 15:20:09 -0700

    Make operation more abstract (#8099)

--- sdk/core/Azure.Core/src/Operation{T}.cs
+++ sdk/core/Azure.Core/src/Operation{T}.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#nullable disable
-
 using System;
 using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
+
+#nullable enable
 
 namespace Azure
 {
@@ -16,24 +16,11 @@ namespace Azure
     /// <typeparam name="T">The final result of the LRO.</typeparam>
     public abstract class Operation<T> where T : notnull
     {
-        private T _value;
-        private Response _response;
-
-        /// <summary>
-        /// Creates a new instance of the Operation representing the specified
-        /// <paramref name="id"/>.
-        /// </summary>
-        /// <param name="id">The ID of the LRO.</param>
-        protected Operation(string id)
-        {
-            Id = id;
-        }
-
         /// <summary>
         /// Gets an ID representing the operation that can be used to poll for
         /// the status of the LRO.
         /// </summary>
-        public string Id { get; }
+        public abstract string Id { get; }
 
         /// <summary>
         /// Final result of the LRO.
@@ -41,21 +28,7 @@ namespace Azure
         /// <remarks>
         /// This property can be accessed only after the operation completes succesfully (HasValue is true).
         /// </remarks>
-        public T Value
-        {
-            get
-            {
-#pragma warning disable CA1065 // Do not raise exceptions in unexpected locations
-                if (!HasValue)
-                    throw new InvalidOperationException("operation has not completed");
-#pragma warning restore CA1065 // Do not raise exceptions in unexpected locations
-                return _value;
-            }
-            protected set
-            {
-                _value = value;
-            }
-        }
+        public abstract T Value { get; }
 
         /// <summary>
         /// The last HTTP response received from the server.
@@ -65,8 +38,7 @@ namespace Azure
         /// An instance of Operation<typeparamref name="T"/> sends requests to a server in UpdateStatusAsync, UpdateStatus, and other methods.
         /// Responses from these requests can be accessed using GetRawResponse.
         /// </remarks>
-        public Response GetRawResponse() => _response;
-        protected void SetRawResponse(Response response) => _response = response;
+        public abstract Response GetRawResponse();
 
         /// <summary>
         /// Returns true if the LRO completed.
@@ -79,11 +51,6 @@ namespace Azure
         public abstract bool HasValue { get; }
 
         /// <summary>
-        /// Gets or sets the value indicating default polling interval used by WaitForCompletion method.
-        /// </summary>
-        protected virtual TimeSpan DefaultPollingInterval { get; } = TimeSpan.FromSeconds(1);
-
-        /// <summary>
         /// Periodically calls the server till the LRO completes.
         /// </summary>
         /// <param name="cancellationToken"></param>
@@ -91,10 +58,7 @@ namespace Azure
         /// <remarks>
         /// This method will periodically call UpdateStatusAsync till HasCompleted is true, then return the final result of the operation.
         /// </remarks>
-        public ValueTask<Response<T>> WaitForCompletionAsync(CancellationToken cancellationToken = default)
-        {
-            return WaitForCompletionAsync(DefaultPollingInterval, cancellationToken);
-        }
+        public abstract ValueTask<Response<T>> WaitForCompletionAsync(CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Periodically calls the server till the LRO completes.
@@ -109,18 +73,7 @@ namespace Azure
         /// <remarks>
         /// This method will periodically call UpdateStatusAsync till HasCompleted is true, then return the final result of the operation.
         /// </remarks>
-        public virtual async ValueTask<Response<T>> WaitForCompletionAsync(TimeSpan pollingInterval, CancellationToken cancellationToken)
-        {
-            while (true)
-            {
-                await UpdateStatusAsync(cancellationToken).ConfigureAwait(false);
-                if (HasCompleted)
-                {
-                    return Response.FromValue(Value, _response);
-                }
-                await Task.Delay(pollingInterval, cancellationToken).ConfigureAwait(false);
-            }
-        }
+        public abstract ValueTask<Response<T>> WaitForCompletionAsync(TimeSpan pollingInterval, CancellationToken cancellationToken);
 
         /// <summary>
         /// Calls the server to get updated status of the LRO.
--- sdk/keyvault/Azure.Security.KeyVault.Certificates/src/CertificateOperation.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Certificates/src/CertificateOperation.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Core;
 
 namespace Azure.Security.KeyVault.Certificates
 {
@@ -12,15 +13,18 @@ namespace Azure.Security.KeyVault.Certificates
     /// </summary>
     public class CertificateOperation : Operation<CertificateWithPolicy>
     {
-        private bool _hasValue = false;
-        private bool _completed = false;
+        private bool _completed;
         private readonly CertificateClient _client;
 
+        private Response _response;
+
+        private CertificateWithPolicy _value;
+
         internal CertificateOperation(Response<CertificateOperationProperties> properties, CertificateClient client)
-            : base(properties.Value.Id.ToString())
         {
             Properties = properties;
 
+            Id = properties.Value.Id.ToString();
             _client = client;
         }
 
@@ -37,7 +41,24 @@ namespace Azure.Security.KeyVault.Certificates
         /// <summary>
         /// Specifies whether the Value property can be safely accessed
         /// </summary>
-        public override bool HasValue => _hasValue;
+        public override bool HasValue => _value != null;
+
+        /// <inheritdoc />
+        public override string Id { get; }
+
+        /// <inheritdoc />
+        public override CertificateWithPolicy Value => _value;
+
+        /// <inheritdoc />
+        public override Response GetRawResponse() => _response;
+
+        /// <inheritdoc />
+        public override ValueTask<Response<CertificateWithPolicy>> WaitForCompletionAsync(CancellationToken cancellationToken = default) =>
+            this.DefaultWaitForCompletionAsync(cancellationToken);
+
+        /// <inheritdoc />
+        public override ValueTask<Response<CertificateWithPolicy>> WaitForCompletionAsync(TimeSpan pollingInterval, CancellationToken cancellationToken) =>
+            this.DefaultWaitForCompletionAsync(pollingInterval, cancellationToken);
 
         /// <summary>
         /// Updates the status of the certificate operation
@@ -50,7 +71,7 @@ namespace Azure.Security.KeyVault.Certificates
             {
                 Response<CertificateOperationProperties> pollResponse = _client.GetPendingCertificate(Properties.Name, cancellationToken);
 
-                SetRawResponse(pollResponse.GetRawResponse());
+                _response = pollResponse.GetRawResponse();
 
                 Properties = pollResponse;
             }
@@ -59,13 +80,11 @@ namespace Azure.Security.KeyVault.Certificates
             {
                 Response<CertificateWithPolicy> getResponse = _client.GetCertificate(Properties.Name, cancellationToken);
 
-                SetRawResponse(getResponse.GetRawResponse());
+                _response = getResponse.GetRawResponse();
 
-                Value = getResponse.Value;
+                _value = getResponse.Value;
 
                 _completed = true;
-
-                _hasValue = true;
             }
             else if (Properties.Status == "cancelled")
             {
@@ -94,7 +113,7 @@ namespace Azure.Security.KeyVault.Certificates
             {
                 Response<CertificateOperationProperties> pollResponse = await _client.GetPendingCertificateAsync(Properties.Name, cancellationToken).ConfigureAwait(false);
 
-                SetRawResponse(pollResponse.GetRawResponse());
+                _response = pollResponse.GetRawResponse();
 
                 Properties = pollResponse;
             }
@@ -103,13 +122,11 @@ namespace Azure.Security.KeyVault.Certificates
             {
                 Response<CertificateWithPolicy> getResponse = await _client.GetCertificateAsync(Properties.Name, cancellationToken).ConfigureAwait(false);
 
-                SetRawResponse(getResponse.GetRawResponse());
+                _response = getResponse.GetRawResponse();
 
-                Value = getResponse.Value;
+                _value = getResponse.Value;
 
                 _completed = true;
-
-                _hasValue = true;
             }
             else if (Properties.Status == "cancelled")
             {
