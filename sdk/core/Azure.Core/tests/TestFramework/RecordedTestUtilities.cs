57239f99c5ff2e69219554d151fdd400ce5cb685
jschrepp-MSFT <41338290+jschrepp-MSFT@users.noreply.github.com>
2019-10-15 15:31:58 -0700

    Reverted test hack

--- sdk/core/Azure.Core/tests/TestFramework/RecordedTestUtilities.cs
+++ sdk/core/Azure.Core/tests/TestFramework/RecordedTestUtilities.cs
@@ -19,7 +19,7 @@ namespace Azure.Core.Testing
                 mode = (RecordedTestMode)Enum.Parse(typeof(RecordedTestMode), modeString, true);
             }
 
-            return RecordedTestMode.Live;
+            return mode;
         }
 
     }
