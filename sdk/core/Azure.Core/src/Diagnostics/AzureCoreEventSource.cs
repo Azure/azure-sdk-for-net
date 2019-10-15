69d1c8bd7d643ccd38dda391dbdffbb2cae3a62f
Pavel Krymets <pavel@krymets.com>
2019-10-15 11:22:54 -0700

    Cleanup

--- sdk/core/Azure.Core/src/Diagnostics/AzureCoreEventSource.cs
+++ sdk/core/Azure.Core/src/Diagnostics/AzureCoreEventSource.cs
@@ -16,7 +16,7 @@ namespace Azure.Core.Diagnostics
         private const int RequestContentTextEvent = 17;
         private const int ResponseEvent = 5;
         private const int ResponseContentEvent = 6;
-        private const int RequestDelayEvent = 7;
+        private const int ResponseDelayEvent = 7;
         private const int ResponseContentTextEvent = 13;
         private const int ResponseContentBlockEvent = 11;
         private const int ResponseContentTextBlockEvent = 15;
@@ -115,10 +115,10 @@ namespace Azure.Core.Diagnostics
             WriteEvent(RequestRetryingEvent, requestId, retryNumber);
         }
 
-        [Event(RequestDelayEvent, Level = EventLevel.Warning, Message = "Request [{0}] took {1:00.0}s")]
+        [Event(ResponseDelayEvent, Level = EventLevel.Warning, Message = "Request [{0}] took {1:00.0}s")]
         public void ResponseDelay(string requestId, double seconds)
         {
-            WriteEvent(RequestDelayEvent, requestId, seconds);
+            WriteEvent(ResponseDelayEvent, requestId, seconds);
         }
     }
 }
