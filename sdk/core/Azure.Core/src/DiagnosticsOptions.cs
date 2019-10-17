d4a4095c4233138cc78cb33a7278d537c856a6fa
Pavel Krymets <pavel@krymets.com>
2019-10-17 12:46:59 -0700

    Expand default header allow-list (#8166)

--- sdk/core/Azure.Core/src/DiagnosticsOptions.cs
+++ sdk/core/Azure.Core/src/DiagnosticsOptions.cs
@@ -18,10 +18,29 @@ namespace Azure.Core
             ApplicationId = DefaultApplicationId;
             LoggedHeaderNames = new List<string>()
             {
-                "Date",
-                "traceparent",
                 "x-ms-client-request-id",
-                "x-ms-request-id"
+                "x-ms-return-client-request-id",
+                "traceparent",
+
+                "Accept",
+                "Cache-Control",
+                "Connection",
+                "Content-Length",
+                "Content-Type",
+                "Date",
+                "ETag",
+                "Expires",
+                "If-Match",
+                "If-Modified-Since",
+                "If-None-Match",
+                "If-Unmodified-Since",
+                "Last-Modified",
+                "Pragma",
+                "Request-Id",
+                "Retry-After",
+                "Server",
+                "Transfer-Encoding",
+                "User-Agent"
             };
             LoggedQueryParameters = new List<string>();
         }
