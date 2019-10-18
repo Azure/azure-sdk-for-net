d137727fd1823d923b47c9e2c9789be1ea47fa7c
seanmcc-msft <seanmcc@microsoft.com>
2019-10-17 17:39:28 -0700

    Added DataLake SDK

---
+++ sdk/storage/Azure.Storage.Files.DataLake/src/Models/PublicAccessType.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Storage.Files.DataLake.Models
+{
+
+    /// <summary>
+    /// Specifies whether data in the container may be accessed publicly and the level of access
+    /// </summary>
+    public enum PublicAccessType
+    {
+        /// <summary>
+        /// none
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// container
+        /// </summary>
+        Container,
+
+        /// <summary>
+        /// blob
+        /// </summary>
+        Blob
+    }
+}
--- sdk/storage/generate.ps1
+++ sdk/storage/generate.ps1
@@ -1,16 +1,16 @@
 pushd $PSScriptRoot/Azure.Storage.Common/swagger/Generator/
 npm install
 
-cd $PSScriptRoot/Azure.Storage.Blobs/swagger/
-autorest --use=$PSScriptRoot/Azure.Storage.Common/swagger/Generator/ --verbose
-
-cd $PSScriptRoot/Azure.Storage.Files/swagger/
-autorest --use=$PSScriptRoot/Azure.Storage.Common/swagger/Generator/ --verbose
-
-cd $PSScriptRoot/Azure.Storage.Queues/swagger/
-autorest --use=$PSScriptRoot/Azure.Storage.Common/swagger/Generator/ --verbose
-
-# cd $PSScriptRoot/Azure.Storage.Files.DataLake/swagger/
+# cd $PSScriptRoot/Azure.Storage.Blobs/swagger/
 # autorest --use=$PSScriptRoot/Azure.Storage.Common/swagger/Generator/ --verbose
+
+# cd $PSScriptRoot/Azure.Storage.Files/swagger/
+# autorest --use=$PSScriptRoot/Azure.Storage.Common/swagger/Generator/ --verbose
+
+# cd $PSScriptRoot/Azure.Storage.Queues/swagger/
+# autorest --use=$PSScriptRoot/Azure.Storage.Common/swagger/Generator/ --verbose
+
+cd $PSScriptRoot/Azure.Storage.Files.DataLake/swagger/
+autorest --use=$PSScriptRoot/Azure.Storage.Common/swagger/Generator/ --verbose
 
 popd
