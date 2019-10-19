ce65d55afa7f1067f5815d06e3af73a723e6a625
Pavel Krymets <pavel@krymets.com>
2019-10-18 17:52:57 -0700

    Update RequestActivityPolicy.cs

--- sdk/core/Azure.Core/src/Pipeline/Internal/RequestActivityPolicy.cs
+++ sdk/core/Azure.Core/src/Pipeline/Internal/RequestActivityPolicy.cs
@@ -61,7 +61,7 @@ namespace Azure.Core.Pipeline
             activity.AddTag("http.url", message.Request.Uri.ToString());
             activity.AddTag("requestId", message.Request.ClientRequestId);
             activity.AddTag("kind", "client");
-            
+
             if (message.Request.Headers.TryGetValue("User-Agent", out string? userAgent))
             {
                 activity.AddTag("http.user_agent", userAgent);
