c6f223dcbebad79f92e949d4dd9b5512fdff3ebb
Rohit Mahamuni <romahamu@microsoft.com>
2019-10-21 13:08:34 -0400

    Add namespace to generate.ps1

--- sdk/storagecache/Microsoft.Azure.Management.StorageCache/src/generate.ps1
+++ sdk/storagecache/Microsoft.Azure.Management.StorageCache/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "storagecache/resource-manager" -AutoRestVersion "latest"
+Start-AutoRestCodeGeneration -ResourceProvider "storagecache/resource-manager" -AutoRestVersion "latest" -SdkGenerationDirectory "$PSScriptRoot\Generated" -Namespace "Microsoft.Azure.Management.StorageCache"
