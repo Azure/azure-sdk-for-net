00e5b2ceb2a7ba7995a5ef3b174967b0231b4d97
Pavel Krymets <pavel@krymets.com>
2019-10-21 11:28:57 -0700

    Make Response[<T>] easier to use in debugger

---
+++ sdk/core/Azure.Core/src/Internal/ResponseDebugView.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure
+{
+    internal class ResponseDebugView<T>
+    {
+        private readonly Response<T> _response;
+
+        public ResponseDebugView(Response<T> response)
+        {
+            _response = response;
+        }
+
+        public Response GetRawResponse => _response.GetRawResponse();
+
+        public T Value => _response.Value;
+    }
+}
