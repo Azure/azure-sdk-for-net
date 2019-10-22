47ce19ba89d0436c0f712a3e1b410159bd33961b
Heath Stewart <heaths@microsoft.com>
2019-10-22 13:43:51 -0700

    Fix references to old "hsm"

--- sdk/keyvault/Azure.Security.KeyVault.Keys/README.md
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/README.md
@@ -138,7 +138,7 @@ Console.WriteLine(rsaKey.KeyType);
 
 // Create a hardware Elliptic Curve key
 // Because only premium key vault supports HSM backed keys , please ensure your key vault
-// SKU is premium when you set "hsm" value to true
+// SKU is premium when you set "hardwareProtected" value to true
 var echsmkey = new CreateEcKeyOptions("ec-key-name", hardwareProtected: true);
 KeyVaultKey ecKey = await client.CreateEcKeyAsync(echsmkey);
 
@@ -242,7 +242,7 @@ Console.WriteLine(rsaKey.KeyType);
 
 // Create a hardware Elliptic Curve key
 // Because only premium key vault supports HSM backed keys , please ensure your key vault
-// SKU is premium when you set "hsm" value to true
+// SKU is premium when you set "hardwareProtected" value to true
 var echsmkey = new CreateEcKeyOptions("ec-key-name", hardwareProtected: true);
 KeyVaultKey ecKey = client.CreateEcKey(echsmkey);
 
--- sdk/keyvault/Azure.Security.KeyVault.Keys/samples/SampleSnippets.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/samples/SampleSnippets.cs
@@ -71,7 +71,7 @@ namespace Azure.Security.KeyVault.Keys.Samples
 
             // Create a hardware Elliptic Curve key
             // Because only premium key vault supports HSM backed keys , please ensure your key vault
-            // SKU is premium when you set "hsm" value to true
+            // SKU is premium when you set "hardwareProtected" value to true
             var echsmkey = new CreateEcKeyOptions("ec-key-name", hardwareProtected: true);
             KeyVaultKey ecKey = await client.CreateEcKeyAsync(echsmkey);
 
@@ -99,7 +99,7 @@ namespace Azure.Security.KeyVault.Keys.Samples
 
             // Create a hardware Elliptic Curve key
             // Because only premium key vault supports HSM backed keys , please ensure your key vault
-            // SKU is premium when you set "hsm" value to true
+            // SKU is premium when you set "hardwareProtected" value to true
             var echsmkey = new CreateEcKeyOptions("ec-key-name", hardwareProtected: true);
             KeyVaultKey ecKey = client.CreateEcKey(echsmkey);
 
