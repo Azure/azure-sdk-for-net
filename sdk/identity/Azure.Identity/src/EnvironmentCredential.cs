2f5945299102b3fb38f644886c6f564585d7a9b2
Heath Stewart <heaths@microsoft.com>
2019-10-25 14:16:24 -0700

    Update KeyVault READMEs and other docs (#8401)
    * Update KeyVault READMEs and other docs
    
    Relates to #6076
    
    * Add mention of "DNS Name" in portal

--- sdk/identity/Azure.Identity/src/EnvironmentCredential.cs
+++ sdk/identity/Azure.Identity/src/EnvironmentCredential.cs
@@ -12,9 +12,12 @@ namespace Azure.Identity
     /// <summary>
     /// Enables authentication to Azure Active Directory using client secret
     /// details configured in the following environment variables:
-    /// - AZURE_TENANT_ID: The Azure Active Directory tenant(directory) ID.
-    /// - AZURE_CLIENT_ID: The client(application) ID of an App Registration in the tenant.
-    /// - AZURE_CLIENT_SECRET: A client secret that was generated for the App Registration.
+    /// <list type="table">
+    /// <listheader><term>Variable</term><description>Description</description></listheader>
+    /// <item><term>AZURE_TENANT_ID</term><description>The Azure Active Directory tenant(directory) ID.</description></item>
+    /// <item><term>AZURE_CLIENT_ID</term><description>The client(application) ID of an App Registration in the tenant.</description></item>
+    /// <item><term>AZURE_CLIENT_SECRET</term><description>A client secret that was generated for the App Registration.</description></item>
+    /// </list>
     /// This credential ultimately uses a <see cref="ClientSecretCredential"/> to
     /// perform the authentication using these details. Please consult the
     /// documentation of that class for more details.
