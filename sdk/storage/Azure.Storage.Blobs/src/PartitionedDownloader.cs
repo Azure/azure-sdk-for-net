5b50a345fdd57aa9e08fd02a00afc423e05d1d14
tg-msft <Ted.Glaza@microsoft.com>
2019-10-28 14:18:20 -0700

    Taking @pakrym's parallel upload/download PR #8344
    Grabbing the PR now that @pakrym's done the heavy lifting to apply feedback and
    walk through the escrow process.  (Thanks @pakrym!)

---
+++ sdk/storage/Azure.Storage.Blobs/src/PartitionedDownloader.cs
@@ -0,0 +1,203 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+
+namespace Azure.Storage.Blobs
+{
+    internal class PartitionedDownloader
+    {
+        private readonly BlobBaseClient _client;
+
+        private readonly long _singleBlockThreshold;
+
+        private readonly int _blockSize;
+        private readonly int _workerCount;
+
+        public PartitionedDownloader(BlobBaseClient client, StorageTransferOptions transferOptions = default, long? singleBlockThreshold = null)
+        {
+            _client = client;
+            _singleBlockThreshold = singleBlockThreshold ?? Constants.Blob.Block.MaxUploadBytes;
+            _workerCount =
+                transferOptions.MaximumConcurrency ?? Constants.Blob.Block.DefaultConcurrentTransfersCount;
+            _blockSize =
+                Math.Min(
+                    Constants.Blob.Block.MaxStageBytes,
+                    transferOptions.MaximumTransferLength ?? Constants.DefaultBufferSize
+                );
+        }
+
+        private static long ParseLength(string s)
+        {
+            int lengthSeparator = s.IndexOf("/", StringComparison.InvariantCultureIgnoreCase);
+
+            if (lengthSeparator == -1)
+            {
+                Errors.ParsingHttpRangeFailed();
+            }
+
+            return long.Parse(s.Substring(lengthSeparator + 1), CultureInfo.InvariantCulture);
+        }
+
+        public async Task<Response<BlobProperties>> DownloadToAsync(Stream stream, BlobRequestConditions conditions, CancellationToken cancellationToken)
+        {
+            var initialRange = new HttpRange(0, _singleBlockThreshold);
+
+            Task<Response<BlobDownloadInfo>> initialResponseTask = _client.DownloadAsync(initialRange, conditions, rangeGetContentHash: false, cancellationToken);
+            Response<BlobDownloadInfo> initialRequest = await initialResponseTask.ConfigureAwait(false);
+
+            long initialLength = initialRequest.Value.ContentLength;
+            long totalLength = ParseLength(initialRequest.Value.Details.ContentRange);
+            ETag etag = initialRequest.Value.Details.ETag;
+            Response<BlobProperties> properties = CreateProperties(initialRequest, totalLength);
+
+            if (initialLength == totalLength)
+            {
+                await CopyToAsync(initialRequest, stream, cancellationToken).ConfigureAwait(false);
+                return properties;
+            }
+
+            Queue<Task<Response<BlobDownloadInfo>>> runningTasks = new Queue<Task<Response<BlobDownloadInfo>>>();
+            runningTasks.Enqueue(initialResponseTask);
+
+            BlobRequestConditions conditionsWithEtag = CreateConditionsWithEtag(conditions, etag);
+
+            async Task ConsumeQueuedTask()
+            {
+                using BlobDownloadInfo result = await runningTasks.Dequeue().ConfigureAwait(false);
+                await CopyToAsync(result, stream, cancellationToken).ConfigureAwait(false);
+            }
+
+            foreach (HttpRange httpRange in GetRanges(initialLength, totalLength))
+            {
+                Task<Response<BlobDownloadInfo>> task = _client.DownloadAsync(httpRange, conditionsWithEtag, rangeGetContentHash: false, cancellationToken);
+
+                runningTasks.Enqueue(task);
+
+                if (runningTasks.Count < _workerCount)
+                {
+                    continue;
+                }
+
+                await ConsumeQueuedTask().ConfigureAwait(false);
+            }
+
+            while (runningTasks.Count > 0)
+            {
+                await ConsumeQueuedTask().ConfigureAwait(false);
+            }
+
+            return properties;
+        }
+
+        private static Response<BlobProperties> CreateProperties(Response<BlobDownloadInfo> response, long totalLength)
+        {
+            return Response.FromValue(new BlobProperties()
+            {
+                ContentLength = totalLength,LastModified = response.Value.Details.LastModified,
+                BlobType = response.Value.BlobType,
+                Metadata = response.Value.Details.Metadata,
+                CopyCompletedOn = response.Value.Details.CopyCompletedOn,
+                CopyStatusDescription = response.Value.Details.CopyStatusDescription,
+                CopyId = response.Value.Details.CopyId,
+                CopyProgress = response.Value.Details.CopyProgress,
+                CopySource = response.Value.Details.CopySource,
+                CopyStatus = response.Value.Details.CopyStatus,
+                LeaseDuration = response.Value.Details.LeaseDuration,
+                LeaseState = response.Value.Details.LeaseState,
+                LeaseStatus = response.Value.Details.LeaseStatus,
+                ContentType = response.Value.ContentType,
+                ETag = response.Value.Details.ETag,
+                ContentHash = response.Value.ContentHash,
+                ContentDisposition = response.Value.Details.ContentDisposition,
+                CacheControl = response.Value.Details.CacheControl,
+                BlobSequenceNumber = response.Value.Details.BlobSequenceNumber,
+                AcceptRanges = response.Value.Details.AcceptRanges,
+                BlobCommittedBlockCount = response.Value.Details.BlobCommittedBlockCount,
+                IsServerEncrypted = response.Value.Details.IsServerEncrypted,
+                EncryptionKeySha256 = response.Value.Details.EncryptionKeySha256,
+
+                ContentEncoding = new string[] { response.Value.Details.ContentEncoding },
+                ContentLanguage = new string[] { response.Value.Details.ContentLanguage },
+
+                // TODO: These do not exist on BlobDownloadInfo
+                //AccessTier = response.Value.Details.AccessTier,
+                //AccessTierInferred = response.Value.Details.AccessTierInferred,
+                //ArchiveStatus = response.Value.Details.ArchiveStatus,
+                //AccessTierChangedOn = response.Value.Details.AccessTierChangedOn,
+                //IsIncrementalCopy = response.Value.Details.IsIncrementalCopy,
+                //DestinationSnapshot = response.Value.Details.DestinationSnapshot,
+                //CreatedOn = response.Value.Details.CreatedOn,
+            }, response.GetRawResponse());
+        }
+
+        public Response<BlobProperties> DownloadTo(Stream stream, BlobRequestConditions conditions, CancellationToken cancellationToken)
+        {
+            var initialRange = new HttpRange(0, _singleBlockThreshold);
+            Response<BlobDownloadInfo> initialRequest = _client.Download(initialRange, conditions, rangeGetContentHash: false, cancellationToken);
+
+            long initialLength = initialRequest.Value.ContentLength;
+            long totalLength = ParseLength(initialRequest.Value.Details.ContentRange);
+            ETag etag = initialRequest.Value.Details.ETag;
+
+            Response<BlobProperties> properties = CreateProperties(initialRequest, totalLength);
+
+            CopyTo(initialRequest, stream, cancellationToken);
+
+            if (initialLength == totalLength)
+            {
+                return properties;
+            }
+
+            BlobRequestConditions conditionsWithEtag = CreateConditionsWithEtag(conditions, etag);
+
+            foreach (HttpRange httpRange in GetRanges(initialLength, totalLength))
+            {
+                BlobDownloadInfo result = _client.Download(httpRange, conditionsWithEtag, rangeGetContentHash: false, cancellationToken);
+
+                CopyTo(result, stream, cancellationToken);
+            }
+
+            return properties;
+        }
+
+        private static BlobRequestConditions CreateConditionsWithEtag(BlobRequestConditions conditions, ETag etag)
+        {
+            BlobRequestConditions conditionsWithEtag = new BlobRequestConditions()
+            {
+                LeaseId = conditions?.LeaseId,
+                IfMatch = conditions?.IfMatch ?? etag,
+                IfNoneMatch = conditions?.IfNoneMatch,
+                IfModifiedSince = conditions?.IfModifiedSince,
+                IfUnmodifiedSince = conditions?.IfUnmodifiedSince
+            };
+            return conditionsWithEtag;
+        }
+
+        private static async Task CopyToAsync(BlobDownloadInfo result, Stream stream, CancellationToken cancellationToken)
+        {
+            await result.Content.CopyToAsync(stream, 8096, cancellationToken).ConfigureAwait(false);
+        }
+
+        private static void CopyTo(BlobDownloadInfo result, Stream stream, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            result.Content.CopyTo(stream, 8096);
+        }
+
+        private IEnumerable<HttpRange> GetRanges(long l, long length)
+        {
+            for (long i = l; i < length; i += _blockSize)
+            {
+                yield return new HttpRange(i, Math.Min(length - i, _blockSize));
+            }
+        }
+    }
+}
---
+++ sdk/storage/Azure.Storage.Blobs/tests/PartitionedDownloaderTests.cs
@@ -0,0 +1,307 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core.Testing;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Moq;
+using NUnit.Framework;
+
+namespace Azure.Storage.Blobs.Tests
+{
+    [TestFixture(true)]
+    [TestFixture(false)]
+    public class PartitionedDownloaderTests
+    {
+        private readonly bool _async;
+
+        // Use constants to verify that we flow them everywhere
+        private static readonly CancellationToken s_cancellationToken = new CancellationTokenSource().Token;
+        private static readonly BlobRequestConditions s_conditions = new BlobRequestConditions()
+        {
+            IfModifiedSince = DateTimeOffset.Now,
+            LeaseId = "MyImportantLease"
+        };
+        private static readonly ETag s_etag = new ETag("0xQWERTY");
+
+        public PartitionedDownloaderTests(bool async)
+        {
+            _async = async;
+        }
+
+        [Test]
+        public async Task ReturnsPropertiesForZeroLength()
+        {
+            MemoryStream stream = new MemoryStream();
+            MockDataSource dataSource = new MockDataSource(0);
+            Mock<BlobBaseClient> blockClient = new Mock<BlobBaseClient>(MockBehavior.Strict);
+
+            SetupDownload(blockClient, dataSource);
+
+            PartitionedDownloader downloader = new PartitionedDownloader(blockClient.Object);
+
+            Response<BlobProperties> result = await InvokeDownloadToAsync(downloader, stream);
+
+            Assert.AreEqual(0, stream.Length);
+            AssertResult(result, 0);
+        }
+
+        [Test]
+        public async Task DownloadsInOneBlockIfUnderLimit()
+        {
+            MemoryStream stream = new MemoryStream();
+            MockDataSource dataSource = new MockDataSource(10);
+            Mock<BlobBaseClient> blockClient = new Mock<BlobBaseClient>(MockBehavior.Strict);
+
+            SetupDownload(blockClient, dataSource);
+
+            PartitionedDownloader downloader = new PartitionedDownloader(blockClient.Object);
+
+            Response<BlobProperties> result = await InvokeDownloadToAsync(downloader, stream);
+
+            AssertContent(10, stream);
+            AssertResult(result, 10);
+        }
+
+        [Test]
+        public async Task DownloadsInBlocksWhenOverTheLimit()
+        {
+            MemoryStream stream = new MemoryStream();
+            MockDataSource dataSource = new MockDataSource(100);
+            Mock<BlobBaseClient> blockClient = new Mock<BlobBaseClient>(MockBehavior.Strict);
+            BlobProperties smallLengthProperties = new BlobProperties()
+            {
+                ContentLength = 100
+            };
+
+            SetupDownload(blockClient, dataSource);
+
+            PartitionedDownloader downloader = new PartitionedDownloader(blockClient.Object, new StorageTransferOptions() { MaximumTransferLength = 10}, 20);
+
+            Response<BlobProperties> result = await InvokeDownloadToAsync(downloader, stream);
+
+            // First block request is 20, and 10 for every block after that.
+            Assert.AreEqual(dataSource.Requests.Count, 9);
+            AssertContent(100, stream);
+            AssertResult(result, 100);
+        }
+
+        private void AssertResult(Response<BlobProperties> result, long expectedLength)
+        {
+            Assert.NotNull(result.GetRawResponse());
+            var value = result.Value;
+            Assert.AreEqual(value.ContentLength, expectedLength);
+
+            Assert.AreNotEqual(value.LastModified, default(DateTimeOffset));
+            Assert.AreNotEqual(value.Metadata, default);
+            Assert.AreNotEqual(value.BlobType, default(BlobType));
+            Assert.AreNotEqual(value.CopyCompletedOn, default(DateTimeOffset));
+            Assert.AreNotEqual(value.CopyStatusDescription, default);
+            Assert.AreNotEqual(value.CopyId, default);
+            Assert.AreNotEqual(value.CopyProgress, default);
+            Assert.AreNotEqual(value.CopySource, default(Uri));
+            Assert.AreNotEqual(value.CopyStatus, default(CopyStatus));
+            Assert.AreNotEqual(value.LeaseDuration, default(LeaseDurationType));
+            Assert.AreNotEqual(value.LeaseState, default(LeaseState));
+            Assert.AreNotEqual(value.LeaseStatus, default(LeaseStatus));
+            Assert.AreNotEqual(value.ContentType, default);
+            Assert.AreNotEqual(value.ETag, default(ETag));
+            Assert.AreNotEqual(value.ContentHash, default);
+            Assert.AreNotEqual(value.ContentEncoding, default);
+            Assert.AreNotEqual(value.ContentDisposition, default);
+            Assert.AreNotEqual(value.ContentLanguage, default);
+            Assert.AreNotEqual(value.CacheControl, default);
+            Assert.AreNotEqual(value.BlobSequenceNumber, default(long));
+            Assert.AreNotEqual(value.AcceptRanges, default);
+            Assert.AreNotEqual(value.BlobCommittedBlockCount, default(int));
+            Assert.AreNotEqual(value.IsServerEncrypted, default(bool));
+            Assert.AreNotEqual(value.EncryptionKeySha256, default);
+        }
+
+        [Test]
+        public async Task IncludesEtagInConditions()
+        {
+            MemoryStream stream = new MemoryStream();
+            MockDataSource dataSource = new MockDataSource(100);
+            Mock<BlobBaseClient> blockClient = new Mock<BlobBaseClient>(MockBehavior.Strict);
+            BlobProperties properties = new BlobProperties()
+            {
+                ContentLength = 100,
+                ETag = s_etag
+            };
+
+            SetupDownload(blockClient, dataSource);
+
+            PartitionedDownloader downloader = new PartitionedDownloader(blockClient.Object, new StorageTransferOptions() { MaximumTransferLength = 10}, 10);
+
+            Response<BlobProperties> result = await InvokeDownloadToAsync(downloader, stream);
+
+            Assert.AreEqual(dataSource.Requests.Count, 10);
+            AssertContent(100, stream);
+            AssertResult(result, 100);
+
+            bool first = true;
+            foreach ((HttpRange Range, BlobRequestConditions Conditions) request in dataSource.Requests)
+            {
+                Assert.AreEqual(s_conditions.LeaseId, request.Conditions.LeaseId);
+                Assert.AreEqual(s_conditions.IfModifiedSince, request.Conditions.IfModifiedSince);
+                Assert.AreEqual(s_conditions.IfUnmodifiedSince, request.Conditions.IfUnmodifiedSince);
+                Assert.AreEqual(s_conditions.IfNoneMatch, request.Conditions.IfNoneMatch);
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    Assert.AreEqual(s_etag, request.Conditions.IfMatch);
+                }
+            }
+        }
+
+        [Test]
+        public void SurfacesDownloadExceptions()
+        {
+            Exception e = new Exception();
+
+            MemoryStream stream = new MemoryStream();
+            Mock<BlobBaseClient> blockClient = new Mock<BlobBaseClient>(MockBehavior.Strict);
+            BlobProperties smallLengthProperties = new BlobProperties()
+            {
+                ContentLength = 100
+            };
+
+            if (_async)
+            {
+                blockClient.Setup(c => c.DownloadAsync(It.IsAny<HttpRange>(), It.IsAny<BlobRequestConditions>(), false, s_cancellationToken))
+                    .ThrowsAsync(e);
+            }
+            else
+            {
+                blockClient.Setup(c => c.Download(It.IsAny<HttpRange>(), It.IsAny<BlobRequestConditions>(), false, s_cancellationToken))
+                    .Throws(e);
+            }
+
+            PartitionedDownloader downloader = new PartitionedDownloader(blockClient.Object, new StorageTransferOptions() { MaximumTransferLength = 10}, 20);
+
+            Exception thrown = Assert.ThrowsAsync<Exception>(async () => await InvokeDownloadToAsync(downloader, stream));
+
+            Assert.AreSame(e, thrown);
+        }
+
+        private void AssertContent(int expectedLength, MemoryStream stream)
+        {
+            Assert.AreEqual(expectedLength, stream.Length);
+
+            byte[] array = stream.ToArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual((byte)i, array[i]);
+            }
+        }
+
+        private void SetupDownload(Mock<BlobBaseClient> blockClient, MockDataSource dataSource)
+        {
+            if (_async)
+            {
+                blockClient.Setup(c => c.DownloadAsync(It.IsAny<HttpRange>(), It.IsAny<BlobRequestConditions>(), false, s_cancellationToken))
+                    .Returns<HttpRange, BlobRequestConditions, bool, CancellationToken>(dataSource.GetStreamAsync);
+            }
+            else
+            {
+                blockClient.Setup(c => c.Download(It.IsAny<HttpRange>(), It.IsAny<BlobRequestConditions>(), false, s_cancellationToken))
+                    .Returns<HttpRange, BlobRequestConditions, bool, CancellationToken>(dataSource.GetStream);
+            }
+        }
+
+        private async Task<Response<BlobProperties>> InvokeDownloadToAsync(PartitionedDownloader downloader, Stream stream)
+        {
+            if (_async)
+            {
+                return await downloader.DownloadToAsync(stream, s_conditions, s_cancellationToken);
+            }
+            else
+            {
+                return downloader.DownloadTo(stream, s_conditions, s_cancellationToken);
+            }
+        }
+
+        private class MockDataSource
+        {
+            private readonly int _length;
+
+            public List<(HttpRange Range, BlobRequestConditions Conditions)> Requests { get; } = new List<(HttpRange Range, BlobRequestConditions Conditions)>();
+
+            public MockDataSource(int length)
+            {
+                _length = length;
+            }
+
+            public async Task<Response<BlobDownloadInfo>> GetStreamAsync(HttpRange range, BlobRequestConditions conditions = default, bool hash = default, CancellationToken token = default)
+            {
+                await Task.Delay(25);
+                return GetStream(range, conditions, hash, token);
+            }
+
+            public HttpRange FullRange => new HttpRange(0, _length);
+
+            public Response<BlobDownloadInfo> GetStream(HttpRange range, BlobRequestConditions conditions, bool hash, CancellationToken token)
+            {
+                lock (Requests)
+                {
+                    Requests.Add((range, conditions));
+                }
+
+                var contentLength = Math.Min(range.Length.Value, _length);
+
+                var memoryStream = new MemoryStream();
+                for (int i = 0; i < contentLength; i++)
+                {
+                    if (i > _length)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    memoryStream.WriteByte((byte)(range.Offset + i));
+                }
+
+                memoryStream.Position = 0;
+
+                return Response.FromValue(new BlobDownloadInfo(new FlattenedDownloadProperties()
+                {
+                    ContentRange = $"bytes {range.Offset}-{range.Offset + contentLength}/{_length}",
+                    ContentLength = contentLength,
+                    Content = memoryStream,
+                    LastModified = DateTimeOffset.Now,
+                    Metadata = new Dictionary<string, string>() { { "meta", "data"}},
+                    BlobType = BlobType.Page,
+                    CopyStatusDescription = "test",
+                    CopyId = "test",
+                    CopyProgress = "test",
+                    CopySource = new Uri("http://example.com"),
+                    CopyStatus = CopyStatus.Failed,
+                    LeaseDuration = LeaseDurationType.Fixed,
+                    LeaseState = LeaseState.Expired,
+                    LeaseStatus = LeaseStatus.Unlocked,
+                    ContentType = "test",
+                    ETag = s_etag,
+                    ContentHash = new byte[]{ 1, 2, 3},
+                    ContentEncoding = "test",
+                    ContentDisposition = "test",
+                    ContentLanguage = "test",
+                    CacheControl = "test",
+                    BlobSequenceNumber = 12,
+                    AcceptRanges = "test",
+                    BlobCommittedBlockCount = 5,
+                    IsServerEncrypted = true,
+                    EncryptionKeySha256 = "test",
+                    CopyCompletionTime =  DateTimeOffset.Now
+                }), new MockResponse(200));
+            }
+        }
+    }
+}
