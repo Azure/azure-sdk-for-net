38c068c597d0ae42827e6e347e091f119392116a
JiaLin <jiadai@microsoft.com>
2019-10-29 11:33:18 +0800

    update AssemblyInfo version

--- sdk/appplatform/Microsoft.Azure.Management.AppPlatform/tests/Properties/AssemblyInfo.cs
+++ sdk/appplatform/Microsoft.Azure.Management.AppPlatform/tests/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
