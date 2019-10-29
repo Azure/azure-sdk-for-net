a58ebb6a8317c7007945f2627d37532228189d2e
Nesma <a-nebadr@microsoft.com>
2019-10-29 17:18:55 +0200

    Adding tests for new apis

--- sdk/cognitiveservices/Language.LUIS.Authoring/tests/BaseTest.cs
+++ sdk/cognitiveservices/Language.LUIS.Authoring/tests/BaseTest.cs
@@ -10,13 +10,13 @@ namespace LUIS.Authoring.Tests.Luis
 
     public abstract class BaseTest
     {
-        private const HttpRecorderMode mode = HttpRecorderMode.Record;
+        private const HttpRecorderMode mode = HttpRecorderMode.Playback;
 
         protected readonly Guid GlobalAppId = new Guid("6c859d36-47a0-4dd9-a2ab-7817e211646b");
         protected const string GlobalVersionId = "0.1";
         protected readonly Guid GlobalAppIdError = new Guid("86226c53-b7a6-416f-876b-226b2b5ab07d");
         protected readonly Guid GlobalNoneId = new Guid("731e7ac1-b1d4-4e4e-bc1b-d79f67e2b890");
-        protected const string AuthoringKey = "120f721e60454544a884022b1bc988a5";
+        protected const string AuthoringKey = "00000000000000000000000000000000";
         protected readonly string OwnerEmail = "a-nebadr@microsoft.com";
 
 
