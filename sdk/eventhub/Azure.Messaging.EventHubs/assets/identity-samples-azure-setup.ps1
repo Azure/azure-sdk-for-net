def933cbb5ccfaf04e4e399828cb05fd4f05faeb
Alberto De Natale <alberto.de.natale@avanade.com>
2019-11-14 06:43:11 +0100

    [Event Hubs Client] Samples, moved line in output

--- sdk/eventhub/Azure.Messaging.EventHubs/assets/identity-samples-azure-setup.ps1
+++ sdk/eventhub/Azure.Messaging.EventHubs/assets/identity-samples-azure-setup.ps1
@@ -181,8 +181,6 @@ try
     Write-Host "After building the sample project, you can run it using the Azure resources that you just created with the following command."
     Write-Host "You can also launch the Samples project from within Visual Studio and enter the Azure information when prompted or set them in the project properties."
     Write-Host ""
-    Write-Host "After building the solution, you may find the executable under the following folder: ""artifacts\bin\Azure.Messaging.EventHubs.Samples\Debug\netcoreapp2.1"""
-    Write-Host ""
     Write-Host "dotnet Azure.Messaging.EventHubs.Samples.dll ``"
     Write-Host "--ConnectionString ""$($namespaceInformation.PrimaryConnectionString)"" ``"
     Write-Host "--FullyQualifiedNamespace ""$($namespaceInformation.FullyQualifiedDomainName)"" ``"
@@ -190,6 +188,8 @@ try
     Write-Host "--Tenant ""$($subscription.TenantId)"" ``"
     Write-Host "--Client ""$($principal.ApplicationId)"" ``"
     Write-Host "--Secret ""$($credentials.Password)"""
+    Write-Host ""
+    Write-Host "After building the solution, you may find the executable under the following folder: ""artifacts\bin\Azure.Messaging.EventHubs.Samples\Debug\netcoreapp2.1"""
     Write-Host ""
 }
 catch 
