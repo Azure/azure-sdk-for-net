ed2409d7e62ce51a236fa230e21ff3547095adcc
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2019-11-21 22:52:02 -0800

    enable storage code gen in CI

--- eng/CodeChecks.ps1
+++ eng/CodeChecks.ps1
@@ -72,18 +72,19 @@ try {
         & $PSScriptRoot\Update-Snippets.ps1 @script:PSBoundParameters
     }
 
-    Write-Host "Re-generating lisgings"
+    Write-Host "Re-generating listings"
     Invoke-Block {
         & $PSScriptRoot\Export-API.ps1 @script:PSBoundParameters
     }
 
     Write-Host "Re-generating clients"
     Invoke-Block {
-        # https://github.com/Azure/azure-sdk-for-net/issues/8584
-        # & $repoRoot\storage\generate.ps1
+        & $repoRoot\storage\generate.ps1
     }
 
     Write-Host "git diff"
+    # prevent warning related to EOL differences which triggers an exception for some reason
+    & git config core.safecrlf false
     & git diff --ignore-space-at-eol --exit-code
     if ($LastExitCode -ne 0) {
         $status = git status -s | Out-String
