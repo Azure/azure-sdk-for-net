c75248ea30169deceb9a3373399134e7460b30a0
seanmcc-msft <seanmcc@microsoft.com>
2019-11-22 14:06:58 -0800

    PR comments

--- sdk/storage/Azure.Storage.Files.Shares/tests/FileTestBase.cs
+++ sdk/storage/Azure.Storage.Files.Shares/tests/FileTestBase.cs
@@ -22,7 +22,7 @@ namespace Azure.Storage.Files.Shares.Tests
         public FileTestBase(bool async) : this(async, null) { }
 
         public FileTestBase(bool async, RecordedTestMode? mode = null)
-            : base(async, RecordedTestMode.Live)
+            : base(async, mode)
         {
         }
 
