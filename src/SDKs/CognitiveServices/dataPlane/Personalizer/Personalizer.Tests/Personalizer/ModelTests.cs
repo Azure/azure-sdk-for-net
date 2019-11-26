630c4a5889b387b7a28e7c27addfc340f3b213f6
Dwaipayan Mukherjee <dwaijam@gmail.com>
2019-11-25 20:58:44 -0800

    Add tests for all personalizer apis

---
+++ src/SDKs/CognitiveServices/dataPlane/Personalizer/Personalizer.Tests/Personalizer/ModelTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.CognitiveServices.Personalizer.Models;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Xunit;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.CognitiveServices.Personalizer.Tests
+{
+    public class ModelTests : BaseTests
+    {
+        [Fact]
+        public async Task GetModel()
+        {
+            using (MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "GetModel");
+
+                IPersonalizerClient client = GetClient(HttpMockServer.CreateInstance());
+
+                Stream stream = await client.Model.GetAsync();
+                Assert.NotEqual(-1 , stream.ReadByte());
+            }
+        }
+
+        [Fact]
+        public async Task ResetModel()
+        {
+            using (MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "ResetModel");
+
+                IPersonalizerClient client = GetClient(HttpMockServer.CreateInstance());
+
+                await client.Model.ResetAsync();
+            }
+        }
+
+        [Fact]
+        public async Task GetModelProperties()
+        {
+            using (MockContext.Start(this.GetType().FullName))
+            {
+                HttpMockServer.Initialize(this.GetType().FullName, "GetModelProperties");
+
+                IPersonalizerClient client = GetClient(HttpMockServer.CreateInstance());
+
+                ModelProperties modelProperties = await client.Model.GetPropertiesAsync();
+                Assert.True(modelProperties.CreationTime != null);
+                Assert.True(modelProperties.LastModifiedTime != null);
+            }
+        }
+    }
+}
