e084fca7450a0ab81fe333f87ba8f04caecb56a0
Anne Loomis Thompson <annelo@microsoft.com>
2019-12-11 08:53:58 -0800

    project file fix

--- sdk/cognitiveservices/textanalytics/Azure.AI.TextAnalytics/tests/TextAnalyticsClientTests.cs
+++ sdk/cognitiveservices/textanalytics/Azure.AI.TextAnalytics/tests/TextAnalyticsClientTests.cs
@@ -1,21 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
-using Azure.Core;
 using Azure.Core.Pipeline;
 using Azure.Core.Testing;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Azure.AI.TextAnalytics.Tests
 {
