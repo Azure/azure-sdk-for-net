bbabe42541eb4c914c493a41738a863d39488e26
Mike Harder <mharder@microsoft.com>
2019-12-10 18:04:20 -0800

    Set per-test timeout to 20 minutes

--- sdk/storage/Azure.Storage.Files.Shares/tests/AssemblyInfo.cs
+++ sdk/storage/Azure.Storage.Files.Shares/tests/AssemblyInfo.cs
@@ -1,11 +1,5 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("d78fd724-3d57-4ecd-ac71-7450e1280c3c")]
-
 // Set per-test timeout to 20 minutes to prevent a single test from hanging the whole suite
 [assembly: NUnit.Framework.Timeout(20 * 60 * 1000)]
