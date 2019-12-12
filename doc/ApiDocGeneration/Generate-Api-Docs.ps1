8c3f2609a5ad302cd35f65408250035cf74b5446
Chidozie Ononiwu <chononiw@microsoft.com>
2019-12-12 11:15:59 -0800

    More Clean up Work

--- doc/ApiDocGeneration/Generate-Api-Docs.ps1
+++ doc/ApiDocGeneration/Generate-Api-Docs.ps1
@@ -44,24 +44,30 @@ mkdir "$($BinDirectory)/$($SafeName)/dll-xml-output"
 mkdir "$($BinDirectory)/$($SafeName)/dll-yaml-output"
 mkdir "$($BinDirectory)/$($SafeName)/docfx-output"
 
+$ApiDir = "$($BinDirectory)/$($SafeName)/dll-docs/my-api"
+$ApiDependenciesDir = "$($BinDirectory)/$($SafeName)/dll-docs/dependencies/my-api"
+$XmlOutDir = "$($BinDirectory)/$($SafeName)/dll-xml-output"
+$YamlOutDir = "$($BinDirectory)/$($SafeName)/dll-yaml-output"
+$DocOutDir = "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project"
+
 if ($LibType -eq '') { 
     Write-Verbose "Build Packages for Doc Generation - Client"
-    dotnet build "$($RepoRoot)/eng/service.proj" /p:ServiceDirectory=$PackageLocation /p:IncludeTests=false /p:IncludeSamples=false /p:OutputPath="$($BinDirectory)/$($SafeName)/dll-docs/my-api" /p:TargetFramework=netstandard2.0
+    dotnet build "$($RepoRoot)/eng/service.proj" /p:ServiceDirectory=$PackageLocation /p:IncludeTests=false /p:IncludeSamples=false /p:OutputPath=$ApiDir /p:TargetFramework=netstandard2.0
 
     Write-Verbose "Include client Dependencies"
-    dotnet build "$($RepoRoot)/eng/service.proj" /p:ServiceDirectory=$PackageLocation /p:IncludeTests=false /p:IncludeSamples=false /p:OutputPath="$($BinDirectory)/$($SafeName)/dll-docs/dependencies/my-api" /p:TargetFramework=netstandard2.0 /p:CopyLocalLockFileAssemblies=true
+    dotnet build "$($RepoRoot)/eng/service.proj" /p:ServiceDirectory=$PackageLocation /p:IncludeTests=false /p:IncludeSamples=false /p:OutputPath=$ApiDependenciesDir /p:TargetFramework=netstandard2.0 /p:CopyLocalLockFileAssemblies=true
 }
 
 if ($LibType -eq 'Management') { # Management Package
     Write-Verbose "Build Packages for Doc Generation - Management"
-    dotnet msbuild "eng/mgmt.proj" /p:scope=$PackageLocation /p:OutputPath="$($BinDirectory)/$($SafeName)/dll-docs/my-api" -maxcpucount:1 -nodeReuse:false
+    dotnet msbuild "eng/mgmt.proj" /p:scope=$PackageLocation /p:OutputPath=$ApiDir -maxcpucount:1 -nodeReuse:false
 
     Write-Verbose "Include Management Dependencies"
-    dotnet msbuild "eng/mgmt.proj" /p:scope=$PackageLocation /p:OutputPath="$($BinDirectory)/$($SafeName)/dll-docs/dependencies/my-api" /p:CopyLocalLockFileAssemblies=true -maxcpucount:1 -nodeReuse:false
+    dotnet msbuild "eng/mgmt.proj" /p:scope=$PackageLocation /p:OutputPath=$ApiDependenciesDir /p:CopyLocalLockFileAssemblies=true -maxcpucount:1 -nodeReuse:false
 }
 
 Write-Verbose "Remove all unneeded artifacts from build output directory"
-Remove-Item –Path "$($BinDirectory)/$($SafeName)/dll-docs/my-api/*" -Include * -Exclude "$($ArtifactName).dll", "$($ArtifactName).xml"
+Remove-Item –Path "$($ApiDir)/*" -Include * -Exclude "$($ArtifactName).dll", "$($ArtifactName).xml"
 
 Write-Verbose "Initialize Frameworks File"
 & "$($BinDirectory)/mdoc/mdoc.exe" fx-bootstrap "$($BinDirectory)/$($SafeName)/dll-docs"
@@ -70,41 +76,41 @@ Write-Verbose "Include XML Files"
 & "$($BinDirectory)/PopImport/popimport.exe" -f "$($BinDirectory)/$($SafeName)/dll-docs"
 
 Write-Verbose "Produce ECMAXML"
-& "$($BinDirectory)/mdoc/mdoc.exe" update -fx "$($BinDirectory)/$($SafeName)/dll-docs" -o "$($BinDirectory)/$($SafeName)/dll-xml-output" --debug -lang docid -lang vb.net -lang fsharp --delete
+& "$($BinDirectory)/mdoc/mdoc.exe" update -fx "$($BinDirectory)/$($SafeName)/dll-docs" -o $XmlOutDir --debug -lang docid -lang vb.net -lang fsharp --delete
 
 Write-Verbose "Generate YAML"
-& "$($BinDirectory)/ECMA2Yml/ECMA2Yaml.exe" -s "$($BinDirectory)/$($SafeName)/dll-xml-output" -o "$($BinDirectory)/$($SafeName)/dll-yaml-output"
+& "$($BinDirectory)/ECMA2Yml/ECMA2Yaml.exe" -s $XmlOutDir -o $YamlOutDir
 
 Write-Verbose "Provision DocFX Directory"
-& "$($BinDirectory)/docfx/docfx.exe" init -q -o "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project"
+& "$($BinDirectory)/docfx/docfx.exe" init -q -o "$($DocOutDir)"
 
 Write-Verbose "Copy over Package ReadMe"
 $PkgReadMePath = "$($RepoRoot)/sdk/$($PackageLocation)/README.md"
 if ([System.IO.File]::Exists($PkgReadMePath))
 {
-    Copy-Item $PkgReadMePath -Destination "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/api/index.md" -Force
-    Copy-Item $PkgReadMePath -Destination "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/index.md" -Force
+    Copy-Item $PkgReadMePath -Destination "$($DocOutDir)/api/index.md" -Force
+    Copy-Item $PkgReadMePath -Destination "$($DocOutDir)/index.md" -Force
 }
 else
 {
-    New-Item "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/api/index.md" -Force
-    Add-Content -Path "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/api/index.md" -Value "This Package Contains no Readme."
-    Copy-Item "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/api/index.md" -Destination "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/index.md"-Force
+    New-Item "$($DocOutDir)/api/index.md" -Force
+    Add-Content -Path "$($DocOutDir)/api/index.md" -Value "This Package Contains no Readme."
+    Copy-Item "$($DocOutDir)/api/index.md" -Destination "$($DocOutDir)/index.md"-Force
     Write-Verbose "Package ReadMe was not found"
 }
 
 Write-Verbose "Copy over generated yml and other assets"
-Copy-Item "$($BinDirectory)/$($SafeName)/dll-yaml-output/*"-Destination "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/api" -Recurse
-Copy-Item "$($RepoRoot)/doc/ApiDocGeneration/assets/docfx.json" -Destination "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project" -Recurse -Force
-New-Item -Path "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project" -Name templates -ItemType directory
-Copy-Item "$($RepoRoot)/doc/ApiDocGeneration/templates/**" -Destination "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/templates" -Recurse -Force
+Copy-Item "$($YamlOutDir)/*"-Destination "$($DocOutDir)/api" -Recurse
+Copy-Item "$($RepoRoot)/doc/ApiDocGeneration/assets/docfx.json" -Destination "$($DocOutDir)" -Recurse -Force
+New-Item -Path "$($DocOutDir)" -Name templates -ItemType directory
+Copy-Item "$($RepoRoot)/doc/ApiDocGeneration/templates/**" -Destination "$($DocOutDir)/templates" -Recurse -Force
 
 Write-Verbose "Create Toc for Site Navigation"
-New-Item "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/toc.yml" -Force
-Add-Content -Path "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/toc.yml" -Value "- name: $ArtifactName`r`n  href: api/`r`n  homepage: api/index.md"
+New-Item "$($DocOutDir)/toc.yml" -Force
+Add-Content -Path "$($DocOutDir)/toc.yml" -Value "- name: $ArtifactName`r`n  href: api/`r`n  homepage: api/index.md"
 
 Write-Verbose "Build Doc Content"
-& "$($BinDirectory)/docfx/docfx.exe" build "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/docfx.json"
+& "$($BinDirectory)/docfx/docfx.exe" build "$($DocOutDir)/docfx.json"
 
 Write-Verbose "Copy over site Logo"
-Copy-Item "$($RepoRoot)/doc/ApiDocGeneration/assets/logo.svg" -Destination "$($BinDirectory)/$($SafeName)/docfx-output/docfx_project/_site" -Recurse -Force
+Copy-Item "$($RepoRoot)/doc/ApiDocGeneration/assets/logo.svg" -Destination "$($DocOutDir)/_site" -Recurse -Force
