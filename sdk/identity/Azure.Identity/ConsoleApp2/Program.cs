abf53b51e3d73e3576abf24ee9ed31be3570b662
wantedfast <inaccuarcy@gmail.com>
2019-12-12 18:57:35 +0800

    add newline

---
+++ sdk/identity/Azure.Identity/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyClient keyClient = new KeyClient(new Uri("https://javatestpre.vault.azure.net/"), new DefaultAzureCredential());
+            var key = keyClient.CreateKey($"key-{Guid.NewGuid()}", KeyType.Rsa);
+            Console.WriteLine(key.Value.Name);
+        }
+    }
+}
