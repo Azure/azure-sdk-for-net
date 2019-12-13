26325accd7fa7112403a9496e1fc8ce47439fa55
Mike Harder <mharder@microsoft.com>
2019-12-13 15:10:01 -0800

    Set per-test timeout in shared AssemblyInfo.cs

--- sdk/storage/Azure.Storage.Blobs.Batch/tests/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-// Set per-test timeout to 20 minutes to prevent a single test from hanging the whole suite
-[assembly: NUnit.Framework.Timeout(20 * 60 * 1000)]
--- sdk/storage/Azure.Storage.Blobs/tests/AssemblyInfo.cs
+++ sdk/storage/Azure.Storage.Blobs/tests/AssemblyInfo.cs
@@ -6,6 +6,3 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("d78fd724-3d57-4ecd-ac71-7450e1280c3c")]
-
-// Set per-test timeout to 20 minutes to prevent a single test from hanging the whole suite
-[assembly: NUnit.Framework.Timeout(20 * 60 * 1000)]
