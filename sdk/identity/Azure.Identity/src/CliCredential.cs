5922ee61a5e93830a5ed7746f987b41e2dd417f0
Geralt Wang <v-xinlwa@microsoft.com>
2019-12-25 16:05:41 +0800

    Revert "Add Nunit Test of CliCredential"
    This reverts commit 20603bdba4939980934c52d45c9d7aeb3dffa17d.

--- sdk/identity/Azure.Identity/src/CliCredential.cs
+++ sdk/identity/Azure.Identity/src/CliCredential.cs
@@ -7,10 +7,12 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Globalization;
+using System.Diagnostics;
 using System.Text.Json;
 using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
 
 namespace Azure.Identity
 {
@@ -23,24 +25,26 @@ namespace Azure.Identity
         private const string WinAzureCLIError = "'az' is not recognized";
 
         private readonly CredentialPipeline _pipeline;
-        private readonly CliCredentialClient _client;
 
         /// <summary>
         /// Create an instance of CliCredential class.
         /// </summary>
         public CliCredential()
-            : this(CredentialPipeline.GetInstance(null), new CliCredentialClient())
+            : this(CredentialPipeline.GetInstance(null))
         { }
+
+        /// <summary>
+        /// Create an instance of CliCredential class.
+        /// </summary>
+        /// <param name="options"></param>
+        public CliCredential(TokenCredentialOptions options)
+         : this(CredentialPipeline.GetInstance(options))
+        {
+        }
 
         internal CliCredential(CredentialPipeline pipeline)
-            : this(pipeline, new CliCredentialClient())
-        { }
-
-        internal CliCredential(CredentialPipeline pipeline, CliCredentialClient client)
         {
             _pipeline = pipeline;
-
-            _client = client;
         }
 
         /// <summary>
@@ -77,30 +81,69 @@ namespace Azure.Identity
 
         private async ValueTask<ExtendedAccessToken> GetTokenImplAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
         {
-            using CredentialDiagnosticScope scope = _pipeline.StartGetTokenScope("Azure.Identity.CliCredential.GetToken", requestContext);
+            using CredentialDiagnosticScope scope = _pipeline.StartGetTokenScope("Azure.Identity.CLICredential.GetToken", requestContext);
 
             try
             {
+                string fileName = string.Empty;
+                string argument = string.Empty;
                 string command = ScopeUtilities.ScopesToResource(requestContext.Scopes);
-                string extendCommand = $"az account get-access-token --resource {command}";
+                string extendCommand = "az account get-access-token --resource " + command;
 
-                (string output, int exitCode) = _client.CreateProcess(extendCommand);
-
-                if (exitCode != 0)
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 {
-                    bool winError = output.StartsWith(WinAzureCLIError, StringComparison.CurrentCultureIgnoreCase);
-                    string pattter = "az:(.*)not found";
-                    bool otherError = Regex.IsMatch(output, pattter);
-
-                    if (winError || otherError)
-                    {
-                        throw new AuthenticationFailedException(AzureCLINotInstalled);
-                    }
-
-                    throw new AuthenticationFailedException(output);
+                    fileName = "cmd";
+                    argument = $"/c \"{extendCommand}\"";
+                }
+                else
+                {
+                    fileName = "/bin/sh";
+                    argument = $"-c \"{extendCommand}\"";
                 }
 
-                byte[] byteArrary = Encoding.ASCII.GetBytes(output);
+                Process proc = new Process();
+
+                ProcessStartInfo procStartInfo = new ProcessStartInfo()
+                {
+                    FileName = fileName,
+                    Arguments = argument,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                proc.StartInfo = procStartInfo;
+
+                StringBuilder stdOutput = new StringBuilder();
+                proc.OutputDataReceived += new DataReceivedEventHandler((sender, e) => stdOutput.AppendLine(e.Data));
+
+                StringBuilder stdError = new StringBuilder();
+                proc.ErrorDataReceived += new DataReceivedEventHandler((sender, e) => stdError.AppendLine(e.Data));
+
+                proc.Start();
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
+                proc.WaitForExit();
+
+                string cliResult = stdOutput.ToString().Trim();
+                string errorMessage = stdError.ToString().Trim();
+
+                if (proc.ExitCode != 0)
+                {
+                    bool winErrorFlag = errorMessage.StartsWith(WinAzureCLIError, StringComparison.CurrentCultureIgnoreCase);
+                    string pattter = "az:(.*)not found";
+                    bool otherErrorFlag = Regex.IsMatch(errorMessage, pattter);
+
+                    if (winErrorFlag || otherErrorFlag)
+                    {
+                        throw new CredentialUnavailableException(AzureCLINotInstalled);
+                    }
+
+                    throw new CredentialUnavailableException(errorMessage);
+                }
+
+                byte[] byteArrary = Encoding.ASCII.GetBytes(cliResult);
                 MemoryStream stream = new MemoryStream(byteArrary);
 
                 Dictionary<string, string> result = await JsonSerializer.DeserializeAsync<Dictionary<string, string>>(stream, null, cancellationToken);
