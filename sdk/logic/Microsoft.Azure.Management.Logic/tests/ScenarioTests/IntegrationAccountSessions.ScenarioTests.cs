f8f898513db2f0d59713fe4346c437898a38e7e8
Rohitha Hewawasam <rohithah@outlook.com>
2020-01-02 18:23:05 -0800

    Rohithah/logic sdk update (#9211)
    * Added ISE reference to workflow
    
    * Added generated files
    
    * Addressed CR comments
    
    * update tests, tags and version
    
    * fix tests
    
    * update assembly and file versions
    
    Co-authored-by: visharm <37120358+visharm@users.noreply.github.com>

--- sdk/logic/Microsoft.Azure.Management.Logic/tests/ScenarioTests/IntegrationAccountSessions.ScenarioTests.cs
+++ sdk/logic/Microsoft.Azure.Management.Logic/tests/ScenarioTests/IntegrationAccountSessions.ScenarioTests.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for
 // license information.
 
@@ -20,7 +20,7 @@ namespace Test.Azure.Management.Logic
         [Fact]
         public void IntegrationAccountSessions_Create_OK()
         {
-            using (var context = MockContext.Start(this.TestClassType))
+            using (var context = MockContext.Start(this.TestClassName))
             {
                 var client = this.GetClient(context);
                 this.CleanResourceGroup(client);
@@ -45,7 +45,7 @@ namespace Test.Azure.Management.Logic
         [Fact]
         public void IntegrationAccountSessions_Get_OK()
         {
-            using (var context = MockContext.Start(this.TestClassType))
+            using (var context = MockContext.Start(this.TestClassName))
             {
                 var client = this.GetClient(context);
                 this.CleanResourceGroup(client);
@@ -74,7 +74,7 @@ namespace Test.Azure.Management.Logic
         [Fact]
         public void IntegrationAccountSessions_List_OK()
         {
-            using (var context = MockContext.Start(this.TestClassType))
+            using (var context = MockContext.Start(this.TestClassName))
             {
                 var client = this.GetClient(context);
                 this.CleanResourceGroup(client);
@@ -118,7 +118,7 @@ namespace Test.Azure.Management.Logic
         [Fact]
         public void IntegrationAccountSessions_Update_OK()
         {
-            using (var context = MockContext.Start(this.TestClassType))
+            using (var context = MockContext.Start(this.TestClassName))
             {
                 var client = this.GetClient(context);
                 this.CleanResourceGroup(client);
@@ -149,7 +149,7 @@ namespace Test.Azure.Management.Logic
         [Fact]
         public void IntegrationAccountSessions_Delete_OK()
         {
-            using (var context = MockContext.Start(this.TestClassType))
+            using (var context = MockContext.Start(this.TestClassName))
             {
                 var client = this.GetClient(context);
                 this.CleanResourceGroup(client);
@@ -166,7 +166,7 @@ namespace Test.Azure.Management.Logic
                     session);
 
                 client.IntegrationAccountSessions.Delete(Constants.DefaultResourceGroup, integrationAccountName, sessionName);
-                Assert.Throws<CloudException>(() => client.IntegrationAccountSessions.Get(Constants.DefaultResourceGroup, integrationAccountName, sessionName));
+                Assert.Throws<ErrorResponseException>(() => client.IntegrationAccountSessions.Get(Constants.DefaultResourceGroup, integrationAccountName, sessionName));
 
                 client.IntegrationAccounts.Delete(Constants.DefaultResourceGroup, integrationAccountName);
             }
@@ -175,7 +175,7 @@ namespace Test.Azure.Management.Logic
         [Fact]
         public void IntegrationAccountSessions_DeleteWhenDeleteIntegrationAccount_OK()
         {
-            using (var context = MockContext.Start(this.TestClassType))
+            using (var context = MockContext.Start(this.TestClassName))
             {
                 var client = this.GetClient(context);
                 this.CleanResourceGroup(client);
@@ -192,7 +192,7 @@ namespace Test.Azure.Management.Logic
                     session);
 
                 client.IntegrationAccounts.Delete(Constants.DefaultResourceGroup, integrationAccountName);
-                Assert.Throws<CloudException>(() => client.IntegrationAccountSessions.Get(Constants.DefaultResourceGroup, integrationAccountName, sessionName));
+                Assert.Throws<ErrorResponseException>(() => client.IntegrationAccountSessions.Get(Constants.DefaultResourceGroup, integrationAccountName, sessionName));
             }
         }
 
