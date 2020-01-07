0c4ab38f98550ec116e084f5514c940e385cc2d2
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-01-06 16:57:08 -0800

    Update request header date for retries

--- sdk/storage/Azure.Storage.Common/tests/Shared/TestExceptionPolicy.cs
+++ sdk/storage/Azure.Storage.Common/tests/Shared/TestExceptionPolicy.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.Pipeline;
@@ -14,9 +15,11 @@ namespace Azure.Storage.Test
     {
         public List<string> HostsSetInRequests { get; private set; }
 
+        public List<DateTime> DatesSetInRequests { get; private set; }
+
         private int CurrentInvocationNumber { get; set; }
 
-        private int NumberOfReadFailuresToSimulate { get; set; }
+        private int NumberOfFailuresToSimulate { get; set; }
 
         private Uri SecondaryUri { get; set; }
 
@@ -24,18 +27,27 @@ namespace Azure.Storage.Test
 
         private List<RequestMethod> TrackedRequestMethods { get; set; }
 
-        public TestExceptionPolicy(int numberOfReadFailuresToSimulate, Uri secondaryUri, bool simulate404 = false, List<RequestMethod> trackedRequestMethods = null)
+        private readonly int _delayBetweenAttempts;
+
+        public TestExceptionPolicy(
+            int numberOfFailuresToSimulate,
+            Uri secondaryUri,
+            bool simulate404 = false,
+            List<RequestMethod> trackedRequestMethods = null,
+            int delayBetweenAttempts = default)
         {
-            NumberOfReadFailuresToSimulate = numberOfReadFailuresToSimulate;
+            NumberOfFailuresToSimulate = numberOfFailuresToSimulate;
             Simulate404 = simulate404;
             SecondaryUri = secondaryUri;
             HostsSetInRequests = new List<string>();
+            DatesSetInRequests = new List<DateTime>();
             TrackedRequestMethods = trackedRequestMethods ?? new List<RequestMethod>(new RequestMethod[] { RequestMethod.Get, RequestMethod.Head });
+            _delayBetweenAttempts = delayBetweenAttempts;
         }
 
         public override void Process(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
         {
-            if (!SimulateFailure(message))
+            if (!SimulateFailureAsync(message, false).EnsureCompleted())
             {
                 ProcessNext(message, pipeline);
             }
@@ -43,19 +55,34 @@ namespace Azure.Storage.Test
 
         public override async ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
         {
-            if (!SimulateFailure(message))
+            if (!await SimulateFailureAsync(message, true))
             {
                 await ProcessNextAsync(message, pipeline).ConfigureAwait(false);
             }
         }
 
-        private bool SimulateFailure(HttpMessage message)
+        private async Task<bool> SimulateFailureAsync(HttpMessage message, bool async)
         {
             if (TrackedRequestMethods.Contains(message.Request.Method))
             {
+                if (_delayBetweenAttempts > 0)
+                {
+                    if (async)
+                    {
+                        await Task.Delay(_delayBetweenAttempts);
+                    }
+                    else
+                    {
+                        Thread.Sleep(_delayBetweenAttempts);
+                    }
+                }
                 CurrentInvocationNumber++;
                 HostsSetInRequests.Add(message.Request.Uri.Host);
-                if (CurrentInvocationNumber <= NumberOfReadFailuresToSimulate)
+                if (message.Request.Headers.TryGetValue("x-ms-date", out string date))
+                {
+                    DatesSetInRequests.Add(Convert.ToDateTime(date));
+                }
+                if (CurrentInvocationNumber <= NumberOfFailuresToSimulate)
                 {
                     message.Response = new MockResponse(Simulate404 && message.Request.Uri.Host == SecondaryUri.Host ? 404 : 429);
                     return true;
