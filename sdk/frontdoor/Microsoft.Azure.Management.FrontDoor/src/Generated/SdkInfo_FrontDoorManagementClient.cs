df70631cc82635605412dec73841362dd0bf652b
SDK Automation <sdkautomation@microsoft.com>
2020-01-10 08:47:55 +0000

    Generated from 388db566203544de71b9f5fb0d6c6a4c54c79b6d
    Add new api-versions for frontdoor

--- sdk/frontdoor/Microsoft.Azure.Management.FrontDoor/src/Generated/SdkInfo_FrontDoorManagementClient.cs
+++ sdk/frontdoor/Microsoft.Azure.Management.FrontDoor/src/Generated/SdkInfo_FrontDoorManagementClient.cs
@@ -33,16 +33,5 @@ namespace Microsoft.Azure.Management.FrontDoor
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4407";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/frontdoor/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\Sources\\OSS\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "5e95a31678db88b8405e189df4aad8f9e9a24c45";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
