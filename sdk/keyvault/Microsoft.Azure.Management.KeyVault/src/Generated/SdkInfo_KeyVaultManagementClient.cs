1d8e527528b261a4d29c5caa51b5f84e130c6841
SDK Automation <sdkautomation@microsoft.com>
2020-01-23 03:48:15 +0000

    Generated from f8168dd0344ade2d5d4b7b253659061c23e8b6c0
    Changes on Key Vault Management Swagger for Private Endpoints

--- sdk/keyvault/Microsoft.Azure.Management.KeyVault/src/Generated/SdkInfo_KeyVaultManagementClient.cs
+++ sdk/keyvault/Microsoft.Azure.Management.KeyVault/src/Generated/SdkInfo_KeyVaultManagementClient.cs
@@ -19,9 +19,11 @@ namespace Microsoft.Azure.Management.KeyVault
           {
               return new Tuple<string, string, string>[]
               {
-                new Tuple<string, string, string>("KeyVault", "Operations", "2018-02-14"),
-                new Tuple<string, string, string>("KeyVault", "Vaults", "2018-02-14"),
-                new Tuple<string, string, string>("KeyVaultManagementClient", "Vaults", "2018-02-14"),
+                new Tuple<string, string, string>("KeyVault", "Operations", "2019-09-01"),
+                new Tuple<string, string, string>("KeyVault", "PrivateEndpointConnections", "2019-09-01"),
+                new Tuple<string, string, string>("KeyVault", "PrivateLinkResources", "2019-09-01"),
+                new Tuple<string, string, string>("KeyVault", "Vaults", "2019-09-01"),
+                new Tuple<string, string, string>("KeyVaultManagementClient", "Vaults", "2019-09-01"),
               }.AsEnumerable();
           }
       }
