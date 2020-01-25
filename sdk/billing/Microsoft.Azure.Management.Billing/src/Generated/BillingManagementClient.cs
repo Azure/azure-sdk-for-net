46c7856f868ab6e488319ab68bae5dc8fd86a980
SDK Automation <sdkautomation@microsoft.com>
2020-01-25 04:32:54 +0000

    Generated from 2004b8fb73763a7b3d6d7da955b722f4a3e5f0df
    Added AccountType
    
    Added AccountType

--- sdk/billing/Microsoft.Azure.Management.Billing/src/Generated/BillingManagementClient.cs
+++ sdk/billing/Microsoft.Azure.Management.Billing/src/Generated/BillingManagementClient.cs
@@ -97,6 +97,11 @@ namespace Microsoft.Azure.Management.Billing
         public virtual IAvailableBalancesOperations AvailableBalances { get; private set; }
 
         /// <summary>
+        /// Gets the IInstructionsOperations.
+        /// </summary>
+        public virtual IInstructionsOperations Instructions { get; private set; }
+
+        /// <summary>
         /// Gets the IBillingProfilesOperations.
         /// </summary>
         public virtual IBillingProfilesOperations BillingProfiles { get; private set; }
@@ -130,7 +135,17 @@ namespace Microsoft.Azure.Management.Billing
         /// Gets the ITransactionsOperations.
         /// </summary>
         public virtual ITransactionsOperations Transactions { get; private set; }
-        
+
+        /// <summary>
+        /// Gets the IDepartmentsOperations.
+        /// </summary>
+        public virtual IDepartmentsOperations Departments { get; private set; }
+
+        /// <summary>
+        /// Gets the IEnrollmentAccountsOperations.
+        /// </summary>
+        public virtual IEnrollmentAccountsOperations EnrollmentAccounts { get; private set; }
+
         /// <summary>
         /// Gets the IInvoicesOperations.
         /// </summary>
@@ -152,6 +167,21 @@ namespace Microsoft.Azure.Management.Billing
         public virtual IBillingPropertyOperations BillingProperty { get; private set; }
 
         /// <summary>
+        /// Gets the ITransfersOperations.
+        /// </summary>
+        public virtual ITransfersOperations Transfers { get; private set; }
+
+        /// <summary>
+        /// Gets the IPartnerTransfersOperations.
+        /// </summary>
+        public virtual IPartnerTransfersOperations PartnerTransfers { get; private set; }
+
+        /// <summary>
+        /// Gets the IRecipientTransfersOperations.
+        /// </summary>
+        public virtual IRecipientTransfersOperations RecipientTransfers { get; private set; }
+
+        /// <summary>
         /// Gets the IOperations.
         /// </summary>
         public virtual IOperations Operations { get; private set; }
@@ -170,6 +200,11 @@ namespace Microsoft.Azure.Management.Billing
         /// Gets the IAgreementsOperations.
         /// </summary>
         public virtual IAgreementsOperations Agreements { get; private set; }
+
+        /// <summary>
+        /// Gets the ILineOfCreditsOperations.
+        /// </summary>
+        public virtual ILineOfCreditsOperations LineOfCredits { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of the BillingManagementClient class.
@@ -416,6 +451,7 @@ namespace Microsoft.Azure.Management.Billing
             PaymentMethods = new PaymentMethodsOperations(this);
             Address = new AddressOperations(this);
             AvailableBalances = new AvailableBalancesOperations(this);
+            Instructions = new InstructionsOperations(this);
             BillingProfiles = new BillingProfilesOperations(this);
             Customers = new CustomersOperations(this);
             InvoiceSections = new InvoiceSectionsOperations(this);
@@ -423,14 +459,20 @@ namespace Microsoft.Azure.Management.Billing
             BillingSubscriptions = new BillingSubscriptionsOperations(this);
             Products = new ProductsOperations(this);
             Transactions = new TransactionsOperations(this);
+            Departments = new DepartmentsOperations(this);
+            EnrollmentAccounts = new EnrollmentAccountsOperations(this);
             Invoices = new InvoicesOperations(this);
             PriceSheet = new PriceSheetOperations(this);
             Policies = new PoliciesOperations(this);
             BillingProperty = new BillingPropertyOperations(this);
+            Transfers = new TransfersOperations(this);
+            PartnerTransfers = new PartnerTransfersOperations(this);
+            RecipientTransfers = new RecipientTransfersOperations(this);
             Operations = new Operations(this);
             BillingRoleDefinitions = new BillingRoleDefinitionsOperations(this);
             BillingRoleAssignments = new BillingRoleAssignmentsOperations(this);
             Agreements = new AgreementsOperations(this);
+            LineOfCredits = new LineOfCreditsOperations(this);
             BaseUri = new System.Uri("https://management.azure.com");
             ApiVersion = "2019-10-01-preview";
             AcceptLanguage = "en-US";
