15a456309106567e715dd31c4252d1c64b47593c
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-01-27 16:25:32 -0800

    Exclude more tests

--- sdk/storage/Azure.Storage.Blobs/tests/BlobClientTests.cs
+++ sdk/storage/Azure.Storage.Blobs/tests/BlobClientTests.cs
@@ -594,10 +594,6 @@ namespace Azure.Storage.Blobs.Test
         [TestCase(33 * Constants.MB, 8)]
         [TestCase(33 * Constants.MB, 16)]
         [TestCase(33 * Constants.MB, null)]
-        [TestCase(257 * Constants.MB, 1)]
-        [TestCase(257 * Constants.MB, 4)]
-        [TestCase(257 * Constants.MB, 8)]
-        [TestCase(257 * Constants.MB, null)]
         public async Task UploadStreamAsync_LargeBlobs(long size, int? maximumThreadCount)
         {
             // TODO: #6781 We don't want to add 1GB of random data in the recordings
@@ -606,7 +602,11 @@ namespace Azure.Storage.Blobs.Test
 
         [Test]
         [LiveOnly]
-        [Explicit("These tests are particularly slow and often hit our 20 minute per test time limit.")]
+        [Explicit("These tests are timing out occasionally due to issue described in https://github.com/Azure/azure-sdk-for-net/issues/9340")]
+        [TestCase(257 * Constants.MB, 1)]
+        [TestCase(257 * Constants.MB, 4)]
+        [TestCase(257 * Constants.MB, 8)]
+        [TestCase(257 * Constants.MB, null)]
         [TestCase(257 * Constants.MB, 16)]
         [TestCase(1 * Constants.GB, 1)]
         [TestCase(1 * Constants.GB, 4)]
@@ -626,10 +626,6 @@ namespace Azure.Storage.Blobs.Test
         [TestCase(33 * Constants.MB, 8)]
         [TestCase(33 * Constants.MB, 16)]
         [TestCase(33 * Constants.MB, null)]
-        [TestCase(257 * Constants.MB, 1)]
-        [TestCase(257 * Constants.MB, 4)]
-        [TestCase(257 * Constants.MB, 8)]
-        [TestCase(257 * Constants.MB, null)]
         public async Task UploadFileAsync_LargeBlobs(long size, int? maximumThreadCount)
         {
             // TODO: #6781 We don't want to add 1GB of random data in the recordings
@@ -638,7 +634,11 @@ namespace Azure.Storage.Blobs.Test
 
         [Test]
         [LiveOnly]
-        [Explicit("These tests are particularly slow and often hit our 20 minute per test time limit.")]
+        [Explicit("These tests are timing out occasionally due to issue described in https://github.com/Azure/azure-sdk-for-net/issues/9340")]
+        [TestCase(257 * Constants.MB, 1)]
+        [TestCase(257 * Constants.MB, 4)]
+        [TestCase(257 * Constants.MB, 8)]
+        [TestCase(257 * Constants.MB, null)]
         [TestCase(257 * Constants.MB, 16)]
         [TestCase(1 * Constants.GB, 1)]
         [TestCase(1 * Constants.GB, 4)]
