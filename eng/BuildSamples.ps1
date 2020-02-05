3a698fddc116eabc494341d9ac06400356ed9ad8
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-02-04 21:21:59 -0800

    Clean up

--- eng/BuildSamples.ps1
+++ eng/BuildSamples.ps1
@@ -1,4 +1,4 @@
-﻿$baseDir = (Get-Item -Path "$env:BUILD_SOURCESDIRECTORY\samples\" -Verbose).FullName
+﻿$baseDir = Join-Path $env:BUILD_SOURCESDIRECTORY "samples"
 $items = Get-ChildItem -Path $baseDir -Include *.csproj -Recurse
 foreach ($item in $items){
     dotnet build $item
