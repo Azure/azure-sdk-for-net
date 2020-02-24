9daa3696b065dd445a5b322df0c99cd6664d6c02
Jingshu923 <52914166+Jingshu923@users.noreply.github.com>
2020-02-24 15:59:00 +0800

    Db2 add connectionString and sftp add UseTempFileRename  (#10122)
    * Update DB2 and Sftp
    
    * add sdk info

--- sdk/datafactory/Microsoft.Azure.Management.DataFactory/tests/JsonSamples/LinkedServiceJsonSamples.cs
+++ sdk/datafactory/Microsoft.Azure.Management.DataFactory/tests/JsonSamples/LinkedServiceJsonSamples.cs
@@ -2426,6 +2426,23 @@ namespace DataFactory.Tests.JsonSamples
 }";
 
         [JsonSample]
+        public const string Db2LinkedServiceWithConnectionString = @"
+{
+    name: ""Db2LinkedService"",
+    properties:
+    {
+        type: ""Db2"",
+        connectVia: {
+            referenceName : ""MSourceDemoIR"",
+            type : ""IntegrationRuntimeReference""
+        },
+        typeProperties: {
+            connectionString : ""Server=<server>;Database=<database>;AuthenticationType=Basic;UserName=<username>;PackageCollection=<packageCollection>;CertificateCommonName=<certificateCommonName>""
+        }
+    }
+}";
+
+        [JsonSample]
         public const string SapOpenHubLinkedService = @"
 {
     ""name"": ""SapBwOpenHubLinkedService"",
