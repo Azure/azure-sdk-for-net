5859ec75485b4caf96b93d0792117b776444a036
Chidozie Ononiwu <chononiw@microsoft.com>
2020-02-24 17:11:59 -0800

    Testing release with Azure.Template Previrew.6

--- eng/Set-PackageReleaseNotes.ps1
+++ eng/Set-PackageReleaseNotes.ps1
@@ -8,8 +8,18 @@ param (
 $ErrorActionPreference = 'Stop'
 
 try {
-  $releaseNotesForVersion = &"${PSScriptRoot}/common/Extract-ReleaseNotes.ps1" -ChangeLogLocation $ChangeLogLocation -VersionString $VersionString
-  Set-Content -Path $ReleaseNotesPropsPath -Value $releaseNotesForVersion
+  $ReleaseNotesForVersion = . ${PSScriptRoot}/common/Extract-ReleaseNotes.ps1 -ChangeLogLocation $ChangeLogLocation -VersionString $VersionString # Dot Sourced to get $RELEASE_TITLE_REGEX into scope
+  $ReleaseNotesArray = $ReleaseNotesForVersion.Split([Environment]::NewLine)
+  $ProcessedNotes = New-Object System.Collections.ArrayList
+  foreach ($line in $ReleaseNotesArray)
+  {
+    $lineIsTitle = $line.Startswith('#') -And ($line -match $RELEASE_TITLE_REGEX)
+    if (-Not $lineIsTitle)
+    {
+      $ProcessedNotes.Add($line.Trim('#')) > $null
+    }
+  }
+  Set-Content -Path $ReleaseNotesPropsPath -Value ($ProcessedNotes -Join "`r")
 }
 catch {
   Write-Host "Entered Catch Block"
--- eng/common/Extract-ReleaseNotes.ps1
+++ eng/common/Extract-ReleaseNotes.ps1
@@ -7,52 +7,44 @@ param (
 
 $RELEASE_TITLE_REGEX = "(?<releaseNoteTitle>^\#+.*(?<version>\b\d+\.\d+\.\d+([^0-9\s][^\s:]+)?))"
 
-$releaseNotes = @{}
-$contentArrays = @{}
-if ($ChangeLogLocation.Length -eq 0)
-{
+$releaseNotes = @{ }
+$contentArrays = @{ }
+if ($ChangeLogLocation.Length -eq 0) {
   return $releaseNotes
 }
 
-try 
-{
+try {
   $contents = Get-Content $ChangeLogLocation
 
   # walk the document, finding where the version specifiers are and creating lists
   $version = ""
-  foreach($line in $contents){
-      if ($line -match $RELEASE_TITLE_REGEX)
-      {
-        $version = $matches["version"]
-        $contentArrays[$version] = @()
-      }
+  foreach ($line in $contents) {
+    if ($line -match $RELEASE_TITLE_REGEX) {
+      $version = $matches["version"]
+      $contentArrays[$version] = @()
+    }
 
-      $contentArrays[$version] += $line
+    $contentArrays[$version] += $line
   }
 
   # resolve each of discovered version specifier string arrays into real content
-  foreach($key in $contentArrays.Keys)
-  {
-      $releaseNotes[$key] = New-Object PSObject -Property @{
+  foreach ($key in $contentArrays.Keys) {
+    $releaseNotes[$key] = New-Object PSObject -Property @{
       ReleaseVersion = $key
       ReleaseContent = $contentArrays[$key] -join [Environment]::NewLine
-      }
+    }
   }
 }
-catch
-{
+catch {
   Write-Host "Error parsing $ChangeLogLocation."
   Write-Host $_.Exception.Message
 }
 
-if ([System.String]::IsNullOrEmpty($VersionString))
-{
+if ([System.String]::IsNullOrEmpty($VersionString)) {
   return $releaseNotes
 }
-else 
-{
-  if ($releaseNotes.ContainsKey($VersionString))
-  {
+else {
+  if ($releaseNotes.ContainsKey($VersionString)) {
     $releaseNotesForVersion = $releaseNotes[$VersionString].ReleaseContent
     return $releaseNotesForVersion
   }
