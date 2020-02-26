137ab973943ffc8c20baec240450c7148d3f1ca6
Jesse Squire <jesse.squire@gmail.com>
2020-02-26 13:49:55 -0500

    [Event Hubs Client] Preparation for Event Processor<T> Implementation (#10147)
    The focus of these changes is to set the groundwork for implementation of
    the Event Processor base class by renaming members, shifting namespaces,
    and flipping some internal types to public.

--- sdk/eventhub/Azure.Messaging.EventHubs.Processor/tests/Processor/EventProcessorClientTests.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs.Processor/tests/Processor/EventProcessorClientTests.cs
@@ -14,6 +14,7 @@ using Azure.Core;
 using Azure.Messaging.EventHubs.Authorization;
 using Azure.Messaging.EventHubs.Consumer;
 using Azure.Messaging.EventHubs.Core;
+using Azure.Messaging.EventHubs.Primitives;
 using Azure.Messaging.EventHubs.Processor;
 using Azure.Messaging.EventHubs.Processor.Diagnostics;
 using Azure.Messaging.EventHubs.Processor.Tests;
@@ -754,14 +755,14 @@ namespace Azure.Messaging.EventHubs.Tests
 
             mockStorage
                 .Setup(storage => storage.ClaimOwnershipAsync(
-                    It.Is<IEnumerable<PartitionOwnership>>(ownershipEnumerable =>
+                    It.Is<IEnumerable<EventProcessorPartitionOwnership>>(ownershipEnumerable =>
                         ownershipEnumerable.Any(ownership => string.IsNullOrEmpty(ownership.OwnerIdentifier))),
                     It.IsAny<CancellationToken>()))
-                .Callback<IEnumerable<PartitionOwnership>, CancellationToken>((ownershipEnumerable, token) =>
+                .Callback<IEnumerable<EventProcessorPartitionOwnership>, CancellationToken>((ownershipEnumerable, token) =>
                 {
                     Interlocked.Increment(ref relinquishAttempts);
                 })
-                .Returns(Task.FromResult(default(IEnumerable<PartitionOwnership>)));
+                .Returns(Task.FromResult(default(IEnumerable<EventProcessorPartitionOwnership>)));
 
             mockConsumer
                 .Setup(consumer => consumer.GetPartitionIdsAsync(It.IsAny<CancellationToken>()))
@@ -1493,8 +1494,8 @@ namespace Azure.Messaging.EventHubs.Tests
 
             mockLoadBalancer
                 .SetupSequence(m => m.RunLoadBalancingAsync(partitionIds, It.IsAny<CancellationToken>()))
-                .ReturnsAsync(new PartitionOwnership("namespace", "eventHub", "consumerGroup", mockProcessor.Identifier, partitionIds[0]))
-                .ReturnsAsync(new PartitionOwnership("namespace", "eventHub", "consumerGroup", mockProcessor.Identifier, partitionIds[1]));
+                .ReturnsAsync(CreatePartitionOwnership("namespace", "eventHub", "consumerGroup", mockProcessor.Identifier, partitionIds[0]))
+                .ReturnsAsync(CreatePartitionOwnership("namespace", "eventHub", "consumerGroup", mockProcessor.Identifier, partitionIds[1]));
 
             // Use the init handler to keep track of the partitions that have been initialized.
 
@@ -2225,7 +2226,7 @@ namespace Azure.Messaging.EventHubs.Tests
 
             mockStorage
                 .Setup(storage => storage.ClaimOwnershipAsync(
-                    It.Is<IEnumerable<PartitionOwnership>>(ownershipEnumerable => ownershipEnumerable.Count() == 1),
+                    It.Is<IEnumerable<EventProcessorPartitionOwnership>>(ownershipEnumerable => ownershipEnumerable.Count() == 1),
                     It.IsAny<CancellationToken>()))
                 .Throws(expectedExceptionReference);
 
@@ -2301,7 +2302,7 @@ namespace Azure.Messaging.EventHubs.Tests
 
             mockStorage
                 .Setup(storage => storage.ClaimOwnershipAsync(
-                    It.Is<IEnumerable<PartitionOwnership>>(ownershipEnumerable => partitionHasBeenClaimed && ownershipEnumerable.All(ownership => ownership.OwnerIdentifier == mockProcessor.Identifier)),
+                    It.Is<IEnumerable<EventProcessorPartitionOwnership>>(ownershipEnumerable => partitionHasBeenClaimed && ownershipEnumerable.All(ownership => ownership.OwnerIdentifier == mockProcessor.Identifier)),
                     It.IsAny<CancellationToken>()))
                 .Throws(expectedExceptionReference);
 
@@ -2636,7 +2637,7 @@ namespace Azure.Messaging.EventHubs.Tests
 
             mockStorage
                 .Setup(storage => storage.ClaimOwnershipAsync(
-                    It.Is<IEnumerable<PartitionOwnership>>(ownershipEnumerable => ownershipEnumerable.Any(ownership => string.IsNullOrEmpty(ownership.OwnerIdentifier))),
+                    It.Is<IEnumerable<EventProcessorPartitionOwnership>>(ownershipEnumerable => ownershipEnumerable.Any(ownership => string.IsNullOrEmpty(ownership.OwnerIdentifier))),
                     It.IsAny<CancellationToken>()))
                 .Throws(expectedExceptionReference);
 
@@ -2722,7 +2723,7 @@ namespace Azure.Messaging.EventHubs.Tests
 
             mockStorage
                 .Setup(storage => storage.UpdateCheckpointAsync(
-                    It.IsAny<Checkpoint>(),
+                    It.IsAny<EventProcessorCheckpoint>(),
                     It.IsAny<CancellationToken>()))
                 .Throws(expectedExceptionReference);
 
@@ -3025,7 +3026,17 @@ namespace Azure.Messaging.EventHubs.Tests
             var consumerGroup = "consumerGroup";
             var partitionId = "3";
             var checkpointOffset = 5631;
-            var checkpoint = new Checkpoint(fqNamespace, eventHub, consumerGroup, partitionId, checkpointOffset, 0);
+
+            var checkpoint = new EventProcessorCheckpoint
+            {
+                FullyQualifiedNamespace = fqNamespace,
+                EventHubName = eventHub,
+                ConsumerGroup = consumerGroup,
+                PartitionId = partitionId,
+                Offset = checkpointOffset,
+                SequenceNumber = 0
+            };
+
             var mockStorage = new MockCheckPointStorage();
             var mockConsumer = new Mock<EventHubConsumerClient>(consumerGroup, Mock.Of<EventHubConnection>(), default);
             var mockProcessor = new InjectableEventSourceProcessorMock(mockStorage, consumerGroup, fqNamespace, eventHub, Mock.Of<Func<EventHubConnection>>(), default, mockConsumer.Object);
@@ -3347,7 +3358,7 @@ namespace Azure.Messaging.EventHubs.Tests
 
             mockStorage
                 .Setup(storage => storage.ClaimOwnershipAsync(
-                    It.Is<IEnumerable<PartitionOwnership>>(ownershipEnumerable => ownershipEnumerable.Any(ownership => !string.IsNullOrEmpty(ownership.OwnerIdentifier))),
+                    It.Is<IEnumerable<EventProcessorPartitionOwnership>>(ownershipEnumerable => ownershipEnumerable.Any(ownership => !string.IsNullOrEmpty(ownership.OwnerIdentifier))),
                     It.IsAny<CancellationToken>()))
                 .Callback(() => renewals++);
 
@@ -3372,6 +3383,38 @@ namespace Azure.Messaging.EventHubs.Tests
             Assert.That(cancellationSource.IsCancellationRequested, Is.False, "The processor should have stopped without cancellation.");
             Assert.That(renewals, Is.EqualTo(cycles).Within(1));
         }
+
+        /// <summary>
+        ///   Acts as a factory for instances of the <see cref="EventProcessorPartitionOwnership" /> class.
+        /// </summary>
+        ///
+        /// <param name="fullyQualifiedNamespace">The fully qualified Event Hubs namespace this partition ownership is associated with.  This is likely to be similar to <c>{yournamespace}.servicebus.windows.net</c>.</param>
+        /// <param name="eventHubName">The name of the specific Event Hub this partition ownership is associated with, relative to the Event Hubs namespace that contains it.</param>
+        /// <param name="consumerGroup">The name of the consumer group this partition ownership is associated with.</param>
+        /// <param name="ownerIdentifier">The identifier of the associated <c>EventProcessorClient</c> instance.</param>
+        /// <param name="partitionId">The identifier of the Event Hub partition this partition ownership is associated with.</param>
+        /// <param name="lastModifiedTime">The date and time, in UTC, that the last update was made to this ownership.</param>
+        /// <param name="version">The version needed to update this ownership.</param>
+        ///
+        /// <returns>A <see cref="EventProcessorPartitionOwnership" /> instance populated with the requested state.</returns>
+        ///
+        private static EventProcessorPartitionOwnership CreatePartitionOwnership(string fullyQualifiedNamespace,
+                                                                                 string eventHubName,
+                                                                                 string consumerGroup,
+                                                                                 string ownerIdentifier,
+                                                                                 string partitionId,
+                                                                                 DateTimeOffset lastModifiedTime = default,
+                                                                                 string version = default) =>
+            new EventProcessorPartitionOwnership
+            {
+                FullyQualifiedNamespace = fullyQualifiedNamespace,
+                EventHubName = eventHubName,
+                ConsumerGroup = consumerGroup,
+                OwnerIdentifier = ownerIdentifier,
+                PartitionId = partitionId,
+                LastModifiedTime = lastModifiedTime,
+                Version = version
+            };
 
         /// <summary>
         ///   Retrieves the StorageManager for the processor client using its private accessor.
