932b94075912ff32ce9f25aee1fb6ea9e4170e2f
SDK Automation <sdkautomation@microsoft.com>
2020-02-27 05:25:43 +0000

    Generated from 2f9d3fc76ca46faf152ff4883db1fa0233b1e150
    replace resource-manager

--- sdk/datashare/Microsoft.Azure.Management.DataShare/src/Generated/SdkInfo_DataShareManagementClient.cs
+++ sdk/datashare/Microsoft.Azure.Management.DataShare/src/Generated/SdkInfo_DataShareManagementClient.cs
@@ -34,16 +34,5 @@ namespace Microsoft.Azure.Management.DataShare
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4407";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/datashare/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --tag=package-2019-11-01 --csharp-sdks-folder=D:\\workspace\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "8170b1dddd0b7e0e99e2940625182a79cb677231";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
