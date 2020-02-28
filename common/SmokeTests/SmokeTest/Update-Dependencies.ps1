a3a0f5c0f51fb27eb70df333fee76cf976592c5c
Daniel Jurek <djurek@microsoft.com>
2020-02-28 10:26:52 -0800

    add logic for using latest package versions

---
+++ common/SmokeTests/SmokeTest/Update-Dependencies.ps1
@@ -0,0 +1,31 @@
+param(
+    $framework = 'netcoreapp2.1' # 'netcoreapp2.1' or 'net461'
+)
+
+$PACKAGE_PATTERN = [Regex]::new('>\s(.*?)\s')
+
+$rawDependencies = dotnet list package --framework $framework
+$packageNames = $PACKAGE_PATTERN.Matches($rawDependencies) |
+    ForEach-Object { $_.Groups[1].Value }
+
+Write-Host Project Packages:
+$packageNames | ForEach-Object { Write-Host $_ }
+
+$availablePackagesList = Find-Package -Source $BLOB_FEED_URL -AllowPrereleaseVersions
+$availablePackages = @{}
+$availablePackagesList | ForEach-Object { $availablePackages[$_.Name] = $_ }
+
+$packageNames | ForEach-Object {
+    if (-not $availablePackages.ContainsKey($_)) {
+        return;
+    }
+    $targetPackage = $availablePackages[$_]
+    Write-Host "Adding Package $($targetPackage.Name) @ $($targetPackage.Version)"
+    dotnet add package $targetPackage.Name --version $targetPackage.Version
+}
+
+Write-Host === CSPROJ CONTENT ===
+Get-Content SmokeTest.csproj | Write-Host
+
+Write-Host Restoring packages...
+dotnet restore
