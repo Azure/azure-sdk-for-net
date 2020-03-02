cf7001ceb515926ebd7a663133de182bd73655aa
Yeming Liu <Yeming.Liu@microsoft.com>
2020-03-02 09:51:48 +0800

    Fix build problem

--- sdk/batch/Microsoft.Azure.Management.Batch/tests/ScenarioTests/AccountTests.ScenarioTests.cs
+++ sdk/batch/Microsoft.Azure.Management.Batch/tests/ScenarioTests/AccountTests.ScenarioTests.cs
@@ -21,8 +21,7 @@ namespace Batch.Tests.ScenarioTests
         [Fact]
         public async Task BatchAccountEndToEndAsync()
         {
-        // dummy update
-            using (MockContext context = StartMockContextAndInitializeClients(this.GetType()))
+            using (MockContext context = StartMockContextAndInitializeClients(this.GetType().FullName))
             {
                 string resourceGroupName = TestUtilities.GenerateName();
                 string batchAccountName = TestUtilities.GenerateName();
