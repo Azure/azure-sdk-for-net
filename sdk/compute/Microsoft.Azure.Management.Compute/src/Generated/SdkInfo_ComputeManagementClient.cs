fe8f1fea4d440311e9acd1b1ff5081038b9a2075
SDK Automation <sdkautomation@microsoft.com>
2020-03-06 03:38:07 +0000

    Generated from 21feff8909aa7134becb557b27837791b6aedd92
    fixed an issue.

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/SdkInfo_ComputeManagementClient.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/SdkInfo_ComputeManagementClient.cs
@@ -22,8 +22,8 @@ namespace Microsoft.Azure.Management.Compute
                 new Tuple<string, string, string>("Compute", "AvailabilitySets", "2019-07-01"),
                 new Tuple<string, string, string>("Compute", "DedicatedHostGroups", "2019-07-01"),
                 new Tuple<string, string, string>("Compute", "DedicatedHosts", "2019-07-01"),
-                new Tuple<string, string, string>("Compute", "DiskEncryptionSets", "2019-07-01"),
-                new Tuple<string, string, string>("Compute", "Disks", "2019-07-01"),
+                new Tuple<string, string, string>("Compute", "DiskEncryptionSets", "2019-11-01"),
+                new Tuple<string, string, string>("Compute", "Disks", "2019-11-01"),
                 new Tuple<string, string, string>("Compute", "Galleries", "2019-12-01"),
                 new Tuple<string, string, string>("Compute", "GalleryApplicationVersions", "2019-12-01"),
                 new Tuple<string, string, string>("Compute", "GalleryApplications", "2019-12-01"),
@@ -34,7 +34,7 @@ namespace Microsoft.Azure.Management.Compute
                 new Tuple<string, string, string>("Compute", "Operations", "2019-07-01"),
                 new Tuple<string, string, string>("Compute", "ProximityPlacementGroups", "2019-07-01"),
                 new Tuple<string, string, string>("Compute", "ResourceSkus", "2019-04-01"),
-                new Tuple<string, string, string>("Compute", "Snapshots", "2019-07-01"),
+                new Tuple<string, string, string>("Compute", "Snapshots", "2019-11-01"),
                 new Tuple<string, string, string>("Compute", "Usage", "2019-07-01"),
                 new Tuple<string, string, string>("Compute", "VirtualMachineExtensionImages", "2019-07-01"),
                 new Tuple<string, string, string>("Compute", "VirtualMachineExtensions", "2019-07-01"),
@@ -51,16 +51,5 @@ namespace Microsoft.Azure.Management.Compute
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/compute/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=C:\\gitrepo\\fork\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "da02a866d96aaacdc6cc4095b44e40defdf1118d";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
