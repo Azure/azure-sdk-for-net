1d902ade10d7dcb024e07b680b2706bfb15b36bc
Pavel Krymets <pavel@krymets.com>
2020-03-06 10:49:03 -0800

    Update Update-Snippets.ps1

--- eng/Update-Snippets.ps1
+++ eng/Update-Snippets.ps1
@@ -8,7 +8,7 @@ param (
 $generatorProject = "$PSScriptRoot/SnippetGenerator/SnippetGenerator.csproj";
 $root = "$PSScriptRoot/../sdk"
 
-# special casing * here because single invocation of SnippetGenerator if much faster than
+# special casing * here because single invocation of SnippetGenerator is much faster than
 # running it per service directory
 if ($ServiceDirectory -and ($ServiceDirectory -ne "*")) {
     $root += '/' + $ServiceDirectory
