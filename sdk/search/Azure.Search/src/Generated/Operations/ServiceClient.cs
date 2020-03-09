5df9e81b1b77221528466e5f267a971e4457327b
Pavel Krymets <pavel@krymets.com>
2020-03-09 08:10:29 -0700

    Update Autorest C# (#10384)

--- sdk/search/Azure.Search/src/Generated/Operations/ServiceClient.cs
+++ sdk/search/Azure.Search/src/Generated/Operations/ServiceClient.cs
@@ -29,14 +29,14 @@ namespace Azure.Search
         /// <summary> Gets service level statistics for a search service. </summary>
         /// <param name="clientRequestId"> The tracking ID sent with the request to help with debugging. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        public async ValueTask<Response<SearchServiceStatistics>> GetServiceStatisticsAsync(Guid? clientRequestId, CancellationToken cancellationToken = default)
+        public virtual async ValueTask<Response<SearchServiceStatistics>> GetServiceStatisticsAsync(Guid? clientRequestId, CancellationToken cancellationToken = default)
         {
             return await RestClient.GetServiceStatisticsAsync(clientRequestId, cancellationToken).ConfigureAwait(false);
         }
         /// <summary> Gets service level statistics for a search service. </summary>
         /// <param name="clientRequestId"> The tracking ID sent with the request to help with debugging. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        public Response<SearchServiceStatistics> GetServiceStatistics(Guid? clientRequestId, CancellationToken cancellationToken = default)
+        public virtual Response<SearchServiceStatistics> GetServiceStatistics(Guid? clientRequestId, CancellationToken cancellationToken = default)
         {
             return RestClient.GetServiceStatistics(clientRequestId, cancellationToken);
         }
