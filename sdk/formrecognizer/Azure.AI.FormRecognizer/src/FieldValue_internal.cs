1b622bd3d60f9abd470787cc0a92147de6714780
Anne Thompson <annelo@microsoft.com>
2020-03-09 16:47:23 -0700

    FormRecognizer preview 1 prototype for UX study (#10319)
    * intial generation
    
    * first authenticated call
    
    * start training LRO is minimally functional
    
    * map Model to CustomModel
    
    * interim commit
    
    * start extract form LRO is minimally functional
    
    * map AnalyzeResult.PageResult to ExtractedForm
    
    * start train form with labels minimally functional
    
    * map Model to CustomLabeledModel
    
    * map DocumentResult to ExtractedForm
    
    * tidy up of public API
    
    * setup for ReceiptClient; adding async variants of LROs to CustomFormClient
    
    * Extract Receipt functioning
    
    * Map AnalyzeResult to ExtractedReceipt
    
    * Functional extract layout
    
    * Map AnalyzeResult to ExtractedTable
    
    * Add Tables to ExtractedForm
    
    * Make internal types internal again
    
    * some renames
    
    * Added Uri overloads
    
    * tweak
    
    * first success with OCR/references
    
    * adding OCR values and mapping types as workarounds
    
    * move custom types from Models to Custom namespace
    
    * interim checkin
    
    * Completed first iteration of bounding box on ExtractedReceipt. Removed AsReadOnly() calls for IReadOnlyList
    
    * updates per APIView feedback
    
    * bug fixes
    
    * initial prototype code for FR
    
    * update solution file
    
    * intermediate checkin for TODOs
    
    * interim checkin for TODOs
    
    * revert template files
    
    * cleaning up /template files
    
    * Updating TODOs with links to github issues
    
    * samples project change
    
    * rebase to new CodeGen
    
    * tidy up
    
    * git attributes for gen code
    
    * pr feedback
    
    * bug fix
    
    * bug fix
    
    * bug fix
    
    * stop tracking samples
    
    * pr feedback
    
    * add api
    
    * remove sln under sdk/cognitiveservices

---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/FieldValue_internal.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.AI.FormRecognizer.Models
+{
+    [CodeGenSchema("FieldValue")]
+    internal partial class FieldValue_internal
+    {
+    }
+}
