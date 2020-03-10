74f0ae1d265eb6ea2e5b537bcc4a0c5243cd7fd7
Scott Schaab <sschaab@microsoft.com>
2020-03-10 13:26:48 -0700

    Azure.Identity Fixing AzureCliCredential check for no logged in account (#10497)
    * Fixing AzureCliCredential check for no logged in account
    
    * switch to IndexOf instead of Contains to specify StringComparison type

--- sdk/identity/Azure.Identity/src/AzureCliCredentialClient.cs
+++ sdk/identity/Azure.Identity/src/AzureCliCredentialClient.cs
@@ -68,7 +68,8 @@ namespace Azure.Identity
 
             if (exitCode != 0)
             {
-                bool isLoginError = output.StartsWith("Please run 'az login'", StringComparison.CurrentCultureIgnoreCase);
+                bool isLoginError = output.IndexOf("az login", StringComparison.OrdinalIgnoreCase) != -1 || output.IndexOf("az account set", StringComparison.OrdinalIgnoreCase) != -1;
+
                 bool isWinError = output.StartsWith(WinAzureCLIError, StringComparison.CurrentCultureIgnoreCase);
 
                 bool isOtherOsError = AzNotFoundPattern.IsMatch(output);
