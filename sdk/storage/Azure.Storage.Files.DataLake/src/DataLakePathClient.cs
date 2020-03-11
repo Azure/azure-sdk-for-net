b2c1e77738c083a44803c8c23240881dc6e57cf6
Kamil Sobol <kasobol@microsoft.com>
2020-03-11 10:32:37 -0700

    extract json parsing.

--- sdk/storage/Azure.Storage.Files.DataLake/src/DataLakePathClient.cs
+++ sdk/storage/Azure.Storage.Files.DataLake/src/DataLakePathClient.cs
@@ -2347,7 +2347,7 @@ namespace Azure.Storage.Files.DataLake
                         continuationToken = jsonResponse.Value.Continuation;
                         using (var document = JsonDocument.Parse(jsonResponse.Value.Body))
                         {
-                            var response = DeserializeSetAccessControlRecursiveResponse(document.RootElement);
+                            var response = document.RootElement.DeserializeSetAccessControlRecursiveResponse();
                             int currentDirectoriesSuccessfulCount = response.DirectoriesSuccessful ?? 0;
                             int currentFilesSuccessfulCount = response.FilesSuccessful ?? 0;
                             int currentFailureCount = response.FailureCount ?? 0;
@@ -2395,92 +2395,6 @@ namespace Azure.Storage.Files.DataLake
                     Pipeline.LogMethodExit(nameof(DataLakePathClient));
                 }
             }
-        }
-
-        private static SetAccessControlRecursiveResponse DeserializeSetAccessControlRecursiveResponse(JsonElement element)
-        {
-            SetAccessControlRecursiveResponse result = new SetAccessControlRecursiveResponse();
-            foreach (var property in element.EnumerateObject())
-            {
-                if (property.NameEquals("directoriesSuccessful"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
-                    result.DirectoriesSuccessful = property.Value.GetInt32();
-                    continue;
-                }
-                if (property.NameEquals("filesSuccessful"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
-                    result.FilesSuccessful = property.Value.GetInt32();
-                    continue;
-                }
-                if (property.NameEquals("failureCount"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
-                    result.FailureCount = property.Value.GetInt32();
-                    continue;
-                }
-                if (property.NameEquals("failedEntries"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
-                    var failedEntries = new List<AclFailedEntryList>();
-                    foreach (var item in property.Value.EnumerateArray())
-                    {
-                        failedEntries.Add(DeserializeAclFailedEntryList(item));
-                    }
-                    result.FailedEntries = failedEntries;
-                    continue;
-                }
-            }
-            return result;
-        }
-
-        private static AclFailedEntryList DeserializeAclFailedEntryList(JsonElement element)
-        {
-            AclFailedEntryList result = new AclFailedEntryList();
-            foreach (var property in element.EnumerateObject())
-            {
-                if (property.NameEquals("name"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
-                    result.Name = property.Value.GetString();
-                    continue;
-                }
-                if (property.NameEquals("type"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
-                    result.Type = property.Value.GetString();
-                    continue;
-                }
-                if (property.NameEquals("errorMessage"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
-                    result.ErrorMessage = property.Value.GetString();
-                    continue;
-                }
-            }
-            return result;
         }
         #endregion Set Access Control Recursive
 
