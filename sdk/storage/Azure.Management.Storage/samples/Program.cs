81570bebe01fa480e97dbe7b844c9f27f6c9c636
Pavel Krymets <pavel@krymets.com>
2020-03-12 08:12:39 -0700

    start samples

--- sdk/storage/Azure.Management.Storage/samples/Program.cs
+++ sdk/storage/Azure.Management.Storage/samples/Program.cs
@@ -4,6 +4,8 @@
 using System;
 using System.Threading.Tasks;
 using Azure.Identity;
+using Azure.Management.Storage.Models;
+
 
 namespace Azure.Management.Storage.Samples
 {
@@ -11,11 +13,57 @@ namespace Azure.Management.Storage.Samples
     {
         public static async Task Main()
         {
-            var serviceClient = new StorageAccountsClient("faa080af-c1d8-40ad-9cce-e1a450ca5b57", new DefaultAzureCredential());
-            await foreach (var account in serviceClient.ListAsync())
+            var storageAccountsClient = new StorageAccountsClient("faa080af-c1d8-40ad-9cce-e1a450ca5b57", new DefaultAzureCredential());
+
+            var accountName = "my0account3";
+            var resourceGroupName = "pakrym-resources";
+
+            var createOperation = storageAccountsClient.StartCreateOperation(resourceGroupName, accountName, new StorageAccountCreateParameters()
             {
-                Console.WriteLine(account.Name);
+                Sku = new Sku() { Name = "Standard_LRS" },
+                Location = "eastus",
+                Kind = Kind.StorageV2
+            });
+
+            StorageAccount account = await createOperation.WaitForCompletionAsync();
+            Console.WriteLine($"{account.Id} {account.Location} {account.Name}");
+
+            StorageAccountListKeysResult keys = await storageAccountsClient.ListKeysAsync(resourceGroupName, accountName);
+            StorageAccountKey lastKey = null;
+
+            foreach (StorageAccountKey storageAccountKey in keys.Keys)
+            {
+                Console.WriteLine($"{storageAccountKey.KeyName}={storageAccountKey.Value}");
+                lastKey = storageAccountKey;
             }
+
+            // TODO: This call succeeds but throws because server responds with 202 while the swagger only defines 200
+            // await storageAccountsClient.RegenerateKeyAsync(resourceGroupName, accountName, new StorageAccountRegenerateKeyParameters()
+            // {
+            //     KeyName = lastKey.Value
+            // });
+
+            account = await storageAccountsClient.UpdateAsync(resourceGroupName, accountName, new StorageAccountUpdateParameters()
+            {
+                Encryption =
+                {
+                    Services =
+                    {
+                        Blob =
+                        {
+                            Enabled = true
+                        }
+                    }
+                }
+            });
+
+            Console.WriteLine($"Encryption:\n" +
+                              $"Blob {account.Encryption.Services.Blob.Enabled}\n"+
+                              $"File {account.Encryption.Services.File.Enabled}\n"+
+                              $"Queue {account.Encryption.Services.Queue.Enabled}\n"+
+                              $"Table {account.Encryption.Services.Table.Enabled}");
+
+            await storageAccountsClient.DeleteAsync(resourceGroupName, accountName);
         }
     }
 }
