a33fa52969a1ae81970c52e3d642b947affc0c8d
SDK Automation <sdkautomation@microsoft.com>
2020-03-21 02:06:54 +0000

    Generated from 184eab9bf76379d9cbc6f61264767e337306c005
    Fix change to list of MountTargets

--- sdk/netapp/Microsoft.Azure.Management.NetApp/src/Generated/Models/Volume.cs
+++ sdk/netapp/Microsoft.Azure.Management.NetApp/src/Generated/Models/Volume.cs
@@ -55,7 +55,7 @@ namespace Microsoft.Azure.Management.NetApp.Models
         /// <param name="volumeType">What type of volume is this</param>
         /// <param name="dataProtection">DataProtection</param>
         /// <param name="isRestoring">Restoring</param>
-        public Volume(string location, string creationToken, long usageThreshold, string subnetId, string id = default(string), string name = default(string), string type = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), string fileSystemId = default(string), string serviceLevel = default(string), VolumePropertiesExportPolicy exportPolicy = default(VolumePropertiesExportPolicy), IList<string> protocolTypes = default(IList<string>), string provisioningState = default(string), string snapshotId = default(string), string baremetalTenantId = default(string), object mountTargets = default(object), string volumeType = default(string), VolumePropertiesDataProtection dataProtection = default(VolumePropertiesDataProtection), bool? isRestoring = default(bool?))
+        public Volume(string location, string creationToken, long usageThreshold, string subnetId, string id = default(string), string name = default(string), string type = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), string fileSystemId = default(string), string serviceLevel = default(string), VolumePropertiesExportPolicy exportPolicy = default(VolumePropertiesExportPolicy), IList<string> protocolTypes = default(IList<string>), string provisioningState = default(string), string snapshotId = default(string), string baremetalTenantId = default(string), IList<MountTargetList> mountTargets = default(IList<MountTargetList>), string volumeType = default(string), VolumePropertiesDataProtection dataProtection = default(VolumePropertiesDataProtection), bool? isRestoring = default(bool?))
         {
             Location = location;
             Id = id;
@@ -209,7 +209,7 @@ namespace Microsoft.Azure.Management.NetApp.Models
         /// List of mount targets
         /// </remarks>
         [JsonProperty(PropertyName = "properties.mountTargets")]
-        public object MountTargets { get; set; }
+        public IList<MountTargetList> MountTargets { get; set; }
 
         /// <summary>
         /// Gets or sets what type of volume is this
@@ -266,6 +266,21 @@ namespace Microsoft.Azure.Management.NetApp.Models
                 if (!System.Text.RegularExpressions.Regex.IsMatch(FileSystemId, "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$"))
                 {
                     throw new ValidationException(ValidationRules.Pattern, "FileSystemId", "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$");
+                }
+            }
+            if (CreationToken != null)
+            {
+                if (CreationToken.Length > 80)
+                {
+                    throw new ValidationException(ValidationRules.MaxLength, "CreationToken", 80);
+                }
+                if (CreationToken.Length < 1)
+                {
+                    throw new ValidationException(ValidationRules.MinLength, "CreationToken", 1);
+                }
+                if (!System.Text.RegularExpressions.Regex.IsMatch(CreationToken, "^[a-zA-Z][a-zA-Z0-9\\-]{0,79}$"))
+                {
+                    throw new ValidationException(ValidationRules.Pattern, "CreationToken", "^[a-zA-Z][a-zA-Z0-9\\-]{0,79}$");
                 }
             }
             if (UsageThreshold > 109951162777600)
