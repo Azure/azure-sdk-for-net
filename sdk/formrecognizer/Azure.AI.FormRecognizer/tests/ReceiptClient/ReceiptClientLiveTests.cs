8b2e7603103d5102ebf2ee06b6a1502db99a5950
Caio Saldanha <camaiaor@microsoft.com>
2020-03-24 15:12:26 -0700

    Implemented basic ReceiptClient tests

---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/tests/ReceiptClient/ReceiptClientLiveTests.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.AI.FormRecognizer.Models;
+using NUnit.Framework;
+
+namespace Azure.AI.FormRecognizer.Tests
+{
+    /// <summary>
+    /// The suite of tests for the <see cref="ReceiptClient"/> class.
+    /// </summary>
+    /// <remarks>
+    /// These tests have a dependency on live Azure services and may incur costs for the associated
+    /// Azure subscription.
+    /// </remarks>
+    [TestFixture]
+    public class ReceiptClientLiveTests
+    {
+        /// <summary>
+        /// Verifies that the <see cref="ReceiptClient" /> is able to connect to the Form
+        /// Recognizer cognitive service and perform operations.
+        /// </summary>
+        ///
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task ExtractReceiptPopulatesExtractedReceipt(bool async)
+        {
+            var endpoint = new Uri("");
+            var key = "";
+            var client = new ReceiptClient(endpoint, new FormRecognizerApiKeyCredential(key));
+
+            using var stream = new FileStream(@"", FileMode.Open);
+            Response<ExtractedReceipt> response;
+
+            if (async)
+            {
+                response = await client.ExtractReceiptAsync(stream, FormContentType.Jpeg);
+            }
+            else
+            {
+                response = client.ExtractReceipt(stream, FormContentType.Jpeg);
+            }
+
+            Assert.IsNotNull(response);
+
+            var receipt = response.Value;
+
+            // The expected values are based on the values returned by the service, and not the actual
+            // values present in the receipt. We are not testing the service here, but the SDK.
+
+            Assert.AreEqual(1, receipt.StartPageNumber);
+            Assert.AreEqual(1, receipt.EndPageNumber);
+
+            Assert.AreEqual("Contoso Contoso", receipt.MerchantName);
+            Assert.AreEqual("123 Main Street Redmond, WA 98052", receipt.MerchantAddress);
+            Assert.AreEqual("123-456-7890", receipt.MerchantPhoneNumber);
+
+            Assert.IsNotNull(receipt.TransactionDate);
+            Assert.IsNotNull(receipt.TransactionTime);
+
+            var date = receipt.TransactionDate.Value;
+            var time = receipt.TransactionTime.Value;
+
+            Assert.AreEqual(10, date.Day);
+            Assert.AreEqual(6, date.Month);
+            Assert.AreEqual(2019, date.Year);
+
+            Assert.AreEqual(13, time.Hour);
+            Assert.AreEqual(59, time.Minute);
+            Assert.AreEqual(0, time.Second);
+
+            var expectedItems = new List<(int? Quantity, string Name, float? Price, float? TotalPrice)>()
+            {
+                (null, "8GB RAM (Black)", null, 999.00f),
+                (1, "SurfacePen", null, 99.99f)
+            };
+
+            // Include a bit of tolerance when comparing float types.
+
+            Assert.AreEqual(expectedItems.Count, receipt.Items.Count);
+
+            for (var itemIndex = 0; itemIndex < receipt.Items.Count; itemIndex++)
+            {
+                var receiptItem = receipt.Items[itemIndex];
+                var expectedItem = expectedItems[itemIndex];
+
+                Assert.AreEqual(expectedItem.Quantity, receiptItem.Quantity, $"{receiptItem.Quantity} mismatch in item with index {itemIndex}.");
+                Assert.AreEqual(expectedItem.Name, receiptItem.Name, $"{receiptItem.Name} mismatch in item with index {itemIndex}.");
+                Assert.That(receiptItem.Price, Is.EqualTo(expectedItem.Price).Within(0.0001), $"{receiptItem.Price} mismatch in item with index {itemIndex}.");
+                Assert.That(receiptItem.TotalPrice, Is.EqualTo(expectedItem.TotalPrice).Within(0.0001), $"{receiptItem.TotalPrice} mismatch in item with index {itemIndex}.");
+            }
+
+            Assert.That(receipt.Subtotal, Is.EqualTo(1098.99).Within(0.0001));
+            Assert.That(receipt.Tax, Is.EqualTo(104.40).Within(0.0001));
+            Assert.IsNull(receipt.Tip);
+            Assert.That(receipt.Total, Is.EqualTo(1203.39).Within(0.0001));
+        }
+    }
+}
---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/tests/ReceiptClient/ReceiptClientTests.cs
@@ -0,0 +1,159 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.AI.FormRecognizer.Models;
+using NUnit.Framework;
+
+namespace Azure.AI.FormRecognizer.Tests
+{
+    /// <summary>
+    /// The suite of tests for the <see cref="ReceiptClient"/> class.
+    /// </summary>
+    [TestFixture]
+    public class ReceiptClientTests
+    {
+        /// <summary>
+        /// Verifies functionality of the <see cref="ReceiptClient"/> constructors.
+        /// </summary>
+        [Test]
+        [Ignore("Argument validation not implemented yet.")]
+        public void ConstructorRequiresTheEndpoint()
+        {
+            var credential = new FormRecognizerApiKeyCredential("key");
+
+            Assert.Throws<ArgumentNullException>(() => new ReceiptClient(null, credential));
+            Assert.Throws<ArgumentNullException>(() => new ReceiptClient(null, credential, new FormRecognizerClientOptions()));
+        }
+
+        /// <summary>
+        /// Verifies functionality of the <see cref="ReceiptClient"/> constructors.
+        /// </summary>
+        [Test]
+        [Ignore("Argument validation not implemented yet.")]
+        public void ConstructorRequiresTheCredential()
+        {
+            var endpoint = new Uri("http://localhost");
+
+            Assert.Throws<ArgumentNullException>(() => new ReceiptClient(endpoint, null));
+            Assert.Throws<ArgumentNullException>(() => new ReceiptClient(endpoint, null, new FormRecognizerClientOptions()));
+        }
+
+        /// <summary>
+        /// Verifies functionality of the <see cref="ReceiptClient"/> constructors.
+        /// </summary>
+        [Test]
+        [Ignore("Argument validation not implemented yet.")]
+        public void ConstructorRequiresTheOptions()
+        {
+            var endpoint = new Uri("http://localhost");
+            var credential = new FormRecognizerApiKeyCredential("key");
+
+            Assert.Throws<ArgumentNullException>(() => new ReceiptClient(endpoint, credential, null));
+        }
+
+        /// <summary>
+        /// Verifies functionality of the <see cref="ReceiptClient.ExtractReceipt(Stream, FormContentType, bool, CancellationToken)"/>
+        /// and <see cref="ReceiptClient.ExtractReceiptAsync(Stream, FormContentType, bool, CancellationToken)"/> methods.
+        /// </summary>
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        [Ignore("Argument validation not implemented yet.")]
+        public void ExtractReceiptWithStreamRequiresTheStream(bool async)
+        {
+            var endpoint = new Uri("http://localhost");
+            var credential = new FormRecognizerApiKeyCredential("key");
+            var client = new ReceiptClient(endpoint, credential);
+
+            if (async)
+            {
+                Assert.ThrowsAsync<ArgumentNullException>(async () => await client.ExtractReceiptAsync(null, FormContentType.Jpeg));
+            }
+            else
+            {
+                Assert.Throws<ArgumentNullException>(() => client.ExtractReceipt(null, FormContentType.Jpeg));
+            }
+        }
+
+        /// <summary>
+        /// Verifies functionality of the <see cref="ReceiptClient.ExtractReceipt(Stream, FormContentType, bool, CancellationToken)"/>
+        /// and <see cref="ReceiptClient.ExtractReceiptAsync(Stream, FormContentType, bool, CancellationToken)"/> methods.
+        /// </summary>
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void ExtractReceiptWithStreamRespectsTheCancellationToken(bool async)
+        {
+            var endpoint = new Uri("http://localhost");
+            var credential = new FormRecognizerApiKeyCredential("key");
+            var client = new ReceiptClient(endpoint, credential);
+
+            using var stream = new MemoryStream(Array.Empty<byte>());
+            using var cancellationSource = new CancellationTokenSource();
+            cancellationSource.Cancel();
+
+            if (async)
+            {
+                Assert.ThrowsAsync<TaskCanceledException>(async () => await client.ExtractReceiptAsync(stream, FormContentType.Jpeg, cancellationToken: cancellationSource.Token));
+            }
+            else
+            {
+                Assert.Throws<TaskCanceledException>(() => client.ExtractReceipt(stream, FormContentType.Jpeg, cancellationToken: cancellationSource.Token));
+            }
+        }
+
+        /// <summary>
+        /// Verifies functionality of the <see cref="ReceiptClient.ExtractReceipt(Uri, bool, CancellationToken)"/>
+        /// and <see cref="ReceiptClient.ExtractReceiptAsync(Uri, bool, CancellationToken)"/> methods.
+        /// </summary>
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        [Ignore("Argument validation not implemented yet.")]
+        public void ExtractReceiptWithEndpointRequiresTheUri(bool async)
+        {
+            var endpoint = new Uri("http://localhost");
+            var credential = new FormRecognizerApiKeyCredential("key");
+            var client = new ReceiptClient(endpoint, credential);
+
+            if (async)
+            {
+                Assert.ThrowsAsync<ArgumentNullException>(async () => await client.ExtractReceiptAsync(null));
+            }
+            else
+            {
+                Assert.Throws<ArgumentNullException>(() => client.ExtractReceipt(null));
+            }
+        }
+
+        /// <summary>
+        /// Verifies functionality of the <see cref="ReceiptClient.ExtractReceipt(Uri, bool, CancellationToken)"/>
+        /// and <see cref="ReceiptClient.ExtractReceiptAsync(Uri, bool, CancellationToken)"/> methods.
+        /// </summary>
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void ExtractReceiptWithEndpointRespectsTheCancellationToken(bool async)
+        {
+            var endpoint = new Uri("http://localhost");
+            var credential = new FormRecognizerApiKeyCredential("key");
+            var client = new ReceiptClient(endpoint, credential);
+
+            using var cancellationSource = new CancellationTokenSource();
+            cancellationSource.Cancel();
+
+            if (async)
+            {
+                Assert.ThrowsAsync<TaskCanceledException>(async () => await client.ExtractReceiptAsync(endpoint, cancellationToken: cancellationSource.Token));
+            }
+            else
+            {
+                Assert.Throws<TaskCanceledException>(() => client.ExtractReceipt(endpoint, cancellationToken: cancellationSource.Token));
+            }
+        }
+    }
+}
