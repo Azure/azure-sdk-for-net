45417144a48b7ee6075ff34bc31f57c69e4aef16
Heath Stewart <heaths@microsoft.com>
2020-03-26 10:22:36 -0700

    Initial single and concurrent requests (#10804)
    * Initial single and concurrent requests
    
    Resolves #10691
    
    * More tests and perf improvements
    
    * Remove Identity tests from solution
    
    They require a special environment and stopped working 100% a while ago, and impact the successful completion of all tests here.
    
    * Resolve PR feedback

--- sdk/keyvault/Azure.Security.KeyVault.Shared/tests/TestExtensions.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Shared/tests/TestExtensions.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.IO;
+using System.Text.Json;
+using Azure.Core;
 using NUnit.Framework;
 
 namespace Azure.Security.KeyVault.Tests
@@ -15,6 +19,41 @@ namespace Azure.Security.KeyVault.Tests
             }
 
             return source;
+        }
+
+        public static Stream ToStream(this AccessToken token)
+        {
+            var stream = new MemoryStream();
+            using (var json = new Utf8JsonWriter(stream))
+            {
+                json.WriteStartObject();
+
+                json.WriteString("access_token", token.Token);
+                json.WriteNumber("expires_in", (long)(token.ExpiresOn - DateTimeOffset.UtcNow).TotalSeconds);
+
+                json.WriteEndObject();
+                json.Flush();
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+            return stream;
+        }
+
+        public static Stream ToStream(this IJsonSerializable obj)
+        {
+            var stream = new MemoryStream();
+            using (var json = new Utf8JsonWriter(stream))
+            {
+                json.WriteStartObject();
+
+                obj.WriteProperties(json);
+
+                json.WriteEndObject();
+                json.Flush();
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+            return stream;
         }
     }
 }
