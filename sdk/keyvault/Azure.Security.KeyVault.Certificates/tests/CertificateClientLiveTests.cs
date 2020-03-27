ad1052410b97c2c7f52048684a39a8de81df49d2
Geralt Wang <v-xinlwa@microsoft.com>
2020-03-27 17:13:43 +0800

    Add issuer name assert, then comment out it.

--- sdk/keyvault/Azure.Security.KeyVault.Certificates/tests/CertificateClientLiveTests.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Certificates/tests/CertificateClientLiveTests.cs
@@ -576,6 +576,7 @@ namespace Azure.Security.KeyVault.Certificates.Tests
             Assert.NotNull(getIssuer);
             Assert.NotNull(getIssuer.Id);
             Assert.AreEqual(issuer.Provider, getIssuer.Provider);
+            //Assert.AreEqual(issuer.Name, getIssuer.Name);
         }
 
         [Test]
@@ -627,6 +628,7 @@ namespace Azure.Security.KeyVault.Certificates.Tests
                 Assert.NotNull(issuerPropertie);
                 IssuerProperties returnPropertie = issuerProperties.Single(s => s.Id == issuerPropertie.Id);
                 Assert.AreEqual(issuerPropertie.Provider, returnPropertie.Provider);
+                //Assert.AreEqual(issuerPropertie.Name, returnPropertie.Name);
             }
         }
 
