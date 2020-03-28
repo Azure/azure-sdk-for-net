2d4cf69239e2e135cc465a33e163843fa0f7b921
Anne Loomis Thompson <annelo@microsoft.com>
2020-03-28 15:59:06 -0700

    missed receipt files

---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/CAReceipt.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Azure.AI.FormRecognizer.Models.Receipts
+{
+    /// <summary>
+    /// </summary>
+    public class CAReceipt : RecognizedReceipt
+    {
+        internal CAReceipt(RecognizedReceipt receipt)
+            : base(receipt)
+        {
+        }
+
+        /// <summary>
+        /// Goods and Services Tax.
+        /// </summary>
+        public FormField<float> TaxGst { get; set; }
+
+        /// <summary>
+        /// Provincial Sales Tax.
+        /// </summary>
+        public FormField<float> TaxPst { get; set; }
+    }
+}
---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/RecognizeOptions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.AI.FormRecognizer.Models
+{
+    /// <summary>
+    /// </summary>
+    public class RecognizeOptions
+    {
+        internal RecognizeOptions()
+        {
+        }
+
+        /// <summary>
+        /// </summary>
+        public bool IncludeTextContent { get; set; } = false;
+
+        /// <summary>
+        /// </summary>
+        public bool IncludeCheckboxes { get; set; } = true;
+
+        /// <summary>
+        /// </summary>
+        public bool IncludeTables { get; set; } = true;
+
+        /// <summary>
+        /// </summary>
+        public string ModelVersion { get; set; }
+    }
+}
