9861e092560d282bf959c1fdc72662436d0db1a9
Wei Wei <weiwei@microsoft.com>
2020-03-30 13:33:42 +0800

    [Storage] Support container with Encryption scope (#10867)

--- sdk/storage/Microsoft.Azure.Management.Storage/src/Generated/IBlobContainersOperations.cs
+++ sdk/storage/Microsoft.Azure.Management.Storage/src/Generated/IBlobContainersOperations.cs
@@ -82,13 +82,8 @@ namespace Microsoft.Azure.Management.Storage
         /// Every dash (-) character must be immediately preceded and followed
         /// by a letter or number.
         /// </param>
-        /// <param name='publicAccess'>
-        /// Specifies whether data in the container may be accessed publicly
-        /// and the level of access. Possible values include: 'Container',
-        /// 'Blob', 'None'
-        /// </param>
-        /// <param name='metadata'>
-        /// A name-value pair to associate with the container as metadata.
+        /// <param name='blobContainer'>
+        /// Properties of the blob container to create.
         /// </param>
         /// <param name='customHeaders'>
         /// The headers that will be added to request.
@@ -105,7 +100,7 @@ namespace Microsoft.Azure.Management.Storage
         /// <exception cref="Microsoft.Rest.ValidationException">
         /// Thrown when a required parameter is null
         /// </exception>
-        Task<AzureOperationResponse<BlobContainer>> CreateWithHttpMessagesAsync(string resourceGroupName, string accountName, string containerName, PublicAccess? publicAccess = default(PublicAccess?), IDictionary<string, string> metadata = default(IDictionary<string, string>), Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
+        Task<AzureOperationResponse<BlobContainer>> CreateWithHttpMessagesAsync(string resourceGroupName, string accountName, string containerName, BlobContainer blobContainer, Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
         /// <summary>
         /// Updates container properties as specified in request body.
         /// Properties not mentioned in the request will be unchanged. Update
@@ -127,13 +122,8 @@ namespace Microsoft.Azure.Management.Storage
         /// Every dash (-) character must be immediately preceded and followed
         /// by a letter or number.
         /// </param>
-        /// <param name='publicAccess'>
-        /// Specifies whether data in the container may be accessed publicly
-        /// and the level of access. Possible values include: 'Container',
-        /// 'Blob', 'None'
-        /// </param>
-        /// <param name='metadata'>
-        /// A name-value pair to associate with the container as metadata.
+        /// <param name='blobContainer'>
+        /// Properties to update for the blob container.
         /// </param>
         /// <param name='customHeaders'>
         /// The headers that will be added to request.
@@ -150,7 +140,7 @@ namespace Microsoft.Azure.Management.Storage
         /// <exception cref="Microsoft.Rest.ValidationException">
         /// Thrown when a required parameter is null
         /// </exception>
-        Task<AzureOperationResponse<BlobContainer>> UpdateWithHttpMessagesAsync(string resourceGroupName, string accountName, string containerName, PublicAccess? publicAccess = default(PublicAccess?), IDictionary<string, string> metadata = default(IDictionary<string, string>), Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
+        Task<AzureOperationResponse<BlobContainer>> UpdateWithHttpMessagesAsync(string resourceGroupName, string accountName, string containerName, BlobContainer blobContainer, Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
         /// <summary>
         /// Gets properties of a specified container.
         /// </summary>
