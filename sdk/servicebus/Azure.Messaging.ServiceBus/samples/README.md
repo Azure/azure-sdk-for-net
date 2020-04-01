4b794720a8199ef064e12a550c67d77c0efbde0b
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-03-31 19:49:50 -0700

    Add separate md files for each sample

---
+++ sdk/servicebus/Azure.Messaging.ServiceBus/samples/README.md
@@ -0,0 +1,17 @@
+﻿---
+page_type: sample
+languages:
+- csharp
+products:
+- azure
+- azure-service-bus
+name: Azure.Messaging.ServiceBus samples for .NET
+description: Samples for the Azure.Messaging.ServiceBus client library
+---
+
+# Azure.Messaging.ServiceBus Samples
+
+- [Sending and Receiving Messages](Sample01_HelloWorld.cs).
+- [Settling Messages](Sample02_MessageSettlement.cs).
+- [Sending and Receiving Session Messages](Sample03_Sessions.cs)
+- [Using the Processor](Sample04_Processor.cs)
--- sdk/servicebus/Azure.Messaging.ServiceBus/tests/Samples/Sample01_HelloWorld.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/tests/Samples/Sample01_HelloWorld.cs
@@ -69,8 +69,9 @@ namespace Azure.Messaging.ServiceBus.Tests.Samples
                 // get a receiver that we can use to receive the message
                 ServiceBusReceiver receiver = client.GetReceiver(queueName);
 
-                // the received message is a different type as it contains some service set properties
+                #region Snippet:ServiceBusPeek
                 ServiceBusReceivedMessage peekedMessage = await receiver.PeekAsync();
+                #endregion
 
                 // get the message body as a string
                 string body = Encoding.Default.GetString(peekedMessage.Body.ToArray());
@@ -140,18 +141,20 @@ namespace Azure.Messaging.ServiceBus.Tests.Samples
                 // create a message that we can send
                 ServiceBusMessage message = new ServiceBusMessage(Encoding.Default.GetBytes("Hello world!"));
 
-                // schedule the message for tomorrow
-                // this prevents the message from being enqueued until tomorrow, but it can still be peeked
+                #region Snippet:ServiceBusSchedule
                 long seq = await sender.ScheduleMessageAsync(
                     message,
                     DateTimeOffset.Now.AddDays(1));
+                #endregion
 
                 // get a receiver that we can use to peek the message
                 ServiceBusReceiver receiver = client.GetReceiver(queueName);
                 Assert.IsNotNull(await receiver.PeekAsync());
 
                 // cancel the scheduled messaged, thereby deleting from the service
+                #region Snippet:ServiceBusCancelScheduled
                 await sender.CancelScheduledMessageAsync(seq);
+                #endregion
                 Assert.IsNull(await receiver.PeekAsync());
             }
         }
