10c2664d1fa6bc8931a4ca14fd7e555fdcacbde9
Pavel Krymets <pavel@krymets.com>
2020-03-31 20:11:41 -0700

    Update Autorest C#

--- sdk/search/Azure.Search.Documents/src/Generated/Models/DistanceScoringFunction.cs
+++ sdk/search/Azure.Search.Documents/src/Generated/Models/DistanceScoringFunction.cs
@@ -12,24 +12,6 @@ namespace Azure.Search.Documents.Models
     /// <summary> Defines a function that boosts scores based on distance from a geographic location. </summary>
     public partial class DistanceScoringFunction : ScoringFunction
     {
-        /// <summary> Initializes a new instance of DistanceScoringFunction. </summary>
-        /// <param name="fieldName"> The name of the field used as input to the scoring function. </param>
-        /// <param name="boost"> A multiplier for the raw score. Must be a positive number not equal to 1.0. </param>
-        /// <param name="parameters"> Parameter values for the distance scoring function. </param>
-        public DistanceScoringFunction(string fieldName, double boost, DistanceScoringParameters parameters) : base(fieldName, boost)
-        {
-            if (fieldName == null)
-            {
-                throw new ArgumentNullException(nameof(fieldName));
-            }
-            if (parameters == null)
-            {
-                throw new ArgumentNullException(nameof(parameters));
-            }
-
-            Parameters = parameters;
-            Type = "distance";
-        }
 
         /// <summary> Initializes a new instance of DistanceScoringFunction. </summary>
         /// <param name="type"> Indicates the type of function to use. Valid values include magnitude, freshness, distance, and tag. The function type must be lower case. </param>
