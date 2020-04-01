189eff86c51f13bd94c2e4e2f65daf2a24c3dad7
Pavel Krymets <pavel@krymets.com>
2020-04-01 10:45:34 -0700

    Thoughts

--- sdk/core/Azure.Core/src/Serialization/AzureJsonSerializer.cs
+++ sdk/core/Azure.Core/src/Serialization/AzureJsonSerializer.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.IO;
+using System.Reflection;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 #pragma warning disable CS1591, SA1649, SA1402
@@ -38,5 +40,21 @@ namespace Azure.Core.Serialization
     // Allows adding more JSON specific/optimized methods
     public abstract class AzureJsonSerializer: AzureObjectSerializer
     {
+        // The following methods are experiments on what JSON specific methods are
+
+        // If this serializer supports reading from Utf8JsonReader but want to add some customizations on top of that
+        public abstract bool SupportsUtf8Json { get; }
+        public abstract void Serialize(object o, Utf8JsonWriter stream);
+        public abstract ValueTask SerializeAsync(object o, Utf8JsonWriter stream);
+
+        // Not sure about these signatures because of how Utf8JsonReader manages memory
+        public abstract object Deserialize(in Utf8JsonReader stream);
+        public abstract ValueTask<object> DeserializeAsync(in Utf8JsonReader stream);
+
+        // In case we ever do LINQ and we need to know what JSON member name any particular property would map to
+        // so we can turn .Where(model => model.CustomerProperty == 2) to
+        // a filter string like 'customer_property EQ 2'
+        public abstract string GetJsonPropertyName(PropertyInfo propertyInfo);
+
     }
 }
