3fd4bf81446050be97d9faab3340ff5eabe59e4f
Tal Klein <taklei@microsoft.com>
2020-04-02 09:16:44 +0300

    re-genereted files

--- sdk/securitycenter/Microsoft.Azure.Management.SecurityCenter/src/Generated/ISecurityCenterClient.cs
+++ sdk/securitycenter/Microsoft.Azure.Management.SecurityCenter/src/Generated/ISecurityCenterClient.cs
@@ -122,11 +122,6 @@ namespace Microsoft.Azure.Management.Security
         IIotSecuritySolutionsAnalyticsRecommendationOperations IotSecuritySolutionsAnalyticsRecommendation { get; }
 
         /// <summary>
-        /// Gets the IAllowedConnectionsOperations.
-        /// </summary>
-        IAllowedConnectionsOperations AllowedConnections { get; }
-
-        /// <summary>
         /// Gets the IDiscoveredSecuritySolutionsOperations.
         /// </summary>
         IDiscoveredSecuritySolutionsOperations DiscoveredSecuritySolutions { get; }
@@ -135,11 +130,6 @@ namespace Microsoft.Azure.Management.Security
         /// Gets the IExternalSecuritySolutionsOperations.
         /// </summary>
         IExternalSecuritySolutionsOperations ExternalSecuritySolutions { get; }
-
-        /// <summary>
-        /// Gets the IJitNetworkAccessPoliciesOperations.
-        /// </summary>
-        IJitNetworkAccessPoliciesOperations JitNetworkAccessPolicies { get; }
 
         /// <summary>
         /// Gets the IAdaptiveApplicationControlsOperations.
@@ -160,11 +150,6 @@ namespace Microsoft.Azure.Management.Security
         /// Gets the ITasksOperations.
         /// </summary>
         ITasksOperations Tasks { get; }
-
-        /// <summary>
-        /// Gets the ITopologyOperations.
-        /// </summary>
-        ITopologyOperations Topology { get; }
 
         /// <summary>
         /// Gets the IAutoProvisioningSettingsOperations.
@@ -230,6 +215,26 @@ namespace Microsoft.Azure.Management.Security
         /// Gets the IAssessmentsOperations.
         /// </summary>
         IAssessmentsOperations Assessments { get; }
+
+        /// <summary>
+        /// Gets the IAdaptiveNetworkHardeningsOperations.
+        /// </summary>
+        IAdaptiveNetworkHardeningsOperations AdaptiveNetworkHardenings { get; }
+
+        /// <summary>
+        /// Gets the IAllowedConnectionsOperations.
+        /// </summary>
+        IAllowedConnectionsOperations AllowedConnections { get; }
+
+        /// <summary>
+        /// Gets the ITopologyOperations.
+        /// </summary>
+        ITopologyOperations Topology { get; }
+
+        /// <summary>
+        /// Gets the IJitNetworkAccessPoliciesOperations.
+        /// </summary>
+        IJitNetworkAccessPoliciesOperations JitNetworkAccessPolicies { get; }
 
     }
 }
