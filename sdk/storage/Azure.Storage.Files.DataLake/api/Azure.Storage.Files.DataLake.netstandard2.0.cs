8f16913a4e5411c05f50d677bc28b5bea1f1de3f
Kamil Sobol <61715331+kasobol-msft@users.noreply.github.com>
2020-04-02 10:28:35 -0700

    export-api

--- sdk/storage/Azure.Storage.Files.DataLake/api/Azure.Storage.Files.DataLake.netstandard2.0.cs
+++ sdk/storage/Azure.Storage.Files.DataLake/api/Azure.Storage.Files.DataLake.netstandard2.0.cs
@@ -305,6 +305,19 @@ namespace Azure.Storage.Files.DataLake.Models
         public System.Collections.Generic.IEnumerable<Azure.Storage.Files.DataLake.Models.AccessControlChangeFailure> BatchFailures { get { throw null; } }
         public string ContinuationToken { get { throw null; } }
     }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public partial struct AccessControlRecursiveChangeOptions
+    {
+        public int? BatchSize { get { throw null; } set { } }
+    }
+    public partial class AccessControlRecursiveChangesSummary
+    {
+        internal AccessControlRecursiveChangesSummary() { }
+        public long ChangedDirectoriesCount { get { throw null; } }
+        public long ChangedFilesCount { get { throw null; } }
+        public string ContinuationToken { get { throw null; } }
+        public long FailedChangesCount { get { throw null; } }
+    }
     public enum AccessControlType
     {
         Other = 0,
