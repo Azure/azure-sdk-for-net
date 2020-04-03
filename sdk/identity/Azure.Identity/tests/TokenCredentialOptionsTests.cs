f78b7d332958309d58256b8254c7ecf58d0cf91e
Tom Yao <v-xuyao@microsoft.com>
2020-04-03 09:57:28 +0800

    add tests of environment authority host.

---
+++ sdk/identity/Azure.Identity/tests/TokenCredentialOptionsTests.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core.Testing;
+using NUnit.Framework;
+
+namespace Azure.Identity.Tests
+{
+    public class TokenCredentialOptionsTests : ClientTestBase
+    {
+        public TokenCredentialOptionsTests(bool isAsync) : base(isAsync)
+        {
+        }
+
+        [Test]
+        public void InvalidEnvAuthorityHost()
+        {
+            using (new TestEnvVar("AZURE_AUTHORITY_HOST", "mock-env-authority-host"))
+            {
+                TokenCredentialOptions option = new TokenCredentialOptions();
+
+                Assert.Throws<UriFormatException>(() => { Uri finalUri = option.AuthorityHost; });
+            }
+        }
+
+        [Test]
+        public void EnvAuthorityHost()
+        {
+            string envHostValue = KnownAuthorityHosts.AzureChinaCloud.ToString();
+
+            using (new TestEnvVar("AZURE_AUTHORITY_HOST", envHostValue))
+            {
+                TokenCredentialOptions option = new TokenCredentialOptions();
+                Uri finalUri = option.AuthorityHost;
+
+                Assert.AreEqual(finalUri, new Uri(envHostValue));
+            }
+        }
+
+        [Test]
+        public void CustomAuthorityHost()
+        {
+            string envHostValue = KnownAuthorityHosts.AzureGermanCloud.ToString();
+
+            using (new TestEnvVar("AZURE_AUTHORITY_HOST", envHostValue))
+            {
+                Uri customUri = KnownAuthorityHosts.AzureChinaCloud;
+
+                TokenCredentialOptions option = new TokenCredentialOptions() { AuthorityHost = customUri };
+                Uri finalUri = option.AuthorityHost;
+
+                Assert.AreNotEqual(finalUri, new Uri(envHostValue));
+                Assert.AreEqual(finalUri, customUri);
+            }
+        }
+
+        [Test]
+        public void DefaultAuthorityHost()
+        {
+            using (new TestEnvVar("AZURE_AUTHORITY_HOST", null))
+            {
+                TokenCredentialOptions option = new TokenCredentialOptions();
+
+                Assert.AreEqual(option.AuthorityHost, KnownAuthorityHosts.AzureCloud);
+            }
+        }
+    }
+}
