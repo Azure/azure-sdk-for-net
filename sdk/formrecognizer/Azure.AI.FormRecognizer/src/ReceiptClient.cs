5e30a0ca3873d54a310924925e35043dd9f3b6a0
Mariana Rios Flores <mariari@microsoft.com>
2020-04-06 14:24:27 -0700

    Use AzureKeyCredential in Form Recognizer (#11081)
    * change keycredential
    
    * update package description
    
    * delete api listing file as we disable it

--- sdk/formrecognizer/Azure.AI.FormRecognizer/src/ReceiptClient.cs
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/ReceiptClient.cs
@@ -33,7 +33,7 @@ namespace Azure.AI.FormRecognizer
         /// Initializes a new instance of the <see cref="ReceiptClient"/>.
         /// </summary>
 #pragma warning disable AZC0007 // DO provide a minimal constructor that takes only the parameters required to connect to the service.
-        public ReceiptClient(Uri endpoint, FormRecognizerApiKeyCredential credential) : this(endpoint, credential, new FormRecognizerClientOptions())
+        public ReceiptClient(Uri endpoint, AzureKeyCredential credential) : this(endpoint, credential, new FormRecognizerClientOptions())
 #pragma warning restore AZC0007 // DO provide a minimal constructor that takes only the parameters required to connect to the service.
         {
         }
@@ -42,11 +42,11 @@ namespace Azure.AI.FormRecognizer
         /// Initializes a new instance of the <see cref="ReceiptClient"/>.
         /// </summary>
 #pragma warning disable AZC0007 // DO provide a minimal constructor that takes only the parameters required to connect to the service.
-        public ReceiptClient(Uri endpoint, FormRecognizerApiKeyCredential credential, FormRecognizerClientOptions options)
+        public ReceiptClient(Uri endpoint, AzureKeyCredential credential, FormRecognizerClientOptions options)
 #pragma warning restore AZC0007 // DO provide a minimal constructor that takes only the parameters required to connect to the service.
         {
             _diagnostics = new ClientDiagnostics(options);
-            _pipeline = HttpPipelineBuilder.Build(options, new ApiKeyAuthenticationPolicy(credential));
+            _pipeline = HttpPipelineBuilder.Build(options, new AzureKeyCredentialPolicy(credential, Constants.AuthorizationHeader));
             _operations = new ServiceClient(_diagnostics, _pipeline, endpoint.ToString());
         }
 
