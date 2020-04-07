5d61da87fd963ca8f69a15332adc91aace64038a
kinelski <camaiaor@microsoft.com>
2020-04-07 14:17:07 -0700

    [FormRecognizer] Preview: Design Changes (merge ReceiptClient and FormLayoutClient into FormRecognizerClient) (#11131)

--- sdk/formrecognizer/Azure.AI.FormRecognizer/tests/FormRecognizerClient/FormRecognizerClientLiveTests.cs
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/tests/FormRecognizerClient/FormRecognizerClientLiveTests.cs
@@ -13,31 +13,31 @@ using NUnit.Framework;
 namespace Azure.AI.FormRecognizer.Tests
 {
     /// <summary>
-    /// The suite of tests for the <see cref="FormLayoutClient"/> class.
+    /// The suite of tests for the <see cref="FormRecognizerClient"/> class.
     /// </summary>
     /// <remarks>
     /// These tests have a dependency on live Azure services and may incur costs for the associated
     /// Azure subscription.
     /// </remarks>
     [LiveOnly]
-    public class FormLayoutClientLiveTests : ClientTestBase
+    public class FormRecognizerClientLiveTests : ClientTestBase
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="FormLayoutClientLiveTests"/> class.
+        /// Initializes a new instance of the <see cref="FormRecognizerClientLiveTests"/> class.
         /// </summary>
         /// <param name="isAsync">A flag used by the Azure Core Test Framework to differentiate between tests for asynchronous and synchronous methods.</param>
-        public FormLayoutClientLiveTests(bool isAsync) : base(isAsync)
+        public FormRecognizerClientLiveTests(bool isAsync) : base(isAsync)
         {
         }
 
         /// <summary>
-        /// Verifies that the <see cref="FormLayoutClient" /> is able to connect to the Form
+        /// Verifies that the <see cref="FormRecognizerClient" /> is able to connect to the Form
         /// Recognizer cognitive service and perform operations.
         /// </summary>
         [Test]
         [TestCase(true)]
-        [TestCase(false, Ignore = "The Invoice_1.pdf hasn't been uploaded to GitHub yet.")]
-        public async Task StartExtractLayoutsPopulatesExtractedLayoutPage(bool useStream)
+        [TestCase(false)]
+        public async Task StartRecognizeContentPopulatesExtractedLayoutPage(bool useStream)
         {
             var client = CreateInstrumentedClient();
             Operation<IReadOnlyList<ExtractedLayoutPage>> operation;
@@ -45,12 +45,12 @@ namespace Azure.AI.FormRecognizer.Tests
             if (useStream)
             {
                 using var stream = new FileStream(TestEnvironment.RetrieveInvoicePath(1), FileMode.Open);
-                operation = await client.StartExtractLayoutsAsync(stream, ContentType.Jpeg);
+                operation = await client.StartRecognizeContentAsync(stream, ContentType.Jpeg);
             }
             else
             {
                 var uri = new Uri(TestEnvironment.RetrieveInvoiceUri(1));
-                operation = await client.StartExtractLayoutsAsync(uri);
+                operation = await client.StartRecognizeContentFromUriAsync(uri);
             }
 
             await operation.WaitForCompletionAsync();
@@ -126,11 +126,91 @@ namespace Azure.AI.FormRecognizer.Tests
         }
 
         /// <summary>
-        /// Creates a <see cref="FormLayoutClient" /> with the endpoint and API key provided via environment
+        /// Verifies that the <see cref="FormRecognizerClient" /> is able to connect to the Form
+        /// Recognizer cognitive service and perform operations.
+        /// </summary>
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task StartRecognizeReceiptsPopulatesExtractedReceipt(bool useStream)
+        {
+            var client = CreateInstrumentedClient();
+            Operation<IReadOnlyList<ExtractedReceipt>> operation;
+
+            if (useStream)
+            {
+                using var stream = new FileStream(TestEnvironment.ReceiptPath, FileMode.Open);
+                operation = await client.StartRecognizeReceiptsAsync(stream, ContentType.Jpeg);
+            }
+            else
+            {
+                var uri = new Uri(TestEnvironment.ReceiptUri);
+                operation = await client.StartRecognizeReceiptsFromUriAsync(uri);
+            }
+
+            await operation.WaitForCompletionAsync();
+
+            Assert.IsTrue(operation.HasValue);
+
+            var receipt = operation.Value.Single();
+
+            // The expected values are based on the values returned by the service, and not the actual
+            // values present in the receipt. We are not testing the service here, but the SDK.
+
+            Assert.AreEqual(1, receipt.StartPageNumber);
+            Assert.AreEqual(1, receipt.EndPageNumber);
+
+            Assert.AreEqual("Contoso Contoso", receipt.MerchantName);
+            Assert.AreEqual("123 Main Street Redmond, WA 98052", receipt.MerchantAddress);
+            Assert.AreEqual("123-456-7890", receipt.MerchantPhoneNumber);
+
+            Assert.IsNotNull(receipt.TransactionDate);
+            Assert.IsNotNull(receipt.TransactionTime);
+
+            var date = receipt.TransactionDate.Value;
+            var time = receipt.TransactionTime.Value;
+
+            Assert.AreEqual(10, date.Day);
+            Assert.AreEqual(6, date.Month);
+            Assert.AreEqual(2019, date.Year);
+
+            Assert.AreEqual(13, time.Hour);
+            Assert.AreEqual(59, time.Minute);
+            Assert.AreEqual(0, time.Second);
+
+            var expectedItems = new List<(int? Quantity, string Name, float? Price, float? TotalPrice)>()
+            {
+                (null, "8GB RAM (Black)", null, 999.00f),
+                (1, "SurfacePen", null, 99.99f)
+            };
+
+            // Include a bit of tolerance when comparing float types.
+
+            Assert.AreEqual(expectedItems.Count, receipt.Items.Count);
+
+            for (var itemIndex = 0; itemIndex < receipt.Items.Count; itemIndex++)
+            {
+                var receiptItem = receipt.Items[itemIndex];
+                var expectedItem = expectedItems[itemIndex];
+
+                Assert.AreEqual(expectedItem.Quantity, receiptItem.Quantity, $"{receiptItem.Quantity} mismatch in item with index {itemIndex}.");
+                Assert.AreEqual(expectedItem.Name, receiptItem.Name, $"{receiptItem.Name} mismatch in item with index {itemIndex}.");
+                Assert.That(receiptItem.Price, Is.EqualTo(expectedItem.Price).Within(0.0001), $"{receiptItem.Price} mismatch in item with index {itemIndex}.");
+                Assert.That(receiptItem.TotalPrice, Is.EqualTo(expectedItem.TotalPrice).Within(0.0001), $"{receiptItem.TotalPrice} mismatch in item with index {itemIndex}.");
+            }
+
+            Assert.That(receipt.Subtotal, Is.EqualTo(1098.99).Within(0.0001));
+            Assert.That(receipt.Tax, Is.EqualTo(104.40).Within(0.0001));
+            Assert.IsNull(receipt.Tip);
+            Assert.That(receipt.Total, Is.EqualTo(1203.39).Within(0.0001));
+        }
+
+        /// <summary>
+        /// Creates a <see cref="FormRecognizerClient" /> with the endpoint and API key provided via environment
         /// variables and instruments it to make use of the Azure Core Test Framework functionalities.
         /// </summary>
-        /// <returns>The instrumented <see cref="FormLayoutClient" />.</returns>
-        private FormLayoutClient CreateInstrumentedClient()
+        /// <returns>The instrumented <see cref="FormRecognizerClient" />.</returns>
+        private FormRecognizerClient CreateInstrumentedClient()
         {
             var endpointEnvironmentVariable = Environment.GetEnvironmentVariable(TestEnvironment.EndpointEnvironmentVariableName);
             var keyEnvironmentVariable = Environment.GetEnvironmentVariable(TestEnvironment.ApiKeyEnvironmentVariableName);
@@ -140,7 +220,7 @@ namespace Azure.AI.FormRecognizer.Tests
 
             var endpoint = new Uri(endpointEnvironmentVariable);
             var credential = new AzureKeyCredential(keyEnvironmentVariable);
-            var client = new FormLayoutClient(endpoint, credential);
+            var client = new FormRecognizerClient(endpoint, credential);
 
             return InstrumentClient(client);
         }
