9e956d80fdc2fdb340d90047089188cf14402610
Heath Stewart <heaths@microsoft.com>
2020-04-08 16:21:18 -0700

    Add additional logging for Key Vault live tests (#11170)
    Needed to properly diagnose and fix issue #10496.

--- sdk/keyvault/Azure.Security.KeyVault.Keys/tests/samples/SampleSnippets.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/tests/samples/SampleSnippets.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.Core.Testing;
 using Azure.Identity;
 using Azure.Security.KeyVault.Keys.Cryptography;
 using NUnit.Framework;
@@ -15,7 +14,6 @@ namespace Azure.Security.KeyVault.Keys.Samples
     /// <summary>
     /// Samples that are used in the associated README.md file.
     /// </summary>
-    [LiveOnly]
     public partial class Snippets
     {
 #pragma warning disable IDE1006 // Naming Styles
