2cf1ba368606a5c78a415f68b5aaf57278e73847
Daniel Jurek <djurek@microsoft.com>
2020-04-13 22:01:05 -0700

    temporarily disable smoke tests and deployments for cosmos db

--- common/SmokeTests/SmokeTest/Program.cs
+++ common/SmokeTests/SmokeTest/Program.cs
@@ -11,13 +11,14 @@ namespace SmokeTest
     class Program
     {
         static async Task<int> Main(string[] args)
-        {                        
+        {
             Console.WriteLine("SMOKE TEST FOR TRACK 2 LIBRARIES");
 
             await KeyVaultTest.RunTests();
             await BlobStorageTest.RunTests();
             await EventHubsTest.RunTests();
-            await CosmosDBTest.RunTests();
+            // Temporarily disable Cosmos DB smoke tests
+            // await CosmosDBTest.RunTests();
 
             return 0;
         }
