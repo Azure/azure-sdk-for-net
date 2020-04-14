1a4a497637cff0e342557bcc7f2e660bd81988a4
Daniel Jurek <djurek@microsoft.com>
2020-04-14 14:14:21 -0700

    add link to re-enable issue

--- common/SmokeTests/SmokeTest/Program.cs
+++ common/SmokeTests/SmokeTest/Program.cs
@@ -18,6 +18,8 @@ namespace SmokeTest
             await BlobStorageTest.RunTests();
             await EventHubsTest.RunTests();
             // Temporarily disable Cosmos DB smoke tests
+            // Issue to re-enable:
+            // https://github.com/Azure/azure-sdk-for-net/issues/11297
             // await CosmosDBTest.RunTests();
 
             return 0;
