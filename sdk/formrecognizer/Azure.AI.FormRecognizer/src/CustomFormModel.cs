989b316a970ed40265031de979a6ba5482d2c0db
Mariana Rios Flores <mariari@microsoft.com>
2020-04-14 10:29:02 -0700

    FR GetCustomModel implementation (#11299)
    * ConvertFromUnlabeled
    
    * ConvertFromLabeled
    
    * docstrings
    
    * Improve docstrings. -> thanks to Anne
    
    Co-Authored-By: Anne Thompson <annelo@microsoft.com>
    
    * PR feedback
    
    * trainresult null
    
    Co-authored-by: Anne Thompson <annelo@microsoft.com>

--- sdk/formrecognizer/Azure.AI.FormRecognizer/src/CustomFormModel.cs
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/CustomFormModel.cs
@@ -7,36 +7,95 @@ using System.Collections.Generic;
 namespace Azure.AI.FormRecognizer.Training
 {
     /// <summary>
+    /// Represents a model trained from custom forms.
     /// </summary>
     public class CustomFormModel
     {
-        internal CustomFormModel()
+        internal CustomFormModel(Model_internal model)
         {
+            ModelId = model.ModelInfo.ModelId.ToString();
+            Status = model.ModelInfo.Status;
+            CreatedOn = model.ModelInfo.CreatedDateTime;
+            LastModified = model.ModelInfo.LastUpdatedDateTime;
+            Models = ConvertToSubmodels(model);
+            TrainingDocuments = model.TrainResult?.TrainingDocuments;
         }
 
         /// <summary>
+        /// Model identifier.
         /// </summary>
         public string ModelId { get; }
 
         /// <summary>
+        /// Status indicating the model's readiness for use.
         /// </summary>
         public CustomFormModelStatus Status { get; }
 
         /// <summary>
+        /// Date and time (UTC) when model training was started.
         /// </summary>
-        public DateTime CreatedOn { get; }
+        public DateTimeOffset CreatedOn { get; }
 
         /// <summary>
+        /// Date and time (UTC) when model training completed.
         /// </summary>
-        public DateTime LastUpdatedOn { get; }
+        public DateTimeOffset LastModified { get; }
 
         /// <summary>
+        /// A list of submodels, each of which extract fields from a different type of form.
         /// </summary>
         public IReadOnlyList<CustomFormSubModel> Models { get; }
 
         /// <summary>
+        ///  Meta-data about each of the documents used to train the model.
         /// </summary>
-        // TODO: for composed models, union what comes back on submodels into this single list.
         public IReadOnlyList<TrainingDocumentInfo> TrainingDocuments { get; }
+
+        private static IReadOnlyList<CustomFormSubModel> ConvertToSubmodels(Model_internal model)
+        {
+            if (model.Keys != null)
+                return ConvertFromUnlabeled(model.Keys);
+
+            if (model.TrainResult != null)
+                return ConvertFromLabeled(model);
+
+            return null;
+        }
+
+        private static IReadOnlyList<CustomFormSubModel> ConvertFromUnlabeled(KeysResult_internal keys)
+        {
+            var subModels = new List<CustomFormSubModel>();
+
+            var fieldMap = new Dictionary<string, CustomFormModelField>();
+            foreach (var cluster in keys.Clusters)
+            {
+                for (int i = 0; i < cluster.Value.Count; i++)
+                {
+                    string fieldName = "field-" + i;
+                    string fieldLabel = cluster.Value[i];
+                    fieldMap.Add(fieldName, new CustomFormModelField(fieldName, fieldLabel, default));
+                }
+                subModels.Add(new CustomFormSubModel(
+                    $"form-{cluster.Key}",
+                    default,
+                    fieldMap));
+            }
+            return subModels;
+        }
+
+        private static IReadOnlyList<CustomFormSubModel> ConvertFromLabeled(Model_internal model)
+        {
+            var fieldMap = new Dictionary<string, CustomFormModelField>();
+            foreach (var formFieldsReport in model.TrainResult.Fields)
+            {
+                fieldMap.Add(formFieldsReport.Name, new CustomFormModelField(formFieldsReport.Name, null, formFieldsReport.Accuracy));
+            }
+
+            return new List<CustomFormSubModel> {
+                new CustomFormSubModel(
+                    $"form-{model.ModelInfo.ModelId}",
+                    model.TrainResult.AverageModelAccuracy,
+                    fieldMap)};
+        }
     }
 }
