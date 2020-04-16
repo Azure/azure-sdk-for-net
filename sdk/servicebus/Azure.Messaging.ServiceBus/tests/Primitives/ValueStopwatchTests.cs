6452a1b3b5c5bc0527cb54182ac29bbca785a320
danielmarbach <daniel.marbach@openplace.net>
2020-04-16 13:38:42 +0200

    Original license

--- sdk/servicebus/Azure.Messaging.ServiceBus/tests/Primitives/ValueStopwatchTests.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/tests/Primitives/ValueStopwatchTests.cs
@@ -1,5 +1,7 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+#pragma warning disable SA1636
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+#pragma warning restore SA1636
 
 using System;
 using System.Threading.Tasks;
