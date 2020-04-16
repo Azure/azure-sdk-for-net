b0bf21428d8a52f1ed7764e610c137530215b282
Pavel Krymets <pavel@krymets.com>
2020-04-16 12:06:54 -0700

    export api

--- sdk/core/Azure.Core/api/Azure.Core.netstandard2.0.cs
+++ sdk/core/Azure.Core/api/Azure.Core.netstandard2.0.cs
@@ -250,6 +250,14 @@ namespace Azure.Core
         PerCall = 0,
         PerRetry = 1,
     }
+    public abstract partial class ObjectSerializer
+    {
+        protected ObjectSerializer() { }
+        public abstract object Deserialize(System.IO.Stream stream, System.Type returnType);
+        public abstract System.Threading.Tasks.ValueTask<object> DeserializeAsync(System.IO.Stream stream, System.Type returnType);
+        public abstract void Serialize(System.IO.Stream stream, object? value, System.Type inputType);
+        public abstract System.Threading.Tasks.ValueTask SerializeAsync(System.IO.Stream stream, object? value, System.Type inputType);
+    }
     public abstract partial class Request : System.IDisposable
     {
         protected Request() { }
@@ -492,16 +500,5 @@ namespace Azure.Core.Pipeline
         public abstract Azure.Core.Request CreateRequest();
         public abstract void Process(Azure.Core.HttpMessage message);
         public abstract System.Threading.Tasks.ValueTask ProcessAsync(Azure.Core.HttpMessage message);
-    }
-}
-namespace Azure.Core.Serialization
-{
-    public abstract partial class AzureObjectSerializer
-    {
-        protected AzureObjectSerializer() { }
-        public abstract object Deserialize(System.IO.Stream stream, System.Type returnType);
-        public abstract System.Threading.Tasks.ValueTask<object> DeserializeAsync(System.IO.Stream stream, System.Type returnType);
-        public abstract void Serialize(System.IO.Stream stream, object? value, System.Type inputType);
-        public abstract System.Threading.Tasks.ValueTask SerializeAsync(System.IO.Stream stream, object? value, System.Type inputType);
     }
 }
