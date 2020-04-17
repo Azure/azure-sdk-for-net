bb82ff2f74c975688157c5682d8ff6d3d9917154
Alexander Sher <AlexanderSher@users.noreply.github.com>
2020-04-17 11:45:18 -0500

    Fix #11406: VisualStudioCredential: Could not find a part of the path .IdentityService\AzureServiceAuth\tokenprovider.json (#11408)

--- sdk/identity/Azure.Identity/tests/VisualStudioCredentialTests.cs
+++ sdk/identity/Azure.Identity/tests/VisualStudioCredentialTests.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -127,6 +128,16 @@ namespace Azure.Identity.Tests
             var (_, _, processOutput) = CreateTestToken();
             var testProcess = new TestProcess { Output = processOutput };
             var credential = InstrumentClient(new VisualStudioCredential(default, default, new TestFileSystemService(), new TestProcessService(testProcess)));
+            Assert.ThrowsAsync<CredentialUnavailableException>(async () => await credential.GetTokenAsync(new TokenRequestContext(new[]{"https://vault.azure.net/"}), CancellationToken.None));
+        }
+
+        [Test]
+        public void AuthenticateWithVsCredential_NoDirectoryFound()
+        {
+            var (_, _, processOutput) = CreateTestToken();
+            var fileSystem = new TestFileSystemService { ReadAllHandler = s => throw new DirectoryNotFoundException() };
+            var testProcess = new TestProcess { Output = processOutput };
+            var credential = InstrumentClient(new VisualStudioCredential(default, default, fileSystem, new TestProcessService(testProcess)));
             Assert.ThrowsAsync<CredentialUnavailableException>(async () => await credential.GetTokenAsync(new TokenRequestContext(new[]{"https://vault.azure.net/"}), CancellationToken.None));
         }
 
