5d4e7c62ef711bc114a813b8c8fb3ea8bce29928
Anne Loomis Thompson <annelo@microsoft.com>
2020-04-20 10:56:14 -0700

    pr fb

--- sdk/formrecognizer/Azure.AI.FormRecognizer/src/OperationExtensions.cs
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/OperationExtensions.cs
@@ -4,12 +4,14 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.AI.FormRecognizer.Models;
+using Azure.AI.FormRecognizer.Training;
 
-namespace Azure.AI.FormRecognizer.Models
+namespace Azure.AI.FormRecognizer
 {
     /// <summary>
     /// </summary>
-    public static class RecognizeOperationExtensions
+    public static class OperationExtensions
     {
         /// <summary>
         /// </summary>
@@ -39,6 +41,17 @@ namespace Azure.AI.FormRecognizer.Models
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         public static async Task<Response<IReadOnlyList<FormPage>>> WaitForCompletionAsync(this Task<RecognizeContentOperation> operation, CancellationToken cancellationToken = default)
+        {
+            var o = await operation.ConfigureAwait(false);
+            return await o.WaitForCompletionAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Response<CustomFormModel>> WaitForCompletionAsync(this Task<TrainingOperation> operation, CancellationToken cancellationToken = default)
         {
             var o = await operation.ConfigureAwait(false);
             return await o.WaitForCompletionAsync(cancellationToken).ConfigureAwait(false);
