cd0c019a60d7daf0b7a68c076e206f80991879ad
Anne Loomis Thompson <annelo@microsoft.com>
2020-04-20 07:29:52 -0700

    added simple LRO extension methods

---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/RecognizeOperationExtensions.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.AI.FormRecognizer.Models
+{
+    /// <summary>
+    /// </summary>
+    public static class RecognizeOperationExtensions
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async ValueTask<Response<IReadOnlyList<RecognizedReceipt>>> WaitForCompletionAsync(this Task<RecognizeReceiptsOperation> operation, CancellationToken cancellationToken = default)
+        {
+            var o = await operation.ConfigureAwait(false);
+            return await o.WaitForCompletionAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async ValueTask<Response<IReadOnlyList<RecognizedForm>>> WaitForCompletionAsync(this Task<RecognizeCustomFormsOperation> operation, CancellationToken cancellationToken = default)
+        {
+            var o = await operation.ConfigureAwait(false);
+            return await o.WaitForCompletionAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async ValueTask<Response<IReadOnlyList<FormPage>>> WaitForCompletionAsync(this Task<RecognizeContentOperation> operation, CancellationToken cancellationToken = default)
+        {
+            var o = await operation.ConfigureAwait(false);
+            return await o.WaitForCompletionAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/TrainingOperationExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.AI.FormRecognizer.Training
+{
+    /// <summary>
+    /// </summary>
+    public static class TrainingOperationExtensions
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async ValueTask<Response<CustomFormModel>> WaitForCompletionAsync(this Task<TrainingOperation> operation, CancellationToken cancellationToken = default)
+        {
+            var o = await operation.ConfigureAwait(false);
+            return await o.WaitForCompletionAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
