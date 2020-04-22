40508a13eb2359c7552c1aa3aa29225fe1cba0c0
Christopher Scott <chriss@microsoft.com>
2020-04-22 15:26:04 -0500

    Initial generated Azure.Data.Tables client (#11506)
    * initial commit of basic structure for Azure.Data.Tables
    
    * add tests.yml
    
    * fix readme
    
    * fixup naming and package version
    
    * ci merge conflict
    
    * add /sdk/tables/ to codeowners
    
    * make canary test work
    
    * create changelog entry
    
    * # Azure Tables client *library* for .NET
    
    * export api

---
+++ sdk/tables/Azure.Data.Tables/src/TableConstants.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Data.Tables
+{
+    internal static class TableConstants
+    {
+        internal static class HeaderNames
+        {
+            public const string Date = "x-ms-date";
+            public const string SharedKey = "SharedKeyLite";
+            public const string Authorization = "Authorization";
+        }
+    }
+}
---
+++ sdk/tables/Azure.Data.Tables/src/autorest.md
@@ -0,0 +1,12 @@
+# Azure.Data.Tables
+
+### AutoRest Configuration
+> see https://aka.ms/autorest
+
+``` yaml
+title: Azure.Data.Tables
+input-file:
+    - https://github.com/shurd/azure-rest-api-specs/tree/tablesSwagger/specification/cosmos-db/data-plane/Microsoft.TablesStorage/preview/2018-10-10/table.json
+namespace: Azure.Data.Tables
+include-csproj: disable
+```
