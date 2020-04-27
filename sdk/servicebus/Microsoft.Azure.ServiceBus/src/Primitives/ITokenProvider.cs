2f5eb48fcc8b3d6041415d44125ad55ca44d268e
Timothy M. Stepanski <slice@wendal.io>
2020-04-27 19:42:49 -0400

    Removed unneeded imports

--- sdk/servicebus/Microsoft.Azure.ServiceBus/src/Primitives/ITokenProvider.cs
+++ sdk/servicebus/Microsoft.Azure.ServiceBus/src/Primitives/ITokenProvider.cs
@@ -4,9 +4,6 @@
 namespace Microsoft.Azure.ServiceBus.Primitives
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Threading.Tasks;
 
     /// <summary>
