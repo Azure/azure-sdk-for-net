d4bf4fe1612b0843a71977e0a3ddcdd95b1ab4ba
Timothy M. Stepanski <slice@wendal.io>
2020-04-27 20:35:29 -0400

    Fixed string localization warning

--- sdk/servicebus/Microsoft.Azure.ServiceBus/src/Management/SubscriptionDescription.cs
+++ sdk/servicebus/Microsoft.Azure.ServiceBus/src/Management/SubscriptionDescription.cs
@@ -239,7 +239,7 @@ namespace Microsoft.Azure.ServiceBus.Management
             {
                 if (value == null)
                 {
-                    throw new ArgumentNullException(nameof(UserMetadata), $"Value cannot be null");
+                    throw new ArgumentNullException(nameof(UserMetadata), @"Value cannot be null");
                 }
 
                 if (value.Length > ManagementClientConstants.MaxUserMetadataLength)
