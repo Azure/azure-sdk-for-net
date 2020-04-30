69c27cd92c57e3f6c0d67e66d3e5ba022e8d3385
Yabo Hu <yabhu@microsoft.com>
2020-04-30 12:27:24 +0800

    Applicationinsights 0.3.0 (#11694)
    * delete outdated generate.cmd
    
    * add generate.ps1
    
    * generate SDK from updated swagger specs
    
    * Update generate.ps1
    
    Co-authored-by: Yeming Liu <Yeming.Liu@microsoft.com>

--- sdk/applicationinsights/Microsoft.Azure.Management.ApplicationInsights/src/Generated/ComponentLinkedStorageAccountsOperationsExtensions.cs
+++ sdk/applicationinsights/Microsoft.Azure.Management.ApplicationInsights/src/Generated/ComponentLinkedStorageAccountsOperationsExtensions.cs
@@ -13,18 +13,17 @@ namespace Microsoft.Azure.Management.ApplicationInsights.Management
     using Microsoft.Rest;
     using Microsoft.Rest.Azure;
     using Models;
-    using System.Collections;
-    using System.Collections.Generic;
     using System.Threading;
     using System.Threading.Tasks;
 
     /// <summary>
-    /// Extension methods for AnnotationsOperations.
+    /// Extension methods for ComponentLinkedStorageAccountsOperations.
     /// </summary>
-    public static partial class AnnotationsOperationsExtensions
+    public static partial class ComponentLinkedStorageAccountsOperationsExtensions
     {
             /// <summary>
-            /// Gets the list of annotations for a component for given time range
+            /// Returns the current linked storage settings for an Application Insights
+            /// component.
             /// </summary>
             /// <param name='operations'>
             /// The operations group for this extension method.
@@ -35,20 +34,14 @@ namespace Microsoft.Azure.Management.ApplicationInsights.Management
             /// <param name='resourceName'>
             /// The name of the Application Insights component resource.
             /// </param>
-            /// <param name='start'>
-            /// The start time to query from for annotations, cannot be older than 90 days
-            /// from current date.
-            /// </param>
-            /// <param name='end'>
-            /// The end time to query for annotations.
-            /// </param>
-            public static IEnumerable<Annotation> List(this IAnnotationsOperations operations, string resourceGroupName, string resourceName, string start, string end)
+            public static ComponentLinkedStorageAccounts Get(this IComponentLinkedStorageAccountsOperations operations, string resourceGroupName, string resourceName)
             {
-                return operations.ListAsync(resourceGroupName, resourceName, start, end).GetAwaiter().GetResult();
+                return operations.GetAsync(resourceGroupName, resourceName).GetAwaiter().GetResult();
             }
 
             /// <summary>
-            /// Gets the list of annotations for a component for given time range
+            /// Returns the current linked storage settings for an Application Insights
+            /// component.
             /// </summary>
             /// <param name='operations'>
             /// The operations group for this extension method.
@@ -58,27 +51,21 @@ namespace Microsoft.Azure.Management.ApplicationInsights.Management
             /// </param>
             /// <param name='resourceName'>
             /// The name of the Application Insights component resource.
-            /// </param>
-            /// <param name='start'>
-            /// The start time to query from for annotations, cannot be older than 90 days
-            /// from current date.
-            /// </param>
-            /// <param name='end'>
-            /// The end time to query for annotations.
             /// </param>
             /// <param name='cancellationToken'>
             /// The cancellation token.
             /// </param>
-            public static async Task<IEnumerable<Annotation>> ListAsync(this IAnnotationsOperations operations, string resourceGroupName, string resourceName, string start, string end, CancellationToken cancellationToken = default(CancellationToken))
+            public static async Task<ComponentLinkedStorageAccounts> GetAsync(this IComponentLinkedStorageAccountsOperations operations, string resourceGroupName, string resourceName, CancellationToken cancellationToken = default(CancellationToken))
             {
-                using (var _result = await operations.ListWithHttpMessagesAsync(resourceGroupName, resourceName, start, end, null, cancellationToken).ConfigureAwait(false))
+                using (var _result = await operations.GetWithHttpMessagesAsync(resourceGroupName, resourceName, null, cancellationToken).ConfigureAwait(false))
                 {
                     return _result.Body;
                 }
             }
 
             /// <summary>
-            /// Create an Annotation of an Application Insights component.
+            /// Replace current linked storage account for an Application Insights
+            /// component.
             /// </summary>
             /// <param name='operations'>
             /// The operations group for this extension method.
@@ -89,17 +76,17 @@ namespace Microsoft.Azure.Management.ApplicationInsights.Management
             /// <param name='resourceName'>
             /// The name of the Application Insights component resource.
             /// </param>
-            /// <param name='annotationProperties'>
-            /// Properties that need to be specified to create an annotation of a
-            /// Application Insights component.
+            /// <param name='linkedStorageAccount'>
+            /// Linked storage account resource ID
             /// </param>
-            public static IList<Annotation> Create(this IAnnotationsOperations operations, string resourceGroupName, string resourceName, Annotation annotationProperties)
+            public static ComponentLinkedStorageAccounts CreateAndUpdate(this IComponentLinkedStorageAccountsOperations operations, string resourceGroupName, string resourceName, string linkedStorageAccount = default(string))
             {
-                return operations.CreateAsync(resourceGroupName, resourceName, annotationProperties).GetAwaiter().GetResult();
+                return operations.CreateAndUpdateAsync(resourceGroupName, resourceName, linkedStorageAccount).GetAwaiter().GetResult();
             }
 
             /// <summary>
-            /// Create an Annotation of an Application Insights component.
+            /// Replace current linked storage account for an Application Insights
+            /// component.
             /// </summary>
             /// <param name='operations'>
             /// The operations group for this extension method.
@@ -110,23 +97,22 @@ namespace Microsoft.Azure.Management.ApplicationInsights.Management
             /// <param name='resourceName'>
             /// The name of the Application Insights component resource.
             /// </param>
-            /// <param name='annotationProperties'>
-            /// Properties that need to be specified to create an annotation of a
-            /// Application Insights component.
+            /// <param name='linkedStorageAccount'>
+            /// Linked storage account resource ID
             /// </param>
             /// <param name='cancellationToken'>
             /// The cancellation token.
             /// </param>
-            public static async Task<IList<Annotation>> CreateAsync(this IAnnotationsOperations operations, string resourceGroupName, string resourceName, Annotation annotationProperties, CancellationToken cancellationToken = default(CancellationToken))
+            public static async Task<ComponentLinkedStorageAccounts> CreateAndUpdateAsync(this IComponentLinkedStorageAccountsOperations operations, string resourceGroupName, string resourceName, string linkedStorageAccount = default(string), CancellationToken cancellationToken = default(CancellationToken))
             {
-                using (var _result = await operations.CreateWithHttpMessagesAsync(resourceGroupName, resourceName, annotationProperties, null, cancellationToken).ConfigureAwait(false))
+                using (var _result = await operations.CreateAndUpdateWithHttpMessagesAsync(resourceGroupName, resourceName, linkedStorageAccount, null, cancellationToken).ConfigureAwait(false))
                 {
                     return _result.Body;
                 }
             }
 
             /// <summary>
-            /// Delete an Annotation of an Application Insights component.
+            /// Update linked storage accounts for an Application Insights component.
             /// </summary>
             /// <param name='operations'>
             /// The operations group for this extension method.
@@ -137,17 +123,16 @@ namespace Microsoft.Azure.Management.ApplicationInsights.Management
             /// <param name='resourceName'>
             /// The name of the Application Insights component resource.
             /// </param>
-            /// <param name='annotationId'>
-            /// The unique annotation ID. This is unique within a Application Insights
-            /// component.
+            /// <param name='linkedStorageAccount'>
+            /// Linked storage account resource ID
             /// </param>
-            public static void Delete(this IAnnotationsOperations operations, string resourceGroupName, string resourceName, string annotationId)
+            public static ComponentLinkedStorageAccounts Update(this IComponentLinkedStorageAccountsOperations operations, string resourceGroupName, string resourceName, string linkedStorageAccount = default(string))
             {
-                operations.DeleteAsync(resourceGroupName, resourceName, annotationId).GetAwaiter().GetResult();
+                return operations.UpdateAsync(resourceGroupName, resourceName, linkedStorageAccount).GetAwaiter().GetResult();
             }
 
             /// <summary>
-            /// Delete an Annotation of an Application Insights component.
+            /// Update linked storage accounts for an Application Insights component.
             /// </summary>
             /// <param name='operations'>
             /// The operations group for this extension method.
@@ -158,64 +143,55 @@ namespace Microsoft.Azure.Management.ApplicationInsights.Management
             /// <param name='resourceName'>
             /// The name of the Application Insights component resource.
             /// </param>
-            /// <param name='annotationId'>
-            /// The unique annotation ID. This is unique within a Application Insights
-            /// component.
+            /// <param name='linkedStorageAccount'>
+            /// Linked storage account resource ID
             /// </param>
             /// <param name='cancellationToken'>
             /// The cancellation token.
             /// </param>
-            public static async Task DeleteAsync(this IAnnotationsOperations operations, string resourceGroupName, string resourceName, string annotationId, CancellationToken cancellationToken = default(CancellationToken))
+            public static async Task<ComponentLinkedStorageAccounts> UpdateAsync(this IComponentLinkedStorageAccountsOperations operations, string resourceGroupName, string resourceName, string linkedStorageAccount = default(string), CancellationToken cancellationToken = default(CancellationToken))
             {
-                (await operations.DeleteWithHttpMessagesAsync(resourceGroupName, resourceName, annotationId, null, cancellationToken).ConfigureAwait(false)).Dispose();
-            }
-
-            /// <summary>
-            /// Get the annotation for given id.
-            /// </summary>
-            /// <param name='operations'>
-            /// The operations group for this extension method.
-            /// </param>
-            /// <param name='resourceGroupName'>
-            /// The name of the resource group. The name is case insensitive.
-            /// </param>
-            /// <param name='resourceName'>
-            /// The name of the Application Insights component resource.
-            /// </param>
-            /// <param name='annotationId'>
-            /// The unique annotation ID. This is unique within a Application Insights
-            /// component.
-            /// </param>
-            public static IList<Annotation> Get(this IAnnotationsOperations operations, string resourceGroupName, string resourceName, string annotationId)
-            {
-                return operations.GetAsync(resourceGroupName, resourceName, annotationId).GetAwaiter().GetResult();
-            }
-
-            /// <summary>
-            /// Get the annotation for given id.
-            /// </summary>
-            /// <param name='operations'>
-            /// The operations group for this extension method.
-            /// </param>
-            /// <param name='resourceGroupName'>
-            /// The name of the resource group. The name is case insensitive.
-            /// </param>
-            /// <param name='resourceName'>
-            /// The name of the Application Insights component resource.
-            /// </param>
-            /// <param name='annotationId'>
-            /// The unique annotation ID. This is unique within a Application Insights
-            /// component.
-            /// </param>
-            /// <param name='cancellationToken'>
-            /// The cancellation token.
-            /// </param>
-            public static async Task<IList<Annotation>> GetAsync(this IAnnotationsOperations operations, string resourceGroupName, string resourceName, string annotationId, CancellationToken cancellationToken = default(CancellationToken))
-            {
-                using (var _result = await operations.GetWithHttpMessagesAsync(resourceGroupName, resourceName, annotationId, null, cancellationToken).ConfigureAwait(false))
+                using (var _result = await operations.UpdateWithHttpMessagesAsync(resourceGroupName, resourceName, linkedStorageAccount, null, cancellationToken).ConfigureAwait(false))
                 {
                     return _result.Body;
                 }
+            }
+
+            /// <summary>
+            /// Delete linked storage accounts for an Application Insights component.
+            /// </summary>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='resourceGroupName'>
+            /// The name of the resource group. The name is case insensitive.
+            /// </param>
+            /// <param name='resourceName'>
+            /// The name of the Application Insights component resource.
+            /// </param>
+            public static void Delete(this IComponentLinkedStorageAccountsOperations operations, string resourceGroupName, string resourceName)
+            {
+                operations.DeleteAsync(resourceGroupName, resourceName).GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// Delete linked storage accounts for an Application Insights component.
+            /// </summary>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='resourceGroupName'>
+            /// The name of the resource group. The name is case insensitive.
+            /// </param>
+            /// <param name='resourceName'>
+            /// The name of the Application Insights component resource.
+            /// </param>
+            /// <param name='cancellationToken'>
+            /// The cancellation token.
+            /// </param>
+            public static async Task DeleteAsync(this IComponentLinkedStorageAccountsOperations operations, string resourceGroupName, string resourceName, CancellationToken cancellationToken = default(CancellationToken))
+            {
+                (await operations.DeleteWithHttpMessagesAsync(resourceGroupName, resourceName, null, cancellationToken).ConfigureAwait(false)).Dispose();
             }
 
     }
