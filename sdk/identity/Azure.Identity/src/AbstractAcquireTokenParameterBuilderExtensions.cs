7ea64c273c363794f0e980fc7912bf3d213624e1
Scott Schaab <sschaab@microsoft.com>
2020-05-01 23:40:02 -0700

    [Identity] User authentication API for applications (#11285)
    * updates for interactive browser authentication
    
    * adding authenticate methods to DeviceCodeCredential
    
    * adding AuthenticationProfile to options classes
    
    * updating api definition
    
    * incorperate arch board feedback
    
    * updating identity to use msal extensions
    
    * adding token cache storage values for Mac and Linux
    
    * missed AuthenticationRecord rename
    
    * adding tests
    
    * adding test recordings
    
    * address pr feedback
    
    * update api listing
    
    * updating changelog
    
    * updating changelog
    
    * address pr feedback

--- sdk/identity/Azure.Identity/src/AbstractAcquireTokenParameterBuilderExtensions.cs
+++ sdk/identity/Azure.Identity/src/AbstractAcquireTokenParameterBuilderExtensions.cs
@@ -12,11 +12,13 @@ namespace Azure.Identity
         public static async Task<AuthenticationResult> ExecuteAsync<T>(this AbstractAcquireTokenParameterBuilder<T> builder, bool async, CancellationToken cancellationToken)
             where T : AbstractAcquireTokenParameterBuilder<T>
         {
-            return async
+            Microsoft.Identity.Client.AuthenticationResult result = async
                 ? await builder.ExecuteAsync(cancellationToken).ConfigureAwait(false)
 #pragma warning disable AZC0102 // Do not use GetAwaiter().GetResult(). Use the TaskExtensions.EnsureCompleted() extension method instead.
                 : builder.ExecuteAsync(cancellationToken).GetAwaiter().GetResult();
 #pragma warning restore AZC0102 // Do not use GetAwaiter().GetResult(). Use the TaskExtensions.EnsureCompleted() extension method instead.
+
+            return result;
         }
     }
 }
