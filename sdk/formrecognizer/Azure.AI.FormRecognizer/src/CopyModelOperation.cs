7963af21808b1fd0b133ac72ddf966a3ffe23092
Anne Loomis Thompson <annelo@microsoft.com>
2020-05-04 19:52:47 -0700

    copy api - missed file

---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/CopyModelOperation.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace Azure.AI.FormRecognizer.Training
+{
+    /// <summary>
+    /// </summary>
+    public class CopyModelOperation : Operation<CustomFormModelInfo>
+    {
+        /// <summary>Provides communication with the Form Recognizer Azure Cognitive Service through its REST API.</summary>
+        private readonly ServiceClient _serviceClient;
+
+        /// <summary>The last HTTP response received from the server. <c>null</c> until the first response is received.</summary>
+        private Response _response;
+
+        /// <summary>The result of the long-running operation. <c>null</c> until result is received on status update.</summary>
+        private CustomFormModelInfo _value;
+
+        /// <summary><c>true</c> if the long-running operation has completed. Otherwise, <c>false</c>.</summary>
+        private bool _hasCompleted;
+
+        /// <inheritdoc/>
+        public override string Id { get; }
+
+        /// <inheritdoc/>
+        public override CustomFormModelInfo Value => OperationHelpers.GetValue(ref _value);
+
+        /// <inheritdoc/>
+        public override bool HasCompleted => _hasCompleted;
+
+        /// <inheritdoc/>
+        public override bool HasValue => _value != null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CopyModelOperation"/> class.
+        /// </summary>
+        /// <param name="operationId">The ID of this operation.</param>
+        /// <param name="client">The client used to check for completion.</param>
+        public CopyModelOperation(string operationId, FormRecognizerClient client)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CopyModelOperation"/> class.
+        /// </summary>
+        /// <param name="serviceClient">The client for communicating with the Form Recognizer Azure Cognitive Service through its REST API.</param>
+        /// <param name="operationLocation">The address of the long-running operation. It can be obtained from the response headers upon starting the operation.</param>
+        internal CopyModelOperation(ServiceClient serviceClient, string operationLocation)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CopyModelOperation"/> class.
+        /// </summary>
+        protected CopyModelOperation()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override Response GetRawResponse() => _response;
+
+        /// <inheritdoc/>
+        public override ValueTask<Response<CustomFormModelInfo>> WaitForCompletionAsync(CancellationToken cancellationToken = default) =>
+            this.DefaultWaitForCompletionAsync(cancellationToken);
+
+        /// <inheritdoc/>
+        public override ValueTask<Response<CustomFormModelInfo>> WaitForCompletionAsync(TimeSpan pollingInterval, CancellationToken cancellationToken = default) =>
+            this.DefaultWaitForCompletionAsync(pollingInterval, cancellationToken);
+
+        /// <inheritdoc/>
+        public override Response UpdateStatus(CancellationToken cancellationToken = default) =>
+            UpdateStatusAsync(false, cancellationToken).EnsureCompleted();
+
+        /// <inheritdoc/>
+        public override async ValueTask<Response> UpdateStatusAsync(CancellationToken cancellationToken = default) =>
+            await UpdateStatusAsync(true, cancellationToken).ConfigureAwait(false);
+
+        /// <summary>
+        /// Calls the server to get updated status of the long-running operation.
+        /// </summary>
+        /// <param name="async">When <c>true</c>, the method will be executed asynchronously; otherwise, it will execute synchronously.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> controlling the request lifetime.</param>
+        /// <returns>The HTTP response from the service.</returns>
+        private async ValueTask<Response> UpdateStatusAsync(bool async, CancellationToken cancellationToken)
+        {
+#pragma warning disable AZC0110 // DO NOT use await keyword in possibly synchronous scope.
+            await Task.Run(() => { }).ConfigureAwait(false);
+#pragma warning restore AZC0110 // DO NOT use await keyword in possibly synchronous scope.
+            throw new NotImplementedException();
+        }
+    }
+}
