786e398e0e2b7ca1f08cc06847e60035b7839a27
SDK Automation <sdkautomation@microsoft.com>
2020-05-06 01:36:50 +0000

    Generated from c0e334513038787686fe5db7729455ac0a8360f5
    update examples for pipeline run

--- sdk/containerregistry/Microsoft.Azure.Management.ContainerRegistry/src/Generated/Models/PublicNetworkAccess.cs
+++ sdk/containerregistry/Microsoft.Azure.Management.ContainerRegistry/src/Generated/Models/PublicNetworkAccess.cs
@@ -12,9 +12,9 @@ namespace Microsoft.Azure.Management.ContainerRegistry.Models
 {
 
     /// <summary>
-    /// Defines values for TriggerStatus.
+    /// Defines values for PublicNetworkAccess.
     /// </summary>
-    public static class TriggerStatus
+    public static class PublicNetworkAccess
     {
         public const string Enabled = "Enabled";
         public const string Disabled = "Disabled";
