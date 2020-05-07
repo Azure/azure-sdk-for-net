9a252c4e93d87d4ad23502cd355cb8896b9981f8
Amanda Nguyen <48961492+amnguye@users.noreply.github.com>
2020-05-07 15:24:55 -0700

    Object Replication (Download Response and Get Blob Properties) (#11909)
    * Adding Object Replication for Download Response and Blob Properties

--- sdk/storage/Azure.Storage.Blobs/src/BlobExtensions.cs
+++ sdk/storage/Azure.Storage.Blobs/src/BlobExtensions.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Linq;
 using System.Text;
 using Azure.Storage.Blobs.Models;
 using Tags = System.Collections.Generic.IDictionary<string, string>;
@@ -97,7 +98,7 @@ namespace Azure.Storage.Blobs
         /// Creates a new BlobProperties object backed by BlobPropertiesInternal.
         /// </summary>
         /// <param name="properties">
-        /// The BlobPropertiesInternal returned with the reques
+        /// The BlobPropertiesInternal returned with the request.
         /// </param>
         internal static BlobProperties ToBlobProperties(this BlobPropertiesInternal properties) =>
             new BlobProperties()
@@ -106,6 +107,7 @@ namespace Azure.Storage.Blobs
                 CreatedOn = properties.CreatedOn,
                 Metadata = properties.Metadata,
                 ObjectReplicationDestinationPolicy = properties.ObjectReplicationPolicyId,
+                ObjectReplicationSourceProperties = BlobExtensions.ParseObjectReplicationIds(properties.ObjectReplicationRules),
                 BlobType = properties.BlobType,
                 CopyCompletedOn = properties.CopyCompletedOn,
                 CopyStatusDescription = properties.CopyStatusDescription,
@@ -142,5 +144,56 @@ namespace Azure.Storage.Blobs
                 ExpiresOn = properties.ExpiresOn,
                 IsSealed = properties.IsSealed,
             };
+
+        /// <summary>
+        /// Internal. Parses Object Replication Policy ID from Rule ID and sets the Policy ID for source blobs.
+        /// </summary>
+        /// <param name="OrIds">
+        /// Unparsed Object Replication headers.
+        /// For source blobs, the dictionary will contain keys that contain the policy id and rule id separated
+        /// by a underscore (e.g. policyId_ruleId). The value of these keys will be the replication status (e.g. Complete, Failed).
+        /// For destination blobs, the dictionary will contain one entry where the key will be "policy-id"
+        /// and the value will be the destination policy id. No parsing will be required for this.
+        /// </param>
+        /// <returns>
+        /// If the blob has object replication policy(s) applied and is the source blob, this method will return a
+        /// dictionary of policy Ids, with a dictionary of rules and replication status for each policy
+        /// (As each policy id, could have multiple rule ids).
+        /// If the blob has object replication policy applied and is the destination blob,
+        /// this method will return default as the policy id should be set in ObjectReplicationDestinationPolicy
+        /// (e.g. <see cref="BlobProperties.ObjectReplicationDestinationPolicy"/>,<see cref="BlobDownloadDetails.ObjectReplicationDestinationPolicy"/>).
+        /// Otherwise null will be returned.
+        /// </returns>
+        internal static IDictionary<string, IDictionary<string, string>> ParseObjectReplicationIds(this IDictionary<string, string> OrIds)
+        {
+            if (OrIds == null)
+            {
+                return null;
+            }
+            // If the dictionary is empty or it contains a key with policy id, we are not required to do any parsing since
+            // the policy id should already be stored in the ObjectReplicationDestinationPolicy.
+            if (OrIds.Count == 0 ||
+                (OrIds.Count > 0 &&
+                (OrIds.First().Key == "policy-id")))
+            {
+                return default;
+            }
+            IDictionary<string, IDictionary<string, string>> OrProperties = new Dictionary<string, IDictionary<string, string>>();
+            foreach (KeyValuePair<string, string> status in OrIds)
+            {
+                string[] ParsedIds = status.Key.Split('_');
+                if (OrProperties.ContainsKey(ParsedIds[0]))
+                {
+                    OrProperties[ParsedIds[0]].Add(ParsedIds[1], status.Value);
+                }
+                else
+                {
+                    IDictionary<string, string> NewRuleStatus = new Dictionary<string, string>();
+                    NewRuleStatus.Add(ParsedIds[1], status.Value);
+                    OrProperties.Add(ParsedIds[0], NewRuleStatus);
+                }
+            }
+            return OrProperties;
+        }
     }
 }
