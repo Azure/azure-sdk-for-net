cb73e3432702d8e00b53f9e9d3fa4d148ca75149
SDK Automation <sdkautomation@microsoft.com>
2020-05-07 01:38:02 +0000

    Generated from de1a45d872b24219a1319329aaf3a21a46c98b96
    merge master

--- sdk/operationalinsights/Microsoft.Azure.Management.OperationalInsights/src/Generated/SdkInfo_OperationalInsightsManagementClient.cs
+++ sdk/operationalinsights/Microsoft.Azure.Management.OperationalInsights/src/Generated/SdkInfo_OperationalInsightsManagementClient.cs
@@ -39,16 +39,5 @@ namespace Microsoft.Azure.Management.OperationalInsights
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "2.0.4413";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/operationalinsights/resource-manager/readme.md --csharp --version=2.0.4413 --reflect-api-versions --csharp.output-folder=C:\\Users\\yabhu\\Documents\\workspace\\azure-sdk-for-net\\sdk\\operationalinsights\\Microsoft.Azure.Management.OperationalInsights\\src\\Generated";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "c3bb4114dbe040ae207922958f29a6724ccb5a50";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
