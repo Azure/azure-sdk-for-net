b7661e66dd55ac0725ae6a1d8ae58ac9a2dbe8cf
Pavel Krymets <pavel@krymets.com>
2020-05-13 10:30:18 -0700

    Sources

--- sdk/core/Azure.Core/src/Shared/AutoRest/ManagementPipelineBuilder.cs
+++ sdk/core/Azure.Core/src/Shared/AutoRest/ManagementPipelineBuilder.cs
@@ -14,9 +14,9 @@ namespace Azure.Core
 {
     internal static class ManagementPipelineBuilder
     {
-        public static HttpPipeline Build(TokenCredential credential, ClientOptions options)
+        public static HttpPipeline Build(TokenCredential credential, string host, ClientOptions options)
         {
-            return HttpPipelineBuilder.Build(options, new BearerTokenAuthenticationPolicy(credential, "https://management.azure.com//.default"));
+            return HttpPipelineBuilder.Build(options, new BearerTokenAuthenticationPolicy(credential, $"{host}/.default"));
         }
     }
 }
