8b528dd5cfd2d535509a4e9def8f034eb3d88432
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-05-14 16:15:38 -0700

    flaky test

--- sdk/servicebus/Azure.Messaging.ServiceBus/tests/Processor/SessionProcessorLiveTests.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/tests/Processor/SessionProcessorLiveTests.cs
@@ -659,7 +659,9 @@ namespace Azure.Messaging.ServiceBus.Tests.Processor
                     {
                         var message = args.Message;
                         var lockedUntil = args.SessionLockedUntil;
-                        await Task.Delay(lockDuration.Add(TimeSpan.FromSeconds(1)));
+                        // wait 2x lock duration in case the
+                        // lock was renewed already
+                        await Task.Delay(lockDuration.Add(lockDuration));
                         if (!args.CancellationToken.IsCancellationRequested)
                         {
                             // only do the assertion if cancellation wasn't requested as otherwise
