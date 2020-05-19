fe452cc671c83a2e8e84e8dacdd41ec9ab90ba5c
Xiangyu Luo <xiangyul@microsoft.com>
2020-05-18 23:44:43 -0700

    Complete Remote Rendering Account Data Contract

--- sdk/mixedreality/Microsoft.Azure.Management.MixedReality/src/Generated/SdkInfo_MixedRealityClient.cs
+++ sdk/mixedreality/Microsoft.Azure.Management.MixedReality/src/Generated/SdkInfo_MixedRealityClient.cs
@@ -32,7 +32,7 @@ namespace Microsoft.Azure.Management.MixedReality
       public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/mixedreality/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=D:\\Git\\GitHub\\my\\azure-sdk-for-net\\sdk";
       public static readonly String GithubForkName = "Azure";
       public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "fc0ea12e0a984b4e8fecaf25ff2332f50b19908d";
+      public static readonly String GithubCommidId = "fd1212399155ecb17199b24434d780a42a6e5ec6";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
