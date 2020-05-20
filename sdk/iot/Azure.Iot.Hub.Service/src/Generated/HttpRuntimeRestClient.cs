c7cde3a940e70e68d39ec012ef75b3e2f546908a
timtay-microsoft <timtay@microsoft.com>
2020-05-20 13:44:31 -0700

    fix(iot): Regenerate iothub PL after rebase from master

--- sdk/iot/Azure.Iot.Hub.Service/src/Generated/HttpRuntimeRestClient.cs
+++ sdk/iot/Azure.Iot.Hub.Service/src/Generated/HttpRuntimeRestClient.cs
@@ -16,24 +16,26 @@ namespace Azure.Iot.Hub.Service
 {
     internal partial class HttpRuntimeRestClient
     {
-        private string host;
+        private Uri endpoint;
         private string apiVersion;
         private ClientDiagnostics _clientDiagnostics;
         private HttpPipeline _pipeline;
 
         /// <summary> Initializes a new instance of HttpRuntimeRestClient. </summary>
-        public HttpRuntimeRestClient(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "https://fully-qualified-iothubname.azure-devices.net", string apiVersion = "2020-03-13")
+        /// <param name="clientDiagnostics"> The handler for diagnostic messaging in the client. </param>
+        /// <param name="pipeline"> The HTTP pipeline for sending and receiving REST requests and responses. </param>
+        /// <param name="endpoint"> server parameter. </param>
+        /// <param name="apiVersion"> Api Version. </param>
+        /// <exception cref="ArgumentNullException"> This occurs when one of the required arguments is null. </exception>
+        public HttpRuntimeRestClient(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Uri endpoint = null, string apiVersion = "2020-03-13")
         {
-            if (host == null)
-            {
-                throw new ArgumentNullException(nameof(host));
-            }
+            endpoint ??= new Uri("https://fully-qualified-iothubname.azure-devices.net");
             if (apiVersion == null)
             {
                 throw new ArgumentNullException(nameof(apiVersion));
             }
 
-            this.host = host;
+            this.endpoint = endpoint;
             this.apiVersion = apiVersion;
             _clientDiagnostics = clientDiagnostics;
             _pipeline = pipeline;
@@ -45,7 +47,7 @@ namespace Azure.Iot.Hub.Service
             var request = message.Request;
             request.Method = RequestMethod.Get;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(host, false);
+            uri.Reset(endpoint);
             uri.AppendPath("/messages/serviceBound/feedback", false);
             uri.AppendQuery("api-version", apiVersion, true);
             request.Uri = uri;
@@ -90,7 +92,7 @@ namespace Azure.Iot.Hub.Service
             var request = message.Request;
             request.Method = RequestMethod.Delete;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(host, false);
+            uri.Reset(endpoint);
             uri.AppendPath("/messages/serviceBound/feedback/", false);
             uri.AppendPath(lockToken, true);
             uri.AppendQuery("api-version", apiVersion, true);
@@ -146,7 +148,7 @@ namespace Azure.Iot.Hub.Service
             var request = message.Request;
             request.Method = RequestMethod.Post;
             var uri = new RawRequestUriBuilder();
-            uri.AppendRaw(host, false);
+            uri.Reset(endpoint);
             uri.AppendPath("/messages/serviceBound/feedback/", false);
             uri.AppendPath(lockToken, true);
             uri.AppendPath("/abandon", false);
