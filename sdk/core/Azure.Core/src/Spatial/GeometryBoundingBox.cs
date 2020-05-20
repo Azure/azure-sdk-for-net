4944ea1f2b9b16076f063b7bf07bf88e61867d41
Pavel Krymets <pavel@krymets.com>
2020-05-20 14:32:18 -0700

    Operators.

--- sdk/core/Azure.Core/src/Spatial/GeometryBoundingBox.cs
+++ sdk/core/Azure.Core/src/Spatial/GeometryBoundingBox.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+
 namespace Azure.Core.Spatial
 {
     /// <summary>
     /// Represents information about the coordinate range of the <see cref="Geometry"/>.
     /// </summary>
-    public readonly struct GeometryBoundingBox
+    public readonly struct GeometryBoundingBox : IEquatable<GeometryBoundingBox>
     {
         /// <summary>
         /// The westmost value of <see cref="Geometry"/> coordinates.
@@ -56,6 +58,29 @@ namespace Azure.Core.Spatial
             North = north;
             MinAltitude = minAltitude;
             MaxAltitude = maxAltitude;
+        }
+
+        /// <inheritdoc />
+        public bool Equals(GeometryBoundingBox other)
+        {
+            return West.Equals(other.West) &&
+                   South.Equals(other.South) &&
+                   East.Equals(other.East) &&
+                   North.Equals(other.North) &&
+                   Nullable.Equals(MinAltitude, other.MinAltitude) &&
+                   Nullable.Equals(MaxAltitude, other.MaxAltitude);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj)
+        {
+            return obj is GeometryBoundingBox other && Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return HashCodeBuilder.Combine(West, South, East, North, MinAltitude, MaxAltitude);
         }
     }
 }
