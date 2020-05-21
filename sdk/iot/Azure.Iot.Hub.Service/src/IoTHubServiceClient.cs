6e0c3a6a1b6785fb2453beb0182ab576b3cc4d7f
bikamani <41314966+bikamani@users.noreply.github.com>
2020-05-21 14:29:54 -0700

    initial changes

---
+++ sdk/iot/Azure.Iot.Hub.Service/src/IoTHubServiceClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Iot.Hub.Service
+{
+    /// <summary>
+    /// The IoT Hub Service Client
+    /// </summary>
+    public class IoTHubServiceClient
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Devices Devices;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Modules Modules;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Statistics Statistics;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Messages Messages;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Files Files;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IoTHubServiceClient"/> class.
+        /// </summary>
+        public IoTHubServiceClient(Uri endpoint, IoTHubServiceClientOptions options)
+        {
+            FaultInjectionClient = new FaultInjectionClient();
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IoTHubServiceClient"/> class.
+        /// </summary>
+        protected IoTHubServiceClient()
+        {
+
+        }
+    }
+}
---
+++ sdk/iot/Azure.Iot.Hub.Service/src/Modules.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Iot.Hub.Service
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Modules
+    {
+    }
+}
