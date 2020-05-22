b6fc0a11463d54c43bf3e00600444e59004e0036
Pavel Krymets <pavel@krymets.com>
2020-05-22 13:43:52 -0700

    Add DynamicJson type

---
+++ sdk/core/Azure.Core.Experimental/src/DynamicJson.cs
@@ -0,0 +1,226 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+namespace Azure.Core
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class DynamicJson : IDynamicMetaObjectProvider
+    {
+        private readonly JsonElement _element;
+
+        private DynamicJson(JsonElement element)
+        {
+            _element = element;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static DynamicJson Parse(string json)
+        {
+            return Create(JsonDocument.Parse(json).RootElement);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public static DynamicJson Create(JsonElement element)
+        {
+            return new DynamicJson(element);
+        }
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        public JsonElement AsJsonElement()
+        {
+            return _element;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            using var memoryStream = new MemoryStream();
+            using (var writer = new Utf8JsonWriter(memoryStream))
+            {
+                _element.WriteTo(writer);
+            }
+            return Encoding.UTF8.GetString(memoryStream.ToArray());
+        }
+
+        /// <inheritdoc />
+        DynamicMetaObject IDynamicMetaObjectProvider.GetMetaObject(Expression parameter) => new MetaObject(parameter, this);
+
+        private object GetValue(string propertyName)
+        {
+            if (propertyName == "Length" && _element.ValueKind == JsonValueKind.Array)
+            {
+                return _element.GetArrayLength();
+            }
+
+            if (_element.TryGetProperty(propertyName, out JsonElement element))
+            {
+                return new DynamicJson(element);
+            }
+
+            throw new InvalidOperationException($"Property {propertyName} not found");
+        }
+
+        private object GetValueAt(int index)
+        {
+            return new DynamicJson(_element[index]);
+        }
+
+        private object? ConvertTo(Type type)
+        {
+            Debug.Assert(type != null);
+
+            if (type == typeof(IEnumerable))
+            {
+                return new Enumerable(this);
+            }
+
+            if (type == typeof(long))
+            {
+                return _element.GetInt64();
+            }
+            if (type == typeof(int))
+            {
+                return _element.GetInt32();
+            }
+            if (type == typeof(bool))
+            {
+                return _element.GetBoolean();
+            }
+
+            if (_element.ValueKind == JsonValueKind.Null)
+            {
+                return null;
+            }
+
+            if (type == typeof(string))
+            {
+                return _element.GetString();
+            }
+
+            throw new InvalidOperationException($"Unknown type {type}");
+        }
+
+        private class MetaObject : DynamicMetaObject
+        {
+            internal MetaObject(Expression parameter, IDynamicMetaObjectProvider value) : base(parameter, BindingRestrictions.Empty, value)
+            { }
+
+
+            public override DynamicMetaObject BindGetMember(GetMemberBinder binder)
+            {
+                var targetObject = Expression.Convert(Expression, LimitType);
+                var methodIplementation = typeof(DynamicJson).GetMethod(nameof(GetValue), BindingFlags.NonPublic | BindingFlags.Instance);
+                var arguments = new Expression[] { Expression.Constant(binder.Name) };
+
+                var getPropertyCall = Expression.Call(targetObject, methodIplementation, arguments);
+                var restrictions = BindingRestrictions.GetTypeRestriction(Expression, LimitType);
+                DynamicMetaObject getProperty = new DynamicMetaObject(getPropertyCall, restrictions);
+                return getProperty;
+            }
+
+            public override DynamicMetaObject BindConvert(ConvertBinder binder)
+            {
+                var sourceInstance = Expression.Convert(Expression, LimitType);
+                var destinationType = binder.Type;
+                var destinationTypeExpression = Expression.Constant(destinationType);
+
+                var restrictions = BindingRestrictions.GetTypeRestriction(Expression, LimitType);
+
+                var methodIplementation = typeof(DynamicJson).GetMethod(nameof(ConvertTo), BindingFlags.NonPublic | BindingFlags.Instance);
+                Expression expression = Expression.Call(sourceInstance, methodIplementation, new Expression[] { destinationTypeExpression });
+                expression = Expression.Convert(expression, binder.Type);
+                return new DynamicMetaObject(expression, restrictions);
+            }
+
+            public override DynamicMetaObject BindGetIndex(GetIndexBinder binder, DynamicMetaObject[] indexes)
+            {
+                if (indexes.Length != 1) throw new InvalidOperationException();
+                var index = indexes[0].Expression;
+
+                var targetObject = Expression.Convert(Expression, LimitType);
+                var methodIplementation = typeof(DynamicJson).GetMethod(nameof(GetValueAt), BindingFlags.NonPublic | BindingFlags.Instance);
+                var arguments = new[] { index };
+
+                var getPropertyCall = Expression.Call(targetObject, methodIplementation, arguments);
+                var restrictions = binder.FallbackGetIndex(this, indexes).Restrictions; // TODO: all these restrictions are a hack. Tthey need to be cleaned up.
+                DynamicMetaObject getProperty = new DynamicMetaObject(getPropertyCall, restrictions);
+                return getProperty;
+            }
+
+        }
+
+        private class Enumerable: IEnumerable<DynamicJson>
+        {
+            private readonly DynamicJson _dynamicJson;
+
+            public Enumerable(DynamicJson dynamicJson)
+            {
+                _dynamicJson = dynamicJson;
+            }
+
+            IEnumerator<DynamicJson> IEnumerable<DynamicJson>.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerator? GetEnumerator()
+            {
+                return new Enumerator(_dynamicJson);
+            }
+        }
+
+        private class Enumerator: IEnumerator<DynamicJson>
+        {
+            private readonly DynamicJson _dynamicJson;
+            private int _index = -1;
+
+            public Enumerator(DynamicJson dynamicJson)
+            {
+                _dynamicJson = dynamicJson;
+            }
+
+            public bool MoveNext()
+            {
+                _index++;
+                return _index < _dynamicJson._element.GetArrayLength();
+            }
+
+            public void Reset()
+            {
+                _index = -1;
+            }
+
+            object IEnumerator.Current => Current;
+
+            public DynamicJson Current => new DynamicJson(_dynamicJson._element[_index]);
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+
+}
---
+++ sdk/core/Azure.Core.Experimental/tests/DynamicJsonTest.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Azure.Core.Tests
+{
+    public class DynamicJsonTest
+    {
+        [Test]
+        public void CanCreateFromJson()
+        {
+            var dynamicJson = DynamicJson.Parse("\"string\"");
+
+            Assert.AreEqual("\"string\"", dynamicJson.ToString());
+        }
+
+        [Test]
+        public void DynamicCanConvertToString() => Assert.AreEqual("string", JsonAsType<string>("\"string\""));
+
+        [Test]
+        public void DynamicCanConvertToInt() => Assert.AreEqual(5, JsonAsType<int>("5"));
+
+        [Test]
+        public void DynamicCanConvertToLong() => Assert.AreEqual(5L, JsonAsType<long>("5"));
+
+        [Test]
+        public void DynamicCanConvertToBool() => Assert.AreEqual(true, JsonAsType<bool>("true"));
+
+        [Test]
+        public void DynamicCanConvertToNullAsString() => Assert.AreEqual(null, JsonAsType<string>("null"));
+
+        [Ignore("nope")]
+        [Test]
+        public void DynamicCanConvertToNullAsNullableInt() => Assert.AreEqual(null, JsonAsType<int?>("null"));
+
+        [Ignore("nope")]
+        [Test]
+        public void DynamicCanConvertToNullAsNullableLong() => Assert.AreEqual(null, JsonAsType<long?>("null"));
+
+        [Ignore("nope")]
+        [Test]
+        public void DynamicCanConvertToNullAsNullableBool() => Assert.AreEqual(null, JsonAsType<bool?>("null"));
+
+        [Test]
+        public void DynamicCanConvertToIEnumerableDynamic()
+        {
+            dynamic dynamicJson = DynamicJson.Parse("[1, null, \"s\"]");
+            int i = 0;
+            foreach (var dynamicItem in dynamicJson)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Assert.AreEqual(1, (int)dynamicItem);
+                        break;
+                    case 1:
+                        Assert.AreEqual(null, (string)dynamicItem);
+                        break;
+                    case 2:
+                        Assert.AreEqual("s", (string)dynamicItem);
+                        break;
+                    default:
+                        Assert.Fail();
+                        break;
+                }
+
+                i++;
+            }
+            Assert.AreEqual(3, i);
+        }
+
+        [Test]
+        public void DynamicCanConvertToIEnumerableInt()
+        {
+            dynamic dynamicJson = DynamicJson.Parse("[0, 1, 2, 3]");
+            int i = 0;
+            foreach (int dynamicItem in dynamicJson)
+            {
+                Assert.AreEqual(i, dynamicItem);
+
+                i++;
+            }
+            Assert.AreEqual(4, i);
+        }
+
+        [Test]
+        public void DynamicArrayHasLength()
+        {
+            dynamic dynamicJson = DynamicJson.Parse("[0, 1, 2, 3]");
+            Assert.AreEqual(4, dynamicJson.Length);
+        }
+
+        [Test]
+        public void DynamicArrayFor()
+        {
+            dynamic dynamicJson = DynamicJson.Parse("[0, 1, 2, 3]");
+            for (int i = 0; i < dynamicJson.Length; i++)
+            {
+                Assert.AreEqual(i, (int)dynamicJson[i]);
+            }
+        }
+
+        [Test]
+        public void CanAccessProperties()
+        {
+            dynamic dynamicJson = DynamicJson.Parse("{ \"primitive\":\"Hello\", \"nested\": { \"nestedPrimitive\":true } }");
+
+            Assert.AreEqual("Hello", (string)dynamicJson.primitive);
+            Assert.AreEqual(true, (bool)dynamicJson.nested.nestedPrimitive);
+        }
+
+        private T JsonAsType<T>(string json)
+        {
+            dynamic dynamicJson = DynamicJson.Parse(json);
+            return (T) dynamicJson;
+        }
+    }
+}
