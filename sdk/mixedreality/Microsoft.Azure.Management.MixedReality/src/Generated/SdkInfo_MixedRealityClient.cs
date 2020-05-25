cd7e8c29f655d25114148150ddbbc26f25c146e2
SDK Automation <sdkautomation@microsoft.com>
2020-05-25 05:58:24 +0000

    Generated from 32e275e150a32c8007a929937fd717819807f4a9
    feat: update java readme

--- sdk/mixedreality/Microsoft.Azure.Management.MixedReality/src/Generated/SdkInfo_MixedRealityClient.cs
+++ sdk/mixedreality/Microsoft.Azure.Management.MixedReality/src/Generated/SdkInfo_MixedRealityClient.cs
@@ -26,16 +26,5 @@ namespace Microsoft.Azure.Management.MixedReality
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/mixedreality/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=D:\\Git\\GitHub\\my\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "fd1212399155ecb17199b24434d780a42a6e5ec6";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
