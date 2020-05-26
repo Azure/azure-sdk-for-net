4ee4db8ec091d56d8706a1dbf35cb50aaf79908a
Dongwei Wang <bean0idear@163.com>
2020-05-26 03:48:13 -0700

    Add file header for code snippet

--- sdk/synapse/Azure.Analytics.Synapse.Development/tests/samples/NotebookSampleSnippets.cs
+++ sdk/synapse/Azure.Analytics.Synapse.Development/tests/samples/NotebookSampleSnippets.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using Azure.Analytics.Synapse.Samples;
 using Azure.Identity;
 using NUnit.Framework;
--- sdk/synapse/Azure.Analytics.Synapse.Spark/tests/samples/SampleSnippets.cs
+++ sdk/synapse/Azure.Analytics.Synapse.Spark/tests/samples/SampleSnippets.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using System.Linq;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using Azure.Analytics.Synapse.Spark.Models;
 using Azure.Analytics.Synapse.Samples;
 using Azure.Identity;
