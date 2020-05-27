48a96749e374ab02f79e8475331d5e2a12b62f3a
Heath Stewart <heaths@microsoft.com>
2020-05-26 17:14:21 -0700

    Collapse several Search models
    Fixes #12159

---
+++ sdk/search/Azure.Search.Documents/src/Indexes/Models/LuceneStandardTokenizer.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.Search.Documents.Indexes.Models
+{
+    [CodeGenModel("LuceneStandardTokenizerV2")]
+    public partial class LuceneStandardTokenizer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LuceneStandardTokenizer"/> class.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the tokenizer. It must only contain letters, digits, spaces, dashes or underscores,
+        /// can only start and end with alphanumeric characters, and is limited to 128 characters.
+        /// </param>
+        public LuceneStandardTokenizer(string name) : base(name)
+        {
+            Argument.AssertNotNull(name, nameof(name));
+
+            ODataType = "#Microsoft.Azure.Search.StandardTokenizerV2";
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum token length.
+        /// okens longer than the maximum length are split.
+        /// The maximum token length that can be used is 300 characters.
+        /// </summary>
+        public int? MaxTokenLength { get; set; }
+    }
+}
---
+++ sdk/search/Azure.Search.Documents/src/Indexes/Models/NGramTokenFilter.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.Search.Documents.Indexes.Models
+{
+    [CodeGenModel("NGramTokenFilterV2")]
+    public partial class NGramTokenFilter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NGramTokenFilter"/> class.
+        /// </summary>
+        /// <param name="name">
+        /// The name of the token filter. It must only contain letters, digits, spaces, dashes or underscores,
+        /// can only start and end with alphanumeric characters, and is limited to 128 characters.
+        /// </param>
+        public NGramTokenFilter(string name) : base(name)
+        {
+            Argument.AssertNotNull(name, nameof(name));
+
+            ODataType = "#Microsoft.Azure.Search.NGramTokenFilterV2";
+        }
+
+        /// <summary>
+        /// Gets or sets the minimum n-gram length.
+        /// Maximum is 300.
+        /// Must be less than the value of maxGram.
+        /// </summary>
+        public int? MinGram { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum n-gram length.
+        /// Maximum is 300.
+        /// </summary>
+        public int? MaxGram { get; set; }
+    }
+}
