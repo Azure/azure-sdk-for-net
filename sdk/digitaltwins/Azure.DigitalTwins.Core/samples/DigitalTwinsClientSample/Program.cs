468cd8ae621bc8d6cbd88f324ece581063053e24
David R. Williamson <drwill@microsoft.com>
2020-05-30 12:11:15 -0700

    Add digital twins service client library

---
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/samples/DigitalTwinsClientSample/Program.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.Core.Pipeline;
+using Azure.DigitalTwins.Samples;
+using Azure.Identity;
+using CommandLine;
+
+namespace Azure.DigitalTwins.Core.Samples
+{
+    public class Program
+    {
+        /// <summary>
+        /// Main entry point to the sample.
+        /// </summary>
+        public static async Task Main(string[] args)
+        {
+            Options options = null;
+            ParserResult<Options> result = Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(parsedOptions => { options = parsedOptions; })
+                .WithNotParsed(errors => Options.HandleParseError(errors));
+
+            var httpClient = new HttpClient();
+            DigitalTwinsClient dtClient = GetDigitalTwinsClient(
+                options.TenantId,
+                options.ClientId,
+                options.ClientSecret,
+                options.AdtEndpoint,
+                httpClient);
+
+            var dtLifecycleSamples = new DigitalTwinsLifecycleSamples(dtClient, options.EventHubName);
+            await dtLifecycleSamples.RunSamplesAsync().ConfigureAwait(false);
+
+            var modelLifecycleSamples = new ModelLifecycleSamples(dtClient);
+            await modelLifecycleSamples.RunSamplesAsync().ConfigureAwait(false);
+
+            var componentSamples = new ComponentSamples(dtClient);
+            await componentSamples.RunSamplesAsync().ConfigureAwait(false);
+
+            httpClient.Dispose();
+        }
+
+        /// <summary>
+        /// Illustrates how to construct a <see cref="DigitalTwinsClient"/> with the fewest required parameters,
+        /// using the <see cref="ClientSecretCredential"/> implementation of <see cref="Azure.Core.TokenCredential"/>.
+        /// </summary>
+        /// <param name="tenantId">The Id of the tenant of the application Id.</param>
+        /// <param name="clientId">The application Id.</param>
+        /// <param name="clientSecret">A client secret for the application Id.</param>
+        /// <param name="adtEndpoint">The endpoint of the digital twins instance.</param>
+        private static DigitalTwinsClient GetDigitalTwinsClient(string tenantId, string clientId, string clientSecret, string adtEndpoint)
+        {
+            #region Snippet:DigitalTwinSampleCreateServiceClient
+
+            var clientSecretCredential = new ClientSecretCredential(
+                tenantId,
+                clientId,
+                clientSecret,
+                new TokenCredentialOptions { AuthorityHost = KnownAuthorityHosts.AzureCloud });
+
+            var dtClient = new DigitalTwinsClient(
+                new Uri(adtEndpoint),
+                clientSecretCredential);
+
+            #endregion Snippet:DigitalTwinSampleCreateServiceClient
+
+            return dtClient;
+        }
+
+        /// <summary>
+        /// Illustrates how to construct a <see cref="DigitalTwinsClient"/> including client options,
+        /// using the <see cref="ClientSecretCredential"/> implementation of <see cref="Azure.Core.TokenCredential"/>.
+        /// </summary>
+        /// <param name="tenantId">The Id of the tenant of the application Id.</param>
+        /// <param name="clientId">The application Id.</param>
+        /// <param name="clientSecret">A client secret for the application Id.</param>
+        /// <param name="adtEndpoint">The endpoint of the digital twins instance.</param>
+        /// <param name="httpClient">An HttpClient instance for the client to use</param>
+        private static DigitalTwinsClient GetDigitalTwinsClient(string tenantId, string clientId, string clientSecret, string adtEndpoint, HttpClient httpClient)
+        {
+            #region Snippet:DigitalTwinSampleCreateServiceClientWithHttpClient
+
+            // This illustrates how to specify client options, in this case, by providing an
+            // instance of HttpClient for the digital twins client to use
+
+            var clientOptions = new DigitalTwinsClientOptions
+            {
+                Transport = new HttpClientTransport(httpClient),
+            };
+
+            var clientSecretCredential = new ClientSecretCredential(
+                tenantId,
+                clientId,
+                clientSecret,
+                new TokenCredentialOptions { AuthorityHost = KnownAuthorityHosts.AzureCloud });
+
+            var dtClient = new DigitalTwinsClient(
+                new Uri(adtEndpoint),
+                clientSecretCredential,
+                clientOptions);
+
+            #endregion Snippet:DigitalTwinSampleCreateServiceClientWithHttpClient
+
+            return dtClient;
+        }
+    }
+}
---
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/src/Serialization/BasicDigitalTwin.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Azure.DigitalTwins.Core.Serialization
+{
+    /// <summary>
+    /// An optional, helper class for deserializing a digital twin.
+    /// </summary>
+    public class BasicDigitalTwin
+    {
+        /// <summary>
+        /// The unique Id of the digital twin in a digital twins instance. This field is present on every digital twin.
+        /// </summary>
+        [JsonPropertyName("$dtId")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Information about the model a digital twin conforms to. This field is present on every digital twin.
+        /// </summary>
+        [JsonPropertyName("$metadata")]
+        public DigitalTwinMetadata Metadata { get; set; }
+
+        /// <summary>
+        /// Additional properties of the digital twin. This field will contain any properties of the digital twin that are not already defined by the other strong types of this class.
+        /// </summary>
+        [JsonExtensionData]
+        public IDictionary<string, object> CustomProperties { get; set; } = new Dictionary<string, object>();
+    }
+}
