3d7694f6711a59fe1753bfdb1c51a49f15f9e2bf
Azad Abbasi <azabbasi@microsoft.com>
2020-06-01 14:21:23 -0700

    Initial commit to master for Azure Digital Twins .Net SDK. (#12398)

---
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/src/Serialization/DigitalTwinMetadata.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Azure.DigitalTwins.Core.Serialization
+{
+    /// <summary>
+    /// An optional, helper class for deserializing a digital twin.
+    /// The $metadata class on a <see cref="BasicDigitalTwin" />.
+    /// </summary>
+    public class DigitalTwinMetadata
+    {
+        /// <summary>
+        /// The Id of the model that this digital twin is modeled by.
+        /// </summary>
+        [JsonPropertyName("$model")]
+        public string ModelId { get; set; }
+
+        /// <summary>
+        /// Additional, model-defined properties.
+        /// </summary>
+        [JsonExtensionData]
+        public IDictionary<string, object> ModelProperties { get; set; } = new Dictionary<string, object>();
+    }
+}
