5c2be9accc78fd1762da63d00ae94b2d7cec6223
Anirban Sarkar <ansar@microsoft.com>
2020-06-01 02:00:43 -0700

    Regenarated the SDKs
    Regenarated the SDKs

--- sdk/billing/Microsoft.Azure.Management.Billing/src/Generated/SdkInfo_BillingManagementClient.cs
+++ sdk/billing/Microsoft.Azure.Management.Billing/src/Generated/SdkInfo_BillingManagementClient.cs
@@ -46,7 +46,7 @@ namespace Microsoft.Azure.Management.Billing
       public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/billing/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\Users\\ansar\\Documents\\GitHub\\azure-sdk-for-net\\sdk";
       public static readonly String GithubForkName = "Azure";
       public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "429e2937619f7cf14561904dd781080c2b6d9a83";
+      public static readonly String GithubCommidId = "e83bb3392a13e1bb0b2c2cbdf4597d0a0edde49d";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
