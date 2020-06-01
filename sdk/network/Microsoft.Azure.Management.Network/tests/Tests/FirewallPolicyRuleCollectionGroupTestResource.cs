60c3d11123e629e3ef9dd4d756440c22b0ce0bfb
gimotwanMSFT <36463144+gimotwanMSFT@users.noreply.github.com>
2020-06-01 02:16:54 -0700

    fix sdk breaking change with firewall policy rule collection group rename (#12381)

--- sdk/network/Microsoft.Azure.Management.Network/tests/Tests/FirewallPolicyRuleCollectionGroupTestResource.cs
+++ sdk/network/Microsoft.Azure.Management.Network/tests/Tests/FirewallPolicyRuleCollectionGroupTestResource.cs
@@ -6,11 +6,11 @@ namespace Networks.Tests
     using System.IO;
     using Newtonsoft.Json.Linq;
 
-    internal static class FirewallPolicyRuleGroupTestResource
+    internal static class FirewallPolicyRuleCollectionGroupTestResource
     {
         internal static JObject GetSampleResource()
         {
-            var sampleResourceJson = File.ReadAllText("TestData/FirewallPolicyRuleGroupSampleResource.json");
+            var sampleResourceJson = File.ReadAllText("TestData/FirewallPolicyRuleCollectionGroupSampleResource.json");
             var json = sampleResourceJson.Replace("'", "\"");
             var resource = JObject.Parse(json).ToObject<JObject>();
 
