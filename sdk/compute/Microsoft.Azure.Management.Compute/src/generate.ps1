549dac87e2bc0e9e17a6355a6729c090c9c97874
Dongwook Chung <dochung@microsoft.com>
2020-06-03 17:12:35 -0700

    Revert the change in generate.ps1

--- sdk/compute/Microsoft.Azure.Management.Compute/src/generate.ps1
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "compute/resource-manager" -AutoRestVersion "v2" -SpecsRepoFork "dochung4" -SpecsRepoBranch "compute-2020-06"
+Start-AutoRestCodeGeneration -ResourceProvider "compute/resource-manager" -AutoRestVersion "v2"
