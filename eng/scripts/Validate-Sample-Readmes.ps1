9019e4fa86959298f8a7da2dd7dd11b4ca48cc1d
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-06-05 15:37:15 -0700

    Support asterisk passed in

--- eng/scripts/Validate-Sample-Readmes.ps1
+++ eng/scripts/Validate-Sample-Readmes.ps1
@@ -7,17 +7,14 @@ param (
 
 $root = "$PSScriptRoot/../../sdk/$ServiceDirectory"
 
-foreach ($projectDirName in Get-ChildItem -Directory $root -Filter "Azure.*")
-{
+foreach ($projectDirName in Get-ChildItem -Recurse -Directory $root -Filter "Azure.*"){
     $sampleReadmePath = $root + '/' + $projectDirName + '/samples/README.md';
     if (Test-Path $sampleReadmePath) {
         $content = Get-Content -Path $sampleReadmePath
-        $matches = $content | Select-String -Pattern ".md\)"
-        foreach ($match in $matches)
-        {
+        $matches = $content | Select-String -Pattern "(.md|.cs)\)"
+        foreach ($match in $matches){
             $relLink = $match | Select-String -NotMatch "\(http"
-            if ($relLink)
-            {
+            if ($relLink){
                 LogError "Absolute links to GitHub should be used in the samples readme file: $sampleReadmePath, link: $relLink"
             }
         }
