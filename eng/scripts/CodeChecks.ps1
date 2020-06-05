ee93c120acf083cac7633b71d18e749d393add28
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-06-05 16:19:34 -0700

    move inline

--- eng/scripts/CodeChecks.ps1
+++ eng/scripts/CodeChecks.ps1
@@ -63,8 +63,19 @@ try {
         }
 
     Write-Host "Validating sample readmes"
-    Invoke-Block {
-        & $PSScriptRoot\Validate-Sample-readmes.ps1 @script:PSBoundParameters
+    $root = "$PSScriptRoot/../../sdk/$ServiceDirectory"
+    foreach ($projectDirName in Get-ChildItem -Recurse -Directory $root -Filter "Azure.*"){
+        $sampleReadmePath = $root + '/' + $projectDirName + '/samples/README.md';
+        if (Test-Path $sampleReadmePath) {
+            $content = Get-Content -Path $sampleReadmePath
+            $matches = $content | Select-String -Pattern "(.md|.cs)\)"
+            foreach ($match in $matches){
+                $relLink = $match | Select-String -NotMatch "\(http"
+                if ($relLink){
+                    LogError "Absolute links to GitHub should be used in the samples readme file: $sampleReadmePath, link: $relLink"
+                }
+            }
+        }
     }
 
     Write-Host "Re-generating readmes"
