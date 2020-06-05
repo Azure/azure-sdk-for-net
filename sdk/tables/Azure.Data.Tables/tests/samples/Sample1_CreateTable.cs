0e1e79dc261cf876a4d77812685fa2992de4a129
Allison Kim <t-allkim@microsoft.com>
2020-06-05 16:10:11 -0700

    Sample template finished with create + delete table working

--- sdk/tables/Azure.Data.Tables/tests/samples/Sample1_CreateTable.cs
+++ sdk/tables/Azure.Data.Tables/tests/samples/Sample1_CreateTable.cs
@@ -9,14 +9,14 @@ using Azure.Data.Tables.Tests;
 namespace Azure.Data.Tables.Samples
 {
     [LiveOnly]
-    public partial class TablesSamples : SamplesBase<TablesTestEnvironment>
+    public partial class TablesSamples : TablesTestEnvironment
     {
         [Test]
         public void CreateTable()
         {
-            string storageUri = TestEnvironment.StorageUri;
-            string accountName = TestEnvironment.AccountName;
-            string storageAccountKey = TestEnvironment.PrimaryStorageAccountKey;
+            string storageUri = StorageUri;
+            string accountName = AccountName;
+            string storageAccountKey = PrimaryStorageAccountKey;
             string tableName = "mytesttable";
 
             #region Snippet:TablesSample1CreateClient
@@ -30,6 +30,7 @@ namespace Azure.Data.Tables.Samples
             #region Snippet:CreateTable
 
             serviceClient.CreateTable(tableName);
+            var client = serviceClient.GetTableClient(tableName);
 
             #endregion
 
