1062174be68a4d45ad864fa921f9fe19643f8752
Azad Abbasi <azabbasi@microsoft.com>
2020-06-05 16:35:52 -0700

    Update IfMatch name and reference Etag object documentation

--- sdk/digitaltwins/Azure.DigitalTwins.Core/src/RequestOptions.cs
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/src/RequestOptions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using static System.Net.WebRequestMethods;
+
 namespace Azure.DigitalTwins.Core
 {
     /// <summary>
@@ -9,13 +11,16 @@ namespace Azure.DigitalTwins.Core
     public class RequestOptions
     {
         /// <summary>
-        /// An Azure.Core.Etag object for the entity that this request performs an operation against, as per RFC7232. The request's operation is performed
+        /// An Etag object for the entity that this request performs an operation against, as per RFC7232. The request's operation is performed
         /// only if this ETag matches the value maintained by the server, indicating that the entity has not been modified since it was last retrieved.
         /// To force the operation to execute only if the entity exists, set the ETag to the wildcard character '*'. <code>new Etag("*")</code>. To force the operation to execute unconditionally, leave this value null.
         /// </summary>
+        /// <see href="https://docs.microsoft.com/en-us/dotnet/api/azure.etag?view=azure-dotnet">
+        /// See the documentation for Etag.
+        /// </see>
         /// <remarks>
         /// If this value is not set, it defaults to null, and the ifMatch header will not be sent with the request. This means that update and delete will be unconditional and the operation will execute regardless of the existence of the resource.
         /// </remarks>
-        public ETag? IfMatchEtag { get; set; }
+        public ETag? IfMatch { get; set; }
     }
 }
