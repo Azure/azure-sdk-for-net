2cc30bb083b50f5e353661d5c27cae60e37024d9
Basel Rustum <barustum@microsoft.com>
2020-06-05 22:46:46 -0700

    export adt apis

--- sdk/digitaltwins/Azure.DigitalTwins.Core/api/Azure.DigitalTwins.Core.netstandard2.0.cs
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/api/Azure.DigitalTwins.Core.netstandard2.0.cs
@@ -122,13 +122,17 @@ namespace Azure.DigitalTwins.Core.Queries
 }
 namespace Azure.DigitalTwins.Core.Serialization
 {
-    public partial class BasicDigitalTwin : Azure.DigitalTwins.Core.Serialization.ModelProperties
+    public partial class BasicDigitalTwin
     {
         public BasicDigitalTwin() { }
+        [System.Text.Json.Serialization.JsonExtensionDataAttribute]
+        public System.Collections.Generic.IDictionary<string, object> CustomProperties { get { throw null; } set { } }
         [System.Text.Json.Serialization.JsonPropertyNameAttribute("$etag")]
         public string ETag { get { throw null; } set { } }
         [System.Text.Json.Serialization.JsonPropertyNameAttribute("$dtId")]
         public string Id { get { throw null; } set { } }
+        [System.Text.Json.Serialization.JsonPropertyNameAttribute("$metadata")]
+        public Azure.DigitalTwins.Core.Serialization.DigitalTwinMetadata Metadata { get { throw null; } set { } }
     }
     public partial class BasicRelationship
     {
@@ -144,6 +148,12 @@ namespace Azure.DigitalTwins.Core.Serialization
         [System.Text.Json.Serialization.JsonPropertyNameAttribute("$targetId")]
         public string TargetId { get { throw null; } set { } }
     }
+    public partial class ComponentMetadata
+    {
+        public ComponentMetadata() { }
+        [System.Text.Json.Serialization.JsonExtensionDataAttribute]
+        public System.Collections.Generic.IDictionary<string, object> WriteableProperties { get { throw null; } }
+    }
     public partial class DigitalTwinMetadata
     {
         public DigitalTwinMetadata() { }
@@ -158,7 +168,7 @@ namespace Azure.DigitalTwins.Core.Serialization
         [System.Text.Json.Serialization.JsonExtensionDataAttribute]
         public System.Collections.Generic.IDictionary<string, object> CustomProperties { get { throw null; } }
         [System.Text.Json.Serialization.JsonPropertyNameAttribute("$metadata")]
-        public Azure.DigitalTwins.Core.Serialization.DigitalTwinMetadata Metadata { get { throw null; } }
+        public Azure.DigitalTwins.Core.Serialization.ComponentMetadata Metadata { get { throw null; } }
     }
     public partial class UpdateOperationsUtility
     {
