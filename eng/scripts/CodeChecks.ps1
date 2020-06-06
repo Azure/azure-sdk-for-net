59e7809e1c40e149b467da0341da5c434c469c0c
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-06-06 13:41:24 -0700

    Revert codechecks changes; update Search readme

--- eng/scripts/CodeChecks.ps1
+++ eng/scripts/CodeChecks.ps1
@@ -62,22 +62,6 @@ try {
                     }
         }
 
-    Write-Host "Validating sample readmes"
-    $root = "$PSScriptRoot/../../sdk/$ServiceDirectory"
-    foreach ($projectDirName in Get-ChildItem -Recurse -Directory $root -Filter "Azure.*"){
-        $sampleReadmePath = "$projectDirName/samples/README.md"
-        if (Test-Path $sampleReadmePath) {
-            $content = Get-Content -Path $sampleReadmePath
-            $matches = $content | Select-String -Pattern "(.md|.cs)\)"
-            foreach ($match in $matches){
-                $relLink = $match | Select-String -NotMatch "\(http"
-                if ($relLink){
-                    LogError "Absolute links to GitHub should be used in the samples readme file: $sampleReadmePath, link: $relLink"
-                }
-            }
-        }
-    }
-
     Write-Host "Re-generating readmes"
     Invoke-Block {
         & $PSScriptRoot\Update-Snippets.ps1 @script:PSBoundParameters
