74dcdd6c425cea2a407f77ec24d066aca8f61377
Sean McCullough <seanmcc@microsoft.com>
2020-06-08 20:51:17 -0500

    Got AppendBlobWriteStream to function correctly

--- sdk/storage/Azure.Storage.Blobs/src/AppendBlobWriteStream.cs
+++ sdk/storage/Azure.Storage.Blobs/src/AppendBlobWriteStream.cs
@@ -6,74 +6,122 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core.Pipeline;
+using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Shared;
 
 namespace Azure.Storage.Blobs
 {
-    internal class AppendBlobWriteStream : Stream
+    internal class AppendBlobWriteStream : WriteStream
     {
         private readonly AppendBlobClient _appendBlobClient;
-        private readonly byte[] _buffer;
-        private int _bufferCount;
+        private readonly AppendBlobRequestConditions _conditions;
+        private readonly IProgress<long> _progressHandler;
+        private readonly MemoryStream _buffer;
 
         public AppendBlobWriteStream(
             AppendBlobClient appendBlobClient,
-            int bufferSize)
+            int bufferSize,
+            long position,
+            AppendBlobRequestConditions conditions = default,
+            IProgress<long> progressHandler = default) : base(position)
         {
             ValidateBufferSize(bufferSize);
             _appendBlobClient = appendBlobClient;
-            _buffer = new byte[bufferSize];
-            _bufferCount = 0;
+            _conditions = conditions;
+            _progressHandler = progressHandler;
+            _buffer = new MemoryStream(bufferSize);
         }
 
-        public override bool CanRead => false;
-
-        public override bool CanSeek => false;
-
-        public override bool CanWrite => true;
-
-        //TODO
-        public override long Length => throw new NotImplementedException();
-
-        //TODO
-        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
-
-        //TODO
-        public override void Flush()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public override int Read(byte[] buffer, int offset, int count)
-            => throw new NotSupportedException();
-
-        public override long Seek(long offset, SeekOrigin origin)
-            => throw new NotSupportedException();
-
-        public override void SetLength(long value)
-            => throw new NotSupportedException();
-
-        public override void Write(byte[] buffer, int offset, int count)
-            => WriteInternal(
-                async: false,
-                buffer,
-                offset,
-                count,
-                cancellationToken: default)
-            .EnsureCompleted();
-
-        private async Task<int> WriteInternal(
+        protected override async Task WriteInternal(
             bool async,
             byte[] buffer,
             int offset,
             int count,
             CancellationToken cancellationToken)
         {
+            ValidateWriteParameters(buffer, offset, count);
+            int remaining = count;
 
+            // New bytes will fit in the buffer.
+            if (count <= _buffer.Capacity - _buffer.Position)
+            {
+                await WriteToBuffer(async, buffer, offset, count, cancellationToken).ConfigureAwait(false);
+            }
+            else
+            {
+                // Flush the buffer.
+                await FlushInternal(async, cancellationToken).ConfigureAwait(false);
+
+                while (remaining > 0)
+                {
+                    await WriteToBuffer(async, buffer, offset, Math.Min(remaining, _buffer.Capacity), cancellationToken).ConfigureAwait(false);
+
+                    // Renaming bytes won't fit in buffer.
+                    if (remaining > _buffer.Capacity)
+                    {
+                        await FlushInternal(async, cancellationToken).ConfigureAwait(false);
+                        remaining -= _buffer.Capacity;
+                        offset += _buffer.Capacity;
+                    }
+
+                    // Remaining bytes will fit in buffer.
+                    else
+                    {
+                        remaining = 0;
+                    }
+                }
+            }
+            _position += count;
         }
 
-        private void ValidateBufferSize(int bufferSize)
+        protected override async Task FlushInternal(
+            bool async,
+            CancellationToken cancellationToken)
+        {
+            if (_buffer.Length > 0)
+            {
+                _buffer.Position = 0;
+
+                if (async)
+                {
+                    await _appendBlobClient.AppendBlockAsync(
+                        _buffer,
+                        conditions: _conditions,
+                        progressHandler: _progressHandler,
+                        cancellationToken: cancellationToken).ConfigureAwait(false);
+                }
+                else
+                {
+                    _appendBlobClient.AppendBlock(
+                        _buffer,
+                        conditions: _conditions,
+                        progressHandler: _progressHandler,
+                        cancellationToken: cancellationToken);
+                }
+            }
+
+            _buffer.SetLength(0);
+        }
+
+        private async Task WriteToBuffer(
+            bool async,
+            byte[] buffer,
+            int offset,
+            int count,
+            CancellationToken cancellationToken)
+        {
+            if (async)
+            {
+                await _buffer.WriteAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
+            }
+            else
+            {
+                _buffer.Write(buffer, offset, count);
+            }
+        }
+
+        private static void ValidateBufferSize(int bufferSize)
         {
             if (bufferSize < 1)
             {
---
+++ sdk/storage/Azure.Storage.Common/src/Shared/WriteStream.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core.Pipeline;
+
+namespace Azure.Storage.Shared
+{
+    internal abstract class WriteStream : Stream
+    {
+        protected long _position;
+
+        protected WriteStream(long position)
+        {
+            _position = position;
+        }
+
+        public override bool CanRead => false;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => throw new NotSupportedException();
+
+        public override long Position { get => _position; set => throw new NotSupportedException(); }
+
+        public override int Read(byte[] buffer, int offset, int count)
+            => throw new NotSupportedException();
+
+        public override long Seek(long offset, SeekOrigin origin)
+            => throw new NotSupportedException();
+
+        public override void SetLength(long value)
+            => throw new NotSupportedException();
+
+        public override void Write(
+            byte[] buffer,
+            int offset,
+            int count)
+            => WriteInternal(
+                async: false,
+                buffer,
+                offset,
+                count,
+                cancellationToken: default)
+            .EnsureCompleted();
+
+        public override async Task WriteAsync(
+            byte[] buffer,
+            int offset,
+            int count,
+            CancellationToken cancellationToken)
+            => await WriteInternal(
+                async: true,
+                buffer,
+                offset,
+                count,
+                cancellationToken)
+            .ConfigureAwait(false);
+
+        protected abstract Task WriteInternal(
+            bool async,
+            byte[] buffer,
+            int offset,
+            int count,
+            CancellationToken cancellationToken);
+
+        public override void Flush()
+            => FlushInternal(
+                async: false,
+                cancellationToken: default).EnsureCompleted();
+
+        public override async Task FlushAsync(CancellationToken cancellationToken)
+            => await FlushInternal(
+                async: true,
+                cancellationToken).ConfigureAwait(false);
+
+        protected abstract Task FlushInternal(bool async, CancellationToken cancellationToken);
+
+        protected static void ValidateWriteParameters(byte[] buffer, int offset, int count)
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException($"{nameof(buffer)}", $"{nameof(buffer)} cannot be null.");
+            }
+
+            if (offset < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} cannot be less than 0.");
+            }
+
+            if (offset > buffer.Length)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} cannot exceed {nameof(buffer)} length.");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(count)} cannot be less than 0.");
+            }
+
+            if (offset + count > buffer.Length)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} + {nameof(count)} cannot exceed {nameof(buffer)} length.");
+            }
+        }
+    }
+}
