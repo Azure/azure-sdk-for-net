6ae03b2098ab16cef2cc77b55b87704b98f5efa9
Hyonho Lee <Hyonho.Lee@microsoft.com>
2020-06-10 12:15:23 -0700

    update of dedicated host group automatic placement

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/VirtualMachine.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/VirtualMachine.cs
@@ -109,6 +109,11 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// <param name="host">Specifies information about the dedicated host
         /// that the virtual machine resides in. &lt;br&gt;&lt;br&gt;Minimum
         /// api-version: 2018-10-01.</param>
+        /// <param name="hostGroup">Specifies information about the dedicated
+        /// host group that the virtual machine resides in.
+        /// &lt;br&gt;&lt;br&gt;Minimum api-version: 2020-06-01.
+        /// &lt;br&gt;&lt;br&gt;NOTE: User cannot specify both host and
+        /// hostGroup properties.</param>
         /// <param name="provisioningState">The provisioning state, which only
         /// appears in the response.</param>
         /// <param name="instanceView">The virtual machine instance
@@ -137,7 +142,7 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// <param name="identity">The identity of the virtual machine, if
         /// configured.</param>
         /// <param name="zones">The virtual machine zones.</param>
-        public VirtualMachine(string location, string id = default(string), string name = default(string), string type = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), Plan plan = default(Plan), HardwareProfile hardwareProfile = default(HardwareProfile), StorageProfile storageProfile = default(StorageProfile), AdditionalCapabilities additionalCapabilities = default(AdditionalCapabilities), OSProfile osProfile = default(OSProfile), NetworkProfile networkProfile = default(NetworkProfile), SecurityProfile securityProfile = default(SecurityProfile), DiagnosticsProfile diagnosticsProfile = default(DiagnosticsProfile), SubResource availabilitySet = default(SubResource), SubResource virtualMachineScaleSet = default(SubResource), SubResource proximityPlacementGroup = default(SubResource), string priority = default(string), string evictionPolicy = default(string), BillingProfile billingProfile = default(BillingProfile), SubResource host = default(SubResource), string provisioningState = default(string), VirtualMachineInstanceView instanceView = default(VirtualMachineInstanceView), string licenseType = default(string), string vmId = default(string), string extensionsTimeBudget = default(string), IList<VirtualMachineExtension> resources = default(IList<VirtualMachineExtension>), VirtualMachineIdentity identity = default(VirtualMachineIdentity), IList<string> zones = default(IList<string>))
+        public VirtualMachine(string location, string id = default(string), string name = default(string), string type = default(string), IDictionary<string, string> tags = default(IDictionary<string, string>), Plan plan = default(Plan), HardwareProfile hardwareProfile = default(HardwareProfile), StorageProfile storageProfile = default(StorageProfile), AdditionalCapabilities additionalCapabilities = default(AdditionalCapabilities), OSProfile osProfile = default(OSProfile), NetworkProfile networkProfile = default(NetworkProfile), SecurityProfile securityProfile = default(SecurityProfile), DiagnosticsProfile diagnosticsProfile = default(DiagnosticsProfile), SubResource availabilitySet = default(SubResource), SubResource virtualMachineScaleSet = default(SubResource), SubResource proximityPlacementGroup = default(SubResource), string priority = default(string), string evictionPolicy = default(string), BillingProfile billingProfile = default(BillingProfile), SubResource host = default(SubResource), SubResource hostGroup = default(SubResource), string provisioningState = default(string), VirtualMachineInstanceView instanceView = default(VirtualMachineInstanceView), string licenseType = default(string), string vmId = default(string), string extensionsTimeBudget = default(string), IList<VirtualMachineExtension> resources = default(IList<VirtualMachineExtension>), VirtualMachineIdentity identity = default(VirtualMachineIdentity), IList<string> zones = default(IList<string>))
             : base(location, id, name, type, tags)
         {
             Plan = plan;
@@ -155,6 +160,7 @@ namespace Microsoft.Azure.Management.Compute.Models
             EvictionPolicy = evictionPolicy;
             BillingProfile = billingProfile;
             Host = host;
+            HostGroup = hostGroup;
             ProvisioningState = provisioningState;
             InstanceView = instanceView;
             LicenseType = licenseType;
@@ -316,6 +322,16 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// </summary>
         [JsonProperty(PropertyName = "properties.host")]
         public SubResource Host { get; set; }
+
+        /// <summary>
+        /// Gets or sets specifies information about the dedicated host group
+        /// that the virtual machine resides in.
+        /// &amp;lt;br&amp;gt;&amp;lt;br&amp;gt;Minimum api-version:
+        /// 2020-06-01. &amp;lt;br&amp;gt;&amp;lt;br&amp;gt;NOTE: User cannot
+        /// specify both host and hostGroup properties.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.hostGroup")]
+        public SubResource HostGroup { get; set; }
 
         /// <summary>
         /// Gets the provisioning state, which only appears in the response.
