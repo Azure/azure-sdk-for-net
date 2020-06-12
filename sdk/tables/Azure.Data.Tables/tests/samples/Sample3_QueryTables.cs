c88934a9250a73633d0a0087eb82b53d66eed982
Allison Kim <t-allkim@microsoft.com>
2020-06-12 15:23:09 -0700

    add query table sample

---
+++ sdk/tables/Azure.Data.Tables/tests/samples/Sample3_QueryTables.cs
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+using Azure.Data.Tables.Tests;
+using Azure.Data.Tables.Models;
+
+namespace Azure.Data.Tables.Samples
+{
+    [LiveOnly]
+    public partial class TablesSamples : TablesTestEnvironment
+    {
+        [Test]
+        public void QueryTables()
+        {
+            string storageUri = StorageUri;
+            string accountName = AccountName;
+            string storageAccountKey = PrimaryStorageAccountKey;
+            string tableName = "OfficeSupplies4";
+            string table2Name = "OfficeSupplies5";
+
+            var serviceClient = new TableServiceClient(
+                new Uri(storageUri),
+                new TableSharedKeyCredential(accountName, storageAccountKey));
+
+            try
+            {
+                serviceClient.CreateTable(tableName);
+                serviceClient.CreateTable(table2Name);
+
+                var queryTableResults = serviceClient.GetTables();
+                Console.WriteLine("The following are the names of the tables in the query results:");
+                foreach (TableItem table in queryTableResults)
+                {
+                    Console.WriteLine(table.TableName);
+                }
+            }
+            finally
+            {
+                serviceClient.DeleteTable(tableName);
+                serviceClient.DeleteTable(table2Name);
+            }
+        }
+    }
+}
