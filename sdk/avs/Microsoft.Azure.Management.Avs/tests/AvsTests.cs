e02644ada6bf399c62c5152e669fd77d6563d53d
Cameron Taggart <cameron.taggart@microsoft.com>
2020-06-12 13:31:36 -0600

    first test and recorded session

--- sdk/avs/Microsoft.Azure.Management.Avs/tests/AvsTests.cs
+++ sdk/avs/Microsoft.Azure.Management.Avs/tests/AvsTests.cs
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Linq;
 using Microsoft.Azure.Management.Avs;
+using Microsoft.Azure.Management.ResourceManager;
+using Microsoft.Azure.Management.ResourceManager.Models;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using Xunit;
-using System.Linq;
 
 namespace Avs.Tests
 {
@@ -13,17 +15,27 @@ namespace Avs.Tests
         [Fact]
         public void AvsCrud()
         {
-            using (var context = MockContext.Start(this.GetType()))
-            {
-                using (var testBase = new AvsTestBase(context))
+            using var context = MockContext.Start(this.GetType());
+            string rgName = TestUtilities.GenerateName("avs-sdk-test-rg");
+            string cloudName = TestUtilities.GenerateName("appplatform-sdk-test-cloud");
+            string location = "centralus";
+
+            CreateResourceGroup(context, location, rgName);
+            using var testBase = new AvsTestBase(context);
+            var client = testBase.AvsClient;
+            var clouds = client.PrivateClouds.List(rgName);
+            Assert.True(clouds.Count() == 0);
+        }
+
+        private ResourceGroup CreateResourceGroup(MockContext context, string location, string rgName)
+        {
+            ResourceManagementClient client = context.GetServiceClient<ResourceManagementClient>();
+            return client.ResourceGroups.CreateOrUpdate(
+                rgName,
+                new ResourceGroup
                 {
-                    var client = testBase.AvsClient;
-                    var clouds = client.PrivateClouds.List("myrg");
-                    Assert.True(clouds.Count() == 0);
-                }
-            }
+                    Location = location
+                });
         }
     }
 }
-
-
