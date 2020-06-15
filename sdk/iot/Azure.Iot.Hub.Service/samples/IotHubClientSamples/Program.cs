4e33820cfeabdc545cd5981d0dd5f2b389f8e5fc
Azad Abbasi <azabbasi@microsoft.com>
2020-06-15 15:28:30 -0700

    Samples skeleton

---
+++ sdk/iot/Azure.Iot.Hub.Service/samples/IotHubClientSamples/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using CommandLine;
+
+namespace Azure.Iot.Hub.Service.Samples
+{
+    public class Program
+    {
+        /// <summary>
+        /// Main entry point to the sample.
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            // Parse and validate paramters
+
+            CommandLineOptions options = null;
+            ParserResult<CommandLineOptions> result = Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed(parsedOptions =>
+                {
+                    options = parsedOptions;
+                })
+                .WithNotParsed(errors =>
+                {
+                    Environment.Exit(1);
+                });
+        }
+    }
+}
