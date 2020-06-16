3c66aeeabbd7b9dd4eeb11059e20f941836b9299
Allison Kim <t-allkim@microsoft.com>
2020-06-15 18:07:14 -0700

    TableServiceProperties get/set methods and test

--- sdk/tables/Azure.Data.Tables/tests/TableServiceClientLiveTests.cs
+++ sdk/tables/Azure.Data.Tables/tests/TableServiceClientLiveTests.cs
@@ -95,5 +95,56 @@ namespace Azure.Data.Tables.Tests
                 }
             }
         }
+
+        [Test]
+        public async Task GetPropertiesReturnsProperties()
+        {
+            IList<CorsRule> corsRules = new List<CorsRule>();
+
+            var tableServicePropertiesToSet = new TableServiceProperties(
+                new LoggingSettings("1.0", true, true, true, new RetentionPolicy(false)), hourMetrics: new TableMetrics("1.0", true, true, new RetentionPolicy(true, 7)), minuteMetrics: new TableMetrics("1.0", false, null, new RetentionPolicy(false)), corsRules);
+
+            await service.SetPropertiesAsync(tableServicePropertiesToSet).ConfigureAwait(false);
+
+            // Get configured properties
+            await Task.Delay(12000);
+            TableServiceProperties response = await service.GetPropertiesAsync().ConfigureAwait(false);
+
+            CompareTableServiceProperties(tableServicePropertiesToSet, response);
+        }
+
+        private void CompareTableServiceProperties(TableServiceProperties expected, TableServiceProperties actual)
+        {
+            Assert.AreEqual(expected.Logging.Read, actual.Logging.Read);
+            Assert.AreEqual(expected.Logging.Version, actual.Logging.Version);
+            Assert.AreEqual(expected.Logging.Write, actual.Logging.Write);
+            Assert.AreEqual(expected.Logging.Delete, actual.Logging.Delete);
+            Assert.AreEqual(expected.Logging.RetentionPolicy.Enabled, actual.Logging.RetentionPolicy.Enabled);
+            Assert.AreEqual(expected.Logging.RetentionPolicy.Days, actual.Logging.RetentionPolicy.Days);
+
+            Assert.AreEqual(expected.HourMetrics.Enabled, actual.HourMetrics.Enabled);
+            Assert.AreEqual(expected.HourMetrics.Version, actual.HourMetrics.Version);
+            Assert.AreEqual(expected.HourMetrics.IncludeAPIs, actual.HourMetrics.IncludeAPIs);
+            Assert.AreEqual(expected.HourMetrics.RetentionPolicy.Enabled, actual.HourMetrics.RetentionPolicy.Enabled);
+            Assert.AreEqual(expected.HourMetrics.RetentionPolicy.Days, actual.HourMetrics.RetentionPolicy.Days);
+
+            Assert.AreEqual(expected.MinuteMetrics.Enabled, actual.MinuteMetrics.Enabled);
+            Assert.AreEqual(expected.MinuteMetrics.Version, actual.MinuteMetrics.Version);
+            Assert.AreEqual(expected.MinuteMetrics.IncludeAPIs, actual.MinuteMetrics.IncludeAPIs);
+            Assert.AreEqual(expected.MinuteMetrics.RetentionPolicy.Enabled, actual.MinuteMetrics.RetentionPolicy.Enabled);
+            Assert.AreEqual(expected.MinuteMetrics.RetentionPolicy.Days, actual.MinuteMetrics.RetentionPolicy.Days);
+
+            Assert.AreEqual(expected.Cors.Count, actual.Cors.Count);
+            for (int i = 0; i < expected.Cors.Count; i++)
+            {
+                CorsRule expectedRule = expected.Cors[i];
+                CorsRule actualRule = actual.Cors[i];
+                Assert.AreEqual(expectedRule.AllowedHeaders, actualRule.AllowedHeaders);
+                Assert.AreEqual(expectedRule.AllowedMethods, actualRule.AllowedMethods);
+                Assert.AreEqual(expectedRule.AllowedOrigins, actualRule.AllowedOrigins);
+                Assert.AreEqual(expectedRule.MaxAgeInSeconds, actualRule.MaxAgeInSeconds);
+                Assert.AreEqual(expectedRule.ExposedHeaders, actualRule.ExposedHeaders);
+            }
+        }
     }
 }
