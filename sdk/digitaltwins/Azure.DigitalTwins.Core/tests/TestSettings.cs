f65b0e0ef600410659304270f0cddb9582501f00
Sindhu Nagesh <vinagesh@microsoft.com>
2020-06-16 09:20:13 -0700

    feat(e2e): Use encrypted file for user settings.

--- sdk/digitaltwins/Azure.DigitalTwins.Core/tests/TestSettings.cs
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/tests/TestSettings.cs
@@ -19,13 +19,6 @@ namespace Azure.DigitalTwins.Core.Tests
 
         // If these environment variables exist in the environment, their values will replace (supersede) config.json values.
 
-        private static readonly string s_adtInstanceEndpointUrlEnv = $"{AdtEnvironmentVariablesPrefix}_ADT_INSTANCE_ENDPOINT_URL";
-        private static readonly string s_applicationTenantIdEnv = $"{AdtEnvironmentVariablesPrefix}_TENANT_ID";
-        private static readonly string s_applicationClientIdEnv = $"{AdtEnvironmentVariablesPrefix}_CLIENT_ID";
-        private static readonly string s_applicationClientSecretEnv = $"{AdtEnvironmentVariablesPrefix}_CLIENT_SECRET";
-        private static readonly string s_digitalTwinHostnameEnv = $"{AdtEnvironmentVariablesPrefix}_URL";
-        private static readonly string s_digitalTwinTestMode = $"AZURE_IOT_TEST_MODE";
-
         public static TestSettings Instance { get; private set; }
 
         public RecordedTestMode TestMode { get; set; }
@@ -34,31 +27,6 @@ namespace Azure.DigitalTwins.Core.Tests
         /// The working directory of the tests.
         /// </summary>
         public string WorkingDirectory { get; private set; }
-
-        /// <summary>
-        /// Host name of the digital twin instance to connect to.
-        /// </summary>
-        public string DigitalTwinsInstanceHostName { get; set; }
-
-        /// <summary>
-        /// Application ID created to authenticate to the digital twin instance.
-        /// </summary>
-        public string ApplicationId { get; set; }
-
-        /// <summary>
-        /// Application registration client secret.
-        /// </summary>
-        public string ClientSecret { get; set; }
-
-        /// <summary>
-        /// Microsoft tenent ID.
-        /// </summary>
-        public string MicrosoftTenantId { get; set; }
-
-        /// <summary>
-        /// The audience of digital twin for which we need to get an access token.
-        /// </summary>
-        public string DigitalTwinsAudience { get; set; }
 
         static TestSettings()
         {
@@ -90,71 +58,6 @@ namespace Azure.DigitalTwins.Core.Tests
 
             Instance = config.Get<TestSettings>();
             Instance.WorkingDirectory = workingDirectory;
-
-            // We will override settings if they can be found in the environment variables.
-            OverrideFromEnvVariables();
-        }
-
-        private static void OverrideFromEnvVariables()
-        {
-            string envAdtEndpoint = Environment.GetEnvironmentVariable(s_adtInstanceEndpointUrlEnv);
-            if (!string.IsNullOrWhiteSpace(envAdtEndpoint))
-            {
-                Instance.DigitalTwinsInstanceHostName = envAdtEndpoint;
-            }
-
-            // Add the following three values to Environment Variables(if not present) as the test framework expects these to be present.
-
-            string envTenantId = Environment.GetEnvironmentVariable(s_applicationTenantIdEnv);
-            if (!string.IsNullOrWhiteSpace(envTenantId))
-            {
-                Instance.MicrosoftTenantId = envTenantId;
-            }
-            else
-            {
-                Environment.SetEnvironmentVariable(s_applicationTenantIdEnv, Instance.MicrosoftTenantId);
-            }
-
-            string envApplicationId = Environment.GetEnvironmentVariable(s_applicationClientIdEnv);
-            if (!string.IsNullOrWhiteSpace(envApplicationId))
-            {
-                Instance.ApplicationId = envApplicationId;
-            }
-            else
-            {
-                Environment.SetEnvironmentVariable(s_applicationClientIdEnv, Instance.ApplicationId);
-            }
-
-            string envClientSecret = Environment.GetEnvironmentVariable(s_applicationClientSecretEnv);
-            if (!string.IsNullOrWhiteSpace(envClientSecret))
-            {
-                Instance.ClientSecret = envClientSecret;
-            }
-            else
-            {
-                Environment.SetEnvironmentVariable(s_applicationClientSecretEnv, Instance.ClientSecret);
-            }
-
-            string digitalTwinHostname = Environment.GetEnvironmentVariable(s_digitalTwinHostnameEnv);
-            if (!string.IsNullOrWhiteSpace(digitalTwinHostname))
-            {
-                Instance.DigitalTwinsInstanceHostName = digitalTwinHostname;
-            }
-            else
-            {
-                Environment.SetEnvironmentVariable(s_digitalTwinHostnameEnv, Instance.DigitalTwinsInstanceHostName);
-            }
-
-            string testMode = Environment.GetEnvironmentVariable(s_digitalTwinTestMode);
-            if (!string.IsNullOrWhiteSpace(testMode))
-            {
-                // Enum.Parse<type>(value) cannot be used in net461 so using the type casting syntax.
-                Instance.TestMode = (RecordedTestMode)Enum.Parse(typeof(RecordedTestMode), testMode);
-            }
-            else
-            {
-                Environment.SetEnvironmentVariable(s_digitalTwinTestMode, Instance.TestMode.ToString());
-            }
         }
     }
 }
