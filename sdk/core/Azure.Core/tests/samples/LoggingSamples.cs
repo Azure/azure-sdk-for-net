1d89a36b596743d77aa315cb708704d77be5e887
Pavel Krymets <pavel@krymets.com>
2020-06-18 14:10:28 -0700

    Add CreateClientRequestIdScope method (#12870)

--- sdk/core/Azure.Core/tests/samples/LoggingSamples.cs
+++ sdk/core/Azure.Core/tests/samples/LoggingSamples.cs
@@ -6,6 +6,7 @@ using System.Diagnostics.Tracing;
 using System.Net.Http;
 using Azure.Core.Diagnostics;
 using Azure.Core.Pipeline;
+using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using NUnit.Framework;
 
@@ -104,6 +105,21 @@ namespace Azure.Core.Samples
             #region Snippet:TraceLogging
             // Setup a listener to monitor logged events.
             using AzureEventSourceListener listener = AzureEventSourceListener.CreateTraceLogger();
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void ClientRequestId()
+        {
+            #region Snippet:ClientRequestId
+            var secretClient = new SecretClient(new Uri("http://example.com"), new DefaultAzureCredential());
+
+            using (HttpPipeline.CreateClientRequestIdScope("<custom-client-request-id>"))
+            {
+                // The HTTP request resulting from the client call would have x-ms-client-request-id value set to <custom-client-request-id>
+                secretClient.GetSecret("<secret-name>");
+            }
             #endregion
         }
     }
