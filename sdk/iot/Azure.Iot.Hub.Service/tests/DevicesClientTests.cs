f9cc8c8167b74f4c0c677872c07e81a9d271254d
Azad Abbasi <azabbasi@microsoft.com>
2020-06-24 14:48:19 -0700

    Initial commit

--- sdk/iot/Azure.Iot.Hub.Service/tests/DevicesClientTests.cs
+++ sdk/iot/Azure.Iot.Hub.Service/tests/DevicesClientTests.cs
@@ -2,7 +2,8 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Net;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Azure.Iot.Hub.Service.Models;
 using FluentAssertions;
@@ -17,50 +18,119 @@ namespace Azure.Iot.Hub.Service.Tests
         {
         }
 
+        /// <summary>
+        /// Test basic lifecycle of a Device Identity.
+        /// This test includes CRUD operations only.
+        /// </summary>
         [Test]
-        public async Task Devices_Lifecycle()
+        public async Task Devices_IdentityLifecycle()
         {
-            // TODO: This is just a verification that tests run and it requires the tester to complete this test however they see fit.
-            string testDeviceName = $"testDevice{GetRandom()}";
+            string testDeviceName = $"IdentityLifecycleDevice{GetRandom()}";
 
             DeviceIdentity device = null;
 
             IoTHubServiceClient client = GetClient();
+
+            // Wrap all the operations in a try block to be able to cleanup in case of any failure.
             try
             {
-                try
-                {
-                    device = (await client.Devices.GetIdentityAsync(testDeviceName).ConfigureAwait(false)).Value;
-                }
-                catch (RequestFailedException ex) when (ex.Status == (int)HttpStatusCode.NotFound)
-                {
-                    // If the Device doesn't exist, we will create it.
-                    device = (await client.Devices.CreateOrUpdateIdentityAsync(
-                        new Models.DeviceIdentity
-                        {
-                            DeviceId = testDeviceName
-                        }).ConfigureAwait(false)).Value;
-                }
+                // CREATE A DEVICE
+                Response<DeviceIdentity> createResponse = await client.Devices.CreateOrUpdateIdentityAsync(
+                    new Models.DeviceIdentity
+                    {
+                        DeviceId = testDeviceName
+                    }).ConfigureAwait(false);
 
-                // Perform another GET. We expect the device to exist.
-                Response<Models.DeviceIdentity> response = await client.Devices.GetIdentityAsync(testDeviceName).ConfigureAwait(false);
-                device = response.Value;
-                response.GetRawResponse().Status.Should().Be(200);
+                device = createResponse.Value;
+
+                // GET DEVICE
+                // Get the device and compare ETag values (should remain unchanged);
+                Response<DeviceIdentity> getResponse = await client.Devices.GetIdentityAsync(testDeviceName).ConfigureAwait(false);
+
+                getResponse.Value.Etag.Should().BeEquivalentTo(device.Etag, "ETag value should not have changed.");
+
+                device = getResponse.Value;
+
+                // UPDATE DEVICE
+                device.Status = DeviceStatus.Disabled;
+
+                // TODO: (azabbasi) We should leave the IfMatchPrecondition to be the default value once we know more about the fix.
+                Response<DeviceIdentity> updateResponse = await client.Devices.CreateOrUpdateIdentityAsync(device, IfMatchPrecondition.UnconditionalIfMatch).ConfigureAwait(false);
+
+                updateResponse.Value.Status.Should().Be(DeviceStatus.Disabled, "Device should have been disabled");
+
+                // DELETE DEVICE
+                // Deleting the device happens in the finally block as cleanup.
             }
             finally
             {
-                // cleanup
-                try
-                {
-                    if (device != null)
+                await Cleanup(client, device);
+            }
+        }
+
+        /// <summary>
+        /// Test basic operations of a Device Twin.
+        /// </summary>
+        [Test]
+        public async Task Devices_TwinLifecycle()
+        {
+            string testDeviceName = $"TwinLifecycleDevice{GetRandom()}";
+
+            DeviceIdentity device = null;
+
+            IoTHubServiceClient client = GetClient();
+
+            // Wrap all the operations in a try block to be able to cleanup in case of any failure.
+            try
+            {
+                // CREATE A DEVICE
+                // Creating a device also creates a twin for the device.
+                Response<DeviceIdentity> createResponse = await client.Devices.CreateOrUpdateIdentityAsync(
+                    new Models.DeviceIdentity
                     {
-                        await client.Devices.DeleteIdentityAsync(device, IfMatchPrecondition.UnconditionalIfMatch).ConfigureAwait(false);
-                    }
-                }
-                catch (Exception ex)
+                        DeviceId = testDeviceName
+                    }).ConfigureAwait(false);
+
+                device = createResponse.Value;
+
+                // GET DEVICE TWIN
+                Response<TwinData> getResponse = await client.Devices.GetTwinAsync(testDeviceName).ConfigureAwait(false);
+                TwinData deviceTwin = getResponse.Value;
+
+                deviceTwin.DeviceId.Should().BeEquivalentTo(testDeviceName, "DeviceId on the Twin should match that of the device.");
+
+                // UPDATE DEVICE TWIN
+                string propName = "username";
+                string propValue = "userA";
+                deviceTwin.Properties.Desired.Add(new KeyValuePair<string, object>(propName, propValue));
+
+                // TODO: (azabbasi) We should leave the IfMatchPrecondition to be the default value once we know more about the fix.
+                Response<TwinData> updateResponse = await client.Devices.UpdateTwinAsync(deviceTwin, IfMatchPrecondition.UnconditionalIfMatch).ConfigureAwait(false);
+
+                updateResponse.Value.Properties.Desired.Where(p => p.Key == propName).First().Value.Should().Be(propValue, "Desired property value is incorrect.");
+
+                // DELETE DEVICE
+                // Deleting the device happens in the finally block as cleanup.
+            }
+            finally
+            {
+                await Cleanup(client, device);
+            }
+        }
+
+        private async Task Cleanup(IoTHubServiceClient client, DeviceIdentity device)
+        {
+            // cleanup
+            try
+            {
+                if (device != null)
                 {
-                    Assert.Fail($"Test clean up failed: {ex.Message}");
+                    await client.Devices.DeleteIdentityAsync(device, IfMatchPrecondition.UnconditionalIfMatch).ConfigureAwait(false);
                 }
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Test clean up failed: {ex.Message}");
             }
         }
     }
