ac9117bf61fe0587e87f96e6729cd9faaf0c310d
Luyunmt <2534523917@qq.com>
2020-06-29 11:11:19 +0800

    add environment resourcemanager

--- sdk/eventhub/Azure.Messaging.EventHubs.Processor/tests/Processor/EventProcessorClientLiveTests.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs.Processor/tests/Processor/EventProcessorClientLiveTests.cs
@@ -40,7 +40,7 @@ namespace Azure.Messaging.EventHubs.Tests
         public async Task EventsCanBeReadByOneProcessorClient()
         {
             // Setup the environment.
-
+            Assert.AreEqual("aaaaaaaaaaa", EventHubsTestEnvironment.Instance.SubscriptionId);
             await using EventHubScope scope = await EventHubScope.CreateAsync(2);
             var connectionString = EventHubsTestEnvironment.Instance.BuildConnectionStringForEventHub(scope.EventHubName);
 
