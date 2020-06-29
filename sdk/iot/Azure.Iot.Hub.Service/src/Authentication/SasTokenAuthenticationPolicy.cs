99f68bc29b6c2e7ec8356339b821afec5158ee40
Azad Abbasi <azabbasi@microsoft.com>
2020-06-29 14:29:31 -0700

    Merge IoT-Hub into master. (#13086)
    * feat(hub): Creating a new feature branch with swagger and generated files
    
    * fix(doc): Fix markdown for API design doc (#11690)
    
    * swagger(iothub): Adding overrides for type names (#12026)
    
    * fix(tests): Fix project reference for the test framework (#12053)
    
    * fix(hub): Fix property accessibility issue (#12055)
    
    * Fix API categories for iothub service client (#12087)
    
    * swagger(iothub): Swagger comment changes (#12149)
    
    * fix(iot): Regenerate iothub PL after rebase from master
    
    * refactor(iot): Remove unnecessary custom code
    
    This class only existed to make the DigitalTwinClient internal, but now all generated clients are internal by default
    
    * Swagger changes for Iot Hub (#12218)
    
    * Revert swagger back to what is currently deployed
    
    This swagger should never be hand edited. We can update it only when service accepts the changes
    
    * Add composite swagger file with all suggested changes for service to make
    
    read only, required params, and comment refactors. OperationId changes will go in here, too
    
    * Regenerate PL with the currently deployed swagger
    
    * Update models to rename CloudToDeviceMethod and CloudToDeviceMethodResult  (#12240)
    
    * Modules API design (#12188)
    
    * Add IoTHub Devices subclient APIs
    
    * Swagger changes for Client grouping (#12245)
    
    * Add suggested type name changes to iothub swagger (#12296)
    
    * Service Client CL and client grouping (#12323)
    
    * Small API design comments fix
    
    * feat(autorest): Generated clients from autorest after sync with master
    
    * Add implementation for Devices APIs (#12611)
    
    * (feat): Implement Modules client (#12673)
    
    * feat(tests): Add test infrastructure and setup.ps1 for local setup (#12719)
    
    * Add test infrastructure and setup
    
    * Add common files, remove specific sub (#12722)
    
    * fix(swagger): Fix IotHub swagger descriptions (#12695)
    
    * fix(pipeline): Update setup script to call test-resources ARM template directly (#12775)
    
    * feat(samples): Samples project skeleton (#12787)
    
    * IoT hub service client authentication via connection string (#12731)
    
    * feat(e2e-tests): Add initial setup for E2E tests
    
    * feat(iot-service): Add authentication via connection string
    
    * fix(iot-service): Fix merge conflict in infrastructure setup file (#12803)
    
    Co-authored-by: Abhipsa Misra <abhipsa.misra@microsoft.com>
    
    * feat(tests): Changes to fix tests and make sure we can run them successfully. (#12819)
    
    * Start recording tests and add intial Session recording (#12827)
    
    * feat(samples): Initial CREATE/DELETE sample for ModuleI (#12850)
    
    * feat(samples): Finish Modules samples (#12989)
    
    * feat(e2e): Devices E2E tests (#12997)
    
    * Update the logic for ETags and preconditions (#13046)
    
    * Fix the CI and test pipelines. (#13091)
    
    Co-authored-by: abhipds <misra.abhi14@gmail.com>
    Co-authored-by: Abhipsa Misra <22563986+abhipsaMisra@users.noreply.github.com>
    Co-authored-by: vinagesh <vinagesh@outlook.com>
    Co-authored-by: timtay-microsoft <timtay@microsoft.com>
    Co-authored-by: bikamani <41314966+bikamani@users.noreply.github.com>
    Co-authored-by: Sindhu Nagesh <vinagesh@microsoft.com>
    Co-authored-by: Abhipsa Misra <abhipsa.misra@microsoft.com>

---
+++ sdk/iot/Azure.Iot.Hub.Service/src/Authentication/SasTokenAuthenticationPolicy.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace Azure.Iot.Hub.Service.Authentication
+{
+    /// <summary>
+    /// The shared access signature based HTTP pipeline policy.
+    /// This authentication policy injects the sas token into the HTTP authentication header for each HTTP request made.
+    /// </summary>
+    internal class SasTokenAuthenticationPolicy : HttpPipelinePolicy
+    {
+        private readonly ISasTokenProvider _sasTokenProvider;
+
+        internal SasTokenAuthenticationPolicy(ISasTokenProvider sasTokenProvider)
+        {
+            _sasTokenProvider = sasTokenProvider;
+        }
+
+        public override void Process(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            AddHeaders(message);
+            ProcessNext(message, pipeline);
+        }
+
+        public override async ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            AddHeaders(message);
+            await ProcessNextAsync(message, pipeline).ConfigureAwait(false);
+        }
+
+        private void AddHeaders(HttpMessage message)
+        {
+            message.Request.Headers.Add(HttpHeader.Names.Authorization, _sasTokenProvider.GetSasToken());
+        }
+    }
+}
---
+++ sdk/iot/Azure.Iot.Hub.Service/tests/IotHubServiceTestEnvironment.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.Iot.Hub.Service.Tests
+{
+    // This class contains the configurations required to be set to run tests against the CI pipeline.
+    public class IotHubServiceTestEnvironment : TestEnvironment
+    {
+        public IotHubServiceTestEnvironment()
+            : base(TestsConstants.IOT_HUB_ENV_VARIABLE_PREFIX.ToLower())
+        {
+        }
+
+        public string IotHubConnectionString => GetRecordedVariable(TestsConstants.IOT_HUB_CONNECTION_STRING);
+    }
+}
