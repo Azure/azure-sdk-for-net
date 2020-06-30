0710abe1633cf3267479cf4122ee186f8733fbb9
SDK Automation <sdkautomation@microsoft.com>
2020-06-30 03:25:04 +0000

    Generated from 9eece7213c25d18ad24db77f4f148837c7d069ab
    Add api-version 2020-03-13 of search for go SDK

--- sdk/search/Microsoft.Azure.Management.Search/src/Generated/SdkInfo_SearchManagementClient.cs
+++ sdk/search/Microsoft.Azure.Management.Search/src/Generated/SdkInfo_SearchManagementClient.cs
@@ -29,16 +29,5 @@ namespace Microsoft.Azure.Management.Search
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/search/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=F:\\code\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "1280dd676549a0631330945508bcee05ee93c800";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
