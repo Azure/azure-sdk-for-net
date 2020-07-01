6cfee9f5d6ff6cacdd73afd7eeeb40f5480fcd01
Chidozie Ononiwu <31145988+chidozieononiwu@users.noreply.github.com>
2020-07-01 15:33:54 -0700

    Enable ChangeLog verification (#13130)
    * enable changelog verification on the pipeline
    
    * Enable Local ChangeLog Verification

--- eng/common/scripts/Verify-ChangeLog.ps1
+++ eng/common/scripts/Verify-ChangeLog.ps1
@@ -1,8 +1,8 @@
 # Wrapper Script for ChangeLog Verification
 param (
-    [Parameter(Mandatory=$true)]
+    [String]$ChangeLogLocation,
+    [String]$VersionString,
     [string]$PackageName,
-    [Parameter(Mandatory=$true)]
     [string]$ServiceName,
     [string]$RepoRoot,
     [ValidateSet("net","java","js","python")]
@@ -11,12 +11,22 @@ param (
     [boolean]$ForRelease=$False
 )
 
-Import-Module "${PSScriptRoot}/modules/common-manifest.psd1"
+. (Join-Path $PSScriptRoot SemVer.ps1)
+Import-Module (Join-Path $PSScriptRoot modules ChangeLog-Operations.psm1)
 
-if ([System.String]::IsNullOrEmpty($Language))
+if ((Test-Path $ChangeLogLocation) -and -not([System.String]::IsNullOrEmpty($VersionString)))
 {
-    $Language = $RepoName.Substring($RepoName.LastIndexOf('-') + 1)
+    Confirm-ChangeLogEntry -ChangeLogLocation $ChangeLogLocation -VersionString $VersionString -ForRelease $ForRelease
+}
+else 
+{
+    Import-Module (Join-Path $PSScriptRoot modules Package-Properties.psm1)
+    if ([System.String]::IsNullOrEmpty($Language))
+    {
+        $Language = $RepoName.Substring($RepoName.LastIndexOf('-') + 1)
+    }
+
+    $PackageProp = Get-PkgProperties -PackageName $PackageName -ServiceName $ServiceName -Language $Language -RepoRoot $RepoRoot
+    Confirm-ChangeLogEntry -ChangeLogLocation $PackageProp.pkgChangeLogPath -VersionString $PackageProp.pkgVersion -ForRelease $ForRelease
 }
 
-$PackageProp = Get-PkgProperties -PackageName $PackageName -ServiceName $ServiceName -Language $Language -RepoRoot $RepoRoot
-Confirm-ChangeLogEntry -ChangeLogLocation $PackageProp.pkgChangeLogPath -VersionString $PackageProp.pkgVersion -ForRelease $ForRelease
