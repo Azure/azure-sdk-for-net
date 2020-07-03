68a5db4073b437feb1eab612782fa683602d5088
Tom Yao <v-xuyao@microsoft.com>
2020-07-03 14:23:53 +0800

    Fix CI check error

--- sdk/tables/Azure.Data.Tables/tests/TablesTestEnvironment.cs
+++ sdk/tables/Azure.Data.Tables/tests/TablesTestEnvironment.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Azure.Core.TestFramework;
 
 namespace Azure.Data.Tables.Tests
@@ -16,6 +17,7 @@ namespace Azure.Data.Tables.Tests
 
         public string PrimaryStorageAccountKey => GetRecordedVariable(PrimaryKeyEnvironmentVariableName);
         public string AccountName => GetRecordedVariable("TABLES_STORAGE_ACCOUNT_NAME");
-        public string StorageUri => !string.IsNullOrEmpty(StorageEndpointSuffix) ? $"https://{AccountName}.table.{StorageEndpointSuffix}" : string.Format(StorageUriFormat, AccountName) ;
+        public string TablesEndpointSuffix = Environment.GetEnvironmentVariable("TABLES_STORAGE_ENDPOINT_SUFFIX");
+        public string StorageUri => !string.IsNullOrEmpty(TablesEndpointSuffix) ? $"https://{AccountName}.{TablesEndpointSuffix}" : string.Format(StorageUriFormat, AccountName);
     }
 }
