b79abb12f745b413801d5b4663242795cfae6c93
Jesse Squire <jesse.squire@gmail.com>
2020-07-07 17:50:09 -0400

    [Event Hubs Client] Enhanced Connection String Validation
    The focus of these changes is to enhance connection string validation for
    all clients to ensure that issues are detected where reasonable messages
    can be displayed.  Previously, validation was delegated to the EventHubConnection
    which left a gap when the connection was not eagerly initialized, such as with
    the event processor types.

---
+++ sdk/eventhub/Azure.Messaging.EventHubs.Shared/tests/Core/ConnectionStringPropertiesTests.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Messaging.EventHubs.Core;
+using NUnit.Framework;
+
+namespace Azure.Messaging.EventHubs.Tests
+{
+    /// <summary>
+    ///   The suite of tests for the <see cref="ConnectionStringProperties" />
+    ///   class.
+    /// </summary>
+    ///
+    [TestFixture]
+    public class ConnectionStringPropertiesTests
+    {
+        /// <summary>
+        ///    Verifies functionality of the <see cref="ConnectionStringProperties.Validate" />
+        ///    method.
+        /// </summary>
+        ///
+        [Test]
+        [TestCase("SharedAccessKeyName=[value];SharedAccessKey=[value];EntityPath=[value]")]
+        [TestCase("Endpoint=value.com;SharedAccessKey=[value];EntityPath=[value]")]
+        [TestCase("Endpoint=value.com;SharedAccessKeyName=[value];EntityPath=[value]")]
+        [TestCase("Endpoint=value.com;SharedAccessKeyName=[value];SharedAccessKey=[value]")]
+        [TestCase("HostName=value.azure-devices.net;SharedAccessKeyName=[value];SharedAccessKey=[value]")]
+        [TestCase("HostName=value.azure-devices.net;SharedAccessKeyName=[value];SharedAccessKey=[value];EntityPath=[value]")]
+        public void ValidateDetectsAnInvalidConnectionString(string connectionString)
+        {
+            var properties = ConnectionStringParser.Parse(connectionString);
+            Assert.That(() => properties.Validate(null, "Dummy"), Throws.ArgumentException.And.Message.StartsWith(Resources.MissingConnectionInformation));
+        }
+
+        /// <summary>
+        ///    Verifies functionality of the <see cref="ConnectionStringProperties.Validate" />
+        ///    method.
+        /// </summary>
+        ///
+        [Test]
+        public void ValidateDetectsMultipleEventNames()
+        {
+            var eventHubName = "myHub";
+            var fakeConnection = $"Endpoint=sb://not-real.servicebus.windows.net/;SharedAccessKeyName=DummyKey;SharedAccessKey=[not_real];EntityPath=[unique_fake]";
+            var properties = ConnectionStringParser.Parse(fakeConnection);
+
+            Assert.That(() => properties.Validate(eventHubName, "Dummy"), Throws.ArgumentException.And.Message.StartsWith(Resources.OnlyOneEventHubNameMayBeSpecified));
+        }
+
+        /// <summary>
+        ///    Verifies functionality of the <see cref="ConnectionStringProperties.Validate" />
+        ///    method.
+        /// </summary>
+        ///
+        [Test]
+        public void ValidateAllowsMultipleEventHubNamesIfEqual()
+        {
+            var eventHubName = "myHub";
+            var fakeConnection = $"Endpoint=sb://not-real.servicebus.windows.net/;SharedAccessKeyName=DummyKey;SharedAccessKey=[not_real];EntityPath={ eventHubName }";
+            var properties = ConnectionStringParser.Parse(fakeConnection);
+
+            Assert.That(() => properties.Validate(eventHubName, "dummy"), Throws.Nothing, "Validation should accept the same Event Hub in multiple places");
+        }
+    }
+}
