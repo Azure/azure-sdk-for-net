b22dfb0d18cd501d69855772283cb359af3d2796
Kamil Sobol <61715331+kasobol-msft@users.noreply.github.com>
2020-07-09 16:49:09 -0700

    trimming.

--- sdk/storage/Azure.Storage.Webjobs.Extensions/src/Microsoft.Azure.WebJobs.Host/Converters/TryParseDelegate.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
-namespace Microsoft.Azure.WebJobs.Host.Converters
-{
-    internal delegate bool TryParseDelegate<TOutput>(string input, out TOutput result);
-}
