e28ea256fcec2437c3a65d53d9a391617289f8b7
Chidozie Ononiwu <chononiw@microsoft.com>
2020-07-13 11:04:39 -0700

    Add Developer setting to generate API listing

--- eng/scripts/Aggregate-Dev-Updates.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-param (
-    [Parameter(Position=0)]
-    [ValidateNotNullOrEmpty()]
-    [string] $ServiceDirectory
-)
-
-&"$PSScriptRoot/Update-Snippets.ps1" $ServiceDirectory
-&"$PSScriptRoot/Export-API.ps1" $ServiceDirectory
