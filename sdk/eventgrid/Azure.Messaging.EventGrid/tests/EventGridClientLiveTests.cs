e30cf3b6b9b7d4631aea053a75be2d995a0357f2
Kerri Lee <t-kerlee@microsoft.com>
2020-07-14 12:41:44 -0700

    Added tests for custom payloads

--- sdk/eventgrid/Azure.Messaging.EventGrid/tests/EventGridClientLiveTests.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid/tests/EventGridClientLiveTests.cs
@@ -12,6 +12,7 @@ using Azure.Core;
 using Azure.Core.TestFramework;
 using Azure.Messaging.EventGrid;
 using Azure.Messaging.EventGrid.Models;
+using Azure.Messaging.EventGrid.Tests.SessionRecords;
 using NUnit.Framework;
 
 namespace Azure.Messaging.EventGrid.Tests
@@ -36,6 +37,18 @@ namespace Azure.Messaging.EventGrid.Tests
         }
 
         [Test]
+        public async Task CanPublishEventWithCustomObjectPayload()
+        {
+            EventGridClientOptions options = Recording.InstrumentClientOptions(new EventGridClientOptions());
+            EventGridClient client = InstrumentClient(
+                new EventGridClient(
+                    new Uri(TestEnvironment.TopicHost),
+                    new AzureKeyCredential(TestEnvironment.TopicKey),
+                    options));
+            await client.PublishEventsAsync(GetEventsListWithCustomPayload());
+        }
+
+        [Test]
         public async Task CanPublishEventToDomain()
         {
             EventGridClientOptions options = Recording.InstrumentClientOptions(new EventGridClientOptions());
@@ -57,6 +70,18 @@ namespace Azure.Messaging.EventGrid.Tests
                     new AzureKeyCredential(TestEnvironment.CloudEventTopicKey),
                     options));
             await client.PublishCloudEventsAsync(GetCloudEventsList());
+        }
+
+        [Test]
+        public async Task CanPublishCloudEventWithCustomObjectPayload()
+        {
+            EventGridClientOptions options = Recording.InstrumentClientOptions(new EventGridClientOptions());
+            EventGridClient client = InstrumentClient(
+                new EventGridClient(
+                    new Uri(TestEnvironment.CloudEventTopicHost),
+                    new AzureKeyCredential(TestEnvironment.CloudEventTopicKey),
+                    options));
+            await client.PublishCloudEventsAsync(GetCloudEventsListWithCustomPayload());
         }
 
         [Test]
@@ -124,6 +149,24 @@ namespace Azure.Messaging.EventGrid.Tests
 
             return eventsList;
         }
+        private IList<EventGridEvent> GetEventsListWithCustomPayload()
+        {
+            List<EventGridEvent> eventsList = new List<EventGridEvent>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                eventsList.Add(
+                    new EventGridEvent(
+                        Recording.Random.NewGuid().ToString(),
+                        $"Subject-{i}",
+                        new TestPayload("name", i),
+                        "Microsoft.MockPublisher.TestEvent",
+                        Recording.Now,
+                        "1.0"));
+            }
+
+            return eventsList;
+        }
 
         private IList<EventGridEvent> GetEventsWithTopicsList()
         {
@@ -158,6 +201,24 @@ namespace Azure.Messaging.EventGrid.Tests
                         $"Subject-{i}",
                         "record",
                         "1.0"));
+            }
+
+            return eventsList;
+        }
+
+        private IList<CloudEvent> GetCloudEventsListWithCustomPayload()
+        {
+            List<CloudEvent> eventsList = new List<CloudEvent>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                CloudEvent cloudEvent = new CloudEvent(
+                    Recording.Random.NewGuid().ToString(),
+                    $"Subject-{i}",
+                    "record",
+                    "1.0");
+                cloudEvent.Data = new TestPayload("name", i);
+                eventsList.Add(cloudEvent);
             }
 
             return eventsList;
