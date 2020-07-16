9be69823aad0299600bc829ee3e503df2da0da72
Pavel Krymets <pavel@krymets.com>
2020-07-16 09:29:30 -0700

    Update Autorest C# - Tables

--- sdk/tables/Azure.Data.Tables/src/Generated/Models/TableEntityQueryResponse.cs
+++ sdk/tables/Azure.Data.Tables/src/Generated/Models/TableEntityQueryResponse.cs
@@ -6,6 +6,7 @@
 #nullable disable
 
 using System.Collections.Generic;
+using Azure.Core;
 
 namespace Azure.Data.Tables.Models
 {
@@ -15,6 +16,7 @@ namespace Azure.Data.Tables.Models
         /// <summary> Initializes a new instance of TableEntityQueryResponse. </summary>
         internal TableEntityQueryResponse()
         {
+            Value = new ChangeTrackingList<IDictionary<string, object>>();
         }
 
         /// <summary> Initializes a new instance of TableEntityQueryResponse. </summary>
