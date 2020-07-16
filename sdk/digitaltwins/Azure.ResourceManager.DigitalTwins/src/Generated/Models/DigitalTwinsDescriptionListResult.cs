71015cc4ea1eec766f4a7b062ca048e108c99b82
Pavel Krymets <pavel@krymets.com>
2020-07-16 11:16:34 -0700

    Update Autorest C# - DigitalTwins

--- sdk/digitaltwins/Azure.ResourceManager.DigitalTwins/src/Generated/Models/DigitalTwinsDescriptionListResult.cs
+++ sdk/digitaltwins/Azure.ResourceManager.DigitalTwins/src/Generated/Models/DigitalTwinsDescriptionListResult.cs
@@ -6,6 +6,7 @@
 #nullable disable
 
 using System.Collections.Generic;
+using Azure.Core;
 
 namespace Azure.ResourceManager.DigitalTwins.Models
 {
@@ -15,6 +16,7 @@ namespace Azure.ResourceManager.DigitalTwins.Models
         /// <summary> Initializes a new instance of DigitalTwinsDescriptionListResult. </summary>
         internal DigitalTwinsDescriptionListResult()
         {
+            Value = new ChangeTrackingList<DigitalTwinsDescription>();
         }
 
         /// <summary> Initializes a new instance of DigitalTwinsDescriptionListResult. </summary>
