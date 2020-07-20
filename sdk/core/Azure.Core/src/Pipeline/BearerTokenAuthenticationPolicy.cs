d5b088feda3a24361e18cfd2ff17bcb0df590f69
Alexander Sher <alsher@microsoft.com>
2020-07-20 16:44:56 -0500

    Attempt to merge TokenCredential.GetToken into AccessTokenCache

--- sdk/core/Azure.Core/src/Pipeline/BearerTokenAuthenticationPolicy.cs
+++ sdk/core/Azure.Core/src/Pipeline/BearerTokenAuthenticationPolicy.cs
@@ -14,10 +14,6 @@ namespace Azure.Core.Pipeline
     /// </summary>
     public class BearerTokenAuthenticationPolicy : HttpPipelinePolicy
     {
-        private readonly TokenCredential _credential;
-
-        private readonly string[] _scopes;
-
         private readonly AccessTokenCache _accessTokenCache;
 
         /// <summary>
@@ -39,9 +35,7 @@ namespace Azure.Core.Pipeline
             Argument.AssertNotNull(credential, nameof(credential));
             Argument.AssertNotNull(scopes, nameof(scopes));
 
-            _credential = credential;
-            _scopes = scopes.ToArray();
-            _accessTokenCache = new AccessTokenCache(tokenRefreshOffset, tokenRefreshRetryTimeout);
+            _accessTokenCache = new AccessTokenCache(credential, scopes.ToArray(), tokenRefreshOffset, tokenRefreshRetryTimeout);
         }
 
         /// <inheritdoc />
@@ -64,16 +58,7 @@ namespace Azure.Core.Pipeline
                 throw new InvalidOperationException("Bearer token authentication is not permitted for non TLS protected (https) endpoints.");
             }
 
-            (string? headerValue, bool accessTokenAboutToExpire) = await _accessTokenCache.GetHeaderValueAsync(async, message.CancellationToken);
-            if (headerValue == null)
-            {
-                headerValue = await GetHeaderValueFromCredentialAsync(message, async);
-            }
-            else if (accessTokenAboutToExpire)
-            {
-                _ = Task.Run(() => GetHeaderValueFromCredentialAsync(message, async));
-            }
-
+            string headerValue = await _accessTokenCache.GetHeaderValueAsync(message, async);
             message.Request.SetHeader(HttpHeader.Names.Authorization, headerValue);
 
             if (async)
@@ -86,27 +71,11 @@ namespace Azure.Core.Pipeline
             }
         }
 
-        private async ValueTask<string> GetHeaderValueFromCredentialAsync(HttpMessage message, bool async)
-        {
-            try
-            {
-                var requestContext = new TokenRequestContext(_scopes, message.Request.ClientRequestId);
-                AccessToken token = async
-                    ? await _credential.GetTokenAsync(requestContext, message.CancellationToken).ConfigureAwait(false)
-                    : _credential.GetToken(requestContext, message.CancellationToken);
-
-                return _accessTokenCache.SaveToken(token);
-            }
-            catch (Exception e)
-            {
-                _accessTokenCache.ResetTokenIfExpired(e);
-                throw;
-            }
-        }
-
         private class AccessTokenCache
         {
             private readonly object _syncObj = new object();
+            private readonly TokenCredential _credential;
+            private readonly string[] _scopes;
             private readonly TimeSpan _tokenRefreshOffset;
             private readonly TimeSpan _tokenRefreshRetryTimeout;
 
@@ -116,41 +85,68 @@ namespace Azure.Core.Pipeline
             private DateTimeOffset _refreshOn;
             private DateTimeOffset _expiresOn;
 
-            public AccessTokenCache(TimeSpan tokenRefreshOffset, TimeSpan tokenRefreshRetryTimeout)
+            public AccessTokenCache(TokenCredential credential, string[] scopes, TimeSpan tokenRefreshOffset, TimeSpan tokenRefreshRetryTimeout)
             {
+                _credential = credential;
+                _scopes = scopes;
                 _tokenRefreshOffset = tokenRefreshOffset;
                 _tokenRefreshRetryTimeout = tokenRefreshRetryTimeout;
             }
 
-            public async ValueTask<(string? headerValue, bool refreshTokenAboutToExpire)> GetHeaderValueAsync(bool async, CancellationToken cancellationToken)
+            public async ValueTask<string> GetHeaderValueAsync(HttpMessage message, bool async)
             {
                 (string? headerValue, Task<string>? pendingTask, bool accessTokenAboutToExpire) = GetHeaderValue();
+
+                if (headerValue != null)
+                {
+                    if (accessTokenAboutToExpire)
+                    {
+                        _ = Task.Run(() => GetHeaderValueFromCredentialAsync(message, async));
+                    }
+
+                    return headerValue;
+                }
+
                 if (pendingTask == null)
                 {
-                    return (headerValue, accessTokenAboutToExpire);
+                    return await GetHeaderValueFromCredentialAsync(message, async).ConfigureAwait(false);
                 }
 
                 if (async)
                 {
-                    return (await pendingTask.AwaitWithCancellation(cancellationToken), false);
+                    return await pendingTask.AwaitWithCancellation(message.CancellationToken);
                 }
 
                 try
                 {
-                    pendingTask.Wait(cancellationToken);
+                    pendingTask.Wait(message.CancellationToken);
                 }
                 catch (AggregateException) { } // ignore exception here to rethrow it with EnsureCompleted
 
-                return (pendingTask.EnsureCompleted(), false);
+                return pendingTask.EnsureCompleted();
             }
 
-            public string SaveToken(AccessToken token)
+            private async ValueTask<string> GetHeaderValueFromCredentialAsync(HttpMessage message, bool async)
+            {
+                try
+                {
+                    var requestContext = new TokenRequestContext(_scopes, message.Request.ClientRequestId);
+                    AccessToken token = async
+                        ? await _credential.GetTokenAsync(requestContext, message.CancellationToken).ConfigureAwait(false)
+                        : _credential.GetToken(requestContext, message.CancellationToken);
+                    return SaveToken(token);
+                }
+                catch (Exception e)
+                {
+                    throw ResetTokenIfExpired(e);
+                }
+            }
+
+            private string SaveToken(AccessToken token)
             {
                 if (string.IsNullOrEmpty(token.Token))
                 {
-                    var exception = new InvalidOperationException($"{nameof(TokenCredential)}.{nameof(TokenCredential.GetToken)} has failed with unknown error.");
-                    ResetTokenIfExpired(exception);
-                    throw exception;
+                    throw new InvalidOperationException($"{nameof(TokenCredential)}.{nameof(TokenCredential.GetToken)} has failed with unknown error.");
                 }
 
                 string headerValue = "Bearer " + token.Token;
@@ -159,7 +155,7 @@ namespace Azure.Core.Pipeline
                 return headerValue;
             }
 
-            public void ResetTokenIfExpired(Exception exception)
+            private Exception ResetTokenIfExpired(Exception exception)
             {
                 DateTimeOffset now = DateTimeOffset.UtcNow;
                 TaskCompletionSource<string>? pendingTcs;
@@ -181,6 +177,7 @@ namespace Azure.Core.Pipeline
                 }
 
                 pendingTcs?.SetException(exception);
+                return exception;
             }
 
             /// <summary>
