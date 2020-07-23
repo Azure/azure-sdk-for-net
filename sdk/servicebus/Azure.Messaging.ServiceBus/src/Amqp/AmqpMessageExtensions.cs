c54a33f161242bf0d37627473247733f53fce5f3
Michael Yanni <MiYanni@microsoft.com>
2020-07-22 18:43:56 -0700

    Removed unused alternative method of data extraction that is potentially faster/less memory, but unknown risks.

--- sdk/servicebus/Azure.Messaging.ServiceBus/src/Amqp/AmqpMessageExtensions.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/src/Amqp/AmqpMessageExtensions.cs
@@ -42,18 +42,6 @@ namespace Azure.Messaging.ServiceBus.Amqp
         private static IEnumerable<byte[]> GetDataViaDataBody(AmqpMessage message) =>
             (message.DataBody ?? Enumerable.Empty<Data>()).Select(db => db.GetByteArray()).Where(ba => ba != null);
 
-        // TODO: [WIP] This gets the data bytes via the BodyStream, and this method is current unused.
-        // This ignores the already parsed DataBody information, which is potentially faster and uses less memory.
-        // However, I'm not confident that doing it this way won't cause unknown issues.
-        // This is a drop-in replacement for GetDataViaDataBody.
-        private static IEnumerable<byte[]> GetDataViaBodyStream(AmqpMessage message)
-        {
-            using var streamData = message.BodyStream;
-            var data = new byte[streamData.Length];
-            streamData.Read(data, 0, (int)streamData.Length);
-            return new[] { data };
-        }
-
         // Returns via the out parameter the flattened collection of bytes.
         // A majority of the time, data will only contain 1 element.
         // The method is optimized for this situation to return the pre-existing array.
