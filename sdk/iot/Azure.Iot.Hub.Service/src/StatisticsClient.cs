a367ba7a8af01fd9c009a0e5fe05924bed9d089b
Azad Abbasi <azabbasi@microsoft.com>
2020-07-24 08:50:09 -0700

    Initial commit

--- sdk/iot/Azure.Iot.Hub.Service/src/StatisticsClient.cs
+++ sdk/iot/Azure.Iot.Hub.Service/src/StatisticsClient.cs
@@ -4,22 +4,56 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Iot.Hub.Service.Models;
 
 namespace Azure.Iot.Hub.Service
 {
     /// <summary>
-    /// Statistics Client place holder
+    /// Statistics client to acquire information about IoT Hub statistics.
     /// </summary>
     public class StatisticsClient
     {
-        /// <summary>
-        /// place holder
-        /// </summary>
-#pragma warning disable AZC0007 // DO provide a minimal constructor that takes only the parameters required to connect to the service.
-        public StatisticsClient()
-#pragma warning restore AZC0007 // DO provide a minimal constructor that takes only the parameters required to connect to the service.
-        {
+        private StatisticsRestClient _statisticsRestClient = null;
 
+        /// <summary>
+        /// Initializes a new instance of StatisticsClient.
+        /// </summary>
+        protected StatisticsClient()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of StatisticsClient.
+        /// <param name="statisticsRestClient"> The REST client to query statistics of the IoT Hub. </param>
+        /// </summary>
+        internal StatisticsClient(StatisticsRestClient statisticsRestClient)
+        {
+            Argument.AssertNotNull(statisticsRestClient, nameof(statisticsRestClient));
+
+            _statisticsRestClient = statisticsRestClient;
+        }
+
+        public virtual Response<RegistryStatistics> GetDeviceStatistics(CancellationToken cancellationToken = default)
+        {
+            return _statisticsRestClient.GetDeviceStatistics(cancellationToken);
+        }
+
+        public virtual Task<Response<RegistryStatistics>> GetDeviceStatisticsAsync(CancellationToken cancellationToken = default)
+        {
+            return _statisticsRestClient.GetDeviceStatisticsAsync(cancellationToken);
+        }
+
+        public virtual Response<ServiceStatistics> GetServiceStatistics(CancellationToken cancellationToken = default)
+        {
+            return _statisticsRestClient.GetServiceStatistics(cancellationToken);
+        }
+
+        public virtual Task<Response<ServiceStatistics>> GetServiceStatisticsAsync(CancellationToken cancellationToken = default)
+        {
+            return _statisticsRestClient.GetServiceStatisticsAsync(cancellationToken);
         }
     }
 }
