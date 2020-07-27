927f31ce9e7042fc771a7ab4545ea08a8b3bad5e
Sri Sruti Mandadapu <smandada@microsoft.com>
2020-07-27 16:55:43 -0700

    Sample code to compute signature for signed urls

---
+++ samples/CdnSignedUrlSignatureComputationSample/README.md
@@ -0,0 +1,23 @@
+﻿SIGNEDURLUTILITIES: 
+This is .Net Console App which is used to generate signature for Signed Urls.
+
+Signature generation algorithm:
+StringToHash = ResourcePath + 
+               "?" +
+               expiresParamName + 
+               "=" +
+               expiresValue +
+               "&" +
+               keyIdParamName +
+               "=" +
+               keyIdValue
+
+// ConvertToUrlSafeBase64String  will replace + with - and / with _. 
+ComputedHash = ConvertToUrlSafeBase64String((new HMACSHA256(Encoding.ASCII.GetBytes(encryptionKey))).ComputeHash(Encoding.UTF8.GetBytes(StringToHash)));
+
+How to use?
+Provide the parameters used to compute the signature in the below order:
+
+./SignatureComputationApp <ResourcePath> <ExpiresParamName> <ExpiresParamValue> <KeyIdParamName> <KeyIdParamValue> <EncryptionKey>
+
+The computed signature is written to the console.
---
+++ samples/CdnSignedUrlSignatureComputationSample/SignedUrlSignature.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace CdnSignedUrlSignatureComputationSample
+{
+    class SignedUrlSignature
+    {
+        /// <summary>
+        /// Converts input to URL safe base64 string.
+        /// </summary>
+        /// <param name="signature">Computed signature.</param>
+        /// <returns>Url safe base64 signature.</returns>
+        private string ConvertToUrlSafeBase64String(byte[] signature)
+        {
+            return Convert.ToBase64String(signature).Replace('/', '_').Replace('+', '-');
+        }
+
+        /// <summary>
+        /// Function to compute signature.
+        /// </summary>
+        /// <param name="resourcePath">Resource Path.</param>
+        /// <param name="expiresParamName">Expires parameter name.</param>
+        /// <param name="expiresParamValue">Expires parameter value.</param>
+        /// <param name="keyParamName">Key parameter name.</param>
+        /// <param name="keyParamValue">Key parameter value.</param>
+        /// <param name="privateKey">Key to use to compute hash.</param>
+        /// <returns>Signature.</returns>
+        public string GetSignature(string resourcePath, string expiresParamName, string expiresParamValue, string keyParamName, string keyParamValue, string privateKey)
+        {
+            if (string.IsNullOrEmpty(resourcePath))
+            {
+                throw new ArgumentException("Input parameter 'resourcePath' cannot be null or empty");
+            }
+
+            if (string.IsNullOrEmpty(expiresParamName))
+            {
+                throw new ArgumentException("Input parameter 'expiresParamName' cannot be null or empty");
+            }
+
+            if (string.IsNullOrEmpty(expiresParamValue))
+            {
+                throw new ArgumentException("Input parameter 'expiresParamValue' cannot be null or empty");
+            }
+
+            if (string.IsNullOrEmpty(keyParamName))
+            {
+                throw new ArgumentException("Input parameter 'keyParamName' cannot be null or empty");
+            }
+
+            if (string.IsNullOrEmpty(keyParamValue))
+            {
+                throw new ArgumentException("Input parameter 'keyParamValue' cannot be null or empty");
+            }
+
+            if (string.IsNullOrEmpty(privateKey))
+            {
+                throw new ArgumentException("Input parameter 'privateKey' cannot be null or empty");
+            }
+
+            // Create the string to hash
+            string data = resourcePath + "?" + expiresParamName + "=" + expiresParamValue + "&" + keyParamName + "=" + keyParamValue;
+
+            byte[] signature;
+            using (HMACSHA256 hmac = new HMACSHA256(Encoding.ASCII.GetBytes(privateKey)))
+            {
+                signature = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+            }
+
+            return this.ConvertToUrlSafeBase64String(signature);
+        }
+    }
+}
