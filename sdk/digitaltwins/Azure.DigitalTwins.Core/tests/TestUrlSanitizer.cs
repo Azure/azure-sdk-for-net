de2742feb808c55002e51efeba4e796023ba61d2
Azad Abbasi <azabbasi@microsoft.com>
2020-07-28 19:41:54 -0700

    Feat(E2Etests): Sanitize host names. (#13791)
    Sanitize host names

---
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/tests/TestUrlSanitizer.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core.TestFramework;
+
+namespace Azure.DigitalTwins.Core.Tests
+{
+    internal class TestUrlSanitizer : RecordedTestSanitizer
+    {
+        private const string FAKE_HOST = "fakeHost.api.wus2.digitaltwins.azure.net";
+
+        public override string SanitizeVariable(string variableName, string environmentVariableValue) =>
+            variableName switch
+            {
+                "DIGITALTWINS_URL" => SanitizeUri(environmentVariableValue),
+                _ => base.SanitizeVariable(variableName, environmentVariableValue)
+            };
+
+        public override string SanitizeUri(string uri)
+        {
+            return uri.Replace(new Uri(uri).Host, FAKE_HOST);
+        }
+    }
+}
