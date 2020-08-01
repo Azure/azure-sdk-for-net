e4d334cd44bb6ee21ac367243b4c9a826bfa3f78
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-07-31 18:58:29 -0700

    Sanitize line endings

--- sdk/servicebus/Azure.Messaging.ServiceBus/tests/Infrastructure/ServiceBusRecordedTestSanitizer.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/tests/Infrastructure/ServiceBusRecordedTestSanitizer.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using Azure.Core.TestFramework;
 
@@ -11,6 +9,7 @@ namespace Azure.Messaging.ServiceBus.Tests.Infrastructure
 {
     public class ServiceBusRecordedTestSanitizer : RecordedTestSanitizer
     {
+        private static bool s_IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
         public ServiceBusRecordedTestSanitizer()
         {
@@ -22,9 +21,13 @@ namespace Azure.Messaging.ServiceBus.Tests.Infrastructure
         private const string SanitizedKeyValue = "SanitizedSanitizedSanitizedSanitizedSanitize";
         public override string SanitizeTextBody(string contentType, string body)
         {
-            var replaced = Regex.Replace(body, "\\u003CPrimaryKey\\u003E.*\\u003C/PrimaryKey\\u003E", $"\u003CPrimaryKey\u003E{SanitizedKeyValue}\u003C/PrimaryKey\u003E");
-            replaced = Regex.Replace(replaced, "\\u003CSecondaryKey\\u003E.*\\u003C/SecondaryKey\\u003E", $"\u003CSecondaryKey\u003E{SanitizedKeyValue}\u003C/SecondaryKey\u003E");
-            return replaced;
+            var sanitized = Regex.Replace(body, "\\u003CPrimaryKey\\u003E.*\\u003C/PrimaryKey\\u003E", $"\u003CPrimaryKey\u003E{SanitizedKeyValue}\u003C/PrimaryKey\u003E");
+            sanitized = Regex.Replace(sanitized, "\\u003CSecondaryKey\\u003E.*\\u003C/SecondaryKey\\u003E", $"\u003CSecondaryKey\u003E{SanitizedKeyValue}\u003C/SecondaryKey\u003E");
+            if (!s_IsWindows)
+            {
+                sanitized = sanitized.Replace("\n", "\r\n");
+            }
+            return sanitized;
         }
         public override string SanitizeVariable(string variableName, string environmentVariableValue)
         {
