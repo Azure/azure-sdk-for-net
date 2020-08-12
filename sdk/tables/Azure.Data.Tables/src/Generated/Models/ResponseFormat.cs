b416d0dba4239a18dcae03c1326fc31e26b10244
Christopher Scott <chriss@microsoft.com>
2020-08-12 11:10:46 -0500

    Implement CreateIfNotExists and CreateTableIfNotExists (#14105)
    * Implement CreateIfNotExists and CreateTableIfNotExists
    
    * cleanup SetAccessPolicyAsync arg default
    
    * prevet cosmos 429 errors on CreateTable
    
    * address #13986
    
    * TableGeoReplicationInfo rename
    
    * make ResponseFormat internal
    
    * TableServiceProperties
    
    * remove hard coded playback mode
    
    * remove autogen comment

--- sdk/tables/Azure.Data.Tables/src/Generated/Models/ResponseFormat.cs
+++ sdk/tables/Azure.Data.Tables/src/Generated/Models/ResponseFormat.cs
@@ -11,7 +11,7 @@ using System.ComponentModel;
 namespace Azure.Data.Tables.Models
 {
     /// <summary> The ResponseFormat. </summary>
-    public readonly partial struct ResponseFormat : IEquatable<ResponseFormat>
+    internal readonly partial struct ResponseFormat : IEquatable<ResponseFormat>
     {
         private readonly string _value;
 
