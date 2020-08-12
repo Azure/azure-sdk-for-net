aeaadab2e75780ad2396f90d89a3933b208d8d0c
Pavel Krymets <pavel@krymets.com>
2020-08-12 13:24:25 -0700

    More working stuff

--- eng/scripts/Rerecord.ps1
+++ eng/scripts/Rerecord.ps1
@@ -1,6 +1,11 @@
 param ($pr, $sdks)
 
-Write-Host "Loading builds for PR $pr"
+$repoRoot = Resolve-Path "$PSScriptRoot/../..";
+$artifactsPath = Join-Path $repoRoot "artifacts"
+
+$token = (az account get-access-token --resource=https://management.core.windows.net/ -o json | ConvertFrom-Json).accessToken
+
+Write-Host "Processing builds for PR $pr"
 $builds = az pipelines runs list --organization https://dev.azure.com/azure-sdk --project internal --branch "refs/pull/$pr/merge" --query-order FinishTimeDesc --status completed -o json 2> $null | ConvertFrom-Json;
 $processedDefinitions = @();
 
@@ -14,5 +19,21 @@ foreach ($build in $builds)
     $processedDefinitions += $definitionName;
 
     Write-Host "Processing results from $definitionName"
-    #az pipelines runs artifact download --organization https://dev.azure.com/azure-sdk --project internal --run-id 493407 --artifact-name SessionRecords --path .
+    $artifacts = az pipelines runs artifact list --organization https://dev.azure.com/azure-sdk --project internal --run-id $build.id -o json 2> $null | ConvertFrom-Json;
+
+    foreach ($artifact in $artifacts)
+    {
+        $downloadUrl = $artifact.resource.downloadUrl
+        if ($artifact.name -eq "SessionRecords")
+        {
+            $destination = Join-Path $artifactsPath "$definitionName.zip"
+
+            Write-Host "Downloading artifact $downloadUrl to '$destination'"
+            Invoke-WebRequest -Uri $downloadUrl -OutFile $destination -Headers @{Authorization="Bearer $token"}
+            Expand-Archive -Path $destination -DestinationPath $artifactsPath -Force
+        }
+    }
 }
+
+$sessionRecordsPaths = Join-Path $artifactsPath "SessionRecords"
+Copy-Item -Path $sessionRecordsPaths -Filter "*.json" -Recurse -Destination $repoRoot -Container
