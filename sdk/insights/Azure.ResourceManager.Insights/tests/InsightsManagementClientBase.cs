510139d635e5f003e864430680d0d3fd08cd10a5
m-nash <64171366+m-nash@users.noreply.github.com>
2020-08-12 14:22:29 -0700

    Add client base class

---
+++ sdk/insights/Azure.ResourceManager.Insights/tests/InsightsManagementClientBase.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.TestFramework;
+
+namespace Azure.ResourceManager.Insights.Tests
+{
+    [RunFrequency(RunTestFrequency.Manually)]
+    public abstract class InsightsManagementClientBase : ManagementRecordedTestBase<InsightsManagementTestEnvironment>
+    {
+        protected InsightsManagementClientBase(bool isAsync) : base(isAsync)
+        {
+
+        }
+    }
+}
