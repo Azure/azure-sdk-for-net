3e8a0ad0d9b383e67e917b023e1897669010d308
Tom Yao <v-xuyao@microsoft.com>
2020-08-12 16:07:16 +0800

    Modify test.yml of tables for live tests

--- sdk/tables/Azure.Data.Tables/tests/TablesTestEnvironment.cs
+++ sdk/tables/Azure.Data.Tables/tests/TablesTestEnvironment.cs
@@ -14,17 +14,16 @@ namespace Azure.Data.Tables.Tests
 
         // Storage Tables
         public const string PrimaryStorageKeyEnvironmentVariableName = "TABLES_PRIMARY_STORAGE_ACCOUNT_KEY";
-        private const string StorageUriFormat = "https://{0}.table.core.windows.net";
-        
-        public string PrimaryStorageAccountKey => GetRecordedVariable(PrimaryKeyEnvironmentVariableName);
-        public string AccountName => GetRecordedVariable("TABLES_STORAGE_ACCOUNT_NAME");
-        public string TablesEndpointSuffix => GetOptionalVariable("TABLES_STORAGE_ENDPOINT_SUFFIX");
-        public string StorageUri => !string.IsNullOrEmpty(TablesEndpointSuffix) ? $"https://{AccountName}.{TablesEndpointSuffix}" : string.Format(StorageUriFormat, AccountName);
+        public string PrimaryStorageAccountKey => GetRecordedVariable(PrimaryStorageKeyEnvironmentVariableName);
+        public string StorageAccountName => GetRecordedVariable("TABLES_STORAGE_ACCOUNT_NAME");
+        public string TablesStorageEndpointSuffix => GetOptionalVariable("TABLES_STORAGE_ENDPOINT_SUFFIX") ?? "table.core.windows.net";
+        public string StorageUri => $"https://{StorageAccountName}.{TablesStorageEndpointSuffix}";
+
         // Cosmos Tables
         public const string PrimaryCosmosKeyEnvironmentVariableName = "TABLES_PRIMARY_COSMOS_ACCOUNT_KEY";
-        private const string CosmosUriFormat = "https://{0}.table.cosmos.azure.com";
         public string PrimaryCosmosAccountKey => GetRecordedVariable(PrimaryCosmosKeyEnvironmentVariableName);
         public string CosmosAccountName => GetRecordedVariable("TABLES_COSMOS_ACCOUNT_NAME");
-        public string CosmosUri => string.Format(CosmosUriFormat, CosmosAccountName);
+        public string TablesComsmosEndpointSuffix => GetOptionalVariable("TABLES_COSMOS_ENDPOINT_SUFFIX") ?? "table.cosmos.azure.com";
+        public string CosmosUri => $"https://{CosmosAccountName}.{TablesComsmosEndpointSuffix}";
     }
 }
