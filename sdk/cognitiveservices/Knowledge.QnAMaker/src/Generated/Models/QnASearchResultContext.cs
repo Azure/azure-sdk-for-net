af85cc648f5a14c4b7eef7d4c6dfce46b44af898
vipeketi <58211229+vipeketi@users.noreply.github.com>
2020-08-13 20:42:21 +0530

    <QnAMaker> GenerateAnswer new field "StrictFiltersCompoundOperationType" (#13936)
    * Metadata Operation Type
    
    * generated
    
    * Update cognitiveservices_data-plane_QnAMaker.txt
    
    folder stucture
    
    * Changelog csproj version changes

--- sdk/cognitiveservices/Knowledge.QnAMaker/src/Generated/Models/QnASearchResultContext.cs
+++ sdk/cognitiveservices/Knowledge.QnAMaker/src/Generated/Models/QnASearchResultContext.cs
@@ -49,15 +49,5 @@ namespace Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker.Models
         /// </summary>
         partial void CustomInit();
 
-        /// <summary>
-        /// Validate the object.
-        /// </summary>
-        /// <exception cref="Rest.ValidationException">
-        /// Thrown if validation fails
-        /// </exception>
-        public override void Validate()
-        {
-            base.Validate();
-        }
     }
 }
