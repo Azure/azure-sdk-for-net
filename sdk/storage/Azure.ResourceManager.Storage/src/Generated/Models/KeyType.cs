f775e1f8b8d33904015800c4d0894115a6d22515
Pavel Krymets <pavel@krymets.com>
2020-08-13 12:41:02 -0700

    Update Autorest C# - storage (#13582)
    Co-authored-by: Harvey Chen <30464227+HarveyLink@users.noreply.github.com>

--- sdk/storage/Azure.ResourceManager.Storage/src/Generated/Models/KeyType.cs
+++ sdk/storage/Azure.ResourceManager.Storage/src/Generated/Models/KeyType.cs
@@ -16,6 +16,7 @@ namespace Azure.ResourceManager.Storage.Models
         private readonly string _value;
 
         /// <summary> Determines if two <see cref="KeyType"/> values are the same. </summary>
+        /// <exception cref="ArgumentNullException"> <paramref name="value"/> is null. </exception>
         public KeyType(string value)
         {
             _value = value ?? throw new ArgumentNullException(nameof(value));
