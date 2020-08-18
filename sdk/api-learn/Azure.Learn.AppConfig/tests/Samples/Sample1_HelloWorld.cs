9992a2e4d208b62ef97f0f58c71594204fd534bd
Anne Thompson <annelo@microsoft.com>
2020-08-18 09:10:26 -0700

    Add training solution to `api-learn` branch (#14247)
    * generated code with empty sample
    
    * remove public client
    
    * Update sdk/api-learn/Azure.Learn.AppConfig/tests/AppConfigClientTestEnvironment.cs
    
    Co-authored-by: Pavel Krymets <pavel@krymets.com>
    
    Co-authored-by: Pavel Krymets <pavel@krymets.com>

---
+++ sdk/api-learn/Azure.Learn.AppConfig/tests/Samples/Sample1_HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.Learn.AppConfig.Samples
+{
+    public class Sample1_HelloWorld : SamplesBase<AppConfigClientTestEnvironment>
+    {
+        [Test]
+        public void GetConfigurationSetting()
+        {
+            // TODO: Add code to retrieve a configuration setting from a config store
+
+
+        }
+    }
+}
