212c5dbb01e973466aa4ce9f183cff6c76ceaee0
Luyunmt <2534523917@qq.com>
2020-08-18 17:07:05 +0800

    change storage

--- sdk/storage/Azure.Storage.Common/tests/Shared/TestExtensions.cs
+++ sdk/storage/Azure.Storage.Common/tests/Shared/TestExtensions.cs
@@ -103,7 +103,10 @@ namespace Azure.Storage
             string endpointSuffix = Constants.ConnectionStrings.DefaultEndpointSuffix,
             bool useHttps = true)
         {
-            endpointSuffix = Environment.GetEnvironmentVariable("STORAGE_ENDPOINT_SUFFIX") ?? Constants.ConnectionStrings.DefaultEndpointSuffix;
+            if (!string.IsNullOrEmpty(endpointSuffix))
+            {
+                endpointSuffix = Environment.GetEnvironmentVariable("STORAGE_ENDPOINT_SUFFIX") ?? Constants.ConnectionStrings.DefaultEndpointSuffix;
+            }
             var conn = new StorageConnectionString(storageCredentials);
             if (storageCredentials == null)
             {
