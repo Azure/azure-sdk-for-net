8ec7c8df418a4a2242017ef5bc04543a33cfa75b
Shivangi Reja <shreja@microsoft.com>
2020-08-18 12:20:58 -0700

    Enable public-clients to true

--- sdk/anomalydetector/Azure.AI.AnomalyDetector/src/AnomalyDetectorClient.cs
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/src/AnomalyDetectorClient.cs
@@ -2,19 +2,15 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Azure.AI.AnomalyDetector.Models;
 using Azure.Core;
 using Azure.Core.Pipeline;
-using Request = Azure.AI.AnomalyDetector.Models.Request;
 
 namespace Azure.AI.AnomalyDetector
 {
     /// <summary>
     /// <see cref="AnomalyDetectorClient"/> is use to connect to the Azure Cognitive Anomaly Detector Service.
     /// </summary>
-    public class AnomalyDetectorClient
+    public partial class AnomalyDetectorClient
     {
         /// <summary>Provides communication with the Anomaly Detector Azure Cognitive Service through its REST API.</summary>
         internal readonly ServiceRestClient ServiceClient;
@@ -99,145 +95,6 @@ namespace Azure.AI.AnomalyDetector
         {
             Diagnostics = diagnostics;
             ServiceClient = serviceClient;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AnomalyDetectorClient"/> class.
-        /// </summary>
-        protected AnomalyDetectorClient()
-        {
-        }
-
-        /// <summary> This operation generates a model using an entire series, each point is detected with the same model. With this method, points before and after a certain point are used to determine whether it is an anomaly. The entire detection can give user an overall status of the time series. </summary>
-        /// <param name="body"> Time series points and period if needed. Advanced model parameters can also be set in the request. </param>
-        /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
-        public virtual async Task<Response<EntireDetectResponse>> EntireDetectAsync(Request body, CancellationToken cancellationToken = default)
-        {
-            Argument.AssertNotNull(body, nameof(body));
-
-            using DiagnosticScope scope = Diagnostics.CreateScope($"{nameof(AnomalyDetectorClient)}.{nameof(EntireDetectAsync)}");
-            scope.Start();
-
-            try
-            {
-                return await ServiceClient.EntireDetectAsync(body, cancellationToken).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                scope.Failed(e);
-                throw;
-            }
-        }
-
-        /// <summary> This operation generates a model using an entire series, each point is detected with the same model. With this method, points before and after a certain point are used to determine whether it is an anomaly. The entire detection can give user an overall status of the time series. </summary>
-        /// <param name="body"> Time series points and period if needed. Advanced model parameters can also be set in the request. </param>
-        /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
-        public virtual Response<EntireDetectResponse> EntireDetect(Request body, CancellationToken cancellationToken = default)
-        {
-            Argument.AssertNotNull(body, nameof(body));
-
-            using DiagnosticScope scope = Diagnostics.CreateScope($"{nameof(AnomalyDetectorClient)}.{nameof(EntireDetect)}");
-            scope.Start();
-
-            try
-            {
-                return ServiceClient.EntireDetect(body, cancellationToken);
-            }
-            catch (Exception e)
-            {
-                scope.Failed(e);
-                throw;
-            }
-        }
-
-        /// <summary> This operation generates a model using points before the latest one. With this method, only historical points are used to determine whether the target point is an anomaly. The latest point detecting operation matches the scenario of real-time monitoring of business metrics. </summary>
-        /// <param name="body"> Time series points and period if needed. Advanced model parameters can also be set in the request. </param>
-        /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
-        public virtual async Task<Response<LastDetectResponse>> LastDetectAsync(Request body, CancellationToken cancellationToken = default)
-        {
-            Argument.AssertNotNull(body, nameof(body));
-
-            using DiagnosticScope scope = Diagnostics.CreateScope($"{nameof(AnomalyDetectorClient)}.{nameof(LastDetectAsync)}");
-            scope.Start();
-
-            try
-            {
-                return await ServiceClient.LastDetectAsync(body, cancellationToken).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                scope.Failed(e);
-                throw;
-            }
-        }
-
-        /// <summary> This operation generates a model using points before the latest one. With this method, only historical points are used to determine whether the target point is an anomaly. The latest point detecting operation matches the scenario of real-time monitoring of business metrics. </summary>
-        /// <param name="body"> Time series points and period if needed. Advanced model parameters can also be set in the request. </param>
-        /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
-        public virtual Response<LastDetectResponse> LastDetect(Request body, CancellationToken cancellationToken = default)
-        {
-            Argument.AssertNotNull(body, nameof(body));
-
-            using DiagnosticScope scope = Diagnostics.CreateScope($"{nameof(AnomalyDetectorClient)}.{nameof(LastDetect)}");
-            scope.Start();
-
-            try
-            {
-                return ServiceClient.LastDetect(body, cancellationToken);
-            }
-            catch (Exception e)
-            {
-                scope.Failed(e);
-                throw;
-            }
-        }
-
-        /// <summary> Evaluate change point score of every series point. </summary>
-        /// <param name="body"> Time series points and granularity is needed. Advanced model parameters can also be set in the request if needed. </param>
-        /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
-        public virtual async Task<Response<ChangePointDetectResponse>> ChangePointDetectAsync(ChangePointDetectRequest body, CancellationToken cancellationToken = default)
-        {
-            Argument.AssertNotNull(body, nameof(body));
-
-            using DiagnosticScope scope = Diagnostics.CreateScope($"{nameof(AnomalyDetectorClient)}.{nameof(ChangePointDetectAsync)}");
-            scope.Start();
-
-            try
-            {
-                return await ServiceClient.ChangePointDetectAsync(body, cancellationToken).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                scope.Failed(e);
-                throw;
-            }
-        }
-
-        /// <summary> Evaluate change point score of every series point. </summary>
-        /// <param name="body"> Time series points and granularity is needed. Advanced model parameters can also be set in the request if needed. </param>
-        /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
-        public virtual Response<ChangePointDetectResponse> ChangePointDetect(ChangePointDetectRequest body, CancellationToken cancellationToken = default)
-        {
-            Argument.AssertNotNull(body, nameof(body));
-
-            using DiagnosticScope scope = Diagnostics.CreateScope($"{nameof(AnomalyDetectorClient)}.{nameof(ChangePointDetect)}");
-            scope.Start();
-
-            try
-            {
-                return ServiceClient.ChangePointDetect(body, cancellationToken);
-            }
-            catch (Exception e)
-            {
-                scope.Failed(e);
-                throw;
-            }
         }
     }
 }
--- sdk/anomalydetector/Azure.AI.AnomalyDetector/src/autorest.md
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/src/autorest.md
@@ -6,4 +6,5 @@ Run `dotnet msbuild /t:GenerateCode` to generate code.
 input-file:
     -  https://github.com/Azure/azure-rest-api-specs/blob/1280dd676549a0631330945508bcee05ee93c800/specification/cognitiveservices/data-plane/AnomalyDetector/preview/v1.0/AnomalyDetector.json
 namespace: Azure.AI.AnomalyDetector
+public-clients: true
 ```
