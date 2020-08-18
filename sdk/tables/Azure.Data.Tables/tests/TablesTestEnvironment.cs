c9e3806b95e78daf259bef76dbaf361f92d2209d
Tom Yao <v-xuyao@microsoft.com>
2020-08-18 17:58:13 +0800

    Fix storage table test errors

--- sdk/tables/Azure.Data.Tables/tests/TablesTestEnvironment.cs
+++ sdk/tables/Azure.Data.Tables/tests/TablesTestEnvironment.cs
@@ -12,19 +12,20 @@ namespace Azure.Data.Tables.Tests
         {
         }
 
+        private const string UriFormat = "https://{0}.{1}";
+
         // Storage Tables
         public const string PrimaryStorageKeyEnvironmentVariableName = "TABLES_PRIMARY_STORAGE_ACCOUNT_KEY";
         public string PrimaryStorageAccountKey => GetRecordedVariable(PrimaryStorageKeyEnvironmentVariableName);
         public string StorageAccountName => GetRecordedVariable("TABLES_STORAGE_ACCOUNT_NAME");
         public string TablesStorageEndpointSuffix => GetOptionalVariable("TABLES_STORAGE_ENDPOINT_SUFFIX") ?? "table.core.windows.net";
-        public string StorageUri => $"https://{StorageAccountName}.{TablesStorageEndpointSuffix}";
+        public string StorageUri => string.Format(UriFormat, StorageAccountName, TablesStorageEndpointSuffix);
 
         // Cosmos Tables
         public const string PrimaryCosmosKeyEnvironmentVariableName = "TABLES_PRIMARY_COSMOS_ACCOUNT_KEY";
-        private const string CosmosUriFormat = "https://{0}.{1}";
         public string PrimaryCosmosAccountKey => GetRecordedVariable(PrimaryCosmosKeyEnvironmentVariableName);
         public string CosmosAccountName => GetRecordedVariable("TABLES_COSMOS_ACCOUNT_NAME");
         public string TablesCosmosEndpointSuffix => GetOptionalVariable("TABLES_COSMOS_ENDPOINT_SUFFIX") ?? "table.cosmos.azure.com";
-        public string CosmosUri => string.Format(CosmosUriFormat, CosmosAccountName, TablesCosmosEndpointSuffix);
+        public string CosmosUri => string.Format(UriFormat, CosmosAccountName, TablesCosmosEndpointSuffix);
     }
 }
