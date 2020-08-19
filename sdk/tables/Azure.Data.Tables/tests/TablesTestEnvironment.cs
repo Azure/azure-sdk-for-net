61ddb83640b1b0dc485ac045a3aaaa3df3a987b3
Tom Yao <v-xuyao@microsoft.com>
2020-08-19 10:32:20 +0800

    Add table into table format

--- sdk/tables/Azure.Data.Tables/tests/TablesTestEnvironment.cs
+++ sdk/tables/Azure.Data.Tables/tests/TablesTestEnvironment.cs
@@ -12,20 +12,20 @@ namespace Azure.Data.Tables.Tests
         {
         }
 
-        private const string UriFormat = "https://{0}.{1}";
+        private const string TableUriFormat = "https://{0}.table.{1}";
 
         // Storage Tables
         public const string PrimaryStorageKeyEnvironmentVariableName = "TABLES_PRIMARY_STORAGE_ACCOUNT_KEY";
         public string PrimaryStorageAccountKey => GetRecordedVariable(PrimaryStorageKeyEnvironmentVariableName);
         public string StorageAccountName => GetRecordedVariable("TABLES_STORAGE_ACCOUNT_NAME");
-        public string TablesStorageEndpointSuffix => GetOptionalVariable("TABLES_STORAGE_ENDPOINT_SUFFIX") ?? "table.core.windows.net";
-        public string StorageUri => string.Format(UriFormat, StorageAccountName, TablesStorageEndpointSuffix);
+        public string TablesStorageEndpointSuffix => StorageEndpointSuffix ?? "core.windows.net";
+        public string StorageUri => string.Format(TableUriFormat, StorageAccountName, TablesStorageEndpointSuffix);
 
         // Cosmos Tables
         public const string PrimaryCosmosKeyEnvironmentVariableName = "TABLES_PRIMARY_COSMOS_ACCOUNT_KEY";
         public string PrimaryCosmosAccountKey => GetRecordedVariable(PrimaryCosmosKeyEnvironmentVariableName);
         public string CosmosAccountName => GetRecordedVariable("TABLES_COSMOS_ACCOUNT_NAME");
-        public string TablesCosmosEndpointSuffix => GetOptionalVariable("TABLES_COSMOS_ENDPOINT_SUFFIX") ?? "table.cosmos.azure.com";
-        public string CosmosUri => string.Format(UriFormat, CosmosAccountName, TablesCosmosEndpointSuffix);
+        public string TablesCosmosEndpointSuffix => GetOptionalVariable("TABLES_COSMOS_ENDPOINT_SUFFIX") ?? "cosmos.azure.com";
+        public string CosmosUri => string.Format(TableUriFormat, CosmosAccountName, TablesCosmosEndpointSuffix);
     }
 }
