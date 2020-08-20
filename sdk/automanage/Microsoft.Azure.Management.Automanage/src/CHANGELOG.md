e793c705b3f9f6b9756916be0c76613871dbfc4c
Stephen Owen <stephen.owen@microsoft.com>
2020-08-20 18:00:09 -0400

    Includes Generated Files And Half Of Unit Test Coverage

---
+++ sdk/automanage/Microsoft.Azure.Management.Automanage/src/CHANGELOG.md
@@ -0,0 +1,11 @@
+﻿# Microsoft.Azure.Management.GuestConfiguration release notes
+
+## Changes in 0.6.0-preview
+### Features
+Provides Microsoft Azure Automanage operations including the ability to create, update and get guest configuration assignments, compliance statuses, guest configuration assignment reports.
+
+### Bug fixes
+None
+
+### REST API version
+This version of the GuestConfiguration .NET client library targets version 2020-06-30-preview of the Azure Automanage REST API.
---
+++ sdk/automanage/Microsoft.Azure.Management.Automanage/tests/ScenarioTests/ConfigurationProfilesAssignmentsTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+namespace Automanage.Tests.ScenarioTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Automanage.Tests.Helpers;
+    using Microsoft.Azure.Management.Automanage;
+    using Microsoft.Azure.Management.Automanage.Models;
+    using Microsoft.Rest.Azure;
+    using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class ConfigurationProfilesAssignmentsTests : TestBase
+    {
+        private RecordedDelegatingHandler handler;
+
+        public ConfigurationProfilesAssignmentsTests()
+            : base()
+        {
+            handler = new RecordedDelegatingHandler { SubsequentStatusCodeToReturn = HttpStatusCode.OK };
+        }
+
+        [Fact]
+        [Trait("Category", "Scenario")]
+        public void ConfigurationProfilesAssignmentsGetsExpectedProfile()
+        {
+            var thisType = this.GetType();
+            using (MockContext context = MockContext.Start(thisType))
+            {
+                var automanageClient = GetAutomanagementClient(context, handler);
+
+                var actual = automanageClient.Accounts.Get("AMVM-SubLib-017-ABP", "AMVM-SubLib-017_group");
+
+                Assert.NotNull(actual);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Scenario")]
+        public void ConfigurationProfilesAssignmentsAssignsExpectedProfile()
+        {
+            var thisType = this.GetType();
+            using (MockContext context = MockContext.Start(thisType))
+            {
+                var automanageClient = GetAutomanagementClient(context, handler);
+
+                var actual = automanageClient.Accounts.Get("AMVM-SubLib-017-ABP", "AMVM-SubLib-017_group");
+
+                Assert.NotNull(actual);
+            }
+        }
+    }
+}
