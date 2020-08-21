27aa68f609cc5e265aee2cb4ce25e1f25166ba33
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-08-21 15:23:12 -0700

    Azure Monitor Exporter - Add SDK version (#14466)

--- sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorTraceExporterEventSource.cs
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorTraceExporterEventSource.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Diagnostics.Tracing;
 
 namespace OpenTelemetry.Exporter.AzureMonitor
@@ -14,13 +15,25 @@ namespace OpenTelemetry.Exporter.AzureMonitor
         [NonEvent]
         public void ConfigurationStringParseWarning(string message)
         {
-            if (this.IsEnabled(EventLevel.Warning, (EventKeywords)(-1)))
+            if (this.IsEnabled(EventLevel.Warning, EventKeywords.All))
             {
                 this.WarnToParseConfigurationString(message);
             }
         }
 
+        [NonEvent]
+        public void SdkVersionCreateFailed(Exception ex)
+        {
+            if (this.IsEnabled(EventLevel.Warning, EventKeywords.All))
+            {
+                this.WarnSdkVersionCreateException(ex.ToInvariantString());
+            }
+        }
+
         [Event(1, Message = "{0}", Level = EventLevel.Warning)]
         public void WarnToParseConfigurationString(string message) => this.WriteEvent(1, message);
+
+        [Event(2, Message = "Error creating SdkVersion : '{0}'", Level = EventLevel.Warning)]
+        public void WarnSdkVersionCreateException(string message) => this.WriteEvent(2, message);
     }
 }
