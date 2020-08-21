4524d60eafc2da2df1f601b27746509031e67cd6
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-08-21 13:34:17 -0700

    Handled exception in SdkVersionUtils

--- sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorTraceExporterEventSource.cs
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorTraceExporterEventSource.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Diagnostics.Tracing;
 
 namespace OpenTelemetry.Exporter.AzureMonitor
@@ -20,7 +21,19 @@ namespace OpenTelemetry.Exporter.AzureMonitor
             }
         }
 
+        [NonEvent]
+        public void SdkVersionCreateFailed(Exception ex)
+        {
+            if (this.IsEnabled(EventLevel.Warning, (EventKeywords)(-1)))
+            {
+                this.WarnSdkVersionCreateException(ex.ToInvariantString());
+            }
+        }
+
         [Event(1, Message = "{0}", Level = EventLevel.Warning)]
         public void WarnToParseConfigurationString(string message) => this.WriteEvent(1, message);
+
+        [Event(2, Message = "Error creating SdkVersion : '{0}'", Level = EventLevel.Warning)]
+        public void WarnSdkVersionCreateException(string message) => this.WriteEvent(2, message);
     }
 }
--- sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/SdkVersionUtils.cs
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/SdkVersionUtils.cs
@@ -21,20 +21,40 @@ namespace OpenTelemetry.Exporter.AzureMonitor
 
         private static string GetSdkVersion()
         {
-            Version dotnetSdkVersion = GetVersion(typeof(object));
-            Version otelSdkVersion = GetVersion(typeof(OpenTelemetry.Sdk));
-            Version extensionVersion = GetVersion(typeof(OpenTelemetry.Exporter.AzureMonitor.AzureMonitorTraceExporter));
+            string sdkVer = null;
 
-            return string.Format(CultureInfo.InvariantCulture, $"dotnet{dotnetSdkVersion.ToString(2)}:otel{otelSdkVersion.ToString(3)}:ext{extensionVersion.ToString(3)}");
+            try
+            {
+                Version dotnetSdkVersion = GetVersion(typeof(object));
+                Version otelSdkVersion = GetVersion(typeof(OpenTelemetry.Sdk));
+                Version extensionVersion = GetVersion(typeof(OpenTelemetry.Exporter.AzureMonitor.AzureMonitorTraceExporter));
+
+                sdkVer = string.Format(CultureInfo.InvariantCulture, $"dotnet{dotnetSdkVersion.ToString(2)}:otel{otelSdkVersion.ToString(3)}:ext{extensionVersion.ToString(3)}");
+            }
+            catch (Exception ex)
+            {
+                AzureMonitorTraceExporterEventSource.Log.SdkVersionCreateFailed(ex);
+            }
+
+            return sdkVer;
         }
 
         private static Version GetVersion(Type type)
         {
+            string versionString = null;
+
+            try
+            {
             // TODO: Distinguish preview/stable release and minor versions. e.g: 5.0.0-preview.8.20365.13
-            var versionString = type.Assembly.GetCustomAttributes(false)
+            versionString = type.Assembly.GetCustomAttributes(false)
                                                 .OfType<AssemblyFileVersionAttribute>()
                                                 .First()
                                                 .Version;
+            }
+            catch (Exception ex)
+            {
+                AzureMonitorTraceExporterEventSource.Log.SdkVersionCreateFailed(ex);
+            }
 
             // Return zeros rather then failing if the version string fails to parse
             return Version.TryParse(versionString, out var version) ? version : new Version();
