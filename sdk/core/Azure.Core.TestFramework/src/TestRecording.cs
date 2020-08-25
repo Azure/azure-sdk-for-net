65fe59626cb16727bac457c0da2e31d50938ad08
Pavel Krymets <pavel@krymets.com>
2020-08-25 13:30:21 -0700

    Speedup retries in playback

--- sdk/core/Azure.Core.TestFramework/src/TestRecording.cs
+++ sdk/core/Azure.Core.TestFramework/src/TestRecording.cs
@@ -194,6 +194,12 @@ namespace Azure.Core.TestFramework
         public T InstrumentClientOptions<T>(T clientOptions) where T : ClientOptions
         {
             clientOptions.Transport = CreateTransport(clientOptions.Transport);
+            if (Mode == RecordedTestMode.Playback)
+            {
+                // Not making the timeout zero so retry code still goes async
+                clientOptions.Retry.Delay = TimeSpan.FromMilliseconds(10);
+                clientOptions.Retry.Mode = RetryMode.Fixed;
+            }
             return clientOptions;
         }
 
