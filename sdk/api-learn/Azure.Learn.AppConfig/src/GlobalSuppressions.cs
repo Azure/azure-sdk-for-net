dac00a9a69b47e9e01a153785f2accc03ac7357a
Anne Loomis Thompson <annelo@microsoft.com>
2020-08-25 09:00:05 -0700

    pr fb

---
+++ sdk/api-learn/Azure.Learn.AppConfig/src/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "AZC0012:Avoid single word type names", Justification = "Will fix as a step in tutorial.", Scope = "type", Target = "~T:Azure.Learn.AppConfig.Models.Enum4")]
+[assembly: SuppressMessage("Usage", "AZC0012:Avoid single word type names", Justification = "Will fix as a step in tutorial.", Scope = "type", Target = "~T:Azure.Learn.AppConfig.Models.Enum5")]
+[assembly: SuppressMessage("Usage", "AZC0012:Avoid single word type names", Justification = "Out of scope of tutorial.", Scope = "type", Target = "~T:Azure.Learn.AppConfig.Models.Key")]
+[assembly: SuppressMessage("Usage", "AZC0012:Avoid single word type names", Justification = "Out of scope of tutorial.", Scope = "type", Target = "~T:Azure.Learn.AppConfig.Models.Label")]
