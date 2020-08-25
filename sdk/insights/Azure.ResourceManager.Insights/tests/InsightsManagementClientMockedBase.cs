073febfa5b8d7c9b3c1be796b3e71b093fef5dde
m-nash <64171366+m-nash@users.noreply.github.com>
2020-08-24 17:16:34 -0700

    adding helper class and mocked base

---
+++ sdk/insights/Azure.ResourceManager.Insights/tests/InsightsManagementClientMockedBase.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Net;
+using System.Text;
+using Azure.Core.TestFramework;
+using Azure.Identity;
+
+namespace Azure.ResourceManager.Insights.Tests
+{
+    public class InsightsManagementClientMockedBase : InsightsManagementClientBase
+    {
+        protected InsightsManagementClientMockedBase(bool isAsync) : base(isAsync, RecordedTestMode.Live) { }
+
+        protected InsightsManagementClient GetInsightsManagementClient(string expectedResponse)
+        {
+            var options = new InsightsManagementClientOptions();
+            var mockResponse = new MockResponse((int)HttpStatusCode.OK);
+            mockResponse.SetContent(Encoding.UTF8.GetBytes(expectedResponse));
+            var transport = new MockTransport(mockResponse);
+            options.Transport = transport;
+            return new InsightsManagementClient(TestEnvironment.SubscriptionId, new DefaultAzureCredential(), options);
+        }
+    }
+}
