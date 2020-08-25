eb44e85dd8ff258c3f513e3156d0912a3ffd01f5
Pavel Krymets <pavel@krymets.com>
2020-08-25 08:09:34 -0700

    small progres

--- sdk/extensions/Azure.Extensions.WebJobs/samples/Function1.cs
+++ sdk/extensions/Azure.Extensions.WebJobs/samples/Function1.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Azure.Storage.Blobs;
 using System.Linq;
 
@@ -16,8 +15,7 @@ namespace Azure.Extensions.WebJobs.Sample
         [FunctionName("Function1")]
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            [AzureClient("AzureWebJobsStorage")] BlobServiceClient client,
-            ILogger log)
+            [AzureClient("AzureWebJobsStorage")] BlobServiceClient client)
         {
             return new OkObjectResult(client.GetBlobContainers().ToArray());
         }
