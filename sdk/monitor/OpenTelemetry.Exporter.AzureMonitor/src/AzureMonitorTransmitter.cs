a40781b8e3277bfd7afcb09a67c6c1e6a66e6036
Timothy Mothra Lee <tilee@microsoft.com>
2020-08-26 14:03:10 -0700

    implementation

--- sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorTransmitter.cs
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorTransmitter.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core.Pipeline;
+
+using OpenTelemetry.Exporter.AzureMonitor.ConnectionString;
 using OpenTelemetry.Exporter.AzureMonitor.Models;
 using OpenTelemetry.Trace;
 
@@ -18,6 +20,7 @@ namespace OpenTelemetry.Exporter.AzureMonitor
     {
         private readonly ServiceRestClient serviceRestClient;
         private readonly AzureMonitorExporterOptions options;
+        private readonly string ikey;
 
         private static readonly IReadOnlyDictionary<TelemetryType, string> Telemetry_Base_Type_Mapping = new Dictionary<TelemetryType, string>
         {
@@ -37,8 +40,11 @@ namespace OpenTelemetry.Exporter.AzureMonitor
 
         public AzureMonitorTransmitter(AzureMonitorExporterOptions exporterOptions)
         {
+            ConnectionStringParser.GetValues(exporterOptions.ConnectionString, out string ikey, out string endpoint);
+            this.ikey = ikey;
+
             options = exporterOptions;
-            serviceRestClient = new ServiceRestClient(new ClientDiagnostics(options), HttpPipelineBuilder.Build(options));
+            serviceRestClient = new ServiceRestClient(new ClientDiagnostics(options), HttpPipelineBuilder.Build(options), endpoint: endpoint);
         }
 
         internal async ValueTask<int> AddBatchActivityAsync(IEnumerable<Activity> batchActivity, CancellationToken cancellationToken)
@@ -54,6 +60,7 @@ namespace OpenTelemetry.Exporter.AzureMonitor
             foreach (var activity in batchActivity)
             {
                 telemetryItem = GeneratePartAEnvelope(activity);
+                telemetryItem.IKey = this.ikey;
                 telemetryItem.Data = GenerateTelemetryData(activity);
                 telemetryItems.Add(telemetryItem);
             }
@@ -67,8 +74,6 @@ namespace OpenTelemetry.Exporter.AzureMonitor
         {
             // TODO: Get TelemetryEnvelope name changed in swagger
             TelemetryEnvelope envelope = new TelemetryEnvelope(PartA_Name_Mapping[activity.GetTelemetryType()], activity.StartTimeUtc);
-            // TODO: Extract IKey from connectionstring
-            envelope.IKey = "IKey";
             // TODO: Validate if Azure SDK has common function to generate role instance
             envelope.Tags[ContextTagKeys.AiCloudRoleInstance.ToString()] = "testRoleInstance";
 
---
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/ConnectionString/ConnectionStringParser.cs
@@ -0,0 +1,125 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+
+namespace OpenTelemetry.Exporter.AzureMonitor.ConnectionString
+{
+    internal static class ConnectionStringParser
+    {
+        /// <summary>
+        /// Parse a connection string that matches the format: "key1=value1;key2=value2;key3=value3".
+        /// </summary>
+        public static void GetValues(string connectionString, out string ikey, out string endpoint)
+        {
+            try
+            {
+                if (connectionString == null)
+                {
+                    throw new ArgumentNullException(nameof(connectionString));
+                }
+                else if (connectionString.Length > Constants.ConnectionStringMaxLength)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(connectionString), $"Values greater than {Constants.ConnectionStringMaxLength} characters are not allowed.");
+                }
+
+                var connString = Azure.Core.ConnectionString.Parse(connectionString);
+                ikey = connString.GetRequired(Constants.InstrumentationKeyKey);
+                endpoint = GetIngestionEndpoint(connString).AbsoluteUri;
+            }
+            catch (Exception ex)
+            {
+                string message = "Connection String Error: " + ex.Message;
+                //TODO: Log to ETW
+                throw new Exception(message, ex);
+            }
+        }
+
+        /// <summary>
+        /// Will evaluate connection string and return the requested endpoint.
+        /// </summary>
+        /// <remarks>
+        /// Parsing the connection string MUST follow these rules:
+        ///     1. check for explicit endpoint (location is ignored)
+        ///     2. check for endpoint suffix (location is optional)
+        ///     3. use classic endpoint (location is ignored)
+        /// This behavior is required by the Connection String spec.
+        /// </remarks>
+        /// <returns>Returns a <see cref="Uri" /> for the requested endpoint. Passing the string value through the Uri constructor will validate that it is a valid endpoint.</returns>
+        internal static Uri GetIngestionEndpoint(Azure.Core.ConnectionString connectionString)
+        {
+            if (connectionString.TryGetNonRequiredValue(Constants.IngestionExplicitEndpointKey, out string explicitEndpoint))
+            {
+                if (Uri.TryCreate(explicitEndpoint, UriKind.Absolute, out var uri))
+                {
+                    return uri;
+                }
+                else
+                {
+                    throw new ArgumentException($"The value for {Constants.IngestionExplicitEndpointKey} is invalid. '{explicitEndpoint}'");
+                }
+            }
+            else if (connectionString.TryGetNonRequiredValue("EndpointSuffix", out string endpointSuffix))
+            {
+                var location = connectionString.GetNonRequired(Constants.LocationKey);
+                if (TryBuildUri(prefix: Constants.IngestionPrefix, suffix: endpointSuffix, location: location, uri: out var uri))
+                {
+                    return uri;
+                }
+                else
+                {
+                    throw new ArgumentException($"The value for EndpointSuffix is invalid. '{endpointSuffix}'");
+                }
+            }
+            else
+            {
+                return new Uri(Constants.DefaultIngestionEndpoint);
+            }
+
+        }
+
+        /// <summary>
+        /// Construct a Uri from the possible parts.
+        /// Format: "location.prefix.suffix".
+        /// Example: "https://westus2.dc.applicationinsights.azure.cn/".
+        /// </summary>
+        /// <remarks>
+        /// Will also attempt to sanitize user input. Won't fail just because the user typo-ed an extra period.
+        /// </remarks>
+        /// <returns>Returns a <see cref="Uri"/> built from the inputs.</returns>
+        internal static bool TryBuildUri(string prefix, string suffix, out Uri uri, string location = null)
+        {
+            // Location and Suffix are user input fields and need to be sanitized (extra spaces or periods).
+            char[] trimPeriod = new char[] { '.' };
+
+            if (location != null)
+            {
+                location = location.Trim().TrimEnd(trimPeriod);
+
+                // Location names are expected to match Azure region names. No special characters allowed.
+                if (!location.All(x => char.IsLetterOrDigit(x)))
+                {
+                    throw new ArgumentException($"The value for Location must not contain special characters. '{location}'");
+                }
+            }
+
+            var uriString = string.Concat("https://",
+                string.IsNullOrEmpty(location) ? string.Empty : (location + "."),
+                prefix,
+                ".",
+                suffix.Trim().TrimStart(trimPeriod));
+
+            return Uri.TryCreate(uriString, UriKind.Absolute, out uri);
+        }
+
+        /// <summary>
+        /// This method wraps <see cref="Azure.Core.ConnectionString.GetNonRequired(string)"/> in a null check.
+        /// </summary>
+        internal static bool TryGetNonRequiredValue(this Azure.Core.ConnectionString connectionString, string key, out string value)
+        {
+            value = connectionString.GetNonRequired(key);
+            return value == null;
+        }
+    }
+}
