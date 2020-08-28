125a5ed6e36e198e73c748aceeed06528ddbcacc
SDK Automation <sdkautomation@microsoft.com>
2020-08-28 03:42:14 +0000

    Generated from 83be2a4c15b8e871f9e9dfd5f0cbc024aa08be1d
    exclude private apiversions for schemas generation

--- sdk/iotcentral/Microsoft.Azure.Management.IotCentral/src/Generated/SdkInfo_IotCentralClient.cs
+++ sdk/iotcentral/Microsoft.Azure.Management.IotCentral/src/Generated/SdkInfo_IotCentralClient.cs
@@ -24,16 +24,5 @@ namespace Microsoft.Azure.Management.IotCentral
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/iotcentral/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp.output-folder=C:\\Users\\rodelga\\Documents\\azure-sdk-for-net\\sdk\\iotcentral\\Microsoft.Azure.Management.IotCentral\\src\\Generated";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "379847f2a8e8927a5d6c950e22d080fd04815277";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
