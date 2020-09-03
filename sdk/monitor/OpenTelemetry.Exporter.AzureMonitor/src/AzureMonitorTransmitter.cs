ae0f53588b8d61d2b663746f9ca1c79ae807ab77
Timothy Mothra Lee <tilee@microsoft.com>
2020-09-03 12:54:12 -0700

    cleanup

--- sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorTransmitter.cs
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorTransmitter.cs
@@ -11,7 +11,6 @@ using System.Threading.Tasks;
 using Azure.Core.Pipeline;
 
 using OpenTelemetry.Exporter.AzureMonitor.ConnectionString;
-using OpenTelemetry.Exporter.AzureMonitor.Extensions;
 using OpenTelemetry.Exporter.AzureMonitor.Models;
 using OpenTelemetry.Trace;
 
