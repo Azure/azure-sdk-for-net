385d2200a9831df3ab80111bfe393d91ebd77891
Mariana Rios Flores <mariari@microsoft.com>
2020-09-03 13:18:54 -0700

    [TA] Samples and documentation for Opinion Mining and Pii (#14829)
    * samples, readme + fix version
    
    * update links
    
    * update samples

--- sdk/textanalytics/Azure.AI.TextAnalytics/tests/samples/Sample5_RecognizePiiEntitiesAsync.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/tests/samples/Sample5_RecognizePiiEntitiesAsync.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Threading.Tasks;
 using Azure.AI.TextAnalytics.Tests;
 using Azure.Core.TestFramework;
 using NUnit.Framework;
@@ -12,19 +13,16 @@ namespace Azure.AI.TextAnalytics.Samples
     public partial class TextAnalyticsSamples : SamplesBase<TextAnalyticsTestEnvironment>
     {
         [Test]
-        public void RecognizePiiEntities()
+        public async Task RecognizePiiEntitiesAsync()
         {
             string endpoint = TestEnvironment.Endpoint;
             string apiKey = TestEnvironment.ApiKey;
 
-            #region Snippet:TextAnalyticsSample5CreateClient
             var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
-            #endregion
 
-            #region Snippet:RecognizePiiEntities
             string document = "A developer with SSN 859-98-0987 whose phone number is 800-102-1100 is building tools with our APIs.";
 
-            PiiEntityCollection entities = client.RecognizePiiEntities(document).Value;
+            PiiEntityCollection entities = await client.RecognizePiiEntitiesAsync(document);
 
             Console.WriteLine($"Redacted Text: {entities.RedactedText}");
             if (entities.Count > 0)
@@ -39,7 +37,6 @@ namespace Azure.AI.TextAnalytics.Samples
             {
                 Console.WriteLine("No entities were found.");
             }
-            #endregion
         }
     }
 }
