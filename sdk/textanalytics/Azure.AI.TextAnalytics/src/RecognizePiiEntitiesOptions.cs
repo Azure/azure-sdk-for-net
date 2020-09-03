07773ac83dbb6d95619e12ea5104259e0b5f4b1a
Mariana Rios Flores <mariari@microsoft.com>
2020-09-02 18:00:33 -0700

    [TA] Add PII back (#14794)
    * all pii code
    
    * tests
    
    * recordings
    
    * samples
    
    * pr feedback
    
    * add mocking
    
    * PR feedback

---
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/RecognizePiiEntitiesOptions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.AI.TextAnalytics
+{
+    /// <summary>
+    /// Options that allow callers to specify details about how the operation
+    /// is run and what information is returned from it by the service.
+    /// </summary>
+    public class RecognizePiiEntitiesOptions : TextAnalyticsRequestOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RecognizePiiEntitiesOptions"/>
+        /// class.
+        /// </summary>
+        public RecognizePiiEntitiesOptions()
+        {
+        }
+
+        /// <summary>
+        /// Filters the response entities to ones only included in the specified domain.
+        /// For more information see <a href="https://aka.ms/tanerpii"/>.
+        /// </summary>
+        public PiiEntityDomainType DomainFilter { get; set; }
+    }
+}
