654335a4a0c0eaeace90def0261a9b172bc4942d
Luyunmt <2534523917@qq.com>
2020-09-04 16:50:15 +0800

    change test code

--- sdk/identity/Azure.Identity/tests/DeviceCodeCredentialTests.cs
+++ sdk/identity/Azure.Identity/tests/DeviceCodeCredentialTests.cs
@@ -198,7 +198,6 @@ namespace Azure.Identity.Tests
                 return CreateTokenResponse(code, token);
 
             }
-            Assert.AreEqual("aaaa", $"{testEnvironment.AuthorityHostUrl}/organizations/oauth2/v2.0/devicecode");
             throw new InvalidOperationException();
         }
 
