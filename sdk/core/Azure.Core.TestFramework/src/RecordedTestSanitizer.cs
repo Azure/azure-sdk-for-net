29a8846bf125158d3cf36ce5488df82d5aeb3332
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-09-03 17:14:08 -0700

    Fix bug when sanitizing empty body

--- sdk/core/Azure.Core.TestFramework/src/RecordedTestSanitizer.cs
+++ sdk/core/Azure.Core.TestFramework/src/RecordedTestSanitizer.cs
@@ -66,7 +66,7 @@ namespace Azure.Core.TestFramework
 
         public virtual void SanitizeBody(RecordEntryMessage message)
         {
-            if (message.Body != null)
+            if (message.Body?.Length > 0)
             {
                 message.TryGetContentType(out string contentType);
 
