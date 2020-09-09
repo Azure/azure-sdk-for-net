52883ddf6ce4dc8410c63b00abbe20169e3945a5
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-09-09 12:25:41 -0700

    Reuse existing test

--- sdk/core/Azure.Core/tests/RecordSessionTests.cs
+++ sdk/core/Azure.Core/tests/RecordSessionTests.cs
@@ -23,6 +23,7 @@ namespace Azure.Core.Tests
         [TestCase("{\"json\" :\"value\"}", "application/json")]
         [TestCase("[\"json\", \"value\"]", "application/json")]
         [TestCase("[{\"json\":\"value\"}, {\"json\":\"value\"}]", "application/json")]
+        [TestCase("{\\u0002json\\u0002:{\\u0002json\\u0002:\\u0002value\\u0002}", "application/json")]
         [TestCase("invalid json", "application/json")]
         [TestCase("{ \"json\": \"value\" }", "unknown")]
         [TestCase("multi\rline", "application/xml")]
@@ -37,10 +38,16 @@ namespace Azure.Core.Tests
             session.Variables["a"] = "value a";
             session.Variables["b"] = "value b";
 
-            RecordEntry recordEntry = new RecordEntry();
+            var content = new Utf8JsonRequestContent();
+            content.JsonWriter.WriteStringValue(body);
+            var request = new MockRequest()
+            {
+                Content = content
+            };
+
+            RecordEntry recordEntry = RecordTransport.CreateEntry(request, new MockResponse(202));
             recordEntry.Request.Headers.Add("Content-Type", new[] { contentType });
             recordEntry.Request.Headers.Add("Other-Header", new[] { "multi", "value" });
-            recordEntry.Request.Body = bodyBytes;
             recordEntry.RequestUri = "url";
             recordEntry.RequestMethod = RequestMethod.Delete;
 
@@ -80,65 +87,8 @@ namespace Azure.Core.Tests
             CollectionAssert.AreEqual(new[] { contentType }, deserializedRecord.Response.Headers["content-type"]);
             CollectionAssert.AreEqual(new[] { "multi", "value" }, deserializedRecord.Response.Headers["other-response-header"]);
 
-            CollectionAssert.AreEqual(bodyBytes, deserializedRecord.Request.Body);
-            CollectionAssert.AreEqual(bodyBytes, deserializedRecord.Response.Body);
-        }
-
-        [Test]
-        public void CanRoundTripEncodedJson()
-        {
-            var body = "{\\u0002json\\u0002:{\\u0002json\\u0002:\\u0002value\\u0002}}";
-            var contentType = "application/json";
-            var session = new RecordSession();
-            session.Variables["a"] = "value a";
-            session.Variables["b"] = "value b";
-            var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteStringValue(body);
-            var request = new MockRequest()
-            {
-                Content = content
-            };
-
-            RecordEntry recordEntry = RecordTransport.CreateEntry(request, new MockResponse(200));
-            recordEntry.Request.Headers.Add("Content-Type", new[] { contentType });
-            recordEntry.Request.Headers.Add("Other-Header", new[] { "multi", "value" });
-            recordEntry.RequestUri = "url";
-            recordEntry.RequestMethod = RequestMethod.Put;
-
-            recordEntry.Response.Headers.Add("Content-Type", new[] { contentType });
-            recordEntry.Response.Headers.Add("Other-Response-Header", new[] { "multi", "value" });
-
-            session.Entries.Add(recordEntry);
-
-            var arrayBufferWriter = new ArrayBufferWriter<byte>();
-            using var jsonWriter = new Utf8JsonWriter(arrayBufferWriter, new JsonWriterOptions()
-            {
-                Indented = true
-            });
-            session.Serialize(jsonWriter);
-            jsonWriter.Flush();
-
-            TestContext.Out.WriteLine(Encoding.UTF8.GetString(arrayBufferWriter.WrittenMemory.ToArray()));
-
-            var document = JsonDocument.Parse(arrayBufferWriter.WrittenMemory);
-            var deserializedSession = RecordSession.Deserialize(document.RootElement);
-
-            Assert.AreEqual("value a", deserializedSession.Variables["a"]);
-            Assert.AreEqual("value b", deserializedSession.Variables["b"]);
-
-            RecordEntry deserializedRecord = deserializedSession.Entries.Single();
-
-            Assert.AreEqual(RequestMethod.Put, recordEntry.RequestMethod);
-            Assert.AreEqual("url", recordEntry.RequestUri);
-            Assert.AreEqual(200, recordEntry.StatusCode);
-
-            CollectionAssert.AreEqual(new[] { contentType }, deserializedRecord.Request.Headers["content-type"]);
-            CollectionAssert.AreEqual(new[] { "multi", "value" }, deserializedRecord.Request.Headers["other-header"]);
-
-            CollectionAssert.AreEqual(new[] { contentType }, deserializedRecord.Response.Headers["content-type"]);
-            CollectionAssert.AreEqual(new[] { "multi", "value" }, deserializedRecord.Response.Headers["other-response-header"]);
-
             CollectionAssert.AreEqual(recordEntry.Request.Body, deserializedRecord.Request.Body);
+            CollectionAssert.AreEqual(bodyBytes, deserializedRecord.Response.Body);
         }
 
         [Test]
