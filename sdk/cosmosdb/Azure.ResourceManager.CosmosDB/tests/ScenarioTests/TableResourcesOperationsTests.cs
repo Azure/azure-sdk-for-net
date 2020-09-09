57618536a7ea616d806bf35a9009e4298abe5332
YalinLi0312 <yall@microsoft.com>
2020-09-08 22:16:19 -0700

    Remove re-initialize CosmosDBClient in test file

--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/tests/ScenarioTests/TableResourcesOperationsTests.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/tests/ScenarioTests/TableResourcesOperationsTests.cs
@@ -46,8 +46,6 @@ namespace Azure.ResourceManager.CosmosDB.Tests
         [TestCase]
         public async Task TableCRUDTest()
         {
-            CosmosDBManagementClient cosmosDBMgmtClient = GetCosmosDBManagementClient();
-
             // prepare a database account
             List<Location> locationList = new List<Location>();
             locationList.Add(new Location(id: null, locationName: "WEST US", documentEndpoint: null, provisioningState: null, failoverPriority: null, isZoneRedundant: null));
@@ -55,47 +53,47 @@ namespace Azure.ResourceManager.CosmosDB.Tests
             databaseAccountsCreateOrUpdateParameters.Kind = DatabaseAccountKind.GlobalDocumentDB;
             databaseAccountsCreateOrUpdateParameters.Location = "WEST US";
             databaseAccountsCreateOrUpdateParameters.Capabilities.Add(new Capability("EnableTable"));
-            await WaitForCompletionAsync(await cosmosDBMgmtClient.DatabaseAccounts.StartCreateOrUpdateAsync(resourceGroupName, databaseAccountName, databaseAccountsCreateOrUpdateParameters));
-            Assert.AreEqual(200, cosmosDBMgmtClient.DatabaseAccounts.CheckNameExistsAsync(databaseAccountName).Result.Status);
+            await WaitForCompletionAsync(await CosmosDBManagementClient.DatabaseAccounts.StartCreateOrUpdateAsync(resourceGroupName, databaseAccountName, databaseAccountsCreateOrUpdateParameters));
+            Assert.AreEqual(200, CosmosDBManagementClient.DatabaseAccounts.CheckNameExistsAsync(databaseAccountName).Result.Status);
 
             var tableCreateUpdateParameters1 = new TableCreateUpdateParameters(new TableResource(tableName), new CreateUpdateOptions());
-            TableGetResults tableGetResults1 = (await WaitForCompletionAsync(await cosmosDBMgmtClient.TableResources.StartCreateUpdateTableAsync(resourceGroupName, databaseAccountName, tableName, tableCreateUpdateParameters1))).Value;
+            TableGetResults tableGetResults1 = (await WaitForCompletionAsync(await CosmosDBManagementClient.TableResources.StartCreateUpdateTableAsync(resourceGroupName, databaseAccountName, tableName, tableCreateUpdateParameters1))).Value;
             Assert.IsNotNull(tableGetResults1);
             Assert.AreEqual(tableName, tableGetResults1.Resource.Id);
 
-            TableGetResults tableGetResults2 = (await cosmosDBMgmtClient.TableResources.GetTableAsync(resourceGroupName, databaseAccountName, tableName)).Value;
+            TableGetResults tableGetResults2 = (await CosmosDBManagementClient.TableResources.GetTableAsync(resourceGroupName, databaseAccountName, tableName)).Value;
             Assert.IsNotNull(tableGetResults2);
             VerifyTables(tableGetResults1, tableGetResults2);
-            var actualThroughput = (await cosmosDBMgmtClient.TableResources.GetTableThroughputAsync(resourceGroupName, databaseAccountName, tableName)).Value.Resource.Throughput;
+            var actualThroughput = (await CosmosDBManagementClient.TableResources.GetTableThroughputAsync(resourceGroupName, databaseAccountName, tableName)).Value.Resource.Throughput;
             Assert.AreEqual(defaultThroughput, actualThroughput);
 
             var tableCreateUpdateParameters2 = new TableCreateUpdateParameters(new TableResource(tableName), new CreateUpdateOptions(sampleThroughput, new AutoscaleSettings()));
-            TableGetResults tableGetResults3 = (await WaitForCompletionAsync(await cosmosDBMgmtClient.TableResources.StartCreateUpdateTableAsync(resourceGroupName, databaseAccountName, tableName, tableCreateUpdateParameters2))).Value;
+            TableGetResults tableGetResults3 = (await WaitForCompletionAsync(await CosmosDBManagementClient.TableResources.StartCreateUpdateTableAsync(resourceGroupName, databaseAccountName, tableName, tableCreateUpdateParameters2))).Value;
             Assert.IsNotNull(tableGetResults3);
             Assert.AreEqual(tableName, tableGetResults3.Resource.Id);
-            actualThroughput = (await cosmosDBMgmtClient.TableResources.GetTableThroughputAsync(resourceGroupName, databaseAccountName, tableName)).Value.Resource.Throughput;
+            actualThroughput = (await CosmosDBManagementClient.TableResources.GetTableThroughputAsync(resourceGroupName, databaseAccountName, tableName)).Value.Resource.Throughput;
             Assert.AreEqual(sampleThroughput, actualThroughput);
 
-            TableGetResults tableGetResults4 = (await cosmosDBMgmtClient.TableResources.GetTableAsync(resourceGroupName, databaseAccountName, tableName)).Value;
+            TableGetResults tableGetResults4 = (await CosmosDBManagementClient.TableResources.GetTableAsync(resourceGroupName, databaseAccountName, tableName)).Value;
             Assert.IsNotNull(tableGetResults4);
             VerifyTables(tableGetResults3, tableGetResults4);
 
-            List<TableGetResults> tables = await cosmosDBMgmtClient.TableResources.ListTablesAsync(resourceGroupName, databaseAccountName).ToEnumerableAsync();
+            List<TableGetResults> tables = await CosmosDBManagementClient.TableResources.ListTablesAsync(resourceGroupName, databaseAccountName).ToEnumerableAsync();
             Assert.IsNotNull(tables);
             Assert.AreEqual(1, tables.Count);
             VerifyTables(tableGetResults4, tables[0]);
 
-            ThroughputSettingsGetResults throughputSettingsGetResults1 = await WaitForCompletionAsync(await cosmosDBMgmtClient.TableResources.StartMigrateTableToAutoscaleAsync(resourceGroupName, databaseAccountName, tableName));
+            ThroughputSettingsGetResults throughputSettingsGetResults1 = await WaitForCompletionAsync(await CosmosDBManagementClient.TableResources.StartMigrateTableToAutoscaleAsync(resourceGroupName, databaseAccountName, tableName));
             Assert.IsNotNull(throughputSettingsGetResults1.Resource.AutoscaleSettings);
             Assert.AreEqual(defaultMaxThroughput, throughputSettingsGetResults1.Resource.AutoscaleSettings.MaxThroughput);
             Assert.AreEqual(defaultThroughput, throughputSettingsGetResults1.Resource.Throughput);
 
-            ThroughputSettingsGetResults throughputSettingsGetResults2 = await WaitForCompletionAsync(await cosmosDBMgmtClient.TableResources.StartMigrateTableToManualThroughputAsync(resourceGroupName, databaseAccountName, tableName));
+            ThroughputSettingsGetResults throughputSettingsGetResults2 = await WaitForCompletionAsync(await CosmosDBManagementClient.TableResources.StartMigrateTableToManualThroughputAsync(resourceGroupName, databaseAccountName, tableName));
             Assert.IsNull(throughputSettingsGetResults2.Resource.AutoscaleSettings);
             Assert.AreEqual(defaultMaxThroughput, throughputSettingsGetResults2.Resource.Throughput);
 
             var throughputSettingsUpdateParameters = new ThroughputSettingsUpdateParameters(new ThroughputSettingsResource(defaultThroughput, null, null, null));
-            ThroughputSettingsGetResults throughputSettingsGetResults = (await WaitForCompletionAsync(await cosmosDBMgmtClient.TableResources.StartUpdateTableThroughputAsync(resourceGroupName, databaseAccountName, tableName, throughputSettingsUpdateParameters))).Value;
+            ThroughputSettingsGetResults throughputSettingsGetResults = (await WaitForCompletionAsync(await CosmosDBManagementClient.TableResources.StartUpdateTableThroughputAsync(resourceGroupName, databaseAccountName, tableName, throughputSettingsUpdateParameters))).Value;
             Assert.AreEqual(defaultThroughput, throughputSettingsGetResults.Resource.Throughput);
         }
 
