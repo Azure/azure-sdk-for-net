b96418a8141a55bddcac777c6479bd6a16b0f2f3
Pavel Krymets <pavel@krymets.com>
2020-09-10 11:24:09 -0700

    Add EventGrid WebJobs extension

---
+++ sdk/eventgrid/Microsoft.Azure.WebJobs.Extensions.EventGrid/tests/Common/LogMessage.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Extensions.EventGrid.Tests.Common
+{
+    public class LogMessage
+    {
+        public LogLevel Level { get; set; }
+
+        public EventId EventId { get; set; }
+
+        public IEnumerable<KeyValuePair<string, object>> State { get; set; }
+
+        public Exception Exception { get; set; }
+
+        public string FormattedMessage { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public override string ToString() => $"[{Timestamp.ToString("HH:mm:ss.fff")}] [{Category}] {FormattedMessage} {Exception}";
+    }
+}
