25fa6064244017287f45561703a0e8ed8d9565e6
Michael Yanni <MiYanni@microsoft.com>
2020-09-11 16:52:40 -0700

    Added example of sending via IEnumerable interface.

--- sdk/eventhub/Azure.Messaging.EventHubs/samples/Sample13_ReadEventsWithSerialization.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/samples/Sample13_ReadEventsWithSerialization.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Avro;
@@ -100,6 +101,87 @@ namespace Azure.Messaging.EventHubs.Samples
                 eventBatch.TryAdd(thirdEvent);
 
                 await producerClient.SendAsync(eventBatch);
+
+                Console.WriteLine("The event batch has been published.");
+            }
+
+            // Now that the events have been published, we'll read back all events from the Event Hub using a consumer client.
+            // It's important to note that because events are not removed from the partition when consuming, that if you're using
+            // an existing Event Hub for the sample, you will see events that were published prior to running this sample as well
+            // as those from the batch that we just sent.
+            //
+            // An Event Hub consumer is associated with a specific Event Hub and consumer group.  The consumer group is
+            // a label that identifies one or more consumers as a set.  Often, consumer groups are named after the responsibility
+            // of the consumer in an application, such as "Telemetry" or "OrderProcessing".  When an Event Hub is created, a default
+            // consumer group is created with it, called "$Default."
+            //
+            // Each consumer has a unique view of the events in a partition that it reads from, meaning that events are available to all
+            // consumers and are not removed from the partition when a consumer reads them.  This allows for one or more consumers to read and
+            // process events from the partition at different speeds and beginning with different events without interfering with
+            // one another.
+            //
+            // When events are published, they will continue to exist in the partition and be available for consuming until they
+            // reach an age where they are older than the retention period.
+            // (see: https://docs.microsoft.com/en-us/azure/event-hubs/event-hubs-faq#what-is-the-maximum-retention-period-for-events)
+            //
+            // In this example, we will create our consumer client using the default consumer group that is created with an Event Hub.
+            // Our consumer will begin watching the partition at the very end, reading only new events that we will publish for it.
+
+            await using (var consumerClient = new EventHubConsumerClient(EventHubConsumerClient.DefaultConsumerGroupName, fullyQualifiedNamespace, eventHubName, credential))
+            {
+                // To ensure that we do not wait for an indeterminate length of time, we'll stop reading after we receive three events.  For a
+                // fresh Event Hub, those will be the three that we had published.  We'll also ask for cancellation after 30 seconds, just to be
+                // safe.
+
+                using CancellationTokenSource cancellationSource = new CancellationTokenSource();
+                cancellationSource.CancelAfter(TimeSpan.FromSeconds(90));
+
+                int eventsRead = 0;
+                int maximumEvents = 3;
+
+                await foreach (PartitionEvent partitionEvent in consumerClient.ReadEventsAsync(cancellationSource.Token))
+                {
+                    BinaryData binaryData = partitionEvent.Data.BodyAsBinary;
+                    GenericRecord eventRecord = binaryData.ToObject<GenericRecord>(avroSerializer);
+                    string messageText = (string)eventRecord["Message"];
+                    Console.WriteLine($"Event Read: { messageText }");
+                    eventsRead++;
+
+                    if (eventsRead >= maximumEvents)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            // At this point, our clients have both passed their "using" scopes and have safely been disposed of.  We
+            // have no further obligations.
+
+            Console.WriteLine();
+
+            // To start, we'll publish a small number of events using a producer client.  To ensure that our client is appropriately closed, we'll
+            // take advantage of the asynchronous dispose when we are done or when an exception is encountered.
+
+            await using (var producerClient = new EventHubProducerClient(fullyQualifiedNamespace, eventHubName, credential))
+            {
+                List<EventData> events = new List<EventData>();
+
+                GenericRecord firstEventRecord = new GenericRecord(exampleSchema);
+                firstEventRecord.Add("Message", "Hello, Event Hubs!");
+                EventData firstEvent = new EventData(new BinaryData(firstEventRecord, avroSerializer, typeof(GenericRecord)));
+                events.Add(firstEvent);
+
+                GenericRecord secondEventRecord = new GenericRecord(exampleSchema);
+                secondEventRecord.Add("Message", "The middle event is this one");
+                EventData secondEvent = new EventData(new BinaryData(secondEventRecord, avroSerializer, typeof(GenericRecord)));
+                events.Add(secondEvent);
+
+                GenericRecord thirdEventRecord = new GenericRecord(exampleSchema);
+                thirdEventRecord.Add("Message", "Goodbye, Event Hubs!");
+                EventData thirdEvent = new EventData(new BinaryData(thirdEventRecord, avroSerializer, typeof(GenericRecord)));
+                events.Add(thirdEvent);
+
+                await producerClient.SendAsync(events);
 
                 Console.WriteLine("The event batch has been published.");
             }
