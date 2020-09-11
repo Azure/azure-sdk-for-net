1e8956845bf06b34636e37ef6daf5e0e18e063c0
George Christoglou <gechris@microsoft.com>
2020-09-11 22:02:26 +0300

    Add tests for FlexibleServers APIs

--- sdk/postgresql/Microsoft.Azure.Management.PostgreSQL/tests/ScenarioTests/CRUDDBForPostgreSQLFlexibleServerTestBase.cs
+++ sdk/postgresql/Microsoft.Azure.Management.PostgreSQL/tests/ScenarioTests/CRUDDBForPostgreSQLFlexibleServerTestBase.cs
@@ -1,42 +1,39 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
-
 using PostgreSQL.Tests.Helpers;
-using Microsoft.Azure.Management.PostgreSQL;
-using Microsoft.Azure.Management.PostgreSQL.Models;
 using Microsoft.Azure.Management.ResourceManager;
 using Microsoft.Azure.Management.ResourceManager.Models;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Microsoft.Azure.Management.PostgreSQL.FlexibleServers;
+using Microsoft.Azure.Management.PostgreSQL.FlexibleServers.Models;
 
-namespace PostgreSQL.Tests.ScenarioTests
+namespace Microsoft.Azure.Management.PostgreSQL.Tests.ScenarioTests
 {
-    public class CRUDPostgreSQLTestsBase : TestBase
+    public class CRUDDBForPostgreSQLFlexibleServerTestBase
     {
         protected static string ResourceGroupName;
         protected static string ServerName;
-        protected static string DmsProjectName;
-        protected static string DmsTaskName;
-        
-        public CRUDPostgreSQLTestsBase()
+
+        public CRUDDBForPostgreSQLFlexibleServerTestBase()
         {
             ResourceGroupName = "pgsdkrg";
-            ServerName = "pgsdkserver";
+            ServerName = "pgsdkflexserver";
         }
 
-        protected Server CreatePostgreSQLInstance(MockContext context,
-            PostgreSQLManagementClient client,
+        protected Server CreatePostgreSQLFlexibleServersInstance(MockContext context,
+            FlexibleServers.PostgreSQLManagementClient client,
             ResourceGroup resourceGroup,
             string serverName)
         {
             return client.Servers.Create(
                 resourceGroup.Name,
                 serverName,
-                new ServerForCreate(
-                    properties: new ServerPropertiesForDefaultCreate(
-                        administratorLogin: "testUser",
-                        administratorLoginPassword: "testPassword1!"),
-                    location: resourceGroup.Location,
-                    sku: new Microsoft.Azure.Management.PostgreSQL.Models.Sku(name: "B_Gen5_1")));
+                new Server(location: resourceGroup.Location,
+                            sku: new FlexibleServers.Models.Sku("Standard_D4s_v3", "GeneralPurpose"),
+                            administratorLogin: "testUser",
+                            administratorLoginPassword: "testPassword1!",
+                            version: "12",
+                            storageProfile: new StorageProfile(storageMB: 524288)));
         }
 
         protected ResourceGroup CreateResourceGroup(MockContext context,
@@ -72,4 +69,3 @@ namespace PostgreSQL.Tests.ScenarioTests
         }
     }
 }
-
