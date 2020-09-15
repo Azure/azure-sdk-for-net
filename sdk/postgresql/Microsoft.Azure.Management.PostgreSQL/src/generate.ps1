b5825fcb98ed1e45955a44e4a47c416169ef71cb
gechris <69321460+gechris@users.noreply.github.com>
2020-09-15 11:35:43 +0300

    Generate PostgreSQL SDKs 0.9.5 public preview (#15115)
    * Generate PostgreSQL SDKs and tests
    
    * updating release notes for PostgreSQL
    
    Co-authored-by: Raj Sellappan <rajsell@microsoft.com>

--- sdk/postgresql/Microsoft.Azure.Management.PostgreSQL/src/generate.ps1
+++ sdk/postgresql/Microsoft.Azure.Management.PostgreSQL/src/generate.ps1
@@ -1,1 +1,2 @@
-Start-AutoRestCodeGeneration -ResourceProvider "postgresql/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "postgresql/resource-manager" -AutoRestVersion "v2" -SdkGenerationDirectory "$PSScriptRoot\postgresql\Generated"
+Start-AutoRestCodeGeneration -ResourceProvider "postgresql/resource-manager" -AutoRestVersion "v2" -SdkGenerationDirectory "$PSScriptRoot\postgresqlflexibleservers\Generated" -ConfigFileTag "package-2020-02-14-privatepreview" -Namespace "Microsoft.Azure.Management.PostgreSQL.FlexibleServers"
