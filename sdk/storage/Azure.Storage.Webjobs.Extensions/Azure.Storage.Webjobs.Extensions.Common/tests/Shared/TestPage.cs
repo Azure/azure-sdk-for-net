96f1337b8d492f60b973ab8d2cbb522bf654b398
Kamil Sobol <kasobol@microsoft.com>
2020-09-17 18:24:45 -0700

    first test moved.

---
+++ sdk/storage/Azure.Storage.Webjobs.Extensions/Azure.Storage.Webjobs.Extensions.Common/tests/Shared/TestPage.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+using Azure;
+
+namespace Azure.WebJobs.Extensions.Storage.Common.Tests
+{
+    public class TestPage<T> : Page<T>
+    {
+        private IEnumerable<T> enumerable;
+        private string continuationToken;
+
+        public TestPage(IEnumerable<T> enumerable, string continuationToken = null)
+        {
+            this.enumerable = enumerable;
+            this.continuationToken = continuationToken;
+        }
+
+        public override IReadOnlyList<T> Values => this.enumerable.ToList();
+
+        public override string ContinuationToken => continuationToken;
+
+        public override Response GetRawResponse()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
