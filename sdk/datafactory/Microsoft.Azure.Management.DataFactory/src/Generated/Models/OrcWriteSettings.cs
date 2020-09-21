f50da3bbee03dc2863ef14e2440d5afd8cc1cedd
SDK Automation <sdkautomation@microsoft.com>
2020-09-21 05:24:06 +0000

    Generated from c32e3a4efe6c5c4638109d8cda3225a459ee80da

--- sdk/datafactory/Microsoft.Azure.Management.DataFactory/src/Generated/Models/OrcWriteSettings.cs
+++ sdk/datafactory/Microsoft.Azure.Management.DataFactory/src/Generated/Models/OrcWriteSettings.cs
@@ -10,35 +10,29 @@
 
 namespace Microsoft.Azure.Management.DataFactory.Models
 {
-    using Microsoft.Rest;
     using Newtonsoft.Json;
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
 
     /// <summary>
-    /// Delimited text write settings.
+    /// Orc write settings.
     /// </summary>
-    public partial class DelimitedTextWriteSettings : FormatWriteSettings
+    public partial class OrcWriteSettings : FormatWriteSettings
     {
         /// <summary>
-        /// Initializes a new instance of the DelimitedTextWriteSettings class.
+        /// Initializes a new instance of the OrcWriteSettings class.
         /// </summary>
-        public DelimitedTextWriteSettings()
+        public OrcWriteSettings()
         {
             CustomInit();
         }
 
         /// <summary>
-        /// Initializes a new instance of the DelimitedTextWriteSettings class.
+        /// Initializes a new instance of the OrcWriteSettings class.
         /// </summary>
-        /// <param name="fileExtension">The file extension used to create the
-        /// files. Type: string (or Expression with resultType string).</param>
         /// <param name="additionalProperties">Unmatched properties from the
         /// message are deserialized this collection</param>
-        /// <param name="quoteAllText">Indicates whether string values should
-        /// always be enclosed with quotes. Type: boolean (or Expression with
-        /// resultType boolean).</param>
         /// <param name="maxRowsPerFile">Limit the written file's row count to
         /// be smaller than or equal to the specified count. Type: integer (or
         /// Expression with resultType integer).</param>
@@ -46,11 +40,9 @@ namespace Microsoft.Azure.Management.DataFactory.Models
         /// &lt;fileNamePrefix&gt;_&lt;fileIndex&gt;.&lt;fileExtension&gt; when
         /// copy from non-file based store without partitionOptions. Type:
         /// string (or Expression with resultType string).</param>
-        public DelimitedTextWriteSettings(object fileExtension, IDictionary<string, object> additionalProperties = default(IDictionary<string, object>), object quoteAllText = default(object), object maxRowsPerFile = default(object), object fileNamePrefix = default(object))
+        public OrcWriteSettings(IDictionary<string, object> additionalProperties = default(IDictionary<string, object>), object maxRowsPerFile = default(object), object fileNamePrefix = default(object))
             : base(additionalProperties)
         {
-            QuoteAllText = quoteAllText;
-            FileExtension = fileExtension;
             MaxRowsPerFile = maxRowsPerFile;
             FileNamePrefix = fileNamePrefix;
             CustomInit();
@@ -60,21 +52,6 @@ namespace Microsoft.Azure.Management.DataFactory.Models
         /// An initialization method that performs custom operations like setting defaults
         /// </summary>
         partial void CustomInit();
-
-        /// <summary>
-        /// Gets or sets indicates whether string values should always be
-        /// enclosed with quotes. Type: boolean (or Expression with resultType
-        /// boolean).
-        /// </summary>
-        [JsonProperty(PropertyName = "quoteAllText")]
-        public object QuoteAllText { get; set; }
-
-        /// <summary>
-        /// Gets or sets the file extension used to create the files. Type:
-        /// string (or Expression with resultType string).
-        /// </summary>
-        [JsonProperty(PropertyName = "fileExtension")]
-        public object FileExtension { get; set; }
 
         /// <summary>
         /// Gets or sets limit the written file's row count to be smaller than
@@ -93,18 +70,5 @@ namespace Microsoft.Azure.Management.DataFactory.Models
         [JsonProperty(PropertyName = "fileNamePrefix")]
         public object FileNamePrefix { get; set; }
 
-        /// <summary>
-        /// Validate the object.
-        /// </summary>
-        /// <exception cref="ValidationException">
-        /// Thrown if validation fails
-        /// </exception>
-        public virtual void Validate()
-        {
-            if (FileExtension == null)
-            {
-                throw new ValidationException(ValidationRules.CannotBeNull, "FileExtension");
-            }
-        }
     }
 }
