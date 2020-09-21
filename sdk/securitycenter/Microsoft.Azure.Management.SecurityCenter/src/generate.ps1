b260bf366ea4b02514c91fba7b4cf0e9d2b355ad
Elia Grady <elia.grady@microsoft.com>
2020-09-21 12:18:42 +0300

    autogenerated code using autorest
    add autogenerated metadata

--- sdk/securitycenter/Microsoft.Azure.Management.SecurityCenter/src/generate.ps1
+++ sdk/securitycenter/Microsoft.Azure.Management.SecurityCenter/src/generate.ps1
@@ -1,1 +1,6 @@
-Start-AutoRestCodeGeneration -ResourceProvider "security/resource-manager" -AutoRestVersion "v2"
+# Running Start-AutoRestCodeGeneration will overwrite the files present in SdkGenerationDirectory based on AutoRestCodeGenerationFlags
+# As ARM has multiple tag packages, it is safer to first Generate the files in a different directory 
+# and copy these new set of files to the main Generated folder. This way exisiting files will not be deleted.
+
+# Generate package with resources tag
+Start-AutoRestCodeGeneration -ResourceProvider "security/resource-manager" -SdkRepoRootPath "$PSScriptRoot\..\..\..\.." -AutoRestVersion "v2" -SdkGenerationDirectory "$PSScriptRoot\Generated\Resources"
