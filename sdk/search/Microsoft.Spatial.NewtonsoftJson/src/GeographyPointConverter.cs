a6fc7bd7dcf715a951923d2ede9b923cf07a32da
Heath Stewart <heaths@microsoft.com>
2020-09-21 17:44:54 -0700

    Add Json.NET converters for Microsoft.Spatial
    Resolves #13165

---
+++ sdk/search/Microsoft.Spatial.NewtonsoftJson/src/GeographyPointConverter.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Newtonsoft.Json;
+
+namespace Microsoft.Spatial.Serialization
+{
+    /// <summary>
+    /// Converts between <see cref="GeographyPoint" /> objects and Geo-JSON points.
+    /// </summary>
+    public class GeographyPointConverter : JsonConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType) =>
+            typeof(GeographyPoint).IsAssignableFrom(objectType);
+
+        /// <inheritdoc/>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) =>
+            reader.ReadGeoJsonPoint();
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) =>
+            writer.WriteGeoJsonPoint((GeographyPoint)value);
+    }
+}
---
+++ sdk/search/Microsoft.Spatial.NewtonsoftJson/tests/GeographyPointConverterTests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Spatial.Serialization;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.IO;
+
+namespace Microsoft.Spatial.NewtonsoftJson.Tests
+{
+    public class GeographyPointConverterTests
+    {
+        [Test]
+        public void CanConvert()
+        {
+            GeographyPointConverter converter = new GeographyPointConverter();
+            Assert.IsTrue(converter.CanConvert(typeof(GeographyPoint)));
+        }
+
+        [Test]
+        public void ReadJson()
+        {
+            JsonSerializer serializer = JsonSerializer.CreateDefault();
+            serializer.Converters.Add(new GeographyPointConverter());
+
+            using StringReader reader = new StringReader(@"{""type"":""Point"",""coordinates"":[-121.726906,46.879967]}");
+            using JsonReader jsonReader = new JsonTextReader(reader);
+            GeographyPoint point = serializer.Deserialize<GeographyPoint>(jsonReader);
+
+            Assert.AreEqual(point.Latitude, 46.879967);
+            Assert.AreEqual(point.Longitude, -121.726906);
+        }
+
+        [Test]
+        public void WriteJson()
+        {
+            GeographyPoint point = GeographyPoint.Create(46.879967, -121.726906);
+
+            JsonSerializer serializer = JsonSerializer.CreateDefault();
+            serializer.Converters.Add(new GeographyPointConverter());
+
+            using StringWriter writer = new StringWriter();
+            serializer.Serialize(writer, point);
+
+            Assert.AreEqual(@"{""type"":""Point"",""coordinates"":[-121.726906,46.879967]}", writer.ToString());
+        }
+    }
+}
