03a1a424592cda35a120e106ef4fe05fefd8b15f
SDK Automation <sdkautomation@microsoft.com>
2020-09-22 03:17:52 +0000

    Generated from da62b471cdc79742d8e0307bbb733c543c8794ee

--- sdk/keyvault/Microsoft.Azure.Management.KeyVault/src/Generated/IKeyVaultManagementClient.cs
+++ sdk/keyvault/Microsoft.Azure.Management.KeyVault/src/Generated/IKeyVaultManagementClient.cs
@@ -49,6 +49,11 @@ namespace Microsoft.Azure.Management.KeyVault
         string SubscriptionId { get; set; }
 
         /// <summary>
+        /// Client Api Version.
+        /// </summary>
+        string ApiVersion { get; }
+
+        /// <summary>
         /// The preferred language for the response.
         /// </summary>
         string AcceptLanguage { get; set; }
@@ -86,11 +91,6 @@ namespace Microsoft.Azure.Management.KeyVault
         /// Gets the IOperations.
         /// </summary>
         IOperations Operations { get; }
-
-        /// <summary>
-        /// Gets the IManagedHsmsOperations.
-        /// </summary>
-        IManagedHsmsOperations ManagedHsms { get; }
 
     }
 }
