49d3db7b9dd9b31ca1fc650422829cff755b5948
TeBeCo <tebeco@gmail.com>
2020-09-23 17:33:57 +0200

    Summary comments were inverted (#15363)

--- sdk/extensions/Azure.Extensions.AspNetCore.Configuration.Secrets/src/KeyVaultSecretManager.cs
+++ sdk/extensions/Azure.Extensions.AspNetCore.Configuration.Secrets/src/KeyVaultSecretManager.cs
@@ -14,21 +14,22 @@ namespace Azure.Extensions.AspNetCore.Configuration.Secrets
     {
         internal static KeyVaultSecretManager Instance { get; } = new KeyVaultSecretManager();
 
-        /// <summary>
-        /// Checks if <see cref="KeyVaultSecret"/> value should be retrieved.
-        /// </summary>
-        /// <param name="secret">The <see cref="KeyVaultSecret"/> instance.</param>
-        /// <returns><code>true</code> if secrets value should be loaded, otherwise <code>false</code>.</returns>
-        public virtual string GetKey(KeyVaultSecret secret)
-        {
-            return secret.Name.Replace("--", ConfigurationPath.KeyDelimiter);
-        }
 
         /// <summary>
         /// Maps secret to a configuration key.
         /// </summary>
         /// <param name="secret">The <see cref="KeyVaultSecret"/> instance.</param>
         /// <returns>Configuration key name to store secret value.</returns>
+        public virtual string GetKey(KeyVaultSecret secret)
+        {
+            return secret.Name.Replace("--", ConfigurationPath.KeyDelimiter);
+        }
+
+        /// <summary>
+        /// Checks if <see cref="KeyVaultSecret"/> value should be retrieved.
+        /// </summary>
+        /// <param name="secret">The <see cref="SecretProperties"/> instance.</param>
+        /// <returns><code>true</code> if secrets value should be loaded, otherwise <code>false</code>.</returns>
         public virtual bool Load(SecretProperties secret)
         {
             return true;
