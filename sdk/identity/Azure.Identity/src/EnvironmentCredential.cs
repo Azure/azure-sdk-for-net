6ed1b6627b800b0f5e4370cb1f912f4e8044d437
Alexander Sher <AlexanderSher@users.noreply.github.com>
2020-09-23 13:09:43 -0500

    Fix #14553: Incorrect term used in EnvironmentCredential summary (#15369)
    * Fix #14553: Incorrect term used in EnvironmentCredential summary
    
    * Change description

--- sdk/identity/Azure.Identity/src/EnvironmentCredential.cs
+++ sdk/identity/Azure.Identity/src/EnvironmentCredential.cs
@@ -18,7 +18,7 @@ namespace Azure.Identity
     /// <item><term>AZURE_TENANT_ID</term><description>The Azure Active Directory tenant(directory) ID.</description></item>
     /// <item><term>AZURE_CLIENT_ID</term><description>The client(application) ID of an App Registration in the tenant.</description></item>
     /// <item><term>AZURE_CLIENT_SECRET</term><description>A client secret that was generated for the App Registration.</description></item>
-    /// <item><term>AZURE_CLIENT_CERTIFICATE_LOCATION</term><description>A path to the certificate that was generate for the App Registration.</description></item>
+    /// <item><term>AZURE_CLIENT_CERTIFICATE_PATH</term><description>A path to certificate and private key pair in PEM or PFX format, which can authenticate the App Registration.</description></item>
     /// <item><term>AZURE_USERNAME</term><description>The username, also known as upn, of an Azure Active Directory user account.</description></item>
     /// <item><term>AZURE_PASSWORD</term><description>The password of the Azure Active Directory user account. Note this does not support accounts with MFA enabled.</description></item>
     /// </list>
