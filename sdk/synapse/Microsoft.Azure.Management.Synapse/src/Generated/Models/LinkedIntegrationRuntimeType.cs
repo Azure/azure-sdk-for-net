7a6761dd1e5b239b812413b6e09939fb64ebab93
SDK Automation <sdkautomation@microsoft.com>
2020-09-23 12:30:54 +0000

    Generated from 200da78a25d3cf0fdb9ea8cfecd69d038a953928
    rename synapse model in java

--- sdk/synapse/Microsoft.Azure.Management.Synapse/src/Generated/Models/LinkedIntegrationRuntimeType.cs
+++ sdk/synapse/Microsoft.Azure.Management.Synapse/src/Generated/Models/LinkedIntegrationRuntimeType.cs
@@ -10,13 +10,11 @@
 
 namespace Microsoft.Azure.Management.Synapse.Models
 {
-    using Newtonsoft.Json;
     using System.Linq;
 
     /// <summary>
     /// The base definition of a linked integration runtime.
     /// </summary>
-    [Newtonsoft.Json.JsonObject("LinkedIntegrationRuntimeType")]
     public partial class LinkedIntegrationRuntimeType
     {
         /// <summary>
