f6b35faf9721da44f5aa25c4750046b028951b6d
Youri <v-lipyo@microsoft.com>
2020-09-25 10:56:49 +0800

    Add SessionRecords

--- sdk/insights/Azure.ResourceManager.Insights/tests/BasicTests/AlertsTests.cs
+++ sdk/insights/Azure.ResourceManager.Insights/tests/BasicTests/AlertsTests.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.Insights.Models;
--- sdk/insights/Azure.ResourceManager.Insights/tests/BasicTests/AutoscaleTests.cs
+++ sdk/insights/Azure.ResourceManager.Insights/tests/BasicTests/AutoscaleTests.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Threading.Tasks;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.Insights.Models;
