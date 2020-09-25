acda28957d23d70f42333481625977ffc9a07ac2
Timothy Mothra Lee <tilee@microsoft.com>
2020-09-24 18:03:49 -0700

    demo tests working with webapp

--- sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/tests/OpenTelemetry.Exporter.AzureMonitor.IntegrationTests/BasicTests.cs
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/tests/OpenTelemetry.Exporter.AzureMonitor.IntegrationTests/BasicTests.cs
@@ -3,19 +3,53 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.Testing;
 
 using Xunit;
 
 namespace OpenTelemetry.Exporter.AzureMonitor.IntegrationTests
 {
-    public class BasicTests
+    public class BasicTests : IClassFixture<WebApplicationFactory<WebApp.Startup>>
     {
-        [Fact]
-        public void Test()
+        private readonly WebApplicationFactory<WebApp.Startup> factory;
+
+        public BasicTests(WebApplicationFactory<WebApp.Startup> factory)
         {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task VerifyRequestSuccess()
+        {
+            // Arrange
+            var client = this.factory.CreateClient();
+            var request = new Uri(client.BaseAddress, "api/values");
+
+            // Act
+            var response = await client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
             Assert.True(true);
+        }
+
+        [Fact]
+        public async Task VerifyRequestFail()
+        {
+            // Arrange
+            var client = this.factory.CreateClient();
+            var request = new Uri(client.BaseAddress, "api/fail");
+
+            // Act
+            var response = await client.GetAsync(request);
+
+            // Assert
+            Assert.Throws<HttpRequestException>(() => response.EnsureSuccessStatusCode());
         }
     }
 }
