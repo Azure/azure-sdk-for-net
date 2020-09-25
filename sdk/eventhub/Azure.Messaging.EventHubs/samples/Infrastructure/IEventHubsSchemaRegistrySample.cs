62a5a722edac45dbf987aeb94f629a33fe6e4c76
Michael Yanni <MiYanni@microsoft.com>
2020-09-25 14:18:06 -0700

    EventHubs - SchemaRegistry (ObjectSerializer via BinaryData) integration (#15433)
    * Implementation and sample of first proposal.
    
    * Added example of sending via IEnumerable interface.
    
    * Added the beta 2 SR package and the sample worked!
    
    * Updated SR sample and sample processing to work with the new sample type.
    
    * Added comment to Experimental package reference.
    
    * Moved BodyAsBinaryData declaration and used the better description text.
    
    * Changed Body and BodyAsStream to use expression-bodied property declaration.
    
    * Aligned parameter declarations and usage.
    
    * Added VersionOverride comments.
    
    * Updated samples to not display the sub-menu for SchemaRegistry samples.
    
    * Forgot to regenerate API.
    
    * Fix whitespacing.

---
+++ sdk/eventhub/Azure.Messaging.EventHubs/samples/Infrastructure/IEventHubsSchemaRegistrySample.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+
+namespace Azure.Messaging.EventHubs.Samples.Infrastructure
+{
+    /// <summary>
+    ///   Provides a well-known means of executing an EventHubsClient using Azure.Data.SchemaRegistry serialization integration.
+    /// </summary>
+    ///
+    /// <seealso href="https://github.com/Azure/azure-sdk-for-net/tree/master/sdk/schemaregistry/Azure.Data.SchemaRegistry" />
+    ///
+    public interface IEventHubsSchemaRegistrySample : ISample
+    {
+        /// <summary>
+        ///   Allows for executing the sample.
+        /// </summary>
+        ///
+        /// <param name="fullyQualifiedNamespace"> The fully qualified Event Hubs namespace.  This is likely to be similar to <c>{yournamespace}.servicebus.windows.net</c>.</param>
+        /// <param name="eventHubName">The name of the Event Hub, sometimes known as its path, that the sample should run against.</param>
+        /// <param name="schemaGroupName">The name of the schema group in the Schema Registry.</param>
+        /// <param name="tenantId">The Azure Active Directory tenant that holds the service principal.</param>
+        /// <param name="clientId">The Azure Active Directory client identifier of the service principal.</param>
+        /// <param name="secret">The Azure Active Directory secret of the service principal.</param>
+        ///
+        Task RunAsync(string fullyQualifiedNamespace,
+                      string eventHubName,
+                      string schemaGroupName,
+                      string tenantId,
+                      string clientId,
+                      string secret);
+    }
+}
