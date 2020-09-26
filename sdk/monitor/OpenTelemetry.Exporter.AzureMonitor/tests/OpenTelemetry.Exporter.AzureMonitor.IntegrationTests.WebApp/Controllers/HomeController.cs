14a70917c516f6e8559c85f6e1a0231f6f4cf385
Timothy Mothra Lee <tilee@microsoft.com>
2020-09-25 23:04:22 -0700

    extra controller

---
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/tests/OpenTelemetry.Exporter.AzureMonitor.IntegrationTests.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Net;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenTelemetry.Exporter.AzureMonitor.IntegrationTests.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET api/home
+        [HttpGet]
+        public ActionResult Get() => StatusCode((int)HttpStatusCode.OK);
+    }
+}
