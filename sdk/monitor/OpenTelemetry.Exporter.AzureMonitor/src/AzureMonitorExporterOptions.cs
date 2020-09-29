3c20c1df40484c8fb7ac4f1ee1ee2d71a4cf8a35
Timothy Mothra Lee <tilee@microsoft.com>
2020-09-29 14:36:40 -0700

    cleanup

--- sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorExporterOptions.cs
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorExporterOptions.cs
@@ -1,15 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-
-using Azure;
 using Azure.Core;
-
-using OpenTelemetry.Exporter.AzureMonitor.Models;
 
 namespace OpenTelemetry.Exporter.AzureMonitor
 {
