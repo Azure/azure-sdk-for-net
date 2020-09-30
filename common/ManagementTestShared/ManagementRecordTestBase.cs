3c1cefd4ab5c35f3c80de15f2b39ae829d36301a
bqauntump <stevens@microsoft.com>
2020-09-30 16:58:25 -0700

    WIP: white space

--- common/ManagementTestShared/ManagementRecordTestBase.cs
+++ common/ManagementTestShared/ManagementRecordTestBase.cs
@@ -1,11 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-
 using Azure.Core;
 using Azure.Core.TestFramework;
-#if RESOURCES_RP
-using Azure.ResourceManager.Resources;	
+#if RESOURCES_RPs
+using Azure.ResourceManager.Resources;
 #else
 using Azure.Management.Resources;
 #endif
