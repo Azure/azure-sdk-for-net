8fbd07c180e2fbd498d3a380681e121cce814802
Timothy Mothra Lee <tilee@microsoft.com>
2020-10-01 10:49:26 -0700

    remove Microsoft.AspNetCore.App to support net461

---
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/tests/OpenTelemetry.Exporter.AzureMonitor.IntegrationTests/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace OpenTelemetry.Exporter.AzureMonitor.Integration.Tests.WebApp
+{
+#if NET461
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+#endif
+}
