9b41713aa0a286dcde32118dcb1707f7ae27eac0
Kamil Sobol <kasobol@microsoft.com>
2020-10-01 10:08:32 -0700

    second round.

--- sdk/storage/Azure.Storage.Webjobs.Extensions.Blobs/tests/Properties/AssemblyInfo.cs
+++ sdk/storage/Azure.Storage.Webjobs.Extensions.Blobs/tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using NUnit.Framework;
 using Xunit;
 
 [assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass)]
+[assembly: Parallelizable(ParallelScope.Fixtures)]
