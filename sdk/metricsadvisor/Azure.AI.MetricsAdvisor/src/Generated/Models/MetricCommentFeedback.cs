a64829f3d9b4437f3bed5479fd2db7e05a2e99d1
Christopher Scott <chriscott@hotmail.com>
2020-10-01 17:26:25 -0500

    Address PR comments (#15616)
    * Address PR comments
    
    * rename back to GetMetricFeedbacks
    
    * export
    
    * pr comments
    
    * export

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/MetricCommentFeedback.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/MetricCommentFeedback.cs
@@ -10,25 +10,25 @@ using System;
 namespace Azure.AI.MetricsAdvisor.Models
 {
     /// <summary> The CommentFeedback. </summary>
-    public partial class CommentFeedback : MetricFeedback
+    public partial class MetricCommentFeedback : MetricFeedback
     {
 
-        /// <summary> Initializes a new instance of CommentFeedback. </summary>
-        /// <param name="feedbackType"> feedback type. </param>
-        /// <param name="feedbackId"> feedback unique id. </param>
+        /// <summary> Initializes a new instance of MetricCommentFeedback. </summary>
+        /// <param name="type"> feedback type. </param>
+        /// <param name="id"> feedback unique id. </param>
         /// <param name="createdTime"> feedback created time. </param>
         /// <param name="userPrincipal"> user who gives this feedback. </param>
         /// <param name="metricId"> metric unique id. </param>
         /// <param name="dimensionFilter"> . </param>
         /// <param name="startTime"> the start timestamp of feedback timerange. </param>
         /// <param name="endTime"> the end timestamp of feedback timerange, when equals to startTime means only one timestamp. </param>
-        /// <param name="value"> . </param>
-        internal CommentFeedback(FeedbackType feedbackType, Guid? feedbackId, DateTimeOffset? createdTime, string userPrincipal, Guid metricId, FeedbackDimensionFilter dimensionFilter, DateTimeOffset? startTime, DateTimeOffset? endTime, CommentFeedbackValue value) : base(feedbackType, feedbackId, createdTime, userPrincipal, metricId, dimensionFilter)
+        /// <param name="valueInternal"> . </param>
+        internal MetricCommentFeedback(FeedbackType type, string id, DateTimeOffset? createdTime, string userPrincipal, string metricId, FeedbackDimensionFilter dimensionFilter, DateTimeOffset? startTime, DateTimeOffset? endTime, CommentFeedbackValue valueInternal) : base(type, id, createdTime, userPrincipal, metricId, dimensionFilter)
         {
             StartTime = startTime;
             EndTime = endTime;
-            Value = value;
-            FeedbackType = feedbackType;
+            ValueInternal = valueInternal;
+            Type = type;
         }
 
         /// <summary> the start timestamp of feedback timerange. </summary>
--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/Metrics/MetricChangePointFeedback.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/Metrics/MetricChangePointFeedback.cs
@@ -2,25 +2,23 @@
 // Licensed under the MIT License.
 
 using System;
+using Azure.Core;
 
 namespace Azure.AI.MetricsAdvisor.Models
 {
-    /// <summary> The ChangePointFeedback. </summary>
-    public partial class ChangePointFeedback : MetricFeedback
+    /// <summary> The <see cref="MetricChangePointFeedback"/>. </summary>
+    [CodeGenModel("ChangePointFeedback")]
+    public partial class MetricChangePointFeedback : MetricFeedback
     {
-        /// <summary> Initializes a new instance of ChangePointFeedback. </summary>
+        /// <summary> Initializes a new instance of <see cref="MetricChangePointFeedback"/>. </summary>
         /// <param name="metricId"> The metric unique id. </param>
         /// <param name="dimensionFilter"> The dimension filter. </param>
         /// <param name="startTime"> The start timestamp of feedback timerange. </param>
         /// <param name="endTime"> The end timestamp of feedback timerange, when equals to startTime means only one timestamp. </param>
         /// <param name="value"> The value for the feedback. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="dimensionFilter"/> or <paramref name="value"/> is null. </exception>
-        public ChangePointFeedback(Guid metricId, FeedbackDimensionFilter dimensionFilter, DateTimeOffset startTime, DateTimeOffset endTime, ChangePointValue value) : base(metricId, dimensionFilter)
+        public MetricChangePointFeedback(string metricId, FeedbackDimensionFilter dimensionFilter, DateTimeOffset startTime, DateTimeOffset endTime, ChangePointValue value) : base(metricId, dimensionFilter)
         {
-            if (dimensionFilter == null)
-            {
-                throw new ArgumentNullException(nameof(dimensionFilter));
-            }
             if (value == default)
             {
                 throw new ArgumentNullException(nameof(value));
@@ -28,8 +26,8 @@ namespace Azure.AI.MetricsAdvisor.Models
 
             StartTime = startTime;
             EndTime = endTime;
-            Value = new ChangePointFeedbackValue(value);
-            FeedbackType = FeedbackType.ChangePoint;
+            ValueInternal = new ChangePointFeedbackValue(value);
+            Type = FeedbackType.ChangePoint;
         }
 
         /// <summary> Initializes a new instance of ChangePointFeedback. </summary>
@@ -39,26 +37,22 @@ namespace Azure.AI.MetricsAdvisor.Models
         /// <param name="endTime"> the end timestamp of feedback timerange, when equals to startTime means only one timestamp. </param>
         /// <param name="value"> . </param>
         /// <exception cref="ArgumentNullException"> <paramref name="dimensionFilter"/> or <paramref name="value"/> is null. </exception>
-        internal ChangePointFeedback(Guid metricId, FeedbackDimensionFilter dimensionFilter, DateTimeOffset startTime, DateTimeOffset endTime, ChangePointFeedbackValue value) : base(metricId, dimensionFilter)
+        internal MetricChangePointFeedback(string metricId, FeedbackDimensionFilter dimensionFilter, DateTimeOffset startTime, DateTimeOffset endTime, ChangePointFeedbackValue value) : base(metricId, dimensionFilter)
         {
-            if (dimensionFilter == null)
-            {
-                throw new ArgumentNullException(nameof(dimensionFilter));
-            }
-            if (value == null)
-            {
-                throw new ArgumentNullException(nameof(value));
-            }
+            Argument.AssertNotNull(value, nameof(value));
 
             StartTime = startTime;
             EndTime = endTime;
-            Value = value;
-            FeedbackType = Models.FeedbackType.ChangePoint;
+            ValueInternal = value;
+            Type = Models.FeedbackType.ChangePoint;
         }
 
         /// <summary>
-        /// The value of the feedback.
+        /// The changepoint value.
         /// </summary>
-        public ChangePointFeedbackValue Value { get; set; }
+        public ChangePointValue ChangePointValue { get => ValueInternal.ChangePointValue; }
+
+        [CodeGenMember("Value")]
+        internal ChangePointFeedbackValue ValueInternal { get; }
     }
 }
