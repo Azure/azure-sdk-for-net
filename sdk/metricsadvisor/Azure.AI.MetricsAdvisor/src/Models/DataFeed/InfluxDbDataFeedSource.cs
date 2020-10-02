fee1df0483a189cc2084ffc8496406da526cee94
kinelski <camaiaor@microsoft.com>
2020-10-02 12:02:45 -0700

    Update sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/DataFeed/InfluxDbDataFeedSource.cs
    Co-authored-by: Christopher Scott <chriscott@hotmail.com>

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/DataFeed/InfluxDbDataFeedSource.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/DataFeed/InfluxDbDataFeedSource.cs
@@ -7,7 +7,7 @@ using Azure.Core;
 namespace Azure.AI.MetricsAdvisor.Models
 {
     /// <summary>
-    /// Ingests data into a <see cref="DataFeed"/> for anomaly detection.
+    /// Describes an Influx DB data source which ingests data into a <see cref="DataFeed"/> for anomaly detection.
     /// </summary>
     public class InfluxDbDataFeedSource : DataFeedSource
     {
