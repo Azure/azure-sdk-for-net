14c747bb5727c9b805b5b5f90d149c80744fce80
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-10-02 13:56:38 -0700

    Add exception to track call

--- sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/ApplicationInsightsRestClient.cs
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/ApplicationInsightsRestClient.cs
@@ -1,7 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using OpenTelemetry.Exporter.AzureMonitor.Models;
 
@@ -9,6 +14,76 @@ namespace OpenTelemetry.Exporter.AzureMonitor
 {
     internal partial class ApplicationInsightsRestClient
     {
+        /// <summary> This operation sends a sequence of telemetry events that will be monitored by Azure Monitor. </summary>
+        /// <param name="body"> The list of telemetry events to track. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
+        public async Task<Response<TrackResponse>> TrackAsync(IEnumerable<TelemetryItem> body, CancellationToken cancellationToken = default)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException(nameof(body));
+            }
+
+            using var message = CreateTrackRequest(body);
+            await _pipeline.SendAsync(message, cancellationToken).ConfigureAwait(false);
+            switch (message.Response.Status)
+            {
+                case 200:
+                case 206:
+                case 408:
+                case 429:
+                case 439:
+                case 500:
+                case 502:
+                case 503:
+                case 504:
+                    {
+                        TrackResponse value = default;
+                        using var document = await JsonDocument.ParseAsync(message.Response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                        value = TrackResponse.DeserializeTrackResponse(document.RootElement);
+                        return Response.FromValue(value, message.Response);
+                    }
+                default:
+                    throw await _clientDiagnostics.CreateRequestFailedExceptionAsync(message.Response).ConfigureAwait(false);
+            }
+        }
+
+        /// <summary> This operation sends a sequence of telemetry events that will be monitored by Azure Monitor. </summary>
+        /// <param name="body"> The list of telemetry events to track. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="body"/> is null. </exception>
+        public Response<TrackResponse> Track(IEnumerable<TelemetryItem> body, CancellationToken cancellationToken = default)
+        {
+            if (body == null)
+            {
+                throw new ArgumentNullException(nameof(body));
+            }
+
+            using var message = CreateTrackRequest(body);
+            _pipeline.Send(message, cancellationToken);
+            switch (message.Response.Status)
+            {
+                case 200:
+                case 206:
+                case 408:
+                case 429:
+                case 439:
+                case 500:
+                case 502:
+                case 503:
+                case 504:
+                    {
+                        TrackResponse value = default;
+                        using var document = JsonDocument.Parse(message.Response.ContentStream);
+                        value = TrackResponse.DeserializeTrackResponse(document.RootElement);
+                        return Response.FromValue(value, message.Response);
+                    }
+                default:
+                    throw _clientDiagnostics.CreateRequestFailedException(message.Response);
+            }
+        }
+
         internal HttpMessage CreateTrackRequest(IEnumerable<TelemetryItem> body)
         {
             var message = _pipeline.CreateMessage();
