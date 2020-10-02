bc014efec68cd4c141e15ebdb04f3ef3e418fa80
Pavel Prystinka <pprystinka@microsoft.com>
2020-10-02 11:43:01 -0700

    add phone number administration SDk

---
+++ sdk/communication/Azure.Communication.Administration/src/PhoneNumberSearchRefreshOperation.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Communication.Administration.Models;
+
+namespace Azure.Communication.Administration
+{
+    /// <summary>
+    /// Represents a long-running phone number search refresh operation.
+    /// </summary>
+    public class PhoneNumberSearchRefreshOperation : Operation<PhoneNumberSearch>
+    {
+        private PhoneNumberSearchOperation _searchOperation;
+
+        /// <summary>
+        /// Initializes a new <see cref="PhoneNumberSearchRefreshOperation"/> instance
+        /// </summary>
+        /// <param name="client">The client used to check for completion.</param>
+        /// <param name="phoneNumberSearchId">The search ID of this operation.</param>
+        /// <param name="initialResponse">The original server response on start operation request.</param>
+        /// <param name="cancellationToken">The cancellation token to use.</param>
+        internal PhoneNumberSearchRefreshOperation(
+            PhoneNumberAdministrationClient client,
+            string phoneNumberSearchId,
+            Response initialResponse,
+            CancellationToken cancellationToken = default)
+        {
+            var terminateStatuses = new SearchStatus []
+            {
+                SearchStatus.Reserved,
+                SearchStatus.Expired,
+                SearchStatus.Success,
+                SearchStatus.Cancelled,
+                SearchStatus.Error
+            };
+            _searchOperation = new PhoneNumberSearchOperation(client, phoneNumberSearchId, initialResponse, terminateStatuses, cancellationToken);
+        }
+
+        /// <inheritdocs />
+        public override string Id => _searchOperation.Id;
+
+        /// <inheritdocs />
+        public override PhoneNumberSearch Value => _searchOperation.Value;
+
+        /// <inheritdocs />
+        public override bool HasCompleted => _searchOperation.HasCompleted;
+
+        /// <inheritdocs />
+        public override bool HasValue => _searchOperation.HasValue;
+
+        /// <inheritdocs />
+        public override Response GetRawResponse()
+        {
+            return _searchOperation.GetRawResponse();
+        }
+
+        /// <inheritdocs />
+        public override Response UpdateStatus(CancellationToken cancellationToken = default)
+        {
+            return _searchOperation.UpdateStatus(cancellationToken);
+        }
+
+        /// <inheritdocs />
+        public override ValueTask<Response> UpdateStatusAsync(CancellationToken cancellationToken = default)
+        {
+            return _searchOperation.UpdateStatusAsync(cancellationToken);
+        }
+
+        /// <inheritdocs />
+        public override ValueTask<Response<PhoneNumberSearch>> WaitForCompletionAsync(CancellationToken cancellationToken = default)
+        {
+            return _searchOperation.WaitForCompletionAsync(cancellationToken);
+        }
+
+        /// <inheritdocs />
+        public override ValueTask<Response<PhoneNumberSearch>> WaitForCompletionAsync(TimeSpan pollingInterval, CancellationToken cancellationToken)
+        {
+            return _searchOperation.WaitForCompletionAsync(pollingInterval, cancellationToken);
+        }
+    }
+}
--- sdk/communication/Azure.Communication.Administration/src/autorest.md
+++ sdk/communication/Azure.Communication.Administration/src/autorest.md
@@ -7,6 +7,11 @@ Run `dotnet msbuild /t:GenerateCode` to generate code.
 
 ``` yaml
 input-file:
+    -  https://raw.githubusercontent.com/Azure/azure-rest-api-specs/257f060be8b60d8468584682aa2d71b1faa5f82c/specification/communication/data-plane/Microsoft.CommunicationServicesAdministration/preview/2020-07-20-preview1/communicationservicesadministration.json
+payload-flattening-threshold: 3
+```
+``` yaml
+input-file:
     -  https://raw.githubusercontent.com/Azure/azure-rest-api-specs/838c5092f11e8ca26e262b1f1099d5c5cdfedc3f/specification/communication/data-plane/Microsoft.CommunicationServicesIdentity/preview/2020-07-20-preview2/CommunicationIdentity.json
 payload-flattening-threshold: 3
 ```
