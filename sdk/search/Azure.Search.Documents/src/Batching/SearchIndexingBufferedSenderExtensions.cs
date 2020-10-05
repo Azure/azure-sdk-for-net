cb0c1f10147988ef4845302887272c59221916d4
tg-msft <Ted.Glaza@microsoft.com>
2020-10-04 20:06:10 -0700

    Search Fancy Batching
    Adds SearchIndexingBufferedSender to index search documents with intelligent
    batching, automatic flushing, and retries for failed indexing actions.
    
    Fixes #11161.

---
+++ sdk/search/Azure.Search.Documents/src/Batching/SearchIndexingBufferedSenderExtensions.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Search.Documents.Models;
+
+namespace Azure.Search.Documents
+{
+    /// <summary>
+    /// Extension methods for <see cref="SearchIndexingBufferedSender{T}"/>.
+    /// </summary>
+    public static class SearchIndexingBufferedSenderExtensions
+    {
+        /// <summary>
+        /// Adds a batch of delete actions to eventually send to the search
+        /// index.
+        /// </summary>
+        /// <param name="indexer">The sender.</param>
+        /// <param name="key">The name of the key field.</param>
+        /// <param name="documentKeys">
+        /// The keys of the documents to delete.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// Optional <see cref="CancellationToken"/> to propagate notifications
+        /// that the operation should be canceled.
+        /// </param>
+        public static void DeleteDocuments(
+            this SearchIndexingBufferedSender<SearchDocument> indexer,
+            string key,
+            IEnumerable<string> documentKeys,
+            CancellationToken cancellationToken = default) =>
+            DeleteDocumentsInternal(
+                indexer,
+                key,
+                documentKeys,
+                async: false,
+                cancellationToken)
+                .EnsureCompleted();
+
+        /// <summary>
+        /// Adds a batch of delete actions to eventually send to the search
+        /// index.
+        /// </summary>
+        /// <param name="indexer">The sender.</param>
+        /// <param name="key">The name of the key field.</param>
+        /// <param name="documentKeys">
+        /// The keys of the documents to delete.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// Optional <see cref="CancellationToken"/> to propagate notifications
+        /// that the operation should be canceled.
+        /// </param>
+        /// <returns>
+        /// A task that completes when the indexing actions have been added but
+        /// not yet sent.
+        /// </returns>
+        public static async Task DeleteDocumentsAsync(
+            this SearchIndexingBufferedSender<SearchDocument> indexer,
+            string key,
+            IEnumerable<string> documentKeys,
+            CancellationToken cancellationToken = default) =>
+            await DeleteDocumentsInternal(
+                indexer,
+                key,
+                documentKeys,
+                async: true,
+                cancellationToken)
+                .ConfigureAwait(false);
+
+        private static async Task DeleteDocumentsInternal(
+            SearchIndexingBufferedSender<SearchDocument> indexer,
+            string key,
+            IEnumerable<string> documentKeys,
+            bool async,
+            CancellationToken cancellationToken = default)
+        {
+            Argument.AssertNotNull(indexer, nameof(indexer));
+            Argument.AssertNotNull(key, nameof(key));
+            Argument.AssertNotNull(documentKeys, nameof(documentKeys));
+
+            var batch = IndexDocumentsBatch.Delete<SearchDocument>(
+                documentKeys.Select(k => new SearchDocument { [key] = k }));
+            if (async)
+            {
+                await indexer.IndexDocumentsAsync(batch, cancellationToken).ConfigureAwait(false);
+            }
+            else
+            {
+                indexer.IndexDocuments(batch, cancellationToken);
+            }
+        }
+    }
+}
