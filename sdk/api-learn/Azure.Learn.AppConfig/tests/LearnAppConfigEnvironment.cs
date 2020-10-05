2ca5f5ef3d607a28bd96fd06b0c184b276d9582b
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-10-04 21:46:57 -0700

    Update test environment and add AssemblyInfo

--- sdk/api-learn/Azure.Learn.AppConfig/tests/LearnAppConfigEnvironment.cs
+++ sdk/api-learn/Azure.Learn.AppConfig/tests/LearnAppConfigEnvironment.cs
@@ -1,14 +1,14 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using Azure.Core.TestFramework;
 
-namespace Azure.Learn.AppConfig.Samples
+namespace Azure.Learn.AppConfig.Tests
 {
     // To learn more about TestEnvironment classes, please see: https://github.com/Azure/azure-sdk-for-net/blob/master/sdk/core/Azure.Core.TestFramework/README.md
-    public class AppConfigClientTestEnvironment : TestEnvironment
+    public class LearnAppConfigEnvironment : TestEnvironment
     {
-        public AppConfigClientTestEnvironment() : base("api-learn")
+        public LearnAppConfigEnvironment() : base("api-learn")
         {
         }
     }
