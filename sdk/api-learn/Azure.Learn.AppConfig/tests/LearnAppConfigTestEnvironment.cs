2ddcbde4936a4f6907c59bedfb47bf32faf082ed
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2020-10-05 10:20:35 -0700

    rename

--- sdk/api-learn/Azure.Learn.AppConfig/tests/LearnAppConfigTestEnvironment.cs
+++ sdk/api-learn/Azure.Learn.AppConfig/tests/LearnAppConfigTestEnvironment.cs
@@ -6,9 +6,9 @@ using Azure.Core.TestFramework;
 namespace Azure.Learn.AppConfig.Tests
 {
     // To learn more about TestEnvironment classes, please see: https://github.com/Azure/azure-sdk-for-net/blob/master/sdk/core/Azure.Core.TestFramework/README.md
-    public class LearnAppConfigEnvironment : TestEnvironment
+    public class LearnAppConfigTestEnvironment : TestEnvironment
     {
-        public LearnAppConfigEnvironment() : base("api-learn")
+        public LearnAppConfigTestEnvironment() : base("api-learn")
         {
         }
     }
