a19f5870fccd0eae450f5aa515bb7aaba823e4f9
Reza Jooyandeh <rejooyan@microsoft.com>
2020-10-05 17:24:54 -0700

    [Communication] Moving the Phone Number administration to the master branch (#15707)
    * Communication phone number administration (#15660)
    
    * add phone number administration SDk
    
    * Remove LRO
    
    * Update readme
    
    * Remove LRO, update readme and changelog
    
    * Changed wording in changelog
    
    * Fix build issues
    
    * Rename Capability to PhoneNumberCapability
    
    * Update Azure.Communication version. (#15702)
    
    * Update Azure.Communication version.
    
    * update change log message
    
    * update changelog messages
    
    Co-authored-by: pavelprystinka <30868871+pavelprystinka@users.noreply.github.com>

---
+++ sdk/communication/Azure.Communication.Administration/src/Models/PhoneNumberCapability.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.Communication.Administration.Models
+{
+    [CodeGenModel("Capability")]
+    public readonly partial struct PhoneNumberCapability
+    {
+    }
+}
