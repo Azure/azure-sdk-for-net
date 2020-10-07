59de0c412cbdfda9d53a2823099164c985de6b26
Pavel Krymets <pavel@krymets.com>
2020-10-06 17:12:57 -0700

    Add serializer support to JsonPatchDocument (#15754)
    Fixes: https://github.com/Azure/azure-sdk-for-net/issues/15752

--- sdk/core/Azure.Core.Experimental/api/Azure.Core.Experimental.netstandard2.0.cs
+++ sdk/core/Azure.Core.Experimental/api/Azure.Core.Experimental.netstandard2.0.cs
@@ -96,12 +96,16 @@ namespace Azure.Core
     public partial class JsonPatchDocument
     {
         public JsonPatchDocument() { }
-        public void AppendAdd(string path, string rawJsonValue) { }
+        public JsonPatchDocument(Azure.Core.Serialization.ObjectSerializer serializer) { }
+        public void AppendAddRaw(string path, string rawJsonValue) { }
+        public void AppendAdd<T>(string path, T value, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { }
         public void AppendCopy(string from, string path) { }
         public void AppendMove(string from, string path) { }
         public void AppendRemove(string path) { }
-        public void AppendReplace(string path, string rawJsonValue) { }
-        public void AppendTest(string path, string rawJsonValue) { }
+        public void AppendReplaceRaw(string path, string rawJsonValue) { }
+        public void AppendReplace<T>(string path, T value, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { }
+        public void AppendTestRaw(string path, string rawJsonValue) { }
+        public void AppendTest<T>(string path, T value, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { }
         public override string ToString() { throw null; }
     }
 }
