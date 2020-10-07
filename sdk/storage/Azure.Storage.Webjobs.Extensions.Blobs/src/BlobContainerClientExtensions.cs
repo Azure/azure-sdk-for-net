8c52b17dbdb905d20f559cd4de48410ebaa92ea7
Kamil Sobol <kasobol@microsoft.com>
2020-10-07 11:17:53 -0700

    remove comment.

--- sdk/storage/Azure.Storage.Webjobs.Extensions.Blobs/src/BlobContainerClientExtensions.cs
+++ sdk/storage/Azure.Storage.Webjobs.Extensions.Blobs/src/BlobContainerClientExtensions.cs
@@ -7,11 +7,8 @@ using Azure.Storage.Blobs.Specialized;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Storage.Blobs
 {
-    // TODO (kasobol-msft) this duplicates "Utility". find way to share code.
     internal static class BlobContainerClientExtensions
     {
-        // CloudBlobDirectory has a private ctor, so we can't actually override it.
-        // This overload is unit-testable
         internal static BlockBlobClient SafeGetBlockBlobReference(this BlobContainerClient container, string dir, string blobName)
         {
             if (!dir.EndsWith("/", StringComparison.OrdinalIgnoreCase))
