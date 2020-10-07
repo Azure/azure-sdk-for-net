b07808b09a680f146a09a84796435e462732ffcb
Pavel Krymets <pavel@krymets.com>
2020-10-07 16:18:19 -0700

    Update Install-Modules.ps1

--- eng/common/TestResources/Install-Modules.ps1
+++ eng/common/TestResources/Install-Modules.ps1
@@ -7,8 +7,8 @@ New-Item $TempPath -ItemType Directory -Force
 Invoke-WebRequest -Uri $ModulesUrl -OutFile $ZipFile 
 Expand-Archive $ZipFile -DestinationPath $TempPath -Force
 
-$p = [Environment]::GetEnvironmentVariable("PSModulePath")
+$p = [Environment]::GetEnvironmentVariable("PSMODULEPATH")
 $p = "$p" + $([System.IO.Path]::PathSeparator) + "$TempPath";
-[Environment]::SetEnvironmentVariable("PSModulePath", $p, "User")
+[Environment]::SetEnvironmentVariable("PSMODULEPATH", $p, "User")
 Write-Host "Updating PSModulePath to $p"
-Write-Host "##vso[task.setvariable variable=PSModulePath]$p"
+Write-Host "##vso[task.setvariable variable=PSMODULEPATH]$p"
