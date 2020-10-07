1e07f686db633df461041601c869f02e2991e2ca
Danny Amirault <dana@microsoft.com>
2020-10-07 15:06:01 -0700

    Update eng/common/TestResources/Install-Modules.ps1
    Co-authored-by: Pavel Krymets <pavel@krymets.com>

--- eng/common/TestResources/Install-Modules.ps1
+++ eng/common/TestResources/Install-Modules.ps1
@@ -2,7 +2,7 @@ $ModulesUrl = "https://pakrym0test0storage.blob.core.windows.net/myblobcontainer
 $TempPath = Join-Path $([System.IO.Path]::GetTempPath()) $(New-Guid)
 $ZipFile = Join-Path $TempPath "Modules.zip"
 
-mkdir $TempPath -Force
+New-Item $TempPath -ItemType Directory -Force
 
 Invoke-WebRequest -Uri $ModulesUrl -OutFile $ZipFile 
 Expand-Archive $ZipFile -DestinationPath $TempPath -Force
