750083b70d3b7e9e5c678ee89b8490edcf6b83c0
tg-msft <tg-msft@users.noreply.github.com>
2020-10-09 12:16:31 -0700

    Search Fancy Batching (#15750)
    Adds SearchIndexingBufferedSender to index search documents with intelligent
    batching, automatic flushing, and retries for failed indexing actions.
    
    Fixes #11161.

---
+++ sdk/search/Azure.Search.Documents/src/Batching/AsyncEventExtensions.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Search.Documents.Batching
+{
+    /// <summary>
+    /// Extensions to raise <see cref="Func{T, CancellationToken, Task}"/>
+    /// events that wait for every Task to complete and throw every exception.
+    /// </summary>
+    internal static class AsyncEventExtensions
+    {
+        /// <summary>
+        /// Wait for all tasks to be completed and throw every exception.
+        /// </summary>
+        /// <param name="tasks">The tasks to execute.</param>
+        /// <returns>A Task representing completion of all handlers.</returns>
+        private static async Task JoinAsync(IEnumerable<Task> tasks)
+        {
+            if (tasks == null) { return; }
+
+            Task joined = Task.WhenAll(tasks);
+            try
+            {
+                await joined.ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+                // awaiting will unwrap the AggregateException which we
+                // don't want if there were multiple failures that should
+                // be surfaced
+                if (joined.Exception?.InnerExceptions?.Count > 1)
+                {
+                    throw joined.Exception;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Raise the event.
+        /// </summary>
+        /// <typeparam name="T">Type of the event argument.</typeparam>
+        /// <param name="evt">The event to raise.</param>
+        /// <param name="args">The event arguments.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <returns>A Task representing completion of all handlers.</returns>
+        public static async Task RaiseAsync<T>(
+            this Func<T, CancellationToken, Task> evt,
+            T args,
+            CancellationToken cancellationToken = default) =>
+            await JoinAsync(
+                evt?.GetInvocationList()?.Select(
+                    f => (f as Func<T, CancellationToken, Task>)?.Invoke(args, cancellationToken)))
+                .ConfigureAwait(false);
+
+        /// <summary>
+        /// Raise the event.
+        /// </summary>
+        /// <typeparam name="T">Type of the first event argument.</typeparam>
+        /// <typeparam name="U">Type of the second event argument.</typeparam>
+        /// <param name="evt">The event to raise.</param>
+        /// <param name="first">The first event argument.</param>
+        /// <param name="second">The second event argument.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <returns>A Task representing completion of all handlers.</returns>
+        public static async Task RaiseAsync<T, U>(
+            this Func<T, U, CancellationToken, Task> evt,
+            T first,
+            U second,
+            CancellationToken cancellationToken = default) =>
+            await JoinAsync(
+                evt?.GetInvocationList()?.Select(
+                    f => (f as Func<T, U, CancellationToken, Task>)?.Invoke(first, second, cancellationToken)))
+                .ConfigureAwait(false);
+
+        /// <summary>
+        /// Raise the event.
+        /// </summary>
+        /// <typeparam name="T">Type of the first event argument.</typeparam>
+        /// <typeparam name="U">Type of the second event argument.</typeparam>
+        /// <typeparam name="V">Type of the third event argument.</typeparam>
+        /// <param name="evt">The event to raise.</param>
+        /// <param name="first">The first event argument.</param>
+        /// <param name="second">The second event argument.</param>
+        /// <param name="third">The third event argument.</param>
+        /// <param name="cancellationToken">A cancellation token.</param>
+        /// <returns>A Task representing completion of all handlers.</returns>
+        public static async Task RaiseAsync<T, U, V>(
+            this Func<T, U, V, CancellationToken, Task> evt,
+            T first,
+            U second,
+            V third,
+            CancellationToken cancellationToken = default) =>
+            await JoinAsync(
+                evt?.GetInvocationList()?.Select(
+                    f => (f as Func<T, U, V, CancellationToken, Task>)?.Invoke(first, second, third, cancellationToken)))
+                .ConfigureAwait(false);
+    }
+}
---
+++ sdk/search/Azure.Search.Documents/tests/Batching/ManualRetryDelayTests.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Azure.Search.Documents.Batching;
+using NUnit.Framework;
+
+namespace Azure.Search.Documents.Tests
+{
+    public class ManualRetryDelayTests
+    {
+        [Test]
+        public async Task AddsDelay()
+        {
+            Stopwatch watch = new Stopwatch();
+            var delay = new ManualRetryDelay
+            {
+                Delay = TimeSpan.FromMilliseconds(100),
+                MaxDelay = TimeSpan.FromMilliseconds(250)
+            };
+
+            delay.Update(throttled: true);
+            watch.Start();
+            await delay.WaitIfNeededAsync();
+            watch.Stop();
+
+            Assert.LessOrEqual(80, watch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public async Task AddsMoreDelay()
+        {
+            Stopwatch watch = new Stopwatch();
+            var delay = new ManualRetryDelay
+            {
+                Delay = TimeSpan.FromMilliseconds(100),
+                MaxDelay = TimeSpan.FromMilliseconds(250)
+            };
+
+            delay.Update(throttled: true);
+            await delay.WaitIfNeededAsync();
+            delay.Update(throttled: true);
+
+            watch.Start();
+            await delay.WaitIfNeededAsync();
+            watch.Stop();
+
+            Assert.LessOrEqual(150, watch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public async Task ClampDelay()
+        {
+            Stopwatch watch = new Stopwatch();
+            var delay = new ManualRetryDelay
+            {
+                Delay = TimeSpan.FromMilliseconds(100),
+                MaxDelay = TimeSpan.FromMilliseconds(250)
+            };
+
+            delay.Update(throttled: true);
+            await delay.WaitIfNeededAsync();
+            delay.Update(throttled: true);
+            await delay.WaitIfNeededAsync();
+            delay.Update(throttled: true);
+            await delay.WaitIfNeededAsync();
+            delay.Update(throttled: true);
+
+            watch.Start();
+            await delay.WaitIfNeededAsync();
+            watch.Stop();
+
+            Assert.IsTrue(
+                248 <= watch.ElapsedMilliseconds && watch.ElapsedMilliseconds <= 500,
+                $"Expected a delay between 248ms and 500ms, not {watch.ElapsedMilliseconds}");
+        }
+
+        [Test]
+        public async Task NoDelay()
+        {
+            Stopwatch watch = new Stopwatch();
+            var delay = new ManualRetryDelay
+            {
+                Delay = TimeSpan.FromMilliseconds(100),
+                MaxDelay = TimeSpan.FromMilliseconds(250)
+            };
+
+            watch.Start();
+            await delay.WaitIfNeededAsync();
+            watch.Stop();
+
+            Assert.Greater(50, watch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public async Task ClearDelay()
+        {
+            Stopwatch watch = new Stopwatch();
+            var delay = new ManualRetryDelay
+            {
+                Delay = TimeSpan.FromMilliseconds(100),
+                MaxDelay = TimeSpan.FromMilliseconds(250)
+            };
+
+            delay.Update(throttled: true);
+            await delay.WaitIfNeededAsync();
+            delay.Update(throttled: true);
+            await delay.WaitIfNeededAsync();
+
+            delay.Update(throttled: false);
+            watch.Start();
+            await delay.WaitIfNeededAsync();
+            watch.Stop();
+
+            Assert.Greater(50, watch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public async Task NoDelayIfCalledAfterWait()
+        {
+            Stopwatch watch = new Stopwatch();
+            var delay = new ManualRetryDelay
+            {
+                Delay = TimeSpan.FromMilliseconds(100),
+                MaxDelay = TimeSpan.FromMilliseconds(250)
+            };
+
+            delay.Update(throttled: true);
+            await Task.Delay(TimeSpan.FromMilliseconds(120));
+
+            watch.Start();
+            await delay.WaitIfNeededAsync();
+            watch.Stop();
+
+            Assert.Greater(50, watch.ElapsedMilliseconds);
+        }
+    }
+}
