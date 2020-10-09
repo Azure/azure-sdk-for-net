06bf8df6b21e6a1b9f05c908b75204fcea7a08ae
SDK Automation <sdkautomation@microsoft.com>
2020-10-09 19:11:13 +0000

    Generated from b31451ace81d6e6a337bf09ff6f6429ac2ec429a

--- sdk/peering/Microsoft.Azure.Management.Peering/src/Generated/IPeeringManagementClient.cs
+++ sdk/peering/Microsoft.Azure.Management.Peering/src/Generated/IPeeringManagementClient.cs
@@ -74,6 +74,11 @@ namespace Microsoft.Azure.Management.Peering
 
 
         /// <summary>
+        /// Gets the ICdnPeeringPrefixesOperations.
+        /// </summary>
+        ICdnPeeringPrefixesOperations CdnPeeringPrefixes { get; }
+
+        /// <summary>
         /// Gets the ILegacyPeeringsOperations.
         /// </summary>
         ILegacyPeeringsOperations LegacyPeerings { get; }
