01a8722249f762acbc0f9c99588a3342664888a0
Christopher Scott <chriscott@hotmail.com>
2020-10-08 19:22:45 -0500

    Add hooks doc strings (#15811)

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/DataFeed/DataFeedIngestionStatus.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/DataFeed/DataFeedIngestionStatus.cs
@@ -6,6 +6,7 @@ using Azure.Core;
 namespace Azure.AI.MetricsAdvisor.Models
 {
     /// <summary>
+    /// The status related to a data feed ingestion.
     /// </summary>
     [CodeGenModel("IngestionStatus")]
     public partial class DataFeedIngestionStatus
