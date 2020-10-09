39e6ee75e4e84c0f75eda6c5c0376dc00b2d7fa2
Christopher Scott <chriscott@hotmail.com>
2020-10-08 19:22:45 -0500

    Add hooks doc strings (#15811)

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/Hook/AlertingHook.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/Hook/AlertingHook.cs
@@ -8,6 +8,7 @@ using Azure.Core;
 namespace Azure.AI.MetricsAdvisor.Models
 {
     /// <summary>
+    /// An alert notification to be triggered after an anomaly is detected by Metrics Advisor.
     /// </summary>
     [CodeGenModel("HookInfo")]
     public partial class AlertingHook
@@ -20,19 +21,31 @@ namespace Azure.AI.MetricsAdvisor.Models
         }
 
         /// <summary>
+        /// The unique identifier for the hook.
         /// </summary>
         [CodeGenMember("HookId")]
         public string Id { get; internal set; }
 
         /// <summary>
+        /// The name of the hook.
         /// </summary>
         [CodeGenMember("HookName")]
         public string Name { get; }
 
         /// <summary>
+        /// The list of user e-mails with administrative rights to manage this hook.
         /// </summary>
         [CodeGenMember("Admins")]
         public IReadOnlyList<string> Administrators { get; }
+
+        /// <summary> The hook type. </summary>
+        internal HookType HookType { get; set; }
+
+        /// <summary> The hook description. </summary>
+        public string Description { get; set; }
+
+        /// <summary> Optional field which enables a customized redirect, such as for troubleshooting notes. </summary>
+        public string ExternalLink { get; set; }
 
         internal static HookInfoPatch GetPatchModel(AlertingHook hook)
         {
--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/Hook/WebHook.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Models/Hook/WebHook.cs
@@ -1,18 +1,26 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 using Azure.Core;
 
 namespace Azure.AI.MetricsAdvisor.Models
 {
     /// <summary>
+    /// A web hook is the entry point for all the information available from the Metrics Advisor service, and calls a user-provided API when an alert is triggered.
+    /// All alerts can be sent through a web hook.
     /// </summary>
     [CodeGenModel("WebhookHookInfo")]
     [CodeGenSuppress(nameof(WebHook), typeof(string), typeof(WebhookHookParameter))]
     public partial class WebHook : AlertingHook
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="WebHook"/> class.
+        /// <param name="name">The name to assign to the hook.</param>
+        /// <param name="endpoint">The API address to be called when an alert is triggered.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="name"/> or <paramref name="endpoint"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="name"/> or <paramref name="endpoint"/> is empty.</exception>
         /// </summary>
         public WebHook(string name, string endpoint)
             : base(name)
@@ -32,26 +40,32 @@ namespace Azure.AI.MetricsAdvisor.Models
         }
 
         /// <summary>
+        /// The API address to be called when an alert is triggered.
         /// </summary>
         public string Endpoint { get => HookParameter.Endpoint; }
 
         /// <summary>
+        /// The username for authenticating to the API address. Leave this blank if authentication isn't needed.
         /// </summary>
         public string Username { get => HookParameter.Username; set => HookParameter.Username = value; }
 
         /// <summary>
+        /// The password for authenticating to the API address. Leave this blank if authentication isn't needed.
         /// </summary>
         public string Password { get => HookParameter.Password; set => HookParameter.Password = value; }
 
         /// <summary>
+        /// The certificate key for authenticating to the API address. Leave this blank if authentication isn't needed.
         /// </summary>
         public string CertificateKey { get => HookParameter.CertificateKey; set => HookParameter.CertificateKey = value; }
 
         /// <summary>
+        /// The certificate password for authenticating to the API address. Leave this blank if authentication isn't needed.
         /// </summary>
         public string CertificatePassword { get => HookParameter.Username; set => HookParameter.Username = value; }
 
         /// <summary>
+        /// Custom headers to send in the API call.
         /// </summary>
         public IDictionary<string, string> Headers
         {
