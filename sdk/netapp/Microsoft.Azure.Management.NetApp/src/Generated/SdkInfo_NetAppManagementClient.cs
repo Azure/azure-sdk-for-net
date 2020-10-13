8d850f0bfc13c2c7fa3022deadcf486b68461e03
SDK Automation <sdkautomation@microsoft.com>
2020-10-13 05:11:08 +0000

    Generated from d11e7ed3af4ecf391175ea5096ee1302a5769308

--- sdk/netapp/Microsoft.Azure.Management.NetApp/src/Generated/SdkInfo_NetAppManagementClient.cs
+++ sdk/netapp/Microsoft.Azure.Management.NetApp/src/Generated/SdkInfo_NetAppManagementClient.cs
@@ -30,21 +30,8 @@ namespace Microsoft.Azure.Management.NetApp
                 new Tuple<string, string, string>("NetApp", "Snapshots", "2020-06-01"),
                 new Tuple<string, string, string>("NetApp", "Vaults", "2020-06-01"),
                 new Tuple<string, string, string>("NetApp", "Volumes", "2020-06-01"),
-                new Tuple<string, string, string>("NetApp", "backupPolicies", "2020-06-01"),
-                new Tuple<string, string, string>("NetApp", "snapshotPolicies", "2020-06-01"),
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/netapp/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\Users\\audunn\\Source\\repos\\azure\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "56d853a348eac1f33085073d1eaeb314d414dd46";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
