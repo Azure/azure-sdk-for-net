aa4e0559ef253e8858b4b0656f59b7c9e3cecb40
Scott Schaab <sschaab@microsoft.com>
2020-10-14 16:56:02 -0700

    removing AuthenticationTokenRecord workaround

--- sdk/identity/Azure.Identity/src/InteractiveBrowserCredential.cs
+++ sdk/identity/Azure.Identity/src/InteractiveBrowserCredential.cs
@@ -208,7 +208,7 @@ namespace Azure.Identity
         {
             AuthenticationResult result = await Client.AcquireTokenInteractiveAsync(scopes, Prompt.SelectAccount, async, cancellationToken).ConfigureAwait(false);
 
-            Record = new AuthenticationTokenRecord(result, ClientId);
+            Record = new AuthenticationRecord(result, ClientId);
 
             return new AccessToken(result.AccessToken, result.ExpiresOn);
         }
