ba77045e5fa008c893b98f480485fa1bade663f7
Sri Susloka Sahithi K <ss.sahithi.kk@outlook.com>
2020-10-14 14:24:31 +0530

    Update QnAMakerPreviewClient.cs

--- sdk/cognitiveservices/Knowledge.QnAMaker/src/Customizations/QnAMakerPreviewClient.cs
+++ sdk/cognitiveservices/Knowledge.QnAMaker/src/Customizations/QnAMakerPreviewClient.cs
@@ -10,7 +10,25 @@ namespace Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker
 {
     public partial class QnAMakerClient
     {
-        public QnAMakerClient(ServiceClientCredentials credentials, bool isPreview = false, params DelegatingHandler[] handlers) : this(credentials, handlers) 
+        /// <summary>
+        /// Initializes a new instance of the QnAMakerClient class.
+        /// </summary>
+        /// <param name='credentials'>
+        /// Required. Subscription credentials which uniquely identify client subscription.
+        /// </param>
+        /// <param name="isPreview">
+        /// Optional. The flag used to change BaseUri if it is true.
+        /// </param>
+        /// <param name='rootHandler'>
+        /// Optional. The http client handler used to handle http transport.
+        /// </param>
+        /// <param name='handlers'>
+        /// Optional. The delegating handlers to add to the http client pipeline.
+        /// </param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when a required parameter is null
+        /// </exception>
+        public QnAMakerClient(ServiceClientCredentials credentials, bool isPreview = false, params DelegatingHandler[] handlers) : this(credentials, handlers)
         {
             if (isPreview)
             {
