625e37e1b7b8ecf060b4cd9ad78b1d8ebf9960e0
sahithikkss <53164851+sahithikkss@users.noreply.github.com>
2020-10-15 00:35:05 +0530

    QnAMakerPreviewClient in QnAMaker SDK  (#15960)
    * BaseUri switch based on isPreview in QnAMakerClient
    
    * Update QnAMakerPreviewClient.cs
    
    * QnAMakerPreviewRuntimeClient in customization with v5-preview.1 version
    
    * changelog to trigger SDK release
    
    * added date inthe changelog
    
    * Missed basetest changes
    
    * place correct playback session record file

---
+++ sdk/cognitiveservices/Knowledge.QnAMaker/tests/QnAMakerPreviewRuntimeTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker;
+using Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker.Models;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace QnAMaker.Tests
+{
+    public class QnAMakerPreviewRuntimeTests : BaseTests
+    {
+        [Fact]
+        public void QnAMakerPreviewRuntimeGenerateAnswerTest()
+        {
+            using (MockContext context = MockContext.Start(this.GetType()))
+            {
+                HttpMockServer.Initialize(this.GetType(), "QnAMakerPreviewRuntimeGenerateAnswerTest");
+
+                var client = GetQnAMakerPreviewRuntimeClient(HttpMockServer.CreateInstance());
+                var queryDTO = new QueryDTO();
+                queryDTO.Question = "new question";
+                queryDTO.IsTest = true;
+                var answer = client.Runtime.GenerateAnswerAsync("192233e1-0ec8-44dc-8285-57a541b7c79c", queryDTO).Result;
+                Assert.Equal(1, answer.Answers.Count);
+                Assert.Equal(100, answer.Answers[0].Score);
+            }
+        }
+    }
+}
