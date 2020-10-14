78a69f93451737ae25a9cc8a54cbe47d13535870
Anne Thompson <annelo@microsoft.com>
2020-10-14 09:28:39 -0700

    Add files from API Design Tutorial branch to Implementation Tutorial branch (#15964)
    * add yml files and computation project to impl tutorial starter
    
    * pr fb

---
+++ sdk/api-learn/Azure.Learn.Computation/tests/LearnComputationTestEnvironment1.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.Learn.Computation.Samples
+{
+    // To learn more about TestEnvironment classes, please see: https://github.com/Azure/azure-sdk-for-net/blob/master/sdk/core/Azure.Core.TestFramework/README.md
+    public class LearnComputationTestEnvironment : TestEnvironment
+    {
+        public LearnComputationTestEnvironment() : base("api-learn")
+        {
+        }
+    }
+}
