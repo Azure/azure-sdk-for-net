2dedcaf017b3d3412d5904e3774af58c400a5f11
SDK Automation <sdkautomation@microsoft.com>
2020-10-16 09:31:15 +0000

    Generated from 2d6e42d6f3edd924b7fd165a15ff79a3cfa7e0eb
    use "subscriptionId"

--- sdk/alertsmanagement/Microsoft.Azure.Management.AlertsManagement/src/Generated/IAlertsManagementClient.cs
+++ sdk/alertsmanagement/Microsoft.Azure.Management.AlertsManagement/src/Generated/IAlertsManagementClient.cs
@@ -46,11 +46,6 @@ namespace Microsoft.Azure.Management.AlertsManagement
         string SubscriptionId { get; set; }
 
         /// <summary>
-        /// client API version
-        /// </summary>
-        string ApiVersion { get; }
-
-        /// <summary>
         /// The preferred language for the response.
         /// </summary>
         string AcceptLanguage { get; set; }
@@ -70,6 +65,11 @@ namespace Microsoft.Azure.Management.AlertsManagement
 
 
         /// <summary>
+        /// Gets the IActionRulesOperations.
+        /// </summary>
+        IActionRulesOperations ActionRules { get; }
+
+        /// <summary>
         /// Gets the IOperations.
         /// </summary>
         IOperations Operations { get; }
@@ -85,9 +85,9 @@ namespace Microsoft.Azure.Management.AlertsManagement
         ISmartGroupsOperations SmartGroups { get; }
 
         /// <summary>
-        /// Gets the IActionRulesOperations.
+        /// Gets the ISmartDetectorAlertRulesOperations.
         /// </summary>
-        IActionRulesOperations ActionRules { get; }
+        ISmartDetectorAlertRulesOperations SmartDetectorAlertRules { get; }
 
     }
 }
