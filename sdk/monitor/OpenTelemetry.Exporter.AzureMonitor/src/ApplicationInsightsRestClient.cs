d486a74a9d95d8e52c9bf77c5bf94a0de482a611
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-10-15 20:24:19 -0700

    Added IngestionResponsePolicy

--- sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/ApplicationInsightsRestClient.cs
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/ApplicationInsightsRestClient.cs
@@ -3,10 +3,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using Azure;
 using Azure.Core;
 using OpenTelemetry.Exporter.AzureMonitor.Models;
 
@@ -14,7 +12,7 @@ namespace OpenTelemetry.Exporter.AzureMonitor
 {
     internal partial class ApplicationInsightsRestClient
     {
-        internal async Task<Response<TrackResponse>> InternalTrackAsync(IEnumerable<TelemetryItem> body, CancellationToken cancellationToken = default)
+        internal async Task<int> InternalTrackAsync(IEnumerable<TelemetryItem> body, CancellationToken cancellationToken = default)
         {
             if (body == null)
             {
@@ -22,27 +20,18 @@ namespace OpenTelemetry.Exporter.AzureMonitor
             }
 
             using var message = CreateTrackRequest(body);
+            message.SetProperty("TelemetryItems", body);
             await _pipeline.SendAsync(message, cancellationToken).ConfigureAwait(false);
-            switch (message.Response.Status)
-            {
-                case 200:
-                case 206:
-                case 408:
-                case 429:
-                case 439:
-                case 500:
-                case 502:
-                case 503:
-                case 504:
-                    {
-                        TrackResponse value = default;
-                        using var document = await JsonDocument.ParseAsync(message.Response.ContentStream, default, cancellationToken).ConfigureAwait(false);
-                        value = TrackResponse.DeserializeTrackResponse(document.RootElement);
-                        return Response.FromValue(value, message.Response);
-                    }
-                default:
-                    throw await _clientDiagnostics.CreateRequestFailedExceptionAsync(message.Response).ConfigureAwait(false);
-            }
+
+            return message.TryGetProperty("ItemsAccepted", out var itemsAccepted) ? (int)itemsAccepted : 0;
+        }
+
+        internal async Task<int> InternalTrackAsync(ReadOnlyMemory<byte> body, CancellationToken cancellationToken = default)
+        {
+            using var message = CreateTrackRequest(body);
+            await _pipeline.SendAsync(message, cancellationToken).ConfigureAwait(false);
+
+            return message.TryGetProperty("ItemsAccepted", out var itemsAccepted) ? (int)itemsAccepted : 0;
         }
 
         internal HttpMessage CreateTrackRequest(IEnumerable<TelemetryItem> body)
@@ -64,6 +53,23 @@ namespace OpenTelemetry.Exporter.AzureMonitor
                 content.WriteNewLine();
             }
             request.Content = RequestContent.Create(content.ToBytes());
+            return message;
+        }
+
+        internal HttpMessage CreateTrackRequest(ReadOnlyMemory<byte> body)
+        {
+            var message = _pipeline.CreateMessage();
+            var request = message.Request;
+            request.Method = RequestMethod.Post;
+            var uri = new RawRequestUriBuilder();
+            uri.AppendRaw(host, false);
+            uri.AppendRaw("/v2", false);
+            uri.AppendPath("/track", false);
+            request.Uri = uri;
+            request.Headers.Add("Content-Type", "application/json");
+            request.Headers.Add("Accept", "application/json");
+            using var content = new NDJsonWriter();
+            request.Content = RequestContent.Create(body);
             return message;
         }
     }
---
+++ sdk/monitor/OpenTelemetry.Exporter.AzureMonitor/src/IngestionResponsePolicy.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Azure;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using OpenTelemetry.Exporter.AzureMonitor.Models;
+
+namespace OpenTelemetry.Exporter.AzureMonitor
+{
+    internal class IngestionResponsePolicy : HttpPipelineSynchronousPolicy
+    {
+        public override void OnReceivedResponse(HttpMessage message)
+        {
+            base.OnReceivedResponse(message);
+
+            int itemsAccepted;
+
+            if (message.TryGetProperty("TelemetryItems", out var telemetryItems))
+            {
+                itemsAccepted = ParseResponse(message, (IEnumerable<TelemetryItem>)telemetryItems);
+            }
+            else
+            {
+                itemsAccepted = ParseResponse(message);
+            }
+
+            message.SetProperty("ItemsAccepted", itemsAccepted);
+        }
+
+        internal static int ParseResponse(HttpMessage message, IEnumerable<TelemetryItem> telemetryItems)
+        {
+            var httpStatus = message?.Response?.Status;
+            int itemsAccepted = 0;
+
+            switch (httpStatus)
+            {
+                case 200:
+                    itemsAccepted = telemetryItems.Count();
+                    break;
+                case 400:
+                case 429:
+                case 439:
+                    // Parse retry-after header
+                    // Send Messages To Storage
+                    break;
+                case 500:
+                case 502:
+                case 503:
+                case 504:
+                    // Send Messages To Storage
+                    break;
+                case 206:
+                    // Parse retry-after header
+                    // Send Failed Messages To Storage
+                    break;
+                case null:
+                    // No HttpMessage. Send TelemetryItems To Storage
+                    break;
+                default:
+                    // Log Non-Retriable Status and don't retry or store;
+                    break;
+            }
+
+            return itemsAccepted;
+        }
+
+        internal static int ParseResponse(HttpMessage message)
+        {
+            var httpStatus = message?.Response?.Status;
+            int itemsAccepted = 0;
+
+            switch (httpStatus)
+            {
+                case 200:
+                    itemsAccepted = GetItemsAccepted(message);
+                    break;
+                case 429:
+                case 439:
+                case 500:
+                case 502:
+                case 503:
+                case 504:
+                case null:
+                    itemsAccepted = 0;
+                    // Request body is already in storage. No need to store again.
+                    break;
+                case 206:
+                    // Send Failed Messages To Storage
+                    break;
+                default:
+                    // Log Non-Retriable Status and don't retry or store;
+                    break;
+            }
+
+            return itemsAccepted;
+        }
+
+        internal static int GetItemsAccepted(HttpMessage message)
+        {
+            int itemsAccepted = 0;
+            using (JsonDocument document = JsonDocument.Parse(message.Response.ContentStream, default))
+            {
+                var value = TrackResponse.DeserializeTrackResponse(document.RootElement);
+                Response.FromValue(value, message.Response);
+                itemsAccepted = value.ItemsAccepted.GetValueOrDefault();
+            }
+
+            return itemsAccepted;
+        }
+    }
+}
