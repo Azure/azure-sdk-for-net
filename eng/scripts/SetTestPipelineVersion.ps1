71d52348df3815487e61a7bfc4a3ae9c63b3a3be
Chidozie Ononiwu <chononiw@microsoft.com>
2020-10-19 17:56:13 -0700

    Prevent template run for sync builds, Update SetTestPipelineVersion.ps1, add CloseAfterOpenForTesting to create-pull-request.yml call

--- eng/scripts/SetTestPipelineVersion.ps1
+++ eng/scripts/SetTestPipelineVersion.ps1
@@ -1,28 +1,18 @@
-# Overides the project file and CHANGELOG.md for the template project using the next publishable version
+# Overides the project file and CHANGELOG.md for the template project using a custom preview version
 # This is to help with testing the release pipeline.
+
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+  [Parameter(Mandatory = $true)]
+  [string]$PreviewVersionNumber
+)
+
 . "${PSScriptRoot}\..\common\scripts\common.ps1"
-$latestTags = git tag -l "Azure.Template_*"
-$semVars = @()
 
 $changeLogFile = "${PSScriptRoot}\..\..\sdk\template\Azure.Template\CHANGELOG.md"
-
-Foreach ($tags in $latestTags)
-{
-  $semVars += $tags.Replace("Azure.Template_", "")
-}
-
-$semVarsSorted = [AzureEngSemanticVersion]::SortVersionStrings($semVars)
-LogDebug "Last Published Version $($semVarsSorted[0])"
-
-$newVersion = [AzureEngSemanticVersion]::ParseVersionString($semVarsSorted[0])
-$newVersion.IncrementAndSetToPrerelease()
-LogDebug "Version to publish [ $($newVersion.ToString()) ]"
+$newVersion = "1.0.3-beta.$PreviewVersionNumber"
 
 &"${PSScriptRoot}/Update-PkgVersion.ps1" -ServiceDirectory "template" `
--PackageName 'Azure.Template' -PackageDirName "Azure.Template" -NewVersionString $newVersion.ToString() `
+-PackageName 'Azure.Template' -PackageDirName "Azure.Template" -NewVersionString $newVersion `
 -ReleaseDate (Get-Date -f "yyyy-MM-dd")
-Set-Content -Path $changeLogFile -Value @"
-# Release History
-## $($newVersion.ToString()) ($(Get-Date -f "yyyy-MM-dd"))
-- Test Release Pipeline
-"@
+Set-TestChangeLog -TestVersion $newVersion -changeLogFile $changeLogFile -ReleaseEntry "Test Release Pipeline"
