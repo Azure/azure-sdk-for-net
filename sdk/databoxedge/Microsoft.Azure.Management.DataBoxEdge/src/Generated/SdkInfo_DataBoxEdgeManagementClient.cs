bb560d7b3e1f2b4c2cbba8dd3f4796807e3597c9
SDK Automation <sdkautomation@microsoft.com>
2020-10-23 09:30:27 +0000

    Generated from fe8321f89cad17d70f0da2f3d3a1a4d05f1a8ac1
    DataboxEdge: update readme.java.md

--- sdk/databoxedge/Microsoft.Azure.Management.DataBoxEdge/src/Generated/SdkInfo_DataBoxEdgeManagementClient.cs
+++ sdk/databoxedge/Microsoft.Azure.Management.DataBoxEdge/src/Generated/SdkInfo_DataBoxEdgeManagementClient.cs
@@ -38,16 +38,5 @@ namespace Microsoft.Azure.Management.DataBoxEdge
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4407";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/databoxedge/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=D:\\Code\\azure-sdk-for-net\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "ef354ec8d6580227707ed935684e533b898beabe";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
