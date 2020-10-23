3ce03d49dfd48a357a0e914d6c9a479db1896a6d
Pavel Krymets <pavel@krymets.com>
2020-10-23 15:30:51 -0700

    Produce ActivitySource events

--- sdk/core/Azure.Core/src/Shared/DiagnosticScope.cs
+++ sdk/core/Azure.Core/src/Shared/DiagnosticScope.cs
@@ -4,6 +4,7 @@
 #nullable enable
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
@@ -12,46 +13,70 @@ namespace Azure.Core.Pipeline
 {
     internal readonly struct DiagnosticScope : IDisposable
     {
-        private readonly DiagnosticActivity? _activity;
-
-        private readonly string _name;
+        private static readonly ConcurrentDictionary<string, ActivitySource> _activitySources = new ConcurrentDictionary<string, ActivitySource>();
+        private readonly DiagnosticActivity? _oldStyleActivity;
 
         private readonly DiagnosticListener _source;
+        private readonly ActivitySourceAdapter? _activityAdapter;
 
-        internal DiagnosticScope(string name, DiagnosticListener source)
+        internal DiagnosticScope(string ns, string name, DiagnosticListener source)
         {
-            _name = name;
+            _activityAdapter = null;
             _source = source;
-            _activity = _source.IsEnabled(_name) ? new DiagnosticActivity(_name) : null;
-            _activity?.SetW3CFormat();
+            _oldStyleActivity = _source.IsEnabled(name) ? new DiagnosticActivity(name) : null;
+            _oldStyleActivity?.SetW3CFormat();
+
+            int indexOfDot = name.IndexOf(".");
+            if (indexOfDot != -1)
+            {
+#if DEBUG
+                throw new ArgumentException("Invalid scope name, expected ClassName.MethodName");
+#else
+                return;
+#endif
+            }
+
+            string clientName = ns + "." + name.Substring(0, indexOfDot);
+            string methodName = name.Substring(indexOfDot);
+
+            var currentSource = _activitySources.GetOrAdd(clientName, n => new ActivitySource(n));
+            if (currentSource != null)
+            {
+                _activityAdapter = new ActivitySourceAdapter(currentSource, methodName);
+            }
         }
 
-        public bool IsEnabled => _activity != null;
+        public bool IsEnabled => _oldStyleActivity != null || _activityAdapter?.HasListeners() == true;
 
         public void AddAttribute(string name, string value)
         {
-            _activity?.AddTag(name, value);
+            _oldStyleActivity?.AddTag(name, value);
+            _activityAdapter?.AddTag(name, value);
         }
 
         public void AddAttribute<T>(string name, T value)
         {
-            if (_activity != null && value != null)
+            if (_oldStyleActivity != null && value != null)
             {
                 AddAttribute(name, value.ToString() ?? string.Empty);
             }
+
+            _activityAdapter?.AddTag(name, value);
         }
 
         public void AddAttribute<T>(string name, T value, Func<T, string> format)
         {
-            if (_activity != null)
+            if (_oldStyleActivity != null || _activityAdapter != null)
             {
-                AddAttribute(name, format(value));
+                var formattedValue = format(value);
+                AddAttribute(name, formattedValue);
+                _activityAdapter?.AddTag(name, formattedValue);
             }
         }
 
         public void AddLink(string id, IDictionary<string, string>? attributes = null)
         {
-            if (_activity != null)
+            if (_oldStyleActivity != null)
             {
                 var linkedActivity = new Activity("LinkedActivity");
                 linkedActivity.SetW3CFormat();
@@ -65,53 +90,53 @@ namespace Azure.Core.Pipeline
                     }
                 }
 
-                _activity.AddLink(linkedActivity);
+                _oldStyleActivity.AddLink(linkedActivity);
             }
+
+            _activityAdapter?.AddLink(id, attributes);
         }
 
         public void Start()
         {
-            if (_activity != null)
+            if (_oldStyleActivity != null)
             {
-                _source.StartActivity(_activity, _activity);
+                _source.StartActivity(_oldStyleActivity, _oldStyleActivity);
             }
+
+            _activityAdapter?.Start();
         }
 
-        public void SetStartTime(DateTime dateTime) => _activity?.SetStartTime(dateTime);
+        public void SetStartTime(DateTime dateTime)
+        {
+            _oldStyleActivity?.SetStartTime(dateTime);
+            _activityAdapter?.SetStartTime(dateTime);
+        }
 
         public void Dispose()
         {
-            if (_activity == null)
+            if (_oldStyleActivity != null)
             {
-                return;
+                _source.StopActivity(_oldStyleActivity, null);
             }
 
-            if (_source != null)
-            {
-                _source.StopActivity(_activity, null);
-            }
-            else
-            {
-                _activity?.Stop();
-            }
+            _activityAdapter?.Dispose();
         }
 
         public void Failed(Exception e)
         {
-            if (_activity == null)
+            if (_oldStyleActivity != null)
             {
-                return;
+                _source?.Write(_oldStyleActivity.OperationName + ".Exception", e);
             }
 
-            _source?.Write(_activity.OperationName + ".Exception", e);
-
+            _activityAdapter?.MarkFailed(e);
         }
 
         private class DiagnosticActivity : Activity
         {
             private List<Activity>? _links;
 
-            public IEnumerable<Activity> Links => (IEnumerable<Activity>?)_links ?? Array.Empty<Activity>();
+            public new IEnumerable<Activity> Links => (IEnumerable<Activity>?)_links ?? Array.Empty<Activity>();
 
             public DiagnosticActivity(string operationName) : base(operationName)
             {
@@ -125,8 +150,65 @@ namespace Azure.Core.Pipeline
         }
     }
 
+    internal class ActivitySourceAdapter
+    {
+        private readonly ActivitySource _activitySource;
+        private readonly string _activityName;
+        private Activity? _currentActivity;
+        private ActivityTagsCollection? _tagCollection;
+        private DateTimeOffset? _startTime;
+        private List<ActivityLink>? _linkCollection;
+
+        public ActivitySourceAdapter(ActivitySource activitySource, string activityName)
+        {
+            _activitySource = activitySource;
+            _activityName = activityName;
+        }
+
+        public void AddTag<T>(string name, T value)
+        {
+            _tagCollection ??= new ActivityTagsCollection();
+            _tagCollection.Add(name, value);
+        }
+
+        public void AddLink(string id, IDictionary<string,string>? attributes)
+        {
+            _linkCollection = new List<ActivityLink>();
+
+            ActivityTagsCollection? linkTagsCollection = null;
+            if (attributes != null)
+            {
+                linkTagsCollection ??= new ActivityTagsCollection();
+            }
+
+            _linkCollection.Add(new ActivityLink(ActivityContext.Parse(id, null), linkTagsCollection));
+        }
+
+        public bool HasListeners() => _activitySource.HasListeners();
+
+        public void Start()
+        {
+            _currentActivity = _activitySource.StartActivity(_activityName, startTime: _startTime);
+        }
+
+        public void SetStartTime(DateTime startTime)
+        {
+            _startTime = startTime;
+        }
+
+        public void MarkFailed(Exception exception)
+        {
+            _currentActivity?.AddTag("exception", exception);
+        }
+
+        public void Dispose()
+        {
+            _currentActivity?.Dispose();
+        }
+    }
+
     /// <summary>
-    /// HACK HACK HACK. Some runtime environments like Azure.Functions downgrade System.Diagnostic.DiagnosticSource package version causing method not found exceptions in customer apps
+    /// WORKAROUND. Some runtime environments like Azure.Functions downgrade System.Diagnostic.DiagnosticSource package version causing method not found exceptions in customer apps
     /// This type is a temporary workaround to avoid the issue.
     /// </summary>
     internal static class ActivityExtensions
