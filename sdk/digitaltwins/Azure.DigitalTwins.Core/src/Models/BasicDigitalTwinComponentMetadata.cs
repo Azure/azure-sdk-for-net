29a9554fc1831d9643a36dbc4d79be3a792f2b35
David R. Williamson <drwill@microsoft.com>
2020-10-27 13:25:58 -0700

    ADT: Add basic component metadata type

--- sdk/digitaltwins/Azure.DigitalTwins.Core/src/Models/BasicDigitalTwinComponentMetadata.cs
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/src/Models/BasicDigitalTwinComponentMetadata.cs
@@ -7,22 +7,16 @@ using System.Text.Json.Serialization;
 namespace Azure.DigitalTwins.Core
 {
     /// <summary>
-    /// An optional, helper class for deserializing a digital twin.
-    /// The $metadata class on a <see cref="BasicDigitalTwin"/>.
+    /// An optional, helper class for deserializing a digital twin component metadata object.
+    /// The $metadata object on a <see cref="BasicDigitalTwinComponent"/>.
     /// </summary>
     /// <remarks>
     /// For more samples, see <see href="https://github.com/Azure/azure-sdk-for-net/tree/master/sdk/digitaltwins/Azure.DigitalTwins.Core/samples">our repo samples</see>.
     /// </remarks>
-    public class BasicDigitalTwinMetadata
+    public class BasicDigitalTwinComponentMetadata
     {
         /// <summary>
-        /// The Id of the model that the digital twin or component is modeled by.
-        /// </summary>
-        [JsonPropertyName(DigitalTwinsJsonPropertyNames.MetadataModel)]
-        public string ModelId { get; set; }
-
-        /// <summary>
-        /// This field will contain metadata about changes on properties on the digital twin.
+        /// This field will contain metadata about changes on properties on a component.
         /// For your convenience, the value can be deserialized into <see cref="BasicDigitalTwinPropertyMetadata"/>.
         /// </summary>
         [JsonExtensionData]
