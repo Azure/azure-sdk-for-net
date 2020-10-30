8b3542f0cf576714ba7a70292dddd1e9e7c4e227
Kristyn Hamasaki <kristynh@microsoft.com>
2020-10-29 17:04:15 -1000

    Change typo in status codes + rerun tests

--- sdk/cosmosdb/Microsoft.Azure.Management.CosmosDB/src/Generated/SqlResourcesOperations.cs
+++ sdk/cosmosdb/Microsoft.Azure.Management.CosmosDB/src/Generated/SqlResourcesOperations.cs
@@ -3570,11 +3570,11 @@ namespace Microsoft.Azure.Management.CosmosDB
             HttpStatusCode _statusCode = _httpResponse.StatusCode;
             cancellationToken.ThrowIfCancellationRequested();
             string _responseContent = null;
-            if (!((int)_statusCode != 200 && (int)_statusCode != 202))
+            if ((int)_statusCode == 200)
             {
                 _httpRequest.RequestUri = _originRequestUri;
             }
-            if ((int)_statusCode != 200 && (int)_statusCode != 202)
+            if ((int)_statusCode != 200)
             {
                 var ex = new DefaultErrorResponseException(string.Format("Operation returned an invalid status code '{0}'", _statusCode));
                 try
@@ -3854,11 +3854,11 @@ namespace Microsoft.Azure.Management.CosmosDB
             HttpStatusCode _statusCode = _httpResponse.StatusCode;
             cancellationToken.ThrowIfCancellationRequested();
             string _responseContent = null;
-            if (!((int)_statusCode != 200 && (int)_statusCode != 202))
+            if ((int)_statusCode == 200)
             {
                 _httpRequest.RequestUri = _originRequestUri;
             }
-            if ((int)_statusCode != 200 && (int)_statusCode != 202)
+            if ((int)_statusCode != 200)
             {
                 var ex = new DefaultErrorResponseException(string.Format("Operation returned an invalid status code '{0}'", _statusCode));
                 try
@@ -5126,11 +5126,11 @@ namespace Microsoft.Azure.Management.CosmosDB
             HttpStatusCode _statusCode = _httpResponse.StatusCode;
             cancellationToken.ThrowIfCancellationRequested();
             string _responseContent = null;
-            if (!((int)_statusCode != 202 && (int)_statusCode != 204))
+            if (!((int)_statusCode != 200 && (int)_statusCode != 202))
             {
                 _httpRequest.RequestUri = _originRequestUri;
             }
-            if ((int)_statusCode != 202 && (int)_statusCode != 204)
+            if ((int)_statusCode != 200 && (int)_statusCode != 202)
             {
                 var ex = new CloudException(string.Format("Operation returned an invalid status code '{0}'", _statusCode));
                 try
@@ -5649,11 +5649,11 @@ namespace Microsoft.Azure.Management.CosmosDB
             HttpStatusCode _statusCode = _httpResponse.StatusCode;
             cancellationToken.ThrowIfCancellationRequested();
             string _responseContent = null;
-            if (!((int)_statusCode != 200 && (int)_statusCode != 202))
+            if (!((int)_statusCode != 202 && (int)_statusCode != 204))
             {
                 _httpRequest.RequestUri = _originRequestUri;
             }
-            if ((int)_statusCode != 200 && (int)_statusCode != 202)
+            if ((int)_statusCode != 202 && (int)_statusCode != 204)
             {
                 var ex = new CloudException(string.Format("Operation returned an invalid status code '{0}'", _statusCode));
                 try
