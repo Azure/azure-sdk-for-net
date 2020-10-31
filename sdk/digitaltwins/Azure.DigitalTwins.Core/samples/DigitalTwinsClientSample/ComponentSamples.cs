e51a18d256956f7d489cb919863d2f7b30e29c92
David R. Williamson <drwill@microsoft.com>
2020-10-31 15:24:06 -0700

    ADT: code clean up

--- sdk/digitaltwins/Azure.DigitalTwins.Core/samples/DigitalTwinsClientSample/ComponentSamples.cs
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/samples/DigitalTwinsClientSample/ComponentSamples.cs
@@ -2,9 +2,6 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Text.Json;
 using System.Threading.Tasks;
 using static Azure.DigitalTwins.Core.Samples.SampleLogger;
