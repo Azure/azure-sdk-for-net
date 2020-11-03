4b0c330ff914324cbb1489158149062fa02d6564
Josh Lai <joshlai@microsoft.com>
2020-11-03 17:34:07 -0500

    [TokenCredentialStub] Copying the TokenCredentialStub so it can be shared

--- sdk/core/Azure.Core/tests/BearerTokenAuthenticationPolicyTests.cs
+++ sdk/core/Azure.Core/tests/BearerTokenAuthenticationPolicyTests.cs
@@ -538,31 +538,5 @@ namespace Azure.Core.Tests
 
             Assert.CatchAsync<InvalidOperationException>(async () => await firstRequestTask);
         }
-
-        private class TokenCredentialStub : TokenCredential
-        {
-            public TokenCredentialStub(Func<TokenRequestContext, CancellationToken, AccessToken> handler, bool isAsync)
-            {
-                if (isAsync)
-                {
-#pragma warning disable 1998
-                    _getTokenAsyncHandler = async (r, c) => handler(r, c);
-#pragma warning restore 1998
-                }
-                else
-                {
-                    _getTokenHandler = handler;
-                }
-            }
-
-            private readonly Func<TokenRequestContext, CancellationToken, ValueTask<AccessToken>> _getTokenAsyncHandler;
-            private readonly Func<TokenRequestContext, CancellationToken, AccessToken> _getTokenHandler;
-
-            public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
-                => _getTokenAsyncHandler(requestContext, cancellationToken);
-
-            public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
-                => _getTokenHandler(requestContext, cancellationToken);
-        }
     }
 }
