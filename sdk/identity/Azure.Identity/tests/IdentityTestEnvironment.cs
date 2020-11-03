14d4f59db2bc5d0024792c0e1fa06aeb58cc4467
Scott Schaab <sschaab@microsoft.com>
2020-11-03 15:47:16 -0800

    removing unsused test helper code

--- sdk/identity/Azure.Identity/tests/IdentityTestEnvironment.cs
+++ sdk/identity/Azure.Identity/tests/IdentityTestEnvironment.cs
@@ -31,7 +31,5 @@ namespace Azure.Identity.Tests
         public string ServicePrincipalCertificatePfxPath => GetOptionalVariable("IDENTITY_SP_CERT_PFX") ?? Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "cert.pfx");
         public string ServicePrincipalCertificatePemPath => GetOptionalVariable("IDENTITY_SP_CERT_PEM") ?? Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "cert.pem");
         public string ServicePrincipalSniCertificatePath => GetOptionalVariable("IDENTITY_SP_CERT_SNI") ?? Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "cert.pfx");
-
-        public string NewId() => Guid.NewGuid().ToString();
     }
 }
