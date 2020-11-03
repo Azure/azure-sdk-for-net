8835f8339c67a0eaeb87d8fe8fb0b463614b3cc1
Cameron Taggart <cameron.taggart@microsoft.com>
2020-11-03 14:25:56 -0600

    add AssemblyFileVersion

---
+++ sdk/avs/Microsoft.Azure.Management.Avs/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyFileVersion("1.0.0.1")]
