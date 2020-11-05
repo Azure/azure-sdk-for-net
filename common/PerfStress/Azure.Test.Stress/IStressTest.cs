55519edd7cbe186e0640e15dca2d7d03c2b1279b
Mike Harder <mharder@microsoft.com>
2020-11-04 21:36:23 -0800

    Initial import of Azure.Test.Stress

---
+++ common/PerfStress/Azure.Test.Stress/IStressTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Test.Stress
+{
+    internal interface IStressTest : IDisposable, IAsyncDisposable
+    {
+        Task SetupAsync();
+        Task RunAsync(CancellationToken cancellationToken);
+        Task CleanupAsync();
+    }
+}
