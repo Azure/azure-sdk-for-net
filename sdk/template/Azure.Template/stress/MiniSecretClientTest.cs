617d8e9cd3f134a147b93772444b36cc9c1ff9b0
Mike Harder <mharder@microsoft.com>
2020-11-05 15:04:06 -0800

    Add Azure.Template.Stress

---
+++ sdk/template/Azure.Template/stress/MiniSecretClientTest.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Identity;
+using Azure.Test.Stress;
+using CommandLine;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Template.Stress
+{
+    public class MiniSecretClientTest : StressTest<MiniSecretClientTest.MiniSecretClientOptions, MiniSecretClientTest.MiniSecretClientMetrics>
+    {
+        protected string KeyVaultUri { get; private set; }
+
+        public MiniSecretClientTest(MiniSecretClientOptions options, MiniSecretClientMetrics metrics) : base(options, metrics)
+        {
+            KeyVaultUri = GetEnvironmentVariable("KEYVAULT_URL");
+        }
+
+        public override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            var client = new MiniSecretClient(new Uri(KeyVaultUri), new DefaultAzureCredential());
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    // Throttle requests to avoid exceeding service limits
+                    await Task.Delay(TimeSpan.FromMilliseconds(Options.Delay), cancellationToken);
+
+                    var secret = await client.GetSecretAsync(Options.SecretName, cancellationToken);
+                    Interlocked.Increment(ref Metrics.SecretsReceived);
+
+                    if (secret.Value.Value == "TestValue")
+                    {
+                        Interlocked.Increment(ref Metrics.CorrectValues);
+                    }
+                    else
+                    {
+                        Interlocked.Increment(ref Metrics.IncorrectValues);
+                    }
+                }
+                catch (Exception e) when (ContainsOperationCanceledException(e))
+                {
+                }
+                catch (Exception e)
+                {
+                    Metrics.Exceptions.Enqueue(e);
+                }
+            }
+        }
+
+        public class MiniSecretClientMetrics : StressMetrics {
+            public long SecretsReceived;
+            public long CorrectValues;
+            public long IncorrectValues;
+        }
+
+        public class MiniSecretClientOptions : StressOptions {
+            [Option("secret-name", Default = "TestSecret", HelpText = "Name of secret to get")]
+            public string SecretName { get; set; }
+
+            [Option("delay", Default = 100, HelpText = "Delay between gets (milliseconds)")]
+            public int Delay { get; set; }
+        }
+    }
+}
