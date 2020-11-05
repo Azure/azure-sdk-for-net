c1701d196b10590e35898af15b5c9c4d64856f39
kinelski <camaiaor@microsoft.com>
2020-11-05 09:09:04 -0800

    [MetricsAdvisor] Added GetIncidentRootCauses overloads (#16574)
    * Made DataFeed.MetricIds a Dictionary
    
    * Added two more GetIncidentRootCauses tests

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/tests/MetricsAdvisorLiveTestBase.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/tests/MetricsAdvisorLiveTestBase.cs
@@ -25,10 +25,16 @@ namespace Azure.AI.MetricsAdvisor.Tests
 
         internal const string DetectionConfigurationId = "fb5a6ed6-2b9e-4b72-8b0c-0046ead1c15c";
         internal const string IncidentId = "736eed64368bb6a372e855322a15a736-174e1756000";
-        internal const string AlertConfigurationId = "08318302-6006-4019-9afc-975bc63ee566";
-        internal const string AlertId = "174995c5800";
+        internal const string AlertConfigurationId = "204a211a-c5f4-45f3-a30e-512fb25d1d2c";
+        internal const string AlertId = "17571a77000";
         internal const string MetricId = "3d48ed3e-6e6e-4391-b78f-b00dfee1e6f5";
         internal const string DataFeedId = "0072a752-1476-4cfa-8cf0-f226995201a0";
+
+        protected int MaximumSamplesCount => 10;
+
+        protected DateTimeOffset SamplingStartTime => DateTimeOffset.Parse("2020-10-01T00:00:00Z");
+
+        protected DateTimeOffset SamplingEndTime => DateTimeOffset.Parse("2020-10-31T00:00:00Z");
 
         public void InitDataFeedSources()
         {
@@ -73,7 +79,7 @@ namespace Azure.AI.MetricsAdvisor.Tests
         internal async Task<string> CreateDetectionConfiguration(MetricsAdvisorAdministrationClient adminClient)
         {
             DataFeed feed = await GetFirstDataFeed(adminClient).ConfigureAwait(false);
-            AnomalyDetectionConfiguration config = PopulateMetricAnomalyDetectionConfiguration(feed.MetricIds.First());
+            AnomalyDetectionConfiguration config = PopulateMetricAnomalyDetectionConfiguration(feed.MetricIds.First().Value);
 
             return await adminClient.CreateDetectionConfigurationAsync(config).ConfigureAwait(false);
         }
