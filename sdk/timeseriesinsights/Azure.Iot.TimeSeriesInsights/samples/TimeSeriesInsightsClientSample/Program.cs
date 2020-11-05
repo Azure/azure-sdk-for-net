de0f30d6b6856456b841017418d4a9e91139e346
Basel Rustum <barustum@microsoft.com>
2020-11-04 22:48:15 -0800

    Initial setup for Time Series Insights track2 SDK

---
+++ sdk/timeseriesinsights/Azure.Iot.TimeSeriesInsights/samples/TimeSeriesInsightsClientSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeSeriesInsightsClientSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
