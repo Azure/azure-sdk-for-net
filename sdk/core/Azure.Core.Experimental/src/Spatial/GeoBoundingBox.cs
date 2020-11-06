787c0e5e189317058fcf84a332c1057a63f6175c
Michael Yanni <MiYanni@microsoft.com>
2020-11-06 10:57:25 -0800

    Apply suggestions from code review

--- sdk/core/Azure.Core.Experimental/src/Spatial/GeoBoundingBox.cs
+++ sdk/core/Azure.Core.Experimental/src/Spatial/GeoBoundingBox.cs
@@ -11,22 +11,22 @@ namespace Azure.Core.GeoJson
     public sealed class GeoBoundingBox : IEquatable<GeoBoundingBox>
     {
         /// <summary>
-        /// The west-most value of <see cref="GeoObject"/> coordinates.
+        /// The westmost value of <see cref="GeoObject"/> coordinates.
         /// </summary>
         public double West { get; }
 
         /// <summary>
-        /// The south-most value of <see cref="GeoObject"/> coordinates.
+        /// The southmost value of <see cref="GeoObject"/> coordinates.
         /// </summary>
         public double South { get; }
 
         /// <summary>
-        /// The east-most value of <see cref="GeoObject"/> coordinates.
+        /// The eastmost value of <see cref="GeoObject"/> coordinates.
         /// </summary>
         public double East { get; }
 
         /// <summary>
-        /// The north-most value of <see cref="GeoObject"/> coordinates.
+        /// The northmost value of <see cref="GeoObject"/> coordinates.
         /// </summary>
         public double North { get; }
 
