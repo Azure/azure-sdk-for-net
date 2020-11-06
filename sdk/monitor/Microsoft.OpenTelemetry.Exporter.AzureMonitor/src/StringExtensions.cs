b16e550d38adbfa64d81297460db508aba93f60e
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-11-05 22:08:09 -0800

    Rename (#16672)

---
+++ sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/src/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.OpenTelemetry.Exporter.AzureMonitor
+{
+    internal static class StringExtensions
+    {
+        public static string Trim(this string message, int lastIndex)
+        {
+            return message.Substring(0, lastIndex);
+        }
+    }
+}
---
+++ sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/tests/Microsoft.OpenTelemetry.Exporter.AzureMonitor.Integration.Tests/TestFramework/ExpectedTelemetryItemValues.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+
+namespace Microsoft.OpenTelemetry.Exporter.AzureMonitor.Integration.Tests.TestFramework
+{
+    public struct ExpectedTelemetryItemValues
+    {
+        public string Name;
+        public Dictionary<string, string> CustomProperties;
+    }
+}
