9cbc354e0f702c31317f350914cf8add558870b9
Suhas Mehta <suhas.mehta@gmail.com>
2020-11-06 12:17:09 -0800

    Feature/textanalytics5.2 Healthcare  (#16434)
    * Initial Setup working
    
    * Added AnalyzeHealth Operation - build passing
    
    * added healthcare samples sync and async
    
    * fix build error - passing build
    
    * added healthcare cancellation - build passing
    
    * added healthcare samples with cancellation - build passing
    
    * HC samples added - build passing
    
    * Added ShowStats samples for healthcare
    
    * fix tests and build passing
    
    * update names and resolved comments
    
    * resolved warnings for adding docstring
    
    * removed unused functions from CLientCOmmon.cs
    
    * resolve comments
    
    * resolved comments
    
    * resolved comments
    
    * resolved comments
    
    * added async pageablle for pagination
    
    * remove analyze commented code
    
    * updated session record
    
    * update tests endpoint to dev and record tests
    
    * update env variables
    
    * run exportapi.ps1
    
    * Added RecogniseHealthcareEntitiesTests
    
    * fixed async tests
    
    * removed stringindextype customization
    
    * removed exception methods
    
    * added more tests and updated samples
    
    * cleanup
    
    * fixed apidev view issues
    
    * resolved comments
    
    * update session records
    
    * update readme
    
    * Revert "update readme"
    
    This reverts commit dbe7b97dcc92a31863cd23ad04bc84da9a7f2275.
    
    * resolved comments
    
    * run export.ps1
    
    * update session records
    
    * updated docstrings
    
    * run update snippets
    
    * update ../test-resources.json
    
    Co-authored-by: Suhas Mehta <suhas.mehta@microsoft.com>

---
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/JobStatus.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using Azure.Core;
+
+namespace Azure.AI.TextAnalytics
+{
+    /// <summary>
+    /// JobStatus.
+    /// </summary>
+    [CodeGenModel("State")]
+    public partial struct JobStatus
+    {
+
+    }
+}
