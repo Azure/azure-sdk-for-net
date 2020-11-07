da5c1905cc6aa1b2cd912dcf3d2c833366de67b3
Mike Harder <mharder@microsoft.com>
2020-11-06 18:35:43 -0800

    Perf Framework (#16671)

---
+++ common/Perf/Azure.Sample.Perf/ExceptionTest.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Test.Perf;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Sample.Perf
+{
+    // Measures the overhead of creating, throwing, and catching an exception (compared to NoOpTest)
+    public class ExceptionTest : PerfTest<PerfOptions>
+    {
+        public ExceptionTest(PerfOptions options) : base(options)
+        {
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+            try
+            {
+                throw new InvalidOperationException();
+            }
+            catch
+            {
+            }
+        }
+
+        public override Task RunAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                throw new InvalidOperationException();
+            }
+            catch
+            {
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ common/Perf/Azure.Sample.Perf/NoOpTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Test.Perf;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Sample.Perf
+{
+    // Used for measuring the overhead of the perf framework with the fastest possible test
+    public class NoOpTest : PerfTest<PerfOptions>
+    {
+        public NoOpTest(PerfOptions options) : base(options)
+        {
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+        }
+
+        public override Task RunAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
