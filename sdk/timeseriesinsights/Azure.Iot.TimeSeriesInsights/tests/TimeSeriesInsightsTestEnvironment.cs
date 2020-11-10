b024b68e833090850f3b188dee5a5c8d0ada38f3
Basel Rustum <barustum@microsoft.com>
2020-11-10 12:26:10 -0800

    Initial setup for bringing in Time Series Insights into the track2 repo (#16781)

---
+++ sdk/timeseriesinsights/Azure.Iot.TimeSeriesInsights/tests/TimeSeriesInsightsTestEnvironment.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.Iot.TimeSeriesInsights.Tests
+{
+    public class TimeSeriesInsightsTestEnvironment : TestEnvironment
+    {
+        public TimeSeriesInsightsTestEnvironment()
+            : base(TestSettings.TsiEnvironmentVariablesPrefix.ToLower())
+        {
+        }
+
+        public string TimeSeriesInsightsHostname => GetRecordedVariable($"{TestSettings.TsiEnvironmentVariablesPrefix}_URL", options => options.IsSecret(TestUrlSanitizer.FAKE_HOST));
+    }
+}
