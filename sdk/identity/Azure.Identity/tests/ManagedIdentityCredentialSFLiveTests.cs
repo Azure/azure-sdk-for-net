fe025baa44df527a2dd58c3dc466e724c12711a4
Scott Schaab <sschaab@microsoft.com>
2020-11-09 19:58:15 -0800

    adding sfmi test file

---
+++ sdk/identity/Azure.Identity/tests/ManagedIdentityCredentialSFLiveTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Security.KeyVault.Secrets;
+using NUnit.Framework;
+
+namespace Azure.Identity.Tests
+{
+    public class ManagedIdentityCredentialSFLiveTests : IdentityRecordedTestBase
+    {
+        public ManagedIdentityCredentialSFLiveTests(bool isAsync) : base(isAsync)
+        {
+        }
+
+        [NonParallelizable]
+        [Test]
+        public async Task ValidateSystemAssignedIdentity()
+        {
+            if (string.IsNullOrEmpty(TestEnvironment.SFEnable))
+            {
+                Assert.Ignore();
+            }
+
+            var vaultUri = new Uri(TestEnvironment.SystemAssignedVault);
+
+            var cred = new ManagedIdentityCredential(options: InstrumentClientOptions(new TokenCredentialOptions()));
+
+            // Hard code service version or recorded tests will fail: https://github.com/Azure/azure-sdk-for-net/issues/10432
+            var kvoptions = InstrumentClientOptions(new SecretClientOptions(SecretClientOptions.ServiceVersion.V7_0));
+
+            var kvclient = new SecretClient(vaultUri, cred, kvoptions);
+
+            KeyVaultSecret secret = await kvclient.SetSecretAsync("identitytestsecret", "value");
+
+            Assert.IsNotNull(secret);
+        }
+    }
+}
