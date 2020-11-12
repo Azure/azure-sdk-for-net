41bbe3a8b279ef445a855616384b57b963790b06
Mariana Rios Flores <mariari@microsoft.com>
2020-11-11 19:21:31 -0800

    [FR] Add ASP.NET Integration (#16891)
    * add aspnet integration
    
    * exportapi

---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/FormRecognizerClientBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure;
+using Azure.AI.FormRecognizer;
+using Azure.Core.Extensions;
+
+namespace Microsoft.Extensions.Azure
+{
+    /// <summary>
+    /// Extension methods to add <see cref="FormRecognizerClient"/> client to clients builder.
+    /// </summary>
+    public static class FormRecognizerClientBuilderExtensions
+    {
+        /// <summary>
+        /// Registers a <see cref="FormRecognizerClient"/> instance with the provided <paramref name="endpoint"/>.
+        /// </summary>
+        public static IAzureClientBuilder<FormRecognizerClient, FormRecognizerClientOptions> AddFormRecognizerClient<TBuilder>(this TBuilder builder, Uri endpoint)
+            where TBuilder : IAzureClientFactoryBuilderWithCredential
+        {
+            return builder.RegisterClientFactory<FormRecognizerClient, FormRecognizerClientOptions>((options, credential) => new FormRecognizerClient(endpoint, credential, options));
+        }
+
+        /// <summary>
+        /// Registers a <see cref="FormRecognizerClient"/> instance with the provided <paramref name="endpoint"/> and <paramref name="credential"/>.
+        /// </summary>
+        public static IAzureClientBuilder<FormRecognizerClient, FormRecognizerClientOptions> AddFormRecognizerClient<TBuilder>(this TBuilder builder, Uri endpoint, AzureKeyCredential credential)
+            where TBuilder : IAzureClientFactoryBuilder
+        {
+            return builder.RegisterClientFactory<FormRecognizerClient, FormRecognizerClientOptions>(options => new FormRecognizerClient(endpoint, credential, options));
+        }
+
+        /// <summary>
+        /// Registers a <see cref="FormRecognizerClient"/> instance with connection options loaded from the provided <paramref name="configuration"/> instance.
+        /// </summary>
+        public static IAzureClientBuilder<FormRecognizerClient, FormRecognizerClientOptions> AddFormRecognizerClient<TBuilder, TConfiguration>(this TBuilder builder, TConfiguration configuration)
+            where TBuilder : IAzureClientFactoryBuilderWithConfiguration<TConfiguration>
+        {
+            return builder.RegisterClientFactory<FormRecognizerClient, FormRecognizerClientOptions>(configuration);
+        }
+    }
+}
