95c9d70fa86b1b38d3ce5c74781a058c0f95e1de
SDKAuto <sdkautomation@microsoft.com>
2020-11-12 05:14:05 +0000

    CodeGen from PR 11631 in Azure/azure-rest-api-specs fix example (#11631)
    Co-authored-by: Ashraf Hamad <ahamad@ntdev.microsoft.com>

--- sdk/eventgrid/Microsoft.Azure.Management.EventGrid/src/Generated/SdkInfo_EventGridManagementClient.cs
+++ sdk/eventgrid/Microsoft.Azure.Management.EventGrid/src/Generated/SdkInfo_EventGridManagementClient.cs
@@ -30,16 +30,5 @@ namespace Microsoft.Azure.Management.EventGrid
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/eventgrid/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=E:\\build2020\\net1\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "e3e0fff3be9befda4fd2cc18ee355b88b024fac7";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
