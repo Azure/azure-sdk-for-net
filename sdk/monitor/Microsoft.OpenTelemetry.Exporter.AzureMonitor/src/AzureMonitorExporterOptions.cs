3547575eb13adaf07430e338f642e13c5cfa2160
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-11-16 11:17:38 -0800

    Azure Monitor Exporter - Remove unused public properties (#16988)
    * Remove unused public properties
    
    * Update API

--- sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorExporterOptions.cs
+++ sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/src/AzureMonitorExporterOptions.cs
@@ -7,13 +7,6 @@ namespace Microsoft.OpenTelemetry.Exporter.AzureMonitor
 {
     public class AzureMonitorExporterOptions : ClientOptions
     {
-        private const int DefaultCapacityKiloBytes = 50 * 1024;
-        private const long StorageCapacity = DefaultCapacityKiloBytes * 1024;
-
         public string ConnectionString { get; set; }
-
-        public long MaxTransmissionStorageCapacity { get; set; } = StorageCapacity;
-
-        public string StorageFolder { get; set; }
     }
 }
