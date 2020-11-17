61ec39532e1070687f00202ee8190df19ec51e3e
Minnie Liu <peiliu@microsoft.com>
2020-11-17 14:12:23 -0800

    Address review comments

--- sdk/communication/Azure.Communication.Sms/tests/SmsClientTestEnvironment.cs
+++ sdk/communication/Azure.Communication.Sms/tests/SmsClientTestEnvironment.cs
@@ -11,8 +11,8 @@ namespace Azure.Communication.Sms.Tests
         {
         }
 
-        internal const string ConnectionStringEnvironmentVariableName = "COMMUNICATION_LIVETEST_CONNECTION_STRING";
-        internal const string PhoneNumberEnvironmentVariableName = "PHONE_NUMBER";
+        internal const string ConnectionStringEnvironmentVariableName = "AZURE_COMMUNICATION_LIVETEST_CONNECTION_STRING";
+        internal const string PhoneNumberEnvironmentVariableName = "AZURE_PHONE_NUMBER";
 
         public string ConnectionString => GetRecordedVariable(ConnectionStringEnvironmentVariableName);
         public string PhoneNumber => GetRecordedVariable(PhoneNumberEnvironmentVariableName);
