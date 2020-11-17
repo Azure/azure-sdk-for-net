5e414a89a1f4136173525a53e60e58fa422e0342
Mike Harder <mharder@microsoft.com>
2020-11-17 15:46:04 -0800

    Improve comment

--- common/PerfStressShared/PerfStressUtilities.cs
+++ common/PerfStressShared/PerfStressUtilities.cs
@@ -49,7 +49,7 @@ namespace Azure.Test.PerfStress
 
             foreach (var t in testTypes)
             {
-                // Test types must contain a public constructor SampleTest(baseOptionsType)
+                // Test types must contain a public constructor SampleTest(baseOptionsType, ...)
                 Type optionsType = null;
                 foreach (var ctor in t.GetConstructors())
                 {
