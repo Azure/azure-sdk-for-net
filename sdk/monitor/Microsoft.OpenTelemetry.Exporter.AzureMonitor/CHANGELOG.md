5e305e377bae9ec3398553914b5d03491da85ea1
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-11-17 15:24:22 -0800

    Azure Monitor Exporter - Set types for Remote Dependency (#17027)
    * Support dependency types
    
    * Update changelog
    
    * PR feedback

--- sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/CHANGELOG.md
+++ sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## 1.0.0-beta.2 (Unreleased)
 
+* [Set Remote Dependency Telemetry semantics](https://github.com/Azure/azure-sdk-for-net/issues/17026)
 
 ## 1.0.0-beta.1 (2020-11-06)
 
--- sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/src/SemanticConventions.cs
+++ sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/src/SemanticConventions.cs
@@ -103,9 +103,10 @@ namespace Microsoft.OpenTelemetry.Exporter.AzureMonitor
         public const string AttributeDbRedisDatabaseIndex = "db.redis.database_index";
         public const string AttributeDbMongoDbCollection = "db.mongodb.collection";
 
-        public const string AttributeRpcSystem = "rpc.system";
-        public const string AttributeRpcService = "rpc.service";
         public const string AttributeRpcMethod = "rpc.method";
+        public const string AttributeRpcStatus = "rpc.grpc.status_code";
+        public const string AttributeRpcService = "rpc.service";
+        public const string AttributeRpcSystem = "rpc.system";
 
         public const string AttributeMessageType = "message.type";
         public const string AttributeMessageId = "message.id";
