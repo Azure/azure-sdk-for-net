1052b2ecd883b25b8e3a04cc8ac1bca91bd8acf0
SDKAuto <sdkautomation@microsoft.com>
2020-11-18 03:13:12 +0000

    CodeGen from PR 11591 in Azure/azure-rest-api-specs s360 fixes for Azure DNS (#11591)
    * s360 fixes for Azure DNS
    
    * Additional property
    
    * Property fix
    
    * Readonly property
    
    * CloudError fix
    
    * Examples. cloud error
    
    * Avocado fix
    
    * read.md
    
    * Prettier

--- sdk/privatedns/Microsoft.Azure.Management.PrivateDns/src/Generated/PrivateDnsManagementClient.cs
+++ sdk/privatedns/Microsoft.Azure.Management.PrivateDns/src/Generated/PrivateDnsManagementClient.cs
@@ -94,6 +94,19 @@ namespace Microsoft.Azure.Management.PrivateDns
         /// <summary>
         /// Initializes a new instance of the PrivateDnsManagementClient class.
         /// </summary>
+        /// <param name='httpClient'>
+        /// HttpClient to be used
+        /// </param>
+        /// <param name='disposeHttpClient'>
+        /// True: will dispose the provided httpClient on calling PrivateDnsManagementClient.Dispose(). False: will not dispose provided httpClient</param>
+        protected PrivateDnsManagementClient(HttpClient httpClient, bool disposeHttpClient) : base(httpClient, disposeHttpClient)
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the PrivateDnsManagementClient class.
+        /// </summary>
         /// <param name='handlers'>
         /// Optional. The delegating handlers to add to the http client pipeline.
         /// </param>
@@ -174,6 +187,33 @@ namespace Microsoft.Azure.Management.PrivateDns
         /// Thrown when a required parameter is null
         /// </exception>
         public PrivateDnsManagementClient(ServiceClientCredentials credentials, params DelegatingHandler[] handlers) : this(handlers)
+        {
+            if (credentials == null)
+            {
+                throw new System.ArgumentNullException("credentials");
+            }
+            Credentials = credentials;
+            if (Credentials != null)
+            {
+                Credentials.InitializeServiceClient(this);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the PrivateDnsManagementClient class.
+        /// </summary>
+        /// <param name='credentials'>
+        /// Required. Credentials needed for the client to connect to Azure.
+        /// </param>
+        /// <param name='httpClient'>
+        /// HttpClient to be used
+        /// </param>
+        /// <param name='disposeHttpClient'>
+        /// True: will dispose the provided httpClient on calling PrivateDnsManagementClient.Dispose(). False: will not dispose provided httpClient</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when a required parameter is null
+        /// </exception>
+        public PrivateDnsManagementClient(ServiceClientCredentials credentials, HttpClient httpClient, bool disposeHttpClient) : this(httpClient, disposeHttpClient)
         {
             if (credentials == null)
             {
--- sdk/privatedns/Microsoft.Azure.Management.PrivateDns/src/Generated/SdkInfo_PrivateDnsManagementClient.cs
+++ sdk/privatedns/Microsoft.Azure.Management.PrivateDns/src/Generated/SdkInfo_PrivateDnsManagementClient.cs
@@ -25,16 +25,5 @@ namespace Microsoft.Azure.Management.PrivateDns
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "latest";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4283";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/privatedns/resource-manager/readme.md --csharp --version=latest --reflect-api-versions --csharp-sdks-folder=D:\\Git\\SDK\\azure-sdk-for-net\\src\\SDKs";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "ee4c2430123161a232d3a75cea368471cbd79f50";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
