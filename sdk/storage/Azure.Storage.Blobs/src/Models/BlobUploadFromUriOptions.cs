4a07d6b4fff20cc9d0f4b156b986d57af5a887da
Amanda Nguyen <amnguye@microsoft.com>
2020-11-19 10:04:13 -0800

    PR comments

--- sdk/storage/Azure.Storage.Blobs/src/Models/BlobUploadFromUriOptions.cs
+++ sdk/storage/Azure.Storage.Blobs/src/Models/BlobUploadFromUriOptions.cs
@@ -55,8 +55,8 @@ namespace Azure.Storage.Blobs.Models
         public AccessTier? AccessTier { get; set; }
 
         /// <summary>
-        /// Optional. An MD5 hash of the block content. This hash is used to verify the integrity of the block during
-        /// transport.When this header is specified, the storage service compares the hash of the content that has arrived
+        /// Optional. An MD5 hash of the content. This hash is used to verify the integrity of the content during
+        /// transport. When this header is specified, the storage service compares the hash of the content that has arrived
         /// with this header value. Note that this MD5 hash is not stored with the blob.If the two hashes do not match, the
         /// operation will fail.
         /// </summary>
