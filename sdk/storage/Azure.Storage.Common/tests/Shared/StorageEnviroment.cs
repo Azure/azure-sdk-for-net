a1b09778ba5f1745f8ee1f1318f12dbb5e817a0f
bquantump <53361486+bquantump@users.noreply.github.com>
2020-11-19 17:56:27 -0800

    Fix storage

---
+++ sdk/storage/Azure.Storage.Common/tests/Shared/StorageEnviroment.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.Storage.Test.Shared
+{
+    public class StorageEnviroment : TestEnvironment
+    {
+        public StorageEnviroment() : base("storage")
+        {
+        }
+    }
+}
