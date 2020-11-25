17ea19afb700c10e06bdc3d22b9d4afcbb296864
Jesse Squire <jesse.squire@gmail.com>
2020-11-25 15:54:11 -0500

    WIP | S04, enable all but checkpoint

--- sdk/eventhub/Azure.Messaging.EventHubs.Processor/tests/Snippets/Sample04_ProcessingEventsLiveTests.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs.Processor/tests/Snippets/Sample04_ProcessingEventsLiveTests.cs
@@ -230,8 +230,8 @@ namespace Azure.Messaging.EventHubs.Tests.Snippets
                 eventHubsConnectionString,
                 eventHubName);
 
-            //const int EventsBeforeCheckpoint = 25;
-            //var partitionEventCount = new ConcurrentDictionary<string, int>();
+            const int EventsBeforeCheckpoint = 25;
+            var partitionEventCount = new ConcurrentDictionary<string, int>();
 
             async Task processEventHandler(ProcessEventArgs args)
             {
@@ -247,18 +247,18 @@ namespace Azure.Messaging.EventHubs.Tests.Snippets
                     // checkpointing threshold, a new checkpoint will be
                     // created and the count reset.
 
-                    //string partition = args.Partition.PartitionId;
+                    string partition = args.Partition.PartitionId;
 
-                    //int eventsSinceLastCheckpoint = partitionEventCount.AddOrUpdate(
-                    //    key: partition,
-                    //    addValue: 1,
-                    //    updateValueFactory: (_, currentCount) => currentCount + 1);
+                    int eventsSinceLastCheckpoint = partitionEventCount.AddOrUpdate(
+                        key: partition,
+                        addValue: 1,
+                        updateValueFactory: (_, currentCount) => currentCount + 1);
 
-                    //if (eventsSinceLastCheckpoint >= EventsBeforeCheckpoint)
-                    //{
-                    //    await args.UpdateCheckpointAsync();
-                    //    partitionEventCount[partition] = 0;
-                    //}
+                    if (eventsSinceLastCheckpoint >= EventsBeforeCheckpoint)
+                    {
+                        //await args.UpdateCheckpointAsync();
+                        partitionEventCount[partition] = 0;
+                    }
                 }
                 catch (Exception ex)
                 {
