8cdf112f10b3de0befb8bfcdf04f56fdbc297c26
pavelprystinka <30868871+pavelprystinka@users.noreply.github.com>
2020-11-24 18:13:46 -0800

    [Communication] - PhoneNumberAdministrationClient - adding live tests (#16962)

---
+++ sdk/communication/Azure.Communication.Administration/tests/Infrastructure/PhoneNumberAdministrationClientTestEnvironment.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.Communication.Administration.Tests
+{
+    public class PhoneNumberAdministrationClientTestEnvironment : TestEnvironment
+    {
+        public PhoneNumberAdministrationClientTestEnvironment() : base("communication")
+        {
+        }
+
+        private const string ConnectionStringEnvironmentVariableName = "COMMUNICATION_CONNECTION_STRING";
+
+        public string ConnectionString => GetRecordedVariable(ConnectionStringEnvironmentVariableName);
+    }
+}
