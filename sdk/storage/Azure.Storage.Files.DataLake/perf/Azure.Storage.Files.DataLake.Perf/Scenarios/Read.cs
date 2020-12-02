700b962480b72fa1262045afc126e9c596ac5202
Jesse Squire <jesse.squire@gmail.com>
2020-12-02 12:30:34 -0500

    [Data Lake Performance] Test Scenarios for T2
    The focus of these changes is to create the initial set of performance
    test scenarios for the v12.x (track two) Data Lake client library.

---
+++ sdk/storage/Azure.Storage.Files.DataLake/perf/Azure.Storage.Files.DataLake.Perf/Scenarios/Read.cs
@@ -0,0 +1,117 @@
+﻿//Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Test.Perf;
+
+namespace Azure.Storage.Files.DataLake.Perf.Scenarios
+{
+    /// <summary>
+    ///   The performance test scenario focused on reading from Data Lake storage.
+    /// </summary>
+    ///
+    /// <seealso cref="Azure.Test.Perf.PerfTest{SizeOptions}" />
+    ///
+    public sealed class Read : PerfTest<SizeOptions>
+    {
+        /// <summary>
+        ///   The ambient test environment associated with the current execution.
+        /// </summary>
+        ///
+        private static PerfTestEnvironment TestEnvironment { get; } = PerfTestEnvironment.Instance;
+
+        /// <summary>
+        ///   The name of the file to use across parallel executions of the scenario.
+        /// </summary>
+        ///
+        private static string Filename { get; } = Path.GetRandomFileName();
+
+        /// <summary>
+        ///   The name of the file system to use across parallel executions of the scenario.
+        /// </summary>
+        ///
+        private static string FileSystemName { get; } = Guid.NewGuid().ToString();
+
+        /// <summary>
+        ///   The client for interaction with the Data Lake file system.
+        /// </summary>
+        ///
+        private DataLakeFileSystemClient FileSystemClient { get; }
+
+        /// <summary>
+        ///   The client to use for interactions with the test file.
+        /// </summary>
+        ///
+        private DataLakeFileClient FileClient { get; }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="Read"/> class.
+        /// </summary>
+        ///
+        /// <param name="options">The set of options to consider for configuring the scenario.</param>
+        ///
+        public Read(SizeOptions options) : base(options)
+        {
+            var serviceClient = new DataLakeServiceClient(TestEnvironment.DataLakeServiceUri, TestEnvironment.DataLakeCredential);
+
+            FileSystemClient = serviceClient.GetFileSystemClient(FileSystemName);
+            FileClient = FileSystemClient.GetFileClient(Filename);
+        }
+
+        /// <summary>
+        ///   Performs the tasks needed to initialize and set up the environment for the test scenario.
+        ///   When multiple instances are run in parallel, the setup will take place once, prior to the
+        ///   execution of the first test instance.
+        /// </summary>
+        ///
+        public async override Task GlobalSetupAsync()
+        {
+            await base.GlobalSetupAsync();
+            await FileSystemClient.CreateAsync();
+
+            // Create the test file that will be used for reading.
+
+            using var randomStream = RandomStream.Create(Options.Size);
+
+            await FileClient.CreateAsync();
+            await FileClient.UploadAsync(randomStream, true);
+        }
+
+        /// <summary>
+        ///   Performs the tasks needed to clean up the environment for the test scenario.
+        ///   When multiple instances are run in parallel, the cleanup will take place once,
+        ///   after the execution of all test instances.
+        /// </summary>
+        ///
+        public async override Task GlobalCleanupAsync()
+        {
+            await base.GlobalCleanupAsync();
+            await FileSystemClient.DeleteAsync();
+        }
+
+        /// <summary>
+        ///   Executes the performance test scenario synchronously.
+        /// </summary>
+        ///
+        /// <param name="cancellationToken">The token used to signal when cancellation is requested.</param>
+        ///
+        public override void Run(CancellationToken cancellationToken)
+        {
+            FileClient.Read(cancellationToken);
+        }
+
+        /// <summary>
+        ///   Executes the performance test scenario asynchronously.
+        /// </summary>
+        ///
+        /// <param name="cancellationToken">The token used to signal when cancellation is requested.</param>
+        ///
+        public async override Task RunAsync(CancellationToken cancellationToken)
+        {
+            await FileClient.ReadAsync(cancellationToken);
+        }
+    }
+}
---
+++ sdk/storage/Azure.Storage.Files.DataLake/perf/README.md
@@ -0,0 +1,15 @@
+# Azure Storage Files Data Lake performance tests
+
+The assets in this area comprise a set of performance tests for the [Azure Storage Files Data Lake client library for .NET](https://github.com/Azure/azure-sdk-for-net/tree/master/sdk/storage/Azure.Storage.Files.DataLake) and its associated ecosystem.  The artifacts in this library are intended to be used primarily with the Azure SDK engineering system's testing infrastructure, by may also be run as stand-alone applications from the command line.
+  
+## Contributing  
+
+This project welcomes contributions and suggestions.  Most contributions require you to agree to a Contributor License Agreement (CLA) declaring that you have the right to, and actually do, grant us the rights to use your contribution. For details, visit https://cla.microsoft.com.
+
+When you submit a pull request, a CLA-bot will automatically determine whether you need to provide a CLA and decorate the PR appropriately (e.g., label, comment). Simply follow the instructions provided by the bot. You will only need to do this once across all repos using our CLA.
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+
+Please see our [contributing guide](https://github.com/Azure/azure-sdk-for-net/blob/master/sdk/eventhub/Azure.Messaging.EventHubs/CONTRIBUTING.md) for more information.
+  
+![Impressions](https://azure-sdk-impressions.azurewebsites.net/api/impressions/azure-sdk-for-net%2Fsdk%2Feventhub%2FAzure.Storage.Files.DataLake.Perf%2FREADME.png)
