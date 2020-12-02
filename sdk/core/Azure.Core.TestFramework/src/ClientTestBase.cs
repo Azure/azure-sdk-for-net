a06cf58a4635f8c93edcf29603524fc1ccd7437a
bqauntump <stevens@microsoft.com>
2020-12-01 17:14:14 -0800

    Put rg ref back

--- sdk/core/Azure.Core.TestFramework/src/ClientTestBase.cs
+++ sdk/core/Azure.Core.TestFramework/src/ClientTestBase.cs
@@ -21,7 +21,7 @@ namespace Azure.Core.TestFramework
         private static readonly IInterceptor s_diagnosticScopeValidatingInterceptor = new DiagnosticScopeValidatingInterceptor();
         private static Dictionary<Type, Exception> s_clientValidation = new Dictionary<Type, Exception>();
         public bool IsAsync { get; }
-        
+
         public bool TestDiagnostics { get; set; } = true;
 
         public ClientTestBase(bool isAsync)
