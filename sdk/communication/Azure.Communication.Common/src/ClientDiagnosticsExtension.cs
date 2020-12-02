548c126e8bd060da4771ef79338f3118256bfee3
Pavel Prystinka <pprystinka@microsoft.com>
2020-12-02 15:21:22 -0800

    introduce ClientDiagnostics.RunScoped

---
+++ sdk/communication/Azure.Communication.Common/src/ClientDiagnosticsExtension.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.CompilerServices;
+using Azure.Core.Pipeline;
+
+namespace Azure.Communication
+{
+    internal static class ClientDiagnosticsExtension
+    {
+        internal static T RunScoped<T>(this ClientDiagnostics clientDiagnostics, Func<T> func, string className, [CallerMemberName] string methodName = "")
+        {
+            using DiagnosticScope scope = clientDiagnostics.CreateScope($"{className}.{methodName}");
+            scope.Start();
+            try
+            {
+                return func();
+            }
+            catch (Exception ex)
+            {
+                scope.Failed(ex);
+                throw;
+            }
+        }
+    }
+}
