097705b25123ceebfb056a812fd8346a59864443
azure-sdk <azuresdk@microsoft.com>
2020-12-03 23:04:35 +0000

    Update AutoRest C# version to 1.0.0-alpha.20201203.1

--- sdk/testcommon/Azure.Graph.Rbac/src/Generated/Models/SignInName.cs
+++ sdk/testcommon/Azure.Graph.Rbac/src/Generated/Models/SignInName.cs
@@ -48,7 +48,7 @@ namespace Azure.Graph.Rbac.Models
         public IEnumerable<string> Keys => AdditionalProperties.Keys;
         /// <inheritdoc />
         public IEnumerable<object> Values => AdditionalProperties.Values;
-        /// <inheritdoc />
+        /// <inheritdoc cref="IReadOnlyCollection{T}.Count"/>
         int IReadOnlyCollection<KeyValuePair<string, object>>.Count => AdditionalProperties.Count;
         /// <inheritdoc />
         public object this[string key]
