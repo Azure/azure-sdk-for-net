053e66fad5dfa8c67a91b1424d11062df47f4c1d
SDKAuto <sdkautomation@microsoft.com>
2020-12-07 09:52:49 +0000

    CodeGen from PR 11939 in Azure/azure-rest-api-specs [Hub Generated] Public private branch 'credscan-fix' (#11939)
    * Fix credscan
    
    * Fix more credentials

--- sdk/mysql/Microsoft.Azure.Management.MySQL/src/mysql/Generated/Models/QueryStatistic.cs
+++ sdk/mysql/Microsoft.Azure.Management.MySQL/src/mysql/Generated/Models/QueryStatistic.cs
@@ -34,12 +34,12 @@ namespace Microsoft.Azure.Management.MySQL.Models
         /// <summary>
         /// Initializes a new instance of the QueryStatistic class.
         /// </summary>
-        /// <param name="id">Fully qualified resource Id for the resource. Ex -
+        /// <param name="id">Fully qualified resource ID for the resource. Ex -
         /// /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}</param>
         /// <param name="name">The name of the resource</param>
-        /// <param name="type">The type of the resource. Ex-
-        /// Microsoft.Compute/virtualMachines or
-        /// Microsoft.Storage/storageAccounts.</param>
+        /// <param name="type">The type of the resource. E.g.
+        /// "Microsoft.Compute/virtualMachines" or
+        /// "Microsoft.Storage/storageAccounts"</param>
         /// <param name="queryId">Database query identifier.</param>
         /// <param name="startTime">Observation start time.</param>
         /// <param name="endTime">Observation end time.</param>
