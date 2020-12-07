5745faae3a906a295b2f07ec847bbf5ef670e186
SDKAuto <sdkautomation@microsoft.com>
2020-12-07 09:56:56 +0000

    CodeGen from PR 11939 in Azure/azure-rest-api-specs [Hub Generated] Public private branch 'credscan-fix' (#11939)
    * Fix credscan
    
    * Fix more credentials

--- sdk/postgresql/Microsoft.Azure.Management.PostgreSQL/src/postgresql/Generated/Models/FirewallRule.cs
+++ sdk/postgresql/Microsoft.Azure.Management.PostgreSQL/src/postgresql/Generated/Models/FirewallRule.cs
@@ -36,12 +36,12 @@ namespace Microsoft.Azure.Management.PostgreSQL.Models
         /// firewall rule. Must be IPv4 format.</param>
         /// <param name="endIpAddress">The end IP address of the server
         /// firewall rule. Must be IPv4 format.</param>
-        /// <param name="id">Fully qualified resource Id for the resource. Ex -
+        /// <param name="id">Fully qualified resource ID for the resource. Ex -
         /// /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}</param>
         /// <param name="name">The name of the resource</param>
-        /// <param name="type">The type of the resource. Ex-
-        /// Microsoft.Compute/virtualMachines or
-        /// Microsoft.Storage/storageAccounts.</param>
+        /// <param name="type">The type of the resource. E.g.
+        /// "Microsoft.Compute/virtualMachines" or
+        /// "Microsoft.Storage/storageAccounts"</param>
         public FirewallRule(string startIpAddress, string endIpAddress, string id = default(string), string name = default(string), string type = default(string))
             : base(id, name, type)
         {
