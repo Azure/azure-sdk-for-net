bfa8adc357d6134ad6e2865da5053f3d1b261536
Chris Hamons <chris.hamons@xamarin.com>
2020-12-07 16:20:40 -0600

    Remove snippet infrastructure, this is a sample

--- sdk/synapse/Azure.Analytics.Synapse.Artifacts/tests/samples/Sample1_ExecutePipeline.cs
+++ sdk/synapse/Azure.Analytics.Synapse.Artifacts/tests/samples/Sample1_ExecutePipeline.cs
@@ -12,33 +12,19 @@ using Azure.Core.TestFramework;
 namespace Azure.Analytics.Synapse.Samples
 {
     /// <summary>
-    /// This sample demonstrates how to execute piplines in Azure Synapse Analytics using synchronous methods of <see cref="PipelineClient"/>.
+    /// This sample demonstrates how to execute piplines in Azure Synapse Analytics using asynchronous methods of <see cref="PipelineClient"/>.
     /// </summary>
     public partial class ExecutePipelines
     {
-        private PipelineClient PipelineClient;
-
-        [OneTimeSetUp]
-        public void CreateClient()
-        {
-            // Environment variable with the Synapse workspace endpoint.
-            string workspaceUrl = TestEnvironment.WorkspaceUrl;
-
-            #region Snippet:CreatePipelineClient
-            // Create a new Pipeline client using the default credential from Azure.Identity using environment variables previously set,
-            // including AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, and AZURE_TENANT_ID.
-            PipelineClient client = new PipelineClient(endpoint: new Uri(workspaceUrl), credential: new DefaultAzureCredential());
-            #endregion
-
-            this.PipelineClient = client;
-        }
-
         [Test]
         public async Task CreateAndRunPipeline()
         {
             const string PipelineName = "Test-Pipeline";
             const string JobName = "SparkJobName";
             const string ActivityName = "ActivityName";
+            string workspaceUrl = TestEnvironment.WorkspaceUrl;
+
+            var pipelineClient = new PipelineClient(endpoint: new Uri(workspaceUrl), credential: new DefaultAzureCredential());
 
             var sparkJob = new SynapseSparkJobReference(SparkJobReferenceType.SparkJobDefinitionReference, JobName);
             var activity = new SynapseSparkJobDefinitionActivity(ActivityName, sparkJob);
@@ -46,11 +32,11 @@ namespace Azure.Analytics.Synapse.Samples
             pipelineResource.Activities.Add(activity);
 
             Console.WriteLine("Create pipeline if not already exists.");
-            await PipelineClient.StartCreateOrUpdatePipelineAsync(PipelineName, pipelineResource);
+            await pipelineClient.StartCreateOrUpdatePipelineAsync(PipelineName, pipelineResource);
             Console.WriteLine("Pipeline created");
 
             Console.WriteLine("Running pipeline.");
-            CreateRunResponse runOperation = await PipelineClient.CreatePipelineRunAsync(PipelineName);
+            CreateRunResponse runOperation = await pipelineClient.CreatePipelineRunAsync(PipelineName);
             Console.WriteLine("Run started. ID: {0}", runOperation.RunId);
         }
 
@@ -58,9 +44,12 @@ namespace Azure.Analytics.Synapse.Samples
         public async Task RunPipeline()
         {
             const string PipelineName = "Test-Pipeline";
+            string workspaceUrl = TestEnvironment.WorkspaceUrl;
+
+            var pipelineClient = new PipelineClient(endpoint: new Uri(workspaceUrl), credential: new DefaultAzureCredential());
 
             Console.WriteLine("Running pipeline.");
-            CreateRunResponse runOperation = await PipelineClient.CreatePipelineRunAsync(PipelineName);
+            CreateRunResponse runOperation = await pipelineClient.CreatePipelineRunAsync(PipelineName);
             Console.WriteLine("Run started. ID: {0}", runOperation.RunId);
         }
     }
