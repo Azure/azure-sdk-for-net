5b9caea13add10cf6fbb1813771141cb3857a430
Chidozie Ononiwu <chononiw@microsoft.com>
2020-12-07 13:59:02 -0800

    Update Collect-ChangeLog Script

---
+++ eng/common/scripts/Collect-ChangeLogs.ps1
@@ -0,0 +1,45 @@
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory=$true)]
+  [ValidateRange(1, 12)]
+  [int] $Month
+)
+
+. (Join-Path $PSScriptRoot common.ps1)
+
+$InstallNotes = "";
+$ReleaseNotes = "";
+
+$date = Get-Date -Month $month -Format "yyyy-MM"
+$date += "-\d\d"
+
+$allPackageProps = Get-AllPkgProperties
+
+foreach ($packageProp in $allPackageProps) {
+    $changeLogEntries = Get-ChangeLogEntries -ChangeLogLocation $packageProp.ChangeLogPath
+    $package = $packageProp.Name
+    $serviceDirectory = $packageProp.ServiceDirectory
+
+    foreach ($changeLogEntry in $changeLogEntries.Values) {
+        if ($changeLogEntry.ReleaseStatus -notmatch $date)
+        {
+            continue;
+        }
+
+        $version = $changeLogEntry.ReleaseVersion
+        $githubAnchor = $changeLogEntry.ReleaseTitle.Replace("## ", "").Replace(".", "").Replace("(", "").Replace(")", "").Replace(" ", "-")
+        $textInfo = (Get-Culture).TextInfo
+        $highlightTitle = $textInfo.ToTitleCase($package.Replace("-", " ").Replace("@azure/",""))
+
+        $InstallNotes += GetPackageInstallNotes -Package $package -Version $version
+        $ReleaseNotes += "### $highlightTitle $version [Changelog](https://github.com/Azure/azure-sdk-for-$LanguageShort/blob/master/sdk/$serviceDirectory/$package/CHANGELOG.md#$githubAnchor)`n"
+        $changeLogEntry.ReleaseContent | %{ 
+
+            $ReleaseNotes += $_.Replace("###", "####")
+            $ReleaseNotes += "`n"            
+        }
+        $ReleaseNotes += "`n"
+    }
+}
+
+return $InstallNotes, $ReleaseNotes
