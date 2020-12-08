18bec39f2d2945f2a33fc210fb0493b0c271182c
Chidozie Ononiwu <chononiw@microsoft.com>
2020-12-08 11:33:25 -0800

    Update Collect-ChangeLog.ps1 and Collect-Unreleased.ps1

--- eng/common/scripts/Prepare-Release.ps1
+++ eng/common/scripts/Prepare-Release.ps1
@@ -11,75 +11,75 @@ param(
 
 . ${PSScriptRoot}\common.ps1
 
-function Get-LevenshteinDistance {
-    <#
-        .SYNOPSIS
-            Get the Levenshtein distance between two strings.
-        .DESCRIPTION
-            The Levenshtein Distance is a way of quantifying how dissimilar two strings (e.g., words) are to one another by counting the minimum number of operations required to transform one string into the other.
-        .EXAMPLE
-            Get-LevenshteinDistance 'kitten' 'sitting'
-        .LINK
-            http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#C.23
-            http://en.wikipedia.org/wiki/Edit_distance
-            https://communary.wordpress.com/
-            https://github.com/gravejester/Communary.PASM
-        .NOTES
-            Author: Ã˜yvind Kallstad
-            Date: 07.11.2014
-            Version: 1.0
-    #>
-    [CmdletBinding()]
-    param(
-        [Parameter(Position = 0)]
-        [string]$String1,
-
-        [Parameter(Position = 1)]
-        [string]$String2,
-
-        # Makes matches case-sensitive. By default, matches are not case-sensitive.
-        [Parameter()]
-        [switch] $CaseSensitive,
-
-        # A normalized output will fall in the range 0 (perfect match) to 1 (no match).
-        [Parameter()]
-        [switch] $NormalizeOutput
-    )
-
-    if (-not($CaseSensitive)) {
-        $String1 = $String1.ToLowerInvariant()
-        $String2 = $String2.ToLowerInvariant()
-    }
-
-    $d = New-Object 'Int[,]' ($String1.Length + 1), ($String2.Length + 1)
-    for ($i = 0; $i -le $d.GetUpperBound(0); $i++) {
-        $d[$i,0] = $i
-    }
-
-    for ($i = 0; $i -le $d.GetUpperBound(1); $i++) {
-        $d[0,$i] = $i
-    }
-
-    for ($i = 1; $i -le $d.GetUpperBound(0); $i++) {
-        for ($j = 1; $j -le $d.GetUpperBound(1); $j++) {
-            $cost = [Convert]::ToInt32((-not($String1[$i-1] -ceq $String2[$j-1])))
-            $min1 = $d[($i-1),$j] + 1
-            $min2 = $d[$i,($j-1)] + 1
-            $min3 = $d[($i-1),($j-1)] + $cost
-            $d[$i,$j] = [Math]::Min([Math]::Min($min1,$min2),$min3)
-        }
-    }
-
-    $distance = ($d[$d.GetUpperBound(0),$d.GetUpperBound(1)])
-
-    if ($NormalizeOutput) {
-        return (1 - ($distance) / ([Math]::Max($String1.Length,$String2.Length)))
-    }
-
-    else {
-        return $distance
-    }
-}
+#function Get-LevenshteinDistance {
+#    <#
+#        .SYNOPSIS
+#            Get the Levenshtein distance between two strings.
+#        .DESCRIPTION
+#            The Levenshtein Distance is a way of quantifying how dissimilar two strings (e.g., words) are to one another by counting the minimum number of operations required to transform one string into the other.
+#        .EXAMPLE
+#            Get-LevenshteinDistance 'kitten' 'sitting'
+#        .LINK
+#            http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#C.23
+#            http://en.wikipedia.org/wiki/Edit_distance
+#            https://communary.wordpress.com/
+#            https://github.com/gravejester/Communary.PASM
+#        .NOTES
+#            Author: Ã˜yvind Kallstad
+#            Date: 07.11.2014
+#            Version: 1.0
+#    #>
+#    [CmdletBinding()]
+#    param(
+#        [Parameter(Position = 0)]
+#        [string]$String1,
+#
+#        [Parameter(Position = 1)]
+#        [string]$String2,
+#
+#        # Makes matches case-sensitive. By default, matches are not case-sensitive.
+#        [Parameter()]
+#        [switch] $CaseSensitive,
+#
+#        # A normalized output will fall in the range 0 (perfect match) to 1 (no match).
+#        [Parameter()]
+#        [switch] $NormalizeOutput
+#    )
+#
+#    if (-not($CaseSensitive)) {
+#        $String1 = $String1.ToLowerInvariant()
+#        $String2 = $String2.ToLowerInvariant()
+#    }
+#
+#    $d = New-Object 'Int[,]' ($String1.Length + 1), ($String2.Length + 1)
+#    for ($i = 0; $i -le $d.GetUpperBound(0); $i++) {
+#        $d[$i,0] = $i
+#    }
+#
+#    for ($i = 0; $i -le $d.GetUpperBound(1); $i++) {
+#        $d[0,$i] = $i
+#    }
+#
+#    for ($i = 1; $i -le $d.GetUpperBound(0); $i++) {
+#        for ($j = 1; $j -le $d.GetUpperBound(1); $j++) {
+#            $cost = [Convert]::ToInt32((-not($String1[$i-1] -ceq $String2[$j-1])))
+#            $min1 = $d[($i-1),$j] + 1
+#            $min2 = $d[$i,($j-1)] + 1
+#            $min3 = $d[($i-1),($j-1)] + $cost
+#            $d[$i,$j] = [Math]::Min([Math]::Min($min1,$min2),$min3)
+#        }
+#    }
+#
+#    $distance = ($d[$d.GetUpperBound(0),$d.GetUpperBound(1)])
+#
+#    if ($NormalizeOutput) {
+#        return (1 - ($distance) / ([Math]::Max($String1.Length,$String2.Length)))
+#    }
+#
+#    else {
+#        return $distance
+#    }
+#}
 
 function Get-ReleaseDay($baseDate)
 {
@@ -97,30 +97,31 @@ function Get-ReleaseDay($baseDate)
 
 $ErrorPreference = 'Stop'
 $repoRoot = Resolve-Path "$PSScriptRoot/../..";
-$commonParameter = @("--organization", "https://dev.azure.com/azure-sdk", "-o", "json", "--only-show-errors")
+#$commonParameter = @("--organization", "https://dev.azure.com/azure-sdk", "-o", "json", "--only-show-errors")
 
-if (!(Get-Command az)) {
-  throw 'You must have the Azure CLI installed: https://aka.ms/azure-cli'
-}
-
-az extension show -n azure-devops > $null
-
-if (!$?){
-  throw 'You must have the azure-devops extension run `az extension add --name azure-devops`'
-}
+#if (!(Get-Command az)) {
+#  throw 'You must have the Azure CLI installed: https://aka.ms/azure-cli'
+#}
+#
+#az extension show -n azure-devops > $null
+#
+#if (!$?){
+#  throw 'You must have the azure-devops extension run `az extension add --name azure-devops`'
+#}
 
 $packageProperties = Get-PkgProperties -PackageName $packageName -ServiceDirectory $serviceDirectoryName
 
 Write-Host "Source directory $serviceDirectoryName"
 
-if ($GetExistingPackageVersions -and (Test-Path "Function:$GetExistingPackageVersions"))
+if (Test-Path "Function:GetExistingPackageVersions")
 {
-    $existing = &$GetExistingPackageVersions -PackageName $packageProperties.Name -GroupId $packageProperties.Group
+    $existing = GetExistingPackageVersions -PackageName $packageProperties.Name -GroupId $packageProperties.Group
     if ($null -eq $existing) { $existing = @() }
 }
 else
 {
-    LogError "The function '$GetExistingPackageVersions' was not found."
+    LogError "The function 'GetExistingPackageVersions' was not found."
+    return $null
 }
 
 if (!$ReleaseDate)
@@ -231,65 +232,73 @@ if ($releasing)
     Write-Host
     Write-Host "Updating versions to $newVersion with date $releaseDateString" -ForegroundColor Green
 
-    SetPackageVersion -PackageName $package -Version $newVersion -ServiceName $serviceDirectory -ReleaseDate $releaseDateString `
-    -BuildType $BuildType $GroupName $packageProperties.Group
-
-    $fields = @{
-        "Permalink usetag"="https://github.com/Azure/azure-sdk-for-net/sdk/$serviceDirectory/$package/README.md"
-        "Package Registry Permalink"="https://nuget.org/packages/$package/$newVersion"
-        "Library Type"=$libraryType
-        "Release Type"=$releaseType
-        "Version Number"=$newVersion
-        "Planned Release Date"=$releaseDateString
-        "New Library Only"=$isNew
+    if (Test-Path "Function:SetPackageVersion")
+    {
+        SetPackageVersion -PackageName $package -Version $newVersion -ServiceName $serviceDirectory -ReleaseDate $releaseDateString `
+        -BuildType $BuildType $GroupName $packageProperties.Group
     }
-    $state = "Active"
+    else
+    {
+        LogError "The function 'SetPackageVersion' was not found."
+        return $null
+    }
+
+#    $fields = @{
+#        "Permalink usetag"="https://github.com/Azure/azure-sdk-for-net/sdk/$serviceDirectory/$package/README.md"
+#        "Package Registry Permalink"="https://nuget.org/packages/$package/$newVersion"
+#        "Library Type"=$libraryType
+#        "Release Type"=$releaseType
+#        "Version Number"=$newVersion
+#        "Planned Release Date"=$releaseDateString
+#        "New Library Only"=$isNew
+#    }
+#    $state = "Active"
 }
 else
 {
-    $fields = @{}
-    $state = "Not In Release"
+#    $fields = @{}
+#    $state = "Not In Release"
 }
 
-$workItems = az boards query @commonParameter --project Release --wiql "SELECT [ID], [State], [Iteration Path], [Title] FROM WorkItems WHERE [State] <> 'Closed' AND [Iteration Path] under 'Release\2020\$month' AND [Title] contains '.NET'" | ConvertFrom-Json;
+#$workItems = az boards query @commonParameter --project Release --wiql "SELECT [ID], [State], [Iteration Path], [Title] FROM WorkItems WHERE [State] <> 'Closed' AND [Iteration Path] under 'Release\2020\$month' AND [Title] contains '.NET'" | ConvertFrom-Json;
 
-Write-Host
-Write-Host "The following work items exist:"
-foreach ($item in $workItems)
-{
-    $id = $item.fields."System.ID";
-    $title = $item.fields."System.Title";
-    $path = $item.fields."System.IterationPath";
-    Write-Host "$id - $path - $title"
-}
+#Write-Host
+#Write-Host "The following work items exist:"
+#foreach ($item in $workItems)
+#{
+#    $id = $item.fields."System.ID";
+#    $title = $item.fields."System.Title";
+#    $path = $item.fields."System.IterationPath";
+#    Write-Host "$id - $path - $title"
+#}
 
 # Sort using fuzzy match
-$workItems = $workItems | Sort-Object -property @{Expression = { Get-LevenshteinDistance $_.fields."System.Title" $package -NormalizeOutput }}
-$mostProbable = $workItems | Select-Object -Last 1
+#$workItems = $workItems | Sort-Object -property @{Expression = { Get-LevenshteinDistance $_.fields."System.Title" $package -NormalizeOutput }}
+#$mostProbable = $workItems | Select-Object -Last 1
 
-$issueId = Read-Host -Prompt "Input the work item ID or press Enter to use '$($mostProbable.fields."System.ID") - $($mostProbable.fields."System.Title")' (fuzzy matched based on title)"
+#$issueId = Read-Host -Prompt "Input the work item ID or press Enter to use '$($mostProbable.fields."System.ID") - $($mostProbable.fields."System.Title")' (fuzzy matched based on title)"
 
-if (!$issueId)
-{
-    $issueId = $mostProbable.fields."System.ID"
-}
+#if (!$issueId)
+#{
+#    $issueId = $mostProbable.fields."System.ID"
+#}
+#
+#Write-Host
+#Write-Host "Going to set the following fields:" -ForegroundColor Green
+#Write-Host "    State = $state"
+#
+#foreach ($field in $fields.Keys)
+#{
+#    Write-Host "    $field = $($fields[$field])"
+#}
 
-Write-Host
-Write-Host "Going to set the following fields:" -ForegroundColor Green
-Write-Host "    State = $state"
+#$decision = $Host.UI.PromptForChoice("Updating work item https://dev.azure.com/azure-sdk/Release/_workitems/edit/$issueId", 'Are you sure you want to proceed?', @('&Yes'; '&No'), 0)
 
-foreach ($field in $fields.Keys)
-{
-    Write-Host "    $field = $($fields[$field])"
-}
-
-$decision = $Host.UI.PromptForChoice("Updating work item https://dev.azure.com/azure-sdk/Release/_workitems/edit/$issueId", 'Are you sure you want to proceed?', @('&Yes'; '&No'), 0)
-
-if ($decision -eq 0)
-{
-    az boards work-item update @commonParameter --id $issueId --state $state > $null
-    foreach ($field in $fields.Keys)
-    {
-        az boards work-item update @commonParameter --id $issueId -f "$field=$($fields[$field])" > $null
-    }
-}
+#if ($decision -eq 0)
+#{
+#    az boards work-item update @commonParameter --id $issueId --state $state > $null
+#    foreach ($field in $fields.Keys)
+#    {
+#        az boards work-item update @commonParameter --id $issueId -f "$field=$($fields[$field])" > $null
+#    }
+#}
