935478ebd3d6099b9f57f7767abe07a8d53e5ea3
Frank Li (Wicresoft North America Ltd) <v-cheli@microsoft.com>
2020-12-08 13:34:43 +0800

    Move CloudClipboard from https://github.com/Azure/azure-sdk-for-net-samples to azure-sdk-for-net/samples

---
+++ samples/CloudClipboard/CloudClipboard/CloudClipboard/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using OpenTelemetry.Collector.AspNetCore;
+using OpenTelemetry.Collector.Dependencies;
+using OpenTelemetry.Exporter.ApplicationInsights;
+using OpenTelemetry.Stats;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Trace.Sampler;
+
+namespace CloudClipboard
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Enable distributed tracing
+            string instrumentationKey = Environment.GetEnvironmentVariable("AZURE_INSTRUMENTATION_KEY");
+            new ApplicationInsightsExporter(Tracing.SpanExporter, Stats.ViewManager, new TelemetryConfiguration(instrumentationKey)).Start();
+            using var dependencies = new DependenciesCollector(new DependenciesCollectorOptions(), Tracing.Tracer, Samplers.AlwaysSample);
+            using var requests = new RequestsCollector(new RequestsCollectorOptions(), Tracing.Tracer, Samplers.AlwaysSample);
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ samples/CloudClipboard/CloudClipboard/GarbageCollector/Properties/FutureExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Storage.Blobs;
+
+namespace GarbageCollector
+{
+    // Enable future APIs today
+    public static class FutureExtensions
+    {
+        public static void AddPolicy(this BlobClientOptions options, HttpPipelinePolicy policy, HttpPipelinePosition position)
+        {
+            options.AddPolicy(policy, position);
+        }
+    }
+}
