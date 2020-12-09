c1f68bb0cf9d3dd8a354194f265ad7f166d64137
Azad Abbasi <azabbasi@microsoft.com>
2020-12-09 21:35:57 +0000

    Wait 30 seconds after deployment is done. (#17431)

---
+++ sdk/digitaltwins/test-resources-post.ps1
@@ -0,0 +1,33 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+# IMPORTANT: Do not invoke this file directly. Please instead run eng/New-TestResources.ps1 from the repository root.
+
+#Requires -Version 6.0
+#Requires -PSEdition Core
+
+# Use same parameter names as declared in eng/New-TestResources.ps1 (assume validation therein).
+[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
+param (
+    [Parameter()]
+    [hashtable] $DeploymentOutputs,
+
+    # Captures any arguments from eng/New-TestResources.ps1 not declared here (no parameter errors).
+    [Parameter(ValueFromRemainingArguments = $true)]
+    $RemainingArguments
+)
+
+# By default stop for any error.
+if (!$PSBoundParameters.ContainsKey('ErrorAction')) {
+    $ErrorActionPreference = 'Stop'
+}
+
+function Log($Message) {
+    Write-Host ('{0} - {1}' -f [DateTime]::Now.ToLongTimeString(), $Message)
+}
+
+# Wait 30 seconds before build/test phase.
+Log "Waiting 30 seconds to allow for permissions to propagate before build/test phase."
+Start-Sleep -s 30
+
+Log "Finished waiting for permission propagation."
