9e811358c5120c065a9a9cf864170030888b3864
praveenkuttappan <prmarott@microsoft.com>
2020-12-09 11:48:46 -0800

    Review comment fixes

--- eng/common/scripts/Create-APIReview.ps1
+++ eng/common/scripts/Create-APIReview.ps1
@@ -13,8 +13,8 @@ Param (
 
 
 # Submit API review request and return status whether current revision is approved or pending or failed to create review
-function Submit-APIReview($packagename, $filePath, $uri, $apiKey, $apiLabel){
-    
+function Submit-APIReview($packagename, $filePath, $uri, $apiKey, $apiLabel)
+{
     $multipartContent = [System.Net.Http.MultipartFormDataContent]::new()
     $FileStream = [System.IO.FileStream]::new($filePath, [System.IO.FileMode]::Open)
     $fileHeader = [System.Net.Http.Headers.ContentDispositionHeaderValue]::new("form-data")
@@ -32,21 +32,18 @@ function Submit-APIReview($packagename, $filePath, $uri, $apiKey, $apiLabel){
     $StringContent.Headers.ContentDisposition = $stringHeader
     $multipartContent.Add($stringContent)
 
-    $form = @{
-        'label' = $apiLabel;
-        'file' = Get-content -Path $filePath
-    }
-
     $headers = @{
         "ApiKey" = $apiKey;
         "content-type" = "multipart/form-data"
     }
 
-    try{
+    try
+    {
         $Response = Invoke-WebRequest -Method 'POST' -Uri $uri -Body $multipartContent -Headers $headers
         $StatusCode = $Response.StatusCode
     }
-    catch{
+    catch
+    {
         $StatusCode = $_.Exception.Response.StatusCode
     }
 
@@ -57,33 +54,39 @@ function Submit-APIReview($packagename, $filePath, $uri, $apiKey, $apiLabel){
 . (Join-Path $PSScriptRoot common.ps1)
 $packages = Find-Artifacts-For-Apireview($ArtifactPath, $PackageName)
 $responses = @{}
-if ($packages){
-    foreach($pkg in $packages.GetEnumerator()){
-        Write-Host "Submitting API Review for package $($pkg.Key)"
-        Write-Host $packages[$pkg.Key]
-        $responses[$pkg.Key] = Submit-APIReview -packagename $pkg.Key -filePath $packages[$pkg.Key] -uri $APIViewUri -apiKey $APIKey -apiLabel $APILabel
+if ($packages)
+{
+    foreach($pkg in $packages.Keys)
+    {
+        Write-Host "Submitting API Review for package $($pkg)"
+        Write-Host $packages[$pkg]
+        $responses[$pkg] = Submit-APIReview -packagename $pkg -filePath $packages[$pkg] -uri $APIViewUri -apiKey $APIKey -apiLabel $APILabel
     }
 }
-else{
+else
+{
     Write-Host "No package is found in artifact path to submit review request"
 }
 
 $FoundFailure = $False
-foreach($res in $responses.GetEnumerator()){
-    
-    $pkgName = $res.Key
+foreach ($pkgName in $responses.Keys)
+{    
     $respCode = $responses[$pkgName]
-    if ($respCode -ne '200'){
+    if ($respCode -ne '200')
+    {
         $FoundFailure = $True
-        if ($respCode -eq '201'){
+        if ($respCode -eq '201')
+        {
             Write-Host "API Review is pending for package $pkgName"
         }
-        else{
+        else
+        {
             Write-Host "Failed to create API Review for package $pkgName"
         }
     }
 }
-if ($FoundFailure){
+if ($FoundFailure)
+{
     Write-Host "Atleast one API review is not yet approved"
 }
 
