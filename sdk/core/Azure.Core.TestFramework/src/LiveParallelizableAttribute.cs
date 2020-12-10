76da7e5537314e95ce40579d5b3fd15f5b8d70a7
Kamil Sobol <kasobol@microsoft.com>
2020-12-10 11:38:47 -0800

    pr feedback.

--- sdk/core/Azure.Core.TestFramework/src/LiveParallelizableAttribute.cs
+++ sdk/core/Azure.Core.TestFramework/src/LiveParallelizableAttribute.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using NUnit.Framework;
 
 namespace Azure.Core.TestFramework
@@ -9,6 +10,7 @@ namespace Azure.Core.TestFramework
     /// Attribute on test assemblies, classes, or methods that defines parallelization behavior when tests are run in <see cref="RecordedTestMode.Live"/> mode.
     /// In other modes it will enforce no parallelization.
     /// </summary>
+    [AttributeUsage( AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method, AllowMultiple=false, Inherited=true )]
     public class LiveParallelizableAttribute : ParallelizableAttribute
     {
         public LiveParallelizableAttribute(ParallelScope scope) : base(ApplyModeToParallelScope(scope))
