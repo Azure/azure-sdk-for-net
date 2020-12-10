32a66fe5e02710a2f6e04161e3e3c1a8f328c7d5
Kamil Sobol <kasobol@microsoft.com>
2020-12-10 10:22:37 -0800

    first attempt.

---
+++ sdk/core/Azure.Core.TestFramework/src/LiveParallelizableAttribute.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Azure.Core.TestFramework
+{
+    public class LiveParallelizableAttribute : ParallelizableAttribute
+    {
+        public LiveParallelizableAttribute(ParallelScope scope) : base(GetParallelScope(scope))
+        {
+        }
+
+        private static ParallelScope GetParallelScope(ParallelScope scope)
+        {
+            RecordedTestMode mode = RecordedTestUtilities.GetModeFromEnvironment();
+            return mode == RecordedTestMode.Live ? scope : ParallelScope.None;
+        }
+    }
+}
