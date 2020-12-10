05587ac8688c80563f6c13d0e0cceda68787ab69
Ben Broderick Phillips <bebroder@microsoft.com>
2020-12-09 21:41:49 -0500

    Run smoke tests against latest published versions in release pipeline

--- common/SmokeTests/SmokeTest/Update-Dependencies.ps1
+++ common/SmokeTests/SmokeTest/Update-Dependencies.ps1
@@ -1,8 +1,11 @@
 param(
     [string]$ProjectFile = './SmokeTest.csproj',
     [switch]$SkipVersionValidation,
-    [switch]$CI
+    [switch]$CI,
+    [switch]$Nightly
 )
+
+. $PSScriptRoot/../../../eng/common/scripts/SemVer.ps1
 
 # To exclude a package version create an entry whose key is the package to
 # exclude whose value is a hash table of versions to exclude.
@@ -20,6 +23,9 @@ $PACKAGE_REFERENCE_XPATH = '//Project/ItemGroup/PackageReference'
 # Matches the dev.yyyymmdd portion of the version string
 $DEV_DATE_REGEX = 'dev\.(\d{8})'
 
+$baselineVersionDate = $null;
+
+$RELEASE_FEED_URL = 'https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-net/nuget/v3/index.json'
 $NIGHTLY_FEED_NAME = 'NightlyFeed'
 $NIGHTLY_FEED_URL = 'https://azuresdkartifacts.blob.core.windows.net/azure-sdk-for-net/index.json'
 
@@ -31,63 +37,93 @@ function Log-Warning($message) {
     }
 }
 
-Register-PackageSource `
-    -Name $NIGHTLY_FEED_NAME `
-    -Location $NIGHTLY_FEED_URL `
-    -ProviderName Nuget `
-    -ErrorAction SilentlyContinue `
+function GetAllNightlyPackages {
+    Register-PackageSource `
+        -Name $NIGHTLY_FEED_NAME `
+        -Location $NIGHTLY_FEED_URL `
+        -ProviderName Nuget `
+        -ErrorAction SilentlyContinue `
 
-# List all packages from the source specified by $FeedName. Packages are sorted
-# ascending by version according to semver rules (e.g. 4.0.0-preview.1 comes
-# before 4.0.0) not lexicographically.
-# Packages cannot be filtered at this stage because the sleet feed to which they
-# are published does not support filtering by name.
-$allPackages = Find-Package -Source $NIGHTLY_FEED_NAME -AllVersion -AllowPrereleaseVersions
+    # List all packages from the source specified by $FeedName. Packages are sorted
+    # ascending by version according to semver rules (e.g. 4.0.0-preview.1 comes
+    # before 4.0.0) not lexicographically.
+    # Packages cannot be filtered at this stage because the sleet feed to which they
+    # are published does not support filtering by name.
+    return Find-Package -Source $NIGHTLY_FEED_NAME -AllVersion -AllowPrereleaseVersions
+}
 
-$baselineVersionDate = $null;
+function GetAllPackages {
+    if ($Nightly) {
+        return GetAllNightlyPackages
+    }
+    return Find-Package -Source $RELEASE_FEED_URL -AllVersion -AllowPrereleaseVersions
+}
 
-# For each PackageReferecne in the csproj, find the latest version of that
-# package from the dev feed which is not in the excluded list.
-$projectFilePath = Resolve-Path -Path $ProjectFile
-[xml]$csproj = Get-Content $ProjectFile
-$csproj |
-    Select-XML $PACKAGE_REFERENCE_XPATH |
-    Where-Object { $_.Node.HasAttribute('Version') } |
-    ForEach-Object {
-        # Resolve package version:
-        $packageName = $_.Node.Include
-
-        # This assumes that the versions coming back from Find-Package are
-        # sorted ascending. It excludes any version that is in the corresponding
-        # $PACKAGE_EXCLUSIONS entry
-        $targetVersion = ($allPackages |
-            Where-Object { $_.Name -eq $packageName } |
-            Where-Object { -not ( $PACKAGE_EXCLUSIONS.ContainsKey($packageName) -and $PACKAGE_EXCLUSIONS[$packageName].ContainsKey($_.Version)) } |
-            Select-Object -Last 1).Version
-
-        if ($targetVersion -eq $null) {
-            return
+function GetLatestPackage([array]$packageList, [string]$packageName) {
+    # This assumes that the versions coming back from Find-Package are
+    # sorted ascending. It excludes any version that is in the corresponding
+    # $PACKAGE_EXCLUSIONS entry
+    $versions = ($packageList `
+        | Where-Object { $_.Name -eq $packageName }
+        | Where-Object { `
+            -not ( $PACKAGE_EXCLUSIONS.ContainsKey($packageName) `
+            -and $PACKAGE_EXCLUSIONS[$packageName].ContainsKey($_.Version)) `
         }
+        | Select-Object -ExpandProperty Version)
 
-        Write-Host "Setting $packageName to $targetVersion"
-        $_.Node.Version = "$targetVersion"
-
-
-        # Validate package version date component matches
-        if ($SkipVersionValidation) {
-            return
-        }
-
-        if ($_.Node.Version -match $DEV_DATE_REGEX) {
-            if ($baselineVersionDate -eq $null) {
-                Write-Host "Using baseline version date: $($matches[1])"
-                $baselineVersionDate = $matches[1]
-            }
-
-            if ($baselineVersionDate -ne $matches[1]) {
-                Log-Warning "$($_.Node.Include) uses invalid version. Expected: $baselineVersionDate, Actual: $($matches[1])"
-            }
-        }
+    if (!$versions) {
+        Write-Warning "Did not find any versions for $($packageName)"
+        return
     }
 
-$csproj.Save($projectFilePath)
+    $sorted = [AzureEngSemanticVersion]::SortVersionStrings($versions)
+    return $sorted | Select-Object -First 1
+}
+
+function SetLatestPackageVersions([object]$csproj) {
+    # For each PackageReference in the csproj, find the latest version of that
+    # package from the dev feed which is not in the excluded list.
+    $allPackages = GetAllPackages
+    $csproj |
+        Select-XML $PACKAGE_REFERENCE_XPATH |
+        Where-Object { $_.Node.HasAttribute('Version') } |
+        ForEach-Object {
+            # Resolve package version:
+            $packageName = $_.Node.Include
+
+            $targetVersion = GetLatestPackage $allPackages $packageName
+
+            if ($targetVersion -eq $null) {
+                return
+            }
+
+            Write-Host "Setting $packageName to $targetVersion"
+            $_.Node.Version = "$targetVersion"
+
+
+            # Validate package version date component matches
+            if ($SkipVersionValidation) {
+                return
+            }
+
+            if ($_.Node.Version -match $DEV_DATE_REGEX) {
+                if ($baselineVersionDate -eq $null) {
+                    Write-Host "Using baseline version date: $($matches[1])"
+                    $baselineVersionDate = $matches[1]
+                }
+
+                if ($baselineVersionDate -ne $matches[1]) {
+                    Log-Warning "$($_.Node.Include) uses invalid version. Expected: $baselineVersionDate, Actual: $($matches[1])"
+                }
+            }
+        }
+}
+
+function UpdateCsprojVersions {
+    $projectFilePath = Resolve-Path -Path $ProjectFile
+    [xml]$csproj = Get-Content $projectFilePath
+    SetLatestPackageVersions $csproj
+    $csproj.Save($projectFilePath)
+}
+
+UpdateCsprojVersions
