1d831b336c5f5483e6f15d35dc763720bc54d16b
Caio Saldanha <camaiaor@microsoft.com>
2020-12-12 17:56:29 -0800

    Added disposable detect config class

---
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/tests/MetricsAdvisorAdministrationClient/AnomalyDetectionConfigurationLiveTests.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.AI.MetricsAdvisor.Tests
+{
+    public class AnomalyDetectionConfigurationLiveTests : MetricsAdvisorLiveTestBase
+    {
+        public AnomalyDetectionConfigurationLiveTests(bool isAsync) : base(isAsync)
+        {
+        }
+    }
+}
