b2e3ed8faeebbfd753f29ecc16324d20de2a4fb6
Harvey Chen <30464227+HarveyLink@users.noreply.github.com>
2020-12-14 16:53:33 +0800

    Update RP Mapping code

--- eng/scripts/Track1CodeCheck.ps1
+++ eng/scripts/Track1CodeCheck.ps1
@@ -27,6 +27,21 @@ function Invoke-Block([scriptblock]$cmd) {
     }
 }
 
+function Find-Mapping([string]$path) {
+    $fileContent = Get-Content $path
+    $name = ''
+    foreach ($item in $fileContent) {
+        if (($item -match '\$\(csharp-sdks-folder\)')) {
+            $matchResult = $item -match "\/([^/]+)\/"
+            $name = $matches[0].Substring(1, $matches[0].Length - 2)
+            if (($name -ne '') -and ($name -notmatch "\$")) {
+                break
+            }
+        }
+    }
+    return $name
+}
+
 # helper to turn PSCustomObject into a list of key/value pairs
 function Get-ObjectMembers {
     [CmdletBinding()]
@@ -42,32 +57,38 @@ function Get-ObjectMembers {
 
 try {
     # Get RP Mapping
-    $RPMapping = @{ }
+    $RPMapping = [ordered]@{ }
     $readmePath = ''
     git clone https://github.com/Azure/azure-rest-api-specs.git ../azure-rest-api-specs
     $folderNames = Get-ChildItem ../azure-rest-api-specs/specification
     $folderNames | ForEach-Object {
-        if (Test-Path "../azure-rest-api-specs/specification/$($_.Name)/resource-manager/readme.csharp.md") {
-            $readmePath = "../azure-rest-api-specs/specification/$($_.Name)/resource-manager/readme.csharp.md"
+        $folderName = ''
+        $readmePath = "../azure-rest-api-specs/specification/$($_.Name)/resource-manager/readme.csharp.md"
+        if (Test-Path $readmePath) {
+            $folderName = Find-Mapping $readmePath
         }
-        elseif (Test-Path "../azure-rest-api-specs/specification/$($_.Name)/resource-manager/readme.md") {
+        if (($folderName -eq '') -or ($folderName -match "\$")) {
             $readmePath = "../azure-rest-api-specs/specification/$($_.Name)/resource-manager/readme.md"
-        } 
-        $fileContent = Get-Content $readmePath
-        foreach ($item in $fileContent) {
-            if (($item -match '\$\(csharp-sdks-folder\)')) {
-                $item -match "\/([^/]+)\/"
-                $folderName = $matches[0].Substring(1, $matches[0].Length - 2)
-                if (($folderName -notmatch "\$") -and (!$RPMapping.ContainsKey($folderName))) {
-                    $RPMapping += @{ $folderName = "$($_.Name)" }
-                }
+            if (Test-Path $readmePath) {
+                $folderName = Find-Mapping $readmePath
             }
+        }
+        if (($folderName -notmatch "\$") -and (!$RPMapping.Contains($folderName)) -and ($folderName -ne '')) {
+            $RPMapping += @{ $folderName = "$($_.Name)" }
         }
     }
 
     # Get Metadata file path
     $Response = Invoke-WebRequest -URI https://api.github.com/repos/$Env:REPOSITORY_NAME/pulls/$Env:PULLREQUEST_ID/files
     $changeList = $Response.Content | ConvertFrom-Json
+    if ($Response.RelationLink) {
+        $lastLink = $Response.RelationLink.Get_Item('last')
+        $lastPage = $lastLink.Substring($lastLink.indexof("=") + 1)
+        for ($i = 2; $i -le $lastPage; $i++) {
+            $Response = Invoke-WebRequest -URI https://api.github.com/repos/$Env:REPOSITORY_NAME/pulls/$Env:PULLREQUEST_ID/files?page=$i
+            $changeList += $Response.Content | ConvertFrom-Json
+        }
+    }
     $mataPath = @()
     $rpIndex = @()
     $folderName = @()
@@ -88,9 +109,14 @@ try {
     }
     foreach ($item in $folderName) {
         $rpName = $RPMapping.Get_Item($item)
-        If ($rpIndex -notcontains $rpName) {
-            $rpIndex += $rpName
+        if ($rpName) {
+            If ($rpIndex -notcontains $rpName) {
+                $rpIndex += $rpName
+            }
         }
+        else {
+            LogError "Can't get proper RP name with folder $item"
+        } 
     }
     $rpIndex | ForEach-Object {
         $path = "eng/mgmt/mgmtmetadata/$_" + "_resource-manager.txt"
@@ -99,40 +125,59 @@ try {
         }
     }
 
-    $mataPath | ForEach-Object {
-        $metaData = Get-Content $mataPath
-        $commit = ''
-        $readme = ''
-        [string]$path = Get-Location
-        $metaData | ForEach-Object {
-            if ($_ -match 'Commit') {
-                $commit = $_.substring($_.length - 40, 40)
+    # Install AutoRest    
+    Invoke-Block {
+        & npm install -g autorest
+    }
+
+    # Invoke AutoRest
+    foreach ($metaData in $mataPath) {
+        $metaDataContent = ''
+        try {
+            $metaDataContent = Get-Content $metaData
+        }
+        catch {
+            LogError "Can't find path $metaData"
+        }
+
+        if ( $metaDataContent -ne '') {
+            $commit = ''
+            $readme = ''
+            [string]$path = Get-Location
+            $metaDataContent | ForEach-Object {
+                if ($_ -match 'Commit') {
+                    $commit = $_.substring($_.length - 40, 40)
+                }
+                if ($_ -match 'cmd.exe') {
+                    $_ -match 'https:[\S]*readme.md'
+                    $readme = $matches[0]
+                }
             }
-            if ($_ -match 'cmd.exe') {
-                $_ -match 'https:[\S]*readme.md'
-                $readme = $matches[0]
+            $readme = $readme -replace "blob/[\S]*/specification", "blob/$commit/specification"
+            $path = ($path -replace "\\", "/") + "/sdk"
+
+            if ($readme -eq '') {
+                LogError "MetaData $metaData content not correct"
+            }
+            else {
+                Write-Output "Ready to execute: autorest $readme --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=$path --use:@microsoft.azure/autorest.csharp@2.3.90"
+                Invoke-Block {
+                    & autorest $readme --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=$path --use:@microsoft.azure/autorest.csharp@2.3.90 
+                }
             }
         }
-        $readme = $readme -replace "blob/[\S]*/specification", "blob/$commit/specification"
-        $path = ($path -replace "\\", "/") + "/sdk"
+    }
+    
+    # prevent warning related to EOL differences which triggers an exception for some reason
+    & git add -A
+    $diffResult = @()
+    $diffResult += git -c core.safecrlf=false diff HEAD --name-only --ignore-space-at-eol
 
-        Invoke-Block {
-            & npm install -g autorest
-        }
-
-        Write-Output "Ready to execute: autorest $readme --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=$path --use:@microsoft.azure/autorest.csharp@2.3.90"
-        Invoke-Block {
-            & autorest $readme --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=$path --use:@microsoft.azure/autorest.csharp@2.3.90 
-        }
-
-        # prevent warning related to EOL differences which triggers an exception for some reason
-        & git add -A
-        $diffResult = @()
-        $diffResult += git -c core.safecrlf=false diff HEAD --name-only --ignore-space-at-eol
-        if ($diffResult.Length -gt 1) {
+    foreach ($item in $diffResult) {
+        if ($item -notmatch 'SdkInfo_') {
             $exitCode ++
         }
-        elseif (($diffResult.Length -eq 1) -And ($diffResult[0] -match 'SdkInfo_')) {
+        else {
             $changeContent = @()
             $content = git -c core.safecrlf=false diff -U0 HEAD --ignore-space-at-eol $diffResult[0]
             $content[4..($content.Length - 1)] | ForEach-Object {
@@ -143,7 +188,7 @@ try {
             if ($changeContent.Length -ne 11) {
                 $exitCode ++
             }
-
+    
             # metaDataContent doesn't contains 'AutoRestCmdExecuted' part since it can't be verified
             $metaDataContent = @('-      // BEGIN: Code Generation Metadata Section',
                 '-      public static readonly String AutoRestVersion = "v2";',
@@ -163,18 +208,15 @@ try {
                 }
             }
         }
-        elseif (($diffResult.Length -eq 1) -And ($diffResult[0] -notmatch 'SdkInfo_')) {
-            $exitCode ++
+    }
+    
+    if ($exitCode -ne 0) {
+        & git -c core.safecrlf=false diff HEAD --ignore-space-at-eol
+        Write-Output "Git Diff file is:" 
+        $diffResult | ForEach-Object {
+            Write-Output $_
         }
-
-        if ($exitCode -ne 0) {
-            & git -c core.safecrlf=false diff HEAD --ignore-space-at-eol
-            Write-Output "Git Diff file is:" 
-            $diffResult | ForEach-Object {
-                Write-Output $_
-            }
-            LogError "Generated code is manually altered, you may need to re-run sdk\<RP Name>\generate.ps1"
-        }
+        LogError "Generated code is manually altered, you may need to re-run sdk\<RP Name>\generate.ps1"
     }
 }
 finally {
