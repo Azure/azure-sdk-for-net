f11fc3cca5328be07dafe3587d43868da00287d3
hivyas <61890270+hivyas@users.noreply.github.com>
2020-12-14 18:38:04 -0600

    [Media] LVA SDK addition (#17289)
    Co-authored-by: giakas <giakas@microsoft.com>

---
+++ sdk/mediaservices/Azure.Media.Analytics.Edge/CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release History
+
+## 1.0.0-beta.1 (2020-12-11)
+Initial release
---
+++ sdk/mediaservices/Azure.Media.Analytics.Edge/src/Customization/MediaGraphTopologySetRequest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json;
+
+namespace Azure.Media.Analytics.Edge.Models
+{
+    public partial class MediaGraphTopologySetRequest
+    {
+        internal MediaGraphTopologySetRequestBody GraphBody;
+
+        /// <summary>
+        ///  Serialize .
+        /// </summary>
+        /// <returns>
+        /// Method payload as Json string.
+        /// </returns>
+        public override string GetPayloadAsJson()
+        {
+            GraphBody = new MediaGraphTopologySetRequestBody(Graph.Name);
+            GraphBody.SystemData = Graph.SystemData;
+            GraphBody.Properties = Graph.Properties;
+            return GraphBody.GetPayloadAsJson();
+        }
+    }
+}
