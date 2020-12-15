5236c7ac1471a460c90e7048e6d15e2292c42e62
Kamil Sobol <61715331+kasobol-msft@users.noreply.github.com>
2020-12-15 11:09:28 -0800

    [Storage] Bump threadpool in tests. (#17547)

--- sdk/storage/Azure.Storage.Common/tests/Shared/StorageTestBase.cs
+++ sdk/storage/Azure.Storage.Common/tests/Shared/StorageTestBase.cs
@@ -31,6 +31,8 @@ namespace Azure.Storage.Test.Shared
 #if !NETCOREAPP
             ServicePointManager.DefaultConnectionLimit = int.MaxValue;
 #endif
+            // To avoid threadpool starvation when we run live tests in parallel.
+            ThreadPool.SetMinThreads(100, 100);
         }
 
         public StorageTestBase(bool async, RecordedTestMode? mode = null)
