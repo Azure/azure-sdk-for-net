6ca404478538ad7ea79f52b0b2c4425f07de0141
Frank Li (Wicresoft North America Ltd) <v-cheli@microsoft.com>
2020-12-17 10:10:57 +0800

    Move CloudClipboard from https://github.com/Azure/azure-sdk-for-net-samples to https://github.com/Azure/azure-sdk-for-net/tree/master/samples

---
+++ samples/CloudClipboard/GarbageCollector/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace GarbageCollector
+{
+    public static class Program
+    {
+        private static readonly Uri BlobServiceUri = new Uri("https://cloudclips.blob.core.windows.net/");
+
+        public static async Task Main()
+        {
+            BlobClientOptions options = new BlobClientOptions();
+            options.Retry.MaxRetries = 10;
+            options.Retry.Delay = TimeSpan.FromSeconds(3);
+            options.Diagnostics.IsLoggingEnabled = true;
+
+            options.AddPolicy(new SimpleTracingPolicy(), HttpPipelinePosition.PerCall);
+
+            BlobServiceClient serviceClient = new BlobServiceClient(BlobServiceUri, new DefaultAzureCredential(), options);
+            await foreach (BlobContainerItem container in serviceClient.GetBlobContainersAsync())
+            {
+                BlobContainerClient containerClient = serviceClient.GetBlobContainerClient(container.Name);
+                await containerClient.DeleteAsync();
+            }
+        }
+    }
+}
