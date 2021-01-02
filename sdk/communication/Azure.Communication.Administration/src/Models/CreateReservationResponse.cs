e977a1a5ed13295d78e66cdaae83971a01b9c3dc
Paola Vicencio <pvicencio@microsoft.com>
2021-01-02 01:08:17 -0600

    adding new attribute exclude from test coverage

--- sdk/communication/Azure.Communication.Administration/src/Models/CreateReservationResponse.cs
+++ sdk/communication/Azure.Communication.Administration/src/Models/CreateReservationResponse.cs
@@ -1,11 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Diagnostics.CodeAnalysis;
 using Azure.Core;
 
 namespace Azure.Communication.Administration.Models
 {
     [CodeGenModel("CreateSearchResponse")]
+    [ExcludeFromCodeCoverage]
     internal partial class CreateReservationResponse
     {
         /// <summary> The id of a created reservation. </summary>
