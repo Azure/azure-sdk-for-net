68d9f13617447401db167e89914646baaa74b3dc
Paola Vicencio <pvicencio@microsoft.com>
2021-01-02 22:12:41 -0600

    filtering code coverage classes

--- sdk/communication/Azure.Communication.Administration/src/Models/CreateReservationOptions.cs
+++ sdk/communication/Azure.Communication.Administration/src/Models/CreateReservationOptions.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Diagnostics.CodeAnalysis;
 using Azure.Core;
 
 namespace Azure.Communication.Administration.Models
 {
     [CodeGenModel("CreateSearchOptions")]
-    [ExcludeFromCodeCoverage]
     public partial class CreateReservationOptions
     {
     }
