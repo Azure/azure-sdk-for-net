383180c23d48337408d310badb43df8dca1f8b1b
Chris Hamons <chris.hamons@xamarin.com>
2021-01-04 09:32:31 -0600

    [Synapse] Add async variants of existing Spark samples

---
+++ sdk/synapse/Azure.Analytics.Synapse.Spark/tests/samples/Sample1_SubmitSparkJobAsync.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Azure.Analytics.Synapse.Spark;
+using Azure.Analytics.Synapse.Spark.Models;
+using Azure.Identity;
+using NUnit.Framework;
+
+namespace Azure.Analytics.Synapse.Samples
+{
+    /// <summary>
+    /// This sample demonstrates how to submit Spark job in Azure Synapse Analytics using asynchronous methods of <see cref="SparkBatchClient"/>.
+    /// </summary>
+    public partial class Sample1_SubmitSparkJobAsync : SampleFixture
+    {
+        [Test]
+        public async Task SubmitSparkJobAsync()
+        {
+            // Environment variable with the Synapse workspace endpoint.
+            string endpoint = TestEnvironment.EndpointUrl;
+
+            // Environment variable with the Synapse Spark pool name.
+            string sparkPoolName = TestEnvironment.SparkPoolName;
+
+            // Environment variable with the ADLS Gen2 storage account associated with the Synapse workspace.
+            string storageAccount = TestEnvironment.StorageAccountName;
+
+            // Environment variable with the file system of ADLS Gen2 storage account associated with the Synapse workspace.
+            string fileSystem = TestEnvironment.StorageFileSystemName;
+
+            #region Snippet:CreateSparkBatchClientAsync
+            SparkBatchClient client = new SparkBatchClient(new Uri(endpoint), sparkPoolName, new DefaultAzureCredential());
+            #endregion
+
+            #region Snippet:SubmitSparkBatchJobAsync
+            string name = $"batch-{Guid.NewGuid()}";
+            string file = string.Format("abfss://{0}@{1}.dfs.core.windows.net/samples/net/wordcount/wordcount.zip", fileSystem, storageAccount);
+            SparkBatchJobOptions request = new SparkBatchJobOptions(name, file)
+            {
+                ClassName = "WordCount",
+                Arguments =
+                {
+                    string.Format("abfss://{0}@{1}.dfs.core.windows.net/samples/net/wordcount/shakespeare.txt", fileSystem, storageAccount),
+                    string.Format("abfss://{0}@{1}.dfs.core.windows.net/samples/net/wordcount/result/", fileSystem, storageAccount),
+                },
+                DriverMemory = "28g",
+                DriverCores = 4,
+                ExecutorMemory = "28g",
+                ExecutorCores = 4,
+                ExecutorCount = 2
+            };
+
+            SparkBatchOperation createOperation = await client.StartCreateSparkBatchJobAsync(request);
+            SparkBatchJob jobCreated = await createOperation.WaitForCompletionAsync();
+            #endregion
+
+            #region Snippet:ListSparkBatchJobsAsync
+            Response<SparkBatchJobCollection> jobs = client.GetSparkBatchJobs();
+            foreach (SparkBatchJob job in jobs.Value.Sessions)
+            {
+                Console.WriteLine(job.Name);
+            }
+            #endregion
+
+            #region Snippet:GetSparkBatchJobAsync
+            SparkBatchOperation getOperation = await client.StartGetSparkBatchJobAsync (jobCreated.Id);
+            SparkBatchJob retrievedJob = await getOperation.WaitForCompletionAsync();
+
+            Debug.WriteLine($"Job is returned with name {retrievedJob.Name} and state {retrievedJob.State}");
+            #endregion
+
+            #region Snippet:DeleteSparkBatchJobAsync
+            Response operation = client.CancelSparkBatchJob(jobCreated.Id);
+            #endregion
+        }
+    }
+}
---
+++ sdk/synapse/Azure.Analytics.Synapse.Spark/tests/samples/Sample2_ExecuteSparkStatementAsync.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Azure.Analytics.Synapse.Spark;
+using Azure.Analytics.Synapse.Spark.Models;
+using Azure.Identity;
+using NUnit.Framework;
+
+namespace Azure.Analytics.Synapse.Samples
+{
+    /// <summary>
+    /// This sample demonstrates how to submit Spark job in Azure Synapse Analytics using asynchronous methods of <see cref="SparkSessionClient"/>.
+    /// </summary>
+    public partial class Sample2_ExecuteSparkStatementAsync : SampleFixture
+    {
+        [Test]
+        public async Task ExecuteSparkStatementSync()
+        {
+            // Environment variable with the Synapse workspace endpoint.
+            string endpoint = TestEnvironment.EndpointUrl;
+
+            // Environment variable with the Synapse Spark pool name.
+            string sparkPoolName = TestEnvironment.SparkPoolName;
+
+            #region Snippet:CreateSparkSessionClientAsync
+            SparkSessionClient client = new SparkSessionClient(new Uri(endpoint), sparkPoolName, new DefaultAzureCredential());
+            #endregion
+
+            #region Snippet:CreateSparkSessionAsync
+            SparkSessionOptions request = new SparkSessionOptions(name: $"session-{Guid.NewGuid()}")
+            {
+                DriverMemory = "28g",
+                DriverCores = 4,
+                ExecutorMemory = "28g",
+                ExecutorCores = 4,
+                ExecutorCount = 2
+            };
+
+            SparkSessionOperation createSessionOperation = await client.StartCreateSparkSessionAsync(request);
+            SparkSession sessionCreated = await createSessionOperation.WaitForCompletionAsync();
+            #endregion
+
+            #region Snippet:GetSparkSessionAsync
+            SparkSessionOperation getOperation = await client.StartGetSparkSessionAsync(sessionCreated.Id);
+            SparkSession session = await getOperation.WaitForCompletionAsync();
+            Debug.WriteLine($"Session is returned with name {session.Name} and state {session.State}");
+            #endregion
+
+            #region Snippet:CreateSparkStatementAsync
+            SparkStatementOptions sparkStatementRequest = new SparkStatementOptions
+            {
+                Kind = SparkStatementLanguageType.Spark,
+                Code = @"print(""Hello world\n"")"
+            };
+
+            SparkStatementOperation createStatementOperation = await client.StartCreateSparkStatementAsync(sessionCreated.Id, sparkStatementRequest);
+            SparkStatement statementCreated = await createStatementOperation.WaitForCompletionAsync();
+            #endregion
+
+            #region Snippet:GetSparkStatementAsync
+            SparkStatementOperation getStatementOperation = await client.StartGetSparkStatementAsync(sessionCreated.Id, statementCreated.Id);
+            SparkStatement statement = await getStatementOperation.WaitForCompletionAsync();
+            Debug.WriteLine($"Statement is returned with id {statement.Id} and state {statement.State}");
+            #endregion
+
+            #region Snippet:CancelSparkStatementAsync
+            SparkStatementCancellationResult cancellationResult = client.CancelSparkStatement(sessionCreated.Id, statementCreated.Id);
+            Debug.WriteLine($"Statement is cancelled with message {cancellationResult.Msg}");
+            #endregion
+
+            #region Snippet:CancelSparkSessionAsync
+            Response operation = client.CancelSparkSession(sessionCreated.Id);
+            #endregion
+        }
+    }
+}
