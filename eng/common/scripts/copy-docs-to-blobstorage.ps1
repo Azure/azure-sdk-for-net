01baf1074ebb8c9bad2e4efc388b59a704774ea3
Sima Zhu <sizhu@microsoft.com>
2021-01-04 17:45:51 -0800

    Update the redirect links and remove everything in latest

--- eng/common/scripts/copy-docs-to-blobstorage.ps1
+++ eng/common/scripts/copy-docs-to-blobstorage.ps1
@@ -223,13 +223,15 @@ function Upload-Blobs
     if (!$latestVersion) {
         $latestVersion = $versionsObj.LatestPreviewPackage 
     }
-
+    LogDebug "Fetching the latest version. $latestVersion"
     # Prepare the index.html which can redirect to the latest GA whenever avaiable, otherwise point to latest preview.
     New-Item -Path $DocDir -Name "latest" -ItemType "directory"
-    New-Item -Path "$($DocDir)/latest" -Name "index.html" -ItemType "file" -Value "<meta http-equiv="refresh" content="0; URL=$($DocDest)/$($PkgName)/$($DocVersion)" />"
+    New-Item -Path "$($DocDir)/latest" -Name "index.html" -ItemType "file" -Value "<meta http-equiv=`"refresh`" content=`"0; URL=$($DocDest)/$($PkgName)/$($latestVersion)/index.html`" />"
     if ($UploadLatest -and $latestVersion)
     {
         LogDebug "Uploading $($PkgName) to latest folder in $($DocDest)..."
+        # Clean up existing folders, will remove this step once we have one or two release cycles.
+        & $($AzCopy) rm "$($DocDest)/$($PkgName)/latest$($SASKey)" --recursive=true
         & $($AzCopy) cp "$($DocDir)/latest/**" "$($DocDest)/$($PkgName)/latest$($SASKey)" --recursive=true --cache-control "max-age=300, must-revalidate"
     }
 }
