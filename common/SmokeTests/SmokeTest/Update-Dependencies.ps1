cea2ba98c4f944092161e993ba2f7fa84904513a
Ben Broderick Phillips <bebroder@microsoft.com>
2021-01-05 16:32:18 -0500

    [smoke tests] Remove package exclusions. Only include GA/beta packages for daily==false

--- common/SmokeTests/SmokeTest/Update-Dependencies.ps1
+++ common/SmokeTests/SmokeTest/Update-Dependencies.ps1
@@ -7,17 +7,6 @@ param(
 
 . $PSScriptRoot/../../../eng/common/scripts/SemVer.ps1
 
-# To exclude a package version create an entry whose key is the package to
-# exclude whose value is a hash table of versions to exclude.
-# Example:
-# $PACKAGE_EXCLUSIONS = @{
-#     'Azure.Security.Keyvault.Secrets' = @{
-#         '4.1.0-dev.20191102.1' = $true;
-#         '4.1.0-dev.20191103.1' = $true;
-#     }
-# }
-$PACKAGE_EXCLUSIONS = @{ }
-
 $PACKAGE_REFERENCE_XPATH = '//Project/ItemGroup/PackageReference'
 
 # Matches the dev.yyyymmdd portion of the version string
@@ -40,19 +29,12 @@ function GetAllPackages {
     if ($Daily) {
         return $packages | Where-Object { $_.Version.Contains("alpha") }
     }
-    return $packages
+    return $packages | Where-Object { !$_.Version.Contains("alpha") -and !$_.Version.Contains("dev") }
 }
 
 function GetLatestPackage([array]$packageList, [string]$packageName) {
-    # This assumes that the versions coming back from Find-Package are
-    # sorted ascending. It excludes any version that is in the corresponding
-    # $PACKAGE_EXCLUSIONS entry
-    $versions = ($packageList `
+    $versions = ($packageList
         | Where-Object { $_.Name -eq $packageName }
-        | Where-Object { `
-            -not ( $PACKAGE_EXCLUSIONS.ContainsKey($packageName) `
-            -and $PACKAGE_EXCLUSIONS[$packageName].ContainsKey($_.Version)) `
-        }
         | Select-Object -ExpandProperty Version)
 
     if (!$versions) {
@@ -64,7 +46,7 @@ function GetLatestPackage([array]$packageList, [string]$packageName) {
     return $sorted | Select-Object -First 1
 }
 
-function SetLatestPackageVersions([object]$csproj) {
+function SetLatestPackageVersions([xml]$csproj) {
     # For each PackageReference in the csproj, find the latest version of that
     # package from the dev feed which is not in the excluded list.
     $allPackages = GetAllPackages
