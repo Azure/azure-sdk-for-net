37b4f0147b024c43ca8a3c440fa122a68ebfe781
Caio Saldanha <camaiaor@microsoft.com>
2021-01-05 12:04:13 -0800

    Fixed bug

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/HttpRequestParameter.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/HttpRequestParameter.cs
@@ -5,8 +5,6 @@
 
 #nullable disable
 
-using System;
-
 namespace Azure.AI.MetricsAdvisor.Models
 {
     /// <summary> The HttpRequestParameter. </summary>
@@ -17,26 +15,8 @@ namespace Azure.AI.MetricsAdvisor.Models
         /// <param name="httpHeader"> HTTP header. </param>
         /// <param name="httpMethod"> HTTP method. </param>
         /// <param name="payload"> HTTP reuqest body. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="url"/>, <paramref name="httpHeader"/>, <paramref name="httpMethod"/>, or <paramref name="payload"/> is null. </exception>
         public HttpRequestParameter(string url, string httpHeader, string httpMethod, string payload)
         {
-            if (url == null)
-            {
-                throw new ArgumentNullException(nameof(url));
-            }
-            if (httpHeader == null)
-            {
-                throw new ArgumentNullException(nameof(httpHeader));
-            }
-            if (httpMethod == null)
-            {
-                throw new ArgumentNullException(nameof(httpMethod));
-            }
-            if (payload == null)
-            {
-                throw new ArgumentNullException(nameof(payload));
-            }
-
             Url = url;
             HttpHeader = httpHeader;
             HttpMethod = httpMethod;
