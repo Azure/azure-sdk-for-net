4270ac2e657e817b1702fe5bf488de1dafbe7523
Malcolm Tyrrell <matyrr@microsoft.com>
2021-01-05 17:04:24 +0000

    Add skeleton test files.

---
+++ sdk/mixedreality/Azure.MixedReality.RemoteRendering/tests/Samples/RemoteRenderingCreateConversionSample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Azure.Core.TestFramework;
+using Azure.MixedReality.RemoteRendering.Tests;
+
+namespace Azure.MixedReality.RemoteRendering.Tests.Samples
+{
+    public class RemoteRenderingCreateConversionSample : SamplesBase<AzureRemoteRenderingTestEnvironment>
+    {
+        public RemoteRenderingCreateConversionSample()
+        {
+        }
+    }
+}
