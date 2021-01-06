91cf48cd855feaf60583096c1222b45afb913173
Malcolm Tyrrell <matyrr@microsoft.com>
2021-01-06 10:29:21 +0000

    Add header

--- sdk/mixedreality/Azure.MixedReality.RemoteRendering/tests/Samples/RemoteRenderingCreateConversionSample.cs
+++ sdk/mixedreality/Azure.MixedReality.RemoteRendering/tests/Samples/RemoteRenderingCreateConversionSample.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 using Azure.Core.TestFramework;
