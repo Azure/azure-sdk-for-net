c68e71a01d7b4fb9d158c565b22def7842880cbc
Malcolm Tyrrell <matyrr@microsoft.com>
2021-01-06 15:32:30 +0000

    Add CreateConversionAsync

--- sdk/mixedreality/Azure.MixedReality.RemoteRendering/src/RemoteRenderingClient.cs
+++ sdk/mixedreality/Azure.MixedReality.RemoteRendering/src/RemoteRenderingClient.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Azure.Core.Pipeline;
 using Azure.MixedReality.Authentication;
 using Azure.MixedReality.RemoteRendering.Models;
@@ -77,6 +78,48 @@ namespace Azure.MixedReality.RemoteRendering
             try
             {
                 ResponseWithHeaders<object, MixedRealityRemoteRenderingCreateConversionHeaders> response = _restClient.CreateConversion(_accountId, conversionId, new ConversionRequest(settings), cancellationToken);
+
+                switch (response.Value)
+                {
+                    case Conversion c:
+                        return ResponseWithHeaders.FromValue(c, response.Headers, response.GetRawResponse());
+                    case ErrorResponse e:
+                        // TODO e.Error.Details
+                        // TODO e.Error.InnerError
+                        throw _clientDiagnostics.CreateRequestFailedException(response, e.Error.Message, e.Error.Code);
+                    case null:
+                    default:
+                        throw _clientDiagnostics.CreateRequestFailedException(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                scope.Failed(ex);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Creates a conversion using an asset stored in an Azure Blob Storage account.
+        /// If the remote rendering account has been linked with the storage account no Shared Access Signatures (storageContainerReadListSas, storageContainerWriteSas) for storage access need to be provided.
+        /// Documentation how to link your Azure Remote Rendering account with the Azure Blob Storage account can be found in the [documentation](https://docs.microsoft.com/azure/remote-rendering/how-tos/create-an-account#link-storage-accounts).
+        /// All files in the input container starting with the blobPrefix will be retrieved to perform the conversion. To cut down on conversion times only necessary files should be available under the blobPrefix.
+        /// .
+        /// </summary>
+        /// <param name="conversionId"> An ID uniquely identifying the conversion for the given account. The ID is case sensitive, can contain any combination of alphanumeric characters including hyphens and underscores, and cannot contain more than 256 characters. </param>
+        /// <param name="settings"> Request body configuring the settings for an asset conversion. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="conversionId"/> or <paramref name="settings"/> is null. </exception>
+        public virtual async Task<Response<Conversion>> CreateConversionAsync(string conversionId, ConversionSettings settings, CancellationToken cancellationToken = default)
+        {
+            using DiagnosticScope scope = _clientDiagnostics.CreateScope($"{nameof(RemoteRenderingClient)}.{nameof(CreateConversion)}");
+            // TODO Add some attributes?
+            //scope.AddAttribute(nameof(headerOptions.ClientRequestId), headerOptions.ClientRequestId);
+            scope.Start();
+
+            try
+            {
+                ResponseWithHeaders<object, MixedRealityRemoteRenderingCreateConversionHeaders> response = await _restClient.CreateConversionAsync(_accountId, conversionId, new ConversionRequest(settings), cancellationToken).ConfigureAwait(false);
 
                 switch (response.Value)
                 {
