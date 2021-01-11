eb7c6c9c61f71f2721608dfee278d2094111b202
Mohit Chakraborty <mohitc@microsoft.com>
2021-01-11 15:53:31 -0800

    Add perf tests for Blob storage scenarios.
    Create stand-alone test apps for Azure.Storage.Blobs and Microsoft.Azure.Storage.Blob

---
+++ sdk/storage/Azure.Storage.Blobs/perf/Microsoft.Azure.Storage.Blob.Perf/Scenarios/UploadBlob.cs
@@ -0,0 +1,29 @@
+﻿//Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Test.Perf;
+
+namespace Microsoft.Azure.Storage.Blob.Perf.Scenarios
+{
+    public class UploadBlob : RandomBlobTest<SizeOptions>
+    {
+        public UploadBlob(SizeOptions options) : base(options)
+        {
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+            using Stream stream = RandomStream.Create(Options.Size);
+            CloudBlockBlob.UploadFromStream(stream);
+        }
+
+        public override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            using Stream stream = RandomStream.Create(Options.Size);
+            await CloudBlockBlob.UploadFromStreamAsync(stream, cancellationToken);
+        }
+    }
+}
