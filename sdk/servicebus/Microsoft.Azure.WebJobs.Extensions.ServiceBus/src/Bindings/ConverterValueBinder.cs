97183e9572605088294e946e2beff83f20b8ba7c
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-01-13 17:19:50 -0800

    Move ServiceBus extension to Azure SDK repo (#17951)
    https://github.com/Azure/azure-sdk-for-net/issues/17560
    Moving the extensions from https://github.com/Azure/azure-functions-servicebus-extension with minimal changes necessary to compile and have tests passed. This will be helpful as a reference for future changes as we migrate to use the Track 2 library.
    
    Notable issues that should be converted to GH issues:
    - signing the sign.snk did not work as there was no private key, so I'm using the SDK repo snk - https://github.com/Azure/azure-sdk-for-net/issues/17958
    - TestCommon package did not support net461 so I updated the RequiredTargetFrameworks for the test project to just be netcoreapp2.1 - https://github.com/Azure/azure-sdk-for-net/issues/17959

---
+++ sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/src/Bindings/ConverterValueBinder.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using System.Diagnostics;
+
+namespace Microsoft.Azure.WebJobs.ServiceBus.Bindings
+{
+    internal class ConverterValueBinder<TInput> : IOrderedValueBinder
+    {
+        private readonly ServiceBusEntity _entity;
+        private readonly IConverter<TInput, Message> _converter;
+        private readonly Guid _functionInstanceId;
+
+        public ConverterValueBinder(ServiceBusEntity entity, IConverter<TInput, Message> converter,
+            Guid functionInstanceId)
+        {
+            _entity = entity;
+            _converter = converter;
+            _functionInstanceId = functionInstanceId;
+        }
+
+        public BindStepOrder StepOrder
+        {
+            get { return BindStepOrder.Enqueue; }
+        }
+
+        public Type Type
+        {
+            get { return typeof(TInput); }
+        }
+
+        public Task<object> GetValueAsync()
+        {
+            return Task.FromResult<object>(default(TInput));
+        }
+
+        public string ToInvokeString()
+        {
+            return _entity.MessageSender.Path;
+        }
+
+        public Task SetValueAsync(object value, CancellationToken cancellationToken)
+        {
+            Message message = _converter.Convert((TInput)value);
+            Debug.Assert(message != null);
+            return _entity.SendAndCreateEntityIfNotExistsAsync(message, _functionInstanceId, cancellationToken);
+        }
+    }
+}
---
+++ sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/src/Triggers/MessageToPocoConverter.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.InteropExtensions;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace Microsoft.Azure.WebJobs.ServiceBus.Triggers
+{
+    // Convert from Message --> T
+    internal class MessageToPocoConverter<TElement> : IConverter<Message, TElement>
+    {
+        public MessageToPocoConverter()
+        {
+        }
+
+        public TElement Convert(Message message)
+        {
+            // 1. If ContentType is "application/json" deserialize as JSON
+            // 2. If ContentType is not "application/json" attempt to deserialize using Message.GetBody, which will handle cases like XML object serialization
+            // 3. If this deserialization fails, do a final attempt at JSON deserialization to catch cases where the content type might be incorrect
+
+            if (message.ContentType == ContentTypes.ApplicationJson)
+            {
+                return DeserializeJsonObject(message);
+            }
+            else
+            {
+                try
+                {
+                    return message.GetBody<TElement>();
+                }
+                catch (SerializationException)
+                {
+                    return DeserializeJsonObject(message);
+                }
+            }
+        }
+
+        private static TElement DeserializeJsonObject(Message message)
+        {
+            string contents = StrictEncodings.Utf8.GetString(message.Body);
+
+            try
+            {
+                return JsonConvert.DeserializeObject<TElement>(contents, Constants.JsonSerializerSettings);
+            }
+            catch (JsonException e)
+            {
+                // Easy to have the queue payload not deserialize properly. So give a useful error.
+                string msg = string.Format(
+                    CultureInfo.InvariantCulture,
+@"Binding parameters to complex objects (such as '{0}') uses Json.NET serialization or XML object serialization. 
+ 1. If ContentType is 'application/json' deserialize as JSON
+ 2. If ContentType is not 'application/json' attempt to deserialize using Message.GetBody, which will handle cases like XML object serialization
+ 3. If this deserialization fails, do a final attempt at JSON deserialization to catch cases where the content type might be incorrect
+The JSON parser failed: {1}
+", typeof(TElement).Name, e.Message);
+                throw new InvalidOperationException(msg);
+            }
+        }
+    }
+}
