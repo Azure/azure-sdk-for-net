fc3927ea1b730a0fb5cf836e7d82ca2d2f1795df
Artyom Pavlichenko <arpavlic@microsoft.com>
2021-01-15 02:12:24 -0800

    Undoing change to avoid CRLF/LF file changes

--- sdk/datamigration/Microsoft.Azure.Management.DataMigration/src/Generated/SdkInfo_DataMigrationManagementClient.cs
+++ sdk/datamigration/Microsoft.Azure.Management.DataMigration/src/Generated/SdkInfo_DataMigrationManagementClient.cs
@@ -30,5 +30,16 @@ namespace Microsoft.Azure.Management.DataMigration
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/datamigration/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=E:\\forks\\azure-sdk-for-net\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "14313ea8bb4807f71b970334262b4e199797a4a1";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
