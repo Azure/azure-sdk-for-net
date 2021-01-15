d128038f8b23dac6a5d448ad2b2349a9790179a2
Pavel Prystinka <pprystinka@microsoft.com>
2021-01-14 20:12:31 -0800

    return admin model factory

--- sdk/communication/Azure.Communication.Administration/src/Models/SearchAvailablePhoneNumbersOperation.cs
+++ sdk/communication/Azure.Communication.Administration/src/Models/SearchAvailablePhoneNumbersOperation.cs
@@ -8,19 +8,6 @@ namespace Azure.Communication.Administration
     [CodeGenModel("PhoneNumbersSearchAvailablePhoneNumbersOperation")]
     public partial class SearchAvailablePhoneNumbersOperation
     {
-        ///// <summary>
-        ///// Initializes a new <see cref="PhoneNumbersSearchAvailablePhoneNumbersOperation"/> instance
-        ///// </summary>
-        ///// <param name="client"></param>
-        ///// <param name="id"></param>
-        //public PhoneNumbersSearchAvailablePhoneNumbersOperation(PhoneNumbersClient client, string id)
-        //{
-        //    Id = id;
-
-        //    _operation = new ArmOperationHelpers<PhoneNumberSearchResult>(this, client.ClientDiagnostics, client.HttpPipeline, request, response, OperationFinalStateVia.Location, "PhoneNumbersSearchAvailablePhoneNumbersOperation");
-
-        //}
-
         /// <inheritdoc />
         public override string Id { get; }
     }
