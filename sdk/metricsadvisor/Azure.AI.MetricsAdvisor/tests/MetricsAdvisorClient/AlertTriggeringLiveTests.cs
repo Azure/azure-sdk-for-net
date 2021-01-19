0c727aecb270de3a452d132bfbe09d2a9a30941d
Caio Saldanha <camaiaor@microsoft.com>
2021-01-19 09:47:31 -0800

    Added AAD tests for MA Client

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/tests/MetricsAdvisorClient/AlertTriggeringLiveTests.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/tests/MetricsAdvisorClient/AlertTriggeringLiveTests.cs
@@ -78,7 +78,6 @@ namespace Azure.AI.MetricsAdvisor.Tests
                 Assert.That(alert.Timestamp, Is.Not.EqualTo(default(DateTimeOffset)));
                 Assert.That(alert.CreatedTime, Is.Not.EqualTo(default(DateTimeOffset)));
                 Assert.That(alert.ModifiedTime, Is.Not.EqualTo(default(DateTimeOffset)));
-
                 Assert.That(alert.Timestamp, Is.InRange(SamplingStartTime, SamplingEndTime));
 
                 if (++alertCount >= MaximumSamplesCount)
@@ -91,9 +90,11 @@ namespace Azure.AI.MetricsAdvisor.Tests
         }
 
         [RecordedTest]
-        public async Task GetAnomalies()
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task GetAnomalies(bool useTokenCredential)
         {
-            MetricsAdvisorClient client = GetMetricsAdvisorClient();
+            MetricsAdvisorClient client = GetMetricsAdvisorClient(useTokenCredential);
 
             var anomalyCount = 0;
 
@@ -120,9 +121,11 @@ namespace Azure.AI.MetricsAdvisor.Tests
         }
 
         [RecordedTest]
-        public async Task GetIncidents()
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task GetIncidents(bool useTokenCredential)
         {
-            MetricsAdvisorClient client = GetMetricsAdvisorClient();
+            MetricsAdvisorClient client = GetMetricsAdvisorClient(useTokenCredential);
 
             var incidentCount = 0;
 
