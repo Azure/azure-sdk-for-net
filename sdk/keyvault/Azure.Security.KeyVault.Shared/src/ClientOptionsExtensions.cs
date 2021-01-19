643cfba0dd4b3100caf6f603fda1982a299a210e
Heath Stewart <heaths@microsoft.com>
2021-01-19 11:01:12 -0800

    Update service version to 7.2

--- sdk/keyvault/Azure.Security.KeyVault.Shared/src/ClientOptionsExtensions.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Shared/src/ClientOptionsExtensions.cs
@@ -14,6 +14,7 @@ namespace Azure.Security.KeyVault
             allowedHeaderNames.Add("x-ms-keyvault-network-info");
             allowedHeaderNames.Add("x-ms-keyvault-region");
             allowedHeaderNames.Add("x-ms-keyvault-service-version");
+            allowedHeaderNames.Add("x-ms-request-id");
 
             clientOptions.Diagnostics.LoggedQueryParameters.Add("api-version");
         }
