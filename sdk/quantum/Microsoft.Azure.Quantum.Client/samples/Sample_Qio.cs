7f6381513344a44450ae0d09531fe1cd0a268b9a
matusthemostlygreat <matusl@microsoft.com>
2021-01-20 20:28:48 -0800

    matusthemostlygreat - added qio sample

---
+++ sdk/quantum/Microsoft.Azure.Quantum.Client/samples/Sample_Qio.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Azure.Identity;
+using Microsoft.Azure.Quantum.Optimization;
+using NUnit.Framework;
+
+
+namespace Azure.Storage.Blobs.Samples
+{
+    /// <summary>
+    /// Authenticate with <see cref="DefaultAzureCredential"/>.
+    /// </summary>
+    public void Solve()
+    {
+        #region Snippet:SampleSnippetsQio
+        using Microsoft.Azure.Quantum.Optimization;
+
+        IProblem problem = new Problem(
+            name: "myOptimizationProblem",
+            type: ProblemType.ising);
+
+        problem.AddTerm(w: -1, indices: new int[2] { 0, 1 });
+
+        // or
+        Term term = new Term(w: -1, indices: new int[2] { 0, 1 });
+        problem.AddTerm(term);
+
+        // or
+        problem.AddTerms(terms);
+        #endregion
+    }
+}
