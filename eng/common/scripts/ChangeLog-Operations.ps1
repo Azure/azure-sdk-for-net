a0b8eccc5448242ce14a99aedbb79c65f6a81fcb
Chidozie Ononiwu <chononiw@microsoft.com>
2021-01-21 14:35:47 -0800

    Lossen up $RELEASE_TITLE_REGEX to accomodate wrongly formated changelogs

--- eng/common/scripts/ChangeLog-Operations.ps1
+++ eng/common/scripts/ChangeLog-Operations.ps1
@@ -2,7 +2,7 @@
 . "${PSScriptRoot}\logging.ps1"
 . "${PSScriptRoot}\SemVer.ps1"
 
-$RELEASE_TITLE_REGEX = "(?<releaseNoteTitle>^\#+.*(?<version>\b\d+\.\d+\.\d+([^0-9\s][^\s:]+)?)(\s+(?<releaseStatus>\(Unreleased\)|\(\d{4}-\d{2}-\d{2}\)))?)"
+$RELEASE_TITLE_REGEX = "(?<releaseNoteTitle>^\#+.*(?<version>\b\d+\.\d+\.\d+([^0-9\s][^\s:]+)?)(\s+(?<releaseStatus>\(.*\)))?)"
 $CHANGELOG_UNRELEASED_STATUS = "(Unreleased)"
 $CHANGELOG_DATE_FORMAT = "yyyy-MM-dd"
 
