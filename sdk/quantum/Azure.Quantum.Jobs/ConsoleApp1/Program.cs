b8c1d4072a72d718f407a61f66cd94d610c3e1b2
matusthemostlygreat <matusl@microsoft.com>
2021-01-22 15:41:35 -0800

    matusthemostlygreat - checking in console app

---
+++ sdk/quantum/Azure.Quantum.Jobs/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using Azure.Quantum.Jobs;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuantumJobsClient client = new QuantumJobsClient(SubscriptionId.value, "sdk-review-rg", "workspace-ms", "westus");
+            var jobs = client.Jobs.ListAsync(CancellationToken.None).Result;
+
+            Console.WriteLine(JsonConvert.SerializeObject(jobs));
+        }
+    }
+}
+
+// Create SubscriptionId.cs like so and put in your id:
+// namespace ConsoleApp1
+// {
+//     class SubscriptionId
+//     {
+//         public string value = "<yourid>";
+//     }
+// }
