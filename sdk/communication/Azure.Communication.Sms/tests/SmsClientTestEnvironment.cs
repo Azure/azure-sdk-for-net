cc1de11435b0c9fb146dc9946af82ddb5c94eb99
Minnie Liu <peiliu@microsoft.com>
2021-01-22 11:46:06 -0800

    Adding live tests for managed identity SMS

--- sdk/communication/Azure.Communication.Sms/tests/SmsClientTestEnvironment.cs
+++ sdk/communication/Azure.Communication.Sms/tests/SmsClientTestEnvironment.cs
@@ -9,8 +9,10 @@ namespace Azure.Communication.Sms.Tests
     {
         internal const string ConnectionStringEnvironmentVariableName = "AZURE_COMMUNICATION_LIVETEST_CONNECTION_STRING";
         internal const string PhoneNumberEnvironmentVariableName = "AZURE_PHONE_NUMBER";
+        internal const string EndpointEnvironmentVariableName = "COMMUNICATION_ENDPOINT_STRING";
 
         public string ConnectionString => GetRecordedVariable(ConnectionStringEnvironmentVariableName);
         public string PhoneNumber => GetRecordedVariable(PhoneNumberEnvironmentVariableName);
+        public string EndpointString => GetRecordedVariable(EndpointEnvironmentVariableName);
     }
 }
