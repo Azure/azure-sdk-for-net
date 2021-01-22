1ff5ecc82cbcedcd2268029415dfbd4b55c38ac2
matusthemostlygreat <matusl@microsoft.com>
2021-01-21 16:35:31 -0800

    matusthemostlygreat - client builds again

---
+++ sdk/quantum/Microsoft.Azure.Quantum.Client/samples/JobTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Azure.Quantum.Client;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Core.TestFramework;
+//using NUnit.Framework;
+
+
+namespace Microsoft.Azure.Quantum.JobTests
+{
+    public class JobTests : ClientTestBase
+    {
+        public JobTests(bool isAsync) : base(isAsync, RecordedTestMode.Record)
+        {
+            [RecordedTest]
+            public void MyTest()
+            {
+                //...
+            }
+        }
+    }
+}
