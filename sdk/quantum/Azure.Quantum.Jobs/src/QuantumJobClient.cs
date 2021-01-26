7839c698a009002d1e4c6de29a8bb05b19535a53
matusthemostlygreat <matusl@microsoft.com>
2021-01-25 23:42:47 -0800

    matusthemostlygreat - exposed other methods

--- sdk/quantum/Azure.Quantum.Jobs/src/QuantumJobClient.cs
+++ sdk/quantum/Azure.Quantum.Jobs/src/QuantumJobClient.cs
@@ -16,28 +16,6 @@ namespace Azure.Quantum.Jobs
     /// </summary>
     public class QuantumJobClient
     {
-        private static Page<JobDetails> ToPage(Response<JobDetailsList> list) =>
-            Page.FromValues(list.Value.Values, list.Value.NextLink, list.GetRawResponse());
-
-        /// <summary> Return list of jobs. </summary>
-        public virtual Pageable<JobDetails> GetJobs(CancellationToken cancellationToken = default)
-        {
-            return PageResponseEnumerator.CreateEnumerable(cont => ToPage(string.IsNullOrEmpty(cont) ? _jobs.List() : _jobs.ListNextPage(cont)));
-        }
-
-        /// <summary> Return list of jobs. </summary>
-        public virtual AsyncPageable<JobDetails> GetJobsAsync(CancellationToken cancellationToken = default)
-        {
-            return PageResponseEnumerator.CreateAsyncEnumerable(async cont => ToPage(string.IsNullOrEmpty(cont) ? await _jobs.ListAsync().ConfigureAwait(false) : await _jobs.ListNextPageAsync(cont).ConfigureAwait(false)));
-        }
-
-        private JobsRestClient _jobs;
-
-        /// <summary> Initializes a new instance of QuantumJobClient for mocking. </summary>
-        protected QuantumJobClient()
-        {
-        }
-
         /// <summary>
         /// Initializes a new instance of the <see cref="QuantumJobClient"/> class.
         /// </summary>
@@ -76,32 +54,82 @@ namespace Azure.Quantum.Jobs
                 new Uri($"https://{location}.quantum.azure.com"));
         }
 
-        /// <summary> Create a job. </summary>
+        /// <summary> Get job by id. </summary>
         /// <param name="jobId"> Id of the job. </param>
-        /// <param name="job"> The complete metadata of the job to submit. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="jobId"/> or <paramref name="job"/> is null. </exception>
-        public Response<JobDetails> Create(string jobId, JobDetails job, CancellationToken cancellationToken = default)
+        /// <exception cref="ArgumentNullException"> <paramref name="jobId"/> is null. </exception>
+        public virtual Response<JobDetails> GetJob(string jobId, CancellationToken cancellationToken = default)
         {
-            return _jobs.Create(jobId, job, cancellationToken);
+            return _jobs.Get(jobId, cancellationToken);
         }
 
         /// <summary> Get job by id. </summary>
         /// <param name="jobId"> Id of the job. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="jobId"/> is null. </exception>
-        public Response<JobDetails> Get(string jobId, CancellationToken cancellationToken = default)
+        public virtual async Task<Response<JobDetails>> GetJobAsync(string jobId, CancellationToken cancellationToken = default)
         {
-            return _jobs.Get(jobId, cancellationToken);
+            return await _jobs.GetAsync(jobId, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary> Return list of jobs. </summary>
+        public virtual Pageable<JobDetails> GetJobs(CancellationToken cancellationToken = default)
+        {
+            return PageResponseEnumerator.CreateEnumerable(cont => ToPage(string.IsNullOrEmpty(cont) ? _jobs.List() : _jobs.ListNextPage(cont)));
+        }
+
+        /// <summary> Return list of jobs. </summary>
+        public virtual AsyncPageable<JobDetails> GetJobsAsync(CancellationToken cancellationToken = default)
+        {
+            return PageResponseEnumerator.CreateAsyncEnumerable(async cont => ToPage(string.IsNullOrEmpty(cont) ? await _jobs.ListAsync().ConfigureAwait(false) : await _jobs.ListNextPageAsync(cont).ConfigureAwait(false)));
+        }
+
+        /// <summary> Create a job. </summary>
+        /// <param name="jobId"> Id of the job. </param>
+        /// <param name="job"> The complete metadata of the job to submit. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="jobId"/> or <paramref name="job"/> is null. </exception>
+        public virtual Response<JobDetails> Create(string jobId, JobDetails job, CancellationToken cancellationToken = default)
+        {
+            return _jobs.Create(jobId, job, cancellationToken);
+        }
+
+        /// <summary> Create a job. </summary>
+        /// <param name="jobId"> Id of the job. </param>
+        /// <param name="job"> The complete metadata of the job to submit. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="jobId"/> or <paramref name="job"/> is null. </exception>
+        public virtual async Task<Response<JobDetails>> CreateAsync(string jobId, JobDetails job, CancellationToken cancellationToken = default)
+        {
+            return await _jobs.CreateAsync(jobId, job, cancellationToken).ConfigureAwait(false);
         }
 
         /// <summary> Cancel a job. </summary>
         /// <param name="jobId"> Id of the job. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="jobId"/> is null. </exception>
-        public Response Cancel(string jobId, CancellationToken cancellationToken = default)
+        public virtual Response Cancel(string jobId, CancellationToken cancellationToken = default)
         {
             return _jobs.Cancel(jobId, cancellationToken);
         }
+
+        /// <summary> Cancel a job. </summary>
+        /// <param name="jobId"> Id of the job. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="jobId"/> is null. </exception>
+        public virtual async Task<Response> CancelAsync(string jobId, CancellationToken cancellationToken = default)
+        {
+            return await _jobs.CancelAsync(jobId, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary> Initializes a new instance of QuantumJobClient for mocking. </summary>
+        protected QuantumJobClient()
+        {
+        }
+
+        private static Page<JobDetails> ToPage(Response<JobDetailsList> list) =>
+            Page.FromValues(list.Value.Values, list.Value.NextLink, list.GetRawResponse());
+
+        private JobsRestClient _jobs;
     }
 }
