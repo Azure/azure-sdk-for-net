884d489320c3cf92a2fa78a45aac7e3ffdbb42cc
matusthemostlygreat <matusl@microsoft.com>
2021-01-26 21:45:27 -0800

    matusthemostlygreat - fixed vars

--- sdk/quantum/Azure.Quantum.Jobs/tests/QuantumJobClientTestEnvironment.cs
+++ sdk/quantum/Azure.Quantum.Jobs/tests/QuantumJobClientTestEnvironment.cs
@@ -7,9 +7,6 @@ namespace Azure.Quantum.Jobs.Tests
 {
     public class QuantumJobClientTestEnvironment : TestEnvironment
     {
-        public new string SubscriptionId => GetRecordedVariable("SUBSCRIPTION_ID");
-        public string ResourceGroupName => GetRecordedVariable("RESOURCE_GROUP_NAME");
-        public string WorkspaceName => GetRecordedVariable("WORKSPACE_NAME");
         public string WorkspaceLocation => GetRecordedVariable("WORKSPACE_LOCATION");
     }
 }
