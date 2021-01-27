af5c730287b049c983cd76d6ca9517ee183e83bb
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2021-01-27 12:13:27 +0800

    Update archetype-sdk-mgmt.yml for Track1 pipeline (#18164)
    * Update archetype-sdk-mgmt.yml
    
    * Update archetype-sdk-mgmt.yml
    
    * Update Track1CodeCheck.ps1
    
    * Update Track1CodeCheck.ps1

--- eng/scripts/Track1CodeCheck.ps1
+++ eng/scripts/Track1CodeCheck.ps1
@@ -94,7 +94,7 @@ try {
     $folderName = @()
     $changeList | ForEach-Object {
         $fileName = $_.filename
-        if ($fileName -match 'eng/mgmt/mgmtmetadata') {
+        if ($fileName -match '(?<!sdk)/eng/mgmt/mgmtmetadata') {
             $mataPath += $fileName
         }
     }
@@ -182,37 +182,6 @@ try {
         if ($item -notmatch 'SdkInfo_') {
             $exitCode ++
         }
-        else {
-            $changeContent = @()
-            $content = git -c core.safecrlf=false diff -U0 HEAD --ignore-space-at-eol $diffResult[0]
-            $content[4..($content.Length - 1)] | ForEach-Object {
-                if ($_.StartsWith('+') -or $_.StartsWith('-')) {
-                    $changeContent += $_
-                }
-            }
-            if ($changeContent.Length -ne 11) {
-                $exitCode ++
-            }
-    
-            # metaDataContent doesn't contains 'AutoRestCmdExecuted' part since it can't be verified
-            $metaDataContent = @('-      // BEGIN: Code Generation Metadata Section',
-                '-      public static readonly String AutoRestVersion = "v2";',
-                '-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";',
-                '-      public static readonly String GithubForkName = "Azure";',
-                '-      public static readonly String GithubBranchName = "master";',
-                '-      public static readonly String GithubCommidId = "{0}";'.Replace('{0}', $commit),
-                '-      public static readonly String CodeGenerationErrors = "";',
-                '-      public static readonly String GithubRepoName = "azure-rest-api-specs";',
-                '-      // END: Code Generation Metadata Section'
-            )
-            foreach ($metaString in $metaDataContent) {
-                if ($metaString -notin $changeContent) {
-                    Write-Output "Diff in" $metaString
-                    $exitCode ++
-                    break
-                }
-            }
-        }
     }
     
     if ($exitCode -ne 0) {
@@ -220,9 +189,11 @@ try {
         $diffResult | ForEach-Object {
             Write-Output $_
         }
-        LogError "============================"
+        Write-Output "Git Diff detail: "
+        git -c core.safecrlf=false diff HEAD --ignore-space-at-eol
+        Write-Host "============================"
         LogError "Discrepancy detected between generated code in PR and reference generation. Please note, the files in the Generated folder should not be modified OR adding/excluding files. You may need to re-run sdk<RP_Name>\generate.ps1."
-        LogError "============================"
+        Write-Host "============================"
         Write-Host "For reference, we are using this command for the code check: " -ForegroundColor red -BackgroundColor white
         Write-Host "  autorest https://github.com/<Repo_Name>/azure-rest-api-specs/blob/<Commit_Id>/specification/<RP_Name>/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=<SDK_Repo_Path>/sdk --use:@microsoft.azure/autorest.csharp@2.3.90" -ForegroundColor red -BackgroundColor white
     }
