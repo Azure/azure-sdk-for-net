b68cb80675e77d1b78cc045f509707710ae4b62e
XField <58103249+vxfield@users.noreply.github.com>
2021-01-27 12:53:08 -0800

    Adding missing using statement

--- sdk/quantum/Microsoft.Azure.Management.Quantum/tests/Properties/AssemblyInfo.cs
+++ sdk/quantum/Microsoft.Azure.Management.Quantum/tests/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Xunit;
