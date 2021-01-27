c34ac4130668ad429a3fd0cabbaee9d62d211c9b
Anthony Noakes <annoakes@microsoft.com>
2021-01-27 14:20:58 -0800

    Update code location to make generation file

--- sdk/purview/Management.Purview/Generated/SdkInfo_PurviewManagementClient.cs
+++ sdk/purview/Management.Purview/Generated/SdkInfo_PurviewManagementClient.cs
@@ -27,5 +27,16 @@ namespace Microsoft.Azure.Management.Purview
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/purview/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --tag=package-2020-12-01-preview --csharp-sdks-folder=D:\\enlist\\arm\\azure-sdk-for-net\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "510bbe951a14d183b553ae9b64b514687842c64c";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
