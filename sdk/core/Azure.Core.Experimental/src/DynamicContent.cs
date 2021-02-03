0faeb4f0ea18bdc3a2be32b3ee54aba78e906c5f
Chris Hamons <chris.hamons@microsoft.com>
2021-02-03 16:01:20 -0600

    [Core.Experimental] Add DynamicContent/DynamicRequest/DynamicResponse for LLC prototype (#18323)
    - Originally from https://github.com/ellismg/AnomalyDetectorLowLevelClient/

---
+++ sdk/core/Azure.Core.Experimental/src/DynamicContent.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Core
+{
+    /// <summary>
+    /// Represents the <see cref="DynamicJson"/> sent as part of the Azure.Core.Request.
+    /// </summary>
+    [DebuggerDisplay("Content: {_body}")]
+    public class DynamicContent : RequestContent
+    {
+        private readonly DynamicJson _body;
+
+        internal DynamicContent(DynamicJson body)
+        {
+            _body = body;
+        }
+
+        internal static RequestContent Create(DynamicJson body) => new DynamicContent(body);
+
+        /// <inheritdoc />
+        public override async Task WriteToAsync(Stream stream, CancellationToken cancellation)
+        {
+            using Utf8JsonWriter writer = new Utf8JsonWriter(stream);
+            _body.WriteTo(writer);
+            await writer.FlushAsync(cancellation).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public override void WriteTo(Stream stream, CancellationToken cancellation)
+        {
+            using Utf8JsonWriter writer = new Utf8JsonWriter(stream);
+            _body.WriteTo(writer);
+            writer.Flush();
+        }
+
+        /// <inheritdoc />
+        public override bool TryComputeLength(out long length)
+        {
+            using MemoryStream stream = new MemoryStream();
+            WriteTo(stream, CancellationToken.None);
+            length = Encoding.UTF8.GetString(stream.ToArray()).Length;
+            return true;
+        }
+
+        /// <inheritdoc />
+        public override void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
