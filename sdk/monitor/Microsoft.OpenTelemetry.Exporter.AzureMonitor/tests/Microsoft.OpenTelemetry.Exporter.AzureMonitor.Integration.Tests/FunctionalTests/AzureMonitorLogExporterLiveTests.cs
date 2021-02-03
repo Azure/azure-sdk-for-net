1fa8451938e4c52255877a11c810800950532389
Timothy Mothra Lee <tilee@microsoft.com>
2021-02-02 16:16:30 -0800

    fully automated test

--- sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/tests/Microsoft.OpenTelemetry.Exporter.AzureMonitor.Integration.Tests/FunctionalTests/AzureMonitorLogExporterLiveTests.cs
+++ sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/tests/Microsoft.OpenTelemetry.Exporter.AzureMonitor.Integration.Tests/FunctionalTests/AzureMonitorLogExporterLiveTests.cs
@@ -3,7 +3,6 @@
 
 namespace Microsoft.OpenTelemetry.Exporter.AzureMonitor.Integration.Tests.FunctionalTests
 {
-    using System;
     using System.Net.Http;
     using System.Threading.Tasks;
 
@@ -15,11 +14,10 @@ namespace Microsoft.OpenTelemetry.Exporter.AzureMonitor.Integration.Tests.Functi
     using Microsoft.Azure.ApplicationInsights.Query;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
-    using Microsoft.Rest.Azure.Authentication;
 
     using NUnit.Framework;
 
-    public class AzureMonitorLogExporterLiveTests : RecordedTestBase<AzureMonitorTestEnvironment>
+    public class AzureMonitorLogExporterLiveTests : AzureMonitorTestBase
     {
         public AzureMonitorLogExporterLiveTests(bool isAsync) : base(isAsync, RecordedTestMode.Record)
         {
@@ -61,65 +59,13 @@ namespace Microsoft.OpenTelemetry.Exporter.AzureMonitor.Integration.Tests.Functi
 
             processor.ForceFlush();
 
-            await Task.Delay(GetIngestionWaitTimeSpan());
+            await this.WaitForIgnestionAsync();
 
-            // VERIFY; TODO: FETCH TELEMETRY FROM AZURE MONITOR
+            // VERIFY
+            var client = await this.GetApplicationInsightsDataClientAsync();
 
-            // TODO: PROGRAMATICALLY FETCH API KEY
-            // https://dev.applicationinsights.io/documentation/Authorization/API-key-and-App-ID
-            // https://docs.microsoft.com/en-us/cli/azure/ext/application-insights/monitor/app-insights/api-key?view=azure-cli-latest
-
-            // THIS WORKS: Query logs from Kusto https://dev.applicationinsights.io/quickstart
-            //client.DefaultRequestHeaders.Add("x-api-key", "");
-            //string appId = TestEnvironment.ApplicationId;
-            //var path = $"https://api.applicationinsights.io/v1/apps/{appId}/query?query=traces%7C%20where%20message%20%3D%3D%20%22Hello%20World!%22";
-            //HttpResponseMessage response = await client.GetAsync(path);
-            //Assert.IsTrue(response.IsSuccessStatusCode);
-            // var responseContent = await response.Content.ReadAsStringAsync();
-
-            // TODO: Might be able to use this instead
-            // https://dev.applicationinsights.io/documentation/Tools/CSharp-Sdk
-            // https://www.nuget.org/packages/Microsoft.Azure.ApplicationInsights.Query
-            // https://github.com/Azure/azure-sdk-for-net/tree/Microsoft.Azure.ApplicationInsights.Query_1.0.0/sdk/applicationinsights/Microsoft.Azure.ApplicationInsights.Query
-            //var apiKey = ""; // DO NOT CHECK IN!!!
-            //var testTimespan = "PT1H"; // ISO 8601 Format for Durations https://en.wikipedia.org/wiki/ISO_8601?oldformat=true#Durations
-            //var creds = new ApiKeyClientCredentials(apiKey);
-            //var client = new ApplicationInsightsDataClient(creds);
-            //var test = await client.Events.GetTraceEventsAsync(appId: TestEnvironment.ApplicationId, timespan: testTimespan);
-            //Assert.AreEqual(testMessage, test.Value[0].Trace.Message);
-
-            var clientId = TestEnvironment.ClientId;
-            var clientSecret = TestEnvironment.ClientSecret;
-            var domain = TestEnvironment.TenantId;
-            var authEndpoint = "https://login.microsoftonline.com";
-            var tokenAudience = "https://api.applicationinsights.io/";
-            var adSettings = new ActiveDirectoryServiceSettings
-            {
-                AuthenticationEndpoint = new Uri(authEndpoint),
-                TokenAudience = new Uri(tokenAudience),
-                ValidateAuthority = true
-            };
-
-            // Authenticate with client secret (app key)
-            var creds = ApplicationTokenProvider.LoginSilentAsync(domain, clientId, clientSecret, adSettings).GetAwaiter().GetResult();
-
-            // Instantiate a client with credentials
-            var testTimespan = "PT1H"; // ISO 8601 Format for Durations https://en.wikipedia.org/wiki/ISO_8601?oldformat=true#Durations
-            var client = new ApplicationInsightsDataClient(creds);
-            var test = await client.Events.GetTraceEventsAsync(appId: TestEnvironment.ApplicationId, timespan: testTimespan);
+            var test = await client.Events.GetTraceEventsAsync(appId: TestEnvironment.ApplicationId, timespan: QueryDuration.TenMinutes);
             Assert.AreEqual(testMessage, test.Value[0].Trace.Message);
-
-            Assert.Inconclusive();
-        }
-
-        private TimeSpan GetIngestionWaitTimeSpan()
-        {
-            return TimeSpan.FromSeconds(0);
-
-            //if (this.Mode == RecordedTestMode.Playback)
-            //{
-            //    return new TimeSpan(hours: 0, minutes: 0, seconds: 0);
-            //}
         }
     }
 }
---
+++ sdk/monitor/Microsoft.OpenTelemetry.Exporter.AzureMonitor/tests/Microsoft.OpenTelemetry.Exporter.AzureMonitor.Integration.Tests/FunctionalTests/AzureMonitorTestBase.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.OpenTelemetry.Exporter.AzureMonitor.Integration.Tests.FunctionalTests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using global::Azure.Core.TestFramework;
+
+    using Microsoft.Azure.ApplicationInsights.Query;
+    using Microsoft.Rest.Azure.Authentication;
+
+    public abstract class AzureMonitorTestBase : RecordedTestBase<AzureMonitorTestEnvironment>
+    {
+        protected string QueryTimeSpan { get; set; }
+
+        public AzureMonitorTestBase(bool isAsync) : base(isAsync)
+        {
+        }
+
+        public AzureMonitorTestBase(bool isAsync, RecordedTestMode mode) : base(isAsync, mode)
+        {
+        }
+
+        /// <summary>
+        /// Uses the TestEnvironment to log into Application Insights using a Service Principal.
+        /// These values are created when running the New-TestResources.ps1 script.
+        /// The code here comes from the sample provided in the Application Insights REST API doc.
+        /// (https://dev.applicationinsights.io/documentation/Tools/CSharp-Sdk).
+        /// </summary>
+        /// <remarks>
+        /// Alternatively for manual testing, can supply an Application Insights API Key.
+        /// (https://dev.applicationinsights.io/documentation/Authorization/API-key-and-App-ID).
+        /// <code>var creds = new ApiKeyClientCredentials("00000000-0000-0000-0000-000000000000");</code>
+        /// </remarks>
+        protected async Task<ApplicationInsightsDataClient> GetApplicationInsightsDataClientAsync()
+        {
+            var clientId = TestEnvironment.ClientId;
+            var clientSecret = TestEnvironment.ClientSecret;
+            var domain = TestEnvironment.TenantId;
+            var authEndpoint = "https://login.microsoftonline.com";
+            var tokenAudience = "https://api.applicationinsights.io/";
+            var adSettings = new ActiveDirectoryServiceSettings
+            {
+                AuthenticationEndpoint = new Uri(authEndpoint),
+                TokenAudience = new Uri(tokenAudience),
+                ValidateAuthority = true
+            };
+
+            var creds = await ApplicationTokenProvider.LoginSilentAsync(domain, clientId, clientSecret, adSettings);
+
+            var client = new ApplicationInsightsDataClient(creds);
+            return client;
+        }
+
+        /// <summary>
+        /// Application Insights ingestion is not immediate.
+        /// On a good day, this can take a few minutes.
+        /// </summary>
+        protected async Task WaitForIgnestionAsync()
+        {
+            TimeSpan timeSpan;
+
+            switch (this.Mode)
+            {
+                case RecordedTestMode.Live:
+                case RecordedTestMode.Record:
+                    timeSpan = TimeSpan.FromMinutes(5);
+                    break;
+                case RecordedTestMode.Playback:
+                    timeSpan = TimeSpan.FromSeconds(0);
+                    break;
+                default:
+                    throw new Exception($"Unknown RecordedTestMode '{this.Mode}'");
+            }
+
+            await Task.Delay(timeSpan);
+        }
+
+        /// <summary>
+        /// ISO 8601 Format for Durations.
+        /// (https://en.wikipedia.org/wiki/ISO_8601?oldformat=true#Durations).
+        /// </summary>
+        protected static class QueryDuration
+        {
+            public static string OneHour = "PT1H";
+            public static string TenMinutes = "PT10M";
+        }
+    }
+}
