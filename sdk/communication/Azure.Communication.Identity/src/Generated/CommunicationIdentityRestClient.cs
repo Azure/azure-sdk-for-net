3e55b6f22b7c2a71c5d90fd49a44d3baee6317f2
Avin Pattath <avinp@microsoft.com>
2021-02-04 15:37:53 -0800

    generate turn sdk api from swagger

--- sdk/communication/Azure.Communication.Identity/src/Generated/CommunicationIdentityRestClient.cs
+++ sdk/communication/Azure.Communication.Identity/src/Generated/CommunicationIdentityRestClient.cs
@@ -20,7 +20,6 @@ namespace Azure.Communication.Identity
     internal partial class CommunicationIdentityRestClient
     {
         private string endpoint;
-        private string apiVersion;
         private ClientDiagnostics _clientDiagnostics;
         private HttpPipeline _pipeline;
 
@@ -28,21 +27,15 @@ namespace Azure.Communication.Identity
         /// <param name="clientDiagnostics"> The handler for diagnostic messaging in the client. </param>
         /// <param name="pipeline"> The HTTP pipeline for sending and receiving REST requests and responses. </param>
         /// <param name="endpoint"> The communication resource, for example https://my-resource.communication.azure.com. </param>
-        /// <param name="apiVersion"> Api Version. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="endpoint"/> or <paramref name="apiVersion"/> is null. </exception>
-        public CommunicationIdentityRestClient(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string endpoint, string apiVersion = "2021-03-07")
+        /// <exception cref="ArgumentNullException"> <paramref name="endpoint"/> is null. </exception>
+        public CommunicationIdentityRestClient(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string endpoint)
         {
             if (endpoint == null)
             {
                 throw new ArgumentNullException(nameof(endpoint));
             }
-            if (apiVersion == null)
-            {
-                throw new ArgumentNullException(nameof(apiVersion));
-            }
 
             this.endpoint = endpoint;
-            this.apiVersion = apiVersion;
             _clientDiagnostics = clientDiagnostics;
             _pipeline = pipeline;
         }
@@ -55,7 +48,7 @@ namespace Azure.Communication.Identity
             var uri = new RawRequestUriBuilder();
             uri.AppendRaw(endpoint, false);
             uri.AppendPath("/identities", false);
-            uri.AppendQuery("api-version", apiVersion, true);
+            uri.AppendQuery("api-version", "2021-03-07", true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
             request.Headers.Add("Content-Type", "application/json");
@@ -122,7 +115,7 @@ namespace Azure.Communication.Identity
             uri.AppendRaw(endpoint, false);
             uri.AppendPath("/identities/", false);
             uri.AppendPath(id, true);
-            uri.AppendQuery("api-version", apiVersion, true);
+            uri.AppendQuery("api-version", "2021-03-07", true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
             return message;
@@ -182,7 +175,7 @@ namespace Azure.Communication.Identity
             uri.AppendPath("/identities/", false);
             uri.AppendPath(id, true);
             uri.AppendPath("/:revokeAccessTokens", false);
-            uri.AppendQuery("api-version", apiVersion, true);
+            uri.AppendQuery("api-version", "2021-03-07", true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
             return message;
@@ -242,7 +235,7 @@ namespace Azure.Communication.Identity
             uri.AppendPath("/identities/", false);
             uri.AppendPath(id, true);
             uri.AppendPath("/:issueAccessToken", false);
-            uri.AppendQuery("api-version", apiVersion, true);
+            uri.AppendQuery("api-version", "2021-03-07", true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
             request.Headers.Add("Content-Type", "application/json");
@@ -310,6 +303,76 @@ namespace Azure.Communication.Identity
                         CommunicationIdentityAccessToken value = default;
                         using var document = JsonDocument.Parse(message.Response.ContentStream);
                         value = CommunicationIdentityAccessToken.DeserializeCommunicationIdentityAccessToken(document.RootElement);
+                        return Response.FromValue(value, message.Response);
+                    }
+                default:
+                    throw _clientDiagnostics.CreateRequestFailedException(message.Response);
+            }
+        }
+
+        internal HttpMessage CreateIssueTurnCredentialsRequest(string id)
+        {
+            var message = _pipeline.CreateMessage();
+            var request = message.Request;
+            request.Method = RequestMethod.Post;
+            var uri = new RawRequestUriBuilder();
+            uri.AppendRaw(endpoint, false);
+            uri.AppendPath("/turn/", false);
+            uri.AppendPath(id, true);
+            uri.AppendPath("/:issueCredentials", false);
+            uri.AppendQuery("api-version", "2021-02-22-preview1", true);
+            request.Uri = uri;
+            request.Headers.Add("Accept", "application/json");
+            return message;
+        }
+
+        /// <summary> Issue TURN credentials for an existing identity. </summary>
+        /// <param name="id"> Identifier of the existing identity to issue credentials for. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="id"/> is null. </exception>
+        public async Task<Response<TurnCredentialsResponse>> IssueTurnCredentialsAsync(string id, CancellationToken cancellationToken = default)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            using var message = CreateIssueTurnCredentialsRequest(id);
+            await _pipeline.SendAsync(message, cancellationToken).ConfigureAwait(false);
+            switch (message.Response.Status)
+            {
+                case 200:
+                    {
+                        TurnCredentialsResponse value = default;
+                        using var document = await JsonDocument.ParseAsync(message.Response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                        value = TurnCredentialsResponse.DeserializeTurnCredentialsResponse(document.RootElement);
+                        return Response.FromValue(value, message.Response);
+                    }
+                default:
+                    throw await _clientDiagnostics.CreateRequestFailedExceptionAsync(message.Response).ConfigureAwait(false);
+            }
+        }
+
+        /// <summary> Issue TURN credentials for an existing identity. </summary>
+        /// <param name="id"> Identifier of the existing identity to issue credentials for. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="id"/> is null. </exception>
+        public Response<TurnCredentialsResponse> IssueTurnCredentials(string id, CancellationToken cancellationToken = default)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            using var message = CreateIssueTurnCredentialsRequest(id);
+            _pipeline.Send(message, cancellationToken);
+            switch (message.Response.Status)
+            {
+                case 200:
+                    {
+                        TurnCredentialsResponse value = default;
+                        using var document = JsonDocument.Parse(message.Response.ContentStream);
+                        value = TurnCredentialsResponse.DeserializeTurnCredentialsResponse(document.RootElement);
                         return Response.FromValue(value, message.Response);
                     }
                 default:
--- sdk/communication/Azure.Communication.Identity/src/autorest.md
+++ sdk/communication/Azure.Communication.Identity/src/autorest.md
@@ -7,6 +7,11 @@ Run `dotnet msbuild /t:GenerateCode` to generate code.
 
 ``` yaml
 input-file:
-    -  https://raw.githubusercontent.com/Azure/azure-rest-api-specs/5b19c6e69cd2bb9dbe4e5c1237b2c5a175d90ca5/specification/communication/data-plane/Microsoft.CommunicationServicesIdentity/stable/2021-03-07/CommunicationIdentity.json
+#    -  https://raw.githubusercontent.com/Azure/azure-rest-api-specs/5b19c6e69cd2bb9dbe4e5c1237b2c5a175d90ca5/specification/communication/data-plane/Microsoft.CommunicationServicesIdentity/stable/2021-03-07/CommunicationIdentity.json
+#    -  https://github.com/apattath/azure-rest-api-specs/blob/08ac674efacce0ae35371f8b1d8c45b5a1420503/specification/communication/data-plane/Microsoft.CommunicationServicesTurn/preview/2021-02-22-preview1/CommunicationTurn.json
+#    - https://github.com/apattath/azure-rest-api-specs/blob/9edffc88b87fdc90a4d339a0b49487d611304c6f/specification/communication/data-plane/Microsoft.CommunicationServicesIdentity/stable/2021-03-07/CommunicationIdentity.json
+#    - https://github.com/apattath/azure-rest-api-specs/blob/9edffc88b87fdc90a4d339a0b49487d611304c6f/specification/communication/data-plane/Microsoft.CommunicationServicesTurn/preview/2021-02-22-preview1/CommunicationTurn.json
+    -  ./swaggers/CommunicationIdentity.json
+    -  ./swaggers/CommunicationTurn.json
 payload-flattening-threshold: 3
 ```
