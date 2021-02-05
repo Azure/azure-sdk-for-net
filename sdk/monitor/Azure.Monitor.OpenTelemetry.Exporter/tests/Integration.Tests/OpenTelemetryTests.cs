9a25ed630f535acdbdce477f7305b1a853d0706c
Timothy Mothra Lee <tilee@microsoft.com>
2021-02-05 13:07:51 -0800

    cleanup

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Integration.Tests/OpenTelemetryTests.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Integration.Tests/OpenTelemetryTests.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Core.TestFramework;
 using Azure.Monitor.OpenTelemetry.Exporter.Integration.Tests.TestFramework;
 
 using Xunit;
