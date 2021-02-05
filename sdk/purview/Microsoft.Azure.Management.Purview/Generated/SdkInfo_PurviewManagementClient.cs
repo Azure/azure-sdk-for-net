3413971f11db1741ad8851b8fc9331ce3a301bcf
Anthony Noakes <annoakes@microsoft.com>
2021-02-05 14:57:53 -0800

    Update SDK generation file location

--- sdk/purview/Microsoft.Azure.Management.Purview/Generated/SdkInfo_PurviewManagementClient.cs
+++ sdk/purview/Microsoft.Azure.Management.Purview/Generated/SdkInfo_PurviewManagementClient.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Azure.Management.Purview
       public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/purview/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --tag=package-2020-12-01-preview --csharp-sdks-folder=D:\\enlist\\arm\\azure-sdk-for-net\\sdk";
       public static readonly String GithubForkName = "Azure";
       public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "510bbe951a14d183b553ae9b64b514687842c64c";
+      public static readonly String GithubCommidId = "d6e60da2c05d341a2926fa9fec0e566f14e8e9ad";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
