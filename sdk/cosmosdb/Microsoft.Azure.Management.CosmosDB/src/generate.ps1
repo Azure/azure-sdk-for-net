e41920f631f96399c9ed4c90db28c046fcef8fe2
Nikhil Chaturvedi <nichatur@microsoft.com>
2021-02-06 19:04:37 -0800

    Updating generate.ps1

--- sdk/cosmosdb/Microsoft.Azure.Management.CosmosDB/src/generate.ps1
+++ sdk/cosmosdb/Microsoft.Azure.Management.CosmosDB/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "cosmos-db/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "cosmos-db/resource-manager" -AutoRestVersion "v2" -ConfigFileTag package-2020-06-preview
