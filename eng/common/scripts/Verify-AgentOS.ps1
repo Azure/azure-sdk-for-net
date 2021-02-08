cbb114a95bf536139f7762a4fee9181ecc567088
Mitch Denny <midenn@microsoft.com>
2021-02-08 11:22:33 +1100

    Address feedback

--- eng/common/scripts/Verify-AgentOS.ps1
+++ eng/common/scripts/Verify-AgentOS.ps1
@@ -8,27 +8,53 @@ param (
     [string] $AgentImage
 )
 
-$lookup = @(
-    @{ AgentPool = "Azure Pipelines"; AgentImage = "windows-2019"; AgentOSMatch = $IsWindows }
-    @{ AgentPool = "Azure Pipelines"; AgentImage = "vs2017-win2016"; AgentOSMatch = $IsWindows }
-    @{ AgentPool = "Azure Pipelines"; AgentImage = "ubuntu-latest"; AgentOSMatch = $IsLinux }
-    @{ AgentPool = "Azure Pipelines"; AgentImage = "ubuntu-20.04"; AgentOSMatch = $IsLinux }
-    @{ AgentPool = "Azure Pipelines"; AgentImage = "ubuntu-18.04"; AgentOSMatch = $IsLinux }
-    @{ AgentPool = "Azure Pipelines"; AgentImage = "ubuntu-16.04"; AgentOSMatch = $IsLinux }
-    @{ AgentPool = "Azure Pipelines"; AgentImage = "macOS-latest"; AgentOSMatch = $IsMacOs }
-    @{ AgentPool = "Azure Pipelines"; AgentImage = "macOS-10.05"; AgentOSMatch = $IsMacOs }
-    @{ AgentPool = "Azure Pipelines"; AgentImage = "macOS-10.04"; AgentOSMatch = $IsMacOs }
-    @{ AgentPool = "azsdk-pool-mms-ubuntu-1604-general"; AgentImage = "MMSUbuntu16.04"; AgentOSMatch = $IsLinux }
-    @{ AgentPool = "azsdk-pool-mms-ubuntu-1804-general"; AgentImage = "MMSUbuntu18.04"; AgentOSMatch = $IsLinux }
-    @{ AgentPool = "azsdk-pool-mms-ubuntu-2004-general"; AgentImage = "MMSUbuntu20.04"; AgentOSMatch = $IsLinux }
-    @{ AgentPool = "azsdk-pool-mms-win-2019-general"; AgentImage = "MMS2019"; AgentOSMatch = $IsWindows }
-)
+class Rule {
+    [string]
+    $AgentPool
+    [string]
+    $AgentImage
+    [string]
+    $AgentOS
 
-foreach ($mapping in $lookup)
-{
-    if (($mapping.AgentPool -eq $AgentPool) -and ($mapping.AgentImage -eq $AgentImage) -and $mapping.AgentOSMatch) {
-        return;
+    Rule([string]$agentPool, [string]$agentImage, [string]$agentOS) {
+        $this.AgentPool = $agentPool
+        $this.AgentImage = $agentImage
+        $this.AgentOS = $agentOS
     }
 }
 
-throw "Agent operating system failed validation!"
+$rules = @(
+    [Rule]::new("Azure Pipelines", "windows-2019", "Windows")
+    [Rule]::new("Azure Pipelines", "vs2017-win2016", "Windows")
+    [Rule]::new("Azure Pipelines", "ubuntu-latest", "Linux")
+    [Rule]::new("Azure Pipelines", "ubuntu-20.04", "Linux")
+    [Rule]::new("Azure Pipelines", "ubuntu-18.04", "Linux")
+    [Rule]::new("Azure Pipelines", "ubuntu-16.04", "Linux")
+    [Rule]::new("Azure Pipelines", "macOS-latest", "MacOS")
+    [Rule]::new("Azure Pipelines", "macOS-10.05", "MacOS")
+    [Rule]::new("Azure Pipelines", "macOS-10.04", "MacOS")
+    [Rule]::new("azsdk-pool-mms-ubuntu-1604-general", "MMSUbuntu16.04", "Linux")
+    [Rule]::new("azsdk-pool-mms-ubuntu-1804-general", "MMSUbuntu18.04", "Linux")
+    [Rule]::new("azsdk-pool-mms-ubuntu-2004-general", "MMSUbuntu20.04", "Linux")
+    [Rule]::new("azsdk-pool-mms-win-2019-general", "MMS2019", "Windows")
+)
+
+Write-Host "Valid rules are:"
+$rules | Format-Table
+
+foreach ($rule in $rules)
+{
+    if (($rule.AgentPool -eq $AgentPool) -and ($rule.AgentImage -eq $AgentImage)) {
+
+        # This strange expression allows us to exploit the built-in PowerShell OS detection
+        # without us having to worry about parsing OS version strings etc. 
+        if (($rule.AgentOS -eq "Windows" -and $IsWindows) -or `
+            ($rule.AgentOS -eq "Linux" -and $IsLinux) -or `
+            ($rule.AgentOS -eq "MacOS" -and $IsMacOS)) {
+            
+            return;
+        }
+    }
+}
+
+throw "Invalid operating system detected. Operating system was: $([System.Runtime.InteropServices.RuntimeInformation]::OSDescription)"
