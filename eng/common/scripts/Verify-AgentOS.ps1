ec9bc8c8426f8ac6ddcede4dcbd7c14e036332a7
Mitch Denny <midenn@microsoft.com>
2021-02-09 09:38:03 +1100

    ! instead of -not

--- eng/common/scripts/Verify-AgentOS.ps1
+++ eng/common/scripts/Verify-AgentOS.ps1
@@ -8,6 +8,6 @@ function Throw-InvalidOperatingSystem {
     throw "Invalid operating system detected. Operating system was: $([System.Runtime.InteropServices.RuntimeInformation]::OSDescription), expected image was: $AgentImage"
 }
 
-if ($AgentImage -match "windows|win|MMS2019" -and (-not $IsWindows)) { Throw-InvalidOperatingSystem }
-if ($AgentImage -match "ubuntu" -and (-not $IsLinux)) { Throw-InvalidOperatingSystem }
-if ($AgentImage -match "macos" -and (-not $IsMacOs)) { Throw-InvalidOperatingSystem }
+if ($AgentImage -match "windows|win|MMS2019" -and !$IsWindows) { Throw-InvalidOperatingSystem }
+if ($AgentImage -match "ubuntu" -and !$IsLinux) { Throw-InvalidOperatingSystem }
+if ($AgentImage -match "macos" -and !$IsMacOs) { Throw-InvalidOperatingSystem }
