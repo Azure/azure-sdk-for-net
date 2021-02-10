51d5e68d15ab485b1defe1400808d1e10adf221f
Timothy Mothra Lee <tilee@microsoft.com>
2021-02-10 14:37:03 -0800

    adding service version

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/AzureMonitorExporterOptions.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/AzureMonitorExporterOptions.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.ComponentModel;
+
 using Azure.Core;
 
 namespace Azure.Monitor.OpenTelemetry.Exporter
@@ -8,5 +10,8 @@ namespace Azure.Monitor.OpenTelemetry.Exporter
     public class AzureMonitorExporterOptions : ClientOptions
     {
         public string ConnectionString { get; set; }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public ServiceVersion ServiceVersion { get; set; } = ServiceVersion.V2020_09_15_Preview;
     }
 }
---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/ServiceVersion.cs
@@ -0,0 +1,11 @@
+﻿namespace Azure.Monitor.OpenTelemetry.Exporter
+{
+    /// <summary>
+    /// These values define the version of the Azure Monitor Ingestion Service.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "<Pending>")]
+    public enum ServiceVersion
+    {
+        V2020_09_15_Preview = 1, // https://github.com/Azure/azure-rest-api-specs/blob/master/specification/applicationinsights/data-plane/Monitor.Exporters/preview/2020-09-15_Preview/swagger.json
+    }
+}
