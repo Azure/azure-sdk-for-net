3c716308d0571ff65ad73ed9916cfe24ef3efe5a
Aleksandar Tomic <altom@microsoft.com>
2021-02-11 22:50:45 +0000

    Addressing PR comments

--- sdk/mixedreality/Azure.MixedReality.RemoteRendering/src/Customizations/Models/RemoteRenderingModelFactory.cs
+++ sdk/mixedreality/Azure.MixedReality.RemoteRendering/src/Customizations/Models/RemoteRenderingModelFactory.cs
@@ -16,28 +16,24 @@ namespace Azure.MixedReality.RemoteRendering
         /// <summary>
         /// Initializes a new instance of the <see cref="AssetConversionOutput"/> class for mocking purposes.
         /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
         public static AssetConversionOutput AssetConversionOutput(Uri outputAssetUri)
             => new AssetConversionOutput(outputAssetUri.ToString());
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RemoteRenderingServiceError"/> class for mocking purposes.
         /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
         public static RemoteRenderingServiceError RemoteRenderingServiceError(string code, string message, IReadOnlyList<RemoteRenderingServiceError> details, string target, RemoteRenderingServiceError innerError)
             => new RemoteRenderingServiceError(code, message, details, target, innerError);
 
         /// <summary>
         /// Initializes a new instance of the <see cref="AssetConversion"/> class for mocking purposes.
         /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
         public static AssetConversion AssetConversion(string conversionId, AssetConversionOptions options, AssetConversionOutput output, RemoteRenderingServiceError error, AssetConversionStatus status, DateTimeOffset createdOn)
             => new AssetConversion(conversionId, options, output, error, status, createdOn);
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RenderingSession"/> class for mocking purposes.
         /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Never)]
         public static RenderingSession RenderingSession(string sessionId, int? arrInspectorPort, int? handshakePort, int? elapsedTimeMinutes, string host, int? maxLeaseTimeMinutes, RenderingServerSize size, RenderingSessionStatus status, float? teraflops, RemoteRenderingServiceError error, DateTimeOffset? createdOn)
             => new RenderingSession(sessionId, arrInspectorPort, handshakePort, elapsedTimeMinutes, host, maxLeaseTimeMinutes, size, status, teraflops, error, createdOn);
     }
