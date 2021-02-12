51881dc5c375512196996d658b423bd88c3a79e2
Alfredo Santamaria Gomez <alsantam@microsoft.com>
2021-02-12 14:12:28 -0800

    generate managed clusters sdk

---
+++ sdk/servicefabricmanagedclusters/Microsoft.Azure.Management.ServiceFabricManagedClusters/src/generate.ps1
@@ -0,0 +1,3 @@
+#TODO alsantam: fix when pr is merged https://github.com/Azure/azure-rest-api-specs/pull/12858
+#Start-AutoRestCodeGeneration -ResourceProvider "servicefabricmanagedclusters/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "servicefabricmanagedclusters/resource-manager" -AutoRestVersion "v2" -SpecsRepoFork a-santamaria -SpecsRepoBranch alsantam_manged2 -Debug
