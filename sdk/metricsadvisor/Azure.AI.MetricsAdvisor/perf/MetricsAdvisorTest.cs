549f38b9e24ba22999e66ff63d4759cda17f43d4
Caio Saldanha <camaiaor@microsoft.com>
2021-02-16 07:39:55 -0800

    Added perf test base + single test

---
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/perf/MetricsAdvisorTest.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Test.Perf;
+
+namespace Azure.AI.MetricsAdvisor
+{
+    public abstract class MetricsAdvisorTest<TOptions> : PerfTest<TOptions> where TOptions : PerfOptions
+    {
+        public MetricsAdvisorTest(TOptions options) : base(options)
+        {
+        }
+
+        protected string MetricsAdvisorUri => $"https://{MetricsAdvisorAccountName}.cognitiveservices.azure.com";
+
+        protected string MetricsAdvisorSubscriptionKey => GetEnvironmentVariable("METRICSADVISOR_SUBSCRIPTION_KEY");
+
+        protected string MetricsAdvisorApiKey => GetEnvironmentVariable("METRICSADVISOR_PRIMARY_API_KEY");
+
+        protected string DataFeedId => GetEnvironmentVariable("METRICSADVISOR_DATA_FEED_ID");
+
+        protected DateTimeOffset SamplingStartTime => DateTimeOffset.Parse("2020-10-01T00:00:00Z");
+
+        protected DateTimeOffset SamplingEndTime => DateTimeOffset.Parse("2020-10-31T00:00:00Z");
+
+        private string MetricsAdvisorAccountName => GetEnvironmentVariable("METRICSADVISOR_ACCOUNT_NAME");
+    }
+}
---
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/perf/Tests/CheckIngestionStatusTest.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.AI.MetricsAdvisor.Administration;
+using Azure.Test.Perf;
+
+namespace Azure.AI.MetricsAdvisor.Perf
+{
+    public class CheckIngestionStatusTest : MetricsAdvisorTest<PerfOptions>
+    {
+        private readonly MetricsAdvisorAdministrationClient _adminClient;
+
+        private readonly GetDataFeedIngestionStatusesOptions _requestOptions;
+
+        public CheckIngestionStatusTest(PerfOptions options) : base(options)
+        {
+            var uri = new Uri(MetricsAdvisorUri);
+            var credential = new MetricsAdvisorKeyCredential(MetricsAdvisorSubscriptionKey, MetricsAdvisorApiKey);
+
+            _adminClient = new MetricsAdvisorAdministrationClient(uri, credential);
+            _requestOptions = new GetDataFeedIngestionStatusesOptions(SamplingStartTime, SamplingEndTime);
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+            foreach (var status in _adminClient.GetDataFeedIngestionStatuses(DataFeedId, _requestOptions))
+            {
+            }
+        }
+
+        public override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            await foreach (var status in _adminClient.GetDataFeedIngestionStatusesAsync(DataFeedId, _requestOptions))
+            {
+            }
+        }
+    }
+}
