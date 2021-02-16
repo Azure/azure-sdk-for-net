94598a672b36752025c5720d8d89a25c871dd627
Sunny Patel <sunpatel@microsoft.com>
2021-02-16 17:36:42 -0500

    updated the autorest version to latest.

--- sdk/orbital/src/generate.ps1
+++ sdk/orbital/src/generate.ps1
@@ -1,4 +1,4 @@
 # Swagger 
 ## https://github.com/Azure/azure-rest-api-specs-pr/tree/orbitalAPIPhase1/specification/orbital/resource-manager/Microsoft.Orbital/preview/2020-09-01-preview
-Start-AutoRestCodeGeneration -ResourceProvider "orbital/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "orbital/resource-manager" -AutoRestVersion "latest"
 
