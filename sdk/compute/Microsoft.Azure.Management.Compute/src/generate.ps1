f059330b3f42d17e73e1fa1b8e3be95cef19d209
Rob Ip <roip@microsoft.com>
2021-02-17 10:15:57 -0800

    update generate.ps1

--- sdk/compute/Microsoft.Azure.Management.Compute/src/generate.ps1
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "compute/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "compute/resource-manager" -AutoRestVersion "v2"  -SpecsRepoFork "roipmsft/azure-rest-api-specs" -SpecsRepoBranch "dev/roip/RestorePoints"
