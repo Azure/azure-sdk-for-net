52801d52baa06d6d5b485ae8978b582d40bcc586
Reza Jooyandeh <rejooyan@microsoft.com>
2021-02-17 10:39:39 -0800

    Onboard the Sms package tests

--- sdk/communication/Azure.Communication.Common/tests/CommunicationRecordedTestSanitizer.cs
+++ sdk/communication/Azure.Communication.Common/tests/CommunicationRecordedTestSanitizer.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
+using Azure.Communication.Tests;
 using Azure.Core;
 using Azure.Core.TestFramework;
 
@@ -10,10 +10,6 @@ namespace Azure.Communication.Pipeline
 {
     public class CommunicationRecordedTestSanitizer : RecordedTestSanitizer
     {
-        private static readonly Regex _azureResourceRegEx = new Regex(@"[^/]+?(?=(.communication.azure))", RegexOptions.Compiled);
-
-        public const string ConnectionStringEnvironmentVariableName = "COMMUNICATION_CONNECTION_STRING";
-
         protected virtual void SanitizeAuthenticationHeader(IDictionary<string, string[]> headers)
         {
             if (headers.ContainsKey(HttpHeader.Names.Authorization))
@@ -30,26 +26,19 @@ namespace Azure.Communication.Pipeline
         public override string SanitizeVariable(string variableName, string environmentVariableValue)
             => variableName switch
             {
-                ConnectionStringEnvironmentVariableName => SanitizeConnectionString(environmentVariableValue),
+                CommunicationTestEnvironment.ConnectionStringEnvironmentVariableName => SanitizeConnectionString(environmentVariableValue),
                 _ => base.SanitizeVariable(variableName, environmentVariableValue)
             };
-
-        private static string SanitizeAzureResource(string uri) => _azureResourceRegEx.Replace(uri, SanitizeValue.ToLower());
 
         private static string SanitizeConnectionString(string connectionString)
         {
             const string accessKey = "accesskey";
-            const string endpoint = "endpoint";
             const string someBase64String = "Kg==";
 
             var parsed = ConnectionString.Parse(connectionString, allowEmptyValues: true);
             parsed.Replace(accessKey, someBase64String);
-            parsed.Replace(endpoint, SanitizeAzureResource(parsed.GetRequired(endpoint)));
 
             return parsed.ToString();
         }
-
-        public override string SanitizeUri(string uri)
-            => base.SanitizeUri(SanitizeAzureResource(uri));
     }
 }
--- sdk/communication/Azure.Communication.Common/tests/CommunicationTestEnvironment.cs
+++ sdk/communication/Azure.Communication.Common/tests/CommunicationTestEnvironment.cs
@@ -2,14 +2,15 @@
 // Licensed under the MIT License.
 
 using System;
-using Azure.Communication.Pipeline;
 using Azure.Core.TestFramework;
 
 namespace Azure.Communication.Tests
 {
     public class CommunicationTestEnvironment : TestEnvironment
     {
-        public string ConnectionString => GetRecordedVariable(CommunicationRecordedTestSanitizer.ConnectionStringEnvironmentVariableName);
+        public const string ConnectionStringEnvironmentVariableName = "COMMUNICATION_CONNECTION_STRING";
+
+        public string ConnectionString => GetRecordedVariable(ConnectionStringEnvironmentVariableName);
 
         public Uri Endpoint => new Uri(Core.ConnectionString.Parse(ConnectionString).GetRequired("endpoint"));
 
