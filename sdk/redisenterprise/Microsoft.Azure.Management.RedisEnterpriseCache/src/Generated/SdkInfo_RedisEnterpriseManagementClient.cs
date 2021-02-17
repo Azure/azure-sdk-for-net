b844a6c61233241eb02551dd6baf15bdf9876ee9
Andrew Green <andrewgreen@microsoft.com>
2021-02-17 10:28:24 -0800

    Include code generation metadata section

--- sdk/redisenterprise/Microsoft.Azure.Management.RedisEnterpriseCache/src/Generated/SdkInfo_RedisEnterpriseManagementClient.cs
+++ sdk/redisenterprise/Microsoft.Azure.Management.RedisEnterpriseCache/src/Generated/SdkInfo_RedisEnterpriseManagementClient.cs
@@ -28,5 +28,16 @@ namespace Microsoft.Azure.Management.RedisEnterprise
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/redisenterprise/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\azure-sdk-for-net\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "202d83c0febf4c8ed7229f6f1c6edfcfe635c36c";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
