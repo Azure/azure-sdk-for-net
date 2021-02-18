138f8e05ef3aa6fec027471861d093dfbb51cc9e
Akash Keshari <akkeshar@microsoft.com>
2021-02-18 14:42:57 +0530

    added assembly info file

--- sdk/hybridkubernetes/Microsoft.Azure.Management.Kubernetes/src/Generated/SdkInfo_ConnectedKubernetesClient.cs
+++ sdk/hybridkubernetes/Microsoft.Azure.Management.Kubernetes/src/Generated/SdkInfo_ConnectedKubernetesClient.cs
@@ -24,5 +24,16 @@ namespace Microsoft.Kubernetes
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/hybridkubernetes/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\Repos\\dotnet_sdk\\azure-sdk-for-net\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "718275ac630059660cc0e9cdd0e62bffa8e3c16d";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
