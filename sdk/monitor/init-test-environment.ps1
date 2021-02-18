6d24f6bf6c706a01d83c6b880712de1eef480e56
Timothy Mothra Lee <tilee@microsoft.com>
2021-02-17 16:10:59 -0800

    fix

--- sdk/monitor/init-test-environment.ps1
+++ sdk/monitor/init-test-environment.ps1
@@ -16,4 +16,4 @@ Connect-AzAccount -Subscription $Subscription
 $RootDirectory = (get-item $PSScriptRoot).parent.parent
 $TestResourcesDirectory = Join-Path -Path $RootDirectory -ChildPath eng\common\TestResources
 
-& $TestResourcesDirectory\New-TestResources.ps1 -ServiceDirectory monitor -Subscription $Subscriptiony
+& $TestResourcesDirectory\New-TestResources.ps1 -ServiceDirectory monitor -Subscription $Subscription
