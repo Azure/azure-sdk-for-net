7261bc1138867adfd7d84a256f6101ea8fa8306a
Jacob Stenzel <jastenze@microsoft.com>
2021-02-17 23:12:53 -0800

    whitespace

--- sdk/mixedreality/Microsoft.Azure.Management.MixedReality/src/generate.ps1
+++ sdk/mixedreality/Microsoft.Azure.Management.MixedReality/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "mixedreality/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider  "mixedreality/resource-manager" -AutoRestVersion "v2"
