0447defc7a89a0c66a410880c7c0c6c49d0ca9c1
Adam Sandor <adsandor@microsoft.com>
2021-02-18 16:49:25 -0500

    Create IVirtualMachineScaleSetVMsOperations.cs
    customizations

---
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Customizations/IVirtualMachineScaleSetVMsOperations.cs
@@ -0,0 +1,23 @@
+namespace Microsoft.Azure.Management.Compute
+{
+    using Microsoft.Rest;
+    using Microsoft.Rest.Azure;
+    using Microsoft.Rest.Azure.OData;
+    using Models;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// VirtualMachineScaleSetVMsOperations operations.
+    /// </summary>
+    public partial interface IVirtualMachineScaleSetVMsOperations
+    {
+    
+        Task<AzureOperationResponse> DeleteWithHttpMessagesAsync(string resourceGroupName, string vmScaleSetName, string instanceId, Dictionary<string, List<string>> customHeaders, CancellationToken cancellationToken);
+        
+        Task<AzureOperationResponse> DeleteWithHttpMessagesAsync(string resourceGroupName, string vmScaleSetName, string instanceId, Dictionary<string, List<string>> customHeaders);
+    
+    }
+}
