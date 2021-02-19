fdd39a8282a1ee3e2be9cca8bee38bc7d68c76f1
Timothy Mothra Lee <tilee@microsoft.com>
2021-02-19 11:43:11 -0800

    working implementation of dynamic wait for ingestion

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Integration.Tests/FunctionalTests/AzureMonitorLogExporterLiveTests.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Integration.Tests/FunctionalTests/AzureMonitorLogExporterLiveTests.cs
@@ -3,6 +3,8 @@
 
 namespace Azure.Monitor.OpenTelemetry.Exporter.Integration.Tests.FunctionalTests
 {
+    using System;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using global::Azure.Core.TestFramework;
@@ -53,14 +55,63 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Integration.Tests.FunctionalTests
             // TODO: MAYBE WE COULD HAVE A SHORT WAIT, AND IF NO TELEMETRY IS FOUND, WAIT FOR A LONGER PERIOD.
             // THIS MIGHT MAKE TEST RUN FASTER, BUT PROVIDE A TRY-AGAIN MECHANIC FOR DAYS THAT INGESTION IS HAVING A BAD DAY.
             // DON'T KNOW IF THIS WOULD BE COMPATIBLE WITH THE RECORDED TESTS
-            await this.WaitForIgnestionAsync();
+            //await this.WaitForIgnestionAsync();
 
-            // VERIFY
-            var client = await this.GetApplicationInsightsDataClientAsync();
+            //// VERIFY
+            //var client = await this.GetApplicationInsightsDataClientAsync();
 
-            var telemetry = await client.Events.GetTraceEventsAsync(appId: TestEnvironment.ApplicationId, timespan: QueryDuration.TenMinutes);
-            Assert.AreEqual(testMessage, telemetry.Value[0].Trace.Message);
-            Assert.AreEqual(TestEnvironment.InstrumentationKey, telemetry.Value[0].Ai.IKey);
+            //var telemetry = await client.Events.GetTraceEventsAsync(appId: TestEnvironment.ApplicationId, timespan: QueryDuration.TenMinutes);
+            //Assert.AreEqual(testMessage, telemetry.Value[0].Trace.Message);
+            //Assert.AreEqual(TestEnvironment.InstrumentationKey, telemetry.Value[0].Ai.IKey);
+
+            var telemetry = await GetTelemetry();
+            Assert.AreEqual(testMessage, telemetry[0].Trace.Message);
+            Assert.AreEqual(TestEnvironment.InstrumentationKey, telemetry[0].Ai.IKey);
+        }
+
+        private async Task<System.Collections.Generic.IList<Microsoft.Azure.ApplicationInsights.Query.Models.EventsTraceResult>> GetTelemetry()
+        {
+            var timeoutDuration = TimeSpan.FromMinutes(5); // timeout after 5 minutes.
+            //var period = TimeSpan.FromSeconds(30); // query once every 30 seconds.
+            var period = GetWaitPeriod(); // query once every 30 seconds.
+
+            System.Collections.Generic.IList<Microsoft.Azure.ApplicationInsights.Query.Models.EventsTraceResult> telemetry = null;
+
+            for (double actualDuration = 0; actualDuration < timeoutDuration.TotalMilliseconds; actualDuration += period.TotalMilliseconds)
+            {
+                await Task.Delay(period);
+
+                var client = await this.GetApplicationInsightsDataClientAsync();
+
+                var queryResult = await client.Events.GetTraceEventsAsync(appId: TestEnvironment.ApplicationId, timespan: QueryDuration.TenMinutes);
+
+                if (queryResult.Value.Any())
+                {
+                    telemetry = queryResult.Value;
+                    break;
+                }
+            }
+
+            if (telemetry == null)
+            {
+                Assert.Inconclusive("Failed to query telemetry from Kusto. This is not necessarily a test failure, this could be a result of an ingestion delay.");
+            }
+
+            return telemetry;
+        }
+
+        private TimeSpan GetWaitPeriod()
+        {
+            switch (this.Mode)
+            {
+                case RecordedTestMode.Live:
+                case RecordedTestMode.Record:
+                    return TimeSpan.FromSeconds(30);
+                case RecordedTestMode.Playback:
+                    return TimeSpan.FromSeconds(0);
+                default:
+                    throw new Exception($"Unknown RecordedTestMode '{this.Mode}'");
+            }
         }
     }
 }
