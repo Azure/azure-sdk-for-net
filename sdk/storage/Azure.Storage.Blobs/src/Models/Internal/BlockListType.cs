1814567d1f6cfb4905539e71d54377ab46ed6737
Sean McCullough <44180881+seanmcc-msft@users.noreply.github.com>
2021-02-19 11:51:16 -0600

    Migrated Azure.Storage.Blobs to new generator. (#18830)

---
+++ sdk/storage/Azure.Storage.Blobs/src/Models/Internal/BlockListType.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Storage.Blobs.Models
+{
+    /// <summary>
+    /// The BlockListType.
+    /// </summary>
+    internal enum BlockListType
+    {
+        /// <summary>
+        /// committed.
+        /// </summary>
+        Committed,
+
+        /// <summary>
+        /// uncommitted.
+        /// </summary>
+        Uncommitted,
+
+        ///<summary>
+        ///all.
+        ///</summary>
+        All
+    }
+}
