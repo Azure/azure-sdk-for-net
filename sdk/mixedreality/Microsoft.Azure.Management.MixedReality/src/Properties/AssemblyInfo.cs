4dac925a2000cc1fb2b5f2411cebd232ca27ee53
Vicente Rivera <virivera@microsoft.com>
2021-02-22 12:42:40 -0800

    Moving version back in the AssemblyInfo file

--- sdk/mixedreality/Microsoft.Azure.Management.MixedReality/src/Properties/AssemblyInfo.cs
+++ sdk/mixedreality/Microsoft.Azure.Management.MixedReality/src/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
