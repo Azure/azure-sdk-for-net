120169e74c8cadfb638cd71adccf7597a39128fe
Jacob Stenzel <jastenze@microsoft.com>
2021-02-22 15:46:46 -0800

    Conditional Content-Length check based on SDK version

--- sdk/objectanchors/Azure.MixedReality.ObjectAnchors/tests/MixedRealityRecordMatcher.cs
+++ sdk/objectanchors/Azure.MixedReality.ObjectAnchors/tests/MixedRealityRecordMatcher.cs
@@ -8,18 +8,29 @@ namespace Azure.MixedReality.ObjectAnchors.Tests
     public class MixedRealityRecordMatcher : RecordMatcher
     {
         private const string ClientCorrelationVectorHeaderName = "X-MRC-CV";
+        private const string ContentLengthHeaderName = "Content-Length";
 
-        public MixedRealityRecordMatcher() : base(
-#if NETCOREAPP2_1
-            compareBodies: true
-#else
-            compareBodies: false
-#endif
-            )
+        public MixedRealityRecordMatcher()
+            : base(compareBodies: ShouldValidateBodies())
         {
             IgnoredHeaders.Add(ClientCorrelationVectorHeaderName);
             VolatileHeaders.Add(ClientCorrelationVectorHeaderName);
             VolatileResponseHeaders.Add(ClientCorrelationVectorHeaderName);
+            if (!ShouldValidateBodies())
+            {
+                IgnoredHeaders.Add(ContentLengthHeaderName);
+                VolatileHeaders.Add(ContentLengthHeaderName);
+                VolatileResponseHeaders.Add(ContentLengthHeaderName);
+            }
+        }
+
+        public static bool ShouldValidateBodies()
+        {
+#if NETCOREAPP2_1
+            return true;
+#else
+            return false;
+#endif
         }
     }
 }
