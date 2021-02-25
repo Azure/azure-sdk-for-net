fc17340c335d2140078c8b0e990e1aa18df7dcb8
m-nash <64171366+m-nash@users.noreply.github.com>
2021-02-25 11:20:48 -0800

    update to require linq 4.0.0 so that don't have to upgrade bcl

--- sdk/resourcemanager/Proto.Client/src/Program.cs
+++ sdk/resourcemanager/Proto.Client/src/Program.cs
@@ -10,7 +10,7 @@ namespace Proto.Client
             Scenario scenario = null;
             try
             {
-                scenario = ScenarioFactory.GetScenario(Scenarios.CreateSingleVmExample);
+                scenario = ScenarioFactory.GetScenario(Scenarios.CreateSingleVMCheckLocation);
                 scenario.Execute();
             }
             finally
