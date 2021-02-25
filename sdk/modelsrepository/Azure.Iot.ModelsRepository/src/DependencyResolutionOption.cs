733e79ab20ae06b8e6572d90fb0752750830ba46
Azad Abbasi <azabbasi@microsoft.com>
2021-02-24 16:15:30 -0800

    (modelsRepo): Initial commit: Azure.Iot.ModelsRepository  (#18998)
    * Initial Commit - Bring ModelRepository Project to the mono repo (#18706)
    * Use resx file and update formattings.
    * Add client diagnostics - Add sync APIs to RemoteFetcher and LocalFetcher (#18807)
    * Add async methods to the ResolverClient (#18814)
    * Add support for recorded tests. (#18836)
    * Add support for recorded tests.
    * Make strings constants
    * Update file names to include plural models
    * Refactor RepositoryHandler and ResolverClient (#18857)
    * Refactor ModelQuery (#18843)
    * Refactor ModelQuery.
    * Combine constant files.
    * Rename Client to ModelsRepoClient (#18862)
    * Remove ResolverException in favor of RequestFailedException (#18874)
    * Remove ResolverException in favor of RequestFailedException. Adds more docstrings.
    * Rename Repo to Repository and minor changes / Import samples project (#18986)
    * Format changes/Remove resx file.
    * Add first draft of readme. Add comment for dtmi validation. (#18987)
    * Add comment for dtmi validation.
    * Fix links.
    * Add diagnostic event info.
    * Source link specifically pointing to 'src' folder.
    * Use fluent assertion
    * Use playback as default
    * fix Links and root service directory casing
    Co-authored-by: Paymaun <Paymaunh@microsoft.com>

---
+++ sdk/modelsrepository/Azure.Iot.ModelsRepository/src/DependencyResolutionOption.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Iot.ModelsRepository
+{
+    /// <summary>
+    /// The dependency processing options.
+    /// </summary>
+    public enum DependencyResolutionOption
+    {
+        /// <summary>
+        /// Do not process external dependencies.
+        /// </summary>
+        Disabled,
+
+        /// <summary>
+        /// Enable external dependencies.
+        /// </summary>
+        Enabled,
+
+        /// <summary>
+        /// Try to get external dependencies using .expanded.json.
+        /// </summary>
+        TryFromExpanded
+    }
+}
