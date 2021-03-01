4087cb02af777835028a0e11a8bf33091f0ac5fd
Adam Sandor <adsandor@microsoft.com>
2021-03-01 16:33:38 -0500

    Update DataDisk.cs

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/DataDisk.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/DataDisk.cs
@@ -199,25 +199,6 @@ namespace Microsoft.Azure.Management.Compute.Models
         public bool? ToBeDetached { get; set; }
 
         /// <summary>
-        /// Gets or sets specifies the detach behavior to be used while
-        /// detaching a disk or which is already in the process of detachment
-        /// from the virtual machine. Supported values: **ForceDetach**.
-        /// &amp;lt;br&amp;gt;&amp;lt;br&amp;gt; detachOption: **ForceDetach**
-        /// is applicable only for managed data disks. If a previous detachment
-        /// attempt of the data disk did not complete due to an unexpected
-        /// failure from the virtual machine and the disk is still not released
-        /// then use force-detach as a last resort option to detach the disk
-        /// forcibly from the VM. All writes might not have been flushed when
-        /// using this detach behavior. &amp;lt;br&amp;gt;&amp;lt;br&amp;gt;
-        /// This feature is still in preview mode and is not supported for
-        /// VirtualMachineScaleSet. To force-detach a data disk update
-        /// toBeDetached to 'true' along with setting detachOption:
-        /// 'ForceDetach'. Possible values include: 'ForceDetach'
-        /// </summary>
-        [JsonProperty(PropertyName = "detachOption")]
-        public string DetachOption { get; set; }
-
-        /// <summary>
         /// Gets specifies the Read-Write IOPS for the managed disk when
         /// StorageAccountType is UltraSSD_LRS. Returned only for
         /// VirtualMachine ScaleSet VM disks. Can be updated only via updates
