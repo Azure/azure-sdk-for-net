5bea9fdfd8caa74f4b54ba17dfd23fcfca8f1a2f
Pavel Krymets <pavel@krymets.com>
2021-03-01 15:07:36 -0800

    Add client and model readmes and fix partition context

---
+++ sdk/eventhub/Microsoft.Azure.WebJobs.Extensions.EventHubs/tests/Samples/TriggerSingleModel.cs
@@ -0,0 +1,27 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Extensions.EventHubs.Tests.Samples
+{
+    public static class TriggerSingleModel
+    {
+        public class Dog
+        {
+            public string Name { get; set; }
+            public string Breed { get; set; }
+            public int Age { get; set; }
+        }
+
+        #region Snippet:TriggerSingleModel
+        [FunctionName("TriggerSingleModel")]
+        public static void Run(
+            [EventHubTrigger("<event_hub_name>", Connection = "<connection_name>")] Dog dog,
+            ILogger logger)
+        {
+            logger.LogInformation($"Who's a good dog? {dog.Name} is!");
+        }
+        #endregion
+    }
+}
