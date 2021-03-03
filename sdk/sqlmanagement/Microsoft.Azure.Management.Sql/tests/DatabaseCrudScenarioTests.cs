66c7d5aa9ce0297db12c846952b4882c4762a0c8
xaliciayang <59986952+xaliciayang@users.noreply.github.com>
2021-03-03 13:51:26 -0800

    Add tests for new Copy/Update-LongTermRetentionBackup (#19040)
    * update SDK for Databases and LTR APIs
    
    * update tests to use new value from Databases API (RequestedBackupStorageRedundancy), add tests for LTR Backup Copy/Update
    
    * Add LTR test recordings
    
    * skip LTR tests
    
    * add updated database test sessions
    
    * mgmt metadata; update version in project

--- sdk/sqlmanagement/Microsoft.Azure.Management.Sql/tests/DatabaseCrudScenarioTests.cs
+++ sdk/sqlmanagement/Microsoft.Azure.Management.Sql/tests/DatabaseCrudScenarioTests.cs
@@ -55,7 +55,7 @@ namespace Sql.Tests
                     Tags = tags,
                     CreateMode = "Default",
                     SampleName = SampleName.AdventureWorksLT,
-                    StorageAccountType = "GRS",
+                    RequestedBackupStorageRedundancy = "Geo",
                 };
                 var db2 = sqlClient.Databases.CreateOrUpdate(resourceGroup.Name, server.Name, dbName, db2Input);
                 Assert.NotNull(db2);
@@ -117,7 +117,7 @@ namespace Sql.Tests
                 var db8Input = new Database()
                 {
                     Location = server.Location,
-                    StorageAccountType = "GRS",
+                    RequestedBackupStorageRedundancy = "Geo",
                 };
                 var db8 = sqlClient.Databases.CreateOrUpdate(resourceGroup.Name, server.Name, dbName, db8Input);
                 Assert.NotNull(db8);
