edf8a407a1770f5a09ec053970d5384a17afb3cc
m-nash <64171366+m-nash@users.noreply.github.com>
2021-03-04 16:54:13 -0800

    merge in armbuildertest and update to use new framework

---
+++ sdk/resourcemanager/Azure.ResourceManager.Core/tests/Scenario/ArmBuilderTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Core.Tests
+{
+    public class ArmBuilderTests : ResourceManagerTestBase
+    {
+        private AzureResourceManagerClient _client;
+
+        public ArmBuilderTests(bool isAsync)
+            : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _client = GetArmClient();
+        }
+
+        [TestCase(null)]
+        [TestCase("  ")]
+        [RecordedTest]
+        public void TestCreateOrUpdate(string value)
+        {
+            Assert.ThrowsAsync<ArgumentException>(async delegate { await _client.DefaultSubscription.GetResourceGroupContainer().Construct(LocationData.WestUS2).CreateOrUpdateAsync(value); });
+        }
+
+        [TestCase(null)]
+        [TestCase("    ")]
+        [RecordedTest]
+        public void TestStartCreateOrUpdate(string value)
+        {
+            Assert.ThrowsAsync<ArgumentException>(async delegate { await _client.DefaultSubscription.GetResourceGroupContainer().Construct(LocationData.WestUS2).StartCreateOrUpdateAsync(value); });
+        }
+    }
+}
