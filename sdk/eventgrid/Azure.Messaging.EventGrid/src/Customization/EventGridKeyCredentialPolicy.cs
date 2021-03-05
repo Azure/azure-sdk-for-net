7389749113973161ff6ab7738afa294635818c7b
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-03-05 15:49:48 -0800

    PR FB

--- sdk/eventgrid/Azure.Messaging.EventGrid/src/Customization/EventGridKeyCredentialPolicy.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid/src/Customization/EventGridKeyCredentialPolicy.cs
@@ -2,16 +2,20 @@
 // Licensed under the MIT License.
 
 using Azure.Core;
+using Azure.Core.Pipeline;
 
 namespace Azure.Messaging.EventGrid
 {
-    internal class EventGridKeyCredentialPolicy : AzureKeyCredentialPolicy
+    internal class EventGridKeyCredentialPolicy : HttpPipelineSynchronousPolicy
     {
         private readonly string _name;
         private readonly AzureKeyCredential _credential;
+        public const string SystemPublisherKey = "AzureSystemPublisher";
 
-        public EventGridKeyCredentialPolicy(AzureKeyCredential credential, string name) : base(credential, name)
+        public EventGridKeyCredentialPolicy(AzureKeyCredential credential, string name)
         {
+            Argument.AssertNotNull(credential, nameof(credential));
+            Argument.AssertNotNullOrEmpty(name, nameof(name));
             _credential = credential;
             _name = name;
         }
@@ -23,7 +27,7 @@ namespace Azure.Messaging.EventGrid
             // AzureSystemPublisher is the key that internal partners will use
             // when authenticating with a certificate. So we don't need to include the key
             // in the request in this case.
-            if (_credential.Key != "AzureSystemPublisher")
+            if (_credential.Key != SystemPublisherKey)
             {
                 message.Request.Headers.SetValue(_name, _credential.Key);
             }
