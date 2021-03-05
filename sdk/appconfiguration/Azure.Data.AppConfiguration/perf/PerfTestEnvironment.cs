e9171bf4c841615c1baeadfb4d6e14257904095d
Pavel Krymets <pavel@krymets.com>
2021-03-05 13:27:57 -0800

    Add AppConfiguration perf test (#19270)

---
+++ sdk/appconfiguration/Azure.Data.AppConfiguration/perf/PerfTestEnvironment.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.Data.AppConfiguration.Perf
+{
+    public sealed class PerfTestEnvironment : TestEnvironment
+    {
+        public string ConnectionString => GetVariable("APPCONFIGURATION_CONNECTION_STRING");
+
+        public static PerfTestEnvironment Instance { get; } = new ();
+    }
+}
