12512eedbea03a19ed185f633e778887e5e86dba
NourEldin Yasser <noursalem95@gmail.com>
2021-03-08 21:48:43 +0200

    [documenttranlsation] make generated models internal by default

--- sdk/documenttranslation/Azure.AI.DocumentTranslation/src/Models/DocumentFilter.cs
+++ sdk/documenttranslation/Azure.AI.DocumentTranslation/src/Models/DocumentFilter.cs
@@ -5,8 +5,8 @@ using Azure.Core;
 
 namespace Azure.AI.DocumentTranslation.Models
 {
-    [CodeGenModel("StatusSummary")]
-    internal partial class StatusSummary
+    [CodeGenModel("DocumentFilter")]
+    public partial class DocumentFilter
     {
     }
 }
--- sdk/documenttranslation/Azure.AI.DocumentTranslation/src/Models/StorageSource.cs
+++ sdk/documenttranslation/Azure.AI.DocumentTranslation/src/Models/StorageSource.cs
@@ -5,8 +5,8 @@ using Azure.Core;
 
 namespace Azure.AI.DocumentTranslation.Models
 {
-    [CodeGenModel("BatchStatusResponse")]
-    internal partial class BatchStatusResponse
+    [CodeGenModel("StorageSource")]
+    public partial struct StorageSource
     {
     }
 }
