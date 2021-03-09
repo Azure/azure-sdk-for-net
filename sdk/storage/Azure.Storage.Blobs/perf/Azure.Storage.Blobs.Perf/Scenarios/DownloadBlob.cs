de522d92a5366b7d0d2d7df9cfbd99fff3373833
Mike Harder <mharder@microsoft.com>
2021-03-08 17:41:42 -0800

    [Perf] Use retryable APIs in Storage test setup (#19327)
    - Prevents errors if client needs to retry a request that has already been accepted by the service

--- sdk/storage/Azure.Storage.Blobs/perf/Azure.Storage.Blobs.Perf/Scenarios/DownloadBlob.cs
+++ sdk/storage/Azure.Storage.Blobs/perf/Azure.Storage.Blobs.Perf/Scenarios/DownloadBlob.cs
@@ -28,7 +28,7 @@ namespace Azure.Storage.Blobs.Perf.Scenarios
             using Stream stream = RandomStream.Create(Options.Size);
 
             // No need to delete file in GlobalCleanup(), since ContainerTest.GlobalCleanup() deletes the whole container
-            await _blobClient.UploadAsync(stream);
+            await _blobClient.UploadAsync(stream, overwrite: true);
         }
 
         public override void Run(CancellationToken cancellationToken)
