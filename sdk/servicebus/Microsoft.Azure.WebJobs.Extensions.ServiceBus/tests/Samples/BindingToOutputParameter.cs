3733b8dd59dc3ffaa379cc63328edf6cfbcd392f
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-03-10 12:41:11 -0800

    Add README for ServiceBus functions extensions (#19415)
    * Add README for ServiceBus functions extensions
    
    * Remove locale from README

---
+++ sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/tests/Samples/BindingToOutputParameter.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Messaging.ServiceBus;
+using System;
+
+namespace Microsoft.Azure.WebJobs.Extensions.ServiceBus.Tests.Samples
+{
+    public static class BindingToOutputParameter
+    {
+        #region Snippet:ServiceBusBindingToOutputParameter
+        [FunctionName("BindingToOutputParameter")]
+        public static void Run(
+        [TimerTrigger("0 */5 * * * *")] TimerInfo myTimer,
+        [ServiceBus("<queue_or_topic_name>", Connection = "<connection_name>")] out ServiceBusMessage message)
+        {
+            message = new ServiceBusMessage($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+        #endregion
+    }
+}
