5f2fd3d11dcb3139a8f4d9dd7b2d9bb62ac1b9d3
Anne Loomis Thompson <annelo@microsoft.com>
2021-03-10 15:21:13 -0800

    added test-resources.json and basic auth policy.

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/src/ContainerRegistryClient.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/ContainerRegistryClient.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Specialized;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Web;
 using Azure.Core;
 using Azure.Core.Pipeline;
@@ -14,41 +13,83 @@ namespace Azure.Containers.ContainerRegistry
     /// <summary> The Repository service client. </summary>
     public partial class ContainerRegistryClient
     {
-        private readonly ClientDiagnostics _clientDiagnostics;
+        private readonly Uri _endpoint;
         private readonly HttpPipeline _pipeline;
-        internal RepositoryRestClient RestClient { get; }
+        private readonly ClientDiagnostics _clientDiagnostics;
+        private readonly RepositoryRestClient _restClient;
+
+        /// <summary>
+        /// <paramref name="endpoint"/>
+        /// </summary>
+        public ContainerRegistryClient(Uri endpoint) : this(endpoint, new ContainerRegistryClientOptions())
+        {
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="options"></param>
+        public ContainerRegistryClient(Uri endpoint, ContainerRegistryClientOptions options)
+        {
+            // The HttpPipelineBuilder.Build method, builds up a pipeline with client options, and any number of additional policies.
+            _pipeline = HttpPipelineBuilder.Build(options, new BasicAuthenticationPolicy());
+
+            _clientDiagnostics = new ClientDiagnostics(options);
+
+            _endpoint = endpoint;
+
+            _restClient = new RepositoryRestClient(_clientDiagnostics, _pipeline, _endpoint.AbsoluteUri);
+        }
 
         /// <summary> Initializes a new instance of RepositoryClient for mocking. </summary>
         protected ContainerRegistryClient()
         {
         }
 
-        /// <summary> Initializes a new instance of RepositoryClient. </summary>
-        /// <param name="clientDiagnostics"> The handler for diagnostic messaging in the client. </param>
-        /// <param name="pipeline"> The HTTP pipeline for sending and receiving REST requests and responses. </param>
-        /// <param name="url"> Registry login URL. </param>
-        internal ContainerRegistryClient(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string url)
-        {
-            RestClient = new RepositoryRestClient(clientDiagnostics, pipeline, url);
-            _clientDiagnostics = clientDiagnostics;
-            _pipeline = pipeline;
-        }
+        ///// <summary> Initializes a new instance of RepositoryClient. </summary>
+        ///// <param name="clientDiagnostics"> The handler for diagnostic messaging in the client. </param>
+        ///// <param name="pipeline"> The HTTP pipeline for sending and receiving REST requests and responses. </param>
+        ///// <param name="url"> Registry login URL. </param>
+        //internal ContainerRegistryClient(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string url)
+        //{
+        //    _restClient = new RepositoryRestClient(clientDiagnostics, pipeline, url);
+        //    _clientDiagnostics = clientDiagnostics;
+        //    _pipeline = pipeline;
+        //}
 
         /// <summary> List repositories. </summary>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        public virtual async AsyncPageable<string> GetRepositoriesAsync(CancellationToken cancellationToken = default)
+        public virtual AsyncPageable<string> GetRepositoriesAsync(CancellationToken cancellationToken = default)
         {
-            using var scope = _clientDiagnostics.CreateScope("ContainerRegistryClient.GetRepositories");
-            scope.Start();
-            try
+            return PageResponseEnumerator.CreateAsyncEnumerable(async (continuationToken, pageSizeHint) =>
             {
-                return await RestClient.GetListAsync(last, n, cancellationToken).ConfigureAwait(false);
-            }
-            catch (Exception e)
-            {
-                scope.Failed(e);
-                throw;
-            }
+                using DiagnosticScope scope = _clientDiagnostics.CreateScope($"{nameof(ContainerRegistryClient)}.{nameof(GetRepositories)}");
+                scope.Start();
+                try
+                {
+                    ResponseWithHeaders<Repositories, RepositoryGetListHeaders> response =
+                        await _restClient.GetListAsync(
+                            continuationToken,
+                            pageSizeHint,
+                            cancellationToken)
+                       .ConfigureAwait(false);
+
+                    string lastRepository = null;
+                    if (!String.IsNullOrEmpty(response.Headers.Link))
+                    {
+                        Uri nextLink = new Uri(response.Headers.Link);
+                        NameValueCollection queryParams = HttpUtility.ParseQueryString(nextLink.Query);
+                        lastRepository = queryParams["last"];
+                    }
+
+                    return Page<string>.FromValues(response.Value.Names, lastRepository, response.GetRawResponse());
+                }
+                catch (Exception ex)
+                {
+                    scope.Failed(ex);
+                    throw;
+                }
+            });
         }
 
         /// <summary> List repositories. </summary>
@@ -61,7 +102,11 @@ namespace Azure.Containers.ContainerRegistry
                 scope.Start();
                 try
                 {
-                    ResponseWithHeaders<Repositories, RepositoryGetListHeaders> response = RestClient.GetList(continuationToken, pageSizeHint, cancellationToken);
+                    ResponseWithHeaders<Repositories, RepositoryGetListHeaders> response =
+                        _restClient.GetList(
+                            continuationToken,
+                            pageSizeHint,
+                            cancellationToken);
                     Uri nextLink = new Uri(response.Headers.Link);
                     NameValueCollection queryParams = HttpUtility.ParseQueryString(nextLink.Query);
                     string lastSeenRepository = queryParams["last"];
---
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Temporary/BasicAuthenticationPolicy.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace Azure.Containers.ContainerRegistry
+{
+    /// <summary>
+    /// This method of authentication is being deprecated by the service.  It is included here to unblock client development
+    /// while the feature crew works on the correct auth policies for this service.
+    /// </summary>
+    internal class BasicAuthenticationPolicy : HttpPipelinePolicy
+    {
+        public override void Process(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            ProcessAsync(message, pipeline, false).EnsureCompleted();
+        }
+
+        public override ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            return ProcessAsync(message, pipeline, true);
+        }
+
+#pragma warning disable CA1822 // Mark members as static
+        private async ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline, bool async)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            var valueBytes = Encoding.UTF8.GetBytes("<username>:<password>");
+            message.Request.Headers.SetValue(HttpHeader.Names.Authorization, $"Basic {Convert.ToBase64String(valueBytes)}");
+
+            if (async)
+            {
+                await ProcessNextAsync(message, pipeline).ConfigureAwait(false);
+            }
+            else
+            {
+                ProcessNext(message, pipeline);
+            }
+        }
+    }
+}
