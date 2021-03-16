f693a3355ca5147385d31b7486f6aa5dbe135b9c
Minnie Liu <peiliu@microsoft.com>
2021-03-16 12:04:11 -0700

    Phone Numbers Tests should use static resource

--- sdk/communication/Azure.Communication.PhoneNumbers/tests/Infrastructure/PhoneNumbersClientTestEnvironment.cs
+++ sdk/communication/Azure.Communication.PhoneNumbers/tests/Infrastructure/PhoneNumbersClientTestEnvironment.cs
@@ -1,11 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Azure.Communication.Tests;
 
 namespace Azure.Communication.PhoneNumbers.Tests
 {
     public class PhoneNumbersClientTestEnvironment : CommunicationTestEnvironment
     {
+        internal const string LiveTestConnectionStringEnvironmentVariableName = "AZURE_COMMUNICATION_LIVETEST_CONNECTION_STRING";
+
+        public string LiveTestConnectionString => GetRecordedVariable(LiveTestConnectionStringEnvironmentVariableName);
     }
 }
