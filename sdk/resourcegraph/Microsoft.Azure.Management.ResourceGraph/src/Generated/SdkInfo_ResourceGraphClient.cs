b96821f0a8a469ed79eb57824c1b87bd5dd9ed59
makharch <makharch@microsoft.com>
2021-03-17 23:30:48 -0700

    rerun generate.ps1 after merge with master

--- sdk/resourcegraph/Microsoft.Azure.Management.ResourceGraph/src/Generated/SdkInfo_ResourceGraphClient.cs
+++ sdk/resourcegraph/Microsoft.Azure.Management.ResourceGraph/src/Generated/SdkInfo_ResourceGraphClient.cs
@@ -24,5 +24,16 @@ namespace Microsoft.Azure.Management.ResourceGraph
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/resourcegraph/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\Graph\\azure-sdk-for-net\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "8d3b22458da4bb9309c0207c2ebc1903de741249";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
