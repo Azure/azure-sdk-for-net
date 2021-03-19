21f478895602627ba9db23c75d5a19842cccd0b1
Audunn Baldvinsson <audunn.baldvinsson@netapp.com>
2021-03-19 17:43:57 +0000

    apply udpates and recordings

--- sdk/netapp/Microsoft.Azure.Management.NetApp/src/Generated/Models/ActiveDirectory.cs
+++ sdk/netapp/Microsoft.Azure.Management.NetApp/src/Generated/Models/ActiveDirectory.cs
@@ -76,7 +76,10 @@ namespace Microsoft.Azure.Management.NetApp.Models
         /// without domain specifier</param>
         /// <param name="ldapOverTLS">Specifies whether or not the LDAP traffic
         /// needs to be secured via TLS.</param>
-        public ActiveDirectory(string activeDirectoryId = default(string), string username = default(string), string password = default(string), string domain = default(string), string dns = default(string), string status = default(string), string statusDetails = default(string), string smbServerName = default(string), string organizationalUnit = default(string), string site = default(string), IList<string> backupOperators = default(IList<string>), string kdcIP = default(string), string adName = default(string), string serverRootCACertificate = default(string), bool? aesEncryption = default(bool?), bool? ldapSigning = default(bool?), IList<string> securityOperators = default(IList<string>), bool? ldapOverTLS = default(bool?))
+        /// <param name="allowLocalNfsUsersWithLdap"> If enabled, NFS client
+        /// local users can also (in addition to LDAP users) access the NFS
+        /// volumes.</param>
+        public ActiveDirectory(string activeDirectoryId = default(string), string username = default(string), string password = default(string), string domain = default(string), string dns = default(string), string status = default(string), string statusDetails = default(string), string smbServerName = default(string), string organizationalUnit = default(string), string site = default(string), IList<string> backupOperators = default(IList<string>), string kdcIP = default(string), string adName = default(string), string serverRootCACertificate = default(string), bool? aesEncryption = default(bool?), bool? ldapSigning = default(bool?), IList<string> securityOperators = default(IList<string>), bool? ldapOverTLS = default(bool?), bool? allowLocalNfsUsersWithLdap = default(bool?))
         {
             ActiveDirectoryId = activeDirectoryId;
             Username = username;
@@ -96,6 +99,7 @@ namespace Microsoft.Azure.Management.NetApp.Models
             LdapSigning = ldapSigning;
             SecurityOperators = securityOperators;
             LdapOverTLS = ldapOverTLS;
+            AllowLocalNfsUsersWithLdap = allowLocalNfsUsersWithLdap;
             CustomInit();
         }
 
@@ -232,6 +236,13 @@ namespace Microsoft.Azure.Management.NetApp.Models
         /// </summary>
         [JsonProperty(PropertyName = "ldapOverTLS")]
         public bool? LdapOverTLS { get; set; }
+
+        /// <summary>
+        /// Gets or sets  If enabled, NFS client local users can also (in
+        /// addition to LDAP users) access the NFS volumes.
+        /// </summary>
+        [JsonProperty(PropertyName = "allowLocalNfsUsersWithLdap")]
+        public bool? AllowLocalNfsUsersWithLdap { get; set; }
 
         /// <summary>
         /// Validate the object.
