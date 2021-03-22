d8eae1b6f4ea616229232777572664f91deac8f5
Allen Zhang <allenzhang@live.com>
2021-03-22 01:22:10 -0700

    Updated test cases

---
+++ sdk/resourcemanager/Azure.ResourceManager.Core/src/Utils/ClientDiagnostics.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Azure.ResourceManager.Core;
+
+#nullable enable
+
+namespace Azure.Core.Pipeline
+{
+    internal sealed partial class ClientDiagnostics : DiagnosticScopeFactory
+    {
+        internal ArmException CreateArmExceptionWithContent(Response response)
+        {
+            string? content = ReadContentAsync(response, false).EnsureCompleted();
+
+            string message = string.Empty;
+            string errorCode = string.Empty;
+            IDictionary<string, string>? additionalInfo = new Dictionary<string, string>();
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            ExtractFailureContent(content, response.Headers, ref message, ref errorCode, ref additionalInfo);
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+            var exception = new ArmException(response.Status, message??string.Empty, null);
+
+            if (additionalInfo != null)
+            {
+                foreach (KeyValuePair<string, string> keyValuePair in additionalInfo)
+                {
+                    exception.Data.Add(keyValuePair.Key, keyValuePair.Value);
+                }
+            }
+
+            return exception;
+        }
+
+#pragma warning disable CA1822 // Member can be static
+        partial void ExtractFailureContent(
+#pragma warning disable CA1801 // Review unused parameters
+            string? content,
+#pragma warning restore CA1801 // Review unused parameters
+#pragma warning disable CA1801 // Review unused parameters
+            ResponseHeaders responseHeaders,
+#pragma warning restore CA1801 // Review unused parameters
+            ref string? message,
+            ref string? errorCode,
+            ref IDictionary<string, string>? additionalInfo)
+        {
+            message = "hello";
+            errorCode = "yes";
+
+            additionalInfo?.Add("Yes", "sir");
+        }
+#pragma warning restore CA1822
+    }
+}
---
+++ sdk/resourcemanager/Azure.ResourceManager.Core/tests/Unit/ArmExceptionTests.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+
+#nullable enable
+
+namespace Azure.ResourceManager.Core.Tests.Unit
+{
+    public class ArmExceptionTests
+    {
+        private const string AppJsonPrefix = "application/json";
+
+        private static readonly string TestAssetPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Unit", "TestAssets", "ArmException");
+
+        private IDictionary<string, string?> headers = new Dictionary<string, string?>
+        {
+            { "Content-Type", AppJsonPrefix },
+        };
+
+        [TestCase("ArmExceptionSimpleV1.json")]
+        //[TestCase("ArmExceptionSimpleV2.json")]
+        public void CreateExceptionTests(string jsonAssetFileName)
+        {
+            //var clientDiagnostics = GetClientDiagnostics();
+            var response = GetMockResponse(jsonAssetFileName);
+
+            ArmException exp = GetClientDiagnostics().CreateArmExceptionWithContent(response);
+
+            Assert.IsNotNull(exp);
+        }
+
+        private ClientDiagnostics GetClientDiagnostics()
+        {
+            var options = new Mock<ClientOptions>();
+
+            return new ClientDiagnostics(options.Object);
+        }
+
+        private Response GetMockResponse(string jsonAssetFileName)
+        {
+            var mock = new Mock<Response>();
+
+            // Set up the content stream on the response
+            var jsonStream = new FileStream(
+                Path.Combine(TestAssetPath, jsonAssetFileName),
+                FileMode.Open);
+            mock.Setup(res => res.ContentStream).Returns(jsonStream);
+
+            // Set up response header
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            // ReSharper disable once PossibleNullReferenceException
+            var responseHeaders = (ResponseHeaders)typeof(ResponseHeaders)
+                .GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(Response) }, null)
+                .Invoke(new object[] { mock.Object });
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+            mock.SetupGet(res => res.Headers).Returns(responseHeaders);
+            mock.Protected()
+                .Setup<bool>("TryGetHeader", ItExpr.IsAny<string>(), ItExpr.Ref<string>.IsAny)
+                .Returns(new TryGetHeaderCallback((string name, out string? value) => headers.TryGetValue(name, out value)));
+
+            return mock.Object;
+        }
+
+        private delegate bool TryGetHeaderCallback(string name, out string? value);
+    }
+}
