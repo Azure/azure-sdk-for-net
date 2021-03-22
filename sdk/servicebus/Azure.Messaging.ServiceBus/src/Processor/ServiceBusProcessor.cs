f0b4c27975e6ef4371a4a45a6ec1a7a07ffc8ca2
danielmarbach <daniel.marbach@openplace.net>
2021-03-22 10:00:04 +0100

    Calculate list size including receiveManagers.Count

--- sdk/servicebus/Azure.Messaging.ServiceBus/src/Processor/ServiceBusProcessor.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/src/Processor/ServiceBusProcessor.cs
@@ -653,7 +653,7 @@ namespace Azure.Messaging.ServiceBus
         private async Task RunReceiveTaskAsync(
             CancellationToken cancellationToken)
         {
-            List<Task> tasks = new List<Task>(MaxConcurrentCalls + 1);
+            List<Task> tasks = new List<Task>(MaxConcurrentCalls + _receiverManagers.Count);
             try
             {
                 while (!cancellationToken.IsCancellationRequested)
