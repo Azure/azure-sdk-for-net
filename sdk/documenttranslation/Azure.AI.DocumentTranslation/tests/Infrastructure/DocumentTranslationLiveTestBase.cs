54a37bde26069f4a7a42336f233ae3ee70472d29
NourEldin Yasser <noursalem95@gmail.com>
2021-03-24 22:39:33 +0200

    [documenttranslation] generate container names

--- sdk/documenttranslation/Azure.AI.DocumentTranslation/tests/Infrastructure/DocumentTranslationLiveTestBase.cs
+++ sdk/documenttranslation/Azure.AI.DocumentTranslation/tests/Infrastructure/DocumentTranslationLiveTestBase.cs
@@ -2,7 +2,14 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using Azure.Core.TestFramework;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
 
 namespace Azure.AI.DocumentTranslation.Tests
 {
@@ -25,6 +32,33 @@ namespace Azure.AI.DocumentTranslation.Tests
 
             credential ??= new AzureKeyCredential(TestEnvironment.ApiKey);
             return InstrumentClient(new DocumentTranslationClient(endpoint, credential, InstrumentClientOptions(options)));
+        }
+
+        public async Task<Uri> CreateSourceContainerAsync(List<string> documents)
+        {
+            string containerName = "source" + Guid.NewGuid().ToString();
+            var containerClient = new BlobContainerClient(TestEnvironment.StorageConnectionString, containerName.ToLower());
+            await containerClient.CreateIfNotExistsAsync(PublicAccessType.BlobContainer).ConfigureAwait(false);
+
+            for (int i = 0; i < documents.Count; i++)
+            {
+                byte[] byteArray = Encoding.ASCII.GetBytes(documents[i]);
+                MemoryStream stream = new MemoryStream(byteArray);
+                await containerClient.UploadBlobAsync($"document{i}.txt", stream);
+            }
+
+            var expiresOn = DateTimeOffset.Now.AddHours(1);
+            return containerClient.GenerateSasUri(BlobContainerSasPermissions.List | BlobContainerSasPermissions.Read, expiresOn);
+        }
+
+        public async Task<Uri> CreateTargetContainerAsync()
+        {
+            string containerName = "target" + Guid.NewGuid().ToString();
+            var containerClient = new BlobContainerClient(TestEnvironment.StorageConnectionString, containerName.ToLower());
+            await containerClient.CreateIfNotExistsAsync(PublicAccessType.BlobContainer).ConfigureAwait(false);
+
+            var expiresOn = DateTimeOffset.Now.AddHours(1);
+            return containerClient.GenerateSasUri(BlobContainerSasPermissions.Write, expiresOn);
         }
     }
 }
---
+++ sdk/documenttranslation/Azure.AI.DocumentTranslation/tests/TranslationOperationLiveTests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Azure.AI.DocumentTranslation.Tests
+{
+    public class TranslationOperationLiveTests : DocumentTranslationLiveTestBase
+    {
+        public TranslationOperationLiveTests(bool isAsync) : base(isAsync) { }
+
+        private List<string> Documents = new List<string>
+        {
+            "This is the first english test document",
+            "This is the second english test document"
+        };
+
+        [Test]
+        public async Task TranslationOperationTest()
+        {
+            Uri source = await CreateSourceContainerAsync(Documents);
+            Uri target = await CreateTargetContainerAsync();
+            Uri endpoint = new Uri(TestEnvironment.Endpoint);
+            AzureKeyCredential credential = new AzureKeyCredential(TestEnvironment.ApiKey);
+
+            var client = new DocumentTranslationClient(endpoint, credential);
+
+            var input = new DocumentTranslationInput(source, target, "fr");
+            var operation = await client.StartTranslationAsync(input);
+
+            await operation.WaitForCompletionAsync();
+
+            Assert.AreEqual(2, operation.DocumentsTotal);
+            Assert.AreEqual(2, operation.DocumentsSucceeded);
+            Assert.AreEqual(0, operation.DocumentsFailed);
+        }
+    }
+}
