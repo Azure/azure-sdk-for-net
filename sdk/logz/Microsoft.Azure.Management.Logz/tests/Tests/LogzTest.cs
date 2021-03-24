c4a47af1fae6c459431b0ad7588b0d3a28b478fb
varun kumar kunchakuri <66476965+vakuncha@users.noreply.github.com>
2021-03-24 11:43:29 +0530

    unit tests working

--- sdk/logz/Microsoft.Azure.Management.Logz/tests/Tests/LogzTest.cs
+++ sdk/logz/Microsoft.Azure.Management.Logz/tests/Tests/LogzTest.cs
@@ -1,32 +1,54 @@
-﻿
-using Microsoft.Azure.Management.Confluent.Models;
-using Microsoft.Azure.Management.Resources;
+﻿using Microsoft.Azure.Management.Resources;
 using Microsoft.Azure.Management.Resources.Models;
-using Microsoft.Rest.Azure;
+using Microsoft.Logz;
+using Microsoft.Logz.Models;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
-using System;
 using Xunit;
 
-namespace Microsoft.Azure.Management.Confluent.Tests
+namespace Microsoft.Azure.Management.Logz.Tests.Tests
 {
-    public class ConfluentTests
+    public class LogzTest
     {
+
         [Fact]
         public void TestResourceLifeCycle()
-        {    
+        {
+
+            using (var context = MockContext.Start(GetType()))
             {
-                var context = MockContext.Start(GetType());
-                string rgName = CreateName("confluent-sdk-test-rg");
-                string resourceName = CreateName("confluent-sdk-test-resource8350");
-              
+                string rgName = CreateName("logz-sdk-test-rg");
+                string resourceName = CreateName("logz-sdk-test-resource");
+
                 CreateResourceGroup(context, rgName);
-                OrganizationResource rp = CreateResource(context, rgName, resourceName);
+                LogzMonitorResource rp = CreateResource(context, rgName, resourceName);
                 Assert.NotNull(rp);
 
-                // DeleteResource(context, rgName, resourceName);
+                DeleteResource(context, rgName, resourceName);
                 DeleteResourceGroup(context, rgName);
             }
         }
+
+        private LogzMonitorResource CreateResource(MockContext context, string rgName, string resourceName)
+        {
+            MicrosoftLogzClient client = GetMicrosoftLogzClient(context);
+            return client.Monitors.Create(
+                rgName,
+                resourceName,
+                new LogzMonitorResource(
+                    name: resourceName,
+                    type: "Microsoft.Logz/monitors",
+                    location: "westus2",
+                    properties: new MonitorProperties(userInfo: new UserInfo(firstName: "varun", lastName: "kunchakuri", emailAddress: "liftrlogzdev@microsoft.com"))
+                    )
+            );
+        }
+
+        private void DeleteResource(MockContext context, string rgName, string resourceName)
+        {
+            MicrosoftLogzClient client = GetMicrosoftLogzClient(context);
+            client.Monitors.Delete(rgName, resourceName);
+        }
+
 
         private ResourceGroup CreateResourceGroup(MockContext context, string rgName)
         {
@@ -39,50 +61,23 @@ namespace Microsoft.Azure.Management.Confluent.Tests
                 });
         }
 
+        private string CreateName(string prefix) => TestUtilities.GenerateName(prefix);
+
         private void DeleteResourceGroup(MockContext context, string rgName)
         {
             ResourceManagementClient client = GetResourceManagementClient(context);
             client.ResourceGroups.Delete(rgName);
         }
 
-        private OrganizationResource CreateResource(MockContext context, string rgName, string resourceName)
-        {
-            ConfluentManagementClient client = GetConfluentManagementClient(context);
-            return client.Organization.Create(
-                rgName,
-                resourceName,
-                new OrganizationResource(
-                    name: resourceName,
-                    type: "Microsoft.Confluent/organizations",
-                    offerDetail: new OrganizationResourcePropertiesOfferDetail(publisherId: "isvtestuklegacy", id: "liftr_cf_dev", planId: "payg", planName: "Pay as you go", termUnit: "P1M"),
-                    userDetail: new OrganizationResourcePropertiesUserDetail(firstName: "Srinivas", lastName: "Alluri", emailAddress: "sralluri@microsoft.com"),
-                    location: "westus2"
-                    )
-            );
-        }
-
-        private void DeleteResource(MockContext context, string rgName, string resourceName)
-        {
-            ConfluentManagementClient client = GetConfluentManagementClient(context);
-            client.Organization.Delete(rgName, resourceName);
-        }
-
-        private IPage<OrganizationResource> ListResources(MockContext context, string rgName)
-        {
-            ConfluentManagementClient client = GetConfluentManagementClient(context);
-            return client.Organization.ListByResourceGroup(rgName);
-        }
-
-        private string CreateName(string prefix) => TestUtilities.GenerateName(prefix);
-
         private ResourceManagementClient GetResourceManagementClient(MockContext context)
         {
             return context.GetServiceClient<ResourceManagementClient>();
         }
 
-        private ConfluentManagementClient GetConfluentManagementClient(MockContext context)
+        private MicrosoftLogzClient GetMicrosoftLogzClient(MockContext context)
         {
-            return context.GetServiceClient<ConfluentManagementClient>();
+            return context.GetServiceClient<MicrosoftLogzClient>();
         }
+
     }
 }
