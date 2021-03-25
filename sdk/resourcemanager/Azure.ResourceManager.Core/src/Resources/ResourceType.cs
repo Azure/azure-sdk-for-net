90000293a4c7a3f3bf70f4cca3d24ece1a67dd79
Harvey Chen <30464227+HarveyLink@users.noreply.github.com>
2021-03-25 17:37:28 +0800

    Resource Serialization

--- sdk/resourcemanager/Azure.ResourceManager.Core/src/Resources/ResourceType.cs
+++ sdk/resourcemanager/Azure.ResourceManager.Core/src/Resources/ResourceType.cs
@@ -4,6 +4,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using Azure.Core;
 
 namespace Azure.ResourceManager.Core
 {
@@ -350,6 +352,45 @@ namespace Azure.ResourceManager.Core
             {
                 throw new ArgumentOutOfRangeException(nameof(resourceIdOrType));
             }
+        }
+
+        /// <summary>
+        /// Serialize the input ResourceType object.
+        /// </summary>
+        /// <param name="writer"> Input Json writer. </param>
+        /// <param name="value"> Input ResourceType object. </param>
+        internal static void Serialize(Utf8JsonWriter writer, ResourceType value)
+        {
+            if (writer is null)
+            {
+                throw new ArgumentNullException(nameof(writer));
+            }
+
+            writer.WriteStartObject();
+            if (Optional.IsDefined(value.Namespace))
+            {
+                writer.WritePropertyName("namespace");
+                writer.WriteStringValue(value.Namespace);
+            }
+            if (Optional.IsDefined(value.Parent))
+            {
+                writer.WritePropertyName("parent");
+                if (!value.Parent.Equals(new ResourceType()))
+                {
+                    Serialize(writer, value.Parent);
+                }
+                else
+                {
+                    writer.WriteStartObject();
+                    writer.WriteEndObject();
+                }
+            }
+            if (Optional.IsDefined(value.Type))
+            {
+                writer.WritePropertyName("type");
+                writer.WriteStringValue(value.Type);
+            }
+            writer.WriteEndObject();
         }
     }
 }
