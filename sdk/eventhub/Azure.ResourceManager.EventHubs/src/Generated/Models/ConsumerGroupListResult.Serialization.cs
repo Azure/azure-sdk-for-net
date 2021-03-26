a587429e68944e129f7defadb2675df6519a1389
Pavel Krymets <pavel@krymets.com>
2021-03-26 08:55:40 -0700

    regen

--- sdk/eventhub/Azure.ResourceManager.EventHubs/src/Generated/Models/ConsumerGroupListResult.Serialization.cs
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/src/Generated/Models/ConsumerGroupListResult.Serialization.cs
@@ -11,7 +11,7 @@ using Azure.Core;
 
 namespace Azure.ResourceManager.EventHubs.Models
 {
-    public partial class ConsumerGroupListResult
+    internal partial class ConsumerGroupListResult
     {
         internal static ConsumerGroupListResult DeserializeConsumerGroupListResult(JsonElement element)
         {
