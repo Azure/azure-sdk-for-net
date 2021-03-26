3cd5b1446d346ff4a0b6c2d2e5c87db23429c6ed
Yanming Tang <yantang@microsoft.com>
2021-03-26 00:47:23 -0700

    Rerun generated.ps1

--- sdk/changeanalysis/management/Microsoft.ChangeAnalysis/GeneratedProtocol/SdkInfo_AzureChangeAnalysisManagementClient.cs
+++ sdk/changeanalysis/management/Microsoft.ChangeAnalysis/GeneratedProtocol/SdkInfo_AzureChangeAnalysisManagementClient.cs
@@ -25,16 +25,5 @@ namespace Microsoft.ChangeAnalysis
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/changeanalysis/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=D:\\Testing\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "dac097e3cd84c72f4eeefc4c0f3346b881c9e473";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
