282dbe3da69deeffdf4145705228b36902724ec1
SDKAuto <sdkautomation@microsoft.com>
2021-03-30 07:52:02 +0000

    CodeGen from PR 13494 in Azure/azure-rest-api-specs datamigration readme t2 config (#13494)
    * datamigration readme t2 config
    
    * datamigration readme change
    
    * datamigration readme change V2
    
    * dataMigration readme change

--- sdk/datamigration/Microsoft.Azure.Management.DataMigration/src/Generated/SdkInfo_DataMigrationManagementClient.cs
+++ sdk/datamigration/Microsoft.Azure.Management.DataMigration/src/Generated/SdkInfo_DataMigrationManagementClient.cs
@@ -30,16 +30,5 @@ namespace Microsoft.Azure.Management.DataMigration
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/datamigration/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=E:\\forks\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "10f3923e845911a8c39bba966b0e9177301cd794";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
