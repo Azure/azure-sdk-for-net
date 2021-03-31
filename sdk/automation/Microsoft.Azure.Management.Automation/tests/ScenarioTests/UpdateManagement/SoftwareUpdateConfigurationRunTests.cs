9f4350ece58751f598591a5b709d880e2cf7f66e
Ayush <aysharm@microsoft.com>
2021-03-31 20:52:39 +0530

    adding CanGetAllRunsByStartTime record 3

--- sdk/automation/Microsoft.Azure.Management.Automation/tests/ScenarioTests/UpdateManagement/SoftwareUpdateConfigurationRunTests.cs
+++ sdk/automation/Microsoft.Azure.Management.Automation/tests/ScenarioTests/UpdateManagement/SoftwareUpdateConfigurationRunTests.cs
@@ -81,7 +81,7 @@
         [Fact]
         public void CanGetAllRunsByStartTime()
         {
-            var startTime = DateTime.Parse("2021-03-31T11:40:39").ToUniversalTime();
+            var startTime = DateTime.Parse("2021-03-31T17:10:39+05:30").ToUniversalTime();
             using (var context = MockContext.Start(this.GetType()))
             {
                 this.CreateAutomationClient(context);
