2ade0c18d0d6ecb982e8c181f1af1ef79d513ac3
Harvey Chen <30464227+HarveyLink@users.noreply.github.com>
2021-04-01 14:17:05 +0800

    Clean Up

--- sdk/resourcemanager/Azure.ResourceManager.Core/tests/Unit/ResourceTests.cs
+++ sdk/resourcemanager/Azure.ResourceManager.Core/tests/Unit/ResourceTests.cs
@@ -65,36 +65,5 @@ namespace Azure.ResourceManager.Core.Tests
             var resource2 = resource1;
             Assert.AreEqual(0, resource1.CompareTo(resource2));
         }
-
-        [Test]
-        public void SerializationTest()
-        {
-            string expected = "{\"Properties\":{\"id\":{\"id\":\"/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testRg/providers/Microsoft.ClassicStorage/storageAccounts/account1\",\"name\":\"account1\",\"parent\":{\"id\":\"/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testRg\",\"name\":\"testRg\",\"parent\":{\"id\":\"/subscriptions/00000000-0000-0000-0000-000000000000\",\"name\":\"00000000-0000-0000-0000-000000000000\",\"subscription\":\"00000000-0000-0000-0000-000000000000\",\"type\":{\"namespace\":\"Microsoft.Resources\",\"parent\":{},\"type\":\"subscriptions\"}},\"resourceGroup\":\"testRg\",\"subscription\":\"00000000-0000-0000-0000-000000000000\",\"type\":{\"namespace\":\"Microsoft.Resources\",\"parent\":{},\"type\":\"resourceGroups\"}},\"resourceGroup\":\"testRg\",\"subscription\":\"00000000-0000-0000-0000-000000000000\",\"type\":{\"namespace\":\"Microsoft.ClassicStorage\",\"parent\":{},\"type\":\"storageAccounts\"}},\"name\":\"account1\",\"type\":{\"namespace\":\"Microsoft.ClassicStorage\",\"parent\":{},\"type\":\"storageAccounts\"}}}";
-            TestResource data = new TestResource("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testRg/providers/Microsoft.ClassicStorage/storageAccounts/account1");
-            var stream = new MemoryStream();
-            Utf8JsonWriter writer = new(stream, new JsonWriterOptions());
-            writer.WriteStartObject();
-            writer.WritePropertyName("Properties");
-            writer.WriteObjectValue(data);
-            writer.WriteEndObject();
-            writer.Flush();
-            string json = Encoding.UTF8.GetString(stream.ToArray());
-            Assert.IsTrue(expected.Equals(json));
-        }
-
-        [Test]
-        public void InvalidSerializationTest()
-        {
-            TestResource data = new TestResource(null);
-            var stream = new MemoryStream();
-            Utf8JsonWriter writer = new(stream, new JsonWriterOptions());
-            writer.WriteStartObject();
-            writer.WritePropertyName("Properties");
-            writer.WriteObjectValue(data);
-            writer.WriteEndObject();
-            writer.Flush();
-            string json = Encoding.UTF8.GetString(stream.ToArray());
-            Assert.IsTrue(json.Equals("{\"Properties\":{}}"));
-        }
     }
 }
