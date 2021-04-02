a29e413853cacdca0b6ed5301759cf5b9c0a9958
Mariana Rios Flores <mariari@microsoft.com>
2021-04-02 14:45:16 -0700

    revert defaultpollingtime

--- sdk/documenttranslation/Azure.AI.Translator.DocumentTranslation/tests/Infrastructure/DocumentTranslationLiveTestBase.cs
+++ sdk/documenttranslation/Azure.AI.Translator.DocumentTranslation/tests/Infrastructure/DocumentTranslationLiveTestBase.cs
@@ -15,7 +15,7 @@ namespace Azure.AI.Translator.DocumentTranslation.Tests
 {
     public class DocumentTranslationLiveTestBase : RecordedTestBase<DocumentTranslationTestEnvironment>
     {
-        protected TimeSpan PollingInterval => TimeSpan.FromSeconds(Mode == RecordedTestMode.Playback ? 0 : 2);
+        protected TimeSpan PollingInterval => TimeSpan.FromSeconds(Mode == RecordedTestMode.Playback ? 0 : 30);
 
         public DocumentTranslationLiveTestBase(bool isAsync, RecordedTestMode? mode = null)
             : base(isAsync, mode ?? RecordedTestUtilities.GetModeFromEnvironment())
