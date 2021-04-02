7fc21a93e584ca4fc31ff041018471f12eb0ba53
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-04-01 20:04:10 -0700

    Close link on open cancellation (#20017)
    * Close link on open cancellation
    
    * TrySetResult
    
    * Update
    
    * Use tuple

--- sdk/servicebus/Azure.Messaging.ServiceBus/tests/Receiver/SessionReceiverLiveTests.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/tests/Receiver/SessionReceiverLiveTests.cs
@@ -944,7 +944,9 @@ namespace Azure.Messaging.ServiceBus.Tests.Receiver
         }
 
         [Test]
-        public async Task CancellingDoesNotLoseSessionMessages()
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task CancellingDoesNotLoseSessionMessages(bool prefetch)
         {
             await using (var scope = await ServiceBusScope.CreateWithQueue(enablePartitioning: false, enableSession: true))
             {
@@ -958,7 +960,7 @@ namespace Azure.Messaging.ServiceBus.Tests.Receiver
                 var receiver = await client.AcceptSessionAsync(
                     scope.QueueName,
                     "sessionId",
-                    new ServiceBusSessionReceiverOptions { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete });
+                    new ServiceBusSessionReceiverOptions { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete, PrefetchCount = prefetch ? 10 : 0 });
 
                 using var cancellationTokenSource = new CancellationTokenSource(500);
                 var received = 0;
@@ -985,6 +987,35 @@ namespace Azure.Messaging.ServiceBus.Tests.Receiver
                     received++;
                 }
                 Assert.AreEqual(messageCount, received);
+            }
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        [Ignore("Not yet implemented")]
+        public async Task CancellingDoesNotBlockSubsequentReceives(bool prefetch)
+        {
+            await using (var scope = await ServiceBusScope.CreateWithQueue(enablePartitioning: false, enableSession: true))
+            {
+                await using var client = CreateClient();
+
+                ServiceBusSender sender = client.CreateSender(scope.QueueName);
+                var receiver = await client.AcceptSessionAsync(scope.QueueName, "sessionId", new ServiceBusSessionReceiverOptions { PrefetchCount = prefetch ? 10 : 0 });
+
+                using var cancellationTokenSource = new CancellationTokenSource(500);
+                var start = DateTime.UtcNow;
+
+                Assert.That(
+                    async () => await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(60), cancellationToken: cancellationTokenSource.Token),
+                    Throws.InstanceOf<TaskCanceledException>());
+
+                await sender.SendMessageAsync(GetMessage("sessionId"));
+                var msg = await receiver.ReceiveMessageAsync();
+                Assert.AreEqual(1, msg.DeliveryCount);
+                var end = DateTime.UtcNow;
+                Assert.NotNull(msg);
+                Assert.Less(end - start, TimeSpan.FromSeconds(5));
             }
         }
     }
