689209bdb7b5487be0e75a5cc5a3b47d5ea15b99
Mariana Rios Flores <mariari@microsoft.com>
2021-04-05 10:41:32 -0700

    rename to Azure.AI.Translation.Document (#20095)

---
+++ sdk/documenttranslation/Azure.AI.Translation.Document/src/StorageInputType.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.AI.Translation.Document
+{
+    [CodeGenModel("StorageInputType")]
+    public readonly partial struct StorageInputType
+    {
+    }
+}
