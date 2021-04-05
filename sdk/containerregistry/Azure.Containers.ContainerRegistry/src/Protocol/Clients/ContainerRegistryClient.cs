64c9092f848ef53f4f33d50be6ed8f2326fa74c0
Anne Loomis Thompson <annelo@microsoft.com>
2021-04-05 14:03:49 -0700

    initial LLC gen

---
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Protocol/Clients/ContainerRegistryClient.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+#pragma warning disable AZC0007
+
+namespace Azure.Containers.ContainerRegistry.Protocol
+{
+    /// <summary> The ContainerRegistry service client. </summary>
+    internal partial class ContainerRegistryClient
+    {
+    }
+}
