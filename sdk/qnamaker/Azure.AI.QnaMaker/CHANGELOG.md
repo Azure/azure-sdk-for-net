b1958dda19a1db72922492a4531e70398799d7c0
Heath Stewart <heaths@microsoft.com>
2021-04-06 20:49:25 -0700

    Initial QnaMakerClient

---
+++ sdk/qnamaker/Azure.AI.QnaMaker/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Release History
+
+## 4.0.0-preview.1 (Unreleased)
+
+- Initial release of the Azure QnA Maker client library for .NET
---
+++ sdk/qnamaker/Azure.AI.QnaMaker/src/QnaMakerClient.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace Azure.AI.QnaMaker
+{
+    public class QnaMakerClient
+    {
+        private const string AuthorizationHeader = "Ocp-Apim-Subscription-Key";
+
+        private readonly ClientDiagnostics _diagnostics;
+        private readonly KnowledgebaseRestClient _knowledgebaseRestClient;
+
+        public QnaMakerClient(Uri endpoint, AzureKeyCredential credential) : this(endpoint, credential, null)
+        {
+        }
+
+        public QnaMakerClient(Uri endpoint, AzureKeyCredential credential, QnaMakerClientOptions options)
+        {
+            Argument.AssertNotNull(endpoint, nameof(endpoint));
+            Argument.AssertNotNull(credential, nameof(credential));
+
+            Endpoint = endpoint;
+            options ??= new QnaMakerClientOptions();
+
+            _diagnostics = new ClientDiagnostics(options);
+
+            HttpPipeline pipeline = HttpPipelineBuilder.Build(
+                options,
+                new AzureKeyCredentialPolicy(credential, AuthorizationHeader));
+
+            // TODO: The api-version is hardcoded into the path and needs to be parameterized.
+            _knowledgebaseRestClient = new KnowledgebaseRestClient(_diagnostics, pipeline, endpoint.AbsoluteUri);
+        }
+
+        /// <summary>
+        /// Protected constructor to allow mocking.
+        /// </summary>
+        protected QnaMakerClient()
+        {
+        }
+
+        /// <summary>
+        /// Get the service endpoint for this client.
+        /// </summary>
+        public Uri Endpoint { get; }
+    }
+}
