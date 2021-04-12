5791452f38c5da79f5180c00abde3b3eb60bc6ea
Pavel Krymets <pavel@krymets.com>
2021-04-12 10:14:46 -0700

    Add test data support, generic query and more tests (#20280)

--- sdk/monitor/Azure.Monitory.Query/src/Models/LogQueryResult.cs
+++ sdk/monitor/Azure.Monitory.Query/src/Models/LogQueryResult.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Linq;
 using Azure.Core;
 
 namespace Azure.Monitory.Query.Models
@@ -8,5 +9,7 @@ namespace Azure.Monitory.Query.Models
     [CodeGenModel("QueryResults")]
     public partial class LogsQueryResult
     {
+        // TODO: Handle not found
+        public LogsQueryResultTable PrimaryTable => Tables.Single(t => t.Name == "PrimaryResult");
     }
 }
