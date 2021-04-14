a4c9ff180e9b439c0c707c77555f7a9a242d16b5
Pavel Krymets <pavel@krymets.com>
2021-04-14 10:04:48 -0700

    Support #if SNIPPET in snippets

--- eng/SnippetGenerator/DirectoryProcessor.cs
+++ eng/SnippetGenerator/DirectoryProcessor.cs
@@ -28,6 +28,7 @@ namespace SnippetGenerator
             RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.CultureInvariant);
 
         private UTF8Encoding _utf8EncodingWithoutBOM;
+        private static string[] _snippetPreprocessorSymbols = new [] {"SNIPPET"};
 
         public DirectoryProcessor(string directory)
         {
@@ -208,7 +209,7 @@ namespace SnippetGenerator
                 {
                     var syntaxTree = CSharpSyntaxTree.ParseText(
                         File.ReadAllText(file),
-                        new CSharpParseOptions(LanguageVersion.Preview),
+                        new CSharpParseOptions(LanguageVersion.Preview, preprocessorSymbols: _snippetPreprocessorSymbols),
                         path: file);
 
                     list.AddRange(GetAllSnippets(syntaxTree));
@@ -225,8 +226,12 @@ namespace SnippetGenerator
         private Snippet[] GetAllSnippets(SyntaxTree syntaxTree)
         {
             var snippets = new List<Snippet>();
+            var newRoot = PreprocessorDirectiveRemover.Instance.Visit(syntaxTree.GetRoot());
+            syntaxTree = syntaxTree.WithRootAndOptions(newRoot, syntaxTree.Options);
+
             var directiveWalker = new DirectiveWalker();
             directiveWalker.Visit(syntaxTree.GetRoot());
+
 
             foreach (var region in directiveWalker.Regions)
             {
@@ -251,6 +256,41 @@ namespace SnippetGenerator
             return snippets.ToArray();
         }
 
+        class PreprocessorDirectiveRemover : CSharpSyntaxRewriter
+        {
+            public static PreprocessorDirectiveRemover Instance = new PreprocessorDirectiveRemover();
+            private PreprocessorDirectiveRemover() : base(visitIntoStructuredTrivia: true)
+            {
+            }
+
+            public override SyntaxTrivia VisitTrivia(SyntaxTrivia trivia)
+            {
+                if (trivia.Kind() == SyntaxKind.DisabledTextTrivia)
+                    return SyntaxFactory.Whitespace("");
+
+                return base.VisitTrivia(trivia);
+            }
+
+            public override SyntaxNode VisitIfDirectiveTrivia(IfDirectiveTriviaSyntax node)
+            {
+                return null;
+            }
+
+            public override SyntaxNode VisitElifDirectiveTrivia(ElifDirectiveTriviaSyntax node)
+            {
+                return null;
+            }
+
+            public override SyntaxNode VisitElseDirectiveTrivia(ElseDirectiveTriviaSyntax node)
+            {
+                return null;
+            }
+
+            public override SyntaxNode VisitEndIfDirectiveTrivia(EndIfDirectiveTriviaSyntax node)
+            {
+                return null;
+            }
+        }
         class DirectiveWalker : CSharpSyntaxWalker
         {
             private Stack<RegionDirectiveTriviaSyntax> _regions = new Stack<RegionDirectiveTriviaSyntax>();
