a04990a1b21e7be61e29e9e92adc8f9ea54284db
Pavel Krymets <pavel@krymets.com>
2021-04-19 11:10:31 -0700

    structs

--- sdk/monitor/Azure.Monitory.Query/tests/TypeBinderTests.cs
+++ sdk/monitor/Azure.Monitory.Query/tests/TypeBinderTests.cs
@@ -56,6 +56,47 @@ namespace Azure.Monitory.Query.Tests
             Assert.False(dictionary.ContainsKey("IgnoredProperty"));
         }
 
+        [Test]
+        public void CanDeserializeStructFromDictionary()
+        {
+            var model = new TestBinder().Deserialize<ModelClass>(new Dictionary<string, object>()
+            {
+                { "IntProperty", 1 },
+                { "IntField", 2 },
+                { "StringProperty", "1" },
+                { "StringField", "2" }
+            });
+
+            Assert.AreEqual(1, model.IntProperty);
+            Assert.AreEqual(2, model.IntField);
+            Assert.AreEqual("1", model.StringProperty);
+            Assert.AreEqual("2", model.StringField);
+        }
+
+        [Test]
+        public void CanSerializeStructToDictionary()
+        {
+            var dictionary = new Dictionary<string, object>();
+
+            new TestBinder().Serialize(new ModelClass()
+            {
+                IntProperty = 1,
+                IntField = 2,
+                StringProperty = "1",
+                StringField = "2",
+                RenamedProperty = "renamed value"
+            }, dictionary);
+
+            Assert.AreEqual(1, dictionary["IntProperty"]);
+            Assert.AreEqual(2, dictionary["IntField"]);
+            Assert.AreEqual(4, dictionary["IntReadOnlyProperty"]);
+            Assert.AreEqual(5, dictionary["IntReadOnlyField"]);
+            Assert.AreEqual("1", dictionary["StringProperty"]);
+            Assert.AreEqual("2", dictionary["StringField"]);
+            Assert.AreEqual("3", dictionary["StringReadOnlyProperty"]);
+            Assert.AreEqual("42", dictionary["StringReadOnlyField"]);
+        }
+
 #pragma warning disable 414
 #pragma warning disable 649
         private class ModelClass
@@ -77,6 +118,21 @@ namespace Azure.Monitory.Query.Tests
 
             [DataMember(Name = "renamed_property")]
             public string RenamedProperty { get; set; } = "renamed property";
+        }
+
+        private struct ModelStruct
+        {
+            public int IntProperty { get; set; }
+            public int IntField;
+
+            public int IntReadOnlyProperty => 4;
+            public readonly int IntReadOnlyField;
+
+            public string StringProperty { get; set; }
+            public string StringField;
+
+            public string StringReadOnlyProperty => "3";
+            public readonly string StringReadOnlyField;
         }
 #pragma warning restore 649
 #pragma warning restore 414
