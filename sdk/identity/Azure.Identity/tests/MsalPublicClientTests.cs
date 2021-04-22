4c8d064346090921bc7288a900619986e9b8fdaa
Christopher Scott <chriss@microsoft.com>
2021-04-22 11:29:00 -0500

    fix test

--- sdk/identity/Azure.Identity/tests/MsalPublicClientTests.cs
+++ sdk/identity/Azure.Identity/tests/MsalPublicClientTests.cs
@@ -13,29 +13,9 @@ namespace Azure.Identity.Tests
     {
         private string cp1 = "CP1";
 
-        // [Test]
-        public void CreateClientRespectsCaeConfig(
-            [Values(true, false)] bool setDisableSwitch,
-            [Values(true, false)] bool setDisableEnvVar)
-        {
-            AppContext.SetSwitch(IdentityCompatSwitches.DisableCP1ExecutionSwitchName, setDisableSwitch);
-            using (var _ = new  TestEnvVar(IdentityCompatSwitches.DisableCP1ExecutionEnvVar, setDisableEnvVar.ToString()))
-            {
-                var mock = new MockMsalPublicClient();
-                mock.PubClientAppFactory = (capabilities) =>
-                {
-                    bool IsCp1Set = cp1 == string.Join("", capabilities);
-                    Assert.AreEqual(setDisableSwitch || setDisableEnvVar, !IsCp1Set);
-                    return Moq.Mock.Of<IPublicClientApplication>();
-                };
-
-                mock.CallCreateClientAsync(false, default);
-            }
-        }
-
         [Test]
         [NonParallelizable]
-        public void GetConfigValue(
+        public void CreateClientRespectsCaeConfig(
             [Values(true, false, null)] bool? setDisableSwitch,
             [Values(true, false, null)] bool? setDisableEnvVar)
         {
