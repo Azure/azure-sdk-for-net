c5b55ec9c241deb63e4c3571fbeddf5c85936b83
Basel Rustum <barustum@microsoft.com>
2021-04-23 01:37:18 -0700

    [TSI] Hide models that do not interface with the client library users

--- sdk/timeseriesinsights/Azure.IoT.TimeSeriesInsights/src/Generated/Models/Availability.cs
+++ sdk/timeseriesinsights/Azure.IoT.TimeSeriesInsights/src/Generated/Models/Availability.cs
@@ -12,19 +12,19 @@ using Azure.Core;
 namespace Azure.IoT.TimeSeriesInsights
 {
     /// <summary> Event availability information when environment contains events. Contains time range of events and approximate distribution of events over time. </summary>
-    public partial class EventAvailability
+    internal partial class Availability
     {
-        /// <summary> Initializes a new instance of EventAvailability. </summary>
-        internal EventAvailability()
+        /// <summary> Initializes a new instance of Availability. </summary>
+        internal Availability()
         {
             Distribution = new ChangeTrackingDictionary<string, int>();
         }
 
-        /// <summary> Initializes a new instance of EventAvailability. </summary>
+        /// <summary> Initializes a new instance of Availability. </summary>
         /// <param name="range"> Minimum and maximum values of event timestamp ($ts) property. </param>
         /// <param name="intervalSize"> Interval size for the returned distribution of the events. Returned interval is selected to return a reasonable number of points. All intervals are the same size. On the wire interval is specified in ISO-8601 duration format. One month is always converted to 30 days, and one year is always 365 days. Examples: 1 minute is &quot;PT1M&quot;, 1 millisecond is &quot;PT0.001S&quot;. For more information, see https://www.w3.org/TR/xmlschema-2/#duration. </param>
         /// <param name="distribution"> Dictionary of &lt;integer&gt;. </param>
-        internal EventAvailability(DateTimeRange range, TimeSpan? intervalSize, IReadOnlyDictionary<string, int> distribution)
+        internal Availability(DateTimeRange range, TimeSpan? intervalSize, IReadOnlyDictionary<string, int> distribution)
         {
             Range = range;
             IntervalSize = intervalSize;
--- sdk/timeseriesinsights/Azure.IoT.TimeSeriesInsights/src/Generated/Models/HierarchiesExpandParameter.Serialization.cs
+++ sdk/timeseriesinsights/Azure.IoT.TimeSeriesInsights/src/Generated/Models/HierarchiesExpandParameter.Serialization.cs
@@ -10,7 +10,7 @@ using Azure.Core;
 
 namespace Azure.IoT.TimeSeriesInsights
 {
-    public partial class HierarchiesExpandParameter : IUtf8JsonSerializable
+    internal partial class HierarchiesExpandParameter : IUtf8JsonSerializable
     {
         void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
         {
