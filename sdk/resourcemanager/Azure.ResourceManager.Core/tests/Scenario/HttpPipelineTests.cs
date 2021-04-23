9a91ef1718b029c87b6aa976245023d78d7fe7f4
YalinLi0312 <yall@microsoft.com>
2021-04-23 14:25:31 -0700

    Add tests

---
+++ sdk/resourcemanager/Azure.ResourceManager.Core/tests/Scenario/HttpPipelineTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.Identity;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Core.Tests.Scenario
+{
+    public class HttpPipelineTests : ResourceManagerTestBase
+    {
+        private ArmClient _client;
+
+        public HttpPipelineTests(bool isAsync)
+            : base(isAsync) //, RecordedTestMode.Record)
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _client = GetArmClient();
+        }
+
+        [Test]
+        public void ValidateHttpPipelines()
+        {
+            ResourceGroup resourceGroup = _client.DefaultSubscription
+                .GetResourceGroups().Construct("westus")
+                .CreateOrUpdateAsync("test-CacheHttpPipeline").ConfigureAwait(false).GetAwaiter().GetResult();
+            Assert.AreEqual(resourceGroup.Pipeline.GetHashCode(), _client.DefaultSubscription.Pipeline.GetHashCode());
+        }
+    }
+}
