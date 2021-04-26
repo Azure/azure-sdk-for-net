63b90fffc1b4f20e3163ffb8f4f7f4557223f8dc
Pavel Krymets <pavel@krymets.com>
2021-04-26 16:17:49 -0700

    Add logs query options (#20598)

--- sdk/monitor/Azure.Monitor.Query/src/LogsClient.cs
+++ sdk/monitor/Azure.Monitor.Query/src/LogsClient.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
@@ -38,27 +40,27 @@ namespace Azure.Monitor.Query
         {
         }
 
-        public virtual Response<IReadOnlyList<T>> Query<T>(string workspaceId, string query, TimeSpan? timeSpan = null, CancellationToken cancellationToken = default)
+        public virtual Response<IReadOnlyList<T>> Query<T>(string workspaceId, string query, TimeSpan? timeSpan = null, LogsQueryOptions options = null, CancellationToken cancellationToken = default)
         {
-            Response<LogsQueryResult> response = Query(workspaceId, query, timeSpan, cancellationToken);
+            Response<LogsQueryResult> response = Query(workspaceId, query, timeSpan, options, cancellationToken);
 
             return Response.FromValue(_rowBinder.BindResults<T>(response), response.GetRawResponse());
         }
 
-        public virtual async Task<Response<IReadOnlyList<T>>> QueryAsync<T>(string workspaceId, string query, TimeSpan? timeSpan = null, CancellationToken cancellationToken = default)
+        public virtual async Task<Response<IReadOnlyList<T>>> QueryAsync<T>(string workspaceId, string query, TimeSpan? timeSpan = null, LogsQueryOptions options = null, CancellationToken cancellationToken = default)
         {
-            Response<LogsQueryResult> response = await QueryAsync(workspaceId, query, timeSpan,  cancellationToken).ConfigureAwait(false);
+            Response<LogsQueryResult> response = await QueryAsync(workspaceId, query, timeSpan, options, cancellationToken).ConfigureAwait(false);
 
             return Response.FromValue(_rowBinder.BindResults<T>(response), response.GetRawResponse());
         }
 
-        public virtual Response<LogsQueryResult> Query(string workspaceId, string query, TimeSpan? timeSpan = null, CancellationToken cancellationToken = default)
+        public virtual Response<LogsQueryResult> Query(string workspaceId, string query, TimeSpan? timeSpan = null, LogsQueryOptions options = null, CancellationToken cancellationToken = default)
         {
             using DiagnosticScope scope = _clientDiagnostics.CreateScope($"{nameof(LogsClient)}.{nameof(Query)}");
             scope.Start();
             try
             {
-                return _queryClient.Execute(workspaceId, CreateQueryBody(query, timeSpan), null, cancellationToken);
+                return ExecuteAsync(workspaceId, query, timeSpan, options, false, cancellationToken).EnsureCompleted();
             }
             catch (Exception e)
             {
@@ -67,13 +69,13 @@ namespace Azure.Monitor.Query
             }
         }
 
-        public virtual async Task<Response<LogsQueryResult>> QueryAsync(string workspaceId, string query, TimeSpan? timeSpan = null, CancellationToken cancellationToken = default)
+        public virtual async Task<Response<LogsQueryResult>> QueryAsync(string workspaceId, string query, TimeSpan? timeSpan = null, LogsQueryOptions options = null, CancellationToken cancellationToken = default)
         {
             using DiagnosticScope scope = _clientDiagnostics.CreateScope($"{nameof(LogsClient)}.{nameof(Query)}");
             scope.Start();
             try
             {
-                return await _queryClient.ExecuteAsync(workspaceId, CreateQueryBody(query, timeSpan), null, cancellationToken).ConfigureAwait(false);
+                return await ExecuteAsync(workspaceId, query, timeSpan, options, true, cancellationToken).ConfigureAwait(false);
             }
             catch (Exception e)
             {
@@ -87,7 +89,7 @@ namespace Azure.Monitor.Query
             return new LogsBatchQuery(_clientDiagnostics, _queryClient, _rowBinder);
         }
 
-        internal static QueryBody CreateQueryBody(string query, TimeSpan? timeSpan)
+        internal static QueryBody CreateQueryBody(string query, TimeSpan? timeSpan, LogsQueryOptions options, out string prefer)
         {
             var queryBody = new QueryBody(query);
             if (timeSpan != null)
@@ -95,7 +97,68 @@ namespace Azure.Monitor.Query
                 queryBody.Timespan = TypeFormatters.ToString(timeSpan.Value, "P");
             }
 
+            prefer = null;
+
+            if (options?.Timeout is TimeSpan timeout)
+            {
+                prefer = "wait=" + (int) timeout.TotalSeconds;
+            }
+
+            if (options?.IncludeStatistics == true)
+            {
+                prefer += " include-statistics=true";
+            }
+
             return queryBody;
+        }
+
+        private async Task<Response<LogsQueryResult>> ExecuteAsync(string workspaceId, string query, TimeSpan? timeSpan, LogsQueryOptions options, bool async, CancellationToken cancellationToken = default)
+        {
+            if (workspaceId == null)
+            {
+                throw new ArgumentNullException(nameof(workspaceId));
+            }
+
+            QueryBody queryBody = CreateQueryBody(query, timeSpan, options, out string prefer);
+            using var message = _queryClient.CreateExecuteRequest(workspaceId, queryBody, prefer);
+
+            if (options?.Timeout != null)
+            {
+                message.SetProperty("NetworkTimeoutOverride", options.Timeout);
+            }
+
+            if (async)
+            {
+                await _pipeline.SendAsync(message, cancellationToken).ConfigureAwait(false);
+            }
+            else
+            {
+                _pipeline.Send(message, cancellationToken);
+            }
+
+            switch (message.Response.Status)
+            {
+                case 200:
+                {
+                    using var document = async ?
+                        await JsonDocument.ParseAsync(message.Response.ContentStream, default, cancellationToken).ConfigureAwait(false) :
+                        JsonDocument.Parse(message.Response.ContentStream, default);
+
+                    LogsQueryResult value = LogsQueryResult.DeserializeLogsQueryResult(document.RootElement);
+                    return Response.FromValue(value, message.Response);
+                }
+                default:
+                {
+                    if (async)
+                    {
+                        throw await _clientDiagnostics.CreateRequestFailedExceptionAsync(message.Response).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        throw _clientDiagnostics.CreateRequestFailedException(message.Response);
+                    }
+                }
+            }
         }
     }
 }
