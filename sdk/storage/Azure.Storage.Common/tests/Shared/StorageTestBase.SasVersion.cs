f20fab3be921fe1603fa216362e600b86a8f33d2
Kamil Sobol <61715331+kasobol-msft@users.noreply.github.com>
2021-04-26 16:06:19 -0700

    [Storage] Finish Version property deprecation. Make SAS version settable in tests. (#20669)
    * finish deprecation.
    
    * continue deprecation of Version property.
    
    * override sas.
    
    * changelog.
    
    * one more.
    
    * api
    
    * recordings 1.
    
    * more recordings.
    
    * getting there.
    
    * ...

---
+++ sdk/storage/Azure.Storage.Common/tests/Shared/StorageTestBase.SasVersion.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+using Azure.Storage.Sas;
+using NUnit.Framework;
+
+namespace Azure.Storage.Test.Shared
+{
+    public abstract partial class StorageTestBase<TEnvironment> : RecordedTestBase<TEnvironment> where TEnvironment : StorageTestEnvironment, new()
+    {
+        [OneTimeSetUp]
+        public void SetSasVersion()
+        {
+            if (Mode != RecordedTestMode.Live)
+            {
+                // WARN! Never ever override SAS version in Live mode. We should test the default there.
+
+                // TODO Uncomment this to record/playback with different sas version
+                // SasQueryParametersInternals.DefaultSasVersionInternal = "2020-08-04";
+            }
+        }
+    }
+}
