5f51c40c09483cd80a24bd2f4dee7c54eb49a717
azure-sdk <azuresdk@microsoft.com>
2021-04-27 00:34:34 +0000

    Update AutoRest C# version to 3.0.0-beta.20210426.2

--- sdk/synapse/Azure.Analytics.Synapse.Artifacts/src/Generated/PipelineRenamePipelineOperation.cs
+++ sdk/synapse/Azure.Analytics.Synapse.Artifacts/src/Generated/PipelineRenamePipelineOperation.cs
@@ -17,7 +17,7 @@ namespace Azure.Analytics.Synapse.Artifacts
     /// <summary> Renames a pipeline. </summary>
     public partial class PipelineRenamePipelineOperation : Operation<Response>, IOperationSource<Response>
     {
-        private readonly ArmOperationHelpers<Response> _operation;
+        private readonly OperationInternals<Response> _operation;
 
         /// <summary> Initializes a new instance of PipelineRenamePipelineOperation for mocking. </summary>
         protected PipelineRenamePipelineOperation()
@@ -26,7 +26,7 @@ namespace Azure.Analytics.Synapse.Artifacts
 
         internal PipelineRenamePipelineOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response)
         {
-            _operation = new ArmOperationHelpers<Response>(this, clientDiagnostics, pipeline, request, response, OperationFinalStateVia.Location, "PipelineRenamePipelineOperation");
+            _operation = new OperationInternals<Response>(this, clientDiagnostics, pipeline, request, response, OperationFinalStateVia.Location, "PipelineRenamePipelineOperation");
         }
         /// <inheritdoc />
         public override string Id => _operation.Id;
--- sdk/synapse/Azure.Analytics.Synapse.Artifacts/src/Generated/SqlScriptDeleteSqlScriptOperation.cs
+++ sdk/synapse/Azure.Analytics.Synapse.Artifacts/src/Generated/SqlScriptDeleteSqlScriptOperation.cs
@@ -17,7 +17,7 @@ namespace Azure.Analytics.Synapse.Artifacts
     /// <summary> Deletes a Sql Script. </summary>
     public partial class SqlScriptDeleteSqlScriptOperation : Operation<Response>, IOperationSource<Response>
     {
-        private readonly ArmOperationHelpers<Response> _operation;
+        private readonly OperationInternals<Response> _operation;
 
         /// <summary> Initializes a new instance of SqlScriptDeleteSqlScriptOperation for mocking. </summary>
         protected SqlScriptDeleteSqlScriptOperation()
@@ -26,7 +26,7 @@ namespace Azure.Analytics.Synapse.Artifacts
 
         internal SqlScriptDeleteSqlScriptOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response)
         {
-            _operation = new ArmOperationHelpers<Response>(this, clientDiagnostics, pipeline, request, response, OperationFinalStateVia.Location, "SqlScriptDeleteSqlScriptOperation");
+            _operation = new OperationInternals<Response>(this, clientDiagnostics, pipeline, request, response, OperationFinalStateVia.Location, "SqlScriptDeleteSqlScriptOperation");
         }
         /// <inheritdoc />
         public override string Id => _operation.Id;
