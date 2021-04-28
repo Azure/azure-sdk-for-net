4968b8ad2285f30db28ecb050db4a2b31efd4d3b
Mariana Rios Flores <mariari@microsoft.com>
2021-04-28 16:05:56 -0700

    separate endpoints into test classes and apply api-version to class

--- sdk/formrecognizer/Azure.AI.FormRecognizer/tests/Infrastructure/FormRecognizerLiveTestBase.cs
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/tests/Infrastructure/FormRecognizerLiveTestBase.cs
@@ -2,10 +2,13 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Azure.AI.FormRecognizer.Models;
 using Azure.AI.FormRecognizer.Training;
 using Azure.Core;
 using Azure.Core.TestFramework;
+using NUnit.Framework;
 
 namespace Azure.AI.FormRecognizer.Tests
 {
@@ -135,6 +138,183 @@ namespace Azure.AI.FormRecognizer.Tests
             SelectionMarks,
             TableVariableRows,
             TableFixedRows
+        }
+
+        protected void ValidatePrebuiltForm(RecognizedForm recognizedForm, bool includeFieldElements, int expectedFirstPageNumber, int expectedLastPageNumber)
+        {
+            Assert.NotNull(recognizedForm.FormType);
+            Assert.IsTrue(recognizedForm.FormTypeConfidence.HasValue);
+            Assert.That(recognizedForm.FormTypeConfidence.Value, Is.LessThanOrEqualTo(1.0).Within(0.005));
+            Assert.IsNull(recognizedForm.ModelId);
+
+            ValidateRecognizedForm(recognizedForm, includeFieldElements, expectedFirstPageNumber, expectedLastPageNumber);
+        }
+
+        protected void ValidateRecognizedForm(RecognizedForm recognizedForm, bool includeFieldElements, int expectedFirstPageNumber, int expectedLastPageNumber)
+        {
+            Assert.AreEqual(expectedFirstPageNumber, recognizedForm.PageRange.FirstPageNumber);
+            Assert.AreEqual(expectedLastPageNumber, recognizedForm.PageRange.LastPageNumber);
+
+            Assert.NotNull(recognizedForm.Pages);
+            Assert.AreEqual(expectedLastPageNumber - expectedFirstPageNumber + 1, recognizedForm.Pages.Count);
+
+            int expectedPageNumber = expectedFirstPageNumber;
+
+            for (int pageIndex = 0; pageIndex < recognizedForm.Pages.Count; pageIndex++)
+            {
+                var formPage = recognizedForm.Pages[pageIndex];
+                ValidateFormPage(formPage, includeFieldElements, expectedPageNumber);
+
+                expectedPageNumber++;
+            }
+
+            Assert.NotNull(recognizedForm.Fields);
+
+            foreach (var field in recognizedForm.Fields.Values)
+            {
+                if (field == null)
+                {
+                    continue;
+                }
+
+                Assert.NotNull(field.Name);
+
+                Assert.That(field.Confidence, Is.GreaterThanOrEqualTo(0.0).Within(0.01));
+                Assert.That(field.Confidence, Is.LessThanOrEqualTo(1.0).Within(0.01));
+
+                ValidateFieldData(field.LabelData, includeFieldElements);
+                ValidateFieldData(field.ValueData, includeFieldElements);
+            }
+        }
+
+        private void ValidateFieldData(FieldData fieldData, bool includeFieldElements)
+        {
+            if (fieldData == null)
+            {
+                return;
+            }
+
+            Assert.Greater(fieldData.PageNumber, 0);
+
+            Assert.NotNull(fieldData.BoundingBox.Points);
+
+            if (fieldData.BoundingBox.Points.Length != 0)
+            {
+                Assert.AreEqual(4, fieldData.BoundingBox.Points.Length);
+            }
+
+            Assert.NotNull(fieldData.Text);
+            Assert.NotNull(fieldData.FieldElements);
+
+            if (!includeFieldElements)
+            {
+                Assert.AreEqual(0, fieldData.FieldElements.Count);
+            }
+        }
+
+        protected void ValidateFormPage(FormPage formPage, bool includeFieldElements, int expectedPageNumber)
+        {
+            Assert.AreEqual(expectedPageNumber, formPage.PageNumber);
+
+            Assert.Greater(formPage.Width, 0.0);
+            Assert.Greater(formPage.Height, 0.0);
+
+            Assert.That(formPage.TextAngle, Is.GreaterThan(-180.0).Within(0.01));
+            Assert.That(formPage.TextAngle, Is.LessThanOrEqualTo(180.0).Within(0.01));
+
+            Assert.NotNull(formPage.Lines);
+
+            if (!includeFieldElements)
+            {
+                Assert.AreEqual(0, formPage.Lines.Count);
+            }
+
+            foreach (var line in formPage.Lines)
+            {
+                Assert.AreEqual(expectedPageNumber, line.PageNumber);
+                Assert.NotNull(line.BoundingBox.Points);
+                Assert.AreEqual(4, line.BoundingBox.Points.Length);
+                Assert.NotNull(line.Text);
+
+                if (line.Appearance != null)
+                {
+                    Assert.IsNotNull(line.Appearance.Style);
+                    Assert.IsTrue(line.Appearance.Style.Name == TextStyleName.Handwriting || line.Appearance.Style.Name == TextStyleName.Other);
+                    Assert.Greater(line.Appearance.Style.Confidence, 0f);
+                }
+
+                Assert.NotNull(line.Words);
+                Assert.Greater(line.Words.Count, 0);
+
+                foreach (var word in line.Words)
+                {
+                    Assert.AreEqual(expectedPageNumber, word.PageNumber);
+                    Assert.NotNull(word.BoundingBox.Points);
+                    Assert.AreEqual(4, word.BoundingBox.Points.Length);
+                    Assert.NotNull(word.Text);
+
+                    Assert.That(word.Confidence, Is.GreaterThanOrEqualTo(0.0).Within(0.01));
+                    Assert.That(word.Confidence, Is.LessThanOrEqualTo(1.0).Within(0.01));
+                }
+            }
+
+            Assert.NotNull(formPage.Tables);
+
+            foreach (var table in formPage.Tables)
+            {
+                Assert.AreEqual(expectedPageNumber, table.PageNumber);
+                Assert.Greater(table.ColumnCount, 0);
+                Assert.Greater(table.RowCount, 0);
+                Assert.AreEqual(4, table.BoundingBox.Points.Count());
+
+                Assert.NotNull(table.Cells);
+
+                foreach (var cell in table.Cells)
+                {
+                    Assert.AreEqual(expectedPageNumber, cell.PageNumber);
+                    Assert.NotNull(cell.BoundingBox.Points);
+                    Assert.AreEqual(4, cell.BoundingBox.Points.Length);
+
+                    Assert.GreaterOrEqual(cell.ColumnIndex, 0);
+                    Assert.GreaterOrEqual(cell.RowIndex, 0);
+                    Assert.GreaterOrEqual(cell.ColumnSpan, 1);
+                    Assert.GreaterOrEqual(cell.RowSpan, 1);
+
+                    Assert.That(cell.Confidence, Is.GreaterThanOrEqualTo(0.0).Within(0.01));
+                    Assert.That(cell.Confidence, Is.LessThanOrEqualTo(1.0).Within(0.01));
+
+                    Assert.NotNull(cell.Text);
+                    Assert.NotNull(cell.FieldElements);
+
+                    if (!includeFieldElements)
+                    {
+                        Assert.AreEqual(0, cell.FieldElements.Count);
+                    }
+
+                    foreach (var element in cell.FieldElements)
+                    {
+                        Assert.AreEqual(expectedPageNumber, element.PageNumber);
+                        Assert.NotNull(element.BoundingBox.Points);
+                        Assert.AreEqual(4, element.BoundingBox.Points.Length);
+
+                        Assert.NotNull(element.Text);
+                        Assert.True(element is FormWord || element is FormLine);
+                    }
+                }
+            }
+
+            Assert.NotNull(formPage.SelectionMarks);
+
+            foreach (var selectionMark in formPage.SelectionMarks)
+            {
+                Assert.AreEqual(expectedPageNumber, selectionMark.PageNumber);
+                Assert.NotNull(selectionMark.BoundingBox.Points);
+                Assert.AreEqual(4, selectionMark.BoundingBox.Points.Length);
+                Assert.IsNull(selectionMark.Text);
+                Assert.NotNull(selectionMark.State);
+                Assert.That(selectionMark.Confidence, Is.GreaterThanOrEqualTo(0.0).Within(0.01));
+                Assert.That(selectionMark.Confidence, Is.LessThanOrEqualTo(1.0).Within(0.01));
+            }
         }
     }
 }
