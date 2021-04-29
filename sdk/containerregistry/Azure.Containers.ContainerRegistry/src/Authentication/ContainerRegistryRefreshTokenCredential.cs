841a9396168f475eb67b230224e1745f117b3843
Anne Loomis Thompson <annelo@microsoft.com>
2021-04-29 14:27:33 -0700

    WIP

---
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Authentication/ContainerRegistryRefreshTokenCredential.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+
+namespace Azure.Containers.ContainerRegistry
+{
+    internal class ContainerRegistryRefreshTokenCredential : TokenCredential
+    {
+        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
