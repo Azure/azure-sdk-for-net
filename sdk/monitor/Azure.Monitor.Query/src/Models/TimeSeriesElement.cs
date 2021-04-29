f9bca912772afd21c39f579d303b66b727282601
Pavel Krymets <pavel@krymets.com>
2021-04-29 11:06:51 -0700

    comment

--- sdk/monitor/Azure.Monitor.Query/src/Models/TimeSeriesElement.cs
+++ sdk/monitor/Azure.Monitor.Query/src/Models/TimeSeriesElement.cs
@@ -11,7 +11,9 @@ namespace Azure.Monitor.Query.Models
         private Dictionary<string,string> _metadata;
         private IReadOnlyList<MetadataValue> Metadatavalues { get; }
 
-        /// <summary> the metadata values returned if $filter was specified in the call. </summary>
+        /// <summary>
+        /// The metadata values returned if <see cref="MetricQueryOptions.Filter"/> was specified in the call.
+        /// </summary>
         public IReadOnlyDictionary<string, string> Metadata => _metadata ??= Metadatavalues.ToDictionary(m => m.Name.Value, m => m.Value);
     }
 }
