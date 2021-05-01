32767e018563b3219e591cd9da73b2a873b26fb8
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2021-04-30 17:24:25 -0700

    GetGenericResourceOperations extension method implementation

---
+++ sdk/resourcemanager/Azure.ResourceManager.Core/src/Extensions/ARMClientExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.ResourceManager.Core
+{
+    /// <summary>
+    /// A class of extensions for the ARM Client.
+    /// </summary>
+    public static class ARMClientExtensions
+    {
+        /// <summary>
+        /// Get the operations for an specif resource.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="id">The ID of the resource to retrieve.</param>
+        /// <returns></returns>
+        public static GenericResourceOperations GetGenericResourceOperations(this ArmClient client, string id)
+        {
+            return new GenericResourceOperations(client.DefaultSubscription, id);
+        }
+    }
+}
