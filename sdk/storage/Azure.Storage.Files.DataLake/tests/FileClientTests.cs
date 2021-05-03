55d5700d097e7f3e5e36c7508e37fc053db7ffcc
Sean McCullough <seanmcc@microsoft.com>
2021-05-03 14:05:25 -0500

    Fixed CI

--- sdk/storage/Azure.Storage.Files.DataLake/tests/FileClientTests.cs
+++ sdk/storage/Azure.Storage.Files.DataLake/tests/FileClientTests.cs
@@ -3762,7 +3762,7 @@ namespace Azure.Storage.Files.DataLake.Tests
         [Test]
         [ServiceVersion(Min = DataLakeClientOptions.ServiceVersion.V2020_08_04)]
         [PlaybackOnly("https://github.com/Azure/azure-sdk-for-net/issues/19575")]
-        [PlaybackOnly("https://github.com/Azure/azure-sdk-for-net/issues/20758")]
+        [Ignore("https://github.com/Azure/azure-sdk-for-net/issues/20758")]
         [RetryOnException(TestConstants.QuickQueryRetryCount, typeof(IOException))]
         public async Task QueryAsync_ParquetConfigurationInput()
         {
@@ -3794,7 +3794,7 @@ namespace Azure.Storage.Files.DataLake.Tests
         [Test]
         [ServiceVersion(Min = DataLakeClientOptions.ServiceVersion.V2020_08_04)]
         [PlaybackOnly("https://github.com/Azure/azure-sdk-for-net/issues/19575")]
-        [PlaybackOnly("https://github.com/Azure/azure-sdk-for-net/issues/20758")]
+        [Ignore("https://github.com/Azure/azure-sdk-for-net/issues/20758")]
         [RetryOnException(TestConstants.QuickQueryRetryCount, typeof(IOException))]
         public async Task QueryAsync_ParquetConfigurationOutputError()
         {
