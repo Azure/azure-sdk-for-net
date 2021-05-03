d3d7253d1241c5d4cbe02c5c2d7885c8d9adf18d
sacheu <sacheu@microsoft.com>
2021-05-02 21:09:06 -0700

    Fix SKIP_SMS_TEST logic

--- sdk/communication/Azure.Communication.Common/tests/CommunicationTestEnvironment.cs
+++ sdk/communication/Azure.Communication.Common/tests/CommunicationTestEnvironment.cs
@@ -35,7 +35,7 @@ namespace Azure.Communication.Tests
 
         public bool ShouldIgnoreTests => TestPackagesEnabled != TestPackagesEnabledDefaultValue
             && TestPackagesEnabled != ExpectedTestPackagesEnabled
-            && !bool.Parse(SkipSmsTestEnvironmentVariableName);
+            || bool.Parse(SkipSmsTestEnvironmentVariableName);
 
         public string TestPackagesEnabled => GetTestPackageEnabled();
 
