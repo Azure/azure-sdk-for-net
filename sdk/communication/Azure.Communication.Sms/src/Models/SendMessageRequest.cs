8327223ef73a0c0b9dcf68eee5297ef93b26ea02
Paola Vicencio <pvicencio@microsoft.com>
2021-05-03 19:36:12 -0500

    adding missing lib

--- sdk/communication/Azure.Communication.Sms/src/Models/SendMessageRequest.cs
+++ sdk/communication/Azure.Communication.Sms/src/Models/SendMessageRequest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Diagnostics.CodeAnalysis;
 using Azure.Core;
 
 namespace Azure.Communication.Sms.Models
