16221bf1c47465d4320909762a748fcc3f24e855
Paola Vicencio <pvicencio@microsoft.com>
2021-05-04 01:54:32 -0500

    re-review of the exclusions

--- sdk/communication/Azure.Communication.Identity/src/Models/CommunicationIdentity.cs
+++ sdk/communication/Azure.Communication.Identity/src/Models/CommunicationIdentity.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Azure.Communication.Identity.Models
 {
+    [ExcludeFromCodeCoverage]
     internal partial class CommunicationIdentity
     {
     }
--- sdk/communication/Azure.Communication.Sms/src/Models/SmsSendResponse.cs
+++ sdk/communication/Azure.Communication.Sms/src/Models/SmsSendResponse.cs
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Diagnostics.CodeAnalysis;
 using Azure.Core;
 
 namespace Azure.Communication.Sms.Models
 {
+    [ExcludeFromCodeCoverage]
     [CodeGenModel("SmsSendResponse")]
     internal partial class SmsSendResponse
     {
