6d9cf36f83788caf5aa697f5c31c0d4c0eba1e65
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-05-04 12:45:39 -0700

    PR FB

--- sdk/servicebus/Azure.Messaging.ServiceBus/samples/TopicFilters/Program.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/samples/TopicFilters/Program.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Threading.Tasks;
@@ -16,9 +17,9 @@ namespace TopicSubscriptionWithRuleOperationsSample
         private const string TopicName = "TopicSubscriptionWithRuleOperationsSample";
 
         private const string NoFilterSubscriptionName = "NoFilterSubscription";
-        private const string SqlFilterOnlySubscriptionName = "SqlFilterOnlySubscription";
-        private const string SqlFilterWithActionSubscriptionName = "SqlFilterWithActionSubscription";
-        private const string CorrelationFilterSubscriptionName = "CorrelationFilterSubscription";
+        private const string SqlFilterOnlySubscriptionName = "RedSqlFilterSubscription";
+        private const string SqlFilterWithActionSubscriptionName = "BlueSqlFilterWithActionSubscription";
+        private const string CorrelationFilterSubscriptionName = "ImportantCorrelationFilterSubscription";
 
         private static ServiceBusClient s_client;
         private static ServiceBusAdministrationClient s_adminClient;
@@ -57,7 +58,7 @@ namespace TopicSubscriptionWithRuleOperationsSample
                 throw new ArgumentException(
                     "Either a fully qualified namespace or a connection string environment variable must be specified.");
             }
-
+            
             Console.WriteLine($"Creating topic {TopicName}");
             await s_adminClient.CreateTopicAsync(TopicName);
 
@@ -73,37 +74,17 @@ namespace TopicSubscriptionWithRuleOperationsSample
                 new CreateRuleOptions(RuleProperties.DefaultRuleName, new TrueRuleFilter()));
 
             // 2nd Subscription: Add SqlFilter on Subscription 2
-            // Delete Default Rule.
-            // Add the required SqlFilter Rule
-            // Note: Does not apply to this sample but if there are multiple rules configured for a 
-            // single subscription, then one message is delivered to the subscription when any of the 
-            // rule matches. If more than one rules match and if there is no `SqlRuleAction` set for the
-            // rule, then only one message will be delivered to the subscription. If more than one rules
-            // match and there is a `SqlRuleAction` specified for the rule, then one message per `SqlRuleAction`
-            // is delivered to the subscription.
+            // In this scenario, rather than deleting the default rule after creating the subscription,
+            // we will create the subscription along with our desired rule in a single operation.
             Console.WriteLine($"Creating subscription {SqlFilterOnlySubscriptionName}");
-            await s_adminClient.CreateSubscriptionAsync(TopicName, SqlFilterOnlySubscriptionName);
+            await s_adminClient.CreateSubscriptionAsync(
+                new CreateSubscriptionOptions(TopicName, SqlFilterOnlySubscriptionName),
+                new CreateRuleOptions { Name = "RedSqlRule", Filter = new SqlRuleFilter("Color = 'Red'") });
 
-            Console.WriteLine($"SubscriptionName: {SqlFilterOnlySubscriptionName}, Removing Default Rule and Adding SqlFilter");
-            await s_adminClient.DeleteRuleAsync(TopicName, SqlFilterOnlySubscriptionName, RuleProperties.DefaultRuleName);
-            await s_adminClient.CreateRuleAsync(
-                TopicName,
-                SqlFilterOnlySubscriptionName,
-                new CreateRuleOptions {Name = "RedSqlRule", Filter = new SqlRuleFilter("Color = 'Red'")});
-
-
-            // 3rd Subscription: Add SqlFilter and SqlRuleAction on Subscription 3
-            // Delete Default Rule
-            // Add the required SqlFilter Rule and Action
+            // 3rd Subscription: Add the SqlFilter Rule and Action
             Console.WriteLine($"Creating subscription {SqlFilterWithActionSubscriptionName}");
-            await s_adminClient.CreateSubscriptionAsync(TopicName, SqlFilterWithActionSubscriptionName);
-
-            Console.WriteLine(
-                $"SubscriptionName: {SqlFilterWithActionSubscriptionName}, Removing Default Rule and Adding SqlFilter and SqlRuleAction");
-            await s_adminClient.DeleteRuleAsync(TopicName, SqlFilterWithActionSubscriptionName, RuleProperties.DefaultRuleName);
-            await s_adminClient.CreateRuleAsync(
-                TopicName,
-                SqlFilterWithActionSubscriptionName,
+            await s_adminClient.CreateSubscriptionAsync(
+                new CreateSubscriptionOptions(TopicName, SqlFilterWithActionSubscriptionName),
                 new CreateRuleOptions
                 {
                     Name = "BlueSqlRule",
@@ -113,16 +94,12 @@ namespace TopicSubscriptionWithRuleOperationsSample
 
             // 4th Subscription: Add Correlation Filter on Subscription 4
             Console.WriteLine($"Creating subscription {CorrelationFilterSubscriptionName}");
-            await s_adminClient.CreateSubscriptionAsync(TopicName, CorrelationFilterSubscriptionName);
-
-            Console.WriteLine($"SubscriptionName: {CorrelationFilterSubscriptionName}, Removing Default Rule and Adding CorrelationFilter");
-            await s_adminClient.DeleteRuleAsync(TopicName, CorrelationFilterSubscriptionName, RuleProperties.DefaultRuleName);
-            await s_adminClient.CreateRuleAsync(
-                TopicName,
-                CorrelationFilterSubscriptionName,
+            await s_adminClient.CreateSubscriptionAsync(
+                new CreateSubscriptionOptions(TopicName, CorrelationFilterSubscriptionName),
                 new CreateRuleOptions
                 {
-                    Name = "ImportantCorrelationRule", Filter = new CorrelationRuleFilter {Subject = "Red", CorrelationId = "important"}
+                    Name = "ImportantCorrelationRule",
+                    Filter = new CorrelationRuleFilter { Subject = "Red", CorrelationId = "important" }
                 });
 
             // Get Rules on Subscription, called here only for one subscription as example
@@ -137,29 +114,23 @@ namespace TopicSubscriptionWithRuleOperationsSample
             await SendMessagesAsync();
 
             // Receive messages from 'NoFilterSubscription'. Should receive all 9 messages 
-            Console.ForegroundColor = ConsoleColor.Yellow;
             await ReceiveMessagesAsync(NoFilterSubscriptionName);
 
-            Console.ForegroundColor = ConsoleColor.Red;
-            // Receive messages from 'sqlFilterOnlySubscription'. Should receive all messages with Color = 'Red' i.e 3 messages
+            // Receive messages from 'SqlFilterOnlySubscription'. Should receive all messages with Color = 'Red' i.e 3 messages
             await ReceiveMessagesAsync(SqlFilterOnlySubscriptionName);
 
-            Console.ForegroundColor = ConsoleColor.Blue;
             // Receive messages from 'SqlFilterWithActionSubscription'. Should receive all messages with Color = 'Blue'
             // i.e 3 messages AND all messages should have color set to 'BlueProcessed'
             await ReceiveMessagesAsync(SqlFilterWithActionSubscriptionName);
 
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
             // Receive messages from 'CorrelationFilterSubscription'. Should receive all messages  with Color = 'Red' and CorrelationId = "important"
             // i.e 1 message
             await ReceiveMessagesAsync(CorrelationFilterSubscriptionName);
             Console.ResetColor();
             
-            Console.WriteLine("======================================================================");
-            Console.WriteLine("Completed Receiving all messages... Press any key to clean up and exit");
-            Console.WriteLine("======================================================================");
-
-            Console.ReadKey();
+            Console.WriteLine("=======================================================================");
+            Console.WriteLine("Completed Receiving all messages. Disposing clients and deleting topic.");
+            Console.WriteLine("=======================================================================");
 
             Console.WriteLine("Disposing sender");
             await s_sender.CloseAsync();
@@ -167,6 +138,7 @@ namespace TopicSubscriptionWithRuleOperationsSample
             await s_client.DisposeAsync();
 
             Console.WriteLine("Deleting topic");
+            
             // Deleting the topic will handle deleting all the subscriptions as well.
             await s_adminClient.DeleteTopicAsync(TopicName);
         }
@@ -174,28 +146,24 @@ namespace TopicSubscriptionWithRuleOperationsSample
         private static async Task SendMessagesAsync()
         {
             Console.WriteLine($"==========================================================================");
-            Console.WriteLine("Sending Messages to Topic");
-            try
-            {
-                await Task.WhenAll(
-                    SendMessageAsync(subject: "Red"),
-                    SendMessageAsync(subject: "Blue"),
-                    SendMessageAsync(subject: "Red", correlationId: "important"),
-                    SendMessageAsync(subject: "Blue", correlationId: "important"),
-                    SendMessageAsync(subject: "Red", correlationId: "notimportant"),
-                    SendMessageAsync(subject: "Blue", correlationId: "notimportant"),
-                    SendMessageAsync(subject: "Green"),
-                    SendMessageAsync(subject: "Green", correlationId: "important"),
-                    SendMessageAsync(subject: "Green", correlationId: "notimportant")
-                );
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
-            }
+            Console.WriteLine("Creating messages to send to Topic");
+            List<ServiceBusMessage> messages = new ();
+            messages.Add(CreateMessage(subject: "Red"));
+            messages.Add(CreateMessage(subject: "Blue"));
+            messages.Add(CreateMessage(subject: "Red", correlationId: "important"));
+            messages.Add(CreateMessage(subject: "Blue", correlationId: "important"));
+            messages.Add(CreateMessage(subject: "Red", correlationId: "notimportant"));
+            messages.Add(CreateMessage(subject: "Blue", correlationId: "notimportant"));
+            messages.Add(CreateMessage(subject: "Green"));
+            messages.Add(CreateMessage(subject: "Green", correlationId: "important"));
+            messages.Add(CreateMessage(subject: "Green", correlationId: "notimportant"));
+
+            Console.WriteLine("Sending messages to send to Topic");
+            await s_sender.SendMessagesAsync(messages);
+            Console.WriteLine($"==========================================================================");
         }
 
-        private static async Task SendMessageAsync(string subject, string correlationId = null)
+        private static ServiceBusMessage CreateMessage(string subject, string correlationId = null)
         {
             ServiceBusMessage message = new() {Subject = subject};
             message.ApplicationProperties.Add("Color", subject);
@@ -205,8 +173,23 @@ namespace TopicSubscriptionWithRuleOperationsSample
                 message.CorrelationId = correlationId;
             }
 
-            await s_sender.SendMessageAsync(message);
-            Console.WriteLine($"Sent Message:: Label: {message.Subject}, CorrelationId: {message.CorrelationId}");
+            PrintMessage(message);
+
+            return message;
+        }
+
+        private static void PrintMessage(ServiceBusMessage message)
+        {
+            Console.ForegroundColor = (ConsoleColor) Enum.Parse(typeof(ConsoleColor), message.Subject);
+            Console.WriteLine($"Created message with color: {message.ApplicationProperties["Color"]}, CorrelationId: {message.CorrelationId}");
+            Console.ResetColor();
+        }
+        
+        private static void PrintReceivedMessage(ServiceBusReceivedMessage message)
+        {
+            Console.ForegroundColor = (ConsoleColor) Enum.Parse(typeof(ConsoleColor), message.Subject);
+            Console.WriteLine($"Received message with color: {message.ApplicationProperties["Color"]}, CorrelationId: {message.CorrelationId}");
+            Console.ResetColor();
         }
 
         private static async Task ReceiveMessagesAsync(string subscriptionName)
@@ -224,8 +207,7 @@ namespace TopicSubscriptionWithRuleOperationsSample
                 var receivedMessage = await subscriptionReceiver.ReceiveMessageAsync(TimeSpan.FromSeconds(1));
                 if (receivedMessage != null)
                 {
-                    receivedMessage.ApplicationProperties.TryGetValue("Color", out object colorProperty);
-                    Console.WriteLine($"Color Property = {colorProperty}, CorrelationId = {receivedMessage.CorrelationId}");
+                    PrintReceivedMessage(receivedMessage);
                     receivedMessageCount++;
                 }
                 else
--- sdk/servicebus/Azure.Messaging.ServiceBus/samples/TopicFilters/README.md
+++ sdk/servicebus/Azure.Messaging.ServiceBus/samples/TopicFilters/README.md
@@ -9,25 +9,25 @@ name: Explore topic filters in Azure Service Bus
 description: This sample shows how to apply topic filters to subscriptions.
 ---
 
-# 
+# Topic Filters
 
 This sample shows how to apply topic filters to subscriptions. 
 
-## What is a Topic filter?
+## What is a Topic Filter?
 
-[Topic filters](https://docs.microsoft.com/azure/service-bus-messaging/topic-filters), or rules, can be applied to subscriptions to allow subscribers to define which messages they want to receive from a topic.
+[Topic filters](https://docs.microsoft.com/azure/service-bus-messaging/topic-filters), or rules, can be applied to subscriptions to allow subscribers to define which messages they want to receive from a topic. For instance, certain subscribers may only be interested in processing messages that fit a certain pattern. Rather than create separate topics for each type of message, or add filtering client side within the application, an application can use a single topic and add filtering logic in the subcriptions to achieve the same result. This is more efficient than filtering client-side as the messages that don't match the filter do not go over the wire. It is also generally more simple and flexible than creating separate topics for each type of message, as it provides a more decoupled architecture between sender and receiver.
 
 ## Sample Code
 
 The sample implements four scenarios:
 
-* Create a subscription with no filters. Technically, all subscriptions are created with the default `TrueFilter`. In the sample, we remove and re-add this filter.
+* Create a subscription with no filters. Technically, all subscriptions are created with the default `TrueFilter`. In the sample, we remove and re-add this filter to demonstrate that all subscriptions will have this filter by default.
 
 * Create a subscription with a SQL filter against a user-defined property. SQL filters hold a SQL-like conditional expression that is evaluated in the broker against user-defined or system properties. If the expression evaluates to `true`, the message is delivered to the subscription.
 
-* Create a subscription with a SQL filter and SQL action. In this scenario, we define a SQL filter along with a SQL-like expression that performs an action on the received message, for any messages that makes it through the filter expression. 
+* Create a subscription with a SQL filter and SQL action. In this scenario, we define a SQL filter along with a SQL expression that performs an action on the received message, for any messages that makes it through the filter expression. 
 
-* Create a subscription with a Correlation filter. A correlation filter provides a strongly typed model for matching against the properties of a received message.
+* Create a subscription with a Correlation filter. A correlation filter provides a strongly typed model for matching against the properties of a received message. Correlation filters are recommended over SQL filters as they are more efficient.
 
 The sample code is further documented inline in the `Program.cs` C# file.
 
@@ -38,7 +38,7 @@ In order to run the sample, you will need a Service Bus Namespace. For more info
 
 To build the sample:
 
-1. Install [.NET Core 3.1](https://dot.net) or newer.
+1. Install [.NET 5.0](https://dot.net) or newer.
 
 2. Run in the project directory:
 
