035e4c03b1ce2bd140edfeb8482c4c9138e75134
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-05-03 23:30:30 -0700

    Remove references to LinkCloseMode.

--- sdk/servicebus/Azure.Messaging.ServiceBus/src/Receiver/ServiceBusReceiver.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/src/Receiver/ServiceBusReceiver.cs
@@ -1100,7 +1100,7 @@ namespace Azure.Messaging.ServiceBus
 
         /// <summary>
         /// Performs the task needed to clean up resources used by the <see cref="ServiceBusReceiver" />.
-        /// This is equivalent to calling <see cref="CloseAsync"/> with the default <see cref="LinkCloseMode"/>.
+        /// This is equivalent to calling <see cref="CloseAsync"/>.
         /// </summary>
         ///
         /// <returns>A task to be resolved on when the operation has completed.</returns>
