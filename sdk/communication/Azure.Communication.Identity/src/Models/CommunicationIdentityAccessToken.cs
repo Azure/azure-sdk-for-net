69911b9cbf41ffd6a024c4161603e51eed6384ef
Paola Vicencio <pvicencio@microsoft.com>
2021-05-04 02:39:39 -0500

    updating generated code

--- sdk/communication/Azure.Communication.Identity/src/Models/CommunicationIdentityAccessToken.cs
+++ sdk/communication/Azure.Communication.Identity/src/Models/CommunicationIdentityAccessToken.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Diagnostics.CodeAnalysis;
 using Azure.Core;
 
 namespace Azure.Communication.Identity
 {
+    [ExcludeFromCodeCoverage]
     [CodeGenModel("CommunicationIdentityAccessToken")]
     internal partial class CommunicationIdentityAccessToken
     {
