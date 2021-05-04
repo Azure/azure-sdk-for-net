56d4bd7a7e30a21efb63467d77d22d61ee00248a
Larry Osterman <larryo@microsoft.com>
2021-05-04 12:57:19 -0700

    Updated TPM attestation APIs at the request of the TPM attestation team

--- sdk/attestation/Azure.Security.Attestation/src/Models/TpmAttestationResponse.cs
+++ sdk/attestation/Azure.Security.Attestation/src/Models/TpmAttestationResponse.cs
@@ -7,13 +7,13 @@ using Azure.Core;
 namespace Azure.Security.Attestation
 {
     /// <summary>
-    /// Represents a response for a TPM attestation call. See https://docs.microsoft.com/en-us/azure/attestation/virtualization-based-security-protocol  for more information.
+    /// Represents a response for a TPM attestation call. See <seealso href="https://docs.microsoft.com/en-us/azure/attestation/virtualization-based-security-protocol"/>  for more information.
     /// </summary>
     [CodeGenModel("TpmAttestationResponse")]
-    internal partial class TpmAttestationResponse
+    public partial class TpmAttestationResponse
     {
         /// <summary>
-        /// Attestation Response data. See  https://docs.microsoft.com/en-us/azure/attestation/virtualization-based-security-protocol for more details
+        /// Attestation Response data. See  <seealso href="https://docs.microsoft.com/en-us/azure/attestation/virtualization-based-security-protocol"/> for more details.
         /// </summary>
         public ReadOnlyMemory<byte> Data { get => Base64Url.Decode(InternalData); }
 
