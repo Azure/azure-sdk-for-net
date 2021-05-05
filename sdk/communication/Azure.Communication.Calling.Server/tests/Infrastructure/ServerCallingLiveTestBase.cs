bf6119206d72cf601b99db33a4b05fdafadc404b
zihzhan <zihzhan@microsoft.com>
2021-05-05 13:58:00 -0700

    Onboard Server Calling Test project.

---
+++ sdk/communication/Azure.Communication.Calling.Server/tests/Infrastructure/ServerCallingLiveTestBase.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Communication.Identity;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.Identity;
+using NUnit.Framework;
+
+namespace Azure.Communication.Calling.Server.Tests
+{
+    public class ServerCallingLiveTestBase : RecordedTestBase<ServerCallingTestEnvironment>
+    {
+        public ServerCallingLiveTestBase(bool isAsync) : base(isAsync)
+            => Sanitizer = new ServerCallingRecordedTestSanitizer();
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            if (TestEnvironment.ShouldIgnoreTests)
+            {
+                Assert.Ignore("ServerCalling tests are skipped " +
+                    "because ServerCalling package is not included in the TEST_PACKAGES_ENABLED variable");
+            }
+        }
+
+        public CallClient CreateServerCallingClient()
+        {
+            var connectionString = TestEnvironment.LiveTestConnectionString;
+            CallClient client = new CallClient(connectionString, CreateServerCallingClientOptions());
+
+            #region Snippet:Azure_Communication_ServerCalling_Tests_Samples_CreateServerCallingClient
+            //@@var connectionString = "<connection_string>"; // Find your Communication Services resource in the Azure portal
+            //@@CallClient client = new CallClient(connectionString);
+            #endregion Snippet:Azure_Communication_ServerCalling_Tests_Samples_CreateServerCallingClient
+            return InstrumentClient(client);
+        }
+        public CallClient CreateSmsClientWithToken()
+        {
+            Uri endpoint = TestEnvironment.LiveTestEndpoint;
+            TokenCredential tokenCredential;
+            if (Mode == RecordedTestMode.Playback)
+            {
+                tokenCredential = new MockCredential();
+            }
+            else
+            {
+                #region Snippet:Azure_Communication_ServerCalling_Tests_Samples_CreateServerCallingClientWithToken
+                //@@ string endpoint = "<endpoint_url>";
+                //@@ TokenCredential tokenCredential = new DefaultAzureCredential();
+                /*@@*/tokenCredential = new DefaultAzureCredential();
+                //@@ CallClient client = new CallClient(new Uri(endpoint), tokenCredential);
+                #endregion Snippet:Azure_Communication_ServerCalling_Tests_Samples_CreateServerCallingClientWithToken
+            }
+            CallClient client = new CallClient(endpoint, tokenCredential, CreateServerCallingClientOptions());
+            return InstrumentClient(client);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="CommunicationIdentityClient" /> with the connectionstring via environment
+        /// variables and instruments it to make use of the Azure Core Test Framework functionalities.
+        /// </summary>
+        /// <returns>The instrumented <see cref="CommunicationIdentityClient" />.</returns>
+        protected CommunicationIdentityClient CreateInstrumentedCommunicationIdentityClient()
+            => InstrumentClient(
+                new CommunicationIdentityClient(
+                    TestEnvironment.ConnectionString,
+                    InstrumentClientOptions(new CommunicationIdentityClientOptions())));
+
+        // Todo: add CorrelationVectorLogs
+        private CallClientOptions CreateServerCallingClientOptions()
+        {
+            CallClientOptions callClientOptions = new CallClientOptions();
+            return InstrumentClientOptions(callClientOptions);
+        }
+    }
+}
---
+++ sdk/communication/Azure.Communication.Calling.Server/tests/Infrastructure/ServerCallingTestEnvironment.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Web;
+using Azure.Communication.Tests;
+
+namespace Azure.Communication.Calling.Server.Tests
+{
+    /// <summary>
+    /// A helper class used to retrieve information to be used for tests.
+    /// </summary>
+    public class ServerCallingTestEnvironment : CommunicationTestEnvironment
+    {
+        // please find the allowed package value in tests.yml
+        private const string ServerCallingTestPackagesEnabled = "servercalling";
+
+        public override string ExpectedTestPackagesEnabled { get { return ServerCallingTestPackagesEnabled; } }
+
+        public string ToPhoneNumber => GetRecordedVariable(AzurePhoneNumber);
+
+        public string FromPhoneNumber => GetRecordedVariable(AzurePhoneNumber);
+
+        /// <summary>
+        /// The source identity.
+        /// </summary>
+        public string SourceIdentity => GetRecordedVariable(SourceIdentity);
+
+        /// <summary>
+        /// The phone number associated with the source.
+        /// </summary>
+        public string SourcePhoneNumber => GetRecordedVariable(SourcePhoneNumber);
+
+        /// <summary>
+        /// The base url of the applicaiton.
+        /// </summary>
+        public string AppBaseUrl => GetRecordedVariable(AppBaseUrl);
+
+        /// <summary>
+        /// The audio file name of the play prompt.
+        /// </summary>
+        public string AudioFileName => GetRecordedVariable(AudioFileName);
+
+        /// <summary>
+        /// The secret for validating incoming request.
+        /// </summary>
+        public string IncomingRequestSecret => GetRecordedVariable(IncomingRequestSecret) ?? "helloworld";
+
+        /// <summary>
+        /// The callback url of the application where notification would be received.
+        /// </summary>
+        public string AppCallbackUrl => $"{AppBaseUrl}/api/incident/callback?SecretKey={HttpUtility.UrlEncode(IncomingRequestSecret)}";
+
+        /// <summary>
+        /// The publicly available url of the audio file which would be played as a prompt.
+        /// </summary>
+        public string AudioFileUrl => $"{AppBaseUrl}/audio/{AudioFileName}";
+    }
+}
