255dab7d3ee3d61ad3edaf5004b3b9d5f8884906
Hai Liu <hailiu@microsoft.com>
2021-05-10 14:53:16 -0700

    remove redundant constructor

--- sdk/botservice/Microsoft.Azure.Management.BotService/src/Customizations/EmailChannelProperties.cs
+++
@@ -1,7 +0,0 @@
-namespace Microsoft.Azure.Management.BotService.Models
-{
-    public partial class EmailChannelProperties
-    {
-        public EmailChannelProperties(string emailAddress, string password, bool isEnabled): this(emailAddress, isEnabled, password) {}
-    }
-}
