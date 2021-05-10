dcbdcf5afd78c4c182f30befae3f3d6b7efb9ff4
Christopher Scott <chriss@microsoft.com>
2021-05-10 09:32:25 -0500

    missed the renames

---
+++ sdk/confidentialledger/Azure.Storage.ConfidentialLedger/src/ConfidentialLedgerConstants.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Storage.ConfidentialLedger
+{
+    public class ConfidentialLedgerConstants
+    {
+        public const string TransactionId = "x-ms-ccf-transaction-id";
+    }
+}
