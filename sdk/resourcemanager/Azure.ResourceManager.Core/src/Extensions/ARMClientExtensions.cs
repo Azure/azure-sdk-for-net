15c7b15d403a7bf2d5002c173cd6c00ac337079f
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2021-05-12 22:02:46 -0700

    GetGenericResourceOperations for single ID

--- sdk/resourcemanager/Azure.ResourceManager.Core/src/Extensions/ARMClientExtensions.cs
+++ sdk/resourcemanager/Azure.ResourceManager.Core/src/Extensions/ARMClientExtensions.cs
@@ -3,25 +3,23 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Azure.ResourceManager.Core
 {
     /// <summary>
     /// A class of extensions for the ARM Client.
     /// </summary>
-    public static class ARMClientExtensions
+    public static class ArmClientExtensions
     {
         /// <summary>
-        /// Get the operations for an specif resource.
+        /// Get the operations for a list of specific resources.
         /// </summary>
         /// <param name="client"></param>
         /// <param name="ids">A list of the IDs of the resources to retrieve.</param>
         /// <returns></returns>
         public static List<GenericResourceOperations> GetGenericResourceOperations(this ArmClient client, List<string> ids)
         {
-            if (ids == null || !ids.Any())
+            if (ids == null)
             {
                 throw new ArgumentNullException(nameof(ids));
             }
@@ -32,6 +30,22 @@ namespace Azure.ResourceManager.Core
                 genericRespirceOperations.Add(new GenericResourceOperations(client.DefaultSubscription, id));
             }
             return genericRespirceOperations;
+        }
+
+        /// <summary>
+        /// Get the operations for an specific resource.
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="id">The ID of the resource to retrieve.</param>
+        /// <returns></returns>
+        public static GenericResourceOperations GetGenericResourceOperations(this ArmClient client, string id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            return new GenericResourceOperations(client.DefaultSubscription, id);
         }
     }
 }
