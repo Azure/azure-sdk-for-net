c37f38b57f3dc681552e4532f56cce3bb393bb81
Anne Thompson <annelo@microsoft.com>
2021-05-18 09:35:49 -0700

    Updates for ACR Beta 3 (#21123)
    * flattening writable properties
    
    * correct build
    
    * make writeable properties writeable
    
    * API updates per consistency
    
    * make created on and last updated on required
    
    * add registry field back to model types
    
    * update registry name to registry login server name
    
    * follow-up
    
    * add constructors to property types
    
    * bug fix
    
    * missed API removal
    
    * regen API and some whitespace nits
    
    * Update sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Models/RepositoryProperties.cs
    
    Co-authored-by: Christopher Scott <chriscott@hotmail.com>
    
    * Update sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Models/RepositoryProperties.cs
    
    Co-authored-by: Christopher Scott <chriscott@hotmail.com>
    
    * pr fb
    
    Co-authored-by: Christopher Scott <chriscott@hotmail.com>

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Generated/Models/RepositoryWriteableProperties.Serialization.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Generated/Models/RepositoryWriteableProperties.Serialization.cs
@@ -10,7 +10,7 @@ using Azure.Core;
 
 namespace Azure.Containers.ContainerRegistry
 {
-    public partial class RepositoryWriteableProperties : IUtf8JsonSerializable
+    internal partial class RepositoryWriteableProperties : IUtf8JsonSerializable
     {
         void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
         {
@@ -41,69 +41,6 @@ namespace Azure.Containers.ContainerRegistry
                 writer.WriteBooleanValue(TeleportEnabled.Value);
             }
             writer.WriteEndObject();
-        }
-
-        internal static RepositoryWriteableProperties DeserializeRepositoryWriteableProperties(JsonElement element)
-        {
-            Optional<bool> deleteEnabled = default;
-            Optional<bool> writeEnabled = default;
-            Optional<bool> listEnabled = default;
-            Optional<bool> readEnabled = default;
-            Optional<bool> teleportEnabled = default;
-            foreach (var property in element.EnumerateObject())
-            {
-                if (property.NameEquals("deleteEnabled"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    deleteEnabled = property.Value.GetBoolean();
-                    continue;
-                }
-                if (property.NameEquals("writeEnabled"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    writeEnabled = property.Value.GetBoolean();
-                    continue;
-                }
-                if (property.NameEquals("listEnabled"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    listEnabled = property.Value.GetBoolean();
-                    continue;
-                }
-                if (property.NameEquals("readEnabled"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    readEnabled = property.Value.GetBoolean();
-                    continue;
-                }
-                if (property.NameEquals("teleportEnabled"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    teleportEnabled = property.Value.GetBoolean();
-                    continue;
-                }
-            }
-            return new RepositoryWriteableProperties(Optional.ToNullable(deleteEnabled), Optional.ToNullable(writeEnabled), Optional.ToNullable(listEnabled), Optional.ToNullable(readEnabled), Optional.ToNullable(teleportEnabled));
         }
     }
 }
