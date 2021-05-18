b8e4c60629ceb7eb9622dd5e706f2da62f0018e7
Anne Thompson <annelo@microsoft.com>
2021-05-18 09:20:50 -0700

    Update sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Models/RepositoryProperties.cs
    Co-authored-by: Christopher Scott <chriscott@hotmail.com>

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Models/RepositoryProperties.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Models/RepositoryProperties.cs
@@ -23,7 +23,7 @@ namespace Azure.Containers.ContainerRegistry
         public bool? CanList { get; set; }
         /// <summary> Read enabled. </summary>
         public bool? CanRead { get; set; }
-        /// <summary> Enables Teleport functionality on new images in the repository improving Container startup performance. </summary>
+        /// <summary> Gets or sets whether Teleport functionality is enabled on new images in the repository. Setting this to true can improve Container startup performance. </summary>
         public bool? TeleportEnabled { get; set; }
     }
 }
