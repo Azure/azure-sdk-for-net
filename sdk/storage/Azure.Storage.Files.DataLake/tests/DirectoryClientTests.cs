2043d61adc3dcbfe863f111536d4f8d7b7b8d33d
Amanda Nguyen <amnguye@microsoft.com>
2021-05-18 15:03:45 -0700

    Change Datalake tests to expect ResourceNotFound for older service versions

--- sdk/storage/Azure.Storage.Files.DataLake/tests/DirectoryClientTests.cs
+++ sdk/storage/Azure.Storage.Files.DataLake/tests/DirectoryClientTests.cs
@@ -451,7 +451,7 @@ namespace Azure.Storage.Files.DataLake.Tests
                 e => Assert.AreEqual(
                     _serviceVersion >= DataLakeClientOptions.ServiceVersion.V2019_12_12 ?
                         "NoAuthenticationInformation" :
-                        "AuthenticationFailed",
+                        "ResourceNotFound",
                     e.ErrorCode));
         }
 
--- sdk/storage/Azure.Storage.Files.DataLake/tests/FileSystemClientTests.cs
+++ sdk/storage/Azure.Storage.Files.DataLake/tests/FileSystemClientTests.cs
@@ -460,7 +460,7 @@ namespace Azure.Storage.Files.DataLake.Tests
                 e => Assert.AreEqual(
                     _serviceVersion >= DataLakeClientOptions.ServiceVersion.V2019_12_12 ?
                         "NoAuthenticationInformation" :
-                        "AuthenticationFailed",
+                        "ResourceNotFound",
                     e.ErrorCode));
         }
 
@@ -504,7 +504,7 @@ namespace Azure.Storage.Files.DataLake.Tests
                 e => Assert.AreEqual(
                     _serviceVersion >= DataLakeClientOptions.ServiceVersion.V2019_12_12 ?
                         "NoAuthenticationInformation" :
-                        "AuthenticationFailed",
+                        "ResourceNotFound",
                     e.ErrorCode));
         }
 
