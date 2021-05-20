87cbdece2fd5dfa0a65b6fd65324c59b55142de8
Amanda Nguyen <amnguye@microsoft.com>
2021-05-20 14:55:26 -0700

    Revert Stream ValidationParameters, added pragma warnings; Rerecorded CanParseLargeContentRange tests

--- sdk/storage/Azure.Storage.Common/src/Shared/LazyLoadingReadOnlyStream.cs
+++ sdk/storage/Azure.Storage.Common/src/Shared/LazyLoadingReadOnlyStream.cs
@@ -233,17 +233,23 @@ namespace Azure.Storage
 
             if (offset < 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(offset), $"{nameof(offset)} cannot be less than 0.");
+#pragma warning disable CA2208 // Instantiate argument exceptions correctly
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} cannot be less than 0.");
+#pragma warning restore CA2208 // Instantiate argument exceptions correctly
             }
 
             if (offset > buffer.Length)
             {
-                throw new ArgumentOutOfRangeException(nameof(offset), $"{nameof(offset)} cannot exceed {nameof(buffer)} length.");
+#pragma warning disable CA2208 // Instantiate argument exceptions correctly
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} cannot exceed {nameof(buffer)} length.");
+#pragma warning restore CA2208 // Instantiate argument exceptions correctly
             }
 
             if (count < 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(count), $"{nameof(count)} cannot be less than 0.");
+#pragma warning disable CA2208 // Instantiate argument exceptions correctly
+                throw new ArgumentOutOfRangeException($"{nameof(count)} cannot be less than 0.");
+#pragma warning restore CA2208 // Instantiate argument exceptions correctly
             }
         }
 
--- sdk/storage/Azure.Storage.Common/src/Shared/StorageWriteStream.cs
+++ sdk/storage/Azure.Storage.Common/src/Shared/StorageWriteStream.cs
@@ -188,22 +188,30 @@ namespace Azure.Storage.Shared
 
             if (offset < 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(offset), $"{nameof(offset)} cannot be less than 0.");
+#pragma warning disable CA2208 // Instantiate argument exceptions correctly
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} cannot be less than 0.");
+#pragma warning restore CA2208 // Instantiate argument exceptions correctly
             }
 
             if (offset > buffer.Length)
             {
-                throw new ArgumentOutOfRangeException(nameof(offset), $"{nameof(offset)} cannot be greater than {nameof(buffer)} length.");
+#pragma warning disable CA2208 // Instantiate argument exceptions correctly
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} cannot be greater than {nameof(buffer)} length.");
+#pragma warning restore CA2208 // Instantiate argument exceptions correctly
             }
 
             if (count < 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(count), $"{nameof(count)} cannot be less than 0.");
+#pragma warning disable CA2208 // Instantiate argument exceptions correctly
+                throw new ArgumentOutOfRangeException($"{nameof(count)} cannot be less than 0.");
+#pragma warning restore CA2208 // Instantiate argument exceptions correctly
             }
 
             if (offset + count > buffer.Length)
             {
-                throw new ArgumentOutOfRangeException($"{nameof(offset)} and {nameof(count)}", $"{nameof(offset)} + {nameof(count)} cannot exceed {nameof(buffer)} length.");
+#pragma warning disable CA2208 // Instantiate argument exceptions correctly
+                throw new ArgumentOutOfRangeException($"{nameof(offset)} + {nameof(count)} cannot exceed {nameof(buffer)} length.");
+#pragma warning restore CA2208 // Instantiate argument exceptions correctly
             }
         }
 
