10a6c20ea56f4a24403ce7fe41c31a5ec89fdc55
Yang Chen <yachen4@microsoft.com>
2021-05-20 17:27:00 -0700

    Overwrite Vision.Face with public preview client
    Replace the client content in Vision.Face with the public preview
    client generated from Face/preview/v1.0-preview/Face.json that points
    to face/v1.0-preview endpoints.

---
+++ sdk/cognitiveservices/Vision.Face/src/Customizations/FaceClientPersonDirectoryCustomization.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.CognitiveServices.Vision.Face
+{
+    public partial class FaceClient
+    {
+        partial void CustomInitialize()
+        {
+            PersonDirectoryExtensions.asyncOperationAction = this.Snapshot.GetOperationStatusAsync;
+        }
+    }
+}
