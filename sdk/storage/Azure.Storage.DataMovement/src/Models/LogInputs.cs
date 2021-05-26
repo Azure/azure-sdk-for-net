74b37194b408b471e2e200fce48264064b8389d9
Amanda Nguyen <amnguye@microsoft.com>
2021-05-26 13:25:24 -0700

    Create packages for DM Common and Blobs

---
+++ sdk/storage/Azure.Storage.DataMovement/src/Models/LogInputs.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Storage.DataMovement.Models
+{
+    internal partial class LogInputs
+    {
+    }
+}
