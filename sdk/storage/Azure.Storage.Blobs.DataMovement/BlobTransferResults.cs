11a32787e3d20cbb208b7b0de6d807d3839511b2
Amanda Nguyen <amnguye@microsoft.com>
2021-05-26 13:25:24 -0700

    Create packages for DM Common and Blobs

---
+++ sdk/storage/Azure.Storage.Blobs.DataMovement/BlobTransferResults.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Azure.Storage.Blobs.DataMovement
+{
+    /// <summary>
+    /// BlobTransferResults
+    /// </summary>
+    public class BlobTransferResults
+    {
+    }
+}
---
+++ sdk/storage/Azure.Storage.Common.DataMovement/StorageTransferManager.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Storage.Common;
+
+namespace Azure.Storage.Common.DataMovement
+{
+    /// <summary>
+    /// TransferManager class
+    /// </summary>
+    public class StorageTransferManager
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public StorageTransferManager()
+        {
+        }
+    }
+}
