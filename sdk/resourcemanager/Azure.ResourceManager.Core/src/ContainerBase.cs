d022ceaf315d57d16a4769cdf20d99d65c05f5f7
m-nash <64171366+m-nash@users.noreply.github.com>
2021-05-26 18:33:18 -0700

    update to allow external access to a constructor that doesn't have an operation base (#21395)
    * Add lambda wrapper in armclient for getting container objects in other RPs
    
    * update xml comments
    
    * update to add externally accesible constructor that doesn't have an operation base like armclient as the parent

--- sdk/resourcemanager/Azure.ResourceManager.Core/src/ContainerBase.cs
+++ sdk/resourcemanager/Azure.ResourceManager.Core/src/ContainerBase.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
 namespace Azure.ResourceManager.Core
 {
     /// <summary>
@@ -31,6 +35,18 @@ namespace Azure.ResourceManager.Core
         /// <param name="id"> The identifier of the resource that is the target of operations. </param>
         internal ContainerBase(ClientContext clientContext, ResourceIdentifier id)
             : base(clientContext, id)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContainerBase"/> class.
+        /// </summary>
+        /// <param name="options"> The options to use. </param>
+        /// <param name="credential"> The credential to use. </param>
+        /// <param name="baseUri"> The base uri to use. </param>
+        /// <param name="pipeline"> The http pipeline policy to use. </param>
+        protected ContainerBase(ArmClientOptions options, TokenCredential credential, Uri baseUri, HttpPipeline pipeline)
+            : this(new ClientContext(options, credential, baseUri, pipeline))
         {
         }
 
