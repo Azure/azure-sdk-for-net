d4bfc4721207ced7270d9864262661ef8e1114e7
Paresh Arvind Patil <papati@microsoft.com>
2021-05-27 16:30:22 -0700

    Updated the PlayAudio url description

--- sdk/communication/Azure.Communication.Calling.Server/src/Generated/Models/PlayAudioRequest.cs
+++ sdk/communication/Azure.Communication.Calling.Server/src/Generated/Models/PlayAudioRequest.cs
@@ -31,7 +31,7 @@ namespace Azure.Communication.Calling.Server
         public string OperationContext { get; set; }
         /// <summary> An id for the media in the AudioFileUri, using which we cache the media resource. </summary>
         public string AudioFileId { get; set; }
-        /// <summary> The callback URI. </summary>
+        /// <summary> The callback Uri to recieve PlayAudio status notifications. </summary>
         public string CallbackUri { get; set; }
     }
 }
