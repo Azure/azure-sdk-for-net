5ef9ed5a49db5445aa12b2e681c4a20b2dad4387
Paresh Arvind Patil <papati@microsoft.com>
2021-05-28 12:56:34 -0700

    Re-build the swagger updating the auto-genrated files

--- sdk/communication/Azure.Communication.Calling.Server/src/Generated/Models/PlayAudioRequest.Serialization.cs
+++ sdk/communication/Azure.Communication.Calling.Server/src/Generated/Models/PlayAudioRequest.Serialization.cs
@@ -35,11 +35,6 @@ namespace Azure.Communication.Calling.Server
                 writer.WritePropertyName("audioFileId");
                 writer.WriteStringValue(AudioFileId);
             }
-            if (Optional.IsDefined(CallbackUri))
-            {
-                writer.WritePropertyName("callbackUri");
-                writer.WriteStringValue(CallbackUri);
-            }
             writer.WriteEndObject();
         }
     }
