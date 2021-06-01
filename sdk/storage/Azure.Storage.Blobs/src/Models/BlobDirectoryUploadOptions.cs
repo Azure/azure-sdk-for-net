ac2bba218d5cb6d702b0529d79bf6a8fc473a936
Amanda Nguyen <amnguye@microsoft.com>
2021-06-01 13:06:20 -0700

    WIP - Added BlobDirectoryUploadOptions and StorageTransferStatus

---
+++ sdk/storage/Azure.Storage.Blobs/src/Models/BlobDirectoryUploadOptions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Storage.Blobs.Models
+using Metadata = System.Collections.Generic.IDictionary<string, string>;
+using Tags = System.Collections.Generic.IDictionary<string, string>;
+{
+    /// <summary>
+    /// BlobDirectoryUploadOptions for
+    /// </summary>
+    public class BlobDirectoryUploadOptions
+    {
+        /// <summary>
+        /// Optional standard HTTP header properties that can be set for the
+        /// new append blob.
+        /// </summary>
+        public BlobHttpHeaders HttpHeaders { get; set; }
+
+        /// <summary>
+        /// Optional custom metadata to set for this append blob.
+        /// </summary>
+#pragma warning disable CA2227 // Collection properties should be readonly
+        public Metadata Metadata { get; set; }
+#pragma warning restore CA2227 // Collection properties should be readonly
+
+        /// <summary>
+        /// Options tags to set for this block blob.
+        /// </summary>
+#pragma warning disable CA2227 // Collection properties should be readonly
+        public Tags Tags { get; set; }
+#pragma warning restore CA2227 // Collection properties should be readonly
+
+        /// <summary>
+        /// Optional <see cref="BlobRequestConditions"/> to add
+        /// conditions on the upload of this Block Blob.
+        /// </summary>
+        public BlobRequestConditions Conditions { get; set; }
+
+        /// <summary>
+        /// Optional <see cref="IProgress{Long}"/> to provide
+        /// progress updates about data transfers.
+        /// </summary>
+        public IProgress<StorageTransferStatus> ProgressHandler { get; set; }
+
+        /// <summary>
+        /// Optional <see cref="AccessTier"/> to set on the
+        /// Block Blob.
+        /// </summary>
+        public AccessTier? AccessTier { get; set; }
+
+        /// <summary>
+        /// Optional <see cref="StorageTransferOptions"/> to configure
+        /// parallel transfer behavior.
+        /// </summary>
+        public StorageTransferOptions TransferOptions { get; set; }
+        /// <summary>
+        /// Optional. If set to false, the operation will terminate quickly on encountering user failures. If true, the operation will ignore
+        /// user failures and proceed with the operation on other sub-entities of the directory.
+        ///
+        /// This is after retry attempts fail.
+        /// </summary>
+        public bool? ContinueOnFailure { get; set; }
+}
---
+++ sdk/storage/Azure.Storage.Common/src/StorageTransferStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.Storage
+{
+    /// <summary>
+    /// StorageTransferStatus keeps track of the transfer results
+    /// </summary>
+    public class StorageTransferStatus
+    {
+        /// <summary>
+        /// Bytes transferred.
+        /// </summary>
+        public long bytesTransferred { get; set; }
+
+        /// <summary>
+        /// Number of Files or Blobs tranferred.
+        /// </summary>
+        public long successfulTransfers { get; internal set; }
+
+        // TODO: failures and skips
+    }
+}
