e9bbd1201489d3a28ae386f65f13047ca6561de6
Caio Saldanha <camaiaor@microsoft.com>
2021-06-01 21:54:11 -0700

    [MetricsAdvisor] Updated data feed sources to not expose sensitive data (#21447)

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/api/Azure.AI.MetricsAdvisor.netstandard2.0.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/api/Azure.AI.MetricsAdvisor.netstandard2.0.cs
@@ -436,7 +436,6 @@ namespace Azure.AI.MetricsAdvisor.Models
     public partial class AzureApplicationInsightsDataFeedSource : Azure.AI.MetricsAdvisor.Models.DataFeedSource
     {
         public AzureApplicationInsightsDataFeedSource(string applicationId, string apiKey, string azureCloud, string query) { }
-        public string ApiKey { get { throw null; } }
         public string ApplicationId { get { throw null; } }
         public string AzureCloud { get { throw null; } }
         public string Query { get { throw null; } }
@@ -445,27 +444,23 @@ namespace Azure.AI.MetricsAdvisor.Models
     {
         public AzureBlobDataFeedSource(string connectionString, string container, string blobTemplate) { }
         public string BlobTemplate { get { throw null; } }
-        public string ConnectionString { get { throw null; } }
         public string Container { get { throw null; } }
     }
     public partial class AzureCosmosDbDataFeedSource : Azure.AI.MetricsAdvisor.Models.DataFeedSource
     {
         public AzureCosmosDbDataFeedSource(string connectionString, string sqlQuery, string database, string collectionId) { }
         public string CollectionId { get { throw null; } }
-        public string ConnectionString { get { throw null; } }
         public string Database { get { throw null; } }
         public string SqlQuery { get { throw null; } }
     }
     public partial class AzureDataExplorerDataFeedSource : Azure.AI.MetricsAdvisor.Models.DataFeedSource
     {
         public AzureDataExplorerDataFeedSource(string connectionString, string query) { }
-        public string ConnectionString { get { throw null; } }
         public string Query { get { throw null; } }
     }
     public partial class AzureDataLakeStorageGen2DataFeedSource : Azure.AI.MetricsAdvisor.Models.DataFeedSource
     {
         public AzureDataLakeStorageGen2DataFeedSource(string accountName, string accountKey, string fileSystemName, string directoryTemplate, string fileTemplate) { }
-        public string AccountKey { get { throw null; } }
         public string AccountName { get { throw null; } }
         public string DirectoryTemplate { get { throw null; } }
         public string FileSystemName { get { throw null; } }
@@ -474,7 +469,6 @@ namespace Azure.AI.MetricsAdvisor.Models
     public partial class AzureTableDataFeedSource : Azure.AI.MetricsAdvisor.Models.DataFeedSource
     {
         public AzureTableDataFeedSource(string connectionString, string table, string query) { }
-        public string ConnectionString { get { throw null; } }
         public string Query { get { throw null; } }
         public string Table { get { throw null; } }
     }
@@ -873,9 +867,7 @@ namespace Azure.AI.MetricsAdvisor.Models
     public partial class InfluxDbDataFeedSource : Azure.AI.MetricsAdvisor.Models.DataFeedSource
     {
         public InfluxDbDataFeedSource(string connectionString, string database, string username, string password, string query) { }
-        public string ConnectionString { get { throw null; } }
         public string Database { get { throw null; } }
-        public string Password { get { throw null; } }
         public string Query { get { throw null; } }
         public string Username { get { throw null; } }
     }
@@ -1068,13 +1060,11 @@ namespace Azure.AI.MetricsAdvisor.Models
     {
         public MongoDbDataFeedSource(string connectionString, string database, string command) { }
         public string Command { get { throw null; } }
-        public string ConnectionString { get { throw null; } }
         public string Database { get { throw null; } }
     }
     public partial class MySqlDataFeedSource : Azure.AI.MetricsAdvisor.Models.DataFeedSource
     {
         public MySqlDataFeedSource(string connectionString, string query) { }
-        public string ConnectionString { get { throw null; } }
         public string Query { get { throw null; } }
     }
     public partial class NotificationHook
@@ -1107,7 +1097,6 @@ namespace Azure.AI.MetricsAdvisor.Models
     public partial class PostgreSqlDataFeedSource : Azure.AI.MetricsAdvisor.Models.DataFeedSource
     {
         public PostgreSqlDataFeedSource(string connectionString, string query) { }
-        public string ConnectionString { get { throw null; } }
         public string Query { get { throw null; } }
     }
     public partial class ServicePrincipalCredentialEntity : Azure.AI.MetricsAdvisor.Models.DataSourceCredentialEntity
@@ -1150,7 +1139,6 @@ namespace Azure.AI.MetricsAdvisor.Models
     public partial class SqlServerDataFeedSource : Azure.AI.MetricsAdvisor.Models.DataFeedSource
     {
         public SqlServerDataFeedSource(string connectionString, string query) { }
-        public string ConnectionString { get { throw null; } }
         public string Query { get { throw null; } }
     }
     public partial class SuppressCondition
