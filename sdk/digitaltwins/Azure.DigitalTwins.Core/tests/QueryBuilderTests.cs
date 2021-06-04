6f0b4fe6c48beb8843698dddc1ff631be798c594
Nick <t-npatilsen@microsoft.com>
2021-06-04 11:15:25 -0700

    added tests

---
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/tests/QueryBuilderTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.DigitalTwins.Core.QueryBuilder;
+using Azure.DigitalTwins.Core.Queries.QueryBuilders;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Azure.DigitalTwins.Core.Tests
+{
+    public class QueryBuilderTests
+    {
+        [Test]
+        public void SimpleTests()
+        {
+            new AdtQueryBuilder().Select("*").From(AdtCollection.DigitalTwins).ToString().Should().Be("SELECT * FROM DIGITALTWINS");
+        }
+    }
+}
