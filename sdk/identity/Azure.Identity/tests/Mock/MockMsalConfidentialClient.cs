93092d65089a39195a5e44f620d1f85b81814d0f
Christopher Scott <chriss@microsoft.com>
2021-06-04 13:49:43 -0500

    Configure ReplyUri for AuthorizationCodeCredential

--- sdk/identity/Azure.Identity/tests/Mock/MockMsalConfidentialClient.cs
+++ sdk/identity/Azure.Identity/tests/Mock/MockMsalConfidentialClient.cs
@@ -10,26 +10,63 @@ namespace Azure.Identity.Tests.Mock
 {
     internal class MockMsalConfidentialClient : MsalConfidentialClient
     {
-        private Func<string[], AuthenticationResult> _factory = null;
+        internal Func<string[], AuthenticationResult> ClientFactory { get; set; }
+        internal Func<string[], string, string, AuthenticationAccount, ValueTask<AuthenticationResult>> SilentFactory  { get; set; }
+        internal Func<string[], string, string, string, AuthenticationResult> AuthcodeFactory { get; set; }
 
         public MockMsalConfidentialClient(AuthenticationResult result)
         {
-            _factory = (_) => result;
+            ClientFactory = (_) => result;
+            AuthcodeFactory = (_, _, _, _) => result;
         }
 
         public MockMsalConfidentialClient(Exception exception)
         {
-            _factory = (_) => throw exception;
+            ClientFactory = (_) => throw exception;
+            SilentFactory = (_, _, _, _) => throw exception;
+            AuthcodeFactory = (_, _, _, _) => throw exception;
         }
 
-        public MockMsalConfidentialClient(Func<string[], AuthenticationResult> factory)
+        public MockMsalConfidentialClient(Func<string[], AuthenticationResult> clientFactory)
         {
-            _factory = factory;
+            ClientFactory = clientFactory;
+        }
+
+        public MockMsalConfidentialClient(Func<string[], string, string, AuthenticationAccount, ValueTask<AuthenticationResult>> factory)
+        {
+            SilentFactory = factory;
+        }
+
+        public MockMsalConfidentialClient(Func<string[], string, string, string, AuthenticationResult> factory)
+        {
+            AuthcodeFactory = factory;
         }
 
         public override ValueTask<AuthenticationResult> AcquireTokenForClientAsync(string[] scopes, bool async, CancellationToken cancellationToken)
         {
-            return new ValueTask<AuthenticationResult>(_factory(scopes));
+            return new(ClientFactory(scopes));
+        }
+
+        public override async ValueTask<AuthenticationResult> AcquireTokenSilentAsync(
+            string[] scopes,
+            AuthenticationAccount account,
+            string tenantId,
+            string replyUri,
+            bool async,
+            CancellationToken cancellationToken)
+        {
+            return await SilentFactory(scopes, tenantId, replyUri, account);
+        }
+
+        public override ValueTask<AuthenticationResult> AcquireTokenByAuthorizationCodeAsync(
+            string[] scopes,
+            string code,
+            string tenantId,
+            string replyUri,
+            bool async,
+            CancellationToken cancellationToken)
+        {
+            return new(AuthcodeFactory(scopes, tenantId, replyUri, code));
         }
     }
 }
