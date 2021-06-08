f4fb5e40c6c24cd395a0c0e0b75ab98f63300caf
Christopher Scott <chriss@microsoft.com>
2021-06-08 07:53:39 -0700

    accessPolicy is nullable (#21695)

--- sdk/tables/Azure.Data.Tables/src/Generated/Models/TableSignedIdentifier.cs
+++ sdk/tables/Azure.Data.Tables/src/Generated/Models/TableSignedIdentifier.cs
@@ -15,16 +15,12 @@ namespace Azure.Data.Tables.Models
         /// <summary> Initializes a new instance of TableSignedIdentifier. </summary>
         /// <param name="id"> A unique id. </param>
         /// <param name="accessPolicy"> The access policy. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="id"/> or <paramref name="accessPolicy"/> is null. </exception>
+        /// <exception cref="ArgumentNullException"> <paramref name="id"/> is null. </exception>
         public TableSignedIdentifier(string id, TableAccessPolicy accessPolicy)
         {
             if (id == null)
             {
                 throw new ArgumentNullException(nameof(id));
-            }
-            if (accessPolicy == null)
-            {
-                throw new ArgumentNullException(nameof(accessPolicy));
             }
 
             Id = id;
