1504edfe536fa0b5f6de6599f0a0b8943a3efb76
Victor XField <vxfield@outlook.com>
2021-06-11 19:42:01 -0700

    Fixes to properly submit a job

--- sdk/quantum/Azure.Quantum.Jobs/samples/Program.cs
+++ sdk/quantum/Azure.Quantum.Jobs/samples/Program.cs
@@ -2,10 +2,15 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Threading;
 using Azure.Identity;
 using Azure.Quantum.Jobs.Models;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace Azure.Quantum.Jobs.Samples
 {
@@ -15,11 +20,11 @@ namespace Azure.Quantum.Jobs.Samples
         {
             #region Snippet:Azure_Quantum_Jobs_CreateClient
             // Create a QuantumJobClient
-            var subscriptionId = "your_subscription_id";
-            var resourceGroupName = "your_resource_group_name";
-            var workspaceName = "your_quantum_workspace_name";
-            var location = "your_location";
-            var storageContainerName = "your_container_name";
+            var subscriptionId = "916dfd6d-030c-4bd9-b579-7bb6d1926e97";
+            var resourceGroupName = "e2e-scenarios";
+            var workspaceName = "e2e-qsharp-tests";
+            var location = "westus2";
+            var storageContainerName = "csharpsample";
             var credential = new DefaultAzureCredential(true);
 
             var quantumJobClient =
@@ -59,18 +64,54 @@ namespace Azure.Quantum.Jobs.Samples
             Console.WriteLine($@"Uploading data into a blob...");
 
             #region Snippet:Azure_Quantum_Jobs_UploadInputData
+            string problemFilePath = "./problem.json";
+
             // Get input data blob Uri with SAS key
-            string blobName = $"myjobinput.json";
+            string blobName = Path.GetFileName(problemFilePath);
             var inputDataUri = (quantumJobClient.GetStorageSasUri(
                 new BlobDetails(storageContainerName)
                 {
                     BlobName = blobName,
                 })).Value.SasUri;
 
-            // Upload input data to blob
-            var blobClient = new BlobClient(new Uri(inputDataUri));
-            var problemFilename = "problem.json";
-            blobClient.Upload(problemFilename, overwrite: true);
+            using (var problemStreamToUpload = new MemoryStream())
+            {
+                using (FileStream problemFileStream = File.OpenRead(problemFilePath))
+                {
+                    // Check if problem file is a gzip file.
+                    // If it is, just read its contents.
+                    // If not, read and compress the content.
+                    var fileExtension = Path.GetExtension(problemFilePath).ToLower();
+                    if (fileExtension == ".gz" ||
+                        fileExtension == ".gzip")
+                    {
+                        problemFileStream.CopyTo(problemStreamToUpload);
+                    }
+                    else
+                    {
+                        using (var gzip = new GZipStream(problemStreamToUpload, CompressionMode.Compress, leaveOpen: true))
+                        {
+                            byte[] buffer = new byte[8192];
+                            int count;
+                            while ((count = problemFileStream.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                gzip.Write(buffer, 0, count);
+                            }
+                        }
+                    }
+                }
+                problemStreamToUpload.Position = 0;
+
+                // Upload input data to blob
+                var blobClient = new BlobClient(new Uri(inputDataUri));
+                var blobHeaders = new BlobHttpHeaders
+                {
+                    ContentType = "application/json",
+                    ContentEncoding = "gzip"
+                };
+                var blobUploadOptions = new BlobUploadOptions { HttpHeaders = blobHeaders };
+                blobClient.Upload(problemStreamToUpload, options: blobUploadOptions);
+            }
             #endregion
 
             Console.WriteLine($@"Input data Uri with SAS key:
@@ -87,28 +128,44 @@ namespace Azure.Quantum.Jobs.Samples
             var outputDataFormat = "microsoft.qio-results.v2";
             var providerId = "microsoft";
             var target = "microsoft.paralleltempering-parameterfree.cpu";
+            var inputParams = new Dictionary<string, object>() { { "params", "{}" } };
             var createJobDetails = new JobDetails(containerUri, inputDataFormat, providerId, target)
             {
                 Id = jobId,
                 InputDataUri = inputDataUri,
                 Name = jobName,
+                InputParams = inputParams,
                 OutputDataFormat = outputDataFormat
             };
-            JobDetails createdJob = (quantumJobClient.CreateJob(jobId, createJobDetails)).Value;
+
+            JobDetails myJob = (quantumJobClient.CreateJob(jobId, createJobDetails)).Value;
             #endregion
 
             Console.WriteLine($@"Job created:
-    Id: {createdJob.Id}
-    Name: {createdJob.Name}
-    CreationTime: {createdJob.CreationTime}
-    Status: {createdJob.Status}
+    Id: {myJob.Id}
+    Name: {myJob.Name}
+    CreationTime: {myJob.CreationTime}
+    Status: {myJob.Status}
 ");
+
+            Console.WriteLine($@"Awaiting job to complete...");
+            while (myJob.Status == JobStatus.Waiting ||
+                   myJob.Status == JobStatus.Executing)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                myJob = (quantumJobClient.GetJob(jobId)).Value;
+                Console.WriteLine($@"Job status: {myJob.Status}");
+            }
+            if (myJob.Status == JobStatus.Failed)
+            {
+                Console.WriteLine($@"Job has failed with error: {myJob.ErrorData.Message}");
+            }
 
             Console.WriteLine($@"Getting Quantum job...");
 
             #region Snippet:Azure_Quantum_Jobs_GetJob
             // Get the job that we've just created based on its jobId
-            JobDetails myJob = (quantumJobClient.GetJob(jobId)).Value;
+            myJob = (quantumJobClient.GetJob(jobId)).Value;
             #endregion
 
             Console.WriteLine($@"Job obtained:
@@ -122,6 +179,9 @@ namespace Azure.Quantum.Jobs.Samples
     OutputDataFormat: {myJob.OutputDataFormat}
     OutputDataUri: {myJob.OutputDataUri}
 ");
+
+            if (String.IsNullOrEmpty(null))
+                return;
 
             Console.WriteLine($@"Getting list of Quantum jobs...");
 
