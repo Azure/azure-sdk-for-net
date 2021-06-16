831972ec107ae7fc27cf81fcb5fb03cb275b7f13
YalinLi0312 <yall@microsoft.com>
2021-06-15 18:42:50 -0700

    Address comment

--- sdk/resourcemanager/Azure.ResourceManager.Core/tests/Scenario/ResourceGroupBuilderTests.cs
+++ sdk/resourcemanager/Azure.ResourceManager.Core/tests/Scenario/ResourceGroupBuilderTests.cs
@@ -17,6 +17,10 @@ namespace Azure.ResourceManager.Core.Tests
         public void CreateOrUpdate(string value)
         {
             Assert.ThrowsAsync<ArgumentException>(async () => _ = await Client.DefaultSubscription.GetResourceGroups().Construct(LocationData.WestUS2).CreateOrUpdateAsync(value));
+
+            var location = LocationData.WestUS2;
+            var rgData = Client.DefaultSubscription.GetResourceGroups().Construct(location).Build();
+            Assert.AreEqual(location, rgData.Location);
         }
 
         [TestCase(null)]
