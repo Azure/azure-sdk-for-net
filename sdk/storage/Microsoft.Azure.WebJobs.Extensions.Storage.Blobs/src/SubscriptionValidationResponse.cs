26c9e5fa3b09370fc27596577d13ebb9b2fd2e18
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-06-21 11:56:59 -0700

    Shadow type

---
+++ sdk/storage/Microsoft.Azure.WebJobs.Extensions.Storage.Blobs/src/SubscriptionValidationResponse.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Messaging.EventGrid.SystemEvents
+{
+    internal class SubscriptionValidationResponse
+    {
+        /// <summary> Initializes a new instance of SubscriptionValidationResponse. </summary>
+        /// <param name="validationResponse"> The validation response sent by the subscriber to Azure Event Grid to complete the validation of an event subscription. </param>
+        public SubscriptionValidationResponse(string validationResponse)
+        {
+            ValidationResponse = validationResponse;
+        }
+
+        /// <summary> The validation response sent by the subscriber to Azure Event Grid to complete the validation of an event subscription. </summary>
+        public string ValidationResponse { get; set; }
+    }
+}
