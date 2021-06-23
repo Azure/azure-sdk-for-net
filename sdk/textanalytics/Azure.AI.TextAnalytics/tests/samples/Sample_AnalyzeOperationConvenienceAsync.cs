516f9a6f04a71f0f1be00414d279e271e2efb4ed
Mariana Rios Flores <mariari@microsoft.com>
2021-06-23 12:56:37 -0700

    enable tests and add action name to linked entitiy samples

--- sdk/textanalytics/Azure.AI.TextAnalytics/tests/samples/Sample_AnalyzeOperationConvenienceAsync.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/tests/samples/Sample_AnalyzeOperationConvenienceAsync.cs
@@ -140,6 +140,7 @@ namespace Azure.AI.TextAnalytics.Samples
                 docNumber = 1;
                 foreach (RecognizeLinkedEntitiesActionResult linkedEntitiesActionResults in entityLinkingResults)
                 {
+                    Console.WriteLine($" Action name: {linkedEntitiesActionResults.ActionName}");
                     foreach (RecognizeLinkedEntitiesResult documentResults in linkedEntitiesActionResults.DocumentsResults)
                     {
                         Console.WriteLine($" Document #{docNumber++}");
