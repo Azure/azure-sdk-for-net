3ab64089eed3b04d26d549d70ca36eb4f9d30f1b
Aashish Rajaram Salokhe <aasalokh@microsoft.com>
2021-06-24 21:24:56 -0700

    Recreate the generated folder

--- sdk/guestconfiguration/Microsoft.Azure.Management.GuestConfiguration/src/Generated/SdkInfo_GuestConfigurationClient.cs
+++ sdk/guestconfiguration/Microsoft.Azure.Management.GuestConfiguration/src/Generated/SdkInfo_GuestConfigurationClient.cs
@@ -29,16 +29,5 @@ namespace Microsoft.Azure.Management.GuestConfiguration
               }.AsEnumerable();
           }
       }
-
-    // BEGIN: Code Generation Metadata Section
-    public static readonly String AutoRestVersion = "v2";
-    public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-    public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/guestconfiguration/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\data\\azure-sdk-for-net\\sdk --tag=package-2021-01-25";
-    public static readonly String GithubForkName = "Azure";
-    public static readonly String GithubBranchName = "master";
-    public static readonly String GithubCommidId = "a46829ba5596efd0670f5c7b2178834bfe912c76";
-    public static readonly String CodeGenerationErrors = "";
-    public static readonly String GithubRepoName = "azure-rest-api-specs";
-    // END: Code Generation Metadata Section
-    }
+  }
 }
