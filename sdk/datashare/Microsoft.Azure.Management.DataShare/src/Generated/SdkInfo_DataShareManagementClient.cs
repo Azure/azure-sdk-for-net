16da3fd5bf70f81760626983cd601efe802f9a32
SDKAuto <sdkautomation@microsoft.com>
2021-06-28 09:01:11 +0000

    CodeGen from PR 15001 in Azure/azure-rest-api-specs [python]track2 config (#15001)
    * track2 config
    
    * directive for required type in readme.python.md
    
    * Update readme.md
    
    * Update readme.python.md
    
    * Update readme.python.md

--- sdk/datashare/Microsoft.Azure.Management.DataShare/src/Generated/SdkInfo_DataShareManagementClient.cs
+++ sdk/datashare/Microsoft.Azure.Management.DataShare/src/Generated/SdkInfo_DataShareManagementClient.cs
@@ -34,16 +34,5 @@ namespace Microsoft.Azure.Management.DataShare
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/datashare/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\Users\\ahermas\\source\\repos\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "3f3a82ed7731b6a212c0a62c7798259d44c99181";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
