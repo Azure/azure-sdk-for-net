98a9d6ca87cd638d7b14901a18c44295f42bf63b
Krzysztof Cwalina <kcwalina@microsoft.com>
2021-06-28 11:24:38 -0700

    fix

---
+++ sdk/core/Azure.Core/tests/AzureKeyCredentialTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using NUnit.Framework;
+
+namespace Azure.Core.Tests
+{
+    public class AzureKeyCredentialTests
+    {
+        [Test]
+        public void KeyChanged()
+        {
+            var akc = new AzureKeyCredential("1");
+            var updateTo = "2";
+
+            akc.KeyChanged += (object sender, string newKey) =>
+            {
+                Assert.AreEqual(updateTo, newKey);
+                Assert.AreEqual(updateTo, akc.Key);
+                Assert.AreEqual(sender, akc);
+            };
+
+            akc.Update(updateTo);
+        }
+    }
+}
