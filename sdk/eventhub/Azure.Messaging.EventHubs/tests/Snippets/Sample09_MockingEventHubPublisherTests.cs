df111fbcfa9c276ba469b47e3a54abe84c524946
Achyut Upadhyay <acupadhy@microsoft.com>
2021-06-30 10:37:42 +0530

    added test for try add false

--- sdk/eventhub/Azure.Messaging.EventHubs/tests/Snippets/Sample09_MockingEventHubPublisherTests.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/tests/Snippets/Sample09_MockingEventHubPublisherTests.cs
@@ -20,13 +20,57 @@ namespace Azure.Messaging.EventHubs.Tests.Snippets
         ///     Performs basic unit test validation of the contained snippet.
         /// </summary>
         [Test]
-        public async Task EventBatch()
+        public async Task AddingEventBatch_TryAddReturnsFalse()
+        {
+            #region Snippet:EventHubs_Sample09_AddingEventBatchMock
+
+            var mockBatchSizeInBytes = 1024;
+            var eventStore = new List<EventData>();
+            var mockEventBatch = EventHubsModelFactory.EventDataBatch(mockBatchSizeInBytes, eventStore, null, eventToAdd => false);
+            var mockEventHubProducerClient = new Mock<EventHubProducerClient>();
+
+            mockEventHubProducerClient
+                .Setup(c => c.CreateBatchAsync(It.IsAny<CancellationToken>()))
+                .Returns(new ValueTask<EventDataBatch>(mockEventBatch));
+
+            var producer = mockEventHubProducerClient.Object;
+
+            try
+            {
+                using var eventBatch = await producer.CreateBatchAsync();
+
+                var eventBody = new BinaryData("This is an event body");
+                var eventData = new EventData(eventBody);
+
+                var ex = Assert.Throws<Exception>(() =>
+                {
+                    if (!eventBatch.TryAdd(eventData))
+                    {
+                        throw new Exception("The event could not be added.");
+                    }
+                });
+
+                Assert.AreEqual("The event could not be added.", ex.Message);
+            }
+            finally
+            {
+                await producer.CloseAsync();
+            }
+
+            #endregion
+        }
+
+        /// <summary>
+        ///     Performs basic unit test validation of the contained snippet.
+        /// </summary>
+        [Test]
+        public async Task PublishingEventBatch()
         {
             #region Snippet:EventHubs_Sample09_MockingPublishingEventBatch
 
             var mockBatchSizeInBytes = 1024;
             var eventStore = new List<EventData>();
-            var mockEventBatch = EventHubsModelFactory.EventDataBatch(mockBatchSizeInBytes, eventStore, null, x => true);
+            var mockEventBatch = EventHubsModelFactory.EventDataBatch(mockBatchSizeInBytes, eventStore, null, eventToAdd => true);
             var mockEventHubProducerClient = new Mock<EventHubProducerClient>();
 
             mockEventHubProducerClient
