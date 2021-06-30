f473271abd25ca6563e0b9ae282d3921a20f5d81
m-nash <prognash@gmail.com>
2021-06-29 19:46:40 -0700

    remove CA1067

--- sdk/resourcemanager/Azure.ResourceManager.Core/tests/Unit/SubResourceTests.cs
+++ sdk/resourcemanager/Azure.ResourceManager.Core/tests/Unit/SubResourceTests.cs
@@ -14,13 +14,13 @@ namespace Azure.ResourceManager.Core.Tests
         public void Deserialization()
         {
             var id = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testRg/providers/Microsoft.ClassicStorage/storageAccounts/account1";
+            var expected = "{\"id\":\"" + id + "\"}";
             var resource1 = new SubResource(id);
             var jsonString = JsonHelper.SerializeToString(resource1);
             var json = JsonDocument.Parse(jsonString).RootElement;
             var resource2 = SubResource.DeserializeSubResource(json);
-            Assert.IsTrue(resource1.Equals(resource2));
-            Assert.IsTrue(resource1.Equals(id));
-            Assert.IsFalse(resource1.Equals(id + "1"));
+            Assert.AreEqual(expected, jsonString);
+            Assert.AreEqual(jsonString, JsonHelper.SerializeToString(resource2));
         }
     }
 }
