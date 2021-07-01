007b407fcdee4c91a5bf8de8b5ed5732a57f71ef
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-06-30 21:43:02 -0700

    Use ILogger<T> for webjob extensions

--- sdk/eventhub/Microsoft.Azure.WebJobs.Extensions.EventHubs/src/Triggers/EventHubTriggerAttributeBindingProvider.cs
+++ sdk/eventhub/Microsoft.Azure.WebJobs.Extensions.EventHubs/src/Triggers/EventHubTriggerAttributeBindingProvider.cs
@@ -18,7 +18,7 @@ namespace Microsoft.Azure.WebJobs.EventHubs
 {
     internal class EventHubTriggerAttributeBindingProvider : ITriggerBindingProvider
     {
-        private readonly ILogger _logger;
+        private readonly ILoggerFactory _loggerFactory;
         private readonly IOptions<EventHubOptions> _options;
         private readonly EventHubClientFactory _clientFactory;
         private readonly IConverterManager _converterManager;
@@ -32,7 +32,7 @@ namespace Microsoft.Azure.WebJobs.EventHubs
             _converterManager = converterManager;
             _options = options;
             _clientFactory = clientFactory;
-            _logger = loggerFactory?.CreateLogger(LogCategories.CreateTriggerCategory("EventHub"));
+            _loggerFactory = loggerFactory;
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
@@ -59,7 +59,7 @@ namespace Microsoft.Azure.WebJobs.EventHubs
                      _clientFactory.GetCheckpointStoreClient(),
                      options.EventProcessorOptions.RetryOptions.ToRetryPolicy(),
                      factoryContext.Descriptor.Id,
-                     _logger);
+                     _loggerFactory.CreateLogger<BlobsCheckpointStore>());
 
                  IListener listener = new EventHubListener(
                                                 factoryContext.Descriptor.Id,
@@ -69,7 +69,7 @@ namespace Microsoft.Azure.WebJobs.EventHubs
                                                 _clientFactory.GetEventHubConsumerClient(attribute.EventHubName, attribute.Connection, attribute.ConsumerGroup),
                                                 checkpointStore,
                                                 options,
-                                                _logger);
+                                                _loggerFactory);
                  return Task.FromResult(listener);
              };
 
