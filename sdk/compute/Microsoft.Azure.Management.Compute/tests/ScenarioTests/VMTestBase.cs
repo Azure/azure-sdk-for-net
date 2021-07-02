70fe92d00d7be4022fcc58e5ce5b85ab34d47d1e
Micah Jo <micahjo@microsoft.com>
2021-07-02 17:36:10 -0400

    adding vmss passing test

--- sdk/compute/Microsoft.Azure.Management.Compute/tests/ScenarioTests/VMTestBase.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/tests/ScenarioTests/VMTestBase.cs
@@ -1052,6 +1052,43 @@ namespace Compute.Tests
                 });
         }
 
+        protected CapacityReservationGroup CreateCapacityReservationGroup(string rgName, string capacityReservationGroupName, List<string> availabilityZones = null)
+        {
+            m_ResourcesClient.ResourceGroups.CreateOrUpdate(
+                   rgName,
+                   new ResourceGroup
+                   {
+                       Location = m_location,
+                       Tags = new Dictionary<string, string>() { { rgName, DateTime.UtcNow.ToString("u") } }
+                   });
+
+            CapacityReservationGroup capacityReservationGroup = new CapacityReservationGroup()
+            {
+                Location = m_location,
+                Zones = availabilityZones
+            };
+            return m_CrpClient.CapacityReservationGroups.CreateOrUpdate(rgName, capacityReservationGroupName, capacityReservationGroup);
+        }
+
+        protected CapacityReservation CreateCapacityReservation(string rgName, string capacityReservationGroupName, string capacityReservationName, string vmSize,
+            string availabilityZone = null, int reservedCount = 1)
+        {
+            //Check if CapacityReservationGroup already exist and if does not exist, create one.
+            CapacityReservationGroup existingCRG = m_CrpClient.CapacityReservationGroups.Get(rgName, capacityReservationGroupName);
+            if (existingCRG == null)
+            {
+                existingCRG = CreateCapacityReservationGroup(rgName, capacityReservationGroupName);
+            }
+            return m_CrpClient.CapacityReservations.CreateOrUpdate(rgName, capacityReservationGroupName, capacityReservationName,
+                new CapacityReservation()
+                {
+                    Location = m_location,
+                    Tags = new Dictionary<string, string>() { { rgName, DateTime.UtcNow.ToString("u") } },
+                    Sku = new CM.Sku() { Name = vmSize, Capacity = reservedCount },
+                    Zones = string.IsNullOrEmpty(availabilityZone) ? null : new List<string> { availabilityZone },
+                });
+        }
+
         protected void ValidateVM(VirtualMachine vm, VirtualMachine vmOut, string expectedVMReferenceId, bool hasManagedDisks = false, bool hasUserDefinedAS = true,
             bool? writeAcceleratorEnabled = null, bool hasDiffDisks = false, string expectedLocation = null, string expectedPpgReferenceId = null,
             bool? encryptionAtHostEnabled = null, string expectedDedicatedHostGroupReferenceId = null)
