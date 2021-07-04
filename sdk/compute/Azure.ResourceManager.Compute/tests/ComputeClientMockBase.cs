2d0ca2d55a6ac6a504a36ec962a970a81c14425b
ArcturusZhang <dapzhang@microsoft.com>
2021-07-04 12:42:43 +0800

    some attempt on the mock tests

---
+++ sdk/compute/Azure.ResourceManager.Compute/tests/ComputeClientMockBase.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.TestFramework;
+
+namespace Azure.ResourceManager.Compute.Tests
+{
+    public class ComputeClientMockBase : MockTestBase
+    {
+        public ComputeClientMockBase(bool isAsync) : base(isAsync)
+        {
+        }
+
+        public ComputeClientMockBase(bool isAsync, RecordedTestMode mode) : base(isAsync, mode)
+        {
+        }
+
+        public string Location => "westus";
+    }
+}
