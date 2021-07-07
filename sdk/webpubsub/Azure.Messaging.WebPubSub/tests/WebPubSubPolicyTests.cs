e26209234b8b4ef0ea3238cfd88142d493763b6c
Krzysztof Cwalina <kcwalina@microsoft.com>
2021-07-07 15:31:00 -0700

    removed capture policy for now

--- sdk/webpubsub/Azure.Messaging.WebPubSub/tests/WebPubSubPolicyTests.cs
+++ sdk/webpubsub/Azure.Messaging.WebPubSub/tests/WebPubSubPolicyTests.cs
@@ -18,7 +18,6 @@ namespace Azure.Messaging.WebPubSub.Tests
         {
             var mockResponse = new MockResponse(202);
             var transport = new MockTransport(mockResponse);
-            var captureMessage = new CaptureMessagePolicy();
 
             var wpsEndpoint = "https://wps.contoso.com/";
             var apimEndpoint = "https://apim.contoso.com/";
@@ -26,7 +25,6 @@ namespace Azure.Messaging.WebPubSub.Tests
 
             var options = new WebPubSubServiceClientOptions();
             options.Transport = transport;
-            options.AddPolicy(captureMessage, HttpPipelinePosition.PerCall);
             options.ReverseProxyEndpoint = new Uri(apimEndpoint);
 
             var client = new WebPubSubServiceClient(new Uri(wpsEndpoint), "test_hub", credentail, options);
@@ -34,21 +32,8 @@ namespace Azure.Messaging.WebPubSub.Tests
             var response = client.SendToAll("Hello World!");
             Assert.AreEqual(202, response.Status);
 
-            var message = captureMessage.Message;
-
-            var requestUri = message.Request.Uri.ToUri();
+            var requestUri = transport.SingleRequest.Uri.ToUri();
             Assert.AreEqual(new Uri(apimEndpoint).Host, requestUri.Host);
-        }
-
-        private class CaptureMessagePolicy : HttpPipelineSynchronousPolicy
-        {
-            public HttpMessage Message { get; set; }
-
-            public override void OnReceivedResponse(HttpMessage message)
-            {
-                base.OnReceivedResponse(message);
-                Message = message;
-            }
         }
     }
 }
