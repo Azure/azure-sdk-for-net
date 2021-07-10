70e1c3f9c51c644fcd64536f623bf3f3eb025af3
Azad Abbasi <azabbasi@microsoft.com>
2021-07-09 17:06:46 -0700

    Fix seealso doc tags.

--- sdk/digitaltwins/Azure.DigitalTwins.Core/src/Customized/Models/DigitalTwinsModelData.cs
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/src/Customized/Models/DigitalTwinsModelData.cs
@@ -19,7 +19,9 @@ namespace Azure.DigitalTwins.Core
         /// <summary>
         /// The model definition that conforms to Digital Twins Definition Language (DTDL) v2.
         /// </summary>
-        /// <seealso href="https://docs.microsoft.com/en-us/azure/digital-twins/concepts-models"/>
+        /// <remarks>
+        /// <see href="https://docs.microsoft.com/en-us/azure/digital-twins/concepts-models">Understand twin models in Azure Digital Twins</see>.
+        /// </remarks>
         [CodeGenMember("Model")]
         public string DtdlModel { get; }
 
