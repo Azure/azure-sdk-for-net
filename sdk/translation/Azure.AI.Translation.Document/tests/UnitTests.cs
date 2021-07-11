127b48ee84b2ccecfcfc80cb8de8a179ed7fab14
MIDDLEEAST\v-moshaban <v-moshaban@microsoft.com>
2021-07-11 18:58:21 +0200

    adding unit tests for guid validation

---
+++ sdk/translation/Azure.AI.Translation.Document/tests/UnitTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.AI.Translation.Document.Tests
+{
+    /// <summary>
+    /// Unit tests for our utility methods and helpers.
+    /// </summary>
+    public class UnitTests
+    {
+        [Test]
+        public void InvalidGuidTest()
+        {
+            var invalidGuid = "Foo Bar";
+            Assert.Throws<ArgumentException>(() => ClientCommon.ValidateModelId(invalidGuid, nameof(invalidGuid)));
+        }
+
+        [Test]
+        public void ValidGuidTest()
+        {
+            var validGuid = "e78a95a6-e268-11eb-ba80-0242ac130004";
+            var parsedGuid = ClientCommon.ValidateModelId(validGuid, nameof(validGuid));
+            Assert.NotNull(parsedGuid);
+        }
+    }
+}
