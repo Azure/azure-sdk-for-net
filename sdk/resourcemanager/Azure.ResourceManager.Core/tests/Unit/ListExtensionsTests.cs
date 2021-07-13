08f575d421cd66f3e46495fb883f4ae4dc2e15f9
YalinLi0312 <yall@microsoft.com>
2021-07-13 13:22:21 -0700

    Add ListExtensions tests

---
+++ sdk/resourcemanager/Azure.ResourceManager.Core/tests/Unit/ListExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Core.Tests
+{
+    [Parallelizable]
+    public class ListExtensionsTests
+    {
+        [Test]
+        public void InvalidTrimTest()
+        {
+            List<int> list = null;
+            Assert.Throws<ArgumentNullException>(() => { list.Trim(2); });
+
+            list = new List<int>() { 1, 2, 3 };
+            Assert.Throws<ArgumentOutOfRangeException>(() => { list.Trim(-1); });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { list.Trim(4); });
+        }
+    }
+}
