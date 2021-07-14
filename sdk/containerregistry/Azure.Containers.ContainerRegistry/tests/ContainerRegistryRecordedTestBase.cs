cd7e617168a2d5b8ea4f5439e33080eb8f9d7fd6
Anne Loomis Thompson <annelo@microsoft.com>
2021-07-14 09:45:26 -0700

    designate cloud environment and pass in authority scope

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/tests/ContainerRegistryRecordedTestBase.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/tests/ContainerRegistryRecordedTestBase.cs
@@ -1,15 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.Core.TestFramework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Azure.Core.TestFramework;
 using Microsoft.Azure.Management.ContainerRegistry;
 using Microsoft.Azure.Management.ContainerRegistry.Models;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
-using NUnit.Framework;
-using System.Linq;
 using Task = System.Threading.Tasks.Task;
 
 namespace Azure.Containers.ContainerRegistry.Tests
@@ -26,20 +25,67 @@ namespace Azure.Containers.ContainerRegistry.Tests
             Sanitizer = new ContainerRegistryRecordedTestSanitizer();
         }
 
-        public ContainerRegistryClient CreateClient(bool anonymousAccess = false, string authenticationScope = null)
+        public ContainerRegistryClient CreateClient(bool anonymousAccess = false)
         {
-            return anonymousAccess ?
+            return anonymousAccess ? CreateAnonymousClient() : CreateAuthenticatedClient();
+        }
 
-                InstrumentClient(new ContainerRegistryClient(
-                    new Uri(TestEnvironment.AnonymousAccessEndpoint),
-                    InstrumentClientOptions(new ContainerRegistryClientOptions())
-                )) :
+        private ContainerRegistryClient CreateAuthenticatedClient()
+        {
+            string endpoint = TestEnvironment.Endpoint;
+            string authenticationScope = GetAuthenticationScope(endpoint);
 
-                InstrumentClient(new ContainerRegistryClient(
-                    new Uri(TestEnvironment.Endpoint),
+            return InstrumentClient(new ContainerRegistryClient(
+                    new Uri(endpoint),
                     TestEnvironment.Credential,
-                    InstrumentClientOptions(new ContainerRegistryClientOptions())
+                    InstrumentClientOptions(new ContainerRegistryClientOptions()
+                    {
+                        AuthenticationScope = authenticationScope
+                    })
                 ));
+        }
+
+        private ContainerRegistryClient CreateAnonymousClient()
+        {
+            string endpoint = TestEnvironment.AnonymousAccessEndpoint;
+            string authenticationScope = GetAuthenticationScope(endpoint);
+
+            return InstrumentClient(new ContainerRegistryClient(
+                    new Uri(endpoint),
+                    InstrumentClientOptions(new ContainerRegistryClientOptions()
+                    {
+                        AuthenticationScope = authenticationScope
+                    })
+                ));
+        }
+
+        private string GetAuthenticationScope(string endpoint)
+        {
+            if (endpoint.Contains(".azurecr.io"))
+            {
+                // ACR's authentication scope for Azure Public cloud
+                return "https://management.core.windows.net/.default";
+            }
+
+            if (endpoint.Contains(".azurecr.cn"))
+            {
+                // ACR's authentication scope for Azure China cloud
+                return "https://management.chinacloudapi.cn/.default";
+            }
+
+            if (endpoint.Contains(".azurecr.us"))
+            {
+                // ACR's authentication scope for US Government cloud
+                return "https://management.usgovcloudapi.net/.default";
+            }
+
+            if (endpoint.Contains(".azurecr.de"))
+            {
+                // ACR's authentication scope for Azure Germany cloud
+                return "https://management.microsoftazure.de/";
+            }
+
+            throw new NotSupportedException($"Cloud for endpoint {endpoint} is not supported.");
         }
 
         public async Task ImportImageAsync(string registry, string repository, string tag)
@@ -56,7 +102,7 @@ namespace Azure.Containers.ContainerRegistry.Tests
                     ClientSecret = TestEnvironment.ClientSecret,
                 },
                 TestEnvironment.TenantId,
-                AzureEnvironment.AzureGlobalCloud);
+                GetManagementCloudEnvironment());
 
             var managementClient = new ContainerRegistryManagementClient(credential.WithDefaultSubscription(TestEnvironment.SubscriptionId));
             managementClient.SubscriptionId = TestEnvironment.SubscriptionId;
@@ -79,6 +125,38 @@ namespace Azure.Containers.ContainerRegistry.Tests
                         Source = importSource,
                         TargetTags = targetTags.ToList()
                     });
+        }
+
+        /// <summary>
+        /// Obtain the track 1 management plane AzureEnvironment value for the
+        /// cloud correponding to the configured endpoint.
+        /// </summary>
+        /// <returns></returns>
+        private AzureEnvironment GetManagementCloudEnvironment()
+        {
+            string endpoint = TestEnvironment.Endpoint;
+
+            if (endpoint.Contains(".azurecr.io"))
+            {
+                return AzureEnvironment.AzureGlobalCloud;
+            }
+
+            if (endpoint.Contains(".azurecr.cn"))
+            {
+                return AzureEnvironment.AzureChinaCloud;
+            }
+
+            if (endpoint.Contains(".azurecr.us"))
+            {
+                return AzureEnvironment.AzureUSGovernment;
+            }
+
+            if (endpoint.Contains(".azurecr.de"))
+            {
+                return AzureEnvironment.AzureGermanCloud;
+            }
+
+            throw new NotSupportedException($"Cloud for endpoint {endpoint} is not supported.");
         }
     }
 }
