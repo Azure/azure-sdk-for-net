e3c3b8cd92c03ba3479bc2d456aefb088293ce90
Feng Zhou <55177366+fengzhou-msft@users.noreply.github.com>
2021-07-14 13:51:43 +0800

    make Types public (#22612)

--- sdk/resourcemanager/Azure.ResourceManager.Core/src/ResourceIdentifier/ResourceType.cs
+++ sdk/resourcemanager/Azure.ResourceManager.Core/src/ResourceIdentifier/ResourceType.cs
@@ -63,7 +63,10 @@ namespace Azure.ResourceManager.Core
         /// </summary>
         public string Type { get; private set; }
 
-        internal IList<string> Types { get; } = new List<string>();
+        /// <summary>
+        /// Gets the resource Types.
+        /// </summary>
+        public IReadOnlyList<string> Types { get; } = new List<string>();
 
         /// <summary>
         /// Determines if this resource type is the parent of the given resource.
