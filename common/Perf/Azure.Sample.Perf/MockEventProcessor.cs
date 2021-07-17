03cd467949c19ed9b5231e159e5e70e2248c1c54
Mike Harder <mharder@microsoft.com>
2021-07-16 18:15:05 -0700

    Tests for MockEventProcessor

---
+++ common/Perf/Azure.Sample.Perf/MockEventProcessor.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Azure.Sample.Perf
+{
+    public class MockEventProcessor
+    {
+        private static readonly MockEventArgs _eventArgs = new MockEventArgs(partition: 0, data: "hello");
+
+        private bool _processing = false;
+        private bool _stopProcessing = false;
+
+        private Func<MockEventArgs, Task> _processEventAsync;
+
+        public event Func<MockEventArgs, Task> ProcessEventAsync
+        {
+            add
+            {
+                _processEventAsync = value;
+            }
+            remove
+            {
+                _processEventAsync = default;
+            }
+        }
+
+        public Task StartProcessingAsync()
+        {
+            if (_processing)
+            {
+                throw new InvalidOperationException("Processing has already been started.");
+            }
+
+            _ = Process();
+            return Task.CompletedTask;
+        }
+
+        private async Task Process()
+        {
+            await Task.Yield();
+
+            _processing = true;
+            while (!_stopProcessing)
+            {
+                await _processEventAsync(_eventArgs);
+            }
+            _processing = false;
+        }
+
+        public async Task StopProcessingAsync()
+        {
+            if (!_processing)
+            {
+                throw new InvalidOperationException("Processing has already been stopped.");
+            }
+
+            _stopProcessing = true;
+
+            while (_processing)
+            {
+                await Task.Delay(10);
+            }
+
+            _stopProcessing = false;
+        }
+    }
+}
---
+++ common/Perf/Azure.Sample.Perf/MockEventProcessorDirectTest.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Test.Perf;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Sample.Perf
+{
+    public class MockEventProcessorDirectTest : PerfTest<PerfOptions>
+    {
+        private readonly MockEventProcessor _eventProcessor;
+
+        private int _eventsProcessed;
+        private TimeSpan _elapsed;
+
+        public MockEventProcessorDirectTest(PerfOptions options) : base(options)
+        {
+            _eventProcessor = new MockEventProcessor();
+            _eventProcessor.ProcessEventAsync += ProcessEventAsync;
+        }
+
+        private Task ProcessEventAsync(MockEventArgs arg)
+        {
+            Interlocked.Increment(ref _eventsProcessed);
+            return Task.CompletedTask;
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            var sw = Stopwatch.StartNew();
+
+            await _eventProcessor.StartProcessingAsync();
+
+            try
+            {
+                await Task.Delay(Timeout.Infinite, cancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+            }
+
+            await _eventProcessor.StopProcessingAsync();
+
+            sw.Stop();
+
+            _elapsed = sw.Elapsed;
+        }
+
+        public override Task CleanupAsync()
+        {
+            var eventsPerSecond = ((double)_eventsProcessed) / _elapsed.TotalSeconds;
+
+            Console.WriteLine($"Events Processed: {_eventsProcessed:N0}, Elapsed: {_elapsed.TotalSeconds:N3}, " +
+                $"Events/Second: {eventsPerSecond:N1}");
+            return base.CleanupAsync();
+        }
+    }
+}
