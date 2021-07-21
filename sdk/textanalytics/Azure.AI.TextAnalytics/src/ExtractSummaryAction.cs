89522fa2a48d9b1b057a16e4f2104ba2eb5e3093
Caio Saldanha <camaiaor@microsoft.com>
2021-07-21 16:11:53 -0700

    Implemented input

---
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/ExtractSummaryAction.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.AI.TextAnalytics
+{
+    /// <summary>
+    /// TODO.
+    /// </summary>
+    public class ExtractSummaryAction
+    {
+        /// <summary>
+        /// TODO.
+        /// </summary>
+        public ExtractSummaryAction()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets a value that, if set, indicates the version of the text
+        /// analytics model that will be used to generate the result.  For supported
+        /// model versions, see operation-specific documentation, for example:
+        /// <see href="https://docs.microsoft.com/azure/cognitive-services/text-analytics/concepts/model-versioning#available-versions"/>.
+        /// </summary>
+        public string ModelVersion { get; set; }
+
+        /// <summary>
+        /// If set, specifies the maximum limit of sentences returned in the result. Defaults to 3.
+        /// </summary>
+        public int? MaxSentenceCount { get; set; }
+
+        /// <summary>
+        /// TODO.
+        /// </summary>
+        public SummarySentencesOrder? OrderBy { get; set; }
+    }
+}
---
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/SummarySentencesOrder.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.AI.TextAnalytics
+{
+    /// <summary>
+    /// TODO.
+    /// </summary>
+    [CodeGenModel("ExtractiveSummarizationTaskParametersSortBy")]
+    public enum SummarySentencesOrder
+    {
+        /// <summary>
+        /// TODO.
+        /// </summary>
+        Offset,
+
+        /// <summary>
+        /// TODO.
+        /// </summary>
+        Rank
+    }
+}
