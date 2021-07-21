199ccc919282b2560511e9efec0fef8d92e7b3e7
Chidozie Ononiwu <chononiw@microsoft.com>
2021-07-21 16:39:07 -0700

    Update eng\scripts\Update-Snippets.ps1 to consume Snippet Generator tools package from azure-sdk-tools DevOps feed
    Remove eng\snippetgenerator
    
    Update Update-Snippets.ps1 to consuem Snippet Generator tyools package from azure-sdk-tools DevOps feed

--- eng/scripts/Update-Snippets.ps1
+++ eng/scripts/Update-Snippets.ps1
@@ -8,7 +8,6 @@ param (
     [switch] $StrictMode
 )
 
-$generatorProject = "$PSScriptRoot/../SnippetGenerator/SnippetGenerator.csproj";
 $root = "$PSScriptRoot/../../sdk"
 
 # special casing * here because single invocation of SnippetGenerator is much faster than
@@ -19,8 +18,20 @@ if ($ServiceDirectory -and ($ServiceDirectory -ne "*")) {
 
 if (-not (Test-Path env:TF_BUILD)) { $StrictMode = $true }
 
-if($StrictMode) {
-    Resolve-Path "$root" | %{ dotnet run -p $generatorProject -b "$_" -sm -c Release }
-} else {
-    Resolve-Path "$root" | %{ dotnet run -p $generatorProject -b "$_" -c Release }
+nuget install SnippetGenerator -Source azure-sdk-tools@DevOps -PreRelease -OutputDirectory .
+$generatorAssembly = Resolve-Path -Path ".\SnippetGenerator*\tools\**\SnippetGenerator.dll"
+
+if (Test-Path $generatorAssembly)
+{
+    if($StrictMode) {
+        Resolve-Path "$root" | %{ dotnet $generatorAssembly -b "$_" -sm}
+    } else {
+        Resolve-Path "$root" | %{ dotnet $generatorAssembly -b "$_" }
+    }
+    git clean ".\SnippetGenerator*" -xfd
+}
+else
+{
+    Write-Error "Could not find assembly at ${generatorAssembly}"
+    exit 1
 }
