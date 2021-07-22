9adea496e7a6b7bfb2b9da30d180a3c7cd7079b5
Caio Saldanha <camaiaor@microsoft.com>
2021-07-22 07:10:02 -0700

    Added tests

--- sdk/textanalytics/Azure.AI.TextAnalytics/tests/AnalyzeOperationMockTests.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/tests/AnalyzeOperationMockTests.cs
@@ -707,6 +707,40 @@ namespace Azure.AI.TextAnalytics.Tests
 
         #endregion Analyze sentiment
 
+        #region Extract summary
+
+        [Test]
+        public void AnalyzeOperationExtractsSummaryWithTwoActions()
+        {
+            var mockResponse = new MockResponse(202);
+            mockResponse.AddHeader(new HttpHeader("Operation-Location", "something/jobs/2a96a91f-7edf-4931-a880-3fdee1d56f15"));
+
+            var mockTransport = new MockTransport(new[] { mockResponse, mockResponse });
+            var client = CreateTestClient(mockTransport);
+
+            var documents = new List<string>
+            {
+                "Elon Musk is the CEO of SpaceX and Tesla."
+            };
+
+            TextAnalyticsActions batchActions = new()
+            {
+                ExtractSummaryActions = new List<ExtractSummaryAction>()
+                {
+                    new ExtractSummaryAction(),
+                    new ExtractSummaryAction()
+                    {
+                        ModelVersion = "InvalidVersion"
+                    }
+                },
+            };
+
+            ArgumentException ex = Assert.ThrowsAsync<ArgumentException>(async () => await client.StartAnalyzeActionsAsync(documents, batchActions));
+            Assert.AreEqual("Multiple of the same action is not currently supported.", ex.Message);
+        }
+
+        #endregion Extract summary
+
         [Test]
         public async Task AnalyzeOperationWithActionsError()
         {
