6de54cc6d744ba8cba9a6278348098e593ce9db9
Praveenkumar Ravikumar <prravikumar@microsoft.com>
2021-07-22 22:18:09 +0530

    regenerating sdk to fix mismatch with reference

--- sdk/redis/Microsoft.Azure.Management.RedisCache/src/Generated/SdkInfo_RedisManagementClient.cs
+++ sdk/redis/Microsoft.Azure.Management.RedisCache/src/Generated/SdkInfo_RedisManagementClient.cs
@@ -29,5 +29,16 @@ namespace Microsoft.Azure.Management.Redis
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/redis/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\Users\\prravikumar\\repo\\azure-sdk-for-net\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "23ff943dbe3b5ccdc63a6195daa0f30b17ed9d88";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
