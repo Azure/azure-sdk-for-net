34a327ec875cc4feade48cedb09ab9f3e99366be
MIDDLEEAST\v-moshaban <v-moshaban@microsoft.com>
2021-07-25 17:02:45 +0200

    adding tests for getalltranslations()

--- sdk/translation/Azure.AI.Translation.Document/tests/FilterTests.cs
+++ sdk/translation/Azure.AI.Translation.Document/tests/FilterTests.cs
@@ -13,20 +13,47 @@ namespace Azure.AI.Translation.Document.Tests
 {
     public partial class DocumentTranslationClientLiveTests
     {
+        private async Task<List<string>> CreateTranslationJobsAsync(DocumentTranslationClient client, int jobsCount = 1, int docsPerJob = 1, DocumentTranslationStatus jobTerminalStatus)
+        {
+            // create source container
+            if (jobTerminalStatus == DocumentTranslationStatus.Cancelled)
+            {
+                docsPerJob = 20; // in order to avoid job completing before cancelling
+            }
+            var testDocs = CreateDummyTestDocuments(count: docsPerJob);
+            var sourceContainer = await CreateSourceContainerAsync(testDocs);
+
+            // create a translation job
+            var resultIds = new List<string>();
+            for (int i = 1; i <= jobsCount; i++)
+            {
+                var targetContainer = await CreateTargetContainerAsync();
+                var input = new DocumentTranslationInput(sourceContainer, targetContainer, "fr");
+                var translationOp = await client.StartTranslationAsync(input);
+                resultIds.Add(translationOp.Id);
+                if (jobTerminalStatus == DocumentTranslationStatus.Succeeded)
+                {
+                    await translationOp.WaitForCompletionAsync();
+                }
+                else if (jobTerminalStatus == DocumentTranslationStatus.Cancelled)
+                {
+                    await translationOp.CancelAsync(default);
+                    Thread.Sleep(3000); // wait for cancel status to propagate!
+                }
+            }
+
+            return resultIds;
+        }
+
         [RecordedTest]
         public async Task GetAllTranslationStatusesFilterByStatusTest()
         {
-            // prepare test
+            // create client
             var client = GetClient();
-            var testDocs = CreateDummyTestDocuments(count: 10); // using a lot of docs -> make operation take long time -> we can cancel and not worry about finished status!
-            var sourceContainer = await CreateSourceContainerAsync(testDocs);
-            var targetContainer = await CreateTargetContainerAsync();
 
-            // create a translation job
-            var input = new DocumentTranslationInput(sourceContainer, targetContainer, "fr");
-            var translationOp = await client.StartTranslationAsync(input);
-            await translationOp.CancelAsync(default);
-            Thread.Sleep(3000); // wait for cancel status to propagate!
+            // create test jobs
+            await CreateTranslationJobsAsync(client, jobsCount: 5, docsPerJob: 1, jobTerminalStatus: DocumentTranslationStatus.Succeeded);
+            var cancelledIds = await CreateTranslationJobsAsync(client, jobsCount: 2, jobTerminalStatus: DocumentTranslationStatus.Cancelled);
 
             // list translations with filter
             var cancelledStatusList = new List<DocumentTranslationStatus> {
@@ -36,11 +63,102 @@ namespace Azure.AI.Translation.Document.Tests
             var filter = new TranslationFilter(
                 statuses: cancelledStatusList
             );
-            var submittedTranslations = await client.GetAllTranslationStatusesAsync(filter: filter).ToEnumerableAsync();
+            var filteredTranslations = await client.GetAllTranslationStatusesAsync(filter: filter).ToEnumerableAsync();
+            var filteredIds = new HashSet<string>(filteredTranslations.Select(t => t.Id).ToList());
 
             // assert
-            Assert.That(submittedTranslations.Any(t => string.Equals(t.Id, translationOp.Id, StringComparison.OrdinalIgnoreCase) ));
-            Assert.That(submittedTranslations.All(t => cancelledStatusList.Contains(t.Status) ));
+            Assert.That(filteredTranslations.All(t => cancelledStatusList.Contains(t.Status)));
+            Assert.True(cancelledIds.All(cancelledId => filteredIds.Contains(cancelledId)));
+        }
+
+        [RecordedTest]
+        public async Task GetAllTranslationStatusesFilterByIdsTest()
+        {
+            // create client
+            var client = GetClient();
+
+            // create test jobs
+            var allIds = await CreateTranslationJobsAsync(client, jobsCount: 10, docsPerJob: 1, jobTerminalStatus: DocumentTranslationStatus.Succeeded);
+            var targetIds = allIds.GetRange(0, 5);
+
+            // list translations with filter
+            var filter = new TranslationFilter(
+                ids: targetIds
+            );
+            var filteredTranslations = await client.GetAllTranslationStatusesAsync(filter: filter).ToEnumerableAsync();
+
+            // assert
+            Assert.That(filteredTranslations.Any(t => targetIds.Contains(t.Id)));
+        }
+
+        [RecordedTest]
+        public async Task GetAllTranslationStatusesFilterByCreatedAfter()
+        {
+            // create client
+            var client = GetClient();
+
+            // create test jobs
+            await CreateTranslationJobsAsync(client, jobsCount: 5, docsPerJob: 1, jobTerminalStatus: DocumentTranslationStatus.Succeeded);
+            var timestamp = DateTimeOffset.UtcNow;
+            var targetIds = await CreateTranslationJobsAsync(client, jobsCount: 5, docsPerJob: 1, jobTerminalStatus: DocumentTranslationStatus.Succeeded);
+
+            // list translations with filter
+            var filter = new TranslationFilter(
+                createdAfter: timestamp
+            );
+            var filteredTranslations = await client.GetAllTranslationStatusesAsync(filter: filter).ToEnumerableAsync();
+
+            // assert
+            Assert.That(filteredTranslations.Any(t => targetIds.Contains(t.Id)));
+            Assert.That(filteredTranslations.All(t => t.CreatedOn >= timestamp));
+        }
+
+        [RecordedTest]
+        public async Task GetAllTranslationStatusesFilterByCreatedBefore()
+        {
+            // create client
+            var client = GetClient();
+
+            // create test jobs
+            var targetIds = await CreateTranslationJobsAsync(client, jobsCount: 5, docsPerJob: 1, jobTerminalStatus: DocumentTranslationStatus.Succeeded);
+            var timestamp = DateTimeOffset.UtcNow;
+            await CreateTranslationJobsAsync(client, jobsCount: 5, docsPerJob: 1, jobTerminalStatus: DocumentTranslationStatus.Succeeded);
+
+            // list translations with filter
+            var filter = new TranslationFilter(
+                createdAfter: timestamp
+            );
+            var filteredTranslations = await client.GetAllTranslationStatusesAsync(filter: filter).ToEnumerableAsync();
+
+            // assert
+            Assert.That(filteredTranslations.Any(t => targetIds.Contains(t.Id)));
+            Assert.That(filteredTranslations.All(t => t.CreatedOn <= timestamp));
+        }
+
+        [RecordedTest]
+        public async Task GetAllTranslationStatusesOrderByCreatedOn()
+        {
+            // create client
+            var client = GetClient();
+
+            // create test jobs
+            await CreateTranslationJobsAsync(client, jobsCount: 5, docsPerJob: 1, jobTerminalStatus: DocumentTranslationStatus.Succeeded);
+
+            // list translations with filter
+            var filter = new TranslationFilter(
+                orderBy: new List<TranslationFilterOrder> {
+                  new TranslationFilterOrder(property: TranslationFilterProperty.CreatedOn, orderBy: DocumentTranslationFilterMode.Desc)
+                }
+            );
+            var filteredTranslations = await client.GetAllTranslationStatusesAsync(filter: filter).ToEnumerableAsync();
+
+            // assert
+            var timestamp = DateTimeOffset.UtcNow;
+            foreach (var result in filteredTranslations)
+            {
+                Assert.IsTrue(result.CreatedOn <= timestamp);
+                timestamp = result.CreatedOn;
+            }
         }
     }
 }
