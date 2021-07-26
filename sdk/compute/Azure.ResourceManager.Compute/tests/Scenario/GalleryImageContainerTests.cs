c1f45b1e244847989306eb457648e78421dd2e5e
ArcturusZhang <dapzhang@microsoft.com>
2021-07-26 13:57:55 +0800

    add test cases for gallery images

---
+++ sdk/compute/Azure.ResourceManager.Compute/tests/Scenario/GalleryImageContainerTests.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Compute.Models;
+using Azure.ResourceManager.Compute.Tests.Helpers;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Compute.Tests
+{
+    public class GalleryImageContainerTests : ComputeTestBase
+    {
+        private ResourceGroup _resourceGroup;
+        private Gallery _gallery;
+
+        public GalleryImageContainerTests(bool isAsync)
+            : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        private GalleryImageData BasicGalleryImageData
+        {
+            get
+            {
+                var identifier = GalleryImageHelper.GetGalleryImageIdentifier(
+                    Recording.GenerateAssetName("publisher"),
+                    Recording.GenerateAssetName("offer"),
+                    Recording.GenerateAssetName("sku"));
+                return GalleryImageHelper.GetBasicGalleryImageData(DefaultLocation, OperatingSystemTypes.Linux, identifier);
+            }
+        }
+
+        private async Task<GalleryImageContainer> GetGalleryImageContainerAsync()
+        {
+            _resourceGroup = await CreateResourceGroupAsync();
+            var galleryName = Recording.GenerateAssetName("testGallery_");
+            var input = GalleryHelper.GetBasicGalleryData(DefaultLocation);
+            _gallery = await _resourceGroup.GetGalleries().CreateOrUpdateAsync(galleryName, input);
+            return _gallery.GetGalleryImages();
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task CreateOrUpdate()
+        {
+            var container = await GetGalleryImageContainerAsync();
+            var name = Recording.GenerateAssetName("testImage_");
+            GalleryImage image = await container.CreateOrUpdateAsync(name, BasicGalleryImageData);
+            Assert.AreEqual(name, image.Data.Name);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Get()
+        {
+            var container = await GetGalleryImageContainerAsync();
+            var name = Recording.GenerateAssetName("testImage_");
+            GalleryImage image = await container.CreateOrUpdateAsync(name, BasicGalleryImageData);
+            GalleryImage image2 = await container.GetAsync(name);
+
+            GalleryImageHelper.AssertGalleryImage(image.Data, image2.Data);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task DoesExist()
+        {
+            var container = await GetGalleryImageContainerAsync();
+            var name = Recording.GenerateAssetName("testImage_");
+            GalleryImage image = await container.CreateOrUpdateAsync(name, BasicGalleryImageData);
+            Assert.IsTrue(await container.DoesExistAsync(name));
+            Assert.IsFalse(await container.DoesExistAsync(name + "1"));
+
+            Assert.ThrowsAsync<ArgumentNullException>(async () => _ = await container.DoesExistAsync(null));
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task List()
+        {
+            var container = await GetGalleryImageContainerAsync();
+            var name1 = Recording.GenerateAssetName("testImage_");
+            var name2 = Recording.GenerateAssetName("testImage_");
+            var input1 = BasicGalleryImageData;
+            var input2 = BasicGalleryImageData;
+            _ = await container.CreateOrUpdateAsync(name1, input1);
+            _ = await container.CreateOrUpdateAsync(name2, input2);
+            int count = 0;
+            await foreach (var galleryImage in container.ListAsync())
+            {
+                count++;
+            }
+            Assert.GreaterOrEqual(count, 2);
+        }
+    }
+}
---
+++ sdk/compute/Azure.ResourceManager.Compute/tests/Scenario/GalleryImageOperationsTests.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Compute.Models;
+using Azure.ResourceManager.Compute.Tests.Helpers;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Compute.Tests
+{
+    public class GalleryImageOperationsTests : ComputeTestBase
+    {
+        private ResourceGroup _resourceGroup;
+        private Gallery _gallery;
+
+        public GalleryImageOperationsTests(bool isAsync)
+            : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        private async Task<GalleryImage> CreateGalleryImageAsync(string galleryImageName)
+        {
+            _resourceGroup = await CreateResourceGroupAsync();
+            var galleryName = Recording.GenerateAssetName("testGallery_");
+            var galleryInput = GalleryHelper.GetBasicGalleryData(DefaultLocation);
+            _gallery = await _resourceGroup.GetGalleries().CreateOrUpdateAsync(galleryName, galleryInput);
+            var identifier = GalleryImageHelper.GetGalleryImageIdentifier(
+                    Recording.GenerateAssetName("publisher"),
+                    Recording.GenerateAssetName("offer"),
+                    Recording.GenerateAssetName("sku"));
+            var imageInput = GalleryImageHelper.GetBasicGalleryImageData(DefaultLocation, Models.OperatingSystemTypes.Linux, identifier);
+            return await _gallery.GetGalleryImages().CreateOrUpdateAsync(galleryImageName, imageInput);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Delete()
+        {
+            var name = Recording.GenerateAssetName("testGallery_");
+            var image = await CreateGalleryImageAsync(name);
+            await image.DeleteAsync();
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task StartDelete()
+        {
+            var name = Recording.GenerateAssetName("testGallery_");
+            var image = await CreateGalleryImageAsync(name);
+            var deleteOp = await image.StartDeleteAsync();
+            await deleteOp.WaitForCompletionResponseAsync();
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Get()
+        {
+            var name = Recording.GenerateAssetName("testGallery_");
+            var image = await CreateGalleryImageAsync(name);
+            GalleryImage image2 = await image.GetAsync();
+
+            GalleryImageHelper.AssertGalleryImage(image.Data, image2.Data);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        [Ignore("Same issue as in VirtualMachineOperationsTests")]
+        public async Task Update()
+        {
+            var name = Recording.GenerateAssetName("testGallery_");
+            var image = await CreateGalleryImageAsync(name);
+            var description = "This is a gallery for test";
+            var update = new GalleryImageUpdate()
+            {
+                OsType = OperatingSystemTypes.Linux, // We have to put this here, otherwise we get a 409 Changing property 'galleryImage.properties.osType' is not allowed.
+                Description = description
+            };
+            GalleryImage updatedGalleryImage = await image.UpdateAsync(update);
+
+            Assert.AreEqual(description, updatedGalleryImage.Data.Description);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task SetTags()
+        {
+            var name = Recording.GenerateAssetName("testGallery_");
+            var image = await CreateGalleryImageAsync(name);
+            var tags = new Dictionary<string, string>()
+            {
+                { "key", "value" }
+            };
+            GalleryImage updatedGalleryImage = await image.SetTagsAsync(tags);
+
+            Assert.AreEqual(tags, updatedGalleryImage.Data.Tags);
+        }
+    }
+}
