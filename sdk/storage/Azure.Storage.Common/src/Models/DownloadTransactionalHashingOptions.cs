ef47b871215c93093bcb72c7fd171617dccdc8b6
jschrepp-MSFT <41338290+jschrepp-MSFT@users.noreply.github.com>
2021-07-26 11:52:57 -0700

    Transactional hashing models

---
+++ sdk/storage/Azure.Storage.Common/src/Models/DownloadTransactionalHashingOptions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Storage.Models
+{
+    /// <summary>
+    /// Hashing options for validating upload content.
+    /// </summary>
+    public class DownloadTransactionalHashingOptions
+    {
+        /// <summary>
+        /// Hashing algorithm to use for the transaction.
+        /// </summary>
+        public TransactionalHashAlgorithm Algorithm { get; set; }
+    }
+}
---
+++ sdk/storage/Azure.Storage.Common/src/Models/UploadTransactionalHashingOptions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Storage.Models
+{
+    /// <summary>
+    /// Hashing options for validating upload content.
+    /// </summary>
+    public class UploadTransactionalHashingOptions
+    {
+        /// <summary>
+        /// Hashing algorithm to use for the transaction.
+        /// </summary>
+        public TransactionalHashAlgorithm Algorithm { get; set; }
+
+        /// <summary>
+        /// Optional. For an upload API that ends up using a single REST call,
+        /// a precalculated hash for the upload contents can be provided to skip
+        /// recalculating. This hash will be ignored if the upload splits into
+        /// multiple REST calls and new ones will be calculated.
+        /// </summary>
+        public byte PrecalculatedHash { get; set; }
+    }
+}
