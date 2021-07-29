1f5a1d3977c3e68bd9ced8e538ac627040796616
Pavel Krymets <pavel@krymets.com>
2021-07-29 16:18:44 -0700

    Use blob primary endpoint in live tests

--- sdk/extensions/Azure.Extensions.AspNetCore.DataProtection.Blobs/tests/DataProtectionTestEnvironment.cs
+++ sdk/extensions/Azure.Extensions.AspNetCore.DataProtection.Blobs/tests/DataProtectionTestEnvironment.cs
@@ -1,13 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Azure.Core.TestFramework;
 
 namespace Azure.Extensions.AspNetCore.DataProtection.Blobs.Tests
 {
     public class DataProtectionTestEnvironment: TestEnvironment
     {
-        public string StorageAccountName => GetVariable("BLOB_STORAGE_ACCOUNT_NAME");
-        public string StorageAccountKey => GetVariable("BLOB_STORAGE_ACCOUNT_KEY");
+        public Uri BlobStorageEndpoint => new(GetVariable("BLOB_STORAGE_ENDPOINT"));
     }
 }
