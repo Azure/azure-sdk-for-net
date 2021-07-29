8aca7f0a1465eab80bdd300cdce83afbefd932e7
Mike Harder <mharder@microsoft.com>
2021-07-29 16:34:35 -0700

    Move "if" check to Process methods

--- common/Perf/Azure.Test.Perf/TestProxyPolicy.cs
+++ common/Perf/Azure.Test.Perf/TestProxyPolicy.cs
@@ -22,40 +22,43 @@ namespace Azure.Test.Perf
 
         public override void Process(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
         {
-            RedirectToTestProxy(message);
+            if (!string.IsNullOrEmpty(RecordingId) && !string.IsNullOrEmpty(Mode))
+            {
+                RedirectToTestProxy(message);
+            }
             ProcessNext(message, pipeline);
         }
 
         public override ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
         {
-            RedirectToTestProxy(message);
+            if (!string.IsNullOrEmpty(RecordingId) && !string.IsNullOrEmpty(Mode))
+            {
+                RedirectToTestProxy(message);
+            }
             return ProcessNextAsync(message, pipeline);
         }
 
         protected void RedirectToTestProxy(HttpMessage message)
         {
-            if (!string.IsNullOrEmpty(RecordingId) && !string.IsNullOrEmpty(Mode))
+            message.Request.Headers.SetValue("x-recording-id", RecordingId);
+            message.Request.Headers.SetValue("x-recording-mode", Mode);
+            message.Request.Headers.SetValue("x-recording-remove", bool.FalseString);
+
+            // Ensure x-recording-upstream-base-uri header is only set once, since the same HttpMessage will be reused on retries
+            if (!message.Request.Headers.Contains("x-recording-upstream-base-uri"))
             {
-                message.Request.Headers.SetValue("x-recording-id", RecordingId);
-                message.Request.Headers.SetValue("x-recording-mode", Mode);
-                message.Request.Headers.SetValue("x-recording-remove", bool.FalseString);
-
-                // Ensure x-recording-upstream-base-uri header is only set once, since the same HttpMessage will be reused on retries
-                if (!message.Request.Headers.Contains("x-recording-upstream-base-uri"))
+                var baseUri = new RequestUriBuilder()
                 {
-                    var baseUri = new RequestUriBuilder()
-                    {
-                        Scheme = message.Request.Uri.Scheme,
-                        Host = message.Request.Uri.Host,
-                        Port = message.Request.Uri.Port,
-                    };
-                    message.Request.Headers.SetValue("x-recording-upstream-base-uri", baseUri.ToString());
-                }
-
-                message.Request.Uri.Scheme = _uri.Scheme;
-                message.Request.Uri.Host = _uri.Host;
-                message.Request.Uri.Port = _uri.Port;
+                    Scheme = message.Request.Uri.Scheme,
+                    Host = message.Request.Uri.Host,
+                    Port = message.Request.Uri.Port,
+                };
+                message.Request.Headers.SetValue("x-recording-upstream-base-uri", baseUri.ToString());
             }
+
+            message.Request.Uri.Scheme = _uri.Scheme;
+            message.Request.Uri.Host = _uri.Host;
+            message.Request.Uri.Port = _uri.Port;
         }
     }
 }
