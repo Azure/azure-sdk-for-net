511946efb4789de741e128f927b724651f40c10c
Anne Thompson <annelo@microsoft.com>
2021-08-03 16:57:09 -0700

    updating service version enum

---
+++ sdk/synapse/Azure.Analytics.Synapse.Artifacts/src/Customization/ArtifactsClientOptions.ServiceVersion.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+//using System;
+//using Azure.Core;
+
+//#pragma warning disable SA1402 // File may only contain a single type
+
+//namespace Azure.Analytics.Synapse.Artifacts
+//{
+//    /// <summary> Provides the client configuration options for connecting to Azure WebPubSub service. </summary>
+//    public partial class ArtifactsClientOptions : ClientOptions
+//    {
+//        /// <summary>
+//        /// The versions of Azure Synapse supported by this client library.
+//        /// </summary>
+//        public enum ServiceVersion
+//        {
+//#pragma warning disable CA1707 // Identifiers should not contain underscores
+//            /// <summary>
+//            /// The 2020_12_01 version of the Azure Synapse Artifacts service.
+//            /// </summary>
+//            V2020_12_01 = 1,
+
+//            /// <summary>
+//            /// The 2021_06_01_Preview version of the Azure Synapse Artifacts service.
+//            /// </summary>
+//            V2021_06_01_Preview = 2,
+//#pragma warning restore CA1707
+//        }
+
+//        /// <summary>
+//        /// The Latest service version supported by this client library.
+//        /// </summary>
+//        internal const ServiceVersion LatestVersion = ServiceVersion.V2021_06_01_Preview;
+
+//        /// <summary>
+//        /// Gets the version of the service API used when making requests.
+//        /// </summary>
+//        public ServiceVersion Version { get; }
+
+//        /// <summary>
+//        /// Initializes a new instance of the <see cref="ArtifactsClientOptions"/>
+//        /// class.
+//        /// </summary>
+//        /// <param name="version">
+//        /// An optional <see cref="ServiceVersion"/> to specify the version of
+//        /// the REST API to use.
+//        /// If not provided, the <paramref name="version"/> will default to the
+//        /// latest supported by this client library.  It is recommended that
+//        /// application authors allow the version to float to the latest and
+//        /// library authors pin to a specific version.
+//        /// </param>
+//        /// <exception cref="ArgumentOutOfRangeException">
+//        /// Thrown when the <paramref name="version"/> is not supported by this
+//        /// client library.
+//        /// </exception>
+//        public ArtifactsClientOptions(ServiceVersion version = LatestVersion)
+//        {
+//            Version = version.Validate();
+//        }
+//    }
+
+//    /// <summary>
+//    /// Synapse Artifacts extension methods.
+//    /// </summary>
+//    internal static partial class SynapseArtifactsExtensions
+//    {
+//        /// <summary>
+//        /// Validate a <see cref="ArtifactsClientOptions.ServiceVersion"/>.
+//        /// </summary>
+//        /// <param name="version">
+//        /// The <see cref="ArtifactsClientOptions.ServiceVersion"/> to validate.
+//        /// </param>
+//        /// <returns>
+//        /// The validated version.
+//        /// </returns>
+//        /// <exception cref="ArgumentOutOfRangeException">
+//        /// Thrown when the <paramref name="version"/> is not supported by this
+//        /// client library.
+//        /// </exception>
+//        public static ArtifactsClientOptions.ServiceVersion Validate(this ArtifactsClientOptions.ServiceVersion version) =>
+//            version switch
+//            {
+//                ArtifactsClientOptions.ServiceVersion.V2020_12_01 => version,
+//                ArtifactsClientOptions.ServiceVersion.V2021_06_01_Preview => version,
+//                _ => throw CreateInvalidVersionException(version)
+//            };
+
+//        /// <summary>
+//        /// Get a version string, like "2020-06-30", corresponding to a given
+//        /// <see cref="ArtifactsClientOptions.ServiceVersion"/> value.
+//        /// </summary>
+//        /// <param name="version">
+//        /// The <see cref="ArtifactsClientOptions.ServiceVersion"/> value to
+//        /// convert into a version string.
+//        /// </param>
+//        /// <returns>
+//        /// The version string.
+//        /// </returns>
+//        /// <exception cref="ArgumentOutOfRangeException">
+//        /// Thrown when the <paramref name="version"/> is not supported by this
+//        /// client library.
+//        /// </exception>
+//        public static string ToVersionString(this ArtifactsClientOptions.ServiceVersion version) =>
+//            version switch
+//            {
+//                ArtifactsClientOptions.ServiceVersion.V2020_12_01 => "2020-12-01",
+//                ArtifactsClientOptions.ServiceVersion.V2021_06_01_Preview => "2021-06-01-preview",
+//                _ => throw CreateInvalidVersionException(version)
+//            };
+
+//        /// <summary>
+//        /// Create an <see cref="ArgumentOutOfRangeException"/> to throw when
+//        /// an invalid <see cref="ArtifactsClientOptions.ServiceVersion"/> value
+//        /// is provided.
+//        /// </summary>
+//        /// <param name="version">The invalid version value.</param>
+//        /// <returns>An exception to throw.</returns>
+//        private static ArgumentOutOfRangeException CreateInvalidVersionException(ArtifactsClientOptions.ServiceVersion version) =>
+//            new ArgumentOutOfRangeException(
+//                nameof(version),
+//                version,
+//                $"The {nameof(ArtifactsClientOptions)}.{nameof(ArtifactsClientOptions.ServiceVersion)} specified is not supported by this library.");
+//    }
+//}
--- sdk/synapse/Azure.Analytics.Synapse.Artifacts/src/autorest.md
+++ sdk/synapse/Azure.Analytics.Synapse.Artifacts/src/autorest.md
@@ -7,8 +7,10 @@ Run `dotnet build /t:GenerateCode` to generate code.
 
 ``` yaml
 tag: package-artifacts-2019-06-01-preview
+#tag: package-artifacts-2021-06-01-preview
 require:
     - https://github.com/Azure/azure-rest-api-specs/blob/fc5e2fbcfc3f585d38bdb1c513ce1ad2c570cf3d/specification/synapse/data-plane/readme.md
+#    - https://github.com/Azure/azure-rest-api-specs/blob/c981b81aa26ad4d0d156e034e6782853b4e747a1/specification/synapse/data-plane/readme.md
 namespace: Azure.Analytics.Synapse.Artifacts
 public-clients: true
 security: AADToken
