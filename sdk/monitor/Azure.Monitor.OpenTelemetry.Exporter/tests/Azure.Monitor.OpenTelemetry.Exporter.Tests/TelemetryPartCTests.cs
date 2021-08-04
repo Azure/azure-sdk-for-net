674896c9104e20fcbd7ff84eb5f090a4071cfda3
Vishwesh Bankwar <vibankwa@microsoft.com>
2021-08-04 13:54:17 -0700

    add test

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/TelemetryPartCTests.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/TelemetryPartCTests.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
+using System.Text;
 using System.Text.Json;
 using Xunit;
 
@@ -113,7 +115,7 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Demo.Tracing
         }
 
         [Fact]
-        public void LinksareTruncatedWhenCannotFitInMaxLength()
+        public void LinksAreTruncatedWhenCannotFitInMaxLength()
         {
             using ActivitySource activitySource = new ActivitySource(ActivitySourceName);
             List<ActivityLink> links = new List<ActivityLink>();
@@ -151,6 +153,88 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Demo.Tracing
                 Assert.Equal(0, 1);
             }
             Assert.True(actualMSlinks.Length <= maxLength);
+        }
+
+        [Fact]
+        public void LinksAreNotTruncatedWhenCanBeFitInMaxLength()
+        {
+            using ActivitySource activitySource = new ActivitySource(ActivitySourceName);
+            List<ActivityLink> links = new List<ActivityLink>();
+
+            int maxLinks = 107; // based on assumption that traceId and SpanId will be of fixed length 32 and 16 respectively.
+            int maxLength = 8192;
+
+            for (int i = 0; i < maxLinks; i++)
+            {
+                ActivityLink activityLink = new ActivityLink(new ActivityContext(
+                ActivityTraceId.CreateRandom(),
+                ActivitySpanId.CreateRandom(),
+                ActivityTraceFlags.None), null);
+                links.Add(activityLink);
+            }
+
+            using var activity = activitySource.StartActivity(
+                ActivityName,
+                ActivityKind.Client,
+                parentContext: default,
+                null,
+                links,
+                startTime: DateTime.UtcNow);
+
+            string expectedMslinks = GetExpectedMSlinks(links);
+
+            // Only checking dependency here. The flow will be same for other telemetry types.
+            var telemetryPartBRemoteDependencyData = TelemetryPartB.GetRemoteDependencyData(activity);
+
+            Assert.True(telemetryPartBRemoteDependencyData.Properties.TryGetValue(msLinks, out var actualMSlinks));
+            try
+            {
+                JsonDocument document = JsonDocument.Parse(actualMSlinks);
+                Assert.Equal(1, 1);
+            }
+            catch (Exception)
+            {
+                Assert.Equal(0, 1);
+            }
+            Assert.True(actualMSlinks.Length <= maxLength);
+            Assert.Equal(expectedMslinks, actualMSlinks);
+        }
+
+        private string GetExpectedMSlinks(IEnumerable<ActivityLink> links)
+        {
+            if (links != null && links.Any())
+            {
+                var linksJson = new StringBuilder();
+                linksJson.Append('[');
+                foreach (var link in links)
+                {
+                    linksJson
+                        .Append('{')
+                        .Append("\"operation_Id\":")
+                        .Append('\"')
+                        .Append(link.Context.TraceId.ToHexString())
+                        .Append('\"')
+                        .Append(',');
+                    linksJson
+                        .Append("\"id\":")
+                        .Append('\"')
+                        .Append(link.Context.SpanId.ToHexString())
+                        .Append('\"');
+                    linksJson.Append("},");
+                }
+
+                if (linksJson.Length > 0)
+                {
+                    // trim trailing comma - json does not support it
+                    linksJson.Remove(linksJson.Length - 1, 1);
+                }
+
+                linksJson.Append(']');
+
+                return linksJson.ToString();
+            }
+
+            return null;
         }
     }
 }
