53b9414a15d890da76bc243a676687dd743456eb
Vishwesh Bankwar <vibankwa@microsoft.com>
2021-08-04 13:23:26 -0700

    add activity Links support

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/TelemetryPartB.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/TelemetryPartB.cs
@@ -11,6 +11,8 @@ using Azure.Monitor.OpenTelemetry.Exporter.Models;
 
 using OpenTelemetry.Logs;
 using OpenTelemetry.Trace;
+using System.Text;
+using System.Linq;
 
 namespace Azure.Monitor.OpenTelemetry.Exporter
 {
@@ -26,6 +28,8 @@ namespace Azure.Monitor.OpenTelemetry.Exporter
             string url = null;
             string urlAuthority = null;
             var monitorTags = EnumerateActivityTags(activity);
+
+            AddActivityLinksToPartCTags(activity.Links, ref monitorTags.PartCTags);
 
             switch (monitorTags.activityType)
             {
@@ -54,6 +58,8 @@ namespace Azure.Monitor.OpenTelemetry.Exporter
         internal static RemoteDependencyData GetRemoteDependencyData(Activity activity)
         {
             var monitorTags = EnumerateActivityTags(activity);
+
+            AddActivityLinksToPartCTags(activity.Links, ref monitorTags.PartCTags);
 
             var dependency = new RemoteDependencyData(2, activity.DisplayName, activity.Duration.ToString("c", CultureInfo.InvariantCulture))
             {
@@ -120,6 +126,58 @@ namespace Azure.Monitor.OpenTelemetry.Exporter
             for (int i = 0; i < PartCTags.Length; i++)
             {
                 destination.Add(PartCTags[i].Key, PartCTags[i].Value?.ToString());
+            }
+        }
+
+        private static void AddActivityLinksToPartCTags(IEnumerable<ActivityLink> links, ref AzMonList PartCTags)
+        {
+            string msLinks = "_MS.links";
+            int maxLength = 8192;
+            int prevIndex = 0;
+
+            if (links != null && links.Any())
+            {
+                var linksJson = new StringBuilder();
+                linksJson.Append('[');
+                foreach (var link in links)
+                {
+                    linksJson
+                        .Append('{')
+                        .Append("\"operation_Id\":")
+                        .Append('\"')
+                        .Append(link.Context.TraceId.ToHexString())
+                        .Append('\"')
+                        .Append(',');
+                    linksJson
+                        .Append("\"id\":")
+                        .Append('\"')
+                        .Append(link.Context.SpanId.ToHexString())
+                        .Append('\"');
+                    linksJson.Append("},");
+
+                    if (linksJson.Length > maxLength)
+                    {
+                        break;
+                    }
+
+                    prevIndex = linksJson.Length;
+                }
+
+                if (linksJson.Length > maxLength)
+                {
+                    // Remove the link that caused linksJson to exceed maxLength.
+                    linksJson.Remove(prevIndex, linksJson.Length - prevIndex);
+                }
+
+                if (linksJson.Length > 0)
+                {
+                    // trim trailing comma - json does not support it
+                    linksJson.Remove(linksJson.Length - 1, 1);
+                }
+
+                linksJson.Append(']');
+
+                AzMonList.Add(ref PartCTags, new KeyValuePair<string, object>(msLinks, linksJson.ToString()));
             }
         }
 
---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/TelemetryPartCTests.cs
@@ -0,0 +1,156 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text.Json;
+using Xunit;
+
+namespace Azure.Monitor.OpenTelemetry.Exporter.Demo.Tracing
+{
+    public class TelemetryPartCTests
+    {
+        private const string ActivitySourceName = "TelemetryPartCTests";
+        private const string ActivityName = "TestActivity";
+        private const string msLinks = "_MS.links";
+
+        static TelemetryPartCTests()
+        {
+            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+            Activity.ForceDefaultIdFormat = true;
+
+            var listener = new ActivityListener
+            {
+                ShouldListenTo = _ => true,
+                Sample = (ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllData,
+            };
+
+            ActivitySource.AddActivityListener(listener);
+        }
+
+        [Fact]
+        public void RequestDataPropertiesDoesNotContainMSLinksWhenActivityHasNoLinks()
+        {
+            using ActivitySource activitySource = new ActivitySource(ActivitySourceName);
+            using var activity = activitySource.StartActivity(
+                ActivityName,
+                ActivityKind.Client,
+                parentContext: default,
+                startTime: DateTime.UtcNow);
+
+            var telemetryPartBRequestData = TelemetryPartB.GetRequestData(activity);
+            Assert.False(telemetryPartBRequestData.Properties.TryGetValue(msLinks, out var mslinks));
+        }
+
+        [Fact]
+        public void RequestDataPropertiesContainsMSLinksWhenActivityHasLinks()
+        {
+            using ActivitySource activitySource = new ActivitySource(ActivitySourceName);
+            ActivityLink activityLink = new ActivityLink(new ActivityContext(
+                ActivityTraceId.CreateRandom(),
+                ActivitySpanId.CreateRandom(),
+                ActivityTraceFlags.None), null);
+
+            List<ActivityLink> links = new List<ActivityLink>();
+            links.Add(activityLink);
+
+            using var activity = activitySource.StartActivity(
+                ActivityName,
+                ActivityKind.Client,
+                parentContext: default,
+                null,
+                links,
+                startTime: DateTime.UtcNow);
+
+            string expectedMSlinks = $"[{{\"operation_Id\":\"{activityLink.Context.TraceId}\",\"id\":\"{activityLink.Context.SpanId}\"}}]";
+            var telemetryPartBRequestData = TelemetryPartB.GetRequestData(activity);
+
+            Assert.True(telemetryPartBRequestData.Properties.TryGetValue(msLinks, out var actualMSlinks));
+            Assert.Equal(expectedMSlinks, actualMSlinks);
+        }
+
+        [Fact]
+        public void RemoteDependencyDataPropertiesDoesNotContainMSLinksWhenActivityHasNoLinks()
+        {
+            using ActivitySource activitySource = new ActivitySource(ActivitySourceName);
+            using var activity = activitySource.StartActivity(
+                ActivityName,
+                ActivityKind.Client,
+                parentContext: default,
+                startTime: DateTime.UtcNow);
+
+            var telemetryPartBRemoteDependencyData = TelemetryPartB.GetRemoteDependencyData(activity);
+            Assert.False(telemetryPartBRemoteDependencyData.Properties.TryGetValue(msLinks, out var mslinks));
+        }
+
+        [Fact]
+        public void RemoteDependencyDataPropertiesContainsMSLinksWhenActivityHasLinks()
+        {
+            using ActivitySource activitySource = new ActivitySource(ActivitySourceName);
+            ActivityLink activityLink = new ActivityLink(new ActivityContext(
+                ActivityTraceId.CreateRandom(),
+                ActivitySpanId.CreateRandom(),
+                ActivityTraceFlags.None), null);
+
+            List<ActivityLink> links = new List<ActivityLink>();
+            links.Add(activityLink);
+
+            using var activity = activitySource.StartActivity(
+                ActivityName,
+                ActivityKind.Client,
+                parentContext: default,
+                null,
+                links,
+                startTime: DateTime.UtcNow);
+
+            string expectedMSlinks = $"[{{\"operation_Id\":\"{activityLink.Context.TraceId}\",\"id\":\"{activityLink.Context.SpanId}\"}}]";
+            var telemetryPartBRemoteDependencyData = TelemetryPartB.GetRemoteDependencyData(activity);
+
+            Assert.True(telemetryPartBRemoteDependencyData.Properties.TryGetValue(msLinks, out var actualMSlinks));
+            Assert.Equal(expectedMSlinks, actualMSlinks);
+        }
+
+        [Fact]
+        public void LinksareTruncatedWhenCannotFitInMaxLength()
+        {
+            using ActivitySource activitySource = new ActivitySource(ActivitySourceName);
+            List<ActivityLink> links = new List<ActivityLink>();
+
+            int maxLength = 8192;
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                ActivityLink activityLink = new ActivityLink(new ActivityContext(
+                ActivityTraceId.CreateRandom(),
+                ActivitySpanId.CreateRandom(),
+                ActivityTraceFlags.None), null);
+                links.Add(activityLink);
+            }
+
+            using var activity = activitySource.StartActivity(
+                ActivityName,
+                ActivityKind.Client,
+                parentContext: default,
+                null,
+                links,
+                startTime: DateTime.UtcNow);
+
+            // Only checking dependency here. The flow will be same for other telemetry types.
+            var telemetryPartBRemoteDependencyData = TelemetryPartB.GetRemoteDependencyData(activity);
+
+            Assert.True(telemetryPartBRemoteDependencyData.Properties.TryGetValue(msLinks, out var actualMSlinks));
+            try
+            {
+                JsonDocument document = JsonDocument.Parse(actualMSlinks);
+                Assert.Equal(1, 1);
+            }
+            catch (Exception)
+            {
+                Assert.Equal(0, 1);
+            }
+            Assert.True(actualMSlinks.Length <= maxLength);
+        }
+    }
+}
