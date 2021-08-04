8906dd5f4394cacc477b7853afa8cd291bd081c7
Chidozie Ononiwu <chononiw@microsoft.com>
2021-08-04 12:04:47 -0700

    Refactor Update-Snippets for Linux Agents

--- eng/scripts/Update-Snippets.ps1
+++ eng/scripts/Update-Snippets.ps1
@@ -16,24 +16,24 @@ if ($ServiceDirectory -and ($ServiceDirectory -ne "*")) {
     $root += '/' + $ServiceDirectory
 }
 
-if (-not (Test-Path env:TF_BUILD)) { $StrictMode = $true }
+if (-not (Test-Path Env:TF_BUILD)) 
+{
+    $StrictMode = $true 
+}
+else 
+{
+    Push-Location $Env:PIPELINE_WORKSPACE
+}
 
 dotnet tool install --global `
  --add-source "https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-net/nuget/v3/index.json" `
- --version 1.0.0-dev.20210730.3 `
- Azure.Sdk.Tools.SnippetGenerator
+ --version "1.0.0-dev.20210730.3" `
+ "Azure.Sdk.Tools.SnippetGenerator"
 
+if($StrictMode) {
+    Resolve-Path "$root" | %{ snippet-generator -b "$_" -sm}
+} else {
+    Resolve-Path "$root" | %{ snippet-generator -b "$_" }
+}
 
-try
-{
-    if($StrictMode) {
-        Resolve-Path "$root" | %{ snippet-generator -b "$_" -sm}
-    } else {
-        Resolve-Path "$root" | %{ snippet-generator -b "$_" }
-    }
-}
-catch
-{
-    Write-Error "snippet-generator run failed."
-    exit 1
-}
+Pop-Location
