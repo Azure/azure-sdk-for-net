76de6244c684ca4268287c61431e24466319717b
Caio Saldanha <camaiaor@microsoft.com>
2021-08-04 07:14:24 -0700

    Added Async summarization sample

---
+++ sdk/textanalytics/Azure.AI.TextAnalytics/tests/samples/Sample8_ExtractSummaryAsync.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.AI.TextAnalytics.Tests;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.AI.TextAnalytics.Samples
+{
+    [LiveOnly]
+    public partial class TextAnalyticsSamples : SamplesBase<TextAnalyticsTestEnvironment>
+    {
+        [Test]
+        public async Task ExtractSummaryAsync()
+        {
+            // create a text analytics client
+            string endpoint = TestEnvironment.Endpoint;
+            string apiKey = TestEnvironment.ApiKey;
+            var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
+
+            // get input document
+            string document = @"We love this trail and make the trip every year. The views are breathtaking and well
+                                worth the hike! Yesterday was foggy though, so we missed the spectacular views.
+                                We tried again today and it was amazing. Everyone in my family liked the trail although
+                                it was too challenging for the less athletic among us.";
+
+            // prepare analyze operation input
+            var batchDocuments = new List<TextDocumentInput>
+            {
+                new TextDocumentInput("1", document)
+                {
+                     Language = "en",
+                }
+            };
+
+            var summaryAction = new ExtractSummaryAction()
+            {
+                MaxSentenceCount = 5,
+                OrderBy = SummarySentencesOrder.Rank
+            };
+
+            TextAnalyticsActions actions = new TextAnalyticsActions()
+            {
+                ExtractSummaryActions = new List<ExtractSummaryAction>() { summaryAction }
+            };
+
+            // start analysis process
+            AnalyzeActionsOperation operation = await client.StartAnalyzeActionsAsync(batchDocuments, actions);
+
+            await operation.WaitForCompletionAsync();
+
+            // view operation status
+            Console.WriteLine($"AnalyzeActions operation has completed");
+            Console.WriteLine();
+
+            Console.WriteLine($"Created On   : {operation.CreatedOn}");
+            Console.WriteLine($"Expires On   : {operation.ExpiresOn}");
+            Console.WriteLine($"Id           : {operation.Id}");
+            Console.WriteLine($"Status       : {operation.Status}");
+            Console.WriteLine($"Last Modified: {operation.LastModified}");
+            Console.WriteLine();
+
+            // view operation results
+            await foreach (AnalyzeActionsResult documentsInPage in operation.Value)
+            {
+                IReadOnlyCollection<ExtractSummaryActionResult> summaryResults = documentsInPage.ExtractSummaryResults;
+
+                foreach (ExtractSummaryActionResult summaryActionResults in summaryResults)
+                {
+                    if (summaryActionResults.HasError)
+                    {
+                        Console.WriteLine($"  Error!");
+                        Console.WriteLine($"  Action error code: {summaryActionResults.Error.ErrorCode}.");
+                        Console.WriteLine($"  Message: {summaryActionResults.Error.Message}");
+                        continue;
+                    }
+
+                    foreach (ExtractSummaryResult documentResults in summaryActionResults.DocumentsResults)
+                    {
+                        if (documentResults.HasError)
+                        {
+                            Console.WriteLine($"  Error!");
+                            Console.WriteLine($"  Document error code: {documentResults.Error.ErrorCode}.");
+                            Console.WriteLine($"  Message: {documentResults.Error.Message}");
+                            continue;
+                        }
+
+                        Console.WriteLine($"  Extracted the following {documentResults.Sentences.Count} sentence(s):");
+                        Console.WriteLine();
+
+                        foreach (SummarySentence sentence in documentResults.Sentences)
+                        {
+                            Console.WriteLine($"  Sentence: {sentence.Text}");
+                            Console.WriteLine($"  Rank Score: {sentence.RankScore}");
+                            Console.WriteLine($"  Offset: {sentence.Offset}");
+                            Console.WriteLine($"  Length: {sentence.Length}");
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
