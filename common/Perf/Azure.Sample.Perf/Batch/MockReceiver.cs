eb84390d4ec3a012ca8aac664cef363261728b3b
Mike Harder <mharder@microsoft.com>
2021-08-13 15:56:10 -0700

    Add BatchPerfTest

---
+++ common/Perf/Azure.Sample.Perf/Batch/MockReceiver.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Azure.Sample.Perf.Batch
+{
+    public class MockReceiver
+    {
+        private readonly Random _random = new Random();
+
+        public Task<IEnumerable<int>> ReceiveAsync(int minMessageCount, int maxMessageCount)
+        {
+            return Task.FromResult(Receive(minMessageCount, maxMessageCount));
+        }
+
+        public IEnumerable<int> Receive(int minMessageCount, int maxMessageCount)
+        {
+            var returnedMessages = _random.Next(minMessageCount, maxMessageCount + 1);
+
+            var messages = new string[returnedMessages];
+            for (var i = 0; i < returnedMessages; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
---
+++ common/Perf/Azure.Sample.Perf/Batch/MockReceiverTest.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Test.Perf;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Sample.Perf.Batch
+{
+    public class MockReceiverTest : BatchPerfTest<MockReceiverOptions>
+    {
+        private readonly MockReceiver _mockReceiver;
+
+        public MockReceiverTest(MockReceiverOptions options) : base(options)
+        {
+            _mockReceiver = new MockReceiver();
+        }
+
+        public override int RunBatch(CancellationToken cancellationToken)
+        {
+            var messages = _mockReceiver.Receive(Options.MinMessageCount, Options.MaxMessageCount);
+            return messages.Count();
+        }
+
+        public override async Task<int> RunBatchAsync(CancellationToken cancellationToken)
+        {
+            var messages = await _mockReceiver.ReceiveAsync(Options.MinMessageCount, Options.MaxMessageCount);
+            return messages.Count();
+        }
+    }
+}
