aaa726ac6003a2109b030f43065e2a0a4dae2d9c
Mike Harder <mharder@microsoft.com>
2021-08-17 14:27:15 -0700

    Ignore transient exceptions after cancellation

--- sdk/eventhub/Azure.Messaging.EventHubs/perf/Infrastructure/BatchPublishPerfTest.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/perf/Infrastructure/BatchPublishPerfTest.cs
@@ -107,30 +107,15 @@ namespace Azure.Messaging.EventHubs.Perf
                 }
             }
 
-            while (true)
+            try
             {
-                try
-                {
-                    await s_producer.SendAsync(batch, cancellationToken).ConfigureAwait(false);
-                    return Options.BatchSize;
-                }
-                catch (EventHubsException e) when (e.IsTransient)
-                {
-                    // Azure.Messaging.EventHubs.EventHubsException(ServiceBusy): The request was terminated because the entity
-                    // is being throttled. Error code : 50002. Sub error : 102. Please wait 4 seconds and try again.
-                    var match = Regex.Match(e.Message, @"Please wait (\d+) seconds and try again", RegexOptions.IgnoreCase);
-
-                    // Default to 1 second if we cannot find suggestion in exception message
-                    var retryDelay = TimeSpan.FromSeconds(1);
-                    if (match.Success)
-                    {
-                        retryDelay = TimeSpan.FromSeconds(int.Parse(match.Groups[1].Value));
-                    }
-
-                    Console.WriteLine($"IsTransient: {e.IsTransient}, FailureReason: {e.Reason}, RetryDelay: {retryDelay}");
-
-                    await Task.Delay(retryDelay, cancellationToken);
-                }
+                await s_producer.SendAsync(batch, cancellationToken).ConfigureAwait(false);
+                return Options.BatchSize;
+            }
+            catch (EventHubsException e) when (cancellationToken.IsCancellationRequested && e.IsTransient)
+            {
+                // If SendAsync() is cancelled during a retry loop, the most recent exception is thrown.
+                // If the exception is transient it should be ignored.
             }
         }
 
