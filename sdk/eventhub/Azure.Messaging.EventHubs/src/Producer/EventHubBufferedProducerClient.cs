10217532747922b4b8f960f9045c30e9c8d4e928
Madalyn Redding <66138537+m-redding@users.noreply.github.com>
2021-08-20 12:16:48 -0400

    Update sdk/eventhub/Azure.Messaging.EventHubs/src/Producer/EventHubBufferedProducerClient.cs
    Co-authored-by: Jesse Squire <jesse.squire@gmail.com>

--- sdk/eventhub/Azure.Messaging.EventHubs/src/Producer/EventHubBufferedProducerClient.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/src/Producer/EventHubBufferedProducerClient.cs
@@ -482,6 +482,8 @@ namespace Azure.Messaging.EventHubs.Producer
         /// <param name="options">The set of options to apply when publishing this event.</param>
         /// <param name="cancellationToken">An optional <see cref="CancellationToken"/> instance to signal the request to cancel the operation.</param>
         ///
+        /// <returns>The total number of events that are currently buffered and waiting to be published, across all partitions.</returns>
+        ///
         /// <remarks>
         ///   Upon the first call to <see cref="EnqueueEventAsync(EventData, EnqueueEventOptions, CancellationToken)" /> or
         ///   <see cref="EnqueueEventsAsync(IEnumerable{EventData}, EnqueueEventOptions, CancellationToken)" />, the <see cref="SendEventBatchSucceededAsync" /> and
