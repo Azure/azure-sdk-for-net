a3096ea9ee9bc95d43874fafb4c6336a66b8478d
m-redding <t-mredding@microsoft.com>
2021-08-20 16:58:28 -0400

    adding tests and proposal

--- sdk/eventhub/Azure.Messaging.EventHubs/tests/Producer/EventHubBufferedProducerClientOptionsTests.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/tests/Producer/EventHubBufferedProducerClientOptionsTests.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Azure.Messaging.EventHubs.Core;
 using Azure.Messaging.EventHubs.Producer;
 using NUnit.Framework;
@@ -17,6 +19,70 @@ namespace Azure.Messaging.EventHubs.Tests
     public class EventHubBufferedProducerClientOptionsTests
     {
         /// <summary>
+        ///   Verifies functionality of the <see cref="EventHubProducerClientOptions.Clone" />
+        ///   method.
+        /// </summary>
+        ///
+        [Test]
+        public void CloneProducesACopy()
+        {
+            var options = new EventHubBufferedProducerClientOptions
+            {
+                MaximumWaitTime = TimeSpan.FromSeconds(2),
+                MaximumEventBufferLength = 3000,
+                EnableIdempotentRetries = true,
+                MaximumConcurrentSendsPerPartition = 5,
+                Identifier = "Test-Options",
+                ConnectionOptions = new EventHubConnectionOptions { TransportType = EventHubsTransportType.AmqpWebSockets },
+                RetryOptions = new EventHubsRetryOptions { TryTimeout = TimeSpan.FromMinutes(36) }
+            };
+
+            var clone = options.Clone();
+            Assert.That(clone, Is.Not.Null, "The clone should not be null.");
+
+            Assert.That(clone.Identifier, Is.EqualTo(options.Identifier), "The identifier should match.");
+            Assert.That(clone.EnableIdempotentRetries, Is.EqualTo(options.EnableIdempotentRetries), "The flag to enable idempotent publishing should have been copied.");
+            Assert.That(clone.ConnectionOptions.TransportType, Is.EqualTo(options.ConnectionOptions.TransportType), "The connection options of the clone should copy properties.");
+            Assert.That(clone.ConnectionOptions, Is.Not.SameAs(options.ConnectionOptions), "The connection options of the clone should be a copy, not the same instance.");
+            Assert.That(clone.RetryOptions.IsEquivalentTo(options.RetryOptions), Is.True, "The retry options of the clone should be considered equal.");
+            Assert.That(clone.RetryOptions, Is.Not.SameAs(options.RetryOptions), "The retry options of the clone should be a copy, not the same instance.");
+            Assert.That(clone.MaximumConcurrentSendsPerPartition, Is.EqualTo(options.MaximumConcurrentSendsPerPartition), "The number of concurrent sends to a partition should have been copied.");
+            Assert.That(clone.MaximumWaitTime, Is.EqualTo(options.MaximumWaitTime), "The maximum wait time should have been copied.");
+            Assert.That(clone.MaximumEventBufferLength, Is.EqualTo(options.MaximumEventBufferLength), "The maximum event buffer length should have been copied.");
+        }
+
+        /// <summary>
+        ///   Verifies functionality of the <see cref="EventHubBufferedProducerClientOptions.Clone" />
+        ///   method.
+        /// </summary>
+        [Test]
+        public void CloneKnowsAllMembersOfEventHubBufferedProducerClientOptions()
+        {
+            // This approach is inelegant and brute force, but allows us to detect
+            // additional members added to the annotated message that we're not currently
+            // cloning and avoid drift, since Azure.Core.Amqp is an external dependency.
+
+            var knownMembers = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                "Identifier",
+                "EnableIdempotentRetries",
+                "ConnectionOptions",
+                "RetryOptions",
+                "MaximumConcurrentSendsPerPartition",
+                "MaximumWaitTime",
+                "MaximumEventBufferLength"
+            };
+
+            var getterSetterProperties = typeof(EventHubBufferedProducerClientOptions)
+                .GetProperties(BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.SetProperty);
+
+            foreach (var property in getterSetterProperties)
+            {
+                Assert.That(knownMembers.Contains(property.Name), $"The property: { property.Name } of EventHubBufferedProducerClientOptions is not being cloned.");
+            }
+        }
+
+        /// <summary>
         ///   Verifies functionality of the <see cref="EventHubBufferedProducerClientOptions.MaximumConcurrentSendsPerPartition"/>
         ///   property.
         /// </summary>
@@ -24,8 +90,9 @@ namespace Azure.Messaging.EventHubs.Tests
         [Test]
         public void MaximumConcurrentSendsPerPartitionTooLarge()
         {
-            var options = new EventHubBufferedProducerClientOptions { MaximumConcurrentSendsPerPartition = 80 };
+            var options = new EventHubBufferedProducerClientOptions {};
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
             Assert.Throws<ArgumentOutOfRangeException>(() => { options.MaximumConcurrentSendsPerPartition = 800; });
@@ -35,6 +102,9 @@ namespace Azure.Messaging.EventHubs.Tests
 =======
             Assert.Throws<ArgumentOutOfRangeException>(() => { options.MaximumConcurrentSendsPerPartition = 800; });
 >>>>>>> 4a59397f3f (test typo)
+=======
+            Assert.Throws<ArgumentOutOfRangeException>(() => { options.MaximumConcurrentSendsPerPartition = 101; });
+>>>>>>> 3f4f15693d (adding tests and proposal)
         }
 
         /// <summary>
@@ -42,9 +112,9 @@ namespace Azure.Messaging.EventHubs.Tests
         ///   property.
         /// </summary>
         [Test]
-        public void MaximumConcurrentSendsPerPartitionTooSmall()
+        public void MaximumConcurrentSendsPerPartitionZeroOutOfRange()
         {
-            var options = new EventHubBufferedProducerClientOptions { MaximumConcurrentSendsPerPartition = 80 };
+            var options = new EventHubBufferedProducerClientOptions {};
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -62,11 +132,60 @@ namespace Azure.Messaging.EventHubs.Tests
         ///   property.
         /// </summary>
         [Test]
-        public void MaximumConcurrentSendsPerPartitionInRange()
+        public void MaximumConcurrentSendsPerPartitionNegativeOutOfRange()
         {
-            var options = new EventHubBufferedProducerClientOptions { MaximumConcurrentSendsPerPartition = 80 };
+            var options = new EventHubBufferedProducerClientOptions {};
 
-            Assert.DoesNotThrow(() => { options.MaximumConcurrentSendsPerPartition = 50; });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { options.MaximumConcurrentSendsPerPartition = -1; });
+        }
+
+        /// <summary>
+        ///   Verifies functionality of the <see cref="EventHubBufferedProducerClientOptions.ConnectionOptions" />
+        ///   property.
+        /// </summary>
+        ///
+        [Test]
+        public void ConnectionOptionsAreValidated()
+        {
+            Assert.That(() => new EventHubBufferedProducerClientOptions { ConnectionOptions = null }, Throws.ArgumentNullException);
+        }
+
+        /// <summary>
+        ///   Verifies functionality of the <see cref="EventHubBufferedProducerClientOptions.RetryOptions" />
+        ///   property.
+        /// </summary>
+        ///
+        [Test]
+        public void RetryOptionsAreValidated()
+        {
+            Assert.That(() => new EventHubBufferedProducerClientOptions { RetryOptions = null }, Throws.ArgumentNullException);
+        }
+
+        /// <summary>
+        ///   Verifies functionality of the <see cref="EventHubBufferedProducerClientOptions.ToEventHubProducerClientOptions" />
+        ///   property.
+        /// </summary>
+        ///
+        [Test]
+        public void ToEventHubProducerClientOptionsProducesAValidInstance()
+        {
+            var options = new EventHubBufferedProducerClientOptions
+            {
+                MaximumWaitTime = TimeSpan.FromSeconds(2),
+                MaximumEventBufferLength = 3000,
+                EnableIdempotentRetries = true,
+                MaximumConcurrentSendsPerPartition = 5,
+                Identifier = "Test-Options",
+                ConnectionOptions = new EventHubConnectionOptions { TransportType = EventHubsTransportType.AmqpWebSockets },
+                RetryOptions = new EventHubsRetryOptions { TryTimeout = TimeSpan.FromMinutes(36) }
+            };
+
+            var transformedOptions = options.ToEventHubProducerClientOptions();
+
+            Assert.That(transformedOptions.Identifier, Is.EqualTo(options.Identifier), "The identifier should match.");
+            Assert.That(transformedOptions.ConnectionOptions.TransportType, Is.EqualTo(options.ConnectionOptions.TransportType), "The connection options of the clone should copy properties.");
+            Assert.That(transformedOptions.RetryOptions.IsEquivalentTo(options.RetryOptions), Is.True, "The retry options of the clone should be considered equal.");
+            Assert.That(transformedOptions.EnableIdempotentPartitions, Is.EqualTo(options.EnableIdempotentRetries), "Idempotent partitions should be the same as idempotent retries.");
         }
     }
 }
