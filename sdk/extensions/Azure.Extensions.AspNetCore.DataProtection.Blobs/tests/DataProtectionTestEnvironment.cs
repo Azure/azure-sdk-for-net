eb3f976a390a2935ffcc804c98f93206fe9bfb5e
Pavel Krymets <pavel@krymets.com>
2021-08-30 09:39:22 -0700

    add env test func

--- sdk/extensions/Azure.Extensions.AspNetCore.DataProtection.Blobs/tests/DataProtectionTestEnvironment.cs
+++ sdk/extensions/Azure.Extensions.AspNetCore.DataProtection.Blobs/tests/DataProtectionTestEnvironment.cs
@@ -2,12 +2,35 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Threading.Tasks;
 using Azure.Core.TestFramework;
+using Azure.Storage.Blobs;
 
 namespace Azure.Extensions.AspNetCore.DataProtection.Blobs.Tests
 {
     public class DataProtectionTestEnvironment: TestEnvironment
     {
         public Uri BlobStorageEndpoint => new(GetVariable("BLOB_STORAGE_ENDPOINT"));
+
+        protected override async ValueTask<bool> IsEnvironmentReadyAsync()
+        {
+            try
+            {
+                var client = new BlobServiceClient(BlobStorageEndpoint, Credential);
+                var container = client.GetBlobContainerClient("test");
+                await container.CreateIfNotExistsAsync();
+
+                var blob = container.GetBlobClient("test-blob");
+                await blob.UploadAsync(new BinaryData(""));
+
+                await container.DeleteAsync();
+
+                return await base.IsEnvironmentReadyAsync();
+            }
+            catch (RequestFailedException e) when (e is { Status: 403})
+            {
+                return false;
+            }
+        }
     }
 }
