5e0b762e94e25077bf508330f3fc962c844a1791
Daeun Yim <daeunyim@microsoft.com>
2021-08-30 15:56:10 -0700

    generate sdk

--- sdk/postgresql/Microsoft.Azure.Management.PostgreSQL/src/postgresqlflexibleservers/Generated/SdkInfo_PostgreSQLManagementClient.cs
+++ sdk/postgresql/Microsoft.Azure.Management.PostgreSQL/src/postgresqlflexibleservers/Generated/SdkInfo_PostgreSQLManagementClient.cs
@@ -31,5 +31,16 @@ namespace Microsoft.Azure.Management.PostgreSQL.FlexibleServers
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/postgresql/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp.namespace=Microsoft.Azure.Management.PostgreSQL.FlexibleServers --tag=package-flexibleserver-2021-06 --csharp.output-folder=C:\\Users\\daeunyim\\work\\SDK\\azure-sdk-for-net\\sdk\\postgresql\\Microsoft.Azure.Management.PostgreSQL\\src\\postgresqlflexibleservers\\Generated";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "b020247789ba2ab0065ebbcfa69050ce729493b8";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
