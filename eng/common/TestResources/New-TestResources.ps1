166c57ea6bd5b4f8440d834c475eaf980c4219c5
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-08-30 16:12:08 -0700

    Fix principal

--- eng/common/TestResources/New-TestResources.ps1
+++ eng/common/TestResources/New-TestResources.ps1
@@ -190,7 +190,7 @@ try {
         $BaseName = 't' + (New-Guid).ToString('n').Substring(0, 16)
         Log "Generated base name '$BaseName' for CI build"
     } elseif (!$BaseName) {
-        $BaseName = "$UserName$ServiceDirectory" + (New-Guid).ToString('n').Substring(0, 4)
+        $BaseName = "$UserName$ServiceDirectory"
         Log "BaseName was not set. Using default base name '$BaseName'"
     }
 
@@ -289,7 +289,8 @@ try {
                 $AzureTestPrincipal
             } else {
                 Log "TestApplicationId was not specified; creating a new service principal in subscription '$SubscriptionId'"
-                $global:AzureTestPrincipal = New-AzADServicePrincipal -Role Owner -Scope "/subscriptions/$SubscriptionId" -DisplayName "test-resources-$($baseName).microsoft.com"
+		$suffix = (New-Guid).ToString('n').Substring(0, 4)
+                $global:AzureTestPrincipal = New-AzADServicePrincipal -Role Owner -Scope "/subscriptions/$SubscriptionId" -DisplayName "test-resources-$($baseName)$suffix.microsoft.com"
                 $global:AzureTestSubscription = $SubscriptionId
 
                 Log "Created service principal '$($AzureTestPrincipal.ApplicationId)'"
