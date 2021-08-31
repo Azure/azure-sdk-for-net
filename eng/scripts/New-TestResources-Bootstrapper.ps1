ed51d74e8dd7014aee35bc5e57bef7faa403b5f6
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-08-31 15:31:44 -0700

    PR FB

--- eng/scripts/New-TestResources-Bootstrapper.ps1
+++ eng/scripts/New-TestResources-Bootstrapper.ps1
@@ -4,9 +4,9 @@
 param(
 [string] $ServiceDirectory
 )
-$run = Read-Host "The test resources needed to run the live tests could not be located.`nWould you like to run the resource creation script? [y/n]"
+$run = Read-Host "The resources needed to run the live tests could not be located.`nWould you like to run the resource creation script? [y/n]"
 if ($run -eq 'y'){
     & "$PSScriptRoot\..\common\TestResources\New-TestResources.ps1" $ServiceDirectory
 
-    Read-Host "If you are not running on Windows, you will need to store the outputted environment variables manually and close and reopen your IDE.`nPress enter to close this window and resume your test run."
+    Read-Host "If you are not running on Windows, you will need to store the outputted environment variables manually and close and reopen your IDE. Please see https://github.com/Azure/azure-sdk-for-net/blob/main/eng/common/TestResources/README.md for details.`nPress enter to close this window and resume your test run."
 }
