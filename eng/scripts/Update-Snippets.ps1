c21611bef586d3bb2433824339c3171a76ac7866
Chidozie Ononiwu (His Righteousness) <31145988+chidozieononiwu@users.noreply.github.com>
2021-08-31 15:56:47 -0700

    Revert "Consume snippet generator as a tool (#22783)"
    This reverts commit e618fb89197d4c39898d66404444e22c3faa0bcb.

--- eng/scripts/Update-Snippets.ps1
+++ eng/scripts/Update-Snippets.ps1
@@ -5,12 +5,10 @@ param (
     [string] $ServiceDirectory,
 
     [Parameter()]
-    [switch] $StrictMode,
-
-    [Parameter()]
-    [string] $SnippetToolPath=''
+    [switch] $StrictMode
 )
 
+$generatorProject = "$PSScriptRoot/../SnippetGenerator/SnippetGenerator.csproj";
 $root = "$PSScriptRoot/../../sdk"
 
 # special casing * here because single invocation of SnippetGenerator is much faster than
@@ -19,29 +17,10 @@ if ($ServiceDirectory -and ($ServiceDirectory -ne "*")) {
     $root += '/' + $ServiceDirectory
 }
 
-if (-not (Test-Path Env:TF_BUILD)) 
-{
-    $StrictMode = $true
-
-    dotnet tool install --global `
-    --add-source "https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-net/nuget/v3/index.json" `
-    --version "1.0.0-dev.20210730.3" `
-    "Azure.Sdk.Tools.SnippetGenerator"
-}
-else
-{
-    if ($Env:AGENT_OS -match "Windows.*")
-    {
-        $SnippetToolPath = "%USERPROFILE%\.dotnet\tools\"
-    }
-    else
-    {
-        $SnippetToolPath = "$HOME/.dotnet/tools/"
-    }
-}
+if (-not (Test-Path env:TF_BUILD)) { $StrictMode = $true }
 
 if($StrictMode) {
-    Resolve-Path "$root" | %{ & "${SnippetToolPath}snippet-generator" -b "$_" -sm}
+    Resolve-Path "$root" | %{ dotnet run -p $generatorProject -b "$_" -sm -c Release }
 } else {
-    Resolve-Path "$root" | %{ & "${SnippetToolPath}snippet-generator" -b "$_" }
+    Resolve-Path "$root" | %{ dotnet run -p $generatorProject -b "$_" -c Release }
 }
