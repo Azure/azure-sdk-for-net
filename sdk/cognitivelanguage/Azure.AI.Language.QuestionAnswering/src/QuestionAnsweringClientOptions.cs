2a15ef10e159d9ed557aba02a70e10ef16f95193
Heath Stewart <heaths@microsoft.com>
2021-08-31 14:18:20 -0700

    Add support for 2021-07-11-preview api-version
    It's not deployed to production yet, however, so the tests have not been enabled for this version or re-recorded.

--- sdk/cognitivelanguage/Azure.AI.Language.QuestionAnswering/src/QuestionAnsweringClientOptions.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.QuestionAnswering/src/QuestionAnsweringClientOptions.cs
@@ -11,7 +11,7 @@ namespace Azure.AI.Language.QuestionAnswering
     /// </summary>
     public partial class QuestionAnsweringClientOptions : ClientOptions
     {
-        private const ServiceVersion LatestVersion = ServiceVersion.V2021_05_01_preview;
+        private const ServiceVersion LatestVersion = ServiceVersion.V2021_07_15_Preview;
 
         /// <summary>
         /// The version of the service to use.
@@ -23,6 +23,11 @@ namespace Azure.AI.Language.QuestionAnswering
             /// Service version "2021-05-01-preview".
             /// </summary>
             V2021_05_01_preview = 1,
+
+            /// <summary>
+            /// Service version "2021-07-15-preview".
+            /// </summary>
+            V2021_07_15_Preview = 2,
 #pragma warning restore CA1707 // Identifiers should not contain underscores
         }
 
@@ -36,6 +41,7 @@ namespace Azure.AI.Language.QuestionAnswering
             Version = version switch
             {
                 ServiceVersion.V2021_05_01_preview => "2021-05-01-preview",
+                ServiceVersion.V2021_07_15_Preview => "2021-07-15-preview",
                 _ => throw new NotSupportedException()
             };
 
