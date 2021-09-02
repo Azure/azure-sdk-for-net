8fb22f2f1368c6a18d9af77acc144b0c7c962b1c
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-09-02 13:42:15 -0700

    batch

--- sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/tests/ServiceBusEndToEndTests.cs
+++ sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/tests/ServiceBusEndToEndTests.cs
@@ -320,6 +320,20 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
         }
 
         [Test]
+        public async Task TestSingle_OutputBinaryData_Batch()
+        {
+            var host = BuildHost<ServiceBusOutputBinaryDataBatchTest>();
+            using (host)
+            {
+                var jobHost = host.GetJobHost();
+                await jobHost.CallAsync(nameof(ServiceBusOutputBinaryDataBatchTest.OutputBinaryData));
+                bool result = _waitHandle1.WaitOne(SBTimeoutMills);
+                Assert.True(result);
+                await host.StopAsync();
+            }
+        }
+
+        [Test]
         public async Task TestBatch_DataContractPoco()
         {
             await TestMultiple<ServiceBusMultipleMessagesTestJob_BindToPocoArray>(true);
@@ -766,6 +780,42 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
             {
                 Assert.AreEqual("message", received.ToString());
                 _waitHandle1.Set();
+            }
+        }
+
+        public class ServiceBusOutputBinaryDataBatchTest
+        {
+            private static volatile bool firstReceived = false;
+            private static volatile bool secondReceived = false;
+            public static void OutputBinaryData(
+                [ServiceBus(FirstQueueNameKey)] ICollector<BinaryData> output)
+            {
+                output.Add(new BinaryData("message1"));
+                output.Add(new BinaryData("message2"));
+            }
+
+            public static void TriggerBinaryData(
+                [ServiceBusTrigger(FirstQueueNameKey)] BinaryData[] received)
+            {
+                foreach (BinaryData binaryData in received)
+                {
+                    switch (binaryData.ToString())
+                    {
+                        case "message1":
+                            firstReceived = true;
+                            break;
+                        case "message2":
+                            secondReceived = true;
+                            break;
+                    }
+                }
+                if (firstReceived && secondReceived)
+                {
+                    // reset for the next test
+                    firstReceived = false;
+                    secondReceived = false;
+                    _waitHandle1.Set();
+                }
             }
         }
 
