153259b4fee1d0965e0300b381bc1b5425d2263b
Kamil Sobol <kasobol@microsoft.com>
2021-09-03 14:12:56 -0700

    run tests in the loop.

--- sdk/storage/chaos/run.sh
+++ sdk/storage/chaos/run.sh
@@ -8,29 +8,31 @@ export AZURE_LIVE_TEST_SERVICE_VERSIONS=V2019_07_07
 
 export $(grep 'APPINSIGHTS_INSTRUMENTATIONKEY' $ENV_FILE | xargs)
 
-UUID=$(cat /proc/sys/kernel/random/uuid)
-export AZURE_STORAGE_TEST_RUN_ID=$UUID
+count=20
+for i in $(seq $count); do
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+    export AZURE_STORAGE_TEST_RUN_ID=$UUID
 
-NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-export AZURE_STORAGE_TEST_RUN_START_TIME=$NOW
+    NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+    export AZURE_STORAGE_TEST_RUN_START_TIME=$NOW
 
-dotnet test /azure-sdk-for-net/eng/service.proj \
-    --filter "(TestCategory!=Manually) & (TestCategory!=NonChaos)" \
-    --framework net5.0 \
-    --logger "trx;LogFileName=net5.0.trx" \
-    --logger:"console;verbosity=normal" \
-    /p:SDKType=all \
-    /p:ServiceDirectory=storage \
-    /p:Project=Azure.Storage.* \
-    /p:IncludeSrc=false \
-    /p:IncludeSamples=false \
-    /p:IncludePerf=false \
-    /p:IncludeStress=false \
-    /p:RunApiCompat=false \
-    /p:InheritDocEnabled=false \
-    /p:Configuration=Debug \
-    /p:CollectCoverage=false \
-    /p:UseProjectReferenceToAzureClients=false
-
+    dotnet test /azure-sdk-for-net/eng/service.proj \
+        --filter "(TestCategory!=Manually) & (TestCategory!=NonChaos)" \
+        --framework net5.0 \
+        --logger "trx;LogFileName=net5.0.trx" \
+        --logger:"console;verbosity=normal" \
+        /p:SDKType=all \
+        /p:ServiceDirectory=storage \
+        /p:Project=Azure.Storage.* \
+        /p:IncludeSrc=false \
+        /p:IncludeSamples=false \
+        /p:IncludePerf=false \
+        /p:IncludeStress=false \
+        /p:RunApiCompat=false \
+        /p:InheritDocEnabled=false \
+        /p:Configuration=Debug \
+        /p:CollectCoverage=false \
+        /p:UseProjectReferenceToAzureClients=false
+done
 
 sleep 1h
