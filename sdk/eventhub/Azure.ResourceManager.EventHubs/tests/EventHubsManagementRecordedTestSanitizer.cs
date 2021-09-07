22a674d0d68e86c77cd48b77220c0889b0729107
Nisha Bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2021-09-06 22:20:02 -0700

    wip

---
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/tests/EventHubsManagementRecordedTestSanitizer.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+using Castle.Core.Internal;
+
+namespace Azure.ResourceManager.CosmosDB.Tests
+{
+    public class EventHubsManagementRecordedTestSanitizer : RecordedTestSanitizer
+    {
+        public EventHubsManagementRecordedTestSanitizer() : base()
+        {
+            // Lazy sanitize fields in the request and response bodies
+            AddJsonPathSanitizer("$..primaryMasterKey");
+            AddJsonPathSanitizer("$..primaryReadonlyMasterKey");
+            AddJsonPathSanitizer("$..secondaryMasterKey");
+            AddJsonPathSanitizer("$..secondaryReadonlyMasterKey");
+        }
+
+        public override void Sanitize(RecordSession session)
+        {
+            base.Sanitize(session);
+        }
+    }
+}
