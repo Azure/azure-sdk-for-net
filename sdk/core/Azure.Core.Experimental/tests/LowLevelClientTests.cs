3e7655da9aac3197e845b34ee13f1cb47aa97edf
Anne Thompson <annelo@microsoft.com>
2021-09-08 15:51:55 -0700

    intial set-up for llc tests

---
+++ sdk/core/Azure.Core.Experimental/tests/LowLevelClientTests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core.Experimental.Tests;
+using Azure.Core.Experimental.Tests.Models;
+using Azure.Core.TestFramework;
+using Azure.Identity;
+using NUnit.Framework;
+
+namespace Azure.Core.Tests
+{
+    public class LowLevelClientTests : ClientTestBase
+    {
+        public LowLevelClientTests(bool isAsync) : base(isAsync)
+        {
+        }
+
+        private PetStoreClient client { get; set; }
+        private readonly Uri _url = new Uri("https://example.azurepetstore.com");
+
+        private TokenCredential GetCredential()
+        {
+            return new EnvironmentCredential();
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+            client = InstrumentClient(new PetStoreClient(_url, GetCredential()));
+        }
+
+        [Test]
+        public async Task CanCallLlcGetMethodAsync()
+        {
+            Response response = await client.GetPetAsync("pet1", new RequestOptions());
+        }
+
+        [Test]
+        public async Task CanCallHlcGetMethodAsync()
+        {
+            Response<Pet> pet = await client.GetPetAsync("pet1");
+        }
+    }
+}
---
+++ sdk/core/Azure.Core.Experimental/tests/autorest.md
@@ -0,0 +1,25 @@
+﻿# Pet Shop Service
+
+Run `dotnet build /t:GenerateCode` to generate code.
+
+### AutoRest Configuration
+> see https://aka.ms/autorest
+
+``` yaml
+input-file:
+  - $(this-folder)/swagger/petStoreService.json
+#namespace: Azure.Analytics.Synapse.Administration
+public-clients: true
+low-level-client: true
+security: AADToken
+security-scopes: https://example.azurepetshop.com/.default
+```
+
+### Make Endpoint type as Uri
+
+``` yaml
+directive:
+  from: swagger-document
+  where: $.parameters.Endpoint
+  transform: $.format = "url"
+```
