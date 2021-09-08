bdcf7875d2f6d627f3a8295bc46ba130a353de83
yukun-dong <dongyukun1995@163.com>
2021-09-08 17:19:23 +0800

    remove unused json record

---
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/tests/Tests/EventHubTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Azure.ResourceManager.Resources;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.EventHubs.Models;
+using Azure.ResourceManager.EventHubs;
+using Azure.ResourceManager.EventHubs.Tests.Helpers;
+using Azure.ResourceManager.Resources.Models;
+
+namespace Azure.ResourceManager.EventHubs.Tests.Tests
+{
+    public class EventHubTests: EventHubTestBase
+    {
+        public EventHubTests(bool isAsync) : base(isAsync)
+        {
+        }
+    }
+}
