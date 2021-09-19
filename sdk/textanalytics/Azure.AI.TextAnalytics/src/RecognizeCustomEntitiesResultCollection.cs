5e7ff40218dae6d986c6669884f8b7fd4ec3972e
Salah Mostafa <v-samostafa@microsoft.com>
2021-09-19 18:02:15 +0200

    Added Recognize Custom Entities Action base models

--- sdk/textanalytics/Azure.AI.TextAnalytics/src/RecognizeCustomEntitiesResultCollection.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/RecognizeCustomEntitiesResultCollection.cs
@@ -1,10 +1,88 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Azure.AI.TextAnalytics
 {
-    class RecognizeCustomEntitiesResultCollection
+    /// <summary>
+    /// Collection of <see cref="RecognizeCustomEntitiesActionResult"/> objects corresponding
+    /// to a batch of documents, and information about the batch operation.
+    /// </summary>
+    [DebuggerTypeProxy(typeof(RecognizeCustomEntitiesResultCollectionDebugView))]
+    public class RecognizeCustomEntitiesResultCollection : ReadOnlyCollection<RecognizeEntitiesResult>
     {
+        internal RecognizeCustomEntitiesResultCollection(IList<RecognizeEntitiesResult> list, TextDocumentBatchStatistics statistics, string projectName, string deploymentName) : base(list)
+        {
+            Statistics = statistics;
+            DeploymentName = deploymentName;
+            ProjectName = projectName;
+        }
+
+        /// <summary>
+        /// Gets statistics about the documents and how it was processed
+        /// by the service.  This property will have a value when IncludeStatistics
+        /// is set to true in the client call.
+        /// </summary>
+        public TextDocumentBatchStatistics Statistics { get; }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public string DeploymentName { get; }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public string ProjectName { get; }
+
+        /// <summary>
+        /// Debugger Proxy class for <see cref="RecognizeCustomEntitiesResultCollectionDebugView"/>.
+        /// </summary>
+        internal class RecognizeCustomEntitiesResultCollectionDebugView
+        {
+            private RecognizeCustomEntitiesResultCollection BaseCollection { get; }
+
+            public RecognizeCustomEntitiesResultCollectionDebugView(RecognizeCustomEntitiesResultCollection collection)
+            {
+                BaseCollection = collection;
+            }
+
+            [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+            public List<RecognizeEntitiesResult> Items
+            {
+                get
+                {
+                    return BaseCollection.ToList();
+                }
+            }
+
+            public TextDocumentBatchStatistics Statistics
+            {
+                get
+                {
+                    return BaseCollection.Statistics;
+                }
+            }
+
+            public string ProjectName
+            {
+                get
+                {
+                    return BaseCollection.ProjectName;
+                }
+            }
+
+            public string DeploymentName
+            {
+                get
+                {
+                    return BaseCollection.DeploymentName;
+                }
+            }
+        }
     }
 }
