aca84c24404f4e9507bde84f692941528303960b
Salah Mostafa <v-samostafa@microsoft.com>
2021-09-19 17:45:59 +0200

    Added Recognize Custom Entities Action base models

---
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/RecognizeCustomEntitiesAction.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.AI.TextAnalytics
+{
+    /// <summary>
+    /// Configurations that allow callers to specify details about how to execute
+    /// a Recognize Custom Entities action in a set of documents.
+    /// For example, set project name and disable deployment name.
+    /// </summary>
+    public class RecognizeCustomEntitiesAction
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RecognizeCustomEntitiesAction"/>
+        /// class which allows callers to specify details about how to execute
+        /// a Recognize Entities action in a set of documents.
+        /// For example, set project name and disable deployment name.
+        /// </summary>
+        public RecognizeCustomEntitiesAction()
+        {
+        }
+
+        /// <summary> Gets the project name. </summary>
+        public string ProjectName { get; }
+        /// <summary> Gets the deployment name. </summary>
+        public string DeploymentName { get; }
+        /// <summary> Gets the deployment name. </summary>
+        public bool? DisableServiceLogs { get; set; }
+    }
+}
