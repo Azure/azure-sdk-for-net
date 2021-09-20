0dd8fe6a4030af66360368a03afa30a8cc5b380a
Salah Mostafa <v-samostafa@microsoft.com>
2021-09-20 12:44:39 +0200

    Added Samples

---
+++ sdk/textanalytics/Azure.AI.TextAnalytics/tests/samples/Sample9_RecognizeCustomEntities.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.AI.TextAnalytics.Tests.samples
+{
+    public partial class RecognizeCustomEntitiesSamples : SamplesBase<TextAnalyticsTestEnvironment>
+    {
+        [Test]
+        public void RecognizeCustomEntities()
+        {
+            // Create a text analytics client.
+            string endpoint = TestEnvironment.Endpoint;
+            string apiKey = TestEnvironment.ApiKey;
+            var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
+
+            // Get input document.
+            var documents = new List<string>() { @"There are so many ways of arranging a deck of cards that, after shuffling it, 
+                                                   it's almost guaranteed that the resulting sequence of cards has never appeared in the history of humanity." };
+
+            //prepare actions
+            var actions = new TextAnalyticsActions()
+            {
+                RecognizeCustomEntitiesActions = new List<RecognizeCustomEntitiesAction>()
+                {
+                    new RecognizeCustomEntitiesAction("<project-name>", "<deployment-name>")
+                }
+            };
+
+            AnalyzeActionsOperation operation = client.StartAnalyzeActions(documents, actions);
+
+            TimeSpan pollingInterval = new TimeSpan(1000);
+
+            while (!operation.HasCompleted)
+            {
+                Thread.Sleep(pollingInterval);
+                operation.UpdateStatus();
+
+                Console.WriteLine($"Status: {operation.Status}");
+                //If operation has not started, all other fields are null
+                if (operation.Status != TextAnalyticsOperationStatus.NotStarted)
+                {
+                    Console.WriteLine($"Expires On: {operation.ExpiresOn}");
+                    Console.WriteLine($"Last modified: {operation.LastModified}");
+                    if (!string.IsNullOrEmpty(operation.DisplayName))
+                        Console.WriteLine($"Display name: {operation.DisplayName}");
+                    Console.WriteLine($"Total actions: {operation.ActionsTotal}");
+                    Console.WriteLine($"  Succeeded actions: {operation.ActionsSucceeded}");
+                    Console.WriteLine($"  Failed actions: {operation.ActionsFailed}");
+                    Console.WriteLine($"  In progress actions: {operation.ActionsInProgress}");
+                }
+            }
+        }
+    }
+}
---
+++ sdk/textanalytics/Azure.AI.TextAnalytics/tests/samples/Sample9_RecognizeCustomEntitiesAsync.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.AI.TextAnalytics.Tests.samples
+{
+    public partial class RecognizeCustomEntitiesSamples : SamplesBase<TextAnalyticsTestEnvironment>
+    {
+        [Test]
+        public async Task RecognizeCustomEntitiesAsync()
+        {
+            // Create a text analytics client.
+            string endpoint = TestEnvironment.Endpoint;
+            string apiKey = TestEnvironment.ApiKey;
+            var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
+
+            // Get input document.
+            var documents = new List<string>() { @"There are so many ways of arranging a deck of cards that, after shuffling it, 
+                                                   it's almost guaranteed that the resulting sequence of cards has never appeared in the history of humanity." };
+
+            //prepare actions
+            var actions = new TextAnalyticsActions()
+            {
+                RecognizeCustomEntitiesActions = new List<RecognizeCustomEntitiesAction>()
+                {
+                    new RecognizeCustomEntitiesAction("<project-name>", "<deployment-name>")
+                }
+            };
+
+            AnalyzeActionsOperation operation = await client.StartAnalyzeActionsAsync(documents, actions);
+
+            await operation.WaitForCompletionAsync();
+
+            await foreach (var result in operation.Value)
+            {
+                var results = result.RecognizeCustomEntitiesActionResult;
+            }
+        }
+    }
+}
