15ff3f40452e5f4cdcf678eed53372a39b5da54d
Amanda Nguyen <amnguye@microsoft.com>
2021-09-20 11:00:51 -0700

    Test recordings test

--- sdk/storage/Azure.Storage.Blobs/tests/BlobTestBase.cs
+++ sdk/storage/Azure.Storage.Blobs/tests/BlobTestBase.cs
@@ -47,7 +47,7 @@ namespace Azure.Storage.Test.Shared
             new Uri(TestConfigSecondary.BlobServiceSecondaryEndpoint).Host;
 
         public BlobTestBase(bool async, BlobClientOptions.ServiceVersion serviceVersion, RecordedTestMode? mode = null)
-            : base(async, RecordedTestMode.Live)
+            : base(async, mode)
         {
             _serviceVersion = serviceVersion;
         }
