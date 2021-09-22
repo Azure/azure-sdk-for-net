1cf6370fa2b1f7b7dcfd5942dac201a6fe1a676f
yukun-dong <dongyukun1995@163.com>
2021-09-22 12:31:24 +0800

    remove unused import

--- sdk/storage/Azure.ResourceManager.Storage/tests/Tests/StorageAccountTests.cs
+++ sdk/storage/Azure.ResourceManager.Storage/tests/Tests/StorageAccountTests.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using Azure.Identity;
 using Azure.ResourceManager.Resources;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.Storage.Models;
