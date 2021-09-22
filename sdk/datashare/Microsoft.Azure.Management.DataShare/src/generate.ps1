374059cfbe604dc27dec07ac78f08c57bbef5a9b
Faisal Al Tell <faisalaltell@microsoft.com>
2021-09-22 11:40:20 -0700

    run generate.ps1 to update sdk

--- sdk/datashare/Microsoft.Azure.Management.DataShare/src/generate.ps1
+++ sdk/datashare/Microsoft.Azure.Management.DataShare/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "datashare/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "datashare/resource-manager" -AutoRestVersion "v3"
