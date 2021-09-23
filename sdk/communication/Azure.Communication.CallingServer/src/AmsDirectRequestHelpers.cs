aa4c05a34c74f7c4278f952e841c7b6ce299ded5
Melissa Neubert <mneubert@microsoft.com>
2021-09-23 13:17:01 -0700

    Delete recording methods for CallingServerClient

---
+++ sdk/communication/Azure.Communication.CallingServer/src/AmsDirectRequestHelpers.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core;
+
+namespace Azure.Communication.CallingServer
+{
+    internal static class AmsDirectRequestHelpers
+    {
+        internal static HttpMessage GetHttpMessage(CallingServerClient client, Uri requestEndpoint, RequestMethod requestMethod, HttpRange? rangeHeader = null)
+        {
+            HttpMessage message = client._pipeline.CreateMessage();
+            Request request = message.Request;
+            request.Method = requestMethod;
+            var uri = new RequestUriBuilder();
+            uri.Reset(requestEndpoint);
+
+            request.Uri = uri;
+
+            if (rangeHeader != null)
+            {
+                request.Headers.Add(Constants.HeaderNames.Range, rangeHeader.ToString());
+            }
+
+            // Even if using an external location, we must use the acs resource's information to sign our request.
+            string path = uri.Path;
+            if (path.StartsWith("/", StringComparison.InvariantCulture))
+            {
+                path = path.Substring(1);
+            }
+
+            request.Headers.Add(Constants.HeaderNames.XMsHost, new Uri(client._resourceEndpoint).Authority);
+            message.SetProperty("uriToSignRequestWith", new Uri(client._resourceEndpoint + path));
+
+            message.BufferResponse = false;
+            return message;
+        }
+    }
+}
