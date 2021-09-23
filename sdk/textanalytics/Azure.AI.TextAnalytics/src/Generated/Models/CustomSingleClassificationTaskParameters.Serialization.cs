03f7f6d9fe4f5e81668361b689852955d2141f80
Ahmed Leithy <v-aleithy@microsoft.com>
2021-09-23 15:15:36 +0200

    Setting Parameters classes to internal

--- sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/CustomSingleClassificationTaskParameters.Serialization.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/CustomSingleClassificationTaskParameters.Serialization.cs
@@ -10,7 +10,7 @@ using Azure.Core;
 
 namespace Azure.AI.TextAnalytics.Models
 {
-    public partial class CustomSingleClassificationTaskParameters : IUtf8JsonSerializable
+    internal partial class CustomSingleClassificationTaskParameters : IUtf8JsonSerializable
     {
         void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
         {
