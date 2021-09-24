efc459ad186f9cb43467e72a61bacb610397b579
Zitong Yang <zityang@microsoft.com>
2021-09-24 12:27:38 +0800

    Remove js,java sample codes and rename directories

--- sdk/signalr/Microsoft.Azure.WebJobs.Extensions.SignalRService/samples/simple-chat/js/functionapp/negotiate/index.js
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-module.exports = function (context, req, connectionInfo) {
-  context.res = { body: connectionInfo };
-  context.done();
-};
