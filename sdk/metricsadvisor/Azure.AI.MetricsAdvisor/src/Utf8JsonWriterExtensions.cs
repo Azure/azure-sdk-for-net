2fd1730c767b86da719823d0dadc16438f900807
Yegun Kim <wlfka102@gmail.com>
2021-09-26 17:32:00 +0900

    Follow the pattern in https://github.com/Azure/autorest.csharp/blob/f790582c450f6f90e173a12e96c8e83a745a0b0f/src/assets/Generator.Shared/Utf8JsonWriterExtensions.cs#L52

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Utf8JsonWriterExtensions.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Utf8JsonWriterExtensions.cs
@@ -9,107 +9,63 @@ namespace Azure.AI.MetricsAdvisor
 {
     internal static class Utf8JsonWriterExtensions
     {
-        public static void WriteNullStringValue(this Utf8JsonWriter writer, string propertyName, string value)
-        {
-            if (value != null)
-            {
-                writer.WritePropertyName(propertyName);
-                writer.WriteStringValue(value);
-            }
-            else
-            {
-                writer.WriteNull(propertyName);
-            }
-        }
+        public static void WriteNullStringValue(this Utf8JsonWriter writer, string propertyName, string value) =>
+            writer.WriteObjectValue(propertyName, value);
 
-        public static void WriteNullStringValue(this Utf8JsonWriter writer, string propertyName, DateTimeOffset? value, string format)
-        {
-            if (value != null)
-            {
-                writer.WritePropertyName(propertyName);
-                writer.WriteStringValue(value.Value, format);
-            }
-            else
-            {
-                writer.WriteNull(propertyName);
-            }
-        }
+        public static void WriteNullStringValue(this Utf8JsonWriter writer, string propertyName, DateTimeOffset? value, string format) =>
+            writer.WriteObjectValue(propertyName, value != null ? TypeFormatters.ToString(value.Value, format) : null);
 
-        public static void WriteNullStringValue<T>(this Utf8JsonWriter writer, string propertyName, T? value) where T : struct
-        {
-            if (value != null)
-            {
-                writer.WritePropertyName(propertyName);
-                writer.WriteStringValue(value.Value.ToString());
-            }
-            else
-            {
-                writer.WriteNull(propertyName);
-            }
-        }
+        public static void WriteNullStringValue<T>(this Utf8JsonWriter writer, string propertyName, T? value) where T : struct =>
+            writer.WriteObjectValue(propertyName, value?.ToString());
 
-        public static void WriteNullNumberValue(this Utf8JsonWriter writer, string propertyName, int? value)
-        {
-            if (value != null)
-            {
-                writer.WritePropertyName(propertyName);
-                writer.WriteNumberValue(value.Value);
-            }
-            else
-            {
-                writer.WriteNull(propertyName);
-            }
-        }
+        public static void WriteNullNumberValue(this Utf8JsonWriter writer, string propertyName, int? value) =>
+            writer.WriteObjectValue(propertyName, value);
 
-        public static void WriteNullNumberValue(this Utf8JsonWriter writer, string propertyName, long? value)
-        {
-            if (value != null)
-            {
-                writer.WritePropertyName(propertyName);
-                writer.WriteNumberValue(value.Value);
-            }
-            else
-            {
-                writer.WriteNull(propertyName);
-            }
-        }
+        public static void WriteNullNumberValue(this Utf8JsonWriter writer, string propertyName, long? value) =>
+            writer.WriteObjectValue(propertyName, value);
 
-        public static void WriteNullNumberValue(this Utf8JsonWriter writer, string propertyName, double? value)
-        {
-            if (value != null)
-            {
-                writer.WritePropertyName(propertyName);
-                writer.WriteNumberValue(value.Value);
-            }
-            else
-            {
-                writer.WriteNull(propertyName);
-            }
-        }
+        public static void WriteNullNumberValue(this Utf8JsonWriter writer, string propertyName, double? value) =>
+            writer.WriteObjectValue(propertyName, value);
 
-        public static void WriteNullBooleanValue(this Utf8JsonWriter writer, string propertyName, bool? value)
-        {
-            if (value != null)
-            {
-                writer.WritePropertyName(propertyName);
-                writer.WriteBooleanValue(value.Value);
-            }
-            else
-            {
-                writer.WriteNull(propertyName);
-            }
-        }
+        public static void WriteNullBooleanValue(this Utf8JsonWriter writer, string propertyName, bool? value) =>
+            writer.WriteObjectValue(propertyName, value);
 
-        public static void WriteNullObjectValue(this Utf8JsonWriter writer, string propertyName, IUtf8JsonSerializable value)
+        public static void WriteNullObjectValue(this Utf8JsonWriter writer, string propertyName, IUtf8JsonSerializable value) =>
+            writer.WriteObjectValue(propertyName, value);
+
+        public static void WriteObjectValue(this Utf8JsonWriter writer, string propertyName, object value)
         {
-            if (value != null)
+            switch (value)
             {
-                writer.WritePropertyName(propertyName);
-                writer.WriteObjectValue(value);
-            }
-            else
-            {
-                writer.WriteNull(propertyName);
+                case null:
+                    writer.WriteNull(propertyName);
+                    break;
+                case string s:
+                    writer.WritePropertyName(propertyName);
+                    writer.WriteStringValue(s);
+                    break;
+                case int i:
+                    writer.WritePropertyName(propertyName);
+                    writer.WriteNumberValue(i);
+                    break;
+                case long l:
+                    writer.WritePropertyName(propertyName);
+                    writer.WriteNumberValue(l);
+                    break;
+                case double d:
+                    writer.WritePropertyName(propertyName);
+                    writer.WriteNumberValue(d);
+                    break;
+                case bool b:
+                    writer.WritePropertyName(propertyName);
+                    writer.WriteBooleanValue(b);
+                    break;
+                case IUtf8JsonSerializable serializable:
+                    writer.WritePropertyName(propertyName);
+                    writer.WriteObjectValue(serializable);
+                    break;
+                default:
+                    throw new NotSupportedException("Not supported type " + value.GetType());
             }
         }
     }
