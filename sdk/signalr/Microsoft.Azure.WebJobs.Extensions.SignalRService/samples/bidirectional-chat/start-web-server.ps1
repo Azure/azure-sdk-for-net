5988d51eff4b4462a26d6e7b8ac2049312c4c9b0
Zitong Yang <zityang@microsoft.com>
2021-09-27 20:16:41 +0800

    Remove samples

--- sdk/signalr/Microsoft.Azure.WebJobs.Extensions.SignalRService/samples/bidirectional-chat/start-web-server.ps1
+++
@@ -1,7 +0,0 @@
-Get-Command live-server | Out-Null
-If (! $?) {
-    # if last command fails, that means live-server not installed.
-    Write-Information "Start to install live-server:" 
-    npm install --global live-server
-}
-live-server ./content/index.html
--- sdk/signalr/Microsoft.Azure.WebJobs.Extensions.SignalRService/samples/bidirectional-chat/start-web-server.sh
+++
@@ -1,6 +0,0 @@
-if ! command -v live-server &> /dev/null
-then
-    echo "Start to install live-server:"
-    npm install --global live-server
-fi
-live-server ./content/index.html
