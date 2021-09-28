9b59ec5d3ccf4fa72bd56bc01412a97a73da1709
liurunliang <vghfvyyg@hotmail.com>
2021-09-28 16:32:34 +0800

    add tests (#24270)
    add tests for GlossaryClient and CatalogClient

--- sdk/purview/Azure.Analytics.Purview.Catalog/tests/CatalogClientTest.cs
+++ sdk/purview/Azure.Analytics.Purview.Catalog/tests/CatalogClientTest.cs
@@ -30,6 +30,34 @@ namespace Azure.Analytics.Purview.Catalog.Tests
             Assert.AreEqual(0, fetchBodyJson.GetProperty("@search.count").GetInt16());
         }
 
+        [RecordedTest]
+        public async Task Suggest()
+        {
+            PurviewCatalogClient client = GetCatalogClient();
+            var data = new
+            {
+                keywords = "sampledata.csv",
+            };
+            Response fetchResponse = await client.SuggestAsync(RequestContent.Create(data));
+            Assert.AreEqual(fetchResponse.Status, 200);
+            JsonElement fetchBodyJson = JsonDocument.Parse(GetContentFromResponse(fetchResponse)).RootElement;
+            Assert.AreEqual("s3://testpurview/sampledata.csv", fetchBodyJson.GetProperty("value")[0].GetProperty("qualifiedName").GetString());
+        }
+
+        [RecordedTest]
+        public async Task AutoComplete()
+        {
+            PurviewCatalogClient client = GetCatalogClient();
+            var data = new
+            {
+                keywords = "sampledata",
+            };
+            Response fetchResponse = await client.AutoCompleteAsync(RequestContent.Create(data));
+            Assert.AreEqual(fetchResponse.Status, 200);
+            JsonElement fetchBodyJson = JsonDocument.Parse(GetContentFromResponse(fetchResponse)).RootElement;
+            Assert.AreEqual("sampledata csv", fetchBodyJson.GetProperty("value")[0].GetProperty("queryPlusText").GetString());
+        }
+
         private static BinaryData GetContentFromResponse(Response r)
         {
             // Workaround azure/azure-sdk-for-net#21048, which prevents .Content from working when dealing with responses
---
+++ sdk/purview/Azure.Analytics.Purview.Catalog/tests/GlossaryClientTest.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.Analytics.Purview.Catalog.Tests
+{
+    public class GlossaryClientTest : GlossaryClientTestBase
+    {
+        public GlossaryClientTest(bool isAsync) : base(isAsync)
+        {
+        }
+
+        [RecordedTest]
+        public async Task GetGlossaryCategory()
+        {
+            var client = GetGlossariesClient();
+            Response fetchResponse = await client.GetGlossariesAsync();
+            Assert.AreEqual(200, fetchResponse.Status);
+            JsonElement fetchBodyJson = JsonDocument.Parse(GetContentFromResponse(fetchResponse)).RootElement;
+            Assert.AreEqual(1, fetchBodyJson.GetArrayLength());
+            JsonElement glossaryItemJson = fetchBodyJson[0];
+            Assert.AreEqual("Glossary", glossaryItemJson.GetProperty("name").GetString());
+        }
+        private static BinaryData GetContentFromResponse(Response r)
+        {
+            // Workaround azure/azure-sdk-for-net#21048, which prevents .Content from working when dealing with responses
+            // from the playback system.
+
+            MemoryStream ms = new MemoryStream();
+            r.ContentStream.CopyTo(ms);
+            return new BinaryData(ms.ToArray());
+        }
+    }
+}
