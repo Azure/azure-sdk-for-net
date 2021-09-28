3daf2b5caf2ac5c96aa070c63b7b08c349ebc1ba
Chengming Ruan <v-cruan@microsoft.com>
2021-09-28 15:11:17 +0800

    Add PrivateEndpointConnection Tests

--- sdk/webpubsub/Azure.ResourceManager.WebPubSub/tests/Tests/PrivateEndpointConnectionTests.cs
+++ sdk/webpubsub/Azure.ResourceManager.WebPubSub/tests/Tests/PrivateEndpointConnectionTests.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Azure.Core.TestFramework;
+using Azure.ResourceManager.Network;
+using Azure.ResourceManager.Network.Models;
 using Azure.ResourceManager.Network.Tests.Helpers;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.Resources.Models;
@@ -65,12 +67,6 @@ namespace Azure.ResourceManager.WebPubSub.Tests
             StopSessionRecording();
         }
 
-        [OneTimeTearDown]
-        public async Task GlobleTearDown()
-        {
-            await _resourceGroup.DeleteAsync();
-        }
-
         [SetUp]
         public async Task TestSetUp()
         {
@@ -78,12 +74,103 @@ namespace Azure.ResourceManager.WebPubSub.Tests
             _resourceGroup = await client.GetResourceGroup(_resourceGroupIdentifier).GetAsync();
         }
 
+        [TearDown]
+        public async Task TestTearDown()
+        {
+            var privateEndpointConnectionlist = await _webPubSub.GetPrivateEndpointConnections().GetAllAsync().ToEnumerableAsync();
+            foreach (var item in privateEndpointConnectionlist)
+            {
+                await item.DeleteAsync();
+            }
+        }
+
+        public async Task CreatePrivateEndpointConnection(string privateEndPointName)
+        {
+            //create vnet
+            string vnetName = SessionRecording.GenerateAssetName("vnet-");
+            var vnetData = new VirtualNetworkData()
+            {
+                Location = "westus2",
+                AddressSpace = new AddressSpace()
+                {
+                    AddressPrefixes = { "10.10.0.0/16", }
+                },
+                Subnets =
+                {
+                    new SubnetData() { Name = "subnet01", AddressPrefix = "10.10.1.0/24", },
+                    new SubnetData() { Name = "subnet02", AddressPrefix = "10.10.2.0/24", PrivateEndpointNetworkPolicies = "Disabled", }
+                },
+            };
+            var vnetContainer = _resourceGroup.GetVirtualNetworks();
+            var vnet = await vnetContainer.CreateOrUpdateAsync(vnetName, vnetData);
+
+            //create private endpoint (privateEndpoint of WebPubSUb will be generated automatically)
+            var privateEndPointData = new PrivateEndpointData()
+            {
+                Subnet = new SubnetData() { Id = $"{vnet.Value.Id}" + "/subnets/subnet02" },
+                Location = "westus2",
+                PrivateLinkServiceConnections =
+                {
+                    new PrivateLinkServiceConnection()
+                    {
+                        Name = privateEndPointName,
+                        PrivateLinkServiceId = _webPubSub.Data.Id.ToString(),
+                        GroupIds = { "webpubsub" },
+                    }
+                },
+            };
+            var privateEndPointContainer = _resourceGroup.GetPrivateEndpoints();
+            var privateEndPointLro = await (await privateEndPointContainer.CreateOrUpdateAsync(privateEndPointName, privateEndPointData)).WaitForCompletionAsync();
+        }
+
+        [Test]
+        [RecordedTest]
+        public async Task Get()
+        {
+            string privateEndPointName = SessionRecording.GenerateAssetName("privateEndPoint-");
+            await CreatePrivateEndpointConnection(privateEndPointName);
+            var list = await _webPubSub.GetPrivateEndpointConnections().GetAllAsync().ToEnumerableAsync();
+            var PrivateEndpointConnection = await _webPubSub.GetPrivateEndpointConnections().GetAsync(list[0].Data.Name);
+            Assert.NotNull(PrivateEndpointConnection.Value.Data);
+            Assert.AreEqual("Approved", PrivateEndpointConnection.Value.Data.PrivateLinkServiceConnectionState.Status.ToString());
+        }
+
         [Test]
         [RecordedTest]
         public async Task GetAll()
         {
-            var container = _webPubSub.GetPrivateEndpointConnections();
-            var list = await container.GetAllAsync().ToEnumerableAsync();
+            var list = await _webPubSub.GetPrivateEndpointConnections().GetAllAsync().ToEnumerableAsync();
+            Assert.AreEqual(0, list.Count);
+            string privateEndPointName = SessionRecording.GenerateAssetName("privateEndPoint-");
+            await CreatePrivateEndpointConnection(privateEndPointName);
+            list = await _webPubSub.GetPrivateEndpointConnections().GetAllAsync().ToEnumerableAsync();
+            Assert.AreEqual(1, list.Count);
+        }
+
+        [Test]
+        [RecordedTest]
+        public async Task CheckIfExist()
+        {
+            string privateEndPointName = SessionRecording.GenerateAssetName("privateEndPoint-");
+            await CreatePrivateEndpointConnection(privateEndPointName);
+            var list = await _webPubSub.GetPrivateEndpointConnections().GetAllAsync().ToEnumerableAsync();
+            Assert.True(_webPubSub.GetPrivateEndpointConnections().CheckIfExists(list[0].Data.Name));
+            Assert.False(_webPubSub.GetPrivateEndpointConnections().CheckIfExists(list[0].Data.Name + "01"));
+        }
+
+        [Test]
+        [RecordedTest]
+        public async Task Delete()
+        {
+            string privateEndPointName = SessionRecording.GenerateAssetName("privateEndPoint-");
+            await CreatePrivateEndpointConnection(privateEndPointName);
+            var list = await _webPubSub.GetPrivateEndpointConnections().GetAllAsync().ToEnumerableAsync();
+            Assert.AreEqual(1, list.Count);
+            foreach (var item in list)
+            {
+                await item.DeleteAsync();
+            }
+            list = await _webPubSub.GetPrivateEndpointConnections().GetAllAsync().ToEnumerableAsync();
             Assert.AreEqual(0, list.Count);
         }
     }
