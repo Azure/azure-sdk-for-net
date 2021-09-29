29f374be84a248f5f77669d4f638bc0dd24e4512
Mike Harder <mharder@microsoft.com>
2021-09-29 12:48:30 -0700

    [Perf] Add helper classes for random values (#24303)

---
+++ common/Perf/Azure.Test.Perf/RandomByteArray.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Test.Perf
+{
+    public static class RandomByteArray
+    {
+        public static byte[] Create(int size)
+        {
+            var bytes = new byte[size];
+            ThreadsafeRandom.NextBytes(bytes);
+            return bytes;
+        }
+    }
+}
