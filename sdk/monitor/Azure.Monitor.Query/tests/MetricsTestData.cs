93b6fbf72adc48d28c693caa9c6d317a032bceaf
Pavel Krymets <pavel@krymets.com>
2021-09-30 13:46:33 -0700

    Try to fix Monitor Query tests hags again

--- sdk/monitor/Azure.Monitor.Query/tests/MetricsTestData.cs
+++ sdk/monitor/Azure.Monitor.Query/tests/MetricsTestData.cs
@@ -31,7 +31,7 @@ namespace Azure.Monitor.Query.Tests
 
             var recordingUtcNow = dateTimeOffset;
             // Snap to 15 minute intervals
-            StartTime = recordingUtcNow.AddTicks(- (Duration.Ticks + recordingUtcNow.Ticks % Duration.Ticks));
+            StartTime = recordingUtcNow.AddTicks(- (recordingUtcNow.Ticks % Duration.Ticks));
 
             MetricName = "CowsHappiness";
             MetricNamespace = "Cows";
