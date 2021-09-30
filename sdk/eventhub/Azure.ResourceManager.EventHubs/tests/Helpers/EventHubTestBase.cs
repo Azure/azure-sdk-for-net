cd384e9cf3293ba20184fec0b98500e528ef796d
yukun-dong <dongyukun1995@163.com>
2021-09-30 17:22:34 +0800

    modify test base

--- sdk/eventhub/Azure.ResourceManager.EventHubs/tests/Helpers/EventHubTestBase.cs
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/tests/Helpers/EventHubTestBase.cs
@@ -21,12 +21,12 @@ namespace Azure.ResourceManager.EventHubs.Tests.Helpers
         internal const string DefaultNamespaceAuthorizationRule = "RootManageSharedAccessKey";
         protected Subscription DefaultSubscription => Client.DefaultSubscription;
         protected ArmClient Client { get; private set; }
-        protected EventHubTestBase(bool isAsync) : base(isAsync,RecordedTestMode.Playback)
+        protected EventHubTestBase(bool isAsync) : base(isAsync)
         {
             Sanitizer = new EventHubRecordedTestSanitizer();
         }
 
-        public EventHubTestBase(bool isAsync, RecordedTestMode mode) : base(isAsync, RecordedTestMode.Playback)
+        public EventHubTestBase(bool isAsync, RecordedTestMode mode) : base(isAsync, mode)
         {
             Sanitizer = new EventHubRecordedTestSanitizer();
         }
