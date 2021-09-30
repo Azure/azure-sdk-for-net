5781cc84aa16625515a9151e37900a5821d29871
jschrepp-MSFT <41338290+jschrepp-MSFT@users.noreply.github.com>
2021-09-30 10:19:37 -0700

    Initial test resource refactor

---
+++ sdk/storage/Azure.Storage.Blobs/tests/ClientBuilderExtensions.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Test.Shared;
+
+namespace Azure.Storage.Blobs.Tests
+{
+    public static class ClientBuilderExtensions
+    {
+        public static string GetGarbageLeaseId(this ClientBuilder<BlobServiceClient, BlobClientOptions> clientBuilder)
+            => clientBuilder.Recording.Random.NewGuid().ToString();
+        public static string GetNewContainerName(this ClientBuilder<BlobServiceClient, BlobClientOptions> clientBuilder)
+            => $"test-container-{clientBuilder.Recording.Random.NewGuid()}";
+        public static string GetNewBlobName(this ClientBuilder<BlobServiceClient, BlobClientOptions> clientBuilder)
+            => $"test-blob-{clientBuilder.Recording.Random.NewGuid()}";
+        public static string GetNewBlockName(this ClientBuilder<BlobServiceClient, BlobClientOptions> clientBuilder)
+            => $"test-block-{clientBuilder.Recording.Random.NewGuid()}";
+        public static string GetNewNonAsciiBlobName(this ClientBuilder<BlobServiceClient, BlobClientOptions> clientBuilder)
+            => $"test-β£©þ‽%3A-{clientBuilder.Recording.Random.NewGuid()}";
+
+        public static async Task<DisposingContainer> GetTestContainerAsync(
+            this ClientBuilder<BlobServiceClient, BlobClientOptions> clientBuilder,
+            BlobServiceClient service = default,
+            string containerName = default,
+            IDictionary<string, string> metadata = default,
+            PublicAccessType? publicAccessType = default,
+            bool premium = default)
+        {
+            containerName ??= clientBuilder.GetNewContainerName();
+            service ??= clientBuilder.GetServiceClient_SharedKey();
+
+            if (publicAccessType == default)
+            {
+                publicAccessType = premium ? PublicAccessType.None : PublicAccessType.BlobContainer;
+            }
+
+            BlobContainerClient container = clientBuilder.AzureCoreRecordedTestBase.InstrumentClient(service.GetBlobContainerClient(containerName));
+            await container.CreateIfNotExistsAsync(metadata: metadata, publicAccessType: publicAccessType.Value);
+            return new DisposingContainer(container);
+        }
+
+        public class DisposingContainer : IAsyncDisposable
+        {
+            public BlobContainerClient Container;
+
+            public DisposingContainer(BlobContainerClient client)
+            {
+                Container = client;
+            }
+
+            public async ValueTask DisposeAsync()
+            {
+                if (Container != null)
+                {
+                    try
+                    {
+                        await Container.DeleteIfExistsAsync();
+                        Container = null;
+                    }
+                    catch
+                    {
+                        // swallow the exception to avoid hiding another test failure
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sdk/storage/Azure.Storage.Common/tests/Shared/ClientBuilder.cs
@@ -0,0 +1,162 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.Storage.Sas;
+
+namespace Azure.Storage.Test.Shared
+{
+    public class ClientBuilder<TServiceClient, TServiceClientOptions>
+        where TServiceClient : class
+        where TServiceClientOptions : ClientOptions
+    {
+        #region Delegate Definitions
+        /// <summary>
+        /// Delegate for accessing a shared key credential constructor for a service client.
+        /// </summary>
+        public delegate TServiceClient GetServiceClientStorageSharedKeyCredential(Uri endpoint, StorageSharedKeyCredential credential, TServiceClientOptions clientOptions);
+
+        /// <summary>
+        /// Delegate for accessing a SAS credential constructor for a service client.
+        /// </summary>
+        public delegate TServiceClient GetServiceClientAzureSasCredential(Uri endpoint, AzureSasCredential credential, TServiceClientOptions clientOptions);
+
+        /// <summary>
+        /// Delegate for accessing a token credential constructor for a service client.
+        /// </summary>
+        public delegate TServiceClient GetServiceClientTokenCredential(Uri endpoint, TokenCredential credential, TServiceClientOptions clientOptions);
+
+        /// <summary>
+        /// Delegate for accessing a credential-less constructor for a service client.
+        /// </summary>
+        public delegate TServiceClient GetServiceClient(Uri endpoint, TServiceClientOptions clientOptions);
+
+        /// <summary>
+        /// Delegate for accessing constructor for service client options.
+        /// </summary>
+        /// <param name="serviceVersion"></param>
+        /// <returns></returns>
+        public delegate TServiceClientOptions GetServiceClientOptions();
+
+        /// <summary>
+        /// Delegate for applying Azure Core client instrumentation to a service client.
+        /// </summary>
+        public delegate TClient InstrumentServiceClient<TClient>(TClient client);
+
+        /// <summary>
+        /// Delegate for applying Azure Core client options instrumentation to client options.
+        /// </summary>
+        public delegate TServiceClientOptions InstrumentClientOptions(TServiceClientOptions client);
+        #endregion
+
+        private readonly GetServiceClientStorageSharedKeyCredential _getServiceClientStorageSharedKeyCredential;
+        private readonly GetServiceClientAzureSasCredential _getServiceClientAzureSasCredential;
+        private readonly GetServiceClientTokenCredential _getServiceClientTokenCredential;
+        private readonly GetServiceClient _getServiceClient;
+        private readonly GetServiceClientOptions _getServiceClientOptions;
+        private readonly InstrumentClientOptions _instrumentClientOptions;
+
+        private readonly TenantConfigurationBuilder _tenantConfigurationBuilder;
+
+        public RecordedTestBase AzureCoreRecordedTestBase => _tenantConfigurationBuilder.AzureCoreRecordedTestBase;
+
+        public TestRecording Recording => AzureCoreRecordedTestBase.Recording;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="tenantConfigurationBuilder">Source of tenants for appropriate service clients.</param>
+        /// <param name="getServiceClient">Constructs a service client from the given arguments.</param>
+        /// <param name="getServiceClientStorageSharedKeyCredential">Constructs a service client from the given arguments.</param>
+        /// <param name="getServiceClientTokenCredential">Constructs a service client from the given arguments.</param>
+        /// <param name="getServiceClientAzureSasCredential">Constructs a service client from the given arguments.</param>
+        /// <param name="instrumentClient">Delegate to the test environment's InstrumentClient functionality.</param>
+        public ClientBuilder(
+            TenantConfigurationBuilder tenantConfigurationBuilder,
+            GetServiceClient getServiceClient,
+            GetServiceClientStorageSharedKeyCredential getServiceClientStorageSharedKeyCredential,
+            GetServiceClientTokenCredential getServiceClientTokenCredential,
+            GetServiceClientAzureSasCredential getServiceClientAzureSasCredential,
+            GetServiceClientOptions getServiceClientOptions)
+        {
+            _tenantConfigurationBuilder = tenantConfigurationBuilder;
+            _getServiceClient = getServiceClient;
+            _getServiceClientStorageSharedKeyCredential = getServiceClientStorageSharedKeyCredential;
+            _getServiceClientTokenCredential = getServiceClientTokenCredential;
+            _getServiceClientAzureSasCredential = getServiceClientAzureSasCredential;
+            _getServiceClientOptions = getServiceClientOptions;
+        }
+
+        public TServiceClient GetServiceClient_SharedKey(TServiceClientOptions options = default)
+            => GetServiceClientFromSharedKeyConfig(_tenantConfigurationBuilder.TestConfigDefault, options);
+
+        public TServiceClient GetServiceClient_SecondaryAccount_SharedKey()
+            => GetServiceClientFromSharedKeyConfig(_tenantConfigurationBuilder.TestConfigSecondary);
+
+        public TServiceClient GetServiceClient_PreviewAccount_SharedKey()
+            => GetServiceClientFromSharedKeyConfig(_tenantConfigurationBuilder.TestConfigPreviewBlob);
+
+        public TServiceClient GetServiceClient_PremiumBlobAccount_SharedKey()
+            => GetServiceClientFromSharedKeyConfig(_tenantConfigurationBuilder.TestConfigPremiumBlob);
+
+        public TServiceClient GetServiceClient_OAuthAccount_SharedKey() =>
+            GetServiceClientFromSharedKeyConfig(_tenantConfigurationBuilder.TestConfigOAuth);
+
+        public TServiceClient GetServiceClient_ManagedDisk() =>
+            GetServiceClientFromSharedKeyConfig(_tenantConfigurationBuilder.TestConfigManagedDisk);
+
+        public TServiceClient GetServiceClient_Hns() =>
+            GetServiceClientFromSharedKeyConfig(_tenantConfigurationBuilder.TestConfigHierarchicalNamespace);
+
+        public TServiceClient GetServiceClient_SoftDelete() =>
+            GetServiceClientFromSharedKeyConfig(_tenantConfigurationBuilder.TestConfigSoftDelete);
+
+        public SasQueryParameters GetNewAccountSas(
+            AccountSasResourceTypes resourceTypes = AccountSasResourceTypes.All,
+            AccountSasPermissions permissions = AccountSasPermissions.All,
+            StorageSharedKeyCredential sharedKeyCredentials = default)
+        {
+            var builder = new AccountSasBuilder
+            {
+                Protocol = SasProtocol.None,
+                Services = AccountSasServices.Blobs,
+                ResourceTypes = resourceTypes,
+                StartsOn = Recording.UtcNow.AddHours(-1),
+                ExpiresOn = Recording.UtcNow.AddHours(+1),
+                IPRange = new SasIPRange(IPAddress.None, IPAddress.None),
+                Version = Constants.DefaultSasVersion
+            };
+            builder.SetPermissions(permissions);
+            return builder.ToSasQueryParameters(sharedKeyCredentials ?? _tenantConfigurationBuilder.GetNewSharedKeyCredentials());
+        }
+
+        private TServiceClient GetServiceClientFromSharedKeyConfig(TenantConfiguration config, TServiceClientOptions options = default)
+            => AzureCoreRecordedTestBase.InstrumentClient(
+                _getServiceClientStorageSharedKeyCredential(
+                    new Uri(config.BlobServiceEndpoint),
+                    new StorageSharedKeyCredential(config.AccountName, config.AccountKey),
+                    options ?? _getServiceClientOptions()));
+
+        public TServiceClientOptions GetOptions(bool parallelRange = false)
+        {
+            var options = _getServiceClientOptions();
+            options.Diagnostics.IsLoggingEnabled = true;
+            options.Retry.Mode = RetryMode.Exponential;
+            options.Retry.MaxRetries = Constants.MaxReliabilityRetries;
+            options.Retry.Delay = TimeSpan.FromSeconds(Recording.Mode == RecordedTestMode.Playback ? 0.01 : 1);
+            options.Retry.MaxDelay = TimeSpan.FromSeconds(Recording.Mode == RecordedTestMode.Playback ? 0.1 : 60);
+            options.Retry.NetworkTimeout = TimeSpan.FromSeconds(Recording.Mode == RecordedTestMode.Playback ? 100 : 400);
+
+            if (Recording.Mode != RecordedTestMode.Live)
+            {
+                options.AddPolicy(new RecordedClientRequestIdPolicy(Recording, parallelRange), HttpPipelinePosition.PerCall);
+            }
+
+            return _instrumentClientOptions(options);
+        }
+    }
+}
