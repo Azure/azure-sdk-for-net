583f5f197ff4abb25ffd8868f8ad18fc067947b0
Zhenyu Zhou <zhezhou@microsoft.com>
2021-10-08 11:06:14 +0800

    Regenerate source code

--- sdk/hdinsight/Microsoft.Azure.Management.HDInsight/src/Generated/SdkInfo_HDInsightManagementClient.cs
+++ sdk/hdinsight/Microsoft.Azure.Management.HDInsight/src/Generated/SdkInfo_HDInsightManagementClient.cs
@@ -39,7 +39,7 @@ namespace Microsoft.Azure.Management.HDInsight
       public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/hdinsight/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=D:\\source\\azure-sdk-for-net\\sdk";
       public static readonly String GithubForkName = "Azure";
       public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "cbb456b8b823424ff498031a84e4892b0fa98480";
+      public static readonly String GithubCommidId = "2ebe584103a362fa87f2f60c9bab89ab410c7571";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
