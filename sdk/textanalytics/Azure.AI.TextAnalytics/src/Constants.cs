96ae48d385babf1b7e9c793df627c429427dc6dc
zedy <zedy@wicresoft.com>
2021-10-08 14:39:32 +0800

    add default value

--- sdk/textanalytics/Azure.AI.TextAnalytics/src/Constants.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/Constants.cs
@@ -9,7 +9,7 @@ namespace Azure.AI.TextAnalytics
     internal static class Constants
     {
         public static readonly StringIndexType DefaultStringIndexType = StringIndexType.Utf16CodeUnit;
-        public static string CognitiveServicesEndpointSuffix => Environment.GetEnvironmentVariable("TEXT_ANALYTICS_DEFAULT_SCOPE");
+        public static string CognitiveServicesEndpointSuffix => Environment.GetEnvironmentVariable("TEXT_ANALYTICS_DEFAULT_SCOPE") ?? ".cognitiveservices.azure.com";
         public const string AuthorizationHeader = "Ocp-Apim-Subscription-Key";
 
         public static string getDefaultCognitiveScope()
