3344386a98820e1181b4a09fb5c3b59a1cde5a08
Anne Thompson <annelo@microsoft.com>
2021-10-12 15:43:18 -0700

    update generated API

--- sdk/synapse/Azure.Analytics.Synapse.AccessControl/api/Azure.Analytics.Synapse.AccessControl.netstandard2.0.cs
+++ sdk/synapse/Azure.Analytics.Synapse.AccessControl/api/Azure.Analytics.Synapse.AccessControl.netstandard2.0.cs
@@ -8,6 +8,16 @@ namespace Azure.Analytics.Synapse.AccessControl
             V2020_12_01 = 1,
         }
     }
+    public partial class RoleAssignmentDetails
+    {
+        internal RoleAssignmentDetails() { }
+        public string Id { get { throw null; } }
+        public System.Guid? PrincipalId { get { throw null; } }
+        public string PrincipalType { get { throw null; } }
+        public System.Guid? RoleDefinitionId { get { throw null; } }
+        public string Scope { get { throw null; } }
+        public static implicit operator Azure.Analytics.Synapse.AccessControl.RoleAssignmentDetails (Azure.Response response) { throw null; }
+    }
     public partial class RoleAssignmentsClient
     {
         protected RoleAssignmentsClient() { }
@@ -19,6 +29,7 @@ namespace Azure.Analytics.Synapse.AccessControl
         public virtual System.Threading.Tasks.Task<Azure.Response> CreateRoleAssignmentAsync(string roleAssignmentId, Azure.Core.RequestContent content, Azure.RequestOptions options = null) { throw null; }
         public virtual Azure.Response DeleteRoleAssignmentById(string roleAssignmentId, string scope = null, Azure.RequestOptions options = null) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> DeleteRoleAssignmentByIdAsync(string roleAssignmentId, string scope = null, Azure.RequestOptions options = null) { throw null; }
+        public virtual Azure.Response<Azure.Analytics.Synapse.AccessControl.RoleAssignmentDetails> GetRoleAssignmentById(string roleAssignmentId) { throw null; }
         public virtual Azure.Response GetRoleAssignmentById(string roleAssignmentId, Azure.RequestOptions options) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> GetRoleAssignmentByIdAsync(string roleAssignmentId, Azure.RequestOptions options) { throw null; }
         public virtual Azure.Response ListRoleAssignments(Azure.RequestOptions options, string roleId = null, string principalId = null, string scope = null, string continuationToken = null) { throw null; }
