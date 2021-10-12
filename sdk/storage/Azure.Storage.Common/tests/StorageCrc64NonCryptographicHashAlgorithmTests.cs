f5b9de0037fd02cd54962900fb68713d7338fc85
jschrepp-MSFT <41338290+jschrepp-MSFT@users.noreply.github.com>
2021-10-12 13:25:18 -0700

    file header

--- sdk/storage/Azure.Storage.Common/tests/StorageCrc64NonCryptographicHashAlgorithmTests.cs
+++ sdk/storage/Azure.Storage.Common/tests/StorageCrc64NonCryptographicHashAlgorithmTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,7 +40,7 @@ namespace Azure.Storage.Tests
                     Array.Copy(data, position, buffer, 0, bufferSize);
                     position += bufferSize;
 
-                    calculator.Append(data);
+                    calculator.Append(buffer);
                 }
                 while (position < data.Length);
 
