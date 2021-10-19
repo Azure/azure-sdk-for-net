d9476385b829599a521cb0198d5b15cba9a094fe
Soohwan Kim <sookim@microsoft.com>
2021-10-19 15:11:22 -0700

    Generated new code from 06-preview version

--- sdk/keyvault/Microsoft.Azure.Management.KeyVault/src/Generated/Models/VaultProperties.cs
+++ sdk/keyvault/Microsoft.Azure.Management.KeyVault/src/Generated/Models/VaultProperties.cs
@@ -44,6 +44,8 @@ namespace Microsoft.Azure.Management.KeyVault.Models
         /// access policies are required.</param>
         /// <param name="vaultUri">The URI of the vault for performing
         /// operations on keys and secrets.</param>
+        /// <param name="hsmPoolResourceId">The resource id of HSM
+        /// Pool.</param>
         /// <param name="enabledForDeployment">Property to specify whether
         /// Azure Virtual Machines are permitted to retrieve certificates
         /// stored as secrets from the key vault.</param>
@@ -82,14 +84,23 @@ namespace Microsoft.Azure.Management.KeyVault.Models
         /// - that is, the property does not accept false as its value.</param>
         /// <param name="networkAcls">Rules governing the accessibility of the
         /// key vault from specific network locations.</param>
+        /// <param name="provisioningState">Provisioning state of the vault.
+        /// Possible values include: 'Succeeded', 'RegisteringDns'</param>
         /// <param name="privateEndpointConnections">List of private endpoint
         /// connections associated with the key vault.</param>
-        public VaultProperties(System.Guid tenantId, Sku sku, IList<AccessPolicyEntry> accessPolicies = default(IList<AccessPolicyEntry>), string vaultUri = default(string), bool? enabledForDeployment = default(bool?), bool? enabledForDiskEncryption = default(bool?), bool? enabledForTemplateDeployment = default(bool?), bool? enableSoftDelete = default(bool?), int? softDeleteRetentionInDays = default(int?), bool? enableRbacAuthorization = default(bool?), CreateMode? createMode = default(CreateMode?), bool? enablePurgeProtection = default(bool?), NetworkRuleSet networkAcls = default(NetworkRuleSet), IList<PrivateEndpointConnectionItem> privateEndpointConnections = default(IList<PrivateEndpointConnectionItem>))
+        /// <param name="publicNetworkAccess">Property to specify whether the
+        /// vault will accept traffic from public internet. If set to
+        /// 'disabled' all traffic except private endpoint traffic and that
+        /// that originates from trusted services will be blocked. This will
+        /// override the set firewall rules, meaning that even if the firewall
+        /// rules are present we will not honor the rules.</param>
+        public VaultProperties(System.Guid tenantId, Sku sku, IList<AccessPolicyEntry> accessPolicies = default(IList<AccessPolicyEntry>), string vaultUri = default(string), string hsmPoolResourceId = default(string), bool? enabledForDeployment = default(bool?), bool? enabledForDiskEncryption = default(bool?), bool? enabledForTemplateDeployment = default(bool?), bool? enableSoftDelete = default(bool?), int? softDeleteRetentionInDays = default(int?), bool? enableRbacAuthorization = default(bool?), CreateMode? createMode = default(CreateMode?), bool? enablePurgeProtection = default(bool?), NetworkRuleSet networkAcls = default(NetworkRuleSet), string provisioningState = default(string), IList<PrivateEndpointConnectionItem> privateEndpointConnections = default(IList<PrivateEndpointConnectionItem>), string publicNetworkAccess = default(string))
         {
             TenantId = tenantId;
             Sku = sku;
             AccessPolicies = accessPolicies;
             VaultUri = vaultUri;
+            HsmPoolResourceId = hsmPoolResourceId;
             EnabledForDeployment = enabledForDeployment;
             EnabledForDiskEncryption = enabledForDiskEncryption;
             EnabledForTemplateDeployment = enabledForTemplateDeployment;
@@ -99,7 +110,9 @@ namespace Microsoft.Azure.Management.KeyVault.Models
             CreateMode = createMode;
             EnablePurgeProtection = enablePurgeProtection;
             NetworkAcls = networkAcls;
+            ProvisioningState = provisioningState;
             PrivateEndpointConnections = privateEndpointConnections;
+            PublicNetworkAccess = publicNetworkAccess;
             CustomInit();
         }
 
@@ -137,6 +150,12 @@ namespace Microsoft.Azure.Management.KeyVault.Models
         /// </summary>
         [JsonProperty(PropertyName = "vaultUri")]
         public string VaultUri { get; set; }
+
+        /// <summary>
+        /// Gets the resource id of HSM Pool.
+        /// </summary>
+        [JsonProperty(PropertyName = "hsmPoolResourceId")]
+        public string HsmPoolResourceId { get; private set; }
 
         /// <summary>
         /// Gets or sets property to specify whether Azure Virtual Machines are
@@ -218,11 +237,29 @@ namespace Microsoft.Azure.Management.KeyVault.Models
         public NetworkRuleSet NetworkAcls { get; set; }
 
         /// <summary>
+        /// Gets or sets provisioning state of the vault. Possible values
+        /// include: 'Succeeded', 'RegisteringDns'
+        /// </summary>
+        [JsonProperty(PropertyName = "provisioningState")]
+        public string ProvisioningState { get; set; }
+
+        /// <summary>
         /// Gets list of private endpoint connections associated with the key
         /// vault.
         /// </summary>
         [JsonProperty(PropertyName = "privateEndpointConnections")]
         public IList<PrivateEndpointConnectionItem> PrivateEndpointConnections { get; private set; }
+
+        /// <summary>
+        /// Gets or sets property to specify whether the vault will accept
+        /// traffic from public internet. If set to 'disabled' all traffic
+        /// except private endpoint traffic and that that originates from
+        /// trusted services will be blocked. This will override the set
+        /// firewall rules, meaning that even if the firewall rules are present
+        /// we will not honor the rules.
+        /// </summary>
+        [JsonProperty(PropertyName = "publicNetworkAccess")]
+        public string PublicNetworkAccess { get; set; }
 
         /// <summary>
         /// Validate the object.
