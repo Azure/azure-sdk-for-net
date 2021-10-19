4039d3c3d57b36e9d5c3e76533fe81e8ec7990a4
Jialin Xin <jixin@microsoft.com>
2021-10-19 16:59:36 +0800

    Init the Microsoft.Azure.WebPubSub.AspNetCore lib.

---
+++ sdk/webpubsub/Microsoft.Azure.WebPubSub.AspNetCore/src/Extensions/WebPubSubDependencyInjectionExtensions.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+#if NETCOREAPP3_0_OR_GREATER
+using System;
+using Microsoft.Azure.WebPubSub.AspNetCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods to add required services to use Azure Web PubSub service Hub methods.
+    /// </summary>
+    public static class WebPubSubDependencyInjectionExtensions
+    {
+        /// <summary>
+        /// Adds the minimum essential Azure Web PubSub services to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="configure">A callback to configure the <see cref="WebPubSubOptions"/>.</param>
+        /// <returns>The same instance of the <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddWebPubSub(this IServiceCollection services, Action<WebPubSubOptions> configure)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (configure == null)
+            {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            var options = new WebPubSubOptions();
+            configure.Invoke(options);
+
+            services.AddWebPubSub()
+                .TryAddSingleton(options);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the minimum essential Azure Web PubSub services to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <returns>The same instance of the <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddWebPubSub(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.TryAddSingleton<WebPubSubOptions>();
+
+            return services.AddSingleton<ServiceRequestHandlerAdapter>()
+                .AddSingleton<WebPubSubMarkerService>();
+        }
+    }
+}
+#endif
---
+++ sdk/webpubsub/Microsoft.Azure.WebPubSub.AspNetCore/tests/Samples/WebPubSubSample.HelloWorld.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Azure.WebPubSub.AspNetCore.Tests.Samples
+{
+    public class WebPubSubSample
+    {
+        #region Snippet:WebPubSubDependencyInjection
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebPubSub(o =>
+            {
+                o.ValidationOptions.Add("<connection-string>");
+            });
+        }
+        #endregion
+
+        #region Snippet:WebPubSubMapHub
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapWebPubSubHub<SampleHub>("/eventhander");
+            });
+        }
+        #endregion
+    }
+}
