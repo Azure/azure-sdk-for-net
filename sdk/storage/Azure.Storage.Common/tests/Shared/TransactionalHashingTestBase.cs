a0bdd6b730e72b71bdf119f43f9dd1bccc0d4fae
Amanda Nguyen <amnguye@microsoft.com>
2021-10-20 14:06:46 -0700

    More recordings

--- sdk/storage/Azure.Storage.Common/tests/Shared/TransactionalHashingTestBase.cs
+++ sdk/storage/Azure.Storage.Common/tests/Shared/TransactionalHashingTestBase.cs
@@ -31,7 +31,7 @@ namespace Azure.Storage.Test.Shared
             bool async,
             string generatedResourceNamePrefix = default,
             RecordedTestMode? mode = null)
-            : base(async, mode)
+            : base(async, RecordedTestMode.Record)
         {
             _generatedResourceNamePrefix = generatedResourceNamePrefix ?? "test-resource-";
         }
