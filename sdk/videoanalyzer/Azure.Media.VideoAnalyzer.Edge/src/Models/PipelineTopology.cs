09097004fabfa8795202031fa00f5f4d7e8da494
hivyas <61890270+hivyas@users.noreply.github.com>
2021-10-22 14:37:56 -0700

    [VideoAnalyzer] Updating sdk to 1.1 (#24800)
    * generated new models and updated sample
    
    * updated sample, readme, and changelog
    
    * addressing pr comments and exported api
    
    * renaming variable to address pr comment
    
    * removing en-us from link
    
    * changes based off pr comments

--- sdk/videoanalyzer/Azure.Media.VideoAnalyzer.Edge/src/Models/PipelineTopology.cs
+++ sdk/videoanalyzer/Azure.Media.VideoAnalyzer.Edge/src/Models/PipelineTopology.cs
@@ -1,10 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.IO;
-using System.Text;
 using System.Text.Json;
-using Azure.Core;
 
 namespace Azure.Media.VideoAnalyzer.Edge.Models
 {
@@ -17,8 +14,8 @@ namespace Azure.Media.VideoAnalyzer.Edge.Models
         /// <returns>A Json string representation of a PipelineTopology.</returns>
         public static PipelineTopology Deserialize(string json)
         {
-            using JsonDocument doc = JsonDocument.Parse(json);
-            JsonElement element = doc.RootElement;
+            using var doc = JsonDocument.Parse(json);
+            var element = doc.RootElement;
             return DeserializePipelineTopology(element);
         }
     }
