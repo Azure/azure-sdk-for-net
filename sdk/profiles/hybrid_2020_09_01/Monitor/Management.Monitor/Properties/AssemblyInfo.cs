c99c841fa39e57f15d8c0009f3256e5d7bb6fac9
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2021-10-22 10:19:28 +0800

    Updated to 1.0.0 (#24727)

--- sdk/profiles/hybrid_2020_09_01/Monitor/Management.Monitor/Properties/AssemblyInfo.cs
+++ sdk/profiles/hybrid_2020_09_01/Monitor/Management.Monitor/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Microsoft Azure Monitor Management hybrid_2020_09_01 Profile")]
 [assembly: AssemblyDescription("Microsoft Azure Monitor Management hybrid_2020_09_01 Profile")]
 
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
