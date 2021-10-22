f4913258f0fd7db99d40b0999fcc56f035b673ac
Mike Harder <mharder@microsoft.com>
2021-10-22 12:38:49 -0700

    [Perf] Add PerfTestBase and BatchPerfTest (#24653)
    - Made IPerfTest more generic to support additional test types
    - Added PostSetupAsync() and PreCleanupAsync() to support features like test-proxy
    - Added PerfTestBase for maximum control over test execution
    - Added BatchPerfTest for APIs which perform multiple operations per call
    - Moved most code from PerfTest to BatchPerfTest
    - Moved test-proxy, run loop, and collect metrics code from PerfProgram to BatchPerfTest
    - Added MockReceiverTest to validate BatchPerfTest
    - Preparing for addition of EventPerfTest in separate PR
    - No breaking changes to existing tests

--- common/Perf/Azure.Test.Perf/IPerfTest.cs
+++ common/Perf/Azure.Test.Perf/IPerfTest.cs
@@ -2,20 +2,66 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Azure.Test.Perf
 {
     internal interface IPerfTest : IDisposable, IAsyncDisposable
     {
+        // Total number of operations completed by RunAll()
+        // Reset after warmup
+        long CompletedOperations { get; }
+
+        // Elapsed time between start of warmup/run and last completed operation
+        // Reset after warmup
+        TimeSpan LastCompletionTime { get; }
+
+        // Elapsed time between start and end of each completed operation
+        // Reset after warmup
+        // Only populated if "--latency" option is enabled
+        IList<TimeSpan> Latencies { get; }
+
+        // Elapsed time between scheduled start and actual end of each completed operation
+        // Reset after warmup
+        // Only populated if both "--latency" and "--rate" options are enabled
+        IList<TimeSpan> CorrectedLatencies { get; }
+
+        // Channel containing the scheduled start time of each operation
+        // Also includes a Stopwatch measuring elapsed time since scheduled start
+        Channel<(TimeSpan Start, Stopwatch Stopwatch)> PendingOperations { get; set; }
+
+        // Setup called once across all parallel test instances
+        // Used to setup state that can be used by all test instances
         Task GlobalSetupAsync();
+
+        // Setup called once per parallel test instance
+        // Used to setup state specific to this test instance
         Task SetupAsync();
-        Task RecordAndStartPlayback();
-        void Run(CancellationToken cancellationToken);
-        Task RunAsync(CancellationToken cancellationToken);
-        Task StopPlayback();
+
+        // Post-setup called once per parallel test instance
+        // Used by base classes to setup state (like test-proxy) after all derived class setup is complete
+        Task PostSetupAsync();
+
+        // Run all sync tests, including both warmup and duration
+        void RunAll(CancellationToken cancellationToken);
+
+        // Run all async tests, including both warmup and duration
+        Task RunAllAsync(CancellationToken cancellationToken);
+
+        // Pre-cleanup called once per parallel test instance
+        // Used by base classes to cleanup state (like test-proxy) before all derived class cleanup runs
+        Task PreCleanupAsync();
+
+        // Cleanup called once per parallel test instance
+        // Used to setup state specific to this test instance
         Task CleanupAsync();
+
+        // Cleanup called once across all parallel test instances
+        // Used to cleanup state that can be used by all test instances
         Task GlobalCleanupAsync();
     }
 }
