54687db65e966b0d303453c34e27cac4dc9f9570
Mariana Rios Flores <mariari@microsoft.com>
2021-10-25 11:42:18 -0700

    change error code

--- sdk/translation/Azure.AI.Translation.Document/tests/TranslationOperationLiveTests.cs
+++ sdk/translation/Azure.AI.Translation.Document/tests/TranslationOperationLiveTests.cs
@@ -352,7 +352,7 @@ namespace Azure.AI.Translation.Document.Tests
             List<DocumentStatusResult> documentsList = await documents.ToEnumerableAsync();
             Assert.AreEqual(1, documentsList.Count);
             Assert.AreEqual(DocumentTranslationStatus.Failed, documentsList[0].Status);
-            Assert.AreEqual(new DocumentTranslationErrorCode("EmptyDocument"), documentsList[0].Error.ErrorCode);
+            Assert.AreEqual(new DocumentTranslationErrorCode("NoTranslatableText"), documentsList[0].Error.ErrorCode);
         }
 
         [RecordedTest]
