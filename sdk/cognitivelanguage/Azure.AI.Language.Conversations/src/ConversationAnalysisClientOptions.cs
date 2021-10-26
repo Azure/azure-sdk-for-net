cd024c857f4a9274d00ec294aaa59dd95df50ae4
Ahmed Leithy <v-aleithy@microsoft.com>
2021-10-26 11:47:16 +0200

    Removed private preview from service version enum

--- sdk/cognitivelanguage/Azure.AI.Language.Conversations/src/ConversationAnalysisClientOptions.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.Conversations/src/ConversationAnalysisClientOptions.cs
@@ -20,14 +20,9 @@ namespace Azure.AI.Language.Conversations
         {
 #pragma warning disable CA1707 // Identifiers should not contain underscores
             /// <summary>
-            /// Service version "2021-07-15-preview".
+            /// Service version "2021-11-01-preview".
             /// </summary>
-            V2021_07_15_Preview = 1,
-
-            /// <summary>
-            /// Service version "2021-07-15-preview".
-            /// </summary>
-            V2021_11_01_Preview = 2,
+            V2021_11_01_Preview = 1,
 #pragma warning restore CA1707 // Identifiers should not contain underscores
         }
 
@@ -40,7 +35,6 @@ namespace Azure.AI.Language.Conversations
         {
             Version = version switch
             {
-                ServiceVersion.V2021_07_15_Preview => "2021-07-15-preview",
                 ServiceVersion.V2021_11_01_Preview => "2021-11-01-preview",
                 _ => throw new NotSupportedException()
             };
