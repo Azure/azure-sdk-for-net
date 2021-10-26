94fcb2f881f3e7334f285de7156a508e4d13913d
Ohad Fein <ohadfein@microsoft.com>
2021-10-26 11:57:17 +0300

    Add error property to QueryResults

---
+++ sdk/operationalinsights/Microsoft.Azure.OperationalInsights/src/Customized/Models/QueryResponseError.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Azure.OperationalInsights.Models
+{
+    using System.Collections.Generic;
+
+    public class QueryResponseError
+    {
+        public string Code { get; set; }
+        public List<QueryResponseError> Details { get; set; }
+        public string Message { get; set; }
+        public QueryResponseInnerError InnerError { get; set; }
+    }
+}
---
+++ sdk/operationalinsights/Microsoft.Azure.OperationalInsights/src/Customized/Models/QueryResponseInnerError.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Azure.OperationalInsights.Models
+{
+    public class QueryResponseInnerError
+    {
+        public int Severity { get; set; }
+        public string SeverityName { get; set; }
+        public string Message { get; set; }
+        public string Code { get; set; }
+    }
+}
