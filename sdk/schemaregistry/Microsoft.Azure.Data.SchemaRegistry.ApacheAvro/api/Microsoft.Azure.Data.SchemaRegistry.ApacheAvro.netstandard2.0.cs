69d3c0eeba7095c7371d50f69fa6ce88cd7336d2
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-10-27 22:01:45 -0700

    Export API

--- sdk/schemaregistry/Microsoft.Azure.Data.SchemaRegistry.ApacheAvro/api/Microsoft.Azure.Data.SchemaRegistry.ApacheAvro.netstandard2.0.cs
+++ sdk/schemaregistry/Microsoft.Azure.Data.SchemaRegistry.ApacheAvro/api/Microsoft.Azure.Data.SchemaRegistry.ApacheAvro.netstandard2.0.cs
@@ -3,6 +3,12 @@ namespace Microsoft.Azure.Data.SchemaRegistry.ApacheAvro
     public partial class SchemaRegistryAvroEncoder
     {
         public SchemaRegistryAvroEncoder(Azure.Data.SchemaRegistry.SchemaRegistryClient client, string groupName, Microsoft.Azure.Data.SchemaRegistry.ApacheAvro.SchemaRegistryAvroObjectEncoderOptions options = null) { }
+        public object DecodeMessageBody(Azure.Messaging.MessageWithMetadata message, System.Type returnType, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public object DecodeMessageBody(Azure.Messaging.ReadOnlyMessageWithMetadata message, System.Type returnType, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public System.Threading.Tasks.ValueTask<object> DecodeMessageBodyAsync(Azure.Messaging.MessageWithMetadata message, System.Type returnType, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public System.Threading.Tasks.ValueTask<object> DecodeMessageBodyAsync(Azure.Messaging.ReadOnlyMessageWithMetadata message, System.Type returnType, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public void EncodeMessageBody(Azure.Messaging.MessageWithMetadata message, object value, System.Type inputType = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { }
+        public System.Threading.Tasks.Task EncodeMessageBodyAsync(Azure.Messaging.MessageWithMetadata message, object value, System.Type inputType = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
     }
     public static partial class SchemaRegistryAvroEncoderExtensions
     {
