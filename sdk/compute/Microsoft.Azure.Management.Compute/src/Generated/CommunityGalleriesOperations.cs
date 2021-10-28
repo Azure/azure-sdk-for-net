f8ab326516ea38a7ef5726a8bddc180288079d50
Theodore Chang <thchan@microsoft.com>
2021-10-28 20:11:59 +0100

    update with api version 2021-11-01

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/CommunityGalleriesOperations.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/CommunityGalleriesOperations.cs
@@ -23,12 +23,12 @@ namespace Microsoft.Azure.Management.Compute
     using System.Threading.Tasks;
 
     /// <summary>
-    /// Operations operations.
+    /// CommunityGalleriesOperations operations.
     /// </summary>
-    internal partial class Operations : IServiceOperations<ComputeManagementClient>, IOperations
+    internal partial class CommunityGalleriesOperations : IServiceOperations<ComputeManagementClient>, ICommunityGalleriesOperations
     {
         /// <summary>
-        /// Initializes a new instance of the Operations class.
+        /// Initializes a new instance of the CommunityGalleriesOperations class.
         /// </summary>
         /// <param name='client'>
         /// Reference to the service client.
@@ -36,7 +36,7 @@ namespace Microsoft.Azure.Management.Compute
         /// <exception cref="System.ArgumentNullException">
         /// Thrown when a required parameter is null
         /// </exception>
-        internal Operations(ComputeManagementClient client)
+        internal CommunityGalleriesOperations(ComputeManagementClient client)
         {
             if (client == null)
             {
@@ -51,8 +51,14 @@ namespace Microsoft.Azure.Management.Compute
         public ComputeManagementClient Client { get; private set; }
 
         /// <summary>
-        /// Gets a list of compute operations.
+        /// Get a community gallery by gallery public name.
         /// </summary>
+        /// <param name='location'>
+        /// Resource location.
+        /// </param>
+        /// <param name='publicGalleryName'>
+        /// The public name of the community gallery.
+        /// </param>
         /// <param name='customHeaders'>
         /// Headers that will be added to request.
         /// </param>
@@ -65,12 +71,30 @@ namespace Microsoft.Azure.Management.Compute
         /// <exception cref="SerializationException">
         /// Thrown when unable to deserialize the response
         /// </exception>
+        /// <exception cref="ValidationException">
+        /// Thrown when a required parameter is null
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when a required parameter is null
+        /// </exception>
         /// <return>
         /// A response object containing the response body and response headers.
         /// </return>
-        public async Task<AzureOperationResponse<IEnumerable<ComputeOperationValue>>> ListWithHttpMessagesAsync(Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<AzureOperationResponse<CommunityGallery>> GetWithHttpMessagesAsync(string location, string publicGalleryName, Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken))
         {
-            string apiVersion = "2021-11-01";
+            if (Client.SubscriptionId == null)
+            {
+                throw new ValidationException(ValidationRules.CannotBeNull, "this.Client.SubscriptionId");
+            }
+            if (location == null)
+            {
+                throw new ValidationException(ValidationRules.CannotBeNull, "location");
+            }
+            if (publicGalleryName == null)
+            {
+                throw new ValidationException(ValidationRules.CannotBeNull, "publicGalleryName");
+            }
+            string apiVersion = "2021-07-01";
             // Tracing
             bool _shouldTrace = ServiceClientTracing.IsEnabled;
             string _invocationId = null;
@@ -78,13 +102,18 @@ namespace Microsoft.Azure.Management.Compute
             {
                 _invocationId = ServiceClientTracing.NextInvocationId.ToString();
                 Dictionary<string, object> tracingParameters = new Dictionary<string, object>();
+                tracingParameters.Add("location", location);
+                tracingParameters.Add("publicGalleryName", publicGalleryName);
                 tracingParameters.Add("apiVersion", apiVersion);
                 tracingParameters.Add("cancellationToken", cancellationToken);
-                ServiceClientTracing.Enter(_invocationId, this, "List", tracingParameters);
+                ServiceClientTracing.Enter(_invocationId, this, "Get", tracingParameters);
             }
             // Construct URL
             var _baseUrl = Client.BaseUri.AbsoluteUri;
-            var _url = new System.Uri(new System.Uri(_baseUrl + (_baseUrl.EndsWith("/") ? "" : "/")), "providers/Microsoft.Compute/operations").ToString();
+            var _url = new System.Uri(new System.Uri(_baseUrl + (_baseUrl.EndsWith("/") ? "" : "/")), "subscriptions/{subscriptionId}/providers/Microsoft.Compute/locations/{location}/communityGalleries/{publicGalleryName}").ToString();
+            _url = _url.Replace("{subscriptionId}", System.Uri.EscapeDataString(Client.SubscriptionId));
+            _url = _url.Replace("{location}", System.Uri.EscapeDataString(location));
+            _url = _url.Replace("{publicGalleryName}", System.Uri.EscapeDataString(publicGalleryName));
             List<string> _queryParameters = new List<string>();
             if (apiVersion != null)
             {
@@ -183,7 +212,7 @@ namespace Microsoft.Azure.Management.Compute
                 throw ex;
             }
             // Create Result
-            var _result = new AzureOperationResponse<IEnumerable<ComputeOperationValue>>();
+            var _result = new AzureOperationResponse<CommunityGallery>();
             _result.Request = _httpRequest;
             _result.Response = _httpResponse;
             if (_httpResponse.Headers.Contains("x-ms-request-id"))
@@ -196,7 +225,7 @@ namespace Microsoft.Azure.Management.Compute
                 _responseContent = await _httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
                 try
                 {
-                    _result.Body = Rest.Serialization.SafeJsonConvert.DeserializeObject<Page<ComputeOperationValue>>(_responseContent, Client.DeserializationSettings);
+                    _result.Body = Rest.Serialization.SafeJsonConvert.DeserializeObject<CommunityGallery>(_responseContent, Client.DeserializationSettings);
                 }
                 catch (JsonException ex)
                 {
--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/SdkInfo_ComputeManagementClient.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/SdkInfo_ComputeManagementClient.cs
@@ -19,16 +19,19 @@ namespace Microsoft.Azure.Management.Compute
           {
               return new Tuple<string, string, string>[]
               {
-                new Tuple<string, string, string>("Compute", "AvailabilitySets", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "CapacityReservationGroups", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "CapacityReservations", "2021-07-01"),
+                new Tuple<string, string, string>("Compute", "AvailabilitySets", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "CapacityReservationGroups", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "CapacityReservations", "2021-11-01"),
                 new Tuple<string, string, string>("Compute", "CloudServiceOperatingSystems", "2021-03-01"),
                 new Tuple<string, string, string>("Compute", "CloudServiceRoleInstances", "2021-03-01"),
                 new Tuple<string, string, string>("Compute", "CloudServiceRoles", "2021-03-01"),
                 new Tuple<string, string, string>("Compute", "CloudServices", "2021-03-01"),
                 new Tuple<string, string, string>("Compute", "CloudServicesUpdateDomain", "2021-03-01"),
-                new Tuple<string, string, string>("Compute", "DedicatedHostGroups", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "DedicatedHosts", "2021-07-01"),
+                new Tuple<string, string, string>("Compute", "CommunityGalleries", "2021-07-01"),
+                new Tuple<string, string, string>("Compute", "CommunityGalleryImageVersions", "2021-07-01"),
+                new Tuple<string, string, string>("Compute", "CommunityGalleryImages", "2021-07-01"),
+                new Tuple<string, string, string>("Compute", "DedicatedHostGroups", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "DedicatedHosts", "2021-11-01"),
                 new Tuple<string, string, string>("Compute", "DiskAccesses", "2021-04-01"),
                 new Tuple<string, string, string>("Compute", "DiskEncryptionSets", "2021-04-01"),
                 new Tuple<string, string, string>("Compute", "DiskRestorePoint", "2021-04-01"),
@@ -39,45 +42,34 @@ namespace Microsoft.Azure.Management.Compute
                 new Tuple<string, string, string>("Compute", "GalleryImageVersions", "2021-07-01"),
                 new Tuple<string, string, string>("Compute", "GalleryImages", "2021-07-01"),
                 new Tuple<string, string, string>("Compute", "GallerySharingProfile", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "Images", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "LogAnalytics", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "Operations", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "ProximityPlacementGroups", "2021-07-01"),
+                new Tuple<string, string, string>("Compute", "Images", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "LogAnalytics", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "Operations", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "ProximityPlacementGroups", "2021-11-01"),
                 new Tuple<string, string, string>("Compute", "ResourceSkus", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "RestorePointCollections", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "RestorePoints", "2021-07-01"),
+                new Tuple<string, string, string>("Compute", "RestorePointCollections", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "RestorePoints", "2021-11-01"),
                 new Tuple<string, string, string>("Compute", "SharedGalleries", "2021-07-01"),
                 new Tuple<string, string, string>("Compute", "SharedGalleryImageVersions", "2021-07-01"),
                 new Tuple<string, string, string>("Compute", "SharedGalleryImages", "2021-07-01"),
                 new Tuple<string, string, string>("Compute", "Snapshots", "2021-04-01"),
-                new Tuple<string, string, string>("Compute", "SshPublicKeys", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "Usage", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineExtensionImages", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineExtensions", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineImages", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineImagesEdgeZone", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineRunCommands", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetExtensions", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetRollingUpgrades", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetVMExtensions", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetVMRunCommands", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetVMs", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSets", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachineSizes", "2021-07-01"),
-                new Tuple<string, string, string>("Compute", "VirtualMachines", "2021-07-01"),
+                new Tuple<string, string, string>("Compute", "SshPublicKeys", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "Usage", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineExtensionImages", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineExtensions", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineImages", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineImagesEdgeZone", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineRunCommands", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetExtensions", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetRollingUpgrades", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetVMExtensions", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetVMRunCommands", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSetVMs", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineScaleSets", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachineSizes", "2021-11-01"),
+                new Tuple<string, string, string>("Compute", "VirtualMachines", "2021-11-01"),
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/compute/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\work\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "92b58eb71fd5eb2ce5600e9233541074ecabf851";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
