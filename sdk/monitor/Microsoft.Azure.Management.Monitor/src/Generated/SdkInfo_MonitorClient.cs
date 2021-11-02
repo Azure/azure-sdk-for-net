a6507bd32d79df44397d9a1f1fe15e2ee0ed60aa
Ritwik Rajendra <rirajend@microsoft.com>
2021-11-02 13:57:03 -0700

    Run latest Start-AutoRestCodeGeneration

--- sdk/monitor/Microsoft.Azure.Management.Monitor/src/Generated/SdkInfo_MonitorClient.cs
+++ sdk/monitor/Microsoft.Azure.Management.Monitor/src/Generated/SdkInfo_MonitorClient.cs
@@ -50,5 +50,16 @@ namespace Microsoft.Azure.Management.Monitor
               }.AsEnumerable();
           }
       }
+      // BEGIN: Code Generation Metadata Section
+      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestBootStrapperVersion = "autorest@3.4.2";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/main/specification/monitor/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --tag=package-2021-09 --csharp-sdks-folder=D:\\ICM\\azure-sdk-for-net\\sdk";
+      public static readonly String GithubForkName = "Azure";
+      public static readonly String GithubBranchName = "main";
+      public static readonly String GithubCommidId = "0fba9ba4b78d20fc1a7ec251dab889b7a7eb8afd";
+      public static readonly String CodeGenerationErrors = "";
+      public static readonly String GithubRepoName = "azure-rest-api-specs";
+      // END: Code Generation Metadata Section
   }
 }
+
