19263203f6d75022562d1f1f0f9062f5ee1db0b2
Thiago Diniz Maia <thdinizm@microsoft.com>
2021-11-09 10:04:49 -0800

    Buildable SDK

---
+++ sdk/communication/Azure.Communication.MediaComposition/src/MediaCompositionClient.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Communication.Pipeline;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace Azure.Communication.MediaComposition
+{
+    /// <summary>
+    /// The Azure Communication Services Media Composition client.
+    /// </summary>
+    public class MediaCompositionClient
+    {
+        internal readonly ClientDiagnostics _clientDiagnostics;
+        internal readonly HttpPipeline _pipeline;
+        internal readonly string _resourceEndpoint;
+
+        internal MediaCompositionRestClient RestClient { get; }
+
+        #region public constructors
+
+        /// <summary> Initializes a new instance of <see cref="MediaCompositionRestClient"/>.</summary>
+        public MediaCompositionClient(string connectionString)
+            : this(
+                  ConnectionString.Parse(Argument.CheckNotNullOrEmpty(connectionString, nameof(connectionString))),
+                  new MediaCompositionClientOptions())
+        { }
+
+        /// <summary> Initializes a new instance of <see cref="MediaCompositionRestClient"/>.</summary>
+        public MediaCompositionClient(string connectionString, MediaCompositionClientOptions options)
+            : this(
+                  ConnectionString.Parse(Argument.CheckNotNullOrEmpty(connectionString, nameof(connectionString))),
+                  options ?? new MediaCompositionClientOptions())
+        { }
+
+        #endregion
+
+        #region private constructors
+
+        private MediaCompositionClient(ConnectionString connectionString, MediaCompositionClientOptions options)
+            : this(connectionString.GetRequired("endpoint"), options.BuildHttpPipeline(connectionString), options)
+        { }
+
+        private MediaCompositionClient(string endpoint, HttpPipeline httpPipeline, MediaCompositionClientOptions options)
+        {
+            _pipeline = httpPipeline;
+            _resourceEndpoint = endpoint;
+            _clientDiagnostics = new ClientDiagnostics(options);
+            RestClient = new MediaCompositionRestClient(_clientDiagnostics, httpPipeline, new Uri(endpoint));
+        }
+
+        #endregion
+
+        #region protected constructors
+        /// <summary> Initializes a new instance of Media Composition Client for mocking. </summary>
+        protected MediaCompositionClient()
+        {
+        }
+        #endregion protected constructors
+    }
+}
---
+++ sdk/communication/Azure.Communication.MediaComposition/src/MediaCompositionClientOptions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core;
+
+namespace Azure.Communication.MediaComposition
+{
+    /// <summary>
+    /// The options for the Media Composition client.
+    /// </summary>
+    public class MediaCompositionClientOptions : ClientOptions
+    {
+        private const ServiceVersion LatestVersion = ServiceVersion.V2021_12_31_Preview;
+
+        internal string ApiVersion { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaCompositionClientOptions"/> class.
+        /// </summary>
+        public MediaCompositionClientOptions(ServiceVersion serviceVersion = LatestVersion)
+        {
+            ApiVersion = serviceVersion switch
+            {
+                ServiceVersion.V2021_12_31_Preview => "2021-12-31-preview",
+                _ => throw new ArgumentOutOfRangeException(nameof(serviceVersion))
+            };
+        }
+
+        /// <summary>
+        /// The Media Composition service version.
+        /// </summary>
+        public enum ServiceVersion
+        {
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+            /// <summary>
+            /// The Private Preview of the Media Composition service.
+            /// </summary>
+            V2021_12_31_Preview = 1
+#pragma warning restore CA1707
+        }
+    }
+}
