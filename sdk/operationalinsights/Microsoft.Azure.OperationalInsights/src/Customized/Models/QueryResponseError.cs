1de0ed3bc498c3e63c96699f931f438dcf784750
Ohad Fein <ohadfein@microsoft.com>
2021-11-10 10:52:49 +0200

    Added documentation comments for public properties

--- sdk/operationalinsights/Microsoft.Azure.OperationalInsights/src/Customized/Models/QueryResponseError.cs
+++ sdk/operationalinsights/Microsoft.Azure.OperationalInsights/src/Customized/Models/QueryResponseError.cs
@@ -4,9 +4,21 @@
 
     public class QueryResponseError
     {
+        /// <summary>
+        /// A machine readable error code.
+        /// </summary>
         public string Code { get; set; }
+        /// <summary>
+        /// Error details.
+        /// </summary>
         public List<QueryResponseError> Details { get; set; }
+        /// <summary>
+        /// A human readable error message.
+        /// </summary>
         public string Message { get; set; }
+        /// <summary>
+        /// Inner error details if they exist.
+        /// </summary>
         public QueryResponseInnerError InnerError { get; set; }
     }
 }
