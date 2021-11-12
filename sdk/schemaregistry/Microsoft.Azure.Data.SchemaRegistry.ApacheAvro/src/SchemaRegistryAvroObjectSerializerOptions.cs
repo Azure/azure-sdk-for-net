f2e4ea46b1704d85e3ee3ef69d7cde7e4eb7a1e4
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-11-11 16:37:30 -0800

    Sr avro serializer revert (#25287)
    * Revert encoder commit
    
    * Update dependency version
    
    * API

--- sdk/schemaregistry/Microsoft.Azure.Data.SchemaRegistry.ApacheAvro/src/SchemaRegistryAvroObjectSerializerOptions.cs
+++ sdk/schemaregistry/Microsoft.Azure.Data.SchemaRegistry.ApacheAvro/src/SchemaRegistryAvroObjectSerializerOptions.cs
@@ -4,9 +4,9 @@
 namespace Microsoft.Azure.Data.SchemaRegistry.ApacheAvro
 {
     /// <summary>
-    /// Options for <see cref="SchemaRegistryAvroEncoder"/>.
+    /// Options for <see cref="SchemaRegistryAvroObjectSerializer"/>.
     /// </summary>
-    public class SchemaRegistryAvroObjectEncoderOptions
+    public class SchemaRegistryAvroObjectSerializerOptions
     {
         /// <summary>
         /// Gets or sets the automatic registration of schemas flag.
