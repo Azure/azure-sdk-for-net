11532ba01297eb19aef0639af718e08122b99093
Ahmed Leithy <v-aleithy@microsoft.com>
2021-11-16 19:23:33 +0200

    Question Answering LLC Init

---
+++ sdk/cognitivelanguage/Azure.AI.Language.QuestionAnswering/src/QuestionAnsweringProjectsClientOptions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Core;
+
+namespace Azure.AI.Language.QuestionAnswering.Projects
+{
+    /// <summary> Client options for QuestionAnsweringProjectsClient. </summary>
+    [CodeGenType("QuestionAnsweringProjectsClientOptions")]
+    public partial class QuestionAnsweringProjectsClientOptions
+    {
+    }
+}
