3b5cb1f0a9de5f95538f818a516428db4f549e0d
Thiago Diniz Maia <thdinizm@microsoft.com>
2021-11-17 15:30:35 -0800

    Update MediaCompositionClient.cs

--- sdk/communication/Azure.Communication.MediaComposition/src/MediaCompositionClient.cs
+++ sdk/communication/Azure.Communication.MediaComposition/src/MediaCompositionClient.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Azure.Communication.MediaComposition.Models;
 using Azure.Core;
 using Azure.Core.Pipeline;
@@ -58,13 +60,16 @@ namespace Azure.Communication.MediaComposition
 
         #endregion protected constructors
 
+        #region sync methods
+
         /// <summary> Create a new Media Composition resource. </summary>
-        public MediaCompositionBody CreateMediaComposition(
+        public virtual Response<MediaCompositionBody> CreateMediaComposition(
             string mediaCompositionId,
             MediaLayout layout,
             IDictionary<string, MediaInput> mediaInputs,
             IDictionary<string, MediaOutput> mediaOutputs,
-            IDictionary<string, MediaSource> sources)
+            IDictionary<string, MediaSource> sources,
+            CancellationToken cancellationToken = default)
         {
             var newMediaComposition = new MediaCompositionBody(
                 mediaCompositionId,
@@ -74,13 +79,99 @@ namespace Azure.Communication.MediaComposition
                 sources,
                 CompositionStreamState.NotStarted);
 
-            return RestClient.Create(mediaCompositionId, newMediaComposition).Value;
+            return RestClient.Create(mediaCompositionId, newMediaComposition, cancellationToken);
+        }
+
+        /// <summary> Retrieves a Media Composition resource. </summary>
+        public virtual Response<MediaCompositionBody> GetMediaComposition(string mediaCompositionId, CancellationToken cancellationToken = default)
+        {
+            return RestClient.Get(mediaCompositionId, cancellationToken);
         }
 
         /// <summary> Starts a Media Composition resource. </summary>
-        public CompositionStreamState StartMediaComposition(string mediaCompositionId)
+        public virtual Response<CompositionStreamState> StartMediaComposition(string mediaCompositionId, CancellationToken cancellationToken = default)
         {
-            return RestClient.Start(mediaCompositionId).Value;
+            return RestClient.Start(mediaCompositionId, cancellationToken);
         }
+
+        /// <summary> Stops a Media Composition resource. </summary>
+        public virtual Response<CompositionStreamState> StopMediaComposition(string mediaCompositionId, CancellationToken cancellationToken = default)
+        {
+            return RestClient.Stop(mediaCompositionId, cancellationToken);
+        }
+
+        /// <summary> Deletes a Media Composition resource. </summary>
+        public virtual Response DeleteMediaComposition(string mediaCompositionId, CancellationToken cancellationToken = default)
+        {
+            return RestClient.Delete(mediaCompositionId, cancellationToken);
+        }
+
+        /// <summary> Updates a Media Composition resource. </summary>
+        public virtual Response<MediaCompositionBody> UpdateMediaComposition(
+            string mediaCompositionId,
+            MediaCompositionBody mediaCompositionBody,
+            CancellationToken cancellationToken = default)
+        {
+            return RestClient.Update(mediaCompositionId, mediaCompositionBody, cancellationToken);
+        }
+
+        #endregion
+
+        #region async methods
+
+        /// <summary> Create a new Media Composition resource. </summary>
+        public virtual async Task<Response<MediaCompositionBody>> CreateMediaCompositionAsync(
+            string mediaCompositionId,
+            MediaLayout layout,
+            IDictionary<string, MediaInput> mediaInputs,
+            IDictionary<string, MediaOutput> mediaOutputs,
+            IDictionary<string, MediaSource> sources,
+            CancellationToken cancellationToken = default)
+        {
+            var newMediaComposition = new MediaCompositionBody(
+                mediaCompositionId,
+                layout,
+                mediaInputs,
+                mediaOutputs,
+                sources,
+                CompositionStreamState.NotStarted);
+
+            return await RestClient.CreateAsync(mediaCompositionId, newMediaComposition, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary> Retrieves a Media Composition resource. </summary>
+        public virtual async Task<Response<MediaCompositionBody>> GetMediaCompositionAsync(string mediaCompositionId, CancellationToken cancellationToken = default)
+        {
+            return await RestClient.GetAsync(mediaCompositionId, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary> Starts a Media Composition resource. </summary>
+        public virtual async Task<Response<CompositionStreamState>> StartMediaCompositionAsync(string mediaCompositionId, CancellationToken cancellationToken = default)
+        {
+            return await RestClient.StartAsync(mediaCompositionId, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary> Stops a Media Composition resource. </summary>
+        public virtual async Task<Response<CompositionStreamState>> StopMediaCompositionAsync(string mediaCompositionId, CancellationToken cancellationToken = default)
+        {
+            return await RestClient.StopAsync(mediaCompositionId, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary> Deletes a Media Composition resource. </summary>
+        public virtual async Task<Response> DeleteMediaCompositionAsync(string mediaCompositionId, CancellationToken cancellationToken = default)
+        {
+            return await RestClient.DeleteAsync(mediaCompositionId, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary> Updates a Media Composition resource. </summary>
+        public virtual async Task<Response<MediaCompositionBody>> UpdateMediaCompositionAsyn(
+            string mediaCompositionId,
+            MediaCompositionBody mediaCompositionBody,
+            CancellationToken cancellationToken = default)
+        {
+            return await RestClient.UpdateAsync(mediaCompositionId, mediaCompositionBody, cancellationToken).ConfigureAwait(false);
+        }
+
+        #endregion
     }
 }
