20d43cacb5df7a911e49a7269b7ed774e525f3b6
sima-zhu <sizhu@microsoft.com>
2021-11-22 22:17:34 -0800

    fix the default path

--- eng/common/scripts/Update-DocsMsMetadata.ps1
+++ eng/common/scripts/Update-DocsMsMetadata.ps1
@@ -81,7 +81,10 @@ function GetAdjustedReadmeContent($ReadmeContent, $PackageInfo, $PackageMetadata
   # Get the first code owners of the package.
   $author = "ramya-rao-a"
   $msauthor = "ramyar"
-  $codeOwnerArray = ."$PSScriptRoot/get-codeowners.ps1" -TargetDirectory $PackageInfo.DirectoryPath 
+  #$codeOwnerFilePath = Join-Path (Get-Item $PSScriptRoot ).Parent.Parent.Parent.FullName ".github/CODEOWNERS"
+  $codeOwnerArray = ."$PSScriptRoot/get-codeowners.ps1" `
+                      -TargetDirectory $PackageInfo.DirectoryPath 
+                      #-CodeOwnerFileLocation $codeOwnerFilePath
   if ($codeOwnerArray) {
     $author = $codeOwnerArray[0]
     $msauthor = $author # This is a placeholder for now. Will change to the right ms alias.
