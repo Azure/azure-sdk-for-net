e9de883fb5b45f18ecac4a52720df689a3bc18fb
danielmarbach <daniel.marbach@openplace.net>
2021-11-24 17:40:48 +0100

    Add ability to extract the state from the annotated message

---
+++ sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/src/Primitives/ServiceBusMessageState.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Azure.WebJobs.ServiceBus
+{
+    enum ServiceBusMessageState
+    {
+        Active = 0,
+        Deferred = 1,
+        Scheduled = 2,
+    }
+}
---
+++ sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/src/Primitives/ServiceBusReceivedMessageExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Messaging.ServiceBus;
+
+namespace Microsoft.Azure.WebJobs.ServiceBus
+{
+    static class ServiceBusReceivedMessageExtensions
+    {
+        public static bool TryGetServiceBusMessageState(this ServiceBusReceivedMessage receivedMessage, out ServiceBusMessageState? state)
+        {
+            var amqpAnnotatedMessage = receivedMessage.GetRawAmqpMessage();
+            if (amqpAnnotatedMessage.MessageAnnotations.TryGetValue(Constants.MessageStateName, out var enumValue)
+                && Enum.IsDefined(typeof(ServiceBusMessageState), enumValue))
+            {
+                state = (ServiceBusMessageState)enumValue;
+                return true;
+            }
+
+            state = default;
+            return false;
+        }
+    }
+}
