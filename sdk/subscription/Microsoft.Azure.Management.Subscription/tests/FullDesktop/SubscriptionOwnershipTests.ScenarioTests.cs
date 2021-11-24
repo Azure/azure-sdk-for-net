189262554f678340b55807bf328b53f1e43a31ae
Ochi <ochisamand@microsoft.com>
2021-11-23 16:37:58 -0800

    Test case for AcceptOwnership is implemented

--- sdk/subscription/Microsoft.Azure.Management.Subscription/tests/FullDesktop/SubscriptionOwnershipTests.ScenarioTests.cs
+++ sdk/subscription/Microsoft.Azure.Management.Subscription/tests/FullDesktop/SubscriptionOwnershipTests.ScenarioTests.cs
@@ -63,19 +63,18 @@ namespace FullDesktop.Tests
             using (MockContext context = MockContext.Start(this.GetType().FullName))
             {
                 const string subscriptionId = "subidprod1";
-                var putAliasRequest = new AcceptOwnershipRequest()
+                var acceptOwnershipRequest = new AcceptOwnershipRequest()
                 {
                     Properties = new AcceptOwnershipRequestProperties()
                     {
-                        DisplayName = "TestSub"
-                        // BillingScope = "/providers/Microsoft.Billing/billingAccounts/4756419/enrollmentAccounts/224190"
+                        DisplayName = "TestOwnership"
                     }
                 };
 
                 var client = GetSubscriptionClient(context, handler);
                 client.SetRetryPolicy(new RetryPolicy<HttpStatusCodeErrorDetectionStrategy>(1));
                 var subscriptionResult = client.Subscription.AcceptOwnershipWithHttpMessagesAsync(
-                    subscriptionId: subscriptionId, body: new AcceptOwnershipRequest()).ConfigureAwait(false).GetAwaiter().GetResult();
+                    subscriptionId: subscriptionId, body: acceptOwnershipRequest).ConfigureAwait(false).GetAwaiter().GetResult();
 
                 Assert.Equal(HttpStatusCode.Accepted, subscriptionResult.Response.StatusCode);
             }
