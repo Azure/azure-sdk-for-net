6ea8dca53991601f31c7eb1bcfbc793ec6c4ca99
Pavel Krymets <pavel@krymets.com>
2021-11-24 14:05:13 -0800

    more

--- eng/scripts/DownloadDevopsLogger.ps1
+++ eng/scripts/DownloadDevopsLogger.ps1
@@ -1,4 +1,5 @@
+mkdir artifacts -Force
 Invoke-WebRequest -Uri https://vstsagenttools.blob.core.windows.net/tools/msbuildlogger/3/msbuildlogger.zip -OutFile artifacts/msbuildlogger.zip
-Expand-Archive artifacts/msbuildlogger.zip -DestinationPath artifacts
+Expand-Archive artifacts/msbuildlogger.zip -DestinationPath artifacts -Force
 $loggerAssembly = Resolve-Path artifacts/Microsoft.TeamFoundation.DistributedTask.MSBuild.Logger.dll
 Write-Host "##vso[task.setvariable variable=DevopsLoggerArguments;]-dl:CentralLogger,`"$loggerAssembly`"*ForwardingLogger,`"$loggerAssembly`""
