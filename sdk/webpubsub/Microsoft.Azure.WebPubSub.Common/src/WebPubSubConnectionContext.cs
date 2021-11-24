3756958a737c713f2744faa321a1aadd19369874
tg-msft <Ted.Glaza@microsoft.com>
2021-11-23 16:00:22 -0800

    Change WPS ConnectionContext.States to a Dict<string, JsonElement>

--- sdk/webpubsub/Microsoft.Azure.WebPubSub.Common/src/WebPubSubConnectionContext.cs
+++ sdk/webpubsub/Microsoft.Azure.WebPubSub.Common/src/WebPubSubConnectionContext.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -57,10 +60,22 @@ namespace Microsoft.Azure.WebPubSub.Common
         public string Origin { get; }
 
         /// <summary>
-        /// The connection states.
+        /// The connection states.  The values in this dictionary are strings of raw JSON and we recommend
+        /// using <see cref="ConnectionStates"/> or <see cref="TryGetConnectionState{T}(string, out T)"/> instead.
+        /// </summary>
+        [JsonIgnore]
+        [JsonPropertyName("states")]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public IReadOnlyDictionary<string, object> States { get; }
+
+        /// <summary>
+        /// The connection states.  You can use the <see cref="TryGetConnectionState{T}(string, out T)"/> method
+        /// to get connection state values deserialized as strong types.
         /// </summary>
         [JsonPropertyName("states")]
-        public IReadOnlyDictionary<string, object> States { get; }
+#pragma warning disable AZC0014 // Avoid using banned types (System.Text.Json) in public APIs
+        public IReadOnlyDictionary<string, JsonElement> ConnectionStates { get; }
+#pragma warning restore AZC0014 // Avoid using banned types (System.Text.Json) in public APIs
 
         /// <summary>
         /// The headers of request.
@@ -78,9 +93,43 @@ namespace Microsoft.Azure.WebPubSub.Common
         /// <param name="userId">User Id.</param>
         /// <param name="signature">Signature of the connection.</param>
         /// <param name="origin">Origin of the event.</param>
-        /// <param name="states">Connection states. Value in <see cref="JsonElement"/> type. Use the <see cref="TryGetState{T}(string, out T)"/> method to tey getting the vaule deserialized as a specific type.</param>
+        /// <param name="states">Connection states. Value in <see cref="JsonElement"/> type. Use the <see cref="TryGetConnectionState{T}(string, out T)"/> method to try getting the value deserialized as a specific type.</param>
         /// <param name="headers">Connection request headers.</param>
-        public WebPubSubConnectionContext(WebPubSubEventType eventType, string eventName, string hub, string connectionId, string userId = null, string signature = null, string origin = null, IReadOnlyDictionary<string, object> states = null, IReadOnlyDictionary<string, string[]> headers = null)
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public WebPubSubConnectionContext(WebPubSubEventType eventType, string eventName, string hub, string connectionId, string userId, string signature, string origin, IReadOnlyDictionary<string, object> states, IReadOnlyDictionary<string, string[]> headers)
+            : this(
+                eventType,
+                eventName,
+                hub,
+                connectionId,
+                userId,
+                signature,
+                origin,
+                states?.ToDictionary(
+                    p => p.Key,
+                    p => p.Value is JsonElement e ?
+                        e :
+                        // TODO: Is this safe?  Will all callers of of this .ctor always pass JSON? Can we have version mismatches between Common and Extensions that break this?
+                        throw new ArgumentException($"{nameof(states)} values must be of type {nameof(JsonElement)}.", nameof(states))),
+                headers)
+        {
+        }
+
+        /// <summary>
+        /// The client connection context contains the CloudEvents headers under Web PubSub protocol.
+        /// </summary>
+        /// <param name="eventType">Event type.</param>
+        /// <param name="eventName">Event name.</param>
+        /// <param name="hub">Hub name.</param>
+        /// <param name="connectionId">Connection Id.</param>
+        /// <param name="userId">User Id.</param>
+        /// <param name="signature">Signature of the connection.</param>
+        /// <param name="origin">Origin of the event.</param>
+        /// <param name="connectionStates">Connection states.  Use the <see cref="TryGetConnectionState{T}(string, out T)"/> method to try getting the value deserialized as a specific type.</param>
+        /// <param name="headers">Connection request headers.</param>
+#pragma warning disable AZC0014 // Avoid using banned types (System.Text.Json) in public APIs
+        public WebPubSubConnectionContext(WebPubSubEventType eventType, string eventName, string hub, string connectionId, string userId = null, string signature = null, string origin = null, IReadOnlyDictionary<string, JsonElement> connectionStates = null, IReadOnlyDictionary<string, string[]> headers = null)
+#pragma warning restore AZC0014 // Avoid using banned types (System.Text.Json) in public APIs
         {
             EventType = eventType;
             EventName = eventName;
@@ -89,20 +138,21 @@ namespace Microsoft.Azure.WebPubSub.Common
             UserId = userId;
             Signature = signature;
             Origin = origin;
-            States = states;
+            ConnectionStates = connectionStates;
+            States = connectionStates != null ? new StringifiedDictionary(connectionStates) : null;
             Headers = headers;
         }
 
         /// <summary>
-        /// Method to deserialize state to customized type.
+        /// Try to get the value of a connection state deserialized as a specific type.
         /// </summary>
         /// <typeparam name="T">Customized type.</typeparam>
-        /// <param name="stateKey">State key.</param>
-        /// <param name="value">Connection state mapped to the key.</param>
-        /// <returns>Returns true if is able to find the key and false means key doesn't exist or value type not able to convert.</returns>
-        public bool TryGetState<T>(string stateKey, out T value)
+        /// <param name="stateKey">The connection state key.</param>
+        /// <param name="value">The connection state value deserialized as <typeparamref name="T"/>.</param>
+        /// <returns>Returns true if able to find the key and false if the key doesn't exist or value cannot be deserialized to <typeparamref name="T"/>.</returns>
+        public bool TryGetConnectionState<T>(string stateKey, out T value)
         {
-            value = default(T);
+            value = default;
             if (States.TryGetValue(stateKey, out var stateValue))
             {
                 if (stateValue is T val)
@@ -125,6 +175,63 @@ namespace Microsoft.Azure.WebPubSub.Common
                 }
             }
             return false;
+        }
+
+        /// <summary>
+        /// Dictionary that wraps access to the ConnectionStates dictionary but
+        /// returns stringifed JSON instead of JsonElement.  This only exists to
+        /// avoid breaking customers using the old <see cref="States"/> property.
+        /// </summary>
+        private class StringifiedDictionary : IReadOnlyDictionary<string, object>
+        {
+            /// <summary>
+            /// The original dictionary to wrap.
+            /// </summary>
+            private readonly IReadOnlyDictionary<string, JsonElement> _original;
+
+            /// <summary>
+            /// Creates a new instance of the RawJsonWrappingDictionary class.
+            /// </summary>
+            /// <param name="original">The original dictionary to wrap.</param>
+            public StringifiedDictionary(IReadOnlyDictionary<string, JsonElement> original) =>
+                _original = original;
+
+            /// <inheritdoc/>
+            public int Count => _original.Count;
+
+            /// <inheritdoc/>
+            public object this[string key] => _original[key].GetRawText();
+
+            /// <inheritdoc/>
+            public IEnumerable<string> Keys => _original.Keys;
+
+            /// <inheritdoc/>
+            public IEnumerable<object> Values => _original.Values.Select(v => v.GetRawText());
+
+            /// <inheritdoc/>
+            public IEnumerator<KeyValuePair<string, object>> GetEnumerator() =>
+                _original.Select(p => new KeyValuePair<string, object>(p.Key, p.Value.GetRawText())).GetEnumerator();
+
+            /// <inheritdoc/>
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+            /// <inheritdoc/>
+            public bool ContainsKey(string key) => _original.ContainsKey(key);
+
+            /// <inheritdoc/>
+            public bool TryGetValue(string key, out object value)
+            {
+                if (_original.TryGetValue(key, out JsonElement e))
+                {
+                    value = e.GetRawText();
+                    return true;
+                }
+                else
+                {
+                    value = default;
+                    return false;
+                }
+            }
         }
     }
 }
