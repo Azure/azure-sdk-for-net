3125713aaead88a6e0af7991eefbc6da2e029c0f
John Call <johncall@microsoft.com>
2021-11-25 13:19:07 -0800

    Remove accidental using.

--- sdk/core/Azure.Core/src/Pipeline/BearerTokenAuthenticationPolicy.cs
+++ sdk/core/Azure.Core/src/Pipeline/BearerTokenAuthenticationPolicy.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core.Diagnostics;
