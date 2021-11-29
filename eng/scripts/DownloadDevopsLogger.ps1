8855214b4bcea8999574cf839ab11acf7913c7d6
Pavel Krymets <pavel@krymets.com>
2021-11-29 15:16:05 -0800

    ..

--- eng/scripts/DownloadDevopsLogger.ps1
+++ eng/scripts/DownloadDevopsLogger.ps1
@@ -1,3 +1,4 @@
+ls D:\a\ -recurse
 New-Item -ItemType directory artifacts -Force
 Invoke-WebRequest -Uri https://vstsagenttools.blob.core.windows.net/tools/msbuildlogger/5/msbuildlogger.zip -OutFile artifacts/msbuildlogger.zip
 Expand-Archive artifacts/msbuildlogger.zip -DestinationPath artifacts -Force
