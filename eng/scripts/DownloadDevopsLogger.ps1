f60e95ffc3c73ae8f8b59ce0821ccd1aa4b6771f
Pavel Krymets <pavel@krymets.com>
2021-11-29 15:48:48 -0800

    Wes made me do it

--- eng/scripts/DownloadDevopsLogger.ps1
+++
@@ -1,7 +0,0 @@
-ls D:\a\ -recurse
-New-Item -ItemType directory artifacts -Force
-Invoke-WebRequest -Uri https://vstsagenttools.blob.core.windows.net/tools/msbuildlogger/5/msbuildlogger.zip -OutFile artifacts/msbuildlogger.zip
-Expand-Archive artifacts/msbuildlogger.zip -DestinationPath artifacts -Force
-$loggerAssembly = Resolve-Path artifacts/Microsoft.TeamFoundation.DistributedTask.MSBuild.Logger.dll
-$arguments = "/dl:CentralLogger,`"$loggerAssembly`";`"enableOrphanedProjectsLogs=true`"*ForwardingLogger,`"$loggerAssembly`""
-$arguments | Out-File Directory.Build.rsp
