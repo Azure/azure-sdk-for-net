b4347521904c0fb16c648f31032a77c685a0e650
zedy <zedy@wicresoft.com>
2021-11-29 12:09:19 +0800

    Remove invalid changes

--- sdk/textanalytics/Azure.AI.TextAnalytics/src/Constants.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/Constants.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using Azure.AI.TextAnalytics.Models;
 
 namespace Azure.AI.TextAnalytics
@@ -9,15 +8,7 @@ namespace Azure.AI.TextAnalytics
     internal static class Constants
     {
         public static readonly StringIndexType DefaultStringIndexType = StringIndexType.Utf16CodeUnit;
-        public static string CognitiveServicesEndpointSuffix => Environment.GetEnvironmentVariable("TEXT_ANALYTICS_DEFAULT_SCOPE");
+        public const string DefaultCognitiveScope = "https://cognitiveservices.azure.com/.default";
         public const string AuthorizationHeader = "Ocp-Apim-Subscription-Key";
-
-        public static string getDefaultCognitiveScope()
-        {
-            string cognitiveServicesEndpointSuffix = CognitiveServicesEndpointSuffix;
-            cognitiveServicesEndpointSuffix = cognitiveServicesEndpointSuffix.Substring(1);
-            string defaultCognitiveScope = $"https://{cognitiveServicesEndpointSuffix}/.default";
-            return defaultCognitiveScope;
-        }
     }
 }
