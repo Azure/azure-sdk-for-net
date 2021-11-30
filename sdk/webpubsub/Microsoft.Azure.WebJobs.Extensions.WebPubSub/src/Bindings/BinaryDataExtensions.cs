b16024de9985bd3641f15e2d4d450758b870fe25
Jialin Xin <jixin@microsoft.com>
2021-11-30 16:33:15 +0800

    remove unused usings.

--- sdk/webpubsub/Microsoft.Azure.WebJobs.Extensions.WebPubSub/src/Bindings/BinaryDataExtensions.cs
+++ sdk/webpubsub/Microsoft.Azure.WebJobs.Extensions.WebPubSub/src/Bindings/BinaryDataExtensions.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using System.IO;
-using System.Text.Json;
 using Newtonsoft.Json.Linq;
 
 namespace System
