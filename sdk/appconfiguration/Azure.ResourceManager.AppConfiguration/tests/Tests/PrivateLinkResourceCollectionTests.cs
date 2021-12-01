e00f9559ad7894040ae0cd7e7b546440b2fda715
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2021-12-01 21:36:19 +0800

    [AppConfiguration] Release Track2 MGMT SDK (#24809)
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Update Track1CodeCheck.ps1
    
    * Intial
    
    * Add test
    
    * Add test
    
    * add test
    
    * Add tests
    
    * Update PrivateLinkResourceContainerTests.cs
    
    * Fix test
    
    * Create PrivateLinkResourceOperationTests.cs
    
    * re-gen
    
    * regen
    
    * Update Azure.ResourceManager.AppConfiguration.netstandard2.0.cs
    
    * Update CHANGELOG.md
    
    * Add Samples
    
    * Delete azure-pipelines.yml
    
    * Update Azure.ResourceManager.AppConfiguration.Tests.csproj
    
    * re-gen
    
    * Update based on comment
    
    * re-gen
    
    * Update sample
    
    * Update CHANGELOG.md
    
    * re-gen

---
+++ sdk/appconfiguration/Azure.ResourceManager.AppConfiguration/tests/Tests/PrivateLinkResourceCollectionTests.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.AppConfiguration.Models;
+using Azure.ResourceManager.Network;
+using Azure.ResourceManager.Network.Models;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.AppConfiguration.Tests
+{
+    public class PrivateLinkResourceCollectionTests : AppConfigurationClientBase
+    {
+        private ResourceGroup ResGroup { get; set; }
+        private ConfigurationStore ConfigStore { get; set; }
+
+        public PrivateLinkResourceCollectionTests(bool isAsync)
+            : base(isAsync)
+        {
+        }
+
+        [SetUp]
+        public async Task TestSetUp()
+        {
+            if (Mode == RecordedTestMode.Record || Mode == RecordedTestMode.Playback)
+            {
+                Initialize();
+                string groupName = Recording.GenerateAssetName(ResourceGroupPrefix);
+                ResGroup = await (await ArmClient.GetDefaultSubscriptionAsync().Result.GetResourceGroups().CreateOrUpdateAsync(groupName, new ResourceGroupData(Location))).WaitForCompletionAsync();
+                string configurationStoreName = Recording.GenerateAssetName("testapp-");
+                ConfigurationStoreData configurationStoreData = new ConfigurationStoreData(Location, new Models.Sku("Standard"))
+                {
+                    PublicNetworkAccess = PublicNetworkAccess.Disabled
+                };
+                ConfigStore = await (await ResGroup.GetConfigurationStores().CreateOrUpdateAsync(configurationStoreName, configurationStoreData)).WaitForCompletionAsync();
+            }
+        }
+
+        [Ignore("Error resource id without '/' in the beginning")]
+        [Test]
+        public async Task GetTest()
+        {
+            PrivateLinkResource linkResource = await ConfigStore.GetPrivateLinkResources().GetAsync("configurationStores");
+
+            Assert.NotNull(linkResource);
+        }
+
+        [Ignore("Error resource id without '/' in the beginning")]
+        [Test]
+        public async Task GetIfExistsTest()
+        {
+            PrivateLinkResource linkResource = await ConfigStore.GetPrivateLinkResources().GetIfExistsAsync("configurationStores");
+
+            Assert.NotNull(linkResource);
+        }
+
+        [Ignore("Error resource id without '/' in the beginning")]
+        [Test]
+        public async Task GetAllTest()
+        {
+            List<PrivateLinkResource> linkResources = await ConfigStore.GetPrivateLinkResources().GetAllAsync().ToEnumerableAsync();
+
+            Assert.IsTrue(linkResources.Count > 0);
+        }
+
+        [Ignore("Error resource id without '/' in the beginning")]
+        [Test]
+        public async Task CheckIfExistsTest()
+        {
+            bool linkResource = await ConfigStore.GetPrivateLinkResources().CheckIfExistsAsync("configurationStores");
+
+            Assert.IsTrue(linkResource);
+        }
+    }
+}
