5a4cc8128e135d077fab80b3c45a04ec926e138d
Ben Broderick Phillips <bebroder@microsoft.com>
2021-12-02 18:48:01 -0500

    Add default parameter set usage comment

--- eng/common/scripts/stress-testing/deploy-stress-tests.ps1
+++ eng/common/scripts/stress-testing/deploy-stress-tests.ps1
@@ -1,3 +1,6 @@
+# Set a default parameter set here so we can call this script without requiring -Login and -Subscription,
+# but if it IS called with either of those, then both parameters need to be required. Not defining a
+# default parameter set makes Login/Subscription required all the time.
 [CmdletBinding(DefaultParameterSetName = 'Default')]
 param(
     [string]$SearchDirectory,
