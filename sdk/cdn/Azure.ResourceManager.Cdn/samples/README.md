6c8cc9a13dc39d480ce07fec1b01511ac493d45b
Yao Kou <yaokou@microsoft.com>
2021-12-02 15:37:23 +0800

    CDN mgmt track 2 library (#24335)
    * generate template for cdn
    
    * modify autorest.md to rename DeliveryRuleAction
    
    * generate compilable code
    
    * add tests for profile
    
    * Delete cdnManagementTestEnvironment.cs
    
    * regenerate code
    
    * add first part tests for endpoint
    
    * add second part test for endpoint
    
    * add tests for origin
    
    * re-record ValidateCustomDomain test
    
    * add tests for origin group
    
    * add tests for custom domain
    
    * add tests for name availability check
    
    * add tests for resource usage and probe
    
    * add tests for AFDProfile, AFDCustomDomain, AFDEndpoint and AFDOriginGroup
    
    * add tests for AFDOrigin, AFDRuleSet, AFDRule and AFDRoute
    
    * add tests for AFDSecurityPolicy and CdnFirewallPolicy
    
    * update api
    
    * add test for managed rule sets
    
    * add samples for cdn
    
    * add tests for azure front door secret
    
    * regenerate code and fix the test compile error
    
    * re-record GetLogAnalyticsResource test
    
    * use type replacement and fix the tests
    
    * add ci file
    
    * rename test
    
    * update readme.md
    
    * regenerate code
    
    * fix tests
    
    * update api
    
    * update samples
    
    * rename test file from container to collection
    
    * fix ci failure
    
    * re-generate code with latest autorest
    
    * rename some operations
    
    * remove some operations
    
    * fix tests
    
    * update samples
    
    * update api
    
    * relax patch of security policy
    
    * customize security policy update and re-test
    
    * relax patch of secret
    
    * customize secret update without test
    
    * rename AFDDomain to AFDCustomDomain
    
    * revert data.props
    
    * update api
    
    * remove update of secret
    
    * clean generated folder
    
    * regen and rename AFD to Afd
    
    * change single word class name
    
    * change parameters suffix to options
    
    * remove all generated files
    
    * still change parameters suffix
    
    * remove some test files
    
    * fix all the tests
    
    * add tests
    
    * update samples
    
    * update api
    
    * fix failed tests
    
    * rename models
    
    * remove generated code
    
    * regenerate code
    
    * rename lro
    
    * delete file
    
    * rename AFD to Afd in models
    
    * fix tests
    
    * update api and samples
    
    * temporarily remove checknameavailability in tenant
    
    * regen code
    
    * bring checknameavailability in tenant back
    
    * rename sku back

---
+++ sdk/cdn/Azure.ResourceManager.Cdn/samples/README.md
@@ -0,0 +1,15 @@
+---
+page_type: sample
+languages:
+- csharp
+products:
+- azure
+- azure-resource-manager
+name: Azure.ResourceManager.Cdn samples for .NET
+description: Samples for the Azure.ResourceManager.Cdn client library
+---
+
+# Azure.ResourceManager.Cdn Samples
+
+- [Managing Cdn Origin Groups](https://github.com/Yao725/azure-sdk-for-net/tree/feature/mgmt-track2-cdn/sdk/cdn/Azure.ResourceManager.Cdn/samples/Sample1_ManagingCdnOriginGroups.md)
+- [Managing Azure Front Door Rules](https://github.com/Yao725/azure-sdk-for-net/tree/feature/mgmt-track2-cdn/sdk/cdn/Azure.ResourceManager.Cdn/samples/Sample2_ManagingAfdRules.md)
---
+++ sdk/cdn/Azure.ResourceManager.Cdn/tests/Scenario/AfdOriginOperationsTests.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Cdn.Models;
+using Azure.ResourceManager.Cdn.Tests.Helper;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Cdn.Tests
+{
+    public class AfdOriginOperationsTests : CdnManagementTestBase
+    {
+        public AfdOriginOperationsTests(bool isAsync)
+            : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Delete()
+        {
+            Subscription subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroup rg = await CreateResourceGroup(subscription, "testRg-");
+            string afdProfileName = Recording.GenerateAssetName("AFDProfile-");
+            Profile afdProfile = await CreateAfdProfile(rg, afdProfileName, SkuName.StandardAzureFrontDoor);
+            string afdOriginGroupName = Recording.GenerateAssetName("AFDOriginGroup-");
+            AfdOriginGroup afdOriginGroupInstance = await CreateAfdOriginGroup(afdProfile, afdOriginGroupName);
+            string afdOriginName = Recording.GenerateAssetName("AFDOrigin-");
+            AfdOrigin afdOriginInstance = await CreateAfdOrigin(afdOriginGroupInstance, afdOriginName);
+            await afdOriginInstance.DeleteAsync();
+            var ex = Assert.ThrowsAsync<RequestFailedException>(async () => await afdOriginInstance.GetAsync());
+            Assert.AreEqual(404, ex.Status);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Update()
+        {
+            Subscription subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroup rg = await CreateResourceGroup(subscription, "testRg-");
+            string afdProfileName = Recording.GenerateAssetName("AFDProfile-");
+            Profile afdProfile = await CreateAfdProfile(rg, afdProfileName, SkuName.StandardAzureFrontDoor);
+            string afdOriginGroupName = Recording.GenerateAssetName("AFDOriginGroup-");
+            AfdOriginGroup afdOriginGroupInstance = await CreateAfdOriginGroup(afdProfile, afdOriginGroupName);
+            string afdOriginName = Recording.GenerateAssetName("AFDOrigin-");
+            AfdOrigin afdOriginInstance = await CreateAfdOrigin(afdOriginGroupInstance, afdOriginName);
+            AfdOriginUpdateOptions updateOptions = new AfdOriginUpdateOptions
+            {
+                Priority = 1,
+                Weight = 150
+            };
+            var lro = await afdOriginInstance.UpdateAsync(updateOptions);
+            AfdOrigin updatedAfdOriginInstance = lro.Value;
+            ResourceDataHelper.AssertAfdOriginUpdate(updatedAfdOriginInstance, updateOptions);
+        }
+    }
+}
