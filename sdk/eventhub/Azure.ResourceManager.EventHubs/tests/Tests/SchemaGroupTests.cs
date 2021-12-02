6d2ba4e4587017400894aeb12b7ed5f120f607ed
yukun-dong <dongyukun1995@163.com>
2021-12-02 13:17:47 +0800

    Release EventHubs track2 mgmt client library first beta (#25544)
    * regenerate code; remove old tests
    
    * generate code
    
    * regenerate code
    
    * add configuration to md
    
    * regenerate code
    
    * change namespace
    
    * checkput to new branch
    
    * add test case
    
    * add namespace test case
    
    * add namespace authrizationrule test case
    
    * remove unused json record
    
    * add test case
    
    * finish namespace test cases
    
    * add eventhub tests
    
    * add consumer group tests
    
    * change check name availability's operation id; regenerate code; add sanitizer to keys; re-record tests
    
    * add a disaster recovery test
    
    * regenerate code using 2021-06-01 preview
    
    * regenerate using stable 2017-04 swagger
    
    * remove unused configurations in autorest.md
    
    * add messaging plan test
    
    * remove unused import
    
    * regenerate code
    
    * add region test
    
    * update autorest.md
    
    * update autorest.md
    
    * modify autorest md
    
    * add ignore to list region by sku test case
    
    * fix an error in autorest.md; add a authorizationrule listkeys test; re-record some tests
    
    * use 2018 preview version
    
    * modify test cases
    
    * add sample
    
    * add samples
    
    * update changelog
    
    * regenerate code using autorest 20210928.3
    
    * update api
    
    * update md files' link
    
    * re-record some tests; update md files
    
    * regenerate code and modify some test cases
    
    * update api
    
    * modify test base
    
    * modify some typos
    
    modify some typos
    
    * modify changelog
    
    * modify changelog and sample files
    
    * add 'get namespaces in subscription' test case
    
    * using 2021-06-01-preview; regenerate code and modify the tests
    
    using 2021-06-01-preview; regenerate code and modify the tests
    
    * update API
    
    * re-record a test case
    
    * ignore SetGetNetworkRuleSets test
    
    * change code format
    
    * modify namespace
    
    * rename some models
    
    * update API
    
    * regenerate code
    
    * update tests and samples
    
    * comment out SetGetNetworkRuleSet test case
    
    * re-record test
    
    * unignore SetGetNetworkRuleSet test case; update snippet
    
    * update API
    
    * remove generated code temporily to solve EventHub upper case issue
    
    * regenerate code
    
    * modify disaster recovery test
    
    modify disaster recovery test case
    
    * regenerate using request-path
    
    * rewrite autorest.md
    
    * update API
    
    * using spec 2021-11-01
    
    * re-record test
    
    * modify test base
    
    * add schema group test
    
    * update API
    
    * modify test
    
    * regenerate code
    
    * regenerate code
    
    * add privateendpointconnection test
    
    * rename parameters to options; config to use arm resourceIdentity
    
    * update API and doc
    
    * ignore some tests that exceed 8s; delete a repeated privateendpoint test
    
    * rename some LROs name; update APIs
    
    * modufy docs
    
    * update changelog
    
    * regen the code
    
    Co-authored-by: Yao Kou <yaokou@microsoft.com>

---
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/tests/Tests/SchemaGroupTests.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Azure.ResourceManager.Resources;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.EventHubs.Models;
+using Azure.ResourceManager.EventHubs;
+using Azure.ResourceManager.EventHubs.Tests.Helpers;
+
+namespace Azure.ResourceManager.EventHubs.Tests
+{
+    public class SchemaGroupTests : EventHubTestBase
+    {
+        public SchemaGroupTests(bool isAsync): base(isAsync)
+        {
+        }
+        private ResourceGroup _resourceGroup;
+        private SchemaGroupCollection _schemaGroupCollection;
+        [SetUp]
+        public async Task CreateNamespaceAndGetEventhubCollection()
+        {
+            _resourceGroup = await CreateResourceGroupAsync();
+            string namespaceName = await CreateValidNamespaceName("testnamespacemgmt");
+            EventHubNamespaceCollection namespaceCollection = _resourceGroup.GetEventHubNamespaces();
+            EventHubNamespace eHNamespace = (await namespaceCollection.CreateOrUpdateAsync(namespaceName, new EventHubNamespaceData(DefaultLocation))).Value;
+            _schemaGroupCollection = eHNamespace.GetSchemaGroups();
+        }
+        [TearDown]
+        public async Task ClearNamespaces()
+        {
+            //remove all namespaces under current resource group
+            if (_resourceGroup != null)
+            {
+                EventHubNamespaceCollection namespaceCollection = _resourceGroup.GetEventHubNamespaces();
+                List<EventHubNamespace> namespaceList = await namespaceCollection.GetAllAsync().ToEnumerableAsync();
+                foreach (EventHubNamespace eventHubNamespace in namespaceList)
+                {
+                    await eventHubNamespace.DeleteAsync();
+                }
+                _resourceGroup = null;
+            }
+        }
+
+        [Test]
+        [RecordedTest]
+        [Ignore("get and list not working")]
+        public async Task CreateDeleteSchemaGroup()
+        {
+            //create schema group
+            string schemaGroupName = Recording.GenerateAssetName("schemagroup");
+            SchemaGroupData parameters = new SchemaGroupData()
+            {
+                SchemaType = SchemaType.Avro
+            };
+            SchemaGroup schemaGroup = (await _schemaGroupCollection.CreateOrUpdateAsync(schemaGroupName, parameters)).Value;
+            Assert.NotNull(schemaGroup);
+            Assert.AreEqual(schemaGroupName, schemaGroup.Id.Name);
+
+            //validate if created successfully
+            schemaGroup = await _schemaGroupCollection.GetIfExistsAsync(schemaGroupName);
+            Assert.NotNull(schemaGroup);
+            Assert.IsTrue(await _schemaGroupCollection.CheckIfExistsAsync(schemaGroupName));
+
+            //delete eventhub
+            await schemaGroup.DeleteAsync();
+
+            //validate
+            schemaGroup = await _schemaGroupCollection.GetIfExistsAsync(schemaGroupName);
+            Assert.Null(schemaGroup);
+            Assert.IsFalse(await _schemaGroupCollection.CheckIfExistsAsync(schemaGroupName));
+        }
+
+        [Test]
+        [RecordedTest]
+        [Ignore("get and list not working")]
+        public async Task GetAllSchemaGroups()
+        {
+            //create a schema group
+            string schemaGroupName1 = Recording.GenerateAssetName("schemagroup1");
+            SchemaGroupData parameters = new SchemaGroupData()
+            {
+                SchemaType = SchemaType.Avro
+            };
+            _ = (await _schemaGroupCollection.CreateOrUpdateAsync(schemaGroupName1, parameters)).Value;
+
+            //validate
+            int count = 0;
+            SchemaGroup schemaGroup1 = null;
+            await foreach (SchemaGroup schemaGroup in _schemaGroupCollection.GetAllAsync())
+            {
+                count++;
+                if (schemaGroup.Id.Name == schemaGroupName1)
+                    schemaGroup1 = schemaGroup;
+            }
+        }
+    }
+}
