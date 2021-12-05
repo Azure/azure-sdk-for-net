a9c31fe008caf69d0d389be72437572ac659c9b1
Taras Vozniuk <tarasvozniuk@microsoft.com>
2021-12-06 00:42:17 +0800

    base empty tests/samples template

---
+++ sdk/maps/Azure.Maps.Search/tests/Samples/SearchClientSamples.HelloWorld.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core.TestFramework;
+using Azure.Identity;
+using Azure.Maps.Search.Models;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Azure.Maps.Search.Tests
+{
+    public class SearchClientSamples: SamplesBase<SearchClientTestEnvironment>
+    {
+        [Test]
+        public void SearchingAnAddress()
+        {
+            #region Snippet:SearchAddress
+            var client = new SearchClient(endpoint: new Uri(TestEnvironment.Endpoint), credential: new DefaultAzureCredential());
+            SearchAddressResult searchResponse = client.SearchAddress("Seattle");
+
+            var primaryResult = searchResponse.Results.First();
+            Console.WriteLine("Country: " + primaryResult.Address.CountryCodeISO3);
+            Console.WriteLine("State: " + primaryResult.Address.CountrySubdivision);
+            #endregion
+        }
+    }
+}
