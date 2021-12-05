1ba463da7d649c73d79512139936bcf80ee68b4f
Taras Vozniuk <tarasvozniuk@microsoft.com>
2021-12-06 00:42:17 +0800

    base empty tests/samples template

---
+++ sdk/maps/Azure.Maps.Search/tests/SearchClientLiveTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+using Azure.Maps.Search;
+using System.Linq;
+
+namespace Azure.Maps.Search.Tests
+{
+    public class SearchClientLiveTests: RecordedTestBase<SearchClientTestEnvironment>
+    {
+        public SearchClientLiveTests(bool isAsync) : base(isAsync)
+        {
+        }
+
+        private SearchClient CreateClient()
+        {
+            return InstrumentClient(new SearchClient(
+                endpoint: new Uri(TestEnvironment.Endpoint),
+                credential: TestEnvironment.Credential,
+                clientId: null,
+                options: InstrumentClientOptions(new SearchClientOptions())
+            ));
+        }
+
+        [RecordedTest]
+        public async Task CanSearchAddress()
+        {
+            var client = CreateClient();
+            var searchResult = await client.SearchAddressAsync("Seattle");
+            Assert.AreEqual("Washington", searchResult.Value.Results.First()?.Address?.CountrySubdivisionName);
+        }
+    }
+}
