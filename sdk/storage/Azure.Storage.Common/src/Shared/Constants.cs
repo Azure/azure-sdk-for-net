956cf8568730eab457159c09faefc5813b8c1f9e
Sean McCullough <seanmcc@microsoft.com>
2021-12-06 14:30:30 -0600

    Bumped default sas version down

--- sdk/storage/Azure.Storage.Common/src/Shared/Constants.cs
+++ sdk/storage/Azure.Storage.Common/src/Shared/Constants.cs
@@ -24,7 +24,7 @@ namespace Azure.Storage
         /// Gets the default service version to use when building shared access
         /// signatures.
         /// </summary>
-        public const string DefaultSasVersion = "2021-04-10";
+        public const string DefaultSasVersion = "2021-02-12";
 
         /// <summary>
         /// The default size of staged blocks when uploading small blobs.
