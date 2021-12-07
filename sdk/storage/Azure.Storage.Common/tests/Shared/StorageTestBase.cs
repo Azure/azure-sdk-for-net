2ca1e7abdb0dba241fdae4707aa4ec24ae7c5597
jschrepp-MSFT <41338290+jschrepp-MSFT@users.noreply.github.com>
2021-12-06 17:12:29 -0800

    reset tests

--- sdk/storage/Azure.Storage.Common/tests/Shared/StorageTestBase.cs
+++ sdk/storage/Azure.Storage.Common/tests/Shared/StorageTestBase.cs
@@ -34,7 +34,7 @@ namespace Azure.Storage.Test.Shared
         }
 
         public StorageTestBase(bool async, RecordedTestMode? mode = null)
-            : base(async, RecordedTestMode.Playback)
+            : base(async, mode)
         {
             Sanitizer = new StorageRecordedTestSanitizer();
             Tenants = new TenantConfigurationBuilder(this);
