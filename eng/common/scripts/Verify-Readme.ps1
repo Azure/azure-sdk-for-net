f3c4d4e93f65ee11de92773a47eea3690e616449
Chidozie Ononiwu <chononiw@microsoft.com>
2021-12-08 13:39:32 -0800

    Add template for docwarden

--- eng/common/scripts/Verify-Readme.ps1
+++ eng/common/scripts/Verify-Readme.ps1
@@ -1,15 +1,23 @@
 # Wrapper Script for Readme Verification
 [CmdletBinding()]
 param (
-  [string]$DocWardenVersion = "0.7.1",
-
+  [string]$DocWardenVersion = "0.7.2",
   [Parameter(Mandatory = $true)]
   [string]$ScanPath,
-
+  [string]$RepoRoot,
   [Parameter(Mandatory = $true)]
   [string]$SettingsPath
 )
 
 pip install setuptools wheel --quiet
 pip install doc-warden==$DocWardenVersion --quiet
-ward scan -d $ScanPath -c $SettingsPath
+
+if (-not [System.String]::IsNullOrWhiteSpace($RepoRoot))
+{
+  ward scan -d $ScanPath -u $RepoRoot -c $SettingsPath
+}
+else
+{
+  ward scan -d $ScanPath -c $SettingsPath
+}
+
