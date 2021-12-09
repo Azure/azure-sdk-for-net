68fda8620df30fe9d80fce82efc751ae44777e80
Scott Schaab <sschaab@microsoft.com>
2021-12-09 12:37:11 -0800

    ignore manual test

--- sdk/identity/Azure.Identity/tests/InteractiveBrowserCredentialLiveTests.cs
+++ sdk/identity/Azure.Identity/tests/InteractiveBrowserCredentialLiveTests.cs
@@ -29,10 +29,9 @@ namespace Azure.Identity.Tests
         }
 
         [Test]
+        [Ignore("This test is an integration test which can only be run with user interaction")]
         public async Task AuthenticateWithBrokerAsync()
         {
-            // to fully manually verify the InteractiveBrowserCredential this test should be run both authenticating with a
-            // school / organization account as well as a personal live account, i.e. a @outlook.com, @live.com, or @hotmail.com
             var cred = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions { PreferBroker = true });
 
             AccessToken token = await cred.GetTokenAsync(new TokenRequestContext(new string[] { "https://vault.azure.net/.default" })).ConfigureAwait(false);
