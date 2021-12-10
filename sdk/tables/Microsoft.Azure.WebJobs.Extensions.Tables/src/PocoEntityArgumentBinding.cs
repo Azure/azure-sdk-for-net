80cb1fca98c7626d394da8fdb294c866c01c7b1b
Pavel Krymets <pavel@krymets.com>
2021-12-09 16:40:53 -0800

    and a few more

--- sdk/tables/Microsoft.Azure.WebJobs.Extensions.Tables/src/PocoEntityArgumentBinding.cs
+++ sdk/tables/Microsoft.Azure.WebJobs.Extensions.Tables/src/PocoEntityArgumentBinding.cs
@@ -11,12 +11,6 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables
     internal class PocoEntityArgumentBinding<TElement> : IArgumentBinding<TableEntityContext>
         where TElement : new()
     {
-#pragma warning disable CS0649
-        private static readonly IConverter<ITableEntity, TElement> Converter;
-#pragma warning restore CS0649
-        // TODO:
-            //TableEntityToPocoConverter<TElement>.Create();
-
         public Type ValueType => typeof(TElement);
 
         public async Task<IValueProvider> BindAsync(TableEntityContext value, ValueBindingContext context)
@@ -24,13 +18,15 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables
             var table = value.Table;
             var result = await table.GetEntityAsync<TableEntity>(
                 value.PartitionKey, value.RowKey).ConfigureAwait(false);
-            TableEntity entity = (TableEntity)result.Value;
+            TableEntity entity = result.Value;
+
             if (entity == null)
             {
                 return new NullEntityValueProvider<TElement>(value);
             }
 
-            TElement userEntity = Converter.Convert(entity);
+            TElement userEntity = TablesTypeBinder.Shared.Deserialize<TElement>(entity);
+
             return new PocoEntityValueBinder<TElement>(value, entity.ETag.ToString(), userEntity);
         }
     }
