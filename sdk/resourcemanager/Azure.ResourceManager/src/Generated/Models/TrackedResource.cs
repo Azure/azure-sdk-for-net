b77bf71f4f29cccd3f7bd20639d6cf32e655e50e
m-nash <prognash@gmail.com>
2021-12-14 12:06:57 -0800

    Revert "temp fix to get autorest updated"
    This reverts commit 6a04eb740ccd250e2aac5276e24dcdb7a238ccb5.

--- sdk/resourcemanager/Azure.ResourceManager/src/Generated/Models/TrackedResource.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/Generated/Models/TrackedResource.cs
@@ -24,11 +24,6 @@ namespace Azure.ResourceManager.Models
         [InitializationConstructor]
         protected TrackedResource(Location location)
         {
-            if (location == null)
-            {
-                throw new ArgumentNullException(nameof(location));
-            }
-
             Tags = new ChangeTrackingDictionary<string, string>();
             Location = location;
         }
