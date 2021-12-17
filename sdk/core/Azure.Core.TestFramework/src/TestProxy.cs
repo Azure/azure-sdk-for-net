cb41791bc8d5ccef504373670a90d32edf7c54c3
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2021-12-16 19:06:56 -0800

    Microsoft.Hosting

--- sdk/core/Azure.Core.TestFramework/src/TestProxy.cs
+++ sdk/core/Azure.Core.TestFramework/src/TestProxy.cs
@@ -71,7 +71,7 @@ namespace Azure.Core.TestFramework
                 EnvironmentVariables =
                 {
                     ["ASPNETCORE_URLS"] = $"http://{IpAddress}:0;https://{IpAddress}:0",
-                    ["Logging__LogLevel__Microsoft"] = "Information",
+                    ["Logging__LogLevel__Microsoft.Hosting.Lifetime"] = "Information",
                     ["ASPNETCORE_Kestrel__Certificates__Default__Path"] = Path.Combine(
                         TestEnvironment.RepositoryRoot,
                         "eng",
