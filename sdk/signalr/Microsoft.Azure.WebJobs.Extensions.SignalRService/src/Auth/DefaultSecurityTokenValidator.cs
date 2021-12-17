2ca25d0f9443538cbc5e32ee6e3d1d4c84879685
Pavel Krymets <pavel@krymets.com>
2021-12-17 15:02:31 -0800

    revert

--- sdk/signalr/Microsoft.Azure.WebJobs.Extensions.SignalRService/src/Auth/DefaultSecurityTokenValidator.cs
+++ sdk/signalr/Microsoft.Azure.WebJobs.Extensions.SignalRService/src/Auth/DefaultSecurityTokenValidator.cs
@@ -1,63 +1,84 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Microsoft.IdentityModel.Tokens;
 
-namespace Microsoft.Azure.WebJobs.Host.Converters
+namespace Microsoft.Azure.WebJobs.Extensions.SignalRService
 {
-    /// <summary>
-    /// An object converter that encapsulates a set of inner converters.
-    /// </summary>
-    /// <typeparam name="T">The output <see cref="Type"/></typeparam>
-    internal class CompositeObjectToTypeConverter<T> : IObjectToTypeConverter<T>
+    internal class DefaultSecurityTokenValidator : ISecurityTokenValidator
     {
-        private readonly IEnumerable<IObjectToTypeConverter<T>> _converters;
+        private const string AuthHeaderName = "Authorization";
+        private const string BearerPrefix = "Bearer ";
+        private readonly TokenValidationParameters _tokenValidationParameters = new();
+        private readonly JwtSecurityTokenHandler _handler = new();
 
-        /// <summary>
-        /// Create a new instance.
-        /// </summary>
-        /// <param name="converters">The set of converters to encapsulate.</param>
-        public CompositeObjectToTypeConverter(IEnumerable<IObjectToTypeConverter<T>> converters)
+        public DefaultSecurityTokenValidator(Action<TokenValidationParameters> configureTokenValidationParameters)
         {
-            if (converters == null)
+            if (configureTokenValidationParameters == null)
             {
-                throw new ArgumentNullException("converters");
+                throw new ArgumentNullException(nameof(configureTokenValidationParameters));
             }
-            _converters = converters;
+            configureTokenValidationParameters(_tokenValidationParameters);
         }
 
-        /// <summary>
-        /// Create a new instance.
-        /// </summary>
-        /// <param name="converters">The set of converters to encapsulate.</param>
-        public CompositeObjectToTypeConverter(params IObjectToTypeConverter<T>[] converters)
-            : this((IEnumerable<IObjectToTypeConverter<T>>)converters)
+        public SecurityTokenResult ValidateToken(HttpRequest request)
         {
-        }
-
-        /// <summary>
-        /// Try to perform a conversion by attempting each inner converter in order
-        /// until one succeeds, or all fail.
-        /// </summary>
-        /// <param name="input">The value to convert.</param>
-        /// <param name="output">The converted value if successful.</param>
-        /// <returns>True if the conversion was successful, false otherwise.</returns>
-        public bool TryConvert(object input, out T output)
-        {
-            foreach (IObjectToTypeConverter<T> converter in _converters)
+            try
             {
-                T possibleConverted;
-
-                if (converter.TryConvert(input, out possibleConverted))
+                var authHeader = default(StringValues);
+                if (request?.Headers.TryGetValue(AuthHeaderName, out authHeader) == true)
                 {
-                    output = possibleConverted;
-                    return true;
-                }
-            }
+                    var authHeaderValue = authHeader.ToString();
+                    if (authHeaderValue.StartsWith(BearerPrefix, StringComparison.OrdinalIgnoreCase))
+                    {
+                        var token = authHeaderValue.Substring(BearerPrefix.Length);
+                        var principal = _handler.ValidateToken(token, _tokenValidationParameters, out _);
 
-            output = default(T);
-            return false;
+                        return SecurityTokenResult.Success(principal);
+                    }
+                }
+
+                // token is null or whitespace
+                return SecurityTokenResult.Empty();
+            }
+            catch (Exception ex) when (
+                // 'exp' claim is less than DateTime.UtcNow
+                ex is SecurityTokenExpiredException ||
+
+                // 1. token's length is greater than TokenHandler.MaximumTokenSizeInBytes
+                // 2. token does not have 3 or 5 parts
+                // 3. token cannot be read
+                ex is ArgumentException ||
+
+                // 1. TokenValidationParameters.ValidAudience is null or whitespace and TokenValidationParameters.ValidAudiences is null. Audience is not validated if TokenValidationParameters.ValidateAudience is set to false.
+                // 2. 'aud' claim did not match either TokenValidationParameters.ValidAudience or one of TokenValidationParameters.ValidAudiences.
+                ex is SecurityTokenInvalidAudienceException ||
+
+                // 'nbf' claim is greater than 'exp' claim
+                ex is SecurityTokenInvalidLifetimeException ||
+
+                    // Signature is not properly formatted.
+                    ex is SecurityTokenInvalidSignatureException ||
+
+                // 1. 'exp' claim is missing and TokenValidationParameters.RequireExpirationTime is true.
+                // 2. TokenValidationParameters.TokenReplayCache is not null and expirationTime.HasValue is false. When a TokenReplayCache is set, tokens require an expiration time
+                ex is SecurityTokenNoExpirationException ||
+
+                // 'nbf' claim is greater than DateTime.UtcNow.
+                ex is SecurityTokenNotYetValidException ||
+
+                // token could not be added to the TokenValidationParameters.TokenReplayCache
+                ex is SecurityTokenReplayAddFailedException ||
+
+                // token is found in the cache
+                ex is SecurityTokenReplayDetectedException)
+            {
+                return SecurityTokenResult.Error(ex);
+            }
         }
     }
 }
