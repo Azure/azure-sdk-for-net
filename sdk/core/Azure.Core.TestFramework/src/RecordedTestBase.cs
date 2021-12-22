0d85d04f7cc4171675ff35ffac15729bb3fc3cac
Pavel Krymets <pavel@krymets.com>
2021-12-22 09:47:48 -0800

    Delete unused records

--- sdk/core/Azure.Core.TestFramework/src/RecordedTestBase.cs
+++ sdk/core/Azure.Core.TestFramework/src/RecordedTestBase.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Threading.Tasks;
 using Castle.DynamicProxy;
 using NUnit.Framework;
@@ -109,22 +110,29 @@ namespace Azure.Core.TestFramework
             TestContext.TestAdapter testAdapter = TestContext.CurrentContext.Test;
 
             string name = new string(testAdapter.Name.Select(c => s_invalidChars.Contains(c) ? '%' : c).ToArray());
-            string additionalParameterName = testAdapter.Properties.ContainsKey(ClientTestFixtureAttribute.RecordingDirectorySuffixKey) ?
-                testAdapter.Properties.Get(ClientTestFixtureAttribute.RecordingDirectorySuffixKey).ToString() :
-                null;
 
+            string fileName = name + (IsAsync ? "Async" : string.Empty) + ".json";
+
+            return Path.Combine(
+                GetSessionFileDirectory(),
+                fileName);
+        }
+
+        private string GetSessionFileDirectory()
+        {
             // Use the current class name instead of the name of the class that declared a test.
             // This can be used in inherited tests that, for example, use a different endpoint for the same tests.
             string className = GetType().Name;
 
-            string fileName = name + (IsAsync ? "Async" : string.Empty) + ".json";
+            TestContext.TestAdapter testAdapter = TestContext.CurrentContext.Test;
 
-            string path = TestEnvironment.GetSourcePath(GetType().Assembly);
-
-            return Path.Combine(path,
+            string additionalParameterName = testAdapter.Properties.ContainsKey(ClientTestFixtureAttribute.RecordingDirectorySuffixKey) ?
+                testAdapter.Properties.Get(ClientTestFixtureAttribute.RecordingDirectorySuffixKey).ToString() :
+                null;
+            return Path.Combine(
+                TestEnvironment.GetSourcePath(GetType().Assembly),
                 "SessionRecords",
-                additionalParameterName == null ? className : $"{className}({additionalParameterName})",
-                fileName);
+                additionalParameterName == null ? className : $"{className}({additionalParameterName})");
         }
 
         public override void GlobalTimeoutTearDown()
@@ -169,6 +177,31 @@ namespace Azure.Core.TestFramework
         {
             Logger?.Dispose();
             Logger = null;
+
+            // Clean up unused test files
+            var knownMethods = GetType()
+                .GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance)
+                .Select(m=>m.Name)
+                .ToList();
+            if (Mode != RecordedTestMode.Live)
+            {
+                foreach (var fileInfo in new DirectoryInfo(GetSessionFileDirectory()).EnumerateFiles())
+                {
+                    bool used = knownMethods.Any(knownMethod => fileInfo.Name.StartsWith(knownMethod, StringComparison.CurrentCulture));
+
+                    if (!used)
+                    {
+                        try
+                        {
+                            fileInfo.Delete();
+                        }
+                        catch
+                        {
+                            // Ignore
+                        }
+                    }
+                }
+            }
         }
 
         [SetUp]
