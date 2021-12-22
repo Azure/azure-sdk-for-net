224fd5dfa6a8c1c082ad0cc570ea45d2da2155d4
Pavel Krymets <pavel@krymets.com>
2021-12-22 08:55:38 -0800

    more recordings

--- sdk/tables/Microsoft.Azure.WebJobs.Extensions.Tables/tests/CompatibilityTests.cs
+++ sdk/tables/Microsoft.Azure.WebJobs.Extensions.Tables/tests/CompatibilityTests.cs
@@ -4,6 +4,7 @@
 extern alias T1;
 
 using System;
+using System.Globalization;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,13 +18,16 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
 {
     public class CompatibilityTests: TablesLiveTestBase
     {
+        private static DateTimeOffset DateTimeOffsetValue = DateTimeOffset.Parse("07-08-1997", null,  DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal);
+        private static DateTime DateTimeValue = DateTime.Parse("07-08-1997", null,  DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal);
+
         public CompatibilityTests(bool isAsync, bool useCosmos) : base(isAsync, useCosmos)
         {
         }
 
         [RecordedTest]
         [TestCaseSource(nameof(SdkExtensionPermutations))]
-        public async Task WriteUsingExtension(IWriter writer, IReader reader)
+        public async Task CanSaveAndLoadITableEntityNullsAndDefaults(IWriter writer, IReader reader)
         {
             var testEntity = new TestITableEntity()
             {
@@ -39,7 +43,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
 
         [RecordedTest]
         [TestCaseSource(nameof(T1T2ExtensionPermutations))]
-        public async Task WriteUsingExtension2(IWriter writer, IReader reader)
+        public async Task CanSaveAndLoadPoco(IWriter writer, IReader reader)
         {
             var testEntity = new TestEntity()
             {
@@ -54,49 +58,17 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
         }
 
         public static object[] SdkExtensionPermutations { get; } = {
-            new object[]
-            {
-                new Sdk(),
-                new Extension()
-            },
-            new object[]
-            {
-                new Extension(),
-                new Sdk(),
-            },
-            new object[]
-            {
-                new Sdk(),
-                new Sdk(),
-            },
-            new object[]
-            {
-                new Extension(),
-                new Extension()
-            }
+            new object[] { Sdk.Instance, Extension.Instance },
+            new object[] { Extension.Instance, Sdk.Instance },
+            new object[] { Sdk.Instance, Sdk.Instance },
+            new object[] { Extension.Instance, Extension.Instance }
         };
 
         public static object[] T1T2ExtensionPermutations { get; } = {
-            new object[]
-            {
-                new ExtensionT1(),
-                new Extension()
-            },
-            new object[]
-            {
-                new Extension(),
-                new ExtensionT1(),
-            },
-            new object[]
-            {
-                new ExtensionT1(),
-                new ExtensionT1(),
-            },
-            new object[]
-            {
-                new Extension(),
-                new Extension()
-            }
+            new object[] { ExtensionT1.Instance, Extension.Instance },
+            new object[] { Extension.Instance, ExtensionT1.Instance },
+            new object[] { ExtensionT1.Instance, ExtensionT1.Instance },
+            new object[] { Extension.Instance, Extension.Instance }
         };
 
         private void AssertAreEqual(object a, object b)
@@ -112,43 +84,17 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
             }
         }
 
-        private class AddEntityProgram<T>
-        {
-            [return: Table(TableNameExpression)]
-            public T Call(T entity) => entity;
-        }
-
-        private class GetEntityProgram<T>
-        {
-            public void Call([Table(TableNameExpression, PartitionKey, RowKey)] T entity)
-            {
-                Entity = entity;
-            }
-
-            public T Entity { get; set; }
-        }
-
-        private class AddEntityProgramT1<T>
-        {
-            [return: T1::Microsoft.Azure.WebJobs.Table(TableNameExpression)]
-            public T Call(T entity) => entity;
-        }
-
-        private class GetEntityProgramT1<T>
-        {
-            public void Call([T1::Microsoft.Azure.WebJobs.TableAttribute(TableNameExpression, PartitionKey, RowKey)] T entity)
-            {
-                Entity = entity;
-            }
-
-            public T Entity { get; set; }
-        }
-
         public class TestITableEntity : ITableEntity
         {
+            public TestITableEntity()
+            {
+                DatetimeOffsetTypeProperty = DateTimeOffsetValue;
+                DatetimeTypeProperty = DateTimeValue;
+            }
+
             public string StringTypeProperty { get; set; }
-            //public DateTime DatetimeTypeProperty { get; set; }
-            //public DateTimeOffset DatetimeOffsetTypeProperty { get; set; }
+            public DateTime DatetimeTypeProperty { get; set; }
+            public DateTimeOffset DatetimeOffsetTypeProperty { get; set; }
             public Guid GuidTypeProperty { get; set; }
             public byte[] BinaryTypeProperty { get; set; }
             public long Int64TypeProperty { get; set; }
@@ -156,8 +102,8 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
             public double DoubleTypeProperty { get; set; }
             public int IntTypeProperty { get; set; }
 
-            //public DateTime? NullableDatetimeTypeProperty { get; set; }
-            //public DateTimeOffset? NullableDatetimeOffsetTypeProperty { get; set; }
+            public DateTime? NullableDatetimeTypeProperty { get; set; }
+            public DateTimeOffset? NullableDatetimeOffsetTypeProperty { get; set; }
             public Guid? NullableGuidTypeProperty { get; set; }
             public long? NullableInt64TypeProperty { get; set; }
             public ulong? NullableUInt64TypeProperty { get; set; }
@@ -171,9 +117,15 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
         }
         public class TestEntity
         {
+            public TestEntity()
+            {
+                DatetimeOffsetTypeProperty = DateTimeOffsetValue;
+                DatetimeTypeProperty = DateTimeValue;
+            }
+
             public string StringTypeProperty { get; set; }
-            //public DateTime DatetimeTypeProperty { get; set; }
-            //public DateTimeOffset DatetimeOffsetTypeProperty { get; set; }
+            public DateTime DatetimeTypeProperty { get; set; }
+            public DateTimeOffset DatetimeOffsetTypeProperty { get; set; }
             public Guid GuidTypeProperty { get; set; }
             public byte[] BinaryTypeProperty { get; set; }
             public long Int64TypeProperty { get; set; }
@@ -181,8 +133,8 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
             public double DoubleTypeProperty { get; set; }
             public int IntTypeProperty { get; set; }
 
-            //public DateTime? NullableDatetimeTypeProperty { get; set; }
-            //public DateTimeOffset? NullableDatetimeOffsetTypeProperty { get; set; }
+            public DateTime? NullableDatetimeTypeProperty { get; set; }
+            public DateTimeOffset? NullableDatetimeOffsetTypeProperty { get; set; }
             public Guid? NullableGuidTypeProperty { get; set; }
             public long? NullableInt64TypeProperty { get; set; }
             public ulong? NullableUInt64TypeProperty { get; set; }
@@ -193,6 +145,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
             public string RowKey { get; set; }
             public DateTimeOffset Timestamp { get; set; }
             public string ETag { get; set; }
+            public TestEntity NestedEntity { get; set; }
         }
 
         public interface IReader
@@ -205,8 +158,9 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
             ValueTask Write<T>(CompatibilityTests test, T entity);
         }
 
-        public class Sdk: IReader, IWriter
+        private class Sdk: IReader, IWriter
         {
+            public static Sdk Instance = new();
             public async ValueTask<T> Read<T>(CompatibilityTests test)
             {
                 return await (Task<Response<T>>)typeof(TableClient)
@@ -226,8 +180,9 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
             public override string ToString() => "SDK";
         }
 
-        public class Extension : IReader, IWriter
+        private class Extension : IReader, IWriter
         {
+            public static Extension Instance = new();
             public async ValueTask<T> Read<T>(CompatibilityTests test)
             {
                 var result = await test.CallAsync<GetEntityProgram<T>>();
@@ -240,10 +195,27 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
             }
 
             public override string ToString() => "Extension";
+
+            private class AddEntityProgram<T>
+            {
+                [return: Table(TableNameExpression)]
+                public T Call(T entity) => entity;
+            }
+
+            private class GetEntityProgram<T>
+            {
+                public void Call([Table(TableNameExpression, PartitionKey, RowKey)] T entity)
+                {
+                    Entity = entity;
+                }
+
+                public T Entity { get; set; }
+            }
         }
 
-        public class ExtensionT1 : IReader, IWriter
+        private class ExtensionT1 : IReader, IWriter
         {
+            public static ExtensionT1 Instance = new();
             public async ValueTask<T> Read<T>(CompatibilityTests test)
             {
                 var result = await test.CallAsync<GetEntityProgramT1<T>>(configure: builder => Configure(builder, test));
@@ -265,6 +237,22 @@ namespace Microsoft.Azure.WebJobs.Extensions.Tables.Tests
             }
 
             public override string ToString() => "ExtensionT1";
+
+            private class AddEntityProgramT1<T>
+            {
+                [return: T1::Microsoft.Azure.WebJobs.Table(TableNameExpression)]
+                public T Call(T entity) => entity;
+            }
+
+            private class GetEntityProgramT1<T>
+            {
+                public void Call([T1::Microsoft.Azure.WebJobs.TableAttribute(TableNameExpression, PartitionKey, RowKey)] T entity)
+                {
+                    Entity = entity;
+                }
+
+                public T Entity { get; set; }
+            }
         }
     }
 }
