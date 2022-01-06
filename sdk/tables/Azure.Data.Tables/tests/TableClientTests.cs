59363bf5417a50010f4850fec66e57710ca90324
Christopher Scott <chriss@microsoft.com>
2022-01-06 12:34:22 -0600

    cleanup usings

--- sdk/tables/Azure.Data.Tables/tests/TableClientTests.cs
+++ sdk/tables/Azure.Data.Tables/tests/TableClientTests.cs
@@ -3,9 +3,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.TestFramework;
