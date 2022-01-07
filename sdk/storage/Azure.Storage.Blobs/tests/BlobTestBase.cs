5b84333ae14594381ce64dd1bdc023a941c820a8
Sean McCullough <seanmcc@microsoft.com>
2022-01-07 13:37:45 -0600

    Recorded unit tests

--- sdk/storage/Azure.Storage.Blobs/tests/BlobTestBase.cs
+++ sdk/storage/Azure.Storage.Blobs/tests/BlobTestBase.cs
@@ -38,7 +38,7 @@ namespace Azure.Storage.Test.Shared
             new Uri(Tenants.TestConfigSecondary.BlobServiceSecondaryEndpoint).Host;
 
         public BlobTestBase(bool async, BlobClientOptions.ServiceVersion serviceVersion, RecordedTestMode? mode = null)
-            : base(async, RecordedTestMode.Playback)
+            : base(async, mode)
         {
             _serviceVersion = serviceVersion;
             BlobsClientBuilder = ClientBuilderExtensions.GetNewBlobsClientBuilder(Tenants, _serviceVersion);
