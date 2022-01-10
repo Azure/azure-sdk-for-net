0239a48524ad56411610c845c478e1a5761062ff
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2022-01-10 14:56:37 +0800

    Create ARecordData.cs

---
+++ sdk/dns/Azure.ResourceManager.Dns/src/Customized/ARecordData.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System.Collections.Generic;
+using Azure.Core;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Dns.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources.Models;
+
+namespace Azure.ResourceManager.Dns
+{
+    /// <summary> A class representing the RecordSet A data model. </summary>
+    public partial class ARecordData : Resource
+    {
+    }
+}
