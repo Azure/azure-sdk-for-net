6ae76eebf726017f62324d49faaffc6e37ccffc9
Mohit Chakraborty <mohitc@microsoft.com>
2022-01-11 12:37:10 -0800

    Rename `ArtifactManifestProperties.Size`->`SizeInbytes`

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Models/ArtifactManifestProperties.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Models/ArtifactManifestProperties.cs
@@ -25,6 +25,9 @@ namespace Azure.Containers.ContainerRegistry
         public bool? CanList { get; set;  }
         /// <summary> Whether or not this tag can be read. </summary>
         public bool? CanRead { get; set; }
+        /// <summary> Image size. </summary>
+        [CodeGenMember("Size")]
+        public long? SizeInBytes { get; }
         /// <summary> Quarantine state. </summary>
         internal string QuarantineState { get; }
         /// <summary> Quarantine details. </summary>
