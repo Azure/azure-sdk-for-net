b82358c744a2359eff290297f15fe2588918b6bb
m-nash <prognash@gmail.com>
2022-01-11 14:27:14 -0800

    update bench

--- sdk/core/Azure.Core/perf/ResourceIdentifierBenchmark.cs
+++ sdk/core/Azure.Core/perf/ResourceIdentifierBenchmark.cs
@@ -33,5 +33,55 @@ namespace Azure.Template.Perf
             x = id.Parent.Parent.Name;
             return id;
         }
+
+        [Benchmark]
+        public string CreateNavigateParentWithResourceType()
+        {
+            var id = new ResourceIdentifier("/subscriptions/0c2f6471-1bf0-4dda-aec3-cb9272f09575/resourceGroups/myRg/providers/Microsoft.Compute/virtualMachines/myVm");
+            AccessMembers(id);
+            return id;
+        }
+
+        private static void AccessMembers(ResourceIdentifier id)
+        {
+            string x = id.Name;
+            x = id.ResourceGroupName;
+            x = id.SubscriptionId;
+            x = id.Parent.Name;
+            x = id.Parent.Parent.Name;
+            x = id.ResourceType.Namespace;
+            x = id.Parent.ResourceType.Namespace;
+            x = id.Parent.Parent.ResourceType.Namespace;
+        }
+
+        [Benchmark]
+        public string CreateAccessResourceTypeMultipleTimes()
+        {
+            var id = new ResourceIdentifier("/subscriptions/0c2f6471-1bf0-4dda-aec3-cb9272f09575/resourceGroups/myRg/providers/Microsoft.Compute/virtualMachines/myVm");
+            for (int i = 0; i < 100; i++)
+            {
+                string x = id.ResourceType.Namespace;
+            }
+            return id;
+        }
+
+        [Benchmark]
+        public void CreateAppendProviderAndChild()
+        {
+            var id = new ResourceIdentifier("/subscriptions/0c2f6471-1bf0-4dda-aec3-cb9272f09575/resourceGroups/myRg");
+            id = id.AppendProviderResource("Microsoft.Compute", "virtualMachines", "myVm");
+            id = id.AppendChildResource("children", "myChild");
+        }
+
+        [Benchmark]
+        public void CreateAppendProviderAndChildAndAccess()
+        {
+            var id = new ResourceIdentifier("/subscriptions/0c2f6471-1bf0-4dda-aec3-cb9272f09575/resourceGroups/myRg");
+            AccessMembers(id);
+            id = id.AppendProviderResource("Microsoft.Compute", "virtualMachines", "myVm");
+            AccessMembers(id);
+            id = id.AppendChildResource("children", "myChild");
+            AccessMembers(id);
+        }
     }
 }
