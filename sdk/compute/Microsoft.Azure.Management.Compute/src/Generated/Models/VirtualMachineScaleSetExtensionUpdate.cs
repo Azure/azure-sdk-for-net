2f450afc6870e854b7178e5a1e4c0223e49ce69f
kamusta-msft <56413142+kamusta-msft@users.noreply.github.com>
2022-01-14 10:55:11 -0800

    init (#25620)
    Co-authored-by: Theodore Chang <thchan@microsoft.com>

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/VirtualMachineScaleSetExtensionUpdate.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/VirtualMachineScaleSetExtensionUpdate.cs
@@ -70,7 +70,10 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// from the extension will be suppressed (Operational failures such as
         /// not connecting to the VM will not be suppressed regardless of this
         /// value). The default is false.</param>
-        public VirtualMachineScaleSetExtensionUpdate(string id = default(string), string name = default(string), string type = default(string), string forceUpdateTag = default(string), string publisher = default(string), string type1 = default(string), string typeHandlerVersion = default(string), bool? autoUpgradeMinorVersion = default(bool?), bool? enableAutomaticUpgrade = default(bool?), object settings = default(object), object protectedSettings = default(object), string provisioningState = default(string), IList<string> provisionAfterExtensions = default(IList<string>), bool? suppressFailures = default(bool?))
+        /// <param name="protectedSettingsFromKeyVault">The extension can contain either
+        /// protectedSettings or protectedSettingsFromKeyVault or no protected
+        /// settings at all.</param>
+        public VirtualMachineScaleSetExtensionUpdate(string id = default(string), string name = default(string), string type = default(string), string forceUpdateTag = default(string), string publisher = default(string), string type1 = default(string), string typeHandlerVersion = default(string), bool? autoUpgradeMinorVersion = default(bool?), bool? enableAutomaticUpgrade = default(bool?), object settings = default(object), object protectedSettings = default(object), string provisioningState = default(string), IList<string> provisionAfterExtensions = default(IList<string>), bool? suppressFailures = default(bool?), object protectedSettingsFromKeyVault = default(object))
             : base(id)
         {
             Name = name;
@@ -86,6 +89,7 @@ namespace Microsoft.Azure.Management.Compute.Models
             ProvisioningState = provisioningState;
             ProvisionAfterExtensions = provisionAfterExtensions;
             SuppressFailures = suppressFailures;
+            ProtectedSettingsFromKeyVault = protectedSettingsFromKeyVault;
             CustomInit();
         }
 
@@ -184,6 +188,13 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// </summary>
         [JsonProperty(PropertyName = "properties.suppressFailures")]
         public bool? SuppressFailures { get; set; }
+
+        /// <summary>
+        /// Gets or sets the extension can contain either protectedSettings or
+        /// protectedSettingsFromKeyVault or no protected settings at all.
+        /// </summary>
+        [JsonProperty(PropertyName = "properties.protectedSettingsFromKeyVault")]
+        public object ProtectedSettingsFromKeyVault { get; set; }
 
     }
 }
