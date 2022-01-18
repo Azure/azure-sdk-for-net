88a87821bdc351d8d66a4bfb56718221dc0a613b
Pavel Krymets <pavel@krymets.com>
2022-01-18 08:49:45 -0800

    Add T1 sanitization

--- sdk/mgmtcommon/TestFramework/Microsoft.Azure.Test.HttpRecorder/RecorderUtilities.cs
+++ sdk/mgmtcommon/TestFramework/Microsoft.Azure.Test.HttpRecorder/RecorderUtilities.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using Newtonsoft.Json.Linq;
+
 namespace Microsoft.Azure.Test.HttpRecorder
 {
     using Newtonsoft.Json;
@@ -18,7 +20,19 @@ namespace Microsoft.Azure.Test.HttpRecorder
 
     public static class RecorderUtilities
     {
+        private const string SanitizeValue = "Sanitized";
+        public static List<string> JsonPathSanitizers { get; } = new List<string>();
+
         static Regex binaryMimeRegex = new Regex("(image/*|audio/*|video/*|application/octet-stream|multipart/form-data)");
+        static RecorderUtilities()
+        {
+            // Lazy sanitize fields in the request and response bodies
+            JsonPathSanitizers.Add("$..primaryKey");
+            JsonPathSanitizers.Add("$..secondaryKey");
+            JsonPathSanitizers.Add("$..primaryConnectionString");
+            JsonPathSanitizers.Add("$..secondaryConnectionString");
+            JsonPathSanitizers.Add("$..connectionString");
+        }
 
         public static bool IsHttpContentBinary(HttpContent content)
         {
@@ -223,7 +237,14 @@ namespace Microsoft.Azure.Test.HttpRecorder
         {
             try
             {
-                object parsedJson = JsonConvert.DeserializeObject(str);
+                JToken parsedJson = JsonConvert.DeserializeObject<JToken>(str);
+                foreach (var jsonPath in JsonPathSanitizers)
+                {
+                    foreach (JToken token in parsedJson.SelectTokens(jsonPath))
+                    {
+                        token.Replace(SanitizeValue);
+                    }
+                }
                 return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
             }
             catch
