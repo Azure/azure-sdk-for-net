dc2c771b400ffd238385d86a37d345c4fbed8d7e
m-nash <prognash@gmail.com>
2022-01-18 09:04:22 -0800

    missed one using in merge

--- sdk/resourcemanager/Azure.ResourceManager/src/ResourceManagerExtensions.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/ResourceManagerExtensions.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Azure.Core;
 using Azure.ResourceManager.Resources;
+
 namespace Azure.ResourceManager.Core
 {
     /// <summary>
