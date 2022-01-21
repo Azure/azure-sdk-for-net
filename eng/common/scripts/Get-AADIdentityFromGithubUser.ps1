623821cc2ef60848b5698de4a1e8570d2170f57d
Sima Zhu <48036328+sima-zhu@users.noreply.github.com>
2022-01-21 11:55:23 -0800

    Update eng/common/scripts/Get-AADIdentityFromGithubUser.ps1
    Co-authored-by: Ben Broderick Phillips <ben@benbp.net>

--- eng/common/scripts/Get-AADIdentityFromGithubUser.ps1
+++ eng/common/scripts/Get-AADIdentityFromGithubUser.ps1
@@ -62,7 +62,7 @@ $Headers = @{
 try {
     $opsAuthToken = Generate-AadToken -TenantId $TenantId -ClientId $ClientId -ClientSecret $ClientSecret
     $Headers["Authorization"] = "Bearer $opsAuthToken"
-    Write-Host "Fetching aad identity for github user: $GithubName."
+    Write-Host "Fetching aad identity for github user: $GithubName"
     $resp = Invoke-RestMethod $OpensourceAPIBaseURI -Method 'GET' -Headers $Headers
 }
 catch { 
