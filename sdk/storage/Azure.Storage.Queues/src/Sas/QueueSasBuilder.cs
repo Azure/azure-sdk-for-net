398c48bb11e8c760b694fc0914269ee95e161950
navba-MSFT <57353862+navba-MSFT@users.noreply.github.com>
2022-01-25 08:37:18 +0530

    (AzureCXP) Fixed the permission's description
    (AzureCXP) Fixed the permission's description

--- sdk/storage/Azure.Storage.Queues/src/Sas/QueueSasBuilder.cs
+++ sdk/storage/Azure.Storage.Queues/src/Sas/QueueSasBuilder.cs
@@ -110,7 +110,7 @@ namespace Azure.Storage.Sas
         /// class.
         /// </summary>
         /// <param name="permissions">
-        /// The permissions associated with the shared access signature. 
+        /// The permissions associated with the shared access signature.
         /// The user is restricted to operations allowed by the permissions.
         /// This field must be omitted if it has been specified in an
         /// associated stored access policy.
