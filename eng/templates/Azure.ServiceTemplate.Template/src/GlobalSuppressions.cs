abe12bc1c1c1254d821da5850c28d9ce23e6580d
chunyu3 <chunyu@microsoft.com>
2022-01-29 10:56:10 +0800

    remove scope of suppression

--- eng/templates/Azure.ServiceTemplate.Template/src/GlobalSuppressions.cs
+++ eng/templates/Azure.ServiceTemplate.Template/src/GlobalSuppressions.cs
@@ -4,4 +4,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Usage", "AZC0002:DO ensure all service methods, both asynchronous and synchronous, take an optional CancellationToken parameter called cancellationToken.", Justification = "CancellationToken can be passed through RequestOptions")]
-[assembly: SuppressMessage("Usage", "AZC0016:Invalid ServiceVersion member name.", Justification = "Generated code: https://github.com/Azure/autorest.csharp/issues/1524", Scope = "type", Target = "~T:Azure.ServiceTemplate.Template.TemplateClientOptions.ServiceVersion")]
+[assembly: SuppressMessage("Usage", "AZC0016:Invalid ServiceVersion member name.", Justification = "Generated code: https://github.com/Azure/autorest.csharp/issues/1524")]
