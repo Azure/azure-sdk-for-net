5a57ffe8bf808ba518000affb858d95f7e68761a
Leigh Rubin <lerubin@microsoft.com>
2022-02-02 10:02:11 -0800

    Generate 2021-10-15 SDK and update tests

--- sdk/deviceprovisioningservices/Microsoft.Azure.Management.DeviceProvisioningServices/tests/ScenarioTests/DeviceProvisioningClientTest.cs
+++ sdk/deviceprovisioningservices/Microsoft.Azure.Management.DeviceProvisioningServices/tests/ScenarioTests/DeviceProvisioningClientTest.cs
@@ -160,5 +160,89 @@ namespace DeviceProvisioningServices.Tests.ScenarioTests
 
             await badCall.Should().ThrowAsync<ErrorDetailsException>();
         }
+
+        [Fact]
+        public async Task CreateAndDeleteDataResidency()
+        {
+            using var context = MockContext.Start(GetType());
+
+            Initialize(context);
+            const string testName = "unitTestingDPSCreateDataResidency";
+            ResourceGroup rg = await GetResourceGroupAsync(testName);
+
+            var availabilityInfo = await _provisioningClient.IotDpsResource
+                .CheckProvisioningServiceNameAvailabilityAsync(testName)
+                .ConfigureAwait(false);
+
+            if (availabilityInfo.NameAvailable.HasValue && !availabilityInfo.NameAvailable.Value)
+            {
+                // it exists, so test the delete
+                await _provisioningClient.IotDpsResource
+                    .DeleteAsync(testName, rg.Name)
+                    .ConfigureAwait(false);
+
+                // check the name is now available
+                availabilityInfo = await _provisioningClient.IotDpsResource
+                    .CheckProvisioningServiceNameAvailabilityAsync(testName)
+                    .ConfigureAwait(false);
+                availabilityInfo.NameAvailable.Should().BeTrue();
+            }
+
+            // try to create a DPS service
+            var createServiceDescription = new ProvisioningServiceDescription(
+                "BrazilSouth",
+                new IotDpsPropertiesDescription(enableDataResidency: true),
+                new IotDpsSkuInfo(
+                    Constants.DefaultSku.Name,
+                    Constants.DefaultSku.Tier,
+                    Constants.DefaultSku.Capacity));
+
+            var dpsInstance = await _provisioningClient.IotDpsResource
+                .CreateOrUpdateAsync(
+                    rg.Name,
+                    testName,
+                    createServiceDescription)
+                .ConfigureAwait(false);
+
+            dpsInstance.Should().NotBeNull();
+            dpsInstance.Sku.Name.Should().Be(Constants.DefaultSku.Name);
+            dpsInstance.Name.Should().Be(testName);
+
+            // verify item exists in list by resource group
+            IPage<ProvisioningServiceDescription> existingServices = await _provisioningClient.IotDpsResource
+                .ListByResourceGroupAsync(rg.Name)
+                .ConfigureAwait(false);
+            existingServices.Should().Contain(x => x.Name == testName);
+
+            // verify can find
+            ProvisioningServiceDescription foundInstance = await _provisioningClient.IotDpsResource
+                .GetAsync(testName, testName)
+                .ConfigureAwait(false);
+            foundInstance.Should().NotBeNull();
+            foundInstance.Name.Should().Be(testName);
+
+            var attempts = Constants.ArmAttemptLimit;
+            while (attempts > 0)
+            {
+                try
+                {
+                    await _provisioningClient.IotDpsResource
+                        .DeleteAsync(testName, rg.Name)
+                        .ConfigureAwait(false);
+                    break;
+                }
+                catch
+                {
+                    attempts--;
+                    await Task.Delay(Constants.ArmAttemptWaitMs).ConfigureAwait(false);
+                }
+            }
+            existingServices = await _provisioningClient.IotDpsResource
+                .ListByResourceGroupAsync(testName)
+                .ConfigureAwait(false);
+
+            // As long as it is gone or deleting, we're good
+            existingServices.Should().NotContain(x => x.Name == testName && x.Properties.State != "Deleting");
+        }
     }
 }
