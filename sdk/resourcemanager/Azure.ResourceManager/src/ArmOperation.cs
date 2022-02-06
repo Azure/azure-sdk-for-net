eb77d5d934e5cc89207f287bf2c970b2f02941bc
m-nash <prognash@gmail.com>
2022-02-05 17:08:59 -0800

    update namespace

--- sdk/resourcemanager/Azure.ResourceManager/src/ArmOperation.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/ArmOperation.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.Pipeline;
 
-namespace Azure.ResourceManager.Resources.Models
+namespace Azure.ResourceManager
 {
     /// <summary> Create a resource by ID. </summary>
     public class ArmOperation : Operation
