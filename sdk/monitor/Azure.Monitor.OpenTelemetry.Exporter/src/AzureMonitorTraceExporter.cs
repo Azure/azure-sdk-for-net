b74974ec78bd58031bec6d41e6474a4127bedc3d
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-02-16 11:18:33 -0800

    update transmit from storage

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/AzureMonitorTraceExporter.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/AzureMonitorTraceExporter.cs
@@ -17,6 +17,8 @@ namespace Azure.Monitor.OpenTelemetry.Exporter
         private readonly AzureMonitorExporterOptions _options;
         private readonly string _instrumentationKey;
         private readonly ResourceParser _resourceParser;
+        private readonly StorageTransmissionEvaluator _storageTransmissionEvaluator;
+        private readonly const int SampleSize = 10;
 
         public AzureMonitorTraceExporter(AzureMonitorExporterOptions options) : this(options, new AzureMonitorTransmitter(options))
         {
@@ -28,16 +30,25 @@ namespace Azure.Monitor.OpenTelemetry.Exporter
             ConnectionString.ConnectionStringParser.GetValues(_options.ConnectionString, out _instrumentationKey, out _);
             _transmitter = transmitter;
             _resourceParser = new ResourceParser();
+
+            // Todo: Add check if offline storage is enabled by user via options
+            // then only initialize.
+            _storageTransmissionEvaluator = new StorageTransmissionEvaluator(sampleSize: 10);
         }
 
         /// <inheritdoc/>
         public override ExportResult Export(in Batch<Activity> batch)
         {
+            _storageTransmissionEvaluator?.UpdateExportInterval();
+
             // Prevent Azure Monitor's HTTP operations from being instrumented.
             using var scope = SuppressInstrumentationScope.Begin();
 
             try
             {
+                // Get timestamp in ticks before export
+                long timeStampInTicksBeforeExport =  Stopwatch.GetTimestamp();
+
                 var resource = ParentProvider.GetResource();
                 _resourceParser.UpdateRoleNameAndInstance(resource);
                 var telemetryItems = TraceHelper.OtelToAzureMonitorTrace(batch, _resourceParser.RoleName, _resourceParser.RoleInstance, _instrumentationKey);
@@ -45,6 +56,19 @@ namespace Azure.Monitor.OpenTelemetry.Exporter
                 // TODO: Handle return value, it can be converted as metrics.
                 // TODO: Validate CancellationToken and async pattern here.
                 _transmitter.TrackAsync(telemetryItems, false, CancellationToken.None).EnsureCompleted();
+
+                // Get timestamp in ticks After export
+                long timeStampInTicksAfterExport = Stopwatch.GetTimestamp();
+
+                // Calculate duration
+                double currentExportDuration = TimeSpan.FromTicks(timeStampInTicksAfterExport - timeStampInTicksBeforeExport).TotalSeconds;
+
+                _storageTransmissionEvaluator.UpdateExportDuration(currentExportDuration);
+
+                long maxFileToTransmit = _storageTransmissionEvaluator.MaxFilesToTransmitFromStorage();
+
+                _transmitter.TransmitFromStorage(maxFileToTransmit);
+
                 return ExportResult.Success;
             }
             catch (Exception ex)
