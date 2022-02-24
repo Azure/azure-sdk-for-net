3177311fcda69d5ef1a0abc021715b2479cbc375
Guilherme dos Santos <gdossantos@microsoft.com>
2022-02-24 17:35:25 -0300

    Add the code for the Short Codes SDK

---
+++ sdk/communication/Azure.Communication.ShortCodes/tests/ShortCodesClientRecordedTestSanitizer.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Communication.Pipeline;
+
+namespace Azure.Communication.ShortCodes.Tests
+{
+    public class ShortCodesClientRecordedTestSanitizer : CommunicationRecordedTestSanitizer
+    {
+    }
+}
