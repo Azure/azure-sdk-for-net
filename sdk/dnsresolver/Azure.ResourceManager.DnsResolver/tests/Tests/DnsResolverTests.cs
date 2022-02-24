4b96f57af6856f330b764d435cbdbef0c91cbbce
Yao Kou <yaokou@microsoft.com>
2022-02-24 17:08:15 +0800

    regen

--- sdk/dnsresolver/Azure.ResourceManager.DnsResolver/tests/Tests/DnsResolverTests.cs
+++ sdk/dnsresolver/Azure.ResourceManager.DnsResolver/tests/Tests/DnsResolverTests.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Azure.Core.TestFramework;
