8cf3a070959c03a01ae2877d9f51458afaac4b46
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-03-01 20:28:45 -0800

    refactor telemetryitem

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/RequestData.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/RequestData.cs
@@ -25,7 +25,7 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Models
             }
 
             Id = activity.Context.SpanId.ToHexString();
-            Name = TelemetryItem.GetOperationName(activity, ref monitorTags.PartBTags);
+            Name = TraceHelper.GetOperationName(activity, ref monitorTags.PartBTags);
             Duration = activity.Duration.ToString("c", CultureInfo.InvariantCulture);
             Success = activity.GetStatus().StatusCode != StatusCode.Error;
             ResponseCode = AzMonList.GetTagValue(ref monitorTags.PartBTags, SemanticConventions.AttributeHttpStatusCode)?.ToString() ?? "0";
