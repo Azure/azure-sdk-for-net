e60480c49efdf3d9d1064bf4ed0f2febb7fab553
m-nash <prognash@gmail.com>
2022-03-01 16:37:51 -0800

    add comments

--- sdk/core/Azure.Core/src/WaitUntil.cs
+++ sdk/core/Azure.Core/src/WaitUntil.cs
@@ -5,16 +5,16 @@ namespace Azure
 {
     /// <summary>
     /// Indicates whether the invocation of a long running operation should return once it has
-    /// started or wait for the operation to fully complete before returning.
+    /// started or wait for the server operation to fully complete before returning.
     /// </summary>
     public enum WaitUntil
     {
         /// <summary>
-        /// Indicates the method should wait until the operation fully completes.
+        /// Indicates the method should wait until the server operation fully completes.
         /// </summary>
         Completed,
         /// <summary>
-        /// Indicates the method should return once the operation has started.
+        /// Indicates the method should return once the server operation has started.
         /// </summary>
         Started
     }
