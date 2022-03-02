ac5ba81b12c77dd3f4aaddd8ad3d2948403bb177
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-03-01 21:52:56 -0800

    Revert "refactor telemetryitem"
    This reverts commit 8cf3a070959c03a01ae2877d9f51458afaac4b46.

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/RequestData.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/RequestData.cs
@@ -25,7 +25,7 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Models
             }
 
             Id = activity.Context.SpanId.ToHexString();
-            Name = TraceHelper.GetOperationName(activity, ref monitorTags.MappedTags);
+            Name = TelemetryItem.GetOperationName(activity, ref monitorTags.PartBTags);
             Duration = activity.Duration.ToString("c", CultureInfo.InvariantCulture);
             Success = activity.GetStatus().StatusCode != StatusCode.Error;
             ResponseCode = AzMonList.GetTagValue(ref monitorTags.MappedTags, SemanticConventions.AttributeHttpStatusCode)?.ToString() ?? "0";
--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/TelemetryItem.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/TelemetryItem.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
@@ -38,8 +39,8 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Models
             // todo: non-server spans
             if (activity.Kind == ActivityKind.Server)
             {
-                Tags[ContextTagKeys.AiOperationName.ToString()] = TraceHelper.GetOperationName(activity, ref monitorTags.MappedTags);
-                Tags[ContextTagKeys.AiLocationIp.ToString()] = TraceHelper.GetLocationIp(ref monitorTags.MappedTags);
+                Tags[ContextTagKeys.AiOperationName.ToString()] = GetOperationName(activity, ref monitorTags.PartBTags);
+                Tags[ContextTagKeys.AiLocationIp.ToString()] = GetLocationIp(ref monitorTags.PartBTags);
             }
 
             Tags[ContextTagKeys.AiInternalSdkVersion.ToString()] = SdkVersionUtils.s_sdkVersion;
@@ -68,6 +69,39 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Models
         {
             Tags[ContextTagKeys.AiCloudRole.ToString()] = roleName;
             Tags[ContextTagKeys.AiCloudRoleInstance.ToString()] = roleInstance;
+        }
+
+        internal static string GetOperationName(Activity activity, ref AzMonList partBTags)
+        {
+            var httpMethod = AzMonList.GetTagValue(ref partBTags, SemanticConventions.AttributeHttpMethod)?.ToString();
+            if (!string.IsNullOrWhiteSpace(httpMethod))
+            {
+                var httpRoute = AzMonList.GetTagValue(ref partBTags, SemanticConventions.AttributeHttpRoute)?.ToString();
+                // ASP.NET instrumentation assigns route as {controller}/{action}/{id} which would result in the same name for different operations.
+                // To work around that we will use path from httpUrl.
+                if (!string.IsNullOrWhiteSpace(httpRoute) && !httpRoute.Contains("{controller}"))
+                {
+                    return $"{httpMethod} {httpRoute}";
+                }
+                var httpUrl = AzMonList.GetTagValue(ref partBTags, SemanticConventions.AttributeHttpUrl)?.ToString();
+                if (!string.IsNullOrWhiteSpace(httpUrl) && Uri.TryCreate(httpUrl.ToString(), UriKind.RelativeOrAbsolute, out var uri) && uri.IsAbsoluteUri)
+                {
+                    return $"{httpMethod} {uri.AbsolutePath}";
+                }
+            }
+
+            return activity.DisplayName;
+        }
+
+        private static string GetLocationIp(ref AzMonList partBTags)
+        {
+            var httpClientIp = AzMonList.GetTagValue(ref partBTags, SemanticConventions.AttributeHttpClientIP)?.ToString();
+            if (!string.IsNullOrWhiteSpace(httpClientIp))
+            {
+                return httpClientIp;
+            }
+
+            return AzMonList.GetTagValue(ref partBTags, SemanticConventions.AttributeNetPeerIp)?.ToString();
         }
 
         internal static string FormatUtcTimestamp(System.DateTime utcTimestamp)
