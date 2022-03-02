3bfad73bfefafbc37d613c9abc2cf089422e2fc1
sima-zhu <sizhu@microsoft.com>
2022-03-02 13:43:13 -0800

    Check not PR triggered case

--- eng/common/scripts/git-diff-changes.ps1
+++ eng/common/scripts/git-diff-changes.ps1
@@ -17,6 +17,12 @@ param (
   [string] $SourceCommittish = ${env:BUILD_SOURCEVERSION},
   [string] $TargetCommittish = ("origin/${env:SYSTEM_PULLREQUEST_TARGETBRANCH}" -replace "refs/heads/")
 )
+
+# If ${env:SYSTEM_PULLREQUEST_TARGETBRANCH} is empty, then return empty.
+if ($TargetCommittish -eq "origin/") {
+    Write-Host "There is no target branch passed in. "
+    return ""
+} 
 # Git PR diff: https://docs.github.com/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-comparing-branches-in-pull-requests#three-dot-and-two-dot-git-diff-comparisons
 Write-Host "git -c core.quotepath=off -c i18n.logoutputencoding=utf-8 diff $TargetCommittish...$SourceCommittish --name-only --diff-filter=d"
 $changedFiles = (git -c core.quotepath=off -c i18n.logoutputencoding=utf-8 diff "$TargetCommittish...$SourceCommittish"  --name-only --diff-filter=d)
