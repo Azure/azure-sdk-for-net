97f57d548f9091679747fcfbe13571e0d96caa77
Mohit Chakraborty <8271806+Mohit-Chakraborty@users.noreply.github.com>
2022-03-02 12:43:53 -0800

    [Search] Expose `AliasesRestClient` operations from `SearchindexClient` (#27197)
    * Update REST Api commit to d850f41f
    Update generated code and public API
    Add `AliasesRestClient`, `SearchAlias` and `ListAliasesResult`
    
    * Expose `AliasesRestClient` operations from `SearchindexClient`
    
    * Change type of `SearchAlias.ETag` to `Azure.ETag`
    
    * Update Changelog
    
    * Rename `ListAliases()` to `GetAliases()`
    Add a ctor to simplify creating a `SearchAlias` against a single index
    
    * Create a new model factory method for `SearchServiceCounters`
    The new method takes in `aliasCounter` as a parameter.
    
    * Make `GetAliases()` return a `Pageable` type
    
    * Remove setter from `SearchAlias.ETag`

--- sdk/search/Azure.Search.Documents/src/Indexes/Models/SearchServiceCounters.cs
+++ sdk/search/Azure.Search.Documents/src/Indexes/Models/SearchServiceCounters.cs
@@ -10,6 +10,7 @@ namespace Azure.Search.Documents.Indexes.Models
     public partial class SearchServiceCounters
     {
         /// <summary> Initializes a new instance of SearchServiceCounters. </summary>
+        /// <param name="aliasCounter"> Total number of aliases. </param>
         /// <param name="documentCounter"> Total number of documents across all indexes in the service. </param>
         /// <param name="indexCounter"> Total number of indexes. </param>
         /// <param name="indexerCounter"> Total number of indexers. </param>
@@ -19,6 +20,7 @@ namespace Azure.Search.Documents.Indexes.Models
         /// <param name="skillsetCounter"> Total number of skillsets. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="documentCounter"/>, <paramref name="indexCounter"/>, <paramref name="indexerCounter"/>, <paramref name="dataSourceCounter"/>, <paramref name="storageSizeCounter"/>, or <paramref name="synonymMapCounter"/> is null. </exception>
         internal SearchServiceCounters(
+            SearchResourceCounter aliasCounter,
             SearchResourceCounter documentCounter,
             SearchResourceCounter indexCounter,
             SearchResourceCounter indexerCounter,
@@ -27,6 +29,7 @@ namespace Azure.Search.Documents.Indexes.Models
             SearchResourceCounter synonymMapCounter,
             SearchResourceCounter skillsetCounter)
         {
+            AliasCounter = aliasCounter;
             DocumentCounter = documentCounter ?? throw new ArgumentNullException(nameof(documentCounter));
             IndexCounter = indexCounter ?? throw new ArgumentNullException(nameof(indexCounter));
             IndexerCounter = indexerCounter ?? throw new ArgumentNullException(nameof(indexerCounter));
