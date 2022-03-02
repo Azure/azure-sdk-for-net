d471d30ee9bd178b6e76290ffacc1caeb1335aa9
sima-zhu <sizhu@microsoft.com>
2022-03-02 13:29:24 -0800

    Common scripts for git diff changes

---
+++ eng/common/scripts/git-diff-changes.ps1
@@ -0,0 +1,29 @@
+<#
+  .SYNOPSIS
+  Returns git diff changes in pull request.
+
+  .DESCRIPTION
+  The script is to return diff changes in pull request.
+
+  .PARAMETER SourceCommittish
+  The branch committish PR merges from.
+  Definition of committish: https://git-scm.com/docs/gitglossary#Documentation/gitglossary.txt-aiddefcommit-ishacommit-ishalsocommittish
+
+  .PARAMETER TargetCommittish
+  The branch committish PR targets to merge into.
+#>
+[CmdletBinding()]
+param (
+  [string] $SourceCommittish = ${env:BUILD_SOURCEVERSION},
+  [string] $TargetCommittish = ("origin/${env:SYSTEM_PULLREQUEST_TARGETBRANCH}" -replace "refs/heads/")
+)
+# Git PR diff: https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-comparing-branches-in-pull-requests#three-dot-and-two-dot-git-diff-comparisons
+Write-Host "git -c core.quotepath=off -c i18n.logoutputencoding=utf-8 diff $TargetCommittish...$SourceCommittish --name-only --diff-filter=d"
+$changedFiles = (git -c core.quotepath=off -c i18n.logoutputencoding=utf-8 diff "$TargetCommittish...$SourceCommittish"  --name-only --diff-filter=d)
+if(!$changedFiles) {
+Write-Host "No changed files in git diff between $TargetCommittish and $SourceCommittish"
+}
+foreach ($file in $changedFiles) {
+Write-Host "The diff file is: $file"
+}
+return $changedFiles
