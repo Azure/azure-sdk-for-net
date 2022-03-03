18babe0cc5fcb1595f8d55ced31bf95e426c5212
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2022-03-02 19:41:52 -0800

    Allow manual lock renewal from processor and function extension (#27308)
    * Allow manual lock renewal from processor and function extension
    
    * Fix
    
    * Remove lockeduntil because it can change
    
    * Fix

--- sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/tests/ServiceBusSessionsEndToEndTests.cs
+++ sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/tests/ServiceBusSessionsEndToEndTests.cs
@@ -696,7 +696,7 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
             public static async Task QueueWithSessionsBatch(
                 [ServiceBusTrigger(FirstQueueNameKey, IsSessionsEnabled = true)]
                 ServiceBusReceivedMessage[] array,
-                ServiceBusMessageActions messageActions,
+                ServiceBusSessionMessageActions messageActions,
                 CancellationToken cancellationToken,
                 ILogger logger)
             {
@@ -709,6 +709,11 @@ namespace Microsoft.Azure.WebJobs.Host.EndToEndTests
                 Assert.True(cancellationToken.IsCancellationRequested);
                 foreach (ServiceBusReceivedMessage msg in array)
                 {
+                    // validate that manual lock renewal works
+                    var initialLockedUntil = messageActions.SessionLockedUntil;
+                    await messageActions.RenewSessionLockAsync();
+                    Assert.Greater(messageActions.SessionLockedUntil, initialLockedUntil);
+
                     await messageActions.CompleteMessageAsync(msg);
                 }
 
