9db66da6a8e50cc8117d362ca5e4d54c6cb9d86d
Mike Pateras <mipatera@microsoft.com>
2022-03-04 16:43:18 -0500

    Added JobScheduleClient

--- sdk/batch/Azure.Batch/src/JobSchedule/JobScheduleClient.cs
+++ sdk/batch/Azure.Batch/src/JobSchedule/JobScheduleClient.cs
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Batch.Service.Utilities;
+using Azure.Core;
+using Azure.Core.Pipeline;
 
 namespace Azure.Batch.Service.JobSchedule
 {
+    /// <summary>
+    /// Azure Batch client, used for getting, creating, and updating job schedules.
+    /// </summary>
     public class JobScheduleClient
     {
+        private readonly Uri _endpoint;
+        private readonly HttpPipeline _pipeline;
+        private readonly ClientDiagnostics _clientDiagnostics;
+        private readonly JobScheduleRestClient _restClient;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JobScheduleClient"/> class.
+        /// </summary>
+        /// <param name="endpoint">The endpoint URL of the batch service.</param>
+        /// <param name="credential">The token credential to use for authentication.</param>
+        /// <param name="options">Client configuration options.</param>
+        public JobScheduleClient(Uri endpoint, TokenCredential credential, ClientOptions options)
+        {
+            _pipeline = HttpPipelineBuilder.Build(options, new BearerTokenAuthenticationPolicy(credential, Helpers.GetDefaultScope(endpoint)));
+            _clientDiagnostics = new ClientDiagnostics(options);
+            _endpoint = endpoint;
+            _restClient = new JobScheduleRestClient(_clientDiagnostics, _pipeline, _endpoint.AbsoluteUri, "2023-01-01.17.0");
+        }
+
+        /// <summary>
+        /// Parameterless constructor for mocking an instance of the <see cref="JobScheduleClient"/> class.
+        /// </summary>
+        protected JobScheduleClient()
+        {
+        }
     }
 }
---
+++ sdk/batch/Azure.Batch/src/Utilities/Helpers.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Azure.Batch.Service.Utilities
+{
+    internal class Helpers
+    {
+        // A helper method to construct the default scope based on the service endpoint.
+        internal static string GetDefaultScope(Uri uri) => $"{uri.GetComponents(UriComponents.SchemeAndServer, UriFormat.SafeUnescaped)}/.default";
+    }
+}
