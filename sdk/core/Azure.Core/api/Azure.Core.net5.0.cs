52bf25087c63b5d09f2ac025cd6d2bb29c056e8b
Christopher Scott <chriss@microsoft.com>
2022-03-07 19:44:59 -0600

    UserAgentValue arch board feedback (#27364)
    * API for setting a dynamic UserAgent string

--- sdk/core/Azure.Core/api/Azure.Core.net5.0.cs
+++ sdk/core/Azure.Core/api/Azure.Core.net5.0.cs
@@ -652,6 +652,14 @@ namespace Azure.Core
         public override bool IsErrorResponse(Azure.Core.HttpMessage message) { throw null; }
     }
     public delegate System.Threading.Tasks.Task SyncAsyncEventHandler<T>(T e) where T : Azure.SyncAsyncEventArgs;
+    public partial class TelemetryDetails
+    {
+        public TelemetryDetails(System.Reflection.Assembly assembly, string? applicationId = null) { }
+        public string? ApplicationId { get { throw null; } }
+        public System.Reflection.Assembly Assembly { get { throw null; } }
+        public void Apply(Azure.Core.HttpMessage message) { }
+        public override string ToString() { throw null; }
+    }
     public abstract partial class TokenCredential
     {
         protected TokenCredential() { }
@@ -902,10 +910,6 @@ namespace Azure.Core.Pipeline
         public override void Process(Azure.Core.HttpMessage message) { }
         public override System.Threading.Tasks.ValueTask ProcessAsync(Azure.Core.HttpMessage message) { throw null; }
     }
-    public static partial class HttpMessageExtensions
-    {
-        public static void SetUserAgentString(this Azure.Core.HttpMessage message, Azure.Core.Pipeline.UserAgentValue userAgentValue) { }
-    }
     public partial class HttpPipeline
     {
         public HttpPipeline(Azure.Core.Pipeline.HttpPipelineTransport transport, Azure.Core.Pipeline.HttpPipelinePolicy[]? policies = null, Azure.Core.ResponseClassifier? responseClassifier = null) { }
@@ -961,12 +965,6 @@ namespace Azure.Core.Pipeline
         public System.Security.Cryptography.X509Certificates.X509Certificate2? Certificate { get { throw null; } }
         public System.Security.Cryptography.X509Certificates.X509Chain? CertificateAuthorityChain { get { throw null; } }
         public System.Net.Security.SslPolicyErrors SslPolicyErrors { get { throw null; } }
-    }
-    public partial class UserAgentValue
-    {
-        public UserAgentValue(System.Type type, string? applicationId = null) { }
-        public static Azure.Core.Pipeline.UserAgentValue FromType<T>(string? applicationId = null) { throw null; }
-        public override string ToString() { throw null; }
     }
 }
 namespace Azure.Core.Serialization
