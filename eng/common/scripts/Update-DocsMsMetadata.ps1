2015af6d0022456cb813572d2ad0d4ed8148318b
sima-zhu <sizhu@microsoft.com>
2022-03-11 10:00:14 -0800

    Read the ms.service value from csv metadata

--- eng/common/scripts/Update-DocsMsMetadata.ps1
+++ eng/common/scripts/Update-DocsMsMetadata.ps1
@@ -80,13 +80,13 @@ $TITLE_REGEX = "(\#\s+(?<filetitle>Azure .+? (?:client|plugin|shared) library fo
 function GetAdjustedReadmeContent($ReadmeContent, $PackageInfo, $PackageMetadata) {
   # The $PackageMetadata could be $null if there is no associated metadata entry
   # based on how the metadata CSV is filtered
-  $service = $PackageInfo.ServiceDirectory.ToLower()
-  if ($PackageMetadata -and $PackageMetadata.ServiceName) {
-    # Normalize service name "Key Vault" -> "keyvault"
+  $service = $PackageInfo.ServiceDirectory.ToLower()    
+  if ($PackageMetadata -and $PackageMetadata.MSDocService) {
+    # Use MSDocService in csv metadata to override the service directory
     # TODO: Use taxonomy for service name -- https://github.com/Azure/azure-sdk-tools/issues/1442
-    # probably from metadata
-    $service = $PackageMetadata.ServiceName.ToLower().Replace(" ", "")
+    $service = $PackageMetadata.MSDocService
   }
+
 
   # Generate the release tag for use in link substitution
   $tag = "$($PackageInfo.Name)_$($PackageInfo.Version)"
