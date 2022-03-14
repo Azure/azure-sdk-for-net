906a5b55246f9db9c0137b49280ddc713a258f29
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-03-14 12:16:08 -0700

    add comment on count

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/MetricData.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/MetricData.cs
@@ -37,7 +37,10 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Models
                 case MetricType.Histogram:
                     metricDataPoint = new MetricDataPoint(metric.Name, metricPoint.GetHistogramSum());
                     metricDataPoint.DataPointType = DataPointType.Aggregation;
-                    metricDataPoint.Count = (int?)metricPoint.GetHistogramCount();
+                    long histogramCount = metricPoint.GetHistogramCount();
+                    // Current schema only supports int values for count
+                    // if the value is within integer range we will use it otherwise ignore it.
+                    metricDataPoint.Count = (histogramCount <= int.MaxValue && histogramCount >= int.MinValue) ? (int?)histogramCount : null;
                     break;
             }
 
