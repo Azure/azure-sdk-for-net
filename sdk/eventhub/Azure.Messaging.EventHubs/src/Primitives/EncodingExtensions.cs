7ad01cf936ab41ccf3cc683dc3caf254738fff16
Daniel Marbach <daniel.marbach@openplace.net>
2022-03-21 14:17:30 +0100

    Documentation

--- sdk/eventhub/Azure.Messaging.EventHubs/src/Primitives/EncodingExtensions.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/src/Primitives/EncodingExtensions.cs
@@ -6,8 +6,22 @@ using System.Text;
 
 namespace Azure.Messaging.EventHubs.Primitives
 {
+    /// <summary>
+    /// Encoding extensions used to provide a compatibility shim for encoding methods that are missing in .NET Standard 2.0.
+    /// </summary>
     internal static class EncodingExtensions
     {
+        /// <summary>
+        /// Encodes into a span of bytes a set of characters from the specified read-only span.
+        /// </summary>
+        /// <param name="encoding">The encoding to be used.</param>
+        /// <param name="src">The span containing the set of characters to encode.</param>
+        /// <param name="dest">The byte span to hold the encoded bytes.</param>
+        /// <returns>The number of encoded bytes.</returns>
+        /// <remarks>The method was introduced as a compatibility shim for .NET Standard and can be replaced once the
+        /// SDK moves to .NET Standard 2.1 or target frameworks that provides those methods out of the box. During
+        /// the reviews it was decided to not multi target due to the added complexity.
+        /// https://docs.microsoft.com/en-us/dotnet/api/system.text.encoding.getbytes?view=netstandard-2.1#system-text-encoding-getbytes(system-readonlyspan((system-char))-system-span((system-byte))).</remarks>
         public static unsafe int GetBytes(this Encoding encoding,
                                            ReadOnlySpan<char> src,
                                            Span<byte> dest)
@@ -35,6 +49,16 @@ namespace Azure.Messaging.EventHubs.Primitives
             }
         }
 
+        /// <summary>
+        /// Calculates the number of bytes produced by encoding the characters in the specified character span.
+        /// </summary>
+        /// <param name="encoding">The encoding to be used.</param>
+        /// <param name="src">The span of characters to encode.</param>
+        /// <returns>The number of bytes produced by encoding the specified character span.</returns>
+        /// <remarks>The method was introduced as a compatibility shim for .NET Standard and can be replaced once the
+        /// SDK moves to .NET Standard 2.1 or target frameworks that provides those methods out of the box. During
+        /// the reviews it was decided to not multi target due to the added complexity.
+        /// https://docs.microsoft.com/en-us/dotnet/api/system.text.encoding.getbytecount?view=netstandard-2.1#system-text-encoding-getbytecount(system-readonlyspan((system-char)))</remarks>
         public static unsafe int GetByteCount(this Encoding encoding,
                                               ReadOnlySpan<char> src)
         {
