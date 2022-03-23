ab6d3732db190dc5b4574a649c6cfcd55b44dec4
Christopher Scott <chriss@microsoft.com>
2022-03-22 18:25:03 -0700

    wip

--- sdk/identity/Azure.Identity/tests/CredentialTestBase.cs
+++ sdk/identity/Azure.Identity/tests/CredentialTestBase.cs
@@ -35,6 +35,7 @@ namespace Azure.Identity.Tests
         protected AuthenticationResult result;
         protected string expectedCode;
         protected DeviceCodeResult deviceCodeResult;
+        protected const string DiscoveryResponseBody = "{\"tenant_discovery_endpoint\": \"https://login.microsoftonline.com/c54fac88-3dd3-461f-a7c4-8a368e0340b3/v2.0/.well-known/openid-configuration\",\"api-version\": \"1.1\",\"metadata\":[{\"preferred_network\": \"login.microsoftonline.com\",\"preferred_cache\": \"login.windows.net\",\"aliases\":[\"login.microsoftonline.com\",\"login.windows.net\",\"login.microsoft.com\",\"sts.windows.net\"]},{\"preferred_network\": \"login.partner.microsoftonline.cn\",\"preferred_cache\": \"login.partner.microsoftonline.cn\",\"aliases\":[\"login.partner.microsoftonline.cn\",\"login.chinacloudapi.cn\"]},{\"preferred_network\": \"login.microsoftonline.de\",\"preferred_cache\": \"login.microsoftonline.de\",\"aliases\":[\"login.microsoftonline.de\"]},{\"preferred_network\": \"login.microsoftonline.us\",\"preferred_cache\": \"login.microsoftonline.us\",\"aliases\":[\"login.microsoftonline.us\",\"login.usgovcloudapi.net\"]},{\"preferred_network\": \"login-us.microsoftonline.com\",\"preferred_cache\": \"login-us.microsoftonline.com\",\"aliases\":[\"login-us.microsoftonline.com\"]}]}";
 
         public CredentialTestBase(bool isAsync) : base(isAsync)
         { }
--- sdk/identity/Azure.Identity/tests/OnBehalfOfCredentialTests.cs
+++ sdk/identity/Azure.Identity/tests/OnBehalfOfCredentialTests.cs
@@ -2,10 +2,16 @@
 // Licensed under the MIT License.
 
 using System;
+using System.IO;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Core.TestFramework;
 using Azure.Identity.Tests.Mock;
+using Microsoft.Diagnostics.Tracing.Parsers.AspNet;
 using NUnit.Framework;
 
 namespace Azure.Identity.Tests
@@ -25,27 +31,41 @@ namespace Azure.Identity.Tests
             string userAssertion = Guid.NewGuid().ToString();
             string clientSecret = Guid.NewGuid().ToString();
 
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(null, ClientId, clientSecret, userAssertion, null));
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, null, clientSecret, userAssertion, null));
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, ClientId, default(string), userAssertion));
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, ClientId, clientSecret, null, null));
+            Assert.Throws<ArgumentNullException>(() =>
+                new OnBehalfOfCredential(null, ClientId, clientSecret, userAssertion, null));
+            Assert.Throws<ArgumentNullException>(() =>
+                new OnBehalfOfCredential(TenantId, null, clientSecret, userAssertion, null));
+            Assert.Throws<ArgumentNullException>(() =>
+                new OnBehalfOfCredential(TenantId, ClientId, default(string), userAssertion));
+            Assert.Throws<ArgumentNullException>(() =>
+                new OnBehalfOfCredential(TenantId, ClientId, clientSecret, null, null));
             cred = new OnBehalfOfCredential(TenantId, ClientId, clientSecret, userAssertion, null);
             // Assert
             Assert.AreEqual(clientSecret, cred._client._clientSecret);
 
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(null, ClientId, _mockCertificate, userAssertion));
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, null, _mockCertificate, userAssertion));
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, ClientId, default(string), userAssertion));
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, ClientId, _mockCertificate, null));
+            Assert.Throws<ArgumentNullException>(() =>
+                new OnBehalfOfCredential(null, ClientId, _mockCertificate, userAssertion));
+            Assert.Throws<ArgumentNullException>(() =>
+                new OnBehalfOfCredential(TenantId, null, _mockCertificate, userAssertion));
+            Assert.Throws<ArgumentNullException>(() =>
+                new OnBehalfOfCredential(TenantId, ClientId, default(string), userAssertion));
+            Assert.Throws<ArgumentNullException>(() =>
+                new OnBehalfOfCredential(TenantId, ClientId, _mockCertificate, null));
             cred = new OnBehalfOfCredential(TenantId, ClientId, _mockCertificate, userAssertion);
             // Assert
             Assert.NotNull(cred._client._certificateProvider);
 
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(null, ClientId, _mockCertificate, userAssertion, new OnBehalfOfCredentialOptions()));
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, null, _mockCertificate, userAssertion, new OnBehalfOfCredentialOptions()));
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, ClientId, default(X509Certificate2), userAssertion, new OnBehalfOfCredentialOptions()));
-            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, ClientId, _mockCertificate, null, new OnBehalfOfCredentialOptions()));
-            cred = new OnBehalfOfCredential(TenantId, ClientId, _mockCertificate, userAssertion, new OnBehalfOfCredentialOptions());
+            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(null, ClientId, _mockCertificate,
+                userAssertion, new OnBehalfOfCredentialOptions()));
+            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, null, _mockCertificate,
+                userAssertion, new OnBehalfOfCredentialOptions()));
+            Assert.Throws<ArgumentNullException>(() => new OnBehalfOfCredential(TenantId, ClientId,
+                default(X509Certificate2), userAssertion, new OnBehalfOfCredentialOptions()));
+            Assert.Throws<ArgumentNullException>(() =>
+                new OnBehalfOfCredential(TenantId, ClientId, _mockCertificate, null,
+                    new OnBehalfOfCredentialOptions()));
+            cred = new OnBehalfOfCredential(TenantId, ClientId, _mockCertificate, userAssertion,
+                new OnBehalfOfCredentialOptions());
             // Assert
             Assert.NotNull(cred._client._certificateProvider);
         }
@@ -58,7 +78,7 @@ namespace Azure.Identity.Tests
         {
             TestSetup();
             options = new OnBehalfOfCredentialOptions();
-            var context = new TokenRequestContext(new[] { Scope }, tenantId: tenantId);
+            var context = new TokenRequestContext(new[] {Scope}, tenantId: tenantId);
             expectedTenantId = TenantIdResolver.Resolve(explicitTenantId, context);
             OnBehalfOfCredential client = InstrumentClient(
                 new OnBehalfOfCredential(
@@ -72,6 +92,100 @@ namespace Azure.Identity.Tests
 
             var token = await client.GetTokenAsync(new TokenRequestContext(MockScopes.Default), default);
             Assert.AreEqual(token.Token, expectedToken, "Should be the expected token value");
+        }
+
+        [Test]
+        public async Task SendCertificateChain([Values(true, false)] bool sendCertChain)
+        {
+            TestSetup();
+            var _transport = new MockTransport((req) =>
+            {
+                if (req.Uri.Path.StartsWith("/common/discovery"))
+                {
+                    return new MockResponse(200).SetContent(DiscoveryResponseBody);
+                }
+
+                if (req.Uri.Path.EndsWith("/token"))
+                {
+                    if (sendCertChain)
+                    {
+                        Assert.IsTrue(AssertionHasHeaderValue(req, "x5c"));
+                    }
+                    else
+                    {
+                        Assert.IsFalse(AssertionHasHeaderValue(req, "x5c"));
+                    }
+
+                    return new MockResponse(200).WithContent(
+                        $"{{\"token_type\": \"Bearer\",\"expires_in\": 3599,\"ext_expires_in\": 3599,\"access_token\": \"{expectedToken}\" }}");
+                }
+
+                return new MockResponse(200);
+            });
+            var _pipeline = new HttpPipeline(_transport, new[] { new BearerTokenAuthenticationPolicy(new MockCredential(), "scope") });
+            options = new OnBehalfOfCredentialOptions();
+            ((OnBehalfOfCredentialOptions)options).SendCertificateChain = sendCertChain;
+            var context = new TokenRequestContext(new[] {Scope}, tenantId: TenantId);
+            var certificatePath = Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "cert.pfx");
+            var certificatePathPem = Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", "cert.pem");
+            var mockCert = new X509Certificate2(certificatePath);
+            OnBehalfOfCredential client = InstrumentClient(
+                new OnBehalfOfCredential(
+                    TenantId,
+                    ClientId,
+                    mockCert,
+                    expectedUserAssertion,
+                    options as OnBehalfOfCredentialOptions,
+                    new CredentialPipeline(new Uri("https://localhost"), _pipeline, new ClientDiagnostics(options)),
+                    null));
+
+            var token = await client.GetTokenAsync(new TokenRequestContext(MockScopes.Default), default);
+            Assert.AreEqual(token.Token, expectedToken, "Should be the expected token value");
+        }
+
+        private bool AssertionHasHeaderValue(Request req, string headerName)
+        {
+            req.Content.TryComputeLength(out var len);
+            byte[] content = new byte[len];
+            var stream = new MemoryStream((int)len);
+            req.Content.WriteTo(stream, default);
+            var body = Encoding.UTF8.GetString(stream.GetBuffer(), 0, (int)stream.Length);
+            var parts = body.Split('&');
+            foreach (var part in parts)
+            {
+                if (part.StartsWith("client_assertion="))
+                {
+                    var assertion = part.AsSpan();
+                    int start = assertion.IndexOf('=') + 1;
+                    assertion = assertion.Slice(start);
+                    int end = assertion.IndexOf('.');
+                    var jwt = assertion.Slice(0, end);
+                    string convertedToken = jwt.ToString().Replace('_', '/').Replace('-', '+');
+                    switch (jwt.Length % 4)
+                    {
+                        case 2:
+                            convertedToken += "==";
+                            break;
+                        case 3:
+                            convertedToken += "=";
+                            break;
+                    }
+                    Utf8JsonReader reader = new Utf8JsonReader(Convert.FromBase64String(convertedToken));
+                    while (reader.Read())
+                    {
+                        if (reader.TokenType == JsonTokenType.PropertyName)
+                        {
+                            var header = reader.GetString();
+                            if (header == headerName)
+                            {
+                                return true;
+                            }
+                            reader.Read();
+                        }
+                    }
+                }
+            }
+            return false;
         }
     }
 }
