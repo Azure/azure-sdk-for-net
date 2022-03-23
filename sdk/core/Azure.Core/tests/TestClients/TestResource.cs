55fce0d2ca3c3c9ece64a5de87552f28276ba9c5
m-nash <prognash@gmail.com>
2022-03-23 09:09:39 -0700

    update core reference to core

--- sdk/core/Azure.Core/tests/TestClients/TestResource.cs
+++ sdk/core/Azure.Core/tests/TestClients/TestResource.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Azure;
 using Azure.Core.Pipeline;
 using Azure.Core.TestFramework;
-using Azure.ResourceManager.Core;
+using Azure.ResourceManager;
 
 namespace Azure.Core.Tests
 {
