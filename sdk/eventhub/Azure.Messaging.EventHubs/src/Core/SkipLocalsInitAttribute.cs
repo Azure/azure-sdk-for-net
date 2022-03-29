876b8cfabddab62a78f3f30137da6baf12c15802
Daniel Marbach <daniel.marbach@openplace.net>
2022-03-29 15:58:55 +0200

    Reformat

--- sdk/eventhub/Azure.Messaging.EventHubs/src/Core/SkipLocalsInitAttribute.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/src/Core/SkipLocalsInitAttribute.cs
@@ -4,9 +4,11 @@
 namespace System.Runtime.CompilerServices
 {
     /// <summary>
-    /// Used to indicate to the compiler that the <c>.locals init</c> flag should not be set in method headers.
+    ///   Used to indicate to the compiler that the <c>.locals init</c> flag should not be set in method headers.
     /// </summary>
+    ///
     /// <remarks>Internal copy of the .NET 5 attribute.</remarks>
+    ///
     [AttributeUsage(
         AttributeTargets.Module |
         AttributeTargets.Class |
