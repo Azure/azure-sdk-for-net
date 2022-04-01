0a3e776b22f4c2e32abad5130ebdac2b757cb818
Xiao Chen <xichen@microsoft.com>
2022-04-01 14:47:01 +0800

    Add Dashboard test

---
+++ sdk/dashboard/Azure.ResourceManager.Dashboard/tests/DashboardTestBase.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Dashboard.Tests
+{
+    public class DashboardTestBase : ManagementRecordedTestBase<DashboardTestEnvironment>
+    {
+        protected AzureLocation DefaultLocation => AzureLocation.EastUS;
+        protected ArmClient Client { get; private set; }
+        protected SubscriptionResource DefaultSubscription { get; private set; }
+
+        public DashboardTestBase(bool isAsync) : base(isAsync)
+        {
+        }
+
+        public DashboardTestBase(bool isAsync, RecordedTestMode mode) : base(isAsync, mode)
+        {
+        }
+
+        [SetUp]
+        public async Task CreateCommonClient()
+        {
+            Client = GetArmClient();
+            DefaultSubscription = await Client.GetDefaultSubscriptionAsync().ConfigureAwait(false);
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroupAsync()
+        {
+            var resourceGroupName = Recording.GenerateAssetName("sdkTestRG-");
+            var rgOp = await DefaultSubscription.GetResourceGroups().CreateOrUpdateAsync(
+                WaitUntil.Completed,
+                resourceGroupName,
+                new ResourceGroupData(DefaultLocation)
+                {
+                    Tags =
+                    {
+                        { "test", "env" }
+                    }
+                });
+            return rgOp.Value;
+        }
+    }
+}
---
+++ sdk/dashboard/Azure.ResourceManager.Dashboard/tests/TestsCase/GrafanaResourceCollectionTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Dashboard.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Dashboard.Tests.TestsCase
+{
+    public class GrafanaResourceCollectionTests : DashboardTestBase
+    {
+        public GrafanaResourceCollectionTests(bool isAsync)
+           : base(isAsync)
+        {
+        }
+
+        private async Task<GrafanaResourceCollection> GetCollectionAsync()
+        {
+            var resourceGroup = await CreateResourceGroupAsync();
+            return resourceGroup.GetGrafanaResources();
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task CreateOrUpdate()
+        {
+            var container = await GetCollectionAsync();
+            var grafanaName = Recording.GenerateAssetName("sdkTestGrafana");
+            var input = ResourceDataHelper.GetGrafanaResourceData(DefaultLocation);
+            var lro = await container.CreateOrUpdateAsync(WaitUntil.Completed, grafanaName, input);
+            GrafanaResource actualResource = lro.Value;
+            Assert.AreEqual(grafanaName, actualResource.Data.Name);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Get()
+        {
+            var container = await GetCollectionAsync();
+            var grafanaName = Recording.GenerateAssetName("sdkTestGrafana");
+            var input = ResourceDataHelper.GetGrafanaResourceData(DefaultLocation);
+            var lro = await container.CreateOrUpdateAsync(WaitUntil.Completed, grafanaName, input);
+            GrafanaResource resource1 = lro.Value;
+            GrafanaResource resource2 = await container.GetAsync(grafanaName);
+            ResourceDataHelper.AssertGrafana(resource1.Data, resource2.Data);
+        }
+    }
+}
