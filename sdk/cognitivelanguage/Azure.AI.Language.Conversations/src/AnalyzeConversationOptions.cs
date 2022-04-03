7582331d442a4d41d538960e6f1bae463273aad7
Salah Mostafa <v-samostafa@microsoft.com>
2022-04-03 15:26:29 +0200

    added verbosae to options

--- sdk/cognitivelanguage/Azure.AI.Language.Conversations/src/AnalyzeConversationOptions.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.Conversations/src/AnalyzeConversationOptions.cs
@@ -7,18 +7,10 @@ using Azure.Core;
 namespace Azure.AI.Language.Conversations
 {
     /// <summary> The request body. </summary>
-    [CodeGenModel("ConversationAnalysisOptions")]
-    [CodeGenSuppress("AnalyzeConversationOptions", typeof(string))]
-    public partial class AnalyzeConversationOptions
+    public partial class ConversationAnalysisOptions
     {
-        /// <summary> Initializes a new instance of AnalyzeConversationOptions. </summary>
-        public AnalyzeConversationOptions()
-        {
-            Parameters = new ChangeTrackingDictionary<string, AnalysisParameters>();
-        }
-
         /// <summary> The conversation utterance to be analyzed. </summary>
-        [CodeGenMember("Query")]
-        internal string Utterance { get; set; }
+        [CodeGenMember("Verbose")]
+        public bool Verbose { get; set; }
     }
 }
