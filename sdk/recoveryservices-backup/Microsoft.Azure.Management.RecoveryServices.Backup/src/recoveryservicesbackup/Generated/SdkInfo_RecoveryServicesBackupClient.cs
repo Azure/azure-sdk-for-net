188f1d723d4d6e629a1e2b81b7f3072274f167c5
Anjor Hemant <anjorsh@users.noreply.github.com>
2022-04-04 16:00:02 -0700

    Additional TieringPolicy and ProtectedItemExtendedInfo details for RecoveryServicesBackup

--- sdk/recoveryservices-backup/Microsoft.Azure.Management.RecoveryServices.Backup/src/recoveryservicesbackup/Generated/SdkInfo_RecoveryServicesBackupClient.cs
+++ sdk/recoveryservices-backup/Microsoft.Azure.Management.RecoveryServices.Backup/src/recoveryservicesbackup/Generated/SdkInfo_RecoveryServicesBackupClient.cs
@@ -77,10 +77,10 @@ namespace Microsoft.Azure.Management.RecoveryServices.Backup
       // BEGIN: Code Generation Metadata Section
       public static readonly String AutoRestVersion = "2.0.4421";
       public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/main/specification/recoveryservicesbackup/resource-manager/readme.md --csharp --version=2.0.4421 --reflect-api-versions --csharp.namespace=Microsoft.Azure.Management.RecoveryServices.Backup --tag=package-2021-12 --csharp.output-folder=E:\\Sources\\Repos\\Azure\\azure-sdk-for-net-anjorsh\\sdk\\recoveryservices-backup\\Microsoft.Azure.Management.RecoveryServices.Backup\\src\\recoveryservicesbackup\\Generated";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/main/specification/recoveryservicesbackup/resource-manager/readme.md --csharp --version=2.0.4421 --reflect-api-versions --csharp.namespace=Microsoft.Azure.Management.RecoveryServices.Backup --tag=package-2021-12 --csharp.output-folder=E:\\AzureSDK\\sdk\\recoveryservices-backup\\Microsoft.Azure.Management.RecoveryServices.Backup\\src\\recoveryservicesbackup\\Generated";
       public static readonly String GithubForkName = "Azure";
       public static readonly String GithubBranchName = "main";
-      public static readonly String GithubCommidId = "4ac6be1b22f88bfbb6ca3e294e73538bcac90b49";
+      public static readonly String GithubCommidId = "6690ec1649168a1b5b2aadf4aa656de99eefdb23";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
