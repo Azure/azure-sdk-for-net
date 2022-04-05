d0969fddc7d4b1b021c60e2f12fdb354bc7ebb9d
Sean McCullough <seanmcc@microsoft.com>
2022-04-04 19:15:42 -0500

    Added 2021-10-04 service version

--- sdk/storage/Azure.Storage.Blobs/src/BlobClientOptions.cs
+++ sdk/storage/Azure.Storage.Blobs/src/BlobClientOptions.cs
@@ -96,7 +96,12 @@ namespace Azure.Storage.Blobs
             /// <summary>
             /// The 2021-08-06 service version.
             /// </summary>
-            V2021_08_06 = 13
+            V2021_08_06 = 13,
+
+            /// <summary>
+            /// The 2021-10-04 service version.
+            /// </summary>
+            V2021_10_04 = 14
 #pragma warning restore CA1707 // Identifiers should not contain underscores
         }
 
