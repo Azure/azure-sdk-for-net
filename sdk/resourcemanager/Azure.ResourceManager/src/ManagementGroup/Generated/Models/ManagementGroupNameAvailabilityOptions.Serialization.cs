cebe1dbfbe71dfe8b90600f67b9fc25105740215
Yao Kou <yaokou@microsoft.com>
2022-04-06 23:02:43 +0800

    update resource manager

--- sdk/resourcemanager/Azure.ResourceManager/src/ManagementGroup/Generated/Models/ManagementGroupNameAvailabilityOptions.Serialization.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/ManagementGroup/Generated/Models/ManagementGroupNameAvailabilityOptions.Serialization.cs
@@ -23,7 +23,7 @@ namespace Azure.ResourceManager.ManagementGroups.Models
             if (Optional.IsDefined(ResourceType))
             {
                 writer.WritePropertyName("type");
-                writer.WriteStringValue(ResourceType);
+                writer.WriteStringValue(ResourceType.Value);
             }
             writer.WriteEndObject();
         }
