3495018a47c78fbeb097105fea2dfc107abfdd19
Peter M <34331512+pmaytak@users.noreply.github.com>
2022-04-11 16:54:15 -0700

    Remove trailing spaces.

--- sdk/identity/Azure.Identity/src/TokenCacheRefreshArgs.cs
+++ sdk/identity/Azure.Identity/src/TokenCacheRefreshArgs.cs
@@ -14,9 +14,9 @@ namespace Azure.Identity
         /// A suggested token cache key, which can be used with general purpose storage mechanisms that allow
         /// storing key-value pairs and key based retrieval. Useful in applications that store one token cache per user,
         /// the recommended pattern for web apps.
-        /// 
-        /// The value is: 
-        /// 
+        ///
+        /// The value is:
+        ///
         /// <list type="bullet">
         /// <item><description><c>homeAccountId</c> for <c>AcquireTokenSilent</c>, <c>GetAccount(homeAccountId)</c>, <c>RemoveAccount</c> and when writing tokens on confidential client calls</description></item>
         /// <item><description><c>"{clientId}__AppTokenCache"</c> for <c>AcquireTokenForClient</c></description></item>
