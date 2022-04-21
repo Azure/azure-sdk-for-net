710a123200a645168ff00a394d0b3a313300baff
Madalyn Redding <mredding@microsoft.com>
2022-04-21 14:24:50 -0700

    README updates

--- sdk/eventhub/Azure.Messaging.EventHubs/stress/buildstress.ps1
+++
@@ -1,6 +0,0 @@
-# TODO: how to update registry path
-
-Set-Location -Path ..\..\..\..\
-Set-Location -Path .\artifacts\bin\Azure.Messaging.EventHubs.Stress\Debug\netcoreapp3.1
-
-dotnet Azure.Messaging.EventHubs.Stress.dll BasicPublishReadTest
