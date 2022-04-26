aa7cccb72463f63d3d035f340fa4520952929a2e
Caio Saldanha <camaiaor@microsoft.com>
2022-04-26 15:45:25 -0700

    Added GetDataFeeds perf tests

---
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/perf/Azure.AI.MetricsAdvisor.Perf/Infrastructure/PageSizeCountOptions.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using CommandLine;
+
+namespace Azure.Test.Perf
+{
+    public class PageSizeCountOptions : CountOptions
+    {
+        [Option("max-page-size", HelpText = "Maximum page size per call")]
+        public int? MaxPageSize { get; set; }
+    }
+}
---
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/perf/Azure.AI.MetricsAdvisor.Perf/Scenarios/GetDataFeeds.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.AI.MetricsAdvisor.Administration;
+using Azure.Test.Perf;
+using NUnit.Framework;
+
+namespace Azure.AI.MetricsAdvisor.Perf
+{
+    public sealed class GetDataFeeds : MetricsAdvisorTest<PageSizeCountOptions>
+    {
+        private readonly int _count;
+
+        private readonly GetDataFeedsOptions _options;
+
+        public GetDataFeeds(PageSizeCountOptions options) : base(options)
+        {
+            _count = options.Count;
+            _options = new GetDataFeedsOptions()
+            {
+                MaxPageSize = options.MaxPageSize
+            };
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+            int i = 0;
+
+            foreach (var _ in AdminClient.GetDataFeeds(_options, cancellationToken))
+            {
+                if (++i >= _count)
+                {
+                    break;
+                }
+            }
+
+#if DEBUG
+            Assert.That(i, Is.EqualTo(_count));
+#endif
+        }
+
+        public override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            int i = 0;
+
+            await foreach (var _ in AdminClient.GetDataFeedsAsync(_options, cancellationToken))
+            {
+                if (++i >= _count)
+                {
+                    break;
+                }
+            }
+
+#if DEBUG
+            Assert.That(i, Is.EqualTo(_count));
+#endif
+        }
+    }
+}
