66dd575942a8f8027bdedb8b823d1b43fc113911
Caio Saldanha <camaiaor@microsoft.com>
2022-04-26 13:51:01 -0700

    Added GetDataFeed perf test

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/perf/Azure.AI.MetricsAdvisor.Perf/Infrastructure/MetricsAdvisorTest.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/perf/Azure.AI.MetricsAdvisor.Perf/Infrastructure/MetricsAdvisorTest.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 using System;
+using Azure.AI.MetricsAdvisor.Administration;
+using Azure.AI.MetricsAdvisor.Models;
 using Azure.Test.Perf;
 
 namespace Azure.AI.MetricsAdvisor.Perf
@@ -16,10 +18,28 @@ namespace Azure.AI.MetricsAdvisor.Perf
             var credential = new MetricsAdvisorKeyCredential(TestEnvironment.MetricsAdvisorSubscriptionKey, TestEnvironment.MetricsAdvisorApiKey);
 
             Client = new MetricsAdvisorClient(uri, credential);
+            AdminClient = new MetricsAdvisorAdministrationClient(uri, credential);
         }
 
         protected PerfTestEnvironment TestEnvironment { get; }
 
         protected MetricsAdvisorClient Client { get; }
+
+        protected MetricsAdvisorAdministrationClient AdminClient { get; }
+
+        protected DataFeed GetDataFeedInstance()
+        {
+            var dataSource = new AzureBlobDataFeedSource("secret", "container", "template");
+            var ingestionStartTime = DateTimeOffset.Parse("2020-08-01T00:00:00Z");
+
+            return new DataFeed()
+            {
+                Name = "net-perf-" + Guid.NewGuid(),
+                DataSource = dataSource,
+                Granularity = new DataFeedGranularity(DataFeedGranularityType.Daily),
+                Schema = new DataFeedSchema() { MetricColumns = { new("cost") } },
+                IngestionSettings = new DataFeedIngestionSettings(ingestionStartTime)
+            };
+        }
     }
 }
---
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/perf/Azure.AI.MetricsAdvisor.Perf/Scenarios/GetDataFeed.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.AI.MetricsAdvisor.Models;
+using Azure.Test.Perf;
+
+namespace Azure.AI.MetricsAdvisor.Perf
+{
+    public sealed class GetDataFeed : MetricsAdvisorTest<PerfOptions>
+    {
+        private DataFeed _dataFeed;
+
+        public GetDataFeed(PerfOptions options) : base(options)
+        {
+        }
+
+        public override async Task GlobalSetupAsync()
+        {
+            _dataFeed = GetDataFeedInstance();
+            _dataFeed = await AdminClient.CreateDataFeedAsync(_dataFeed);
+        }
+
+        public override async Task GlobalCleanupAsync()
+        {
+            await AdminClient.DeleteDataFeedAsync(_dataFeed.Id);
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+            AdminClient.GetDataFeed(_dataFeed.Id, cancellationToken);
+        }
+
+        public override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            await AdminClient.GetDataFeedAsync(_dataFeed.Id, cancellationToken);
+        }
+    }
+}
