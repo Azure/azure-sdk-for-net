b354d1b40c9524dee42ebfd2eed94f8f8e9334b0
Anjor S. Hemant <anjorsh@users.noreply.github.com>
2022-04-28 01:56:11 -0700

    Additional TieringPolicy and ProtectedItemExtendedInfo details for RecoveryServicesBackup (#27620)
    * Additional TieringPolicy and ProtectedItemExtendedInfo details for RecoveryServicesBackup
    
    * Update recoveryservicesbackup_resource-manager.txt
    
    * Updated AssemblyInfo and CSProj files
    
    * Added Customizations
    
    * Rolling Back AssemblyVersion
    
    Co-authored-by: Minghao Chen <30464227+HarveyLink@users.noreply.github.com>

--- sdk/recoveryservices-backup/Microsoft.Azure.Management.RecoveryServices.Backup/src/recoveryservicesbackup/Generated/SdkInfo_RecoveryServicesBackupClient.cs
+++ sdk/recoveryservices-backup/Microsoft.Azure.Management.RecoveryServices.Backup/src/recoveryservicesbackup/Generated/SdkInfo_RecoveryServicesBackupClient.cs
@@ -75,12 +75,12 @@ namespace Microsoft.Azure.Management.RecoveryServices.Backup
           }
       }
       // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
+      public static readonly String AutoRestVersion = "2.0.4421";
       public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/main/specification/recoveryservicesbackup/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp.namespace=Microsoft.Azure.Management.RecoveryServices.Backup --tag=package-2021-12 --csharp.output-folder=C:\\azure-sdk-for-net\\sdk\\recoveryservices-backup\\Microsoft.Azure.Management.RecoveryServices.Backup\\src\\recoveryservicesbackup\\Generated";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/main/specification/recoveryservicesbackup/resource-manager/readme.md --csharp --version=2.0.4421 --reflect-api-versions --csharp.namespace=Microsoft.Azure.Management.RecoveryServices.Backup --tag=package-2021-12 --csharp.output-folder=E:\\AzureSDK\\sdk\\recoveryservices-backup\\Microsoft.Azure.Management.RecoveryServices.Backup\\src\\recoveryservicesbackup\\Generated";
       public static readonly String GithubForkName = "Azure";
       public static readonly String GithubBranchName = "main";
-      public static readonly String GithubCommidId = "0e85ddcc82e288d60b444213c0beb7f698ce5649";
+      public static readonly String GithubCommidId = "6690ec1649168a1b5b2aadf4aa656de99eefdb23";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
