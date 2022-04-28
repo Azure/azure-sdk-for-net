66334ae3c0f27dd59aea40818fbdda135c14d441
Anjor Hemant <anjorsh@users.noreply.github.com>
2022-04-28 00:48:37 -0700

    Rolling Back AssemblyVersion

--- sdk/recoveryservices-backup/Microsoft.Azure.Management.RecoveryServices.Backup/src/Properties/AssemblyInfo.cs
+++ sdk/recoveryservices-backup/Microsoft.Azure.Management.RecoveryServices.Backup/src/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure Recovery Services Backup Library")]
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure Recovery Services Backup Resources.")]
 
-[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyVersion("4.4.0.0")]
 [assembly: AssemblyFileVersion("4.5.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
