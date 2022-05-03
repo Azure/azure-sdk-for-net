2f0e4392a911c2aba95b18dc8a67bfccc9c6f5de
Sean McCullough <44180881+seanmcc-msft@users.noreply.github.com>
2022-05-02 19:48:23 -0500

    Revert "Added Tag Indexing for Versions (#27705)" (#28499)
    This reverts commit 32802845d0a79dfa37492d47cb0c82dbf2f873cb.

--- sdk/storage/Azure.Storage.Blobs/src/Models/TaggedBlobItem.cs
+++ sdk/storage/Azure.Storage.Blobs/src/Models/TaggedBlobItem.cs
@@ -21,16 +21,6 @@ namespace Azure.Storage.Blobs.Models
         public string BlobContainerName { get; internal set; }
 
         /// <summary>
-        /// VersionId.
-        /// </summary>
-        public string VersionId { get; internal set; }
-
-        /// <summary>
-        /// IsCurrentVersion.
-        /// </summary>
-        public bool? IsLatestVersion { get; internal set; }
-
-        /// <summary>
         /// Blob Tags.
         /// </summary>
         public Tags Tags { get; internal set; }
