6f41c4a1d498f4a754864d5be674f8cfbbf5978e
Nisha Bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2022-05-09 11:57:21 -0700

    wip

--- sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/LinkedEntity.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/LinkedEntity.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 namespace Azure.AI.TextAnalytics
 {
     /// <summary> The LinkedEntity. </summary>
+    /// This property only applies for <see cref="TextAnalyticsClientOptions.ServiceVersion.V3_1"/>, <see cref="TextAnalyticsClientOptions.ServiceVersion.V2022_04_01_Preview"/>, and up.
     public readonly partial struct LinkedEntity
     {
     }
