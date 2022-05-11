fca8c4641eaea9130bab985ff527f55b6bf1e665
archerzz <archerzz@gmail.com>
2022-05-11 17:15:35 +0800

    update api

--- sdk/confidentialledger/Azure.Security.ConfidentialLedger/api/Azure.Security.ConfidentialLedger.netstandard2.0.cs
+++ sdk/confidentialledger/Azure.Security.ConfidentialLedger/api/Azure.Security.ConfidentialLedger.netstandard2.0.cs
@@ -9,36 +9,40 @@ namespace Azure.Security.ConfidentialLedger
         public virtual System.Threading.Tasks.Task<Azure.Response> CreateOrUpdateUserAsync(string userId, Azure.Core.RequestContent content, Azure.RequestContext context = null) { throw null; }
         public virtual Azure.Response DeleteUser(string userId, Azure.RequestContext context = null) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> DeleteUserAsync(string userId, Azure.RequestContext context = null) { throw null; }
+        public virtual Azure.Response GetCollections(Azure.RequestContext context = null) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response> GetCollectionsAsync(Azure.RequestContext context = null) { throw null; }
         public virtual Azure.Response GetConsortiumMembers(Azure.RequestContext context = null) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> GetConsortiumMembersAsync(Azure.RequestContext context = null) { throw null; }
         public virtual Azure.Response GetConstitution(Azure.RequestContext context = null) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> GetConstitutionAsync(Azure.RequestContext context = null) { throw null; }
-        public virtual Azure.Response GetCurrentLedgerEntry(string subLedgerId = null, Azure.RequestContext context = null) { throw null; }
-        public virtual System.Threading.Tasks.Task<Azure.Response> GetCurrentLedgerEntryAsync(string subLedgerId = null, Azure.RequestContext context = null) { throw null; }
+        public virtual Azure.Response GetCurrentLedgerEntry(string collectionId = null, Azure.RequestContext context = null) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response> GetCurrentLedgerEntryAsync(string collectionId = null, Azure.RequestContext context = null) { throw null; }
         public virtual Azure.Response GetEnclaveQuotes(Azure.RequestContext context = null) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> GetEnclaveQuotesAsync(Azure.RequestContext context = null) { throw null; }
-        public virtual Azure.Pageable<System.BinaryData> GetLedgerEntries(string subLedgerId = null, string fromTransactionId = null, string toTransactionId = null, Azure.RequestContext context = null) { throw null; }
-        public virtual Azure.AsyncPageable<System.BinaryData> GetLedgerEntriesAsync(string subLedgerId = null, string fromTransactionId = null, string toTransactionId = null, Azure.RequestContext context = null) { throw null; }
-        public virtual Azure.Response GetLedgerEntry(string transactionId, string subLedgerId = null, Azure.RequestContext context = null) { throw null; }
-        public virtual System.Threading.Tasks.Task<Azure.Response> GetLedgerEntryAsync(string transactionId, string subLedgerId = null, Azure.RequestContext context = null) { throw null; }
+        public virtual Azure.Pageable<System.BinaryData> GetLedgerEntries(string collectionId = null, string fromTransactionId = null, string toTransactionId = null, Azure.RequestContext context = null) { throw null; }
+        public virtual Azure.AsyncPageable<System.BinaryData> GetLedgerEntriesAsync(string collectionId = null, string fromTransactionId = null, string toTransactionId = null, Azure.RequestContext context = null) { throw null; }
+        public virtual Azure.Response GetLedgerEntry(string transactionId, string collectionId = null, Azure.RequestContext context = null) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response> GetLedgerEntryAsync(string transactionId, string collectionId = null, Azure.RequestContext context = null) { throw null; }
         public virtual Azure.Response GetReceipt(string transactionId, Azure.RequestContext context = null) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> GetReceiptAsync(string transactionId, Azure.RequestContext context = null) { throw null; }
+        public virtual Azure.Response<Azure.Security.ConfidentialLedger.Models.TransactionReceipt> GetReceiptValue(string transactionId, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response<Azure.Security.ConfidentialLedger.Models.TransactionReceipt>> GetReceiptValueAsync(string transactionId, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual Azure.Response GetTransactionStatus(string transactionId, Azure.RequestContext context = null) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> GetTransactionStatusAsync(string transactionId, Azure.RequestContext context = null) { throw null; }
         public virtual Azure.Response GetUser(string userId, Azure.RequestContext context = null) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> GetUserAsync(string userId, Azure.RequestContext context = null) { throw null; }
-        public virtual Azure.Response PostLedgerEntry(Azure.Core.RequestContent content, string subLedgerId = null, Azure.RequestContext context = null) { throw null; }
+        public virtual Azure.Response PostLedgerEntry(Azure.Core.RequestContent content, string collectionId = null, Azure.RequestContext context = null) { throw null; }
         public virtual Azure.Security.ConfidentialLedger.PostLedgerEntryOperation PostLedgerEntry(Azure.Core.RequestContent content, string subLedgerId = null, bool waitForCompletion = true, Azure.RequestContext context = null) { throw null; }
-        public virtual System.Threading.Tasks.Task<Azure.Response> PostLedgerEntryAsync(Azure.Core.RequestContent content, string subLedgerId = null, Azure.RequestContext context = null) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response> PostLedgerEntryAsync(Azure.Core.RequestContent content, string collectionId = null, Azure.RequestContext context = null) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Security.ConfidentialLedger.PostLedgerEntryOperation> PostLedgerEntryAsync(Azure.Core.RequestContent content, string subLedgerId = null, bool waitForCompletion = true, Azure.RequestContext context = null) { throw null; }
     }
     public partial class ConfidentialLedgerClientOptions : Azure.Core.ClientOptions
     {
-        public ConfidentialLedgerClientOptions(Azure.Security.ConfidentialLedger.ConfidentialLedgerClientOptions.ServiceVersion version = Azure.Security.ConfidentialLedger.ConfidentialLedgerClientOptions.ServiceVersion.V0_1_preview) { }
+        public ConfidentialLedgerClientOptions(Azure.Security.ConfidentialLedger.ConfidentialLedgerClientOptions.ServiceVersion version = Azure.Security.ConfidentialLedger.ConfidentialLedgerClientOptions.ServiceVersion.V2022_04_20_preview) { }
         public System.TimeSpan OperationPollingInterval { get { throw null; } set { } }
         public enum ServiceVersion
         {
-            V0_1_preview = 1,
+            V2022_04_20_preview = 1,
         }
     }
     public static partial class ConfidentialLedgerConstants
@@ -63,5 +67,58 @@ namespace Azure.Security.ConfidentialLedger
         public override Azure.Response GetRawResponse() { throw null; }
         public override Azure.Response UpdateStatus(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public override System.Threading.Tasks.ValueTask<Azure.Response> UpdateStatusAsync(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+    }
+}
+namespace Azure.Security.ConfidentialLedger.Models
+{
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public readonly partial struct ConfidentialLedgerQueryState : System.IEquatable<Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState>
+    {
+        private readonly object _dummy;
+        private readonly int _dummyPrimitive;
+        public ConfidentialLedgerQueryState(string value) { throw null; }
+        public static Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState Loading { get { throw null; } }
+        public static Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState Ready { get { throw null; } }
+        public bool Equals(Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState other) { throw null; }
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
+        public override bool Equals(object obj) { throw null; }
+        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
+        public override int GetHashCode() { throw null; }
+        public static bool operator ==(Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState left, Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState right) { throw null; }
+        public static implicit operator Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState (string value) { throw null; }
+        public static bool operator !=(Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState left, Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState right) { throw null; }
+        public override string ToString() { throw null; }
+    }
+    public partial class ReceiptContents
+    {
+        internal ReceiptContents() { }
+        public string Cert { get { throw null; } }
+        public string Leaf { get { throw null; } }
+        public Azure.Security.ConfidentialLedger.Models.ReceiptLeafComponents LeafComponents { get { throw null; } }
+        public string NodeId { get { throw null; } }
+        public System.Collections.Generic.IReadOnlyList<Azure.Security.ConfidentialLedger.Models.ReceiptElement> Proof { get { throw null; } }
+        public string Root { get { throw null; } }
+        public System.Collections.Generic.IReadOnlyList<string> ServiceEndorsements { get { throw null; } }
+        public string Signature { get { throw null; } }
+    }
+    public partial class ReceiptElement
+    {
+        internal ReceiptElement() { }
+        public string Left { get { throw null; } }
+        public string Right { get { throw null; } }
+    }
+    public partial class ReceiptLeafComponents
+    {
+        internal ReceiptLeafComponents() { }
+        public string ClaimsDigest { get { throw null; } }
+        public string CommitEvidence { get { throw null; } }
+        public string WriteSetDigest { get { throw null; } }
+    }
+    public partial class TransactionReceipt
+    {
+        internal TransactionReceipt() { }
+        public Azure.Security.ConfidentialLedger.Models.ReceiptContents Receipt { get { throw null; } }
+        public Azure.Security.ConfidentialLedger.Models.ConfidentialLedgerQueryState State { get { throw null; } }
+        public string TransactionId { get { throw null; } }
     }
 }
