150f3eec2bb6dd381f5c643e21e72f0d9bff97fc
Mariana Rios Flores <mariari@microsoft.com>
2022-05-13 14:23:55 -0700

    All analyze (no pagination) working (#28772)
    Co-authored-by: Jesse Squire <jesse.squire@gmail.com>

--- sdk/textanalytics/Azure.AI.TextAnalytics/tests/samples/Sample11_MultiCategoryClassifyConvenienceAsync.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/tests/samples/Sample11_MultiCategoryClassifyConvenienceAsync.cs
@@ -11,14 +11,13 @@ namespace Azure.AI.TextAnalytics.Samples
     public partial class TextAnalyticsSamples : TextAnalyticsSampleBase
     {
         [Test]
-        [Ignore("LRO not implemented")]
         public async Task MultiCategoryClassifyConvenienceAsync()
         {
             // Create a text analytics client.
             string endpoint = TestEnvironment.Endpoint;
             string apiKey = TestEnvironment.ApiKey;
 
-            var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(apiKey), CreateSampleOptions());
+            var client = new TextAnalyticsClient(new Uri(endpoint), new AzureKeyCredential(apiKey), CreateSampleOptions(TextAnalyticsClientOptions.ServiceVersion.V2022_04_01_Preview));
 
             #region Snippet:TextAnalyticsMultiCategoryClassifyAsync
             // Get input document.
