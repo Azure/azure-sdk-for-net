5b21c1b744a607e1efce5008029ff2346830d5e4
Anne Thompson <annelo@microsoft.com>
2022-05-13 10:48:31 -0700

    add test files

---
+++ sdk/deviceupdate/Azure.IoT.DeviceUpdate/TestRunner/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Azure.IoT.DeviceUpdate;
+using Azure.Identity;
+using Azure;
+
+string endpoint = "https://example.iot.com";
+string instanceId = "<instance>";
+
+
+DeviceManagementClient client = new DeviceManagementClient(endpoint, instanceId, new DefaultAzureCredential());
+Operation<BinaryData> operation = await client.ImportDevicesAsync(WaitUntil.Started, "<action>", new BinaryData("<data>"));
+await operation.UpdateStatusAsync();
+
+if (!operation.HasCompleted)
+{
+    // Save out operation id
+    Console.WriteLine(operation.Id);
+}
+
+string operationId = operation.Id;
+
+// Coming back later
+
+Operation<BinaryData> resumeOperation = client.GetUpdateStatusOperation(operationId);
+
+
+
+Console.WriteLine("All done. :)");
