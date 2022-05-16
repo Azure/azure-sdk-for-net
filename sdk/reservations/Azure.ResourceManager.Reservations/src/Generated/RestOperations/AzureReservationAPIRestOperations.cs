f31f9bc02f848c6ff43f815818f4efd5996268ed
gaoyp830 <51546332+gaoyp830@users.noreply.github.com>
2022-05-16 16:22:56 -0700

    Update AzureReservationAPIRestOperations.cs

--- sdk/reservations/Azure.ResourceManager.Reservations/src/Generated/RestOperations/AzureReservationAPIRestOperations.cs
+++ sdk/reservations/Azure.ResourceManager.Reservations/src/Generated/RestOperations/AzureReservationAPIRestOperations.cs
@@ -42,6 +42,7 @@ namespace Azure.ResourceManager.Reservations
         {
             var message = _pipeline.CreateMessage();
             var request = message.Request;
+            // SqlDatabases is currently not supported in 2022-03-01 version, using 2020-10-01-preview for it instead
             var apiVersion = reservedResourceType.Equals("SqlDatabases", StringComparison.OrdinalIgnoreCase) ? "2020-10-01-preview" : _apiVersion;
             request.Method = RequestMethod.Get;
             var uri = new RawRequestUriBuilder();
