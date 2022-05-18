05d49aaaefba1358d475f7713bf96bf133380331
Anne Thompson <annelo@microsoft.com>
2022-05-18 11:20:54 -0700

    updates to add new ProtocolOperation type

---
+++ sdk/core/Azure.Core/src/ProtocolOperation.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core.Pipeline;
+
+namespace Azure.Core
+{
+    /// <summary>
+    /// </summary>
+    public class ProtocolOperation : Operation
+    {
+        private readonly OperationInternal _operation;
+        private readonly IOperation _nextLinkOperation;
+
+        internal ProtocolOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia, string scopeName)
+        {
+            _nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
+            _operation = new OperationInternal(clientDiagnostics, this, response, scopeName);
+        }
+
+        /// <summary>
+        /// For Mocking.
+        /// </summary>
+        protected ProtocolOperation()
+        {
+            // What does mocking mean for this type?
+            // _nextLinkOperation = ?
+            // _operation = ?
+        }
+
+        /// <summary>
+        /// </summary>
+        public override string Id => /* If following pattern, this will be in response.  If not, we'll throw NotSupportedException */
+
+        /// <summary>
+        /// </summary>
+        public override bool HasCompleted => throw new NotImplementedException();
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override Response GetRawResponse()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override Response UpdateStatus(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Response UpdateStatus(RequestContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public override ValueTask<Response> UpdateStatusAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- sdk/deviceupdate/Azure.IoT.DeviceUpdate/TestRunner/Program.cs
+++ sdk/deviceupdate/Azure.IoT.DeviceUpdate/TestRunner/Program.cs
@@ -1,30 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using Azure.IoT.DeviceUpdate;
+﻿using Azure.IoT.DeviceUpdate;
 using Azure.Identity;
 using Azure;
 
+#region LRO
+
 string endpoint = "https://example.iot.com";
 string instanceId = "<instance>";
-
-
 DeviceManagementClient client = new DeviceManagementClient(endpoint, instanceId, new DefaultAzureCredential());
-Operation<BinaryData> operation = await client.ImportDevicesAsync(WaitUntil.Started, "<action>", new BinaryData("<data>"));
-await operation.UpdateStatusAsync();
-
-if (!operation.HasCompleted)
-{
-    // Save out operation id
-    Console.WriteLine(operation.Id);
-}
-
+Operation operation = await client.ImportDevicesAsync(WaitUntil.Started, "<action>", new BinaryData("<update>"));
 string operationId = operation.Id;
 
+
 // Coming back later
+Operation<BinaryData> rehydratedOperation = new ProtocolOperation()
 
-Operation<BinaryData> resumeOperation = client.GetUpdateStatusOperation(operationId);
 
 
+////await operation.UpdateStatusAsync();
+
+//if (!operation.HasCompleted)
+//{
+//    // Save out operation id
+//    Console.WriteLine(operation.Id);
+//}
+
+//string operationId = operation.Id;
 
 Console.WriteLine("All done. :)");
+
+#endregion
+
+//#region Samples
+//// APIView: https://apiview.dev/Assemblies/Review/d8425ea2f8bd4d9eb58b27062f0b0a50
+
+//string endpoint = "https://example.iot.com";
+//string instanceId = "<instance>";
+
+//DeviceManagementClient client = new DeviceManagementClient(endpoint, instanceId, new DefaultAzureCredential());
+
+//#endregion
+
