d4e061fba0351b4ac522eee2ed0ec834deeb0184
sima-zhu <sizhu@microsoft.com>
2022-05-18 09:02:41 -0700

    Address comments

--- eng/common/scripts/Delete-RemoteBranches.ps1
+++ eng/common/scripts/Delete-RemoteBranches.ps1
@@ -8,9 +8,8 @@ param(
   # CentralRepoId the original PR to generate sync PR. E.g Azure/azure-sdk-tools for eng/common
   $CentralRepoId,
   # We start from the sync PRs, use the branch name to get the PR number of central repo. E.g. sync-eng/common-(<branchName>)-(<PrNumber>). Have group name on PR number.
-  $CentralPRRegex,
   [Parameter(Mandatory = $true)]
-  $BranchPrefix,  
+  $CentralPRRegex,
   # Date format: e.g. Tuesday, April 12, 2022 1:36:02 PM. Allow to use other date format.
   [AllowNull()]
   [DateTime]$LastCommitOlderThan,
@@ -20,34 +19,12 @@ param(
 
 . (Join-Path $PSScriptRoot common.ps1)
 
-LogDebug "Operating on Repo [ $RepoId ]"
-
-try{
-  $responses = Get-GitHubSourceReferences -RepoId $RepoId -Ref "heads/$BranchPrefix" -AuthToken $AuthToken
-}
-catch {
-  LogError "Get-GitHubSourceReferences failed with exception:`n$_"
-  exit 1
-}
-
-foreach ($res in $responses)
+function RetrievePRAndClose($pullRequestNumber)
 {
-  if (!$res -or !$res.ref) {
-    LogDebug "No branch returned from the branch prefix $BranchPrefix on $Repo. Skipping..."
-    continue
-  }
-  $branch = $res.ref
-  $branchName = $branch.Replace("refs/heads/","")
-  $branchName -match $CentralPRRegex | Out-Null
-  $pullRequestNumber = $Matches["PrNumber"]
-  if (!$pullRequestNumber) {
-    LogWarning "Did not fetch any pr number from the branch. Please double check $branchName"
-    continue
-  }
   try {
     $centralPR = Get-GitHubPullRequest -RepoId $CentralRepoId -PullRequestNumber $pullRequestNumber -AuthToken $AuthToken
-    if ($centralPR.state -ne "closed") {
-      continue
+    if ($centralPR.state -eq "closed") {
+      return
     }
   }
   catch 
@@ -80,7 +57,24 @@ foreach ($res in $responses)
       exit 1
     }
   }
-  
+}
+
+LogDebug "Operating on Repo [ $RepoId ]"
+
+try{
+  $responses = Get-GitHubSourceReferences -RepoId $RepoId -Ref "heads/$BranchPrefix" -AuthToken $AuthToken
+}
+catch {
+  LogError "Get-GitHubSourceReferences failed with exception:`n$_"
+  exit 1
+}
+
+foreach ($res in $responses)
+{
+  if (!$res -or !$res.ref) {
+    LogDebug "No branch returned from the branch prefix $BranchPrefix on $Repo. Skipping..."
+    continue
+  }
   if ($LastCommitOlderThan) {
     if (!$res.object -or !$res.object.url) {
       LogWarning "No commit url returned from response. Skipping... "
@@ -105,6 +99,15 @@ foreach ($res in $responses)
       exit 1
     }
   } 
+  
+  $branch = $res.ref
+  $branchName = $branch.Replace("refs/heads/","")
+  $branchName -match $CentralPRRegex | Out-Null
+  $pullRequestNumber = $Matches["PrNumber"]
+  if ($pullRequestNumber) {
+    RetrievePRAndClose -pullRequestNumber $pullRequestNumber
+  }
+  
   try {
     Remove-GitHubSourceReferences -RepoId $RepoId -Ref $branch -AuthToken $AuthToken
     LogDebug "The branch [ $branchName ] in [ $RepoId ] has been deleted."
