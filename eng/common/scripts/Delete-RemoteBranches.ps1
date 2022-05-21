cbea4dc3b190932f017ffe5c04b69b3d2a4f23e9
sima-zhu <sizhu@microsoft.com>
2022-05-21 08:26:26 -0700

    change the function to inline logic

--- eng/common/scripts/Delete-RemoteBranches.ps1
+++ eng/common/scripts/Delete-RemoteBranches.ps1
@@ -20,61 +20,6 @@ param(
 
 . (Join-Path $PSScriptRoot common.ps1)
 
-# RetrievePRAndClose will go to central repo to fetch the central PR using the given $pullRequetNumver, check whether it is closed or open. 
-# If central PR is merged/closed, then go ahead close sync PRs. Return true to the function.
-# If central PR is open, then return false to the function.
-function RetrievePRAndClose {
-  [CmdletBinding(SupportsShouldProcess)]
-  param (
-    $pullRequestNumber
-  )
-
-  if (!$pullRequestNumber) {    
-    LogError "Failed to fetch PR number from regex: $BranchRegex."
-    exit 1
-  }
-  try {
-    $centralPR = Get-GitHubPullRequest -RepoId $CentralRepoId -PullRequestNumber $pullRequestNumber -AuthToken $AuthToken
-    if ($centralPR.state -ne "closed") {
-      return $false
-    }
-    LogDebug "Found closed/merged pull request: $($centralPR.html_url)"
-  }
-  catch 
-  {
-    LogError "Get-GitHubPullRequests failed with exception:`n$_"
-    exit 1
-  }
-  
-  try {
-    $head = "${RepoId}:${branchName}"
-    LogDebug "Operating on branch [ $branchName ]"
-    $pullRequests = Get-GitHubPullRequests -RepoId $RepoId -State "all" -Head $head -AuthToken $AuthToken
-  }
-  catch
-  {
-    LogError "Get-GitHubPullRequests failed with exception:`n$_"
-    exit 1
-  }
-  $openPullRequests = $pullRequests | ? { $_.State -eq "open" }
-
-  foreach ($openPullRequest in $openPullRequests) {
-    try 
-    {
-      if ($PSCmdlet.ShouldProcess("[ $($openPullRequest.html_url) ] with branch [ $branchName ] in [ $RepoId ]", "Closing the pull request")) {
-        #Close-GithubPullRequest -apiurl $openPullRequest.url -AuthToken $AuthToken | Out-Null
-        Write-Host "Open pull Request [ $($openPullRequest.html_url) ] has been closed."
-      }
-    }
-    catch
-    {
-      LogError "Close-GithubPullRequest failed with exception:`n$_"
-      exit 1
-    }
-  }
-  return $true
-}
-
 LogDebug "Operating on Repo [ $RepoId ]"
 
 try{
@@ -97,13 +42,65 @@ foreach ($res in $responses)
   if (!($branchName -match $BranchRegex)) {
     continue
   }
-  $hasCentralPRClosedOrSkipChecking = $true
-  if ($CentralRepoId) {
-    # RetrievePRAndClose will go to central repo to fetch the central PR, check whether it is closed or merged. 
-    # If central PR is merged/closed, then go ahead close sync PRs. Return true to the function.
-    # If central PR is open, then return false to the function.
-    $hasCentralPRClosedOrSkipChecking = RetrievePRAndClose -pullRequestNumber $Matches["PrNumber"] 
+
+  # check central PR
+  # If central PR exist and open, then skip
+  $pullRequestNumber = $Matches["PrNumber"]
+    
+  # Get all open sync PRs associate with branch.
+  try {
+    $head = "${RepoId}:${branchName}"
+    LogDebug "Operating on branch [ $branchName ]"
+    $pullRequests = Get-GitHubPullRequests -RepoId $RepoId -State "all" -Head $head -AuthToken $AuthToken
   }
+  catch
+  {
+    LogError "Get-GitHubPullRequests failed with exception:`n$_"
+    exit 1
+  }
+  $openPullRequests = $pullRequests | ? { $_.State -eq "open" }
+
+  # If no central PR numver retrieved from branch regex, and there are open sync PR(s) associate with branch.
+  if (!$pullRequestNumber -and $openPullRequest.Count -gt 0) {
+    LogError "PR number not found from $CentralPRRegex. And there are open sync PR(s) associate with branch. Skipping."
+    continue
+  } 
+  if ($pullRequestNumber) {
+    try {
+      $centralPR = Get-GitHubPullRequest -RepoId $CentralRepoId -PullRequestNumber $pullRequestNumber -AuthToken $AuthToken
+      if ($centralPR.state -ne "closed") {
+        continue
+      }
+      LogDebug "Found closed/merged pull request: $($centralPR.html_url)"
+    }
+    catch 
+    {
+      if ($openPullRequest.Count -gt 0) {
+        LogDebug "PR number [ $pullRequestNumber ] from [ $CentralRepoId ]. And there are open sync PR(s) associate with branch. Skipping."
+        continue
+      }
+    }
+  }
+
+  # Two conditions to close sync open PRs.
+  # 2. Central repo PR not exist and no open sync PRs
+  # 2. Central repo PR exists and closed.
+  foreach ($openPullRequest in $openPullRequests) {
+    try 
+    {
+      if ($PSCmdlet.ShouldProcess("[ $($openPullRequest.html_url) ] with branch [ $branchName ] in [ $RepoId ]", "Closing the pull request")) {
+        Close-GithubPullRequest -apiurl $openPullRequest.url -AuthToken $AuthToken | Out-Null
+        Write-Host "Open pull Request [ $($openPullRequest.html_url) ] has been closed."
+      }
+    }
+    catch
+    {
+      LogError "Close-GithubPullRequest failed with exception:`n$_"
+      exit 1
+    }
+  }
+
+  # If there is date filter, then check if branch last commit older than the date.
   if ($LastCommitOlderThan) {
     if (!$res.object -or !$res.object.url) {
       LogWarning "No commit url returned from response. Skipping... "
@@ -130,7 +127,7 @@ foreach ($res in $responses)
   
   try {
     if ($hasCentralPRClosedOrSkipChecking -and $PSCmdlet.ShouldProcess("[ $branchName ] in [ $RepoId ]", "Deleting branches on cleanup script")) {
-      #Remove-GitHubSourceReferences -RepoId $RepoId -Ref $branch -AuthToken $AuthToken
+      Remove-GitHubSourceReferences -RepoId $RepoId -Ref $branch -AuthToken $AuthToken
       Write-Host "The branch [ $branchName ] with sha [$($res.object.sha)] in [ $RepoId ] has been deleted."
     }
   }
