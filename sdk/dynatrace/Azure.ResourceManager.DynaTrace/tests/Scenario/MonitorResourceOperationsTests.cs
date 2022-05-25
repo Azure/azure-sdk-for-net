879b9a3b4cce0ed6012957d5ff6809dd249709f3
Divyansh Agarwal <agarwald@microsoft.com>
2022-05-26 01:33:11 +0530

    Adding tests for Dynatrace SDK

---
+++ sdk/dynatrace/Azure.ResourceManager.DynaTrace/tests/Scenario/MonitorResourceOperationsTests.cs
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.DynaTrace.Models;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.DynaTrace.Tests
+{
+    public class MonitorResourceOperationsTests : MonitorTestBase
+    {
+        public MonitorResourceOperationsTests(bool isAsync)
+            : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Get()
+        {
+            var collection = await GetMonitorResourceCollectionAsync();
+            string monitorName = Recording.GenerateAssetName("testDT-");
+
+            var input = GetMonitorInput();
+
+            var lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, monitorName, input);
+            MonitorResource monitorResource1 = lro.Value;
+
+            MonitorResource monitorResource2 = await collection.GetAsync(monitorName);
+
+            Assert.AreEqual(monitorResource1.Data.Name, monitorResource2.Data.Name);
+            Assert.AreEqual(monitorResource1.Data.Id, monitorResource2.Data.Id);
+            Assert.AreEqual(monitorResource1.Data.ResourceType, monitorResource2.Data.ResourceType);
+            Assert.AreEqual(monitorResource1.Data.Location, monitorResource2.Data.Location);
+            Assert.AreEqual(monitorResource1.Data.Tags, monitorResource2.Data.Tags);
+        }
+
+        //[TestCase]
+        //[RecordedTest]
+        //public async Task Update()
+        //{
+        //    string monitorName = Recording.GenerateAssetName("testDT-");
+        //    MonitorResource monitorResource = await CreateMonitorResourceAsync(monitorName);
+
+        //    var update = new MonitorResourcePatch()
+        //    {
+        //        MonitoringStatus = MonitoringStatus.Enabled
+        //    };
+
+        //    var lro = await monitorResource.UpdateAsync(update);
+
+        //    MonitorResource updatedMonitorResource = lro.Value;
+
+        //    Assert.AreEqual(MonitoringStatus.Enabled, updatedMonitorResource.Data.MonitoringStatus);
+        //}
+
+        [TestCase]
+        [RecordedTest]
+        public async Task GetVMPayload()
+        {
+            string monitorName = Recording.GenerateAssetName("testDT-");
+            MonitorResource monitorResource = await CreateMonitorResourceAsync(monitorName);
+
+            var payload = monitorResource.GetVmHostPayloadAsync().Result.Value;
+
+            Assert.AreEqual(payload.EnvironmentId, monitorResource.Data.DynatraceEnvironmentProperties.EnvironmentInfo.EnvironmentId);
+        }
+    }
+}
