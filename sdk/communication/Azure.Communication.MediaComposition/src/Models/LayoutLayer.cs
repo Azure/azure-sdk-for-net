9a9100d362de6d5890ec8ff7f098d2aeda158544
minnieliu <minnieliu96@hotmail.com>
2022-05-26 14:46:06 -0700

    [MediaComposition] Introducing SDK Private Preview (#28816)
    * Generate code and fix errors
    
    * Fix build
    
    * Add README and CHANGELOG placeholder
    
    * Adding the public Media Composition Clients
    
    * Clean
    
    * Add Media Composition Client samples
    
    * Adding readmes for sample and test proj
    
    * Fix build and analyze
    
    * Regenerating after swagger update
    
    * Update sample syntax
    
    * Add skipReleaseStage: true
    
    * Final cleanup
    
    Co-authored-by: Minnie Liu <peiliu@microsoft.com>

---
+++ sdk/communication/Azure.Communication.MediaComposition/src/Models/LayoutLayer.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.Communication.MediaComposition.Models
+{
+    /// <summary>Layout Layer.</summary>
+    [CodeGenModel("Layer")]
+    public partial class LayoutLayer
+    {
+    }
+}
