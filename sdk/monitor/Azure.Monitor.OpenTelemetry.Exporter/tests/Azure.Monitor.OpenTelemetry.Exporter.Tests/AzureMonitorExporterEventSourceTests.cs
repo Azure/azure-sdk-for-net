f55cc1b579fa85b7b201a354be34726aa288787c
TimothyMothra <tilee@microsoft.com>
2022-05-27 14:31:32 -0700

    refactor EventSource

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/AzureMonitorExporterEventSourceTests.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/AzureMonitorExporterEventSourceTests.cs
@@ -21,7 +21,19 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Tests
         public void VerifyEventSource_Error() => Test(writeAction: AzureMonitorExporterEventSource.Log.WriteError, expectedId: 2, expectedName: "WriteError");
 
         [Fact]
+        public void VerifyEventSource_Error_WithException() => TestException(writeAction: AzureMonitorExporterEventSource.Log.WriteError, expectedId: 2, expectedName: "WriteError");
+
+        [Fact]
+        public void VerifyEventSource_Error_WithAggregateException() => TestAggregateException(writeAction: AzureMonitorExporterEventSource.Log.WriteError, expectedId: 2, expectedName: "WriteError");
+
+        [Fact]
         public void VerifyEventSource_Warning() => Test(writeAction: AzureMonitorExporterEventSource.Log.WriteWarning, expectedId: 3, expectedName: "WriteWarning");
+
+        [Fact]
+        public void VerifyEventSource_Warning_WithException() => TestException(writeAction: AzureMonitorExporterEventSource.Log.WriteWarning, expectedId: 3, expectedName: "WriteWarning");
+
+        [Fact]
+        public void VerifyEventSource_Warning_WithAggregateException() => TestAggregateException(writeAction: AzureMonitorExporterEventSource.Log.WriteWarning, expectedId: 3, expectedName: "WriteWarning");
 
         [Fact]
         public void VerifyEventSource_Informational() => Test(writeAction: AzureMonitorExporterEventSource.Log.WriteInformational, expectedId: 4, expectedName: "WriteInformational");
@@ -29,7 +41,7 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Tests
         [Fact]
         public void VerifyEventSource_Verbose() => Test(writeAction: AzureMonitorExporterEventSource.Log.WriteVerbose, expectedId: 5, expectedName: "WriteVerbose");
 
-        private void Test(Action<string> writeAction, int expectedId, string expectedName)
+        private void Test(Action<string, string> writeAction, int expectedId, string expectedName)
         {
             try
             {
@@ -38,14 +50,54 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Tests
                 var writer = new TestWriter();
                 TelemetryDebugWriter.Writer = writer;
 
-                writeAction("hello world");
+                writeAction(nameof(AzureMonitorExporterEventSourceTests), "hello world");
 
                 var test = writer.Messages.Single();
-                Assert.Equal($"OpenTelemetry-AzureMonitor-Exporter - EventId: [{expectedId}], EventName: [{expectedName}], Message: [hello world]", test);
+                Assert.Equal($"OpenTelemetry-AzureMonitor-Exporter - EventId: [{expectedId}], EventName: [{expectedName}], Message: [AzureMonitorExporterEventSourceTests - hello world]", test);
             }
             finally
             {
                 // Reset singleton to not affect other tests.
+                TelemetryDebugWriter.Writer = new TelemetryDebugWriter();
+            }
+        }
+
+        private void TestException(Action<string, Exception> writeAction, int expectedId, string expectedName)
+        {
+            try
+            {
+                // Normally our Listener will write to the Debugger.
+                // Here, we override the behavior of the TelemetryDebugWriter to intercept messages.
+                var writer = new TestWriter();
+                TelemetryDebugWriter.Writer = writer;
+
+                writeAction(nameof(AzureMonitorExporterEventSourceTests), new Exception("hello world"));
+
+                var test = writer.Messages.Single();
+                Assert.Equal($"OpenTelemetry-AzureMonitor-Exporter - EventId: [{expectedId}], EventName: [{expectedName}], Message: [AzureMonitorExporterEventSourceTests - System.Exception: hello world]", test);
+            }
+            finally
+            {
+                TelemetryDebugWriter.Writer = new TelemetryDebugWriter();
+            }
+        }
+
+        private void TestAggregateException(Action<string, Exception> writeAction, int expectedId, string expectedName)
+        {
+            try
+            {
+                // Normally our Listener will write to the Debugger.
+                // Here, we override the behavior of the TelemetryDebugWriter to intercept messages.
+                var writer = new TestWriter();
+                TelemetryDebugWriter.Writer = writer;
+
+                writeAction(nameof(AzureMonitorExporterEventSourceTests), new AggregateException(new Exception("hello world1"), new Exception("hello world2)")));
+
+                var test = writer.Messages.Single();
+                Assert.Equal($"OpenTelemetry-AzureMonitor-Exporter - EventId: [{expectedId}], EventName: [{expectedName}], Message: [AzureMonitorExporterEventSourceTests - System.Exception: hello world1]", test);
+            }
+            finally
+            {
                 TelemetryDebugWriter.Writer = new TelemetryDebugWriter();
             }
         }
