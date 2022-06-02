73d9e0f1efdd4f19af96aa5ab4d97c7288f5019f
Sean McCullough <44180881+seanmcc-msft@users.noreply.github.com>
2022-06-01 19:58:48 -0500

    Added constructors for DataLakePAthScheduleDeletionOptions (#29070)

--- sdk/storage/Azure.Storage.Files.DataLake/src/Models/DataLakePathScheduleDeletionOptions.cs
+++ sdk/storage/Azure.Storage.Files.DataLake/src/Models/DataLakePathScheduleDeletionOptions.cs
@@ -15,7 +15,7 @@ namespace Azure.Storage.Files.DataLake.Models
         /// Does not apply to directories.
         /// <see cref="TimeToExpire"/> and <see cref="ExpiresOn"/> cannot both be set.
         /// </summary>
-        public TimeSpan? TimeToExpire { get; set; }
+        public TimeSpan? TimeToExpire { get; private set; }
 
         /// <summary>
         /// The <see cref="DateTimeOffset"/> to set for when
@@ -24,6 +24,29 @@ namespace Azure.Storage.Files.DataLake.Models
         /// Does not apply to directories.
         /// <see cref="ExpiresOn"/> and <see cref="TimeToExpire"/> cannot both be set.
         /// </summary>
-        public DateTimeOffset? ExpiresOn { get; set; }
+        public DateTimeOffset? ExpiresOn { get; private set; }
+
+        /// <summary>
+        /// Constructor.  Sets the <see cref="DateTimeOffset"/> when the path will
+        /// be deleted.
+        /// </summary>
+        /// <param name="expiresOn">
+        /// The DateTimeOffset when the file will be deleted.
+        /// </param>
+        public DataLakePathScheduleDeletionOptions(DateTimeOffset? expiresOn)
+        {
+            ExpiresOn = expiresOn;
+        }
+
+        /// <summary>
+        /// Constructor.  Sets time when the path will be deleted, relative to the current time.
+        /// </summary>
+        /// <param name="timeToExpire">
+        /// Duration before path will be deleted.
+        /// </param>
+        public DataLakePathScheduleDeletionOptions(TimeSpan? timeToExpire)
+        {
+            TimeToExpire = timeToExpire;
+        }
     }
 }
