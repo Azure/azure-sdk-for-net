d8f31204278e3c6df838fc22897031da297b5f46
yanfa317 <53584318+yanfa317@users.noreply.github.com>
2022-06-06 19:56:13 -0700

    generated code

--- sdk/network/Microsoft.Azure.Management.Network/src/generate.ps1
+++ sdk/network/Microsoft.Azure.Management.Network/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "network/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "network/resource-manager" -AutoRestVersion "v2" -AutoRestCodeGenerationFlags "--tag=package-2022-04-preview"
