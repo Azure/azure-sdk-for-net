f28fbae35de14e5d225a589cc5342b241480091f
Emma Friesner <emfriesn@microsoft.com>
2022-06-09 12:36:23 -0400

    add tests and session records

--- sdk/digitaltwins/Microsoft.Azure.Management.DigitalTwins/tests/ScenarioTests/DigitalTwinsTestBase.cs
+++ sdk/digitaltwins/Microsoft.Azure.Management.DigitalTwins/tests/ScenarioTests/DigitalTwinsTestBase.cs
@@ -14,10 +14,22 @@ namespace DigitalTwins.Tests.ScenarioTests
     {
         private readonly object _initializeLock = new object();
 
-        protected static string DefaultLocation = "westus2";
+        protected static string DefaultLocation = "westcentralus";
         protected static string DefaultInstanceName = "DigitalTwinsSdk";
         protected static string DefaultEndpointName = "DigitalTwinsSdkEndpoint";
+        protected static string DefaultTsdbConnectionName = "DigitalTwinsSdkTsdbConnection";
         protected static string DefaultResourceGroupName = "DigitalTwinsSdkRg";
+
+        // Time series database connection endpoints configuration
+        protected static string DefaultAdxClusterName = "DigitalTwinsSdkCluster";
+        protected static string DefaultAdxEndpointUri = $"https://{DefaultAdxClusterName}.kusto.windows.net";
+        protected static string DefaultAdxDatabaseName = "DigitalTwinsSdkDatabase";
+        protected static string DefaultAdxTableName = "DigitalTwinsSdkDatabase";
+        protected static string DefaultAdxResourceId = $"/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/{DefaultResourceGroupName}/providers/Microsoft.Kusto/clusters/{DefaultAdxClusterName}";
+        protected static string DefaultEventHubNamespaceName = "DigitalTwinsSdkEventHubNamespace";
+        protected static string DefaultEventHubEndpointUri = $"sb://{DefaultEventHubNamespaceName}.servicebus.windows.net/";
+        protected static string DefaultEventHubNamespaceResourceId = $"/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/{DefaultResourceGroupName}/providers/Microsoft.EventHub/namespaces/{DefaultEventHubNamespaceName}";
+        protected static string DefaultEventHubName = "DigitalTwinsSdkEventHub";
 
         protected bool IsInitialized { get; private set; } = false;
         protected ResourceManagementClient ResourcesClient { get; private set; }
