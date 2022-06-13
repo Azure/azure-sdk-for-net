96214b17c5eb2f85f4ebfad951b10650e728e275
Jiri Burant <83883278+jiriburant@users.noreply.github.com>
2022-06-13 16:52:08 +0200

    Feature/communication siprouting (#26644)
    * Initial commit.
    
    * Implementation of the siprouting as a standalone package
    
    * Moved sipRouting functionality to PhoneNumbers SDK.
    
    * Updated testing environment to properly record the tests.
    
    * Fixed samples records
    
    * Removing changes in original PhoneNumbers files.
    
    * Removing trailing whitespace.
    
    * Changed sanitizer to match sanitize request URIs with communication connection string.
    
    * Fixed versioning.
    
    * Cleaned up autorest config.
    
    * Changed public interface to set trunks and routes separately.
    
    * Changed SipTrunk to contain name of the trunk.
    
    * Changed name of the factory.
    
    * Changed interface to use specific methods for trunks and routes instead of generic for Sip configuration.
    
    * Changed generation of code.
    
    * Changed public interface.
    
    * Updated the SIP routing branch.
    
    * Added missing unit tests for Setting and getting trunks and routes.
    
    * Created new test recordings.
    
    * Fixing null pointer bug on SipTrunkRoute trunkList.
    
    * Added sample files for SIP routing.
    
    * Updated recordings
    
    * Updated generated models to have SipSignalingPort as required. Putting together tests.
    
    * Regenerated test recordings. Added ignore pragma to ClientOptions to avoid errors in build pipeline.
    
    * Merge branch 'master' into feature/communication-siprouting
    
    * Changing and unifying tests namespaces.
    
    * Restoring csproj files to try fix build.
    
    * Removing problematic code.
    
    * Changed sanitizer to new style, removing common recordSanitizer.
    
    * Updated API code.
    
    * Implemented samples for SIP routing client and updated README.
    
    * Fixing README to pass the checks.
    
    * Updated README to reflect SIP routing changes.
    
    * Fixing readme.
    
    * Added necessary snippet formatting.
    
    * Updated Readme and Changelog, fixed an issue with client.
    
    * Regenerating readme
    
    * Addressing comments from review.
    
    * Fixing locale in URI.
    
    * Moved and fixed .md
    
    * Addressing comments from review.
    
    * Changed SetTrunks functions to avoid extra PATCH call.

---
+++ sdk/communication/Azure.Communication.PhoneNumbers/src/SipRouting/SipRoutingClientOptions.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core;
+
+namespace Azure.Communication.PhoneNumbers.SipRouting
+{
+    /// <summary>
+    /// The options for calling configuration client options <see cref="SipRoutingClientOptions"/>.
+    /// </summary>
+    public class SipRoutingClientOptions : ClientOptions
+    {
+        /// <summary>
+        /// The latest version of the calling configuration service.
+        /// </summary>
+        internal const ServiceVersion LatestVersion = ServiceVersion.V2021_05_01_preview1;
+
+        internal string ApiVersion { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SipRoutingClientOptions"/>.
+        /// </summary>
+        public SipRoutingClientOptions(ServiceVersion version = LatestVersion)
+        {
+            ApiVersion = version switch
+            {
+                ServiceVersion.V2021_05_01_preview1 => "2021-05-01-preview1",
+                _ => throw new ArgumentOutOfRangeException(nameof(version)),
+            };
+        }
+
+        /// <summary>
+        /// The phone number configuration service version.
+        /// </summary>
+        public enum ServiceVersion
+        {
+            /// <summary>
+            /// The V1 of the calling configuration service.
+            /// </summary>
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+#pragma warning disable AZC0016 // All parts of ServiceVersion members' names must begin with a number or uppercase letter and cannot have consecutive underscores
+            V2021_05_01_preview1 = 1
+#pragma warning restore AZC0016 // All parts of ServiceVersion members' names must begin with a number or uppercase letter and cannot have consecutive underscores
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+
+        }
+    }
+}
