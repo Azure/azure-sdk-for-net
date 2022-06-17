17537311e4bac35c9e24ed43ca796ba4b0f90daf
Heath Stewart <heaths@microsoft.com>
2022-06-17 13:45:27 -0700

    Merge the upstream release/clu-2022-05-01 branc (#29359)
    * Merge the upstream release/clu-2022-05-01 branch
    
    * Define LROs' 200 response models for codegen

--- sdk/cognitivelanguage/Azure.AI.Language.Conversations/src/ConversationAnalysisClientOptions.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.Conversations/src/ConversationAnalysisClientOptions.cs
@@ -9,23 +9,9 @@ namespace Azure.AI.Language.Conversations
     /// <summary>
     /// Client options for <see cref="ConversationAnalysisClient"/>.
     /// </summary>
+    [CodeGenModel("ConversationAnalysisProjectsClientOptions")]
     public partial class ConversationAnalysisClientOptions : ClientOptions
     {
-        private const ServiceVersion LatestVersion = ServiceVersion.V2022_05_01;
-
-        /// <summary>
-        /// The version of the service to use.
-        /// </summary>
-        public enum ServiceVersion
-        {
-#pragma warning disable CA1707 // Identifiers should not contain underscores
-            /// <summary>
-            /// Service version "2022-05-01".
-            /// </summary>
-            V2022_05_01 = 1,
-#pragma warning restore CA1707 // Identifiers should not contain underscores
-        }
-
         internal string Version { get; }
 
         /// <summary>
@@ -41,10 +27,5 @@ namespace Azure.AI.Language.Conversations
 
             this.ConfigureLogging();
         }
-
-        /// <summary>
-        /// Gets the method used to interpret string offsets, which is always <see cref="StringIndexType.Utf16CodeUnit"/> for .NET.
-        /// </summary>
-        internal static StringIndexType DefaultStringIndexType { get; } = StringIndexType.Utf16CodeUnit;
     }
 }
