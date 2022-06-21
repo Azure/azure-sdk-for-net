387a07bf15867ee53113791db293468a0af44ff1
Keerthi Thatipally <kthatipally@microsoft.com>
2022-06-21 12:43:20 -0500

    Azure Lab Services: updated assemblyVersion

--- sdk/labservices/Microsoft.Azure.Management.LabServices/src/Properties/AssemblyInfo.cs
+++ sdk/labservices/Microsoft.Azure.Management.LabServices/src/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Resources;
 
 [assembly: AssemblyTitle("Microsoft Azure Lab Services Management Library")]
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure Lab Services Resources.")]
-[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.1.0.0")]
 [assembly: AssemblyFileVersion("4.1.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
