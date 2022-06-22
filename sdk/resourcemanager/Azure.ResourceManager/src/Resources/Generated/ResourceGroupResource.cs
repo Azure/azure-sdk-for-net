8907f9f52985bd04a6c8925e1e6953ad3be45972
m-nash <prognash@gmail.com>
2022-06-22 10:33:23 -0700

    update api

--- sdk/resourcemanager/Azure.ResourceManager/src/Resources/Generated/ResourceGroupResource.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/Resources/Generated/ResourceGroupResource.cs
@@ -66,7 +66,7 @@ namespace Azure.ResourceManager.Resources
             TryGetApiVersion(ResourceType, out string resourceGroupResourcesApiVersion);
             _resourceGroupResourcesRestClient = new ResourcesRestOperations(Pipeline, Diagnostics.ApplicationId, Endpoint, resourceGroupResourcesApiVersion);
 #if DEBUG
-            ValidateResourceId(Id);
+			ValidateResourceId(Id);
 #endif
         }
 
