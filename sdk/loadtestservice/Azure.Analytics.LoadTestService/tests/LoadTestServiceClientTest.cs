8065be6cf4287481bf5181aebe90aa4a7f68aba9
Yashika Jain <t-yjain@microsoft.com>
2022-06-27 16:20:48 +0530

    link

--- sdk/loadtestservice/Azure.Analytics.LoadTestService/tests/LoadTestServiceClientTest.cs
+++ sdk/loadtestservice/Azure.Analytics.LoadTestService/tests/LoadTestServiceClientTest.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Core.Pipeline;
+using Azure.Core;
 using Azure.Core.TestFramework;
 using NUnit.Framework;
 
@@ -32,7 +33,25 @@ namespace Azure.Analytics.LoadTestService.Tests
         {
             Assert.IsTrue(true);
         }
-        [Test]
+        [RecordedTest]
+        public async Task CreateorUpdateLoadTest()
+        {
+            var client = CreateClient();
+            string test_id = Guid.NewGuid().ToString();
+            TestModel inputTestModel = new TestModel();
+            inputTestModel.testId = "a011890b-12cd-000a-0150-12aed8880000";
+            inputTestModel.displayName = "dotnetTest";
+            inputTestModel.resourceId = "/subscriptions/7c71b563-0dc0-4bc0-bcf6-06f8f0516c7a/resourceGroups/yashika-rg/providers/Microsoft.LoadTestService/loadtests/loadtestsdk";
+            inputTestModel.loadTestConfig = new LoadTestConfig();
+            inputTestModel.loadTestConfig.engineInstances = 1;
+            string testId = "a011890b-12cd-000a-0150-12aed8880000";
+            string jsonString = JsonSerializer.Serialize(inputTestModel);
+            RequestContent content = RequestContent.Create(jsonString);
+            var response = await client.CreateOrUpdateTestAsync(testId, content);
+            Assert.IsNotNull(response);
+        }
+
+        [RecordedTest]
         public async Task GetTest()
         {
             TestClient client = CreateClient();
