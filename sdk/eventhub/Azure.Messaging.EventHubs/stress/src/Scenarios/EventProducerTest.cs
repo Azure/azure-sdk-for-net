c2b94b85917c644bfa4707887a426a6b434b64b1
Madalyn Redding <mredding@microsoft.com>
2022-06-29 11:07:01 -0700

    indentation error

--- sdk/eventhub/Azure.Messaging.EventHubs/stress/src/Scenarios/EventProducerTest.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/stress/src/Scenarios/EventProducerTest.cs
@@ -79,7 +79,7 @@ public class EventProducerTest
     /// <param name="cancellationToken">A <see cref="CancellationToken"/> instance to signal the request to cancel the operation.</param>
     ///
     private Task RunRoleAsync(Role role,
-                                   CancellationToken cancellationToken)
+                              CancellationToken cancellationToken)
     {
         switch (role)
         {
