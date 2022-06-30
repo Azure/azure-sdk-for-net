feb623ccca0591f9daacb7e2c7e3070134faf5dc
James Schreppler <jaschrep@microsoft.com>
2022-06-30 14:38:46 -0400

    exportapi

--- sdk/storage/Azure.Storage.Common/api/Azure.Storage.Common.netstandard2.0.cs
+++ sdk/storage/Azure.Storage.Common/api/Azure.Storage.Common.netstandard2.0.cs
@@ -10,7 +10,7 @@ namespace Azure.Storage
     }
     public enum ClientSideEncryptionVersion
     {
-        [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
+        [System.ObsoleteAttribute("This version is considered insecure. Applications are encouraged to migrate to version 2.0 or to one of Azure Storage's server-side encryption solutions. See https://techcommunity.microsoft.com/t5/azure-storage-blog/preview-azure-storage-updating-client-side-encryption-in-sdk-to/ba-p/3522620 for more details.")]
         V1_0 = 1,
         V2_0 = 2,
     }
