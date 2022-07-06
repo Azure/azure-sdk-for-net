41dbabee782ecaa3fcc5bfe7e3a9bb9031465439
Sebastian Herzig-Patel <seherzig@microsoft.com>
2022-07-05 23:07:28 -0700

    Enable nullable arguments in model factory

--- sdk/digitaltwins/Azure.DigitalTwins.Core/src/Customized/DigitalTwinsModelFactory.cs
+++ sdk/digitaltwins/Azure.DigitalTwins.Core/src/Customized/DigitalTwinsModelFactory.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using Azure.Core;
 
+#nullable enable
+
 namespace Azure.DigitalTwins.Core
 {
     /// <summary> Model factory for read-only models. </summary>
