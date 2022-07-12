288b8e49c6d018dea8b85db4794cca7c605f3fb1
archerzz <archerzz@gmail.com>
2022-07-12 18:54:30 +0800

    fix(dataplane): `ServiceVersion` should be pascal case in manual codes
    - fix case problem in `ServiceVersion` in manual codes
    - remove suppression of `AZC0016`
    
    part of https://github.com/Azure/autorest.csharp/issues/1524

--- sdk/formrecognizer/Azure.AI.FormRecognizer/tests/DocumentAnalysisClient/DocumentAnalysisClientLiveTests.cs
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/tests/DocumentAnalysisClient/DocumentAnalysisClientLiveTests.cs
@@ -22,7 +22,7 @@ namespace Azure.AI.FormRecognizer.DocumentAnalysis.Tests
     /// </remarks>
     [IgnoreServiceError(400, "InvalidRequest", Message = "Content is not accessible: Invalid data URL", Reason = "https://github.com/Azure/azure-sdk-for-net/issues/28923")]
     [ClientTestFixture(
-     DocumentAnalysisClientOptions.ServiceVersion.V2022_06_30_preview)]
+     DocumentAnalysisClientOptions.ServiceVersion.V2022_06_30_Preview)]
     public class DocumentAnalysisClientLiveTests : DocumentAnalysisLiveTestBase
     {
         /// <summary>
