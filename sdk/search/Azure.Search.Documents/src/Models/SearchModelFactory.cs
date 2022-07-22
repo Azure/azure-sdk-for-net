67dc28b37ad6d87585c5e6b6836b871655222a10
Shivangi Reja <shreja@microsoft.com>
2022-07-22 14:23:07 -0700

    Add an example for SearchModelFactory.FacetResult

--- sdk/search/Azure.Search.Documents/src/Models/SearchModelFactory.cs
+++ sdk/search/Azure.Search.Documents/src/Models/SearchModelFactory.cs
@@ -317,6 +317,33 @@ namespace Azure.Search.Documents.Models
             string queryPlusText) =>
             new AutocompleteItem(text, queryPlusText);
 
+        /// <summary> Initializes a new instance of FacetResult. </summary>
+        /// <param name="count"> The approximate count of documents falling within the bucket described by this facet. </param>
+        /// <param name="additionalProperties"> Additional Properties. </param>
+        /// <returns> A new <see cref="Models.FacetResult"/> instance for mocking. </returns>
+        /// <example>
+        /// This sample shows how to mock <see cref="Models.FacetResult"/> type.
+        /// <code><![CDATA[
+        /// var count = 2;
+        /// var additionalProperties = new Dictionary<string, object>()
+        /// {
+        ///      ["value"] = "Luxury"
+        /// }
+        /// var facetResult = SearchModelFactory.FacetResult(count, additionalProperties);
+        /// Assert.AreEqual(count, facetResult.Count);
+        ///
+        /// var additionalProperty = additionalProperties.First();
+        /// Assert.AreEqual(additionalProperty.Value, facetResult.TryGetValue(additionalProperty.Key, out object value) ? value : null);
+        /// ]]></code>
+        /// </example>
+        /// <remarks> For more details please refer <see href="https://docs.microsoft.com/en-us/rest/api/searchservice/search-documents#query-parameters"/></remarks>
+        public static FacetResult FacetResult(long? count = null, IReadOnlyDictionary<string, object> additionalProperties = null)
+        {
+            additionalProperties ??= new Dictionary<string, object>();
+
+            return new FacetResult(count, additionalProperties);
+        }
+
         /// <summary> Initializes a new instance of IndexDocumentsResult. </summary>
         /// <param name="results"> The list of status information for each document in the indexing request. </param>
         /// <returns> A new IndexDocumentsResult instance for mocking. </returns>
