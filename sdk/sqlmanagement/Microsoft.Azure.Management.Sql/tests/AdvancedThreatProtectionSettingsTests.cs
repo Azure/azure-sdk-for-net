b427cfa5ce24a6ebfc1f6d6faa13efc6ef99dc63
Uriel Cohen <urielc@microsoft.com>
2022-07-24 02:17:51 -0700

    Adding AdvancedThreatProtectionSettings tests for ManagedInstance

--- sdk/sqlmanagement/Microsoft.Azure.Management.Sql/tests/AdvancedThreatProtectionSettingsTests.cs
+++ sdk/sqlmanagement/Microsoft.Azure.Management.Sql/tests/AdvancedThreatProtectionSettingsTests.cs
@@ -14,7 +14,7 @@ namespace Sql.Tests
     public class AdvancedThreatProtectionTests
     {
         [Fact]
-        public void TestThreatProtection()
+        public void TestThreatProtectionOnServerAndDatabase()
         {
             string testPrefix = "server-threat-protection-test-";
 
@@ -80,6 +80,79 @@ namespace Sql.Tests
                 sqlClient.DatabaseAdvancedThreatProtectionSettings.CreateOrUpdate(resourceGroup.Name, server.Name, dbName, updatedDatabaseSettings);
 
                 var getUpdatedDatabaseSettingsResponse = sqlClient.DatabaseAdvancedThreatProtectionSettings.Get(resourceGroup.Name, server.Name, dbName);
+
+                // Verify that the Get request contains the updated settings.
+                Assert.Equal(updatedDatabaseSettings.State, getUpdatedDatabaseSettingsResponse.State);
+            }
+        }
+
+        [Fact]
+        public void TestThreatProtectionOnManagedInstance()
+        {
+            string testPrefix = "mi-threat-protection-test-";
+
+            using (SqlManagementTestContext context = new SqlManagementTestContext(this))
+            {
+                ResourceGroup resourceGroup = context.CreateResourceGroup();
+                ManagedInstance managedInstance = context.CreateManagedInstance(resourceGroup);
+                SqlManagementClient sqlClient = context.GetClient<SqlManagementClient>();
+
+                // Create some small databases in the Managed Instance
+                ManagedDatabase[] databases = SqlManagementTestUtilities.CreateManagedDatabasesAsync(
+                    sqlClient, resourceGroup.Name, managedInstance, testPrefix, 2).Result;
+
+                // ManagedInstance Advanced Threat Protection settings
+                ManagedInstanceAdvancedThreatProtection defaultInstanceSettingsResponse = sqlClient.ManagedInstanceAdvancedThreatProtectionSettings.Get(resourceGroup.Name, managedInstance.Name);
+
+                // Verify that the initial Get request contains the default settings.
+                Assert.Equal(AdvancedThreatProtectionState.Disabled, defaultInstanceSettingsResponse.State);
+
+                // Modify the settings. Then send, receive and see if its still ok.
+                ManagedInstanceAdvancedThreatProtection updatedInstanceSettings = new ManagedInstanceAdvancedThreatProtection
+                {
+                    State = AdvancedThreatProtectionState.Enabled
+                };
+
+                // Set Advanced Threat Protection settings for server.
+                sqlClient.ManagedInstanceAdvancedThreatProtectionSettings.CreateOrUpdate(resourceGroup.Name, managedInstance.Name, updatedInstanceSettings);
+
+                // Get Advanced Threat Protection settings from the managed instance.
+                var getUpdatedInstanceSettingsResponse = sqlClient.ManagedInstanceAdvancedThreatProtectionSettings.Get(resourceGroup.Name, managedInstance.Name);
+
+                // Verify that the Get request contains the updated settings.
+                Assert.Equal(updatedInstanceSettings.State, getUpdatedInstanceSettingsResponse.State);
+
+                // Modify the settings again. Then send, receive and see if its still ok.
+                updatedInstanceSettings = new ManagedInstanceAdvancedThreatProtection
+                {
+                    State = AdvancedThreatProtectionState.Disabled
+                };
+
+                // Set Advanced Threat Protection settings for managed instance.
+                sqlClient.ManagedInstanceAdvancedThreatProtectionSettings.CreateOrUpdate(resourceGroup.Name, managedInstance.Name, updatedInstanceSettings);
+
+                // Get Advanced Threat Protection settings from the managed instance.
+                getUpdatedInstanceSettingsResponse = sqlClient.ManagedInstanceAdvancedThreatProtectionSettings.Get(resourceGroup.Name, managedInstance.Name);
+
+                // Verify that the Get request contains the updated settings.
+                Assert.Equal(updatedInstanceSettings.State, getUpdatedInstanceSettingsResponse.State);
+
+                // ManagedDatabase Advanced Threat Protection settings.
+                string dbName = databases[0].Name;
+                ManagedDatabaseAdvancedThreatProtection defaultDatabaseSettingsResponse = sqlClient.ManagedDatabaseAdvancedThreatProtectionSettings.Get(resourceGroup.Name, managedInstance.Name, dbName);
+
+                // Verify that the initial Get request contains the default settings.
+                Assert.Equal(AdvancedThreatProtectionState.Disabled, defaultDatabaseSettingsResponse.State);
+
+                // Modify the policy properties. Then send, receive and see if its still ok.
+                ManagedDatabaseAdvancedThreatProtection updatedDatabaseSettings = new ManagedDatabaseAdvancedThreatProtection
+                {
+                    State = AdvancedThreatProtectionState.Disabled
+                };
+
+                sqlClient.ManagedDatabaseAdvancedThreatProtectionSettings.CreateOrUpdate(resourceGroup.Name, managedInstance.Name, dbName, updatedDatabaseSettings);
+
+                var getUpdatedDatabaseSettingsResponse = sqlClient.ManagedDatabaseAdvancedThreatProtectionSettings.Get(resourceGroup.Name, managedInstance.Name, dbName);
 
                 // Verify that the Get request contains the updated settings.
                 Assert.Equal(updatedDatabaseSettings.State, getUpdatedDatabaseSettingsResponse.State);
