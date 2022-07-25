d53d33d9d4042c53d5713f770f85957e8b793cac
Anika Tabassum Sardar Neela <aneela@microsoft.com>
2022-07-25 14:12:17 -0700

    first iter

---
+++ sdk/guestconfiguration/Azure.ResourceManager.GuestConfiguration/tests/Scenario/GuestConfigurationAssignmentCollectionTests.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.GuestConfiguration.Tests.Utilities;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.GuestConfiguration.Tests.Scenario
+{
+    [TestFixture]
+    public class GuestConfigurationAssignmentCollectionTests : GuestConfigurationManagementTestBase
+    {
+        public GuestConfigurationAssignmentCollectionTests() : base(true)
+        {
+        }
+
+        [SetUp]
+        public async Task ClearAndInitialize()
+        {
+            if (Mode == RecordedTestMode.Record || Mode == RecordedTestMode.Playback)
+            {
+                await InitializeClients();
+            }
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            CleanupResourceGroups();
+        }
+
+        [TestCase]
+        public async Task CanCreateGetUpdateGuestConfigurationAssignment()
+        {
+            var resourceGroupName = GuestConfigurationManagementUtilities.DefaultResourceGroupName;
+            var vmName = GuestConfigurationManagementUtilities.DefaultAzureVMName;
+            GuestConfigurationAssignmentCollection guestConfigurationAssignmentCollection = await GetGuestConfigurationAssignmentCollectionAsync(resourceGroupName, vmName);
+
+            // Create a new guest configuration assignment
+            try
+            {
+                GuestConfigurationAssignmentData gcAssignmentData = GetDefaultContactGuestConfigurationAssignmentData(guestConfigurationAssignmentCollection.Id);
+
+                ArmOperation<GuestConfigurationAssignmentResource> createAssignmentOperation = await guestConfigurationAssignmentCollection.CreateOrUpdateAsync(WaitUntil.Completed, GuestConfigurationManagementUtilities.DefaultAssignmentName, gcAssignmentData);
+                await createAssignmentOperation.WaitForCompletionAsync();
+                Assert.IsTrue(createAssignmentOperation.HasCompleted);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+           //Assert.IsTrue(createAssignmentOperation.HasValue);
+
+            // Get created guest configuration assignment
+            //Response<GuestConfigurationAssignmentResource> getGuestAssignmentResponse = await guestConfigurationAssignmentCollection.GetAsync(GuestConfigurationManagementUtilities.DefaultAssignmentName);
+            //GuestConfigurationAssignmentResource guestAssignmentResource = getGuestAssignmentResponse.Value;
+            //Assert.IsNotNull(guestAssignmentResource);
+            //Assert.Equals(gcAssignmentData.Location, guestAssignmentResource.Data.Location);
+        }
+    }
+}
---
+++ sdk/guestconfiguration/Azure.ResourceManager.GuestConfiguration/tests/Utilities/GuestConfigurationManagementUtilities.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.ResourceManager.GuestConfiguration.Tests.Utilities
+{
+    public class GuestConfigurationManagementUtilities
+    {
+        public const string DefaultResourceGroupName = "GuestConfigurationSDKTestRecord";
+        //public const string DefaultResourceLocation = "westcentralus";
+        public const string DefaultResourceLocation = "centraluseuap";
+        public const string DefaultAzureVMName = "SDKTestRecordVM002";
+        public const string DefaultAssignmentType = "ApplyAndAutoCorrect";
+        public const string DefaultKind = "DSC";
+        public const string DefaultAssignmentVersion = "1.0.0.3";
+        public const string DefaultContext = "Azure policy A";
+
+        public const string HybridRG = "neela-sdk-rg";
+        public const string HybridMachineName = "LAPTOP-4B77J53J";
+        public const string DefaultAssignmentName = "AuditSecureProtocol";
+
+        public const string VMSSRG = "aashishDeleteRG";
+        public const string VMSSName = "vmss6";
+        public const string VMSSAssignmentName = "EnforcePasswordHistory$pid23q5eseudwr5y";
+        public const string VMSSReportID = "21a601c0-f39e-48a0-82f2-7eb17e2c899c";
+    }
+}
