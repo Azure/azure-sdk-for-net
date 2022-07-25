f221b232719a7f0555c9d9173b6ca2be5402cd40
Sean McCullough <seanmcc@microsoft.com>
2022-07-25 11:29:28 -0500

    Bumped default SAS version and default service version

--- sdk/storage/Azure.Storage.Common/src/Shared/StorageVersionExtensions.cs
+++ sdk/storage/Azure.Storage.Common/src/Shared/StorageVersionExtensions.cs
@@ -36,7 +36,7 @@ namespace Azure.Storage
         /// </summary>
         public const ServiceVersion LatestVersion =
 #if BlobSDK || QueueSDK || FileSDK || DataLakeSDK || ChangeFeedSDK
-            ServiceVersion.V2021_08_06;
+            ServiceVersion.V2021_10_04;
 #else
             ERROR_STORAGE_SERVICE_NOT_DEFINED;
 #endif
