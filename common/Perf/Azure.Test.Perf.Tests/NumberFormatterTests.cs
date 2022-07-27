328af387621714133af945cb402ae4ad45ac0cea
Mike Harder <mharder@microsoft.com>
2022-07-27 14:29:58 -0700

    Display perf results with 4 significant figures

---
+++ common/Perf/Azure.Test.Perf.Tests/NumberFormatterTests.cs
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using NUnit.Framework;
+using System;
+
+namespace Azure.Test.Perf.Tests
+{
+    public class NumberFormatterTests
+    {
+        [TestCase(0.00012345, 1, "0.0001")]
+        [TestCase(0.00012345, 2, "0.00012")]
+        [TestCase(0.00012345, 3, "0.000123")]
+        [TestCase(0.00012345, 4, "0.0001235")]
+        [TestCase(0.00012345, 5, "0.00012345")]
+        [TestCase(0.00012345, 6, "0.000123450")]
+        [TestCase(0.00012345, 7, "0.0001234500")]
+        [TestCase(1.2345, 1, "1")]
+        [TestCase(1.2345, 2, "1.2")]
+        [TestCase(1.2345, 3, "1.23")]
+        [TestCase(1.2345, 4, "1.235")]
+        [TestCase(1.2345, 5, "1.2345")]
+        [TestCase(1.2345, 6, "1.23450")]
+        [TestCase(1.2345, 7, "1.234500")]
+        [TestCase(12_345, 1, "10,000")]
+        [TestCase(12_345, 2, "12,000")]
+        [TestCase(12_345, 3, "12,300")]
+        [TestCase(12_345, 4, "12,350")]
+        [TestCase(12_345, 5, "12,345")]
+        [TestCase(12_345, 6, "12,345.0")]
+        [TestCase(12_345, 7, "12,345.00")]
+        [TestCase(8.22929639076288, 4, "8.229")]
+        public void Format(double value, int significantDigits, string expected)
+        {
+            Assert.AreEqual(expected, NumberFormatter.Format(value, significantDigits));
+        }
+
+        [TestCase(0, 0, typeof(ArgumentException))]
+        [TestCase(-1, 0, typeof(ArgumentException))]
+        [TestCase(1.2345, 0, typeof(ArgumentException))]
+        [TestCase(1.2345, -1, typeof(ArgumentException))]
+        public void FormatException(double value, int significantDigits, Type expectedExceptionType)
+        {
+            Assert.Throws(expectedExceptionType, () => NumberFormatter.Format(value, significantDigits));
+        }
+    }
+}
---
+++ common/Perf/Azure.Test.Perf/NumberFormatter.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+
+namespace Azure.Test.Perf
+{
+    public static class NumberFormatter
+    {
+        // Formats a positive double with the specified number of significant digits
+        // Example: Format(Math.PI, 4) -> "3.142"
+        public static string Format(double value, int significantDigits)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Must be greater than zero", nameof(value));
+            }
+
+            if (significantDigits <= 0)
+            {
+                throw new ArgumentException("Must be greater than zero", nameof(significantDigits));
+            }
+
+            var log = Math.Log10(value);
+            var divisor = Math.Pow(10, Math.Ceiling(log - significantDigits));
+            var rounded = divisor * Math.Round(value / divisor, MidpointRounding.AwayFromZero);
+
+            var decimals = Math.Ceiling(Math.Max(0, significantDigits - log - 1));
+
+            return rounded.ToString($"N{decimals}");
+        }
+    }
+}
