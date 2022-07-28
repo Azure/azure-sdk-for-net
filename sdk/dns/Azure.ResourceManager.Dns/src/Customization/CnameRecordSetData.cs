7b10ceee3c8a2d90a9d82ae87dc0d6dffdab36dd
Chengming Ruan <61542202+dvbb@users.noreply.github.com>
2022-07-28 17:31:42 +0800

    [Dns] Resolve Apiview Comments (#30120)
    * sync latest common rules
    
    * Mx  Ns => MX NS
    
    * Mx => MX
    
    * Etag => ETag
    
    * Ptrdname => PtrdName . Nsdname => NsdName
    
    * TTL => Ttl
    
    * Fix test cases
    
    * Update Azure.ResourceManager.Dns.netstandard2.0.cs
    
    * update test cases
    
    * test cases: Mx Ns => MX NS
    
    * re-record
    
    * Mx Ns => MX NS
    
    * TTL => Ttl
    
    * sync latest rules
    
    * type of ifMatch: string => ETag?
    
    * Add prepend name
    
    * Update Azure.ResourceManager.Dns.netstandard2.0.cs
    
    * Re-Gen
    
    * Add prepend name to 10 RecordSet
    
    * update customization
    
    * Update Azure.ResourceManager.Dns.netstandard2.0.cs
    
    * RecordType =>DnsRecordType
    
    * update customization
    
    * Add IPAddress rename rules
    
    * expireTime => expireTimeInSeconds
    
    * FooTime => FooTimeInSeconds

--- sdk/dns/Azure.ResourceManager.Dns/src/Customization/CnameRecordSetData.cs
+++ sdk/dns/Azure.ResourceManager.Dns/src/Customization/CnameRecordSetData.cs
@@ -6,6 +6,7 @@
 #nullable disable
 
 using System.Collections.Generic;
+using Azure;
 using Azure.Core;
 using Azure.ResourceManager.Dns.Models;
 using Azure.ResourceManager.Models;
@@ -29,16 +30,16 @@ namespace Azure.ResourceManager.Dns
         /// <param name="systemData"> The systemData. </param>
         /// <param name="etag"> The etag of the record set. </param>
         /// <param name="metadata"> The metadata attached to the record set. </param>
-        /// <param name="ttl"> The TTL (time-to-live) of the records in the record set. </param>
+        /// <param name="ttl"> The Ttl (time-to-live) of the records in the record set. </param>
         /// <param name="fqdn"> Fully qualified domain name of the record set. </param>
         /// <param name="provisioningState"> provisioning State of the record set. </param>
         /// <param name="targetResource"> A reference to an azure resource from where the dns resource value is taken. </param>
         /// <param name="cnameRecord"> The CNAME record in the  record set. </param>
-        internal CnameRecordSetData(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, string etag, IDictionary<string, string> metadata, long? ttl, string fqdn, string provisioningState, WritableSubResource targetResource, CnameRecord cnameRecord) : base(id, name, resourceType, systemData)
+        internal CnameRecordSetData(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, ETag? etag, IDictionary<string, string> metadata, long? ttl, string fqdn, string provisioningState, WritableSubResource targetResource, DnsCnameRecord cnameRecord) : base(id, name, resourceType, systemData)
         {
-            Etag = etag;
+            ETag = etag;
             Metadata = metadata;
-            TTL = ttl;
+            Ttl = ttl;
             Fqdn = fqdn;
             ProvisioningState = provisioningState;
             TargetResource = targetResource;
@@ -46,11 +47,11 @@ namespace Azure.ResourceManager.Dns
         }
 
         /// <summary> The etag of the record set. </summary>
-        public string Etag { get; set; }
+        public ETag? ETag { get; set; }
         /// <summary> The metadata attached to the record set. </summary>
         public IDictionary<string, string> Metadata { get; }
-        /// <summary> The TTL (time-to-live) of the records in the record set. </summary>
-        public long? TTL { get; set; }
+        /// <summary> The Ttl (time-to-live) of the records in the record set. </summary>
+        public long? Ttl { get; set; }
         /// <summary> Fully qualified domain name of the record set. </summary>
         public string Fqdn { get; }
         /// <summary> provisioning State of the record set. </summary>
@@ -70,7 +71,7 @@ namespace Azure.ResourceManager.Dns
         }
 
         /// <summary> The CNAME record in the  record set. </summary>
-        internal CnameRecord CnameRecord { get; set; }
+        internal DnsCnameRecord CnameRecord { get; set; }
         /// <summary> The canonical name for this CNAME record. </summary>
         public string Cname
         {
@@ -78,7 +79,7 @@ namespace Azure.ResourceManager.Dns
             set
             {
                 if (CnameRecord is null)
-                    CnameRecord = new CnameRecord();
+                    CnameRecord = new DnsCnameRecord();
                 CnameRecord.Cname = value;
             }
         }
