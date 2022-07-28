99f67c25031660dad942119e36e088986ad3fc1d
Anika Tabassum Sardar Neela <aneela@microsoft.com>
2022-07-28 12:03:53 -0700

    second test -in prog

--- sdk/guestconfiguration/Azure.ResourceManager.GuestConfiguration/tests/Scenario/GuestConfigurationAssignmentCollectionTests.cs
+++ sdk/guestconfiguration/Azure.ResourceManager.GuestConfiguration/tests/Scenario/GuestConfigurationAssignmentCollectionTests.cs
@@ -61,5 +61,42 @@ namespace Azure.ResourceManager.GuestConfiguration.Tests.Scenario
             GuestConfigurationAssignmentResource updatedGuestAssignmentResourceRetrieved = updateAssignmentOperation.Value;
             Assert.AreEqual(updatedContext, updatedGuestAssignmentResourceRetrieved.Data.Properties.Context);
         }
+
+        [TestCase]
+        public async Task CanCreateGetUpdateGuestConfigurationHCRPAssignment()
+        {
+            var resourceGroupName = GuestConfigurationManagementUtilities.HybridRG;
+            var vmName = GuestConfigurationManagementUtilities.HybridMachineName;
+            GuestConfigurationAssignmentCollection guestConfigurationAssignmentCollection = await GetGuestConfigurationAssignmentCollectionAsync(resourceGroupName, vmName);
+            GuestConfigurationAssignmentData gcAssignmentData = GetDefaultContactGuestConfigurationAssignmentData(guestConfigurationAssignmentCollection.Id);
+
+            try
+            {
+                // Create a new guest configuration assignment
+                ArmOperation<GuestConfigurationAssignmentResource> createAssignmentOperation = await guestConfigurationAssignmentCollection.CreateOrUpdateAsync(WaitUntil.Completed, GuestConfigurationManagementUtilities.DefaultAssignmentName, gcAssignmentData);
+                await createAssignmentOperation.WaitForCompletionAsync();
+                Assert.IsTrue(createAssignmentOperation.HasCompleted);
+                Assert.IsTrue(createAssignmentOperation.HasValue);
+
+                // Get created guest configuration assignment
+                Response<GuestConfigurationAssignmentResource> getGuestAssignmentResponse = await guestConfigurationAssignmentCollection.GetAsync(GuestConfigurationManagementUtilities.DefaultAssignmentName);
+                GuestConfigurationAssignmentResource guestAssignmentResourceRetrieved = getGuestAssignmentResponse.Value;
+                Assert.IsNotNull(guestAssignmentResourceRetrieved);
+                Assert.AreEqual(gcAssignmentData.Location, guestAssignmentResourceRetrieved.Data.Location);
+
+                // Update guest configuration assignment
+                string updatedContext = "Azure Policy Updated";
+                gcAssignmentData.Properties.Context = updatedContext;
+                ArmOperation<GuestConfigurationAssignmentResource> updateAssignmentOperation = await guestConfigurationAssignmentCollection.CreateOrUpdateAsync(WaitUntil.Completed, GuestConfigurationManagementUtilities.DefaultAssignmentName, gcAssignmentData);
+                await updateAssignmentOperation.WaitForCompletionAsync();
+                Assert.IsTrue(updateAssignmentOperation.HasCompleted);
+                Assert.IsTrue(updateAssignmentOperation.HasValue);
+                GuestConfigurationAssignmentResource updatedGuestAssignmentResourceRetrieved = updateAssignmentOperation.Value;
+                Assert.AreEqual(updatedContext, updatedGuestAssignmentResourceRetrieved.Data.Properties.Context);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Neela: " + e);
+            }
+        }
     }
 }
