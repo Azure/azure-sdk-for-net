65b17907c727ff245a67afd5a81a610dde10b442
Mike Harder <mharder@microsoft.com>
2022-07-28 21:57:18 -0700

    Update copy

--- common/Perf/Azure.Test.Perf/NumberFormatter.cs
+++ common/Perf/Azure.Test.Perf/NumberFormatter.cs
@@ -7,12 +7,13 @@ namespace Azure.Test.Perf
 {
     public static class NumberFormatter
     {
-        // Formats a double with the specified minimum number of significant digits.
-        // Digits to the left of the decimal point are never dropped.
+        // Formats a number with a minimum number of significant digits.
+        // Digits to the left of the decimal point are always significant.
         // Examples:
+        // - Format(0, 4) -> "0.000"
         // - Format(12345, 4) -> "12,345"
         // - Format(1.2345, 4) -> "1.235"
-        // - Format(0.00012345, 4) -> "0.0001234"
+        // - Format(0.00012345, 4) -> "0.0001235"
         public static string Format(double value, int minSignificantDigits, bool groupSeparator = true)
         {
             if (minSignificantDigits <= 0)
@@ -20,19 +21,19 @@ namespace Azure.Test.Perf
                 throw new ArgumentException("Must be greater than zero", nameof(minSignificantDigits));
             }
 
-            // Signficant digits are undefined for the number zero, so hardcode to string "0".
+            // Special case since log(0) is undefined
             if (value == 0)
             {
-                return "0";
+                return value.ToString($"N{minSignificantDigits - 1}");
             }
 
             double log = Math.Log10(Math.Abs(value));
-            int significantDigits = (int)Math.Ceiling(Math.Max(log, minSignificantDigits));
+            int significantDigits = (int)Math.Max(Math.Ceiling(log), minSignificantDigits);
 
-            double divisor = Math.Pow(10, Math.Ceiling(log - significantDigits));
+            double divisor = Math.Pow(10, Math.Ceiling(log) - significantDigits);
             double rounded = divisor * Math.Round(value / divisor, MidpointRounding.AwayFromZero);
 
-            int decimals = (int)Math.Ceiling(Math.Max(0, significantDigits - log - 1));
+            int decimals = (int)Math.Max(0, significantDigits - Math.Floor(log) - 1);
 
             return groupSeparator ? rounded.ToString($"N{decimals}") : rounded.ToString($"F{decimals}");
         }
