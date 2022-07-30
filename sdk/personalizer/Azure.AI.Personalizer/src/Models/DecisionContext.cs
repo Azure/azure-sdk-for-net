ba8683bfcd9abfef2454b5f83bdd7f16939e4449
Tejaswi Paruchuri <tparuchuri@microsoft.com>
2022-07-30 11:39:53 -0700

    cleanup

--- sdk/personalizer/Azure.AI.Personalizer/src/Models/DecisionContext.cs
+++ sdk/personalizer/Azure.AI.Personalizer/src/Models/DecisionContext.cs
@@ -1,14 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Collections.Generic;
 using System.Linq;
 using System;
-using Azure;
-using System.Dynamic;
-using Azure.Core;
 
 namespace Azure.AI.Personalizer
 {
