2995bc5d4e698f7f7f9077306dd6c50cdf454368
Franko Morales <fmorales@microsoft.com>
2022-08-02 15:15:34 -0700

    Initial commit

---
+++ sdk/communication/Azure.Communication.CallingServer/tests/CallRecording/CallRecordingLiveTests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Communication.CallingServer.Tests.Infrastructure;
+using NUnit.Framework;
+
+namespace Azure.Communication.CallingServer
+{
+    internal class CallRecordingLiveTests : CallingServerClientLiveTestsBase
+    {
+        public CallRecordingLiveTests(bool isAsync) : base(isAsync)
+        { }
+
+        [Test]
+        public async Task RecordingOperations()
+        {
+            //if (SkipCallingServerInteractionLiveTests)
+            //{
+            //    Assert.Ignore("Skip callingserver interaction live tests flag is on");
+            //}
+
+            CallingServerClient client = CreateInstrumentedCallingServerClientWithConnectionString();
+            bool wasConnected = false;
+
+            try
+            {
+                string callLocatorId = "aHR0cHM6Ly9hcGkuZmxpZ2h0cHJveHkuc2t5cGUuY29tL2FwaS92Mi9jcC9jb252LXVzc2MtMDYuY29udi5za3lwZS5jb20vY29udi9pemF4eVdxOXZFT3FtSE14VktNeUtRP2k9MTYmZT02Mzc5NTA2NzA0OTI2OTM2MTU";
+                string ngrok = "https://6961-2001-4898-80e8-9-b17d-4a04-3083-2cc4.ngrok.io";
+                CallRecording callRecording = client.GetCallRecording();
+                var recordingResponse = await callRecording.StartRecordingAsync(new ServerCallLocator(callLocatorId), new Uri(ngrok)).ConfigureAwait(false);
+                Assert.NotNull(recordingResponse.Value);
+
+                var recordingId = recordingResponse.Value.RecordingId;
+                Assert.NotNull(recordingId);
+                await WaitForOperationCompletion().ConfigureAwait(false);
+
+                //await callRecording.PauseRecordingAsync(recordingId);
+                //await WaitForOperationCompletion().ConfigureAwait(false);
+                //recordingResponse = await callRecording.GetRecordingStateAsync(recordingId).ConfigureAwait(false);
+                //Assert.NotNull(recordingResponse.Value);
+                //Assert.NotNull(recordingResponse.Value.RecordingStatus);
+                //Assert.Equals(recordingResponse.Value.RecordingStatus ?? "", "inactive");
+
+                //await callRecording.ResumeRecordingAsync(recordingId);
+                //await WaitForOperationCompletion().ConfigureAwait(false);
+                //recordingResponse = await callRecording.GetRecordingStateAsync(recordingId).ConfigureAwait(false);
+                //Assert.NotNull(recordingResponse.Value);
+                //Assert.NotNull(recordingResponse.Value.RecordingStatus);
+                //Assert.Equals(recordingResponse.Value.RecordingStatus ?? "", "active");
+
+                await callRecording.StopRecordingAsync(recordingId);
+                await WaitForOperationCompletion().ConfigureAwait(false);
+                recordingResponse = await callRecording.GetRecordingStateAsync(recordingId).ConfigureAwait(false);
+            }
+            catch (RequestFailedException ex)
+            {
+                if (ex.Status == 404 && wasConnected)
+                {
+                    Assert.Pass();
+                }
+                Assert.Fail($"Unexpected Error: {ex}");
+            }
+        }
+    }
+}
---
+++ sdk/communication/Azure.Communication.CallingServer/tests/Infrastructure/CallingServerClientLiveTestsBase.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Communication.Identity;
+using Azure.Core.TestFramework;
+using Azure.Core.TestFramework.Models;
+
+namespace Azure.Communication.CallingServer.Tests.Infrastructure
+{
+    internal class CallingServerClientLiveTestsBase : RecordedTestBase<CallingServerClientTestEnvironment>
+    {
+        private const string URIDomainRegEx = @"https://([^/?]+)";
+
+        public CallingServerClientLiveTestsBase(bool isAsync) : base(isAsync, RecordedTestMode.Record)
+        {
+            SanitizedHeaders.Add("x-ms-content-sha256");
+            SanitizedHeaders.Add("X-FORWARDED-HOST");
+            JsonPathSanitizers.Add("$..id");
+            JsonPathSanitizers.Add("$..rawId");
+            JsonPathSanitizers.Add("$..value");
+            UriRegexSanitizers.Add(new UriRegexSanitizer(URIDomainRegEx, "https://sanitized.skype.com"));
+        }
+
+        public bool SkipCallingServerInteractionLiveTests
+            => TestEnvironment.Mode == RecordedTestMode.Live && Environment.GetEnvironmentVariable("SKIP_CALLINGSERVER_INTERACTION_LIVE_TESTS") == "TRUE";
+
+        /// <summary>
+        /// Creates a <see cref="CallingServerClient" />
+        /// </summary>
+        /// <returns>The instrumented <see cref="CallingServerClient" />.</returns>
+        protected CallingServerClient CreateInstrumentedCallingServerClientWithConnectionString()
+        {
+            //var connectionString = TestEnvironment.LiveTestStaticConnectionString;
+            var connectionString = "endpoint=https://acs-recording-common-e2e.communication.azure.com/;accesskey=bSHJgxhB3/I52CmZkwf3u2ojPNVwEuEyba7SeWbfrKFmYgd7C2eujSD/ArlSBcQzo/dp5ZX7OS2LZ86hIt5UQg==";
+
+            CallingServerClient callingServerClient;
+            //if (TestEnvironment.PMAEndpoint == null || TestEnvironment.PMAEndpoint.Length == 0)
+            //{
+            //    callingServerClient = new CallingServer.CallingServerClient(connectionString, CreateServerCallingClientOptionsWithCorrelationVectorLogs());
+            //}
+            //else
+            //{
+            //    callingServerClient = new CallingServer.CallingServerClient(new Uri(TestEnvironment.PMAEndpoint), connectionString, CreateServerCallingClientOptionsWithCorrelationVectorLogs());
+            //}
+            callingServerClient = new CallingServerClient(new Uri("https://pma-dev-fmorales.plat-dev.skype.net/"), connectionString, CreateServerCallingClientOptionsWithCorrelationVectorLogs());
+            return InstrumentClient(callingServerClient);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="CallingServerClientOptions" />
+        /// </summary>
+        /// <returns>The instrumented <see cref="CallingServerClientOptions" />.</returns>
+        private CallingServerClientOptions CreateServerCallingClientOptionsWithCorrelationVectorLogs()
+        {
+            CallingServerClientOptions callClientOptions = new CallingServerClientOptions();
+            callClientOptions.Diagnostics.LoggedHeaderNames.Add("MS-CV");
+            return InstrumentClientOptions(callClientOptions);
+        }
+
+        protected async Task WaitForOperationCompletion(int milliSeconds = 10000)
+        {
+            if (TestEnvironment.Mode != RecordedTestMode.Playback)
+                await Task.Delay(milliSeconds);
+        }
+
+        protected string GetResourceId()
+        {
+            return TestEnvironment.ResourceIdentifier;
+        }
+
+        /// <summary>
+        /// Creates a <see cref="CommunicationIdentityClient" /> with the connectionstring via environment
+        /// variables and instruments it to make use of the Azure Core Test Framework functionalities.
+        /// </summary>
+        /// <returns>The instrumented <see cref="CommunicationIdentityClient" />.</returns>
+        protected CommunicationIdentityClient CreateInstrumentedCommunicationIdentityClient()
+            => InstrumentClient(
+                new CommunicationIdentityClient(
+                    "endpoint=https://minwoolee-comm2.communication.azure.com/;accesskey=75HV1Ivj4upqENZnOaKcxaJ6tQGbd9pkIEHHuyAefb8tKFGiYxOLjHeRbg4f/9W3vSWKztx+YLmXxrl9mzArfA==",
+                    InstrumentClientOptions(new CommunicationIdentityClientOptions(CommunicationIdentityClientOptions.ServiceVersion.V2021_03_07))));
+
+        protected async Task<CommunicationUserIdentifier> CreateIdentityUserAsync()
+        {
+            CommunicationIdentityClient communicationIdentityClient = CreateInstrumentedCommunicationIdentityClient();
+            return await communicationIdentityClient.CreateUserAsync().ConfigureAwait(false);
+        }
+    }
+}
