27650058e018fecfb9851006bd991043677bdc61
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2022-08-04 13:18:07 -0700

    Add AzureClientsCore overload with enableLogging parameter

--- sdk/extensions/Microsoft.Extensions.Azure/src/AzureClientServiceCollectionExtensions.cs
+++ sdk/extensions/Microsoft.Extensions.Azure/src/AzureClientServiceCollectionExtensions.cs
@@ -17,6 +17,7 @@ namespace Microsoft.Extensions.Azure
     {
         /// <summary>
         /// Adds the <see cref="IAzureClientFactory{TClient}"/> and related services to the <see cref="IServiceCollection"/>.
+        /// Azure SDK logging is enabled once the configured client is created.
         /// </summary>
         /// <param name="collection">The <see cref="IServiceCollection"/>.</param>
         /// <param name="configureClients">An <see cref="AzureClientFactoryBuilder"/> that can be used to configure the client.</param>
@@ -28,12 +29,36 @@ namespace Microsoft.Extensions.Azure
 
         /// <summary>
         /// Adds the minimum essential Azure SDK interop services like <see cref="AzureEventSourceLogForwarder"/> and <see cref="AzureComponentFactory"/> to the specified <see cref="IServiceCollection"/> without registering any client types.
+        /// Azure SDK logging will not be enabled by default, but can be enabled by calling the <see cref="AzureEventSourceLogForwarder.Start"/> method.
+        /// Alternatively, you can use the <see cref="AddAzureClientsCore(Microsoft.Extensions.DependencyInjection.IServiceCollection, bool)"/> overload
+        /// and pass <value>true</value> to enable logging.
         /// </summary>
         /// <param name="collection">The <see cref="IServiceCollection"/>.</param>
         public static void AddAzureClientsCore(this IServiceCollection collection)
         {
+            collection.AddAzureClientsCore(false);
+        }
+
+        /// <summary>
+        /// Adds the minimum essential Azure SDK interop services like <see cref="AzureEventSourceLogForwarder"/> and <see cref="AzureComponentFactory"/> to the specified <see cref="IServiceCollection"/> without registering any client types.
+        /// </summary>
+        /// <param name="collection">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="enableLogging">Whether or not to enable Azure SDK logging. Even if this is set to <value>false</value>, logging can be enabled
+        /// by calling the <see cref="AzureEventSourceLogForwarder.Start"/> method.</param>
+        public static void AddAzureClientsCore(this IServiceCollection collection, bool enableLogging)
+        {
             collection.AddOptions();
-            collection.TryAddSingleton<AzureEventSourceLogForwarder>();
+            collection.TryAddSingleton<AzureEventSourceLogForwarder>(provider =>
+            {
+                var loggerFactory = provider.GetRequiredService<ILoggerFactory>();
+                var forwarder = new AzureEventSourceLogForwarder(loggerFactory);
+                if (enableLogging)
+                {
+                    forwarder.Start();
+                }
+
+                return forwarder;
+            });
             collection.TryAddSingleton<ILoggerFactory, NullLoggerFactory>();
             collection.TryAddSingleton<AzureComponentFactory, AzureComponentFactoryImpl>();
         }
