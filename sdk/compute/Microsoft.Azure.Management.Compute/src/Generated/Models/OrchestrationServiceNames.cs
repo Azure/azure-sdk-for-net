d00c31fc59c387efe612b4a6cd9bf0fdde13fc53
Theodore Chang <theodore.l.chang@gmail.com>
2022-08-05 17:26:33 -0400

    regenerate sdk with proper swagger updates

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/OrchestrationServiceNames.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/OrchestrationServiceNames.cs
@@ -17,5 +17,6 @@ namespace Microsoft.Azure.Management.Compute.Models
     public static class OrchestrationServiceNames
     {
         public const string AutomaticRepairs = "AutomaticRepairs";
+
     }
 }
