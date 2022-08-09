a01d4c6732e6e3ec7aba1534cdfcee07e75f349b
Feng Zhou <fenzhou@microsoft.com>
2022-08-09 13:48:57 +0800

    add tests

---
+++ sdk/containerregistry/Azure.ResourceManager.ContainerRegistry/tests/Scenario/ContainerRegistryOperationTests.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.ResourceManager.ContainerRegistry.Tests
+{
+    public class ContainerRegistryOperationTests: ContainerRegistryManagementTestBase
+    {
+        public ContainerRegistryOperationTests(bool isAsync)
+            : base(isAsync)
+        {
+        }
+    }
+}
