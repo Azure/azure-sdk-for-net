f5dec229625f1e6a8dcbdcac8dd9ed4f1f0b4eed
Minnie Liu <peiliu@microsoft.com>
2022-08-10 16:00:55 -0700

    Adding new test recordings and serializing participantInput Id

--- sdk/communication/Azure.Communication.MediaComposition/src/Generated/Models/ParticipantInput.cs
+++ sdk/communication/Azure.Communication.MediaComposition/src/Generated/Models/ParticipantInput.cs
@@ -14,18 +14,6 @@ namespace Azure.Communication.MediaComposition.Models
     /// <summary> The Participant. </summary>
     public partial class ParticipantInput : MediaInput
     {
-
-        /// <summary> Initializes a new instance of ParticipantInput. </summary>
-        /// <param name="kind"> Kind of media input. </param>
-        /// <param name="placeholderImageUri"> Image url to be used if participant has no video stream. </param>
-        /// <param name="serviceId"> Identifies a participant in Azure Communication services. A participant is, for example, a phone number or an Azure communication user. This model is polymorphic: Apart from kind and rawId, at most one further property may be set which must match the kind enum value. </param>
-        /// <param name="call"> The id of the teams meeting or call. </param>
-        internal ParticipantInput(MediaInputType kind, string placeholderImageUri, CommunicationIdentifierModel serviceId, string call) : base(kind, placeholderImageUri)
-        {
-            ServiceId = serviceId;
-            Call = call;
-            Kind = kind;
-        }
         /// <summary> The id of the teams meeting or call. </summary>
         public string Call { get; set; }
     }
--- sdk/communication/Azure.Communication.MediaComposition/src/Models/ParticipantInput.cs
+++ sdk/communication/Azure.Communication.MediaComposition/src/Models/ParticipantInput.cs
@@ -16,20 +16,16 @@ namespace Azure.Communication.MediaComposition.Models
         /// <summary> The CommunicationIdentifier that identifies the participant.</summary>
         public CommunicationIdentifier Id { get; private set; }
 
-        internal ParticipantInput(CommunicationIdentifierModel serviceId, string call)
+        /// <summary> Initializes a new instance of ParticipantInput. </summary>
+        /// <param name="kind"> Kind of media input. </param>
+        /// <param name="placeholderImageUri"> Image url to be used if participant has no video stream. </param>
+        /// <param name="serviceId"> Identifies a participant in Azure Communication services. A participant is, for example, a phone number or an Azure communication user. This model is polymorphic: Apart from kind and rawId, at most one further property may be set which must match the kind enum value. </param>
+        /// <param name="call"> The id of the teams meeting or call. </param>
+        internal ParticipantInput(MediaInputType kind, string placeholderImageUri, CommunicationIdentifierModel serviceId, string call) : base(kind, placeholderImageUri)
         {
-            if (serviceId == null)
-            {
-                throw new ArgumentNullException(nameof(serviceId));
-            }
-            if (call == null)
-            {
-                throw new ArgumentNullException(nameof(call));
-            }
-
             ServiceId = serviceId;
             Call = call;
-            Kind = MediaInputType.Participant;
+            Kind = kind;
             Id = CommunicationIdentifierSerializer.Deserialize(serviceId);
         }
 
@@ -38,6 +34,20 @@ namespace Azure.Communication.MediaComposition.Models
         /// <param name="call"> The id of the teams meeting or call. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="id"/> or <paramref name="call"/> is null. </exception>
         public ParticipantInput(CommunicationIdentifier id, string call)
-            : this(CommunicationIdentifierSerializer.Serialize(id), call) { }
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            if (call == null)
+            {
+                throw new ArgumentNullException(nameof(call));
+            }
+
+            ServiceId = CommunicationIdentifierSerializer.Serialize(id);
+            Call = call;
+            Kind = MediaInputType.Participant;
+            Id = id;
+        }
     }
 }
