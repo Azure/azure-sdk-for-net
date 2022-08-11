d75e8948ee322bfffe42d282cb89bb2ed6d7c8ac
Minnie Liu <peiliu@microsoft.com>
2022-08-11 12:21:53 -0700

    Fixing broken pipeline tests

--- sdk/communication/Azure.Communication.MediaComposition/src/Models/ParticipantInput.cs
+++ sdk/communication/Azure.Communication.MediaComposition/src/Models/ParticipantInput.cs
@@ -53,7 +53,7 @@ namespace Azure.Communication.MediaComposition.Models
         /// <exception cref="ArgumentNullException"> <paramref name="id"/> or <paramref name="call"/> is null. </exception>
         public ParticipantInput(CommunicationIdentifier id, string call)
         {
-            if (id == null)
+            if (((object)id) == null)
             {
                 throw new ArgumentNullException(nameof(id));
             }
