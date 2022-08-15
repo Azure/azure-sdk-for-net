d1d680eaaeee0c7b4bba9ddf33894398525bdcfe
Vishwesh Bankwar <vishweshbankwar@users.noreply.github.com>
2022-08-15 15:58:19 -0700

    Update sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/Statsbeat/Statsbeat.cs
    Co-authored-by: Rajkumar Rangaraj <rajrang@microsoft.com>

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/Statsbeat/Statsbeat.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/Statsbeat/Statsbeat.cs
@@ -26,7 +26,7 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Internals
 
         internal static string s_runtimeVersion => SdkVersionUtils.GetVersion(typeof(object));
 
-        internal static string s_sdkVersion = SdkVersionUtils.GetVersion(typeof(AzureMonitorTraceExporter));
+        internal static string s_sdkVersion => SdkVersionUtils.GetVersion(typeof(AzureMonitorTraceExporter));
 
         static Statsbeat()
         {
