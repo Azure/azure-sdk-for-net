d2c5b1079a68f555ef3563e165619326849bc17b
andrew <andrsmith@microsoft.com>
2022-08-15 14:58:19 -0700

    Add VerifyConfigurationProfileProperties() method

--- sdk/automanage/Azure.ResourceManager.Automanage/tests/automanageManagementTestBase.cs
+++ sdk/automanage/Azure.ResourceManager.Automanage/tests/automanageManagementTestBase.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.TestFramework;
 using NUnit.Framework;
-using System.Threading.Tasks;
 
 namespace Azure.ResourceManager.Automanage.Tests
 {
@@ -36,6 +36,17 @@ namespace Azure.ResourceManager.Automanage.Tests
             ResourceGroupData input = new ResourceGroupData(location);
             var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
             return lro.Value;
+        }
+
+        public static void VerifyConfigurationProfileProperties(ConfigurationProfileResource profile)
+        {
+            Assert.NotNull(profile);
+            Assert.True(profile.HasData);
+            Assert.NotNull(profile.Id);
+            Assert.NotNull(profile.Id.Name);
+            Assert.NotNull(profile.Data);
+            Assert.NotNull(profile.Data.Configuration);
+            Assert.NotNull(profile.Data.Location);
         }
     }
 }
