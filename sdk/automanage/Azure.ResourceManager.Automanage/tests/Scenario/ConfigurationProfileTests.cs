31adccaf76b037f99436c904c239ef82c3fccc58
andrew <andrsmith@microsoft.com>
2022-08-16 14:44:18 -0700

    Simplify CanGetAndCreateConfigurationProfile()

--- sdk/automanage/Azure.ResourceManager.Automanage/tests/Scenario/ConfigurationProfileTests.cs
+++ sdk/automanage/Azure.ResourceManager.Automanage/tests/Scenario/ConfigurationProfileTests.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
@@ -11,8 +10,14 @@ namespace Azure.ResourceManager.Automanage.Tests.Scenario
     {
         public ConfigurationProfileTests(bool async) : base(async) { }
 
+        //[TestCase]
+        //public async Task CanDeleteConfigurationProfile()
+        //{
+
+        //}
+
         [TestCase]
-        public async Task CanGetConfigurationProfile()
+        public async Task CanGetAndCreateConfigurationProfile()
         {
             string profileName = Recording.GenerateAssetName("SDKAutomanageProfile-");
 
@@ -22,29 +27,8 @@ namespace Azure.ResourceManager.Automanage.Tests.Scenario
             // fetch configuration profile collection
             var collection = rg.GetConfigurationProfiles();
 
-            // create configruation profile
-            string configuration = "{" +
-                "\"Antimalware/Enable\":true," +
-                "\"Antimalware/EnableRealTimeProtection\":true," +
-                "\"Antimalware/RunScheduledScan\":true," +
-                "\"Backup/Enable\":true," +
-                "\"WindowsAdminCenter/Enable\":false," +
-                "\"VMInsights/Enable\":true," +
-                "\"AzureSecurityCenter/Enable\":true," +
-                "\"UpdateManagement/Enable\":true," +
-                "\"ChangeTrackingAndInventory/Enable\":true," +
-                "\"GuestConfiguration/Enable\":true," +
-                "\"AutomationAccount/Enable\":true," +
-                "\"LogAnalytics/Enable\":true," +
-                "\"BootDiagnostics/Enable\":true" +
-                "}";
-
-            ConfigurationProfileData data = new ConfigurationProfileData(DefaultLocation)
-            {
-                Configuration = new BinaryData(configuration)
-            };
-
-            await collection.CreateOrUpdateAsync(WaitUntil.Completed, profileName, data);
+            // create configuration profile
+            await CreateConfigurationProfile(collection, profileName);
 
             // fetch new configuration profile
             var profile = await collection.GetAsync(profileName);
