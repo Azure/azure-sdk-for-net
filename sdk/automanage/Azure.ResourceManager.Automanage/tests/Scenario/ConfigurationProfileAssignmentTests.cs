b0fafc91e8317a3a9edcd6609fca75404717ab1c
andrew <andrsmith@microsoft.com>
2022-08-17 11:02:12 -0700

    Add CanCreateCustomProfileAssignment() test

---
+++ sdk/automanage/Azure.ResourceManager.Automanage/tests/Scenario/ConfigurationProfileAssignmentTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Automanage.Tests.Scenario
+{
+    internal class ConfigurationProfileAssignmentTests : AutomanageTestBase
+    {
+        public ConfigurationProfileAssignmentTests(bool async) : base(async) { }
+
+        [TestCase]
+        public async Task CanCreateCustomProfileAssignment()
+        {
+            string profileName = Recording.GenerateAssetName("SDKAutomanageProfile-");
+
+            // create resource group
+            var rg = await CreateResourceGroup(Subscription, "SDKAutomanage-", DefaultLocation);
+
+            // fetch configuration profile and assignments collections
+            var profileCollection = rg.GetConfigurationProfiles();
+            var assignmentCollection = rg.GetConfigurationProfileAssignments();
+
+            // create configuration profile
+            await CreateConfigurationProfile(profileCollection, profileName);
+
+            // create VM
+
+            // create assignment between profile and VM
+        }
+    }
+}
