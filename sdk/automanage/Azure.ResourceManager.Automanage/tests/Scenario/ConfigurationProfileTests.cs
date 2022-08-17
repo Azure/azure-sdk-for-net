d536003d7f56af682f1e76b85d3437a3455fdf2a
andrew <andrsmith@microsoft.com>
2022-08-17 10:00:10 -0700

    Add CanCreateConfigurationProfile() test

--- sdk/automanage/Azure.ResourceManager.Automanage/tests/Scenario/ConfigurationProfileTests.cs
+++ sdk/automanage/Azure.ResourceManager.Automanage/tests/Scenario/ConfigurationProfileTests.cs
@@ -11,6 +11,52 @@ namespace Azure.ResourceManager.Automanage.Tests.Scenario
         public ConfigurationProfileTests(bool async) : base(async) { }
 
         [TestCase]
+        public async Task CanCreateConfigurationProfile()
+        {
+            string profileName = Recording.GenerateAssetName("SDKAutomanageProfile-");
+
+            // create resource group
+            var rg = await CreateResourceGroup(Subscription, "SDKAutomanage-", DefaultLocation);
+
+            // fetch configuration profile collection
+            var collection = rg.GetConfigurationProfiles();
+
+            // create configuration profile and resync the collection
+            await CreateConfigurationProfile(collection, profileName);
+
+            // fetch all profiles and count them
+            var profiles = collection.GetAllAsync().ConfigureAwait(false);
+
+            int count = 0;
+            await foreach (var item in profiles)
+                count++;
+
+            //assert
+            Assert.AreEqual(1, count);
+        }
+
+        [TestCase]
+        public async Task CanGetConfigurationProfile()
+        {
+            string profileName = Recording.GenerateAssetName("SDKAutomanageProfile-");
+
+            // create resource group
+            var rg = await CreateResourceGroup(Subscription, "SDKAutomanage-", DefaultLocation);
+
+            // fetch configuration profile collection
+            var collection = rg.GetConfigurationProfiles();
+
+            // create configuration profile
+            await CreateConfigurationProfile(collection, profileName);
+
+            // fetch new configuration profile
+            var profile = await collection.GetAsync(profileName);
+
+            // assert
+            VerifyConfigurationProfileProperties(profile);
+        }
+
+        [TestCase]
         public async Task CanDeleteConfigurationProfile()
         {
             string profileName = Recording.GenerateAssetName("SDKAutomanageProfile-");
@@ -33,27 +79,6 @@ namespace Azure.ResourceManager.Automanage.Tests.Scenario
 
             // assert
             Assert.False(exists);
-        }
-
-        [TestCase]
-        public async Task CanGetConfigurationProfile()
-        {
-            string profileName = Recording.GenerateAssetName("SDKAutomanageProfile-");
-
-            // create resource group
-            var rg = await CreateResourceGroup(Subscription, "SDKAutomanage-", DefaultLocation);
-
-            // fetch configuration profile collection
-            var collection = rg.GetConfigurationProfiles();
-
-            // create configuration profile
-            await CreateConfigurationProfile(collection, profileName);
-
-            // fetch new configuration profile
-            var profile = await collection.GetAsync(profileName);
-
-            // assert
-            VerifyConfigurationProfileProperties(profile);
         }
     }
 }
