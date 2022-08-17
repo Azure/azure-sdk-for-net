35e26ee61ccee00e20da9e909977d4f3dbaa454e
Chengming Ruan <v-cruan@microsoft.com>
2022-08-17 16:25:06 +0800

    Media Transform

---
+++ sdk/mediaservices/Azure.ResourceManager.Media/tests/Scenario/LiveOutputTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Media.Models;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Media.Tests
+{
+    public class LiveOutputTests : MediaManagementTestBase
+    {
+        private ResourceIdentifier _mediaServiceIdentifier;
+        private LiveEventResource _liveEventResource;
+
+        private LiveOutputCollection liveOutputCollection => _liveEventResource.GetLiveOutputs();
+
+        public LiveOutputTests(bool isAsync) : base(isAsync)
+        {
+        }
+
+        [OneTimeSetUp]
+        public async Task GlobalSetup()
+        {
+            var rgLro = await (await GlobalClient.GetDefaultSubscriptionAsync()).GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Started, SessionRecording.GenerateAssetName(ResourceGroupNamePrefix), new ResourceGroupData(AzureLocation.WestUS2));
+            var storage = await CreateStorageAccount(rgLro.Value, SessionRecording.GenerateAssetName(StorageAccountNamePrefix));
+            var mediaService = await CreateMediaService(rgLro.Value, SessionRecording.GenerateAssetName("mediaservice"), storage.Id);
+            _mediaServiceIdentifier = mediaService.Id;
+            await StopSessionRecordingAsync();
+        }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            _liveEventResource = await Client.GetLiveEventResource(_mediaServiceIdentifier).GetAsync();
+        }
+
+        [Test]
+        [RecordedTest]
+        [Ignore("block")]
+        public async Task GetAll()
+        {
+            var list = await liveOutputCollection.GetAllAsync().ToEnumerableAsync();
+            Assert.IsEmpty(list);
+        }
+    }
+}
