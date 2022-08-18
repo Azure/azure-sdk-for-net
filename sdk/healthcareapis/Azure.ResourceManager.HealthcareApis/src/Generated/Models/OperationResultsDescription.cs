e35f4e70ab190016fcafb9a99db29e48774e0751
Arthur Ma <arthurma@microsoft.com>
2022-08-18 21:38:28 +0800

    Update

--- sdk/healthcareapis/Azure.ResourceManager.HealthcareApis/src/Generated/Models/OperationResultsDescription.cs
+++ sdk/healthcareapis/Azure.ResourceManager.HealthcareApis/src/Generated/Models/OperationResultsDescription.cs
@@ -44,7 +44,36 @@ namespace Azure.ResourceManager.HealthcareApis.Models
         public string StartTime { get; }
         /// <summary> The time that the operation finished. </summary>
         public string EndTime { get; }
-        /// <summary> Additional properties of the operation result. </summary>
+        /// <summary>
+        /// Additional properties of the operation result..
+        /// <para>
+        /// To assign an object to this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formated json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
         public BinaryData Properties { get; }
     }
 }
