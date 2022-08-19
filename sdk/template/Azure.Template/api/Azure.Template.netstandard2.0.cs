ab2ed9c69d69fce3bd91322504c8855aeb5efcf6
Anne Thompson <annelo@microsoft.com>
2022-08-19 16:15:56 -0700

    multiversion DPG client, operation added

--- sdk/template/Azure.Template/api/Azure.Template.netstandard2.0.cs
+++ sdk/template/Azure.Template/api/Azure.Template.netstandard2.0.cs
@@ -1,9 +1,22 @@
-namespace Azure.Template.Models
+namespace Azure.Template
 {
-    public partial class InputModel
+    public partial class MultiVersionClient
     {
-        public InputModel(string requiredString, int requiredInt) { }
-        public int RequiredInt { get { throw null; } }
-        public string RequiredString { get { throw null; } }
+        protected MultiVersionClient() { }
+        public MultiVersionClient(System.Uri endpoint, Azure.Core.TokenCredential credential) { }
+        public MultiVersionClient(System.Uri endpoint, Azure.Core.TokenCredential credential, Azure.Template.MultiVersionClientOptions options) { }
+        public virtual Azure.Core.Pipeline.HttpPipeline Pipeline { get { throw null; } }
+        public virtual Azure.Response ServiceV1Operation(Azure.RequestContext context = null) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response> ServiceV1OperationAsync(Azure.RequestContext context = null) { throw null; }
+        public virtual Azure.Response ServiceV2Operation(Azure.RequestContext context = null) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response> ServiceV2OperationAsync(Azure.RequestContext context = null) { throw null; }
+    }
+    public partial class MultiVersionClientOptions : Azure.Core.ClientOptions
+    {
+        public MultiVersionClientOptions(Azure.Template.MultiVersionClientOptions.ServiceVersion version = Azure.Template.MultiVersionClientOptions.ServiceVersion.V2022_02_02) { }
+        public enum ServiceVersion
+        {
+            V2022_02_02 = 1,
+        }
     }
 }
