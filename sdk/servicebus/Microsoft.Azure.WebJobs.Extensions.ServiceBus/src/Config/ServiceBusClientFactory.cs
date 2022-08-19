d4cb19b39587c1031038d997003df4ff800a8f19
Vincent Chiang <vchiang@microsoft.com>
2022-08-19 11:47:37 -0700

    make createclientfromsetting public

--- sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/src/Config/ServiceBusClientFactory.cs
+++ sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/src/Config/ServiceBusClientFactory.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.ServiceBus.Config
             logForwarder.Start();
         }
 
-        internal ServiceBusClient CreateClientFromSetting(string connection)
+        public ServiceBusClient CreateClientFromSetting(string connection)
         {
             var connectionInfo = ResolveConnectionInformation(connection);
 
