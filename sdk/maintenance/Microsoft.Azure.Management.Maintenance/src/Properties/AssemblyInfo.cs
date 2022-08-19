6da1a075a04f20d47c5e12dfd007b7053d133204
Paul Selvaraj <paulse@microsoft.com>
2022-08-19 07:08:07 -0700

    Updated the SDK Metadata

--- sdk/maintenance/Microsoft.Azure.Management.Maintenance/src/Properties/AssemblyInfo.cs
+++ sdk/maintenance/Microsoft.Azure.Management.Maintenance/src/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
