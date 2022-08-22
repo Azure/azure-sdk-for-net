f58ab18c2517e5e4f41ad79f6da123a58fa4f58c
Anne Thompson <annelo@microsoft.com>
2022-08-22 12:09:39 -0700

    export api

--- sdk/template/Azure.Template/api/Azure.Template.netstandard2.0.cs
+++ sdk/template/Azure.Template/api/Azure.Template.netstandard2.0.cs
@@ -7,19 +7,19 @@ namespace Azure.Template
         public MultiVersionClient(System.Uri endpoint, Azure.Core.TokenCredential credential, Azure.Template.MultiVersionClientOptions options) { }
         public virtual Azure.Core.Pipeline.HttpPipeline Pipeline { get { throw null; } }
         public virtual Azure.Response SetEvolvingModel(Azure.Core.RequestContent content, Azure.RequestContext context = null) { throw null; }
+        public virtual Azure.Response<Azure.Template.EvolvingModel> SetEvolvingModel(Azure.Template.EvolvingModel value, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> SetEvolvingModelAsync(Azure.Core.RequestContent content, Azure.RequestContext context = null) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response<Azure.Template.EvolvingModel>> SetEvolvingModelAsync(Azure.Template.EvolvingModel value, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
     }
     public partial class MultiVersionClientOptions : Azure.Core.ClientOptions
     {
         public MultiVersionClientOptions(Azure.Template.MultiVersionClientOptions.ServiceVersion version = Azure.Template.MultiVersionClientOptions.ServiceVersion.V2022_02_02) { }
         public enum ServiceVersion
         {
-            V2022_02_02 = 1,
+            V2022_01_01 = 1,
+            V2022_02_02 = 2,
         }
     }
-}
-namespace Azure.Template.Models
-{
     public partial class EvolvingModel
     {
         public EvolvingModel(int requiredInt, string requiredString) { }
@@ -28,4 +28,5 @@ namespace Azure.Template.Models
         public int RequiredInt { get { throw null; } set { } }
         public string RequiredString { get { throw null; } set { } }
     }
+
 }
