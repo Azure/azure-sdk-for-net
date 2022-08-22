0f1bcdd9f6f1915ef4ea9745b60c15fc0526ab02
Arthur Ma <arthurma@microsoft.com>
2022-08-22 17:35:18 +0800

    Upgrade Authorization to latest swagger spec (#30632)

--- sdk/authorization/Azure.ResourceManager.Authorization/src/Generated/Models/RoleAssignmentCreateOrUpdateContent.Serialization.cs
+++ sdk/authorization/Azure.ResourceManager.Authorization/src/Generated/Models/RoleAssignmentCreateOrUpdateContent.Serialization.cs
@@ -16,7 +16,37 @@ namespace Azure.ResourceManager.Authorization.Models
         {
             writer.WriteStartObject();
             writer.WritePropertyName("properties");
-            writer.WriteObjectValue(Properties);
+            writer.WriteStartObject();
+            writer.WritePropertyName("roleDefinitionId");
+            writer.WriteStringValue(RoleDefinitionId);
+            writer.WritePropertyName("principalId");
+            writer.WriteStringValue(PrincipalId);
+            if (Optional.IsDefined(PrincipalType))
+            {
+                writer.WritePropertyName("principalType");
+                writer.WriteStringValue(PrincipalType.Value.ToString());
+            }
+            if (Optional.IsDefined(Description))
+            {
+                writer.WritePropertyName("description");
+                writer.WriteStringValue(Description);
+            }
+            if (Optional.IsDefined(Condition))
+            {
+                writer.WritePropertyName("condition");
+                writer.WriteStringValue(Condition);
+            }
+            if (Optional.IsDefined(ConditionVersion))
+            {
+                writer.WritePropertyName("conditionVersion");
+                writer.WriteStringValue(ConditionVersion);
+            }
+            if (Optional.IsDefined(DelegatedManagedIdentityResourceId))
+            {
+                writer.WritePropertyName("delegatedManagedIdentityResourceId");
+                writer.WriteStringValue(DelegatedManagedIdentityResourceId);
+            }
+            writer.WriteEndObject();
             writer.WriteEndObject();
         }
     }
