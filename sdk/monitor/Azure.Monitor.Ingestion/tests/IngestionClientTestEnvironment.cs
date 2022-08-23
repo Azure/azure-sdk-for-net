57f0664d476befeaea651fdd714f65580e0200ec
Nisha Bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2022-08-23 01:13:57 -0700

    Update IngestionClientTestEnvironment.cs

--- sdk/monitor/Azure.Monitor.Ingestion/tests/IngestionClientTestEnvironment.cs
+++ sdk/monitor/Azure.Monitor.Ingestion/tests/IngestionClientTestEnvironment.cs
@@ -9,15 +9,15 @@ namespace Azure.Monitor.Ingestion.Tests
 {
     public class IngestionClientTestEnvironment : TestEnvironment
     {
-        public string DCRImmutableId => GetRecordedVariable("LOGS_DCR_RULE_ID");
+        public string DCRImmutableId => GetRecordedVariable("AZURE_MONITOR_INGESTION_LOGS_DCR_RULE_ID");
 
-        public string DCREndpoint => GetRecordedVariable("DATA_COLLECTION_ENDPOINT");
+        public string DCREndpoint => GetRecordedVariable("AZURE_MONITOR_INGESTION_DATA_COLLECTION_ENDPOINT");
 
-        public string Ingestion_WorkspaceId => GetRecordedVariable("INGESTION_WORKSPACE_ID");
+        public string Ingestion_WorkspaceId => GetRecordedVariable("AZURE_MONITOR_INGESTION_INGESTION_WORKSPACE_ID");
 
-        public string StreamName => GetRecordedVariable("LOGS_DCR_STREAM_NAME");
+        public string StreamName => GetRecordedVariable("AZURE_MONITOR_INGESTION_LOGS_DCR_STREAM_NAME");
 
-        public string TableName => GetRecordedVariable("LOGS_TABLE_NAME");
+        public string TableName => GetRecordedVariable("AZURE_MONITOR_INGESTION_LOGS_TABLE_NAME");
 
         public ClientSecretCredential ClientSecretCredential => new ClientSecretCredential(GetRecordedVariable("AZURE_TENANT_ID"), GetRecordedVariable("AZURE_CLIENT_ID"), GetRecordedVariable("AZURE_CLIENT_SECRET"));
     }
