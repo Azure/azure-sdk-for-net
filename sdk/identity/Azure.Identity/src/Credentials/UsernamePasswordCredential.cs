d7c22f4bb0a9b66e519b643753f04d905776f9fa
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2022-08-23 15:04:56 -0500

    Update XML docs for Identity credential types (#30668)
    * Update XML docs for Identity credential types
    
    * React to feedback

--- sdk/identity/Azure.Identity/src/Credentials/UsernamePasswordCredential.cs
+++ sdk/identity/Azure.Identity/src/Credentials/UsernamePasswordCredential.cs
@@ -145,9 +145,10 @@ namespace Azure.Identity
         }
 
         /// <summary>
-        /// Obtains a token for a user account, authenticating them using the given username and password.  Note: This will fail with
-        /// an <see cref="AuthenticationFailedException"/> if the specified user account has MFA enabled. This method is called automatically by Azure SDK client libraries.
-        /// You may call this method directly, but you must also handle token caching and token refreshing.
+        /// Obtains a token for a user account, authenticating them using the given username and password. Note: This will fail with an
+        /// <see cref="AuthenticationFailedException"/> if the specified user account has MFA enabled. Acquired tokens are cached by the
+        /// credential instance. Token lifetime and refreshing is handled automatically. Where possible, reuse credential instances to
+        /// optimize cache effectiveness.
         /// </summary>
         /// <param name="requestContext">The details of the authentication request.</param>
         /// <param name="cancellationToken">A <see cref="CancellationToken"/> controlling the request lifetime.</param>
@@ -158,9 +159,10 @@ namespace Azure.Identity
         }
 
         /// <summary>
-        /// Obtains a token for a user account, authenticating them using the given username and password.  Note: This will fail with
-        /// an <see cref="AuthenticationFailedException"/> if the specified user account has MFA enabled. This method is called automatically by Azure SDK client libraries.
-        /// You may call this method directly, but you must also handle token caching and token refreshing.
+        /// Obtains a token for a user account, authenticating them using the given username and password. Note: This will fail with an
+        /// <see cref="AuthenticationFailedException"/> if the specified user account has MFA enabled. Acquired tokens are cached by the
+        /// credential instance. Token lifetime and refreshing is handled automatically. Where possible, reuse credential instances to
+        /// optimize cache effectiveness.
         /// </summary>
         /// <param name="requestContext">The details of the authentication request.</param>
         /// <param name="cancellationToken">A <see cref="CancellationToken"/> controlling the request lifetime.</param>
