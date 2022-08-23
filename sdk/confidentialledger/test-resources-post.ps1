7d8b5d4b48b5687dee79418f4e98f1934912dd25
Christopher Scott <chriscott@hotmail.com>
2022-08-23 16:41:08 -0500

    Update test-resources-post.ps1

--- sdk/confidentialledger/test-resources-post.ps1
+++ sdk/confidentialledger/test-resources-post.ps1
@@ -1,3 +1,3 @@
 # Confidential Ledger resources can take a few minutes to resolve in DNS after being newly provisioned.
 Write-Host 'Waiting to let the new resource propagate to DNS'
-Start-Sleep -Seconds 240
+Start-Sleep -Seconds 360
