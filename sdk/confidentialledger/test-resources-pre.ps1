3b677da0d6f33ccf21bd419c48d3fccb0daecc76
Christopher Scott <chriss@microsoft.com>
2022-08-24 18:22:52 -0500

    fix multi-line PEMs

--- sdk/confidentialledger/test-resources-pre.ps1
+++ sdk/confidentialledger/test-resources-pre.ps1
@@ -1,5 +1,9 @@
 Import-Module -Name ./eng/scripts/X509Certificate2 -Verbose
 
 $cert = New-X509Certificate2 -SubjectName 'E=opensource@microsoft.com, CN=Azure SDK, OU=Azure SDK, O=Microsoft, L=Frisco, S=TX, C=US' -ValidDays 3652
-$templateFileParameters['ConfidentialLedgerPrincipalPEM'] = Format-X509Certificate2 -Certificate $cert
-$templateFileParameters['ConfidentialLedgerPrincipalPEMPK'] = Format-X509Certificate2 -Type Pkcs8 -Certificate $cert
+
+$pem = Format-X509Certificate2 -Certificate $cert
+$templateFileParameters['ConfidentialLedgerPrincipalPEM'] = $pem -replace "\\r\\n", ""
+
+$pemPk = Format-X509Certificate2 -Type Pkcs8 -Certificate $cert
+$templateFileParameters['ConfidentialLedgerPrincipalPEMPK'] = $pemPk -replace "\\r\\n", ""
