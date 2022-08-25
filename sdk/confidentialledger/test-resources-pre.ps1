0c619ee83affe46682e0174ab1eb191487955206
Christopher Scott <chriss@microsoft.com>
2022-08-25 10:42:20 -0500

    try again

--- sdk/confidentialledger/test-resources-pre.ps1
+++ sdk/confidentialledger/test-resources-pre.ps1
@@ -1,10 +1,10 @@
 Import-Module -Name ./eng/scripts/X509Certificate2 -Verbose
 
-$cert = New-X509Certificate2 -SubjectName 'E=opensource@microsoft.com, CN=Azure SDK, OU=Azure SDK, O=Microsoft, L=Frisco, S=TX, C=US' -ValidDays 3652
+$cert = New-X509Certificate2 -SubjectName 'E=opensource@microsoft.com, CN=Azure SDK, OU=Azure SDK, O=Microsoft, L=Frisco, S=TX, C=US' -ValidDays 365
 
-$pem = Format-X509Certificate2 -Certificate $cert
-$pem = $pem.Replace("`n", "")
-$templateFileParameters['ConfidentialLedgerPrincipalPEM'] = $pem -replace "\\r\\n", ""
-$pemPk = Format-X509Certificate2 -Type Pkcs8 -Certificate $cert
-$pemPk = $pemPk.Replace("`n", "")
-$templateFileParameters['ConfidentialLedgerPrincipalPEMPK'] = $pemPk -replace "\\r\\n", ""
+$pem = (Format-X509Certificate2 -Certificate $cert)
+$pem = [string]::join("",($pem.Split("`n")))
+$templateFileParameters['ConfidentialLedgerPrincipalPEM'] = $pem
+$pemPk = (Format-X509Certificate2 -Type Pkcs8 -Certificate $cert)
+$pemPk = [string]::join("",($pemPk.Split("`n")))
+$templateFileParameters['ConfidentialLedgerPrincipalPEMPK'] = $pemPk
