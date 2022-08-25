3fe0da540cb6d975df23f9813334ba85d63c2d35
Christopher Scott <chriss@microsoft.com>
2022-08-25 09:51:46 -0500

    more newline removal

--- sdk/confidentialledger/test-resources-pre.ps1
+++ sdk/confidentialledger/test-resources-pre.ps1
@@ -3,7 +3,8 @@ Import-Module -Name ./eng/scripts/X509Certificate2 -Verbose
 $cert = New-X509Certificate2 -SubjectName 'E=opensource@microsoft.com, CN=Azure SDK, OU=Azure SDK, O=Microsoft, L=Frisco, S=TX, C=US' -ValidDays 3652
 
 $pem = Format-X509Certificate2 -Certificate $cert
+$pem = $pem.Replace("`n", "")
 $templateFileParameters['ConfidentialLedgerPrincipalPEM'] = $pem -replace "\\r\\n", ""
-
 $pemPk = Format-X509Certificate2 -Type Pkcs8 -Certificate $cert
+$pemPk = $pemPk.Replace("`n", "")
 $templateFileParameters['ConfidentialLedgerPrincipalPEMPK'] = $pemPk -replace "\\r\\n", ""
