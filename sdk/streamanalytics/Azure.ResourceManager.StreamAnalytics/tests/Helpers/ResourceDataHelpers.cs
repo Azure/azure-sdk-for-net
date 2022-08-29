f0860f23c073cc7600a0dce3d97979a8bc854728
mcgallan <1186003288@qq.com>
2022-08-29 17:17:29 +0800

    Update ResourceDataHelpers.cs

--- sdk/streamanalytics/Azure.ResourceManager.StreamAnalytics/tests/Helpers/ResourceDataHelpers.cs
+++ sdk/streamanalytics/Azure.ResourceManager.StreamAnalytics/tests/Helpers/ResourceDataHelpers.cs
@@ -15,9 +15,7 @@ namespace Azure.ResourceManager.StreamAnalytics.Tests.Helpers
 {
     public static class ResourceDataHelpers
     {
-        private const string dummySSHKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+wWK73dCr+jgQOAxNsHAnNNNMEMWOHYEccp6wJm2gotpr9katuF/ZAdou5AaW1C61slRkHRkpRRX9FA9CYBiitZgvCCz+3nWNN7l/Up54Zps/pHWGZLHNJZRYyAB6j5yVLMVHIHriY49d/GZTZVNB8GoJv9Gakwc/fuEZYYl4YDFiGMBP///TzlI4jhiJzjKnEvqPFki5p2ZRJqcbCiF4pJrxUQR/RXqVFQdbRLZgYfJ8xGB878RENq3yQ39d8dVOkq4edbkzwcUmwwwkYVPIoDGsYLaRHnG+To7FvMeyO7xDVQkMKzopTQV8AuKpyvpqu0a9pWOMaiCyDytO7GGN you@me.com";
-
-        // Temporary solution since the one in Azure.ResourceManager.CognitiveServices is internal
+        // Temporary solution since the one in Azure.ResourceManager.StreamAnalytics is internal
         public static IDictionary<string, string> ReplaceWith(this IDictionary<string, string> dest, IDictionary<string, string> src)
         {
             dest.Clear();
