9b7d91c4b6bf9937fa0397fa226469e41e0fdc29
Min Woo Lee 🧊 <77083090+minwoolee-ms@users.noreply.github.com>
2022-09-02 15:44:35 -0700

    Updated with unique id generation Updated to cleanup process for service bus after test

--- sdk/communication/Azure.Communication.CallingServer/tests/Infrastructure/CallAutomationClientAutomatedLiveTestsBase.cs
+++ sdk/communication/Azure.Communication.CallingServer/tests/Infrastructure/CallAutomationClientAutomatedLiveTestsBase.cs
@@ -7,13 +7,19 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Sockets;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
 using Azure.Communication.Identity;
 using Azure.Core.TestFramework;
 using Azure.Core.TestFramework.Models;
 using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Formatting = Newtonsoft.Json.Formatting;
 
 namespace Azure.Communication.CallingServer
 {
@@ -46,13 +52,20 @@ namespace Azure.Communication.CallingServer
             UriRegexSanitizers.Add(new UriRegexSanitizer(URIDomainRegEx, "https://sanitized.skype.com"));
         }
 
-        ~CallAutomationClientAutomatedLiveTestsBase()
+        [TearDown]
+        public async Task CleanUp()
         {
             // cleanup servicebus
+            List<string> unsubList = new List<string>();
             foreach (var processor in _processorStore)
             {
-                // TODO: unsub
+                unsubList.Add(processor.Key);
             }
+
+            // unsubscribe;
+            var jsonString = JsonConvert.SerializeObject(unsubList);
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            _ = await _httpClient.PostAsync(TestEnvironment.DispatcherEndpoint + $"/api/servicebuscallback/unsubscribe", content);
         }
 
         public bool SkipCallingServerInteractionLiveTests
@@ -79,22 +92,18 @@ namespace Azure.Communication.CallingServer
             return InstrumentClient(callAutomationClient);
         }
 
-        protected async Task ServiceBusWithNewCall(string callerMRIInput)
+        protected async Task<string> ServiceBusWithNewCall(CommunicationIdentifier caller, CommunicationIdentifier reciever)
         {
-            string caller = RemoveAllNonChar(callerMRIInput);
-
-            if (_processorStore.ContainsKey(caller))
-            {
-                // already subscribed - skip
-                return;
-            }
+            string callerId = ParseIdsFromIdentifier(caller);
+            string recieverId = ParseIdsFromIdentifier(reciever);
+            string uniqueId = callerId + recieverId;
 
             // subscribe;
-            HttpResponseMessage response = await _httpClient.PostAsync(TestEnvironment.DispatcherEndpoint + $"/api/servicebuscallback/subscribe?q={caller}", null);
+            HttpResponseMessage response = await _httpClient.PostAsync(TestEnvironment.DispatcherEndpoint + $"/api/servicebuscallback/subscribe?q={uniqueId}", null);
             response.EnsureSuccessStatusCode();
 
             // create a processor that we can use to process the messages
-            var processor = _serviceBusClient.CreateProcessor(caller, new ServiceBusProcessorOptions());
+            var processor = _serviceBusClient.CreateProcessor(uniqueId, new ServiceBusProcessorOptions());
 
             // add handler to process messages
             processor.ProcessMessageAsync += MessageHandler;
@@ -105,7 +114,8 @@ namespace Azure.Communication.CallingServer
             // start processing
             await processor.StartProcessingAsync();
 
-            _processorStore.TryAdd(caller, processor);
+            _processorStore.TryAdd(uniqueId, processor);
+            return uniqueId;
         }
 
         protected async Task<string?> WaitForIncomingCallContext(string callerMRIInput, TimeSpan timeOut)
@@ -146,20 +156,44 @@ namespace Azure.Communication.CallingServer
             return null;
         }
 
+        protected string GetResourceId()
+        {
+            return TestEnvironment.ResourceIdentifier;
+        }
+
+        /// <summary>
+        /// Creates a <see cref="CommunicationIdentityClient" /> with the connectionstring via environment
+        /// variables and instruments it to make use of the Azure Core Test Framework functionalities.
+        /// </summary>
+        /// <returns>The instrumented <see cref="CommunicationIdentityClient" />.</returns>
+        protected CommunicationIdentityClient CreateInstrumentedCommunicationIdentityClient()
+            => InstrumentClient(
+                new CommunicationIdentityClient(
+                    TestEnvironment.LiveTestStaticConnectionString,
+                    InstrumentClientOptions(new CommunicationIdentityClientOptions(CommunicationIdentityClientOptions.ServiceVersion.V2021_03_07))));
+
+        protected async Task<CommunicationUserIdentifier> CreateIdentityUserAsync()
+        {
+            CommunicationIdentityClient communicationIdentityClient = CreateInstrumentedCommunicationIdentityClient();
+            return await communicationIdentityClient.CreateUserAsync().ConfigureAwait(false);
+        }
+
         private async Task MessageHandler(ProcessMessageEventArgs args)
         {
             string body = args.Message.Body.ToString();
 
             if (!string.IsNullOrEmpty(body))
             {
+                Console.WriteLine($"Events: {JToken.Parse(body).ToString(Formatting.Indented)}");
+
                 if (body.Contains("\"incomingCallContext\":\""))
                 {
                     // for incoming call event grid event
                     string incomingCallContext = body.Split(new[] { "\"incomingCallContext\":\"" }, StringSplitOptions.None)[1].Split(new[] { "\"" }, StringSplitOptions.None)[0];
 
-                    string voipinc = body.Split(new[] { "\"from\":{\"kind\":\"CommunicationUser\",\"rawId\":\"" }, StringSplitOptions.None)[1].Split(new[] { "\"" }, StringSplitOptions.None)[0];
+                    string uniqueId = ParseFromAndTo(body);
 
-                    _incomingcontextstore.TryAdd(RemoveAllNonChar(voipinc), incomingCallContext);
+                    _incomingcontextstore.TryAdd(RemoveAllNonChar(uniqueId), incomingCallContext);
                 }
                 else
                 {
@@ -199,31 +233,36 @@ namespace Azure.Communication.CallingServer
             return InstrumentClientOptions(callClientOptions);
         }
 
-        protected string GetResourceId()
+        private string ParseFromAndTo(string unparsed)
         {
-            return TestEnvironment.ResourceIdentifier;
+            // TODO: Update this part once incmoing call can be parsed with event grid in the future
+            string fromId = unparsed.Split(new string[] { "\"from\":{\"kind\":" }, StringSplitOptions.None)[1].Split(new string[] { "\"rawId\":\"" }, StringSplitOptions.None)[1].Split(new string[] { "\"" }, StringSplitOptions.None)[0];
+            string toId = unparsed.Split(new string[] { "\"to\":{\"kind\":" }, StringSplitOptions.None)[1].Split(new string[] { "\"rawId\":\"" }, StringSplitOptions.None)[1].Split(new string[] { "\"" }, StringSplitOptions.None)[0];
+
+            return fromId + toId;
+        }
+
+        private string ParseIdsFromIdentifier(CommunicationIdentifier inputIdentifier)
+        {
+            switch (inputIdentifier)
+            {
+                case null:
+                    throw new ArgumentNullException();
+                case CommunicationUserIdentifier:
+                    return RemoveAllNonChar(((CommunicationUserIdentifier)inputIdentifier).RawId);
+                case PhoneNumberIdentifier:
+                    return RemoveAllNonChar(((PhoneNumberIdentifier)inputIdentifier).RawId);
+                case MicrosoftTeamsUserIdentifier:
+                    return RemoveAllNonChar(((MicrosoftTeamsUserIdentifier)inputIdentifier).RawId);
+                default:
+                    throw new NotSupportedException();
+            }
         }
 
         private static string RemoveAllNonChar(string inputId)
         {
             Regex rgx = new Regex("[^a-zA-Z0-9_-]");
             return rgx.Replace(inputId, "");
-        }
-
-        /// <summary>
-        /// Creates a <see cref="CommunicationIdentityClient" /> with the connectionstring via environment
-        /// variables and instruments it to make use of the Azure Core Test Framework functionalities.
-        /// </summary>
-        /// <returns>The instrumented <see cref="CommunicationIdentityClient" />.</returns>
-        protected CommunicationIdentityClient CreateInstrumentedCommunicationIdentityClient()
-            => InstrumentClient(
-                new CommunicationIdentityClient(
-                    TestEnvironment.LiveTestStaticConnectionString,
-                    InstrumentClientOptions(new CommunicationIdentityClientOptions(CommunicationIdentityClientOptions.ServiceVersion.V2021_03_07))));
-
-        protected async Task<CommunicationUserIdentifier> CreateIdentityUserAsync() {
-            CommunicationIdentityClient communicationIdentityClient = CreateInstrumentedCommunicationIdentityClient();
-            return await communicationIdentityClient.CreateUserAsync().ConfigureAwait(false);
         }
     }
 }
