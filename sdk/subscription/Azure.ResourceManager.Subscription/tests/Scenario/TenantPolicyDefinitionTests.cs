1a50bce34e1d23f374a25e57dbb11eaa181cd9c5
Chengming Ruan (WICRESOFT NORTH AMERICA LTD) <v-cruan@microsoft.com>
2022-09-13 17:48:20 +0800

    add some tests

---
+++ sdk/subscription/Azure.ResourceManager.Subscription/tests/Scenario/TenantPolicyDefinitionTests.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Subscription.Tests
+{
+    internal class TenantPolicyDefinitionTests : SubscriptionManagementTestBase
+    {
+        private TenantPolicyDefinitionCollection _tenantPolicyDefinitionCollection => GetTenantPolicyDefinition().Result;
+
+        public TenantPolicyDefinitionTests(bool isAsync) : base(isAsync)
+        {
+        }
+
+        private async Task<TenantPolicyDefinitionCollection> GetTenantPolicyDefinition()
+        {
+            var tenants = await Client.GetTenants().GetAllAsync().ToEnumerableAsync();
+            return tenants.FirstOrDefault().GetTenantPolicyDefinitions();
+        }
+
+        [RecordedTest]
+        public async Task GetAll()
+        {
+            var list = await _tenantPolicyDefinitionCollection.GetAllAsync().ToEnumerableAsync();
+            Assert.IsNotEmpty(list);
+        }
+    }
+}
