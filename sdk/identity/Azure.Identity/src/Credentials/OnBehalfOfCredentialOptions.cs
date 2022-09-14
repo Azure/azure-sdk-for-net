f62d4e7ec62acdbfe0d0086791b16223db00b762
Scott Schaab <sschaab@microsoft.com>
2022-09-14 08:51:35 -0700

    Update sdk/identity/Azure.Identity/src/Credentials/OnBehalfOfCredentialOptions.cs
    Co-authored-by: Scott Addie <10702007+scottaddie@users.noreply.github.com>

--- sdk/identity/Azure.Identity/src/Credentials/OnBehalfOfCredentialOptions.cs
+++ sdk/identity/Azure.Identity/src/Credentials/OnBehalfOfCredentialOptions.cs
@@ -21,7 +21,7 @@ namespace Azure.Identity
         public bool SendCertificateChain { get; set; }
 
         /// <summary>
-        /// For multi-tenant applications, specifies additional tenants for which the credential may acquire tokens. Add the wildcard value "*" to allow the credential to acquire tokens for any tenant the application is installed.
+        /// For multi-tenant applications, specifies additional tenants for which the credential may acquire tokens. Add the wildcard value "*" to allow the credential to acquire tokens for any tenant in which the application is installed.
         /// </summary>
         public IList<string> AdditionallyAllowedTenants => AdditionallyAllowedTenantsCore;
     }
