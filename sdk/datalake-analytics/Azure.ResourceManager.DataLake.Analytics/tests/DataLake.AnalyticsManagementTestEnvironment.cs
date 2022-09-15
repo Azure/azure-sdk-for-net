d1de413bc4505af7d76f7ab4d72a22b7bfba7bbc
Arthur Ma <arthurma@microsoft.com>
2022-09-15 17:38:31 +0800

    Init Azure.ResourceManager.DataLake.Analytics

---
+++ sdk/datalake-analytics/Azure.ResourceManager.DataLake.Analytics/tests/DataLake.AnalyticsManagementTestEnvironment.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.ResourceManager.DataLake.Analytics.Tests
+{
+    public class DataLake.AnalyticsManagementTestEnvironment : TestEnvironment
+    {
+    }
+}
