ccde4cfc61d002711991bc37930c009d475320fa
Franko Morales <fmorales@microsoft.com>
2022-09-20 11:18:29 -0700

    Updating Media's Recognize tests

--- sdk/communication/Azure.Communication.CallAutomation/tests/CallMedia/CallMediaLiveTests.cs
+++ sdk/communication/Azure.Communication.CallAutomation/tests/CallMedia/CallMediaLiveTests.cs
@@ -15,6 +15,9 @@ namespace Azure.Communication.CallAutomation
         [Test]
         public async Task PlayAudio()
         {
+            if (SkipCallingServerInteractionLiveTests)
+                Assert.Ignore("Skip callingserver interaction live tests flag is on.");
+
             CallAutomationClient client = CreateInstrumentedCallAutomationClientWithConnectionString();
             string callConnectionId = "";
             try
@@ -31,10 +34,53 @@ namespace Azure.Communication.CallAutomation
                 var callConnection = callResponse.Value.CallConnection;
                 callConnectionId = callConnection.CallConnectionId;
                 var playResponse = await callConnection.GetCallMedia().PlayAsync(
-                    new FileSource(new Uri(playAudioUri)) { PlaySourceId = "playSourceId"},
+                    new FileSource(new Uri(playAudioUri)) { PlaySourceId = "playSourceId" },
                     new CommunicationUserIdentifier[] { targetUser });
                 Assert.NotNull(playResponse);
                 Assert.AreEqual(202, playResponse.Status);
+            }
+            catch (RequestFailedException ex)
+            {
+                Assert.Fail($"Unexpected error: {ex}");
+            }
+            finally
+            {
+                var callConnection = client.GetCallConnection(callConnectionId);
+                await callConnection.HangUpAsync(true).ConfigureAwait(false);
+            }
+        }
+
+        [Test]
+        public async Task RecognizeDtmf()
+        {
+            if (SkipCallingServerInteractionLiveTests)
+                Assert.Ignore("Skip callingserver interaction live tests flag is on.");
+
+            CallAutomationClient client = CreateInstrumentedCallAutomationClientWithConnectionString();
+            string callConnectionId = "";
+            try
+            {
+                var user = await CreateIdentityUserAsync().ConfigureAwait(false);
+                var targetUserId = TestEnvironment.TargetUserId;
+                var targetUser = new CommunicationUserIdentifier(targetUserId);
+                string ngrok = "https://localhost";
+                string promptUrl = "https://localhost/audio/bot-hold-music-2.wav";
+                var targets = new CommunicationIdentifier[] { targetUser };
+                var callResponse = await client.CreateCallAsync(new CallSource(user), targets, new Uri(ngrok)).ConfigureAwait(false);
+                await WaitForOperationCompletion().ConfigureAwait(false);
+                Assert.NotNull(callResponse);
+                Assert.NotNull(callResponse.Value);
+                var callConnection = callResponse.Value.CallConnection;
+                callConnectionId = callConnection.CallConnectionId;
+                var dtmfResponse = await callConnection.GetCallMedia().StartRecognizingAsync(new CallMediaRecognizeDtmfOptions(targetUser, 5)
+                {
+                    InitialSilenceTimeout = TimeSpan.FromSeconds(15),
+                    InterToneTimeout = TimeSpan.FromSeconds(5),
+                    StopTones = new DtmfTone[] { DtmfTone.Pound },
+                    Prompt = new FileSource(new Uri(promptUrl)) { PlaySourceId = "playSourceId" }
+                });
+                Assert.NotNull(dtmfResponse);
+                Assert.AreEqual(202, dtmfResponse.Status);
             }
             catch (RequestFailedException ex)
             {
