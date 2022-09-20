71c848150d616d2d7c5e721f377f21b77f85392e
Yeming Liu <11371776+isra-fel@users.noreply.github.com>
2022-09-20 09:49:57 +0800

    regen public api

--- sdk/identity/Azure.Identity.BrokeredAuthentication/api/Azure.Identity.BrokeredAuthentication.netstandard2.0.cs
+++ sdk/identity/Azure.Identity.BrokeredAuthentication/api/Azure.Identity.BrokeredAuthentication.netstandard2.0.cs
@@ -2,11 +2,11 @@ namespace Azure.Identity.BrokeredAuthentication
 {
     public partial class InteractiveBrowserCredentialBrokerOptions : Azure.Identity.InteractiveBrowserCredentialOptions
     {
-        public InteractiveBrowserCredentialBrokerOptions(System.IntPtr parentWindowHandle) { }
+        public InteractiveBrowserCredentialBrokerOptions(System.IntPtr parentWindowHandle, bool msaPassthrough = false) { }
     }
     public partial class SharedTokenCacheCredentialBrokerOptions : Azure.Identity.SharedTokenCacheCredentialOptions
     {
-        public SharedTokenCacheCredentialBrokerOptions() { }
-        public SharedTokenCacheCredentialBrokerOptions(Azure.Identity.TokenCachePersistenceOptions tokenCacheOptions) { }
+        public SharedTokenCacheCredentialBrokerOptions(Azure.Identity.TokenCachePersistenceOptions tokenCacheOptions, bool msaPassthrough = false) { }
+        public SharedTokenCacheCredentialBrokerOptions(bool msaPassthrough = false) { }
     }
 }
