8e11c370f6d8d5b67871be1a1c15541af7215236
Arthur Ma <arthurma@microsoft.com>
2022-09-21 17:03:34 +0800

    Polish Azure.ResourceManager.LabServices (#31305)

--- sdk/labservices/Azure.ResourceManager.LabServices/src/Generated/Models/PagedSchedules.cs
+++ sdk/labservices/Azure.ResourceManager.LabServices/src/Generated/Models/PagedSchedules.cs
@@ -17,20 +17,20 @@ namespace Azure.ResourceManager.LabServices.Models
         /// <summary> Initializes a new instance of PagedSchedules. </summary>
         internal PagedSchedules()
         {
-            Value = new ChangeTrackingList<ScheduleData>();
+            Value = new ChangeTrackingList<LabServicesScheduleData>();
         }
 
         /// <summary> Initializes a new instance of PagedSchedules. </summary>
         /// <param name="value"> The array page of schedule results. </param>
         /// <param name="nextLink"> The link to get the next page of schedule results. </param>
-        internal PagedSchedules(IReadOnlyList<ScheduleData> value, string nextLink)
+        internal PagedSchedules(IReadOnlyList<LabServicesScheduleData> value, string nextLink)
         {
             Value = value;
             NextLink = nextLink;
         }
 
         /// <summary> The array page of schedule results. </summary>
-        public IReadOnlyList<ScheduleData> Value { get; }
+        public IReadOnlyList<LabServicesScheduleData> Value { get; }
         /// <summary> The link to get the next page of schedule results. </summary>
         public string NextLink { get; }
     }
