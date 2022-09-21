db59f75fc6949d279695f22da73dd7ad77ff3a61
Franko Morales <67804607+cochi2@users.noreply.github.com>
2022-09-21 10:56:23 -0700

    Adding live tests for MediaStreaming/Recognize operations. (#30757)
    * Adding Media Streaming
    
    * Adding Recognize
    
    * Updating Recognize Method
    
    * Adding MediaStreamingConfiguration
    
    * Running Export API
    
    * Cleaning PR
    
    * Added unit testing
    
    * Applying internal board comments
    
    * Moving to nullable TimeSpan
    
    * Fixing tests
    
    * Adding recognize unit tests
    
    * Fixing tests after merge
    
    * Adding Recognize events. (#30663)
    
    * Merging latest swagger
    
    * Running Autorest and making events public
    
    * Events implementation Pt. 1
    
    * Events implementation Pt. 2
    
    * Adding Streaming models. (#30686)
    
    * Adding Streaming models
    
    * Adding 'Media' to the streaming models
    
    * Part 1 of the comments
    
    * Breaking RecognizeConfigurations into specialized ones
    
    * Adding MediaStreamingPackages parser
    
    * Fixing some merging issues
    
    * Moving ParticipantId to be Participant
    
    * Fixing naming stuff
    
    * Fixing naming stuff Pt 2
    
    * Adding Live Tests for Recognize/Media Streaming
    
    * Changing the name of Play/Recognize to StartPlaying/StartRecognizing
    
    * Fixing tests after merge
    
    * Restoring lines
    
    * Updating Media's Recognize tests
    
    * Adding Media Streaming Live Tests
    
    * Fixing merge
    
    * Fixing the live test skip
    
    * Fixing comment

--- sdk/communication/Azure.Communication.CallAutomation/tests/CallAutomationClient/CallAutomationClientLiveTests.cs
+++ sdk/communication/Azure.Communication.CallAutomation/tests/CallAutomationClient/CallAutomationClientLiveTests.cs
@@ -118,5 +118,59 @@ namespace Azure.Communication.CallAutomation
                 Assert.Fail($"Unexpected error: {ex}");
             }
         }
+
+        [Test]
+        public async Task CreateCallWithMediaStreaming()
+        {
+            if (SkipCallAutomationInteractionLiveTests)
+                Assert.Ignore("Skip CallAutomation interaction live tests flag is on.");
+
+            CallAutomationClient client = CreateInstrumentedCallAutomationClientWithConnectionString();
+            bool wasConnected = false;
+
+            try
+            {
+                var user = await CreateIdentityUserAsync().ConfigureAwait(false);
+                var targetId = TestEnvironment.TargetUserId;
+                var callBackUri = TestEnvironment.AppCallbackUrl;
+                var targets = new CommunicationIdentifier[] { new CommunicationUserIdentifier(targetId) };
+                CreateCallOptions options = new CreateCallOptions(new CallSource(user), targets, new Uri(callBackUri))
+                {
+                    MediaStreamingOptions = new MediaStreamingOptions(
+                        new Uri(TestEnvironment.WebsocketUrl),
+                        MediaStreamingTransport.Websocket,
+                        MediaStreamingContent.Audio,
+                        MediaStreamingAudioChannel.Mixed)
+                };
+                CreateCallResult response = await client.CreateCallAsync(options).ConfigureAwait(false);
+                await WaitForOperationCompletion().ConfigureAwait(false);
+                Assert.IsNotEmpty(response.CallConnectionProperties.CallConnectionId);
+                Assert.AreEqual(CallConnectionState.Connecting, response.CallConnectionProperties.CallConnectionState);
+                Assert.IsNotEmpty(response.CallConnectionProperties.MediaSubscriptionId);
+
+                Response<CallConnectionProperties> properties = await response.CallConnection.GetCallConnectionPropertiesAsync().ConfigureAwait(false);
+                Assert.AreEqual(CallConnectionState.Connected, properties.Value.CallConnectionState);
+                wasConnected = true;
+
+                await response.CallConnection.HangUpAsync(true).ConfigureAwait(false);
+                await WaitForOperationCompletion().ConfigureAwait(false);
+                properties = await response.CallConnection.GetCallConnectionPropertiesAsync().ConfigureAwait(false);
+
+                Assert.Fail("Call Connection should not be found after calling HangUp");
+            }
+            catch (RequestFailedException ex)
+            {
+                if (ex.Status == 404 && wasConnected)
+                {
+                    // call hung up successfully
+                    Assert.Pass();
+                }
+                Assert.Fail($"Unexpected error: {ex}");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Unexpected error: {ex}");
+            }
+        }
     }
 }
