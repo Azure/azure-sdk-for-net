7f52a4a2821816a81e5f34bd6cd107b1b59cd237
Franko Morales <fmorales@microsoft.com>
2022-09-21 10:28:34 -0700

    Adding new enum

---
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/FailureReason.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Communication.CallAutomation.Models
+{
+    /// <summary>
+    /// Failure Reasons for the upcoming webhook Events.
+    /// </summary>
+    public enum FailureReason
+    {
+        /// <summary>
+        /// Action failed, initial silence timeout reached.
+        /// </summary>
+        InitialSilenceTimeout = 8510,
+
+        /// <summary>
+        /// Action failed, inter-digit silence timeout reached.
+        /// </summary>
+        InterDigitTimeout = 8532,
+
+        /// <summary>
+        /// Action failed, encountered failure while trying to play the prompt.
+        /// </summary>
+        PlayPromptFailed = 8511,
+
+        /// <summary>
+        /// Unknown internal server error.
+        /// </summary>
+        UspecifiedError = 9999
+    }
+}
