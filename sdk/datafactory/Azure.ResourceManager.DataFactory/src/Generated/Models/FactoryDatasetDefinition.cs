0f596fac46ca808d4f0fe3951a2499ad871c6009
azure-sdk <azuresdk@microsoft.com>
2022-09-23 13:33:35 +0000

    Update AutoRest C# version to 3.0.0-beta.20220923.1

--- sdk/datafactory/Azure.ResourceManager.DataFactory/src/Generated/Models/FactoryDatasetDefinition.cs
+++ sdk/datafactory/Azure.ResourceManager.DataFactory/src/Generated/Models/FactoryDatasetDefinition.cs
@@ -127,7 +127,36 @@ namespace Azure.ResourceManager.DataFactory.Models
         public FactoryLinkedServiceReference LinkedServiceName { get; set; }
         /// <summary> Parameters for dataset. </summary>
         public IDictionary<string, EntityParameterSpecification> Parameters { get; }
-        /// <summary> List of tags that can be used for describing the Dataset. </summary>
+        /// <summary>
+        /// List of tags that can be used for describing the Dataset.
+        /// <para>
+        /// To assign an object to the element of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formated json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
         public IList<BinaryData> Annotations { get; }
         /// <summary> The folder that this Dataset is in. If not specified, Dataset will appear at the root level. </summary>
         internal DatasetFolder Folder { get; set; }
@@ -143,7 +172,36 @@ namespace Azure.ResourceManager.DataFactory.Models
             }
         }
 
-        /// <summary> Additional Properties. </summary>
+        /// <summary>
+        /// Additional Properties
+        /// <para>
+        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formated json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
         public IDictionary<string, BinaryData> AdditionalProperties { get; }
     }
 }
