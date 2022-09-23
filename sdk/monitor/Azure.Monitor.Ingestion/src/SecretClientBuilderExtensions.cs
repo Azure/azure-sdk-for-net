18a1a93585167f0ad92633a232a9c467801ab873
Nisha Bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2022-09-23 00:21:51 -0700

    Create SecretClientBuilderExtensions.cs

---
+++ sdk/monitor/Azure.Monitor.Ingestion/src/SecretClientBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core;
+using Azure.Core.Extensions;
+using Azure.Monitor.Ingestion;
+
+namespace Microsoft.Extensions.Azure
+{
+    /// <summary>
+    /// Extension methods to add <see cref="SecretClient"/> to clients builder.
+    /// </summary>
+    public static class SecretClientBuilderExtensions
+    {
+        /// <summary>Uri endpoint, TokenCredential credential, LogsIngestionClientOptions options
+        /// Registers a <see cref="SecretClient"/> instance with the provided <paramref name="endpoint"/>
+        /// </summary>
+        public static IAzureClientBuilder<SecretClient, SecretClientOptions> AddSecretClient<TBuilder>(this TBuilder builder, Uri endpoint, TokenCredential credential, LogsIngestionClientOptions options)
+            where TBuilder : IAzureClientFactoryBuilderWithCredential
+        {
+            return builder.RegisterClientFactory<SecretClient, SecretClientOptions>((endpoint, credential, options) => new SecretClient(endpoint, credential, options));
+        }
+
+        /// <summary>
+        /// Registers a <see cref="SecretClient"/> instance with connection options loaded from the provided <paramref name="configuration"/> instance.
+        /// </summary>
+        public static IAzureClientBuilder<SecretClient, SecretClientOptions> AddSecretClient<TBuilder, TConfiguration>(this TBuilder builder, TConfiguration configuration)
+            where TBuilder : IAzureClientFactoryBuilderWithConfiguration<TConfiguration>
+        {
+            return builder.RegisterClientFactory<SecretClient, SecretClientOptions>(configuration);
+        }
+    }
+}
