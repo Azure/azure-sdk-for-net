679821aa5cfea02235b0712b2216675f6702fbc3
Arthur Ma <arthurma@microsoft.com>
2022-09-25 00:38:08 +0800

    update

--- sdk/providerhub/Azure.ResourceManager.ProviderHub/src/Generated/Models/ResourceProviderManagement.cs
+++ sdk/providerhub/Azure.ResourceManager.ProviderHub/src/Generated/Models/ResourceProviderManagement.cs
@@ -58,7 +58,36 @@ namespace Azure.ResourceManager.ProviderHub.Models
         public IList<ServiceTreeInfo> ServiceTreeInfos { get; }
         /// <summary> Gets or sets the resource access policy. </summary>
         public ResourceProviderManagementResourceAccessPolicy? ResourceAccessPolicy { get; set; }
-        /// <summary> Gets the resource access roles. </summary>
+        /// <summary>
+        /// Gets the resource access roles
+        /// <para>
+        /// To assign an object to the element of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formated json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
         public IList<BinaryData> ResourceAccessRoles { get; }
     }
 }
