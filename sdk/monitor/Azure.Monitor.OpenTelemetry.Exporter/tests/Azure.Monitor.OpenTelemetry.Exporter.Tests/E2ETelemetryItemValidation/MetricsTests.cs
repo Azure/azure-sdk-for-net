bdb2904fbf4a1a28a0d8f315615c7fda82e47bab
Timothy Mothra <tilee@microsoft.com>
2022-09-29 13:42:44 -0700

    [AzureMonitorExporter] new tests to validate `TelemetryItem` (#31478)
    * new tests
    
    * remove Integration.Tests
    
    * renamed namespace E2ETelemetryItemValidation

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/E2ETelemetryItemValidation/MetricsTests.cs
@@ -0,0 +1,114 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using Azure.Monitor.OpenTelemetry.Exporter.Models;
+using Azure.Monitor.OpenTelemetry.Exporter.Tests.CommonTestFramework;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using Xunit;
+
+namespace Azure.Monitor.OpenTelemetry.Exporter.Tests.E2ETelemetryItemValidation
+{
+    /// <summary>
+    /// The purpose of these tests is to validate the <see cref="TelemetryItem"/> that is created
+    /// based on interacting with <see cref="MeterProvider"/> and <see cref="Meter"/>.
+    /// </summary>
+    public class MetricsTests
+    {
+        private const string MeterName = "CompanyA.ProductA.Library1";
+        private static readonly Meter MyMeter = new(MeterName, "1.0");
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void VerifyCounter(bool asView)
+        {
+            // SETUP
+            var meterProviderBulider = Sdk.CreateMeterProviderBuilder()
+                .AddMeter(MeterName)
+                .AddAzureMonitorMetricExporterForTest(out ConcurrentBag<TelemetryItem> telemetryItems);
+
+            if (asView)
+            {
+                meterProviderBulider
+                    // Rename an instrument to new name.
+                    .AddView(instrumentName: "MyCounter", name: "MyCounterRenamed");
+            }
+
+            var meterProvider = meterProviderBulider.Build();
+
+            // ACT
+            var counter = MyMeter.CreateCounter<long>("MyCounter");
+            for (int i = 0; i < 20000; i++)
+            {
+                counter.Add(1, new("tag1", "value1"), new("tag2", "value2"));
+            }
+
+            // CLEANUP
+            meterProvider.Dispose();
+
+            // ASSERT
+            Assert.True(telemetryItems.Any(), "Unit test failed to collect telemetry.");
+            var telemetryItem = telemetryItems.Single();
+
+            TelemetryItemValidationHelper.AssertCounter_As_MetricTelemetry(
+                telemetryItem: telemetryItem,
+                expectedMetricsName: asView ? "MyCounterRenamed" : "MyCounter",
+                expectedMetricsNamespace: MeterName,
+                expectedMetricsValue: 20000,
+                expectedMetricsProperties: new Dictionary<string, string> { { "tag1", "value1" }, { "tag2", "value2" } });
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void VerifyHistogram(bool asView)
+        {
+            // SETUP
+            var meterProviderBulider = Sdk.CreateMeterProviderBuilder()
+                .AddMeter(MeterName)
+                .AddAzureMonitorMetricExporterForTest(out ConcurrentBag<TelemetryItem> telemetryItems);
+
+            if (asView)
+            {
+                meterProviderBulider
+                // Change Histogram boundaries
+                .AddView(instrumentName: "MyHistogram", new ExplicitBucketHistogramConfiguration() { Boundaries = new double[] { 10, 20 } });
+            }
+
+            var meterProvider = meterProviderBulider.Build();
+
+            // ACT
+            var histogram = MyMeter.CreateHistogram<long>("MyHistogram");
+            var random = new Random();
+            int loop = 20000, sum = 0;
+            for (int i = 0; i < loop; i++)
+            {
+                var value = random.Next(1, 1000);
+                sum += value;
+                histogram.Record(value, new("tag1", "value1"), new("tag2", "value2"));
+            }
+
+            // CLEANUP
+            meterProvider.Dispose();
+
+            // ASSERT
+            Assert.True(telemetryItems.Any(), "Unit test failed to collect telemetry.");
+            var telemetryItem = telemetryItems.Single();
+
+            TelemetryItemValidationHelper.AssertHistogram_As_MetricTelemetry(
+                telemetryItem: telemetryItem,
+                expectedMetricsName: "MyHistogram",
+                expectedMetricsNamespace: MeterName,
+                expectedMetricsCount: loop,
+                expectedMetricsValue: sum,
+                expectedMetricsProperties: new Dictionary<string, string> { { "tag1", "value1" }, { "tag2", "value2" } });
+        }
+    }
+}
