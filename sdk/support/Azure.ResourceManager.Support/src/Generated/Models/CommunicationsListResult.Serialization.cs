4a5b2d43e1a5790d52700f43e2b217794852323d
Feng Zhou <fenzhou@microsoft.com>
2022-09-29 17:24:29 +0800

    address api review comments

--- sdk/support/Azure.ResourceManager.Support/src/Generated/Models/CommunicationsListResult.Serialization.cs
+++ sdk/support/Azure.ResourceManager.Support/src/Generated/Models/CommunicationsListResult.Serialization.cs
@@ -16,7 +16,7 @@ namespace Azure.ResourceManager.Support.Models
     {
         internal static CommunicationsListResult DeserializeCommunicationsListResult(JsonElement element)
         {
-            Optional<IReadOnlyList<CommunicationDetailData>> value = default;
+            Optional<IReadOnlyList<SupportTicketCommunicationData>> value = default;
             Optional<string> nextLink = default;
             foreach (var property in element.EnumerateObject())
             {
@@ -27,10 +27,10 @@ namespace Azure.ResourceManager.Support.Models
                         property.ThrowNonNullablePropertyIsNull();
                         continue;
                     }
-                    List<CommunicationDetailData> array = new List<CommunicationDetailData>();
+                    List<SupportTicketCommunicationData> array = new List<SupportTicketCommunicationData>();
                     foreach (var item in property.Value.EnumerateArray())
                     {
-                        array.Add(CommunicationDetailData.DeserializeCommunicationDetailData(item));
+                        array.Add(SupportTicketCommunicationData.DeserializeSupportTicketCommunicationData(item));
                     }
                     value = array;
                     continue;
