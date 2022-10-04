916f69769b99ad1cccdea87af1278dab83504e14
Jesse Squire <jesse.squire@gmail.com>
2022-10-04 16:56:28 -0400

    Tweaking verbiage for the processor to match suggestions

--- sdk/eventhub/Azure.Messaging.EventHubs/src/Primitives/EventProcessor.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/src/Primitives/EventProcessor.cs
@@ -1130,7 +1130,7 @@ namespace Azure.Messaging.EventHubs.Primitives
         ///   This method will be invoked concurrently, limited to one call per partition.  The processor will await each invocation before beginning to process
         ///   the associated partition.
         ///
-        ///   No time limit is imposed on an invocation; the processor will wait indefinitely for execution to complete.  It is recommended for implementations to avoid
+        ///   The processor will wait indefinitely for execution of the handler to complete. It is recommended for implementations to avoid
         ///   long-running operations, as they will delay processing for the associated partition.
         /// </remarks>
         ///
