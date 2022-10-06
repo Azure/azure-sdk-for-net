c0c0d28c424540c29bca6a3c6dff936572d6bfc5
Chris Miller <chrismiller@microsoft.com>
2022-10-06 16:19:00 -0400

    Add tests and bicep

---
+++ sdk/devcenter/Azure.Developer.DevCenter/tests/Samples/Sample_CreateDeleteDevBoxAsync.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.Developer.DevCenter.Tests.Samples
+{
+    public partial class DevCenterSamples: SamplesBase<DevCenterClientTestEnvironment>
+    {
+        /* please refer to https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/template/Azure.Template/tests/Samples/Sample1.HelloWorld.cs to write samples. */
+        #region Snippet:Azure_DevCenter_DevBox_Scenario
+        public async Task CreateDeleteDevBoxAsync(string tenantId, string devCenterName)
+        {
+            // Create and delete a user devbox
+            var credential = new DefaultAzureCredential();
+            var devCenterClient = new DevCenterClient(tenantId, devCenterName, credential);
+            string targetProjectName = null;
+            await foreach (BinaryData data in devCenterClient.GetProjectsAsync(filter: null, top: 1))
+            {
+                JsonElement result = JsonDocument.Parse(data.ToStream()).RootElement;
+                targetProjectName = result.GetProperty("name").ToString();
+            }
+
+            if (targetProjectName is null)
+            {
+                throw new InvalidOperationException($"No valid project resources found in DevCenter {devCenterName}/tenant {tenantId}.");
+            }
+
+            // Grab a pool
+            var devBoxesClient = new DevBoxesClient(tenantId, devCenterName, targetProjectName, credential);
+            string targetPoolName = null;
+            await foreach (BinaryData data in devBoxesClient.GetPoolsAsync(filter: null, top: 1))
+            {
+                JsonElement result = JsonDocument.Parse(data.ToStream()).RootElement;
+                targetPoolName = result.GetProperty("name").ToString();
+            }
+
+            if (targetPoolName is null)
+            {
+                throw new InvalidOperationException($"No valid pool resources found in Project {targetProjectName}/DevCenter {devCenterName}/tenant {tenantId}.");
+            }
+
+            // Provision your dev box in the selected pool
+            var content = new
+            {
+                poolName = targetPoolName,
+            };
+
+            Operation<BinaryData> devBoxCreateOperation = await devBoxesClient.CreateDevBoxAsync(WaitUntil.Completed, "MyDevBox", RequestContent.Create(content));
+            BinaryData devBoxData = await devBoxCreateOperation.WaitForCompletionAsync();
+            JsonElement devBox = JsonDocument.Parse(devBoxData.ToStream()).RootElement;
+            Console.WriteLine($"Completed provisioning for dev box with status {devBox.GetProperty("provisioningState")}.");
+
+            // Fetch the web connection URL to access your dev box from the browser
+            Response remoteConnectionResponse = await devBoxesClient.GetRemoteConnectionAsync("MyDevBox");
+            JsonElement remoteConnectionData = JsonDocument.Parse(remoteConnectionResponse.ContentStream).RootElement;
+            Console.WriteLine($"Connect using web URL {remoteConnectionData.GetProperty("webUrl")}.");
+
+            // Delete your dev box when finished
+            Operation devBoxDeleteOperation = await devBoxesClient.DeleteDevBoxAsync(WaitUntil.Completed, "MyDevBox");
+            await devBoxDeleteOperation.WaitForCompletionResponseAsync();
+            Console.WriteLine($"Completed dev box deletion.");
+        }
+        #endregion
+    }
+}
---
+++ sdk/devcenter/Azure.Developer.DevCenter/tests/Samples/Sample_CreateDeleteEnvironmentAsync.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
+using NUnit.Framework;
+
+namespace Azure.Developer.DevCenter.Tests.Samples
+{
+    public partial class DevCenterSamples: SamplesBase<DevCenterClientTestEnvironment>
+    {
+        /* please refer to https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/template/Azure.Template/tests/Samples/Sample1.HelloWorld.cs to write samples. */
+        #region Snippet:Azure_DevCenter_Environment_Scenario
+        public async Task CreateDeleteEnvironmentAsync(string tenantId, string devCenterName)
+        {
+            // Create and delete a user environment
+            var credential = new DefaultAzureCredential();
+            var devCenterClient = new DevCenterClient(tenantId, devCenterName, credential);
+            string projectName = null;
+            await foreach (BinaryData data in devCenterClient.GetProjectsAsync(filter: null, top: 1))
+            {
+                JsonElement result = JsonDocument.Parse(data.ToStream()).RootElement;
+                projectName = result.GetProperty("name").ToString();
+            }
+
+            if (projectName is null)
+            {
+                throw new InvalidOperationException($"No valid project resources found in DevCenter {devCenterName}/tenant {tenantId}.");
+            }
+
+            var environmentsClient = new EnvironmentsClient(tenantId, devCenterName, projectName, credential);
+            string catalogItemName = null;
+            await foreach (BinaryData data in environmentsClient.GetCatalogItemsAsync(top: 1))
+            {
+                JsonElement result = JsonDocument.Parse(data.ToStream()).RootElement;
+                catalogItemName = result.GetProperty("name").ToString();
+            }
+
+            if (catalogItemName is null)
+            {
+                throw new InvalidOperationException($"No valid catalog item resources found in Project {projectName}/DevCenter {devCenterName}/tenant {tenantId}.");
+            }
+
+            string environmentTypeName = null;
+            await foreach (BinaryData data in environmentsClient.GetEnvironmentTypesAsync(top: 1))
+            {
+                JsonElement result = JsonDocument.Parse(data.ToStream()).RootElement;
+                environmentTypeName = result.GetProperty("name").ToString();
+            }
+
+            if (environmentTypeName is null)
+            {
+                throw new InvalidOperationException($"No valid catalog item resources found in Project {projectName}/DevCenter {devCenterName}/tenant {tenantId}.");
+            }
+
+            var content = new
+            {
+                environmentType = environmentTypeName,
+                catalogItemName = catalogItemName,
+            };
+
+            // Deploy the environment
+            Operation<BinaryData> environmentCreateOperation = await environmentsClient.CreateOrUpdateEnvironmentAsync(WaitUntil.Completed, "DevEnvironment", RequestContent.Create(content));
+            BinaryData environmentData = await environmentCreateOperation.WaitForCompletionAsync();
+            JsonElement environment = JsonDocument.Parse(environmentData.ToStream()).RootElement;
+            Console.WriteLine($"Completed provisioning for environment with status {environment.GetProperty("provisioningState")}.");
+
+            // Fetch and output the deployment artifacts
+            await foreach (BinaryData data in environmentsClient.GetArtifactsByEnvironmentAsync(projectName, "DevEnvironment"))
+            {
+                JsonElement result = JsonDocument.Parse(data.ToStream()).RootElement;
+                Console.WriteLine(result.GetProperty("name").ToString());
+                Console.WriteLine(result.GetProperty("isDirectory").ToString());
+                Console.WriteLine(result.GetProperty("downloadUri").ToString());
+            }
+
+            // Delete the environment when finished
+            Operation environmentDeleteOperation = await environmentsClient.DeleteEnvironmentAsync(WaitUntil.Completed, projectName, "DevEnvironment");
+            await environmentDeleteOperation.WaitForCompletionResponseAsync();
+            Console.WriteLine($"Completed environment deletion.");
+        }
+        #endregion
+    }
+}
