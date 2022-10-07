a0d89ed4f9911c899d48a9c2d65a91be866236a7
Nivedit Jain <jain.22@iitj.ac.in>
2022-10-07 19:04:26 +0530

    fixed pipelines

--- sdk/loadtestservice/Azure.Developer.LoadTesting/src/LoadTestAdministrationClient.cs
+++ sdk/loadtestservice/Azure.Developer.LoadTesting/src/LoadTestAdministrationClient.cs
@@ -13,7 +13,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Json.Serialization;
 using System.Text.Json;
-using System.Text.Json.Nodes;
 using System.Threading;
 
 namespace Azure.Developer.LoadTesting
