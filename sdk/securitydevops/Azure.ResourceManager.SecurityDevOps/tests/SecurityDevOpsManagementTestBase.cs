635fa3c9dd3c6104333cfcf02bda4cee37e573d3
Johnathon Mohr <JohnathonMohr@users.noreply.github.com>
2022-10-07 18:30:32 -0700

    SecurityDevOps initial SDK generation

---
+++ sdk/securitydevops/Azure.ResourceManager.SecurityDevOps/tests/SecurityDevOpsManagementTestBase.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.TestFramework;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Azure.ResourceManager.SecurityDevOps.Tests
+{
+    public class SecurityDevOpsManagementTestBase : ManagementRecordedTestBase<SecurityDevOpsManagementTestEnvironment>
+    {
+        protected ArmClient Client { get; private set; }
+
+        protected SecurityDevOpsManagementTestBase(bool isAsync, RecordedTestMode mode)
+        : base(isAsync, mode)
+        {
+        }
+
+        protected SecurityDevOpsManagementTestBase(bool isAsync)
+            : base(isAsync)
+        {
+        }
+
+        [SetUp]
+        public void CreateCommonClient()
+        {
+            Client = GetArmClient();
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        {
+            string rgName = Recording.GenerateAssetName(rgNamePrefix);
+            ResourceGroupData input = new ResourceGroupData(location);
+            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            return lro.Value;
+        }
+    }
+}
