f06a2db34c57b6a44449c4d05002d4124433101b
Anne Thompson <annelo@microsoft.com>
2022-10-10 14:39:01 -0700

    add a test app for creating sampleS

---
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/apps/TestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Azure;
+using Azure.Containers.ContainerRegistry;
+using Azure.Identity;
+
+var endpoint = new Uri("https://annelocontainerregistry.azurecr.io");
+var credential = new ClientSecretCredential(
+    Environment.GetEnvironmentVariable("CONTAINERREGISTRY_TENANT_ID"),
+    Environment.GetEnvironmentVariable("CONTAINERREGISTRY_CLIENT_ID"),
+    Environment.GetEnvironmentVariable("CONTAINERREGISTRY_CLIENT_SECRET"),
+    new ClientSecretCredentialOptions()
+    {
+        AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+    }
+);
+
+var options = new ContainerRegistryClientOptions() { Audience = ContainerRegistryAudience.AzureResourceManagerPublicCloud };
+
+ContainerRegistryClient client = new ContainerRegistryClient(endpoint, credential, options);
+var names = client.GetRepositoryNamesAsync();
+
+await foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine("Done.");
+Console.ReadLine();
