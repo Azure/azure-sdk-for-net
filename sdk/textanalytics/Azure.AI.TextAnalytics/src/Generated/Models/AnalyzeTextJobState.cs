271b268525c4530eb83ba8ed5ed829886ff49533
Jose Arriaga Maldonado <45773732+joseharriaga@users.noreply.github.com>
2022-10-11 15:21:14 -0700

    [Text Analytics] Re-gen based on version 2022-10-01-preview (#31657)

--- sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/AnalyzeTextJobState.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/AnalyzeTextJobState.cs
@@ -18,7 +18,7 @@ namespace Azure.AI.TextAnalytics.Models
         /// <param name="createdDateTime"></param>
         /// <param name="jobId"></param>
         /// <param name="lastUpdatedDateTime"></param>
-        /// <param name="status"></param>
+        /// <param name="status"> The status of the task at the mentioned last update time. </param>
         /// <param name="tasks"></param>
         /// <exception cref="ArgumentNullException"> <paramref name="jobId"/> or <paramref name="tasks"/> is null. </exception>
         internal AnalyzeTextJobState(DateTimeOffset createdDateTime, string jobId, DateTimeOffset lastUpdatedDateTime, TextAnalyticsOperationStatus status, AnalyzeTasks tasks) : base(createdDateTime, jobId, lastUpdatedDateTime, status)
@@ -41,7 +41,7 @@ namespace Azure.AI.TextAnalytics.Models
         /// <param name="expirationDateTime"></param>
         /// <param name="jobId"></param>
         /// <param name="lastUpdatedDateTime"></param>
-        /// <param name="status"></param>
+        /// <param name="status"> The status of the task at the mentioned last update time. </param>
         /// <param name="errors"></param>
         /// <param name="nextLink"></param>
         /// <param name="tasks"></param>
