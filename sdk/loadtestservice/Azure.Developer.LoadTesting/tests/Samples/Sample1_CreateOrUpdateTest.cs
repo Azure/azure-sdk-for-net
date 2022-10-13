ec841c9fe994edc4288c5ab22a5652335e28064d
Nivedit Jain <jain.22@iitj.ac.in>
2022-10-13 18:50:59 +0530

    added first sample, testing pipeline

---
+++ sdk/loadtestservice/Azure.Developer.LoadTesting/tests/Samples/Sample1_CreateOrUpdateTest.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
+using NUnit.Framework;
+
+namespace Azure.Developer.LoadTesting.Tests.Samples
+{
+    public partial class LoadTestingSamples: SamplesBase<LoadTestingClientTestEnvironment>
+    {
+        [Test]
+        [SyncOnly]
+        public void CreateOrUpdateTest()
+        {
+            #region Snippet:CreatingClient
+
+            string endpoint = TestEnvironment.Endpoint;
+            TokenCredential credential = TestEnvironment.Credential;
+
+            // creating LoadTesting Client
+            LoadTestingClient loadTestingClient = new LoadTestingClient(endpoint, credential);
+
+            // getting appropirate Subclient
+            LoadTestAdministrationClient loadTestAdministrationClient = loadTestingClient.getLoadTestAdministration();
+
+            #endregion
+
+            #region Snippet:CreatOrUpdateTest
+
+            // provide unique identifier for your test
+            string testId = "my-test-id";
+
+            // all data needs to be passed while creating a loadtest
+            var data = new
+            {
+                description = "This is created using SDK",
+                displayName = "SDK's LoadTest",
+                loadTestConfig = new
+                {
+                    engineInstances = 1,
+                    splitAllCSVs = false,
+                },
+                secrets = new { },
+                enviornmentVariables = new { },
+                passFailCriteria = new
+                {
+                    passFailMetrics = new { },
+                }
+            };
+
+            try
+            {
+                Response response = loadTestAdministrationClient.CreateOrUpdateTest(testId, RequestContent.Create(data));
+
+                // if the test is created successfully, printing response
+                Console.WriteLine(response.Content);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(String.Format("Error : ", e.Message));
+            }
+            #endregion
+        }
+    }
+}
