e632724c41766f31fbe9175413d99cb54aa16a38
Amanda Nguyen <amnguye@microsoft.com>
2022-10-17 14:35:49 -0700

    WIP - Remade API for blobs DM, removed CopyMethod

--- sdk/storage/Azure.Storage.Blobs.DataMovement/api/Azure.Storage.Blobs.DataMovement.netstandard2.0.cs
+++ sdk/storage/Azure.Storage.Blobs.DataMovement/api/Azure.Storage.Blobs.DataMovement.netstandard2.0.cs
@@ -6,12 +6,3 @@ namespace Azure.Storage.Blobs.DataMovement
         public static Azure.Storage.DataMovement.StorageResource GetBlockBlob(Azure.Storage.Blobs.Specialized.BlockBlobClient blobClient) { throw null; }
     }
 }
-namespace Azure.Storage.Blobs.DataMovement.Models
-{
-    public enum BlobCopyMethod
-    {
-        Copy = 0,
-        SyncCopy = 1,
-        UploadFromUriCopy = 2,
-    }
-}
