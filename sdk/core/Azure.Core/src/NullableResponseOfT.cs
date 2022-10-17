08e285492c3f6dc39786f24b9264741cd115fe35
Christopher Scott <chriss@microsoft.com>
2022-10-17 10:54:49 -0500

    formatting

--- sdk/core/Azure.Core/src/NullableResponseOfT.cs
+++ sdk/core/Azure.Core/src/NullableResponseOfT.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using System.ComponentModel;
