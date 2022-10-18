ee5b086ed39f23b371aaa7e2048ef0bebb2b6bc3
LuoxingXiong <luxiong@microsoft.com>
2022-10-18 14:37:56 +0800

    remove masked using models

--- sdk/anomalydetector/Azure.AI.AnomalyDetector/tests/Infrastructure/TestData.cs
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/tests/Infrastructure/TestData.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using Newtonsoft.Json.Linq;
-//using Azure.AI.AnomalyDetector.Models;
 
 namespace Azure.AI.AnomalyDetector.Tests.Infrastructure
 {
