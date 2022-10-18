3443463e11fcb663e9e367d452735b284a23886c
Nisha Bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2022-10-18 16:00:17 -0700

    wip

--- sdk/monitor/Azure.Monitor.Ingestion/perf/IngestionClientTest.cs
+++ sdk/monitor/Azure.Monitor.Ingestion/perf/IngestionClientTest.cs
@@ -2,9 +2,11 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Identity;
+using Azure.Monitor.Ingestion.Tests;
 using Azure.Test.Perf;
 using CommandLine;
 
@@ -14,8 +16,27 @@ namespace Azure.Monitor.Ingestion.Perf
     {
         /* please refer to https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/template/Azure.Template/perf/TemplateClientTest.cs to write perf test. */
 
+        protected static MonitorIngestionTestEnvironment TestEnvironment = new MonitorIngestionTestEnvironment();
+        protected readonly LogsIngestionClient LogsIngestionClient;
+        protected static List<Object> GenerateEntries(int numEntries)
+        {
+            var entries = new List<Object>();
+            for (int i = 0; i < numEntries; i++)
+            {
+                entries.Add(new Object[] {
+                    new {
+                        Time = DateTime.UtcNow,
+                        Computer = "Computer" + i.ToString(),
+                        AdditionalContext = i
+                    }
+                });
+            }
+            return entries;
+        }
+
         public IngestionClientTest(IngestionClientPerfOptions options) : base(options)
         {
+            LogsIngestionClient = new LogsIngestionClient(new Uri(TestEnvironment.DCREndpoint), TestEnvironment.Credential, ConfigureClientOptions(new LogsIngestionClientOptions()));
         }
         public class IngestionClientPerfOptions : PerfOptions
         {
@@ -23,14 +44,14 @@ namespace Azure.Monitor.Ingestion.Perf
 
         public override void Run(CancellationToken cancellationToken)
         {
+            LogsIngestionClient.Upload(TestEnvironment.DCRImmutableId, TestEnvironment.StreamName, GenerateEntries(100), null, cancellationToken);
         }
 
         public override async Task RunAsync(CancellationToken cancellationToken)
         {
-            await Task.Run(() =>
+            await Task.Run(async () =>
             {
-                Console.WriteLine("exec some async operation");
-                throw new NotImplementedException();
+                await LogsIngestionClient.UploadAsync(TestEnvironment.DCRImmutableId, TestEnvironment.StreamName, GenerateEntries(100), null, cancellationToken).ConfigureAwait(false);
             });
         }
     }
