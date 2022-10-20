b93033b53e4146222c57afbbb01e16a2b728c291
Anne Thompson <annelo@microsoft.com>
2022-10-20 08:35:21 -0700

    add abstract base class for manifest

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Blob/OciManifest.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Blob/OciManifest.cs
@@ -9,12 +9,9 @@ namespace Azure.Containers.ContainerRegistry.Specialized
     /// A manifest for an OCI (Open Container Initiative) artifact.
     /// </summary>
     [CodeGenModel("OCIManifest")]
-    public partial class OciManifest
+    public partial class OciManifest : ArtifactManifest
     {
         /// <summary> Additional information provided through arbitrary metadata. </summary>
         public OciAnnotations Annotations { get; }
-
-        /// <summary> Schema version. </summary>
-        public int? SchemaVersion { get; set; }
     }
 }
