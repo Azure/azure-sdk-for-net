09236b34d589f9b067a895e588c3835a71dc4832
Franko Morales <67804607+cochi2@users.noreply.github.com>
2022-10-20 11:41:00 -0700

    Fixing name for RecordingStateChanged event (#31909)

--- sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/RecordingStateChanged.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/RecordingStateChanged.cs
@@ -10,21 +10,21 @@ using System;
 namespace Azure.Communication.CallAutomation
 {
     /// <summary> The RecordingStateChanged. </summary>
-    public partial class CallRecordingStateChanged
+    public partial class RecordingStateChanged
     {
-        /// <summary> Initializes a new instance of CallRecordingStateChanged. </summary>
-        internal CallRecordingStateChanged()
+        /// <summary> Initializes a new instance of RecordingStateChanged. </summary>
+        internal RecordingStateChanged()
         {
         }
 
-        /// <summary> Initializes a new instance of CallRecordingStateChanged. </summary>
+        /// <summary> Initializes a new instance of RecordingStateChanged. </summary>
         /// <param name="callConnectionId"> Call connection ID. </param>
         /// <param name="serverCallId"> Server call ID. </param>
         /// <param name="correlationId"> Correlation ID for event to call correlation. Also called ChainId for skype chain ID. </param>
         /// <param name="recordingId"> The call recording id. </param>
         /// <param name="state"></param>
         /// <param name="startDateTime"> The time of the recording started. </param>
-        internal CallRecordingStateChanged(string callConnectionId, string serverCallId, string correlationId, string recordingId, RecordingState state, DateTimeOffset? startDateTime)
+        internal RecordingStateChanged(string callConnectionId, string serverCallId, string correlationId, string recordingId, RecordingState state, DateTimeOffset? startDateTime)
         {
             CallConnectionId = callConnectionId;
             ServerCallId = serverCallId;
