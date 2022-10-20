c3a94f5e71e3b100735d21214e26dd9bd6854494
Austin Tolani <46536757+austintolani@users.noreply.github.com>
2022-10-19 20:54:08 -0700

    Redis track 1 .NET SDK 2022-06-01 API version updates (#31799)
    * generate Redis SDK
    
    * Update ManagedIdentityTests.cs
    
    * Update BeginCreateFunctionalTests.cs
    
    * use 'latest' as redisVersion
    
    * begin update tests
    
    * Update PatchSchedulesFunctionalTests.cs
    
    * add new properties to CreateOrUpdateTests
    
    * Update GeoReplicationFunctionalTests.cs with async + sync method testing
    
    * Rename BeginCreateFunctionalTests -> BeginCreateBeginUpdateFunctionalTests
    
    * Prevent key sanitization
    
    * disable failing geo-replication tests
    
    * session recordings
    
    * Revert "Prevent key sanitization"
    
    This reverts commit 83c1e40ca2b7cb2676cf081a00e1e7384ea123a9.
    
    * sanitized keys in RegenerateKeyTest.json
    
    * sdk release info
    
    * santize keys
    
    * regenerate session records
    
    * edit key santization in RegenerateKeyTest.json
    
    * Update sdk/redis/Microsoft.Azure.Management.Redis/src/Microsoft.Azure.Management.Redis.csproj
    
    Co-authored-by: Yao Kou <yaokou@microsoft.com>
    
    Co-authored-by: Austin Tolani <austintolani@microsoft.com>
    Co-authored-by: Yao Kou <yaokou@microsoft.com>

--- sdk/redis/Microsoft.Azure.Management.Redis/tests/ScenarioTests/BeginCreateBeginUpdateFunctionalTests.cs
+++ sdk/redis/Microsoft.Azure.Management.Redis/tests/ScenarioTests/BeginCreateBeginUpdateFunctionalTests.cs
@@ -11,10 +11,10 @@ using Xunit;
 
 namespace AzureRedisCache.Tests
 {
-    public class BeginCreateFunctionalTests : TestBase
+    public class BeginCreateBeginUpdateFunctionalTests : TestBase
     {
         [Fact]
-        public void BeginCreateFunctionalTest()
+        public void BeginCreateBeginUpdateFunctionalTest()
         {
             using (var context = MockContext.Start(this.GetType()))
             {
@@ -38,22 +38,22 @@ namespace AzureRedisCache.Tests
                                             },
                                             MinimumTlsVersion = TlsVersion.OneFullStopTwo,
                                             ReplicasPerMaster = 2,
-                                            RedisVersion = "6",
+                                            RedisVersion = "latest",
                                             RedisConfiguration = new RedisCommonPropertiesRedisConfiguration(
                                                 maxmemoryPolicy: "allkeys-lru",
-                                            additionalProperties: new Dictionary<string, object>() { { "maxmemory-reserved", "210" } })
+                                            additionalProperties: new Dictionary<string, object>() { { "maxmemory-reserved", "700" } })
                                         });
 
                 Assert.Contains(redisCacheName, response.Id);
-                Assert.Equal("210", response.RedisConfiguration.MaxmemoryDelta);
-                Assert.Equal("210", response.RedisConfiguration.MaxmemoryReserved);
+                Assert.Equal("700", response.RedisConfiguration.MaxmemoryDelta);
+                Assert.Equal("700", response.RedisConfiguration.MaxmemoryReserved);
                 Assert.Equal(redisCacheName, response.Name);
                 Assert.Equal(ProvisioningState.Creating, response.ProvisioningState, ignoreCase: true);
                 Assert.Equal(SkuName.Premium, response.Sku.Name);
                 Assert.Equal(SkuFamily.P, response.Sku.Family);
                 Assert.Equal(TlsVersion.OneFullStopTwo, response.MinimumTlsVersion);
                 Assert.Equal(2, response.ReplicasPerMaster);
-                Assert.Equal("6", response.RedisVersion.Split(".")[0]);
+                Assert.Equal("6", response.RedisVersion.Split(".")[0]); // 6 is the current 'latest' version. Will change in the future. 
 
                 Assert.Equal(3, response.Instances.Count);
                 for (int i = 0; i < response.Instances.Count; i++)
@@ -73,6 +73,37 @@ namespace AzureRedisCache.Tests
                     }
                     TestUtilities.Wait(new TimeSpan(0, 0, 30));
                 }
+
+                Sku p2 = new Sku()
+                {
+                    Name = SkuName.Premium,
+                    Family = SkuFamily.P,
+                    Capacity = 2
+                };
+                RedisResource updateResponse = _client.Redis.BeginUpdate(resourceGroupName, redisCacheName, parameters: new RedisUpdateParameters
+                {
+
+                    Sku = p2,
+                    MinimumTlsVersion = TlsVersion.OneFullStopTwo,
+                    ReplicasPerMaster = 2,
+                    RedisVersion = "latest",
+                    RedisConfiguration = new RedisCommonPropertiesRedisConfiguration(
+                                                maxmemoryPolicy: "allkeys-lru",
+                                            additionalProperties: new Dictionary<string, object>() { { "maxmemory-reserved", "700" } })
+                });
+
+                Assert.Equal(ProvisioningState.Scaling, updateResponse.ProvisioningState);
+
+                for (int i = 0; i < 60; i++)
+                {
+                    updateResponse = _client.Redis.Get(resourceGroupName, redisCacheName);
+                    if (ProvisioningState.Succeeded.Equals(updateResponse.ProvisioningState, StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+                    TestUtilities.Wait(new TimeSpan(0, 0, 30));
+                }
+                Assert.Equal(p2.Capacity, updateResponse.Sku.Capacity);
 
                 _client.Redis.Delete(resourceGroupName: resourceGroupName, name: redisCacheName);
                 _redisCacheManagementHelper.DeleteResourceGroup(resourceGroupName);
