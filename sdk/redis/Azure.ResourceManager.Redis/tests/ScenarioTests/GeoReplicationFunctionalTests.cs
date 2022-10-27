8673159d39089c4ad810314766739174085edae0
Austin Tolani <austintolani@microsoft.com>
2022-10-27 13:59:38 -0700

    test primaryHostName

--- sdk/redis/Azure.ResourceManager.Redis/tests/ScenarioTests/GeoReplicationFunctionalTests.cs
+++ sdk/redis/Azure.ResourceManager.Redis/tests/ScenarioTests/GeoReplicationFunctionalTests.cs
@@ -68,6 +68,7 @@ namespace Azure.ResourceManager.Redis.Tests
             Assert.AreEqual(AzureLocation.SouthCentralUS, linkServerWithProperties.Data.LinkedRedisCacheLocation);
             Assert.AreEqual(RedisLinkedServerRole.Secondary, linkServerWithProperties.Data.ServerRole);
             Assert.False(string.IsNullOrEmpty(linkServerWithProperties.Data.GeoReplicatedPrimaryHostName));
+            Assert.False(string.IsNullOrEmpty(linkServerWithProperties.Data.PrimaryHostName));
 
             // test get response from primary
             var primaryLinkProperties = (await linkCollection.GetAsync(redisCacheName2)).Value;
