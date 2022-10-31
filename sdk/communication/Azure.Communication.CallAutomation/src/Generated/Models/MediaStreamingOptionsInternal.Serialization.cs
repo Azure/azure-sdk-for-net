4fb633249a7763ae832101c1ee16fce9ba31c89d
Min Woo Lee 🧊 <77083090+minwoolee-ms@users.noreply.github.com>
2022-10-30 22:12:33 -0700

    Hiding all media streaming and fixing tests

--- sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/MediaStreamingOptionsInternal.Serialization.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/MediaStreamingOptionsInternal.Serialization.cs
@@ -10,7 +10,7 @@ using Azure.Core;
 
 namespace Azure.Communication.CallAutomation
 {
-    public partial class MediaStreamingConfiguration : IUtf8JsonSerializable
+    internal partial class MediaStreamingOptionsInternal : IUtf8JsonSerializable
     {
         void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
         {
--- sdk/communication/Azure.Communication.CallAutomation/src/Models/MediaStreamingOptionsInternal.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/MediaStreamingOptionsInternal.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Azure.Core;
+
 namespace Azure.Communication.CallAutomation
 {
-    /// <summary>
-    /// Media Streaming Package.
-    /// </summary>
-    public abstract class MediaStreamingPackageBase
+    [CodeGenModel("MediaStreamingConfiguration")]
+    internal partial class MediaStreamingOptionsInternal
     {
     }
 }
