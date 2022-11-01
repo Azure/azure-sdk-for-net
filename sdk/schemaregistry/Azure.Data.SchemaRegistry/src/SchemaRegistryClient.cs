d017c4e620e2bada8f24d652602c94dfa0b455ee
Madalyn Redding <mredding@microsoft.com>
2022-11-01 16:14:13 -0700

    format -> content type

--- sdk/schemaregistry/Azure.Data.SchemaRegistry/src/SchemaRegistryClient.cs
+++ sdk/schemaregistry/Azure.Data.SchemaRegistry/src/SchemaRegistryClient.cs
@@ -125,11 +125,11 @@ namespace Azure.Data.SchemaRegistry
                 ResponseWithHeaders<SchemaRegisterHeaders> response;
                 if (async)
                 {
-                    response = await RestClient.RegisterAsync(groupName, schemaName, format.ContentType, new BinaryData(schemaDefinition).ToStream(), cancellationToken).ConfigureAwait(false);
+                    response = await RestClient.RegisterAsync(groupName, schemaName, ConstructContentType(format), new BinaryData(schemaDefinition).ToStream(), cancellationToken).ConfigureAwait(false);
                 }
                 else
                 {
-                    response = RestClient.Register(groupName, schemaName,format.ContentType, new BinaryData(schemaDefinition).ToStream(), cancellationToken);
+                    response = RestClient.Register(groupName, schemaName, ConstructContentType(format) , new BinaryData(schemaDefinition).ToStream(), cancellationToken);
                 }
 
                 var properties = new SchemaProperties(format, response.Headers.SchemaId, response.Headers.SchemaGroupName, response.Headers.SchemaName, response.Headers.SchemaVersion!.Value);
@@ -197,11 +197,11 @@ namespace Azure.Data.SchemaRegistry
                 ResponseWithHeaders<SchemaQueryIdByContentHeaders> response;
                 if (async)
                 {
-                    response = await RestClient.QueryIdByContentAsync(groupName, schemaName, format.ContentType, new BinaryData(schemaDefinition).ToStream(), cancellationToken).ConfigureAwait(false);
+                    response = await RestClient.QueryIdByContentAsync(groupName, schemaName, ConstructContentType(format) , new BinaryData(schemaDefinition).ToStream(), cancellationToken).ConfigureAwait(false);
                 }
                 else
                 {
-                    response = RestClient.QueryIdByContent(groupName, schemaName, format.ContentType, new BinaryData(schemaDefinition).ToStream(), cancellationToken);
+                    response = RestClient.QueryIdByContent(groupName, schemaName, ConstructContentType(format), new BinaryData(schemaDefinition).ToStream(), cancellationToken);
                 }
 
                 var properties = new SchemaProperties(format, response.Headers.SchemaId, response.Headers.SchemaGroupName, response.Headers.SchemaName, response.Headers.SchemaVersion!.Value);
