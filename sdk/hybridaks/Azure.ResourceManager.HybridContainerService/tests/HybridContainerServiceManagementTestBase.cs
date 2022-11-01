63738a60e3235000ed6bd56f04efa94bd9e943b6
HariPrasadV <hariv97@gmail.com>
2022-11-01 14:53:53 -0700

    Added vnet creation test

--- sdk/hybridaks/Azure.ResourceManager.HybridContainerService/tests/HybridContainerServiceManagementTestBase.cs
+++ sdk/hybridaks/Azure.ResourceManager.HybridContainerService/tests/HybridContainerServiceManagementTestBase.cs
@@ -13,6 +13,9 @@ namespace Azure.ResourceManager.HybridContainerService.Tests
     public class HybridContainerServiceManagementTestBase : ManagementRecordedTestBase<HybridContainerServiceManagementTestEnvironment>
     {
         protected ArmClient Client { get; private set; }
+        protected SubscriptionResource Subscription { get; private set; }
+        protected ResourceGroupResource ResourceGroup { get; private set; }
+        protected AzureLocation DefaultLocation => AzureLocation.WestEurope;
 
         protected HybridContainerServiceManagementTestBase(bool isAsync, RecordedTestMode mode)
         : base(isAsync, mode)
@@ -25,14 +28,15 @@ namespace Azure.ResourceManager.HybridContainerService.Tests
         }
 
         [SetUp]
-        public void CreateCommonClient()
+        protected async Task InitializeClient()
         {
             Client = GetArmClient();
+            Subscription = await Client.GetDefaultSubscriptionAsync().ConfigureAwait(false);
+            ResourceGroup = await CreateResourceGroup(Subscription, "hybridaksresgrp-netsdk", DefaultLocation);
         }
 
-        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgName, AzureLocation location)
         {
-            string rgName = Recording.GenerateAssetName(rgNamePrefix);
             ResourceGroupData input = new ResourceGroupData(location);
             var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
             return lro.Value;
---
+++ sdk/hybridaks/Azure.ResourceManager.HybridContainerService/tests/Tests/ProvisionedClusterTests.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.HybridContainerService.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.HybridContainerService.Tests.Tests
+{
+    [TestFixture]
+    public class ProvisionedClusterTests : HybridContainerServiceManagementTestBase
+    {
+        public ProvisionedClusterTests() : base(true)
+        {
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public void ListClustersInResourceGroup()
+        {
+            // ResourceIdentifier id = ProvisionedClustersResponseResource.CreateResourceIdentifier(Subscription.Data.SubscriptionId, ResourceGroup.Data.Name, "test-cluster");
+            ProvisionedClustersResponseCollection clusterCollection = new ProvisionedClustersResponseCollection(Client, ResourceGroup.Id);
+            var provisionedClusters = clusterCollection.GetAll();
+            Assert.AreEqual(provisionedClusters.ToList().Count, 0);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public void ListVnetsInResourceGroup()
+        {
+            VirtualNetworkCollection vnetCollection = new VirtualNetworkCollection(Client, ResourceGroup.Id);
+            var vnets = vnetCollection.GetAll();
+            Assert.AreEqual(vnets.ToList().Count, 0);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public void CreateVnet()
+        {
+            VirtualNetworkCollection vnetCollection = new VirtualNetworkCollection(Client, ResourceGroup.Id);
+            VirtualNetworkData vnetData = new VirtualNetworkData(DefaultLocation);
+            vnetData.ExtendedLocation = new VirtualNetworksExtendedLocation();
+            vnetData.ExtendedLocation.VirtualNetworksExtendedLocationType = "CustomLocation";
+            vnetData.ExtendedLocation.Name = "/subscriptions/0709bd7a-8383-4e1d-98c8-f81d1b3443fc/resourcegroups/hybridaksresgrp-1945484400/providers/microsoft.extendedlocation/customlocations/applhybridaks-1945484400-hybridaks-cl";
+            vnetData.Properties = new VirtualNetworksProperties();
+            vnetData.Properties.InfraVnetProfile = new VirtualNetworksPropertiesInfraVnetProfile();
+            vnetData.Properties.InfraVnetProfile.Hci = new VirtualNetworksPropertiesInfraVnetProfileHci();
+            vnetData.Properties.InfraVnetProfile.Hci.MocVnetName = "hybridaks-vnet";
+            vnetData.Properties.InfraVnetProfile.Hci.MocGroup = "target-group";
+            vnetData.Properties.InfraVnetProfile.Hci.MocLocation = "MocLocation";
+            var vnet = vnetCollection.CreateOrUpdate(WaitUntil.Completed, "azvnet-sdk", vnetData);
+            Assert.AreEqual(vnet.Value.Data.Properties.ProvisioningState, ProvisioningState.Succeeded);
+        }
+    }
+}
