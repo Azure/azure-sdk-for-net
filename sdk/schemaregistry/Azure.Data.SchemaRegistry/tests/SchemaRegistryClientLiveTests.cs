6a9318d2b19ee535834b39c4987d50e61d9c24bd
Madalyn Redding <mredding@microsoft.com>
2022-11-04 12:19:16 -0700

    removing test mode

--- sdk/schemaregistry/Azure.Data.SchemaRegistry/tests/SchemaRegistryClientLiveTests.cs
+++ sdk/schemaregistry/Azure.Data.SchemaRegistry/tests/SchemaRegistryClientLiveTests.cs
@@ -13,7 +13,7 @@ namespace Azure.Data.SchemaRegistry.Tests
 {
     public class SchemaRegistryClientLiveTests : RecordedTestBase<SchemaRegistryClientTestEnvironment>
     {
-        public SchemaRegistryClientLiveTests(bool isAsync) : base(isAsync, RecordedTestMode.Live)
+        public SchemaRegistryClientLiveTests(bool isAsync) : base(isAsync)
         {
             TestDiagnostics = false;
         }
