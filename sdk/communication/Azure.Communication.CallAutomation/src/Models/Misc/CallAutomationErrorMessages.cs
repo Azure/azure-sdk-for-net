81d5082ec7134c5be9cde325b7026447c99656a6
Min Woo Lee 🧊 <77083090+minwoolee-ms@users.noreply.github.com>
2022-11-04 15:27:33 -0700

    Updated operationContext error msg

--- sdk/communication/Azure.Communication.CallAutomation/src/Models/Misc/CallAutomationErrorMessages.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/Misc/CallAutomationErrorMessages.cs
@@ -26,6 +26,6 @@ namespace Azure.Communication.CallAutomation
         /// <summary>
         /// OperationContext exceeds max length message.
         /// </summary>
-        public const string OperationContextExceedsMaxLength = "UserToUserInformation exceeds maximum string length of 5000.";
+        public const string OperationContextExceedsMaxLength = "OperationContext exceeds maximum string length of 5000.";
     }
 }
