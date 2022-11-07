93e06b5ea4f22343270b959d105d619e9dd7020e
yifat <yibirnba@microsoft.com>
2022-11-07 11:04:05 +0200

    Add more information about breaking changes

--- sdk/securitycenter/Microsoft.Azure.Management.SecurityCenter/src/Properties/AssemblyInfo.cs
+++ sdk/securitycenter/Microsoft.Azure.Management.SecurityCenter/src/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Resources;
 [assembly: AssemblyTitle("Microsoft Azure Security Center Library")]
 [assembly: AssemblyDescription("Provides management functionality for Microsoft Azure Security Center Resources.")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Microsoft Azure .NET SDK")]
