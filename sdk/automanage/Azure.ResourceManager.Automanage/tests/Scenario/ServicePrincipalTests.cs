14b7dccf28f1ed88e03b8c615ff49a8dcd81cf3e
andrew <andrsmith@microsoft.com>
2022-11-09 18:10:09 -0500

    Add service principal unit test

---
+++ sdk/automanage/Azure.ResourceManager.Automanage/tests/Scenario/ServicePrincipalTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Automanage.Tests.Scenario
+{
+    internal class ServicePrincipalTests : AutomanageTestBase
+    {
+        public ServicePrincipalTests(bool async) : base(async) { }
+
+        [TestCase]
+        public async Task CanGetServicePrincipal()
+        {
+            // arrange
+            string profileName = Recording.GenerateAssetName("SDKAutomanageProfile-");
+
+            // act
+            var rg = await CreateResourceGroup("SDKAutomanage-", DefaultLocation);
+            var servicePrincipal = Subscription.GetServicePrincipal();
+
+            // assert
+            Assert.IsNotNull(servicePrincipal);
+        }
+    }
+}
