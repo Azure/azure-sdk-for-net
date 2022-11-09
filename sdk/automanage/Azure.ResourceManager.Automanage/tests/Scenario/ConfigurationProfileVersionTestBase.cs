3691a65a27a275ec6de4ed08a9afbb8f608d9a34
andrew <andrsmith@microsoft.com>
2022-11-09 11:34:23 -0500

    Add class

---
+++ sdk/automanage/Azure.ResourceManager.Automanage/tests/Scenario/ConfigurationProfileVersionTestBase.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Automanage.Tests.Scenario
+{
+    internal class ConfigurationProfileVersionTestBase : AutomanageTestBase
+    {
+        protected ConfigurationProfileVersionTestBase(bool isAsync, RecordedTestMode mode) : base(isAsync, mode)
+        {
+        }
+
+        /// <summary>
+        /// Asserts multiple values
+        /// </summary>
+        /// <param name="version">ConfigurationProfileVersionResource to assert</param>
+        /// <param name="versionName">ConfigurationProfileVersionResource name to verify</param>
+        protected void AssertValues(ConfigurationProfileVersionResource version, string versionName)
+        {
+            Assert.NotNull(version);
+            Assert.True(version.HasData);
+            Assert.AreEqual(versionName, version.Id.Name);
+            Assert.NotNull(version.Id);
+            Assert.NotNull(version.Data);
+            Assert.NotNull(version.Data.Configuration);
+        }
+    }
+}
