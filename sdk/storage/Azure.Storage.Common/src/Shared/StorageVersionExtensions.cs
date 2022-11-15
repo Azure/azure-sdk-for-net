14eba8cc8b7733cd6b6e5cf735dec20a80965b2b
Sean McCullough <seanmcc@microsoft.com>
2022-11-15 15:17:23 -0600

    Fixed issue where Latest and Max version were swapped

--- sdk/storage/Azure.Storage.Common/src/Shared/StorageVersionExtensions.cs
+++ sdk/storage/Azure.Storage.Common/src/Shared/StorageVersionExtensions.cs
@@ -36,7 +36,7 @@ namespace Azure.Storage
         /// </summary>
         public const ServiceVersion LatestVersion =
 #if BlobSDK || QueueSDK || FileSDK || DataLakeSDK || ChangeFeedSDK
-            ServiceVersion.V2022_11_02;
+            ServiceVersion.V2021_12_02;
 #else
             ERROR_STORAGE_SERVICE_NOT_DEFINED;
 #endif
@@ -46,7 +46,7 @@ namespace Azure.Storage
         /// </summary>
         internal const ServiceVersion MaxVersion =
 #if BlobSDK || QueueSDK || FileSDK || DataLakeSDK || ChangeFeedSDK
-            ServiceVersion.V2021_12_02;
+            ServiceVersion.V2022_11_02;
 #else
             ERROR_STORAGE_SERVICE_NOT_DEFINED;
 #endif
