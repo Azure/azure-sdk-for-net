b33e38a1a53524e2fe4444c6d43e145871b194ad
mcgallan <1186003288@qq.com>
2022-11-15 15:10:37 +0800

    finshed

--- sdk/securityinsights/Azure.ResourceManager.SecurityInsights/tests/Helpers/ResourceDataHelpers.cs
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/tests/Helpers/ResourceDataHelpers.cs
@@ -11,7 +11,6 @@ using System;
 using System.Text;
 using Azure.ResourceManager.Resources;
 using System.Threading.Tasks;
-using ContentType = Azure.ResourceManager.SecurityInsights.Models.ContentType;
 using NUnit.Framework.Internal;
 using Azure.ResourceManager.Logic;
 using Azure.ResourceManager.Logic.Models;
@@ -182,115 +181,8 @@ namespace Azure.ResourceManager.SecurityInsights.Tests.Helpers
         {
             var data = new ASCDataConnector()
             {
-                Alerts = new DataConnectorDataTypeCommon(DataTypeState.Enabled),
-                SubscriptionId = "db1ab6f0-4769-4b27-930e-01e2ef9c123c"
-            };
-            return data;
-        }
-        #endregion
-        #region EntityQueryData
-        public static void AssertEntityQueryData(EntityQueryData data1, EntityQueryData data2)
-        {
-            AssertResource(data1, data2);
-            Assert.AreEqual(data1.Kind, data2.Kind);
-        }
-        public static EntityQueryCreateOrUpdateContent GetEntityQueryData()
-        {
-            var data = new ActivityCustomEntityQuery()
-            {
-                Title = "The user consented to OAuth application",
-                InputEntityType = "Account",
-                Content = "The user consented to the OAuth application named {{Target_CloudApplication_Name}} {{Count}} time(s)",
-                Description = "This activity lists user's consents to an OAuth applications.",
-                QueryDefinitions = new ActivityEntityQueriesPropertiesQueryDefinitions()
-                {
-                    Query = "let UserConsentToApplication = (Account_Name:string, Account_UPNSuffix:string, Account_AadUserId:string){\nlet account_upn = iff(Account_Name != \"\" and Account_UPNSuffix != \"\"\n, strcat(Account_Name,\"@\",Account_UPNSuffix)\n,\"\" );\nAuditLogs\n| where OperationName == \"Consent to application\"\n| extend Source_Account_UPNSuffix = tostring(todynamic(InitiatedBy) [\"user\"][\"userPrincipalName\"]), Source_Account_AadUserId = tostring(todynamic(InitiatedBy) [\"user\"][\"id\"])\n| where (account_upn != \"\" and account_upn =~ Source_Account_UPNSuffix) \nor (Account_AadUserId != \"\" and Account_AadUserId =~ Source_Account_AadUserId)\n| extend Target_CloudApplication_Name = tostring(todynamic(TargetResources)[0][\"displayName\"]), Target_CloudApplication_AppId = tostring(todynamic(TargetResources)[0][\"id\"])\n};\nUserConsentToApplication('{{Account_Name}}', '{{Account_UPNSuffix}}', '{{Account_AadUserId}}')  \n| project Target_CloudApplication_AppId, Target_CloudApplication_Name, TimeGenerated"
-                },
-                RequiredInputFieldsSets =
-                {
-                    new List<string>()
-                    {
-                        "Account_AadUserId"
-                    },
-                    new List<string>()
-                    {
-                        "Account_Name",
-                        "Account_UPNSuffix"
-                    }
-                }
-            };
-            return data;
-        }
-        #endregion
-        #region MetadataModelData
-        public static void AssertMetadataModelData(MetadataModelData data1, MetadataModelData data2)
-        {
-            AssertResource(data1, data2);
-            Assert.AreEqual(data1.ContentId, data2.ContentId);
-            Assert.AreEqual(data1.ParentId, data2.ParentId);
-            Assert.AreEqual(data1.Version, data2.Version);
-            Assert.AreEqual(data1.Icon, data2.Icon);
-            Assert.AreEqual(data1.Kind, data2.Kind);
-        }
-        public static MetadataModelData GetMetadataModelData(string defaultSubscription, string resourceGroup, string workspaceName)
-        {
-            var data = new MetadataModelData()
-            {
-                Kind = SecurityInsightsKind.AnalyticsRule,
-                ParentId = defaultSubscription + "/resourceGroups/"+ resourceGroup + "/providers/" + "Microsoft.OperationalInsights" + "/workspaces/" + workspaceName + "/providers/Microsoft.SecurityInsights/alertRules/" + "9c251d58-a711-4883-8",
-                ContentId = "ea26fc04-3702-46be-8",
-            };
-            return data;
-        }
-        #endregion
-        #region SettingData
-        public static void AssertSettingData(SettingData data1, SettingData data2)
-        {
-            AssertResource(data1, data2);
-            Assert.AreEqual(data1.Kind, data2.Kind);
-        }
-        public static SettingData GetSettingData()
-        {
-            /*var data = new Ueba()
-            {
-                DataSources =
-                {
-                    UebaDataSource.AuditLogs
-                },
-                ETag = ETag.All
-            };*/
-            var data = new EyesOn()
-            {
-            };
-            return data;
-        }
-        #endregion
-
-        #region SourceControlData(?)
-        public static void AssertSourceControlData(SourceControlData data1, SourceControlData data2)
-        {
-            AssertResource(data1, data2);
-            Assert.AreEqual(data1.DisplayName, data2.DisplayName);
-            Assert.AreEqual(data1.Description, data2.Description);
-            Assert.AreEqual(data1.Version, data2.Version);
-            Assert.AreEqual(data1.IdPropertiesId, data2.IdPropertiesId);
-            Assert.AreEqual(data1.RepoType, data2.RepoType);
-        }
-        public static SourceControlData GetSourceControlData()
-        {
-            var data = new SourceControlData()
-            {
-                DisplayName = "SDK Test",
-                RepoType = RepoType.Github,
-                ContentTypes =
-                {
-                    ContentType.Workbook,
-                },
-                Repository = new Repository()
-                {
-                    Branch = "main",
-                    Uri = new Uri("https://github.com/SecCxPNinja/Ninja")
-                }
+                SubscriptionId = "db1ab6f0-4769-4b27-930e-01e2ef9c123c",
+                DataTypes = new AlertsDataTypeOfDataConnector(new DataConnectorDataTypeCommon(DataTypeState.Enabled))
             };
             return data;
         }
@@ -336,10 +228,10 @@ namespace Azure.ResourceManager.SecurityInsights.Tests.Helpers
         {
             return new WatchlistItemData()
             {
-                ItemsKeyValue =
-                {
+                ItemsKeyValue = BinaryData.FromString("{\"ipaddress\":\"1.1.1.2\"}")
+                /*{
                     {"ipaddress", BinaryData.FromString("\"1.1.1.2\"")}
-                }
+                }*/
             };
         }
         #endregion
