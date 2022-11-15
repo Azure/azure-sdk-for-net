7d6d7c0b2b0e6af568a90f5a55fa39a44ae9ba0e
Sean McCullough <44180881+seanmcc-msft@users.noreply.github.com>
2022-11-15 10:06:18 -0600

    Storage STG 85/86 (#31840)

--- sdk/storage/Azure.Storage.Files.Shares/src/Generated/Models/ListFilesAndDirectoriesSegmentResponse.cs
+++ sdk/storage/Azure.Storage.Files.Shares/src/Generated/Models/ListFilesAndDirectoriesSegmentResponse.cs
@@ -21,7 +21,7 @@ namespace Azure.Storage.Files.Shares.Models
         /// <param name="segment"> Abstract for entries that can be listed from Directory. </param>
         /// <param name="nextMarker"></param>
         /// <exception cref="ArgumentNullException"> <paramref name="serviceEndpoint"/>, <paramref name="shareName"/>, <paramref name="directoryPath"/>, <paramref name="prefix"/>, <paramref name="segment"/> or <paramref name="nextMarker"/> is null. </exception>
-        internal ListFilesAndDirectoriesSegmentResponse(string serviceEndpoint, string shareName, string directoryPath, string prefix, FilesAndDirectoriesListSegment segment, string nextMarker)
+        internal ListFilesAndDirectoriesSegmentResponse(string serviceEndpoint, string shareName, string directoryPath, StringEncoded prefix, FilesAndDirectoriesListSegment segment, string nextMarker)
         {
             Argument.AssertNotNull(serviceEndpoint, nameof(serviceEndpoint));
             Argument.AssertNotNull(shareName, nameof(shareName));
@@ -42,6 +42,7 @@ namespace Azure.Storage.Files.Shares.Models
         /// <param name="serviceEndpoint"></param>
         /// <param name="shareName"></param>
         /// <param name="shareSnapshot"></param>
+        /// <param name="encoded"></param>
         /// <param name="directoryPath"></param>
         /// <param name="prefix"></param>
         /// <param name="marker"></param>
@@ -49,11 +50,12 @@ namespace Azure.Storage.Files.Shares.Models
         /// <param name="segment"> Abstract for entries that can be listed from Directory. </param>
         /// <param name="nextMarker"></param>
         /// <param name="directoryId"></param>
-        internal ListFilesAndDirectoriesSegmentResponse(string serviceEndpoint, string shareName, string shareSnapshot, string directoryPath, string prefix, string marker, int? maxResults, FilesAndDirectoriesListSegment segment, string nextMarker, string directoryId)
+        internal ListFilesAndDirectoriesSegmentResponse(string serviceEndpoint, string shareName, string shareSnapshot, bool? encoded, string directoryPath, StringEncoded prefix, string marker, int? maxResults, FilesAndDirectoriesListSegment segment, string nextMarker, string directoryId)
         {
             ServiceEndpoint = serviceEndpoint;
             ShareName = shareName;
             ShareSnapshot = shareSnapshot;
+            Encoded = encoded;
             DirectoryPath = directoryPath;
             Prefix = prefix;
             Marker = marker;
@@ -69,10 +71,12 @@ namespace Azure.Storage.Files.Shares.Models
         public string ShareName { get; }
         /// <summary> Gets the share snapshot. </summary>
         public string ShareSnapshot { get; }
+        /// <summary> Gets the encoded. </summary>
+        public bool? Encoded { get; }
         /// <summary> Gets the directory path. </summary>
         public string DirectoryPath { get; }
         /// <summary> Gets the prefix. </summary>
-        public string Prefix { get; }
+        public StringEncoded Prefix { get; }
         /// <summary> Gets the marker. </summary>
         public string Marker { get; }
         /// <summary> Gets the max results. </summary>
