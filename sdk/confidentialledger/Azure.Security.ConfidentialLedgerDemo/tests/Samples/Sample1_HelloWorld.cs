d8fcdf4d72ca23a49e2402b643811ce00de7e8f0
azure-sdk <no-reply@azure-sdk.com>
2022-11-16 03:01:31 +0000

    [auto]create by sdk generation

---
+++ sdk/confidentialledger/Azure.Security.ConfidentialLedgerDemo/tests/Samples/Sample1_HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.Security.ConfidentialLedgerDemo.Tests.Samples
+{
+    public partial class ConfidentialLedgerDemoSamples: SamplesBase<ConfidentialLedgerDemoClientTestEnvironment>
+    {
+        /* please refer to https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/template/Azure.Template/tests/Samples/Sample1.HelloWorld.cs to write samples. */
+        #region Snippet:Azure_Security_ConfidentialLedgerDemo_Scenario
+
+        #endregion
+    }
+}
