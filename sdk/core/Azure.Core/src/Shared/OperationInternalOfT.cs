ba786a7cc706ad06769f1b8bdd2d0a1084778e63
Feng Zhou <fenzhou@microsoft.com>
2022-11-16 13:47:48 +0800

    make RawResponse non-nullable

--- sdk/core/Azure.Core/src/Shared/OperationInternalOfT.cs
+++ sdk/core/Azure.Core/src/Shared/OperationInternalOfT.cs
@@ -6,8 +6,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading;
