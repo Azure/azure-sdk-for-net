3d169238189ae2e8ffafd926748b6537f2fd737b
m-nash <prognash@gmail.com>
2022-11-17 14:27:15 -0800

    address feedback

--- eng/scripts/Cadl-Project-Sync.ps1
+++ eng/scripts/Cadl-Project-Sync.ps1
@@ -6,8 +6,11 @@ param (
 )
 
 $ErrorActionPreference = "Stop"
+. $PSScriptRoot/../common/scripts/Helpers/PSModule-Helpers.ps1
+Install-ModuleIfNotInstalled "powershell-yaml" "0.4.1" | Import-Module
 
-function AddSparseCheckoutPath([string]$subDirectory, [string]$file) {
+function AddSparseCheckoutPath([string]$subDirectory) {
+    $file = ".git/info/sparse-checkout"
     if (!(Test-Path $file) -or !((Get-Content $file).Contains($subDirectory))) {
         Write-Output $subDirectory >> .git/info/sparse-checkout
     }
@@ -15,18 +18,21 @@ function AddSparseCheckoutPath([string]$subDirectory, [string]$file) {
 
 function NpmInstallForProject([string]$workingDirectory) {
     Push-Location $workingDirectory
-
-    $currentDur = Resolve-Path "."
-    Write-Host "Generating from $currentDur"
-    if (Test-Path ".\package.json") {
-        Remove-Item -Path ".\package.json" -Force
+    try {
+        $currentDur = Resolve-Path "."
+        Write-Host "Generating from $currentDur"
+        if (Test-Path "package.json") {
+            Remove-Item -Path "package.json" -Force
+        }
+        if (Test-Path ".npmrc") {
+            Remove-Item -Path ".npmrc" -Force
+        }
+        npm install "@azure-tools/cadl-csharp"
+        if ($LASTEXITCODE) { exit $LASTEXITCODE }
     }
-    if (Test-Path ".\.npmrc") {
-        Remove-Item -Path ".\.npmrc" -Force
+    finally {
+        Pop-Location
     }
-    npm install "@azure-tools/cadl-csharp"
-
-    Pop-Location
 }
 
 function CopySpecToProjectIfNeeded([string]$pullResult, [string]$specCloneRoot, [string]$mainSpecDir, [string]$dest, [string[]]$specAdditionalSubDirectories) {
@@ -45,26 +51,33 @@ function CopySpecToProjectIfNeeded([string]$pullResult, [string]$specCloneRoot, 
 }
 
 function UpdateSparseCheckoutFile([string]$mainSpecDir, [string[]]$specAdditionalSubDirectories) {
-    $sparseCheckoutFile = ".git/info/sparse-checkout"
-    AddSparseCheckoutPath $mainSpecDir $sparseCheckoutFile
+    AddSparseCheckoutPath $mainSpecDir
     foreach($subDir in $specAdditionalSubDirectories)
     {
-        AddSparseCheckoutPath $subDir $sparseCheckoutFile
+        AddSparseCheckoutPath $subDir
     }
 }
 
 function InitializeSparseGitClone([string]$repo) {
     if (!(Test-Path ".git")) {
         git init
+        if ($LASTEXITCODE) { exit $LASTEXITCODE }
         git remote add origin $repo
+        if ($LASTEXITCODE) { exit $LASTEXITCODE }
         git config core.sparseCheckout true
+        if ($LASTEXITCODE) { exit $LASTEXITCODE }
     }
 }
 
 function GetSpecCloneDir([string]$projectName) {
     Push-Location $ProjectDirectory
-    $root = git rev-parse --show-toplevel
-    Pop-Location
+    try {
+        $root = git rev-parse --show-toplevel
+    }
+    finally {
+        Pop-Location
+    }
+
     $sparseSpecCloneDir = "$root/../temp/$projectName"
     New-Item $sparseSpecCloneDir -Type Directory -Force | Out-Null
     $createResult = Resolve-Path $sparseSpecCloneDir
@@ -75,27 +88,36 @@ $cadlConfigurationFile = Resolve-Path "$ProjectDirectory/src/cadl-location.yaml"
 Write-Host "Reading configuration from $cadlConfigurationFile"
 $configuration = Get-Content -Path $cadlConfigurationFile -Raw | ConvertFrom-Yaml
 
-$pieces = $cadlConfigurationFile.Path.Split("\")
+$pieces = $cadlConfigurationFile.Path.Replace("\","/").Split("/")
 $projectName = $pieces[$pieces.Count - 3]
 
 $specCloneDir = GetSpecCloneDir $projectName
 
 Write-Host "Setting up sparse clone for $projectName"
 Push-Location $specCloneDir.Path
-InitializeSparseGitClone $configuration["repo"]
+try {
+    InitializeSparseGitClone $configuration["repo"]
 
-$specSubDirectory = $configuration["directory"]
-UpdateSparseCheckoutFile $specSubDirectory $configuration["additionalDirectories"]
+    $specSubDirectory = $configuration["directory"]
+    UpdateSparseCheckoutFile $specSubDirectory $configuration["additionalDirectories"]
 
-$result = (git pull origin $configuration["commit"])
-Write-Host $result
-Pop-Location
+    $result = (git pull origin $configuration["commit"])
+    Write-Host $result
+}
+finally {
+    Pop-Location
+}
 
 $specDir = Resolve-Path "$specCloneDir/$specSubDirectory"
 
 $tempCadlDir = "$ProjectDirectory/temp"
 New-Item $tempCadlDir -Type Directory -Force | Out-Null
-CopySpecToProjectIfNeeded $result $specCloneDir $specSubDirectory $tempCadlDir $configuration["additionalDirectories"]
+CopySpecToProjectIfNeeded `
+    -pullResult $result `
+    -specCloneRoot $specCloneDir `
+    -mainSpecDir $specSubDirectory `
+    -dest $tempCadlDir `
+    -specAdditionalSubDirectories $configuration["additionalDirectories"]
 
 $innerFolder = Split-Path $specDir -Leaf
-NpmInstallForProject (Resolve-Path "$tempCadlDir\$innerFolder")
+NpmInstallForProject (Resolve-Path "$tempCadlDir/$innerFolder")
