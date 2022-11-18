42e97df809fd4b1f57e9016b7805878dee6e6e31
Yang Chen <yache@microsoft.com>
2022-11-17 22:43:33 -0800

    Update Face to 2.7.0-preview.3

--- sdk/cognitiveservices/Vision.Face/src/Generated/Models/Snapshot.cs
+++ sdk/cognitiveservices/Vision.Face/src/Generated/Models/Snapshot.cs
@@ -18,7 +18,6 @@ namespace Microsoft.Azure.CognitiveServices.Vision.Face.Models
 
     /// <summary>
     /// Snapshot object.
-    /// </summary>
     public partial class Snapshot
     {
         /// <summary>
