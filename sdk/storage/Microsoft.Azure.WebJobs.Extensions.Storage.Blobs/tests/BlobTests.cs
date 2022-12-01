3cab5a7865f413c4b641d760e55182c7f30ff915
Surgupta <surgupta@microsoft.com>
2022-12-01 10:39:10 -0800

    Deleting unused build file

--- sdk/storage/Microsoft.Azure.WebJobs.Extensions.Storage.Blobs/tests/BlobTests.cs
+++ sdk/storage/Microsoft.Azure.WebJobs.Extensions.Storage.Blobs/tests/BlobTests.cs
@@ -224,6 +224,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.Storage.Blobs
                 b.AddAzureStorageQueues();
             }, programType,
             settings: new Dictionary<string, string>() {
+                // This takes precedence over env variables.
                 { "ConnectionStrings:AzureWebJobsStorage", AzuriteNUnitFixture.Instance.GetAzureAccount().ConnectionString }
             });
         }
