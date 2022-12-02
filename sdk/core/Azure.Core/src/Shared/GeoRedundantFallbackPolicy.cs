c3b1da54f654ab77dba5e11842e5d04658fdee1c
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2022-12-01 21:08:21 -0800

    Prototype GeoRedundantPolicy

---
+++ sdk/core/Azure.Core/src/Shared/GeoRedundantFallbackPolicy.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Azure.Core.Pipeline;
+
+namespace Azure.Core.Shared
+{
+    /// <summary>
+    ///
+    /// </summary>
+    internal class GeoRedundantReadFallbackPolicy : HttpPipelineSynchronousPolicy
+    {
+        private readonly string[] _readFallbackHosts;
+        private int? _fallbackIndex;
+        private readonly object _syncLock = new();
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="readFallbackHosts"></param>
+        public GeoRedundantReadFallbackPolicy(string[] readFallbackHosts)
+        {
+            _readFallbackHosts = readFallbackHosts;
+        }
+
+        public static void SetHostAffinity(HttpMessage message, bool hostAffinity)
+        {
+            message.SetInternalProperty(typeof(GeoRedundantReadFallbackPolicy), hostAffinity);
+        }
+
+        public static bool GetHostAffinity(HttpMessage message)
+        {
+            return message.TryGetInternalProperty(typeof(GeoRedundantReadFallbackPolicy), out object? hostAffinity) && hostAffinity is true;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="message"></param>
+        public override void OnSendingRequest(HttpMessage message)
+        {
+            // TODO implement policy for write hosts as well (or include in this policy)
+            if (message.Request.Method != RequestMethod.Get && message.Request.Method != RequestMethod.Head)
+                return;
+
+            // must be under lock so that this can be synchronized across client calls - each call should leverage the fallback information
+            // from all other calls
+            lock (_syncLock)
+            {
+                // cannot change the host - client is responsible for setting this on a message that cannot fallback. Additionally,
+                // client must set the host to the correct host based on previous calls.
+                // Example - Method Foo makes two requests that must go to the same endpoint:
+                // var firstMessage = _pipeline.CreateMessage();
+                // ...
+                // _pipeline.Send(firstMessage);
+                //
+                // var secondMessage = _pipeline.CreateMessage();
+                // secondMessage.Request.Uri.Host = firstMessage.Request.Uri.Host;
+                // GeoRedundantReadFallbackPolicy.SetHostAffinity(secondMessage, true);
+                // secondMessage.ProcessingContext.HostAffinity = true;
+                // ...
+                // _pipeline.Send(secondMessage);
+
+                if (GetHostAffinity(message))
+                    return;
+
+                // first attempt - use fallback host if set
+                if (message.ProcessingContext.RetryNumber == 0)
+                {
+                    UpdateHost(message);
+                    return;
+                }
+
+                // subsequent attempt
+
+                // only fallback if no response was received
+                if (message.HasResponse)
+                    return;
+
+                // advance the index
+                _fallbackIndex ??= -1;
+                _fallbackIndex++;
+
+                // reset to null to indicate primary should be used when we reach the end
+                if (_fallbackIndex >= _readFallbackHosts.Length)
+                    _fallbackIndex = null;
+
+                UpdateHost(message);
+            }
+        }
+
+        private void UpdateHost(HttpMessage message)
+        {
+            if (_fallbackIndex.HasValue)
+            {
+                message.Request.Uri.Host = _readFallbackHosts[_fallbackIndex.Value];
+            }
+        }
+    }
+}
