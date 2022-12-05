3cb4693d410f42f5d39374a02d023ef971762fb8
Brent Rusinow <brrusino@microsoft.com>
2022-12-05 12:37:53 -0800

    working changes

--- sdk/chaos/Azure.ResourceManager.Chaos/tests/ChaosManagementTestBase.cs
+++ sdk/chaos/Azure.ResourceManager.Chaos/tests/ChaosManagementTestBase.cs
@@ -3,16 +3,34 @@
 
 using Azure.Core;
 using Azure.Core.TestFramework;
+using Azure.ResourceManager.Chaos.Tests.TestDependencies.Experiments;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.TestFramework;
 using NUnit.Framework;
+using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace Azure.ResourceManager.Chaos.Tests
 {
     public class ChaosManagementTestBase : ManagementRecordedTestBase<ChaosManagementTestEnvironment>
     {
+        public const string ExperimentNamePrefix = "sdktest-chaos-";
+        public const string VmssNameFormat = "chaossdk-vmss-{0}-{1}";
+
         protected ArmClient Client { get; private set; }
+
+        public string ExperimentName { get; private set; }
+
+        public int VmssId { get; private set; }
+
+        public SubscriptionResource Subscription { get; private set; }
+
+        public ResourceGroupResource ResourceGroupResource { get; private set; }
+
+        public ExperimentCollection ExperimentCollection { get; private set; }
+
+        public MockExperimentEntities MockExperimentEntities { get; private set; }
 
         protected ChaosManagementTestBase(bool isAsync, RecordedTestMode mode)
         : base(isAsync, mode)
@@ -30,12 +48,31 @@ namespace Azure.ResourceManager.Chaos.Tests
             Client = GetArmClient();
         }
 
-        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        protected async Task Initialize()
         {
-            string rgName = Recording.GenerateAssetName(rgNamePrefix);
-            ResourceGroupData input = new ResourceGroupData(location);
-            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
-            return lro.Value;
+            this.SetVmssId();
+            this.Subscription = await this.Client.GetDefaultSubscriptionAsync();
+            this.ResourceGroupResource = await this.Subscription.GetResourceGroupAsync(TestEnvironment.ResourceGroup).ConfigureAwait(false);
+            this.ExperimentCollection = this.ResourceGroupResource.GetExperiments();
+            this.ExperimentName = Recording.GenerateAssetName(ExperimentNamePrefix);
+            this.MockExperimentEntities = new MockExperimentEntities(TestEnvironment.SubscriptionId, TestEnvironment.ResourceGroup, string.Format(VmssNameFormat, TestEnvironment.Location, this.VmssId));
+        }
+
+        public void SetVmssId()
+        {
+            var framework = RuntimeInformation.FrameworkDescription;
+            if (framework.IndexOf(".NET Framework", StringComparison.OrdinalIgnoreCase) != -1)
+            {
+                this.VmssId = this.IsAsync ? 1 : 0;
+            }
+            else if (framework.IndexOf(".NET Core", StringComparison.OrdinalIgnoreCase) != -1)
+            {
+                this.VmssId = this.IsAsync ? 3 : 2;
+            }
+            else
+            {
+                this.VmssId = this.IsAsync ? 5 : 4;
+            }
         }
     }
 }
---
+++ sdk/chaos/Azure.ResourceManager.Chaos/tests/Scenario/ExperimentTests.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Chaos.Tests.TestDependencies.Utilities;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Chaos.Tests
+{
+    [NonParallelizable]
+    public class ExperimentTests : ChaosManagementTestBase
+    {
+        public ExperimentTests(bool isAsync)
+            : base(isAsync, RecordedTestMode.Record)
+        {
+        }
+
+        // Typical constructor setup requires an init method instead to not break Record functionality
+        [SetUp]
+        public async Task ClearChallengeCacheForRecord()
+        {
+            if (Mode == RecordedTestMode.Record || Mode == RecordedTestMode.Playback)
+            {
+                await Initialize().ConfigureAwait(false);
+            }
+        }
+
+        [TestCase, Order(1)]
+        [RecordedTest]
+        public async Task CreateOrUpdate()
+        {
+            var resourceResponse = await this.ExperimentCollection.CreateOrUpdateAsync(WaitUntil.Completed, this.ExperimentName, this.MockExperimentEntities.GetVmssShutdownV2v0Experiment());
+            Assert.AreEqual(this.ExperimentName, resourceResponse.Value.Data.Name);
+        }
+
+        [TestCase, Order(2)]
+        [RecordedTest]
+        public async Task Get()
+        {
+            await this.ExperimentCollection.CreateOrUpdateAsync(WaitUntil.Completed, this.ExperimentName, this.MockExperimentEntities.GetVmssShutdownV2v0Experiment());
+            var getResourceResponse = await this.ExperimentCollection.GetAsync(this.ExperimentName).ConfigureAwait(false);
+            Assert.AreEqual(this.ExperimentName, getResourceResponse.Value.Data.Name);
+        }
+
+        [TestCase, Order(3)]
+        [RecordedTest]
+        public async Task List()
+        {
+            var experimentList = await this.ExperimentCollection.GetAllAsync().ToListAsync().ConfigureAwait(false);
+            Assert.True(experimentList.Count > 0);
+        }
+
+        [TestCase, Order(4)]
+        [RecordedTest]
+        public async Task Delete()
+        {
+            await this.ExperimentCollection.CreateOrUpdateAsync(WaitUntil.Completed, this.ExperimentName, this.MockExperimentEntities.GetVmssShutdownV2v0Experiment());
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await subscription.GetResourceGroupAsync(TestEnvironment.ResourceGroup).ConfigureAwait(false);
+            var experimentResourceResponse = await this.ExperimentCollection.GetAsync(this.ExperimentName).ConfigureAwait(false);
+            var deleteResponse = await experimentResourceResponse.Value.DeleteAsync(WaitUntil.Completed).ConfigureAwait(false);
+            Assert.AreEqual(200, deleteResponse.GetRawResponse().Status);
+
+            var existsResponse = await rg.GetExperiments().ExistsAsync(this.ExperimentName).ConfigureAwait(false);
+            Assert.AreEqual(false, existsResponse.Value);
+        }
+
+        [TestCase, Order(5)]
+        [RecordedTest]
+        public async Task StartAndCheckStatus()
+        {
+            var experimentName = $"{ExperimentNamePrefix}execution-{this.VmssId}";
+            await this.ExperimentCollection.CreateOrUpdateAsync(WaitUntil.Completed, experimentName, this.MockExperimentEntities.GetVmssShutdownV2v0Experiment());
+            var experimentResourceResponse = await this.ExperimentCollection.GetAsync(experimentName).ConfigureAwait(false);
+            var startResponse = await experimentResourceResponse.Value.StartAsync().ConfigureAwait(false);
+            Assert.AreEqual(experimentName, startResponse.Value.Name);
+            Assert.AreEqual(202, startResponse.GetRawResponse().Status);
+
+            var statusId = UrlUtility.GetStatusId(startResponse.Value.StatusUri);
+            var statusResponse = await experimentResourceResponse.Value.GetExperimentStatusAsync(statusId).ConfigureAwait(false);
+            Assert.AreEqual(statusId, statusResponse.Value.Id);
+            Assert.AreEqual(200, statusResponse.GetRawResponse().Status);
+        }
+    }
+}
