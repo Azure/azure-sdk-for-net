32bb9b6f2cb28093d796349142e8ed8f1f78300a
pshao25 <97225342+pshao25@users.noreply.github.com>
2022-12-05 14:02:24 +0800

    Fix

--- sdk/anomalydetector/Azure.AI.AnomalyDetector/tests/samples/Sample4_MultivariateDetect.cs
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/tests/samples/Sample4_MultivariateDetect.cs
@@ -163,9 +163,9 @@ namespace Azure.AI.AnomalyDetector.Tests.Samples
 
                 // get detection result
                 MultivariateDetectionResult resultResponse = client.GetMultivariateBatchDetectionResultValue(result_id);
-                MultivariateDetectionStatus result_status = resultResponse.Summary.Status;
+                MultivariateBatchDetectionStatus result_status = resultResponse.Summary.Status;
                 int tryout_count = 0;
-                while (tryout_count < max_tryout & result_status != MultivariateDetectionStatus.Ready & result_status != MultivariateDetectionStatus.Failed)
+                while (tryout_count < max_tryout & result_status != MultivariateBatchDetectionStatus.Ready & result_status != MultivariateBatchDetectionStatus.Failed)
                 {
                     System.Threading.Thread.Sleep(1000);
                     resultResponse = client.GetMultivariateBatchDetectionResultValue(result_id);
@@ -174,7 +174,7 @@ namespace Azure.AI.AnomalyDetector.Tests.Samples
                     Console.Out.Flush();
                 }
 
-                if (result_status == MultivariateDetectionStatus.Failed)
+                if (result_status == MultivariateBatchDetectionStatus.Failed)
                 {
                     Console.WriteLine("Detection failed.");
                     Console.WriteLine("Errors:");
