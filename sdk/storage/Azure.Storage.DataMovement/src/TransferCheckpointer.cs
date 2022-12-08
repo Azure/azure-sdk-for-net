14258fa51d3852d3a72279327f690db299b19563
Amanda Nguyen <amnguye@microsoft.com>
2022-12-07 23:44:43 -0800

    Make Checkpointing internal temporarily

--- sdk/storage/Azure.Storage.DataMovement/src/TransferCheckpointer.cs
+++ sdk/storage/Azure.Storage.DataMovement/src/TransferCheckpointer.cs
@@ -15,7 +15,7 @@ namespace Azure.Storage.DataMovement
     /// Base Checkpointer class to create the checkpointing logic
     /// to resume from.
     /// </summary>
-    public abstract class TransferCheckpointer
+    internal abstract class TransferCheckpointer
     {
         internal List<string> _ids;
 
