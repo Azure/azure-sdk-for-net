24ac28c0897bec7042cf3d5a6bfc9255a31b57b6
Anne Thompson <annelo@microsoft.com>
2022-12-12 14:02:03 -0800

    rearranging for clarity

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/tests/ContainerRegistryRecordedTestBase.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/tests/ContainerRegistryRecordedTestBase.cs
@@ -5,7 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Azure.Containers.ContainerRegistry.Specialized;
+using Azure.Core;
 using Azure.Core.TestFramework;
+using Azure.Core.TestFramework.Models;
 using Azure.Identity;
 using Microsoft.Azure.Management.ContainerRegistry;
 using Microsoft.Azure.Management.ContainerRegistry.Models;
@@ -13,8 +15,6 @@ using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
 using NUnit.Framework;
 using Task = System.Threading.Tasks.Task;
-using Azure.Core;
-using Azure.Core.TestFramework.Models;
 
 namespace Azure.Containers.ContainerRegistry.Tests
 {
@@ -22,26 +22,130 @@ namespace Azure.Containers.ContainerRegistry.Tests
     {
         public ContainerRegistryRecordedTestBase(bool isAsync, RecordedTestMode? mode = default) : base(isAsync, mode)
         {
-            DateTimeOffset expiresOn = DateTimeOffset.UtcNow + TimeSpan.FromDays(365 * 30); // Never expire in software years
-            string encodedBody = Base64Url.EncodeString($"{{\"exp\":{expiresOn.ToUnixTimeSeconds()}}}");
-            var jwtSanitizedValue = $"{SanitizeValue}.{encodedBody}.{SanitizeValue}";
-            BodyKeySanitizers.Add(new BodyKeySanitizer(jwtSanitizedValue)
+            InitializeRecordingSanitizers();
+        }
+
+        [SetUp]
+        public void ContainerRegistryTestSetup()
+        {
+            if (GetAuthorityHost(TestEnvironment.Endpoint) != AzureAuthorityHosts.AzurePublicCloud &&
+                UsingAnonymousClient())
             {
-                JsonPath = "$..refresh_token"
-            });
-            BodyRegexSanitizers.Add(new BodyRegexSanitizer(@"access_token=(?<group>.*?)(?=&|$)", SanitizeValue)
-            {
-                GroupForReplace = "group"
-            });
-            BodyRegexSanitizers.Add(new BodyRegexSanitizer(@"refresh_token=(?<group>.*?)(?=&|$)", SanitizeValue)
-            {
-                GroupForReplace = "group"
-            });
+                Assert.Ignore("Anonymous client is not enabled in national clouds.");
+            }
         }
 
         public ContainerRegistryClient CreateClient(bool anonymousAccess = false)
         {
             return anonymousAccess ? CreateAnonymousClient() : CreateAuthenticatedClient();
+        }
+
+        public ContainerRegistryBlobClient CreateBlobClient(string repository)
+        {
+            string endpoint = TestEnvironment.Endpoint;
+            Uri authorityHost = GetAuthorityHost(endpoint);
+            ContainerRegistryAudience audience = GetAudience(authorityHost);
+
+            return InstrumentClient(new ContainerRegistryBlobClient(
+                    new Uri(endpoint),
+                    TestEnvironment.Credential,
+                    repository,
+                    InstrumentClientOptions(new ContainerRegistryClientOptions()
+                    {
+                        Audience = audience
+                    })
+                ));
+        }
+
+        public async Task ImportImageAsync(string registry, string repository, string tag)
+        {
+            await ImportImageAsync(registry, repository, new List<string>() { tag });
+        }
+
+        public async Task ImportImageAsync(string registry, string repository, List<string> tags)
+        {
+            AzureEnvironment environment = GetManagementCloudEnvironment();
+
+            var credential = new AzureCredentials(
+                new ServicePrincipalLoginInformation
+                {
+                    ClientId = TestEnvironment.ClientId,
+                    ClientSecret = TestEnvironment.ClientSecret,
+                },
+                TestEnvironment.TenantId,
+                environment);
+
+            var managementClient = new ContainerRegistryManagementClient(
+                new Uri(environment.ResourceManagerEndpoint),
+                credential.WithDefaultSubscription(TestEnvironment.SubscriptionId));
+            managementClient.SubscriptionId = TestEnvironment.SubscriptionId;
+
+            var importSource = new ImportSource
+            {
+                SourceImage = repository,
+                RegistryUri = "registry.hub.docker.com"
+            };
+
+            var targetTags = tags.Select(tag => $"{repository}:{tag}");
+
+            await managementClient.Registries.ImportImageAsync(
+                resourceGroupName: TestEnvironment.ResourceGroup,
+                registryName: registry,
+                parameters:
+                    new ImportImageParameters
+                    {
+                        Mode = ImportMode.Force,
+                        Source = importSource,
+                        TargetTags = targetTags.ToList()
+                    });
+        }
+
+        public static Uri GetAuthorityHost(string endpoint)
+        {
+            if (endpoint.Contains(".azurecr.io"))
+            {
+                return AzureAuthorityHosts.AzurePublicCloud;
+            }
+
+            if (endpoint.Contains(".azurecr.cn"))
+            {
+                return AzureAuthorityHosts.AzureChina;
+            }
+
+            if (endpoint.Contains(".azurecr.us"))
+            {
+                return AzureAuthorityHosts.AzureGovernment;
+            }
+
+            if (endpoint.Contains(".azurecr.de"))
+            {
+                return AzureAuthorityHosts.AzureGermany;
+            }
+
+            throw new NotSupportedException($"Cloud for endpoint {endpoint} is not supported.");
+        }
+
+        private void InitializeRecordingSanitizers()
+        {
+            DateTimeOffset expiresOn = DateTimeOffset.UtcNow + TimeSpan.FromDays(365 * 30); // Never expire in software years
+
+            string encodedBody = Base64Url.EncodeString($"{{\"exp\":{expiresOn.ToUnixTimeSeconds()}}}");
+            var jwtSanitizedValue = $"{SanitizeValue}.{encodedBody}.{SanitizeValue}";
+
+            BodyKeySanitizers.Add(new BodyKeySanitizer(jwtSanitizedValue)
+            {
+                JsonPath = "$..refresh_token"
+            });
+
+            BodyRegexSanitizers.Add(new BodyRegexSanitizer(@"access_token=(?<group>.*?)(?=&|$)", SanitizeValue)
+            {
+                GroupForReplace = "group"
+            });
+
+            BodyRegexSanitizers.Add(new BodyRegexSanitizer(@"refresh_token=(?<group>.*?)(?=&|$)", SanitizeValue)
+            {
+                GroupForReplace = "group"
+            });
         }
 
         private ContainerRegistryClient CreateAuthenticatedClient()
@@ -74,47 +178,6 @@ namespace Azure.Containers.ContainerRegistry.Tests
                     })
                 ));
         }
-        public ContainerRegistryBlobClient CreateBlobClient(string repository)
-        {
-            string endpoint = TestEnvironment.Endpoint;
-            Uri authorityHost = GetAuthorityHost(endpoint);
-            ContainerRegistryAudience audience = GetAudience(authorityHost);
-
-            return InstrumentClient(new ContainerRegistryBlobClient(
-                    new Uri(endpoint),
-                    TestEnvironment.Credential,
-                    repository,
-                    InstrumentClientOptions(new ContainerRegistryClientOptions()
-                    {
-                        Audience = audience
-                    })
-                ));
-        }
-
-        internal static Uri GetAuthorityHost(string endpoint)
-        {
-            if (endpoint.Contains(".azurecr.io"))
-            {
-                return AzureAuthorityHosts.AzurePublicCloud;
-            }
-
-            if (endpoint.Contains(".azurecr.cn"))
-            {
-                return AzureAuthorityHosts.AzureChina;
-            }
-
-            if (endpoint.Contains(".azurecr.us"))
-            {
-                return AzureAuthorityHosts.AzureGovernment;
-            }
-
-            if (endpoint.Contains(".azurecr.de"))
-            {
-                return AzureAuthorityHosts.AzureGermany;
-            }
-
-            throw new NotSupportedException($"Cloud for endpoint {endpoint} is not supported.");
-        }
 
         private ContainerRegistryAudience GetAudience(Uri authorityHost)
         {
@@ -141,16 +204,6 @@ namespace Azure.Containers.ContainerRegistry.Tests
             throw new NotSupportedException($"Cloud for authority host {authorityHost} is not supported.");
         }
 
-        [SetUp]
-        public void ContainerRegistryTestSetup()
-        {
-            string endpoint = TestEnvironment.Endpoint;
-            if (GetAuthorityHost(endpoint) != AzureAuthorityHosts.AzurePublicCloud && UsingAnonymousClient())
-            {
-                Assert.Ignore("Anonymous client is not enabled in national clouds.");
-            }
-        }
-
         private bool UsingAnonymousClient()
         {
             var args = TestContext.CurrentContext.Test.Arguments;
@@ -160,51 +213,6 @@ namespace Azure.Containers.ContainerRegistry.Tests
             }
 
             return false;
-        }
-
-        #region Methods using Track 1 Management Plane library
-
-        public async Task ImportImageAsync(string registry, string repository, string tag)
-        {
-            await ImportImageAsync(registry, repository, new List<string>() { tag });
-        }
-
-        public async Task ImportImageAsync(string registry, string repository, List<string> tags)
-        {
-            AzureEnvironment environment = GetManagementCloudEnvironment();
-
-            var credential = new AzureCredentials(
-                new ServicePrincipalLoginInformation
-                {
-                    ClientId = TestEnvironment.ClientId,
-                    ClientSecret = TestEnvironment.ClientSecret,
-                },
-                TestEnvironment.TenantId,
-                environment);
-
-            var managementClient = new ContainerRegistryManagementClient(
-                new Uri (environment.ResourceManagerEndpoint),
-                credential.WithDefaultSubscription(TestEnvironment.SubscriptionId));
-            managementClient.SubscriptionId = TestEnvironment.SubscriptionId;
-
-            var importSource = new ImportSource
-            {
-                SourceImage = repository,
-                RegistryUri = "registry.hub.docker.com"
-            };
-
-            var targetTags = tags.Select(tag => $"{repository}:{tag}");
-
-            await managementClient.Registries.ImportImageAsync(
-                resourceGroupName: TestEnvironment.ResourceGroup,
-                registryName: registry,
-                parameters:
-                    new ImportImageParameters
-                    {
-                        Mode = ImportMode.Force,
-                        Source = importSource,
-                        TargetTags = targetTags.ToList()
-                    });
         }
 
         /// <summary>
@@ -239,6 +247,5 @@ namespace Azure.Containers.ContainerRegistry.Tests
 
             throw new NotSupportedException($"Cloud for authority host {authorityHost} is not supported.");
         }
-        #endregion
     }
 }
