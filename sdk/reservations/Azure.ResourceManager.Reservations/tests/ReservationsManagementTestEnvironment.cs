70a0e91b07391d19749970af8b06ae8f3ceaf99f
gaoyp830 <51546332+gaoyp830@users.noreply.github.com>
2022-12-12 17:21:36 -0800

    Update ReservationsManagementTestEnvironment.cs

--- sdk/reservations/Azure.ResourceManager.Reservations/tests/ReservationsManagementTestEnvironment.cs
+++ sdk/reservations/Azure.ResourceManager.Reservations/tests/ReservationsManagementTestEnvironment.cs
@@ -8,13 +8,5 @@ namespace Azure.ResourceManager.Reservations.Tests
 {
     public class ReservationsManagementTestEnvironment : TestEnvironment
     {
-        public ReservationsManagementTestEnvironment() : base()
-        {
-            Environment.SetEnvironmentVariable("CLIENT_ID", "d8d6ed95-c525-4509-a4b6-7f49d6379f91");
-            Environment.SetEnvironmentVariable("CLIENT_SECRET", "VCB8Q~yIdJ0BdklAVP.VOhbaK8QvzTFZ~2F5EacZ");
-            Environment.SetEnvironmentVariable("SUBSCRIPTION_ID", "fed2a274-8787-4a13-8371-f5282597b779");
-            Environment.SetEnvironmentVariable("TENANT_ID", "3a689a02-340f-4a4e-a32c-6fce276c6bdc");
-            Environment.SetEnvironmentVariable("AZURE_AUTHORITY_HOST", "https://login.microsoftonline.com");
-        }
     }
 }
