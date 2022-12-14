a96ffdec1b1216d7206f20052a67dbc8a6d23615
gaoyp830 <51546332+gaoyp830@users.noreply.github.com>
2022-12-13 19:32:10 -0800

    update

--- sdk/billingbenefits/Azure.ResourceManager.BillingBenefits/src/Generated/Models/ReservationOrderAliasModelCreateOrUpdateContent.Serialization.cs
+++ sdk/billingbenefits/Azure.ResourceManager.BillingBenefits/src/Generated/Models/ReservationOrderAliasModelCreateOrUpdateContent.Serialization.cs
@@ -143,7 +143,7 @@ namespace Azure.ResourceManager.BillingBenefits.Models
                         property.ThrowNonNullablePropertyIsNull();
                         continue;
                     }
-                    systemData = JsonSerializer.Deserialize<SystemData>(property.Value.ToString());
+                    systemData = JsonSerializer.Deserialize<SystemData>(property.Value.GetRawText());
                     continue;
                 }
                 if (property.NameEquals("properties"))
