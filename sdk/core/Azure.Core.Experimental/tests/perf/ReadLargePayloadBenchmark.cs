a436b30ecbdba069a35d61bb905b83c5f98c9c51
Anne Thompson <annelo@microsoft.com>
2022-12-14 10:14:08 -0800

    Add DocumentSentiment large JSON sample and add parse and read benchmarks

---
+++ sdk/core/Azure.Core.Experimental/tests/perf/ReadLargePayloadBenchmark.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json;
+using Azure.Core.Dynamic;
+using BenchmarkDotNet.Attributes;
+
+namespace Azure.Core.Experimental.Perf.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class ReadLargePayloadBenchmark
+    {
+        private dynamic _json = JsonSamples.DocumentSentiment.ToDynamic();
+        private JsonDocument _document = JsonDocument.Parse(JsonSamples.DocumentSentiment);
+
+        [Benchmark(Baseline = true)]
+        public string ReadJsonElement()
+        {
+            // This should return the string "neutral".
+            return _document.RootElement.GetProperty("documents")[0].GetProperty("sentences")[1].GetProperty("sentiment").GetString();
+        }
+
+        [Benchmark]
+        public string ReadJsonData()
+        {
+            return _json.documents[0].sentences[1].sentiment;
+        }
+    }
+}
