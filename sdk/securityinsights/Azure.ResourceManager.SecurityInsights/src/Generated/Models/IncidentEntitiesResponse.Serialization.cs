bfbbb15950b8de595f22d14c0fa5cc569065fb4a
mcgallan <1186003288@qq.com>
2022-12-15 15:48:43 +0800

    finished

--- sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/IncidentEntitiesResponse.Serialization.cs
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/IncidentEntitiesResponse.Serialization.cs
@@ -8,6 +8,7 @@
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
+using Azure.ResourceManager.SecurityInsights;
 
 namespace Azure.ResourceManager.SecurityInsights.Models
 {
--- sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/IncidentEntitiesResponse.cs
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/IncidentEntitiesResponse.cs
@@ -7,6 +7,7 @@
 
 using System.Collections.Generic;
 using Azure.Core;
+using Azure.ResourceManager.SecurityInsights;
 
 namespace Azure.ResourceManager.SecurityInsights.Models
 {
