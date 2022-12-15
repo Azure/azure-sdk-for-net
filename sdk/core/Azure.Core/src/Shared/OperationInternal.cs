197309210883bf5c7fe2b5d3e231f89084b653f0
Feng Zhou <fenzhou@microsoft.com>
2022-12-15 20:16:09 +0800

    revert changes of IOperationSource

--- sdk/core/Azure.Core/src/Shared/OperationInternal.cs
+++ sdk/core/Azure.Core/src/Shared/OperationInternal.cs
@@ -3,11 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core.Pipeline;
--- sdk/core/Azure.Core/src/Shared/OperationInternalOfT.cs
+++ sdk/core/Azure.Core/src/Shared/OperationInternalOfT.cs
@@ -5,9 +5,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core.Pipeline;
