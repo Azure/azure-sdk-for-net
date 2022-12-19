0f8706cec916a6bd871739c8e7c0f3ad25ba6510
Arcturus Zhang <dapzhang@microsoft.com>
2022-12-19 12:28:56 +0800

    regenerate

--- sdk/compute/Azure.ResourceManager.Compute/src/Customize/Models/VirtualMachineScaleSetExtensionData.Serialization.cs
+++ sdk/compute/Azure.ResourceManager.Compute/src/Customize/Models/VirtualMachineScaleSetExtensionData.Serialization.cs
@@ -14,11 +14,13 @@ namespace Azure.ResourceManager.Compute
         void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
         {
             writer.WriteStartObject();
+            // this part is added in this customization
             if (Optional.IsDefined(Name))
             {
                 writer.WritePropertyName("name");
                 writer.WriteStringValue(Name);
             }
+            // end of the customization
             writer.WritePropertyName("properties");
             writer.WriteStartObject();
             if (Optional.IsDefined(ForceUpdateTag))
@@ -84,14 +86,10 @@ namespace Azure.ResourceManager.Compute
                 writer.WritePropertyName("suppressFailures");
                 writer.WriteBooleanValue(SuppressFailures.Value);
             }
-            if (Optional.IsDefined(ProtectedSettingsFromKeyVault))
+            if (Optional.IsDefined(KeyVaultProtectedSettings))
             {
                 writer.WritePropertyName("protectedSettingsFromKeyVault");
-#if NET6_0_OR_GREATER
-				writer.WriteRawValue(ProtectedSettingsFromKeyVault);
-#else
-                JsonSerializer.Serialize(writer, JsonDocument.Parse(ProtectedSettingsFromKeyVault.ToString()).RootElement);
-#endif
+                writer.WriteObjectValue(KeyVaultProtectedSettings);
             }
             writer.WriteEndObject();
             writer.WriteEndObject();
