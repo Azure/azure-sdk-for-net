c0b0c01af243abeb44ed00e3a98980fde3e1cfb8
Rajkumar Rangaraj <rajrang@microsoft.com>
2022-12-21 14:39:00 -0800

    IngestionRedirectionPolicy

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/IngestionRedirectPolicy.cs
@@ -0,0 +1,164 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using System.Threading.Tasks;
+using System;
+using Azure.Core.Pipeline;
+using System.Net.Http.Headers;
+
+namespace Azure.Monitor.OpenTelemetry.Exporter.Internals
+{
+    internal sealed class IngestionRedirectPolicy : HttpPipelinePolicy
+    {
+        internal const int MaxRedirect = 10;
+        internal readonly TimeSpan DefaultCacheExpirationDuration = TimeSpan.FromHours(12);
+
+        private readonly Cache<Uri> cache = new Cache<Uri>();
+
+        internal async ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline, bool async)
+        {
+            if (message.TryGetProperty("redirectionComplete", out object isComplete) && (bool)isComplete)
+            {
+                return;
+            }
+
+            Request request = message.Request;
+
+            if (cache.TryRead(out Uri redirectUri))
+            {
+                // Set up for the redirect
+                request.Uri.Reset(redirectUri);
+            }
+
+            if (async)
+            {
+                await ProcessNextAsync(message, pipeline).ConfigureAwait(false);
+            }
+            else
+            {
+                ProcessNext(message, pipeline);
+            }
+
+            uint redirectCount = 1;
+            Response response = message.Response;
+
+            while (redirectCount < MaxRedirect && IsRedirection(response.Status))
+            {
+                if (!TryGetRedirectUri(response, out redirectUri))
+                {
+                    AzureMonitorExporterEventSource.Log.WriteInformational("RedirectHeaderParseFailed", "Failed to parse redirect headers.");
+                    break;
+                }
+
+                if (!TryGetRedirectCacheTimeSpan(response, out TimeSpan cacheExpirationDuration))
+                {
+                    // if failed to read cache, use default
+                    AzureMonitorExporterEventSource.Log.WriteWarning("ParseRedirectCacheFailed","Failed to parse redirect cache, using default.");
+                    cacheExpirationDuration = DefaultCacheExpirationDuration;
+                }
+
+                cache.Set(redirectUri, cacheExpirationDuration);
+
+                response.Dispose();
+
+                // Clear the authorization header.
+                request.Headers.Remove(HttpHeader.Names.Authorization);
+
+                // Set up for the redirect
+                request.Uri.Reset(redirectUri);
+
+                // Issue the redirected request.
+                if (async)
+                {
+                    await ProcessNextAsync(message, pipeline).ConfigureAwait(false);
+                }
+                else
+                {
+                    ProcessNext(message, pipeline);
+                }
+
+                response = message.Response;
+
+                redirectCount++;
+            }
+
+            message.SetProperty("redirectionComplete", true);
+            return;
+        }
+
+        private static bool TryGetRedirectUri(Response response, out Uri redirectUri)
+        {
+            response.Headers.TryGetValue("Location", out string locationString);
+            return Uri.TryCreate(locationString, UriKind.Absolute, out redirectUri);
+        }
+
+        private static bool TryGetRedirectCacheTimeSpan(Response response, out TimeSpan cacheExpirationDuration)
+        {
+            response.Headers.TryGetValue("CacheControl", out string cacheControlHeader);
+            if (CacheControlHeaderValue.TryParse(cacheControlHeader, out CacheControlHeaderValue cacheControlHeaderValue))
+            {
+                cacheExpirationDuration = cacheControlHeaderValue?.MaxAge ?? default;
+            }
+
+            return cacheExpirationDuration != default;
+        }
+
+        private static bool IsRedirection(int status)
+        {
+            switch (status)
+            {
+                case 307: // StatusCodes.Status307TemporaryRedirect
+                case 308: // StatusCodes.Status308PermanentRedirect
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public override void Process(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            ProcessAsync(message, pipeline, false).EnsureCompleted();
+        }
+
+        public override ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+        {
+            return ProcessAsync(message, pipeline, true);
+        }
+
+        /// <summary>
+        /// Simple class to encapsulate redirect cache.
+        /// </summary>
+        private class Cache<T>
+        {
+            private readonly object _lockObj = new object();
+
+            private T _cachedValue;
+
+            private DateTimeOffset _expiration = DateTimeOffset.MinValue;
+
+            public bool TryRead(out T cachedValue)
+            {
+                if (DateTimeOffset.UtcNow < _expiration)
+                {
+                    cachedValue = _cachedValue;
+                    return true;
+                }
+                else
+                {
+                    cachedValue = default;
+                    return false;
+                }
+            }
+
+            public void Set(T cachingValue, TimeSpan expire)
+            {
+                lock (_lockObj)
+                {
+                    _cachedValue = cachingValue;
+                    _expiration = DateTimeOffset.UtcNow.Add(expire);
+                }
+            }
+        }
+    }
+}
---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/RedirectPolicyTests.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+using Azure.Core.Pipeline;
+using Azure.Monitor.OpenTelemetry.Exporter.Internals;
+using Azure.Monitor.OpenTelemetry.Exporter.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Xunit;
+
+namespace Azure.Monitor.OpenTelemetry.Exporter.Tests
+{
+    public class RedirectPolicyTests
+    {
+        private static readonly ActivitySource activitySource = new ActivitySource("OTel.RedirectPolicy");
+
+        private const string testIkey = "test_ikey";
+        private const string testEndpoint = "http://localhost:5050";
+
+        static RedirectPolicyTests()
+        {
+            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+            Activity.ForceDefaultIdFormat = true;
+
+            var listener = new ActivityListener
+            {
+                ShouldListenTo = _ => true,
+                Sample = (ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllData,
+            };
+
+            ActivitySource.AddActivityListener(listener);
+
+            HttpPipelineHelper.MinimumRetryInterval = 6000;
+        }
+
+        [Fact]
+        public void UsesLocationResponseHeaderAsNewRequestUri()
+        {
+            using var activity = CreateActivity("TestActivity");
+            var telemetryItem = CreateTelemetryItem(activity);
+            List<TelemetryItem> telemetryItems = new List<TelemetryItem>();
+            telemetryItems.Add(telemetryItem);
+
+            // Transmit
+            var fistResponse = new MockResponse(307).AddHeader("Location", "http://new.host/");
+            var transmitter = GetTransmitter(fistResponse);
+            transmitter.TrackAsync(telemetryItems, false, CancellationToken.None).EnsureCompleted();
+        }
+
+        private static AzureMonitorTransmitter GetTransmitter(MockResponse mockResponse)
+        {
+            // MockTransport mockTransport = new MockTransport(mockResponse, new MockResponse(200));
+            var mockTransport = new MockTransport(_ =>
+               new MockResponse(307).AddHeader("Location", "http://new.host/"));
+            AzureMonitorExporterOptions options = new AzureMonitorExporterOptions();
+            options.ConnectionString = $"InstrumentationKey={testIkey};IngestionEndpoint={testEndpoint}";
+            options.Transport = mockTransport;
+            AzureMonitorTransmitter transmitter = new AzureMonitorTransmitter(options);
+
+            return transmitter;
+        }
+
+        private static Activity CreateActivity(string activityName)
+        {
+            ActivitySource activitySource = new ActivitySource("OTel.RedirectPolicy");
+            var activity = activitySource.StartActivity(
+                activityName,
+                ActivityKind.Client,
+                parentContext: default,
+                startTime: DateTime.UtcNow);
+
+            return activity;
+        }
+
+        private static TelemetryItem CreateTelemetryItem(Activity activity)
+        {
+            var monitorTags = TraceHelper.EnumerateActivityTags(activity);
+            return new TelemetryItem(activity, ref monitorTags, null, null, null);
+        }
+    }
+}
