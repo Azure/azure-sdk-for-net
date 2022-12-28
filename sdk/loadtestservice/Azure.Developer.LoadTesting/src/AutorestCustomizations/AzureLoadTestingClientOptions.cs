ee6b016754194447bab6478329b759d09e95270b
Nivedit Jain <jain.22@iitj.ac.in>
2022-12-28 13:23:12 +0530

    fixed compilation issues

--- sdk/loadtestservice/Azure.Developer.LoadTesting/src/AutorestCustomizations/AzureLoadTestingClientOptions.cs
+++ sdk/loadtestservice/Azure.Developer.LoadTesting/src/AutorestCustomizations/AzureLoadTestingClientOptions.cs
@@ -1,4 +1,9 @@
-﻿using Azure.Core;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using Azure.Core;
 
 namespace Azure.Developer.LoadTesting
 {
