37fd39db1debe65b471480e7b9d4238343e1b430
michaelshikh <michaelshikh@microsoft.com>
2023-01-02 14:35:58 +0200

    adding new ps scripts

---
+++ sdk/kusto/Azure.ResourceManager.Kusto/tests/Scripts/RunTestsInRecordMode.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Going to run test in record mode, please make sure required resources exists, if not, please run 'CreateTestResurces.ps1'" -ForegroundColor Green
+
+# Navigate to Azure.ResourceManager.Kusto
+cd ..\..
+
+$Env:AZURE_TEST_MODE = "Record"
+
+dotnet build
+dotnet test -f net6.0
