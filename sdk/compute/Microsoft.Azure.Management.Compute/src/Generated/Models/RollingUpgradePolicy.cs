89bba62c95a1053b2067925a486d9dd9612e5828
John Zhang <116316220+johnzhang-microsoft@users.noreply.github.com>
2023-01-04 10:40:56 -0800

    Generate new compute import for SDK. Includes OS Image profile and maxSurge in Microsoft.Compute (#32880)
    * add reimage notification
    
    * add scenario test
    
    * add max surge
    
    * fix grammar, add release note, take release version back
    
    * add successful test record
    
    * updated csproj
    
    * not sure why merge conflict not resolving
    
    * add maxsurge test
    
    * Update TestVMScaleSetScenarioOperations_ScheduledEvents.json
    
    * passing max surge test
    
    Co-authored-by: Theodore Chang <thchan@microsoft.com>

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/RollingUpgradePolicy.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/Models/RollingUpgradePolicy.cs
@@ -58,7 +58,11 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// determine the batch size.</param>
         /// <param name="prioritizeUnhealthyInstances">Upgrade all unhealthy
         /// instances in a scale set before any healthy instances.</param>
-        public RollingUpgradePolicy(int? maxBatchInstancePercent = default(int?), int? maxUnhealthyInstancePercent = default(int?), int? maxUnhealthyUpgradedInstancePercent = default(int?), string pauseTimeBetweenBatches = default(string), bool? enableCrossZoneUpgrade = default(bool?), bool? prioritizeUnhealthyInstances = default(bool?))
+        /// <param name="maxSurge">Create new virtual machines to upgrade the
+        /// scale set, rather than updating the existing virtual machines.
+        /// Existing virtual machines will be deleted once the new virtual
+        /// machines are created for each batch.</param>
+        public RollingUpgradePolicy(int? maxBatchInstancePercent = default(int?), int? maxUnhealthyInstancePercent = default(int?), int? maxUnhealthyUpgradedInstancePercent = default(int?), string pauseTimeBetweenBatches = default(string), bool? enableCrossZoneUpgrade = default(bool?), bool? prioritizeUnhealthyInstances = default(bool?), bool? maxSurge = default(bool?))
         {
             MaxBatchInstancePercent = maxBatchInstancePercent;
             MaxUnhealthyInstancePercent = maxUnhealthyInstancePercent;
@@ -66,6 +70,7 @@ namespace Microsoft.Azure.Management.Compute.Models
             PauseTimeBetweenBatches = pauseTimeBetweenBatches;
             EnableCrossZoneUpgrade = enableCrossZoneUpgrade;
             PrioritizeUnhealthyInstances = prioritizeUnhealthyInstances;
+            MaxSurge = maxSurge;
             CustomInit();
         }
 
@@ -129,6 +134,15 @@ namespace Microsoft.Azure.Management.Compute.Models
         /// </summary>
         [JsonProperty(PropertyName = "prioritizeUnhealthyInstances")]
         public bool? PrioritizeUnhealthyInstances { get; set; }
+
+        /// <summary>
+        /// Gets or sets create new virtual machines to upgrade the scale set,
+        /// rather than updating the existing virtual machines. Existing
+        /// virtual machines will be deleted once the new virtual machines are
+        /// created for each batch.
+        /// </summary>
+        [JsonProperty(PropertyName = "maxSurge")]
+        public bool? MaxSurge { get; set; }
 
         /// <summary>
         /// Validate the object.
