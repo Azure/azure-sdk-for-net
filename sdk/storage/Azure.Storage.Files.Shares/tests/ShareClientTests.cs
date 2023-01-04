e8a0a70d9ec99b21a60af47917194bd534869eb7
Amanda Nguyen <amnguye@microsoft.com>
2023-01-03 16:13:56 -0800

    Fix for share exists live tests

--- sdk/storage/Azure.Storage.Files.Shares/tests/ShareClientTests.cs
+++ sdk/storage/Azure.Storage.Files.Shares/tests/ShareClientTests.cs
@@ -486,12 +486,18 @@ namespace Azure.Storage.Files.Shares.Tests
             var shareName = GetNewShareName();
             ShareServiceClient service = SharesClientBuilder.GetServiceClient_SharedKey();
             ShareClient share = InstrumentClient(service.GetShareClient(shareName));
-            ShareClient unauthorizesShareClient = InstrumentClient(new ShareClient(share.Uri, GetOptions()));
+
+            ShareUriBuilder uriBuilder = new ShareUriBuilder(share.Uri)
+            {
+                Sas = GetNewFileServiceSasCredentialsShare(share.Name, permissions: ShareSasPermissions.Read)
+            };
+
+            ShareClient unauthorizedShareClient = InstrumentClient(new ShareClient(uriBuilder.ToUri(), GetOptions()));
 
             // Act
             await TestHelper.AssertExpectedExceptionAsync<RequestFailedException>(
-                unauthorizesShareClient.ExistsAsync(),
-                e => Assert.AreEqual("ResourceNotFound", e.ErrorCode));
+                unauthorizedShareClient.ExistsAsync(),
+                e => Assert.AreEqual("AuthorizationFailure", e.ErrorCode));
         }
 
         [RecordedTest]
@@ -532,12 +538,18 @@ namespace Azure.Storage.Files.Shares.Tests
             var shareName = GetNewShareName();
             ShareServiceClient service = SharesClientBuilder.GetServiceClient_SharedKey();
             ShareClient share = InstrumentClient(service.GetShareClient(shareName));
-            ShareClient unauthorizesShareClient = InstrumentClient(new ShareClient(share.Uri, GetOptions()));
+
+            ShareUriBuilder uriBuilder = new ShareUriBuilder(share.Uri)
+            {
+                Sas = GetNewFileServiceSasCredentialsShare(share.Name, permissions: ShareSasPermissions.Read)
+            };
+
+            ShareClient unauthorizedShareClient = InstrumentClient(new ShareClient(uriBuilder.ToUri(), GetOptions()));
 
             // Act
             await TestHelper.AssertExpectedExceptionAsync<RequestFailedException>(
-                unauthorizesShareClient.DeleteIfExistsAsync(),
-                e => Assert.AreEqual("ResourceNotFound", e.ErrorCode));
+                unauthorizedShareClient.DeleteIfExistsAsync(),
+                e => Assert.AreEqual("AuthorizationFailure", e.ErrorCode));
         }
 
         [RecordedTest]
