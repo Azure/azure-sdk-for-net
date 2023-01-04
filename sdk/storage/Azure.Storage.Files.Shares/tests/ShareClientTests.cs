cd026148601609a9e2c433389fa822e00355bee6
Amanda Nguyen <amnguye@microsoft.com>
2023-01-04 13:43:08 -0800

    Create Share for the exists error tests too

--- sdk/storage/Azure.Storage.Files.Shares/tests/ShareClientTests.cs
+++ sdk/storage/Azure.Storage.Files.Shares/tests/ShareClientTests.cs
@@ -486,6 +486,7 @@ namespace Azure.Storage.Files.Shares.Tests
             var shareName = GetNewShareName();
             ShareServiceClient service = SharesClientBuilder.GetServiceClient_SharedKey();
             ShareClient share = InstrumentClient(service.GetShareClient(shareName));
+            await share.CreateIfNotExistsAsync();
 
             ShareUriBuilder uriBuilder = new ShareUriBuilder(share.Uri)
             {
