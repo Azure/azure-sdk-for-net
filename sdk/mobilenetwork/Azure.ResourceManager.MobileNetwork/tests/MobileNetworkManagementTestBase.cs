2d471ecc7f876d65753b5c56c24764f46d5acf85
Andrew Lee <andrewlee@microsoft.com>
2023-01-05 13:55:39 +0000

    All the non-autogenerated work - add tests, README, CI

--- sdk/mobilenetwork/Azure.ResourceManager.MobileNetwork/tests/MobileNetworkManagementTestBase.cs
+++ sdk/mobilenetwork/Azure.ResourceManager.MobileNetwork/tests/MobileNetworkManagementTestBase.cs
@@ -3,9 +3,12 @@
 
 using Azure.Core;
 using Azure.Core.TestFramework;
+using Azure.ResourceManager.MobileNetwork.Models;
+using Azure.ResourceManager.MobileNetwork.Tests.Utilities;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.TestFramework;
 using NUnit.Framework;
+using System;
 using System.Threading.Tasks;
 
 namespace Azure.ResourceManager.MobileNetwork.Tests
@@ -13,6 +16,7 @@ namespace Azure.ResourceManager.MobileNetwork.Tests
     public class MobileNetworkManagementTestBase : ManagementRecordedTestBase<MobileNetworkManagementTestEnvironment>
     {
         protected ArmClient Client { get; private set; }
+        protected SubscriptionResource Subscription { get; private set; }
 
         protected MobileNetworkManagementTestBase(bool isAsync, RecordedTestMode mode)
         : base(isAsync, mode)
@@ -25,16 +29,24 @@ namespace Azure.ResourceManager.MobileNetwork.Tests
         }
 
         [SetUp]
-        public void CreateCommonClient()
+        public async Task CreateCommonClient()
         {
             Client = GetArmClient();
+            Subscription = await Client.GetDefaultSubscriptionAsync();
         }
 
-        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        protected async Task<ResourceGroupResource> CreateResourceGroupAsync(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
         {
             string rgName = Recording.GenerateAssetName(rgNamePrefix);
             ResourceGroupData input = new ResourceGroupData(location);
             var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            return lro.Value;
+        }
+
+        protected async Task<MobileNetworkResource> CreateMobileNetworkResource(ResourceGroupResource resourceGroup, string mobileNetworkName)
+        {
+            var mobileNetworkData = new MobileNetworkData(MobileNetworkManagementUtilities.DefaultResourceLocation, new PlmnId("001", "01"));
+            var lro = await resourceGroup.GetMobileNetworks().CreateOrUpdateAsync(WaitUntil.Completed, mobileNetworkName, mobileNetworkData);
             return lro.Value;
         }
     }
