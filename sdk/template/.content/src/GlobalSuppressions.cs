7869d1057bbb289e321293d56aacfec6a17e9555
archerzz <archerzz@gmail.com>
2023-01-11 15:12:41 +0800

    remove `GlobalSuppressions.cs` since its content is empty

--- sdk/template/.content/src/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using System.Diagnostics.CodeAnalysis;
-
-// add global suppression here
-// [assembly: SuppressMessage(category, checkId)]
