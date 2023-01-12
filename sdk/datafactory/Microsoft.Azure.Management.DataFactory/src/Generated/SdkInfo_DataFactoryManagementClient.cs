12d89297a82134f6714db72ff1258f5aef996c86
Jingshu Pan <v-jinpan@microsoft.com>
2023-01-12 12:53:17 +0800

    revert

--- sdk/datafactory/Microsoft.Azure.Management.DataFactory/src/Generated/SdkInfo_DataFactoryManagementClient.cs
+++ sdk/datafactory/Microsoft.Azure.Management.DataFactory/src/Generated/SdkInfo_DataFactoryManagementClient.cs
@@ -20,13 +20,11 @@ namespace Microsoft.Azure.Management.DataFactory
               return new Tuple<string, string, string>[]
               {
                 new Tuple<string, string, string>("DataFactory", "ActivityRuns", "2018-06-01"),
-                new Tuple<string, string, string>("DataFactory", "CredentialOperations", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "DataFlowDebugSession", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "DataFlows", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "Datasets", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "ExposureControl", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "Factories", "2018-06-01"),
-                new Tuple<string, string, string>("DataFactory", "GlobalParameters", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "IntegrationRuntimeNodes", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "IntegrationRuntimeObjectMetadata", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "IntegrationRuntimes", "2018-06-01"),
@@ -39,18 +37,19 @@ namespace Microsoft.Azure.Management.DataFactory
                 new Tuple<string, string, string>("DataFactory", "PrivateEndpointConnection", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "TriggerRuns", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "Triggers", "2018-06-01"),
+                new Tuple<string, string, string>("DataFactory", "managedPrivateEndpoints", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "privateEndPointConnections", "2018-06-01"),
                 new Tuple<string, string, string>("DataFactory", "privateLinkResources", "2018-06-01"),
               }.AsEnumerable();
           }
       }
       // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "2.0.4421";
+      public static readonly String AutoRestVersion = "v2";
       public static readonly String AutoRestBootStrapperVersion = "autorest@2.0.4413";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/main/specification/datafactory/resource-manager/readme.md --csharp --version=2.0.4421 --reflect-api-versions --tag=package-2018-06 --csharp-sdks-folder=D:\\Code\\azure-sdk-for-net\\sdk";
+      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/datafactory/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --tag=package-2018-06 --csharp-sdks-folder=D:\\Projects\\azure-sdk-for-net-1\\sdk";
       public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "main";
-      public static readonly String GithubCommidId = "6e011aae8dd50b5acb4bd5e951f5b930a010bc41";
+      public static readonly String GithubBranchName = "master";
+      public static readonly String GithubCommidId = "2a8a9a79b23c72c6092d5b1212acf6d69b1b3850";
       public static readonly String CodeGenerationErrors = "";
       public static readonly String GithubRepoName = "azure-rest-api-specs";
       // END: Code Generation Metadata Section
