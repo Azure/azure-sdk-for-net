522ced24998566bb17716b64175b486a809191fb
Olena Stoliarova <ostoliarova@microsoft.com>
2023-01-12 10:14:18 +0200

    remove unused import

--- sdk/communication/Azure.Communication.Common/tests/Identity/CommunicationTokenCredentialTest.cs
+++ sdk/communication/Azure.Communication.Common/tests/Identity/CommunicationTokenCredentialTest.cs
@@ -7,7 +7,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
-using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace Azure.Communication.Identity
