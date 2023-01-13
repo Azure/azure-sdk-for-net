31afda6b38da916a01a70f0b4b652d54387d825d
Anne Thompson <annelo@microsoft.com>
2023-01-12 16:07:58 -0800

    missed file

---
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Blob/BlobHelper.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Azure.Containers.ContainerRegistry.Specialized
+{
+    internal class BlobHelper
+    {
+        internal static string ComputeDigest(Stream stream)
+        {
+            Debug.Assert(stream.CanSeek, "Should only be called on seekable streams.");
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                var position = stream.Position;
+                string digest = default;
+
+                // Compute and print the hash values for each file in directory.
+                try
+                {
+                    stream.Position = 0;
+                    var hashValue = sha256.ComputeHash(stream);
+                    digest = FormatDigest(hashValue);
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine($"I/O Exception: {e.Message}");
+                    throw;
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine($"Access Exception: {e.Message}");
+                    throw;
+                }
+                finally
+                {
+                    stream.Position = position;
+                }
+
+                return digest;
+            }
+        }
+
+        internal static string FormatDigest(byte[] hash)
+        {
+            return $"sha256:{BytesToString(hash)}";
+        }
+
+        private static string BytesToString(byte[] bytes)
+        {
+            var builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+#pragma warning disable CA1305 // Specify IFormatProvider
+                builder.AppendFormat("{0:X2}", bytes[i]);
+#pragma warning restore CA1305 // Specify IFormatProvider
+            }
+#pragma warning disable CA1304 // Specify CultureInfo
+            return builder.ToString().ToLower();
+#pragma warning restore CA1304 // Specify CultureInfo
+        }
+    }
+}
