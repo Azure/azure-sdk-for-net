eb420a02903065ae7a361b039fae9fb180a7d708
Theodore Chang <theodore.l.chang@gmail.com>
2023-01-18 18:21:36 -0500

    customize

---
+++ sdk/compute/Azure.ResourceManager.Compute/src/Customize/VirtualMachineData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.ResourceManager.Compute.Customize
+{
+    internal class VirtualMachineData
+    {
+    }
+}
