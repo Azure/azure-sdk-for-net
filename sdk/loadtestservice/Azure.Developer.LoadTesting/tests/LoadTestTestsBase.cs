3a56757bbde738926dae8354f7d0cfcc5dd2b6bb
Nivedit Jain <jain.22@iitj.ac.in>
2023-01-18 21:39:05 +0530

    testing new approach

--- sdk/loadtestservice/Azure.Developer.LoadTesting/tests/LoadTestTestsBase.cs
+++ sdk/loadtestservice/Azure.Developer.LoadTesting/tests/LoadTestTestsBase.cs
@@ -35,10 +35,7 @@ namespace Azure.Developer.LoadTesting.Tests
                 Regex = @"sig=(?<group>.*?)(?=$|&)"
             });
 
-            BodyRegexSanitizers.Add(new BodyRegexSanitizer("[^\\r](?<break>\\n)", "\r")
-            {
-                    GroupForReplace = "break"
-            });
+            BodyRegexSanitizers.Add(new BodyRegexSanitizer("\r\n", "\n"));
         }
 
         internal LoadTestAdministrationClient CreateAdministrationClient()
