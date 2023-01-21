0c422a351258a62ddf4567651aab26c2ba8f3914
Franko Morales <fmorales@microsoft.com>
2023-01-20 16:13:55 -0800

    Removing unused import

--- sdk/communication/Azure.Communication.CallAutomation/src/CallConnection.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/CallConnection.cs
@@ -8,7 +8,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.Pipeline;
-using Azure.Communication.CallAutomation.Models;
 
 namespace Azure.Communication.CallAutomation
 {
