28139cf772eb2de93e7cdd61bfe3ed02ce40876d
Rajkumar Rangaraj <rajrang@microsoft.com>
2023-01-24 15:58:50 -0800

    Resource changes

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/AzureMonitorResource.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable // TODO: remove and fix errors
+
+namespace Azure.Monitor.OpenTelemetry.Exporter
+{
+    internal class AzureMonitorResource
+    {
+        internal string RoleName { get; set; }
+
+        internal string RoleInstance { get; set; }
+    }
+}
