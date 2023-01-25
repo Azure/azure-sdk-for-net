d35ed52fee366777d3df7c57be51e67858f9bc0a
Rajkumar Rangaraj <rajrang@microsoft.com>
2023-01-24 16:29:09 -0800

    remove nullable disable.

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/AzureMonitorResource.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/AzureMonitorResource.cs
@@ -1,14 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#nullable disable // TODO: remove and fix errors
-
 namespace Azure.Monitor.OpenTelemetry.Exporter
 {
     internal class AzureMonitorResource
     {
-        internal string RoleName { get; set; }
+        internal string? RoleName { get; set; }
 
-        internal string RoleInstance { get; set; }
+        internal string? RoleInstance { get; set; }
     }
 }
