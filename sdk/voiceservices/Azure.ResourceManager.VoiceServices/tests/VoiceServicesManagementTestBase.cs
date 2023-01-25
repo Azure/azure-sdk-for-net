dba8ebf797b33877c684fa5c1701dad6fb865fd3
Sebastian Rex <sebastianrex@microsoft.com>
2023-01-25 16:10:47 +0000

    Add tests

--- sdk/voiceservices/Azure.ResourceManager.VoiceServices/tests/VoiceServicesManagementTestBase.cs
+++ sdk/voiceservices/Azure.ResourceManager.VoiceServices/tests/VoiceServicesManagementTestBase.cs
@@ -1,18 +1,25 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.TestFramework;
+using Azure.ResourceManager.VoiceServices.Models;
 using NUnit.Framework;
-using System.Threading.Tasks;
 
 namespace Azure.ResourceManager.VoiceServices.Tests
 {
     public class VoiceServicesManagementTestBase : ManagementRecordedTestBase<VoiceServicesManagementTestEnvironment>
     {
-        protected ArmClient Client { get; private set; }
+        public string SubscriptionId { get; set; }
+        public ArmClient ArmClient { get; private set; }
+        public ResourceGroupCollection ResourceGroupsOperations { get; set; }
+        public SubscriptionResource Subscription { get; set; }
+
+        protected static AzureLocation Location = new AzureLocation("centraluseuap", "CentralUSEUAP");
 
         protected VoiceServicesManagementTestBase(bool isAsync, RecordedTestMode mode)
         : base(isAsync, mode)
@@ -25,17 +32,47 @@ namespace Azure.ResourceManager.VoiceServices.Tests
         }
 
         [SetUp]
-        public void CreateCommonClient()
+        protected async Task InitializeClients()
         {
-            Client = GetArmClient();
+            ArmClient = GetArmClient();
+            Subscription = await ArmClient.GetDefaultSubscriptionAsync();
+            ResourceGroupsOperations = Subscription.GetResourceGroups();
         }
 
-        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        [OneTimeTearDown]
+        public void Cleanup()
         {
-            string rgName = Recording.GenerateAssetName(rgNamePrefix);
-            ResourceGroupData input = new ResourceGroupData(location);
-            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            CleanupResourceGroups();
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroup()
+        {
+            string rgName = Recording.GenerateAssetName("SDKTest");
+            ResourceGroupData data = new ResourceGroupData(Location);
+            var lro = await Subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, data);
             return lro.Value;
+        }
+
+        protected static CommunicationsGatewayData GetDefaultCommunicationsGatewayData()
+        {
+            var gateway = new CommunicationsGatewayData(Location)
+            {
+                Connectivity = Connectivity.PublicAddress,
+                E911Type = E911Type.Standard,
+            };
+            gateway.ServiceLocations.Add(new ServiceRegionProperties("eastus", new PrimaryRegionProperties(new List<string> { "1.1.1.1" })));
+            gateway.ServiceLocations.Add(new ServiceRegionProperties("westus", new PrimaryRegionProperties(new List<string> { "1.1.1.2" })));
+            gateway.Codecs.Add(TeamsCodec.Pcma);
+            gateway.Platforms.Add(CommunicationsPlatform.OperatorConnect);
+            return gateway;
+        }
+
+        protected async Task<CommunicationsGatewayResource> CreateDefaultCommunicationsGateway()
+        {
+            var rg = await CreateResourceGroup();
+            var resourceName = Recording.GenerateAssetName("SDKTest");
+            var createOp = await rg.GetCommunicationsGateways().CreateOrUpdateAsync(WaitUntil.Completed, resourceName, GetDefaultCommunicationsGatewayData());
+            return createOp.Value;
         }
     }
 }
