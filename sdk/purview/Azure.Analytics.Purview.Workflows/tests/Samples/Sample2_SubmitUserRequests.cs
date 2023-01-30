c29174c6c8781d62969a2b32df2af21f758d0cec
Jing Wang <jiwang6@microsoft.com>
2023-01-30 18:26:27 +0800

    Add Snippets

---
+++ sdk/purview/Azure.Analytics.Purview.Workflows/tests/Samples/Sample2_SubmitUserRequests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.Identity;
+using NUnit.Framework;
+
+namespace Azure.Analytics.Purview.Workflows.Tests.Samples
+{
+    public partial class WorkflowsSamples: SamplesBase<WorkflowsClientTestEnvironment>
+    {
+        [Test]
+        public async Task SubmitUserRequest()
+        {
+            Uri endpoint = new Uri(Environment.GetEnvironmentVariable("Workflow_ENDPOINT"));
+            string clientId = Environment.GetEnvironmentVariable("ClientId");
+            string tenantId = Environment.GetEnvironmentVariable("TenantId");
+            string username = Environment.GetEnvironmentVariable("Username");
+            string password = Environment.GetEnvironmentVariable("Password");
+
+            TokenCredential usernamePasswordCredential = new UsernamePasswordCredential(clientId,tenantId, username,password, null);
+            var client = new PurviewWorkflowServiceClient(endpoint, usernamePasswordCredential);
+
+            #region Snippet:Azure_Analytics_Purview_Workflows_SubmitUserRequests
+
+            string request = "{\"operations\":[{\"type\":\"CreateTerm\",\"payload\":{\"glossaryTerm\":{\"name\":\"term\",\"anchor\":{\"glossaryGuid\":\"20031e20-b4df-4a66-a61d-1b0716f3fa48\"},\"status\":\"Approved\",\"nickName\":\"term\"}}}],\"comment\":\"Thanks!\"}";
+
+            Response submitResult = await client.SubmitUserRequestsAsync(RequestContent.Create(request));
+
+            #endregion
+
+            Assert.AreEqual(200, submitResult.Status);
+        }
+    }
+}
