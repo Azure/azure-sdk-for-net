c23a7371be4b845435a24908823231b49d58c5d1
Wan Yang <wanyang@microsoft.com>
2023-02-01 12:47:49 +0800

    add test case for synapse track 2 mgmt sdk

--- sdk/synapse/Azure.ResourceManager.Synapse/tests/SynapseManagementTestBase.cs
+++ sdk/synapse/Azure.ResourceManager.Synapse/tests/SynapseManagementTestBase.cs
@@ -4,8 +4,14 @@
 using Azure.Core;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Storage;
+using Azure.ResourceManager.Storage.Models;
+using Azure.ResourceManager.Synapse.Models;
+using Azure.ResourceManager.Synapse.Tests.Helpers;
 using Azure.ResourceManager.TestFramework;
 using NUnit.Framework;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Azure.ResourceManager.Synapse.Tests
@@ -13,6 +19,9 @@ namespace Azure.ResourceManager.Synapse.Tests
     public class SynapseManagementTestBase : ManagementRecordedTestBase<SynapseManagementTestEnvironment>
     {
         protected ArmClient Client { get; private set; }
+        protected SubscriptionResource DefaultSubscription { get; private set; }
+        protected CommonTestFixture CommonData { get; private set; }
+        protected ResourceGroupResource ResourceGroup { get; private set; }
 
         protected SynapseManagementTestBase(bool isAsync, RecordedTestMode mode)
         : base(isAsync, mode)
@@ -25,17 +34,94 @@ namespace Azure.ResourceManager.Synapse.Tests
         }
 
         [SetUp]
-        public void CreateCommonClient()
+        public async Task CreateCommonClient()
         {
             Client = GetArmClient();
+            DefaultSubscription = await Client.GetDefaultSubscriptionAsync().ConfigureAwait(false);
         }
 
-        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        protected async Task TestInitialize()
         {
-            string rgName = Recording.GenerateAssetName(rgNamePrefix);
-            ResourceGroupData input = new ResourceGroupData(location);
-            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
-            return lro.Value;
+            CommonData = InitializeCommonTestFixture();
+
+            // create resource group
+            ResourceGroup = await CreateResourceGroup(CommonData.ResourceGroupName, CommonData.Location);
+
+            // create data lake storage
+            CommonData.StorageAccountKey = await CreateStorageAccount(CommonData.ResourceGroupName, CommonData.StorageAccountName, CommonData.Location);
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroup(string resourceGroupName, AzureLocation location)
+        {
+            ArmOperation<ResourceGroupResource> operation = await DefaultSubscription.GetResourceGroups().CreateOrUpdateAsync(
+                WaitUntil.Completed,
+                resourceGroupName,
+                new ResourceGroupData(location));
+            return operation.Value;
+        }
+
+        protected async Task<string> CreateStorageAccount(string resourceGroupName, string storageAccountName, AzureLocation location)
+        {
+            var parameters = new StorageAccountCreateOrUpdateContent(new StorageSku(StorageSkuName.StandardGrs), StorageKind.StorageV2, location);
+            StorageAccountCollection storageAccountCollection = ResourceGroup.GetStorageAccounts();
+            StorageAccountResource account = (await storageAccountCollection.CreateOrUpdateAsync(WaitUntil.Completed, storageAccountName, parameters)).Value;
+
+            // Retrieve the storage account primary access key
+            var accessKey = (await account.GetKeysAsync().ToEnumerableAsync()).First().Value;
+
+            ThrowIfTrue(
+                string.IsNullOrEmpty(accessKey),
+                "storageAccountResource.GetKeys returned null."
+            );
+
+            // Set the storage account url
+            CommonData.DefaultDataLakeStorageAccountUrl = account.Data.PrimaryEndpoints.DfsUri;
+
+            return accessKey;
+        }
+
+        /// <summary>
+        /// Throw expception if the given condition is satisfied
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <param name="message"></param>
+        protected void ThrowIfTrue(bool condition, string message)
+        {
+            if (condition)
+            {
+                throw new Exception(message);
+            }
+        }
+
+        private CommonTestFixture InitializeCommonTestFixture()
+        {
+            return new CommonTestFixture()
+            {
+                Location = AzureLocation.EastUS2,
+                LocationString = "eastus2",
+                ResourceGroupName = Recording.GenerateAssetName("synapsesdkrp"),
+                StorageAccountName = Recording.GenerateAssetName("synapsesdkstorage"),
+                SshUsername = Recording.GenerateAssetName("sshuser"),
+                SshPassword = Recording.GenerateAssetName("Password1!"),
+                DefaultDataLakeStorageFilesystem = Recording.GenerateAssetName("synapsesdkfilesys"),
+                PerformanceLevel = "DW200c",
+                NodeCount = 3,
+                NodeSize = "Small",
+                SparkVersion = "2.4",
+                AutoScaleMinNodeCount = 3,
+                AutoScaleMaxNodeCount = 6,
+                AutoPauseDelayInMinute = 15,
+                StartIpAddress = "0.0.0.0",
+                EndIpAddress = "255.255.255.255",
+                UpdatedStartIpAddress = "10.0.0.0",
+                UpdatedEndIpAddress = "255.0.0.0",
+                KustoSku = new SynapseDataSourceSku(SynapseSkuName.StorageOptimized, KustoPoolSkuSize.Medium),
+                UpdatedKustoSku = new SynapseDataSourceSku(SynapseSkuName.StorageOptimized, KustoPoolSkuSize.Large),
+                SoftDeletePeriod = TimeSpan.FromDays(4),
+                HotCachePeriod = TimeSpan.FromDays(2),
+                UpdatedSoftDeletePeriod = TimeSpan.FromDays(6),
+                UpdatedHotCachePeriod = TimeSpan.FromDays(3)
+            };
         }
     }
 }
