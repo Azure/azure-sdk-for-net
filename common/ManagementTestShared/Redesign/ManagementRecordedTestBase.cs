25b8cd13eed87fd6810723a45831504e7a048f37
Feng Zhou <55177366+fengzhou-msft@users.noreply.github.com>
2023-02-03 16:44:19 +0800

    Support ignoring API versions of more ResourceManager resources when used as test dependencies (#33882)

--- common/ManagementTestShared/Redesign/ManagementRecordedTestBase.cs
+++ common/ManagementTestShared/Redesign/ManagementRecordedTestBase.cs
@@ -73,20 +73,30 @@ namespace Azure.ResourceManager.TestFramework
 
         private void IgnoreArmCoreDependencyVersions()
         {
+            // Ignore the api-version of resource group operations
             UriRegexSanitizers.Add(new UriRegexSanitizer(
                 @"/resourcegroups/[^/]+api-version=(?<group>[a-z0-9-]+)", "**"
             )
             {
                 GroupForReplace = "group"
             });
+            // Ignore the api-version of LRO query status operation for resource group deletion
             UriRegexSanitizers.Add(new UriRegexSanitizer(
                 @"/subscriptions/[^/]+/operationresults/[^/]+api-version=(?<group>[a-z0-9-]+)", "**"
             )
             {
                 GroupForReplace = "group"
             });
+            // Ignore the api-version of TagResource operations
             UriRegexSanitizers.Add(new UriRegexSanitizer(
                 @"/providers/Microsoft.Resources/tags/default\?api-version=(?<group>[a-z0-9-]+)", "**"
+            )
+            {
+                GroupForReplace = "group"
+            });
+            // Ignore the api-version of the operation to query resource providers
+            UriRegexSanitizers.Add(new UriRegexSanitizer(
+                @"/providers/([^/]+)api-version=(?<group>[a-z0-9-]+)", "**"
             )
             {
                 GroupForReplace = "group"
