6f6b080f72daf63ae71a447b04d579fabd87f0a0
danielmarbach <daniel.marbach@openplace.net>
2023-02-03 15:00:21 +0100

    Optimize big endian write

--- sdk/servicebus/Azure.Messaging.ServiceBus/src/Primitives/GuidUtilities.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/src/Primitives/GuidUtilities.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Buffers.Binary;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Azure.Messaging.ServiceBus.Primitives;
@@ -61,11 +62,54 @@ internal static class GuidUtilities
             return;
         }
 
-        guid.ToByteArray().AsSpan().CopyTo(buffer);
+        GuidData data = Unsafe.As<Guid, GuidData>(ref guid);
+
+        Span<byte> bytesSpan = buffer.AsSpan();
+        buffer[15] = data.K; // hoist bounds checks
+        BinaryPrimitives.WriteInt32LittleEndian(buffer, data.A);
+        BinaryPrimitives.WriteInt16LittleEndian(bytesSpan.Slice(4), data.B);
+        BinaryPrimitives.WriteInt16LittleEndian(bytesSpan.Slice(6), data.C);
+        buffer[8] = data.D;
+        buffer[9] = data.E;
+        buffer[10] = data.F;
+        buffer[11] = data.G;
+        buffer[12] = data.H;
+        buffer[13] = data.I;
+        buffer[14] = data.J;
 
         static void ThrowArgumentException()
         {
             throw new ArgumentException("TBD", nameof(buffer));
+        }
+    }
+
+    private readonly struct GuidData
+    {
+        public readonly int A;   // Do not rename (binary serialization)
+        public readonly short B; // Do not rename (binary serialization)
+        public readonly short C; // Do not rename (binary serialization)
+        public readonly byte D;  // Do not rename (binary serialization)
+        public readonly byte E;  // Do not rename (binary serialization)
+        public readonly byte F;  // Do not rename (binary serialization)
+        public readonly byte G;  // Do not rename (binary serialization)
+        public readonly byte H;  // Do not rename (binary serialization)
+        public readonly byte I;  // Do not rename (binary serialization)
+        public readonly byte J;  // Do not rename (binary serialization)
+        public readonly byte K;  // Do not rename (binary serialization)
+
+        public GuidData(int a, short b, short c, byte d, byte e, byte f, byte g, byte h, byte i, byte j, byte k)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+            E = e;
+            F = f;
+            G = g;
+            H = h;
+            I = i;
+            J = j;
+            K = k;
         }
     }
 }
