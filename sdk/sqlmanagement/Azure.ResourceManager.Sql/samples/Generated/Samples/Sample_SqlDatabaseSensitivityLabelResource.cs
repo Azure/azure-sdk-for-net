8ff341d4b0b156bfcffaa45bf42dcc0d02c82f04
Chengming <v-cruan@microsoft.com>
2023-02-06 11:33:00 +0800

    update sql

--- sdk/sqlmanagement/Azure.ResourceManager.Sql/samples/Generated/Samples/Sample_SqlDatabaseSensitivityLabelResource.cs
+++ sdk/sqlmanagement/Azure.ResourceManager.Sql/samples/Generated/Samples/Sample_SqlDatabaseSensitivityLabelResource.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
@@ -51,6 +52,81 @@ namespace Azure.ResourceManager.Sql.Samples
             SensitivityLabelData resourceData = result.Data;
             // for demo we just print out the id
             Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+        }
+
+        // Updates the sensitivity label of a given column with all parameters
+        [NUnit.Framework.Test]
+        [NUnit.Framework.Ignore("Only verifying that the sample builds")]
+        public async Task Update_UpdatesTheSensitivityLabelOfAGivenColumnWithAllParameters()
+        {
+            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2020-11-01-preview/examples/ColumnSensitivityLabelCreateMax.json
+            // this example is just showing the usage of "SensitivityLabels_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this SqlDatabaseSensitivityLabelResource created on azure
+            // for more information of creating SqlDatabaseSensitivityLabelResource, please refer to the document of SqlDatabaseSensitivityLabelResource
+            string subscriptionId = "00000000-1111-2222-3333-444444444444";
+            string resourceGroupName = "myRG";
+            string serverName = "myServer";
+            string databaseName = "myDatabase";
+            string schemaName = "dbo";
+            string tableName = "myTable";
+            string columnName = "myColumn";
+            ResourceIdentifier sqlDatabaseSensitivityLabelResourceId = SqlDatabaseSensitivityLabelResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, serverName, databaseName, schemaName, tableName, columnName);
+            SqlDatabaseSensitivityLabelResource sqlDatabaseSensitivityLabel = client.GetSqlDatabaseSensitivityLabelResource(sqlDatabaseSensitivityLabelResourceId);
+
+            // invoke the operation
+            SensitivityLabelData data = new SensitivityLabelData()
+            {
+                LabelName = "PII",
+                LabelId = "bf91e08c-f4f0-478a-b016-25164b2a65ff",
+                InformationType = "PhoneNumber",
+                InformationTypeId = "d22fa6e9-5ee4-3bde-4c2b-a409604c4646",
+                Rank = SensitivityLabelRank.Low,
+            };
+            ArmOperation<SqlDatabaseSensitivityLabelResource> lro = await sqlDatabaseSensitivityLabel.UpdateAsync(WaitUntil.Completed, data);
+            SqlDatabaseSensitivityLabelResource result = lro.Value;
+
+            // the variable result is a resource, you could call other operations on this instance as well
+            // but just for demo, we get its data from this resource instance
+            SensitivityLabelData resourceData = result.Data;
+            // for demo we just print out the id
+            Console.WriteLine($"Succeeded on id: {resourceData.Id}");
+        }
+
+        // Deletes the sensitivity label of a given column
+        [NUnit.Framework.Test]
+        [NUnit.Framework.Ignore("Only verifying that the sample builds")]
+        public async Task Delete_DeletesTheSensitivityLabelOfAGivenColumn()
+        {
+            // Generated from example definition: specification/sql/resource-manager/Microsoft.Sql/preview/2020-11-01-preview/examples/ColumnSensitivityLabelDelete.json
+            // this example is just showing the usage of "SensitivityLabels_Delete" operation, for the dependent resources, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this SqlDatabaseSensitivityLabelResource created on azure
+            // for more information of creating SqlDatabaseSensitivityLabelResource, please refer to the document of SqlDatabaseSensitivityLabelResource
+            string subscriptionId = "00000000-1111-2222-3333-444444444444";
+            string resourceGroupName = "myRG";
+            string serverName = "myServer";
+            string databaseName = "myDatabase";
+            string schemaName = "dbo";
+            string tableName = "myTable";
+            string columnName = "myColumn";
+            ResourceIdentifier sqlDatabaseSensitivityLabelResourceId = SqlDatabaseSensitivityLabelResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, serverName, databaseName, schemaName, tableName, columnName);
+            SqlDatabaseSensitivityLabelResource sqlDatabaseSensitivityLabel = client.GetSqlDatabaseSensitivityLabelResource(sqlDatabaseSensitivityLabelResourceId);
+
+            // invoke the operation
+            await sqlDatabaseSensitivityLabel.DeleteAsync(WaitUntil.Completed);
+
+            Console.WriteLine($"Succeeded");
         }
     }
 }
--- sdk/sqlmanagement/Azure.ResourceManager.Sql/samples/autorest.tests.md
+++ sdk/sqlmanagement/Azure.ResourceManager.Sql/samples/autorest.tests.md
@@ -10,9 +10,4 @@ require: ../src/autorest.md
 include-x-ms-examples-original-file: true
 testgen:
   sample: true
-  skipped-operations:
-  - ManagedDatabaseSensitivityLabels_CreateOrUpdate
-  - ManagedDatabaseSensitivityLabels_Delete
-  - SensitivityLabels_CreateOrUpdate
-  - SensitivityLabels_Delete
 ```
