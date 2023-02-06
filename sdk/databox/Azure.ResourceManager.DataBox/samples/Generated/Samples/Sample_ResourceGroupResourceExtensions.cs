41dc16dee0e808b38f46d578d0c48b4b49f555a0
Chengming <v-cruan@microsoft.com>
2023-02-06 10:39:08 +0800

    update databox

--- sdk/databox/Azure.ResourceManager.DataBox/samples/Generated/Samples/Sample_ResourceGroupResourceExtensions.cs
+++ sdk/databox/Azure.ResourceManager.DataBox/samples/Generated/Samples/Sample_ResourceGroupResourceExtensions.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.DataBox;
 using Azure.ResourceManager.DataBox.Models;
 using Azure.ResourceManager.Resources;
 
