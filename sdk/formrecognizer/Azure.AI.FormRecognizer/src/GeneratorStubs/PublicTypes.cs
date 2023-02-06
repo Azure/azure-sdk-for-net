deac3150d8a693a53f4489da2c34bb6078e8b303
Caio Saldanha <camaiaor@microsoft.com>
2023-02-06 14:47:26 -0800

    [FormRecognizer] Aggregate empty types in a single file

---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/GeneratorStubs/PublicTypes.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+#pragma warning disable SA1402 // File may only contain a single type
+
+namespace Azure.AI.FormRecognizer.DocumentAnalysis
+{
+    [CodeGenModel("OperationKind")]
+    public partial struct DocumentOperationKind { }
+}
+
+namespace Azure.AI.FormRecognizer
+{
+    [CodeGenModel("Language")]
+    public partial struct FormRecognizerLanguage { }
+
+    [CodeGenModel("Locale")]
+    public partial struct FormRecognizerLocale { }
+}
+
+namespace Azure.AI.FormRecognizer.Models
+{
+    [CodeGenModel("TextStyle")]
+    public partial struct TextStyleName { }
+}
+
+#pragma warning restore SA1402 // File may only contain a single type
