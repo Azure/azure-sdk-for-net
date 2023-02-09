c256d88f4856f0ca5a19bbba092914cb7bfbff91
Chengming <v-cruan@microsoft.com>
2023-02-09 15:44:34 +0800

    Add tests

---
+++ sdk/changeanalysis/Azure.ResourceManager.ChangeAnalysis/tests/Scenario/ChangeAnalysisTests.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.ChangeAnalysis.Models;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.ChangeAnalysis.Tests.Scenario
+{
+    internal class ChangeAnalysisTests : ChangeAnalysisManagementTestBase
+    {
+        private DateTimeOffset EndTime;
+        private DateTimeOffset StartTime;
+        protected long TotalSecondsInAWeek = 604800;
+
+        public ChangeAnalysisTests(bool isAsync) : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            EndTime = Recording.Now;
+            StartTime = DateTimeOffset.FromUnixTimeSeconds(EndTime.ToUnixTimeSeconds() - TotalSecondsInAWeek);
+        }
+
+        [RecordedTest]
+        public async Task GetChangesBySubscription()
+        {
+            var list = await DefaultSubscription.GetChangesBySubscriptionAsync(StartTime, EndTime).ToEnumerableAsync();
+            Assert.IsNotEmpty(list);
+            ValidateDetectedChangeData(list.FirstOrDefault());
+            Assert.AreEqual(list.FirstOrDefault().ResourceType, "Microsoft.ChangeAnalysis/changes");
+        }
+
+        [RecordedTest]
+        public async Task GetChangesByResourceGroup()
+        {
+            var resourceGroup = await CreateResourceGroup();
+            var list = await resourceGroup.GetChangesByResourceGroupAsync(StartTime, EndTime).ToEnumerableAsync();
+        }
+
+        [RecordedTest]
+        public async Task GetChangesByTenants()
+        {
+            var tenants = await Client.GetTenants().GetAllAsync().ToEnumerableAsync();
+            var tenant = tenants.FirstOrDefault();
+
+            var subscriptionChanges = await DefaultSubscription.GetChangesBySubscriptionAsync(StartTime, EndTime).ToEnumerableAsync();
+            string changeId = subscriptionChanges.FirstOrDefault().Id.ToString();
+            string resourceId = changeId.Substring(0, changeId.IndexOf("/providers/Microsoft.ChangeAnalysis/"));
+            var list = await tenant.GetResourceChangesAsync(resourceId, StartTime, EndTime).ToEnumerableAsync();
+            Assert.IsNotEmpty(list);
+            ValidateDetectedChangeData(list.FirstOrDefault());
+            Assert.AreEqual(list.FirstOrDefault().ResourceType, "Microsoft.ChangeAnalysis/resourceChanges");
+        }
+
+        private void ValidateDetectedChangeData(DetectedChangeData detectedChange)
+        {
+            Assert.IsNotNull(detectedChange.Id);
+            Assert.IsNotNull(detectedChange.Name);
+        }
+    }
+}
