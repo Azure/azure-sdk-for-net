c0a8bbd752ea2f89553ffa7d324033c1371c377b
Feng Zhou <fenzhou@microsoft.com>
2023-02-09 16:46:29 +0800

    fix serialization with custom code

--- sdk/resources/Azure.ResourceManager.Resources/src/Generated/Models/ArmApplicationPackageSupportUris.Serialization.cs
+++ sdk/resources/Azure.ResourceManager.Resources/src/Generated/Models/ArmApplicationPackageSupportUris.Serialization.cs
@@ -13,34 +13,5 @@ namespace Azure.ResourceManager.Resources.Models
 {
     public partial class ArmApplicationPackageSupportUris
     {
-        internal static ArmApplicationPackageSupportUris DeserializeArmApplicationPackageSupportUris(JsonElement element)
-        {
-            Optional<Uri> publicAzure = default;
-            Optional<Uri> governmentCloud = default;
-            foreach (var property in element.EnumerateObject())
-            {
-                if (property.NameEquals("publicAzure"u8))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null || property.Value.GetString().Length == 0)
-                    {
-                        publicAzure = null;
-                        continue;
-                    }
-                    publicAzure = new Uri(property.Value.GetString());
-                    continue;
-                }
-                if (property.NameEquals("governmentCloud"u8))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null || property.Value.GetString().Length == 0)
-                    {
-                        governmentCloud = null;
-                        continue;
-                    }
-                    governmentCloud = new Uri(property.Value.GetString());
-                    continue;
-                }
-            }
-            return new ArmApplicationPackageSupportUris(publicAzure.Value, governmentCloud.Value);
-        }
     }
 }
