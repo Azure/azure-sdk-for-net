d9d00e906ddea156be63105f2191197cf5399f78
unknown <schaudhari@microsoft.com>
2023-02-10 11:43:27 +0530

    Summary added

--- sdk/eventhub/Azure.ResourceManager.EventHubs/src/Custom/EventHubData.cs
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/src/Custom/EventHubData.cs
@@ -13,6 +13,7 @@ using Azure.ResourceManager.Models;
 namespace Azure.ResourceManager.EventHubs
 {
     /// <summary> A class representing the EventHubsNamespace data model. </summary>
+    /// <summary> MessageRetentionInDays :- Number of days to retain the events for this Event Hub, value should be 1 to 7 days. </summary>
     public partial class EventHubData : ResourceData
     {
         [Obsolete("This property is obsolete and will be removed in a future release", false)]
