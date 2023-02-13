748b34ed141ea06427eba23d72752f084250ac88
Anne Thompson <annelo@microsoft.com>
2023-02-13 14:30:39 -0800

    MutableJsonDocument namespace, IDisposable (#34126)
    * move MutableJsonDocument and related to Azure.Core.Json namespace
    
    * Make MutableJsonDocument disposable
    
    * Make DynamicJson disposable
    
    * Add tests and update API listings
    
    * PR fb
    
    * Update namespace in perf project

--- sdk/core/Azure.Core.Experimental/src/MutableJsonElement.ArrayEnumerator.cs
+++ sdk/core/Azure.Core.Experimental/src/MutableJsonElement.ArrayEnumerator.cs
@@ -1,15 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Text;
 using System.Text.Json;
 
-namespace Azure.Core.Dynamic
+namespace Azure.Core.Json
 {
     public partial struct MutableJsonElement
     {
