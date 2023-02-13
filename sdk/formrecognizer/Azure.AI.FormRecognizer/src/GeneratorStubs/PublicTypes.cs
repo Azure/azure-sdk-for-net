49f7c28b9ecdaf9f872a2e85986f0885908bf112
Caio Saldanha <camaiaor@microsoft.com>
2023-02-13 15:34:58 -0800

    [FormRecognizer] Aggregate empty types in a single file (#33963)

---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/GeneratorStubs/PublicTypes.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+// This file contains only empty declarations used to overwrite the default behavior
+// of the .NET code generator. We use them for two reasons:
+// - Overwriting the type's default access modifier.
+// - Renaming a type with the 'CodeGenModel' attribute.
+//
+// If there's any logic or members to be added to the partial declaration, a separate
+// file should be created for it.
+
+#pragma warning disable SA1402 // File may only contain a single type
+
+namespace Azure.AI.FormRecognizer.DocumentAnalysis
+{
+    [CodeGenModel("OperationKind")]
+    public partial struct DocumentOperationKind { }
+}
+
+namespace Azure.AI.FormRecognizer
+{
+    [CodeGenModel("Language")]
+    public partial struct FormRecognizerLanguage { }
+
+    [CodeGenModel("Locale")]
+    public partial struct FormRecognizerLocale { }
+}
+
+namespace Azure.AI.FormRecognizer.Models
+{
+    [CodeGenModel("TextStyle")]
+    public partial struct TextStyleName { }
+}
+
+#pragma warning restore SA1402 // File may only contain a single type
