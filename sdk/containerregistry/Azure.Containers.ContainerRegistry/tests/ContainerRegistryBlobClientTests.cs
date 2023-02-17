6ab76f282653b69d2d7d07c52ebefe66de6a0284
Anne Thompson <annelo@microsoft.com>
2023-02-17 10:23:01 -0800

    Implement updated digest validation for DownloadManifest

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/tests/ContainerRegistryBlobClientTests.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/tests/ContainerRegistryBlobClientTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Threading.Tasks;
 using Azure.Containers.ContainerRegistry.Specialized;
 using Azure.Core;
 using Azure.Core.TestFramework;
@@ -59,6 +60,55 @@ namespace Azure.Containers.ContainerRegistry.Tests
             Assert.That(async () => await client.UploadManifestAsync(manifest: null), Throws.InstanceOf<ArgumentNullException>(), "The method should validate that `manifest` is not null.");
             Assert.That(async () => await client.UploadManifestAsync(manifestStream: null), Throws.InstanceOf<ArgumentNullException>(), "The method should validate that `manifest stream` is not null.");
             Assert.That(async () => await client.UploadBlobAsync(null), Throws.InstanceOf<ArgumentNullException>(), "The method should validate that `stream` is not null.");
+        }
+
+        /// <summary>
+        /// Validates digest checks for DownloadManifest.
+        /// </summary>
+        [Test]
+        public async Task DownloadManifestValidatesDigest()
+        {
+            // Arrange
+            Uri endpoint = new("https://example.acr.io");
+            string repository = "TestRepository";
+            string tagName = "v1";
+            BinaryData manifest = BinaryData.FromObjectAsJson(ContainerRegistryTestDataHelpers.CreateManifest());
+            string manifestContent = manifest.ToString();
+            string digest = BlobHelper.ComputeDigest(manifest.ToStream());
+
+            ContainerRegistryClientOptions options = new()
+            {
+                Transport = new MockTransport(
+                    new MockResponse(200).SetContent(manifestContent).AddHeader("Docker-Content-Digest", digest),
+                    new MockResponse(200).SetContent(manifestContent).AddHeader("Docker-Content-Digest", digest),
+                    new MockResponse(200).SetContent(manifestContent).AddHeader("Docker-Content-Digest", digest),
+                    new MockResponse(200).SetContent(manifestContent).AddHeader("Docker-Content-Digest", "Invalid server digest")),
+                Audience = ContainerRegistryAudience.AzureResourceManagerPublicCloud
+            };
+
+            ContainerRegistryBlobClient client = new(endpoint, new MockCredential(), repository, options);
+
+            // Act
+
+            // Request with digest
+            DownloadManifestResult result = await client.DownloadManifestAsync(digest);
+            Assert.AreEqual(manifestContent, result.Content.ToString());
+
+            // Request with tag
+            result = await client.DownloadManifestAsync(tagName);
+            Assert.AreEqual(manifestContent, result.Content.ToString());
+
+            // Request with digest that doesn't match the content
+            Assert.ThrowsAsync<RequestFailedException>(async () =>
+            {
+                await client.DownloadManifestAsync(digest.Replace('0', '1'));
+            });
+
+            // Request with tag, getting a response with invalid digest header.
+            Assert.ThrowsAsync<RequestFailedException>(async () =>
+            {
+                await client.DownloadManifestAsync(tagName);
+            });
         }
     }
 }
