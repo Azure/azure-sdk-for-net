2e876ec7b06c5fccadacb84ffe68f169b537886e
Alexander Sher <AlexanderSher@users.noreply.github.com>
2023-02-17 18:03:20 -0600

    Handle special case when HeaderStringValues.Count == 1 (#34338)
    * Handle special case when HeaderStringValues.Count == 1
    
    * Revert changes, use HeaderStringValues.ToString() instead
    
    * Revert JoinHeaderValues

--- sdk/core/Azure.Core/src/Pipeline/HttpClientTransport.cs
+++ sdk/core/Azure.Core/src/Pipeline/HttpClientTransport.cs
@@ -325,6 +325,18 @@ namespace Azure.Core.Pipeline
                 return string.Empty;
             }
 
+            // Special case when HeaderStringValues.Count == 1, because HttpHeaders also special cases it and creates HeaderStringValues instance from a single string
+            // https://github.com/dotnet/runtime/blob/ef5e27eacecf34a36d72a8feb9082f408779675a/src/libraries/System.Net.Http/src/System/Net/Http/Headers/HttpHeadersNonValidated.cs#L150
+            // https://github.com/dotnet/runtime/blob/ef5e27eacecf34a36d72a8feb9082f408779675a/src/libraries/System.Net.Http/src/System/Net/Http/Headers/HttpHeaders.cs#L1105
+            // Which is later used in HeaderStringValues.ToString:
+            // https://github.com/dotnet/runtime/blob/729bf92e6e2f91aa337da9459bef079b14a0bf34/src/libraries/System.Net.Http/src/System/Net/Http/Headers/HeaderStringValues.cs#L47
+            if (count == 1)
+            {
+                return values.ToString();
+            }
+
+            // While HeaderStringValueToStringVsEnumerator performance test shows that `HeaderStringValues.ToString` is faster than DefaultInterpolatedStringHandler,
+            // we can't use it here because it uses ", " as default separator and doesn't allow customization.
             var interpolatedStringHandler = new DefaultInterpolatedStringHandler(count-1, count);
             var isFirst = true;
             foreach (var str in values)
