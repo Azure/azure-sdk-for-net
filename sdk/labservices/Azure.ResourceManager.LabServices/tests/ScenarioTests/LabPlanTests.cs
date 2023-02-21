34fd8df8a294f7aa305a3cea2a3f38bc56ccedb4
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2023-02-21 14:26:50 +0800

    Add tests

---
+++ sdk/labservices/Azure.ResourceManager.LabServices/tests/ScenarioTests/LabPlanTests.cs
@@ -0,0 +1,130 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.LabServices.Models;
+using NUnit.Framework;
+using Azure.Core.TestFramework;
+using System.Xml;
+using Azure.Core;
+
+namespace Azure.ResourceManager.LabServices.Tests
+{
+    public class LabPlanTests : LabServicesManagementTestBase
+    {
+        public LabPlanTests(bool isAsync)
+                    : base(isAsync, RecordedTestMode.Record)
+        {
+        }
+
+        [Test]
+        public async Task LabPlanCRUDTest()
+        {
+            var rg = await CreateResourceGroupAsync();
+            var collection = rg.GetLabPlans();
+
+            // Create test
+            var data = GetData();
+            var resourceName = Recording.GenerateAssetName("sdklabplan-");
+            var resource = (await collection.CreateOrUpdateAsync(WaitUntil.Completed, resourceName, data)).Value;
+            AssertLabPlanData(data, resource.Data);
+
+            // Get test - 1
+            resource = await collection.GetAsync(resourceName);
+            AssertLabPlanData(data, resource.Data);
+
+            // Update with Put test
+            data.DefaultConnectionProfile.WebSshAccess = LabVirtualMachineConnectionType.Private;
+            resource = (await collection.CreateOrUpdateAsync(WaitUntil.Completed, resourceName, data)).Value;
+            AssertLabPlanData(data, resource.Data);
+
+            // Get test - 2
+            resource = await resource.GetAsync();
+            AssertLabPlanData(data, resource.Data);
+
+            // Update with Patch test
+            data.DefaultConnectionProfile.WebSshAccess = LabVirtualMachineConnectionType.Public;
+            var patch = new LabPlanPatch()
+            {
+                DefaultConnectionProfile = new LabConnectionProfile()
+                {
+                    WebSshAccess = LabVirtualMachineConnectionType.Public,
+                    WebRdpAccess = LabVirtualMachineConnectionType.None,
+                    ClientSshAccess = LabVirtualMachineConnectionType.Public,
+                    ClientRdpAccess = LabVirtualMachineConnectionType.Public,
+                },
+            };
+            resource = (await resource.UpdateAsync(WaitUntil.Completed, patch)).Value;
+            AssertLabPlanData(data, resource.Data);
+
+            // Exists test
+            bool boolResult = await collection.ExistsAsync(resourceName);
+            Assert.IsTrue(boolResult);
+
+            // GetAll test
+            var list = await collection.GetAllAsync().ToEnumerableAsync();
+            Assert.AreEqual(1, list.Count);
+            AssertLabPlanData(data, list[0].Data);
+
+            // Delete test
+            await resource.DeleteAsync(WaitUntil.Completed);
+            boolResult = await collection.ExistsAsync(resourceName);
+            Assert.IsFalse(boolResult);
+        }
+
+        public LabPlanData GetData()
+        {
+            return new LabPlanData(DefaultLocation)
+            {
+                DefaultConnectionProfile = new LabConnectionProfile()
+                {
+                    WebSshAccess = LabVirtualMachineConnectionType.None,
+                    WebRdpAccess = LabVirtualMachineConnectionType.None,
+                    ClientSshAccess = LabVirtualMachineConnectionType.Public,
+                    ClientRdpAccess = LabVirtualMachineConnectionType.Public,
+                },
+                DefaultAutoShutdownProfile = new LabAutoShutdownProfile()
+                {
+                    ShutdownOnDisconnect = LabServicesEnableState.Enabled,
+                    ShutdownWhenNotConnected = LabServicesEnableState.Enabled,
+                    ShutdownOnIdle = LabVirtualMachineShutdownOnIdleMode.UserAbsence,
+                    DisconnectDelay = XmlConvert.ToTimeSpan("PT15M"),
+                    NoConnectDelay = XmlConvert.ToTimeSpan("PT15M"),
+                    IdleDelay = XmlConvert.ToTimeSpan("PT15M"),
+                },
+                SupportInfo = new LabPlanSupportInfo()
+                {
+                    Uri = new Uri("http://help.contoso.com"),
+                    Email = "help@contoso.com",
+                    Phone = "+1-202-555-0123",
+                    Instructions = "Contact support for help.",
+                },
+                AllowedRegions = { AzureLocation.EastUS, AzureLocation.EastUS2 }
+            };
+        }
+
+        public void AssertLabPlanData(LabPlanData expected, LabPlanData actual)
+        {
+            Assert.AreEqual(expected.DefaultConnectionProfile.WebSshAccess, actual.DefaultConnectionProfile.WebSshAccess);
+            Assert.AreEqual(expected.DefaultConnectionProfile.WebRdpAccess, actual.DefaultConnectionProfile.WebRdpAccess);
+            Assert.AreEqual(expected.DefaultConnectionProfile.ClientSshAccess, actual.DefaultConnectionProfile.ClientSshAccess);
+            Assert.AreEqual(expected.DefaultConnectionProfile.ClientRdpAccess, actual.DefaultConnectionProfile.ClientRdpAccess);
+            Assert.AreEqual(expected.DefaultAutoShutdownProfile.ShutdownOnDisconnect, actual.DefaultAutoShutdownProfile.ShutdownOnDisconnect);
+            Assert.AreEqual(expected.DefaultAutoShutdownProfile.ShutdownWhenNotConnected, actual.DefaultAutoShutdownProfile.ShutdownWhenNotConnected);
+            Assert.AreEqual(expected.DefaultAutoShutdownProfile.ShutdownOnIdle, actual.DefaultAutoShutdownProfile.ShutdownOnIdle);
+            Assert.AreEqual(expected.DefaultAutoShutdownProfile.DisconnectDelay, actual.DefaultAutoShutdownProfile.DisconnectDelay);
+            Assert.AreEqual(expected.DefaultAutoShutdownProfile.NoConnectDelay, actual.DefaultAutoShutdownProfile.NoConnectDelay);
+            Assert.AreEqual(expected.DefaultAutoShutdownProfile.IdleDelay, actual.DefaultAutoShutdownProfile.IdleDelay);
+            Assert.AreEqual(expected.SupportInfo.Uri, actual.SupportInfo.Uri);
+            Assert.AreEqual(expected.SupportInfo.Email, actual.SupportInfo.Email);
+            Assert.AreEqual(expected.SupportInfo.Phone, actual.SupportInfo.Phone);
+            Assert.AreEqual(expected.SupportInfo.Instructions, actual.SupportInfo.Instructions);
+            Assert.AreEqual(expected.AllowedRegions.Count, 2);
+        }
+    }
+}
---
+++ sdk/labservices/Azure.ResourceManager.LabServices/tests/ScenarioTests/LabServicesScheduleTests.cs
@@ -0,0 +1,148 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.LabServices.Models;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.LabServices.Tests
+{
+    public class LabServicesScheduleTests : LabServicesManagementTestBase
+    {
+        public LabServicesScheduleTests(bool isAsync)
+                    : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        [Test]
+        public async Task LabServicesScheduleCRUD()
+        {
+            // Prepare lab resource
+            var rg = await CreateResourceGroupAsync();
+            var labCollection = rg.GetLabs();
+            var labData = GetLabData();
+            var labName = Recording.GenerateAssetName("sdklab-");
+            var lab = (await labCollection.CreateOrUpdateAsync(WaitUntil.Completed, labName, labData)).Value;
+
+            // Create test
+            var scheduleCollection = lab.GetLabServicesSchedules();
+            var scheduleData = GetScheduleData();
+            var scheduleName = Recording.GenerateAssetName("sdklabsch-");
+            var schedule = (await scheduleCollection.CreateOrUpdateAsync(WaitUntil.Completed, scheduleName, scheduleData)).Value;
+            AssertSchedule(scheduleData, schedule.Data);
+
+            // Get test - 1
+            schedule = await scheduleCollection.GetAsync(scheduleName);
+            AssertSchedule(scheduleData, schedule.Data);
+
+            // Update test with PUT
+            scheduleData.Notes = BinaryData.FromString("\"New Schedule for students\"");
+            schedule = (await scheduleCollection.CreateOrUpdateAsync(WaitUntil.Completed, scheduleName, scheduleData)).Value;
+            AssertSchedule(scheduleData, schedule.Data);
+
+            // Get test - 2
+            schedule = await schedule.GetAsync();
+            AssertSchedule(scheduleData, schedule.Data);
+
+            // Update with PATCH
+            scheduleData.TimeZoneId = "America/New_York";
+            var patch = new LabServicesSchedulePatch()
+            {
+                TimeZoneId = "America/New_York"
+            };
+            schedule = await schedule.UpdateAsync(patch);
+            AssertSchedule(scheduleData, schedule.Data);
+
+            // Exists test
+            bool boolResult = await scheduleCollection.ExistsAsync(scheduleName);
+            Assert.IsTrue(boolResult);
+
+            // GetAll test
+            var list = await scheduleCollection.GetAllAsync().ToEnumerableAsync();
+            Assert.AreEqual(1, list.Count);
+            AssertSchedule(scheduleData, list[0].Data);
+
+            // Delete test
+            await schedule.DeleteAsync(WaitUntil.Completed);
+            boolResult = await scheduleCollection.ExistsAsync(scheduleName);
+            Assert.IsFalse(boolResult);
+        }
+
+        public LabServicesScheduleData GetScheduleData()
+        {
+            return new LabServicesScheduleData()
+            {
+                StartOn = DateTimeOffset.Parse("2020-05-26T12:00:00Z"),
+                StopOn = DateTimeOffset.Parse("2020-05-26T18:00:00Z"),
+                RecurrencePattern = new LabServicesRecurrencePattern(LabServicesRecurrenceFrequency.Daily, DateTimeOffset.Parse("2020-08-14T23:59:59Z"))
+                {
+                    Interval = 2,
+                },
+                TimeZoneId = "America/Los_Angeles",
+                Notes = BinaryData.FromString("\"Schedule 1 for students\""),
+            };
+        }
+
+        public LabData GetLabData()
+        {
+            return new LabData(DefaultLocation)
+            {
+                Title = "test lab",
+                AutoShutdownProfile = new LabAutoShutdownProfile()
+                {
+                    ShutdownOnDisconnect = LabServicesEnableState.Disabled,
+                    ShutdownOnIdle = LabVirtualMachineShutdownOnIdleMode.UserAbsence,
+                    ShutdownWhenNotConnected = LabServicesEnableState.Disabled,
+                    IdleDelay = TimeSpan.FromMinutes(15)
+                },
+                ConnectionProfile = new LabConnectionProfile()
+                {
+                    ClientRdpAccess = LabVirtualMachineConnectionType.Public,
+                    ClientSshAccess = LabVirtualMachineConnectionType.Public,
+                    WebRdpAccess = LabVirtualMachineConnectionType.None,
+                    WebSshAccess = LabVirtualMachineConnectionType.None
+                },
+                VirtualMachineProfile = new LabVirtualMachineProfile(
+                    createOption: LabVirtualMachineCreateOption.TemplateVm,
+                    imageReference: new LabVirtualMachineImageReference()
+                    {
+                        Sku = "20_04-lts",
+                        Offer = "0001-com-ubuntu-server-focal",
+                        Publisher = "canonical",
+                        Version = "latest"
+                    },
+                    sku: new LabServicesSku("Standard_Fsv2_2_4GB_64_S_SSD")
+                    {
+                        Capacity = 0
+                    },
+                    usageQuota: TimeSpan.FromHours(2),
+                    adminUser: new LabVirtualMachineCredential("tester")
+                    {
+                        // Set the password if not in playback mode
+                        Password = Mode == RecordedTestMode.Playback ? "Sanitized" : Environment.GetEnvironmentVariable("USER_PWD"),
+                    }
+                    )
+                {
+                    UseSharedPassword = LabServicesEnableState.Enabled
+                },
+                SecurityProfile = new LabSecurityProfile()
+                {
+                    OpenAccess = LabServicesEnableState.Disabled
+                }
+            };
+        }
+
+        public void AssertSchedule(LabServicesScheduleData expected, LabServicesScheduleData actual)
+        {
+            Assert.AreEqual(expected.StartOn, actual.StartOn);
+            Assert.AreEqual(expected.StopOn, actual.StopOn);
+            Assert.AreEqual(expected.RecurrencePattern.Frequency, actual.RecurrencePattern.Frequency);
+            Assert.AreEqual(expected.RecurrencePattern.ExpireOn, actual.RecurrencePattern.ExpireOn);
+            Assert.AreEqual(expected.RecurrencePattern.Interval, actual.RecurrencePattern.Interval);
+            Assert.AreEqual(expected.TimeZoneId, actual.TimeZoneId);
+            Assert.AreEqual(expected.Notes.ToString(), actual.Notes.ToString());
+        }
+    }
+}
