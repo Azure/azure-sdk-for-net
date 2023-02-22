5f1038c97e3fe8ec7c42c1bed58698d6d345e82f
TimothyMothra <tilee@microsoft.com>
2023-02-21 19:06:43 -0800

    wip

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/AzureMonitorTraceExporterTests.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/AzureMonitorTraceExporterTests.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-//#nullable disable // TODO: remove and fix errors
-
 using OpenTelemetry.Trace;
 using System;
 using System.Reflection;
