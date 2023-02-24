7b382c85ad93ca30faa2866f63956cc8179f5944
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-02-24 15:25:19 -0800

    Add back using

--- sdk/core/Azure.Core/src/Shared/DiagnosticScope.cs
+++ sdk/core/Azure.Core/src/Shared/DiagnosticScope.cs
@@ -8,6 +8,7 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq.Expressions;
 using System.Reflection;
