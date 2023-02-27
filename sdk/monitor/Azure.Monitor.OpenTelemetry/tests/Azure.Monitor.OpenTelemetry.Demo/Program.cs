c911f815e256eab644a8f426ae38914fb5a05172
Rajkumar Rangaraj <rajrang@microsoft.com>
2023-02-27 13:04:50 -0800

    Update comment.

--- sdk/monitor/Azure.Monitor.OpenTelemetry/tests/Azure.Monitor.OpenTelemetry.Demo/Program.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry/tests/Azure.Monitor.OpenTelemetry.Demo/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddAzureMonitorOpenTelemetry(o =>
 {
     o.ConnectionString = "InstrumentationKey=00000000-0000-0000-0000-000000000000";
     // To send telemetry data using AAD, set Credential property in AzureMonitorOpenTelemetryOptions.
+    // For example:
     // o.Credential = new DefaultAzureCredential();
 });
 
