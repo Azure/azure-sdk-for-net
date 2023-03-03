af91bdb92c548c48b35196e2cf5d064ea42b0fb3
Olena Stoliarova <ostoliarova@microsoft.com>
2023-03-03 13:27:24 +0200

    refactor

--- sdk/communication/Azure.Communication.Identity/tests/Infrastructure/CommunicationTestEnvironment.cs
+++ sdk/communication/Azure.Communication.Identity/tests/Infrastructure/CommunicationTestEnvironment.cs
@@ -10,11 +10,6 @@ namespace Azure.Communication.Identity.Tests.Infrastructure
     {
         public const string LiveTestDynamicConnectionStringEnvironmentVariableName = "COMMUNICATION_LIVETEST_DYNAMIC_CONNECTION_STRING";
         public const string LiveTestStaticConnectionStringEnvironmentVariableName = "COMMUNICATION_LIVETEST_STATIC_CONNECTION_STRING";
-        public const string CommunicationConnectionStringEmailVariableName = "COMMUNICATION_CONNECTION_STRING_EMAIL";
-        public const string AzurePhoneNumber = "AZURE_PHONE_NUMBER";
-        private const string SkipIntSmsTestEnvironmentVariableName = "SKIP_INT_SMS_TEST";
-        private const string SkipIntPhoneNumbersTestEnvironmentVariableName = "SKIP_INT_PHONENUMBERS_TEST";
-        private const string AzureTestAgentVariableName = "AZURE_TEST_AGENT";
 
         public string LiveTestDynamicConnectionString => GetRecordedVariable(
             LiveTestDynamicConnectionStringEnvironmentVariableName,
@@ -31,27 +26,5 @@ namespace Azure.Communication.Identity.Tests.Infrastructure
         public Uri LiveTestStaticEndpoint => new(Core.ConnectionString.Parse(LiveTestStaticConnectionString).GetRequired("endpoint"));
 
         public string LiveTestStaticAccessKey => Core.ConnectionString.Parse(LiveTestStaticConnectionString).GetRequired("accesskey");
-
-        public string CommunicationConnectionStringEmail => GetRecordedVariable(
-            CommunicationConnectionStringEmailVariableName,
-            options => options.HasSecretConnectionStringParameter("accessKey", SanitizedValue.Base64));
-
-        public Uri CommunicationEmailEndpoint => new(Core.ConnectionString.Parse(CommunicationConnectionStringEmail).GetRequired("endpoint"));
-
-        public string CommunicationEmailAccessKey => Core.ConnectionString.Parse(CommunicationConnectionStringEmail).GetRequired("accesskey");
-
-        public string DefaultTestPhoneNumber => GetRecordedVariable(AzurePhoneNumber, options => options.IsSecret("+14255550123"));
-
-        public string SkipSmsTest => GetOptionalVariable(SkipIntSmsTestEnvironmentVariableName) ?? "False";
-
-        public string SkipPhoneNumbersTest => GetOptionalVariable(SkipIntPhoneNumbersTestEnvironmentVariableName) ?? "False";
-
-        public bool ShouldIgnoreSMSTests => bool.Parse(SkipSmsTest);
-
-        public bool ShouldIgnorePhoneNumbersTests => bool.Parse(SkipPhoneNumbersTest);
-
-        public string TestAgentPhoneNumber => GetRecordedVariable($"{AzurePhoneNumber}_{AzureTestAgent}", options => options.IsSecret("+14255550123"));
-
-        private string AzureTestAgent => GetVariable(AzureTestAgentVariableName);
     }
 }
