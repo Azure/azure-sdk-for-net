ef914825d66a9ff581c890c88eaa367e94ebc8fa
Rajkumar Rangaraj <rajrang@microsoft.com>
2023-03-06 14:22:33 -0800

    Test PR to validate directory structure.

--- sdk/monitor/Azure.Monitor.OpenTelemetry/Azure.Monitor.OpenTelemetry/tests/Azure.Monitor.OpenTelemetry.Demo/Program.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry/Azure.Monitor.OpenTelemetry/tests/Azure.Monitor.OpenTelemetry.Demo/Program.cs
@@ -10,16 +10,14 @@ using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAzureMonitor();
+// builder.Services.AddAzureMonitor();
 
-/*
 builder.Services.AddAzureMonitor(o =>
 {
     o.ConnectionString = "InstrumentationKey=00000000-0000-0000-0000-000000000000";
     // Set the Credential property to enable AAD based authentication:
     // o.Credential = new DefaultAzureCredential();
 });
-*/
 
 // To customize sampling, Set ApplicationInsightsSampler to desired sampling ratio and
 // configure with OpenTelemetryTracerProvider.
