7464782c51764cd2b33a307346bb425b7936cebe
Anne Thompson <annelo@microsoft.com>
2023-03-07 09:17:11 -0800

    Some thinking around enabling Console.WriteLine() out of the box

--- sdk/core/Azure.Core.Experimental/src/DynamicJson.Operators.cs
+++ sdk/core/Azure.Core.Experimental/src/DynamicJson.Operators.cs
@@ -21,66 +21,79 @@ namespace Azure.Core.Dynamic
         /// Converts the value to a <see cref="bool"/>.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator bool(DynamicJson value) => value._element.GetBoolean();
+        public static explicit operator bool(DynamicJson value) => value._element.GetBoolean();
 
         /// <summary>
         /// Converts the value to a <see cref="int"/>.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator int(DynamicJson value) => value._element.GetInt32();
+        public static explicit operator int(DynamicJson value) => value._element.GetInt32();
 
         /// <summary>
         /// Converts the value to a <see cref="long"/>.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator long(DynamicJson value) => value._element.GetInt64();
+        public static explicit operator long(DynamicJson value) => value._element.GetInt64();
 
         /// <summary>
         /// Converts the value to a <see cref="string"/>.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator string?(DynamicJson value) => value._element.GetString();
+        public static implicit operator string?(DynamicJson value)
+        {
+            if (value._element.ValueKind == JsonValueKind.String)
+            {
+                return value._element.GetString();
+            }
+
+            if (value._element.ValueKind == JsonValueKind.Null)
+            {
+                return null;
+            }
+
+            return value._element.ToString();
+        }
 
         /// <summary>
         /// Converts the value to a <see cref="float"/>.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator float(DynamicJson value) => value._element.GetSingle();
+        public static explicit operator float(DynamicJson value) => value._element.GetSingle();
 
         /// <summary>
         /// Converts the value to a <see cref="double"/>.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator double(DynamicJson value) => value._element.GetDouble();
+        public static explicit operator double(DynamicJson value) => value._element.GetDouble();
 
         /// <summary>
         /// Converts the value to a <see cref="bool"/> or null.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator bool?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetBoolean();
+        public static explicit operator bool?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetBoolean();
 
         /// <summary>
         /// Converts the value to a <see cref="int"/> or null.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator int?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetInt32();
+        public static explicit operator int?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetInt32();
 
         /// <summary>
         /// Converts the value to a <see cref="long"/> or null.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator long?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetInt64();
+        public static explicit operator long?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetInt64();
 
         /// <summary>
         /// Converts the value to a <see cref="float"/> or null.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator float?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetSingle();
+        public static explicit operator float?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetSingle();
 
         /// <summary>
         /// Converts the value to a <see cref="double"/> or null.
         /// </summary>
         /// <param name="value">The value to convert.</param>
-        public static implicit operator double?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetDouble();
+        public static explicit operator double?(DynamicJson value) => value._element.ValueKind == JsonValueKind.Null ? null : value._element.GetDouble();
     }
 }
