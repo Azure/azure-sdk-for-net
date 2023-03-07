48ae82d80061755a5b624582f5e0a7399f4dc5ed
Konrad Jamrozik <kojamroz@microsoft.com>
2023-03-06 23:36:03 -0800

    add todos on needed changes in cpp repo

--- eng/common/scripts/get-codeowners/get-codeowners.ps1
+++ eng/common/scripts/get-codeowners/get-codeowners.ps1
@@ -12,4 +12,5 @@ param (
 return Get-Codeowners `
   -TargetPath $TargetPath `
   -TargetDirectory $TargetDirectory
-
+# kja need to restore param: https://github.com/Azure/azure-sdk-for-cpp/blob/2850c5d32c8a86491b49e801433b8f186fa81745/eng/pipelines/templates/stages/archetype-cpp-release.yml#L196
+# kja need to fix paths, move back to /eng/common/scripts/, because that is the end goal, and the path would be broken here: https://github.com/Azure/azure-sdk-for-cpp/blob/2850c5d32c8a86491b49e801433b8f186fa81745/eng/pipelines/templates/stages/archetype-cpp-release.yml#L196
