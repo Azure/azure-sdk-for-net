58090f346065b6924ab58cf81c1d74dc15b85f9b
Rui Wang <wangrui@microsoft.com>
2023-03-07 15:35:54 -0800

    Add Dtmf Tone string comparison
    The Dtmf tone is casted to single char like "1", "#" etc. The collecton
    of dtmf tones are translated to string like "1234#*".

--- sdk/communication/Azure.Communication.CallAutomation/src/Models/DtmfTone.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/DtmfTone.cs
@@ -13,5 +13,75 @@ namespace Azure.Communication.CallAutomation
     [JsonConverter(typeof(EquatableEnumJsonConverter<DtmfTone>))]
     public readonly partial struct DtmfTone
     {
+        /// <summary>
+        /// The Dtmf Tone in character format.
+        /// </summary>
+        public string ToChar()
+        {
+            string originalValue = ToString();
+            return TransferToChar(originalValue);
+        }
+
+        /// <summary>
+        /// Convert the tone from string to char digit like "1", "2", "#".
+        /// </summary>
+        private static string TransferToChar(string value)
+        {
+            string charValue = "";
+            switch (value)
+            {
+                case "zero":
+                    charValue = "0";
+                    break;
+                case "one":
+                    charValue = "1";
+                    break;
+                case "two":
+                    charValue = "2";
+                    break;
+                case "three":
+                    charValue = "3";
+                    break;
+                case "four":
+                    charValue = "4";
+                    break;
+                case "five":
+                    charValue = "5";
+                    break;
+                case "six":
+                    charValue = "6";
+                    break;
+                case "seven":
+                    charValue = "7";
+                    break;
+                case "eight":
+                    charValue = "8";
+                    break;
+                case "nine":
+                    charValue = "9";
+                    break;
+                case "a":
+                    charValue = "a";
+                    break;
+                case "b":
+                    charValue = "b";
+                    break;
+                case "c":
+                    charValue = "c";
+                    break;
+                case "d":
+                    charValue = "d";
+                    break;
+                case "pound":
+                    charValue = "#";
+                    break;
+                case "asterisk":
+                    charValue = "*";
+                    break;
+                default:
+                    break;
+            }
+            return charValue;
+        }
     }
 }
--- sdk/communication/Azure.Communication.CallAutomation/src/Models/Events/CollectTonesResult.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/Events/CollectTonesResult.cs
@@ -14,5 +14,18 @@ namespace Azure.Communication.CallAutomation
         /// </summary>
         [CodeGenMember("Tones")]
         public IReadOnlyList<DtmfTone> Tones { get; }
+
+        /// <summary>
+        /// Convert the collection of tones to a string like "12345#".
+        /// </summary>
+        public string ConvertToString()
+        {
+            string initial = "";
+            foreach (DtmfTone atone in Tones)
+            {
+                initial += atone.ToChar();
+            }
+            return initial;
+        }
     }
 }
