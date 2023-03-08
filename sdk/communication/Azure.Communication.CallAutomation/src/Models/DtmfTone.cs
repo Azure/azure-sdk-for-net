11be7d43a43250f3d6858826237312975dcb1851
Rui Wang <wangrui@microsoft.com>
2023-03-08 10:18:24 -0800

    change tone from string to char

--- sdk/communication/Azure.Communication.CallAutomation/src/Models/DtmfTone.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/DtmfTone.cs
@@ -16,67 +16,67 @@ namespace Azure.Communication.CallAutomation
         /// <summary>
         /// The Dtmf Tone in character format.
         /// </summary>
-        public string ToChar()
+        public char ToChar()
         {
             string originalValue = ToString();
-            return TransferToChar(originalValue);
+            return ConvertToChar(originalValue);
         }
 
         /// <summary>
         /// Convert the tone from string to char digit like "1", "2", "#".
         /// </summary>
-        private static string TransferToChar(string value)
+        private static char ConvertToChar(string value)
         {
-            string charValue = "";
+            char charValue = ' ';
             switch (value)
             {
                 case "zero":
-                    charValue = "0";
+                    charValue = '0';
                     break;
                 case "one":
-                    charValue = "1";
+                    charValue = '1';
                     break;
                 case "two":
-                    charValue = "2";
+                    charValue = '2';
                     break;
                 case "three":
-                    charValue = "3";
+                    charValue = '3';
                     break;
                 case "four":
-                    charValue = "4";
+                    charValue = '4';
                     break;
                 case "five":
-                    charValue = "5";
+                    charValue = '5';
                     break;
                 case "six":
-                    charValue = "6";
+                    charValue = '6';
                     break;
                 case "seven":
-                    charValue = "7";
+                    charValue = '7';
                     break;
                 case "eight":
-                    charValue = "8";
+                    charValue = '8';
                     break;
                 case "nine":
-                    charValue = "9";
+                    charValue = '9';
                     break;
                 case "a":
-                    charValue = "a";
+                    charValue = 'a';
                     break;
                 case "b":
-                    charValue = "b";
+                    charValue = 'b';
                     break;
                 case "c":
-                    charValue = "c";
+                    charValue = 'c';
                     break;
                 case "d":
-                    charValue = "d";
+                    charValue = 'd';
                     break;
                 case "pound":
-                    charValue = "#";
+                    charValue = '#';
                     break;
                 case "asterisk":
-                    charValue = "*";
+                    charValue = '*';
                     break;
                 default:
                     break;
