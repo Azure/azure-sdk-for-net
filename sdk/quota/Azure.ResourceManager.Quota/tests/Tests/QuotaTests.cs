9c6abc1cd651444a1cce71d87ef341f81ce86ca7
Allison Inman <alinman@microsoft.com>
2023-03-08 11:38:03 -0800

    Get first test running, need API version to be available in all of ARM before testing can complete

---
+++ sdk/quota/Azure.ResourceManager.Quota/tests/Tests/QuotaTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Quota.Tests.Tests
+{
+    [TestFixture]
+    public class QuotaTests : QuotaManagementTestBase
+    {
+        private const string Scope = "subscriptions/9f6cce51-6baf-4de5-a3c4-6f58b85315b9/providers/Microsoft.Network/locations/westus";
+
+        public QuotaTests() : base(true)
+        {
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            CreateCommonClient();
+        }
+
+        [TestCase]
+        public async Task ListQuotaRequests()
+        {
+            var response = await this.Client.GetQuotaRequestDetails(new Core.ResourceIdentifier(Scope)).GetAllAsync().ToEnumerableAsync();
+
+            Assert.AreEqual(0, response.Count);
+        }
+    }
+}
