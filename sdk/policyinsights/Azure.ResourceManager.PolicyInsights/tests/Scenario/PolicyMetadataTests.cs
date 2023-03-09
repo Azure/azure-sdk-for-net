75bdc09f5a708e3de142a37766b201530dd3e1bc
Chengming <v-cruan@microsoft.com>
2023-03-09 10:50:51 +0800

    update

--- sdk/policyinsights/Azure.ResourceManager.PolicyInsights/tests/Scenario/PolicyMetadataTests.cs
+++ sdk/policyinsights/Azure.ResourceManager.PolicyInsights/tests/Scenario/PolicyMetadataTests.cs
@@ -8,24 +8,44 @@ using System.Text;
 using System.Threading.Tasks;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.PolicyInsights.Models;
+using NUnit.Framework;
 
 namespace Azure.ResourceManager.PolicyInsights.Tests
 {
     internal class PolicyMetadataTests : PolicyInsightsManagementTestBase
     {
-        public PolicyMetadataTests(bool isAsync) : base(isAsync, RecordedTestMode.Record)
+        private PolicyMetadataCollection _metadataCollection;
+        public PolicyMetadataTests(bool isAsync) : base(isAsync)//, RecordedTestMode.Record)
         {
         }
 
+        [SetUp]
+        public void TestSetUp()
+        {
+            _metadataCollection = DefaultTenant.GetAllPolicyMetadata();
+        }
+
         [RecordedTest]
+        [Ignore("Deserialize Error: System.UriFormatException: Invalid URI: The URI is empty.")]
         public async Task GetAll()
         {
-            var collection = DefaultTenant.GetAllPolicyMetadata();
             var query = new PolicyQuerySettings()
             {
-                Top = 1,
+                Top = 3, //The restricted parameter does not work, still return 800+ items
             };
-            var list = await collection.GetAllAsync(query).ToEnumerableAsync();
+            var list = await _metadataCollection.GetAllAsync(query).ToEnumerableAsync();
+            Assert.IsNotEmpty(list);
+        }
+
+        [RecordedTest]
+        public async Task Get()
+        {
+            string metadataName = "CIS_Azure_1.3.0_7.1";
+            var metadata = await _metadataCollection.GetAsync(metadataName);
+            Assert.IsNotNull(metadata);
+            Assert.AreEqual(metadataName, metadata.Value.Data.Name);
+            Assert.AreEqual("Microsoft.PolicyInsights/policyMetadata", metadata.Value.Data.ResourceType.ToString());
+            Assert.AreEqual("Shared", metadata.Value.Data.Owner);
         }
     }
 }
