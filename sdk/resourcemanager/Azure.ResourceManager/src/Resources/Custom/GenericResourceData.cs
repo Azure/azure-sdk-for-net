a540b42854ffccc1f22d52fbca6c6e94123f06de
Wei Hu <huwe@microsoft.com>
2023-03-10 17:07:50 +0800

    revert

--- sdk/resourcemanager/Azure.ResourceManager/src/Resources/Custom/GenericResourceData.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/Resources/Custom/GenericResourceData.cs
@@ -3,8 +3,6 @@
 
 #nullable disable
 
-using System;
-using System.Buffers;
 using Azure.ResourceManager.Resources.Models;
 
 namespace Azure.ResourceManager.Resources
@@ -12,34 +10,5 @@ namespace Azure.ResourceManager.Resources
     // this piece of customization code is used for fixing the base class here
     public partial class GenericResourceData : TrackedResourceExtendedData
     {
-    }
-
-    public partial class GenericResourceData : ISerializable
-    {
-        /// <summary>
-        /// xxx
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="bytesConsumed"></param>
-        /// <param name="format"></param>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public bool TryDeserialize(ReadOnlySpan<byte> data, out int bytesConsumed, StandardFormat format = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// xxx
-        /// </summary>
-        /// <param name="buffer"></param>
-        /// <param name="bytesWritten"></param>
-        /// <param name="format"></param>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public bool TrySerialize(Span<byte> buffer, out int bytesWritten, StandardFormat format = default)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
