ed2710239ac72c32bc0f1200db78c229bce7c252
ArcturusZhang <dapzhang@microsoft.com>
2023-03-10 16:38:56 +0800

    fix issue #34745

---
+++ sdk/websites/Azure.ResourceManager.AppService/src/Customization/HybridConnectionData.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.ComponentModel;
+
+namespace Azure.ResourceManager.AppService
+{
+    public partial class HybridConnectionData
+    {
+        /// <summary> The ARM URI to the Service Bus relay. </summary>
+        [Obsolete("This property is obsolete and will be removed in a future release", false)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public Uri RelayArmUri { get; set; }
+    }
+}
