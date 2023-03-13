93acf6a9bda04b2ed2e9362594ea17b5426f4e62
Matt Hohn <matthohn@microsoft.com>
2023-03-13 11:33:08 -0700

    Reverting initial change

--- sdk/communication/Azure.Communication.Email/src/Generated/Models/ErrorAdditionalInfo.cs
+++ sdk/communication/Azure.Communication.Email/src/Generated/Models/ErrorAdditionalInfo.cs
@@ -8,7 +8,7 @@
 namespace Azure.Communication.Email
 {
     /// <summary> The resource management error additional info. </summary>
-    internal partial class ErrorAdditionalInfo
+    public partial class ErrorAdditionalInfo
     {
         /// <summary> Initializes a new instance of ErrorAdditionalInfo. </summary>
         internal ErrorAdditionalInfo()
