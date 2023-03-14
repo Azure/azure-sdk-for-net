c7ca93f21ccad649cb90ec414531ad8449eb44b2
Allison Inman <alinman@microsoft.com>
2023-03-14 16:36:43 -0700

    Add first few tests

--- sdk/quota/Azure.ResourceManager.Quota/tests/Tests/QuotaTests.cs
+++ sdk/quota/Azure.ResourceManager.Quota/tests/Tests/QuotaTests.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Azure.Core;
 using Azure.Core.TestFramework;
 using NUnit.Framework;
 
@@ -11,6 +14,7 @@ namespace Azure.ResourceManager.Quota.Tests.Tests
     public class QuotaTests : QuotaManagementTestBase
     {
         private const string Scope = "subscriptions/9f6cce51-6baf-4de5-a3c4-6f58b85315b9/providers/Microsoft.Network/locations/westus";
+        private const string ResourceName = "PublicIPAddresses";
 
         public QuotaTests() : base(true)
         {
@@ -25,9 +29,62 @@ namespace Azure.ResourceManager.Quota.Tests.Tests
         [TestCase]
         public async Task ListQuotaRequests()
         {
-            var response = await this.Client.GetQuotaRequestDetails(new Core.ResourceIdentifier(Scope)).GetAllAsync().ToEnumerableAsync();
+            var response = await this.Client.GetQuotaRequestDetails(new ResourceIdentifier(Scope)).GetAllAsync().ToEnumerableAsync();
 
-            Assert.AreEqual(0, response.Count);
+            Assert.IsTrue(response.All(x => string.Equals(x.Data.ResourceType, "Microsoft.Quota/quotaRequests", StringComparison.OrdinalIgnoreCase)));
+        }
+
+        [TestCase]
+        public async Task ListQuotaRequestsWithFilter()
+        {
+            int top = 5;
+            var response = await this.Client.GetQuotaRequestDetails(new ResourceIdentifier(Scope))
+                .GetAllAsync(filter: "provisioningState eq 'Failed'", top: top)
+                .ToEnumerableAsync();
+
+            Assert.IsTrue(response.All(x => string.Equals(x.Data.ProvisioningState.ToString(), "Failed", StringComparison.OrdinalIgnoreCase)));
+            Assert.AreEqual(top, response.Count);
+        }
+
+        [TestCase]
+        public async Task GetQuotaRequest()
+        {
+            string requestId = "2779d9c5-f58a-4163-8f5d-8de32a546c25";
+            var response = await this.Client.GetQuotaRequestDetailAsync(new ResourceIdentifier(Scope), requestId);
+
+            Assert.AreEqual(requestId, response.Value.Data.Name);
+        }
+
+        [TestCase]
+        public async Task GetQuota()
+        {
+            var response = await this.Client.GetCurrentQuotaLimitBaseAsync(new ResourceIdentifier(Scope), ResourceName);
+
+            Assert.AreEqual(ResourceName, response.Value.Data.Name);
+        }
+
+        [TestCase]
+        public async Task ListQuotas()
+        {
+            var response = await this.Client.GetCurrentQuotaLimitBases(new ResourceIdentifier(Scope)).GetAllAsync().ToEnumerableAsync();
+
+            Assert.IsTrue(response.All(x => string.Equals(x.Data.ResourceType, "Microsoft.Quota/quotas", StringComparison.OrdinalIgnoreCase)));
+        }
+
+        [TestCase]
+        public async Task GetUsage()
+        {
+            var response = await this.Client.GetCurrentUsagesBaseAsync(new ResourceIdentifier(Scope), ResourceName);
+
+            Assert.AreEqual(ResourceName, response.Value.Data.Name);
+        }
+
+        [TestCase]
+        public async Task ListUsages()
+        {
+            var response = await this.Client.GetCurrentUsagesBases(new ResourceIdentifier(Scope)).GetAllAsync().ToEnumerableAsync();
+
+            Assert.IsTrue(response.All(x => string.Equals(x.Data.ResourceType, "Microsoft.Quota/usages", StringComparison.OrdinalIgnoreCase)));
         }
     }
 }
