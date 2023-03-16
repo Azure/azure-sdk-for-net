af52bbcd913692d2905b8b8c036af51ef39027eb
Adam Sandor <adsandor@microsoft.com>
2023-03-16 12:44:19 -0400

    generated .net changes from base swagger api version update

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/SharedGalleryImagesOperations.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/SharedGalleryImagesOperations.cs
@@ -98,7 +98,7 @@ namespace Microsoft.Azure.Management.Compute
             {
                 throw new ValidationException(ValidationRules.CannotBeNull, "galleryUniqueName");
             }
-            string apiVersion = "2022-03-03";
+            string apiVersion = "2022-08-03";
             // Tracing
             bool _shouldTrace = ServiceClientTracing.IsEnabled;
             string _invocationId = null;
@@ -305,7 +305,7 @@ namespace Microsoft.Azure.Management.Compute
             {
                 throw new ValidationException(ValidationRules.CannotBeNull, "galleryImageName");
             }
-            string apiVersion = "2022-03-03";
+            string apiVersion = "2022-08-03";
             // Tracing
             bool _shouldTrace = ServiceClientTracing.IsEnabled;
             string _invocationId = null;
