8d2836ea9e4c3c405bd056a5102bfe5d175dfc65
Nisha Bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2023-03-17 13:51:32 -0700

    wip

--- sdk/monitor/Azure.Monitor.Query/tests/LogsQueryClientClientLiveTests.cs
+++ sdk/monitor/Azure.Monitor.Query/tests/LogsQueryClientClientLiveTests.cs
@@ -562,7 +562,7 @@ namespace Azure.Monitor.Query.Tests
             if (include)
             {
                 using JsonDocument document = JsonDocument.Parse(response.Value.GetStatistics());
-                Assert.Greater(document.RootElement.GetProperty("query").GetProperty("executionTime").GetDouble(), 0);
+                Assert.GreaterOrEqual(document.RootElement.GetProperty("query").GetProperty("executionTime").GetDouble(), 0);
             }
             else
             {
