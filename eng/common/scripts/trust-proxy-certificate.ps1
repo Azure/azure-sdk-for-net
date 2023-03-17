2a218e8a39002e1cafde61094b223355877775a5
scbedd <45376673+scbedd@users.noreply.github.com>
2023-03-17 11:36:12 -0700

    add some default output to see about minimizing any occurrence of the task failing for no reason. perhaps having some output will allow devops to have an easier time with the invocation

--- eng/common/scripts/trust-proxy-certificate.ps1
+++ eng/common/scripts/trust-proxy-certificate.ps1
@@ -4,3 +4,6 @@ if ($TestProxyTrustCertFn -and (Test-Path "Function:$TestProxyTrustCertFn"))
 {
     &$TestProxyTrustCertFn
 }
+else {
+    Write-Host "No implementation of TestProxyTrustCertFn provided in eng/scripts/Language-Settings.ps1"
+}
