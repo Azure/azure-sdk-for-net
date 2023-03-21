ef3cb102dd0fa5f607875da1392696a3351ce5c9
Nisha Bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2023-03-21 14:50:57 -0700

    Update LogsQueryClientBuilderExtensions.cs

--- sdk/monitor/Azure.Monitor.Query/src/LogsQueryClientBuilderExtensions.cs
+++ sdk/monitor/Azure.Monitor.Query/src/LogsQueryClientBuilderExtensions.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Extensions.Azure
     {
         /// <summary> Registers a <see cref="LogsQueryClient"/> instance. </summary>
         /// <param name="builder"> The builder to register with. </param>
-        /// <param name="endpoint"> The resource manager service endpoint to use. For example <c>https://management.azure.com/</c> for public cloud. </param>
+        /// <param name="endpoint"> The service endpoint to use. Default is 'https://api.loganalytics.io'. </param>
         public static IAzureClientBuilder<LogsQueryClient, LogsQueryClientOptions> AddLogsQueryClient<TBuilder>(this TBuilder builder, Uri endpoint)
         where TBuilder : IAzureClientFactoryBuilderWithCredential
         {
