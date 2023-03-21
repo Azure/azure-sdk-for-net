a9a133a89fdd2d3f4b40642776059b88a06c8345
nisha-bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2023-03-20 17:33:28 -0700

    [Azure.Monitor.Query] Add Extensions (#34968)
    * wip
    
    * wip
    
    * wip

---
+++ sdk/monitor/Azure.Monitor.Query/src/LogsQueryClientBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core.Extensions;
+using Azure.Monitor.Query;
+
+namespace Microsoft.Extensions.Azure
+{
+    /// <summary> Extension methods to add <see cref="LogsQueryClient"/> to client builder. </summary>
+    public static class LogsQueryClientBuilderExtensions
+    {
+        /// <summary> Registers a <see cref="LogsQueryClient"/> instance. </summary>
+        /// <param name="builder"> The builder to register with. </param>
+        /// <param name="endpoint"> The resource manager service endpoint to use. For example <c>https://management.azure.com/</c> for public cloud. </param>
+        public static IAzureClientBuilder<LogsQueryClient, LogsQueryClientOptions> AddLogsQueryClient<TBuilder>(this TBuilder builder, Uri endpoint)
+        where TBuilder : IAzureClientFactoryBuilderWithCredential
+        {
+            return builder.RegisterClientFactory<LogsQueryClient, LogsQueryClientOptions>((options, cred) => new LogsQueryClient(endpoint, cred, options));
+        }
+
+        /// <summary> Registers a <see cref="LogsQueryClient"/> instance. </summary>
+        /// <param name="builder"> The builder to register with. </param>
+        /// <param name="configuration"> The configuration values. </param>
+        public static IAzureClientBuilder<LogsQueryClient, LogsQueryClientOptions> AddLogsQueryClient<TBuilder, TConfiguration>(this TBuilder builder, TConfiguration configuration)
+        where TBuilder : IAzureClientFactoryBuilderWithConfiguration<TConfiguration>
+        {
+            return builder.RegisterClientFactory<LogsQueryClient, LogsQueryClientOptions>(configuration);
+        }
+    }
+}
