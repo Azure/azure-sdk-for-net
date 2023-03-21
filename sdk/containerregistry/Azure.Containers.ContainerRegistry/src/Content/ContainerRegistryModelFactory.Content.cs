7ed0534a970cc30657a4b38a224147a42fb424a9
Anne Thompson <annelo@microsoft.com>
2023-03-20 17:18:33 -0700

    More renames

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Content/ContainerRegistryModelFactory.Content.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/Content/ContainerRegistryModelFactory.Content.cs
@@ -25,20 +25,20 @@ namespace Azure.Containers.ContainerRegistry
             return new DownloadManifestResult(digest, mediaType, content);
         }
 
-        /// <summary> Initializes a new instance of <see cref="UploadBlobResult" />. </summary>
+        /// <summary> Initializes a new instance of <see cref="UploadRegistryBlobResult" />. </summary>
         /// <param name="digest"> The digest of the uploaded blob, calculated by the registry. </param>
         /// <param name="sizeInBytes">The size of the uploaded blob.</param>
-        /// <returns> A new <see cref="UploadBlobResult"/> instance for mocking. </returns>
-        public static UploadRegistryBlobResult UploadBlobResult(string digest, long sizeInBytes)
+        /// <returns> A new <see cref="UploadRegistryBlobResult"/> instance for mocking. </returns>
+        public static UploadRegistryBlobResult UploadRegistryBlobResult(string digest, long sizeInBytes)
         {
             return new UploadRegistryBlobResult(digest, sizeInBytes);
         }
 
-        /// <summary> Initializes a new instance of <see cref="DownloadBlobResult" />. </summary>
+        /// <summary> Initializes a new instance of <see cref="DownloadRegistryBlobResult" />. </summary>
         /// <param name="digest"> The blob's digest, calculated by the registry. </param>
         /// <param name="content">The blob content.</param>
-        /// <returns> A new <see cref="DownloadBlobResult"/> instance for mocking. </returns>
-        public static DownloadRegistryBlobResult DownloadBlobResult(string digest = null, BinaryData content = null)
+        /// <returns> A new <see cref="DownloadRegistryBlobResult"/> instance for mocking. </returns>
+        public static DownloadRegistryBlobResult DownloadRegistryBlobResult(string digest = null, BinaryData content = null)
         {
             return new DownloadRegistryBlobResult(digest, content);
         }
