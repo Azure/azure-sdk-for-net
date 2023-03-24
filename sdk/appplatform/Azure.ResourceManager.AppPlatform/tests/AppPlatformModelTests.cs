8192d463e8403306b27807b37a084e7fbf620c9b
archerzz <archerzz@gmail.com>
2023-03-24 14:31:45 +0800

    fix(appplatform): failed to parse different formats of guid value
    `AppPlatformServiceProperties` could have GUID values with different foramts. One format doesn't follow the RFC UUID spec, so there will be exception when parsing the value.
    
    This commit will fix the issue by:
    - add a new property `ServiceInstanceId` with string type so that various formats can be accepted
    - use customization codes to keep `ServiceId` property for backward compatibility
      - its value is from parsing `ServiceInstanceId` as UUID
      - mark it as obsolete and editor visiblity to never
    - add unit tests
    - update api signature
    - bump version to `1.0.2`
    
    fix #35061

---
+++ sdk/appplatform/Azure.ResourceManager.AppPlatform/tests/AppPlatformModelTests.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.ResourceManager.AppPlatform.Models;
+using NUnit.Framework;
+using System;
+using System.Text.Json;
+
+namespace Azure.ResourceManager.AppPlatform.Tests
+{
+    public class AppPlatformModelTests
+    {
+        [TestCase("79d502c7359c4afc99095965f28ca37f", "79d502c7-359c-4afc-9909-5965f28ca37f")]
+        [TestCase("77af734a-f643-467d-a31f-f873b15cc869", "77af734a-f643-467d-a31f-f873b15cc869")]
+        [Obsolete]
+        public void AppPlatformServicePropertiesServiceId(string rawId, string guid)
+        {
+            var jsonElement = JsonDocument.Parse($"{{\"serviceId\":\"{rawId}\"}}").RootElement;
+            var properties = AppPlatformServiceProperties.DeserializeAppPlatformServiceProperties(jsonElement);
+
+            Assert.AreEqual(new Guid(guid), properties.ServiceId);
+            Assert.AreEqual(rawId, properties.ServiceInstanceId);
+        }
+
+        [Test]
+        [Obsolete]
+        public void AppPlatformServicePropertiesUndefinedServiceId()
+        {
+            var jsonElement = JsonDocument.Parse("{}").RootElement;
+            var properties = AppPlatformServiceProperties.DeserializeAppPlatformServiceProperties(jsonElement);
+
+            Assert.IsNull(properties.ServiceId);
+            Assert.IsNull(properties.ServiceInstanceId);
+        }
+
+        [Test]
+        [Obsolete]
+        public void AppPlatformServicePropertiesNullServiceId()
+        {
+            var jsonElement = JsonDocument.Parse("{\"serviceId\":null}").RootElement;
+            var properties = AppPlatformServiceProperties.DeserializeAppPlatformServiceProperties(jsonElement);
+
+            Assert.IsNull(properties.ServiceId);
+            Assert.IsNull(properties.ServiceInstanceId);
+        }
+
+        [Test]
+        [Obsolete]
+        public void AppPlatformServicePropertiesUnknownServiceId()
+        {
+            var jsonElement = JsonDocument.Parse("{\"serviceId\":\"1232dcf\"}").RootElement;
+            var properties = AppPlatformServiceProperties.DeserializeAppPlatformServiceProperties(jsonElement);
+
+            try
+            {
+                var serviceId = properties.ServiceId;
+                Assert.Fail("Should throw exception");
+            }
+            catch (FormatException) { }
+            Assert.AreEqual("1232dcf", properties.ServiceInstanceId);
+        }
+    }
+}
