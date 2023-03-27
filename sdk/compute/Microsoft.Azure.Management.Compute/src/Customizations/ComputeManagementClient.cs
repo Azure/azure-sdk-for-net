7c2ee91c243123db10acb1f3b021c4acf88fa1cd
Theodore Chang <theodore.l.chang@gmail.com>
2023-03-27 19:56:36 -0400

    custom data parsing

---
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Customizations/ComputeManagementClient.cs
@@ -0,0 +1,24 @@
+﻿namespace Microsoft.Azure.Management.Compute
+{
+    using Microsoft.Rest;
+    using Microsoft.Rest.Azure;
+    using Microsoft.Rest.Serialization;
+    using Models;
+    using Newtonsoft.Json;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+
+    /// <summary>
+    /// Compute Client
+    /// </summary>
+    public partial class ComputeManagementClient : ServiceClient<ComputeManagementClient>, IComputeManagementClient, IAzureClient
+    {
+        partial void CustomInitialize()
+        {
+            this.DeserializationSettings.DateParseHandling = DateParseHandling.None;
+        }
+    }
+}
