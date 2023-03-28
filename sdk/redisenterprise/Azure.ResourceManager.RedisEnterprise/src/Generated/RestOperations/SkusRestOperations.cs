5b3b033a0c21bba0c11456c525906ca20e8986d9
revanthballa1188 <125347990+revanthballa1188@users.noreply.github.com>
2023-03-28 12:23:52 +0530

    Update Azure.ResourceManager.RedisEnterprise .Net SDK to latest API (2023-03-01-preview) (#34693)
    * Update Azure.ResourceManager.RedisEnterprise .Net SDK to latest
    API(2023-03-01-preview)
    
    * Add generated test samples
    
    * Update autorest.md to latest
    
    * Rebase to latest
    
    * Renaming properties
    
    * Renaming flush content text
    
    * Update operation name for better understanding
    
    * Update SDK with latest swagger change by removing the duplicated enum
    
    * Update to the correct types based on comments
    
    * Rebase to main
    
    * Rename get sku operation
    
    * Update tests
    
    * Update change log
    
    * update
    
    ---------
    
    Co-authored-by: ArcturusZhang <dapzhang@microsoft.com>

--- sdk/redisenterprise/Azure.ResourceManager.RedisEnterprise/src/Generated/RestOperations/SkusRestOperations.cs
+++ sdk/redisenterprise/Azure.ResourceManager.RedisEnterprise/src/Generated/RestOperations/SkusRestOperations.cs
@@ -16,20 +16,20 @@ using Azure.ResourceManager.RedisEnterprise.Models;
 
 namespace Azure.ResourceManager.RedisEnterprise
 {
-    internal partial class OperationsStatusRestOperations
+    internal partial class SkusRestOperations
     {
         private readonly TelemetryDetails _userAgent;
         private readonly HttpPipeline _pipeline;
         private readonly Uri _endpoint;
         private readonly string _apiVersion;
 
-        /// <summary> Initializes a new instance of OperationsStatusRestOperations. </summary>
+        /// <summary> Initializes a new instance of SkusRestOperations. </summary>
         /// <param name="pipeline"> The HTTP pipeline for sending and receiving REST requests and responses. </param>
         /// <param name="applicationId"> The application id to use for user agent. </param>
         /// <param name="endpoint"> server parameter. </param>
         /// <param name="apiVersion"> Api Version. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="pipeline"/> or <paramref name="apiVersion"/> is null. </exception>
-        public OperationsStatusRestOperations(HttpPipeline pipeline, string applicationId, Uri endpoint = null, string apiVersion = default)
+        public SkusRestOperations(HttpPipeline pipeline, string applicationId, Uri endpoint = null, string apiVersion = default)
         {
             _pipeline = pipeline ?? throw new ArgumentNullException(nameof(pipeline));
             _endpoint = endpoint ?? new Uri("https://management.azure.com");
@@ -37,7 +37,7 @@ namespace Azure.ResourceManager.RedisEnterprise
             _userAgent = new TelemetryDetails(GetType().Assembly, applicationId);
         }
 
-        internal HttpMessage CreateGetRequest(string subscriptionId, AzureLocation location, string operationId)
+        internal HttpMessage CreateListRequest(string subscriptionId, AzureLocation location)
         {
             var message = _pipeline.CreateMessage();
             var request = message.Request;
@@ -48,8 +48,7 @@ namespace Azure.ResourceManager.RedisEnterprise
             uri.AppendPath(subscriptionId, true);
             uri.AppendPath("/providers/Microsoft.Cache/locations/", false);
             uri.AppendPath(location, true);
-            uri.AppendPath("/operationsStatus/", false);
-            uri.AppendPath(operationId, true);
+            uri.AppendPath("/skus", false);
             uri.AppendQuery("api-version", _apiVersion, true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
@@ -57,27 +56,25 @@ namespace Azure.ResourceManager.RedisEnterprise
             return message;
         }
 
-        /// <summary> Gets the status of operation. </summary>
+        /// <summary> Gets information about skus in specified location for the given subscription id. </summary>
         /// <param name="subscriptionId"> The ID of the target subscription. </param>
         /// <param name="location"> The name of Azure region. </param>
-        /// <param name="operationId"> The ID of an ongoing async operation. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/> or <paramref name="operationId"/> is null. </exception>
-        /// <exception cref="ArgumentException"> <paramref name="subscriptionId"/> or <paramref name="operationId"/> is an empty string, and was expected to be non-empty. </exception>
-        public async Task<Response<RedisEnterpriseOperationStatus>> GetAsync(string subscriptionId, AzureLocation location, string operationId, CancellationToken cancellationToken = default)
+        /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/> is null. </exception>
+        /// <exception cref="ArgumentException"> <paramref name="subscriptionId"/> is an empty string, and was expected to be non-empty. </exception>
+        public async Task<Response<RegionSkuDetails>> ListAsync(string subscriptionId, AzureLocation location, CancellationToken cancellationToken = default)
         {
             Argument.AssertNotNullOrEmpty(subscriptionId, nameof(subscriptionId));
-            Argument.AssertNotNullOrEmpty(operationId, nameof(operationId));
 
-            using var message = CreateGetRequest(subscriptionId, location, operationId);
+            using var message = CreateListRequest(subscriptionId, location);
             await _pipeline.SendAsync(message, cancellationToken).ConfigureAwait(false);
             switch (message.Response.Status)
             {
                 case 200:
                     {
-                        RedisEnterpriseOperationStatus value = default;
+                        RegionSkuDetails value = default;
                         using var document = await JsonDocument.ParseAsync(message.Response.ContentStream, default, cancellationToken).ConfigureAwait(false);
-                        value = RedisEnterpriseOperationStatus.DeserializeRedisEnterpriseOperationStatus(document.RootElement);
+                        value = RegionSkuDetails.DeserializeRegionSkuDetails(document.RootElement);
                         return Response.FromValue(value, message.Response);
                     }
                 default:
@@ -85,27 +82,25 @@ namespace Azure.ResourceManager.RedisEnterprise
             }
         }
 
-        /// <summary> Gets the status of operation. </summary>
+        /// <summary> Gets information about skus in specified location for the given subscription id. </summary>
         /// <param name="subscriptionId"> The ID of the target subscription. </param>
         /// <param name="location"> The name of Azure region. </param>
-        /// <param name="operationId"> The ID of an ongoing async operation. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/> or <paramref name="operationId"/> is null. </exception>
-        /// <exception cref="ArgumentException"> <paramref name="subscriptionId"/> or <paramref name="operationId"/> is an empty string, and was expected to be non-empty. </exception>
-        public Response<RedisEnterpriseOperationStatus> Get(string subscriptionId, AzureLocation location, string operationId, CancellationToken cancellationToken = default)
+        /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/> is null. </exception>
+        /// <exception cref="ArgumentException"> <paramref name="subscriptionId"/> is an empty string, and was expected to be non-empty. </exception>
+        public Response<RegionSkuDetails> List(string subscriptionId, AzureLocation location, CancellationToken cancellationToken = default)
         {
             Argument.AssertNotNullOrEmpty(subscriptionId, nameof(subscriptionId));
-            Argument.AssertNotNullOrEmpty(operationId, nameof(operationId));
 
-            using var message = CreateGetRequest(subscriptionId, location, operationId);
+            using var message = CreateListRequest(subscriptionId, location);
             _pipeline.Send(message, cancellationToken);
             switch (message.Response.Status)
             {
                 case 200:
                     {
-                        RedisEnterpriseOperationStatus value = default;
+                        RegionSkuDetails value = default;
                         using var document = JsonDocument.Parse(message.Response.ContentStream);
-                        value = RedisEnterpriseOperationStatus.DeserializeRedisEnterpriseOperationStatus(document.RootElement);
+                        value = RegionSkuDetails.DeserializeRegionSkuDetails(document.RootElement);
                         return Response.FromValue(value, message.Response);
                     }
                 default:
