0384477872a85d7a0dcf200ed8816a54073e84ba
Anne Thompson <annelo@microsoft.com>
2023-04-05 11:08:12 -0700

    Updates to parser to improve buffering

--- sdk/containerregistry/Azure.Containers.ContainerRegistry/src/ContainerRegistryRequestFailedDetailsParser.cs
+++ sdk/containerregistry/Azure.Containers.ContainerRegistry/src/ContainerRegistryRequestFailedDetailsParser.cs
@@ -21,25 +21,23 @@ namespace Azure.Containers.ContainerRegistry
                 return false;
             }
 
-            BinaryData content = BinaryData.FromStream(response.ContentStream);
-            if (content.ToMemory().Length == 0)
+            if (response.ContentStream is not MemoryStream)
+            {
+                // Buffer the stream to enable content logging in the exception message.
+                // See: https://github.com/Azure/azure-sdk-for-net/issues/35355
+                BufferResponse(response);
+            }
+
+            BinaryData content = response.Content;
+
+            // Optimistic check for JSON error object
+            if (content.ToMemory().Length == 0 || content.ToMemory().Span[0] != (byte)'{')
             {
                 return false;
             }
 
-            // Buffer the stream to log the content in the message
-            // See: https://github.com/Azure/azure-sdk-for-net/issues/35355
-            MemoryStream stream = new(content.ToArray());
-            response.ContentStream = stream;
-
             try
             {
-                // Optimistic check for JSON error object
-                if (content.ToMemory().Span[0] != (byte)'{')
-                {
-                    return false;
-                }
-
                 using JsonDocument doc = JsonDocument.Parse(content);
                 AcrErrors errors = AcrErrors.DeserializeAcrErrors(doc.RootElement);
                 if (errors?.Errors?.Count > 0)
@@ -50,12 +48,21 @@ namespace Azure.Containers.ContainerRegistry
             }
             catch
             {
-                // We won't get default logging of content since stream is unbuffered,
-                // so provide the message as an error message to surface it in the exception.
-                error = new(code: null, message: content.ToString());
+                // Content should be logged in exception message.
             }
 
             return error != null;
+        }
+
+        private static void BufferResponse(Response response)
+        {
+            Stream responseContentStream = response.ContentStream;
+
+            BinaryData content = BinaryData.FromStream(responseContentStream);
+
+            responseContentStream.Dispose();
+
+            response.ContentStream = new MemoryStream(content.ToArray());
         }
     }
 }
