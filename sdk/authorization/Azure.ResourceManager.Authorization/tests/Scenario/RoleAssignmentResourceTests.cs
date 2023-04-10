0c7d05ce573517c0935a3fafbe13ee1db94c45ba
Arthur Ma <arthurma@microsoft.com>
2023-04-10 14:15:40 +0800

    Add `DeleteIfNotExist` for the `RoleAssignmentResource`

---
+++ sdk/authorization/Azure.ResourceManager.Authorization/tests/Scenario/RoleAssignmentResourceTests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Authorization.Tests.Scenario
+{
+    internal class RoleAssignmentResourceTests : AuthorizationManagementTestBase
+    {
+        public RoleAssignmentResourceTests(bool isAsync)
+            : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        [Test]
+        public async Task DeleteIfNotExist()
+        {
+            var roleAssignmentName = "06640649-9667-478a-a27e-59e940fee731";
+            var roleAssignmentResourceId = $"/subscriptions/{DefaultSubscription.Data.SubscriptionId}/providers/Microsoft.Authorization/roleAssignments/{roleAssignmentName}";
+            var resource = Client.GetRoleAssignmentResource(new ResourceIdentifier(roleAssignmentResourceId));
+            try
+            {
+                await resource.DeleteAsync(WaitUntil.Started);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex is NullReferenceException);
+            }
+        }
+    }
+}
