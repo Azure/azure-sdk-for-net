2f022e87411423c72c9b9b3ccfad3a176e7ea6da
Wes Haggard <Wes.Haggard@microsoft.com>
2023-04-10 16:58:13 -0700

    Switch to using standard PAT tokens instead of base 64
    For most of these we can use the standard System.AccessToken given to
    the build instead of maintaining a specific token. However that
    token isn't base 64 encoded so we need to encode it.
    
    With this we can stop explicitly passing PAT's unless we need to
    access another DevOps org and we also don't have to remember
    to keep the PAT's in KV base 64 encoded.
    
    Add error detection for queue build script to fail if we get login response.

--- eng/common/scripts/Invoke-DevOpsAPI.ps1
+++ eng/common/scripts/Invoke-DevOpsAPI.ps1
@@ -2,6 +2,24 @@
 
 $DevOpsAPIBaseURI = "https://dev.azure.com/{0}/{1}/_apis/{2}/{3}?{4}api-version=6.0"
 
+function Get-Base64EncodedToken([string]$AuthToken)
+{
+  $unencodedAuthToken = "nobody:$AuthToken"
+  $unencodedAuthTokenBytes = [System.Text.Encoding]::UTF8.GetBytes($unencodedAuthToken)
+  $encodedAuthToken = [System.Convert]::ToBase64String($unencodedAuthTokenBytes)
+
+  if ($isDevOpsRun) {
+    # We are doing this here so that there is zero chance that this token is emitted in Azure Pipelines
+    # build logs. Azure Pipelines will see this text and register the secret as a value it should *** out
+    # before being transmitted to the server (and shown in logs). It means if the value is accidentally
+    # leaked anywhere else that it won't be visible. The downside is that when the script is executed
+    # on a local development box, it will be visible.
+    Write-Host "##vso[task.setvariable variable=_throwawayencodedaccesstoken;issecret=true;]$($encodedAuthToken)"
+  }
+
+  return $encodedAuthToken
+}
+
 function Get-DevOpsApiHeaders ($Base64EncodedToken) {
   $headers = @{
     Authorization = "Basic $Base64EncodedToken"
