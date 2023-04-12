69fcaf8d2b2b1ca191bf9975ba0c8b057e4098f0
Amanda Nguyen <amnguye@microsoft.com>
2023-04-12 14:51:28 -0700

    Rerecord expectedfailure tests

--- sdk/storage/Azure.Storage.DataMovement/src/Shared/DataTransferState.cs
+++ sdk/storage/Azure.Storage.DataMovement/src/Shared/DataTransferState.cs
@@ -5,7 +5,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
-using static Azure.Core.Pipeline.TaskExtensions;
+using Azure.Core.Pipeline;
 
 namespace Azure.Storage.DataMovement
 {
