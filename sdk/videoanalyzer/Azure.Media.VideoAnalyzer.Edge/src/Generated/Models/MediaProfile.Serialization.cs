bfad134992044935596d3f33e844c4d63796db3b
azure-sdk <azuresdk@microsoft.com>
2023-04-12 09:44:11 +0000

    Update SDK codes v*,w*,x*,y*,z*

--- sdk/videoanalyzer/Azure.Media.VideoAnalyzer.Edge/src/Generated/Models/MediaProfile.Serialization.cs
+++ sdk/videoanalyzer/Azure.Media.VideoAnalyzer.Edge/src/Generated/Models/MediaProfile.Serialization.cs
@@ -53,7 +53,6 @@ namespace Azure.Media.VideoAnalyzer.Edge.Models
                 {
                     if (property.Value.ValueKind == JsonValueKind.Null)
                     {
-                        property.ThrowNonNullablePropertyIsNull();
                         continue;
                     }
                     mediaUri = property.Value.GetObject();
@@ -63,7 +62,6 @@ namespace Azure.Media.VideoAnalyzer.Edge.Models
                 {
                     if (property.Value.ValueKind == JsonValueKind.Null)
                     {
-                        property.ThrowNonNullablePropertyIsNull();
                         continue;
                     }
                     videoEncoderConfiguration = VideoEncoderConfiguration.DeserializeVideoEncoderConfiguration(property.Value);
--- sdk/workloads/Azure.ResourceManager.Workloads/src/Generated/Models/StorageConfiguration.Serialization.cs
+++ sdk/workloads/Azure.ResourceManager.Workloads/src/Generated/Models/StorageConfiguration.Serialization.cs
@@ -36,7 +36,6 @@ namespace Azure.ResourceManager.Workloads.Models
                 {
                     if (property.Value.ValueKind == JsonValueKind.Null)
                     {
-                        property.ThrowNonNullablePropertyIsNull();
                         continue;
                     }
                     transportFileShareConfiguration = FileShareConfiguration.DeserializeFileShareConfiguration(property.Value);
