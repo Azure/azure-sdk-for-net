d137ca13bf2995c4a8f02dcb44d4f87c47ae83fa
Nikitha Chettiar <nikithauc@gmail.com>
2023-04-17 13:17:21 -0700

    testing with int pnh changes

--- sdk/communication/Azure.Communication.Chat/src/Notifications/CommunicationListener.cs
+++ sdk/communication/Azure.Communication.Chat/src/Notifications/CommunicationListener.cs
@@ -6,11 +6,11 @@ using System.Threading;
 using System.Threading.Tasks;
 using Azure.Communication.Chat.Notifications.Models;
 using Azure.Core;
-//using Microsoft.Trouter;
+using Microsoft.Trouter;
 
 namespace Azure.Communication.Chat.Notifications
 {
-    internal class CommunicationListener//: TrouterListener
+    internal class CommunicationListener:TrouterListener
     {
         private SyncAsyncEventHandler<ChatMessageReceivedEvent> _eventHandler;
 
@@ -23,22 +23,22 @@ namespace Azure.Communication.Chat.Notifications
             _eventHandler = eventHandler;
         }
 
-//        public override Task<TrouterResponse> ProcessRequestAsync(TrouterRequest request, CancellationToken cancellationToken = default)
-//        {
-//            ChatMessageReceivedEvent s = null;
-//            if (request.Body != null)
-//            {
-//                var str = request.Body.ToString();
-//                s = request.Body.ToObjectFromJson<ChatMessageReceivedEvent>();
-//            }
-//            _eventHandler(s);
-//#pragma warning disable CA1303 // Do not pass literals as localized parameters
-//            Console.WriteLine("this works");
+        public override Task<TrouterResponse> ProcessRequestAsync(TrouterRequest request, CancellationToken cancellationToken = default)
+        {
+            ChatMessageReceivedEvent s = null;
+            if (request.Body != null)
+            {
+                var str = request.Body.ToString();
+                s = request.Body.ToObjectFromJson<ChatMessageReceivedEvent>();
+            }
+            _eventHandler(s);
+#pragma warning disable CA1303 // Do not pass literals as localized parameters
+            Console.WriteLine("this works");
 
-//            Console.WriteLine("*******");
-//#pragma warning restore CA1303 // Do not pass literals as localized parameters
-//            // Avoid this trouter response return and may be have a void method
-//            return Task.FromResult(new TrouterResponse { Body = new BinaryData("") });
-//        }
+            Console.WriteLine("*******");
+#pragma warning restore CA1303 // Do not pass literals as localized parameters
+            // Avoid this trouter response return and may be have a void method
+            return Task.FromResult(new TrouterResponse { Body = new BinaryData("") });
+        }
     }
 }
--- sdk/communication/Azure.Communication.Chat/src/Notifications/CommunicationSignalingClient.cs
+++ sdk/communication/Azure.Communication.Chat/src/Notifications/CommunicationSignalingClient.cs
@@ -7,7 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Azure.Communication.Chat.Notifications.Models;
 using Azure.Core;
-//using Microsoft.Trouter;
+using Microsoft.Trouter;
 
 namespace Azure.Communication.Chat.Notifications
 {
@@ -16,7 +16,7 @@ namespace Azure.Communication.Chat.Notifications
     internal class CommunicationSignalingClient
 #pragma warning restore CA1001 // Types that own disposable fields should be disposable
     {
-       // private TrouterClient _trouterClient;
+       private TrouterClient _trouterClient;
         private bool _isRealTimeNotificationsStarted = true;
         private CommunicationTokenCredential _tokenCredential;
         private Dictionary<ChatEventType, List<CommunicationListener>> _eventHandlersMap = new Dictionary<ChatEventType, List<CommunicationListener>>();
@@ -33,8 +33,8 @@ namespace Azure.Communication.Chat.Notifications
 #pragma warning disable CA1822 // Mark members as static
         public async Task Start()
         {
-            //CreateTrouterService();
-            //await _trouterClient.StartAsync().ConfigureAwait(false);
+            CreateTrouterService();
+            await _trouterClient.StartAsync().ConfigureAwait(false);
         }
 
         /// <summary>
@@ -47,40 +47,60 @@ namespace Azure.Communication.Chat.Notifications
             {
                 return;
             }
-            //await _trouterClient.StopAsync().ConfigureAwait(false);
+            await _trouterClient.StopAsync().ConfigureAwait(false);
         }
 
         /// <summary>
         ///
         /// </summary>
 #pragma warning disable CA1822 // Mark members as static
-//        private void CreateTrouterService()
-//#pragma warning restore CA1822 // Mark members as static
-//        {
-//            _trouterClient = new TrouterClient(new TestSkypetokenCredential(_tokenCredential.GetToken().Token), CreateTrouterClientOptions());
-//        }
+        private void CreateTrouterService()
+#pragma warning restore CA1822 // Mark members as static
+        {
+            _trouterClient = new TrouterClient(new TestSkypetokenCredential(_tokenCredential.GetToken().Token), CreateTrouterClientOptions());
+        }
 
-        //private static TrouterClientOptions CreateTrouterClientOptions()
-        //{
-        //    var options = new TrouterClientOptions
-        //    {
-        //        ApplicationName = "Microsoft.Trouter.Tests",
-        //        TrouterHostName = "go.trouter.skype.com",
-        //        RegistrarHostName = "edge.skype.com/registrar/prod/",
-        //        RegistrationOptions = new RegistrationOptions
-        //        {
-        //            EndpointId = "trouter_test",
-        //            PlatformId = "Android",
-        //            PnhAppId = "AcsAndroid",
-        //            PnhTemplate = "AcsAndroid_Chat_1.3",
-        //            //PlatformId = "SPOOL",
-        //            //PnhAppId = "AcsWeb",
-        //            //PnhTemplate = "AcsWeb_Chat_1.5",
-        //        }
-        //    };
+        private static TrouterClientOptions CreateTrouterClientOptions()
+        {
+            Console.WriteLine("int wa..");
+            var options = new TrouterClientOptions
+            {
+                ApplicationName = "Microsoft.Trouter.Tests",
+                TrouterHostName = "go.trouter-int.skype.net",
+                RegistrarHostName = "edge.skype.net/registrar/testenv",
+                RegistrationOptions = new RegistrationOptions
+                {
+                    EndpointId = "go.trouter-int.skype.net",
+                    PlatformId = "Android",
+                    PnhAppId = "AcsDotNet_test",
+                    PnhTemplate = "AcsDotNet_Chat_test_1.6",
+                    //PlatformId = "SPOOL",
+                    //PnhAppId = "AcsWeb",
+                    //PnhTemplate = "AcsWeb_Chat_1.5",
+                }
+            };
 
-        //    return options;
-        //}
+            // int
+
+            //var options = new TrouterClientOptions
+            //{
+            //    ApplicationName = "Microsoft.Trouter.Tests",
+            //    TrouterHostName = "go.trouter.skype.com",
+            //    RegistrarHostName = "edge.skype.com/registrar/prod/",
+            //    RegistrationOptions = new RegistrationOptions
+            //    {
+            //        EndpointId = "trouter_test",
+            //        PlatformId = "Android",
+            //        PnhAppId = "AcsAndroid",
+            //        PnhTemplate = "AcsAndroid_Chat_1.3",
+            //        //PlatformId = "SPOOL",
+            //        //PnhAppId = "AcsWeb",
+            //        //PnhTemplate = "AcsWeb_Chat_1.5",
+            //    }
+            //};
+
+            return options;
+        }
 #pragma warning disable CA1822 // Mark members as static
         public void on(SyncAsyncEventHandler<ChatMessageReceivedEvent> eventHandler)
         {
@@ -95,7 +115,7 @@ namespace Azure.Communication.Chat.Notifications
                 _eventHandlersMap[ChatEventType.ChatMessageReceived] = new List<CommunicationListener> { listener };
             }
             Console.WriteLine("Registering event handler");
-           // _trouterClient.RegisterListener("chatMessageReceived", listener);
+           _trouterClient.RegisterListener("chatMessageReceived", listener);
         }
 
 //        public void off(SyncAsyncEventHandler<ChatMessageReceivedEvent> eventHandler)
@@ -108,30 +128,30 @@ namespace Azure.Communication.Chat.Notifications
     }
 
 #pragma warning disable SA1402 // File may only contain a single type
-    internal class TrouterClient
-#pragma warning restore SA1402 // File may only contain a single type
+//    internal class TrouterClient
+//#pragma warning restore SA1402 // File may only contain a single type
+//    {
+//        private readonly string _chatEventType = "";
+//        internal async Task StartAsync()
+//        {
+//            await Console.Out.WriteLineAsync(_chatEventType).ConfigureAwait(false);
+//        }
+//    }
+
+    internal sealed class TestSkypetokenCredential : SkypetokenCredential
     {
-        private readonly string _chatEventType = "";
-        internal async Task StartAsync()
+        private string _token;
+
+        public TestSkypetokenCredential(string token)
         {
-            await Console.Out.WriteLineAsync(_chatEventType).ConfigureAwait(false);
+            _token = token;
+        }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public override async Task<Skypetoken> GetTokenAsync(CancellationToken cancellationToken = default)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            return new Skypetoken(_token);
         }
     }
-
-    //    internal sealed class TestSkypetokenCredential : SkypetokenCredential
-    //    {
-    //        private string _token;
-
-    //        public TestSkypetokenCredential(string token)
-    //        {
-    //            _token = token;
-    //        }
-
-    //#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-    //        public override async Task<Skypetoken> GetTokenAsync(CancellationToken cancellationToken = default)
-    //#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
-    //        {
-    //            return new Skypetoken(_token);
-    //        }
-    //    }
 }
