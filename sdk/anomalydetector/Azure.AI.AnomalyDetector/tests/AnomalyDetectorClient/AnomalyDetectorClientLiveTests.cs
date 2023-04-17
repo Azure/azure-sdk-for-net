bf8b386704d30ff509ecb9b10f480ef965b6d2d1
Xingyu Yue <xingyuyue@microsoft.com>
2023-04-17 20:05:15 +0800

    Add multivariate test

--- sdk/anomalydetector/Azure.AI.AnomalyDetector/tests/AnomalyDetectorClient/AnomalyDetectorClientLiveTests.cs
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/tests/AnomalyDetectorClient/AnomalyDetectorClientLiveTests.cs
@@ -1,10 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.AI.AnomalyDetector.Tests.Infrastructure;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
+using Newtonsoft.Json;
 
 namespace Azure.AI.AnomalyDetector.Tests
 {
@@ -17,6 +23,7 @@ namespace Azure.AI.AnomalyDetector.Tests
     /// </remarks>
     public class AnomalyDetectorClientLiveTests : AnomalyDetectorLiveTestBase
     {
+        private string modelId;
         /// <summary>
         /// Initializes a new instance of the <see cref="AnomalyDetectorClientLiveTests"/> class.
         /// </summary>
@@ -94,6 +101,86 @@ namespace Azure.AI.AnomalyDetector.Tests
             }
 
             Assert.IsTrue(model_number >= 0);
+        }
+
+        [Test]
+        public async Task TrainMultivariateModel()
+        {
+            var client = CreateAnomalyDetectorClient();
+
+            string dataSource = TestEnvironment.DataSource;
+            TimeSpan offset = new TimeSpan(0);
+            DateTimeOffset startTime = new DateTimeOffset(2021, 1, 2, 0, 0, 0, offset);
+            DateTimeOffset endTime = new DateTimeOffset(2021, 1, 2, 5, 0, 0, offset);
+            ModelInfo modelInfo = new ModelInfo(dataSource, startTime, endTime);
+            modelInfo.DataSchema = DataSchema.OneTable;
+            modelInfo.SlidingWindow = 100;
+            modelInfo.AlignPolicy = new AlignPolicy();
+            modelInfo.AlignPolicy.AlignMode = new AlignMode("Outer");
+            modelInfo.AlignPolicy.FillNAMethod = new FillNAMethod("Linear");
+            modelInfo.AlignPolicy.PaddingValue = 0;
+            AnomalyDetectionModel response = await client.TrainMultivariateModelAsync(modelInfo);
+            string modelId = response.ModelId;
+            ModelStatus? status = response.ModelInfo.Status;
+            Assert.IsNotNull(status);
+            while (status != ModelStatus.Failed && status != ModelStatus.Ready)
+            {
+                System.Threading.Thread.Sleep(10000);
+                response = await client.GetMultivariateModelValueAsync(modelId);
+                status = response.ModelInfo.Status;
+            }
+            Assert.AreEqual(ModelStatus.Ready, status);
+            this.modelId = modelId;
+        }
+
+        [Test]
+        public async Task MultivariateBatchDetection()
+        {
+            var client = CreateAnomalyDetectorClient();
+            string dataSource = TestEnvironment.DataSource;
+            TimeSpan offset = new TimeSpan(0);
+            DateTimeOffset startTime = new DateTimeOffset(2021, 1, 2, 0, 0, 0, offset);
+            DateTimeOffset endTime = new DateTimeOffset(2021, 1, 2, 5, 0, 0, offset);
+            MultivariateBatchDetectionOptions request = new MultivariateBatchDetectionOptions(dataSource, 10, startTime, endTime);
+            MultivariateDetectionResult response = await client.DetectMultivariateBatchAnomalyAsync(this.modelId, request);
+            String resultId = response.ResultId;
+            MultivariateBatchDetectionStatus status = response.Summary.Status;
+            Assert.IsNotNull(status);
+            while (status != MultivariateBatchDetectionStatus.Ready && status != MultivariateBatchDetectionStatus.Failed)
+            {
+                System.Threading.Thread.Sleep(10000);
+                response = await client.GetMultivariateBatchDetectionResultValueAsync(resultId);
+                status = response.Summary.Status;
+            }
+        }
+
+        [Test]
+        public async Task MultivariateLastDetection()
+        {
+            var client = CreateAnomalyDetectorClient();
+            List<VariableValues> variables = new List<VariableValues>();
+            using (StreamReader r = new StreamReader("./samples/data/multivariate_sample_data.json"))
+            {
+                string json = r.ReadToEnd();
+                JsonElement lastDetectVariables = JsonDocument.Parse(json).RootElement.GetProperty("variables");
+                foreach (var index in Enumerable.Range(0, lastDetectVariables.GetArrayLength()))
+                {
+                    IEnumerable<string> timestamps = JsonConvert.DeserializeObject<IEnumerable<String>>(lastDetectVariables[index].GetProperty("timestamps").ToString());
+                    IEnumerable<float> values = JsonConvert.DeserializeObject<IEnumerable<float>>(lastDetectVariables[index].GetProperty("values").ToString());
+                    variables.Add(new VariableValues(lastDetectVariables[index].GetProperty("variable").ToString(), timestamps, values));
+                }
+            }
+            MultivariateLastDetectionOptions request = new MultivariateLastDetectionOptions(variables);
+            MultivariateLastDetectionResult response = await client.DetectMultivariateLastAnomalyAsync(this.modelId, request);
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Results);
+        }
+
+        [Test]
+        public async Task DeleteMultivariateModel()
+        {
+            var client = CreateAnomalyDetectorClient();
+            await client.DeleteMultivariateModelAsync(this.modelId);
         }
     }
 }
