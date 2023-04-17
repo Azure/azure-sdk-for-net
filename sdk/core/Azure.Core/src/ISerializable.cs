d22823c6a533534ed5cdc2f622d34b8322f901ae
Nisha Bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2023-04-17 15:35:05 -0700

    wip

---
+++ sdk/core/Azure.Core/src/ISerializable.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+
+public interface ISerializable
+{
+    bool TrySerialize(MemoryStream buffer, out int bytesWritten, SerializableOptions options = default);
+    bool TryDeserialize(ReadOnlyMemory<byte> data, out int bytesConsumed, SerializableOptions options = default);
+}
---
+++ sdk/core/Azure.Core/src/SerializableOptions.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+public class SerializableOptions
+{
+    public bool SerializeReadonlyProperties { get; set; } = true;
+    public bool HandleUnknownElements { get; set; } = true;
+    public bool PrettyPrint { get; set; } = false;
+}
