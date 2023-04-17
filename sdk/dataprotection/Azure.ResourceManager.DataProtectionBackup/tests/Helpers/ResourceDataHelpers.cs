54ff36cbed8c3506a993d1f22e7c1d68252c969b
mcgallan <1186003288@qq.com>
2023-04-17 11:13:39 +0800

    1

---
+++ sdk/dataprotection/Azure.ResourceManager.DataProtectionBackup/tests/Helpers/ResourceDataHelpers.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using Azure.ResourceManager.DataProtectionBackup.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources.Models;
+using NUnit.Framework;
+using Azure.Core;
+using System;
+using System.Text;
+using Azure.ResourceManager.Resources;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using System.IO;
+
+namespace Azure.ResourceManager.DataProtectionBackup.Tests.Helpers
+{
+    public static class ResourceDataHelpers
+    {
+        public static IDictionary<string, string> ReplaceWith(this IDictionary<string, string> dest, IDictionary<string, string> src)
+        {
+            dest.Clear();
+            foreach (var kv in src)
+            {
+                dest.Add(kv);
+            }
+
+            return dest;
+        }
+
+        public static void AssertResource(ResourceData r1, ResourceData r2)
+        {
+            Assert.AreEqual(r1.Name, r2.Name);
+            Assert.AreEqual(r1.Id, r2.Id);
+            Assert.AreEqual(r1.ResourceType, r2.ResourceType);
+        }
+
+        #region policy
+        public DataProtectionBackupPolicyData GetPolicyData()
+        {
+            var data = new DataProtectionBackupPolicyData()
+            {
+                Properties
+            };
+        }
+        #endregion
+    }
+}
