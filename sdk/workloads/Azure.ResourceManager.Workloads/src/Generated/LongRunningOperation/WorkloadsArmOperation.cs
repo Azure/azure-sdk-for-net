8278ecdcaaa4fdb0c943c79b5cd5b915cd4a1e6c
azure-sdk <azuresdk@microsoft.com>
2023-04-18 01:34:54 +0000

    Update SDK codes v*,w*,x*,y*,z*

--- sdk/workloads/Azure.ResourceManager.Workloads/src/Generated/LongRunningOperation/WorkloadsArmOperation.cs
+++ sdk/workloads/Azure.ResourceManager.Workloads/src/Generated/LongRunningOperation/WorkloadsArmOperation.cs
@@ -34,7 +34,7 @@ namespace Azure.ResourceManager.Workloads
         internal WorkloadsArmOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia)
         {
             var nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
-            _operation = new OperationInternal(clientDiagnostics, nextLinkOperation, response, "WorkloadsArmOperation", fallbackStrategy: new ExponentialDelayStrategy());
+            _operation = new OperationInternal(clientDiagnostics, nextLinkOperation, response, "WorkloadsArmOperation", fallbackStrategy: new Core.SequentialDelayStrategy());
         }
 
         /// <inheritdoc />
