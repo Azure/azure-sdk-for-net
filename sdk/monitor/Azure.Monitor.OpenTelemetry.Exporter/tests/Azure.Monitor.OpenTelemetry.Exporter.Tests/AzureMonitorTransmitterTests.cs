6eea13b993aa25758dfc4c56f5ad89750c4d994e
TimothyMothra <tilee@microsoft.com>
2023-04-20 10:41:33 -0700

    fix flaky test

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/AzureMonitorTransmitterTests.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/AzureMonitorTransmitterTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Azure.Monitor.OpenTelemetry.Exporter.Internals;
 using Azure.Monitor.OpenTelemetry.Exporter.Tests.CommonTestFramework;
 using Xunit;
@@ -30,6 +31,20 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Tests
             var connectionVars = AzureMonitorTransmitter.InitializeConnectionVars(options, platform);
 
             Assert.Equal(expectedIkey, connectionVars.InstrumentationKey);
+        }
+
+        [Fact]
+        public void VerifyConnectionString_ThrowsExceptionWhenInvalid()
+        {
+            Assert.Throws<InvalidOperationException>(() => new AzureMonitorTransmitter(new AzureMonitorExporterOptions { ConnectionString = null }, new MockPlatform()));
+        }
+
+        [Fact]
+        public void VerifyConnectionString_ThrowsExceptionWhenMissingInstrumentationKey()
+        {
+            var testEndpoint = "https://www.bing.com/";
+
+            Assert.Throws<InvalidOperationException>(() => new AzureMonitorTransmitter(new AzureMonitorExporterOptions { ConnectionString = $"IngestionEndpoint={testEndpoint}" }, new MockPlatform()));
         }
     }
 }
