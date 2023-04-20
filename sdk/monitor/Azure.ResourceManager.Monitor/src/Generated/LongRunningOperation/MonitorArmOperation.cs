e4d0c31d488ea4ef17810071afc02199a25fd6a4
azure-sdk <azuresdk@microsoft.com>
2023-04-20 18:49:11 +0000

    Update SDK codes m*,n*,o*

--- sdk/monitor/Azure.ResourceManager.Monitor/src/Generated/LongRunningOperation/MonitorArmOperation.cs
+++ sdk/monitor/Azure.ResourceManager.Monitor/src/Generated/LongRunningOperation/MonitorArmOperation.cs
@@ -34,7 +34,7 @@ namespace Azure.ResourceManager.Monitor
         internal MonitorArmOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia)
         {
             var nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
-            _operation = new OperationInternal(clientDiagnostics, nextLinkOperation, response, "MonitorArmOperation", fallbackStrategy: new ExponentialDelayStrategy());
+            _operation = new OperationInternal(nextLinkOperation, clientDiagnostics, response, "MonitorArmOperation", fallbackStrategy: new SequentialDelayStrategy());
         }
 
         /// <inheritdoc />
