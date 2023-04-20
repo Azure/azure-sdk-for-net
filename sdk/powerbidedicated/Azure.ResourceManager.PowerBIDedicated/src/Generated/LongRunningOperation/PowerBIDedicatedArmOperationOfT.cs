3091867f22f9af8a17435750ca9425dc75abfda3
azure-sdk <azuresdk@microsoft.com>
2023-04-20 05:59:48 +0000

    Update SDK codes p*,q*,r*

--- sdk/powerbidedicated/Azure.ResourceManager.PowerBIDedicated/src/Generated/LongRunningOperation/PowerBIDedicatedArmOperationOfT.cs
+++ sdk/powerbidedicated/Azure.ResourceManager.PowerBIDedicated/src/Generated/LongRunningOperation/PowerBIDedicatedArmOperationOfT.cs
@@ -34,7 +34,7 @@ namespace Azure.ResourceManager.PowerBIDedicated
         internal PowerBIDedicatedArmOperation(IOperationSource<T> source, ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia)
         {
             var nextLinkOperation = NextLinkOperationImplementation.Create(source, pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
-            _operation = new OperationInternal<T>(clientDiagnostics, nextLinkOperation, response, "PowerBIDedicatedArmOperation", fallbackStrategy: new ExponentialDelayStrategy());
+            _operation = new OperationInternal<T>(nextLinkOperation, clientDiagnostics, response, "PowerBIDedicatedArmOperation", fallbackStrategy: new SequentialDelayStrategy());
         }
 
         /// <inheritdoc />
