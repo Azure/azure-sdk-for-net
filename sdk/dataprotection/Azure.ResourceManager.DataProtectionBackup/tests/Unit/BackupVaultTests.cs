66a494c8bd4393975a5a80572b9f9f0d4483d15e
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2023-04-24 11:08:30 +0800

    Update BackupVaultTests.cs

--- sdk/dataprotection/Azure.ResourceManager.DataProtectionBackup/tests/Unit/BackupVaultTests.cs
+++ sdk/dataprotection/Azure.ResourceManager.DataProtectionBackup/tests/Unit/BackupVaultTests.cs
@@ -12,13 +12,21 @@ namespace Azure.ResourceManager.DataProtectionBackup.Tests.Unit
         [TestCase]
         public void ValidateDataSourceInfo()
         {
-            var dataSourceInfo = new DataSourceInfo(new ResourceIdentifier("/subs/id"));
-            var dataSourceSetInfo = new DataSourceSetInfo(new ResourceIdentifier("/subs/id"));
+            var dataSourceInfo = new DataSourceInfo(new ResourceIdentifier("/subs/id"))
+            {
+                ResourceUriString = "/sub/test"
+            };
+            var dataSourceSetInfo = new DataSourceSetInfo(new ResourceIdentifier("/subs/id"))
+            {
+                ResourceUriString = "/sub/test"
+            };
 
 #pragma warning disable CS0618 // Type or member is obsolete
             Assert.IsNull(dataSourceInfo.ResourceUri);
             Assert.IsNull(dataSourceSetInfo.ResourceUri);
 #pragma warning restore CS0618 // Type or member is obsolete
+            Assert.AreEqual("/sub/test", dataSourceInfo.ResourceUriString);
+            Assert.NotNull("/sub/test", dataSourceSetInfo.ResourceUriString);
         }
     }
 }
