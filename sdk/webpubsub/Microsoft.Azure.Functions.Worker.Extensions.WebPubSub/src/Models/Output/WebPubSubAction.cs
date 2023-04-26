50100a52b6a7f6b9f7e09c9f7a863752107de0ab
Jialin Xin <jixin@microsoft.com>
2023-04-26 14:16:46 +0800

    Support polymorphic serialization in output.

--- sdk/webpubsub/Microsoft.Azure.Functions.Worker.Extensions.WebPubSub/src/Models/Output/WebPubSubAction.cs
+++ sdk/webpubsub/Microsoft.Azure.Functions.Worker.Extensions.WebPubSub/src/Models/Output/WebPubSubAction.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Text.Json.Serialization;
+
 namespace Microsoft.Azure.Functions.Worker
 {
     /// <summary>
     /// Abstract class of operation to invoke service.
     /// </summary>
+    [JsonConverter(typeof(WebPubSubActionJsonConverter))]
     public abstract class WebPubSubAction
     {
         /// <summary>
