edc3ec8313d740765368a45f058591b0142e311e
mcgallan <1186003288@qq.com>
2023-04-27 16:28:30 +0800

    update

---
+++ sdk/healthbot/Azure.ResourceManager.HealthBot/tests/TestCase/HealthBotTests.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
+using Azure.ResourceManager.HealthBot.Models;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.HealthBot.Tests.TestCase
+{
+    public class HealthBotTests : HealthBotManagementTestBase
+    {
+
+        public HealthBotTests(bool isAsync)
+            : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        public async Task<HealthBotCollection> GetHealthBotCollection()
+        {
+            var group = await CreateResourceGroupAsync();
+            var collection = group.GetHealthBots();
+            return collection;
+        }
+
+        public HealthBotData GetHealthBotData()
+        {
+            var data = new HealthBotData(DefaultLocation, new HealthBotSku(HealthBotSkuName.F0));
+            return data;
+        }
+        public static void AssertData(HealthBotData data1, HealthBotData data2)
+        {
+            Assert.AreEqual(data1.Name, data2.Name);
+            Assert.AreEqual(data1.Identity, data2.Identity);
+            Assert.AreEqual(data1.Id, data2.Id);
+            Assert.AreEqual(data1.Location, data2.Location);
+            Assert.AreEqual(data1.SystemData, data2.SystemData);
+        }
+
+        [RecordedTest]
+        public async Task HealthBotApiTests()
+        {
+            //1.CreateOrUpdate
+            var collection = await GetHealthBotCollection();
+            var name = Recording.GenerateAssetName("healthbot-");
+            var name2 = Recording.GenerateAssetName("healthbot-");
+            var name3 = Recording.GenerateAssetName("healthbot-");
+            var input = GetHealthBotData();
+            var lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, name, input);
+            HealthBotResource healthBot1 = lro.Value;
+            Assert.AreEqual(name, healthBot1.Data.Name);
+            //2.Get
+            HealthBotResource HealthBot2 = await collection.GetAsync(name);
+            AssertData(healthBot1.Data, HealthBot2.Data);
+            //3.GetAll
+            _ = await collection.CreateOrUpdateAsync(WaitUntil.Completed, name, input);
+            _ = await collection.CreateOrUpdateAsync(WaitUntil.Completed, name2, input);
+            _ = await collection.CreateOrUpdateAsync(WaitUntil.Completed, name3, input);
+            int count = 0;
+            await foreach (var num in collection.GetAllAsync())
+            {
+                count++;
+            }
+            Assert.GreaterOrEqual(count, 3);
+            //4Exists
+            Assert.IsTrue(await collection.ExistsAsync(name));
+            Assert.IsFalse(await collection.ExistsAsync(name + "1"));
+
+            Assert.ThrowsAsync<ArgumentNullException>(async () => _ = await collection.ExistsAsync(null));
+            //Resource
+            //5.Get
+            HealthBotResource healthBot3 = await healthBot1.GetAsync();
+
+            AssertData(healthBot1.Data, healthBot3.Data);
+            //2.Delete
+            await healthBot1.DeleteAsync(WaitUntil.Completed);
+        }
+    }
+}
