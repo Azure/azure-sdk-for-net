6c020e6faa5014cfdd67fffe533003db9b210741
Anne Thompson <annelo@microsoft.com>
2023-05-03 12:03:51 -0700

    fix bad merge

--- sdk/core/Azure.Core/src/DynamicData/DynamicData.cs
+++ sdk/core/Azure.Core/src/DynamicData/DynamicData.cs
@@ -33,11 +33,11 @@ namespace Azure.Core.Dynamic
         private DynamicDataOptions _options;
         private JsonSerializerOptions _serializerOptions;
 
-        internal DynamicData(MutableJsonElement element, DynamicDataOptions options = default)
+        internal DynamicData(MutableJsonElement element, DynamicDataOptions? options = default)
         {
             _element = element;
-            _options = options;
-            _serializerOptions = options.NameMapping == DynamicDataNameMapping.None ?
+            _options = options ?? new DynamicDataOptions();
+            _serializerOptions = _options.NameMapping == DynamicDataNameMapping.None ?
                 new JsonSerializerOptions() :
                 new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
         }
