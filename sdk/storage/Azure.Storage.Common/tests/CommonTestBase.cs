311017d34686e1ad1e4b42d478e178403220b1aa
Sean McCullough <seanmcc@microsoft.com>
2023-05-03 16:12:36 -0500

    Added STG 89 and 90 service versions

--- sdk/storage/Azure.Storage.Common/tests/CommonTestBase.cs
+++ sdk/storage/Azure.Storage.Common/tests/CommonTestBase.cs
@@ -27,8 +27,10 @@ namespace Azure.Storage.Test
     BlobClientOptions.ServiceVersion.V2021_12_02,
     BlobClientOptions.ServiceVersion.V2022_11_02,
     BlobClientOptions.ServiceVersion.V2023_01_03,
-    RecordingServiceVersion = BlobClientOptions.ServiceVersion.V2023_01_03,
-    LiveServiceVersions = new object[] { BlobClientOptions.ServiceVersion.V2021_12_02 })]
+    BlobClientOptions.ServiceVersion.V2023_05_03,
+    BlobClientOptions.ServiceVersion.V2023_08_03,
+    RecordingServiceVersion = BlobClientOptions.ServiceVersion.V2023_08_03,
+    LiveServiceVersions = new object[] { BlobClientOptions.ServiceVersion.V2023_01_03 })]
     public abstract class CommonTestBase : StorageTestBase<StorageTestEnvironment>
     {
         protected readonly BlobClientOptions.ServiceVersion _serviceVersion;
--- sdk/storage/Azure.Storage.Files.DataLake/api/Azure.Storage.Files.DataLake.net6.0.cs
+++ sdk/storage/Azure.Storage.Files.DataLake/api/Azure.Storage.Files.DataLake.net6.0.cs
@@ -27,6 +27,8 @@ namespace Azure.Storage.Files.DataLake
             V2021_12_02 = 15,
             V2022_11_02 = 16,
             V2023_01_03 = 17,
+            V2023_05_03 = 18,
+            V2023_08_03 = 19,
         }
     }
     public partial class DataLakeDirectoryClient : Azure.Storage.Files.DataLake.DataLakePathClient
