638c72df9e73b610ad4f10da8145df318b764282
Meng Ai <mengai@microsoft.com>
2023-05-03 18:48:17 +0800

    Add blocklist management sample

---
+++ sdk/contentsafety/Azure.AI.ContentSafety/tests/Samples/Sample3_ManageBlocklist.cs
@@ -0,0 +1,146 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Azure.AI.ContentSafety.Tests.Samples
+{
+    public partial class ContentSafetySamples : SamplesBase<ContentSafetyClientTestEnvironment>
+    {
+        [Test]
+        [SyncOnly]
+        public void ManageBlocklist()
+        {
+            #region Snippet:CreateContentSafetyClient
+
+            string endpoint = TestEnvironment.Endpoint;
+            string key = TestEnvironment.Key;
+
+            ContentSafetyClient client = new ContentSafetyClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+            #endregion
+
+            #region Snippet:ListBlocklists
+
+            var blocklists = client.GetTextBlocklists();
+            Console.WriteLine("My blocklists:");
+            foreach (var blocklist in blocklists)
+            {
+                Console.WriteLine(String.Format("BlocklistName: {0}, Description: {1}", blocklist.BlocklistName, blocklist.Description));
+            }
+            #endregion
+
+            #region Snippet:CreateNewBlocklist
+
+            var blocklistName = "TestBlocklist";
+            //var blocklistDescription = "Test blocklist management";
+
+            //client.CreateOrUpdateTextBlocklist(blocklistName);
+
+            #endregion
+
+            #region Snippet:GetBlocklist
+
+            var newBlocklist = client.GetTextBlocklist(blocklistName);
+            if (newBlocklist != null && newBlocklist.Value != null)
+            {
+                Console.WriteLine("Blocklist created:");
+                Console.WriteLine(String.Format("BlocklistName: {0}, Description: {1}", newBlocklist.Value.BlocklistName, newBlocklist.Value.Description));
+            }
+
+            #endregion
+
+            #region Snippet:AddBlockItems
+
+            string blockItemText1 = "k*ll";
+            string blockItemText2 = "h*te";
+
+            var blockItems = new TextBlockItemInfo[] { new TextBlockItemInfo(blockItemText1), new TextBlockItemInfo(blockItemText2) };
+            var addedBlockItems = client.AddBlockItems(blocklistName, new AddBlockItemsOptions(blockItems));
+
+            if (addedBlockItems != null && addedBlockItems.Value != null)
+            {
+                Console.WriteLine("BlockItems added:");
+                foreach (var addedBlockItem in addedBlockItems.Value.Value)
+                {
+                    {
+                        Console.WriteLine(String.Format("BlockItemId: {0}, Text: {1}, Description: {2}", addedBlockItem.BlockItemId, addedBlockItem.Text, addedBlockItem.Description));
+                    }
+                }
+
+                #endregion
+
+                #region Snippet:RemoveBlockItems
+
+                var removeBlockItemId = addedBlockItems.Value.Value[1].BlockItemId;
+                var removeBlockItemIds = new List<string> { removeBlockItemId };
+                var removeResult = client.RemoveBlockItems(blocklistName, new RemoveBlockItemsOptions(removeBlockItemIds));
+
+                if (removeResult != null && removeResult.Status == 204)
+                {
+                    Console.WriteLine(String.Format("BlockItem {0} removed.", removeBlockItemId));
+                }
+
+                #endregion
+
+                //#region Snippet: ListBlockItems
+
+                //var remainingBlockItems = client.(blocklistName);
+                //foreach (var blocklistItem in remainingBlockItems)
+                //{
+                //    Console.WriteLine(String.Format("BlockItemId: {0}, Text: {1}, Description: {2}", blocklistItem, blocklistItem.Text, blocklistItem.Description));
+                //}
+
+                //#endregion
+
+                #region Snippet:AnalyzeTextWithBlocklist
+                Thread.Sleep(10000);
+                var request = new AnalyzeTextOptions("I want to k*ll you");
+                request.BlocklistNames.Add(blocklistName);
+                request.BreakByBlocklists = true;
+                Response<AnalyzeTextResult> response;
+                try
+                {
+                    response = client.AnalyzeText(request);
+                }
+                catch (RequestFailedException ex)
+                {
+                    Console.WriteLine(String.Format("Analyze text failed: {0}", ex.Message));
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(String.Format("Analyze text error: {0}", ex.Message));
+                    throw;
+                }
+
+                if (response.Value.BlocklistsMatchResults != null)
+                {
+                    foreach (var matchResult in response.Value.BlocklistsMatchResults)
+                    {
+                        Console.WriteLine(String.Format("{0} -{1}-{2}-{3}-{4}", matchResult.BlocklistName, matchResult.BlockItemId, matchResult.BlocklistName, matchResult.Offset, matchResult.Length));
+                    }
+                }
+
+                #endregion
+
+                #region Snippet:DeleteBlocklist
+
+                var deleteResult = client.DeleteTextBlocklist(blocklistName);
+                if (deleteResult != null && deleteResult.Status == 204)
+                {
+                    Console.WriteLine("Delete blocklist succeded.");
+                }
+
+                #endregion
+            }
+        }
+    }
+}
