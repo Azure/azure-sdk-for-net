9080710d6910f8c8afea9a324ef4f5684a524673
azure-sdk <azuresdk@microsoft.com>
2023-05-05 05:44:27 +0000

    Update SDK codes

--- sdk/eventgrid/Azure.ResourceManager.EventGrid/src/Generated/LongRunningOperation/EventGridArmOperation.cs
+++ sdk/eventgrid/Azure.ResourceManager.EventGrid/src/Generated/LongRunningOperation/EventGridArmOperation.cs
@@ -31,9 +31,9 @@ namespace Azure.ResourceManager.EventGrid
             _operation = OperationInternal.Succeeded(response);
         }
 
-        internal EventGridArmOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia)
+        internal EventGridArmOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia, bool skipApiVersionOverride = false)
         {
-            var nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
+            var nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia, skipApiVersionOverride);
             _operation = new OperationInternal(nextLinkOperation, clientDiagnostics, response, "EventGridArmOperation", fallbackStrategy: new SequentialDelayStrategy());
         }
 
