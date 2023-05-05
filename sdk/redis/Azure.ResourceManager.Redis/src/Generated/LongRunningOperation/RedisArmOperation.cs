c2b8c8b824bd17cc224228ea0c21d8fde2b19919
azure-sdk <azuresdk@microsoft.com>
2023-05-05 06:38:34 +0000

    Update SDK codes

--- sdk/redis/Azure.ResourceManager.Redis/src/Generated/LongRunningOperation/RedisArmOperation.cs
+++ sdk/redis/Azure.ResourceManager.Redis/src/Generated/LongRunningOperation/RedisArmOperation.cs
@@ -31,9 +31,9 @@ namespace Azure.ResourceManager.Redis
             _operation = OperationInternal.Succeeded(response);
         }
 
-        internal RedisArmOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia)
+        internal RedisArmOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia, bool skipApiVersionOverride = false)
         {
-            var nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
+            var nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia, skipApiVersionOverride);
             _operation = new OperationInternal(nextLinkOperation, clientDiagnostics, response, "RedisArmOperation", fallbackStrategy: new SequentialDelayStrategy());
         }
 
--- sdk/servicefabric/Azure.ResourceManager.ServiceFabric/src/Generated/LongRunningOperation/ServiceFabricArmOperation.cs
+++ sdk/servicefabric/Azure.ResourceManager.ServiceFabric/src/Generated/LongRunningOperation/ServiceFabricArmOperation.cs
@@ -31,9 +31,9 @@ namespace Azure.ResourceManager.ServiceFabric
             _operation = OperationInternal.Succeeded(response);
         }
 
-        internal ServiceFabricArmOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia)
+        internal ServiceFabricArmOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia, bool skipApiVersionOverride = false)
         {
-            var nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
+            var nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia, skipApiVersionOverride);
             _operation = new OperationInternal(nextLinkOperation, clientDiagnostics, response, "ServiceFabricArmOperation", fallbackStrategy: new SequentialDelayStrategy());
         }
 
