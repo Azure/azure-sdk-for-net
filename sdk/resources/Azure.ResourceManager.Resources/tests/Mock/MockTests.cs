23b4475ce2a002cbcbd27b3464048b806d411117
Arcturus Zhang <dapzhang@microsoft.com>
2023-05-05 11:47:04 +0800

    add a simplest sample for mockign extension method

---
+++ sdk/resources/Azure.ResourceManager.Resources/tests/Mock/MockTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.ResourceManager.Resources.Mock;
+using Azure.ResourceManager.Resources.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Resources.Tests.Mock
+{
+    public class MockTests
+    {
+        public MockTests() { }
+
+        [Test]
+        public async Task MockExtensionMethods()
+        {
+            // setup the mock for the extension
+            var armDeploymentResourceExtensionMock = new Mock<ArmDeploymentResourceExtension>();
+            var mockTemplate = BinaryData.FromString("mockTemplate");
+            var mockResult = ArmResourcesModelFactory.TemplateHashResult("minifiedTemplate", "mockTemplateHash");
+            armDeploymentResourceExtensionMock.Setup(extension => extension.CalculateDeploymentTemplateHashAsync(mockTemplate, default)).Returns(Task.FromResult(Response.FromValue(mockResult, null)));
+
+            // we have to setup the mock for subscriptionResource.GetCachedClient to let it returns the mock of ArmDeploymentResourceExtension
+            var tenantResourceMock = new Mock<TenantResource>();
+            tenantResourceMock.Setup(sub => sub.GetCachedClient(It.IsAny<Func<ArmClient, ArmDeploymentResourceExtension>>())).Returns(armDeploymentResourceExtensionMock.Object);
+            var tenantResource = tenantResourceMock.Object;
+
+            // invoke the mock
+            var response = await tenantResource.CalculateDeploymentTemplateHashAsync(mockTemplate);
+        }
+    }
+}
