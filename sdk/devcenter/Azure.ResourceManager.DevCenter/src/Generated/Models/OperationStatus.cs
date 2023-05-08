6498a4b9e9c639af59b736d68dc1091a71b9b906
tbyfield <68976176+tbyfield@users.noreply.github.com>
2023-05-07 22:53:16 -0700

    Work around autorest generation issue with operation status id. The base class wants this as a ResourceIdentifier, but the generated code is passing it as a string.

--- sdk/devcenter/Azure.ResourceManager.DevCenter/src/Generated/Models/OperationStatus.cs
+++ sdk/devcenter/Azure.ResourceManager.DevCenter/src/Generated/Models/OperationStatus.cs
@@ -24,23 +24,6 @@ namespace Azure.ResourceManager.DevCenter.Models
             Argument.AssertNotNull(status, nameof(status));
         }
 
-        /// <summary> Initializes a new instance of OperationStatus. </summary>
-        /// <param name="id"> Fully qualified ID for the async operation. </param>
-        /// <param name="name"> Name of the async operation. </param>
-        /// <param name="status"> Operation status. </param>
-        /// <param name="percentComplete"> Percent of the operation that is complete. </param>
-        /// <param name="startOn"> The start time of the operation. </param>
-        /// <param name="endOn"> The end time of the operation. </param>
-        /// <param name="operations"> The operations list. </param>
-        /// <param name="error"> If present, details of the operation error. </param>
-        /// <param name="resourceId"> The id of the resource. </param>
-        /// <param name="properties"> Custom operation properties, populated only for a successful operation. </param>
-        internal OperationStatus(string id, string name, string status, float? percentComplete, DateTimeOffset? startOn, DateTimeOffset? endOn, IReadOnlyList<OperationStatusResult> operations, ResponseError error, string resourceId, BinaryData properties) : base(new ResourceIdentifier(id), name, status, percentComplete, startOn, endOn, operations, error)
-        {
-            ResourceId = resourceId;
-            Properties = properties;
-        }
-
         /// <summary> The id of the resource. </summary>
         public string ResourceId { get; }
         /// <summary>
