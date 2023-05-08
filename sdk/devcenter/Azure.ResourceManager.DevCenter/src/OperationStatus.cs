5e80ed9379fbc12f193d9f0b755c8218006dbc56
tbyfield <68976176+tbyfield@users.noreply.github.com>
2023-05-07 23:05:09 -0700

    Add missing file header

--- sdk/devcenter/Azure.ResourceManager.DevCenter/src/OperationStatus.cs
+++ sdk/devcenter/Azure.ResourceManager.DevCenter/src/OperationStatus.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using System.Collections.Generic;
-using System.Text;
 using Azure.Core;
 using Azure.ResourceManager.Models;
 
