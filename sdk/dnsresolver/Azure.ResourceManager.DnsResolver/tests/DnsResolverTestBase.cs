d02b89e22e621ed1f0397eaa8d671adc4a4c31b3
mcgallan <1186003288@qq.com>
2023-05-09 16:21:42 +0800

    DnsResolver

--- sdk/dnsresolver/Azure.ResourceManager.DnsResolver/tests/DnsResolverTestBase.cs
+++ sdk/dnsresolver/Azure.ResourceManager.DnsResolver/tests/DnsResolverTestBase.cs
@@ -75,20 +75,9 @@ namespace Azure.ResourceManager.DnsResolver.Tests
             //await virtualNetworks.CreateOrUpdateAsync(WaitUntil.Completed, virtualNetworkName, vnet);
             ResourceIdentifier vnetID;
             ResourceIdentifier subnetID;
-            if (Mode == RecordedTestMode.Playback)
-            {
-                subnetID = SubnetResource.CreateResourceIdentifier(resourceGroup.Id.SubscriptionId, resourceGroup.Id.Name, vnetName, SubnetName);
-                vnetID = VirtualNetworkResource.CreateResourceIdentifier(resourceGroup.Id.SubscriptionId, resourceGroup.Id.Name, vnetName);
-            }
-            else
-            {
-                using (Recording.DisableRecording())
-                {
-                    var vnetResource = await resourceGroup.GetVirtualNetworks().CreateOrUpdateAsync(WaitUntil.Completed, vnetName, vnet);
-                    vnetID = vnetResource.Value.Data.Id;
-                    subnetID = vnetResource.Value.Data.Subnets[0].Id;
-                }
-            }
+            var vnetResource = await resourceGroup.GetVirtualNetworks().CreateOrUpdateAsync(WaitUntil.Completed, vnetName, vnet);
+            vnetID = vnetResource.Value.Data.Id;
+            subnetID = vnetResource.Value.Data.Subnets[0].Id;
             DefaultVnetID = vnetID;
             DefaultSubnetID = subnetID;
             return (DefaultVnetID, DefaultSubnetID);
