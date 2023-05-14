190c5b27ff44a0bdb4585397e0c02714e453d841
felipmiguel <fmiguel@microsoft.com>
2023-05-14 22:56:44 +0200

    basic tests for access token retrieval

---
+++ sdk/extensions/Microsoft.Azure.Data.Extensions.Common/tests/ConfigurationTestEnvironment.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Data.Extensions.Common.Tests
+{
+    public class ConfigurationTestEnvironment : TestEnvironment
+    {
+    }
+}
--- sdk/extensions/Microsoft.Azure.Data.Extensions.Common/tests/TokenCredentialBaseAuthenticationProviderTests.cs
+++ sdk/extensions/Microsoft.Azure.Data.Extensions.Common/tests/TokenCredentialBaseAuthenticationProviderTests.cs
@@ -1,55 +1,41 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.Identity;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Data.Extensions.Common.Tests
 {
-    public class TokenCredentialBaseAuthenticationProviderTests
+    public class TokenCredentialBaseAuthenticationProviderTests : LiveTestBase<ConfigurationTestEnvironment>
     {
-        //[Test]
-        //public async Task NoExtensionDefaultAzureCredential()
-        //{
-        //    TokenCredentialNpgsqlPasswordProvider passwordProvider = new TokenCredentialNpgsqlPasswordProvider(new DefaultAzureCredential());
-        //    // Connection string does not contain password
-        //    NpgsqlDataSourceBuilder dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration.GetConnectionString());
-        //    NpgsqlDataSource dataSource = dataSourceBuilder
-        //                    .UsePeriodicPasswordProvider(passwordProvider.PasswordProvider, TimeSpan.FromMinutes(2), TimeSpan.FromMilliseconds(100))
-        //                    .Build();
-        //    await ValidateDataSourceAsync(dataSource);
-        //}
+        [Test]
+        public async Task CachingCreds()
+        {
+            TokenCredentialBaseAuthenticationProvider authenticationPlugin = new TokenCredentialBaseAuthenticationProvider(TestEnvironment.Credential);
+            string[] tokens = await Task.WhenAll(authenticationPlugin.GetAuthenticationTokenAsync().AsTask(), authenticationPlugin.GetAuthenticationTokenAsync().AsTask());
+            Assert.AreEqual(tokens[0], tokens[1]);
+            string accessToken1 = await authenticationPlugin.GetAuthenticationTokenAsync();
+            string accessToken2 = await authenticationPlugin.GetAuthenticationTokenAsync();
+            Assert.AreEqual(accessToken1, accessToken2);
+            Assert.AreEqual(accessToken1, tokens[1]);
+        }
 
-        ////[TestCategory("server-only")]
-        //[Test]
-        //public async Task NoExtensionConstructorWithManagedIdentity()
-        //{
-        //    Assert.IsNotNull(configuration);
-        //    string? managedIdentityClientId = configuration.GetManagedIdentityClientId();
-        //    Assert.IsNotNull(managedIdentityClientId);
-        //    TokenCredentialNpgsqlPasswordProvider passwordProvider = new TokenCredentialNpgsqlPasswordProvider(new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = managedIdentityClientId }));
-        //    NpgsqlDataSourceBuilder dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration.GetConnectionString());
-        //    NpgsqlDataSource dataSource = dataSourceBuilder
-        //                    .UsePeriodicPasswordProvider(passwordProvider.PasswordProvider, TimeSpan.FromMinutes(2), TimeSpan.FromMilliseconds(100))
-        //                    .Build();
-        //    await ValidateDataSourceAsync(dataSource);
-        //}
+        [Test]
+        public async Task NoCachingCreds()
+        {
+            const string OSSRDBMS_SCOPE = "https://ossrdbms-aad.database.windows.net/.default";
+            TokenRequestContext requestContext = new TokenRequestContext(new string[] { OSSRDBMS_SCOPE });
+            var token1 = await TestEnvironment.Credential.GetTokenAsync(requestContext, CancellationToken.None);
 
-        ////[TestCategory("local-only")]
-        //[Test]
-        //public async Task NoExtensionConstructorWithTokenCredential()
-        //{
-        //    Assert.IsNotNull(configuration);
-        //    AzureCliCredential credential = new AzureCliCredential();
-        //    TokenCredentialNpgsqlPasswordProvider passwordProvider = new TokenCredentialNpgsqlPasswordProvider(credential);
-        //    NpgsqlDataSourceBuilder dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration.GetConnectionString());
-        //    NpgsqlDataSource dataSource = dataSourceBuilder
-        //                    .UsePeriodicPasswordProvider(passwordProvider.PasswordProvider, TimeSpan.FromMinutes(2), TimeSpan.FromMilliseconds(100))
-        //                    .Build();
-        //    await ValidateDataSourceAsync(dataSource);
-        //}
+            var token2 = await TestEnvironment.Credential.GetTokenAsync(requestContext, CancellationToken.None);
+            Assert.AreEqual(token1.Token, token2.Token);
+        }
     }
 }
