8aa47e0141844fcb0e5650a0772045cf25350152
felipmiguel <fmiguel@microsoft.com>
2023-05-15 17:19:52 +0200

    updated API definitions

---
+++ sdk/extensions/Microsoft.Azure.Data.Extensions.Npgsql.EntityFrameworkCore/api/Microsoft.Azure.Data.Extensions.Npgsql.EntityFrameworkCore.net6.0.cs
@@ -0,0 +1,7 @@
+namespace Microsoft.EntityFrameworkCore
+{
+    public static partial class DbContextOptionsBuilderExtension
+    {
+        public static Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.NpgsqlDbContextOptionsBuilder UseAzureADAuthentication(this Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.NpgsqlDbContextOptionsBuilder optionsBuilder, Azure.Core.TokenCredential credential) { throw null; }
+    }
+}
--- sdk/extensions/Microsoft.Azure.Data.Extensions.Npgsql/api/Microsoft.Azure.Data.Extensions.Npgsql.netstandard2.0.cs
+++ sdk/extensions/Microsoft.Azure.Data.Extensions.Npgsql/api/Microsoft.Azure.Data.Extensions.Npgsql.netstandard2.0.cs
@@ -1,63 +1,25 @@
-using Azure.Core;
-using Microsoft.Azure.Data.Extensions.Common;
-using Npgsql;
-
-namespace Microsoft.Azure.Data.Extensions.Npgsql
+namespace Microsoft.Azure.Data.Extensions.Common
 {
-    /// <summary>
-    /// Provides implementations for Npgsql delegates to get passwords that can be used with Azure Database for Postgresql
-    /// Passwords provided are access tokens issued by Azure AD.
-    /// </summary>
-    public partial class TokenCredentialNpgsqlPasswordProvider : TokenCredentialBaseAuthenticationProvider
+    public partial class TokenCredentialBaseAuthenticationProvider
     {
-        /// <summary>
-        /// Token credential provided by the caller that will be used to retrieve AAD access tokens.
-        /// </summary>
-        /// <param name="credential">TokenCredential to use to retrieve AAD access tokens</param>
-        public TokenCredentialNpgsqlPasswordProvider(TokenCredential credential) : base(credential)
-        { }
-
-        /// <summary>
-        /// Method that implements NpgsqlDbContextOptionsBuilder.ProvidePasswordCallback delegate signature.
-        /// It can used in Entity Framework DbContext configuration
-        /// </summary>
-        /// <param name="host">Just part of the delegate signature. It is ignored</param>
-        /// <param name="port">Just part of the delegate signature. It is ignored</param>
-        /// <param name="database">Just part of the delegate signature. It is ignored</param>
-        /// <param name="username">Just part of the delegate signature. It is ignored</param>
-        /// <returns></returns>
-        public partial string ProvidePasswordCallback(string host, int port, string database, string username) { }
-
-        /// <summary>
-        /// Method that implements NpgsqlDataSourceBuilder.UsePeriodicPasswordProvider delegate signature.
-        /// <see href="https://www.npgsql.org/doc/security.html?tabs=tabid-1#auth-token-rotation-and-dynamic-password"/>
-        /// </summary>
-        /// <param name="settings">ConnectionString settings</param>
-        /// <param name="cancellationToken">token to propagate cancellation</param>
-        /// <returns>AAD issued access token that can be used as password for Azure Database for Postgresql</returns>
-        private partial ValueTask<string> PeriodicPasswordProvider(NpgsqlConnectionStringBuilder settings, CancellationToken cancellationToken = default) { }
-
-        /// <summary>
-        /// Property that holds an implementation of NpgsqlDataSourceBuilder.UsePeriodicPasswordProvider delegate signature.
-        /// </summary>
-        public partial Func<NpgsqlConnectionStringBuilder, CancellationToken, ValueTask<string>> PasswordProvider { get; }
+        public TokenCredentialBaseAuthenticationProvider(Azure.Core.TokenCredential credential) { }
+        public string GetAuthenticationToken(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public System.Threading.Tasks.ValueTask<string> GetAuthenticationTokenAsync(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
     }
 }
-
+namespace Microsoft.Azure.Data.Extensions.Npgsql
+{
+    public partial class TokenCredentialNpgsqlPasswordProvider : Microsoft.Azure.Data.Extensions.Common.TokenCredentialBaseAuthenticationProvider
+    {
+        public TokenCredentialNpgsqlPasswordProvider(Azure.Core.TokenCredential credential) : base (default(Azure.Core.TokenCredential)) { }
+        public System.Func<Npgsql.NpgsqlConnectionStringBuilder, System.Threading.CancellationToken, System.Threading.Tasks.ValueTask<string>> PasswordProvider { get { throw null; } }
+        public string ProvidePasswordCallback(string host, int port, string database, string username) { throw null; }
+    }
+}
 namespace Npgsql
 {
-    /// <summary>
-    /// NpgsqlDataSourceBuilder extensions that simplify the configuration to use AAD authentication when connecting to Azure Database for Postgresql
-    /// </summary>
     public static partial class NpgsqlDataSourceBuilderExtensions
     {
-        /// <summary>
-        /// Configures NpgsqlDataSourceBuilder to use AAD authentication to connect to Azure Database for Postgresql using the provided TokenCredential
-        /// </summary>
-        /// <param name="builder">NpgsqlDataSourceBuilder to be configured with AAD authentication</param>
-        /// <param name="credential">TokenCredential that will be used to retrieve AAD access tokens</param>
-        /// <returns>NpgsqlDataSourceBuilder configured with AAD authentication</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="builder"/> or <paramref name="credential"/> are null</exception>
-        public static partial NpgsqlDataSourceBuilder UseAzureADAuthentication(this NpgsqlDataSourceBuilder builder, TokenCredential credential) { }
+        public static Npgsql.NpgsqlDataSourceBuilder UseAzureADAuthentication(this Npgsql.NpgsqlDataSourceBuilder builder, Azure.Core.TokenCredential credential) { throw null; }
     }
 }
