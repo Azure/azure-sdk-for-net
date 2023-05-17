7e28e1d69c3e12851dacb06e0f27a4aab4bc946f
m-nash <prognash@gmail.com>
2023-05-17 15:28:24 -0700

    add scaffolding for readme

---
+++ sdk/core/Azure.Core/samples/Serialization.md
@@ -0,0 +1,71 @@
+# Azure.Core public serialization samples
+
+## Using IJsonSerialization Try methods
+
+Serialization
+
+```C# Snippet:Try_Serialize
+//TODO
+```
+
+Deserialization
+
+```C# Snippet:Try_Deserialize
+//TODO
+```
+
+## Using IJsonSerialization Non-Try methods
+
+Serialization
+
+```C# Snippet:NonTry_Serialize
+//TODO
+```
+
+Deserialization
+
+```C# Snippet:NonTry_Deserialize
+//TODO
+```
+
+## Using explicit cast
+
+Serialization
+
+```C# Snippet:ExplicitCast_Serialize
+//TODO
+```
+
+Deserialization
+
+```C# Snippet:ExplicitCast_Deserialize
+//TODO
+```
+
+## Using System.Text.Json
+
+Serialization
+
+```C# Snippet:Stj_Serialize
+//TODO
+```
+
+Deserialization
+
+```C# Snippet:Stj_Deserialize
+//TODO
+```
+
+## Using static deserializer
+
+Serialization
+
+```C# Snippet:Static_Serialize
+//TODO
+```
+
+Deserialization
+
+```C# Snippet:Static_Deserialize
+//TODO
+```
---
+++ sdk/core/Azure.Core/tests/samples/SerializationSamples.cs
@@ -0,0 +1,107 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using NUnit.Framework;
+
+namespace Azure.Core.Samples
+{
+    public class ResponseSamples
+    {
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task TrySerialize()
+        {
+            #region Snippet:Try_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task TryDeserialize()
+        {
+            #region Snippet:Try_Deserialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task NonTrySerialize()
+        {
+            #region Snippet:NonTry_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task NonTryDeserialize()
+        {
+            #region Snippet:NonTry_Deserialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task ExplicitCastSerialize()
+        {
+            #region Snippet:ExplicitCast_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task ExplicitCastSerialize()
+        {
+            #region Snippet:ExplicitCast_Deserialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task StjSerialize()
+        {
+            #region Snippet:Stj_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task StjDeserialize()
+        {
+            #region Snippet:Stj_Deserialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task StaticSerialize()
+        {
+            #region Snippet:Static_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task StaticDeserialize()
+        {
+            #region Snippet:Static_Deserialize
+            //TODO
+            #endregion
+        }
+    }
+}
