eaf193c17ef0d9040cd7aed2246e4c8cc7c78e1a
Chengming <v-cruan@microsoft.com>
2023-05-17 16:57:29 +0800

    Update Track2EnableModelFactory.ps1

--- eng/scripts/Track2EnableModelFactory.ps1
+++ eng/scripts/Track2EnableModelFactory.ps1
@@ -2,8 +2,9 @@ function Update-AutorestMd([string]$file) {
     $fileContent = Get-Content $file
     $store = @()
     foreach ($item in $fileContent) {
-        if ($item.Contains("skip-csproj: true")) {
-            $store += "generate-modelfactory: true"
+        if ($item.Contains("generate-model-factory:")) {
+            $store += "generate-model-factory: true"
+            continue
         }
         $store += $item
     }
@@ -16,7 +17,7 @@ function Update-ChangeLog([string]$file) {
     $store = @()
     foreach ($item in $fileContent) {
         if ($item.Contains("### Breaking Changes") -and $flag) {
-            $store += "- Add model factory"
+            $store += "- Add model factory`n"
             $flag = $false
         }
         $store += $item
@@ -25,10 +26,10 @@ function Update-ChangeLog([string]$file) {
 }
 
 function  Enable-ModelFactory {
-    param(
-        [Parameter()]
-        [bool]$output = $false
-    )
+    # param(
+    #     [Parameter()]
+    #     [bool]$output = $false
+    # )
     process {
         try {
             $sdkRootPath = Resolve-Path "$PSScriptRoot/../../sdk/$ServiceDirectory"
@@ -39,28 +40,35 @@ function  Enable-ModelFactory {
     
         # Add model factory config & re-generate
         $files = Get-ChildItem -Path $sdkRootPath -Recurse -Filter autorest.md | % { $_.FullName }
-        # foreach ($file in $files) {
-        #     if ($file.Contains("Azure.ResourceManager")) {
-        #         Update-AutorestMd($file)
-        #         $workPath = Split-Path -Path $file -Parent
-        #         Set-Location -Path $workPath
-            
-        #         # if ([string]::IsNullOrWhitespace($AutorestPath)) { 
-        #         #     dotnet build /t:GenerateCode
-        #         # }
-        #         # else {
-        #         #     autorest .\autorest.md --use:$AutorestPath
-        #         # }
-        #     }
-        # }
-
-        # Update CHANGLOG file
-        $files = Get-ChildItem -Path $sdkRootPath -Recurse -Filter CHANGELOG.md | % { $_.FullName }
         foreach ($file in $files) {
             if ($file.Contains("Azure.ResourceManager")) {
-                Update-ChangeLog($file)
+                Update-AutorestMd($file)
+                $workPath = Split-Path -Path $file -Parent
+                Set-Location -Path $workPath
+            
+                & dotnet build /t:GenerateCode
+
+                $changelogFile = $file.Replace("src\autorest.md","CHANGELOG.md")
+                if ($?) {
+                    Update-ChangeLog($changelogFile)
+                }
+
+                # if ([string]::IsNullOrWhitespace($AutorestPath)) { 
+                #     dotnet build /t:GenerateCode
+                # }
+                # else {
+                #     autorest .\autorest.md --use:$AutorestPath
+                # }
             }
         }
+
+        # Update CHANGLOG file
+        # $files = Get-ChildItem -Path $sdkRootPath -Recurse -Filter CHANGELOG.md | % { $_.FullName }
+        # foreach ($file in $files) {
+        #     if ($file.Contains("Azure.ResourceManager")) {
+        #         Update-ChangeLog($file)
+        #     }
+        # }
     }
 }
 
