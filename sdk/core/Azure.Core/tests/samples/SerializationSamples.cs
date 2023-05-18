678a804a627311c09a5be78d2200d418e49011b6
m-nash <64171366+m-nash@users.noreply.github.com>
2023-05-17 22:29:35 -0700

    Add scaffolding for readme (#36342)
    * add scaffolding for readme
    
    * fix type on class name
    
    * one more typo :-(
    
    * make tests sync

---
+++ sdk/core/Azure.Core/tests/samples/SerializationSamples.cs
@@ -0,0 +1,107 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using NUnit.Framework;
+
+namespace Azure.Core.Samples
+{
+    public class SerializationSamples
+    {
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void TrySerialize()
+        {
+            #region Snippet:Try_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void TryDeserialize()
+        {
+            #region Snippet:Try_Deserialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void NonTrySerialize()
+        {
+            #region Snippet:NonTry_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void NonTryDeserialize()
+        {
+            #region Snippet:NonTry_Deserialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void ExplicitCastSerialize()
+        {
+            #region Snippet:ExplicitCast_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void ExplicitCastDeserialize()
+        {
+            #region Snippet:ExplicitCast_Deserialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void StjSerialize()
+        {
+            #region Snippet:Stj_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void StjDeserialize()
+        {
+            #region Snippet:Stj_Deserialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void StaticSerialize()
+        {
+            #region Snippet:Static_Serialize
+            //TODO
+            #endregion
+        }
+
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public void StaticDeserialize()
+        {
+            #region Snippet:Static_Deserialize
+            //TODO
+            #endregion
+        }
+    }
+}
