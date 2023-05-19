0b3bf7a37fa98aa888e13018199d336b3d4596d7
azure-sdk <azuresdk@microsoft.com>
2023-05-19 02:35:36 +0000

    Update SDK codes

--- sdk/healthinsights/Azure.Health.Insights.CancerProfiling/src/Generated/OncoPhenotypeResult.cs
+++ sdk/healthinsights/Azure.Health.Insights.CancerProfiling/src/Generated/OncoPhenotypeResult.cs
@@ -29,7 +29,7 @@ namespace Azure.Health.Insights.CancerProfiling
         /// <param name="status"> The status of the processing job. </param>
         /// <param name="errors"> An array of errors, if any errors occurred during the processing job. </param>
         /// <param name="results"> The inference results for the Onco Phenotype request. </param>
-        internal OncoPhenotypeResult(string jobId, DateTimeOffset createdDateTime, DateTimeOffset expirationDateTime, DateTimeOffset lastUpdateDateTime, JobStatus status, IReadOnlyList<ResponseError> errors, OncoPhenotypeResults results)
+        internal OncoPhenotypeResult(Guid jobId, DateTimeOffset createdDateTime, DateTimeOffset expirationDateTime, DateTimeOffset lastUpdateDateTime, JobStatus status, IReadOnlyList<ResponseError> errors, OncoPhenotypeResults results)
         {
             JobId = jobId;
             CreatedDateTime = createdDateTime;
@@ -41,7 +41,7 @@ namespace Azure.Health.Insights.CancerProfiling
         }
 
         /// <summary> A processing job identifier. </summary>
-        public string JobId { get; }
+        public Guid JobId { get; }
         /// <summary> The date and time when the processing job was created. </summary>
         public DateTimeOffset CreatedDateTime { get; }
         /// <summary> The date and time when the processing job is set to expire. </summary>
