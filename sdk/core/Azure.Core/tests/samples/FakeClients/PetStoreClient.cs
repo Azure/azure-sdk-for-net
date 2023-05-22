7b3e94c95c5d358b305a5d1a5f427c74675ca026
Anne Thompson <annelo@microsoft.com>
2023-05-22 14:33:37 -0700

    Add sample illustrating anonymous type usage

---
+++ sdk/core/Azure.Core/tests/samples/FakeClients/PetStoreClient.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace Azure.Core.Samples.FakeClients
+{
+    internal class PetStoreClient
+    {
+        public PetStoreClient(Uri endpoint, TokenCredential credential, PetStoreClientOptions options)
+        {
+        }
+
+        // Request: "id" in the context path, like "/pets/{id}"
+        // Response: {
+        //      "name": "snoopy",
+        //      "species": "beagle"
+        // }
+        public Response GetPet(string id, RequestContext context = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Request: {
+        //      "name": "snoopy",
+        //      "species": "beagle"
+        // }
+        // Response: {
+        //      "name": "snoopy",
+        //      "species": "beagle"
+        // }
+        public Response SetPet(RequestContent requestBody, RequestContext context = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+#pragma warning disable SA1402 // File may only contain a single type
+    internal class PetStoreClientOptions : ClientOptions
+#pragma warning restore SA1402 // File may only contain a single type
+    {
+    }
+}
---
+++ sdk/core/Azure.Core/tests/samples/ProtocolMethodSamples.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core.Samples.FakeClients;
+using Azure.Core.Serialization;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.Core.Samples
+{
+    public class ProtocolMethodSamples
+    {
+        [Test]
+        public void SetWithAnonymousType()
+        {
+            PetStoreClient client = new(new Uri("https://example.azure.com"), new MockCredential(), new PetStoreClientOptions());
+            #region Snippet:AzureCoreSetWithAnonymousType
+            // anonymous class is serialized by System.Text.Json using runtime reflection
+            var data = new
+            {
+                Name = "snoopy",
+                Species = "beagle"
+            };
+            /*
+            {
+                "name": "snoopy",
+                "species": "beagle"
+            }
+            */
+            client.SetPet(RequestContent.Create(data, PropertyNameHandling.ToCamelCase));
+            #endregion
+        }
+    }
+}
