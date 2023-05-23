5a614a41c886a363fd04c40830b8fd7cd493fe88
Michael Lucadamo <mlucadamo@microsoft.com>
2023-05-23 14:30:16 -0400

    adding workspace delete test

--- sdk/fist/Azure.ResourceManager.IoTFirmwareDefense/tests/Scenario/WorkspaceCollectionTest.cs
+++ sdk/fist/Azure.ResourceManager.IoTFirmwareDefense/tests/Scenario/WorkspaceCollectionTest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.TestFramework;
@@ -50,6 +51,25 @@ namespace Azure.ResourceManager.IoTFirmwareDefense.Tests
 
             var retrievedWorkspace = await rg.GetWorkspaceAsync(resourceName);
             retrievedWorkspace.Value.Data.Name.Should().Be(resourceName);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task TestDeleteWorkspace()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            rg = await CreateResourceGroup(subscription, "testRg", AzureLocation.EastUS);
+            string resourceName = Recording.GenerateAssetName("resource");
+            WorkspaceCollection resource = rg.GetWorkspaces();
+            await resource.CreateOrUpdateAsync(
+                WaitUntil.Completed,
+                resourceName,
+                new WorkspaceData(AzureLocation.EastUS));
+
+            var retrievedWorkspace = await rg.GetWorkspaceAsync(resourceName);
+            await retrievedWorkspace.Value.DeleteAsync(WaitUntil.Completed);
+            var action = async () => await rg.GetWorkspaceAsync(resourceName);
+            await action.Should ().ThrowAsync<Exception> ();
         }
     }
 }
