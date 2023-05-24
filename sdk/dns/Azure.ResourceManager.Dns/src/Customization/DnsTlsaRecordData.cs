1879aa1caf5425a35dcb628478908d5a1a7241f8
Chengming <v-cruan@microsoft.com>
2023-05-24 17:17:30 +0800

    TLSA record

---
+++ sdk/dns/Azure.ResourceManager.Dns/src/Customization/DnsTlsaRecordData.cs
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System.Collections.Generic;
+using Azure;
+using Azure.Core;
+using Azure.ResourceManager.Dns.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources.Models;
+
+namespace Azure.ResourceManager.Dns
+{
+    /// <summary> A class representing the DnsTlsaRecord data model. </summary>
+    public partial class DnsTlsaRecordData : DnsBaseRecordData
+    {
+        /// <summary> Initializes a new instance of DnsTlsaRecordData. </summary>
+        public DnsTlsaRecordData()
+        {
+            DnsTlsaRecords = new ChangeTrackingList<DnsTlsaRecordInfo>();
+        }
+
+        /// <summary> Initializes a new instance of DnsTlsaRecordData. </summary>
+        /// <param name="id"> The id. </param>
+        /// <param name="name"> The name. </param>
+        /// <param name="resourceType"> The resourceType. </param>
+        /// <param name="systemData"> The systemData. </param>
+        /// <param name="etag"> The etag of the record set. </param>
+        /// <param name="metadata"> The metadata attached to the record set. </param>
+        /// <param name="ttl"> The Ttl (time-to-live) of the records in the record set. </param>
+        /// <param name="fqdn"> Fully qualified domain name of the record set. </param>
+        /// <param name="provisioningState"> provisioning State of the record set. </param>
+        /// <param name="targetResource"> A reference to an azure resource from where the dns resource value is taken. </param>
+        /// <param name="TlsaRecords"> The list of Tlsa records in the record set. </param>
+        internal DnsTlsaRecordData(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, ETag? etag, IDictionary<string, string> metadata, long? ttl, string fqdn, string provisioningState, WritableSubResource targetResource, IList<DnsTlsaRecordInfo> TlsaRecords) : base(id, name, resourceType, systemData, etag, metadata, ttl, fqdn, provisioningState, targetResource)
+        {
+            DnsTlsaRecords = TlsaRecords;
+        }
+
+        /// <summary> The list of Tlsa records in the record set. </summary>
+        public IList<DnsTlsaRecordInfo> DnsTlsaRecords { get; }
+    }
+}
