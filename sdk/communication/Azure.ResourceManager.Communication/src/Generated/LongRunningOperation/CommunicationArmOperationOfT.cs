862268fe31548a711ffbdfad830df43cbad3fd26
williamzhao87 <williamzhao87@gmail.com>
2023-05-25 17:21:53 -0400

    Add re-offertime UTC to decline API

--- sdk/communication/Azure.ResourceManager.Communication/src/Generated/LongRunningOperation/CommunicationArmOperationOfT.cs
+++ sdk/communication/Azure.ResourceManager.Communication/src/Generated/LongRunningOperation/CommunicationArmOperationOfT.cs
@@ -31,9 +31,9 @@ namespace Azure.ResourceManager.Communication
             _operation = OperationInternal<T>.Succeeded(response.GetRawResponse(), response.Value);
         }
 
-        internal CommunicationArmOperation(IOperationSource<T> source, ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia, bool skipApiVersionOverride = false, string apiVersionOverrideValue = null)
+        internal CommunicationArmOperation(IOperationSource<T> source, ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia)
         {
-            var nextLinkOperation = NextLinkOperationImplementation.Create(source, pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia, skipApiVersionOverride, apiVersionOverrideValue);
+            var nextLinkOperation = NextLinkOperationImplementation.Create(source, pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
             _operation = new OperationInternal<T>(nextLinkOperation, clientDiagnostics, response, "CommunicationArmOperation", fallbackStrategy: new SequentialDelayStrategy());
         }
 
