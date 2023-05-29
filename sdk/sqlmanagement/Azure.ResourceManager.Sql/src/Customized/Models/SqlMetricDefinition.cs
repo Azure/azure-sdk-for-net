291b16f840e3bccc186d654037178580bf143682
mcgallan <1186003288@qq.com>
2023-05-29 10:35:29 +0800

    Update SqlMetricDefinition.cs

--- sdk/sqlmanagement/Azure.ResourceManager.Sql/src/Customized/Models/SqlMetricDefinition.cs
+++ sdk/sqlmanagement/Azure.ResourceManager.Sql/src/Customized/Models/SqlMetricDefinition.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.Text;
 
