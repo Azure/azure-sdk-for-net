753b92e2fa4555f11a8b338c72ee2a4e0ae3be3b
andrewjw1995 <andrewjw1995@gmail.com>
2023-06-01 09:01:36 +1200

    Added unit tests and fixed issues discovered by tests

--- sdk/eventgrid/Microsoft.Azure.WebJobs.Extensions.EventGrid/src/Config/EventGridAsyncCollectorFactory.cs
+++ sdk/eventgrid/Microsoft.Azure.WebJobs.Extensions.EventGrid/src/Config/EventGridAsyncCollectorFactory.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using Azure;
+using Azure.Core;
 using Azure.Messaging.EventGrid;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
@@ -25,11 +26,21 @@ namespace Microsoft.Azure.WebJobs.Extensions.EventGrid.Config
 
         internal void Validate(EventGridAttribute attribute)
         {
+            if (attribute.TopicKeySetting != null)
+            {
+                if (!string.IsNullOrWhiteSpace(attribute.Connection))
+                {
+                    throw new InvalidOperationException($"Conflicting Event Grid topic credentials have been set in '{nameof(EventGridAttribute.Connection)}' and '{nameof(EventGridAttribute.TopicKeySetting)}'");
+                }
+
+                if (string.IsNullOrWhiteSpace(attribute.TopicKeySetting))
+                {
+                    throw new InvalidOperationException($"The '{nameof(EventGridAttribute.TopicKeySetting)}' property must be the name of an application setting containing the Topic Key");
+                }
+            }
+
             if (!string.IsNullOrWhiteSpace(attribute.Connection))
             {
-                if (attribute.TopicKeySetting != null)
-                    throw new InvalidOperationException($"Conflicting Event Grid topic credentials have been set in '{nameof(EventGridAttribute.Connection)}' and '{nameof(EventGridAttribute.TopicKeySetting)}'");
-
                 var connectionSection = _configuration.GetSection(attribute.Connection);
                 if (!connectionSection.Exists())
                     throw new InvalidOperationException($"Event Grid topic connection string '{attribute.Connection}' does not exist. " +
@@ -37,35 +48,32 @@ namespace Microsoft.Azure.WebJobs.Extensions.EventGrid.Config
 
                 if (!string.IsNullOrWhiteSpace(connectionSection.Value))
                 {
-                    if (!Uri.IsWellFormedUriString(connectionSection.Value, UriKind.Absolute))
+                    var topicEndpointUri = connectionSection.Value;
+                    if (!Uri.IsWellFormedUriString(topicEndpointUri, UriKind.Absolute))
                     {
                         throw new InvalidOperationException($"Event Grid topic connection string '{attribute.Connection}' must be a valid absolute Uri");
                     }
 
-                    if (!string.IsNullOrWhiteSpace(attribute.TopicEndpointUri) && attribute.TopicEndpointUri != connectionSection.Value)
+                    if (!string.IsNullOrWhiteSpace(attribute.TopicEndpointUri) && topicEndpointUri != attribute.TopicEndpointUri)
                     {
-                        throw new InvalidOperationException($"Event Grid topic connection string '{attribute.Connection}' is set to a different Uri");
+                        throw new InvalidOperationException($"Conflicting Event Grid topic connection strings have been set in '{nameof(EventGridAttribute.Connection)}' and '{nameof(EventGridAttribute.TopicEndpointUri)}'");
                     }
-                }
-            }
-            else if (string.IsNullOrWhiteSpace(attribute.TopicEndpointUri))
-            {
-                throw new InvalidOperationException($"The '{nameof(EventGridAttribute.Connection)}' property or '{nameof(EventGridAttribute.TopicEndpointUri)}' property must be set");
-            }
-            else
-            {
-                // if app setting is missing, it will be caught by runtime
-                // this logic tries to validate the practicality of attribute properties
-                if (string.IsNullOrWhiteSpace(attribute.TopicKeySetting))
-                {
-                    throw new InvalidOperationException($"The '{nameof(EventGridAttribute.TopicKeySetting)}' property must be the name of an application setting containing the Topic Key");
-                }
 
+                    return;
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(attribute.TopicEndpointUri))
+            {
                 if (!Uri.IsWellFormedUriString(attribute.TopicEndpointUri, UriKind.Absolute))
                 {
                     throw new InvalidOperationException($"The '{nameof(EventGridAttribute.TopicEndpointUri)}' property must be a valid absolute Uri");
                 }
+
+                return;
             }
+
+            throw new InvalidOperationException($"The '{nameof(EventGridAttribute.Connection)}' property or '{nameof(EventGridAttribute.TopicEndpointUri)}' property must be set");
         }
 
         internal virtual IAsyncCollector<object> CreateCollector(EventGridAttribute attribute)
@@ -73,20 +81,58 @@ namespace Microsoft.Azure.WebJobs.Extensions.EventGrid.Config
 
         private EventGridPublisherClient CreateClient(EventGridAttribute attribute)
         {
+            var connectionInformation = ResolveConnectionInformation(attribute);
+            if (connectionInformation.AzureKeyCredential != null)
+            {
+                return new EventGridPublisherClient(connectionInformation.Endpoint, connectionInformation.AzureKeyCredential);
+            }
+            else
+            {
+                return new EventGridPublisherClient(connectionInformation.Endpoint, connectionInformation.TokenCredential);
+            }
+        }
+
+        internal EventGridConnectionInformation ResolveConnectionInformation(EventGridAttribute attribute)
+        {
+            if (!string.IsNullOrWhiteSpace(attribute.TopicKeySetting))
+            {
+                return new EventGridConnectionInformation(new Uri(attribute.TopicEndpointUri), new AzureKeyCredential(attribute.TopicKeySetting));
+            }
+
             if (string.IsNullOrWhiteSpace(attribute.Connection))
             {
-                return new EventGridPublisherClient(new Uri(attribute.TopicEndpointUri), new AzureKeyCredential(attribute.TopicKeySetting));
+                var emptyConfiguration = new ConfigurationBuilder().Build();
+                return new EventGridConnectionInformation(new Uri(attribute.TopicEndpointUri), _componentFactory.CreateTokenCredential(emptyConfiguration));
             }
 
             var connectionSection = _configuration.GetSection(attribute.Connection);
             if (!string.IsNullOrWhiteSpace(connectionSection.Value))
             {
-                return new EventGridPublisherClient(new Uri(connectionSection.Value), _componentFactory.CreateTokenCredential(connectionSection));
+                return new EventGridConnectionInformation(new Uri(connectionSection.Value), _componentFactory.CreateTokenCredential(connectionSection));
             }
             else
             {
-                return new EventGridPublisherClient(new Uri(attribute.TopicEndpointUri), _componentFactory.CreateTokenCredential(connectionSection));
+                return new EventGridConnectionInformation(new Uri(attribute.TopicEndpointUri), _componentFactory.CreateTokenCredential(connectionSection));
             }
+        }
+
+        internal record EventGridConnectionInformation
+        {
+            public EventGridConnectionInformation(Uri endpoint, AzureKeyCredential azureKeyCredential)
+            {
+                Endpoint = endpoint;
+                AzureKeyCredential = azureKeyCredential;
+            }
+
+            public EventGridConnectionInformation(Uri endpoint, TokenCredential tokenCredential)
+            {
+                Endpoint = endpoint;
+                TokenCredential = tokenCredential;
+            }
+
+            public Uri Endpoint { get; }
+            public AzureKeyCredential AzureKeyCredential { get; }
+            public TokenCredential TokenCredential { get; }
         }
     }
 }
---
+++ sdk/eventgrid/Microsoft.Azure.WebJobs.Extensions.EventGrid/tests/Config/EventGridAsyncCollectorFactoryTests.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Identity;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid.Config;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.WebJobs.Extensions.EventGrid.Tests.Config
+{
+    internal class EventGridAsyncCollectorFactoryTests
+    {
+        [Test]
+        [TestCase(null, null, null, "The 'Connection' property or 'TopicEndpointUri' property must be set")]
+        [TestCase("", null, null, "The 'Connection' property or 'TopicEndpointUri' property must be set")]
+        [TestCase(null, null, "", "The 'Connection' property or 'TopicEndpointUri' property must be set")]
+        [TestCase(null, null, "EmptyUri", "The 'Connection' property or 'TopicEndpointUri' property must be set")]
+        [TestCase("bar.com", "baz", null, "The 'TopicEndpointUri' property must be a valid absolute Uri")]
+        [TestCase(null, "baz", "ValidUri", "Conflicting Event Grid topic credentials have been set in 'Connection' and 'TopicKeySetting'")]
+        [TestCase("https://foo.com", null, "MissingUri", "Event Grid topic connection string 'MissingUri' does not exist. Make sure that it is a defined App Setting.")]
+        [TestCase("https://foo.com", null, "InvalidUri", "Event Grid topic connection string 'InvalidUri' must be a valid absolute Uri")]
+        [TestCase("https://foo.com", null, "AnotherUri", "Conflicting Event Grid topic connection strings have been set in 'Connection' and 'TopicEndpointUri'")]
+        public void ValidateOutputBindingAttributeTests(string topicEndpointUri, string topicKeySetting, string connection, string message)
+        {
+            var host = TestHelpers.NewHost<Empty>(configuration: new Dictionary<string, string>
+            {
+                { "EmptyUri", "" },
+                { "InvalidUri", "bar.com" },
+                { "ValidUri", "https://foo.com" },
+                { "AnotherUri", "https://bar.com" },
+            });
+
+            var factory = host.Services.GetRequiredService<EventGridAsyncCollectorFactory>();
+            var exception = Assert.Throws<InvalidOperationException>(() => factory.Validate(new EventGridAttribute
+            {
+                TopicEndpointUri = topicEndpointUri,
+                TopicKeySetting = topicKeySetting,
+                Connection = connection
+            }));
+
+            Assert.That(exception.Message, Is.EqualTo(message));
+        }
+
+        [Test]
+        public void TestTopicKeySetting()
+        {
+            var host = TestHelpers.NewHost<Empty>();
+            var factory = host.Services.GetRequiredService<EventGridAsyncCollectorFactory>();
+
+            var connectionInformation = factory.ResolveConnectionInformation(new EventGridAttribute
+            {
+                TopicEndpointUri = "https://foo.com",
+                TopicKeySetting = "Bar",
+                Connection = null
+            });
+
+            Assert.That(connectionInformation.AzureKeyCredential, Is.Not.Null);
+        }
+
+        [Test]
+        [TestCase("https://foo.com", null, null)]
+        [TestCase(null, null, "ValidUri")]
+        [TestCase("https://foo.com", null, "EmptyUri")]
+        public void TestSystemAssignedIdentity(string topicEndpointUri, string topicKeySetting, string connection)
+        {
+            var host = TestHelpers.NewHost<Empty>(configuration: new Dictionary<string, string>
+            {
+                { "EmptyUri", "" },
+                { "ValidUri", "https://foo.com" },
+            });
+            var factory = host.Services.GetRequiredService<EventGridAsyncCollectorFactory>();
+
+            var connectionInformation = factory.ResolveConnectionInformation(new EventGridAttribute
+            {
+                TopicEndpointUri = topicEndpointUri,
+                TopicKeySetting = topicKeySetting,
+                Connection = connection
+            });
+
+            Assert.That(connectionInformation.TokenCredential, Is.Not.Null.And.TypeOf<DefaultAzureCredential>());
+        }
+
+        public class Empty
+        {
+        }
+    }
+}
