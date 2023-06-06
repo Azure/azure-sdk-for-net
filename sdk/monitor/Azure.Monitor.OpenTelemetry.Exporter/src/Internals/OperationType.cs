2513fa0dce6c92d427d95de796149b7b8ca425c8
Rajkumar Rangaraj <rajrang@microsoft.com>
2023-06-05 17:45:46 -0700

    Remove flag

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/OperationType.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/OperationType.cs
@@ -1,20 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-
 namespace Azure.Monitor.OpenTelemetry.Exporter.Internals
 {
-    [Flags]
     internal enum OperationType
     {
-        Unknown = 0,
-        Azure = 1 << 0,
-        Common = 1 << 1,
-        Db = 1 << 2,
-        FaaS = 1 << 3,
-        Http = 1 << 4,
-        Messaging = 1 << 5,
-        Rpc = 1 << 6
+        Unknown,
+        Azure,
+        Common,
+        Db,
+        FaaS,
+        Http,
+        Messaging,
+        Rpc
     };
 }
