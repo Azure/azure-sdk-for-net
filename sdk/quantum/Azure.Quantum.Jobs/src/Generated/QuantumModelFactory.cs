24ed39ae9717c88eb5257202578769ceb459b819
azure-sdk <azuresdk@microsoft.com>
2023-06-07 03:45:19 +0000

    Update SDK codes

--- sdk/quantum/Azure.Quantum.Jobs/src/Generated/QuantumModelFactory.cs
+++ sdk/quantum/Azure.Quantum.Jobs/src/Generated/QuantumModelFactory.cs
@@ -8,8 +8,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Azure.Quantum.Jobs.Models;
 
-namespace Azure.Quantum.Jobs.Models
+namespace QuantumClient.Models
 {
     /// <summary> Model factory for models. </summary>
     public static partial class QuantumModelFactory
@@ -34,7 +35,7 @@ namespace Azure.Quantum.Jobs.Models
         /// <param name="costEstimate"> The job cost billed by the provider. The final cost on your bill might be slightly different due to added taxes and currency conversion rates. </param>
         /// <param name="errorData"> The error data for the job. This is expected only when Status &apos;Failed&apos;. </param>
         /// <param name="tags"> List of user-supplied tags associated with the job. </param>
-        /// <returns> A new <see cref="Models.JobDetails"/> instance for mocking. </returns>
+        /// <returns> A new <see cref="Azure.Quantum.Jobs.Models.JobDetails"/> instance for mocking. </returns>
         public static JobDetails JobDetails(string id = null, string name = null, string containerUri = null, string inputDataUri = null, string inputDataFormat = null, object inputParams = null, string providerId = null, string target = null, IDictionary<string, string> metadata = null, string outputDataUri = null, string outputDataFormat = null, JobStatus? status = null, DateTimeOffset? creationTime = null, DateTimeOffset? beginExecutionTime = null, DateTimeOffset? endExecutionTime = null, DateTimeOffset? cancellationTime = null, CostEstimate costEstimate = null, ErrorData errorData = null, IEnumerable<string> tags = null)
         {
             metadata ??= new Dictionary<string, string>();
@@ -47,7 +48,7 @@ namespace Azure.Quantum.Jobs.Models
         /// <param name="currencyCode"> The currency code. </param>
         /// <param name="events"> List of usage events. </param>
         /// <param name="estimatedTotal"> The estimated total. </param>
-        /// <returns> A new <see cref="Models.CostEstimate"/> instance for mocking. </returns>
+        /// <returns> A new <see cref="Azure.Quantum.Jobs.Models.CostEstimate"/> instance for mocking. </returns>
         public static CostEstimate CostEstimate(string currencyCode = null, IEnumerable<UsageEvent> events = null, float? estimatedTotal = null)
         {
             events ??= new List<UsageEvent>();
@@ -62,7 +63,7 @@ namespace Azure.Quantum.Jobs.Models
         /// <param name="amountBilled"> The amount billed. </param>
         /// <param name="amountConsumed"> The amount consumed. </param>
         /// <param name="unitPrice"> The unit price. </param>
-        /// <returns> A new <see cref="Models.UsageEvent"/> instance for mocking. </returns>
+        /// <returns> A new <see cref="Azure.Quantum.Jobs.Models.UsageEvent"/> instance for mocking. </returns>
         public static UsageEvent UsageEvent(string dimensionId = null, string dimensionName = null, string measureUnit = null, float? amountBilled = null, float? amountConsumed = null, float? unitPrice = null)
         {
             return new UsageEvent(dimensionId, dimensionName, measureUnit, amountBilled, amountConsumed, unitPrice);
@@ -71,7 +72,7 @@ namespace Azure.Quantum.Jobs.Models
         /// <summary> Initializes a new instance of ErrorData. </summary>
         /// <param name="code"> An identifier for the error. Codes are invariant and are intended to be consumed programmatically. </param>
         /// <param name="message"> A message describing the error, intended to be suitable for displaying in a user interface. </param>
-        /// <returns> A new <see cref="Models.ErrorData"/> instance for mocking. </returns>
+        /// <returns> A new <see cref="Azure.Quantum.Jobs.Models.ErrorData"/> instance for mocking. </returns>
         public static ErrorData ErrorData(string code = null, string message = null)
         {
             return new ErrorData(code, message);
@@ -81,7 +82,7 @@ namespace Azure.Quantum.Jobs.Models
         /// <param name="id"> Provider id. </param>
         /// <param name="currentAvailability"> Provider availability. </param>
         /// <param name="targets"></param>
-        /// <returns> A new <see cref="Models.ProviderStatus"/> instance for mocking. </returns>
+        /// <returns> A new <see cref="Azure.Quantum.Jobs.Models.ProviderStatus"/> instance for mocking. </returns>
         public static ProviderStatus ProviderStatus(string id = null, ProviderAvailability? currentAvailability = null, IEnumerable<TargetStatus> targets = null)
         {
             targets ??= new List<TargetStatus>();
@@ -94,7 +95,7 @@ namespace Azure.Quantum.Jobs.Models
         /// <param name="currentAvailability"> Target availability. </param>
         /// <param name="averageQueueTime"> Average queue time in seconds. </param>
         /// <param name="statusPage"> A page with detailed status of the provider. </param>
-        /// <returns> A new <see cref="Models.TargetStatus"/> instance for mocking. </returns>
+        /// <returns> A new <see cref="Azure.Quantum.Jobs.Models.TargetStatus"/> instance for mocking. </returns>
         public static TargetStatus TargetStatus(string id = null, TargetAvailability? currentAvailability = null, long? averageQueueTime = null, string statusPage = null)
         {
             return new TargetStatus(id, currentAvailability, averageQueueTime, statusPage);
@@ -102,7 +103,7 @@ namespace Azure.Quantum.Jobs.Models
 
         /// <summary> Initializes a new instance of SasUriResponse. </summary>
         /// <param name="sasUri"> A URL with a SAS token to upload a blob for execution in the given workspace. </param>
-        /// <returns> A new <see cref="Models.SasUriResponse"/> instance for mocking. </returns>
+        /// <returns> A new <see cref="Azure.Quantum.Jobs.Models.SasUriResponse"/> instance for mocking. </returns>
         public static SasUriResponse SasUriResponse(string sasUri = null)
         {
             return new SasUriResponse(sasUri);
@@ -111,7 +112,7 @@ namespace Azure.Quantum.Jobs.Models
         /// <summary> Initializes a new instance of QuantumJobQuotaList. </summary>
         /// <param name="value"></param>
         /// <param name="nextLink"> Link to the next page of results. </param>
-        /// <returns> A new <see cref="Models.QuantumJobQuotaList"/> instance for mocking. </returns>
+        /// <returns> A new <see cref="Azure.Quantum.Jobs.Models.QuantumJobQuotaList"/> instance for mocking. </returns>
         public static QuantumJobQuotaList QuantumJobQuotaList(IEnumerable<QuantumJobQuota> value = null, string nextLink = null)
         {
             value ??= new List<QuantumJobQuota>();
@@ -127,7 +128,7 @@ namespace Azure.Quantum.Jobs.Models
         /// <param name="holds"> The amount of the usage that has been reserved but not applied for the current period. </param>
         /// <param name="limit"> The maximum amount of usage allowed for the current period. </param>
         /// <param name="period"> The time period in which the quota&apos;s underlying meter is accumulated. Based on calendar year. &apos;None&apos; is used for concurrent quotas. </param>
-        /// <returns> A new <see cref="Models.QuantumJobQuota"/> instance for mocking. </returns>
+        /// <returns> A new <see cref="Azure.Quantum.Jobs.Models.QuantumJobQuota"/> instance for mocking. </returns>
         public static QuantumJobQuota QuantumJobQuota(string dimension = null, DimensionScope? scope = null, string providerId = null, float? utilization = null, float? holds = null, float? limit = null, MeterPeriod? period = null)
         {
             return new QuantumJobQuota(dimension, scope, providerId, utilization, holds, limit, period);
