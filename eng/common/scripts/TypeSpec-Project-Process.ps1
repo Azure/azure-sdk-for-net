742757e667688b142041b6664a1fe644b58519fe
raychen <raychen@microsoft.com>
2023-06-07 08:54:42 +0800

    Fixed git remote repo parsing

--- eng/common/scripts/TypeSpec-Project-Process.ps1
+++ eng/common/scripts/TypeSpec-Project-Process.ps1
@@ -177,15 +177,12 @@ if ($generateFromLocalTypeSpec) {
       Push-Location $specRepoRoot
       $CommitHash = $(git rev-parse HEAD)
       $gitOriginUrl = (git remote get-url origin)
-      foreach ($remote in $gitOriginUrl) {
-        Write-Host "Checking remote $remote"
-        if ($remote.StartsWith("origin")) {
-          if ($remote -match '(.*)?github.com:(?<repo>[^/]*/azure-rest-api-specs(-pr)?)(.git)?') {
-            $repo = $Matches["repo"]
-            Write-Host "Found remote origin: $repo"
-            break
-          }
-        }
+      if ($gitOriginUrl -and $gitOriginUrl -match '(.*)?github.com:(?<repo>[^/]*/azure-rest-api-specs(-pr)?)(.git)?') {
+        $repo = $Matches["repo"]
+        Write-Host "Found git origin repo: $repo"
+      }
+      else {
+        Write-Warning "Failed to find git origin repo of the local spec repo at specRepoRoot: $specRepoRoot. Using default repo: $repo"
       }
     }
     catch {
