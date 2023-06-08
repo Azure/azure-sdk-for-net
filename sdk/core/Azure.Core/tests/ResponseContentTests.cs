1b82fa694baa712d5af7d974cbb91e3f09a3e14d
Anne Thompson <annelo@microsoft.com>
2023-06-08 15:06:14 -0700

    Enable configuring dynamic behavior through client options (#36887)
    * Move naming enum to Azure.Core.Serialization
    
    * Rename to PropertyNameConversion
    
    * Add dynamic options to response content
    
    * Plumb through response content options
    
    * Update ClientOptions API
    
    * Enable it in DynamicData
    
    * RequestContent and CLU
    
    * Update docs and samples
    
    * Unify request and response content
    
    * Add TODO
    
    * Rename internal enum
    
    * pr fb
    
    * nit
    
    * undo
    
    * missed update
    
    * more updates from pr fb
    
    * architect API feedback
    
    * clu
    
    * refactor pipeline bits to use ProtocolOptions (remove dependency on types in Dynamic namespace)
    
    * Update set behavior as discussed in arch board
    
    * nits
    
    * PR fb
    
    * clu

---
+++ sdk/core/Azure.Core/tests/ResponseContentTests.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.Core.Dynamic;
+using Azure.Core.Pipeline;
+using Azure.Core.Serialization;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.Core.Tests
+{
+    public class ResponseContentTests
+    {
+        [Test]
+        public async Task CanSetProtocolOptionsFromClientOptions()
+        {
+            MockClientOptions options = new MockClientOptions();
+            MockClient client = new MockClient(options);
+            Response response = await client.GetValueAsync();
+            ResponseContent content = response.Content as ResponseContent;
+
+            Assert.IsNotNull(content);
+            Assert.AreEqual(content.ProtocolOptions.ResponseContentConvention, PropertyNamingConvention.None);
+        }
+
+        [Test]
+        public async Task ProtocolOptionsSetDynamicOptionsNaming()
+        {
+            MockClientOptions options = new MockClientOptions();
+            options.ProtocolMethods.ResponseContentConvention = PropertyNamingConvention.CamelCase;
+            MockClient client = new MockClient(options);
+            Response response = await client.GetValueAsync();
+            ResponseContent content = response.Content as ResponseContent;
+            DynamicDataOptions dynamicOptions = content.ProtocolOptions.GetDynamicOptions();
+            Assert.AreEqual(PropertyNamingConvention.CamelCase, dynamicOptions.PropertyNamingConvention);
+        }
+
+        [Test]
+        public async Task DefaultProtocolOptionsUseStrictCasing()
+        {
+            MockClientOptions options = new MockClientOptions();
+            MockClient client = new MockClient(options);
+            Response response = await client.GetValueAsync();
+            dynamic value = response.Content.ToDynamicFromJson();
+            Assert.IsNull(value.Foo);
+            Assert.AreEqual(1, (int)value.foo);
+        }
+
+        [Test]
+        public async Task UseCamelCaseOptionEnablesPropertyNameConversion()
+        {
+            MockClientOptions options = new MockClientOptions();
+            options.ProtocolMethods.ResponseContentConvention = PropertyNamingConvention.CamelCase;
+
+            MockClient client = new MockClient(options);
+            Response response = await client.GetValueAsync();
+            dynamic value = response.Content.ToDynamicFromJson();
+
+            Assert.AreEqual(1, (int)value.Foo);
+            Assert.AreEqual(1, (int)value.foo);
+        }
+
+        #region Helpers
+        private class MockClient
+        {
+            private HttpPipeline _pipeline;
+
+            public MockClient(MockClientOptions options)
+            {
+                options.Transport = new MockTransport(
+                    new MockResponse(200).SetContent("""{"foo": 1}""")
+                 );
+
+                _pipeline = HttpPipelineBuilder.Build(options);
+            }
+
+            public async Task<Response> GetValueAsync()
+            {
+                using HttpMessage message = _pipeline.CreateMessage();
+                return await _pipeline.ProcessMessageAsync(message, new RequestContext()).ConfigureAwait(false);
+            }
+        }
+
+        private class MockClientOptions : ClientOptions
+        {
+        }
+        #endregion
+    }
+}
