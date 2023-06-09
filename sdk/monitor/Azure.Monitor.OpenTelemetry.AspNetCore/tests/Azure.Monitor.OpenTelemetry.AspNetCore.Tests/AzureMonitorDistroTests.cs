5de780887e2cf503383f8a8ea540464ddaf81d0f
Timothy Mothra Lee <tilee@microsoft.com>
2023-06-09 16:25:31 -0700

    inspect content of sent telemetry

--- sdk/monitor/Azure.Monitor.OpenTelemetry.AspNetCore/tests/Azure.Monitor.OpenTelemetry.AspNetCore.Tests/AzureMonitorDistroTests.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.AspNetCore/tests/Azure.Monitor.OpenTelemetry.AspNetCore.Tests/AzureMonitorDistroTests.cs
@@ -11,6 +11,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Xunit;
 using Azure.Core.TestFramework;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Nodes;
 
 namespace Azure.Monitor.OpenTelemetry.AspNetCore.Tests
 {
@@ -43,10 +48,18 @@ namespace Azure.Monitor.OpenTelemetry.AspNetCore.Tests
             WaitForRequest(transport);
 
             // Assert
-            // TODO: Validate request content
             Assert.True(transport.Requests.Count > 0);
 
             await app.DisposeAsync();
+
+            var data = ParseContent<ParsedData>(transport.Requests);
+            Assert.Equal(15, data.Count);
+
+            var summary = data.GroupBy(x => x.name).Select(x => new SummaryData { Name = x.Key, Count = x.Count() }).OrderBy(x => x.Name).ToList();
+            Assert.Equivalent(new SummaryData { Name = "Message", Count = 8 }, summary[0]);
+            Assert.Equivalent(new SummaryData { Name = "Metric", Count = 5 }, summary[1]);
+            Assert.Equivalent(new SummaryData { Name = "RemoteDependency", Count = 1 }, summary[2]);
+            Assert.Equivalent(new SummaryData { Name = "Request", Count = 1 }, summary[3]);
         }
 
         private void WaitForRequest(MockTransport transport)
@@ -58,6 +71,47 @@ namespace Azure.Monitor.OpenTelemetry.AspNetCore.Tests
                     return transport.Requests.Count > 0;
                 },
                 timeout: TimeSpan.FromSeconds(10));
+        }
+
+        private static List<T> ParseContent<T>(List<MockRequest> requests)
+        {
+            var returnData = new List<T>();
+
+            foreach (var request in requests)
+            {
+                var stream = new MemoryStream();
+                request.Content.WriteTo(stream, CancellationToken.None);
+
+                stream.Position = 0;
+                var streamReader = new StreamReader(stream);
+                var test = streamReader.ReadToEnd();
+
+                using (StringReader stringReader = new StringReader(test))
+                {
+                    string? line;
+                    while ((line = stringReader.ReadLine()) != null)
+                    {
+                        var jsonObject = JsonSerializer.Deserialize<T>(line);
+                        if (jsonObject != null)
+                        {
+                            returnData.Add(jsonObject);
+                        }
+                    }
+                }
+            }
+
+            return returnData;
+        }
+
+        private class ParsedData
+        {
+            public string? name { get; set; }
+        }
+
+        private class SummaryData
+        {
+            public string? Name { get; set; }
+            public int? Count { get; set; }
         }
     }
 }
