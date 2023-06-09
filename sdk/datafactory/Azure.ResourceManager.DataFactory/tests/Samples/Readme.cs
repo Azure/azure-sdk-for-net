50629d520174b5ed3c9015990c09b576f5c8ee43
Wei Hu <huwe@microsoft.com>
2023-06-09 12:59:56 +0800

    add folder

---
+++ sdk/datafactory/Azure.ResourceManager.DataFactory/tests/Samples/Readme.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#region Snippet:Readme_AuthClient_Namespaces
+#endregion Snippet:Readme_AuthClient_Namespaces
+
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.DataFactory.Tests.Samples
+{
+    internal class Readme
+    {
+        [Test]
+        public void Test() { }
+    }
+}
