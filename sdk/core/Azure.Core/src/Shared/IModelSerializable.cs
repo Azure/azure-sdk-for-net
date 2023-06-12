cfd8728753b0fbafd7ed16a43563194d4761bff9
m-nash <prognash@gmail.com>
2023-06-12 13:56:35 -0700

    add nullable

--- sdk/core/Azure.Core/src/Shared/IModelSerializable.cs
+++ sdk/core/Azure.Core/src/Shared/IModelSerializable.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#nullable enable
+
 using System.Text.Json;
 using Azure.Core.Serialization;
 
--- sdk/core/Azure.Core/src/Shared/SerializableOptions.cs
+++ sdk/core/Azure.Core/src/Shared/SerializableOptions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#nullable enable
+
 namespace Azure.Core.Serialization
 {
     /// <summary>
