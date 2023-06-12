7290508738b787aba9b317125cf6f3d331a2c7bd
m-nash <prognash@gmail.com>
2023-06-12 16:30:36 -0700

    temp changes in branch to mimic shared code in main

---
+++ sdk/core/Azure.Core/src/Shared/SerializableOptions.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable enable
+
+namespace Azure.Core.Serialization
+{
+    /// <summary>
+    /// Provides the client options for serializing models.
+    /// </summary>
+    internal class SerializableOptions
+    {
+        /// <summary>
+        /// Bool that determines if ReadOnlyProperties will be serialized. Default is false.
+        /// </summary>
+        public bool IgnoreReadOnlyProperties { get; set; }
+
+        /// <summary>
+        /// Bool that determines if AdditionalProperties will be serialized. Default is false.
+        /// </summary>
+        public bool IgnoreAdditionalProperties { get; set; }
+
+        /// <summary>
+        /// Bool that determines if Json will be PrettyPrinted. Default is false.
+        /// </summary>
+        public bool PrettyPrint { get; set; }
+
+        /// <summary>
+        /// todo
+        /// </summary>
+        public ObjectSerializer? Serializer { get; set; }
+    }
+}
