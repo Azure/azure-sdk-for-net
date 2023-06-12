088a9ba7e40ae7eb426b578a137ac1095585fc69
Timothy Mothra Lee <tilee@microsoft.com>
2023-06-12 12:58:02 -0700

    refactor ExceptionExtensions

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/ExceptionExtensionsTests.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Tests/ExceptionExtensionsTests.cs
@@ -4,9 +4,7 @@
 using System;
 using System.Globalization;
 using System.Threading;
-
 using Azure.Monitor.OpenTelemetry.Exporter.Internals;
-
 using Xunit;
 
 namespace Azure.Monitor.OpenTelemetry.Exporter.Tests
@@ -36,6 +34,52 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Tests
         {
             ExceptionExtensions.ToInvariantString(new Exception());
             Assert.Equal(this.originalUICulture, Thread.CurrentThread.CurrentUICulture);
+        }
+
+        [Fact]
+        public void VerifyFlattenException_HandlesAggregate()
+        {
+            var aggregateException = new AggregateException(new Exception("hello world 1"), new Exception("hello world 2)"));
+
+            var test = aggregateException.FlattenException();
+
+            Assert.NotSame(test, aggregateException);
+
+#if NETFRAMEWORK
+            Assert.Equal("One or more errors occurred.", test.Message);
+            Assert.Equal("System.AggregateException: One or more errors occurred. ---> System.Exception: hello world 1\r\n   --- End of inner exception stack trace ---\r\n---> (Inner Exception #0) System.Exception: hello world 1<---\r\n\r\n---> (Inner Exception #1) System.Exception: hello world 2)<---\r\n", test.ToString());
+#else
+            Assert.Equal("One or more errors occurred. (hello world 1) (hello world 2))", test.Message);
+            Assert.Equal("System.AggregateException: One or more errors occurred. (hello world 1) (hello world 2))\r\n ---> System.Exception: hello world 1\r\n   --- End of inner exception stack trace ---\r\n ---> (Inner Exception #1) System.Exception: hello world 2)<---\r\n", test.ToString());
+#endif
+        }
+
+        [Fact]
+        public void VerifyFlattenException_HandlesNestedAggregate()
+        {
+            var aggregateException = new AggregateException(new AggregateException(new Exception("hello world 1"), new Exception("hello world 2"), new Exception("hello world 3")));
+
+            var test = aggregateException.FlattenException();
+
+            Assert.NotSame(test, aggregateException);
+
+#if NETFRAMEWORK
+            Assert.Equal("One or more errors occurred.", test.Message);
+            Assert.Equal("System.AggregateException: One or more errors occurred. ---> System.Exception: hello world 1\r\n   --- End of inner exception stack trace ---\r\n---> (Inner Exception #0) System.Exception: hello world 1<---\r\n\r\n---> (Inner Exception #1) System.Exception: hello world 2<---\r\n\r\n---> (Inner Exception #2) System.Exception: hello world 3<---\r\n", test.ToString());
+#else
+            Assert.Equal("One or more errors occurred. (hello world 1) (hello world 2) (hello world 3)", test.Message);
+            Assert.Equal("System.AggregateException: One or more errors occurred. (hello world 1) (hello world 2) (hello world 3)\r\n ---> System.Exception: hello world 1\r\n   --- End of inner exception stack trace ---\r\n ---> (Inner Exception #1) System.Exception: hello world 2<---\r\n\r\n ---> (Inner Exception #2) System.Exception: hello world 3<---\r\n", test.ToString());
+#endif
+        }
+
+        [Fact]
+        public void VerifyFlattenException_HandlesNormalException()
+        {
+            var ex = new Exception("hello world");
+
+            var test = ex.FlattenException();
+
+            Assert.Same(ex, test);
         }
     }
 }
