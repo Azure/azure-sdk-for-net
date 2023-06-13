ff6fc04e1da625efd1786d94480a9dfc506bf363
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-06-13 12:55:07 -0700

    Add samples

--- sdk/eventgrid/Microsoft.Azure.WebJobs.Extensions.EventGrid/tests/Samples/CloudEventOutputBindingWithIdentityFunction.cs
+++ sdk/eventgrid/Microsoft.Azure.WebJobs.Extensions.EventGrid/tests/Samples/CloudEventOutputBindingWithIdentityFunction.cs
@@ -1,28 +1,24 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Azure.Messaging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 
 namespace Microsoft.Azure.WebJobs.Extensions.EventGrid.Tests.Samples
 {
-    #region Snippet:CloudEventBindingFunction
-    public static class CloudEventBindingFunction
+    #region Snippet:CloudEventBindingFunctionWithIdentity
+    public static class CloudEventOutputBindingWithIdentityFunction
     {
-        [FunctionName("CloudEventBindingFunction")]
+        [FunctionName("CloudEventOutputBindingWithIdentityFunction")]
         public static async Task<IActionResult> RunAsync(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            [EventGrid(TopicEndpointUri = "EventGridEndpoint", TopicKeySetting = "EventGridKey")] IAsyncCollector<CloudEvent> eventCollector)
+            [EventGrid(Connection = "MyConnection")] IAsyncCollector<CloudEvent> eventCollector)
         {
             CloudEvent e = new CloudEvent("IncomingRequest", "IncomingRequest", await req.ReadAsStringAsync());
             await eventCollector.AddAsync(e);
             return new OkResult();
         }
     }
+
     #endregion
 }
