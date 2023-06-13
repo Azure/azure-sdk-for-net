01aa3025f9cad0d71a6c76e78ce8a1fe69f3aa1b
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-06-13 12:14:14 -0700

    Update validation for EventGrid output binding and add live tests

---
+++ sdk/eventgrid/Microsoft.Azure.WebJobs.Extensions.EventGrid/tests/InstrumentedCollectorFactory.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+using Azure.Messaging.EventGrid;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid.Config;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Azure.WebJobs.Extensions.EventGrid.Tests
+{
+    internal class InstrumentedCollectorFactory : EventGridAsyncCollectorFactory
+    {
+        private readonly RecordedTestBase _recording;
+
+        public InstrumentedCollectorFactory(RecordedTestBase recording, IConfiguration configuration, AzureComponentFactory componentFactory) : base(configuration, componentFactory)
+        {
+            _recording = recording;
+        }
+        internal override IAsyncCollector<object> CreateCollector(EventGridAttribute attribute)
+        {
+            return new EventGridAsyncCollector(_recording.InstrumentClient(CreateClient(attribute, _recording.InstrumentClientOptions(new EventGridPublisherClientOptions()))));
+        }
+    }
+}
