213bd779a6c7352254550316b66d7b0110fa833a
Adrian Tang <adtang@microsoft.com>
2023-06-14 15:08:55 -0700

    Revert unwated changes.

--- sdk/communication/Azure.Communication.PhoneNumbers/src/Models/PhoneNumberOperationType.cs
+++ sdk/communication/Azure.Communication.PhoneNumbers/src/Models/PhoneNumberOperationType.cs
@@ -7,20 +7,6 @@ using Azure.Core;
 namespace Azure.Communication.PhoneNumbers
 {
     [CodeGenModel("PhoneNumberOperationType")]
-    /* [CodeGenSuppress("PhoneNumberOperationType", typeof(string))]
-    [CodeGenSuppress("Purchase")]
-    [CodeGenSuppress("PurchaseValue")]
-    [CodeGenSuppress("ReleasePhoneNumber")]
-    [CodeGenSuppress("ReleasePhoneNumberValue")]
-    [CodeGenSuppress("Search")]
-    [CodeGenSuppress("SearchValue")]
-    [CodeGenSuppress("UpdatePhoneNumberCapabilities")]
-    [CodeGenSuppress("UpdatePhoneNumberCapabilitiesValue")]
-    [CodeGenSuppress("_value")]
-    [CodeGenSuppress("Equals", typeof(object))]
-    [CodeGenSuppress("Equals", typeof(PhoneNumberOperationType))]
-    [CodeGenSuppress("GetHashCode")]
-    [CodeGenSuppress("ToString")] */
     public partial struct PhoneNumberOperationType
     {
     }
