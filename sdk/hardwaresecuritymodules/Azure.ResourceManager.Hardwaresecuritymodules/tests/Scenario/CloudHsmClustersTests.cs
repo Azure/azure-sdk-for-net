e77525457c5bf61ca82f65eee5cdd10bf9ded74e
Emmeleia Arakleitou <earakleitou@microsoft.com>
2023-06-26 15:31:15 +0100

    Fix errors

--- sdk/hardwaresecuritymodules/Azure.ResourceManager.Hardwaresecuritymodules/tests/Scenario/CloudHsmClustersTests.cs
+++ sdk/hardwaresecuritymodules/Azure.ResourceManager.Hardwaresecuritymodules/tests/Scenario/CloudHsmClustersTests.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System.Threading.Tasks;
+using Azure.Core;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.Hardwaresecuritymodules.Models;
+using Azure.ResourceManager.Resources;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 
 namespace Azure.ResourceManager.Hardwaresecuritymodules.Tests
 {
@@ -24,7 +27,25 @@ namespace Azure.ResourceManager.Hardwaresecuritymodules.Tests
         [RecordedTest]
         public async Task CreateOrUpdateCloudHsmClusterTest()
         {
-            
+            string resourceName = Recording.GenerateAssetName("CloudhsmSDKTest");
+
+            CloudHsmCluster cloudHsmClusterBody = new CloudHsmCluster(new AzureLocation("eastus2"))
+            {
+                SecurityDomain = new CloudHsmClusterSecurityDomainProperties()
+                {
+                    FipsState = 2,
+                },
+                Sku = new CloudHsmClusterSku(CloudHsmClusterSkuName.StandardB1, CloudHsmClusterSkuFamily.B),
+                Tags =
+                {
+                    ["Dept"] = "hsm",
+                    ["Env"] = "df",
+                },
+            };
+
+            ArmOperation<CloudHsmCluster> armOperationChsm = await ResourceGroupResource.CreateOrUpdateCloudHsmClusterAsync(WaitUntil.Completed, resourceName, cloudHsmClusterBody);
+            CloudHsmCluster cloudHsmClusterResult = armOperationChsm.Value;
+            Assert.AreEqual(resourceName, cloudHsmClusterResult.Name);
         }
     }
 }
