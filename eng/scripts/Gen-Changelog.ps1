d332c0727651b68b6a6fb2c039fa76a2e21c0671
Rodge Fu <wenjiefu@microsoft.com>
2023-06-26 17:52:51 +0800

    add script for changeloggen

---
+++ eng/scripts/Gen-Changelog.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+param (
+    [Parameter(Position=0)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ServiceName,
+    [Parameter(Position=1)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ReleaseVersion, # i.e. 1.0.1
+    [Parameter(Position=2)]
+    [ValidateNotNullOrEmpty()]
+    [string] $ReleaseDate # i.e. 2023-01-01
+)
+
+$apiFilePathArray = Resolve-Path "$PSScriptRoot/../../sdk/$ServiceName/Azure.ResourceManager.*/api/Azure.ResourceManager.*.netstandard2.0.cs"
+
+if($apiFilePathArray.Length -ne 1)
+{
+    Write-Error "Multiple api files resolved."
+    $apiFilePathArray | Write-Error
+}
+else {
+    dotnet tool update Azure.SDK.Management.ChangelogGen -g --add-source https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-net/nuget/v3/index.json  --prerelease --interactive
+
+    changelog-gen-mgmt $apiFilePathArray[0] $ReleaseVersion $ReleaseDate    
+}
