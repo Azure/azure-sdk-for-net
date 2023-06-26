db4b127e9db2bf77ca9b9fce964c910e9d17304a
Rodge Fu <wenjiefu@microsoft.com>
2023-06-26 18:08:54 +0800

    add descriptions to script arguments

--- eng/scripts/Gen-Mgmt-Changelog.ps1
+++ eng/scripts/Gen-Mgmt-Changelog.ps1
@@ -1,14 +1,14 @@
 [CmdletBinding()]
 param (
-    [Parameter(Position=0, Mandatory=$true)]
+    [Parameter(Position=0, Mandatory=$true, HelpMessage="The service name under folder .../sdk/")]
     [ValidateNotNullOrEmpty()]
     [string] $ServiceName,
-    [Parameter(Position=1, Mandatory=$true)]
+    [Parameter(Position=1, Mandatory=$true, HelpMessage="Release version, i.e. 1.0.1")]
     [ValidateNotNullOrEmpty()]
-    [string] $ReleaseVersion, # i.e. 1.0.1
-    [Parameter(Position=2, Mandatory=$true)]
+    [string] $ReleaseVersion,
+    [Parameter(Position=2, Mandatory=$true, HelpMessage="Release date in format xxxx-xx-xx. i.e. 2023-01-10")]
     [ValidateNotNullOrEmpty()]
-    [string] $ReleaseDate # i.e. 2023-01-01
+    [string] $ReleaseDate
 )
 
 $apiFilePathArray = Resolve-Path "$PSScriptRoot/../../sdk/$ServiceName/Azure.ResourceManager.*/api/Azure.ResourceManager.*.netstandard2.0.cs"
