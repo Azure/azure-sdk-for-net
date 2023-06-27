701f611ad3de2391f7533f5591e73eacd8b9205d
Anne Thompson <annelo@microsoft.com>
2023-06-27 09:24:09 -0700

    Make model use MutableJsonDocument for TestId

---
+++ sdk/loadtestservice/Azure.Developer.LoadTesting/tests/LoadTestModelTests.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Text.Json;
+using Azure.Developer.LoadTesting.Models;
+using NUnit.Framework;
+
+namespace Azure.Developer.LoadTesting.Tests
+{
+    public class LoadTestModelTests
+    {
+        [Test]
+        public void CanSetTestId()
+        {
+            Test test = new()
+            {
+                TestId = "abc"
+            };
+
+            Assert.AreEqual("abc", test.TestId);
+        }
+
+        [Test]
+        public void CanGetTestId()
+        {
+            JsonDocument doc = JsonDocument.Parse("""{"testId": "abc"}""");
+            Test test = new(doc.RootElement);
+
+            Assert.AreEqual("abc", test.TestId);
+        }
+
+        [Test]
+        public void CanPatchTestId_NoChanges()
+        {
+            JsonDocument doc = JsonDocument.Parse("""{"testId": "abc"}""");
+            Test test = new(doc.RootElement);
+
+            BinaryData utf8;
+            using (MemoryStream stream = new())
+            {
+                test.WritePatch(stream);
+                stream.Position = 0;
+                utf8 = BinaryData.FromStream(stream);
+            }
+
+            CollectionAssert.AreEqual(""u8.ToArray(), utf8.ToArray());
+        }
+
+        //[Test]
+        //public void CanPatchTestId_OneChange()
+        //{
+        //    JsonDocument doc = JsonDocument.Parse("""{"testId": "abc"}""");
+        //    Test test = new(doc.RootElement);
+
+        //    test.TestId = "def";
+
+        //    BinaryData utf8;
+        //    using (MemoryStream stream = new())
+        //    {
+        //        test.WritePatch(stream);
+        //        stream.Position = 0;
+        //        utf8 = BinaryData.FromStream(stream);
+        //    }
+
+        //    CollectionAssert.AreEqual("""{"testId": "def"}"""u8.ToArray(), utf8.ToArray());
+        //}
+    }
+}
