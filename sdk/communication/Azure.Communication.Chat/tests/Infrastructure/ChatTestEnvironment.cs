9ff103f1a0b158ce13e414d5125177f3357ef36d
Nikitha Chettiar <nikithauc@gmail.com>
2023-06-28 19:16:30 -0700

    add test and exclude code coverage

--- sdk/communication/Azure.Communication.Chat/tests/Infrastructure/ChatTestEnvironment.cs
+++ sdk/communication/Azure.Communication.Chat/tests/Infrastructure/ChatTestEnvironment.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Diagnostics.CodeAnalysis;
 using Azure.Communication.Tests;
 
 namespace Azure.Communication.Chat.Tests
 {
+    [ExcludeFromCodeCoverage]
     /// <summary>
     /// A helper class used to retrieve information to be used for tests.
     /// </summary>
