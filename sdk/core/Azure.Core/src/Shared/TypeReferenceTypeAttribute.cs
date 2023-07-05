3255d8f49510c6dcc22146704fef59c1dc91ff0e
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-07-04 20:28:34 -0700

    Improve docs

--- sdk/core/Azure.Core/src/Shared/TypeReferenceTypeAttribute.cs
+++ sdk/core/Azure.Core/src/Shared/TypeReferenceTypeAttribute.cs
@@ -23,10 +23,12 @@ namespace Azure.Core
         /// Constructs a new instance of <see cref="TypeReferenceTypeAttribute"/>.
         /// </summary>
         /// <param name="ignoreExtraProperties">Whether to allow replacement to occur when the type to be replaced
-        /// contains extra properties as compared to the type attributed with <see cref="TypeReferenceTypeAttribute"/> that it will be replaced with.
-        /// Defaults to false.</param>
+        /// contains extra properties as compared to the reference type attributed with <see cref="TypeReferenceTypeAttribute"/> that it will
+        /// be replaced with. Defaults to false.</param>
         /// <param name="internalPropertiesToInclude">An array of internal properties to include for the reference type when evaluating whether type
-        /// replacement should occur. When evaluating a type for replacement, internal properties are considered on the type to be replaced.</param>
+        /// replacement should occur. When evaluating a type for replacement with a reference type, all internal properties are considered on the
+        /// type to be replaced. Thus this parameter can be used to specify internal properties to allow replacement to occur on a type with internal
+        /// properties.</param>
         public TypeReferenceTypeAttribute(bool ignoreExtraProperties, string[] internalPropertiesToInclude)
         {
             IgnoreExtraProperties = ignoreExtraProperties;
