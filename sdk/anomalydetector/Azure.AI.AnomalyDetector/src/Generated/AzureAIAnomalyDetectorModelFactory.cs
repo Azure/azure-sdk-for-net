620b8d84021027c84fc33eb9d8010c6e27ea658c
azure-sdk <azuresdk@microsoft.com>
2023-07-12 22:48:17 +0000

    Update SDK codes

--- sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/AzureAIAnomalyDetectorModelFactory.cs
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/AzureAIAnomalyDetectorModelFactory.cs
@@ -12,7 +12,7 @@ using System.Linq;
 namespace Azure.AI.AnomalyDetector
 {
     /// <summary> Model factory for models. </summary>
-    public static partial class AIAnomalyDetectorModelFactory
+    public static partial class AzureAIAnomalyDetectorModelFactory
     {
         /// <summary> Initializes a new instance of UnivariateLastDetectionResult. </summary>
         /// <param name="period">
