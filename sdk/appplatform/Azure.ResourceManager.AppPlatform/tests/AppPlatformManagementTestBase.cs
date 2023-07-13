adb9ed349f4e0a48466aa2958f0791a6f215c22a
Chengming <v-cruan@microsoft.com>
2023-07-13 16:04:04 +0800

    TestBase

--- sdk/appplatform/Azure.ResourceManager.AppPlatform/tests/AppPlatformManagementTestBase.cs
+++ sdk/appplatform/Azure.ResourceManager.AppPlatform/tests/AppPlatformManagementTestBase.cs
@@ -3,9 +3,11 @@
 
 using Azure.Core;
 using Azure.Core.TestFramework;
+using Azure.ResourceManager.AppPlatform.Models;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.TestFramework;
 using NUnit.Framework;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Azure.ResourceManager.AppPlatform.Tests
@@ -13,6 +15,10 @@ namespace Azure.ResourceManager.AppPlatform.Tests
     public class AppPlatformManagementTestBase : ManagementRecordedTestBase<AppPlatformManagementTestEnvironment>
     {
         protected ArmClient Client { get; private set; }
+        protected TenantResource DefaultTenant { get; private set; }
+        protected SubscriptionResource DefaultSubscription { get; private set; }
+        protected AzureLocation DefaultLocation = AzureLocation.EastUS;
+        protected const string DefaultRGNamePrefix = "AppPlatfromRG";
 
         protected AppPlatformManagementTestBase(bool isAsync, RecordedTestMode mode)
         : base(isAsync, mode)
@@ -25,16 +31,38 @@ namespace Azure.ResourceManager.AppPlatform.Tests
         }
 
         [SetUp]
-        public void CreateCommonClient()
+        public async Task CreateCommonClient()
         {
             Client = GetArmClient();
+            var tenants = await Client.GetTenants().GetAllAsync().ToEnumerableAsync();
+            DefaultTenant = tenants.FirstOrDefault();
+            DefaultSubscription = await Client.GetDefaultSubscriptionAsync();
         }
 
-        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        protected async Task<ResourceGroupResource> CreateResourceGroup()
         {
-            string rgName = Recording.GenerateAssetName(rgNamePrefix);
-            ResourceGroupData input = new ResourceGroupData(location);
-            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            string rgName = Recording.GenerateAssetName(DefaultRGNamePrefix);
+            ResourceGroupData input = new ResourceGroupData(DefaultLocation);
+            var lro = await DefaultSubscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            return lro.Value;
+        }
+
+        protected async Task<AppPlatformServiceResource> CreateAppPlatformService(ResourceGroupResource rg, string serviceName)
+        {
+            AppPlatformServiceData data = new AppPlatformServiceData(rg.Data.Location)
+            {
+                Properties = new AppPlatformServiceProperties(),
+                Sku = new AppPlatformSku()
+                {
+                    Name = "S0",
+                    Tier = "Standard",
+                },
+                Tags =
+                {
+                    ["key1"] = "value1",
+                },
+            };
+            var lro = await rg.GetAppPlatformServices().CreateOrUpdateAsync(WaitUntil.Completed, serviceName, data);
             return lro.Value;
         }
     }
---
+++ sdk/appplatform/Azure.ResourceManager.AppPlatform/tests/Scenario/AppPlatfromServiceTests.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.AppPlatform.Models;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.AppPlatform.Tests
+{
+    internal class AppPlatfromServiceTests : AppPlatformManagementTestBase
+    {
+        private ResourceGroupResource _resourceGroup;
+        private AppPlatformServiceCollection _serviceCollection;
+        private AppPlatformServiceResource _appPlatformService;
+        private string _serviceName;
+
+        public AppPlatfromServiceTests(bool isAsync) : base(isAsync, RecordedTestMode.Record)
+        {
+        }
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _resourceGroup = await CreateResourceGroup();
+            _serviceCollection = _resourceGroup.GetAppPlatformServices();
+            _serviceName = Recording.GenerateAssetName("aztestservice");
+            _appPlatformService = await CreateAppPlatformService(_resourceGroup, _serviceName);
+        }
+
+        [Test]
+        public void CreateOrUpdate()
+        {
+            ValidateAppPlatformService(_appPlatformService.Data, _serviceName);
+        }
+
+        [Test]
+        public async Task Exist()
+        {
+            var flag = await _serviceCollection.ExistsAsync(_serviceName);
+            Assert.IsTrue(flag);
+        }
+
+        [Test]
+        public async Task Get()
+        {
+            var AppPlatformService = await _serviceCollection.GetAsync(_serviceName);
+            ValidateAppPlatformService(AppPlatformService.Value.Data, _serviceName);
+        }
+
+        [Test]
+        public async Task GetAll()
+        {
+            var list = await _serviceCollection.GetAllAsync().ToEnumerableAsync();
+            Assert.IsNotEmpty(list);
+            ValidateAppPlatformService(list.FirstOrDefault().Data, _serviceName);
+        }
+
+        [Test]
+        public async Task Delete()
+        {
+            await _appPlatformService.DeleteAsync(WaitUntil.Completed);
+            bool flag = await _serviceCollection.ExistsAsync(_serviceName);
+            Assert.IsFalse(flag);
+        }
+
+        private void ValidateAppPlatformService(AppPlatformServiceData service, string _serviceName)
+        {
+            Assert.IsNotNull(service);
+        }
+    }
+}
