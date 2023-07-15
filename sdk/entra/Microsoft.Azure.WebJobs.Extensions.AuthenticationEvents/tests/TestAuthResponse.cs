e340d157fbd48ebe935c3ac834983826e1f833a2
Abdul Hakim <abdulhakim@microsoft.com>
2023-07-14 17:08:45 -0700

    Updates based on feedback

---
+++ sdk/entra/Microsoft.Azure.WebJobs.Extensions.AuthenticationEvents/tests/TestAuthResponse.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.AuthenticationEvents.Framework;
+using System.Net.Http;
+using System.Net;
+
+namespace Microsoft.Azure.WebJobs.Extensions.AuthenticationEvents.Tests
+{
+
+	internal class TestAuthResponse : AuthenticationEventResponse
+	{
+		internal TestAuthResponse(HttpStatusCode code, string content)
+		: this(code)
+		{
+			Content = new StringContent(content);
+		}
+
+		internal TestAuthResponse(HttpStatusCode code)
+		{
+			StatusCode = code;
+		}
+
+		internal override void Invalidate()
+		{ }
+
+		internal override void ValidateActions()
+		{ }
+	}
+}
