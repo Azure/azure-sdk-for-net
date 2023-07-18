90a0baf9c42f8cb7f999ed8891e1fe776423b3de
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-07-18 14:16:48 +0800

    Update

---
+++ sdk/cognitivelanguage/Azure.AI.Language.QuestionAnswering/src/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "AZC0018: Protocol method should have requestContext as the last parameter and don't have model as parameter type or return type. Protocol method should not have optional parameters if ambiguity exists between protocol method and convenience method.", Justification = "Flag keep-non-overloadable-protocol-signature is on.")]
