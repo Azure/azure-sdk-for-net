1944f63cfb8030139f18aafd2bc09ece2daaa03f
Vishwesh Bankwar <vibankwa@microsoft.com>
2023-07-19 14:01:11 -0700

    Stress test for trace exporter

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Stress.Tests/Meat.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Runtime.CompilerServices;
+using OpenTelemetry.Trace;
+using Azure.Core.TestFramework;
+using System.Diagnostics;
+using OpenTelemetry;
+using Azure.Identity;
+
+namespace Azure.Monitor.OpenTelemetry.Exporter.Stress.Tests;
+
+public partial class Program
+{
+    private const string ActivitySourceName = "StressTest";
+    private static ActivitySource s_activitySource = new ActivitySource(ActivitySourceName);
+    private static AzureMonitorTraceExporter? s_azureMonitorTraceExporter;
+    private static Batch<Activity> s_batch;
+    public static void Main()
+    {
+        Environment.SetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING", "InstrumentationKey=00000000-0000-0000-0000-000000000000");
+
+        //TODO: Metrics and Logs
+        InitTraces();
+
+        Stress(concurrency: 0, prometheusPort: 9464);
+    }
+
+    private static void InitTraces()
+    {
+        // Set up SDK
+        var options = new AzureMonitorExporterOptions();
+        MockResponse response = new MockResponse(200, "OK");
+        options.Transport = new MockTransport((_) => response);
+        s_azureMonitorTraceExporter = new AzureMonitorTraceExporter(options);
+        Sdk.CreateTracerProviderBuilder()
+            .SetSampler(new AlwaysOnSampler())
+            .AddSource(ActivitySourceName)
+            .AddProcessor(new BatchActivityExportProcessor(s_azureMonitorTraceExporter))
+            .Build();
+
+        // Create Batch
+        Activity[] activities = new Activity[2];
+        using (var client = s_activitySource.StartActivity("Client", ActivityKind.Client))
+        {
+            client?.SetStatus(Status.Ok);
+            client?.SetTag(SemanticConventions.AttributeHttpRequestMethod, "Get");
+            client?.SetTag(SemanticConventions.AttributeUrlFull, "https://localhost:8080/api/123");
+            client?.SetTag(SemanticConventions.AttributeNetworkProtocolVersion, "1.1");
+            client?.SetTag(SemanticConventions.AttributeServerAddress, "localhost");
+            client?.SetTag(SemanticConventions.AttributeServerPort, "8080");
+            client?.SetTag(SemanticConventions.AttributeHttpResponseStatusCode, 200);
+            client?.SetTag(SemanticConventions.AttributeUserAgentOriginal, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0");
+
+            using (var server = s_activitySource.StartActivity("Client", ActivityKind.Server))
+            {
+                server?.SetStatus(Status.Ok);
+                server?.SetTag(SemanticConventions.AttributeHttpRequestMethod, "Get");
+                server?.SetTag(SemanticConventions.AttributeUrlScheme, "https");
+                server?.SetTag(SemanticConventions.AttributeUrlPath, "/api/123");
+                server?.SetTag(SemanticConventions.AttributeMessagingProtocolVersion, "1.1");
+                server?.SetTag(SemanticConventions.AttributeHttpRoute, "api/{searchId}");
+                server?.SetTag(SemanticConventions.AttributeServerAddress, "localhost");
+                server?.SetTag(SemanticConventions.AttributeServerPort, "8080");
+                server?.SetTag(SemanticConventions.AttributeUserAgentOriginal, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0");
+                server?.SetTag(SemanticConventions.AttributeHttpResponseStatusCode, 200);
+
+                activities[0] = server!;
+            }
+
+            activities[1] = client!;
+        }
+
+        s_batch = new Batch<Activity>(activities, 2);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    protected static void Run()
+    {
+        s_azureMonitorTraceExporter?.Export(s_batch);
+    }
+}
---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.Stress.Tests/Skeleton.cs
@@ -0,0 +1,177 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+
+namespace Azure.Monitor.OpenTelemetry.Exporter.Stress.Tests;
+
+public partial class Program
+{
+    private static volatile bool bContinue = true;
+    private static volatile string output = "Test results not available yet.";
+
+    static Program()
+    {
+    }
+
+    public static void Stress(int concurrency = 0, int prometheusPort = 0)
+    {
+#if DEBUG
+        Console.WriteLine("***WARNING*** The current build is DEBUG which may affect timing!");
+        Console.WriteLine();
+#endif
+
+        if (concurrency < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(concurrency), "concurrency level should be a non-negative number.");
+        }
+
+        if (concurrency == 0)
+        {
+            concurrency = Environment.ProcessorCount;
+        }
+
+        using var meter = new Meter("OpenTelemetry.Tests.Stress." + Guid.NewGuid().ToString("D"));
+        var cntLoopsTotal = 0UL;
+        meter.CreateObservableCounter(
+            "OpenTelemetry.Tests.Stress.Loops",
+            () => unchecked((long)cntLoopsTotal),
+            description: "The total number of `Run()` invocations that are completed.");
+        var dLoopsPerSecond = 0D;
+        meter.CreateObservableGauge(
+            "OpenTelemetry.Tests.Stress.LoopsPerSecond",
+            () => dLoopsPerSecond,
+            description: "The rate of `Run()` invocations based on a small sliding window of few hundreds of milliseconds.");
+        var dCpuCyclesPerLoop = 0D;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            meter.CreateObservableGauge(
+                "OpenTelemetry.Tests.Stress.CpuCyclesPerLoop",
+                () => dCpuCyclesPerLoop,
+                description: "The average CPU cycles for each `Run()` invocation, based on a small sliding window of few hundreds of milliseconds.");
+        }
+
+        using var meterProvider = prometheusPort != 0 ? Sdk.CreateMeterProviderBuilder()
+            .AddMeter(meter.Name)
+            .AddRuntimeInstrumentation()
+            .AddPrometheusHttpListener(
+                options => options.UriPrefixes = new string[] { $"http://localhost:{prometheusPort}/" })
+            .Build() : null;
+
+        var statistics = new long[concurrency];
+        var watchForTotal = Stopwatch.StartNew();
+
+        Parallel.Invoke(
+            () =>
+            {
+                Console.Write($"Running (concurrency = {concurrency}");
+
+                if (prometheusPort != 0)
+                {
+                    Console.Write($", prometheusEndpoint = http://localhost:{prometheusPort}/metrics/");
+                }
+
+                Console.WriteLine("), press <Esc> to stop...");
+
+                var bOutput = false;
+                var watch = new Stopwatch();
+                while (true)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        var key = Console.ReadKey(true).Key;
+
+                        switch (key)
+                        {
+                            case ConsoleKey.Enter:
+                                Console.WriteLine(string.Format("{0} {1}", DateTime.UtcNow.ToString("O"), output));
+                                break;
+                            case ConsoleKey.Escape:
+                                bContinue = false;
+                                return;
+                            case ConsoleKey.Spacebar:
+                                bOutput = !bOutput;
+                                break;
+                        }
+
+                        continue;
+                    }
+
+                    if (bOutput)
+                    {
+                        Console.WriteLine(string.Format("{0} {1}", DateTime.UtcNow.ToString("O"), output));
+                    }
+
+                    var cntLoopsOld = (ulong)statistics.Sum();
+                    var cntCpuCyclesOld = GetCpuCycles();
+
+                    watch.Restart();
+                    Thread.Sleep(200);
+                    watch.Stop();
+
+                    cntLoopsTotal = (ulong)statistics.Sum();
+                    var cntCpuCyclesNew = GetCpuCycles();
+
+                    var nLoops = cntLoopsTotal - cntLoopsOld;
+                    var nCpuCycles = cntCpuCyclesNew - cntCpuCyclesOld;
+
+                    dLoopsPerSecond = (double)nLoops / ((double)watch.ElapsedMilliseconds / 1000.0);
+                    dCpuCyclesPerLoop = nLoops == 0 ? 0 : nCpuCycles / nLoops;
+
+                    output = $"Loops: {cntLoopsTotal:n0}, Loops/Second: {dLoopsPerSecond:n0}, CPU Cycles/Loop: {dCpuCyclesPerLoop:n0}, RunwayTime (Seconds): {watchForTotal.Elapsed.TotalSeconds:n0} ";
+                    Console.Title = output;
+                }
+            },
+            () =>
+            {
+                Parallel.For(0, concurrency, (i) =>
+                {
+                    statistics[i] = 0;
+                    while (bContinue)
+                    {
+                        Run();
+                        statistics[i]++;
+                    }
+                });
+            });
+
+        watchForTotal.Stop();
+        cntLoopsTotal = (ulong)statistics.Sum();
+        var totalLoopsPerSecond = (double)cntLoopsTotal / ((double)watchForTotal.ElapsedMilliseconds / 1000.0);
+        var cntCpuCyclesTotal = GetCpuCycles();
+        var cpuCyclesPerLoopTotal = cntLoopsTotal == 0 ? 0 : cntCpuCyclesTotal / cntLoopsTotal;
+        Console.WriteLine("Stopping the stress test...");
+        Console.WriteLine($"* Total Runaway Time (seconds) {watchForTotal.Elapsed.TotalSeconds:n0}");
+        Console.WriteLine($"* Total Loops: {cntLoopsTotal:n0}");
+        Console.WriteLine($"* Average Loops/Second: {totalLoopsPerSecond:n0}");
+        Console.WriteLine($"* Average CPU Cycles/Loop: {cpuCyclesPerLoopTotal:n0}");
+    }
+
+    [DllImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool QueryProcessCycleTime(IntPtr hProcess, out ulong cycles);
+
+    private static ulong GetCpuCycles()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return 0;
+        }
+
+        if (!QueryProcessCycleTime((IntPtr)(-1), out var cycles))
+        {
+            return 0;
+        }
+
+        return cycles;
+    }
+}
