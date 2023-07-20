d25956131ab547294f759772f1dbd1602020160a
Abhijeet Baranwal <abaranwal@microsoft.com>
2023-07-20 15:41:36 +0530

    changes for token based upload artifact

---
+++ sdk/edgeorder/Azure.ResourceManager.EdgeOrder/src/Customizations/Models/SiteKey.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.ResourceManager.EdgeOrder.Customizations.Models
+{
+    internal class SiteKey
+    {
+        /// <summary>
+        /// resource id for boostrap resource
+        /// </summary>
+        internal string resourceId;
+
+        /// <summary>
+        /// aadendpoint for authentication
+        /// </summary>
+        internal string aadEndpoint;
+
+        /// <summary>
+        /// tenant id of customer
+        /// </summary>
+        internal string tenantId;
+
+        /// <summary>
+        /// client id
+        /// </summary>
+        internal string clientId;
+
+        /// <summary>
+        /// secret data
+        /// </summary>
+        internal string clientSecret;
+    }
+}
