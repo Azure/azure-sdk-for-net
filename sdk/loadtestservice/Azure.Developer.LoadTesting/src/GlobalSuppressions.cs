4df596e346b802d862a5b4454ee28219bf39a279
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-07-25 17:30:02 +0800

    Update

--- sdk/loadtestservice/Azure.Developer.LoadTesting/src/GlobalSuppressions.cs
+++ sdk/loadtestservice/Azure.Developer.LoadTesting/src/GlobalSuppressions.cs
@@ -4,4 +4,3 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Usage", "AZC0001:Use one of the following pre-approved namespace groups (https://azure.github.io/azure-sdk/registered_namespaces.html): Azure.AI, Azure.Analytics, Azure.Communication, Azure.Data, Azure.DigitalTwins, Azure.IoT, Azure.Learn, Azure.Media, Azure.Management, Azure.Messaging, Azure.ResourceManager, Azure.Search, Azure.Security, Azure.Storage, Azure.Template, Azure.Identity, Microsoft.Extensions.Azure", Justification = "<Pending>", Scope = "namespace", Target = "~N:Azure.Developer.LoadTesting")]
-[assembly: SuppressMessage("Usage", "AZC0018: Protocol method should have requestContext as the last parameter and don't have model as parameter type or return type. Protocol method should not have optional parameters if ambiguity exists between protocol method and convenience method.", Justification = "Flag keep-non-overloadable-protocol-signature is on.")]
