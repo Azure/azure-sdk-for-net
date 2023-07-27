4c34592ed7392acf2b4248fb89d264a2ac9a6492
ArcturusZhang <dapzhang@microsoft.com>
2023-07-27 12:46:15 +0800

    add a test case to validate the mocking pattern using reflection

--- sdk/compute/Azure.ResourceManager.Compute/tests/Mock/MockingPatternValidationTests.cs
+++ sdk/compute/Azure.ResourceManager.Compute/tests/Mock/MockingPatternValidationTests.cs
@@ -4,29 +4,67 @@
 using System;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using NUnit.Framework;
 
 namespace Azure.ResourceManager.Compute.Tests.Mock
 {
     public class MockingPatternValidationTests
     {
+        private const string RPNamespace = "Azure.ResourceManager.Compute";
+        private const string RPName = "Compute";
+
         // TODO -- after enough test cases on each individual RP, we should move this to mgmt test base class so that every RP will automatically get this test case
         [Test]
         public void ValidateMockingPattern()
         {
-            var current = Assembly.GetExecutingAssembly();
-            var assemblyName = current.GetName().Name;
-            if (!assemblyName.EndsWith(".Tests"))
-                throw new InvalidOperationException("Wrong assembly");
+            var assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == RPNamespace);
 
-            var sdkAssemblyName = assemblyName.Substring(0, assemblyName.Length - 6);
-            var sdkAssembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == sdkAssemblyName);
-            Assert.IsNotNull(sdkAssembly);
+            Assert.IsNotNull(assembly);
 
             // find the extension class
-            Assert.IsTrue(sdkAssemblyName.StartsWith("Azure.ResourceManager"));
-            sdkAssemblyName = sdkAssemblyName.Substring("Azure.ResourceManager".Length);
-            //var rpName = string.Join("", sdkAssemblyName.Split(".", StringSplitOptions.RemoveEmptyEntries));
+            var extensionName = $"{RPNamespace}.{RPName}Extensions";
+            var extensionType = assembly.GetType(extensionName);
+
+            Assert.IsNotNull(extensionType);
+
+            foreach (var method in extensionType.GetMethods(BindingFlags.Public | BindingFlags.Static))
+            {
+                ValidateMethod(assembly, method);
+            }
+        }
+
+        private static void ValidateMethod(Assembly assembly, MethodInfo method)
+        {
+            // the method should be public, static and extension
+            if (!method.IsStatic || !method.IsPublic || !method.IsDefined(typeof(ExtensionAttribute), true))
+                return;
+
+            // finds the corresponding mocking extension class
+            var parameters = method.GetParameters();
+            var extendeeType = parameters[0].ParameterType;
+
+            var mockingExtensionTypeName = GetMockingExtensionTypeName(RPNamespace, RPName, extendeeType.Name);
+
+            var mockingExtensionType = assembly.GetType(mockingExtensionTypeName);
+            Assert.IsNotNull(mockingExtensionType);
+
+            // validate the mocking extension class has a method with the exact name and parameter list
+            var expectedTypes = parameters.Skip(1).Select(p => p.ParameterType).ToArray();
+            var methodOnExtension = mockingExtensionType.GetMethod(method.Name, parameters.Skip(1).Select(p => p.ParameterType).ToArray());
+
+            Assert.IsNotNull(methodOnExtension, $"expect class {mockingExtensionType} to have method {method}, but found none");
+        }
+
+        private static string GetMockingExtensionTypeName(string rpNamespace, string rpName, string extendeeName)
+        {
+            // trim resource suffix
+            if (extendeeName.EndsWith("Resource"))
+            {
+                extendeeName = extendeeName.Substring(0, extendeeName.Length - 8);
+            }
+
+            return $"{rpNamespace}.Mocking.{rpName}{extendeeName}MockingExtension";
         }
     }
 }
