d8e57f1dc03397e2deea3419a5ed18e9d883bd5b
vidit-msft <78883096+vidit-msft@users.noreply.github.com>
2023-07-27 22:21:50 -0700

    address more feedback

--- sdk/sqlmanagement/Azure.ResourceManager.Sql/tests/Scenario/ElasticPoolTests.cs
+++ sdk/sqlmanagement/Azure.ResourceManager.Sql/tests/Scenario/ElasticPoolTests.cs
@@ -102,58 +102,65 @@ namespace Azure.ResourceManager.Sql.Tests.Scenario
         }
 
         [Test]
+        [TestCase("Default")]
+        [TestCase("VBS")]
         [RecordedTest]
-        public async Task ElasticPoolApiTestsWithEnclaves()
+        public async Task ElasticPoolApiTestsWithEnclaves(string preferredEnclaveType)
         {
-            foreach (SqlAlwaysEncryptedEnclaveType enclaveType in enclaveTypes)
+            SqlAlwaysEncryptedEnclaveType enclaveType;
+            if (preferredEnclaveType == SqlAlwaysEncryptedEnclaveType.Default.ToString())
             {
-                string preferredEnclaveType = enclaveType.ToString();
-                string poolName1 = Recording.GenerateAssetName($"sql-pool-{preferredEnclaveType}-");
-                string poolName2 = Recording.GenerateAssetName($"sql-pool-{preferredEnclaveType}-");
-
-                // 1.CreateOrUpdate
-                ElasticPoolData data = new ElasticPoolData(Location)
-                {
-                    PreferredEnclaveType = preferredEnclaveType
-                };
-
-                var pool1 = await collection.CreateOrUpdateAsync(WaitUntil.Completed, poolName1, data);
-                Assert.IsNotNull(pool1.Value.Data);
-                Assert.AreEqual(poolName1, pool1.Value.Data.Name);
-                Assert.AreEqual(enclaveType, pool1.Value.Data.PreferredEnclaveType);
-
-                // 2.CheckIfExist
-                Assert.IsTrue(await collection.ExistsAsync(poolName1));
-                Assert.IsFalse(await collection.ExistsAsync(poolName2));
-
-                // 3.Get
-                var getPool = await collection.GetAsync(poolName1);
-                Assert.IsNotNull(getPool.Value.Data);
-                Assert.AreEqual(poolName1, getPool.Value.Data.Name);
-                Assert.AreEqual(enclaveType, getPool.Value.Data.PreferredEnclaveType);
-
-                var pool2 = await collection.CreateOrUpdateAsync(WaitUntil.Completed, poolName2, data);
-                Assert.IsNotNull(pool2.Value.Data);
-                Assert.AreEqual(poolName2, pool2.Value.Data.Name);
-
-                // 4.GetAll
-                var list = await collection.GetAllAsync().ToEnumerableAsync();
-                Assert.IsNotEmpty(list);
-                Assert.AreEqual(2, list.Count);
-                string[] poolNamesList = { list.First().Data.Name, list.Last().Data.Name };
-                Assert.Contains(poolName1, poolNamesList);
-                Assert.Contains(poolName2, poolNamesList);
-
-                // 5.Delete
-                var deletePool1 = await collection.GetAsync(poolName1);
-                await deletePool1.Value.DeleteAsync(WaitUntil.Completed);
-
-                var deletePool2 = await collection.GetAsync(poolName2);
-                await deletePool2.Value.DeleteAsync(WaitUntil.Completed);
-
-                list = await collection.GetAllAsync().ToEnumerableAsync();
-                Assert.AreEqual(0, list.Count);
+                enclaveType = SqlAlwaysEncryptedEnclaveType.Default;
             }
+            else
+            {
+                enclaveType = SqlAlwaysEncryptedEnclaveType.Vbs;
+            }
+            string poolName1 = Recording.GenerateAssetName($"sql-pool-{preferredEnclaveType}-");
+            string poolName2 = Recording.GenerateAssetName($"sql-pool-{preferredEnclaveType}-");
+
+            // 1.CreateOrUpdate
+            ElasticPoolData data = new ElasticPoolData(Location)
+            {
+                PreferredEnclaveType = preferredEnclaveType
+            };
+
+            var pool1 = await collection.CreateOrUpdateAsync(WaitUntil.Completed, poolName1, data);
+            Assert.IsNotNull(pool1.Value.Data);
+            Assert.AreEqual(poolName1, pool1.Value.Data.Name);
+            Assert.AreEqual(enclaveType, pool1.Value.Data.PreferredEnclaveType);
+
+            // 2.CheckIfExist
+            Assert.IsTrue(await collection.ExistsAsync(poolName1));
+            Assert.IsFalse(await collection.ExistsAsync(poolName2));
+
+            // 3.Get
+            var getPool = await collection.GetAsync(poolName1);
+            Assert.IsNotNull(getPool.Value.Data);
+            Assert.AreEqual(poolName1, getPool.Value.Data.Name);
+            Assert.AreEqual(enclaveType, getPool.Value.Data.PreferredEnclaveType);
+
+            var pool2 = await collection.CreateOrUpdateAsync(WaitUntil.Completed, poolName2, data);
+            Assert.IsNotNull(pool2.Value.Data);
+            Assert.AreEqual(poolName2, pool2.Value.Data.Name);
+
+            // 4.GetAll
+            var list = await collection.GetAllAsync().ToEnumerableAsync();
+            Assert.IsNotEmpty(list);
+            Assert.AreEqual(2, list.Count);
+            string[] poolNamesList = { list.First().Data.Name, list.Last().Data.Name };
+            Assert.Contains(poolName1, poolNamesList);
+            Assert.Contains(poolName2, poolNamesList);
+
+            // 5.Delete
+            var deletePool1 = await collection.GetAsync(poolName1);
+            await deletePool1.Value.DeleteAsync(WaitUntil.Completed);
+
+            var deletePool2 = await collection.GetAsync(poolName2);
+            await deletePool2.Value.DeleteAsync(WaitUntil.Completed);
+
+            list = await collection.GetAllAsync().ToEnumerableAsync();
+            Assert.AreEqual(0, list.Count);
         }
     }
 }
