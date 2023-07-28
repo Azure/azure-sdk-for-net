512832af4a8f435c7cd3ddb2fa751adcf09644ea
Srinikhil Naravamakula <srinikhil.r@gmail.com>
2023-07-28 03:14:20 -0500

    [CosmosDB] Changes for Rest API Version 2023-03-15-preview (#36950)
    * [CosmosDB] Changes for Rest API Version 2023-03-15-preview
    
    * add tests for connection strings changes
    
    * Save changes
    
    * Save changes
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * Save Recordings
    
    * Save Recordings
    
    * adding materializedview tests
    
    * Save Recordings
    
    * resolving comments
    
    * Save changes
    
    * Save changes
    
    * Save Recordings
    
    * Save Recordings
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * saving work
    
    * Renames
    
    * Operation Rename
    
    * Address code comments.
    
    * Address code changes.
    
    * error fix.
    
    * Export API
    
    * Update
    
    * rename ConnectionString
    
    ---------
    
    Co-authored-by: Carolyn Jackson <carjackson@microsoft.com>
    Co-authored-by: Chandrasekhar Gunturi <chguntur@microsoft.com>
    Co-authored-by: Himanshu Sunil Dhawale <hidhawal@microsoft.com>
    Co-authored-by: himanshudhawale <46133071+himanshudhawale@users.noreply.github.com>
    Co-authored-by: Wei Hu <huwe@microsoft.com>

--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/MongoDBDatabaseResource.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/MongoDBDatabaseResource.cs
@@ -357,6 +357,82 @@ namespace Azure.ResourceManager.CosmosDB
         }
 
         /// <summary>
+        /// Merges the partitions of a MongoDB database
+        /// <list type="bullet">
+        /// <item>
+        /// <term>Request Path</term>
+        /// <description>/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.DocumentDB/databaseAccounts/{accountName}/mongodbDatabases/{databaseName}/partitionMerge</description>
+        /// </item>
+        /// <item>
+        /// <term>Operation Id</term>
+        /// <description>MongoDBResources_MongoDBDatabasePartitionMerge</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
+        /// <param name="mergeParameters"> The parameters for the merge operation. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="mergeParameters"/> is null. </exception>
+        public virtual async Task<ArmOperation<PhysicalPartitionStorageInfoCollection>> MongoDBDatabasePartitionMergeAsync(WaitUntil waitUntil, MergeParameters mergeParameters, CancellationToken cancellationToken = default)
+        {
+            Argument.AssertNotNull(mergeParameters, nameof(mergeParameters));
+
+            using var scope = _mongoDBDatabaseMongoDBResourcesClientDiagnostics.CreateScope("MongoDBDatabaseResource.MongoDBDatabasePartitionMerge");
+            scope.Start();
+            try
+            {
+                var response = await _mongoDBDatabaseMongoDBResourcesRestClient.MongoDBDatabasePartitionMergeAsync(Id.SubscriptionId, Id.ResourceGroupName, Id.Parent.Name, Id.Name, mergeParameters, cancellationToken).ConfigureAwait(false);
+                var operation = new CosmosDBArmOperation<PhysicalPartitionStorageInfoCollection>(new PhysicalPartitionStorageInfoCollectionOperationSource(), _mongoDBDatabaseMongoDBResourcesClientDiagnostics, Pipeline, _mongoDBDatabaseMongoDBResourcesRestClient.CreateMongoDBDatabasePartitionMergeRequest(Id.SubscriptionId, Id.ResourceGroupName, Id.Parent.Name, Id.Name, mergeParameters).Request, response, OperationFinalStateVia.Location);
+                if (waitUntil == WaitUntil.Completed)
+                    await operation.WaitForCompletionAsync(cancellationToken).ConfigureAwait(false);
+                return operation;
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Merges the partitions of a MongoDB database
+        /// <list type="bullet">
+        /// <item>
+        /// <term>Request Path</term>
+        /// <description>/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.DocumentDB/databaseAccounts/{accountName}/mongodbDatabases/{databaseName}/partitionMerge</description>
+        /// </item>
+        /// <item>
+        /// <term>Operation Id</term>
+        /// <description>MongoDBResources_MongoDBDatabasePartitionMerge</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
+        /// <param name="mergeParameters"> The parameters for the merge operation. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="mergeParameters"/> is null. </exception>
+        public virtual ArmOperation<PhysicalPartitionStorageInfoCollection> MongoDBDatabasePartitionMerge(WaitUntil waitUntil, MergeParameters mergeParameters, CancellationToken cancellationToken = default)
+        {
+            Argument.AssertNotNull(mergeParameters, nameof(mergeParameters));
+
+            using var scope = _mongoDBDatabaseMongoDBResourcesClientDiagnostics.CreateScope("MongoDBDatabaseResource.MongoDBDatabasePartitionMerge");
+            scope.Start();
+            try
+            {
+                var response = _mongoDBDatabaseMongoDBResourcesRestClient.MongoDBDatabasePartitionMerge(Id.SubscriptionId, Id.ResourceGroupName, Id.Parent.Name, Id.Name, mergeParameters, cancellationToken);
+                var operation = new CosmosDBArmOperation<PhysicalPartitionStorageInfoCollection>(new PhysicalPartitionStorageInfoCollectionOperationSource(), _mongoDBDatabaseMongoDBResourcesClientDiagnostics, Pipeline, _mongoDBDatabaseMongoDBResourcesRestClient.CreateMongoDBDatabasePartitionMergeRequest(Id.SubscriptionId, Id.ResourceGroupName, Id.Parent.Name, Id.Name, mergeParameters).Request, response, OperationFinalStateVia.Location);
+                if (waitUntil == WaitUntil.Completed)
+                    operation.WaitForCompletion(cancellationToken);
+                return operation;
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+
+        /// <summary>
         /// Add a tag to the current resource.
         /// <list type="bullet">
         /// <item>
