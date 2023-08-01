6db3fee5cb1e746f6c03c332185f45fe595e71bb
archerzz <archerzz@gmail.com>
2023-08-01 15:45:02 +0800

    draft of applying mgmt sdk analyzers
    - proposed way to change eng props to add mgmt sdk analyzers
    - add a example of how to suppress warning
    
    https://github.com/Azure/autorest.csharp/issues/3486
    https://github.com/Azure/autorest.csharp/issues/3488

---
+++ sdk/websites/Azure.ResourceManager.AppService/src/GlobalSuppresions.cs
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Azure.MgmtSdk.Analyzers", "AZM0020", Justification = "Already GA",
+    Scope = "member", Target = "Azure.ResourceManager.AppService.StaticSiteData.#AllowConfigFileUpdates")]
