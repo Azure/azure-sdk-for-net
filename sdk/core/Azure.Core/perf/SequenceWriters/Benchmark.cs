32d24fa27c44056e492b418c0d666a0377072475
m-nash <prognash@gmail.com>
2023-08-02 16:16:45 -0700

    remove empty file

--- sdk/core/Azure.Core/perf/SequenceWriters/Benchmark.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-namespace Azure.Core.Perf.SequenceWriters
-{
-    public class Benchmark
-    {
-    }
-}
