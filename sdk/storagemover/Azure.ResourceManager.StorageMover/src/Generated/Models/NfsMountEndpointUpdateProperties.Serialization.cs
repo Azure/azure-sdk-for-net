953aaf186d9c43d914f22d831e7783d2ae840965
yifanz7 <131133995+yifanz7@users.noreply.github.com>
2023-08-02 12:02:33 +0800

    [StorageMover] Upgrade to API version 2023-07-01-preview  (#37928)

--- sdk/storagemover/Azure.ResourceManager.StorageMover/src/Generated/Models/NfsMountEndpointUpdateProperties.Serialization.cs
+++ sdk/storagemover/Azure.ResourceManager.StorageMover/src/Generated/Models/NfsMountEndpointUpdateProperties.Serialization.cs
@@ -10,7 +10,7 @@ using Azure.Core;
 
 namespace Azure.ResourceManager.StorageMover.Models
 {
-    public partial class EndpointBaseUpdateProperties : IUtf8JsonSerializable
+    public partial class NfsMountEndpointUpdateProperties : IUtf8JsonSerializable
     {
         void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
         {
