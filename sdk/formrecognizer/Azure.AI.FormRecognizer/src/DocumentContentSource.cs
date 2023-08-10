ce1d6332c5ac2b1b6d81b1d2f3155481346c06a7
Caio Saldanha <camaiaor@microsoft.com>
2023-08-10 14:25:11 -0700

    [FormRecognizer] Updated docs for 4.1.0 + final architect feedback (#38092)

--- sdk/formrecognizer/Azure.AI.FormRecognizer/src/DocumentContentSource.cs
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/DocumentContentSource.cs
@@ -5,15 +5,15 @@ namespace Azure.AI.FormRecognizer.DocumentAnalysis
 {
     /// <summary>
     /// The location containing data to train Document Models and Document Classifiers.
-    /// Supported data sources are:
+    /// Supported content sources are:
     /// <list type="bullet">
     ///   <item><description><see cref="BlobContentSource"/></description></item>
     ///   <item><description><see cref="BlobFileListContentSource"/></description></item>
     /// </list>
     /// </summary>
-    public abstract class ContentSource
+    public abstract class DocumentContentSource
     {
-        internal ContentSource(ContentSourceKind kind)
+        internal DocumentContentSource(DocumentContentSourceKind kind)
         {
             Kind = kind;
         }
@@ -21,6 +21,6 @@ namespace Azure.AI.FormRecognizer.DocumentAnalysis
         /// <summary>
         /// The source kind.
         /// </summary>
-        public ContentSourceKind Kind { get; }
+        public DocumentContentSourceKind Kind { get; }
     }
 }
