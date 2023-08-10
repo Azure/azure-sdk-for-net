2b64b232259260ed72673a718d8d463fce46397b
Madalyn Redding <mredding@microsoft.com>
2023-08-10 16:18:20 -0700

    adding compatibility test

---
+++ sdk/core/Azure.Core/aotcompatibility/AotCompatibilityTestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Azure.Core.Pipeline;
+using System;
+
+namespace Azure.Core.Tests.AotCompatibility.TestApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
