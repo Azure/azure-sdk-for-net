cbac87f4afb869c0605c03dbca6ea6b49a8db069
Jesse Squire <jsquire@microsoft.com>
2023-08-11 07:32:13 -0700

    Trailing whitespace (again)

--- sdk/servicebus/Azure.Messaging.ServiceBus/src/Client/ServiceBusClientOptions.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/src/Client/ServiceBusClientOptions.cs
@@ -92,7 +92,7 @@ namespace Azure.Messaging.ServiceBus
         /// </summary>
         /// <remarks>
         /// The retry options are only considered for interactions with the Service Bus service. They do not apply to failures in the
-        /// <see cref="ServiceBusProcessor.ProcessMessageAsync" /> handler. Developers are responsible for error handling and retries 
+        /// <see cref="ServiceBusProcessor.ProcessMessageAsync" /> handler. Developers are responsible for error handling and retries
         /// as part of their event handler.
         ///</remarks>
         public ServiceBusRetryOptions RetryOptions
