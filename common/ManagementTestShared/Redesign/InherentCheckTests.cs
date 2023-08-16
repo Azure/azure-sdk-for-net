e8e5be3669b5bf59683826c31a3004e14e647027
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2023-08-16 16:42:26 +0800

    Update InherentCheckTests.cs

--- common/ManagementTestShared/Redesign/InherentCheckTests.cs
+++ common/ManagementTestShared/Redesign/InherentCheckTests.cs
@@ -3,15 +3,9 @@
 
 using NUnit.Framework;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace Azure.ResourceManager.TestFramework
 {
