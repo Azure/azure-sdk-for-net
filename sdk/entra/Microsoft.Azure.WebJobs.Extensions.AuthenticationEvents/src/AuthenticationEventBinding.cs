14ec594eb0c61aff3ccb76e49e73f3571a60e40a
Abdul Hakim <abdulhakim@microsoft.com>
2023-08-22 14:34:27 -0700

    Replaced tabs with spaces

--- sdk/entra/Microsoft.Azure.WebJobs.Extensions.AuthenticationEvents/src/AuthenticationEventBinding.cs
+++ sdk/entra/Microsoft.Azure.WebJobs.Extensions.AuthenticationEvents/src/AuthenticationEventBinding.cs
@@ -127,8 +127,8 @@ namespace Microsoft.Azure.WebJobs.Extensions.AuthenticationEvents
             {
                 UnauthorizedAccessException => RequestStatusType.TokenInvalid,
                 ValidationException => RequestStatusType.ValidationError,
-				RequestValidationException => RequestStatusType.ValidationError,
-				_ => RequestStatusType.Failed,
+                RequestValidationException => RequestStatusType.ValidationError,
+                _ => RequestStatusType.Failed,
             };
 
             if (eventResponseHandler.Response != null)
