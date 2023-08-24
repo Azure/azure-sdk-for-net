e6409b7274d2f04b7753be1c27cf211068652371
Anne Thompson <annelo@microsoft.com>
2023-08-24 06:17:42 -0700

    Add bit vector

--- sdk/core/Azure.Core/src/Shared/MergePatchChanges.cs
+++ sdk/core/Azure.Core/src/Shared/MergePatchChanges.cs
@@ -9,15 +9,44 @@ namespace Azure.Core.Serialization
 {
     internal readonly struct MergePatchChanges
     {
-        private readonly bool[] _changed;
+        private readonly BitVector _changed;
 
         public MergePatchChanges(int propertyCount)
         {
-            _changed = new bool[propertyCount];
+            _changed = new BitVector(propertyCount);
         }
 
         public void Change(int index) => _changed[index] = true;
 
         public bool HasChanged(int index) => _changed[index];
+
+        private readonly struct BitVector
+        {
+            private readonly ulong[] _bits;
+
+            public BitVector(int size)
+            {
+                _bits = new ulong[(size >> 6) + 1];
+            }
+
+            public bool this[int i]
+            {
+                get
+                {
+                    int index = i >> 6;
+                    int mod = i & 0b111111;
+                    ulong mask = 1ul << mod;
+                    ulong bit = _bits[index] & mask;
+                    return bit == mask;
+                }
+                set
+                {
+                    int index = i >> 6;
+                    int mod = i & 0b111111;
+                    ulong mask = 1ul << mod;
+                    _bits[index] |= mask;
+                }
+            }
+        }
     }
 }
---
+++ sdk/core/Azure.Core/tests/DynamicData/MergePatchChangesTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Text.Json;
+using Azure.Core.Serialization;
+using NUnit.Framework;
+
+namespace Azure.Core.Tests
+{
+    public class MergePatchChangesTests
+    {
+        [Test]
+        public void CanSetChanges()
+        {
+            MergePatchChanges changes = new(100);
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.IsFalse(changes.HasChanged(i), $"{i}");
+
+                changes.Change(i);
+
+                Assert.IsTrue(changes.HasChanged(i), $"{i}");
+            }
+        }
+    }
+}
