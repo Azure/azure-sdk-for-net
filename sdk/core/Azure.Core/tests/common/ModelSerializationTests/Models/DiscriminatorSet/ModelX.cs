1aaa4a230d7920651d45cffc63e34309c99d957a
nisha-bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2023-08-28 14:04:07 -0700

    Update ModelX.cs

--- sdk/core/Azure.Core/tests/common/ModelSerializationTests/Models/DiscriminatorSet/ModelX.cs
+++ sdk/core/Azure.Core/tests/common/ModelSerializationTests/Models/DiscriminatorSet/ModelX.cs
@@ -205,8 +205,7 @@ namespace Azure.Core.Tests.ModelSerializationTests.Models
         {
             ModelSerializerHelper.ValidateFormat(this, options.Format);
 
-            using var writer = new ModelWriter(this, options);
-            return writer.ToBinaryData();
+            return ModelSerializer.SerializeCore(this, options);
         }
     }
 }
