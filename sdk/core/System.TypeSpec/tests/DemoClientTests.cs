fc012665266a33052e7e3558b7a1be0c89c6f323
Krzysztof Cwalina <kcwalina@microsoft.com>
2023-08-29 09:47:35 -0700

    added proof of concept APIs

---
+++ sdk/core/System.TypeSpec/tests/DemoClientTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace System.Tests;
+
+public class DemoClientTests
+{
+    [Fact]
+    public void DemoClientDoesNotExposeAzureTypes()
+    {
+    }
+}
