5cc7e69e71ce54b82b288865be001cb39162286b
azure-sdk <azuresdk@microsoft.com>
2023-08-30 20:12:40 +0000

    Update SDK codes

--- sdk/synapse/Azure.ResourceManager.Synapse/src/Generated/SynapseSqlPoolCollection.cs
+++ sdk/synapse/Azure.ResourceManager.Synapse/src/Generated/SynapseSqlPoolCollection.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
+using Autorest.CSharp.Core;
 using Azure;
 using Azure.Core;
 using Azure.Core.Pipeline;
@@ -227,7 +228,7 @@ namespace Azure.ResourceManager.Synapse
         {
             HttpMessage FirstPageRequest(int? pageSizeHint) => _synapseSqlPoolSqlPoolsRestClient.CreateListByWorkspaceRequest(Id.SubscriptionId, Id.ResourceGroupName, Id.Name);
             HttpMessage NextPageRequest(int? pageSizeHint, string nextLink) => _synapseSqlPoolSqlPoolsRestClient.CreateListByWorkspaceNextPageRequest(nextLink, Id.SubscriptionId, Id.ResourceGroupName, Id.Name);
-            return PageableHelpers.CreateAsyncPageable(FirstPageRequest, NextPageRequest, e => new SynapseSqlPoolResource(Client, SynapseSqlPoolData.DeserializeSynapseSqlPoolData(e)), _synapseSqlPoolSqlPoolsClientDiagnostics, Pipeline, "SynapseSqlPoolCollection.GetAll", "value", "nextLink", cancellationToken);
+            return GeneratorPageableHelpers.CreateAsyncPageable(FirstPageRequest, NextPageRequest, e => new SynapseSqlPoolResource(Client, SynapseSqlPoolData.DeserializeSynapseSqlPoolData(e)), _synapseSqlPoolSqlPoolsClientDiagnostics, Pipeline, "SynapseSqlPoolCollection.GetAll", "value", "nextLink", cancellationToken);
         }
 
         /// <summary>
@@ -249,7 +250,7 @@ namespace Azure.ResourceManager.Synapse
         {
             HttpMessage FirstPageRequest(int? pageSizeHint) => _synapseSqlPoolSqlPoolsRestClient.CreateListByWorkspaceRequest(Id.SubscriptionId, Id.ResourceGroupName, Id.Name);
             HttpMessage NextPageRequest(int? pageSizeHint, string nextLink) => _synapseSqlPoolSqlPoolsRestClient.CreateListByWorkspaceNextPageRequest(nextLink, Id.SubscriptionId, Id.ResourceGroupName, Id.Name);
-            return PageableHelpers.CreatePageable(FirstPageRequest, NextPageRequest, e => new SynapseSqlPoolResource(Client, SynapseSqlPoolData.DeserializeSynapseSqlPoolData(e)), _synapseSqlPoolSqlPoolsClientDiagnostics, Pipeline, "SynapseSqlPoolCollection.GetAll", "value", "nextLink", cancellationToken);
+            return GeneratorPageableHelpers.CreatePageable(FirstPageRequest, NextPageRequest, e => new SynapseSqlPoolResource(Client, SynapseSqlPoolData.DeserializeSynapseSqlPoolData(e)), _synapseSqlPoolSqlPoolsClientDiagnostics, Pipeline, "SynapseSqlPoolCollection.GetAll", "value", "nextLink", cancellationToken);
         }
 
         /// <summary>
