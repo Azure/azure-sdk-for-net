efb661fdb0ae99d98b72ef1c1d134c8c8ad16b9a
Caio Saldanha <camaiaor@microsoft.com>
2023-08-30 15:50:45 -0700

    Implemented cache for v2

---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/tests/Infrastructure/FormRecognizerSetUpFixture.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Azure.AI.FormRecognizer.Tests
+{
+    [SetUpFixture]
+    public class FormRecognizerSetUpFixture
+    {
+        [OneTimeTearDown]
+        public async Task DeleteCachedModelsAsync()
+        {
+            await TrainedModelCache.DeleteModelsAsync();
+        }
+    }
+}
---
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/tests/Infrastructure/TrainedModelCache.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using ServiceVersion = Azure.AI.FormRecognizer.FormRecognizerClientOptions.ServiceVersion;
+
+namespace Azure.AI.FormRecognizer.Tests
+{
+    internal static class TrainedModelCache
+    {
+        public static Dictionary<ModelKey, DisposableTrainedModel> Models { get; } = new();
+
+        public static async Task DeleteModelsAsync()
+        {
+            foreach (var model in Models.Values)
+            {
+                try
+                {
+                    await model.DeleteModelAsync();
+                }
+                catch
+                {
+                    // ignore
+                }
+            }
+
+            Models.Clear();
+        }
+
+        public readonly struct ModelKey
+        {
+            public ModelKey(ServiceVersion serviceVersion, string containerType, bool useTrainingLabels, string modelName)
+            {
+                ServiceVersion = serviceVersion;
+                ContainerType = containerType;
+                UseTrainingLabels = useTrainingLabels;
+                ModelName = modelName;
+            }
+
+            public ServiceVersion ServiceVersion { get; }
+
+            public string ContainerType { get; }
+
+            public bool UseTrainingLabels { get; }
+
+            public string ModelName { get; }
+        }
+    }
+}
