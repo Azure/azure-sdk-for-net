665a95b4e07708b74c0168e62fe13f1111bfeebf
Patrick Hallisey <pahallis@microsoft.com>
2023-08-31 11:48:47 -0700

    Add command logging to TypeSpec-Project-Generate.ps1

---
+++ eng/common/scripts/Helpers/CommandInvocation-Helpers.ps1
@@ -0,0 +1,51 @@
+function Invoke-LoggedCommand($command, $executePath)
+{
+    $pipelineBuild = !!$env:TF_BUILD
+    $startTime = Get-Date
+
+    if($pipelineBuild) {
+        Write-Host "##[group]> $command"
+    } else {
+        Write-Host "> $command"
+    }
+
+    if($executePath) {
+      Push-Location $executePath
+    }
+
+    try {
+      if ($IsLinux -or $IsMacOs)
+      {
+          sh -c "$command 2>&1"
+      }
+      else
+      {
+          cmd /c "$command 2>&1"
+      }
+
+      $duration = (Get-Date) - $startTime
+
+      if($pipelineBuild) {
+        Write-Host "##[endgroup]"
+      }
+
+      if($LastExitCode -ne 0)
+      {
+          if($pipelineBuild) {
+              Write-Error "##[error]Command failed to execute ($duration): $command`n"
+          } else {
+              Write-Error "Command failed to execute ($duration): $command`n"
+          }
+      }
+      else {
+          Write-Host "Command succeeded ($duration)`n"
+      }
+    }
+    finally {
+      if($executePath) {
+        Pop-Location
+      }
+    }
+}
+
+Set-Alias -Name invoke -Value Invoke-LoggedCommand
---
+++ eng/common/scripts/Invoke-NpmInstall.ps1
@@ -0,0 +1,74 @@
+[CmdLetBinding()]
+param
+(
+  [string]$WorkingDirectory
+)
+
+. $PSScriptRoot/Helpers/PSModule-Helpers.ps1
+. $PSScriptRoot/Helpers/CommandInvocation-Helpers.ps1
+. $PSScriptRoot/common.ps1
+
+Push-Location $WorkingDirectory
+try {
+    $currentDur = Resolve-Path "."
+    Write-Host "Generating from $currentDur"
+
+    if (Test-Path "package.json") {
+        Remove-Item -Path "package.json" -Force
+    }
+
+    if (Test-Path ".npmrc") {
+        Remove-Item -Path ".npmrc" -Force
+    }
+
+    if (Test-Path "node_modules") {
+        Remove-Item -Path "node_modules" -Force -Recurse
+    }
+
+    if (Test-Path "package-lock.json") {
+        Remove-Item -Path "package-lock.json" -Force
+    }
+
+    #default to root/eng/emitter-package.json but you can override by writing
+    #Get-${Language}-EmitterPackageJsonPath in your Language-Settings.ps1
+    $replacementPackageJson = Join-Path $PSScriptRoot "../../emitter-package.json"
+    if (Test-Path "Function:$GetEmitterPackageJsonPathFn") {
+        $replacementPackageJson = &$GetEmitterPackageJsonPathFn
+    }
+
+    Write-Host("Copying package.json from $replacementPackageJson")
+    Copy-Item -Path $replacementPackageJson -Destination "package.json" -Force
+
+    #default to root/eng/emitter-package-lock.json but you can override by writing
+    #Get-${Language}-EmitterPackageLockPath in your Language-Settings.ps1
+    $emitterPackageLock = Join-Path $PSScriptRoot "../../emitter-package-lock.json"
+    if (Test-Path "Function:$GetEmitterPackageLockPathFn") {
+        $emitterPackageLock = &$GetEmitterPackageLockPathFn
+    }
+
+    $usingLockFile = Test-Path $emitterPackageLock
+
+    if ($usingLockFile) {
+        Write-Host("Copying package-lock.json from $emitterPackageLock")
+        Copy-Item -Path $emitterPackageLock -Destination "package-lock.json" -Force
+    }
+
+    $useAlphaNpmRegistry = (Get-Content $replacementPackageJson -Raw).Contains("-alpha.")
+
+    if($useAlphaNpmRegistry) {
+        Write-Host "Package.json contains '-alpha.' in the version, Creating .npmrc using public/azure-sdk-for-js-test-autorest feed."
+        "registry=https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-js-test-autorest@local/npm/registry/ `n`nalways-auth=true" | Out-File '.npmrc'
+    }
+
+    if ($usingLockFile) {
+        Invoke-LoggedCommand "npm ci"
+    }
+    else {
+        Invoke-LoggedCommand "npm install"
+    }
+
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+}
+finally {
+    Pop-Location
+}
