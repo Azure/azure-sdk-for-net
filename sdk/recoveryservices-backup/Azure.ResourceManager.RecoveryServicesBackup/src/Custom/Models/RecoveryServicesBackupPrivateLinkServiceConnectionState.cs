9d30d7b3c3518c020c817ba30ea5eb0e739b90c8
Ankur Singh <ankur@microsoft.com>
2023-09-05 15:19:53 +0530

    Added Custom Files for breaking change

---
+++ sdk/recoveryservices-backup/Azure.ResourceManager.RecoveryServicesBackup/src/Custom/Models/RecoveryServicesBackupPrivateLinkServiceConnectionState.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+namespace Azure.ResourceManager.RecoveryServicesBackup.Models
+{
+    /// <summary> Private Link Service Connection State. </summary>
+    public partial class RecoveryServicesBackupPrivateLinkServiceConnectionState
+    {
+        /// <summary>
+        /// ActionRequired
+        /// </summary>
+        public string ActionRequired
+        {
+            get => ActionsRequired;
+            set
+            {
+                ActionRequired = value;
+            }
+        }
+    }
+}
