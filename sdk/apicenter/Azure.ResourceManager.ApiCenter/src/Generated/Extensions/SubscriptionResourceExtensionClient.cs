89b6c97479878b3ba048a28eacc2acb9bddd886e
Alexander Zaslonov <alzaslon@microsoft.com>
2023-09-06 17:19:40 -0700

    Regenerated code.

--- sdk/apicenter/Azure.ResourceManager.ApiCenter/src/Generated/Extensions/SubscriptionResourceExtensionClient.cs
+++ sdk/apicenter/Azure.ResourceManager.ApiCenter/src/Generated/Extensions/SubscriptionResourceExtensionClient.cs
@@ -6,6 +6,7 @@
 #nullable disable
 
 using System.Threading;
+using Autorest.CSharp.Core;
 using Azure;
 using Azure.Core;
 using Azure.Core.Pipeline;
@@ -59,7 +60,7 @@ namespace Azure.ResourceManager.ApiCenter
         {
             HttpMessage FirstPageRequest(int? pageSizeHint) => ServiceRestClient.CreateListBySubscriptionRequest(Id.SubscriptionId);
             HttpMessage NextPageRequest(int? pageSizeHint, string nextLink) => ServiceRestClient.CreateListBySubscriptionNextPageRequest(nextLink, Id.SubscriptionId);
-            return PageableHelpers.CreateAsyncPageable(FirstPageRequest, NextPageRequest, e => new ServiceResource(Client, ServiceData.DeserializeServiceData(e)), ServiceClientDiagnostics, Pipeline, "SubscriptionResourceExtensionClient.GetServices", "value", "nextLink", cancellationToken);
+            return GeneratorPageableHelpers.CreateAsyncPageable(FirstPageRequest, NextPageRequest, e => new ServiceResource(Client, ServiceData.DeserializeServiceData(e)), ServiceClientDiagnostics, Pipeline, "SubscriptionResourceExtensionClient.GetServices", "value", "nextLink", cancellationToken);
         }
 
         /// <summary>
@@ -81,7 +82,7 @@ namespace Azure.ResourceManager.ApiCenter
         {
             HttpMessage FirstPageRequest(int? pageSizeHint) => ServiceRestClient.CreateListBySubscriptionRequest(Id.SubscriptionId);
             HttpMessage NextPageRequest(int? pageSizeHint, string nextLink) => ServiceRestClient.CreateListBySubscriptionNextPageRequest(nextLink, Id.SubscriptionId);
-            return PageableHelpers.CreatePageable(FirstPageRequest, NextPageRequest, e => new ServiceResource(Client, ServiceData.DeserializeServiceData(e)), ServiceClientDiagnostics, Pipeline, "SubscriptionResourceExtensionClient.GetServices", "value", "nextLink", cancellationToken);
+            return GeneratorPageableHelpers.CreatePageable(FirstPageRequest, NextPageRequest, e => new ServiceResource(Client, ServiceData.DeserializeServiceData(e)), ServiceClientDiagnostics, Pipeline, "SubscriptionResourceExtensionClient.GetServices", "value", "nextLink", cancellationToken);
         }
     }
 }
