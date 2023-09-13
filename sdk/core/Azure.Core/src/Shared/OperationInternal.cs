9a7fc8014a6f0d9bdc79a65471ef078d63614321
Anne Thompson <annelo@microsoft.com>
2023-09-13 16:10:03 -0700

    updates

--- sdk/core/Azure.Core/src/Shared/OperationInternal.cs
+++ sdk/core/Azure.Core/src/Shared/OperationInternal.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core.Shared.Asserts;
+using Azure.Core.Shared.Delay;
 using Azure.Core.Shared.Pipeline.Diagnostics;
 
 #nullable enable
--- sdk/core/Azure.Core/src/Shared/OperationInternalOfT.cs
+++ sdk/core/Azure.Core/src/Shared/OperationInternalOfT.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core.Shared.Asserts;
+using Azure.Core.Shared.Delay;
 using Azure.Core.Shared.Pipeline.Diagnostics;
 using Azure.Core.Shared.Pipeline.Tasks;
 
