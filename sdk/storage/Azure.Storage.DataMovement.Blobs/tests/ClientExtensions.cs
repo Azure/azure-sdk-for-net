c1a1d577cd9e52ac6fea44320921f475f9604d37
Jocelyn Schreppler <jaschrep@microsoft.com>
2023-09-13 10:45:48 -0400

    license header

--- sdk/storage/Azure.Storage.DataMovement.Blobs/tests/ClientExtensions.cs
+++ sdk/storage/Azure.Storage.DataMovement.Blobs/tests/ClientExtensions.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Specialized;
 
