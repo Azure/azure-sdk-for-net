244720a0b9627afd930554f94ea5914e5d4696d8
Patrick Hallisey <pahallis@microsoft.com>
2023-09-13 20:34:12 -0700

    Revert unnecessary eng/common changes

--- eng/common/scripts/TypeSpec-Project-Generate.ps1
+++ eng/common/scripts/TypeSpec-Project-Generate.ps1
@@ -6,8 +6,7 @@ param (
     [ValidateNotNullOrEmpty()]
     [string] $ProjectDirectory,
     [string] $TypespecAdditionalOptions = $null, ## addtional typespec emitter options, separated by semicolon if more than one, e.g. option1=value1;option2=value2
-    [switch] $SaveInputs = $false, ## saves the temporary files during execution, default false
-    [switch] $SkipNpmInstall = $false ## skips npm install, default false
+    [switch] $SaveInputs = $false ## saves the temporary files during execution, default false
 )
 
 $ErrorActionPreference = "Stop"
@@ -15,6 +14,65 @@ $ErrorActionPreference = "Stop"
 . $PSScriptRoot/Helpers/CommandInvocation-Helpers.ps1
 . $PSScriptRoot/common.ps1
 Install-ModuleIfNotInstalled "powershell-yaml" "0.4.1" | Import-Module
+
+function NpmInstallForProject([string]$workingDirectory) {
+    Push-Location $workingDirectory
+    try {
+        $currentDur = Resolve-Path "."
+        Write-Host "Generating from $currentDur"
+
+        if (Test-Path "package.json") {
+            Write-Host "Removing existing package.json"
+            Remove-Item -Path "package.json" -Force
+        }
+
+        if (Test-Path ".npmrc") {
+            Write-Host "Removing existing .nprc"
+            Remove-Item -Path ".npmrc" -Force
+        }
+
+        if (Test-Path "node_modules") {
+            Write-Host "Removing existing node_modules"
+            Remove-Item -Path "node_modules" -Force -Recurse
+        }
+
+        if (Test-Path "package-lock.json") {
+            Write-Host "Removing existing package-lock.json"
+            Remove-Item -Path "package-lock.json" -Force
+        }
+
+        $replacementPackageJson = Join-Path $PSScriptRoot "../../emitter-package.json"
+
+        Write-Host("Copying package.json from $replacementPackageJson")
+        Copy-Item -Path $replacementPackageJson -Destination "package.json" -Force
+        $emitterPackageLock = Join-Path $PSScriptRoot "../../emitter-package-lock.json"
+        $usingLockFile = Test-Path $emitterPackageLock
+
+        if ($usingLockFile) {
+            Write-Host("Copying package-lock.json from $emitterPackageLock")
+            Copy-Item -Path $emitterPackageLock -Destination "package-lock.json" -Force
+        }
+
+        $useAlphaNpmRegistry = (Get-Content $replacementPackageJson -Raw).Contains("-alpha.")
+
+        if($useAlphaNpmRegistry) {
+            Write-Host "Package.json contains '-alpha.' in the version, Creating .npmrc using public/azure-sdk-for-js-test-autorest feed."
+            "registry=https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-js-test-autorest@local/npm/registry/ `n`nalways-auth=true" | Out-File '.npmrc'
+        }
+
+        if ($usingLockFile) {
+            Invoke-LoggedCommand "npm ci"
+        }
+        else {
+            Invoke-LoggedCommand "npm install"
+        }
+
+        if ($LASTEXITCODE) { exit $LASTEXITCODE }
+    }
+    finally {
+        Pop-Location
+    }
+}
 
 $resolvedProjectDirectory = Resolve-Path $ProjectDirectory
 $emitterName = &$GetEmitterNameFn
@@ -30,20 +88,19 @@ $tempFolder = "$ProjectDirectory/TempTypeSpecFiles"
 $npmWorkingDir = Resolve-Path $tempFolder/$innerFolder
 $mainTypeSpecFile = If (Test-Path "$npmWorkingDir/client.*") { Resolve-Path "$npmWorkingDir/client.*" } Else { Resolve-Path "$npmWorkingDir/main.*"}
 
-if (!$SkipNpmInstall) {
-  . $PSScriptRoot/Invoke-EmitterNpmInstall.ps1
-  if ($LASTEXITCODE) { exit $LASTEXITCODE }
-}
-
-Push-Location $npmWorkingDir
 try {
+    Push-Location $npmWorkingDir
+    NpmInstallForProject $npmWorkingDir
+
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
+
     if (Test-Path "Function:$GetEmitterAdditionalOptionsFn") {
         $emitterAdditionalOptions = &$GetEmitterAdditionalOptionsFn $resolvedProjectDirectory
         if ($emitterAdditionalOptions.Length -gt 0) {
             $emitterAdditionalOptions = " $emitterAdditionalOptions"
         }
     }
-    $typespecCompileCommand = "npx --no tsp compile $mainTypeSpecFile --emit $emitterName$emitterAdditionalOptions"
+    $typespecCompileCommand = "npx tsp compile $mainTypeSpecFile --emit $emitterName$emitterAdditionalOptions"
     if ($TypespecAdditionalOptions) {
         $options = $TypespecAdditionalOptions.Split(";");
         foreach ($option in $options) {
@@ -55,7 +112,8 @@ try {
         $typespecCompileCommand += " --option $emitterName.save-inputs=true"
     }
 
-    Invoke-LoggedCommand $typespecCompileCommand
+    Write-Host($typespecCompileCommand)
+    Invoke-Expression $typespecCompileCommand
 
     if ($LASTEXITCODE) { exit $LASTEXITCODE }
 }
