474eb6150304f6fab013c9d601a05e45d9c3644f
Jocelyn Schreppler <jaschrep@microsoft.com>
2023-09-14 12:53:26 -0400

    StorageResource types added

---
+++ sdk/storage/Azure.Storage.DataMovement.Files.Shares/src/ShareDirectoryStorageResourceContainer.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Azure.Storage.Files.Shares;
+
+namespace Azure.Storage.DataMovement.Files.Shares
+{
+    internal class ShareDirectoryStorageResourceContainer : StorageResourceContainer
+    {
+        internal ShareDirectoryClient ShareDirectoryClient { get; }
+
+        public override Uri Uri => ShareDirectoryClient.Uri;
+
+        protected override StorageResourceItem GetStorageResourceReference(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IAsyncEnumerable<StorageResource> GetStorageResourcesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ sdk/storage/Azure.Storage.DataMovement.Files.Shares/src/ShareStorageResourceContainer.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Azure.Storage.Files.Shares;
+
+namespace Azure.Storage.DataMovement.Files.Shares
+{
+    internal class ShareStorageResourceContainer : StorageResourceContainer
+    {
+        internal ShareClient ShareClient { get; }
+
+        public override Uri Uri => ShareClient.Uri;
+
+        protected override StorageResourceItem GetStorageResourceReference(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IAsyncEnumerable<StorageResource> GetStorageResourcesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
