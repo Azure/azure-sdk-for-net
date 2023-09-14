bcf0214b87876ba5b020d1e059cb572012dffcbe
Jocelyn Schreppler <jaschrep@microsoft.com>
2023-09-14 12:48:23 -0400

    provider

---
+++ sdk/storage/Azure.Storage.DataMovement.Files.Shares/src/ShareFilesStorageResourceProvider.cs
@@ -0,0 +1,368 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Storage.Files.Shares;
+
+namespace Azure.Storage.DataMovement.Blobs
+{
+    /// <summary>
+    /// Provider for a <see cref="StorageResource"/> configured for an Azure Blob Storage resource.
+    /// </summary>
+    public class ShareFilesStorageResourceProvider : StorageResourceProvider
+    {
+        /// <summary>
+        /// Delegate for fetching a shared key credential for a given URI.
+        /// </summary>
+        /// <param name="uri">
+        /// URI of resource to fetch credential for.
+        /// </param>
+        /// <param name="readOnly">
+        /// Whether the permission can be read-only.
+        /// </param>
+        public delegate StorageSharedKeyCredential GetStorageSharedKeyCredential(string uri, bool readOnly);
+
+        /// <summary>
+        /// Delegate for fetching a token credential for a given URI.
+        /// </summary>
+        /// <param name="uri">
+        /// URI of resource to fetch credential for.
+        /// </param>
+        /// <param name="readOnly">
+        /// Whether the permission can be read-only.
+        /// </param>
+        public delegate TokenCredential GetTokenCredential(string uri, bool readOnly);
+
+        /// <summary>
+        /// Delegate for fetching a SAS credential for a given URI.
+        /// </summary>
+        /// <param name="uri">
+        /// URI of resource to fetch credential for.
+        /// </param>
+        /// <param name="readOnly">
+        /// Whether the permission can be read-only.
+        /// </param>
+        public delegate AzureSasCredential GetAzureSasCredential(string uri, bool readOnly);
+
+        private enum ResourceType
+        {
+            Unknown = 0,
+            Share = 1,
+            Directory = 2,
+            File = 3,
+        }
+
+        private enum CredentialType
+        {
+            None = 0,
+            SharedKey = 1,
+            Token = 2,
+            Sas = 4
+        }
+
+        /// <inheritdoc/>
+        protected override string TypeId => "blob";
+
+        private readonly CredentialType _credentialType;
+        private readonly GetStorageSharedKeyCredential _getStorageSharedKeyCredential;
+        private readonly GetTokenCredential _getTokenCredential;
+        private readonly GetAzureSasCredential _getAzureSasCredential;
+
+        /// <summary>
+        /// Default constrctor.
+        /// </summary>
+        public ShareFilesStorageResourceProvider()
+        {
+            _credentialType = CredentialType.None;
+        }
+
+        /// <summary>
+        /// <para>
+        /// Constructs this provider to use the given credential when making a new Blob Storage
+        /// <see cref="StorageResource"/>.
+        /// </para>
+        /// <para>
+        /// This instance will use the given <see cref="StorageSharedKeyCredential"/> when constructing the underlying
+        /// Azure.Storage.Blobs client, e.g. <see cref="ShareFileClient(Uri, StorageSharedKeyCredential, ShareClientOptions)"/>.
+        /// The credential will only be used when the provider needs to construct a client in the first place. It will
+        /// not be used when creating a <see cref="StorageResource"/> from a pre-existing client, e.g.
+        /// <see cref="FromClient(ShareFileClient, ShareClientOptions)"/>.
+        /// </para>
+        /// </summary>
+        /// <param name="credential">
+        /// Shared key credential to use when constructing resources.
+        /// </param>
+        public ShareFilesStorageResourceProvider(StorageSharedKeyCredential credential)
+        {
+            _credentialType = CredentialType.SharedKey;
+            _getStorageSharedKeyCredential = (_, _) => credential;
+        }
+
+        /// <summary>
+        /// <para>
+        /// Constructs this provider to use the given credential when making a new Blob Storage
+        /// <see cref="StorageResource"/>.
+        /// </para>
+        /// <para>
+        /// This instance will use the given <see cref="TokenCredential"/> when constructing the underlying
+        /// Azure.Storage.Blobs client, e.g. <see cref="BlockBlobClient(Uri, TokenCredential, BlobClientOptions)"/>.
+        /// The credential will only be used when the provider needs to construct a client in the first place. It will
+        /// not be used when creating a <see cref="StorageResource"/> from a pre-existing client, e.g.
+        /// <see cref="FromClient(BlockBlobClient, BlockBlobStorageResourceOptions)"/>.
+        /// </para>
+        /// </summary>
+        /// <param name="credential">
+        /// Token credential to use when constructing resources.
+        /// </param>
+        public ShareFilesStorageResourceProvider(TokenCredential credential)
+        {
+            _credentialType = CredentialType.Token;
+            _getTokenCredential = (_, _) => credential;
+        }
+
+        /// <summary>
+        /// <para>
+        /// Constructs this provider to use the given credential when making a new Blob Storage
+        /// <see cref="StorageResource"/>.
+        /// </para>
+        /// <para>
+        /// This instance will use the given <see cref="AzureSasCredential"/> when constructing the underlying
+        /// Azure.Storage.Blobs client, e.g. <see cref="BlockBlobClient(Uri, AzureSasCredential, BlobClientOptions)"/>.
+        /// The credential will only be used when the provider needs to construct a client in the first place. It will
+        /// not be used when creating a <see cref="StorageResource"/> from a pre-existing client, e.g.
+        /// <see cref="FromClient(BlockBlobClient, BlockBlobStorageResourceOptions)"/>.
+        /// Additionally, if the given target blob resource already has a SAS token in the URI, that token will be
+        /// preferred over this credential.
+        /// </para>
+        /// </summary>
+        /// <param name="credential">
+        /// SAS credential to use when constructing resources.
+        /// </param>
+        public ShareFilesStorageResourceProvider(AzureSasCredential credential)
+        {
+            _credentialType = CredentialType.Sas;
+            _getAzureSasCredential = (_, _) => credential;
+        }
+
+        /// <summary>
+        /// <para>
+        /// Constructs this provider to use the given delegate for acquiring a credential when making a new Blob
+        /// Storage <see cref="StorageResource"/>.
+        /// </para>
+        /// <para>
+        /// This instance will use the given <see cref="GetStorageSharedKeyCredential"/> to fetch a credential
+        /// when constructing the underlying Azure.Storage.Blobs client, e.g.
+        /// <see cref="BlockBlobClient(Uri, StorageSharedKeyCredential, BlobClientOptions)"/>.
+        /// The delegate will only be used when the provider needs to construct a client in the first place. It will
+        /// not be used when creating a <see cref="StorageResource"/> from a pre-existing client, e.g.
+        /// <see cref="FromClient(BlockBlobClient, BlockBlobStorageResourceOptions)"/>.
+        /// </para>
+        /// </summary>
+        /// <param name="getStorageSharedKeyCredentialAsync">
+        /// Delegate for acquiring a credential.
+        /// </param>
+        public ShareFilesStorageResourceProvider(GetStorageSharedKeyCredential getStorageSharedKeyCredentialAsync)
+        {
+            _credentialType = CredentialType.SharedKey;
+            _getStorageSharedKeyCredential = getStorageSharedKeyCredentialAsync;
+        }
+
+        /// <summary>
+        /// <para>
+        /// Constructs this provider to use the given delegate for acquiring a credential when making a new Blob
+        /// Storage <see cref="StorageResource"/>.
+        /// </para>
+        /// <para>
+        /// This instance will use the given <see cref="GetTokenCredential"/> to fetch a credential
+        /// when constructing the underlying Azure.Storage.Blobs client, e.g.
+        /// <see cref="BlockBlobClient(Uri, TokenCredential, BlobClientOptions)"/>.
+        /// The delegate will only be used when the provider needs to construct a client in the first place. It will
+        /// not be used when creating a <see cref="StorageResource"/> from a pre-existing client, e.g.
+        /// <see cref="FromClient(BlockBlobClient, BlockBlobStorageResourceOptions)"/>.
+        /// </para>
+        /// </summary>
+        /// <param name="getTokenCredentialAsync">
+        /// Delegate for acquiring a credential.
+        /// </param>
+        public ShareFilesStorageResourceProvider(GetTokenCredential getTokenCredentialAsync)
+        {
+            _credentialType = CredentialType.SharedKey;
+            _getTokenCredential = getTokenCredentialAsync;
+        }
+
+        /// <summary>
+        /// <para>
+        /// Constructs this provider to use the given delegate for acquiring a credential when making a new Blob
+        /// Storage <see cref="StorageResource"/>.
+        /// </para>
+        /// <para>
+        /// This instance will use the given <see cref="GetAzureSasCredential"/> to fetch a credential
+        /// when constructing the underlying Azure.Storage.Blobs client, e.g.
+        /// <see cref="BlockBlobClient(Uri, AzureSasCredential, BlobClientOptions)"/>.
+        /// The delegate will only be used when the provider needs to construct a client in the first place. It will
+        /// not be used when creating a <see cref="StorageResource"/> from a pre-existing client, e.g.
+        /// <see cref="FromClient(BlockBlobClient, BlockBlobStorageResourceOptions)"/>.
+        /// Additionally, if the given target blob resource already has a SAS token in the URI, that token will be
+        /// preferred over this delegate.
+        /// </para>
+        /// </summary>
+        /// <param name="getAzureSasCredentialAsync">
+        /// Delegate for acquiring a credential.
+        /// </param>
+        public ShareFilesStorageResourceProvider(GetAzureSasCredential getAzureSasCredentialAsync)
+        {
+            _credentialType = CredentialType.SharedKey;
+            _getAzureSasCredential = getAzureSasCredentialAsync;
+        }
+
+        #region Abstract Class Implementation
+        /// <inheritdoc/>
+        protected override async Task<StorageResource> FromSourceAsync(DataTransferProperties properties, CancellationToken cancellationToken)
+            => await FromTransferPropertiesAsync(properties, getSource: true, cancellationToken).ConfigureAwait(false);
+
+        /// <inheritdoc/>
+        protected override async Task<StorageResource> FromDestinationAsync(DataTransferProperties properties, CancellationToken cancellationToken)
+            => await FromTransferPropertiesAsync(properties, getSource: false, cancellationToken).ConfigureAwait(false);
+
+        private async Task<StorageResource> FromTransferPropertiesAsync(
+            DataTransferProperties properties,
+            bool getSource,
+            CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// For use in testing. Internal wrapper for protected member
+        /// <see cref="StorageResourceProvider.FromSourceAsync(DataTransferProperties, CancellationToken)"/>.
+        /// </summary>
+        internal async Task<StorageResource> FromSourceInternalHookAsync(
+            DataTransferProperties props,
+            CancellationToken cancellationToken = default)
+            => await FromSourceAsync(props, cancellationToken).ConfigureAwait(false);
+
+        /// <summary>
+        /// For use in testing. Internal wrapper for protected member
+        /// <see cref="StorageResourceProvider.FromDestinationAsync(DataTransferProperties, CancellationToken)"/>.
+        /// </summary>
+        internal async Task<StorageResource> FromDestinationInternalHookAsync(
+            DataTransferProperties props,
+            CancellationToken cancellationToken = default)
+            => await FromDestinationAsync(props, cancellationToken).ConfigureAwait(false);
+        #endregion
+
+        #region From Uri
+        /// <summary>
+        /// Creates a storage resource pointing towards the given container URI.
+        /// </summary>
+        /// <param name="shareUri">
+        /// Target location.
+        /// </param>
+        /// <param name="options">
+        /// Options for creating the storage resource.
+        /// </param>
+        /// <returns>
+        /// The configured storage resource.
+        /// </returns>
+        public StorageResource FromShare(string shareUri, ShareStorageResourceContainerOptions options = default)
+        {
+            ShareClient client = _credentialType switch
+            {
+                CredentialType.None => new ShareClient(new Uri(shareUri)),
+                CredentialType.SharedKey => new ShareClient(new Uri(shareUri), _getStorageSharedKeyCredential(shareUri, false)),
+                CredentialType.Token => new ShareClient(new Uri(shareUri), _getTokenCredential(shareUri, false)),
+                CredentialType.Sas => new ShareClient(new Uri(shareUri), _getAzureSasCredential(shareUri, false)),
+                _ => throw BadCredentialTypeException(_credentialType),
+            };
+            return new ShareStorageResourceContainer(client, options);
+        }
+
+        /// <summary>
+        /// Creates a storage resource pointing towards the given container URI.
+        /// </summary>
+        /// <param name="directoryUri">
+        /// Target location.
+        /// </param>
+        /// <param name="options">
+        /// Options for creating the storage resource.
+        /// </param>
+        /// <returns>
+        /// The configured storage resource.
+        /// </returns>
+        public StorageResource FromPath(string directoryUri, ShareStorageResourceOptions options = default)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region From Client
+        /// <summary>
+        /// Creates a storage resource pointing towards the given Azure SDK client.
+        /// </summary>
+        /// <param name="client">
+        /// Target resource presented within an Azure SDK client.
+        /// </param>
+        /// <param name="options">
+        /// Options for creating the storage resource.
+        /// </param>
+        /// <returns>
+        /// The configured storage resource.
+        /// </returns>
+        public StorageResource FromClient(
+            ShareClient client,
+            ShareStorageResourceContainerOptions options = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a storage resource pointing towards the given Azure SDK client.
+        /// </summary>
+        /// <param name="client">
+        /// Target resource presented within an Azure SDK client.
+        /// </param>
+        /// <param name="options">
+        /// Options for creating the storage resource.
+        /// </param>
+        /// <returns>
+        /// The configured storage resource.
+        /// </returns>
+        public StorageResource FromClient(
+            ShareDirectoryClient client,
+            ShareStorageResourceContainerOptions options = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a storage resource pointing towards the given Azure SDK client.
+        /// </summary>
+        /// <param name="client">
+        /// Target resource presented within an Azure SDK client.
+        /// </param>
+        /// <param name="options">
+        /// Options for creating the storage resource.
+        /// </param>
+        /// <returns>
+        /// The configured storage resource.
+        /// </returns>
+        public StorageResource FromClient(
+            ShareFileClient client,
+            ShareStorageResourceOptions options = default)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        private static ArgumentException BadResourceTypeException(ResourceType resourceType)
+            => new ArgumentException(
+                $"No support for resource type {Enum.GetName(typeof(ResourceType), resourceType)}.");
+
+        private static ArgumentException BadCredentialTypeException(CredentialType credentialType)
+            => new ArgumentException(
+                $"No support for credential type {Enum.GetName(typeof(CredentialType), credentialType)}.");
+    }
+}
