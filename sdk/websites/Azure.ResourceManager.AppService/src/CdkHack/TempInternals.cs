46862104c8d19a48bf82ded1c7ca4e4a5eb89373
m-nash <prognash@gmail.com>
2023-09-13 20:23:15 -0700

    update code to not add new types

---
+++ sdk/websites/Azure.ResourceManager.AppService/src/CdkHack/TempInternals.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+//This file is needed because we had to turn off the post processor which makes a bunch of types public.
+
+using Azure.Core;
+
+[assembly: CodeGenSuppressType("ArmIdWrapper")]
+[assembly: CodeGenSuppressType("AppServiceGithubToken")]
+[assembly: CodeGenSuppressType("AppServiceResource")]
+[assembly: CodeGenSuppressType("AppserviceGithubTokenRequest")]
+[assembly: CodeGenSuppressType("CsmMoveResourceEnvelope")]
+[assembly: CodeGenSuppressType("DefaultErrorResponse")]
+[assembly: CodeGenSuppressType("DefaultErrorResponseError")]
+[assembly: CodeGenSuppressType("DefaultErrorResponseErrorDetailsItem")]
+[assembly: CodeGenSuppressType("ExtendedLocation")]
+[assembly: CodeGenSuppressType("ManagedServiceIdentity")]
+[assembly: CodeGenSuppressType("ManagedServiceIdentityTypeExtensions")]
+[assembly: CodeGenSuppressType("ProxyOnlyResource")]
+[assembly: CodeGenSuppressType("StaticSiteUserProvidedFunctionAppProperties")]
+[assembly: CodeGenSuppressType("UserAssignedIdentity")]
+
+namespace Azure.ResourceManager.AppService.CdkHack
+{
+#pragma warning disable SA1402 // File may only contain a single type
+    internal partial class AllowedAudiencesValidation { }
+    internal partial class ApiManagementConfig { }
+    internal partial class AppServiceApiDefinitionInfo { }
+    internal partial class AppServiceBlobStorageTokenStore { }
+    internal partial class AppServiceHttpSettingsRoutes { }
+    internal partial class AppServiceStaticWebAppsRegistration { }
+    internal partial class CsmOperationDescriptionProperties { }
+    internal partial class DetectorMetadata { }
+    internal partial class FileSystemTokenStore { }
+    internal partial class FrontEndConfiguration { }
+    internal partial class LoginRoutes { }
+    internal partial class LoginScopes { }
+    internal partial class PrivateLinkResourcesWrapper { }
+    internal partial class RoutingRuleExperiments { }
+    internal partial class WebAppEnabledConfig { }
+#pragma warning restore SA1402 // File may only contain a single type
+}
