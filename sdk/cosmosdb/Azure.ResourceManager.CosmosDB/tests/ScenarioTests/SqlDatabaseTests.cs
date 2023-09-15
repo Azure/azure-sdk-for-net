eb6f32f2797f1e85322f91398c8889df425ec443
Achint Agrawal <acagrawal@microsoft.com>
2023-09-15 15:30:32 +0530

    added passed null for newly added parameters in ThroughputSettingsResourceInfo

--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/tests/ScenarioTests/SqlDatabaseTests.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/tests/ScenarioTests/SqlDatabaseTests.cs
@@ -112,7 +112,7 @@ namespace Azure.ResourceManager.CosmosDB.Tests
             Assert.AreEqual(TestThroughput1, throughput.Data.Resource.Throughput);
 
             CosmosDBSqlDatabaseThroughputSettingResource throughput2 = (await throughput.CreateOrUpdateAsync(WaitUntil.Completed, new ThroughputSettingsUpdateData(AzureLocation.WestUS,
-                new ThroughputSettingsResourceInfo(TestThroughput2, null, null, null)))).Value;
+                new ThroughputSettingsResourceInfo(TestThroughput2, null, null, null, null, null)))).Value;
 
             Assert.AreEqual(TestThroughput2, throughput2.Data.Resource.Throughput);
         }
