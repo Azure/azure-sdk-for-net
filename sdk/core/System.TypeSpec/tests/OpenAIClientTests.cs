b4b567a98049c254df9d8c5b08d35f639dc6eb2d
Anne Thompson <annelo@microsoft.com>
2023-09-15 16:45:50 -0700

    tinker

--- sdk/core/System.TypeSpec/tests/OpenAIClientTests.cs
+++ sdk/core/System.TypeSpec/tests/OpenAIClientTests.cs
@@ -14,11 +14,14 @@ public class OpenAIClientTests
     public void ClientDoesNotExposeAzureTypes()
     {
         string key = Environment.GetEnvironmentVariable("OPENAI_KEY");
+
         KeyCredential credential = new KeyCredential(key);
         OpenAIClient client = new OpenAIClient(credential);
 
         Result<Completions> result = client.GetCompletions("tell me something about life.");
         Choice choice = result.Value.Choices[0];
         Debug.WriteLine(choice.Text);
+
+        Result httpResult = result.GetRawResult();
     }
 }
