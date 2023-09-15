375ab8511933d6649fd0ea11e34c1fb1311bbd21
Arthur Ma <arthurma@microsoft.com>
2023-09-15 21:08:34 +0800

    update

--- sdk/resourceconnector/Azure.ResourceManager.ResourceConnector/tests/Scenario/ApplianceOperationTests.cs
+++ sdk/resourceconnector/Azure.ResourceManager.ResourceConnector/tests/Scenario/ApplianceOperationTests.cs
@@ -38,7 +38,6 @@ namespace Azure.ResourceManager.ResourceConnector.Tests
         }
 
         [Test]
-        [Ignore("New version hasn't been deployed.")]
         public async Task TestOperationsAppliances()
         {
             await SetCollectionsAsync();
