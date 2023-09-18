78bd3ea3dfb4175188c6783579df9da8ee6462b5
Krzysztof Cwalina <kcwalina@microsoft.com>
2023-09-18 11:40:26 -0700

    simplified

--- sdk/core/System.TypeSpec/src/PipelineOptions.cs
+++ sdk/core/System.TypeSpec/src/PipelineOptions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
 using System.Threading;
 
 namespace System.ServiceModel.Rest;
@@ -11,17 +10,6 @@ namespace System.ServiceModel.Rest;
 /// </summary>
 public class PipelineOptions // base of ClientOptions and RequestContext
 {
-    internal List<(PipelinePolicy, PipelinePosition)> policies = new List<(PipelinePolicy, PipelinePosition)>();
-    /// <summary>
-    /// TBD.
-    /// </summary>
-    /// <param name="policy"></param>
-    /// <param name="position"></param>
-    public void AddPolicy(PipelinePolicy policy, PipelinePosition position)
-    {
-        policies.Add((policy, position));
-    }
-
     /// <summary>
     /// TBD.
     /// </summary>
