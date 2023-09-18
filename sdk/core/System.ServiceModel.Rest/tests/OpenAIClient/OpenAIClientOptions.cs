c1216a28263573f40f4a6411104adc68b1612c6b
Anne Thompson <annelo@microsoft.com>
2023-09-18 16:22:35 -0700

    updates

--- sdk/core/System.ServiceModel.Rest/tests/OpenAIClient/OpenAIClientOptions.cs
+++ sdk/core/System.ServiceModel.Rest/tests/OpenAIClient/OpenAIClientOptions.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.ServiceModel.Rest.Core;
+using System.ServiceModel.Rest;
 
 namespace OpenAI;
 
--- sdk/core/System.ServiceModel.Rest/tests/OpenAIClientTests.cs
+++ sdk/core/System.ServiceModel.Rest/tests/OpenAIClientTests.cs
@@ -10,18 +10,4 @@ namespace System.Tests;
 
 public class OpenAIClientTests
 {
-    //[Fact]
-    //public void ClientDoesNotExposeAzureTypes()
-    //{
-    //    string key = Environment.GetEnvironmentVariable("OPENAI_KEY");
-
-    //    KeyCredential credential = new KeyCredential(key);
-    //    OpenAIClient client = new OpenAIClient(credential);
-
-    //    Result<Completions> result = client.GetCompletions("tell me something about life.");
-    //    Choice choice = result.Value.Choices[0];
-    //    Debug.WriteLine(choice.Text);
-
-    //    Result httpResult = result.GetRawResult();
-    //}
 }
