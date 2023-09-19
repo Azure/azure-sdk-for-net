f631defcc214fd882d49547bf836c46013374dce
Mike Hang <108955604+mikehang-msft@users.noreply.github.com>
2023-09-19 16:15:27 -0700

    Updated new tag in Rooms assets.json

--- sdk/communication/Azure.Communication.Rooms/src/Generated/RoomsModelFactory.cs
+++ sdk/communication/Azure.Communication.Rooms/src/Generated/RoomsModelFactory.cs
@@ -17,23 +17,6 @@ namespace Azure.Communication.Rooms
         /// <param name="createdAt"> The timestamp when the room was created at the server. The timestamp is in RFC3339 format: `yyyy-MM-ddTHH:mm:ssZ`. </param>
         /// <param name="validFrom"> The timestamp from when the room is open for joining. The timestamp is in RFC3339 format: `yyyy-MM-ddTHH:mm:ssZ`. </param>
         /// <param name="validUntil"> The timestamp from when the room can no longer be joined. The timestamp is in RFC3339 format: `yyyy-MM-ddTHH:mm:ssZ`. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="id"/> is null. </exception>
-        /// <returns> A new <see cref="Rooms.CommunicationRoom"/> instance for mocking. </returns>
-        public static CommunicationRoom CommunicationRoom(string id = null, DateTimeOffset createdAt = default, DateTimeOffset validFrom = default, DateTimeOffset validUntil = default)
-        {
-            if (id == null)
-            {
-                throw new ArgumentNullException(nameof(id));
-            }
-
-            return RoomsModelFactory.CommunicationRoom(id, createdAt, validFrom, validUntil, false);
-        }
-
-        /// <summary> Initializes a new instance of CommunicationRoom. </summary>
-        /// <param name="id"> Unique identifier of a room. This id is server generated. </param>
-        /// <param name="createdAt"> The timestamp when the room was created at the server. The timestamp is in RFC3339 format: `yyyy-MM-ddTHH:mm:ssZ`. </param>
-        /// <param name="validFrom"> The timestamp from when the room is open for joining. The timestamp is in RFC3339 format: `yyyy-MM-ddTHH:mm:ssZ`. </param>
-        /// <param name="validUntil"> The timestamp from when the room can no longer be joined. The timestamp is in RFC3339 format: `yyyy-MM-ddTHH:mm:ssZ`. </param>
         /// <param name="pstnDialOutEnabled"> Set this flag to true if, at the time of the call, dial out to a PSTN number is enabled in a particular room. By default, this flag is set to false. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="id"/> is null. </exception>
         /// <returns> A new <see cref="Rooms.CommunicationRoom"/> instance for mocking. </returns>
