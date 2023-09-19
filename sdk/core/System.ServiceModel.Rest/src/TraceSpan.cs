3be60aa5e9a2cdcdfc6d188f86a26321a06e8cf6
Anne Thompson <annelo@microsoft.com>
2023-09-19 10:52:08 -0700

    Add TraceSpan

---
+++ sdk/core/System.ServiceModel.Rest/src/TraceSpan.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.Pipeline;
+
+namespace System.ServiceModel.Rest
+{
+    /// <summary>
+    /// TBD.
+    /// </summary>
+    public class TraceSpan : IDisposable
+    {
+        private readonly DiagnosticScope _scope;
+
+        internal TraceSpan(DiagnosticScope scope)
+        {
+            _scope = scope;
+        }
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        public void Start() => _scope.Start();
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        /// <param name="exception"></param>
+        public void Failed(Exception exception) => _scope.Failed(exception);
+
+        /// <inheritdoc/>
+        public void Dispose() => _scope.Dispose();
+    }
+}
---
+++ sdk/core/System.ServiceModel.Rest/src/TraceSpanFactory.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.Pipeline;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace System.ServiceModel.Rest
+{
+    /// <summary>
+    /// TBD.
+    /// </summary>
+    public class TraceSpanFactory
+    {
+        private readonly DiagnosticScopeFactory _factory;
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="suppressNestedClientActivities"></param>
+        public TraceSpanFactory(PipelineOptions options, bool suppressNestedClientActivities = true)
+        {
+            _factory = new DiagnosticScopeFactory(
+                options.GetType().Namespace!,
+                GetResourceProviderNamespace(options.GetType().Assembly),
+                // TODO: get the following value from options.Diagnostics.IsDistributedTracingEnabled
+                true,
+                suppressNestedClientActivities);
+        }
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="kind"></param>
+        /// <returns></returns>
+        public TraceSpan CreateSpan(string name, ActivityKind kind = ActivityKind.Internal)
+            => new TraceSpan(_factory.CreateScope(name, kind));
+
+        private static string? GetResourceProviderNamespace(Assembly assembly)
+        {
+            foreach (var customAttribute in assembly.GetCustomAttributes(true))
+            {
+                // Weak bind internal shared type
+                var attributeType = customAttribute.GetType();
+                if (attributeType.Name == "AzureResourceProviderNamespaceAttribute")
+                {
+                    return attributeType.GetProperty("ResourceProviderNamespace")?.GetValue(customAttribute) as string;
+                }
+            }
+
+            return null;
+        }
+    }
+}
