c22f8228219de37af072c0f138f24dea3868f977
Anne Thompson <annelo@microsoft.com>
2023-09-20 11:54:40 -0700

    IUtf8JsonSerializable

---
+++ sdk/core/System.ServiceModel.Rest/src/IUtf8JsonWriteable.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json;
+
+namespace System.ServiceModel.Rest.Shared.Serialization
+{
+    /// <summary>
+    /// TBD.
+    /// </summary>
+    public interface IUtf8JsonWriteable
+    {
+#pragma warning disable AZC0014 // Avoid using banned types in public API
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        /// <param name="writer"></param>
+        void Write(Utf8JsonWriter writer);
+#pragma warning restore AZC0014 // Avoid using banned types in public API
+    }
+}
