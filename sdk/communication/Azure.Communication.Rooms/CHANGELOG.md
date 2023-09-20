7e6220971969fd3749f2dcdc25f615507be9fdf5
Mike Hang <108955604+mikehang-msft@users.noreply.github.com>
2023-09-20 10:23:58 -0700

    Updated ChangeLog and Generated new export API

--- sdk/communication/Azure.Communication.Rooms/CHANGELOG.md
+++ sdk/communication/Azure.Communication.Rooms/CHANGELOG.md
@@ -1,13 +1,5 @@
 # Release History
 
-## 1.1.0-beta.1 (Unreleased)
-
-### Features Added
-
-### Breaking Changes
-
-### Bugs Fixed
-
 ### Other Changes
 ## 1.1.0-beta.1 (2023-09-19)
 
--- sdk/communication/Azure.Communication.Rooms/api/Azure.Communication.Rooms.netstandard2.0.cs
+++ sdk/communication/Azure.Communication.Rooms/api/Azure.Communication.Rooms.netstandard2.0.cs
@@ -5,9 +5,17 @@ namespace Azure.Communication.Rooms
         internal CommunicationRoom() { }
         public System.DateTimeOffset CreatedAt { get { throw null; } }
         public string Id { get { throw null; } }
+        public bool PstnDialOutEnabled { get { throw null; } }
         public System.DateTimeOffset ValidFrom { get { throw null; } }
         public System.DateTimeOffset ValidUntil { get { throw null; } }
-        public System.Boolean PstnDialOutEnabled { get { throw null; } }
+    }
+    public partial class CreateRoomOptions
+    {
+        public CreateRoomOptions() { }
+        public System.Collections.Generic.IEnumerable<Azure.Communication.Rooms.RoomParticipant> Participants { get { throw null; } set { } }
+        public bool? PstnDialOutEnabled { get { throw null; } set { } }
+        public System.DateTimeOffset? ValidFrom { get { throw null; } set { } }
+        public System.DateTimeOffset? ValidUntil { get { throw null; } set { } }
     }
     [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
     public readonly partial struct ParticipantRole : System.IEquatable<Azure.Communication.Rooms.ParticipantRole>
@@ -43,10 +51,10 @@ namespace Azure.Communication.Rooms
         public RoomsClient(System.Uri endpoint, Azure.Core.TokenCredential credential, Azure.Communication.Rooms.RoomsClientOptions options = null) { }
         public virtual Azure.Response AddOrUpdateParticipants(string roomId, System.Collections.Generic.IEnumerable<Azure.Communication.Rooms.RoomParticipant> participants, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> AddOrUpdateParticipantsAsync(string roomId, System.Collections.Generic.IEnumerable<Azure.Communication.Rooms.RoomParticipant> participants, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public virtual Azure.Response<Azure.Communication.Rooms.CommunicationRoom> CreateRoom(Azure.Communication.Rooms.CreateRoomOptions roomCreateOptions, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual Azure.Response<Azure.Communication.Rooms.CommunicationRoom> CreateRoom(System.DateTimeOffset? validFrom = default(System.DateTimeOffset?), System.DateTimeOffset? validUntil = default(System.DateTimeOffset?), System.Collections.Generic.IEnumerable<Azure.Communication.Rooms.RoomParticipant> participants = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
-        public virtual Azure.Response<Azure.Communication.Rooms.CommunicationRoom> CreateRoom(Azure.Communication.Rooms.RoomCreateOptions roomCreateOptions, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response<Azure.Communication.Rooms.CommunicationRoom>> CreateRoomAsync(Azure.Communication.Rooms.CreateRoomOptions roomCreateOptions, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response<Azure.Communication.Rooms.CommunicationRoom>> CreateRoomAsync(System.DateTimeOffset? validFrom = default(System.DateTimeOffset?), System.DateTimeOffset? validUntil = default(System.DateTimeOffset?), System.Collections.Generic.IEnumerable<Azure.Communication.Rooms.RoomParticipant> participants = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
-        public virtual System.Threading.Tasks.Task<Azure.Response<Azure.Communication.Rooms.CommunicationRoom>> CreateRoomAsync((Azure.Communication.Rooms.RoomCreateOptions roomCreateOptions, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual Azure.Response DeleteRoom(string roomId, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> DeleteRoomAsync(string roomId, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual Azure.Pageable<Azure.Communication.Rooms.RoomParticipant> GetParticipants(string roomId, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
@@ -57,10 +65,10 @@ namespace Azure.Communication.Rooms
         public virtual Azure.AsyncPageable<Azure.Communication.Rooms.CommunicationRoom> GetRoomsAsync(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual Azure.Response RemoveParticipants(string roomId, System.Collections.Generic.IEnumerable<Azure.Communication.CommunicationIdentifier> participantIdentifiers, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response> RemoveParticipantsAsync(string roomId, System.Collections.Generic.IEnumerable<Azure.Communication.CommunicationIdentifier> participantIdentifiers, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public virtual Azure.Response<Azure.Communication.Rooms.CommunicationRoom> UpdateRoom(Azure.Communication.Rooms.UpdateRoomOptions roomUpdateOptions, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual Azure.Response<Azure.Communication.Rooms.CommunicationRoom> UpdateRoom(string roomId, System.DateTimeOffset? validFrom = default(System.DateTimeOffset?), System.DateTimeOffset? validUntil = default(System.DateTimeOffset?), System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
-        public virtual Azure.Response<Azure.Communication.Rooms.CommunicationRoom> UpdateRoom(Azure.Communication.Rooms.RoomUpdateOptions roomUpdateOptions, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        public virtual System.Threading.Tasks.Task<Azure.Response<Azure.Communication.Rooms.CommunicationRoom>> UpdateRoomAsync(Azure.Communication.Rooms.UpdateRoomOptions roomUpdateOptions, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public virtual System.Threading.Tasks.Task<Azure.Response<Azure.Communication.Rooms.CommunicationRoom>> UpdateRoomAsync(string roomId, System.DateTimeOffset? validFrom = default(System.DateTimeOffset?), System.DateTimeOffset? validUntil = default(System.DateTimeOffset?), System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
-        public virtual System.Threading.Tasks.Task<Azure.Response<Azure.Communication.Rooms.CommunicationRoom>> UpdateRoomAsync(Azure.Communication.Rooms.RoomUpdateOptions roomUpdateOptions, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
     }
     public partial class RoomsClientOptions : Azure.Core.ClientOptions
     {
@@ -73,7 +81,16 @@ namespace Azure.Communication.Rooms
     }
     public static partial class RoomsModelFactory
     {
-        public static Azure.Communication.Rooms.CommunicationRoom CommunicationRoom(string id = null, System.DateTimeOffset createdAt = default(System.DateTimeOffset), System.DateTimeOffset validFrom = default(System.DateTimeOffset), System.DateTimeOffset validUntil = default(System.DateTimeOffset), System.DateTimeOffset validUntil = default(System.DateTimeOffset), System.Boolean pstnDialOutEnabled = default(System.Boolean)) { throw null; }
+        public static Azure.Communication.Rooms.CommunicationRoom CommunicationRoom(string id = null, System.DateTimeOffset createdAt = default(System.DateTimeOffset), System.DateTimeOffset validFrom = default(System.DateTimeOffset), System.DateTimeOffset validUntil = default(System.DateTimeOffset)) { throw null; }
+        public static Azure.Communication.Rooms.CommunicationRoom CommunicationRoom(string id = null, System.DateTimeOffset createdAt = default(System.DateTimeOffset), System.DateTimeOffset validFrom = default(System.DateTimeOffset), System.DateTimeOffset validUntil = default(System.DateTimeOffset), bool pstnDialOutEnabled = false) { throw null; }
         public static Azure.Communication.Rooms.RoomParticipant RoomParticipant(string rawId, Azure.Communication.Rooms.ParticipantRole role) { throw null; }
+    }
+    public partial class UpdateRoomOptions
+    {
+        public UpdateRoomOptions() { }
+        public string Id { get { throw null; } set { } }
+        public bool? PstnDialOutEnabled { get { throw null; } set { } }
+        public System.DateTimeOffset? ValidFrom { get { throw null; } set { } }
+        public System.DateTimeOffset? ValidUntil { get { throw null; } set { } }
     }
 }
