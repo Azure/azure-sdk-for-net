1e281884b2d3613dd7b770fc2325157682d5d597
azure-sdk <azuresdk@microsoft.com>
2023-09-21 01:19:42 +0000

    Update SDK code ta_wo_19

--- sdk/template/Azure.Template/tests/Generated/Samples/Samples_TemplateClient.cs
+++ sdk/template/Azure.Template/tests/Generated/Samples/Samples_TemplateClient.cs
@@ -6,12 +6,13 @@
 #nullable disable
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Azure;
 using Azure.Core;
 using Azure.Identity;
-using Azure.Template;
 using NUnit.Framework;
 
 namespace Azure.Template.Samples
@@ -22,23 +23,10 @@ namespace Azure.Template.Samples
         [Ignore("Only validating compilation of examples")]
         public void Example_GetSecret()
         {
-            TokenCredential credential = new DefaultAzureCredential();
-            TemplateClient client = new TemplateClient("<VaultBaseUrl>", credential);
+            var credential = new DefaultAzureCredential();
+            var client = new TemplateClient("<vaultBaseUrl>", credential);
 
-            Response response = client.GetSecret("<secretName>", null);
-
-            JsonElement result = JsonDocument.Parse(response.ContentStream).RootElement;
-            Console.WriteLine(result.ToString());
-        }
-
-        [Test]
-        [Ignore("Only validating compilation of examples")]
-        public async Task Example_GetSecret_Async()
-        {
-            TokenCredential credential = new DefaultAzureCredential();
-            TemplateClient client = new TemplateClient("<VaultBaseUrl>", credential);
-
-            Response response = await client.GetSecretAsync("<secretName>", null);
+            Response response = client.GetSecret("<secretName>", new RequestContext());
 
             JsonElement result = JsonDocument.Parse(response.ContentStream).RootElement;
             Console.WriteLine(result.ToString());
@@ -48,34 +36,47 @@ namespace Azure.Template.Samples
         [Ignore("Only validating compilation of examples")]
         public void Example_GetSecret_AllParameters()
         {
-            TokenCredential credential = new DefaultAzureCredential();
-            TemplateClient client = new TemplateClient("<VaultBaseUrl>", credential);
+            var credential = new DefaultAzureCredential();
+            var client = new TemplateClient("<vaultBaseUrl>", credential);
 
-            Response response = client.GetSecret("<secretName>", null);
+            Response response = client.GetSecret("<secretName>", new RequestContext());
 
             JsonElement result = JsonDocument.Parse(response.ContentStream).RootElement;
             Console.WriteLine(result.GetProperty("value").ToString());
             Console.WriteLine(result.GetProperty("id").ToString());
             Console.WriteLine(result.GetProperty("contentType").ToString());
-            Console.WriteLine(result.GetProperty("tags").GetProperty("<key>").ToString());
+            Console.WriteLine(result.GetProperty("tags").GetProperty("<test>").ToString());
             Console.WriteLine(result.GetProperty("kid").ToString());
             Console.WriteLine(result.GetProperty("managed").ToString());
         }
 
         [Test]
         [Ignore("Only validating compilation of examples")]
+        public async Task Example_GetSecret_Async()
+        {
+            var credential = new DefaultAzureCredential();
+            var client = new TemplateClient("<vaultBaseUrl>", credential);
+
+            Response response = await client.GetSecretAsync("<secretName>", new RequestContext());
+
+            JsonElement result = JsonDocument.Parse(response.ContentStream).RootElement;
+            Console.WriteLine(result.ToString());
+        }
+
+        [Test]
+        [Ignore("Only validating compilation of examples")]
         public async Task Example_GetSecret_AllParameters_Async()
         {
-            TokenCredential credential = new DefaultAzureCredential();
-            TemplateClient client = new TemplateClient("<VaultBaseUrl>", credential);
+            var credential = new DefaultAzureCredential();
+            var client = new TemplateClient("<vaultBaseUrl>", credential);
 
-            Response response = await client.GetSecretAsync("<secretName>", null);
+            Response response = await client.GetSecretAsync("<secretName>", new RequestContext());
 
             JsonElement result = JsonDocument.Parse(response.ContentStream).RootElement;
             Console.WriteLine(result.GetProperty("value").ToString());
             Console.WriteLine(result.GetProperty("id").ToString());
             Console.WriteLine(result.GetProperty("contentType").ToString());
-            Console.WriteLine(result.GetProperty("tags").GetProperty("<key>").ToString());
+            Console.WriteLine(result.GetProperty("tags").GetProperty("<test>").ToString());
             Console.WriteLine(result.GetProperty("kid").ToString());
             Console.WriteLine(result.GetProperty("managed").ToString());
         }
