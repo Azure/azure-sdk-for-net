8a3b5492159cc363851dd78601bcb550abf75999
Anne Thompson <annelo@microsoft.com>
2023-09-21 10:03:20 -0700

    starting to think about policy

---
+++ sdk/core/System.ServiceModel.Rest/src/Temporary/KeyCredentialPolicy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.ServiceModel.Rest.Temporary
+{
+    public class KeyCredentialPolicy
+    {
+    }
+}
---
+++ sdk/core/System.ServiceModel.Rest/src/Temporary/PipelinePolicy.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.ServiceModel.Rest.Core.Pipeline
+{
+    /// <summary>
+    /// TBD.
+    /// </summary>
+    public abstract class PipelinePolicy
+    {
+    }
+}
