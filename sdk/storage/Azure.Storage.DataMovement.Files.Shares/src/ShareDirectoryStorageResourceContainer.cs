60f21a1d4fd2e398ab18a58d773e59b93dcde3d8
Jocelyn <41338290+jaschrep-msft@users.noreply.github.com>
2023-09-22 09:04:32 -0400

    DataMovement `ShareDirectoryStorageResourceContainer` implementation (#38892)
    * resource container listing implementation
    
    PathScanner for share directories.
    Test PathScanner.
    
    * tests
    
    * getstorageresourcereference
    
    * fix net462 issue

--- sdk/storage/Azure.Storage.DataMovement.Files.Shares/src/ShareDirectoryStorageResourceContainer.cs
+++ sdk/storage/Azure.Storage.DataMovement.Files.Shares/src/ShareDirectoryStorageResourceContainer.cs
@@ -3,14 +3,18 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
+using System.Threading.Tasks;
 using Azure.Storage.Files.Shares;
 
 namespace Azure.Storage.DataMovement.Files.Shares
 {
     internal class ShareDirectoryStorageResourceContainer : StorageResourceContainer
     {
-        internal readonly ShareFileStorageResourceOptions _options;
+        internal ShareFileStorageResourceOptions ResourceOptions { get; set; }
+        internal PathScanner PathScanner { get; set; }
 
         internal ShareDirectoryClient ShareDirectoryClient { get; }
 
@@ -19,17 +23,44 @@ namespace Azure.Storage.DataMovement.Files.Shares
         internal ShareDirectoryStorageResourceContainer(ShareDirectoryClient shareDirectoryClient, ShareFileStorageResourceOptions options)
         {
             ShareDirectoryClient = shareDirectoryClient;
-            _options = options;
+            ResourceOptions = options;
         }
 
         protected override StorageResourceItem GetStorageResourceReference(string path)
         {
-            throw new NotImplementedException();
+            List<string> pathSegments = path.Split('/').Where(s => !string.IsNullOrEmpty(s)).ToList();
+            ShareDirectoryClient dir = ShareDirectoryClient;
+            foreach (string pathSegment in pathSegments.Take(pathSegments.Count - 1))
+            {
+                dir = dir.GetSubdirectoryClient(pathSegment);
+            }
+            ShareFileClient file = dir.GetFileClient(pathSegments.Last());
+            return new ShareFileStorageResourceItem(file, ResourceOptions);
         }
 
-        protected override IAsyncEnumerable<StorageResource> GetStorageResourcesAsync(CancellationToken cancellationToken = default)
+        protected override async IAsyncEnumerable<StorageResource> GetStorageResourcesAsync(
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await foreach (ShareFileClient client in PathScanner.ScanFilesAsync(
+                ShareDirectoryClient, cancellationToken).ConfigureAwait(false))
+            {
+                yield return new ShareFileStorageResourceItem(client, ResourceOptions);
+            }
         }
+
+        #region Protected Hooks
+        // Internal func to access protected member for testing.
+        internal async IAsyncEnumerable<StorageResource> GetStorageResourcesInternal(
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            await foreach (StorageResource resource in GetStorageResourcesAsync(cancellationToken).ConfigureAwait(false))
+            {
+                yield return resource;
+            }
+        }
+
+        internal StorageResourceItem GetStorageResourceReferenceInternal(string path)
+            => GetStorageResourceReference(path);
+        #endregion
     }
 }
