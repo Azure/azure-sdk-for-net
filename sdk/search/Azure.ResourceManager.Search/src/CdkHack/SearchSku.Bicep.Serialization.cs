fc23838a092ed494070e640548631a52b5a29a7f
Jon Gallant <2163001+jongio@users.noreply.github.com>
2023-09-22 17:31:20 +0000

    Change to ToSerialString

--- sdk/search/Azure.ResourceManager.Search/src/CdkHack/SearchSku.Bicep.Serialization.cs
+++ sdk/search/Azure.ResourceManager.Search/src/CdkHack/SearchSku.Bicep.Serialization.cs
@@ -39,7 +39,7 @@ namespace Azure.ResourceManager.Search.Models
         private BinaryData SerializeBicep(ModelSerializerOptions options)
         {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"  name: '{Name.Value.ToLower()}'");
+            sb.AppendLine($"  name: '{Name.ToSerialString()}'");
             return new BinaryData(sb.ToString());
         }
     }
