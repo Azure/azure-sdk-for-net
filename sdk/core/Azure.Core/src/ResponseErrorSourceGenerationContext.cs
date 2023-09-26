eb6c3c352f3a4a5a8ad79dd9cfc180a20933cb5c
Madalyn Redding <mredding@microsoft.com>
2023-09-26 10:11:27 -0400

    iteration 1

---
+++ sdk/core/Azure.Core/src/ResponseErrorSourceGenerationContext.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Azure
+{
+#if NET6_0_OR_GREATER
+    [JsonSerializable(typeof(ResponseError))]
+    [JsonSerializable(typeof(ResponseInnerError))]
+    [JsonSerializable(typeof(JsonElement))]
+    [JsonSerializable(typeof(string))]
+    [JsonSerializable(typeof(IReadOnlyList<ResponseError>))]
+    internal partial class ResponseErrorSourceGenerationContext : JsonSerializerContext
+    {
+    }
+#endif
+}
