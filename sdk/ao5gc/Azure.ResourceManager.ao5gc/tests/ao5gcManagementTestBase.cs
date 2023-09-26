1c98d38bff5d66df0e5dd1f5e9a7839c11fd381a
ishanchopra <ishanchopra@microsoft.com>
2023-09-26 16:52:59 +0530

    Generated SDK

---
+++ sdk/ao5gc/Azure.ResourceManager.ao5gc/tests/ao5gcManagementTestBase.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.TestFramework;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Azure.ResourceManager.ao5gc.Tests
+{
+    public class ao5gcManagementTestBase : ManagementRecordedTestBase<ao5gcManagementTestEnvironment>
+    {
+        protected ArmClient Client { get; private set; }
+        protected SubscriptionResource DefaultSubscription { get; private set; }
+
+        protected ao5gcManagementTestBase(bool isAsync, RecordedTestMode mode)
+        : base(isAsync, mode)
+        {
+        }
+
+        protected ao5gcManagementTestBase(bool isAsync)
+            : base(isAsync)
+        {
+        }
+
+        [SetUp]
+        public async Task CreateCommonClient()
+        {
+            Client = GetArmClient();
+            DefaultSubscription = await Client.GetDefaultSubscriptionAsync().ConfigureAwait(false);
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        {
+            string rgName = Recording.GenerateAssetName(rgNamePrefix);
+            ResourceGroupData input = new ResourceGroupData(location);
+            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            return lro.Value;
+        }
+    }
+}
---
+++ sdk/ao5gc/Azure.ResourceManager.ao5gc/tests/ao5gcManagementTestEnvironment.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.ResourceManager.ao5gc.Tests
+{
+    public class ao5gcManagementTestEnvironment : TestEnvironment
+    {
+    }
+}
