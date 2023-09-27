f7e263e04cce6c80eef1077a41635e5fa7c7ae47
Madalyn Redding <mredding@microsoft.com>
2023-09-27 09:48:54 -0400

    adding checkpointpositiontests.cs

--- sdk/eventhub/Azure.Messaging.EventHubs/src/Consumer/CheckpointPosition.cs
+++ sdk/eventhub/Azure.Messaging.EventHubs/src/Consumer/CheckpointPosition.cs
@@ -3,7 +3,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using Azure.Core;
 
 namespace Azure.Messaging.EventHubs.Consumer
 {
@@ -71,5 +73,89 @@ namespace Azure.Messaging.EventHubs.Consumer
         {
             return new CheckpointPosition(eventData.SequenceNumber, eventData.ReplicationSegment, eventData.Offset);
         }
+
+        /// <summary>
+        ///   Determines whether the specified <see cref="CheckpointPosition" /> is equal to this instance.
+        /// </summary>
+        ///
+        /// <param name="other">The <see cref="CheckpointPosition" /> to compare with this instance.</param>
+        ///
+        /// <returns><c>true</c> if the specified <see cref="CheckpointPosition" /> is equal to this instance; otherwise, <c>false</c>.</returns>
+        ///
+        public bool Equals(CheckpointPosition other)
+        {
+            return (Offset == other.Offset)
+                && (SequenceNumber == other.SequenceNumber)
+                && (ReplicationSegment == other.ReplicationSegment);
+        }
+
+        /// <summary>
+        ///   Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+        /// </summary>
+        ///
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        ///
+        /// <returns><c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.</returns>
+        ///
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj) =>
+            obj switch
+            {
+                CheckpointPosition other => Equals(other),
+                _ => false
+            };
+
+        /// <summary>
+        ///   Returns a hash code for this instance.
+        /// </summary>
+        ///
+        /// <returns>A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.</returns>
+        ///
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode()
+        {
+            var hashCode = new HashCodeBuilder();
+            hashCode.Add(Offset);
+            hashCode.Add(SequenceNumber);
+            hashCode.Add(ReplicationSegment);
+
+            return hashCode.ToHashCode();
+        }
+
+        /// <summary>
+        ///   Converts the instance to string representation.
+        /// </summary>
+        ///
+        /// <returns>A <see cref="System.String" /> that represents the position in the event stream.</returns>
+        ///
+        public override string ToString()
+        {
+            var stringRepresentation = $"Sequence Number: [{SequenceNumber}] | Replication Segment: [{ReplicationSegment}] | Offset: [{Offset}]";
+            return stringRepresentation;
+        }
+
+        /// <summary>
+        ///   Determines whether the specified <see cref="CheckpointPosition" /> instances are equal to each other.
+        /// </summary>
+        ///
+        /// <param name="left">The first <see cref="CheckpointPosition" /> to consider.</param>
+        /// <param name="right">The second <see cref="CheckpointPosition" /> to consider.</param>
+        ///
+        /// <returns><c>true</c> if the two specified <see cref="CheckpointPosition" /> instances are equal; otherwise, <c>false</c>.</returns>
+        ///
+        public static bool operator ==(CheckpointPosition left,
+                                       CheckpointPosition right) => left.Equals(right);
+
+        /// <summary>
+        ///   Determines whether the specified <see cref="CheckpointPosition" /> instances are not equal to each other.
+        /// </summary>
+        ///
+        /// <param name="left">The first <see cref="CheckpointPosition" /> to consider.</param>
+        /// <param name="right">The second <see cref="CheckpointPosition" /> to consider.</param>
+        ///
+        /// <returns><c>true</c> if the two specified <see cref="CheckpointPosition" /> instances are not equal; otherwise, <c>false</c>.</returns>
+        ///
+        public static bool operator !=(CheckpointPosition left,
+                                       CheckpointPosition right) => (!left.Equals(right));
     }
 }
