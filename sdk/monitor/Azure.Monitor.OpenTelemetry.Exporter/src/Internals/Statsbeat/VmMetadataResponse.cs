e9d72c3ee9803e750b4010912d80680a806979cd
Timothy Mothra Lee <tilee@microsoft.com>
2023-09-27 14:36:09 -0700

    cleanup

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/Statsbeat/VmMetadataResponse.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/Statsbeat/VmMetadataResponse.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Text.Json.Serialization;
-
 namespace Azure.Monitor.OpenTelemetry.Exporter.Internals.Statsbeat
 {
     internal class VmMetadataResponse
