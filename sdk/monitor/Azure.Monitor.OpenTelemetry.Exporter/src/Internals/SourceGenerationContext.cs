cbc97573705e5a4ed6862c2a68fe39917153537b
Timothy Mothra Lee <tilee@microsoft.com>
2023-09-28 11:48:27 -0700

    update comment

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/SourceGenerationContext.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/SourceGenerationContext.cs
@@ -7,8 +7,12 @@ using System.Text.Json.Serialization;
 namespace Azure.Monitor.OpenTelemetry.Exporter.Internals;
 
 #if NET6_0_OR_GREATER
-// "Source Generation" is a mitigation for AOT IL3050:RequiresDynamicCode and IL2026:RequiresUnreferencedCode
-// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/source-generation
+// "Source Generation" is feature added to System.Text.Json in .NET 6.0.
+// This is a performance optimization that avoids runtime reflection when performing serialization.
+// Serialization metadata will be computed at compile-time and included in the assembly.
+// https://learn.microsoft.com/dotnet/standard/serialization/system-text-json/source-generation-modes
+// https://learn.microsoft.com/dotnet/standard/serialization/system-text-json/source-generation
+// https://devblogs.microsoft.com/dotnet/try-the-new-system-text-json-source-generator/
 [JsonSerializable(typeof(VmMetadataResponse))]
 [JsonSerializable(typeof(string))]
 [JsonSerializable(typeof(IngestionResponseHelper.ResponseObject))]
