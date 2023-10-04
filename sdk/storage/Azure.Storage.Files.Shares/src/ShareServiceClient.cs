ecb6df57faf47579399349b026f8482296848c3c
Amanda Nguyen <amnguye@microsoft.com>
2023-10-03 19:52:57 -0700

    Renamed Storage PublicAudience to DefaultAudience, Get..Audience to Create..Audience

--- sdk/storage/Azure.Storage.Files.Shares/src/ShareServiceClient.cs
+++ sdk/storage/Azure.Storage.Files.Shares/src/ShareServiceClient.cs
@@ -274,7 +274,7 @@ namespace Azure.Storage.Files.Shares
             : this(
                   serviceUri: serviceUri,
                   authentication: credential.AsPolicy(
-                    string.IsNullOrEmpty(options?.Audience?.ToString()) ? ShareAudience.PublicAudience.CreateDefaultScope() : options.Audience.Value.CreateDefaultScope(),
+                    string.IsNullOrEmpty(options?.Audience?.ToString()) ? ShareAudience.DefaultAudience.CreateDefaultScope() : options.Audience.Value.CreateDefaultScope(),
                     options),
                   options: options ?? new ShareClientOptions(),
                   sharedKeyCredential: null,
