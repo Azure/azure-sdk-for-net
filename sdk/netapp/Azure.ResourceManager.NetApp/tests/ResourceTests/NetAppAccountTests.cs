b1ba8471ed0d1f29bb8469eed86e9fa3ea804e36
Audunn Baldvinsson <audunn.baldvinsson@netapp.com>
2023-10-11 16:54:46 +0000

    assets

--- sdk/netapp/Azure.ResourceManager.NetApp/tests/ResourceTests/NetAppAccountTests.cs
+++ sdk/netapp/Azure.ResourceManager.NetApp/tests/ResourceTests/NetAppAccountTests.cs
@@ -21,7 +21,7 @@ namespace Azure.ResourceManager.NetApp.Tests
     public class NetAppAccountTests: NetAppTestBase
     {
         private const string namePrefix = "testNetAppNetSDKmgmt";
-        public static new AzureLocation DefaultLocation = " eastus2euap";
+        public static new AzureLocation DefaultLocation = " eastus2";
 
         public NetAppAccountTests(bool isAsync) : base(isAsync)
         {
