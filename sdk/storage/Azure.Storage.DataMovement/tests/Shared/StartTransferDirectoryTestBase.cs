669b54267a118c81474c90a9f2db5bdacfb69d36
Amanda Nguyen <amnguye@microsoft.com>
2023-10-12 16:30:38 -0700

    WIP

---
+++ sdk/storage/Azure.Storage.DataMovement/tests/Shared/StartTransferDirectoryTestBase.cs
@@ -0,0 +1,164 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.Storage.Test.Shared;
+
+namespace Azure.Storage.DataMovement.Tests
+{
+    public abstract class StartTransferDirectoryTestBase
+            <TSourceServiceClient,
+            TSourceContainerClient,
+            TSourceObjectClient,
+            TSourceClientOptions,
+            TDestinationServiceClient,
+            TDestinationContainerClient,
+            TDestinationObjectClient,
+            TDestinationClientOptions,
+            TEnvironment> : StorageTestBase<TEnvironment>
+        where TSourceServiceClient : class
+        where TSourceContainerClient : class
+        where TSourceObjectClient : class
+        where TSourceClientOptions : ClientOptions
+        where TDestinationServiceClient : class
+        where TDestinationContainerClient : class
+        where TDestinationObjectClient : class
+        where TDestinationClientOptions : ClientOptions
+        where TEnvironment : StorageTestEnvironment, new()
+    {
+        private readonly string _generatedResourceNamePrefix;
+        private readonly string _expectedOverwriteExceptionMessage;
+
+        public ClientBuilder<TSourceServiceClient, TSourceClientOptions> SourceClientBuilder { get; protected set; }
+        public ClientBuilder<TDestinationServiceClient, TDestinationClientOptions> DestinationClientBuilder { get; protected set; }
+
+        /// <summary>
+        /// Constructor for TransferManager.StartTransferAsync tests
+        ///
+        /// The async is defaulted to true, since we do not have sync StartTransfer methods.
+        /// </summary>
+        /// <param name="generatedResourcenamePrefix"></param>
+        /// <param name="mode"></param>
+        public StartTransferDirectoryTestBase(
+            bool async,
+            string expectedOverwriteExceptionMessage,
+            string generatedResourceNamePrefix = default,
+            RecordedTestMode? mode = null) : base(async, mode)
+        {
+            Argument.CheckNotNullOrEmpty(expectedOverwriteExceptionMessage, nameof(expectedOverwriteExceptionMessage));
+            _generatedResourceNamePrefix = generatedResourceNamePrefix ?? "test-resource-";
+            _expectedOverwriteExceptionMessage = expectedOverwriteExceptionMessage;
+        }
+
+        #region Service-Specific Methods
+        /// <summary>
+        /// Gets a service-specific disposing container for use with tests in this class.
+        /// </summary>
+        /// <param name="service">Optionally specified service client to get container from.</param>
+        /// <param name="containerName">Optional container name specification.</param>
+        protected abstract Task<IDisposingContainer<TSourceContainerClient>> GetSourceDisposingContainerAsync(
+            TSourceServiceClient service = default,
+            string containerName = default);
+
+        /// <summary>
+        /// Gets a new service-specific child object client from a given container, e.g. a BlobClient from a
+        /// BlobContainerClient or a TDestinationObjectClient from a ShareClient.
+        /// </summary>
+        /// <param name="container">Container to get resource from.</param>
+        /// <param name="objectLength">Sets the resource size in bytes, for resources that require this upfront.</param>
+        /// <param name="createResource">Whether to call CreateAsync on the resource, if necessary.</param>
+        /// <param name="objectName">Optional name for the resource.</param>
+        /// <param name="options">ClientOptions for the resource client.</param>
+        /// <param name="contents">If specified, the contents will be uploaded to the object client.</param>
+        protected abstract Task<TSourceObjectClient> GetSourceObjectClientAsync(
+            TSourceContainerClient container,
+            long? objectLength = default,
+            bool createResource = false,
+            string objectName = default,
+            TSourceClientOptions options = default,
+            Stream contents = default);
+
+        /// <summary>
+        /// Gets the specific storage resource from the given TDestinationObjectClient
+        /// e.g. ShareFileClient to a ShareFileStorageResource, BlockBlobClient to a BlockBlobStorageResource.
+        /// </summary>
+        /// <param name="objectClient">The object client to create the storage resource object.</param>
+        /// <returns></returns>
+        protected abstract StorageResourceItem GetSourceStorageResourceItem(TSourceObjectClient objectClient);
+
+        /// <summary>
+        /// Calls the OpenRead method on the TDestinationObjectClient.
+        ///
+        /// This is mainly used to verify the contents of the Object Client.
+        /// </summary>
+        /// <param name="objectClient">The object client to get the Open Read Stream from.</param>
+        /// <returns></returns>
+        protected abstract Task<Stream> SourceOpenReadAsync(TSourceObjectClient objectClient);
+
+        /// <summary>
+        /// Checks if the Object Client exists.
+        /// </summary>
+        /// <param name="objectClient">Object Client to call exists on.</param>
+        /// <returns></returns>
+        protected abstract Task<bool> SourceExistsAsync(TSourceObjectClient objectClient);
+
+        /// <summary>
+        /// Gets a service-specific disposing container for use with tests in this class.
+        /// </summary>
+        /// <param name="service">Optionally specified service client to get container from.</param>
+        /// <param name="containerName">Optional container name specification.</param>
+        protected abstract Task<IDisposingContainer<TDestinationContainerClient>> GetDisposingContainerAsync(
+            TDestinationServiceClient service = default,
+            string containerName = default);
+
+        /// <summary>
+        /// Gets a new service-specific child object client from a given container, e.g. a BlobClient from a
+        /// BlobContainerClient or a TDestinationObjectClient from a ShareClient.
+        /// </summary>
+        /// <param name="container">Container to get resource from.</param>
+        /// <param name="objectLength">Sets the resource size in bytes, for resources that require this upfront.</param>
+        /// <param name="createResource">Whether to call CreateAsync on the resource, if necessary.</param>
+        /// <param name="objectName">Optional name for the resource.</param>
+        /// <param name="options">ClientOptions for the resource client.</param>
+        /// <param name="contents">If specified, the contents will be uploaded to the object client.</param>
+        protected abstract Task<TDestinationObjectClient> GeTDestinationObjectClientAsync(
+            TDestinationContainerClient container,
+            long? objectLength = default,
+            bool createResource = false,
+            string objectName = default,
+            TDestinationClientOptions options = default,
+            Stream contents = default);
+
+        /// <summary>
+        /// Gets the specific storage resource from the given TDestinationObjectClient
+        /// e.g. ShareFileClient to a ShareFileStorageResource, BlockBlobClient to a BlockBlobStorageResource.
+        /// </summary>
+        /// <param name="objectClient">The object client to create the storage resource object.</param>
+        /// <returns></returns>
+        protected abstract StorageResourceItem GetStorageResourceItem(TDestinationObjectClient objectClient);
+
+        /// <summary>
+        /// Calls the OpenRead method on the TDestinationObjectClient.
+        ///
+        /// This is mainly used to verify the contents of the Object Client.
+        /// </summary>
+        /// <param name="objectClient">The object client to get the Open Read Stream from.</param>
+        /// <returns></returns>
+        protected abstract Task<Stream> OpenReadAsync(TDestinationObjectClient objectClient);
+
+        /// <summary>
+        /// Checks if the Object Client exists.
+        /// </summary>
+        /// <param name="objectClient">Object Client to call exists on.</param>
+        /// <returns></returns>
+        protected abstract Task<bool> ExistsAsync(TDestinationObjectClient objectClient);
+        #endregion
+    }
+}
