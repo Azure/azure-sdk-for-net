22642ccbcdfef126e80c83e47a3f2ed6451a30f6
m-nash <prognash@gmail.com>
2023-10-13 13:39:04 -0700

    customize the access for ImageSize

--- sdk/openai/Azure.AI.OpenAI/src/Generated/ImageSize.cs
+++ sdk/openai/Azure.AI.OpenAI/src/Generated/ImageSize.cs
@@ -11,7 +11,7 @@ using System.ComponentModel;
 namespace Azure.AI.OpenAI
 {
     /// <summary> The desired size of the generated images. Must be one of 256x256, 512x512, or 1024x1024. </summary>
-    internal readonly partial struct ImageSize : IEquatable<ImageSize>
+    public readonly partial struct ImageSize : IEquatable<ImageSize>
     {
         private readonly string _value;
 
