ee12a8245a3a5a6e60ace30bf3a106d1490d9e19
Anne Thompson <annelo@microsoft.com>
2023-10-17 17:07:41 -0700

    Revert "Adjust test projects (#39338)" (#39342)
    This reverts commit 2b576896da7a317576e06d09d8ba261cf03c7afe.

--- sdk/core/System.Net.ClientModel/tests/OpenAIClient/OpenAIClient.cs
+++ sdk/core/System.Net.ClientModel/tests/OpenAIClient/OpenAIClient.cs
@@ -1,20 +1,23 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Azure.Core;
+using Azure.Core.Pipeline;
+using System;
 using System.Net.ClientModel;
-using System.Net.ClientModel.Core.Pipeline;
+using System.Threading;
 
 namespace OpenAI;
 
 public class OpenAIClient
 {
-    private readonly MessagePipeline _pipeline;
+    private readonly HttpPipeline _pipeline;
     private readonly KeyCredential _credential;
 
     public OpenAIClient(KeyCredential credential, OpenAIClientOptions options = default)
     {
         _credential = credential;
-        _pipeline = MessagePipeline.Create(options);
+        _pipeline = HttpPipelineBuilder.Build(new PipelineBuilderOptions());
     }
 
     //public Result<Completions> GetCompletions(string prompt, CancellationToken cancellationToken = default)
@@ -44,5 +47,5 @@ public class OpenAIClient
     //    return Result.FromValue(completions, message.Response);
     //}
 
-    private class PipelineBuilderOptions : PipelineOptions { }
+    private class PipelineBuilderOptions : ClientOptions { }
 }
