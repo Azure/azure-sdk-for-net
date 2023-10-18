8fba4b9a2f7db01dc356f4e459a51c3fde95b68e
Anne Thompson <annelo@microsoft.com>
2023-10-17 17:01:48 -0700

    Revert "Adjust test projects (#39338)"
    This reverts commit 2b576896da7a317576e06d09d8ba261cf03c7afe.

--- sdk/core/System.Net.ClientModel/tests/OpenAIClientTests.cs
+++ sdk/core/System.Net.ClientModel/tests/OpenAIClientTests.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using OpenAI;
+using System.Diagnostics;
+using System.Net.ClientModel;
+using Xunit;
+
 namespace System.Tests;
 
 public class OpenAIClientTests
