4c6bc02bff5581d24af51559744cb479f7182374
Anne Thompson <annelo@microsoft.com>
2023-10-18 13:02:50 -0700

    fix break to clients project

--- sdk/core/System.Net.ClientModel/tests/clients/OpenAIClientTests.cs
+++ sdk/core/System.Net.ClientModel/tests/clients/OpenAIClientTests.cs
@@ -1,13 +1,35 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using NUnit.Framework;
 using OpenAI;
-using System.Diagnostics;
-using System.Net.ClientModel;
-using Xunit;
 
-namespace System.Tests;
+namespace System.Net.ClientModel.Tests;
 
 public class OpenAIClientTests
 {
+    [Test]
+    public void TestClientSync()
+    {
+        string key = Environment.GetEnvironmentVariable("OPENAI_KEY");
+
+        KeyCredential credential = new KeyCredential(key);
+        OpenAIClient client = new OpenAIClient(new Uri("https://api.openai.com/"), credential);
+
+        CompletionsOptions input = new(new string[] { "tell me something about life." })
+        {
+            InternalNonAzureModelName = "text-davinci-003"
+        };
+
+        // TODO: Mock this out.
+        // We don't have test recordings, so we won't actually assert this.
+        // But, if you uncomment this code and run it, the test should pass.
+
+        //Result<Completions> result = client.GetCompletions(
+        //    "<unused in public service>",
+        //    input);
+        //Choice choice = result.Value.Choices[0];
+
+        //Assert.IsTrue(choice.Text.StartsWith("\n\nLife is"));
+    }
 }
