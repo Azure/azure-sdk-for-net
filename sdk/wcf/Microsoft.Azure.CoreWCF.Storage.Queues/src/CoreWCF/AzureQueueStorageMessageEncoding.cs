19745f75735bbd5451f3101a015de14b88c770fb
subhra231 <subhra.nits@gmail.com>
2023-10-19 12:33:27 -0700

    update namespace

--- sdk/wcf/Microsoft.Azure.CoreWCF.Storage.Queues/src/CoreWCF/AzureQueueStorageMessageEncoding.cs
+++ sdk/wcf/Microsoft.Azure.CoreWCF.Storage.Queues/src/CoreWCF/AzureQueueStorageMessageEncoding.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-namespace Azure.Storage.CoreWCF.Channels
+namespace Azure.Storage.CoreWCF
 {
     /// <summary>
     /// The enum which will be used by message encoder.
