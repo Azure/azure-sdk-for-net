55061d881259dcee4e6502b4dcd5c61eed72f307
azure-sdk <azuresdk@microsoft.com>
2023-10-24 08:12:26 +0000

    Update SDK codes co_da_1

--- sdk/connectedvmwarevsphere/Azure.ResourceManager.ConnectedVMwarevSphere/src/Generated/Models/MachineExtensionPatch.Serialization.cs
+++ sdk/connectedvmwarevsphere/Azure.ResourceManager.ConnectedVMwarevSphere/src/Generated/Models/MachineExtensionPatch.Serialization.cs
@@ -59,7 +59,10 @@ namespace Azure.ResourceManager.ConnectedVMwarevSphere.Models
 #if NET6_0_OR_GREATER
 				writer.WriteRawValue(Settings);
 #else
-                JsonSerializer.Serialize(writer, JsonDocument.Parse(Settings.ToString()).RootElement);
+                using (JsonDocument document = JsonDocument.Parse(Settings))
+                {
+                    JsonSerializer.Serialize(writer, document.RootElement);
+                }
 #endif
             }
             if (Optional.IsDefined(ProtectedSettings))
@@ -68,7 +71,10 @@ namespace Azure.ResourceManager.ConnectedVMwarevSphere.Models
 #if NET6_0_OR_GREATER
 				writer.WriteRawValue(ProtectedSettings);
 #else
-                JsonSerializer.Serialize(writer, JsonDocument.Parse(ProtectedSettings.ToString()).RootElement);
+                using (JsonDocument document = JsonDocument.Parse(ProtectedSettings))
+                {
+                    JsonSerializer.Serialize(writer, document.RootElement);
+                }
 #endif
             }
             writer.WriteEndObject();
