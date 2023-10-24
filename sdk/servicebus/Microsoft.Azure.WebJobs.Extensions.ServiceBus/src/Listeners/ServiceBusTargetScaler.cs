bd6bd1d28fc30fb55fe2972cee60aedfcc008821
Vincent Chiang <vchiang@microsoft.com>
2023-10-24 13:58:37 -0400

    Revert "handle overflow"
    This reverts commit dba066189f252936a2d21e0d7406cab95e089358.

--- sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/src/Listeners/ServiceBusTargetScaler.cs
+++ sdk/servicebus/Microsoft.Azure.WebJobs.Extensions.ServiceBus/src/Listeners/ServiceBusTargetScaler.cs
@@ -94,17 +94,7 @@ namespace Microsoft.Azure.WebJobs.Extensions.ServiceBus.Listeners
                 throw new ArgumentOutOfRangeException($"Unexpected concurrency='{concurrency}' - the value must be > 0.");
             }
 
-            int targetWorkerCount;
-
-            try
-            {
-                targetWorkerCount = (int)Math.Ceiling(messageCount / (decimal)concurrency);
-            }
-            catch (OverflowException)
-            {
-                targetWorkerCount = int.MaxValue;
-            }
-
+            int targetWorkerCount = (int)Math.Ceiling(messageCount / (decimal)concurrency);
             _logger.LogInformation($"Target worker count for function '{_functionId}' is '{targetWorkerCount}' (EntityPath='{_entityPath}', MessageCount ='{messageCount}', Concurrency='{concurrency}').");
 
             return new TargetScalerResult
