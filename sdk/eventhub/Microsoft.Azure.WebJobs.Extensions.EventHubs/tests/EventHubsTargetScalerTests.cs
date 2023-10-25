424658d7a271a08aadb84346a17c2b68fc700f9d
Vincent Chiang <vchiang@microsoft.com>
2023-10-25 17:07:44 -0400

    fix merge conflict

--- sdk/eventhub/Microsoft.Azure.WebJobs.Extensions.EventHubs/tests/EventHubsTargetScalerTests.cs
+++ sdk/eventhub/Microsoft.Azure.WebJobs.Extensions.EventHubs/tests/EventHubsTargetScalerTests.cs
@@ -76,19 +76,18 @@ namespace Microsoft.Azure.WebJobs.EventHubs.UnitTests
         }
 
         [Test]
-        // Using concurrency of 10.
-        [TestCase(10, 10, 1)]
-        [TestCase(20, 10, 2)]
-        [TestCase(30, 10, 3)]
-        [TestCase(60, 10, 10)]
-        [TestCase(70, 10, 10)]
-        [TestCase(150, 10, 10)]
-        [TestCase(2147483650, 1, 1)] // Testing eventCount > int.MaxInt is 2147483647
-        [TestCase(21474836500, 2147483647, 2147483647)] // Testing eventCount > int.MaxInt is 2147483647, with default concurrency 10
-        [TestCase(2147483650, 214748365, 214748365)] // Event count is > int.MaxInt, but final target worker count is within bounds
-        public void GetScaleResultInternal_ReturnsExpected(long eventCount, int partitionCount, int expectedTargetWorkerCount)
+        // Using default concurrency of 10.
+        [TestCase(10, 10, null, 1)]
+        [TestCase(20, 10, null, 2)]
+        [TestCase(30, 10, null, 3)]
+        [TestCase(60, 10, null, 10)]
+        [TestCase(70, 10, null, 10)]
+        [TestCase(150, 10, null, 10)]
+        [TestCase(2147483650, 1, 1, 1)] // Testing eventCount > int.MaxInt is 2147483647
+        [TestCase(21474836500, 1000, 1, 1000)] // Testing eventCount > int.MaxInt is 2147483647, with concurrency 1 to force overflow
+        public void GetScaleResultInternal_ReturnsExpected(long eventCount, int partitionCount, int? concurrency, int expectedTargetWorkerCount)
         {
-            TargetScalerResult result = _targetScaler.GetScaleResultInternal(new TargetScalerContext { InstanceConcurrency = 10 }, eventCount, partitionCount);
+            TargetScalerResult result = _targetScaler.GetScaleResultInternal(new TargetScalerContext { InstanceConcurrency = concurrency }, eventCount, partitionCount);
             Assert.AreEqual(expectedTargetWorkerCount, result.TargetWorkerCount);
         }
 
