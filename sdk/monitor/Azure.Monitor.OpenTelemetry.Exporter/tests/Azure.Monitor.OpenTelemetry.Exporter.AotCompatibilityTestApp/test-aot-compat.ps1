95accc4f7816d27d75bcdbd69d4b584be8ad37d9
Timothy Mothra Lee <tilee@microsoft.com>
2023-10-26 13:53:50 -0700

    update warning count.

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.AotCompatibilityTestApp/test-aot-compat.ps1
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/tests/Azure.Monitor.OpenTelemetry.Exporter.AotCompatibilityTestApp/test-aot-compat.ps1
@@ -24,22 +24,10 @@ foreach ($line in $($publishOutput -split "`r`n"))
 }
 
 Write-Host "Actual warning count is:", $actualWarningCount
-$expectedWarningCount = 16
+$expectedWarningCount = 4
 # Known warnings:
 # - IL2026: Azure.Core.Serialization.DynamicData: Using member 'Azure.Core.Serialization.DynamicData.DynamicDataJsonConverter.DynamicDataJsonConverter()' which has 'RequiresUnreferencedCodeAttribute'
 # - IL3050: Azure.Core.Serialization.DynamicData: Using member 'Azure.Core.Serialization.DynamicData.DynamicDataJsonConverter.DynamicDataJsonConverter()' which has 'RequiresDynamicCodeAttribute'
-# - AzureCoreEventSource.cs(61): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.Request(String,String,String,String,String): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(83): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.RequestContent(String,Byte[]): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(104): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.Response(String,Int32,String,String,Double): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(126): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.ResponseContent(String,Byte[]): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(154): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.ResponseContentBlock(String,Int32,Byte[]): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(160): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.ResponseContentTextBlock(String,Int32,String): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(175): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.ErrorResponse(String,Int32,String,String,Double): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(197): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.ErrorResponseContent(String,Byte[]): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(225): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.ErrorResponseContentBlock(String,Int32,Byte[]): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(231): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.ErrorResponseContentTextBlock(String,Int32,String): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(244): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.ResponseDelay(String,Double): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
-# - AzureCoreEventSource.cs(265): Trim analysis warning IL2026: Azure.Core.Diagnostics.AzureCoreEventSource.RequestRedirect(String,String,String,Int32): Using member 'System.Diagnostics.Tracing.EventSource.WriteEvent(Int32,Object[])' which has 'RequiresUnreferencedCodeAttribute'
 # - IL2026: Azure.Core.Json.MutableJsonDocument: Using member 'Azure.Core.Json.MutableJsonDocument.MutableJsonDocumentConverter.MutableJsonDocumentConverter()' which has 'RequiresUnreferencedCodeAttribute'
 # - IL3050: Azure.Core.Json.MutableJsonDocument: Using member 'Azure.Core.Json.MutableJsonDocument.MutableJsonDocumentConverter.MutableJsonDocumentConverter()' which has 'RequiresDynamicCodeAttribute'
 
