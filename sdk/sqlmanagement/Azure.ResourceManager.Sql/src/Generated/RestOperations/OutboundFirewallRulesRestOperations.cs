80572c66255f703468122f4844ffaabdf3be0392
azure-sdk <azuresdk@microsoft.com>
2023-10-27 22:33:37 +0000

    Update SDK codes pu_sq_5

--- sdk/sqlmanagement/Azure.ResourceManager.Sql/src/Generated/RestOperations/OutboundFirewallRulesRestOperations.cs
+++ sdk/sqlmanagement/Azure.ResourceManager.Sql/src/Generated/RestOperations/OutboundFirewallRulesRestOperations.cs
@@ -156,7 +156,7 @@ namespace Azure.ResourceManager.Sql
         /// <param name="resourceGroupName"> The name of the resource group that contains the resource. You can obtain this value from the Azure Resource Manager API or the portal. </param>
         /// <param name="serverName"> The name of the server. </param>
         /// <param name="outboundRuleFqdn"> The String to use. </param>
-        /// <param name="data"> The OutboundFirewallRule to use. </param>
+        /// <param name="data"> The OutboundFirewallRuleData to use. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, <paramref name="serverName"/>, <paramref name="outboundRuleFqdn"/> or <paramref name="data"/> is null. </exception>
         /// <exception cref="ArgumentException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, <paramref name="serverName"/> or <paramref name="outboundRuleFqdn"/> is an empty string, and was expected to be non-empty. </exception>
@@ -186,7 +186,7 @@ namespace Azure.ResourceManager.Sql
         /// <param name="resourceGroupName"> The name of the resource group that contains the resource. You can obtain this value from the Azure Resource Manager API or the portal. </param>
         /// <param name="serverName"> The name of the server. </param>
         /// <param name="outboundRuleFqdn"> The String to use. </param>
-        /// <param name="data"> The OutboundFirewallRule to use. </param>
+        /// <param name="data"> The OutboundFirewallRuleData to use. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, <paramref name="serverName"/>, <paramref name="outboundRuleFqdn"/> or <paramref name="data"/> is null. </exception>
         /// <exception cref="ArgumentException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, <paramref name="serverName"/> or <paramref name="outboundRuleFqdn"/> is an empty string, and was expected to be non-empty. </exception>
