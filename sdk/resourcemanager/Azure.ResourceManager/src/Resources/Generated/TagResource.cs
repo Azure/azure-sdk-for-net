f134406031061ed8aae5a733589a223771635164
azure-sdk <azuresdk@microsoft.com>
2023-10-29 03:54:26 +0000

    Update SDK codes pu_sq_5

--- sdk/resourcemanager/Azure.ResourceManager/src/Resources/Generated/TagResource.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/Resources/Generated/TagResource.cs
@@ -309,7 +309,7 @@ namespace Azure.ResourceManager.Resources
         /// </list>
         /// </summary>
         /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
-        /// <param name="data"> The TagResource to use. </param>
+        /// <param name="data"> The TagResourceData to use. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="data"/> is null. </exception>
         public virtual async Task<ArmOperation<TagResource>> CreateOrUpdateAsync(WaitUntil waitUntil, TagResourceData data, CancellationToken cancellationToken = default)
@@ -347,7 +347,7 @@ namespace Azure.ResourceManager.Resources
         /// </list>
         /// </summary>
         /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
-        /// <param name="data"> The TagResource to use. </param>
+        /// <param name="data"> The TagResourceData to use. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="data"/> is null. </exception>
         public virtual ArmOperation<TagResource> CreateOrUpdate(WaitUntil waitUntil, TagResourceData data, CancellationToken cancellationToken = default)
--- sdk/search/Azure.Search.Documents/src/Generated/Models/UnknownScoringFunction.cs
+++ sdk/search/Azure.Search.Documents/src/Generated/Models/UnknownScoringFunction.cs
@@ -5,9 +5,7 @@
 
 #nullable disable
 
-using Azure.Search.Documents.Indexes.Models;
-
-namespace Azure.Search.Documents.Models
+namespace Azure.Search.Documents.Indexes.Models
 {
     /// <summary> The UnknownScoringFunction. </summary>
     internal partial class UnknownScoringFunction : ScoringFunction
