1b53203fd55c3cfbe668933a997567ebaca79cdd
Jose Arriaga Maldonado <josar@microsoft.com>
2023-10-30 11:04:38 -0700

    Change representation of embeddings from IReadOnlyList<float> to ReadOnlyMemory<float>

---
+++ sdk/openai/Azure.AI.OpenAI/tests/Samples/Sample11_Embeddings.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using NUnit.Framework;
+
+namespace Azure.AI.OpenAI.Tests.Samples
+{
+    public partial class EmbeddingsSamples
+    {
+        [Test]
+        [Ignore("Only verifying that the sample builds")]
+        public async Task GenerateEmbeddings()
+        {
+            string endpoint = "https://myaccount.openai.azure.com/";
+            var client = new OpenAIClient(new Uri(endpoint), new DefaultAzureCredential());
+
+            #region Snippet:GenerateEmbeddings
+            string deploymentOrModelName = "text-embedding-ada-002";
+            EmbeddingsOptions embeddingsOptions = new("Your text string goes here");
+            Response<Embeddings> response = await client.GetEmbeddingsAsync(deploymentOrModelName, embeddingsOptions);
+
+            // The response includes the generated embedding.
+            EmbeddingItem item = response.Value.Data[0];
+            ReadOnlyMemory<float> embedding = item.Embedding;
+            #endregion
+        }
+    }
+}
