275f09c205bd89c1efe3737dbc1fb312103bf7e2
mcgallan <1186003288@qq.com>
2023-11-06 14:31:13 +0800

    Update TestEnvironment.cs

--- sdk/core/Azure.Core.TestFramework/src/TestEnvironment.cs
+++ sdk/core/Azure.Core.TestFramework/src/TestEnvironment.cs
@@ -1,11 +1,29 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.Core.TestFramework;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Identity;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Azure.Core.Pipeline;
+using NUnit.Framework;
 
-namespace Azure.ResourceManager.ApplicationInsights.Tests
+namespace Azure.Core.TestFramework
 {
-    public class ApplicationInsightsManagementTestEnvironment : TestEnvironment
+    /// <summary>
+    ///   Represents the ambient environment in which the test suite is
+    ///   being run.
+    /// </summary>
+    public abstract class TestEnvironment
     {
     }
 }
