3675efedcdc8e72718f7bf9aea0529c3008fdf90
m-nash <prognash@gmail.com>
2023-11-08 12:20:57 -0800

    address feedback

--- sdk/core/System.Net.ClientModel/src/ModelReaderWriter/IJsonModel.cs
+++ sdk/core/System.Net.ClientModel/src/ModelReaderWriter/IJsonModel.cs
@@ -17,7 +17,7 @@ namespace System.Net.ClientModel.Core
         /// <param name="writer">The <see cref="Utf8JsonWriter"/> to write into.</param>
         /// <param name="options">The <see cref="ModelReaderWriterOptions"/> to use.</param>
         /// <exception cref="FormatException">If the model does not support the requested <see cref="ModelReaderWriterOptions.Format"/>.</exception>
-        ///// <exception cref="InvalidOperationException">If <see cref="ModelReaderWriterFormat.Wire"/> format is passed in and the model does not use JSON for its wire format.</exception>
+        /// <exception cref="InvalidOperationException">If <see cref="ModelReaderWriterOptions.Wire"/> format is passed in and the model does not use JSON for its wire format.</exception>
         void Write(Utf8JsonWriter writer, ModelReaderWriterOptions options);
 
         /// <summary>
@@ -27,7 +27,7 @@ namespace System.Net.ClientModel.Core
         /// <param name="options">The <see cref="ModelReaderWriterOptions"/> to use.</param>
         /// <returns>A <typeparamref name="T"/> representation of the JSON value.</returns>
         /// <exception cref="FormatException">If the model does not support the requested <see cref="ModelReaderWriterOptions.Format"/>.</exception>
-        ///// <exception cref="InvalidOperationException">If <see cref="ModelReaderWriterFormat.Wire"/> format is passed in and the model does not use JSON for its wire format.</exception>
+        /// <exception cref="InvalidOperationException">If <see cref="ModelReaderWriterOptions.Wire"/> format is passed in and the model does not use JSON for its wire format.</exception>
         T Read(ref Utf8JsonReader reader, ModelReaderWriterOptions options);
     }
 }
