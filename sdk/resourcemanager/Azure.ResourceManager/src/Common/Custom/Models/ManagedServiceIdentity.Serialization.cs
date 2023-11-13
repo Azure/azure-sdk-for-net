Azure/azure-sdk-for-net

commit 1efa389b67e5708f7aaa0a4208ed5b78a1155534

Refine customized code in preparation for public serialization (#39753)

sdk/resourcemanager/Azure.ResourceManager/src/Common/Custom/Models/ManagedServiceIdentity.Serialization.cs
@@ -15,15 +15,16 @@ namespace Azure.ResourceManager.Models
     [JsonConverter(typeof(ManagedServiceIdentityConverter))]
     public partial class ManagedServiceIdentity
     {
-        internal static void Write(Utf8JsonWriter writer, ManagedServiceIdentity model, JsonSerializerOptions options = default)
+        internal void Write(Utf8JsonWriter writer, JsonSerializerOptions options = default)
         {
             writer.WriteStartObject();
-            JsonSerializer.Serialize(writer, model.ManagedServiceIdentityType, options);
-            if (Optional.IsCollectionDefined(model.UserAssignedIdentities))
+            writer.WritePropertyName("type");
+            JsonSerializer.Serialize(writer, ManagedServiceIdentityType, options);
+            if (Optional.IsCollectionDefined(UserAssignedIdentities))
             {
                 writer.WritePropertyName("userAssignedIdentities"u8);
                 writer.WriteStartObject();
-                foreach (var item in model.UserAssignedIdentities)
+                foreach (var item in UserAssignedIdentities)
                 {
                     writer.WritePropertyName(item.Key);
                     if (item.Value == null)
@@ -70,7 +71,7 @@ namespace Azure.ResourceManager.Models
                 }
                 if (property.NameEquals("type"u8))
                 {
-                    type = JsonSerializer.Deserialize<ManagedServiceIdentityType>("{"+property.ToString()+"}", options);
+                    type = JsonSerializer.Deserialize<ManagedServiceIdentityType>(property.Value.GetRawText(), options);
                     continue;
                 }
                 if (property.NameEquals("userAssignedIdentities"u8))
@@ -96,7 +97,7 @@ namespace Azure.ResourceManager.Models
         {
             public override void Write(Utf8JsonWriter writer, ManagedServiceIdentity model, JsonSerializerOptions options)
             {
-                ManagedServiceIdentity.Write(writer, model, options);
+                model.Write(writer, options);
             }
             public override ManagedServiceIdentity Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             {

sdk/resourcemanager/Azure.ResourceManager/src/Common/Custom/Models/ManagedServiceIdentityType.cs
@@ -17,17 +17,12 @@ namespace Azure.ResourceManager.Models
         {
             public override void Write(Utf8JsonWriter writer, ManagedServiceIdentityType model, JsonSerializerOptions options)
             {
-                writer.WritePropertyName("type");
                 writer.WriteStringValue(model.ToString());
             }
             public override ManagedServiceIdentityType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             {
                 using var document = JsonDocument.ParseValue(ref reader);
-                foreach (var property in document.RootElement.EnumerateObject())
-                {
-                    return new ManagedServiceIdentityType(property.Value.GetString());
-                }
-                return null;
+                return new ManagedServiceIdentityType(document.RootElement.GetString());
             }
         }
     }

sdk/resourcemanager/Azure.ResourceManager/src/Shared/ManagedServiceIdentityTypeV3Converter.cs
@@ -20,7 +20,6 @@ namespace Azure.ResourceManager.Models
         /// <param name="options"> The options for JsonSerializer. </param>
         public override void Write(Utf8JsonWriter writer, ManagedServiceIdentityType model, JsonSerializerOptions options)
         {
-            writer.WritePropertyName("type");
             if (model == ManagedServiceIdentityType.SystemAssignedUserAssigned)
             {
                 writer.WriteStringValue(SystemAssignedUserAssignedV3Value);
@@ -38,16 +37,12 @@ namespace Azure.ResourceManager.Models
         public override ManagedServiceIdentityType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             using var document = JsonDocument.ParseValue(ref reader);
-            foreach (var property in document.RootElement.EnumerateObject())
+            var typeValue = document.RootElement.GetString();
+            if (typeValue.Equals(SystemAssignedUserAssignedV3Value, StringComparison.OrdinalIgnoreCase))
             {
-                var typeValue = property.Value.GetString();
-                if (typeValue.Equals(SystemAssignedUserAssignedV3Value, StringComparison.OrdinalIgnoreCase))
-                {
-                    return ManagedServiceIdentityType.SystemAssignedUserAssigned;
-                }
-                return new ManagedServiceIdentityType(typeValue);
+                return ManagedServiceIdentityType.SystemAssignedUserAssigned;
             }
-            return null;
+            return new ManagedServiceIdentityType(typeValue);
         }
     }
 }

sdk/compute/Azure.ResourceManager.Compute/src/Customize/Models/ComputeResourceSkuLocationInfo.Serialization.cs
@@ -1,93 +1,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#nullable disable
-
-using System.Collections.Generic;
-using System.Text.Json;
-using Azure.Core;
-
-namespace Azure.ResourceManager.Compute.Models
-{
-    // TEMPORARY: this piece of customized code replaces the ExtendedLocationType with the one in resourcemanager
-    public partial class ComputeResourceSkuLocationInfo
-    {
-        internal static ComputeResourceSkuLocationInfo DeserializeComputeResourceSkuLocationInfo(JsonElement element)
-        {
-            Optional<AzureLocation> location = default;
-            Optional<IReadOnlyList<string>> zones = default;
-            Optional<IReadOnlyList<ComputeResourceSkuZoneDetails>> zoneDetails = default;
-            Optional<IReadOnlyList<string>> extendedLocations = default;
-            Optional<Azure.ResourceManager.Resources.Models.ExtendedLocationType> type = default;
-            foreach (var property in element.EnumerateObject())
-            {
-                if (property.NameEquals("location"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    location = new AzureLocation(property.Value.GetString());
-                    continue;
-                }
-                if (property.NameEquals("zones"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    List<string> array = new List<string>();
-                    foreach (var item in property.Value.EnumerateArray())
-                    {
-                        array.Add(item.GetString());
-                    }
-                    zones = array;
-                    continue;
-                }
-                if (property.NameEquals("zoneDetails"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    List<ComputeResourceSkuZoneDetails> array = new List<ComputeResourceSkuZoneDetails>();
-                    foreach (var item in property.Value.EnumerateArray())
-                    {
-                        array.Add(ComputeResourceSkuZoneDetails.DeserializeComputeResourceSkuZoneDetails(item));
-                    }
-                    zoneDetails = array;
-                    continue;
-                }
-                if (property.NameEquals("extendedLocations"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    List<string> array = new List<string>();
-                    foreach (var item in property.Value.EnumerateArray())
-                    {
-                        array.Add(item.GetString());
-                    }
-                    extendedLocations = array;
-                    continue;
-                }
-                if (property.NameEquals("type"))
-                {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        property.ThrowNonNullablePropertyIsNull();
-                        continue;
-                    }
-                    type = new Azure.ResourceManager.Resources.Models.ExtendedLocationType(property.Value.GetString());
-                    continue;
-                }
-            }
-            return new ComputeResourceSkuLocationInfo(Optional.ToNullable(location), Optional.ToList(zones), Optional.ToList(zoneDetails), Optional.ToList(extendedLocations), Optional.ToNullable(type));
-        }
-    }
-}

sdk/compute/Azure.ResourceManager.Compute/src/Customize/Models/ComputeResourceSkuLocationInfo.cs
@@ -3,6 +3,8 @@
 
 #nullable disable
 
+using System.Runtime.CompilerServices;
+using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.Compute.Models
@@ -12,6 +14,23 @@ namespace Azure.ResourceManager.Compute.Models
     {
         /// <summary> The type of the extended location. </summary>
         [CodeGenMember("ExtendedLocationType")]
+        [CodeGenMemberSerializationHooks(SerializationValueHook = nameof(WriteExtendedLocationType), DeserializationValueHook = nameof(ReadExtendedLocationType))]
         public Azure.ResourceManager.Resources.Models.ExtendedLocationType? ExtendedLocationType { get; }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void WriteExtendedLocationType(Utf8JsonWriter writer)
+        {
+            writer.WriteStringValue(ExtendedLocationType.Value.ToString());
+        }
+
+        // deserialization hook for required property
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void ReadExtendedLocationType(JsonProperty property, ref Optional<Azure.ResourceManager.Resources.Models.ExtendedLocationType> type)
+        {
+            if (property.Value.ValueKind == JsonValueKind.Null)
+                return;
+
+            type = new Azure.ResourceManager.Resources.Models.ExtendedLocationType(property.Value.GetString());
+        }
     }
 }

sdk/compute/Azure.ResourceManager.Compute/src/Generated/Models/ComputeResourceSkuLocationInfo.Serialization.cs
@@ -5,9 +5,86 @@
 
 #nullable disable
 
+using System.Collections.Generic;
+using System.Text.Json;
+using Azure.Core;
+using Azure.ResourceManager.Resources.Models;
+
 namespace Azure.ResourceManager.Compute.Models
 {
     public partial class ComputeResourceSkuLocationInfo
     {
+        internal static ComputeResourceSkuLocationInfo DeserializeComputeResourceSkuLocationInfo(JsonElement element)
+        {
+            if (element.ValueKind == JsonValueKind.Null)
+            {
+                return null;
+            }
+            Optional<AzureLocation> location = default;
+            Optional<IReadOnlyList<string>> zones = default;
+            Optional<IReadOnlyList<ComputeResourceSkuZoneDetails>> zoneDetails = default;
+            Optional<IReadOnlyList<string>> extendedLocations = default;
+            Optional<ExtendedLocationType> type = default;
+            foreach (var property in element.EnumerateObject())
+            {
+                if (property.NameEquals("location"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
+                    location = new AzureLocation(property.Value.GetString());
+                    continue;
+                }
+                if (property.NameEquals("zones"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
+                    List<string> array = new List<string>();
+                    foreach (var item in property.Value.EnumerateArray())
+                    {
+                        array.Add(item.GetString());
+                    }
+                    zones = array;
+                    continue;
+                }
+                if (property.NameEquals("zoneDetails"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
+                    List<ComputeResourceSkuZoneDetails> array = new List<ComputeResourceSkuZoneDetails>();
+                    foreach (var item in property.Value.EnumerateArray())
+                    {
+                        array.Add(ComputeResourceSkuZoneDetails.DeserializeComputeResourceSkuZoneDetails(item));
+                    }
+                    zoneDetails = array;
+                    continue;
+                }
+                if (property.NameEquals("extendedLocations"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
+                    List<string> array = new List<string>();
+                    foreach (var item in property.Value.EnumerateArray())
+                    {
+                        array.Add(item.GetString());
+                    }
+                    extendedLocations = array;
+                    continue;
+                }
+                if (property.NameEquals("type"u8))
+                {
+                    ReadExtendedLocationType(property, ref type);
+                    continue;
+                }
+            }
+            return new ComputeResourceSkuLocationInfo(Optional.ToNullable(location), Optional.ToList(zones), Optional.ToList(zoneDetails), Optional.ToList(extendedLocations), Optional.ToNullable(type));
+        }
     }
 }

sdk/databox/Azure.ResourceManager.DataBox/src/Custom/Models/MitigateJobContent.Serialization.cs
@@ -1,38 +1,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#nullable disable
-
-using System.Text.Json;
-using Azure.Core;
-
-namespace Azure.ResourceManager.DataBox.Models
-{
-    public partial class MitigateJobContent : IUtf8JsonSerializable
-    {
-        void global::Azure.Core.IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
-        {
-            writer.WriteStartObject();
-            if (Optional.IsDefined(_customerResolutionCode))
-            {
-                writer.WritePropertyName("customerResolutionCode"u8);
-                writer.WriteStringValue(_customerResolutionCode.Value.ToSerialString());
-            }
-            writer.WritePropertyName("customerResolutionCode"u8);
-
-            writer.WriteStringValue(CustomerResolutionCode.ToSerialString());
-            if (Optional.IsCollectionDefined(SerialNumberCustomerResolutionMap))
-            {
-                writer.WritePropertyName("serialNumberCustomerResolutionMap"u8);
-                writer.WriteStartObject();
-                foreach (var item in SerialNumberCustomerResolutionMap)
-                {
-                    writer.WritePropertyName(item.Key);
-                    writer.WriteStringValue(item.Value.ToSerialString());
-                }
-                writer.WriteEndObject();
-            }
-            writer.WriteEndObject();
-        }
-    }
-}

sdk/databox/Azure.ResourceManager.DataBox/src/Custom/Models/MitigateJobContent.cs
@@ -1,14 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Runtime.CompilerServices;
+using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.DataBox.Models
 {
     public partial class MitigateJobContent
     {
-        // This is used to represent default null for breaking change of CustomerResolutionCode changed from requried to optional
-        internal CustomerResolutionCode? _customerResolutionCode;
+        // This is used to represent default null for breaking change of CustomerResolutionCode changed from required to optional
+        private CustomerResolutionCode? _customerResolutionCode;
 
         /// <summary> Initializes a new instance of MitigateJobContent. </summary>
         public MitigateJobContent()
@@ -20,10 +22,20 @@ namespace Azure.ResourceManager.DataBox.Models
         /// <param name="customerResolutionCode"> Resolution code for the job. </param>
         public MitigateJobContent(CustomerResolutionCode customerResolutionCode) : this()
         {
-            _customerResolutionCode = customerResolutionCode;
+            CustomerResolutionCode = customerResolutionCode;
         }
 
         /// <summary> Resolution code for the job. </summary>
-        public CustomerResolutionCode CustomerResolutionCode => _customerResolutionCode ?? default;
+        public CustomerResolutionCode CustomerResolutionCode
+        {
+            get
+            {
+                return _customerResolutionCode ?? default;
+            }
+            private set
+            {
+                _customerResolutionCode = value;
+            }
+        }
     }
 }

commit f4ca255e6d5d27e73efca52593e84bd86858a0b1

revert the breaking changes introduced by #39753

sdk/resourcemanager/Azure.ResourceManager/src/Common/Custom/Models/ManagedServiceIdentity.Serialization.cs
@@ -18,7 +18,6 @@ namespace Azure.ResourceManager.Models
         internal void Write(Utf8JsonWriter writer, JsonSerializerOptions options = default)
         {
             writer.WriteStartObject();
-            writer.WritePropertyName("type");
             JsonSerializer.Serialize(writer, ManagedServiceIdentityType, options);
             if (Optional.IsCollectionDefined(UserAssignedIdentities))
             {
@@ -71,7 +70,7 @@ namespace Azure.ResourceManager.Models
                 }
                 if (property.NameEquals("type"u8))
                 {
-                    type = JsonSerializer.Deserialize<ManagedServiceIdentityType>(property.Value.GetRawText(), options);
+                    type = JsonSerializer.Deserialize<ManagedServiceIdentityType>($"{{{property}}}", options);
                     continue;
                 }
                 if (property.NameEquals("userAssignedIdentities"u8))

sdk/resourcemanager/Azure.ResourceManager/src/Common/Custom/Models/ManagedServiceIdentityType.cs
@@ -17,12 +17,17 @@ namespace Azure.ResourceManager.Models
         {
             public override void Write(Utf8JsonWriter writer, ManagedServiceIdentityType model, JsonSerializerOptions options)
             {
+                writer.WritePropertyName("type");
                 writer.WriteStringValue(model.ToString());
             }
             public override ManagedServiceIdentityType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             {
                 using var document = JsonDocument.ParseValue(ref reader);
-                return new ManagedServiceIdentityType(document.RootElement.GetString());
+                foreach (var property in document.RootElement.EnumerateObject())
+                {
+                    return new ManagedServiceIdentityType(property.Value.GetString());
+                }
+                return null;
             }
         }
     }

sdk/resourcemanager/Azure.ResourceManager/src/Shared/ManagedServiceIdentityTypeV3Converter.cs
@@ -20,6 +20,7 @@ namespace Azure.ResourceManager.Models
         /// <param name="options"> The options for JsonSerializer. </param>
         public override void Write(Utf8JsonWriter writer, ManagedServiceIdentityType model, JsonSerializerOptions options)
         {
+            writer.WritePropertyName("type");
             if (model == ManagedServiceIdentityType.SystemAssignedUserAssigned)
             {
                 writer.WriteStringValue(SystemAssignedUserAssignedV3Value);
@@ -37,12 +38,16 @@ namespace Azure.ResourceManager.Models
         public override ManagedServiceIdentityType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             using var document = JsonDocument.ParseValue(ref reader);
-            var typeValue = document.RootElement.GetString();
-            if (typeValue.Equals(SystemAssignedUserAssignedV3Value, StringComparison.OrdinalIgnoreCase))
+            foreach (var property in document.RootElement.EnumerateObject())
             {
-                return ManagedServiceIdentityType.SystemAssignedUserAssigned;
+                var typeValue = property.Value.GetString();
+                if (typeValue.Equals(SystemAssignedUserAssignedV3Value, StringComparison.OrdinalIgnoreCase))
+                {
+                    return ManagedServiceIdentityType.SystemAssignedUserAssigned;
+                }
+                return new ManagedServiceIdentityType(typeValue);
             }
-            return new ManagedServiceIdentityType(typeValue);
+            return null;
         }
     }
 }