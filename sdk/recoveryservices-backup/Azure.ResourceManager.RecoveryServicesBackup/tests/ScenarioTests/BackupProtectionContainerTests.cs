5954e4ea9f2a636310b25b5488384dc76e2d6ac6
Arthur Ma <mars.arthur@263.net>
2023-11-16 11:35:36 +0800

    Update

--- sdk/recoveryservices-backup/Azure.ResourceManager.RecoveryServicesBackup/tests/ScenarioTests/BackupProtectionContainerTests.cs
+++ sdk/recoveryservices-backup/Azure.ResourceManager.RecoveryServicesBackup/tests/ScenarioTests/BackupProtectionContainerTests.cs
@@ -20,7 +20,7 @@ namespace Azure.ResourceManager.RecoveryServicesBackup.Tests
         {
         }
 
-        [Test]
+        [RecordedTest]
         public async Task CreateTest()
         {
             var sub = await Client.GetDefaultSubscriptionAsync();
