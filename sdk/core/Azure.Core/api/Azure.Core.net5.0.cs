042a3dcda5e9d1341fe14146ed643964fe4d6814
George Pollard <gpollard@microsoft.com>
2023-11-21 10:35:41 +1300

    Update generated code

--- sdk/core/Azure.Core/api/Azure.Core.net5.0.cs
+++ sdk/core/Azure.Core/api/Azure.Core.net5.0.cs
@@ -267,7 +267,7 @@ namespace Azure
         protected Response() { }
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         public override bool HasValue { get { throw null; } }
-        public abstract override T Value { get { throw null; } }
+        public abstract override T Value { get; }
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         public override bool Equals(object? obj) { throw null; }
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
