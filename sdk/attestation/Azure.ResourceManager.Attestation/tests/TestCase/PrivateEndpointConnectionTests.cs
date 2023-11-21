2ab34d71fdd951bd8e7b4cbbf42a5d8b92b8221e
mcgallan <1186003288@qq.com>
2023-11-21 16:17:56 +0800

    update

--- sdk/attestation/Azure.ResourceManager.Attestation/tests/TestCase/PrivateEndpointConnectionTests.cs
+++ sdk/attestation/Azure.ResourceManager.Attestation/tests/TestCase/PrivateEndpointConnectionTests.cs
@@ -1,20 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Azure.ResourceManager.AppConfiguration;
-using Azure.ResourceManager.AppConfiguration.Models;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Attestation.Tests.Helpers;
 using Azure.ResourceManager.Network;
 using Azure.ResourceManager.Network.Models;
-using Azure.Core.TestFramework;
-using Azure.ResourceManager.Attestation.Models;
-using Azure.ResourceManager.Attestation.Tests.Helpers;
-using NUnit.Framework;
-using Azure.Core;
 using Azure.ResourceManager.Resources;
-using System.Linq;
+using NUnit.Framework;
 
 namespace Azure.ResourceManager.Attestation.Tests
 {
