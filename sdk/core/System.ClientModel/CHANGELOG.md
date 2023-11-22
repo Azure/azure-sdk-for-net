8b96eb1c920d290bf78017228ead7d080f69b341
m-nash <64171366+m-nash@users.noreply.github.com>
2023-11-22 10:18:57 -0800

    Initial move to separate model reader writer in System.ClientModel (#39478)
    * initial move to separate model reader writer in System.Net.ClientModel
    
    * update api
    
    * update doc settings for readme
    
    * address pr feedback
    
    * first cut at readme / changelog
    
    * fix perf issue in helper methods
    
    * add perf for srcgen
    
    * address feedback
    
    * update IModel to have a GetWireFormat
    
    * update api
    
    * update ci
    
    * remove GetOptions
    
    * WIP on new format changes (#39817)
    
    * wip on new format changes
    
    * more tweaks to the API
    
    * impl tweaks
    
    * address feedback
    
    * update api
    
    * fix tests
    
    * address feedback
    
    * rename based on feedback (#39885)
    
    * Remove wire (#40021)
    
    * remove wire
    
    * Update sdk/core/Azure.Core/tests/common/Internal/ModelReaderWriterHelper.cs
    
    Co-authored-by: Anne Thompson <annelo@microsoft.com>
    
    * revert nullability since azure core tests don't support this right now
    
    ---------
    
    Co-authored-by: Anne Thompson <annelo@microsoft.com>
    
    * Rename package (#40030)
    
    * rename package
    
    * comment out bad test
    
    * feedback
    
    * more feedback
    
    * update basic sample
    
    * remove next steps
    
    * update docs
    
    * address feedback
    
    * remove unused frozen code
    
    * address feedback
    
    * address additional feedback
    
    * update ci.yml
    
    * move file inside mrw folder
    
    * Add exception documentation
    
    * move helper file
    
    * update comment
    
    * readme feedback
    
    * follow static field convention
    
    * remove unecessary interlock
    
    * add azure sdk analyzers back
    
    * throw when cancelled
    
    * remove unused #if
    
    * remove azureicon
    
    * update readme
    
    * fix package icon
    
    * remove unnecessary conditions
    
    ---------
    
    Co-authored-by: Anne Thompson <annelo@microsoft.com>

---
+++ sdk/core/System.ClientModel/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Release History
+
+## 1.0.0-beta.1 (Unreleased)
+
+### Features Added
+
+- ModelReaderWriter APIs for reading and writing models in different formats.
---
+++ sdk/core/System.ClientModel/tests/internal.perf/ModelReaderWriter/AvailabilitySetDataModel.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json;
+using System.ClientModel.Tests.Client.Models.ResourceManager.Compute;
+
+namespace System.ClientModel.Tests.Internal.Perf
+{
+    public class AvailabilitySetDataModel : JsonModelBenchmark<AvailabilitySetData>
+    {
+        protected override AvailabilitySetData Read(JsonElement jsonElement)
+            => AvailabilitySetData.DeserializeAvailabilitySetData(jsonElement, _options);
+
+        protected override string JsonFileName => "AvailabilitySetData/AvailabilitySetData.json";
+    }
+}
