0c3d6c4ebdf6ab639c25194bc029efdb55f9371a
Anne Thompson <annelo@microsoft.com>
2023-11-27 08:50:05 -0800

    Tidy up after internals reshuffling (#40165)

--- sdk/core/System.ClientModel/src/Internal/HttpClientPipelineTransport.cs
+++ sdk/core/System.ClientModel/src/Internal/HttpClientPipelineTransport.cs
@@ -7,7 +7,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace System.ClientModel.Internal.Primitives;
+namespace System.ClientModel.Internal;
 
 internal class HttpClientPipelineTransport : PipelineTransport
 {
