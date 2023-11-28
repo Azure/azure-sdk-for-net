d4ef6eb024cf5a1ac2e805bd0740f5d3fff89970
azure-sdk <azuresdk@microsoft.com>
2023-11-28 10:14:08 +0000

    Update SDK codes co_da_1

--- sdk/datafactory/Azure.ResourceManager.DataFactory/src/Generated/Models/CustomDataset.Serialization.cs
+++ sdk/datafactory/Azure.ResourceManager.DataFactory/src/Generated/Models/CustomDataset.Serialization.cs
@@ -87,17 +87,20 @@ namespace Azure.ResourceManager.DataFactory.Models
                 writer.WritePropertyName("folder"u8);
                 writer.WriteObjectValue(Folder);
             }
-            foreach (var item in AdditionalProperties)
+            if (AdditionalProperties != null)
             {
-                writer.WritePropertyName(item.Key);
+                foreach (var item in AdditionalProperties)
+                {
+                    writer.WritePropertyName(item.Key);
 #if NET6_0_OR_GREATER
 				writer.WriteRawValue(item.Value);
 #else
-                using (JsonDocument document = JsonDocument.Parse(item.Value))
-                {
-                    JsonSerializer.Serialize(writer, document.RootElement);
-                }
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
 #endif
+                }
             }
             writer.WriteEndObject();
         }
--- sdk/datafactory/Azure.ResourceManager.DataFactory/src/Generated/Models/HiveLinkedService.Serialization.cs
+++ sdk/datafactory/Azure.ResourceManager.DataFactory/src/Generated/Models/HiveLinkedService.Serialization.cs
@@ -145,17 +145,20 @@ namespace Azure.ResourceManager.DataFactory.Models
                 writer.WriteStringValue(EncryptedCredential);
             }
             writer.WriteEndObject();
-            foreach (var item in AdditionalProperties)
+            if (AdditionalProperties != null)
             {
-                writer.WritePropertyName(item.Key);
+                foreach (var item in AdditionalProperties)
+                {
+                    writer.WritePropertyName(item.Key);
 #if NET6_0_OR_GREATER
 				writer.WriteRawValue(item.Value);
 #else
-                using (JsonDocument document = JsonDocument.Parse(item.Value))
-                {
-                    JsonSerializer.Serialize(writer, document.RootElement);
-                }
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
 #endif
+                }
             }
             writer.WriteEndObject();
         }
