f4347b17d05174e298ee397066e41d64c540b68f
mcgallan <1186003288@qq.com>
2023-12-05 14:25:08 +0800

    Update

---
+++ sdk/botservice/Azure.ResourceManager.BotService/tests/Helpers/ResourceDataHelpers.cs
@@ -0,0 +1,179 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Azure.Core;
+using Azure.ResourceManager.BotService.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources.Models;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.BotService.Tests.Helpers
+{
+    public class ResourceDataHelpers
+    {
+        public static void AssertResource(ResourceData r1, ResourceData r2)
+        {
+            Assert.AreEqual(r1.Name, r2.Name);
+            Assert.AreEqual(r1.Id, r2.Id);
+            Assert.AreEqual(r1.ResourceType, r2.ResourceType);
+        }
+
+        public static void AssertTrackedResource(TrackedResourceData r1, TrackedResourceData r2)
+        {
+            Assert.AreEqual(r1.Name, r2.Name);
+            Assert.AreEqual(r1.Id, r2.Id);
+            Assert.AreEqual(r1.ResourceType, r2.ResourceType);
+            Assert.AreEqual(r1.Location, r2.Location);
+            Assert.AreEqual(r1.Tags, r2.Tags);
+        }
+
+        #region BotService
+        public static BotData GetBotData()
+        {
+            var data = new BotData(AzureLocation.WestUS)
+            {
+                Properties = new BotProperties("The Name of the bot", new Uri("http://mybot.coffee"), "exampleappid")
+                {
+                    Description = "The description of the bot",
+                    IconUri = new Uri("http://myicon"),
+                    MsaAppType = BotMsaAppType.UserAssignedMSI,
+                    MsaAppTenantId = "exampleapptenantid",
+                    MsaAppMSIResourceId = new ResourceIdentifier("/subscriptions/foo/resourcegroups/bar/providers/microsoft.managedidentity/userassignedidentities/sampleId"),
+                    DeveloperAppInsightKey = "appinsightskey",
+                    DeveloperAppInsightsApiKey = "appinsightsapikey",
+                    DeveloperAppInsightsApplicationId = "appinsightsappid",
+                    LuisAppIds =
+                    {
+                        "luisappid1","luisappid2"
+                    },
+                    LuisKey = "luiskey",
+                    IsCmekEnabled = true,
+                    CmekKeyVaultUri = new Uri("https://myCmekKey"),
+                    PublicNetworkAccess = BotServicePublicNetworkAccess.Enabled,
+                    IsLocalAuthDisabled = true,
+                    SchemaTransformationVersion = "1.0",
+                },
+                Sku = new BotServiceSku(BotServiceSkuName.S1),
+                Kind = BotServiceKind.Sdk,
+                ETag = new ETag("etag1"),
+                Tags =
+                {
+                    ["tag1"] = "value1",
+                    ["tag2"] = "value2",
+                },
+            };
+            return data;
+        }
+
+        public static void AssertBotServiceData(BotData data1, BotData data2)
+        {
+            AssertTrackedResource(data1, data2);
+            Assert.AreEqual(data1.Properties.AppPasswordHint, data2.Properties.AppPasswordHint);
+            Assert.AreEqual(data1.Kind, data2.Kind);
+            Assert.AreEqual(data1.Sku, data2.Sku);
+        }
+        #endregion
+
+        #region BotChannel
+        public static BotChannelData GetEmailChannelData()
+        {
+            BotChannelData data = new BotChannelData(AzureLocation.WestUS)
+            {
+                Properties = new EmailChannel()
+                {
+                    Properties = new EmailChannelProperties("a@b.com", true)
+                    {
+                        Password = "123456"
+                    }
+                }
+            };
+            return data;
+        }
+
+        public static BotChannelData GetDirectLineSpeechChannelData()
+        {
+            BotChannelData data = new BotChannelData(AzureLocation.WestUS)
+            {
+                Properties = new DirectLineSpeechChannel()
+                {
+                    Properties = new DirectLineSpeechChannelProperties()
+                    {
+                        CognitiveServiceRegion = "XcognitiveServiceRegionX",
+                        CognitiveServiceSubscriptionKey = "XcognitiveServiceSubscriptionKeyX",
+                        IsEnabled = true,
+                    },
+                }
+            };
+            return data;
+        }
+
+        public static BotChannelData GetLineChannelData()
+        {
+            BotChannelData data = new BotChannelData(AzureLocation.WestUS)
+            {
+                Properties = new LineChannel()
+                {
+                    Properties = new LineChannelProperties(new LineRegistration[]
+                    {
+                        new LineRegistration()
+                        {
+                            ChannelSecret = "channelSecret",
+                            ChannelAccessToken = "channelAccessToken",
+                        }
+                    }),
+                }
+            };
+            return data;
+        }
+
+        public static void AssertBotChannel(BotChannelData data1, BotChannelData data2)
+        {
+            AssertTrackedResource(data1, data2);
+            Assert.AreEqual(data1.Sku, data2.Sku);
+            Assert.AreEqual(data1.Kind, data2.Kind);
+            Assert.AreEqual(data1.Properties.ProvisioningState, data2.Properties.ProvisioningState);
+        }
+        #endregion
+
+        #region
+        public static BotConnectionSettingData GetBotConnectionSettingData()
+        {
+            BotConnectionSettingData data = new BotConnectionSettingData(AzureLocation.WestUS)
+            {
+                Properties = new BotConnectionSettingProperties()
+                {
+                    ClientId = "sampleclientid",
+                    ClientSecret = "samplesecret",
+                    Scopes = "samplescope",
+                    ServiceProviderId = "serviceproviderid",
+                    Parameters =
+                    {
+                        new BotConnectionSettingParameter()
+                        {
+                        Key = "key1",
+                        Value = "value1",
+                        },
+                        new BotConnectionSettingParameter()
+                        {
+                        Key = "key2",
+                        Value = "value2",
+                        }
+                    }
+                },
+                ETag = new ETag("etag1")
+            };
+            return data;
+        }
+
+        public static void AssertBotConnectionSettingData(BotConnectionSettingData data1, BotConnectionSettingData data2)
+        {
+            AssertTrackedResource(data1, data2);
+            Assert.AreEqual(data1.Sku, data2.Sku);
+            Assert.AreEqual(data1.Kind, data2.Kind);
+            Assert.AreEqual(data1.Properties.Parameters.Count, data2.Properties.Parameters.Count);
+        }
+        #endregion
+    }
+}
---
+++ sdk/botservice/Azure.ResourceManager.BotService/tests/TestCase/BotServiceTest.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.BotService.Models;
+using Azure.ResourceManager.BotService.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.BotService.Tests
+{
+    public class BotServiceTest : BotServiceManagementTestBase
+    {
+        public BotServiceTest(bool isAsync)
+            : base(isAsync, RecordedTestMode.Record)
+        {
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task ProviderApiTest()
+        {
+            //1.Create
+            var botName = Recording.GenerateAssetName("testbotService");
+            var botName2 = Recording.GenerateAssetName("testbotservice");
+            var botName3 = Recording.GenerateAssetName("testbotService");
+            var resourceGroup = await CreateResourceGroupAsync();
+            var collection = resourceGroup.GetBots();
+            var input = ResourceDataHelpers.GetBotData();
+            var resource = (await collection.CreateOrUpdateAsync(WaitUntil.Completed, botName, input)).Value;
+            Assert.AreEqual(botName, resource.Data.Name);
+            //2.Get
+            var resource2 = (await resource.GetAsync()).Value;
+            ResourceDataHelpers.AssertBotServiceData(resource.Data, resource2.Data);
+            //3.GetAll
+            _ = await collection.CreateOrUpdateAsync(WaitUntil.Completed, botName2, input);
+            _ = await collection.CreateOrUpdateAsync(WaitUntil.Completed, botName3, input);
+            int count = 0;
+            await foreach (var item in collection.GetAllAsync())
+            {
+                count++;
+            }
+            Assert.GreaterOrEqual(count, 2);
+            //4.Exist
+            Assert.IsTrue(await collection.ExistsAsync(botName));
+            Assert.IsFalse(await collection.ExistsAsync(botName + "1"));
+
+            Assert.ThrowsAsync<ArgumentNullException>(async () => _ = await collection.ExistsAsync(null));
+            //Resouece operation
+            //1.Get
+            var rResource3 = (await resource.GetAsync()).Value;
+            ResourceDataHelpers.AssertBotServiceData(resource.Data, rResource3.Data);
+            //2.Update
+            var patch = new BotData(AzureLocation.EastUS)
+            {
+                Properties = new BotProperties("The Name of the bot", new Uri("http://mybot.coffee"), "exampleappid")
+                {
+                    Description = "The description of the bot",
+                    IconUri = new Uri("http://myicon"),
+                    MsaAppType = BotMsaAppType.UserAssignedMSI,
+                    MsaAppTenantId = "exampleapptenantid",
+                    MsaAppMSIResourceId = new ResourceIdentifier("/subscriptions/foo/resourcegroups/bar/providers/microsoft.managedidentity/userassignedidentities/sampleId"),
+                    DeveloperAppInsightKey = "appinsightskey",
+                    DeveloperAppInsightsApiKey = "appinsightsapikey",
+                    DeveloperAppInsightsApplicationId = "appinsightsappid",
+                    LuisAppIds =
+                    {
+                        "luisappid1","luisappid2"
+                    },
+                    LuisKey = "luiskey",
+                    IsCmekEnabled = true,
+                    CmekKeyVaultUri = new Uri("https://myCmekKey"),
+                    PublicNetworkAccess = BotServicePublicNetworkAccess.Enabled,
+                    IsLocalAuthDisabled = true,
+                    SchemaTransformationVersion = "1.0",
+                },
+                Sku = new BotServiceSku(BotServiceSkuName.S1),
+                Kind = BotServiceKind.Sdk,
+                ETag = new ETag("etag1"),
+                Tags =
+                {
+                    ["UpdateKey1"] = "UpdateValue1",
+                    ["UpdateKey1"] = "UpdateValue1",
+                    ["UpdateKey1"] = "UpdateValue1"
+                },
+            };
+            var resource4 = (await rResource3.UpdateAsync(patch)).Value;
+            ResourceDataHelpers.AssertBotServiceData(patch, resource4.Data);
+            //3. Delete
+            await resource4.DeleteAsync(WaitUntil.Completed);
+        }
+    }
+}
