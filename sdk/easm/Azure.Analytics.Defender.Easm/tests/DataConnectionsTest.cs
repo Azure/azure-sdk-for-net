c0d3584a2e64cc9cc192febfb91b5df9ee618505
thang-bit <thangle@microsoft.com>
2023-12-05 13:37:46 -0600

    Add Data Connection test

---
+++ sdk/easm/Azure.Analytics.Defender.Easm/tests/DataConnectionsTest.cs
@@ -0,0 +1,87 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Analytics.Defender.Easm;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.Analytics.Defender.Easm.Tests
+{
+    internal class DataConnectionsTest : EasmClientTest
+    {
+        private string DataConnectionName;
+        private string NewDataConnectionName;
+        private string ClusterName;
+        private string DatabaseName;
+
+        public DataConnectionsTest(bool isAsync) : base(isAsync)
+        {
+            DataConnectionName = "new-sample-dc";
+            NewDataConnectionName = "new-sample-dc";
+            ClusterName = "sample-cluster";
+            DatabaseName = "sample-db";
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task DataConnectionsListTest()
+        {
+            var results = client.GetDataConnectionsAsync();
+            var asyncNumerator = results.GetAsyncEnumerator();
+            var something = asyncNumerator.Current;
+            Assert.IsNotNull(results);
+            await foreach (DataConnection result in results)
+            {
+                Assert.IsNotNull(result.Name);
+                Assert.IsNotNull(result.DisplayName);
+                Assert.IsNotNull(result.Active);
+                Assert.IsNotNull(result.Content);
+            }
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task DataConnectionsGetTest()
+        {
+            var result = await client.GetDataConnectionAsync(DataConnectionName);
+            DataConnection dataConnection = result.Value;
+            Assert.AreEqual(DataConnectionName, dataConnection.Name);
+            Assert.AreEqual(DataConnectionName, dataConnection.DisplayName);
+            Assert.IsNotNull(dataConnection.Active);
+            Assert.IsNotNull(dataConnection.Content);
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task DataConnectionsValidateTest()
+        {
+            AzureDataExplorerDataConnectionProperties properties = new AzureDataExplorerDataConnectionProperties();
+            properties.ClusterName = ClusterName;
+            properties.DatabaseName = DatabaseName;
+            properties.Region = "eastus";
+            AzureDataExplorerDataConnectionData request = new AzureDataExplorerDataConnectionData(properties);
+            request.Name = DatabaseName;
+            request.Content = DataConnectionContent.Assets;
+            request.Frequency = DataConnectionFrequency.Daily;
+            var response = await client.ValidateDataConnectionAsync(request).ConfigureAwait(false);
+            Assert.IsNotNull(response);
+            Assert.IsNull(response.Value.Error);
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task DataConenctionsCreateOrReplaceTest()
+        {
+            AzureDataExplorerDataConnectionProperties properties = new AzureDataExplorerDataConnectionProperties();
+            properties.ClusterName = ClusterName;
+            properties.DatabaseName = DatabaseName;
+            properties.Region = "eastus";
+            AzureDataExplorerDataConnectionData request = new AzureDataExplorerDataConnectionData(properties);
+            request.Name = NewDataConnectionName;
+            request.Content = DataConnectionContent.Assets;
+            request.Frequency = DataConnectionFrequency.Daily;
+            var response = await client.CreateOrReplaceDataConnectionAsync(NewDataConnectionName, request).ConfigureAwait(false);
+            Assert.AreEqual(NewDataConnectionName, response.Value.Name);
+            Assert.AreEqual(NewDataConnectionName, response.Value.DisplayName);
+            Assert.AreEqual(DataConnectionFrequency.Daily, response.Value.Frequency);
+        }
+    }
+}
