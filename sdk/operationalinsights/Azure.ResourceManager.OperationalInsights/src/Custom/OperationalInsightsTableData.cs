0d5cf1d30a8c024061b62d9d12ac33f8f5830854
Chengming <v-cruan@microsoft.com>
2023-12-06 16:50:24 +0800

    Regenerate and add custom code

---
+++ sdk/operationalinsights/Azure.ResourceManager.OperationalInsights/src/Custom/OperationalInsightsTableData.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using Azure.Core;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.OperationalInsights.Models;
+
+namespace Azure.ResourceManager.OperationalInsights
+{
+    /// <summary>
+    /// A class representing the OperationalInsightsTable data model.
+    /// Workspace data table definition.
+    /// </summary>
+    public partial class OperationalInsightsTableData : ResourceData
+    {
+        /// <summary> True - Value originates from workspace retention in days, False - Customer specific. </summary>
+        public RetentionInDaysAsDefaultState? RetentionInDaysAsDefault => new RetentionInDaysAsDefaultState(IsRetentionInDaysAsDefault.ToString());
+        /// <summary> True - Value originates from retention in days, False - Customer specific. </summary>
+        public TotalRetentionInDaysAsDefaultState? TotalRetentionInDaysAsDefault => new TotalRetentionInDaysAsDefaultState(IsTotalRetentionInDaysAsDefault.ToString());
+    }
+}
