92f9a48d581ead6f7409c0e86e9cab1ea2b3a222
Heath Stewart <heaths@microsoft.com>
2023-12-07 15:40:25 -0800

    Attribute clients, methods that should be audited (#40577)
    * Attribute clients, methods that should be audited
    
    Resolves #40522
    
    * Attribute KeyClient.ReleaseKey
    
    * Attribute all of CryptographyClient per feedback

--- sdk/core/Azure.Core/src/Shared/CallerShouldAuditAttribute.cs
+++ sdk/core/Azure.Core/src/Shared/CallerShouldAuditAttribute.cs
@@ -8,14 +8,14 @@ using System;
 namespace Azure.Core
 {
     /// <summary>
-    /// Decorates an operation whose invocation should potentially be audited
+    /// Decorates an operation or all operations for a client whose invocation should potentially be audited
     /// by Azure service implementations.  Auditing could be recommended
     /// because the operation changes critical service state, creates delegated
     /// access to a resource, affects data retention, etc.  It's a best guess
     /// from the service team that the operation should be audited to mitigate
     /// any potential future issues.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     internal class CallerShouldAuditAttribute : Attribute
     {
         /// <summary>
