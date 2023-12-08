c3a18e3aa5b2db7fd313f696f4ed656a11e2c5e1
Jocelyn Schreppler <jaschrep@microsoft.com>
2023-12-08 12:56:08 -0500

    Move enum

--- sdk/storage/Azure.Storage.Common/src/ExpectContinueOptions.cs
+++ sdk/storage/Azure.Storage.Common/src/ExpectContinueOptions.cs
@@ -11,39 +11,9 @@ namespace Azure.Storage
     public class ExpectContinueOptions
     {
         /// <summary>
-        /// Mode for applying expect-continue to a PUT request.
-        /// </summary>
-        public enum ApplyHeaderMode
-        {
-            /// <summary>
-            /// If no options are provided, this is the default behavior.
-            /// <para>
-            /// Expect-continue will not be applied until specific errors are encountered from the
-            /// service, at which point they will be applied until a period of time after the last
-            /// of those errors occured.
-            /// </para>
-            /// <para>
-            /// Response codes that trigger this behavior are 429, 500, and 503.
-            /// </para>
-            /// </summary>
-            ApplyOnThrottle = 0,
-
-            /// <summary>
-            /// Expect-continue will be applied regardless of recent error status. There may be
-            /// some additionally defined thresholds for applying the header.
-            /// </summary>
-            On = 1,
-
-            /// <summary>
-            /// Expect-Continue will never be applied.
-            /// </summary>
-            Off = 2,
-        }
-
-        /// <summary>
         /// Mode for these options.
         /// </summary>
-        public ApplyHeaderMode Mode { get; set; }
+        public ExpectContinueMode Mode { get; set; }
 
         /// <summary>
         /// The minimum value of HTTP request Content-Length for applying expect-continue.
@@ -51,7 +21,7 @@ namespace Azure.Storage
         public long ContentLengthThreshold { get; set; }
 
         /// <summary>
-        /// In mode <see cref="ApplyHeaderMode.ApplyOnThrottle"/>, the time interval to apply the header
+        /// In mode <see cref="ExpectContinueMode.ApplyOnThrottle"/>, the time interval to apply the header
         /// after recieving a triggering error. The default time is one minute.
         /// </summary>
         public TimeSpan ThrottleInterval { get; set; } = TimeSpan.FromMinutes(1);
--- sdk/storage/Azure.Storage.Common/src/Shared/StorageClientOptions.cs
+++ sdk/storage/Azure.Storage.Common/src/Shared/StorageClientOptions.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
 using Azure.Core;
 using Azure.Core.Pipeline;
 using Azure.Storage.Shared;
