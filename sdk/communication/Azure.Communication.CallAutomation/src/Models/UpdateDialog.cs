1e3a225bee68fc1978e27d8595578431b6d6e59d
Calvin Tang <calvintang@microsoft.com>
2023-12-13 21:00:31 -0800

    Remove unused ctor

--- sdk/communication/Azure.Communication.CallAutomation/src/Models/UpdateDialog.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/UpdateDialog.cs
@@ -15,15 +15,6 @@ namespace Azure.Communication.CallAutomation
         /// <summary>
         /// Creates a new instance of the UpdateDialogOptions.
         /// </summary>
-        /// <param name="dialog"></param>
-        public UpdateDialog(DialogUpdateBase dialog)
-        {
-            DialogId = Guid.NewGuid().ToString();
-            Dialog = dialog;
-        }
-        /// <summary>
-        /// Creates a new instance of the UpdateDialogOptions.
-        /// </summary>
         /// <param name="dialogId"></param>
         /// <param name="dialog"></param>
         public UpdateDialog(string dialogId, DialogUpdateBase dialog)
