d0ef57bcfb67d31d0d6a56f9b47d6b12ccdee2a5
Wei Hu <huwe@microsoft.com>
2023-12-15 20:18:39 +0800

    fix build

--- sdk/keyvault/Azure.Security.KeyVault.Keys/src/RecoverDeletedKeyOperation.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Keys/src/RecoverDeletedKeyOperation.cs
@@ -104,5 +104,15 @@ namespace Azure.Security.KeyVault.Keys
                     return OperationState.Failure(response, new RequestFailedException(response));
             }
         }
+
+        /// <summary>
+        /// XXX
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public string GetOperationId()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
