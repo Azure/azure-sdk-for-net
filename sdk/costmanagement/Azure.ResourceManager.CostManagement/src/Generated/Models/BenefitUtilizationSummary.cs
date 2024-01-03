ad6fb8aea30287c17110a8a13a3c03a20cffa875
azure-sdk <azuresdk@microsoft.com>
2024-01-03 01:07:33 +0000

    Update SDK codes co_da_1

--- sdk/costmanagement/Azure.ResourceManager.CostManagement/src/Generated/Models/BenefitUtilizationSummary.cs
+++ sdk/costmanagement/Azure.ResourceManager.CostManagement/src/Generated/Models/BenefitUtilizationSummary.cs
@@ -15,10 +15,10 @@ namespace Azure.ResourceManager.CostManagement.Models
     /// Please note <see cref="BenefitUtilizationSummary"/> is the base class. According to the scenario, a derived class of the base class might need to be assigned here, or this property needs to be casted to one of the possible derived classes.
     /// The available derived classes include <see cref="IncludedQuantityUtilizationSummary"/> and <see cref="SavingsPlanUtilizationSummary"/>.
     /// </summary>
-    public partial class BenefitUtilizationSummary : ResourceData
+    public abstract partial class BenefitUtilizationSummary : ResourceData
     {
         /// <summary> Initializes a new instance of <see cref="BenefitUtilizationSummary"/>. </summary>
-        public BenefitUtilizationSummary()
+        protected BenefitUtilizationSummary()
         {
         }
 
