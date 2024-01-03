8576268c356ab1430e4fa8a9469ea0748caa79e2
Anne Thompson <annelo@microsoft.com>
2024-01-03 14:21:31 -0800

    ClientModel: Add tests for ClientModel types  (#40957)
    * Add pipeline functional tests
    
    * Pipeline tests
    
    * Pipeline tests
    
    * update
    
    * Message tests
    
    * Add first transport test
    
    * More transport tests
    
    * More transport tests

--- sdk/core/System.ClientModel/tests/TestFramework/Mocks/MockSyncAsyncExtensions.cs
+++ sdk/core/System.ClientModel/tests/TestFramework/Mocks/MockSyncAsyncExtensions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using System.ClientModel.Primitives;
 using System.Threading.Tasks;
 
@@ -21,6 +20,18 @@ public static class MockSyncAsyncExtensions
         }
     }
 
+    public static async Task ProcessSyncOrAsync(this HttpClientPipelineTransport transport, PipelineMessage message, bool isAsync)
+    {
+        if (isAsync)
+        {
+            await transport.ProcessAsync(message).ConfigureAwait(false);
+        }
+        else
+        {
+            transport.Process(message);
+        }
+    }
+
     public static async Task DelaySyncOrAsync(this MessageDelay delay, PipelineMessage message, bool isAsync)
     {
         if (isAsync)
@@ -32,16 +43,4 @@ public static class MockSyncAsyncExtensions
             delay.Delay(message, default);
         }
     }
-
-    //public static async Task ShouldRetryCoreSyncOrAsync(this MockRetryPolicy policy, PipelineMessage message, Exception? exception, bool isAsync)
-    //{
-    //    if (isAsync)
-    //    {
-    //        await policy.ShouldRetryAsync(message, default).ConfigureAwait(false);
-    //    }
-    //    else
-    //    {
-    //        policy.ShouldRetry(message, default);
-    //    }
-    //}
 }
