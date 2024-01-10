ca0c2c6de888d86f1bff0a0c94ffec116106df06
mcgallan <1186003288@qq.com>
2024-01-10 17:10:08 +0800

    regen

--- sdk/customer-insights/Azure.ResourceManager.CustomerInsights/src/Generated/Models/KpiThresholds.Serialization.cs
+++ sdk/customer-insights/Azure.ResourceManager.CustomerInsights/src/Generated/Models/KpiThresholds.Serialization.cs
@@ -5,15 +5,26 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.CustomerInsights.Models
 {
-    public partial class KpiThresholds : IUtf8JsonSerializable
+    public partial class KpiThresholds : IUtf8JsonSerializable, IJsonModel<KpiThresholds>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<KpiThresholds>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<KpiThresholds>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<KpiThresholds>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(KpiThresholds)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             writer.WritePropertyName("lowerLimit"u8);
             writer.WriteNumberValue(LowerLimit);
@@ -21,11 +32,40 @@ namespace Azure.ResourceManager.CustomerInsights.Models
             writer.WriteNumberValue(UpperLimit);
             writer.WritePropertyName("increasingKpi"u8);
             writer.WriteBooleanValue(IncreasingKpi);
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static KpiThresholds DeserializeKpiThresholds(JsonElement element)
+        KpiThresholds IJsonModel<KpiThresholds>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<KpiThresholds>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(KpiThresholds)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeKpiThresholds(document.RootElement, options);
+        }
+
+        internal static KpiThresholds DeserializeKpiThresholds(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
@@ -33,6 +73,8 @@ namespace Azure.ResourceManager.CustomerInsights.Models
             decimal lowerLimit = default;
             decimal upperLimit = default;
             bool increasingKpi = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("lowerLimit"u8))
@@ -50,8 +92,44 @@ namespace Azure.ResourceManager.CustomerInsights.Models
                     increasingKpi = property.Value.GetBoolean();
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new KpiThresholds(lowerLimit, upperLimit, increasingKpi);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new KpiThresholds(lowerLimit, upperLimit, increasingKpi, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<KpiThresholds>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<KpiThresholds>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(KpiThresholds)} does not support '{options.Format}' format.");
+            }
+        }
+
+        KpiThresholds IPersistableModel<KpiThresholds>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<KpiThresholds>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeKpiThresholds(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(KpiThresholds)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<KpiThresholds>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
