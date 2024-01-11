ee206a3f574b2e84ed8d3832b7afe921fbe0910f
mcgallan <1186003288@qq.com>
2024-01-11 10:33:34 +0800

    regen

--- sdk/datalake-analytics/Azure.ResourceManager.DataLakeAnalytics/src/Generated/Models/DataLakeAnalyticsSasTokenInformation.cs
+++ sdk/datalake-analytics/Azure.ResourceManager.DataLakeAnalytics/src/Generated/Models/DataLakeAnalyticsSasTokenInformation.cs
@@ -5,6 +5,9 @@
 
 #nullable disable
 
+using System;
+using System.Collections.Generic;
+
 namespace Azure.ResourceManager.DataLakeAnalytics.Models
 {
     /// <summary>
@@ -13,6 +16,38 @@ namespace Azure.ResourceManager.DataLakeAnalytics.Models
     /// </summary>
     public partial class DataLakeAnalyticsSasTokenInformation
     {
+        /// <summary>
+        /// Keeps track of any properties unknown to the library.
+        /// <para>
+        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
+        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+
         /// <summary> Initializes a new instance of <see cref="DataLakeAnalyticsSasTokenInformation"/>. </summary>
         internal DataLakeAnalyticsSasTokenInformation()
         {
@@ -23,9 +58,11 @@ namespace Azure.ResourceManager.DataLakeAnalytics.Models
         /// The access token for the associated Azure Storage Container.
         /// Serialized Name: SasTokenInformation.accessToken
         /// </param>
-        internal DataLakeAnalyticsSasTokenInformation(string accessToken)
+        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
+        internal DataLakeAnalyticsSasTokenInformation(string accessToken, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
             AccessToken = accessToken;
+            _serializedAdditionalRawData = serializedAdditionalRawData;
         }
 
         /// <summary>
