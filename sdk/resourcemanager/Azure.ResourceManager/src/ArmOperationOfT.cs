e61adf682f761900854092047c45edcb8ab72c3b
Wei Hu <huwe@microsoft.com>
2024-01-11 17:22:37 +0800

    Update for LRO rehydration

--- sdk/resourcemanager/Azure.ResourceManager/src/ArmOperationOfT.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/ArmOperationOfT.cs
@@ -5,11 +5,97 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.Pipeline;
 
 namespace Azure.ResourceManager
 {
     /// <inheritdoc/>
-    public abstract class ArmOperation<T> : Operation<T>
+    public class ArmOperation<T> : Operation<T>
     {
+        private readonly OperationInternal<T> _operation;
+
+        /// <summary> Initializes a new instance of ArmOperation. </summary>
+        public ArmOperation(ArmClient client, string id)
+        {
+            Argument.AssertNotNullOrEmpty(id, nameof(id));
+
+            var isResource = typeof(T).GetConstructor(
+                BindingFlags.NonPublic | BindingFlags.Instance,
+                null,
+                CallingConventions.Any,
+                new Type[] { typeof(ArmClient), typeof(ResourceIdentifier) },
+                null) is not null;
+            var obj = Activator.CreateInstance(typeof(T), BindingFlags.NonPublic | BindingFlags.Instance, null, null, null);
+            if (!isResource && obj is not IJsonModel<object>)
+            {
+                throw new InvalidOperationException($"Type {typeof(T)} should be Resource or ReousrceData");
+            }
+
+            IOperationSource<T> source = new GenericOperationSource<T>(client, isResource);
+            var nextLinkOperation = NextLinkOperationImplementation.Create(source, client.Pipeline, id);
+            // TODO: Do we need more specific OptionsNamespace, ProviderNamespace and OperationTypeName and possibly from id?
+            var clientDiagnostics = new ClientDiagnostics("Azure.ResourceManager", "Microsoft.Resources", client.Diagnostics);
+            _operation = new OperationInternal<T>(nextLinkOperation, clientDiagnostics, null, operationTypeName: null);
+        }
+
+        /// <summary> Initializes a new instance of ArmOperation for mocking. </summary>
+        protected ArmOperation()
+        {
+        }
+
+        internal ArmOperation(OperationInternal<T> operation)
+        {
+            _operation = operation;
+        }
+
+        internal ArmOperation(Response<T> response, string operationId)
+        {
+            _operation = OperationInternal<T>.Succeeded(response.GetRawResponse(), response.Value, operationId);
+        }
+
+        internal ArmOperation(IOperationSource<T> source, ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia, string resourceTypeName, bool skipApiVersionOverride = false, string apiVersionOverrideValue = null)
+        {
+            var nextLinkOperation = NextLinkOperationImplementation.Create(source, pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia, skipApiVersionOverride, apiVersionOverrideValue);
+            _operation = new OperationInternal<T>(nextLinkOperation, clientDiagnostics, response, resourceTypeName);
+        }
+
+        /// <inheritdoc />
+        public override string Id => _operation.GetOperationId();
+
+        /// <inheritdoc />
+        public override T Value => _operation.Value;
+
+        /// <inheritdoc />
+        public override bool HasValue => _operation.HasValue;
+
+        /// <inheritdoc />
+        public override bool HasCompleted => _operation.HasCompleted;
+
+        /// <inheritdoc />
+        public override Response GetRawResponse() => _operation.RawResponse;
+
+        /// <inheritdoc />
+        public override Response UpdateStatus(CancellationToken cancellationToken = default) => _operation.UpdateStatus(cancellationToken);
+
+        /// <inheritdoc />
+        public override ValueTask<Response> UpdateStatusAsync(CancellationToken cancellationToken = default) => _operation.UpdateStatusAsync(cancellationToken);
+
+        /// <inheritdoc />
+        public override Response<T> WaitForCompletion(CancellationToken cancellationToken = default) => _operation.WaitForCompletion(cancellationToken);
+
+        /// <inheritdoc />
+        public override Response<T> WaitForCompletion(TimeSpan pollingInterval, CancellationToken cancellationToken = default) => _operation.WaitForCompletion(pollingInterval, cancellationToken);
+
+        /// <inheritdoc />
+        public override ValueTask<Response<T>> WaitForCompletionAsync(CancellationToken cancellationToken = default) => _operation.WaitForCompletionAsync(cancellationToken);
+
+        /// <inheritdoc />
+        public override ValueTask<Response<T>> WaitForCompletionAsync(TimeSpan pollingInterval, CancellationToken cancellationToken = default) => _operation.WaitForCompletionAsync(pollingInterval, cancellationToken);
     }
 }
