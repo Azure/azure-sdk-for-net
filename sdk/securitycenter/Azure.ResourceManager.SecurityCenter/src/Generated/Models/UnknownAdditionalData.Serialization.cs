e0fc0b1cc36234ae839920f3ede0770fe8c9a488
Elia Grady <eliagrady@users.noreply.github.com>
2024-01-15 04:56:04 +0200

    Microsoft.security updatesdk 1.3.0 (#40692)
    * commit expose api
    
    * commit temp autorest
    
    * Fix autorest mapping and add unit tests
    
    * Rollback change in msbuild
    
    * Remove parameter
    
    * update autorest.md with latest readme ref
    
    * update test to use .net 462 compat async
    
    * update autorest ref to use new package-dotnet-sdk
    
    * update Generated code /t:GenerateCode
    
    * update Exported api (Export-API.ps1)
    
    * update tests project with recent Microsoft.NET.Test.Sdk package (test discovery fix)
    
    * update autorest.md to point to azure repo
    
    * run code generation
    
    * take ivadim devops connector test changes
    
    * revert sdk/azurestackhci/Azure.ResourceManager.Hci/src/Generated/Models/VirtualMachineInstancePropertiesStorageProfileOSDisk.Serialization.cs
    
    * bump to 1.3.0 + add to changelog
    
    * Add more tests to cover securityConnectors DevOps
    
    * Push new assets
    
    * update changelog.md
    
    * re-run Export-API.ps1
    
    * update apiCompatVersion to last SDK release
    
    * spell check fixes
    
    * fix cspell failure
    
    * fix cspell failure
    
    * update
    
    * update
    
    * update
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Elia Grady <elgrady@microsoft.com>
    Co-authored-by: Dmitry Ivanov <dmivanov@microsoft.com>
    Co-authored-by: Arthur Ma <mars.arthur@263.net>
    Co-authored-by: Mingzhe Huang <archerzz@users.noreply.github.com>

--- sdk/securitycenter/Azure.ResourceManager.SecurityCenter/src/Generated/Models/UnknownAdditionalData.Serialization.cs
+++ sdk/securitycenter/Azure.ResourceManager.SecurityCenter/src/Generated/Models/UnknownAdditionalData.Serialization.cs
@@ -5,28 +5,70 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.SecurityCenter.Models
 {
-    internal partial class UnknownAdditionalData : IUtf8JsonSerializable
+    internal partial class UnknownAdditionalData : IUtf8JsonSerializable, IJsonModel<SecuritySubAssessmentAdditionalInfo>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<SecuritySubAssessmentAdditionalInfo>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<SecuritySubAssessmentAdditionalInfo>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<SecuritySubAssessmentAdditionalInfo>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(SecuritySubAssessmentAdditionalInfo)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             writer.WritePropertyName("assessedResourceType"u8);
             writer.WriteStringValue(AssessedResourceType.ToString());
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static UnknownAdditionalData DeserializeUnknownAdditionalData(JsonElement element)
+        SecuritySubAssessmentAdditionalInfo IJsonModel<SecuritySubAssessmentAdditionalInfo>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<SecuritySubAssessmentAdditionalInfo>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(SecuritySubAssessmentAdditionalInfo)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeUnknownAdditionalData(document.RootElement, options);
+        }
+
+        internal static UnknownAdditionalData DeserializeUnknownAdditionalData(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             AssessedResourceType assessedResourceType = "Unknown";
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("assessedResourceType"u8))
@@ -34,8 +76,44 @@ namespace Azure.ResourceManager.SecurityCenter.Models
                     assessedResourceType = new AssessedResourceType(property.Value.GetString());
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new UnknownAdditionalData(assessedResourceType);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new UnknownAdditionalData(assessedResourceType, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<SecuritySubAssessmentAdditionalInfo>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<SecuritySubAssessmentAdditionalInfo>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(SecuritySubAssessmentAdditionalInfo)} does not support '{options.Format}' format.");
+            }
+        }
+
+        SecuritySubAssessmentAdditionalInfo IPersistableModel<SecuritySubAssessmentAdditionalInfo>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<SecuritySubAssessmentAdditionalInfo>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeUnknownAdditionalData(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(SecuritySubAssessmentAdditionalInfo)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<SecuritySubAssessmentAdditionalInfo>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
