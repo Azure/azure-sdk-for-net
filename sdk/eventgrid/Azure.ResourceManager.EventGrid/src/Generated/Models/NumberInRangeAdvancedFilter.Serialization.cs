39f26ae8d38310ce485572869de728b0e363095f
mcgallan <1186003288@qq.com>
2024-01-16 15:20:09 +0800

    regen

--- sdk/eventgrid/Azure.ResourceManager.EventGrid/src/Generated/Models/NumberInRangeAdvancedFilter.Serialization.cs
+++ sdk/eventgrid/Azure.ResourceManager.EventGrid/src/Generated/Models/NumberInRangeAdvancedFilter.Serialization.cs
@@ -5,16 +5,26 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.EventGrid.Models
 {
-    public partial class NumberInRangeAdvancedFilter : IUtf8JsonSerializable
+    public partial class NumberInRangeAdvancedFilter : IUtf8JsonSerializable, IJsonModel<NumberInRangeAdvancedFilter>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<NumberInRangeAdvancedFilter>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<NumberInRangeAdvancedFilter>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<NumberInRangeAdvancedFilter>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(NumberInRangeAdvancedFilter)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             if (Optional.IsCollectionDefined(Values))
             {
@@ -43,11 +53,40 @@ namespace Azure.ResourceManager.EventGrid.Models
                 writer.WritePropertyName("key"u8);
                 writer.WriteStringValue(Key);
             }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static NumberInRangeAdvancedFilter DeserializeNumberInRangeAdvancedFilter(JsonElement element)
+        NumberInRangeAdvancedFilter IJsonModel<NumberInRangeAdvancedFilter>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<NumberInRangeAdvancedFilter>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(NumberInRangeAdvancedFilter)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeNumberInRangeAdvancedFilter(document.RootElement, options);
+        }
+
+        internal static NumberInRangeAdvancedFilter DeserializeNumberInRangeAdvancedFilter(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
@@ -55,6 +94,8 @@ namespace Azure.ResourceManager.EventGrid.Models
             Optional<IList<IList<double>>> values = default;
             AdvancedFilterOperatorType operatorType = default;
             Optional<string> key = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("values"u8))
@@ -93,8 +134,44 @@ namespace Azure.ResourceManager.EventGrid.Models
                     key = property.Value.GetString();
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new NumberInRangeAdvancedFilter(operatorType, key.Value, Optional.ToList(values));
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new NumberInRangeAdvancedFilter(operatorType, key.Value, serializedAdditionalRawData, Optional.ToList(values));
         }
+
+        BinaryData IPersistableModel<NumberInRangeAdvancedFilter>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<NumberInRangeAdvancedFilter>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(NumberInRangeAdvancedFilter)} does not support '{options.Format}' format.");
+            }
+        }
+
+        NumberInRangeAdvancedFilter IPersistableModel<NumberInRangeAdvancedFilter>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<NumberInRangeAdvancedFilter>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeNumberInRangeAdvancedFilter(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(NumberInRangeAdvancedFilter)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<NumberInRangeAdvancedFilter>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
