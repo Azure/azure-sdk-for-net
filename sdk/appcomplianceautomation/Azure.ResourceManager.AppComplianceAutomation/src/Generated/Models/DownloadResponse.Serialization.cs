4599c70e46e96377b344799e7535b522a2137070
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-18 18:33:00 +0800

    Enable the new model serialization for Appcomplianceautomation (#41060)

--- sdk/appcomplianceautomation/Azure.ResourceManager.AppComplianceAutomation/src/Generated/Models/DownloadResponse.Serialization.cs
+++ sdk/appcomplianceautomation/Azure.ResourceManager.AppComplianceAutomation/src/Generated/Models/DownloadResponse.Serialization.cs
@@ -5,16 +5,91 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.AppComplianceAutomation.Models
 {
-    public partial class DownloadResponse
+    public partial class DownloadResponse : IUtf8JsonSerializable, IJsonModel<DownloadResponse>
     {
-        internal static DownloadResponse DeserializeDownloadResponse(JsonElement element)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<DownloadResponse>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<DownloadResponse>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<DownloadResponse>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(DownloadResponse)} does not support '{format}' format.");
+            }
+
+            writer.WriteStartObject();
+            if (options.Format != "W" && Optional.IsCollectionDefined(ResourceList))
+            {
+                writer.WritePropertyName("resourceList"u8);
+                writer.WriteStartArray();
+                foreach (var item in ResourceList)
+                {
+                    writer.WriteObjectValue(item);
+                }
+                writer.WriteEndArray();
+            }
+            if (options.Format != "W" && Optional.IsCollectionDefined(ComplianceReport))
+            {
+                writer.WritePropertyName("complianceReport"u8);
+                writer.WriteStartArray();
+                foreach (var item in ComplianceReport)
+                {
+                    writer.WriteObjectValue(item);
+                }
+                writer.WriteEndArray();
+            }
+            if (options.Format != "W" && Optional.IsDefined(CompliancePdfReport))
+            {
+                writer.WritePropertyName("compliancePdfReport"u8);
+                writer.WriteObjectValue(CompliancePdfReport);
+            }
+            if (options.Format != "W" && Optional.IsDefined(ComplianceDetailedPdfReport))
+            {
+                writer.WritePropertyName("complianceDetailedPdfReport"u8);
+                writer.WriteObjectValue(ComplianceDetailedPdfReport);
+            }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
+            writer.WriteEndObject();
+        }
+
+        DownloadResponse IJsonModel<DownloadResponse>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DownloadResponse>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(DownloadResponse)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeDownloadResponse(document.RootElement, options);
+        }
+
+        internal static DownloadResponse DeserializeDownloadResponse(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
@@ -23,6 +98,8 @@ namespace Azure.ResourceManager.AppComplianceAutomation.Models
             Optional<IReadOnlyList<ComplianceReportItem>> complianceReport = default;
             Optional<DownloadResponseCompliancePdfReport> compliancePdfReport = default;
             Optional<DownloadResponseComplianceDetailedPdfReport> complianceDetailedPdfReport = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("resourceList"u8))
@@ -71,8 +148,44 @@ namespace Azure.ResourceManager.AppComplianceAutomation.Models
                     complianceDetailedPdfReport = DownloadResponseComplianceDetailedPdfReport.DeserializeDownloadResponseComplianceDetailedPdfReport(property.Value);
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new DownloadResponse(Optional.ToList(resourceList), Optional.ToList(complianceReport), compliancePdfReport.Value, complianceDetailedPdfReport.Value);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new DownloadResponse(Optional.ToList(resourceList), Optional.ToList(complianceReport), compliancePdfReport.Value, complianceDetailedPdfReport.Value, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<DownloadResponse>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DownloadResponse>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(DownloadResponse)} does not support '{options.Format}' format.");
+            }
+        }
+
+        DownloadResponse IPersistableModel<DownloadResponse>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DownloadResponse>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeDownloadResponse(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(DownloadResponse)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<DownloadResponse>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
