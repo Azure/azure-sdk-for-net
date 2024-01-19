c45ea2362b68199de754485799610a81532217b1
mcgallan <1186003288@qq.com>
2024-01-19 17:22:31 +0800

    Update MySqlServerData.Serialization.cs

--- sdk/mysql/Azure.ResourceManager.MySql/src/MySql/Generated/Models/MySqlServerData.Serialization.cs
+++ sdk/mysql/Azure.ResourceManager.MySql/src/MySql/Generated/Models/MySqlServerData.Serialization.cs
@@ -7,10 +7,8 @@
 
 using System;
 using System.ClientModel.Primitives;
-using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
-using Azure.ResourceManager.Models;
 using Azure.ResourceManager.MySql.Models;
 
 namespace Azure.ResourceManager.MySql
