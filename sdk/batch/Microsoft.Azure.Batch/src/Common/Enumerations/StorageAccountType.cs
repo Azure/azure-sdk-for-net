282e7fdde2ec609711af30f15050c97177efe97f
wiboris <wiboris@microsoft.com>
2024-01-22 14:57:47 -0800

    updating comments

--- sdk/batch/Microsoft.Azure.Batch/src/Common/Enumerations/StorageAccountType.cs
+++ sdk/batch/Microsoft.Azure.Batch/src/Common/Enumerations/StorageAccountType.cs
@@ -1,22 +1,22 @@
 ﻿namespace Microsoft.Azure.Batch.Common
 {
     /// <summary>
-    /// The storage account type for use in creating data disks.
+    /// The storage Account type for use in creating data disks or OS disk.
     /// </summary>
     public enum StorageAccountType
     {
         /// <summary>
-        /// The data disk should use standard locally redundant storage.
+        /// The data disk / OS disk should use standard locally redundant storage.
         /// </summary>
         StandardLrs,
 
         /// <summary>
-        /// The data disk should use premium locally redundant storage.
+        /// The data disk / OS disk should use premium locally redundant storage.
         /// </summary>
         PremiumLrs,
 
         /// <summary>
-        /// The data disk / OS disk should use standard SSD locally redundant
+        /// The data disk / OS disk should use standard SSD locally redundant storage.
         /// </summary>
         StandardSSDLRS
     }
