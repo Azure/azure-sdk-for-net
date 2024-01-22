ebc5684170a885ab01def9dfd49c9c6969bb51b6
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-22 22:11:39 +0800

    Enable the new model serialization for custom insights (#41135)

--- sdk/customer-insights/Azure.ResourceManager.CustomerInsights/src/Generated/Models/AuthorizationPolicyResourceFormatData.Serialization.cs
+++ sdk/customer-insights/Azure.ResourceManager.CustomerInsights/src/Generated/Models/AuthorizationPolicyResourceFormatData.Serialization.cs
@@ -5,6 +5,8 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
@@ -13,13 +15,46 @@ using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.CustomerInsights
 {
-    public partial class AuthorizationPolicyResourceFormatData : IUtf8JsonSerializable
+    public partial class AuthorizationPolicyResourceFormatData : IUtf8JsonSerializable, IJsonModel<AuthorizationPolicyResourceFormatData>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<AuthorizationPolicyResourceFormatData>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<AuthorizationPolicyResourceFormatData>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<AuthorizationPolicyResourceFormatData>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(AuthorizationPolicyResourceFormatData)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
+            if (options.Format != "W")
+            {
+                writer.WritePropertyName("id"u8);
+                writer.WriteStringValue(Id);
+            }
+            if (options.Format != "W")
+            {
+                writer.WritePropertyName("name"u8);
+                writer.WriteStringValue(Name);
+            }
+            if (options.Format != "W")
+            {
+                writer.WritePropertyName("type"u8);
+                writer.WriteStringValue(ResourceType);
+            }
+            if (options.Format != "W" && Optional.IsDefined(SystemData))
+            {
+                writer.WritePropertyName("systemData"u8);
+                JsonSerializer.Serialize(writer, SystemData);
+            }
             writer.WritePropertyName("properties"u8);
             writer.WriteStartObject();
+            if (options.Format != "W" && Optional.IsDefined(PolicyName))
+            {
+                writer.WritePropertyName("policyName"u8);
+                writer.WriteStringValue(PolicyName);
+            }
             if (Optional.IsCollectionDefined(Permissions))
             {
                 writer.WritePropertyName("permissions"u8);
@@ -41,11 +76,40 @@ namespace Azure.ResourceManager.CustomerInsights
                 writer.WriteStringValue(SecondaryKey);
             }
             writer.WriteEndObject();
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static AuthorizationPolicyResourceFormatData DeserializeAuthorizationPolicyResourceFormatData(JsonElement element)
+        AuthorizationPolicyResourceFormatData IJsonModel<AuthorizationPolicyResourceFormatData>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<AuthorizationPolicyResourceFormatData>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(AuthorizationPolicyResourceFormatData)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeAuthorizationPolicyResourceFormatData(document.RootElement, options);
+        }
+
+        internal static AuthorizationPolicyResourceFormatData DeserializeAuthorizationPolicyResourceFormatData(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
@@ -58,6 +122,8 @@ namespace Azure.ResourceManager.CustomerInsights
             Optional<IList<PermissionType>> permissions = default;
             Optional<string> primaryKey = default;
             Optional<string> secondaryKey = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("id"u8))
@@ -125,8 +191,44 @@ namespace Azure.ResourceManager.CustomerInsights
                     }
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new AuthorizationPolicyResourceFormatData(id, name, type, systemData.Value, policyName.Value, Optional.ToList(permissions), primaryKey.Value, secondaryKey.Value);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new AuthorizationPolicyResourceFormatData(id, name, type, systemData.Value, policyName.Value, Optional.ToList(permissions), primaryKey.Value, secondaryKey.Value, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<AuthorizationPolicyResourceFormatData>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<AuthorizationPolicyResourceFormatData>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(AuthorizationPolicyResourceFormatData)} does not support '{options.Format}' format.");
+            }
+        }
+
+        AuthorizationPolicyResourceFormatData IPersistableModel<AuthorizationPolicyResourceFormatData>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<AuthorizationPolicyResourceFormatData>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeAuthorizationPolicyResourceFormatData(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(AuthorizationPolicyResourceFormatData)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<AuthorizationPolicyResourceFormatData>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
--- sdk/customer-insights/Azure.ResourceManager.CustomerInsights/src/Generated/Models/ParticipantProfilePropertyReference.Serialization.cs
+++ sdk/customer-insights/Azure.ResourceManager.CustomerInsights/src/Generated/Models/ParticipantProfilePropertyReference.Serialization.cs
@@ -5,31 +5,73 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.CustomerInsights.Models
 {
-    public partial class ParticipantProfilePropertyReference : IUtf8JsonSerializable
+    public partial class ParticipantProfilePropertyReference : IUtf8JsonSerializable, IJsonModel<ParticipantProfilePropertyReference>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<ParticipantProfilePropertyReference>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<ParticipantProfilePropertyReference>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ParticipantProfilePropertyReference>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ParticipantProfilePropertyReference)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             writer.WritePropertyName("interactionPropertyName"u8);
             writer.WriteStringValue(InteractionPropertyName);
             writer.WritePropertyName("profilePropertyName"u8);
             writer.WriteStringValue(ProfilePropertyName);
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static ParticipantProfilePropertyReference DeserializeParticipantProfilePropertyReference(JsonElement element)
+        ParticipantProfilePropertyReference IJsonModel<ParticipantProfilePropertyReference>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ParticipantProfilePropertyReference>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ParticipantProfilePropertyReference)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeParticipantProfilePropertyReference(document.RootElement, options);
+        }
+
+        internal static ParticipantProfilePropertyReference DeserializeParticipantProfilePropertyReference(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             string interactionPropertyName = default;
             string profilePropertyName = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("interactionPropertyName"u8))
@@ -42,8 +84,44 @@ namespace Azure.ResourceManager.CustomerInsights.Models
                     profilePropertyName = property.Value.GetString();
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new ParticipantProfilePropertyReference(interactionPropertyName, profilePropertyName);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new ParticipantProfilePropertyReference(interactionPropertyName, profilePropertyName, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<ParticipantProfilePropertyReference>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ParticipantProfilePropertyReference>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(ParticipantProfilePropertyReference)} does not support '{options.Format}' format.");
+            }
+        }
+
+        ParticipantProfilePropertyReference IPersistableModel<ParticipantProfilePropertyReference>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ParticipantProfilePropertyReference>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeParticipantProfilePropertyReference(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(ParticipantProfilePropertyReference)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<ParticipantProfilePropertyReference>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
