471178281628c6a4fef07a80f6684f1203076e86
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-22 21:33:20 +0800

    Enable the new model serialization for container instance (#41125)

--- sdk/containerinstance/Azure.ResourceManager.ContainerInstance/src/Generated/Models/ContainerExecResult.Serialization.cs
+++ sdk/containerinstance/Azure.ResourceManager.ContainerInstance/src/Generated/Models/ContainerExecResult.Serialization.cs
@@ -6,21 +6,78 @@
 #nullable disable
 
 using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.ContainerInstance.Models
 {
-    public partial class ContainerExecResult
+    public partial class ContainerExecResult : IUtf8JsonSerializable, IJsonModel<ContainerExecResult>
     {
-        internal static ContainerExecResult DeserializeContainerExecResult(JsonElement element)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<ContainerExecResult>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<ContainerExecResult>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ContainerExecResult>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ContainerExecResult)} does not support '{format}' format.");
+            }
+
+            writer.WriteStartObject();
+            if (Optional.IsDefined(WebSocketUri))
+            {
+                writer.WritePropertyName("webSocketUri"u8);
+                writer.WriteStringValue(WebSocketUri.AbsoluteUri);
+            }
+            if (Optional.IsDefined(Password))
+            {
+                writer.WritePropertyName("password"u8);
+                writer.WriteStringValue(Password);
+            }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
+            writer.WriteEndObject();
+        }
+
+        ContainerExecResult IJsonModel<ContainerExecResult>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ContainerExecResult>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ContainerExecResult)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeContainerExecResult(document.RootElement, options);
+        }
+
+        internal static ContainerExecResult DeserializeContainerExecResult(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             Optional<Uri> webSocketUri = default;
             Optional<string> password = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("webSocketUri"u8))
@@ -37,8 +94,44 @@ namespace Azure.ResourceManager.ContainerInstance.Models
                     password = property.Value.GetString();
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new ContainerExecResult(webSocketUri.Value, password.Value);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new ContainerExecResult(webSocketUri.Value, password.Value, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<ContainerExecResult>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ContainerExecResult>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(ContainerExecResult)} does not support '{options.Format}' format.");
+            }
+        }
+
+        ContainerExecResult IPersistableModel<ContainerExecResult>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ContainerExecResult>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeContainerExecResult(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(ContainerExecResult)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<ContainerExecResult>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
