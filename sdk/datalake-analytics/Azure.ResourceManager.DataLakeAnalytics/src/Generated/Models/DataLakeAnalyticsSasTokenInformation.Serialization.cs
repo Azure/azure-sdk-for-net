bf952ea2b8d60a3bbfe240dbc574e2ba9de31a2e
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-22 23:03:18 +0800

    Enable the new model serialization for datalake analytics (#41162)

--- sdk/datalake-analytics/Azure.ResourceManager.DataLakeAnalytics/src/Generated/Models/DataLakeAnalyticsSasTokenInformation.Serialization.cs
+++ sdk/datalake-analytics/Azure.ResourceManager.DataLakeAnalytics/src/Generated/Models/DataLakeAnalyticsSasTokenInformation.Serialization.cs
@@ -5,20 +5,73 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.DataLakeAnalytics.Models
 {
-    public partial class DataLakeAnalyticsSasTokenInformation
+    public partial class DataLakeAnalyticsSasTokenInformation : IUtf8JsonSerializable, IJsonModel<DataLakeAnalyticsSasTokenInformation>
     {
-        internal static DataLakeAnalyticsSasTokenInformation DeserializeDataLakeAnalyticsSasTokenInformation(JsonElement element)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<DataLakeAnalyticsSasTokenInformation>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<DataLakeAnalyticsSasTokenInformation>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<DataLakeAnalyticsSasTokenInformation>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(DataLakeAnalyticsSasTokenInformation)} does not support '{format}' format.");
+            }
+
+            writer.WriteStartObject();
+            if (options.Format != "W" && Optional.IsDefined(AccessToken))
+            {
+                writer.WritePropertyName("accessToken"u8);
+                writer.WriteStringValue(AccessToken);
+            }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
+            writer.WriteEndObject();
+        }
+
+        DataLakeAnalyticsSasTokenInformation IJsonModel<DataLakeAnalyticsSasTokenInformation>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DataLakeAnalyticsSasTokenInformation>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(DataLakeAnalyticsSasTokenInformation)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeDataLakeAnalyticsSasTokenInformation(document.RootElement, options);
+        }
+
+        internal static DataLakeAnalyticsSasTokenInformation DeserializeDataLakeAnalyticsSasTokenInformation(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             Optional<string> accessToken = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("accessToken"u8))
@@ -26,8 +79,44 @@ namespace Azure.ResourceManager.DataLakeAnalytics.Models
                     accessToken = property.Value.GetString();
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new DataLakeAnalyticsSasTokenInformation(accessToken.Value);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new DataLakeAnalyticsSasTokenInformation(accessToken.Value, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<DataLakeAnalyticsSasTokenInformation>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DataLakeAnalyticsSasTokenInformation>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(DataLakeAnalyticsSasTokenInformation)} does not support '{options.Format}' format.");
+            }
+        }
+
+        DataLakeAnalyticsSasTokenInformation IPersistableModel<DataLakeAnalyticsSasTokenInformation>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DataLakeAnalyticsSasTokenInformation>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeDataLakeAnalyticsSasTokenInformation(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(DataLakeAnalyticsSasTokenInformation)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<DataLakeAnalyticsSasTokenInformation>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
