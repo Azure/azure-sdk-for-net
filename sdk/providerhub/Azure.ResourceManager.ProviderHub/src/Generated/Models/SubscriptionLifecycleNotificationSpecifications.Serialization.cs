8c480ea33b84bcc54d9868fbfa35bef34b2b8a12
mcgallan <1186003288@qq.com>
2024-01-23 15:32:06 +0800

    regen

--- sdk/providerhub/Azure.ResourceManager.ProviderHub/src/Generated/Models/SubscriptionLifecycleNotificationSpecifications.Serialization.cs
+++ sdk/providerhub/Azure.ResourceManager.ProviderHub/src/Generated/Models/SubscriptionLifecycleNotificationSpecifications.Serialization.cs
@@ -6,16 +6,25 @@
 #nullable disable
 
 using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.ProviderHub.Models
 {
-    public partial class SubscriptionLifecycleNotificationSpecifications : IUtf8JsonSerializable
+    public partial class SubscriptionLifecycleNotificationSpecifications : IUtf8JsonSerializable, IJsonModel<SubscriptionLifecycleNotificationSpecifications>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<SubscriptionLifecycleNotificationSpecifications>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<SubscriptionLifecycleNotificationSpecifications>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<SubscriptionLifecycleNotificationSpecifications>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(SubscriptionLifecycleNotificationSpecifications)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             if (Optional.IsCollectionDefined(SubscriptionStateOverrideActions))
             {
@@ -32,17 +41,48 @@ namespace Azure.ResourceManager.ProviderHub.Models
                 writer.WritePropertyName("softDeleteTTL"u8);
                 writer.WriteStringValue(SoftDeleteTtl.Value, "P");
             }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static SubscriptionLifecycleNotificationSpecifications DeserializeSubscriptionLifecycleNotificationSpecifications(JsonElement element)
+        SubscriptionLifecycleNotificationSpecifications IJsonModel<SubscriptionLifecycleNotificationSpecifications>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<SubscriptionLifecycleNotificationSpecifications>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(SubscriptionLifecycleNotificationSpecifications)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeSubscriptionLifecycleNotificationSpecifications(document.RootElement, options);
+        }
+
+        internal static SubscriptionLifecycleNotificationSpecifications DeserializeSubscriptionLifecycleNotificationSpecifications(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             Optional<IList<SubscriptionStateOverrideAction>> subscriptionStateOverrideActions = default;
             Optional<TimeSpan> softDeleteTtl = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("subscriptionStateOverrideActions"u8))
@@ -68,8 +108,44 @@ namespace Azure.ResourceManager.ProviderHub.Models
                     softDeleteTtl = property.Value.GetTimeSpan("P");
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new SubscriptionLifecycleNotificationSpecifications(Optional.ToList(subscriptionStateOverrideActions), Optional.ToNullable(softDeleteTtl));
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new SubscriptionLifecycleNotificationSpecifications(Optional.ToList(subscriptionStateOverrideActions), Optional.ToNullable(softDeleteTtl), serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<SubscriptionLifecycleNotificationSpecifications>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<SubscriptionLifecycleNotificationSpecifications>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(SubscriptionLifecycleNotificationSpecifications)} does not support '{options.Format}' format.");
+            }
+        }
+
+        SubscriptionLifecycleNotificationSpecifications IPersistableModel<SubscriptionLifecycleNotificationSpecifications>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<SubscriptionLifecycleNotificationSpecifications>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeSubscriptionLifecycleNotificationSpecifications(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(SubscriptionLifecycleNotificationSpecifications)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<SubscriptionLifecycleNotificationSpecifications>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
