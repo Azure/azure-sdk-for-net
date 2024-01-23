d369bc84832f0c19ae8dca1cbcf3be8f0dbb5f48
mcgallan <1186003288@qq.com>
2024-01-23 14:44:23 +0800

    Update PostgreSqlServerData.Serialization.cs

--- sdk/postgresql/Azure.ResourceManager.PostgreSql/src/PostgreSql/Generated/Models/PostgreSqlServerData.Serialization.cs
+++ sdk/postgresql/Azure.ResourceManager.PostgreSql/src/PostgreSql/Generated/Models/PostgreSqlServerData.Serialization.cs
@@ -7,10 +7,8 @@
 
 using System;
 using System.ClientModel.Primitives;
-using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
-using Azure.ResourceManager.Models;
 using Azure.ResourceManager.PostgreSql.Models;
 
 namespace Azure.ResourceManager.PostgreSql
