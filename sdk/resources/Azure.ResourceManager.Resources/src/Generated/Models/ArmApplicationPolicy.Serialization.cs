6c20f89625d998729b4778d50bd0daab3a51ffea
mcgallan <1186003288@qq.com>
2024-01-24 14:55:46 +0800

    regen

--- sdk/resources/Azure.ResourceManager.Resources/src/Generated/Models/ArmApplicationPolicy.Serialization.cs
+++ sdk/resources/Azure.ResourceManager.Resources/src/Generated/Models/ArmApplicationPolicy.Serialization.cs
@@ -5,15 +5,26 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.Resources.Models
 {
-    public partial class ArmApplicationPolicy : IUtf8JsonSerializable
+    public partial class ArmApplicationPolicy : IUtf8JsonSerializable, IJsonModel<ArmApplicationPolicy>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<ArmApplicationPolicy>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<ArmApplicationPolicy>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ArmApplicationPolicy>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ArmApplicationPolicy)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             if (Optional.IsDefined(Name))
             {
@@ -30,11 +41,40 @@ namespace Azure.ResourceManager.Resources.Models
                 writer.WritePropertyName("parameters"u8);
                 writer.WriteStringValue(Parameters);
             }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static ArmApplicationPolicy DeserializeArmApplicationPolicy(JsonElement element)
+        ArmApplicationPolicy IJsonModel<ArmApplicationPolicy>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ArmApplicationPolicy>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ArmApplicationPolicy)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeArmApplicationPolicy(document.RootElement, options);
+        }
+
+        internal static ArmApplicationPolicy DeserializeArmApplicationPolicy(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
@@ -42,6 +82,8 @@ namespace Azure.ResourceManager.Resources.Models
             Optional<string> name = default;
             Optional<string> policyDefinitionId = default;
             Optional<string> parameters = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("name"u8))
@@ -59,8 +101,44 @@ namespace Azure.ResourceManager.Resources.Models
                     parameters = property.Value.GetString();
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new ArmApplicationPolicy(name.Value, policyDefinitionId.Value, parameters.Value);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new ArmApplicationPolicy(name.Value, policyDefinitionId.Value, parameters.Value, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<ArmApplicationPolicy>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ArmApplicationPolicy>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(ArmApplicationPolicy)} does not support '{options.Format}' format.");
+            }
+        }
+
+        ArmApplicationPolicy IPersistableModel<ArmApplicationPolicy>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ArmApplicationPolicy>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeArmApplicationPolicy(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(ArmApplicationPolicy)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<ArmApplicationPolicy>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
--- sdk/resources/Azure.ResourceManager.Resources/src/Generated/Models/ArmDeploymentWhatIfContent.Serialization.cs
+++ sdk/resources/Azure.ResourceManager.Resources/src/Generated/Models/ArmDeploymentWhatIfContent.Serialization.cs
@@ -5,15 +5,26 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.Resources.Models
 {
-    public partial class ArmDeploymentWhatIfContent : IUtf8JsonSerializable
+    public partial class ArmDeploymentWhatIfContent : IUtf8JsonSerializable, IJsonModel<ArmDeploymentWhatIfContent>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<ArmDeploymentWhatIfContent>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<ArmDeploymentWhatIfContent>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ArmDeploymentWhatIfContent>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ArmDeploymentWhatIfContent)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             if (Optional.IsDefined(Location))
             {
@@ -22,7 +33,102 @@ namespace Azure.ResourceManager.Resources.Models
             }
             writer.WritePropertyName("properties"u8);
             writer.WriteObjectValue(Properties);
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
+
+        ArmDeploymentWhatIfContent IJsonModel<ArmDeploymentWhatIfContent>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ArmDeploymentWhatIfContent>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ArmDeploymentWhatIfContent)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeArmDeploymentWhatIfContent(document.RootElement, options);
+        }
+
+        internal static ArmDeploymentWhatIfContent DeserializeArmDeploymentWhatIfContent(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
+            if (element.ValueKind == JsonValueKind.Null)
+            {
+                return null;
+            }
+            Optional<AzureLocation> location = default;
+            ArmDeploymentWhatIfProperties properties = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
+            foreach (var property in element.EnumerateObject())
+            {
+                if (property.NameEquals("location"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        continue;
+                    }
+                    location = new AzureLocation(property.Value.GetString());
+                    continue;
+                }
+                if (property.NameEquals("properties"u8))
+                {
+                    properties = ArmDeploymentWhatIfProperties.DeserializeArmDeploymentWhatIfProperties(property.Value);
+                    continue;
+                }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
+            }
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new ArmDeploymentWhatIfContent(Optional.ToNullable(location), properties, serializedAdditionalRawData);
+        }
+
+        BinaryData IPersistableModel<ArmDeploymentWhatIfContent>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ArmDeploymentWhatIfContent>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(ArmDeploymentWhatIfContent)} does not support '{options.Format}' format.");
+            }
+        }
+
+        ArmDeploymentWhatIfContent IPersistableModel<ArmDeploymentWhatIfContent>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ArmDeploymentWhatIfContent>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeArmDeploymentWhatIfContent(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(ArmDeploymentWhatIfContent)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<ArmDeploymentWhatIfContent>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
