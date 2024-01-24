581819891170a29ad76551d864757e357051647f
mcgallan <1186003288@qq.com>
2024-01-24 16:57:15 +0800

    regen

--- sdk/servicefabric/Azure.ResourceManager.ServiceFabric/src/Generated/Models/ClusterEndpointRangeDescription.Serialization.cs
+++ sdk/servicefabric/Azure.ResourceManager.ServiceFabric/src/Generated/Models/ClusterEndpointRangeDescription.Serialization.cs
@@ -5,31 +5,73 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.ServiceFabric.Models
 {
-    public partial class ClusterEndpointRangeDescription : IUtf8JsonSerializable
+    public partial class ClusterEndpointRangeDescription : IUtf8JsonSerializable, IJsonModel<ClusterEndpointRangeDescription>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<ClusterEndpointRangeDescription>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<ClusterEndpointRangeDescription>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ClusterEndpointRangeDescription>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ClusterEndpointRangeDescription)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             writer.WritePropertyName("startPort"u8);
             writer.WriteNumberValue(StartPort);
             writer.WritePropertyName("endPort"u8);
             writer.WriteNumberValue(EndPort);
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static ClusterEndpointRangeDescription DeserializeClusterEndpointRangeDescription(JsonElement element)
+        ClusterEndpointRangeDescription IJsonModel<ClusterEndpointRangeDescription>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ClusterEndpointRangeDescription>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ClusterEndpointRangeDescription)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeClusterEndpointRangeDescription(document.RootElement, options);
+        }
+
+        internal static ClusterEndpointRangeDescription DeserializeClusterEndpointRangeDescription(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             int startPort = default;
             int endPort = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("startPort"u8))
@@ -42,8 +84,44 @@ namespace Azure.ResourceManager.ServiceFabric.Models
                     endPort = property.Value.GetInt32();
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new ClusterEndpointRangeDescription(startPort, endPort);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new ClusterEndpointRangeDescription(startPort, endPort, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<ClusterEndpointRangeDescription>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ClusterEndpointRangeDescription>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(ClusterEndpointRangeDescription)} does not support '{options.Format}' format.");
+            }
+        }
+
+        ClusterEndpointRangeDescription IPersistableModel<ClusterEndpointRangeDescription>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ClusterEndpointRangeDescription>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeClusterEndpointRangeDescription(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(ClusterEndpointRangeDescription)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<ClusterEndpointRangeDescription>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
