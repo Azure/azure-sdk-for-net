16e93674a8c3e3f6c2fd7b0526ed8e9996f7086b
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-24 22:09:53 +0800

    Enable the new model serialization for dataprotection (#41172)

--- sdk/dataprotection/Azure.ResourceManager.DataProtectionBackup/src/Generated/Models/UnknownDataStoreParameters.Serialization.cs
+++ sdk/dataprotection/Azure.ResourceManager.DataProtectionBackup/src/Generated/Models/UnknownDataStoreParameters.Serialization.cs
@@ -5,31 +5,73 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.DataProtectionBackup.Models
 {
-    internal partial class UnknownDataStoreParameters : IUtf8JsonSerializable
+    internal partial class UnknownDataStoreParameters : IUtf8JsonSerializable, IJsonModel<DataStoreSettings>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<DataStoreSettings>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<DataStoreSettings>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<DataStoreSettings>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(DataStoreSettings)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             writer.WritePropertyName("objectType"u8);
             writer.WriteStringValue(ObjectType);
             writer.WritePropertyName("dataStoreType"u8);
             writer.WriteStringValue(DataStoreType.ToString());
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static UnknownDataStoreParameters DeserializeUnknownDataStoreParameters(JsonElement element)
+        DataStoreSettings IJsonModel<DataStoreSettings>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<DataStoreSettings>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(DataStoreSettings)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeUnknownDataStoreParameters(document.RootElement, options);
+        }
+
+        internal static UnknownDataStoreParameters DeserializeUnknownDataStoreParameters(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             string objectType = "Unknown";
             DataStoreType dataStoreType = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("objectType"u8))
@@ -42,8 +84,44 @@ namespace Azure.ResourceManager.DataProtectionBackup.Models
                     dataStoreType = new DataStoreType(property.Value.GetString());
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new UnknownDataStoreParameters(objectType, dataStoreType);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new UnknownDataStoreParameters(objectType, dataStoreType, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<DataStoreSettings>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DataStoreSettings>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(DataStoreSettings)} does not support '{options.Format}' format.");
+            }
+        }
+
+        DataStoreSettings IPersistableModel<DataStoreSettings>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DataStoreSettings>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeUnknownDataStoreParameters(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(DataStoreSettings)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<DataStoreSettings>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
