88fbf5b598499c9801eac256df3ddef9b4fa968b
Chengming <v-cruan@microsoft.com>
2024-01-24 16:08:40 +0800

    add aaaa record modelfactory

---
+++ sdk/dns/Azure.ResourceManager.Dns/src/Customization/ArmDnsModelFactory.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System.Collections.Generic;
+using System.Linq;
+using Azure;
+using Azure.Core;
+using Azure.ResourceManager.Dns;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources.Models;
+
+namespace Azure.ResourceManager.Dns.Models
+{
+    /// <summary> Model factory for models. </summary>
+    public static partial class ArmDnsModelFactory
+    {
+        /// <summary> Initializes a new instance of DnsAaaaRecordData. </summary>
+        /// <param name="id"> The id. </param>
+        /// <param name="name"> The name. </param>
+        /// <param name="resourceType"> The resourceType. </param>
+        /// <param name="systemData"> The systemData. </param>
+        /// <param name="etag"> The etag of the record set. </param>
+        /// <param name="metadata"> The metadata attached to the record set. </param>
+        /// <param name="ttl"> The Ttl (time-to-live) of the records in the record set. </param>
+        /// <param name="fqdn"> Fully qualified domain name of the record set. </param>
+        /// <param name="provisioningState"> provisioning State of the record set. </param>
+        /// <param name="targetResource"> A reference to an azure resource from where the dns resource value is taken. </param>
+        /// <param name="aaaaRecords"> The list of AAAA records in the record set. </param>
+        public static DnsAaaaRecordData DnsAaaaRecordData(ResourceIdentifier id = null, string name = null, ResourceType resourceType = default, SystemData systemData = null, ETag? etag = default, IDictionary<string, string> metadata = null, long? ttl  = null, string fqdn = null, string provisioningState = null, WritableSubResource targetResource = null, IList<DnsAaaaRecordInfo> aaaaRecords = null)
+        {
+            return new DnsAaaaRecordData(id, name, resourceType, systemData, etag, metadata, ttl, fqdn, provisioningState, targetResource, aaaaRecords);
+        }
+    }
+}
---
+++ sdk/dns/Azure.ResourceManager.Dns/tests/Scenario/ModelFactoryTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.ResourceManager.Dns.Models;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.Resources.Models;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Dns.Tests.Scenario
+{
+    internal class ModelFactoryTests
+    {
+        private const string provisioningState = "Succeeded";
+
+        [Test]
+        public void AaaaRecordDataTest()
+        {
+            var id = new ResourceIdentifier("{/subscriptions/000000000-0000-0000-0000-000000000/resourceGroups/dnsrg2497/providers/Microsoft.Network/dnszones/2022dnszone9605.com/AAAA/aaaa9739}");
+            string name = "aaaa";
+            ResourceType resourceType = "Microsoft.Network/dnszones/AAAA";
+            SystemData systemData = default;
+            ETag? etag = new ETag("75a9c76c-eeee-5555-9999-109960666a19");
+            IDictionary<string, string> metadata = default;
+            long? ttl = 3600;
+            string fqdn = "aaaa3349.2022dnszone6060.com.";
+            WritableSubResource targetResource = null;
+            List<DnsAaaaRecordInfo> aaaaRecords = new List<DnsAaaaRecordInfo>
+            {
+                new DnsAaaaRecordInfo()
+                {
+                    IPv6Address = IPAddress.Parse("3f0d:8079:32a1:9c1d:dd7c:afc6:fc15:d55")
+                },
+                new DnsAaaaRecordInfo()
+                {
+                    IPv6Address = IPAddress.Parse("3f0d:8079:32a1:9c1d:dd7c:afc6:fc15:d56")
+                }
+            };
+            var aaaa = ArmDnsModelFactory.DnsAaaaRecordData(id, name, resourceType, systemData, etag, metadata, ttl, fqdn, provisioningState, targetResource, aaaaRecords);
+            Assert.AreEqual(3600, aaaa.TtlInSeconds);
+        }
+    }
+}
