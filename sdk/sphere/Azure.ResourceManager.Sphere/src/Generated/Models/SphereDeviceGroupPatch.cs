61ce95b1112bf1349116a28b7b77cc89aca2ae45
mcgallan <1186003288@qq.com>
2024-01-25 13:54:46 +0800

    regen

--- sdk/sphere/Azure.ResourceManager.Sphere/src/Generated/Models/SphereDeviceGroupPatch.cs
+++ sdk/sphere/Azure.ResourceManager.Sphere/src/Generated/Models/SphereDeviceGroupPatch.cs
@@ -5,11 +5,46 @@
 
 #nullable disable
 
+using System;
+using System.Collections.Generic;
+
 namespace Azure.ResourceManager.Sphere.Models
 {
     /// <summary> The type used for update operations of the DeviceGroup. </summary>
     public partial class SphereDeviceGroupPatch
     {
+        /// <summary>
+        /// Keeps track of any properties unknown to the library.
+        /// <para>
+        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
+        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+
         /// <summary> Initializes a new instance of <see cref="SphereDeviceGroupPatch"/>. </summary>
         public SphereDeviceGroupPatch()
         {
@@ -21,13 +56,15 @@ namespace Azure.ResourceManager.Sphere.Models
         /// <param name="updatePolicy"> Update policy of the device group. </param>
         /// <param name="allowCrashDumpsCollection"> Flag to define if the user allows for crash dump collection. </param>
         /// <param name="regionalDataBoundary"> Regional data boundary for the device group. </param>
-        internal SphereDeviceGroupPatch(string description, SphereOSFeedType? osFeedType, SphereUpdatePolicy? updatePolicy, SphereAllowCrashDumpCollectionStatus? allowCrashDumpsCollection, RegionalDataBoundary? regionalDataBoundary)
+        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
+        internal SphereDeviceGroupPatch(string description, SphereOSFeedType? osFeedType, SphereUpdatePolicy? updatePolicy, SphereAllowCrashDumpCollectionStatus? allowCrashDumpsCollection, RegionalDataBoundary? regionalDataBoundary, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
             Description = description;
             OSFeedType = osFeedType;
             UpdatePolicy = updatePolicy;
             AllowCrashDumpsCollection = allowCrashDumpsCollection;
             RegionalDataBoundary = regionalDataBoundary;
+            _serializedAdditionalRawData = serializedAdditionalRawData;
         }
 
         /// <summary> Description of the device group. </summary>
--- sdk/sphere/Azure.ResourceManager.Sphere/src/Generated/Models/SphereDevicePatch.Serialization.cs
+++ sdk/sphere/Azure.ResourceManager.Sphere/src/Generated/Models/SphereDevicePatch.Serialization.cs
@@ -5,15 +5,26 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.Sphere.Models
 {
-    public partial class SphereDevicePatch : IUtf8JsonSerializable
+    public partial class SphereDevicePatch : IUtf8JsonSerializable, IJsonModel<SphereDevicePatch>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<SphereDevicePatch>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<SphereDevicePatch>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<SphereDevicePatch>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(SphereDevicePatch)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             writer.WritePropertyName("properties"u8);
             writer.WriteStartObject();
@@ -23,7 +34,104 @@ namespace Azure.ResourceManager.Sphere.Models
                 writer.WriteStringValue(DeviceGroupId);
             }
             writer.WriteEndObject();
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
+
+        SphereDevicePatch IJsonModel<SphereDevicePatch>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<SphereDevicePatch>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(SphereDevicePatch)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeSphereDevicePatch(document.RootElement, options);
+        }
+
+        internal static SphereDevicePatch DeserializeSphereDevicePatch(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
+            if (element.ValueKind == JsonValueKind.Null)
+            {
+                return null;
+            }
+            Optional<string> deviceGroupId = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
+            foreach (var property in element.EnumerateObject())
+            {
+                if (property.NameEquals("properties"u8))
+                {
+                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    {
+                        property.ThrowNonNullablePropertyIsNull();
+                        continue;
+                    }
+                    foreach (var property0 in property.Value.EnumerateObject())
+                    {
+                        if (property0.NameEquals("deviceGroupId"u8))
+                        {
+                            deviceGroupId = property0.Value.GetString();
+                            continue;
+                        }
+                    }
+                    continue;
+                }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
+            }
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new SphereDevicePatch(deviceGroupId.Value, serializedAdditionalRawData);
+        }
+
+        BinaryData IPersistableModel<SphereDevicePatch>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<SphereDevicePatch>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(SphereDevicePatch)} does not support '{options.Format}' format.");
+            }
+        }
+
+        SphereDevicePatch IPersistableModel<SphereDevicePatch>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<SphereDevicePatch>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeSphereDevicePatch(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(SphereDevicePatch)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<SphereDevicePatch>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
