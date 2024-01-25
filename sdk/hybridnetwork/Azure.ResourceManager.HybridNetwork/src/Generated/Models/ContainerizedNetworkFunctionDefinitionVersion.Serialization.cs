c55a3f78ab3784dcba6f22975342e9317c878354
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-25 18:43:00 +0800

    Enable the new model serialization for hybirdnetwork (#41340)

--- sdk/hybridnetwork/Azure.ResourceManager.HybridNetwork/src/Generated/Models/ContainerizedNetworkFunctionDefinitionVersion.Serialization.cs
+++ sdk/hybridnetwork/Azure.ResourceManager.HybridNetwork/src/Generated/Models/ContainerizedNetworkFunctionDefinitionVersion.Serialization.cs
@@ -5,20 +5,41 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.HybridNetwork.Models
 {
-    public partial class ContainerizedNetworkFunctionDefinitionVersion : IUtf8JsonSerializable
+    public partial class ContainerizedNetworkFunctionDefinitionVersion : IUtf8JsonSerializable, IJsonModel<ContainerizedNetworkFunctionDefinitionVersion>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<ContainerizedNetworkFunctionDefinitionVersion>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<ContainerizedNetworkFunctionDefinitionVersion>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ContainerizedNetworkFunctionDefinitionVersion>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ContainerizedNetworkFunctionDefinitionVersion)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             if (Optional.IsDefined(NetworkFunctionTemplate))
             {
                 writer.WritePropertyName("networkFunctionTemplate"u8);
                 writer.WriteObjectValue(NetworkFunctionTemplate);
+            }
+            if (options.Format != "W" && Optional.IsDefined(ProvisioningState))
+            {
+                writer.WritePropertyName("provisioningState"u8);
+                writer.WriteStringValue(ProvisioningState.Value.ToString());
+            }
+            if (options.Format != "W" && Optional.IsDefined(VersionState))
+            {
+                writer.WritePropertyName("versionState"u8);
+                writer.WriteStringValue(VersionState.Value.ToString());
             }
             if (Optional.IsDefined(Description))
             {
@@ -32,11 +53,40 @@ namespace Azure.ResourceManager.HybridNetwork.Models
             }
             writer.WritePropertyName("networkFunctionType"u8);
             writer.WriteStringValue(NetworkFunctionType.ToString());
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static ContainerizedNetworkFunctionDefinitionVersion DeserializeContainerizedNetworkFunctionDefinitionVersion(JsonElement element)
+        ContainerizedNetworkFunctionDefinitionVersion IJsonModel<ContainerizedNetworkFunctionDefinitionVersion>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ContainerizedNetworkFunctionDefinitionVersion>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ContainerizedNetworkFunctionDefinitionVersion)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeContainerizedNetworkFunctionDefinitionVersion(document.RootElement, options);
+        }
+
+        internal static ContainerizedNetworkFunctionDefinitionVersion DeserializeContainerizedNetworkFunctionDefinitionVersion(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
@@ -47,6 +97,8 @@ namespace Azure.ResourceManager.HybridNetwork.Models
             Optional<string> description = default;
             Optional<string> deployParameters = default;
             NetworkFunctionType networkFunctionType = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("networkFunctionTemplate"u8))
@@ -91,8 +143,44 @@ namespace Azure.ResourceManager.HybridNetwork.Models
                     networkFunctionType = new NetworkFunctionType(property.Value.GetString());
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new ContainerizedNetworkFunctionDefinitionVersion(Optional.ToNullable(provisioningState), Optional.ToNullable(versionState), description.Value, deployParameters.Value, networkFunctionType, networkFunctionTemplate.Value);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new ContainerizedNetworkFunctionDefinitionVersion(Optional.ToNullable(provisioningState), Optional.ToNullable(versionState), description.Value, deployParameters.Value, networkFunctionType, serializedAdditionalRawData, networkFunctionTemplate.Value);
         }
+
+        BinaryData IPersistableModel<ContainerizedNetworkFunctionDefinitionVersion>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ContainerizedNetworkFunctionDefinitionVersion>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(ContainerizedNetworkFunctionDefinitionVersion)} does not support '{options.Format}' format.");
+            }
+        }
+
+        ContainerizedNetworkFunctionDefinitionVersion IPersistableModel<ContainerizedNetworkFunctionDefinitionVersion>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ContainerizedNetworkFunctionDefinitionVersion>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeContainerizedNetworkFunctionDefinitionVersion(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(ContainerizedNetworkFunctionDefinitionVersion)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<ContainerizedNetworkFunctionDefinitionVersion>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
