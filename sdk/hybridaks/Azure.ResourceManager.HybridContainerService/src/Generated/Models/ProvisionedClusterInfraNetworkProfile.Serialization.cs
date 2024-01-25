2fdbbca329b0aee8b686d2766955267b33ef6e50
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-25 18:41:12 +0800

    Enable the new model serialization for hybirdaks (#41335)

--- sdk/hybridaks/Azure.ResourceManager.HybridContainerService/src/Generated/Models/ProvisionedClusterInfraNetworkProfile.Serialization.cs
+++ sdk/hybridaks/Azure.ResourceManager.HybridContainerService/src/Generated/Models/ProvisionedClusterInfraNetworkProfile.Serialization.cs
@@ -5,16 +5,26 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.HybridContainerService.Models
 {
-    internal partial class ProvisionedClusterInfraNetworkProfile : IUtf8JsonSerializable
+    internal partial class ProvisionedClusterInfraNetworkProfile : IUtf8JsonSerializable, IJsonModel<ProvisionedClusterInfraNetworkProfile>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<ProvisionedClusterInfraNetworkProfile>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<ProvisionedClusterInfraNetworkProfile>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ProvisionedClusterInfraNetworkProfile>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ProvisionedClusterInfraNetworkProfile)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             if (Optional.IsCollectionDefined(VnetSubnetIds))
             {
@@ -31,16 +41,47 @@ namespace Azure.ResourceManager.HybridContainerService.Models
                 }
                 writer.WriteEndArray();
             }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static ProvisionedClusterInfraNetworkProfile DeserializeProvisionedClusterInfraNetworkProfile(JsonElement element)
+        ProvisionedClusterInfraNetworkProfile IJsonModel<ProvisionedClusterInfraNetworkProfile>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<ProvisionedClusterInfraNetworkProfile>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(ProvisionedClusterInfraNetworkProfile)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeProvisionedClusterInfraNetworkProfile(document.RootElement, options);
+        }
+
+        internal static ProvisionedClusterInfraNetworkProfile DeserializeProvisionedClusterInfraNetworkProfile(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             Optional<IList<ResourceIdentifier>> vnetSubnetIds = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("vnetSubnetIds"u8))
@@ -64,8 +105,44 @@ namespace Azure.ResourceManager.HybridContainerService.Models
                     vnetSubnetIds = array;
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new ProvisionedClusterInfraNetworkProfile(Optional.ToList(vnetSubnetIds));
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new ProvisionedClusterInfraNetworkProfile(Optional.ToList(vnetSubnetIds), serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<ProvisionedClusterInfraNetworkProfile>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ProvisionedClusterInfraNetworkProfile>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(ProvisionedClusterInfraNetworkProfile)} does not support '{options.Format}' format.");
+            }
+        }
+
+        ProvisionedClusterInfraNetworkProfile IPersistableModel<ProvisionedClusterInfraNetworkProfile>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<ProvisionedClusterInfraNetworkProfile>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeProvisionedClusterInfraNetworkProfile(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(ProvisionedClusterInfraNetworkProfile)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<ProvisionedClusterInfraNetworkProfile>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
