bbe195fe0499d589081f6345e06bb3799857e417
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-26 18:34:25 +0800

    Enable the new model serialization for support (#41571)

--- sdk/support/Azure.ResourceManager.Support/src/Generated/Models/QuotaTicketDetails.Serialization.cs
+++ sdk/support/Azure.ResourceManager.Support/src/Generated/Models/QuotaTicketDetails.Serialization.cs
@@ -5,16 +5,26 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.Support.Models
 {
-    public partial class QuotaTicketDetails : IUtf8JsonSerializable
+    public partial class QuotaTicketDetails : IUtf8JsonSerializable, IJsonModel<QuotaTicketDetails>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<QuotaTicketDetails>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<QuotaTicketDetails>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<QuotaTicketDetails>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(QuotaTicketDetails)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             if (Optional.IsDefined(QuotaChangeRequestSubType))
             {
@@ -36,11 +46,40 @@ namespace Azure.ResourceManager.Support.Models
                 }
                 writer.WriteEndArray();
             }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static QuotaTicketDetails DeserializeQuotaTicketDetails(JsonElement element)
+        QuotaTicketDetails IJsonModel<QuotaTicketDetails>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<QuotaTicketDetails>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(QuotaTicketDetails)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeQuotaTicketDetails(document.RootElement, options);
+        }
+
+        internal static QuotaTicketDetails DeserializeQuotaTicketDetails(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
@@ -48,6 +87,8 @@ namespace Azure.ResourceManager.Support.Models
             Optional<string> quotaChangeRequestSubType = default;
             Optional<string> quotaChangeRequestVersion = default;
             Optional<IList<SupportQuotaChangeContent>> quotaChangeRequests = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("quotaChangeRequestSubType"u8))
@@ -74,8 +115,44 @@ namespace Azure.ResourceManager.Support.Models
                     quotaChangeRequests = array;
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new QuotaTicketDetails(quotaChangeRequestSubType.Value, quotaChangeRequestVersion.Value, Optional.ToList(quotaChangeRequests));
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new QuotaTicketDetails(quotaChangeRequestSubType.Value, quotaChangeRequestVersion.Value, Optional.ToList(quotaChangeRequests), serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<QuotaTicketDetails>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<QuotaTicketDetails>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(QuotaTicketDetails)} does not support '{options.Format}' format.");
+            }
+        }
+
+        QuotaTicketDetails IPersistableModel<QuotaTicketDetails>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<QuotaTicketDetails>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeQuotaTicketDetails(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(QuotaTicketDetails)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<QuotaTicketDetails>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
