5f984140b7db7b531dec819d090c06ad2d366374
azure-sdk <azuresdk@microsoft.com>
2024-01-26 08:10:36 +0000

    Update SDK codes co_da_1

--- sdk/contentsafety/Azure.AI.ContentSafety/src/Generated/AddOrUpdateTextBlocklistItemsResult.Serialization.cs
+++ sdk/contentsafety/Azure.AI.ContentSafety/src/Generated/AddOrUpdateTextBlocklistItemsResult.Serialization.cs
@@ -5,21 +5,76 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure;
+using Azure.Core;
 
 namespace Azure.AI.ContentSafety
 {
-    public partial class AddOrUpdateTextBlocklistItemsResult
+    public partial class AddOrUpdateTextBlocklistItemsResult : IUtf8JsonSerializable, IJsonModel<AddOrUpdateTextBlocklistItemsResult>
     {
-        internal static AddOrUpdateTextBlocklistItemsResult DeserializeAddOrUpdateTextBlocklistItemsResult(JsonElement element)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<AddOrUpdateTextBlocklistItemsResult>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<AddOrUpdateTextBlocklistItemsResult>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<AddOrUpdateTextBlocklistItemsResult>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(AddOrUpdateTextBlocklistItemsResult)} does not support '{format}' format.");
+            }
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("blocklistItems"u8);
+            writer.WriteStartArray();
+            foreach (var item in BlocklistItems)
+            {
+                writer.WriteObjectValue(item);
+            }
+            writer.WriteEndArray();
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
+            writer.WriteEndObject();
+        }
+
+        AddOrUpdateTextBlocklistItemsResult IJsonModel<AddOrUpdateTextBlocklistItemsResult>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<AddOrUpdateTextBlocklistItemsResult>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(AddOrUpdateTextBlocklistItemsResult)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeAddOrUpdateTextBlocklistItemsResult(document.RootElement, options);
+        }
+
+        internal static AddOrUpdateTextBlocklistItemsResult DeserializeAddOrUpdateTextBlocklistItemsResult(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             IReadOnlyList<TextBlocklistItem> blocklistItems = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("blocklistItems"u8))
@@ -32,9 +87,45 @@ namespace Azure.AI.ContentSafety
                     blocklistItems = array;
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new AddOrUpdateTextBlocklistItemsResult(blocklistItems);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new AddOrUpdateTextBlocklistItemsResult(blocklistItems, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<AddOrUpdateTextBlocklistItemsResult>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<AddOrUpdateTextBlocklistItemsResult>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(AddOrUpdateTextBlocklistItemsResult)} does not support '{options.Format}' format.");
+            }
+        }
+
+        AddOrUpdateTextBlocklistItemsResult IPersistableModel<AddOrUpdateTextBlocklistItemsResult>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<AddOrUpdateTextBlocklistItemsResult>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeAddOrUpdateTextBlocklistItemsResult(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(AddOrUpdateTextBlocklistItemsResult)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<AddOrUpdateTextBlocklistItemsResult>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
 
         /// <summary> Deserializes the model from a raw response. </summary>
         /// <param name="response"> The response to deserialize the model from. </param>
@@ -42,6 +133,14 @@ namespace Azure.AI.ContentSafety
         {
             using var document = JsonDocument.Parse(response.Content);
             return DeserializeAddOrUpdateTextBlocklistItemsResult(document.RootElement);
+        }
+
+        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        internal virtual RequestContent ToRequestContent()
+        {
+            var content = new Utf8JsonRequestContent();
+            content.JsonWriter.WriteObjectValue(this);
+            return content;
         }
     }
 }
