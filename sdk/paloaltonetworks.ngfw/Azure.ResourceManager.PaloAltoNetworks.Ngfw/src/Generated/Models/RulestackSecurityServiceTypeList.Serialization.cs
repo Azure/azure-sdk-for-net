6bdaa4e6fa33144c66cfa93cdcbeb59eb4175821
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-26 18:19:45 +0800

    Enable the new model serialization for paloaltonetworks (#41447)

--- sdk/paloaltonetworks.ngfw/Azure.ResourceManager.PaloAltoNetworks.Ngfw/src/Generated/Models/RulestackSecurityServiceTypeList.Serialization.cs
+++ sdk/paloaltonetworks.ngfw/Azure.ResourceManager.PaloAltoNetworks.Ngfw/src/Generated/Models/RulestackSecurityServiceTypeList.Serialization.cs
@@ -5,22 +5,81 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.PaloAltoNetworks.Ngfw.Models
 {
-    public partial class RulestackSecurityServiceTypeList
+    public partial class RulestackSecurityServiceTypeList : IUtf8JsonSerializable, IJsonModel<RulestackSecurityServiceTypeList>
     {
-        internal static RulestackSecurityServiceTypeList DeserializeRulestackSecurityServiceTypeList(JsonElement element)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<RulestackSecurityServiceTypeList>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<RulestackSecurityServiceTypeList>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<RulestackSecurityServiceTypeList>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(RulestackSecurityServiceTypeList)} does not support '{format}' format.");
+            }
+
+            writer.WriteStartObject();
+            if (Optional.IsDefined(SecurityServicesTypeListType))
+            {
+                writer.WritePropertyName("type"u8);
+                writer.WriteStringValue(SecurityServicesTypeListType);
+            }
+            writer.WritePropertyName("entry"u8);
+            writer.WriteStartArray();
+            foreach (var item in Entry)
+            {
+                writer.WriteObjectValue(item);
+            }
+            writer.WriteEndArray();
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
+            writer.WriteEndObject();
+        }
+
+        RulestackSecurityServiceTypeList IJsonModel<RulestackSecurityServiceTypeList>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<RulestackSecurityServiceTypeList>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(RulestackSecurityServiceTypeList)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeRulestackSecurityServiceTypeList(document.RootElement, options);
+        }
+
+        internal static RulestackSecurityServiceTypeList DeserializeRulestackSecurityServiceTypeList(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             Optional<string> type = default;
             IReadOnlyList<NameDescriptionObject> entry = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("type"u8))
@@ -38,8 +97,44 @@ namespace Azure.ResourceManager.PaloAltoNetworks.Ngfw.Models
                     entry = array;
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new RulestackSecurityServiceTypeList(type.Value, entry);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new RulestackSecurityServiceTypeList(type.Value, entry, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<RulestackSecurityServiceTypeList>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<RulestackSecurityServiceTypeList>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(RulestackSecurityServiceTypeList)} does not support '{options.Format}' format.");
+            }
+        }
+
+        RulestackSecurityServiceTypeList IPersistableModel<RulestackSecurityServiceTypeList>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<RulestackSecurityServiceTypeList>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeRulestackSecurityServiceTypeList(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(RulestackSecurityServiceTypeList)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<RulestackSecurityServiceTypeList>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
