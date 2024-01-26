d73293e6a7340cbb0d31f9150389413147334b5d
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-26 18:08:29 +0800

    Enable the new model serialization for quantum (#41480)

--- sdk/quantum/Azure.ResourceManager.Quantum/src/Generated/Models/PricingDimension.Serialization.cs
+++ sdk/quantum/Azure.ResourceManager.Quantum/src/Generated/Models/PricingDimension.Serialization.cs
@@ -5,21 +5,79 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.Quantum.Models
 {
-    public partial class PricingDimension
+    public partial class PricingDimension : IUtf8JsonSerializable, IJsonModel<PricingDimension>
     {
-        internal static PricingDimension DeserializePricingDimension(JsonElement element)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<PricingDimension>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<PricingDimension>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<PricingDimension>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(PricingDimension)} does not support '{format}' format.");
+            }
+
+            writer.WriteStartObject();
+            if (Optional.IsDefined(Id))
+            {
+                writer.WritePropertyName("id"u8);
+                writer.WriteStringValue(Id);
+            }
+            if (Optional.IsDefined(Name))
+            {
+                writer.WritePropertyName("name"u8);
+                writer.WriteStringValue(Name);
+            }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
+            writer.WriteEndObject();
+        }
+
+        PricingDimension IJsonModel<PricingDimension>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<PricingDimension>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(PricingDimension)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializePricingDimension(document.RootElement, options);
+        }
+
+        internal static PricingDimension DeserializePricingDimension(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             Optional<string> id = default;
             Optional<string> name = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("id"u8))
@@ -32,8 +90,44 @@ namespace Azure.ResourceManager.Quantum.Models
                     name = property.Value.GetString();
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new PricingDimension(id.Value, name.Value);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new PricingDimension(id.Value, name.Value, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<PricingDimension>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<PricingDimension>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(PricingDimension)} does not support '{options.Format}' format.");
+            }
+        }
+
+        PricingDimension IPersistableModel<PricingDimension>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<PricingDimension>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializePricingDimension(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(PricingDimension)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<PricingDimension>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
