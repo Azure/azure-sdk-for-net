c106aad68297868eeb8e4cd503554b7a2d829a04
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-26 18:30:04 +0800

    Enable the new model serialization for spring app discovery (#41559)

--- sdk/springappdiscovery/Azure.ResourceManager.SpringAppDiscovery/src/Generated/Models/SpringBootSiteErrorSummaryModel.cs
+++ sdk/springappdiscovery/Azure.ResourceManager.SpringAppDiscovery/src/Generated/Models/SpringBootSiteErrorSummaryModel.cs
@@ -5,6 +5,9 @@
 
 #nullable disable
 
+using System;
+using System.Collections.Generic;
+
 namespace Azure.ResourceManager.SpringAppDiscovery.Models
 {
     /// <summary>
@@ -13,6 +16,38 @@ namespace Azure.ResourceManager.SpringAppDiscovery.Models
     /// </summary>
     public partial class SpringBootSiteErrorSummaryModel
     {
+        /// <summary>
+        /// Keeps track of any properties unknown to the library.
+        /// <para>
+        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
+        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+
         /// <summary> Initializes a new instance of <see cref="SpringBootSiteErrorSummaryModel"/>. </summary>
         public SpringBootSiteErrorSummaryModel()
         {
@@ -27,10 +62,12 @@ namespace Azure.ResourceManager.SpringAppDiscovery.Models
         /// The count.
         /// Serialized Name: ErrorSummaryModel.affectedObjectsCount
         /// </param>
-        internal SpringBootSiteErrorSummaryModel(string affectedResourceType, long? affectedObjectsCount)
+        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
+        internal SpringBootSiteErrorSummaryModel(string affectedResourceType, long? affectedObjectsCount, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
             AffectedResourceType = affectedResourceType;
             AffectedObjectsCount = affectedObjectsCount;
+            _serializedAdditionalRawData = serializedAdditionalRawData;
         }
 
         /// <summary>
