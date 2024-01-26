6dd6379d6845b8a9334b1ac6409ba3bf800fed2b
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-26 13:29:07 +0800

    Enable the new model serialization for dns (#41258)

--- sdk/dns/Azure.ResourceManager.Dns/src/Generated/Models/DnsCnameRecordInfo.Serialization.cs
+++ sdk/dns/Azure.ResourceManager.Dns/src/Generated/Models/DnsCnameRecordInfo.Serialization.cs
@@ -5,31 +5,73 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
 
 namespace Azure.ResourceManager.Dns.Models
 {
-    internal partial class DnsCnameRecordInfo : IUtf8JsonSerializable
+    internal partial class DnsCnameRecordInfo : IUtf8JsonSerializable, IJsonModel<DnsCnameRecordInfo>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<DnsCnameRecordInfo>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<DnsCnameRecordInfo>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<DnsCnameRecordInfo>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(DnsCnameRecordInfo)} does not support '{format}' format.");
+            }
+
             writer.WriteStartObject();
             if (Optional.IsDefined(Cname))
             {
                 writer.WritePropertyName("cname"u8);
                 writer.WriteStringValue(Cname);
             }
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
             writer.WriteEndObject();
         }
 
-        internal static DnsCnameRecordInfo DeserializeDnsCnameRecordInfo(JsonElement element)
+        DnsCnameRecordInfo IJsonModel<DnsCnameRecordInfo>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<DnsCnameRecordInfo>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(DnsCnameRecordInfo)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeDnsCnameRecordInfo(document.RootElement, options);
+        }
+
+        internal static DnsCnameRecordInfo DeserializeDnsCnameRecordInfo(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             Optional<string> cname = default;
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("cname"u8))
@@ -37,8 +79,44 @@ namespace Azure.ResourceManager.Dns.Models
                     cname = property.Value.GetString();
                     continue;
                 }
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new DnsCnameRecordInfo(cname.Value);
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new DnsCnameRecordInfo(cname.Value, serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<DnsCnameRecordInfo>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DnsCnameRecordInfo>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(DnsCnameRecordInfo)} does not support '{options.Format}' format.");
+            }
+        }
+
+        DnsCnameRecordInfo IPersistableModel<DnsCnameRecordInfo>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<DnsCnameRecordInfo>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeDnsCnameRecordInfo(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(DnsCnameRecordInfo)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<DnsCnameRecordInfo>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
