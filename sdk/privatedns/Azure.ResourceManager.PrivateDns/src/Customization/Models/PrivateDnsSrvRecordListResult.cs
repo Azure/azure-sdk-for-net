2dd0b29686a3948a7d0c1694c8c7b6445f636484
mcgallan <1186003288@qq.com>
2024-01-26 15:21:47 +0800

    Update

--- sdk/privatedns/Azure.ResourceManager.PrivateDns/src/Customization/Models/PrivateDnsSrvRecordListResult.cs
+++ sdk/privatedns/Azure.ResourceManager.PrivateDns/src/Customization/Models/PrivateDnsSrvRecordListResult.cs
@@ -3,6 +3,7 @@
 
 #nullable disable
 
+using System;
 using System.Collections.Generic;
 using Azure.Core;
 using Azure.ResourceManager.PrivateDns;
@@ -12,6 +13,37 @@ namespace Azure.ResourceManager.PrivateDns.Models
     /// <summary> The response to a record set List operation. </summary>
     internal partial class PrivateDnsSrvRecordListResult
     {
+        /// <summary>
+        /// Keeps track of any properties unknown to the library.
+        /// <para>
+        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
+        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
         /// <summary> Initializes a new instance of PrivateDnsSrvRecordSetListResult. </summary>
         internal PrivateDnsSrvRecordListResult()
         {
@@ -21,10 +53,12 @@ namespace Azure.ResourceManager.PrivateDns.Models
         /// <summary> Initializes a new instance of PrivateDnsSrvRecordSetListResult. </summary>
         /// <param name="value"> Information about the record sets in the response. </param>
         /// <param name="nextLink"> The continuation token for the next page of results. </param>
-        internal PrivateDnsSrvRecordListResult(IReadOnlyList<PrivateDnsSrvRecordData> value, string nextLink)
+        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
+        internal PrivateDnsSrvRecordListResult(IReadOnlyList<PrivateDnsSrvRecordData> value, string nextLink, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
             Value = value;
             NextLink = nextLink;
+            _serializedAdditionalRawData = serializedAdditionalRawData;
         }
 
         /// <summary> Information about the record sets in the response. </summary>
