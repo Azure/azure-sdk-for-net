dfea727a6a760d7f48bc6bc04153f1d6487faa36
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-01-29 18:56:05 +0800

    Enable the new model serialization for workloads (#41601)

--- sdk/workloads/Azure.ResourceManager.Workloads/src/Generated/Models/DatabaseConfiguration.cs
+++ sdk/workloads/Azure.ResourceManager.Workloads/src/Generated/Models/DatabaseConfiguration.cs
@@ -14,6 +14,38 @@ namespace Azure.ResourceManager.Workloads.Models
     /// <summary> Gets or sets the database configuration. </summary>
     public partial class DatabaseConfiguration
     {
+        /// <summary>
+        /// Keeps track of any properties unknown to the library.
+        /// <para>
+        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
+        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+
         /// <summary> Initializes a new instance of <see cref="DatabaseConfiguration"/>. </summary>
         /// <param name="subnetId"> The subnet id. </param>
         /// <param name="virtualMachineConfiguration"> Gets or sets the virtual machine configuration. </param>
@@ -35,13 +67,20 @@ namespace Azure.ResourceManager.Workloads.Models
         /// <param name="virtualMachineConfiguration"> Gets or sets the virtual machine configuration. </param>
         /// <param name="instanceCount"> The number of database VMs. </param>
         /// <param name="diskConfiguration"> Gets or sets the disk configuration. </param>
-        internal DatabaseConfiguration(SapDatabaseType? databaseType, ResourceIdentifier subnetId, SapVirtualMachineConfiguration virtualMachineConfiguration, long instanceCount, DiskConfiguration diskConfiguration)
+        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
+        internal DatabaseConfiguration(SapDatabaseType? databaseType, ResourceIdentifier subnetId, SapVirtualMachineConfiguration virtualMachineConfiguration, long instanceCount, DiskConfiguration diskConfiguration, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
             DatabaseType = databaseType;
             SubnetId = subnetId;
             VirtualMachineConfiguration = virtualMachineConfiguration;
             InstanceCount = instanceCount;
             DiskConfiguration = diskConfiguration;
+            _serializedAdditionalRawData = serializedAdditionalRawData;
+        }
+
+        /// <summary> Initializes a new instance of <see cref="DatabaseConfiguration"/> for deserialization. </summary>
+        internal DatabaseConfiguration()
+        {
         }
 
         /// <summary> The database type. </summary>
