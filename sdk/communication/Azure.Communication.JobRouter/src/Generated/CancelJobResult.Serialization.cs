5496c1a8f6c0e634ec8af7d96839ed4c5ba2460f
Arcturus Zhang <dapzhang@microsoft.com>
2024-01-29 11:16:34 +0800

    regenerate

--- sdk/communication/Azure.Communication.JobRouter/src/Generated/CancelJobResult.Serialization.cs
+++ sdk/communication/Azure.Communication.JobRouter/src/Generated/CancelJobResult.Serialization.cs
@@ -5,24 +5,109 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
 using System.Text.Json;
 using Azure;
+using Azure.Core;
 
 namespace Azure.Communication.JobRouter
 {
-    internal partial class CancelJobResult
+    internal partial class CancelJobResult : IUtf8JsonSerializable, IJsonModel<CancelJobResult>
     {
-        internal static CancelJobResult DeserializeCancelJobResult(JsonElement element)
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<CancelJobResult>)this).Write(writer, new ModelReaderWriterOptions("W"));
+
+        void IJsonModel<CancelJobResult>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            var format = options.Format == "W" ? ((IPersistableModel<CancelJobResult>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(CancelJobResult)} does not support '{format}' format.");
+            }
+
+            writer.WriteStartObject();
+            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            {
+                foreach (var item in _serializedAdditionalRawData)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+				writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
+            writer.WriteEndObject();
+        }
+
+        CancelJobResult IJsonModel<CancelJobResult>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<CancelJobResult>)this).GetFormatFromOptions(options) : options.Format;
+            if (format != "J")
+            {
+                throw new FormatException($"The model {nameof(CancelJobResult)} does not support '{format}' format.");
+            }
+
+            using JsonDocument document = JsonDocument.ParseValue(ref reader);
+            return DeserializeCancelJobResult(document.RootElement, options);
+        }
+
+        internal static CancelJobResult DeserializeCancelJobResult(JsonElement element, ModelReaderWriterOptions options = null)
+        {
+            options ??= new ModelReaderWriterOptions("W");
+
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
+            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
+            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
+                if (options.Format != "W")
+                {
+                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                }
             }
-            return new CancelJobResult();
+            serializedAdditionalRawData = additionalPropertiesDictionary;
+            return new CancelJobResult(serializedAdditionalRawData);
         }
+
+        BinaryData IPersistableModel<CancelJobResult>.Write(ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<CancelJobResult>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options);
+                default:
+                    throw new FormatException($"The model {nameof(CancelJobResult)} does not support '{options.Format}' format.");
+            }
+        }
+
+        CancelJobResult IPersistableModel<CancelJobResult>.Create(BinaryData data, ModelReaderWriterOptions options)
+        {
+            var format = options.Format == "W" ? ((IPersistableModel<CancelJobResult>)this).GetFormatFromOptions(options) : options.Format;
+
+            switch (format)
+            {
+                case "J":
+                    {
+                        using JsonDocument document = JsonDocument.Parse(data);
+                        return DeserializeCancelJobResult(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(CancelJobResult)} does not support '{options.Format}' format.");
+            }
+        }
+
+        string IPersistableModel<CancelJobResult>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
 
         /// <summary> Deserializes the model from a raw response. </summary>
         /// <param name="response"> The response to deserialize the model from. </param>
@@ -30,6 +115,14 @@ namespace Azure.Communication.JobRouter
         {
             using var document = JsonDocument.Parse(response.Content);
             return DeserializeCancelJobResult(document.RootElement);
+        }
+
+        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        internal virtual RequestContent ToRequestContent()
+        {
+            var content = new Utf8JsonRequestContent();
+            content.JsonWriter.WriteObjectValue(this);
+            return content;
         }
     }
 }
