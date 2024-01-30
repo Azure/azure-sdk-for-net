2cc2cee35538ebbd4ee9705a9b162fe9ea4c8579
Vishwesh Bankwar <vibankwa@microsoft.com>
2024-01-30 12:45:40 -0800

    clean up

--- sdk/monitor/Azure.Monitor.OpenTelemetry.AspNetCore/src/LogFilteringProcessor.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.AspNetCore/src/LogFilteringProcessor.cs
@@ -8,9 +8,6 @@ namespace Azure.Monitor.OpenTelemetry.AspNetCore
 {
     internal class LogFilteringProcessor : BatchLogRecordExportProcessor
     {
-        private readonly bool _enableSampling;
-        private readonly BaseProcessor<LogRecord> _processor;
-
         public LogFilteringProcessor(BaseExporter<LogRecord> exporter)
             : base(exporter)
         {
@@ -22,21 +19,6 @@ namespace Azure.Monitor.OpenTelemetry.AspNetCore
             {
                 base.OnEnd(logRecord);
             }
-        }
-
-        protected override bool OnForceFlush(int timeoutMilliseconds)
-        {
-            return _processor.ForceFlush(timeoutMilliseconds);
-        }
-
-        protected override bool OnShutdown(int timeoutMilliseconds)
-        {
-            return _processor.Shutdown();
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            _processor.Dispose();
         }
     }
 }
