4e516e0b899046e0c9e78936db1c10d6ce372909
Wei Hu <huwe@microsoft.com>
2024-01-30 17:06:46 +0800

    clean up

--- sdk/core/Azure.Core/src/Shared/NextLinkOperationImplementation.cs
+++ sdk/core/Azure.Core/src/Shared/NextLinkOperationImplementation.cs
@@ -450,6 +450,14 @@ namespace Azure.Core
             return HeaderSource.None;
         }
 
+        private enum HeaderSource
+        {
+            None,
+            OperationLocation,
+            AzureAsyncOperation,
+            Location
+        }
+
         private class CompletedOperation : IOperation
         {
             private readonly OperationState _operationState;
