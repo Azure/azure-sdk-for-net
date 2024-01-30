d715ab96f53b6562cc45f999c20334ca18a610a0
Wei Hu <huwe@microsoft.com>
2024-01-30 16:54:55 +0800

    move EmptyResponse from internal share to private

--- sdk/core/Azure.Core/src/Shared/OperationInternalOfT.cs
+++ sdk/core/Azure.Core/src/Shared/OperationInternalOfT.cs
@@ -6,6 +6,7 @@
 using System;
 using System.ClientModel.Primitives;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -320,6 +321,63 @@ namespace Azure.Core
             }
 
             throw state.OperationFailedException!;
+        }
+
+        private class EmptyResponse : Response
+        {
+            public EmptyResponse(HttpStatusCode status)
+            {
+                Status = (int)status;
+                ReasonPhrase = status.ToString();
+            }
+
+            public override int Status { get; }
+
+            public override string ReasonPhrase { get; }
+
+            public override Stream? ContentStream { get => null; set => throw new System.NotImplementedException(); }
+            public override string ClientRequestId { get => string.Empty; set => throw new System.NotImplementedException(); }
+
+            public override void Dispose()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+#if HAS_INTERNALS_VISIBLE_CORE
+            internal
+#endif
+            protected override bool ContainsHeader(string name)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+#if HAS_INTERNALS_VISIBLE_CORE
+            internal
+#endif
+            protected override IEnumerable<HttpHeader> EnumerateHeaders()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+#if HAS_INTERNALS_VISIBLE_CORE
+            internal
+#endif
+            protected override bool TryGetHeader(string name, out string value)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            /// <inheritdoc />
+#if HAS_INTERNALS_VISIBLE_CORE
+            internal
+#endif
+            protected override bool TryGetHeaderValues(string name, out IEnumerable<string> values)
+            {
+                throw new System.NotImplementedException();
+            }
         }
 
         private class FinalOperation : IOperation<T>
