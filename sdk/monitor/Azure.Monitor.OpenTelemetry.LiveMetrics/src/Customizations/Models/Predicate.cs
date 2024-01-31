577995d39f419235c628efab88ad649023c1dd8c
xiang17 <xili9@microsoft.com>
2024-01-30 22:40:32 -0800

    Add Predicate.cs file

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.LiveMetrics/src/Customizations/Models/Predicate.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.ApplicationInsights.Extensibility.Filtering
+{
+    internal enum Predicate
+    {
+        Equal = 0,
+
+        NotEqual = 1,
+
+        LessThan = 2,
+
+        GreaterThan = 3,
+
+        LessThanOrEqual = 4,
+
+        GreaterThanOrEqual = 5,
+
+        Contains = 6,
+
+        DoesNotContain = 7,
+    }
+}
