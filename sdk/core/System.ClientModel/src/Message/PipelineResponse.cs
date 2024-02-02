84a8d7fc554d443f82fba773d8c5eb6744c257e7
Anne Thompson <annelo@microsoft.com>
2024-02-02 15:59:54 -0800

    nit

--- sdk/core/System.ClientModel/src/Message/PipelineResponse.cs
+++ sdk/core/System.ClientModel/src/Message/PipelineResponse.cs
@@ -73,7 +73,7 @@ public abstract class PipelineResponse : IDisposable
 
     protected virtual void SetIsErrorCore(bool isError) => _isError = isError;
 
-    internal TimeSpan NetworkTimeout { get; set; } = ClientPipeline.DefaultNetworkTimeout;
+    internal TimeSpan NetworkTimeout { get; set; }
 
     public abstract void Dispose();
 
