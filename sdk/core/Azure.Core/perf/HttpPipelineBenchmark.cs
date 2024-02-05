8edd5e7d4708d924f3b47d4da9344d06718e5e06
Anne Thompson <annelo@microsoft.com>
2024-02-05 13:23:21 -0800

    Initial check-in

---
+++ sdk/core/Azure.Core/perf/HttpPipelineBenchmark.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core.Pipeline;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace Azure.Core.Perf;
+
+[SimpleJob(RuntimeMoniker.Net60)]
+public class HttpPipelineBenchmark
+{
+    private HttpPipeline _pipeline;
+
+    [GlobalSetup]
+    public void SetUp()
+    {
+        ClientOptions options = new BenchmarkOptions
+        {
+            Transport = new HttpClientTransport(new HttpClient(new MockHttpMessageHandler()))
+        };
+
+        _pipeline = HttpPipelineBuilder.Build(options);
+    }
+
+    [Benchmark]
+    public async Task CreateAndSendMessage()
+    {
+        HttpMessage message = _pipeline.CreateMessage();
+        message.Request.Uri.Reset(new Uri("https://www.example.com"));
+        await _pipeline.SendAsync(message, CancellationToken.None);
+    }
+
+    private class BenchmarkOptions : ClientOptions
+    {
+    }
+
+    private class MockHttpMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage httpResponse = new()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent("Mock Content")
+            };
+
+            httpResponse.Headers.Add("MockHeader1", "Mock Header Value");
+            httpResponse.Headers.Add("MockHeader2", "Mock Header Value");
+
+            return Task.FromResult(httpResponse);
+        }
+    }
+}
