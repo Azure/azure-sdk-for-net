19cc2e9a021bcda80cec79d6e213ca182741bead
jolov <jolov@microsoft.com>
2024-02-07 13:13:52 -0800

    Don't drain from session processor as it is not necessary for FIFO

--- sdk/servicebus/Azure.Messaging.ServiceBus/tests/Processor/SessionProcessorLiveTests.cs
+++ sdk/servicebus/Azure.Messaging.ServiceBus/tests/Processor/SessionProcessorLiveTests.cs
@@ -2679,6 +2679,54 @@ namespace Azure.Messaging.ServiceBus.Tests.Processor
             }
         }
 
+        [Test]
+        public async Task SessionOrderingIsGuaranteedProcessor()
+        {
+            await using (var scope = await ServiceBusScope.CreateWithQueue(enablePartitioning: false, enableSession: true))
+            {
+                await using var client = new ServiceBusClient(TestEnvironment.ServiceBusConnectionString);
+                long lastSequenceNumber = 0;
+
+                await using var processor = client.CreateSessionProcessor(scope.QueueName,
+                    new ServiceBusSessionProcessorOptions
+                    {
+                        MaxConcurrentCallsPerSession = 1, MaxConcurrentSessions = 1
+                    });
+                processor.ProcessMessageAsync += ProcessMessage;
+                processor.ProcessErrorAsync += SessionErrorHandler;
+
+                var sender = client.CreateSender(scope.QueueName);
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                cts.CancelAfter(TimeSpan.FromSeconds(60));
+                await processor.StartProcessingAsync();
+                await SendMessagesAsync();
+
+                await processor.StopProcessingAsync();
+
+                async Task SendMessagesAsync()
+                {
+                    while (!cts.IsCancellationRequested)
+                    {
+                        await sender.SendMessageAsync(ServiceBusTestUtilities.GetMessage("session"));
+                        await Task.Delay(TimeSpan.FromMilliseconds(100));
+                    }
+                }
+
+                Task ProcessMessage(ProcessSessionMessageEventArgs args)
+                {
+                    if (args.Message.SequenceNumber != lastSequenceNumber + 1)
+                    {
+                        Assert.Fail(
+                            $"Last sequence number: {lastSequenceNumber}, current sequence number: {args.Message.SequenceNumber}");
+                    }
+
+                    lastSequenceNumber = args.Message.SequenceNumber;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+
         private Task SessionErrorHandler(ProcessErrorEventArgs args)
         {
             // If the connection drops due to network flakiness
