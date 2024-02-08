cbb2c9df9d9dc2a04e98172494754abdb92edb42
xiang17 <xili9@microsoft.com>
2024-02-07 16:26:44 -0800

    Add ArrayHelpers.cs file

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.LiveMetrics/tests/Azure.Monitor.OpenTelemetry.LiveMetrics.Tests/Filtering/ArrayHelpers.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.ApplicationInsights.Tests
+{
+    using System;
+
+    internal static class ArrayHelpers
+    {
+        public static void ForEach<T>(T[] array, Action<T> action)
+        {
+            foreach (T item in array)
+            {
+                action(item);
+            }
+        }
+    }
+}
