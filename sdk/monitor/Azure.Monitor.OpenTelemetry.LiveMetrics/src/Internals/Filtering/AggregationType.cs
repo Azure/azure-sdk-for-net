5ad4ca094d4d71c778cb170917f469e01c2762f1
xiang17 <xili9@microsoft.com>
2024-02-07 16:23:11 -0800

    Add AggregationType.cs file

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.LiveMetrics/src/Internals/Filtering/AggregationType.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.ApplicationInsights.Extensibility.Filtering
+{
+    internal enum AggregationType
+    {
+        Avg = 0,
+
+        Sum = 1,
+
+        Min = 2,
+
+        Max = 3,
+    }
+}
