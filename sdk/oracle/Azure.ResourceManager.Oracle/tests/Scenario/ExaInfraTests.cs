06306308232f9adc31dfd74a569b1b75b21d5b42
Zhuoyun Wang <zhuoyun.wang@oracle.com>
2024-02-07 16:53:25 -0800

    add tests for exaInfra

--- sdk/oracle/Azure.ResourceManager.Oracle/tests/Scenario/ExaInfraTests.cs
+++ sdk/oracle/Azure.ResourceManager.Oracle/tests/Scenario/ExaInfraTests.cs
@@ -1,12 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Azure.Core;
 using Azure.Core.TestFramework;
-using Azure.Identity;
-using Azure.ResourceManager.Oracle.Models;
-using Azure.ResourceManager.Oracle.Tests.Helpers;
 using Azure.ResourceManager.Resources;
 using NUnit.Framework;
 
@@ -47,6 +44,26 @@ namespace Azure.ResourceManager.Oracle.Tests.Scenario
             Response<CloudExadataInfrastructureResource> getExaInfraResponse = await cloudExadataInfrastructureCollection.GetAsync(cloudExadataInfrastructureName);
             CloudExadataInfrastructureResource exaInfraResource = getExaInfraResponse.Value;
             Assert.IsNotNull(exaInfraResource);
+
+            // Check Existence
+            Response<bool> exaInfraExists = await cloudExadataInfrastructureCollection.ExistsAsync("OFake_SdkExadata_test_1");
+            bool checkResult = exaInfraExists.Value;
+            Assert.IsTrue(checkResult);
+
+            // Get if exists - exists
+            NullableResponse<CloudExadataInfrastructureResource> getExaInfraNullableResponse = await cloudExadataInfrastructureCollection.GetIfExistsAsync(cloudExadataInfrastructureName);
+            CloudExadataInfrastructureResource exaInfraResource2 = getExaInfraNullableResponse.Value;
+            Assert.IsNotNull(exaInfraResource2);
+
+            // Get if exists - not exist
+            NullableResponse<CloudExadataInfrastructureResource> getExaInfraNullableResponse2 = await cloudExadataInfrastructureCollection.GetIfExistsAsync("OFake_not_exist");
+            Assert.IsFalse(getExaInfraNullableResponse2.HasValue);
+
+            // ListByResourceGroup
+            AsyncPageable<CloudExadataInfrastructureResource> ExaInfras = cloudExadataInfrastructureCollection.GetAllAsync();
+            List<CloudExadataInfrastructureResource> exaInfraResult = await ExaInfras.ToEnumerableAsync();
+            Assert.NotNull(exaInfraResult);
+            Assert.IsTrue(exaInfraResult.Count >= 1);
         }
     }
 }
