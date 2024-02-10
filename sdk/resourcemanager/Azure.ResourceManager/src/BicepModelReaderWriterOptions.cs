bad1afd441864d0898bc7531c13801d8643647c6
jolov <jolov@microsoft.com>
2024-02-10 10:05:24 -0800

    Add BicepModelReaderWriterOptions

---
+++ sdk/resourcemanager/Azure.ResourceManager/src/BicepModelReaderWriterOptions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
+
+namespace Azure.ResourceManager
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class BicepModelReaderWriterOptions : ModelReaderWriterOptions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public BicepModelReaderWriterOptions() : base("bicep")
+        {
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public Dictionary<object, Dictionary<string, object>> ParameterOverrides { get; } = new();
+    }
+}
