b81d831984c62766cb234b011bc871e37b62f3ba
glorialimicrosoft <67171398+glorialimicrosoft@users.noreply.github.com>
2024-02-09 16:08:29 -0800

    Add GA changes for Azure.Communication.Messages  (#41458)
    * iteration 1, addressed renaming, reran live test recordings
    
    * rerun with latest GA swagger, and rerun live test recordings
    
    * revert accidental check in of recordmode change
    
    * rename keyCredential to credential
    
    * add SanitizedHeaders for Repeatability
    
    * Generate SDK from TypeSpec, fix convenience layers
    
    * rerun recording
    
    * add [CodeGenModel("AzureCommunicationMessagesClientOptions")]
    
    * use V2024_02_01 as latest
    
    * Update MessageTemplateItem.cs
    
    * revert accidental check in of recordmode change
    
    * rename keyCredential to credential
    
    * attempt 1
    
    * add Kind to MessageTemplateBindings
    
    * rerun tests
    
    * regenerate
    
    * regenerate: Typespec with Polymorphism
    
    * regenerate current api listing from code
    
    * Azure.Communication.Messages: delete all unnecessary custom code
    
    * fix names and model sub namespace
    
    * remove dead comments and usings
    
    * remove more dead comments
    
    * change operation name from sendMessage to send
    
    * add WhatsAppMessageButtonSubType to Azure.Communication.Messages.Models.Channels namespace
    
    * Use GeoPosition to represent Latitude and Longitude in MessageTemplateLocation
    
    * make WhatsAppMessageTemplateBindings internal
    
    * make Latitude and Longitude internal
    
    * update api listing (please always update with Export-API.ps1)
    
    * Revert "make WhatsAppMessageTemplateBindings internal"
    
    This reverts commit 2706f9dac22472c2f376ad01d5766ead184aaa2a.
    
    * rerun recording
    
    * use latestest typespec
    
    * regenerate with latest typespec - rename url to uri, make downloadmedia internal
    
    * regenerate with latest typespec - fixing some typos
    
    * revert accidental deletion of phoneNumber api
    
    * regenerate from latest TypeSpec
    
    * re-run recordings
    
    * regenerate from latest typespec
    
    * re-run recordings
    
    * revert accidental check in of local testing changes
    
    * add more tests
    
    * add more tests
    
    * rerun recordings
    
    * update tests and rerun recording
    
    * fix test name
    
    * Update README.md
    
    * regenerate and fix build pipeline issue
    
    * fix broken links in readme
    
    * move some tests out of live tests into tests file
    
    * rerun recordings
    
    * re-run recording with proper recorded variables
    
    * change destination path for downloadMedia
    
    ---------
    
    Co-authored-by: Dominik Messinger <domessin@microsoft.com>

--- sdk/communication/Azure.Communication.Messages/src/Models/MessageTemplateLocation.cs
+++ sdk/communication/Azure.Communication.Messages/src/Models/MessageTemplateLocation.cs
@@ -1,42 +1,75 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using Azure.Core;
+using Azure.Core.GeoJson;
+
 namespace Azure.Communication.Messages
 {
-    /// <summary>  </summary>
-    public class MessageTemplateLocation : MessageTemplateValue
+    /// <summary> The message template's location value information. </summary>
+    [CodeGenModel("MessageTemplateLocation")]
+    public partial class MessageTemplateLocation
     {
-        /// <summary>  </summary>
-        public MessageTemplateLocation(string name, double latitude, double longitude, string locationName = null, string address = null)
-         : base(name)
-        {
-            Latitude = latitude;
-            Longitude = longitude;
-            LocationName = locationName;
-            Address = address;
+        /// <summary> The latitude of the location. </summary>
+        [CodeGenMember("Latitude")]
+        internal double LatitudeInternal {
+            get
+            {
+                return Position.Latitude;
+            }
         }
 
-        /// <summary> The name of the location. </summary>
-        public string LocationName { get; set; }
-        /// <summary> The address of the location. </summary>
-        public string Address { get; set; }
-        /// <summary> The latitude of the location. </summary>
-        public double Latitude { get; set; }
         /// <summary> The longitude of the location. </summary>
-        public double Longitude { get; set; }
-
-        internal override MessageTemplateValueInternal ToMessageTemplateValueInternal()
+        [CodeGenMember("Longitude")]
+        internal double LongitudeInternal
         {
-            return new MessageTemplateValueInternal(MessageTemplateValueKind.Location)
+            get
             {
-                Location = new MessageTemplateParameterLocation
-                {
-                    Name = LocationName,
-                    Address = Address,
-                    Latitude = Latitude,
-                    Longitude = Longitude
-                }
-            };
+                return Position.Longitude;
+            }
+        }
+
+        /// <summary> The geo position of the location. </summary>
+        public GeoPosition Position { get; set; } = new GeoPosition();
+
+        /// <summary> Initializes a new instance of <see cref="MessageTemplateLocation"/>. </summary>
+        /// <param name="name"> Name of the Template value. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="name"/> is null. </exception>
+        public MessageTemplateLocation(string name) : base(name)
+        {
+            Argument.AssertNotNull(name, nameof(name));
+            Kind = "location";
+        }
+
+        // This is a direct copy of the auto-rest generated constructor but we want to make the internal Latitude and Longitude read only
+        /// <summary> Initializes a new instance of <see cref="MessageTemplateLocation"/>. </summary>
+        /// <param name="name"> Name of the Template value. </param>
+        /// <param name="latitudeInternal"> The latitude of the location. </param>
+        /// <param name="longitudeInternal"> The longitude of the location. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="name"/> is null. </exception>
+        internal MessageTemplateLocation(string name, double latitudeInternal, double longitudeInternal) : base(name)
+        {
+            Argument.AssertNotNull(name, nameof(name));
+            Kind = "location";
+        }
+
+        // This is a direct copy of the auto-rest generated constructor but we want to make the internal Latitude and Longitude read only
+        /// <summary> Initializes a new instance of <see cref="MessageTemplateLocation"/>. </summary>
+        /// <param name="name"> Name of the Template value. </param>
+        /// <param name="kind"> The type discriminator describing a template parameter type. </param>
+        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
+        /// <param name="locationName"> The [Optional] name of the location. </param>
+        /// <param name="address"> The [Optional] address of the location. </param>
+        /// <param name="latitudeInternal"> The latitude of the location. </param>
+        /// <param name="longitudeInternal"> The longitude of the location. </param>
+        internal MessageTemplateLocation(string name, string kind, IDictionary<string, BinaryData> serializedAdditionalRawData, string locationName, string address, double latitudeInternal, double longitudeInternal) : base(name, kind, serializedAdditionalRawData)
+        {
+            LocationName = locationName;
+            Address = address;
         }
     }
 }
