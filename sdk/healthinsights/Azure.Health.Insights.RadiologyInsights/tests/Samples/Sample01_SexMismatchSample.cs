5b41d559c25458b414761580088b4ee34fa50a61
Rameez Shaikh <rameezshaikhmicrosoft@gmail.com>
2024-02-13 16:39:28 +0530

    more inference samples

--- sdk/healthinsights/Azure.Health.Insights.RadiologyInsights/tests/Samples/Sample01_SexMismatchSample.cs
+++ sdk/healthinsights/Azure.Health.Insights.RadiologyInsights/tests/Samples/Sample01_SexMismatchSample.cs
@@ -4,12 +4,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Azure.Health.Insights.RadiologyInsights.Tests
 {
-    internal class Sample01_CriticalResultSampleAsync
+    internal class Sample01_SexMismatchSample
     {
         private const string DOC_CONTENT = "CLINICAL HISTORY:   "
             + "\r\n20-year-old female presenting with abdominal pain. Surgical history significant for appendectomy."
@@ -34,7 +33,7 @@ namespace Azure.Health.Insights.RadiologyInsights.Tests
             + "\r\n";
 
         [Test]
-        public async Task RadiologyInsightsCriticalResultScenario()
+        public void RadiologyInsightsSexMismatchScenario()
         {
             Uri endpoint = new Uri("AZURE_HEALTH_INSIGHTS_ENDPOINT");
             AzureKeyCredential credential = new AzureKeyCredential("AZURE_HEALTH_INSIGHTS_KEY");
@@ -42,16 +41,21 @@ namespace Azure.Health.Insights.RadiologyInsights.Tests
 
             RadiologyInsightsData radiologyInsightsData = GetRadiologyInsightsData();
 
-            Operation<RadiologyInsightsInferenceResult> operation = await client.InferRadiologyInsightsAsync(WaitUntil.Completed, radiologyInsightsData);
-
+            Operation<RadiologyInsightsInferenceResult> operation = client.InferRadiologyInsights(WaitUntil.Completed, radiologyInsightsData);
             RadiologyInsightsInferenceResult responseData = operation.Value;
             IReadOnlyList<RadiologyInsightsInference> inferences = responseData.PatientResults[0].Inferences;
 
             foreach (RadiologyInsightsInference inference in inferences)
             {
-                if (inference is CriticalResultInference criticalResultInference)
+                if (inference is SexMismatchInference sexMismatchInference)
                 {
-                    Console.Write("Critical Result Inference found: " + criticalResultInference.Result.Description);
+                    CodeableConcept sexIndeication = sexMismatchInference.SexIndication;
+                    IList<Coding> codingList = sexIndeication.Coding;
+                    Console.Write("Critical Result Inference found: ");
+                    foreach (Coding coding in codingList)
+                    {
+                        Console.Write("   Coding: " + coding.System + ", " + coding.Code + ", " + coding.Display);
+                    }
                 }
             }
         }
