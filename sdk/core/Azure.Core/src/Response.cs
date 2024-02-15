6eef730ffa572ecc22f28bea8eec919d288cd100
Anne Thompson <annelo@microsoft.com>
2024-02-14 17:47:06 -0800

    nits

--- sdk/core/Azure.Core/src/Response.cs
+++ sdk/core/Azure.Core/src/Response.cs
@@ -4,8 +4,6 @@
 using System;
 using System.ClientModel.Primitives;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading;
