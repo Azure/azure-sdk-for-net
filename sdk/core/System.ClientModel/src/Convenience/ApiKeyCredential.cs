a012e453a00ed4a45c34f9aa0822974621b94e3e
Anne Thompson <annelo@microsoft.com>
2024-02-16 08:48:51 -0800

    ApiKeyCredential

--- sdk/core/System.ClientModel/src/Convenience/ApiKeyCredential.cs
+++ sdk/core/System.ClientModel/src/Convenience/ApiKeyCredential.cs
@@ -6,6 +6,11 @@ using System.Threading;
 
 namespace System.ClientModel;
 
+/// <summary>
+/// API key credential used to authenticate to a service.
+/// This credential type provides the ability to update the key's value without
+/// creating a new client.
+/// </summary>
 public class ApiKeyCredential
 {
     private string _key;
@@ -13,7 +18,7 @@ public class ApiKeyCredential
     /// <summary>
     /// Initializes a new instance of the <see cref="ApiKeyCredential"/> class.
     /// </summary>
-    /// <param name="key">Key to use to authenticate with the Azure service.</param>
+    /// <param name="key">Key to use to authenticate with the service.</param>
     /// <exception cref="System.ArgumentNullException">
     /// Thrown when the <paramref name="key"/> is null.
     /// </exception>
@@ -24,14 +29,33 @@ public class ApiKeyCredential
     public ApiKeyCredential(string key) => Update(key);
 #pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
 
-    // Note: this is uses a method instead of a Key property so if an instance of
-    // the type were passed to a serializer that serializes public properties
-    // it doesn't inadvertently expose a secret.  We call it Deconstruct to allow
-    // consistent naming across credential types.  For example, for
-    // NamedKeyCredential, we expect to have a Deconstruct method with two out
-    // params to enable atomicity in reading multiple credential values.
+    // This type uses a method instead of a Key property so that, if an instance
+    // of the type were passed to a serializer that serializes public properties
+    // by default, it doesn't inadvertently expose a secret. The method has the
+    // name Deconstruct to allow consistent naming across credential types.  For
+    // example, a corresponding NamedKeyCredential type will have a Deconstruct
+    // method with two out params to make reading multiple credential values an
+    // atomic operation.
+
+    /// <summary>
+    /// Deconstructs the credential into component values.
+    /// </summary>
+    /// <param name="key">When this method returns, contains the value of the
+    /// API key this instance represents.</param>
     public void Deconstruct(out string key) => key = Volatile.Read(ref _key);
 
+    /// <summary>
+    /// Updates the service key.
+    /// This is intended to be used when a service key has been regenerated
+    /// and long-lived clients need to use the new value.
+    /// </summary>
+    /// <param name="key">Key to authenticate the service against.</param>
+    /// <exception cref="System.ArgumentNullException">
+    /// Thrown when the <paramref name="key"/> is null.
+    /// </exception>
+    /// <exception cref="System.ArgumentException">
+    /// Thrown when the <paramref name="key"/> is empty.
+    /// </exception>
     public void Update(string key)
     {
         Argument.AssertNotNullOrEmpty(key, nameof(key));
