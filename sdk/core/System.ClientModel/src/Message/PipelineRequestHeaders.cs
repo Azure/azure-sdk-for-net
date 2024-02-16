fdef046210f8fead633601eda41e035c44e31849
Anne Thompson <annelo@microsoft.com>
2024-02-15 21:49:31 -0800

    RequestOptions

--- sdk/core/System.ClientModel/src/Message/PipelineRequestHeaders.cs
+++ sdk/core/System.ClientModel/src/Message/PipelineRequestHeaders.cs
@@ -14,8 +14,20 @@ public abstract class PipelineRequestHeaders : IEnumerable<KeyValuePair<string, 
 
     public abstract bool Remove(string name);
 
+    /// <summary>
+    /// Attempts to retrieve the value associated with the specified header name.
+    /// </summary>
+    /// <param name="name">The name of the header to retrieve.</param>
+    /// <param name="value">The specified header value.</param>
+    /// <returns>true if the specified header name and value are stored in the collection; otherwise false.</returns>
     public abstract bool TryGetValue(string name, out string? value);
 
+    /// <summary>
+    /// Attempts to retrieve the values associated with the specified header name.
+    /// </summary>
+    /// <param name="name">The name of the header to retrieve.</param>
+    /// <param name="values">The specified header values.</param>
+    /// <returns>true if the specified header name and values are stored in the collection; otherwise false.</returns>
     public abstract bool TryGetValues(string name, out IEnumerable<string>? values);
 
     public abstract IEnumerator<KeyValuePair<string, string>> GetEnumerator();
