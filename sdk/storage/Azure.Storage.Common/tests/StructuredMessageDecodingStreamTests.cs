9a5f80ee9b1f5940a7f7c1442ef4784677188e81
Jocelyn Schreppler <jaschrep@microsoft.com>
2024-02-20 10:20:45 -0500

    seek/write tests

--- sdk/storage/Azure.Storage.Common/tests/StructuredMessageDecodingStreamTests.cs
+++ sdk/storage/Azure.Storage.Common/tests/StructuredMessageDecodingStreamTests.cs
@@ -4,6 +4,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs.Tests;
 using Azure.Storage.Shared;
@@ -80,6 +81,7 @@ namespace Azure.Storage.Tests
         public async Task DecodesData(
             [Values(2048, 2005)] int dataLength,
             [Values(default, 512)] int? seglen,
+            [Values(8*Constants.KB, 512, 530, 3)] int? readLen,
             [Values(true, false)] bool useCrc)
         {
             int segmentContentLength = seglen ?? int.MaxValue;
@@ -98,6 +100,38 @@ namespace Azure.Storage.Tests
             }
 
             Assert.That(new Span<byte>(decodedData).SequenceEqual(originalData));
+        }
+
+        [Test]
+        public void NoSeek()
+        {
+            StructuredMessageDecodingStream stream = new(new MemoryStream());
+
+            Assert.That(stream.CanSeek, Is.False);
+            Assert.That(() => stream.Length, Throws.TypeOf<NotSupportedException>());
+            Assert.That(() => stream.Position, Throws.TypeOf<NotSupportedException>());
+            Assert.That(() => stream.Position = 0, Throws.TypeOf<NotSupportedException>());
+            Assert.That(() => stream.Seek(0, SeekOrigin.Begin), Throws.TypeOf<NotSupportedException>());
+        }
+
+        [Test]
+        public void NoWrite()
+        {
+            StructuredMessageDecodingStream stream = new(new MemoryStream());
+            byte[] data = new byte[1024];
+            new Random().NextBytes(data);
+
+            Assert.That(stream.CanWrite, Is.False);
+            Assert.That(() => stream.Write(data, 0, data.Length),
+                Throws.TypeOf<NotSupportedException>());
+            Assert.That(async () => await stream.WriteAsync(data, 0, data.Length, CancellationToken.None),
+                Throws.TypeOf<NotSupportedException>());
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+            Assert.That(() => stream.Write(new Span<byte>(data)),
+                Throws.TypeOf<NotSupportedException>());
+            Assert.That(async () => await stream.WriteAsync(new Memory<byte>(data), CancellationToken.None),
+                Throws.TypeOf<NotSupportedException>());
+#endif
         }
     }
 }
