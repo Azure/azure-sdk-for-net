9a9e64a253b6fb3de9ac8e71f1cd89405c93e932
m-nash <64171366+m-nash@users.noreply.github.com>
2024-02-20 11:33:08 -0800

    Add support for indexers in assign param (#42084)
    * add support for indexers in assign param
    
    * update api

--- sdk/provisioning/Azure.Provisioning/api/Azure.Provisioning.net6.0.cs
+++ sdk/provisioning/Azure.Provisioning/api/Azure.Provisioning.net6.0.cs
@@ -208,7 +208,7 @@ namespace Azure.Provisioning.Sql
     }
     public partial class SqlDatabase : Azure.Provisioning.Resource<Azure.ResourceManager.Sql.SqlDatabaseData>
     {
-        public SqlDatabase(Azure.Provisioning.IConstruct scope, string name = "db", string version = "2022-08-01-preview", Azure.Core.AzureLocation? location = default(Azure.Core.AzureLocation?)) : base (default(Azure.Provisioning.IConstruct), default(Azure.Provisioning.Resource), default(string), default(Azure.Core.ResourceType), default(string), default(System.Func<string, Azure.ResourceManager.Sql.SqlDatabaseData>)) { }
+        public SqlDatabase(Azure.Provisioning.IConstruct scope, Azure.Provisioning.Sql.SqlServer? parent = null, string name = "db", string version = "2022-08-01-preview", Azure.Core.AzureLocation? location = default(Azure.Core.AzureLocation?)) : base (default(Azure.Provisioning.IConstruct), default(Azure.Provisioning.Resource), default(string), default(Azure.Core.ResourceType), default(string), default(System.Func<string, Azure.ResourceManager.Sql.SqlDatabaseData>)) { }
         protected override Azure.Provisioning.Resource? FindParentInScope(Azure.Provisioning.IConstruct scope) { throw null; }
         public Azure.Provisioning.Sql.ConnectionString GetConnectionString(Azure.Provisioning.Parameter passwordSecret, string userName = "appUser") { throw null; }
     }
