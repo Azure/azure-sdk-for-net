cc6336f00e12ec156e861caaddc75e748d7c78a4
Jaxel Rojas <jrojaslopez@microsoft.com>
2024-02-23 16:02:18 -0400

    feat: initial version of the Azure.Developer.Signing docs, tests & samples

---
+++ sdk/developersigning/Azure.Developer.Signing/src/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "AZC0001:Use one of the following pre-approved namespace groups (https://azure.github.io/azure-sdk/registered_namespaces.html): Azure.AI, Azure.Analytics, Azure.Communication, Azure.Containers, Azure.Core.Expressions, Azure.Data, Azure.DigitalTwins, Azure.Identity, Azure.IoT, Azure.Learn, Azure.Management, Azure.Media, Azure.Messaging, Azure.MixedReality, Azure.Monitor, Azure.ResourceManager, Azure.Search, Azure.Security, Azure.Storage, Azure.Template, Microsoft.Extensions.Azure", Justification = "Temporary while awaiting namespace group approval", Scope = "namespace", Target = "~N:Azure.Developer.Signing")]
+[assembly: SuppressMessage("Naming", "AZC0030:Improper model name suffix", Justification = "Temporary suppression", Scope = "type", Target = "~T:Azure.Developer.Signing.SigningPayloadOptions")]
