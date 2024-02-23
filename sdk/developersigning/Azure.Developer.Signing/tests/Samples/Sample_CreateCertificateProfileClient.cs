2336df30a30ecf83f0685848d294b3ae6a4802ad
Jaxel Rojas <jrojaslopez@microsoft.com>
2024-02-23 16:02:18 -0400

    feat: initial version of the Azure.Developer.Signing docs, tests & samples

---
+++ sdk/developersigning/Azure.Developer.Signing/tests/Samples/Sample_CreateCertificateProfileClient.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core.TestFramework;
+
+namespace Azure.Developer.Signing.Tests.Samples
+{
+    public partial class DeveloperSigningSample : SamplesBase<DevSigningTestEnvironment>
+    {
+        public void CreateClients(string region)
+        {
+            #region Snippet:Azure_Developer_Signing_CreateCertificateProfileClient_Scenario
+
+            var credential = new DefaultAzureCredential();
+            var signClient = new SigningClient(credential);
+            var CertificatProfileClient = signClient.GetCertificateProfileClient(region);
+
+            #endregion Snippet:Azure_Developer_Signing_CreateCertificateProfileClient_Scenario
+        }
+    }
+}
