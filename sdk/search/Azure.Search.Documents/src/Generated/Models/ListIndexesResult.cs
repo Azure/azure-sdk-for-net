e124d07c572a62df64160510cb16f7c527a0303f
azure-sdk <azuresdk@microsoft.com>
2024-02-26 19:14:51 +0000

    Update SDK codes pu_sq_5

--- sdk/search/Azure.Search.Documents/src/Generated/Models/ListIndexesResult.cs
+++ sdk/search/Azure.Search.Documents/src/Generated/Models/ListIndexesResult.cs
@@ -5,7 +5,6 @@
 
 #nullable disable
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,14 +15,8 @@ namespace Azure.Search.Documents.Indexes.Models
     {
         /// <summary> Initializes a new instance of <see cref="ListIndexesResult"/>. </summary>
         /// <param name="indexes"> The indexes in the Search service. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="indexes"/> is null. </exception>
         internal ListIndexesResult(IEnumerable<SearchIndex> indexes)
         {
-            if (indexes == null)
-            {
-                throw new ArgumentNullException(nameof(indexes));
-            }
-
             Indexes = indexes.ToList();
         }
 
--- sdk/search/Azure.Search.Documents/src/Generated/Models/SuggestResult.cs
+++ sdk/search/Azure.Search.Documents/src/Generated/Models/SuggestResult.cs
@@ -5,7 +5,6 @@
 
 #nullable disable
 
-using System;
 using System.Collections.Generic;
 using Azure.Core;
 
@@ -16,14 +15,8 @@ namespace Azure.Search.Documents.Models
     {
         /// <summary> Initializes a new instance of <see cref="SuggestResult"/>. </summary>
         /// <param name="text"> The text of the suggestion result. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="text"/> is null. </exception>
         internal SuggestResult(string text)
         {
-            if (text == null)
-            {
-                throw new ArgumentNullException(nameof(text));
-            }
-
             Text = text;
             AdditionalProperties = new ChangeTrackingDictionary<string, object>();
         }
