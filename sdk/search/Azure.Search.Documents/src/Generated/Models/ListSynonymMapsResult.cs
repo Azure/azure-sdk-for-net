a791621b724a141e7496468ec50d512ec7953259
azure-sdk <azuresdk@microsoft.com>
2024-02-26 20:13:24 +0000

    Update SDK codes pu_sq_5

--- sdk/search/Azure.Search.Documents/src/Generated/Models/ListSynonymMapsResult.cs
+++ sdk/search/Azure.Search.Documents/src/Generated/Models/ListSynonymMapsResult.cs
@@ -5,7 +5,6 @@
 
 #nullable disable
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,14 +15,8 @@ namespace Azure.Search.Documents.Indexes.Models
     {
         /// <summary> Initializes a new instance of <see cref="ListSynonymMapsResult"/>. </summary>
         /// <param name="synonymMaps"> The synonym maps in the Search service. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="synonymMaps"/> is null. </exception>
         internal ListSynonymMapsResult(IEnumerable<SynonymMap> synonymMaps)
         {
-            if (synonymMaps == null)
-            {
-                throw new ArgumentNullException(nameof(synonymMaps));
-            }
-
             SynonymMaps = synonymMaps.ToList();
         }
 
