5a5c86516d4b969c2f7fe078e61979bcb1ead714
azure-sdk <azuresdk@microsoft.com>
2024-02-26 21:30:39 +0000

    Update SDK codes pu_sq_5

--- sdk/search/Azure.Search.Documents/src/Generated/Models/SearchServiceError.cs
+++ sdk/search/Azure.Search.Documents/src/Generated/Models/SearchServiceError.cs
@@ -5,7 +5,6 @@
 
 #nullable disable
 
-using System;
 using System.Collections.Generic;
 using Azure.Core;
 
@@ -16,14 +15,8 @@ namespace Azure.Search.Documents.Indexes.Models
     {
         /// <summary> Initializes a new instance of <see cref="SearchServiceError"/>. </summary>
         /// <param name="message"> A human-readable representation of the error. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="message"/> is null. </exception>
         internal SearchServiceError(string message)
         {
-            if (message == null)
-            {
-                throw new ArgumentNullException(nameof(message));
-            }
-
             Message = message;
             Details = new ChangeTrackingList<SearchServiceError>();
         }
