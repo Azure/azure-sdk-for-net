879b4db5452f368c494aecd0a6063ff6b2798e3f
Deepika N <deepikan@microsoft.com>
2024-02-27 13:53:23 +0530

    Added ArmConfluentModelFactory and the missing method for ConfluentOfferDetail

---
+++ sdk/confluent/Azure.ResourceManager.Confluent/src/Custom/ArmConfluentModelFactory.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System.ComponentModel;
+using Azure.ResourceManager.Confluent.Models;
+
+namespace Azure.ResourceManager.Confluent.Custom
+{
+    /// <summary> Model factory for models. </summary>
+    public partial class ArmConfluentModelFactory
+    {
+        /// <summary> Initializes a new instance of <see cref="ConfluentOfferDetail"/>. </summary>
+        /// <param name="publisherId"> Publisher Id. </param>
+        /// <param name="id"> Offer Id. </param>
+        /// <param name="planId"> Offer Plan Id. </param>
+        /// <param name="planName"> Offer Plan Name. </param>
+        /// <param name="termUnit"> Offer Plan Term unit. </param>
+        /// <param name="status"> SaaS Offer Status. </param>
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static ConfluentOfferDetail ConfluentOfferDetail(string publisherId, string id, string planId, string planName, string termUnit, ConfluentSaaSOfferStatus? status)
+        {
+            return new ConfluentOfferDetail(publisherId, id, planId, planName, termUnit, status);
+        }
+    }
+}
