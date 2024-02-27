be640c4b55a864d25e680ee18ce9ddb94eb32884
Crystal YU <chunyu@microsoft.com>
2024-02-27 17:42:34 +0800

    upgrade openai to latest commit (#42216)
    * upgrade openai to latest commit
    
    * refresh openai
    
    * resolve build failure
    
    * update openai api
    
    * revert to previous commit

--- sdk/openai/Azure.AI.OpenAI/src/Generated/AzureCosmosDBChatExtensionParameters.Serialization.cs
+++ sdk/openai/Azure.AI.OpenAI/src/Generated/AzureCosmosDBChatExtensionParameters.Serialization.cs
@@ -60,11 +60,8 @@ namespace Azure.AI.OpenAI
             writer.WriteStringValue(IndexName);
             writer.WritePropertyName("fieldsMapping"u8);
             writer.WriteObjectValue(FieldMappingOptions);
-            if (EmbeddingDependency != null)
-            {
-                writer.WritePropertyName("embeddingDependency"u8);
-                writer.WriteObjectValue(EmbeddingDependency);
-            }
+            writer.WritePropertyName("embeddingDependency"u8);
+            writer.WriteObjectValue(EmbeddingDependency);
             if (options.Format != "W" && _serializedAdditionalRawData != null)
             {
                 foreach (var item in _serializedAdditionalRawData)
@@ -112,7 +109,7 @@ namespace Azure.AI.OpenAI
             string containerName = default;
             string indexName = default;
             AzureCosmosDBFieldMappingOptions fieldsMapping = default;
-            Optional<OnYourDataVectorizationSource> embeddingDependency = default;
+            OnYourDataVectorizationSource embeddingDependency = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
             Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
@@ -180,10 +177,6 @@ namespace Azure.AI.OpenAI
                 }
                 if (property.NameEquals("embeddingDependency"u8))
                 {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
                     embeddingDependency = OnYourDataVectorizationSource.DeserializeOnYourDataVectorizationSource(property.Value, options);
                     continue;
                 }
@@ -203,7 +196,7 @@ namespace Azure.AI.OpenAI
                 containerName,
                 indexName,
                 fieldsMapping,
-                embeddingDependency.Value,
+                embeddingDependency,
                 serializedAdditionalRawData);
         }
 
--- sdk/openai/Azure.AI.OpenAI/src/Generated/PineconeChatExtensionParameters.Serialization.cs
+++ sdk/openai/Azure.AI.OpenAI/src/Generated/PineconeChatExtensionParameters.Serialization.cs
@@ -58,11 +58,8 @@ namespace Azure.AI.OpenAI
             writer.WriteStringValue(IndexName);
             writer.WritePropertyName("fieldsMapping"u8);
             writer.WriteObjectValue(FieldMappingOptions);
-            if (EmbeddingDependency != null)
-            {
-                writer.WritePropertyName("embeddingDependency"u8);
-                writer.WriteObjectValue(EmbeddingDependency);
-            }
+            writer.WritePropertyName("embeddingDependency"u8);
+            writer.WriteObjectValue(EmbeddingDependency);
             if (options.Format != "W" && _serializedAdditionalRawData != null)
             {
                 foreach (var item in _serializedAdditionalRawData)
@@ -109,7 +106,7 @@ namespace Azure.AI.OpenAI
             string environment = default;
             string indexName = default;
             PineconeFieldMappingOptions fieldsMapping = default;
-            Optional<OnYourDataVectorizationSource> embeddingDependency = default;
+            OnYourDataVectorizationSource embeddingDependency = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
             Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
@@ -172,10 +169,6 @@ namespace Azure.AI.OpenAI
                 }
                 if (property.NameEquals("embeddingDependency"u8))
                 {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
-                    {
-                        continue;
-                    }
                     embeddingDependency = OnYourDataVectorizationSource.DeserializeOnYourDataVectorizationSource(property.Value, options);
                     continue;
                 }
@@ -194,7 +187,7 @@ namespace Azure.AI.OpenAI
                 environment,
                 indexName,
                 fieldsMapping,
-                embeddingDependency.Value,
+                embeddingDependency,
                 serializedAdditionalRawData);
         }
 
