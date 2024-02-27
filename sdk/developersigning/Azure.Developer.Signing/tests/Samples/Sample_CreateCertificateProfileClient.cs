605782ae843641e560cb2328438de6f717cfe3ed
Jaxel Rojas <jrojaslopez@microsoft.com>
2024-02-27 11:26:43 -0400

    docs: update README.md with samples and tests

--- sdk/developersigning/Azure.Developer.Signing/tests/Samples/Sample_CreateCertificateProfileClient.cs
+++ sdk/developersigning/Azure.Developer.Signing/tests/Samples/Sample_CreateCertificateProfileClient.cs
@@ -10,12 +10,12 @@ namespace Azure.Developer.Signing.Tests.Samples
     {
         public void CreateClients(string region)
         {
-            #region Snippet:Azure_Developer_Signing_CreateCertificateProfileClient_Scenario
+            #region Snippet:Azure_Developer_Signing_CreateCertificateProfileClient
 
             var credential = new DefaultAzureCredential();
             CertificateProfile certificateProfileClient = new SigningClient(credential).GetCertificateProfileClient(region);
 
-            #endregion Snippet:Azure_Developer_Signing_CreateCertificateProfileClient_Scenario
+            #endregion Snippet:Azure_Developer_Signing_CreateCertificateProfileClient
         }
     }
 }
