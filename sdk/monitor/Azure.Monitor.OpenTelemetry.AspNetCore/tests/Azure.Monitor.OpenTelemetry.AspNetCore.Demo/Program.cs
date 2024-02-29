e7713bc12f91ab9d67631ea37a5226703156d7fe
Timothy Mothra Lee <tilee@microsoft.com>
2024-02-28 16:46:03 -0800

    Revert "use Demo for testing. REVERT THIS BEFORE MERGING"
    This reverts commit 38bf3f3a83eee996dc489643121bd2a472035cfb.

--- sdk/monitor/Azure.Monitor.OpenTelemetry.AspNetCore/tests/Azure.Monitor.OpenTelemetry.AspNetCore.Demo/Program.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.AspNetCore/tests/Azure.Monitor.OpenTelemetry.AspNetCore.Demo/Program.cs
@@ -2,21 +2,16 @@
 // Licensed under the MIT License.
 
 #if NET6_0_OR_GREATER
-using System;
 using System.Diagnostics;
 using System.Net.Http;
-using System.Threading.Tasks;
-using System.Threading;
 using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddOpenTelemetry().UseAzureMonitor();
+builder.Services.AddOpenTelemetry().UseAzureMonitor();
 
 /*
 builder.Services.AddOpenTelemetry().UseAzureMonitor(o =>
@@ -38,52 +33,5 @@ app.MapGet("/", () =>
     return $"Hello World! OpenTelemetry Trace: {Activity.Current?.Id}";
 });
 
-#if !NETFRAMEWORK
-app.MapGet("/StressTest", () =>
-{
-    RunStressTest();
-    return "StressTest!";
-});
-#endif
-
 app.Run();
-#endif
-
-#if !NETFRAMEWORK
-void RunStressTest()
-{
-    // Get the number of available processors
-    int numProcessors = Environment.ProcessorCount;
-
-    Task[] tasks = new Task[numProcessors];
-
-    // Start each task
-    for (int i = 0; i < numProcessors; i++)
-    {
-        tasks[i] = Task.Run(() => Compute(cancellationTokenSource.Token));
-    }
-
-    var timeStamp = DateTime.Now.AddSeconds(20);
-    while (DateTime.Now < timeStamp)
-    {
-        // do nothing
-    }
-
-    cancellationTokenSource.Cancel();
-
-    Task.WaitAll(tasks);
-}
-
-void Compute(CancellationToken cancellationToken)
-{
-    while (!cancellationToken.IsCancellationRequested)
-    {
-        // Simulate intensive computation
-        double result = 0;
-        for (int i = 0; i < 1000000; i++)
-        {
-            result += Math.Sqrt(i);
-        }
-    }
-}
 #endif
