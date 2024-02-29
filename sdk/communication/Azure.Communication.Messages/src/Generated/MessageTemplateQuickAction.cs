cfffc06d7a74ccecb69df6cf9abbc5f134a17afb
glorialimicrosoft <67171398+glorialimicrosoft@users.noreply.github.com>
2024-02-29 15:38:54 -0500

    Update the typespec to the one in Main branch (#42296)

--- sdk/communication/Azure.Communication.Messages/src/Generated/MessageTemplateQuickAction.cs
+++ sdk/communication/Azure.Communication.Messages/src/Generated/MessageTemplateQuickAction.cs
@@ -14,7 +14,7 @@ namespace Azure.Communication.Messages
     public partial class MessageTemplateQuickAction : MessageTemplateValue
     {
         /// <summary> Initializes a new instance of <see cref="MessageTemplateQuickAction"/>. </summary>
-        /// <param name="name"> Name of the Template value. </param>
+        /// <param name="name"> Template binding reference name. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="name"/> is null. </exception>
         public MessageTemplateQuickAction(string name) : base(name)
         {
@@ -27,7 +27,7 @@ namespace Azure.Communication.Messages
         }
 
         /// <summary> Initializes a new instance of <see cref="MessageTemplateQuickAction"/>. </summary>
-        /// <param name="name"> Name of the Template value. </param>
+        /// <param name="name"> Template binding reference name. </param>
         /// <param name="kind"> The type discriminator describing a template parameter type. </param>
         /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
         /// <param name="text"> The [Optional] quick action text. </param>
