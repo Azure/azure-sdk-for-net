dcab699cc05c200fd14d09d920c7e4e1f21fb2d2
azure-sdk <azuresdk@microsoft.com>
2024-03-01 18:07:42 +0000

    Update SDK codes hy_mi_3

--- sdk/iot/Azure.ResourceManager.IotFirmwareDefense/src/Generated/Models/CveResult.cs
+++ sdk/iot/Azure.ResourceManager.IotFirmwareDefense/src/Generated/Models/CveResult.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Collections.Generic;
 using Azure.Core;
+using Azure.ResourceManager.IotFirmwareDefense;
 using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.IotFirmwareDefense.Models
--- sdk/maps/Azure.ResourceManager.Maps/src/Generated/Models/MapsAccountPatch.Serialization.cs
+++ sdk/maps/Azure.ResourceManager.Maps/src/Generated/Models/MapsAccountPatch.Serialization.cs
@@ -10,6 +10,7 @@ using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
+using Azure.ResourceManager.Maps;
 using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.Maps.Models
