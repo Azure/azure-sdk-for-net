3eccf8e0c73a413aeaf2000992e61ab917920985
azure-sdk <azuresdk@microsoft.com>
2024-03-01 18:16:05 +0000

    Update SDK codes pu_sq_5

--- sdk/resources/Azure.ResourceManager.Resources/src/Generated/Models/WhatIfOperationResult.Serialization.cs
+++ sdk/resources/Azure.ResourceManager.Resources/src/Generated/Models/WhatIfOperationResult.Serialization.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Text.Json;
 using Azure;
 using Azure.Core;
+using Azure.ResourceManager.Resources;
 
 namespace Azure.ResourceManager.Resources.Models
 {
