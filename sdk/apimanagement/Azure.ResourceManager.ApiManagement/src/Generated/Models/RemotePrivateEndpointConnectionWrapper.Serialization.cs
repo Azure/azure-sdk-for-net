1ad58d3ac9025d7f6f892d9b9d03f646844a7946
azure-sdk <azuresdk@microsoft.com>
2024-03-02 00:13:31 +0000

    Update SDK codes ad_co_0

--- sdk/apimanagement/Azure.ResourceManager.ApiManagement/src/Generated/Models/RemotePrivateEndpointConnectionWrapper.Serialization.cs
+++ sdk/apimanagement/Azure.ResourceManager.ApiManagement/src/Generated/Models/RemotePrivateEndpointConnectionWrapper.Serialization.cs
@@ -10,6 +10,7 @@ using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
+using Azure.ResourceManager.ApiManagement;
 using Azure.ResourceManager.Resources.Models;
 
 namespace Azure.ResourceManager.ApiManagement.Models
