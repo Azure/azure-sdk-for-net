2dd9c5bf5912eccf26b3bd0c6c67fdac7b92e696
m-nash <64171366+m-nash@users.noreply.github.com>
2024-03-01 16:42:17 -0800

    test

--- sdk/postgresql/Azure.ResourceManager.PostgreSql/src/PostgreSqlFlexibleServers/Custom/Internal/HelperSuppressions.cs
+++ sdk/postgresql/Azure.ResourceManager.PostgreSql/src/PostgreSqlFlexibleServers/Custom/Internal/HelperSuppressions.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.Core;
+using Azure.Core; 
 
 [assembly: CodeGenSuppressType("Azure.ResourceManager.PostgreSql.ChangeTrackingList")]
