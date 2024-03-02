638e17b1deb1852ac799ad179440a94df0cfac55
azure-sdk <azuresdk@microsoft.com>
2024-03-02 00:19:53 +0000

    Update SDK codes co_da_1

--- sdk/consumption/Azure.ResourceManager.Consumption/src/Generated/Models/ConsumptionLegacyReservationRecommendation.Serialization.cs
+++ sdk/consumption/Azure.ResourceManager.Consumption/src/Generated/Models/ConsumptionLegacyReservationRecommendation.Serialization.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Text.Json;
 using Azure;
 using Azure.Core;
+using Azure.ResourceManager.Consumption;
 using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.Consumption.Models
--- sdk/containerapps/Azure.ResourceManager.AppContainers/src/Generated/Models/ContainerAppDiagnosticsMetadata.cs
+++ sdk/containerapps/Azure.ResourceManager.AppContainers/src/Generated/Models/ContainerAppDiagnosticsMetadata.cs
@@ -8,6 +8,7 @@
 using System;
 using System.Collections.Generic;
 using Azure.Core;
+using Azure.ResourceManager.AppContainers;
 using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.AppContainers.Models
