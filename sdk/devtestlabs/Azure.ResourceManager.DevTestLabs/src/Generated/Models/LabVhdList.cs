015222e99db72f29ea074c3c3fad51181bc8d7a4
azure-sdk <azuresdk@microsoft.com>
2024-03-04 22:08:45 +0000

    Update SDK codes de_he_2

--- sdk/devtestlabs/Azure.ResourceManager.DevTestLabs/src/Generated/Models/LabVhdList.cs
+++ sdk/devtestlabs/Azure.ResourceManager.DevTestLabs/src/Generated/Models/LabVhdList.cs
@@ -7,7 +7,7 @@
 
 using System;
 using System.Collections.Generic;
-using Azure.Core;
+using Azure.ResourceManager.DevTestLabs;
 using Azure.ResourceManager.Resources.Models;
 
 namespace Azure.ResourceManager.DevTestLabs.Models
