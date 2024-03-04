6ae6e446fc0f063d90e0d441c55967ab0577882a
azure-sdk <azuresdk@microsoft.com>
2024-03-04 19:51:15 +0000

    Update SDK codes mi_pu_4

--- sdk/operationalinsights/Azure.ResourceManager.OperationalInsights/src/Generated/Models/SearchMetadata.cs
+++ sdk/operationalinsights/Azure.ResourceManager.OperationalInsights/src/Generated/Models/SearchMetadata.cs
@@ -8,7 +8,7 @@
 using System;
 using System.Collections.Generic;
 using Azure;
-using Azure.Core;
+using Azure.ResourceManager.OperationalInsights;
 
 namespace Azure.ResourceManager.OperationalInsights.Models
 {
