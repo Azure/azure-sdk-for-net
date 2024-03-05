251273b2cc1e7520597da21679cbbee699a709f6
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2024-03-05 15:27:34 -0800

    Prepare for GA (#42371)
    * Prepare for GA
    
    * other changes

--- sdk/core/Azure.Core.Expressions.DataFactory/src/DataFactorySecretDefinition.cs
+++ sdk/core/Azure.Core.Expressions.DataFactory/src/DataFactorySecretDefinition.cs
@@ -5,22 +5,22 @@ namespace Azure.Core.Expressions.DataFactory
 {
     /// <summary>
     /// The base definition of a secret type.
-    /// Please note <see cref="DataFactorySecretBaseDefinition"/> is the base class. According to the scenario, a derived class of the base class might need to be assigned here, or this property needs to be casted to one of the possible derived classes.
+    /// Please note <see cref="DataFactorySecretDefinition"/> is the base class. According to the scenario, a derived class of the base class might need to be assigned here, or this property needs to be casted to one of the possible derived classes.
     /// The available derived classes include <see cref="DataFactorySecretString"/> and <see cref="DataFactoryKeyVaultSecretReference"/>.
     /// </summary>
     [PropertyReferenceType(new string[0], new[]{ nameof(SecretBaseType)})]
-    public abstract partial class DataFactorySecretBaseDefinition
+    public abstract partial class DataFactorySecretDefinition
     {
         /// <summary> Initializes a new instance of DataFactorySecretBaseDefinition. </summary>
         [InitializationConstructor]
-        protected DataFactorySecretBaseDefinition()
+        protected DataFactorySecretDefinition()
         {
         }
 
         /// <summary> Initializes a new instance of DataFactorySecretBaseDefinition. </summary>
         /// <param name="secretBaseType"> Type of the secret. </param>
         [SerializationConstructor]
-        internal DataFactorySecretBaseDefinition(string? secretBaseType)
+        internal DataFactorySecretDefinition(string? secretBaseType)
         {
             SecretBaseType = secretBaseType;
         }
