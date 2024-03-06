423de452e189a365989820a4b0f60b7c1c39e15a
Liudmila Molkova <limolkova@microsoft.com>
2024-03-06 11:13:03 -0800

    address feedback on core

--- sdk/core/Azure.Core/src/Shared/EventSourceEvent.cs
+++ sdk/core/Azure.Core/src/Shared/EventSourceEvent.cs
@@ -10,8 +10,15 @@ using System.Diagnostics.Tracing;
 
 namespace Azure.Core.Shared
 {
+    /// <summary>
+    /// Wraps <see cref="EventWrittenEventArgs"/> into <see cref="IReadOnlyList{T}"/> simplifying iterating over
+    /// payload properties and providing them to logging libraries in a structured way.
+    /// </summary>
     internal readonly struct EventSourceEvent : IReadOnlyList<KeyValuePair<string, object?>>
     {
+        /// <summary>
+        /// Gets underlying EventSource event.
+        /// </summary>
         public EventWrittenEventArgs EventData { get; }
 
         public EventSourceEvent(EventWrittenEventArgs eventData)
@@ -19,6 +26,7 @@ namespace Azure.Core.Shared
             EventData = eventData;
         }
 
+        /// <inheritdoc />
         public IEnumerator<KeyValuePair<string, object?>> GetEnumerator()
         {
             if (EventData.PayloadNames == null || EventData.Payload == null)
@@ -37,18 +45,25 @@ namespace Azure.Core.Shared
             return GetEnumerator();
         }
 
+        /// <summary>
+        /// Returns the count of payload properties in the Eventsource event.
+        /// </summary>
         public int Count => EventData.PayloadNames?.Count ?? 0;
 
+        /// <summary>
+        /// Formats EventSource event as a string including all payload properties.
+        /// </summary>
         public string Format()
         {
             return EventSourceEventFormatting.Format(EventData);
         }
 
+        /// <inheritdoc />
         public KeyValuePair<string, object?> this[int index]
         {
             get
             {
-                if (EventData.PayloadNames == null || EventData.Payload == null || index >= EventData.PayloadNames.Count)
+                if (EventData.PayloadNames == null || EventData.Payload == null || index >= EventData.PayloadNames.Count || index < 0)
                 {
                     throw new IndexOutOfRangeException("Index was out of range.");
                 }
