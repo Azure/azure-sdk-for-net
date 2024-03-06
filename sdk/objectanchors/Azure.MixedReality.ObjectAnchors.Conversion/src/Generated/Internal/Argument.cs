44e538aecda817ce565c3dd32af8b255b11f604f
m-nash <64171366+m-nash@users.noreply.github.com>
2024-03-06 14:18:36 -0800

    update objectanchors

--- sdk/objectanchors/Azure.MixedReality.ObjectAnchors.Conversion/src/Generated/Internal/Argument.cs
+++ sdk/objectanchors/Azure.MixedReality.ObjectAnchors.Conversion/src/Generated/Internal/Argument.cs
@@ -9,7 +9,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Azure.MixedReality.ObjectAnchors.Conversion
+namespace Azure.MixedReality.Common
 {
     internal static class Argument
     {
