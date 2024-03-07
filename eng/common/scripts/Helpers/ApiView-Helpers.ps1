184af8a7b64606a5463ea6718ca91266577090af
Praveen Kuttappan <prmarott@microsoft.com>
2024-03-07 17:05:47 -0500

    Create api review script changes to add a param to set release tag

--- eng/common/scripts/Helpers/ApiView-Helpers.ps1
+++ eng/common/scripts/Helpers/ApiView-Helpers.ps1
@@ -20,7 +20,7 @@ function MapLanguageName($language)
     return $lang
 }
 
-function Check-ApiReviewStatus($packageName, $packageVersion, $language, $url, $apiKey)
+function Check-ApiReviewStatus($packageName, $packageVersion, $language, $url, $apiKey, $apiApprovalStatus = $null, $packageNameStatus = $null)
 {
   # Get API view URL and API Key to check status
   Write-Host "Checking API review status"
@@ -35,31 +35,86 @@ function Check-ApiReviewStatus($packageName, $packageVersion, $language, $url, $
     packageVersion = $packageVersion
   }
 
+  if (!$apiApprovalStatus) {
+    $apiApprovalStatus = [PSCustomObject]@{
+      IsApproved = $false
+      Details = ""
+    }
+  }
+
+  if (!$packageNameStatus) {
+    $packageNameStatus = [PSCustomObject]@{
+      IsApproved = $false
+      Details = ""
+    }
+  }
+
   try
   {
     $response = Invoke-WebRequest $url -Method 'GET' -Headers $headers -Body $body
-    if ($response.StatusCode -eq '200')
-    {
-      Write-Host "API Review is approved for package $($packageName)"
+    Process-ReviewStatusCode -statusCode $response.StatusCode -packageName $packageName -apiApprovalStatus $apiApprovalStatus -packageNameStatus $packageNameStatus
+    if ($apiApprovalStatus.IsApproved) {
+      Write-Host $($apiApprovalStatus.Details)
     }
-    elseif ($response.StatusCode -eq '202')
-    {
-      Write-Host "Package name $($packageName) is not yet approved by an SDK API approver. Package name must be approved to release a beta version if $($packageName) was never released a stable version."
-      Write-Host "You can check http://aka.ms/azsdk/engsys/apireview/faq for more details on package name Approval."
+    else {
+      Write-warning $($apiApprovalStatus.Details)
     }
-    elseif ($response.StatusCode -eq '201')
-    {
-      Write-Warning "API Review is not approved for package $($packageName). Release pipeline will fail if API review is not approved for a stable version release."
-      Write-Host "You can check http://aka.ms/azsdk/engsys/apireview/faq for more details on API Approval."
+    if ($packageNameStatus.IsApproved) {
+      Write-Host $($packageNameStatus.Details)
     }
-    else
-    {
-      Write-Warning "API review status check returned unexpected response. $($response)"
-      Write-Host "You can check http://aka.ms/azsdk/engsys/apireview/faq for more details on API Approval."
+    else {
+      Write-warning $($packageNameStatus.Details)
     }
   }
   catch
   {
     Write-Warning "Failed to check API review status for package $($PackageName). You can check http://aka.ms/azsdk/engsys/apireview/faq for more details on API Approval."
   }
+}
+
+function Process-ReviewStatusCode($statusCode, $packageName, $apiApprovalStatus, $packageNameStatus)
+{
+  $apiApproved = $false
+  $apiApprovalDetails = "API Review is not approved for package $($packageName). Release pipeline will fail if API review is not approved for a GA version release. You can check http://aka.ms/azsdk/engsys/apireview/faq for more details on API Approval."
+
+  $packageNameApproved = $false
+  $packageNameApprovalDetails = ""
+
+  # 200 API approved and Package name approved
+  # 201 API review is not approved, Package name is approved
+  # 202 API review is not approved, Package name is not approved
+
+  switch ($statusCode)
+  {
+    200
+    {
+      $apiApprovalDetails = "API Review is approved for package $($packageName)"
+      $apiApproved = $true
+
+      $packageNameApproved = $true
+      $packageNameApprovalDetails = "Package name is approved for package $($packageName)"
+    }
+    201
+    {
+      $packageNameApproved = $true
+      $packageNameApprovalDetails = "Package name is approved for package $($packageName)"
+    }
+    202
+    {
+      $packageNameApprovalDetails = "Package name $($packageName) is not yet approved by an SDK API approver. Package name must be approved to release a beta version if $($packageName) was never released as a stable version."
+      $packageNameApprovalDetails += " You can check http://aka.ms/azsdk/engsys/apireview/faq for more details on package name Approval."
+    }
+    default
+    {
+      $apiApprovalDetails = "Invalid status code from APIView. status code $($statusCode)"
+      $packageNameApprovalDetails = "Invalid status code from APIView. status code $($statusCode)"
+      Write-Error "Failed to process API Review status for for package $($PackageName). Please reach out to Azure SDK engineering systems on teams channel."
+    }
+  }
+
+  $apiApprovalStatus.IsApproved = $apiApproved
+  $apiApprovalStatus.Details = $apiApprovalDetails
+
+  $packageNameStatus.IsApproved = $packageNameApproved
+  $packageNameStatus.Details = $packageNameApprovalDetails
 }
