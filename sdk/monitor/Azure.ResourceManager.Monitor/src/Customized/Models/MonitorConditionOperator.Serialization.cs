03268703630596ceb21e6c9cc669f09d0710e5c3
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2024-03-07 17:04:51 +0800

    Update MonitorConditionOperator.Serialization.cs

--- sdk/monitor/Azure.ResourceManager.Monitor/src/Customized/Models/MonitorConditionOperator.Serialization.cs
+++ sdk/monitor/Azure.ResourceManager.Monitor/src/Customized/Models/MonitorConditionOperator.Serialization.cs
@@ -17,6 +17,7 @@ namespace Azure.ResourceManager.Monitor.Models
             MonitorConditionOperator.GreaterThanOrEqual => "GreaterThanOrEqual",
             MonitorConditionOperator.LessThan => "LessThan",
             MonitorConditionOperator.LessThanOrEqual => "LessThanOrEqual",
+            // Original enum value is `Equals`, changed to actual service response `Equal`
             MonitorConditionOperator.EqualsValue => "Equal",
             _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown MonitorConditionOperator value.")
         };
@@ -27,6 +28,7 @@ namespace Azure.ResourceManager.Monitor.Models
             if (StringComparer.OrdinalIgnoreCase.Equals(value, "GreaterThanOrEqual")) return MonitorConditionOperator.GreaterThanOrEqual;
             if (StringComparer.OrdinalIgnoreCase.Equals(value, "LessThan")) return MonitorConditionOperator.LessThan;
             if (StringComparer.OrdinalIgnoreCase.Equals(value, "LessThanOrEqual")) return MonitorConditionOperator.LessThanOrEqual;
+            // Original enum value is `Equals`, changed to actual service response `Equal`
             if (StringComparer.OrdinalIgnoreCase.Equals(value, "Equal")) return MonitorConditionOperator.EqualsValue;
             throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown MonitorConditionOperator value.");
         }
