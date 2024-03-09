ceb982025fed1a72d2fb58836d68e9151f35a01e
azure-sdk <azuresdk@microsoft.com>
2024-03-09 02:56:53 +0000

    Update SDK codes de_he_2

--- sdk/eventgrid/Azure.ResourceManager.EventGrid/src/Generated/Models/UnknownPartnerUpdateDestinationInfo.cs
+++ sdk/eventgrid/Azure.ResourceManager.EventGrid/src/Generated/Models/UnknownPartnerUpdateDestinationInfo.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 
 namespace Azure.ResourceManager.EventGrid.Models
 {
-    /// <summary> The UnknownPartnerUpdateDestinationInfo. </summary>
+    /// <summary> Unknown version of PartnerUpdateDestinationInfo. </summary>
     internal partial class UnknownPartnerUpdateDestinationInfo : PartnerUpdateDestinationInfo
     {
         /// <summary> Initializes a new instance of <see cref="UnknownPartnerUpdateDestinationInfo"/>. </summary>
