2b1d7301c5c15e2cc4a3f1339adc7a04c3491168
azure-sdk <azuresdk@microsoft.com>
2024-03-09 02:58:24 +0000

    Update SDK codes he_mi_3

--- sdk/machinelearningservices/Azure.ResourceManager.MachineLearning/src/Generated/Models/UnknownWorkspaceConnectionPropertiesV2.cs
+++ sdk/machinelearningservices/Azure.ResourceManager.MachineLearning/src/Generated/Models/UnknownWorkspaceConnectionPropertiesV2.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 
 namespace Azure.ResourceManager.MachineLearning.Models
 {
-    /// <summary> The UnknownWorkspaceConnectionPropertiesV2. </summary>
+    /// <summary> Unknown version of WorkspaceConnectionPropertiesV2. </summary>
     internal partial class UnknownWorkspaceConnectionPropertiesV2 : MachineLearningWorkspaceConnectionProperties
     {
         /// <summary> Initializes a new instance of <see cref="UnknownWorkspaceConnectionPropertiesV2"/>. </summary>
