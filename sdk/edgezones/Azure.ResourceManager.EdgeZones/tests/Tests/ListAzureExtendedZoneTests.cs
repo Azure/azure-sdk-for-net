a3a392cbd46b4eaeccc403229c452f3b49234a61
Jason Urban 🍨 <jurban@microsoft.com>
2024-03-09 15:52:49 -0800

    The Azure SDK Release Planner says to write at least one test for the most common usage scenario.

---
+++ sdk/edgezones/Azure.ResourceManager.EdgeZones/tests/Tests/ListAzureExtendedZoneTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.ResourceManager.EdgeZones.Tests.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Threading.Tasks;
+    using Azure.Core.TestFramework;
+    using Azure.ResourceManager.EdgeZones.Models;
+    using Microsoft.Identity.Client;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class ListAzureExtendedZoneTests : EdgeZonesManagementTestBase
+    {
+        public ListAzureExtendedZoneTests() : base(true)
+        {
+        }
+
+        [TestCase, Order(1)]
+        [RecordedTest]
+        public void TestListAzureExtendedZones()
+        {
+            var azureExtendedZones = EdgeZonesExtensions.GetAzureExtendedZones(DefaultSubscription);
+            Assert.NotNull(azureExtendedZones);
+            Assert.IsTrue(azureExtendedZones.Count() >= 1);
+        }
+    }
+}
