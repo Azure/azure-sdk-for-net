dbf3ef934d5b89f73c72645f164e9a597807385b
Jason Urban 🍨 <jurban@microsoft.com>
2024-03-08 21:02:42 -0800

    Adding AzureExtendedZone tests.

---
+++ sdk/edgezones/Azure.ResourceManager.EdgeZones/tests/AzureExtendedZoneTests.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.ResourceManager.EdgeZones.Tests
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Azure.Core.TestFramework;
+    using Azure.ResourceManager.EdgeZones.Models;
+    using NUnit.Framework;
+
+    internal class AzureExtendedZoneTests
+    {
+    }
+}
