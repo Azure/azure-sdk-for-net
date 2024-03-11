e8a1267020371be59926606d227f6bbf52bbfff5
azure-sdk <azuresdk@microsoft.com>
2024-03-11 17:41:11 +0000

    Update SDK codes ad_co_0

--- sdk/chaos/Azure.ResourceManager.Chaos/src/Generated/Models/ChaosExperimentPatch.cs
+++ sdk/chaos/Azure.ResourceManager.Chaos/src/Generated/Models/ChaosExperimentPatch.cs
@@ -7,7 +7,7 @@
 
 using System;
 using System.Collections.Generic;
-using Azure.Core;
+using Azure.ResourceManager.Chaos;
 using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.Chaos.Models
