00d65782b14db529fcfd50febd33ef7ea3fc05ae
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2024-03-11 16:35:20 -0700

    Use globally unique name for top level resources (#42613)
    * Use globally unique name for top level resources
    
    * api

--- sdk/provisioning/Azure.Provisioning/api/Azure.Provisioning.net6.0.cs
+++ sdk/provisioning/Azure.Provisioning/api/Azure.Provisioning.net6.0.cs
@@ -126,6 +126,7 @@ namespace Azure.Provisioning.AppConfiguration
     {
         public AppConfigurationStore(Azure.Provisioning.IConstruct scope, Azure.Provisioning.ResourceManager.ResourceGroup? parent = null, string name = "store", string version = "2023-03-01", Azure.Core.AzureLocation? location = default(Azure.Core.AzureLocation?)) : base (default(Azure.Provisioning.IConstruct), default(Azure.Provisioning.Resource), default(string), default(Azure.Core.ResourceType), default(string), default(System.Func<string, Azure.ResourceManager.AppConfiguration.AppConfigurationStoreData>)) { }
         public static Azure.Provisioning.AppConfiguration.AppConfigurationStore FromExisting(Azure.Provisioning.IConstruct scope, string name, Azure.Provisioning.ResourceManager.ResourceGroup? parent = null) { throw null; }
+        protected override string GetAzureName(Azure.Provisioning.IConstruct scope, string resourceName) { throw null; }
     }
 }
 namespace Azure.Provisioning.AppService
@@ -204,6 +205,7 @@ namespace Azure.Provisioning.CognitiveServices
     {
         public CognitiveServicesAccount(Azure.Provisioning.IConstruct scope, string? kind = null, Azure.ResourceManager.CognitiveServices.Models.CognitiveServicesSku? sku = null, Azure.Provisioning.ResourceManager.ResourceGroup? parent = null, string name = "cs", string version = "2023-05-01", Azure.Core.AzureLocation? location = default(Azure.Core.AzureLocation?)) : base (default(Azure.Provisioning.IConstruct), default(Azure.Provisioning.Resource), default(string), default(Azure.Core.ResourceType), default(string), default(System.Func<string, Azure.ResourceManager.CognitiveServices.CognitiveServicesAccountData>)) { }
         public static Azure.Provisioning.CognitiveServices.CognitiveServicesAccount FromExisting(Azure.Provisioning.IConstruct scope, string name, Azure.Provisioning.ResourceManager.ResourceGroup? parent = null) { throw null; }
+        protected override string GetAzureName(Azure.Provisioning.IConstruct scope, string resourceName) { throw null; }
     }
     public partial class CognitiveServicesAccountDeployment : Azure.Provisioning.Resource<Azure.ResourceManager.CognitiveServices.CognitiveServicesAccountDeploymentData>
     {
