aff306e101b6469c2f382500c957970b65ed3810
azure-sdk <azuresdk@microsoft.com>
2024-03-11 02:05:56 +0000

    Update SDK codes co_da_1

--- sdk/communication/Azure.Communication.Messages/src/Generated/WhatsAppMessageTemplateBindingsComponent.Serialization.cs
+++ sdk/communication/Azure.Communication.Messages/src/Generated/WhatsAppMessageTemplateBindingsComponent.Serialization.cs
@@ -10,7 +10,6 @@ using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure;
-using Azure.Communication.Messages;
 using Azure.Core;
 
 namespace Azure.Communication.Messages.Models.Channels
