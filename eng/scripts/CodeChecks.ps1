9aea8c72565b04c1ba59cf75aeba8c4b7a125e50
Mike Harder <mharder@microsoft.com>
2024-03-12 06:33:27 +0000

    Keep using npm exec

--- eng/scripts/CodeChecks.ps1
+++ eng/scripts/CodeChecks.ps1
@@ -57,7 +57,7 @@ function Invoke-Block([scriptblock]$cmd) {
 try {
     Write-Host "Initialize npx cache"
     Invoke-Block {
-        & npx autorest@ --version
+        & npm exec --yes -- autorest@ --version
     }
 
     if ($ProjectDirectory -and -not $ServiceDirectory)
