a32a06c5e37945e08833febe74474c8b3f03c077
jolov <jolov@microsoft.com>
2024-03-12 11:29:02 -0700

    rename

---
+++ sdk/provisioning/Azure.Provisioning/src/eventhubs/EventHubsConsumerGroup.cs
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Azure.Core;
+using Azure.ResourceManager.EventHubs;
+using Azure.ResourceManager.EventHubs.Models;
+
+namespace Azure.Provisioning.EventHubs
+{
+    /// <summary>
+    /// Represents an Event Hub consumer group.
+    /// </summary>
+    public class EventHubsConsumerGroup : Resource<EventHubsConsumerGroupData>
+    {
+        private const string ResourceTypeName = "Microsoft.EventHub/namespaces/eventhubs/consumergroups";
+        private static readonly Func<string, EventHubsConsumerGroupData> Empty = (name) => ArmEventHubsModelFactory.EventHubsConsumerGroupData();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventHub"/>.
+        /// </summary>
+        /// <param name="scope">The scope.</param>
+        /// <param name="parent">The parent.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="version">The version.</param>
+        /// <param name="location">The location.</param>
+        public EventHubsConsumerGroup(IConstruct scope, EventHub? parent = null, string name = "hub", string version = EventHubsNamespace.DefaultVersion, AzureLocation? location = default)
+            : this(scope, parent, name, version, false, (name) => ArmEventHubsModelFactory.EventHubsConsumerGroupData(
+                name: name,
+                resourceType: ResourceTypeName,
+                location: location ?? Environment.GetEnvironmentVariable("AZURE_LOCATION") ?? AzureLocation.WestUS))
+        {
+        }
+
+        private EventHubsConsumerGroup(IConstruct scope, EventHub? parent = null, string name = "hub", string version = EventHubsNamespace.DefaultVersion, bool isExisting = true, Func<string, EventHubsConsumerGroupData>? creator = null)
+            : base(scope, parent, name, ResourceTypeName, EventHubsNamespace.DefaultVersion, creator ?? Empty, isExisting)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="EventHub"/> class referencing an existing instance.
+        /// </summary>
+        /// <param name="scope">The scope.</param>
+        /// <param name="name">The resource name.</param>
+        /// <param name="parent">The resource group.</param>
+        /// <returns>The KeyVault instance.</returns>
+        public static EventHubsConsumerGroup FromExisting(IConstruct scope, string name, EventHub parent)
+            => new EventHubsConsumerGroup(scope, parent: parent, name: name, isExisting: true);
+
+        /// <inheritdoc/>
+        protected override Resource? FindParentInScope(IConstruct scope)
+        {
+            return scope.GetSingleResource<EventHub>() ?? new EventHub(scope);
+        }
+    }
+}
