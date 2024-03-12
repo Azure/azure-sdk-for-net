162a6db174488df73bbdd95df1f6de5e532a59f9
Fang Chen - Microsoft <fanche@microsoft.com>
2024-03-12 16:01:40 -0700

    [ACS][CallAutomation]Move SourceCallerIdNumber from Answer to Transfer api (#42642)
    * remove SourceCallerIdNumber from answercallOptions, update autorest with new swagger
    
    * create internal classes for media feature and ops feature models
    
    * update api
    
    * add SourceCallIdNumber to Transfer

--- sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/HoldRequestInternal.Serialization.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/HoldRequestInternal.Serialization.cs
@@ -10,7 +10,7 @@ using Azure.Core;
 
 namespace Azure.Communication.CallAutomation
 {
-    internal partial class StartHoldMusicRequestInternal : IUtf8JsonSerializable
+    internal partial class HoldRequestInternal : IUtf8JsonSerializable
     {
         void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
         {
