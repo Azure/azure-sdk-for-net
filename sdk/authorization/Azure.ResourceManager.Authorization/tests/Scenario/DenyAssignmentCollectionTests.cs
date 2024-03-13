65d2506fe10e90a5d2097211e9854fbdb16b3fd7
melina5656 <140602263+melina5656@users.noreply.github.com>
2024-03-13 17:17:25 +0800

    update

--- sdk/authorization/Azure.ResourceManager.Authorization/tests/Scenario/DenyAssignmentCollectionTests.cs
+++ sdk/authorization/Azure.ResourceManager.Authorization/tests/Scenario/DenyAssignmentCollectionTests.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.TestFramework;
-using Azure.ResourceManager.Resources;
 using NUnit.Framework;
 
 namespace Azure.ResourceManager.Authorization.Tests.Scenario
@@ -17,23 +15,14 @@ namespace Azure.ResourceManager.Authorization.Tests.Scenario
         {
         }
 
-        public ResourceGroupResource ResourceGroup { get; set; }
-
         [Test]
-        public async Task Get()
+        public async Task GetAndExists()
         {
             var randomGuid = Recording.Random.NewGuid().ToString();
             var scope = DefaultSubscription.Id;
             var denyAssignmentCollection = Client.GetDenyAssignments(scope);
-            try
-            {
-                var denyAssignment = (await denyAssignmentCollection.GetAsync(randomGuid)).Value;
-                Assert.AreEqual(1, 0);
-            }
-            catch (RequestFailedException ex) when (ex.Status == 404)
-            {
-                Assert.AreEqual(1, 1);
-            }
+            Assert.ThrowsAsync<RequestFailedException>(() => denyAssignmentCollection.GetAsync(randomGuid));
+            Assert.IsFalse(await denyAssignmentCollection.ExistsAsync(randomGuid));
         }
 
         [Test]
@@ -42,7 +31,7 @@ namespace Azure.ResourceManager.Authorization.Tests.Scenario
             var scope = DefaultSubscription.Id;
             var denyAssignmentCollection = Client.GetDenyAssignments(scope);
             var denyAssignments = await denyAssignmentCollection.GetAllAsync().ToEnumerableAsync();
-            Assert.AreEqual(0, denyAssignments.Count);
+            Assert.IsEmpty(denyAssignments);
         }
 
         [Test]
@@ -52,7 +41,7 @@ namespace Azure.ResourceManager.Authorization.Tests.Scenario
             var scope = new ResourceIdentifier($"{subscriptionId}/resourceGroups/resourceGroupTestName1");
             var denyAssignmentCollection = Client.GetDenyAssignments(scope);
             var denyAssignments = await denyAssignmentCollection.GetAllAsync().ToEnumerableAsync();
-            Assert.AreEqual(0, denyAssignments.Count);
+            Assert.IsEmpty(denyAssignments);
         }
 
         [Test]
@@ -62,16 +51,7 @@ namespace Azure.ResourceManager.Authorization.Tests.Scenario
             var scope = new ResourceIdentifier($"{subscriptionId}/resourceGroups/discoverProtectableItemTest/providers/Microsoft.RecoveryServices/vaults/recoveryVault");
             var denyAssignmentCollection = Client.GetDenyAssignments(scope);
             var denyAssignments = await denyAssignmentCollection.GetAllAsync().ToEnumerableAsync();
-            Assert.AreEqual(0, denyAssignments.Count);
-        }
-
-        [Test]
-        public async Task Exists()
-        {
-            var randomGuid = Recording.Random.NewGuid().ToString();
-            var scope = DefaultSubscription.Id;
-            var denyAssignmentCollection = Client.GetDenyAssignments(scope);
-            Assert.IsFalse(await denyAssignmentCollection.ExistsAsync(randomGuid));
+            Assert.IsEmpty(denyAssignments);
         }
     }
 }
