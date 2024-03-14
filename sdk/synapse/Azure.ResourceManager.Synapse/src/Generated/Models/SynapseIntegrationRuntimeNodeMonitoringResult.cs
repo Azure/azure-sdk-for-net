8f8d41ebe8e0344b3285554ba19ac16923df1cea
azure-sdk <azuresdk@microsoft.com>
2024-03-14 19:35:20 +0000

    Update SDK codes sq_wo_6

--- sdk/synapse/Azure.ResourceManager.Synapse/src/Generated/Models/SynapseIntegrationRuntimeNodeMonitoringResult.cs
+++ sdk/synapse/Azure.ResourceManager.Synapse/src/Generated/Models/SynapseIntegrationRuntimeNodeMonitoringResult.cs
@@ -7,7 +7,7 @@
 
 using System;
 using System.Collections.Generic;
-using Azure.ResourceManager.Synapse;
+using Azure.Core;
 
 namespace Azure.ResourceManager.Synapse.Models
 {
--- sdk/websites/Azure.ResourceManager.AppService/src/Generated/Models/FunctionAppHostKeys.cs
+++ sdk/websites/Azure.ResourceManager.AppService/src/Generated/Models/FunctionAppHostKeys.cs
@@ -7,7 +7,7 @@
 
 using System;
 using System.Collections.Generic;
-using Azure.ResourceManager.AppService;
+using Azure.Core;
 
 namespace Azure.ResourceManager.AppService.Models
 {
