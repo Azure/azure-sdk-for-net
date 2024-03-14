07dfb5bde6914b9e730dbfa05df6e9855cefc362
azure-sdk <azuresdk@microsoft.com>
2024-03-14 19:32:23 +0000

    Update SDK codes de_he_2

--- sdk/devspaces/Azure.ResourceManager.DevSpaces/src/Generated/Models/ControllerPatch.cs
+++ sdk/devspaces/Azure.ResourceManager.DevSpaces/src/Generated/Models/ControllerPatch.cs
@@ -7,7 +7,7 @@
 
 using System;
 using System.Collections.Generic;
-using Azure.ResourceManager.DevSpaces;
+using Azure.Core;
 
 namespace Azure.ResourceManager.DevSpaces.Models
 {
--- sdk/devtestlabs/Azure.ResourceManager.DevTestLabs/src/Generated/Models/DevTestLabResourcePatch.cs
+++ sdk/devtestlabs/Azure.ResourceManager.DevTestLabs/src/Generated/Models/DevTestLabResourcePatch.cs
@@ -7,7 +7,7 @@
 
 using System;
 using System.Collections.Generic;
-using Azure.ResourceManager.DevTestLabs;
+using Azure.Core;
 
 namespace Azure.ResourceManager.DevTestLabs.Models
 {
