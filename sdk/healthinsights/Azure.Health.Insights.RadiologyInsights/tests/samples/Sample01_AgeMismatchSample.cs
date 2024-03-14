71cd07962f62ff9ec4ce4f01361307a9b68172cb
Rameez Shaikh <rameezmicrosoft@gmail.com>
2024-03-14 17:10:48 +0530

    added inferences

---
+++ sdk/healthinsights/Azure.Health.Insights.RadiologyInsights/tests/samples/Sample01_AgeMismatchSample.cs
@@ -0,0 +1,209 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Azure.Core.TestFramework;
+using Azure.Health.Insights.RadiologyInsights.Tests.Infrastructure;
+using NUnit.Framework;
+
+namespace Azure.Health.Insights.RadiologyInsights.Tests
+{
+    internal class Sample01_AgeMismatchSample : SamplesBase<HealthInsightsTestEnvironment>
+    {
+        private const string DOC_CONTENT = "CLINICAL HISTORY:   "
+            + "\r\n20-year-old female presenting with abdominal pain. Surgical history significant for appendectomy."
+            + "\r\n "
+            + "\r\nCOMPARISON:   "
+            + "\r\nRight upper quadrant sonographic performed 1 day prior."
+            + "\r\n "
+            + "\r\nTECHNIQUE:   "
+            + "\r\nTransabdominal grayscale pelvic sonography with duplex color Doppler "
+            + "\r\nand spectral waveform analysis of the ovaries."
+            + "\r\n "
+            + "\r\nFINDINGS:   "
+            + "\r\nThe uterus is unremarkable given the transabdominal technique with "
+            + "\r\nendometrial echo complex within physiologic normal limits. The "
+            + "\r\novaries are symmetric in size, measuring 2.5 x 1.2 x 3.0 cm and the "
+            + "\r\nleft measuring 2.8 x 1.5 x 1.9 cm.\n \r\nOn duplex imaging, Doppler signal is symmetric."
+            + "\r\n "
+            + "\r\nIMPRESSION:   "
+            + "\r\n1. Normal pelvic sonography. Findings of testicular torsion."
+            + "\r\n\nA new US pelvis within the next 6 months is recommended."
+            + "\n\nThese results have been discussed with Dr. Jones at 3 PM on November 5 2020.\n "
+            + "\r\n";
+
+        [Test]
+        public void RadiologyInsightsAgeMismatchScenario()
+        {
+            // Read endpoint and apiKey
+            string endpoint = TestEnvironment.Endpoint;
+            string apiKey = TestEnvironment.ApiKey;
+
+            Uri endpointUri = new Uri(endpoint);
+            AzureKeyCredential credential = new AzureKeyCredential(apiKey);
+            RadiologyInsightsClient client = new RadiologyInsightsClient(endpointUri, credential);
+
+            RadiologyInsightsData radiologyInsightsData = GetRadiologyInsightsData();
+
+            Operation<RadiologyInsightsInferenceResult> operation = client.InferRadiologyInsights(WaitUntil.Completed, radiologyInsightsData);
+            RadiologyInsightsInferenceResult responseData = operation.Value;
+            IReadOnlyList<RadiologyInsightsInference> inferences = responseData.PatientResults[0].Inferences;
+
+            foreach (RadiologyInsightsInference inference in inferences)
+            {
+                if (inference is AgeMismatchInference ageMismatchInference)
+                {
+                    Console.Write("Age Mismatch Inference found: ");
+                    IReadOnlyList<FhirR4Extension> extensions = ageMismatchInference.Extension;
+                    Console.Write("   Evidence: " + ExtractEvidence(extensions));
+                }
+            }
+        }
+
+        private static String ExtractEvidence(IReadOnlyList<FhirR4Extension> extensions)
+        {
+            String evidence = "";
+            foreach (FhirR4Extension extension in extensions)
+            {
+                IReadOnlyList<FhirR4Extension> subExtensions = extension.Extension;
+                if (subExtensions != null)
+                {
+                    evidence += extractEvidenceToken(subExtensions) + " ";
+                }
+            }
+            return evidence;
+        }
+
+        private static String extractEvidenceToken(IReadOnlyList<FhirR4Extension> subExtensions)
+        {
+            String evidence = "";
+            int offset = -1;
+            int length = -1;
+            foreach (FhirR4Extension iExtension in subExtensions)
+            {
+                if (iExtension.Url.Equals("offset"))
+                {
+                    offset = (int)iExtension.ValueInteger;
+                }
+                if (iExtension.Url.Equals("length"))
+                {
+                    length = (int)iExtension.ValueInteger;
+                }
+            }
+            if (offset > 0 && length > 0)
+            {
+                Console.Write(Math.Min(offset + length, DOC_CONTENT.Length));
+                //System.out.println("Offset: " + offset + ", length: " + length);
+                evidence = DOC_CONTENT.Substring(offset, Math.Min(offset + length, DOC_CONTENT.Length - offset));
+            }
+            return evidence;
+        }
+
+        private static RadiologyInsightsData GetRadiologyInsightsData()
+        {
+            PatientRecord patientRecord = CreatePatientRecord();
+            List<PatientRecord> patientRecords = new() { patientRecord };
+            RadiologyInsightsData radiologyInsightsData = new(patientRecords);
+            radiologyInsightsData.Configuration = CreateConfiguration();
+            return radiologyInsightsData;
+        }
+
+        private static RadiologyInsightsModelConfiguration CreateConfiguration()
+        {
+            RadiologyInsightsInferenceOptions radiologyInsightsInferenceOptions = GetRadiologyInsightsInferenceOptions();
+
+            RadiologyInsightsModelConfiguration radiologyInsightsModelConfiguration = new()
+            {
+                Locale = "en-US",
+                IncludeEvidence = true,
+                InferenceOptions = radiologyInsightsInferenceOptions
+            };
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.Finding);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.AgeMismatch);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.LateralityDiscrepancy);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.SexMismatch);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.CompleteOrderDiscrepancy);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.LimitedOrderDiscrepancy);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.CriticalResult);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.FollowupCommunication);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.FollowupRecommendation);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.RadiologyProcedure);
+
+            return radiologyInsightsModelConfiguration;
+        }
+
+        private static RadiologyInsightsInferenceOptions GetRadiologyInsightsInferenceOptions()
+        {
+            RadiologyInsightsInferenceOptions radiologyInsightsInferenceOptions = new();
+            FollowupRecommendationOptions followupRecommendationOptions = new();
+            FindingOptions findingOptions = new();
+            followupRecommendationOptions.IncludeRecommendationsWithNoSpecifiedModality = true;
+            followupRecommendationOptions.IncludeRecommendationsInReferences = true;
+            followupRecommendationOptions.ProvideFocusedSentenceEvidence = true;
+            findingOptions.ProvideFocusedSentenceEvidence = true;
+            radiologyInsightsInferenceOptions.FollowupRecommendationOptions = followupRecommendationOptions;
+            radiologyInsightsInferenceOptions.FindingOptions = findingOptions;
+            return radiologyInsightsInferenceOptions;
+        }
+
+        private static PatientRecord CreatePatientRecord()
+        {
+            string id = "patient_id2";
+            PatientDetails patientInfo = new()
+            {
+                BirthDate = new System.DateTime(1959, 11, 11),
+                Sex = PatientSex.Female,
+            };
+            Encounter encounter = new("encounterid1")
+            {
+                Class = EncounterClass.InPatient,
+                Period = new TimePeriod
+                {
+                    Start = new System.DateTime(2021, 08, 28),
+                    End = new System.DateTime(2021, 08, 28)
+                }
+            };
+            List<Encounter> encounterList = new() { encounter };
+            DocumentContent documentContent = new(DocumentContentSourceType.Inline, DOC_CONTENT);
+            PatientDocument patientDocument = new(DocumentType.Note, "doc2", documentContent)
+            {
+                ClinicalType = ClinicalDocumentType.RadiologyReport,
+                CreatedDateTime = new System.DateTime(2021, 08, 28),
+                AdministrativeMetadata = CreateDocumentAdministrativeMetadata()
+            };
+            PatientRecord patientRecord = new(id);
+            patientRecord.Info = patientInfo;
+            patientRecord.Encounters.Add(encounter);
+            patientRecord.PatientDocuments.Add(patientDocument);
+            return patientRecord;
+        }
+
+        private static DocumentAdministrativeMetadata CreateDocumentAdministrativeMetadata()
+        {
+            DocumentAdministrativeMetadata documentAdministrativeMetadata = new DocumentAdministrativeMetadata();
+
+            FhirR4Coding coding = new()
+            {
+                Display = "US PELVIS COMPLETE",
+                Code = "USPELVIS",
+                System = "Http://hl7.org/fhir/ValueSet/cpt-all"
+            };
+
+            FhirR4CodeableConcept codeableConcept = new();
+            codeableConcept.Coding.Add(coding);
+
+            FhirR4Extendible orderedProcedure = new()
+            {
+                Description = "US PELVIS COMPLETE",
+                Code = codeableConcept
+            };
+
+            documentAdministrativeMetadata.OrderedProcedures.Add(orderedProcedure);
+
+            return documentAdministrativeMetadata;
+        }
+    }
+}
---
+++ sdk/healthinsights/Azure.Health.Insights.RadiologyInsights/tests/samples/Sample01_LateralityDiscrepancySample.cs
@@ -0,0 +1,159 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Azure.Core.TestFramework;
+using Azure.Health.Insights.RadiologyInsights.Tests.Infrastructure;
+using NUnit.Framework;
+
+namespace Azure.Health.Insights.RadiologyInsights.Tests
+{
+    internal class Sample01_LateralityDiscrepancySample : SamplesBase<HealthInsightsTestEnvironment>
+    {
+        private const string DOC_CONTENT = "Exam:   US LT BREAST TARGETED"
+            + "\r\n\r\nTechnique:  Targeted imaging of the  right breast  is performed."
+            + "\r\n\r\nFindings:\\r\\n\\r\\nTargeted imaging of the left breast is performed from the 6:00 to the 9:00 position.  "
+            + "\r\n\r\nAt the 6:00 position, 5 cm from the nipple, there is a 3 x 2 x 4 mm minimally hypoechoic mass with a peripheral calcification. This may correspond to the mammographic finding. No other cystic or solid masses visualized."
+            + "\r\n";
+
+        [Test]
+        public void RadiologyInsightsLateralityDiscrepancyScenario()
+        {
+            // Read endpoint and apiKey
+            string endpoint = TestEnvironment.Endpoint;
+            string apiKey = TestEnvironment.ApiKey;
+
+            Uri endpointUri = new Uri(endpoint);
+            AzureKeyCredential credential = new AzureKeyCredential(apiKey);
+            RadiologyInsightsClient client = new RadiologyInsightsClient(endpointUri, credential);
+
+            RadiologyInsightsData radiologyInsightsData = GetRadiologyInsightsData();
+
+            Operation<RadiologyInsightsInferenceResult> operation = client.InferRadiologyInsights(WaitUntil.Completed, radiologyInsightsData);
+            RadiologyInsightsInferenceResult responseData = operation.Value;
+            IReadOnlyList<RadiologyInsightsInference> inferences = responseData.PatientResults[0].Inferences;
+
+            foreach (RadiologyInsightsInference inference in inferences)
+            {
+                if (inference is LateralityDiscrepancyInference lateralityDiscrepancyInference)
+                {
+                    FhirR4CodeableConcept lateralityIndication = lateralityDiscrepancyInference.LateralityIndication;
+                    IList<FhirR4Coding> codingList = lateralityIndication.Coding;
+                    Console.Write("Laterality Discrepancy Inference found: ");
+                    var discrepancyType = lateralityDiscrepancyInference.DiscrepancyType;
+                    foreach (FhirR4Coding fhirR4Coding in codingList)
+                    {
+                        Console.Write("   Coding: " + fhirR4Coding.Code + ", " + fhirR4Coding.Display + " (" + fhirR4Coding.System + "), type: " + discrepancyType);
+                    }
+                }
+            }
+        }
+
+        private static RadiologyInsightsData GetRadiologyInsightsData()
+        {
+            PatientRecord patientRecord = CreatePatientRecord();
+            List<PatientRecord> patientRecords = new() { patientRecord };
+            RadiologyInsightsData radiologyInsightsData = new(patientRecords);
+            radiologyInsightsData.Configuration = CreateConfiguration();
+            return radiologyInsightsData;
+        }
+
+        private static RadiologyInsightsModelConfiguration CreateConfiguration()
+        {
+            RadiologyInsightsInferenceOptions radiologyInsightsInferenceOptions = GetRadiologyInsightsInferenceOptions();
+
+            RadiologyInsightsModelConfiguration radiologyInsightsModelConfiguration = new()
+            {
+                Locale = "en-US",
+                IncludeEvidence = true,
+                InferenceOptions = radiologyInsightsInferenceOptions
+            };
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.Finding);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.AgeMismatch);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.LateralityDiscrepancy);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.SexMismatch);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.CompleteOrderDiscrepancy);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.LimitedOrderDiscrepancy);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.CriticalResult);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.FollowupCommunication);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.FollowupRecommendation);
+            radiologyInsightsModelConfiguration.InferenceTypes.Add(RadiologyInsightsInferenceType.RadiologyProcedure);
+
+            return radiologyInsightsModelConfiguration;
+        }
+
+        private static RadiologyInsightsInferenceOptions GetRadiologyInsightsInferenceOptions()
+        {
+            RadiologyInsightsInferenceOptions radiologyInsightsInferenceOptions = new();
+            FollowupRecommendationOptions followupRecommendationOptions = new();
+            FindingOptions findingOptions = new();
+            followupRecommendationOptions.IncludeRecommendationsWithNoSpecifiedModality = true;
+            followupRecommendationOptions.IncludeRecommendationsInReferences = true;
+            followupRecommendationOptions.ProvideFocusedSentenceEvidence = true;
+            findingOptions.ProvideFocusedSentenceEvidence = true;
+            radiologyInsightsInferenceOptions.FollowupRecommendationOptions = followupRecommendationOptions;
+            radiologyInsightsInferenceOptions.FindingOptions = findingOptions;
+            return radiologyInsightsInferenceOptions;
+        }
+
+        private static PatientRecord CreatePatientRecord()
+        {
+            string id = "patient_id2";
+            PatientDetails patientInfo = new()
+            {
+                BirthDate = new System.DateTime(1959, 11, 11),
+                Sex = PatientSex.Female,
+            };
+            Encounter encounter = new("encounterid1")
+            {
+                Class = EncounterClass.InPatient,
+                Period = new TimePeriod
+                {
+                    Start = new System.DateTime(2021, 08, 28),
+                    End = new System.DateTime(2021, 08, 28)
+                }
+            };
+            List<Encounter> encounterList = new() { encounter };
+            DocumentContent documentContent = new(DocumentContentSourceType.Inline, DOC_CONTENT);
+            PatientDocument patientDocument = new(DocumentType.Note, "doc2", documentContent)
+            {
+                ClinicalType = ClinicalDocumentType.RadiologyReport,
+                CreatedDateTime = new System.DateTime(2021, 08, 28),
+                AdministrativeMetadata = CreateDocumentAdministrativeMetadata()
+            };
+            PatientRecord patientRecord = new(id);
+            patientRecord.Info = patientInfo;
+            patientRecord.Encounters.Add(encounter);
+            patientRecord.PatientDocuments.Add(patientDocument);
+            return patientRecord;
+        }
+
+        private static DocumentAdministrativeMetadata CreateDocumentAdministrativeMetadata()
+        {
+            DocumentAdministrativeMetadata documentAdministrativeMetadata = new DocumentAdministrativeMetadata();
+
+            FhirR4Coding coding = new()
+            {
+                Display = "US BREAST - LEFT LIMITED",
+                Code = "26688-1",
+                System = "Http://hl7.org/fhir/ValueSet/cpt-all"
+            };
+
+            FhirR4CodeableConcept codeableConcept = new();
+            codeableConcept.Coding.Add(coding);
+
+            FhirR4Extendible orderedProcedure = new()
+            {
+                Description = "US BREAST - LEFT LIMITED",
+                Code = codeableConcept
+            };
+
+            documentAdministrativeMetadata.OrderedProcedures.Add(orderedProcedure);
+
+            return documentAdministrativeMetadata;
+        }
+    }
+}
