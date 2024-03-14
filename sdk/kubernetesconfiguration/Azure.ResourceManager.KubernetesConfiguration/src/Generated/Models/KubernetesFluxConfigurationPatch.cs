3e82f0c29707730f013b9b8b5e3a879f29fd6d52
azure-sdk <azuresdk@microsoft.com>
2024-03-14 19:32:57 +0000

    Update SDK codes he_mi_3

--- sdk/kubernetesconfiguration/Azure.ResourceManager.KubernetesConfiguration/src/Generated/Models/KubernetesFluxConfigurationPatch.cs
+++ sdk/kubernetesconfiguration/Azure.ResourceManager.KubernetesConfiguration/src/Generated/Models/KubernetesFluxConfigurationPatch.cs
@@ -7,7 +7,7 @@
 
 using System;
 using System.Collections.Generic;
-using Azure.ResourceManager.KubernetesConfiguration;
+using Azure.Core;
 
 namespace Azure.ResourceManager.KubernetesConfiguration.Models
 {
