6dee38a31afd64603f363af86cd2f4886058cf2c
azure-sdk <azuresdk@microsoft.com>
2024-03-15 02:46:58 +0000

    Update SDK codes co_da_1

--- sdk/datamigration/Azure.ResourceManager.DataMigration/src/Generated/Models/CommandProperties.Serialization.cs
+++ sdk/datamigration/Azure.ResourceManager.DataMigration/src/Generated/Models/CommandProperties.Serialization.cs
@@ -86,10 +86,10 @@ namespace Azure.ResourceManager.DataMigration.Models
             {
                 switch (discriminator.GetString())
                 {
-                    case "Migrate.SqlServer.AzureDbSqlMi.Complete": return MigrateMISyncCompleteCommandProperties.DeserializeMigrateMISyncCompleteCommandProperties(element, options);
-                    case "Migrate.Sync.Complete.Database": return MigrateSyncCompleteCommandProperties.DeserializeMigrateSyncCompleteCommandProperties(element, options);
                     case "cancel": return MongoDBCancelCommand.DeserializeMongoDBCancelCommand(element, options);
                     case "finish": return MongoDBFinishCommand.DeserializeMongoDBFinishCommand(element, options);
+                    case "Migrate.SqlServer.AzureDbSqlMi.Complete": return MigrateMISyncCompleteCommandProperties.DeserializeMigrateMISyncCompleteCommandProperties(element, options);
+                    case "Migrate.Sync.Complete.Database": return MigrateSyncCompleteCommandProperties.DeserializeMigrateSyncCompleteCommandProperties(element, options);
                     case "restart": return MongoDBRestartCommand.DeserializeMongoDBRestartCommand(element, options);
                 }
             }
