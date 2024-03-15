c76c299bb8207e0169731bc0ba8e312556dc0a75
jolov <jolov@microsoft.com>
2024-03-14 17:14:46 -0700

    add helper suppression

--- sdk/resourcemanager/Azure.ResourceManager/src/ManagementGroup/Custom/HelperSuppressions.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/ManagementGroup/Custom/HelperSuppressions.cs
@@ -7,5 +7,6 @@ using Azure.Core;
 [assembly: CodeGenSuppressType("Azure.ResourceManager.ChangeTrackingList")]
 [assembly: CodeGenSuppressType("Azure.ResourceManager.RequestContentHelper")]
 [assembly: CodeGenSuppressType("Azure.ResourceManager.Argument")]
+[assembly: CodeGenSuppressType("Azure.ResourceManager.Utf8JsonRequestContent")]
 [assembly: CodeGenSuppressType("Azure.ResourceManager.ChangeTrackingDictionary")]
 [assembly: CodeGenSuppressType("Azure.ResourceManager.BicepSerializationHelpers")]
