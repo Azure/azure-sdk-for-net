5f347adf6223f04cd230a0f5d0f45167c2317241
jolov <jolov@microsoft.com>
2024-03-17 21:04:27 -0700

    Add user assigned identity resource

--- sdk/provisioning/Azure.Provisioning/api/Azure.Provisioning.net6.0.cs
+++ sdk/provisioning/Azure.Provisioning/api/Azure.Provisioning.net6.0.cs
@@ -309,6 +309,15 @@ namespace Azure.Provisioning.KeyVaults
         public static Azure.Provisioning.KeyVaults.KeyVaultSecret FromExisting(Azure.Provisioning.IConstruct scope, string name, Azure.Provisioning.KeyVaults.KeyVault parent) { throw null; }
     }
 }
+namespace Azure.Provisioning.ManagedServiceIdentities
+{
+    public partial class UserAssignedIdentity : Azure.Provisioning.Resource<Azure.ResourceManager.ManagedServiceIdentities.UserAssignedIdentityData>
+    {
+        public UserAssignedIdentity(Azure.Provisioning.IConstruct scope, Azure.Provisioning.ResourceManager.ResourceGroup? parent = null, string name = "useridentity", string version = "2023-01-31", Azure.Core.AzureLocation? location = default(Azure.Core.AzureLocation?)) : base (default(Azure.Provisioning.IConstruct), default(Azure.Provisioning.Resource), default(string), default(Azure.Core.ResourceType), default(string), default(System.Func<string, Azure.ResourceManager.ManagedServiceIdentities.UserAssignedIdentityData>)) { }
+        public static Azure.Provisioning.ManagedServiceIdentities.UserAssignedIdentity FromExisting(Azure.Provisioning.IConstruct scope, string name, Azure.Provisioning.ResourceManager.ResourceGroup? parent = null) { throw null; }
+        protected override string GetAzureName(Azure.Provisioning.IConstruct scope, string resourceName) { throw null; }
+    }
+}
 namespace Azure.Provisioning.OperationalInsights
 {
     public partial class OperationalInsightsWorkspace : Azure.Provisioning.Resource<Azure.ResourceManager.OperationalInsights.OperationalInsightsWorkspaceData>
