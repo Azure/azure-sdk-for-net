55b0c5b3216c17d8d1b79dcf9ab6e972540d45bf
Madalyn Redding <mredding@microsoft.com>
2024-03-19 14:47:18 -0400

    initial file stubs

---
+++ sdk/core/System.ClientModel/src/Internal/ClientEventSourceEventFormatting.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace System.ClientModel.Internal;
+
+internal static class ClientEventSourceEventFormatting
+{
+    public static string Format(EventWrittenEventArgs eventData)
+    {
+        var payloadArray = eventData.Payload?.ToArray() ?? Array.Empty<object?>();
+
+        ProcessPayloadArray(payloadArray);
+
+        if (eventData.Message != null)
+        {
+            try
+            {
+                return string.Format(CultureInfo.InvariantCulture, eventData.Message, payloadArray);
+            }
+            catch (FormatException)
+            {
+            }
+        }
+
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append(eventData.EventName);
+
+        if (!string.IsNullOrWhiteSpace(eventData.Message))
+        {
+            stringBuilder.AppendLine();
+            stringBuilder.Append(nameof(eventData.Message)).Append(" = ").Append(eventData.Message);
+        }
+
+        if (eventData.PayloadNames != null)
+        {
+            for (int i = 0; i < eventData.PayloadNames.Count; i++)
+            {
+                stringBuilder.AppendLine();
+                stringBuilder.Append(eventData.PayloadNames[i]).Append(" = ").Append(payloadArray[i]);
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    private static void ProcessPayloadArray(object?[] payloadArray)
+    {
+        for (int i = 0; i < payloadArray.Length; i++)
+        {
+            payloadArray[i] = FormatValue(payloadArray[i]);
+        }
+    }
+
+    private static object? FormatValue(object? o)
+    {
+        if (o is byte[] bytes)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                stringBuilder.AppendFormat(CultureInfo.InvariantCulture, "{0:X2}", b);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        return o;
+    }
+}
