9474fc5e9c8c04df6001f85dbf4d7b9464100b0f
azure-sdk <azuresdk@microsoft.com>
2024-03-20 23:28:38 +0000

    Update SDK code ba_co_1

--- sdk/cognitiveservices/Azure.ResourceManager.CognitiveServices/src/Generated/Models/ServiceAccountApiProperties.cs
+++ sdk/cognitiveservices/Azure.ResourceManager.CognitiveServices/src/Generated/Models/ServiceAccountApiProperties.cs
@@ -8,7 +8,6 @@
 using System;
 using System.Collections.Generic;
 using Azure.Core;
-using Azure.ResourceManager.CognitiveServices;
 
 namespace Azure.ResourceManager.CognitiveServices.Models
 {
--- sdk/communication/Azure.Communication.AlphaIds/src/Generated/Models/CommunicationErrorResponse.cs
+++ sdk/communication/Azure.Communication.AlphaIds/src/Generated/Models/CommunicationErrorResponse.cs
@@ -6,7 +6,6 @@
 #nullable disable
 
 using System;
-using Azure.Communication.AlphaIds;
 
 namespace Azure.Communication.AlphaIds.Models
 {
