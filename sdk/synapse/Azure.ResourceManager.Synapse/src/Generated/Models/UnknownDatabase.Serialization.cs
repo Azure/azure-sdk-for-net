9242f4e90bc8d68ee1ef34f67380d790894ef5e4
azure-sdk <azuresdk@microsoft.com>
2024-03-20 14:07:31 +0000

    Update SDK code st_wo_9

--- sdk/synapse/Azure.ResourceManager.Synapse/src/Generated/Models/UnknownDatabase.Serialization.cs
+++ sdk/synapse/Azure.ResourceManager.Synapse/src/Generated/Models/UnknownDatabase.Serialization.cs
@@ -28,13 +28,13 @@ namespace Azure.ResourceManager.Synapse.Models
             }
 
             writer.WriteStartObject();
+            writer.WritePropertyName("kind"u8);
+            writer.WriteStringValue(Kind.ToString());
             if (Optional.IsDefined(Location))
             {
                 writer.WritePropertyName("location"u8);
                 writer.WriteStringValue(Location.Value);
             }
-            writer.WritePropertyName("kind"u8);
-            writer.WriteStringValue(Kind.ToString());
             if (options.Format != "W")
             {
                 writer.WritePropertyName("id"u8);
@@ -93,8 +93,8 @@ namespace Azure.ResourceManager.Synapse.Models
             {
                 return null;
             }
-            AzureLocation? location = default;
             SynapseKind kind = "Unknown";
+            AzureLocation? location = default;
             ResourceIdentifier id = default;
             string name = default;
             ResourceType type = default;
@@ -103,6 +103,11 @@ namespace Azure.ResourceManager.Synapse.Models
             Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
+                if (property.NameEquals("kind"u8))
+                {
+                    kind = new SynapseKind(property.Value.GetString());
+                    continue;
+                }
                 if (property.NameEquals("location"u8))
                 {
                     if (property.Value.ValueKind == JsonValueKind.Null)
@@ -110,11 +115,6 @@ namespace Azure.ResourceManager.Synapse.Models
                         continue;
                     }
                     location = new AzureLocation(property.Value.GetString());
-                    continue;
-                }
-                if (property.NameEquals("kind"u8))
-                {
-                    kind = new SynapseKind(property.Value.GetString());
                     continue;
                 }
                 if (property.NameEquals("id"u8))
