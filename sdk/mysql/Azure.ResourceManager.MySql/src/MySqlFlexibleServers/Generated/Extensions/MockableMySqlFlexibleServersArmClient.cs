aaba7b3235004614bc1d8d5caca0975c2b79a132
azure-sdk <azuresdk@microsoft.com>
2024-03-20 14:37:27 +0000

    Update SDK codes mi_pu_4

--- sdk/mysql/Azure.ResourceManager.MySql/src/MySqlFlexibleServers/Generated/Extensions/MockableMySqlFlexibleServersArmClient.cs
+++ sdk/mysql/Azure.ResourceManager.MySql/src/MySqlFlexibleServers/Generated/Extensions/MockableMySqlFlexibleServersArmClient.cs
@@ -6,8 +6,6 @@
 #nullable disable
 
 using Azure.Core;
-using Azure.ResourceManager;
-using Azure.ResourceManager.MySql.FlexibleServers;
 
 namespace Azure.ResourceManager.MySql.FlexibleServers.Mocking
 {
