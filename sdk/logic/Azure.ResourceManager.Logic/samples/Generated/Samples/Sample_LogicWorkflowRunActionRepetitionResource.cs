177fe91e12d7ef221316f302055f2c90278581f1
azure-sdk <azuresdk@microsoft.com>
2024-03-20 23:39:18 +0000

    Update SDK code io_ma_5

--- sdk/logic/Azure.ResourceManager.Logic/samples/Generated/Samples/Sample_LogicWorkflowRunActionRepetitionResource.cs
+++ sdk/logic/Azure.ResourceManager.Logic/samples/Generated/Samples/Sample_LogicWorkflowRunActionRepetitionResource.cs
@@ -9,8 +9,6 @@ using System;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
-using Azure.ResourceManager;
-using Azure.ResourceManager.Logic;
 using Azure.ResourceManager.Logic.Models;
 
 namespace Azure.ResourceManager.Logic.Samples
--- sdk/managednetwork/Azure.ResourceManager.ManagedNetwork/src/Generated/Models/Scope.Serialization.cs
+++ sdk/managednetwork/Azure.ResourceManager.ManagedNetwork/src/Generated/Models/Scope.Serialization.cs
@@ -10,7 +10,6 @@ using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
-using Azure.ResourceManager.ManagedNetwork;
 using Azure.ResourceManager.Resources.Models;
 
 namespace Azure.ResourceManager.ManagedNetwork.Models
