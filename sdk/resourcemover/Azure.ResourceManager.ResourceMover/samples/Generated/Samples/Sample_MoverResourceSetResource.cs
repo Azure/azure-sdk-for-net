7a1e1f14d5d4c0f7089bb0f700c987a92d155224
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2024-03-20 10:25:16 +0800

    [ResourceMover] Add operation: MoveCollections_Delete back (#42792)
    * Fix issue 30952
    
    * Update autorest.md

--- sdk/resourcemover/Azure.ResourceManager.ResourceMover/samples/Generated/Samples/Sample_MoverResourceSetResource.cs
+++ sdk/resourcemover/Azure.ResourceManager.ResourceMover/samples/Generated/Samples/Sample_MoverResourceSetResource.cs
@@ -59,6 +59,34 @@ namespace Azure.ResourceManager.ResourceMover.Samples
             Console.WriteLine($"Succeeded on id: {resourceData.Id}");
         }
 
+        // MoveCollections_Delete
+        [NUnit.Framework.Test]
+        [NUnit.Framework.Ignore("Only verifying that the sample builds")]
+        public async Task Delete_MoveCollectionsDelete()
+        {
+            // Generated from example definition: specification/resourcemover/resource-manager/Microsoft.Migrate/stable/2023-08-01/examples/MoveCollections_Delete.json
+            // this example is just showing the usage of "MoveCollections_Delete" operation, for the dependent resources, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this MoverResourceSetResource created on azure
+            // for more information of creating MoverResourceSetResource, please refer to the document of MoverResourceSetResource
+            string subscriptionId = "subid";
+            string resourceGroupName = "rg1";
+            string moverResourceSetName = "movecollection1";
+            ResourceIdentifier moverResourceSetResourceId = MoverResourceSetResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, moverResourceSetName);
+            MoverResourceSetResource moverResourceSet = client.GetMoverResourceSetResource(moverResourceSetResourceId);
+
+            // invoke the operation
+            ArmOperation<MoverOperationStatus> lro = await moverResourceSet.DeleteAsync(WaitUntil.Completed);
+            MoverOperationStatus result = lro.Value;
+
+            Console.WriteLine($"Succeeded: {result}");
+        }
+
         // MoveCollections_Get
         [NUnit.Framework.Test]
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
