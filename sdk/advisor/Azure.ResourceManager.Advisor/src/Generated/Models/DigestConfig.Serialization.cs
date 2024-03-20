03f5f9d7878fe83f759908736059590357cfc32b
azure-sdk <azuresdk@microsoft.com>
2024-03-20 23:35:22 +0000

    Update SDK code ad_ba_0

--- sdk/advisor/Azure.ResourceManager.Advisor/src/Generated/Models/DigestConfig.Serialization.cs
+++ sdk/advisor/Azure.ResourceManager.Advisor/src/Generated/Models/DigestConfig.Serialization.cs
@@ -10,7 +10,6 @@ using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
-using Azure.ResourceManager.Advisor;
 
 namespace Azure.ResourceManager.Advisor.Models
 {
