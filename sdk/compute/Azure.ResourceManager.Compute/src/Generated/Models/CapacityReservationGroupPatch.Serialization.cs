09e16008c4d77f5028cb764e674e409909d353a9
azure-sdk <azuresdk@microsoft.com>
2024-03-20 23:44:55 +0000

    Update SDK code co_da_2

--- sdk/compute/Azure.ResourceManager.Compute/src/Generated/Models/CapacityReservationGroupPatch.Serialization.cs
+++ sdk/compute/Azure.ResourceManager.Compute/src/Generated/Models/CapacityReservationGroupPatch.Serialization.cs
@@ -10,7 +10,6 @@ using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
-using Azure.ResourceManager.Compute;
 using Azure.ResourceManager.Resources.Models;
 
 namespace Azure.ResourceManager.Compute.Models
