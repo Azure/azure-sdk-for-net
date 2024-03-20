c62d7db416b02006083f1e623c6de90e1c89a329
azure-sdk <azuresdk@microsoft.com>
2024-03-20 05:47:33 +0000

    Update SDK code ev_io_4

--- sdk/hdinsight/Azure.ResourceManager.HDInsight.Containers/src/Generated/Extensions/HDInsightContainersExtensions.cs
+++ sdk/hdinsight/Azure.ResourceManager.HDInsight.Containers/src/Generated/Extensions/HDInsightContainersExtensions.cs
@@ -8,9 +8,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Azure;
 using Azure.Core;
-using Azure.ResourceManager;
 using Azure.ResourceManager.HDInsight.Containers.Mocking;
 using Azure.ResourceManager.HDInsight.Containers.Models;
 using Azure.ResourceManager.Resources;
