5e5c9c1d1e31a3cd1e379c940d04d5e3acde601f
Anne Thompson <annelo@microsoft.com>
2024-03-20 17:20:34 -0700

    nitcs

--- sdk/core/Azure.Core/src/RequestContent.cs
+++ sdk/core/Azure.Core/src/RequestContent.cs
@@ -175,7 +175,10 @@ namespace Azure.Core
             public StreamContent(Stream stream)
             {
                 if (!stream.CanSeek)
+                {
                     throw new ArgumentException("stream must be seekable", nameof(stream));
+                }
+
                 _origin = stream.Position;
                 _stream = stream;
             }
