cdf8b0a6b4246e861f8ea97f282b88c919653db3
Anne Thompson <annelo@microsoft.com>
2024-03-21 08:57:47 -0700

    Initial implementation

---
+++ sdk/core/System.ClientModel/src/Convenience/StreamingClientResultOfT.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace System.ClientModel;
+
+#pragma warning disable CS1591 // public XML comments
+public abstract class StreamingClientResult<T> : ClientResult, IAsyncEnumerable<T>
+{
+    protected internal StreamingClientResult(PipelineResponse response) : base(response)
+    {
+    }
+
+    public abstract IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default);
+}
+#pragma warning restore CS1591 // public XML comments
