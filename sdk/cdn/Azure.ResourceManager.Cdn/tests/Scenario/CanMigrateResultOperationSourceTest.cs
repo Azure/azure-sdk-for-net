3fb334fc046322502c91cbe4460e9fd5646287e0
Jingnan Xu <jingnanxu@microsoft.com>
2024-03-21 16:16:34 +0800

    add test

---
+++ sdk/cdn/Azure.ResourceManager.Cdn/tests/Scenario/CanMigrateResultOperationSourceTest.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Cdn.Models;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Cdn.Tests
+{
+    public class CanMigrateResultOperationSourceTest : CdnManagementTestBase
+    {
+        public CanMigrateResultOperationSourceTest(bool isAsync)
+            : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task CanMigrateResultTest()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await CreateResourceGroup(subscription, "testRg-");
+            var classic = new Resources.Models.WritableSubResource();
+            CanMigrateContent canMigrateContent = new(classic);
+            var output = await rg.CanMigrateProfileAsync(WaitUntil.Completed, canMigrateContent);
+            Assert.IsNotNull(output);
+        }
+    }
+}
---
+++ sdk/cdn/Azure.ResourceManager.Cdn/tests/Scenario/MigrateResultOperationSourceTest.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Cdn.Models;
+using Azure.ResourceManager.Resources;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.Cdn.Tests
+{
+    public class MigrateResultOperationSourceTest : CdnManagementTestBase
+    {
+        public MigrateResultOperationSourceTest(bool isAsync)
+            : base(isAsync)//, RecordedTestMode.Record)
+        {
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task MigrateResultTest()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await CreateResourceGroup(subscription, "testRg-");
+            var classic = new Resources.Models.WritableSubResource();
+            var sku = new CdnSku { Name = CdnSkuName.StandardMicrosoft };
+            MigrationContent migrationContent = new(sku, classic, "test");
+            var output = await rg.MigrateProfileAsync(WaitUntil.Completed, migrationContent);
+            Assert.IsNotNull(output);
+        }
+    }
+}
