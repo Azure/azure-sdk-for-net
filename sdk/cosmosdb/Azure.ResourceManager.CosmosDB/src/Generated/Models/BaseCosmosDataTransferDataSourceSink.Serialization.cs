4006cbce87dbf4e07757f9d465ca487137c69f5d
azure-sdk <azuresdk@microsoft.com>
2024-03-22 07:07:34 +0000

    Update SDK code co_da_2

--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/Models/BaseCosmosDataTransferDataSourceSink.Serialization.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/Models/BaseCosmosDataTransferDataSourceSink.Serialization.cs
@@ -10,7 +10,6 @@ using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Core;
-using Azure.ResourceManager.CosmosDB;
 
 namespace Azure.ResourceManager.CosmosDB.Models
 {
@@ -23,7 +22,7 @@ namespace Azure.ResourceManager.CosmosDB.Models
             var format = options.Format == "W" ? ((IPersistableModel<BaseCosmosDataTransferDataSourceSink>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
-                throw new FormatException($"The model {nameof(BaseCosmosDataTransferDataSourceSink)} does not support '{format}' format.");
+                throw new FormatException($"The model {nameof(BaseCosmosDataTransferDataSourceSink)} does not support writing '{format}' format.");
             }
 
             writer.WriteStartObject();
@@ -57,7 +56,7 @@ namespace Azure.ResourceManager.CosmosDB.Models
             var format = options.Format == "W" ? ((IPersistableModel<BaseCosmosDataTransferDataSourceSink>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
-                throw new FormatException($"The model {nameof(BaseCosmosDataTransferDataSourceSink)} does not support '{format}' format.");
+                throw new FormatException($"The model {nameof(BaseCosmosDataTransferDataSourceSink)} does not support reading '{format}' format.");
             }
 
             using JsonDocument document = JsonDocument.ParseValue(ref reader);
@@ -115,7 +114,7 @@ namespace Azure.ResourceManager.CosmosDB.Models
                 case "J":
                     return ModelReaderWriter.Write(this, options);
                 default:
-                    throw new FormatException($"The model {nameof(BaseCosmosDataTransferDataSourceSink)} does not support '{options.Format}' format.");
+                    throw new FormatException($"The model {nameof(BaseCosmosDataTransferDataSourceSink)} does not support writing '{options.Format}' format.");
             }
         }
 
@@ -131,7 +130,7 @@ namespace Azure.ResourceManager.CosmosDB.Models
                         return DeserializeBaseCosmosDataTransferDataSourceSink(document.RootElement, options);
                     }
                 default:
-                    throw new FormatException($"The model {nameof(BaseCosmosDataTransferDataSourceSink)} does not support '{options.Format}' format.");
+                    throw new FormatException($"The model {nameof(BaseCosmosDataTransferDataSourceSink)} does not support reading '{options.Format}' format.");
             }
         }
 
--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/ThroughputPoolAccountResourceData.Serialization.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/ThroughputPoolAccountResourceData.Serialization.cs
@@ -24,7 +24,7 @@ namespace Azure.ResourceManager.CosmosDB
             var format = options.Format == "W" ? ((IPersistableModel<ThroughputPoolAccountResourceData>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
-                throw new FormatException($"The model {nameof(ThroughputPoolAccountResourceData)} does not support '{format}' format.");
+                throw new FormatException($"The model {nameof(ThroughputPoolAccountResourceData)} does not support writing '{format}' format.");
             }
 
             writer.WriteStartObject();
@@ -94,7 +94,7 @@ namespace Azure.ResourceManager.CosmosDB
             var format = options.Format == "W" ? ((IPersistableModel<ThroughputPoolAccountResourceData>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
-                throw new FormatException($"The model {nameof(ThroughputPoolAccountResourceData)} does not support '{format}' format.");
+                throw new FormatException($"The model {nameof(ThroughputPoolAccountResourceData)} does not support reading '{format}' format.");
             }
 
             using JsonDocument document = JsonDocument.ParseValue(ref reader);
@@ -208,7 +208,7 @@ namespace Azure.ResourceManager.CosmosDB
                 case "J":
                     return ModelReaderWriter.Write(this, options);
                 default:
-                    throw new FormatException($"The model {nameof(ThroughputPoolAccountResourceData)} does not support '{options.Format}' format.");
+                    throw new FormatException($"The model {nameof(ThroughputPoolAccountResourceData)} does not support writing '{options.Format}' format.");
             }
         }
 
@@ -224,7 +224,7 @@ namespace Azure.ResourceManager.CosmosDB
                         return DeserializeThroughputPoolAccountResourceData(document.RootElement, options);
                     }
                 default:
-                    throw new FormatException($"The model {nameof(ThroughputPoolAccountResourceData)} does not support '{options.Format}' format.");
+                    throw new FormatException($"The model {nameof(ThroughputPoolAccountResourceData)} does not support reading '{options.Format}' format.");
             }
         }
 
