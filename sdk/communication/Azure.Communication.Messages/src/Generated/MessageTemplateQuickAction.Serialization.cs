91b04bbcd5a136692b168bcb5c169e1569483aea
azure-sdk <azuresdk@microsoft.com>
2024-03-25 08:53:51 +0000

    Update SDK code co_da_2

--- sdk/communication/Azure.Communication.Messages/src/Generated/MessageTemplateQuickAction.Serialization.cs
+++ sdk/communication/Azure.Communication.Messages/src/Generated/MessageTemplateQuickAction.Serialization.cs
@@ -39,7 +39,7 @@ namespace Azure.Communication.Messages
             writer.WritePropertyName("name"u8);
             writer.WriteStringValue(Name);
             writer.WritePropertyName("kind"u8);
-            writer.WriteStringValue(Kind);
+            writer.WriteStringValue(Kind.ToString());
             if (options.Format != "W" && _serializedAdditionalRawData != null)
             {
                 foreach (var item in _serializedAdditionalRawData)
@@ -81,7 +81,7 @@ namespace Azure.Communication.Messages
             string text = default;
             string payload = default;
             string name = default;
-            string kind = default;
+            MessageTemplateValueKind kind = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
             Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
@@ -103,7 +103,7 @@ namespace Azure.Communication.Messages
                 }
                 if (property.NameEquals("kind"u8))
                 {
-                    kind = property.Value.GetString();
+                    kind = new MessageTemplateValueKind(property.Value.GetString());
                     continue;
                 }
                 if (options.Format != "W")
