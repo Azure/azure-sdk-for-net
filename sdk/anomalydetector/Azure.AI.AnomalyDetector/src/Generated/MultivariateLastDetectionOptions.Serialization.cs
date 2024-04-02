8c364eb29b8fe9e60158629d734fa783b6345e55
azure-sdk <azuresdk@microsoft.com>
2024-04-02 05:17:07 +0000

    Update SDK code ad_ba_0

--- sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/MultivariateLastDetectionOptions.Serialization.cs
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/MultivariateLastDetectionOptions.Serialization.cs
@@ -79,7 +79,7 @@ namespace Azure.AI.AnomalyDetector
             IList<VariableValues> variables = default;
             int? topContributorCount = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
-            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
+            Dictionary<string, BinaryData> rawDataDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("variables"u8))
@@ -103,10 +103,10 @@ namespace Azure.AI.AnomalyDetector
                 }
                 if (options.Format != "W")
                 {
-                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                    rawDataDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
                 }
             }
-            serializedAdditionalRawData = additionalPropertiesDictionary;
+            serializedAdditionalRawData = rawDataDictionary;
             return new MultivariateLastDetectionOptions(variables, topContributorCount, serializedAdditionalRawData);
         }
 
--- sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/MultivariateLastDetectionResult.Serialization.cs
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/MultivariateLastDetectionResult.Serialization.cs
@@ -87,7 +87,7 @@ namespace Azure.AI.AnomalyDetector
             IReadOnlyList<VariableState> variableStates = default;
             IReadOnlyList<AnomalyState> results = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
-            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
+            Dictionary<string, BinaryData> rawDataDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("variableStates"u8))
@@ -120,10 +120,10 @@ namespace Azure.AI.AnomalyDetector
                 }
                 if (options.Format != "W")
                 {
-                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                    rawDataDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
                 }
             }
-            serializedAdditionalRawData = additionalPropertiesDictionary;
+            serializedAdditionalRawData = rawDataDictionary;
             return new MultivariateLastDetectionResult(variableStates ?? new ChangeTrackingList<VariableState>(), results ?? new ChangeTrackingList<AnomalyState>(), serializedAdditionalRawData);
         }
 
