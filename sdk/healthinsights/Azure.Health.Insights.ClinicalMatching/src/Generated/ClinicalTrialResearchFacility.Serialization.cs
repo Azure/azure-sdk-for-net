1e38600ae44f99010293744ece19941a7f4f1dbd
azure-sdk <azuresdk@microsoft.com>
2024-04-02 05:15:32 +0000

    Update SDK code ev_io_4

--- sdk/healthinsights/Azure.Health.Insights.ClinicalMatching/src/Generated/ClinicalTrialResearchFacility.Serialization.cs
+++ sdk/healthinsights/Azure.Health.Insights.ClinicalMatching/src/Generated/ClinicalTrialResearchFacility.Serialization.cs
@@ -83,7 +83,7 @@ namespace Azure.Health.Insights.ClinicalMatching
             string state = default;
             string countryOrRegion = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
-            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
+            Dictionary<string, BinaryData> rawDataDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("name"u8))
@@ -108,10 +108,10 @@ namespace Azure.Health.Insights.ClinicalMatching
                 }
                 if (options.Format != "W")
                 {
-                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                    rawDataDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
                 }
             }
-            serializedAdditionalRawData = additionalPropertiesDictionary;
+            serializedAdditionalRawData = rawDataDictionary;
             return new ClinicalTrialResearchFacility(name, city, state, countryOrRegion, serializedAdditionalRawData);
         }
 
