37f56b23695ea0ae20b00b4fc70668897b62d4a9
azure-sdk <azuresdk@microsoft.com>
2024-04-03 07:24:38 +0000

    Update SDK codes ad_co_0

--- sdk/attestation/Azure.Security.Attestation/src/Generated/Models/PolicyCertificateModification.Serialization.cs
+++ sdk/attestation/Azure.Security.Attestation/src/Generated/Models/PolicyCertificateModification.Serialization.cs
@@ -54,6 +54,7 @@ namespace Azure.Security.Attestation
             {
                 writer.WriteObjectValue<PolicyCertificateModification>(model);
             }
+
             public override PolicyCertificateModification Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             {
                 using var document = JsonDocument.ParseValue(ref reader);
