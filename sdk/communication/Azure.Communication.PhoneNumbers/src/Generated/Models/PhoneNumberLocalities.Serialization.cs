54feab5192a49554e739ee4263be1f939d433aa2
azure-sdk <azuresdk@microsoft.com>
2024-04-04 20:02:15 +0000

    Update SDK code bi_co_1

--- sdk/communication/Azure.Communication.PhoneNumbers/src/Generated/Models/PhoneNumberLocalities.Serialization.cs
+++ sdk/communication/Azure.Communication.PhoneNumbers/src/Generated/Models/PhoneNumberLocalities.Serialization.cs
@@ -12,5 +12,12 @@ namespace Azure.Communication.PhoneNumbers
 {
     internal partial class PhoneNumberLocalities
     {
+        /// <summary> Deserializes the model from a raw response. </summary>
+        /// <param name="response"> The response to deserialize the model from. </param>
+        internal static PhoneNumberLocalities FromResponse(Response response)
+        {
+            using var document = JsonDocument.Parse(response.Content);
+            return PhoneNumberLocalities.DeserializePhoneNumberLocalities(document.RootElement);
+        }
     }
 }
