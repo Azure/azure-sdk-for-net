af18918e3639d7b1aa5b53edf64e17e4dcba5c1d
azure-sdk <azuresdk@microsoft.com>
2024-04-04 17:15:25 +0000

    Update SDK codes co_de_1

--- sdk/communication/Azure.Communication.PhoneNumbers/src/Generated/Models/PhoneNumberOperation.Serialization.cs
+++ sdk/communication/Azure.Communication.PhoneNumbers/src/Generated/Models/PhoneNumberOperation.Serialization.cs
@@ -12,5 +12,12 @@ namespace Azure.Communication.PhoneNumbers
 {
     internal partial class PhoneNumberOperation
     {
+        /// <summary> Deserializes the model from a raw response. </summary>
+        /// <param name="response"> The response to deserialize the model from. </param>
+        internal static PhoneNumberOperation FromResponse(Response response)
+        {
+            using var document = JsonDocument.Parse(response.Content);
+            return PhoneNumberOperation.DeserializePhoneNumberOperation(document.RootElement);
+        }
     }
 }
