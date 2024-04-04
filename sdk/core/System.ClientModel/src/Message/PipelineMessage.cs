767282069cecce7e1684829b35b5ae09006af5b7
Anne Thompson <annelo@microsoft.com>
2024-04-04 14:15:07 -0700

    update refdocs for null options case

--- sdk/core/System.ClientModel/src/Message/PipelineMessage.cs
+++ sdk/core/System.ClientModel/src/Message/PipelineMessage.cs
@@ -107,7 +107,8 @@ public class PipelineMessage : IDisposable
     /// this <see cref="PipelineMessage"/> instance.
     /// </summary>
     /// <param name="options">The <see cref="RequestOptions"/> to apply to this
-    /// <see cref="PipelineMessage"/> instance.</param>
+    /// <see cref="PipelineMessage"/> instance.  If <c>null</c> is passed,
+    /// no changes will be made to this message instance.</param>
     /// <remarks> This method is intended to be called after the creation of
     /// the <see cref="PipelineMessage"/> and its <see cref="Request"/> is
     /// complete, and prior to the call to
