1532c8bbf647ba6e12ea2c3378f1ca5e7e63d5e6
Anne Thompson <annelo@microsoft.com>
2024-04-03 17:13:55 -0700

    updates from regenerating

--- sdk/openai/Azure.AI.OpenAI/src/Generated/Completions.Serialization.cs
+++ sdk/openai/Azure.AI.OpenAI/src/Generated/Completions.Serialization.cs
@@ -93,7 +93,7 @@ namespace Azure.AI.OpenAI
             IReadOnlyList<Choice> choices = default;
             CompletionsUsage usage = default;
             IDictionary<string, BinaryData> serializedAdditionalRawData = default;
-            Dictionary<string, BinaryData> additionalPropertiesDictionary = new Dictionary<string, BinaryData>();
+            Dictionary<string, BinaryData> rawDataDictionary = new Dictionary<string, BinaryData>();
             foreach (var property in element.EnumerateObject())
             {
                 if (property.NameEquals("id"u8))
@@ -137,10 +137,10 @@ namespace Azure.AI.OpenAI
                 }
                 if (options.Format != "W")
                 {
-                    additionalPropertiesDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                    rawDataDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
                 }
             }
-            serializedAdditionalRawData = additionalPropertiesDictionary;
+            serializedAdditionalRawData = rawDataDictionary;
             return new Completions(
                 id,
                 created,
