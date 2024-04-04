686ec391af6b9420bb3a436b5b37d043132ce768
azure-sdk <azuresdk@microsoft.com>
2024-04-04 21:09:33 +0000

    Update SDK code ad_ba_0

--- sdk/attestation/Azure.Security.Attestation/src/Generated/Models/AttestOpenEnclaveRequest.Serialization.cs
+++ sdk/attestation/Azure.Security.Attestation/src/Generated/Models/AttestOpenEnclaveRequest.Serialization.cs
@@ -37,5 +37,13 @@ namespace Azure.Security.Attestation
             }
             writer.WriteEndObject();
         }
+
+        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        internal virtual RequestContent ToRequestContent()
+        {
+            var content = new Utf8JsonRequestContent();
+            content.JsonWriter.WriteObjectValue<AttestOpenEnclaveRequest>(this);
+            return content;
+        }
     }
 }
