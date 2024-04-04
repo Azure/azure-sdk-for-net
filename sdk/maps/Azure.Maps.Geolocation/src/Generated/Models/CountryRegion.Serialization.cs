893cdabefd9555a99d6b2b4e4a35be7a5fc15704
azure-sdk <azuresdk@microsoft.com>
2024-04-04 17:06:29 +0000

    Update SDK codes he_mi_3

--- sdk/maps/Azure.Maps.Geolocation/src/Generated/Models/CountryRegion.Serialization.cs
+++ sdk/maps/Azure.Maps.Geolocation/src/Generated/Models/CountryRegion.Serialization.cs
@@ -28,5 +28,13 @@ namespace Azure.Maps.Geolocation
             }
             return new CountryRegion(isoCode);
         }
+
+        /// <summary> Deserializes the model from a raw response. </summary>
+        /// <param name="response"> The response to deserialize the model from. </param>
+        internal static CountryRegion FromResponse(Response response)
+        {
+            using var document = JsonDocument.Parse(response.Content);
+            return DeserializeCountryRegion(document.RootElement);
+        }
     }
 }
