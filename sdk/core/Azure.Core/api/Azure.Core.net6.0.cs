f19fe64e12d35bc34bf90f2adcb3fd23cf4f48b7
Wei Hu <huwe@microsoft.com>
2024-04-07 11:55:33 +0800

    export API

--- sdk/core/Azure.Core/api/Azure.Core.net6.0.cs
+++ sdk/core/Azure.Core/api/Azure.Core.net6.0.cs
@@ -534,7 +534,7 @@ namespace Azure.Core
     {
         private readonly object _dummy;
         private readonly int _dummyPrimitive;
-        public string? Id { get { throw null; } }
+        public string Id { get { throw null; } }
         Azure.Core.RehydrationToken System.ClientModel.Primitives.IJsonModel<Azure.Core.RehydrationToken>.Create(ref System.Text.Json.Utf8JsonReader reader, System.ClientModel.Primitives.ModelReaderWriterOptions options) { throw null; }
         void System.ClientModel.Primitives.IJsonModel<Azure.Core.RehydrationToken>.Write(System.Text.Json.Utf8JsonWriter writer, System.ClientModel.Primitives.ModelReaderWriterOptions options) { }
         object System.ClientModel.Primitives.IJsonModel<object>.Create(ref System.Text.Json.Utf8JsonReader reader, System.ClientModel.Primitives.ModelReaderWriterOptions options) { throw null; }
