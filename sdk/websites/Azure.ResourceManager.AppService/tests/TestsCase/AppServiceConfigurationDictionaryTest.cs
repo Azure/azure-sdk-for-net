3441ad7e082c508a4352551d08345689909c5770
Wenming Liu <v-wenmingliu@microsoft.com>
2024-04-08 14:52:58 +0800

    removed some useless code

--- sdk/websites/Azure.ResourceManager.AppService/tests/TestsCase/AppServiceConfigurationDictionaryTest.cs
+++ sdk/websites/Azure.ResourceManager.AppService/tests/TestsCase/AppServiceConfigurationDictionaryTest.cs
@@ -20,49 +20,8 @@ namespace Azure.ResourceManager.AppService.Tests.TestsCase
     : base(isAsync)//, RecordedTestMode.Record)
         {
         }
-        private async Task<WebSiteCollection> GetSiteCollectionAsync()
-        {
-            var resourceGroup = await CreateResourceGroupAsync();
-            return resourceGroup.GetWebSites();
-        }
 
-        public async Task<WebSiteResource> SiteCreateOrUpdate()
-        {
-            var webSiteResources = await GetSiteCollectionAsync();
-            var name = Recording.GenerateAssetName("testSite");
-            var input = ResourceDataHelper.GetBasicSiteData(DefaultLocation);
-            var lro = await webSiteResources.CreateOrUpdateAsync(WaitUntil.Completed, name, input);
-            var site = lro.Value;
-            return site;
-        }
-        [TestCase]
-        [RecordedTest]
-        [Ignore("could note create the function trigger using the code below")]
-        public async Task FunctionTriggers()
-        {
-            var rg = await CreateResourceGroupAsync();
-
-            //create websiteresource
-            var appName = Recording.GenerateAssetName("functest-");
-            var appData = new WebSiteData(AzureLocation.EastUS)
-            {
-                ContainerSize = 1536,
-                ClientCertMode = ClientCertMode.Required,
-                Kind = "functionapp",
-                IsEnabled = true,
-                KeyVaultReferenceIdentity = "SystemAssigned"
-            };
-            var app = (await rg.GetWebSites().CreateOrUpdateAsync(WaitUntil.Completed, appName, appData)).Value;
-            var getApp = (await app.GetAsync()).Value;
-
-            var siteFuncCollection = getApp.GetSiteFunctions();
-            var str1 = """{"name":"HttpTrigger0407","entryPoint":"Wicresoft0407.Function.HttpTrigger0407.Run","scriptFile":"06AzureFunction.dll","language":"dotnet-isolated","functionDirectory":"","bindings":[{"name":"req","type":"httpTrigger","direction":"In","authLevel":"Function","methods":["get","post"]},{"name":"$return","type":"http","direction":"Out"}]}""";
-            FunctionEnvelopeData functionEnvelopeData = new FunctionEnvelopeData { Config = BinaryData.FromObjectAsJson(str1) };
-            await siteFuncCollection.CreateOrUpdateAsync(WaitUntil.Completed, $"HttpTrigger0407", functionEnvelopeData);
-            var result = await getApp.SyncFunctionTriggersAsync();
-            Assert.IsNotNull(result);
-        }
-
+        //Manual operation needed to create website/function resource, get it from existing one.
         [TestCase]
         [RecordedTest]
         public async Task WebSiteResource_Test()
@@ -76,6 +35,8 @@ namespace Azure.ResourceManager.AppService.Tests.TestsCase
             Assert.NotZero(appDic.Properties.Count);
             Assert.NotNull(metaData.Properties);
         }
+
+        //Manual operation needed to create website/function resource, get it from existing one.
         [TestCase]
         [RecordedTest]
         public async Task WebSiteSlotResource_Test()
@@ -85,6 +46,8 @@ namespace Azure.ResourceManager.AppService.Tests.TestsCase
             AppServiceConfigurationDictionary appset = await wsSlotResource.GetApplicationSettingsSlotAsync();
             Assert.GreaterOrEqual(appset.Properties.Count, 1);
         }
+
+        //Manual operation needed to create website/function resource, get it from existing one.
         [TestCase]
         [RecordedTest]
         public async Task SiteFunctionResource_Test()
@@ -97,6 +60,8 @@ namespace Azure.ResourceManager.AppService.Tests.TestsCase
             IReadOnlyDictionary<string, string> funtionKeys = (await siteFuncResource.GetFunctionKeysAsDictionaryAsync()).Value;
             Assert.GreaterOrEqual(funtionKeys.Count, 1);
         }
+
+        //Manual operation needed to create website/function resource, get it from existing one.
         [TestCase]
         [RecordedTest]
         public async Task SiteSlotFunctionResource_Test()
@@ -107,6 +72,8 @@ namespace Azure.ResourceManager.AppService.Tests.TestsCase
             IReadOnlyDictionary<string, string> funtionKeys = (await slot.GetFunctionKeysSlotAsDictionaryAsync()).Value;
             Assert.NotZero(funtionKeys.Count);
         }
+
+        //Manual operation needed to create website/function resource, get it from existing one.
         [TestCase]
         [RecordedTest]
         public async Task StaticSiteResource_Test()
