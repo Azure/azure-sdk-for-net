4589b07ca77de410c9440ff187bdf41e15331d57
jolov <jolov@microsoft.com>
2024-04-09 08:19:04 -0700

    throw if there are no resources

--- sdk/provisioning/Azure.Provisioning/CHANGELOG.md
+++ sdk/provisioning/Azure.Provisioning/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release History
 
-## 0.2.0-beta.2 (2024-04-08)
+## 0.2.0-beta.2 (2024-04-09)
 
 ### Features Added
 
--- sdk/provisioning/Azure.Provisioning/src/ModuleInfrastructure.cs
+++ sdk/provisioning/Azure.Provisioning/src/ModuleInfrastructure.cs
@@ -30,14 +30,15 @@ namespace Azure.Provisioning
             outputPath ??= $".\\{GetType().Name}";
             outputPath = Path.GetFullPath(outputPath);
 
-            WriteBicepFile(_rootConstruct, outputPath);
             if (_rootConstruct == null)
             {
-                return;
+                throw new InvalidOperationException("No resources were added to the Infrastructure. Add resources to the Infrastructure before calling Build.");
             }
 
+            WriteBicepFile(_rootConstruct, outputPath);
+
             var queue = new Queue<ModuleConstruct>();
-            queue.Enqueue(_rootConstruct!);
+            queue.Enqueue(_rootConstruct);
             WriteConstructsByLevel(queue, outputPath);
         }
 
@@ -201,14 +202,10 @@ namespace Azure.Provisioning
             return fileName;
         }
 
-        private void WriteBicepFile(ModuleConstruct? construct, string outputPath)
+        private void WriteBicepFile(ModuleConstruct construct, string outputPath)
         {
             using var stream = new FileStream(GetFilePath(construct, outputPath), FileMode.Create);
-            // just create an empty file if there is no construct
-            if (construct == null)
-            {
-                return;
-            }
+
 #if NET6_0_OR_GREATER
             stream.Write(construct.SerializeModule());
 #else
