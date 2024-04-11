339ab65026cdcb84c6a39f5dad8f1bd504d5a4f1
azure-sdk <azuresdk@microsoft.com>
2024-04-11 03:40:00 +0000

    Update SDK code ke_mg_5

--- sdk/maps/Azure.Maps.Search/src/Generated/Models/GeoJsonMultiPoint.Serialization.cs
+++ sdk/maps/Azure.Maps.Search/src/Generated/Models/GeoJsonMultiPoint.Serialization.cs
@@ -88,7 +88,7 @@ namespace Azure.Maps.Search.Models
             return DeserializeGeoJsonMultiPoint(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal override RequestContent ToRequestContent()
         {
             var content = new Common.Utf8JsonRequestContent();
--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/SeriesIdentity.Serialization.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/SeriesIdentity.Serialization.cs
@@ -58,7 +58,7 @@ namespace Azure.AI.MetricsAdvisor.Models
             return DeserializeSeriesIdentity(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
