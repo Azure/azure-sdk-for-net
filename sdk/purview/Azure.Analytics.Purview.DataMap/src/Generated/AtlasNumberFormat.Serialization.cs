03f72666ac44ca30cb9f203fa71961570d385354
azure-sdk <azuresdk@microsoft.com>
2024-04-11 03:41:12 +0000

    Update SDK code pr_re_7

--- sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AtlasNumberFormat.Serialization.cs
+++ sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AtlasNumberFormat.Serialization.cs
@@ -347,7 +347,7 @@ namespace Azure.Analytics.Purview.DataMap
             return DeserializeAtlasNumberFormat(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
--- sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AtlasTypeDefHeader.Serialization.cs
+++ sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AtlasTypeDefHeader.Serialization.cs
@@ -153,7 +153,7 @@ namespace Azure.Analytics.Purview.DataMap
             return DeserializeAtlasTypeDefHeader(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
