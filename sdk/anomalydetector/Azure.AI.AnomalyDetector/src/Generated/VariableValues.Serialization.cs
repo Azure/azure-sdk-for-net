1576bedc00bb14ed4093828e880e3a13461a856c
azure-sdk <azuresdk@microsoft.com>
2024-04-12 09:58:18 +0000

    Update SDK code ad_ba_0

--- sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/VariableValues.Serialization.cs
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/VariableValues.Serialization.cs
@@ -160,12 +160,11 @@ namespace Azure.AI.AnomalyDetector
             return DeserializeVariableValues(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
-            var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(this, new ModelReaderWriterOptions("W"));
-            return content;
+            BinaryData binaryData = ModelReaderWriter.Write(this, new ModelReaderWriterOptions("W"));
+            return RequestContent.Create(binaryData);
         }
     }
 }
