e836f4f828f7310c4c91667ee3145a7565f7680a
azure-sdk <azuresdk@microsoft.com>
2024-04-12 04:02:47 +0000

    Update SDK code ke_mg_5

--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/NotificationHook.Serialization.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/NotificationHook.Serialization.cs
@@ -50,7 +50,7 @@ namespace Azure.AI.MetricsAdvisor.Administration
             return DeserializeNotificationHook(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
--- sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/SmartDetectionCondition.Serialization.cs
+++ sdk/metricsadvisor/Azure.AI.MetricsAdvisor/src/Generated/Models/SmartDetectionCondition.Serialization.cs
@@ -62,7 +62,7 @@ namespace Azure.AI.MetricsAdvisor.Models
             return DeserializeSmartDetectionCondition(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
