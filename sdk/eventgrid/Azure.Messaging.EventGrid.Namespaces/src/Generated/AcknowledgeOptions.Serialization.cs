dbb9e25e220962298bd5866a8d24b339d38b1787
azure-sdk <azuresdk@microsoft.com>
2024-04-12 10:00:32 +0000

    Update SDK code da_ev_3

--- sdk/eventgrid/Azure.Messaging.EventGrid.Namespaces/src/Generated/AcknowledgeOptions.Serialization.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid.Namespaces/src/Generated/AcknowledgeOptions.Serialization.cs
@@ -134,12 +134,11 @@ namespace Azure.Messaging.EventGrid.Namespaces
             return DeserializeAcknowledgeOptions(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
-            var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(this, new ModelReaderWriterOptions("W"));
-            return content;
+            BinaryData binaryData = ModelReaderWriter.Write(this, new ModelReaderWriterOptions("W"));
+            return RequestContent.Create(binaryData);
         }
     }
 }
