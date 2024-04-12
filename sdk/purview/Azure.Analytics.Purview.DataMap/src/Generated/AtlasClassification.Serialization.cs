af1f894daa6448aa00a39f1ddc27bb527fbc4a22
azure-sdk <azuresdk@microsoft.com>
2024-04-12 10:04:04 +0000

    Update SDK code pr_re_7

--- sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AtlasClassification.Serialization.cs
+++ sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AtlasClassification.Serialization.cs
@@ -257,12 +257,11 @@ namespace Azure.Analytics.Purview.DataMap
             return DeserializeAtlasClassification(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
-            var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(this, new ModelReaderWriterOptions("W"));
-            return content;
+            BinaryData binaryData = ModelReaderWriter.Write(this, new ModelReaderWriterOptions("W"));
+            return RequestContent.Create(binaryData);
         }
     }
 }
--- sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/SearchResultValue.Serialization.cs
+++ sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/SearchResultValue.Serialization.cs
@@ -481,12 +481,11 @@ namespace Azure.Analytics.Purview.DataMap
             return DeserializeSearchResultValue(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
-            var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(this, new ModelReaderWriterOptions("W"));
-            return content;
+            BinaryData binaryData = ModelReaderWriter.Write(this, new ModelReaderWriterOptions("W"));
+            return RequestContent.Create(binaryData);
         }
     }
 }
