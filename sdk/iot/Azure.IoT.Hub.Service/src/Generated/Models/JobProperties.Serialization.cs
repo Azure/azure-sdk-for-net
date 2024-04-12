1fc4fea847d96ee219881451da4e810751b09dd5
azure-sdk <azuresdk@microsoft.com>
2024-04-12 10:01:15 +0000

    Update SDK code ex_ke_4

--- sdk/iot/Azure.IoT.Hub.Service/src/Generated/Models/JobProperties.Serialization.cs
+++ sdk/iot/Azure.IoT.Hub.Service/src/Generated/Models/JobProperties.Serialization.cs
@@ -6,6 +6,7 @@
 #nullable disable
 
 using System;
+using System.ClientModel.Primitives;
 using System.Text.Json;
 using Azure.Core;
 
@@ -251,12 +252,11 @@ namespace Azure.IoT.Hub.Service.Models
             return DeserializeJobProperties(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
-            var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(this);
-            return content;
+            BinaryData binaryData = ModelReaderWriter.Write(this, new ModelReaderWriterOptions("W"));
+            return RequestContent.Create(binaryData);
         }
     }
 }
