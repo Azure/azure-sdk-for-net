5525a56219bb3758b1dca4586d95128bfb25adee
azure-sdk <azuresdk@microsoft.com>
2024-04-12 09:11:23 +0000

    Update SDK codes pu_sq_5

--- sdk/search/Azure.Search.Documents/src/Generated/Models/KeyPhraseExtractionSkill.Serialization.cs
+++ sdk/search/Azure.Search.Documents/src/Generated/Models/KeyPhraseExtractionSkill.Serialization.cs
@@ -194,7 +194,7 @@ namespace Azure.Search.Documents.Indexes.Models
             return DeserializeKeyPhraseExtractionSkill(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal override RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
