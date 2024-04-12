4f58f0bbe2a00f5a92e382321ebad2cf608a2cce
azure-sdk <azuresdk@microsoft.com>
2024-04-12 09:18:59 +0000

    Update SDK codes ad_co_0

--- sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/PhoneNumberIdentifierModel.Serialization.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/PhoneNumberIdentifierModel.Serialization.cs
@@ -47,7 +47,7 @@ namespace Azure.Communication
             return DeserializePhoneNumberIdentifierModel(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new CallAutomation.Utf8JsonRequestContent();
--- sdk/communication/Azure.Communication.CallingServer/src/Generated/Models/TransferToParticipantRequestInternal.Serialization.cs
+++ sdk/communication/Azure.Communication.CallingServer/src/Generated/Models/TransferToParticipantRequestInternal.Serialization.cs
@@ -35,7 +35,7 @@ namespace Azure.Communication.CallingServer
             writer.WriteEndObject();
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
