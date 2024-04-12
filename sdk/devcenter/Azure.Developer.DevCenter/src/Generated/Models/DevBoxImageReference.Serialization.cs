798dabda3105de1df396a00e458f71cf7c79e927
azure-sdk <azuresdk@microsoft.com>
2024-04-12 04:08:49 +0000

    Update SDK code da_ev_3

--- sdk/devcenter/Azure.Developer.DevCenter/src/Generated/Models/DevBoxImageReference.Serialization.cs
+++ sdk/devcenter/Azure.Developer.DevCenter/src/Generated/Models/DevBoxImageReference.Serialization.cs
@@ -181,7 +181,7 @@ namespace Azure.Developer.DevCenter.Models
             return DeserializeDevBoxImageReference(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
--- sdk/documentintelligence/Azure.AI.DocumentIntelligence/src/Generated/DocumentModelCopyToOperationDetails.Serialization.cs
+++ sdk/documentintelligence/Azure.AI.DocumentIntelligence/src/Generated/DocumentModelCopyToOperationDetails.Serialization.cs
@@ -258,7 +258,7 @@ namespace Azure.AI.DocumentIntelligence
             return DeserializeDocumentModelCopyToOperationDetails(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal override RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
