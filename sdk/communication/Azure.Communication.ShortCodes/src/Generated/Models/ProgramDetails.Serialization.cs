9ef8106e81cd90353608f8079b1d4a376e33dddf
azure-sdk <azuresdk@microsoft.com>
2024-04-12 11:05:27 +0000

    Update SDK codes co_de_1

--- sdk/communication/Azure.Communication.ShortCodes/src/Generated/Models/ProgramDetails.Serialization.cs
+++ sdk/communication/Azure.Communication.ShortCodes/src/Generated/Models/ProgramDetails.Serialization.cs
@@ -244,7 +244,7 @@ namespace Azure.Communication.ShortCodes.Models
             return DeserializeProgramDetails(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
