b87ade106c39a0bc280344ab52414cd6c75cb1e0
azure-sdk <azuresdk@microsoft.com>
2024-04-12 04:13:50 +0000

    Update SDK code mg_pr_6

--- sdk/openai/Azure.AI.OpenAI.Assistants/src/Generated/UpdateThreadRequest.Serialization.cs
+++ sdk/openai/Azure.AI.OpenAI.Assistants/src/Generated/UpdateThreadRequest.Serialization.cs
@@ -149,7 +149,7 @@ namespace Azure.AI.OpenAI.Assistants
             return DeserializeUpdateThreadRequest(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
--- sdk/openai/Azure.AI.OpenAI/src/Generated/AzureChatExtensionsMessageContext.Serialization.cs
+++ sdk/openai/Azure.AI.OpenAI/src/Generated/AzureChatExtensionsMessageContext.Serialization.cs
@@ -152,7 +152,7 @@ namespace Azure.AI.OpenAI
             return DeserializeAzureChatExtensionsMessageContext(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
