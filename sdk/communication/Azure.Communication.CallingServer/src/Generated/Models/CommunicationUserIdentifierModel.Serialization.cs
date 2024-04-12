575345b107f6f48ce5137cbc706b1b487e682e74
azure-sdk <azuresdk@microsoft.com>
2024-04-12 09:51:47 +0000

    Update SDK code bi_co_1

--- sdk/communication/Azure.Communication.CallingServer/src/Generated/Models/CommunicationUserIdentifierModel.Serialization.cs
+++ sdk/communication/Azure.Communication.CallingServer/src/Generated/Models/CommunicationUserIdentifierModel.Serialization.cs
@@ -5,8 +5,9 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
 using System.Text.Json;
-using Azure.Communication.CallingServer;
 using Azure.Core;
 
 namespace Azure.Communication
@@ -47,12 +48,11 @@ namespace Azure.Communication
             return DeserializeCommunicationUserIdentifierModel(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
-            var content = new CallingServer.Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(this);
-            return content;
+            BinaryData binaryData = ModelReaderWriter.Write(this, new ModelReaderWriterOptions("W"));
+            return RequestContent.Create(binaryData);
         }
     }
 }
--- sdk/communication/Azure.Communication.MediaComposition/src/Generated/Models/MediaComposition.Serialization.cs
+++ sdk/communication/Azure.Communication.MediaComposition/src/Generated/Models/MediaComposition.Serialization.cs
@@ -5,6 +5,8 @@
 
 #nullable disable
 
+using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
 using Azure.Communication.MediaComposition.Models;
@@ -133,12 +135,11 @@ namespace Azure.Communication.MediaComposition
             return DeserializeMediaComposition(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
-            var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(this);
-            return content;
+            BinaryData binaryData = ModelReaderWriter.Write(this, new ModelReaderWriterOptions("W"));
+            return RequestContent.Create(binaryData);
         }
     }
 }
