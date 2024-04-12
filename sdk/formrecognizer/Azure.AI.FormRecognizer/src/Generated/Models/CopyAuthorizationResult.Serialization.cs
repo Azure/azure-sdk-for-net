594f6a766dc85bebb60ab9046edef97967c40ef1
azure-sdk <azuresdk@microsoft.com>
2024-04-12 09:14:53 +0000

    Update SDK codes de_he_2

--- sdk/formrecognizer/Azure.AI.FormRecognizer/src/Generated/Models/CopyAuthorizationResult.Serialization.cs
+++ sdk/formrecognizer/Azure.AI.FormRecognizer/src/Generated/Models/CopyAuthorizationResult.Serialization.cs
@@ -62,7 +62,7 @@ namespace Azure.AI.FormRecognizer.Models
             return DeserializeCopyAuthorizationResult(document.RootElement);
         }
 
-        /// <summary> Convert into a Utf8JsonRequestContent. </summary>
+        /// <summary> Convert into a <see cref="RequestContent"/>. </summary>
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
