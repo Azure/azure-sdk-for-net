9d33b78abc620bf765e1970548610a160701ab03
mcgallan <1186003288@qq.com>
2024-04-16 16:35:06 +0800

    Update AppServicePlanCollectionTests.cs

--- sdk/websites/Azure.ResourceManager.AppService/tests/TestsCase/AppServicePlanCollectionTests.cs
+++ sdk/websites/Azure.ResourceManager.AppService/tests/TestsCase/AppServicePlanCollectionTests.cs
@@ -13,7 +13,7 @@ namespace Azure.ResourceManager.AppService.Tests.TestsCase
     public class AppServicePlanCollectionTests : AppServiceTestBase
     {
         public AppServicePlanCollectionTests(bool isAsync)
-           : base(isAsync)
+           : base(isAsync, RecordedTestMode.Record)
         {
         }
 
