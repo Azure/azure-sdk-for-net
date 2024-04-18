fbf81798724ed0a56db523bf0134c3f96352377b
Timothy Mothra Lee <tilee@microsoft.com>
2024-04-18 11:41:38 -0700

    fix another shared file

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/ConnectionString/ConnectionStringParser.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Internals/ConnectionString/ConnectionStringParser.cs
@@ -6,6 +6,8 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 #if AZURE_MONITOR_EXPORTER
 using Azure.Monitor.OpenTelemetry.Exporter.Internals.Diagnostics;
+#elif LIVE_METRICS_EXPORTER
+using Azure.Monitor.OpenTelemetry.LiveMetrics.Internals.Diagnostics;
 #elif ASP_NET_CORE_DISTRO
 using Azure.Monitor.OpenTelemetry.AspNetCore.Internals.LiveMetrics.Diagnostics;
 #endif
@@ -52,7 +54,7 @@ namespace Azure.Monitor.OpenTelemetry.Exporter.Internals.ConnectionString
             {
 #if AZURE_MONITOR_EXPORTER
                 AzureMonitorExporterEventSource.Log.FailedToParseConnectionString(ex);
-#elif ASP_NET_CORE_DISTRO
+#elif (LIVE_METRICS_EXPORTER || ASP_NET_CORE_DISTRO)
                 LiveMetricsExporterEventSource.Log.FailedToParseConnectionString(ex);
 #endif
                 throw new InvalidOperationException("Connection String Error: " + ex.Message, ex);
