cfa67af4f120331f7e75cd5d16823a9f070021b7
Rameez Shaikh <rameezmicrosoft@gmail.com>
2024-04-22 13:23:22 +0530

    changes for Laterality

--- sdk/healthinsights/Azure.Health.Insights.RadiologyInsights/tests/samples/Sample01_LateralityDiscrepancySample.cs
+++ sdk/healthinsights/Azure.Health.Insights.RadiologyInsights/tests/samples/Sample01_LateralityDiscrepancySample.cs
@@ -34,14 +34,15 @@ namespace Azure.Health.Insights.RadiologyInsights.Tests
             RadiologyInsightsClient client = new RadiologyInsightsClient(endpointUri, credential);
             #endregion
 
-            RadiologyInsightsData radiologyInsightsData = GetRadiologyInsightsData();
+            RadiologyInsightsJob radiologyInsightsjob = GetRadiologyInsightsJob();
 
             #region Snippet:Laterality_Discrepancy_Sync_Tests_Samples_synccall
-            Operation<RadiologyInsightsInferenceResult> operation = client.InferRadiologyInsights(WaitUntil.Completed, radiologyInsightsData);
+            var jobId = "job" + DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            Operation<RadiologyInsightsInferenceResult> operation = client.InferRadiologyInsights(WaitUntil.Completed, jobId, radiologyInsightsjob);
             #endregion
 
             RadiologyInsightsInferenceResult responseData = operation.Value;
-            IReadOnlyList<RadiologyInsightsInference> inferences = responseData.PatientResults[0].Inferences;
+            IList<RadiologyInsightsInference> inferences = responseData.PatientResults[0].Inferences;
 
             foreach (RadiologyInsightsInference inference in inferences)
             {
@@ -59,6 +60,13 @@ namespace Azure.Health.Insights.RadiologyInsights.Tests
                     #endregion
                 }
             }
+        }
+
+        private static RadiologyInsightsJob GetRadiologyInsightsJob()
+        {
+            RadiologyInsightsJob radiologyInsightsJob = new RadiologyInsightsJob();
+            radiologyInsightsJob.JobData = GetRadiologyInsightsData();
+            return radiologyInsightsJob;
         }
 
         private static RadiologyInsightsData GetRadiologyInsightsData()
@@ -113,7 +121,7 @@ namespace Azure.Health.Insights.RadiologyInsights.Tests
                 BirthDate = new System.DateTime(1959, 11, 11),
                 Sex = PatientSex.Female,
             };
-            Encounter encounter = new("encounterid1")
+            PatientEncounter encounter = new("encounterid1")
             {
                 Class = EncounterClass.InPatient,
                 Period = new TimePeriod
@@ -122,16 +130,16 @@ namespace Azure.Health.Insights.RadiologyInsights.Tests
                     End = new System.DateTime(2021, 08, 28)
                 }
             };
-            List<Encounter> encounterList = new() { encounter };
+            List<PatientEncounter> encounterList = new() { encounter };
             DocumentContent documentContent = new(DocumentContentSourceType.Inline, DOC_CONTENT);
             PatientDocument patientDocument = new(DocumentType.Note, "doc2", documentContent)
             {
                 ClinicalType = ClinicalDocumentType.RadiologyReport,
-                CreatedDateTime = new System.DateTime(2021, 08, 28),
+                CreatedAt = new System.DateTime(2021, 08, 28),
                 AdministrativeMetadata = CreateDocumentAdministrativeMetadata()
             };
             PatientRecord patientRecord = new(id);
-            patientRecord.Info = patientInfo;
+            patientRecord.Details = patientInfo;
             patientRecord.Encounters.Add(encounter);
             patientRecord.PatientDocuments.Add(patientDocument);
             #endregion
@@ -153,7 +161,7 @@ namespace Azure.Health.Insights.RadiologyInsights.Tests
             FhirR4CodeableConcept codeableConcept = new();
             codeableConcept.Coding.Add(coding);
 
-            FhirR4Extendible orderedProcedure = new()
+            OrderedProcedure orderedProcedure = new()
             {
                 Description = "US BREAST - LEFT LIMITED",
                 Code = codeableConcept
