a99644d75c3e65980f0cffc28091604390b1e9a0
azure-sdk <azuresdk@microsoft.com>
2024-04-24 16:32:47 +0000

    Update SDK code da_ev_3

--- sdk/eventhub/Azure.ResourceManager.EventHubs/src/Generated/Models/EventHubsApplicationGroupPolicy.Serialization.cs
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/src/Generated/Models/EventHubsApplicationGroupPolicy.Serialization.cs
@@ -92,15 +92,16 @@ namespace Azure.ResourceManager.EventHubs.Models
             builder.AppendLine("{");
 
             hasPropertyOverride = hasObjectOverride && propertyOverrides.TryGetValue(nameof(Name), out propertyOverride);
-            if (Optional.IsDefined(Name) || hasPropertyOverride)
+            if (hasPropertyOverride)
             {
                 builder.Append("  name: ");
-                if (hasPropertyOverride)
+                builder.AppendLine(propertyOverride);
+            }
+            else
+            {
+                if (Optional.IsDefined(Name))
                 {
-                    builder.AppendLine($"{propertyOverride}");
-                }
-                else
-                {
+                    builder.Append("  name: ");
                     if (Name.Contains(Environment.NewLine))
                     {
                         builder.AppendLine("'''");
@@ -114,13 +115,14 @@ namespace Azure.ResourceManager.EventHubs.Models
             }
 
             hasPropertyOverride = hasObjectOverride && propertyOverrides.TryGetValue(nameof(ApplicationGroupPolicyType), out propertyOverride);
-            builder.Append("  type: ");
             if (hasPropertyOverride)
             {
-                builder.AppendLine($"{propertyOverride}");
+                builder.Append("  type: ");
+                builder.AppendLine(propertyOverride);
             }
             else
             {
+                builder.Append("  type: ");
                 builder.AppendLine($"'{ApplicationGroupPolicyType.ToString()}'");
             }
 
