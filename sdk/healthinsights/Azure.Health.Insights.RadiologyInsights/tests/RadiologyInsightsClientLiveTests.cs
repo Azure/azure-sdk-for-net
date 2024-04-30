531c3facb4de96a6b0e105fd560c509dcdd83824
Rameez Shaikh <rameezshaikh@microsoft.com>
2024-04-30 20:49:30 +0530

    removed space

--- sdk/healthinsights/Azure.Health.Insights.RadiologyInsights/tests/RadiologyInsightsClientLiveTests.cs
+++ sdk/healthinsights/Azure.Health.Insights.RadiologyInsights/tests/RadiologyInsightsClientLiveTests.cs
@@ -40,7 +40,6 @@ namespace Azure.Health.Insights.RadiologyInsights.Tests
             var request = GetRequestContent("RadiologyInsightsClientTest.request.json");
             var jobId = "job1714464011094";
             var operation = await client.InferRadiologyInsightsAsync(WaitUntil.Completed, jobId, request);
-
             Assert.IsNotNull(operation);
             Response response = operation.GetRawResponse();
             Assert.IsNotNull(response);
