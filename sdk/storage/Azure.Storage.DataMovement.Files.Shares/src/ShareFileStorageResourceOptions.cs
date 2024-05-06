8eddfe9250a8694669ae8921e5ad6c8272adbf38
Amanda Nguyen <48961492+amnguye@users.noreply.github.com>
2024-05-06 15:17:49 -0700

    [Storage] [DataMovement] Preserve Share Files Properties and Attributes (#43799)
    * WIP - api change on files options bag
    
    * WIP - Updated checkpointer to contain new preserved properties and attr
    
    * WIP - Change permission key to non-preserve property
    
    * WIP - fixed current tests to record uploading with preserve last written time
    
    * WIP - e2e sharefile properties tests
    
    * Record new e2e share file start transfer properties tests
    
    * Export API + some cleanup
    
    * Revert unnecessary test changes
    
    * PR comments - Move some share constants to dm constants; Share File Create properties objecT
    
    * Moved NtfsFileAttributes extension to internal shared file, and included to DM share package
    
    * Added test for incorrect schema version
    
    * Removed DirectoryMetdata

--- sdk/storage/Azure.Storage.DataMovement.Files.Shares/src/ShareFileStorageResourceOptions.cs
+++ sdk/storage/Azure.Storage.DataMovement.Files.Shares/src/ShareFileStorageResourceOptions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Azure.Storage.Files.Shares.Models;
 using Metadata = System.Collections.Generic.IDictionary<string, string>;
 
@@ -12,24 +13,11 @@ namespace Azure.Storage.DataMovement.Files.Shares
     public class ShareFileStorageResourceOptions
     {
         /// <summary>
-        /// Optional SMB properties to set for the directory and/or file resource.
-        /// </summary>
-        public FileSmbProperties SmbProperties { get; set; }
-
-        /// <summary>
         /// Optional. The file permission to set on the destination directory and/or file.
         ///
         /// Applies to copy and upload transfers.
-        /// TODO: determine whether this is something we want to apply and override
-        /// the potential default of copying over permissions when we go to do
-        /// file share to file share copy transfer.
         /// </summary>
         public string FilePermissions { get; set; }
-
-        /// <summary>
-        /// Optional SMB properties to set for the directory and/or file.
-        /// </summary>
-        public ShareFileHttpHeaders HttpHeaders { get; set; }
 
         /// <summary>
         /// Optional. See <see cref="ShareFileRequestConditions"/>.
@@ -48,21 +36,114 @@ namespace Azure.Storage.DataMovement.Files.Shares
         public ShareFileRequestConditions DestinationConditions { get; set; }
 
         /// <summary>
-        /// Optional. Defines custom metadata to set on the destination resource.
+        /// Optional. Sets the Cache Control header which
+        /// specifies directives for caching mechanisms.
+        ///
+        /// By default preserves the Cache Control from the source.
         ///
         /// Applies to upload and copy transfers.
         /// </summary>
+        public DataTransferProperty<string> CacheControl { get; set; }
+
+        /// <summary>
+        /// Optional. Sets the Content Disposition header which
+        /// conveys additional information about how to process the response
+        /// payload, and also can be used to attach additional metadata.  For
+        /// example, if set to attachment, it indicates that the user-agent
+        /// should not display the response, but instead show a Save As dialog
+        /// with a filename other than the blob name specified.
+        ///
+        /// By default preserves the Content Disposition from the source.
+        ///
+        /// Applies to upload and copy transfers.
+        /// </summary>
+        public DataTransferProperty<string> ContentDisposition { get; set; }
+
+        /// <summary>
+        /// Optional. Sets the Content Encoding header which
+        /// specifies which content encodings have been applied to the blob.
+        /// This value is returned to the client when the Get Blob operation
+        /// is performed on the blob resource. The client can use this value
+        /// when returned to decode the blob content.
+        ///
+        /// By default preserves the Content Encoding from the source.
+        ///
+        /// Applies to upload and copy transfers.
+        /// </summary>
+        public DataTransferProperty<string[]> ContentEncoding { get; set; }
+
+        /// <summary>
+        /// Optional. Sets the Content Language header which
+        /// specifies the natural languages used by this resource.
+        ///
+        /// By default preserves the Content Language from the source.
+        ///
+        /// Applies to upload and copy transfers.
+        /// </summary>
+        public DataTransferProperty<string[]> ContentLanguage { get; set; }
+
+        /// <summary>
+        /// Optional. Sets the Content Type header which
+        /// specifies the MIME content type of the blob.
+        ///
+        /// By default preserves the Content Type from the source.
+        ///
+        /// Applies to upload and copy transfers.
+        /// </summary>
+        public DataTransferProperty<string> ContentType { get; set; }
+
+        /// <summary>
+        /// The file system attributes for this file.
+        ///
+        /// By default preserves the File Attributes from the source.
+        /// </summary>
+        public DataTransferProperty<NtfsFileAttributes?> FileAttributes { get; set; }
+
+        /// <summary>
+        /// The key of the file permission.
+        /// </summary>
+        public string FilePermissionKey { get; set; }
+
+        /// <summary>
+        /// The creation time of the file.
+        ///
+        /// By default preserves the File Created On Time from the source.
+        /// </summary>
+        public DataTransferProperty<DateTimeOffset?> FileCreatedOn { get; set; }
+
+        /// <summary>
+        /// The last write time of the file.
+        ///
+        /// By default preserves the File Last Written On Time from the source.
+        /// </summary>
+        public DataTransferProperty<DateTimeOffset?> FileLastWrittenOn { get; set; }
+
+        /// <summary>
+        /// The change time of the file.
+        ///
+        /// By default preserves the File Changed On Time from the source.
+        /// </summary>
+        public DataTransferProperty<DateTimeOffset?> FileChangedOn { get; set; }
+
+        /// <summary>
+        /// Optional. Defines custom metadata to set on the destination resource.
+        ///
+        /// Applies to upload and copy transfers.
+        ///
+        /// Preserves Metadata from the source by default.
+        /// </summary>
 #pragma warning disable CA2227 // Collection properties should be readonly
-        public Metadata DirectoryMetadata { get; set; }
+        public DataTransferProperty<Metadata> DirectoryMetadata { get; set; }
 #pragma warning restore CA2227 // Collection properties should be readonly
 
         /// <summary>
         /// Optional. Defines custom metadata to set on the destination resource.
         ///
         /// Applies to upload and copy transfers.
+        ///
+        /// Preserves Metdata from the source by default.
         /// </summary>
 #pragma warning disable CA2227 // Collection properties should be readonly
-        public Metadata FileMetadata { get; set; }
-#pragma warning restore CA2227 // Collection properties should be readonly
+        public DataTransferProperty<Metadata> FileMetadata { get; set; }
     }
 }
