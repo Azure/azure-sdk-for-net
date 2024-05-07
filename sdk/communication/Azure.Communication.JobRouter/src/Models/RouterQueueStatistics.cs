66ee54f5fad60d2d3922180b00a0c1ea2da4b307
nisha-bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2024-05-07 15:05:25 -0700

    wip

--- sdk/communication/Azure.Communication.JobRouter/src/Models/RouterQueueStatistics.cs
+++ sdk/communication/Azure.Communication.JobRouter/src/Models/RouterQueueStatistics.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
