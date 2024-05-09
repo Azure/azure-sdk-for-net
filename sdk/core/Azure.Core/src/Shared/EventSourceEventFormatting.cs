1add85a430ede1ca7d55cfb877a238654c112bb3
Paul Harrington <pharring@microsoft.com>
2024-05-08 20:31:04 -0700

    Use a rented StringBuilder in EventSourceEventFormatting

--- sdk/core/Azure.Core/src/Shared/EventSourceEventFormatting.cs
+++ sdk/core/Azure.Core/src/Shared/EventSourceEventFormatting.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.Core.Diagnostics;
 using System;
+using System.Buffers;
 using System.Diagnostics.Tracing;
 using System.Globalization;
 using System.Linq;
@@ -10,70 +10,116 @@ using System.Text;
 
 #nullable enable
 
-namespace Azure.Core.Shared
+namespace Azure.Core.Shared;
+
+internal static class EventSourceEventFormatting
 {
-    internal static class EventSourceEventFormatting
+    [ThreadStatic]
+    private static StringBuilder? s_cachedStringBuilder;
+    private const int CachedStringBuilderCapacity = 512;
+
+    public static string Format(EventWrittenEventArgs eventData)
     {
-        public static string Format(EventWrittenEventArgs eventData)
+        var payloadArray = eventData.Payload?.ToArray() ?? Array.Empty<object?>();
+
+        ProcessPayloadArray(payloadArray);
+
+        if (eventData.Message != null)
         {
-            var payloadArray = eventData.Payload?.ToArray() ?? Array.Empty<object?>();
-
-            ProcessPayloadArray(payloadArray);
-
-            if (eventData.Message != null)
+            try
             {
-                try
-                {
-                    return string.Format(CultureInfo.InvariantCulture, eventData.Message, payloadArray);
-                }
-                catch (FormatException)
-                {
-                }
+                return string.Format(CultureInfo.InvariantCulture, eventData.Message, payloadArray);
             }
+            catch (FormatException)
+            {
+            }
+        }
 
-            var stringBuilder = new StringBuilder();
-            stringBuilder.Append(eventData.EventName);
+        StringBuilder stringBuilder = RentStringBuilder();
+        stringBuilder.Append(eventData.EventName);
 
-            if (!string.IsNullOrWhiteSpace(eventData.Message))
+        if (!string.IsNullOrWhiteSpace(eventData.Message))
+        {
+            stringBuilder.AppendLine();
+            stringBuilder.Append(nameof(eventData.Message)).Append(" = ").Append(eventData.Message);
+        }
+
+        if (eventData.PayloadNames != null)
+        {
+            for (int i = 0; i < eventData.PayloadNames.Count; i++)
             {
                 stringBuilder.AppendLine();
-                stringBuilder.Append(nameof(eventData.Message)).Append(" = ").Append(eventData.Message);
+                stringBuilder.Append(eventData.PayloadNames[i]).Append(" = ").Append(payloadArray[i]);
+            }
+        }
+
+        return ToStringAndReturnStringBuilder(stringBuilder);
+    }
+
+    private static void ProcessPayloadArray(object?[] payloadArray)
+    {
+        for (int i = 0; i < payloadArray.Length; i++)
+        {
+            payloadArray[i] = FormatValue(payloadArray[i]);
+        }
+    }
+
+    private static object? FormatValue(object? o)
+    {
+        if (o is byte[] bytes)
+        {
+#if NET6_0_OR_GREATER
+            return Convert.ToHexString(bytes);
+#else
+            int length = 2 * bytes.Length; // Two hex characters per byte
+            if (length < 256)
+            {
+                static char ToHex(int value) => (char)(value < 10 ? value + '0' : value - 10 + 'A');
+
+                Span<char> buffer = stackalloc char[length];
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    byte b = bytes[i];
+                    buffer[i * 2] = ToHex(b >> 4);
+                    buffer[i * 2 + 1] = ToHex(b & 0xF);
+                }
+
+                return buffer.ToString();
             }
 
-            if (eventData.PayloadNames != null)
+            var stringBuilder = new StringBuilder(length);
+            foreach (byte b in bytes)
             {
-                for (int i = 0; i < eventData.PayloadNames.Count; i++)
-                {
-                    stringBuilder.AppendLine();
-                    stringBuilder.Append(eventData.PayloadNames[i]).Append(" = ").Append(payloadArray[i]);
-                }
+                stringBuilder.AppendFormat(CultureInfo.InvariantCulture, "{0:X2}", b);
             }
 
             return stringBuilder.ToString();
+#endif
         }
 
-        private static void ProcessPayloadArray(object?[] payloadArray)
+        return o;
+    }
+
+    private static StringBuilder RentStringBuilder()
+    {
+        StringBuilder? builder = s_cachedStringBuilder;
+        if (builder is null)
         {
-            for (int i = 0; i < payloadArray.Length; i++)
-            {
-                payloadArray[i] = FormatValue(payloadArray[i]);
-            }
+            return new StringBuilder(CachedStringBuilderCapacity);
         }
 
-        private static object? FormatValue(object? o)
+        s_cachedStringBuilder = null;
+        return builder;
+    }
+
+    private static string ToStringAndReturnStringBuilder(StringBuilder builder)
+    {
+        string result = builder.ToString();
+        if (builder.Capacity <= CachedStringBuilderCapacity)
         {
-            if (o is byte[] bytes)
-            {
-                var stringBuilder = new StringBuilder();
-                foreach (byte b in bytes)
-                {
-                    stringBuilder.AppendFormat(CultureInfo.InvariantCulture, "{0:X2}", b);
-                }
-
-                return stringBuilder.ToString();
-            }
-
-            return o;
+            s_cachedStringBuilder = builder.Clear();
         }
+
+        return result;
     }
 }
