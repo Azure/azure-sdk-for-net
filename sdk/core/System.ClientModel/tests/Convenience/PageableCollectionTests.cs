209d468bb79b27d2bc1548768d90fe739359bfaa
Anne Thompson <annelo@microsoft.com>
2024-05-14 17:27:08 -0700

    wip: tests

--- sdk/core/System.ClientModel/tests/Convenience/PageableCollectionTests.cs
+++ sdk/core/System.ClientModel/tests/Convenience/PageableCollectionTests.cs
@@ -10,28 +10,48 @@ namespace System.ClientModel.Tests.Results;
 
 public class PageableCollectionTests //: SyncAsyncTestBase
 {
-    //public PageableCollectionTests(bool isAsync) : base(isAsync)
-    //{
-    //}
-
-    [Test]
-    public void CanEnumerateValues()
-    {
-        string[] mockPageContents = { """
+    private static readonly string[] MockPageContents = { """
             [
                 { "intValue" : 0, "stringValue" : "0" },
-                { "intValue" : 1, "stringValue" : "1" }
+                { "intValue" : 1, "stringValue" : "1" },
+                { "intValue" : 2, "stringValue" : "2" }
             ]
-            """, """
+            ""","""
             [
-                { "intValue" : 2, "stringValue" : "2" },
-                { "intValue" : 3, "stringValue" : "3" }
+                { "intValue" : 3, "stringValue" : "3" },
+                { "intValue" : 4, "stringValue" : "4" },
+                { "intValue" : 5, "stringValue" : "5" }
+            ]
+            ""","""
+            [
+                { "intValue" : 6, "stringValue" : "6" },
+                { "intValue" : 7, "stringValue" : "7" },
+                { "intValue" : 8, "stringValue" : "8" }
             ]
             """,
         };
 
+    // Page enumeration stops on an empty page, so we add one to the count
+    private static readonly int PageCount = MockPageContents.Length + 1;
+    private static readonly int ItemCount = 9;
+
+    //public PageableCollectionTests(bool isAsync) : base(isAsync)
+    //{
+    //}
+
+    // TODO:
+    // Enumerate values - sync and async
+    // Get as pages
+    // Modify page size
+    // Get from middle page
+    // Enumerate forward and backward
+    // Get first and last pages per TypeSpec
+
+    [Test]
+    public void CanEnumerateValues()
+    {
         MockPageableClient client = new();
-        PageableCollection<MockJsonModel> models = client.GetModels(mockPageContents);
+        PageableCollection<MockJsonModel> models = client.GetModels(MockPageContents);
 
         int i = 0;
         foreach (MockJsonModel model in models)
@@ -42,13 +62,57 @@ public class PageableCollectionTests //: SyncAsyncTestBase
             i++;
         }
 
-        Assert.AreEqual(4, i);
+        Assert.AreEqual(ItemCount, i);
     }
 
-    // Enumerate values - sync and async
-    // Get as pages
-    // Modify page size
-    // Get from middle page
-    // Enumerate forward and backward
-    // Get first and last pages per TypeSpec
+    [Test]
+    public void CanEnumeratePages()
+    {
+        MockPageableClient client = new();
+        PageableCollection<MockJsonModel> models = client.GetModels(MockPageContents);
+
+        int pageCount = 0;
+        int itemCount = 0;
+        foreach (ClientPage<MockJsonModel> page in models.AsPages())
+        {
+            foreach (MockJsonModel model in page)
+            {
+                Assert.AreEqual(itemCount, model.IntValue);
+                Assert.AreEqual(itemCount.ToString(), model.StringValue);
+
+                itemCount++;
+            }
+
+            pageCount++;
+        }
+
+        Assert.AreEqual(ItemCount, itemCount);
+        Assert.AreEqual(PageCount, pageCount);
+    }
+
+    [Test]
+    public void CanStartPageEnumerationMidway()
+    {
+        MockPageableClient client = new();
+        PageableCollection<MockJsonModel> models = client.GetModels(MockPageContents);
+
+        int pageCount = 0;
+        int i = 6;
+        foreach (ClientPage<MockJsonModel> page in models.AsPages(continuationToken: "5"))
+        {
+            foreach (MockJsonModel model in page)
+            {
+                Assert.AreEqual(i, model.IntValue);
+                Assert.AreEqual(i.ToString(), model.StringValue);
+
+                i++;
+            }
+
+            pageCount++;
+        }
+
+        Assert.AreEqual(ItemCount, i);
+        // Two pages - the last one with items 6-8 in it, and the final empty one.
+        Assert.AreEqual(2, pageCount);
+    }
 }
