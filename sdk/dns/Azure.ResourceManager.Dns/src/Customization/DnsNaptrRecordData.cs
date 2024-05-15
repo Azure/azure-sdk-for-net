8592551a894661f02821526626cbd0c12a724e5b
Sandeep Kadagathur Vadiraj <skadagat@microsoft.com>
2024-05-15 13:35:18 -0700

    Typo fixes for the NAPTR record type

--- sdk/dns/Azure.ResourceManager.Dns/src/Customization/DnsNaptrRecordData.cs
+++ sdk/dns/Azure.ResourceManager.Dns/src/Customization/DnsNaptrRecordData.cs
@@ -18,7 +18,7 @@ namespace Azure.ResourceManager.Dns
         /// <summary> Initializes a new instance of DnsNaptrRecordData. </summary>
         public DnsNaptrRecordData()
         {
-            DnsNaptrRecorNaptr = new ChangeTrackingList<DnsNaptrRecordInfo>();
+            DnsNaptrRecords = new ChangeTrackingList<DnsNaptrRecordInfo>();
         }
 
         /// <summary> Initializes a new instance of DnsNaptrRecordData. </summary>
@@ -28,18 +28,18 @@ namespace Azure.ResourceManager.Dns
         /// <param name="systemData"> The systemData. </param>
         /// <param name="etag"> The etag of the record set. </param>
         /// <param name="metadata"> The metadata attached to the record set. </param>
-        /// <param name="ttl"> The Ttl (time-to-live) of the recorNaptr in the record set. </param>
+        /// <param name="ttl"> The Ttl (time-to-live) of the records in the record set. </param>
         /// <param name="fqdn"> Fully qualified domain name of the record set. </param>
         /// <param name="provisioningState"> provisioning State of the record set. </param>
         /// <param name="targetResource"> A reference to an azure resource from where the dns resource value is taken. </param>
         /// <param name="trafficManagementProfile"> A reference to an azure traffic manager profile resource from where the dns resource value is taken. </param>
-        /// <param name="NaptrRecorNaptr"> The list of Naptr recorNaptr in the record set. </param>
-        internal DnsNaptrRecordData(ResourceIdentifier id, string name, ResourceType resourceType, ResourceManager.Models.SystemData systemData, ETag? etag, IDictionary<string, string> metadata, long? ttl, string fqdn, string provisioningState, WritableSubResource targetResource, WritableSubResource trafficManagementProfile,  IList<DnsNaptrRecordInfo> NaptrRecorNaptr) : base(id, name, resourceType, systemData, etag, metadata, ttl, fqdn, provisioningState, targetResource, trafficManagementProfile)
+        /// <param name="NaptrRecords"> The list of Naptr records in the record set. </param>
+        internal DnsNaptrRecordData(ResourceIdentifier id, string name, ResourceType resourceType, ResourceManager.Models.SystemData systemData, ETag? etag, IDictionary<string, string> metadata, long? ttl, string fqdn, string provisioningState, WritableSubResource targetResource, WritableSubResource trafficManagementProfile,  IList<DnsNaptrRecordInfo> NaptrRecords) : base(id, name, resourceType, systemData, etag, metadata, ttl, fqdn, provisioningState, targetResource, trafficManagementProfile)
         {
-            DnsNaptrRecorNaptr = NaptrRecorNaptr;
+            DnsNaptrRecords = NaptrRecords;
         }
 
-        /// <summary> The list of Naptr recorNaptr in the record set. </summary>
-        public IList<DnsNaptrRecordInfo> DnsNaptrRecorNaptr { get; }
+        /// <summary> The list of Naptr records in the record set. </summary>
+        public IList<DnsNaptrRecordInfo> DnsNaptrRecords { get; }
     }
 }
