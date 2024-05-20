f1683622bc521c4e5d3adc81f833cc13b04cfc98
Matt Connew <mconnew@users.noreply.github.com>
2024-05-20 15:14:41 -0700

    Stopping tests from listening for HTTPS

--- sdk/extension-wcf/Microsoft.CoreWCF.Azure.StorageQueues/tests/Helpers/ServiceHelper.cs
+++ sdk/extension-wcf/Microsoft.CoreWCF.Azure.StorageQueues/tests/Helpers/ServiceHelper.cs
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using Microsoft.AspNetCore;
+using System.Net;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.CoreWCF.Azure.StorageQueues.Tests.Helpers
@@ -13,15 +14,28 @@ namespace Microsoft.CoreWCF.Azure.StorageQueues.Tests.Helpers
         public static IWebHostBuilder CreateWebHostBuilder<TStartup>()
             where TStartup : class
         {
-            return WebHost.CreateDefaultBuilder(Array.Empty<string>())
-#if DEBUG
-                .ConfigureLogging((logging) =>
+            return new WebHostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
                 {
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging(logging =>
+                {
+#if DEBUG
                     logging.AddFilter("Default", LogLevel.Debug);
                     logging.AddFilter("Microsoft", LogLevel.Debug);
                     logging.SetMinimumLevel(LogLevel.Debug);
-                })
+                    logging.AddDebug();
 #endif // DEBUG
+                })
+                .UseDefaultServiceProvider((context, options) =>
+                {
+                    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                })
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, 0);
+                })
                 .UseStartup<TStartup>();
         }
     }
