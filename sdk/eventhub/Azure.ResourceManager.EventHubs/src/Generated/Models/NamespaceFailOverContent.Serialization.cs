c81909762184cfa88736a1541fb2ea3f51ff7f38
azure-sdk <azuresdk@microsoft.com>
2024-05-22 08:23:01 +0000

    Update SDK code da_ex_3

--- sdk/eventhub/Azure.ResourceManager.EventHubs/src/Generated/Models/NamespaceFailOverContent.Serialization.cs
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/src/Generated/Models/NamespaceFailOverContent.Serialization.cs
@@ -8,6 +8,7 @@
 using System;
 using System.ClientModel.Primitives;
 using System.Collections.Generic;
+using System.Text;
 using System.Text.Json;
 using Azure.Core;
 
@@ -122,6 +123,54 @@ namespace Azure.ResourceManager.EventHubs.Models
             return new NamespaceFailOverContent(primaryLocation, maximumGracePeriodInMins, serializedAdditionalRawData);
         }
 
+        private BinaryData SerializeBicep(ModelReaderWriterOptions options)
+        {
+            StringBuilder builder = new StringBuilder();
+            BicepModelReaderWriterOptions bicepOptions = options as BicepModelReaderWriterOptions;
+            IDictionary<string, string> propertyOverrides = null;
+            bool hasObjectOverride = bicepOptions != null && bicepOptions.PropertyOverrides.TryGetValue(this, out propertyOverrides);
+            bool hasPropertyOverride = false;
+            string propertyOverride = null;
+
+            builder.AppendLine("{");
+
+            builder.Append("  properties:");
+            builder.AppendLine(" {");
+            hasPropertyOverride = hasObjectOverride && propertyOverrides.TryGetValue(nameof(PrimaryLocation), out propertyOverride);
+            if (hasPropertyOverride)
+            {
+                builder.Append("    primaryLocation: ");
+                builder.AppendLine(propertyOverride);
+            }
+            else
+            {
+                if (Optional.IsDefined(PrimaryLocation))
+                {
+                    builder.Append("    primaryLocation: ");
+                    builder.AppendLine($"'{PrimaryLocation.Value.ToString()}'");
+                }
+            }
+
+            hasPropertyOverride = hasObjectOverride && propertyOverrides.TryGetValue(nameof(MaximumGracePeriodInMins), out propertyOverride);
+            if (hasPropertyOverride)
+            {
+                builder.Append("    maximumGracePeriodInMins: ");
+                builder.AppendLine(propertyOverride);
+            }
+            else
+            {
+                if (Optional.IsDefined(MaximumGracePeriodInMins))
+                {
+                    builder.Append("    maximumGracePeriodInMins: ");
+                    builder.AppendLine($"{MaximumGracePeriodInMins.Value}");
+                }
+            }
+
+            builder.AppendLine("  }");
+            builder.AppendLine("}");
+            return BinaryData.FromString(builder.ToString());
+        }
+
         BinaryData IPersistableModel<NamespaceFailOverContent>.Write(ModelReaderWriterOptions options)
         {
             var format = options.Format == "W" ? ((IPersistableModel<NamespaceFailOverContent>)this).GetFormatFromOptions(options) : options.Format;
@@ -130,6 +179,8 @@ namespace Azure.ResourceManager.EventHubs.Models
             {
                 case "J":
                     return ModelReaderWriter.Write(this, options);
+                case "bicep":
+                    return SerializeBicep(options);
                 default:
                     throw new FormatException($"The model {nameof(NamespaceFailOverContent)} does not support writing '{options.Format}' format.");
             }
