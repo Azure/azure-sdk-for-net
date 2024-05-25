6b77577e701e7e96b1e3c9b64ec2c4cf6c4ab239
Jamie Cheung <jamiecheung@Jamies-Virtual-Machine.local>
2024-05-25 21:27:45 +0000

    Added tests to ExaInfraTests

--- sdk/oracle/Azure.ResourceManager.OracleDatabase/tests/Scenario/ExaInfraTests.cs
+++ sdk/oracle/Azure.ResourceManager.OracleDatabase/tests/Scenario/ExaInfraTests.cs
@@ -18,5 +18,50 @@ namespace Azure.ResourceManager.OracleDatabase.Tests.Scenario
         public ExaInfraTests() : base(true)
         {
         }
+        [SetUp]
+        public async Task ClearAndInitialize()
+        {
+            Console.WriteLine("HERE: ClearAndInitialize");
+            if (Mode == RecordedTestMode.Record || Mode == RecordedTestMode.Playback){
+                await CreateCommonClient();
+            }
+        }
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            Console.WriteLine("HERE: Cleanup");
+            CleanupResourceGroups();
+        }
+        [TestCase]
+        [RecordedTest]
+        public async Task TestExaInfraOperations()
+        {
+            Console.WriteLine("HERE: TestExaInfraOperations");
+            var resourceGroupName = Recording.GenerateAssetName("SdkRg");
+            await TryRegisterResourceGroupAsync(ResourceGroupsOperations, "eastus", resourceGroupName);
+            CloudExadataInfrastructureCollection cloudExadataInfrastructureCollection = await GetCloudExadataInfrastructureCollectionAsync(resourceGroupName);
+            var cloudExadataInfrastructureName = Recording.GenerateAssetName("OFake_Jamie");
+            CloudExadataInfrastructureData exadataInfrastructureData = GetDefaultCloudExadataInfrastructureData(cloudExadataInfrastructureName);
+
+            // Create
+            Console.WriteLine("HERE: TestExaInfraOperations Create");
+            var createExadataOperation = await cloudExadataInfrastructureCollection.CreateOrUpdateAsync(WaitUntil.Completed,
+            cloudExadataInfrastructureName, exadataInfrastructureData);
+            await createExadataOperation.WaitForCompletionAsync();
+            Assert.IsTrue(createExadataOperation.HasCompleted);
+            Assert.IsTrue(createExadataOperation.HasValue);
+
+            // Get
+            Console.WriteLine("HERE: TestExaInfraOperations Get");
+            Response<CloudExadataInfrastructureResource> getExaInfraResponse = await cloudExadataInfrastructureCollection.GetAsync(cloudExadataInfrastructureName);
+            CloudExadataInfrastructureResource exaInfraResource = getExaInfraResponse.Value;
+            Assert.IsNotNull(exaInfraResource);
+
+            // Delete
+            Console.WriteLine("HERE: TestExaInfraOperations Delete");
+            var deleteExaInfraOperation = await exaInfraResource.DeleteAsync(WaitUntil.Completed);
+            await deleteExaInfraOperation.WaitForCompletionResponseAsync();
+            Assert.IsTrue(deleteExaInfraOperation.HasCompleted);
+        }
     }
 }
