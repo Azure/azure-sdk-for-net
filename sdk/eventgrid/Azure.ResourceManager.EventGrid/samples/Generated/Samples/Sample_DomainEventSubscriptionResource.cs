0e4fe24aac32f3c9aa8064ef9f6b63134fa23c23
azure-sdk <azuresdk@microsoft.com>
2024-05-28 05:21:56 +0000

    Update SDK code da_ex_3

--- sdk/eventgrid/Azure.ResourceManager.EventGrid/samples/Generated/Samples/Sample_DomainEventSubscriptionResource.cs
+++ sdk/eventgrid/Azure.ResourceManager.EventGrid/samples/Generated/Samples/Sample_DomainEventSubscriptionResource.cs
@@ -162,7 +162,7 @@ namespace Azure.ResourceManager.EventGrid.Samples
         public async Task GetFullUri_DomainEventSubscriptionsGetFullUrl()
         {
             // Generated from example definition: specification/eventgrid/resource-manager/Microsoft.EventGrid/preview/2023-12-15-preview/examples/DomainEventSubscriptions_GetFullUrl.json
-            // this example is just showing the usage of "DomainEventSubscriptions_GetFullUrl" operation, for the dependent resources, they will have to be created separately.
+            // this example is just showing the usage of "DomainEventSubscriptions_GetFullUri" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
             TokenCredential cred = new DefaultAzureCredential();
