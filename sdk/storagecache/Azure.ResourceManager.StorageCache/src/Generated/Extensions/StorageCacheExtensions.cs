061c1d5c5aae71ff6427e2c72cf262c5bafe56bd
azure-sdk <azuresdk@microsoft.com>
2024-05-30 06:01:34 +0000

    Update SDK code st_wo_9

--- sdk/storagecache/Azure.ResourceManager.StorageCache/src/Generated/Extensions/StorageCacheExtensions.cs
+++ sdk/storagecache/Azure.ResourceManager.StorageCache/src/Generated/Extensions/StorageCacheExtensions.cs
@@ -357,7 +357,7 @@ namespace Azure.ResourceManager.StorageCache
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>CheckAmlFSSubnets</description>
+        /// <description>checkAmlFSSubnets</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -389,7 +389,7 @@ namespace Azure.ResourceManager.StorageCache
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>CheckAmlFSSubnets</description>
+        /// <description>checkAmlFSSubnets</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -421,7 +421,7 @@ namespace Azure.ResourceManager.StorageCache
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>GetRequiredAmlFSSubnetsSize</description>
+        /// <description>getRequiredAmlFSSubnetsSize</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -453,7 +453,7 @@ namespace Azure.ResourceManager.StorageCache
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>GetRequiredAmlFSSubnetsSize</description>
+        /// <description>getRequiredAmlFSSubnetsSize</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
--- sdk/synapse/Azure.ResourceManager.Synapse/samples/Generated/Samples/Sample_SynapseDatabaseResource.cs
+++ sdk/synapse/Azure.ResourceManager.Synapse/samples/Generated/Samples/Sample_SynapseDatabaseResource.cs
@@ -149,10 +149,10 @@ namespace Azure.ResourceManager.Synapse.Samples
         // KustoPoolDataConnectionValidation
         [NUnit.Framework.Test]
         [NUnit.Framework.Ignore("Only verifying that the sample builds")]
-        public async Task ValidateDataConnection_KustoPoolDataConnectionValidation()
+        public async Task DataConnectionValidationKustoPoolDataConnection_KustoPoolDataConnectionValidation()
         {
             // Generated from example definition: specification/synapse/resource-manager/Microsoft.Synapse/preview/2021-06-01-preview/examples/KustoPoolDataConnectionValidation.json
-            // this example is just showing the usage of "KustoPoolDataConnections_DataConnectionValidation" operation, for the dependent resources, they will have to be created separately.
+            // this example is just showing the usage of "KustoPoolDataConnections_dataConnectionValidation" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
             TokenCredential cred = new DefaultAzureCredential();
@@ -175,7 +175,7 @@ namespace Azure.ResourceManager.Synapse.Samples
                 DataConnectionName = "DataConnections8",
                 Properties = new SynapseEventHubDataConnection(),
             };
-            ArmOperation<SynapseDataConnectionValidationListResult> lro = await synapseDatabase.ValidateDataConnectionAsync(WaitUntil.Completed, synapseDataConnectionValidation);
+            ArmOperation<SynapseDataConnectionValidationListResult> lro = await synapseDatabase.DataConnectionValidationKustoPoolDataConnectionAsync(WaitUntil.Completed, synapseDataConnectionValidation);
             SynapseDataConnectionValidationListResult result = lro.Value;
 
             Console.WriteLine($"Succeeded: {result}");
