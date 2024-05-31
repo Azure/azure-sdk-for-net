7282cf8710c672fce43fcdf2118e31f44e98fa60
azure-sdk <azuresdk@microsoft.com>
2024-05-31 09:39:33 +0000

    Update SDK code da_ex_3

--- sdk/easm/Azure.Analytics.Defender.Easm/src/Generated/AnalyticsDefenderEasmModelFactory.cs
+++ sdk/easm/Azure.Analytics.Defender.Easm/src/Generated/AnalyticsDefenderEasmModelFactory.cs
@@ -15,7 +15,7 @@ namespace Azure.Analytics.Defender.Easm
     public static partial class AnalyticsDefenderEasmModelFactory
     {
         /// <summary> Initializes a new instance of <see cref="Easm.AssetResource"/>. </summary>
-        /// <param name="kind"> Discriminator. </param>
+        /// <param name="kind"> Discriminator property for AssetResource. </param>
         /// <param name="id"> The system generated unique id for the resource. </param>
         /// <param name="name"> The caller provided unique name for the resource. </param>
         /// <param name="displayName"> The name that can be used for display purposes. </param>
@@ -98,7 +98,7 @@ namespace Azure.Analytics.Defender.Easm
         }
 
         /// <summary> Initializes a new instance of <see cref="Easm.DataConnection"/>. </summary>
-        /// <param name="kind"> Discriminator. </param>
+        /// <param name="kind"> Discriminator property for DataConnection. </param>
         /// <param name="id"> The system generated unique id for the resource. </param>
         /// <param name="name"> The caller provided unique name for the resource. </param>
         /// <param name="displayName"> The name that can be used for display purposes. </param>
