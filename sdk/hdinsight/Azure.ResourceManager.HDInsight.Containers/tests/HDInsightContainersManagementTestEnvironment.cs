4ad213bcc6c7d8036da9b897efd0230a1d5e6a1b
v-yuchenli <v-yuchenli@microsoft.com>
2024-06-06 16:05:05 +0800

    Update Version

--- sdk/hdinsight/Azure.ResourceManager.HDInsight.Containers/tests/HDInsightContainersManagementTestEnvironment.cs
+++ sdk/hdinsight/Azure.ResourceManager.HDInsight.Containers/tests/HDInsightContainersManagementTestEnvironment.cs
@@ -7,8 +7,8 @@ namespace Azure.ResourceManager.HDInsight.Containers.Tests
 {
     public class HDInsightContainersManagementTestEnvironment : TestEnvironment
     {
-        public new string SubscriptionId => GetRecordedVariable("SUBSCRIPTION_ID", options => options.IsSecret("00000000-0000-0000-0000-000000000000"));
+        public new string SubscriptionId => GetRecordedVariable("SUBSCRIPTION_ID", options => options.IsSecret("10e32bab-26da-4cc4-a441-52b318f824e6"));
         public new string ClientId => GetRecordedVariable("CLIENT_ID", options => options.IsSecret());
-        public new string TenantId => GetRecordedVariable("TENANT_ID", options => options.IsSecret("00000000-0000-0000-0000-000000000000"));
+        public new string TenantId => GetRecordedVariable("TENANT_ID", options => options.IsSecret("72f988bf-86f1-41af-91ab-2d7cd011db47"));
     }
 }
