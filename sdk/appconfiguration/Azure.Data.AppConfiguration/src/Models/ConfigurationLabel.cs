855a7c164c089f4c3eadbb019e919bfc927dace0
Jorge Rangel <jorgerangel@microsoft.com>
2024-06-06 17:50:46 -0500

    feat: add support for list labels operation

---
+++ sdk/appconfiguration/Azure.Data.AppConfiguration/src/Models/ConfigurationLabel.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Data.AppConfiguration
+{
+    /// <summary> A label used to group key-values. </summary>
+    public partial class ConfigurationLabel
+    {
+        /// <summary> Initializes a new instance of <see cref="ConfigurationLabel"/>. </summary>
+        /// <param name="name"> The name of the label. </param>
+        internal ConfigurationLabel(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary> The name of the label. </summary>
+        public string Name { get; }
+    }
+}
---
+++ sdk/appconfiguration/Azure.Data.AppConfiguration/tests/ConfigurationLabelTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Text.Json;
+using NUnit.Framework;
+
+namespace Azure.Data.AppConfiguration.Tests
+{
+    public class ConfigurationLabelTests
+    {
+        [TestCaseSource(nameof(DeserializeConfigurationLabelTestCases))]
+        public void DeserializeConfigurationLabel(string json, bool hasName)
+        {
+            var element = JsonDocument.Parse(json).RootElement;
+
+            // Act
+            var label = ConfigurationLabel.DeserializeLabel(element);
+
+            if (hasName)
+            {
+                Assert.IsNotNull(label.Name);
+                Assert.AreEqual("testName", label.Name);
+            }
+            else
+            {
+                Assert.IsNull(label.Name);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> DeserializeConfigurationLabelTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(@"{
+                ""name"": ""testName""
+                }", true);
+                yield return new TestCaseData(@"{}", false);
+            }
+        }
+    }
+}
