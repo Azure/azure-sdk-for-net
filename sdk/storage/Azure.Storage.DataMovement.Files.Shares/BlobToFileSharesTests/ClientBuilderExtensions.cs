98fdf8c7b2389758cdd14ad9c12e4285fe358128
Sean McCullough <seanmcc@microsoft.com>
2024-06-06 20:08:18 -0500

    update

--- sdk/storage/Azure.Storage.DataMovement.Files.Shares/BlobToFileSharesTests/ClientBuilderExtensions.cs
+++ sdk/storage/Azure.Storage.DataMovement.Files.Shares/BlobToFileSharesTests/ClientBuilderExtensions.cs
@@ -103,7 +103,7 @@ namespace Azure.Storage.DataMovement.Blobs.Files.Shares.Tests
 
             if (publicAccessType == default)
             {
-                publicAccessType = premium ? PublicAccessType.None : PublicAccessType.BlobContainer;
+                publicAccessType = PublicAccessType.None;
             }
 
             BlobContainerClient container = clientBuilder.AzureCoreRecordedTestBase.InstrumentClient(service.GetBlobContainerClient(containerName));
