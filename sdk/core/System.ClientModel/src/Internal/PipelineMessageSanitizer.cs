525ebb119b47a2e83a7976d92bac9abbf4c5f60b
Madalyn Redding <mredding@microsoft.com>
2024-06-07 14:47:29 -0400

    WIP FB

--- sdk/core/System.ClientModel/src/Internal/PipelineMessageSanitizer.cs
+++ sdk/core/System.ClientModel/src/Internal/PipelineMessageSanitizer.cs
@@ -16,9 +16,13 @@ internal class PipelineMessageSanitizer
     private const string LogAllValue = "*";
     private readonly bool _logAllHeaders;
     private readonly bool _logFullQueries;
+    private readonly string[] _allowedQueryParameters;
     private readonly string _redactedPlaceholder;
-    private readonly HashSet<string> _loggedHeaderNames;
-    private readonly IEnumerable<string> _loggedQueryParameters;
+    private readonly HashSet<string> _allowedHeaders;
+
+    [ThreadStatic]
+    private static StringBuilder? s_cachedStringBuilder;
+    private const int MaxCachedStringBuilderCapacity = 1024;
 
     /// <summary>
     /// TODO.
@@ -26,14 +30,14 @@ internal class PipelineMessageSanitizer
     /// <param name="allowedQueryParameters"></param>
     /// <param name="allowedHeaders"></param>
     /// <param name="redactedPlaceholder"></param>
-    public PipelineMessageSanitizer(IEnumerable<string> allowedQueryParameters, IEnumerable<string> allowedHeaders, string redactedPlaceholder = "REDACTED")
+    public PipelineMessageSanitizer(string[] allowedQueryParameters, string[] allowedHeaders, string redactedPlaceholder = "REDACTED")
     {
         _logAllHeaders = allowedHeaders.Contains(LogAllValue);
         _logFullQueries = allowedQueryParameters.Contains(LogAllValue);
 
-        _loggedQueryParameters = allowedQueryParameters;
+        _allowedQueryParameters = allowedQueryParameters;
         _redactedPlaceholder = redactedPlaceholder;
-        _loggedHeaderNames = new HashSet<string>(allowedHeaders, StringComparer.InvariantCultureIgnoreCase);
+        _allowedHeaders = new HashSet<string>(allowedHeaders, StringComparer.InvariantCultureIgnoreCase);
     }
 
     /// <summary>
@@ -44,7 +48,7 @@ internal class PipelineMessageSanitizer
     /// <returns></returns>
     public string SanitizeHeader(string name, string value)
     {
-        if (_logAllHeaders || _loggedHeaderNames.Contains(name))
+        if (_logAllHeaders || _allowedHeaders.Contains(name))
         {
             return value;
         }
@@ -64,7 +68,7 @@ internal class PipelineMessageSanitizer
             return url;
         }
 
-#if NET6_0_OR_GREATER
+#if NET5_0_OR_GREATER
         int indexOfQuerySeparator = url.IndexOf('?', StringComparison.Ordinal);
 #else
         int indexOfQuerySeparator = url.IndexOf('?');
@@ -75,23 +79,32 @@ internal class PipelineMessageSanitizer
             return url;
         }
 
-        StringBuilder stringBuilder = new StringBuilder(url.Length);
-        stringBuilder.Append(url, 0, indexOfQuerySeparator);
+        // PERF: Avoid allocations in this heavily-used method:
+        // 1. Use ReadOnlySpan<char> to avoid creating substrings.
+        // 2. Defer creating a StringBuilder until absolutely necessary.
+        // 3. Use a rented StringBuilder to avoid allocating a new one
+        //    each time.
 
-        string query = url.Substring(indexOfQuerySeparator);
+        // Create the StringBuilder only when necessary (when we encounter
+        // a query parameter that needs to be redacted)
+        StringBuilder? stringBuilder = null;
 
-        int queryIndex = 1;
-        stringBuilder.Append('?');
+        // Keeps track of the number of characters we've processed so far
+        // so that, if we need to create a StringBuilder, we know how many
+        // characters to copy over from the original URL.
+        int lengthSoFar = indexOfQuerySeparator + 1;
 
-        do
+        ReadOnlySpan<char> query = url.AsSpan(indexOfQuerySeparator + 1); // +1 to skip the '?'
+
+        while (query.Length > 0)
         {
-            int endOfParameterValue = query.IndexOf('&', queryIndex);
-            int endOfParameterName = query.IndexOf('=', queryIndex);
+            int endOfParameterValue = query.IndexOf('&');
+            int endOfParameterName = query.IndexOf('=');
             bool noValue = false;
 
             // Check if we have parameter without value
-            if (endOfParameterValue == -1 && endOfParameterName == -1 ||
-                endOfParameterValue != -1 && (endOfParameterName == -1 || endOfParameterName > endOfParameterValue))
+            if ((endOfParameterValue == -1 && endOfParameterName == -1) ||
+                (endOfParameterValue != -1 && (endOfParameterName == -1 || endOfParameterName > endOfParameterValue)))
             {
                 endOfParameterName = endOfParameterValue;
                 noValue = true;
@@ -112,10 +125,10 @@ internal class PipelineMessageSanitizer
                 endOfParameterValue++;
             }
 
-            ReadOnlySpan<char> parameterName = query.AsSpan(queryIndex, endOfParameterName - queryIndex);
+            ReadOnlySpan<char> parameterName = query.Slice(0, endOfParameterName);
 
             bool isAllowed = false;
-            foreach (string name in _loggedQueryParameters)
+            foreach (string name in _allowedQueryParameters)
             {
                 if (parameterName.Equals(name.AsSpan(), StringComparison.OrdinalIgnoreCase))
                 {
@@ -124,34 +137,79 @@ internal class PipelineMessageSanitizer
                 }
             }
 
-            int valueLength = endOfParameterValue - queryIndex;
-            int nameLength = endOfParameterName - queryIndex;
+            int valueLength = endOfParameterValue;
+            int nameLength = endOfParameterName;
 
-            if (isAllowed)
+            if (isAllowed || noValue)
             {
-                stringBuilder.Append(query, queryIndex, valueLength);
-            }
-            else
-            {
-                if (noValue)
+                if (stringBuilder is null)
                 {
-                    stringBuilder.Append(query, queryIndex, valueLength);
+                    lengthSoFar += valueLength;
                 }
                 else
                 {
-                    stringBuilder.Append(query, queryIndex, nameLength);
-                    stringBuilder.Append('=');
-                    stringBuilder.Append(_redactedPlaceholder);
-                    if (query[endOfParameterValue - 1] == '&')
-                    {
-                        stringBuilder.Append('&');
-                    }
+                    AppendReadOnlySpan(stringBuilder, query.Slice(0, valueLength));
+                }
+            }
+            else
+            {
+                // Encountered a query value that needs to be redacted.
+                // Create the StringBuilder if we haven't already.
+                stringBuilder ??= RentStringBuilder(url.Length).Append(url, 0, lengthSoFar);
+
+                AppendReadOnlySpan(stringBuilder, query.Slice(0, nameLength))
+                    .Append('=')
+                    .Append(_redactedPlaceholder);
+
+                if (query[endOfParameterValue - 1] == '&')
+                {
+                    stringBuilder.Append('&');
                 }
             }
 
-            queryIndex += valueLength;
-        } while (queryIndex < query.Length);
+            query = query.Slice(valueLength);
+        }
 
-        return stringBuilder.ToString();
+        return stringBuilder is null ? url : ToStringAndReturnStringBuilder(stringBuilder);
+
+        static StringBuilder AppendReadOnlySpan(StringBuilder builder, ReadOnlySpan<char> span)
+        {
+#if NET6_0_OR_GREATER
+            return builder.Append(span);
+#else
+            foreach (char c in span)
+            {
+                builder.Append(c);
+            }
+
+            return builder;
+#endif
+        }
     }
+
+        private static StringBuilder RentStringBuilder(int capacity)
+        {
+            if (capacity <= MaxCachedStringBuilderCapacity)
+            {
+                StringBuilder? builder = s_cachedStringBuilder;
+                if (builder is not null && builder.Capacity >= capacity)
+                {
+                    s_cachedStringBuilder = null;
+                    return builder;
+                }
+            }
+
+            return new StringBuilder(capacity);
+        }
+
+        private static string ToStringAndReturnStringBuilder(StringBuilder builder)
+        {
+            string result = builder.ToString();
+            if (builder.Capacity <= MaxCachedStringBuilderCapacity)
+            {
+                s_cachedStringBuilder = builder.Clear();
+            }
+
+            return result;
+        }
 }
