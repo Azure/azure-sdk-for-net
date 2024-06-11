6484621e11f3aa2818ca3a8f937bff970616b107
Graham Thomas <gthomas@microsoft.com>
2024-06-11 13:32:52 -0700

    Removes test code

--- sdk/healthdataaiservices/Azure.Health.Deidentification/tests/Tests/Helpers/DeidentificationTestEnvironment.cs
+++ sdk/healthdataaiservices/Azure.Health.Deidentification/tests/Tests/Helpers/DeidentificationTestEnvironment.cs
@@ -8,8 +8,7 @@ namespace Azure.Health.Deidentification.Tests
     public class DeidentificationTestEnvironment : TestEnvironment
     {
         public const string FakeSASUri = "https://localhost/fakecontainer";
-        // public string Endpoint => GetRecordedVariable("DEID_SERVICE_ENDPOINT");
-        public string Endpoint => "https://localhost:5020";
+        public string Endpoint => GetRecordedVariable("DEID_SERVICE_ENDPOINT");
         public string FakeNextLink => $"{Endpoint}/jobs?api-version=2024-01-16-preview&continuationToken=1234";
 
         public string StorageAccountSASUri => GetRecordedVariable("STORAGE_ACCOUNT_SAS_URI", options => options.IsSecret(FakeSASUri));
