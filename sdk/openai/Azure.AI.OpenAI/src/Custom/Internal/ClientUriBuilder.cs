2dc32fc8c38f42a0f75b78da4f4d043b7feb558f
Travis Wilson <travisw@microsoft.com>
2024-06-12 11:53:44 -0700

    offline PR feedback: use TrimStart for simplicity

--- sdk/openai/Azure.AI.OpenAI/src/Custom/Internal/ClientUriBuilder.cs
+++ sdk/openai/Azure.AI.OpenAI/src/Custom/Internal/ClientUriBuilder.cs
@@ -1,32 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace Azure.AI.OpenAI;
 
 internal partial class ClientUriBuilder
 {
     public Uri ToUri()
     {
-        if (_pathBuilder != null)
+        if (_pathBuilder is not null)
         {
             UriBuilder.Path = _pathBuilder.ToString();
         }
+
         // CUSTOMIZATION: UriBuilder.Query.set always prepends its own '?' pre-net5.0, which results in a double '??'.
-        //                To mitigate pending generated ClientUriBuilder updates, check for this condition and fall back.
+        //                To mitigate pending generated ClientUriBuilder updates, ensure we only get one '?' by letting
+        //                the builder add it.
         //           see: https://github.com/Azure/autorest.csharp/issues/4815
-        if (_queryBuilder != null)
+        if (_queryBuilder is not null)
         {
-            string queryStringParameters = _queryBuilder.ToString();
-            UriBuilder.Query = queryStringParameters;
-            if (UriBuilder.Query.StartsWith("??"))
-            {
-                UriBuilder.Query = queryStringParameters.Substring(1);
-            }
+            UriBuilder.Query = _queryBuilder.ToString().TrimStart('?');
         }
 
         return UriBuilder.Uri;
