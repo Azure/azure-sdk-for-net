8ce933389d7faab3af88e8d64b64ef21880c971d
Ben Eshed (from Dev Box) <bewaters@microsoft.com>
2024-06-16 10:52:58 +0300

    Azure Firewall Save and Commit UTs

--- sdk/network/Azure.ResourceManager.Network/tests/Tests/FirewallPolicyTests.cs
+++ sdk/network/Azure.ResourceManager.Network/tests/Tests/FirewallPolicyTests.cs
@@ -1,14 +1,16 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Azure.Core;
 using Azure.Core.TestFramework;
-using Azure.ResourceManager.Resources;
-using Azure.ResourceManager.Resources.Models;
 using Azure.ResourceManager.Network.Models;
 using Azure.ResourceManager.Network.Tests.Helpers;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
 using NUnit.Framework;
-using Azure.Core;
 
 namespace Azure.ResourceManager.Network.Tests
 {
@@ -24,31 +26,43 @@ namespace Azure.ResourceManager.Network.Tests
         private ResourceIdentifier _publicIPAddressIdentifier;
         private ResourceIdentifier _firewallIdentifier;
 
-        public FirewallPolicyTests(bool isAsync) : base(isAsync)
-        {
-        }
+        public FirewallPolicyTests(bool isAsync)
+            : base(isAsync) { }
 
         [OneTimeSetUp]
         public async Task GlobalSetUp()
         {
             SubscriptionResource subscription = await GlobalClient.GetDefaultSubscriptionAsync();
-            var rgLro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, SessionRecording.GenerateAssetName("FirewallPolicyRG-"), new ResourceGroupData(AzureLocation.WestUS2));
+            var rgLro = await subscription
+                .GetResourceGroups()
+                .CreateOrUpdateAsync(
+                    WaitUntil.Completed,
+                    SessionRecording.GenerateAssetName("FirewallPolicyRG-"),
+                    new ResourceGroupData(AzureLocation.WestUS2)
+                );
             ResourceGroupResource rg = rgLro.Value;
             _resourceGroupIdentifier = rg.Id;
 
             VirtualNetworkData vnetData = new VirtualNetworkData()
             {
                 Location = AzureLocation.WestUS2,
-                AddressSpace = new AddressSpace()
+                AddressSpace = new AddressSpace() { AddressPrefixes = { "10.26.0.0/16", } },
+                Subnets =
                 {
-                    AddressPrefixes = { "10.26.0.0/16", }
-                },
-                Subnets = {
                     new SubnetData() { Name = "Default", AddressPrefix = "10.26.1.0/26", },
-                    new SubnetData() { Name = "AzureFirewallSubnet", AddressPrefix = "10.26.2.0/26", },
+                    new SubnetData()
+                    {
+                        Name = "AzureFirewallSubnet",
+                        AddressPrefix = "10.26.2.0/26",
+                    },
                 },
             };
-            var vnetLro = await rg.GetVirtualNetworks().CreateOrUpdateAsync(WaitUntil.Completed, SessionRecording.GenerateAssetName("vnet-"), vnetData);
+            var vnetLro = await rg.GetVirtualNetworks()
+                .CreateOrUpdateAsync(
+                    WaitUntil.Completed,
+                    SessionRecording.GenerateAssetName("vnet-"),
+                    vnetData
+                );
             _network = vnetLro.Value;
             _networkIdentifier = _network.Id;
 
@@ -58,19 +72,37 @@ namespace Azure.ResourceManager.Network.Tests
                 PublicIPAllocationMethod = NetworkIPAllocationMethod.Static,
                 Sku = new PublicIPAddressSku() { Name = PublicIPAddressSkuName.Standard },
             };
-            var ipLro = await rg.GetPublicIPAddresses().CreateOrUpdateAsync(WaitUntil.Completed, SessionRecording.GenerateAssetName("publicIp-"), ipData);
+            var ipLro = await rg.GetPublicIPAddresses()
+                .CreateOrUpdateAsync(
+                    WaitUntil.Completed,
+                    SessionRecording.GenerateAssetName("publicIp-"),
+                    ipData
+                );
             _publicIPAddress = ipLro.Value;
             _publicIPAddressIdentifier = _publicIPAddress.Id;
 
             AzureFirewallData firewallData = new AzureFirewallData();
             firewallData.Location = AzureLocation.WestUS2;
-            firewallData.IPConfigurations.Add(new AzureFirewallIPConfiguration()
-            {
-                Name = "fwpip",
-                PublicIPAddress = new WritableSubResource() { Id = _publicIPAddressIdentifier },
-                Subnet = new WritableSubResource() { Id = _networkIdentifier.AppendChildResource("subnets", "AzureFirewallSubnet") },
-            });
-            var firewallLro = await rg.GetAzureFirewalls().CreateOrUpdateAsync(WaitUntil.Completed, SessionRecording.GenerateAssetName("firewall-"), firewallData);
+            firewallData.IPConfigurations.Add(
+                new AzureFirewallIPConfiguration()
+                {
+                    Name = "fwpip",
+                    PublicIPAddress = new WritableSubResource() { Id = _publicIPAddressIdentifier },
+                    Subnet = new WritableSubResource()
+                    {
+                        Id = _networkIdentifier.AppendChildResource(
+                            "subnets",
+                            "AzureFirewallSubnet"
+                        )
+                    },
+                }
+            );
+            var firewallLro = await rg.GetAzureFirewalls()
+                .CreateOrUpdateAsync(
+                    WaitUntil.Completed,
+                    SessionRecording.GenerateAssetName("firewall-"),
+                    firewallData
+                );
             _firewall = firewallLro.Value;
             _firewallIdentifier = _firewall.Id;
 
@@ -81,9 +113,13 @@ namespace Azure.ResourceManager.Network.Tests
         public async Task TestSetUp()
         {
             var client = GetArmClient();
-            _resourceGroup = await client.GetResourceGroupResource(_resourceGroupIdentifier).GetAsync();
+            _resourceGroup = await client
+                .GetResourceGroupResource(_resourceGroupIdentifier)
+                .GetAsync();
             _network = await _resourceGroup.GetVirtualNetworks().GetAsync(_networkIdentifier.Name);
-            _publicIPAddress = await _resourceGroup.GetPublicIPAddresses().GetAsync(_publicIPAddressIdentifier.Name);
+            _publicIPAddress = await _resourceGroup
+                .GetPublicIPAddresses()
+                .GetAsync(_publicIPAddressIdentifier.Name);
             _firewall = await _resourceGroup.GetAzureFirewalls().GetAsync(_firewallIdentifier.Name);
         }
 
@@ -95,7 +131,129 @@ namespace Azure.ResourceManager.Network.Tests
             string FirewallPolicyName = Recording.GenerateAssetName("policy-");
             FirewallPolicyData data = new FirewallPolicyData();
             data.Location = AzureLocation.WestUS2;
-            await _resourceGroup.GetFirewallPolicies().CreateOrUpdateAsync(WaitUntil.Completed, FirewallPolicyName, data);
+            await _resourceGroup
+                .GetFirewallPolicies()
+                .CreateOrUpdateAsync(WaitUntil.Completed, FirewallPolicyName, data);
+        }
+
+        [Test]
+        [RecordedTest]
+        public async Task CreateAndDeployDraft()
+        {
+            var policyName = Recording.GenerateAssetName("policy-");
+            var policyData = new FirewallPolicyData();
+            policyData.SkuTier = FirewallPolicySkuTier.Standard;
+            policyData.Location = AzureLocation.WestUS2;
+            policyData.ThreatIntelMode = AzureFirewallThreatIntelMode.Alert;
+            var policy = await _resourceGroup
+                .GetFirewallPolicies()
+                .CreateOrUpdateAsync(WaitUntil.Completed, policyName, policyData);
+            Assert.AreEqual(FirewallPolicySkuTier.Standard, policy.Value.Data.Sku);
+            Assert.AreEqual(AzureFirewallThreatIntelMode.Alert, policy.Value.Data.ThreatIntelMode);
+            var rcg = new FirewallPolicyRuleCollectionGroupData()
+            {
+                Priority = 110,
+                RuleCollections =
+                {
+                    new FirewallPolicyFilterRuleCollectionInfo()
+                    {
+                        ActionType = FirewallPolicyFilterRuleCollectionActionType.Deny,
+                        Rules =
+                        {
+                            new ApplicationRule()
+                            {
+                                SourceAddresses = { "216.58.216.164", "10.0.0.0/24" },
+                                Protocols =
+                                {
+                                    new FirewallPolicyRuleApplicationProtocol()
+                                    {
+                                        ProtocolType =
+                                            FirewallPolicyRuleApplicationProtocolType.Https,
+                                        Port = 443,
+                                    }
+                                },
+                                WebCategories = { "Hacking" },
+                                Name = "rule1",
+                                Description = "Deny inbound rule",
+                            }
+                        },
+                        Name = "RuleCollectionGroup",
+                    }
+                }
+            };
+            var policyResource = await _resourceGroup.GetFirewallPolicies().GetAsync(policyName);
+            var client = GetArmClient();
+            var rcgResponse = await client
+                .GetFirewallPolicyRuleCollectionGroupResource(policyResource.Value.Id)
+                .UpdateAsync(WaitUntil.Completed, rcg);
+            Assert.AreEqual(
+                rcgResponse.Value.Data.ProvisioningState,
+                NetworkProvisioningState.Succeeded
+            );
+            var policyDraft = new FirewallPolicyDraftData()
+            {
+                ThreatIntelMode = AzureFirewallThreatIntelMode.Deny,
+            };
+            var draftResource = new FirewallPolicyDraftResource(client, policyDraft);
+            await draftResource.CreateOrUpdateAsync(WaitUntil.Completed, policyDraft);
+            var rcgPolicyDraft = new FirewallPolicyRuleCollectionGroupDraftData()
+            {
+                Priority = 111,
+                RuleCollections =
+                {
+                    new FirewallPolicyFilterRuleCollectionInfo()
+                    {
+                        ActionType = FirewallPolicyFilterRuleCollectionActionType.Deny,
+                        Rules =
+                        {
+                            new ApplicationRule()
+                            {
+                                SourceAddresses = { "*" },
+                                Protocols =
+                                {
+                                    new FirewallPolicyRuleApplicationProtocol()
+                                    {
+                                        ProtocolType =
+                                            FirewallPolicyRuleApplicationProtocolType.Https,
+                                        Port = 443,
+                                    }
+                                },
+                                WebCategories = { "Hacking" },
+                                Name = "rule1",
+                                Description = "Deny inbound rule",
+                            }
+                        },
+                        Name = "RuleCollectionGroup",
+                    }
+                }
+            };
+            var rcgDraftResource = new FirewallPolicyRuleCollectionGroupDraftResource(
+                client,
+                rcgPolicyDraft
+            );
+            await rcgDraftResource.CreateOrUpdateAsync(WaitUntil.Completed, rcgPolicyDraft);
+            await policyResource.Value.DeployFirewallPolicyDeploymentAsync(WaitUntil.Completed);
+            var updatedPolicy = await _resourceGroup.GetFirewallPolicies().GetAsync(policyName);
+            Assert.AreEqual(
+                updatedPolicy.Value.Data.ThreatIntelMode,
+                AzureFirewallThreatIntelMode.Deny
+            );
+            Assert.AreEqual(
+                updatedPolicy.Value.Data.ProvisioningState,
+                NetworkProvisioningState.Succeeded
+            );
+            var updatedRcg = await updatedPolicy.Value.GetFirewallPolicyRuleCollectionGroupAsync(
+                "RuleCollectionGroup"
+            );
+            Assert.AreEqual(
+                updatedRcg.Value.Data.ProvisioningState,
+                NetworkProvisioningState.Succeeded
+            );
+            Assert.AreEqual(updatedRcg.Value.Data.Priority, 111);
+            var ruleCollection = (FirewallPolicyFilterRuleCollectionInfo)
+                updatedRcg.Value.Data.RuleCollections[0];
+            var rule = (ApplicationRule)ruleCollection.Rules[0];
+            Assert.AreEqual(rule.SourceAddresses[0], "*");
         }
     }
 }
