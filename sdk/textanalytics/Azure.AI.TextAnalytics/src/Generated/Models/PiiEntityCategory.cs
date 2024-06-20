4dcf91165eaa8be0dffe44670b6be67a774b60c8
azure-sdk <azuresdk@microsoft.com>
2024-06-20 09:32:27 +0000

    Update SDK code st_wo_9

--- sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/PiiEntityCategory.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/src/Generated/Models/PiiEntityCategory.cs
@@ -557,7 +557,7 @@ namespace Azure.AI.TextAnalytics
 
         /// <inheritdoc />
         [EditorBrowsable(EditorBrowsableState.Never)]
-        public override int GetHashCode() => _value?.GetHashCode() ?? 0;
+        public override int GetHashCode() => StringComparer.InvariantCultureIgnoreCase.GetHashCode(_value);
         /// <inheritdoc />
         public override string ToString() => _value;
     }
