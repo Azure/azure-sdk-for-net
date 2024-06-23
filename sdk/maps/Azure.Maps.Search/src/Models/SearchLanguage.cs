927c375eaf267c06c75d75ded55b92ffb9b127fb
jecmenicanikola <nikola.jecmenica@janeasystems.com>
2024-06-23 07:23:08 -0700

    Update sreach langugage class

--- sdk/maps/Azure.Maps.Search/src/Models/SearchLanguage.cs
+++ sdk/maps/Azure.Maps.Search/src/Models/SearchLanguage.cs
@@ -171,12 +171,6 @@ namespace Azure.Maps.Search
 
         /// <inheritdoc />
         [EditorBrowsable(EditorBrowsableState.Never)]
-        public override bool Equals(object obj) => obj is SearchLanguage other && Equals(other);
-        /// <inheritdoc />
-        public bool Equals(SearchLanguage other) => string.Equals(_value, other._value, StringComparison.InvariantCultureIgnoreCase);
-
-        /// <inheritdoc />
-        [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode() => _value?.GetHashCode() ?? 0;
         /// <inheritdoc />
         public override string ToString() => _value;
