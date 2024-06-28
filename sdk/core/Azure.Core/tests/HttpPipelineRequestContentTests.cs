c4ea7c931d509e2fc878bde00aa2734e613ff214
Madalyn Redding <mredding@microsoft.com>
2024-06-28 17:16:30 -0400

    tweak tests

--- sdk/core/Azure.Core/tests/HttpPipelineRequestContentTests.cs
+++ sdk/core/Azure.Core/tests/HttpPipelineRequestContentTests.cs
@@ -239,39 +239,5 @@ namespace Azure.Core.Tests
 
             CollectionAssert.AreEqual(expected.ToArray(), destination.ToArray());
         }
-
-        public class MockPersistableModel : IPersistableModel<MockPersistableModel>
-        {
-            public int IntValue { get; set; }
-
-            public string StringValue { get; set; }
-
-            public string SerializedValue { get; }
-
-            public MockPersistableModel(int intValue, string stringValue)
-            {
-                IntValue = intValue;
-                StringValue = stringValue;
-
-                dynamic json = BinaryData.FromString("{}").ToDynamicFromJson(JsonPropertyNames.CamelCase);
-                json.IntValue = IntValue;
-                json.StringValue = StringValue;
-                SerializedValue = json.ToString();
-            }
-
-            MockPersistableModel IPersistableModel<MockPersistableModel>.Create(BinaryData data, ModelReaderWriterOptions options)
-            {
-                dynamic json = data.ToDynamicFromJson(JsonPropertyNames.CamelCase);
-                return new MockPersistableModel(json.IntValue, json.StringValue);
-            }
-
-            string IPersistableModel<MockPersistableModel>.GetFormatFromOptions(ModelReaderWriterOptions options)
-                => "J";
-
-            BinaryData IPersistableModel<MockPersistableModel>.Write(ModelReaderWriterOptions options)
-            {
-                return BinaryData.FromString(SerializedValue);
-            }
-        }
     }
 }
