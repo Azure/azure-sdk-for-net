91126e36c7f6873d3e224dc8da0c887abe80eeea
Anne Thompson <annelo@microsoft.com>
2024-07-02 15:53:38 -0700

    add internal emitted files that generated code will use

---
+++ sdk/core/System.ClientModel/tests/TestFramework/Emitted/PageCollectionHelpers.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ClientModel;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ClientModel.Tests.Emitted;
+
+internal class PageCollectionHelpers
+{
+    public static PageCollection<T> Create<T>(PageEnumerator<T> enumerator)
+        => new EnumeratorPageCollection<T>(enumerator);
+
+    public static AsyncPageCollection<T> CreateAsync<T>(PageEnumerator<T> enumerator)
+        => new AsyncEnumeratorPageCollection<T>(enumerator);
+
+    public static IEnumerable<ClientResult> Create(PageResultEnumerator enumerator)
+    {
+        do
+        {
+            if (enumerator.Current is not null)
+            {
+                yield return enumerator.Current;
+            }
+        }
+        while (enumerator.MoveNext());
+    }
+
+    public static async IAsyncEnumerable<ClientResult> CreateAsync(PageResultEnumerator enumerator)
+    {
+        do
+        {
+            if (enumerator.Current is not null)
+            {
+                yield return enumerator.Current;
+            }
+        }
+        while (await enumerator.MoveNextAsync().ConfigureAwait(false));
+    }
+
+    private class EnumeratorPageCollection<T> : PageCollection<T>
+    {
+        private readonly PageEnumerator<T> _enumerator;
+
+        public EnumeratorPageCollection(PageEnumerator<T> enumerator)
+        {
+            _enumerator = enumerator;
+        }
+
+        protected override IEnumerator<PageResult<T>> GetEnumeratorCore()
+            => _enumerator;
+    }
+
+    private class AsyncEnumeratorPageCollection<T> : AsyncPageCollection<T>
+    {
+        private readonly PageEnumerator<T> _enumerator;
+
+        public AsyncEnumeratorPageCollection(PageEnumerator<T> enumerator)
+        {
+            _enumerator = enumerator;
+        }
+
+        protected override IAsyncEnumerator<PageResult<T>> GetAsyncEnumeratorCore(CancellationToken cancellationToken = default)
+            => _enumerator;
+    }
+}
---
+++ sdk/core/System.ClientModel/tests/TestFramework/Emitted/PageEnumerator.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ClientModel;
+using System.Collections.Generic;
+
+namespace ClientModel.Tests.Emitted;
+
+internal abstract class PageEnumerator<T> : PageResultEnumerator,
+    IAsyncEnumerator<PageResult<T>>,
+    IEnumerator<PageResult<T>>
+{
+    public abstract PageResult<T> GetPageFromResult(ClientResult result);
+
+    PageResult<T> IEnumerator<PageResult<T>>.Current
+    {
+        get
+        {
+            if (Current is null)
+            {
+                return default!;
+            }
+
+            return GetPageFromResult(Current);
+        }
+    }
+
+    PageResult<T> IAsyncEnumerator<PageResult<T>>.Current
+    {
+        get
+        {
+            if (Current is null)
+            {
+                return default!;
+            }
+
+            return GetPageFromResult(Current);
+        }
+    }
+}
