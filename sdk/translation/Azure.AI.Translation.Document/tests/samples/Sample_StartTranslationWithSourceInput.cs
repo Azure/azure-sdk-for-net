c48332091423fb262377acb077d18d2f4289a9b3
Hamshavathi Munibyraiah <v-hamunibyra@microsoft.com>
2024-07-03 16:10:52 -0700

    SouceInput options are made public. Tests are updated to access stoarge accounts using managed identity and no loger through SAS urls. Single document translation client can be created using endpoint alone, mainly for SDK to work against containers.

---
+++ sdk/translation/Azure.AI.Translation.Document/tests/samples/Sample_StartTranslationWithSourceInput.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using Azure.AI.Translation.Document.Tests;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.AI.Translation.Document.Samples
+{
+    public partial class DocumentTranslationSamples : DocumentTranslationLiveTestBase
+    {
+        [Test]
+        [SyncOnly]
+        public void StartTranslationwithSourceInput()
+        {
+#if SNIPPET
+            string endpoint = "<Document Translator Resource Endpoint>";
+            string apiKey = "<Document Translator Resource API Key>";
+#else
+            string endpoint = TestEnvironment.Endpoint;
+            string apiKey = TestEnvironment.ApiKey;
+#endif
+
+            var client = new DocumentTranslationClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
+
+            #region Snippet:StartTranslationwithSourceInput
+#if SNIPPET
+            Uri sourceUri = new Uri("<source SAS URI>");
+            Uri targetUri = new Uri("<target SAS URI>");
+#else
+            Uri sourceUri = CreateSourceContainer(oneTestDocuments);
+            Uri targetUri = CreateTargetContainer();
+#endif
+            //Creating a TranslationSource object with sourceURI, sourceLanguage, storageSource, DocumentFilterPrefix and DocumentFilterSuffix
+            TranslationSource translationSource = new TranslationSource(sourceUri, "en", "AzureBlob", "File", "txt");
+            var input = new DocumentTranslationInput(translationSource, targetUri, "fr");
+            DocumentTranslationOperation operation = client.StartTranslation(input);
+
+            TimeSpan pollingInterval = new(1000);
+
+            while (true)
+            {
+                operation.UpdateStatus();
+
+                Console.WriteLine($"Status: {operation.Status}");
+                Console.WriteLine($"Created on: {operation.CreatedOn}");
+                Console.WriteLine($"Last modified: {operation.LastModified}");
+                Console.WriteLine($"Total documents: {operation.DocumentsTotal}");
+                Console.WriteLine($"Succeeded: {operation.DocumentsSucceeded}");
+                Console.WriteLine($"Failed: {operation.DocumentsFailed}");
+                Console.WriteLine($"In Progress: {operation.DocumentsInProgress}");
+                Console.WriteLine($"Not started: {operation.DocumentsNotStarted}");
+
+                if (operation.HasCompleted)
+                {
+                    break;
+                }
+                else
+                {
+                    if (operation.GetRawResponse().Headers.TryGetValue("Retry-After", out string value))
+                    {
+                        pollingInterval = TimeSpan.FromSeconds(Convert.ToInt32(value));
+                    }
+                    Thread.Sleep(pollingInterval);
+                }
+            }
+
+            foreach (DocumentStatusResult document in operation.GetValues())
+            {
+                Console.WriteLine($"Document with Id: {document.Id}");
+                Console.WriteLine($"Status:{document.Status}");
+                if (document.Status == DocumentTranslationStatus.Succeeded)
+                {
+                    Console.WriteLine($"Translated Document Uri: {document.TranslatedDocumentUri}");
+                    Console.WriteLine($"Translated to language code: {document.TranslatedToLanguageCode}.");
+                    Console.WriteLine($"Document source Uri: {document.SourceDocumentUri}");
+                }
+                else
+                {
+                    Console.WriteLine($"Document source Uri: {document.SourceDocumentUri}");
+                    Console.WriteLine($"Error Code: {document.Error.Code}");
+                    Console.WriteLine($"Message: {document.Error.Message}");
+                }
+            }
+#endregion
+        }
+    }
+}
