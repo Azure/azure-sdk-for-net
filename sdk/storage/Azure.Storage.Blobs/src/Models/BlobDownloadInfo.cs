a3f6a5c6cbc7c70f27534310abd3f03dc6c0876e
Jocelyn Schreppler <jaschrep@microsoft.com>
2024-07-11 11:52:38 -0400

    disable new API for presenting CRC from structured message

--- sdk/storage/Azure.Storage.Blobs/src/Models/BlobDownloadInfo.cs
+++ sdk/storage/Azure.Storage.Blobs/src/Models/BlobDownloadInfo.cs
@@ -51,13 +51,14 @@ namespace Azure.Storage.Blobs.Models
         /// </summary>
         public BlobDownloadDetails Details { get; internal set; }
 
-        /// <summary>
-        /// Indicates some contents of <see cref="Details"/> are mixed into the response stream.
-        /// They will not be set until <see cref="Content"/> has been fully consumed. These details
-        /// will be extracted from the content stream by the library before the calling code can
-        /// encounter them.
-        /// </summary>
-        public bool ExpectTrailingDetails { get; internal set; }
+        // TODO enable in following PR
+        ///// <summary>
+        ///// Indicates some contents of <see cref="Details"/> are mixed into the response stream.
+        ///// They will not be set until <see cref="Content"/> has been fully consumed. These details
+        ///// will be extracted from the content stream by the library before the calling code can
+        ///// encounter them.
+        ///// </summary>
+        //public bool ExpectTrailingDetails { get; internal set; }
 
         /// <summary>
         /// Constructor.
