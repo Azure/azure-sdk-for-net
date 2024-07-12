7c12c98e38bdfa6df436da62a27b0cc229dca056
Christopher Scott <chriscott@hotmail.com>
2024-07-12 16:51:22 -0500

    wip

--- sdk/identity/Azure.Identity/src/ManagedIdentityClientOptions.cs
+++ sdk/identity/Azure.Identity/src/ManagedIdentityClientOptions.cs
@@ -22,6 +22,7 @@ namespace Azure.Identity
 
         public bool ExcludeTokenExchangeManagedIdentitySource { get; set; }
 
+        // TODO: revert before GA
         public bool EnableManagedIdentityLegacyBehavior { get; set; } = Environment.GetEnvironmentVariable("AZURE_IDENTITY_ENABLE_LEGACY_IMDS_BEHAVIOR") != null;
 
         public bool IsForceRefreshEnabled { get; set; }
