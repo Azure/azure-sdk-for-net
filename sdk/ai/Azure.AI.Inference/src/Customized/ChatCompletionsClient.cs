3cc12779798b91dfc1a8a21e2436e42df42097e8
Travis Angevine <trangevi@microsoft.com>
2024-07-12 14:27:22 -0700

    Remove unused usings
    Signed-off-by: Travis Angevine <trangevi@microsoft.com>

--- sdk/ai/Azure.AI.Inference/src/Customized/ChatCompletionsClient.cs
+++ sdk/ai/Azure.AI.Inference/src/Customized/ChatCompletionsClient.cs
@@ -3,9 +3,6 @@
 
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
