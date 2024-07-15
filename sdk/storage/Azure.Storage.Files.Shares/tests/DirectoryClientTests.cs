f86a69b2dc252391e03ba3dc031c19439ace1395
Sean McCullough <44180881+seanmcc-msft@users.noreply.github.com>
2024-07-15 17:02:59 -0500

    Reenabled live Audience tests (#44972)

--- sdk/storage/Azure.Storage.Files.Shares/tests/DirectoryClientTests.cs
+++ sdk/storage/Azure.Storage.Files.Shares/tests/DirectoryClientTests.cs
@@ -133,7 +133,6 @@ namespace Azure.Storage.Files.Shares.Tests
         }
 
         [RecordedTest]
-        [PlaybackOnly("https://github.com/Azure/azure-sdk-for-net/issues/44967")]
         public async Task Ctor_CustomAudience()
         {
             // Arrange
@@ -161,7 +160,6 @@ namespace Azure.Storage.Files.Shares.Tests
         }
 
         [RecordedTest]
-        [PlaybackOnly("https://github.com/Azure/azure-sdk-for-net/issues/44967")]
         public async Task Ctor_StorageAccountAudience()
         {
             // Arrange
