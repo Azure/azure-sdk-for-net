380ae0b205e0a61fe8b662e9b385bb7cc6659e70
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2024-07-18 22:14:18 +0800

    [AppService] Bump api version to 2023-12-01 (#43706)

--- sdk/websites/Azure.ResourceManager.AppService/src/Customization/RestOperations/WebAppsRestOperations.cs
+++ sdk/websites/Azure.ResourceManager.AppService/src/Customization/RestOperations/WebAppsRestOperations.cs
@@ -1,15 +1,12 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using Azure.ResourceManager.AppService.Models;
 using System.Text.Json;
-using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 using Azure.Core;
+using Azure.ResourceManager.AppService.Models;
 
 namespace Azure.ResourceManager.AppService
 {
