fdc2d2d96188787e0a5b8dd71c7270e257002e4c
Hamshavathi Munibyraiah <v-hamunibyra@microsoft.com>
2024-07-24 11:55:36 -0700

    Trying az login

--- sdk/translation/test-resources-post.ps1
+++ sdk/translation/test-resources-post.ps1
@@ -9,7 +9,8 @@ param (
     [hashtable] $DeploymentOutputs,
 	[string] $ResourceGroupName, # this is the resourceGroup name where the Translator resource is created
 	[string] $BaseName, # this is the Translator resource name
-	[string] $SubscriptionId # this is the SubscriptionId
+	[string] $TestApplicationId, # this is the TestApplicationId
+	[string] $TenantId, # this is the TenantId
 )
 
 if($DeploymentOutputs.ContainsKey('DOCUMENT_TRANSLATION_STORAGE_NAME')){
@@ -27,8 +28,8 @@ function Log($Message) {
 Log 'Starting sdk\translation\test-resources-post.ps1'
 
 Log 'Login to azure'
-# PowerShell cmd : az account set --subscription $SubscriptionId
-az account set --subscription $SubscriptionId
+# PowerShell cmd : az login --service-principal -u $TestApplicationId --tenant $TenantId --allow-no-subscriptions --federated-token $env:ARM_OIDC_TOKEN
+az login --service-principal -u $TestApplicationId --tenant $TenantId --allow-no-subscriptions # --federated-token $env:ARM_OIDC_TOKEN
 
 Log 'Enable Managed identity on the Translator resource'
 # PowerShell cmd : az cognitiveservices account identity assign --name <TranslatorResourceName> --resource-group <ResourceGroupName>
