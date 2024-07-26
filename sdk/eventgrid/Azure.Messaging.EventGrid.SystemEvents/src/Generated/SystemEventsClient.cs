a3fbdf28889b2f1e992113ec974ee6d38d4fb371
azure-sdk <azuresdk@microsoft.com>
2024-07-26 02:39:31 +0000

    Update SDK code de_ex_3

--- sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/SystemEventsClient.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid.SystemEvents/src/Generated/SystemEventsClient.cs
@@ -12,10 +12,11 @@ using Azure.Core.Pipeline;
 namespace Azure.Messaging.EventGrid.SystemEvents
 {
     // Data plane generated client.
-    /// <summary> The SystemEvents service client. </summary>
+    /// <summary> Azure Messaging EventGrid SystemEvents. </summary>
     internal partial class SystemEventsClient
     {
         private readonly HttpPipeline _pipeline;
+        private readonly Uri _endpoint;
 
         /// <summary> The ClientDiagnostics is used to provide tracing support for the client library. </summary>
         internal ClientDiagnostics ClientDiagnostics { get; }
@@ -23,19 +24,30 @@ namespace Azure.Messaging.EventGrid.SystemEvents
         /// <summary> The HTTP pipeline for sending and receiving REST requests and responses. </summary>
         public virtual HttpPipeline Pipeline => _pipeline;
 
-        /// <summary> Initializes a new instance of SystemEventsClient. </summary>
-        public SystemEventsClient() : this(new SystemEventsClientOptions())
+        /// <summary> Initializes a new instance of SystemEventsClient for mocking. </summary>
+        protected SystemEventsClient()
         {
         }
 
         /// <summary> Initializes a new instance of SystemEventsClient. </summary>
-        /// <param name="options"> The options for configuring the client. </param>
-        public SystemEventsClient(SystemEventsClientOptions options)
+        /// <param name="endpoint"> The <see cref="Uri"/> to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="endpoint"/> is null. </exception>
+        public SystemEventsClient(Uri endpoint) : this(endpoint, new SystemEventsClientOptions())
         {
+        }
+
+        /// <summary> Initializes a new instance of SystemEventsClient. </summary>
+        /// <param name="endpoint"> The <see cref="Uri"/> to use. </param>
+        /// <param name="options"> The options for configuring the client. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="endpoint"/> is null. </exception>
+        public SystemEventsClient(Uri endpoint, SystemEventsClientOptions options)
+        {
+            Argument.AssertNotNull(endpoint, nameof(endpoint));
             options ??= new SystemEventsClientOptions();
 
             ClientDiagnostics = new ClientDiagnostics(options, true);
             _pipeline = HttpPipelineBuilder.Build(options, Array.Empty<HttpPipelinePolicy>(), Array.Empty<HttpPipelinePolicy>(), new ResponseClassifier());
+            _endpoint = endpoint;
         }
     }
 }
