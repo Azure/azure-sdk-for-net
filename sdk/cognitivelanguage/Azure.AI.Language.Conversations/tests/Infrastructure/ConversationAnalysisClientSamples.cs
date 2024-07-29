4d8764271f13d5c3b52eacd446bbbdd3b08ba376
Bidisha Chakraborty <29803402+bidisha-c@users.noreply.github.com>
2024-07-29 16:38:35 -0700

    SDK For Azure.AI.Language.Conversations (#45031)
    * Remove older client code
    
    * Conversations - Version 2.0.0-beta-1 generated code
    
    * update changelog
    
    * Fix tests
    
    * fix readme
    
    * Rename client
    
    * regen
    
    * fix readme
    
    * fix samples
    
    * fix compile
    
    * fix test
    
    * Update api
    
    * typo fix
    
    * typo
    
    * Update doc
    
    * Update assets.json
    
    * Fix broken link
    
    * fix
    
    * update
    
    * Update client
    
    * Update snippets
    
    * Updates
    
    * update for clu
    
    * update tests
    
    * update readme
    
    * realign
    
    * Fix incorrect sample
    
    * Update doc
    
    * update samples
    
    * update variables
    
    * comments
    
    * Update major version and enable back compat check
    
    * Missed tests
    
    * update tests
    
    * remove async
    
    * style
    
    * disable warnings for deprecated code
    
    * Fix comments
    
    * update obsolete message
    
    * Update README
    
    * Slight change in verbiage
    
    * update docs
    
    * update readme
    
    * update
    
    * minor update
    
    * small update
    
    * update readme
    
    * change docs
    
    * Update tsp-config
    
    * missed update
    
    * Update README
    
    ---------
    
    Co-authored-by: Bidisha Chakraborty <bidishac@microsoft.com>

--- sdk/cognitivelanguage/Azure.AI.Language.Conversations/tests/Infrastructure/ConversationAnalysisClientSamples.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.Conversations/tests/Infrastructure/ConversationAnalysisClientSamples.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Threading.Tasks;
-using Azure.AI.Language.Conversations.Authoring;
-using Azure.Core.TestFramework;
-
 namespace Azure.AI.Language.Conversations.Tests.Samples
 {
     public partial class ConversationAnalysisClientSamples : ConversationAnalysisTestBase<ConversationAnalysisClient>
@@ -12,25 +8,6 @@ namespace Azure.AI.Language.Conversations.Tests.Samples
         public ConversationAnalysisClientSamples(bool isAsync, ConversationsClientOptions.ServiceVersion serviceVersion)
             : base(isAsync, serviceVersion, null /* RecordedTestMode.Record /* to record */)
         {
-        }
-
-        protected ConversationAuthoringClient ProjectsClient { get; private set; }
-
-        public override async Task StartTestRecordingAsync()
-        {
-            await base.StartTestRecordingAsync();
-
-            ProjectsClient = CreateClient<ConversationAuthoringClient>(
-                TestEnvironment.Endpoint,
-                new AzureKeyCredential(TestEnvironment.ApiKey),
-                InstrumentClientOptions(
-                    new ConversationsClientOptions(ServiceVersion)
-                    {
-                        Retry =
-                        {
-                            MaxRetries = 10,
-                        },
-                    }));
         }
     }
 }
