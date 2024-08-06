a76f28b9f039f6f1dd4778cd4c29cc043737ca3d
Han Chiang <jimmy4834@hotmail.com>
2024-08-06 09:33:01 +0000

    Update customization

--- sdk/face/Azure.AI.Vision.Face/src/Custom/FaceSessionClient.cs
+++ sdk/face/Azure.AI.Vision.Face/src/Custom/FaceSessionClient.cs
@@ -13,10 +13,10 @@ namespace Azure.AI.Vision.Face
     public partial class FaceSessionClient
     {
         /// <summary> Create a new liveness session with verify. Provide the verify image during session creation. </summary>
-        /// <param name="createLivenessSessionContent"> Parameters for liveness with verify session creation. </param>
+        /// <param name="jsonContent"> Parameters for liveness with verify session creation. </param>
         /// <param name="verifyImage"> Image binary data for verify image, can be provided as session creation time or during the /detectLivenessWithVerify/singleModal  </param>///
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="createLivenessSessionContent"/> is null. </exception>
+        /// <exception cref="ArgumentNullException"> <paramref name="jsonContent"/> is null. </exception>
         /// <remarks>
         /// A session is best for client device scenarios where developers want to authorize a client device to perform only a liveness detection without granting full access to their resource. Created sessions have a limited life span and only authorize clients to perform the desired action before access is expired.
         ///
@@ -39,24 +39,24 @@ namespace Azure.AI.Vision.Face
         /// &gt; Extra measures should be taken to validate that the client is sending the expected VerifyImage.
         /// </remarks>
         [ForwardsClientCalls]
-        public virtual async Task<Response<CreateLivenessWithVerifySessionResult>> CreateLivenessWithVerifySessionAsync(CreateLivenessSessionContent createLivenessSessionContent, Stream verifyImage, CancellationToken cancellationToken = default)
+        public virtual async Task<Response<CreateLivenessWithVerifySessionResult>> CreateLivenessWithVerifySessionAsync(CreateLivenessWithVerifySessionJsonContent jsonContent, Stream verifyImage, CancellationToken cancellationToken = default)
         {
-            Argument.AssertNotNull(createLivenessSessionContent, nameof(createLivenessSessionContent));
+            Argument.AssertNotNull(jsonContent, nameof(jsonContent));
 
             if (verifyImage == null)
             {
-                return await CreateLivenessWithVerifySessionAsync(createLivenessSessionContent, cancellationToken).ConfigureAwait(false);
+                return await CreateLivenessWithVerifySessionAsync(jsonContent, cancellationToken).ConfigureAwait(false);
             }
 
-            var createLivenessWithVerifySessionContent = new CreateLivenessWithVerifySessionContent(createLivenessSessionContent, verifyImage);
-            return await CreateLivenessWithVerifySessionWithVerifyImageAsync(createLivenessWithVerifySessionContent, cancellationToken).ConfigureAwait(false);
+            CreateLivenessWithVerifySessionMultipartContent multipartContent = new CreateLivenessWithVerifySessionMultipartContent(jsonContent, verifyImage);
+            return await CreateLivenessWithVerifySessionWithVerifyImageAsync(multipartContent, cancellationToken).ConfigureAwait(false);
         }
 
         /// <summary> Create a new liveness session with verify. Provide the verify image during session creation. </summary>
-        /// <param name="createLivenessSessionContent"> Parameters for liveness with verify session creation. </param>
+        /// <param name="jsonContent"> Parameters for liveness with verify session creation. </param>
         /// <param name="verifyImage"> Image binary data for verify image, can be provided as session creation time or during the /detectLivenessWithVerify/singleModal  </param>///
         /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="createLivenessSessionContent"/> is null. </exception>
+        /// <exception cref="ArgumentNullException"> <paramref name="jsonContent"/> is null. </exception>
         /// <remarks>
         /// A session is best for client device scenarios where developers want to authorize a client device to perform only a liveness detection without granting full access to their resource. Created sessions have a limited life span and only authorize clients to perform the desired action before access is expired.
         ///
@@ -79,17 +79,17 @@ namespace Azure.AI.Vision.Face
         /// &gt; Extra measures should be taken to validate that the client is sending the expected VerifyImage.
         /// </remarks>
         [ForwardsClientCalls]
-        public virtual Response<CreateLivenessWithVerifySessionResult> CreateLivenessWithVerifySession(CreateLivenessSessionContent createLivenessSessionContent, Stream verifyImage, CancellationToken cancellationToken = default)
+        public virtual Response<CreateLivenessWithVerifySessionResult> CreateLivenessWithVerifySession(CreateLivenessWithVerifySessionJsonContent jsonContent, Stream verifyImage, CancellationToken cancellationToken = default)
         {
-            Argument.AssertNotNull(createLivenessSessionContent, nameof(createLivenessSessionContent));
+            Argument.AssertNotNull(jsonContent, nameof(jsonContent));
 
             if (verifyImage == null)
             {
-                return CreateLivenessWithVerifySession(createLivenessSessionContent, cancellationToken);
+                return CreateLivenessWithVerifySession(jsonContent, cancellationToken);
             }
 
-            var createLivenessWithVerifySessionContent = new CreateLivenessWithVerifySessionContent(createLivenessSessionContent, verifyImage);
-            return CreateLivenessWithVerifySessionWithVerifyImage(createLivenessWithVerifySessionContent, cancellationToken);
+            CreateLivenessWithVerifySessionMultipartContent multipartContent = new CreateLivenessWithVerifySessionMultipartContent(jsonContent, verifyImage);
+            return CreateLivenessWithVerifySessionWithVerifyImage(multipartContent, cancellationToken);
         }
     }
 }
