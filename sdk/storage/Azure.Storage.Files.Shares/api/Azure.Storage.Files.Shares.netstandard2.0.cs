80ef26d70e324dd738c35a561dfba74adae85f8e
Sean McCullough <seanmcc@microsoft.com>
2024-08-06 21:20:33 -0500

    Added STG 96 service version

--- sdk/storage/Azure.Storage.Files.Shares/api/Azure.Storage.Files.Shares.netstandard2.0.cs
+++ sdk/storage/Azure.Storage.Files.Shares/api/Azure.Storage.Files.Shares.netstandard2.0.cs
@@ -147,6 +147,7 @@ namespace Azure.Storage.Files.Shares
             V2024_05_04 = 22,
             V2024_08_04 = 23,
             V2024_11_04 = 24,
+            V2025_01_05 = 25,
         }
     }
     public partial class ShareDirectoryClient
