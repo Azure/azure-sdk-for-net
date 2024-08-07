bc278f4265d872c7e726f37a448c2942b9d4fa4a
Chenyang Liu <chenyl@microsoft.com>
2024-08-07 14:31:01 +0800

    Initial Web Job (Function binding) SDK for Web PubSub for Socket.IO (#45050)
    * Init socketio binding
    
    * Update code snippets

---
+++ sdk/webpubsub/Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO/tests/Common/TestExtensionConfig.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Azure.Messaging.WebPubSub;
+using Microsoft.Azure.WebJobs.Description;
+using Microsoft.Azure.WebJobs.Host.Config;
+using Moq;
+
+namespace Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO.Tests
+{
+    public class TestExtensionConfig : IExtensionConfigProvider
+    {
+        public void Initialize(ExtensionConfigContext context)
+        {
+            context.AddBindingRule<BindingDataAttribute>().
+                BindToInput(attr => attr.ToBeAutoResolve);
+
+            var rule1 = context.AddBindingRule<SocketIOAttribute>();
+            rule1.BindToCollector(CreateTestCollector);
+        }
+
+        private object Mock<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IAsyncCollector<SocketIOAction> CreateTestCollector(SocketIOAttribute attribute)
+        {
+            var service = new Mock<WebPubSubServiceClient>();
+            //service.Setup(x => x.SendToAll(It.IsAny<RequestContent>(), It.IsAny<string>(), null, It.IsAny<CancellationToken>()))
+            //    .Returns(new MockResponse(200));
+            var store = new SocketLifetimeStore();
+            return new WebPubSubForSocketIOAsyncCollector(new WebPubSubForSocketIOService(service.Object), store);
+        }
+
+        [Binding]
+        private sealed class BindingDataAttribute : Attribute
+        {
+            public BindingDataAttribute(string toBeAutoResolve)
+            {
+                ToBeAutoResolve = toBeAutoResolve;
+            }
+
+            [AutoResolve]
+            public string ToBeAutoResolve { get; set; }
+        }
+    }
+}
