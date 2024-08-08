d855acea01231a1cc797a4c2170e72acfc956feb
m-nash <64171366+m-nash@users.noreply.github.com>
2024-08-07 17:11:52 -0700

    Initial draft

--- sdk/core/System.ClientModel/src/ModelReaderWriter/ModelReaderWriterOptions.cs
+++ sdk/core/System.ClientModel/src/ModelReaderWriter/ModelReaderWriterOptions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+
 namespace System.ClientModel.Primitives;
 
 /// <summary>
@@ -8,6 +10,13 @@ namespace System.ClientModel.Primitives;
 /// </summary>
 public class ModelReaderWriterOptions
 {
+    private Dictionary<Type, object>? _proxies;
+    /// <summary>
+    /// .
+    /// </summary>
+    private IReadOnlyDictionary<Type, object> Proxies
+        => _proxies ??= new Dictionary<Type, object>();
+
     private static ModelReaderWriterOptions? s_jsonOptions;
     /// <summary>
     /// Default options for writing models into JSON format.
@@ -24,7 +33,7 @@ public class ModelReaderWriterOptions
     /// Initializes a new instance of <see cref="ModelReaderWriterOptions"/>.
     /// </summary>
     /// <param name="format">The format to read and write models.</param>
-    public ModelReaderWriterOptions (string format)
+    public ModelReaderWriterOptions(string format)
     {
         Format = format;
     }
@@ -33,4 +42,47 @@ public class ModelReaderWriterOptions
     /// Gets the format to read and write the model.
     /// </summary>
     public string Format { get; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public void AddProxy<T>(Type type, IPersistableModel<T> proxy)
+    {
+        if (_proxies is null)
+        {
+            _proxies = new Dictionary<Type, object>();
+        }
+
+        //multiple of same type?
+        _proxies.Add(type, proxy);
+    }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    public object? WriteContext { get; private set; }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    public IPersistableModel<T> GetPersistableInterface<T>(IPersistableModel<T> model)
+    {
+        WriteContext = model;
+        return Proxies.TryGetValue(typeof(T), out var proxy) && proxy is IPersistableModel<T> proxyAsT ? proxyAsT : model;
+    }
+
+    /// <summary>
+    /// .
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    public IJsonModel<T> GetJsonInterface<T>(IJsonModel<T> model)
+    {
+        WriteContext = model;
+        return Proxies.TryGetValue(typeof(T), out var proxy) && proxy is IJsonModel<T> proxyAsT ? proxyAsT : model;
+    }
 }
