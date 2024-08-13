e9afe2221d16bdc6a0bf2c418c1bd9c48ee13ee6
Chenyang Liu <chenyl@microsoft.com>
2024-08-13 17:18:23 +0800

    fix typo

--- sdk/webpubsub/Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO/src/Trigger/WebPubSubForSocketIOTriggerBinding.cs
+++ sdk/webpubsub/Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO/src/Trigger/WebPubSubForSocketIOTriggerBinding.cs
@@ -63,12 +63,12 @@ namespace Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO
                 {
                     if (triggerEvent.Request is not SocketIOMessageRequest eventReq)
                     {
-                        throw new InvalidDataException($"Expect incomming event to be {nameof(SocketIOMessageRequest)}, but it's {triggerEvent.Request.GetType()}");
+                        throw new InvalidDataException($"Expect incoming event to be {nameof(SocketIOMessageRequest)}, but it's {triggerEvent.Request.GetType()}");
                     }
 
                     if (eventReq.Parameters == null || eventReq.Parameters.Count != _attribute.ParameterNames.Length)
                     {
-                        throw new ArgumentException($"Parameter length of Incomming data dismatch. Expected to be {_attribute.ParameterNames.Length}, but actually {eventReq.Parameters.Count}. Update the {nameof(SocketIOTriggerAttribute.ParameterNames)} to match the actual data");
+                        throw new ArgumentException($"Parameter length of incoming data dismatch. Expected to be {_attribute.ParameterNames.Length}, but actually {eventReq.Parameters.Count}. Update the {nameof(SocketIOTriggerAttribute.ParameterNames)} to match the actual data");
                     }
 
                     // Add binding data for arguments
