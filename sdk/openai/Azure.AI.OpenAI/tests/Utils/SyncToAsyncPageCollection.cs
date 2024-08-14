3276559de939908bf54f3545fa398ee7254ae6ab
Ralph El Hage <ralphe@microsoft.com>
2024-08-14 14:22:31 -0700

    Fix bug in adapter. Clean up how to add custom OpenAI assembly, and support for custom Azure.Core assembly.

--- sdk/openai/Azure.AI.OpenAI/tests/Utils/SyncToAsyncPageCollection.cs
+++ sdk/openai/Azure.AI.OpenAI/tests/Utils/SyncToAsyncPageCollection.cs
@@ -10,59 +10,64 @@ using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
 using System.Threading;
 
-namespace Azure.AI.OpenAI.Tests.Utils
+namespace Azure.AI.OpenAI.Tests.Utils;
+
+/// <summary>
+/// An adapter to make a <see cref="PageableCollection{T}"/> look and work like a <see cref="AsyncPageableCollection{T}"/>. This
+/// simplifies writing test cases.
+/// </summary>
+/// <typeparam name="T">The type of the items the enumerator returns.</typeparam>
+public class SyncToAsyncPageCollection<T> : AsyncPageCollection<T>
 {
+    private PageCollection<T>? _syncCollection;
+    private Exception? _ex;
+
     /// <summary>
-    /// An adapter to make a <see cref="PageableCollection{T}"/> look and work like a <see cref="AsyncPageableCollection{T}"/>. This
-    /// simplifies writing test cases.
+    /// Creates a new instance.
     /// </summary>
-    /// <typeparam name="T">The type of the items the enumerator returns.</typeparam>
-    public class SyncToAsyncPageCollection<T> : AsyncPageCollection<T>
+    /// <param name="syncCollection">The synchronous collection to wrap.</param>
+    /// <exception cref="ArgumentNullException">If the collection was null.</exception>
+    public SyncToAsyncPageCollection(PageCollection<T> syncCollection)
     {
-        private PageCollection<T>? _syncCollection;
-        private Exception? _ex;
-        private PageResult<T>? _currentPage;
+        _syncCollection = syncCollection ?? throw new ArgumentNullException(nameof(syncCollection));
+    }
 
-        /// <summary>
-        /// Creates a new instance.
-        /// </summary>
-        /// <param name="syncCollection">The synchronous collection to wrap.</param>
-        /// <exception cref="ArgumentNullException">If the collection was null.</exception>
-        public SyncToAsyncPageCollection(PageCollection<T> syncCollection)
+    /// <summary>
+    /// Creates a new instance.
+    /// </summary>
+    /// <param name="ex">The exception to throw.</param>
+    /// <exception cref="ArgumentNullException">If the exception was null.</exception>
+    public SyncToAsyncPageCollection(Exception ex)
+    {
+        _ex = ex ?? throw new ArgumentNullException(nameof(ex));
+        _syncCollection = null;
+    }
+
+    /// <inheritdoc />
+    protected override Task<PageResult<T>> GetCurrentPageAsyncCore()
+    {
+        if (_ex != null)
         {
-            _syncCollection = syncCollection ?? throw new ArgumentNullException(nameof(syncCollection));
+            return Task.FromException<PageResult<T>>(_ex);
+        }
+        else
+        {
+            return Task.FromResult(_syncCollection!.GetCurrentPage());
+        }
+    }
+
+    /// <inheritdoc />
+    protected override async IAsyncEnumerator<PageResult<T>> GetAsyncEnumeratorCore(CancellationToken cancellationToken = default)
+    {
+        if (_ex != null)
+        {
+            ExceptionDispatchInfo.Capture(_ex).Throw();
         }
 
-        /// <summary>
-        /// Creates a new instance.
-        /// </summary>
-        /// <param name="ex">The exception to throw.</param>
-        /// <exception cref="ArgumentNullException">If the exception was null.</exception>
-        public SyncToAsyncPageCollection(Exception ex)
-        {
-            _ex = ex ?? throw new ArgumentNullException(nameof(ex));
-            _syncCollection = null;
-        }
-
-        /// <inheritdoc />
-        protected override Task<PageResult<T>> GetCurrentPageAsyncCore()
-            => Task.FromResult(_currentPage ?? throw new InvalidOperationException("Please call MoveNextAsync first."));
-
-        /// <inheritdoc />
-        protected override async IAsyncEnumerator<PageResult<T>> GetAsyncEnumeratorCore(CancellationToken cancellationToken = default)
+        foreach (PageResult<T> page in _syncCollection!)
         {
             await Task.Delay(0).ConfigureAwait(false);
-
-            if (_ex != null)
-            {
-                ExceptionDispatchInfo.Capture(_ex).Throw();
-            }
-
-            foreach (PageResult<T> page in _syncCollection!)
-            {
-                _currentPage = page;
-                yield return page;
-            }
+            yield return page;
         }
     }
 }
