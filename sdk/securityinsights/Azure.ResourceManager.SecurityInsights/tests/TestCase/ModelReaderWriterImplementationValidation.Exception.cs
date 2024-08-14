022f63006b2a3af69442fcebc50d0250ddea6e8a
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2024-08-14 14:04:53 +0800

    Create ModelReaderWriterImplementationValidation.Exception.cs

---
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/tests/TestCase/ModelReaderWriterImplementationValidation.Exception.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.ResourceManager.TestFramework
+{
+    public sealed partial class ModelReaderWriterImplementationValidation
+    {
+        public ModelReaderWriterImplementationValidation()
+        {
+            ExceptionList = new[]
+            {
+                "Azure.ResourceManager.SecurityInsights.Models.PackageModelCollectionGetAllOptions",
+                "Azure.ResourceManager.SecurityInsights.Models.ProductTemplateModelCollectionGetAllOptions",
+                "Azure.ResourceManager.SecurityInsights.Models.TemplateModelCollectionGetAllOptions"
+            };
+        }
+    }
+}
