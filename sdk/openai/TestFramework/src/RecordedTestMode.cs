1d344bd7f1d343bffeec3892e656a5bbc067559c
Ralph El Hage <ralphe@microsoft.com>
2024-08-15 23:46:06 -0700

    Move RecordedTestMode enum to top level namespace

--- sdk/openai/TestFramework/src/RecordedTestMode.cs
+++ sdk/openai/TestFramework/src/RecordedTestMode.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-namespace OpenAI.TestFramework.Recording;
+namespace OpenAI.TestFramework;
 
 /// <summary>
 /// The recording mode.
