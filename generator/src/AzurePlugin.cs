de5cb3f40cd3ec166e792127b4b3144361fb24a7
Wei Hu <huwe@microsoft.com>
2024-08-20 18:10:21 +0800

    init

---
+++ generator/src/AzurePlugin.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Generator;
+
+/// <summary>
+/// XXX
+/// </summary>
+public class AzurePlugin/* : ClientModelPlugin*/
+{
+}
