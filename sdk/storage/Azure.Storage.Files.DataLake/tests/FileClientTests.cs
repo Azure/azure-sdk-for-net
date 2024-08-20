8ead01df5b7392fbcd5dddf5b79ff8055e5a2d5b
nickliu-msft <nicliu@microsoft.com>
2024-08-20 14:25:51 -0400

    removed unused import

--- sdk/storage/Azure.Storage.Files.DataLake/tests/FileClientTests.cs
+++ sdk/storage/Azure.Storage.Files.DataLake/tests/FileClientTests.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Mime;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
