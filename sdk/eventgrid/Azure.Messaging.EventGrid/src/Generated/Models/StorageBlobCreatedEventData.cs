985aff3df6ca7b073d4a95cf153d485d19bcda87
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2024-08-21 12:57:37 -0700

    Prepare for release (#45565)

--- sdk/eventgrid/Azure.Messaging.EventGrid/src/Generated/Models/StorageBlobCreatedEventData.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid/src/Generated/Models/StorageBlobCreatedEventData.cs
@@ -11,8 +11,10 @@ namespace Azure.Messaging.EventGrid.SystemEvents
     public partial class StorageBlobCreatedEventData
     {
         /// <summary> Initializes a new instance of <see cref="StorageBlobCreatedEventData"/>. </summary>
-        internal StorageBlobCreatedEventData()
+        /// <param name="accessTier"> The current tier of the blob. </param>
+        internal StorageBlobCreatedEventData(StorageBlobAccessTier accessTier)
         {
+            AccessTier = accessTier;
         }
 
         /// <summary> Initializes a new instance of <see cref="StorageBlobCreatedEventData"/>. </summary>
@@ -24,11 +26,12 @@ namespace Azure.Messaging.EventGrid.SystemEvents
         /// <param name="contentLength"> The size of the blob in bytes. This is the same as what would be returned in the Content-Length header from the blob. </param>
         /// <param name="contentOffset"> The offset of the blob in bytes. </param>
         /// <param name="blobType"> The type of blob. </param>
+        /// <param name="accessTier"> The current tier of the blob. </param>
         /// <param name="url"> The path to the blob. </param>
         /// <param name="sequencer"> An opaque string value representing the logical sequence of events for any particular blob name. Users can use standard string comparison to understand the relative sequence of two events on the same blob name. </param>
         /// <param name="identity"> The identity of the requester that triggered this event. </param>
         /// <param name="storageDiagnostics"> For service use only. Diagnostic data occasionally included by the Azure Storage service. This property should be ignored by event consumers. </param>
-        internal StorageBlobCreatedEventData(string api, string clientRequestId, string requestId, string eTag, string contentType, long? contentLength, long? contentOffset, string blobType, string url, string sequencer, string identity, object storageDiagnostics)
+        internal StorageBlobCreatedEventData(string api, string clientRequestId, string requestId, string eTag, string contentType, long? contentLength, long? contentOffset, string blobType, StorageBlobAccessTier accessTier, string url, string sequencer, string identity, object storageDiagnostics)
         {
             Api = api;
             ClientRequestId = clientRequestId;
@@ -38,6 +41,7 @@ namespace Azure.Messaging.EventGrid.SystemEvents
             ContentLength = contentLength;
             ContentOffset = contentOffset;
             BlobType = blobType;
+            AccessTier = accessTier;
             Url = url;
             Sequencer = sequencer;
             Identity = identity;
@@ -60,6 +64,8 @@ namespace Azure.Messaging.EventGrid.SystemEvents
         public long? ContentOffset { get; }
         /// <summary> The type of blob. </summary>
         public string BlobType { get; }
+        /// <summary> The current tier of the blob. </summary>
+        public StorageBlobAccessTier AccessTier { get; }
         /// <summary> The path to the blob. </summary>
         public string Url { get; }
         /// <summary> An opaque string value representing the logical sequence of events for any particular blob name. Users can use standard string comparison to understand the relative sequence of two events on the same blob name. </summary>
