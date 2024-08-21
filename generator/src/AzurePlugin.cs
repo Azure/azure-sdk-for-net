6501f1216c765881b57babe7d64cef0e8c2a2a6e
Wei Hu <huwe@microsoft.com>
2024-08-21 15:36:43 +0800

    add MGC as dependency

--- generator/src/AzurePlugin.cs
+++ generator/src/AzurePlugin.cs
@@ -1,11 +1,24 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Microsoft.Generator.CSharp;
+using Microsoft.Generator.CSharp.ClientModel;
+using System.ComponentModel.Composition;
+
 namespace Azure.Generator;
 
 /// <summary>
 /// XXX
 /// </summary>
-public class AzurePlugin/* : ClientModelPlugin*/
+[Export(typeof(CodeModelPlugin))]
+[ExportMetadata("PluginName", nameof(AzurePlugin))]
+public class AzurePlugin : ClientModelPlugin
 {
+    [ImportingConstructor]
+    public AzurePlugin(GeneratorContext context) : base(context) { }
+
+    public override void Configure()
+    {
+        AddVisitor(new AzureVisitor());
+    }
 }
