16a351e3959bd0b08215838d67c67816324a756e
Paul Harrington <pharring@users.noreply.github.com>
2024-08-22 14:05:03 -0700

    Introduce overloaded constructor on AzureEventSourceListener (#45191)
    * Introduce overloaded constructor on AzureEventSourceListener taking a delegate without the formatted message.
    
    * Update CHANGELOG
    
    * Use new constructor in tests, where appropriate
    
    * Update public API

--- sdk/core/Azure.Core/CHANGELOG.md
+++ sdk/core/Azure.Core/CHANGELOG.md
@@ -10,6 +10,8 @@
 
 ### Other Changes
 
+- Added a new constructor on `AzureEventSourceListener` for callers that don't need the formatted message ([#45191](https://github.com/Azure/azure-sdk-for-net/pull/45191)).
+
 ## 1.42.0 (2024-08-01)
 
 ### Other Changes
--- sdk/core/Azure.Core/perf/EventSourceBenchmark.cs
+++ sdk/core/Azure.Core/perf/EventSourceBenchmark.cs
@@ -33,7 +33,7 @@ namespace Azure.Core.Perf
         [GlobalSetup]
         public void SetUp()
         {
-            _sourceListener = new AzureEventSourceListener((_, _) => { }, EventLevel.LogAlways);
+            _sourceListener = new AzureEventSourceListener(_ => { }, EventLevel.LogAlways);
             _eventSource = new EventSource();
             _sourceListener.EnableEvents(_eventSource, EventLevel.LogAlways);
 
@@ -84,6 +84,7 @@ namespace Azure.Core.Perf
                 string newValue = Sanitizer.SanitizeHeader(header.Name, header.Value);
                 stringBuilder.AppendLine(newValue);
             }
+
             return Encoding.UTF8.GetBytes(stringBuilder.ToString());
         }
 
