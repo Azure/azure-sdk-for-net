05dd60204824b1b04f6f1f684ccefe3366c97e77
Siddharth Singha Roy <ssingharoy@microsoft.com>
2024-08-23 13:02:39 +0530

    chore(): add playwright-testing to product slugs

--- eng/common/scripts/Test-SampleMetadata.ps1
+++ eng/common/scripts/Test-SampleMetadata.ps1
@@ -422,6 +422,7 @@ begin {
         "office-word",
         "office-yammer",
         "passport-azure-ad",
+        "playwright-testing",
         "power-apps",
         "power-automate",
         "power-bi",
