6816fc86b449882d8502899ec47ef6cf6688a7ee
Vishwesh Bankwar <Vishwesh.Bankwar@gmail.com>
2024-08-26 15:18:28 -0700

    Edit APi summary

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/CustomEvents/IApplicationInsightsEventLogger.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/CustomEvents/IApplicationInsightsEventLogger.cs
@@ -11,10 +11,16 @@ namespace Azure.Monitor.OpenTelemetry.Events
     public interface IApplicationInsightsEventLogger
     {
         /// <summary>
-        /// Tracks Application Insights custom events.
+        /// Tracks Application Insights Custom Events.
         /// </summary>
-        /// <param name="name">Name of the CustomEvent.</param>
-        /// <param name="attributes">Custom dimensions of the event.</param>
+        /// <param name="name">Name of the Custom Event.</param>
+        /// <param name="attributes">Custom dimensions of the Event.</param>
+        /// <remarks>
+        /// <para>
+        /// This API is intended for use only in specific cases where you need to log data as <a href="https://learn.microsoft.com/azure/azure-monitor/app/api-custom-events-metrics#trackevent">Custom Events</a>.
+        /// For general logging purposes, it is recommended to use <a href="https://learn.microsoft.com/dotnet/api/microsoft.extensions.logging.ilogger">ILogger</a>.
+        /// </para>
+        /// </remarks>
         public void TrackEvent(string name, IReadOnlyList<KeyValuePair<string, object?>>? attributes = null);
     }
 }
