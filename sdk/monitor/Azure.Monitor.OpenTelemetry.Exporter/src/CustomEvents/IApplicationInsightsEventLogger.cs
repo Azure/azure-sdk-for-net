9468266f24767733b9c3f20b73246223ca97138c
Vishwesh Bankwar <Vishwesh.Bankwar@gmail.com>
2024-08-27 09:14:51 -0700

    nt

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/CustomEvents/IApplicationInsightsEventLogger.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/CustomEvents/IApplicationInsightsEventLogger.cs
@@ -17,7 +17,7 @@ namespace Azure.Monitor.OpenTelemetry.Events
         /// <param name="attributes">Custom dimensions of the Event.</param>
         /// <remarks>
         /// <para>
-        /// This API is intended for use only in specific cases where you need to log data as <a href="https://learn.microsoft.com/azure/azure-monitor/app/api-custom-events-metrics#trackevent">Custom Events</a>.
+        /// This API is intended for use only in specific cases where you need to log data as <a href="https://learn.microsoft.com/azure/azure-monitor/app/api-custom-events-metrics#trackevent">Application Insights Custom Events</a>.
         /// For general logging purposes, it is recommended to use <a href="https://learn.microsoft.com/dotnet/api/microsoft.extensions.logging.ilogger">ILogger</a>.
         /// </para>
         /// </remarks>
