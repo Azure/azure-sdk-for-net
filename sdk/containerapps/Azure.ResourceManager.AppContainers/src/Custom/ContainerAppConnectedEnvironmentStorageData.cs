efb4a58ca9761e67e9d3fcdbeab5a5815c7448eb
Juliehzl <zuh@microsoft.com>
2024-08-28 12:02:50 +0800

    update

---
+++ sdk/containerapps/Azure.ResourceManager.AppContainers/src/Custom/ContainerAppConnectedEnvironmentStorageData.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using Azure.ResourceManager.AppContainers.Models;
+
+namespace Azure.ResourceManager.AppContainers
+{
+    public partial class ContainerAppConnectedEnvironmentStorageData
+    {
+        /// <summary>
+        /// Azure file properties for backward compatibility.
+        /// </summary>
+        public ContainerAppAzureFileProperties ConnectedEnvironmentStorageAzureFile
+        {
+            get => Properties is null ? default : Properties.AzureFile;
+            set
+            {
+                if (Properties is null)
+                    Properties = new ConnectedEnvironmentStorageProperties();
+                Properties.AzureFile = value;
+            }
+        }
+    }
+}
