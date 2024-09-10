abde8249f8a84c0d4e0fe7f6cbdefaef2926771f
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-10 17:06:45 +0800

    1st commit (#45876)

--- sdk/appcomplianceautomation/Azure.ResourceManager.AppComplianceAutomation/src/Generated/Models/CustomerResponsibility.Serialization.cs
+++ sdk/appcomplianceautomation/Azure.ResourceManager.AppComplianceAutomation/src/Generated/Models/CustomerResponsibility.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.AppComplianceAutomation.Models
 
         void IJsonModel<CustomerResponsibility>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<CustomerResponsibility>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(CustomerResponsibility)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (options.Format != "W" && Optional.IsDefined(ResponsibilityId))
             {
                 writer.WritePropertyName("responsibilityId"u8);
@@ -126,7 +134,6 @@ namespace Azure.ResourceManager.AppComplianceAutomation.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         CustomerResponsibility IJsonModel<CustomerResponsibility>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
