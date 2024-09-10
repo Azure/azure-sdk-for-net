630baf8eb2030f84d15d51be43946d3df037f897
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-10 13:11:11 +0800

    1st commit

--- sdk/appcomplianceautomation/Azure.ResourceManager.AppComplianceAutomation/src/Generated/Models/SyncCertRecordResult.Serialization.cs
+++ sdk/appcomplianceautomation/Azure.ResourceManager.AppComplianceAutomation/src/Generated/Models/SyncCertRecordResult.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.AppComplianceAutomation.Models
 
         void IJsonModel<SyncCertRecordResult>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<SyncCertRecordResult>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(SyncCertRecordResult)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(CertRecord))
             {
                 writer.WritePropertyName("certRecord"u8);
@@ -46,7 +54,6 @@ namespace Azure.ResourceManager.AppComplianceAutomation.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         SyncCertRecordResult IJsonModel<SyncCertRecordResult>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
