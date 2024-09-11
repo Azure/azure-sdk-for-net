4db239eeb567f9aed2725f832dbf038d651c6815
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2024-09-11 17:52:36 +0800

    Fix deserialize for `Type`

---
+++ sdk/machinelearningservices/Azure.ResourceManager.MachineLearning/src/Customized/MachineLearningEnvironmentContainerData.Serialization.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using Azure.Core;
+using Azure.ResourceManager.MachineLearning.Models;
+using Azure.ResourceManager.Models;
+
+namespace Azure.ResourceManager.MachineLearning
+{
+    // Fix the responsed error type "environments" to "Microsoft.MachineLearningServices/registries/environments"
+    // Issue:https://github.com/Azure/azure-sdk-for-net/issues/45884
+    [CodeGenSerialization(nameof(ResourceType), DeserializationValueHook = nameof(DeserializeTypeValue))]
+    public partial class MachineLearningEnvironmentContainerData
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void DeserializeTypeValue(JsonProperty property, ref ResourceType type)
+        {
+            var propVal = property.Value.GetString();
+            type = propVal == "environments" ? new ResourceType("Microsoft.MachineLearningServices/registries/environments") : new ResourceType(propVal);
+        }
+    }
+}
