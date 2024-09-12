48241761e348432bc8e259f73d7817034cb64039
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2024-09-12 17:13:35 +0800

    Override ValidateResourceId

---
+++ sdk/machinelearningservices/Azure.ResourceManager.MachineLearning/src/Customized/MachineLearningDataContainerResource.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace Azure.ResourceManager.MachineLearning
+{
+    // Override the "ValidateResourceId" method since the resource id is not correctly formatted
+    // Issue:https://github.com/Azure/azure-sdk-for-net/issues/45884
+    public partial class MachineLearningDataContainerResource
+    {
+        internal static void ValidateResourceId(ResourceIdentifier id)
+        {
+        }
+    }
+}
