365e284dc7925eccdb2ded5f7f04b628a8b2cd79
melina5656 <140602263+melina5656@users.noreply.github.com>
2024-09-12 15:15:55 +0800

    Enable WriteCore feature for appplatform

--- sdk/appplatform/Azure.ResourceManager.AppPlatform/src/Generated/Models/AppPlatformBuildResourceRequirements.Serialization.cs
+++ sdk/appplatform/Azure.ResourceManager.AppPlatform/src/Generated/Models/AppPlatformBuildResourceRequirements.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.AppPlatform.Models
 
         void IJsonModel<AppPlatformBuildResourceRequirements>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<AppPlatformBuildResourceRequirements>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(AppPlatformBuildResourceRequirements)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Cpu))
             {
                 writer.WritePropertyName("cpu"u8);
@@ -51,7 +59,6 @@ namespace Azure.ResourceManager.AppPlatform.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         AppPlatformBuildResourceRequirements IJsonModel<AppPlatformBuildResourceRequirements>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
--- sdk/appplatform/Azure.ResourceManager.AppPlatform/src/Generated/Models/AppPlatformUserSourceInfo.Serialization.cs
+++ sdk/appplatform/Azure.ResourceManager.AppPlatform/src/Generated/Models/AppPlatformUserSourceInfo.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.AppPlatform.Models
 
         void IJsonModel<AppPlatformUserSourceInfo>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<AppPlatformUserSourceInfo>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(AppPlatformUserSourceInfo)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("type"u8);
             writer.WriteStringValue(UserSourceInfoType);
             if (Optional.IsDefined(Version))
@@ -48,7 +56,6 @@ namespace Azure.ResourceManager.AppPlatform.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         AppPlatformUserSourceInfo IJsonModel<AppPlatformUserSourceInfo>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
