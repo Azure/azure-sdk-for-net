027e80902ea665ea108e8528de0ddf6ec695ae9a
Arcturus Zhang <dapzhang@microsoft.com>
2024-09-13 18:04:49 +0800

    add test cases

---
+++ eng/packages/http-client-csharp/generator/Azure.Generator/test/AzureTypeFactoryTests.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Azure.Generator.Tests.Common;
+using Azure.Generator.Tests.TestHelpers;
+using NUnit.Framework;
+using System;
+
+namespace Azure.Generator.Tests
+{
+    public class AzureTypeFactoryTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            MockHelpers.LoadMockPlugin();
+        }
+
+        [Test]
+        public void Uuid()
+        {
+            var input = InputFactory.Primitive.String("uuid", "Azure.Core.uuid");
+
+            var actual = AzureClientPlugin.Instance.TypeFactory.CreateCSharpType(input);
+
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual?.IsFrameworkType);
+            Assert.AreEqual(typeof(Guid), actual?.FrameworkType);
+        }
+
+        [Test]
+        public void AzureLocation()
+        {
+            var input = InputFactory.Primitive.String("azureLocation", "Azure.Core.azureLocation");
+
+            var actual = AzureClientPlugin.Instance.TypeFactory.CreateCSharpType(input);
+
+            Assert.IsNotNull(actual);
+            Assert.IsTrue(actual?.IsFrameworkType);
+            Assert.AreEqual(typeof(AzureLocation), actual?.FrameworkType);
+        }
+    }
+}
