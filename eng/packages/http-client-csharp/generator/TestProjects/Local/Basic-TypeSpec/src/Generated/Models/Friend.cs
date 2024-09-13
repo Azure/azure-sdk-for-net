13f86d84e6cac4273e193e5ba663fedfd3d34524
Arcturus Zhang <dapzhang@microsoft.com>
2024-09-13 11:54:03 +0800

    regen

--- eng/packages/http-client-csharp/generator/TestProjects/Local/Basic-TypeSpec/src/Generated/Models/Friend.cs
+++ eng/packages/http-client-csharp/generator/TestProjects/Local/Basic-TypeSpec/src/Generated/Models/Friend.cs
@@ -14,17 +14,17 @@ namespace BasicTypeSpec.Models
     public partial class Friend
     {
         /// <summary> Keeps track of any properties unknown to the library. </summary>
-        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+        private protected IDictionary<string, BinaryData> _additionalBinaryDataProperties;
 
         internal Friend(string name)
         {
             Name = name;
         }
 
-        internal Friend(string name, IDictionary<string, BinaryData> serializedAdditionalRawData)
+        internal Friend(string name, IDictionary<string, BinaryData> additionalBinaryDataProperties)
         {
             Name = name;
-            _serializedAdditionalRawData = serializedAdditionalRawData;
+            _additionalBinaryDataProperties = additionalBinaryDataProperties;
         }
 
         /// <summary> name of the NotFriend. </summary>
