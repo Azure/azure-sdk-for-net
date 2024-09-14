810595dca0e103568232896a59c7aa0e5e795b15
melina5656 <140602263+melina5656@users.noreply.github.com>
2024-09-14 13:44:02 +0800

    Enable WriteCore feature for azurestackhci

--- sdk/azurestackhci/Azure.ResourceManager.Hci/src/Generated/Models/HciEdgeDeviceProperties.Serialization.cs
+++ sdk/azurestackhci/Azure.ResourceManager.Hci/src/Generated/Models/HciEdgeDeviceProperties.Serialization.cs
@@ -20,13 +20,21 @@ namespace Azure.ResourceManager.Hci.Models
 
         void IJsonModel<HciEdgeDeviceProperties>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<HciEdgeDeviceProperties>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(HciEdgeDeviceProperties)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(DeviceConfiguration))
             {
                 writer.WritePropertyName("deviceConfiguration"u8);
@@ -52,7 +60,6 @@ namespace Azure.ResourceManager.Hci.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         HciEdgeDeviceProperties IJsonModel<HciEdgeDeviceProperties>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
