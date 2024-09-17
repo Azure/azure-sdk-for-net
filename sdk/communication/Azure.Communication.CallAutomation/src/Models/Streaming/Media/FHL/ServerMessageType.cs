5786b4eec2be5c678fe63bac80c4ae2c03e60877
Paresh Arvind Patil <papati@microsoft.com>
2024-09-16 20:41:30 -0700

    add contracts for bi=directional streaming

---
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/Streaming/Media/FHL/ServerMessageType.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Communication.CallAutomation.FHL
+{
+    /// <summary>
+    /// Messages sent from websocket server
+    /// </summary>
+    public enum ServerMessageType
+    {
+        /// <summary>
+        /// Audio data type
+        /// </summary>
+        AudioData,
+        /// <summary>
+        /// Mark metadata tyoe
+        /// </summary>
+        Mark,
+        /// <summary>
+        /// stop audio data type
+        /// </summary>
+        StopAudio
+    }
+}
---
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/Streaming/Media/FHL/ServerStreamingData.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Communication.CallAutomation.FHL
+{
+    public class ServerStreamingData
+    {
+        /// <summary>
+        /// The WebSocket data object which is then serialized to JSON format
+        /// </summary>
+        /// <param name="kind">Server message type kind</param>
+        /// <param name="audioData">Audio data</param>
+        public ServerStreamingData(ServerMessageType kind, AudioData audioData)
+        {
+            Kind = kind;
+            AudioData = audioData;
+        }
+
+        /// <summary>
+        /// Message that determines the format that of the subsequent messages
+        /// received through the WebSocket.
+        /// IsRequired = true
+        /// </summary>
+        public ServerMessageType Kind { get; }
+
+        /// <summary>
+        /// Audio Data which contains the audio data stream information
+        /// IsRequired = false
+        /// </summary>
+        public AudioData AudioData { get; }
+
+        /// <summary>
+        /// Mark determines end of stream of data
+        /// IsRequired = false
+        /// </summary>
+        public Mark Mark { get; set; }
+
+        /// <summary>
+        /// Stop playback and clear buffers
+        /// IsRequired = false
+        /// </summary>
+        public StopAudio StopAudio { get; set; }
+
+    }
+}
