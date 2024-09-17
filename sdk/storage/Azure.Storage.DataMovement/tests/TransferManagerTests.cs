2ebc8d2acb874ab80991860d816f08d49f4b0ec0
Jocelyn Schreppler <jaschrep@microsoft.com>
2024-09-17 15:33:18 -0400

    container test

--- sdk/storage/Azure.Storage.DataMovement/tests/TransferManagerTests.cs
+++ sdk/storage/Azure.Storage.DataMovement/tests/TransferManagerTests.cs
@@ -6,13 +6,16 @@ using System.Buffers;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.Pipeline;
 using Azure.Storage.DataMovement.Tests.Models;
+using Castle.Core.Resource;
 using Moq;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace Azure.Storage.DataMovement.Tests;
 
@@ -69,42 +72,15 @@ internal class TransferManagerTests
         Mock<JobBuilder> jobBuilder = new(ArrayPool<byte>.Shared, default, new ClientDiagnostics(ClientOptions.Default));
         Mock<TransferCheckpointer> checkpointer = new();
 
-        var resources = Enumerable.Range(0, items)
-            .Select(_ =>
-            {
-                Mock<StorageResourceItem> srcResource = new(MockBehavior.Strict);
-                Mock<StorageResourceItem> dstResource = new(MockBehavior.Strict);
+        var resources = Enumerable.Range(0, items).Select(_ =>
+        {
+            Mock<StorageResourceItem> srcResource = new(MockBehavior.Strict);
+            Mock<StorageResourceItem> dstResource = new(MockBehavior.Strict);
 
-                srcResource.Setup(r => r.Uri).Returns(srcUri);
-                dstResource.Setup(r => r.Uri).Returns(dstUri);
+            (srcResource, dstResource).BasicSetup(srcUri, dstUri, itemSize);
 
-                srcResource.SetupGet(r => r.ResourceId).Returns("Mock");
-                dstResource.SetupGet(r => r.ResourceId).Returns("Mock");
-
-                dstResource.SetupGet(r => r.TransferType).Returns(default(DataTransferOrder));
-                dstResource.SetupGet(r => r.MaxSupportedChunkSize).Returns(Constants.GB);
-
-                srcResource.Setup(r => r.GetPropertiesAsync(It.IsAny<CancellationToken>()))
-                    .Returns(Task.FromResult(new StorageResourceItemProperties(resourceLength: itemSize, default, default, default)));
-
-                srcResource.Setup(r => r.ReadStreamAsync(It.IsAny<long>(), It.IsAny<long?>(), It.IsAny<CancellationToken>()))
-                    .Returns<long, long?, CancellationToken>((position, length, cancellation)
-                        => Task.FromResult(new StorageResourceReadStreamResult(
-                            new Mock<Stream>().Object,
-                            new HttpRange(position, length),
-                            new(itemSize, default, default, new()))));
-                dstResource.Setup(r => r.CopyFromStreamAsync(
-                    It.IsAny<Stream>(), It.IsAny<long>(), It.IsAny<bool>(), It.IsAny<long>(),
-                    It.IsAny<StorageResourceWriteToOffsetOptions>(), It.IsAny<CancellationToken>()))
-                    .Returns(Task.CompletedTask);
-                dstResource.Setup(r => r.CompleteTransferAsync(
-                    It.IsAny<bool>(),
-                    It.IsAny<StorageResourceCompleteTransferOptions>(),
-                    It.IsAny<CancellationToken>()))
-                    .Returns(Task.CompletedTask);
-
-                return (Source: srcResource, Destination: dstResource);
-            }).ToList();
+            return (Source: srcResource, Destination: dstResource);
+        }).ToList();
 
         await using TransferManager transferManager = new(
             jobsProcessor,
@@ -172,7 +148,7 @@ internal class TransferManagerTests
             dstResource.VerifyNoOtherCalls();
         }
 
-        await Task.Delay(10); // TODO flaky that we need this; a random one will often fail without
+        await Task.Delay(20); // TODO flaky that we need this; a random one will often fail without
 
         foreach (DataTransfer transfer in transfers)
         {
@@ -181,9 +157,105 @@ internal class TransferManagerTests
     }
 
     [Test]
-    public void StartContainerTransafer()
+    [Combinatorial]
+    public async Task StartContainerTransafer(
+        [Values(1, 5)] int numJobs,
+        [Values(333, 500, 1024)] int itemSize,
+        [Values(333, 1024)] int chunkSize)
     {
-        Assert.Fail();
+        static int GetItemCountFromContainerIndex(int i) => i*i + 1;
+
+        int numJobParts = Enumerable.Range(1, numJobs).Select(GetItemCountFromContainerIndex).Sum();
+        int chunksPerPart = (int)Math.Ceiling((float)itemSize / chunkSize);
+        // TODO: below should be only `items * chunksPerPart` but can't in some cases due to
+        //       a bug in how work items are processed on multipart uploads.
+        int numChunks = Math.Max(chunksPerPart - 1, 1) * numJobParts;
+
+        Uri srcUri = new("file:///foo/bar");
+        Uri dstUri = new("https://example.com/fizz/buzz");
+
+        StepProcessor<TransferJobInternal> jobsProcessor = new();
+        StepProcessor<JobPartInternal> partsProcessor = new();
+        StepProcessor<Func<Task>> chunksProcessor = new();
+        Mock<JobBuilder> jobBuilder = new(ArrayPool<byte>.Shared, default, new ClientDiagnostics(ClientOptions.Default));
+        Mock<TransferCheckpointer> checkpointer = new();
+
+        var resources = Enumerable.Range(1, numJobs).Select(i =>
+        {
+            Mock<StorageResourceContainer> srcResource = new(MockBehavior.Strict);
+            Mock<StorageResourceContainer> dstResource = new(MockBehavior.Strict);
+            (srcResource, dstResource).BasicSetup(srcUri, dstUri, GetItemCountFromContainerIndex(i), itemSize);
+            return (Source: srcResource, Destination: dstResource);
+        }).ToList();
+
+        await using TransferManager transferManager = new(
+            jobsProcessor,
+            partsProcessor,
+            chunksProcessor,
+            jobBuilder.Object,
+            checkpointer.Object,
+            default);
+
+        List<DataTransfer> transfers = new();
+
+        // queue jobs
+        foreach ((Mock<StorageResourceContainer> srcResource, Mock<StorageResourceContainer> dstResource) in resources)
+        {
+            DataTransfer transfer = await transferManager.StartTransferAsync(
+                srcResource.Object,
+                dstResource.Object,
+                new()
+                {
+                    InitialTransferSize = chunkSize,
+                    MaximumTransferChunkSize = chunkSize,
+                });
+            Assert.That(transfer.HasCompleted, Is.False);
+            transfers.Add(transfer);
+
+            srcResource.VerifySourceResourceOnQueue();
+            dstResource.VerifyDestinationResourceOnQueue();
+            srcResource.VerifyNoOtherCalls();
+            dstResource.VerifyNoOtherCalls();
+        }
+        Assert.That(jobsProcessor.ItemsInQueue, Is.EqualTo(numJobs), "Error during initial Job queueing.");
+
+        // process jobs
+        Assert.That(await jobsProcessor.StepAll(), Is.EqualTo(numJobs));
+        Assert.That(jobsProcessor.ItemsInQueue, Is.EqualTo(0));
+        Assert.That(partsProcessor.ItemsInQueue, Is.EqualTo(numJobParts), "Error during Job => Part processing.");
+        foreach ((Mock<StorageResourceContainer> srcResource, Mock<StorageResourceContainer> dstResource) in resources)
+        {
+            srcResource.VerifySourceResourceOnJobProcess();
+            dstResource.VerifyDestinationResourceOnJobProcess();
+            srcResource.VerifyNoOtherCalls();
+            dstResource.VerifyNoOtherCalls();
+        }
+
+        // process parts
+        Assert.That(await partsProcessor.StepAll(), Is.EqualTo(numJobParts));
+        Assert.That(partsProcessor.ItemsInQueue, Is.EqualTo(0));
+        Assert.That(chunksProcessor.ItemsInQueue, Is.EqualTo(numChunks), "Error during Part => Chunk processing.");
+        foreach ((Mock<StorageResourceContainer> srcResource, Mock<StorageResourceContainer> dstResource) in resources)
+        {
+            srcResource.VerifyNoOtherCalls();
+            dstResource.VerifyNoOtherCalls();
+        }
+
+        // process chunks
+        Assert.That(await chunksProcessor.StepAll(), Is.EqualTo(numChunks));
+        Assert.That(chunksProcessor.ItemsInQueue, Is.EqualTo(0));
+        foreach ((Mock<StorageResourceContainer> srcResource, Mock<StorageResourceContainer> dstResource) in resources)
+        {
+            srcResource.VerifyNoOtherCalls();
+            dstResource.VerifyNoOtherCalls();
+        }
+
+        await Task.Delay(10); // TODO flaky that we need this; a random one will often fail without
+
+        foreach (DataTransfer transfer in transfers)
+        {
+            Assert.That(transfer.HasCompleted);
+        }
     }
 }
 
@@ -197,6 +269,84 @@ internal static partial class MockExtensions
         {
             setup.Callback(onQueue);
         }
+    }
+
+    public static void BasicSetup(
+        this (Mock<StorageResourceItem> Source, Mock<StorageResourceItem> Destination) items,
+        Uri srcUri,
+        Uri dstUri,
+        int itemSize = Constants.KB
+        )
+    {
+        items.Source.Setup(r => r.Uri).Returns(srcUri);
+        items.Destination.Setup(r => r.Uri).Returns(dstUri);
+
+        items.Source.SetupGet(r => r.ResourceId).Returns("Mock");
+        items.Destination.SetupGet(r => r.ResourceId).Returns("Mock");
+
+        items.Destination.SetupGet(r => r.TransferType).Returns(default(DataTransferOrder));
+        items.Destination.SetupGet(r => r.MaxSupportedChunkSize).Returns(Constants.GB);
+
+        items.Source.Setup(r => r.GetPropertiesAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(new StorageResourceItemProperties(resourceLength: itemSize, default, default, default)));
+
+        items.Source.Setup(r => r.ReadStreamAsync(It.IsAny<long>(), It.IsAny<long?>(), It.IsAny<CancellationToken>()))
+            .Returns<long, long?, CancellationToken>((position, length, cancellation)
+                => Task.FromResult(new StorageResourceReadStreamResult(
+                    new Mock<Stream>().Object,
+                    new HttpRange(position, length),
+                    new(itemSize, default, default, new()))));
+        items.Destination.Setup(r => r.CopyFromStreamAsync(
+            It.IsAny<Stream>(), It.IsAny<long>(), It.IsAny<bool>(), It.IsAny<long>(),
+            It.IsAny<StorageResourceWriteToOffsetOptions>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+        items.Destination.Setup(r => r.CompleteTransferAsync(
+            It.IsAny<bool>(),
+            It.IsAny<StorageResourceCompleteTransferOptions>(),
+            It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+    }
+
+    public static void BasicSetup(
+        this (Mock<StorageResourceContainer> Source, Mock<StorageResourceContainer> Destination) containers,
+        Uri srcUri,
+        Uri dstUri,
+        int numItems = 5,
+        int itemSize = Constants.KB
+        )
+    {
+        var subResources = Enumerable.Range(0, numItems).Select(_ =>
+        {
+            string name = "/" + Guid.NewGuid().ToString();
+            var items = (
+                Source: new Mock<StorageResourceItem>(MockBehavior.Strict),
+                Destination: new Mock<StorageResourceItem>(MockBehavior.Strict));
+            items.BasicSetup(new Uri(srcUri.ToString() + name), new Uri(dstUri.ToString() + name), itemSize);
+            items.Source.SetupGet(r => r.IsContainer).Returns(false);
+            return items;
+        }).ToList();
+
+        async IAsyncEnumerable<StorageResource> SubResourcesAsAsyncEnumerable(
+            StorageResourceContainer src,
+            [EnumeratorCancellation] CancellationToken ct)
+        {
+            foreach (int i in Enumerable.Range(0, numItems))
+            {
+                yield return await Task.FromResult(subResources[i].Source.Object);
+            }
+        }
+
+        containers.Source.SetupGet(r => r.Uri).Returns(srcUri);
+        containers.Destination.SetupGet(r => r.Uri).Returns(dstUri);
+
+        containers.Source.Setup(r => r.GetStorageResourcesAsync(It.IsAny<StorageResourceContainer>(), It.IsAny<CancellationToken>()))
+            .Returns(SubResourcesAsAsyncEnumerable);
+
+        containers.Destination.Setup(r => r.GetStorageResourceReference(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns<string, string>((path, resId) => subResources
+                .Where(pair => pair.Source.Object.Uri.AbsolutePath.Contains(path))
+                .FirstOrDefault().Destination?.Object
+            );
     }
 
     public static void VerifyTransferManagerCtorInvocations<T>(this Mock<IProcessor<T>> processor)
@@ -215,6 +365,16 @@ internal static partial class MockExtensions
         dstResource.VerifyGet(r => r.Uri);
     }
 
+    public static void VerifySourceResourceOnQueue(this Mock<StorageResourceContainer> srcResource)
+    {
+        srcResource.VerifyGet(r => r.Uri);
+    }
+
+    public static void VerifyDestinationResourceOnQueue(this Mock<StorageResourceContainer> dstResource)
+    {
+        dstResource.VerifyGet(r => r.Uri);
+    }
+
     public static void VerifySourceResourceOnJobProcess(this Mock<StorageResourceItem> srcResource)
     {
         srcResource.VerifyGet(r => r.Uri);
@@ -226,6 +386,21 @@ internal static partial class MockExtensions
         dstResource.VerifyGet(r => r.Uri);
         dstResource.VerifyGet(r => r.ResourceId);
         dstResource.VerifyGet(r => r.MaxSupportedChunkSize);
+    }
+
+    public static void VerifySourceResourceOnJobProcess(this Mock<StorageResourceContainer> srcResource)
+    {
+        srcResource.Verify(
+            r => r.GetStorageResourcesAsync(It.IsAny<StorageResourceContainer>(), It.IsAny<CancellationToken>()),
+            Times.Once);
+        srcResource.VerifyGet(r => r.Uri, Times.AtLeastOnce());
+    }
+
+    public static void VerifyDestinationResourceOnJobProcess(this Mock<StorageResourceContainer> dstResource)
+    {
+        dstResource.Verify(
+            r => r.GetStorageResourceReference(It.IsAny<string>(), It.IsAny<string>()),
+            Times.AtLeastOnce());
     }
 
     public static void VerifySourceResourceOnPartProcess(this Mock<StorageResourceItem> srcResource)
