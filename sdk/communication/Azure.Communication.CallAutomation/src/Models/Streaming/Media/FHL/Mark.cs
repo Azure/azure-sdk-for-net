bede8feda38ea73fab2d7c6981acda11174d41dc
Paresh Arvind Patil <patilaparesh@gmail.com>
2024-09-18 11:42:34 -0700

    [FHL] Add contracts for Bi-directional streaming (#45999)
    * add contracts for bi=directional streaming
    
    * Make client take pma uri
    
    * Update the contracts
    
    ---------
    
    Co-authored-by: Paresh Arvind Patil <papati@microsoft.com>

---
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/Streaming/Media/FHL/Mark.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Communication.CallAutomation.FHL
+{
+    public class Mark
+    {
+        /// <summary>
+        /// Mark constructor
+        /// </summary>
+        /// <param name="sequence">Incrementing sequence of the audio byte</param>
+        public Mark(string sequence)
+        {
+            Sequence = sequence;
+        }
+
+        /// <summary>
+        /// Increasing Sequence
+        /// IsRequired = true
+        /// </summary>
+        public string Sequence { get; }
+    }
+}
