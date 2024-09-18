271a379185b4a7e336e95a7fdc50d2bd898f2dda
melina5656 <140602263+melina5656@users.noreply.github.com>
2024-09-18 10:57:41 +0800

    Enable WriteCore feature for chaos

--- sdk/chaos/Azure.ResourceManager.Chaos/src/Generated/Models/ChaosExperimentAction.Serialization.cs
+++ sdk/chaos/Azure.ResourceManager.Chaos/src/Generated/Models/ChaosExperimentAction.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Chaos.Models
 
         void IJsonModel<ChaosExperimentAction>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ChaosExperimentAction>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ChaosExperimentAction)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("type"u8);
             writer.WriteStringValue(ActionType);
             writer.WritePropertyName("name"u8);
@@ -45,7 +53,6 @@ namespace Azure.ResourceManager.Chaos.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ChaosExperimentAction IJsonModel<ChaosExperimentAction>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
--- sdk/chaos/Azure.ResourceManager.Chaos/src/Generated/Models/ChaosExperimentRunStepStatus.Serialization.cs
+++ sdk/chaos/Azure.ResourceManager.Chaos/src/Generated/Models/ChaosExperimentRunStepStatus.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Chaos.Models
 
         void IJsonModel<ChaosExperimentRunStepStatus>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ChaosExperimentRunStepStatus>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ChaosExperimentRunStepStatus)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (options.Format != "W" && Optional.IsDefined(StepName))
             {
                 writer.WritePropertyName("stepName"u8);
@@ -66,7 +74,6 @@ namespace Azure.ResourceManager.Chaos.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ChaosExperimentRunStepStatus IJsonModel<ChaosExperimentRunStepStatus>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
