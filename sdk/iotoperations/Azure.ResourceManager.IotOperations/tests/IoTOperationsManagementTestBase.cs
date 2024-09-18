835dc28e0cbf7c7e4e968104887377a4d283a344
Mitchell Morrison <mmorrison@microsoft.com>
2024-09-18 12:45:37 -0700

    add instance name test

--- sdk/iotoperations/Azure.ResourceManager.IotOperations/tests/IoTOperationsManagementTestBase.cs
+++ sdk/iotoperations/Azure.ResourceManager.IotOperations/tests/IoTOperationsManagementTestBase.cs
@@ -1,22 +1,24 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Azure.Core;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.TestFramework;
 using NUnit.Framework;
 using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.ResourceManager.IoTOperations;
 
 namespace Azure.ResourceManager.IotOperations.Tests
 {
     public class IoTOperationsManagementTestBase : ManagementRecordedTestBase<IoTOperationsManagementTestEnvironment>
     {
         protected ArmClient Client { get; private set; }
-        protected SubscriptionResource DefaultSubscription { get; private set; }
 
         protected IoTOperationsManagementTestBase(bool isAsync, RecordedTestMode mode)
-        : base(isAsync, mode)
+            : base(isAsync, mode)
         {
         }
 
@@ -26,18 +28,10 @@ namespace Azure.ResourceManager.IotOperations.Tests
         }
 
         [SetUp]
-        public async Task CreateCommonClient()
+        public Task CreateCommonTestResources()
         {
-            Client = GetArmClient();
-            DefaultSubscription = await Client.GetDefaultSubscriptionAsync().ConfigureAwait(false);
-        }
-
-        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
-        {
-            string rgName = Recording.GenerateAssetName(rgNamePrefix);
-            ResourceGroupData input = new ResourceGroupData(location);
-            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
-            return lro.Value;
+            Client = new ArmClient(new DefaultAzureCredential());
+            return Task.CompletedTask;
         }
     }
 }
---
+++ sdk/iotoperations/Azure.ResourceManager.IotOperations/tests/Scenario/IoTOperationsManagementTests.cs
@@ -0,0 +1,48 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.ResourceManager.Resources;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.IoTOperations.Models;
+using Azure.Identity;
+using Azure.ResourceManager.IoTOperations;
+
+namespace Azure.ResourceManager.IotOperations.Tests
+{
+    public class IoTOperationsManagementTests : IoTOperationsManagementTestBase
+    {
+        public IoTOperationsManagementTests(bool isAsync)
+            : base(isAsync)
+        {
+        }
+
+        [TestCase]
+        public async Task GetInstanceNameTest()
+        {
+            // FILL IN CLUSTER VARIABLES HERES
+            // CLUSTER MUST BE ARC ENABLED, WITH AIO DEPLOYED
+            var subId = "d4ccd08b-0809-446d-a8b7-7af8a90109cd";
+            var rg = "mitch-bb-2-103545027";
+            var instanceName = "aio-zk3mr";
+            Console.WriteLine("Starting the instance name test");
+
+            // Call the Get method on InstanceResource using generated MGMT API
+            var instanceResourceId = InstanceResource.CreateResourceIdentifier(subId, rg, instanceName);
+            var instanceResource = Client.GetInstanceResource(instanceResourceId);
+
+            // Retrieve the instance resource
+            Response<InstanceResource> response = await instanceResource.GetAsync();
+            InstanceResource instance = response.Value;
+
+            // Use the instance resource as needed
+            Console.WriteLine($"Instance Name: {instance.Data.Name}");
+            Console.WriteLine($"Expected Instance Name: {instanceName}");
+            Assert.AreEqual(instanceName, instance.Data.Name);
+        }
+    }
+}
