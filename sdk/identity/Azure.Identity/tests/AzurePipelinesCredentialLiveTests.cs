ff8c0fe8e466a66c24786ed351099ec4463d1a45
Ahson Khan <ahkha@microsoft.com>
2024-09-19 15:38:22 -0700

    Update assert about expected exception

--- sdk/identity/Azure.Identity/tests/AzurePipelinesCredentialLiveTests.cs
+++ sdk/identity/Azure.Identity/tests/AzurePipelinesCredentialLiveTests.cs
@@ -69,9 +69,8 @@ namespace Azure.Identity.Tests
 
             var cred = new AzurePipelinesCredential(tenantId, clientId, serviceConnectionId, systemAccessToken);
 
-            AccessToken token = await cred.GetTokenAsync(new TokenRequestContext(new[] { "https://management.azure.com//.default" }), CancellationToken.None);
-
-            Assert.IsNotNull(token.Token);
+            var ex = Assert.ThrowsAsync<AuthenticationFailedException>(async () => await cred.GetTokenAsync(new TokenRequestContext(new[] { "https://management.azure.com//.default" }), CancellationToken.None));
+            Assert.That(ex.Message, Does.Contain("302 (Found)"));
         }
     }
 }
