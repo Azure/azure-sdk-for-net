72582480d7a77d6d462b3163851070503e7012ee
Krzysztof Cwalina <kcwalina@microsoft.com>
2024-09-20 08:43:09 -0700

    added solution, and a placeholder test file

---
+++ sdk/provisioning/Azure.Provisioning.CloudMachine/tests/CloudMachineTests.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable enable
+
+using NUnit.Framework;
+
+namespace Azure.CloudMachine.Tests;
+
+public class CloudMachineTests
+{
+    [TestCase("test")]
+    public void NotNull(object? value)
+    {
+        Assert.AreEqual("test1", "test1");
+    }
+}
