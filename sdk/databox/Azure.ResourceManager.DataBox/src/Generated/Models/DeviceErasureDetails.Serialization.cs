a2b6526fdad38674c2c5aca42742596fedfa8c6d
melina5656 <140602263+melina5656@users.noreply.github.com>
2024-09-23 13:54:32 +0800

    Enable WriteCore feature for databox

--- sdk/databox/Azure.ResourceManager.DataBox/src/Generated/Models/DeviceErasureDetails.Serialization.cs
+++ sdk/databox/Azure.ResourceManager.DataBox/src/Generated/Models/DeviceErasureDetails.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.DataBox.Models
 
         void IJsonModel<DeviceErasureDetails>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<DeviceErasureDetails>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(DeviceErasureDetails)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (options.Format != "W" && Optional.IsDefined(DeviceErasureStatus))
             {
                 writer.WritePropertyName("deviceErasureStatus"u8);
@@ -51,7 +59,6 @@ namespace Azure.ResourceManager.DataBox.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         DeviceErasureDetails IJsonModel<DeviceErasureDetails>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
