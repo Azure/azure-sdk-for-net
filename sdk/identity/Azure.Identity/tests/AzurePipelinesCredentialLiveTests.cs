fc754180b91d366d65a2e9fd429e7b9527e380d4
Ahson Khan <ahkha@microsoft.com>
2024-09-23 16:04:36 -0700

    Updated the request header sent to the OIDC endpoint in so it doesn't result in a redirect response when an invalid system access token is provided.

--- sdk/identity/Azure.Identity/tests/AzurePipelinesCredentialLiveTests.cs
+++ sdk/identity/Azure.Identity/tests/AzurePipelinesCredentialLiveTests.cs
@@ -44,5 +44,36 @@ namespace Azure.Identity.Tests
 
             Assert.IsNotNull(token.Token);
         }
+
+        [Test]
+        [LiveOnly]
+        public async Task AzurePipelineCredentialLiveTest_GetToken_InvalidToken()
+        {
+            string systemAccessToken = "invalidSystemAccessToken";
+            var tenantId = Environment.GetEnvironmentVariable("AZURE_SERVICE_CONNECTION_TENANT_ID");
+            var clientId = Environment.GetEnvironmentVariable("AZURE_SERVICE_CONNECTION_CLIENT_ID");
+            var serviceConnectionId = Environment.GetEnvironmentVariable("AZURE_SERVICE_CONNECTION_ID");
+
+            if (string.IsNullOrEmpty(tenantId) || string.IsNullOrEmpty(clientId) || string.IsNullOrEmpty(serviceConnectionId))
+            {
+                var envVars = Environment.GetEnvironmentVariables();
+                StringBuilder sb = new StringBuilder();
+                foreach (var key in envVars.Keys)
+                {
+                    sb.AppendLine($"{key}: {envVars[key]}");
+                }
+                Console.WriteLine(sb);
+                Assert.Fail($"{sb} SYSTEM_ACCESSTOKEN: {systemAccessToken}, AZURE_SERVICE_CONNECTION_TENANT_ID: {tenantId}, AZURE_SERVICE_CONNECTION_CLIENT_ID: {clientId}, AZURE_SERVICE_CONNECTION_ID: {serviceConnectionId}");
+                Assert.Ignore("AzurePipelinesCredentialLiveTests disabled because required environment variables are not set");
+            }
+
+            var cred = new AzurePipelinesCredential(tenantId, clientId, serviceConnectionId, systemAccessToken);
+
+            var ex = Assert.ThrowsAsync<AuthenticationFailedException>(async () => await cred.GetTokenAsync(new TokenRequestContext(new[] { "https://management.azure.com//.default" }), CancellationToken.None));
+            Assert.That(ex.Message, Does.Contain("401 (Unauthorized)"));
+
+            AccessToken token = await cred.GetTokenAsync(new TokenRequestContext(new[] { "https://management.azure.com//.default" }), CancellationToken.None);
+            Assert.IsNotNull(token.Token);
+        }
     }
 }
