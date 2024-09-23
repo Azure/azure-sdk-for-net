61efe68f0ee4f60d2618da09d966f62a1574fd2d
zhz0704 <eatcoc10@gmail.com>
2024-09-23 10:41:57 +0800

    Update sdk/maps/Azure.Maps.Weather/src/Models/Options/GetDailyIndicesOptions.cs
    Co-authored-by: Charlie Chen <yuchungchen@microsoft.com>

--- sdk/maps/Azure.Maps.Weather/src/Models/Options/GetDailyIndicesOptions.cs
+++ sdk/maps/Azure.Maps.Weather/src/Models/Options/GetDailyIndicesOptions.cs
@@ -12,7 +12,7 @@ namespace Azure.Maps.Weather.Models.Options
     {
         /// <summary> Specifies for how long the responses are returned. </summary>
         public int? DurationInDays { get; set; }
-        /// <summary> Numeric index identifier that can be used for restricting returned results to the corresponding index type. Cannot be paired with `indexGroupId`. Please refer to [Weather services in Azure Maps](/azure/azure-maps/weather-services-concepts#index-ids-and-index-groups-ids) for details and to see the supported indices. </summary>
+        /// <summary> Numeric index identifier that can be used for restricting returned results to the corresponding index type. Cannot be paired with <c>indexGroupId</c>. Please refer to [Weather services in Azure Maps](/azure/azure-maps/weather-services-concepts#index-ids-and-index-groups-ids) for details and to see the supported indices. </summary>
         public int? IndexId { get; set; }
         /// <summary> Numeric index group identifier that can be used for restricting returned results to the corresponding subset of indices (index group). Cannot be paired with `indexId`. Please refer to [Weather services in Azure Maps](/azure/azure-maps/weather-services-concepts#index-ids-and-index-groups-ids) for details and to see the supported index groups. </summary>
         public int? IndexGroupId { get; set; }
