e86b03165d27f5963b85d64d22408a89d97d7c2a
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-24 17:41:13 +0800

    Enable-WriteCore-feature-for-machinelearningcompute

--- sdk/machinelearningcompute/Azure.ResourceManager.MachineLearningCompute/src/Generated/Models/StorageAccountProperties.Serialization.cs
+++ sdk/machinelearningcompute/Azure.ResourceManager.MachineLearningCompute/src/Generated/Models/StorageAccountProperties.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.MachineLearningCompute.Models
 
         void IJsonModel<StorageAccountProperties>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<StorageAccountProperties>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(StorageAccountProperties)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(ResourceId))
             {
                 writer.WritePropertyName("resourceId"u8);
@@ -46,7 +54,6 @@ namespace Azure.ResourceManager.MachineLearningCompute.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         StorageAccountProperties IJsonModel<StorageAccountProperties>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
