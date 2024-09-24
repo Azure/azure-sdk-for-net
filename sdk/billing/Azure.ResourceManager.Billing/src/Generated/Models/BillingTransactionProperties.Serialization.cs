5bad8d6b6bdea3bd5da9c0de97df16f21b548ebf
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-24 15:14:09 +0800

    update (#45980)

--- sdk/billing/Azure.ResourceManager.Billing/src/Generated/Models/BillingTransactionProperties.Serialization.cs
+++ sdk/billing/Azure.ResourceManager.Billing/src/Generated/Models/BillingTransactionProperties.Serialization.cs
@@ -20,13 +20,21 @@ namespace Azure.ResourceManager.Billing.Models
 
         void IJsonModel<BillingTransactionProperties>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<BillingTransactionProperties>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(BillingTransactionProperties)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (options.Format != "W" && Optional.IsDefined(AzureCreditApplied))
             {
                 writer.WritePropertyName("azureCreditApplied"u8);
@@ -244,7 +252,6 @@ namespace Azure.ResourceManager.Billing.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         BillingTransactionProperties IJsonModel<BillingTransactionProperties>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
