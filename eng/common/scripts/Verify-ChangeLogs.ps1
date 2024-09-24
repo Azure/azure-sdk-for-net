aca02501cc2039d38b70d547f458d6fb58f19528
Scott Beddall <scbedd@microsoft.com>
2024-09-24 11:37:40 -0700

    don't verify changelog for packages that aren't actively published anymore

--- eng/common/scripts/Verify-ChangeLogs.ps1
+++ eng/common/scripts/Verify-ChangeLogs.ps1
@@ -7,6 +7,29 @@ Set-StrictMode -Version 3
 
 . (Join-Path $PSScriptRoot common.ps1)
 
+function ShouldVerifyChangeLog ($ServiceDirectory, $PackageName) {
+    $jsonCiYmlPath = Join-Path $ServiceDirectory "ci.yml.json"
+
+    if (Test-Path $jsonCiYmlPath)
+    {
+        $ciYml = ConvertFrom-Json (Get-Content $jsonCiYmlPath -Raw)
+
+        if ($ciYml.extends -and $ciYml.extends.parameters -and $ciYml.extends.parameters.Artifacts) {
+            $packagesCheckingChangeLog = $ciYml.extends.parameters.Artifacts `
+                | Where-Object { -not ($_["skipVerifyChangelog"] -eq $true) }
+                | Select-Object -ExpandProperty name
+
+            if ($packagesCheckingChangeLog -contains $PackageName)
+            {
+                return $true
+            }
+            else {
+                return $false
+            }
+        }
+    }
+}
+
 # find which packages we need to confirm the changelog for
 $packageProperties = Get-ChildItem -Recurse "$PackagePropertiesFolder" *.json
 
@@ -15,13 +38,17 @@ $allPassing = $true
 foreach($propertiesFile in $packageProperties) {
   $PackageProp = Get-Content -Path $propertiesFile | ConvertFrom-Json
 
+  if (-not (ShouldVerifyChangeLog -ServiceDirectory (Join-Path $RepoRoot "sdk" $packageInfo.ServiceDirectory) -PackageName $packageInfo.Name)) {
+        Write-Host "Skipping changelog verification for $($packageInfo.Name)"
+        continue
+  }
+
   $validChangeLog =  Confirm-ChangeLogEntry -ChangeLogLocation $PackageProp.ChangeLogPath -VersionString $PackageProp.Version -ForRelease $false
 
   if (-not $validChangeLog) {
     $allPassing = $false
   }
 }
-
 
 if (!$allPassing)
 {
