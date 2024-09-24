dedef0c5d4f6c131e076db8db6a5ad8dc596709d
Xinni Tong <v-xinnitong@microsoft.com>
2024-09-24 17:43:05 +0800

    Enable WriteCore feature for datamigration (#46140)

--- sdk/datamigration/Azure.ResourceManager.DataMigration/src/Generated/Models/MigrateMySqlAzureDBForMySqlSyncTaskInput.Serialization.cs
+++ sdk/datamigration/Azure.ResourceManager.DataMigration/src/Generated/Models/MigrateMySqlAzureDBForMySqlSyncTaskInput.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.DataMigration.Models
 
         void IJsonModel<MigrateMySqlAzureDBForMySqlSyncTaskInput>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<MigrateMySqlAzureDBForMySqlSyncTaskInput>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(MigrateMySqlAzureDBForMySqlSyncTaskInput)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("sourceConnectionInfo"u8);
             writer.WriteObjectValue(SourceConnectionInfo, options);
             writer.WritePropertyName("targetConnectionInfo"u8);
@@ -52,7 +60,6 @@ namespace Azure.ResourceManager.DataMigration.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         MigrateMySqlAzureDBForMySqlSyncTaskInput IJsonModel<MigrateMySqlAzureDBForMySqlSyncTaskInput>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
