43fccd8d7cf3c47f107248f3f49ca43486ea2550
melina5656 <140602263+melina5656@users.noreply.github.com>
2024-09-24 17:38:02 +0800

    Enable WriteCore feature for hybridcompute

--- sdk/hybridcompute/Azure.ResourceManager.HybridCompute/src/Generated/Models/HybridComputeProductFeature.Serialization.cs
+++ sdk/hybridcompute/Azure.ResourceManager.HybridCompute/src/Generated/Models/HybridComputeProductFeature.Serialization.cs
@@ -20,13 +20,21 @@ namespace Azure.ResourceManager.HybridCompute.Models
 
         void IJsonModel<HybridComputeProductFeature>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<HybridComputeProductFeature>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(HybridComputeProductFeature)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Name))
             {
                 writer.WritePropertyName("name"u8);
@@ -77,7 +85,6 @@ namespace Azure.ResourceManager.HybridCompute.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         HybridComputeProductFeature IJsonModel<HybridComputeProductFeature>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
