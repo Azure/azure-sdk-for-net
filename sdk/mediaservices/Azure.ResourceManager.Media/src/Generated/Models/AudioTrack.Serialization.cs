60fb5b82777a5f0ab7f0f8c527bb6211a9c93075
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-24 18:03:11 +0800

    Enable-WriteCore-feature-for-mediaservices

--- sdk/mediaservices/Azure.ResourceManager.Media/src/Generated/Models/AudioTrack.Serialization.cs
+++ sdk/mediaservices/Azure.ResourceManager.Media/src/Generated/Models/AudioTrack.Serialization.cs
@@ -19,13 +19,22 @@ namespace Azure.ResourceManager.Media.Models
 
         void IJsonModel<AudioTrack>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected override void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<AudioTrack>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(AudioTrack)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
+            base.JsonModelWriteCore(writer, options);
             if (Optional.IsDefined(FileName))
             {
                 writer.WritePropertyName("fileName"u8);
@@ -68,24 +77,6 @@ namespace Azure.ResourceManager.Media.Models
                 writer.WritePropertyName("bitRate"u8);
                 writer.WriteNumberValue(BitRate.Value);
             }
-            writer.WritePropertyName("@odata.type"u8);
-            writer.WriteStringValue(OdataType);
-            if (options.Format != "W" && _serializedAdditionalRawData != null)
-            {
-                foreach (var item in _serializedAdditionalRawData)
-                {
-                    writer.WritePropertyName(item.Key);
-#if NET6_0_OR_GREATER
-				writer.WriteRawValue(item.Value);
-#else
-                    using (JsonDocument document = JsonDocument.Parse(item.Value))
-                    {
-                        JsonSerializer.Serialize(writer, document.RootElement);
-                    }
-#endif
-                }
-            }
-            writer.WriteEndObject();
         }
 
         AudioTrack IJsonModel<AudioTrack>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
--- sdk/mediaservices/Azure.ResourceManager.Media/src/Generated/Models/MediaLayerBase.Serialization.cs
+++ sdk/mediaservices/Azure.ResourceManager.Media/src/Generated/Models/MediaLayerBase.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Media.Models
 
         void IJsonModel<MediaLayerBase>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<MediaLayerBase>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(MediaLayerBase)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Width))
             {
                 writer.WritePropertyName("width"u8);
@@ -56,7 +64,6 @@ namespace Azure.ResourceManager.Media.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         MediaLayerBase IJsonModel<MediaLayerBase>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
