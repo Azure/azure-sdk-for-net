1a8ec461220fa2bd359df524673dc0228c919b33
Xinni Tong <v-xinnitong@microsoft.com>
2024-09-24 15:45:21 +0800

    Enable WriteCore feature for datadog (#46119)

--- sdk/datadog/Azure.ResourceManager.Datadog/src/Generated/Models/DatadogHost.Serialization.cs
+++ sdk/datadog/Azure.ResourceManager.Datadog/src/Generated/Models/DatadogHost.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Datadog.Models
 
         void IJsonModel<DatadogHost>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<DatadogHost>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(DatadogHost)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Name))
             {
                 writer.WritePropertyName("name"u8);
@@ -71,7 +79,6 @@ namespace Azure.ResourceManager.Datadog.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         DatadogHost IJsonModel<DatadogHost>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
