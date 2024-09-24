2cbdd4e7f26207c46ec9f5690432156d0421aefc
Anne Thompson <annelo@microsoft.com>
2024-09-23 17:58:44 -0700

    nits

--- sdk/core/System.ClientModel/src/Options/ClientRetryOptions.cs
+++ sdk/core/System.ClientModel/src/Options/ClientRetryOptions.cs
@@ -1,20 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace System.ClientModel.Primitives;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 public class ClientRetryOptions
 {
-    //private static readonly ClientRetryOptions _defaultRetryOptions = new();
-    //internal static ClientRetryOptions Default => _defaultRetryOptions;
-
-    // This should remove the retry policy from the pipeline, or disable it if
-    // passed to a policy constructor.
+    // Setting this option to true either causes ClientPipeline.Create to
+    // create a pipeline with no retry policy, or disables retry functionality
+    // in a policy if it is passed to a policy constructor.
     public bool? DisableRetries { get; set; }
 
     public TimeSpan? MaxDelay { get; set; }
