6e3cbdae39b787f11710511c9d05b53076b78da6
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-24 16:53:31 +0800

    Enable-WriteCore-feature-for-hdinsight

--- sdk/hdinsight/Azure.ResourceManager.HDInsight/src/Generated/Models/RegionalQuotaCapability.Serialization.cs
+++ sdk/hdinsight/Azure.ResourceManager.HDInsight/src/Generated/Models/RegionalQuotaCapability.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.HDInsight.Models
 
         void IJsonModel<RegionalQuotaCapability>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<RegionalQuotaCapability>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(RegionalQuotaCapability)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Region))
             {
                 writer.WritePropertyName("regionName"u8);
@@ -56,7 +64,6 @@ namespace Azure.ResourceManager.HDInsight.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         RegionalQuotaCapability IJsonModel<RegionalQuotaCapability>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
