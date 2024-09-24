c1e5c39ef0eecd4b4e471996ba36c42b9fa6bd2c
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-24 15:38:58 +0800

    update (#46049)

--- sdk/cdn/Azure.ResourceManager.Cdn/src/Generated/Models/ManagedRuleOverrideSetting.Serialization.cs
+++ sdk/cdn/Azure.ResourceManager.Cdn/src/Generated/Models/ManagedRuleOverrideSetting.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Cdn.Models
 
         void IJsonModel<ManagedRuleOverrideSetting>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ManagedRuleOverrideSetting>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ManagedRuleOverrideSetting)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("ruleId"u8);
             writer.WriteStringValue(RuleId);
             if (Optional.IsDefined(EnabledState))
@@ -53,7 +61,6 @@ namespace Azure.ResourceManager.Cdn.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ManagedRuleOverrideSetting IJsonModel<ManagedRuleOverrideSetting>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
