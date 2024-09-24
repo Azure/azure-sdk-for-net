574eda444f70bd628df69993c1459968c0dd4c46
Xinni Tong <v-xinnitong@microsoft.com>
2024-09-24 15:43:47 +0800

    Enable WriteCore feature for databox (#46116)

--- sdk/databox/Azure.ResourceManager.DataBox/src/Generated/Models/JobSecrets.Serialization.cs
+++ sdk/databox/Azure.ResourceManager.DataBox/src/Generated/Models/JobSecrets.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.DataBox.Models
 
         void IJsonModel<JobSecrets>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<JobSecrets>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(JobSecrets)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("jobSecretsType"u8);
             writer.WriteStringValue(JobSecretsType.ToSerialString());
             if (options.Format != "W" && Optional.IsDefined(DataCenterAccessSecurityCode))
@@ -53,7 +61,6 @@ namespace Azure.ResourceManager.DataBox.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         JobSecrets IJsonModel<JobSecrets>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
