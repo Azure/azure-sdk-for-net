542e257a26e727dc0b98416ba57981051729ec73
Xinni Tong <v-xinnitong@microsoft.com>
2024-09-24 15:43:25 +0800

    Enable WriteCore feature for customer-insights (#46115)

--- sdk/customer-insights/Azure.ResourceManager.CustomerInsights/src/Generated/Models/DataSourcePrecedence.Serialization.cs
+++ sdk/customer-insights/Azure.ResourceManager.CustomerInsights/src/Generated/Models/DataSourcePrecedence.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.CustomerInsights.Models
 
         void IJsonModel<DataSourcePrecedence>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<DataSourcePrecedence>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(DataSourcePrecedence)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Precedence))
             {
                 writer.WritePropertyName("precedence"u8);
@@ -74,7 +82,6 @@ namespace Azure.ResourceManager.CustomerInsights.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         DataSourcePrecedence IJsonModel<DataSourcePrecedence>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
