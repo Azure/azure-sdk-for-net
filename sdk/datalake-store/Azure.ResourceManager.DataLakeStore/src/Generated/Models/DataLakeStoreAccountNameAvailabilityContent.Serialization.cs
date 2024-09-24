d1f2a2ff939f463083eaf666d52c74be8381db8d
Xinni Tong <v-xinnitong@microsoft.com>
2024-09-24 17:46:10 +0800

    Enable WriteCore feature for datalake-store (#46139)

--- sdk/datalake-store/Azure.ResourceManager.DataLakeStore/src/Generated/Models/DataLakeStoreAccountNameAvailabilityContent.Serialization.cs
+++ sdk/datalake-store/Azure.ResourceManager.DataLakeStore/src/Generated/Models/DataLakeStoreAccountNameAvailabilityContent.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.DataLakeStore.Models
 
         void IJsonModel<DataLakeStoreAccountNameAvailabilityContent>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<DataLakeStoreAccountNameAvailabilityContent>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(DataLakeStoreAccountNameAvailabilityContent)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("name"u8);
             writer.WriteStringValue(Name);
             writer.WritePropertyName("type"u8);
@@ -45,7 +53,6 @@ namespace Azure.ResourceManager.DataLakeStore.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         DataLakeStoreAccountNameAvailabilityContent IJsonModel<DataLakeStoreAccountNameAvailabilityContent>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
