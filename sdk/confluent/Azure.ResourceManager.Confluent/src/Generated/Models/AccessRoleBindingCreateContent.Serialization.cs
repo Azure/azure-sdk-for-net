ac86c8539e9d0c35038c1c43d08111b9be110f9d
Xinni Tong <v-xinnitong@microsoft.com>
2024-09-24 15:38:15 +0800

    Enable WriteCore feature for confluent (#46052)

--- sdk/confluent/Azure.ResourceManager.Confluent/src/Generated/Models/AccessRoleBindingCreateContent.Serialization.cs
+++ sdk/confluent/Azure.ResourceManager.Confluent/src/Generated/Models/AccessRoleBindingCreateContent.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Confluent.Models
 
         void IJsonModel<AccessRoleBindingCreateContent>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<AccessRoleBindingCreateContent>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(AccessRoleBindingCreateContent)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Principal))
             {
                 writer.WritePropertyName("principal"u8);
@@ -56,7 +64,6 @@ namespace Azure.ResourceManager.Confluent.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         AccessRoleBindingCreateContent IJsonModel<AccessRoleBindingCreateContent>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
--- sdk/confluent/Azure.ResourceManager.Confluent/src/Generated/Models/SchemaRegistryClusterEnvironmentRegionEntity.Serialization.cs
+++ sdk/confluent/Azure.ResourceManager.Confluent/src/Generated/Models/SchemaRegistryClusterEnvironmentRegionEntity.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Confluent.Models
 
         void IJsonModel<SchemaRegistryClusterEnvironmentRegionEntity>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<SchemaRegistryClusterEnvironmentRegionEntity>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(SchemaRegistryClusterEnvironmentRegionEntity)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Id))
             {
                 writer.WritePropertyName("id"u8);
@@ -56,7 +64,6 @@ namespace Azure.ResourceManager.Confluent.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         SchemaRegistryClusterEnvironmentRegionEntity IJsonModel<SchemaRegistryClusterEnvironmentRegionEntity>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
