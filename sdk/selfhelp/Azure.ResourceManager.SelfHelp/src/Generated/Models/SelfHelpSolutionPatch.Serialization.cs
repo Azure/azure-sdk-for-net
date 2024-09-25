b97a983fc4ec28fbc2f723231dce97f7cf170f7a
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 16:51:01 +0800

    Enable-WriteCore-feature-for-selfhelp

--- sdk/selfhelp/Azure.ResourceManager.SelfHelp/src/Generated/Models/SelfHelpSolutionPatch.Serialization.cs
+++ sdk/selfhelp/Azure.ResourceManager.SelfHelp/src/Generated/Models/SelfHelpSolutionPatch.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.SelfHelp.Models
 
         void IJsonModel<SelfHelpSolutionPatch>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<SelfHelpSolutionPatch>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(SelfHelpSolutionPatch)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("properties"u8);
             writer.WriteStartObject();
             if (Optional.IsCollectionDefined(TriggerCriteria))
@@ -100,7 +108,6 @@ namespace Azure.ResourceManager.SelfHelp.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         SelfHelpSolutionPatch IJsonModel<SelfHelpSolutionPatch>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
