73041cf69e53eeaf4f25efe5cda6819906e672a0
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 13:33:39 +0800

    Enable-WriteCore-feature-for-devcenter (#46147)

--- sdk/devcenter/Azure.ResourceManager.DevCenter/src/Generated/Models/DevCenterTrackedResourceUpdate.Serialization.cs
+++ sdk/devcenter/Azure.ResourceManager.DevCenter/src/Generated/Models/DevCenterTrackedResourceUpdate.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.DevCenter.Models
 
         void IJsonModel<DevCenterTrackedResourceUpdate>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<DevCenterTrackedResourceUpdate>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(DevCenterTrackedResourceUpdate)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsCollectionDefined(Tags))
             {
                 writer.WritePropertyName("tags"u8);
@@ -57,7 +65,6 @@ namespace Azure.ResourceManager.DevCenter.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         DevCenterTrackedResourceUpdate IJsonModel<DevCenterTrackedResourceUpdate>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
