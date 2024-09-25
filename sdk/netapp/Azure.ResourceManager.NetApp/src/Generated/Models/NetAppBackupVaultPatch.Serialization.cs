945cea5a496cc995c4fecc1e2bc2dfe346083275
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 09:20:06 +0800

    Enable-WriteCore-feature-for-netapp

--- sdk/netapp/Azure.ResourceManager.NetApp/src/Generated/Models/NetAppBackupVaultPatch.Serialization.cs
+++ sdk/netapp/Azure.ResourceManager.NetApp/src/Generated/Models/NetAppBackupVaultPatch.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.NetApp.Models
 
         void IJsonModel<NetAppBackupVaultPatch>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<NetAppBackupVaultPatch>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(NetAppBackupVaultPatch)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsCollectionDefined(Tags))
             {
                 writer.WritePropertyName("tags"u8);
@@ -52,7 +60,6 @@ namespace Azure.ResourceManager.NetApp.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         NetAppBackupVaultPatch IJsonModel<NetAppBackupVaultPatch>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
