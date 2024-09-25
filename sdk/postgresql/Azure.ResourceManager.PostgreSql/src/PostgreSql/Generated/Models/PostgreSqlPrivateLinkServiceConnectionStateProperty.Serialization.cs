663e3fb0395a5fb64ef60847c1f303d0e69183c6
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 15:01:55 +0800

    Enable-WriteCore-feature-for-postgresql (#46235)

--- sdk/postgresql/Azure.ResourceManager.PostgreSql/src/PostgreSql/Generated/Models/PostgreSqlPrivateLinkServiceConnectionStateProperty.Serialization.cs
+++ sdk/postgresql/Azure.ResourceManager.PostgreSql/src/PostgreSql/Generated/Models/PostgreSqlPrivateLinkServiceConnectionStateProperty.Serialization.cs
@@ -20,13 +20,21 @@ namespace Azure.ResourceManager.PostgreSql.Models
 
         void IJsonModel<PostgreSqlPrivateLinkServiceConnectionStateProperty>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<PostgreSqlPrivateLinkServiceConnectionStateProperty>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(PostgreSqlPrivateLinkServiceConnectionStateProperty)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("status"u8);
             writer.WriteStringValue(Status);
             writer.WritePropertyName("description"u8);
@@ -51,7 +59,6 @@ namespace Azure.ResourceManager.PostgreSql.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         PostgreSqlPrivateLinkServiceConnectionStateProperty IJsonModel<PostgreSqlPrivateLinkServiceConnectionStateProperty>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
