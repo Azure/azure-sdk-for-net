9c280b705941fca89774a51d1bf97d7d1fab5ff9
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 15:24:33 +0800

    Enable-WriteCore-feature-for-reservations (#46249)

--- sdk/reservations/Azure.ResourceManager.Reservations/src/Generated/Models/ReservationToPurchaseExchange.Serialization.cs
+++ sdk/reservations/Azure.ResourceManager.Reservations/src/Generated/Models/ReservationToPurchaseExchange.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Reservations.Models
 
         void IJsonModel<ReservationToPurchaseExchange>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ReservationToPurchaseExchange>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ReservationToPurchaseExchange)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(ReservationOrderId))
             {
                 writer.WritePropertyName("reservationOrderId"u8);
@@ -66,7 +74,6 @@ namespace Azure.ResourceManager.Reservations.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ReservationToPurchaseExchange IJsonModel<ReservationToPurchaseExchange>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
