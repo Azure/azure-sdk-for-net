3214fa49f2eee455841d7047888a7b70e0f053a0
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 15:16:33 +0800

    Enable-WriteCore-feature-for-mediaservices (#46195)

--- sdk/mediaservices/Azure.ResourceManager.Media/src/Generated/Models/EdgeUsageDataEventHub.Serialization.cs
+++ sdk/mediaservices/Azure.ResourceManager.Media/src/Generated/Models/EdgeUsageDataEventHub.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Media.Models
 
         void IJsonModel<EdgeUsageDataEventHub>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<EdgeUsageDataEventHub>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(EdgeUsageDataEventHub)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Name))
             {
                 writer.WritePropertyName("name"u8);
@@ -56,7 +64,6 @@ namespace Azure.ResourceManager.Media.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         EdgeUsageDataEventHub IJsonModel<EdgeUsageDataEventHub>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
