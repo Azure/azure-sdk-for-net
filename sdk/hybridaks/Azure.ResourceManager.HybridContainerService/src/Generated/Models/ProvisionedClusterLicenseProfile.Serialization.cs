cc3d918884bbd11a329f8307d1f7726617283bbe
Xinni Tong <v-xinnitong@microsoft.com>
2024-09-25 13:55:10 +0800

    Enable WriteCore feature for hybridaks (#46182)

--- sdk/hybridaks/Azure.ResourceManager.HybridContainerService/src/Generated/Models/ProvisionedClusterLicenseProfile.Serialization.cs
+++ sdk/hybridaks/Azure.ResourceManager.HybridContainerService/src/Generated/Models/ProvisionedClusterLicenseProfile.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.HybridContainerService.Models
 
         void IJsonModel<ProvisionedClusterLicenseProfile>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ProvisionedClusterLicenseProfile>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ProvisionedClusterLicenseProfile)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(AzureHybridBenefit))
             {
                 writer.WritePropertyName("azureHybridBenefit"u8);
@@ -46,7 +54,6 @@ namespace Azure.ResourceManager.HybridContainerService.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ProvisionedClusterLicenseProfile IJsonModel<ProvisionedClusterLicenseProfile>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
