6ff0bff069692d31851d956eb0c2eac7c5baa013
azure-sdk <azuresdk@microsoft.com>
2024-09-25 03:55:25 +0000

    Update SDK code co_co_1

--- sdk/cognitiveservices/Azure.ResourceManager.CognitiveServices/src/Generated/Models/CognitiveServicesPrivateEndpointServiceConnectionStatus.cs
+++ sdk/cognitiveservices/Azure.ResourceManager.CognitiveServices/src/Generated/Models/CognitiveServicesPrivateEndpointServiceConnectionStatus.cs
@@ -36,7 +36,7 @@ namespace Azure.ResourceManager.CognitiveServices.Models
         public static bool operator ==(CognitiveServicesPrivateEndpointServiceConnectionStatus left, CognitiveServicesPrivateEndpointServiceConnectionStatus right) => left.Equals(right);
         /// <summary> Determines if two <see cref="CognitiveServicesPrivateEndpointServiceConnectionStatus"/> values are not the same. </summary>
         public static bool operator !=(CognitiveServicesPrivateEndpointServiceConnectionStatus left, CognitiveServicesPrivateEndpointServiceConnectionStatus right) => !left.Equals(right);
-        /// <summary> Converts a string to a <see cref="CognitiveServicesPrivateEndpointServiceConnectionStatus"/>. </summary>
+        /// <summary> Converts a <see cref="string"/> to a <see cref="CognitiveServicesPrivateEndpointServiceConnectionStatus"/>. </summary>
         public static implicit operator CognitiveServicesPrivateEndpointServiceConnectionStatus(string value) => new CognitiveServicesPrivateEndpointServiceConnectionStatus(value);
 
         /// <inheritdoc />
