cda06285d726c7e13bb92e1528dca980f9057f15
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 14:58:50 +0800

    Enable-WriteCore-feature-for-oracle (#46226)

--- sdk/oracle/Azure.ResourceManager.OracleDatabase/src/Generated/Models/AutonomousDatabaseBackupUpdateProperties.Serialization.cs
+++ sdk/oracle/Azure.ResourceManager.OracleDatabase/src/Generated/Models/AutonomousDatabaseBackupUpdateProperties.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.OracleDatabase.Models
 
         void IJsonModel<AutonomousDatabaseBackupUpdateProperties>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<AutonomousDatabaseBackupUpdateProperties>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(AutonomousDatabaseBackupUpdateProperties)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(RetentionPeriodInDays))
             {
                 writer.WritePropertyName("retentionPeriodInDays"u8);
@@ -46,7 +54,6 @@ namespace Azure.ResourceManager.OracleDatabase.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         AutonomousDatabaseBackupUpdateProperties IJsonModel<AutonomousDatabaseBackupUpdateProperties>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
