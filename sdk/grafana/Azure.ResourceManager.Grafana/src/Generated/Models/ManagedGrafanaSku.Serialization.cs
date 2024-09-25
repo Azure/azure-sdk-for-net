791139b222e3d69bb7ebcab81cc5c4534a514c9d
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 14:01:01 +0800

    Enable-WriteCore-feature-for-grafana (#46166)

--- sdk/grafana/Azure.ResourceManager.Grafana/src/Generated/Models/ManagedGrafanaSku.Serialization.cs
+++ sdk/grafana/Azure.ResourceManager.Grafana/src/Generated/Models/ManagedGrafanaSku.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Grafana.Models
 
         void IJsonModel<ManagedGrafanaSku>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ManagedGrafanaSku>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ManagedGrafanaSku)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("name"u8);
             writer.WriteStringValue(Name);
             if (options.Format != "W" && _serializedAdditionalRawData != null)
@@ -43,7 +51,6 @@ namespace Azure.ResourceManager.Grafana.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ManagedGrafanaSku IJsonModel<ManagedGrafanaSku>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
