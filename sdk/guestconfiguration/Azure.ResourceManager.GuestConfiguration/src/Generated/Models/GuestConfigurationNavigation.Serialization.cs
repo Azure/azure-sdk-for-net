88a7bf95946170395b7a33853e370f753d88c178
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 14:00:17 +0800

    Enable-WriteCore-feature-for-guestconfiguration (#46168)

--- sdk/guestconfiguration/Azure.ResourceManager.GuestConfiguration/src/Generated/Models/GuestConfigurationNavigation.Serialization.cs
+++ sdk/guestconfiguration/Azure.ResourceManager.GuestConfiguration/src/Generated/Models/GuestConfigurationNavigation.Serialization.cs
@@ -21,13 +21,21 @@ namespace Azure.ResourceManager.GuestConfiguration.Models
 
         void IJsonModel<GuestConfigurationNavigation>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<GuestConfigurationNavigation>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(GuestConfigurationNavigation)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Kind))
             {
                 if (Kind != null)
@@ -148,7 +156,6 @@ namespace Azure.ResourceManager.GuestConfiguration.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         GuestConfigurationNavigation IJsonModel<GuestConfigurationNavigation>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
