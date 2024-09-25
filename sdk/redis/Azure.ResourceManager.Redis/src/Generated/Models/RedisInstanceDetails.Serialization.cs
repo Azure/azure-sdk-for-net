2fffb213d5a3881c8950ef8db47a3bd3f50c7537
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-25 14:40:21 +0800

    Enable-WriteCore-feature-for-redis

--- sdk/redis/Azure.ResourceManager.Redis/src/Generated/Models/RedisInstanceDetails.Serialization.cs
+++ sdk/redis/Azure.ResourceManager.Redis/src/Generated/Models/RedisInstanceDetails.Serialization.cs
@@ -20,13 +20,21 @@ namespace Azure.ResourceManager.Redis.Models
 
         void IJsonModel<RedisInstanceDetails>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<RedisInstanceDetails>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(RedisInstanceDetails)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (options.Format != "W" && Optional.IsDefined(SslPort))
             {
                 writer.WritePropertyName("sslPort"u8);
@@ -72,7 +80,6 @@ namespace Azure.ResourceManager.Redis.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         RedisInstanceDetails IJsonModel<RedisInstanceDetails>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
