71b36d3db2c39ced2067ee43166dbc9d8cc5d119
azure-sdk <azuresdk@microsoft.com>
2024-09-25 04:05:18 +0000

    Update SDK code pr_sc_7

--- sdk/resourcemanager/Azure.ResourceManager/src/Resources/Generated/Models/PolicyType.cs
+++ sdk/resourcemanager/Azure.ResourceManager/src/Resources/Generated/Models/PolicyType.cs
@@ -39,7 +39,7 @@ namespace Azure.ResourceManager.Resources.Models
         public static bool operator ==(PolicyType left, PolicyType right) => left.Equals(right);
         /// <summary> Determines if two <see cref="PolicyType"/> values are not the same. </summary>
         public static bool operator !=(PolicyType left, PolicyType right) => !left.Equals(right);
-        /// <summary> Converts a string to a <see cref="PolicyType"/>. </summary>
+        /// <summary> Converts a <see cref="string"/> to a <see cref="PolicyType"/>. </summary>
         public static implicit operator PolicyType(string value) => new PolicyType(value);
 
         /// <inheritdoc />
