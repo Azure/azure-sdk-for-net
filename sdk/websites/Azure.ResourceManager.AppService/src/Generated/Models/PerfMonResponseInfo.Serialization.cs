832f638d8935d5470a74da1f3eed58dbb5316192
Wenming Liu <v-wenmingliu@microsoft.com>
2024-09-26 13:50:52 +0800

    Enable-WriteCore-feature-for-websites

--- sdk/websites/Azure.ResourceManager.AppService/src/Generated/Models/PerfMonResponseInfo.Serialization.cs
+++ sdk/websites/Azure.ResourceManager.AppService/src/Generated/Models/PerfMonResponseInfo.Serialization.cs
@@ -20,13 +20,21 @@ namespace Azure.ResourceManager.AppService.Models
 
         void IJsonModel<PerfMonResponseInfo>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<PerfMonResponseInfo>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(PerfMonResponseInfo)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Code))
             {
                 writer.WritePropertyName("code"u8);
@@ -57,7 +65,6 @@ namespace Azure.ResourceManager.AppService.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         PerfMonResponseInfo IJsonModel<PerfMonResponseInfo>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
