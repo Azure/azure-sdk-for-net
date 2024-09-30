8ee823ae7ddde7f909107506ce20da4be940d94d
Anne Thompson <annelo@microsoft.com>
2024-09-30 15:26:07 -0700

    AddHttpClient

---
+++ sdk/core/System.ClientModel/tests/Options/LoggingOptionsTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ClientModel.Tests;
+using ClientModel.Tests.Mocks;
+using NUnit.Framework;
+using ClientModel.ReferenceClients;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+
+namespace System.ClientModel.Tests.Options;
+
+public class LoggingOptionsTests
+{
+    [Test]
+    public void CanUseHttpClientLoggingInsteadOfScmLogging()
+    {
+        ServiceCollection services = new();
+
+        // see https://learn.microsoft.com/en-us/dotnet/core/extensions/httpclient-factory
+        services.AddHttpClient();
+
+        HttpClient httpClient = new HttpClient();
+
+        RequestResponseClient client = new();
+    }
+}
