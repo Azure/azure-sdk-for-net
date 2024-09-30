5489362d17c288e2742a080b95bff47f72353d5e
Anne Thompson <annelo@microsoft.com>
2024-09-30 14:49:20 -0700

    add reference client

---
+++ sdk/core/System.ClientModel/tests/client/TestClients/RequestResponseClient/RequestResponseClient.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.ClientModel;
+using System.ClientModel.Primitives;
+using System.Threading;
+using System.Threading.Tasks;
+using ClientModel.ReferenceClients.Models;
+
+namespace ClientModel.ReferenceClients;
+
+public class RequestResponseClient
+{
+    private readonly Uri _endpoint;
+    private readonly ApiKeyCredential _credential;
+    private readonly ClientPipeline _pipeline;
+    private readonly string _apiVersion;
+
+    public RequestResponseClient(Uri endpoint, ApiKeyCredential credential, RequestResponseClientOptions? options = default)
+    {
+        Argument.AssertNotNull(endpoint, nameof(endpoint));
+        Argument.AssertNotNull(credential, nameof(credential));
+
+        options ??= new RequestResponseClientOptions();
+
+        _endpoint = endpoint;
+        _credential = credential;
+        _apiVersion = options.Version;
+
+        var authenticationPolicy = ApiKeyAuthenticationPolicy.CreateHeaderApiKeyPolicy(credential, "subscription-key");
+        _pipeline = ClientPipeline.Create(options,
+            perCallPolicies: ReadOnlySpan<PipelinePolicy>.Empty,
+            perTryPolicies: new PipelinePolicy[] { authenticationPolicy },
+            beforeTransportPolicies: ReadOnlySpan<PipelinePolicy>.Empty);
+    }
+
+    // Convenience method - async
+    public Task<ClientResult<OutputModel>> GetModelAsync(InputModel input, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Convenience method - sync
+    public ClientResult<OutputModel> GetModel(InputModel input, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Protocol method - async
+    public Task<ClientResult> GetModelAsync(BinaryContent content, RequestOptions? options = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Protocol method - sync
+    public ClientResult GetModel(BinaryContent content, RequestOptions? options = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Request creation helper
+    private PipelineMessage CreateGetModelRequest(BinaryContent content, RequestOptions? options)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ sdk/core/System.ClientModel/tests/client/TestClients/RequestResponseClient/RequestResponseClientOptions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.ClientModel.Primitives;
+
+namespace ClientModel.ReferenceClients;
+
+public class RequestResponseClientOptions : ClientPipelineOptions
+{
+    private const ServiceVersion LatestVersion = ServiceVersion.V1;
+
+    public enum ServiceVersion
+    {
+        V1 = 1
+    }
+
+    internal string Version { get; }
+
+    public RequestResponseClientOptions(ServiceVersion version = LatestVersion)
+    {
+        Version = version switch
+        {
+            ServiceVersion.V1 => "1.0",
+            _ => throw new NotSupportedException()
+        };
+    }
+}
