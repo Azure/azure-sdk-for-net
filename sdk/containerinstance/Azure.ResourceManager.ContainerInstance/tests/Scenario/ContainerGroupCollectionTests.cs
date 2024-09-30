aded57a7e4356b944aa6a3afdd862425d084bc23
Drishti Peshwani <dpeshwani@microsoft.com>
2024-09-30 14:57:51 +0530

    Added tests for container group profile resource

--- sdk/containerinstance/Azure.ResourceManager.ContainerInstance/tests/Scenario/ContainerGroupCollectionTests.cs
+++ sdk/containerinstance/Azure.ResourceManager.ContainerInstance/tests/Scenario/ContainerGroupCollectionTests.cs
@@ -157,5 +157,146 @@ namespace Azure.ResourceManager.ContainerInstance.Tests
             ContainerGroupResource containerGroup3FromList = await result.FirstOrDefaultAsync(cg => cg.Data.Name.Equals(containerGroupName3));
             VerifyContainerGroupProperties(containerGroup3.Data, containerGroup3FromList.Data);
         }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task CreateOrUpdate_ContainerGroupProfile_RegularPriority()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await CreateResourceGroupAsync(subscription, "testRg", AzureLocation.WestUS);
+            string containerGroupProfileName = Recording.GenerateAssetName("containergrpcgp");
+            var containerGroupProfileData = CreateContainerGroupProfileData(containerGroupProfileName, "Regular");
+            var containerGroupProfiles = rg.GetContainerGroupProfiles();
+            ContainerGroupProfileResource containerGroupProfile = CreateContainerGroupProfileAsync(containerGroupProfileName, containerGroupProfileData, rg).Result;
+            Assert.AreEqual(containerGroupProfileName, containerGroupProfile.Data.Name);
+            VerifyContainerGroupProfileProperties(containerGroupProfileData, containerGroupProfile.Data);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task CreateOrUpdate_ContainerGroupProfile_SpotPriority()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await CreateResourceGroupAsync(subscription, "testRg", AzureLocation.WestUS);
+            string containerGroupProfileName = Recording.GenerateAssetName("containergrpcgp");
+            var containerGroupProfileData = CreateContainerGroupProfileData(containerGroupProfileName, "Spot");
+            var containerGroupProfiles = rg.GetContainerGroupProfiles();
+            ContainerGroupProfileResource containerGroupProfile = CreateContainerGroupProfileAsync(containerGroupProfileName, containerGroupProfileData, rg).Result;
+            Assert.AreEqual(containerGroupProfileName, containerGroupProfile.Data.Name);
+            VerifyContainerGroupProfileProperties(containerGroupProfileData, containerGroupProfile.Data);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task CreateOrUpdate_ContainerGroupProfile_ConfidentialContainerGroup()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await CreateResourceGroupAsync(subscription, "testRg", AzureLocation.WestUS);
+            string containerGroupProfileName = Recording.GenerateAssetName("containergrpcgp");
+            var containerGroupProfileData = CreateContainerGroupProfileData(containerGroupProfileName, isConfidentialSku: true, ccepolicy: null);
+            var containerGroupProfiles = rg.GetContainerGroupProfiles();
+            ContainerGroupProfileResource containerGroupProfile = CreateContainerGroupProfileAsync(containerGroupProfileName, containerGroupProfileData, rg).Result;
+            Assert.AreEqual(containerGroupProfileName, containerGroupProfile.Data.Name);
+            VerifyContainerGroupProfileProperties(containerGroupProfileData, containerGroupProfile.Data);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Get_ContainerGroupProfile_RegularPriority()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await CreateResourceGroupAsync(subscription, "testRg", AzureLocation.WestUS);
+            string containerGroupProfileName = Recording.GenerateAssetName("containergrpcgp");
+            var containerGroupProfileData = CreateContainerGroupProfileData(containerGroupProfileName, "Regular");
+            var containerGroupProfiles = rg.GetContainerGroupProfiles();
+            ContainerGroupProfileResource containerGroupProfile = (await containerGroupProfiles.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName, containerGroupProfileData)).Value;
+
+            ContainerGroupProfileResource retrievedContainerGroupProfile = await containerGroupProfiles.GetAsync(containerGroupProfileName);
+            VerifyContainerGroupProfileProperties(containerGroupProfile.Data, retrievedContainerGroupProfile.Data);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Get_ContainerGroupProfile_SpotPriority()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await CreateResourceGroupAsync(subscription, "testRg", AzureLocation.WestUS);
+            string containerGroupProfileName = Recording.GenerateAssetName("containergrpcgp");
+            var containerGroupProfileData = CreateContainerGroupProfileData(containerGroupProfileName, "Spot");
+            var containerGroupProfiles = rg.GetContainerGroupProfiles();
+            ContainerGroupProfileResource containerGroupProfile = (await containerGroupProfiles.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName, containerGroupProfileData)).Value;
+
+            ContainerGroupProfileResource retrievedContainerGroupProfile = await containerGroupProfiles.GetAsync(containerGroupProfileName);
+            VerifyContainerGroupProfileProperties(containerGroupProfile.Data, retrievedContainerGroupProfile.Data);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task Get_ContainerGroupProfile_ConfidentialContainerGroup()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await CreateResourceGroupAsync(subscription, "testRg", AzureLocation.WestUS);
+            string containerGroupProfileName = Recording.GenerateAssetName("containergrpcgp");
+            var containerGroupProfileData = CreateContainerGroupProfileData(containerGroupProfileName, isConfidentialSku: true, ccepolicy: null);
+            var containerGroupProfiles = rg.GetContainerGroupProfiles();
+            ContainerGroupProfileResource containerGroupProfile = (await containerGroupProfiles.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName, containerGroupProfileData)).Value;
+
+            ContainerGroupProfileResource retrievedContainerGroupProfile = await containerGroupProfiles.GetAsync(containerGroupProfileName);
+            VerifyContainerGroupProfileProperties(containerGroupProfile.Data, retrievedContainerGroupProfile.Data);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task List_ContainerGroupProfile()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg = await CreateResourceGroupAsync(subscription, "testRg", AzureLocation.WestUS);
+            var containerGroupProfiles = rg.GetContainerGroupProfiles();
+
+            string containerGroupProfileName1 = Recording.GenerateAssetName("containergrpcgp");
+            var containerGroupProfileData1 = CreateContainerGroupProfileData(containerGroupProfileName1, "Regular");
+            ContainerGroupProfileResource containerGroupProfile1 = (await containerGroupProfiles.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName1, containerGroupProfileData1)).Value;
+            string containerGroupProfileName2 = Recording.GenerateAssetName("containergrpcgp");
+            var containerGroupProfileData2 = CreateContainerGroupProfileData(containerGroupProfileName2, "Spot");
+            ContainerGroupProfileResource containerGroupProfile2 = (await containerGroupProfiles.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName2, containerGroupProfileData2)).Value;
+
+            AsyncPageable<ContainerGroupProfileResource> result = containerGroupProfiles.GetAllAsync();
+            ContainerGroupProfileResource containerGroupProfile1FromList = await result.FirstOrDefaultAsync(cg => cg.Data.Name.Equals(containerGroupProfileName1));
+            VerifyContainerGroupProfileProperties(containerGroupProfile1.Data, containerGroupProfile1FromList.Data);
+            ContainerGroupProfileResource containerGroupProfile2FromList = await result.FirstOrDefaultAsync(cg => cg.Data.Name.Equals(containerGroupProfileName2));
+            VerifyContainerGroupProfileProperties(containerGroupProfile2.Data, containerGroupProfile2FromList.Data);
+        }
+
+        [TestCase]
+        [RecordedTest]
+        public async Task ListBySubscription_ContainerGroupProfile()
+        {
+            SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
+            ResourceGroupResource rg1 = await CreateResourceGroupAsync(subscription, "testRg", AzureLocation.WestUS);
+            var containerGroupProfiles1 = rg1.GetContainerGroupProfiles();
+            string containerGroupProfileName1 = Recording.GenerateAssetName("containergrpcgp");
+            var containerGroupProfileData1 = CreateContainerGroupProfileData(containerGroupProfileName1, "Regular");
+            ContainerGroupProfileResource containerGroupProfile1 = (await containerGroupProfiles1.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName1, containerGroupProfileData1)).Value;
+
+            ResourceGroupResource rg2 = await CreateResourceGroupAsync(subscription, "testRg2", AzureLocation.WestUS);
+            var containerGroupProfiles2 = rg2.GetContainerGroupProfiles();
+            string containerGroupProfileName2 = Recording.GenerateAssetName("containergrp2cgp");
+            var containerGroupProfileData2 = CreateContainerGroupProfileData(containerGroupProfileName2, "Spot");
+            ContainerGroupProfileResource containerGroupProfile2 = (await containerGroupProfiles2.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName2, containerGroupProfileData2)).Value;
+
+            ResourceGroupResource rg3 = await CreateResourceGroupAsync(subscription, "testRg3", AzureLocation.WestUS);
+            var containerGroupProfiles3 = rg3.GetContainerGroupProfiles();
+            string containerGroupProfileName3 = Recording.GenerateAssetName("containergrp3cgp");
+            var containerGroupProfileData3 = CreateContainerGroupProfileData(containerGroupProfileName3, isConfidentialSku: true, ccepolicy: null);
+            ContainerGroupProfileResource containerGroupProfile3 = (await containerGroupProfiles3.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName3, containerGroupProfileData3)).Value;
+
+            AsyncPageable<ContainerGroupProfileResource> result = subscription.GetContainerGroupProfilesAsync();
+            ContainerGroupProfileResource containerGroupProfile1FromList = await result.FirstOrDefaultAsync(cgp => cgp.Data.Name.Equals(containerGroupProfileName1));
+            VerifyContainerGroupProfileProperties(containerGroupProfile1.Data, containerGroupProfile1FromList.Data);
+            ContainerGroupProfileResource containerGroupProfile2FromList = await result.FirstOrDefaultAsync(cgp => cgp.Data.Name.Equals(containerGroupProfileName2));
+            VerifyContainerGroupProfileProperties(containerGroupProfile2.Data, containerGroupProfile2FromList.Data);
+            ContainerGroupProfileResource containerGroupProfile3FromList = await result.FirstOrDefaultAsync(cgp => cgp.Data.Name.Equals(containerGroupProfileName3));
+            VerifyContainerGroupProfileProperties(containerGroupProfile3.Data, containerGroupProfile3FromList.Data);
+        }
     }
 }
