a96d37a51dea0fe960e232402d02635e3059232c
ShivangiReja <shivangi.reja@microsoft.com>
2024-10-01 12:58:21 -0700

    Move to ai folder

--- sdk/azureai/tools/TestFramework/tests/Helpers/MockClientContext.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-namespace OpenAI.TestFramework.Tests.Helpers;
-
-public class MockClientContext
-{
-    public string Id { get; } = Guid.NewGuid().ToString();
-}
