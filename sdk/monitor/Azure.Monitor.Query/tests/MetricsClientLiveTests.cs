7dff003636e2f10eda09a68780a88a0454b18ff3
nisha-bhatia <67986960+nisha-bhatia@users.noreply.github.com>
2024-10-01 15:01:16 -0700

    update Changelog

--- sdk/monitor/Azure.Monitor.Query/tests/MetricsClientLiveTests.cs
+++ sdk/monitor/Azure.Monitor.Query/tests/MetricsClientLiveTests.cs
@@ -3,14 +3,11 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.TestFramework;
 using Azure.Monitor.Query.Models;
 using NUnit.Framework;
-using NUnit.Framework.Internal.Commands;
 
 namespace Azure.Monitor.Query.Tests
 {
