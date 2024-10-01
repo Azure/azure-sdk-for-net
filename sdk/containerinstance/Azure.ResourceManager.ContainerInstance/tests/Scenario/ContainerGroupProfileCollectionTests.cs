81a72400033763d177348cec741e9f0102b4f037
Drishti Peshwani <dpeshwani@microsoft.com>
2024-10-01 20:06:27 +0530

    Updated the cg profile list and revision tests

--- sdk/containerinstance/Azure.ResourceManager.ContainerInstance/tests/Scenario/ContainerGroupProfileCollectionTests.cs
+++ sdk/containerinstance/Azure.ResourceManager.ContainerInstance/tests/Scenario/ContainerGroupProfileCollectionTests.cs
@@ -115,15 +115,17 @@ namespace Azure.ResourceManager.ContainerInstance.Tests
             string containerGroupProfileName1 = Recording.GenerateAssetName("containergrpcgp1");
             var containerGroupProfileData1 = CreateContainerGroupProfileData(containerGroupProfileName1, "Regular");
             ContainerGroupProfileResource containerGroupProfile1 = (await containerGroupProfiles.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName1, containerGroupProfileData1)).Value;
+
             string containerGroupProfileName2 = Recording.GenerateAssetName("containergrpcgp2");
             var containerGroupProfileData2 = CreateContainerGroupProfileData(containerGroupProfileName2, "Spot");
             ContainerGroupProfileResource containerGroupProfile2 = (await containerGroupProfiles.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName2, containerGroupProfileData2)).Value;
 
-            AsyncPageable<ContainerGroupProfileResource> result = containerGroupProfiles.GetAllAsync();
-            ContainerGroupProfileResource containerGroupProfile1FromList = await result.FirstOrDefaultAsync(cgp => cgp.Data.Name.Equals(containerGroupProfileName1));
-            VerifyContainerGroupProfileProperties(containerGroupProfile1.Data, containerGroupProfile1FromList.Data);
-            ContainerGroupProfileResource containerGroupProfile2FromList = await result.FirstOrDefaultAsync(cgp => cgp.Data.Name.Equals(containerGroupProfileName2));
-            VerifyContainerGroupProfileProperties(containerGroupProfile2.Data, containerGroupProfile2FromList.Data);
+            ContainerGroupProfileCollection result = rg.GetContainerGroupProfiles();
+            ContainerGroupProfileResource retrievedContainerGroupProfile1 = await result.GetAsync(containerGroupProfileName1);
+            VerifyContainerGroupProfileProperties(containerGroupProfile1.Data, retrievedContainerGroupProfile1.Data);
+
+            ContainerGroupProfileResource retrievedContainerGroupProfile2 = await result.GetAsync(containerGroupProfileName2);
+            VerifyContainerGroupProfileProperties(containerGroupProfile2.Data, retrievedContainerGroupProfile2.Data);
         }
 
         [TestCase]
@@ -160,7 +162,7 @@ namespace Azure.ResourceManager.ContainerInstance.Tests
 
         [TestCase]
         [RecordedTest]
-        public async Task List_GetSpecifiedRevision()
+        public async Task Check_ContainerGroupProfile_RevisionUpdate()
         {
             SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
             ResourceGroupResource rg = await CreateResourceGroupAsync(subscription, "testRg", AzureLocation.WestUS);
@@ -172,8 +174,12 @@ namespace Azure.ResourceManager.ContainerInstance.Tests
             var containerGroupProfileData2 = CreateContainerGroupProfileData(containerGroupProfileName, "Regular");
             containerGroupProfile = (await containerGroupProfiles.CreateOrUpdateAsync(WaitUntil.Completed, containerGroupProfileName, containerGroupProfileData2)).Value;
 
-            ContainerGroupProfileRevisionResource retreivedContainerGroupProfileRevision = await containerGroupProfile.GetContainerGroupProfileRevisionAsync("2");
-            VerifyContainerGroupProfileProperties(containerGroupProfile.Data, retreivedContainerGroupProfileRevision.Data);
+            ContainerGroupProfileRevisionCollection result = containerGroupProfile.GetContainerGroupProfileRevisions();
+            Assert.IsNotNull(result);
+            bool revision1exists = await result.ExistsAsync("1");
+            Assert.IsTrue(revision1exists);
+            bool revision2exists = await result.ExistsAsync("2");
+            Assert.IsTrue(revision2exists);
         }
     }
 }
