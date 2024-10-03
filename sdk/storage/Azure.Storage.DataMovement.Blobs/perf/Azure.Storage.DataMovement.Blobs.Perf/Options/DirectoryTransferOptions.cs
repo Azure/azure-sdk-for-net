a2a09f9ccf58cb241564b4cf0d379ec8f3d16a58
Jacob Lauzon <jalauzon@microsoft.com>
2024-10-02 17:44:14 -0700

    Initial perf testing work

---
+++ sdk/storage/Azure.Storage.DataMovement.Blobs/perf/Azure.Storage.DataMovement.Blobs.Perf/Options/DirectoryTransferOptions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Test.Perf;
+using CommandLine;
+
+namespace Azure.Storage.DataMovement.Blobs.Perf
+{
+    public class DirectoryTransferOptions : PerfOptions
+    {
+        [Option('c', "count", Default = 10, HelpText = "Number of items in each transfer.")]
+        public int Count { get; set; }
+
+        [Option('s', "size", Default = 1024, HelpText = "Size of each file (in bytes)")]
+        public long Size { get; set; }
+
+        [Option("initial-transfer-size", HelpText = "The initial size to use during transfers (in bytes)")]
+        public long? InitialTransferSize { get; set; }
+
+        [Option("chunk-size", HelpText = "The chunk/block size to use during transfers (in bytes)")]
+        public long? ChunkSize { get; set; }
+
+        // Override warmup to set default to 0
+        [Option('w', "warmup", Default = 0, HelpText = "Duration of warmup in seconds")]
+        public new int Warmup { get; set; }
+    }
+}
