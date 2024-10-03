d865a49aeb85a8a81bcc6941010b3cd992d9d9e5
Amanda Nguyen <amnguye@microsoft.com>
2024-10-03 10:26:44 -0700

    Cleanup

--- sdk/storage/Azure.Storage.DataMovement.Blobs/tests/BlockBlobStartTransferUploadTests.cs
+++ sdk/storage/Azure.Storage.DataMovement.Blobs/tests/BlockBlobStartTransferUploadTests.cs
@@ -13,8 +13,6 @@ using DMBlobs::Azure.Storage.DataMovement.Blobs;
 using System.IO;
 using Azure.Core.TestFramework;
 using Azure.Core;
-using System.Threading;
-using NUnit.Framework;
 
 namespace Azure.Storage.DataMovement.Blobs.Tests
 {
@@ -102,82 +100,6 @@ namespace Azure.Storage.DataMovement.Blobs.Tests
             }
 
             return InstrumentClientOptions(options);
-        }
-
-        public async Task<StorageResource> GetTemporaryFileStorageResourceAsync(
-                string prefixPath,
-                string fileName = default,
-                int bufferSize = Constants.MB,
-                int? fileSize = 4 * Constants.MB,
-                CancellationToken cancellationToken = default)
-        {
-            if (string.IsNullOrEmpty(prefixPath))
-            {
-                throw new ArgumentException("prefixPath cannot be null or empty", nameof(prefixPath));
-            }
-            fileName ??= GetNewObjectName();
-
-            // Create new source file
-            using Stream originalStream = await CreateLimitedMemoryStream(fileSize.Value);
-            string localSourceFile = Path.Combine(prefixPath, fileName);
-            // create a new file and copy contents of stream into it, and then close the FileStream
-            // so the StagedUploadAsync call is not prevented from reading using its FileStream.
-            Console.Out.Write($"Creating File: {localSourceFile}..");
-            using (FileStream fileStream = File.Create(localSourceFile))
-            {
-                Console.Out.WriteLine("Copying Stream to File..");
-                await originalStream.CopyToAsync(
-                    fileStream,
-                    bufferSize,
-                    cancellationToken: cancellationToken);
-                fileStream.Close();
-            }
-            LocalFilesStorageResourceProvider files = new();
-            return files.FromFile(localSourceFile);
-        }
-
-        [Test]
-        public async Task TempTestAsync()
-        {
-            int _blobSize = Constants.KB;
-            TransferManagerOptions _transferManagerOptions = new();
-            DataTransferOptions _dataTransferOptions = new();
-            CancellationToken cancellationToken = CancellationToken.None;
-            BlobsStorageResourceProvider _blobsStorageResourceProvider = new();
-
-            DisposingLocalDirectory disposingLocalDirectory = DisposingLocalDirectory.GetTestDirectory();
-            IDisposingContainer<BlobContainerClient> disposingBlobContainer = await GetDisposingContainerAsync();
-            BlobContainerClient destinationContainerClient = disposingBlobContainer.Container;
-            await destinationContainerClient.CreateIfNotExistsAsync();
-            string blobName = GetNewObjectName();
-
-            // Create Local Source Storage Resource
-            StorageResource sourceResource = await GetTemporaryFileStorageResourceAsync(
-                disposingLocalDirectory.DirectoryPath,
-                fileName: blobName,
-                fileSize: _blobSize,
-                cancellationToken: cancellationToken);
-
-            // Create Destination Storage Resource
-            BlobUriBuilder blobUriBuilder = new BlobUriBuilder(destinationContainerClient.Uri)
-            {
-                BlobContainerName = destinationContainerClient.Name,
-                BlobName = blobName
-            };
-            BlockBlobClient destinationBlob = destinationContainerClient.GetBlockBlobClient(blobName);
-            StorageResource destinationResource = _blobsStorageResourceProvider.FromClient(destinationBlob);
-
-            // Start Transfer
-            await new TransferValidator()
-            {
-                TransferManager = new(_transferManagerOptions)
-            }.TransferAndVerifyAsync(
-                sourceResource,
-                destinationResource,
-                cToken => Task.FromResult(File.OpenRead(sourceResource.Uri.AbsolutePath) as Stream),
-                async cToken => await destinationBlob.OpenReadAsync(default, cToken),
-                options: _dataTransferOptions,
-                cancellationToken: cancellationToken);
         }
     }
 }
