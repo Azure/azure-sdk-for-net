7589b26299ed955395887623721957ac32d7d230
Timothy Mothra Lee (from Dev Box) <tilee@microsoft.com>
2024-10-03 17:19:12 -0700

    remove counter

--- sdk/monitor/Azure.Monitor.OpenTelemetry.AspNetCore/src/LiveMetrics/DataCollection/DocumentBuffer.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.AspNetCore/src/LiveMetrics/DataCollection/DocumentBuffer.cs
@@ -12,28 +12,14 @@ namespace Azure.Monitor.OpenTelemetry.AspNetCore.LiveMetrics.DataCollection
     internal class DocumentBuffer
     {
         private readonly ConcurrentQueue<DocumentIngress> _documents = new();
-        // ConcurrentQueue<T>.Count is not used because it is not an O(1) operation. Instead, we use a separate counter.
-        // Atomic counter for the number of documents in the queue.
-        private int _count = 0;
 
         public void Add(DocumentIngress document)
         {
-            // Ensure the queue does not exceed capacity.
-            if (Interlocked.CompareExchange(ref _count, 0, 0) < int.MaxValue)
-            {
-                _documents.Enqueue(document);
-                Interlocked.Increment(ref _count);
-            }
-            else
-            {
-                AzureMonitorAspNetCoreEventSource.Log.DroppedDocument(documentType: document.DocumentType);
-            }
+            _documents.Enqueue(document);
         }
 
         public IEnumerable<DocumentIngress> ReadAllAndClear()
         {
-            // There is no need to decrement the count since we are clearing the queue. After this operation, the instance will not be used anymore.
-            // The method 'Add' is not called while this method is running; therefore, the count will remain unchanged.
             while (_documents.TryDequeue(out DocumentIngress? item))
             {
                 yield return item;
