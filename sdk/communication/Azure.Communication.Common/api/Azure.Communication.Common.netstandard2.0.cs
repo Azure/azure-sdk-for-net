2e0cdce4ca7dc144e4bfca819ba684601f5196dc
Aigerim Beishenbekova <aigerimb@microsoft.com>
2024-10-08 10:32:18 +0100

    fixed generated api

--- sdk/communication/Azure.Communication.Common/api/Azure.Communication.Common.netstandard2.0.cs
+++ sdk/communication/Azure.Communication.Common/api/Azure.Communication.Common.netstandard2.0.cs
@@ -56,6 +56,7 @@ namespace Azure.Communication
     }
     public partial class EntraCommunicationTokenCredentialOptions
     {
+        public EntraCommunicationTokenCredentialOptions(string resourceEndpoint, Azure.Core.TokenCredential entraTokenCredential) { }
         public EntraCommunicationTokenCredentialOptions(string resourceEndpoint, Azure.Core.TokenCredential entraTokenCredential, string[] scopes) { }
         public string ResourceEndpoint { get { throw null; } }
         public string[] Scopes { get { throw null; } }
