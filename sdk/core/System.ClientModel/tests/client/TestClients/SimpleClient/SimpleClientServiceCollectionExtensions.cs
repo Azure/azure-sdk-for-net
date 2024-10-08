a737883207578b87ec274448f7a5cc3b252dec2c
Anne Loomis Thompson <annelo@microsoft.com>
2024-10-07 17:51:48 -0700

    backup

--- sdk/core/System.ClientModel/tests/client/TestClients/SimpleClient/SimpleClientServiceCollectionExtensions.cs
+++ sdk/core/System.ClientModel/tests/client/TestClients/SimpleClient/SimpleClientServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
@@ -16,19 +17,50 @@ namespace ClientModel.ReferenceClients.SimpleClient;
 // see: https://learn.microsoft.com/en-us/dotnet/core/extensions/options-library-authors#parameterless
 public static class SimpleClientServiceCollectionExtensions
 {
+    // No parameters uses defaults
     public static IServiceCollection AddSimpleClient(this IServiceCollection services)
     {
         services.AddOptions<SimpleClientOptions>();
+
         services.AddSingleton<SimpleClient>(sp =>
         {
-            IConfiguration configuration = sp.GetRequiredService<IConfiguration>();
+            //IConfiguration configuration = sp.GetRequiredService<IConfiguration>().GetSection("ClientDefaults");
+            ILoggerFactory loggerFactory = sp.GetRequiredService<ILoggerFactory>();
 
             // TODO: to roll a credential, this will need to be IOptionsMonitor
             // not IOptions -- come back to this.
             SimpleClientOptions options = sp.GetRequiredService<IOptions<SimpleClientOptions>>().Value;
 
+            //// TODO: I think this should already have been bound -- but how?
+            //ConfigurationBinder.Bind(configuration, options);
+
             return new SimpleClient(options);
         });
+
+        return services;
+    }
+
+    // Taking an IConfiguration uses that to configure the options per the pattern
+    public static IServiceCollection AddSimpleClient(this IServiceCollection services,
+        IConfiguration configurationSection)
+    {
+        services.Configure<SimpleClientOptions>(configurationSection);
+
+        services.AddSingleton<SimpleClient>(sp =>
+        {
+            //IConfiguration configuration = sp.GetRequiredService<IConfiguration>().GetSection("ClientDefaults");
+            ILoggerFactory loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+
+            // TODO: to roll a credential, this will need to be IOptionsMonitor
+            // not IOptions -- come back to this.
+            SimpleClientOptions options = sp.GetRequiredService<IOptions<SimpleClientOptions>>().Value;
+
+            //// TODO: I think this should already have been bound -- but how?
+            //ConfigurationBinder.Bind(configuration, options);
+
+            return new SimpleClient(options);
+        });
+
         return services;
     }
 }
