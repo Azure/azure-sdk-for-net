80cfda6cce8057bbd9197d38560481e7dc422486
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-08 13:38:29 +0800

    Enable-WriteCore-feature-for-edgeorder

--- sdk/edgeorder/Azure.ResourceManager.EdgeOrder/src/Generated/Models/ProductDescription.Serialization.cs
+++ sdk/edgeorder/Azure.ResourceManager.EdgeOrder/src/Generated/Models/ProductDescription.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.EdgeOrder.Models
 
         void IJsonModel<ProductDescription>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ProductDescription>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ProductDescription)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (options.Format != "W" && Optional.IsDefined(DescriptionType))
             {
                 writer.WritePropertyName("descriptionType"u8);
@@ -86,7 +94,6 @@ namespace Azure.ResourceManager.EdgeOrder.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ProductDescription IJsonModel<ProductDescription>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
