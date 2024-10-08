3a8db9fa5559b7ced05861fb95f29e7f8d19ea29
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-08 10:52:50 +0800

    Enable-WriteCore-feature-for-devtestlabs

--- sdk/devtestlabs/Azure.ResourceManager.DevTestLabs/src/Generated/Models/DevTestLabInboundNatRule.Serialization.cs
+++ sdk/devtestlabs/Azure.ResourceManager.DevTestLabs/src/Generated/Models/DevTestLabInboundNatRule.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.DevTestLabs.Models
 
         void IJsonModel<DevTestLabInboundNatRule>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<DevTestLabInboundNatRule>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(DevTestLabInboundNatRule)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(TransportProtocol))
             {
                 writer.WritePropertyName("transportProtocol"u8);
@@ -56,7 +64,6 @@ namespace Azure.ResourceManager.DevTestLabs.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         DevTestLabInboundNatRule IJsonModel<DevTestLabInboundNatRule>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
