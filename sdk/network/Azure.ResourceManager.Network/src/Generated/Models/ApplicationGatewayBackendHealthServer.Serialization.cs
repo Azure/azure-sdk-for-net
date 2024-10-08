70eba9cbd01441c281a070bee2fc02152bb69684
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-08 16:42:26 +0800

    Enable-WriteCore-feature-for-network

--- sdk/network/Azure.ResourceManager.Network/src/Generated/Models/ApplicationGatewayBackendHealthServer.Serialization.cs
+++ sdk/network/Azure.ResourceManager.Network/src/Generated/Models/ApplicationGatewayBackendHealthServer.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Network.Models
 
         void IJsonModel<ApplicationGatewayBackendHealthServer>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ApplicationGatewayBackendHealthServer>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ApplicationGatewayBackendHealthServer)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Address))
             {
                 writer.WritePropertyName("address"u8);
@@ -61,7 +69,6 @@ namespace Azure.ResourceManager.Network.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ApplicationGatewayBackendHealthServer IJsonModel<ApplicationGatewayBackendHealthServer>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
