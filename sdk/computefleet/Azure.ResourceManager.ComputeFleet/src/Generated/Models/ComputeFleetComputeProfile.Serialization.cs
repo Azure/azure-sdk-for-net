608ca1285473f6ad35b8f2375539f6bd6f074758
azure-sdk <azuresdk@microsoft.com>
2024-10-08 04:08:29 +0000

    Update SDK codes co_de_1

--- sdk/computefleet/Azure.ResourceManager.ComputeFleet/src/Generated/Models/ComputeFleetComputeProfile.Serialization.cs
+++ sdk/computefleet/Azure.ResourceManager.ComputeFleet/src/Generated/Models/ComputeFleetComputeProfile.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.ComputeFleet.Models
 
         void IJsonModel<ComputeFleetComputeProfile>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ComputeFleetComputeProfile>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ComputeFleetComputeProfile)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("baseVirtualMachineProfile"u8);
             writer.WriteObjectValue(BaseVirtualMachineProfile, options);
             if (Optional.IsDefined(ComputeApiVersion))
@@ -53,7 +61,6 @@ namespace Azure.ResourceManager.ComputeFleet.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ComputeFleetComputeProfile IJsonModel<ComputeFleetComputeProfile>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
--- sdk/computefleet/Azure.ResourceManager.ComputeFleet/src/Generated/Models/ComputeFleetVmssOSProfile.Serialization.cs
+++ sdk/computefleet/Azure.ResourceManager.ComputeFleet/src/Generated/Models/ComputeFleetVmssOSProfile.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.ComputeFleet.Models
 
         void IJsonModel<ComputeFleetVmssOSProfile>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ComputeFleetVmssOSProfile>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ComputeFleetVmssOSProfile)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(ComputerNamePrefix))
             {
                 writer.WritePropertyName("computerNamePrefix"u8);
@@ -91,7 +99,6 @@ namespace Azure.ResourceManager.ComputeFleet.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ComputeFleetVmssOSProfile IJsonModel<ComputeFleetVmssOSProfile>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
