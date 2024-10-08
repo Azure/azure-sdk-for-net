2efb676d09eb6b036817bcd13962cf151b02c8f2
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2024-10-07 22:56:44 -0700

    Experiment: Revert some tests improvements to last successful run

--- sdk/keyvault/Azure.Security.KeyVault.Secrets/tests/ContinousAccessEvaluationTests.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Secrets/tests/ContinousAccessEvaluationTests.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Core.Pipeline;
 using Azure.Core.TestFramework;
 using Azure.Security.KeyVault.Tests;
 using NUnit.Framework;
@@ -13,11 +14,18 @@ namespace Azure.Security.KeyVault.Secrets.Tests
     [NonParallelizable]
     internal class ContinousAccessEvaluationTests : ContinuousAccessEvaluationTestsBase
     {
+        [SetUp]
+        public void Setup()
+        {
+            ChallengeBasedAuthenticationPolicy.ClearCache();
+        }
+
         [Test]
         [TestCase(@"Bearer realm="""", authorization_uri=""https://login.microsoftonline.com/common/oauth2/authorize"", error=""insufficient_claims"", claims=""eyJhY2Nlc3NfdG9rZW4iOnsibmJmIjp7ImVzc2VudGlhbCI6dHJ1ZSwidmFsdWUiOiIxNzI2MDc3NTk1In0sInhtc19jYWVlcnJvciI6eyJ2YWx1ZSI6IjEwMDEyIn19fQ==""", """{"access_token":{"nbf":{"essential":true,"value":"1726077595"},"xms_caeerror":{"value":"10012"}}}""")]
         public async Task VerifyCaeClaims(string challenge, string expectedClaims)
         {
             int callCount = 0;
+            string claims = null;
 
             MockResponse responseWithSecret = new MockResponse(200)
             {
@@ -28,20 +36,23 @@ namespace Azure.Security.KeyVault.Secrets.Tests
 
             var credential = new TokenCredentialStub((r, c) =>
             {
+                claims = r.Claims;
                 if (callCount == 0)
                 {
                     // The first challenge should not have any claims.
-                    Assert.IsNull(r.Claims);
+                    Assert.IsNull(claims);
                 }
                 else if (callCount == 1)
                 {
-                    Assert.AreEqual(expectedClaims, r.Claims);
+                    Assert.AreEqual(expectedClaims, claims);
                 }
                 Interlocked.Increment(ref callCount);
                 Assert.AreEqual(true, r.IsCaeEnabled);
 
                 return new(callCount.ToString(), DateTimeOffset.Now.AddHours(2));
             }, true);
+
+            var policy = new BearerTokenAuthenticationPolicy(credential, "scope");
 
             SecretClient client = new(
             VaultUri,
