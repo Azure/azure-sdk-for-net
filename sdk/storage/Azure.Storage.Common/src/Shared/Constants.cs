1a7a69b442acff392f8481dd9e459ab1841818e2
Sean McCullough <seanmcc@microsoft.com>
2024-10-08 19:36:45 -0500

    Added STG 97 x-ms-version

--- sdk/storage/Azure.Storage.Common/src/Shared/Constants.cs
+++ sdk/storage/Azure.Storage.Common/src/Shared/Constants.cs
@@ -25,7 +25,7 @@ namespace Azure.Storage
         /// Gets the default service version to use when building shared access
         /// signatures.
         /// </summary>
-        public const string DefaultSasVersion = "2025-01-05";
+        public const string DefaultSasVersion = "2025-05-05";
 
         /// <summary>
         /// Max download range size while requesting a transactional hash.
