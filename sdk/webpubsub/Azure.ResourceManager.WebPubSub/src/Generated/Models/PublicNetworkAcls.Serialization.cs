63aabcaeecd794d6b573356c487aee7570fbee07
melina5656 <140602263+melina5656@users.noreply.github.com>
2024-10-09 13:57:06 +0800

    Enable WriteCore feature for webpubsub

--- sdk/webpubsub/Azure.ResourceManager.WebPubSub/src/Generated/Models/PublicNetworkAcls.Serialization.cs
+++ sdk/webpubsub/Azure.ResourceManager.WebPubSub/src/Generated/Models/PublicNetworkAcls.Serialization.cs
@@ -21,13 +21,21 @@ namespace Azure.ResourceManager.WebPubSub.Models
 
         void IJsonModel<PublicNetworkAcls>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<PublicNetworkAcls>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(PublicNetworkAcls)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsCollectionDefined(Allow))
             {
                 writer.WritePropertyName("allow"u8);
@@ -63,7 +71,6 @@ namespace Azure.ResourceManager.WebPubSub.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         PublicNetworkAcls IJsonModel<PublicNetworkAcls>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
