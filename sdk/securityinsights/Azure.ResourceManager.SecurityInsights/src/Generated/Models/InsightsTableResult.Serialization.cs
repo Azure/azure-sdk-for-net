f9d9438127caee03e117f1eff80d892f9446b95c
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-09 16:34:15 +0800

    Enable-WriteCore-feature-for-securityinsights

--- sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/InsightsTableResult.Serialization.cs
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/InsightsTableResult.Serialization.cs
@@ -21,13 +21,21 @@ namespace Azure.ResourceManager.SecurityInsights.Models
 
         void IJsonModel<InsightsTableResult>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<InsightsTableResult>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(InsightsTableResult)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsCollectionDefined(Columns))
             {
                 writer.WritePropertyName("columns"u8);
@@ -73,7 +81,6 @@ namespace Azure.ResourceManager.SecurityInsights.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         InsightsTableResult IJsonModel<InsightsTableResult>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
--- sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/SecurityMLAnalyticsSettingsList.Serialization.cs
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/SecurityMLAnalyticsSettingsList.Serialization.cs
@@ -21,13 +21,21 @@ namespace Azure.ResourceManager.SecurityInsights.Models
 
         void IJsonModel<SecurityMLAnalyticsSettingsList>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<SecurityMLAnalyticsSettingsList>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(SecurityMLAnalyticsSettingsList)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (options.Format != "W" && Optional.IsDefined(NextLink))
             {
                 writer.WritePropertyName("nextLink"u8);
@@ -55,7 +63,6 @@ namespace Azure.ResourceManager.SecurityInsights.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         SecurityMLAnalyticsSettingsList IJsonModel<SecurityMLAnalyticsSettingsList>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
