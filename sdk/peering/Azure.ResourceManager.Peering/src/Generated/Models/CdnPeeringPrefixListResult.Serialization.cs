f6127abd436258db3448d6decf70993285814156
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-09 13:54:38 +0800

    Enable-WriteCore-feature-for-peering

--- sdk/peering/Azure.ResourceManager.Peering/src/Generated/Models/CdnPeeringPrefixListResult.Serialization.cs
+++ sdk/peering/Azure.ResourceManager.Peering/src/Generated/Models/CdnPeeringPrefixListResult.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Peering.Models
 
         void IJsonModel<CdnPeeringPrefixListResult>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<CdnPeeringPrefixListResult>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(CdnPeeringPrefixListResult)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsCollectionDefined(Value))
             {
                 writer.WritePropertyName("value"u8);
@@ -56,7 +64,6 @@ namespace Azure.ResourceManager.Peering.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         CdnPeeringPrefixListResult IJsonModel<CdnPeeringPrefixListResult>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
