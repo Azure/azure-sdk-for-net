7a01ac6c08862cc3500877cd2ac248d338d007a3
Amanda Nguyen <48961492+amnguye@users.noreply.github.com>
2024-10-10 16:03:45 -0700

    [Storage] Prepare for patch release for Storage GA releases (except for blobs)  (#46538)
    * Prepare for patch release for storage GA (except for blobs)
    
    * bump queues version
    
    * Updated packages.props to have other dependencies version bumped
    
    * Update System.Memory.Data (#46134)
    
    * link fixes
    
    * Revert "link fixes"
    
    This reverts commit 61d9eaaf21843daf3ec8f7997b2f1be2f842c4d9.
    
    * update
    
    * AOT updates
    
    * SCM test fix
    
    * update API
    
    * Another storage fix
    
    * fixes for communication / openai
    
    * update snippets
    
    * fix
    
    * fixes
    
    ---------
    
    Co-authored-by: Madalyn Redding Heaps <66138537+m-redding@users.noreply.github.com>

--- sdk/openai/tools/TestFramework/src/Mocks/MockRestServiceClient.cs
+++ sdk/openai/tools/TestFramework/src/Mocks/MockRestServiceClient.cs
@@ -88,8 +88,10 @@ public class MockRestServiceClient<TData> : IDisposable
                 .ConfigureAwait(false);
 
             var response = result.GetRawResponse();
+            var entry = response.Content.ToObjectFromJson<MockRestService<TData>.Entry>();
+            var entryData = entry == null ? default : entry.data;
             return ClientResult.FromOptionalValue(
-                response.Content.ToObjectFromJson<MockRestService<TData>.Entry>().data,
+                entryData,
                 response);
         }
         catch (ClientResultException ex)
@@ -118,8 +120,10 @@ public class MockRestServiceClient<TData> : IDisposable
         {
             ClientResult result = SendSyncOrAsync(false, HttpMethod.Get, id, default, token).GetAwaiter().GetResult();
             var response = result.GetRawResponse();
+            var entry = response.Content.ToObjectFromJson<MockRestService<TData>.Entry>();
+            var entryData = entry == null ? default : entry.data;
             return ClientResult.FromOptionalValue(
-                response.Content.ToObjectFromJson<MockRestService<TData>.Entry>().data,
+                entryData,
                 response);
         }
         catch (ClientResultException ex)
@@ -263,7 +267,7 @@ public class MockRestServiceClient<TData> : IDisposable
             if (message.Response.Content?.ToMemory().Length > 0)
             {
                 var error = message.Response.Content.ToObjectFromJson<MockRestService<TData>.Error>();
-                throw new ClientResultException($"Error {error.error}: {error.message}", message.Response);
+                throw new ClientResultException($"Error {error?.error}: {error?.message}", message.Response);
             }
 
             throw new ClientResultException(message.Response);
