94384c78c9c1725887d9907a08b6fe3cd1ea96a2
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-11 15:10:16 +0800

    Enable-WriteCore-feature-for-securityinsights (#46504)

--- sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/ConnectorRequiredPermissions.Serialization.cs
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/src/Generated/Models/ConnectorRequiredPermissions.Serialization.cs
@@ -20,13 +20,21 @@ namespace Azure.ResourceManager.SecurityInsights.Models
 
         void IJsonModel<ConnectorRequiredPermissions>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ConnectorRequiredPermissions>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ConnectorRequiredPermissions)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(IsCustomAction))
             {
                 writer.WritePropertyName("action"u8);
@@ -62,7 +70,6 @@ namespace Azure.ResourceManager.SecurityInsights.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ConnectorRequiredPermissions IJsonModel<ConnectorRequiredPermissions>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
