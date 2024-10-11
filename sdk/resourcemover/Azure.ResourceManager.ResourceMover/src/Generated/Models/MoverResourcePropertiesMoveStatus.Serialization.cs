6af3487061d992919920f85d31d354bae6e36317
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-11 15:21:00 +0800

    Enable-WriteCore-feature-for-resourcemover (#46499)

--- sdk/resourcemover/Azure.ResourceManager.ResourceMover/src/Generated/Models/MoverResourcePropertiesMoveStatus.Serialization.cs
+++ sdk/resourcemover/Azure.ResourceManager.ResourceMover/src/Generated/Models/MoverResourcePropertiesMoveStatus.Serialization.cs
@@ -19,58 +19,22 @@ namespace Azure.ResourceManager.ResourceMover.Models
 
         void IJsonModel<MoverResourcePropertiesMoveStatus>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected override void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<MoverResourcePropertiesMoveStatus>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(MoverResourcePropertiesMoveStatus)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
-            if (options.Format != "W" && Optional.IsDefined(MoveState))
-            {
-                writer.WritePropertyName("moveState"u8);
-                writer.WriteStringValue(MoveState.Value.ToString());
-            }
-            if (Optional.IsDefined(JobStatus))
-            {
-                if (JobStatus != null)
-                {
-                    writer.WritePropertyName("jobStatus"u8);
-                    writer.WriteObjectValue(JobStatus, options);
-                }
-                else
-                {
-                    writer.WriteNull("jobStatus");
-                }
-            }
-            if (Optional.IsDefined(Errors))
-            {
-                if (Errors != null)
-                {
-                    writer.WritePropertyName("errors"u8);
-                    writer.WriteObjectValue(Errors, options);
-                }
-                else
-                {
-                    writer.WriteNull("errors");
-                }
-            }
-            if (options.Format != "W" && _serializedAdditionalRawData != null)
-            {
-                foreach (var item in _serializedAdditionalRawData)
-                {
-                    writer.WritePropertyName(item.Key);
-#if NET6_0_OR_GREATER
-				writer.WriteRawValue(item.Value);
-#else
-                    using (JsonDocument document = JsonDocument.Parse(item.Value))
-                    {
-                        JsonSerializer.Serialize(writer, document.RootElement);
-                    }
-#endif
-                }
-            }
-            writer.WriteEndObject();
+            base.JsonModelWriteCore(writer, options);
         }
 
         MoverResourcePropertiesMoveStatus IJsonModel<MoverResourcePropertiesMoveStatus>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
