a508c5fccd32e73d67fcbd80a4681ac94fd045e4
guptakashish <guptakashish@microsoft.com>
2024-10-11 18:33:02 +0530

    undo last commit

--- sdk/playwrighttesting/Azure.Developer.MicrosoftPlaywrightTesting.NUnit/api/Azure.Developer.MicrosoftPlaywrightTesting.NUnit.netstandard2.0.cs
+++ sdk/playwrighttesting/Azure.Developer.MicrosoftPlaywrightTesting.NUnit/api/Azure.Developer.MicrosoftPlaywrightTesting.NUnit.netstandard2.0.cs
@@ -6,7 +6,7 @@ namespace Azure.Developer.MicrosoftPlaywrightTesting.NUnit
         public PlaywrightServiceNUnit(Azure.Core.TokenCredential? credential = null) : base (default(Azure.Developer.MicrosoftPlaywrightTesting.TestLogger.PlaywrightServiceOptions), default(Azure.Core.TokenCredential)) { }
         public static Azure.Developer.MicrosoftPlaywrightTesting.TestLogger.PlaywrightServiceOptions playwrightServiceOptions { get { throw null; } }
         [NUnit.Framework.OneTimeSetUpAttribute]
-        public System.Threading.Tasks.Task SetupAsync() { throw null; }
+        public System.Threading.Tasks.Task SetupAsync(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         [NUnit.Framework.OneTimeTearDownAttribute]
         public void Teardown() { }
     }
