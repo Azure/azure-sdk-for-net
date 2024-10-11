e84f5af87b3bcc81bad6fe0880c2dbaad12e22e1
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-11 15:07:44 +0800

    Enable-WriteCore-feature-for-servicefabricmanagedclusters (#46518)

--- sdk/servicefabricmanagedclusters/Azure.ResourceManager.ServiceFabricManagedClusters/src/Generated/Models/ManagedAzResiliencyStatus.Serialization.cs
+++ sdk/servicefabricmanagedclusters/Azure.ResourceManager.ServiceFabricManagedClusters/src/Generated/Models/ManagedAzResiliencyStatus.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.ServiceFabricManagedClusters.Models
 
         void IJsonModel<ManagedAzResiliencyStatus>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ManagedAzResiliencyStatus>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ManagedAzResiliencyStatus)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsCollectionDefined(BaseResourceStatus))
             {
                 writer.WritePropertyName("baseResourceStatus"u8);
@@ -56,7 +64,6 @@ namespace Azure.ResourceManager.ServiceFabricManagedClusters.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         ManagedAzResiliencyStatus IJsonModel<ManagedAzResiliencyStatus>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
