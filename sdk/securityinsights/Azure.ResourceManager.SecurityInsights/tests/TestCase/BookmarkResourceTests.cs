94605cd6e3b7f63c059297806056912962a231f0
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-12 17:47:35 +0800

    Update BookmarkResourceTests.cs

--- sdk/securityinsights/Azure.ResourceManager.SecurityInsights/tests/TestCase/BookmarkResourceTests.cs
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/tests/TestCase/BookmarkResourceTests.cs
@@ -61,7 +61,7 @@ namespace Azure.ResourceManager.SecurityInsights.Tests.TestCase
         }
 
         [TestCase]
-        [Ignore("This test case will cause errors in the pipeline. After fixing the problem, restore this test")
+        [Ignore("This test case will cause errors in the pipeline. After fixing the problem, restore this test")]
         public async Task BookmarkResourceApiTests()
         {
             //0.prepare
