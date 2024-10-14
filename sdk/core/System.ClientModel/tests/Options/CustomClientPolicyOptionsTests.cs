e09685a95aa01fb7711a98b74a88608206693b6b
Anne Loomis Thompson <annelo@microsoft.com>
2024-10-14 16:48:08 -0700

    notes on pros and cons of pager policy approaches

--- sdk/core/System.ClientModel/tests/Options/CustomClientPolicyOptionsTests.cs
+++ sdk/core/System.ClientModel/tests/Options/CustomClientPolicyOptionsTests.cs
@@ -8,6 +8,8 @@ using NUnit.Framework;
 
 namespace System.ClientModel.Tests.Options;
 
+// Scenarios where a client provides a custom policy -- how does it expose
+// options to configure the policy's behavior?
 public class CustomClientPolicyOptionsTests
 {
     [Test]
@@ -40,6 +42,23 @@ public class CustomClientPolicyOptionsTests
     [Test]
     public void CanSetInternalPolicyBehaviorsFromClientOptions()
     {
+        // Why would a client use one or another approach?
+        // ... We could say ...
+        //   - Expose a public property on client options to set the policy
+        //     if you want client users to be able to provide their own policy
+        //     implementation
+        //   - Expose the policy as a public type if you want users to be able
+        //     to extend your policy implementation functionality
+        //   - If none of these things are in the future, consider making the
+        //     policy internal and exposing client options to configure it in
+        //     the client's internal pipeline.
+        //
+
+        // Note that there is not a good evolution path with this approach - if
+        // public options aren't added to client options, if you later want to
+        // make the policy public to take the options type as a required
+        // parameter, you'd need to EBN the prior properties.
+
         PagingClientOptions clientOptions = new();
         clientOptions.PagerNumber = "555-555-1234";
 
