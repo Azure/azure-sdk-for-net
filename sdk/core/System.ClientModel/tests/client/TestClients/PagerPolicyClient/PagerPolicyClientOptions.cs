edfd20cad227d6d896a54a1a3947969ec5d64a0d
Anne Loomis Thompson <annelo@microsoft.com>
2024-10-14 17:31:16 -0700

    PagingPolicyClient => PagerPolicyClient

---
+++ sdk/core/System.ClientModel/tests/client/TestClients/PagerPolicyClient/PagerPolicyClientOptions.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.ClientModel.Primitives;
+
+namespace ClientModel.ReferenceClients.PagerPolicyClient;
+
+public class PagerPolicyClientOptions : ClientPipelineOptions
+{
+    private const ServiceVersion LatestVersion = ServiceVersion.V1;
+
+    // TODO: Change to generated code pattern needed to support DI binding
+    // from IConfiguration settings.
+    public PagerPolicyClientOptions() : this(LatestVersion)
+    {
+    }
+
+    public PagerPolicyClientOptions(ServiceVersion version)
+    {
+        Pager = new();
+
+        Version = version switch
+        {
+            ServiceVersion.V1 => "1.0",
+            _ => throw new NotSupportedException()
+        };
+    }
+
+    internal string Version { get; }
+
+    public PagerPolicyOptions Pager { get; }
+
+    public PipelinePolicy? PagerPolicy { get; set; }
+
+    public enum ServiceVersion
+    {
+        V1 = 1
+    }
+
+    // TODO: do we need an "on freeze" ?
+}
