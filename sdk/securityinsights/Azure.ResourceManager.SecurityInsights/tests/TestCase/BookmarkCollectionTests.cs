1d26289cec7962a618a963ecc457d3e4aed1ca4e
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-15 10:03:29 +0800

    remove ignore attribute

--- sdk/securityinsights/Azure.ResourceManager.SecurityInsights/tests/TestCase/BookmarkCollectionTests.cs
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/tests/TestCase/BookmarkCollectionTests.cs
@@ -60,7 +60,6 @@ namespace Azure.ResourceManager.SecurityInsights.Tests.TestCase
         }
 
         [TestCase]
-        [Ignore("This test case will cause errors in the pipeline. After fixing the problem, restore this test")]
         public async Task BookmarkCollectionApiTests()
         {
             //0.prepare
