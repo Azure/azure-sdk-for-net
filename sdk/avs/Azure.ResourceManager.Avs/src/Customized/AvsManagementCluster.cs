2e6514c1dc5ca170130da48fd670c3d4496a86b3
Ana Arias Rosales <anaarias@microsoft.com>
2024-10-15 16:49:20 -0400

    added VsanDatastoreName serialization AVSManagementCluster

--- sdk/avs/Azure.ResourceManager.Avs/src/Customized/AvsManagementCluster.cs
+++ sdk/avs/Azure.ResourceManager.Avs/src/Customized/AvsManagementCluster.cs
@@ -7,11 +7,24 @@
 
 using System;
 using System.Collections.Generic;
+using System.ClientModel.Primitives;
+using System.Text.Json;
 
 namespace Azure.ResourceManager.Avs.Models
 {
     /// <summary> The properties of a management cluster. </summary>
     public partial class AvsManagementCluster : CommonClusterProperties
     {
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected override void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
+            base.JsonModelWriteCore(writer, options);
+            if (Optional.IsDefined(VsanDatastoreName))
+            {
+                writer.WritePropertyName("vsanDatastoreName"u8);
+                writer.WriteStringValue(VsanDatastoreName);
+            }
+        }
     }
 }
