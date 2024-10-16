174e3d8b83cf8c85061ff156acf44069185ee6ff
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-16 10:52:34 +0800

    Enable-WriteCore-feature-for-logic And Release (#46184)
    * Enable-WriteCore-feature-for-logic
    
    * [Ignore("This test case will cause errors in the pipeline. After fixing the problem, restore this test")]
    
    * [Ignore("This test case will cause errors in the pipeline. After fixing the problem, restore this test")]
    
    * release

--- sdk/logic/Azure.ResourceManager.Logic/src/Generated/Models/IntegrationAccountTrackingEventErrorInfo.Serialization.cs
+++ sdk/logic/Azure.ResourceManager.Logic/src/Generated/Models/IntegrationAccountTrackingEventErrorInfo.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.Logic.Models
 
         void IJsonModel<IntegrationAccountTrackingEventErrorInfo>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<IntegrationAccountTrackingEventErrorInfo>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(IntegrationAccountTrackingEventErrorInfo)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Message))
             {
                 writer.WritePropertyName("message"u8);
@@ -51,7 +59,6 @@ namespace Azure.ResourceManager.Logic.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         IntegrationAccountTrackingEventErrorInfo IJsonModel<IntegrationAccountTrackingEventErrorInfo>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
