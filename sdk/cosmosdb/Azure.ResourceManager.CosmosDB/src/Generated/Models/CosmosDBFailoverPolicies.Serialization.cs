ba54b3ccca1629811b0efcd48ded41803529231e
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-17 10:10:50 +0800

    Enable-WriteCore-feature-for-cosmosdb (#46137)
    * Enable-WriteCore-feature-for-cosmosdb
    
    * Update assets.json

--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/Models/CosmosDBFailoverPolicies.Serialization.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/Models/CosmosDBFailoverPolicies.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.CosmosDB.Models
 
         void IJsonModel<CosmosDBFailoverPolicies>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<CosmosDBFailoverPolicies>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(CosmosDBFailoverPolicies)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("failoverPolicies"u8);
             writer.WriteStartArray();
             foreach (var item in FailoverPolicies)
@@ -48,7 +56,6 @@ namespace Azure.ResourceManager.CosmosDB.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         CosmosDBFailoverPolicies IJsonModel<CosmosDBFailoverPolicies>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/Models/MergeParameters.Serialization.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/Models/MergeParameters.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.ResourceManager.CosmosDB.Models
 
         void IJsonModel<MergeParameters>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<MergeParameters>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(MergeParameters)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(IsDryRun))
             {
                 writer.WritePropertyName("isDryRun"u8);
@@ -46,7 +54,6 @@ namespace Azure.ResourceManager.CosmosDB.Models
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         MergeParameters IJsonModel<MergeParameters>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
