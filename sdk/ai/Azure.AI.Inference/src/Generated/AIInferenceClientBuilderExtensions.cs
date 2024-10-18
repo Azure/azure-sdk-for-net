6e961caf775ce8fd18ae62f4556972ed8ffe37fa
trangevi <trangevi@microsoft.com>
2024-10-18 16:04:55 -0700

    Regenerated
    Signed-off-by: trangevi <trangevi@microsoft.com>

--- sdk/ai/Azure.AI.Inference/src/Generated/AIInferenceClientBuilderExtensions.cs
+++ sdk/ai/Azure.AI.Inference/src/Generated/AIInferenceClientBuilderExtensions.cs
@@ -36,7 +36,7 @@ namespace Microsoft.Extensions.Azure
 
         /// <summary> Registers a <see cref="EmbeddingsClient"/> instance. </summary>
         /// <param name="builder"> The builder to register with. </param>
-        /// <param name="endpoint"> The <see cref="Uri"/> to use. </param>
+        /// <param name="endpoint"> Service host. </param>
         /// <param name="credential"> A credential used to authenticate to an Azure Service. </param>
         public static IAzureClientBuilder<EmbeddingsClient, AzureAIInferenceClientOptions> AddEmbeddingsClient<TBuilder>(this TBuilder builder, Uri endpoint, AzureKeyCredential credential)
         where TBuilder : IAzureClientFactoryBuilder
@@ -46,7 +46,7 @@ namespace Microsoft.Extensions.Azure
 
         /// <summary> Registers a <see cref="EmbeddingsClient"/> instance. </summary>
         /// <param name="builder"> The builder to register with. </param>
-        /// <param name="endpoint"> The <see cref="Uri"/> to use. </param>
+        /// <param name="endpoint"> Service host. </param>
         public static IAzureClientBuilder<EmbeddingsClient, AzureAIInferenceClientOptions> AddEmbeddingsClient<TBuilder>(this TBuilder builder, Uri endpoint)
         where TBuilder : IAzureClientFactoryBuilderWithCredential
         {
