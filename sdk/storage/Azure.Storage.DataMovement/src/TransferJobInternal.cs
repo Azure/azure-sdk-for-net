41ffb4452d0c922048133b99b3155aa17c69ea54
nickliu-msft <nicliu@microsoft.com>
2024-10-22 10:44:41 -0400

    removed unused import

--- sdk/storage/Azure.Storage.DataMovement/src/TransferJobInternal.cs
+++ sdk/storage/Azure.Storage.DataMovement/src/TransferJobInternal.cs
@@ -4,7 +4,6 @@ using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
