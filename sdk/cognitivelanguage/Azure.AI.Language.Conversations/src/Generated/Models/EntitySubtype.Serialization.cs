4af2ff6b71f5311f5f5927ff4b1144c9e21ef0c2
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-22 17:01:17 +0800

    Enable-WriteCore-for-Azure.AI.Language.Conversations

--- sdk/cognitivelanguage/Azure.AI.Language.Conversations/src/Generated/Models/EntitySubtype.Serialization.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.Conversations/src/Generated/Models/EntitySubtype.Serialization.cs
@@ -19,13 +19,22 @@ namespace Azure.AI.Language.Conversations.Models
 
         void IJsonModel<EntitySubtype>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected override void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<EntitySubtype>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(EntitySubtype)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
+            base.JsonModelWriteCore(writer, options);
             if (Optional.IsDefined(Value))
             {
                 writer.WritePropertyName("value"u8);
@@ -41,24 +50,6 @@ namespace Azure.AI.Language.Conversations.Models
                 }
                 writer.WriteEndArray();
             }
-            writer.WritePropertyName("extraInformationKind"u8);
-            writer.WriteStringValue(ExtraInformationKind.ToString());
-            if (options.Format != "W" && _serializedAdditionalRawData != null)
-            {
-                foreach (var item in _serializedAdditionalRawData)
-                {
-                    writer.WritePropertyName(item.Key);
-#if NET6_0_OR_GREATER
-				writer.WriteRawValue(item.Value);
-#else
-                    using (JsonDocument document = JsonDocument.Parse(item.Value))
-                    {
-                        JsonSerializer.Serialize(writer, document.RootElement);
-                    }
-#endif
-                }
-            }
-            writer.WriteEndObject();
         }
 
         EntitySubtype IJsonModel<EntitySubtype>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
