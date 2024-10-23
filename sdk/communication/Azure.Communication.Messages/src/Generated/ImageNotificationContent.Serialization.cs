4946872c906e747b033394c34280b567916c66bf
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-23 16:18:05 +0800

    Enable-WriteCore-for-Azure.Communication.Messages

--- sdk/communication/Azure.Communication.Messages/src/Generated/ImageNotificationContent.Serialization.cs
+++ sdk/communication/Azure.Communication.Messages/src/Generated/ImageNotificationContent.Serialization.cs
@@ -19,13 +19,22 @@ namespace Azure.Communication.Messages
 
         void IJsonModel<ImageNotificationContent>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected override void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ImageNotificationContent>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ImageNotificationContent)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
+            base.JsonModelWriteCore(writer, options);
             if (Optional.IsDefined(Caption))
             {
                 writer.WritePropertyName("caption"u8);
@@ -33,33 +42,6 @@ namespace Azure.Communication.Messages
             }
             writer.WritePropertyName("mediaUri"u8);
             writer.WriteStringValue(MediaUri.AbsoluteUri);
-            writer.WritePropertyName("channelRegistrationId"u8);
-            writer.WriteStringValue(ChannelRegistrationId);
-            writer.WritePropertyName("to"u8);
-            writer.WriteStartArray();
-            foreach (var item in To)
-            {
-                writer.WriteStringValue(item);
-            }
-            writer.WriteEndArray();
-            writer.WritePropertyName("kind"u8);
-            writer.WriteStringValue(Kind.ToString());
-            if (options.Format != "W" && _serializedAdditionalRawData != null)
-            {
-                foreach (var item in _serializedAdditionalRawData)
-                {
-                    writer.WritePropertyName(item.Key);
-#if NET6_0_OR_GREATER
-				writer.WriteRawValue(item.Value);
-#else
-                    using (JsonDocument document = JsonDocument.Parse(item.Value))
-                    {
-                        JsonSerializer.Serialize(writer, document.RootElement);
-                    }
-#endif
-                }
-            }
-            writer.WriteEndObject();
         }
 
         ImageNotificationContent IJsonModel<ImageNotificationContent>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
