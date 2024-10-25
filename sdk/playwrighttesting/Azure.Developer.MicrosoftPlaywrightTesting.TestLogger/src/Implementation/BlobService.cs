f808edad2147361a128eb789dee9e54f94c414ed
guptakashish <guptakashish@microsoft.com>
2024-10-25 12:20:59 +0530

    added test for bobService

--- sdk/playwrighttesting/Azure.Developer.MicrosoftPlaywrightTesting.TestLogger/src/Implementation/BlobService.cs
+++ sdk/playwrighttesting/Azure.Developer.MicrosoftPlaywrightTesting.TestLogger/src/Implementation/BlobService.cs
@@ -35,7 +35,7 @@ namespace Azure.Developer.MicrosoftPlaywrightTesting.TestLogger.Implementation
             }
         }
 
-        private static string GetCloudFilePath(string uri, string fileRelativePath)
+        public static string GetCloudFilePath(string uri, string fileRelativePath)
         {
             string[] parts = uri.Split(new string[] { ReporterConstants.s_sASUriSeparator }, StringSplitOptions.None);
             string containerUri = parts[0];
