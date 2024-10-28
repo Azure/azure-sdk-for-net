bbb40171dff9183810427ce69a4891ca1df64ecc
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-28 10:12:46 +0800

    Enable-WriteCore-for-Azure.AI.Vision.Face

--- sdk/face/Azure.AI.Vision.Face/src/Generated/LivenessWithVerifyOutputs.Serialization.cs
+++ sdk/face/Azure.AI.Vision.Face/src/Generated/LivenessWithVerifyOutputs.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.AI.Vision.Face
 
         void IJsonModel<LivenessWithVerifyOutputs>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<LivenessWithVerifyOutputs>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(LivenessWithVerifyOutputs)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("verifyImage"u8);
             writer.WriteObjectValue(VerifyImage, options);
             writer.WritePropertyName("matchConfidence"u8);
@@ -47,7 +55,6 @@ namespace Azure.AI.Vision.Face
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         LivenessWithVerifyOutputs IJsonModel<LivenessWithVerifyOutputs>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
