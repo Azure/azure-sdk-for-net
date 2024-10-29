eb556242d2d6e5da64ad8a6c2138ac7b4bd9c1fb
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-29 14:59:25 +0800

    Enable-WriteCore-for-Azure.Analytics.Purview.DataMap

--- sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AtlasRelatedCategoryHeader.Serialization.cs
+++ sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AtlasRelatedCategoryHeader.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.Analytics.Purview.DataMap
 
         void IJsonModel<AtlasRelatedCategoryHeader>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<AtlasRelatedCategoryHeader>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(AtlasRelatedCategoryHeader)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(CategoryGuid))
             {
                 writer.WritePropertyName("categoryGuid"u8);
@@ -66,7 +74,6 @@ namespace Azure.Analytics.Purview.DataMap
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         AtlasRelatedCategoryHeader IJsonModel<AtlasRelatedCategoryHeader>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
