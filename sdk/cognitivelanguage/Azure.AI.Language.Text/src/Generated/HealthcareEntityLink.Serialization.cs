e3cda658f04d14bc105bad7b4a062c3bdf10918f
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-29 17:27:45 +0800

    Enable-WriteCore-for-Azure.AI.Language.Text (#46754)

--- sdk/cognitivelanguage/Azure.AI.Language.Text/src/Generated/HealthcareEntityLink.Serialization.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.Text/src/Generated/HealthcareEntityLink.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.AI.Language.Text
 
         void IJsonModel<HealthcareEntityLink>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<HealthcareEntityLink>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(HealthcareEntityLink)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("dataSource"u8);
             writer.WriteStringValue(DataSource);
             writer.WritePropertyName("id"u8);
@@ -45,7 +53,6 @@ namespace Azure.AI.Language.Text
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         HealthcareEntityLink IJsonModel<HealthcareEntityLink>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
--- sdk/cognitivelanguage/Azure.AI.Language.Text/src/Generated/LanguageDetectionTextInput.Serialization.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.Text/src/Generated/LanguageDetectionTextInput.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.AI.Language.Text
 
         void IJsonModel<LanguageDetectionTextInput>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<LanguageDetectionTextInput>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(LanguageDetectionTextInput)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsCollectionDefined(LanguageInputs))
             {
                 writer.WritePropertyName("documents"u8);
@@ -51,7 +59,6 @@ namespace Azure.AI.Language.Text
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         LanguageDetectionTextInput IJsonModel<LanguageDetectionTextInput>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
