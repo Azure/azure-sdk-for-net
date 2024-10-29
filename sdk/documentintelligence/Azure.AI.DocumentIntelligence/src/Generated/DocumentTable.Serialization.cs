da6a4dfaa950ad2fa209cc2a9252621231e7b617
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-29 17:31:51 +0800

    Enable-WriteCore-for-Azure.AI.DocumentIntelligence (#46836)

--- sdk/documentintelligence/Azure.AI.DocumentIntelligence/src/Generated/DocumentTable.Serialization.cs
+++ sdk/documentintelligence/Azure.AI.DocumentIntelligence/src/Generated/DocumentTable.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.AI.DocumentIntelligence
 
         void IJsonModel<DocumentTable>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<DocumentTable>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(DocumentTable)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("rowCount"u8);
             writer.WriteNumberValue(RowCount);
             writer.WritePropertyName("columnCount"u8);
@@ -84,7 +92,6 @@ namespace Azure.AI.DocumentIntelligence
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         DocumentTable IJsonModel<DocumentTable>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
