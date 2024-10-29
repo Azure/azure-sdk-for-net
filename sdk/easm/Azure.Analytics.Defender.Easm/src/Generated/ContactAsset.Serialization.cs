ace51c03341caa2730aebc06d64a83e8e6367713
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-29 17:22:11 +0800

    Enable-WriteCore-for-Azure.Analytics.Defender.Easm (#46838)

--- sdk/easm/Azure.Analytics.Defender.Easm/src/Generated/ContactAsset.Serialization.cs
+++ sdk/easm/Azure.Analytics.Defender.Easm/src/Generated/ContactAsset.Serialization.cs
@@ -19,13 +19,22 @@ namespace Azure.Analytics.Defender.Easm
 
         void IJsonModel<ContactAsset>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected override void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<ContactAsset>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(ContactAsset)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
+            base.JsonModelWriteCore(writer, options);
             if (Optional.IsDefined(Email))
             {
                 writer.WritePropertyName("email"u8);
@@ -76,22 +85,6 @@ namespace Azure.Analytics.Defender.Easm
                 writer.WritePropertyName("count"u8);
                 writer.WriteNumberValue(Count.Value);
             }
-            if (options.Format != "W" && _serializedAdditionalRawData != null)
-            {
-                foreach (var item in _serializedAdditionalRawData)
-                {
-                    writer.WritePropertyName(item.Key);
-#if NET6_0_OR_GREATER
-				writer.WriteRawValue(item.Value);
-#else
-                    using (JsonDocument document = JsonDocument.Parse(item.Value))
-                    {
-                        JsonSerializer.Serialize(writer, document.RootElement);
-                    }
-#endif
-                }
-            }
-            writer.WriteEndObject();
         }
 
         ContactAsset IJsonModel<ContactAsset>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
