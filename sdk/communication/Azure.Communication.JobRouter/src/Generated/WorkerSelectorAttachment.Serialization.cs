c4367428aa6b1c1442530bab4412de79208b817c
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-29 17:27:07 +0800

    Enable-WriteCore-for-Azure.Communication.JobRouter (#46772)

--- sdk/communication/Azure.Communication.JobRouter/src/Generated/WorkerSelectorAttachment.Serialization.cs
+++ sdk/communication/Azure.Communication.JobRouter/src/Generated/WorkerSelectorAttachment.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.Communication.JobRouter
 
         void IJsonModel<WorkerSelectorAttachment>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<WorkerSelectorAttachment>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(WorkerSelectorAttachment)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("kind"u8);
             writer.WriteStringValue(Kind.ToString());
             if (options.Format != "W" && _serializedAdditionalRawData != null)
@@ -43,7 +51,6 @@ namespace Azure.Communication.JobRouter
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         WorkerSelectorAttachment IJsonModel<WorkerSelectorAttachment>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
