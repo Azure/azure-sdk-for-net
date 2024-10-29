804f51fab9433ab5743d20d991a585c251e71add
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-29 14:34:39 +0800

    Enable-WriteCore-for-Azure.AI.AnomalyDetector

--- sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/AnomalyInterpretation.Serialization.cs
+++ sdk/anomalydetector/Azure.AI.AnomalyDetector/src/Generated/AnomalyInterpretation.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.AI.AnomalyDetector
 
         void IJsonModel<AnomalyInterpretation>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<AnomalyInterpretation>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(AnomalyInterpretation)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (Optional.IsDefined(Variable))
             {
                 writer.WritePropertyName("variable"u8);
@@ -56,7 +64,6 @@ namespace Azure.AI.AnomalyDetector
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         AnomalyInterpretation IJsonModel<AnomalyInterpretation>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
