ac127ac17deecc4dde36a82339635e6ce7cf01b5
Christopher Scott <chriscott@hotmail.com>
2024-10-31 15:59:06 -0500

    fixup workspace

--- sdk/cloudmachine/Azure.CloudMachine/src/CloudMachineWorkspace.cs
+++ sdk/cloudmachine/Azure.CloudMachine/src/CloudMachineWorkspace.cs
@@ -4,9 +4,11 @@
 using System;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using Azure.Core;
 using Azure.Identity;
-using Azure.Provisioning.CloudMachine;
 using Microsoft.Extensions.Configuration;
 
 namespace Azure.CloudMachine;
@@ -22,30 +24,30 @@ public class CloudMachineWorkspace : ClientWorkspace
     public string Id { get; }
 
     [SuppressMessage("Usage", "AZC0007:DO provide a minimal constructor that takes only the parameters required to connect to the service.", Justification = "<Pending>")]
-    public CloudMachineWorkspace(TokenCredential? credential = default, IConfiguration? configuration = default)
+    public CloudMachineWorkspace(TokenCredential credential = default, IConfiguration configuration = default)
     {
         if (credential != default)
         {
             Credential = credential;
         }
 
-        string? cmid;
+        string cmid;
         if (configuration == default)
         {
-            cmid = AzdHelpers.ReadOrCreateCmid();
+            cmid = ReadOrCreateCmid();
         }
         else
         {
             cmid = configuration["CloudMachine:ID"];
             if (cmid == null)
-            throw new Exception("CloudMachine:ID configuration value missing");
+                throw new Exception("CloudMachine:ID configuration value missing");
         }
 
         Id = cmid!;
     }
 
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public override ClientConnectionOptions GetConnectionOptions(Type clientType, string? instanceId = default)
+    public override ClientConnectionOptions GetConnectionOptions(Type clientType, string instanceId = default)
     {
         string clientId = clientType.FullName;
         switch (clientId)
@@ -55,10 +57,12 @@ public class CloudMachineWorkspace : ClientWorkspace
             case "Azure.Messaging.ServiceBus.ServiceBusClient":
                 return new ClientConnectionOptions(new($"https://{this.Id}.servicebus.windows.net"), Credential);
             case "Azure.Messaging.ServiceBus.ServiceBusSender":
-                if (instanceId == default) instanceId = CloudMachineInfrastructure.SB_PRIVATE_TOPIC;
+                if (instanceId == default)
+                    instanceId = "cm_servicebus_subscription_private";
                 return new ClientConnectionOptions(instanceId);
             case "Azure.Storage.Blobs.BlobContainerClient":
-                if (instanceId == default) instanceId = "default";
+                if (instanceId == default)
+                    instanceId = "default";
                 return new ClientConnectionOptions(new($"https://{this.Id}.blob.core.windows.net/{instanceId}"), Credential);
             case "Azure.AI.OpenAI.AzureOpenAIClient":
                 return new ClientConnectionOptions(new($"https://{this.Id}.openai.azure.com"), Credential);
@@ -72,9 +76,84 @@ public class CloudMachineWorkspace : ClientWorkspace
     }
 
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public override bool Equals(object? obj) => base.Equals(obj);
+    public override bool Equals(object obj) => base.Equals(obj);
     [EditorBrowsable(EditorBrowsableState.Never)]
     public override int GetHashCode() => base.GetHashCode();
     [EditorBrowsable(EditorBrowsableState.Never)]
     public override string ToString() => Id;
+
+    // TODO: Decide if this should live here.
+    internal static string ReadOrCreateCmid()
+    {
+        string appsettings = Path.Combine(".", "appsettings.json");
+
+        string cmid;
+        if (!File.Exists(appsettings))
+        {
+            cmid = GenerateCloudMachineId();
+
+            using FileStream file = File.OpenWrite(appsettings);
+            Utf8JsonWriter writer = new Utf8JsonWriter(file);
+            writer.WriteStartObject();
+            writer.WritePropertyName("CloudMachine"u8);
+            writer.WriteStartObject();
+            writer.WriteString("ID"u8, cmid);
+            writer.WriteEndObject();
+            writer.WriteEndObject();
+            writer.Flush();
+            return cmid;
+        }
+
+        using FileStream json = File.OpenRead(appsettings);
+        using JsonDocument jd = JsonDocument.Parse(json);
+        JsonElement je = jd.RootElement;
+        // attempt to read CM configuration from existing configuration file
+        if (je.TryGetProperty("CloudMachine"u8, out JsonElement cm))
+        {
+            if (!cm.TryGetProperty("ID"u8, out JsonElement id))
+            {
+                throw new NotImplementedException();
+            }
+            cmid = id.GetString();
+            if (cmid == null)
+                throw new NotImplementedException();
+            return cmid;
+        }
+        else
+        {   // add CM configuration to existing file
+            json.Seek(0, SeekOrigin.Begin);
+            JsonNode root = JsonNode.Parse(json);
+            json.Close();
+            if (root is null || root is not JsonObject obj)
+                throw new InvalidOperationException("Existing appsettings.json is not a valid JSON object");
+
+            var cmProperties = new JsonObject();
+            cmid = GenerateCloudMachineId();
+            cmProperties.Add("ID", cmid);
+            obj.Add("CloudMachine", cmProperties);
+
+            using FileStream file = File.OpenWrite(appsettings);
+            JsonWriterOptions writerOptions = new()
+            {
+                Indented = true,
+            };
+            Utf8JsonWriter writer = new(file, writerOptions);
+            JsonSerializerOptions options = new()
+            {
+                WriteIndented = true,
+            };
+            root.WriteTo(writer, options);
+            writer.Flush();
+        }
+
+        return cmid;
+
+        static string GenerateCloudMachineId()
+        {
+            var guid = Guid.NewGuid();
+            var guidString = guid.ToString("N");
+            var cnId = "cm" + guidString.Substring(0, 15); // we can increase it to 20, but the template name cannot be that long
+            return cnId;
+        }
+    }
 }
