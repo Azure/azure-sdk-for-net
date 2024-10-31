ec8a178fef12eafaf3d1284aa43466158ddc071a
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-31 11:30:39 +0800

    Enable-WriteCore-for-Azure.Health.Insights.CancerProfiling (#46858)

--- sdk/healthinsights/Azure.Health.Insights.CancerProfiling/src/Generated/OncoPhenotypeData.Serialization.cs
+++ sdk/healthinsights/Azure.Health.Insights.CancerProfiling/src/Generated/OncoPhenotypeData.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.Health.Insights.CancerProfiling
 
         void IJsonModel<OncoPhenotypeData>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<OncoPhenotypeData>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(OncoPhenotypeData)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             writer.WritePropertyName("patients"u8);
             writer.WriteStartArray();
             foreach (var item in Patients)
@@ -53,7 +61,6 @@ namespace Azure.Health.Insights.CancerProfiling
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         OncoPhenotypeData IJsonModel<OncoPhenotypeData>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
