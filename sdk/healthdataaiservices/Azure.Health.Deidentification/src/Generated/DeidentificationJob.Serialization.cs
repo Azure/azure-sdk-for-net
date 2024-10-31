02d1236dc90a756a6ef20c6fe81678b35cb1e7b6
Wenming Liu <v-wenmingliu@microsoft.com>
2024-10-31 11:30:54 +0800

    Enable-WriteCore-for-Azure.Health.Deidentification (#46857)

--- sdk/healthdataaiservices/Azure.Health.Deidentification/src/Generated/DeidentificationJob.Serialization.cs
+++ sdk/healthdataaiservices/Azure.Health.Deidentification/src/Generated/DeidentificationJob.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.Health.Deidentification
 
         void IJsonModel<DeidentificationJob>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<DeidentificationJob>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(DeidentificationJob)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (options.Format != "W")
             {
                 writer.WritePropertyName("name"u8);
@@ -95,7 +103,6 @@ namespace Azure.Health.Deidentification
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         DeidentificationJob IJsonModel<DeidentificationJob>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
