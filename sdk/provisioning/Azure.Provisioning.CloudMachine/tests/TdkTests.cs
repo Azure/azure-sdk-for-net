6e18cd8cbb24a8327090adcc61e2f3d988265a57
Krzysztof Cwalina <kcwalina@microsoft.com>
2024-11-04 15:24:51 -0800

    PR feedback

--- sdk/provisioning/Azure.Provisioning.CloudMachine/tests/TdkTests.cs
+++ sdk/provisioning/Azure.Provisioning.CloudMachine/tests/TdkTests.cs
@@ -3,8 +3,10 @@
 
 #nullable enable
 
+using System;
+using System.ClientModel.TypeSpec;
+using System.IO;
 using System.Threading.Tasks;
-using Azure.Provisioning.CloudMachine;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
@@ -13,15 +15,46 @@ namespace Azure.CloudMachine.Tests;
 
 public class TdkTests
 {
-    [Theory]
-    [TestCase([new string[] { "-tsp" }])]
-    public void BasicTsp(string[] args)
+    [Test]
+    public void GenerateIAssistantService()
     {
-        CloudMachineInfrastructure.Configure(args, (cm) =>
-        {
-            cm.AddEndpoints<IAssistantService>();
-        });
+        MemoryStream stream = new();
+        TypeSpecWriter.WriteServer<IAssistantService>(stream);
+        stream.Position = 0;
+
+        BinaryData data = BinaryData.FromStream(stream);
+        Assert.AreEqual(IAssistantServiceTsp, data.ToString());
     }
+
+    private static string IAssistantServiceTsp =
+    """
+    import "@typespec/http";
+    import "@typespec/rest";
+    import "@azure-tools/typespec-client-generator-core";
+
+    @service({
+      title: "AssistantService",
+    })
+
+    namespace Azure.CloudMachine.Tests;
+
+    using TypeSpec.Http;
+    using TypeSpec.Rest;
+    using Azure.ClientGenerator.Core;
+
+    @client interface AssistantServiceClient {
+      @put @route("upload") Upload(@header contentType: "application/octet-stream", @body document: bytes) : {
+        @statusCode statusCode: 200;
+        @body response : void;
+      };
+      @get @route("send") Send(@query message: string) : {
+        @statusCode statusCode: 200;
+        @body response : string;
+      };
+    }
+
+
+    """;
 }
 
 internal interface IAssistantService
