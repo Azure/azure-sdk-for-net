f9995e7189263cff16dba238cab30cc0a4e6ed03
Krzysztof Cwalina <kcwalina@microsoft.com>
2024-11-07 11:04:53 -0800

    Added CM ASP.NET extensions and CM metapackage (#47021)
    * added ASp.NET extensions package
    
    * added ASP.NET extensions package
    
    * updated apis

---
+++ sdk/cloudmachine/Azure.CloudMachine.Web/api/Azure.CloudMachine.Web.net8.0.cs
@@ -0,0 +1,8 @@
+namespace System.ClientModel.TypeSpec
+{
+    public static partial class CloudMachineExtensions
+    {
+        public static void Map<T>(this Microsoft.AspNetCore.Routing.IEndpointRouteBuilder routeBuilder, T serviceImplementation) where T : class { }
+        public static System.Threading.Tasks.Task UploadFormAsync(this Azure.CloudMachine.StorageServices storage, Microsoft.AspNetCore.Http.HttpRequest multiPartFormData) { throw null; }
+    }
+}
