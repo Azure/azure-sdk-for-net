8d82464e74bae7ac0a3da236becac61320165299
Christopher Scott <chriscott@hotmail.com>
2024-11-12 15:20:54 -0600

    wip

--- sdk/tables/Azure.Data.Tables/tests/TableClientQueryExpressionTests.cs
+++ sdk/tables/Azure.Data.Tables/tests/TableClientQueryExpressionTests.cs
@@ -23,6 +23,7 @@ namespace Azure.Data.Tables.Tests
         private const string TableName = "someTableName";
         private const string TableName2 = "otherTableName";
         private const string Partition = "partition";
+        private const string MixedCasePK = "PartitionKey";
         private const string Row = "row";
         private const int SomeInt = 10;
         private const double SomeDouble = 10.10;
@@ -94,6 +95,8 @@ namespace Azure.Data.Tables.Tests
         private static readonly Expression<Func<TableEntity, bool>> s_tableEntExpImplicitBooleanCmpCasted = ent => (bool)ent.GetBoolean("Bool");
         private static readonly Expression<Func<TableEntity, bool>> s_tableEntExpImplicitBooleanCmpOr = ent => ent.GetBoolean("Bool").Value || (bool)ent.GetBoolean("Bool");
         private static readonly Expression<Func<TableEntity, bool>> s_tableEntExpImplicitBooleanCmpNot = ent => !ent.GetBoolean("Bool").Value;
+        private static readonly Expression<Func<TableEntity, bool>> s_tableEntExpEquals = ent => ent.PartitionKey.Equals(MixedCasePK);
+        private static readonly Expression<Func<TableEntity, bool>> s_tableEntExpEqualsOIC = ent => ent.PartitionKey.Equals(MixedCasePK, StringComparison.OrdinalIgnoreCase);
 
         public static object[] TableEntityExpressionTestCases =
         {
@@ -125,7 +128,7 @@ namespace Azure.Data.Tables.Tests
             new object[] { $"not (Bool eq true)", s_complexExpImplicitBooleanCmpNot },
             new object[] { $"BoolN eq true", s_complexExpImplicitCastedNullableBooleanCmp },
             new object[] { $"(Bool eq true) and (BoolN eq true)", s_complexExpImplicitBooleanCmpAnd },
-            new object[] { $"(Bool eq true) or (BoolN eq true)", s_complexExpImplicitCastedBooleanCmpOr }
+            new object[] { $"(Bool eq true) or (BoolN eq true)", s_complexExpImplicitCastedBooleanCmpOr },
         };
 
         public static object[] TableItemExpressionTestCases =
@@ -162,7 +165,9 @@ namespace Azure.Data.Tables.Tests
             new object[] { $"Bool eq true", s_tableEntExpImplicitBooleanCmp },
             new object[] { $"Bool eq true", s_tableEntExpImplicitBooleanCmpCasted },
             new object[] { $"(Bool eq true) or (Bool eq true)", s_tableEntExpImplicitBooleanCmpOr },
-            new object[] { $"not (Bool eq true)", s_tableEntExpImplicitBooleanCmpNot }
+            new object[] { $"not (Bool eq true)", s_tableEntExpImplicitBooleanCmpNot },
+            new object[] { $"PartitionKey eq '{MixedCasePK}'", s_tableEntExpEquals },
+            new object[] { $"PartitionKey eq '{MixedCasePK.ToLower()}'", s_tableEntExpEqualsOIC },
         };
 
         [TestCaseSource(nameof(TableItemExpressionTestCases))]
