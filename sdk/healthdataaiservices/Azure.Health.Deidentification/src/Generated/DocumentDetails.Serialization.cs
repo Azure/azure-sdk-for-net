bf8e76534e0f68a7a71e47b063f724a983e3ad6c
Alexa Thomases <athomases@microsoft.com>
2024-11-14 18:24:48 -0800

    Updated serialization with dotnet build codegen

--- sdk/healthdataaiservices/Azure.Health.Deidentification/src/Generated/DocumentDetails.Serialization.cs
+++ sdk/healthdataaiservices/Azure.Health.Deidentification/src/Generated/DocumentDetails.Serialization.cs
@@ -19,13 +19,21 @@ namespace Azure.Health.Deidentification
 
         void IJsonModel<DocumentDetails>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
+            writer.WriteStartObject();
+            JsonModelWriteCore(writer, options);
+            writer.WriteEndObject();
+        }
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
             var format = options.Format == "W" ? ((IPersistableModel<DocumentDetails>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(DocumentDetails)} does not support writing '{format}' format.");
             }
 
-            writer.WriteStartObject();
             if (options.Format != "W")
             {
                 writer.WritePropertyName("id"u8);
@@ -60,7 +68,6 @@ namespace Azure.Health.Deidentification
 #endif
                 }
             }
-            writer.WriteEndObject();
         }
 
         DocumentDetails IJsonModel<DocumentDetails>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
