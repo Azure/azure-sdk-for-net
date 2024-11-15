6e1bc99ec5d71600e0a807c5c6120fa219a0ca39
Madalyn Redding <mredding@microsoft.com>
2024-11-15 18:29:35 -0500

    feedback and get latest changes

--- sdk/communication/Azure.Communication.CallAutomation/src/CallAutomationEventProcessor/EventBacklog.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/CallAutomationEventProcessor/EventBacklog.cs
@@ -52,16 +52,16 @@ namespace Azure.Communication.CallAutomation
         {
             // Match any event that matches in the events backlog
             var matchingKvp = _eventBacklog.FirstOrDefault(kvp => predicate(kvp.Value.Item1));
+            matchingEvent = default;
 
             // Try remove the item - if successful, return it as keyValuePair
             if (matchingKvp.Key != default && _eventBacklog.TryRemove(matchingKvp.Key, out var returnedValue))
             {
-                matchingEvent = new KeyValuePair<string, CallAutomationEventBase>(matchingKvp.Key, returnedValue.Item1);
+                matchingEvent = new KeyValuePair<string, CallAutomationEventBase>(matchingEvent.Key, returnedValue.Item1);
                 return true;
             }
             else
             {
-                matchingEvent = default;
                 return false;
             }
         }
