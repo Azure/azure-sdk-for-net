634bf0df939d3865a7d97c7fd0a9d450e00ac963
Madalyn Redding <mredding@microsoft.com>
2024-11-18 10:18:11 -0500

    maps

--- sdk/maps/Azure.Maps.Search/src/MapsSearchClient.cs
+++ sdk/maps/Azure.Maps.Search/src/MapsSearchClient.cs
@@ -274,7 +274,7 @@ namespace Azure.Maps.Search
                 }
 
                 IEnumerable<double> coordinates = null;
-#pragma warning disable CS8073 // The result of the expression is always true in .NET 8, but not this is not true in netstandard2.0
+#pragma warning disable CS8073 // The result of the expression is always true in net8.0, but not this is not true in netstandard2.0
                 if (options.Coordinates != null)
 #pragma warning restore CS8073
                 {
@@ -312,7 +312,7 @@ namespace Azure.Maps.Search
                 }
 
                 IEnumerable<double> coordinates = null;
-#pragma warning disable CS8073 // The result of the expression is always true in .NET 8, but not this is not true in netstandard2.0
+#pragma warning disable CS8073 // The result of the expression is always true in net8.0, but not this is not true in netstandard2.0
                 if (options.Coordinates != null)
 #pragma warning restore CS8073
                 {
@@ -348,7 +348,7 @@ namespace Azure.Maps.Search
                 }
 
                 IEnumerable<double> coordinatesList = null;
-#pragma warning disable CS8073 // The result of the expression is always true in .NET 8, but not this is not true in netstandard2.0
+#pragma warning disable CS8073 // The result of the expression is always true in net8.0, but not this is not true in netstandard2.0
                 if (coordinates != null)
 #pragma warning restore CS8073
                 {
@@ -384,7 +384,7 @@ namespace Azure.Maps.Search
                 }
 
                 IEnumerable<double> coordinatesList = null;
-#pragma warning disable CS8073 // The result of the expression is always true in .NET 8, but not this is not true in netstandard2.0
+#pragma warning disable CS8073 // The result of the expression is always true in net8.0, but not this is not true in netstandard2.0
                 if (coordinates != null)
 #pragma warning restore CS8073
                 {
@@ -472,7 +472,7 @@ namespace Azure.Maps.Search
             {
                 ReverseGeocodingBatchRequestItem item = new ReverseGeocodingBatchRequestItem();
                 item.OptionalId = query.OptionalId;
-#pragma warning disable CS8073 // The result of the expression is always true in .NET 8, but not this is not true in netstandard2.0
+#pragma warning disable CS8073 // The result of the expression is always true in net8.0, but not this is not true in netstandard2.0
                 if (query.Coordinates != null)
 #pragma warning restore CS8073
                 {
