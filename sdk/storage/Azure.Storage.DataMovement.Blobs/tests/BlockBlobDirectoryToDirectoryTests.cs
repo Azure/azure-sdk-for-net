e68e25067dd7292637c30045b157b372c6f1df76
Jacob Lauzon <jalauzon@microsoft.com>
2024-11-18 16:15:51 -0800

    Unused import

--- sdk/storage/Azure.Storage.DataMovement.Blobs/tests/BlockBlobDirectoryToDirectoryTests.cs
+++ sdk/storage/Azure.Storage.DataMovement.Blobs/tests/BlockBlobDirectoryToDirectoryTests.cs
@@ -11,7 +11,6 @@ using BaseBlobs::Azure.Storage.Blobs;
 using BaseBlobs::Azure.Storage.Blobs.Models;
 using BaseBlobs::Azure.Storage.Blobs.Specialized;
 using DMBlobs::Azure.Storage.DataMovement.Blobs;
-using NUnit.Framework;
 
 namespace Azure.Storage.DataMovement.Blobs.Tests
 {
