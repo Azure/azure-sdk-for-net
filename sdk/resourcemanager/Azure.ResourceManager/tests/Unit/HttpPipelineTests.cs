1965735320d0de9c7edf59698998f9705544897b
Mingzhe Huang (from Dev Box) <mingzhehuang@microsoft.com>
2024-11-19 17:01:34 +0800

    add invalid cae response cas

--- sdk/resourcemanager/Azure.ResourceManager/tests/Unit/HttpPipelineTests.cs
+++ sdk/resourcemanager/Azure.ResourceManager/tests/Unit/HttpPipelineTests.cs
@@ -34,7 +34,34 @@ namespace Azure.ResourceManager.Tests.Unit
             Assert.IsEmpty(subscription.Data.Tags);
         }
 
+        [Test]
+        public void InvalidCaeResponse()
+        {
+            int callCount = 0;
+            var option = new ArmClientOptions()
+            {
+                // we mock a CAE challenge before the actual response is returned
+                Transport = new MockTransport((r) =>
+                {
+                    callCount++;
+
+                    if (callCount == 1)
+                    {
+                        return new MockResponse(401).WithHeader("WWW-Authenticate", InvalidCaeChallenge);
+                    }
+                    var response = new MockResponse(200);
+                    response.SetContent(SubscriptionData);
+                    return response;
+                })
+            };
+            var client = new ArmClient(new MockCredential(), "83aa47df-e3e9-49ff-877b-94304bf3d3ad", option);
+            Assert.Throws<RequestFailedException>(() => client.GetDefaultSubscription());
+            Assert.AreEqual(1, callCount);
+        }
+
         private const string CaeChallenge = """Bearer realm="", error_description="Continuous access evaluation resulted in challenge", error="insufficient_claims", claims="eyJhY2Nlc3NfdG9rZW4iOnsibmJmIjp7ImVzc2VudGlhbCI6dHJ1ZSwgInZhbHVlIjoiMTcyNjI1ODEyMiJ9fX0=" """;
+
+        private const string InvalidCaeChallenge = """Bearer realm="", error_description="", error="insufficient_claims", claims="" """;
 
         private const string SubscriptionData = @"{
   ""id"": ""/subscriptions/83aa47df-e3e9-49ff-877b-94304bf3d3ad"",
