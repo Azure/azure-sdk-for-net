3f43a74d8ddd01fc0bf8ac9e26ddb6d6a3ec4846
azure-sdk <azuresdk@microsoft.com>
2024-11-19 06:49:19 +0000

    Update SDK code co_de_2

--- sdk/costmanagement/Azure.ResourceManager.CostManagement/samples/Generated/Samples/Sample_TenantScheduledActionCollection.cs
+++ sdk/costmanagement/Azure.ResourceManager.CostManagement/samples/Generated/Samples/Sample_TenantScheduledActionCollection.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager.CostManagement.Models;
+using Azure.ResourceManager.Resources;
 using NUnit.Framework;
 
 namespace Azure.ResourceManager.CostManagement.Samples
@@ -28,33 +29,22 @@ namespace Azure.ResourceManager.CostManagement.Samples
             // authenticate your client
             ArmClient client = new ArmClient(cred);
 
-            // this example assumes you already have this TenantResource created on azure
-            // for more information of creating TenantResource, please refer to the document of TenantResource
-            var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+            TenantResource tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
 
             // get the collection of this TenantScheduledActionResource
             TenantScheduledActionCollection collection = tenantResource.GetTenantScheduledActions();
 
             // invoke the operation
             string name = "monthlyCostByResource";
-            ScheduledActionData data = new ScheduledActionData()
+            ScheduledActionData data = new ScheduledActionData
             {
                 DisplayName = "Monthly Cost By Resource",
-                Notification = new NotificationProperties(new string[]
-            {
-"user@gmail.com","team@gmail.com"
-            }, "Cost by resource this month"),
+                Notification = new NotificationProperties(new string[] { "user@gmail.com", "team@gmail.com" }, "Cost by resource this month"),
                 Schedule = new ScheduleProperties(ScheduleFrequency.Monthly, DateTimeOffset.Parse("2020-06-19T22:21:51.1287144Z"), DateTimeOffset.Parse("2021-06-19T22:21:51.1287144Z"))
                 {
                     HourOfDay = 10,
-                    DaysOfWeek =
-{
-ScheduledActionDaysOfWeek.Monday
-},
-                    WeeksOfMonth =
-{
-ScheduledActionWeeksOfMonth.First,ScheduledActionWeeksOfMonth.Third
-},
+                    DaysOfWeek = { ScheduledActionDaysOfWeek.Monday },
+                    WeeksOfMonth = { ScheduledActionWeeksOfMonth.First, ScheduledActionWeeksOfMonth.Third },
                 },
                 Status = ScheduledActionStatus.Enabled,
                 ViewId = new ResourceIdentifier("/providers/Microsoft.CostManagement/views/swaggerExample"),
@@ -83,9 +73,7 @@ ScheduledActionWeeksOfMonth.First,ScheduledActionWeeksOfMonth.Third
             // authenticate your client
             ArmClient client = new ArmClient(cred);
 
-            // this example assumes you already have this TenantResource created on azure
-            // for more information of creating TenantResource, please refer to the document of TenantResource
-            var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+            TenantResource tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
 
             // get the collection of this TenantScheduledActionResource
             TenantScheduledActionCollection collection = tenantResource.GetTenantScheduledActions();
@@ -113,9 +101,7 @@ ScheduledActionWeeksOfMonth.First,ScheduledActionWeeksOfMonth.Third
             // authenticate your client
             ArmClient client = new ArmClient(cred);
 
-            // this example assumes you already have this TenantResource created on azure
-            // for more information of creating TenantResource, please refer to the document of TenantResource
-            var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+            TenantResource tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
 
             // get the collection of this TenantScheduledActionResource
             TenantScheduledActionCollection collection = tenantResource.GetTenantScheduledActions();
@@ -139,9 +125,7 @@ ScheduledActionWeeksOfMonth.First,ScheduledActionWeeksOfMonth.Third
             // authenticate your client
             ArmClient client = new ArmClient(cred);
 
-            // this example assumes you already have this TenantResource created on azure
-            // for more information of creating TenantResource, please refer to the document of TenantResource
-            var tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
+            TenantResource tenantResource = client.GetTenants().GetAllAsync().GetAsyncEnumerator().Current;
 
             // get the collection of this TenantScheduledActionResource
             TenantScheduledActionCollection collection = tenantResource.GetTenantScheduledActions();
