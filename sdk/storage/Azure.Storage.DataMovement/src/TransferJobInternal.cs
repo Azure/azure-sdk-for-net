4d42d7b91407bf21426acd67cc08ea19e0c68f71
Nick Liu <nicliu@microsoft.com>
2024-11-22 14:29:30 -0500

    Initial commit (#47268)

--- sdk/storage/Azure.Storage.DataMovement/src/TransferJobInternal.cs
+++ sdk/storage/Azure.Storage.DataMovement/src/TransferJobInternal.cs
@@ -283,7 +283,12 @@ namespace Azure.Storage.DataMovement
         {
             try
             {
-                await OnJobStateChangedAsync(DataTransferState.InProgress).ConfigureAwait(false);
+                // only change state to 'InProgress' if it was 'Queued'
+                // (this is to prevent 'Pausing' to be changed)
+                if (_dataTransfer.TransferStatus.State == DataTransferState.Queued)
+                {
+                    await OnJobStateChangedAsync(DataTransferState.InProgress).ConfigureAwait(false);
+                }
             }
             catch (Exception ex)
             {
