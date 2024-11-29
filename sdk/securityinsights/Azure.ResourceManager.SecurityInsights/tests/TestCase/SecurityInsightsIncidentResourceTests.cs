85bc876142b48d662369098ab045eb68bd66892c
mcgallan <1186003288@qq.com>
2024-11-29 16:04:45 +0800

    Create SecurityInsightsIncidentResourceTests.cs

---
+++ sdk/securityinsights/Azure.ResourceManager.SecurityInsights/tests/TestCase/SecurityInsightsIncidentResourceTests.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.Identity;
+using Azure.ResourceManager.OperationalInsights;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
+using Azure.ResourceManager.SecurityInsights.Models;
+using Azure.ResourceManager.SecurityInsights.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Azure.ResourceManager.SecurityInsights.Tests
+{
+    public class SecurityInsightsIncidentResourceTests : SecurityInsightsManagementTestBase
+    {
+        public SecurityInsightsIncidentResourceTests(bool isAsync)
+            : base(isAsync, RecordedTestMode.Record)
+        {
+        }
+
+        private async Task<ResourceGroupResource> GetResourceGroupAsync()
+        {
+            var resourceGroup = await CreateResourceGroupAsync();
+            return resourceGroup;
+        }
+
+        #region Workspace
+        private OperationalInsightsWorkspaceCollection GetWorkspaceCollectionAsync(ResourceGroupResource resourceGroup)
+        {
+            return resourceGroup.GetOperationalInsightsWorkspaces();
+        }
+        private async Task<OperationalInsightsWorkspaceResource> GetWorkspaceResourceAsync(ResourceGroupResource resourceGroup)
+        {
+            var workspaceCollection = GetWorkspaceCollectionAsync(resourceGroup);
+            var workspaceName1 = groupName + "-ws";
+            var workspaceInput = GetWorkspaceData();
+            var lrow = await workspaceCollection.CreateOrUpdateAsync(WaitUntil.Completed, workspaceName1, workspaceInput);
+            OperationalInsightsWorkspaceResource workspace = lrow.Value;
+            return workspace;
+        }
+        #endregion
+
+        #region Onboard
+        private async Task<SecurityInsightsSentinelOnboardingStateResource> GetSentinelOnboardingStateResourceAsync(OperationalInsightsWorkspaceSecurityInsightsResource operationalInsights)
+        {
+            var onboardCollection = operationalInsights.GetSecurityInsightsSentinelOnboardingStates();
+            var onboardName = "default";
+            var onboardInput = ResourceDataHelpers.GetSentinelOnboardingStateData();
+            var lroo = await onboardCollection.CreateOrUpdateAsync(WaitUntil.Completed, onboardName, onboardInput);
+            SecurityInsightsSentinelOnboardingStateResource onboard1 = lroo.Value;
+            return onboard1;
+        }
+        #endregion
+
+        private SecurityInsightsIncidentCollection GetIncidentCollectionAsync(OperationalInsightsWorkspaceSecurityInsightsResource operationalInsights)
+        {
+            return operationalInsights.GetSecurityInsightsIncidents();
+        }
+
+        [TestCase]
+        public async Task GetEntitiesResultsTest()
+        {
+            var resourceGroup = await GetResourceGroupAsync();
+            var workspace = await GetWorkspaceResourceAsync(resourceGroup);
+            var workspaceName = groupName + "-ws";
+            var ResourceID = CreateResourceIdentifier("4d042dc6-fe17-4698-a23f-ec6a8d1e98f4", groupName, workspaceName);
+            var operationalInsights = new OperationalInsightsWorkspaceSecurityInsightsResource(Client, ResourceID);
+            var sOS = await GetSentinelOnboardingStateResourceAsync(operationalInsights);
+            var collection = GetIncidentCollectionAsync(operationalInsights);
+            var name = Recording.GenerateAssetName("incidents-");
+            var input = ResourceDataHelpers.GetIncidentData();
+            var lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, name, input);
+            SecurityInsightsIncidentResource incident1 = lro.Value;
+            var entity1 = new SecurityInsightsUriEntity();
+            var entities = await incident1.GetEntitiesResultAsync(CancellationToken.None);
+        }
+    }
+}
