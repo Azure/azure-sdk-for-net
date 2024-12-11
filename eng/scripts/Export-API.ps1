e8fc3f30bef39493c239b77d1cfc192cccbfc9b3
Prajjwal Kamboj <kamboj.prjwl@gmail.com>
2024-12-11 15:24:25 +0530

    Revert Export API script change

--- eng/scripts/Export-API.ps1
+++ eng/scripts/Export-API.ps1
@@ -17,6 +17,7 @@ $servicesProj = Resolve-Path "$PSScriptRoot/../service.proj"
 $debugLogging = $env:SYSTEM_DEBUG -eq "true"
 $logsFolder = $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 $diagnosticArguments = ($debugLogging -and $logsFolder) ? "/binarylogger:$logsFolder/exportapi.binlog" : ""
+
 dotnet build /t:ExportApi /p:RunApiCompat=false /p:InheritDocEnabled=false /p:GeneratePackageOnBuild=false /p:Configuration=Release /p:IncludeSamples=false /p:IncludePerf=false /p:IncludeStress=false /p:IncludeTests=false /p:Scope="$ServiceDirectory" /p:SDKType=$SDKType /restore $servicesProj $diagnosticArguments
 
 if ($SpellCheckPublicApiSurface) {
