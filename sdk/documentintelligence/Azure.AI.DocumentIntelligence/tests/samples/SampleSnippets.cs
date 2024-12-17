16a2a0ab432ec3b5b68cb94bc8a5eb74abf542e2
Caio Saldanha <camaiaor@microsoft.com>
2024-12-16 16:06:29 -0800

    Fixing build issue

--- sdk/documentintelligence/Azure.AI.DocumentIntelligence/tests/samples/SampleSnippets.cs
+++ sdk/documentintelligence/Azure.AI.DocumentIntelligence/tests/samples/SampleSnippets.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Azure.AI.DocumentIntelligence.Tests;
 using Azure.Core.TestFramework;
+using Azure.Identity;
 
 namespace Azure.AI.DocumentIntelligence.Samples
 {
