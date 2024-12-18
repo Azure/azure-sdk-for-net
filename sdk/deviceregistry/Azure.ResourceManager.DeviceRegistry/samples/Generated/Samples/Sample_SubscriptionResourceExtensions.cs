d370c492094431fa7a9c90cba67d550ed2f6b0a7
Arthur Ma <mars.arthur@263.net>
2024-12-18 08:24:29 +0800

    Update Sample_SubscriptionResourceExtensions.cs

--- sdk/deviceregistry/Azure.ResourceManager.DeviceRegistry/samples/Generated/Samples/Sample_SubscriptionResourceExtensions.cs
+++ sdk/deviceregistry/Azure.ResourceManager.DeviceRegistry/samples/Generated/Samples/Sample_SubscriptionResourceExtensions.cs
@@ -19,32 +19,6 @@ namespace Azure.ResourceManager.DeviceRegistry.Samples
     {
         [Test]
         [Ignore("Only validating compilation of examples")]
-        public async Task GetOperationStatu_GetOperationStatus()
-        {
-            // Generated from example definition: 2024-09-01-preview/Get_OperationStatus.json
-            // this example is just showing the usage of "OperationStatus_Get" operation, for the dependent resources, they will have to be created separately.
-
-            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
-            TokenCredential cred = new DefaultAzureCredential();
-            // authenticate your client
-            ArmClient client = new ArmClient(cred);
-
-            // this example assumes you already have this SubscriptionResource created on azure
-            // for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
-            string subscriptionId = "00000000-0000-0000-0000-000000000000";
-            ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
-            SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
-
-            // invoke the operation
-            string location = "testLocation";
-            string operationId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
-            OperationStatusResult result = await subscriptionResource.GetOperationStatuAsync(location, operationId);
-
-            Console.WriteLine($"Succeeded: {result}");
-        }
-
-        [Test]
-        [Ignore("Only validating compilation of examples")]
         public async Task GetDeviceRegistryAssets_ListAssetsSubscription()
         {
             // Generated from example definition: 2024-09-01-preview/List_Assets_Subscription.json
