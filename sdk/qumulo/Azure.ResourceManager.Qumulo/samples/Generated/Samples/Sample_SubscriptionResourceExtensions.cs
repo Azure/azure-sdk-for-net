dbb48aac6aff3b3b465c75126b2836c3a3ebd3b5
Ajay Verma <aries.ajay1@outlook.com>
2024-12-19 09:53:40 +0000

    new SDK changes for Qumulo

--- sdk/qumulo/Azure.ResourceManager.Qumulo/samples/Generated/Samples/Sample_SubscriptionResourceExtensions.cs
+++ sdk/qumulo/Azure.ResourceManager.Qumulo/samples/Generated/Samples/Sample_SubscriptionResourceExtensions.cs
@@ -18,10 +18,10 @@ namespace Azure.ResourceManager.Qumulo.Samples
     {
         [Test]
         [Ignore("Only validating compilation of examples")]
-        public async Task GetQumuloFileSystemResources_FileSystemsListBySubscriptionMaximumSetGen()
+        public async Task GetFileSystemResources_FileSystemsListBySubscription()
         {
-            // Generated from example definition: specification/liftrqumulo/resource-manager/Qumulo.Storage/stable/2022-10-12/examples/FileSystems_ListBySubscription_MaximumSet_Gen.json
-            // this example is just showing the usage of "FileSystems_ListBySubscription" operation, for the dependent resources, they will have to be created separately.
+            // Generated from example definition: 2024-06-19/FileSystems_ListBySubscription_MaximumSet_Gen.json
+            // this example is just showing the usage of "FileSystemResource_ListBySubscription" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
             TokenCredential cred = new DefaultAzureCredential();
@@ -30,16 +30,16 @@ namespace Azure.ResourceManager.Qumulo.Samples
 
             // this example assumes you already have this SubscriptionResource created on azure
             // for more information of creating SubscriptionResource, please refer to the document of SubscriptionResource
-            string subscriptionId = "ulseeqylxb";
+            string subscriptionId = "382E8C7A-AC80-4D70-8580-EFE99537B9B7";
             ResourceIdentifier subscriptionResourceId = SubscriptionResource.CreateResourceIdentifier(subscriptionId);
             SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
 
             // invoke the operation and iterate over the result
-            await foreach (QumuloFileSystemResource item in subscriptionResource.GetQumuloFileSystemResourcesAsync())
+            await foreach (FileSystemResource item in subscriptionResource.GetFileSystemResourcesAsync())
             {
                 // the variable item is a resource, you could call other operations on this instance as well
                 // but just for demo, we get its data from this resource instance
-                QumuloFileSystemResourceData resourceData = item.Data;
+                FileSystemResourceData resourceData = item.Data;
                 // for demo we just print out the id
                 Console.WriteLine($"Succeeded on id: {resourceData.Id}");
             }
@@ -49,10 +49,10 @@ namespace Azure.ResourceManager.Qumulo.Samples
 
         [Test]
         [Ignore("Only validating compilation of examples")]
-        public async Task GetQumuloFileSystemResources_FileSystemsListBySubscriptionMinimumSetGen()
+        public async Task GetFileSystemResources_FileSystemsListBySubscriptionMinimumSetGen()
         {
-            // Generated from example definition: specification/liftrqumulo/resource-manager/Qumulo.Storage/stable/2022-10-12/examples/FileSystems_ListBySubscription_MinimumSet_Gen.json
-            // this example is just showing the usage of "FileSystems_ListBySubscription" operation, for the dependent resources, they will have to be created separately.
+            // Generated from example definition: 2024-06-19/FileSystems_ListBySubscription_MinimumSet_Gen.json
+            // this example is just showing the usage of "FileSystemResource_ListBySubscription" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
             TokenCredential cred = new DefaultAzureCredential();
@@ -66,11 +66,11 @@ namespace Azure.ResourceManager.Qumulo.Samples
             SubscriptionResource subscriptionResource = client.GetSubscriptionResource(subscriptionResourceId);
 
             // invoke the operation and iterate over the result
-            await foreach (QumuloFileSystemResource item in subscriptionResource.GetQumuloFileSystemResourcesAsync())
+            await foreach (FileSystemResource item in subscriptionResource.GetFileSystemResourcesAsync())
             {
                 // the variable item is a resource, you could call other operations on this instance as well
                 // but just for demo, we get its data from this resource instance
-                QumuloFileSystemResourceData resourceData = item.Data;
+                FileSystemResourceData resourceData = item.Data;
                 // for demo we just print out the id
                 Console.WriteLine($"Succeeded on id: {resourceData.Id}");
             }
--- sdk/qumulo/Azure.ResourceManager.Qumulo/src/Generated/Models/MarketplaceSubscriptionStatus.cs
+++ sdk/qumulo/Azure.ResourceManager.Qumulo/src/Generated/Models/MarketplaceSubscriptionStatus.cs
@@ -5,18 +5,53 @@
 
 #nullable disable
 
+using System;
+using System.ComponentModel;
+
 namespace Azure.ResourceManager.Qumulo.Models
 {
     /// <summary> Marketplace subscription status of the file system resource. </summary>
-    public enum MarketplaceSubscriptionStatus
+    public readonly partial struct MarketplaceSubscriptionStatus : IEquatable<MarketplaceSubscriptionStatus>
     {
+        private readonly string _value;
+
+        /// <summary> Initializes a new instance of <see cref="MarketplaceSubscriptionStatus"/>. </summary>
+        /// <exception cref="ArgumentNullException"> <paramref name="value"/> is null. </exception>
+        public MarketplaceSubscriptionStatus(string value)
+        {
+            _value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        private const string PendingFulfillmentStartValue = "PendingFulfillmentStart";
+        private const string SubscribedValue = "Subscribed";
+        private const string SuspendedValue = "Suspended";
+        private const string UnsubscribedValue = "Unsubscribed";
+
         /// <summary> Fulfillment has not started. </summary>
-        PendingFulfillmentStart,
+        public static MarketplaceSubscriptionStatus PendingFulfillmentStart { get; } = new MarketplaceSubscriptionStatus(PendingFulfillmentStartValue);
         /// <summary> Marketplace offer is subscribed. </summary>
-        Subscribed,
+        public static MarketplaceSubscriptionStatus Subscribed { get; } = new MarketplaceSubscriptionStatus(SubscribedValue);
         /// <summary> Marketplace offer is suspended because of non payment. </summary>
-        Suspended,
+        public static MarketplaceSubscriptionStatus Suspended { get; } = new MarketplaceSubscriptionStatus(SuspendedValue);
         /// <summary> Marketplace offer is unsubscribed. </summary>
-        Unsubscribed
+        public static MarketplaceSubscriptionStatus Unsubscribed { get; } = new MarketplaceSubscriptionStatus(UnsubscribedValue);
+        /// <summary> Determines if two <see cref="MarketplaceSubscriptionStatus"/> values are the same. </summary>
+        public static bool operator ==(MarketplaceSubscriptionStatus left, MarketplaceSubscriptionStatus right) => left.Equals(right);
+        /// <summary> Determines if two <see cref="MarketplaceSubscriptionStatus"/> values are not the same. </summary>
+        public static bool operator !=(MarketplaceSubscriptionStatus left, MarketplaceSubscriptionStatus right) => !left.Equals(right);
+        /// <summary> Converts a <see cref="string"/> to a <see cref="MarketplaceSubscriptionStatus"/>. </summary>
+        public static implicit operator MarketplaceSubscriptionStatus(string value) => new MarketplaceSubscriptionStatus(value);
+
+        /// <inheritdoc />
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj) => obj is MarketplaceSubscriptionStatus other && Equals(other);
+        /// <inheritdoc />
+        public bool Equals(MarketplaceSubscriptionStatus other) => string.Equals(_value, other._value, StringComparison.InvariantCultureIgnoreCase);
+
+        /// <inheritdoc />
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode() => _value != null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(_value) : 0;
+        /// <inheritdoc />
+        public override string ToString() => _value;
     }
 }
