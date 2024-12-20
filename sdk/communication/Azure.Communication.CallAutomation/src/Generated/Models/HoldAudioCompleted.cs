214282ff0e6b98072ef8d04840bfdba2fbe4a693
Vinothini Dharmaraj <v-vdharmaraj@microsoft.com>
2024-12-20 13:41:19 -0800

    adding the events

--- sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/HoldAudioCompleted.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/HoldAudioCompleted.cs
@@ -14,20 +14,5 @@ namespace Azure.Communication.CallAutomation
         internal HoldAudioCompleted()
         {
         }
-
-        /// <summary> Initializes a new instance of <see cref="HoldAudioCompleted"/>. </summary>
-        /// <param name="callConnectionId"> Call connection ID. </param>
-        /// <param name="serverCallId"> Server call ID. </param>
-        /// <param name="correlationId"> Correlation ID for event to call correlation. Also called ChainId for skype chain ID. </param>
-        /// <param name="operationContext"> Used by customers when calling mid-call actions to correlate the request to the response event. </param>
-        /// <param name="resultInformation"> Contains the resulting SIP code, sub-code and message. </param>
-        internal HoldAudioCompleted(string callConnectionId, string serverCallId, string correlationId, string operationContext, ResultInformation resultInformation)
-        {
-            CallConnectionId = callConnectionId;
-            ServerCallId = serverCallId;
-            CorrelationId = correlationId;
-            OperationContext = operationContext;
-            ResultInformation = resultInformation;
-        }
     }
 }
--- sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/HoldAudioStarted.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/Generated/Models/HoldAudioStarted.cs
@@ -14,20 +14,5 @@ namespace Azure.Communication.CallAutomation
         internal HoldAudioStarted()
         {
         }
-
-        /// <summary> Initializes a new instance of <see cref="HoldAudioStarted"/>. </summary>
-        /// <param name="callConnectionId"> Call connection ID. </param>
-        /// <param name="serverCallId"> Server call ID. </param>
-        /// <param name="correlationId"> Correlation ID for event to call correlation. Also called ChainId for skype chain ID. </param>
-        /// <param name="operationContext"> Used by customers when calling mid-call actions to correlate the request to the response event. </param>
-        /// <param name="resultInformation"> Contains the resulting SIP code, sub-code and message. </param>
-        internal HoldAudioStarted(string callConnectionId, string serverCallId, string correlationId, string operationContext, ResultInformation resultInformation)
-        {
-            CallConnectionId = callConnectionId;
-            ServerCallId = serverCallId;
-            CorrelationId = correlationId;
-            OperationContext = operationContext;
-            ResultInformation = resultInformation;
-        }
     }
 }
