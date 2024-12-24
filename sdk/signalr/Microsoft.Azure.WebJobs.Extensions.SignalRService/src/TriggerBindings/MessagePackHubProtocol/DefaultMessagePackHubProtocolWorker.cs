0047da907f13ac9c730c1cd9cb0787967daa9b14
Zitong Yang <zityang@microsoft.com>
2024-12-24 13:34:09 +0800

    Add MessagePack Hub Protocol

--- sdk/signalr/Microsoft.Azure.WebJobs.Extensions.SignalRService/src/TriggerBindings/MessagePackHubProtocol/DefaultMessagePackHubProtocolWorker.cs
+++ sdk/signalr/Microsoft.Azure.WebJobs.Extensions.SignalRService/src/TriggerBindings/MessagePackHubProtocol/DefaultMessagePackHubProtocolWorker.cs
@@ -1,5 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 using System;
 using System.IO;
@@ -8,6 +8,11 @@ using MessagePack;
 
 namespace Microsoft.AspNetCore.SignalR.Protocol;
 
+#nullable enable
+
+/// <summary>
+/// Copied from https://github.com/dotnet/aspnetcore/blob/0825def633c99d9fdd74e47e69bcde3935a5fe74/
+/// </summary>
 internal sealed class DefaultMessagePackHubProtocolWorker : MessagePackHubProtocolWorker
 {
     private readonly MessagePackSerializerOptions _messagePackSerializerOptions;
