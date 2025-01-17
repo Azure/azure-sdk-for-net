969db65a3c06878d0039051b7b065428ee4f8bbb
Davide Montanari <damontan@microsoft.com>
2025-01-16 17:36:48 -0800

    Fixed string

--- sdk/deviceregistry/Azure.ResourceManager.DeviceRegistry/tests/Scenario/DeviceRegistryDiscoveredAssetsOperationsTest.cs
+++ sdk/deviceregistry/Azure.ResourceManager.DeviceRegistry/tests/Scenario/DeviceRegistryDiscoveredAssetsOperationsTest.cs
@@ -41,7 +41,7 @@ namespace Azure.ResourceManager.DeviceRegistry.Tests.Scenario
                 Properties = new("assetEndpointProfileReference", "discoveryIdSample", 1)
                 {
                     Manufacturer = "Contoso",
-                    ManufacturerUri = "http://contoso.com"
+                    ManufacturerUri = "http://contoso.com/"
                 }
             };
             var discoveredAssetCreateOrUpdateResponse = await discoveredAssetsCollection.CreateOrUpdateAsync(WaitUntil.Completed, discoveredAssetName, discoveredAssetData, CancellationToken.None);
