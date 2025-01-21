9db53e192d5cf5437208793419f46c92afa43193
Wei Hu <live1206@gmail.com>
2025-01-21 11:00:56 +0800

    Skip empty client (#47859)

--- eng/packages/http-client-csharp/generator/Azure.Generator/src/AzureTypeFactory.cs
+++ eng/packages/http-client-csharp/generator/Azure.Generator/src/AzureTypeFactory.cs
@@ -109,8 +109,16 @@ namespace Azure.Generator
         }
 
         /// <inheritdoc/>
-        protected override ClientProvider CreateClientCore(InputClient inputClient)
-            => AzureClientPlugin.Instance.IsAzureArm.Value ? base.CreateClientCore(InputClientTransformer.TransformInputClient(inputClient)) : base.CreateClientCore(inputClient);
+        protected override ClientProvider? CreateClientCore(InputClient inputClient)
+        {
+            if (!AzureClientPlugin.Instance.IsAzureArm.Value)
+            {
+                return base.CreateClientCore(inputClient);
+            }
+
+            var transformedClient = InputClientTransformer.TransformInputClient(inputClient);
+            return transformedClient is null ? null : base.CreateClientCore(transformedClient);
+        }
 
         /// <inheritdoc/>
         protected override IReadOnlyList<TypeProvider> CreateSerializationsCore(InputType inputType, TypeProvider typeProvider)
