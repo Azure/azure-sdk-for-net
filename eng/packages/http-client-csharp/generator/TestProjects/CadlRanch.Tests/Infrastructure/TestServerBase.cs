ba2f09223ec8d431b443c8d682813d7a89aa50e5
m-nash <64171366+m-nash@users.noreply.github.com>
2025-01-24 10:31:08 -0800

    use process tracker from testframework

--- eng/packages/http-client-csharp/generator/TestProjects/CadlRanch.Tests/Infrastructure/TestServerBase.cs
+++ eng/packages/http-client-csharp/generator/TestProjects/CadlRanch.Tests/Infrastructure/TestServerBase.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Azure.Core.TestFramework;
 using System;
 using System.Diagnostics;
 using System.IO;
