d17defbc77ec012940315e7bda6436a861ac612b
Ben Broderick Phillips <bebroder@microsoft.com>
2025-02-04 14:02:39 -0500

    Use supportsSafeSecretStandard parameter instead

--- eng/common/TestResources/New-TestResources.ps1
+++ eng/common/TestResources/New-TestResources.ps1
@@ -528,7 +528,7 @@ try {
         $templateParameters.Add('azsdkPipelineSubnetList', @($env:PoolSubnet))
     }
     # Some arm/bicep templates may want to change deployment settings (e.g. local auth) in sandboxed TME tenants
-    $templateParameters.Add('tenantIsTME', ($context.Tenant.Name -like '*TME*'))
+    $templateParameters.Add('supportsSafeSecretStandard', ($context.Tenant.Name -like '*TME*'))
 
     $defaultCloudParameters = LoadCloudConfig $Environment
     MergeHashes $defaultCloudParameters $(Get-Variable templateParameters)
