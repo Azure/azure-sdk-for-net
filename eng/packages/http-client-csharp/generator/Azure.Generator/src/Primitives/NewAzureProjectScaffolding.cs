6665a1ef78368edf01a1f2ab16c55ba18531f7d4
Wei Hu <huwe@microsoft.com>
2025-02-06 11:25:26 +0800

    Add project scaffolding for test projects

--- eng/packages/http-client-csharp/generator/Azure.Generator/src/Primitives/NewAzureProjectScaffolding.cs
+++ eng/packages/http-client-csharp/generator/Azure.Generator/src/Primitives/NewAzureProjectScaffolding.cs
@@ -29,30 +29,82 @@ namespace Azure.Generator.Primitives
                 PackageTags = AzureClientPlugin.Instance.Configuration.RootNamespace,
                 GenerateDocumentationFile = true,
             };
-            foreach (var packages in _unbrandedDependencyPackages)
+
+            foreach (var packages in _azureDependencyPackages)
             {
                 builder.PackageReferences.Add(packages);
             }
 
+            int pathSegmentCount = GetPathSegmentCount();
             if (AzureClientPlugin.Instance.InputLibrary.InputNamespace.Auth.ApiKey is not null)
             {
-                ReadOnlySpan<char> text = AzureClientPlugin.Instance.Configuration.OutputDirectory.AsSpan();
-                // we are either a spector project in the eng folder or a real sdk in the sdk folder
-                int beginning = text.IndexOf("eng");
-                if (beginning == -1)
-                {
-                    beginning = text.IndexOf("sdk");
-                }
-                text = text.Slice(beginning);
-                // starting with 2 to include eng at the beginning and src at the end
-                int pathSegmentCount = 2 + text.Count('/');
-                // count both path separators to normalize
-                pathSegmentCount += text.Count('\\');
                 builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("AzureKeyCredentialPolicy.cs", pathSegmentCount), "Shared/Core"));
+            }
+
+            TraverseInput(out bool hasOperation, out bool hasLongRunningOperation);
+            if (hasOperation)
+            {
                 builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("RawRequestUriBuilder.cs", pathSegmentCount), "Shared/Core"));
             }
 
+            if (hasLongRunningOperation)
+            {
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("AppContextSwitchHelper.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("AsyncLockWithValue.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("FixedDelayWithNoJitterStrategy.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("ClientDiagnostics.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("DiagnosticScopeFactory.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("DiagnosticScope.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("HttpMessageSanitizer.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("IOperationSource.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("NextLinkOperationImplementation.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("OperationFinalStateVia.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("OperationInternal.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("OperationInternalBase.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("OperationInternalOfT.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("OperationPoller.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("SequentialDelayStrategy.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("TaskExtensions.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("TrimmingAttribute.cs", pathSegmentCount), "Shared/Core"));
+                builder.CompileIncludes.Add(new CSharpProjectWriter.CSProjCompileInclude(GetCompileInclude("VoidValue.cs", pathSegmentCount), "Shared/Core"));
+            }
+
             return builder.Write();
+        }
+
+        private void TraverseInput(out bool hasOperation, out bool hasLongRunningOperation)
+        {
+            hasOperation = false;
+            hasLongRunningOperation = false;
+            foreach (var inputClient in AzureClientPlugin.Instance.InputLibrary.InputNamespace.Clients)
+            {
+                foreach (var operation in inputClient.Operations)
+                {
+                    hasOperation = true;
+                    if (operation.LongRunning != null)
+                    {
+                        hasLongRunningOperation = true;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static int GetPathSegmentCount()
+        {
+            ReadOnlySpan<char> text = AzureClientPlugin.Instance.Configuration.OutputDirectory.AsSpan();
+            // we are either a spector project in the eng folder or a real sdk in the sdk folder
+            int beginning = text.IndexOf("eng");
+            if (beginning == -1)
+            {
+                beginning = text.IndexOf("sdk");
+            }
+            text = text.Slice(beginning);
+            // starting with 2 to include eng at the beginning and src at the end
+            int pathSegmentCount = 2 + text.Count('/');
+            // count both path separators to normalize
+            pathSegmentCount += text.Count('\\');
+            return pathSegmentCount;
         }
 
         private string GetCompileInclude(string fileName, int pathSegmentCount)
@@ -60,11 +112,24 @@ namespace Azure.Generator.Primitives
             return $"{MSBuildThisFileDirectory}{string.Concat(Enumerable.Repeat(ParentDirectory, pathSegmentCount))}{RelativeCoreSegment}{fileName}";
         }
 
-        private static readonly IReadOnlyList<CSharpProjectWriter.CSProjDependencyPackage> _unbrandedDependencyPackages =
-        [
-            new("Azure.Core"),
-            new("System.Text.Json")
-        ];
+        private static readonly IReadOnlyList<CSharpProjectWriter.CSProjDependencyPackage> _azureDependencyPackages =
+            AzureClientPlugin.Instance.IsAzureArm.Value == true
+            ? [
+                new("Azure.Core"),
+                new("Azure.ResourceManager"),
+                new("System.Text.Json"),
+                new("System.ClientModel")
+            ]
+            : [
+                new("Azure.Core"),
+                new("System.Text.Json")
+            ];
+
+        private static readonly IReadOnlyList<CSharpProjectWriter.CSProjDependencyPackage> _brandedDependencyPackages =
+            [
+                new("Azure.Core"),
+                new("System.Text.Json")
+            ];
 
         /// <inheritdoc/>
         protected override string GetSolutionFileContent()
