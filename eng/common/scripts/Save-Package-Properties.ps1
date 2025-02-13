f7834d305693d758db0a5bc6846349c7546fb538
Praveen Kuttappan <prmarott@microsoft.com>
2025-02-13 18:39:21 -0500

    Remove debug lines

--- eng/common/scripts/Save-Package-Properties.ps1
+++ eng/common/scripts/Save-Package-Properties.ps1
@@ -44,11 +44,6 @@ Param (
 
 . (Join-Path $PSScriptRoot common.ps1)
 
-Write-Host "Service directory: $serviceDirectory"
-Write-Host "Output directory: $outDirectory"
-Write-Host "PR diff: $prDiff"
-Write-Host "Add dev version: $addDevVersion"
-
 function SetOutput($outputPath, $incomingPackageSpec)
 {
 
@@ -68,7 +63,6 @@ function SetOutput($outputPath, $incomingPackageSpec)
   {
     # Use the "Version" property which was provided by the incoming package spec
     # as the DevVersion. This may be overridden later.
-    Write-Host "Adding DevVersion property to package info json. Dev version: $($incomingPackageSpec.Version)"
     $outputObject.DevVersion = $incomingPackageSpec.Version
   }
 
