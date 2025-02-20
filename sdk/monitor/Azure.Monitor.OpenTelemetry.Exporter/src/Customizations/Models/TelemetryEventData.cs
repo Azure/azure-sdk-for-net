81ab0a773738d57e52931543dafef0e9976197d5
Timothy Mothra Lee (from Dev Box) <tilee@microsoft.com>
2025-02-20 15:24:04 -0800

    initial POC

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/Customizations/Models/TelemetryEventData.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using OpenTelemetry.Logs;
+
+namespace Azure.Monitor.OpenTelemetry.Exporter.Models
+{
+    internal partial class TelemetryEventData
+    {
+        public TelemetryEventData(int version, string name, ChangeTrackingDictionary<string, string> properties, string? message, LogRecord logRecord) : base(version)
+        {
+            Name = name;
+            Properties = properties;
+            Measurements = new ChangeTrackingDictionary<string, double>();
+        }
+    }
+}
