6e81a0704f97cbd7f2a9626e99e73d209b3ce504
kevinzz6 <632604666@qq.com>
2025-02-20 15:28:06 +0800

    generate with latest code

--- sdk/synapse/Azure.Analytics.Synapse.Artifacts/src/Generated/Models/OracleAuthenticationType.cs
+++ sdk/synapse/Azure.Analytics.Synapse.Artifacts/src/Generated/Models/OracleAuthenticationType.cs
@@ -30,7 +30,7 @@ namespace Azure.Analytics.Synapse.Artifacts.Models
         public static bool operator ==(OracleAuthenticationType left, OracleAuthenticationType right) => left.Equals(right);
         /// <summary> Determines if two <see cref="OracleAuthenticationType"/> values are not the same. </summary>
         public static bool operator !=(OracleAuthenticationType left, OracleAuthenticationType right) => !left.Equals(right);
-        /// <summary> Converts a string to a <see cref="OracleAuthenticationType"/>. </summary>
+        /// <summary> Converts a <see cref="string"/> to a <see cref="OracleAuthenticationType"/>. </summary>
         public static implicit operator OracleAuthenticationType(string value) => new OracleAuthenticationType(value);
 
         /// <inheritdoc />
@@ -41,7 +41,7 @@ namespace Azure.Analytics.Synapse.Artifacts.Models
 
         /// <inheritdoc />
         [EditorBrowsable(EditorBrowsableState.Never)]
-        public override int GetHashCode() => _value?.GetHashCode() ?? 0;
+        public override int GetHashCode() => _value != null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(_value) : 0;
         /// <inheritdoc />
         public override string ToString() => _value;
     }
