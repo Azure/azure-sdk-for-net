fee3a18ee6ae3067c54ef7c37bc7ba9a24dfe0e2
Rajkumar Rangaraj <rajrang@microsoft.com>
2025-02-21 17:22:50 -0800

    UseAzureMonitorExporter POC

---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/ExporterRegistrationHostedService.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Trace;
+using OpenTelemetry;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Azure.Monitor.OpenTelemetry.Exporter.Internals;
+using Microsoft.Extensions.Options;
+
+namespace Azure.Monitor.OpenTelemetry.Exporter
+{
+    internal sealed class ExporterRegistrationHostedService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ExporterRegistrationHostedService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Initialize(_serviceProvider);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private static void Initialize(IServiceProvider serviceProvider)
+        {
+            Debug.Assert(serviceProvider != null, "serviceProvider was null");
+
+            // Note: For MeterProvider just do normal registration style and call
+            // meterProviderBuilder.AddReader because the order doesn't matter for
+            // metrics
+
+            var tracerProvider = serviceProvider!.GetService<TracerProvider>();
+            if (tracerProvider != null)
+            {
+                // Add a processor manually to the TracerProvider created by the SDK
+                var exporterOptions = serviceProvider!.GetRequiredService<IOptionsMonitor<AzureMonitorExporterOptions>>().Get(Options.DefaultName);
+                tracerProvider.AddProcessor(new CompositeProcessor<Activity>(new BaseProcessor<Activity>[]
+                {
+                    new StandardMetricsExtractionProcessor(new AzureMonitorMetricExporter(exporterOptions)),
+                    new BatchActivityExportProcessor(new AzureMonitorTraceExporter(exporterOptions))
+                }));
+            }
+
+            var loggerProvider = serviceProvider!.GetService<LoggerProvider>();
+            if (loggerProvider != null)
+            {
+                // Add a processor manually to the LoggerProvider created by the SDK
+                var exporterOptions = serviceProvider!.GetRequiredService<IOptionsMonitor<AzureMonitorExporterOptions>>().Get(Options.DefaultName);
+                loggerProvider.AddProcessor(new BatchLogRecordExportProcessor(new AzureMonitorLogExporter(exporterOptions)));
+            }
+        }
+    }
+}
---
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/OpenTelemetryBuilderExtensions.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using System;
+
+namespace Azure.Monitor.OpenTelemetry.Exporter
+{
+    /// <summary>
+    /// Extension methods for setting up Azure Monitor in an <see cref="OpenTelemetryBuilder" />.
+    /// </summary>
+    public static class OpenTelemetryBuilderExtensions
+    {
+        /// <summary>
+        /// Configures Azure Monitor Exporter for all signals.
+        /// </summary>
+        /// <param name="builder"><see cref="OpenTelemetryBuilder"/>.</param>
+        /// <returns>The supplied <see cref="OpenTelemetryBuilder"/> for chaining calls.</returns>
+        /// <remarks>
+        /// <para>
+        /// This method configures Azure Monitor for use with OpenTelemetry by adding the Azure Monitor exporter for logging,
+        /// distributed tracing, and metrics. It also configures the OpenTelemetry logger to include formatted messages and
+        /// parsed state values.
+        /// </para>
+        /// </remarks>
+        public static OpenTelemetryBuilder UseAzureMonitorExporter(this OpenTelemetryBuilder builder)
+        {
+            builder.Services.TryAddSingleton<IConfigureOptions<AzureMonitorExporterOptions>,
+                        DefaultAzureMonitorExporterOptions>();
+            return builder.UseAzureMonitorExporter(o => { });
+        }
+
+        /// <summary>
+        /// Configures Azure Monitor Exporter for logging, distributed tracing, and metrics.
+        /// </summary>
+        /// <param name="builder"><see cref="OpenTelemetryBuilder"/>.</param>
+        /// <param name="configureAzureMonitor">Callback action for configuring <see cref="AzureMonitorExporterOptions"/>.</param>
+        /// <returns>The supplied <see cref="OpenTelemetryBuilder"/> for chaining calls.</returns>
+        /// <remarks>
+        /// <para>
+        /// This method configures Azure Monitor for use with OpenTelemetry by adding the Azure Monitor exporter for logging,
+        /// distributed tracing, and metrics. It also configures the OpenTelemetry logger to include formatted messages and
+        /// parsed state values.
+        /// </para>
+        /// </remarks>
+        public static OpenTelemetryBuilder UseAzureMonitorExporter(this OpenTelemetryBuilder builder, Action<AzureMonitorExporterOptions> configureAzureMonitor)
+        {
+            if (builder.Services == null)
+            {
+                throw new ArgumentNullException(nameof(builder.Services));
+            }
+
+            if (configureAzureMonitor != null)
+            {
+                builder.Services.Configure(configureAzureMonitor);
+            }
+
+            builder
+                .WithLogging()
+                .WithMetrics(metrics => metrics.AddAzureMonitorMetricExporter())
+                .WithTracing();
+
+            builder.Services.Configure<OpenTelemetryLoggerOptions>((loggingOptions) =>
+            {
+                loggingOptions.IncludeFormattedMessage = true;
+            });
+
+            builder.Services.AddHostedService(sp =>
+            {
+                var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+                var loggerFilterOptions = sp.GetRequiredService<IOptionsMonitor<LoggerFilterOptions>>().CurrentValue;
+                return new ExporterRegistrationHostedService(sp);
+            });
+
+            return builder;
+        }
+    }
+}
