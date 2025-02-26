9161f2fe42b210052774a80a130f59b8619ecba0
Amanda Nguyen <48961492+amnguye@users.noreply.github.com>
2025-02-26 12:26:13 -0800

    [Storage] [DataMovement] Added back default constructors for ResourceProviders for Anonymous Access (#48438)
    * Added back default constructors for StorageResourceProviders to support Anonymous acces
    
    * Removed comment about static call for FromClient
    
    * Rerecord test to avoid 500 recordings in net-pullrequest
    
    * Trying to rerecord different tests
    
    * bumped version to 12.1.0

--- sdk/storage/Azure.Storage.DataMovement.Files.Shares/tests/ShareFilesStorageResourceProviderTests.cs
+++ sdk/storage/Azure.Storage.DataMovement.Files.Shares/tests/ShareFilesStorageResourceProviderTests.cs
@@ -1,23 +1,22 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-extern alias DMBlobs;
-extern alias BaseBlobs;
+extern alias DMShare;
+extern alias BaseShares;
 
 using System;
 using Azure.Core;
-using BaseBlobs::Azure.Storage.Blobs;
-using BaseBlobs::Azure.Storage.Blobs.Specialized;
+using BaseShares::Azure.Storage.Files.Shares;
 using Azure.Storage.Tests;
-using DMBlobs::Azure.Storage.DataMovement.Blobs;
+using DMShare::Azure.Storage.DataMovement.Files.Shares;
 using Moq;
 using NUnit.Framework;
 using System.Threading.Tasks;
 using System.Threading;
 
-namespace Azure.Storage.DataMovement.Blobs.Tests
+namespace Azure.Storage.DataMovement.Files.Shares.Tests
 {
-    public class BlobsStorageResourceProviderTests
+    public class ShareFilesStorageResourceProviderTests
     {
         public enum CredType
         {
@@ -26,15 +25,8 @@ namespace Azure.Storage.DataMovement.Blobs.Tests
             Token,
             Sas
         }
-        public enum BlobType
-        {
-            Unspecified,
-            Block,
-            Page,
-            Append
-        }
 
-        private void AssertCredPresent(BlobClientConfiguration clientConfig, CredType credType)
+        private void AssertCredPresent(ShareClientConfiguration clientConfig, CredType credType)
         {
             Assert.IsNotNull(clientConfig);
             switch (credType)
@@ -64,38 +56,6 @@ namespace Azure.Storage.DataMovement.Blobs.Tests
             }
         }
 
-        private void AssertBlobStorageResourceType(
-            StorageResource resource,
-            BlobType blobType,
-            out BlobBaseClient underlyingClient)
-        {
-            Assert.IsNotNull(resource);
-            switch (blobType)
-            {
-                case BlobType.Unspecified:
-                case BlobType.Block:
-                    Assert.That(resource, Is.InstanceOf<BlockBlobStorageResource>());
-                    BlockBlobStorageResource blockResource = resource as BlockBlobStorageResource;
-                    Assert.IsNotNull(blockResource.BlobClient);
-                    underlyingClient = blockResource.BlobClient;
-                    break;
-                case BlobType.Page:
-                    Assert.That(resource, Is.InstanceOf<PageBlobStorageResource>());
-                    PageBlobStorageResource pageResource = resource as PageBlobStorageResource;
-                    Assert.IsNotNull(pageResource.BlobClient);
-                    underlyingClient = pageResource.BlobClient;
-                    break;
-                case BlobType.Append:
-                    Assert.That(resource, Is.InstanceOf<AppendBlobStorageResource>());
-                    AppendBlobStorageResource appendResource = resource as AppendBlobStorageResource;
-                    Assert.IsNotNull(appendResource.BlobClient);
-                    underlyingClient = appendResource.BlobClient;
-                    break;
-                default:
-                    throw new ArgumentException("No assertion support for blob type " + blobType.ToString());
-            }
-        }
-
         private (Mock<StorageSharedKeyCredential> SharedKey, Mock<TokenCredential> Token, Mock<AzureSasCredential> Sas) GetMockCreds()
         {
             return (
@@ -106,7 +66,7 @@ namespace Azure.Storage.DataMovement.Blobs.Tests
 
         [Test]
         [Combinatorial]
-        public async Task FromContainer(
+        public async Task FromDirectoryAsync(
             [Values(true, false)] bool withPrefix,
             [Values(CredType.SharedKey, CredType.Token, CredType.Sas, CredType.None)] CredType credType)
         {
@@ -115,7 +75,7 @@ namespace Azure.Storage.DataMovement.Blobs.Tests
             Uri uri = new Uri($"https://myaccount.blob.core.windows.net/{containerName}" + (withPrefix ? $"/{prefix}" : ""));
             (Mock<StorageSharedKeyCredential> SharedKey, Mock<TokenCredential> Token, Mock<AzureSasCredential> Sas) mockCreds = GetMockCreds();
 
-            BlobsStorageResourceProvider provider = credType switch
+            ShareFilesStorageResourceProvider provider = credType switch
             {
                 CredType.SharedKey => new(mockCreds.SharedKey.Object),
                 CredType.Token => new(mockCreds.Token.Object),
@@ -123,26 +83,27 @@ namespace Azure.Storage.DataMovement.Blobs.Tests
                 CredType.None => new(),
                 _ => throw new ArgumentException("Bad cred type"),
             };
-            BlobStorageResourceContainer resource = await provider.FromContainerAsync(uri) as BlobStorageResourceContainer;
+            ShareDirectoryStorageResourceContainer resource = await provider.FromDirectoryAsync(uri) as ShareDirectoryStorageResourceContainer;
 
             Assert.IsNotNull(resource);
+            Assert.IsNotNull(resource.ShareDirectoryClient);
             Assert.AreEqual(uri, resource.Uri);
-            Assert.AreEqual(uri, resource.BlobContainerClient.Uri);
-            AssertCredPresent(resource.BlobContainerClient.ClientConfiguration, credType);
+            Assert.AreEqual(uri, resource.ShareDirectoryClient.Uri);
+            AssertCredPresent(resource.ShareDirectoryClient.ClientConfiguration, credType);
         }
 
         [Test]
         [Combinatorial]
-        public async Task FromBlob(
-            [Values(BlobType.Unspecified, BlobType.Block, BlobType.Page, BlobType.Append)] BlobType blobType,
+        public async Task FromFileAsync(
             [Values(CredType.SharedKey, CredType.Token, CredType.Sas, CredType.None)] CredType credType)
         {
-            const string containerName = "mycontainer";
-            const string blobName = "my/blob.txt";
-            Uri uri = new Uri($"https://myaccount.blob.core.windows.net/{containerName}/{blobName}");
+            const string shareName = "mycontainer";
+            const string directoryName = "directoryName";
+            const string fileName = "file.txt";
+            Uri uri = new Uri($"https://myaccount.blob.core.windows.net/{shareName}/{directoryName}/{fileName}");
             (Mock<StorageSharedKeyCredential> SharedKey, Mock<TokenCredential> Token, Mock<AzureSasCredential> Sas) mockCreds = GetMockCreds();
 
-            BlobsStorageResourceProvider provider = credType switch
+            ShareFilesStorageResourceProvider provider = credType switch
             {
                 CredType.SharedKey => new(mockCreds.SharedKey.Object),
                 CredType.Token => new(mockCreds.Token.Object),
@@ -150,30 +111,24 @@ namespace Azure.Storage.DataMovement.Blobs.Tests
                 CredType.None => new(),
                 _ => throw new ArgumentException("Bad cred type"),
             };
-
-            StorageResource resource = blobType switch
-            {
-                BlobType.Unspecified => await provider.FromBlobAsync(uri),
-                BlobType.Block => await provider.FromBlobAsync(uri, new BlockBlobStorageResourceOptions()),
-                BlobType.Page => await provider.FromBlobAsync(uri, new PageBlobStorageResourceOptions()),
-                BlobType.Append => await provider.FromBlobAsync(uri, new AppendBlobStorageResourceOptions()),
-                _ => throw new ArgumentException("Bad blob type")
-            };
+            StorageResource resource = await provider.FromFileAsync(uri);
 
             Assert.IsNotNull(resource);
-            AssertBlobStorageResourceType(resource, blobType, out BlobBaseClient underlyingClient);
+            ShareFileStorageResource fileResource = resource as ShareFileStorageResource;
+            Assert.IsNotNull(fileResource.ShareFileClient);
             Assert.AreEqual(uri, resource.Uri);
-            Assert.AreEqual(uri, underlyingClient.Uri);
-            AssertCredPresent(underlyingClient.ClientConfiguration, credType);
+            Assert.AreEqual(uri, fileResource.ShareFileClient.Uri);
+            AssertCredPresent(fileResource.ShareFileClient.ClientConfiguration, credType);
         }
 
         [Test]
         public async Task CredentialCallback(
             [Values(CredType.SharedKey, CredType.Sas)] CredType credType)
         {
-            const string containerName = "mycontainer";
-            const string blobName = "my/blob.txt";
-            Uri uri = new Uri($"https://myaccount.blob.core.windows.net/{containerName}/{blobName}");
+            const string shareName = "mycontainer";
+            const string directoryName = "directoryName";
+            const string fileName = "file.txt";
+            Uri uri = new Uri($"https://myaccount.blob.core.windows.net/{shareName}/{directoryName}/{fileName}");
             (Mock<StorageSharedKeyCredential> SharedKey, Mock<TokenCredential> Token, Mock<AzureSasCredential> Sas) mockCreds = GetMockCreds();
 
             ValueTask<StorageSharedKeyCredential> GetSharedKeyCredential(Uri uri, CancellationToken cancellationToken)
@@ -185,19 +140,21 @@ namespace Azure.Storage.DataMovement.Blobs.Tests
                 return new ValueTask<AzureSasCredential>(mockCreds.Sas.Object);
             }
 
-            BlobsStorageResourceProvider provider = credType switch
+            ShareFilesStorageResourceProvider provider = credType switch
             {
                 CredType.SharedKey => new(GetSharedKeyCredential),
                 CredType.Sas => new(GetSasCredential),
                 _ => throw new ArgumentException("Bad cred type"),
             };
-            StorageResource resource = await provider.FromBlobAsync(uri);
+            StorageResource resource = await provider.FromFileAsync(uri);
 
             Assert.IsNotNull(resource);
-            AssertBlobStorageResourceType(resource, BlobType.Block, out BlobBaseClient underlyingClient);
             Assert.AreEqual(uri, resource.Uri);
-            Assert.AreEqual(uri, underlyingClient.Uri);
-            AssertCredPresent(underlyingClient.ClientConfiguration, credType);
+            ShareFileStorageResource fileResource = resource as ShareFileStorageResource;
+            Assert.IsNotNull(fileResource.ShareFileClient);
+            Assert.AreEqual(uri, resource.Uri);
+            Assert.AreEqual(uri, fileResource.ShareFileClient.Uri);
+            AssertCredPresent(fileResource.ShareFileClient.ClientConfiguration, credType);
         }
     }
 }
