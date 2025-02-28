da8d60819cda4bc2dcef620d48b5b613559c2505
Krzysztof Cwalina <kcwalina@microsoft.com>
2025-02-28 11:45:44 -0800

    major CM change

---
+++ sdk/cloudmachine/Azure.Projects.Provisioning/src/ProjectInfrastructure/FeatureRole.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Projects.Core;
+
+public struct FeatureRole
+{
+    public FeatureRole(string name, string id)
+    {
+        Name = name;
+        Id = id;
+    }
+
+    public string Name { get; }
+    public string Id { get; }
+}
