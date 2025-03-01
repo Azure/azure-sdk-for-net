78b5cb5c33cf45a8aedee65c8b1ee7cc3d648cf3
Wei Hu <huwe@microsoft.com>
2025-03-01 22:12:40 +0800

    update

--- eng/packages/http-client-csharp/generator/Azure.Generator/src/RestClientVisitor.cs
+++ eng/packages/http-client-csharp/generator/Azure.Generator/src/RestClientVisitor.cs
@@ -15,24 +15,23 @@ namespace Azure.Generator
         /// <inheritdoc/>
         protected override TypeProvider? VisitType(TypeProvider type)
         {
-            if (type is not null && type is ClientProvider)
+            if (type is not null && type is ClientProvider client)
             {
                 // omit methods for ClientProvider, MPG will implement its own client methods
-                type.Update(methods: [], modifiers: TransfromPublicModifiersToInternal(type), relativeFilePath: TransformRelativeFilePathForClient(type));
+                // put create request methods to client directly and remove RestClientProvider
+                type.Update(methods: [.. client.RestClient.Methods], modifiers: TransfromPublicModifiersToInternal(type), relativeFilePath: TransformRelativeFilePathForClient(type));
             }
 
             if (type is RestClientProvider)
             {
-                type.Update(modifiers: TransfromPublicModifiersToInternal(type), relativeFilePath: TransformRelativeFilePathForRestClient(type));
+                return null;
             }
+
             return type;
         }
 
         private static string TransformRelativeFilePathForClient(TypeProvider type)
             => Path.Combine("src", "Generated", "RestOperations", $"{type.Name}RestOperations.cs");
-
-        private static string TransformRelativeFilePathForRestClient(TypeProvider type)
-            => Path.Combine("src", "Generated", "RestOperations", $"{type.Name}.RestClient.cs");
 
         private static TypeSignatureModifiers TransfromPublicModifiersToInternal(TypeProvider type)
         {
