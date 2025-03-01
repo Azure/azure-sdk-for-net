535d75f427fc811d21f4acaebee97fe7074b224f
REDMOND\wiboris <wiboris@microsoft.com>
2025-02-28 17:10:31 -0800

    inital checkin

---
+++ sdk/batch/Azure.Compute.Batch/src/Custom/Models/BatchError.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Compute.Batch
+{
+    public partial class BatchError
+    {
+        /// <summary>
+        /// Extracts a <see cref="BatchError"/> from a <see cref="Azure.RequestFailedException"/>.
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public static BatchError ExtractBatchErrorFromException(RequestFailedException e)
+        {
+            return BatchError.FromResponse(e.GetRawResponse());
+        }
+    }
+}
