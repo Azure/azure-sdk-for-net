f1c1e786674e0be219bae29cce9cc719e3bc3b5c
ShivangiReja <45216704+ShivangiReja@users.noreply.github.com>
2025-03-03 12:07:53 -0800

    Cleanup

--- sdk/ai/Azure.AI.Projects/tests/Samples/Extensions/Sample_AIInference.cs
+++ sdk/ai/Azure.AI.Projects/tests/Samples/Extensions/Sample_AIInference.cs
@@ -4,6 +4,7 @@
 #nullable disable
 
 using System;
+using System.Collections.Generic;
 using Azure.AI.Inference;
 using Azure.Core.TestFramework;
 using NUnit.Framework;
@@ -12,25 +13,47 @@ namespace Azure.AI.Projects.Tests;
 
 public class Sample_AIInference : SamplesBase<AIProjectsTestEnvironment>
 {
-        [Test]
-        public void InferenceChatCompletion()
-        {
-            var connectionString = TestEnvironment.AzureAICONNECTIONSTRING;
-            var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
-            AIProjectClient client = new AIProjectClient(connectionString);
-            var chatClient = client.GetChatCompletionsClient();
+    [Test]
+    public void InferenceChatCompletion()
+    {
+        var connectionString = TestEnvironment.AzureAICONNECTIONSTRING;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+        AIProjectClient client = new AIProjectClient(connectionString);
+        var chatClient = client.GetChatCompletionsClient();
 
-            var requestOptions = new ChatCompletionsOptions()
-            {
-                Messages =
+        var requestOptions = new ChatCompletionsOptions()
+        {
+            Messages =
                 {
                     new ChatRequestSystemMessage("You are a helpful assistant."),
                     new ChatRequestUserMessage("How many feet are in a mile?"),
                 },
-                Model = modelDeploymentName
-            };
+            Model = modelDeploymentName
+        };
 
-            Response<ChatCompletions> response = chatClient.Complete(requestOptions);
-            Console.WriteLine(response.Value.Content);
+        Response<ChatCompletions> response = chatClient.Complete(requestOptions);
+        Console.WriteLine(response.Value.Content);
+    }
+
+    [Test]
+    public void InferenceEmbedding()
+    {
+        var connectionString = TestEnvironment.AzureAICONNECTIONSTRING;
+        var modelDeploymentName = TestEnvironment.EMBEDDINGMODELDEPLOYMENTNAME;
+
+        EmbeddingsClient embeddingsClient = new AIProjectClient(connectionString, new DefaultAzureCredential()).GetEmbeddingsClient();
+
+        var input = new List<string> { "first phrase", "second phrase", "third phrase" };
+        var requestOptions = new EmbeddingsOptions(input)
+        {
+            Model = modelDeploymentName
+        };
+
+        Response<EmbeddingsResult> response = embeddingsClient.Embed(requestOptions);
+        foreach (EmbeddingItem item in response.Value.Data)
+        {
+            List<float> embedding = item.Embedding.ToObjectFromJson<List<float>>();
+            Console.WriteLine($"Index: {item.Index}, Embedding: <{string.Join(", ", embedding)}>");
         }
+    }
 }
