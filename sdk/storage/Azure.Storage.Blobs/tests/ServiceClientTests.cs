d066407be444c56fc02e8009824642a759a3e09a
nickliu-msft <nicliu@microsoft.com>
2025-03-04 15:53:21 -0500

    removed unused import

--- sdk/storage/Azure.Storage.Blobs/tests/ServiceClientTests.cs
+++ sdk/storage/Azure.Storage.Blobs/tests/ServiceClientTests.cs
@@ -3,10 +3,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core.TestFramework;
 using Azure.Identity;
