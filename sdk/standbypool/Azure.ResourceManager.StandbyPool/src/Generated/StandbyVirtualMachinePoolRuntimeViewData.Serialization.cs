9ccc7ab8aaffb47f76f7354c8e5774646d6c30dc
azure-sdk <azuresdk@microsoft.com>
2025-03-06 06:44:53 +0000

    Update SDK codes sq_wo_6

--- sdk/standbypool/Azure.ResourceManager.StandbyPool/src/Generated/StandbyVirtualMachinePoolRuntimeViewData.Serialization.cs
+++ sdk/standbypool/Azure.ResourceManager.StandbyPool/src/Generated/StandbyVirtualMachinePoolRuntimeViewData.Serialization.cs
@@ -142,7 +142,7 @@ namespace Azure.ResourceManager.StandbyPool
             {
                 case "J":
                     {
-                        using JsonDocument document = JsonDocument.Parse(data);
+                        using JsonDocument document = JsonDocument.Parse(data, ModelSerializationExtensions.JsonDocumentOptions);
                         return DeserializeStandbyVirtualMachinePoolRuntimeViewData(document.RootElement, options);
                     }
                 default:
