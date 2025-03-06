66ede61177ddae5fb426e41544cb66c4e8c8f2a8
azure-sdk <azuresdk@microsoft.com>
2025-03-06 06:45:12 +0000

    Update SDK codes pu_sp_5

--- sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/Entity.cs
+++ sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/Entity.cs
@@ -2582,7 +2582,7 @@ namespace Azure.Analytics.Purview.DataMap
             RequestContext context = FromCancellationToken(cancellationToken);
             Response response = await BatchSetClassificationsAsync(content, context).ConfigureAwait(false);
             IReadOnlyList<string> value = default;
-            using var document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+            using var document = await JsonDocument.ParseAsync(response.ContentStream, ModelSerializationExtensions.JsonDocumentOptions, cancellationToken).ConfigureAwait(false);
             List<string> array = new List<string>();
             foreach (var item in document.RootElement.EnumerateArray())
             {
@@ -2605,7 +2605,7 @@ namespace Azure.Analytics.Purview.DataMap
             RequestContext context = FromCancellationToken(cancellationToken);
             Response response = BatchSetClassifications(content, context);
             IReadOnlyList<string> value = default;
-            using var document = JsonDocument.Parse(response.ContentStream);
+            using var document = JsonDocument.Parse(response.ContentStream, ModelSerializationExtensions.JsonDocumentOptions);
             List<string> array = new List<string>();
             foreach (var item in document.RootElement.EnumerateArray())
             {
--- sdk/servicenetworking/Azure.ResourceManager.ServiceNetworking/src/Generated/LongRunningOperation/TrafficControllerFrontendOperationSource.cs
+++ sdk/servicenetworking/Azure.ResourceManager.ServiceNetworking/src/Generated/LongRunningOperation/TrafficControllerFrontendOperationSource.cs
@@ -5,7 +5,7 @@
 
 #nullable disable
 
-using System.Text.Json;
+using System.ClientModel.Primitives;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
@@ -23,16 +23,14 @@ namespace Azure.ResourceManager.ServiceNetworking
 
         TrafficControllerFrontendResource IOperationSource<TrafficControllerFrontendResource>.CreateResult(Response response, CancellationToken cancellationToken)
         {
-            using var document = JsonDocument.Parse(response.ContentStream);
-            var data = TrafficControllerFrontendData.DeserializeTrafficControllerFrontendData(document.RootElement);
+            var data = ModelReaderWriter.Read<TrafficControllerFrontendData>(response.Content);
             return new TrafficControllerFrontendResource(_client, data);
         }
 
         async ValueTask<TrafficControllerFrontendResource> IOperationSource<TrafficControllerFrontendResource>.CreateResultAsync(Response response, CancellationToken cancellationToken)
         {
-            using var document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
-            var data = TrafficControllerFrontendData.DeserializeTrafficControllerFrontendData(document.RootElement);
-            return new TrafficControllerFrontendResource(_client, data);
+            var data = ModelReaderWriter.Read<TrafficControllerFrontendData>(response.Content);
+            return await Task.FromResult(new TrafficControllerFrontendResource(_client, data)).ConfigureAwait(false);
         }
     }
 }
