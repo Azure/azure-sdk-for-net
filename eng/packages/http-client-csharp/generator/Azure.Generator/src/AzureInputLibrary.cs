41721a8a48e83db63b491da0509ebb5bc4e1f668
Wei Hu <huwe@microsoft.com>
2025-03-10 17:37:09 +0800

    cleanup

--- eng/packages/http-client-csharp/generator/Azure.Generator/src/AzureInputLibrary.cs
+++ eng/packages/http-client-csharp/generator/Azure.Generator/src/AzureInputLibrary.cs
@@ -13,16 +13,11 @@ namespace Azure.Generator
         private IReadOnlyDictionary<string, InputModelType>? _inputModelsByCrossLanguageDefinitionId = null;
         private IReadOnlyDictionary<string, InputModelType> InputModelsByCrossLanguageDefinitionId => _inputModelsByCrossLanguageDefinitionId ??= InputNamespace.Models.DistinctBy(x => x.CrossLanguageDefinitionId).ToDictionary(m => m.CrossLanguageDefinitionId, m => m);
 
-        private IReadOnlyDictionary<string, InputModelType>? _inputModelsByName = null;
-        private IReadOnlyDictionary<string, InputModelType> InputModelsByName => _inputModelsByName ??= InputNamespace.Models.ToDictionary(m => m.Name, m => m);
-
         /// <inheritdoc/>
         public AzureInputLibrary(string configPath) : base(configPath)
         {
         }
 
         internal InputModelType? GetModelByCrossLanguageDefinitionId(string crossLanguageDefinitionId) => InputModelsByCrossLanguageDefinitionId.TryGetValue(crossLanguageDefinitionId, out var model) ? model : null;
-
-        internal InputModelType? GetModelByName(string name) => InputModelsByName.TryGetValue(name, out var model) ? model : null;
     }
 }
