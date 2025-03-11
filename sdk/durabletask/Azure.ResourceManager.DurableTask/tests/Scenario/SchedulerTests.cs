7e0404d76689a20fe05e290224da219aa9cf97d0
Bernd Verst <github@bernd.dev>
2025-03-10 22:22:44 -0700

    Update tests

--- sdk/durabletask/Azure.ResourceManager.DurableTask/tests/Scenario/SchedulerTests.cs
+++ sdk/durabletask/Azure.ResourceManager.DurableTask/tests/Scenario/SchedulerTests.cs
@@ -1,21 +1,37 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Core.TestFramework;
 using Azure.ResourceManager.DurableTask.Models;
+using Azure.ResourceManager.Models;
 using Azure.ResourceManager.Resources;
+using Microsoft.AspNetCore.Http;
 using NUnit.Framework;
 
 namespace Azure.ResourceManager.DurableTask.Tests.Scenario;
 
 public class SchedulerTests : DurableTaskSchedulerManagementTestBase
 {
+    // IP address allowlist test constants
+    private const string IpRange1 = "12.14.16.18/20";
+    private const string IpRange2 = "11.13.15.17/20";
+    private const string IpRange3 = "123.124.125.126";
+    private const string UpdatedIpRange1 = "12.14.16.18/22";
+    private const string UpdatedIpRange2 = "11.13.15.17/22";
+    private const string PatchIpRange = "21.22.23.24/25";
+
+    // Tags key value test constants
+    private const string TagKeyOrg = "org";
+    private const string TagValueOrg = "Contoso";
+    private const string TagKeyEnv = "env";
+    private const string TagValueEnv = "test";
+
     public SchedulerTests(bool isAsync) : base(isAsync)
     {
     }
@@ -24,125 +40,139 @@ public class SchedulerTests : DurableTaskSchedulerManagementTestBase
     [RecordedTest]
     public async Task CreateUpdateGetPatchDeleteTest()
     {
+        // At the moment we only support the Dedicated SKU
+        const string skuType = "Dedicated";
+
         SubscriptionResource subscription = await Client.GetDefaultSubscriptionAsync();
         ResourceGroupResource rg = await CreateResourceGroup(subscription, "testRg", AzureLocation.NorthCentralUS);
-        await TestContext.Error.WriteLineAsync(
-            $"subscription: {subscription.Data.SubscriptionId}, resource group {rg.Data.Name}");
+        await TestContext.Error.WriteLineAsync($"subscription: {subscription.Data.SubscriptionId}, resource group {rg.Data.Name}");
         string resourceName = Recording.GenerateAssetName("resource");
 
         // Create Scheduler
-        SchedulerData createSchedulerData = new SchedulerData
-        {
-            Location = AzureLocation.NorthCentralUS,
+        SchedulerData createSchedulerData = new(AzureLocation.NorthCentralUS) {
             Properties = new SchedulerProperties(
-                ipAllowlist: new List<string> { "12.14.16.18/20", "11.13.15.17/20", "123.124.125.126" },
-                sku: new SchedulerSku { Name = "Dedicated", Capacity = 1 }),
+                ipAllowlist: [IpRange1, IpRange2, IpRange3],
+                sku: new SchedulerSku() { Name = skuType, Capacity = 1 }
+            )
         };
 
         ArmOperation<SchedulerResource> longRunningOperation =
             await rg.GetSchedulers().CreateOrUpdateAsync(WaitUntil.Started, resourceName, createSchedulerData);
 
-        // while provisioning is in progress the resource is in provisioning state
+        // While provisioning is in progress the resource is in provisioning state
         SchedulerResource resource = await rg.GetSchedulerAsync(resourceName);
         Assert.NotNull(resource);
         Assert.AreEqual(ProvisioningState.Provisioning, resource.Data.Properties.ProvisioningState);
 
-        // we keep track of the resource id to delete it later
+        // Keep track of the resource id to delete it later
         string resourceId = resource.Id;
 
         resource = await longRunningOperation.WaitForCompletionAsync();
         Assert.AreEqual(resourceName, resource.Data.Name);
-        Assert.AreEqual("Dedicated", resource.Data.Properties.Sku.Name);
-        Assert.AreEqual(RedundancyState.Zone, resource.Data.Properties.Sku.RedundancyState);
-        Assert.Contains("12.14.16.18/20", resource.Data.Properties.IPAllowlist as IList);
-        Assert.Contains("11.13.15.17/20", resource.Data.Properties.IPAllowlist as IList);
-        Assert.Contains("123.124.125.126", resource.Data.Properties.IPAllowlist as IList);
+        Assert.AreEqual(skuType, resource.Data.Properties.Sku.Name);
+        Assert.AreEqual(RedundancyState.None, resource.Data.Properties.Sku.RedundancyState);
+        Assert.Contains(IpRange1, resource.Data.Properties.IPAllowlist as IList);
+        Assert.Contains(IpRange2, resource.Data.Properties.IPAllowlist as IList);
+        Assert.Contains(IpRange3, resource.Data.Properties.IPAllowlist as IList);
         Assert.AreEqual(ProvisioningState.Succeeded, resource.Data.Properties.ProvisioningState);
 
         // Get Scheduler
         resource = await rg.GetSchedulerAsync(resourceName);
         Assert.NotNull(resource);
         Assert.AreEqual(resourceName, resource.Data.Name);
-        Assert.AreEqual("Dedicated", resource.Data.Properties.Sku.Name);
-        Assert.AreEqual(RedundancyState.Zone, resource.Data.Properties.Sku.RedundancyState);
-        Assert.Contains("12.14.16.18/20", resource.Data.Properties.IPAllowlist as IList);
-        Assert.Contains("11.13.15.17/20", resource.Data.Properties.IPAllowlist as IList);
-        Assert.Contains("123.124.125.126", resource.Data.Properties.IPAllowlist as IList);
+        Assert.AreEqual(skuType, resource.Data.Properties.Sku.Name);
+        Assert.AreEqual(RedundancyState.None, resource.Data.Properties.Sku.RedundancyState);
+        Assert.Contains(IpRange1, resource.Data.Properties.IPAllowlist as IList);
+        Assert.Contains(IpRange2, resource.Data.Properties.IPAllowlist as IList);
+        Assert.Contains(IpRange3, resource.Data.Properties.IPAllowlist as IList);
         Assert.AreEqual(ProvisioningState.Succeeded, resource.Data.Properties.ProvisioningState);
 
-        SchedulerData updateSchedulerData = new SchedulerData
+        SchedulerData updateSchedulerData = new(AzureLocation.NorthCentralUS)
         {
-            Location = AzureLocation.NorthCentralUS,
             Properties = new SchedulerProperties(
-                ipAllowlist: new List<string> { "12.14.16.18/22", "11.13.15.17/22", "123.124.125.126" },
-                sku: new SchedulerSku { Name = "Dedicated", Capacity = 1 })
+                ipAllowlist: [UpdatedIpRange1, UpdatedIpRange2, IpRange3],
+                sku: new SchedulerSku() { Name = skuType, Capacity = 1 }
+            ),
+            Tags = { { TagKeyOrg, TagValueOrg }, { TagKeyEnv, TagValueEnv } }
         };
 
-        longRunningOperation =
-            await rg.GetSchedulers().CreateOrUpdateAsync(WaitUntil.Started, resourceName, updateSchedulerData);
-        // while the update is in progress the resource is in updating state
+        longRunningOperation = await rg.GetSchedulers().CreateOrUpdateAsync(WaitUntil.Started, resourceName, updateSchedulerData);
+        // While the update is in progress the resource is in updating state
         resource = await rg.GetSchedulerAsync(resourceName);
         Assert.AreEqual(ProvisioningState.Updating, resource.Data.Properties.ProvisioningState);
 
-        // wait for the update to complete
+        // Wait for the update to complete
         resource = await longRunningOperation.WaitForCompletionAsync();
         Assert.AreEqual(resourceName, resource.Data.Name);
-        Assert.AreEqual("Dedicated", resource.Data.Properties.Sku.Name);
-        Assert.AreEqual(RedundancyState.Zone, resource.Data.Properties.Sku.RedundancyState);
-        Assert.Contains("12.14.16.18/22", resource.Data.Properties.IPAllowlist as IList);
-        Assert.Contains("11.13.15.17/22", resource.Data.Properties.IPAllowlist as IList);
-        Assert.Contains("123.124.125.126", resource.Data.Properties.IPAllowlist as IList);
+        Assert.AreEqual(skuType, resource.Data.Properties.Sku.Name);
+        Assert.AreEqual(RedundancyState.None, resource.Data.Properties.Sku.RedundancyState);
+        Assert.Contains(UpdatedIpRange1, resource.Data.Properties.IPAllowlist as IList);
+        Assert.Contains(UpdatedIpRange2, resource.Data.Properties.IPAllowlist as IList);
+        Assert.Contains(IpRange3, resource.Data.Properties.IPAllowlist as IList);
+        Assert.AreEqual(2, resource.Data.Tags.Count);
+        Assert.Contains(TagKeyOrg, resource.Data.Tags.Keys as IList);
+        Assert.Contains(TagKeyEnv, resource.Data.Tags.Keys as IList);
+        Assert.AreEqual(TagValueOrg, resource.Data.Tags[TagKeyOrg]);
+        Assert.AreEqual(TagValueEnv, resource.Data.Tags[TagKeyEnv]);
         Assert.AreEqual(ProvisioningState.Succeeded, resource.Data.Properties.ProvisioningState);
 
         // List all schedulers and verify the updated scheduler is present
         List<SchedulerResource> schedulers = await rg.GetSchedulers().GetAllAsync().ToEnumerableAsync();
-        // look for the scheduler with the ARM resource ID matching our resource
+        // Look for the scheduler with the ARM resource ID matching our resource
         resource = schedulers.FirstOrDefault(s => s.Data.Id == resourceId);
         Assert.NotNull(resource);
         Assert.AreEqual(resourceName, resource.Data.Name);
-        Assert.AreEqual("Dedicated", resource.Data.Properties.Sku.Name);
-        Assert.AreEqual(RedundancyState.Zone, resource.Data.Properties.Sku.RedundancyState);
-        Assert.Contains("12.14.16.18/22", resource.Data.Properties.IPAllowlist as IList);
-        Assert.Contains("11.13.15.17/22", resource.Data.Properties.IPAllowlist as IList);
-        Assert.Contains("123.124.125.126", resource.Data.Properties.IPAllowlist as IList);
+        Assert.AreEqual(skuType, resource.Data.Properties.Sku.Name);
+        Assert.AreEqual(RedundancyState.None, resource.Data.Properties.Sku.RedundancyState);
+        Assert.Contains(UpdatedIpRange1, resource.Data.Properties.IPAllowlist as IList);
+        Assert.Contains(UpdatedIpRange2, resource.Data.Properties.IPAllowlist as IList);
+        Assert.Contains(IpRange3, resource.Data.Properties.IPAllowlist as IList);
+        Assert.AreEqual(2, resource.Data.Tags.Count);
+        Assert.Contains(TagKeyOrg, resource.Data.Tags.Keys as IList);
+        Assert.Contains(TagKeyEnv, resource.Data.Tags.Keys as IList);
+        Assert.AreEqual(TagValueOrg, resource.Data.Tags[TagKeyOrg]);
+        Assert.AreEqual(TagValueEnv, resource.Data.Tags[TagKeyEnv]);
         Assert.AreEqual(ProvisioningState.Succeeded, resource.Data.Properties.ProvisioningState);
 
         // Update select Scheduler properties (Patch)
-
-        SchedulerData patchSchedulerData = new SchedulerData
+        SchedulerData patchSchedulerData = new()
         {
-            Properties = new SchedulerProperties(
-                ipAllowlist: new List<string> { "21.22.23.24/25" },
-                sku: null)
+            Properties = new SchedulerProperties (
+                ipAllowlist: [PatchIpRange],
+                sku: new SchedulerSku(skuType)), // capacity can optionally be changed
+            Tags = { { TagKeyEnv, TagValueEnv } }
         };
 
         longRunningOperation = await resource.UpdateAsync(waitUntil: WaitUntil.Started, patchSchedulerData);
-        // while the update is in progress the resource is in updating state
+        // While the update is in progress the resource is in updating state
         Assert.AreEqual(ProvisioningState.Updating, resource.Data.Properties.ProvisioningState);
 
-        // wait for the update to complete
+        // Wait for the update to complete
         resource = await longRunningOperation.WaitForCompletionAsync();
         Assert.AreEqual(resourceName, resource.Data.Name);
-        Assert.AreEqual("Dedicated", resource.Data.Properties.Sku.Name);
+        Assert.AreEqual(skuType, resource.Data.Properties.Sku.Name);
         Assert.AreEqual(RedundancyState.Zone, resource.Data.Properties.Sku.RedundancyState);
-        Assert.Equals(1, resource.Data.Properties.IPAllowlist.Count);
-        Assert.Contains("21.22.23.24/25", resource.Data.Properties.IPAllowlist as IList);
+        Assert.AreEqual(1, resource.Data.Properties.IPAllowlist.Count);
+        Assert.Contains(PatchIpRange, resource.Data.Properties.IPAllowlist as IList);
+        Assert.AreEqual(1, resource.Data.Tags.Count);
+        Assert.Contains(TagKeyEnv, resource.Data.Tags.Keys as IList);
+        Assert.AreEqual(TagValueEnv, resource.Data.Tags[TagKeyEnv]);
         Assert.AreEqual(ProvisioningState.Succeeded, resource.Data.Properties.ProvisioningState);
 
         // Delete Scheduler
         var longRunningDeleteOperation = await resource.DeleteAsync(WaitUntil.Started);
 
-        // while delete is in progress the resource is in deleting state
+        // While delete is in progress the resource is in deleting state
         SchedulerResource toBeDeleted = await resource.GetAsync();
         Assert.NotNull(toBeDeleted);
         Assert.Equals(ProvisioningState.Deleting, toBeDeleted.Data.Properties.ProvisioningState ?? "Unknown");
 
         var done = await longRunningDeleteOperation.WaitForCompletionResponseAsync();
-        Assert.True(done.Status is 200 or 204);
+        Assert.True(done.Status is StatusCodes.Status200OK or StatusCodes.Status204NoContent);
 
-        // verify the scheduler is deleted
+        // Verify the scheduler is deleted
         Response<SchedulerResource> notFoundResource = await rg.GetSchedulerAsync(resourceName);
         Assert.False(notFoundResource.HasValue);
-        Assert.Equals(404, notFoundResource.GetRawResponse().Status);
+        Assert.Equals(StatusCodes.Status404NotFound, notFoundResource.GetRawResponse().Status);
     }
 }
