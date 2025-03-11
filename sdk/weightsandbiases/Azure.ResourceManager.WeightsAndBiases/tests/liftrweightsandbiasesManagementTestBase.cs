80e7b683aa7a73c2586b995726b03abe5cb0221d
Swati Aggarwal <aggarwalsw@microsoft.com>
2025-03-11 00:49:00 +0000

    Weights & Biases .NET SDK Preview Release

---
+++ sdk/weightsandbiases/Azure.ResourceManager.WeightsAndBiases/tests/liftrweightsandbiasesManagementTestBase.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.TestFramework;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Azure.ResourceManager.WeightsAndBiases.Tests
+{
+    public class liftrweightsandbiasesManagementTestBase : ManagementRecordedTestBase<liftrweightsandbiasesManagementTestEnvironment>
+    {
+        protected ArmClient Client { get; private set; }
+        protected SubscriptionResource DefaultSubscription { get; private set; }
+
+        protected liftrweightsandbiasesManagementTestBase(bool isAsync, RecordedTestMode mode)
+        : base(isAsync, mode)
+        {
+        }
+
+        protected liftrweightsandbiasesManagementTestBase(bool isAsync)
+            : base(isAsync)
+        {
+        }
+
+        [SetUp]
+        public async Task CreateCommonClient()
+        {
+            Client = GetArmClient();
+            DefaultSubscription = await Client.GetDefaultSubscriptionAsync().ConfigureAwait(false);
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        {
+            string rgName = Recording.GenerateAssetName(rgNamePrefix);
+            ResourceGroupData input = new ResourceGroupData(location);
+            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            return lro.Value;
+        }
+    }
+}
