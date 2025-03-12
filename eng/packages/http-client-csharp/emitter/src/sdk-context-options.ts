c738cf4184627c661fbc723e31716eb609424b22
Wei Hu <huwe@microsoft.com>
2025-03-12 17:40:32 +0800

    fix format

--- eng/packages/http-client-csharp/emitter/src/sdk-context-options.ts
+++ eng/packages/http-client-csharp/emitter/src/sdk-context-options.ts
@@ -4,11 +4,11 @@
 import { CreateSdkContextOptions } from "@azure-tools/typespec-client-generator-core";
 
 export const azureSDKContextOptions: CreateSdkContextOptions = {
-    versioning: {},
-    additionalDecorators: [
-        // https://github.com/Azure/typespec-azure/blob/main/packages/typespec-client-generator-core/README.md#usesystemtextjsonconverter
-        "Azure\\.ClientGenerator\\.Core\\.@useSystemTextJsonConverter",
-        // https://github.com/Azure/typespec-azure/blob/main/packages/typespec-azure-resource-manager/README.md#armprovidernamespace
-        "Azure\\.ResourceManager\\.@armProviderNamespace"
-    ]
+  versioning: {},
+  additionalDecorators: [
+    // https://github.com/Azure/typespec-azure/blob/main/packages/typespec-client-generator-core/README.md#usesystemtextjsonconverter
+    "Azure\\.ClientGenerator\\.Core\\.@useSystemTextJsonConverter",
+    // https://github.com/Azure/typespec-azure/blob/main/packages/typespec-azure-resource-manager/README.md#armprovidernamespace
+    "Azure\\.ResourceManager\\.@armProviderNamespace"
+  ]
 };
