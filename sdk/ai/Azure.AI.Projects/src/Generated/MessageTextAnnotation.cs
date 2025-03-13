e03c7a0fc4e8addd057e225dc00992d8db897d30
Travis Angevine <trangevi@microsoft.com>
2025-03-13 09:42:45 -0700

    [Azure.AI.Projects] URL Citations (#48759)
    * Update with typespec changes for assistant -> agent id. Update sample for url citation
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * Test updates
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * update changelog
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * regen
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    * Changelog update
    
    Signed-off-by: trangevi <trangevi@microsoft.com>
    
    ---------
    
    Signed-off-by: trangevi <trangevi@microsoft.com>

--- sdk/ai/Azure.AI.Projects/src/Generated/MessageTextAnnotation.cs
+++ sdk/ai/Azure.AI.Projects/src/Generated/MessageTextAnnotation.cs
@@ -13,7 +13,7 @@ namespace Azure.AI.Projects
     /// <summary>
     /// An abstract representation of an annotation to text thread message content.
     /// Please note <see cref="MessageTextAnnotation"/> is the base class. According to the scenario, a derived class of the base class might need to be assigned here, or this property needs to be casted to one of the possible derived classes.
-    /// The available derived classes include <see cref="MessageTextFileCitationAnnotation"/> and <see cref="MessageTextFilePathAnnotation"/>.
+    /// The available derived classes include <see cref="MessageTextFileCitationAnnotation"/>, <see cref="MessageTextFilePathAnnotation"/> and <see cref="MessageTextUrlCitationAnnotation"/>.
     /// </summary>
     public abstract partial class MessageTextAnnotation
     {
