11896005ddbbd799c3d4189d376370114ae37292
Caio Saldanha <camaiaor@microsoft.com>
2025-03-13 06:23:09 -0700

    wip

--- sdk/search/Azure.Search.Documents/tests/SearchIndexClientTests.cs
+++ sdk/search/Azure.Search.Documents/tests/SearchIndexClientTests.cs
@@ -125,6 +125,23 @@ namespace Azure.Search.Documents.Tests
             Assert.NotZero(response.Value.Counters.IndexCounter.Usage);
         }
 
+        [Test]
+        [LiveOnly]
+        [ServiceVersion(Min = SearchClientOptions.ServiceVersion.V2025_03_01_Preview)]
+        public async Task GetIndexStatsSummary()
+        {
+            await using SearchResources resources = await SearchResources.GetSharedHotelsIndexAsync(this);
+
+            SearchIndexClient client = resources.GetIndexClient();
+            Response<ListIndexStatsSummary> response = await client.GetIndexStatsSummaryAsync();
+            Assert.AreEqual(200, response.GetRawResponse().Status);
+            Assert.IsNotNull(response.Value);
+            Assert.IsNotNull(response.Value.IndexesStatistics);
+            Assert.AreEqual(1, response.Value.IndexesStatistics.Count);
+
+            IndexStatisticsSummary summary = response.Value.IndexesStatistics[0];
+        }
+
         public void CreateIndexParameterValidation()
         {
             var endpoint = new Uri($"https://my-svc-name.search.windows.net");
