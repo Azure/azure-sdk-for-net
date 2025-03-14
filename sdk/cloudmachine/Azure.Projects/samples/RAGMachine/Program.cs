bfcbac9646df72f8b2dd938e22eaa78b10a2db87
Krzysztof Cwalina <kcwalina@microsoft.com>
2025-03-14 08:55:52 -0700

    cleaned up ragmachine app

--- sdk/cloudmachine/Azure.Projects/samples/RAGMachine/Program.cs
+++ sdk/cloudmachine/Azure.Projects/samples/RAGMachine/Program.cs
@@ -11,45 +11,38 @@ ProjectInfrastructure infrastructure = new();
 infrastructure.AddFeature(new OpenAIModelFeature("gpt-35-turbo", "0125"));
 infrastructure.AddFeature(new OpenAIModelFeature("text-embedding-ada-002", "2", AIModelKind.Embedding));
 infrastructure.AddFeature(new AppServiceFeature());
-
-// the app can be called with -init switch to generate bicep and prepare for azd deployment.
+// the app can be called with -bicep switch to generate bicep and prepare for azd deployment.
 if (infrastructure.TryExecuteCommand(args)) return;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
 
-// Add ProjectClient to the DI container
+// add oppinionated framework client to the DI container
 OfxProjectClient client = builder.AddOfxClient();
+EmbeddingsStore vectorDb = new(client.GetOpenAIEmbeddingClient());
+client.Storage.WhenUploaded(vectorDb.Add); // update vector db when a new file is uploaded
 
 var app = builder.Build();
 app.MapRazorPages();
 app.UseStaticFiles();
 
-EmbeddingsStore vectorDb = new(client.GetOpenAIEmbeddingClient());
-List<ChatMessage> prompt = [];
-
-// Register the vector db to be updated when a new file is uploaded
-client.Storage.WhenUploaded(vectorDb.Add);
-
-app.MapPost("/upload", async (HttpRequest request, OfxProjectClient client)
-    => await client.Storage.UploadFormAsync(request));
-
+List<ChatMessage> conversationThread = [];
 app.MapPost("/chat", async (HttpRequest request) =>
 {
     try
     {
         var message = await new StreamReader(request.Body).ReadToEndAsync();
         IEnumerable<VectorbaseEntry> related = vectorDb.Find(message);
-        prompt.Add(related);
-        prompt.Add(ChatMessage.CreateUserMessage(message));
+        conversationThread.Add(related);
+        conversationThread.Add(ChatMessage.CreateUserMessage(message));
 
         ChatClient chat = client.GetOpenAIChatClient();
-        ChatCompletion completion = await chat.CompleteChatAsync(prompt);
+        ChatCompletion completion = await chat.CompleteChatAsync(conversationThread);
         switch (completion.FinishReason)
         {
             case ChatFinishReason.Stop:
-                prompt.Add(completion);
+                conversationThread.Add(completion);
                 string response = completion.Content[0].Text;
                 return response;
             #region NotImplemented
@@ -62,10 +55,13 @@ app.MapPost("/chat", async (HttpRequest request) =>
                 #endregion
         }
     }
-    catch (Exception ex)
+    catch (Exception e)
     {
-        return ex.Message;
+        return e.Message;
     }
 });
+
+app.MapPost("/upload", async (HttpRequest request, OfxProjectClient client)
+    => await client.Storage.UploadFormAsync(request));
 
 app.Run();
