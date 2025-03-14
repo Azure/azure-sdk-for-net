444ee6530f00096c5a546d883631182ba22bbcc2
Arthur Ma <arthurma@microsoft.com>
2025-03-14 23:48:26 +0800

    update

--- common/ManagementTestShared/Redesign/InheritanceCheckTests.cs
+++ common/ManagementTestShared/Redesign/InheritanceCheckTests.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.ResourceManager.RecoveryServicesDataReplication.Tests.Helpers;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
