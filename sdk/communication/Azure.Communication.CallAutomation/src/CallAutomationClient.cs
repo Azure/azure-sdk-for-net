3478f2f930d25316f7014525992c5e2eda21e30b
v-saasomani <v-saasomani@microsoft.com>
2025-03-18 12:02:28 -0700

    Ported IncomingCall webhook to Beta3. Developed unit test for IncomingCall Event. Modified CustomCallingContextInternal with appropriate parameter ordering. (#48679)

--- sdk/communication/Azure.Communication.CallAutomation/src/CallAutomationClient.cs
+++ sdk/communication/Azure.Communication.CallAutomation/src/CallAutomationClient.cs
@@ -259,8 +259,8 @@ namespace Azure.Communication.CallAutomation
             request.AnsweredBy = Source == null ? null : new CommunicationUserIdentifierModel(Source.Id);
             request.OperationContext = options.OperationContext;
             request.CustomCallingContext = new CustomCallingContextInternal(
-                options.CustomCallingContext?.SipHeaders ?? new ChangeTrackingDictionary<string, string>(),
-                options.CustomCallingContext?.VoipHeaders ?? new ChangeTrackingDictionary<string, string>());
+                options.CustomCallingContext?.VoipHeaders ?? new ChangeTrackingDictionary<string, string>(),
+                options.CustomCallingContext?.SipHeaders ?? new ChangeTrackingDictionary<string, string>());
 
             return request;
         }
