69215a311ddf04ee09555c124b5bdb7a0f2a1f01
Durgesh Suryawanshi <v-durgeshs@microsoft.com>
2025-03-19 22:00:39 +0530

    Added SipC.ommunicationErrorResponse

---
+++ sdk/communication/Azure.Communication.PhoneNumbers/src/SipRouting/SipCommunicationErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.Communication.PhoneNumbers.SipRouting
+{
+    [CodeGenModel("CommunicationErrorResponse")]
+    internal partial class SipCommunicationErrorResponse
+    {
+        /// <summary> The Communication Services error. </summary>
+        public CommunicationError Error { get; }
+    }
+}
