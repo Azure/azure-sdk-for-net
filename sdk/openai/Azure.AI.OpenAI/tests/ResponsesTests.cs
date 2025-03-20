a85d8678a3114ba10990e1cf3d936df9d298b0af
Nisha Bhatia <Nisha.Bhatia@microsoft.com>
2025-03-20 16:21:56 -0700

    Update ResponsesTests.cs

--- sdk/openai/Azure.AI.OpenAI/tests/ResponsesTests.cs
+++ sdk/openai/Azure.AI.OpenAI/tests/ResponsesTests.cs
@@ -214,7 +214,7 @@ public class ResponsesTests : AoaiTestBase<OpenAIResponseClient>
         Assert.That(string.Join(string.Empty, deltaTextSegments), Is.EqualTo(finalResponseText));
     }
 
-    [RecordedTest]
+    [Test]
     [Ignore("https://github.com/Azure/azure-sdk-for-net/issues/49020")]
     [TestCase(Gpt4oMiniDeployment)]
     [TestCase(ComputerUseDeployment)]
