f04d6aee4401536a5a61816df7c361f55a9596ca
Christopher Scott <chriss@microsoft.com>
2025-03-21 09:39:12 -0500

    Add support for MCP Servers (#48919)

--- sdk/cloudmachine/Azure.Projects/samples/OFXRAG/Program.cs
+++ sdk/cloudmachine/Azure.Projects/samples/OFXRAG/Program.cs
@@ -7,7 +7,7 @@ using Azure.Projects.OpenAI;
 using OpenAI.Chat;
 
 ProjectInfrastructure infrastructure = new();
-infrastructure.AddFeature(new OpenAIModelFeature("gpt-35-turbo", "0125"));
+infrastructure.AddFeature(new OpenAIModelFeature("gpt-4o-mini", "2024-07-18"));
 infrastructure.AddFeature(new OpenAIModelFeature("text-embedding-ada-002", "2", AIModelKind.Embedding));
 
 // the app can be called with -init switch to generate bicep and prepare for azd deployment.
@@ -29,16 +29,24 @@ while (true)
 {
     Console.Write("> ");
     string prompt = Console.ReadLine();
-    if (string.IsNullOrEmpty(prompt)) continue;
-    if (string.Equals(prompt, "bye", StringComparison.OrdinalIgnoreCase)) break;
+    if (string.IsNullOrEmpty(prompt))
+        continue;
+    if (string.Equals(prompt, "bye", StringComparison.OrdinalIgnoreCase))
+        break;
     if (prompt.StartsWith("fact:", StringComparison.OrdinalIgnoreCase))
     {
         string fact = prompt[5..].Trim();
         processor.VectorDb.Add(fact);
         continue;
     }
-
-    ChatCompletion completion = processor.TakeTurn(conversation, prompt);
+    if (prompt.StartsWith("addmcp:", StringComparison.OrdinalIgnoreCase))
+    {
+        string mcp = prompt[7..].Trim();
+        Console.WriteLine($"Adding MCP server {mcp}");
+        await tools.AddMcpServerAsync(new Uri(mcp)).ConfigureAwait(false);
+        continue;
+    }
+    ChatCompletion completion = await processor.TakeTurnAsync(conversation, prompt).ConfigureAwait(false);
 
     Console.WriteLine(completion.AsText());
 }
