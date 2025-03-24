6036ec2f5e0d9fd53f16e95b8467b6d610d6ee88
Anna Pankiewicz <anna.pankiewicz@microsoft.com>
2025-03-24 18:28:13 +0000

    Fix recorded variables

--- sdk/networkcloud/Azure.ResourceManager.NetworkCloud/tests/NetworkCloudManagementTestEnvironment.cs
+++ sdk/networkcloud/Azure.ResourceManager.NetworkCloud/tests/NetworkCloudManagementTestEnvironment.cs
@@ -22,8 +22,8 @@ namespace Azure.ResourceManager.NetworkCloud.Tests
         public string ClusterExtendedLocation => GetRecordedVariable("CLUSTER_EXTENDED_LOCATION");
         public string ClusterRG => GetRecordedVariable("CLUSTER_RG");
         public string ClusterVersion => GetRecordedVariable("CLUSTER_VERSION");
-        public string ContainerUri => GetVariable("CONTAINER_URI");
-        public string UserAssignedIdentity => GetVariable("USER_ASSIGNED_IDENTITY");
+        public string ContainerUri => GetRecordedVariable("CONTAINER_URI");
+        public string UserAssignedIdentity => GetRecordedVariable("USER_ASSIGNED_IDENTITY");
 
         // Kubernetes Cluster
 
