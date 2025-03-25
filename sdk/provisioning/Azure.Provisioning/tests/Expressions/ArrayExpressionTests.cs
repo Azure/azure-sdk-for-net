aac1c2a57dbcad5bca6b26853230928e9ea1143d
Arcturus Zhang <dapzhang@microsoft.com>
2025-03-25 14:32:36 +0800

    add tests for array

---
+++ sdk/provisioning/Azure.Provisioning/tests/Expressions/ArrayExpressionTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Azure.Provisioning.Expressions;
+using NUnit.Framework;
+
+namespace Azure.Provisioning.Tests.Expressions
+{
+    public class ArrayExpressionTests
+    {
+        [TestCaseSource(nameof(_validateArrayExpressionTestData))]
+        public string ValidateArrayExpressions(ArrayExpression expression)
+        {
+            return expression.ToString();
+        }
+
+        private static IEnumerable<TestCaseData> _validateArrayExpressionTestData()
+        {
+            // NOTE: our AST always produces multi-line arrays with indents
+            // empty array
+            yield return new TestCaseData(new ArrayExpression())
+                .Returns($"[]");
+            // array of literals
+            yield return new TestCaseData(new ArrayExpression(new IntLiteralExpression(314), new BoolLiteralExpression(false), new StringLiteralExpression("foo")))
+                .Returns(@"[
+  314
+  false
+  'foo'
+]");
+            // array of objects
+            var obj = new ObjectExpression(
+                new PropertyExpression("p1", new StringLiteralExpression("p1 value")));
+            yield return new TestCaseData(new ArrayExpression(obj))
+                .Returns(@"[
+  {
+    p1: 'p1 value'
+  }
+]");
+        }
+    }
+}
---
+++ sdk/provisioning/Azure.Provisioning/tests/Expressions/LiteralExpressionTests.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using Azure.Provisioning.Expressions;
+using NUnit.Framework;
+
+namespace Azure.Provisioning.Tests.Expressions
+{
+    public class LiteralExpressionTests
+    {
+        [TestCaseSource(nameof(_validateLiteralExpressionTestData))]
+        public string? ValidateLiteralExpression(LiteralExpression literal)
+        {
+            return literal.ToString();
+        }
+
+        private static IEnumerable<TestCaseData> _validateLiteralExpressionTestData()
+        {
+            yield return new TestCaseData(new NullLiteralExpression()).Returns("null");
+
+            yield return new TestCaseData(new BoolLiteralExpression(true)).Returns("true");
+            yield return new TestCaseData(new BoolLiteralExpression(false)).Returns("false");
+
+            yield return new TestCaseData(new IntLiteralExpression(3141592)).Returns("3141592");
+            yield return new TestCaseData(new IntLiteralExpression(-271828)).Returns("-271828");
+
+            yield return new TestCaseData(new StringLiteralExpression("ordinary string")).Returns("'ordinary string'");
+            yield return new TestCaseData(new StringLiteralExpression("string with 'single quote'")).Returns("'string with \\'single quote\\''");
+        }
+    }
+}
