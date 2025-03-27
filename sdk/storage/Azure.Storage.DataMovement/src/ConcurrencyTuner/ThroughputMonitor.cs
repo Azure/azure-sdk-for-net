1ed24ed20d678c59774a2c50d3c9d09594dc37f7
David Brown <t-davidbrown@microsoft.com>
2025-03-27 18:38:34 -0400

    complete Throughput monitor

---
+++ sdk/storage/Azure.Storage.DataMovement/src/ConcurrencyTuner/ThroughputMonitor.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using static Azure.Storage.DataMovement.TransferProgressTracker;
+
+namespace Azure.Storage.DataMovement
+{
+    internal class ThroughputMonitor : IAsyncDisposable
+    {
+        private int _totalBytesTransferred;
+
+        private IProcessor<int> _bytesTransferredProcessor;
+
+        public int BytesTransferred {  get; set; }
+        public int TotalBytesTransferred { get => _totalBytesTransferred; }
+
+        public ThroughputMonitor(ThroughputMonitorOptions options = default) : this
+            (
+                ChannelProcessing.NewProcessor<int>(readers: 1)
+            ){ }
+
+        internal ThroughputMonitor(IProcessor<int> bytesTransferredProcessor)
+        {
+            _bytesTransferredProcessor = bytesTransferredProcessor;
+            _bytesTransferredProcessor.Process = ProcessBytesTransferredAsync;
+        }
+
+        private async Task ProcessBytesTransferredAsync(int item, CancellationToken cancellationToken)
+        {
+            await Task.Run(() => _totalBytesTransferred += item, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async ValueTask Add(int bytesTransferred, CancellationToken cancellationToken)
+        {
+            await _bytesTransferredProcessor.QueueAsync(bytesTransferred, cancellationToken).ConfigureAwait(false);
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ sdk/storage/Azure.Storage.DataMovement/tests/ThroughputMonitorTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Azure.Storage.DataMovement.Tests
+{
+    [TestFixture]
+    internal class ThroughputMonitorTests
+    {
+        [Test]
+        public void ThroughputMonitor_ConstructorShouldConstructWithoutError()
+        {
+            ThroughputMonitor tm = new ThroughputMonitor();
+            Assert.DoesNotThrow(() => new ThroughputMonitor());
+        }
+
+        [Test]
+        public void ThroughputMonitor_ShouldAllowAsync()
+        {
+            // Arrange
+            ThroughputMonitor tm = new ThroughputMonitor();
+            CancellationToken cancellationToken = new CancellationToken();
+
+            // Act
+            for (int i = 0; i < 10; i++)
+            {
+                tm.Add(i, cancellationToken).ConfigureAwait(false);
+                Thread.Sleep(100);
+            }
+
+            // Assert
+            Assert.AreEqual(45, tm.TotalBytesTransferred);
+        }
+    }
+}
