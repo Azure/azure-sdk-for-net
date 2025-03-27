5b3b5b3ddfa96d262b2e5de971d85ad6ad8e3812
Christopher Scott <chriscott@hotmail.com>
2025-03-27 10:20:43 -0500

    fb

--- sdk/identity/Azure.Identity/src/DefaultAzureCredentialFactory.cs
+++ sdk/identity/Azure.Identity/src/DefaultAzureCredentialFactory.cs
@@ -62,10 +62,12 @@ namespace Azure.Identity
                 chain.Add(CreateVisualStudioCredential());
             }
 
+#pragma warning disable CS0618 // Type or member is obsolete
             if (!Options.ExcludeVisualStudioCodeCredential)
             {
                 chain.Add(CreateVisualStudioCodeCredential());
             }
+#pragma warning restore CS0618 // Type or member is obsolete
 
             if (!Options.ExcludeAzureCliCredential)
             {
