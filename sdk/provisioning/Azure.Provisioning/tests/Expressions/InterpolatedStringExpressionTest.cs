814c6608e6da8c94a565098dc13142a35571a9e7
Arcturus Zhang <dapzhang@microsoft.com>
2025-03-28 11:23:23 +0800

    resolve comments

--- sdk/provisioning/Azure.Provisioning/tests/Expressions/InterpolatedStringExpressionTest.cs
+++ sdk/provisioning/Azure.Provisioning/tests/Expressions/InterpolatedStringExpressionTest.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
 using Azure.Provisioning.Expressions;
 using NUnit.Framework;
 
@@ -9,29 +8,25 @@ namespace Azure.Provisioning.Tests.Expressions
 {
     public class InterpolatedStringExpressionTest
     {
-        [TestCaseSource(nameof(BicepFunctionInterpolateTestData))]
-        public string ValidateBicepFunctionInterpolate(BicepValue<string> expression)
-        {
-            return expression.ToString();
-        }
-
-        private static IEnumerable<TestCaseData> BicepFunctionInterpolateTestData()
+        public void ValidateBicepFunctionInterpolate()
         {
             // test provisionable variable
-            yield return new TestCaseData(
+            Assert.AreEqual(
+                "'Var=${foo}'",
                 BicepFunction.Interpolate(
                     $"Var={new ProvisioningVariable("foo", typeof(string))}"
-                    ))
-                .Returns("'Var=${foo}'");
+                    )
+                );
 
             // test index expression in interpolation
-            yield return new TestCaseData(
+            Assert.AreEqual(
+                "'Endpoint=${foo['bar']}'",
                 BicepFunction.Interpolate(
                     $"Endpoint={new IndexExpression(
                         new IdentifierExpression("foo"),
                         new StringLiteralExpression("bar")
-                    )}"))
-                .Returns("'Endpoint=${foo['bar']}'");
+                    )}")
+                );
         }
     }
 }
