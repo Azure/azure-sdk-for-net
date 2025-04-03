5ab8d1e3651ed6d1ccfa3e34e21e2bd32f411c2f
Christopher Scott <chriscott@hotmail.com>
2025-04-03 14:31:51 -0500

    MCP filtering based on embeddings

--- sdk/cloudmachine/Azure.Projects/samples/ConsoleAgent/Program.cs
+++ sdk/cloudmachine/Azure.Projects/samples/ConsoleAgent/Program.cs
@@ -23,13 +23,18 @@ runner.Tools.AddLocalTools(typeof(Tools));
 
 ChatThread conversation = new();
 
+
+await runner.Tools.AddMcpServerAsync(new Uri("http://localhost:5000/sse")).ConfigureAwait(false);
+
 while (true)
 {
     Console.Write("> ");
     string prompt = Console.ReadLine();
 
-    if (string.IsNullOrEmpty(prompt)) continue;
-    if (string.Equals(prompt, "bye", StringComparison.OrdinalIgnoreCase)) break;
+    if (string.IsNullOrEmpty(prompt))
+        continue;
+    if (string.Equals(prompt, "bye", StringComparison.OrdinalIgnoreCase))
+        break;
     if (prompt.StartsWith("fact:", StringComparison.OrdinalIgnoreCase))
     {
         string fact = prompt[5..].Trim();
@@ -38,7 +43,7 @@ while (true)
     }
     if (prompt.StartsWith("mcp:", StringComparison.OrdinalIgnoreCase))
     {
-        string mcp = prompt[7..].Trim();
+        string mcp = prompt[4..].Trim();
         Console.WriteLine($"adding MCP server {mcp}");
         await runner.Tools.AddMcpServerAsync(new Uri(mcp)).ConfigureAwait(false);
         continue;
