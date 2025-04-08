c0066cb88f3bb7747743a666fc36697e1b34572c
vikramkotturu <vikram.kotturu@live.in>
2025-04-08 13:11:28 +0530

    Recovery service backup stable 2025-04-01. 2025-April release with support for ASEBackup.

--- sdk/recoveryservices-backup/Azure.ResourceManager.RecoveryServicesBackup/src/Generated/Models/WorkloadRecoveryPoint.Serialization.cs
+++ sdk/recoveryservices-backup/Azure.ResourceManager.RecoveryServicesBackup/src/Generated/Models/WorkloadRecoveryPoint.Serialization.cs
@@ -98,6 +98,8 @@ namespace Azure.ResourceManager.RecoveryServicesBackup.Models
                 switch (discriminator.GetString())
                 {
                     case "AzureWorkloadPointInTimeRecoveryPoint": return WorkloadPointInTimeRecoveryPoint.DeserializeWorkloadPointInTimeRecoveryPoint(element, options);
+                    case "AzureWorkloadSAPAsePointInTimeRecoveryPoint": return AzureWorkloadSapAsePointInTimeRecoveryPoint.DeserializeAzureWorkloadSapAsePointInTimeRecoveryPoint(element, options);
+                    case "AzureWorkloadSAPAseRecoveryPoint": return AzureWorkloadSapAseRecoveryPoint.DeserializeAzureWorkloadSapAseRecoveryPoint(element, options);
                     case "AzureWorkloadSAPHanaPointInTimeRecoveryPoint": return WorkloadSapHanaPointInTimeRecoveryPoint.DeserializeWorkloadSapHanaPointInTimeRecoveryPoint(element, options);
                     case "AzureWorkloadSAPHanaRecoveryPoint": return WorkloadSapHanaRecoveryPoint.DeserializeWorkloadSapHanaRecoveryPoint(element, options);
                     case "AzureWorkloadSQLPointInTimeRecoveryPoint": return WorkloadSqlPointInTimeRecoveryPoint.DeserializeWorkloadSqlPointInTimeRecoveryPoint(element, options);
