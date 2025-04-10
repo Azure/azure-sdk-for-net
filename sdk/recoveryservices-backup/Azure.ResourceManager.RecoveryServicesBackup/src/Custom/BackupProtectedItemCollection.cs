602f853620fbd6f784b87dea6d6ca503487efeb0
vikramkotturu <vikram.kotturu@live.in>
2025-04-10 20:38:14 +0530

    Corrected custome file location based on names space. Added missing Async/non-Async counterparts to missing methods.

--- sdk/recoveryservices-backup/Azure.ResourceManager.RecoveryServicesBackup/src/Custom/BackupProtectedItemCollection.cs
+++ sdk/recoveryservices-backup/Azure.ResourceManager.RecoveryServicesBackup/src/Custom/BackupProtectedItemCollection.cs
@@ -3,6 +3,7 @@
 
 using System.ComponentModel;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Azure.ResourceManager.RecoveryServicesBackup
 {
@@ -17,10 +18,22 @@ namespace Azure.ResourceManager.RecoveryServicesBackup
         /// <param name="data"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        [EditorBrowsable(EditorBrowsableState.Never)]
         public virtual ArmOperation<BackupProtectedItemResource> CreateOrUpdate(WaitUntil waitUntil, string protectedItemName, BackupProtectedItemData data, CancellationToken cancellationToken)
         {
             return CreateOrUpdate(waitUntil, protectedItemName, data, xMsAuthorizationAuxiliary: null, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <summary>Create or Update Async.</summary>
+        /// </summary>
+        /// <param name="waitUntil"></param>
+        /// <param name="protectedItemName"></param>
+        /// <param name="data"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public virtual Task<ArmOperation<BackupProtectedItemResource>> CreateOrUpdateAsync(WaitUntil waitUntil, string protectedItemName, BackupProtectedItemData data, CancellationToken cancellationToken)
+        {
+            return CreateOrUpdateAsync(waitUntil, protectedItemName, data, xMsAuthorizationAuxiliary: null, cancellationToken: cancellationToken);
         }
     }
 }
--- sdk/recoveryservices-backup/Azure.ResourceManager.RecoveryServicesBackup/src/Custom/BackupProtectedItemResource.cs
+++ sdk/recoveryservices-backup/Azure.ResourceManager.RecoveryServicesBackup/src/Custom/BackupProtectedItemResource.cs
@@ -11,13 +11,24 @@ namespace Azure.ResourceManager.RecoveryServicesBackup
     public partial class BackupProtectedItemResource
     {
         /// <summary>
+        /// Update.
+        /// </summary>
+        /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
+        /// <param name="backupProtectedItemData"> resource backed up item. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns></returns>
+        public virtual ArmOperation<BackupProtectedItemResource> Update(WaitUntil waitUntil, BackupProtectedItemData backupProtectedItemData, CancellationToken cancellationToken)
+        {
+            return Update(waitUntil, backupProtectedItemData, xMsAuthorizationAuxiliary: null, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
         /// Update async.
         /// </summary>
         /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
         /// <param name="backupProtectedItemData"> resource backed up item. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <returns></returns>
-        [EditorBrowsable(EditorBrowsableState.Never)]
         public virtual Task<ArmOperation<BackupProtectedItemResource>> UpdateAsync(WaitUntil waitUntil, BackupProtectedItemData backupProtectedItemData, CancellationToken cancellationToken)
         {
             return UpdateAsync(waitUntil, backupProtectedItemData, xMsAuthorizationAuxiliary: null, cancellationToken: cancellationToken);
