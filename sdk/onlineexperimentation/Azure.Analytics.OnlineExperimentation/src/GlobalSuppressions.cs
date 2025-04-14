cbcc04d8407489b0eb331aaf55e5619b3eb60350
smichtch <smichtch@users.noreply.github.com>
2025-04-14 08:58:03 -0700

    Update SuppressMessage attribute justifications.

--- sdk/onlineexperimentation/Azure.Analytics.OnlineExperimentation/src/GlobalSuppressions.cs
+++ sdk/onlineexperimentation/Azure.Analytics.OnlineExperimentation/src/GlobalSuppressions.cs
@@ -3,10 +3,10 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.AverageMetricDefinition")]
-[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.EventCountMetricDefinition")]
-[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.EventRateMetricDefinition")]
-[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.SumMetricDefinition")]
-[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.PercentileMetricDefinition")]
-[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.UserCountMetricDefinition")]
-[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "<Pending>", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.UserRateMetricDefinition")]
+[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "Names approved by SDK review board.", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.AverageMetricDefinition")]
+[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "Names approved by SDK review board.", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.EventCountMetricDefinition")]
+[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "Names approved by SDK review board.", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.EventRateMetricDefinition")]
+[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "Names approved by SDK review board.", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.SumMetricDefinition")]
+[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "Names approved by SDK review board.", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.PercentileMetricDefinition")]
+[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "Names approved by SDK review board.", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.UserCountMetricDefinition")]
+[assembly: SuppressMessage("Naming", "AZC0031:Improper model name suffix", Justification = "Names approved by SDK review board.", Scope = "type", Target = "~T:Azure.Analytics.OnlineExperimentation.UserRateMetricDefinition")]
