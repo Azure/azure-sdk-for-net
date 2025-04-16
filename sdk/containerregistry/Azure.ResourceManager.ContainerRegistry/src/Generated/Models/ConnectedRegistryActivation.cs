6a1cf5584be0036b2eb1e812f348464cabecf716
Josh Baunach <joshbaunach@microsoft.com>
2025-04-16 18:05:16 -0400

    Generate the files

--- sdk/containerregistry/Azure.ResourceManager.ContainerRegistry/src/Generated/Models/ConnectedRegistryActivation.cs
+++ sdk/containerregistry/Azure.ResourceManager.ContainerRegistry/src/Generated/Models/ConnectedRegistryActivation.cs
@@ -43,7 +43,7 @@ namespace Azure.ResourceManager.ContainerRegistry.Models
         /// </list>
         /// </para>
         /// </summary>
-        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+        private protected IDictionary<string, BinaryData> _serializedAdditionalRawData;
 
         /// <summary> Initializes a new instance of <see cref="ConnectedRegistryActivation"/>. </summary>
         internal ConnectedRegistryActivation()
