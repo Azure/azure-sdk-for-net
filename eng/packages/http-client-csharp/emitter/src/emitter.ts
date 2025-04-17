560dcdc178113a1e17fd106bf5d9aa4285d07968
jolov <jolov@microsoft.com>
2025-04-17 10:10:17 -0700

    Add namespace option to emitter

--- eng/packages/http-client-csharp/emitter/src/emitter.ts
+++ eng/packages/http-client-csharp/emitter/src/emitter.ts
@@ -1,16 +1,41 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import { EmitContext } from "@typespec/compiler";
+import { createTypeSpecLibrary, EmitContext, JSONSchemaType } from "@typespec/compiler";
 
 import {
   $onEmit as $onMTGEmit,
-  CSharpEmitterOptions
+  CSharpEmitterOptions,
+  CSharpEmitterOptionsSchema,
+  $lib as httpClientCSharpLib,
 } from "@typespec/http-client-csharp";
 
-export async function $onEmit(context: EmitContext<CSharpEmitterOptions>) {
+export async function $onEmit(context: EmitContext<AzureEmitterOptions>) {
   context.options["generator-name"] ??= "AzureClientGenerator";
   context.options["emitter-extension-path"] ??= import.meta.url;
   context.options["license"] ??= { name: "MIT License", company: "Microsoft Corporation" };
   await $onMTGEmit(context);
 }
+
+export interface AzureEmitterOptions extends CSharpEmitterOptions {
+  namespace?: string;
+}
+
+export const AzureEmitterOptionsSchema: JSONSchemaType<AzureEmitterOptions> = {
+  type: "object",
+  properties: {
+    ...CSharpEmitterOptionsSchema.properties,
+    namespace: {
+      type: "string",
+      description: "The namespace to use for the generated code.",
+    },
+  },
+};
+
+export const $lib = createTypeSpecLibrary({
+  name: "@azure-typespec/http-client-csharp",
+  diagnostics: httpClientCSharpLib.diagnostics,
+  emitter: {
+    options: AzureEmitterOptionsSchema,
+  },
+});
