fe08fe89e713ad65ca12614683d5e146a6eaf789
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2025-04-17 21:28:53 -0700

    Use tspd in emitters (#49477)
    * Use tspd in emitters
    
    * mgmt
    
    * prettier
    
    * run prettier on package.json
    
    * fix
    
    * add emitter.md

--- eng/packages/http-client-csharp-mgmt/emitter/src/lib/lib.ts
+++ eng/packages/http-client-csharp-mgmt/emitter/src/lib/lib.ts
@@ -1,12 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+import { AzureEmitterOptionsSchema } from "@azure-typespec/http-client-csharp";
 import { createTypeSpecLibrary } from "@typespec/compiler";
 import { $lib as httpClientCSharpLib } from "@typespec/http-client-csharp";
-import { AzureEmitterOptionsSchema } from "../options.js";
 
 export const $lib = createTypeSpecLibrary({
-  name: "@azure-typespec/http-client-csharp",
+  name: "@azure-typespec/http-client-csharp-mgmt",
   diagnostics: httpClientCSharpLib.diagnostics,
   emitter: {
     options: AzureEmitterOptionsSchema
--- eng/packages/http-client-csharp/emitter/src/emitter.ts
+++ eng/packages/http-client-csharp/emitter/src/emitter.ts
@@ -3,14 +3,15 @@
 
 import { EmitContext } from "@typespec/compiler";
 
-import {
-  $onEmit as $onMTGEmit,
-} from "@typespec/http-client-csharp";
+import { $onEmit as $onMTGEmit } from "@typespec/http-client-csharp";
 import { AzureEmitterOptions } from "./options.js";
 
 export async function $onEmit(context: EmitContext<AzureEmitterOptions>) {
   context.options["generator-name"] ??= "AzureClientGenerator";
   context.options["emitter-extension-path"] ??= import.meta.url;
-  context.options["license"] ??= { name: "MIT License", company: "Microsoft Corporation" };
+  context.options["license"] ??= {
+    name: "MIT License",
+    company: "Microsoft Corporation"
+  };
   await $onMTGEmit(context);
 }
