6370881ae9231894a4cc52577a7babddf1d98046
jolov <jolov@microsoft.com>
2025-04-21 14:50:44 -0700

    Support model-namespace option

--- eng/packages/http-client-csharp/emitter/src/lib/lib.ts
+++ eng/packages/http-client-csharp/emitter/src/lib/lib.ts
@@ -1,13 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import { createTypeSpecLibrary } from "@typespec/compiler";
+import { createTypeSpecLibrary, DiagnosticDefinition, DiagnosticMessages } from "@typespec/compiler";
 import { $lib as httpClientCSharpLib } from "@typespec/http-client-csharp";
 import { AzureEmitterOptionsSchema } from "../options.js";
 
+const diags: { [code: string]: DiagnosticDefinition<DiagnosticMessages> } = {
+  ...httpClientCSharpLib.diagnostics,
+  "invalid-model-namespace-usage": {
+    severity: "warning",
+    messages: {
+      default:
+        "Cannot generate CSharp SDK since no public root client is defined in typespec file.",
+    },
+  }
+};
+
 export const $lib = createTypeSpecLibrary({
   name: "@azure-typespec/http-client-csharp",
-  diagnostics: httpClientCSharpLib.diagnostics,
+  diagnostics: diags,
   emitter: {
     options: AzureEmitterOptionsSchema
   }
---
+++ eng/packages/http-client-csharp/generator/Azure.Generator/src/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.TypeSpec.Generator;
+
+namespace Azure.Generator
+{
+    internal static class ConfigurationExtensions
+    {
+        public static bool UseModelNamespace(this Configuration config) =>
+            UseModelNamespace(config.AdditionalConfigurationOptions);
+
+        internal static bool UseModelNamespace(IReadOnlyDictionary<string, BinaryData> options)
+        {
+            return !options.TryGetValue("model-namespace", out var value)
+                   // default to true if no value is provided or it cannot be parsed
+                   || !bool.TryParse(value.ToString(), out var parsed)
+                   || parsed;
+        }
+    }
+}
