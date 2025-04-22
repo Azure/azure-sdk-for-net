c4bae2beef1afd4867c5c83f94273abc5010be27
Amari West <amariwest@microsoft.com>
2025-04-22 09:22:12 -0700

    added transcription and summarization

---
+++ sdk/communication/Azure.Communication.CallAutomation/src/Models/PostProcessingOptions.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+using System;
+
+namespace Azure.Communication.CallAutomation
+{
+    /// <summary>
+    /// PostProcessingOptions base class
+    /// </summary>
+    public abstract partial class PostProcessingOptions
+    {
+    }
+}
