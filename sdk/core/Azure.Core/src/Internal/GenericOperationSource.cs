9e3dadc99fec0669d08e7054d01ec4951951378b
m-nash <64171366+m-nash@users.noreply.github.com>
2025-04-22 16:27:55 -0700

    update azure core to use new analyzers and scm and update shared source

--- sdk/core/Azure.Core/src/Internal/GenericOperationSource.cs
+++ sdk/core/Azure.Core/src/Internal/GenericOperationSource.cs
@@ -17,6 +17,10 @@ namespace Azure.Core
             => new ValueTask<T>(CreateResult(response));
 
         private T CreateResult(Response response)
+#pragma warning disable AZC0150 // Use ModelReaderWriter overloads with ModelReaderWriterContext
+            // use this will never be a collection so with the annotation this is safe
+            // in the future we can consider how to pull in the right context for perf improvement
             => ModelReaderWriter.Read<T>(response.Content)!;
+#pragma warning restore AZC0150 // Use ModelReaderWriter overloads with ModelReaderWriterContext
     }
 }
--- sdk/core/System.ClientModel/tests/ModelReaderWriterTests/BadContextTests.cs
+++ sdk/core/System.ClientModel/tests/ModelReaderWriterTests/BadContextTests.cs
@@ -2,9 +2,7 @@
 // Licensed under the MIT License.
 
 using System.ClientModel.Primitives;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using NUnit.Framework;
 
