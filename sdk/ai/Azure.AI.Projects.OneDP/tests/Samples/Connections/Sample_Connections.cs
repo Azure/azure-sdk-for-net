ef334ea133e7430afbd86f28f94c54b5c79e5cc7
sophia-ramsey <sophiaramsey@microsoft.com>
2025-04-24 13:39:44 -0700

    add sample files

--- sdk/ai/Azure.AI.Projects.OneDP/tests/Samples/Connections/Sample_Connections.cs
+++ sdk/ai/Azure.AI.Projects.OneDP/tests/Samples/Connections/Sample_Connections.cs
@@ -51,5 +51,44 @@ namespace Azure.AI.Projects.OneDP.Tests
 
             #endregion
         }
+
+        [Test]
+        [AsyncOnly]
+        public async Task ConnectionsExampleAsync()
+        {
+            #region Snippet:ConnectionsExampleAsync
+#if SNIPPET
+            var endpoint = Environment.GetEnvironmentVariable("PROJECT_ENDPOINT");
+            var connectionName = Environment.GetEnvironmentVariable("CONNECTION_NAME");
+#else
+            var endpoint = TestEnvironment.PROJECTENDPOINT;
+            var connectionName = TestEnvironment.CONNECTIONNAME;
+#endif
+            AIProjectClient projectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+            Connections connectionsClient = projectClient.GetConnectionsClient();
+
+            Console.WriteLine("List the properties of all connections:");
+            await foreach (var connection in connectionsClient.GetConnectionsAsync())
+            {
+                Console.WriteLine(connection);
+                Console.Write(connection.Name);
+            }
+
+            Console.WriteLine("List the properties of all connections of a particular type (e.g., Azure OpenAI connections):");
+            await foreach (var connection in connectionsClient.GetConnectionsAsync(connectionType: ConnectionType.AzureOpenAI))
+            {
+                Console.WriteLine(connection);
+            }
+
+            Console.WriteLine($"Get the properties of a connection named `{connectionName}`:");
+            var specificConnection = await connectionsClient.GetConnectionAsync(connectionName);
+            Console.WriteLine(specificConnection);
+
+            Console.WriteLine("Get the properties of a connection with credentials:");
+            var specificConnectionCredentials = await connectionsClient.GetWithCredentialsAsync(connectionName);
+            Console.WriteLine(specificConnectionCredentials);
+
+            #endregion
+        }
     }
 }
