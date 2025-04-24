f35e6f9e68f0c659bfeefe0d07c18beebdb5b64b
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2025-04-24 09:26:16 -0700

    Adopt body-optionality and spread scenarios (#49622)
    * Adopt body-optionality scenarios
    
    * Add spread tests

---
+++ eng/packages/http-client-csharp/generator/TestProjects/Spector.Tests/Http/Parameters/BodyOptionality/BodyOptionalityTests.cs
@@ -0,0 +1,36 @@
+﻿using System.ClientModel;
+using System.Threading.Tasks;
+using Azure;
+using NUnit.Framework;
+using Parameters.BodyOptionality;
+
+namespace TestProjects.Spector.Tests.Http.Parameters.BodyOptionality
+{
+    public class BodyOptionalityTests : SpectorTestBase
+    {
+        [SpectorTest]
+        public Task Parameters_BodyOptionality_requiredExplicit() => Test(async (host) =>
+        {
+            Response response = await new BodyOptionalityClient(host, null).RequiredExplicitAsync(new BodyModel("foo"));
+            Assert.AreEqual(204, response.Status);
+        });
+
+        [SpectorTest]
+        public Task Parameters_BodyOptionality_OptionalExplicit() => Test(async (host) =>
+        {
+            var client = new BodyOptionalityClient(host, null).GetOptionalExplicitClient();
+            Response response = await client.SetAsync(new BodyModel("foo"));
+            Assert.AreEqual(204, response.Status);
+
+            response = await client.OmitAsync();
+            Assert.AreEqual(204, response.Status);
+        });
+
+        [SpectorTest]
+        public Task Parameters_BodyOptionality_requiredImplicit() => Test(async (host) =>
+        {
+            Response response = await new BodyOptionalityClient(host, null).RequiredImplicitAsync("foo");
+            Assert.AreEqual(204, response.Status);
+        });
+    }
+}
