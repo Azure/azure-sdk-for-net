16f8bd9db1790fe386a77ef58b55800468e1055a
howieleung <howieleung@microsoft.com>
2025-04-25 09:02:01 -0700

    auto function call

--- sdk/ai/Azure.AI.Projects/src/Custom/AIProjectClientOptions.cs
+++ sdk/ai/Azure.AI.Projects/src/Custom/AIProjectClientOptions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+using System;
 using Azure.Core;
 
 namespace Azure.AI.Projects
@@ -10,5 +12,20 @@ namespace Azure.AI.Projects
     {
         /// <summary> The size of the client cache. </summary>
         public int ClientCacheSize { get; set; } = 100;
+
+        private void ValidateAutoFunctions(Dictionary<string, Delegate> delegates)
+        {
+            if (delegates == null || delegates.Count == 0)
+            {
+                throw new InvalidOperationException("The delegate dictionary must have at least one delegate.");
+            }
+            foreach (var kvp in delegates)
+            {
+                if (kvp.Value.Method.ReturnType != typeof(string))
+                {
+                    throw new InvalidOperationException($"The Delegates must have string as return type.");
+                }
+            }
+        }
     }
 }
