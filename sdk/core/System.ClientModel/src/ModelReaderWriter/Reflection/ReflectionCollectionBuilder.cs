82fcd29f40be7388cb0d11fd8b862c1b4adb6c32
m-nash <64171366+m-nash@users.noreply.github.com>
2025-04-30 14:30:18 -0700

    Add non aot compatible support for collection reading and writing

---
+++ sdk/core/System.ClientModel/src/ModelReaderWriter/Reflection/ReflectionCollectionBuilder.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.ClientModel.Primitives;
+
+[RequiresUnreferencedCode("This method uses reflection use the overload that takes a ModelReaderWriterContext to be AOT compatible.")]
+internal class ReflectionCollectionBuilder : ModelReaderWriterTypeBuilder
+{
+    private Type _collectionType;
+    private string _addMethod;
+
+    public ReflectionCollectionBuilder(Type collectionType, string addMethod = "Add")
+    {
+        _collectionType = collectionType;
+        _addMethod = addMethod;
+    }
+
+    protected override Type BuilderType => _collectionType;
+
+    protected override object CreateInstance() => Activator.CreateInstance(_collectionType)!;
+
+    protected override Type? ItemType => _collectionType.GetGenericArguments()[0];
+
+    protected override void AddItem(object collection, object? item)
+        => _collectionType.GetMethod(_addMethod, [ ItemType! ])!.Invoke(collection, [ item ]);
+}
---
+++ sdk/core/System.ClientModel/src/ModelReaderWriter/Reflection/ReflectionReadOnlyDictionaryBuilder.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.ClientModel.Primitives;
+
+[RequiresDynamicCode("This method uses reflection use the overload that takes a ModelReaderWriterContext to be AOT compatible.")]
+[RequiresUnreferencedCode("This method uses reflection use the overload that takes a ModelReaderWriterContext to be AOT compatible.")]
+internal class ReflectionReadOnlyDictionaryBuilder : ModelReaderWriterTypeBuilder
+{
+    private Type _dictionaryType;
+
+    public ReflectionReadOnlyDictionaryBuilder(Type dictionaryType)
+    {
+        _dictionaryType = dictionaryType;
+    }
+
+    protected override Type BuilderType => typeof(Dictionary<,>).MakeGenericType(_dictionaryType.GenericTypeArguments);
+
+    protected override object CreateInstance() => Activator.CreateInstance(BuilderType)!;
+
+    protected override Type? ItemType => _dictionaryType.GetGenericArguments()[1];
+
+    protected override void AddKeyValuePair(object dictionary, string key, object? item)
+    {
+        var addMethod = BuilderType.GetMethod("Add", [typeof(string), ItemType!])!.Invoke(dictionary, [key, item]);
+    }
+
+    protected override object ToCollection(object builder) => Activator.CreateInstance(_dictionaryType, [builder])!;
+}
