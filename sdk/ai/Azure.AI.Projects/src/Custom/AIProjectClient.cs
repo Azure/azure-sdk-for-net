919a0c6674ecf6212d9ad23a40d0dc33dde6a17f
Glenn Harper <glharper@microsoft.com>
2025-05-02 08:02:37 -0700

    add custom AIProjectClient back in

---
+++ sdk/ai/Azure.AI.Projects/src/Custom/AIProjectClient.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Identity;
+
+namespace Azure.AI.Projects
+{
+    // Data plane generated client.
+    /// <summary> The AzureAI service client. </summary>
+    public partial class AIProjectClient : ConnectionProvider
+    {
+        private readonly ConnectionCacheManager _cacheManager;
+        private readonly Connections _connectionsClient;
+
+        /// <summary> Initializes a new instance of AIProjectClient. </summary>
+        /// <param name="endpoint">
+        /// Project endpoint. In the form "https://&lt;your-ai-services-account-name&gt;.services.ai.azure.com/api/projects/_project"
+        /// if your Foundry Hub has only one Project, or to use the default Project in your Hub. Or in the form
+        /// "https://&lt;your-ai-services-account-name&gt;.services.ai.azure.com/api/projects/&lt;your-project-name&gt;" if you want to explicitly
+        /// specify the Foundry Project name.
+        /// </param>
+        /// <param name="credential"> A credential used to authenticate to an Azure Service. </param>
+        /// <param name="options"> The options for configuring the client. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="endpoint"/> or <paramref name="credential"/> is null. </exception>
+        public AIProjectClient(Uri endpoint, TokenCredential credential, AIProjectClientOptions options)
+        {
+            Argument.AssertNotNull(endpoint, nameof(endpoint));
+            Argument.AssertNotNull(credential, nameof(credential));
+            options ??= new AIProjectClientOptions();
+
+            ClientDiagnostics = new ClientDiagnostics(options, true);
+            _tokenCredential = credential;
+            _pipeline = HttpPipelineBuilder.Build(options, Array.Empty<HttpPipelinePolicy>(), new HttpPipelinePolicy[] { new BearerTokenAuthenticationPolicy(_tokenCredential, AuthorizationScopes) }, new ResponseClassifier());
+            _endpoint = endpoint;
+
+            _connectionsClient = GetConnectionsClient();
+            _cacheManager = new ConnectionCacheManager(_connectionsClient, _tokenCredential);
+        }
+
+        /// <summary>
+        /// Retrieves the connection options for a specified client type and instance ID.
+        /// </summary>
+        public override ClientConnection GetConnection(string connectionId) => _cacheManager.GetConnection(connectionId);
+
+        /// <summary>
+        /// Retrieves all connection options.
+        /// </summary>
+        public override IEnumerable<ClientConnection> GetAllConnections() => _cacheManager.GetAllConnections();
+    }
+}
