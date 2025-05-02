904e4c3676d27f0c0be1b30a9617504119708abe
Swati Aggarwal <aggarwalsw@microsoft.com>
2025-05-02 10:02:58 +0000

    LambdaTest Stable SDK Release

--- sdk/lambdatesthyperexecute/Azure.ResourceManager.LambdaTestHyperExecute/CHANGELOG.md
+++ sdk/lambdatesthyperexecute/Azure.ResourceManager.LambdaTestHyperExecute/CHANGELOG.md
@@ -1,16 +1,8 @@
 # Release History
 
-## 1.0.0-beta.2 (Unreleased)
+## 1.0.0-beta.1 (Unreleased)
 
-### Features Added
 
-### Breaking Changes
-
-### Bugs Fixed
-
-### Other Changes
-
-## 1.0.0-beta.1 (2025-04-24)
 
 ### Features Added
 
--- sdk/lambdatesthyperexecute/Azure.ResourceManager.LambdaTestHyperExecute/src/Generated/RestOperations/OrganizationsRestOperations.cs
+++ sdk/lambdatesthyperexecute/Azure.ResourceManager.LambdaTestHyperExecute/src/Generated/RestOperations/OrganizationsRestOperations.cs
@@ -32,7 +32,7 @@ namespace Azure.ResourceManager.LambdaTestHyperExecute
         {
             _pipeline = pipeline ?? throw new ArgumentNullException(nameof(pipeline));
             _endpoint = endpoint ?? new Uri("https://management.azure.com");
-            _apiVersion = apiVersion ?? "2024-02-01-preview";
+            _apiVersion = apiVersion ?? "2024-02-01";
             _userAgent = new TelemetryDetails(GetType().Assembly, applicationId);
         }
 
