4bb9b14e5303ae4b63ab68a3a2184a1f7a8d7a81
gregocmsft <v-gregorioc@microsoft.com>
2025-05-05 10:15:15 -0300

    + Added get and delete response from id to Responses sample

--- sdk/openai/Azure.AI.OpenAI/tests/Samples/04_Responses.cs
+++ sdk/openai/Azure.AI.OpenAI/tests/Samples/04_Responses.cs
@@ -7,6 +7,7 @@ using OpenAI.Responses;
 using Azure.Identity;
 using System.Collections.Generic;
 using System;
+using System.ClientModel;
 
 namespace Azure.AI.OpenAI.Samples;
 
@@ -60,6 +61,26 @@ public partial class AzureOpenAISamples
                 ]);
 
         Console.WriteLine($"{response.Id}: {((MessageResponseItem)response.OutputItems[0]).Content[0].Text}");
+
+        OpenAIResponse getResponse = client.GetResponse(response.Id);
+        Console.WriteLine($"Get response from id {response.Id}...: {((MessageResponseItem)response.OutputItems[0]).Content[0].Text}");
+
+        ResponseDeletionResult deleteResponse = client.DeleteResponse(response.Id);
+        Console.WriteLine($"Deleting response from id... \nResponse deleted: {deleteResponse.Deleted}");
+
+        try
+        {
+            OpenAIResponse getDeletedResponse = client.GetResponse(response.Id);
+            Console.WriteLine($"Response not deleted properly: {((MessageResponseItem)getDeletedResponse.OutputItems[0]).Content[0].Text}");
+        }
+        catch (ClientResultException ex)
+        {
+            if (!ex.Message.Contains("404"))
+            {
+                throw;
+            }
+            Console.WriteLine($"Response was not found as expected: {ex.Message}");
+        }
 
         #endregion
     }
