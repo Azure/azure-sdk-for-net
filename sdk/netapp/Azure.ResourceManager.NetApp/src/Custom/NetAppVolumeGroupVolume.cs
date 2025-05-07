b5ed56119d5f524a581a922083e4b39311823b1a
Auðunn <audunn@netapp.com>
2025-05-07 09:49:59 +0000

    [NetAppFiles] update to 2025-01-01 (#49220)
    Co-authored-by: Arcturus Zhang <dapzhang@microsoft.com>

---
+++ sdk/netapp/Azure.ResourceManager.NetApp/src/Custom/NetAppVolumeGroupVolume.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System.ComponentModel;
+using Azure.Core;
+using Azure.ResourceManager.Models;
+
+namespace Azure.ResourceManager.NetApp.Models
+{
+    /// <summary> Volume resource. </summary>
+    [CodeGenSerialization(nameof(IsRestoring), "isRestoring")]
+    public partial class NetAppVolumeGroupVolume
+    {
+        /// <summary> Restoring. ReadOnly property indicating if volume is being resored </summary>
+        public bool? IsRestoring
+        {
+            get;
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            set;
+        }
+    }
+}
