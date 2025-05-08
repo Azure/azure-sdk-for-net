835857e7ebdb115b557dfaa38c22365acfc5a252
m-nash <64171366+m-nash@users.noreply.github.com>
2025-05-07 19:17:59 -0700

    regen azure ai agents persistent

--- sdk/ai/Azure.AI.Agents.Persistent/src/Generated/OpenApiFunctionDefinition.Serialization.cs
+++ sdk/ai/Azure.AI.Agents.Persistent/src/Generated/OpenApiFunctionDefinition.Serialization.cs
@@ -164,7 +164,7 @@ namespace Azure.AI.Agents.Persistent
             switch (format)
             {
                 case "J":
-                    return ModelReaderWriter.Write(this, options);
+                    return ModelReaderWriter.Write(this, options, AzureAIAgentsPersistentContext.Default);
                 default:
                     throw new FormatException($"The model {nameof(OpenApiFunctionDefinition)} does not support writing '{options.Format}' format.");
             }
