eb8f5eb4f39d56984165f8fe14afa920adb57c6a
Arthur Ma <arthurma@microsoft.com>
2025-05-08 19:29:06 +0800

    update

---
+++ sdk/secretsstoreextension/Azure.ResourceManager.SecretsStoreExtension/src/Custom/Models/SecretSyncPatchProperties.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using Azure.Core;
+
+namespace Azure.ResourceManager.SecretsStoreExtension.Models
+{
+    /// <summary> The updatable properties of the SecretSync. </summary>
+    [CodeGenModel("SecretSyncUpdateProperties")]
+    public partial class SecretSyncPatchProperties
+    {
+    }
+}
