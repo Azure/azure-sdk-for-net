729efe929dd6d1f97415e0152d6873be71030fbd
Glenn Harper <glharper@microsoft.com>
2025-05-08 14:39:06 -0700

    async sample

--- sdk/ai/Azure.AI.Projects/tests/Samples/Extensions/Sample_AIAgents.cs
+++ sdk/ai/Azure.AI.Projects/tests/Samples/Extensions/Sample_AIAgents.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.AI.Agents.Persistent;
@@ -111,14 +112,83 @@ public class Sample_AIAgents : SamplesBase<AIProjectsTestEnvironment>
         AIProjectClient projectClient = new(new Uri(endpoint), new DefaultAzureCredential());
         PersistentAgentsClient agentsClient = projectClient.GetPersistentAgentsClient();
 
+        // Step 1: Create an agent
+        #region Snippet:AgentsOverviewCreateAgent
         PersistentAgent agent = await agentsClient.Administration.CreateAgentAsync(
             model: modelDeploymentName,
             name: "Math Tutor",
             instructions: "You are a personal math tutor. Write and run code to answer math questions."
         );
+        #endregion
+
+        //// Step 2: Create a thread
+        #region Snippet:AgentsOverviewCreateThread
+        PersistentAgentThread thread = await agentsClient.Threads.CreateThreadAsync();
+        #endregion
+
+        // Step 3: Add a message to a thread
+        #region Snippet:AgentsOverviewCreateMessage
+        ThreadMessage message = await agentsClient.Messages.CreateMessageAsync(
+            thread.Id,
+            MessageRole.User,
+            "I need to solve the equation `3x + 11 = 14`. Can you help me?");
+        #endregion
+
+        // Intermission: message is now correlated with thread
+        // Intermission: listing messages will retrieve the message just added
+
+        AsyncPageable<ThreadMessage> messagesList = agentsClient.Messages.GetMessagesAsync(thread.Id);
+        List<ThreadMessage> messagesOne = await messagesList.ToListAsync();
+        Assert.AreEqual(message.Id, messagesOne[0].Id);
+
+        // Step 4: Run the agent
+        #region Snippet:AgentsOverviewCreateRun
+        ThreadRun run = await agentsClient.Runs.CreateRunAsync(
+            thread.Id,
+            agent.Id,
+            additionalInstructions: "Please address the user as Jane Doe. The user has a premium account.");
+        #endregion
+
+        #region Snippet:AgentsOverviewWaitForRun
+        do
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(500));
+            run = await agentsClient.Runs.GetRunAsync(thread.Id, run.Id);
+        }
+        while (run.Status == RunStatus.Queued
+            || run.Status == RunStatus.InProgress);
+        Assert.AreEqual(
+            RunStatus.Completed,
+            run.Status,
+            run.LastError?.Message);
+        #endregion
+
+        #region Snippet:AgentsOverviewListUpdatedMessages
+        AsyncPageable<ThreadMessage> messages
+            = agentsClient.Messages.GetMessagesAsync(
+                threadId: thread.Id, order: ListSortOrder.Ascending);
+
+        await foreach (ThreadMessage threadMessage in messages)
+        {
+            Console.Write($"{threadMessage.CreatedAt:yyyy-MM-dd HH:mm:ss} - {threadMessage.Role,10}: ");
+            foreach (MessageContent contentItem in threadMessage.ContentItems)
+            {
+                if (contentItem is MessageTextContent textItem)
+                {
+                    Console.Write(textItem.Text);
+                }
+                else if (contentItem is MessageImageFileContent imageFileItem)
+                {
+                    Console.Write($"<image from ID: {imageFileItem.FileId}");
+                }
+                Console.WriteLine();
+            }
+        }
+        #endregion
+        #region Snippet:AgentsOverviewCleanup
+        await agentsClient.Threads.DeleteThreadAsync(threadId: thread.Id);
 
         await agentsClient.Administration.DeleteAgentAsync(agentId: agent.Id);
-
         #endregion
     }
 }
