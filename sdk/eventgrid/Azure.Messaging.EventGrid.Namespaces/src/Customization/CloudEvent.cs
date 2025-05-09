47e1e8801e50a430f69b434801dd06f2926d88bc
jolov <jolov@microsoft.com>
2025-05-08 17:24:33 -0700

    Delete usings

--- sdk/eventgrid/Azure.Messaging.EventGrid.Namespaces/src/Customization/CloudEvent.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid.Namespaces/src/Customization/CloudEvent.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.Core;
-
 namespace Azure.Messaging.EventGrid.Namespaces
 {
     // make this type internal because it should be replaced by Azure.Messaging.CloudEvent from Azure.Core assembly
--- sdk/eventgrid/Azure.Messaging.EventGrid.Namespaces/src/Customization/EventGridNamespacesModelFactory.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid.Namespaces/src/Customization/EventGridNamespacesModelFactory.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.Core;
-
 namespace Azure.Messaging.EventGrid.Namespaces
 {
     [CodeGenType("AzureMessagingEventGridNamespacesModelFactory")]
