78c45587e7e873ca13edd75cbf8dba7206eb2dc3
Glenn Harper <glharper@microsoft.com>
2025-05-12 11:00:55 -0700

    [AI] [Projects] Add openai sample using authenticated client

---
+++ sdk/ai/Azure.AI.Projects/tests/Samples/Extensions/Sample_AzureOpenAI.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.Threading.Tasks;
+using Azure.AI.OpenAI;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+using OpenAI.Chat;
+
+namespace Azure.AI.Projects.Tests;
+
+public class Sample_AzureOpenAI : SamplesBase<AIProjectsTestEnvironment>
+{
+    [Test]
+    [SyncOnly]
+    public void AzureOpenAIChatCompletion()
+    {
+        #region Snippet:AzureOpenAISync
+#if SNIPPET
+        var endpoint = System.Environment.GetEnvironmentVariable("PROJECT_ENDPOINT");
+        var modelDeploymentName = System.Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME");
+#else
+        var endpoint = TestEnvironment.PROJECTENDPOINT;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+#endif
+        AIProjectClient projectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+        ChatClient chatClient = projectClient.GetAzureOpenAIChatClient(modelDeploymentName);
+
+        ChatCompletion result = chatClient.CompleteChat("List all the rainbow colors");
+        Console.WriteLine(result.Content[0].Text);
+        #endregion
+    }
+
+    [Test]
+    [AsyncOnly]
+    public async Task AzureOpenAIChatCompletionAsync()
+    {
+        #region Snippet:AzureOpenAIAsync
+#if SNIPPET
+        var endpoint = System.Environment.GetEnvironmentVariable("PROJECT_ENDPOINT");
+        var modelDeploymentName = System.Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME");
+#else
+        var endpoint = TestEnvironment.PROJECTENDPOINT;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+#endif
+        AIProjectClient projectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+        ChatClient chatClient = projectClient.GetAzureOpenAIChatClient(modelDeploymentName);
+
+        ChatCompletion result = await chatClient.CompleteChatAsync("List all the rainbow colors");
+        Console.WriteLine(result.Content[0].Text);
+        #endregion
+    }
+}
