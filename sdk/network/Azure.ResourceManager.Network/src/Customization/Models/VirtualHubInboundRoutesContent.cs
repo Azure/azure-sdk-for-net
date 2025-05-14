da0ae02ae475a3886daa8de304f1fc87404baee0
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2025-05-14 16:08:29 +0800

    Fix issue #49779

---
+++ sdk/network/Azure.ResourceManager.Network/src/Customization/Models/VirtualHubInboundRoutesContent.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Azure.Core;
+
+namespace Azure.ResourceManager.Network.Models
+{
+    // This is the fix for issue #49779
+    [CodeGenSerialization(nameof(ResourceUri), SerializationValueHook = nameof(WriteResourceUri), DeserializationValueHook = nameof(DeserializeResourceUri))]
+    public partial class VirtualHubInboundRoutesContent
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void WriteResourceUri(Utf8JsonWriter writer, ModelReaderWriterOptions options)
+        {
+            if (ResourceUri.IsAbsoluteUri)
+                writer.WriteStringValue(ResourceUri.AbsoluteUri);
+            else
+                writer.WriteStringValue(ResourceUri.OriginalString);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void DeserializeResourceUri(JsonProperty property, ref Uri keyVaultUri)
+        {
+            if (property.Value.ValueKind != JsonValueKind.Null)
+            {
+                keyVaultUri = String.IsNullOrEmpty(property.Value.GetString()) ? new Uri("", UriKind.Relative) : new Uri(property.Value.GetString());
+            }
+        }
+    }
+}
