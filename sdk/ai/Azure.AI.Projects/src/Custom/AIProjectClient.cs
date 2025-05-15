8623c39f8aa39ccb7a48d4dd06b7753b5a4fcbcd
trangevi <trangevi@microsoft.com>
2025-05-14 18:14:55 -0700

    Add "extension" methods to AIProjectClient
    Signed-off-by: trangevi <trangevi@microsoft.com>

--- sdk/ai/Azure.AI.Projects/src/Custom/AIProjectClient.cs
+++ sdk/ai/Azure.AI.Projects/src/Custom/AIProjectClient.cs
@@ -4,6 +4,8 @@
 using System;
 using System.ClientModel.Primitives;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.AI.Inference;
 using Azure.Core;
 using Azure.Core.Pipeline;
 using Azure.Identity;
@@ -68,6 +70,66 @@ namespace Azure.AI.Projects
         /// Retrieves all connection options.
         /// </summary>
         public override IEnumerable<ClientConnection> GetAllConnections() => _cacheManager.GetAllConnections();
+
+        #nullable enable
+        public ChatCompletionsClient GetChatCompletionsClient()
+        {
+            ChatCompletionsClientKey chatCompletionsClientKey = new();
+            ChatCompletionsClient chatClient = this.Subclients.GetClient(chatCompletionsClientKey, () => CreateChatCompletionsClient());
+            return chatClient;
+        }
+
+        private ChatCompletionsClient CreateChatCompletionsClient()
+        {
+            ClientConnection connection = this.GetConnection(typeof(ChatCompletionsClient).FullName!);
+
+            if (!connection.TryGetLocatorAsUri(out Uri? uri) || uri is null)
+            {
+                throw new InvalidOperationException("Invalid URI.");
+            }
+
+            AzureAIInferenceClientOptions options = new AzureAIInferenceClientOptions();
+            // OverrideApiVersionPolicy overrideApiVersionPolicy = new OverrideApiVersionPolicy(AIProjectClientOptions.ServiceVersion.V2025_05_15_Preview.ToString());
+            OverrideApiVersionPolicy overrideApiVersionPolicy = new OverrideApiVersionPolicy("2024-05-01-preview");
+
+            options.AddPolicy(overrideApiVersionPolicy, HttpPipelinePosition.PerRetry);
+            BearerTokenAuthenticationPolicy tokenPolicy = new BearerTokenAuthenticationPolicy((TokenCredential)connection.Credential!, AuthorizationScopes);
+            options.AddPolicy(tokenPolicy, HttpPipelinePosition.PerRetry);
+
+            return connection.CredentialKind switch
+            {
+                CredentialKind.ApiKeyString => new ChatCompletionsClient(uri, new AzureKeyCredential((string)connection.Credential!)),
+                CredentialKind.TokenCredential => new ChatCompletionsClient(uri, (TokenCredential)connection.Credential!, options),
+                _ => throw new InvalidOperationException($"Unsupported credential kind: {connection.CredentialKind}")
+            };
+        }
+
+        private record ChatCompletionsClientKey();
+
+        private class OverrideApiVersionPolicy : HttpPipelinePolicy
+        {
+            private string ApiVersion { get; }
+
+            public OverrideApiVersionPolicy(string apiVersion)
+            {
+                ApiVersion = apiVersion;
+            }
+
+            public override void Process(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+            {
+                message.Request.Uri.Query = $"?api-version={ApiVersion}";
+                ProcessNext(message, pipeline);
+            }
+
+            public override ValueTask ProcessAsync(HttpMessage message, ReadOnlyMemory<HttpPipelinePolicy> pipeline)
+            {
+                message.Request.Uri.Query = $"?api-version={ApiVersion}";
+                var task = ProcessNextAsync(message, pipeline);
+
+                return task;
+            }
+        }
+#nullable disable
 
         private static TokenCredential BuildCredential(TokenCredential credential)
         {
