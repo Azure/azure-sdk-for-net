874c963879c9a0de971a8fda564282c4ec0e2bfd
Krzysztof Cwalina <kcwalina@microsoft.com>
2025-05-15 14:35:40 -0700

    trying extension properties

---
+++ sdk/openai/Azure.AI.OpenAI/src/Custom/Chat/AzureChatPropertyExtensions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#pragma warning disable AZC0112
+
+namespace Azure.AI.OpenAI.Chat;
+
+public static class AzureChatPropertyExtensions
+{
+    extension(ChatCompletionOptions source)
+    {
+        public IReadOnlyList<ChatDataSource> DataSources => source.GetDataSources();
+    }
+}
