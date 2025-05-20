81c298084316967b8d0f16db5b86f83c75337eab
Arcturus Zhang <dapzhang@microsoft.com>
2025-05-20 17:38:50 +0800

    initial commit

---
+++ eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Providers/MockableResourceProvider.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Generator.Management.Providers
+{
+    internal class MockableResourceProvider
+    {
+    }
+}
