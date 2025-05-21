e08175772416c59cc687c1e2b77a090102a03364
Nikolay Rovinskiy <30440255+nick863@users.noreply.github.com>
2025-05-20 19:19:17 -0700

    Switch to the preview version; generate code for preview tools. (#50166)
    * Switch to the preview version
    
    * Revert change
    
    * Update changelog and generate code from the main branch

--- sdk/ai/Azure.AI.Agents.Persistent/src/Generated/SharepointGroundingToolParameters.cs
+++ sdk/ai/Azure.AI.Agents.Persistent/src/Generated/SharepointGroundingToolParameters.cs
@@ -10,12 +10,8 @@ using System.Collections.Generic;
 
 namespace Azure.AI.Agents.Persistent
 {
-    /// <summary>
-    /// An abstract representation of an input tool definition that an agent can use.
-    /// Please note <see cref="ToolDefinition"/> is the base class. According to the scenario, a derived class of the base class might need to be assigned here, or this property needs to be casted to one of the possible derived classes.
-    /// The available derived classes include <see cref="AzureAISearchToolDefinition"/>, <see cref="AzureFunctionToolDefinition"/>, <see cref="BingCustomSearchToolDefinition"/>, <see cref="BingGroundingToolDefinition"/>, <see cref="CodeInterpreterToolDefinition"/>, <see cref="ConnectedAgentToolDefinition"/>, <see cref="MicrosoftFabricToolDefinition"/>, <see cref="FileSearchToolDefinition"/>, <see cref="FunctionToolDefinition"/>, <see cref="OpenApiToolDefinition"/> and <see cref="SharepointToolDefinition"/>.
-    /// </summary>
-    public abstract partial class ToolDefinition
+    /// <summary> The sharepoint grounding tool parameters. </summary>
+    public partial class SharepointGroundingToolParameters
     {
         /// <summary>
         /// Keeps track of any properties unknown to the library.
@@ -47,23 +43,30 @@ namespace Azure.AI.Agents.Persistent
         /// </list>
         /// </para>
         /// </summary>
-        private protected IDictionary<string, BinaryData> _serializedAdditionalRawData;
+        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
 
-        /// <summary> Initializes a new instance of <see cref="ToolDefinition"/>. </summary>
-        protected ToolDefinition()
+        /// <summary> Initializes a new instance of <see cref="SharepointGroundingToolParameters"/>. </summary>
+        public SharepointGroundingToolParameters()
         {
+            ConnectionList = new ChangeTrackingList<ToolConnection>();
         }
 
-        /// <summary> Initializes a new instance of <see cref="ToolDefinition"/>. </summary>
-        /// <param name="type"> The object type. </param>
+        /// <summary> Initializes a new instance of <see cref="SharepointGroundingToolParameters"/>. </summary>
+        /// <param name="connectionList">
+        /// The connections attached to this tool. There can be a maximum of 1 connection
+        /// resource attached to the tool.
+        /// </param>
         /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
-        internal ToolDefinition(string type, IDictionary<string, BinaryData> serializedAdditionalRawData)
+        internal SharepointGroundingToolParameters(IList<ToolConnection> connectionList, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
-            Type = type;
+            ConnectionList = connectionList;
             _serializedAdditionalRawData = serializedAdditionalRawData;
         }
 
-        /// <summary> The object type. </summary>
-        internal string Type { get; set; }
+        /// <summary>
+        /// The connections attached to this tool. There can be a maximum of 1 connection
+        /// resource attached to the tool.
+        /// </summary>
+        public IList<ToolConnection> ConnectionList { get; }
     }
 }
