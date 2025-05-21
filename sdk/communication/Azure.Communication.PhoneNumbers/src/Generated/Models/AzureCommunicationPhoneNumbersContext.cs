b4cc462d781c43ba948cfe09282ae9b1a640d4ca
Sofia Robles Sandoval <sofiar@microsoft.com>
2025-05-21 10:43:00 -0600

    Add AzureCommunicationPhoneNumbersContext with CodeGenSupress

---
+++ sdk/communication/Azure.Communication.PhoneNumbers/src/Generated/Models/AzureCommunicationPhoneNumbersContext.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.Communication.PhoneNumbers
+{
+    /// <summary>
+    /// Context class which will be filled in by the System.ClientModel.SourceGeneration.
+    /// For more information see 'https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/System.ClientModel/src/docs/ModelReaderWriterContext.md'
+    /// </summary>
+    [CodeGenModel("AzureCommunicationPhoneNumbersContext")]
+    internal partial class AzureCommunicationPhoneNumbersContext
+    {
+    }
+}
