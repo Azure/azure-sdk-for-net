30bdf555ed5116e418e2df1f067dce51dc482af8
Wei Hu <huwe@microsoft.com>
2025-05-23 16:24:06 +0800

    bump version of Azure.Generator

--- eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/FooResource.Serialization.cs
+++ eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/FooResource.Serialization.cs
@@ -5,7 +5,9 @@
 
 #nullable disable
 
+using System;
 using System.ClientModel.Primitives;
+using System.Text.Json;
 using MgmtTypeSpec.Models;
 
 namespace MgmtTypeSpec
@@ -16,5 +18,23 @@ namespace MgmtTypeSpec
         private static FooData s_dataDeserializationInstance;
 
         private static FooData DataDeserializationInstance => s_dataDeserializationInstance = new FooData();
+
+        /// <param name="writer"></param>
+        /// <param name="options"></param>
+        void IJsonModel<FooData>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options) => ((IJsonModel<FooData>)Data).Write(writer, options);
+
+        /// <param name="reader"></param>
+        /// <param name="options"></param>
+        FooData IJsonModel<FooData>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options) => ((IJsonModel<FooData>)DataDeserializationInstance).Create(ref reader, options);
+
+        /// <param name="options"></param>
+        BinaryData IPersistableModel<FooData>.Write(ModelReaderWriterOptions options) => ModelReaderWriter.Write<FooData>(Data, options, MgmtTypeSpecContext.Default);
+
+        /// <param name="data"></param>
+        /// <param name="options"></param>
+        FooData IPersistableModel<FooData>.Create(BinaryData data, ModelReaderWriterOptions options) => ModelReaderWriter.Read<FooData>(data, options, MgmtTypeSpecContext.Default);
+
+        /// <param name="options"></param>
+        string IPersistableModel<FooData>.GetFormatFromOptions(ModelReaderWriterOptions options) => ((IPersistableModel<FooData>)DataDeserializationInstance).GetFormatFromOptions(options);
     }
 }
