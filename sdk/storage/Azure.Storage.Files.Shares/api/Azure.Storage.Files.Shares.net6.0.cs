58fe4294cdddd6ad67bb9d79d6541baf167c0f39
Sean McCullough <seanmcc@microsoft.com>
2025-05-27 19:29:34 -0500

    Added STG 100 x-ms-version

--- sdk/storage/Azure.Storage.Files.Shares/api/Azure.Storage.Files.Shares.net6.0.cs
+++ sdk/storage/Azure.Storage.Files.Shares/api/Azure.Storage.Files.Shares.net6.0.cs
@@ -158,6 +158,7 @@ namespace Azure.Storage.Files.Shares
             V2025_05_05 = 26,
             V2025_07_05 = 27,
             V2025_11_05 = 28,
+            V2026_02_06 = 29,
         }
     }
     public partial class ShareDirectoryClient
