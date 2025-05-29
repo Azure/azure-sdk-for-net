ee0a599b0d1fc58c5741feb3bb3e55689e08ebb2
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2025-05-29 20:24:08 +0800

    [http-client-csharp-mgmt] code refactoring (#50205)
    * refine method BuildOperationMethodTryStatement.
    
    * address remaining unresolved review comments from ealier PR.
    
    * remove useless comments.
    
    * fix typo.
    
    * new tspCodeModel.json.
    
    * regen tspCodeModel.json.
    
    * fix rebase issue.
    
    * address review comments.
    
    * fix.
    
    * address code review comments.
    
    * create new class ResourceOperationMethodProvider.
    
    * add two extension methods to CSharpType.
    
    * address new review comments from copilot.
    
    * further refine.
    
    * more refine.
    
    * Apply WrapAsync everywhere possible.
    
    * Replace SkipMethodParameter with ImplicitParameterNames property
    
    * introduce GetAllResourceOperationMethodProvider.
    
    * address new review comments.
    
    * rename GetAllResourceOperationMethodProvider to GetAllOperationMethodProvider
    
    * small refine.
    
    * address review comments.
    
    * add one TODO item inline in the code.

---
+++ eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Providers/OperationMethodProviders/ExistsOperationMethodProvider.cs
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Generator.Management.Utilities;
+using Microsoft.TypeSpec.Generator.Expressions;
+using Microsoft.TypeSpec.Generator.Input;
+using Microsoft.TypeSpec.Generator.Primitives;
+using Microsoft.TypeSpec.Generator.Providers;
+using Microsoft.TypeSpec.Generator.Statements;
+using System.Collections.Generic;
+using static Microsoft.TypeSpec.Generator.Snippets.Snippet;
+
+namespace Azure.Generator.Management.Providers.OperationMethodProviders
+{
+    internal class ExistsOperationMethodProvider(
+        ResourceClientProvider resourceClientProvider,
+        InputServiceMethod method,
+        MethodProvider convenienceMethod,
+        bool isAsync) : ResourceOperationMethodProvider(resourceClientProvider, method, convenienceMethod, isAsync)
+    {
+        protected override MethodSignature CreateSignature()
+        {
+            var returnType = new CSharpType(typeof(Response<>), typeof(bool))
+                .WrapAsync(_isAsync);
+
+            return new MethodSignature(
+                _isAsync ? "ExistsAsync" : "Exists",
+                $"Checks to see if the resource exists in azure.",
+                _convenienceMethod.Signature.Modifiers,
+                returnType,
+                _convenienceMethod.Signature.ReturnDescription,
+                GetOperationMethodParameters(),
+                _convenienceMethod.Signature.Attributes,
+                _convenienceMethod.Signature.GenericArguments,
+                _convenienceMethod.Signature.GenericParameterConstraints,
+                _convenienceMethod.Signature.ExplicitInterface,
+                _convenienceMethod.Signature.NonDocumentComment);
+        }
+
+        protected override IReadOnlyList<MethodBodyStatement> BuildReturnStatements(ValueExpression responseVariable, MethodSignature signature)
+        {
+            // For Exists methods, we check if Value is not null and return a boolean
+            var returnValueExpression = responseVariable.Property("Value").NotEqual(Null);
+
+            return [
+                Return(
+                    Static(typeof(Response)).Invoke(
+                        nameof(Response.FromValue),
+                        returnValueExpression,
+                        responseVariable.Invoke("GetRawResponse")
+                    )
+                )
+            ];
+        }
+    }
+}
