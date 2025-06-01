dfdaa4cd8cb66e1925b726e7bcd30e68399ee865
Dheeraj Raghavendra Madiraju <dmadiraju@microsoft.com>
2025-06-02 00:23:05 +0530

    SDK for WorkloadsVirtualInstance

---
+++ sdk/workloadsvirtualinstance/Azure.ResourceManager.WorkloadsVirtualInstance/tests/WorkloadsVirtualInstanceManagementTestBase.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.TestFramework;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Azure.ResourceManager.WorkloadsVirtualInstance.Tests
+{
+    public class WorkloadsVirtualInstanceManagementTestBase : ManagementRecordedTestBase<WorkloadsVirtualInstanceManagementTestEnvironment>
+    {
+        protected ArmClient Client { get; private set; }
+        protected SubscriptionResource DefaultSubscription { get; private set; }
+
+        protected WorkloadsVirtualInstanceManagementTestBase(bool isAsync, RecordedTestMode mode)
+        : base(isAsync, mode)
+        {
+        }
+
+        protected WorkloadsVirtualInstanceManagementTestBase(bool isAsync)
+            : base(isAsync)
+        {
+        }
+
+        [SetUp]
+        public async Task CreateCommonClient()
+        {
+            Client = GetArmClient();
+            DefaultSubscription = await Client.GetDefaultSubscriptionAsync().ConfigureAwait(false);
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        {
+            string rgName = Recording.GenerateAssetName(rgNamePrefix);
+            ResourceGroupData input = new ResourceGroupData(location);
+            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            return lro.Value;
+        }
+    }
+}
