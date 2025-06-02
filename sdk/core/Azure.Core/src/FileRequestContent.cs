7b893a84f031d72fe2654c3abad92e1c0e6c7c25
Jorge Rangel <jorgerangel@microsoft.com>
2025-06-02 12:26:28 -0500

    add initial azure.core APIs

---
+++ sdk/core/Azure.Core/src/FileRequestContent.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ClientModel.Internal;
+using Azure.Core;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace Azure.Core;
+
+/// <summary>
+/// FileRequestContent
+/// </summary>
+public sealed partial class FileRequestContent : RequestContent
+{
+    private readonly RequestContent _content;
+
+    /// <summary>
+    /// Creates an instance of <see cref="FileRequestContent"/>.
+    /// </summary>
+    /// <param name="data"></param>
+    public FileRequestContent(BinaryData data)
+    {
+        Argument.AssertNotNull(data, nameof(data));
+        _content = Create(data);
+    }
+
+    /// <summary>
+    /// Creates an instance of <see cref="FileRequestContent"/>.
+    /// </summary>
+    public FileRequestContent(Stream stream)
+    {
+        Argument.AssertNotNull(stream, nameof(stream));
+        _content = Create(stream);
+    }
+
+    /// <summary>
+    /// Creates an instance of <see cref="FileRequestContent"/>.
+    /// </summary>
+    public FileRequestContent(string path)
+    {
+        Argument.AssertNotNullOrEmpty(path, nameof(path));
+
+        FileStream fileStream = File.OpenRead(path);
+        _content = Create(fileStream);
+    }
+
+    /// <inheritdoc/>
+    public override string? ContentType { get; set; } = "application/octet-stream";
+
+    /// <summary>
+    /// The name of the file to be used when uploading this content.
+    /// </summary>
+    public string? Filename { get; set; }
+
+    /// <inheritdoc/>
+    public override void Dispose()
+    {
+        _content.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    /// <inheritdoc/>
+    public override bool TryComputeLength(out long length)
+        => _content.TryComputeLength(out length);
+
+    /// <inheritdoc/>
+    public override void WriteTo(Stream stream, CancellationToken cancellationToken = default)
+        => _content.WriteTo(stream, cancellationToken);
+
+    /// <inheritdoc/>
+    public override Task WriteToAsync(Stream stream, CancellationToken cancellationToken = default)
+        => _content.WriteToAsync(stream, cancellationToken);
+}
--- sdk/core/Azure.Core/src/RequestContent.cs
+++ sdk/core/Azure.Core/src/RequestContent.cs
@@ -3,14 +3,21 @@
 
 using System;
 using System.Buffers;
+using System.ClientModel;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net;
 using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core.Buffers;
 using Azure.Core.Serialization;
+using Azure.Core.Pipeline;
 
 namespace Azure.Core
 {
@@ -21,6 +28,11 @@ namespace Azure.Core
     {
         internal const string SerializationRequiresUnreferencedCode = "This method uses reflection-based serialization which is incompatible with trimming. Try using one of the 'Create' overloads that doesn't wrap a serialized version of an object.";
         private static readonly Encoding s_UTF8NoBomEncoding = new UTF8Encoding(false);
+
+        /// <summary>
+        /// The content type of the binary content.
+        /// </summary>
+        public virtual string? ContentType { get; set; }
 
         /// <summary>
         /// Creates an instance of <see cref="RequestContent"/> that wraps a <see cref="Stream"/>.
@@ -345,6 +357,211 @@ namespace Azure.Core
                 _data.WriteTo(stream);
                 return Task.CompletedTask;
             }
+        }
+        private sealed class MultiPartFormDataRequestContent : RequestContent
+        {
+            private readonly MultipartFormDataContent _multipartContent;
+
+            private const int BoundaryLength = 70;
+            private const string BoundaryValues = "0123456789=ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz";
+
+            public MultiPartFormDataRequestContent(IEnumerable<RequestContent> parts)
+                : this(CreateBoundary(), parts) { }
+
+            public MultiPartFormDataRequestContent(string boundary, IEnumerable<RequestContent> parts)
+            {
+                Argument.AssertNotNullOrEmpty(boundary, nameof(boundary));
+                Argument.AssertNotNull(parts, nameof(parts));
+
+                _multipartContent = new MultipartFormDataContent(boundary);
+
+                foreach (RequestContent part in parts)
+                {
+                    if (part is not MultipartFormDataPartRequestContent partRequestContent)
+                    {
+                        throw new InvalidOperationException($"The type is not a valid part: '{part.GetType()}'.");
+                    }
+                    Add(partRequestContent);
+                }
+            }
+
+            public override string? ContentType
+            {
+                get
+                {
+                    Debug.Assert(_multipartContent.Headers.ContentType is not null);
+
+                    return _multipartContent.Headers.ContentType!.ToString();
+                }
+            }
+
+            public void Add(MultipartFormDataPartRequestContent content)
+            {
+                Argument.AssertNotNull(content, nameof(content));
+                Argument.AssertNotNull(content.Name, nameof(content.Name));
+
+                HttpContent httpContent = new HttpContentAdapter(content);
+                if (content.ContentType is not null)
+                {
+                    httpContent.Headers.ContentType = MediaTypeHeaderValue.Parse(content.ContentType);
+                }
+
+                string? filename = content.Content is FileRequestContent fileContent ? fileContent.Filename : null;
+                Add(httpContent, content.Name!, filename);
+            }
+
+            private void Add(HttpContent content, string name, string? fileName = default)
+            {
+                Argument.AssertNotNull(content, nameof(content));
+                Argument.AssertNotNull(name, nameof(name));
+
+                if (fileName is not null)
+                {
+                    _multipartContent.Add(content, name, fileName);
+                }
+                else
+                {
+                    _multipartContent.Add(content, name);
+                }
+            }
+
+#if NET6_0_OR_GREATER
+    private static string CreateBoundary() =>
+        string.Create(BoundaryLength, 0, (chars, _) =>
+        {
+            Span<byte> random = stackalloc byte[BoundaryLength];
+            Random.Shared.NextBytes(random);
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = BoundaryValues[random[i] % BoundaryValues.Length];
+            }
+        });
+#else
+            private static readonly Random _random = new();
+
+            private static string CreateBoundary()
+            {
+                Span<char> chars = stackalloc char[BoundaryLength];
+
+                byte[] random = new byte[BoundaryLength];
+                lock (_random)
+                {
+                    _random.NextBytes(random);
+                }
+                const int Mask = 255 >> 2;
+                Debug.Assert(BoundaryValues.Length - 1 == Mask);
+
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    chars[i] = BoundaryValues[random[i] & Mask];
+                }
+
+                return chars.ToString();
+            }
+#endif
+
+            public override bool TryComputeLength(out long length)
+            {
+                if (_multipartContent.Headers.ContentLength is long contentLength)
+                {
+                    length = contentLength;
+                    return true;
+                }
+
+                length = 0;
+                return false;
+            }
+
+            public override void WriteTo(Stream stream, CancellationToken cancellationToken = default)
+            {
+#if NET5_0_OR_GREATER
+                _multipartContent.CopyTo(stream, default, cancellationToken);
+#else
+#pragma warning disable AZC0107 // DO NOT call public asynchronous method in synchronous scope.
+                _multipartContent.CopyToAsync(stream, default).EnsureCompleted();
+#pragma warning restore AZC0107 // DO NOT call public asynchronous method in synchronous scope.
+#endif
+            }
+
+            public override async Task WriteToAsync(Stream stream, CancellationToken cancellationToken = default)
+            {
+#if NET5_0_OR_GREATER
+                await _multipartContent.CopyToAsync(stream, cancellationToken).ConfigureAwait(false);
+#else
+                await _multipartContent.CopyToAsync(stream).ConfigureAwait(false);
+#endif
+            }
+
+            public override void Dispose()
+            {
+                _multipartContent.Dispose();
+            }
+
+            private sealed class HttpContentAdapter : HttpContent
+            {
+                private readonly RequestContent _content;
+
+                public HttpContentAdapter(RequestContent content)
+                {
+                    Argument.AssertNotNull(content, nameof(content));
+
+                    _content = content;
+                }
+
+                protected override async Task SerializeToStreamAsync(Stream stream, TransportContext? context)
+                    => await _content.WriteToAsync(stream, CancellationToken.None).ConfigureAwait(false);
+
+                protected override bool TryComputeLength(out long length)
+                    => _content.TryComputeLength(out length);
+
+#if NET6_0_OR_GREATER
+                protected override async Task SerializeToStreamAsync(Stream stream, TransportContext? context, CancellationToken cancellationToken)
+                    => await _content!.WriteToAsync(stream, cancellationToken).ConfigureAwait(false);
+
+                protected override void SerializeToStream(Stream stream, TransportContext? context, CancellationToken cancellationToken)
+                    => _content.WriteTo(stream, cancellationToken);
+#endif
+            }
+        }
+
+        private sealed class MultipartFormDataPartRequestContent : RequestContent
+        {
+            private readonly RequestContent _content;
+
+            public MultipartFormDataPartRequestContent(string name, RequestContent content)
+            {
+                Argument.AssertNotNullOrEmpty(name, nameof(name));
+                Argument.AssertNotNull(content, nameof(content));
+
+                _content = content;
+                Name = name;
+            }
+
+            public override string? ContentType
+            {
+                get
+                {
+                    return _content.ContentType;
+                }
+            }
+
+            internal string Name { get; }
+            internal RequestContent Content => _content;
+
+            public override void Dispose()
+            {
+                _content.Dispose();
+                GC.SuppressFinalize(this);
+            }
+
+            public override bool TryComputeLength(out long length)
+                => _content.TryComputeLength(out length);
+
+            public override void WriteTo(Stream stream, CancellationToken cancellationToken = default)
+                => _content.WriteTo(stream, cancellationToken);
+            public override Task WriteToAsync(Stream stream, CancellationToken cancellationToken = default)
+                => _content.WriteToAsync(stream, cancellationToken);
         }
     }
 }
