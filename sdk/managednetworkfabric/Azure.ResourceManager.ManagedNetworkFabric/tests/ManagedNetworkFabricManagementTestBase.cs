9c79b2fc57c91f923a4fcf02ccf3b40f26da5a0a
nafizhaider <nafizhaider@microsoft.com>
2025-06-05 17:06:29 -0400

    one test added not quite working

--- sdk/managednetworkfabric/Azure.ResourceManager.ManagedNetworkFabric/tests/ManagedNetworkFabricManagementTestBase.cs
+++ sdk/managednetworkfabric/Azure.ResourceManager.ManagedNetworkFabric/tests/ManagedNetworkFabricManagementTestBase.cs
@@ -1,11 +1,13 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using Azure.Core;
 using Azure.Core.TestFramework;
+using Azure.Core.TestFramework.Models;
 using Azure.ResourceManager.Resources;
 using Azure.ResourceManager.TestFramework;
 using NUnit.Framework;
+using System;
 using System.Threading.Tasks;
 
 namespace Azure.ResourceManager.ManagedNetworkFabric.Tests
@@ -13,6 +15,7 @@ namespace Azure.ResourceManager.ManagedNetworkFabric.Tests
     public class ManagedNetworkFabricManagementTestBase : ManagementRecordedTestBase<ManagedNetworkFabricManagementTestEnvironment>
     {
         protected ArmClient Client { get; private set; }
+        protected ResourceGroupResource ResourceGroupResource { get; private set; }
         protected SubscriptionResource DefaultSubscription { get; private set; }
 
         protected ManagedNetworkFabricManagementTestBase(bool isAsync, RecordedTestMode mode)
@@ -26,10 +29,19 @@ namespace Azure.ResourceManager.ManagedNetworkFabric.Tests
         }
 
         [SetUp]
-        public async Task CreateCommonClient()
+        public void SetUp()
         {
             Client = GetArmClient();
-            DefaultSubscription = await Client.GetDefaultSubscriptionAsync().ConfigureAwait(false);
+
+            var subscriptionId = SubscriptionResource.CreateResourceIdentifier(TestEnvironment.SubscriptionId);
+            TestContext.Out.WriteLine($"Provided subscription-Id : {subscriptionId.SubscriptionId}");
+
+            string resourceGroupName = TestEnvironment.ResourceGroupName;
+
+            TestContext.Out.WriteLine($"Provided ResourceGroup: {resourceGroupName}");
+            DefaultSubscription = Client.GetSubscriptionResource(subscriptionId);
+            var resourceGroupId = ResourceGroupResource.CreateResourceIdentifier(TestEnvironment.SubscriptionId, TestEnvironment.ResourceGroupName);
+            ResourceGroupResource = Client.GetResourceGroupResource(resourceGroupId);
         }
 
         protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
