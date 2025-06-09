4c7fe3b0e76ce590f72b9b21b5f502b3df34df39
Wei Hu <huwe@microsoft.com>
2025-06-09 10:47:47 +0800

    simplify

--- sdk/core/Azure.Core.Expressions.DataFactory/src/DataFactoryElement.Serialization.cs
+++ sdk/core/Azure.Core.Expressions.DataFactory/src/DataFactoryElement.Serialization.cs
@@ -149,60 +149,8 @@ namespace Azure.Core.Expressions.DataFactory
         {
             switch (value)
             {
-                case null:
-                    writer.WriteNullValue();
-                    break;
                 case IJsonModel<T1> jsonModel:
                     jsonModel.Write(writer, options);
-                    break;
-                case IUtf8JsonSerializable serializable:
-                    serializable.Write(writer);
-                    break;
-                case byte[] bytes:
-                    writer.WriteBase64StringValue(bytes);
-                    break;
-                case BinaryData bytes:
-                    writer.WriteBase64StringValue(bytes);
-                    break;
-                case JsonElement json:
-                    json.WriteTo(writer);
-                    break;
-                case int i:
-                    writer.WriteNumberValue(i);
-                    break;
-                case decimal d:
-                    writer.WriteNumberValue(d);
-                    break;
-                case double d:
-                    if (double.IsNaN(d))
-                    {
-                        writer.WriteStringValue("NaN");
-                    }
-                    else
-                    {
-                        writer.WriteNumberValue(d);
-                    }
-                    break;
-                case float f:
-                    writer.WriteNumberValue(f);
-                    break;
-                case long l:
-                    writer.WriteNumberValue(l);
-                    break;
-                case string s:
-                    writer.WriteStringValue(s);
-                    break;
-                case bool b:
-                    writer.WriteBooleanValue(b);
-                    break;
-                case Guid g:
-                    writer.WriteStringValue(g);
-                    break;
-                case DateTimeOffset dateTimeOffset:
-                    writer.WriteStringValue(dateTimeOffset, "O");
-                    break;
-                case DateTime dateTime:
-                    writer.WriteStringValue(dateTime, "O");
                     break;
                 case IEnumerable<KeyValuePair<string, object>> enumerable:
                     writer.WriteStartObject();
@@ -221,8 +169,23 @@ namespace Azure.Core.Expressions.DataFactory
                     }
                     writer.WriteEndArray();
                     break;
-                case TimeSpan timeSpan:
-                    writer.WriteStringValue(timeSpan, "P");
+                case null:
+                case IUtf8JsonSerializable:
+                case byte[]:
+                case BinaryData:
+                case JsonElement:
+                case int:
+                case decimal:
+                case double:
+                case float:
+                case long:
+                case string:
+                case bool:
+                case Guid:
+                case DateTimeOffset:
+                case DateTime:
+                case TimeSpan:
+                    writer.WriteObjectValue(value!);
                     break;
 
                 default:
