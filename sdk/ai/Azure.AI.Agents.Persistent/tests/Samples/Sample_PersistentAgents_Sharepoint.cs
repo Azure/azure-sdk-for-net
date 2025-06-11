545ad69d4dbb9ed184c816f27d20a4fe887d5867
Glenn Harper <glharper@microsoft.com>
2025-06-11 15:13:33 -0700

    [AI] [Agents] add sharerpoint sample

---
+++ sdk/ai/Azure.AI.Agents.Persistent/tests/Samples/Sample_PersistentAgents_Sharepoint.cs
@@ -0,0 +1,185 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.AI.Agents.Persistent.Tests;
+
+public partial class Sample_PersistentAgents_Sharepoint : SamplesBase<AIAgentsTestEnvironment>
+{
+    [Test]
+    [AsyncOnly]
+    public async Task SharepointExampleAsync()
+    {
+#if SNIPPET
+        var projectEndpoint = System.Environment.GetEnvironmentVariable("PROJECT_ENDPOINT");
+        var modelDeploymentName = System.Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME");
+        var connectionId = System.Environment.GetEnvironmentVariable("AZURE_SHAREPOINT_CONECTION_ID");
+#else
+        var projectEndpoint = TestEnvironment.PROJECT_ENDPOINT;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+        var connectionId = TestEnvironment.SHAREPOINT_CONNECTION_ID;
+#endif
+        PersistentAgentsClient agentClient = new(projectEndpoint, new DefaultAzureCredential());
+        SharepointToolDefinition sharepointTool = new(
+            new SharepointGroundingToolParameters(
+                connectionId
+            )
+        );
+        PersistentAgent agent = await agentClient.Administration.CreateAgentAsync(
+           model: modelDeploymentName,
+           name: "my-agent",
+           instructions: "You are a helpful agent.",
+           tools: [ sharepointTool ]);
+
+        // Create thread for communication
+        PersistentAgentThread thread = await agentClient.Threads.CreateThreadAsync();
+
+        // Create message to thread
+        PersistentThreadMessage message = await agentClient.Messages.CreateMessageAsync(
+            thread.Id,
+            MessageRole.User,
+            "<Your Sharepoint Query Here>");
+
+        // Run the agent
+        ThreadRun run = await agentClient.Runs.CreateRunAsync(thread, agent);
+        do
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(500));
+            run = await agentClient.Runs.GetRunAsync(thread.Id, run.Id);
+        }
+        while (run.Status == RunStatus.Queued
+            || run.Status == RunStatus.InProgress);
+
+        Assert.AreEqual(
+            RunStatus.Completed,
+            run.Status,
+            run.LastError?.Message);
+
+        AsyncPageable<PersistentThreadMessage> messages = agentClient.Messages.GetMessagesAsync(
+            threadId: thread.Id,
+            order: ListSortOrder.Ascending
+        );
+
+        await foreach (PersistentThreadMessage threadMessage in messages)
+        {
+            Console.Write($"{threadMessage.CreatedAt:yyyy-MM-dd HH:mm:ss} - {threadMessage.Role,10}: ");
+            foreach (MessageContent contentItem in threadMessage.ContentItems)
+            {
+                if (contentItem is MessageTextContent textItem)
+                {
+                    string response = textItem.Text;
+                    if (textItem.Annotations != null)
+                    {
+                        foreach (MessageTextAnnotation annotation in textItem.Annotations)
+                        {
+                            if (annotation is MessageTextUriCitationAnnotation uriAnnotation)
+                            {
+                                response = response.Replace(uriAnnotation.Text, $" [{uriAnnotation.UriCitation.Title}]({uriAnnotation.UriCitation.Uri})");
+                            }
+                        }
+                    }
+                    Console.Write($"Agent response: {response}");
+                }
+                else if (contentItem is MessageImageFileContent imageFileItem)
+                {
+                    Console.Write($"<image from ID: {imageFileItem.FileId}");
+                }
+                Console.WriteLine();
+            }
+        }
+        await agentClient.Threads.DeleteThreadAsync(threadId: thread.Id);
+        await agentClient.Administration.DeleteAgentAsync(agentId: agent.Id);
+    }
+
+    [Test]
+    [SyncOnly]
+    public void SharepointExample()
+    {
+#if SNIPPET
+        var projectEndpoint = System.Environment.GetEnvironmentVariable("PROJECT_ENDPOINT");
+        var modelDeploymentName = System.Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME");
+        var connectionId = System.Environment.GetEnvironmentVariable("AZURE_SHAREPOINT_CONECTION_ID");
+#else
+        var projectEndpoint = TestEnvironment.PROJECT_ENDPOINT;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+        var connectionId = TestEnvironment.SHAREPOINT_CONNECTION_ID;
+#endif
+        PersistentAgentsClient agentClient = new(projectEndpoint, new DefaultAzureCredential());
+        SharepointToolDefinition sharepointTool = new(
+            new SharepointGroundingToolParameters(
+                connectionId
+            )
+        );
+        PersistentAgent agent = agentClient.Administration.CreateAgent(
+           model: modelDeploymentName,
+           name: "my-agent",
+           instructions: "You are a helpful agent.",
+           tools: [sharepointTool]);
+
+        // Create thread for communication
+        PersistentAgentThread thread = agentClient.Threads.CreateThread();
+
+        // Create message to thread
+        PersistentThreadMessage message = agentClient.Messages.CreateMessage(
+            thread.Id,
+            MessageRole.User,
+            "How does wikipedia explain Euler's Identity?");
+
+        // Run the agent
+        ThreadRun run = agentClient.Runs.CreateRun(thread, agent);
+        do
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(500));
+            run = agentClient.Runs.GetRun(thread.Id, run.Id);
+        }
+        while (run.Status == RunStatus.Queued
+            || run.Status == RunStatus.InProgress);
+
+        Assert.AreEqual(
+            RunStatus.Completed,
+            run.Status,
+            run.LastError?.Message);
+
+        Pageable<PersistentThreadMessage> messages = agentClient.Messages.GetMessages(
+            threadId: thread.Id,
+            order: ListSortOrder.Ascending
+        );
+
+        foreach (PersistentThreadMessage threadMessage in messages)
+        {
+            Console.Write($"{threadMessage.CreatedAt:yyyy-MM-dd HH:mm:ss} - {threadMessage.Role,10}: ");
+            foreach (MessageContent contentItem in threadMessage.ContentItems)
+            {
+                if (contentItem is MessageTextContent textItem)
+                {
+                    string response = textItem.Text;
+                    if (textItem.Annotations != null)
+                    {
+                        foreach (MessageTextAnnotation annotation in textItem.Annotations)
+                        {
+                            if (annotation is MessageTextUriCitationAnnotation uriAnnotation)
+                            {
+                                response = response.Replace(uriAnnotation.Text, $" [{uriAnnotation.UriCitation.Title}]({uriAnnotation.UriCitation.Uri})");
+                            }
+                        }
+                    }
+                    Console.Write($"Agent response: {response}");
+                }
+                else if (contentItem is MessageImageFileContent imageFileItem)
+                {
+                    Console.Write($"<image from ID: {imageFileItem.FileId}");
+                }
+                Console.WriteLine();
+            }
+        }
+        agentClient.Threads.DeleteThread(threadId: thread.Id);
+        agentClient.Administration.DeleteAgent(agentId: agent.Id);
+    }
+}
