2976108ee6acb196f74b419988528157cdc7c920
Christopher Scott <chriscott@hotmail.com>
2025-06-11 13:01:09 -0500

    ignore CallIntegrationTestAzFunction

--- sdk/identity/Azure.Identity/tests/ManagedIdentityCredentialIntegrationTests.cs
+++ sdk/identity/Azure.Identity/tests/ManagedIdentityCredentialIntegrationTests.cs
@@ -40,6 +40,7 @@ namespace Azure.Identity.Tests
             Assert.AreEqual("Successfully acquired a token from ManagedIdentityCredential", response.Content.ToString(), response.Content.ToString());
         }
 
+        [Ignore("https://github.com/Azure/azure-sdk-for-net/issues/50550")]
         [RecordedTest]
         [SyncOnly]
         // This test leverages the test app found in Azure.Identity\integration\Integration.Identity.Func
