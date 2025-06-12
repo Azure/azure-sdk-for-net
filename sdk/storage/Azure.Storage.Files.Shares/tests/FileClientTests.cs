1eef3898a3a5d7b3283c14fca8be130b4bf774db
Sean McCullough <seanmcc@microsoft.com>
2025-06-12 12:14:38 -0500

    checkpoint

--- sdk/storage/Azure.Storage.Files.Shares/tests/FileClientTests.cs
+++ sdk/storage/Azure.Storage.Files.Shares/tests/FileClientTests.cs
@@ -908,6 +908,66 @@ namespace Azure.Storage.Files.Shares.Tests
         }
 
         [RecordedTest]
+        [ServiceVersion(Min = ShareClientOptions.ServiceVersion.V2026_02_06)]
+        public async Task CreateFile_Data_PrecalculatedChecksum()
+        {
+            // Arrange
+            await using DisposingShare test = await GetTestShareAsync();
+            ShareFileClient fileClient = InstrumentClient(test.Share.GetRootDirectoryClient().GetFileClient(GetNewFileName()));
+
+            byte[] data = GetRandomBuffer(Constants.KB);
+            using Stream stream = new MemoryStream(data);
+            byte[] md5 = MD5.Create().ComputeHash(data);
+
+            ShareFileCreateOptions options = new ShareFileCreateOptions
+            {
+                Content = stream,
+                TransferValidation = new UploadTransferValidationOptions
+                {
+                    ChecksumAlgorithm = StorageChecksumAlgorithm.MD5,
+                    PrecalculatedChecksum = md5
+                }
+            };
+
+            // Act
+            await fileClient.CreateAsync(
+                maxSize: Constants.KB,
+                options: options);
+
+            // Assert
+            // TODO
+        }
+
+        [RecordedTest]
+        [ServiceVersion(Min = ShareClientOptions.ServiceVersion.V2026_02_06)]
+        public async Task CreateFile_Data_AutoChecksumCalculation()
+        {
+            // Arrange
+            await using DisposingShare test = await GetTestShareAsync();
+            ShareFileClient fileClient = InstrumentClient(test.Share.GetRootDirectoryClient().GetFileClient(GetNewFileName()));
+
+            byte[] data = GetRandomBuffer(Constants.KB);
+            using Stream stream = new MemoryStream(data);
+
+            ShareFileCreateOptions options = new ShareFileCreateOptions
+            {
+                Content = stream,
+                TransferValidation = new UploadTransferValidationOptions
+                {
+                    ChecksumAlgorithm = StorageChecksumAlgorithm.MD5,
+                }
+            };
+
+            // Act
+            await fileClient.CreateAsync(
+                maxSize: Constants.KB,
+                options: options);
+
+            // Assert
+            // TODO
+        }
+
+        [RecordedTest]
         public async Task ExistsAsync_Exists()
         {
             // Arrange
