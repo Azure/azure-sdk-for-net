0736e865e97fbe9804efd00e4b1c4cfd16009d4e
Scott Addie <scott.addie@microsoft.com>
2025-06-13 14:43:42 -0500

    Delete AzureApplicationCredential

--- sdk/identity/Azure.Identity/src/Credentials/AzureApplicationCredentialOptions.cs
+++ sdk/identity/Azure.Identity/src/Credentials/AzureApplicationCredentialOptions.cs
@@ -5,19 +5,4 @@ using System;
 
 namespace Azure.Identity
 {
-    /// <summary>
-    /// Options to configure the <see cref="AzureApplicationCredential"/> authentication flow and requests made to Azure Identity services.
-    /// </summary>
-    internal class AzureApplicationCredentialOptions : TokenCredentialOptions
-    {
-        /// <summary>
-        /// Specifies the client id of the azure ManagedIdentity in the case of user assigned identity.
-        /// </summary>
-        public ManagedIdentityId ManagedIdentityId { get; set; } = GetManagedIdentityIdFromEnvironment(EnvironmentVariables.ClientId);
-
-        private static ManagedIdentityId GetManagedIdentityIdFromEnvironment(string clientId)
-        {
-            return !string.IsNullOrEmpty(clientId) ? ManagedIdentityId.FromUserAssignedClientId(clientId) : ManagedIdentityId.SystemAssigned;
-        }
-    }
 }
