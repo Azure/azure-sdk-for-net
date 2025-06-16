362d5f21297c5fc51aa76cc0b4a4248ccf21a018
Qingquan Zhao <qingquanzhao@microsoft.com>
2025-06-16 15:32:22 -0700

    Manually update the customization for smtp

---
+++ sdk/grafana/Azure.ResourceManager.Grafana/src/Customization/ManagedGrafanaProperties.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.ResourceManager.Grafana.Models
+{
+    /// <summary>
+    /// A class representing the ManagedGrafanaProperties data model.
+    /// </summary>
+    public partial class ManagedGrafanaProperties
+    {
+        /// <summary>
+        /// Email server settings.
+        /// https://grafana.com/docs/grafana/v9.0/setup-grafana/configure-grafana/#smtp
+        /// </summary>
+        public Smtp GrafanaConfigurationsSmtp
+        {
+            get => GrafanaConfigurations is null ? default : GrafanaConfigurations.Smtp;
+            set
+            {
+                if (GrafanaConfigurations is null)
+                    GrafanaConfigurations = new GrafanaConfigurations();
+                GrafanaConfigurations.Smtp = value;
+            }
+        }
+    }
+}
