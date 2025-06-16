633f1381252e9976101101ca53f808b9f97edade
CJ Ochs <111904639+CarlOchs@users.noreply.github.com>
2025-06-16 04:29:19 -0400

    Update for 2025-06-01 stable API release. (#50580)

--- sdk/cognitiveservices/Azure.ResourceManager.CognitiveServices/src/Generated/Models/AadAuthTypeConnectionProperties.cs
+++ sdk/cognitiveservices/Azure.ResourceManager.CognitiveServices/src/Generated/Models/AadAuthTypeConnectionProperties.cs
@@ -24,15 +24,15 @@ namespace Azure.ResourceManager.CognitiveServices.Models
         /// <param name="authType"> Authentication type of the connection target. </param>
         /// <param name="category"> Category of the connection. </param>
         /// <param name="createdByWorkspaceArmId"></param>
-        /// <param name="error"></param>
+        /// <param name="error"> Provides the error message if the connection fails. </param>
         /// <param name="expiryOn"></param>
         /// <param name="group"> Group based on connection category. </param>
         /// <param name="isSharedToAll"></param>
         /// <param name="metadata"> Store user metadata for this connection. </param>
-        /// <param name="peRequirement"></param>
-        /// <param name="peStatus"></param>
+        /// <param name="peRequirement"> Specifies how private endpoints are used with this connection: 'Required', 'NotRequired', or 'NotApplicable'. </param>
+        /// <param name="peStatus"> Specifies the status of private endpoints for this connection: 'Inactive', 'Active', or 'NotApplicable'. </param>
         /// <param name="sharedUserList"></param>
-        /// <param name="target"></param>
+        /// <param name="target"> The connection URL to be used. </param>
         /// <param name="useWorkspaceManagedIdentity"></param>
         /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
         internal AadAuthTypeConnectionProperties(ConnectionAuthType authType, CognitiveServicesConnectionCategory? category, ResourceIdentifier createdByWorkspaceArmId, string error, DateTimeOffset? expiryOn, CognitiveServicesConnectionGroup? group, bool? isSharedToAll, IDictionary<string, string> metadata, ManagedPERequirement? peRequirement, ManagedPEStatus? peStatus, IList<string> sharedUserList, string target, bool? useWorkspaceManagedIdentity, IDictionary<string, BinaryData> serializedAdditionalRawData) : base(authType, category, createdByWorkspaceArmId, error, expiryOn, group, isSharedToAll, metadata, peRequirement, peStatus, sharedUserList, target, useWorkspaceManagedIdentity, serializedAdditionalRawData)
