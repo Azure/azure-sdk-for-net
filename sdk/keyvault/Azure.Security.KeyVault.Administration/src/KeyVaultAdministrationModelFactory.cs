33fc035d6f7156812f0c4873f87bd77ce460deef
jolov <jolov@microsoft.com>
2025-06-25 16:21:25 -0700

    add back model factory method

--- sdk/keyvault/Azure.Security.KeyVault.Administration/src/KeyVaultAdministrationModelFactory.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Administration/src/KeyVaultAdministrationModelFactory.cs
@@ -151,5 +151,15 @@ namespace Azure.Security.KeyVault.Administration
                     new Dictionary<string, BinaryData>()),
                 new Dictionary<string, BinaryData>());
         }
+
+        /// <summary> Initializes a new instance of <see cref="Administration.KeyVaultSetting"/>. </summary>
+        /// <param name="name"> The account setting to be updated. </param>
+        /// <param name="content"> The value of the pool setting. </param>
+        /// <param name="settingType"> The type specifier of the value. </param>
+        /// <returns> A new <see cref="Administration.KeyVaultSetting"/> instance for mocking. </returns>
+        public static KeyVaultSetting KeyVaultSetting(string name = null, string content = null, KeyVaultSettingType? settingType = null)
+        {
+            return new KeyVaultSetting(name, settingType, content, new Dictionary<string, BinaryData>());
+        }
     }
 }
