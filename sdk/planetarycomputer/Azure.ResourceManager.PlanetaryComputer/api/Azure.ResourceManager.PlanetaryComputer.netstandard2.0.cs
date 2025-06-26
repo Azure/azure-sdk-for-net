050ae1f201111b2b2f00064923ee581fdf5f0f92
shaktichetan23 <shakrao@microsoft.com>
2025-06-26 09:37:35 -0700

    Update API surface files after code generation

--- sdk/planetarycomputer/Azure.ResourceManager.PlanetaryComputer/api/Azure.ResourceManager.PlanetaryComputer.netstandard2.0.cs
+++ sdk/planetarycomputer/Azure.ResourceManager.PlanetaryComputer/api/Azure.ResourceManager.PlanetaryComputer.netstandard2.0.cs
@@ -1,5 +1,11 @@
 namespace Azure.ResourceManager.PlanetaryComputer
 {
+    public partial class AzureResourceManagerPlanetaryComputerContext : System.ClientModel.Primitives.ModelReaderWriterContext
+    {
+        internal AzureResourceManagerPlanetaryComputerContext() { }
+        public static Azure.ResourceManager.PlanetaryComputer.AzureResourceManagerPlanetaryComputerContext Default { get { throw null; } }
+        protected override bool TryGetTypeBuilderCore(System.Type type, out System.ClientModel.Primitives.ModelReaderWriterTypeBuilder builder) { throw null; }
+    }
     public partial class GeoCatalogCollection : Azure.ResourceManager.ArmCollection, System.Collections.Generic.IAsyncEnumerable<Azure.ResourceManager.PlanetaryComputer.GeoCatalogResource>, System.Collections.Generic.IEnumerable<Azure.ResourceManager.PlanetaryComputer.GeoCatalogResource>, System.Collections.IEnumerable
     {
         protected GeoCatalogCollection() { }
