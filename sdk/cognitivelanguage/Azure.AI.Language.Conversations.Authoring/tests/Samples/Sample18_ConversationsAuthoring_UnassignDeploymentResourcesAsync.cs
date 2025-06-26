0c6319bb56f4b20779ba62023db6a2ac959fbeea
Xiong Chen <v-xiongchen@microsoft.com>
2025-06-25 17:32:31 -0700

    add test samples for unassignDeploymentResource and getUnassignDeploymentResourceStatus

---
+++ sdk/cognitivelanguage/Azure.AI.Language.Conversations.Authoring/tests/Samples/Sample18_ConversationsAuthoring_UnassignDeploymentResourcesAsync.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.Language.Conversations.Authoring;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.AI.Language.Conversations.Authoring.Tests.Samples
+{
+    public partial class Sample18_ConversationsAuthoring_UnassignDeploymentResourcesAsync : SamplesBase<AuthoringClientTestEnvironment>
+    {
+        [Test]
+        [AsyncOnly]
+        public async Task UnassignDeploymentResourcesAsync()
+        {
+            Uri endpoint = TestEnvironment.Endpoint;
+            DefaultAzureCredential credential = new DefaultAzureCredential();
+            ConversationAnalysisAuthoringClientOptions options =
+                new ConversationAnalysisAuthoringClientOptions(ConversationAnalysisAuthoringClientOptions.ServiceVersion.V2025_05_15_Preview);
+
+            ConversationAnalysisAuthoringClient client = new ConversationAnalysisAuthoringClient(endpoint, credential, options);
+
+            #region Snippet:Sample18_ConversationsAuthoring_UnassignDeploymentResourcesAsync
+            // Set project name and create client for the project
+            string projectName = "EmailApp";
+            ConversationAuthoringProject projectClient = client.GetProject(projectName);
+
+            // Define assigned resource ID to be unassigned
+            var assignedResourceIds = new List<string>
+            {
+                "/subscriptions/b72743ec-8bb3-453f-83ad-a53e8a50712e/resourceGroups/language-sdk-rg/providers/Microsoft.CognitiveServices/accounts/sdk-test-02"
+            };
+
+            // Build the unassignment details
+            var unassignDetails = new ConversationAuthoringUnassignDeploymentResourcesDetails(assignedResourceIds);
+
+            // Call the operation
+            Operation operation = await projectClient.UnassignDeploymentResourcesAsync(
+                waitUntil: WaitUntil.Started,
+                details: unassignDetails
+            );
+
+            Console.WriteLine($"UnassignDeploymentResourcesAsync initiated. Status: {operation.GetRawResponse().Status}");
+
+            // Print jobId from Operation-Location
+            if (operation.GetRawResponse().Headers.TryGetValue("Operation-Location", out string location))
+            {
+                string jobId = new Uri(location).Segments.Last().Split('?')[0];
+                Console.WriteLine($"Job ID: {jobId}");
+            }
+            else
+            {
+                Console.WriteLine("Operation-Location header not found.");
+            }
+            #endregion
+        }
+    }
+}
---
+++ sdk/cognitivelanguage/Azure.AI.Language.Conversations.Authoring/tests/Samples/Sample19_ConversationsAuthoring_GetUnassignDeploymentResourcesStatusAsync.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.Language.Conversations.Authoring;
+using Azure.Core;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.AI.Language.Conversations.Authoring.Tests.Samples
+{
+    public partial class Sample19_ConversationsAuthoring_GetUnassignDeploymentResourcesStatusAsync : SamplesBase<AuthoringClientTestEnvironment>
+    {
+        [Test]
+        [AsyncOnly]
+        public async Task GetUnassignDeploymentResourcesStatusAsync()
+        {
+            Uri endpoint = TestEnvironment.Endpoint;
+            DefaultAzureCredential credential = new DefaultAzureCredential();
+            ConversationAnalysisAuthoringClientOptions options =
+                new ConversationAnalysisAuthoringClientOptions(ConversationAnalysisAuthoringClientOptions.ServiceVersion.V2025_05_15_Preview);
+
+            ConversationAnalysisAuthoringClient client = new ConversationAnalysisAuthoringClient(endpoint, credential, options);
+
+            #region Snippet:Sample19_ConversationsAuthoring_GetUnassignDeploymentResourcesStatusAsync
+            // Set project name and create client for the project
+            string projectName = "EmailApp";
+            ConversationAuthoringProject projectClient = client.GetProject(projectName);
+
+            // Replace with your actual job ID retrieved from the unassign operation
+            string jobId = "your-job-id-here";
+
+            // Call the API to get unassign job status
+            Response<ConversationAuthoringDeploymentResourcesState> response =
+                await projectClient.GetUnassignDeploymentResourcesStatusAsync(jobId);
+
+            Console.WriteLine($"Job Status: {response.Value.Status}");
+
+            if (response.Value.Errors != null && response.Value.Errors.Any())
+            {
+                Console.WriteLine("Errors:");
+                foreach (var error in response.Value.Errors)
+                {
+                    Console.WriteLine($"- Code: {error.Code}, Message: {error.Message}");
+                }
+            }
+            #endregion
+        }
+    }
+}
