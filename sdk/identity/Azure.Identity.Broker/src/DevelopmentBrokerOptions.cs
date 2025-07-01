9811c89bc91f94e50321287d76825208f75a6f22
Christopher Scott <chriscott@hotmail.com>
2025-07-01 13:54:28 -0500

    wip

--- sdk/identity/Azure.Identity.Broker/src/DevelopmentBrokerOptions.cs
+++ sdk/identity/Azure.Identity.Broker/src/DevelopmentBrokerOptions.cs
@@ -44,7 +44,7 @@ namespace Azure.Identity.Broker
         private void AddBroker(PublicClientApplicationBuilder builder)
         {
             builder.WithParentActivityOrWindow(() => IntPtr.Zero);
-            var options = new BrokerOptions(BrokerOptions.OperatingSystems.Windows | BrokerOptions.OperatingSystems.Linux);
+            var options = new BrokerOptions(BrokerOptions.OperatingSystems.Windows | BrokerOptions.OperatingSystems.Linux | BrokerOptions.OperatingSystems.OSX);
             if (IsLegacyMsaPassthroughEnabled.HasValue)
             {
                 options.MsaPassthrough = IsLegacyMsaPassthroughEnabled.Value;
