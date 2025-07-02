c7cccf34d695b07ce66ebfaff9cb81d1218ea121
Ana Arias Rosales <anaarias@microsoft.com>
2025-07-01 21:53:32 -0400

    update tests

--- sdk/avs/Azure.ResourceManager.Avs/tests/AvsManagementTestBase.cs
+++ sdk/avs/Azure.ResourceManager.Avs/tests/AvsManagementTestBase.cs
@@ -26,14 +26,12 @@ namespace Azure.ResourceManager.Avs.Tests
         protected AzureLocation DefaultLocation => AzureLocation.EastUS;
 
         public const string RESOURCE_GROUP_NAME = "avs-dotnet-test";
-        public const string PRIVATE_CLOUD_NAME = "avs-dotnet-test-w2";
+        public const string PRIVATE_CLOUD_NAME = "avs-dotnet-test-9318";
         public const string CLUSTER1_NAME = "Cluster-1";
         public const string CLUSTER2_NAME = "Cluster-2";
         public const string ISCSI_PATH_NAME = "default";
         public const string WORKLOAD_NETWORK_NAME = "03335585-9574-4d44-a001-818ca9a9c282";
-        public const string HOST_ID = "esx14-r04.p02.westus2.avs.azure.com";
-        public const string PROVISIONED_NETWORK_NAME = "vsan";
-        public const string STORAGE_POLICY_NAME = "storagePolicy1";
+        public const string HOST_ID = "esx20-r16.p04.eastus.avs.azure.com";
         protected AvsManagementTestBase(bool isAsync, RecordedTestMode mode)
         : base(isAsync, mode)
         {
@@ -110,36 +108,6 @@ namespace Azure.ResourceManager.Avs.Tests
             ResourceIdentifier avsPrivateCloudClusterResourceId = AvsPrivateCloudClusterResource.CreateResourceIdentifier(subscriptionId, RESOURCE_GROUP_NAME, PRIVATE_CLOUD_NAME, CLUSTER1_NAME);
             AvsPrivateCloudClusterResource avsPrivateCloudCluster = Client.GetAvsPrivateCloudClusterResource(avsPrivateCloudClusterResourceId);
             AvsHostCollection collection = avsPrivateCloudCluster.GetAvsHosts();
-            return collection;
-        }
-        protected AvsProvisionedNetworkResource getAvsProvisionedNetworksResource()
-        {
-            string subscriptionId = DefaultSubscription.Data.SubscriptionId;
-            ResourceIdentifier avsProvisionedNetworkResourceId = AvsProvisionedNetworkResource.CreateResourceIdentifier(subscriptionId, RESOURCE_GROUP_NAME, PRIVATE_CLOUD_NAME, PROVISIONED_NETWORK_NAME);
-            AvsProvisionedNetworkResource avsProvisionedNetwork = Client.GetAvsProvisionedNetworkResource(avsProvisionedNetworkResourceId);
-            return avsProvisionedNetwork;
-        }
-        protected AvsProvisionedNetworkCollection getAvsProvisionedNetworksCollection()
-        {
-            string subscriptionId = DefaultSubscription.Data.SubscriptionId;
-            ResourceIdentifier avsPrivateCloudResourceId = AvsPrivateCloudResource.CreateResourceIdentifier(subscriptionId, RESOURCE_GROUP_NAME, PRIVATE_CLOUD_NAME);
-            AvsPrivateCloudResource avsPrivateCloud = Client.GetAvsPrivateCloudResource(avsPrivateCloudResourceId);
-            AvsProvisionedNetworkCollection collection = avsPrivateCloud.GetAvsProvisionedNetworks();
-            return collection;
-        }
-        protected AvsPureStoragePolicyResource getAvsPureStoragePolicyResource()
-        {
-            string subscriptionId = DefaultSubscription.Data.SubscriptionId;
-            ResourceIdentifier avsPureStoragePolicyResourceId = AvsPureStoragePolicyResource.CreateResourceIdentifier(subscriptionId, RESOURCE_GROUP_NAME, PRIVATE_CLOUD_NAME, STORAGE_POLICY_NAME);
-            AvsPureStoragePolicyResource avsPureStoragePolicy = Client.GetAvsPureStoragePolicyResource(avsPureStoragePolicyResourceId);
-            return avsPureStoragePolicy;
-        }
-        protected AvsPureStoragePolicyCollection getAvsPureStoragePolicyCollection()
-        {
-            string subscriptionId = DefaultSubscription.Data.SubscriptionId;
-            ResourceIdentifier avsPrivateCloudResourceId = AvsPrivateCloudResource.CreateResourceIdentifier(subscriptionId, RESOURCE_GROUP_NAME, PRIVATE_CLOUD_NAME);
-            AvsPrivateCloudResource avsPrivateCloud = Client.GetAvsPrivateCloudResource(avsPrivateCloudResourceId);
-            AvsPureStoragePolicyCollection collection = avsPrivateCloud.GetAvsPureStoragePolicies();
             return collection;
         }
     }
