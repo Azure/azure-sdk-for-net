03b9d9992d9bbb8fb2eeeb14300c019f47c6058a
Wes Haggard <weshaggard@users.noreply.github.com>
2025-07-02 13:30:22 -0700

    Update eng/scripts/docs/tests/Toc-Generation.Tests.ps1

--- eng/scripts/docs/tests/Toc-Generation.Tests.ps1
+++ eng/scripts/docs/tests/Toc-Generation.Tests.ps1
@@ -6,7 +6,7 @@ Invoke-Pester -Output Detailed $PSScriptRoot\Toc-Generation.Tests.ps1
 
 Import-Module Pester
 
-if (!(Get-Command nuget -ErrorAction SilentlyContinue))) {
+if (!(Get-Command nuget -ErrorAction SilentlyContinue)) {
     Write-Host "Skipping tests because 'nuget' is not on the path"
     exit 0
 }
