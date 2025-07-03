ea710044c93b9c4fc7d32aa39c34c4016739483f
Wei Hu <huwe@microsoft.com>
2025-07-03 18:22:00 +0800

    apply IAzureResponseWithTypeApi

---
+++ eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Providers/Abstraction/ManagementResponseWithTypeProvider.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Generator.Providers.Abstraction;
+using Microsoft.TypeSpec.Generator.Expressions;
+using Microsoft.TypeSpec.Generator.Primitives;
+using static Microsoft.TypeSpec.Generator.Snippets.Snippet;
+
+namespace Azure.Generator.Management.Providers.Abstraction
+{
+    internal record ManagementResponseWithTypeProvider : AzureResponseWIthTypeApi
+    {
+        private static ManagementResponseWithTypeProvider? _instance;
+        internal static ManagementResponseWithTypeProvider Instance => _instance ??= new ManagementResponseWithTypeProvider(Empty);
+
+        public ManagementResponseWithTypeProvider(ValueExpression value) : base(typeof(ManagementResponseWithTypeProvider), value)
+        {
+        }
+
+        public override ValueExpression CastToType(CSharpType type)
+            => Static(type).Invoke(SerializationVisitor.FromResponseMethodName, [Original]);
+
+        public override AzureResponseWIthTypeApi FromExpression(ValueExpression original)
+            => new ManagementResponseWithTypeProvider(original);
+
+        public override AzureResponseWIthTypeApi ToExpression() => this;
+    }
+}
