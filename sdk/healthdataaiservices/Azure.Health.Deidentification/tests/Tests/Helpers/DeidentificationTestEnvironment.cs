067c8fcdac7c5f38412e586b12971ccdcd42670f
Josiah Vinson <jovinson@microsoft.com>
2025-07-09 12:26:13 -0700

    Updating test environment variables

--- sdk/healthdataaiservices/Azure.Health.Deidentification/tests/Tests/Helpers/DeidentificationTestEnvironment.cs
+++ sdk/healthdataaiservices/Azure.Health.Deidentification/tests/Tests/Helpers/DeidentificationTestEnvironment.cs
@@ -7,9 +7,7 @@ namespace Azure.Health.Deidentification.Tests
 {
     public class DeidentificationTestEnvironment : TestEnvironment
     {
-        public string Endpoint => GetRecordedVariable("DEID_SERVICE_ENDPOINT");
-        public string SasUri => GetRecordedVariable("SAS_URI", options => options.IsSecret());
-
+        public string Endpoint => GetRecordedVariable("HEALTHDATAAISERVICES_DEID_SERVICE_ENDPOINT");
         public static string FakeNextLink => "https://localhost:5020/jobs/net-sdk-job-1234/documents?api-version=2024-11-15&maxpagesize=2&continuationToken=1234";
         public static string FakeStorageLocation => "https://fakeblobstorage.blob.core.windows.net/container";
         public static string FakeJobName => "net-sdk-job-1234";
@@ -17,16 +15,7 @@ namespace Azure.Health.Deidentification.Tests
 
         public string GetStorageAccountLocation()
         {
-            if (!string.IsNullOrEmpty(SasUri))
-            {
-                // In playback mode, the SAS URI is sanitized to avoid sharing secrets.
-                if (string.Equals(SasUri, "Sanitized", System.StringComparison.OrdinalIgnoreCase))
-                {
-                    return FakeStorageLocation;
-                }
-                return SasUri;
-            }
-            return $"https://{GetRecordedVariable("STORAGE_ACCOUNT_NAME")}.blob.core.windows.net/{GetRecordedVariable("STORAGE_CONTAINER_NAME")}";
+            return $"https://{GetRecordedVariable("HEALTHDATAAISERVICES_STORAGE_ACCOUNT_NAME")}.blob.core.windows.net/{GetRecordedVariable("HEALTHDATAAISERVICES_STORAGE_CONTAINER_NAME")}";
         }
     }
 }
