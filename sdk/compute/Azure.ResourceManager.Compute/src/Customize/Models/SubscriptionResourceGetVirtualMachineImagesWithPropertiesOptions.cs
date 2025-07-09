4d07798298f5304c8968da94a6d7ccf50ac96170
Theodore Chang <thchan@microsoft.com>
2025-07-09 18:43:27 -0400

    remove suppresion and add customization for brekaing change mitigation

---
+++ sdk/compute/Azure.ResourceManager.Compute/src/Customize/Models/SubscriptionResourceGetVirtualMachineImagesWithPropertiesOptions.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+using System;
+using System.ComponentModel;
+using Azure.Core;
+namespace Azure.ResourceManager.Compute.Models
+{
+    /// <summary> The SubscriptionResourceGetVirtualMachineImagesWithPropertiesOptions. </summary>
+    public partial class SubscriptionResourceGetVirtualMachineImagesWithPropertiesOptions
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public SubscriptionResourceGetVirtualMachineImagesWithPropertiesOptions(AzureLocation location, string publisherName, string offer, string skus, GetVirtualMachineImagesWithPropertiesExpand expand)
+        {
+            Argument.AssertNotNull(publisherName, nameof(publisherName));
+            Argument.AssertNotNull(offer, nameof(offer));
+            Argument.AssertNotNull(skus, nameof(skus));
+            Argument.AssertNotNull(expand, nameof(expand));
+
+            string expandValue = expand.ToString();
+
+            Location = location;
+            PublisherName = publisherName;
+            Offer = offer;
+            Skus = skus;
+            Expand = expandValue;
+        }
+    }
+}
