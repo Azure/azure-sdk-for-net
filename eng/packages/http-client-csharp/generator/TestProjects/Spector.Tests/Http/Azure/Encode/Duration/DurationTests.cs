10ab18a0c9b3c8d832865df661749883eb8c158b
jolov <jolov@microsoft.com>
2025-07-11 19:23:39 -0700

    fix

--- eng/packages/http-client-csharp/generator/TestProjects/Spector.Tests/Http/Azure/Encode/Duration/DurationTests.cs
+++ eng/packages/http-client-csharp/generator/TestProjects/Spector.Tests/Http/Azure/Encode/Duration/DurationTests.cs
@@ -13,7 +13,7 @@ namespace TestProjects.Spector.Tests.Http.Azure.Encode.Duration
         [SpectorTest]
         public Task Azure_Encode_Duration() => Test(async (host) =>
         {
-            var request = new DurationModel(new TimeSpan(1, 2, 58, 59, 500));
+            var request = new DurationModel(new TimeSpan(1, 2, 59, 59, 500));
             var response = await new DurationClient(host, null).DurationConstantAsync(request);
             Assert.AreEqual(204, response.Status);
         });
