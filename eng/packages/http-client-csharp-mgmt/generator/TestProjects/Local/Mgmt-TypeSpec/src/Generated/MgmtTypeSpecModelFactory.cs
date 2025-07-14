78a5d95ad4805e39cd06da546546de21c593a68b
Wei Hu <live1206@gmail.com>
2025-07-14 12:38:59 +0800

    Remove unexpected methods from ModelFactory for management (#51205)
    * Add ModelFacotryVisitor to omit methods for mgmt
    
    * fix for safe flatten
    
    * Update modelFactory for safe-flatten
    
    * Address comments

--- eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/MgmtTypeSpecModelFactory.cs
+++ eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/MgmtTypeSpecModelFactory.cs
@@ -45,24 +45,6 @@ namespace MgmtTypeSpec.Models
                 extendedLocation);
         }
 
-        /// <summary> The FooProperties. </summary>
-        /// <param name="serviceUri"> the service url. </param>
-        /// <param name="something"> something. </param>
-        /// <param name="boolValue"> boolean value. </param>
-        /// <param name="floatValue"> float value. </param>
-        /// <param name="doubleValue"> double value. </param>
-        /// <returns> A new <see cref="Models.FooProperties"/> instance for mocking. </returns>
-        public static FooProperties FooProperties(Uri serviceUri = default, string something = default, bool? boolValue = default, float? floatValue = default, double? doubleValue = default)
-        {
-            return new FooProperties(
-                serviceUri,
-                something,
-                boolValue,
-                floatValue,
-                doubleValue,
-                additionalBinaryDataProperties: null);
-        }
-
         /// <summary> Concrete proxy resource types can be created by aliasing this type using a specific property type. </summary>
         /// <param name="id"> Fully qualified resource ID for the resource. Ex - /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}. </param>
         /// <param name="name"> The name of the resource. </param>
@@ -90,22 +72,6 @@ namespace MgmtTypeSpec.Models
             return new FooSettingsProperties(accessControlEnabled, provisioningState, additionalBinaryDataProperties: null);
         }
 
-        /// <summary> The type used for update operations of the FooSettings. </summary>
-        /// <param name="properties"> The resource-specific properties for this resource. </param>
-        /// <returns> A new <see cref="Models.FooSettingsPatch"/> instance for mocking. </returns>
-        public static FooSettingsPatch FooSettingsPatch(FooSettingsUpdateProperties properties = default)
-        {
-            return new FooSettingsPatch(properties, additionalBinaryDataProperties: null);
-        }
-
-        /// <summary> The updatable properties of the FooSettings. </summary>
-        /// <param name="accessControlEnabled"></param>
-        /// <returns> A new <see cref="Models.FooSettingsUpdateProperties"/> instance for mocking. </returns>
-        public static FooSettingsUpdateProperties FooSettingsUpdateProperties(bool? accessControlEnabled = default)
-        {
-            return new FooSettingsUpdateProperties(accessControlEnabled, additionalBinaryDataProperties: null);
-        }
-
         /// <summary> Concrete tracked resource types can be created by aliasing this type using a specific property type. </summary>
         /// <param name="id"> Fully qualified resource ID for the resource. Ex - /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}. </param>
         /// <param name="name"> The name of the resource. </param>
@@ -130,32 +96,13 @@ namespace MgmtTypeSpec.Models
                 properties);
         }
 
-        /// <summary> The BarProperties. </summary>
-        /// <param name="serviceUri"> the service url. </param>
-        /// <param name="something"> something. </param>
-        /// <param name="boolValue"> boolean value. </param>
-        /// <param name="floatValue"> float value. </param>
-        /// <param name="doubleValue"> double value. </param>
-        /// <returns> A new <see cref="Models.BarProperties"/> instance for mocking. </returns>
-        public static BarProperties BarProperties(Uri serviceUri = default, string something = default, bool? boolValue = default, float? floatValue = default, double? doubleValue = default)
-        {
-            return new BarProperties(
-                serviceUri,
-                something,
-                boolValue,
-                floatValue,
-                doubleValue,
-                additionalBinaryDataProperties: null);
-        }
-
-        /// <summary> Concrete proxy resource types can be created by aliasing this type using a specific property type. </summary>
         /// <param name="id"> Fully qualified resource ID for the resource. Ex - /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}. </param>
         /// <param name="name"> The name of the resource. </param>
         /// <param name="resourceType"> The type of the resource. E.g. "Microsoft.Compute/virtualMachines" or "Microsoft.Storage/storageAccounts". </param>
         /// <param name="systemData"> Azure Resource Manager metadata containing createdBy and modifiedBy information. </param>
-        /// <param name="properties"> The resource-specific properties for this resource. </param>
+        /// <param name="isEnabled"> enabled. </param>
         /// <returns> A new <see cref="MgmtTypeSpec.BarSettingsData"/> instance for mocking. </returns>
-        public static BarSettingsData BarSettingsData(ResourceIdentifier id = default, string name = default, ResourceType resourceType = default, SystemData systemData = default, BarSettingsProperties properties = default)
+        public static BarSettingsData BarSettingsData(ResourceIdentifier id = default, string name = default, ResourceType resourceType = default, SystemData systemData = default, bool? isEnabled = default)
         {
             return new BarSettingsData(
                 id,
@@ -163,15 +110,7 @@ namespace MgmtTypeSpec.Models
                 resourceType,
                 systemData,
                 additionalBinaryDataProperties: null,
-                properties);
-        }
-
-        /// <summary> The BarSettingsProperties. </summary>
-        /// <param name="isEnabled"> enabled. </param>
-        /// <returns> A new <see cref="Models.BarSettingsProperties"/> instance for mocking. </returns>
-        public static BarSettingsProperties BarSettingsProperties(bool? isEnabled = default)
-        {
-            return new BarSettingsProperties(isEnabled, additionalBinaryDataProperties: null);
+                isEnabled is null ? default : new BarSettingsProperties(isEnabled, new Dictionary<string, BinaryData>()));
         }
     }
 }
