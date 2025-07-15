1d1d30e0aad1d1e8019fe5e565e0710c82d45482
jolov <jolov@microsoft.com>
2025-07-15 14:18:25 -0700

    Remove deserialize spec

--- eng/packages/http-client-csharp/eng/scripts/Generate.ps1
+++ eng/packages/http-client-csharp/eng/scripts/Generate.ps1
@@ -63,6 +63,7 @@ $failingSpecs = @(
     # Azure scenarios not yet buildable
     Join-Path 'http' 'client' 'namespace'
     Join-Path 'http' 'azure' 'client-generator-core' 'client-initialization'
+    Join-Path 'http' 'azure' 'client-generator-core' 'deserialize-empty-string-as-null' # long path issue and also not needed for Azure emitter
     Join-Path 'http' 'azure' 'core' 'scalar'
     Join-Path 'http' 'azure' 'core' 'traits'
     Join-Path 'http' 'azure' 'payload' 'pageable'
