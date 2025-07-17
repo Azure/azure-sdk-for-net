4be69a46fb11337ef26d0e3bb60ab0954f253928
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2025-07-17 10:26:44 -0400

    Manually add AzureResourceManagerResourcesDeploymentStacksContext and add attributes for all IJsonModels.

---
+++ sdk/resources/Azure.ResourceManager.Resources.DeploymentStacks/src/Custom/AzureResourceManagerResourcesDeploymentStacksContext.Custom.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Core;
+using Azure.ResourceManager.Resources.DeploymentStacks.Models;
+using System.ClientModel.Primitives;
+
+namespace Azure.ResourceManager.Resources.DeploymentStacks;
+
+[ModelReaderWriterBuildable(typeof(ActionOnUnmanage))]
+[ModelReaderWriterBuildable(typeof(DenySettings))]
+[ModelReaderWriterBuildable(typeof(DeploymentParameter))]
+[ModelReaderWriterBuildable(typeof(DeploymentStackData))]
+[ModelReaderWriterBuildable(typeof(DeploymentStackResource))]
+[ModelReaderWriterBuildable(typeof(DeploymentStackListResult))]
+[ModelReaderWriterBuildable(typeof(DeploymentStacksDebugSetting))]
+[ModelReaderWriterBuildable(typeof(DeploymentStacksParametersLink))]
+[ModelReaderWriterBuildable(typeof(DeploymentStacksTemplateLink))]
+[ModelReaderWriterBuildable(typeof(DeploymentStackTemplateDefinition))]
+[ModelReaderWriterBuildable(typeof(DeploymentStackValidateProperties))]
+[ModelReaderWriterBuildable(typeof(DeploymentStackValidateResult))]
+[ModelReaderWriterBuildable(typeof(HttpMessage))]
+[ModelReaderWriterBuildable(typeof(KeyVaultParameterReference))]
+[ModelReaderWriterBuildable(typeof(ManagedResourceReference))]
+[ModelReaderWriterBuildable(typeof(ResourceReference))]
+[ModelReaderWriterBuildable(typeof(ResourceReferenceExtended))]
+public partial class AzureResourceManagerResourcesDeploymentStacksContext
+{
+}
