f550b55ab1b549eaac3690f22d73508e02447c05
jolov <jolov@microsoft.com>
2025-07-22 20:29:20 -0700

    using

--- sdk/core/System.ClientModel/gen/ModelReaderWriterContextGenerator.Emitter.cs
+++ sdk/core/System.ClientModel/gen/ModelReaderWriterContextGenerator.Emitter.cs
@@ -4,7 +4,6 @@
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using Microsoft.CodeAnalysis.Text;
