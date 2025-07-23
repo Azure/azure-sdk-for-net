9dd87a8e4475f7f663ef57d1c37727d3fc2358af
Wenming Liu <v-wenmingliu@microsoft.com>
2025-07-23 17:55:57 +0800

    [deviceprovisioningservices] Generate SDK from TypeSpec (#51325)

--- sdk/deviceprovisioningservices/Azure.ResourceManager.DeviceProvisioningServices/src/Generated/Models/ProvisioningServiceDescriptionListResult.cs
+++ sdk/deviceprovisioningservices/Azure.ResourceManager.DeviceProvisioningServices/src/Generated/Models/ProvisioningServiceDescriptionListResult.cs
@@ -7,10 +7,11 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Azure.ResourceManager.DeviceProvisioningServices.Models
 {
-    /// <summary> List of provisioning service descriptions. </summary>
+    /// <summary> The response of a ProvisioningServiceDescription list operation. </summary>
     internal partial class ProvisioningServiceDescriptionListResult
     {
         /// <summary>
@@ -46,25 +47,34 @@ namespace Azure.ResourceManager.DeviceProvisioningServices.Models
         private IDictionary<string, BinaryData> _serializedAdditionalRawData;
 
         /// <summary> Initializes a new instance of <see cref="ProvisioningServiceDescriptionListResult"/>. </summary>
-        internal ProvisioningServiceDescriptionListResult()
+        /// <param name="value"> The ProvisioningServiceDescription items on this page. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="value"/> is null. </exception>
+        internal ProvisioningServiceDescriptionListResult(IEnumerable<DeviceProvisioningServiceData> value)
         {
-            Value = new ChangeTrackingList<DeviceProvisioningServiceData>();
+            Argument.AssertNotNull(value, nameof(value));
+
+            Value = value.ToList();
         }
 
         /// <summary> Initializes a new instance of <see cref="ProvisioningServiceDescriptionListResult"/>. </summary>
-        /// <param name="value"> List of provisioning service descriptions. </param>
-        /// <param name="nextLink"> the next link. </param>
+        /// <param name="value"> The ProvisioningServiceDescription items on this page. </param>
+        /// <param name="nextLink"> The link to the next page of items. </param>
         /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
-        internal ProvisioningServiceDescriptionListResult(IReadOnlyList<DeviceProvisioningServiceData> value, string nextLink, IDictionary<string, BinaryData> serializedAdditionalRawData)
+        internal ProvisioningServiceDescriptionListResult(IReadOnlyList<DeviceProvisioningServiceData> value, Uri nextLink, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
             Value = value;
             NextLink = nextLink;
             _serializedAdditionalRawData = serializedAdditionalRawData;
         }
 
-        /// <summary> List of provisioning service descriptions. </summary>
+        /// <summary> Initializes a new instance of <see cref="ProvisioningServiceDescriptionListResult"/> for deserialization. </summary>
+        internal ProvisioningServiceDescriptionListResult()
+        {
+        }
+
+        /// <summary> The ProvisioningServiceDescription items on this page. </summary>
         public IReadOnlyList<DeviceProvisioningServiceData> Value { get; }
-        /// <summary> the next link. </summary>
-        public string NextLink { get; }
+        /// <summary> The link to the next page of items. </summary>
+        public Uri NextLink { get; }
     }
 }
