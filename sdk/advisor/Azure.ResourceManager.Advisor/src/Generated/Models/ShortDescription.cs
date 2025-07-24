d7795af87d36d3ad6b9697b137b3f0e0074cdd8a
azure-sdk <azuresdk@microsoft.com>
2025-07-24 11:38:54 +0000

    Configurations:  'specification/advisor/resource-manager/readme.md', API Version: 2025-01-01, SDK Release Type: stable, and CommitSHA: '577f11cd1ee4b1a3405bd9a2c8b51c47f918126a' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs'

--- sdk/advisor/Azure.ResourceManager.Advisor/src/Generated/Models/ShortDescription.cs
+++ sdk/advisor/Azure.ResourceManager.Advisor/src/Generated/Models/ShortDescription.cs
@@ -51,8 +51,8 @@ namespace Azure.ResourceManager.Advisor.Models
         }
 
         /// <summary> Initializes a new instance of <see cref="ShortDescription"/>. </summary>
-        /// <param name="problem"> The issue or opportunity identified by the recommendation. </param>
-        /// <param name="solution"> The remediation action suggested by the recommendation. </param>
+        /// <param name="problem"> The issue or opportunity identified by the recommendation and proposed solution. </param>
+        /// <param name="solution"> The issue or opportunity identified by the recommendation and proposed solution. </param>
         /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
         internal ShortDescription(string problem, string solution, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
@@ -61,9 +61,9 @@ namespace Azure.ResourceManager.Advisor.Models
             _serializedAdditionalRawData = serializedAdditionalRawData;
         }
 
-        /// <summary> The issue or opportunity identified by the recommendation. </summary>
+        /// <summary> The issue or opportunity identified by the recommendation and proposed solution. </summary>
         public string Problem { get; set; }
-        /// <summary> The remediation action suggested by the recommendation. </summary>
+        /// <summary> The issue or opportunity identified by the recommendation and proposed solution. </summary>
         public string Solution { get; set; }
     }
 }
