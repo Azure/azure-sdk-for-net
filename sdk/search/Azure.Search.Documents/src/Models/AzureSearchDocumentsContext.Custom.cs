860874eb957ba5865dee57f90406a6f4d2409e50
Wei Hu <huwe@microsoft.com>
2025-07-24 18:40:33 +0800

    add doc

--- sdk/search/Azure.Search.Documents/src/Models/AzureSearchDocumentsContext.Custom.cs
+++ sdk/search/Azure.Search.Documents/src/Models/AzureSearchDocumentsContext.Custom.cs
@@ -5,6 +5,10 @@ using System.ComponentModel;
 
 namespace Azure.Search.Documents
 {
+    /// <summary>
+    /// Context class which will be filled in by the System.ClientModel.SourceGeneration.
+    /// For more information see 'https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/System.ClientModel/src/docs/ModelReaderWriterContext.md'
+    /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
     public partial class AzureSearchDocumentsContext
     {
