37c60dc377b8dd602741e7ea1b8ca01fce27beff
Wei Hu <huwe@microsoft.com>
2025-07-24 14:37:38 +0800

    resolve breaking changes

---
+++ sdk/communication/Azure.Communication.Chat/src/Models/AzureCommunicationChatContext.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ComponentModel;
+
+namespace Azure.Communication.Chat
+{
+    public partial class AzureCommunicationChatContext
+    {
+        private static AzureCommunicationChatContext _azureCommunicationChatContext;
+
+        /// <summary> Gets the default instance </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public AzureCommunicationChatContext Default => _azureCommunicationChatContext ??= new();
+    }
+}
---
+++ sdk/communication/Azure.Communication.PhoneNumbers/src/Models/AzureCommunicationPhoneNumbersContext.Custom.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ComponentModel;
+
+namespace Azure.Communication.PhoneNumbers
+{
+    public partial class AzureCommunicationPhoneNumbersContext
+    {
+        private AzureCommunicationPhoneNumbersContext _azureCommunicationPhoneNumbersContext;
+
+        /// <summary> Gets the default instance </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public AzureCommunicationPhoneNumbersContext Default => _azureCommunicationPhoneNumbersContext ??= new();
+    }
+}
