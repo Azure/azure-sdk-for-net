ad1ac6164772b4e07ab7b70388f38750e6bfcfb4
azure-sdk <azuresdk@microsoft.com>
2025-07-29 22:52:33 +0000

    Update SDK code ag_co_0

--- sdk/appconfiguration/Azure.Data.AppConfiguration/src/Generated/Models/AzureDataAppConfigurationContext.cs
+++ sdk/appconfiguration/Azure.Data.AppConfiguration/src/Generated/Models/AzureDataAppConfigurationContext.cs
@@ -10,7 +10,7 @@ using Azure;
 
 namespace Azure.Data.AppConfiguration
 {
-/// <summary>
+    /// <summary>
     /// Context class which will be filled in by the System.ClientModel.SourceGeneration.
     /// For more information <see href='https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/System.ClientModel/src/docs/ModelReaderWriterContext.md' />
     /// </summary>
