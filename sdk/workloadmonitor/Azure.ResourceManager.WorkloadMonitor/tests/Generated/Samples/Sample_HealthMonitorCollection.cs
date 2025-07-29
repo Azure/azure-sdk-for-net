568d13a6dd06829e69e294272ef7783d9a180c32
Wei Hu <huwe@microsoft.com>
2025-07-29 20:16:01 +0800

    regen

--- sdk/workloadmonitor/Azure.ResourceManager.WorkloadMonitor/tests/Generated/Samples/Sample_HealthMonitorCollection.cs
+++ sdk/workloadmonitor/Azure.ResourceManager.WorkloadMonitor/tests/Generated/Samples/Sample_HealthMonitorCollection.cs
@@ -168,45 +168,6 @@ namespace Azure.ResourceManager.WorkloadMonitor.Samples
 
         [Test]
         [Ignore("Only validating compilation of examples")]
-        public async Task GetAll_GetMonitorListWithFilteredValues()
-        {
-            // Generated from example definition: specification/workloadmonitor/resource-manager/Microsoft.WorkloadMonitor/preview/2020-01-13-preview/examples/MonitorList_GetFiltered.json
-            // this example is just showing the usage of "HealthMonitors_List" operation, for the dependent resources, they will have to be created separately.
-
-            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
-            TokenCredential cred = new DefaultAzureCredential();
-            // authenticate your client
-            ArmClient client = new ArmClient(cred);
-
-            // this example assumes you already have this ResourceGroupResource created on azure
-            // for more information of creating ResourceGroupResource, please refer to the document of ResourceGroupResource
-            string subscriptionId = "bc27da3b-3ba2-4e00-a6ec-1fde64aa1e21";
-            string resourceGroupName = "tugamidiAlerts";
-            ResourceIdentifier resourceGroupResourceId = ResourceGroupResource.CreateResourceIdentifier(subscriptionId, resourceGroupName);
-            ResourceGroupResource resourceGroupResource = client.GetResourceGroupResource(resourceGroupResourceId);
-
-            // get the collection of this HealthMonitorResource
-            string providerName = "Microsoft.Compute";
-            string resourceCollectionName = "virtualMachines";
-            string resourceName = "linuxEUS";
-            HealthMonitorCollection collection = resourceGroupResource.GetHealthMonitors(providerName, resourceCollectionName, resourceName);
-
-            // invoke the operation and iterate over the result
-            string filter = "monitorName eq 'root'";
-            await foreach (HealthMonitorResource item in collection.GetAllAsync(filter: filter))
-            {
-                // the variable item is a resource, you could call other operations on this instance as well
-                // but just for demo, we get its data from this resource instance
-                HealthMonitorData resourceData = item.Data;
-                // for demo we just print out the id
-                Console.WriteLine($"Succeeded on id: {resourceData.Id}");
-            }
-
-            Console.WriteLine("Succeeded");
-        }
-
-        [Test]
-        [Ignore("Only validating compilation of examples")]
         public async Task Exists_GetMonitorWithDefaultValues()
         {
             // Generated from example definition: specification/workloadmonitor/resource-manager/Microsoft.WorkloadMonitor/preview/2020-01-13-preview/examples/Monitor_GetDefault.json
