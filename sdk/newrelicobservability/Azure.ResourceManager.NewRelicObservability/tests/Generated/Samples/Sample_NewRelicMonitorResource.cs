b073a8ee73f31b3aa6ee9f82836ef1e5311c3c8c
Wei Hu <huwe@microsoft.com>
2025-07-29 22:03:26 +0800

    revert sample

--- sdk/newrelicobservability/Azure.ResourceManager.NewRelicObservability/tests/Generated/Samples/Sample_NewRelicMonitorResource.cs
+++ sdk/newrelicobservability/Azure.ResourceManager.NewRelicObservability/tests/Generated/Samples/Sample_NewRelicMonitorResource.cs
@@ -470,6 +470,39 @@ namespace Azure.ResourceManager.NewRelicObservability.Samples
 
         [Test]
         [Ignore("Only validating compilation of examples")]
+        public async Task GetHosts_MonitorsListHostsMinimumSetGen()
+        {
+            // Generated from example definition: specification/newrelic/resource-manager/NewRelic.Observability/stable/2024-03-01/examples/Monitors_ListHosts_MinimumSet_Gen.json
+            // this example is just showing the usage of "Monitors_ListHosts" operation, for the dependent resources, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this NewRelicMonitorResource created on azure
+            // for more information of creating NewRelicMonitorResource, please refer to the document of NewRelicMonitorResource
+            string subscriptionId = "00000000-0000-0000-0000-000000000000";
+            string resourceGroupName = "rgopenapi";
+            string monitorName = "ipxmlcbonyxtolzejcjshkmlron";
+            ResourceIdentifier newRelicMonitorResourceId = NewRelicMonitorResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, monitorName);
+            NewRelicMonitorResource newRelicMonitorResource = client.GetNewRelicMonitorResource(newRelicMonitorResourceId);
+
+            // invoke the operation and iterate over the result
+            NewRelicHostsGetContent content = new NewRelicHostsGetContent("ruxvg@xqkmdhrnoo.hlmbpm")
+            {
+                VmIds = { new ResourceIdentifier("xzphvxvfmvjrnsgyns") },
+            };
+            await foreach (NewRelicObservabilityVmInfo item in newRelicMonitorResource.GetHostsAsync(content))
+            {
+                Console.WriteLine($"Succeeded: {item}");
+            }
+
+            Console.WriteLine("Succeeded");
+        }
+
+        [Test]
+        [Ignore("Only validating compilation of examples")]
         public async Task GetMonitoredResources_MonitorsListMonitoredResourcesMaximumSetGen()
         {
             // Generated from example definition: specification/newrelic/resource-manager/NewRelic.Observability/stable/2024-03-01/examples/Monitors_ListMonitoredResources_MaximumSet_Gen.json
