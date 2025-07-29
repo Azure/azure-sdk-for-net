89f54db350e0b4fe27f689dced11bf81b23baabc
azure-sdk <azuresdk@microsoft.com>
2025-07-29 03:14:36 +0000

    Update SDK code st_wo_9

--- sdk/synapse/Azure.ResourceManager.Synapse/src/Generated/SynapseWorkspaceData.Serialization.cs
+++ sdk/synapse/Azure.ResourceManager.Synapse/src/Generated/SynapseWorkspaceData.Serialization.cs
@@ -41,7 +41,7 @@ namespace Azure.ResourceManager.Synapse
             if (Optional.IsDefined(Identity))
             {
                 writer.WritePropertyName("identity"u8);
-                ((IJsonModel<ManagedServiceIdentity>)Identity).Write(writer, new ModelReaderWriterOptions("W|v3"));
+                ((IJsonModel<ManagedServiceIdentity>)Identity).Write(writer, ModelSerializationExtensions.WireV3Options);
             }
             writer.WritePropertyName("properties"u8);
             writer.WriteStartObject();
@@ -258,7 +258,7 @@ namespace Azure.ResourceManager.Synapse
                     {
                         continue;
                     }
-                    identity = ModelReaderWriter.Read<ManagedServiceIdentity>(new BinaryData(Encoding.UTF8.GetBytes(property.Value.GetRawText())), new ModelReaderWriterOptions("W|v3"), AzureResourceManagerSynapseContext.Default);
+                    identity = ModelReaderWriter.Read<ManagedServiceIdentity>(new BinaryData(Encoding.UTF8.GetBytes(property.Value.GetRawText())), ModelSerializationExtensions.WireV3Options, AzureResourceManagerSynapseContext.Default);
                     continue;
                 }
                 if (property.NameEquals("tags"u8))
