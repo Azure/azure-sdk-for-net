fee012fe0f91949bdf528c7cab5eb518e9b6b15c
Wei Hu <huwe@microsoft.com>
2025-07-30 10:23:05 +0800

    Make `Azure.ResourceManager.Sql` AOT-compatible

--- sdk/sqlmanagement/Azure.ResourceManager.Sql/src/Custom/2014-04-01/Models/ElasticPoolActivity.Serialization.cs
+++ sdk/sqlmanagement/Azure.ResourceManager.Sql/src/Custom/2014-04-01/Models/ElasticPoolActivity.Serialization.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
 using Azure.Core;
+using Azure.ResourceManager.Models;
 
 namespace Azure.ResourceManager.Sql.Models
 {
@@ -223,7 +224,7 @@ namespace Azure.ResourceManager.Sql.Models
                     {
                         continue;
                     }
-                    systemData = JsonSerializer.Deserialize<ResourceManager.Models.SystemData>(property.Value.GetRawText());
+                    systemData = ModelReaderWriter.Read<SystemData>(new BinaryData(Encoding.UTF8.GetBytes(property.Value.GetRawText())), ModelSerializationExtensions.WireOptions, AzureResourceManagerSqlContext.Default);
                     continue;
                 }
                 if (property.NameEquals("properties"u8))
