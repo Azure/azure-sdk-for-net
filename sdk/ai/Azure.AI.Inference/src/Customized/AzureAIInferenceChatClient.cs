f4d6ff7fb2f6832c264d2e875e9c970c999073e1
David Cantú <dacantu@microsoft.com>
2025-08-01 09:38:48 -0500

    Port - Add ChatOptions.Instructions

--- sdk/ai/Azure.AI.Inference/src/Customized/AzureAIInferenceChatClient.cs
+++ sdk/ai/Azure.AI.Inference/src/Customized/AzureAIInferenceChatClient.cs
@@ -272,7 +272,7 @@ namespace Microsoft.Extensions.AI
             new(s);
 
         private ChatCompletionsOptions CreateAzureAIOptions(IEnumerable<ChatMessage> chatContents, ChatOptions options) =>
-            new(ToAzureAIInferenceChatMessages(chatContents))
+            new(ToAzureAIInferenceChatMessages(chatContents, options))
             {
                 Model = options?.ModelId ?? _metadata.DefaultModelId ??
                     throw new InvalidOperationException("No model id was provided when either constructing the client or in the chat options.")
@@ -288,7 +288,7 @@ namespace Microsoft.Extensions.AI
 
             if (options.RawRepresentationFactory?.Invoke(this) is ChatCompletionsOptions result)
             {
-                result.Messages = ToAzureAIInferenceChatMessages(chatContents).ToList();
+                result.Messages = ToAzureAIInferenceChatMessages(chatContents, options).ToList();
                 result.Model ??= options.ModelId ?? _metadata.DefaultModelId ??
                     throw new InvalidOperationException("No model id was provided when either constructing the client or in the chat options.");
             }
@@ -411,10 +411,15 @@ namespace Microsoft.Extensions.AI
         }
 
         /// <summary>Converts an Extensions chat message enumerable to an AzureAI chat message enumerable.</summary>
-        private static IEnumerable<ChatRequestMessage> ToAzureAIInferenceChatMessages(IEnumerable<ChatMessage> inputs)
+        private static IEnumerable<ChatRequestMessage> ToAzureAIInferenceChatMessages(IEnumerable<ChatMessage> inputs, ChatOptions options)
         {
             // Maps all of the M.E.AI types to the corresponding AzureAI types.
             // Unrecognized or non-processable content is ignored.
+
+            if (options?.Instructions is { } instructions && !string.IsNullOrWhiteSpace(instructions))
+            {
+                yield return new ChatRequestSystemMessage(instructions);
+            }
 
             foreach (ChatMessage input in inputs)
             {
