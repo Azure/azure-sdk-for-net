b8cd52628ff05612a90437a24f5b8ed04b1acfb7
sophia-ramsey <sophiaramsey@microsoft.com>
2025-08-01 17:11:22 -0700

    initial tests

---
+++ sdk/ai/Azure.AI.Projects/tests/Tests/AzureOpenAI_InferenceTest.cs
@@ -0,0 +1,96 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.AI.OpenAI;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+using OpenAI.Chat;
+using Azure.Identity;
+
+namespace Azure.AI.Projects.Tests;
+
+public class AzureOpenAI_ChatTest : RecordedTestBase<AIProjectsTestEnvironment>
+{
+    public AzureOpenAI_ChatTest(bool isAsync) : base(isAsync)
+    {
+        TestDiagnostics = false;
+    }
+    [RecordedTest]
+    public void AzureOpenAI_ChatTestSync()
+    {
+        var endpoint = TestEnvironment.PROJECTENDPOINT;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+        var connectionName = TestEnvironment.AOAICONNECTIONNAME;
+
+        Console.WriteLine("Create the Azure OpenAI chat client");
+        AIProjectClient projectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+        AzureOpenAIClient azureOpenAIClient = (AzureOpenAIClient)projectClient.GetOpenAIClient(connectionName: connectionName, apiVersion: null);
+        ChatClient chatClient = azureOpenAIClient.GetChatClient(deploymentName: modelDeploymentName);
+
+        Console.WriteLine("Complete a chat");
+        ChatCompletion result = chatClient.CompleteChat("How many feet are in a mile?");
+        Console.WriteLine(result.Content[0].Text);
+        var contains = new[] { "5280", "5,280", "five thousand two hundred eighty", "five thousand two hundred and eighty" };
+        Assert.That(contains.Any(item => result.Content[0].Text.Contains(item)), "The response should contain the number of feet in a mile.");
+    }
+
+    [RecordedTest]
+    public void AzureOpenAI_ChatTestSync_NoConnection()
+    {
+        var endpoint = TestEnvironment.PROJECTENDPOINT;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+
+        Console.WriteLine("Create the Azure OpenAI chat client");
+        AIProjectClient projectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+        AzureOpenAIClient azureOpenAIClient = (AzureOpenAIClient)projectClient.GetOpenAIClient(connectionName: null, apiVersion: null);
+        ChatClient chatClient = azureOpenAIClient.GetChatClient(deploymentName: modelDeploymentName);
+
+        Console.WriteLine("Complete a chat");
+        ChatCompletion result = chatClient.CompleteChat("How many feet are in a mile?");
+        Console.WriteLine(result.Content[0].Text);
+        var contains = new[] { "5280", "5,280", "five thousand two hundred eighty", "five thousand two hundred and eighty" };
+        Assert.That(contains.Any(item => result.Content[0].Text.Contains(item)), "The response should contain the number of feet in a mile.");
+    }
+
+    [RecordedTest]
+    public async Task AzureOpenAI_ChatTestAsync()
+    {
+        var endpoint = TestEnvironment.PROJECTENDPOINT;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+        var connectionName = TestEnvironment.AOAICONNECTIONNAME;
+
+        Console.WriteLine("Create the Azure OpenAI chat client");
+        AIProjectClient projectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+        AzureOpenAIClient azureOpenAIClient = (AzureOpenAIClient)projectClient.GetOpenAIClient(connectionName: connectionName, apiVersion: null);
+        ChatClient chatClient = azureOpenAIClient.GetChatClient(deploymentName: modelDeploymentName);
+
+        Console.WriteLine("Complete a chat");
+        ChatCompletion result = await chatClient.CompleteChatAsync("How many feet are in a mile?");
+        Console.WriteLine(result.Content[0].Text);
+        var contains = new[] { "5280", "5,280", "five thousand two hundred eighty", "five thousand two hundred and eighty" };
+        Assert.That(contains.Any(item => result.Content[0].Text.Contains(item)), "The response should contain the number of feet in a mile.");
+    }
+
+    [RecordedTest]
+    public async Task AzureOpenAI_ChatTestAsync_NoConnection()
+    {
+        var endpoint = TestEnvironment.PROJECTENDPOINT;
+        var modelDeploymentName = TestEnvironment.MODELDEPLOYMENTNAME;
+
+        Console.WriteLine("Create the Azure OpenAI chat client");
+        AIProjectClient projectClient = new(new Uri(endpoint), new DefaultAzureCredential());
+        AzureOpenAIClient azureOpenAIClient = (AzureOpenAIClient)projectClient.GetOpenAIClient(connectionName: null, apiVersion: null);
+        ChatClient chatClient = azureOpenAIClient.GetChatClient(deploymentName: modelDeploymentName);
+
+        Console.WriteLine("Complete a chat");
+        ChatCompletion result = await chatClient.CompleteChatAsync("How many feet are in a mile?");
+        Console.WriteLine(result.Content[0].Text);
+        var contains = new[] { "5280", "5,280", "five thousand two hundred eighty", "five thousand two hundred and eighty" };
+        Assert.That(contains.Any(item => result.Content[0].Text.Contains(item)), "The response should contain the number of feet in a mile.");
+    }
+}
