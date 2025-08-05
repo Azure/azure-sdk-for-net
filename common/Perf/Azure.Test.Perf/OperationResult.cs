1d20c465cab2337bf9b4ec0ffa8a7a1d04090927
Jacob Lauzon <jalauzon@microsoft.com>
2025-08-05 16:54:16 -0700

    Add support for advanced stats tracking

---
+++ common/Perf/Azure.Test.Perf/OperationResult.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Azure.Test.Perf
+{
+    public class OperationResult
+    {
+        public TimeSpan Time { get; set; }
+        public long Size { get; set; }
+    }
+}
