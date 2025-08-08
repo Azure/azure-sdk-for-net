55081f8c233c51e8300268f268a6a8ac6b22172d
mcgallan <1186003288@qq.com>
2025-08-08 10:45:18 +0800

    update

--- sdk/recoveryservices/Azure.ResourceManager.RecoveryServices/src/Custom/ArmRecoveryServicesModelFactory.cs
+++ sdk/recoveryservices/Azure.ResourceManager.RecoveryServices/src/Custom/ArmRecoveryServicesModelFactory.cs
@@ -16,11 +16,11 @@ namespace Azure.ResourceManager.RecoveryServices.Models
 {
     public static partial class ArmRecoveryServicesModelFactory
     {
+        // Manually provided factory method because VaultPropertiesRedundancySettings has public constructor with settable properties
         /// <summary> Initializes a new instance of <see cref="Models.VaultPropertiesRedundancySettings"/>. </summary>
         /// <param name="standardTierStorageRedundancy"> The storage redundancy setting of a vault. </param>
         /// <param name="crossRegionRestore"> Flag to show if Cross Region Restore is enabled on the Vault or not. </param>
         /// <returns> A new <see cref="Models.VaultPropertiesRedundancySettings"/> instance for mocking. </returns>
-        [System.ComponentModel.Description("Manually provided factory method because VaultPropertiesRedundancySettings has public constructor with settable properties")]
         public static VaultPropertiesRedundancySettings VaultPropertiesRedundancySettings(StandardTierStorageRedundancy? standardTierStorageRedundancy = null, CrossRegionRestore? crossRegionRestore = null)
         {
             return new VaultPropertiesRedundancySettings(standardTierStorageRedundancy, crossRegionRestore, serializedAdditionalRawData: null);
