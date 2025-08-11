e7820ac3987788b18bf3d3d9f9c3f20a3afbb18d
Arcturus Zhang <dapzhang@microsoft.com>
2025-08-11 15:17:37 +0800

    more refactors

---
+++ eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Models/MethodCarrier.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Generator.Management.Models
+{
+    internal enum MethodCarrier
+    {
+        ResourceClient = 0,
+        ResourceCollectionClient = 1,
+        MockableResourceClient = 2,
+    }
+}
