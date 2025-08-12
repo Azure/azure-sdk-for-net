c5f9082614f32a6b61b20c6055cec42e30d18be5
Madalyn Redding <mredding@microsoft.com>
2025-08-12 16:24:33 -0700

    add missing docs

--- sdk/core/Microsoft.ClientModel.TestFramework/src/TestUtilities/AsyncAssert.cs
+++ sdk/core/Microsoft.ClientModel.TestFramework/src/TestUtilities/AsyncAssert.cs
@@ -8,17 +8,21 @@ using System.Threading.Tasks;
 namespace Microsoft.ClientModel.TestFramework;
 
 /// <summary>
-/// TODO.
+/// Provides assertion methods for testing asynchronous operations that are expected to throw exceptions.
+/// This class extends NUnit's assertion capabilities to handle async/await patterns more effectively.
 /// </summary>
 public static class AsyncAssert
 {
     /// <summary>
-    /// TODO.
+    /// Verifies that an asynchronous operation throws an exception of the specified type.
+    /// This method awaits the provided asynchronous action and validates that it throws
+    /// an exception of type <typeparamref name="T"/> or a derived type.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="action"></param>
-    /// <returns></returns>
-    /// <exception cref="AssertionException"></exception>
+    /// <typeparam name="T">The type of exception that is expected to be thrown. Must inherit from <see cref="Exception"/>.</typeparam>
+    /// <param name="action">
+    /// A function that returns a <see cref="Task"/> representing the asynchronous operation
+    /// that is expected to throw an exception.
+    /// </param>
     public static async Task<T> ThrowsAsync<T>(Func<Task> action) where T : Exception
     {
         Exception? triggeredException = null;
