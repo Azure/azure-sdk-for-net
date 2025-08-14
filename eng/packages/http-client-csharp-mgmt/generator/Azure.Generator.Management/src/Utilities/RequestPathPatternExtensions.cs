b62f340c6588707baf46e3f2d3e9d2b1714d7abc
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2025-08-14 13:47:57 +0800

    fix issue in handling enum based rest client parameter.

--- eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Utilities/RequestPathPatternExtensions.cs
+++ eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Utilities/RequestPathPatternExtensions.cs
@@ -52,7 +52,8 @@ namespace Azure.Generator.Management.Utilities
                 }
                 else
                 {
-                    arguments.Add(methodParameters.Single(p => p.WireInfo.SerializedName == parameter.WireInfo.SerializedName));
+                    var methodPara = methodParameters.Single(p => p.WireInfo.SerializedName == parameter.WireInfo.SerializedName);
+                    arguments.Add(Convert(methodPara, methodPara.Type, parameter.Type));
                 }
             }
             return arguments;
@@ -67,6 +68,11 @@ namespace Azure.Generator.Management.Utilities
                 if (toType.IsFrameworkType && toType.FrameworkType == typeof(Guid))
                 {
                     return Static<Guid>().Invoke(nameof(Guid.Parse), expression);
+                }
+
+                if (fromType.IsEnum && toType.FrameworkType == typeof(string))
+                {
+                    return expression.Invoke("ToString");
                 }
 
                 // other unhandled cases, we will add when we need them in the future.
