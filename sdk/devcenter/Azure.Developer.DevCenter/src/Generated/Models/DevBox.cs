ae21f7e4658430a976c54dfa5f57ef9bc2d465c9
Wei Hu <huwe@microsoft.com>
2025-08-14 21:21:43 +0800

    mitigate breaking change for devcenter

--- sdk/devcenter/Azure.Developer.DevCenter/src/Generated/Models/DevBox.cs
+++ sdk/devcenter/Azure.Developer.DevCenter/src/Generated/Models/DevBox.cs
@@ -142,7 +142,5 @@ namespace Azure.Developer.DevCenter.Models
         public DevBoxImageReference ImageReference { get; }
         /// <summary> Creation time of this Dev Box, in RFC3339 format. </summary>
         public DateTimeOffset? CreatedTime { get; }
-        /// <summary> Indicates whether the owner of the Dev Box is a local administrator. </summary>
-        public LocalAdministratorStatus? LocalAdministratorStatus { get; }
     }
 }
