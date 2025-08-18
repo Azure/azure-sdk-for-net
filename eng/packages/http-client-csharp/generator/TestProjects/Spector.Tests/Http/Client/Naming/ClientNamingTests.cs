61c50e49d290c1c2d13b2c9fdfb73e60c7a5b9e0
jolov <jolov@microsoft.com>
2025-08-18 14:00:06 -0700

    move tests

--- eng/packages/http-client-csharp/generator/TestProjects/Spector.Tests/Http/Client/Naming/ClientNamingTests.cs
+++ eng/packages/http-client-csharp/generator/TestProjects/Spector.Tests/Http/Client/Naming/ClientNamingTests.cs
@@ -4,9 +4,6 @@
 using System.Threading.Tasks;
 using Client.Naming;
 using Client.Naming._UnionEnum;
-using Client.Naming.EnumConflict;
-using Client.Naming.EnumConflict.FirstNamespace;
-using Client.Naming.EnumConflict.SecondNamespace;
 using Client.Naming.Model;
 using Client.Naming.Property;
 using NUnit.Framework;
@@ -108,26 +105,6 @@ namespace TestProjects.Spector.Tests.Http.Client.Naming
         {
             var response = await new NamingClient(host, null).GetUnionEnumClient().UnionEnumMemberNameAsync(ExtensibleEnum.ClientEnumValue1);
             Assert.AreEqual(204, response.Status);
-        });
-
-        [SpectorTest]
-        public Task Client_Naming_EnumConflict_first() => Test(async (host) =>
-        {
-            var firstModel = new FirstModel(Status.Active, "test");
-            var response = await new EnumConflictClient(host, null).GetFirstOperationsClient().FirstAsync(firstModel);
-            Assert.AreEqual(200, response.GetRawResponse().Status);
-            Assert.AreEqual(Status.Active, response.Value.Status);
-            Assert.AreEqual("test", response.Value.Name);
-        });
-
-        [SpectorTest]
-        public Task Client_Naming_EnumConflict_second() => Test(async (host) =>
-        {
-            var secondModel = new SecondModel(SecondStatus.Running, "test description");
-            var response = await new EnumConflictClient(host, null).GetSecondOperationsClient().SecondAsync(secondModel);
-            Assert.AreEqual(200, response.GetRawResponse().Status);
-            Assert.AreEqual(SecondStatus.Running, response.Value.Status);
-            Assert.AreEqual("test description", response.Value.Description);
         });
     }
 }
