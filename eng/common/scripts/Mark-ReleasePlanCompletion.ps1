f286913e8dba06f0734fe8900b871cc8d9a61d89
Praveen Kuttappan <praveen.kuttappan@gmail.com>
2025-08-19 10:54:49 -0400

    Update release plan completion script to take package info file

--- eng/common/scripts/Mark-ReleasePlanCompletion.ps1
+++ eng/common/scripts/Mark-ReleasePlanCompletion.ps1
@@ -1,12 +1,9 @@
 param(    
     [Parameter(Mandatory = $true)]
-    [string]$PackageName,
+    [string]$PackageArtifactName,
 
     [Parameter(Mandatory = $true)]
-    [string]$PackageVersion,
-    
-    [Parameter(Mandatory = $false)]
-    [int]$MaxCount = 5
+    [string]$PackageConfigPath
 )
 
 <#
@@ -16,23 +13,35 @@ param(
 .DESCRIPTION
     This script helps to mark release plan completion by finding the active release plans for a package name
 
-.PARAMETER PackageName
-    The package name being released (required)
+.PARAMETER PackageArtifactName
+    The package artifact name being released (required)
 
-.PARAMETER PackageVersion
-    The package version being released (required)
-
-.PARAMETER MaxCount
-    Maximum number of recent pull request merges to check (default: 5)
-
+.PARAMETER PackageConfigPath
+    The path to the package configuration file (required)
 #>
 
 Set-StrictMode -Version 3
 . (Join-Path $PSScriptRoot common.ps1)
 . (Join-Path $PSScriptRoot Helpers DevOps-WorkItem-Helpers.ps1)
 
-$AzureDevOpsOrganization = "azure-sdk"
-$AzureDevOpsProject = "Release"
+
+#Get package properties
+$pkgPropPath = Join-Path -Path $PackageConfigPath "$PackageArtifactName.json"
+if (-Not (Test-Path $pkgPropPath))
+{
+    Write-Host "Package property file path $($pkgPropPath) is invalid."
+    exit 0
+}
+# Get package info from json file created before updating version to daily dev
+$pkgInfo = Get-Content $pkgPropPath | ConvertFrom-Json
+$PackageVersion = $pkgInfo.Version
+$PackageName = $pkgInfo.Name
+if (!$PackageName -or !$PackageVersion)
+{
+    Write-Host "Package name or version is not available in the package property file. Skipping the release plan status update for the package."
+    exit 0
+}
+
 # Check Azure DevOps Release Plan work items if LanguageShort is available
 Write-Host "Checking active release plan work items for package: $PackageName"
 $workItems = Get-ReleasePlanForPackage $PackageName
