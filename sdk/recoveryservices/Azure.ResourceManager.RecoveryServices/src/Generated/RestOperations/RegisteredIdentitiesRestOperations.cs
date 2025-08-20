8855ddc6822a4a92b86355baeaad65b48cca7bf3
mcgallan <1186003288@qq.com>
2025-08-20 11:19:52 +0800

    update

--- sdk/recoveryservices/Azure.ResourceManager.RecoveryServices/src/Generated/RestOperations/RegisteredIdentitiesRestOperations.cs
+++ sdk/recoveryservices/Azure.ResourceManager.RecoveryServices/src/Generated/RestOperations/RegisteredIdentitiesRestOperations.cs
@@ -67,7 +67,6 @@ namespace Azure.ResourceManager.RecoveryServices
             uri.AppendPath(identityName, true);
             uri.AppendQuery("api-version", _apiVersion, true);
             request.Uri = uri;
-            request.Headers.Add("Accept", "application/json");
             _userAgent.Apply(message);
             return message;
         }
--- sdk/recoveryservices/Azure.ResourceManager.RecoveryServices/src/Generated/RestOperations/VaultsRestOperations.cs
+++ sdk/recoveryservices/Azure.ResourceManager.RecoveryServices/src/Generated/RestOperations/VaultsRestOperations.cs
@@ -357,7 +357,6 @@ namespace Azure.ResourceManager.RecoveryServices
             uri.AppendPath(vaultName, true);
             uri.AppendQuery("api-version", _apiVersion, true);
             request.Uri = uri;
-            request.Headers.Add("Accept", "application/json");
             _userAgent.Apply(message);
             return message;
         }
