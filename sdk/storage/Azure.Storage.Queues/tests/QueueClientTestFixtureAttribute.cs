c48b36ee86072d77bc903fb07ea01bd7e1094fb6
Sean McCullough <seanmcc@microsoft.com>
2025-08-20 11:15:25 -0500

    PR comments

--- sdk/storage/Azure.Storage.Queues/tests/QueueClientTestFixtureAttribute.cs
+++ sdk/storage/Azure.Storage.Queues/tests/QueueClientTestFixtureAttribute.cs
@@ -47,7 +47,7 @@ namespace Azure.Storage.Queues.Tests
                 additionalParameters: additionalParameters)
                 {
                     RecordingServiceVersion = StorageVersionExtensions.MaxVersion;
-                    LiveServiceVersions = new object[] { StorageVersionExtensions.LatestVersion };
+                    LiveServiceVersions = new object[] { StorageVersionExtensions.LatestVersion, };
                 }
     }
 }
