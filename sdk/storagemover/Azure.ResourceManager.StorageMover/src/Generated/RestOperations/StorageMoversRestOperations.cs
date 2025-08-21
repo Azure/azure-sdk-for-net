a732d4a0ee496e10e69cbdb2659ae71b1f7b3b77
Wenming Liu <v-wenmingliu@microsoft.com>
2025-08-21 16:01:42 +0800

    update

--- sdk/storagemover/Azure.ResourceManager.StorageMover/src/Generated/RestOperations/StorageMoversRestOperations.cs
+++ sdk/storagemover/Azure.ResourceManager.StorageMover/src/Generated/RestOperations/StorageMoversRestOperations.cs
@@ -361,7 +361,6 @@ namespace Azure.ResourceManager.StorageMover
             uri.AppendPath(storageMoverName, true);
             uri.AppendQuery("api-version", _apiVersion, true);
             request.Uri = uri;
-            request.Headers.Add("Accept", "application/json");
             _userAgent.Apply(message);
             return message;
         }
