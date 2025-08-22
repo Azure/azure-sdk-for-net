2b6564e86b4367b9a3ebe3a62ad645894d2d240b
Wenming Liu <v-wenmingliu@microsoft.com>
2025-08-22 10:01:07 +0800

    [storagemover]  Generate SDK from TypeSpec (#51156)
    * gen from tsp
    
    * Update tsp-location.yaml
    
    * fix for: [PersistableModelProxy(typeof(UnknownCredentials))]
    
    * Update CHANGELOG.md
    
    * Update tsp-location.yaml
    
    * Update CHANGELOG.md
    
    * Update sdk/storagemover/Azure.ResourceManager.StorageMover/CHANGELOG.md
    
    Co-authored-by: Dapeng Zhang <ufo54153@gmail.com>
    
    * Merge remote-tracking branch 'upstream/main' into storagemover-sdk-tsp
    
    * update
    
    * Update assets.json
    
    ---------
    
    Co-authored-by: Dapeng Zhang <ufo54153@gmail.com>

--- sdk/storagemover/Azure.ResourceManager.StorageMover/src/Generated/StorageMoverAgentCollection.cs
+++ sdk/storagemover/Azure.ResourceManager.StorageMover/src/Generated/StorageMoverAgentCollection.cs
@@ -60,7 +60,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_CreateOrUpdate</description>
+        /// <description>Agent_CreateOrUpdate</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -111,7 +111,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_CreateOrUpdate</description>
+        /// <description>Agent_CreateOrUpdate</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -162,7 +162,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_Get</description>
+        /// <description>Agent_Get</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -207,7 +207,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_Get</description>
+        /// <description>Agent_Get</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -252,7 +252,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_List</description>
+        /// <description>Agent_List</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -282,7 +282,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_List</description>
+        /// <description>Agent_List</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -312,7 +312,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_Get</description>
+        /// <description>Agent_Get</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -355,7 +355,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_Get</description>
+        /// <description>Agent_Get</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -398,7 +398,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_Get</description>
+        /// <description>Agent_Get</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
@@ -443,7 +443,7 @@ namespace Azure.ResourceManager.StorageMover
         /// </item>
         /// <item>
         /// <term>Operation Id</term>
-        /// <description>Agents_Get</description>
+        /// <description>Agent_Get</description>
         /// </item>
         /// <item>
         /// <term>Default Api Version</term>
