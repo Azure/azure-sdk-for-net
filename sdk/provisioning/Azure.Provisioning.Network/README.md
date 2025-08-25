ffa66ffef5580135c00c7d566d7a9d66b230623a
Arcturus Zhang <dapzhang@microsoft.com>
2025-08-25 16:57:43 +0800

    add a new test

--- sdk/provisioning/Azure.Provisioning.Network/README.md
+++ sdk/provisioning/Azure.Provisioning.Network/README.md
@@ -26,45 +26,47 @@ This library allows you to specify your infrastructure in a declarative style us
 
 ### Create a Virtual Network with two Subnets
 
+This template allows you to create a Virtual Network with two subnets.
+
 ```C# Snippet:VNetTwoSubnets
 Infrastructure infra = new();
-ProvisioningParameter vnetName = new("vnetName", typeof(string))
+ProvisioningParameter vnetName = new(nameof(vnetName), typeof(string))
 {
     Description = "VNet name",
     Value = "VNet1"
 };
 infra.Add(vnetName);
-ProvisioningParameter vnetAddressPrefix = new("vnetAddressPrefix", typeof(string))
+ProvisioningParameter vnetAddressPrefix = new(nameof(vnetAddressPrefix), typeof(string))
 {
     Description = "Address prefix",
     Value = "10.0.0.0/16"
 };
 infra.Add(vnetAddressPrefix);
-ProvisioningParameter subnet1Prefix = new("subnet1Prefix", typeof(string))
+ProvisioningParameter subnet1Prefix = new(nameof(subnet1Prefix), typeof(string))
 {
     Description = "Subnet 1 Prefix",
     Value = "10.0.0.0/24"
 };
 infra.Add(subnet1Prefix);
-ProvisioningParameter subnet1Name = new("subnet1Name", typeof(string))
+ProvisioningParameter subnet1Name = new(nameof(subnet1Name), typeof(string))
 {
     Description = "Subnet 1 Name",
     Value = "Subnet1"
 };
 infra.Add(subnet1Name);
-ProvisioningParameter subnet2Prefix = new("subnet2Prefix", typeof(string))
+ProvisioningParameter subnet2Prefix = new(nameof(subnet2Prefix), typeof(string))
 {
     Description = "Subnet 2 Prefix",
     Value = "10.0.1.0/24"
 };
 infra.Add(subnet2Prefix);
-ProvisioningParameter subnet2Name = new("subnet2Name", typeof(string))
+ProvisioningParameter subnet2Name = new(nameof(subnet2Name), typeof(string))
 {
     Description = "Subnet 2 Name",
     Value = "Subnet2"
 };
 infra.Add(subnet2Name);
-VirtualNetwork vnet = new("vnet", "2021-08-01")
+VirtualNetwork vnet = new(nameof(vnet), VirtualNetwork.ResourceVersions.V2021_08_01)
 {
     Name = vnetName,
     AddressSpace = new VirtualNetworkAddressSpace()
@@ -87,6 +89,112 @@ VirtualNetwork vnet = new("vnet", "2021-08-01")
             AddressPrefix = subnet2Prefix
         }
     ]
+};
+infra.Add(vnet);
+```
+
+### Virtual Network NAT
+
+This template deploys a NAT gateway and virtual network with a single subnet and public IP resource for the NAT gateway.
+
+```C# Snippet:NatGatewayVNet
+Infrastructure infra = new();
+ProvisioningParameter vnetName = new(nameof(vnetName), typeof(string))
+{
+    Description = "Name of the virtual network",
+    Value = "myVnet"
+};
+infra.Add(vnetName);
+ProvisioningParameter subnetName = new(nameof(subnetName), typeof(string))
+{
+    Description = "Name of the subnet for virtual network",
+    Value = "mySubnet"
+};
+infra.Add(subnetName);
+ProvisioningParameter vnetAddressSpace = new(nameof(vnetAddressSpace), typeof(string))
+{
+    Description = "Address space for virtual network",
+    Value = "192.168.0.0/16"
+};
+infra.Add(vnetAddressSpace);
+ProvisioningParameter vnetSubnetPrefix = new(nameof(vnetSubnetPrefix), typeof(string))
+{
+    Description = "Subnet prefix for virtual network",
+    Value = "192.168.0.0/24"
+};
+infra.Add(vnetSubnetPrefix);
+ProvisioningParameter natGatewayName = new(nameof(natGatewayName), typeof(string))
+{
+    Description = "Name of the NAT gateway resource",
+    Value = "myNATgateway"
+};
+infra.Add(natGatewayName);
+ProvisioningParameter publicIpDns = new(nameof(publicIpDns), typeof(string))
+{
+    Description = "dns of the public ip address, leave blank for no dns",
+    Value = BicepFunction.Interpolate($"gw-{BicepFunction.GetUniqueString(BicepFunction.GetResourceGroup().Id)}")
+};
+infra.Add(publicIpDns);
+ProvisioningVariable publicIpName = new(nameof(publicIpName), typeof(string))
+{
+    Value = BicepFunction.Interpolate($"{natGatewayName}-ip")
+};
+infra.Add(publicIpName);
+PublicIPAddress publicIp = new(nameof(publicIp), PublicIPAddress.ResourceVersions.V2020_06_01)
+{
+    Name = publicIpName,
+    Sku = new PublicIPAddressSku()
+    {
+        Name = PublicIPAddressSkuName.Standard
+    },
+    PublicIPAddressVersion = NetworkIPVersion.IPv4,
+    PublicIPAllocationMethod = NetworkIPAllocationMethod.Static,
+    IdleTimeoutInMinutes = 4,
+    DnsSettings = new PublicIPAddressDnsSettings()
+    {
+        DomainNameLabel = publicIpDns
+    },
+};
+infra.Add(publicIp);
+NatGateway natGateway = new(nameof(natGateway), NatGateway.ResourceVersions.V2020_06_01)
+{
+    Name = natGatewayName,
+    SkuName = NatGatewaySkuName.Standard,
+    IdleTimeoutInMinutes = 4,
+    PublicIPAddresses =
+    [
+        new WritableSubResource()
+        {
+            Id = publicIp.Id
+        }
+    ]
+};
+infra.Add(natGateway);
+
+// Add the missing VirtualNetwork with subnet linked to NAT gateway
+VirtualNetwork vnet = new(nameof(vnet), VirtualNetwork.ResourceVersions.V2020_06_01)
+{
+    Name = vnetName,
+    AddressSpace = new VirtualNetworkAddressSpace()
+    {
+        AddressPrefixes =
+        [
+            vnetAddressSpace
+        ]
+    },
+    Subnets =
+    [
+        new Subnet()
+        {
+            Name = subnetName,
+            AddressPrefix = vnetSubnetPrefix,
+            NatGatewayId = natGateway.Id,
+            PrivateEndpointNetworkPolicy = VirtualNetworkPrivateEndpointNetworkPolicy.Enabled,
+            PrivateLinkServiceNetworkPolicy = VirtualNetworkPrivateLinkServiceNetworkPolicy.Enabled
+        }
+    ],
+    EnableDdosProtection = false,
+    EnableVmProtection = false
 };
 infra.Add(vnet);
 ```
--- sdk/provisioning/Azure.Provisioning.Network/tests/BasicNetworkTests.cs
+++ sdk/provisioning/Azure.Provisioning.Network/tests/BasicNetworkTests.cs
@@ -2,6 +2,9 @@
 // Licensed under the MIT License.
 
 using System.Threading.Tasks;
+using Azure.Provisioning.Expressions;
+using Azure.Provisioning.Primitives;
+using Azure.Provisioning.Resources;
 using Azure.Provisioning.Tests;
 using NUnit.Framework;
 
@@ -19,43 +22,43 @@ public class BasicNetworkTests(bool async) : ProvisioningTestBase(async)
             {
                 #region Snippet:VNetTwoSubnets
                 Infrastructure infra = new();
-                ProvisioningParameter vnetName = new("vnetName", typeof(string))
+                ProvisioningParameter vnetName = new(nameof(vnetName), typeof(string))
                 {
                     Description = "VNet name",
                     Value = "VNet1"
                 };
                 infra.Add(vnetName);
-                ProvisioningParameter vnetAddressPrefix = new("vnetAddressPrefix", typeof(string))
+                ProvisioningParameter vnetAddressPrefix = new(nameof(vnetAddressPrefix), typeof(string))
                 {
                     Description = "Address prefix",
                     Value = "10.0.0.0/16"
                 };
                 infra.Add(vnetAddressPrefix);
-                ProvisioningParameter subnet1Prefix = new("subnet1Prefix", typeof(string))
+                ProvisioningParameter subnet1Prefix = new(nameof(subnet1Prefix), typeof(string))
                 {
                     Description = "Subnet 1 Prefix",
                     Value = "10.0.0.0/24"
                 };
                 infra.Add(subnet1Prefix);
-                ProvisioningParameter subnet1Name = new("subnet1Name", typeof(string))
+                ProvisioningParameter subnet1Name = new(nameof(subnet1Name), typeof(string))
                 {
                     Description = "Subnet 1 Name",
                     Value = "Subnet1"
                 };
                 infra.Add(subnet1Name);
-                ProvisioningParameter subnet2Prefix = new("subnet2Prefix", typeof(string))
+                ProvisioningParameter subnet2Prefix = new(nameof(subnet2Prefix), typeof(string))
                 {
                     Description = "Subnet 2 Prefix",
                     Value = "10.0.1.0/24"
                 };
                 infra.Add(subnet2Prefix);
-                ProvisioningParameter subnet2Name = new("subnet2Name", typeof(string))
+                ProvisioningParameter subnet2Name = new(nameof(subnet2Name), typeof(string))
                 {
                     Description = "Subnet 2 Name",
                     Value = "Subnet2"
                 };
                 infra.Add(subnet2Name);
-                VirtualNetwork vnet = new("vnet", "2021-08-01")
+                VirtualNetwork vnet = new(nameof(vnet), VirtualNetwork.ResourceVersions.V2021_08_01)
                 {
                     Name = vnetName,
                     AddressSpace = new VirtualNetworkAddressSpace()
@@ -125,6 +128,205 @@ public class BasicNetworkTests(bool async) : ProvisioningTestBase(async)
                     name: subnet2Name
                     properties: {
                       addressPrefix: subnet2Prefix
+                    }
+                  }
+                ]
+              }
+              location: location
+            }
+            """)
+        .Lint()
+        .ValidateAsync();
+    }
+
+    [Test]
+    [Description("https://github.com/Azure/azure-quickstart-templates/blob/master/quickstarts/microsoft.network/nat-gateway-vnet/main.bicep")]
+    public async Task NatGatewayVNet()
+    {
+        await using Trycep test = CreateBicepTest();
+        await test.Define(
+            ctx =>
+            {
+                #region Snippet:NatGatewayVNet
+                Infrastructure infra = new();
+                ProvisioningParameter vnetName = new(nameof(vnetName), typeof(string))
+                {
+                    Description = "Name of the virtual network",
+                    Value = "myVnet"
+                };
+                infra.Add(vnetName);
+                ProvisioningParameter subnetName = new(nameof(subnetName), typeof(string))
+                {
+                    Description = "Name of the subnet for virtual network",
+                    Value = "mySubnet"
+                };
+                infra.Add(subnetName);
+                ProvisioningParameter vnetAddressSpace = new(nameof(vnetAddressSpace), typeof(string))
+                {
+                    Description = "Address space for virtual network",
+                    Value = "192.168.0.0/16"
+                };
+                infra.Add(vnetAddressSpace);
+                ProvisioningParameter vnetSubnetPrefix = new(nameof(vnetSubnetPrefix), typeof(string))
+                {
+                    Description = "Subnet prefix for virtual network",
+                    Value = "192.168.0.0/24"
+                };
+                infra.Add(vnetSubnetPrefix);
+                ProvisioningParameter natGatewayName = new(nameof(natGatewayName), typeof(string))
+                {
+                    Description = "Name of the NAT gateway resource",
+                    Value = "myNATgateway"
+                };
+                infra.Add(natGatewayName);
+                ProvisioningParameter publicIpDns = new(nameof(publicIpDns), typeof(string))
+                {
+                    Description = "dns of the public ip address, leave blank for no dns",
+                    Value = BicepFunction.Interpolate($"gw-{BicepFunction.GetUniqueString(BicepFunction.GetResourceGroup().Id)}")
+                };
+                infra.Add(publicIpDns);
+                ProvisioningVariable publicIpName = new(nameof(publicIpName), typeof(string))
+                {
+                    Value = BicepFunction.Interpolate($"{natGatewayName}-ip")
+                };
+                infra.Add(publicIpName);
+                PublicIPAddress publicIp = new(nameof(publicIp), PublicIPAddress.ResourceVersions.V2020_06_01)
+                {
+                    Name = publicIpName,
+                    Sku = new PublicIPAddressSku()
+                    {
+                        Name = PublicIPAddressSkuName.Standard
+                    },
+                    PublicIPAddressVersion = NetworkIPVersion.IPv4,
+                    PublicIPAllocationMethod = NetworkIPAllocationMethod.Static,
+                    IdleTimeoutInMinutes = 4,
+                    DnsSettings = new PublicIPAddressDnsSettings()
+                    {
+                        DomainNameLabel = publicIpDns
+                    },
+                };
+                infra.Add(publicIp);
+                NatGateway natGateway = new(nameof(natGateway), NatGateway.ResourceVersions.V2020_06_01)
+                {
+                    Name = natGatewayName,
+                    SkuName = NatGatewaySkuName.Standard,
+                    IdleTimeoutInMinutes = 4,
+                    PublicIPAddresses =
+                    [
+                        new WritableSubResource()
+                        {
+                            Id = publicIp.Id
+                        }
+                    ]
+                };
+                infra.Add(natGateway);
+
+                // Add the missing VirtualNetwork with subnet linked to NAT gateway
+                VirtualNetwork vnet = new(nameof(vnet), VirtualNetwork.ResourceVersions.V2020_06_01)
+                {
+                    Name = vnetName,
+                    AddressSpace = new VirtualNetworkAddressSpace()
+                    {
+                        AddressPrefixes =
+                        [
+                            vnetAddressSpace
+                        ]
+                    },
+                    Subnets =
+                    [
+                        new Subnet()
+                        {
+                            Name = subnetName,
+                            AddressPrefix = vnetSubnetPrefix,
+                            NatGatewayId = natGateway.Id,
+                            PrivateEndpointNetworkPolicy = VirtualNetworkPrivateEndpointNetworkPolicy.Enabled,
+                            PrivateLinkServiceNetworkPolicy = VirtualNetworkPrivateLinkServiceNetworkPolicy.Enabled
+                        }
+                    ],
+                    EnableDdosProtection = false,
+                    EnableVmProtection = false
+                };
+                infra.Add(vnet);
+                #endregion
+                return infra;
+            })
+        .Compare(
+            """
+            @description('Name of the virtual network')
+            param vnetName string = 'myVnet'
+
+            @description('Name of the subnet for virtual network')
+            param subnetName string = 'mySubnet'
+
+            @description('Address space for virtual network')
+            param vnetAddressSpace string = '192.168.0.0/16'
+
+            @description('Subnet prefix for virtual network')
+            param vnetSubnetPrefix string = '192.168.0.0/24'
+
+            @description('Name of the NAT gateway resource')
+            param natGatewayName string = 'myNATgateway'
+
+            @description('dns of the public ip address, leave blank for no dns')
+            param publicIpDns string = 'gw-${uniqueString(resourceGroup().id)}'
+
+            @description('The location for the resource(s) to be deployed.')
+            param location string = resourceGroup().location
+
+            var publicIpName = '${natGatewayName}-ip'
+
+            resource publicIp 'Microsoft.Network/publicIPAddresses@2020-06-01' = {
+              name: publicIpName
+              properties: {
+                dnsSettings: {
+                  domainNameLabel: publicIpDns
+                }
+                idleTimeoutInMinutes: 4
+                publicIPAddressVersion: 'IPv4'
+                publicIPAllocationMethod: 'Static'
+              }
+              location: location
+              sku: {
+                name: 'Standard'
+              }
+            }
+
+            resource natGateway 'Microsoft.Network/natGateways@2020-06-01' = {
+              name: natGatewayName
+              properties: {
+                idleTimeoutInMinutes: 4
+                publicIpAddresses: [
+                  {
+                    id: publicIp.id
+                  }
+                ]
+              }
+              location: location
+              sku: {
+                name: 'Standard'
+              }
+            }
+
+            resource vnet 'Microsoft.Network/virtualNetworks@2020-06-01' = {
+              name: vnetName
+              properties: {
+                addressSpace: {
+                  addressPrefixes: [
+                    vnetAddressSpace
+                  ]
+                }
+                enableDdosProtection: false
+                enableVmProtection: false
+                subnets: [
+                  {
+                    name: subnetName
+                    properties: {
+                      addressPrefix: vnetSubnetPrefix
+                      natGateway: {
+                        id: natGateway.id
+                      }
+                      privateEndpointNetworkPolicies: 'Enabled'
+                      privateLinkServiceNetworkPolicies: 'Enabled'
                     }
                   }
                 ]
