0c65a88d259c93aeaa3fabe62a49c7a7fa5d50ab
jolov <jolov@microsoft.com>
2025-08-25 12:27:18 -0700

    regen

---
+++ eng/packages/http-client-csharp/generator/TestProjects/Spector.Tests/Http/Azure/Core/Scalar/ScalarTests.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using _Specs_.Azure.Core.Scalar;
+using Azure.Core;
+using NUnit.Framework;
+
+namespace TestProjects.Spector.Tests.Http.Azure.Core.Scalar
+{
+    public class AzureCoreScalarTests : SpectorTestBase
+    {
+        [Test]
+        public Task Azure_Core_Scalar_AzureLocation_get() => Test(async (host) =>
+        {
+            var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().GetAsync();
+            Assert.AreEqual(AzureLocation.EastUS, response.Value);
+        });
+        [Test]
+        public Task Azure_Core_Scalar_AzureLocation_put() => Test(async (host) =>
+        {
+            var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().PutAsync(new AzureLocation("eastus"));
+            Assert.AreEqual(204, response.Status);
+        });
+
+        [Test]
+        public Task Azure_Core_Scalar_AzureLocation_post() => Test(async (host) =>
+        {
+            var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().PostAsync(new AzureLocationModel(new AzureLocation("eastus")));
+            Assert.AreEqual(AzureLocation.EastUS, response.Value.Location);
+        });
+        [Test]
+        public Task Azure_Core_Scalar_AzureLocation_header() => Test(async (host) =>
+        {
+            var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().HeaderAsync(AzureLocation.EastUS);
+            Assert.AreEqual(204, response.Status);
+        });
+        [Test]
+        public Task Azure_Core_Scalar_AzureLocation_query() => Test(async (host) =>
+        {
+            var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().QueryAsync(AzureLocation.EastUS);
+            Assert.AreEqual(204, response.Status);
+        });
+    }
+}
