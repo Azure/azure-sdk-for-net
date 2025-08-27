0e7bf1bdf00a1dcecd5c2e9f247806baa3405d83
jolov <jolov@microsoft.com>
2025-08-26 20:07:01 -0700

    scalar and traits tests

--- eng/packages/http-client-csharp/generator/TestProjects/Spector.Tests/Http/Azure/Core/Scalar/ScalarTests.cs
+++ eng/packages/http-client-csharp/generator/TestProjects/Spector.Tests/Http/Azure/Core/Scalar/ScalarTests.cs
@@ -10,32 +10,35 @@ namespace TestProjects.Spector.Tests.Http.Azure.Core.Scalar
 {
     public class AzureCoreScalarTests : SpectorTestBase
     {
-        [Test]
+        [SpectorTest]
         public Task Azure_Core_Scalar_AzureLocation_get() => Test(async (host) =>
         {
             var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().GetAsync();
             Assert.AreEqual(AzureLocation.EastUS, response.Value);
         });
-        [Test]
+
+        [SpectorTest]
         public Task Azure_Core_Scalar_AzureLocation_put() => Test(async (host) =>
         {
             var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().PutAsync(new AzureLocation("eastus"));
             Assert.AreEqual(204, response.Status);
         });
 
-        [Test]
+        [SpectorTest]
         public Task Azure_Core_Scalar_AzureLocation_post() => Test(async (host) =>
         {
             var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().PostAsync(new AzureLocationModel(new AzureLocation("eastus")));
             Assert.AreEqual(AzureLocation.EastUS, response.Value.Location);
         });
-        [Test]
+
+        [SpectorTest]
         public Task Azure_Core_Scalar_AzureLocation_header() => Test(async (host) =>
         {
             var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().HeaderAsync(AzureLocation.EastUS);
             Assert.AreEqual(204, response.Status);
         });
-        [Test]
+
+        [SpectorTest]
         public Task Azure_Core_Scalar_AzureLocation_query() => Test(async (host) =>
         {
             var response = await new ScalarClient(host, null).GetAzureLocationScalarClient().QueryAsync(AzureLocation.EastUS);
