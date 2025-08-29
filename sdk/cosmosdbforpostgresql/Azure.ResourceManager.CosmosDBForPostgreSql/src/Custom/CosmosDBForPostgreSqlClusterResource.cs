46b804138632ec22ae577b85eb8b5e8f0d5acf68
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2025-08-29 16:07:34 +0800

    Update CosmosDBForPostgreSqlClusterResource.cs

--- sdk/cosmosdbforpostgresql/Azure.ResourceManager.CosmosDBForPostgreSql/src/Custom/CosmosDBForPostgreSqlClusterResource.cs
+++ sdk/cosmosdbforpostgresql/Azure.ResourceManager.CosmosDBForPostgreSql/src/Custom/CosmosDBForPostgreSqlClusterResource.cs
@@ -37,7 +37,7 @@ namespace Azure.ResourceManager.CosmosDBForPostgreSql
         /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         [EditorBrowsable(EditorBrowsableState.Never)]
-        public virtual async Task<ArmOperation> PromoteReadReplicaAsync(WaitUntil waitUntil, CancellationToken cancellationToken = default)
+        public virtual async Task<ArmOperation> PromoteReadReplicaAsync(WaitUntil waitUntil, CancellationToken cancellationToken)
         {
             return await PromoteReadReplicaAsync(waitUntil, null, cancellationToken).ConfigureAwait(false);
         }
@@ -66,7 +66,7 @@ namespace Azure.ResourceManager.CosmosDBForPostgreSql
         /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         [EditorBrowsable(EditorBrowsableState.Never)]
-        public virtual ArmOperation PromoteReadReplica(WaitUntil waitUntil, CancellationToken cancellationToken = default)
+        public virtual ArmOperation PromoteReadReplica(WaitUntil waitUntil, CancellationToken cancellationToken)
         {
             return PromoteReadReplica(waitUntil,null, cancellationToken);
         }
