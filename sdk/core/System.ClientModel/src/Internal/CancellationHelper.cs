eadc6e9553f6ebec110ca8eb5ed33737b3969241
Christopher Scott <chriscott@hotmail.com>
2025-08-29 11:41:35 -0500

    Improve the exception message when the client side network timeout is reached

--- sdk/core/System.ClientModel/src/Internal/CancellationHelper.cs
+++ sdk/core/System.ClientModel/src/Internal/CancellationHelper.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.ClientModel.Primitives;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -66,7 +67,8 @@ internal static class CancellationHelper
             throw CreateOperationCanceledException(
                 innerException,
                 timeoutToken,
-                $"The operation was cancelled because it exceeded the configured timeout of {timeout:g}. ");
+                $"The operation was cancelled because it exceeded the configured timeout of {timeout:g}. " +
+                    $"The default network timeout can be adjusted in the client options type via {nameof(ClientPipelineOptions)}.{nameof(ClientPipelineOptions.NetworkTimeout)}");
         }
     }
 }
