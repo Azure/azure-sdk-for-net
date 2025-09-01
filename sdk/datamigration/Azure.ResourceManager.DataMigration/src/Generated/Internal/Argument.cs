4cbc14ba7c0cbc0728fdba8f79d231d7910c1fd5
Amarjeet Kumar <amakum@microsoft.com>
2025-09-02 00:57:36 +0530

    Publish stable version 2025-06-30

--- sdk/datamigration/Azure.ResourceManager.DataMigration/src/Generated/Internal/Argument.cs
+++ sdk/datamigration/Azure.ResourceManager.DataMigration/src/Generated/Internal/Argument.cs
@@ -9,7 +9,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Azure.ResourceManager.DataMigration
+namespace SqlMigrationManagementClient
 {
     internal static class Argument
     {
