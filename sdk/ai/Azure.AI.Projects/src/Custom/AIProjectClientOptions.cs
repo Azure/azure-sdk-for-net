c754cdfcbb6950c76334b1de0fc1fdd6eba81936
trangevi <trangevi@microsoft.com>
2025-09-05 09:33:32 -0700

    Remove Azure.Core constructors. Update tests with new class names after merge from main
    Signed-off-by: trangevi <trangevi@microsoft.com>

--- sdk/ai/Azure.AI.Projects/src/Custom/AIProjectClientOptions.cs
+++ sdk/ai/Azure.AI.Projects/src/Custom/AIProjectClientOptions.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
-using System;
-using Azure.Core;
 using System.ClientModel.Primitives;
 
 namespace Azure.AI.Projects
@@ -13,20 +10,5 @@ namespace Azure.AI.Projects
     {
         /// <summary> The size of the client cache. </summary>
         public int ClientCacheSize { get; set; } = 100;
-
-        private void ValidateAutoFunctions(Dictionary<string, Delegate> delegates)
-        {
-            if (delegates == null || delegates.Count == 0)
-            {
-                throw new InvalidOperationException("The delegate dictionary must have at least one delegate.");
-            }
-            foreach (var kvp in delegates)
-            {
-                if (kvp.Value.Method.ReturnType != typeof(string))
-                {
-                    throw new InvalidOperationException($"The Delegates must have string as return type.");
-                }
-            }
-        }
     }
 }
--- sdk/ai/Azure.AI.Projects/src/Custom/ClientConnectionCacheManager.cs
+++ sdk/ai/Azure.AI.Projects/src/Custom/ClientConnectionCacheManager.cs
@@ -1,28 +1,28 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Concurrent;
-using System.ClientModel.Primitives;
-using System.Collections.Generic;
 using System;
-using Azure.Core;
+using System.ClientModel;
+using System.ClientModel.Primitives;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Azure.AI.Projects
 {
     /// <summary>
     /// Manages connection caching and synchronization.
     /// </summary>
-    internal class ConnectionCacheManager
+    internal class ClientConnectionCacheManager
     {
-        private readonly TokenCredential _tokenCredential;
+        private readonly AuthenticationTokenProvider _tokenProvider;
         private readonly Uri _endpoint;
         private readonly ConcurrentDictionary<ConnectionType, Uri> _connectionCache = new();
         private readonly ConcurrentDictionary<string, ClientConnection> _connections = new();
 
-        public ConnectionCacheManager(Uri endpoint, TokenCredential tokenCredential)
+        public ClientConnectionCacheManager(Uri endpoint, AuthenticationTokenProvider tokenProvider)
         {
             _endpoint = endpoint;
-            _tokenCredential = tokenCredential;
+            _tokenProvider = tokenProvider;
         }
 
         /// <summary>
@@ -43,7 +43,7 @@ namespace Azure.AI.Projects
                 throw new ArgumentException($"The AAD authentication target URI is missing or invalid for {connectionId}.");
             }
 
-            var newConnection = new ClientConnection(connectionId, _endpoint.AbsoluteUri, _tokenCredential, CredentialKind.TokenCredential);
+            var newConnection = new ClientConnection(connectionId, _endpoint.AbsoluteUri, _tokenProvider, CredentialKind.TokenCredential);
             return _connections.GetOrAdd(connectionId, newConnection);
         }
 
