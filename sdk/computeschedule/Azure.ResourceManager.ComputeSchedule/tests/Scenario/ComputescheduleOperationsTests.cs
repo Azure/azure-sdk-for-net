5fd7a669b586005c6e8519599043f2f21cc47bba
Arthur Ma <arthurma@microsoft.com>
2025-09-11 11:58:44 +0800

    update

--- sdk/computeschedule/Azure.ResourceManager.ComputeSchedule/tests/Scenario/ComputescheduleOperationsTests.cs
+++ sdk/computeschedule/Azure.ResourceManager.ComputeSchedule/tests/Scenario/ComputescheduleOperationsTests.cs
@@ -16,8 +16,7 @@ namespace Azure.ResourceManager.ComputeSchedule.Tests.Scenario
     {
         private static readonly int s_cancelOperationsDelayedDays = 5;
         public ComputescheduleOperationsTests(bool isAsync)
-            : base(isAsync, RecordedTestMode.Playback)
-        //, RecordedTestMode.Record)
+            : base(isAsync)
         {
         }
 
