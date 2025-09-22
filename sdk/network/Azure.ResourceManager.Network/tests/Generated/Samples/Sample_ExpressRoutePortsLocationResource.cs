f1b57f8e0e1ca609826a19c0b2c937d89866aa7c
Prajjwal Kamboj <kamboj.prjwl@gmail.com>
2025-09-22 14:49:17 +0530

    .NET Release Network API Version 2024-10-01 (#52727)

--- sdk/network/Azure.ResourceManager.Network/tests/Generated/Samples/Sample_ExpressRoutePortsLocationResource.cs
+++ sdk/network/Azure.ResourceManager.Network/tests/Generated/Samples/Sample_ExpressRoutePortsLocationResource.cs
@@ -19,7 +19,7 @@ namespace Azure.ResourceManager.Network.Samples
         [Ignore("Only validating compilation of examples")]
         public async Task Get_ExpressRoutePortsLocationGet()
         {
-            // Generated from example definition: specification/network/resource-manager/Microsoft.Network/stable/2024-07-01/examples/ExpressRoutePortsLocationGet.json
+            // Generated from example definition: specification/network/resource-manager/Microsoft.Network/stable/2024-10-01/examples/ExpressRoutePortsLocationGet.json
             // this example is just showing the usage of "ExpressRoutePortsLocations_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
