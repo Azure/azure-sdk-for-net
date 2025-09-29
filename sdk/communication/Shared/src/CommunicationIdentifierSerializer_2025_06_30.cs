1936671f408ffd484c7d7115db21e85163c5d3ab
Pilli Vamshi <v-pivamshi@microsoft.com>
2025-09-29 22:40:22 +0530

    removed the import.

--- sdk/communication/Shared/src/CommunicationIdentifierSerializer_2025_06_30.cs
+++ sdk/communication/Shared/src/CommunicationIdentifierSerializer_2025_06_30.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.Communication.CallAutomation;
 using System.Collections.Generic;
 using System.Text.Json;
 
