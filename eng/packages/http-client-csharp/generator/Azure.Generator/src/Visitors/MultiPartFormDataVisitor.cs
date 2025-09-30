109d098520a3c3cc1acb256a78443e4aa9524e41
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2025-09-30 10:07:04 -0700

    Add visitor for MultiPartFormData WriteTo method (#52933)
    * Add visitor for MultiPartFormData WriteTo method
    
    * Include TaskExtensions
    
    * fix
    
    * revert
    
    * regen

---
+++ eng/packages/http-client-csharp/generator/Azure.Generator/src/Visitors/MultiPartFormDataVisitor.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.Pipeline;
+using Microsoft.TypeSpec.Generator.ClientModel;
+using Microsoft.TypeSpec.Generator.ClientModel.Providers;
+using Microsoft.TypeSpec.Generator.Expressions;
+using Microsoft.TypeSpec.Generator.Providers;
+using Microsoft.TypeSpec.Generator.Statements;
+
+namespace Azure.Generator.Visitors
+{
+    internal class MultiPartFormDataVisitor : ScmLibraryVisitor
+    {
+        protected override MethodBodyStatement VisitExpressionStatement(ExpressionStatement statement, MethodProvider method)
+        {
+            if (method is { EnclosingType: MultiPartFormDataBinaryContentDefinition, Signature.Name: "WriteTo" })
+            {
+                // Replaces GetAwaiter().GetResult() with EnsureCompleted() for the sync over async path.
+                if (statement.Expression is InvokeMethodExpression invokeMethod)
+                {
+                    if (invokeMethod.InstanceReference is InvokeMethodExpression innerInvoke)
+                    {
+                        invokeMethod.Update(
+                            instanceReference: innerInvoke.InstanceReference,
+                            methodName: nameof(TaskExtensions.EnsureCompleted),
+                            extensionType: typeof(TaskExtensions));
+                    }
+                }
+            }
+
+            return statement;
+        }
+    }
+}
