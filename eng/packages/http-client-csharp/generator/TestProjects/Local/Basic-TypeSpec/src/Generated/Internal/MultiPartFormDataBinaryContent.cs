cfc19f1cf7fef1dd685ffc36cde11fadfe386fb8
jolov <jolov@microsoft.com>
2025-09-30 13:25:07 -0700

    regen

--- eng/packages/http-client-csharp/generator/TestProjects/Local/Basic-TypeSpec/src/Generated/Internal/MultiPartFormDataBinaryContent.cs
+++ eng/packages/http-client-csharp/generator/TestProjects/Local/Basic-TypeSpec/src/Generated/Internal/MultiPartFormDataBinaryContent.cs
@@ -229,7 +229,9 @@ namespace BasicTypeSpec
 #if NET6_0_OR_GREATER
             _multipartContent.CopyTo(stream, default, cancellationToken);
 #else
+#pragma warning disable AZC0107 // Public asynchronous method shouldn't be called in synchronous scope. Use synchronous version of the method if it is available.
             _multipartContent.CopyToAsync(stream).EnsureCompleted();
+#pragma warning restore AZC0107 // Public asynchronous method shouldn't be called in synchronous scope. Use synchronous version of the method if it is available.
 #endif
         }
 
