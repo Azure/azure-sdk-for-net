0603929d88acad32be97f4d0b4ee4bb871d0ddf7
Ryan Hurey <rhurey@microsoft.com>
2025-09-30 14:04:39 -0700

    Use composition and not inheritance

--- sdk/ai/Azure.AI.VoiceLive/src/Customizations/VoiceLiveSessionResponse.cs
+++ sdk/ai/Azure.AI.VoiceLive/src/Customizations/VoiceLiveSessionResponse.cs
@@ -4,14 +4,60 @@
 #nullable disable
 
 using System;
+using System.ClientModel.Primitives;
 using System.Collections.Generic;
+using System.Text.Json;
 
 namespace Azure.AI.VoiceLive
 {
     /// <summary> Base for session configuration in the response. </summary>
-    public partial class VoiceLiveSessionResponse : VoiceLiveSessionOptions
+    public partial class VoiceLiveSessionResponse
     {
         /// <summary> The agent configuration for the session, if applicable. </summary>
         internal RespondingAgentOptions Agent { get; }
+
+        /// <summary>
+        /// Gets or sets the Voice.
+        /// </summary>
+        public VoiceProvider Voice { get; }
+
+        /// <summary>
+        /// Gets or sets the maximum number of tokens to generate in the response.
+        /// </summary>
+        public MaxResponseOutputTokensOption MaxResponseOutputTokens { get;}
+
+        /// <summary>
+        /// Gets or sets the tool choice strategy for response generation.
+        /// </summary>
+        public ToolChoiceOption ToolChoice { get; }
+
+        [CodeGenMember("TurnDetection")]
+        private BinaryData _turnDetection;
+
+        /// <summary>
+        /// Gets or sets the TurnDetection.
+        /// </summary>
+        public TurnDetection TurnDetection
+        {
+            get
+            {
+                var tdAsString = _turnDetection?.ToString();
+                if (string.IsNullOrEmpty(tdAsString))
+                {
+                    return null;
+                }
+                else if ("null" == tdAsString.ToLower(System.Globalization.CultureInfo.InvariantCulture))
+                {
+                    return new NoTurnDetection();
+                }
+                else
+                {
+                    using (JsonDocument document = JsonDocument.Parse(_turnDetection))
+                    {
+                        return TurnDetection.DeserializeTurnDetection(document.RootElement, new ModelReaderWriterOptions("J"));
+                    }
+                }
+            }
+        }
     }
 }
