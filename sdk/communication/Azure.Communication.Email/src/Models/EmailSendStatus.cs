2eba0edfb72f42441d95c97a7b9c2592fbcda81a
Dominque Douglas <ddouglas@microsoft.com>
2025-10-06 09:37:12 -0700

    updates from review

---
+++ sdk/communication/Azure.Communication.Email/src/Models/EmailSendStatus.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using Azure.Core;
+
+namespace Azure.Communication.Email
+{
+    [CodeGenModel("EmailSendStatus")]
+    [CodeGenSuppress("EmailSendStatus", typeof(string), typeof(string), typeof(string))]
+    public partial struct EmailSendStatus
+    {
+        public static class Values
+        {
+            /// <summary> Canceled </summary>
+            public const string Canceled = "Canceled";
+            /// <summary> Failed </summary>
+            public const string Failed = "Failed";
+            /// <summary> NotStarted </summary>
+            public const string NotStarted = "NotStarted";
+            /// <summary> Running </summary>
+            public const string Running = "Running";
+            /// <summary> Succeeded </summary>
+            public const string Succeeded = "Succeeded";
+        }
+    }
+}
