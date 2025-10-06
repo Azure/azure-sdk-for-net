302bef162a842b1e07225a51b65bbb0ae5ab9dd4
Dominque Douglas <ddouglas@microsoft.com>
2025-10-06 01:16:16 -0700

    update api

--- sdk/communication/Azure.Communication.Email/api/Azure.Communication.Email.netstandard2.0.cs
+++ sdk/communication/Azure.Communication.Email/api/Azure.Communication.Email.netstandard2.0.cs
@@ -36,13 +36,14 @@ namespace Azure.Communication.Email
     }
     public partial class EmailClientOptions : Azure.Core.ClientOptions
     {
-        public EmailClientOptions(Azure.Communication.Email.EmailClientOptions.ServiceVersion version = Azure.Communication.Email.EmailClientOptions.ServiceVersion.V2024_07_01_Preview) { }
+        public EmailClientOptions(Azure.Communication.Email.EmailClientOptions.ServiceVersion version = Azure.Communication.Email.EmailClientOptions.ServiceVersion.V2025_09_01) { }
         public enum ServiceVersion
         {
             V2021_10_01_Preview = 1,
             V2023_01_15_Preview = 2,
             V2023_03_31 = 3,
             V2024_07_01_Preview = 4,
+            V2025_09_01 = 5,
         }
     }
     public partial class EmailContent
