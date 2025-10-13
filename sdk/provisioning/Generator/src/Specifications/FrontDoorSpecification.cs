def8e0832a13f4466824b12ff53bc6cac6827402
Arcturus Zhang <dapzhang@microsoft.com>
2025-10-13 17:59:20 +0800

    update

---
+++ sdk/provisioning/Generator/src/Specifications/FrontDoorSpecification.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Provisioning.Generator.Model;
+
+namespace Azure.Provisioning.Generator.Specifications;
+
+public class FrontDoorSpecification() :
+    Specification("FrontDoor", typeof(FrontDoorSpecification), ignorePropertiesWithoutPath: true)
+{
+    protected override void Customize()
+    {
+    }
+}
