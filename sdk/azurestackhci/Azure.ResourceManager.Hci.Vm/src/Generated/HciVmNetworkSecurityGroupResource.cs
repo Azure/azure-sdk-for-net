c4a3f1e463980abf840cefc258fca81c38cbb829
Wei Hu <huwe@microsoft.com>
2025-10-13 14:55:23 +0800

    rege

--- sdk/azurestackhci/Azure.ResourceManager.Hci.Vm/src/Generated/HciVmNetworkSecurityGroupResource.cs
+++ sdk/azurestackhci/Azure.ResourceManager.Hci.Vm/src/Generated/HciVmNetworkSecurityGroupResource.cs
@@ -565,5 +565,31 @@ namespace Azure.ResourceManager.Hci.Vm
         {
             return GetCachedClient(client => new HciVmSecurityRuleCollection(client, Id));
         }
+
+        /// <summary> Gets the specified security rule. </summary>
+        /// <param name="securityRuleName"> Name of the security rule. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="securityRuleName"/> is null. </exception>
+        /// <exception cref="ArgumentException"> <paramref name="securityRuleName"/> is an empty string, and was expected to be non-empty. </exception>
+        [ForwardsClientCalls]
+        public virtual async Task<Response<HciVmSecurityRuleResource>> GetHciVmSecurityRuleAsync(string securityRuleName, CancellationToken cancellationToken = default)
+        {
+            Argument.AssertNotNullOrEmpty(securityRuleName, nameof(securityRuleName));
+
+            return await GetHciVmSecurityRules().GetAsync(securityRuleName, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary> Gets the specified security rule. </summary>
+        /// <param name="securityRuleName"> Name of the security rule. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="securityRuleName"/> is null. </exception>
+        /// <exception cref="ArgumentException"> <paramref name="securityRuleName"/> is an empty string, and was expected to be non-empty. </exception>
+        [ForwardsClientCalls]
+        public virtual Response<HciVmSecurityRuleResource> GetHciVmSecurityRule(string securityRuleName, CancellationToken cancellationToken = default)
+        {
+            Argument.AssertNotNullOrEmpty(securityRuleName, nameof(securityRuleName));
+
+            return GetHciVmSecurityRules().Get(securityRuleName, cancellationToken);
+        }
     }
 }
