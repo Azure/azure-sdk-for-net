2ae50b8a346ad928fe1f3f919db3e271d8eb48a5
Arcturus Zhang <dapzhang@microsoft.com>
2025-10-15 16:22:49 +0800

    initial commit

---
+++ eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Visitors/WirePathVisitor.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Generator.Management.Visitors
+{
+    internal class WirePathVisitor
+    {
+    }
+}
