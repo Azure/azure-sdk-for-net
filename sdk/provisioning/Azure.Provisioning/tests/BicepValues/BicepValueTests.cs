8e2ba5fca352095b94eab73ae5a9885e6fd3d4ba
Arcturus Zhang <dapzhang@microsoft.com>
2025-10-20 17:09:33 +0800

    align the style of tests

--- sdk/provisioning/Azure.Provisioning/tests/BicepValues/BicepValueTests.cs
+++ sdk/provisioning/Azure.Provisioning/tests/BicepValues/BicepValueTests.cs
@@ -15,39 +15,34 @@ public class BicepValueTests
     public void ValidateLiteralBicepValue()
     {
         // string value
-        AssertExpression("'test'", new BicepValue<string>("test"));
+        TestHelpers.AssertExpression("'test'", new BicepValue<string>("test"));
 
         // int value
-        AssertExpression("42", new BicepValue<int>(42));
-        AssertExpression("-42", new BicepValue<int>(-42));
+        TestHelpers.AssertExpression("42", new BicepValue<int>(42));
+        TestHelpers.AssertExpression("-42", new BicepValue<int>(-42));
 
         // long value
-        AssertExpression("42", new BicepValue<long>(42L));
-        AssertExpression("2147483647", new BicepValue<long>(2147483647L));
-        AssertExpression("json('2147483648')", new BicepValue<long>(2147483648));
-        AssertExpression("-2147483648", new BicepValue<long>(-2147483648L));
-        AssertExpression("json('-2147483649')", new BicepValue<long>(-2147483649));
-        AssertExpression("json('9223372036854775807')", new BicepValue<long>(9223372036854775807));
+        TestHelpers.AssertExpression("42", new BicepValue<long>(42L));
+        TestHelpers.AssertExpression("2147483647", new BicepValue<long>(2147483647L));
+        TestHelpers.AssertExpression("json('2147483648')", new BicepValue<long>(2147483648));
+        TestHelpers.AssertExpression("-2147483648", new BicepValue<long>(-2147483648L));
+        TestHelpers.AssertExpression("json('-2147483649')", new BicepValue<long>(-2147483649));
+        TestHelpers.AssertExpression("json('9223372036854775807')", new BicepValue<long>(9223372036854775807));
 
         // bool value
-        AssertExpression("true", new BicepValue<bool>(true));
-        AssertExpression("false", new BicepValue<bool>(false));
+        TestHelpers.AssertExpression("true", new BicepValue<bool>(true));
+        TestHelpers.AssertExpression("false", new BicepValue<bool>(false));
 
         // double value
-        AssertExpression("json('3.14')", new BicepValue<double>(3.14));
-        AssertExpression("json('-3.14')", new BicepValue<double>(-3.14));
+        TestHelpers.AssertExpression("json('3.14')", new BicepValue<double>(3.14));
+        TestHelpers.AssertExpression("json('-3.14')", new BicepValue<double>(-3.14));
         // double value with whole numbers
-        AssertExpression("314", new BicepValue<double>(314d));
-        AssertExpression("2147483647", new BicepValue<double>(2147483647d));
-        AssertExpression("json('2147483648')", new BicepValue<double>(2147483648d));
-        AssertExpression("-2147483647", new BicepValue<double>(-2147483647d));
-        AssertExpression("-2147483648", new BicepValue<double>(-2147483648d));
-        AssertExpression("json('-2147483649')", new BicepValue<double>(-2147483649d));
-
-        static void AssertExpression(string expected, BicepValue bicepValue)
-        {
-            Assert.AreEqual(expected, bicepValue.ToString());
-        }
+        TestHelpers.AssertExpression("314", new BicepValue<double>(314d));
+        TestHelpers.AssertExpression("2147483647", new BicepValue<double>(2147483647d));
+        TestHelpers.AssertExpression("json('2147483648')", new BicepValue<double>(2147483648d));
+        TestHelpers.AssertExpression("-2147483647", new BicepValue<double>(-2147483647d));
+        TestHelpers.AssertExpression("-2147483648", new BicepValue<double>(-2147483648d));
+        TestHelpers.AssertExpression("json('-2147483649')", new BicepValue<double>(-2147483649d));
     }
 
     [Test]
