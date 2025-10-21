acc9cc717bd31236ad4012e35b338227f55ec4a1
Wei Hu <huwe@microsoft.com>
2025-10-21 12:08:21 +0800

    fix build

--- eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Primitives/NewManagementProjectScaffolding.cs
+++ eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Primitives/NewManagementProjectScaffolding.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 using Azure.Generator.Primitives;
 using Microsoft.TypeSpec.Generator.Primitives;
