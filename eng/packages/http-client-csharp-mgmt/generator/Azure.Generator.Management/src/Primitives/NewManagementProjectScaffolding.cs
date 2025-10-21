736eb890d70debf5c74d89daefa402f7a581d109
Wei Hu <huwe@microsoft.com>
2025-10-21 12:05:30 +0800

    Skip shared/arm files during scaffolding, it has been handled in central place

--- eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Primitives/NewManagementProjectScaffolding.cs
+++ eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Primitives/NewManagementProjectScaffolding.cs
@@ -9,24 +9,7 @@ namespace Azure.Generator.Management.Primitives
 {
     internal class NewManagementProjectScaffolding : NewAzureProjectScaffolding
     {
-        private const string SharedArmLinkBase = "Shared/Arm";
-
-        private const string RelativeArmSegment = "sdk/resourcemanager/Azure.ResourceManager/src/Shared/";
-
         protected override IReadOnlyList<CSharpProjectCompileInclude> BuildCompileIncludes()
-        {
-            var compileIncludes = new List<CSharpProjectCompileInclude>();
-            foreach (var armShareFile in armSharedFiles)
-            {
-                compileIncludes.Add(new CSharpProjectCompileInclude(GetCompileInclude(armShareFile, RelativeArmSegment), SharedArmLinkBase));
-            }
-
-            return compileIncludes;
-        }
-
-        private static IReadOnlyList<string> armSharedFiles = new List<string>
-        {
-            "SharedExtensions.cs"
-        };
+            => new List<CSharpProjectCompileInclude>();
     }
 }
