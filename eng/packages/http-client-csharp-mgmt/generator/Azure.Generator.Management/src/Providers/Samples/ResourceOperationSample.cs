ceb0cf46fb6b8bd9b9c24b0171c459b4e44950ac
Arcturus Zhang <dapzhang@microsoft.com>
2025-10-22 13:52:05 +0800

    some attempts

--- eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Providers/Samples/ResourceOperationSample.cs
+++ eng/packages/http-client-csharp-mgmt/generator/Azure.Generator.Management/src/Providers/Samples/ResourceOperationSample.cs
@@ -1,8 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Azure.Generator.Management.Primitives;
+using Microsoft.TypeSpec.Generator.Input;
+using Microsoft.TypeSpec.Generator.Providers;
+
 namespace Azure.Generator.Management.Providers.Samples;
 
 internal class ResourceOperationSample
 {
+    public ResourceOperationSample(TypeProvider owner, ServiceMethodProvider serviceMethod, InputOperation inputOperation, InputOperationExample inputOperationExample)
+    {
+    }
 }
