c5748eb92c4a1a0632c574649bd1ade9640e8181
wangamber <wangamber@microsoft.com>
2025-10-23 17:06:38 -0700

    Add missing 'using Autorest.CSharp.Core' directive to BatchTranscriptionClient

--- sdk/cognitiveservices/Azure.AI.Speech.BatchTranscription/src/Custom/BatchTranscriptionClient.cs
+++ sdk/cognitiveservices/Azure.AI.Speech.BatchTranscription/src/Custom/BatchTranscriptionClient.cs
@@ -1,5 +1,7 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
+using Autorest.CSharp.Core;
 
 using Azure.Core;
 using System.Threading.Tasks;
