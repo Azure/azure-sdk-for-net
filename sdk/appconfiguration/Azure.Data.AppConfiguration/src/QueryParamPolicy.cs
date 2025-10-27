287571bd60ba430cfddc3be7f69d536ae3419519
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-10-27 11:37:40 +0800

    catch UriFormatException

--- sdk/appconfiguration/Azure.Data.AppConfiguration/src/QueryParamPolicy.cs
+++ sdk/appconfiguration/Azure.Data.AppConfiguration/src/QueryParamPolicy.cs
@@ -20,92 +20,99 @@ namespace Azure.Data.AppConfiguration
 
         public override void OnSendingRequest(HttpMessage message)
         {
-            Uri originalUri = message.Request.Uri.ToUri();
-            string queryString = originalUri.Query;
-
-            if (string.IsNullOrEmpty(queryString))
+            try
             {
-                return;
-            }
+                Uri originalUri = message.Request.Uri.ToUri();
+                string queryString = originalUri.Query;
 
-            ReadOnlyMemory<char> queryMemory = queryString.AsMemory();
-
-            if (queryMemory.Span[0] == '?')
-            {
-                queryMemory = queryMemory.Slice(1);
-            }
-
-            var segments = new List<QueryParameterEntry>();
-            int startIndex = 0;
-
-            for (int i = 0; i < queryMemory.Length; i++)
-            {
-                if (queryMemory.Span[i] == '&')
+                if (string.IsNullOrEmpty(queryString))
                 {
-                    if (i > startIndex)
+                    return;
+                }
+
+                ReadOnlyMemory<char> queryMemory = queryString.AsMemory();
+
+                if (queryMemory.Span[0] == '?')
+                {
+                    queryMemory = queryMemory.Slice(1);
+                }
+
+                var segments = new List<QueryParameterEntry>();
+                int startIndex = 0;
+
+                for (int i = 0; i < queryMemory.Length; i++)
+                {
+                    if (queryMemory.Span[i] == '&')
                     {
-                        AddParameter(queryMemory.Slice(startIndex, i - startIndex), segments);
+                        if (i > startIndex)
+                        {
+                            AddParameter(queryMemory.Slice(startIndex, i - startIndex), segments);
+                        }
+                        startIndex = i + 1;
                     }
-                    startIndex = i + 1;
                 }
-            }
 
-            // Add the last parameter
-            if (startIndex < queryMemory.Length)
-            {
-                AddParameter(queryMemory.Slice(startIndex), segments);
-            }
-
-            // List<T>.Sort method is not stable. See the remark section: https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.sort
-            // Sort by lowercase name, preserving relative order for duplicates
-            segments.Sort((a, b) =>
-            {
-                var nameComparison = MemoryExtensions.CompareTo(
-                    a.Name.Span,
-                    b.Name.Span,
-                    StringComparison.OrdinalIgnoreCase);
-
-                if (nameComparison != 0)
+                // Add the last parameter
+                if (startIndex < queryMemory.Length)
                 {
-                    return nameComparison;
+                    AddParameter(queryMemory.Slice(startIndex), segments);
                 }
-                return a.Index.CompareTo(b.Index);
-            });
 
-            var sb = new StringBuilder();
-            sb.Append(originalUri.Scheme);
-            sb.Append("://");
-            sb.Append(originalUri.Host);
-            if (!originalUri.IsDefaultPort)
-            {
-                sb.Append(':');
-                sb.Append(originalUri.Port);
-            }
-            sb.Append(originalUri.AbsolutePath);
-            sb.Append('?');
-
-            for (int i = 0; i < segments.Count; i++)
-            {
-                if (i > 0)
+                // List<T>.Sort method is not stable. See the remark section: https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.sort
+                // Sort by lowercase name, preserving relative order for duplicates
+                segments.Sort((a, b) =>
                 {
-                    sb.Append('&');
+                    var nameComparison = MemoryExtensions.CompareTo(
+                        a.Name.Span,
+                        b.Name.Span,
+                        StringComparison.OrdinalIgnoreCase);
+
+                    if (nameComparison != 0)
+                    {
+                        return nameComparison;
+                    }
+                    return a.Index.CompareTo(b.Index);
+                });
+
+                var sb = new StringBuilder();
+                sb.Append(originalUri.Scheme);
+                sb.Append("://");
+                sb.Append(originalUri.Host);
+                if (!originalUri.IsDefaultPort)
+                {
+                    sb.Append(':');
+                    sb.Append(originalUri.Port);
+                }
+                sb.Append(originalUri.AbsolutePath);
+                sb.Append('?');
+
+                for (int i = 0; i < segments.Count; i++)
+                {
+                    if (i > 0)
+                    {
+                        sb.Append('&');
+                    }
+
+                    sb.Append(segments[i].Name.ToString().ToLowerInvariant());
+                    sb.Append('=');
+                    sb.Append(segments[i].Value);
                 }
 
-                sb.Append(segments[i].Name.ToString().ToLowerInvariant());
-                sb.Append('=');
-                sb.Append(segments[i].Value);
+                if (!string.IsNullOrEmpty(originalUri.Fragment))
+                {
+                    sb.Append(originalUri.Fragment);
+                }
+
+                string newUrl = sb.ToString();
+
+                if (newUrl != message.Request.Uri.ToString())
+                {
+                    message.Request.Uri.Reset(new Uri(newUrl));
+                }
             }
-
-            if (!string.IsNullOrEmpty(originalUri.Fragment))
+            catch (UriFormatException ex)
             {
-                sb.Append(originalUri.Fragment);
-            }
-
-            string newUrl = sb.ToString();
-
-            if (newUrl != message.Request.Uri.ToString())
-            {
-                message.Request.Uri.Reset(new Uri(newUrl));
+                System.Diagnostics.Debug.WriteLine($"QueryParamPolicy failed to normalize the request query parameters: {ex}");
             }
         }
 
