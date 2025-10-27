fbbfd943b748fd09af3909b029376a0f7024b43f
azure-sdk <azuresdk@microsoft.com>
2025-10-27 03:59:58 +0000

    Update SDK code se_wo_4

--- sdk/trustedsigning/Azure.ResourceManager.TrustedSigning/src/Generated/TrustedSigningCertificateProfileResource.cs
+++ sdk/trustedsigning/Azure.ResourceManager.TrustedSigning/src/Generated/TrustedSigningCertificateProfileResource.cs
@@ -260,7 +260,7 @@ namespace Azure.ResourceManager.TrustedSigning
             }
         }
 
-        /// <summary> Create a certificate profile. </summary>
+        /// <summary> Update a TrustedSigningCertificateProfile. </summary>
         /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
         /// <param name="data"> Parameters to create the certificate profile. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
@@ -299,7 +299,7 @@ namespace Azure.ResourceManager.TrustedSigning
             }
         }
 
-        /// <summary> Create a certificate profile. </summary>
+        /// <summary> Update a TrustedSigningCertificateProfile. </summary>
         /// <param name="waitUntil"> <see cref="WaitUntil.Completed"/> if the method should wait to return until the long-running operation has completed on the service; <see cref="WaitUntil.Started"/> if it should return after starting the operation. For more information on long-running operations, please see <see href="https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/LongRunningOperations.md"> Azure.Core Long-Running Operation samples</see>. </param>
         /// <param name="data"> Parameters to create the certificate profile. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
