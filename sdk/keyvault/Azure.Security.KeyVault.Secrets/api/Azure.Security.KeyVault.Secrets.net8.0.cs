26e7e35bac0a2becd1c58e7734ec0f6af0fe1fc6
Daniel Jurek <djurek@microsoft.com>
2025-10-28 12:38:42 -0700

    public api surface

--- sdk/keyvault/Azure.Security.KeyVault.Secrets/api/Azure.Security.KeyVault.Secrets.net8.0.cs
+++ sdk/keyvault/Azure.Security.KeyVault.Secrets/api/Azure.Security.KeyVault.Secrets.net8.0.cs
@@ -24,8 +24,8 @@ namespace Azure.Security.KeyVault.Secrets
     {
         public KeyVaultSecret(string name, string value) { }
         public System.Uri Id { get { throw null; } }
-        public string ImpropppppperlySepledProperyyyy { get { throw null; } }
         public string Name { get { throw null; } }
+        public string ProperlySpelledProperty { get { throw null; } }
         public Azure.Security.KeyVault.Secrets.SecretProperties Properties { get { throw null; } }
         public string Value { get { throw null; } }
     }
