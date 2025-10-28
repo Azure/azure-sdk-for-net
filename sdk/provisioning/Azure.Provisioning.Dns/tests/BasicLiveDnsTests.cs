c2a4f2ef2e3ca7b85b3a5678b3ec135154872ad3
Arcturus Zhang <dapzhang@microsoft.com>
2025-10-28 15:46:00 +0800

    add the live tests

---
+++ sdk/provisioning/Azure.Provisioning.Dns/tests/BasicLiveDnsTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Azure.Provisioning.Tests;
+using NUnit.Framework;
+
+namespace Azure.Provisioning.Dns.Tests;
+
+internal class BasicLiveDnsTests(bool async)
+    : ProvisioningTestBase(async /*, skipTools: true, skipLiveCalls: true */)
+{
+    [Test]
+    [Description("https://github.com/Azure/azure-quickstart-templates/blob/master/quickstarts/microsoft.network/azure-dns-new-zone/main.bicep")]
+    public async Task CreateAzureDnsNewZone()
+    {
+        await using Trycep test = BasicDnsTests.CreateAzureDnsNewZoneTest();
+        await test.SetupLiveCalls(this)
+            .Lint()
+            .ValidateAsync();
+    }
+}
