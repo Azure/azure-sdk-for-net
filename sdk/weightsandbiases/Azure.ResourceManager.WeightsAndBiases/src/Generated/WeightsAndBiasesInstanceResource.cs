c11403645aa96dad1030021a29e02ce373b13c8b
azure-sdk <azuresdk@microsoft.com>
2025-10-29 07:02:02 +0000

    Update SDK code se_wo_4

--- sdk/weightsandbiases/Azure.ResourceManager.WeightsAndBiases/src/Generated/WeightsAndBiasesInstanceResource.cs
+++ sdk/weightsandbiases/Azure.ResourceManager.WeightsAndBiases/src/Generated/WeightsAndBiasesInstanceResource.cs
@@ -189,7 +189,27 @@ namespace Azure.ResourceManager.WeightsAndBiases
             }
         }
 
-        /// <summary> Update a InstanceResource. </summary>
+        /// <summary>
+        /// Update a InstanceResource
+        /// <list type="bullet">
+        /// <item>
+        /// <term> Request Path. </term>
+        /// <description> /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.WeightsAndBiases/instances/{instancename}. </description>
+        /// </item>
+        /// <item>
+        /// <term> Operation Id. </term>
+        /// <description> Update. </description>
+        /// </item>
+        /// <item>
+        /// <term> Default Api Version. </term>
+        /// <description> 2024-09-18. </description>
+        /// </item>
+        /// <item>
+        /// <term> Resource. </term>
+        /// <description> <see cref="WeightsAndBiasesInstanceResource"/>. </description>
+        /// </item>
+        /// </list>
+        /// </summary>
         /// <param name="patch"> The resource properties to be updated. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="patch"/> is null. </exception>
@@ -221,7 +241,27 @@ namespace Azure.ResourceManager.WeightsAndBiases
             }
         }
 
-        /// <summary> Update a InstanceResource. </summary>
+        /// <summary>
+        /// Update a InstanceResource
+        /// <list type="bullet">
+        /// <item>
+        /// <term> Request Path. </term>
+        /// <description> /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.WeightsAndBiases/instances/{instancename}. </description>
+        /// </item>
+        /// <item>
+        /// <term> Operation Id. </term>
+        /// <description> Update. </description>
+        /// </item>
+        /// <item>
+        /// <term> Default Api Version. </term>
+        /// <description> 2024-09-18. </description>
+        /// </item>
+        /// <item>
+        /// <term> Resource. </term>
+        /// <description> <see cref="WeightsAndBiasesInstanceResource"/>. </description>
+        /// </item>
+        /// </list>
+        /// </summary>
         /// <param name="patch"> The resource properties to be updated. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="patch"/> is null. </exception>
