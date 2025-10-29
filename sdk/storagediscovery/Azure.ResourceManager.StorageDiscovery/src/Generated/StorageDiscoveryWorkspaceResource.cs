7558ef04743a5e40cc087514bfdd186d34030997
azure-sdk <azuresdk@microsoft.com>
2025-10-29 10:15:03 +0000

    Update SDK code se_wo_4

--- sdk/storagediscovery/Azure.ResourceManager.StorageDiscovery/src/Generated/StorageDiscoveryWorkspaceResource.cs
+++ sdk/storagediscovery/Azure.ResourceManager.StorageDiscovery/src/Generated/StorageDiscoveryWorkspaceResource.cs
@@ -189,7 +189,27 @@ namespace Azure.ResourceManager.StorageDiscovery
             }
         }
 
-        /// <summary> Update a StorageDiscoveryWorkspace. </summary>
+        /// <summary>
+        /// Update a StorageDiscoveryWorkspace
+        /// <list type="bullet">
+        /// <item>
+        /// <term> Request Path. </term>
+        /// <description> /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.StorageDiscovery/storageDiscoveryWorkspaces/{storageDiscoveryWorkspaceName}. </description>
+        /// </item>
+        /// <item>
+        /// <term> Operation Id. </term>
+        /// <description> Update. </description>
+        /// </item>
+        /// <item>
+        /// <term> Default Api Version. </term>
+        /// <description> 2025-09-01. </description>
+        /// </item>
+        /// <item>
+        /// <term> Resource. </term>
+        /// <description> <see cref="StorageDiscoveryWorkspaceResource"/>. </description>
+        /// </item>
+        /// </list>
+        /// </summary>
         /// <param name="patch"> The resource properties to be updated. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="patch"/> is null. </exception>
@@ -221,7 +241,27 @@ namespace Azure.ResourceManager.StorageDiscovery
             }
         }
 
-        /// <summary> Update a StorageDiscoveryWorkspace. </summary>
+        /// <summary>
+        /// Update a StorageDiscoveryWorkspace
+        /// <list type="bullet">
+        /// <item>
+        /// <term> Request Path. </term>
+        /// <description> /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.StorageDiscovery/storageDiscoveryWorkspaces/{storageDiscoveryWorkspaceName}. </description>
+        /// </item>
+        /// <item>
+        /// <term> Operation Id. </term>
+        /// <description> Update. </description>
+        /// </item>
+        /// <item>
+        /// <term> Default Api Version. </term>
+        /// <description> 2025-09-01. </description>
+        /// </item>
+        /// <item>
+        /// <term> Resource. </term>
+        /// <description> <see cref="StorageDiscoveryWorkspaceResource"/>. </description>
+        /// </item>
+        /// </list>
+        /// </summary>
         /// <param name="patch"> The resource properties to be updated. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="patch"/> is null. </exception>
