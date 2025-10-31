767cff7bc38e7c7a478c4871335605eed1659a5a
jolov <jolov@microsoft.com>
2025-10-31 15:23:07 -0700

    Fix docs

--- sdk/eventgrid/Azure.Messaging.EventGrid.Namespaces/src/Customization/EventGridSenderClientOptions.cs
+++ sdk/eventgrid/Azure.Messaging.EventGrid.Namespaces/src/Customization/EventGridSenderClientOptions.cs
@@ -8,7 +8,7 @@ using Azure.Core;
 
 namespace Azure.Messaging.EventGrid.Namespaces
 {
-    /// <summary> Client options for clients in this library. </summary>
+    /// <summary> Client options for <see cref="EventGridSenderClient"/>. </summary>
     public partial class EventGridSenderClientOptions : ClientOptions
     {
         private const ServiceVersion LatestVersion = ServiceVersion.V2024_06_01;
