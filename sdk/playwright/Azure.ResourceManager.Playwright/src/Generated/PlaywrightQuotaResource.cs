e83a993f92c1c6345d3fdff822b1790cc94978de
azure-sdk <azuresdk@microsoft.com>
2025-10-31 03:10:00 +0000

    Update SDK code mo_sc_3

--- sdk/playwright/Azure.ResourceManager.Playwright/src/Generated/PlaywrightQuotaResource.cs
+++ sdk/playwright/Azure.ResourceManager.Playwright/src/Generated/PlaywrightQuotaResource.cs
@@ -101,7 +101,7 @@ namespace Azure.ResourceManager.Playwright
         /// </item>
         /// <item>
         /// <term> Operation Id. </term>
-        /// <description> Get. </description>
+        /// <description> PlaywrightQuotas_Get. </description>
         /// </item>
         /// <item>
         /// <term> Default Api Version. </term>
@@ -149,7 +149,7 @@ namespace Azure.ResourceManager.Playwright
         /// </item>
         /// <item>
         /// <term> Operation Id. </term>
-        /// <description> Get. </description>
+        /// <description> PlaywrightQuotas_Get. </description>
         /// </item>
         /// <item>
         /// <term> Default Api Version. </term>
