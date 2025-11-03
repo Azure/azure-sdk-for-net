1eaee0cf3fb299ac271732cac58a165d39ebb561
jolov <jolov@microsoft.com>
2025-11-03 14:24:49 -0800

    Update interface

--- sdk/core/Azure.Core/src/Shared/IOperationSource.cs
+++ sdk/core/Azure.Core/src/Shared/IOperationSource.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,7 +9,12 @@ namespace Azure.Core
 {
     internal interface IOperationSource<T>
     {
+        [RequiresDynamicCode("This method uses reflection.")]
+        [RequiresUnreferencedCode("This method uses reflection.")]
         T CreateResult(Response response, CancellationToken cancellationToken);
+
+        [RequiresDynamicCode("This method uses reflection.")]
+        [RequiresUnreferencedCode("This method uses reflection.")]
         ValueTask<T> CreateResultAsync(Response response, CancellationToken cancellationToken);
     }
 }
