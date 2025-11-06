11b3901119900814eeba5bfa53eb897c218554d0
Qi Yin <v-qiyin1@microsoft.com>
2025-11-06 17:16:58 +0800

    fix build error,remove unused csproj file

--- sdk/cognitivelanguage/Azure.AI.Language.QuestionAnswering.Inference/src/QuestionAnsweringClient.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.QuestionAnswering.Inference/src/QuestionAnsweringClient.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
+using Azure.Core.Pipeline;
 
 namespace Azure.AI.Language.QuestionAnswering
 {
--- sdk/cognitivelanguage/Azure.AI.Language.QuestionAnswering.Inference/tests/InferenceClientTestEnvironment.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.QuestionAnswering.Inference/tests/InferenceClientTestEnvironment.cs
@@ -1,14 +1,36 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Azure.Core.TestFramework;
 
 namespace Azure.AI.Language.QuestionAnswering.Inference.Tests
 {
     public class InferenceClientTestEnvironment : TestEnvironment
     {
-        public string Endpoint => GetRecordedVariable("Inference_ENDPOINT");
+        /// <summary>
+        /// Gets the master API key.
+        /// </summary>
+        public string ApiKey => GetRecordedVariable("QUESTIONANSWERING_KEY", options => options.IsSecret());
 
-        // Add other client paramters here as above.
+        /// <summary>
+        /// Gets a <see cref="QuestionAnsweringProject"/> using the <see cref="ProjectName"/> and <see cref="DeploymentName"/>.
+        /// </summary>
+        public QuestionAnsweringProject Project => new QuestionAnsweringProject(ProjectName, DeploymentName);
+
+        /// <summary>
+        /// Gets the primary test project name.
+        /// </summary>
+        public string ProjectName => GetRecordedVariable("QUESTIONANSWERING_PROJECT");
+
+        /// <summary>
+        /// Gets the deployment name.
+        /// </summary>
+        public string DeploymentName => "test";
+
+        /// <summary>
+        /// Gets the endpoint.
+        /// </summary>
+        public Uri Endpoint => new(GetRecordedVariable("QUESTIONANSWERING_ENDPOINT"), UriKind.Absolute);
     }
 }
