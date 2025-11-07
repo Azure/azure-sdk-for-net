5cfe0907f600219d5ec50fa0792941f567dee270
Maddy Redding Heaps <mredding@microsoft.com>
2025-11-06 16:43:03 -0800

    add todos

---
+++ eng/scripts/azsdkcli/RunCIChecks.ps1
@@ -0,0 +1,7 @@
+# TODO
+# Handle CI calls
+# Takes input for which calls to perform (TBD)
+# Takes in a package path and parses necessary information
+# calls sub scripts in CIChecksHelpers
+# outputs LLM friendly summary of results
+# Takes care of logging, unexpected errors, etc
---
+++ eng/scripts/azsdkcli/UpdateMetadata.ps1
@@ -0,0 +1,6 @@
+# TODO
+# Handle update metadata
+# Takes in a package path and parses necessary information
+# calls export api script
+# outputs LLM friendly summary of results
+# Takes care of logging, unexpected errors, etc
