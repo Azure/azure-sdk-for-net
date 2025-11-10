6bed6770b574e195a72325f9395d4781508c97e2
junanchen <junanchen@microsoft.com>
2025-11-09 20:23:14 -0800

    revert Language-Settings.ps1

--- eng/scripts/Language-Settings.ps1
+++ eng/scripts/Language-Settings.ps1
@@ -27,7 +27,6 @@ function Get-AllPackageInfoFromRepo($serviceDirectory)
     /p:ServiceDirectory=$serviceDirectory `
     /p:AddDevVersion=$shouldAddDevVersion `
     /p:OutputProjectInfoListFilePath="$outputFilePath" `
-    -v:n `
     -tl:off | Out-Host
 
   # Check if msbuild succeeded
