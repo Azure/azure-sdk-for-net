81cbd3b2dd3155c91e369362a77b51c62f6b9d27
Arcturus Zhang <dapzhang@microsoft.com>
2025-11-11 10:35:29 +0800

    update commit id

--- sdk/resourceconnector/Azure.ResourceManager.ResourceConnector/tests/Generated/Samples/Sample_ResourceConnectorApplianceCollection.cs
+++ sdk/resourceconnector/Azure.ResourceManager.ResourceConnector/tests/Generated/Samples/Sample_ResourceConnectorApplianceCollection.cs
@@ -21,7 +21,7 @@ namespace Azure.ResourceManager.ResourceConnector.Samples
         [Ignore("Only validating compilation of examples")]
         public async Task CreateOrUpdate_CreateUpdateAppliance()
         {
-            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/stable/2022-10-27/examples/AppliancesCreate_Update.json
+            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/ResourceConnector/stable/2022-10-27/examples/AppliancesCreate_Update.json
             // this example is just showing the usage of "Appliances_CreateOrUpdate" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -60,7 +60,7 @@ namespace Azure.ResourceManager.ResourceConnector.Samples
         [Ignore("Only validating compilation of examples")]
         public async Task Get_GetAppliance()
         {
-            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/stable/2022-10-27/examples/AppliancesGet.json
+            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/ResourceConnector/stable/2022-10-27/examples/AppliancesGet.json
             // this example is just showing the usage of "Appliances_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -93,7 +93,7 @@ namespace Azure.ResourceManager.ResourceConnector.Samples
         [Ignore("Only validating compilation of examples")]
         public async Task GetAll_ListAppliancesByResourceGroup()
         {
-            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/stable/2022-10-27/examples/AppliancesListByResourceGroup.json
+            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/ResourceConnector/stable/2022-10-27/examples/AppliancesListByResourceGroup.json
             // this example is just showing the usage of "Appliances_ListByResourceGroup" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -128,7 +128,7 @@ namespace Azure.ResourceManager.ResourceConnector.Samples
         [Ignore("Only validating compilation of examples")]
         public async Task Exists_GetAppliance()
         {
-            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/stable/2022-10-27/examples/AppliancesGet.json
+            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/ResourceConnector/stable/2022-10-27/examples/AppliancesGet.json
             // this example is just showing the usage of "Appliances_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
@@ -157,7 +157,7 @@ namespace Azure.ResourceManager.ResourceConnector.Samples
         [Ignore("Only validating compilation of examples")]
         public async Task GetIfExists_GetAppliance()
         {
-            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/stable/2022-10-27/examples/AppliancesGet.json
+            // Generated from example definition: specification/resourceconnector/resource-manager/Microsoft.ResourceConnector/ResourceConnector/stable/2022-10-27/examples/AppliancesGet.json
             // this example is just showing the usage of "Appliances_Get" operation, for the dependent resources, they will have to be created separately.
 
             // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
