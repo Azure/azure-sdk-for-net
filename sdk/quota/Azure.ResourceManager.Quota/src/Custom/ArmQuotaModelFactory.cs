ebad0c70b36e0ac2a2c0849e93ebc25882d9da9e
Wei Hu <huwe@microsoft.com>
2025-11-12 10:25:03 +0800

    custom code

---
+++ sdk/quota/Azure.ResourceManager.Quota/src/Custom/ArmQuotaModelFactory.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System.ComponentModel;
+
+namespace Azure.ResourceManager.Quota.Models
+{
+    public partial class ArmQuotaModelFactory
+    {
+        /// <summary> Initializes a new instance of <see cref="T:Azure.ResourceManager.Quota.Models.GroupQuotasEntityProperties" />. </summary>
+        /// <param name="displayName"> Display name of the GroupQuota entity. </param>
+        /// <param name="provisioningState"> Provisioning state of the operation. </param>
+        /// <returns> A new <see cref="T:Azure.ResourceManager.Quota.Models.GroupQuotasEntityProperties" /> instance for mocking. </returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static GroupQuotasEntityProperties GroupQuotasEntityProperties(string displayName, QuotaRequestStatus? provisioningState)
+        {
+            return GroupQuotasEntityProperties(displayName: displayName, groupType: default, provisioningState: provisioningState);
+        }
+    }
+}
---
+++ sdk/quota/Azure.ResourceManager.Quota/src/Custom/Models/QuotaRequestDetailData.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+
+namespace Azure.ResourceManager.Quota.Models
+{
+    internal partial class QuotaRequestDetailData
+    {
+        /// <summary> Quota request details. </summary>
+        [WirePath("properties.value")]
+        public IReadOnlyList<QuotaSubRequestDetail> Value { get; }
+    }
+}
