48d826e65575d6ab4387b6621c6364f2ce0c28ef
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2025-11-12 13:39:06 -0800

    Pass MRWContext in call to MRW.Write (#53845)

--- eng/packages/http-client-csharp/generator/TestProjects/Local/Basic-TypeSpec/src/Generated/BasicTypeSpecClientGetWithNextLinkCollectionResult.cs
+++ eng/packages/http-client-csharp/generator/TestProjects/Local/Basic-TypeSpec/src/Generated/BasicTypeSpecClientGetWithNextLinkCollectionResult.cs
@@ -46,7 +46,7 @@ namespace BasicTypeSpec
                 List<BinaryData> items = new List<BinaryData>();
                 foreach (var item in result.Things)
                 {
-                    items.Add(ModelReaderWriter.Write(item, ModelSerializationExtensions.WireOptions));
+                    items.Add(ModelReaderWriter.Write(item, ModelSerializationExtensions.WireOptions, BasicTypeSpecContext.Default));
                 }
                 yield return Page<BinaryData>.FromValues(items, nextPage?.AbsoluteUri, response);
                 nextPage = result.Next;
