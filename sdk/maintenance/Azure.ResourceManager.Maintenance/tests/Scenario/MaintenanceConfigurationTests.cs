ec4d0735b9b36524eab12d81a03e7f27b0e897e6
Wei Hu <huwe@microsoft.com>
2025-11-13 14:42:46 +0800

    update test

--- sdk/maintenance/Azure.ResourceManager.Maintenance/tests/Scenario/MaintenanceConfigurationTests.cs
+++ sdk/maintenance/Azure.ResourceManager.Maintenance/tests/Scenario/MaintenanceConfigurationTests.cs
@@ -41,73 +41,74 @@ namespace Azure.ResourceManager.Maintenance.Tests
             await _resourceGroup.DeleteAsync(WaitUntil.Completed);
         }
 
-        // TODO: These tests are commented out for patch release.
-        //[RecordedTest]
-        //public async Task Delete()
-        //{
-        //    MaintenanceConfigurationResource maintenanceConfigurationResource = await CreateMaintenanceConfiguration();
+        [RecordedTest]
+        public async Task Delete()
+        {
+            MaintenanceConfigurationResource maintenanceConfigurationResource = await CreateMaintenanceConfiguration();
 
-        //    var deleted = await maintenanceConfigurationResource.DeleteAsync(WaitUntil.Completed);
-        //    Assert.IsNotEmpty(deleted.Value.Data.Id);
+            var deleted = await maintenanceConfigurationResource.DeleteAsync(WaitUntil.Completed);
+            Assert.IsNotEmpty(deleted.Value.Data.Id);
 
-        //    MaintenanceConfigurationCollection collection = _resourceGroup.GetMaintenanceConfigurations();
+            MaintenanceConfigurationCollection collection = _resourceGroup.GetMaintenanceConfigurations();
 
-        //    var exist = await collection.ExistsAsync(maintenanceConfigurationResource.Data.Name);
-        //    Assert.IsFalse(exist.Value);
-        //}
+            var exist = await collection.ExistsAsync(maintenanceConfigurationResource.Data.Name);
+            Assert.IsFalse(exist.Value);
+        }
 
-        //[RecordedTest]
-        //public async Task Create()
-        //{
-        //    MaintenanceConfigurationResource maintenanceConfigurationResource = await CreateMaintenanceConfiguration();
-        //    Assert.IsNotEmpty(maintenanceConfigurationResource.Data.Id);
-        //    Assert.AreEqual(maintenanceConfigurationResource.Data.StartOn, new DateTimeOffset(2024, 12, 31, 14, 0, 0, TimeSpan.Zero));
-        //    Assert.AreEqual(maintenanceConfigurationResource.Data.ExpireOn, new DateTimeOffset(9999, 12, 31, 0, 0, 0, TimeSpan.Zero));
-        //}
+        [RecordedTest]
+        public async Task Create()
+        {
+            MaintenanceConfigurationResource maintenanceConfigurationResource = await CreateMaintenanceConfiguration();
+            Assert.IsNotEmpty(maintenanceConfigurationResource.Data.Id);
+            Assert.AreEqual(maintenanceConfigurationResource.Data.StartOn, new DateTimeOffset(2024, 12, 31, 14, 0, 0, TimeSpan.Zero));
+            Assert.AreEqual(maintenanceConfigurationResource.Data.ExpireOn, new DateTimeOffset(9999, 12, 31, 0, 0, 0, TimeSpan.Zero));
+        }
 
-        //[RecordedTest]
-        //public async Task List()
-        //{
-        //    MaintenanceConfigurationResource config1 = await CreateMaintenanceConfiguration();
-        //    MaintenanceConfigurationResource config2 = await CreateMaintenanceConfiguration();
+        [RecordedTest]
+        public async Task List()
+        {
+            MaintenanceConfigurationResource config1 = await CreateMaintenanceConfiguration();
+            MaintenanceConfigurationResource config2 = await CreateMaintenanceConfiguration();
 
-        //    var list = await _configCollection.GetAllAsync().ToEnumerableAsync();
-        //    Assert.IsTrue(list.Count >= 2);
-        //    Assert.IsNotEmpty(list);
-        //    Assert.IsTrue(list.Exists(item => item.Data.Name == config1.Data.Name));
-        //    Assert.IsTrue(list.Exists(item => item.Data.Name == config2.Data.Name));
-        //}
+            var list = await _configCollection.GetAllAsync().ToEnumerableAsync();
+            Assert.IsTrue(list.Count >= 2);
+            Assert.IsNotEmpty(list);
+            Assert.IsTrue(list.Exists(item => item.Data.Name == config1.Data.Name));
+            Assert.IsTrue(list.Exists(item => item.Data.Name == config2.Data.Name));
+        }
 
-        //[RecordedTest]
-        //public async Task Get()
-        //{
-        //    MaintenanceConfigurationResource config = await CreateMaintenanceConfiguration();
+        [RecordedTest]
+        public async Task Get()
+        {
+            MaintenanceConfigurationResource config = await CreateMaintenanceConfiguration();
 
-        //    var retrieveConfig = await _configCollection.GetAsync(config.Data.Name);
-        //    Assert.IsNotEmpty(retrieveConfig.Value.Data.Id);
-        //}
+            var retrieveConfig = await _configCollection.GetAsync(config.Data.Name);
+            Assert.IsNotEmpty(retrieveConfig.Value.Data.Id);
+        }
 
-        //private async Task<MaintenanceConfigurationResource> CreateMaintenanceConfiguration()
-        //{
-        //    string resourceName = Recording.GenerateAssetName("maintenance-config-");
-        //    MaintenanceConfigurationData data = new MaintenanceConfigurationData(Location)
-        //    {
-        //        Namespace = "Microsoft.Maintenance",
-        //        MaintenanceScope = MaintenanceScope.InGuestPatch,
-        //        Visibility = MaintenanceConfigurationVisibility.Custom,
-        //        StartOn = DateTimeOffset.Parse("2024-12-31 14:00"),
-        //        ExpireOn = DateTimeOffset.Parse("9999-12-31 00:00"),
-        //        Duration = TimeSpan.Parse("03:00"),
-        //        TimeZone = "Pacific Standard Time",
-        //        RecurEvery = "Day",
-        //        InstallPatches = new MaintenancePatchConfiguration(MaintenanceRebootOption.Always,
-        //            new MaintenanceWindowsPatchSettings(new List<string>(), new List<string>(), new List<string>() { "Security", "Critical" }, false, null),
-        //            new MaintenanceLinuxPatchSettings(new List<string>(), new List<string>(), new List<string>() { "Security", "Critical" }, null), null)
-        //    };
-        //    data.ExtensionProperties.Add("InGuestPatchMode", "User");
+        private async Task<MaintenanceConfigurationResource> CreateMaintenanceConfiguration()
+        {
+            string resourceName = Recording.GenerateAssetName("maintenance-config-");
+            MaintenanceConfigurationData data = new MaintenanceConfigurationData(Location)
+            {
+                Namespace = "Microsoft.Maintenance",
+                MaintenanceScope = MaintenanceScope.InGuestPatch,
+                Visibility = MaintenanceConfigurationVisibility.Custom,
+                StartOn = DateTimeOffset.Parse("2024-12-31 14:00"),
+                ExpireOn = DateTimeOffset.Parse("9999-12-31 00:00"),
+                Duration = TimeSpan.Parse("03:00"),
+                TimeZone = "Pacific Standard Time",
+                RecurEvery = "Day",
 
-        //    ArmOperation<MaintenanceConfigurationResource> lro = await _configCollection.CreateOrUpdateAsync(WaitUntil.Completed, resourceName, data);
-        //    return lro.Value;
-        //}
+                // TODO: These tests are commented out for patch release.
+                //InstallPatches = new MaintenancePatchConfiguration(MaintenanceRebootOption.Always,
+                //    new MaintenanceWindowsPatchSettings(new List<string>(), new List<string>(), new List<string>() { "Security", "Critical" }, false, null),
+                //    new MaintenanceLinuxPatchSettings(new List<string>(), new List<string>(), new List<string>() { "Security", "Critical" }, null), null)
+            };
+            data.ExtensionProperties.Add("InGuestPatchMode", "User");
+
+            ArmOperation<MaintenanceConfigurationResource> lro = await _configCollection.CreateOrUpdateAsync(WaitUntil.Completed, resourceName, data);
+            return lro.Value;
+        }
     }
 }
--- sdk/maintenance/Azure.ResourceManager.Maintenance/tests/Scenario/MaintenancePublicConfigurationTests.cs
+++ sdk/maintenance/Azure.ResourceManager.Maintenance/tests/Scenario/MaintenancePublicConfigurationTests.cs
@@ -19,22 +19,23 @@ namespace Azure.ResourceManager.Maintenance.Tests
         public MaintenancePublicConfigurationTests(bool isAsync) : base(isAsync) //, RecordedTestMode.Record)
         { }
 
-        // TODO: This test is commented out for patch release.
-        //[RecordedTest]
-        //public async Task MaintenancePublicConfigurationGetForResourceTest()
-        //{
-        //    //string assetName = "maintenance-config-";
-        //    string subscriptionId = "eee2cef4-bc47-4278-b4f8-cfc65f25dfd8";
-        //    string resourceName = "aks-mrp-cfg-weekday_utc-7-eastus2euap";
-        //    ResourceIdentifier maintenanceConfigurationResourceId = MaintenancePublicConfigurationResource.CreateResourceIdentifier(subscriptionId, resourceName);
-        //    MaintenancePublicConfigurationResource maintenancePublicConfiguration = Client.GetMaintenancePublicConfigurationResource(maintenanceConfigurationResourceId);
+        [RecordedTest]
+        public async Task MaintenancePublicConfigurationGetForResourceTest()
+        {
+            //string assetName = "maintenance-config-";
+            string subscriptionId = "eee2cef4-bc47-4278-b4f8-cfc65f25dfd8";
+            string resourceName = "aks-mrp-cfg-weekday_utc-7-eastus2euap";
+            ResourceIdentifier maintenanceConfigurationResourceId = MaintenancePublicConfigurationResource.CreateResourceIdentifier(subscriptionId, resourceName);
+            MaintenancePublicConfigurationResource maintenancePublicConfiguration = Client.GetMaintenancePublicConfigurationResource(maintenanceConfigurationResourceId);
 
-        //    MaintenancePublicConfigurationResource result = await maintenancePublicConfiguration.GetAsync();
+            MaintenancePublicConfigurationResource result = await maintenancePublicConfiguration.GetAsync();
 
-        //    MaintenanceConfigurationData resourceData = result.Data;
+            MaintenanceConfigurationData resourceData = result.Data;
 
-        //    Assert.IsNotNull(resourceData);
-        //    Assert.IsTrue(resourceData.MaintenanceScope.Equals(MaintenanceScope.Resource));
-        //}
+            Assert.IsNotNull(resourceData);
+
+            // TODO: This test is commented out for patch release.
+            //Assert.IsTrue(resourceData.MaintenanceScope.Equals(MaintenanceScope.Resource));
+        }
     }
 }
