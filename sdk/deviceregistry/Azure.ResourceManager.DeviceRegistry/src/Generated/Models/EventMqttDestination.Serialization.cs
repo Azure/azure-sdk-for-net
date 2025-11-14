28ce4ba66b446892ca2a8f3004a51fce7e150599
azure-sdk <azuresdk@microsoft.com>
2025-11-14 06:16:03 +0000

    Update SDK code co_ea_1

--- sdk/deviceregistry/Azure.ResourceManager.DeviceRegistry/src/Generated/Models/EventMqttDestination.Serialization.cs
+++ sdk/deviceregistry/Azure.ResourceManager.DeviceRegistry/src/Generated/Models/EventMqttDestination.Serialization.cs
@@ -124,7 +124,7 @@ namespace Azure.ResourceManager.DeviceRegistry.Models
             switch (format)
             {
                 case "J":
-                    using (JsonDocument document = JsonDocument.Parse(data))
+                    using (JsonDocument document = JsonDocument.Parse(data, ModelSerializationExtensions.JsonDocumentOptions))
                     {
                         return DeserializeEventMqttDestination(document.RootElement, options);
                     }
