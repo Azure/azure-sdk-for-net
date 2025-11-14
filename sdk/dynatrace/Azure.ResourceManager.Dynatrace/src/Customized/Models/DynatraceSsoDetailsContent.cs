db0dcade0bd5c2b1ddabc6b62f99bfc89c7f11ae
arushiarora24 <arushi2405@gmail.com>
2025-11-14 15:26:43 +0530

    Reverting change as generated code notin sync withmanual changes

---
+++ sdk/dynatrace/Azure.ResourceManager.Dynatrace/src/Customized/Models/DynatraceSsoDetailsContent.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+
+namespace Azure.ResourceManager.Dynatrace.Models
+{
+    public partial class DynatraceSsoDetailsContent
+    {
+        /// <summary> Initializes a new instance of <see cref="DynatraceSsoDetailsContent"/>. </summary>
+        public DynatraceSsoDetailsContent()
+        {
+        }
+
+        /// <summary> user principal id of the user. </summary>
+        public string UserPrincipal { get; set; }
+    }
+}
