cac33dbfb81a01f6f9c9aed990f7a4027f9b8b7d
Arcturus Zhang <dapzhang@microsoft.com>
2025-11-14 12:14:19 +0800

    update documentation

--- sdk/compute/Azure.ResourceManager.Compute/src/Customize/CommunityGalleryData.cs
+++ sdk/compute/Azure.ResourceManager.Compute/src/Customize/CommunityGalleryData.cs
@@ -13,7 +13,12 @@ namespace Azure.ResourceManager.Compute
     public partial class CommunityGalleryData
     {
         // we also must add back this property to avoid breaking changes, but its payload never have this property.
-        /// <summary> The resource identifier. </summary>
+        /// <summary>
+        /// The resource identifier.
+        ///
+        /// This property is depracated and will be removed in a future release.
+        /// There is possibility that this property will be null.
+        /// </summary>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public ResourceIdentifier Id { get; internal set; }
     }
--- sdk/compute/Azure.ResourceManager.Compute/src/Customize/CommunityGalleryImageData.cs
+++ sdk/compute/Azure.ResourceManager.Compute/src/Customize/CommunityGalleryImageData.cs
@@ -23,7 +23,12 @@ namespace Azure.ResourceManager.Compute
         };
 
         // we also must add back this property to avoid breaking changes, but its payload never have this property.
-        /// <summary> The resource identifier. </summary>
+        /// <summary>
+        /// The resource identifier.
+        ///
+        /// This property is depracated and will be removed in a future release.
+        /// There is possibility that this property will be null.
+        /// </summary>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public ResourceIdentifier Id { get; internal set; }
     }
