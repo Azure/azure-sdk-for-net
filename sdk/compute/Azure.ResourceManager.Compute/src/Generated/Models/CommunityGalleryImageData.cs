f9aaf399edab8e872b62fd33011b28b197f989e5
Dapeng Zhang <dapzhang@microsoft.com>
2025-11-14 14:23:11 +0800

    update `Azure.ResourceManager.Compute` (#53863)

--- sdk/compute/Azure.ResourceManager.Compute/src/Generated/Models/CommunityGalleryImageData.cs
+++ sdk/compute/Azure.ResourceManager.Compute/src/Generated/Models/CommunityGalleryImageData.cs
@@ -12,43 +12,42 @@ using Azure.ResourceManager.Compute.Models;
 
 namespace Azure.ResourceManager.Compute
 {
-    /// <summary>
-    /// A class representing the SharedGalleryImage data model.
-    /// Specifies information about the gallery image definition that you want to create or update.
-    /// </summary>
-    public partial class SharedGalleryImageData : PirSharedGalleryResourceData
+    /// <summary> Specifies information about the gallery image definition that you want to create or update. </summary>
+    public partial class CommunityGalleryImageData : PirCommunityGalleryResourceData
     {
-        /// <summary> Initializes a new instance of <see cref="SharedGalleryImageData"/>. </summary>
-        internal SharedGalleryImageData()
+        /// <summary> Initializes a new instance of <see cref="CommunityGalleryImageData"/>. </summary>
+        internal CommunityGalleryImageData()
         {
             Features = new ChangeTrackingList<GalleryImageFeature>();
             ArtifactTags = new ChangeTrackingDictionary<string, string>();
         }
 
-        /// <summary> Initializes a new instance of <see cref="SharedGalleryImageData"/>. </summary>
+        /// <summary> Initializes a new instance of <see cref="CommunityGalleryImageData"/>. </summary>
         /// <param name="name"> Resource name. </param>
         /// <param name="location"> Resource location. </param>
+        /// <param name="resourceType"> Resource type. </param>
+        /// <param name="uniqueId"> The unique id of this community gallery. </param>
         /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
-        /// <param name="uniqueId"> The unique id of this shared gallery. </param>
         /// <param name="osType"> This property allows you to specify the type of the OS that is included in the disk when creating a VM from a managed image. Possible values are: **Windows,** **Linux.**. </param>
         /// <param name="osState"> This property allows the user to specify whether the virtual machines created under this image are 'Generalized' or 'Specialized'. </param>
         /// <param name="endOfLifeOn"> The end of life date of the gallery image definition. This property can be used for decommissioning purposes. This property is updatable. </param>
-        /// <param name="identifier"> This is the gallery image definition identifier. </param>
+        /// <param name="imageIdentifier"> This is the community gallery image definition identifier. </param>
         /// <param name="recommended"> The properties describe the recommended machine configuration for this Image Definition. These properties are updatable. </param>
         /// <param name="disallowed"> Describes the disallowed disk types. </param>
         /// <param name="hyperVGeneration"> The hypervisor generation of the Virtual Machine. Applicable to OS disks only. </param>
         /// <param name="features"> A list of gallery image features. </param>
         /// <param name="purchasePlan"> Describes the gallery image definition purchase plan. This is used by marketplace images. </param>
         /// <param name="architecture"> The architecture of the image. Applicable to OS disks only. </param>
-        /// <param name="privacyStatementUri"> Privacy statement uri for the current community gallery image. </param>
-        /// <param name="eula"> End-user license agreement for the current community gallery image. </param>
-        /// <param name="artifactTags"> The artifact tags of a shared gallery resource. </param>
-        internal SharedGalleryImageData(string name, AzureLocation? location, IDictionary<string, BinaryData> serializedAdditionalRawData, string uniqueId, SupportedOperatingSystemType? osType, OperatingSystemStateType? osState, DateTimeOffset? endOfLifeOn, GalleryImageIdentifier identifier, RecommendedMachineConfiguration recommended, Disallowed disallowed, HyperVGeneration? hyperVGeneration, IReadOnlyList<GalleryImageFeature> features, ImagePurchasePlan purchasePlan, ArchitectureType? architecture, Uri privacyStatementUri, string eula, IReadOnlyDictionary<string, string> artifactTags) : base(name, location, serializedAdditionalRawData, uniqueId)
+        /// <param name="privacyStatementUri"> Privacy statement URI for the current community gallery image. </param>
+        /// <param name="eula"> The end-user license agreement for the current community gallery image. </param>
+        /// <param name="disclaimer"> The disclaimer for a community gallery resource. </param>
+        /// <param name="artifactTags"> The artifact tags of a community gallery resource. </param>
+        internal CommunityGalleryImageData(string name, AzureLocation? location, ResourceType? resourceType, string uniqueId, IDictionary<string, BinaryData> serializedAdditionalRawData, SupportedOperatingSystemType? osType, OperatingSystemStateType? osState, DateTimeOffset? endOfLifeOn, CommunityGalleryImageIdentifier imageIdentifier, RecommendedMachineConfiguration recommended, Disallowed disallowed, HyperVGeneration? hyperVGeneration, IReadOnlyList<GalleryImageFeature> features, ImagePurchasePlan purchasePlan, ArchitectureType? architecture, Uri privacyStatementUri, string eula, string disclaimer, IReadOnlyDictionary<string, string> artifactTags) : base(name, location, resourceType, uniqueId, serializedAdditionalRawData)
         {
             OSType = osType;
             OSState = osState;
             EndOfLifeOn = endOfLifeOn;
-            Identifier = identifier;
+            ImageIdentifier = imageIdentifier;
             Recommended = recommended;
             Disallowed = disallowed;
             HyperVGeneration = hyperVGeneration;
@@ -57,6 +56,7 @@ namespace Azure.ResourceManager.Compute
             Architecture = architecture;
             PrivacyStatementUri = privacyStatementUri;
             Eula = eula;
+            Disclaimer = disclaimer;
             ArtifactTags = artifactTags;
         }
 
@@ -66,8 +66,8 @@ namespace Azure.ResourceManager.Compute
         public OperatingSystemStateType? OSState { get; }
         /// <summary> The end of life date of the gallery image definition. This property can be used for decommissioning purposes. This property is updatable. </summary>
         public DateTimeOffset? EndOfLifeOn { get; }
-        /// <summary> This is the gallery image definition identifier. </summary>
-        public GalleryImageIdentifier Identifier { get; }
+        /// <summary> This is the community gallery image definition identifier. </summary>
+        public CommunityGalleryImageIdentifier ImageIdentifier { get; }
         /// <summary> The properties describe the recommended machine configuration for this Image Definition. These properties are updatable. </summary>
         public RecommendedMachineConfiguration Recommended { get; }
         /// <summary> Describes the disallowed disk types. </summary>
@@ -86,13 +86,13 @@ namespace Azure.ResourceManager.Compute
         public ImagePurchasePlan PurchasePlan { get; }
         /// <summary> The architecture of the image. Applicable to OS disks only. </summary>
         public ArchitectureType? Architecture { get; }
-        /// <summary> Privacy statement uri for the current community gallery image. </summary>
+        /// <summary> Privacy statement URI for the current community gallery image. </summary>
         public Uri PrivacyStatementUri { get; }
-        /// <summary> End-user license agreement for the current community gallery image. </summary>
+        /// <summary> The end-user license agreement for the current community gallery image. </summary>
         public string Eula { get; }
-        /// <summary> The artifact tags of a shared gallery resource. </summary>
+        /// <summary> The disclaimer for a community gallery resource. </summary>
+        public string Disclaimer { get; }
+        /// <summary> The artifact tags of a community gallery resource. </summary>
         public IReadOnlyDictionary<string, string> ArtifactTags { get; }
-        /// <summary> The resource identifier. </summary>
-        public ResourceIdentifier Id { get; internal set; }
     }
 }
