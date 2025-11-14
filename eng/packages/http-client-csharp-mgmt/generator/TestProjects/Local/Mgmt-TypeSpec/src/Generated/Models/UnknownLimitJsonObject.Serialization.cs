cea5699320a10a6b0b93c4bc795771daf0bdaa73
Wei Hu <live1206@gmail.com>
2025-11-14 13:31:37 +0800

    Bump azure generator (#53833)

--- eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/Models/UnknownLimitJsonObject.Serialization.cs
+++ eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/Models/UnknownLimitJsonObject.Serialization.cs
@@ -111,7 +111,7 @@ namespace Azure.Generator.MgmtTypeSpec.Tests.Models
             switch (format)
             {
                 case "J":
-                    using (JsonDocument document = JsonDocument.Parse(data))
+                    using (JsonDocument document = JsonDocument.Parse(data, ModelSerializationExtensions.JsonDocumentOptions))
                     {
                         return DeserializeLimitJsonObject(document.RootElement, options);
                     }
