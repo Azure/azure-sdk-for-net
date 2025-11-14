1e1d3983593d4cc794d4c6fb544b2c232094d47a
azure-sdk <azuresdk@microsoft.com>
2025-11-14 06:15:16 +0000

    Update SDK code el_mo_2

--- sdk/hybridkubernetes/Azure.ResourceManager.Kubernetes/src/Generated/Models/ArcAgentryConfigurations.Serialization.cs
+++ sdk/hybridkubernetes/Azure.ResourceManager.Kubernetes/src/Generated/Models/ArcAgentryConfigurations.Serialization.cs
@@ -202,7 +202,7 @@ namespace Azure.ResourceManager.Kubernetes.Models
             switch (format)
             {
                 case "J":
-                    using (JsonDocument document = JsonDocument.Parse(data))
+                    using (JsonDocument document = JsonDocument.Parse(data, ModelSerializationExtensions.JsonDocumentOptions))
                     {
                         return DeserializeArcAgentryConfigurations(document.RootElement, options);
                     }
