16ab24560f4729337021e72824ef57f6b6b9a6ec
Daniel Jurek <djurek@microsoft.com>
2025-11-17 21:57:06 +0000

    Review feedback

--- eng/scripts/spell-check-public-api.ps1
+++ eng/scripts/spell-check-public-api.ps1
@@ -3,6 +3,6 @@ Set-StrictMode -Version 3.0
 
 $files = Get-ChildItem -Path "$PSScriptRoot/../../sdk/*/*/api/*.cs" -File
 Write-Host "Found $($files.Count) public API surface files to spell check."
-."eng/common/spelling/Invoke-Cspell.ps1" `
+."$PSScriptRoot/../common/spelling/Invoke-Cspell.ps1" `
   -CSpellConfigPath "./.vscode/cspell.json" `
   -FileList $files.FullName
