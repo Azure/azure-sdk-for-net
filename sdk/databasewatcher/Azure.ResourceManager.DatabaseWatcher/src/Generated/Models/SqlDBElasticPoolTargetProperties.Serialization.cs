8ae59089856bd1dd994844369228ca776ef3950a
azure-sdk <azuresdk@microsoft.com>
2025-11-19 00:34:34 +0000

    Update SDK code co_el_1

--- sdk/databasewatcher/Azure.ResourceManager.DatabaseWatcher/src/Generated/Models/SqlDBElasticPoolTargetProperties.Serialization.cs
+++ sdk/databasewatcher/Azure.ResourceManager.DatabaseWatcher/src/Generated/Models/SqlDBElasticPoolTargetProperties.Serialization.cs
@@ -185,7 +185,7 @@ namespace Azure.ResourceManager.DatabaseWatcher.Models
             switch (format)
             {
                 case "J":
-                    using (JsonDocument document = JsonDocument.Parse(data))
+                    using (JsonDocument document = JsonDocument.Parse(data, ModelSerializationExtensions.JsonDocumentOptions))
                     {
                         return DeserializeSqlDBElasticPoolTargetProperties(document.RootElement, options);
                     }
