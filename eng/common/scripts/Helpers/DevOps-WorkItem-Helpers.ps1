5d43329888cf04fa6a19b5a041945ec60bcdaae9
Daniel Jurek <djurek@microsoft.com>
2025-11-21 18:01:45 +0000

    Fix accidental removal of $title

--- eng/common/scripts/Helpers/DevOps-WorkItem-Helpers.ps1
+++ eng/common/scripts/Helpers/DevOps-WorkItem-Helpers.ps1
@@ -552,6 +552,7 @@ function CreateOrUpdatePackageWorkItem($lang, $pkg, $verMajorMinor, $existingIte
     if ($pkgRepoPath -ne $existingItem.fields["Custom.PackageRepoPath"]) { $changedField = "Custom.PackageRepoPath" }
     if ($specProjectPath -ne $existingItem.fields["Custom.SpecProjectPath"]) { $changedField = "Custom.SpecProjectPath" }
     if ($serviceName -ne $existingItem.fields["Custom.ServiceName"]) { $changedField = "Custom.ServiceName" }
+    if ($title -ne $existingItem.fields["System.Title"]) { $changedField = "System.Title" }
 
     if ($changedField) {
       Write-Host "At least field $changedField ($($existingItem.fields[$changedField])) changed so updating."
