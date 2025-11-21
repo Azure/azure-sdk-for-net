16e30cbe5480f74b1f89575c60790fdbb46d2e39
Christopher Scott <chriscott@hotmail.com>
2025-11-21 10:13:34 -0600

    wip

--- sdk/core/Azure.Core/tests/Identity/Mock/MockMsalManagedIdentityClient.cs
+++ sdk/core/Azure.Core/tests/Identity/Mock/MockMsalManagedIdentityClient.cs
@@ -35,13 +35,13 @@ namespace Azure.Core.Tests.Identity.Mock
             return new ValueTask<IManagedIdentityApplication>(ClientAppFactory(cancellationToken));
         }
 
-        public override ValueTask<AuthenticationResult> AcquireTokenForManagedIdentityAsyncCore(bool async, TokenRequestContext requestContext, CancellationToken cancellationToken)
+        public override ValueTask<AuthenticationResult> AcquireTokenForManagedIdentityAsyncCore(bool async, TokenRequestContext requestContext, bool isTokenBindingAvailable, CancellationToken cancellationToken)
         {
             if (AcquireTokenForManagedIdentityAsyncFactory != null)
             {
                 return new ValueTask<AuthenticationResult>(AcquireTokenForManagedIdentityAsyncFactory(requestContext, cancellationToken));
             }
-            return base.AcquireTokenForManagedIdentityAsyncCore(async, requestContext, cancellationToken);
+            return base.AcquireTokenForManagedIdentityAsyncCore(async, requestContext, isTokenBindingAvailable, cancellationToken);
         }
     }
 }
