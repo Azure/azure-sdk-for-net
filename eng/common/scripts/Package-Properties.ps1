b3ae1709a3281d47b4e5d32f9046164161e7f65e
ray chen <raychen@microsoft.com>
2025-11-26 17:55:32 +0000

    Sync eng/common directory with azure-sdk-tools repository for Tools PR 13076

--- eng/common/scripts/Package-Properties.ps1
+++ eng/common/scripts/Package-Properties.ps1
@@ -576,3 +576,25 @@ function Get-PkgPropsForEntireService ($serviceDirectoryPath) {
 
     return $projectProps
 }
+
+# Get the full package name based on packageInfo properties
+# Returns Group+ArtifactName if Group exists and has a value, otherwise returns Name
+# If UseColonSeparator switch is enabled, returns Group:ArtifactName format (colon separator)
+function Get-FullPackageName {
+    param (
+        [Parameter(Mandatory=$true)]
+        [PSCustomObject]$PackageInfo,
+        [switch]$UseColonSeparator
+    )
+    
+    if ($PackageInfo.PSObject.Members.Name -contains "Group") {
+        $groupId = $PackageInfo.Group
+        if ($groupId) {
+            if ($UseColonSeparator) {
+                return "${groupId}:$($PackageInfo.Name)"
+            }
+            return "${groupId}+$($PackageInfo.Name)"
+        }
+    }
+    return $PackageInfo.Name
+}
