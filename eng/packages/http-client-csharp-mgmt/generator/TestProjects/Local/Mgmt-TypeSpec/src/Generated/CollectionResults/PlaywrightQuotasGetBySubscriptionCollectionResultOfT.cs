9be9dc1860e6187794e4b5028ba86ee930241fbc
Wei Hu <huwe@microsoft.com>
2025-11-26 09:33:17 +0800

    bump version of azure generator

--- eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/CollectionResults/PlaywrightQuotasGetBySubscriptionCollectionResultOfT.cs
+++ eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/CollectionResults/PlaywrightQuotasGetBySubscriptionCollectionResultOfT.cs
@@ -64,7 +64,7 @@ namespace Azure.Generator.MgmtTypeSpec.Tests
         private Response GetNextResponse(int? pageSizeHint, Uri nextLink)
         {
             HttpMessage message = nextLink != null ? _client.CreateNextGetBySubscriptionRequest(nextLink, _subscriptionId, _location, _context) : _client.CreateGetBySubscriptionRequest(_subscriptionId, _location, _context);
-            using DiagnosticScope scope = _client.ClientDiagnostics.CreateScope("PlaywrightQuotaCollection.GetAll");
+            using DiagnosticScope scope = _client.ClientDiagnostics.CreateScope("PlaywrightQuotas.GetBySubscription");
             scope.Start();
             try
             {
