6c726dce277b0ce6001319bf69aa5d1406126261
azure-sdk <azuresdk@microsoft.com>
2025-11-26 09:23:21 +0000

    Update SDK code se_wo_4

--- sdk/weightsandbiases/Azure.ResourceManager.WeightsAndBiases/src/Generated/CollectionResults/InstancesGetBySubscriptionCollectionResultOfT.cs
+++ sdk/weightsandbiases/Azure.ResourceManager.WeightsAndBiases/src/Generated/CollectionResults/InstancesGetBySubscriptionCollectionResultOfT.cs
@@ -61,7 +61,7 @@ namespace Azure.ResourceManager.WeightsAndBiases
         private Response GetNextResponse(int? pageSizeHint, Uri nextLink)
         {
             HttpMessage message = nextLink != null ? _client.CreateNextGetBySubscriptionRequest(nextLink, _subscriptionId, _context) : _client.CreateGetBySubscriptionRequest(_subscriptionId, _context);
-            using DiagnosticScope scope = _client.ClientDiagnostics.CreateScope("MockableWeightsAndBiasesSubscriptionResource.GetWeightsAndBiasesInstances");
+            using DiagnosticScope scope = _client.ClientDiagnostics.CreateScope("Instances.GetBySubscription");
             scope.Start();
             try
             {
