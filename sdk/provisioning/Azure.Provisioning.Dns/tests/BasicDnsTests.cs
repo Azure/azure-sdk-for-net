9617fcdcc7eae6b700ad7a97befc86bab2df86c6
Tiago Alves Macambira <tiagoa@microsoft.com>
2025-12-01 23:12:06 +0000

    Adds new test with validated Bicep output.

--- sdk/provisioning/Azure.Provisioning.Dns/tests/BasicDnsTests.cs
+++ sdk/provisioning/Azure.Provisioning.Dns/tests/BasicDnsTests.cs
@@ -97,4 +97,175 @@ public class BasicDnsTests
             }
             """);
     }
+
+    [Test]
+    public async Task TestCnameAndIpv4andIpv6ARecords()
+    {
+        await using Trycep test = new Trycep().Define(
+            ctx =>
+            {
+                Infrastructure infra = new();
+
+                DnsZone zone = DnsZone.FromExisting(nameof(zone), DnsZone.ResourceVersions.V2018_05_01);
+                zone.Name = "contoso.com";
+                infra.Add(zone);
+
+                DnsCnameRecord cnameRecord = new(nameof(cnameRecord), DnsCnameRecord.ResourceVersions.V2018_05_01)
+                {
+                    Parent = zone,
+                    Name = "www",
+                    TtlInSeconds = 3600,
+                    Cname = "contoso.azurewebsites.net"
+                };
+                infra.Add(cnameRecord);
+
+                DnsARecord dnsRecordA = new("dnsRecordA")
+                {
+                    Parent = zone,
+                    Name = "privatev4",
+                    TtlInSeconds = 3600,
+                    ARecords =
+                    [
+                        new DnsARecordInfo()
+                        {
+                            Ipv4Address = IPAddress.Parse("127.0.0.1")
+                        },
+                        new DnsARecordInfo()
+                        {
+                            Ipv4Address = IPAddress.Parse("192.168.0.1")
+                        }
+                    ],
+                };
+                infra.Add(dnsRecordA);
+
+                DnsAaaaRecord aaaaRecord = new(nameof(aaaaRecord), DnsAaaaRecord.ResourceVersions.V2018_05_01)
+                {
+                    Parent = zone,
+                    Name = "privatev6",
+                    TtlInSeconds = 3600,
+                    AaaaRecords =
+                    {
+                        new DnsAaaaRecordInfo() { Ipv6Address = IPAddress.Parse("0000:0000:0000:0000:0000:0000:0000:0001") },
+                        new DnsAaaaRecordInfo() { Ipv6Address = IPAddress.Parse("FD00::45:AA:1") }
+                    }
+                };
+                infra.Add(aaaaRecord);
+
+                DnsTxtRecord spfTxtRecord = new(nameof(spfTxtRecord), DnsTxtRecord.ResourceVersions.V2018_05_01)
+                {
+                    Parent = zone,
+                    Name = "@",
+                    TtlInSeconds = 3600,
+                    TxtRecords = {
+                        new DnsTxtRecordInfo()
+                        {
+                            Values = [ "v=spf1 -all" ]
+                        }
+                    }
+                };
+                infra.Add(spfTxtRecord);
+
+                DnsTxtRecord notesTXTRecord = new(nameof(notesTXTRecord), DnsTxtRecord.ResourceVersions.V2018_05_01)
+                {
+                    Parent = zone,
+                    Name = "notes",
+                    TtlInSeconds = 3600,
+                    TxtRecords = {
+                        new DnsTxtRecordInfo()
+                        {
+                            Values = [
+                                "Usually people set SPF rules in TXT records.",
+                                "But they are actually free form text records."
+                            ]
+                        }
+                    }
+                };
+                infra.Add(notesTXTRecord);
+
+                return infra;
+            }
+        );
+
+        test.Compare(
+            """
+            resource zone 'Microsoft.Network/dnsZones@2018-05-01' existing = {
+              name: 'contoso.com'
+            }
+
+            resource cnameRecord 'Microsoft.Network/dnsZones/CNAME@2018-05-01' = {
+              name: 'www'
+              parent: zone
+              properties: {
+                CNAMERecord: {
+                  cname: 'contoso.azurewebsites.net'
+                }
+                TTL: 3600
+              }
+            }
+
+            resource dnsRecordA 'Microsoft.Network/dnsZones/A@2018-05-01' = {
+              name: 'privatev4'
+              parent: zone
+              properties: {
+                ARecords: [
+                  {
+                    ipv4Address: '127.0.0.1'
+                  }
+                  {
+                    ipv4Address: '192.168.0.1'
+                  }
+                ]
+                TTL: 3600
+              }
+            }
+
+            resource aaaaRecord 'Microsoft.Network/dnsZones/AAAA@2018-05-01' = {
+              name: 'privatev6'
+              parent: zone
+              properties: {
+                AAAARecords: [
+                  {
+                    ipv6Address: '::1'
+                  }
+                  {
+                    ipv6Address: 'fd00::45:aa:1'
+                  }
+                ]
+                TTL: 3600
+              }
+            }
+
+            resource spfTxtRecord 'Microsoft.Network/dnsZones/TXT@2018-05-01' = {
+              name: '@'
+              properties: {
+                TXTRecords: [
+                  {
+                    value: [
+                      'v=spf1 -all'
+                    ]
+                  }
+                ]
+                TTL: 3600
+              }
+              parent: zone
+            }
+
+            resource notesTXTRecord 'Microsoft.Network/dnsZones/TXT@2018-05-01' = {
+              name: 'notes'
+              properties: {
+                TXTRecords: [
+                  {
+                    value: [
+                      'Usually people set SPF rules in TXT records.'
+                      'But they are actually free form text records.'
+                    ]
+                  }
+                ]
+                TTL: 3600
+              }
+              parent: zone
+            }
+            """
+        );
+    }
 }
