a3e6b64ca4488b521f912d003340592fdabf9df0
azure-sdk <azuresdk@microsoft.com>
2025-12-09 07:54:16 +0000

    Update SDK code mo_re_3

--- sdk/portalservices/Azure.ResourceManager.PortalServicesCopilot/src/Generated/Internal/CodeGenMemberAttribute.cs
+++ sdk/portalservices/Azure.ResourceManager.PortalServicesCopilot/src/Generated/Internal/CodeGenMemberAttribute.cs
@@ -7,7 +7,7 @@
 
 using System;
 
-namespace Azure.ResourceManager.PortalServicesCopilot
+namespace Microsoft.TypeSpec.Generator.Customizations
 {
     [AttributeUsage((AttributeTargets.Property | AttributeTargets.Field))]
     internal partial class CodeGenMemberAttribute : CodeGenTypeAttribute
