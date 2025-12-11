1e407a965d8e0027f5fc7863d7129e552d455a3f
m-nash <64171366+m-nash@users.noreply.github.com>
2025-12-11 13:29:53 -0800

    update api after merge

--- sdk/core/System.ClientModel/api/System.ClientModel.net10.0.cs
+++ sdk/core/System.ClientModel/api/System.ClientModel.net10.0.cs
@@ -57,6 +57,12 @@ namespace System.ClientModel
         public virtual T Value { get { throw null; } }
         public static implicit operator T (System.ClientModel.ClientResult<T> result) { throw null; }
     }
+    public abstract partial class ClientSettings : System.ClientModel.Primitives.ClientSettingsBase
+    {
+        protected ClientSettings() { }
+        public System.ClientModel.Primitives.ClientPipelineOptions ClientOptions { get { throw null; } set { } }
+        protected override System.ClientModel.Primitives.ClientConnection GetClientConnectionCore() { throw null; }
+    }
     public abstract partial class CollectionResult<T> : System.ClientModel.Primitives.CollectionResult, System.Collections.Generic.IEnumerable<T>, System.Collections.IEnumerable
     {
         protected internal CollectionResult() { }
@@ -64,12 +70,26 @@ namespace System.ClientModel
         protected abstract System.Collections.Generic.IEnumerable<T> GetValuesFromPage(System.ClientModel.ClientResult page);
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() { throw null; }
     }
+    public static partial class ConfigurationManagerExtensions
+    {
+        public static T GetClientSettings<T>(this Microsoft.Extensions.Configuration.IConfiguration configuration, string sectionName) where T : System.ClientModel.ClientSettings, new() { throw null; }
+        public static T GetClientSettings<T>(this Microsoft.Extensions.Configuration.IConfigurationSection section) where T : System.ClientModel.ClientSettings, new() { throw null; }
+        public static System.ClientModel.Primitives.ClientConnection GetConnection(this Microsoft.Extensions.Configuration.IConfigurationManager configuration, string sectionName) { throw null; }
+        public static System.ClientModel.Primitives.ClientConnection GetConnection(this Microsoft.Extensions.Configuration.IConfigurationManager configuration, string sectionName, System.ClientModel.AuthenticationTokenProvider tokenProvider) { throw null; }
+    }
     public partial class ContinuationToken
     {
         protected ContinuationToken() { }
         protected ContinuationToken(System.BinaryData bytes) { }
         public static System.ClientModel.ContinuationToken FromBytes(System.BinaryData bytes) { throw null; }
         public virtual System.BinaryData ToBytes() { throw null; }
+    }
+    public partial class CredentialSettings
+    {
+        public CredentialSettings(Microsoft.Extensions.Configuration.IConfigurationSection section) { }
+        public string? CredentialSource { get { throw null; } set { } }
+        public string? Key { get { throw null; } set { } }
+        public Microsoft.Extensions.Configuration.IConfigurationSection? Properties { get { throw null; } set { } }
     }
 }
 namespace System.ClientModel.Primitives
@@ -97,6 +117,7 @@ namespace System.ClientModel.Primitives
     public abstract partial class AuthenticationPolicy : System.ClientModel.Primitives.PipelinePolicy
     {
         protected AuthenticationPolicy() { }
+        public static System.ClientModel.Primitives.AuthenticationPolicy Create(System.ClientModel.Primitives.ClientConnection connection, string scope) { throw null; }
     }
     public partial class AuthenticationToken
     {
@@ -123,13 +144,15 @@ namespace System.ClientModel.Primitives
     {
         private readonly object _dummy;
         private readonly int _dummyPrimitive;
-        public ClientConnection(string id, string locator) { throw null; }
-        public ClientConnection(string id, string locator, object credential, System.ClientModel.Primitives.CredentialKind credentialKind) { throw null; }
-        public ClientConnection(string id, string locator, object? credential, System.ClientModel.Primitives.CredentialKind credentialKind, System.Collections.Generic.IReadOnlyDictionary<string, string>? metadata) { throw null; }
+        public ClientConnection(string id, string? locator) { throw null; }
+        public ClientConnection(string id, string? locator, object credential, System.ClientModel.Primitives.CredentialKind credentialKind) { throw null; }
+        public ClientConnection(string id, string? locator, object? credential, System.ClientModel.Primitives.CredentialKind credentialKind, Microsoft.Extensions.Configuration.IConfigurationSection configurationSection) { throw null; }
+        public ClientConnection(string id, string? locator, object? credential, System.ClientModel.Primitives.CredentialKind credentialKind, System.Collections.Generic.IReadOnlyDictionary<string, string>? metadata) { throw null; }
+        public Microsoft.Extensions.Configuration.IConfiguration? Configuration { get { throw null; } }
         public object? Credential { get { throw null; } }
         public System.ClientModel.Primitives.CredentialKind CredentialKind { get { throw null; } }
         public string Id { get { throw null; } }
-        public string Locator { get { throw null; } }
+        public string? Locator { get { throw null; } }
         public System.Collections.Generic.IReadOnlyDictionary<string, string> Metadata { get { throw null; } }
         public override string ToString() { throw null; }
         public bool TryGetLocatorAsUri(out System.Uri? uri) { throw null; }
@@ -208,6 +231,18 @@ namespace System.ClientModel.Primitives
         protected virtual void Wait(System.TimeSpan time, System.Threading.CancellationToken cancellationToken) { }
         protected virtual System.Threading.Tasks.Task WaitAsync(System.TimeSpan time, System.Threading.CancellationToken cancellationToken) { throw null; }
     }
+    public abstract partial class ClientSettingsBase
+    {
+        protected ClientSettingsBase() { }
+        public System.ClientModel.CredentialSettings? Credential { get { throw null; } set { } }
+        public object? CredentialObject { get { throw null; } set { } }
+        protected bool Initialized { get { throw null; } }
+        public Microsoft.Extensions.Configuration.IConfigurationSection? Properties { get { throw null; } set { } }
+        public System.ClientModel.Primitives.ClientConnection GetClientConnection() { throw null; }
+        protected abstract System.ClientModel.Primitives.ClientConnection GetClientConnectionCore();
+        public void Read(Microsoft.Extensions.Configuration.IConfigurationSection section) { }
+        protected abstract void ReadCore(Microsoft.Extensions.Configuration.IConfigurationSection section);
+    }
     public abstract partial class CollectionResult
     {
         protected CollectionResult() { }
@@ -242,6 +277,11 @@ namespace System.ClientModel.Primitives
         protected virtual void OnSendingRequest(System.ClientModel.Primitives.PipelineMessage message, System.Net.Http.HttpRequestMessage httpRequest) { }
         protected sealed override void ProcessCore(System.ClientModel.Primitives.PipelineMessage message) { }
         protected sealed override System.Threading.Tasks.ValueTask ProcessCoreAsync(System.ClientModel.Primitives.PipelineMessage message) { throw null; }
+    }
+    public partial interface IClientBuilder : Microsoft.Extensions.Hosting.IHostApplicationBuilder
+    {
+        Microsoft.Extensions.Configuration.IConfigurationSection ConfigurationSection { get; }
+        void SetCredentialObject(object credential);
     }
     public partial interface IJsonModel<out T> : System.ClientModel.Primitives.IPersistableModel<T>
     {
