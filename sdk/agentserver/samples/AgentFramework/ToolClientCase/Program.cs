e04f5b5462e06e64efe21db9a63e239392a9af3a
lixiaoli <lixiaoli@microsoft.com>
2025-12-11 16:08:42 -0800

    misc: add ToolClientCase

---
+++ sdk/agentserver/samples/AgentFramework/ToolClientCase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Azure.AI.AgentServer.AgentFramework.Extensions;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ??
+               throw new InvalidOperationException("AZURE_OPENAI_ENDPOINT is not set.");
+var deploymentName = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-4o-mini";
+
+var chatClient = new AzureOpenAIClient(
+        new Uri(endpoint),
+        new DefaultAzureCredential())
+    .GetChatClient(deploymentName)
+    .AsIChatClient()
+    .AsBuilder()
+    .UseOpenTelemetry(sourceName: "Agents", configure: (cfg) => cfg.EnableSensitiveData = true)
+    .Build();
+
+var agent = new ChatClientAgent(chatClient,
+        name: "test-agent",
+        instructions: "You are a helpful assistant, you can help the user with weather information.")
+    .AsBuilder()
+    .UseOpenTelemetry(sourceName: "Agents", configure: (cfg) => cfg.EnableSensitiveData = true)
+    .Build();
+
+// Run container agent adapter
+await agent.RunAIAgentAsync(telemetrySourceName: "Agents", [{ "type": "mcp", "project_connection_id": tool_connection_id}]);
