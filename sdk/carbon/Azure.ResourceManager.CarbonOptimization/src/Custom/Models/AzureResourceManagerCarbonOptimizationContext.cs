180df958cc6b0ba3a756d6939266712684eb90cd
Wei Hu <huwe@microsoft.com>
2025-12-12 17:18:12 +0800

    try to fix test

---
+++ sdk/carbon/Azure.ResourceManager.CarbonOptimization/src/Custom/Models/AzureResourceManagerCarbonOptimizationContext.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.ClientModel.Primitives;
+using Azure.Core;
+
+namespace Azure.ResourceManager.CarbonOptimization
+{
+    [ModelReaderWriterBuildable(typeof(ResourceType))]
+    public partial class AzureResourceManagerCarbonOptimizationContext
+    {
+    }
+}
