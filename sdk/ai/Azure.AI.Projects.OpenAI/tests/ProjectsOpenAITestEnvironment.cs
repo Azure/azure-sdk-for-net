08497ff2d60a0bf5a68a9b3dbdf7158170b2f731
nick863 <30440255+nick863@users.noreply.github.com>
2025-12-19 16:23:24 -0800

    Required fixes

--- sdk/ai/Azure.AI.Projects.OpenAI/tests/ProjectsOpenAITestEnvironment.cs
+++ sdk/ai/Azure.AI.Projects.OpenAI/tests/ProjectsOpenAITestEnvironment.cs
@@ -12,29 +12,29 @@ namespace Azure.AI.Projects.OpenAI.Tests
 {
     public class ProjectsOpenAITestEnvironment : TestEnvironment
     {
-        public string PROJECT_ENDPOINT => WrappedGetRecordedVariable("PROJECT_ENDPOINT", isSecret: false);
+        public string PROJECT_ENDPOINT => WrappedGetRecordedVariable(nameof(PROJECT_ENDPOINT), isSecret: false);
         public string AGENT_NAME => WrappedGetRecordedVariable("AZURE_AI_FOUNDRY_AGENT_NAME", isSecret: false);
         public string MODELDEPLOYMENTNAME => WrappedGetRecordedVariable("MODEL_DEPLOYMENT_NAME", isSecret: false);
         public string EMBEDDINGMODELDEPLOYMENTNAME => WrappedGetRecordedVariable("EMBEDDING_MODEL_DEPLOYMENT_NAME", isSecret: false);
-        public string INGRESS_SUBDOMAIN_SUFFIX => WrappedGetRecordedVariable("INGRESS_SUBDOMAIN_SUFFIX", isSecret: false);
-        public string OPENAI_FILE_ID => WrappedGetRecordedVariable("OPENAI_FILE_ID", isSecret: false    );
-        public string COMPUTER_SCREENSHOTS => WrappedGetRecordedVariable("COMPUTER_SCREENSHOTS", isSecret: false);
-        public string IMAGE_GENERATION_DEPLOYMENT_NAME => WrappedGetRecordedVariable("IMAGE_GENERATION_DEPLOYMENT_NAME", isSecret: false);
-        public string COMPUTER_USE_DEPLOYMENT_NAME => WrappedGetRecordedVariable("COMPUTER_USE_DEPLOYMENT_NAME", isSecret: false);
-        public string CONTAINER_APP_RESOURCE_ID => WrappedGetRecordedVariable("CONTAINER_APP_RESOURCE_ID", isSecret: false);
-        public string KNOWN_CONVERSATION_ID => WrappedGetRecordedVariable("KNOWN_CONVERSATION_ID", isSecret: false);
+        public string INGRESS_SUBDOMAIN_SUFFIX => WrappedGetRecordedVariable(nameof(INGRESS_SUBDOMAIN_SUFFIX), isSecret: false);
+        public string OPENAI_FILE_ID => WrappedGetRecordedVariable(nameof(OPENAI_FILE_ID), isSecret: false    );
+        public string COMPUTER_SCREENSHOTS => WrappedGetRecordedVariable(nameof(COMPUTER_SCREENSHOTS), isSecret: false);
+        public string IMAGE_GENERATION_DEPLOYMENT_NAME => WrappedGetRecordedVariable(nameof(IMAGE_GENERATION_DEPLOYMENT_NAME), isSecret: false);
+        public string COMPUTER_USE_DEPLOYMENT_NAME => WrappedGetRecordedVariable(nameof(COMPUTER_USE_DEPLOYMENT_NAME), isSecret: false);
+        public string CONTAINER_APP_RESOURCE_ID => WrappedGetRecordedVariable(nameof(CONTAINER_APP_RESOURCE_ID), isSecret: false);
+        public string KNOWN_CONVERSATION_ID => WrappedGetRecordedVariable(nameof(KNOWN_CONVERSATION_ID), isSecret: false);
         public string PARITY_OPENAI_API_KEY => WrappedGetRecordedVariable("OPENAI_API_KEY");
-        public string AI_SEARCH_CONNECTION_NAME => GetRecordedVariable("AI_SEARCH_CONNECTION_NAME");
-        public string BING_CONNECTION_NAME => GetRecordedVariable("BING_CONNECTION_NAME");
-        public string CUSTOM_BING_CONNECTION_NAME => GetRecordedVariable("CUSTOM_BING_CONNECTION_NAME");
-        public string BING_CUSTOM_SEARCH_INSTANCE_NAME => GetRecordedVariable("BING_CUSTOM_SEARCH_INSTANCE_NAME");
-        public string MCP_PROJECT_CONNECTION_NAME => GetRecordedOptionalVariable("MCP_PROJECT_CONNECTION_NAME");
-        public string PLAYWRIGHT_CONNECTION_NAME => GetRecordedOptionalVariable("PLAYWRIGHT_CONNECTION_NAME");
-        public string SHAREPOINT_CONNECTION_NAME => GetRecordedOptionalVariable("SHAREPOINT_CONNECTION_NAME");
-        public string FABRIC_CONNECTION_NAME => GetRecordedOptionalVariable("FABRIC_CONNECTION_NAME");
-        public string A2A_CONNECTION_NAME => GetRecordedOptionalVariable("A2A_CONNECTION_NAME");
-        public string A2A_BASE_URI => GetRecordedOptionalVariable("A2A_BASE_URI");
-        public string PUBLISHED_ENDPOINT => GetRecordedOptionalVariable("PUBLISHED_ENDPOINT");
+        public string AI_SEARCH_CONNECTION_NAME => GetRecordedVariable(nameof(AI_SEARCH_CONNECTION_NAME));
+        public string BING_CONNECTION_NAME => GetRecordedVariable(nameof(BING_CONNECTION_NAME));
+        public string CUSTOM_BING_CONNECTION_NAME => GetRecordedVariable(nameof(CUSTOM_BING_CONNECTION_NAME));
+        public string BING_CUSTOM_SEARCH_INSTANCE_NAME => GetRecordedVariable(nameof(BING_CUSTOM_SEARCH_INSTANCE_NAME));
+        public string MCP_PROJECT_CONNECTION_NAME => GetRecordedOptionalVariable(nameof(MCP_PROJECT_CONNECTION_NAME));
+        public string PLAYWRIGHT_CONNECTION_NAME => GetRecordedOptionalVariable(nameof(PLAYWRIGHT_CONNECTION_NAME));
+        public string SHAREPOINT_CONNECTION_NAME => GetRecordedOptionalVariable(nameof(SHAREPOINT_CONNECTION_NAME));
+        public string FABRIC_CONNECTION_NAME => GetRecordedOptionalVariable(nameof(FABRIC_CONNECTION_NAME));
+        public string A2A_CONNECTION_NAME => GetRecordedOptionalVariable(nameof(A2A_CONNECTION_NAME));
+        public string A2A_BASE_URI => GetRecordedOptionalVariable(nameof(A2A_BASE_URI));
+        public string PUBLISHED_ENDPOINT => GetRecordedOptionalVariable(nameof(PUBLISHED_ENDPOINT));
         public string WrappedGetRecordedVariable(string key, bool isSecret = true)
         {
             try
