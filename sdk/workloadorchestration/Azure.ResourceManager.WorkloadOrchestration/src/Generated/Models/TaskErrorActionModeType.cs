600ea63819cc3f15ca1588a8aeb5aed847cc53d8
Arthur Ma <arthurma@microsoft.com>
2025-12-20 13:42:03 +0800

    Migrage the WorkloadOrchestration to support new MPG

--- sdk/workloadorchestration/Azure.ResourceManager.WorkloadOrchestration/src/Generated/Models/TaskErrorActionModeType.cs
+++ sdk/workloadorchestration/Azure.ResourceManager.WorkloadOrchestration/src/Generated/Models/TaskErrorActionModeType.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.ComponentModel;
+using Azure.ResourceManager.WorkloadOrchestration;
 
 namespace Azure.ResourceManager.WorkloadOrchestration.Models
 {
@@ -14,41 +15,62 @@ namespace Azure.ResourceManager.WorkloadOrchestration.Models
     public readonly partial struct TaskErrorActionModeType : IEquatable<TaskErrorActionModeType>
     {
         private readonly string _value;
+        /// <summary> Stop on any failure. </summary>
+        private const string StopOnAnyFailureValue = "stopOnAnyFailure";
+        /// <summary> Stop after N cumulative failures. </summary>
+        private const string StopOnNFailuresValue = "stopOnNFailures";
+        /// <summary> Continue silently despite errors. </summary>
+        private const string SilentlyContinueValue = "silentlyContinue";
 
         /// <summary> Initializes a new instance of <see cref="TaskErrorActionModeType"/>. </summary>
+        /// <param name="value"> The value. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="value"/> is null. </exception>
         public TaskErrorActionModeType(string value)
         {
-            _value = value ?? throw new ArgumentNullException(nameof(value));
-        }
+            Argument.AssertNotNull(value, nameof(value));
 
-        private const string StopOnAnyFailureValue = "stopOnAnyFailure";
-        private const string StopOnNFailuresValue = "stopOnNFailures";
-        private const string SilentlyContinueValue = "silentlyContinue";
+            _value = value;
+        }
 
         /// <summary> Stop on any failure. </summary>
         public static TaskErrorActionModeType StopOnAnyFailure { get; } = new TaskErrorActionModeType(StopOnAnyFailureValue);
+
         /// <summary> Stop after N cumulative failures. </summary>
         public static TaskErrorActionModeType StopOnNFailures { get; } = new TaskErrorActionModeType(StopOnNFailuresValue);
+
         /// <summary> Continue silently despite errors. </summary>
         public static TaskErrorActionModeType SilentlyContinue { get; } = new TaskErrorActionModeType(SilentlyContinueValue);
+
         /// <summary> Determines if two <see cref="TaskErrorActionModeType"/> values are the same. </summary>
+        /// <param name="left"> The left value to compare. </param>
+        /// <param name="right"> The right value to compare. </param>
         public static bool operator ==(TaskErrorActionModeType left, TaskErrorActionModeType right) => left.Equals(right);
+
         /// <summary> Determines if two <see cref="TaskErrorActionModeType"/> values are not the same. </summary>
+        /// <param name="left"> The left value to compare. </param>
+        /// <param name="right"> The right value to compare. </param>
         public static bool operator !=(TaskErrorActionModeType left, TaskErrorActionModeType right) => !left.Equals(right);
-        /// <summary> Converts a <see cref="string"/> to a <see cref="TaskErrorActionModeType"/>. </summary>
+
+        /// <summary> Converts a string to a <see cref="TaskErrorActionModeType"/>. </summary>
+        /// <param name="value"> The value. </param>
         public static implicit operator TaskErrorActionModeType(string value) => new TaskErrorActionModeType(value);
 
-        /// <inheritdoc />
+        /// <summary> Converts a string to a <see cref="TaskErrorActionModeType"/>. </summary>
+        /// <param name="value"> The value. </param>
+        public static implicit operator TaskErrorActionModeType?(string value) => value == null ? null : new TaskErrorActionModeType(value);
+
+        /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override bool Equals(object obj) => obj is TaskErrorActionModeType other && Equals(other);
-        /// <inheritdoc />
+
+        /// <inheritdoc/>
         public bool Equals(TaskErrorActionModeType other) => string.Equals(_value, other._value, StringComparison.InvariantCultureIgnoreCase);
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode() => _value != null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(_value) : 0;
-        /// <inheritdoc />
+
+        /// <inheritdoc/>
         public override string ToString() => _value;
     }
 }
