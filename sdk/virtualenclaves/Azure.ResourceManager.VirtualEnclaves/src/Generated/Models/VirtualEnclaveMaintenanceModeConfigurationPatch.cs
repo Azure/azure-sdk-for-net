323466290dcfe2685ab032cf16899122abe3c23b
Arthur Ma <mars.arthur@263.net>
2025-12-22 16:09:04 +0800

    Migrage the VirtualEnclaves to support new MPG (#54633)

--- sdk/virtualenclaves/Azure.ResourceManager.VirtualEnclaves/src/Generated/Models/VirtualEnclaveMaintenanceModeConfigurationPatch.cs
+++ sdk/virtualenclaves/Azure.ResourceManager.VirtualEnclaves/src/Generated/Models/VirtualEnclaveMaintenanceModeConfigurationPatch.cs
@@ -7,43 +7,15 @@
 
 using System;
 using System.Collections.Generic;
+using Azure.ResourceManager.VirtualEnclaves;
 
 namespace Azure.ResourceManager.VirtualEnclaves.Models
 {
     /// <summary> Maintenance Mode Patch Model. </summary>
     public partial class VirtualEnclaveMaintenanceModeConfigurationPatch
     {
-        /// <summary>
-        /// Keeps track of any properties unknown to the library.
-        /// <para>
-        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
-        /// </para>
-        /// <para>
-        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
-        /// </para>
-        /// <para>
-        /// Examples:
-        /// <list type="bullet">
-        /// <item>
-        /// <term>BinaryData.FromObjectAsJson("foo")</term>
-        /// <description>Creates a payload of "foo".</description>
-        /// </item>
-        /// <item>
-        /// <term>BinaryData.FromString("\"foo\"")</term>
-        /// <description>Creates a payload of "foo".</description>
-        /// </item>
-        /// <item>
-        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
-        /// <description>Creates a payload of { "key": "value" }.</description>
-        /// </item>
-        /// <item>
-        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
-        /// <description>Creates a payload of { "key": "value" }.</description>
-        /// </item>
-        /// </list>
-        /// </para>
-        /// </summary>
-        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+        /// <summary> Keeps track of any properties unknown to the library. </summary>
+        private protected readonly IDictionary<string, BinaryData> _additionalBinaryDataProperties;
 
         /// <summary> Initializes a new instance of <see cref="VirtualEnclaveMaintenanceModeConfigurationPatch"/>. </summary>
         /// <param name="mode"> Current mode of Maintenance Mode Configuration. </param>
@@ -57,24 +29,21 @@ namespace Azure.ResourceManager.VirtualEnclaves.Models
         /// <param name="mode"> Current mode of Maintenance Mode Configuration. </param>
         /// <param name="principals"> The user, group or service principal object affected by Maintenance Mode. </param>
         /// <param name="justification"> Justification for entering or exiting Maintenance Mode. </param>
-        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
-        internal VirtualEnclaveMaintenanceModeConfigurationPatch(VirtualEnclaveMaintenanceMode mode, IList<VirtualEnclavePrincipal> principals, VirtualEnclaveMaintenanceJustification? justification, IDictionary<string, BinaryData> serializedAdditionalRawData)
+        /// <param name="additionalBinaryDataProperties"> Keeps track of any properties unknown to the library. </param>
+        internal VirtualEnclaveMaintenanceModeConfigurationPatch(VirtualEnclaveMaintenanceMode mode, IList<VirtualEnclavePrincipal> principals, VirtualEnclaveMaintenanceJustification? justification, IDictionary<string, BinaryData> additionalBinaryDataProperties)
         {
             Mode = mode;
             Principals = principals;
             Justification = justification;
-            _serializedAdditionalRawData = serializedAdditionalRawData;
-        }
-
-        /// <summary> Initializes a new instance of <see cref="VirtualEnclaveMaintenanceModeConfigurationPatch"/> for deserialization. </summary>
-        internal VirtualEnclaveMaintenanceModeConfigurationPatch()
-        {
+            _additionalBinaryDataProperties = additionalBinaryDataProperties;
         }
 
         /// <summary> Current mode of Maintenance Mode Configuration. </summary>
         public VirtualEnclaveMaintenanceMode Mode { get; }
+
         /// <summary> The user, group or service principal object affected by Maintenance Mode. </summary>
         public IList<VirtualEnclavePrincipal> Principals { get; }
+
         /// <summary> Justification for entering or exiting Maintenance Mode. </summary>
         public VirtualEnclaveMaintenanceJustification? Justification { get; set; }
     }
