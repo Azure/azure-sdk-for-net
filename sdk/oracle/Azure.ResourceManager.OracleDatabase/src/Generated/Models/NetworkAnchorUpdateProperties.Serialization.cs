aa9e404dd1b430f956379deb516ff26c0f6e44d1
Arthur Ma <arthurma@microsoft.com>
2025-12-27 16:51:06 +0800

    Migrage the OracleDatabase to support new MPG

--- sdk/oracle/Azure.ResourceManager.OracleDatabase/src/Generated/Models/NetworkAnchorUpdateProperties.Serialization.cs
+++ sdk/oracle/Azure.ResourceManager.OracleDatabase/src/Generated/Models/NetworkAnchorUpdateProperties.Serialization.cs
@@ -9,14 +9,15 @@ using System;
 using System.ClientModel.Primitives;
 using System.Collections.Generic;
 using System.Text.Json;
-using Azure.Core;
+using Azure.ResourceManager.OracleDatabase;
 
 namespace Azure.ResourceManager.OracleDatabase.Models
 {
-    public partial class NetworkAnchorUpdateProperties : IUtf8JsonSerializable, IJsonModel<NetworkAnchorUpdateProperties>
+    /// <summary> The updatable properties of the NetworkAnchor. </summary>
+    public partial class NetworkAnchorUpdateProperties : IJsonModel<NetworkAnchorUpdateProperties>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<NetworkAnchorUpdateProperties>)this).Write(writer, ModelSerializationExtensions.WireOptions);
-
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
         void IJsonModel<NetworkAnchorUpdateProperties>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
             writer.WriteStartObject();
@@ -28,12 +29,11 @@ namespace Azure.ResourceManager.OracleDatabase.Models
         /// <param name="options"> The client options for reading and writing models. </param>
         protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
-            var format = options.Format == "W" ? ((IPersistableModel<NetworkAnchorUpdateProperties>)this).GetFormatFromOptions(options) : options.Format;
+            string format = options.Format == "W" ? ((IPersistableModel<NetworkAnchorUpdateProperties>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(NetworkAnchorUpdateProperties)} does not support writing '{format}' format.");
             }
-
             if (Optional.IsDefined(OciBackupCidrBlock))
             {
                 writer.WritePropertyName("ociBackupCidrBlock"u8);
@@ -54,15 +54,15 @@ namespace Azure.ResourceManager.OracleDatabase.Models
                 writer.WritePropertyName("isOracleDnsForwardingEndpointEnabled"u8);
                 writer.WriteBooleanValue(IsOracleDnsForwardingEndpointEnabled.Value);
             }
-            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            if (options.Format != "W" && _additionalBinaryDataProperties != null)
             {
-                foreach (var item in _serializedAdditionalRawData)
+                foreach (var item in _additionalBinaryDataProperties)
                 {
                     writer.WritePropertyName(item.Key);
 #if NET6_0_OR_GREATER
-				writer.WriteRawValue(item.Value);
+                    writer.WriteRawValue(item.Value);
 #else
-                    using (JsonDocument document = JsonDocument.Parse(item.Value, ModelSerializationExtensions.JsonDocumentOptions))
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
                     {
                         JsonSerializer.Serialize(writer, document.RootElement);
                     }
@@ -71,22 +71,27 @@ namespace Azure.ResourceManager.OracleDatabase.Models
             }
         }
 
-        NetworkAnchorUpdateProperties IJsonModel<NetworkAnchorUpdateProperties>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        /// <param name="reader"> The JSON reader. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        NetworkAnchorUpdateProperties IJsonModel<NetworkAnchorUpdateProperties>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options) => JsonModelCreateCore(ref reader, options);
+
+        /// <param name="reader"> The JSON reader. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual NetworkAnchorUpdateProperties JsonModelCreateCore(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
-            var format = options.Format == "W" ? ((IPersistableModel<NetworkAnchorUpdateProperties>)this).GetFormatFromOptions(options) : options.Format;
+            string format = options.Format == "W" ? ((IPersistableModel<NetworkAnchorUpdateProperties>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(NetworkAnchorUpdateProperties)} does not support reading '{format}' format.");
             }
-
             using JsonDocument document = JsonDocument.ParseValue(ref reader);
             return DeserializeNetworkAnchorUpdateProperties(document.RootElement, options);
         }
 
-        internal static NetworkAnchorUpdateProperties DeserializeNetworkAnchorUpdateProperties(JsonElement element, ModelReaderWriterOptions options = null)
+        /// <param name="element"> The JSON element to deserialize. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        internal static NetworkAnchorUpdateProperties DeserializeNetworkAnchorUpdateProperties(JsonElement element, ModelReaderWriterOptions options)
         {
-            options ??= ModelSerializationExtensions.WireOptions;
-
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
@@ -95,55 +100,56 @@ namespace Azure.ResourceManager.OracleDatabase.Models
             bool? isOracleToAzureDnsZoneSyncEnabled = default;
             bool? isOracleDnsListeningEndpointEnabled = default;
             bool? isOracleDnsForwardingEndpointEnabled = default;
-            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
-            Dictionary<string, BinaryData> rawDataDictionary = new Dictionary<string, BinaryData>();
-            foreach (var property in element.EnumerateObject())
+            IDictionary<string, BinaryData> additionalBinaryDataProperties = new ChangeTrackingDictionary<string, BinaryData>();
+            foreach (var prop in element.EnumerateObject())
             {
-                if (property.NameEquals("ociBackupCidrBlock"u8))
+                if (prop.NameEquals("ociBackupCidrBlock"u8))
                 {
-                    ociBackupCidrBlock = property.Value.GetString();
+                    ociBackupCidrBlock = prop.Value.GetString();
                     continue;
                 }
-                if (property.NameEquals("isOracleToAzureDnsZoneSyncEnabled"u8))
+                if (prop.NameEquals("isOracleToAzureDnsZoneSyncEnabled"u8))
                 {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    if (prop.Value.ValueKind == JsonValueKind.Null)
                     {
                         continue;
                     }
-                    isOracleToAzureDnsZoneSyncEnabled = property.Value.GetBoolean();
+                    isOracleToAzureDnsZoneSyncEnabled = prop.Value.GetBoolean();
                     continue;
                 }
-                if (property.NameEquals("isOracleDnsListeningEndpointEnabled"u8))
+                if (prop.NameEquals("isOracleDnsListeningEndpointEnabled"u8))
                 {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    if (prop.Value.ValueKind == JsonValueKind.Null)
                     {
                         continue;
                     }
-                    isOracleDnsListeningEndpointEnabled = property.Value.GetBoolean();
+                    isOracleDnsListeningEndpointEnabled = prop.Value.GetBoolean();
                     continue;
                 }
-                if (property.NameEquals("isOracleDnsForwardingEndpointEnabled"u8))
+                if (prop.NameEquals("isOracleDnsForwardingEndpointEnabled"u8))
                 {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    if (prop.Value.ValueKind == JsonValueKind.Null)
                     {
                         continue;
                     }
-                    isOracleDnsForwardingEndpointEnabled = property.Value.GetBoolean();
+                    isOracleDnsForwardingEndpointEnabled = prop.Value.GetBoolean();
                     continue;
                 }
                 if (options.Format != "W")
                 {
-                    rawDataDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
                 }
             }
-            serializedAdditionalRawData = rawDataDictionary;
-            return new NetworkAnchorUpdateProperties(ociBackupCidrBlock, isOracleToAzureDnsZoneSyncEnabled, isOracleDnsListeningEndpointEnabled, isOracleDnsForwardingEndpointEnabled, serializedAdditionalRawData);
+            return new NetworkAnchorUpdateProperties(ociBackupCidrBlock, isOracleToAzureDnsZoneSyncEnabled, isOracleDnsListeningEndpointEnabled, isOracleDnsForwardingEndpointEnabled, additionalBinaryDataProperties);
         }
 
-        BinaryData IPersistableModel<NetworkAnchorUpdateProperties>.Write(ModelReaderWriterOptions options)
-        {
-            var format = options.Format == "W" ? ((IPersistableModel<NetworkAnchorUpdateProperties>)this).GetFormatFromOptions(options) : options.Format;
+        /// <param name="options"> The client options for reading and writing models. </param>
+        BinaryData IPersistableModel<NetworkAnchorUpdateProperties>.Write(ModelReaderWriterOptions options) => PersistableModelWriteCore(options);
 
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual BinaryData PersistableModelWriteCore(ModelReaderWriterOptions options)
+        {
+            string format = options.Format == "W" ? ((IPersistableModel<NetworkAnchorUpdateProperties>)this).GetFormatFromOptions(options) : options.Format;
             switch (format)
             {
                 case "J":
@@ -153,15 +159,20 @@ namespace Azure.ResourceManager.OracleDatabase.Models
             }
         }
 
-        NetworkAnchorUpdateProperties IPersistableModel<NetworkAnchorUpdateProperties>.Create(BinaryData data, ModelReaderWriterOptions options)
-        {
-            var format = options.Format == "W" ? ((IPersistableModel<NetworkAnchorUpdateProperties>)this).GetFormatFromOptions(options) : options.Format;
+        /// <param name="data"> The data to parse. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        NetworkAnchorUpdateProperties IPersistableModel<NetworkAnchorUpdateProperties>.Create(BinaryData data, ModelReaderWriterOptions options) => PersistableModelCreateCore(data, options);
 
+        /// <param name="data"> The data to parse. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual NetworkAnchorUpdateProperties PersistableModelCreateCore(BinaryData data, ModelReaderWriterOptions options)
+        {
+            string format = options.Format == "W" ? ((IPersistableModel<NetworkAnchorUpdateProperties>)this).GetFormatFromOptions(options) : options.Format;
             switch (format)
             {
                 case "J":
+                    using (JsonDocument document = JsonDocument.Parse(data, ModelSerializationExtensions.JsonDocumentOptions))
                     {
-                        using JsonDocument document = JsonDocument.Parse(data, ModelSerializationExtensions.JsonDocumentOptions);
                         return DeserializeNetworkAnchorUpdateProperties(document.RootElement, options);
                     }
                 default:
@@ -169,6 +180,7 @@ namespace Azure.ResourceManager.OracleDatabase.Models
             }
         }
 
+        /// <param name="options"> The client options for reading and writing models. </param>
         string IPersistableModel<NetworkAnchorUpdateProperties>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
