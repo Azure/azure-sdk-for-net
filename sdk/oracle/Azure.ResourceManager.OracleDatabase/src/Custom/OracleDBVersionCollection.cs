1cdc9f431b5200092ab68125358f9e1e09b5b564
Arthur Ma <arthurma@microsoft.com>
2026-01-01 15:32:03 +0800

    update

--- sdk/oracle/Azure.ResourceManager.OracleDatabase/src/Custom/OracleDBVersionCollection.cs
+++ sdk/oracle/Azure.ResourceManager.OracleDatabase/src/Custom/OracleDBVersionCollection.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
