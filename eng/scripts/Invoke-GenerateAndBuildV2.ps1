e3bfc8fd12fe5a3976d1cd92a12c3312f2ef6ad8
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2026-01-05 17:09:28 +0000

    disable dotnet sdk validation in spec pr for MPG migration.

--- eng/scripts/Invoke-GenerateAndBuildV2.ps1
+++ eng/scripts/Invoke-GenerateAndBuildV2.ps1
@@ -45,6 +45,30 @@ $relatedTypeSpecProjectFolder = $inputJson.relatedTypeSpecProjectFolder
 $apiVersion = $inputJson.apiVersion
 $sdkReleaseType = $inputJson.sdkReleaseType
 
+function IsMgmtSdkUsingNewGenerator {
+    param(
+        [string]$serviceType,
+        [string]$tspConfigFile,
+        [string]$sdkProjectFolder
+    )
+
+    if ($serviceType -ne 'resource-manager') {
+        return $false
+    }
+
+    $tspLocationFile = Join-Path $sdkProjectFolder "tsp-location.yaml"
+
+    if (-not (Test-Path $tspConfigFile) -or -not (Test-Path $tspLocationFile)) {
+        return $false
+    }
+
+    $tspConfigContent = Get-Content $tspConfigFile -Raw
+    $IsNewMgmtEmitter = $tspConfigContent -match '@azure-typespec/http-client-csharp-mgmt'
+    $tspLocationContent = Get-Content $tspLocationFile -Raw
+    $hasNewEmitterPackageJson = $tspLocationContent -match 'emitterPackageJsonPath:\s*eng/azure-typespec-http-client-csharp-mgmt-emitter-package.json'
+    return ($IsNewMgmtEmitter -and $hasNewEmitterPackageJson)
+}
+
 function Update-PackageVersionSuffix {
     param(
         [string]$csprojPath,
@@ -263,8 +287,9 @@ if ($relatedTypeSpecProjectFolder) {
             -specRepoRoot $swaggerDir
         }
 
-        $tspConfigContent = Get-Content $tspConfigFile -Raw
-        if (($serviceType -eq 'resource-manager' -and $tspConfigContent -match '@azure-typespec/http-client-csharp-mgmt') -or $serviceType -eq "data-plane") {
+        Write-Host "@@@@@@@@@@@@sdkPath: $sdkPath, relativeSdkPath: $relativeSdkPath, sdkProjectFolder: $sdkProjectFolder"
+
+        if ((IsMgmtSdkUsingNewGenerator -serviceType $serviceType -tspConfigFile $tspConfigFile -sdkProjectFolder $sdkProjectFolder) -or $serviceType -eq "data-plane") {
             $generatedSDKPackages[$generatedSDKPackages.Count - 1]['typespecProject'] = @($typespecRelativeFolder)
         }
     }
