9ddece605c9e40004ea338f6a9ef09a423cf9bf3
Arthur Ma <arthurma@microsoft.com>
2026-01-06 16:15:42 +0800

    Update the FileShares for first beta release

--- sdk/fileshares/Azure.ResourceManager.FileShares/CHANGELOG.md
+++ sdk/fileshares/Azure.ResourceManager.FileShares/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Release History
 
-## 1.0.0-beta.1 (2026-01-09)
+## 1.0.0-beta.1 (2026-01-06)
 
 ### Features Added
 
--- sdk/fileshares/Azure.ResourceManager.FileShares/src/Generated/Models/FileShareProvisioningRecommendationInputProperties.cs
+++ sdk/fileshares/Azure.ResourceManager.FileShares/src/Generated/Models/FileShareProvisioningRecommendationInputProperties.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 namespace Azure.ResourceManager.FileShares.Models
 {
     /// <summary> File share provisioning parameters recommendation API input structure. </summary>
-    public partial class FileShareProvisioningRecommendationInputProperties
+    internal partial class FileShareProvisioningRecommendationInputProperties
     {
         /// <summary> Keeps track of any properties unknown to the library. </summary>
         private protected readonly IDictionary<string, BinaryData> _additionalBinaryDataProperties;
