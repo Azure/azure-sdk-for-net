fc5dde59e62dc66e16fd4ffdff8c6be2f5d4dbe7
RodgeFu <wenjiefu@microsoft.com>
2026-01-06 11:02:50 +0800

    update

--- sdk/servicenetworking/Azure.ResourceManager.ServiceNetworking/src/Custom/Models/ProvisioningState.cs
+++ sdk/servicenetworking/Azure.ResourceManager.ServiceNetworking/src/Custom/Models/ProvisioningState.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 #nullable disable
@@ -56,6 +56,7 @@ namespace Azure.ResourceManager.ServiceNetworking.Models
         public override bool Equals(object obj) => obj is ProvisioningState other && Equals(other);
         /// <inheritdoc />
         public bool Equals(ProvisioningState other) => string.Equals(_value, other._value, StringComparison.InvariantCultureIgnoreCase);
+
         /// <inheritdoc />
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode() => _value != null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(_value) : 0;
