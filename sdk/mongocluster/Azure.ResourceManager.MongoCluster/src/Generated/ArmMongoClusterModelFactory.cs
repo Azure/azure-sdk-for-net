039c9a26768f7e4b0077d5e01a962823f39416cf
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2026-01-07 16:33:43 +0800

    refine

--- sdk/mongocluster/Azure.ResourceManager.MongoCluster/src/Generated/ArmMongoClusterModelFactory.cs
+++ sdk/mongocluster/Azure.ResourceManager.MongoCluster/src/Generated/ArmMongoClusterModelFactory.cs
@@ -7,7 +7,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using Azure;
 using Azure.Core;
@@ -347,20 +346,6 @@ namespace Azure.ResourceManager.MongoCluster.Models
             roles ??= new ChangeTrackingList<MongoClusterDatabaseRole>();
 
             return new MongoClusterUserProperties(provisioningState, identityProvider, roles.ToList(), additionalBinaryDataProperties: null);
-        }
-
-        /// <summary> Initializes a new instance of <see cref="Models.MongoClusterPrivateEndpointConnectionProperties"/>. </summary>
-        /// <param name="groupIds"> The group ids for the private endpoint resource. </param>
-        /// <param name="privateEndpointId"> The private endpoint resource. </param>
-        /// <param name="privateLinkServiceConnectionState"> A collection of information about the state of the connection between service consumer and provider. </param>
-        /// <param name="provisioningState"> The provisioning state of the private endpoint connection resource. </param>
-        /// <returns> A new <see cref="Models.MongoClusterPrivateEndpointConnectionProperties"/> instance for mocking. </returns>
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public static MongoClusterPrivateEndpointConnectionProperties MongoClusterPrivateEndpointConnectionProperties(IEnumerable<string> groupIds, ResourceIdentifier privateEndpointId, MongoClusterPrivateLinkServiceConnectionState privateLinkServiceConnectionState, MongoClusterPrivateEndpointConnectionProvisioningState? provisioningState)
-        {
-            groupIds ??= new ChangeTrackingList<string>();
-
-            return new MongoClusterPrivateEndpointConnectionProperties(groupIds.ToList(), default, privateLinkServiceConnectionState, provisioningState, additionalBinaryDataProperties: null);
         }
     }
 }
