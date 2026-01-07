3993c0210745ea6ce23bbe3e909236c28561a4be
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2026-01-07 12:54:34 +0800

    rebase and regen

--- sdk/mongocluster/Azure.ResourceManager.MongoCluster/src/Generated/Models/MongoClusterEntraIdentityProvider.cs
+++ sdk/mongocluster/Azure.ResourceManager.MongoCluster/src/Generated/Models/MongoClusterEntraIdentityProvider.cs
@@ -7,7 +7,6 @@
 
 using System;
 using System.Collections.Generic;
-using Azure.ResourceManager.MongoCluster;
 
 namespace Azure.ResourceManager.MongoCluster.Models
 {
@@ -15,13 +14,11 @@ namespace Azure.ResourceManager.MongoCluster.Models
     public partial class MongoClusterEntraIdentityProvider : MongoClusterIdentityProvider
     {
         /// <summary> Initializes a new instance of <see cref="MongoClusterEntraIdentityProvider"/>. </summary>
-        /// <param name="properties"> The Entra identity properties for the user. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="properties"/> is null. </exception>
-        public MongoClusterEntraIdentityProvider(MongoClusterEntraIdentityProviderProperties properties) : base(IdentityProviderType.MicrosoftEntraID)
+        /// <param name="mongoClusterEntraIdentityProviderPrincipalType"> The principal type of the user. </param>
+        public MongoClusterEntraIdentityProvider(MongoClusterEntraPrincipalType? mongoClusterEntraIdentityProviderPrincipalType) : base(IdentityProviderType.MicrosoftEntraID)
         {
-            Argument.AssertNotNull(properties, nameof(properties));
 
-            Properties = properties;
+            Properties = mongoClusterEntraIdentityProviderPrincipalType is null ? default : new MongoClusterEntraIdentityProviderProperties(mongoClusterEntraIdentityProviderPrincipalType.Value);
         }
 
         /// <summary> Initializes a new instance of <see cref="MongoClusterEntraIdentityProvider"/>. </summary>
