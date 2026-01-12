ee6ead1ed8d8c3f17b51e1c94611026fc6880cce
m-nash <64171366+m-nash@users.noreply.github.com>
2026-01-12 14:11:08 -0800

    fixes

--- sdk/core/System.ClientModel/src/Convenience/ClientConnection.cs
+++ sdk/core/System.ClientModel/src/Convenience/ClientConnection.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.Extensions.Configuration;
-
 namespace System.ClientModel.Primitives;
 
 /// <summary>
