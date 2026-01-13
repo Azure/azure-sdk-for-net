561725edfbcf594981a072939ff1c419346364cd
JonathanCrd <JonathanCrd@users.noreply.github.com>
2026-01-13 13:35:42 -0800

    rename model factory and regenerate

--- sdk/purview/Azure.Analytics.Purview.DataMap/src/Custom/AnalyticsPurviewDataMapModelFactory.cs
+++ sdk/purview/Azure.Analytics.Purview.DataMap/src/Custom/AnalyticsPurviewDataMapModelFactory.cs
@@ -1,9 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using Azure.Core;
+using Microsoft.TypeSpec.Generator.Customizations;
 
 namespace Azure.Analytics.Purview.DataMap;
 
--- sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AnalyticsPurviewDataMapModelFactory.cs
+++ sdk/purview/Azure.Analytics.Purview.DataMap/src/Generated/AnalyticsPurviewDataMapModelFactory.cs
@@ -12,7 +12,7 @@ using System.Linq;
 namespace Azure.Analytics.Purview.DataMap
 {
     /// <summary> A factory class for creating instances of the models for mocking. </summary>
-    public static partial class PurviewDataMapModelFactory
+    public static partial class AnalyticsPurviewDataMapModelFactory
     {
         /// <summary>
         /// An instance of an entity along with extended info - like hive_table,
