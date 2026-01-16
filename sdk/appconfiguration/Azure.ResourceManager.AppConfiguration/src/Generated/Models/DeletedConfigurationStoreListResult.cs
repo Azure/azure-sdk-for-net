43252d6fd15f0df5c4737149140f9d6cd8241de6
azure-sdk <azuresdk@microsoft.com>
2026-01-16 17:12:35 +0000

    Configurations:  'specification/appconfiguration/resource-manager/Microsoft.AppConfiguration/AppConfiguration/tspconfig.yaml', API Version: 2025-06-01-preview, SDK Release Type: beta, and CommitSHA: '073bfdce33b47ec2c5b0cd50e7b95f6734604daf' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=5763717 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/appconfiguration/Azure.ResourceManager.AppConfiguration/src/Generated/Models/DeletedConfigurationStoreListResult.cs
+++ sdk/appconfiguration/Azure.ResourceManager.AppConfiguration/src/Generated/Models/DeletedConfigurationStoreListResult.cs
@@ -7,64 +7,38 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Azure.ResourceManager.AppConfiguration.Models
+namespace Azure.ResourceManager.AppConfiguration
 {
-    /// <summary> List of deleted configuration stores. </summary>
+    /// <summary> The response of a DeletedConfigurationStore list operation. </summary>
     internal partial class DeletedConfigurationStoreListResult
     {
-        /// <summary>
-        /// Keeps track of any properties unknown to the library.
-        /// <para>
-        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
-        /// </para>
-        /// <para>
-        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
-        /// </para>
-        /// <para>
-        /// Examples:
-        /// <list type="bullet">
-        /// <item>
-        /// <term>BinaryData.FromObjectAsJson("foo")</term>
-        /// <description>Creates a payload of "foo".</description>
-        /// </item>
-        /// <item>
-        /// <term>BinaryData.FromString("\"foo\"")</term>
-        /// <description>Creates a payload of "foo".</description>
-        /// </item>
-        /// <item>
-        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
-        /// <description>Creates a payload of { "key": "value" }.</description>
-        /// </item>
-        /// <item>
-        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
-        /// <description>Creates a payload of { "key": "value" }.</description>
-        /// </item>
-        /// </list>
-        /// </para>
-        /// </summary>
-        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+        /// <summary> Keeps track of any properties unknown to the library. </summary>
+        private protected readonly IDictionary<string, BinaryData> _additionalBinaryDataProperties;
 
         /// <summary> Initializes a new instance of <see cref="DeletedConfigurationStoreListResult"/>. </summary>
-        internal DeletedConfigurationStoreListResult()
+        /// <param name="value"> The DeletedConfigurationStore items on this page. </param>
+        internal DeletedConfigurationStoreListResult(IEnumerable<DeletedConfigurationStore> value)
         {
-            Value = new ChangeTrackingList<DeletedAppConfigurationStoreData>();
+            Value = value.ToList();
         }
 
         /// <summary> Initializes a new instance of <see cref="DeletedConfigurationStoreListResult"/>. </summary>
-        /// <param name="value"> The list of deleted configuration store. </param>
-        /// <param name="nextLink"> The URL to get the next set of deleted configuration stores. </param>
-        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
-        internal DeletedConfigurationStoreListResult(IReadOnlyList<DeletedAppConfigurationStoreData> value, string nextLink, IDictionary<string, BinaryData> serializedAdditionalRawData)
+        /// <param name="value"> The DeletedConfigurationStore items on this page. </param>
+        /// <param name="nextLink"> The link to the next page of items. </param>
+        /// <param name="additionalBinaryDataProperties"> Keeps track of any properties unknown to the library. </param>
+        internal DeletedConfigurationStoreListResult(IList<DeletedConfigurationStore> value, Uri nextLink, IDictionary<string, BinaryData> additionalBinaryDataProperties)
         {
             Value = value;
             NextLink = nextLink;
-            _serializedAdditionalRawData = serializedAdditionalRawData;
+            _additionalBinaryDataProperties = additionalBinaryDataProperties;
         }
 
-        /// <summary> The list of deleted configuration store. </summary>
-        public IReadOnlyList<DeletedAppConfigurationStoreData> Value { get; }
-        /// <summary> The URL to get the next set of deleted configuration stores. </summary>
-        public string NextLink { get; }
+        /// <summary> The DeletedConfigurationStore items on this page. </summary>
+        public IList<DeletedConfigurationStore> Value { get; }
+
+        /// <summary> The link to the next page of items. </summary>
+        public Uri NextLink { get; }
     }
 }
