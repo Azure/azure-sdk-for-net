1d6669891e9d503e8b1eb534d0e3acfbfac163a8
Zhonglei Ma <v-zhongleima@microsoft.com>
2026-01-22 21:40:23 +0800

    [NeonPostgres] Verify that the new code generator generates SDK code from TSP (#54329)

--- sdk/neonpostgres/Azure.ResourceManager.NeonPostgres/src/Generated/Models/PreflightCheckResult.cs
+++ sdk/neonpostgres/Azure.ResourceManager.NeonPostgres/src/Generated/Models/PreflightCheckResult.cs
@@ -13,37 +13,8 @@ namespace Azure.ResourceManager.NeonPostgres.Models
     /// <summary> Result of the pre-deletion validation operation. </summary>
     public partial class PreflightCheckResult
     {
-        /// <summary>
-        /// Keeps track of any properties unknown to the library.
-        /// <para>
-        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
-        /// </para>
-        /// <para>
-        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
-        /// </para>
-        /// <para>
-        /// Examples:
-        /// <list type="bullet">
-        /// <item>
-        /// <term>BinaryData.FromObjectAsJson("foo")</term>
-        /// <description>Creates a payload of "foo".</description>
-        /// </item>
-        /// <item>
-        /// <term>BinaryData.FromString("\"foo\"")</term>
-        /// <description>Creates a payload of "foo".</description>
-        /// </item>
-        /// <item>
-        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
-        /// <description>Creates a payload of { "key": "value" }.</description>
-        /// </item>
-        /// <item>
-        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
-        /// <description>Creates a payload of { "key": "value" }.</description>
-        /// </item>
-        /// </list>
-        /// </para>
-        /// </summary>
-        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+        /// <summary> Keeps track of any properties unknown to the library. </summary>
+        private protected readonly IDictionary<string, BinaryData> _additionalBinaryDataProperties;
 
         /// <summary> Initializes a new instance of <see cref="PreflightCheckResult"/>. </summary>
         /// <param name="isValid"> Indicates whether action is allowed. </param>
@@ -55,21 +26,17 @@ namespace Azure.ResourceManager.NeonPostgres.Models
         /// <summary> Initializes a new instance of <see cref="PreflightCheckResult"/>. </summary>
         /// <param name="isValid"> Indicates whether action is allowed. </param>
         /// <param name="reason"> Optional message in case action is not allowed. </param>
-        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
-        internal PreflightCheckResult(bool isValid, string reason, IDictionary<string, BinaryData> serializedAdditionalRawData)
+        /// <param name="additionalBinaryDataProperties"> Keeps track of any properties unknown to the library. </param>
+        internal PreflightCheckResult(bool isValid, string reason, IDictionary<string, BinaryData> additionalBinaryDataProperties)
         {
             IsValid = isValid;
             Reason = reason;
-            _serializedAdditionalRawData = serializedAdditionalRawData;
-        }
-
-        /// <summary> Initializes a new instance of <see cref="PreflightCheckResult"/> for deserialization. </summary>
-        internal PreflightCheckResult()
-        {
+            _additionalBinaryDataProperties = additionalBinaryDataProperties;
         }
 
         /// <summary> Indicates whether action is allowed. </summary>
         public bool IsValid { get; }
+
         /// <summary> Optional message in case action is not allowed. </summary>
         public string Reason { get; }
     }
