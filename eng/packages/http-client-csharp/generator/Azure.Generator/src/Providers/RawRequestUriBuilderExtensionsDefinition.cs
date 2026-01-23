4b1c2ba2f8b79e027f5d7cc7afce1110bcea9f7a
Radhika Gupta <radhgupta@microsoft.com>
2026-01-23 09:56:15 -0800

    updateQuery

--- eng/packages/http-client-csharp/generator/Azure.Generator/src/Providers/RawRequestUriBuilderExtensionsDefinition.cs
+++ eng/packages/http-client-csharp/generator/Azure.Generator/src/Providers/RawRequestUriBuilderExtensionsDefinition.cs
@@ -10,6 +10,7 @@ using Microsoft.TypeSpec.Generator.Expressions;
 using Microsoft.TypeSpec.Generator.Primitives;
 using Microsoft.TypeSpec.Generator.Providers;
 using Microsoft.TypeSpec.Generator.Snippets;
+using Microsoft.TypeSpec.Generator.Statements;
 using static Microsoft.TypeSpec.Generator.Snippets.Snippet;
 
 namespace Azure.Generator.Providers
@@ -79,9 +80,9 @@ namespace Azure.Generator.Providers
 
         private MethodProvider BuildUpdateQueryMethod()
         {
-            var uriBuilder = new ParameterProvider("builder", "The request URI builder instance.", typeof(RawRequestUriBuilder));
-            var nameParameter = new ParameterProvider("name", "The name of the query parameter.", typeof(string));
-            var valueParameter = new ParameterProvider("value", "The value of the query parameter.", typeof(string));
+            var uriBuilder = new ParameterProvider("builder", $"The request URI builder instance.", typeof(RawRequestUriBuilder));
+            var nameParameter = new ParameterProvider("name", $"The name of the query parameter.", typeof(string));
+            var valueParameter = new ParameterProvider("value", $"The value of the query parameter.", typeof(string));
 
             var parameters = new[] { uriBuilder, nameParameter, valueParameter };
             var modifiers = MethodSignatureModifiers.Public | MethodSignatureModifiers.Static | MethodSignatureModifiers.Extension;
@@ -91,54 +92,70 @@ namespace Azure.Generator.Providers
                 Modifiers: modifiers,
                 Parameters: parameters,
                 ReturnType: null,
-                Description: "Updates an existing query parameter or adds a new one if it doesn't exist.",
+                Description: $"Updates an existing query parameter or adds a new one if it doesn't exist.",
                 ReturnDescription: null);
 
-            // Get the current query string
-            var currentQuery = uriBuilder.Property("Query");
-            var searchPattern = nameParameter.Invoke("Concat", Literal("="));
-            
-            var methodBody = new MethodBodyStatement[]
+            var body = new[]
             {
-                // string currentQuery = builder.Query;
-                Declare("currentQuery", typeof(string), currentQuery, out var currentQueryVar),
-                
-                // Check if parameter exists in query
-                new IfStatement(currentQueryVar.Invoke("Contains", searchPattern))
+                MethodBodyStatement.Empty,
+                // Get the current query string
+                Declare("currentQuery", typeof(string), uriBuilder.Property("Query"), out var currentQueryVar),
+
+                // Create search pattern "name="
+                Declare("searchPattern", typeof(string), new BinaryOperatorExpression("+", nameParameter, Literal("=")), out var searchPattern),
+
+                // Check if parameter exists in query string (at start or after &)
+                Declare("paramStartIndex", typeof(int), Literal(-1), out var paramStartIndex),
+
+                // Check if parameter is at the beginning of the query
+                new IfStatement(currentQueryVar.Invoke("StartsWith", searchPattern))
                 {
-                    // Parameter exists - update its value
-                    Declare("paramIndex", typeof(int), currentQueryVar.Invoke("IndexOf", searchPattern), out var paramIndexVar),
-                    Declare("valueStartIndex", typeof(int), paramIndexVar.Add(searchPattern.Property("Length")), out var valueStartIndexVar),
-                    Declare("valueEndIndex", typeof(int), currentQueryVar.Invoke("IndexOf", [Literal('&'), valueStartIndexVar]), out var valueEndIndexVar),
-                    
-                    // If valueEndIndex is -1, set it to the length of the query
-                    new IfStatement(valueEndIndexVar.Equal(Int(-1)))
-                    {
-                        valueEndIndexVar.Assign(currentQueryVar.Property("Length")).Terminate()
-                    },
-                    
-                    // Build the new query string
-                    Declare("newQuery", typeof(string), 
-                        currentQueryVar.Invoke("Substring", [Int(0), valueStartIndexVar])
-                            .Invoke("Concat", valueParameter)
-                            .Invoke("Concat", currentQueryVar.Invoke("Substring", valueEndIndexVar)), 
-                        out var newQueryVar),
-                    
-                    // Update the builder's query
-                    uriBuilder.Property("Query").Assign(newQueryVar).Terminate()
+                    paramStartIndex.Assign(Int(0)).Terminate()
                 },
-                new ElseStatement
+
+                // Check if parameter is in the middle/end (preceded by &)
+                new IfStatement(paramStartIndex.Equal(Int(-1)))
                 {
-                    // Parameter doesn't exist - append it
+                    Declare("prefixedPattern", typeof(string), new BinaryOperatorExpression("+", Literal("&"), searchPattern), out var prefixedPattern),
+                    Declare("prefixedIndex", typeof(int), currentQueryVar.Invoke("IndexOf", prefixedPattern), out var prefixedIndex),
+                    new IfStatement(prefixedIndex.GreaterThanOrEqual(Int(0)))
+                    {
+                        paramStartIndex.Assign(new BinaryOperatorExpression("+", prefixedIndex, Int(1))).Terminate()
+                    }
+                },
+
+                new IfElseStatement(
+                    paramStartIndex.GreaterThanOrEqual(Int(0)),
+                    // Parameter exists - replace its value
+                    new MethodBodyStatement[]
+                    {
+                        Declare("valueStartIndex", typeof(int), new BinaryOperatorExpression("+", paramStartIndex, searchPattern.Property("Length")), out var valueStartIndex),
+                        Declare("valueEndIndex", typeof(int), currentQueryVar.Invoke("IndexOf", [Literal('&'), valueStartIndex]), out var valueEndIndex),
+
+                        // If no & found after parameter, it goes to end of query
+                        new IfStatement(valueEndIndex.Equal(Int(-1)))
+                        {
+                            valueEndIndex.Assign(currentQueryVar.Property("Length")).Terminate()
+                        },
+
+                        // Build new query: before + newValue + after
+                        Declare("beforeParam", typeof(string), currentQueryVar.Invoke("Substring", [Int(0), valueStartIndex]), out var beforeParam),
+                        Declare("afterParam", typeof(string), currentQueryVar.Invoke("Substring", valueEndIndex), out var afterParam),
+                        Declare("newQuery", typeof(string), new BinaryOperatorExpression("+", new BinaryOperatorExpression("+", beforeParam, valueParameter), afterParam), out var newQuery),
+
+                        // Set the new query
+                        uriBuilder.Property("Query").Assign(newQuery).Terminate()
+                    },
+                    // Parameter doesn't exist - append it using existing method
                     new InvokeMethodExpression(
-                        uriBuilder, 
-                        nameof(RawRequestUriBuilder.AppendQuery), 
+                        uriBuilder,
+                        nameof(RawRequestUriBuilder.AppendQuery),
                         [nameParameter, valueParameter, Bool(true)]
                     ).Terminate()
-                }
+                )
             };
 
-            return new MethodProvider(signature, methodBody, this, XmlDocProvider.Empty);
+            return new MethodProvider(signature, body, this, XmlDocProvider.Empty);
         }
     }
 }
