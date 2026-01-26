0af8ab752d617174b5b1e6dbbbeef4c784c27fd2
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2026-01-26 04:39:46 +0000

    refine

--- sdk/selfhelp/Azure.ResourceManager.SelfHelp/src/Custom/ArmSelfHelpModelFactory.cs
+++ sdk/selfhelp/Azure.ResourceManager.SelfHelp/src/Custom/ArmSelfHelpModelFactory.cs
@@ -27,45 +27,5 @@ namespace Azure.ResourceManager.SelfHelp.Models
         {
             return new SelfHelpSolutionMetadata(id, name, resourceType, systemData, default, null);
         }
-
-        /// <summary> Solution replacement maps. </summary>
-        /// <param name="webResults"> Solution AzureKB results. </param>
-        /// <param name="diagnostics"> Solution diagnostics results. </param>
-        /// <param name="troubleshooters"> Solutions Troubleshooters. </param>
-        /// <param name="metricsBasedCharts"> Solution metrics based charts. </param>
-        /// <param name="videos"> Video solutions, which have the power to engage the customer by stimulating their senses. </param>
-        /// <param name="videoGroups"> Group of Videos. </param>
-        /// <returns> A new <see cref="Models.SolutionReplacementMaps"/> instance for mocking. </returns>
-        public static SolutionReplacementMaps SolutionReplacementMaps(IEnumerable<KBWebResult> webResults = default, IEnumerable<SolutionsDiagnostic> diagnostics = default, IEnumerable<SolutionsTroubleshooters> troubleshooters = default, IEnumerable<MetricsBasedChart> metricsBasedCharts = default, IEnumerable<SelfHelpVideo> videos = default, IEnumerable<VideoGroupDetail> videoGroups = default)
-        {
-            webResults ??= new ChangeTrackingList<KBWebResult>();
-            diagnostics ??= new ChangeTrackingList<SolutionsDiagnostic>();
-            troubleshooters ??= new ChangeTrackingList<SolutionsTroubleshooters>();
-            metricsBasedCharts ??= new ChangeTrackingList<MetricsBasedChart>();
-            videos ??= new ChangeTrackingList<SelfHelpVideo>();
-            videoGroups ??= new ChangeTrackingList<VideoGroupDetail>();
-
-            return new SolutionReplacementMaps(
-                webResults.ToList(),
-                diagnostics.ToList(),
-                troubleshooters.ToList(),
-                metricsBasedCharts.ToList(),
-                videos.ToList(),
-                videoGroups.ToList(),
-                additionalBinaryDataProperties: null);
-        }
-        /// <summary> Solution replacement maps. </summary>
-        /// <param name="webResults"> Solution AzureKB results. </param>
-        /// <param name="videos"> Video solutions, which have the power to engage the customer by stimulating their senses. </param>
-        /// <param name="videoGroups"> Group of Videos. </param>
-        /// <returns> A new <see cref="Models.ReplacementMapsResult"/> instance for mocking. </returns>
-        public static ReplacementMapsResult ReplacementMapsResult(IEnumerable<KBWebResult> webResults = default, IEnumerable<SelfHelpVideo> videos = default, IEnumerable<VideoGroupDetail> videoGroups = default)
-        {
-            webResults ??= new ChangeTrackingList<KBWebResult>();
-            videos ??= new ChangeTrackingList<SelfHelpVideo>();
-            videoGroups ??= new ChangeTrackingList<VideoGroupDetail>();
-
-            return new ReplacementMapsResult(webResults.ToList(), videos.ToList(), videoGroups.ToList(), additionalBinaryDataProperties: null);
-        }
     }
 }
