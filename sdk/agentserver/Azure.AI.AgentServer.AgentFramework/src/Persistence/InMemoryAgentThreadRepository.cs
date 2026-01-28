cced67009f9452222c39724614067f537629be46
lusu-msft <68949729+lusu-msft@users.noreply.github.com>
2026-01-28 13:56:06 -0800

    [AgentServer][AF] add AgentThreadRepository (#55284)
    * add agent thread repository
    
    * add AgentThreadRepository
    
    * refined agent thread repository
    
    * remove unused code

---
+++ sdk/agentserver/Azure.AI.AgentServer.AgentFramework/src/Persistence/InMemoryAgentThreadRepository.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Agents.AI;
+
+namespace Azure.AI.AgentServer.AgentFramework.Persistence
+{
+    /// <summary>
+    /// An in-memory implementation of the IAgentThreadRepository interface.
+    /// </summary>
+    public class InMemoryAgentThreadRepository : IAgentThreadRepository
+    {
+        private readonly Dictionary<string, AgentThread> _threads = new();
+        private readonly AIAgent? _agent;
+
+        /// <summary>
+        /// Instructor of InMemoryAgentThreadRepository
+        /// </summary>
+        /// <param name="agent">AIAgent instance to associate with the threads.</param>
+        public InMemoryAgentThreadRepository(AIAgent? agent = null)
+        {
+            _agent = agent;
+        }
+
+        /// <inheritdoc/>
+        public Task<AgentThread> Get(string conversationId, AIAgent? agent = null)
+        {
+            if (!_threads.ContainsKey(conversationId))
+            {
+                agent ??= _agent;
+
+                if (agent == null)
+                {
+                    throw new InvalidOperationException("Agent instance must be provided either in the instructor or in the method call.");
+                }
+                return Task.FromResult(agent.GetNewThread());
+            }
+            return Task.FromResult(_threads[conversationId]);
+        }
+
+        /// <inheritdoc/>
+        public Task Set(string conversationId, AgentThread agentThread)
+        {
+            _threads[conversationId] = agentThread;
+            return Task.CompletedTask;
+        }
+    }
+}
