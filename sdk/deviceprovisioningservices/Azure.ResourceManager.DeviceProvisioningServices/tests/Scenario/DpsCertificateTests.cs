bb08d92e741f4943b2790ae7c72a85963668bbe8
Arthur Ma <arthurma@microsoft.com>
2026-01-29 13:38:58 +0800

    update

--- sdk/deviceprovisioningservices/Azure.ResourceManager.DeviceProvisioningServices/tests/Scenario/DpsCertificateTests.cs
+++ sdk/deviceprovisioningservices/Azure.ResourceManager.DeviceProvisioningServices/tests/Scenario/DpsCertificateTests.cs
@@ -31,7 +31,7 @@ namespace Azure.ResourceManager.DeviceProvisioningServices.Tests
             _dpsCertificateCollection = dps.GetDeviceProvisioningServicesCertificates();
         }
 
-        [Test]
+        [RecordedTest]
         [PlaybackOnly("Re-record needs to give a correct _certContent value")]
         public async Task DpsCertificateOperations()
         {
