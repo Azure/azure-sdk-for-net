cd777051c1a41ec80df4d43b2278ec5d6579b907
Meghana Gupta <meghanagupta@microsoft.com>
2026-02-04 13:27:31 -0500

    Update tests for servicegroups SDK

--- sdk/servicegroups/Azure.ResourceManager.ServiceGroups/tests/Scenario/ServiceGroupCollectionTests.cs
+++ sdk/servicegroups/Azure.ResourceManager.ServiceGroups/tests/Scenario/ServiceGroupCollectionTests.cs
@@ -3,8 +3,9 @@
 
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Core;
 using Azure.Core.TestFramework;
-using Azure.ResourceManager.ServiceGroups;
+using Azure.ResourceManager.ServiceGroups.Models;
 using NUnit.Framework;
 
 namespace Azure.ResourceManager.ServiceGroups.Tests.Scenario
@@ -16,65 +17,125 @@ namespace Azure.ResourceManager.ServiceGroups.Tests.Scenario
         {
         }
 
-        [TestCase]
         [RecordedTest]
-        public async Task CreateOrUpdate()
+        public async Task ServiceGroupCollectionApiTests()
         {
-            await foreach (var tenant in Client.GetTenants().GetAllAsync())
+            var tenantCollection = await Client.GetTenants().GetAllAsync().ToEnumerableAsync();
+            var tenant = tenantCollection.Find(t => t.Data.TenantId.ToString().Equals(TestEnvironment.TenantId));
+            var serviceGroupCollection = tenant.GetServiceGroups();
+
+            // 1. Create Collection
+            string serviceGroupName = Recording.GenerateAssetName("testsg-");
+
+            ServiceGroupData data = new ServiceGroupData
             {
-                var collection = tenant.GetServiceGroups();
+                Properties = new ServiceGroupProperties
+                {
+                    DisplayName = $"Test ServiceGroup {serviceGroupName}",
+                    Parent = new ParentServiceGroupProperties
+                    {
+                        ResourceId = new ResourceIdentifier($"/providers/Microsoft.Management/serviceGroups/{tenant.Data.TenantId}"),
+                    }
+                },
+            };
 
-                string serviceGroupName = Recording.GenerateAssetName("testsg");
-                var data = new ServiceGroupData();
+            var lro = await serviceGroupCollection.CreateOrUpdateAsync(WaitUntil.Completed, serviceGroupName, data);
+            ServiceGroupResource serviceGroup = lro.Value;
 
-                var lro = await collection.CreateOrUpdateAsync(WaitUntil.Completed, serviceGroupName, data);
-                var serviceGroup = lro.Value;
+            Assert.IsNotNull(serviceGroup);
+            Assert.IsNotNull(serviceGroup.Data);
+            Assert.AreEqual(serviceGroupName, serviceGroup.Data.Name);
+            Assert.AreEqual($"Test ServiceGroup {serviceGroupName}", serviceGroup.Data.Properties.DisplayName);
+            Assert.AreEqual(ServiceGroupResource.ResourceType, serviceGroup.Data.ResourceType);
 
-                Assert.IsNotNull(serviceGroup);
-                Assert.AreEqual(serviceGroupName, serviceGroup.Data.Name);
-                break;
-            }
-        }
+            // 2. Get collection
+            serviceGroup = await serviceGroupCollection.GetAsync(serviceGroupName);
 
-        [TestCase]
-        [RecordedTest]
-        public async Task Get()
-        {
-            await foreach (var tenant in Client.GetTenants().GetAllAsync())
+            Assert.IsNotNull(serviceGroup);
+            Assert.IsNotNull(serviceGroup.Data);
+            Assert.AreEqual(serviceGroupName, serviceGroup.Data.Name);
+            Assert.AreEqual(ServiceGroupResource.ResourceType, serviceGroup.Data.ResourceType);
+
+            // 3. Update Collection
+            ServiceGroupData updatedData = new ServiceGroupData
             {
-                var collection = tenant.GetServiceGroups();
+                Properties = new ServiceGroupProperties
+                {
+                    DisplayName = $"Updated ServiceGroup {serviceGroupName}",
+                    Parent = new ParentServiceGroupProperties
+                    {
+                        ResourceId = new ResourceIdentifier($"/providers/Microsoft.Management/serviceGroups/{tenant.Data.TenantId}"),
+                    }
+                },
+            };
 
-                string serviceGroupName = Recording.GenerateAssetName("testsg");
-                var data = new ServiceGroupData();
+            ServiceGroupResource updatedServiceGroup = (await serviceGroupCollection.CreateOrUpdateAsync(WaitUntil.Completed, serviceGroupName, updatedData)).Value;
 
-                await collection.CreateOrUpdateAsync(WaitUntil.Completed, serviceGroupName, data);
+            Assert.IsNotNull(updatedServiceGroup);
+            Assert.AreEqual(serviceGroupName, updatedServiceGroup.Data.Name);
+            Assert.AreEqual(updatedData.Properties.DisplayName, updatedServiceGroup.Data.Properties.DisplayName);
+            Assert.AreEqual(TestEnvironment.TenantId, updatedServiceGroup.Data.Properties.Parent.ResourceId.Name);
 
-                var serviceGroup = await collection.GetAsync(serviceGroupName);
+            // 4. List Ancestors Collection
+            var serviceGroupName2 = Recording.GenerateAssetName("testsg-");
 
-                Assert.IsNotNull(serviceGroup);
-                Assert.AreEqual(serviceGroupName, serviceGroup.Value.Data.Name);
-                break;
-            }
-        }
-
-        [TestCase]
-        [RecordedTest]
-        public async Task Exists()
-        {
-            await foreach (var tenant in Client.GetTenants().GetAllAsync())
+            ServiceGroupData data2 = new ServiceGroupData
             {
-                var collection = tenant.GetServiceGroups();
+                Properties = new ServiceGroupProperties
+                {
+                    DisplayName = $"Test ServiceGroup {serviceGroupName2}",
+                    Parent = new ParentServiceGroupProperties
+                    {
+                        ResourceId = new ResourceIdentifier(serviceGroup.Id),
+                    }
+                },
+            };
 
-                string serviceGroupName = Recording.GenerateAssetName("testsg");
-                var data = new ServiceGroupData();
+            lro = await serviceGroupCollection.CreateOrUpdateAsync(WaitUntil.Completed, serviceGroupName2, data2);
+            ServiceGroupResource serviceGroup2 = lro.Value;
 
-                await collection.CreateOrUpdateAsync(WaitUntil.Completed, serviceGroupName, data);
+            var serviceGroupName3 = Recording.GenerateAssetName("testsg-");
 
-                bool exists = await collection.ExistsAsync(serviceGroupName);
+            ServiceGroupData data3 = new ServiceGroupData
+            {
+                Properties = new ServiceGroupProperties
+                {
+                    DisplayName = $"Test ServiceGroup {serviceGroupName3}",
+                    Parent = new ParentServiceGroupProperties
+                    {
+                        ResourceId = new ResourceIdentifier(serviceGroup2.Id),
+                    }
+                },
+            };
 
-                Assert.IsTrue(exists);
-                break;
-            }
+            lro = await serviceGroupCollection.CreateOrUpdateAsync(WaitUntil.Completed, serviceGroupName3, data3);
+            ServiceGroupResource serviceGroup3 = lro.Value;
+
+            var ancestors = await serviceGroupCollection.GetAncestorsAsync(serviceGroup3.Data.Name).ToEnumerableAsync();
+            Assert.IsNotNull(ancestors);
+            Assert.IsNotEmpty(ancestors);
+            Assert.IsTrue(ancestors.Any(ancestor => ancestor.Data.Name.Equals(serviceGroupName)));
+            Assert.IsTrue(ancestors.Any(ancestor => ancestor.Data.Name.Equals(serviceGroupName2)));
+            Assert.IsTrue(ancestors.Any(ancestor => ancestor.Data.Name.Equals(serviceGroupName3)));
+            Assert.IsTrue(ancestors.Any(ancestor => ancestor.Data.Name.Equals(tenant.Data.TenantId.ToString())));
+            Assert.AreEqual(4, ancestors.Count);
+
+            // 5. Exists
+            Assert.IsTrue(await serviceGroupCollection.ExistsAsync(serviceGroup.Data.Name));
+            Assert.IsFalse(await serviceGroupCollection.ExistsAsync(serviceGroup.Data.Name + "x"));
+
+            // 6. GetIfExists
+            var serviceGroupIfExists = await serviceGroupCollection.GetIfExistsAsync(serviceGroup.Data.Name);
+            Assert.IsTrue(serviceGroupIfExists.HasValue);
+            Assert.IsNotNull(serviceGroupIfExists.Value);
+            serviceGroupIfExists = await serviceGroupCollection.GetIfExistsAsync(serviceGroup.Data.Name + "x");
+            Assert.IsFalse(serviceGroupIfExists.HasValue);
+            Assert.AreEqual(404, serviceGroupIfExists.GetRawResponse().Status);
+
+            // 7. Clean up all created SGs
+            await serviceGroup3.DeleteAsync(WaitUntil.Completed);
+            await serviceGroup2.DeleteAsync(WaitUntil.Completed);
+            await serviceGroup.DeleteAsync(WaitUntil.Completed);
         }
     }
 }
