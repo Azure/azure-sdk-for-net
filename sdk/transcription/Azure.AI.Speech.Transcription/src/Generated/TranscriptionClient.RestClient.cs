7a10e1793b53c0d4fdb5a2dc85b1b1866f9796f4
wangamber <wangamber@microsoft.com>
2026-02-04 11:36:30 -0800

    regenerate sdk

--- sdk/transcription/Azure.AI.Speech.Transcription/src/Generated/TranscriptionClient.RestClient.cs
+++ sdk/transcription/Azure.AI.Speech.Transcription/src/Generated/TranscriptionClient.RestClient.cs
@@ -12,7 +12,7 @@ namespace Azure.AI.Speech.Transcription
     {
         private static PipelineMessageClassifier _pipelineMessageClassifier200;
 
-        private static PipelineMessageClassifier PipelineMessageClassifier200 => _pipelineMessageClassifier200 = PipelineMessageClassifier.Create(stackalloc ushort[] { 200 });
+        private static PipelineMessageClassifier PipelineMessageClassifier200 => _pipelineMessageClassifier200 ??= PipelineMessageClassifier.Create(stackalloc ushort[] { 200 });
 
         internal PipelineMessage CreateTranscribeRequest(BinaryContent content, string contentType, RequestOptions options)
         {
