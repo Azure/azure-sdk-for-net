5cd78b782d9122f7e458bbea969ee14f3fc0c4f3
Jorge Rangel <jorgerangel@microsoft.com>
2026-02-11 10:28:04 -0600

    regen transcription

--- sdk/transcription/Azure.AI.Speech.Transcription/src/Generated/Models/TranscriptionOptions.Serialization.cs
+++ sdk/transcription/Azure.AI.Speech.Transcription/src/Generated/Models/TranscriptionOptions.Serialization.cs
@@ -29,6 +29,19 @@ namespace Azure.AI.Speech.Transcription
             }
         }
 
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual BinaryData PersistableModelWriteCore(ModelReaderWriterOptions options)
+        {
+            string format = options.Format == "W" ? ((IPersistableModel<TranscriptionOptions>)this).GetFormatFromOptions(options) : options.Format;
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options, AzureAISpeechTranscriptionContext.Default);
+                default:
+                    throw new FormatException($"The model {nameof(TranscriptionOptions)} does not support writing '{options.Format}' format.");
+            }
+        }
+
         /// <param name="writer"> The JSON writer. </param>
         /// <param name="options"> The client options for reading and writing models. </param>
         void IJsonModel<TranscriptionOptions>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
@@ -286,19 +299,6 @@ namespace Azure.AI.Speech.Transcription
 
         /// <param name="options"> The client options for reading and writing models. </param>
         BinaryData IPersistableModel<TranscriptionOptions>.Write(ModelReaderWriterOptions options) => PersistableModelWriteCore(options);
-
-        /// <param name="options"> The client options for reading and writing models. </param>
-        protected virtual BinaryData PersistableModelWriteCore(ModelReaderWriterOptions options)
-        {
-            string format = options.Format == "W" ? ((IPersistableModel<TranscriptionOptions>)this).GetFormatFromOptions(options) : options.Format;
-            switch (format)
-            {
-                case "J":
-                    return ModelReaderWriter.Write(this, options, AzureAISpeechTranscriptionContext.Default);
-                default:
-                    throw new FormatException($"The model {nameof(TranscriptionOptions)} does not support writing '{options.Format}' format.");
-            }
-        }
 
         /// <param name="data"> The data to parse. </param>
         /// <param name="options"> The client options for reading and writing models. </param>
