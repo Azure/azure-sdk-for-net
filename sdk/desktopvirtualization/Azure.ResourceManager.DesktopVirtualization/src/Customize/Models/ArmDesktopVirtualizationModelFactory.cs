28bfb4580d68c36d32d9379273ab914846f747a5
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2026-02-27 19:17:04 +0000

    fix

--- sdk/desktopvirtualization/Azure.ResourceManager.DesktopVirtualization/src/Customize/Models/ArmDesktopVirtualizationModelFactory.cs
+++ sdk/desktopvirtualization/Azure.ResourceManager.DesktopVirtualization/src/Customize/Models/ArmDesktopVirtualizationModelFactory.cs
@@ -43,47 +43,28 @@ namespace Azure.ResourceManager.DesktopVirtualization.Models
             return DesktopVirtualizationPrivateEndpointConnectionDataData(id, name, resourceType, systemData, groupIds: default, privateEndpointId, connectionState, provisioningState);
         }
 
-        /// <summary> Initializes a new instance of <see cref="DesktopVirtualization.VirtualApplicationData"/>. </summary>
-        /// <param name="id"> The id. </param>
-        /// <param name="name"> The name. </param>
-        /// <param name="resourceType"> The resourceType. </param>
-        /// <param name="systemData"> The systemData. </param>
-        /// <param name="objectId"> ObjectId of Application. (internal use). </param>
-        /// <param name="description"> Description of Application. </param>
-        /// <param name="friendlyName"> Friendly name of Application. </param>
-        /// <param name="filePath"> Specifies a path for the executable file for the application. </param>
-        /// <param name="msixPackageFamilyName"> Specifies the package family name for MSIX applications. </param>
-        /// <param name="msixPackageApplicationId"> Specifies the package application Id for MSIX applications. </param>
-        /// <param name="applicationType"> Resource Type of Application. </param>
-        /// <param name="commandLineSetting"> Specifies whether this published application can be launched with command line arguments provided by the client, command line arguments specified at publish time, or no command line arguments at all. </param>
-        /// <param name="commandLineArguments"> Command Line Arguments for Application. </param>
-        /// <param name="showInPortal"> Specifies whether to show the RemoteApp program in the RD Web Access server. </param>
-        /// <param name="iconPath"> Path to icon. </param>
-        /// <param name="iconIndex"> Index of the icon. </param>
-        /// <param name="iconHash"> Hash of the icon. </param>
-        /// <param name="iconContent"> the icon a]  64bit stringEde icon. </param>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="resourceType"></param>
+        /// <param name="systemData"></param>
+        /// <param name="objectId"></param>
+        /// <param name="description"></param>
+        /// <param name="friendlyName"></param>
+        /// <param name="filePath"></param>
+        /// <param name="msixPackageFamilyName"></param>
+        /// <param name="msixPackageApplicationId"></param>
+        /// <param name="applicationType"></param>
+        /// <param name="commandLineSetting"></param>
+        /// <param name="commandLineArguments"></param>
+        /// <param name="showInPortal"></param>
+        /// <param name="iconPath"></param>
+        /// <param name="iconIndex"></param>
+        /// <param name="iconHash"></param>
+        /// <param name="iconContent"></param>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public static VirtualApplicationData VirtualApplicationData(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, string objectId, string description, string friendlyName, string filePath, string msixPackageFamilyName, string msixPackageApplicationId, RemoteApplicationType? applicationType, VirtualApplicationCommandLineSetting commandLineSetting, string commandLineArguments, bool? showInPortal, string iconPath, int? iconIndex, string iconHash, BinaryData iconContent)
         {
-            return VirtualApplicationData(
-                id: id,
-                name: name,
-                resourceType: resourceType,
-                systemData: systemData,
-                objectId: objectId,
-                description: description,
-                friendlyName: friendlyName,
-                filePath: filePath,
-                msixPackageFamilyName: msixPackageFamilyName,
-                msixPackageApplicationId: msixPackageApplicationId,
-                applicationType: applicationType,
-                commandLineSetting: commandLineSetting,
-                commandLineArguments: commandLineArguments,
-                showInPortal: showInPortal,
-                iconPath: iconPath,
-                iconIndex: iconIndex,
-                iconHash: iconHash,
-                iconContent: iconContent);
+            return VirtualApplicationData(id, name, resourceType, systemData, objectId, description, friendlyName, filePath, msixPackageFamilyName, msixPackageApplicationId, applicationType, (VirtualApplicationCommandLineSetting?)commandLineSetting, commandLineArguments, showInPortal, iconPath, iconIndex, iconHash, iconContent);
         }
     }
 }
