a64e44eb277e40c037844d5df7c0ab739a728800
James May <fowl2@users.noreply.github.com>
2026-02-27 12:36:45 +1100

    Update sdk/identity/Azure.Identity/src/Credentials/ClientCertificateCredential.cs
    Co-authored-by: Christopher Scott <chriscott@hotmail.com>

--- sdk/identity/Azure.Identity/src/Credentials/ClientCertificateCredential.cs
+++ sdk/identity/Azure.Identity/src/Credentials/ClientCertificateCredential.cs
@@ -53,9 +53,11 @@ namespace Azure.Identity
         /// <param name="clientId">The client (application) ID of the service principal</param>
         /// <param name="clientCertificatePath">The file system or certificate store path to the client certificate and private key.
         /// For example:
-        /// * c:\data\certificate.pfx
-        /// * /etc/app/cert.pem
-        /// * cert:/CurrentUser/My/E661583E8FABEF4C0BEF694CBC41C28FB81CD870
+        /// <list type="bullet">
+        ///   <item><description><c>c:\data\certificate.pfx</c></description></item>
+        ///   <item><description><c>/etc/app/cert.pem</c></description></item>
+        ///   <item><description><c>cert:/CurrentUser/My/E661583E8FABEF4C0BEF694CBC41C28FB81CD870</c></description></item>
+        /// </list>
         /// </param>
         public ClientCertificateCredential(string tenantId, string clientId, string clientCertificatePath)
             : this(tenantId, clientId, clientCertificatePath, null, null, null, null)
