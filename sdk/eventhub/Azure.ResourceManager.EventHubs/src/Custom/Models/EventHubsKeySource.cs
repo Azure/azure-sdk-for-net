c38b2fd0eec8d85dde3426e8b2621369030915d4
Zhonglei Ma <v-zhongleima@microsoft.com>
2026-03-06 13:53:16 +0800

    regen by copilot

---
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/src/Custom/Models/EventHubsKeySource.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using Microsoft.TypeSpec.Generator.Customizations;
+
+namespace Azure.ResourceManager.EventHubs.Models
+{
+    /// <summary> Enumerates the possible value of KeySource for Encryption. </summary>
+    [CodeGenType("EventHubsEncryptionKeySource")]
+    public readonly partial struct EventHubsKeySource
+    {
+    }
+}
