e23d246abd78b312cf7d7203f01ba0c3683fd0a5
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2026-03-08 15:45:33 +0000

    MPG migration - relay

--- sdk/relay/Azure.ResourceManager.Relay/src/Generated/Models/RelayAccessRight.cs
+++ sdk/relay/Azure.ResourceManager.Relay/src/Generated/Models/RelayAccessRight.cs
@@ -7,48 +7,67 @@
 
 using System;
 using System.ComponentModel;
+using Azure.ResourceManager.Relay;
 
 namespace Azure.ResourceManager.Relay.Models
 {
-    /// <summary> The RelayAccessRight. </summary>
+    /// <summary></summary>
     public readonly partial struct RelayAccessRight : IEquatable<RelayAccessRight>
     {
         private readonly string _value;
-
-        /// <summary> Initializes a new instance of <see cref="RelayAccessRight"/>. </summary>
-        /// <exception cref="ArgumentNullException"> <paramref name="value"/> is null. </exception>
-        public RelayAccessRight(string value)
-        {
-            _value = value ?? throw new ArgumentNullException(nameof(value));
-        }
-
         private const string ManageValue = "Manage";
         private const string SendValue = "Send";
         private const string ListenValue = "Listen";
 
-        /// <summary> Manage. </summary>
+        /// <summary> Initializes a new instance of <see cref="RelayAccessRight"/>. </summary>
+        /// <param name="value"> The value. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="value"/> is null. </exception>
+        public RelayAccessRight(string value)
+        {
+            Argument.AssertNotNull(value, nameof(value));
+
+            _value = value;
+        }
+
+        /// <summary> Gets the Manage. </summary>
         public static RelayAccessRight Manage { get; } = new RelayAccessRight(ManageValue);
-        /// <summary> Send. </summary>
+
+        /// <summary> Gets the Send. </summary>
         public static RelayAccessRight Send { get; } = new RelayAccessRight(SendValue);
-        /// <summary> Listen. </summary>
+
+        /// <summary> Gets the Listen. </summary>
         public static RelayAccessRight Listen { get; } = new RelayAccessRight(ListenValue);
+
         /// <summary> Determines if two <see cref="RelayAccessRight"/> values are the same. </summary>
+        /// <param name="left"> The left value to compare. </param>
+        /// <param name="right"> The right value to compare. </param>
         public static bool operator ==(RelayAccessRight left, RelayAccessRight right) => left.Equals(right);
+
         /// <summary> Determines if two <see cref="RelayAccessRight"/> values are not the same. </summary>
+        /// <param name="left"> The left value to compare. </param>
+        /// <param name="right"> The right value to compare. </param>
         public static bool operator !=(RelayAccessRight left, RelayAccessRight right) => !left.Equals(right);
-        /// <summary> Converts a <see cref="string"/> to a <see cref="RelayAccessRight"/>. </summary>
+
+        /// <summary> Converts a string to a <see cref="RelayAccessRight"/>. </summary>
+        /// <param name="value"> The value. </param>
         public static implicit operator RelayAccessRight(string value) => new RelayAccessRight(value);
 
-        /// <inheritdoc />
+        /// <summary> Converts a string to a <see cref="RelayAccessRight"/>. </summary>
+        /// <param name="value"> The value. </param>
+        public static implicit operator RelayAccessRight?(string value) => value == null ? null : new RelayAccessRight(value);
+
+        /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override bool Equals(object obj) => obj is RelayAccessRight other && Equals(other);
-        /// <inheritdoc />
+
+        /// <inheritdoc/>
         public bool Equals(RelayAccessRight other) => string.Equals(_value, other._value, StringComparison.InvariantCultureIgnoreCase);
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode() => _value != null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(_value) : 0;
-        /// <inheritdoc />
+
+        /// <inheritdoc/>
         public override string ToString() => _value;
     }
 }
