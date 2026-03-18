1f8957a23836b27439768e77082375e16cc8fa6f
nick863 <30440255+nick863@users.noreply.github.com>
2026-03-18 16:35:30 -0700

    Fix deserialization of keepalive event

--- sdk/ai/Azure.AI.Agents.Persistent/tests/UnitTests.cs
+++ sdk/ai/Azure.AI.Agents.Persistent/tests/UnitTests.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Net.ServerSentEvents;
 using System.Text;
 using NUnit.Framework;
@@ -151,9 +153,6 @@ namespace Azure.AI.Agents.Persistent.Tests
         [TestCase("Mock error", "MockEvent")]
         [TestCase("Mock error", "")]
         [TestCase("Mock error", null)]
-        [TestCase("", "MockEvent")]
-        [TestCase("", "")]
-        [TestCase("", null)]
         [TestCase("long", "MockEvent")]
         public void TestSseError(string errorText, string eventType)
         {
@@ -179,6 +178,16 @@ namespace Azure.AI.Agents.Persistent.Tests
             InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() => StreamingUpdate.FromEvent(badItem));
             Assert.That(exception.Message.StartsWith($"The stream returned invalid JSON: \"{expectedError}\" for event of type {expectedEvent}. Original error:"), Is.True, exception.Message);
         }
+
+        [Test]
+        public void TestSseKeepalive()
+        {
+            SseItem<byte[]> keepaliveItem = new(new byte[] { }, "keepalive");
+            List<StreamingUpdate> updates = [..StreamingUpdate.FromEvent(keepaliveItem)];
+            Assert.That(updates.Count, Is.EqualTo(1));
+            Assert.That(updates[0], Is.InstanceOf<KeepAliveUpdate>());
+        }
+
         #region helpers
         private static void AssertMcpListsEqual(IEnumerable<string> expectedAlways, IEnumerable<string> expectedNever, MCPApprovalPerTool observedApproval)
         {
