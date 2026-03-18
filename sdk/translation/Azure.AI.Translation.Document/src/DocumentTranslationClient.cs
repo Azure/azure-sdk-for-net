97f45945a2e84265df4b22573ae43ab7afba95c9
JonathanCrd <JonathanCrd@users.noreply.github.com>
2026-03-18 03:25:05 -0700

    Revert change in parameter name for GetTranslationStatus

--- sdk/translation/Azure.AI.Translation.Document/src/DocumentTranslationClient.cs
+++ sdk/translation/Azure.AI.Translation.Document/src/DocumentTranslationClient.cs
@@ -17,6 +17,10 @@ namespace Azure.AI.Translation.Document
     [CodeGenSuppress("DocumentTranslationClient", typeof(Uri), typeof(AzureKeyCredential), typeof(DocumentTranslationClientOptions))]
     [CodeGenSuppress("StartTranslation", typeof(WaitUntil), typeof(TranslationBatch), typeof(CancellationToken))]
     [CodeGenSuppress("StartTranslationAsync", typeof(WaitUntil), typeof(TranslationBatch), typeof(CancellationToken))]
+    [CodeGenSuppress("GetTranslationsStatus", typeof(int?), typeof(int?), typeof(int?), typeof(IEnumerable<Guid>), typeof(IEnumerable<string>), typeof(DateTimeOffset?), typeof(DateTimeOffset?), typeof(IEnumerable<string>), typeof(CancellationToken))]
+    [CodeGenSuppress("GetTranslationsStatusAsync", typeof(int?), typeof(int?), typeof(int?), typeof(IEnumerable<Guid>), typeof(IEnumerable<string>), typeof(DateTimeOffset?), typeof(DateTimeOffset?), typeof(IEnumerable<string>), typeof(CancellationToken))]
+    [CodeGenSuppress("GetTranslationsStatus", typeof(int?), typeof(int?), typeof(int?), typeof(IEnumerable<Guid>), typeof(IEnumerable<string>), typeof(DateTimeOffset?), typeof(DateTimeOffset?), typeof(IEnumerable<string>), typeof(RequestContext))]
+    [CodeGenSuppress("GetTranslationsStatusAsync", typeof(int?), typeof(int?), typeof(int?), typeof(IEnumerable<Guid>), typeof(IEnumerable<string>), typeof(DateTimeOffset?), typeof(DateTimeOffset?), typeof(IEnumerable<string>), typeof(RequestContext))]
     public partial class DocumentTranslationClient
     {
         internal readonly DocumentTranslationClient _serviceClient;
@@ -264,11 +268,11 @@ namespace Azure.AI.Translation.Document
             var orderByList = options?.OrderBy.Count > 0 ? options.OrderBy.Select(order => order.ToGenerated()) : null;
 
             return GetDocumentTranslationClient().GetTranslationsStatus(
-                    translationIds: idList,
+                    ids: idList,
                     statuses: statusList,
                     createdDateTimeUtcStart: options?.CreatedAfter,
                     createdDateTimeUtcEnd: options?.CreatedBefore,
-                    orderby: orderByList,
+                    orderBy: orderByList,
                     cancellationToken: cancellationToken);
         }
 
@@ -284,12 +288,118 @@ namespace Azure.AI.Translation.Document
             var orderByList = options?.OrderBy.Count > 0 ? options.OrderBy.Select(order => order.ToGenerated()) : null;
 
             return GetDocumentTranslationClient().GetTranslationsStatusAsync(
-            translationIds: idList,
+            ids: idList,
             statuses: statusList,
             createdDateTimeUtcStart: options?.CreatedAfter,
             createdDateTimeUtcEnd: options?.CreatedBefore,
-            orderby: orderByList,
+            orderBy: orderByList,
             cancellationToken: cancellationToken);
+        }
+
+        /// <summary> Returns a list of batch requests submitted and the status for each request. </summary>
+        /// <param name="maxCount"> top indicates the total number of records the user wants to be returned across all pages. </param>
+        /// <param name="skip"> skip indicates the number of records to skip from the list of records held by the server based on the sorting method specified. By default, we sort by descending start time. </param>
+        /// <param name="maxpagesize"> maxpagesize is the maximum items returned in a page. </param>
+        /// <param name="ids"> Ids to use in filtering. </param>
+        /// <param name="statuses"> Statuses to use in filtering. </param>
+        /// <param name="createdDateTimeUtcStart"> the start datetime to get items after. </param>
+        /// <param name="createdDateTimeUtcEnd"> the end datetime to get items before. </param>
+        /// <param name="orderBy"> the sorting query for the collection (ex: 'CreatedDateTimeUtc asc','CreatedDateTimeUtc desc'). </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        public virtual AsyncPageable<TranslationStatusResult> GetTranslationsStatusAsync(int? maxCount = null, int? skip = null, int? maxpagesize = null, IEnumerable<Guid> ids = null, IEnumerable<string> statuses = null, DateTimeOffset? createdDateTimeUtcStart = null, DateTimeOffset? createdDateTimeUtcEnd = null, IEnumerable<string> orderBy = null, CancellationToken cancellationToken = default)
+        {
+            return new DocumentTranslationClientGetTranslationsStatusAsyncCollectionResultOfT(
+                this,
+                maxCount,
+                skip,
+                maxpagesize,
+                ids,
+                statuses,
+                createdDateTimeUtcStart,
+                createdDateTimeUtcEnd,
+                orderBy,
+                cancellationToken.ToRequestContext());
+        }
+
+        /// <summary> Returns a list of batch requests submitted and the status for each request. </summary>
+        /// <param name="maxCount"> top indicates the total number of records the user wants to be returned across all pages. </param>
+        /// <param name="skip"> skip indicates the number of records to skip from the list of records held by the server based on the sorting method specified. By default, we sort by descending start time. </param>
+        /// <param name="maxpagesize"> maxpagesize is the maximum items returned in a page. </param>
+        /// <param name="ids"> Ids to use in filtering. </param>
+        /// <param name="statuses"> Statuses to use in filtering. </param>
+        /// <param name="createdDateTimeUtcStart"> the start datetime to get items after. </param>
+        /// <param name="createdDateTimeUtcEnd"> the end datetime to get items before. </param>
+        /// <param name="orderBy"> the sorting query for the collection (ex: 'CreatedDateTimeUtc asc','CreatedDateTimeUtc desc'). </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        public virtual Pageable<TranslationStatusResult> GetTranslationsStatus(int? maxCount = null, int? skip = null, int? maxpagesize = null, IEnumerable<Guid> ids = null, IEnumerable<string> statuses = null, DateTimeOffset? createdDateTimeUtcStart = null, DateTimeOffset? createdDateTimeUtcEnd = null, IEnumerable<string> orderBy = null, CancellationToken cancellationToken = default)
+        {
+            return new DocumentTranslationClientGetTranslationsStatusCollectionResultOfT(
+                this,
+                maxCount,
+                skip,
+                maxpagesize,
+                ids,
+                statuses,
+                createdDateTimeUtcStart,
+                createdDateTimeUtcEnd,
+                orderBy,
+                cancellationToken.ToRequestContext());
+        }
+
+        /// <summary>
+        /// [Protocol Method] Returns a list of batch requests submitted and the status for each request.
+        /// </summary>
+        /// <param name="maxCount"> top indicates the total number of records the user wants to be returned across all pages. </param>
+        /// <param name="skip"> skip indicates the number of records to skip from the list of records held by the server based on the sorting method specified. By default, we sort by descending start time. </param>
+        /// <param name="maxpagesize"> maxpagesize is the maximum items returned in a page. </param>
+        /// <param name="ids"> Ids to use in filtering. </param>
+        /// <param name="statuses"> Statuses to use in filtering. </param>
+        /// <param name="createdDateTimeUtcStart"> the start datetime to get items after. </param>
+        /// <param name="createdDateTimeUtcEnd"> the end datetime to get items before. </param>
+        /// <param name="orderBy"> the sorting query for the collection (ex: 'CreatedDateTimeUtc asc','CreatedDateTimeUtc desc'). </param>
+        /// <param name="context"> The request context, which can override default behaviors of the client pipeline on a per-call basis. </param>
+        /// <exception cref="RequestFailedException"> Service returned a non-success status code. </exception>
+        public virtual AsyncPageable<BinaryData> GetTranslationsStatusAsync(int? maxCount, int? skip, int? maxpagesize, IEnumerable<Guid> ids, IEnumerable<string> statuses, DateTimeOffset? createdDateTimeUtcStart, DateTimeOffset? createdDateTimeUtcEnd, IEnumerable<string> orderBy, RequestContext context)
+        {
+            return new DocumentTranslationClientGetTranslationsStatusAsyncCollectionResult(
+                this,
+                maxCount,
+                skip,
+                maxpagesize,
+                ids,
+                statuses,
+                createdDateTimeUtcStart,
+                createdDateTimeUtcEnd,
+                orderBy,
+                context);
+        }
+
+        /// <summary>
+        /// [Protocol Method] Returns a list of batch requests submitted and the status for each request.
+        /// </summary>
+        /// <param name="maxCount"> top indicates the total number of records the user wants to be returned across all pages. </param>
+        /// <param name="skip"> skip indicates the number of records to skip from the list of records held by the server based on the sorting method specified. By default, we sort by descending start time. </param>
+        /// <param name="maxpagesize"> maxpagesize is the maximum items returned in a page. </param>
+        /// <param name="ids"> Ids to use in filtering. </param>
+        /// <param name="statuses"> Statuses to use in filtering. </param>
+        /// <param name="createdDateTimeUtcStart"> the start datetime to get items after. </param>
+        /// <param name="createdDateTimeUtcEnd"> the end datetime to get items before. </param>
+        /// <param name="orderBy"> the sorting query for the collection (ex: 'CreatedDateTimeUtc asc','CreatedDateTimeUtc desc'). </param>
+        /// <param name="context"> The request context, which can override default behaviors of the client pipeline on a per-call basis. </param>
+        /// <exception cref="RequestFailedException"> Service returned a non-success status code. </exception>
+        public virtual Pageable<BinaryData> GetTranslationsStatus(int? maxCount, int? skip, int? maxpagesize, IEnumerable<Guid> ids, IEnumerable<string> statuses, DateTimeOffset? createdDateTimeUtcStart, DateTimeOffset? createdDateTimeUtcEnd, IEnumerable<string> orderBy, RequestContext context)
+        {
+            return new DocumentTranslationClientGetTranslationsStatusCollectionResult(
+                this,
+                maxCount,
+                skip,
+                maxpagesize,
+                ids,
+                statuses,
+                createdDateTimeUtcStart,
+                createdDateTimeUtcEnd,
+                orderBy,
+                context);
         }
 
         #region supported formats functions nobody wants to see these
