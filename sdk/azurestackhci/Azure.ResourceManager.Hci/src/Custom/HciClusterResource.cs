93597878f01877656019159e4eef77d179be77ac
Wei Hu <huwe@microsoft.com>
2026-04-07 14:06:51 +0800

    remove unnecessary obsolete

--- sdk/azurestackhci/Azure.ResourceManager.Hci/src/Custom/HciClusterResource.cs
+++ sdk/azurestackhci/Azure.ResourceManager.Hci/src/Custom/HciClusterResource.cs
@@ -12,13 +12,11 @@ namespace Azure.ResourceManager.Hci
     public partial class HciClusterResource
     {
         /// <summary> List Offers available across publishers for the HCI Cluster (backward-compat). </summary>
-        [Obsolete("This method is now deprecated. Please use GetByCluster instead.")]
         [EditorBrowsable(EditorBrowsableState.Never)]
         public virtual AsyncPageable<HciClusterOfferResource> GetHciClusterOffersAsync(string expand = default, CancellationToken cancellationToken = default)
             => GetByClusterAsync(expand, cancellationToken);
 
         /// <summary> List Offers available across publishers for the HCI Cluster (backward-compat). </summary>
-        [Obsolete("This method is now deprecated. Please use GetByCluster instead.")]
         [EditorBrowsable(EditorBrowsableState.Never)]
         public virtual Pageable<HciClusterOfferResource> GetHciClusterOffers(string expand = default, CancellationToken cancellationToken = default)
             => GetByCluster(expand, cancellationToken);
