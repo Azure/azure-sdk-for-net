12befa07155b09c0686e9493abb64f7c1af06a04
haiyuan_zhang <haiyuan.zhang1982@gmail.com>
2026-04-14 13:26:44 +0800

    address review comments

--- sdk/providerhub/Azure.ResourceManager.ProviderHub/src/Custom/Models/ProviderFrontloadPayloadProperties.cs
+++ sdk/providerhub/Azure.ResourceManager.ProviderHub/src/Custom/Models/ProviderFrontloadPayloadProperties.cs
@@ -1,9 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
