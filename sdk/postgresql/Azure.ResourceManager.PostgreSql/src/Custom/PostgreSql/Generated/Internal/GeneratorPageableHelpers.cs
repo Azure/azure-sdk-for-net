ac216f1b9ec8d7ae782f2739c3f01331769ddcfa
Wei Hu <huwe@microsoft.com>
2026-04-29 13:57:56 +0800

    Revert "cleanup"
    This reverts commit 89d3f38a541c99ca793f559ba3ced1de0c07b6cd.

---
+++ sdk/postgresql/Azure.ResourceManager.PostgreSql/src/Custom/PostgreSql/Generated/Internal/GeneratorPageableHelpers.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Text.Json;
+using System.Threading;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace Azure.ResourceManager.PostgreSql
+{
+    internal static class GeneratorPageableHelpers
+    {
+        public static AsyncPageable<T> CreateAsyncPageable<T>(Func<int?, HttpMessage> createFirstPageRequest, Func<int?, string, HttpMessage> createNextPageRequest, Func<JsonElement, T> valueFactory, ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string scopeName, string itemPropertyName, string nextLinkPropertyName, CancellationToken cancellationToken) where T : notnull
+        {
+            return PageableHelpers.CreateAsyncPageable(createFirstPageRequest, createNextPageRequest, valueFactory, clientDiagnostics, pipeline, scopeName, itemPropertyName, nextLinkPropertyName, cancellationToken);
+        }
+
+        public static Pageable<T> CreatePageable<T>(Func<int?, HttpMessage> createFirstPageRequest, Func<int?, string, HttpMessage> createNextPageRequest, Func<JsonElement, T> valueFactory, ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string scopeName, string itemPropertyName, string nextLinkPropertyName, CancellationToken cancellationToken) where T : notnull
+        {
+            return PageableHelpers.CreatePageable(createFirstPageRequest, createNextPageRequest, valueFactory, clientDiagnostics, pipeline, scopeName, itemPropertyName, nextLinkPropertyName, cancellationToken);
+        }
+    }
+}
