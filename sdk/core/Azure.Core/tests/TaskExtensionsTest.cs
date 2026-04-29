3199dd82b2a8d463f617cf7d36eaa6278b4f350d
JonathanCrd <JonathanCrd@users.noreply.github.com>
2026-04-29 11:11:12 -0700

    Update and add tests

--- sdk/core/Azure.Core/tests/TaskExtensionsTest.cs
+++ sdk/core/Azure.Core/tests/TaskExtensionsTest.cs
@@ -65,21 +65,29 @@ namespace Azure.Core.Tests
             var tcs = new TaskCompletionSource<int>();
             var cts = new CancellationTokenSource();
             var awaiter = tcs.Task.AwaitWithCancellation(cts.Token).GetAwaiter();
+            using var continuationCalledEvent = new ManualResetEventSlim(false);
             var continuationCalled = 0;
 
             Assert.AreEqual(false, awaiter.IsCompleted);
-            awaiter.UnsafeOnCompleted(() => continuationCalled++);
+            awaiter.UnsafeOnCompleted(() =>
+            {
+                Interlocked.Increment(ref continuationCalled);
+                continuationCalledEvent.Set();
+            });
 
             Assert.AreEqual(false, awaiter.IsCompleted);
             Assert.AreEqual(0, continuationCalled);
             cts.Cancel();
 
-            Assert.AreEqual(1, continuationCalled);
+            // The continuation is now scheduled asynchronously on the thread pool
+            // to prevent StackOverflowException from deep synchronous call stacks.
+            Assert.IsTrue(continuationCalledEvent.Wait(TimeSpan.FromSeconds(5)), "Continuation was not called within timeout");
+            Assert.AreEqual(1, Volatile.Read(ref continuationCalled));
             Assert.AreEqual(true, awaiter.IsCompleted);
             Assert.Catch<OperationCanceledException>(() => awaiter.GetResult());
 
             tcs.SetResult(0);
-            Assert.AreEqual(1, continuationCalled);
+            Assert.AreEqual(1, Volatile.Read(ref continuationCalled));
         }
 
         [Test]
@@ -156,21 +164,29 @@ namespace Azure.Core.Tests
             var tcs = new TaskCompletionSource<int>();
             var cts = new CancellationTokenSource();
             var awaiter = new ValueTask<int>(tcs.Task).AwaitWithCancellation(cts.Token).GetAwaiter();
+            using var continuationCalledEvent = new ManualResetEventSlim(false);
             var continuationCalled = 0;
 
             Assert.AreEqual(false, awaiter.IsCompleted);
-            awaiter.UnsafeOnCompleted(() => continuationCalled++);
+            awaiter.UnsafeOnCompleted(() =>
+            {
+                Interlocked.Increment(ref continuationCalled);
+                continuationCalledEvent.Set();
+            });
 
             Assert.AreEqual(false, awaiter.IsCompleted);
             Assert.AreEqual(0, continuationCalled);
             cts.Cancel();
 
-            Assert.AreEqual(1, continuationCalled);
+            // The continuation is now scheduled asynchronously on the thread pool
+            // to prevent StackOverflowException from deep synchronous call stacks.
+            Assert.IsTrue(continuationCalledEvent.Wait(TimeSpan.FromSeconds(5)), "Continuation was not called within timeout");
+            Assert.AreEqual(1, Volatile.Read(ref continuationCalled));
             Assert.AreEqual(true, awaiter.IsCompleted);
             Assert.Catch<OperationCanceledException>(() => awaiter.GetResult());
 
             tcs.SetResult(0);
-            Assert.AreEqual(1, continuationCalled);
+            Assert.AreEqual(1, Volatile.Read(ref continuationCalled));
         }
 
         [Test]
@@ -191,6 +207,104 @@ namespace Azure.Core.Tests
             Assert.AreEqual(true, continuationCalled);
             Assert.AreEqual(true, awaiter.IsCompleted);
             Assert.Catch<OperationCanceledException>(() => awaiter.GetResult(), "Error");
+        }
+
+        [Test]
+        public void TaskExtensions_CancellationDoesNotRunContinuationSynchronouslyOnCallerStack()
+        {
+            // Regression test for https://github.com/Azure/azure-sdk-for-net/issues/57412
+            // Verifies that cancelling a token does not run AwaitWithCancellation continuations
+            // synchronously on the CancellationTokenSource.Cancel() caller's stack, which would
+            // cause StackOverflowException on constrained-stack environments.
+            var tcs = new TaskCompletionSource<int>();
+            var cts = new CancellationTokenSource();
+            var awaiter = tcs.Task.AwaitWithCancellation(cts.Token).GetAwaiter();
+
+            int cancelThreadId = -1;
+            int continuationThreadId = -1;
+            using var continuationRanEvent = new ManualResetEventSlim(false);
+            using var cancelCompletedEvent = new ManualResetEventSlim(false);
+
+            awaiter.UnsafeOnCompleted(() =>
+            {
+                continuationThreadId = Environment.CurrentManagedThreadId;
+                continuationRanEvent.Set();
+            });
+
+            var cancelThread = new Thread(() =>
+            {
+                cancelThreadId = Environment.CurrentManagedThreadId;
+                cts.Cancel();
+                cancelCompletedEvent.Set();
+            });
+
+            // Cancel on a dedicated thread; continuation should not run synchronously on that stack.
+            cancelThread.Start();
+            Assert.IsTrue(cancelCompletedEvent.Wait(TimeSpan.FromSeconds(5)), "Cancellation did not complete within timeout");
+            cancelThread.Join();
+
+            Assert.IsTrue(continuationRanEvent.Wait(TimeSpan.FromSeconds(5)), "Continuation was not called within timeout");
+
+            // The continuation must have run on a different thread than the Cancel() caller's thread.
+            Assert.AreNotEqual(cancelThreadId, continuationThreadId,
+                "Continuation ran synchronously on the Cancel() caller's thread; this can cause StackOverflowException on constrained stacks");
+        }
+
+        [Test]
+        public void TaskExtensions_CancellationContinuationProducesOperationCanceledException()
+        {
+            // End-to-end test: an async method awaiting AwaitWithCancellation should throw
+            // OperationCanceledException when the token is cancelled, even with async scheduling.
+            var tcs = new TaskCompletionSource<int>();
+            var cts = new CancellationTokenSource();
+
+            async Task<int> AwaitWithCancellationAsync()
+            {
+                return await tcs.Task.AwaitWithCancellation(cts.Token);
+            }
+
+            var task = AwaitWithCancellationAsync();
+            Assert.IsFalse(task.IsCompleted);
+
+            cts.Cancel();
+
+            Assert.CatchAsync<OperationCanceledException>(async () => await task);
+        }
+
+        [Test]
+        public void TaskExtensions_NonGenericTask_CancellationDoesNotRunContinuationSynchronously()
+        {
+            // Same test as above but for the non-generic Task overload.
+            var tcs = new TaskCompletionSource<bool>();
+            var cts = new CancellationTokenSource();
+            Task nonGenericTask = tcs.Task;
+            var awaiter = nonGenericTask.AwaitWithCancellation(cts.Token).GetAwaiter();
+
+            int cancelThreadId = -1;
+            int continuationThreadId = -1;
+            using var continuationRanEvent = new ManualResetEventSlim(false);
+            using var cancelCompletedEvent = new ManualResetEventSlim(false);
+
+            awaiter.UnsafeOnCompleted(() =>
+            {
+                continuationThreadId = Environment.CurrentManagedThreadId;
+                continuationRanEvent.Set();
+            });
+
+            var cancelThread = new Thread(() =>
+            {
+                cancelThreadId = Environment.CurrentManagedThreadId;
+                cts.Cancel();
+                cancelCompletedEvent.Set();
+            });
+
+            cancelThread.Start();
+            Assert.IsTrue(cancelCompletedEvent.Wait(TimeSpan.FromSeconds(5)), "Cancellation did not complete within timeout");
+            cancelThread.Join();
+
+            Assert.IsTrue(continuationRanEvent.Wait(TimeSpan.FromSeconds(5)), "Continuation was not called within timeout");
+            Assert.AreNotEqual(cancelThreadId, continuationThreadId,
+                "Continuation ran synchronously on the Cancel() caller's thread");
         }
     }
 }
