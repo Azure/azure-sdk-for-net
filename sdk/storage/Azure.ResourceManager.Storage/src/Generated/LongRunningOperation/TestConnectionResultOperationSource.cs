70d70e96bd394d3d58967daf22a9c22eef557745
Nate Prewitt <nate.prewitt@gmail.com>
2026-05-14 13:55:45 -0600

    Export API with latest merge from main

--- sdk/storage/Azure.ResourceManager.Storage/src/Generated/LongRunningOperation/TestConnectionResultOperationSource.cs
+++ sdk/storage/Azure.ResourceManager.Storage/src/Generated/LongRunningOperation/TestConnectionResultOperationSource.cs
@@ -28,8 +28,7 @@ namespace Azure.ResourceManager.Storage
         TestConnectionResult IOperationSource<TestConnectionResult>.CreateResult(Response response, CancellationToken cancellationToken)
         {
             using JsonDocument document = JsonDocument.Parse(response.ContentStream);
-            TestConnectionResult result = TestConnectionResult.DeserializeTestConnectionResult(document.RootElement, ModelSerializationExtensions.WireOptions);
-            return result;
+            return TestConnectionResult.DeserializeTestConnectionResult(document.RootElement, ModelSerializationExtensions.WireOptions);
         }
 
         /// <param name="response"> The response from the service. </param>
@@ -38,8 +37,7 @@ namespace Azure.ResourceManager.Storage
         async ValueTask<TestConnectionResult> IOperationSource<TestConnectionResult>.CreateResultAsync(Response response, CancellationToken cancellationToken)
         {
             using JsonDocument document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
-            TestConnectionResult result = TestConnectionResult.DeserializeTestConnectionResult(document.RootElement, ModelSerializationExtensions.WireOptions);
-            return result;
+            return TestConnectionResult.DeserializeTestConnectionResult(document.RootElement, ModelSerializationExtensions.WireOptions);
         }
     }
 }
