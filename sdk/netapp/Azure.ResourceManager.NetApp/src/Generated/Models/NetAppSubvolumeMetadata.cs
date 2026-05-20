e1eb0bce7c95941a952769bd08aa0803a9da6ae8
azure-sdk <azuresdk@microsoft.com>
2026-05-20 01:24:52 +0000

    Update SDK code ne_re_3

--- sdk/netapp/Azure.ResourceManager.NetApp/src/Generated/Models/NetAppSubvolumeMetadata.cs
+++ sdk/netapp/Azure.ResourceManager.NetApp/src/Generated/Models/NetAppSubvolumeMetadata.cs
@@ -25,12 +25,12 @@ namespace Azure.ResourceManager.NetApp.Models
 
         /// <summary> Initializes a new instance of <see cref="NetAppSubvolumeMetadata"/>. </summary>
         /// <param name="id"> Fully qualified resource ID for the resource. Ex - /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}. </param>
+        /// <param name="name"> The name of the resource. </param>
         /// <param name="resourceType"> The type of the resource. E.g. "Microsoft.Compute/virtualMachines" or "Microsoft.Storage/storageAccounts". </param>
         /// <param name="systemData"> Azure Resource Manager metadata containing createdBy and modifiedBy information. </param>
         /// <param name="additionalBinaryDataProperties"> Keeps track of any properties unknown to the library. </param>
-        /// <param name="name"> Resource name. </param>
         /// <param name="properties"> It represents the minimal properties of the subvolume. </param>
-        internal NetAppSubvolumeMetadata(ResourceIdentifier id, ResourceType resourceType, SystemData systemData, IDictionary<string, BinaryData> additionalBinaryDataProperties, string name, SubvolumeModelProperties properties) : base(id, name, resourceType, systemData)
+        internal NetAppSubvolumeMetadata(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, IDictionary<string, BinaryData> additionalBinaryDataProperties, SubvolumeModelProperties properties) : base(id, name, resourceType, systemData)
         {
             _additionalBinaryDataProperties = additionalBinaryDataProperties;
             Properties = properties;
