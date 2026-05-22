e1a47835b90af5dc820c138ad43dca51b4b6d677
mcgallan <1186003288@qq.com>
2026-05-22 15:26:34 +0800

    remove custom code

--- sdk/hybridcompute/Azure.ResourceManager.HybridCompute/src/Custom/Models/HybridComputePrivateEndpointConnectionProperties.cs
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-// The PrivateEndpointId property was changed from ResourceIdentifier to string during migration.
-// This breaking change is tracked in the API compatibility baseline.
