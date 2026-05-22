0ff91a57d0b2669193f3ee67937cedede910e29e
Arcturus Zhang <dapzhang@microsoft.com>
2026-05-22 16:25:42 +0800

    add justification of custom code

--- sdk/storage/Azure.ResourceManager.Storage/src/Customize/Models/PolicyNameCompatibility.cs
+++ sdk/storage/Azure.ResourceManager.Storage/src/Customize/Models/PolicyNameCompatibility.cs
@@ -5,6 +5,11 @@
 
 namespace Azure.ResourceManager.Storage.Models
 {
+    // these customizations are here because the spec defined these enums as extensible enums,
+    // but our resource detection logic requires them to be fixed enums to recognize those resources as singleton resources.
+    // we worked around this issue by making the enums fixed in the spec,
+    // then adding below customizations to change them back to extensible enums
+    // because in previous GA versions, they are extensible enums (structs)
     public readonly partial struct BlobInventoryPolicyName
     {
     }
