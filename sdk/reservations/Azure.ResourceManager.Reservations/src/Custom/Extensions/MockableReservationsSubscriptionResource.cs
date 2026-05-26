dfa38217de730175e1114534096c71fbacefdb0d
Wenming Liu <v-wenmingliu@microsoft.com>
2026-05-26 09:48:37 +0800

    Update MockableReservationsSubscriptionResource.cs

--- sdk/reservations/Azure.ResourceManager.Reservations/src/Custom/Extensions/MockableReservationsSubscriptionResource.cs
+++ sdk/reservations/Azure.ResourceManager.Reservations/src/Custom/Extensions/MockableReservationsSubscriptionResource.cs
@@ -6,8 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Azure;
 using Azure.Core;
-
-#pragma warning disable CS1591
+using Azure.ResourceManager.Reservations.Models;
 
 namespace Azure.ResourceManager.Reservations.Mocking
 {
@@ -16,10 +15,49 @@ namespace Azure.ResourceManager.Reservations.Mocking
     // The TypeSpec generator emits collection or long-parameter methods, so these forwarders preserve
     // the GA convenience surface and mocking target.
     public partial class MockableReservationsSubscriptionResource
-    {
+    {    /// <summary>
+        /// Get the regions and skus that are available for RI purchase for the specified Azure subscription.
+        /// <list type="bullet">
+        /// <item>
+        /// <term>Request Path</term>
+        /// <description>/subscriptions/{subscriptionId}/providers/Microsoft.Capacity/catalogs</description>
+        /// </item>
+        /// <item>
+        /// <term>Operation Id</term>
+        /// <description>GetCatalog</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <param name="reservedResourceType"> The type of the resource for which the skus should be provided. </param>
+        /// <param name="location"> Filters the skus based on the location specified in this parameter. This can be an azure region or global. </param>
+        /// <param name="publisherId"> Publisher id used to get the third party products. </param>
+        /// <param name="offerId"> Offer id used to get the third party products. </param>
+        /// <param name="planId"> Plan id used to get the third party products. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns> An async collection of <see cref="ReservationCatalog" /> that may take multiple service requests to iterate over. </returns>
         public virtual AsyncPageable<Models.ReservationCatalog> GetCatalogAsync(string reservedResourceType = null, AzureLocation? location = null, string publisherId = null, string offerId = null, string planId = null, CancellationToken cancellationToken = default)
             => GetCatalogAsync(reservedResourceType, location, publisherId, offerId, planId, filter: default, skip: default, take: default, cancellationToken);
 
+         /// <summary>
+        /// Get the regions and skus that are available for RI purchase for the specified Azure subscription.
+        /// <list type="bullet">
+        /// <item>
+        /// <term>Request Path</term>
+        /// <description>/subscriptions/{subscriptionId}/providers/Microsoft.Capacity/catalogs</description>
+        /// </item>
+        /// <item>
+        /// <term>Operation Id</term>
+        /// <description>GetCatalog</description>
+        /// </item>
+        /// </list>
+        /// </summary>
+        /// <param name="reservedResourceType"> The type of the resource for which the skus should be provided. </param>
+        /// <param name="location"> Filters the skus based on the location specified in this parameter. This can be an azure region or global. </param>
+        /// <param name="publisherId"> Publisher id used to get the third party products. </param>
+        /// <param name="offerId"> Offer id used to get the third party products. </param>
+        /// <param name="planId"> Plan id used to get the third party products. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns> A collection of <see cref="ReservationCatalog" /> that may take multiple service requests to iterate over. </returns>
         public virtual Pageable<Models.ReservationCatalog> GetCatalog(string reservedResourceType = null, AzureLocation? location = null, string publisherId = null, string offerId = null, string planId = null, CancellationToken cancellationToken = default)
             => GetCatalog(reservedResourceType, location, publisherId, offerId, planId, filter: default, skip: default, take: default, cancellationToken);
 
