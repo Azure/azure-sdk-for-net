70eb91f38fafd503ccbd296f8a1d5dcca3a5701b
Lavish Singal <t-lsingal@microsoft.com>
2026-05-29 16:40:52 +0530

    Remove trailing blank line from AssemblyInfo.cs

--- sdk/azureresiliencemanagement/Azure.ResourceManager.ResilienceManagement/src/Properties/AssemblyInfo.cs
+++ sdk/azureresiliencemanagement/Azure.ResourceManager.ResilienceManagement/src/Properties/AssemblyInfo.cs
@@ -5,4 +5,3 @@ using System.Diagnostics.CodeAnalysis;
 
 // Suppress AZC0030 for ArmResponseErrorResponse as it's generated from TypeSpec and the name comes from the API specification
 [assembly: SuppressMessage("Azure.Sdk.Analyzers", "AZC0030:Model name 'ArmResponseErrorResponse' ends with 'Response'. We suggest renaming it to 'ResilienceManagementArmResponseErrorResponseResult' or another name with this suffix.", Justification = "Generated from TypeSpec API specification - model name is defined in Azure REST API specs", Scope = "type", Target = "~T:Azure.ResourceManager.ResilienceManagement.Models.ArmResponseErrorResponse")]
-
