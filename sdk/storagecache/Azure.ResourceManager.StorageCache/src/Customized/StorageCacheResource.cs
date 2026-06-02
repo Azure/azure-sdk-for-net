11cff837436e3c2cd226740a8e74868d6f1106a8
Wenming Liu <v-wenmingliu@microsoft.com>
2026-06-02 10:36:56 +0800

    Update StorageCacheResource.cs

--- sdk/storagecache/Azure.ResourceManager.StorageCache/src/Customized/StorageCacheResource.cs
+++ sdk/storagecache/Azure.ResourceManager.StorageCache/src/Customized/StorageCacheResource.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
-using System.IO;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
