fda07ec73f6c9ca621cef04db94e24c25075d393
azure-sdk <azuresdk@microsoft.com>
2026-06-02 04:07:52 +0000

    Update SDK code ne_re_3

--- sdk/netapp/Azure.ResourceManager.NetApp/src/Generated/NetAppSubscriptionQuotaItemData.cs
+++ sdk/netapp/Azure.ResourceManager.NetApp/src/Generated/NetAppSubscriptionQuotaItemData.cs
@@ -29,12 +29,12 @@ namespace Azure.ResourceManager.NetApp
         /// <param name="name"> The name of the resource. </param>
         /// <param name="resourceType"> The type of the resource. E.g. "Microsoft.Compute/virtualMachines" or "Microsoft.Storage/storageAccounts". </param>
         /// <param name="systemData"> Azure Resource Manager metadata containing createdBy and modifiedBy information. </param>
-        /// <param name="additionalBinaryDataProperties"> Keeps track of any properties unknown to the library. </param>
         /// <param name="properties"> QuotaItem properties. </param>
-        internal NetAppSubscriptionQuotaItemData(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, IDictionary<string, BinaryData> additionalBinaryDataProperties, QuotaItemProperties properties) : base(id, name, resourceType, systemData)
+        /// <param name="additionalBinaryDataProperties"> Keeps track of any properties unknown to the library. </param>
+        internal NetAppSubscriptionQuotaItemData(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, QuotaItemProperties properties, IDictionary<string, BinaryData> additionalBinaryDataProperties) : base(id, name, resourceType, systemData)
         {
-            _additionalBinaryDataProperties = additionalBinaryDataProperties;
             Properties = properties;
+            _additionalBinaryDataProperties = additionalBinaryDataProperties;
         }
 
         /// <summary> QuotaItem properties. </summary>
