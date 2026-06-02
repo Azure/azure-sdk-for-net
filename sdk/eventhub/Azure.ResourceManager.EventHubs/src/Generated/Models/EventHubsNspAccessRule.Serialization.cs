1c7041b2afbf3b59c42ab44bde9b44db58172d35
azure-sdk <azuresdk@microsoft.com>
2026-06-02 04:07:07 +0000

    Update SDK code co_ev_1

--- sdk/eventhub/Azure.ResourceManager.EventHubs/src/Generated/Models/EventHubsNspAccessRule.Serialization.cs
+++ sdk/eventhub/Azure.ResourceManager.EventHubs/src/Generated/Models/EventHubsNspAccessRule.Serialization.cs
@@ -83,6 +83,21 @@ namespace Azure.ResourceManager.EventHubs.Models
                 writer.WritePropertyName("properties"u8);
                 writer.WriteObjectValue(Properties, options);
             }
+            if (options.Format != "W" && _additionalBinaryDataProperties != null)
+            {
+                foreach (var item in _additionalBinaryDataProperties)
+                {
+                    writer.WritePropertyName(item.Key);
+#if NET6_0_OR_GREATER
+                    writer.WriteRawValue(item.Value);
+#else
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
+                    {
+                        JsonSerializer.Serialize(writer, document.RootElement);
+                    }
+#endif
+                }
+            }
         }
 
         /// <param name="reader"> The JSON reader. </param>
@@ -114,8 +129,8 @@ namespace Azure.ResourceManager.EventHubs.Models
             string name = default;
             ResourceType resourceType = default;
             SystemData systemData = default;
-            IDictionary<string, BinaryData> additionalBinaryDataProperties = new ChangeTrackingDictionary<string, BinaryData>();
             EventHubsNspAccessRuleProperties properties = default;
+            IDictionary<string, BinaryData> additionalBinaryDataProperties = new ChangeTrackingDictionary<string, BinaryData>();
             foreach (var prop in element.EnumerateObject())
             {
                 if (prop.NameEquals("id"u8))
@@ -169,8 +184,8 @@ namespace Azure.ResourceManager.EventHubs.Models
                 name,
                 resourceType,
                 systemData,
-                additionalBinaryDataProperties,
-                properties);
+                properties,
+                additionalBinaryDataProperties);
         }
     }
 }
