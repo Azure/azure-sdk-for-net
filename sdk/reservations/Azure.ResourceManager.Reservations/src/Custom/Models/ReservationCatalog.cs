2e9fecd2f48dc4027682e7a128f9c41f6c0c6fdf
Wenming Liu <v-wenmingliu@microsoft.com>
2026-06-09 09:29:40 +0800

    Update ReservationCatalog.cs

--- sdk/reservations/Azure.ResourceManager.Reservations/src/Custom/Models/ReservationCatalog.cs
+++ sdk/reservations/Azure.ResourceManager.Reservations/src/Custom/Models/ReservationCatalog.cs
@@ -9,10 +9,10 @@ using System.ComponentModel;
 namespace Azure.ResourceManager.Reservations.Models
 {
     /// <summary> The ReservationCatalog. </summary>
-    [EditorBrowsable(EditorBrowsableState.Never)]
     public partial class ReservationCatalog
     {
         /// <summary> Amount in pricing currency. Tax not included. </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
         public PurchasePrice MsrpP1Y
         {
             get => Msrp?.P1Y;
