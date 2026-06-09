77819a2007c949d3ccb176e0abbb5f6d5bc33be1
Matt Metcalf <mrm9084@gmail.com>
2026-06-09 14:44:30 -0700

    AAD to EntraID, ConnectionString to EntraID.

--- sdk/appconfiguration/Azure.Data.AppConfiguration/tests/AppConfigurationTestEnvironment.cs
+++ sdk/appconfiguration/Azure.Data.AppConfiguration/tests/AppConfigurationTestEnvironment.cs
@@ -9,7 +9,6 @@ namespace Azure.Data.AppConfiguration
 {
     public class AppConfigurationTestEnvironment : TestEnvironment
     {
-        public string ConnectionString => GetRecordedVariable("APPCONFIGURATION_CONNECTION_STRING", options => options.HasSecretConnectionStringParameter("secret", SanitizedValue.Base64));
         public string Endpoint => GetRecordedVariable("APPCONFIGURATION_ENDPOINT_STRING");
         public string SecretId => GetRecordedVariable("KEYVAULT_SECRET_URL");
 
