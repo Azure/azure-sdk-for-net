36c073dd0f7f25cc634ea34cdb6574263924400e
Fuming Zhang <fumingzhang@microsoft.com>
2026-06-10 11:15:29 +1000

    Fix ACRONYM001: rename model-factory parameter gatewayAPIImplementationsIstioMode -> gatewayApiImplementationsIstioMode
    Suppress the generated ManagedClusterIngressProfileWebAppRouting model-factory overload and re-add a customized overload with the corrected parameter casing so it matches the public GatewayApiImplementationsIstioMode property. Addresses code-owner review feedback on ArmContainerServiceModelFactory.cs.

--- sdk/containerservice/Azure.ResourceManager.ContainerService/src/Custom/ArmContainerServiceModelFactory.cs
+++ sdk/containerservice/Azure.ResourceManager.ContainerService/src/Custom/ArmContainerServiceModelFactory.cs
@@ -9,13 +9,35 @@ using System.ComponentModel;
 using System.Linq;
 using Azure.Core;
 using Azure.ResourceManager.Models;
+using Microsoft.TypeSpec.Generator.Customizations;
 
 // NOTE: The following customization is intentionally retained for backward compatibility.
 namespace Azure.ResourceManager.ContainerService.Models
 {
     /// <summary> Model factory for models. </summary>
+    [CodeGenSuppress("ManagedClusterIngressProfileWebAppRouting", typeof(bool?), typeof(GatewayApiIstioMode?), typeof(IEnumerable<ResourceIdentifier>), typeof(NginxIngressControllerType?), typeof(ContainerServiceUserAssignedIdentity))]
     public static partial class ArmContainerServiceModelFactory
     {
+        /// <summary> Initializes a new instance of <see cref="Models.ManagedClusterIngressProfileWebAppRouting"/>. </summary>
+        /// <param name="isEnabled"> Whether to enable the Application Routing add-on. </param>
+        /// <param name="gatewayApiImplementationsIstioMode"> Whether to enable Istio as a Gateway API implementation for managed ingress with App Routing. </param>
+        /// <param name="dnsZoneResourceIds"> Resource IDs of the DNS zones to be associated with the Application Routing add-on. Used only when Application Routing add-on is enabled. Public and private DNS zones can be in different resource groups, but all public DNS zones must be in the same resource group and all private DNS zones must be in the same resource group. </param>
+        /// <param name="nginxDefaultIngressControllerType"> Ingress type for the default NginxIngressController custom resource. </param>
+        /// <param name="identity"> Managed identity of the Application Routing add-on. This is the identity that should be granted permissions, for example, to manage the associated Azure DNS resource and get certificates from Azure Key Vault. See [this overview of the add-on](https://learn.microsoft.com/en-us/azure/aks/web-app-routing?tabs=with-osm) for more instructions. </param>
+        /// <returns> A new <see cref="Models.ManagedClusterIngressProfileWebAppRouting"/> instance for mocking. </returns>
+        public static ManagedClusterIngressProfileWebAppRouting ManagedClusterIngressProfileWebAppRouting(bool? isEnabled = default, GatewayApiIstioMode? gatewayApiImplementationsIstioMode = default, IEnumerable<ResourceIdentifier> dnsZoneResourceIds = default, NginxIngressControllerType? nginxDefaultIngressControllerType = default, ContainerServiceUserAssignedIdentity identity = default)
+        {
+            dnsZoneResourceIds ??= new ChangeTrackingList<ResourceIdentifier>();
+
+            return new ManagedClusterIngressProfileWebAppRouting(
+                isEnabled,
+                gatewayApiImplementationsIstioMode is null ? default : new ManagedClusterWebAppRoutingGatewayAPIImplementations { IstioMode = gatewayApiImplementationsIstioMode },
+                (dnsZoneResourceIds ?? new ChangeTrackingList<ResourceIdentifier>()).ToList(),
+                nginxDefaultIngressControllerType is null ? default : new ManagedClusterIngressProfileNginx(nginxDefaultIngressControllerType, default),
+                identity,
+                default);
+        }
+
         /// <summary> Initializes a new instance of <see cref="ContainerService.OSOptionProfileData"/>. </summary>
         /// <param name="id"> The id. </param>
         /// <param name="name"> The name. </param>
