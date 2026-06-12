c2d7e2640b257d55fb6705a21da2a260c8364438
samudra <sabaner@microsoft.com>
2026-06-11 20:45:03 -0700

    Rerun code checks

--- sdk/computefleet/Azure.ResourceManager.ComputeFleet/src/Generated/ArmComputeFleetModelFactory.cs
+++ sdk/computefleet/Azure.ResourceManager.ComputeFleet/src/Generated/ArmComputeFleetModelFactory.cs
@@ -1492,7 +1492,6 @@ namespace Azure.ResourceManager.ComputeFleet.Models
             return new ComputeFleetInnerError(exceptionType, errorDetail, default);
         }
 
-        /// <summary> A Launch mode Fleet's virtual machine. </summary>
         /// <param name="id"> Fully qualified resource ID for the resource. Ex - /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}. </param>
         /// <param name="name"> The name of the resource. </param>
         /// <param name="resourceType"> The type of the resource. E.g. "Microsoft.Compute/virtualMachines" or "Microsoft.Storage/storageAccounts". </param>
