e021352c70cfe39f61c439a8c04a3ea5289fe5b7
Matt Metcalf <mrm9084@gmail.com>
2026-06-12 09:16:12 -0700

    Update ConfigurationModelFactory.cs

--- sdk/appconfiguration/Azure.Data.AppConfiguration/src/Models/ConfigurationModelFactory.cs
+++ sdk/appconfiguration/Azure.Data.AppConfiguration/src/Models/ConfigurationModelFactory.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Microsoft.TypeSpec.Generator.Customizations;
 
 namespace Azure.Data.AppConfiguration
@@ -24,6 +25,8 @@ namespace Azure.Data.AppConfiguration
         /// <param name="value">The configuration setting's value.</param>
         /// <param name="label">A label used to group configuration settings.</param>
         /// <param name="contentType">The content type of the configuration setting's value.</param>
+        /// <param name="tags">A dictionary of tags used to assign additional properties to the configuration setting.</param>
+        /// <param name="description">A description of the configuration setting.</param>
         /// <param name="eTag">An ETag indicating the version of a configuration setting within a configuration store.</param>
         /// <param name="lastModified">The last time a modifying operation was performed on the given configuration setting.</param>
         /// <param name="isReadOnly">A value indicating whether the configuration setting is read only.</param>
@@ -32,17 +35,57 @@ namespace Azure.Data.AppConfiguration
             string value,
             string label = null,
             string contentType = null,
+            IDictionary<string, string> tags = null,
+            string description = null,
             ETag eTag = default,
             DateTimeOffset? lastModified = null,
             bool? isReadOnly = null)
         {
-            return new ConfigurationSetting(key, value, label)
+            ConfigurationSetting setting = new ConfigurationSetting(key, value, label)
             {
                 ContentType = contentType,
+                Description = description,
                 ETag = eTag,
                 LastModified = lastModified,
-                IsReadOnly = isReadOnly
+                IsReadOnly = isReadOnly,
             };
+            if (tags != null)
+            {
+                setting.Tags = tags;
+            }
+            return setting;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppConfiguration.ConfigurationSetting"/> for mocking purposes.
+        /// </summary>
+        /// <param name="key">The primary identifier of a configuration setting.</param>
+        /// <param name="value">The configuration setting's value.</param>
+        /// <param name="label">A label used to group configuration settings.</param>
+        /// <param name="contentType">The content type of the configuration setting's value.</param>
+        /// <param name="eTag">An ETag indicating the version of a configuration setting within a configuration store.</param>
+        /// <param name="lastModified">The last time a modifying operation was performed on the given configuration setting.</param>
+        /// <param name="isReadOnly">A value indicating whether the configuration setting is read only.</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static ConfigurationSetting ConfigurationSetting(
+            string key,
+            string value,
+            string label,
+            string contentType,
+            ETag eTag,
+            DateTimeOffset? lastModified,
+            bool? isReadOnly)
+        {
+            return ConfigurationSetting(
+                key: key,
+                value: value,
+                label: label,
+                contentType: contentType,
+                tags: default,
+                description: default,
+                eTag: eTag,
+                lastModified: lastModified,
+                isReadOnly: isReadOnly);
         }
 
         /// <summary>
