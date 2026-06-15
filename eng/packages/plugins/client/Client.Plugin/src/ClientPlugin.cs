79a24b4ede522cee5d4b72be13af91c894ad8b59
Linh Phan <mailinhphan@microsoft.com>
2026-06-15 09:24:29 -0700

    pull unbranded visitor out as a subclass as well

--- eng/packages/plugins/client/Client.Plugin/src/ClientPlugin.cs
+++ eng/packages/plugins/client/Client.Plugin/src/ClientPlugin.cs
@@ -1,12 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.ClientModel.Primitives;
 using System.IO;
 using Azure.Generator.Visitors;
 using Microsoft.TypeSpec.Generator;
 using Microsoft.TypeSpec.Generator.ClientModel;
-using Microsoft.TypeSpec.Generator.Primitives;
 
 namespace Client.Plugin
 {
@@ -35,9 +33,7 @@ namespace Client.Plugin
 
             // Rest of the visitors can be added in any order.
             generator.AddVisitor(new NamespaceVisitor());
-            generator.AddVisitor(new DistributedTracingVisitor(
-                new CSharpType(typeof(ClientDiagnostics)),
-                new CSharpType(typeof(DiagnosticScope))));
+            generator.AddVisitor(new UnbrandedDistributedTracingVisitor());
             generator.AddVisitor(new ClientRequestIdHeaderVisitor(includeXmsClientRequestIdInRequest: true));
             // Note the shared source TaskExtensions must be added manually to the csproj currently as plugins
             // don't support modifying the shared source files currently.
