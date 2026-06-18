b69810e2501bae314394be765fb6a0191360434f
Zhonglei Ma <v-zhongleima@microsoft.com>
2026-06-18 11:23:37 +0800

    MPG migration SDK - Billing (#59328)

--- sdk/billing/Azure.ResourceManager.Billing/src/Generated/Models/BillingAgreementType.cs
+++ sdk/billing/Azure.ResourceManager.Billing/src/Generated/Models/BillingAgreementType.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.ComponentModel;
+using Azure.ResourceManager.Billing;
 
 namespace Azure.ResourceManager.Billing.Models
 {
@@ -14,47 +15,72 @@ namespace Azure.ResourceManager.Billing.Models
     public readonly partial struct BillingAgreementType : IEquatable<BillingAgreementType>
     {
         private readonly string _value;
+        /// <summary> Other. </summary>
+        private const string OtherValue = "Other";
+        /// <summary> MicrosoftCustomerAgreement. </summary>
+        private const string MicrosoftCustomerAgreementValue = "MicrosoftCustomerAgreement";
+        /// <summary> EnterpriseAgreement. </summary>
+        private const string EnterpriseAgreementValue = "EnterpriseAgreement";
+        /// <summary> MicrosoftOnlineServicesProgram. </summary>
+        private const string MicrosoftOnlineServicesProgramValue = "MicrosoftOnlineServicesProgram";
+        /// <summary> MicrosoftPartnerAgreement. </summary>
+        private const string MicrosoftPartnerAgreementValue = "MicrosoftPartnerAgreement";
 
         /// <summary> Initializes a new instance of <see cref="BillingAgreementType"/>. </summary>
+        /// <param name="value"> The value. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="value"/> is null. </exception>
         public BillingAgreementType(string value)
         {
-            _value = value ?? throw new ArgumentNullException(nameof(value));
-        }
+            Argument.AssertNotNull(value, nameof(value));
 
-        private const string OtherValue = "Other";
-        private const string MicrosoftCustomerAgreementValue = "MicrosoftCustomerAgreement";
-        private const string EnterpriseAgreementValue = "EnterpriseAgreement";
-        private const string MicrosoftOnlineServicesProgramValue = "MicrosoftOnlineServicesProgram";
-        private const string MicrosoftPartnerAgreementValue = "MicrosoftPartnerAgreement";
+            _value = value;
+        }
 
         /// <summary> Other. </summary>
         public static BillingAgreementType Other { get; } = new BillingAgreementType(OtherValue);
+
         /// <summary> MicrosoftCustomerAgreement. </summary>
         public static BillingAgreementType MicrosoftCustomerAgreement { get; } = new BillingAgreementType(MicrosoftCustomerAgreementValue);
+
         /// <summary> EnterpriseAgreement. </summary>
         public static BillingAgreementType EnterpriseAgreement { get; } = new BillingAgreementType(EnterpriseAgreementValue);
+
         /// <summary> MicrosoftOnlineServicesProgram. </summary>
         public static BillingAgreementType MicrosoftOnlineServicesProgram { get; } = new BillingAgreementType(MicrosoftOnlineServicesProgramValue);
+
         /// <summary> MicrosoftPartnerAgreement. </summary>
         public static BillingAgreementType MicrosoftPartnerAgreement { get; } = new BillingAgreementType(MicrosoftPartnerAgreementValue);
+
         /// <summary> Determines if two <see cref="BillingAgreementType"/> values are the same. </summary>
+        /// <param name="left"> The left value to compare. </param>
+        /// <param name="right"> The right value to compare. </param>
         public static bool operator ==(BillingAgreementType left, BillingAgreementType right) => left.Equals(right);
+
         /// <summary> Determines if two <see cref="BillingAgreementType"/> values are not the same. </summary>
+        /// <param name="left"> The left value to compare. </param>
+        /// <param name="right"> The right value to compare. </param>
         public static bool operator !=(BillingAgreementType left, BillingAgreementType right) => !left.Equals(right);
-        /// <summary> Converts a <see cref="string"/> to a <see cref="BillingAgreementType"/>. </summary>
+
+        /// <summary> Converts a string to a <see cref="BillingAgreementType"/>. </summary>
+        /// <param name="value"> The value. </param>
         public static implicit operator BillingAgreementType(string value) => new BillingAgreementType(value);
 
-        /// <inheritdoc />
+        /// <summary> Converts a string to a <see cref="BillingAgreementType"/>. </summary>
+        /// <param name="value"> The value. </param>
+        public static implicit operator BillingAgreementType?(string value) => value == null ? null : new BillingAgreementType(value);
+
+        /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override bool Equals(object obj) => obj is BillingAgreementType other && Equals(other);
-        /// <inheritdoc />
+
+        /// <inheritdoc/>
         public bool Equals(BillingAgreementType other) => string.Equals(_value, other._value, StringComparison.InvariantCultureIgnoreCase);
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode() => _value != null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(_value) : 0;
-        /// <inheritdoc />
+
+        /// <inheritdoc/>
         public override string ToString() => _value;
     }
 }
--- sdk/billing/Azure.ResourceManager.Billing/src/Generated/Models/BillingPaymentTerm.Serialization.cs
+++ sdk/billing/Azure.ResourceManager.Billing/src/Generated/Models/BillingPaymentTerm.Serialization.cs
@@ -8,16 +8,56 @@
 using System;
 using System.ClientModel.Primitives;
 using System.Collections.Generic;
-using System.Text;
 using System.Text.Json;
-using Azure.Core;
+using Azure.ResourceManager.Billing;
 
 namespace Azure.ResourceManager.Billing.Models
 {
-    public partial class BillingPaymentTerm : IUtf8JsonSerializable, IJsonModel<BillingPaymentTerm>
+    /// <summary> The properties of payment term. </summary>
+    public partial class BillingPaymentTerm : IJsonModel<BillingPaymentTerm>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<BillingPaymentTerm>)this).Write(writer, ModelSerializationExtensions.WireOptions);
+        /// <param name="data"> The data to parse. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual BillingPaymentTerm PersistableModelCreateCore(BinaryData data, ModelReaderWriterOptions options)
+        {
+            string format = options.Format == "W" ? ((IPersistableModel<BillingPaymentTerm>)this).GetFormatFromOptions(options) : options.Format;
+            switch (format)
+            {
+                case "J":
+                    using (JsonDocument document = JsonDocument.Parse(data, ModelSerializationExtensions.JsonDocumentOptions))
+                    {
+                        return DeserializeBillingPaymentTerm(document.RootElement, options);
+                    }
+                default:
+                    throw new FormatException($"The model {nameof(BillingPaymentTerm)} does not support reading '{options.Format}' format.");
+            }
+        }
 
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual BinaryData PersistableModelWriteCore(ModelReaderWriterOptions options)
+        {
+            string format = options.Format == "W" ? ((IPersistableModel<BillingPaymentTerm>)this).GetFormatFromOptions(options) : options.Format;
+            switch (format)
+            {
+                case "J":
+                    return ModelReaderWriter.Write(this, options, AzureResourceManagerBillingContext.Default);
+                default:
+                    throw new FormatException($"The model {nameof(BillingPaymentTerm)} does not support writing '{options.Format}' format.");
+            }
+        }
+
+        /// <param name="options"> The client options for reading and writing models. </param>
+        BinaryData IPersistableModel<BillingPaymentTerm>.Write(ModelReaderWriterOptions options) => PersistableModelWriteCore(options);
+
+        /// <param name="data"> The data to parse. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        BillingPaymentTerm IPersistableModel<BillingPaymentTerm>.Create(BinaryData data, ModelReaderWriterOptions options) => PersistableModelCreateCore(data, options);
+
+        /// <param name="options"> The client options for reading and writing models. </param>
+        string IPersistableModel<BillingPaymentTerm>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
+
+        /// <param name="writer"> The JSON writer. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
         void IJsonModel<BillingPaymentTerm>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
             writer.WriteStartObject();
@@ -29,12 +69,11 @@ namespace Azure.ResourceManager.Billing.Models
         /// <param name="options"> The client options for reading and writing models. </param>
         protected virtual void JsonModelWriteCore(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
-            var format = options.Format == "W" ? ((IPersistableModel<BillingPaymentTerm>)this).GetFormatFromOptions(options) : options.Format;
+            string format = options.Format == "W" ? ((IPersistableModel<BillingPaymentTerm>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(BillingPaymentTerm)} does not support writing '{format}' format.");
             }
-
             if (Optional.IsDefined(Term))
             {
                 writer.WritePropertyName("term"u8);
@@ -55,15 +94,15 @@ namespace Azure.ResourceManager.Billing.Models
                 writer.WritePropertyName("isDefault"u8);
                 writer.WriteBooleanValue(IsDefault.Value);
             }
-            if (options.Format != "W" && _serializedAdditionalRawData != null)
+            if (options.Format != "W" && _additionalBinaryDataProperties != null)
             {
-                foreach (var item in _serializedAdditionalRawData)
+                foreach (var item in _additionalBinaryDataProperties)
                 {
                     writer.WritePropertyName(item.Key);
 #if NET6_0_OR_GREATER
-				writer.WriteRawValue(item.Value);
+                    writer.WriteRawValue(item.Value);
 #else
-                    using (JsonDocument document = JsonDocument.Parse(item.Value, ModelSerializationExtensions.JsonDocumentOptions))
+                    using (JsonDocument document = JsonDocument.Parse(item.Value))
                     {
                         JsonSerializer.Serialize(writer, document.RootElement);
                     }
@@ -72,192 +111,76 @@ namespace Azure.ResourceManager.Billing.Models
             }
         }
 
-        BillingPaymentTerm IJsonModel<BillingPaymentTerm>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
+        /// <param name="reader"> The JSON reader. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        BillingPaymentTerm IJsonModel<BillingPaymentTerm>.Create(ref Utf8JsonReader reader, ModelReaderWriterOptions options) => JsonModelCreateCore(ref reader, options);
+
+        /// <param name="reader"> The JSON reader. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        protected virtual BillingPaymentTerm JsonModelCreateCore(ref Utf8JsonReader reader, ModelReaderWriterOptions options)
         {
-            var format = options.Format == "W" ? ((IPersistableModel<BillingPaymentTerm>)this).GetFormatFromOptions(options) : options.Format;
+            string format = options.Format == "W" ? ((IPersistableModel<BillingPaymentTerm>)this).GetFormatFromOptions(options) : options.Format;
             if (format != "J")
             {
                 throw new FormatException($"The model {nameof(BillingPaymentTerm)} does not support reading '{format}' format.");
             }
-
             using JsonDocument document = JsonDocument.ParseValue(ref reader);
             return DeserializeBillingPaymentTerm(document.RootElement, options);
         }
 
-        internal static BillingPaymentTerm DeserializeBillingPaymentTerm(JsonElement element, ModelReaderWriterOptions options = null)
+        /// <param name="element"> The JSON element to deserialize. </param>
+        /// <param name="options"> The client options for reading and writing models. </param>
+        internal static BillingPaymentTerm DeserializeBillingPaymentTerm(JsonElement element, ModelReaderWriterOptions options)
         {
-            options ??= ModelSerializationExtensions.WireOptions;
-
             if (element.ValueKind == JsonValueKind.Null)
             {
                 return null;
             }
             string term = default;
-            DateTimeOffset? startDate = default;
-            DateTimeOffset? endDate = default;
+            DateTimeOffset? startOn = default;
+            DateTimeOffset? endOn = default;
             bool? isDefault = default;
-            IDictionary<string, BinaryData> serializedAdditionalRawData = default;
-            Dictionary<string, BinaryData> rawDataDictionary = new Dictionary<string, BinaryData>();
-            foreach (var property in element.EnumerateObject())
+            IDictionary<string, BinaryData> additionalBinaryDataProperties = new ChangeTrackingDictionary<string, BinaryData>();
+            foreach (var prop in element.EnumerateObject())
             {
-                if (property.NameEquals("term"u8))
+                if (prop.NameEquals("term"u8))
                 {
-                    term = property.Value.GetString();
+                    term = prop.Value.GetString();
                     continue;
                 }
-                if (property.NameEquals("startDate"u8))
+                if (prop.NameEquals("startDate"u8))
                 {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    if (prop.Value.ValueKind == JsonValueKind.Null)
                     {
                         continue;
                     }
-                    startDate = property.Value.GetDateTimeOffset("O");
+                    startOn = prop.Value.GetDateTimeOffset("O");
                     continue;
                 }
-                if (property.NameEquals("endDate"u8))
+                if (prop.NameEquals("endDate"u8))
                 {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    if (prop.Value.ValueKind == JsonValueKind.Null)
                     {
                         continue;
                     }
-                    endDate = property.Value.GetDateTimeOffset("O");
+                    endOn = prop.Value.GetDateTimeOffset("O");
                     continue;
                 }
-                if (property.NameEquals("isDefault"u8))
+                if (prop.NameEquals("isDefault"u8))
                 {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    if (prop.Value.ValueKind == JsonValueKind.Null)
                     {
                         continue;
                     }
-                    isDefault = property.Value.GetBoolean();
+                    isDefault = prop.Value.GetBoolean();
                     continue;
                 }
                 if (options.Format != "W")
                 {
-                    rawDataDictionary.Add(property.Name, BinaryData.FromString(property.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
                 }
             }
-            serializedAdditionalRawData = rawDataDictionary;
-            return new BillingPaymentTerm(term, startDate, endDate, isDefault, serializedAdditionalRawData);
+            return new BillingPaymentTerm(term, startOn, endOn, isDefault, additionalBinaryDataProperties);
         }
-
-        private BinaryData SerializeBicep(ModelReaderWriterOptions options)
-        {
-            StringBuilder builder = new StringBuilder();
-            BicepModelReaderWriterOptions bicepOptions = options as BicepModelReaderWriterOptions;
-            IDictionary<string, string> propertyOverrides = null;
-            bool hasObjectOverride = bicepOptions != null && bicepOptions.PropertyOverrides.TryGetValue(this, out propertyOverrides);
-            bool hasPropertyOverride = false;
-            string propertyOverride = null;
-
-            builder.AppendLine("{");
-
-            hasPropertyOverride = hasObjectOverride && propertyOverrides.TryGetValue(nameof(Term), out propertyOverride);
-            if (hasPropertyOverride)
-            {
-                builder.Append("  term: ");
-                builder.AppendLine(propertyOverride);
-            }
-            else
-            {
-                if (Optional.IsDefined(Term))
-                {
-                    builder.Append("  term: ");
-                    if (Term.Contains(Environment.NewLine))
-                    {
-                        builder.AppendLine("'''");
-                        builder.AppendLine($"{Term}'''");
-                    }
-                    else
-                    {
-                        builder.AppendLine($"'{Term}'");
-                    }
-                }
-            }
-
-            hasPropertyOverride = hasObjectOverride && propertyOverrides.TryGetValue(nameof(StartOn), out propertyOverride);
-            if (hasPropertyOverride)
-            {
-                builder.Append("  startDate: ");
-                builder.AppendLine(propertyOverride);
-            }
-            else
-            {
-                if (Optional.IsDefined(StartOn))
-                {
-                    builder.Append("  startDate: ");
-                    var formattedDateTimeString = TypeFormatters.ToString(StartOn.Value, "o");
-                    builder.AppendLine($"'{formattedDateTimeString}'");
-                }
-            }
-
-            hasPropertyOverride = hasObjectOverride && propertyOverrides.TryGetValue(nameof(EndOn), out propertyOverride);
-            if (hasPropertyOverride)
-            {
-                builder.Append("  endDate: ");
-                builder.AppendLine(propertyOverride);
-            }
-            else
-            {
-                if (Optional.IsDefined(EndOn))
-                {
-                    builder.Append("  endDate: ");
-                    var formattedDateTimeString = TypeFormatters.ToString(EndOn.Value, "o");
-                    builder.AppendLine($"'{formattedDateTimeString}'");
-                }
-            }
-
-            hasPropertyOverride = hasObjectOverride && propertyOverrides.TryGetValue(nameof(IsDefault), out propertyOverride);
-            if (hasPropertyOverride)
-            {
-                builder.Append("  isDefault: ");
-                builder.AppendLine(propertyOverride);
-            }
-            else
-            {
-                if (Optional.IsDefined(IsDefault))
-                {
-                    builder.Append("  isDefault: ");
-                    var boolValue = IsDefault.Value == true ? "true" : "false";
-                    builder.AppendLine($"{boolValue}");
-                }
-            }
-
-            builder.AppendLine("}");
-            return BinaryData.FromString(builder.ToString());
-        }
-
-        BinaryData IPersistableModel<BillingPaymentTerm>.Write(ModelReaderWriterOptions options)
-        {
-            var format = options.Format == "W" ? ((IPersistableModel<BillingPaymentTerm>)this).GetFormatFromOptions(options) : options.Format;
-
-            switch (format)
-            {
-                case "J":
-                    return ModelReaderWriter.Write(this, options, AzureResourceManagerBillingContext.Default);
-                case "bicep":
-                    return SerializeBicep(options);
-                default:
-                    throw new FormatException($"The model {nameof(BillingPaymentTerm)} does not support writing '{options.Format}' format.");
-            }
-        }
-
-        BillingPaymentTerm IPersistableModel<BillingPaymentTerm>.Create(BinaryData data, ModelReaderWriterOptions options)
-        {
-            var format = options.Format == "W" ? ((IPersistableModel<BillingPaymentTerm>)this).GetFormatFromOptions(options) : options.Format;
-
-            switch (format)
-            {
-                case "J":
-                    {
-                        using JsonDocument document = JsonDocument.Parse(data, ModelSerializationExtensions.JsonDocumentOptions);
-                        return DeserializeBillingPaymentTerm(document.RootElement, options);
-                    }
-                default:
-                    throw new FormatException($"The model {nameof(BillingPaymentTerm)} does not support reading '{options.Format}' format.");
-            }
-        }
-
-        string IPersistableModel<BillingPaymentTerm>.GetFormatFromOptions(ModelReaderWriterOptions options) => "J";
     }
 }
