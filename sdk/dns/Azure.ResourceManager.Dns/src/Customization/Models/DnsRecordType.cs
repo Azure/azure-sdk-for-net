0fc18d783e37f4a822ec2bf057c98df38ec161fb
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2026-06-18 17:34:17 +0800

    Fix comment

---
+++ sdk/dns/Azure.ResourceManager.Dns/src/Customization/Models/DnsRecordType.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System.ComponentModel;
+
+namespace Azure.ResourceManager.Dns.Models
+{
+    /// <summary></summary>
+    public enum DnsRecordType
+    {
+        /// <summary> A. </summary>
+        A = 0,
+        /// <summary> AAAA. </summary>
+        Aaaa = 1,
+        /// <summary> CAA. </summary>
+        Caa = 2,
+        /// <summary> CNAME. </summary>
+        Cname = 3,
+        /// <summary> MX. </summary>
+        MX = 4,
+        /// <summary> NS. </summary>
+        NS = 5,
+        /// <summary> PTR. </summary>
+        Ptr = 6,
+        /// <summary> SOA. </summary>
+        Soa = 7,
+        /// <summary> SRV. </summary>
+        Srv = 8,
+        /// <summary> TXT. </summary>
+        Txt = 9,
+        /// <summary> TLSA. </summary>
+        Tlsa = 10,
+        /// <summary> DS. </summary>
+        DS = 11,
+        /// <summary> NAPTR. </summary>
+        Naptr = 12,
+        /// <summary> CAA. </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        CAA = 2,
+        /// <summary> PTR. </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        PTR = 6,
+        /// <summary> SOA. </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        SOA = 7,
+        /// <summary> SRV. </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        SRV = 8,
+        /// <summary> TXT. </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        TXT = 9,
+    }
+}
