6390c92420469a759afe5fda558d2e56f01db889
hardikginwala <113458906+hardikginwala@users.noreply.github.com>
2026-06-19 17:32:08 -0400

    adding partial class for making SubId guid

---
+++ sdk/compute/Azure.ResourceManager.Compute.BulkActions/src/Custom/ComputeBulkOperationDetails.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Azure.ResourceManager.Compute.BulkActions.Models
+{
+    public partial class ComputeBulkOperationDetails
+    {
+        public Guid SubscriptionId { get; }
+    }
+}
