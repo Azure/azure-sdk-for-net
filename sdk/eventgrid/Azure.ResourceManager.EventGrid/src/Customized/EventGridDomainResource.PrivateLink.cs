db9282851be1a37a017da5f14ed62a8c95af210b
Weidong Xu <weidxu@microsoft.com>
2026-06-30 10:25:29 +0800

    Migrate EventGrid mgmt package to new generator (#59842)

---
+++ sdk/eventgrid/Azure.ResourceManager.EventGrid/src/Customized/EventGridDomainResource.PrivateLink.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System.Threading;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.ResourceManager;
+
+namespace Azure.ResourceManager.EventGrid
+{
+    // GA-compat private-link surface: the generator emits one generic PrivateLinkResources group; main exposes
+    // typed per-resource (Domain/Topic/PartnerNamespace) collections/resources. Rationale: PrivateLinkResourceCompat.cs.
+    public partial class EventGridDomainResource
+    {
+        /// <summary> Gets the private link resources for this Event Grid domain. </summary>
+        /// <returns> The requested resource. </returns>
+        public virtual EventGridDomainPrivateLinkResourceCollection GetEventGridDomainPrivateLinkResources()
+        {
+            return GetCachedClient(client => new EventGridDomainPrivateLinkResourceCollection(client, Id));
+        }
+
+        /// <summary> Gets a specific private link resource. </summary>
+        /// <param name="privateLinkResourceName"> The name of the private link resource. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns> The requested resource. </returns>
+        [ForwardsClientCalls]
+        public virtual async Task<Response<EventGridDomainPrivateLinkResource>> GetEventGridDomainPrivateLinkResourceAsync(string privateLinkResourceName, CancellationToken cancellationToken = default)
+        {
+            return await GetEventGridDomainPrivateLinkResources().GetAsync(privateLinkResourceName, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary> Gets a specific private link resource. </summary>
+        /// <param name="privateLinkResourceName"> The name of the private link resource. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <returns> The requested resource. </returns>
+        [ForwardsClientCalls]
+        public virtual Response<EventGridDomainPrivateLinkResource> GetEventGridDomainPrivateLinkResource(string privateLinkResourceName, CancellationToken cancellationToken = default)
+        {
+            return GetEventGridDomainPrivateLinkResources().Get(privateLinkResourceName, cancellationToken);
+        }
+    }
+}
--- sdk/eventgrid/Azure.ResourceManager.EventGrid/src/Generated/Models/PartnerConfigurationProvisioningState.cs
+++ sdk/eventgrid/Azure.ResourceManager.EventGrid/src/Generated/Models/PartnerConfigurationProvisioningState.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.ComponentModel;
+using Azure.ResourceManager.EventGrid;
 
 namespace Azure.ResourceManager.EventGrid.Models
 {
@@ -14,50 +15,77 @@ namespace Azure.ResourceManager.EventGrid.Models
     public readonly partial struct PartnerConfigurationProvisioningState : IEquatable<PartnerConfigurationProvisioningState>
     {
         private readonly string _value;
+        /// <summary> Creating. </summary>
+        private const string CreatingValue = "Creating";
+        /// <summary> Updating. </summary>
+        private const string UpdatingValue = "Updating";
+        /// <summary> Deleting. </summary>
+        private const string DeletingValue = "Deleting";
+        /// <summary> Succeeded. </summary>
+        private const string SucceededValue = "Succeeded";
+        /// <summary> Canceled. </summary>
+        private const string CanceledValue = "Canceled";
+        /// <summary> Failed. </summary>
+        private const string FailedValue = "Failed";
 
         /// <summary> Initializes a new instance of <see cref="PartnerConfigurationProvisioningState"/>. </summary>
+        /// <param name="value"> The value. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="value"/> is null. </exception>
         public PartnerConfigurationProvisioningState(string value)
         {
-            _value = value ?? throw new ArgumentNullException(nameof(value));
-        }
+            Argument.AssertNotNull(value, nameof(value));
 
-        private const string CreatingValue = "Creating";
-        private const string UpdatingValue = "Updating";
-        private const string DeletingValue = "Deleting";
-        private const string SucceededValue = "Succeeded";
-        private const string CanceledValue = "Canceled";
-        private const string FailedValue = "Failed";
+            _value = value;
+        }
 
         /// <summary> Creating. </summary>
         public static PartnerConfigurationProvisioningState Creating { get; } = new PartnerConfigurationProvisioningState(CreatingValue);
+
         /// <summary> Updating. </summary>
         public static PartnerConfigurationProvisioningState Updating { get; } = new PartnerConfigurationProvisioningState(UpdatingValue);
+
         /// <summary> Deleting. </summary>
         public static PartnerConfigurationProvisioningState Deleting { get; } = new PartnerConfigurationProvisioningState(DeletingValue);
+
         /// <summary> Succeeded. </summary>
         public static PartnerConfigurationProvisioningState Succeeded { get; } = new PartnerConfigurationProvisioningState(SucceededValue);
+
         /// <summary> Canceled. </summary>
         public static PartnerConfigurationProvisioningState Canceled { get; } = new PartnerConfigurationProvisioningState(CanceledValue);
+
         /// <summary> Failed. </summary>
         public static PartnerConfigurationProvisioningState Failed { get; } = new PartnerConfigurationProvisioningState(FailedValue);
+
         /// <summary> Determines if two <see cref="PartnerConfigurationProvisioningState"/> values are the same. </summary>
+        /// <param name="left"> The left value to compare. </param>
+        /// <param name="right"> The right value to compare. </param>
         public static bool operator ==(PartnerConfigurationProvisioningState left, PartnerConfigurationProvisioningState right) => left.Equals(right);
+
         /// <summary> Determines if two <see cref="PartnerConfigurationProvisioningState"/> values are not the same. </summary>
+        /// <param name="left"> The left value to compare. </param>
+        /// <param name="right"> The right value to compare. </param>
         public static bool operator !=(PartnerConfigurationProvisioningState left, PartnerConfigurationProvisioningState right) => !left.Equals(right);
-        /// <summary> Converts a <see cref="string"/> to a <see cref="PartnerConfigurationProvisioningState"/>. </summary>
+
+        /// <summary> Converts a string to a <see cref="PartnerConfigurationProvisioningState"/>. </summary>
+        /// <param name="value"> The value. </param>
         public static implicit operator PartnerConfigurationProvisioningState(string value) => new PartnerConfigurationProvisioningState(value);
 
-        /// <inheritdoc />
+        /// <summary> Converts a string to a <see cref="PartnerConfigurationProvisioningState"/>. </summary>
+        /// <param name="value"> The value. </param>
+        public static implicit operator PartnerConfigurationProvisioningState?(string value) => value == null ? null : new PartnerConfigurationProvisioningState(value);
+
+        /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override bool Equals(object obj) => obj is PartnerConfigurationProvisioningState other && Equals(other);
-        /// <inheritdoc />
+
+        /// <inheritdoc/>
         public bool Equals(PartnerConfigurationProvisioningState other) => string.Equals(_value, other._value, StringComparison.InvariantCultureIgnoreCase);
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
         [EditorBrowsable(EditorBrowsableState.Never)]
         public override int GetHashCode() => _value != null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(_value) : 0;
-        /// <inheritdoc />
+
+        /// <inheritdoc/>
         public override string ToString() => _value;
     }
 }
