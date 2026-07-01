7aa1c680e2f8bd0a62c5feaf8f1b0e2912bccbaa
azure-sdk <azuresdk@microsoft.com>
2026-07-01 05:29:17 +0000

    Update SDK code re_re_14

--- sdk/resources/Azure.ResourceManager.Resources.Policy/src/Generated/CollectionResults/PolicyDefinitionsGetAllAsyncCollectionResultOfT.cs
+++ sdk/resources/Azure.ResourceManager.Resources.Policy/src/Generated/CollectionResults/PolicyDefinitionsGetAllAsyncCollectionResultOfT.cs
@@ -56,8 +56,8 @@ namespace Azure.ResourceManager.Resources.Policy
                     yield break;
                 }
                 PolicyDefinitionListResult result = PolicyDefinitionListResult.FromResponse(response);
-                yield return Page<PolicyDefinitionData>.FromValues((IReadOnlyList<PolicyDefinitionData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 nextPage = result.NextLink;
+                yield return Page<PolicyDefinitionData>.FromValues((IReadOnlyList<PolicyDefinitionData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 if (nextPage == null)
                 {
                     yield break;
