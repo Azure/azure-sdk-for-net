abbdfef2502fc04528c32e016bfb79d58140fd0c
azure-sdk <azuresdk@microsoft.com>
2026-07-01 05:38:33 +0000

    Update SDK code cd_co_2

--- sdk/cdn/Azure.ResourceManager.Cdn/src/Generated/CollectionResults/EndpointsGetByProfileAsyncCollectionResultOfT.cs
+++ sdk/cdn/Azure.ResourceManager.Cdn/src/Generated/CollectionResults/EndpointsGetByProfileAsyncCollectionResultOfT.cs
@@ -56,8 +56,8 @@ namespace Azure.ResourceManager.Cdn
                     yield break;
                 }
                 EndpointListResult result = EndpointListResult.FromResponse(response);
-                yield return Page<CdnEndpointData>.FromValues((IReadOnlyList<CdnEndpointData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 nextPage = result.NextLink;
+                yield return Page<CdnEndpointData>.FromValues((IReadOnlyList<CdnEndpointData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 if (nextPage == null)
                 {
                     yield break;
