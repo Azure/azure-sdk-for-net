4aa0e1a0a4ca92f5393950d830afed1c95d30f7c
azure-sdk <azuresdk@microsoft.com>
2026-07-01 05:29:37 +0000

    Update SDK code fa_he_7

--- sdk/fileshares/Azure.ResourceManager.FileShares/src/Generated/CollectionResults/FileShareSnapshotsGetByFileShareAsyncCollectionResultOfT.cs
+++ sdk/fileshares/Azure.ResourceManager.FileShares/src/Generated/CollectionResults/FileShareSnapshotsGetByFileShareAsyncCollectionResultOfT.cs
@@ -56,8 +56,8 @@ namespace Azure.ResourceManager.FileShares
                     yield break;
                 }
                 FileShareSnapshotListResult result = FileShareSnapshotListResult.FromResponse(response);
-                yield return Page<FileShareSnapshotData>.FromValues((IReadOnlyList<FileShareSnapshotData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 nextPage = result.NextLink;
+                yield return Page<FileShareSnapshotData>.FromValues((IReadOnlyList<FileShareSnapshotData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 if (nextPage == null)
                 {
                     yield break;
