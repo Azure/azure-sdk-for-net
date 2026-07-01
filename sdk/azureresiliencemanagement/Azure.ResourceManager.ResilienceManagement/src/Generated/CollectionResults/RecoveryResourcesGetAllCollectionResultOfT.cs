90259949394ae0de48e764c9d00298cd7fe8a00a
azure-sdk <azuresdk@microsoft.com>
2026-07-01 05:33:41 +0000

    Update SDK code au_ca_1

--- sdk/azureresiliencemanagement/Azure.ResourceManager.ResilienceManagement/src/Generated/CollectionResults/RecoveryResourcesGetAllCollectionResultOfT.cs
+++ sdk/azureresiliencemanagement/Azure.ResourceManager.ResilienceManagement/src/Generated/CollectionResults/RecoveryResourcesGetAllCollectionResultOfT.cs
@@ -52,8 +52,8 @@ namespace Azure.ResourceManager.ResilienceManagement
                     yield break;
                 }
                 RecoveryResourceListResult result = RecoveryResourceListResult.FromResponse(response);
-                yield return Page<RecoveryMembersData>.FromValues((IReadOnlyList<RecoveryMembersData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 nextPage = result.NextLink;
+                yield return Page<RecoveryMembersData>.FromValues((IReadOnlyList<RecoveryMembersData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 if (nextPage == null)
                 {
                     yield break;
