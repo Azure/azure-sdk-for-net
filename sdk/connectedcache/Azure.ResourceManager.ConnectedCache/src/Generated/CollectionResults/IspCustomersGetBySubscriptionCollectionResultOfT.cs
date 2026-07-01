3c61c9f14a8960381a13442a2e116fabe68dd822
azure-sdk <azuresdk@microsoft.com>
2026-07-01 05:31:28 +0000

    Update SDK code co_co_3

--- sdk/connectedcache/Azure.ResourceManager.ConnectedCache/src/Generated/CollectionResults/IspCustomersGetBySubscriptionCollectionResultOfT.cs
+++ sdk/connectedcache/Azure.ResourceManager.ConnectedCache/src/Generated/CollectionResults/IspCustomersGetBySubscriptionCollectionResultOfT.cs
@@ -49,8 +49,8 @@ namespace Azure.ResourceManager.ConnectedCache
                     yield break;
                 }
                 IspCustomerResourceListResult result = IspCustomerResourceListResult.FromResponse(response);
-                yield return Page<IspCustomerData>.FromValues((IReadOnlyList<IspCustomerData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 nextPage = result.NextLink;
+                yield return Page<IspCustomerData>.FromValues((IReadOnlyList<IspCustomerData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 if (nextPage == null)
                 {
                     yield break;
