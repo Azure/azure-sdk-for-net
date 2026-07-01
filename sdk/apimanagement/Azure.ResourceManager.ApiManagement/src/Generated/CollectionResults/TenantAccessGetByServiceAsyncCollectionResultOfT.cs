2ed937b3f41210596a2e895075b305aad2f78250
azure-sdk <azuresdk@microsoft.com>
2026-07-01 05:33:07 +0000

    Update SDK code ad_at_0

--- sdk/apimanagement/Azure.ResourceManager.ApiManagement/src/Generated/CollectionResults/TenantAccessGetByServiceAsyncCollectionResultOfT.cs
+++ sdk/apimanagement/Azure.ResourceManager.ApiManagement/src/Generated/CollectionResults/TenantAccessGetByServiceAsyncCollectionResultOfT.cs
@@ -59,13 +59,13 @@ namespace Azure.ResourceManager.ApiManagement
                     yield break;
                 }
                 AccessInformationCollection result = AccessInformationCollection.FromResponse(response);
-                yield return Page<TenantAccessInfoData>.FromValues(result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 string nextPageString = result.NextLink;
-                if (string.IsNullOrEmpty(nextPageString))
+                nextPage = string.IsNullOrEmpty(nextPageString) ? null : new Uri(nextPageString, UriKind.RelativeOrAbsolute);
+                yield return Page<TenantAccessInfoData>.FromValues(result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
+                if (nextPage == null)
                 {
                     yield break;
                 }
-                nextPage = new Uri(nextPageString, UriKind.RelativeOrAbsolute);
             }
         }
 
