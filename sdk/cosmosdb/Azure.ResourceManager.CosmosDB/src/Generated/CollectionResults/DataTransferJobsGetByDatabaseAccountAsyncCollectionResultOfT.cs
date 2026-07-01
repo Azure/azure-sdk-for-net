6ea56ea21a80f5f37e6e84dd2ff44dff50de6fe7
azure-sdk <azuresdk@microsoft.com>
2026-07-01 09:49:07 +0000

    Update SDK code co_da_4

--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/CollectionResults/DataTransferJobsGetByDatabaseAccountAsyncCollectionResultOfT.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/CollectionResults/DataTransferJobsGetByDatabaseAccountAsyncCollectionResultOfT.cs
@@ -56,8 +56,8 @@ namespace Azure.ResourceManager.CosmosDB
                     yield break;
                 }
                 DataTransferJobFeedResults result = DataTransferJobFeedResults.FromResponse(response);
-                yield return Page<DataTransferJobData>.FromValues((IReadOnlyList<DataTransferJobData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 nextPage = result.NextLink;
+                yield return Page<DataTransferJobData>.FromValues((IReadOnlyList<DataTransferJobData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 if (nextPage == null)
                 {
                     yield break;
--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/CollectionResults/GremlinResourcesGetGremlinGraphsCollectionResultOfT.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Generated/CollectionResults/GremlinResourcesGetGremlinGraphsCollectionResultOfT.cs
@@ -58,13 +58,13 @@ namespace Azure.ResourceManager.CosmosDB
                     yield break;
                 }
                 GremlinGraphListResult result = GremlinGraphListResult.FromResponse(response);
-                yield return Page<GremlinGraphData>.FromValues(result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 string nextPageString = result.NextLink;
-                if (string.IsNullOrEmpty(nextPageString))
+                nextPage = string.IsNullOrEmpty(nextPageString) ? null : new Uri(nextPageString, UriKind.RelativeOrAbsolute);
+                yield return Page<GremlinGraphData>.FromValues(result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
+                if (nextPage == null)
                 {
                     yield break;
                 }
-                nextPage = new Uri(nextPageString, UriKind.RelativeOrAbsolute);
             }
         }
 
