dbae3a144c55dc3abb84e59e1535df92747c5ff7
Dapeng Zhang <dapzhang@microsoft.com>
2026-07-01 17:44:23 +0800

    Split #60550: co_co_3 and da_dn_5 (#60554)
    * Regenerate repository SDK with TypeSpec build 20260630.2
    
    * Update SDK code co_co_3
    
    * Update SDK code da_dn_5
    
    ---------
    
    Co-authored-by: azure-sdk <azuresdk@microsoft.com>

--- sdk/containerinstance/Azure.ResourceManager.ContainerInstance/src/Generated/CollectionResults/LocationGetUsagesWithLocationCollectionResultOfT.cs
+++ sdk/containerinstance/Azure.ResourceManager.ContainerInstance/src/Generated/CollectionResults/LocationGetUsagesWithLocationCollectionResultOfT.cs
@@ -52,13 +52,13 @@ namespace Azure.ResourceManager.ContainerInstance
                     yield break;
                 }
                 UsageListResult result = UsageListResult.FromResponse(response);
-                yield return Page<ContainerInstanceUsage>.FromValues(result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 string nextPageString = result.NextLink;
-                if (string.IsNullOrEmpty(nextPageString))
+                nextPage = string.IsNullOrEmpty(nextPageString) ? null : new Uri(nextPageString, UriKind.RelativeOrAbsolute);
+                yield return Page<ContainerInstanceUsage>.FromValues(result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
+                if (nextPage == null)
                 {
                     yield break;
                 }
-                nextPage = new Uri(nextPageString, UriKind.RelativeOrAbsolute);
             }
         }
 
--- sdk/disconnectedoperations/Azure.ResourceManager.DisconnectedOperations/src/Generated/CollectionResults/HardwareSettingsGetByParentCollectionResultOfT.cs
+++ sdk/disconnectedoperations/Azure.ResourceManager.DisconnectedOperations/src/Generated/CollectionResults/HardwareSettingsGetByParentCollectionResultOfT.cs
@@ -55,8 +55,8 @@ namespace Azure.ResourceManager.DisconnectedOperations
                     yield break;
                 }
                 HardwareSettingListResult result = HardwareSettingListResult.FromResponse(response);
-                yield return Page<DisconnectedOperationsHardwareSettingData>.FromValues((IReadOnlyList<DisconnectedOperationsHardwareSettingData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 nextPage = result.NextLink;
+                yield return Page<DisconnectedOperationsHardwareSettingData>.FromValues((IReadOnlyList<DisconnectedOperationsHardwareSettingData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 if (nextPage == null)
                 {
                     yield break;
