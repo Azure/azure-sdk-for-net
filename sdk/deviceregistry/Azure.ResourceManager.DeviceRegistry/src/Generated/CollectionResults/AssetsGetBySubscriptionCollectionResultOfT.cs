d79a391a69c885d83027c2f92ca4c32a1c9ff9df
azure-sdk <azuresdk@microsoft.com>
2026-07-01 09:34:27 +0000

    Update SDK code da_dn_5

--- sdk/deviceregistry/Azure.ResourceManager.DeviceRegistry/src/Generated/CollectionResults/AssetsGetBySubscriptionCollectionResultOfT.cs
+++ sdk/deviceregistry/Azure.ResourceManager.DeviceRegistry/src/Generated/CollectionResults/AssetsGetBySubscriptionCollectionResultOfT.cs
@@ -49,8 +49,8 @@ namespace Azure.ResourceManager.DeviceRegistry
                     yield break;
                 }
                 AssetListResult result = AssetListResult.FromResponse(response);
-                yield return Page<DeviceRegistryAssetData>.FromValues((IReadOnlyList<DeviceRegistryAssetData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 nextPage = result.NextLink;
+                yield return Page<DeviceRegistryAssetData>.FromValues((IReadOnlyList<DeviceRegistryAssetData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 if (nextPage == null)
                 {
                     yield break;
