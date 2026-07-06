43f94061e7003922a710041b2c8e09d33f57c3bb
Ranjana Sinha <ranjanasinha@microsoft.com>
2026-07-06 17:41:14 -0400

    Revert local-dev AzureCliCredential override

--- sdk/trafficmanager/Azure.ResourceManager.TrafficManager/tests/TrafficManagerManagementTestEnvironment.cs
+++ sdk/trafficmanager/Azure.ResourceManager.TrafficManager/tests/TrafficManagerManagementTestEnvironment.cs
@@ -1,14 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.Core;
 using Azure.Core.TestFramework;
-using Azure.Identity;
 
 namespace Azure.ResourceManager.TrafficManager.Tests
 {
     public class TrafficManagerManagementTestEnvironment : TestEnvironment
     {
-        protected override TokenCredential CreateDeveloperCredential() => new AzureCliCredential();
     }
 }
