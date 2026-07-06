dc4f6ae99e4c39923ad718633b4f8ad914b6ba0b
Anuj Sharma <sharmaanu@microsoft.com>
2026-07-06 15:44:43 +0530

    Trigger CI rebuild

--- sdk/mongodbatlas/Azure.ResourceManager.MongoDBAtlas/src/Generated/CollectionResults/ClustersGetAllCollectionResultOfT.cs
+++ sdk/mongodbatlas/Azure.ResourceManager.MongoDBAtlas/src/Generated/CollectionResults/ClustersGetAllCollectionResultOfT.cs
@@ -58,8 +58,8 @@ namespace Azure.ResourceManager.MongoDBAtlas
                     yield break;
                 }
                 ClusterListResult result = ClusterListResult.FromResponse(response);
-                yield return Page<MongoDBAtlasClusterData>.FromValues((IReadOnlyList<MongoDBAtlasClusterData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 nextPage = result.NextLink;
+                yield return Page<MongoDBAtlasClusterData>.FromValues((IReadOnlyList<MongoDBAtlasClusterData>)result.Value, nextPage?.IsAbsoluteUri == true ? nextPage.AbsoluteUri : nextPage?.OriginalString, response);
                 if (nextPage == null)
                 {
                     yield break;
