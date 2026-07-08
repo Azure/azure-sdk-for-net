ece49a507322a5407bf2fc6cf713ee8c893fd8d7
Praful Johari <pjohari@microsoft.com>
2026-07-08 13:35:03 -0500

    Fix BOOL001: rename EnforceHierarchicalPartitionKeyIdLastLevel to IsHierarchicalPartitionKeyIdLastLevelEnforced via CodeGenMember customization

---
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Custom/Models/DatabaseAccountCreateUpdateProperties.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using Microsoft.TypeSpec.Generator.Customizations;
+
+namespace Azure.ResourceManager.CosmosDB.Models
+{
+    internal partial class DatabaseAccountCreateUpdateProperties
+    {
+        /// <summary> Flag to indicate enabling/disabling of hierarchical partition key ID last level enforcement on the account. </summary>
+        [CodeGenMember("EnforceHierarchicalPartitionKeyIdLastLevel")]
+        public bool? IsHierarchicalPartitionKeyIdLastLevelEnforced { get; set; }
+    }
+}
