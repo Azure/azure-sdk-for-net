d53ff4401429dd620877ab7044df8b37380f28ff
Jorge Rangel <jorgerangel@microsoft.com>
2026-07-23 14:44:53 -0500

    revert voicelive

--- sdk/voicelive/Azure.AI.VoiceLive/src/Customizations/VoiceProvider.cs
+++ sdk/voicelive/Azure.AI.VoiceLive/src/Customizations/VoiceProvider.cs
@@ -24,8 +24,6 @@ namespace Azure.AI.VoiceLive
                     case "azure-standard":
                     case "azure-personal":
                         return AzureVoice.DeserializeAzureVoice(element, options);
-                    case "azure-realtime-native":
-                        return AzureRealtimeNativeVoice.DeserializeAzureRealtimeNativeVoice(element, options);
                     default:
                         return null;
                 }
