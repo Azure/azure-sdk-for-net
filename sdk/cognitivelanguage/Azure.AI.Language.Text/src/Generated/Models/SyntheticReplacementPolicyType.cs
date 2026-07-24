699b84e92a6ff52ae2a91b4d504bb57f55558a19
azure-sdk <azuresdk@microsoft.com>
2026-07-24 06:43:19 +0000

    Configurations:  'specification/cognitiveservices/data-plane/LanguageAnalyzeText/tspconfig.yaml', API Version: 2026-05-15-preview, SDK Release Type: beta, and CommitSHA: '00f0e0e39d642c91cb706e0aa5bda169e9b84129' in SpecRepo: 'https://github.com/Azure/azure-rest-api-specs' Pipeline run: https://dev.azure.com/azure-sdk/internal/_build/results?buildId=6612206 Refer to https://eng.ms/docs/products/azure-developer-experience/develop/sdk-release/sdk-release-prerequisites to prepare for SDK release.

--- sdk/cognitivelanguage/Azure.AI.Language.Text/src/Generated/Models/SyntheticReplacementPolicyType.cs
+++ sdk/cognitivelanguage/Azure.AI.Language.Text/src/Generated/Models/SyntheticReplacementPolicyType.cs
@@ -22,10 +22,10 @@ namespace Azure.AI.Language.Text
         /// <param name="policyKind"> The entity RedactionPolicy object kind. </param>
         /// <param name="entityTypes"> (Optional) describes the PII categories to which the redaction policy will be applied. If not specified, the redaction policy will be applied to all PII categories. </param>
         /// <param name="policyName"> (Optional) name of the redaction policy for identification purposes. </param>
-        /// <param name="isDefaultPolicy"> (Optional) flag to indicate whether this redaction policy is the default policy to be applied when no specific policy is defined for a PII category. Only one policy can be marked as default. </param>
+        /// <param name="isDefault"> (Optional) flag to indicate whether this redaction policy is the default policy to be applied when no specific policy is defined for a PII category. Only one policy can be marked as default. </param>
         /// <param name="additionalBinaryDataProperties"> Keeps track of any properties unknown to the library. </param>
         /// <param name="preserveDataFormat"> Optional flag to indicate whether to preserve the original data format in the synthetic replacement. Default is false. </param>
-        internal SyntheticReplacementPolicyType(RedactionPolicyKind policyKind, IList<PiiCategoriesExclude> entityTypes, string policyName, bool? isDefaultPolicy, IDictionary<string, BinaryData> additionalBinaryDataProperties, bool? preserveDataFormat) : base(policyKind, entityTypes, policyName, isDefaultPolicy, additionalBinaryDataProperties)
+        internal SyntheticReplacementPolicyType(RedactionPolicyKind policyKind, IList<PiiCategoriesExclude> entityTypes, string policyName, bool? isDefault, IDictionary<string, BinaryData> additionalBinaryDataProperties, bool? preserveDataFormat) : base(policyKind, entityTypes, policyName, isDefault, additionalBinaryDataProperties)
         {
             PreserveDataFormat = preserveDataFormat;
         }
