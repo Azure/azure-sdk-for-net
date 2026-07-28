ac6c33cc27fd1bf7214ac03f6ba3db75a7a2a31d
azure-sdk <azuresdk@microsoft.com>
2026-07-28 17:35:57 +0000

    Update SDK code su_wo_17

--- sdk/support/Azure.ResourceManager.Support/src/Generated/Models/AzureResourceManagerSupportContext.cs
+++ sdk/support/Azure.ResourceManager.Support/src/Generated/Models/AzureResourceManagerSupportContext.cs
@@ -6,7 +6,6 @@
 #nullable disable
 
 using System.ClientModel.Primitives;
-using Azure;
 using Azure.ResourceManager.Models;
 using Azure.ResourceManager.Support.Models;
 
@@ -34,7 +33,6 @@ namespace Azure.ResourceManager.Support
     [ModelReaderWriterBuildable(typeof(ProblemClassificationsClassificationResult))]
     [ModelReaderWriterBuildable(typeof(ProblemClassificationsListResult))]
     [ModelReaderWriterBuildable(typeof(QuotaTicketDetails))]
-    [ModelReaderWriterBuildable(typeof(ResponseError))]
     [ModelReaderWriterBuildable(typeof(SecondaryConsent))]
     [ModelReaderWriterBuildable(typeof(SecondaryConsentEnabled))]
     [ModelReaderWriterBuildable(typeof(ServiceProperties))]
