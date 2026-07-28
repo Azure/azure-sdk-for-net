4af6fa3abd5975b2bbdc3588c29d9984f13aa0a9
Jocelyn <jaschrep@microsoft.com>
2026-07-28 14:16:38 -0400

    fix introduced CRC bug

--- sdk/storage/Azure.Storage.Blobs/src/PartitionedDownloader.cs
+++ sdk/storage/Azure.Storage.Blobs/src/PartitionedDownloader.cs
@@ -441,13 +441,14 @@ namespace Azure.Storage.Blobs
             foreach (Func<Task<Response<BlobDownloadStreamingResult>>> getResponse in allResponses)
             {
                 partitionChecksum.Clear();
-                await CopyToInternal(await getResponse().ConfigureAwait(false), destination, new(partitionChecksum, 0, _checksumSize), async, cancellationToken).ConfigureAwait(false);
+                long copied = await CopyToInternal(await getResponse().ConfigureAwait(false), destination, new(partitionChecksum, 0, _checksumSize), async, cancellationToken).ConfigureAwait(false);
                 if (UseMasterCrc)
                 {
                     StorageCrc64Composer.Compose(
                         (composedCrcBuf, 0L),
-                        (partitionChecksum, initialResponse.Value.Details.ContentRange.GetContentRangeLengthOrDefault()
-                            ?? initialResponse.Value.Details.ContentLength)
+                        //(partitionChecksum, initialResponse.Value.Details.ContentRange.GetContentRangeLengthOrDefault()
+                        //    ?? initialResponse.Value.Details.ContentLength)
+                        (partitionChecksum, copied)
                     ).AsSpan(0, Crc64Len).CopyTo(composedCrcBuf);
                 }
             }
@@ -492,7 +493,7 @@ namespace Azure.Storage.Blobs
             }
         }
 
-        private async Task CopyToInternal(
+        private async Task<long> CopyToInternal(
             Response<BlobDownloadStreamingResult> response,
             Stream destination,
             Memory<byte> checksumBuffer,
@@ -508,7 +509,7 @@ namespace Azure.Storage.Blobs
                 ? ChecksumCalculatingStream.GetReadStream(rawSource, hasher.AppendHash)
                 : rawSource;
 
-            await source.CopyToInternal(
+            long copied = await source.CopyToInternal(
                 destination,
                 async,
                 cancellationToken)
@@ -530,6 +531,8 @@ namespace Azure.Storage.Blobs
                     throw Errors.HashMismatchOnStreamedDownload(response.Value.Details.ContentRange);
                 }
             }
+
+            return copied;
         }
 
         /// <summary>
