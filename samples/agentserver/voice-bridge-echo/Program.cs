b0b7d9b0d0ae3ee2b6163d9fd4a01e8d7c718c72
wujin <wujin@microsoft.com>
2026-08-13 16:46:16 +0800

    remove sample

--- samples/agentserver/voice-bridge-echo/Program.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using Azure.AI.AgentServer.Invocations.Voice;
-using VoiceBridgeEcho;
-
-VoiceServer.Run<EchoVoiceHandler>(args);
