b2c388ce55ee251616285cbc11b3a84ee4f49be3
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2026-08-18 15:06:52 +0800

    Document ResourceHealth metadata customization

--- sdk/resourcehealth/Azure.Provisioning.ResourceHealth/src/Custom/ResourceHealthMetadataEntity.cs
+++ sdk/resourcehealth/Azure.Provisioning.ResourceHealth/src/Custom/ResourceHealthMetadataEntity.cs
@@ -8,6 +8,10 @@ using Microsoft.TypeSpec.Generator.Customizations;
 
 namespace Azure.Provisioning.ResourceHealth
 {
+    // The service's properties.dependsOn list is unrelated to ProvisionableResource.DependsOn,
+    // which controls Bicep deployment ordering. Rename only the flattened provisioning member
+    // to avoid hiding the inherited property. A shared TypeSpec rename would also change the
+    // DependsOn API already shipped by Azure.ResourceManager.ResourceHealth 1.1.0.
     public partial class ResourceHealthMetadataEntity
     {
         /// <summary> Gets the list of keys on which this entity depends. </summary>
