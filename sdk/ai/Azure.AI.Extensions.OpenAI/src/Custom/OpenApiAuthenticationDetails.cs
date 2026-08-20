7ee4e230272bc2434a76428bcc752933e1da35c3
nick863 <30440255+nick863@users.noreply.github.com>
2026-08-20 16:35:12 -0700

    Backporting first pass

---
+++ sdk/ai/Azure.AI.Extensions.OpenAI/src/Custom/OpenApiAuthenticationDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Azure.AI.Extensions.OpenAI;
+
+/// <summary>
+/// authentication details for OpenApiFunctionDefinition
+/// Please note this is the abstract base class. The derived classes available for instantiation are: <see cref="OpenApiAnonymousAuthenticationDetails"/>, <see cref="OpenApiProjectConnectionAuthenticationDetails"/>, and <see cref="OpenApiManagedAuthenticationDetails"/>.
+/// </summary>
+public abstract partial class OpenApiAuthenticationDetails
+{
+    /// <summary> Initializes a new instance of <see cref="OpenApiAuthenticationDetails"/>. </summary>
+    /// <param name="kind"> The type of authentication, must be anonymous/project_connection/managed_identity. </param>
+    protected OpenApiAuthenticationDetails(OpenApiAuthenticationKind kind)
+    {
+        Kind = kind;
+    }
+
+    /// <summary> The type of authentication, must be anonymous/project_connection/managed_identity. </summary>
+    protected OpenApiAuthenticationKind Kind { get; set; }
+}
