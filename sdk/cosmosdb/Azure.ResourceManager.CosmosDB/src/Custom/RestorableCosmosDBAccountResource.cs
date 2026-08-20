ce483f317891694a1d8ce69a733da642c46510af
WeiKang Lim <weikanglim@gmail.com>
2026-08-20 00:29:56 +0000

    Trigger twenty-package dependency experiment
    Apply the same behavior-neutral 20-package source changes used by the prior sparse-checkout timing experiment.

--- sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Custom/RestorableCosmosDBAccountResource.cs
+++ sdk/cosmosdb/Azure.ResourceManager.CosmosDB/src/Custom/RestorableCosmosDBAccountResource.cs
@@ -77,3 +77,5 @@ namespace Azure.ResourceManager.CosmosDB
             => GetRestorableMongoDBCollections(restorableMongoDBDatabaseRid, null, null, cancellationToken);
     }
 }
+
+// CI experiment: include this package in the 20-package timing workload.
