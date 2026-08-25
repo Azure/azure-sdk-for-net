185a89086de5062d3498a12a1006a92f463b40b8
Nicola Camillucci <ncamillucci@microsoft.com>
2026-08-25 15:12:05 +0100

    Fixed Analyze PRBatch

--- sdk/hardwaresecuritymodules/Azure.ResourceManager.HardwareSecurityModules/src/Custom/CloudHsmClusterSkuNameExtensions.cs
+++ sdk/hardwaresecuritymodules/Azure.ResourceManager.HardwareSecurityModules/src/Custom/CloudHsmClusterSkuNameExtensions.cs
@@ -7,7 +7,7 @@ using System;
 
 namespace Azure.ResourceManager.HardwareSecurityModules.Models
 {
-    internal static class CloudHsmClusterSkuNameExtensions
+    internal static partial class CloudHsmClusterSkuNameExtensions
     {
         public static string ToSerialString(this CloudHsmClusterSkuName value) => value switch
         {
@@ -23,13 +23,20 @@ namespace Azure.ResourceManager.HardwareSecurityModules.Models
 
         public static CloudHsmClusterSkuName ToCloudHsmClusterSkuName(this string value)
         {
-            if (string.Equals(value, "Standard_B1", StringComparison.InvariantCultureIgnoreCase)) return CloudHsmClusterSkuName.StandardB1;
-            if (string.Equals(value, "Standard B10", StringComparison.InvariantCultureIgnoreCase)) return CloudHsmClusterSkuName.StandardB10;
-            if (string.Equals(value, "Standard_B1v2", StringComparison.InvariantCultureIgnoreCase)) return CloudHsmClusterSkuName.StandardB1v2;
-            if (string.Equals(value, "Standard_B5v2", StringComparison.InvariantCultureIgnoreCase)) return CloudHsmClusterSkuName.StandardB5v2;
-            if (string.Equals(value, "Standard_B10v2", StringComparison.InvariantCultureIgnoreCase)) return CloudHsmClusterSkuName.StandardB10v2;
-            if (string.Equals(value, "Standard_B15v2", StringComparison.InvariantCultureIgnoreCase)) return CloudHsmClusterSkuName.StandardB15v2;
-            if (string.Equals(value, "Standard_B20v2", StringComparison.InvariantCultureIgnoreCase)) return CloudHsmClusterSkuName.StandardB20v2;
+            if (string.Equals(value, "Standard_B1", StringComparison.InvariantCultureIgnoreCase))
+                return CloudHsmClusterSkuName.StandardB1;
+            if (string.Equals(value, "Standard B10", StringComparison.InvariantCultureIgnoreCase))
+                return CloudHsmClusterSkuName.StandardB10;
+            if (string.Equals(value, "Standard_B1v2", StringComparison.InvariantCultureIgnoreCase))
+                return CloudHsmClusterSkuName.StandardB1v2;
+            if (string.Equals(value, "Standard_B5v2", StringComparison.InvariantCultureIgnoreCase))
+                return CloudHsmClusterSkuName.StandardB5v2;
+            if (string.Equals(value, "Standard_B10v2", StringComparison.InvariantCultureIgnoreCase))
+                return CloudHsmClusterSkuName.StandardB10v2;
+            if (string.Equals(value, "Standard_B15v2", StringComparison.InvariantCultureIgnoreCase))
+                return CloudHsmClusterSkuName.StandardB15v2;
+            if (string.Equals(value, "Standard_B20v2", StringComparison.InvariantCultureIgnoreCase))
+                return CloudHsmClusterSkuName.StandardB20v2;
             throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown CloudHsmClusterSkuName value.");
         }
     }
