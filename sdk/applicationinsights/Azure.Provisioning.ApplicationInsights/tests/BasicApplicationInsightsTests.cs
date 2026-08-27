7f2eeed47c53d1f208d8071b164dc725ac5d7e1a
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2026-08-27 14:16:52 +0800

    fix

--- sdk/applicationinsights/Azure.Provisioning.ApplicationInsights/tests/BasicApplicationInsightsTests.cs
+++ sdk/applicationinsights/Azure.Provisioning.ApplicationInsights/tests/BasicApplicationInsightsTests.cs
@@ -35,6 +35,60 @@ public class BasicApplicationInsightsTests
             });
     }
 
+    internal static Trycep CreateLinkedStorageAccountsTest()
+    {
+        return new Trycep().Define(
+            ctx =>
+            {
+                Infrastructure infra = new();
+
+                ApplicationInsightsComponent appInsights =
+                    new(nameof(appInsights))
+                    {
+                        Kind = "web",
+                        ApplicationType = ApplicationInsightsApplicationType.Web
+                    };
+                infra.Add(appInsights);
+
+                ComponentLinkedStorageAccounts linkedStorage =
+                    new(nameof(linkedStorage))
+                    {
+                        Name = "ServiceProfiler",
+                        Parent = appInsights,
+                        LinkedStorageAccount = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/storage"
+                    };
+                infra.Add(linkedStorage);
+
+                return infra;
+            });
+    }
+
+    internal static Trycep CreateWorkbookRevisionTest()
+    {
+        return new Trycep().Define(
+            ctx =>
+            {
+                Infrastructure infra = new();
+
+                ApplicationInsightsWorkbook workbook = ApplicationInsightsWorkbook.FromExisting(nameof(workbook));
+                workbook.Name = "00000000-0000-0000-0000-000000000000";
+                infra.Add(workbook);
+
+                ApplicationInsightsWorkbookRevision revision =
+                    new(nameof(revision))
+                    {
+                        Name = "revision-1",
+                        Parent = workbook,
+                        DisplayName = "Workbook revision",
+                        SerializedData = "{}",
+                        Category = "workbook"
+                    };
+                infra.Add(revision);
+
+                return infra;
+            });
+    }
+
     [Test]
     [Description("https://github.com/Azure/azure-quickstart-templates/blob/master/quickstarts/microsoft.web/function-app-create-dynamic/main.bicep")]
     public async Task CreateComponent()
@@ -59,5 +113,65 @@ public class BasicApplicationInsightsTests
 
             output appInsightsKey string = appInsights.properties.InstrumentationKey
             """);
+    }
+
+    [Test]
+    public async Task CreateLinkedStorageAccounts()
+    {
+        await using Trycep test = CreateLinkedStorageAccountsTest();
+        test.Compare(
+            string.Join(
+                "\n",
+                new[]
+                {
+                    "@description('The location for the resource(s) to be deployed.')",
+                    "param location string = resourceGroup().location",
+                    "",
+                    "resource appInsights 'Microsoft.Insights/components@2020-02-02' = {",
+                    "  name: take('appInsights-${uniqueString(resourceGroup().id)}', 260)",
+                    "  location: location",
+                    "  kind: 'web'",
+                    "  properties: {",
+                    "    Application_Type: 'web'",
+                    "  }",
+                    "}",
+                    "",
+                    "resource linkedStorage 'Microsoft.Insights/components/linkedStorageAccounts@2020-03-01-preview' = {",
+                    "  name: 'ServiceProfiler'",
+                    "  properties: {",
+                    "    linkedStorageAccount: '/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Storage/storageAccounts/storage'",
+                    "  }",
+                    "  parent: appInsights",
+                    "}"
+                }));
+    }
+
+    [Test]
+    public async Task CreateWorkbookRevision()
+    {
+        await using Trycep test = CreateWorkbookRevisionTest();
+        test.Compare(
+            string.Join(
+                "\n",
+                new[]
+                {
+                    "@description('The location for the resource(s) to be deployed.')",
+                    "param location string = resourceGroup().location",
+                    "",
+                    "resource workbook 'Microsoft.Insights/workbooks@2023-06-01' existing = {",
+                    "  name: '00000000-0000-0000-0000-000000000000'",
+                    "}",
+                    "",
+                    "resource revision 'Microsoft.Insights/workbooks/revisions@2023-06-01' = {",
+                    "  name: 'revision-1'",
+                    "  location: location",
+                    "  properties: {",
+                    "    displayName: 'Workbook revision'",
+                    "    serializedData: '{}'",
+                    "    category: 'workbook'",
+                    "  }",
+                    "  parent: workbook",
+                    "}"
+                }));
     }
 }
