214c02f50b68011e0c88b1359122641e6915b118
Dapeng Zhang <dapzhang@microsoft.com>
2026-09-11 13:32:23 +0800

    Migrate Azure.Provisioning.AppConfiguration to TypeSpec (#62814)
    * Migrate App Configuration provisioning to TypeSpec
    
    ---------
    
    Copilot-Session: 3edaff76-ef28-4774-816f-c173e37b2d58

---
+++ sdk/appconfiguration/Azure.Provisioning.AppConfiguration/src/Custom/AppConfigurationReplica.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Azure.Provisioning.AppConfiguration
+{
+    public partial class AppConfigurationReplica
+    {
+        /// <summary> Supported API versions retained for compatibility. </summary>
+        public static partial class ResourceVersions
+        {
+            /// <summary> API version "2024-06-01". </summary>
+            public static readonly string V2024_06_01 = "2024-06-01";
+            /// <summary> API version "2024-05-01". </summary>
+            public static readonly string V2024_05_01 = "2024-05-01";
+            /// <summary> API version "2023-03-01". </summary>
+            public static readonly string V2023_03_01 = "2023-03-01";
+        }
+    }
+}
