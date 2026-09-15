26b52777db85c6b67f740db4f5078c1ef9d81305
azure-sdk <azuresdk@microsoft.com>
2026-09-15 18:51:33 +0000

    Update SDK code co_co_4

--- sdk/contentsafety/Azure.AI.ContentSafety/src/Generated/Models/PagedTextBlocklistItem.Serialization.cs
+++ sdk/contentsafety/Azure.AI.ContentSafety/src/Generated/Models/PagedTextBlocklistItem.Serialization.cs
@@ -166,7 +166,7 @@ namespace Azure.AI.ContentSafety
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new PagedTextBlocklistItem(value, nextLink, additionalBinaryDataProperties);
--- sdk/contentsafety/Azure.AI.ContentSafety/src/Generated/Models/ShieldPromptResult.Serialization.cs
+++ sdk/contentsafety/Azure.AI.ContentSafety/src/Generated/Models/ShieldPromptResult.Serialization.cs
@@ -168,7 +168,7 @@ namespace Azure.AI.ContentSafety
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new ShieldPromptResult(userPromptAnalysis, documentsAnalysis ?? new ChangeTrackingList<DocumentInjectionAnalysisResult>(), additionalBinaryDataProperties);
