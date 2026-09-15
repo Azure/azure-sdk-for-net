be66298a49ca9e6307ee7f65bfc3728699ed03c6
azure-sdk <azuresdk@microsoft.com>
2026-09-15 18:51:03 +0000

    Update SDK code de_ea_6

--- sdk/deviceregistry/Azure.IoT.DeviceRegistry.SoftwareUpdate/src/Generated/Models/UpdateId.Serialization.cs
+++ sdk/deviceregistry/Azure.IoT.DeviceRegistry.SoftwareUpdate/src/Generated/Models/UpdateId.Serialization.cs
@@ -149,7 +149,7 @@ namespace Azure.IoT.DeviceRegistry._SoftwareUpdate
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new UpdateId(provider, name, version, additionalBinaryDataProperties);
--- sdk/discovery/Azure.AI.Discovery/src/Generated/Models/TaskResult.Serialization.cs
+++ sdk/discovery/Azure.AI.Discovery/src/Generated/Models/TaskResult.Serialization.cs
@@ -169,7 +169,7 @@ namespace Azure.AI.Discovery
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new TaskResult(text, storageAssetIds ?? new ChangeTrackingList<ResourceIdentifier>(), additionalBinaryDataProperties);
