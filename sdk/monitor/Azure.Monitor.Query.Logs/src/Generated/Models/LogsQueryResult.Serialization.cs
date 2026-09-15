834b2bd1797d51cddac23ac74fd136e032e3c15c
azure-sdk <azuresdk@microsoft.com>
2026-09-15 18:58:06 +0000

    Update SDK code mo_ng_11

--- sdk/monitor/Azure.Monitor.Query.Logs/src/Generated/Models/LogsQueryResult.Serialization.cs
+++ sdk/monitor/Azure.Monitor.Query.Logs/src/Generated/Models/LogsQueryResult.Serialization.cs
@@ -185,7 +185,7 @@ namespace Azure.Monitor.Query.Logs.Models
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new LogsQueryResult(allTables, error, statistics, visualization, additionalBinaryDataProperties);
--- sdk/monitor/Azure.Monitor.Query.Metrics/src/Generated/Models/MetricsQueryResourcesResult.Serialization.cs
+++ sdk/monitor/Azure.Monitor.Query.Metrics/src/Generated/Models/MetricsQueryResourcesResult.Serialization.cs
@@ -154,7 +154,7 @@ namespace Azure.Monitor.Query.Metrics.Models
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new MetricsQueryResourcesResult(values ?? new ChangeTrackingList<MetricsQueryResult>(), additionalBinaryDataProperties);
