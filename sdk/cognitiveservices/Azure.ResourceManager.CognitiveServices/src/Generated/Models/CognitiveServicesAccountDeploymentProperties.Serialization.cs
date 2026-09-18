9aa94031a1864cc3872463a10c4ce83fa2343586
Arthur Ma <arthurma@microsoft.com>
2026-09-18 08:34:39 +0800

    Update

--- sdk/cognitiveservices/Azure.ResourceManager.CognitiveServices/src/Generated/Models/CognitiveServicesAccountDeploymentProperties.Serialization.cs
+++ sdk/cognitiveservices/Azure.ResourceManager.CognitiveServices/src/Generated/Models/CognitiveServicesAccountDeploymentProperties.Serialization.cs
@@ -84,6 +84,11 @@ namespace Azure.ResourceManager.CognitiveServices.Models
                 writer.WritePropertyName("model"u8);
                 writer.WriteObjectValue(Model, options);
             }
+            if (Optional.IsDefined(ContextCacheContainerId))
+            {
+                writer.WritePropertyName("contextCacheContainerId"u8);
+                writer.WriteStringValue(ContextCacheContainerId);
+            }
             if (Optional.IsDefined(ScaleSettings))
             {
                 writer.WritePropertyName("scaleSettings"u8);
@@ -214,6 +219,7 @@ namespace Azure.ResourceManager.CognitiveServices.Models
             }
             CognitiveServicesAccountDeploymentProvisioningState? provisioningState = default;
             CognitiveServicesAccountDeploymentModel model = default;
+            string contextCacheContainerId = default;
             CognitiveServicesAccountDeploymentScaleSettings scaleSettings = default;
             IReadOnlyDictionary<string, string> capabilities = default;
             string raiPolicyName = default;
@@ -247,6 +253,11 @@ namespace Azure.ResourceManager.CognitiveServices.Models
                         continue;
                     }
                     model = CognitiveServicesAccountDeploymentModel.DeserializeCognitiveServicesAccountDeploymentModel(prop.Value, options);
+                    continue;
+                }
+                if (prop.NameEquals("contextCacheContainerId"u8))
+                {
+                    contextCacheContainerId = prop.Value.GetString();
                     continue;
                 }
                 if (prop.NameEquals("scaleSettings"u8))
@@ -390,6 +401,7 @@ namespace Azure.ResourceManager.CognitiveServices.Models
             return new CognitiveServicesAccountDeploymentProperties(
                 provisioningState,
                 model,
+                contextCacheContainerId,
                 scaleSettings,
                 capabilities ?? new ChangeTrackingDictionary<string, string>(),
                 raiPolicyName,
