916624df91f247c0612f594195e3da72ca9a6702
huliang-microsoft <54422042+huliang-microsoft@users.noreply.github.com>
2026-09-18 17:36:45 -0700

    Regenerate Content Safety serialization code (#63215)

--- sdk/contentsafety/Azure.AI.ContentSafety/src/Generated/Models/AcsVerdict.Serialization.cs
+++ sdk/contentsafety/Azure.AI.ContentSafety/src/Generated/Models/AcsVerdict.Serialization.cs
@@ -273,7 +273,7 @@ namespace Azure.AI.ContentSafety
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new AcsVerdict(
