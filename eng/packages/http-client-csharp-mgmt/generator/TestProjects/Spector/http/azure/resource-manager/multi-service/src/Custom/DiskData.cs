ce99801122ff90f9d27640fa24cced3efa5603bc
live1206 <live1206@users.noreply.github.com>
2026-09-20 04:29:31 +0000

    Restore multi-service Spector customizations

---
+++ eng/packages/http-client-csharp-mgmt/generator/TestProjects/Spector/http/azure/resource-manager/multi-service/src/Custom/DiskData.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using Azure.Core;
+using Azure.ResourceManager.Models;
+using Azure.ResourceManager.MultiService.Combined.Models;
+using Microsoft.TypeSpec.Generator.Customizations;
+
+namespace Azure.ResourceManager.MultiService.Combined
+{
+    [CodeGenSuppress("DiskData", typeof(AzureLocation))]
+    [CodeGenSuppress("DiskData", typeof(string), typeof(string), typeof(ResourceType), typeof(SystemData), typeof(IDictionary<string, string>), typeof(AzureLocation), typeof(DiskProperties), typeof(IDictionary<string, BinaryData>))]
+    public partial class DiskData
+    {
+        /// <summary> Initializes a new instance of <see cref="DiskData"/>. </summary>
+        /// <param name="location"> The geo-location where the resource lives. </param>
+        public DiskData(AzureLocation location) : base(location)
+        {
+            Properties = new DiskProperties();
+        }
+
+        internal DiskData(string id, string name, ResourceType resourceType, SystemData systemData, IDictionary<string, string> tags, AzureLocation location, DiskProperties properties, IDictionary<string, BinaryData> additionalBinaryDataProperties)
+            : base(id is null ? null : new ResourceIdentifier(id), name, resourceType, systemData, tags, location)
+        {
+            Properties = properties;
+            _additionalBinaryDataProperties = additionalBinaryDataProperties;
+        }
+    }
+}
