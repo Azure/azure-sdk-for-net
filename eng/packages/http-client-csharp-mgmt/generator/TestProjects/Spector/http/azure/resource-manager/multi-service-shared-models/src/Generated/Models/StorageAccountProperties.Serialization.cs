d9864f877503fff1f487c40b804763bba314f670
live1206 <huwe@microsoft.com>
2026-09-23 05:49:12 +0000

    Regenerate management Spector test projects

--- eng/packages/http-client-csharp-mgmt/generator/TestProjects/Spector/http/azure/resource-manager/multi-service-shared-models/src/Generated/Models/StorageAccountProperties.Serialization.cs
+++ eng/packages/http-client-csharp-mgmt/generator/TestProjects/Spector/http/azure/resource-manager/multi-service-shared-models/src/Generated/Models/StorageAccountProperties.Serialization.cs
@@ -152,7 +152,7 @@ namespace Azure.ResourceManager.MultiServiceSharedModels.Combined.Models
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new StorageAccountProperties(provisioningState, metadata, additionalBinaryDataProperties);
