ca0eb25f0b8fa81b5cbfa70dce72fa5b0eea786d
Dapeng Zhang <dapzhang@microsoft.com>
2026-09-24 11:44:00 +0800

    Bump management generator base version (#62963)

--- eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/Models/OptionalFlattenPropertyType.Serialization.cs
+++ eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/Models/OptionalFlattenPropertyType.Serialization.cs
@@ -156,7 +156,7 @@ namespace Azure.Generator.MgmtTypeSpec.Tests.Models
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new OptionalFlattenPropertyType(randomCollectionProp, additionalBinaryDataProperties);
--- eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/Models/SAPAvailabilityZoneDetailsResult.Serialization.cs
+++ eng/packages/http-client-csharp-mgmt/generator/TestProjects/Local/Mgmt-TypeSpec/src/Generated/Models/SAPAvailabilityZoneDetailsResult.Serialization.cs
@@ -142,7 +142,7 @@ namespace Azure.Generator.MgmtTypeSpec.Tests.Models
                 }
                 if (options.Format != "W")
                 {
-                    additionalBinaryDataProperties.Add(prop.Name, BinaryData.FromString(prop.Value.GetRawText()));
+                    additionalBinaryDataProperties.Add(prop.Name, prop.Value.GetUtf8Bytes());
                 }
             }
             return new SAPAvailabilityZoneDetailsResult(recommendedAvailabilityZonePair, additionalBinaryDataProperties);
